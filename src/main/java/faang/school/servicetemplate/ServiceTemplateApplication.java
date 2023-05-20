f950c56f5c089e6319d4300ce706c94d5dfdbfc8
mighty-mhsl <61280870+mighty-mhsl@users.noreply.github.com>
2023-05-20 12:21:52 +0200

    Initial commit

---
+++ src/main/java/faang/school/servicetemplate/ServiceTemplateApplication.java
@@ -0,0 +1,18 @@
+package faang.school.servicetemplate;
+
+import org.springframework.boot.Banner;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+@SpringBootApplication
+@EnableScheduling
+@EnableFeignClients("school.faang.servicetemplate.client")
+public class ServiceTemplateApplication {
+    public static void main(String[] args) {
+        new SpringApplicationBuilder(ServiceTemplateApplication.class)
+                .bannerMode(Banner.Mode.OFF)
+                .run(args);
+    }
+}
---
+++ src/main/java/faang/school/servicetemplate/client/ProjectServiceClient.java
@@ -0,0 +1,9 @@
+package faang.school.servicetemplate.client;
+
+
+import org.springframework.cloud.openfeign.FeignClient;
+
+@FeignClient(name = "project-service", url = "${project-service.host}:${project-service.port}")
+public interface ProjectServiceClient {
+
+}
