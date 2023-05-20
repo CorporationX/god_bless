ebb57e132360acc50e8d127161386c1f7e7660f7
mighty-mhsl <mihusle@protonmail.com>
2023-05-20 12:46:58 +0200

    Introduced Module 1 god bless project

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,8 +1,5 @@
 plugins {
     java
-    id("org.springframework.boot") version "3.0.6"
-    id("io.spring.dependency-management") version "1.1.0"
-
 }
 
 group = "faang.school"
@@ -15,24 +12,6 @@ repositories {
 
 dependencies {
     /**
-     * Spring boot starters
-     */
-    implementation("org.springframework.boot:spring-boot-starter-data-jdbc")
-    implementation("org.springframework.boot:spring-boot-starter-data-jpa")
-    implementation("org.springframework.boot:spring-boot-starter-data-redis")
-    implementation("org.springframework.boot:spring-boot-starter-web")
-    implementation("org.springframework.boot:spring-boot-starter-validation")
-    implementation("org.springframework.cloud:spring-cloud-starter-openfeign:4.0.2")
-    annotationProcessor("org.springframework.boot:spring-boot-configuration-processor")
-
-    /**
-     * Database
-     */
-    implementation("org.liquibase:liquibase-core")
-    implementation("redis.clients:jedis:4.3.2")
-    runtimeOnly("org.postgresql:postgresql")
-
-    /**
      * Utils & Logging
      */
     implementation("com.fasterxml.jackson.core:jackson-databind:2.14.2")
@@ -40,22 +19,12 @@ dependencies {
     implementation("ch.qos.logback:logback-classic:1.4.6")
     implementation("org.projectlombok:lombok:1.18.26")
     annotationProcessor("org.projectlombok:lombok:1.18.26")
-    implementation("org.mapstruct:mapstruct:1.5.3.Final")
-    annotationProcessor("org.mapstruct:mapstruct-processor:1.5.3.Final")
-
-    /**
-     * Test containers
-     */
-    implementation(platform("org.testcontainers:testcontainers-bom:1.17.6"))
-    testImplementation("org.testcontainers:junit-jupiter")
-    testImplementation("org.testcontainers:postgresql")
 
     /**
      * Tests
      */
     testImplementation("org.junit.jupiter:junit-jupiter-params:5.9.2")
     testImplementation("org.assertj:assertj-core:3.24.2")
-    testImplementation("org.springframework.boot:spring-boot-starter-test")
 }
 
 tasks.withType<Test> {
--- src/main/java/faang/school/servicetemplate/client/ProjectServiceClient.java
+++
@@ -1,9 +0,0 @@
-package faang.school.servicetemplate.client;
-
-
-import org.springframework.cloud.openfeign.FeignClient;
-
-@FeignClient(name = "project-service", url = "${project-service.host}:${project-service.port}")
-public interface ProjectServiceClient {
-
-}
