5d72f8cb1788a7381640316434b392fc22c1f77b
Alexander Kolesnikov <kolesnikov.kite@gmail.com>
2023-06-11 19:20:31 +0100

    Added method validateInputs Method is checking that all the inputs are corresponding to the condition of the task. Otherwise, it throws IllegalArgumentException with explanation what went wrong.

--- src/main/java/faang/school/godbless/groupusers/User.java
+++ src/main/java/faang/school/godbless/groupusers/User.java
@@ -2,31 +2,52 @@ package faang.school.godbless.groupusers;
 
 import lombok.Getter;
 
+import java.util.Arrays;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
+import java.util.Set;
 import java.util.stream.Collectors;
 
 @Getter
 public class User {
 
+    private final Set<String> VALID_JOBS = new HashSet<>(Arrays.asList("Google", "Uber", "Amazon"));
+    private final Set<String> VALID_ADDRESSES = new HashSet<>(Arrays.asList("London", "New York", "Amsterdam"));
     private String name;
     private int age;
     private String placeOfWork;
     private String address;
 
     public User(String name, int age, String placeOfWork, String address) {
+        validateInputs(name, age, placeOfWork, address);
+
         this.name = name;
         this.placeOfWork = placeOfWork;
         this.address = address;
-        if(age < 0){
-            throw new IllegalArgumentException("Age cant be less then 0");
-        }else{
-            this.age = age;
-        }
+        this.age = age;
     }
 
     public static Map<Integer, List<User>> groupUsers(List<User> users) {
         return users.stream()
                 .collect(Collectors.groupingBy(User::getAge));
+    }
+
+    private void validateInputs(String name, int age, String placeOfWork, String address) {
+        if (name.isBlank()) {
+            throw new IllegalArgumentException("Name cannot be empty");
+        }
+
+        if (age < 18) {
+            throw new IllegalArgumentException("Age cannot be less than 18");
+        }
+
+        if (!VALID_JOBS.contains(placeOfWork)) {
+            throw new IllegalArgumentException("Invalid place of work");
+        }
+
+        if (!VALID_ADDRESSES.contains(address)) {
+            throw new IllegalArgumentException("Invalid address");
+        }
     }
 }
