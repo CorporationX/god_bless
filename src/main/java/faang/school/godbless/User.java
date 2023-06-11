af1ae2d5e1cb152a8ab19097f9c175779b2ccb41
Pavel Voedilov <voedilov.p@gmail.com>
2023-06-11 16:14:03 +0200

    - Add VALID_JOBS & VALID_ADDRESSES to User.java - Add constructor with checks to User.java
    - Add jupiter-engine:5.9.2 dependency
    - Add UserTest that tests User constructor behavior

---
+++ src/main/java/faang/school/godbless/User.java
@@ -0,0 +1,62 @@
+package faang.school.godbless;
+
+import lombok.Builder;
+import lombok.Data;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
+@Data
+@Builder
+public class User {
+
+    private String name;
+
+    private Integer age;
+
+    private String company;
+
+    private String address;
+
+    private static final Set<String> VALID_JOBS = Set.of("Google", "Uber", "Amazon");
+
+    private static final Set<String> VALID_ADDRESSES = Set.of("London", "New York", "Amsterdam");
+
+    private static final int LEGAL_WORKING_AGE = 18;
+
+
+    public User(String name, Integer age, String company, String address) {
+
+        if (name.isBlank() || age < LEGAL_WORKING_AGE || !VALID_JOBS.contains(company)
+                || !VALID_ADDRESSES.contains(address)) {
+            throw new IllegalArgumentException("Please, enter only valid values");
+        }
+
+        this.name = name;
+        this.age = age;
+        this.company = company;
+        this.address = address;
+    }
+
+    public static Map<Integer, List<User>> groupUsersByAge(List<User> users) {
+
+        Map<Integer, List<User>> usersMap = new HashMap<>();
+
+        for (User user : users) {
+            Integer userAge = user.getAge();
+
+            if (!usersMap.containsKey(userAge)) {
+                usersMap.put(userAge, new ArrayList<User>());
+            }
+
+            usersMap.get(userAge).add(user);
+        }
+        
+        return usersMap;
+    }
+}
