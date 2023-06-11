05f1a8edb8b2fb3fde04af87ac1306dad8bc7b09
Basimov Rustam <sombra1nf@yandex.ru>
2023-06-11 20:04:00 +0500

    Создал класс Юзер

---
+++ src/main/java/faang/school/godbless/classes/User.java
@@ -0,0 +1,15 @@
+package faang.school.godbless.classes;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+public class User {
+    private String name;
+    private int age;
+    private String jobLocate;
+    private String address;
+}
