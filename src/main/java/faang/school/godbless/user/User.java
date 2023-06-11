1553a7ef563f8e464e9ba73ee4e93398ea61a377
Serhii Rubets <serhii.rubets@gmail.com>
2023-06-11 14:37:05 -0500

    Added private, get, set improved variables name moved tests to other place

--- src/main/java/faang/school/godbless/user/User.java
+++ src/main/java/faang/school/godbless/user/User.java
@@ -6,34 +6,64 @@ import java.util.List;
 import java.util.Map;
 
 public class User {
-    String name;
-    int age;
-    String job;
-    String address;
+  private String name;
+  private int age;
+  private String job;
+  private String address;
 
-    public User(String name, int age, String job, String address) {
-        this.name = name;
-        this.age = age;
-        this.job = job;
-        this.address = address;
+  public User(String name, int age, String job, String address) {
+    this.name = name;
+    this.age = age;
+    this.job = job;
+    this.address = address;
+  }
+
+  public static Map<Integer, List<User>> groupUsers(List<User> users) {
+    Map<Integer, List<User>> userAgeToUsers = new HashMap<Integer, List<User>>();
+
+    for (User user : users) {
+      if (userAgeToUsers.containsKey(user.age)) {
+        userAgeToUsers.get(user.age).add(user);
+      } else {
+        List<User> newUsers = new ArrayList<User>();
+        newUsers.add(user);
+
+        userAgeToUsers.put(user.age, newUsers);
+      }
     }
 
-    public static Map<Integer, List<User>> groupUsers(List<User> users) {
-        Map<Integer, List<User>> userAgeToUsers = new HashMap<Integer, List<User>>();
+    return userAgeToUsers;
+  }
 
-        for (User user : users) {
-            int age = user.age;
+  public String getName() {
+    return name;
+  }
 
-            if (userAgeToUsers.get(age) instanceof ArrayList<User>) {
-                userAgeToUsers.get(age).add(user);
-            } else {
-                List<User> newUsers = new ArrayList<User>();
-                newUsers.add(user);
+  public int getAge() {
+    return age;
+  }
 
-                userAgeToUsers.put(age, newUsers);
-            }
-        }
+  public String getJob() {
+    return job;
+  }
 
-        return userAgeToUsers;
-    }
+  public String getAddress() {
+    return address;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
+
+  public void setAge(int age) {
+    this.age = age;
+  }
+
+  public void setJob(String job) {
+    this.job = job;
+  }
+
+  public void setAddress(String address) {
+    this.address = address;
+  }
 }
