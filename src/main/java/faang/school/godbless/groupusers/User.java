8fa06125732ade3b314678dff246aba1d747725c
Alexander Kolesnikov <kolesnikov.kite@gmail.com>
2023-06-11 16:21:29 +0100

    For PR

---
+++ src/main/java/faang/school/godbless/groupusers/User.java
@@ -0,0 +1,28 @@
+package faang.school.godbless.groupusers;
+
+import lombok.Getter;
+
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+@Getter
+public class User {
+
+    private String name;
+    private int age;
+    private String placeOfWork;
+    private String address;
+
+    public User(String name, int age, String placeOfWork, String address) {
+        this.name = name;
+        this.age = age;
+        this.placeOfWork = placeOfWork;
+        this.address = address;
+    }
+
+    public static Map<Integer, List<User>> groupUsers(List<User> users) {
+        return users.stream()
+                .collect(Collectors.groupingBy(User::getAge));
+    }
+}
