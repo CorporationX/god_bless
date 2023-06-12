201486ef159ef24fb886da6f19cbaabae59f37dc
qween <youlka25@yandex.ru>
2023-06-12 12:07:37 +0300

    Тесты для User factory

--- src/main/java/faang/school/godbless/User.java
+++ src/main/java/faang/school/godbless/User.java
@@ -1,14 +1,13 @@
 package faang.school.godbless;
 
-import lombok.AllArgsConstructor;
 import lombok.Data;
 
 import java.util.List;
 import java.util.Map;
+import java.util.Set;
 import java.util.stream.Collectors;
 
 @Data
-@AllArgsConstructor
 public class User {
     private final String name;
 
@@ -16,7 +15,26 @@ public class User {
     private String workplace;
     private String address;
 
+    static final Set<String> VALID_JOBS = Set.of("Google", "Uber", "Amazon");
+    static final Set<String> VALID_ADDRESSES = Set.of("London", "New York", "Amsterdam");
+    static final int LEGAL_AGE = 18;
+
+    public User(String name, int age, String workplace, String address) {
+        if (name.isBlank() || age < LEGAL_AGE || !VALID_JOBS.contains(workplace) || !VALID_ADDRESSES.contains(address)) {
+            throw new IllegalArgumentException();
+        } else {
+            this.name = name;
+            this.age = age;
+            this.workplace = workplace;
+            this.address = address;
+        }
+    }
+
     public static Map<Integer, List<User>> groupUsers(List<User> users) {
         return users.stream().collect(Collectors.groupingBy(User::getAge));
+    }
+
+    public static User create(String name, int age, String workplace, String address) {
+        return new User(name, age, workplace, address);
     }
 }
--- src/test/java/faang/school/goodbless/UserTest.java
+++ src/test/java/faang/school/goodbless/UserTest.java
@@ -7,14 +7,15 @@ import java.util.List;
 import java.util.Map;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 class UserTest {
 
     @Test
     void groupUsersSuccessTest() {
-        User user1 = new User("Ivan", 25, "Work1", "Address1");
-        User user2 = new User("Egor", 25, "Work1", "Address2");
-        User user3 = new User("Eva", 30, "Work1", "Address3");
+        User user1 = new User("Ivan", 25, "Amazon", "London");
+        User user2 = new User("Egor", 25, "Uber", "Amsterdam");
+        User user3 = new User("Eva", 30, "Google", "New York");
 
         Map<Integer, List<User>> expectedMap = Map.of(
                 25, List.of(user1, user2),
@@ -22,5 +23,32 @@ class UserTest {
         );
 
         assertEquals(expectedMap, User.groupUsers(List.of(user1, user2, user3)));
+    }
+
+    @Test
+    void successUserTest() {
+        User user = new User("Ivan", 20, "Amazon", "London");
+
+        assertEquals(user, User.create("Ivan", 20, "Amazon", "London"));
+    }
+
+    @Test
+    void wrongAddressUserTest() {
+        assertThrows(IllegalArgumentException.class, () -> User.create("Ivan", 25, "Uber", "Address1"));
+    }
+
+    @Test
+    void wrongWorkplaceUserTest() {
+        assertThrows(IllegalArgumentException.class, () -> User.create("Ivan", 25, "Work", "London"));
+    }
+
+    @Test
+    void wrongAgeUserTest() {
+        assertThrows(IllegalArgumentException.class, () -> User.create("Ivan", 15, "Amazon", "London"));
+    }
+
+    @Test
+    void wrongNameUserTest() {
+        assertThrows(IllegalArgumentException.class, () -> User.create("", 15, "Uber", "London"));
     }
 }
