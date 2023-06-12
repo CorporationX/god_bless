240b30da510ef3cab899534277a02ed44da32a8d
Basimov Rustam <sombra1nf@yandex.ru>
2023-06-12 19:31:05 +0500

    Добавил абстрактный класс и двух его наследников. Реализовал методы

---
+++ src/main/java/faang/school/godbless/classes/Warrior.java
@@ -0,0 +1,15 @@
+package faang.school.godbless.classes;
+
+public class Warrior extends Character{
+    public Warrior(String name){
+        super(name, 10, 5, 3);
+    }
+
+    @Override
+    public void attack(Character character) {
+        character.setHealth(
+                character.getHealth()-this.getStrength()
+        );
+    }
+
+}
