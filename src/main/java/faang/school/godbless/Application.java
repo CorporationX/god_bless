0533e02246df383c05f03c134b3260beee3df76b
Dmitry Sorokin <dm.sorokin21@gmail.com>
2023-06-12 14:28:16 +0300

    completed task reverse array

--- src/main/java/faang/school/godbless/Application.java
+++ src/main/java/faang/school/godbless/Application.java
@@ -1,7 +1,12 @@
 package faang.school.godbless;
 
+import java.util.Arrays;
+
 public class Application {
     public static void main(String... args) {
-
+        int[] numbers = new int[]{1, 2, 3, 4, 5, 6};
+        System.out.println(Arrays.toString(numbers));
+        Example.reverse(numbers);
+        System.out.println(Arrays.toString(numbers));
     }
 }
