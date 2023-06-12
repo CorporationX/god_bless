831a3346a29c269f89f1f749bad4dbf9b3afce4f
ipavelbel <ilpavel94@gmail.com>
2023-06-12 12:58:40 +0200

    added User class implementation

---
+++ src/main/java/faang/school/godbless/users_by_age/User.java
@@ -0,0 +1,22 @@
+package faang.school.godbless.users_by_age;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+@Data
+@AllArgsConstructor
+public class User {
+
+    private String name;
+    private int age;
+    private String placeOfWork;
+    private String address;
+
+    public static Map<Integer, List<User>> groupUsers(List<User> users) {
+        return users.stream().collect(Collectors.groupingBy(User::getAge));
+    }
+}
