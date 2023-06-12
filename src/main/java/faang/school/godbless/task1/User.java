886de1e2ecfbb6aff3782118566fce52102d7139
Sergey Gerasimov <czar997@yandex.ru>
2023-06-12 23:31:25 +0300

    добавлен класс User

---
+++ src/main/java/faang/school/godbless/task1/User.java
@@ -0,0 +1,30 @@
+package faang.school.godbless.task1;
+
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+@AllArgsConstructor
+@Getter
+@Setter
+@EqualsAndHashCode
+public class User {
+
+    private String name;
+
+    private int age;
+
+    private String place_of_work;
+
+    private String address;
+
+    public static Map<Integer, List<User>> groupUsers(List<User> users) {
+        return users.stream()
+                .collect(Collectors.groupingBy(User::getAge, Collectors.toList()));
+    }
+}
