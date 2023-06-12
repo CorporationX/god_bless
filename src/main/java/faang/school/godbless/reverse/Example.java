223a8bb043df273d42ac3f0a3a132555f95fb1fd
Alexander Kolesnikov <kolesnikov.kite@gmail.com>
2023-06-12 10:44:22 +0100

    Complete "Turn around!" task Wrote algorithm to reverse the array

---
+++ src/main/java/faang/school/godbless/reverse/Example.java
@@ -0,0 +1,15 @@
+package faang.school.godbless.reverse;
+
+public class Example {
+
+    public static void reverse(int[] array) {
+        int j = array.length - 1;
+
+        for (int i = 0; i < array.length / 2; i++) {
+            int temp = array[j];
+            array[j] = array[i];
+            array[i] = temp;
+            j--;
+        }
+    }
+}
