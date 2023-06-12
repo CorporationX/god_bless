cef7316842613f6cb164ca838ec5d4b40e53d7b2
Pashkevich <ppavel012100@gmail.com>
2023-06-12 10:40:45 +0200

    Added registration user and reworked groupUsers method

--- src/main/java/faang/school/godbless/task1/User.java
+++ src/main/java/faang/school/godbless/task1/User.java
@@ -1,31 +1,65 @@
 package faang.school.godbless.task1;
 
-import lombok.AllArgsConstructor;
+
 import lombok.Data;
 
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Set;
 
 @Data
-@AllArgsConstructor
 public class User {
     private String name;
     private int age;
     private String workPlace;
     private String address;
 
+    public static final Set<String> VALID_JOBS = Set.of("Google", "Uber", "Amazon");
+    public static final Set<String> VALID_ADDRESSES = Set.of("London", "New York", "Amsterdam");
+
+    public User(String name, int age, String workPlace, String address) {
+        validName(name);
+        validAge(age);
+        validWorkPlace(workPlace);
+        validAddresses(address);
+
+        this.name = name;
+        this.age = age;
+        this.workPlace = workPlace;
+        this.address = address;
+    }
+
+    private void validName(String name) {
+        if (name == null || name.trim().isEmpty()) {
+            throw new IllegalArgumentException();
+        }
+    }
+
+    private void validAge(int age) {
+        if (age < 18) {
+            throw new IllegalArgumentException();
+        }
+    }
+
+    private void validWorkPlace(String workPlace) {
+        if (!VALID_JOBS.contains(workPlace)) {
+            throw new IllegalArgumentException();
+        }
+    }
+
+    private void validAddresses(String address) {
+        if (!VALID_ADDRESSES.contains(address)) {
+            throw new IllegalArgumentException();
+        }
+    }
+
     public static Map<Integer, List<User>> groupUsers(List<User> listUsers) {
         Map<Integer, List<User>> sortByAge = new HashMap<>();
         for (User user : listUsers) {
-            if (sortByAge.containsKey(user.getAge())) {
-                sortByAge.get(user.getAge()).add(user);
-            } else {
-                ArrayList<User> usersSameAge = new ArrayList<>();
-                usersSameAge.add(user);
-                sortByAge.put(user.getAge(), usersSameAge);
-            }
+            int age = user.getAge();
+            sortByAge.computeIfAbsent(age, c -> new ArrayList<>()).add(user);
         }
         return sortByAge;
     }
