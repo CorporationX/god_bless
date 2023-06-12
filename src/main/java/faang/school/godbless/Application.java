03b68d8c9dd38f0de06fce5ed7623acc91497e46
Smurfetti <matveynikishaev@gmail.com>
2023-06-12 16:26:46 +0300

    Implemented first intro task (groupUsers)

--- src/main/java/faang/school/godbless/Application.java
+++ src/main/java/faang/school/godbless/Application.java
@@ -1,7 +1,15 @@
 package faang.school.godbless;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Application {
     public static void main(String... args) {
-
+        List<User> users = new ArrayList<>();
+        for (int i = 0; i < 10; i++) {
+            users.add(new User(Integer.toString(i),i%3,Integer.toString(i),Integer.toString(i)));
+        }
+        System.out.println(users);
+        System.out.println(User.groupUsers(users));
     }
 }
