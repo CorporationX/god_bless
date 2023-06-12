2964bc7a331da8f4458124ce6a99c24c25d37afe
Smurfetti <matveynikishaev@gmail.com>
2023-06-12 17:09:33 +0300

    Implemented second into task (registration)

--- src/main/java/faang/school/godbless/Application.java
+++ src/main/java/faang/school/godbless/Application.java
@@ -1,7 +1,21 @@
 package faang.school.godbless;
 
+import java.util.ArrayList;
+import java.util.List;
+
+
 public class Application {
     public static void main(String... args) {
-
+        List<User> users = new ArrayList<>();
+        for (int i = 0; i < 10; i++) {
+            try {
+                users.add(new User(Integer.toString(i),i%3,Integer.toString(i),Integer.toString(i)));
+            } catch (IllegalArgumentException e){
+                System.out.println(e.getMessage());
+            }
+        }
+        System.out.println(users);
+        System.out.println(User.groupUsers(users));
     }
 }
+
