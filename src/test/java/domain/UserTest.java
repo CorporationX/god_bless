3beaa0e45406fa219e3c0d37ac66448fe278f709
Dregid <ufl852@gmail.com>
2023-06-12 13:56:24 +0600

    Исправлены тесты, удаленна проверка на пустую коллекцию

--- src/test/java/domain/UserTest.java
+++ src/test/java/domain/UserTest.java
@@ -1,6 +1,6 @@
 package domain;
 
-import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 import java.util.ArrayList;
@@ -11,25 +11,26 @@ import java.util.Map;
 public class UserTest {
     @Test
     public void shouldReturnSortedMapByAge() {
-        Map<Integer, List<User>> usersGroup = User.groupUsers(getUsers());
-        Assertions.assertThat(usersGroup).isNotEmpty();
+        List<User> usersGroup = getUsers();
+        Map<Integer, List<User>> receivedSortUsers = User.groupUsers(usersGroup);
 
-        Assertions.assertThat(getUsers().get(0).getAge()).isEqualTo(23);
-        Assertions.assertThat(getUsers().get(1).getAge()).isEqualTo(23);
-        Assertions.assertThat(getUsers().get(2).getAge()).isEqualTo(19);
-        Assertions.assertThat(getUsers().get(3).getAge()).isEqualTo(19);
-        Assertions.assertThat(getUsers().get(4).getAge()).isEqualTo(25);
-        Assertions.assertThat(getUsers().get(5).getAge()).isEqualTo(29);
+        Assertions.assertEquals(usersGroup.get(0), receivedSortUsers.get(23).get(0));
+        Assertions.assertEquals(usersGroup.get(1), receivedSortUsers.get(23).get(1));
+        Assertions.assertEquals(usersGroup.get(2), receivedSortUsers.get(19).get(0));
+        Assertions.assertEquals(usersGroup.get(3), receivedSortUsers.get(19).get(1));
+        Assertions.assertEquals(usersGroup.get(4), receivedSortUsers.get(25).get(0));
+        Assertions.assertEquals(usersGroup.get(5), receivedSortUsers.get(29).get(0));
+
     }
 
     private static List<User> getUsers() {
         List<User> users = new ArrayList<>();
-        users.add(new User("Arman", 23, "programmer", "1212"));
-        users.add(new User("Oleg", 23, "programmer", "1212"));
-        users.add(new User("Gold", 19, "programmer", "1212"));
-        users.add(new User("Mark", 19, "programmer", "1212"));
-        users.add(new User("Sasha", 25, "programmer", "1212"));
-        users.add(new User("Olga", 29, "programmer", "1212"));
+        users.add(new User("Arman", 23, "Google", "1212"));
+        users.add(new User("Oleg", 23, "Google", "1212"));
+        users.add(new User("Gold", 19, "Amazon", "1212"));
+        users.add(new User("Mark", 19, "Google", "1212"));
+        users.add(new User("Sasha", 25, "Uber", "1212"));
+        users.add(new User("Olga", 29, "Google", "1212"));
 
         return users;
     }
