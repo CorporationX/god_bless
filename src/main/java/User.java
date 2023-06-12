629dd62d2b739d19782d3f2b5e723d0a89ac274a
svetlana.obrancova <obrancova.s@gmail.com>
2023-06-12 12:52:44 +0200

    UR-task2-1 Добавила стат. константы, в конструктор + валидацию параметров

--- src/main/java/User.java
+++ src/main/java/User.java
@@ -1,7 +1,9 @@
 import lombok.Getter;
 
+import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
+import java.util.Set;
 import java.util.stream.Collectors;
 
 @Getter
@@ -12,14 +14,32 @@ public class User {
     private String address;
 
     public User(String name, int age, String workplace, String address) {
+        if (age <= 0) {
+            throw new IllegalArgumentException("Incorrect age");
+        }
+
         this.name = name;
         this.age = age;
         this.workplace = workplace;
         this.address = address;
     }
 
-    public static boolean isValidAge(Integer age) {
-        return age != null && age > 0;
+    public static boolean isValidAge(int age) {
+        return age != 0 && age > 0;
+    }
+
+    public static final Set<String> VALID_JOBS;
+    public static final Set<String> VALID_ADDRESSES;
+    static {
+        VALID_JOBS = new HashSet<>();
+        VALID_JOBS.add("Google");
+        VALID_JOBS.add("Uber");
+        VALID_JOBS.add("Amazon");
+
+        VALID_ADDRESSES = new HashSet<>();
+        VALID_ADDRESSES.add("London");
+        VALID_ADDRESSES.add("New York");
+        VALID_ADDRESSES.add("Amsterdam");
     }
 
     public static Map<Integer, List<User>> groupUsers(List<User> userList) {
