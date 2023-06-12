067ca5b6d5eef46a7d8bae74dd0b5cda7dc3ad15
Devilserker <demonserker@gmail.com>
2023-06-13 02:42:03 +0600

    Задание "Регистрация пользователя" выполнено и покрыто тестами.

--- src/main/java/faang/school/godbless/group_users/User.java
+++ src/main/java/faang/school/godbless/group_users/User.java
@@ -1,25 +1,60 @@
 package faang.school.godbless.group_users;
 
-import lombok.AllArgsConstructor;
+import lombok.Getter;
 import lombok.NonNull;
 
 import java.util.List;
 import java.util.Map;
+import java.util.Set;
 import java.util.stream.Collectors;
 
-@AllArgsConstructor
+@Getter
 public class User {
+    private final static Set<String> VALID_JOBS = Set.of("Google", "Uber", "Amazon");
+    private final static Set<String> VALID_ADDRESSES = Set.of("London", "New York", "Amsterdam");
     private String name;
     private int age;
     private String workplace;
     private String address;
 
-    public static Map<Integer, List<User>> groupUsers(@NonNull List<User> usersList) {
-        return usersList.stream().filter(item -> item != null).collect(Collectors.groupingBy(User::getAge));
+    public User(@NonNull String name, int age, @NonNull String workplace, @NonNull String address) {
+        validateName(name);
+        validateAge(age);
+        validateWorkplace(workplace);
+        validateAddress(address);
+
+        this.name = name;
+        this.age = age;
+        this.workplace = workplace;
+        this.address = address;
     }
 
-    private int getAge() {
-        return age;
+    private void validateName(String name) {
+        if (name.isBlank()) {
+            throw new IllegalArgumentException("Name cannot be empty or contain only spaces");
+        }
+    }
+
+    private void validateAge(int age) {
+        if (age < 18) {
+            throw new IllegalArgumentException("Age cannot be less than 18");
+        }
+    }
+
+    private void validateWorkplace(String workplace) {
+        if (!VALID_JOBS.contains(workplace)) {
+            throw new IllegalArgumentException("Incorrect value, workplace must be from the list");
+        }
+    }
+
+    private void validateAddress(String address) {
+        if (!VALID_ADDRESSES.contains(address)) {
+            throw new IllegalArgumentException("Incorrect value, address must be from the list");
+        }
+    }
+
+    public static Map<Integer, List<User>> groupUsers(@NonNull List<User> usersList) {
+        return usersList.stream().filter(item -> item != null).collect(Collectors.groupingBy(User::getAge));
     }
 }
 
--- src/test/java/faang/school/godbless/group_users/UserTest.java
+++ src/test/java/faang/school/godbless/group_users/UserTest.java
@@ -8,6 +8,8 @@ import java.util.Map;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 
@@ -15,16 +17,16 @@ class UserTest {
 
     @Test
     void testGroupUsers() {
-        User user1 = new User("Alice", 25, "Company A", "123 Main St");
-        User user2 = new User("Bob", 25, "Company B", "456 Elm St");
-        User user3 = new User("Charlie", 25, "Company C", "789 Oak St");
-        User user4 = new User("David", 27, "Company A", "321 Pine St");
-        User user5 = new User("Eve", 27, "Company B", "654 Cedar St");
-        User user6 = new User("Frank", 27, "Company C", "987 Birch St");
-        User user7 = new User("Grace", 30, "Company A", "135 Walnut St");
-        User user8 = new User("Henry", 30, "Company B", "468 Maple St");
-        User user9 = new User("Ivy", 30, "Company C", "791 Cherry St");
-        User user10 = new User("Jack", 32, "Company A", "246 Spruce St");
+        User user1 = new User("Alice", 25, "Google", "London");
+        User user2 = new User("Bob", 25, "Uber", "New York");
+        User user3 = new User("Charlie", 25, "Amazon", "Amsterdam");
+        User user4 = new User("David", 27, "Google", "London");
+        User user5 = new User("Eve", 27, "Uber", "New York");
+        User user6 = new User("Frank", 27, "Amazon", "Amsterdam");
+        User user7 = new User("Grace", 30, "Google", "London");
+        User user8 = new User("Henry", 30, "Uber", "New York");
+        User user9 = new User("Ivy", 30, "Amazon", "Amsterdam");
+        User user10 = new User("Jack", 32, "Google", "London");
         List<User> users = Arrays.asList(user1, user2, user3, user4, null, user5, user6, user7, user8, user9, user10);
 
         Map<Integer, List<User>> testMap = User.groupUsers(users);
@@ -40,4 +42,47 @@ class UserTest {
         assertEquals(Arrays.asList(user7, user8, user9), testMap.get(30));
         assertEquals(Arrays.asList(user10), testMap.get(32));
     }
+
+    @Test
+    void testConstructorWithInvalidName() {
+        assertThrows(IllegalArgumentException.class, () ->
+                new User("  ", 25, "Google", "London"));
+        assertThrows(IllegalArgumentException.class, () ->
+                new User("", 25, "Google", "London"));
+        assertThrows(NullPointerException.class, () ->
+                new User(null, 25, "Google", "London"));
+    }
+
+    @Test
+    void testConstructorWithInvalidAge() {
+        assertThrows(IllegalArgumentException.class, () ->
+                new User("Alice", -25, "Google", "London"));
+        assertThrows(IllegalArgumentException.class, () ->
+                new User("Alice", 17, "Google", "London"));
+
+    }
+
+    @Test
+    void testConstructorWithInvalidWorkplace() {
+        assertThrows(IllegalArgumentException.class, () ->
+                new User("Alice", 25, "Invalid workplace", "London"));
+
+    }
+
+    @Test
+    void testConstructorWithInvalidAddress() {
+        assertThrows(IllegalArgumentException.class, () ->
+                new User("Alice", 25, "Google", "Invalid Address"));
+    }
+
+    @Test
+    void testConstructorWithValidParameters() {
+        User user = new User("Alice", 18, "Google", "London");
+        assertNotNull(user);
+        assertEquals("Alice", user.getName());
+        assertEquals(18, user.getAge());
+        assertEquals("Google", user.getWorkplace());
+        assertEquals("London", user.getAddress());
+    }
+
 }
