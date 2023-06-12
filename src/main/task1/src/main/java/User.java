bf5a6e1751fa00531db5306ba15ec6dd45f1e462
Alex Tarasov <tarasof.av@gmail.com>
2023-06-12 17:30:46 +0300

    Update with Lombok

--- src/main/task1/src/main/java/User.java
+++ src/main/task1/src/main/java/User.java
@@ -1,24 +1,16 @@
+import lombok.Data;
+
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+@Data
 public class User {
     private String name;
     private int age;
     private String workPlace;
     private String address;
-
-    public User(String name, int age, String workPlace, String address) {
-        this.name = name;
-        this.age = age;
-        this.workPlace = workPlace;
-        this.address = address;
-    }
-
-    public int getAge() {
-        return age;
-    }
 
 
     public static Map<Integer, List<User>> groupUsers(List<User> users){
