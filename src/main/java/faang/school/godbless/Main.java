0a2f70d96db3f032f235153bff09dc6f4cffea3b
RasulMurtuzaliev <ras8705@mail.ru>
2023-06-12 13:28:07 +0500

    created a grouping of users by age

---
+++ src/main/java/faang/school/godbless/Main.java
@@ -0,0 +1,20 @@
+package faang.school.godbless;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+public class Main {
+    public static void main(String[] args) {
+        User user1 = new User("name1", 25, "X", "Palo Alto");
+        User user2 = new User("name2", 25, "Y", "Chicago");
+        User user3 = new User("name3", 35, "Z", "Miami");
+        List<User> userList = new ArrayList<>();
+        userList.add(user1);
+        userList.add(user2);
+        userList.add(user3);
+
+        Map<Integer, List<User>> groupMap = User.groupUsers(userList);
+        System.out.println(groupMap);
+    }
+}
