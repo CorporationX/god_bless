bb6589ab5b60d9cb4849f45a8923f68513c2ceba
An Dima <an.dima@slexn.com>
2023-06-12 11:24:45 +0900

    groupUsers added

--- src/main/java/faang/school/godbless/User.java
+++ src/main/java/faang/school/godbless/User.java
@@ -1,4 +1,79 @@
 package faang.school.godbless;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 public class User {
+    private String name;
+    private int age;
+    private String work;
+    private String address;
+
+    public static Map<Integer, List<User>> groupUsers(List<User> userList) {
+        Map<Integer, List<User>> grouped = new HashMap<>();
+        userList.forEach(user -> {
+            grouped.computeIfPresent(user.getAge(), (key, value) -> {
+                value.add(user);
+                return value;
+            });
+            if (!grouped.containsKey(user.getAge())) {
+                List<User> users = new ArrayList<>();
+                users.add(user);
+                grouped.put(user.getAge(), users);
+            }
+        });
+        return grouped;
+    }
+
+    public User(String name, int age, String work, String address) {
+        this.name = name;
+        this.age = age;
+        this.work = work;
+        this.address = address;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getWork() {
+        return work;
+    }
+
+    public void setWork(String work) {
+        this.work = work;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "name='" + name + '\'' +
+                ", age=" + age +
+                ", work='" + work + '\'' +
+                ", address='" + address + '\'' +
+                '}';
+    }
 }
