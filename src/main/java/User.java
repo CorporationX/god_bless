26010d89eab890182c8b84a38cc270df58e3f7d1
svetlana.obrancova <obrancova.s@gmail.com>
2023-06-12 10:54:28 +0200

    AG-task1-2 Добавила проверку возраста,убрала Integer

--- src/main/java/User.java
+++ src/main/java/User.java
@@ -12,14 +12,18 @@ public class User {
     private String address;
 
     public User(String name, int age, String workplace, String address) {
+        if (age <= 0) {
+            throw new IllegalArgumentException("Incorrect age");
+        }
+
         this.name = name;
         this.age = age;
         this.workplace = workplace;
         this.address = address;
     }
 
-    public static boolean isValidAge(Integer age) {
-        return age != null && age > 0;
+    public static boolean isValidAge(int age) {
+        return age != 0 && age > 0;
     }
 
     public static Map<Integer, List<User>> groupUsers(List<User> userList) {
