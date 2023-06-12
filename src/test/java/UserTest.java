15a148cd45f546c6fc8f20b740fd1f8d4ca0f263
DiegaLoL <alex.eey@mail.ru>
2023-06-12 22:15:43 +0900

    BC-290. User registration.

--- src/test/java/UserTest.java
+++ src/test/java/UserTest.java
@@ -1,19 +1,21 @@
-
-
 import faang.school.godbless.bc223.User;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
+
 
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 
 class UserTest {
-
     @Test
-    void getGroupUsersTest() {
+    void testIsNotEmpty(){
         Map<Integer, List<User>> groupUsers = User.groupUsers(getUsers());
         Assertions.assertThat(groupUsers).isNotEmpty();
+    }
+    @Test
+    void testUsersAreGrouped() {
+        //Assert.assertEquals();
         Assertions.assertThat(getUsers().get(0).getAge()).isEqualTo(25);
         Assertions.assertThat(getUsers().get(1).getAge()).isEqualTo(25);
         Assertions.assertThat(getUsers().get(2).getAge()).isEqualTo(30);
