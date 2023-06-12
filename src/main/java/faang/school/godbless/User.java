0fa419436cdc1c109b61440767ee18005e8b3b55
Rustam Nigmatullin <rustamnigma@mail.ru>
2023-06-12 20:54:10 +0500

    BC-350, Abstraction

---
+++ src/main/java/faang/school/godbless/User.java
@@ -0,0 +1,31 @@
+package faang.school.godbless;
+
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+@Getter
+@Setter
+@EqualsAndHashCode
+public class User {
+    private int id;
+    private String name;
+    private int age;
+    private Set<String> activities;
+
+    public static Map<User, String> findHobbyLovers(List<User> users, Set<String> activities){
+        Map<User, String> hobbyLovers = new LinkedHashMap<>();
+        for (User user : users){
+            for(String activity : activities){
+                if(user.getActivities().contains(activity)){
+                    hobbyLovers.put(user, activity);
+                }
+            }
+        }
+        return hobbyLovers;
+    }
+}
