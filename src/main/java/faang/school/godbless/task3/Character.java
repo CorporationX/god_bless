3c69095f975102ead75b0b30f04c4638d82765af
Andrey <khalyavinandrey@gmail.com>
2023-06-12 15:00:17 +0500

    Super class for warrior and archer

---
+++ src/main/java/faang/school/godbless/task3/Character.java
@@ -0,0 +1,30 @@
+package faang.school.godbless.task3;
+
+import lombok.Data;
+
+@Data
+public abstract class Character {
+    private String name;
+    private int strength;
+    private int agility;
+    private int intelligence;
+    private int health = 100;
+
+    public Character(String name) {
+        this.name = name;
+    }
+
+    public Character(String name, int strength, int agility, int intelligence) {
+        this.name = name;
+        this.strength = strength;
+        this.agility = agility;
+        this.intelligence = intelligence;
+    }
+
+    protected abstract void attack(Character character);
+
+    public boolean isDead() {
+        return this.health <= 0;
+    }
+
+}
