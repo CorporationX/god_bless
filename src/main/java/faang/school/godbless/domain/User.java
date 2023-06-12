82a4c02152b42460c5c3b2202bf91b86d2c393d5
Max Dokuchaev <mixamus@icloud.com>
2023-06-12 09:29:11 +0300

    BC-303 registration-users added if non match age, job, address and test

--- src/main/java/faang/school/godbless/domain/User.java
+++ src/main/java/faang/school/godbless/domain/User.java
@@ -2,23 +2,33 @@ package faang.school.godbless.domain;
 
 import lombok.Value;
 
-import java.util.Collections;
-import java.util.List;
-import java.util.Map;
-import java.util.Objects;
+import java.util.*;
 import java.util.stream.Collectors;
 
 @Value
 public class User {
 
+    public static final Set<String> VALID_JOBS = Set.of("Google", "Uber", "Amazon");
+    public static final Set<String> VALID_ADDRESSES = Set.of("London", "New York", "Amsterdam");
+
     String name;
     Integer age;
-    String placeWork;
+    String job;
     String address;
 
     public static Map<Integer, List<User>> getGroupUsers(List<User> users) {
         return Objects.isNull(users) ? Collections.emptyMap() : users.stream()
             .filter(Objects::nonNull)
+            .filter(User::isAgeJobAddressNonMatch)
             .collect(Collectors.groupingBy(User::getAge));
+    }
+
+    private static boolean isAgeJobAddressNonMatch(User user) {
+        if (user.getAge() > 18 || VALID_JOBS.stream().anyMatch(userJob -> userJob.equals(user.getJob())) ||
+            VALID_ADDRESSES.stream().anyMatch(userAddress -> userAddress.equals(user.getAddress()))) {
+            return true;
+        } else {
+            throw new IllegalArgumentException("Age, job and address does not match");
+        }
     }
 }
--- src/test/java/faang/school/godbless/domain/UserTest.java
+++ src/test/java/faang/school/godbless/domain/UserTest.java
@@ -1,6 +1,7 @@
 package faang.school.godbless.domain;
 
 import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import java.util.List;
@@ -9,23 +10,41 @@ import java.util.Map;
 class UserTest {
 
     @Test
+    @DisplayName("Group users if match")
     void getGroupUsersTest() {
-        Map<Integer, List<User>> groupUsers = User.getGroupUsers(getUsers());
+        Map<Integer, List<User>> groupUsers = User.getGroupUsers(usersMatch());
         Assertions.assertThat(groupUsers).isNotEmpty();
-        Assertions.assertThat(getUsers().get(0).getAge()).isEqualTo(34);
-        Assertions.assertThat(getUsers().get(1).getAge()).isEqualTo(24);
-        Assertions.assertThat(getUsers().get(2).getAge()).isEqualTo(54);
-        Assertions.assertThat(getUsers().get(3).getAge()).isEqualTo(40);
+        Assertions.assertThat(usersMatch().get(0).getAge()).isEqualTo(34);
+        Assertions.assertThat(usersMatch().get(1).getAge()).isEqualTo(24);
+        Assertions.assertThat(usersMatch().get(2).getAge()).isEqualTo(54);
+        Assertions.assertThat(usersMatch().get(3).getAge()).isEqualTo(40);
 
-        Assertions.assertThat(getUsers().get(3)).isEqualTo(new User("Valery", 40, "Amazon", "LA"));
+        Assertions.assertThat(usersMatch().get(3)).isEqualTo(new User("Valery", 40, "Amazon", "LA"));
     }
 
-    private List<User> getUsers() {
+    @Test
+    @DisplayName("Group users if not match age, job, address test")
+    void getGroupUsersNotMatchAgeAndJobAndAddressTest() {
+        Assertions.assertThatThrownBy(() -> User.getGroupUsers(usersNonMatch()))
+            .isInstanceOf(IllegalArgumentException.class)
+            .hasMessageContaining("Age, job and address does not match");
+    }
+
+    private List<User> usersMatch() {
         return List.of(
             new User("Max", 34, "Luxoft", "Moscow"),
             new User("Melisa", 24, "Google", "NY"),
             new User("Bob", 54, "Epum", "SF"),
             new User("Valery", 40, "Amazon", "LA")
+        );
+    }
+
+    private List<User> usersNonMatch() {
+        return List.of(
+            new User("Max", 14, "Luxoft", "Moscow"),
+            new User("Melisa", 14, "Lombos", "NY"),
+            new User("Bob", 13, "Epum", "SF"),
+            new User("Valery", 16, "Babl", "LA")
         );
     }
 }
