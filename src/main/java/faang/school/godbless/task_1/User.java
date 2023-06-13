58efb46ae02998e4f8471627fb0ad5d3a69ecd32
OneWayMy <zhenya.ivashchenko.9494@gmail.com>
2023-06-12 20:04:49 -0700

    task_2/UserRegistration

--- src/main/java/faang/school/godbless/task_1/User.java
+++ src/main/java/faang/school/godbless/task_1/User.java
@@ -4,8 +4,10 @@ import lombok.ToString;
 
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
+import java.util.Set;
 
 @ToString
 public class User {
@@ -13,25 +15,40 @@ public class User {
     private int age;
     private String placeOfWork;
     private String address;
+    public static final Set<String> VALID_JOBS = new HashSet<>();
+    public static final Set<String> VALID_ADDRESSES = new HashSet<>();
+
+    static {
+        VALID_JOBS.add("Google");
+        VALID_JOBS.add("Uber");
+        VALID_JOBS.add("Amazon");
+        VALID_ADDRESSES.add("London");
+        VALID_ADDRESSES.add("New York");
+        VALID_ADDRESSES.add("Amsterdam");
+    }
 
     public User(String name, int age, String placeOfWork, String address) {
-        this.name = name;
-        this.age = age;
-        this.placeOfWork = placeOfWork;
-        this.address = address;
+        if (!name.isBlank() && age >= 18 && VALID_JOBS.contains(placeOfWork) && VALID_ADDRESSES.contains(address)) {
+            this.name = name;
+            this.age = age;
+            this.placeOfWork = placeOfWork;
+            this.address = address;
+        } else {
+            throw new IllegalArgumentException();
+        }
     }
 
     public static void main(String[] args) {
         List<User> userList = new ArrayList<>();
-        userList.add(new User("Vasya", 18, "Coorp - x", "Zhukova 28"));
-        userList.add(new User("Natasha", 25, "Google", "Vermishenskogo 13"));
-        userList.add(new User("Igor", 18, "Uber", "Barburova 22 - A"));
-        userList.add(new User("Yevhenii", 13, "FAANG", "Mikatina 2A"));
-        userList.add(new User("Matvei", 17, "SCHOOL 23", "Boguna 33/3"));
-        userList.add(new User("Ksenia", 18, "Stepik", "Krasnoarmeyska 8"));
-        userList.add(new User("Rostislav", 25, "Amazon", "Gorlova 13"));
-        userList.add(new User("Nikita", 25, "Amazon", "Paradna 33"));
-        userList.add(new User("Nadia", 13, "Apple", "Zhukova 28"));
+        userList.add(new User("Vasya", 19, "FAANG", "London"));
+        userList.add(new User("Natasha", 13, "Google", "New York"));
+        userList.add(new User("Igor", 18, "Uber", "Voronezh"));
+        userList.add(new User("Yevhenii", 25, "Google", "Amsterdam"));
+        userList.add(new User("Matvei", 33, "Amazon", "London"));
+        userList.add(new User("Ksenia", 18, "Uber", "Amsterdam"));
+        userList.add(new User("Rostislav", 25, "Google", "Amsterdam"));
+        userList.add(new User("Nikita", 25, "Uber", "New York"));
+        userList.add(new User("Nadia", 19, "Google", "New York"));
         System.out.println(groupUsers(userList));
     }
 
