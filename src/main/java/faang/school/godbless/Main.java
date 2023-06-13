a61fec9aff0b6c0baa8b9c3b5fa0dbddae66e8cd
Паша <petrov.pashapet@yandex.ru>
2023-06-14 01:26:01 +0300

    Collecting users

--- src/main/java/faang/school/godbless/Main.java
+++ src/main/java/faang/school/godbless/Main.java
@@ -1,10 +1,16 @@
 package faang.school.godbless;
 
+import java.util.List;
+import java.util.Set;
+
 public class Main {
     public static void main(String[] args) {
-        Character attacker = new Warrior("War");
-        Character defender = new Archer("Arch");
-        attacker.attack(defender);
-        System.out.println(defender.health);
+        Set<String> set1 = Set.of("прес качат", "атжуманя", "бегит");
+        User user1 = new User(1, "a", 1, set1);
+        Set<String> set2 = Set.of("плакат", "галаса в галаве", "грусные мысли");
+        User user2 = new User(2, "b", 2, set2);
+        List<User> users = List.of(user1, user2);
+        Set<String> activities = Set.of("шашлык", "галаса в галаве", "бегит");
+        System.out.println(User.findHobbyLovers(users, activities));
     }
 }
