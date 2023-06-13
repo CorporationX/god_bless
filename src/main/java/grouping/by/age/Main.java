d2c53438184b8c038c16777f094d21b76b3ca350
Sergey Ivahnenko <ivahnenkose.2003@gmail.com>
2023-06-13 11:35:50 +0700

    Task number two completed

--- src/main/java/grouping/by/age/Main.java
+++ src/main/java/grouping/by/age/Main.java
@@ -3,12 +3,13 @@ package grouping.by.age;
 import java.util.List;
 
 public class Main {
-    public static void main(String[] args) {
-        User user1 = new User("Sergey", 19, "Netflix", "Kirova 2");
-        User user2 = new User("Viktor", 19, "Yandex", "Mira 3");
-        User user3 = new User("Andrew", 20, "McDon", "Lenina 4");
-        User user4 = new User("Bika", 24, "KFC", "Matrosova 5");
-        User user5 = new User("Alex", 12, "SpaceX", "Marksa 6");
+    public static void main(String[] args){
+
+        User user1 = new User("Sergey", 19, "Google", "London");
+        User user2 = new User("Viktor", 19, "Uber", "Amsterdam");
+        User user3 = new User("Andrew", 20, "Google", "London");
+        User user4 = new User("Bika", 24, "Uber", "Amsterdam");
+        User user5 = new User("Alex", 13, "Amazon", "New York");
         List<User> userList = List.of(user1, user2, user3, user4, user5);
         System.out.println(User.groupUsers(userList));
     }
