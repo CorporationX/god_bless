2982ff37709c78e7d8551690ad657e5d0e6ff70f
Basimov Rustam <sombra1nf@yandex.ru>
2023-06-13 20:18:22 +0500

    BC-346. Добавил проверку на пустой массив и null. Добавил маленькую оптимизацию. Исправил ошибку в цикле

--- src/main/java/faang/school/godbless/TurnAround/Example.java
+++ src/main/java/faang/school/godbless/TurnAround/Example.java
@@ -1,10 +1,16 @@
 package faang.school.godbless.TurnAround;
 
 public class Example {
-    public static int[] reverse(int[] ints){
+    public static int[] reverse(int[] ints) throws IllegalArgumentException {
+        if (ints == null || ints.length == 0){
+            throw new IllegalArgumentException("Пустой массив");
+        }
+        if (ints.length == 1){
+            return new int[]{ints[0]};
+        }
         int [] reversed = new int[ints.length];
         int j = 0;
-        for (int i = ints.length-1; i > 0; i--){
+        for (int i = ints.length-1; i >= 0; i--){
             reversed[j++] = ints[i];
         }
         return reversed;
