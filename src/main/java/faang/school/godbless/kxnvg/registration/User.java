7c1df72c5a30b5f027bad7005c83fc3d94f7623d
kxnvg <nikita.armeec.88@mail.ru>
2023-06-13 16:22:02 +0300

    Удалены лишние аннотации lombok'a, изменены валидации констант.

--- src/main/java/faang/school/godbless/kxnvg/registration/User.java
+++ src/main/java/faang/school/godbless/kxnvg/registration/User.java
@@ -1,28 +1,23 @@
 package faang.school.godbless.kxnvg.registration;
 
-import lombok.EqualsAndHashCode;
 import lombok.Getter;
-import lombok.ToString;
 
-import java.util.Arrays;
-import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
 import java.util.stream.Collectors;
 
 @Getter
-@EqualsAndHashCode
-@ToString
 public class User {
+
+    private static final Set<String> VALID_JOBS = Set.of("Google", "Uber", "Amazon");
+    private static final Set<String> VALID_ADDRESSES = Set.of("London", "New York", "Amsterdam");
+    private static final int VALID_USER_AGE = 18;
 
     private String userName;
     private int userAge;
     private String userJob;
     private String userAddress;
-
-    private static final Set<String> VALID_JOBS = new HashSet<>(Arrays.asList("Google", "Uber", "Amazon"));
-    private static final Set<String> VALID_ADDRESSES = new HashSet<>(Arrays.asList("London", "New York", "Amsterdam"));
 
     public User(String userName, int userAge, String userJob, String userAddress) {
         constructorValidation(userName, userAge, userJob, userAddress);
@@ -41,7 +36,7 @@ public class User {
     private void constructorValidation(String userName, int userAge, String userJob, String userAddress) throws IllegalArgumentException {
         if (userName == null || userName == "") {
             throw new IllegalArgumentException("Имя не может быть пустым");
-        } else if (userAge < 18) {
+        } else if (userAge < VALID_USER_AGE) {
             throw  new IllegalArgumentException("Возраст не может быть меньше 18");
         } else if (!VALID_JOBS.contains(userJob)) {
             throw new IllegalArgumentException("Такое место работы не зарегестрировано");
@@ -49,6 +44,4 @@ public class User {
             throw new IllegalArgumentException("Такой адрес не разрегестрирован");
         }
     }
-
-
 }
