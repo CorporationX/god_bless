2fafc4987b166bb7e1108194dbeb571b66d52f83
Sergey Gerasimov <czar997@yandex.ru>
2023-06-13 17:30:32 +0300

    добавлен класс User

---
+++ src/main/java/faang/school/godbless/task4/User.java
@@ -0,0 +1,21 @@
+package faang.school.godbless.task4;
+
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
+public class User {
+
+    private int id;
+
+    private String name;
+
+    private int age;
+
+    private Set<String> activities;
+
+    public Map<User, String> findHobbyLovers(List<User> users, Set<String> activities) {
+
+        return null;
+    }
+}
