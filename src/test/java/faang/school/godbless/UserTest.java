e6ba78881d5a795a14b4a2ae59f1f5dad5e6deee
Ivan Averkov <averkovivan@gmail.com>
2023-06-13 11:32:01 +0300

    commit

--- src/test/java/faang/school/godbless/UserTest.java
+++ src/test/java/faang/school/godbless/UserTest.java
@@ -1,4 +1,30 @@
+package faang.school.godbless;
+
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
+
 class UserTest {
-  
+    @Test
+    void constructorTestPos() {
+        User user = new User("Ivan", 18, "Google", "London");
+
+        assertEquals("Ivan", user.getName());
+        assertEquals(18, user.getAge());
+        assertEquals("Google", user.getPlaceOfWork());
+        assertEquals("London", user.getAddress());
+    }
+
+    @Test
+    void constructorTestNeg() {
+        IllegalArgumentException nameException = assertThrows(IllegalArgumentException.class, () -> new User(" ", 18, "Google", "London"));
+        IllegalArgumentException ageException = assertThrows(IllegalArgumentException.class, () -> new User("Ivan", 10, "Google", "London"));
+        IllegalArgumentException workException = assertThrows(IllegalArgumentException.class, () -> new User("Ivan", 18, "Yandex", "London"));
+        IllegalArgumentException addressException = assertThrows(IllegalArgumentException.class, () -> new User("Ivan", 18, "Google", "Roetov"));
+
+        assertEquals("Name is empty", nameException.getMessage());
+        assertEquals("Too young", ageException.getMessage());
+        assertEquals("Work exception", workException.getMessage());
+        assertEquals("Address exception", addressException.getMessage());
+    }
 }
