ccdd84a24ce5e582aa13eebeb3a744bd6abc6996
Abramov Nikita <mr.abramovn@gmail.com>
2023-06-13 16:01:31 +0300

    Code refactoring

--- src/main/java/faang/school/godbless/User.java
+++ src/main/java/faang/school/godbless/User.java
@@ -1,33 +1,31 @@
 package faang.school.godbless;
 
 import lombok.AllArgsConstructor;
-import lombok.Data;
-
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
-@Data
 @AllArgsConstructor
 public class User {
     private String name;
     private int age;
-    private String workPlease;
+    private String workPlace;
     private String address;
 
 
-    public static HashMap<Integer, List<User>> groupUsers(List<User> users) {
-        HashMap<Integer, List<User>> result = new HashMap<>();
+
+    public static Map<Integer, List<User>> groupUsers(List<User> users) {
+        Map<Integer, List<User>> result = new HashMap<>();
         for (User user : users) {
+            List<User> usersWithBirthdayInOneDay;
             if (!result.containsKey(user.age)) {
-                List<User> usersWithBirthdayInOneDay = new ArrayList<>();
-                usersWithBirthdayInOneDay.add(user);
-                result.put(user.age, usersWithBirthdayInOneDay);
+                usersWithBirthdayInOneDay = new ArrayList<>();
             } else {
-                List<User> usersWithBirthdayInOneDay = result.get(user.age);
-                usersWithBirthdayInOneDay.add(user);
-                result.put(user.age, usersWithBirthdayInOneDay);
+                usersWithBirthdayInOneDay = result.get(user.age);
             }
+            usersWithBirthdayInOneDay.add(user);
+            result.put(user.age, usersWithBirthdayInOneDay);
         }
         return result;
     }
