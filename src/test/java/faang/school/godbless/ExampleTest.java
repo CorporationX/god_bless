339fd63bbb2c1fe89e4928cfdcf5f3ba8f32e7b3
Andrey <khalyavinandrey@gmail.com>
2023-06-13 22:21:37 +0500

    created ExampleTest test class with tests for reversing not empty array and empty array

---
+++ src/test/java/faang/school/godbless/ExampleTest.java
@@ -0,0 +1,34 @@
+package faang.school.godbless;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+
+public class ExampleTest {
+    private Example example;
+
+    @BeforeEach
+    public void setUp() {
+        example = new Example();
+    }
+
+    @Test
+    public void reverseNotEmptyArray() {
+        int[] arr = new int[] {1, 2, 3, 4, 5};
+
+        int[] resultArr = example.reverse(arr);
+
+        Assertions.assertEquals(Arrays.toString(new int[] {5, 4, 3, 2, 1}), Arrays.toString(resultArr));
+    }
+
+    @Test
+    public void reverseEmptyArray() {
+        int[] arr = new int[] {};
+
+        int[] resultArr = example.reverse(arr);
+
+        Assertions.assertEquals(Arrays.toString(new int[] {}), Arrays.toString(resultArr));
+    }
+}
