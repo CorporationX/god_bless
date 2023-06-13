abc94fa88e97f9aac5eedde0405b62dccbbfad3a
Pashkevich <ppavel012100@gmail.com>
2023-06-13 09:08:18 +0200

    Rework collecting users task

--- src/main/java/faang/school/godbless/Collectingusers/User.java
+++ src/main/java/faang/school/godbless/Collectingusers/User.java
@@ -23,9 +23,30 @@ public class User {
             for (String activity : activities) {
                 if (user.getActivities().contains(activity)) {
                     mapHobbyLovers.put(user, activity);
+                    break;
                 }
             }
         }
         return mapHobbyLovers;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof User user)) return false;
+
+        if (getId() != user.getId()) return false;
+        if (getAge() != user.getAge()) return false;
+        if (getName() != null ? !getName().equals(user.getName()) : user.getName() != null) return false;
+        return getActivities() != null ? getActivities().equals(user.getActivities()) : user.getActivities() == null;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = getId();
+        result = 31 * result + getAge();
+        result = 31 * result + (getName() != null ? getName().hashCode() : 0);
+        result = 31 * result + (getActivities() != null ? getActivities().hashCode() : 0);
+        return result;
     }
 }
