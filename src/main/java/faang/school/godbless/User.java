7c4431bbbc4eb7fae0e077881f84cf895c7b0713
Zhugalev Sergei <zhugalcf@gmail.com>
2023-06-13 22:40:44 +0300

    Add new final and refactor method

--- src/main/java/faang/school/godbless/User.java
+++ src/main/java/faang/school/godbless/User.java
@@ -3,56 +3,47 @@ package faang.school.godbless;
 import lombok.ToString;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.HashMap;
-import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
 
 @ToString
 public class User {
+    private static final Set<String> VALID_JOBS = Set.of("Google", "Uber", "Amazon");
+    private static final Set<String> VALID_ADDRESSES = Set.of("London", "NewYork", "Amsterdam");
+    private static final int MIN_AGE = 18;
 
     private String name;
     private int age;
     private String workPlace;
     private String address;
 
-    private static final Set<String> VALID_JOBS = new HashSet<>(Arrays.asList("Google", "Uber", "Amazon"));
-    private static final Set<String> VALID_ADDRESSES = new HashSet<>(Arrays.asList("London", "NewYork", "Amsterdam"));
 
     public User(String name, int age, String workPlace, String address) {
-        if (name != null || name.isEmpty()){
-            this.name = name;
-        } else {
+        if (!(name != null || name.isEmpty())) {
             throw new IllegalArgumentException("Name is blank");
         }
-        if (age >= 18){
-            this.age = age;
-        } else {
+        if (!(age >= MIN_AGE)) {
             throw new IllegalArgumentException("Children can't work");
         }
-        if (VALID_JOBS.contains(workPlace)){
-            this.workPlace = workPlace;
-        } else {
+        if (!(VALID_JOBS.contains(workPlace))) {
             throw new IllegalArgumentException("Not valid workplace");
         }
-        if (VALID_ADDRESSES.contains(address)){
-            this.address = address;
-        } else {
+        if (!(VALID_ADDRESSES.contains(address))) {
             throw new IllegalArgumentException("Not valid address");
         }
+        this.name = name;
+        this.address = address;
+        this.age = age;
+        this.workPlace = workPlace;
     }
 
-    public static Map<Integer, List<User>> groupUsers(List<User> users){
-        Map<Integer,List<User>> groupUsers = new HashMap<>();
-        for (User user: users){
-            if (groupUsers.containsKey(user.age)){
-                groupUsers.get(user.age).add(user);
-            } else {
-                groupUsers.put(user.age, new ArrayList<>());
-                groupUsers.get(user.age).add(user);
-            }
+    public static Map<Integer, List<User>> groupUsers(List<User> users) {
+        Map<Integer, List<User>> groupUsers = new HashMap<>();
+        for (User user : users) {
+            groupUsers.putIfAbsent(user.age, new ArrayList<>());
+            groupUsers.get(user.age).add(user);
         }
         return groupUsers;
     }
