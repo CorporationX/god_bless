88ef913a6eaa513570056d4581f1d87994e38102
Иван Стадник <stadivany@gmail.com>
2023-06-13 22:45:18 +0300

    add method groupUsers

---
+++ src/main/java/task_1/User.java
@@ -0,0 +1,18 @@
+package task_1;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class User {
+    String name;
+    int age;
+    String placeOfWork;
+    String address;
+
+    static List<User> groupUsers(){
+        HashMap <String, List> map = new HashMap<>();
+
+    }
+
+}
