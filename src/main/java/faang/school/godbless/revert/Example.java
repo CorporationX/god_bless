0c7b1e4bfcf259a9d8d1e9d2443ff0cdb9c05994
Pashkevich <ppavel012100@gmail.com>
2023-06-13 08:21:42 +0200

    Rework reverse task

--- src/main/java/faang/school/godbless/revert/Example.java
+++ src/main/java/faang/school/godbless/revert/Example.java
@@ -1,23 +1,20 @@
 package faang.school.godbless.revert;
 
-
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
-
+import java.util.Arrays;
 
 public class Example {
     public static void main(String[] args) {
-        List<Integer> list = new ArrayList<>();
-        list.add(1);
-        list.add(2);
-        list.add(3);
-        list.add(4);
-        list.add(5);
-        reverse(list);
+        int[] array = new int[]{1, 2, 3, 4, 5};
+        reverse(array);
+        System.out.println(Arrays.toString(array));
     }
-    public static void reverse (List<Integer> list ){
-        Collections.reverse(list);
-        System.out.println(list);
+
+    public static void reverse(int[] array) {
+        for (int i = 0; i < array.length / 2; i++) {
+            int temp = array[i];
+            array[i] = array[array.length - 1 - i];
+            array[array.length - 1 - i] = temp;
+        }
+
     }
 }
