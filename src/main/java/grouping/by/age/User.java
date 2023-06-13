296fd968391c0c371c050900d77ad66a128d309d
Sergey Ivahnenko <ivahnenkose.2003@gmail.com>
2023-06-13 11:51:35 +0700

    Task number two completed

--- src/main/java/grouping/by/age/User.java
+++ src/main/java/grouping/by/age/User.java
@@ -4,18 +4,34 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Set;
 
 public class User {
-    private String name;
-    private int age;
-    private String job;
-    private String address;
+    private final String name;
+    private final int age;
+    private final String job;
+    private final String address;
+    private static final Set<String> VALID_JOBS = Set.of("Google", "Uber", "Amazon");
+    private static final Set<String> VALID_ADDRESSES = Set.of("London", "New York", "Amsterdam");
 
-    public User(String name, int age, String job, String address) {
+    public User (String name, int age, String job, String address){
         this.name = name;
         this.age = age;
         this.job = job;
         this.address = address;
+
+        if (name.isEmpty()) {
+            throw new IllegalArgumentException("Имя не может быть пустым");
+        }
+        if (age<18) {
+            throw new IllegalArgumentException("На работу с 18 лет");
+        }
+        if (!(VALID_JOBS.contains(job))) {
+            throw new IllegalArgumentException("Ты не можешь тут работать");
+        }
+        if (!(VALID_ADDRESSES.contains(address))) {
+            throw new IllegalArgumentException("Ты не можешь тут жить");
+        }
     }
 
     public static Map<Integer, List<User>> groupUsers(List<User> userList) {
