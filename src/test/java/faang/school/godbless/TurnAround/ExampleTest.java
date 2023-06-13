b45416e1477df4f18c29a15bc2667fb4a735a395
Basimov Rustam <sombra1nf@yandex.ru>
2023-06-13 20:19:40 +0500

    BC-346. Тесты

---
+++ src/test/java/faang/school/godbless/TurnAround/ExampleTest.java
@@ -0,0 +1,39 @@
+package faang.school.godbless.TurnAround;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class ExampleTest {
+
+    //Positive
+    @Test
+    @DisplayName("Reverse Test")
+    public void revertTest(){
+        int [] expected = {1, 2, 3, 4};
+        Assertions.assertArrayEquals(expected, Example.reverse(new int[]{4, 3, 2, 1}));
+    }
+
+    @Test
+    @DisplayName("Reverse one element")
+    public void reverseOneElementTest(){
+        Assertions.assertArrayEquals(new int[]{1},Example.reverse(new int[]{1}));
+    }
+
+    //Negative
+    @Test
+    @DisplayName("Reverse empty array")
+    public void reverseEmptyArray(){
+        Assertions.assertThrows(IllegalArgumentException.class, () -> {
+            Example.reverse(new int[]{});
+        });
+    }
+
+    @Test
+    @DisplayName("Try reverse null")
+    public void reverseNull(){
+        Assertions.assertThrows(IllegalArgumentException.class, () -> {
+            Example.reverse(null);
+        });
+    }
+}
