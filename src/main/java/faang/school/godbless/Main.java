fe00d37895670f9518d983161fbc512eacd369c3
Паша <petrov.pashapet@yandex.ru>
2023-06-14 02:02:40 +0300

    Turn around!

--- src/main/java/faang/school/godbless/Main.java
+++ src/main/java/faang/school/godbless/Main.java
@@ -1,10 +1,9 @@
 package faang.school.godbless;
 
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
-        Character attacker = new Warrior("War");
-        Character defender = new Archer("Arch");
-        attacker.attack(defender);
-        System.out.println(defender.health);
+        System.out.println(Arrays.toString(Example.reverse(new int[]{5, 7, 32, 4000, 1})));
     }
 }
