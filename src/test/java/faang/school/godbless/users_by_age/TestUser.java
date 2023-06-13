2307488c863b61bcb14f079a1bc43e6b98ae5505
Smurfetti <matveynikishaev@gmail.com>
2023-06-13 17:39:37 +0300

    Implemented first task

--- src/test/java/faang/school/godbless/users_by_age/TestUser.java
+++ src/test/java/faang/school/godbless/users_by_age/TestUser.java
@@ -1,28 +1,23 @@
 package faang.school.godbless.users_by_age;
 
+import faang.school.godbless.User;
 import org.junit.jupiter.api.Test;
 
-import java.util.Arrays;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertTrue;
 
 class TestUser {
     @Test
     void testGroupUsers() {
-        User userOne = new User("NameOne", 21, "CompanyOne", "Main street");
-        User userTwo = new User("NameTwo", 22, "CompanyTwo", "Main street");
-        User userThree = new User("NameThree", 23, "CompanyThree", "Main street");
-        User userFour = new User("NameFour", 24, "CompanyFour", "Main street");
-        User userFive = new User("NameFive", 25, "CompanyFive", "Main street");
-        List<User> users = Arrays.asList(userOne, userTwo, userThree, userFour, userFive);
+        List<User> users = new ArrayList<>();
+        for (int i = 0; i < 10; i++) {
+            users.add(new User(Integer.toString(i), i % 3, Integer.toString(i), Integer.toString(i)));
+        }
+        Map<Integer, List<User>> sortedUsers = User.groupUsers(users);
 
-        Map<Integer, List<User>> usersForCheck = User.groupUsers(users);
-
-        assertEquals(5, usersForCheck.size());
-        assertTrue(usersForCheck.containsKey(23));
-        assertTrue(usersForCheck.containsKey(22));
+        assertEquals(3, sortedUsers.size());
     }
 }
