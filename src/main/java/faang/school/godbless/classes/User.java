c4fb017c0a00240f390a2b60dd579c5ba731ce1f
Basimov Rustam <sombra1nf@yandex.ru>
2023-06-13 19:02:41 +0500

    Вынес валидацию в отдельный метод

--- src/main/java/faang/school/godbless/classes/User.java
+++ src/main/java/faang/school/godbless/classes/User.java
@@ -2,7 +2,6 @@ package faang.school.godbless.classes;
 
 import lombok.Getter;
 import lombok.Setter;
-import lombok.ToString;
 
 import java.util.Arrays;
 import java.util.HashSet;
@@ -19,22 +18,28 @@ public class User {
     private final Set<String> VALID_JOB = new HashSet<>(Arrays.asList("Google", "Uber", "Amazon"));
     private final Set<String> VALID_ADDRESSES = new HashSet<>(Arrays.asList("London", "New York", "Amsterdam"));
 
-    public User(String name, int age, String company,String location){
-        if (name == null || name.equals("")){
+    public User(String name, int age, String company, String location) {
+        if (isValidate(name, age, company, location)) {
+            this.name = name;
+            this.age = age;
+            this.company = company;
+            this.location = location;
+        }
+    }
+
+    private boolean isValidate(String name, int age, String company, String location) {
+        if (name == null || name.equals("")) {
             throw new IllegalArgumentException("Имя не можеть быть быть пустым");
         }
-        if (age < 18){
+        if (age < 18) {
             throw new IllegalArgumentException("Вам должно быть 18 лет или больше");
         }
-        if (!VALID_JOB.contains(company)){
+        if (!VALID_JOB.contains(company)) {
             throw new IllegalArgumentException("Такого места работы нет");
         }
-        if (!VALID_ADDRESSES.contains(location)){
+        if (!VALID_ADDRESSES.contains(location)) {
             throw new IllegalArgumentException("В этом месте нет работы");
         }
-        this.name = name;
-        this.age = age;
-        this.company = company;
-        this.location = location;
+        return true;
     }
 }
