1fd6a14c53d34d6abc73d9aeb31aecd00d73832a
Basimov Rustam <sombra1nf@yandex.ru>
2023-06-13 19:55:26 +0500

    BC-346. Добавил метод разворачивающий массив

--- src/main/java/faang/school/godbless/TurnAround/Example.java
+++ src/main/java/faang/school/godbless/TurnAround/Example.java
@@ -1,4 +1,12 @@
 package faang.school.godbless.TurnAround;
 
 public class Example {
+    public static int[] reverse(int[] ints){
+        int [] reversed = new int[ints.length];
+        int j = 0;
+        for (int i = ints.length-1; i > 0; i--){
+            reversed[j++] = ints[i];
+        }
+        return reversed;
+    }
 }
