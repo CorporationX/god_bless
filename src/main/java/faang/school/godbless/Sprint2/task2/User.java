c8a7bb2997bd9486f3696890bf0a6a6c5e2a6e66
RasulMurtuzaliev <ras8705@mail.ru>
2023-06-13 11:23:47 +0500

    added two const

--- src/main/java/faang/school/godbless/Sprint2/task2/User.java
+++ src/main/java/faang/school/godbless/Sprint2/task2/User.java
@@ -4,12 +4,15 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Set;
 
 public class User {
     private String name;
     private int age;
     private String corporation;
     private String address;
+    private static final Set<String> VALID_JOBS = Set.of("Google", "Uber", "Amazon");
+    private static final Set<String> VALID_ADDRESSES = Set.of("London", "New York", "Amsterdam");
 
     public User(String name, int age, String corporation, String address) {
         this.name = name;
