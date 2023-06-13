29ee91946493c6c87155973d2fbd5a34068078a5
kxnvg <nikita.armeec.88@mail.ru>
2023-06-13 19:02:52 +0300

    Изменен метод reverse в классе Example

--- src/main/java/faang/school/godbless/kxnvg/reverse/Example.java
+++ src/main/java/faang/school/godbless/kxnvg/reverse/Example.java
@@ -3,20 +3,22 @@ package faang.school.godbless.kxnvg.reverse;
 public class Example {
 
     public static int[] reverse(int[] arr) {
-        int[] resultArray = new int[arr.length];
-
         if (arr == null) {
             throw new NullPointerException("Массив не должен быть null");
         } else if (arr.length == 0 || arr.length == 1) {
             return arr;
         }
 
-        int count = arr.length - 1;
-        for (Integer value : arr) {
-            resultArray[count] = value;
-            count--;
+        int left = 0;
+        int right = arr.length - 1;
+        while (left < right) {
+            int value = arr[right];
+            arr[right] = arr[left];
+            arr[left] = value;
+            right--;
+            left++;
         }
 
-        return resultArray;
+        return arr;
     }
 }
