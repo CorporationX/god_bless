aee6c16a63900664f585472d1971bd73e62efe30
Serhii Rubets <serhii.rubets@holbertonschool.com>
2023-06-13 12:23:22 -0500

    Changed code order

--- src/test/java/user/UserTest.java
+++ src/test/java/user/UserTest.java
@@ -7,6 +7,7 @@ import org.junit.jupiter.api.Test;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Set;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -16,11 +17,12 @@ public class UserTest {
   private final static int AGE = 20;
   private final static String JOB = "Google";
   private final static String ADDRESS = "London";
+  private final static Set<String> ACTIVITIES = Set.of("Running", "Swimming", "Jumping");
 
 
   @BeforeEach
   public void setUp() {
-    user = new User(FIRST_NAME, AGE, JOB, ADDRESS);
+    user = new User(FIRST_NAME, AGE, JOB, ADDRESS, ACTIVITIES);
   }
 
   @Test
@@ -34,7 +36,7 @@ public class UserTest {
   @Test
   public void testCorrectInputName() {
     Exception exception = assertThrows(IllegalArgumentException.class, () -> {
-      new User("", 22, "Google", "London");
+      new User("", 22, "Google", "London", ACTIVITIES);
     });
 
     assertEquals(User.REQUIRED_NAME_ERROR, exception.getMessage());
@@ -43,7 +45,7 @@ public class UserTest {
   @Test
   public void testCorrectInputAge() {
     Exception exception = assertThrows(IllegalArgumentException.class, () -> {
-      new User("Max", 15, "Google", "London");
+      new User("Max", 15, "Google", "London", ACTIVITIES);
     });
 
     assertEquals(User.MIN_AGE_ERROR, exception.getMessage());
@@ -52,7 +54,7 @@ public class UserTest {
   @Test
   public void testCorrectInputJob() {
     Exception exception = assertThrows(IllegalArgumentException.class, () -> {
-      new User("Max", 25, "Twtitter", "London");
+      new User("Max", 25, "Twtitter", "London", ACTIVITIES);
     });
 
     assertEquals(User.VALID_JOBS_ERROR, exception.getMessage());
@@ -61,7 +63,7 @@ public class UserTest {
   @Test
   public void testCorrectInputAddress() {
     try {
-      User user = new User("Max", 25, "Google", "Kyiv");
+      User user = new User("Max", 25, "Google", "Kyiv", ACTIVITIES);
       // If code above will be executed without errors - it's a problem, we should have age validation
       throw new IllegalArgumentException("Should not be executed");
     } catch (Exception e) {
@@ -71,11 +73,11 @@ public class UserTest {
 
   @Test
   public void testGroupUsers() {
-    User dima = new User("Dima", 21, "Google", "London");
-    User max = new User("Max", 20, "Uber", "New York");
-    User alex = new User("Alex", 21, "Amazon", "Amsterdam");
-    User petr = new User("Petr", 20, "Google", "London");
-    User bryan = new User("Bryan", 19, "Uber", "Amsterdam");
+    User dima = new User("Dima", 21, "Google", "London", ACTIVITIES);
+    User max = new User("Max", 20, "Uber", "New York", ACTIVITIES);
+    User alex = new User("Alex", 21, "Amazon", "Amsterdam",ACTIVITIES);
+    User petr = new User("Petr", 20, "Google", "London", ACTIVITIES);
+    User bryan = new User("Bryan", 19, "Uber", "Amsterdam", ACTIVITIES);
 
     List<User> users = List.of(dima, max, alex, petr, bryan);
 
@@ -88,5 +90,17 @@ public class UserTest {
     Map<Integer, List<User>> actualUsersAgeToUsersResult = User.groupUsers(users);
 
     assertEquals(expectedGroups, actualUsersAgeToUsersResult);
+  }
+
+  @Test
+  public void findHobbyLoversTest() {
+    Map<User, String> expectedActivityResult = new HashMap<>();
+    User max = new User("Max", 19, "Google", "London", Set.of("Running"));
+    User matt = new User("Matt", 21, "Uber", "London", Set.of("Jumping"));
+    expectedActivityResult.put(max, "Running");
+    expectedActivityResult.put(matt, "Jumping");
+
+    Map<User, String> actualActivityResult = User.findHobbyLovers(List.of(max, matt), Set.of("Running", "Swimming", "Jumping"));
+    assertEquals(expectedActivityResult, actualActivityResult);
   }
 }
