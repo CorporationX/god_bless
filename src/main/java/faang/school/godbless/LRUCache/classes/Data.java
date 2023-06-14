c423304e57f90bfa683c0d7608a430a604c6f682
Basimov Rustam <sombra1nf@yandex.ru>
2023-06-14 23:52:30 +0500

    Задача явно не готова. Хочу получить фидбек по тому, что уже готово.

---
+++ src/main/java/faang/school/godbless/LRUCache/classes/Data.java
@@ -0,0 +1,20 @@
+package faang.school.godbless.LRUCache.classes;
+
+import lombok.Getter;
+import lombok.NonNull;
+import lombok.RequiredArgsConstructor;
+import lombok.Setter;
+
+import java.util.Date;
+
+
+@Getter
+@Setter
+@RequiredArgsConstructor
+public class Data {
+    @NonNull
+    private int id;
+    @NonNull
+    private String value;
+    private Date timestamp;
+}
