bffc95c63cdb44d96d6d5820d2cc3744b44a3006
Serhii Rubets <serhii.rubets@holbertonschool.com>
2023-06-14 09:20:55 -0500

    Added reverse method

---
+++ src/test/java/example/ExampleTest.java
@@ -0,0 +1,29 @@
+package example;
+
+import faang.school.godbless.example.Example;
+import faang.school.godbless.user.User;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class ExampleTest {
+  Example example;
+  @BeforeEach
+  public void setUp() {
+    example = new Example();
+  }
+
+  @Test
+  public void testReverse() {
+    List<Integer> numbers = new ArrayList<>(Arrays.asList(1, 2, 3, 4, 5));
+    List<Integer> expectedNumbers = List.of(5, 4, 3, 2, 1);
+    List<Integer> actualResults = example.reverse(numbers);
+    assertEquals(expectedNumbers, actualResults);
+    // assertEquals(expectedNumbers, example.reverse(numbers)); - Doesn't work
+  }
+}
