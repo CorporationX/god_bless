c8dfc1c850923feb0d2fa3c676d6298c8ec81342
OneWayMy <zhenya.ivashchenko.9494@gmail.com>
2023-06-13 19:58:02 -0700

    task_4/User_Collection

---
+++ src/main/java/faang/school/godbless/task_3/User.java
@@ -0,0 +1,24 @@
+package faang.school.godbless.task_3;
+
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+import java.util.Set;
+
+public class User {
+    private int id;
+    private String name;
+    private int age;
+    public Set<String> activities = new HashSet<>();
+
+    public Map<User, String> findHobbyLovers(List<User> users, Set<String> activities) {
+        Map<User, String> userAndHobby = new HashMap<>();
+        for (User user : users) {
+            Optional<String> optionalS = user.activities.stream().filter(activities::contains).findFirst();
+            optionalS.ifPresent(s -> userAndHobby.put(user, s));
+        }
+        return userAndHobby;
+    }
+}
