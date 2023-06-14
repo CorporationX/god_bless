6960c9e5abdead7b1f1f26f551f1912b5bf3db48
Ivan Averkov <averkovivan@gmail.com>
2023-06-14 15:14:24 +0300

    Исправил недочеты

--- src/main/java/faang/school/godbless/User.java
+++ src/main/java/faang/school/godbless/User.java
@@ -1,11 +1,14 @@
 package faang.school.godbless;
+import lombok.ToString;
+
 import java.util.*;
 
+@ToString
 public class User {
-    private String name;
-    private int age;
-    private String placeOfWork;
-    private String address;
+    private final String name;
+    private final int age;
+    private final String placeOfWork;
+    private final String address;
 
     public User(String name, int age, String placeOfWork, String address) {
         this.name = name;
@@ -14,33 +17,17 @@ public class User {
         this.address = address;
     }
 
-    @Override
-    public String toString() {
-        return "User{" +
-                "name='" + name + '\'' +
-                ", age=" + age +
-                ", placeOfWork='" + placeOfWork + '\'' +
-                ", address='" + address + '\'' +
-                '}';
-    }
-
-    public int getAge() {
-        return this.age;
-    }
 
     public static Map<Integer, List<User>> groupUsers(List<User> userList) {
         Map<Integer, List<User>> res = new HashMap<>();
         for(User user : userList) {
-            int age = user.getAge();
-            res.computeIfPresent(age, (key, value) -> {
-                value.add(user);
-                return value;
-            });
+            int age = user.age;
             if(!res.containsKey(age)) {
                 List<User> list = new ArrayList<>();
-                list.add(user);
                 res.put(age, list);
             }
+            res.get(age).add(user);
+
 
         }
         return res;
