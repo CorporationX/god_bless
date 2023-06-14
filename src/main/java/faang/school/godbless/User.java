c8f833ffcbb3e7ce0f86c334b32e57346b203424
Stepan Palkov <step.palkov@gmail.com>
2023-06-14 13:21:45 +0300

    clean cod

--- src/main/java/faang/school/godbless/User.java
+++ src/main/java/faang/school/godbless/User.java
@@ -11,34 +11,38 @@ import java.util.stream.Collectors;
 
 @Getter
 public class User {
-    private final static Set<String> VALID_JOBS = new HashSet<>(Arrays.asList("Google" , "Uber", "Amazon"));
+    private final static Set<String> VALID_JOBS = new HashSet<>(Arrays.asList("Google", "Uber", "Amazon"));
     private static final Set<String> VALID_ADDRESSES = new HashSet<>(Arrays.asList("London", "New York", "Amsterdam"));
     private String name;
     private int age;
     private String job;
     private String address;
 
-    public User(String userName, int age, String job, String userAddress){
-        if(name.isEmpty()){
+    public User(String userName, int age, String job, String userAddress) {
+        if (name.isEmpty()) {
             throw new IllegalArgumentException("Name cannot be empty!");
         } else {
             this.name = userName;
-        } if (age<0){
+        }
+        if (age < 0) {
             throw new IllegalArgumentException("Age cannot be less 0");
         } else {
             this.age = age;
-        } if (!(VALID_JOBS.contains(job))){
+        }
+        if (!(VALID_JOBS.contains(job))) {
             throw new IllegalArgumentException("Invalid job");
         } else {
             this.job = job;
-        } if (!(VALID_ADDRESSES.contains(address))){
+        }
+        if (!(VALID_ADDRESSES.contains(address))) {
             throw new IllegalArgumentException("Invalid address");
         } else {
             this.address = userAddress;
         }
 
     }
-    public static Map<Integer,List<User>> groupUsers(List<User> users){
+
+    public static Map<Integer, List<User>> groupUsers(List<User> users) {
         return users.stream()
                 .collect(Collectors.groupingBy(User::getAge));
     }
