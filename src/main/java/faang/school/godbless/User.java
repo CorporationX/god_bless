404757af17302bd88f339baa561d48a036b75615
chaykinks <chaykinks@gmail.com>
2023-06-14 20:45:37 +0500

    добавил класс User и метод groupUsers

--- src/main/java/faang/school/godbless/User.java
+++ src/main/java/faang/school/godbless/User.java
@@ -1,10 +1,38 @@
 package faang.school.godbless;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 public class User {
     private String name;
     private int age;
-    private String workPlace;
+    private String workplace;
     private String address;
+
+    public int getAge() {
+        return age;
+    }
+
+    public User(String name, int age, String workplace, String address) {
+        this.name = name;
+        this.age = age;
+        this.workplace = workplace;
+        this.address = address;
+    }
+
+    public static Map<Integer, List<User>> groupUsers(List<User> userList) {
+        Map<Integer, List<User>> groupedUserMap = new HashMap<>();
+        for (User user : userList) {
+            int age = user.getAge();
+            if (!groupedUserMap.containsKey(age)) {
+                groupedUserMap.put(age, new ArrayList<>());
+            }
+            groupedUserMap.get(age).add(user);
+        }
+        return groupedUserMap;
+    }
 }
 
 
--- src/test/java/faang/school/godbless/users_by_age/TestUser.java
+++ src/test/java/faang/school/godbless/users_by_age/TestUser.java
@@ -10,19 +10,5 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 class TestUser {
-    @Test
-    void testGroupUsers() {
-        User userOne = new User("NameOne", 21, "CompanyOne", "Main street");
-        User userTwo = new User("NameTwo", 22, "CompanyTwo", "Main street");
-        User userThree = new User("NameThree", 23, "CompanyThree", "Main street");
-        User userFour = new User("NameFour", 24, "CompanyFour", "Main street");
-        User userFive = new User("NameFive", 25, "CompanyFive", "Main street");
-        List<User> users = Arrays.asList(userOne, userTwo, userThree, userFour, userFive);
 
-        Map<Integer, List<User>> usersForCheck = User.groupUsers(users);
-
-        assertEquals(5, usersForCheck.size());
-        assertTrue(usersForCheck.containsKey(23));
-        assertTrue(usersForCheck.containsKey(22));
-    }
 }
