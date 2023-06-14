e8fa22aca44f7a1c83821123d5d8bfa36fc436db
Rustam Nigmatullin <rustamnigma@mail.ru>
2023-06-14 11:37:19 +0500

    BC-235, add test

--- src/main/java/faang/school/godbless/User.java
+++ src/main/java/faang/school/godbless/User.java
@@ -1,6 +1,7 @@
 package faang.school.godbless;
 
 import lombok.Data;
+import lombok.NonNull;
 
 import java.util.List;
 import java.util.Map;
@@ -9,13 +10,29 @@ import java.util.stream.Collectors;
 
 @Data
 public class User {
+    @NonNull
     private String name;
+    @NonNull
     private int age;
+    @NonNull
     private String job;
+    @NonNull
     private String address;
 
-    public static Map<Integer, List<User>> groupUsers(List<User> users) {
+    public User(String name, int age, String job, String address){
+        this.name = name;
+        this.age = validateAge(age);
+        this.job = job;
+        this.address = address;
+    }
+
+    public Map<Integer, List<User>> groupUsers(List<User> users) {
         return users.stream()
                 .collect(Collectors.groupingBy(User::getAge));
+    }
+    private int validateAge(int age){
+         if(age < 0) throw new IllegalArgumentException();
+         if(age > 100) throw new IllegalArgumentException();
+         return age;
     }
 }
