5afdfeccb2ff42a327f7cf714ed063e22ab96aa2
Zhugalev Sergei <zhugalcf@gmail.com>
2023-06-14 13:24:49 +0300

    Creating User class, findHobbyLovers method and test

---
+++ src/test/java/faang/school/godbless/UserTest.java
@@ -0,0 +1,46 @@
+package faang.school.godbless;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class UserTest {
+    User andrey;
+    User max;
+    User peter;
+    List<User> users;
+    List<String> hobbies;
+
+    @BeforeEach
+    void setUp() {
+        hobbies = Arrays.asList("Tennis", "Volleyball", "Playing games");
+
+        andrey = new User("Andrey", 29, Arrays.asList("Football", "Volleyball", "Basketball"));
+        max = new User("Max", 36, Arrays.asList("Tennis", "Volleyball", "Basketball"));
+        peter = new User("Peter", 17, Arrays.asList("Football", "Horse riding", "Basketball"));
+
+        users = new ArrayList<>();
+        users.add(andrey);
+        users.add(max);
+        users.add(peter);
+
+
+    }
+
+    @Test
+    void testToFindHobbyLovers() {
+        Map<User, String> testHobbies = new HashMap<>();
+        testHobbies.put(andrey, "Volleyball");
+        testHobbies.put(max, "Tennis");
+
+        Assertions.assertEquals(testHobbies, User.findHobbyLovers(users, hobbies));
+    }
+}
