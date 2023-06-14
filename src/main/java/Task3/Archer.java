83f54716e73139177a5040bcae4839d1d3ca8e8e
DiegaLoL <alex.eey@mail.ru>
2023-06-14 18:04:30 +0900

    bc-424 Task and tests.

---
+++ src/main/java/Task3/Archer.java
@@ -0,0 +1,13 @@
+package Task3;
+
+public class Archer extends Character {
+    public Archer(String name) {
+        super(name, 3, 10, 5);
+    }
+
+    @Override
+    void attack(Character enemy) {
+        dealDamage(enemy, this.agility);
+    }
+
+}
--- src/main/java/Task3/Character.java
+++ src/main/java/Task3/Character.java
@@ -1,4 +1,40 @@
 package Task3;
 
+import lombok.Data;
+import org.jetbrains.annotations.NotNull;
+
+@Data
 public abstract class Character {
+    private final String name;
+    protected int power;
+    protected int agility;
+    protected int intellect;
+    protected int health = 100;
+
+    public Character(String name) {
+        this.name = name;
+    }
+
+    public Character(String name, int power, int agility, int intellect) {
+        this.name = name;
+        this.power = power;
+        this.agility = agility;
+        this.intellect = intellect;
+    }
+
+    abstract void attack(Character character);
+
+    public boolean isAlive(int damage) {
+        return this.health > damage;
+    }
+
+    protected static void dealDamage(@NotNull Character enemy, int damage) {
+        if (enemy.isAlive(damage)) {
+            enemy.health -= damage;
+        } else {
+            enemy.health = 0;
+            System.out.println("Enemy is dead!");
+        }
+    }
+
 }
