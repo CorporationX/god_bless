fceb296ff4b5ce7714b000fd842a0083e938097f
OneWayMy <zhenya.ivashchenko.9494@gmail.com>
2023-06-14 00:26:05 -0700

    task_5/Reverse

---
+++ src/main/java/faang/school/godbless/task_4/Example.java
@@ -0,0 +1,10 @@
+package faang.school.godbless.task_4;
+
+import java.util.Arrays;
+import java.util.Collections;
+
+public class Example {
+    public static void reverse(Integer[] numbers) {
+        Collections.reverse(Arrays.asList(numbers));
+    }
+}
