78eb0a50e89226f1ca469c3ae6310c8caa2b522c
Serhii Rubets <serhii.rubets@holbertonschool.com>
2023-06-14 12:12:40 -0500

    Fixed comments

--- src/main/java/faang/school/godbless/example/Example.java
+++ src/main/java/faang/school/godbless/example/Example.java
@@ -3,17 +3,18 @@ package faang.school.godbless.example;
 import java.util.List;
 
 public class Example {
-  private void reverseElements(List<Integer> nums, int left, int right) {
+  private void swapElements(List<Integer> nums, int left, int right) {
     int temp = nums.get(left);
     nums.set(left,  nums.get(right));
     nums.set(right,  temp);
   }
+
   public List<Integer> reverse(List<Integer> nums) {
     int left = 0;
     int right = nums.size() - 1;
 
     while (left < right) {
-      reverseElements(nums, left, right);
+      swapElements(nums, left, right);
       left++;
       right--;
     }
