03e6afd56d9110e36832bcf53fb39d9cdd4ae3cb
Rustam Nigmatullin <rustamnigma@mail.ru>
2023-06-15 13:16:35 +0500

    BC-345, fixed PR

--- src/main/java/faang/school/godbless/Application.java
+++ src/main/java/faang/school/godbless/Application.java
@@ -2,5 +2,6 @@ package faang.school.godbless;
 
 public class Application {
     public static void main(String... args) {
+        System.out.println(new User("Alexey" , 19, "Google", "London"));
     }
 }
--- src/main/java/faang/school/godbless/User.java
+++ src/main/java/faang/school/godbless/User.java
@@ -3,13 +3,10 @@ package faang.school.godbless;
 import lombok.Data;
 import lombok.NonNull;
 
-import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
-import java.util.stream.Collector;
 import java.util.stream.Collectors;
-import java.util.stream.Stream;
 
 @Data
 public class User {
@@ -22,32 +19,25 @@ public class User {
     @NonNull
     private String address;
 
-    public final static Set<String> VALID_JOBS = Stream.of("Google", "Uber", "Amazon")
-            .collect(Collectors.toSet());
-    public final static Set<String> VALID_ADDRESSES = Stream.of("London", "New York", "Amsterdam")
-            .collect(Collectors.toSet());
+    public final static Set<String> VALID_JOBS = Set.of("Google", "Uber", "Amazon");
+    public final static Set<String> VALID_ADDRESSES = Set.of("London", "New York", "Amsterdam");
+    public User(@NonNull String name, @NonNull int age, @NonNull String job, @NonNull String address) {
+        validate(name, age, job, address);
+        this.name = name;
+        this.age = age;
+        this.job = job;
+        this.address = address;
+    }
 
     public static Map<Integer, List<User>> groupUsers(List<User> users) {
         return users.stream()
                 .collect(Collectors.groupingBy(User::getAge));
     }
 
-    public User(@NonNull String name, @NonNull int age, @NonNull String job, @NonNull String address) {
-        this.name = name;
-        this.age = validateAge(age);
-        this.job = validateJob(job);
-        this.address = validateAddress(address);
-    }
-    private int validateAge(int age){
-        if(age < 18) throw new IllegalArgumentException();
-        else return age;
-    }
-    private String validateJob(String job){
-        if(!(VALID_JOBS.contains(job))) throw new IllegalArgumentException();
-        else return job;
-    }
-    private String validateAddress(String address){
-        if(!(VALID_ADDRESSES.contains(address))) throw new IllegalArgumentException();
-        else return address;
+    private void validate(String name, int age, String job, String address){
+        if(name.equals("\\s+") || name.length() < 2 || age < 18 || age > 100 ||
+                !(VALID_JOBS.contains(job)) || !(VALID_ADDRESSES.contains(address))){
+            throw new IllegalArgumentException();
+        }
     }
 }
