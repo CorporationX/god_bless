2117144720893716a641f623d693daacf382946d
Zhargal Budaev <budaev17@mail.ru>
2023-06-15 18:01:35 +0700

    Добавил проверку входных данных в конструктор, добавил статические константы в поле User. В Application слегка изменил передаваемые значения в юзеры для удобства работы в рамках данной задачи.

--- src/main/java/faang/school/godbless/Application.java
+++ src/main/java/faang/school/godbless/Application.java
@@ -5,11 +5,11 @@ import java.util.Map;
 
 public class Application {
     public static void main(String[] args) {
-        User user1 = new User("Kevin Flynn", 35, "ENCOM", "Center-City");
-        User user2 = new User("Alan Bradley", 35, "ENCOM", "Center-City");
-        User user3 = new User("Laura Bradley", 35, "ENCOM", "Center-City");
-        User user4 = new User("Mr. Henderson", 40, "Savings & Loan", "Washington");
-        User user5 = new User("Kevin Flynn", 27, "ENCOM", "Center-City");
+        User user1 = new User("Kevin", 17, "Uber", "Amsterdam");
+        User user2 = new User("Alan", 35, "Google", "New York");
+        User user3 = new User("Laura Bradley", 35, "Amazon", "London");
+        User user4 = new User("Mr. Henderson", 40, "Uber", "London");
+        User user5 = new User("Kevin", 27, "Google", "Amsterdam");
         List<User> users = List.of(user1, user2, user3, user4, user5);
         for(Map.Entry<Integer, List<User>> entry : User.groupUsers(users).entrySet()){
             System.out.println("age " + entry.getKey());
