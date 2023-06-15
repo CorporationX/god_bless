70bfb2b4dcb3d55a1ce36665a9d7a7e98beaa59c
zazozhik <ya.940@mail.ru>
2023-06-15 21:54:34 +0300

    Реализовал условию Четвертой задачи и написал тесты

--- src/main/java/Task4/User.java
+++ src/main/java/Task4/User.java
@@ -1,25 +1,28 @@
 package Task4;
 
+import lombok.Data;
+
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+@Data
 public class User {
     private int id;
     private String name;
     private int age;
     private List<String> manyActivities;
-    public Map<User,String> findHobbyLovers(List<User> list, List<String> manyActivities){
-        Map<User,String> map = new HashMap<>();
-        for (User user : list){
 
-            for (String activities : manyActivities){
-                if (false){
-                    map.put(user,activities);
+    public Map<User, String> findHobbyLovers(List<User> list, List<String> manyActivities) {
+        Map<User, String> map = new HashMap<>();
+        for (User user : list) {
+            for (String activities : manyActivities) {
+                if (user.getManyActivities().contains(activities)) {
+                    map.put(user, activities);
+                    break;
                 }
             }
         }
-
         return map;
     }
 }
---
+++ src/test/java/Task4/TestUser.java
@@ -0,0 +1,10 @@
+package Task4;
+
+import org.junit.jupiter.api.Test;
+
+public class TestUser {
+    @Test
+    public void testCorrectOperationOfTheMethod(){
+
+    }
+}
