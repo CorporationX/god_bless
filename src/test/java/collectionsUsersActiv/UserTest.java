1909c6cd2eb063697fb001b8739bc30f67e8388a
24k40 <rastochca.ru>
2023-06-15 15:39:54 +0700

    исправил метод сортировки

---
+++ src/test/java/collectionsUsersActiv/UserTest.java
@@ -0,0 +1,36 @@
+package collectionsUsersActiv;
+
+
+import faang.school.godbless.collectors.User;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class UserTest {
+
+    @Test
+    void findHobbyLoversTestTrue() {
+        Set<String> set = Set.of("лыжи", "теннис");
+
+        User user = new User(2332, "pavel", 34);
+        user.setActivities(Set.of("футбол"));
+
+        User user1 = new User(4545, "alex", 32);
+        user1.setActivities(Set.of("лыжы"));
+
+        List<User> users = List.of(user, user1);
+
+        Map<User, String> expected = Map.of(user, "футбол", user1, "лыжы");
+
+        Map<User, String> result = user.findHobbyLovers(users, set);
+
+        String exp = expected.get(user1);
+        String res = result.get(user1);
+        assertEquals(exp, res);
+    }
+
+}
