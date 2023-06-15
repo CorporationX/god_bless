db69528f618fe075ad894aae86ea1728a85efbbd
zazozhik <ya.940@mail.ru>
2023-06-15 13:45:14 +0300

    Реализовал условию Второй задачи и написал тесты

--- src/test/java/Task2/UserTest.java
+++ src/test/java/Task2/UserTest.java
@@ -39,22 +39,34 @@ public class UserTest {
 
         Map<Integer, List<User>> map = User.groupUsers(testUser);
 
-//        Assertions.assertEquals(testUser.get(0), map.get(22).get(0));
-//        Assertions.assertEquals(testUser.get(1), map.get(23).get(0));
-//        Assertions.assertEquals(testUser.get(2), map.get(18).get(0));
-//
-//        Assertions.assertEquals(testUser.get(3), map.get(22).get(1));
-//        Assertions.assertEquals(testUser.get(4), map.get(23).get(1));
-//        Assertions.assertEquals(testUser.get(5), map.get(18).get(1));
+        Assertions.assertEquals(testUser.get(0), map.get(22).get(0));
+        Assertions.assertEquals(testUser.get(1), map.get(23).get(0));
+        Assertions.assertEquals(testUser.get(2), map.get(18).get(0));
 
-//        List<Integer> list1;
-//        for (Map.Entry<Integer, List<User>> entry : map.entrySet()) {
-//            list1 = new ArrayList<>();
-//            int key = entry.getKey();
-//            list1.add(key);
-//        }
+        Assertions.assertEquals(testUser.get(3), map.get(22).get(1));
+        Assertions.assertEquals(testUser.get(4), map.get(23).get(1));
+        Assertions.assertEquals(testUser.get(5), map.get(18).get(1));
 
+
+    }
+
+    @Test
+    public void testGetList2() {
+        // Вторая реализация проверки корекного добавление пользователей в список,
+        // тут сравниваем объекты по hashCode, чтоб убедится, что они равны.
+        // Тест писал не один, Mark помог, самому к такой логике сложно прийти)
+        List<User> testUser = new ArrayList<>();
+
+        testUser.add(new User("Nikita", 22, "Uber", "London"));
+        testUser.add(new User("Anna", 23, "Uber", "Amsterdam"));
+        testUser.add(new User("Sofia", 18, "Google", "London"));
+        testUser.add(new User("Anton", 22, "Uber", "Amsterdam"));
+        testUser.add(new User("Ben", 23, "Amazon", "London"));
+        testUser.add(new User("Maksim", 18, "Amazon", "Amsterdam"));
+
+        Map<Integer, List<User>> map = User.groupUsers(testUser);
         Map<Integer, Integer> attempts = new HashMap<>();
+
         for (int i = 0; i < testUser.size(); i++) {
             User user = testUser.get(i);
             if (!attempts.containsKey(user.getAge())) {
@@ -62,11 +74,10 @@ public class UserTest {
             } else {
                 attempts.put(user.getAge(), attempts.get(user.getAge()) + 1);
             }
-            System.out.printf("%s+%d - %d\n", user.getName(), user.hashCode(),
+            System.out.printf("%s : %d и %d\n", user.getName(), user.hashCode(),
                     map.get(user.getAge()).get(attempts.get(user.getAge())).hashCode());
             Assertions.assertEquals(user, map.get(user.getAge()).get(attempts.get(user.getAge())));
         }
-
 
     }
 }
