f8e16aa465d47e6ec4d4c311f5561764b26ed120
M1khail <mishalion888@gmail.com>
2023-06-15 18:01:20 +0200

    task #5 (fixed algorithm & println)

--- src/main/java/faang/school/godbless/task_5/Example.java
+++ src/main/java/faang/school/godbless/task_5/Example.java
@@ -3,8 +3,8 @@ package faang.school.godbless.task_5;
 import java.util.ArrayList;
 
 public class Example {
-    public void reverse(int[] nums) {
-        for (int i = 0; i < nums.length; i++) {
+    public static void reverse(int[] nums) {
+        for (int i = 0; i < nums.length / 2; i++) {
             int head = nums[i];
             nums[i] = nums[nums.length - i - 1];
             nums[nums.length - i - 1] = head;
