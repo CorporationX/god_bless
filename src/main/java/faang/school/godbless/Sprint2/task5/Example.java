c33e91c79a432e012e76bfc9dccc425f1bfb0c4a
RasulMurtuzaliev <ras8705@mail.ru>
2023-06-15 18:25:31 +0500

    created test fo reverse

--- src/main/java/faang/school/godbless/Sprint2/task5/Example.java
+++ src/main/java/faang/school/godbless/Sprint2/task5/Example.java
@@ -1,14 +1,12 @@
 package faang.school.godbless.Sprint2.task5;
 
-import java.util.Arrays;
-
 public class Example {
-    public static void reverse(int[] nums){
-        for(int i=0; i<nums.length/2; i++){
+    public static int[] reverse(int[] nums) {
+        for (int i = 0; i < nums.length / 2; i++) {
             int temp = nums[i];
-            nums[i] = nums[nums.length-1-i];
-            nums[nums.length-1-i] = temp;
+            nums[i] = nums[nums.length - 1 - i];
+            nums[nums.length - 1 - i] = temp;
         }
-        System.out.println(Arrays.toString(nums));
+        return nums;
     }
 }
