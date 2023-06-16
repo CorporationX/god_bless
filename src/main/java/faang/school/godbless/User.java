ae5b9eb3d59214f84eaf7cbb92d0bb4c08605145
Zlata Galaburdina <585zlata@mail.ru>
2023-06-16 23:02:15 +0300

    new commit

---
+++ src/main/java/faang/school/godbless/User.java
@@ -0,0 +1,21 @@
+package faang.school.godbless;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class User {
+    public int id;
+    public String name;
+    public int age;
+    public ArrayList <String>;
+
+    public Map<User, String> findHobbyLovers(List<String> listOfUsers, ){
+        List<String> listOfUsers = new ArrayList<String>();
+
+        Map <User, String > map = new HashMap<>();
+        return map;
+
+    }
+}
