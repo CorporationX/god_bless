dcefb5b111319f818723dc7f2b2be2556da74833
chaykinks <chaykinks@gmail.com>
2023-06-16 18:35:47 +0500

    создал классы Warrior и Archer, переопределил конструкторы

---
+++ src/main/java/faang/school/godbless/Archer.java
@@ -0,0 +1,11 @@
+package faang.school.godbless;
+
+public class Archer extends Character{
+    public Archer(String name) {
+        super(name);
+    }
+
+    public Archer(String name, int strength, int dexterity, int intelligence) {
+        super(name, 3, 10, 5);
+    }
+}
---
+++ src/main/java/faang/school/godbless/Warrior.java
@@ -0,0 +1,11 @@
+package faang.school.godbless;
+
+public class Warrior extends Character {
+    public Warrior(String name) {
+        super(name);
+    }
+
+    public Warrior(String name, int strength, int dexterity, int intelligence) {
+        super(name, 10, 5, 3);
+    }
+}
