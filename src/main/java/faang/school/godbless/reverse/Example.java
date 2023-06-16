fdf4879d9d53cd4eb583e706a4819d5e661d723a
Julia Koroleva <youlka25@yandex.ru>
2023-06-16 21:59:02 +0300

    Правит

--- src/main/java/faang/school/godbless/reverse/Example.java
+++ src/main/java/faang/school/godbless/reverse/Example.java
@@ -6,20 +6,11 @@ public class Example {
         var i = 0;
         var j = array.length - 1;
 
-        if (array.length % 2 != 0) {
-            while (i != j) {
-                var tmp = array[i];
-                array[i] = array[j];
-                array[j] = tmp;
-                i++; j--;
-            }
-        } else {
-            while (i != array.length / 2) {
-                var tmp = array[i];
-                array[i] = array[j];
-                array[j] = tmp;
-                i++; j--;
-            }
+        while (i < j) {
+            var tmp = array[i];
+            array[i] = array[j];
+            array[j] = tmp;
+            i++; j--;
         }
 
         return array;
