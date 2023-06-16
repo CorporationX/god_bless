1afc609c8df58b3198aade2d6fb1dda244d84660
Abramov Nikita <mr.abramovn@gmail.com>
2023-06-16 23:58:21 +0300

    fix bug

--- src/main/java/faang/school/godbless/reverse/Example.java
+++ src/main/java/faang/school/godbless/reverse/Example.java
@@ -1,22 +1,13 @@
 package faang.school.godbless.reverse;
 
-import java.lang.reflect.Array;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-
 public class Example {
-    public static void main(String[] args) {
-        List<Integer> nums = Arrays.asList(1, 2, 3, 4, 5);
-        reverse(nums);
-    }
-
-    public static void reverse(List<Integer> nums) {
-        List<Integer> result = new ArrayList<>();
-        System.out.println(nums);
-        for (int i = nums.size() - 1; i >= 0; i--) {
-            result.add(nums.get(i));
+    public static void reverse(int[] array) {
+        int j = array.length - 1;
+        for (int i = 0; i < array.length / 2; i++) {
+            int temp = array[j];
+            array[j] = array[i];
+            array[i] = temp;
+            j--;
         }
-        System.out.println(result);
     }
 }
