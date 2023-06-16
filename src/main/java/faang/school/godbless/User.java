20b9c555e67af376f1dbb8a40fdcfad4bd204f91
Dobrovolsky Artem <artdobrov@mail.ru>
2023-06-16 23:47:29 +0300

    Изменены аннотации в классе User

--- src/main/java/faang/school/godbless/User.java
+++ src/main/java/faang/school/godbless/User.java
@@ -1,14 +1,18 @@
 package faang.school.godbless;
 
 
+import lombok.AllArgsConstructor;
 import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
 
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
 
+@AllArgsConstructor
 @EqualsAndHashCode
+@NoArgsConstructor
 public class User {
 
     private int id;
@@ -17,16 +21,18 @@ public class User {
     private Set<String> hobbies;
 
     public Map<User, String> findHobbyLovers(List<User> users, Set<String> hobbiesSet) {
-        Map<User, String> hobbyLover = new HashMap<>();
+        Map<User, String> hobbyLovers = new HashMap<>();
 
         for (User user : users) {
             for (String hobby : user.hobbies) {
                 if (hobbiesSet.contains(hobby)) {
-                    hobbyLover.put(user, hobby);
+                    if (!hobbyLovers.containsKey(user)) {
+                        hobbyLovers.put(user, hobby);
+                    }
                 }
             }
         }
-        return hobbyLover;
+        return hobbyLovers;
     }
 
 }
