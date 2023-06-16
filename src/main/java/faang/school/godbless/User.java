72f02258898f6ca525f98c20bdf13d08f3ddd17c
chaykinks <chaykinks@gmail.com>
2023-06-16 19:55:50 +0500

    добавил класс User и метод findHobbyLovers

--- src/main/java/faang/school/godbless/User.java
+++ src/main/java/faang/school/godbless/User.java
@@ -1,38 +1,34 @@
 package faang.school.godbless;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+
+import java.util.*;
 
 public class User {
+    private int id;
     private String name;
     private int age;
-    private String workplace;
-    private String address;
+    private Set<String> hobbies;
 
-    public int getAge() {
-        return age;
+    public Set<String> getHobbies() {
+        return hobbies;
     }
 
-    public User(String name, int age, String workplace, String address) {
-        this.name = name;
-        this.age = age;
-        this.workplace = workplace;
-        this.address = address;
-    }
-
-    public static Map<Integer, List<User>> groupUsers(List<User> userList) {
-        Map<Integer, List<User>> groupedUserMap = new HashMap<>();
-        for (User user : userList) {
-            int age = user.getAge();
-            if (!groupedUserMap.containsKey(age)) {
-                groupedUserMap.put(age, new ArrayList<>());
+    public Map<User, String> findHobbyLovers(List<User> users, Set<String> activities) {
+        Map<User, String> hobbyLoversMap = new HashMap<>();
+        for (User user : users) {
+            Set<String> userHobbies = new HashSet<>(user.getHobbies());
+            if (!userHobbies.isEmpty()) {
+                for (String hobby : userHobbies) {
+                    if (activities.contains(hobby)) {
+                        hobbyLoversMap.put(user, hobby);
+                        break;
+                    }
+                }
             }
-            groupedUserMap.get(age).add(user);
         }
-        return groupedUserMap;
+        return hobbyLoversMap;
     }
+
 }
 
 
