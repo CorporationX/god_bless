29d928a32f4b9a043685e0b390d681551c2633cc
Dobrovolsky Artem <artdobrov@mail.ru>
2023-06-16 21:23:16 +0300

    Челендж был в том, чтобы решить никуда не подсматривая. Когда написал, появилось ощущение, что я где-то такое видел уже. Вроде работает, написал правда сам, доволен собой)

--- src/main/java/faang/school/godbless/Example.java
+++ src/main/java/faang/school/godbless/Example.java
@@ -1,12 +1,13 @@
 package faang.school.godbless;
 
 public class Example {
-    public static int[] reverse(int nums[]) {
-        int[] result = new int[nums.length];
+    public static int[] reverse(int[] nums) {
 
-        for (int i = 0; i < result.length; i++) {
-            result[i] = nums[nums.length - 1 - i];
+        for (int i = 0; i < nums.length / 2; i++) {
+            nums[i] += nums[nums.length - 1 - i];
+            nums[nums.length - 1 - i] = nums[i] - nums[nums.length - 1 - i];
+            nums[i] -= nums[nums.length - 1 - i];
         }
-        return result;
+        return nums;
     }
 }
