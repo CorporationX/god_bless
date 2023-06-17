75d3fc14a5d24f64aec0ea6fe67c49d9934b6a4b
Mikhail <mikchailmartinov@gmail.com>
2023-06-17 22:03:17 +0300

    Task 2

---
+++ src/main/java/faang/school/godbless/sprint2/task2/Main.java
@@ -0,0 +1,10 @@
+package faang.school.godbless.sprint2.task2;
+
+public class Main {
+    public static void main(String[] args) {
+        User user1 = new User("Zhenya", 21, "Google", "London");
+        User user2 = new User("", 20, "Google", "London");
+        User user3 = new User("Ivan", 17, "Google", "London");
+        System.out.println("!");
+    }
+}
