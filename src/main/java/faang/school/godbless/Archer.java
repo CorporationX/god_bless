2bda5583fbed87d3359524767eb56ae0461e4f80
Angie <angie@>
2023-06-17 15:28:44 +0300

    add commit

---
+++ src/main/java/faang/school/godbless/Archer.java
@@ -0,0 +1,15 @@
+package main.java.faang.school.godbless;
+    class Archer extends Character {
+        public Archer(String name) {
+            super(name);
+            this.strength = 3;
+            this.agility = 10;
+            this.intelligence = 5;
+        }
+
+        @Override
+        public void attack(Character character) {
+            character.health -= this.agility;
+        }
+    }
+
--- src/main/java/faang/school/godbless/Main.java
+++ src/main/java/faang/school/godbless/Main.java
@@ -1,4 +1,7 @@
 package main.java.faang.school.godbless;
 
 public class Main {
+    public static void main(String[] args) {
+
+    }
 }
