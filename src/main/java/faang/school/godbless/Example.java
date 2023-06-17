3d1a5fc8498c9ddfe78e659450598228139cab66
Zlata Galaburdina <585zlata@mail.ru>
2023-06-17 10:42:03 +0300

    Понимаю, что очень длинная программа получилась. Добавила для своей видимости строки: System.out.println(Arrays.toString(array)); System.out.println(Arrays.toString(array)); (могу убрать в следующем push). Я ведь правильно понимаю, что считывание целых чисел в массив происходит с клавиатуры? А ещё, после считывания с клавиатуры программа дальше не выполняется... Спросила GPT, он пишет, что : "Проблема заключается в том, что вы забыли присвоить измененный массив обратно в переменную array." И предлаагает через copyOf, вот так " int[] originalArray = Arrays.copyOf(array, array.length); int[] reversedArray = reverse(array);" Но мне не понятно все еще, отчего array не читается начитается со строки 24...

--- src/main/java/faang/school/godbless/Example.java
+++ src/main/java/faang/school/godbless/Example.java
@@ -1,17 +1,30 @@
 package faang.school.godbless;
+
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Example {
 
     public static void main(String[] args) {
         Scanner console = new Scanner(System.in);
-        int[] array = new int[5];
-        for (int i = 0; i < array.length; i++) {
-            array[i] = console.nextInt();
+        ArrayList<Integer> nums = new ArrayList<>();
+
+        int num;
+        while (console.hasNextInt()){
+            num = console.nextInt();
+            nums.add(num);
         }
-        System.out.println(array);
+
+        int[] array = new int[nums.size()];
+        for (int i = 0; i < nums.size(); i++) {
+            array[i] = nums.get(i);
+        }
+
+        System.out.println(Arrays.toString(array));
 
         reverse(array);
+        System.out.println(Arrays.toString(array));
     }
 
     public static int[] reverse(int[] array) {
@@ -24,3 +37,15 @@ public class Example {
         return array;
     }
 }
+//
+//    public static void main(String[] args) {
+//        Scanner console = new Scanner(System.in);
+//        int[] array = new int[array.length];
+//        for (int i = 0; i < array.length; i++) {
+//            array[i] = console.nextInt();
+//        }
+//        System.out.println(Arrays.toString(array));
+//
+//        reverse(array);
+//        System.out.println(Arrays.toString(array));
+//    }
