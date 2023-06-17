06c54467b011816839a5ce0dcc1bf26af6328511
Angie <angie@>
2023-06-17 19:54:10 +0300

    done

---
+++ src/main/java/faang/school/godbless/Main.java
@@ -0,0 +1,12 @@
+package main.java.faang.school.godbless;
+
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) {
+        int [] nums = new int[] {7, 4, 3, 5, 8, 9 };
+        Example.reverse(nums);
+        System.out.println(Arrays.toString(nums));
+    }
+}
+
