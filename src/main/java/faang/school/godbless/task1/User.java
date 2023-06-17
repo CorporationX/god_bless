da316cb2d602bd765f11db75565d487587f33265
bvxxvd <bvxxvd@gmail.com>
2023-06-17 18:00:09 +0300

    Выполнена задача "Регистрация пользователя"

--- src/main/java/faang/school/godbless/task1/User.java
+++ src/main/java/faang/school/godbless/task1/User.java
@@ -2,18 +2,41 @@ package faang.school.godbless.task1;
 
 import lombok.Getter;
 import lombok.Setter;
-import lombok.AllArgsConstructor;
 
 import java.util.*;
 
 @Getter
 @Setter
-@AllArgsConstructor
 public class User {
+
+    private static final Set<String> VALID_JOBS = Set.of("Google", "Uber", "Amazon");
+    private static final Set<String> VALID_ADDRESSES = Set.of("London", "New York", "Amsterdam");
+    private static final int MIN_AGE = 18;
+
     private String name;
     private int age;
     private String workplace;
     private String address;
+
+    public User(String name, int age, String workplace, String address) {
+        if (name == null || name.isEmpty()) {
+            throw new IllegalArgumentException("Name cannot be empty.");
+        }
+        if (age < MIN_AGE) {
+            throw new IllegalArgumentException("Age cannot be less than " + MIN_AGE + ".");
+        }
+        if (!VALID_JOBS.contains(workplace)) {
+            throw new IllegalArgumentException("Invalid job.");
+        }
+        if (!VALID_ADDRESSES.contains(address)) {
+            throw new IllegalArgumentException("Invalid address.");
+        }
+
+        this.name = name;
+        this.age = age;
+        this.workplace = workplace;
+        this.address = address;
+    }
 
     public static Map<Integer, List<User>> groupUsers(List<User> users) {
         Map<Integer, List<User>> groupedUsers = new HashMap<>();
