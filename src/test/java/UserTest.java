835326c5291843338aec3c23a0bd937437c852d9
”Ramazan” <mansurov.r13@gmail.com>
2023-06-17 16:01:55 +0300

    Added user constructor and test for it

--- src/test/java/UserTest.java
+++ src/test/java/UserTest.java
@@ -2,33 +2,72 @@ import faang.school.godbless.model.User;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 public class UserTest {
-    private List<User> userList;
+    private static final List<String> JOBS = List.of("Google", "Uber", "Amazon");
+    private static final List<String> ADDRESSES = List.of("London", "New York", "Amsterdam");
+    private static final Random RANDOM = new Random();
+    private static List<User> userList;
 
     @BeforeEach
-    private void init() {
+    private void setUpUserList() {
         userList = new ArrayList<>();
         for (int i = 0; i < 30; i++) {
             userList.add(new User(
                     "User-" + i,
-                    (int) (Math.random() * 40 + 18),
-                    "Job-" + i,
-                    "Address-" + i
+                    RANDOM.nextInt(18, 100),
+                    JOBS.get(RANDOM.nextInt(0,3)),
+                    ADDRESSES.get(RANDOM.nextInt(0, 3))
             ));
         }
     }
 
     @Test
-    public void groupUsersByAgeTest() {
+    void groupUsersByAgeTest() {
         for (List<User> users : User.groupUsers(userList).values()) {
             assertTrue(users.stream()
                     .allMatch(user -> user.getAge() == users.get(0).getAge())
             );
         }
+    }
+
+    @Test
+    void groupList_shouldReturnEmptyMapTest() {
+        assertTrue(User.groupUsers(Collections.emptyList()).isEmpty());
+    }
+
+    @Test
+    void userConstructor_shouldThrowExceptionWithBlankName() {
+        assertThrows(IllegalArgumentException.class, () -> new User("", 30, "Uber","London"));
+        assertThrows(
+                IllegalArgumentException.class, () -> new User("", 30, "Uber","London"),
+                "Name could not be blank");
+    }
+
+    @Test
+    void userConstructor_shouldThrowExceptionWithInvalidAge() {
+        assertThrows(IllegalArgumentException.class, () -> new User("Tom", 13, "Uber","London"),
+                "Age could not be less than 18");
+    }
+
+    @Test
+    void userConstructor_shouldThrowExceptionWithIncorrectJob() {
+        assertThrows(IllegalArgumentException.class, () -> new User("Tom", 30, "Avito","London"),
+                "Job name is incorrect");
+    }
+
+    @Test
+    void userConstructor_shouldThrowExceptionWithIncorrectAddress() {
+        assertThrows(IllegalArgumentException.class, () -> new User("Tom", 30, "Uber","Moscow"),
+                "Address is incorrect");
+    }
+
+    @Test
+    void userConstructor_shouldReturnNewUser() {
+        User actual = new User("Tom", 30, "Google", "London");
+        assertEquals(actual, new User("Tom", 30, "Google", "London"));
     }
 }
