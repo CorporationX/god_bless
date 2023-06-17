04a103afc45c9ab3cf970f5b60560b8fa89b6cea
Stepan Palkov <step.palkov@gmail.com>
2023-06-17 21:40:13 +0300

    add test package

---
+++ src/test/java/faang/school/godbless/UserTest.java
@@ -0,0 +1,11 @@
+package faang.school.godbless;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+class UserTest {
+
+    @Test
+    void groupUsers() {
+    }
+}
