e12680328531594aa21253009169d420c6cdeac9
Artem Afonin <art002.2000@mail.ru>
2023-06-19 01:42:01 +0300

    Выполнил задачу "Регистрация пользователей"

---
+++ src/main/java/faang/school/godbless/user_registration/Application.java
@@ -0,0 +1,8 @@
+package faang.school.godbless.user_registration;
+
+public class Application {
+    public static void main(String... args) {
+        User user1 = new User("Artem", 23, "Google", "New York");
+        System.out.println(user1);
+    }
+}
