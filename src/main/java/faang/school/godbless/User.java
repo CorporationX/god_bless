20bd315081abbd4f3fd6f1a05d8a81d5a213d6a2
Adil Ilchubaev <adil.200191@gmail.com>
2023-06-18 18:44:02 +0600

    Создал новый класс User с необходимым функционалом группировки пользователей по возрасту

---
+++ src/main/java/faang/school/godbless/User.java
@@ -0,0 +1,66 @@
+package faang.school.godbless;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class User {
+    private String name;
+    private int age;
+    private String jobLocation;
+    private String address;
+
+    public User(String name, int age, String jobLocation, String address) {
+        this.name = name;
+        this.age = age;
+        this.jobLocation = jobLocation;
+        this.address = address;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getJobLocation() {
+        return jobLocation;
+    }
+
+    public void setJobLocation(String jobLocation) {
+        this.jobLocation = jobLocation;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+    public static Map<Integer, List<User>> groupUsers(List<User> users) {
+        Map<Integer, List<User>> result = new HashMap<>();
+        for (User user : users) {
+            List<User> usersGroup;
+            if (!result.containsKey(user.age)) {
+                usersGroup = new ArrayList<>();
+            } else {
+                usersGroup = result.get(user.age);
+            }
+            usersGroup.add(user);
+            result.put(user.age, usersGroup);
+        }
+        return result;
+    }
+}
