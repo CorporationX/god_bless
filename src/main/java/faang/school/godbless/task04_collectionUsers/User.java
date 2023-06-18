f222ab02b2b273fed31a5e4a039caf9604ed5ef7
Azat <azat.takhautdinov@gmail.com>
2023-06-18 23:44:15 +0500

    Исправил ошибки

--- src/main/java/faang/school/godbless/task04_collectionUsers/User.java
+++ src/main/java/faang/school/godbless/task04_collectionUsers/User.java
@@ -9,15 +9,14 @@ public class User {
     private int id;
     private int age;
     private String name;
-    private Set<String> arrayActivities;
-
+    private Set<String> Activities;
 
     public Map<User, String> findHobbyLovers(List<User> users, Set<String> arrayActivities) {
         Map<User, String> mapActivities = new HashMap<>();
-        for (User user:users) {
-            for (String activity:user.arrayActivities) {
+        for (User user : users) {
+            for (String activity : user.Activities) {
                 if (arrayActivities.contains(activity)) {
-                    mapActivities.put(user,activity);
+                    mapActivities.put(user, activity);
                     break;
                 }
             }
