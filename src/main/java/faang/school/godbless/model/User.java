ded4c65ace050b112eed5b2bc5a1e1fb037669fa
Sabina-Vishnya <Poecsta@gmail.com>
2023-06-18 03:52:01 +0300

    BC-551 User registration

--- src/main/java/faang/school/godbless/model/User.java
+++ src/main/java/faang/school/godbless/model/User.java
@@ -5,6 +5,7 @@ import lombok.Data;
 
 import java.util.List;
 import java.util.Map;
+import java.util.Set;
 import java.util.stream.Collectors;
 
 @Data
@@ -14,6 +15,20 @@ public class User {
     private byte age;
     private String job;
     private String address;
+    private static byte VALID_AGE = 18;
+    private static Set<String> VALID_JOBS = Set.of("Google", "Uber", "Amazon");
+    private static Set<String> VALID_ADDRESSES = Set.of("London", "New York", "Amsterdam");
+
+    public User(String name, byte age, String job, String address) {
+        if (!name.isBlank() && age >= VALID_AGE && VALID_JOBS.contains(job) && VALID_ADDRESSES.contains(address)) {
+            this.name = name;
+            this.age = age;
+            this.job = job;
+            this.address = address;
+        } else {
+            throw new IllegalArgumentException();
+        }
+    }
 
     public static Map<Byte, List<User>> groupUsers(List<User> users) {
         return users.stream().collect(Collectors.groupingBy(User::getAge));
--- src/test/java/faang/school/godbless/model/UserTest.java
+++ src/test/java/faang/school/godbless/model/UserTest.java
@@ -1,58 +1,82 @@
 package faang.school.godbless.model;
 
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 
 import java.util.List;
 import java.util.Map;
 
 import static org.junit.jupiter.api.Assertions.assertAll;
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 class UserTest {
-    private static final byte AGE_BY_1 = 1;
-    private static final byte AGE_BY_2 = 2;
-    private static final byte AGE_BY_5 = 5;
-    private static final String USER_NAME_TEST1 = "Test1";
-    private static final String USER_NAME_TEST2 = "Test2";
-    private static final String USER_NAME_TEST3 = "Test3";
-    private static final String USER_NAME_TEST4 = "Test4";
-    private static final String USER_NAME_TEST5 = "Test5";
 
-    @Test
-    void groupUsersTest() {
-        Map<Byte, List<User>> actual = User.groupUsers(getUsers());
+    private static final String USER_NAME = "Username";
+    private static final byte VALID_AGE = 18;
+    private static final String VALID_JOB = "Uber";
+    private static final String VALID_ADDRESS = "New York";
+    private static final byte INVALID_AGE = 17;
+    private static final String INVALID_JOB = "Invalid job";
+    private static final String INVALID_ADDRESS = "Invalid address";
 
-        assertEquals(3, actual.size());
 
-        assertAll("Grouped users by age 1",
-            () -> assertTrue(actual.containsKey(AGE_BY_1)),
-            () -> assertEquals(3, actual.get(AGE_BY_1).size()),
-            () -> assertTrue(actual.get(AGE_BY_1).stream().anyMatch(user -> user.getName().equals(USER_NAME_TEST1))),
-            () -> assertTrue(actual.get(AGE_BY_1).stream().anyMatch(user -> user.getName().equals(USER_NAME_TEST4))),
-            () -> assertTrue(actual.get(AGE_BY_1).stream().anyMatch(user -> user.getName().equals(USER_NAME_TEST5)))
-        );
+    @Nested
+    @DisplayName("User creation")
+    class UserCreationTest {
 
-        assertAll("Grouped users by age 3",
-            () -> assertTrue(actual.containsKey(AGE_BY_2)),
-            () -> assertEquals(1, actual.get(AGE_BY_2).size()),
-            () -> assertTrue(actual.get(AGE_BY_2).stream().anyMatch(user -> user.getName().equals(USER_NAME_TEST2)))
-        );
+        @Test
+        void whenValidUserThenCreate() {
+            assertDoesNotThrow(() -> new User(USER_NAME, VALID_AGE, VALID_JOB, VALID_ADDRESS));
+        }
 
-        assertAll("Grouped users by age 5",
-            () -> assertTrue(actual.containsKey(AGE_BY_5)),
-            () -> assertEquals(1, actual.get(AGE_BY_5).size()),
-            () -> assertTrue(actual.get(AGE_BY_5).stream().anyMatch(user -> user.getName().equals(USER_NAME_TEST3)))
-        );
+        @Test
+        void whenInvalidUserThenThrowExc() {
+            assertThrows(IllegalArgumentException.class, () -> new User(USER_NAME, INVALID_AGE, INVALID_JOB, INVALID_ADDRESS));
+        }
     }
 
-    private List<User> getUsers() {
-        return List.of(
-            User.builder().name(USER_NAME_TEST1).age(AGE_BY_1).job("job").address("address").build(),
-            User.builder().name(USER_NAME_TEST2).age(AGE_BY_2).job("job").address("address").build(),
-            User.builder().name(USER_NAME_TEST3).age(AGE_BY_5).job("job").address("address").build(),
-            User.builder().name(USER_NAME_TEST4).age(AGE_BY_1).job("job").address("address").build(),
-            User.builder().name(USER_NAME_TEST5).age(AGE_BY_1).job("job").address("address").build()
-        );
+    @Nested
+    @DisplayName("groupUsers")
+    class GroupUsersTest {
+
+        @Test
+        void whenValidUserThenReturnMap() {
+            Map<Byte, List<User>> actual = User.groupUsers(getValidUsers());
+
+            assertEquals(1, actual.size());
+
+            assertAll("Grouped users by age 18",
+                () -> assertTrue(actual.containsKey(VALID_AGE)),
+                () -> assertEquals(3, actual.get(VALID_AGE).size()),
+                () -> assertTrue(actual.get(VALID_AGE).stream().anyMatch(user -> user.getName().equals(USER_NAME))),
+                () -> assertTrue(actual.get(VALID_AGE).stream().anyMatch(user -> user.getName().equals(USER_NAME))),
+                () -> assertTrue(actual.get(VALID_AGE).stream().anyMatch(user -> user.getName().equals(USER_NAME)))
+            );
+        }
+
+        @Test
+        void whenInvalidUserThenThrowExc() {
+            assertThrows(IllegalArgumentException.class, () -> User.groupUsers(getInvalidUsers()));
+        }
+
+        private List<User> getValidUsers() {
+            return List.of(
+                User.builder().name(USER_NAME).age(VALID_AGE).job(VALID_JOB).address(VALID_ADDRESS).build(),
+                User.builder().name(USER_NAME).age(VALID_AGE).job(VALID_JOB).address(VALID_ADDRESS).build(),
+                User.builder().name(USER_NAME).age(VALID_AGE).job(VALID_JOB).address(VALID_ADDRESS).build()
+            );
+        }
+
+        private List<User> getInvalidUsers() {
+            return List.of(
+                User.builder().name(USER_NAME).age(INVALID_AGE).job(INVALID_JOB).address(INVALID_ADDRESS).build(),
+                User.builder().name(USER_NAME).age(INVALID_AGE).job(INVALID_JOB).address(INVALID_ADDRESS).build(),
+                User.builder().name(USER_NAME).age(INVALID_AGE).job(INVALID_JOB).address(INVALID_ADDRESS).build()
+            );
+        }
     }
 }
