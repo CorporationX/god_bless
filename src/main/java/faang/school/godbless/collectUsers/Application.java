4afd28bec60551d41c0ca1c4649a60cd8aeff224
Dmitry Sorokin <dm.sorokin21@gmail.com>
2023-06-18 16:31:19 +0300

    created package

--- src/main/java/faang/school/godbless/collectUsers/Application.java
+++ src/main/java/faang/school/godbless/collectUsers/Application.java
@@ -1,8 +1,7 @@
-package faang.school.godbless;
+package faang.school.godbless.collectUsers;
 
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 
--- src/main/java/faang/school/godbless/collectUsers/User.java
+++ src/main/java/faang/school/godbless/collectUsers/User.java
@@ -1,4 +1,4 @@
-package faang.school.godbless;
+package faang.school.godbless.collectUsers;
 
 import java.util.HashMap;
 import java.util.List;
