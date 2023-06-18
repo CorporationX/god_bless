d889ea41ebc59df1032b765d3327f250766a7a34
Zlata Galaburdina <585zlata@mail.ru>
2023-06-18 17:09:06 +0300

    First attempt

--- src/main/java/faang/school/godbless/User.java
+++ src/main/java/faang/school/godbless/User.java
@@ -1,6 +1,5 @@
 package faang.school.godbless;
 
-import java.lang.ref.SoftReference;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
@@ -10,30 +9,20 @@ public class User {
     public int id;
     public String name;
     public int age;
-    public List <String> actions;
+    public List<String> actions;
 
-    public Map<User, String> findHobbyLovers(List<String> listOfUsers, List <String> overallActions){
+    public Map<String, String> findHobbyLovers(List<String> listOfUsers, List<String> overallActions) {
         listOfUsers = new ArrayList<String>();
         overallActions = new ArrayList<String>();
         // overallActions - большой общий list с активностями
         // а у каждого user есть свой (list) actions  его обственными активностями
-        Map <User, String > map = new HashMap<>();
-        for (String user: listOfUsers){
-            for (String commonAction: overallActions) {
-                if (user.contains(commonAction))
-                map.putIfAbsent(user, commonAction);
+        Map<String, String> map = new HashMap<>();
+        for (String user : listOfUsers) {
+            for (String userAction : actions) {
+                if (overallActions.contains(userAction))
+                    map.putIfAbsent(user, userAction);
             }
         }
         return map;
     }
 }
-
-//    public Map<User, String> findHobbyLovers(List<String> listOfUsers, List <String> actions){
-//        listOfUsers = new ArrayList<String>();
-//        Map <User, String > map = new HashMap<>();
-//        for (String user: listOfUsers){
-//            for (String action: actions) {
-//                map.putIfAbsent(user, action);
-//            }
-//        }
-//        return map;
