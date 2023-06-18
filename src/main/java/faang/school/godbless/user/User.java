66a16faee0cbf24cba104e138cc79a060502a4a3
Dmitry Sorokin <dm.sorokin21@gmail.com>
2023-06-18 16:45:41 +0300

    resolved task2

--- src/main/java/faang/school/godbless/user/User.java
+++ src/main/java/faang/school/godbless/user/User.java
@@ -4,14 +4,30 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Set;
 
 public class User {
+    private static final Set<String> VALID_JOBS = Set.of("Google", "Uber", "Amazon");
+    private static final Set<String> VALID_ADDRESSES = Set.of("London", "New York", "Amsterdam");
     private String name;
     private int age;
     private String workPlace;
     private String address;
 
     public User(String name, int age, String workPlace, String address) {
+        if (name == null || name.isBlank()) {
+            throw new IllegalArgumentException("User name null or blank");
+        }
+        if (age < 18) {
+            throw new IllegalArgumentException("User age is not valid");
+        }
+        if (!VALID_JOBS.contains(workPlace)) {
+            throw new IllegalArgumentException("User workPlace is not valid");
+        }
+        if (!VALID_ADDRESSES.contains(address)) {
+            throw new IllegalArgumentException("User address is not valid");
+        }
+
         this.name = name;
         this.age = age;
         this.workPlace = workPlace;
