390e51499dcf82dbadc8c5e6aa15c35c3339d100
Zlata Galaburdina <585zlata@mail.ru>
2023-06-18 15:31:22 +0300

    First commit

--- src/main/java/faang/school/godbless/User.java
+++ src/main/java/faang/school/godbless/User.java
@@ -1,5 +1,6 @@
 package faang.school.godbless;
 
+import java.lang.ref.SoftReference;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
@@ -9,13 +10,27 @@ public class User {
     public int id;
     public String name;
     public int age;
-    public ArrayList <String> actions;
+    public List <String> actions;
 
-    public Map<User, String> findHobbyLovers(List<String> listOfUsers, ArrayList <String> actions){
-        List<String> listOfUsers = new ArrayList<String>();
-
+    public Map<User, String> findHobbyLovers(List<String> listOfUsers, List <String> actions){
+        listOfUsers = new ArrayList<String>();
         Map <User, String > map = new HashMap<>();
+        for (String user: listOfUsers){
+            for (String action: actions) {
+                if ()
+                map.putIfAbsent(user, action);
+            }
+        }
         return map;
-
     }
 }
+
+//    public Map<User, String> findHobbyLovers(List<String> listOfUsers, List <String> actions){
+//        listOfUsers = new ArrayList<String>();
+//        Map <User, String > map = new HashMap<>();
+//        for (String user: listOfUsers){
+//            for (String action: actions) {
+//                map.putIfAbsent(user, action);
+//            }
+//        }
+//        return map;
