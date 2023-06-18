6e2b780492f3c6791df6d708b8bbc247358e27e8
bvxxvd <bvxxvd@gmail.com>
2023-06-18 10:10:47 +0300

    Выполнена задача "Развернитесь!"

---
+++ src/main/java/faang/school/godbless/task4/Example.java
@@ -0,0 +1,20 @@
+package faang.school.godbless.task4;
+
+public class Example {
+    public static int[] reverse(int[] nums) {
+        int left = 0;
+        int right = nums.length - 1;
+
+        while (left < right) {
+            int temp = nums[left];
+            nums[left] = nums[right];
+            nums[right] = temp;
+
+            left++;
+            right--;
+        }
+
+        return nums;
+    }
+}
+
