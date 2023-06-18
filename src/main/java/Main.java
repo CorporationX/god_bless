97cd6f822794d9f499664091afc3cb686381d1ce
b.mukhtarkhanov <b.mukhtarkhanov>
2023-06-18 17:48:03 +0600

    BC-543. Reverse

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,14 +1,11 @@
 import faang.school.godbless.groupUsers.model.User;
+import faang.school.godbless.reverse.Example;
+
+import java.util.Arrays;
 import java.util.List;
 import java.util.Set;
 
 public class Main {
     public static void main(String[] args) {
-        var users = List.of(new User(1, "John", 23, Set.of("swimming", "games")),
-                new User(2, "Alex", 25, Set.of("swimming", "cooking")),
-                new User(3, "Marty", 18, Set.of("boxing", "swimming")),
-                new User(4, "Gloria", 18, Set.of("reading", "cooking")));
-        var map = User.findHobbyLovers(users, Set.of("swimming", "cooking"));
-        System.out.println(map);
     }
 }
--- src/main/java/faang/school/godbless/Example.java
+++
@@ -1,7 +0,0 @@
-package faang.school.godbless;
-
-public class Example {
-    public int[] reverse(int[] nums) {
-        return null;
-    }
-}
