ef9451e75086c385f2c1635e1b523066106ec374
Viktor Ivanov <avacode78@gmail.com>
2023-06-18 11:45:38 +0700

    implement task and write test for it.

---
+++ src/main/java/handle_exception_beautifully/ExceptionHandlingUtil.java
@@ -0,0 +1,13 @@
+package handle_exception_beautifully;
+
+import java.util.function.Supplier;
+
+public class ExceptionHandlingUtil {
+    public static <T> T withErrorHandling(Supplier<T> action, ExceptionHandler<T> onError) {
+        try {
+            return action.get();
+        } catch (Exception e) {
+            return onError.handle(e);
+        }
+    }
+}
