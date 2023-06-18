321a9ee42b89234cd5984011451d4131965ae140
Egor Selivanov <egorselivanov0@gmail.com>
2023-06-19 01:16:25 +0300

    Завершена задача "Развернитесь!"

--- src/main/java/faang/school/godbless/Application.java
+++ src/main/java/faang/school/godbless/Application.java
@@ -1,7 +1,15 @@
 package faang.school.godbless;
 
+import faang.school.godbless.turnAround.Example;
+
 public class Application {
     public static void main(String... args) {
+        int[] array = new int[] {1, 2, 4, 7};
+        Example example = new Example();
+        array = example.reverse(array);
 
+        for (int num : array) {
+            System.out.print(num + " ");
+        }
     }
 }
