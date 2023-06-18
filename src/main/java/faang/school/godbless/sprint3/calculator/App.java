98e4178db09c4c928d42d613469f72229fd0dbfd
Basimov Rustam <sombra1nf@yandex.ru>
2023-06-18 18:10:47 +0500

    BC-582. Завершил задачу. Покрыл тестами

---
+++ src/main/java/faang/school/godbless/sprint3/calculator/App.java
@@ -0,0 +1,29 @@
+package faang.school.godbless.sprint3.calculator;
+
+import faang.school.godbless.sprint3.calculator.interfaces.Calculator;
+
+import java.util.List;
+
+public class App {
+    public final static String ARRAY_IS_EMPTY_EXCEPTION_MESSAGE = "Array is empty";
+
+    public static int product(List<Integer> nums) {
+        return calculate(nums, ((a, b) -> a * b));
+    }
+
+    public static int sum(List<Integer> nums) {
+        return calculate(nums, ((a, b) -> a + b)); // Как вариант использовать Integer::sum
+    }
+
+    public static int calculate(List<Integer> nums, Calculator calculator) {
+        if (nums.isEmpty()) {
+            throw new IllegalArgumentException(ARRAY_IS_EMPTY_EXCEPTION_MESSAGE);
+        }
+        int result = nums.get(0);
+        for (int i = 1; i < nums.size(); i++){
+            result = calculator.calculate(result, nums.get(i));
+        }
+
+        return result;
+    }
+}
---
+++ src/main/java/faang/school/godbless/sprint3/calculator/interfaces/Calculator.java
@@ -0,0 +1,6 @@
+package faang.school.godbless.sprint3.calculator.interfaces;
+
+@FunctionalInterface
+public interface Calculator {
+    int calculate(int a, int b);
+}
