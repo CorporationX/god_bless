559abc2f5dd4eec3968c5f15ee74cb66ebeaea71
b.mukhtarkhanov <b.mukhtarkhanov>
2023-06-19 12:02:46 +0600

    BC-676. Hogwarts

--- src/test/java/faang/school/godbless/hogwarts/SpellCasterTest.java
+++ src/test/java/faang/school/godbless/hogwarts/SpellCasterTest.java
@@ -1,9 +1,6 @@
 package faang.school.godbless.hogwarts;
 
-import faang.school.godbless.hogwarts.model.SpellCaster;
 import org.junit.jupiter.api.Test;
-
-import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class SpellCasterTest {
     @Test
