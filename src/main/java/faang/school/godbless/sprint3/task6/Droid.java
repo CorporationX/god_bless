a6b833bc66667d9ddcb5fb8c3b7e488587d7009a
Sergey Gerasimov <czar997@yandex.ru>
2023-06-19 11:46:22 +0300

    добавления валидация

--- src/main/java/faang/school/godbless/sprint3/task6/Droid.java
+++ src/main/java/faang/school/godbless/sprint3/task6/Droid.java
@@ -3,6 +3,12 @@ package faang.school.godbless.sprint3.task6;
 public class Droid {
 
     public void sendEncryptedMessage(Droid droid, String message, int encryptionKey) {
+        if (droid == null) {
+            throw new NullPointerException("Droid не может быть null");
+        }
+        if (message.isBlank()) {
+            throw new IllegalArgumentException("Сообщение не может быть пустым");
+        }
         DroidMessageEncryptor droidMessageEncryptor = (mess, encryptKey) -> {
             char[] c = mess.toCharArray();
             for (int i = 0; i < c.length; i++) {
