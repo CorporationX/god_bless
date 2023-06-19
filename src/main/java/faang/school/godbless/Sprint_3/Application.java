669343764dfadf99ec86a837a8c3296b91d200d6
Ivan Averkov <averkovivan@gmail.com>
2023-06-19 15:58:58 +0300

    new main x3

---
+++ src/main/java/faang/school/godbless/Sprint_3/Application.java
@@ -0,0 +1,7 @@
+package faang.school.godbless.Sprint_2.collectUsers_Task4;
+
+public class Application {
+    public static void main(String... args) {
+        System.out.println("Hello world!");
+    }
+}
