d190018fbc4ab587261f9bbb755f9d95c19b4892
Zhugalev Sergei <zhugalcf@gmail.com>
2023-06-19 11:10:09 +0300

    Add new package of task

--- src/main/java/faang/school/godbless/revert/Example.java
+++ src/main/java/faang/school/godbless/revert/Example.java
@@ -1,4 +1,4 @@
-package faang.school.godbless;
+package faang.school.godbless.revert;
 
 public class Example {
     public void reverse(int[] nums) {
--- src/test/java/faang/school/godbless/revert/ExampleTest.java
+++ src/test/java/faang/school/godbless/revert/ExampleTest.java
@@ -1,5 +1,6 @@
-package faang.school.godbless;
+package faang.school.godbless.revert;
 
+import faang.school.godbless.revert.Example;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
