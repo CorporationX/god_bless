c13878293c2bead79f11c0a9be0977730449dca6
Sergey Gerasimov <czar997@yandex.ru>
2023-06-19 10:47:09 +0300

    ErrorHandling + тесты

---
+++ src/main/java/faang/school/godbless/sprint3/task5/ErrorHandling.java
@@ -0,0 +1,14 @@
+package faang.school.godbless.sprint3.task5;
+
+import java.util.function.Supplier;
+
+public class ErrorHandling<T> {
+
+    public T withErrorHandling(Supplier<T> supplier, ExceptionHandler<T> exceptionHandler) {
+        try {
+            return supplier.get();
+        } catch (Exception e) {
+            return exceptionHandler.exceptionHandler(e);
+        }
+    }
+}
---
+++ src/test/java/faang/school/godbless/sprint3/task5/ErrorHandlingTest.java
@@ -0,0 +1,36 @@
+package faang.school.godbless.sprint3.task5;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ErrorHandlingTest {
+
+    @Test
+    void testWithErrorHandlingNegative() {
+        ErrorHandling<Boolean> errorHandling = new ErrorHandling<>();
+
+        boolean actual = errorHandling.withErrorHandling(
+                () -> RemoteService.call(4),
+                e -> {
+                    System.out.println("Не удалось получить доступ к Remote Service. Вернем default значение");
+                    return false;
+                });
+
+        assertFalse(actual);
+    }
+
+    @Test
+    void testWithErrorHandlingPositive() {
+        ErrorHandling<Boolean> errorHandling = new ErrorHandling<>();
+
+        boolean actual = errorHandling.withErrorHandling(
+                () -> RemoteService.call(1),
+                e -> {
+                    System.out.println("Не удалось получить доступ к Remote Service. Вернем default значение");
+                    return false;
+                });
+
+        assertTrue(actual);
+    }
+}
