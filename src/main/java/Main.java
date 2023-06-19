dbf4a541f8ad40d2a81a1ebd51cdce201f93ff4f
b.mukhtarkhanov <b.mukhtarkhanov>
2023-06-19 12:01:43 +0600

    BC-676. Hogwarts

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,4 +1,4 @@
-import faang.school.godbless.groupUsers.model.User;
+import faang.school.godbless.hogwarts.model.SpellCaster;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -6,18 +6,14 @@ import java.util.Map;
 
 public class Main {
     public static void main(String[] args) {
-        List<User> users = new ArrayList<>();
-        User user1 = new User("John", 23, "Amazon", "New York");
-        User user2 = new User("Alice", 30, "Uber", "Washington");
-        User user3 = new User("Bred", 23, "Apple", "Los Angeles");
-        User user4 = new User("Aron", 18, "Meta", "New York");
-        User user5 = new User("Justin", 30, "Google", "Los Angeles");
-        users.add(user1);
-        users.add(user2);
-        users.add(user3);
-        users.add(user4);
-        users.add(user5);
-        Map<Integer, List<User>> resultGroupsMap = User.groupUsersByAge(users);
-        System.out.println(resultGroupsMap);
+        SpellCaster spellCaster = new SpellCaster();
+
+        String alohomora = "Alohomora";
+        String lumos = "Lumos";
+        String expelliarmus = "Expelliarmus";
+
+        spellCaster.cast(alohomora, (spell) -> "The door is unlocked by " + spell);
+        spellCaster.cast(lumos, (spell) -> "A beam of light is created by " + spell);
+        spellCaster.cast(expelliarmus, (spell) -> "The opponent is disarmed by " + spell);
     }
 }
---
+++ src/test/java/faang/school/godbless/hogwarts/SpellCasterTest.java
@@ -0,0 +1,12 @@
+package faang.school.godbless.hogwarts;
+
+import faang.school.godbless.hogwarts.model.SpellCaster;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class SpellCasterTest {
+    @Test
+    public void cast_shouldReturnSpellActionBySpellName() {
+    }
+}
