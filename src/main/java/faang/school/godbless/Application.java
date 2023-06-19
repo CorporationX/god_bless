3a286a53a088529a5fdf716d1b016f173c2126ff
chenso29 <you@example.com>
2023-06-19 22:31:32 +0900

    transform matrix

--- src/main/java/faang/school/godbless/Application.java
+++ src/main/java/faang/school/godbless/Application.java
@@ -4,4 +4,26 @@ public class Application {
     public static void main(String... args) {
 
     }
+
+    public static int[][] transformMatrix(int[][] matrix, MatrixTransformer transformer) {
+        if (matrix == null) {
+            throw new IllegalArgumentException("Empty matrix");
+        }
+        int[][] result = new int[matrix.length][matrix[0].length];
+        for (int row = 0; row < matrix.length; row++) {
+            for (int column = 0; column < matrix[row].length; column++) {
+                Coordinates coordinates = transformer.transform(row, column);
+                result[coordinates.getRow()][coordinates.getColumn()] = matrix[row][column];
+            }
+        }
+        return result;
+    }
+
+    public static int[][] flipMatrix(int[][] matrix, FlipDirection flipDirection) {
+        if (flipDirection.equals(FlipDirection.HORIZONTAL)) {
+            return transformMatrix(matrix, ((row, column) -> new Coordinates(row, matrix[row].length - column - 1)));
+        } else {
+            return transformMatrix(matrix, ((row, column) -> new Coordinates(matrix.length - row - 1, column)));
+        }
+    }
 }
---
+++ src/test/java/TestFlip.java
@@ -0,0 +1,26 @@
+import faang.school.godbless.FlipDirection;
+import org.junit.jupiter.api.Test;
+
+import static faang.school.godbless.Application.flipMatrix;
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
+
+public class TestFlip {
+    @Test
+    void flipTest() {
+        int[][] matrix = {{1, 2, 3},
+                {4, 5, 6},
+                {7, 8, 9}};
+        int[][] expectedHorizontal = {{3, 2, 1},
+                {6, 5, 4},
+                {9, 8, 7}};
+        int[][] expectedVertical = {{7, 8, 9},
+                {4, 5, 6},
+                {1, 2, 3}};
+
+        int[][] flippedHorizontal = flipMatrix(matrix, FlipDirection.HORIZONTAL);
+        int[][] flippedVertical = flipMatrix(matrix, FlipDirection.VERTICAL);
+
+        assertArrayEquals(expectedHorizontal, flippedHorizontal);
+        assertArrayEquals(expectedVertical, flippedVertical);
+    }
+}
