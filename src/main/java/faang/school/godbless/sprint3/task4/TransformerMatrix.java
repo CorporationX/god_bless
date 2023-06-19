951c21a6e626d0544ed09127f59d6f405e37b30a
Sergey Gerasimov <czar997@yandex.ru>
2023-06-19 03:14:25 +0300

    рефакторинг

--- src/main/java/faang/school/godbless/sprint3/task4/TransformerMatrix.java
+++ src/main/java/faang/school/godbless/sprint3/task4/TransformerMatrix.java
@@ -2,45 +2,6 @@ package faang.school.godbless.sprint3.task4;
 
 public class TransformerMatrix {
 
-    public static void main(String[] args) throws InterruptedException {
-        int[][] matrix = {
-                {1, 3, 5, 6, 7, 8, 0},
-                {2, 3, 4, 3, 0, 2, 3}
-        };
-
-        int[] stroka= {1, 3, 5, 6, 7, 8, 0};
-        System.out.println("длина одномерного массива = " + stroka.length);
-
-        System.out.println("длина строки = " + matrix[1].length); // длина строки
-        System.out.println("длина столбца = " + matrix.length); // длина столбца
-
-//        for (int i = 0; i < matrix.length; i++) {
-//            for (int j = 0; j < matrix[i].length; j++) {
-//                Thread.sleep(2000);
-//                System.out.print(matrix[i][j] + " ");
-//            }
-//            System.out.println();
-//        }
-//        System.out.println("________________");
-        int[][] ints = flipMatrix(matrix, FlipDirection.HORIZONTAL);
-
-        for (int i = 0; i < ints.length; i++) {
-            for (int j = 0; j < ints[i].length; j++) {
-                System.out.print(ints[i][j] + " ");
-            }
-            System.out.println();
-        }
-        System.out.println("________________");
-        int[][] intsV = flipMatrix(ints, FlipDirection.VERTICAL);
-
-        for (int i = 0; i < intsV.length; i++) {
-            for (int j = 0; j < intsV[i].length; j++) {
-                System.out.print(intsV[i][j] + " ");
-            }
-            System.out.println();
-        }
-    }
-
     static int[][] transformMatrixHorizontal(int[][] matrix, MatrixTransformer transformer) {
         for (int i = 0; i < matrix.length; i++) {
             for (int j = 0; j < matrix[i].length / 2; j++) {
@@ -64,6 +25,9 @@ public class TransformerMatrix {
     }
 
     static int[][] flipMatrix(int[][] matrix, FlipDirection flipDirection) {
+        if (matrix == null || matrix[0].length == 0 || flipDirection == null) {
+            throw new IllegalArgumentException();
+        }
         MatrixTransformer matrixTransformer = (x, y) -> new Coordinates(y, x);
         if (flipDirection.equals(FlipDirection.HORIZONTAL)) {
             return transformMatrixHorizontal(matrix, matrixTransformer);
@@ -71,4 +35,6 @@ public class TransformerMatrix {
             return transformMatrixVertical(matrix, matrixTransformer);
         }
     }
+
+
 }
