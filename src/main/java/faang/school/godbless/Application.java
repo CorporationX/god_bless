f243e22b09ee74fb395258fa755f1ea50cbe1309
An Dima <an.dima@slexn.com>
2023-06-19 12:39:14 +0900

    init

--- src/main/java/faang/school/godbless/Application.java
+++ src/main/java/faang/school/godbless/Application.java
@@ -1,7 +1,35 @@
 package faang.school.godbless;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Application {
     public static void main(String... args) {
 
     }
+
+    public static String toCsv(List<List<String>> table) throws IllegalArgumentException {
+        if (table.isEmpty()) {
+            throw new IllegalArgumentException("Table is empty");
+        }
+        VectorJoiner<String> vectorJoiner = vector -> {
+            StringBuilder stringBuilder = new StringBuilder();
+            stringBuilder.append(vector.get(0));
+            for (int i = 1; i < vector.size(); i++) {
+                stringBuilder.append(", ").append(vector.get(i));
+            }
+            return stringBuilder.toString();
+        };
+
+        MatrixJoiner<String> matrixJoiner = matrix -> {
+            StringBuilder res = new StringBuilder(vectorJoiner.join(matrix.get(0)));
+            for (int i = 1; i < matrix.size(); i++) {
+                res.append("\n").append(vectorJoiner.join(matrix.get(i)));
+            }
+            return res.toString();
+        };
+
+        return matrixJoiner.join(table);
+    }
+
 }
