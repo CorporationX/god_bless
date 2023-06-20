be8511fba5d8f7af73478e7e4cc695b6cecde455
b.mukhtarkhanov <b.mukhtarkhanov>
2023-06-20 16:11:56 +0600

    BC-688. Meta

---
+++ src/main/java/faang/school/godbless/meta/model/Notification.java
@@ -0,0 +1,30 @@
+package faang.school.godbless.meta.model;
+
+public class Notification {
+    private String type;
+    private String message;
+
+    public Notification() {
+    }
+
+    public Notification(String type, String message) {
+        this.type = type;
+        this.message = message;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+}
