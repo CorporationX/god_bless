fcedf2a307b285882a9ab87c03a653b055b6da03
Rustam Nigmatullin <rustamnigma@mail.ru>
2023-06-20 22:17:58 +0500

    BC-918

---
+++ src/main/java/faang/school/godbless/Gmail/EmailProcessor.java
@@ -0,0 +1,20 @@
+package faang.school.godbless.Gmail;
+
+
+import java.util.*;
+import java.util.function.Consumer;
+import java.util.function.Function;
+import java.util.function.Predicate;
+
+public class EmailProcessor {
+    public void processEmails(List<Email> mails, Predicate<Email> filterEmails,
+                              Consumer<Email> handling,
+                              Function<Email, String> transformation){
+        for (Email mail : mails) {
+            if(filterEmails.test(mail)) {
+                handling.accept(mail);
+                transformation.apply(mail);
+            }
+        }
+    }
+}
