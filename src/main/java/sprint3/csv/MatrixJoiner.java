2b9fdda3ef2268a997e92e9b5d5517e44013c1b7
”Ramazan” <mansurov.r13@gmail.com>
2023-06-20 13:20:24 +0300

    BC-699, CSV, fixed

--- src/main/java/sprint3/csv/MatrixJoiner.java
+++ src/main/java/sprint3/csv/MatrixJoiner.java
@@ -1,16 +1,20 @@
 package sprint3.csv;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import java.util.List;
 import java.util.stream.Collectors;
 
 @FunctionalInterface
 public interface MatrixJoiner<T> {
+    Logger LOGGER = LoggerFactory.getLogger(MatrixJoiner.class);
     T join(List<List<T>> matrix);
 
     static String toCsv(List<List<String>> matrix) {
         MatrixJoiner<String> joiner = (m) -> {
             if (matrix.isEmpty()) {
-                throw new IllegalArgumentException("Empty list");
+                LOGGER.error("Empty list");
             }
             return matrix.stream()
                     .map(VectorJoiner::toCsv)
--- src/main/java/sprint3/csv/VectorJoiner.java
+++ src/main/java/sprint3/csv/VectorJoiner.java
@@ -1,15 +1,19 @@
 package sprint3.csv;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import java.util.List;
 
 @FunctionalInterface
 public interface VectorJoiner<T> {
+    Logger LOGGER = LoggerFactory.getLogger(VectorJoiner.class);
     T join(List<T> vector);
 
-    static String toCsv(List<String> vector) throws IllegalArgumentException {
+    static String toCsv(List<String> vector) {
         VectorJoiner<String> joiner = (v) -> {
             if (vector.isEmpty()) {
-                throw new IllegalArgumentException("Empty list");
+                LOGGER.error("Empty list");
             }
             return String.join(", ", vector);
         };
