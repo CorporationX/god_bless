b745a3584443473ba1723453df81f0a46cb92002
Zhargal Budaev <budaev17@mail.ru>
2023-06-21 01:16:54 +0700

    Создал класс Email и класс EmailProcessor с методом processEmails, который принимает список входящих писем и обрабатывает их с помощью функциональных интерфейсов согласно условию.

---
+++ src/main/java/faang/school/godbless/gmail_rich_filters/Email.java
@@ -0,0 +1,23 @@
+package faang.school.godbless.gmail_rich_filters;
+
+public class Email {
+    private String subject;
+    private String body;
+    private boolean isImportant;
+
+    Email(String subject, String body, boolean isImportant) {
+        this.subject = subject;
+        this.body = body;
+        this.isImportant = isImportant;
+    }
+
+    public String getSubject() {
+        return subject;
+    }
+    public String getBody() {
+        return body;
+    }
+    public boolean getIsImportant() {
+        return isImportant;
+    }
+}
---
+++ src/main/java/faang/school/godbless/gmail_rich_filters/EmailProcessor.java
@@ -0,0 +1,37 @@
+package faang.school.godbless.gmail_rich_filters;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.function.Consumer;
+import java.util.function.Function;
+import java.util.function.Predicate;
+
+public class EmailProcessor {
+    public void processEmails(List<Email> emails, Predicate<Email> predicate, Consumer<Email> consumer, Function<Email, String> function) {
+        if(emails == null || emails.isEmpty()) {
+            throw new IllegalStateException("List is empty!");
+        }
+        for(Email email : emails) {
+            if(predicate.test(email)) {
+                consumer.accept(email);
+                function.apply(email);
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        List<Email> emails = Arrays.asList(
+                new Email("Письмо 1", "Текст письма 1", false),
+                new Email("Письмо 2", "Текст письма 2", true),
+                new Email("Спам", "Текст спама", false)
+        );
+
+        EmailProcessor emailProcessor = new EmailProcessor();
+
+        Predicate<Email> importantFilter = email -> email.getIsImportant();
+        Consumer<Email> printEmail = email -> System.out.println("Обработано письмо: " + email.getSubject());
+        Function<Email, String> toUpperCase = email -> email.getBody().toUpperCase();
+
+        emailProcessor.processEmails(emails, importantFilter, printEmail, toUpperCase);
+    }
+}
