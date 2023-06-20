a6376dd0b4aa98679c4673a0637ba931499a7bbe
An Dima <an.dima@slexn.com>
2023-06-20 17:20:40 +0900

    changed package

--- src/main/java/faang/school/godbless/FilterSpam/Application.java
+++ src/main/java/faang/school/godbless/FilterSpam/Application.java
@@ -1,4 +1,4 @@
-package faang.school.godbless;
+package faang.school.godbless.FilterSpam;
 
 import java.util.Arrays;
 import java.util.List;
--- src/main/java/faang/school/godbless/FilterSpam/MessageFilter.java
+++ src/main/java/faang/school/godbless/FilterSpam/MessageFilter.java
@@ -1,4 +1,4 @@
-package faang.school.godbless;
+package faang.school.godbless.FilterSpam;
 
 @FunctionalInterface
 public interface MessageFilter {
