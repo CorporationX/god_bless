19588757174fbd930eff465c864a6766c7cb61f4
chenso29 <you@example.com>
2023-06-20 21:13:50 +0900

    lotr

--- src/main/java/faang/school/godbless/LOTR/Character.java
+++ src/main/java/faang/school/godbless/LOTR/Character.java
@@ -7,7 +7,12 @@ import java.util.List;
 
 @Data
 public class Character {
+    private String name;
     private List<Item> inventory;
+
+    public Character(String name) {
+        this.name = name;
+    }
 
     public void addItem(Item item) {
         if (this.inventory == null) {
@@ -16,14 +21,18 @@ public class Character {
         this.inventory.add(item);
     }
 
-    public void removeItem(Item item){
-        if (this.inventory == null){
+    public void removeItem(Item item) {
+        if (this.inventory == null) {
             throw new IllegalArgumentException("Empty inventory");
         }
         this.inventory.remove(item);
     }
 
-    public void updateItem(Item item){
-
+    public void updateItem(int index,Item item) {
+        if (this.inventory == null) {
+            throw new IllegalArgumentException("Empty inventory");
+        }
+        this.inventory.remove(index);
+        this.inventory.add(index, item);
     }
 }
--- src/main/java/faang/school/godbless/LOTR/InventoryManager.java
+++ src/main/java/faang/school/godbless/LOTR/InventoryManager.java
@@ -5,20 +5,24 @@ import java.util.function.Function;
 import java.util.function.Predicate;
 
 public class InventoryManager {
-    public void addItem(Character character, Item item, Consumer<Item> itemConsumer){
+    public void addItem(Character character, Item item, Consumer<Item> itemConsumer) {
         character.addItem(item);
         itemConsumer.accept(item);
     }
 
-    public void removeItem(Character character, Predicate<Item> itemPredicate){
-        character.getInventory().forEach(item -> {
-            if (itemPredicate.test(item)){
-                character.removeItem(item);
+    public void removeItem(Character character, Predicate<Item> itemPredicate) {
+        for (int i = 0; i< character.getInventory().size(); i++){
+            if (itemPredicate.test(character.getInventory().get(i))) {
+                character.removeItem(character.getInventory().get(i));
             }
-        });
+        }
     }
 
-    public void updateItem(Character character, Predicate<Item> itemPredicate, Function<Item, Item> itemFunction){
-
+    public void updateItem(Character character, Predicate<Item> itemPredicate, Function<Item, Item> itemFunction) {
+        for (int i = 0; i < character.getInventory().size(); i++) {
+            if (itemPredicate.test(character.getInventory().get(i))){
+                character.updateItem(i, itemFunction.apply(character.getInventory().get(i)));
+            }
+        }
     }
 }
