948f921d823b87a01998d0ec13f6de67c602281d
An Dima <an.dima@slexn.com>
2023-06-20 10:41:33 +0900

    meta universe

---
+++ src/main/java/faang/school/godbless/NotificationManager.java
@@ -0,0 +1,22 @@
+package faang.school.godbless;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.function.Consumer;
+
+public class NotificationManager {
+    private final Map<String, Consumer<Notification>> notificationMap;
+
+    public NotificationManager() {
+        this.notificationMap = new HashMap<>();
+    }
+
+    public void registerHandler(String type, Consumer<Notification> notificationConsumer) {
+       notificationMap.put(type, notificationConsumer);
+    }
+
+    public void sendNotification(Notification notification) {
+        Consumer<Notification> notificationConsumer = notificationMap.get(notification.getType());
+        notificationConsumer.accept(notification);
+    }
+}
