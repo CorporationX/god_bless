e39db8a80bbed31c3cc1aed8f5e5822c47923484
OneWayMy <zhenya.ivashchenko.9494@gmail.com>
2023-06-20 15:43:53 -0700

    Sprint_3_MillionsUber_PR

---
+++ src/main/java/faang/school/godbless/sprint_3/milionsUber/FareCalculator.java
@@ -0,0 +1,14 @@
+package faang.school.godbless.sprint_3.milionsUber;
+
+
+import java.util.function.BiFunction;
+
+public class FareCalculator {
+    public double calculateFare(double distance, double time, BiFunction<Double, Double, Double> function) {
+        double result = function.apply(distance, time);
+        if (Double.isNaN(result) || result == 0.0) {
+            throw new IllegalStateException("Некорректный результат вычислений.");
+        }
+        return result;
+    }
+}
---
+++ src/main/java/faang/school/godbless/sprint_3/milionsUber/Main.java
@@ -0,0 +1,17 @@
+package faang.school.godbless.sprint_3.milionsUber;
+
+import java.util.function.BiFunction;
+
+public class Main {
+    public static void main(String[] args) {
+        FareCalculator fareCalculator = new FareCalculator();
+
+        BiFunction<Double,Double,Double> firstPrice = ((distance, time) -> distance * (time * 1.5));
+        BiFunction<Double,Double,Double> secondPrice = ((distance, time) -> distance * 1.4 + time * 1.2);
+
+        double first = fareCalculator.calculateFare(2.0, 2.0, firstPrice);
+        double second = fareCalculator.calculateFare(3.5, 0.4, (distance, time) -> distance * 1.5 + time * 0.5);
+        double third = fareCalculator.calculateFare(5.5, 1.0,secondPrice);
+        System.out.println(first + ", " + second + ", " + third);
+    }
+}
