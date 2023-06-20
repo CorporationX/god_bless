2285739959f147fd5a87c89c6afe2280e3414b21
Никита <nikita.armeec.88@mail.ru>
2023-06-20 22:13:17 +0300

    Созданы класс Email и EmailProcessor для обработки входящих писем

---
+++ src/main/java/faang/school/godbless/kxnvg/gmail/EmailProcessor.java
@@ -0,0 +1,19 @@
+package faang.school.godbless.kxnvg.gmail;
+
+import java.util.List;
+import java.util.function.Consumer;
+import java.util.function.Function;
+import java.util.function.Predicate;
+
+public class EmailProcessor {
+
+    public void processEmails(List<Email> emailList, Predicate<Email> predicate, Consumer<Email> consumer,
+                              Function<Email, String> function) {
+        emailList.forEach((email) -> {
+            if (predicate.test(email)) {
+                consumer.accept(email);
+                email.setBody(function.apply(email));
+            }
+        });
+    }
+}
