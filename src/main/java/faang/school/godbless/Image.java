4aab414a2f5a4d3f4289b755209a3e333b519988
Rustam Nigmatullin <rustamnigma@mail.ru>
2023-06-20 21:19:29 +0500

    BC-904

---
+++ src/main/java/faang/school/godbless/Image.java
@@ -0,0 +1,11 @@
+package faang.school.godbless;
+
+import lombok.Data;
+import lombok.NonNull;
+@Data
+public class Image {
+    @NonNull
+    private String Name;
+    @NonNull
+    private String description;
+}
