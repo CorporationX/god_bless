b38cdb67651f8a50a10ee2a9bd571e30ca2c9a52
Dobrovolsky Artem <artdobrov@mail.ru>
2023-06-21 00:47:56 +0300

    ctrl + alt + L

--- src/main/java/faang/school/godbless/GoogleTranslator/DictionaryProcessor.java
+++ src/main/java/faang/school/godbless/GoogleTranslator/DictionaryProcessor.java
@@ -3,7 +3,7 @@ package faang.school.godbless.GoogleTranslator;
 import java.util.function.BiConsumer;
 
 public class DictionaryProcessor {
-    public void processWord(String word, String translation, BiConsumer<String, String> biConsumer){
+    public void processWord(String word, String translation, BiConsumer<String, String> biConsumer) {
         biConsumer.accept(word, translation);
     }
 }
