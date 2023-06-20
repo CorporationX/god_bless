7d24936a14ee4a05d7730a71ff9ce3b4def5560b
An Dima <an.dima@slexn.com>
2023-06-20 12:43:20 +0900

    gmail rich filters

---
+++ src/main/java/faang/school/godbless/EmailProcessor.java
@@ -0,0 +1,20 @@
+package faang.school.godbless;
+
+import java.util.List;
+import java.util.function.Consumer;
+import java.util.function.Function;
+import java.util.function.Predicate;
+
+public class EmailProcessor {
+    public void processEmails(List<Email> emailList, Predicate<Email> emailPredicate, Consumer<Email> emailConsumer, Function<Email, String> emailFunction) {
+        if (emailList == null) {
+            throw new IllegalArgumentException("List is empty");
+        }
+        for (Email email : emailList) {
+            emailPredicate.test(email);
+            emailConsumer.accept(email);
+            emailFunction.apply(email);
+        }
+    }
+
+}
