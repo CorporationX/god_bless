3b93657b94041be0ad6b67ee8c71ee0672a1206d
Pashkevich <ppavel012100@gmail.com>
2023-06-20 17:21:21 +0200

    resolve Calculator task

---
+++ src/main/java/faang/school/godbless/sprint2/calculator/Calculate.java
@@ -0,0 +1,27 @@
+package faang.school.godbless.sprint2.calculator;
+
+import java.util.List;
+
+public class Calculate {
+    public static int calculate(List<Integer> nums, Calculator calculator) {
+        if (nums == null || nums.isEmpty()) {
+            throw new NullPointerException();
+        }
+
+        int result = nums.get(0);
+        for (int i = 1; i < nums.size(); ++i) {
+            result = calculator.calc(result, nums.get(i));
+        }
+        return result;
+
+    }
+
+    public static int sum(List<Integer> nums) {
+        return calculate(nums, (Integer::sum));
+    }
+
+    public static int product(List<Integer> nums) {
+        return calculate(nums, ((a, b) -> a * b));
+    }
+
+}
