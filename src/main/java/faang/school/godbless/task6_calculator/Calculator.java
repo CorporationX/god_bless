f076cdfcaf25cb3f44fb2cad284a2fb6c611b018
Alex <matcnev2001@mail.ru>
2023-06-20 19:14:24 +0300

    task6 M4nk1n, Calculate (с учетом подсказок)

--- src/main/java/faang/school/godbless/task6_calculator/Calculator.java
+++ src/main/java/faang/school/godbless/task6_calculator/Calculator.java
@@ -1,11 +1,6 @@
 package faang.school.godbless.task6_calculator;
 
-import java.util.List;
-
+@FunctionalInterface
 public interface Calculator {
-    int culcilateSpeshial(List<Integer> list);
-
     int calculate(int res, int num);
-
-
 }
--- src/main/java/faang/school/godbless/task6_calculator/Main.java
+++ src/main/java/faang/school/godbless/task6_calculator/Main.java
@@ -4,62 +4,29 @@ import java.util.Arrays;
 import java.util.List;
 
 public class Main {
-    static Calculator sum = new Calculator() {
-        @Override
-        public int calculate(int res, int num) {
-            return num + res;
-        }
-
-        @Override
-        public int culcilateSpeshial(List<Integer> list) {
-            int res = 0;
-            for (int i : list) {
-                res = calculate(i, res);
-            }
-            return res;
-        }
-    };
-    static Calculator product = new Calculator() {
-        @Override
-        public int calculate(int res, int num) {
-            return res * num;
-        }
-
-        @Override
-        public int culcilateSpeshial(List<Integer> list) {
-            int res = 1;
-            for (int i : list) {
-                res = calculate(i, res);
-            }
-            return res;
-        }
-    };
 
     public static void main(String[] args) {
         List<Integer> list = Arrays.asList(1, 2, 3, 4, 5, 6);
-        System.out.println(calculate(list, sum));
-        System.out.println(calculate(list, product));
-        sum(list);
-        product(list);
+
     }
 
     public static int calculate(List<Integer> nums, Calculator calculator) throws IllegalArgumentException {
-        return calculator.culcilateSpeshial(nums);
+        if (nums.isEmpty()) {
+            throw new IllegalArgumentException("empty");
+        }
+
+        int res = nums.get(0);
+        for (int i = 1; i < nums.size(); i++) {
+            res = calculator.calculate(res, nums.get(i));
+        }
+        return res;
     }
 
     public static int sum(List<Integer> nums) {
-        int res = 0;
-        for (int i : nums) {
-            res = sum.calculate(res, i);
-        }
-        return res;
+        return calculate(nums, (a, b) -> a + b);
     }
 
     public static int product(List<Integer> nums) {
-        int res = 1;
-        for (int i : nums) {
-            res = sum.calculate(res, i);
-        }
-        return res;
+        return calculate(nums, (a, b) -> a * b);
     }
 }
