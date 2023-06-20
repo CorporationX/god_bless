6e81b8d0ee776b67d27d5f59895cc725999c3fd1
Alex Tarasov <tarasof.av@gmail.com>
2023-06-20 14:18:29 +0300

    task Meta-space

---
+++ src/main/task2/src/main/java/Main.java
@@ -0,0 +1,19 @@
+public class Main {
+    public static void main(String[] args) {
+        NotificationManager notificationManager = new NotificationManager();
+
+// Регистрация обработчиков оповещений
+        notificationManager.registerHandler("email", (notification) -> System.out.println("Отправка по электронной почте: " + notification.message()));
+        notificationManager.registerHandler("sms", (notification) -> System.out.println("Отправка SMS: " + notification.message()));
+        notificationManager.registerHandler("push", (notification) -> System.out.println("Отправка push-уведомления: " + notification.message()));
+
+// Отправка оповещений
+        Notification emailNotification = new Notification("email", "Ваша учетная запись успешно активирована");
+        Notification smsNotification = new Notification("sms", "Вы успешно изменили свой пароль");
+        Notification pushNotification = new Notification("push", "Новый пост от пользователя: JohnDoe");
+
+        notificationManager.sendNotification(emailNotification);
+        notificationManager.sendNotification(smsNotification);
+        notificationManager.sendNotification(pushNotification);
+    }
+}
