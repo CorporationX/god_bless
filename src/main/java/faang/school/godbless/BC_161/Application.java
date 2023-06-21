fd6ca9020ff83e34e29bdcd17ed39622e1e96845
Adam-D-Beno <wrofdt@yandex.ru>
2023-06-21 12:16:35 +0300

    Fix №1

--- src/main/java/faang/school/godbless/BC_161/Application.java
+++ src/main/java/faang/school/godbless/BC_161/Application.java
@@ -7,7 +7,7 @@ public class Application {
     public static void main(String... args) {
         Set<String> setActivities = Set.of("str1", "activity88", "activity16");
         List<User> userList = List.of(new User(1, "Adam", 35, Set.of("str1", "str2", "str3")),
-                                      new User(2, "Adam2", 35, Set.of("str4", "str5", "str6"))
+                                      new User(2, "Adam2", 35, Set.of("str0", "str5", "str6"))
         );
         System.out.println(User.findHobbyLovers(userList, setActivities));
     }
