ca0d2441b5970cb1e30b1ddfb290949fbaeeb858
Pashkevich <ppavel012100@gmail.com>
2023-06-21 09:02:26 +0200

    refactor googleTranslator task

--- src/main/java/faang/school/godbless/sprint2/googleTranslate/DictionaryProcessor.java
+++ src/main/java/faang/school/godbless/sprint2/googleTranslate/DictionaryProcessor.java
@@ -3,7 +3,7 @@ package faang.school.godbless.sprint2.googleTranslate;
 import java.util.function.BiConsumer;
 
 public class DictionaryProcessor {
-    public void processWord (String word, String translate, BiConsumer<String,String> biConsumer){
-        biConsumer.accept(word,translate);
+    public void processWord(String word, String translate, BiConsumer<String, String> biConsumer) {
+        biConsumer.accept(word, translate);
     }
 }
--- src/test/java/faang/school/godbless/googleTranslateTest/DictionaryProcessorTest.java
+++ src/test/java/faang/school/godbless/googleTranslateTest/DictionaryProcessorTest.java
@@ -1,4 +1,5 @@
 package faang.school.godbless.googleTranslateTest;
+
 import faang.school.godbless.sprint2.googleTranslate.DictionaryProcessor;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -7,21 +8,24 @@ import java.util.HashMap;
 import java.util.Map;
 import java.util.function.BiConsumer;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class DictionaryProcessorTest {
     DictionaryProcessor dictionaryProcessor;
     Map<String, String> dictionary;
     BiConsumer<String, String> addWordToDictionary;
+
     @BeforeEach
-    public void init(){
+    public void init() {
         dictionaryProcessor = new DictionaryProcessor();
         dictionary = new HashMap<>();
         addWordToDictionary = (word, translation) -> dictionary.put(word, translation);
     }
+
     @Test
     public void DictionaryProcessorTest() {
         dictionaryProcessor.processWord("привет", "hello", addWordToDictionary);
         String expected = "hello";
-        assertEquals(expected,dictionary.get("привет"));
+        assertEquals(expected, dictionary.get("привет"));
     }
 }
