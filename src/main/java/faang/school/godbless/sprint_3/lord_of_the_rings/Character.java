029b61ac5edb788d4cc95c74e3b2f8427baddd91
OneWayMy <zhenya.ivashchenko.9494@gmail.com>
2023-06-21 00:11:03 -0700

    Sprint_3_lord_of_the_rings_PR

---
+++ src/main/java/faang/school/godbless/sprint_3/lord_of_the_rings/Character.java
@@ -0,0 +1,14 @@
+package faang.school.godbless.sprint_3.lord_of_the_rings;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Getter
+@Setter
+public class Character {
+    private List<Item> inventory = new ArrayList<>();
+
+}
---
+++ src/main/java/faang/school/godbless/sprint_3/lord_of_the_rings/InventoryManager.java
@@ -0,0 +1,27 @@
+package faang.school.godbless.sprint_3.lord_of_the_rings;
+
+import java.util.function.Consumer;
+import java.util.function.Function;
+import java.util.function.Predicate;
+
+public class InventoryManager {
+
+    public void addItem(Character character, Item item, Consumer<Item> consumer) {
+        character.getInventory().add(item);
+        consumer.accept(item);
+    }
+
+    public void removeItem(Character character, Predicate<Item> predicate) {
+        character.getInventory().removeIf(predicate);
+    }
+
+    public void updateItem(Character character, Predicate<Item> predicate, Function<Item, Item> function) {
+        if (!character.getInventory().isEmpty()) {
+            character.getInventory().stream().filter(predicate).forEach(item -> {
+                Item newItem = function.apply(item);
+                int index = character.getInventory().indexOf(item);
+                character.getInventory().set(index, newItem);
+            });
+        }
+    }
+}
