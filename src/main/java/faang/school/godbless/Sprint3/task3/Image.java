d496e5f1b03b97fb3700808241100849312ad358
RasulMurtuzaliev <ras8705@mail.ru>
2023-06-21 15:09:27 +0500

    completed the task "Instagram Filters"

---
+++ src/main/java/faang/school/godbless/Sprint3/task3/Image.java
@@ -0,0 +1,11 @@
+package faang.school.godbless.Sprint3.task3;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@AllArgsConstructor
+@Data
+public class Image {
+    private String name;
+    private String description;
+}
