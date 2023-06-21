f2f6b84e6fd8f826d2f83f84ffc06e9ea0668b98
Devilserker <demonserker@gmail.com>
2023-06-21 19:45:27 +0600

    Класс Image вынесен в отдельный файл

--- src/main/java/faang/school/godbless/instagram_filters/FilterProcessor.java
+++ src/main/java/faang/school/godbless/instagram_filters/FilterProcessor.java
@@ -1,7 +1,5 @@
 package faang.school.godbless.instagram_filters;
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
 
 import java.util.function.Function;
 
@@ -13,11 +11,4 @@ public class FilterProcessor {
     public Function<Image, Image> combineFilters(Function<Image, Image> filter1, Function<Image, Image> filter2) {
         return filter1.andThen(filter2);
     }
-}
-
-@AllArgsConstructor
-@Getter
-class Image {
-    private String name;
-    private String description;
 }
