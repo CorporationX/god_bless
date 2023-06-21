0266c1c2e1e71282cafe6fcf51bd6ed98b4f95fa
Adam-D-Beno <wrofdt@yandex.ru>
2023-06-21 14:47:18 +0300

    Solved BC-959. Разверни.

--- src/main/java/faang/school/godbless/Example.java
+++ src/main/java/faang/school/godbless/Example.java
@@ -5,11 +5,17 @@ import java.util.Collections;
 
 public class Example {
     public static void main(String[] args) {
-        Integer[] nums = {1, 2, 3, 4, 5};
+        int[] nums = {1, 2, 3, 4, 5};
         reverse(nums);
+    }
+    public static void reverse(int[] nums) {
+        int size = nums.length;
+        int temp;
+        for (int i = 0; i < size / 2; i++) {
+            temp = nums[i];
+            nums[i] = nums[(size-1) - i];
+            nums[(size - 1) - i] = temp;
+        }
         System.out.println(Arrays.toString(nums));
-    }
-    public static void reverse(Integer[] nums) {
-        Collections.reverse(Arrays.asList(nums));
-    }
+     }
 }
