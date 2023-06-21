8b19b8b542646325104680c17eb16e9f039b9c2c
Pashkevich <ppavel012100@gmail.com>
2023-06-21 09:00:58 +0200

    refactor FareCalculator task

--- src/main/java/faang/school/godbless/sprint2/uber/FareCalculator.java
+++ src/main/java/faang/school/godbless/sprint2/uber/FareCalculator.java
@@ -1,10 +1,9 @@
 package faang.school.godbless.sprint2.uber;
 
-import java.util.Date;
 import java.util.function.BiFunction;
 
 public class FareCalculator {
-    public static double calculateFare (double distance, double time, BiFunction<Double, Double, Double> function){
-        return function.apply(distance,time);
+    public static double calculateFare(double distance, double time, BiFunction<Double, Double, Double> function) {
+        return function.apply(distance, time);
     }
 }
