51a34880c211b5088410916a835c33138880048a
Zlata Galaburdina <585zlata@mail.ru>
2023-06-21 09:07:27 +0300

    "nd" attempt

--- src/main/java/sprint3task2/NotificationManager.java
+++ src/main/java/sprint3task2/NotificationManager.java
@@ -1,5 +1,6 @@
 package sprint3task2;
-
+//класс NotificationManager содержит Map, связывающую идентификаторы оповещений
+// с их обработчиками - популярными функциональными интерфейсами
 import lombok.Data;
 import java.util.HashMap;
 import java.util.Map;
@@ -15,21 +16,31 @@ public class NotificationManager {
     public void sendNotification (Notification notification) {
         //вызывает соответствующий обработчик оповещения из Map
         //обработчики - функциональные интерфейсы Java
-        Consumer<Notification> consumer = map.entrySet().getValue();
+//        if (map.containsKey(notification.getType()) || !map.isEmpty()) {
+//            Consumer<Notification> consumer = map.get(notification.getType());
+//            consumer.accept(notification);
+//        } else throw new IllegalArgumentException("Исключение");
+        Consumer <Notification> consumer = map.get(notification.getType());
+        consumer.accept(notification);
+
     }
     public static void main(String[] args) {
         NotificationManager notificationManager = new NotificationManager();
 
 // Регистрация обработчиков оповещений
+        //складываем в map ключи и значения
         notificationManager.registerHandler("email", (notification) -> System.out.println("Отправка по электронной почте: " + notification.getMessage()));
         notificationManager.registerHandler("sms", (notification) -> System.out.println("Отправка SMS: " + notification.getMessage()));
         notificationManager.registerHandler("push", (notification) -> System.out.println("Отправка push-уведомления: " + notification.getMessage()));
 
 // Отправка оповещений
+        //сохдаем объекты класса Notification для их использования в sendNotification
         Notification emailNotification = new Notification("email", "Ваша учетная запись успешно активирована");
         Notification smsNotification = new Notification("sms", "Вы успешно изменили свой пароль");
         Notification pushNotification = new Notification("push", "Новый пост от пользователя: JohnDoe");
 
+        //вызываем соответствующи обработчик оповещения из Map с помощью метода sendNotification
+        //через объект класса делается
         notificationManager.sendNotification(emailNotification);
         notificationManager.sendNotification(smsNotification);
         notificationManager.sendNotification(pushNotification);
---
+++ src/main/java/sprint3task3/FilterProcessor.java
@@ -0,0 +1,17 @@
+//package sprint3task3;
+//
+//import java.util.function.Function;
+////будет содержать функции высокого порядка для обработки изображений
+//// и применения фильтров
+//
+//public class FilterProcessor {
+//    //функции высокого порядка
+//    public Image applyFilter (Image image, Function<Image, Image> function){
+//        image = new Image();
+//        return ;
+//    }
+//    public Function<Image, Image> combineFilters (Function<Image, Image> firstFunction, Function<Image, Image> secondFunction){
+//        Function<Image, Image> unitedFunction = ;
+//        return ;
+//    }
+//}
