18b34bdd83051ab8974d5eba6b02ac27c4639416
Alex <matcnev2001@mail.ru>
2023-06-21 09:21:25 +0300

    M4nk1n, Instagram add tests

--- src/main/java/faang/school/godbless/Instagram_Sprint3/Image.java
+++ src/main/java/faang/school/godbless/Instagram_Sprint3/Image.java
@@ -1,14 +1,15 @@
 package faang.school.godbless.Instagram_Sprint3;
 
 import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.ToString;
 
+@EqualsAndHashCode
 @AllArgsConstructor
 @Getter
 @ToString
 public class Image {
     private String name;
     private String description;
-
 }
