c44130b09114479698a1d4afffa23b04957bc67e
Tamerlan <100015209+Devilserk@users.noreply.github.com>
2023-06-21 20:24:58 +0600

    Задание Калькулятор выполнено (#541)
    * Задание Калькулятор выполнено
    
    * Интерфейс Calculator вынесен в отдельный файл

---
+++ src/main/java/faang/school/godbless/calculator/Calculator.java
@@ -0,0 +1,6 @@
+package faang.school.godbless.calculator;
+
+@FunctionalInterface
+interface Calculator {
+    int compute(int a, int b);
+}
---
+++ src/main/java/faang/school/godbless/calculator/Solution.java
@@ -0,0 +1,22 @@
+package faang.school.godbless.calculator;
+
+import lombok.NonNull;
+
+import java.util.List;
+import java.util.Objects;
+
+public class Solution {
+    private static int calculate(@NonNull List<Integer> nums, Calculator calculator) {
+        return nums.stream()
+                .filter(Objects::nonNull)
+                .reduce(calculator::compute).orElseThrow(IllegalArgumentException::new);
+    }
+
+    static int sum(List<Integer> nums) {
+        return calculate(nums, ((a, b) -> a + b));
+    }
+
+    static int product(List<Integer> nums) {
+        return calculate(nums, ((a, b) -> a * b));
+    }
+}
