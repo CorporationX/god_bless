67404e74aa1ba9f85d7df15786a52289977715e0
Паша <petrov.pashapet@yandex.ru>
2023-06-21 19:34:53 +0300

    Добавил тест

--- src/test/java/faang/school/godbless/secondSprint/GoogleTranslator/DictionaryProcessorTest.java
+++ src/test/java/faang/school/godbless/secondSprint/GoogleTranslator/DictionaryProcessorTest.java
@@ -1,19 +1,27 @@
 package faang.school.godbless.secondSprint.GoogleTranslator;
 
+import org.junit.jupiter.api.Test;
+
 import java.util.HashMap;
 import java.util.Map;
 import java.util.function.BiConsumer;
 
-public class Main {
-    public static void main(String[] args) {
+import static org.junit.jupiter.api.Assertions.*;
+
+class DictionaryProcessorTest {
+    @Test
+    public void testProcessWord() {
         DictionaryProcessor dictionaryProcessor = new DictionaryProcessor();
-        Map<String,String> dictionary = new HashMap<>();
+        Map<String, String> dictionary = new HashMap<>();
         BiConsumer<String, String> biConsumer = dictionary::put;
 
         dictionaryProcessor.processWord("apple", "яблоко", biConsumer);
         dictionaryProcessor.processWord("orange", "апельсин", biConsumer);
-        dictionaryProcessor.processWord("banana", "банан", biConsumer);
+        Map<String, String> expected = Map.of(
+                "apple", "яблоко",
+                "orange", "апельсин"
+        );
 
-        System.out.println(dictionary);
+        assertEquals(expected, dictionary);
     }
 }
