05f66bf4898b82864ab540104d11dcf695cc9eee
Alex <matcnev2001@mail.ru>
2023-06-21 19:17:48 +0300

    M4nk1n, Hogvards add tests

--- src/main/java/faang/school/godbless/Hogvards/SpellCaster.java
+++ src/main/java/faang/school/godbless/Hogvards/SpellCaster.java
@@ -1,6 +1,7 @@
 package faang.school.godbless.Hogvards;
 public class SpellCaster {
-    public void cast(String spell, SpellAction act){
+    public String cast(String spell, SpellAction act){
         System.out.println(act.cast(spell));
+        return spell;
     }
 }
---
+++ src/test/java/faang/school/godbless/Hogvards/SpellCasterTest.java
@@ -0,0 +1,15 @@
+package faang.school.godbless.Hogvards;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class SpellCasterTest {
+    SpellCaster spellCaster = new SpellCaster();
+
+    @Test
+    void testCastAlohomora() {
+        String res = spellCaster.cast("Alohomora", thisSpell -> "The door is unlocked by " + thisSpell);
+        assertEquals("The door is unlocked by Alohomora", res);
+    }
+}
