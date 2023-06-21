ec2641085d22a436349fc013ea8090ed2bf725ba
Devilserker <demonserker@gmail.com>
2023-06-21 20:33:28 +0600

    Убрал if для пробела в алгоритме шифра

--- src/main/java/faang/school/godbless/droid_secrets/Droid.java
+++ src/main/java/faang/school/godbless/droid_secrets/Droid.java
@@ -5,20 +5,16 @@ interface DroidMessageEncryptor {
 }
 
 public class Droid {
-//  У меня здесь шифр Цезаря, алгоритм шифрования и дешифрования очень похож, отличается только +key или -key,
+    //  У меня здесь шифр Цезаря, алгоритм шифрования и дешифрования очень похож, отличается только +key или -key,
 //  но как я понял задание, здесь лямбды для удобного изменения логики шифрования и дешифр, верно понимаю ?.
     public String sendEncryptedMessage(Droid toDroid, String message, int key) {
         DroidMessageEncryptor droidMessageEncryptor = (m, k) -> {
             StringBuilder result = new StringBuilder();
             for (int i = 0; i < m.length(); i++) {
                 char ch = m.charAt(i);
-                if (ch != ' ') {
-                    int originalPosition = ch;
-                    char newCh = (char) (originalPosition + k);
-                    result.append(newCh);
-                } else {
-                    result.append(ch);
-                }
+                int originalPosition = ch;
+                char newCh = (char) (originalPosition + k);
+                result.append(newCh);
             }
             return result.toString();
         };
@@ -32,13 +28,9 @@ class DroidMessageReceiver {
             StringBuilder result = new StringBuilder();
             for (int i = 0; i < m.length(); i++) {
                 char ch = m.charAt(i);
-                if (ch != ' ') {
-                    int originalPosition = ch;
-                    char newCh = (char) (originalPosition - k);
-                    result.append(newCh);
-                } else {
-                    result.append(ch);
-                }
+                int originalPosition = ch;
+                char newCh = (char) (originalPosition - k);
+                result.append(newCh);
             }
             return result.toString();
         };
