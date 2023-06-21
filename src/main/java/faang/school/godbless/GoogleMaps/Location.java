b12427299ee950d0b99ddfb8b7b0174b9bbebc50
An Dima <an.dima@slexn.com>
2023-06-21 10:47:19 +0900

    google maps

---
+++ src/main/java/faang/school/godbless/GoogleMaps/Location.java
@@ -0,0 +1,12 @@
+package faang.school.godbless.GoogleMaps;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class Location {
+    private String name;
+    private Double latitude;
+    private Double longitude;
+}
