54fb57298f8dfbaf02922b0dff568108c754cfce
Zhargal Budaev <budaev17@mail.ru>
2023-06-21 23:51:15 +0700

    Решил задачу Meta вселенные?

---
+++ src/main/java/faang/school/godbless/meta_universe/Notification.java
@@ -0,0 +1,18 @@
+package faang.school.godbless.meta_universe;
+
+public class Notification {
+    private String type;
+    private String message;
+    Notification(String type, String message) {
+        this.type = type;
+        this.message = message;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+}
