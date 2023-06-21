c58bf75be04fae7c60d19fb5b6ef0c0e8ae64917
DiegaLoL <alex.eey@mail.ru>
2023-06-22 01:08:30 +0900

    bc-1007 UberFare

---
+++ src/main/java/UberFare/FareCalculator.java
@@ -0,0 +1,10 @@
+package UberFare;
+
+
+import java.util.function.BiFunction;
+
+public class FareCalculator {
+    public double calculateFare(double distance, double time, BiFunction<Double, Double, Double> fare) {
+        return fare.apply(distance, time);
+    }
+}
