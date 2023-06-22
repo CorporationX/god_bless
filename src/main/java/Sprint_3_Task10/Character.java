24e77aea379ba1f89861e108cb42cbd6963c8417
zazozhik <ya.940@mail.ru>
2023-06-22 14:03:34 +0300

    Реализовал условию задачи Lord of the Rings RPG

--- src/main/java/Sprint_3_Task10/Character.java
+++ src/main/java/Sprint_3_Task10/Character.java
@@ -1,9 +1,10 @@
 package Sprint_3_Task10;
 
+import lombok.Data;
+
 import java.util.ArrayList;
 import java.util.List;
-
+@Data
 public class Character {
     List<Item> inventory = new ArrayList<>();
-
 }
--- src/main/java/Sprint_3_Task10/Item.java
+++ src/main/java/Sprint_3_Task10/Item.java
@@ -6,4 +6,9 @@ import lombok.Data;
 public class Item {
     private String name;
     private int value;
+
+    public Item(String name, int value) {
+        this.name = name;
+        this.value = value;
+    }
 }
