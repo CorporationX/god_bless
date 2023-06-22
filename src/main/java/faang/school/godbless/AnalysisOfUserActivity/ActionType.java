f96c01fb2af4784d882a215e52d8ce7b7262eb5d
Zhugalev Sergei <zhugalcf@gmail.com>
2023-06-22 16:37:20 +0300

    Revert "Not final"
    This reverts commit 14f5d52295f6f9c503bc141db080ad8b60761dce.

--- src/main/java/faang/school/godbless/AnalysisOfUserActivity/ActionType.java
+++
@@ -1,8 +0,0 @@
-package faang.school.godbless.AnalysisOfUserActivity;
-
-public enum ActionType {
-    POST,
-    COMMENT,
-    LIKE,
-    SHARE,
-}
--- src/main/java/faang/school/godbless/AnalysisOfUserActivity/UserAction.java
+++
@@ -1,6 +0,0 @@
-package faang.school.godbless.AnalysisOfUserActivity;
-
-import java.time.LocalDate;
-
-public record UserAction(int id, String name, ActionType actionType, LocalDate date, String content) {
-}
