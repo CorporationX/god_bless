5aba8fa00abe459b49c75215f00b42403432b94b
Никита <nikita.armeec.88@mail.ru>
2023-06-22 14:56:54 +0300

    Изменен класс Coordinates

--- src/main/java/faang/school/godbless/kxnvg/matrix/Coordinates.java
+++ src/main/java/faang/school/godbless/kxnvg/matrix/Coordinates.java
@@ -2,10 +2,8 @@ package faang.school.godbless.kxnvg.matrix;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
-import lombok.Setter;
 
 @Getter
-@Setter
 @AllArgsConstructor
 public class Coordinates {
 
