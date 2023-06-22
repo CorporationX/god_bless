2fb4af49257ca67032c20e02fac4f9dfb1fc6cb7
Tamerlan <100015209+Devilserk@users.noreply.github.com>
2023-06-22 14:06:07 +0600

    Задание "Фильтры Instagram" выполнено (#596)
    * Задание "Фильтры Instagram" выполнено
    
    * Класс Image вынесен в отдельный файл

---
+++ src/main/java/faang/school/godbless/instagram_filters/Image.java
@@ -0,0 +1,11 @@
+package faang.school.godbless.instagram_filters;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@AllArgsConstructor
+@Getter
+class Image {
+    private String name;
+    private String description;
+}
---
+++ src/test/java/faang/school/godbless/instagram_filters/FilterProcessorTest.java
@@ -0,0 +1,41 @@
+package faang.school.godbless.instagram_filters;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.function.Function;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class FilterProcessorTest {
+    Image originalImage;
+    FilterProcessor filterProcessor = new FilterProcessor();
+    Function<Image, Image> grayscaleFilter;
+    Function<Image, Image> sepiaFilter;
+    Function<Image, Image> vignetteFilter;
+
+    @BeforeEach
+    void setUp() {
+        originalImage = new Image("photo", "No filters");
+        grayscaleFilter = (image) -> new Image(image.getName() + "_grayscale", "Фильтр: черно-белый");
+        sepiaFilter = (image) -> new Image(image.getName() + "_sepia", "Фильтр: сепия");
+        vignetteFilter = (image) -> new Image(
+                image.getName() + "_vignette",
+                image.getDescription() + " виньетка");
+    }
+
+    @Test
+    void testApplyFilter() {
+        Image newImage = filterProcessor.applyFilter(originalImage, grayscaleFilter);
+        assertTrue(newImage.getName().contains("grayscale"));
+        assertTrue(newImage.getDescription().contains("Фильтр: черно-белый"));
+    }
+
+    @Test
+    void testCombineFilters() {
+        Function<Image, Image> sepiaAndVignette = filterProcessor.combineFilters(sepiaFilter, vignetteFilter);
+        Image newImage = filterProcessor.applyFilter(originalImage, sepiaAndVignette);
+        assertTrue(newImage.getName().contains("sepia_vignette"));
+        assertTrue(newImage.getDescription().contains("Фильтр: сепия виньетка"));
+    }
+}
