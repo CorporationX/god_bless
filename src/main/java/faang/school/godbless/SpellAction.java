68e1953abd046093a9330b8ec0d5e11512fdea35
Stepashkins <121392656+Stepashkins@users.noreply.github.com>
2023-06-22 16:31:02 +0300

    Feature/stepan bc 943(hogwards) (#641)

---
+++ src/main/java/faang/school/godbless/SpellAction.java
@@ -0,0 +1,5 @@
+package faang.school.godbless;
+
+public interface SpellAction {
+    String spell(String spell);
+}
---
+++ src/main/java/faang/school/godbless/SpellCaster.java
@@ -0,0 +1,19 @@
+package faang.school.godbless;
+
+public class SpellCaster {
+    public void cast(String spell, SpellAction spellAction){
+        System.out.println(spellAction.spell(spell));
+    }
+
+    public static void main(String[] args) {
+        SpellCaster spellCaster = new SpellCaster();
+
+        String alohomora = "Alohomora";
+        String lumos = "Lumos";
+        String expelliarmus = "Expelliarmus";
+
+        spellCaster.cast(alohomora, (spell) -> "The door is unlocked by " + spell);
+        spellCaster.cast(lumos, (spell) -> "A beam of light is created by " + spell);
+        spellCaster.cast(expelliarmus, (spell) -> "The opponent is disarmed by " + spell);
+    }
+}
