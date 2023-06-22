6f6a090cd472416538093418e2d225b05d239d6d
Dobrovolsky Artem <artdobrov@mail.ru>
2023-06-23 00:01:18 +0300

    Создан классы Location, LocationSearchEngine и запускаемый класс для проверки работоспособности.

---
+++ src/main/java/faang/school/godbless/Google/Maps/Location.java
@@ -0,0 +1,14 @@
+package faang.school.godbless.Google.Maps;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@AllArgsConstructor
+@Setter
+@Getter
+public class Location {
+    private String name;
+    private double latitude;
+    private double longitude;
+}
---
+++ src/main/java/faang/school/godbless/Google/Maps/LocationSearchEngine.java
@@ -0,0 +1,45 @@
+package faang.school.godbless.Google.Maps;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.function.Consumer;
+import java.util.function.Function;
+import java.util.function.Predicate;
+
+public class LocationSearchEngine {
+    public List<Location> filterLocations(List<Location> locationList, Predicate<Location> predicate) {
+        List<Location> correctLocations = null;
+
+        try {
+            correctLocations = locationList.getClass().newInstance();
+        } catch (InstantiationException e) {
+            throw new RuntimeException(e);
+        } catch (IllegalAccessException e) {
+            throw new RuntimeException(e);
+        }
+
+        for (Location location : locationList) {
+            if (predicate.test(location)) {
+                correctLocations.add(location);
+            }
+        }
+        // TODO: 22.06.2023 Добавить Optional, чтобы избежать NullPointerException
+        return correctLocations;
+    }
+
+    public void processLocations(List<Location> locationList, Consumer<Location> consumer) {
+        for (Location location: locationList){
+            consumer.accept(location);
+        }
+    }
+
+    public List<Double> calculateDistances(List<Location> locationList, Function<Location, Double> function) {
+        List<Double> distances = new ArrayList<>();
+
+        for (Location location: locationList){
+            distances.add(function.apply(location));
+        }
+        return distances;
+    }
+
+}
