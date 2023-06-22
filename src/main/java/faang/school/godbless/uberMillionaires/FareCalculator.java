8f007ce853d6a8f7edd39f121332d1a2619af8e2
Egor Selivanov <egorselivanov0@gmail.com>
2023-06-22 17:01:12 +0300

    Решена задача "Миллиончики Uber"

---
+++ src/main/java/faang/school/godbless/uberMillionaires/FareCalculator.java
@@ -0,0 +1,13 @@
+package faang.school.godbless.uberMillionaires;
+
+import java.util.function.BiFunction;
+
+public class FareCalculator {
+
+    public double calculateFare(double distance, double time, BiFunction<Double, Double, Double> biFunction) {
+        if (biFunction == null) {
+            throw new IllegalArgumentException("Объект biFunction равен null!");
+        }
+        return biFunction.apply(distance, time);
+    }
+}
