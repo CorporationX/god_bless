b0e4b637c8ddc050547ed1f2d91861f89fb00d05
Kirill <soshnikov17@gmail.com>
2023-06-23 00:01:24 +0300

    Выполнена задачи про мета-вселенную и написаны тесты

---
+++ src/main/java/faang/school/godbless/metaverse/Notification.java
@@ -0,0 +1,13 @@
+package faang.school.godbless.metaverse;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+public class Notification {
+    private String type;
+    private String message;
+}
