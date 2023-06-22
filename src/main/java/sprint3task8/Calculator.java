aca939fa068ab412e249a04f2bec386effecf6a7
Zlata Galaburdina <585zlata@mail.ru>
2023-06-22 22:50:28 +0300

    new commit

--- src/main/java/sprint3task8/Calculator.java
+++ src/main/java/sprint3task8/Calculator.java
@@ -1,9 +1,8 @@
 package sprint3task8;
 
 @FunctionalInterface
-public interface Calculator<U, R> {
-    R calculate(U p1, U p2);
+public interface Calculator {
+    int calculateIt(int p1, int p2);
 
-    void calculate(Integer num);
 }
    // public static int calculate(List<Integer> nums, Calculator calculator) throws IllegalArgumentException
--- src/main/java/sprint3task8/ClassForCalculator.java
+++ src/main/java/sprint3task8/ClassForCalculator.java
@@ -2,22 +2,30 @@ package sprint3task8;
 import java.util.List;
 
 public class ClassForCalculator {
-
     public static int calculate
             (List<Integer> nums, Calculator calculator)
-            throws IllegalArgumentException{
-        for (Integer num: nums) {
-            calculator.calculate(num);
-            //два аргумента в интерфейсе ж
-            return num;
+            throws IllegalArgumentException {
+        //2 3 4 5 6   140  2 5 9...140 140
+        int temp = nums.get(0);
+        for (int i = 1; i < nums.size(); i++) {
+            temp = calculator.calculateIt(temp, nums.get(i));
+            //temp = nums.get(i)*nums.get(i-1);
+            //nums.get(i)= temp;
         }
+        return temp;
     }
-
-    public static int product(List<Integer> nums){
+    public static int product(List<Integer> nums) {
         //Произведение чисел во всем списке
-
+        return calculate(nums, (p1, p2) -> p1 * p2);
     }
+
     public static int sum(List<Integer> nums) {
         //Сумма чисел всего списка
+        return calculate(nums, (p1, p2) -> p1 + p2);
+    }
+    public static void main(String[] args) {
+        List<Integer> list = List.of(4, 1, 2, -1, 1);
+        System.out.println(sum(list));
+        System.out.println(product(list));
     }
 }
