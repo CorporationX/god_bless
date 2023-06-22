40a43a1df2099a5277abe4b3fce88b4ef442a2a0
Egor Selivanov <egorselivanov0@gmail.com>
2023-06-22 19:48:07 +0300

    Решена задача "Gmail Rich Filters"

---
+++ src/main/java/faang/school/godbless/gmailRichFilters/EmailProcessor.java
@@ -0,0 +1,29 @@
+package faang.school.godbless.gmailRichFilters;
+
+import java.util.List;
+import java.util.function.Consumer;
+import java.util.function.Function;
+import java.util.function.Predicate;
+
+public class EmailProcessor {
+    public void processEmails(List<Email> emailList, Predicate<Email> predicate, Consumer<Email> consumer,
+                              Function<Email, String> function) {
+        if (emailList == null) {
+            throw new IllegalArgumentException("Переданный список равен null!");
+        }
+        if (predicate == null) {
+            throw new IllegalArgumentException("Метод для фильтрации писем равен null!");
+        }
+        if (consumer == null) {
+            throw new IllegalArgumentException("Метод для обработки писем равен null!");
+        }
+        if (function == null) {
+            throw new IllegalArgumentException("Метод для преобразования писем равен null!");
+        }
+        for (Email email: emailList) {
+            email.setImportant(predicate.test(email));
+            consumer.accept(email);
+            email.setBody(function.apply(email));
+        }
+    }
+}
