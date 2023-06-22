f6b0fa021590cf183e5b0558e87c67d85e2746ae
Stepan Palkov <step.palkov@gmail.com>
2023-06-23 00:08:23 +0300

    complete task10 (ExceptionHandler)

---
+++ src/main/java/faang/school/godbless/ErrorHandler.java
@@ -0,0 +1,13 @@
+package faang.school.godbless;
+
+import java.util.function.Function;
+
+public class ErrorHandler {
+    public static <T, P> T withErrorHandling(P parameter, Function<P, T> function, ExceptionHandler<T> exceptionHandler){
+        try{
+            return function.apply(parameter);
+        } catch (Exception e){
+            return exceptionHandler.handleException(e);
+        }
+    }
+}
