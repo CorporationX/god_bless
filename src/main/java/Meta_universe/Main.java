3190fe5d30147922f8b18296a780833fc49579d8
Irina Vereshchagina <irankamoya@gmail.com>
2023-06-22 15:26:08 +0300

    Задача - Meta-вселенная?

---
+++ src/main/java/Meta_universe/Main.java
@@ -0,0 +1,28 @@
+package Meta_universe;
+
+import java.util.function.Predicate;
+
+public class Main {
+    public static void main(String[] args) {
+        NotificationManager notificationManager = new NotificationManager();
+
+// Регистрация обработчиков оповещений
+        notificationManager.registerHandler("email", (notification) -> System.out.println("Отправка по электронной почте: " + notification.getMessage()));
+        notificationManager.registerHandler("sms", (notification) -> System.out.println("Отправка SMS: " + notification.getMessage()));
+        notificationManager.registerHandler("push", (notification) -> System.out.println("Отправка push-уведомления: " + notification.getMessage()));
+
+// Отправка оповещений
+        Notification emailNotification = new Notification("email", "Ваша учетная запись успешно активирована");
+        Notification smsNotification = new Notification("sms", "Вы успешно изменили свой пароль");
+        Notification pushNotification = new Notification("push", "Новый пост от пользователя: JohnDoe");
+
+        notificationManager.sendNotification(emailNotification);
+        notificationManager.sendNotification(smsNotification);
+        notificationManager.sendNotification(pushNotification);
+
+//Отправка отфильтрованных сообщений
+        Predicate<Notification> isEmail = notification -> notification.getType().equals("email");
+        notificationManager.filterAndSendNotification(emailNotification,isEmail);
+        notificationManager.filterAndSendNotification(smsNotification,isEmail);
+    }
+}
---
+++ src/main/java/Meta_universe/Notification.java
@@ -0,0 +1,17 @@
+package Meta_universe;
+
+public class Notification {
+    private String type;
+    private String message;
+
+    public Notification(String type, String message){
+        this.type = type;
+        this.message = message;
+    }
+    public String getMessage(){
+        return message;
+    }
+    public String getType(){
+        return type;
+    }
+}
