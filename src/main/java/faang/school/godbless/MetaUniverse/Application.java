719af5aba273ca6ae8e4ec618750ba7772b82e1f
chenso29 <you@example.com>
2023-06-22 21:20:14 +0900

    additional task

--- src/main/java/faang/school/godbless/MetaUniverse/Application.java
+++ src/main/java/faang/school/godbless/MetaUniverse/Application.java
@@ -4,18 +4,22 @@ public class Application {
     public static void main(String... args) {
         NotificationManager notificationManager = new NotificationManager();
 
-// Регистрация обработчиков оповещений
+        // Регистрация обработчиков оповещений
         notificationManager.registerHandler("email", (notification) -> System.out.println("Отправка по электронной почте: " + notification.getMessage()));
         notificationManager.registerHandler("sms", (notification) -> System.out.println("Отправка SMS: " + notification.getMessage()));
         notificationManager.registerHandler("push", (notification) -> System.out.println("Отправка push-уведомления: " + notification.getMessage()));
 
-// Отправка оповещений
+        // Отправка оповещений
         Notification emailNotification = new Notification("email", "Ваша учетная запись успешно активирована");
         Notification smsNotification = new Notification("sms", "Вы успешно изменили свой пароль");
         Notification pushNotification = new Notification("push", "Новый пост от пользователя: JohnDoe");
+        Notification spamNotification = new Notification("sms", "пост:");
+        Notification shortNotification = new Notification("push", "Новый пост от пользователя: Spam");
 
         notificationManager.sendNotification(emailNotification);
         notificationManager.sendNotification(smsNotification);
         notificationManager.sendNotification(pushNotification);
+        notificationManager.sendNotification(spamNotification);
+        notificationManager.sendNotification(shortNotification);
     }
 }
--- src/main/java/faang/school/godbless/MetaUniverse/NotificationManager.java
+++ src/main/java/faang/school/godbless/MetaUniverse/NotificationManager.java
@@ -1,10 +1,16 @@
 package faang.school.godbless.MetaUniverse;
 
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 import java.util.function.Consumer;
+import java.util.function.Function;
+import java.util.function.Predicate;
 
 public class NotificationManager {
+    private final Map<String, Predicate<Notification>> notificationFilters = Map.of(
+            "Spam", (notification) -> !notification.getMessage().toLowerCase().contains("spam"),
+            "TooShort", (notification -> notification.getMessage().length() > 10));
     private final Map<String, Consumer<Notification>> notificationMap;
 
     public NotificationManager() {
@@ -12,11 +18,24 @@ public class NotificationManager {
     }
 
     public void registerHandler(String type, Consumer<Notification> notificationConsumer) {
-       notificationMap.put(type, notificationConsumer);
+        notificationMap.put(type, notificationConsumer);
     }
 
+    //Реализация не очень. Сделал просто чтобы показать, что понимаю о чем речь
     public void sendNotification(Notification notification) {
         Consumer<Notification> notificationConsumer = notificationMap.get(notification.getType());
-        notificationConsumer.accept(notification);
+        if (notificationFilters.values().stream().allMatch(filter -> filter.test(notification))) {
+            notificationConsumer.accept(notification);
+        } else {
+            Function<Notification, Notification> function = (input) -> {
+                if (!notificationFilters.get("Spam").test(input)) {
+                    return new Notification(input.getType(), input.getMessage().toLowerCase().replaceAll("spam", "NonSP*M"));
+                } else {
+                    return new Notification(input.getType(), "Новое сообщение, сформированное взамен короткого");
+                }
+            };
+            notificationConsumer.accept(function.apply(notification));
+        }
     }
+
 }
