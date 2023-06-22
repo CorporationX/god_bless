5444113934807b325b4188ed5023c062b1d7e83f
Adil Ilchubaev <adil.200191@gmail.com>
2023-06-22 23:46:17 +0600

    Задание сделал, надо еще дописать юнит тест

--- src/main/java/faang/school/godbless/Application.java
+++ src/main/java/faang/school/godbless/Application.java
@@ -1,7 +1,35 @@
 package faang.school.godbless;
+import java.util.List;
 
 public class Application {
-    public static void main(String... args) {
 
+    public static int calculate(List<Integer> nums, Calculator<Integer> calculator) throws IllegalArgumentException {
+        if (nums == null || calculator == null)
+            throw new IllegalArgumentException("Invalid input");
+
+        int result = nums.get(0);
+        for (int i = 1; i < nums.size(); i++) {
+            result = calculator.calculate(result, nums.get(i));
+        }
+
+        return result;
+    }
+
+    public static int product(List<Integer> nums) {
+        return calculate(nums, (a, b) -> a * b);
+    }
+
+    public static int sum(List<Integer> nums) {
+        return calculate(nums, (a, b) -> a + b);
+    }
+
+    public static void main(String... args) {
+        List<Integer> numbers = List.of(2, 4, 6, 8);
+
+        int productResult = product(numbers);
+        int sumResult = sum(numbers);
+
+        System.out.println("Product: " + productResult);
+        System.out.println("Sum: " + sumResult);
     }
 }
