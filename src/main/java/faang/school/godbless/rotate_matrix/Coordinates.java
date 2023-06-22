0e130fd66d0384bef93b0ed6b7263489fbe80a68
Zhargal Budaev <budaev17@mail.ru>
2023-06-22 20:48:24 +0700

    Решил задачу Повернём матрицу

---
+++ src/main/java/faang/school/godbless/rotate_matrix/Coordinates.java
@@ -0,0 +1,11 @@
+package faang.school.godbless.rotate_matrix;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@AllArgsConstructor
+@Getter
+public class Coordinates {
+    private int x;
+    private int y;
+}
