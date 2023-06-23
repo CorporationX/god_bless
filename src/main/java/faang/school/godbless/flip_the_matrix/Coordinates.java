9742a06be187601cffe6e2846a39dbff81a42a38
Alexander Kolesnikov <kolesnikov.kite@gmail.com>
2023-06-23 18:02:34 +0100

    Initial commit

---
+++ src/main/java/faang/school/godbless/flip_the_matrix/Coordinates.java
@@ -0,0 +1,12 @@
+package faang.school.godbless.flip_the_matrix;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class Coordinates {
+
+    private int x;
+    private int y;
+}
---
+++ src/main/java/faang/school/godbless/flip_the_matrix/FlipMatrix.java
@@ -0,0 +1,30 @@
+package faang.school.godbless.flip_the_matrix;
+
+public class FlipMatrix {
+
+  public int[][] flipMatrix(int[][] matrix, FlipDirection flipDirection) {
+        MatrixTransformer transformer;
+
+        if (flipDirection == FlipDirection.HORIZONTAL) {
+            transformer = (x, y) -> new Coordinates(x, matrix[0].length - 1 - y);
+        } else if (flipDirection == FlipDirection.VERTICAL) {
+            transformer = (x, y) -> new Coordinates(matrix.length - 1 - x, y);
+        } else {
+            throw new IllegalArgumentException("Invalid flip direction");
+        }
+
+        return transformMatrix(matrix, transformer);
+    }
+
+    private int[][] transformMatrix(int[][] matrix, MatrixTransformer transformer) {
+        int[][] transformedMatrix = new int[matrix.length][matrix[0].length];
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[i].length; j++) {
+                Coordinates newCoordinates = transformer.transform(i, j);
+                transformedMatrix[newCoordinates.getX()][newCoordinates.getY()] = matrix[i][j];
+            }
+        }
+
+        return transformedMatrix;
+    }
+}
