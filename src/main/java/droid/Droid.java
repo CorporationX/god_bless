1ca707f611f6992608b50213c4462b3a7a6327af
Dregid <ufl852@gmail.com>
2023-06-23 23:37:46 +0600

    Рефакторинг полей и метода дешифровки класса Droid

--- src/main/java/droid/Droid.java
+++ src/main/java/droid/Droid.java
@@ -1,13 +1,13 @@
 package droid;
 
 public class Droid {
-    private final int engAlphabetSize = 26;
-    private final DroidMessageEncryptor encryptor = (inputMessage, inputKey) -> {
+    private static final int ENG_ALPHABET_SIZE = 26;
+    private static final DroidMessageEncryptor ENCRYPTOR = (inputMessage, inputKey) -> {
         StringBuilder builder = new StringBuilder();
         for (char ch : inputMessage.toCharArray()) {
             if (Character.isAlphabetic(ch)) {
                 int currentAlphabetPosition = Character.isUpperCase(ch) ? ch - 'A' : ch - 'a';
-                int newAlphabetPosition = (currentAlphabetPosition + inputKey) % engAlphabetSize;
+                int newAlphabetPosition = (currentAlphabetPosition + inputKey) % ENG_ALPHABET_SIZE;
 
                 char newChar = (char) ((Character.isUpperCase(ch) ? 'A' : 'a') + newAlphabetPosition);
                 builder.append(newChar);
@@ -19,16 +19,13 @@ public class Droid {
     };
 
     public String sendEncryptedMessage(String message, int key) {
-        if ((key % engAlphabetSize) == 0) {
+        if ((key % ENG_ALPHABET_SIZE) == 0) {
             return message;
         }
-        return encryptor.encrypt(message, key);
+        return ENCRYPTOR.encrypt(message, key);
     }
 
     public String receiveEncryptedMessage(String message, int key) {
-        if ((key % engAlphabetSize) == 0) {
-            return message;
-        }
-        return encryptor.encrypt(message, engAlphabetSize - (key % engAlphabetSize));
+        return sendEncryptedMessage(message, ENG_ALPHABET_SIZE - (key % ENG_ALPHABET_SIZE));
     }
 }
