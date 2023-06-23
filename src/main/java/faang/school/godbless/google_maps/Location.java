3384942f92882ccbe05ccfaef9c4184f32766f63
Alexander Kolesnikov <kolesnikov.kite@gmail.com>
2023-06-23 13:02:44 +0100

    For PR Complete "Google Maps" task with tests

--- src/main/java/faang/school/godbless/google_maps/Location.java
+++ src/main/java/faang/school/godbless/google_maps/Location.java
@@ -1,13 +1,26 @@
 package faang.school.godbless.google_maps;
 
-import lombok.AllArgsConstructor;
 import lombok.Getter;
 
 @Getter
-@AllArgsConstructor
+
 public class Location {
 
     private String name;
     private double latitude;
     private double longitude;
+
+    public Location(String name, double latitude, double longitude) {
+        validateName(name);
+
+        this.name = name;
+        this.latitude = latitude;
+        this.longitude = longitude;
+    }
+
+    private void validateName(String name) {
+        if (name.isBlank()) {
+            throw new IllegalArgumentException("Name can't be empty");
+        }
+    }
 }
