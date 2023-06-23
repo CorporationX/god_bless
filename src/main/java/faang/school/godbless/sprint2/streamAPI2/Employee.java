8e0a6a92904b431d51ad23fbab9fbbe91f347b45
Pashkevich <ppavel012100@gmail.com>
2023-06-23 22:06:47 +0200

    resolve train StreamAPI #2 task Без тестов) и 4 условия

---
+++ src/main/java/faang/school/godbless/sprint2/streamAPI2/Employee.java
@@ -0,0 +1,13 @@
+package faang.school.godbless.sprint2.streamAPI2;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class Employee {
+    private String name;
+    private String Department;
+    private int salary;
+
+}
