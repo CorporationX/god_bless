d30e4db959a10fec05de7fc4c99cdfe44d5f5e33
An Dima <an.dima@slexn.com>
2023-06-23 09:54:57 +0900

    npe handled

--- src/main/java/faang/school/godbless/UberMillions/FareCalculator.java
+++ src/main/java/faang/school/godbless/UberMillions/FareCalculator.java
@@ -4,6 +4,9 @@ import java.util.function.BiFunction;
 
 public class FareCalculator {
     public Double calculateFare(Double distance, Double time, BiFunction<Double, Double, Double> biFunction) {
+        if(distance == null || time == null){
+            throw new NullPointerException();
+        }
         return biFunction.apply(distance,time);
     }
 }
