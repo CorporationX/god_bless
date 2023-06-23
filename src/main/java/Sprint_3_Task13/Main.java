37102e23205f63004b7439bafc547da61dfa64ea
zazozhik <ya.940@mail.ru>
2023-06-23 14:54:32 +0300

    Реализовал условию задачи CSV_build

--- src/main/java/Sprint_3_Task13/Main.java
+++ src/main/java/Sprint_3_Task13/Main.java
@@ -34,14 +34,14 @@ public class Main {
     }
 
     public static void main(String[] args) {
-//        System.out.println(toCsv(
-//                List.of(
-//                        List.of("1", "2", "3", "4", "5"),
-//                        List.of("6", "7", "8", "9", "10"),
-//                        List.of("11", "12", "13", "14", "15"),
-//                        List.of("16", "17", "18", "19", "20"),
-//                        List.of("21", "22", "23", "24", "25")
-//                )
-//        ));
+        System.out.println(toCsv(
+                List.of(
+                        List.of("1", "2", "3", "4", "5"),
+                        List.of("6", "7", "8", "9", "10"),
+                        List.of("11", "12", "13", "14", "15"),
+                        List.of("16", "17", "18", "19", "20"),
+                        List.of("21", "22", "23", "24", "25")
+                )
+        ));
     }
 }
