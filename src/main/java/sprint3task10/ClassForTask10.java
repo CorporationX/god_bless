9682921f7d7d3dfbc31c1357738acaad3574ac78
Zlata Galaburdina <585zlata@mail.ru>
2023-06-23 16:48:56 +0300

    Next new commit task10

--- src/main/java/sprint3task10/ClassForTask10.java
+++ src/main/java/sprint3task10/ClassForTask10.java
@@ -2,40 +2,64 @@ package sprint3task10;
 import java.util.function.Supplier;
 
 public class ClassForTask10 {
-    RemoteService remoteService = new RemoteService();
+
     public static void main(String[] args) {
-        //нужен метод, который будет писать:
-        try {
-            return remoteService.call(param);
-        } catch (Exception e) {
-            log.warn("Не удалось получить доступ к Remote Service. Вернем default значение");
-            return "DEFAULT";
-        }
+//        RemoteService remoteService = new RemoteService();
+//        //нужен метод, который будет писать:
+//        try {
+//            return remoteService.call(param);
+//        } catch (Exception e) {
+//            log.warn("Не удалось получить доступ к Remote Service. Вернем default значение");
+//            return "DEFAULT";
+//        }
+
+        String url = "1234";
+        String result = withErrorHandling1(
+                ()->{ System.out.println("Вызываем " + url);
+                    return "Успех";},
+                (ohNo) -> { System.out.println("Не удалось получить доступ к Remote Service. Вернем default значение");
+                return url;
+                }
+        );
+        System.out.println(result);
     }
-    public static <T> T withErrorHandling
-            (Supplier<T>  lambda1, V ExceptionHandler <T> lambda2, RemoteService param) {
-//Внутри withErrorHandling нужно написать try/catch.
-//Внутри try использовать action, внутри catch использовать onError
-//и передать туда пойманный Exception.
+//    public static <T> T withErrorHandling
+//            (Supplier<T>  lambda1, ExceptionHandler <T> lambda2) {
+////Внутри withErrorHandling нужно написать try/catch.
+////Внутри try использовать action, внутри catch использовать onError
+////и передать туда пойманный Exception.
+//
+//        RemoteService remoteService = new RemoteService();
+////Первая лямбда - то, что нужно сделать; вызов удаленного сервиса remoteService.call(param);
+//        try {
+//            Supplier<T> lambda1 =  () -> {
+//                remoteService.call(" ");
+//            return lambda1;};
+//        }
+////.call = .get ?
+////Вторая лямбда - то, что нужно сделать при ошибке первого действия.
+//        catch (Exception e) {
+//            ExceptionHandler<T> lambda2 = ohNo -> {
+//                System.out.println("Не удалось получить доступ к Remote Service. Вернем default значение") +
+//                return "DEFAULT";
+//            };
+//            lambda2.handlingWithException();
+//        }
+//        return T;
+//    }
 
-//Первая лямбда - то, что нужно сделать; вызов удаленного сервиса remoteService.call(param);
-        try {
-            Supplier<T> lambda1 = param -> {
-                remoteService.call(param);};
-            return lambda1;
+    public static String withErrorHandling1
+            (Supplier<String>  lambda1, ExceptionHandler <String> lambda2) {
+
+        try{
+           //throw new IllegalArgumentException();
+            return lambda1.get();
         }
-//.call = .get ?
-// Вторая лямбда - то, что нужно сделать при ошибке первого действия.
-        catch (Exception e) {
-            ExceptionHandler<T> lambda2 = ohNo -> {
-                System.out.println("Не удалось получить доступ к Remote Service. Вернем default значение") +
-                return "DEFAULT";
-            };
-            lambda2.handlingWithException();
+        catch (Exception e){
+            return lambda2.handlingWithException(e);
         }
-        return null;
-    }
-}
 
 
 
+
+    }}
