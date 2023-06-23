3f904ec3377a684e5346a1374a628f3a6f11e3c8
Andrey <khalyavinandrey@gmail.com>
2023-06-23 18:50:06 +0500

    notification manager class

---
+++ src/main/java/faang/school/godbless/NotificationManager.java
@@ -0,0 +1,24 @@
+package faang.school.godbless;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.function.Consumer;
+
+
+public class NotificationManager {
+    private final Map<String, Consumer<Notification>> data = new HashMap<>();
+
+    public Map<String, Consumer<Notification>> getData() {
+        return data;
+    }
+
+    public void registerHandler(String type,
+                                Consumer<Notification> notificationConsumer) {
+        data.put(type, notificationConsumer);
+    }
+
+    public void sendNotification(Notification notification) {
+        Consumer<Notification> notificationConsumer = data.get(notification.getType());
+        notificationConsumer.accept(notification);
+    }
+}
