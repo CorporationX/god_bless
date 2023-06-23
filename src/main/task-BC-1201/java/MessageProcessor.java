2bc5175d74b0542df4a68c2e47d797cef1dcfbe7
Alex Tarasov <tarasof.av@gmail.com>
2023-06-23 16:35:04 +0300

    fix task Spam Filter

--- src/main/task-BC-1201/java/MessageProcessor.java
+++ src/main/task-BC-1201/java/MessageProcessor.java
@@ -3,6 +3,6 @@ import java.util.List;
 public class MessageProcessor {
     public boolean processMessage(String message, List<MessageFilter> messageFilters) {
 
-        return messageFilters.stream().anyMatch(messageFilter -> !messageFilter.filter(message));
+        return messageFilters.stream().allMatch(messageFilter -> messageFilter.filter(message));
     }
 }
