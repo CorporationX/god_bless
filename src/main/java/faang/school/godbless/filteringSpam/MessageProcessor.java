c744faf5be172f7cf37cd979e250ea42ccb40512
M1khail <mishalion888@gmail.com>
2023-06-23 12:07:37 +0200

    filtering spam (with description)

--- src/main/java/faang/school/godbless/filteringSpam/MessageProcessor.java
+++ src/main/java/faang/school/godbless/filteringSpam/MessageProcessor.java
@@ -5,5 +5,6 @@ import java.util.List;
 public class MessageProcessor {
     public boolean processMessage(String message, List<MessageFilter> messageFilters) {
         return messageFilters.stream().allMatch(MessageFilter -> MessageFilter.filter(message));
+        // allMatch - не один элемент потока не соот. условию
     }
 }
--- src/test/java/MessageProcessorTest.java
+++ src/test/java/MessageProcessorTest.java
@@ -18,29 +18,30 @@ public class MessageProcessorTest {
     public void init() {
         messageProcessor = new MessageProcessor();
 
-        // Обработка сообщений
+        // Список сообщений
         messages = new String[]{"Привет!", "Это спам!", "Как дела? 😀", "Длинное сообщение без спама и эмодзи"};
 
         // Создание фильтров
-        MessageFilter spamFilter = message -> !message.toLowerCase().contains("спам");
+        MessageFilter spamFilter = message -> !message.toLowerCase().contains("спам"); // слово или предложение в нижнем регистре не имеет слово "спам"
         MessageFilter lengthFilter = message -> message.length() < 10;
-        MessageFilter emojiFilter = message -> !message.contains("😀");
+        MessageFilter emojiFilter = message -> !message.contains("😀"); // слово или предложение не содержит смайлика
 
-        messageFilters = Arrays.asList(spamFilter, lengthFilter, emojiFilter);
+        messageFilters = Arrays.asList(spamFilter, lengthFilter, emojiFilter); // сохраняем фильтры
 
         for (String message : messages) {
-            boolean isFiltered = messageProcessor.processMessage(message, messageFilters);
-            System.out.println("Сообщение: " + message + " | Пропущено: " + isFiltered);
+            boolean isFiltered = messageProcessor.processMessage(message, messageFilters); // каждому сообщению применяем фильтр
+            System.out.println("Сообщение: " + message + " | Пропущено: " + isFiltered); // если сообщение чистое ? true : false
         }
     }
     @Test
     public void messageProcessorTest() {
+        // к слово применяем к слову фильтры из списка
         boolean firstFilteredMessage = messageProcessor.processMessage(messages[0], messageFilters);
         boolean secondFilteredMessage = messageProcessor.processMessage(messages[1], messageFilters);
         boolean thirdFilteredMessage = messageProcessor.processMessage(messages[2], messageFilters);
         boolean fourthFilteredMessage = messageProcessor.processMessage(messages[3], messageFilters);
-        assertTrue(firstFilteredMessage);
-        assertFalse(secondFilteredMessage);
+        assertTrue(firstFilteredMessage); // ожидаем что слово "Привет" - чистое
+        assertFalse(secondFilteredMessage); // слово "Это спам!" - грязное
         assertFalse(thirdFilteredMessage);
         assertFalse(fourthFilteredMessage);
     }
