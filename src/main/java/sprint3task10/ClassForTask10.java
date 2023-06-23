5099422ad43d7921fe7332e274bcbec42128efb7
Zlata Galaburdina <585zlata@mail.ru>
2023-06-23 17:23:43 +0300

    Next new commit task10

--- src/main/java/sprint3task10/ClassForTask10.java
+++ src/main/java/sprint3task10/ClassForTask10.java
@@ -3,14 +3,7 @@ import java.util.function.Supplier;
 
 public class ClassForTask10 {
 
-    public static void main(String[] args) {
-        //объект класса RemoteService не нужен на самом деле
-        //будем считать, что для доступа к удаленному "микро"сервису
-        //нам достаточно иметь ссылку (URL) на него (String remoteServiceUrl = "url:262642";).
-
-        RemoteService remoteService = new RemoteService();
-
-//        //нужен метод, который будет писать:
+//        нужен метод, который будет писать:
 //        try {
 //            return remoteService.call(param);
 //        } catch (Exception e) {
@@ -18,31 +11,47 @@ public class ClassForTask10 {
 //            return "DEFAULT";
 //        }
 
-        String remoteServiceUrl = "url:262642";
-        String result = withErrorHandling1(
+    public static void main(String[] args) {
+        //объект класса RemoteService не нужен на самом деле
+        //будем считать, что для доступа к удаленному "микро"сервису
+        //нам достаточно иметь ссылку (URL) на него (String remoteServiceUrl = "url:262642";).
+        RemoteService remoteService = new RemoteService();
+
+        String remoteServiceUrl = "262642";
+
+        //пишем лямбды в main, хотя в жизни нужно их прям в коде прописывать.
+        String result = withErrorHandling(
                 () -> {
                     System.out.println("Вызываем удаленный сервис (remoteService) по URL:" + remoteServiceUrl);
                     return "Успех";
                 },
                 (ohNo) -> {
                     System.out.println("Не удалось получить доступ к Remote Service. Вернем default значение");
-                    return remoteServiceUrl;
+                    return "Возвращаем обратно URL: "+ remoteServiceUrl;
                 }
         );
+        //Нельзя было бы прописать просто System.out.println(withErrorHandling);
+        //тк в методе должно быть указано, что с лямбами случается.
+        //Поэтому через result. result на один раз.
         System.out.println(result);
     }
 
-    public static String withErrorHandling1
-            (Supplier<String> lambda1, ExceptionHandler<String> lambda2) {
+    public static <T> T withErrorHandling
+            (Supplier<T> lambda1, ExceptionHandler<T> lambda2) {
 
         try {
-            //throw new IllegalArgumentException();
-            return lambda1.get();
+            throw new IllegalArgumentException();
+            //включаем запускаем исключение и выключаем return,
+            //если хотим чтобы выполнилось catch,
+            //return lambda1.get();
         } catch (Exception e) {
             return lambda2.handlingWithException(e);
         }
     }
 }
+
+//просто старый метод withErrorHandling с первыми мылсями
+
 //    public static <T> T withErrorHandling
 //            (Supplier<T>  lambda1, ExceptionHandler <T> lambda2) {
 ////Внутри withErrorHandling нужно написать try/catch.
@@ -56,6 +65,7 @@ public class ClassForTask10 {
 //                remoteService.call(" ");
 //            return lambda1;};
 //        }
+//
 ////.call = .get ?
 ////Вторая лямбда - то, что нужно сделать при ошибке первого действия.
 //        catch (Exception e) {
