8fdd88574897c227218fa9a0d12be8501ce44b22
Pavel <pavlik1123.pg@gmail.com>
2023-06-23 19:18:08 +0700

    дбавил метод поворота матрици

--- src/main/java/faang/school/godbless/rotate_the_matrix/Main.java
+++ src/main/java/faang/school/godbless/rotate_the_matrix/Main.java
@@ -1,34 +1,32 @@
 package faang.school.godbless.rotate_the_matrix;
 
-import javax.print.DocFlavor;
-
 public class Main {
-    private static final MatrixTransformer TRANSHORIZONTEL = (x, y) -> new Coordinates(x, y);
-    private static final MatrixTransformer TRANSVERTICAL = (x, y) -> new Coordinates(y, x);
     public static void main(String[] args) {
 
-
-
         int[][] arr = {{1, 2, 3}, {4, 5, 6}, {7, 8, 9}};
+        printArr(arr);
 
+        printArr(flipMatrix(arr, FlipDirection.HORIZONTAL));
+
+        printArr(flipMatrix(arr, FlipDirection.VERTICAL));
+    }
+
+    private static void printArr(int[][] arr) {
         for (int[] ints : arr) {
             for (int num : ints) {
                 System.out.print(num + " ");
             }
             System.out.println();
         }
-
-        int[][] newArr = flipMatrix(arr, FlipDirection.VERTICAL);
-
-        for (int[] ints : newArr) {
-            for (int num : ints) {
-                System.out.print(num + " ");
-            }
-            System.out.println();
-        }
+        System.out.println();
     }
 
-    private static int[][] flipMatrix(int[][] matrix, FlipDirection flipDirection){
+    private static int[][] flipMatrix(int[][] matrix, FlipDirection flipDirection) {
+
+        MatrixTransformer TRANSHORIZONTEL = Coordinates::new;
+
+        MatrixTransformer TRANSVERTICAL = (x, y) -> new Coordinates(matrix.length - x - 1, matrix.length - y - 1);
+
         if (flipDirection.equals(FlipDirection.HORIZONTAL)) {
             return transformMatrix(matrix, TRANSHORIZONTEL);
         } else if (flipDirection.equals(FlipDirection.VERTICAL)) {
@@ -38,8 +36,11 @@ public class Main {
     }
 
     private static int[][] transformMatrix(int[][] matrix, MatrixTransformer transformer) {
-        int[][] newArrays = new int[matrix.length][matrix.length];
+        if (matrix.length == 0) {
+            return new int[0][0];
+        }
 
+        int[][] newArrays = new int[matrix.length][matrix.length];
         for (int x = 0; x < matrix.length; x++) {
             for (int y = 0; y < matrix.length; y++) {
                 Coordinates newCoordinates = transformer.transform(x, y);
