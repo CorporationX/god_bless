a76115fe022f9888e260e45b634b16f2f29efca8
Rustam Nigmatullin <rustamnigma@mail.ru>
2023-06-23 12:38:07 +0500

    BC-1001-mistakes

--- src/main/java/faang/school/godbless/mistakes/Main.java
+++ src/main/java/faang/school/godbless/mistakes/Main.java
@@ -5,7 +5,7 @@ import java.util.function.Function;
 public class Main <T> {
     public static void withErrorHandling(Function<String, String> callService, ExceptionHandler<String> exceptionHandler){
         RemoteService<String> remoteService = new RemoteService<String>();
-        String paramOfService = "parametr";
+        String paramOfService = "param";
         try {
             remoteService.call(callService.apply(paramOfService));
         } catch (Exception e){
