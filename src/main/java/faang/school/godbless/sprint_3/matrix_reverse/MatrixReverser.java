1581278898a36a92a70586d5dd0f6096c63ff66c
OneWayMy <zhenya.ivashchenko.9494@gmail.com>
2023-06-22 21:46:47 -0700

    Sprint_ReverseMatrix_PR_changes

--- src/main/java/faang/school/godbless/sprint_3/matrix_reverse/MatrixReverser.java
+++ src/main/java/faang/school/godbless/sprint_3/matrix_reverse/MatrixReverser.java
@@ -1,57 +1,34 @@
 package faang.school.godbless.sprint_3.matrix_reverse;
 
-import java.util.Arrays;
-
 public class MatrixReverser {
-    public static int[][] transformMatrix(int[][] matrix, MatrixTransformer transformer) {
-        Coordinates coordinates = transformer.change(2, 1);
-        dataValidation(coordinates, matrix);
-        int size = coordinates.getY() + 1;
-        int[][] result = new int[size][];
-        for (int i = 0; i < result.length; i++) {
-            int[] firstMassive = Arrays.copyOfRange(matrix[i], 0, coordinates.getX() + 1);
-            result[i] = firstMassive;
-        }
-        return result;
-    }
 
-    public static int[][] flipMatrix(int[][] matrix, FlipDirection flipDirection) {
-        if (matrix.length == 0) {
-            throw new IllegalArgumentException("Empty massive");
-        } else if (flipDirection == FlipDirection.HORIZONTAL) {
-            for (int i = 0; i < matrix.length; i++) {
-                int count = matrix[i].length - 1;
-                for (int j = 0; j < matrix[i].length / 2; j++) {
-                    int oldValue = matrix[i][j];
-                    matrix[i][j] = matrix[i][count];
-                    matrix[i][count] = oldValue;
-                    --count;
-                }
-            }
-            return matrix;
+    public static int[][] transformMatrix(int[][] massive, MatrixTransformer transformer) {
+        if (massive.length == 0) {
+            return new int[0][0];
         }
-        int count = matrix.length - 1;
-        for (int j = 0; j < matrix.length / 2; j++) {
-            int[] oldValue = matrix[j];
-            matrix[j] = matrix[count];
-            matrix[count] = oldValue;
-            --count;
-        }
-        return matrix;
-    }
+        int[][] newMassive = new int[massive.length][massive[0].length];
 
-    public static void dataValidation(Coordinates coordinates, int[][] matrix) {
-        int numberX = coordinates.getX();
-        int numberY = coordinates.getY();
-        if (matrix.length == 0) {
-            throw new IllegalArgumentException("Empty massive");
-        } else if (numberY < 0 || numberY >= matrix.length) {
-            throw new IllegalArgumentException("Wrong Y value");
-        }
-        for (int[] massive : matrix) {
-            if (numberX < 0 || numberX >= massive.length) {
-                throw new IllegalArgumentException("Wrong X value");
+        for (int i = 0; i < massive.length; i++) {
+            for (int j = 0; j < massive[i].length; j++) {
+                Coordinates coordinates = transformer.change(i, j);
+                newMassive[coordinates.getX()][coordinates.getY()] = massive[i][j];
             }
         }
+        return newMassive;
+    }
+
+    public static int[][] flipMatrix(int[][] massive, FlipDirection flipDirection) {
+        if (massive.length == 0) {
+            return new int[0][0];
+        }
+        int[][] reversedMatrix = new int[massive.length][massive[0].length];
+        if (flipDirection == FlipDirection.HORIZONTAL) {
+            MatrixTransformer matrixTransformer = (x, y) -> new Coordinates(x, massive[0].length - y - 1);
+            reversedMatrix = transformMatrix(massive, matrixTransformer);
+        } else if (flipDirection == FlipDirection.VERTICAL) {
+            MatrixTransformer matrixTransformer = (x, y) -> new Coordinates(massive.length - 1 - x, y);
+            reversedMatrix = transformMatrix(massive, matrixTransformer);
+        }
+        return reversedMatrix;
     }
 }
--- src/test/java/faang/school/godbless/sprint_3/matrix_reverse/MatrixReverserTest.java
+++ src/test/java/faang/school/godbless/sprint_3/matrix_reverse/MatrixReverserTest.java
@@ -8,10 +8,13 @@ class MatrixReverserTest {
 
     @Test
     void transformMatrixTest() {
-        int[][] twoDimensionalArray = {{1, 2, 3, 4, 5}, {1, 2, 3, 4, 5}, {1, 2, 3, 4, 5}};
-        int[][] expected = {{1, 2, 3}, {1, 2, 3}};
-        int[][] result = MatrixReverser.transformMatrix(twoDimensionalArray, (Coordinates::new));
+        int[][] twoDimensionalArray = {{1, 2, 2, 2, 2}, {2, 3, 3, 3, 3}, {3, 4, 4, 4, 4}};
+        int[][] expected = {{3, 4, 4, 4, 4}, {2, 3, 3, 3, 3}, {1, 2, 2, 2, 2}};
+        int[][] emptyArray = new int[0][0];
+        int[][] emptyResult = MatrixReverser.transformMatrix(emptyArray, (x, y) -> new Coordinates(twoDimensionalArray.length - 1 - x, y));
+        int[][] result = MatrixReverser.transformMatrix(twoDimensionalArray, (x, y) -> new Coordinates(twoDimensionalArray.length - 1 - x, y));
 
+        assertArrayEquals(emptyArray, emptyResult);
         assertNotEquals(0, result.length);
         assertArrayEquals(expected, result);
     }
@@ -21,28 +24,16 @@ class MatrixReverserTest {
         int[][] firstDimensionalArray = {{1, 2, 3, 4, 5}, {2, 3, 4, 5, 6}, {3, 4, 5, 6, 7}};
         int[][] firstExpected = {{5, 4, 3, 2, 1}, {6, 5, 4, 3, 2}, {7, 6, 5, 4, 3}};
         int[][] firstResult = MatrixReverser.flipMatrix(firstDimensionalArray, FlipDirection.HORIZONTAL);
-
-        assertArrayEquals(firstExpected, firstResult);
-        assertEquals(firstExpected[1][4], firstResult[1][4]);
-
         int[][] secondDimensionalArray = {{1, 1, 1, 1, 1}, {2, 2, 2, 2, 2}, {3, 3, 3, 3, 3}};
         int[][] secondExpected = {{3, 3, 3, 3, 3}, {2, 2, 2, 2, 2}, {1, 1, 1, 1, 1}};
         int[][] secondResult = MatrixReverser.flipMatrix(secondDimensionalArray, FlipDirection.VERTICAL);
+        int[][] emptyArray = new int[0][0];
+        int[][] emptyResult = MatrixReverser.flipMatrix(emptyArray, FlipDirection.HORIZONTAL);
 
+        assertArrayEquals(emptyArray, emptyResult);
+        assertArrayEquals(firstExpected, firstResult);
+        assertEquals(firstExpected[1][4], firstResult[1][4]);
         assertArrayEquals(secondExpected, secondResult);
         assertEquals(secondExpected[1][2], secondResult[1][2]);
-
-        int[][] emptyArray = new int[0][];
-
-        assertThrows(IllegalArgumentException.class, () -> MatrixReverser.flipMatrix(emptyArray, FlipDirection.HORIZONTAL));
-    }
-
-    @Test
-    void transformMatrixThrowsArrayIndexOfBoundException() {
-        int[][] wrongDimensionArray = new int[][]{{1}, {1, 2, 3, 4, 5}, {1, 2, 3, 4, 5}};
-        int[][] emptyMassive = new int[0][];
-
-        assertThrows(IllegalArgumentException.class, () -> MatrixReverser.transformMatrix(emptyMassive, (Coordinates::new)));
-        assertThrows(IllegalArgumentException.class, () -> MatrixReverser.transformMatrix(wrongDimensionArray, (Coordinates::new)));
     }
 }
