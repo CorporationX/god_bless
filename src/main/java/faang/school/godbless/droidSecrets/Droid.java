eaecdd5e254796b7e42905836d82688bfd9cba20
Julia Koroleva <youlka25@yandex.ru>
2023-06-23 22:15:26 +0300

    BC-1267 Секреты дроидов

---
+++ src/main/java/faang/school/godbless/droidSecrets/Droid.java
@@ -0,0 +1,40 @@
+package faang.school.godbless.droidSecrets;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class Droid {
+    public String sendEncryptedMessage(String message, Integer key) {
+        checkArgs(message, key);
+        DroidMessageEncryptor encryptor = (m, k) -> {
+            StringBuilder result = new StringBuilder();
+            for (int i = 0; i < m.length(); i++) {
+                result.append((char) (m.charAt(i) + k));
+            }
+            return result.toString();
+        };
+
+        return encryptor.encrypt(message, key);
+    }
+
+    public String receiveEncryptedMessage(String encryptedMessage, Integer key) {
+        checkArgs(encryptedMessage, key);
+        DroidMessageEncryptor decoder = (m, k) -> {
+            StringBuilder result = new StringBuilder();
+            for (int i = 0; i < m.length(); i++) {
+                result.append((char) (m.charAt(i) - k));
+            }
+            return result.toString();
+        };
+
+        return decoder.encrypt(encryptedMessage, key);
+    }
+
+    private static void checkArgs(String message, Integer key) {
+        if (message.isEmpty() || key < 1) {
+            throw new IllegalArgumentException("Переданы неверные данные");
+        }
+    }
+}
