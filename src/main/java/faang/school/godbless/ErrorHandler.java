b29cb9b61cc31e4a6d54bf4702a62d6786bf83ed
Matvey <matveynikishaev@gmail.com>
2023-06-23 18:38:04 +0300

    Exceptions handler

---
+++ src/main/java/faang/school/godbless/ErrorHandler.java
@@ -0,0 +1,13 @@
+package faang.school.godbless;
+
+import java.util.function.Supplier;
+
+public class ErrorHandler {
+    public static <T> T withErrorHandling(Supplier<T> action, ExceptionHandler<T> handler) {
+        try {
+            return action.get();
+        } catch (Exception e) {
+            return handler.handle(e);
+        }
+    }
+}
