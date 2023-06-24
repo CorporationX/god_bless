bec9023ad55869c4b53d582a6b8c8ffd01f19dc8
Adam-D-Beno <wrofdt@yandex.ru>
2023-06-24 23:32:47 +0300

    Solved BC-1371. Фильтруем спам

--- src/main/java/faang/school/godbless/Application.java
+++ src/main/java/faang/school/godbless/Application.java
@@ -5,6 +5,20 @@ import java.util.Set;
 
 public class Application {
     public static void main(String... args) {
+        MessageProcessor messageProcessor = new MessageProcessor();
 
+        MessageFilter spamFilter = message -> !message.toLowerCase().contains("спам".toLowerCase());
+        MessageFilter lengthFilter = message -> message.length() > 10;
+        MessageFilter emojiFilter = message -> !message.contains("смайл");
+
+        List<MessageFilter> filters = List.of(spamFilter, lengthFilter, emojiFilter);
+
+        String[] messages = {"Привет!", "Это спам!", "Как дела? смайл", "Длинное сообщение без спама и эмодзи"};
+
+        for (String message : messages) {
+            List<Boolean> isFiltered = messageProcessor.processMassage(message, filters);
+            System.out.println("Сообщение: " + message + " | Пропущено: " + isFiltered);
+//        }
+        }
     }
 }
