600159d0219699d21ab21f5bcee735f29fd8384a
Adam-D-Beno <wrofdt@yandex.ru>
2023-06-24 16:42:00 +0300

    Solved BC-1288. Google Переводчик под нашим контролем…

--- src/main/java/faang/school/godbless/Application.java
+++ src/main/java/faang/school/godbless/Application.java
@@ -1,10 +1,17 @@
 package faang.school.godbless;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.Set;
+import java.util.function.BiConsumer;
 
 public class Application {
     public static void main(String... args) {
-
+        DictionaryProcessor dictionaryProcessor = new DictionaryProcessor();
+        Map<String, String> dictionary = new HashMap<>();
+        BiConsumer<String, String> addWordToDictionary = dictionary::put;
+        dictionaryProcessor.processWord("Салам", "Salam", addWordToDictionary);
+        System.out.printf("Словарь " + dictionary);
     }
 }
