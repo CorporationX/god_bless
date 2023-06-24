07d6de53c7f395c74e56cbcbbbb86919747d6a1d
Andrey <khalyavinandrey@gmail.com>
2023-06-24 11:37:56 +0500

    email processor class

---
+++ src/main/java/faang/school/godbless/EmailProcessor.java
@@ -0,0 +1,21 @@
+package faang.school.godbless;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.function.Consumer;
+import java.util.function.Function;
+import java.util.function.Predicate;
+
+public class EmailProcessor {
+    public void processEmails(List<Email> emails, Predicate<Email> predicate,
+                              Consumer<Email> consumer, Function<Email, String> function) {
+        for (Email email : emails) {
+            String upperCaseSubject = function.apply(email);
+            email.setSubject(upperCaseSubject);
+
+            if (predicate.test(email)) {
+                consumer.accept(email);
+            }
+        }
+    }
+}
