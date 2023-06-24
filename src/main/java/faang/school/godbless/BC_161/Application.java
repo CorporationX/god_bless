5969763a392e42ed094865c4a9f19610eb7ba6c7
Adam-D-Beno <wrofdt@yandex.ru>
2023-06-24 07:58:39 +0300

    fix changes

--- src/main/java/faang/school/godbless/BC_161/Application.java
+++ src/main/java/faang/school/godbless/BC_161/Application.java
@@ -5,10 +5,6 @@ import java.util.Set;
 
 public class Application {
     public static void main(String... args) {
-        Set<String> setActivities = Set.of("str1", "activity88", "activity16");
-        List<User> userList = List.of(new User(1, "Adam", 35, Set.of("str1", "str2", "str3")),
-                                      new User(2, "Adam2", 35, Set.of("str0", "str5", "str6"))
-        );
-        System.out.println(User.findHobbyLovers(userList, setActivities));
+
     }
 }
