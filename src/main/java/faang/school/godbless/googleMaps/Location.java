ec93a036b75c2216ac12ee6ea473cd535a60e134
ipavelbel <ilpavel94@gmail.com>
2023-06-24 10:29:33 +0200

    Sprint 3 Task Google Maps implementation

---
+++ src/main/java/faang/school/godbless/googleMaps/Location.java
@@ -0,0 +1,12 @@
+package faang.school.godbless.googleMaps;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@AllArgsConstructor
+@Getter
+public class Location {
+    private String name;
+    private double latitude;
+    private double longitude;
+}
