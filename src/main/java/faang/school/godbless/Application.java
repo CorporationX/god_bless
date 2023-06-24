c3619da3a0616502ab807c198b604ac4503ed3f0
Andrey <khalyavinandrey@gmail.com>
2023-06-24 17:46:36 +0500

    testing inventory functions

--- src/main/java/faang/school/godbless/Application.java
+++ src/main/java/faang/school/godbless/Application.java
@@ -1,7 +1,26 @@
 package faang.school.godbless;
 
+import java.sql.SQLOutput;
+
 public class Application {
     public static void main(String... args) {
-        System.out.println("Hello World!");
+        Character frodo = new Character("Frodo");
+        Item ring = new Item("The One Ring", 1000);
+
+        InventoryManager manager = new InventoryManager();
+
+        // Добавляем предмет в инвентарь
+        manager.addItem(frodo, ring, (item) -> System.out.println(item.getName() + " was added to the inventory."));
+        System.out.println(frodo.getInventory());
+
+
+        // Удаляем предмет из инвентаря
+        manager.removeItem(frodo, (item) -> item.getName().equals("The One Ring"));
+        System.out.println(frodo.getInventory());
+
+
+        // Обновляем предмет в инвентаре
+        manager.updateItem(frodo, (item) -> item.getName().equals("The One Ring"), (item) -> new Item(item.getName(), item.getValue() * 2));
+        System.out.println(frodo.getInventory());
     }
 }
