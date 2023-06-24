60dfa750480aa6176a450ae718ff4817a857aa78
Andrey <khalyavinandrey@gmail.com>
2023-06-24 18:35:26 +0500

    testing error handling

--- src/main/java/faang/school/godbless/Application.java
+++ src/main/java/faang/school/godbless/Application.java
@@ -1,7 +1,16 @@
 package faang.school.godbless;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.function.Supplier;
+
 public class Application {
     public static void main(String... args) {
-        System.out.println("Hello World!");
+        List<Integer> list = List.of(3);
+
+        Supplier<Integer> supplier = () -> list.get(1); // иммитация exception
+        ExceptionHandler<Integer> exceptionHandler = exception -> list.get(0);
+
+        ExceptionHandler.withErrorHandling(supplier, exceptionHandler);
     }
 }
