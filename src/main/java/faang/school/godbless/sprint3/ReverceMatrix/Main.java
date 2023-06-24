5b1354731849e09827e5025183d7debb9816ae38
Alex <matcnev2001@mail.ru>
2023-06-24 14:00:52 +0300

    M4nk1n, flip matrix

--- src/main/java/faang/school/godbless/sprint3/ReverceMatrix/Main.java
+++ src/main/java/faang/school/godbless/sprint3/ReverceMatrix/Main.java
@@ -2,15 +2,6 @@ package faang.school.godbless.sprint3.ReverceMatrix;
 
 public class Main {
     public static void main(String[] args) {
-        FlipDirection<int[]> reverse = (line) -> {
-            faang.school.godbless.Sprint2.task5.Example.reverse(line);
-            return line;
-        };
-        StringBuilder builder = new StringBuilder();
-        int[][] matrix1 = new int[][]{{1, 2},
-                {3, 4}};
-        int[][] newMatrix = flipMatrix(matrix1,reverse);
-        System.out.println(newMatrix);
     }
 
     static int[][] flipMatrix(int[][] matrix, FlipDirection flipDirection) {
