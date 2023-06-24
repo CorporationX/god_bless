2499269031c7245ab001011456ef98d6707fa75a
Adam-D-Beno <wrofdt@yandex.ru>
2023-06-24 08:29:20 +0300

    Solved BC-1286. Миллиончики Uber

---
+++ src/main/java/faang/school/godbless/FareCalculator.java
@@ -0,0 +1,9 @@
+package faang.school.godbless;
+
+import java.util.function.BiFunction;
+
+public class FareCalculator {
+    public double calculateFare(double distance, double time, BiFunction<Double, Double, Double> calculate) {
+        return calculate.apply(distance, time);
+    }
+}
