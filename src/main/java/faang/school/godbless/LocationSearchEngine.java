c114dae2214ce3aa8bf6acd099adf802adcaa254
Andrey <khalyavinandrey@gmail.com>
2023-06-24 19:15:19 +0500

    location search engine class

---
+++ src/main/java/faang/school/godbless/LocationSearchEngine.java
@@ -0,0 +1,37 @@
+package faang.school.godbless;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.function.Consumer;
+import java.util.function.Function;
+import java.util.function.Predicate;
+
+public class LocationSearchEngine {
+    public List<Location> filterLocations(List<Location> locations, Predicate<Location> filter) {
+        List<Location> filteredLocations = new ArrayList<>();
+
+        for (Location location : locations) {
+            if (filter.test(location)) {
+                filteredLocations.add(location);
+            }
+        }
+
+        return filteredLocations;
+    }
+
+    public void processLocations(List<Location> locations, Consumer<Location> consumer) {
+        for (Location location : locations) {
+            consumer.accept(location);
+        }
+    }
+
+    public List<Double> calculateDistances(List<Location> locations, Function<Location, Double> function) {
+        List<Double> distances = new ArrayList<>();
+
+        for (Location location : locations) {
+            distances.add(function.apply(location));
+        }
+
+        return distances;
+    }
+}
