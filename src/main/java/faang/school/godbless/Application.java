05cc1bbce55a369c902ad704a55d88d856abcfbb
Andrey <khalyavinandrey@gmail.com>
2023-06-24 12:27:37 +0500

    test message filtering

--- src/main/java/faang/school/godbless/Application.java
+++ src/main/java/faang/school/godbless/Application.java
@@ -1,7 +1,23 @@
 package faang.school.godbless;
 
+import java.util.Arrays;
+import java.util.List;
+
 public class Application {
     public static void main(String... args) {
-        System.out.println("Hello World!");
+        MessageProcessor messageProcessor = new MessageProcessor();
+
+        MessageFilter spamFilter = message -> !message.toLowerCase().contains("spam"); // true - не содержит спам
+        MessageFilter lengthFilter = message -> message.length() > 10; // true - длина сообщений > 10
+        MessageFilter emojiFilter = message -> !message.contains("123"); // true - не содержит "123"
+
+        List<MessageFilter> filters = Arrays.asList(spamFilter, lengthFilter, emojiFilter);
+
+        String[] messages = {"Hello!", "It's spam!", "How are you? 123", "Long message without spam and emoji"};
+
+        for (String message : messages) {
+            boolean isFiltered = messageProcessor.processMessage(message, filters);
+            System.out.println("Сообщение: " + message + " | Пропущено: " + isFiltered);
+        }
     }
 }
