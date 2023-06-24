9f511fcd094b45eca5590ed7a2106ae2ec4fc76d
mo <nataleshen.ko@yandex.ru>
2023-06-24 09:58:16 +0300

    gmail-rich-filter done, Task6

---
+++ src/main/java/faang/school/godbless/sprint2/gmail_rich_filters/EmailProcessor.java
@@ -0,0 +1,12 @@
+package faang.school.godbless.sprint2.gmail_rich_filters;
+
+import java.util.List;
+import java.util.function.Consumer;
+import java.util.function.Function;
+import java.util.function.Predicate;
+
+public class EmailProcessor {
+    public void emailProcess(List<Email> emailList, Predicate<Email> predicate, Consumer<Email> consumer, Function<Email, String> function) {
+
+    }
+}
