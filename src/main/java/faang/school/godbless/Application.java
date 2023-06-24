ee737f5e54f23e946c43611fe4eb949d59393b46
Andrey <khalyavinandrey@gmail.com>
2023-06-24 11:44:55 +0500

    test email processoring

--- src/main/java/faang/school/godbless/Application.java
+++ src/main/java/faang/school/godbless/Application.java
@@ -1,7 +1,27 @@
 package faang.school.godbless;
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.function.Consumer;
+import java.util.function.Function;
+import java.util.function.Predicate;
+
 public class Application {
     public static void main(String... args) {
-        System.out.println("Hello World!");
+
+        EmailProcessor emailProcessor = new EmailProcessor();
+
+        List<Email> emails = Arrays.asList(
+                new Email("Письмо 1", "Текст письма 1", true),
+                new Email("Письмо 2", "Текст письма 2", true),
+                new Email("Спам", "Текст спама", false)
+        );
+
+        // Создание фильтров, обработчиков и преобразователей
+        Predicate<Email> importantFilter = Email::isImportant;
+        Consumer<Email> printEmail = email -> System.out.println("Обработано письмо: " + email.getSubject());
+        Function<Email, String> toUpperCase = email -> email.getSubject().toUpperCase();
+
+        emailProcessor.processEmails(emails, importantFilter, printEmail, toUpperCase);
     }
 }
