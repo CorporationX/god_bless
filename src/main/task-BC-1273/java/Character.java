74c81a9cd4f578d40e2b2ca062db58ab54d4d544
Alex Tarasov <tarasof.av@gmail.com>
2023-06-24 13:07:52 +0300

    task Lord of the Rings RPG

---
+++ src/main/task-BC-1273/java/Character.java
@@ -0,0 +1,12 @@
+import lombok.Data;
+import java.util.ArrayList;
+import java.util.List;
+@Data
+public class Character {
+    private String name;
+    private List<Item> inventory = new ArrayList<>();
+
+    public Character(String name) {
+        this.name = name;
+    }
+}
---
+++ src/main/task-BC-1273/java/InventoryManager.java
@@ -0,0 +1,24 @@
+import java.util.function.Consumer;
+import java.util.function.Function;
+import java.util.function.Predicate;
+
+public class InventoryManager {
+
+    public void addItem(Character character, Item item, Consumer<Item> consumer) {
+        character.getInventory().add(item);
+        consumer.accept(item);
+    }
+
+    public void removeItem(Character character, Predicate<Item> predicate) {
+        character.getInventory().removeIf(predicate);
+    }
+
+    public void updateItem(Character character, Predicate<Item> predicate, Function<Item, Item> function) {
+        for (int i = 0; i < character.getInventory().size(); i++) {
+            if (predicate.test(character.getInventory().get(i))) {
+                Item item1 = function.apply(character.getInventory().get(i));
+                character.getInventory().add(i, item1);
+            }
+        }
+    }
+}
