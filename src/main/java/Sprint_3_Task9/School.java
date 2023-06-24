5c95df9155006e8bfd61203e6cfd3fb80d5615c6
zazozhik <ya.940@mail.ru>
2023-06-24 14:56:47 +0300

    Реализовал условию задачи School

--- src/main/java/Sprint_3_Task9/School.java
+++ src/main/java/Sprint_3_Task9/School.java
@@ -4,19 +4,30 @@ import java.util.function.Function;
 
 public class School {
 
-    Double triangleArea(double a, double b, double c) throws IllegalArgumentException {
+    public static Double triangleArea(double a, double b, double c) throws IllegalArgumentException {
         Function<Double, Function<Double, Double>> sum = (x) -> y -> x + y;
         Function<Double, Function<Double, Double>> mul = (x) -> y -> x * y;
         Function<Double, Function<Double, Double>> sub = (x) -> y -> x - y;
         Function<Double, Function<Double, Double>> div = (x) -> y -> x / y;
         Function<Double, Double> sqrt = (x) -> Math.sqrt(x);
 
-        double pDiv2 = div.apply(sum.apply(sum.apply(a).apply(b)).apply(c)).apply(2.0);
+        double pDiv2 = div.apply(sum.apply(
+                sum.apply(a).apply(b)).apply(c)
+        ).apply(2.0);
 
         double pDiv2MinusA = sub.apply(pDiv2).apply(a);
         double pDiv2MinusB = sub.apply(pDiv2).apply(b);
         double pDiv2MinusC = sub.apply(pDiv2).apply(c);
 
-        return pDiv2MinusB;
+        return sqrt.apply(mul.apply(mul.apply
+                (mul.apply(pDiv2
+                ).apply(pDiv2MinusA)).apply(pDiv2MinusB)).apply(pDiv2MinusC));
+    }
+
+    public static void main(String[] args) {
+        double a = 3;
+        double b = 4;
+        double c = 5;
+        System.out.println(triangleArea(a,b,c));
     }
 }
