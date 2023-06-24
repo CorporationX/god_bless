868fe6a87445d362e6a57ba5318fa9d39a624c09
streetunit <sstreetunit@yandex.ru>
2023-06-24 22:30:32 +0300

    BC-1220 googleMaps. implemented classes: Location, LocationSearchEngine. Added test.

--- src/main/java/faang/school/godbless/googleMaps/Location.java
+++ src/main/java/faang/school/godbless/googleMaps/Location.java
@@ -1,7 +1,12 @@
 package faang.school.godbless.googleMaps;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
 public class Location {
     private String name;
-    private int latitude;
-    private int longitude;
+    private double latitude;
+    private double longitude;
 }
