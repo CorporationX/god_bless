6590c9d67857583b21bc6ac5796fb40cd4054e6f
Dobrovolsky Artem <artdobrov@mail.ru>
2023-06-24 21:35:06 +0300

    Добавлены класс CSVConverter и исполняемый класс для проверки

---
+++ src/main/java/faang/school/godbless/Lets/build/CSV/VectorJoiner.java
@@ -0,0 +1,9 @@
+package faang.school.godbless.Lets.build.CSV;
+
+import java.util.List;
+
+@FunctionalInterface
+public interface VectorJoiner<T> {
+    // Соединяет список элементов типа T в один элемент типа T
+    T join(List<T> vector);
+}
