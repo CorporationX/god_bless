4934aeb1e0c7d9d76f4f72670cb2cbe9b7720160
Eggur <eggurin2033@gmail.com>
2023-06-24 17:25:32 +0300

    Добавил тесты, убрал метод main из класса

--- src/main/java/Calculator/Main.java
+++ src/main/java/Calculator/Main.java
@@ -23,10 +23,4 @@ public class Main {
     public static int sum(List<Integer> nums) {
         return calculate(nums, (a, b) -> a + b);
     }
-
-    public static void main(String[] args) {
-        List<Integer> numbers = List.of(1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
-
-        System.out.println(sum(numbers));
-    }
 }
---
+++ src/test/java/Calculator/CalculatorTest.java
@@ -0,0 +1,60 @@
+package Calculator;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+public class CalculatorTest {
+    Calculator calculator;
+    List<Integer> numbers;
+
+    @BeforeEach
+    void init() {
+        numbers = List.of(1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
+        calculator = (first, second) -> first + second;
+    }
+
+    @Test
+    @DisplayName("method test calculateTest")
+    void calculateTest() {
+
+        int result = Main.calculate(numbers, calculator);
+        int expected = 55;
+
+        assertEquals(expected, result);
+    }
+
+    @Test
+    @DisplayName("empty list test")
+    void calculateThrowIAETest() {
+        List<Integer> arr = List.of();
+
+        assertThrows(IllegalArgumentException.class, () -> Main.calculate(arr, calculator));
+    }
+
+    @Test
+    @DisplayName("test method productTest")
+    void productTest() {
+
+        int result = Main.product(numbers);
+        int expected = 3628800;
+
+        assertEquals(expected, result);
+    }
+
+    @Test
+    @DisplayName("test method sumTest")
+    void sumTest() {
+
+        int result = Main.sum(numbers);
+        int expected = 55;
+
+        assertEquals(expected, result);
+    }
+}
