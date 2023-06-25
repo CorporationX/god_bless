73ea6fe0e4c772f5afa65a8893636711b6497ea3
Adam-D-Beno <wrofdt@yandex.ru>
2023-06-25 12:07:31 +0300

    Solved BC-1392. Калькулятор

--- src/main/java/faang/school/godbless/Application.java
+++ src/main/java/faang/school/godbless/Application.java
@@ -4,21 +4,10 @@ import java.util.List;
 import java.util.Set;
 
 public class Application {
-    public static void main(String... args) {
-        MessageProcessor messageProcessor = new MessageProcessor();
+    public static void main(String[] args) {
+        List<Integer> nums = List.of(1, 2, 3, 4, 5);
+        System.out.println("sum " + GoogleCalculator.sum(nums));
+        System.out.println("product " + GoogleCalculator.product(nums));
 
-        MessageFilter spamFilter = message -> !message.toLowerCase().contains("спам".toLowerCase());
-        MessageFilter lengthFilter = message -> message.length() > 10;
-        MessageFilter emojiFilter = message -> !message.contains("смайл");
-
-        List<MessageFilter> filters = List.of(spamFilter, lengthFilter, emojiFilter);
-
-        String[] messages = {"Привет!", "Это спам!", "Как дела? смайл", "Длинное сообщение без спама и эмодзи"};
-
-        for (String message : messages) {
-            List<Boolean> isFiltered = messageProcessor.processMassage(message, filters);
-            System.out.println("Сообщение: " + message + " | Пропущено: " + isFiltered);
-//        }
-        }
     }
 }
