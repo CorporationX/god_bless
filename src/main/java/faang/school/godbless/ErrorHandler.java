d7357182081624364f22e700c04f671ec8d9ab62
Stepan Palkov <step.palkov@gmail.com>
2023-06-25 03:22:32 +0300

    fixed task

--- src/main/java/faang/school/godbless/ErrorHandler.java
+++ src/main/java/faang/school/godbless/ErrorHandler.java
@@ -1,11 +1,12 @@
 package faang.school.godbless;
 
 import java.util.function.Function;
+import java.util.function.Supplier;
 
 public class ErrorHandler {
-    public static <T, P> T withErrorHandling(P parameter, Function<P, T> function, ExceptionHandler<T> exceptionHandler){
+    public static <T> T withErrorHandling(Supplier<T> action, ExceptionHandler<T> exceptionHandler){
         try{
-            return function.apply(parameter);
+            return action.get();
         } catch (Exception e){
             return exceptionHandler.handleException(e);
         }
