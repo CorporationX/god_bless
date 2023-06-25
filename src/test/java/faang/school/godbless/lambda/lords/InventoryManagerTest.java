6874f645ffdd85847a007a8f63aa276d068ce312
Dmitry Sorokin <dm.sorokin21@gmail.com>
2023-06-25 17:45:37 +0300

    with failed test

---
+++ src/test/java/faang/school/godbless/lambda/lords/InventoryManagerTest.java
@@ -0,0 +1,17 @@
+package faang.school.godbless.lambda.lords;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class InventoryManagerTest {
+    private Character frodo = new Character("Frodo");
+    private Item ring = new Item("The One Ring", 1000);
+    private Item panties = new Item("SuperMan", 250);
+    InventoryManager manager = new InventoryManager();
+    @Test
+    void addItemTestNotNull() {
+        manager.addItem(frodo, ring, item -> System.out.println(item.getName() + " was added to the inventory."));
+        assertFalse(frodo.getItemList().isEmpty());
+    }
+}
