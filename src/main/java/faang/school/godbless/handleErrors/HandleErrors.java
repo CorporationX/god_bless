26747431172da8127d8a0493d4bf5b72460a0e6e
M1khail <mishalion888@gmail.com>
2023-06-25 19:54:41 +0200

    handler errors (fixed mini errors)

--- src/main/java/faang/school/godbless/handleErrors/HandleErrors.java
+++ src/main/java/faang/school/godbless/handleErrors/HandleErrors.java
@@ -1,11 +1,10 @@
 package faang.school.godbless.handleErrors;
 
-import ch.qos.logback.core.net.SocketConnector;
 
 import java.util.function.Supplier;
 
 public class HandleErrors {
-    public static <T> T withErrorHandling(Supplier<T> activity, ExceptionHandler<T> exceptionHandler) {
+    public static <T> T withErrorHandling(Supplier<T> activity, Handler<T> exceptionHandler) {
         try {
             return activity.get();
         } catch (Exception e) {
