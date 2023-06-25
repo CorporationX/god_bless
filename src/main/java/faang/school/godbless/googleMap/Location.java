95cf469499dc7778bca9813f6ae7004eb39d858e
Abramov Nikita <mr.abramovn@gmail.com>
2023-06-26 01:04:44 +0300

    add Location class add LocationSearchEngine class

---
+++ src/main/java/faang/school/godbless/googleMap/Location.java
@@ -0,0 +1,12 @@
+package faang.school.godbless.googleMap;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class Location {
+    String name;
+    double latitude;
+    double longitude;
+}
