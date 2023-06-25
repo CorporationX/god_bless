6d4b48cd38258c65045a7ecd88ee7d2ea4447d4d
Adam-D-Beno <wrofdt@yandex.ru>
2023-06-25 12:07:31 +0300

    Solved BC-1392. Калькулятор

--- src/main/java/faang/school/godbless/Application.java
+++ src/main/java/faang/school/godbless/Application.java
@@ -4,7 +4,10 @@ import java.util.List;
 import java.util.Set;
 
 public class Application {
-    public static void main(String... args) {
+    public static void main(String[] args) {
+        List<Integer> nums = List.of(1, 2, 3, 4, 5);
+        System.out.println("sum " + GoogleCalculator.sum(nums));
+        System.out.println("product " + GoogleCalculator.product(nums));
 
     }
 }
