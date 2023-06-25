d477b809c8339017e1af3f3dfd565ae9afb5592e
Pavel <pavlik1123.pg@gmail.com>
2023-06-25 21:31:41 +0700

    sdf

--- src/main/java/faang/school/godbless/StreamAPI_2/Main.java
+++ src/main/java/faang/school/godbless/StreamAPI_2/Main.java
@@ -1,5 +1,27 @@
 package faang.school.godbless.StreamAPI_2;
 
-public class Main {
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.List;
 
+public class Main {
+    public static void main(String[] args) {
+        List<Integer> list = List.of(2, 23, 5, 45, 34, 2, 5, 3, 4);
+        list = new HashSet<>(list).stream().toList();
+        int key = 7;
+
+        List<int[]> list1 = new ArrayList<>();
+        for (int i = 0; i < list.size(); i++) {
+            for (int j = i + 1; j < list.size(); j++) {
+                int i1 = list.get(i);
+                int j1 = list.get(j);
+                if (i1 + j1 == key) {
+                    list1.add(new int[]{i1, j1});
+                }
+            }
+        }
+
+        list1.forEach(ints -> System.out.println(Arrays.toString(ints)));
+    }
 }
--- src/main/java/faang/school/godbless/StreamAPI_2/Tron.java
+++ src/main/java/faang/school/godbless/StreamAPI_2/Tron.java
@@ -1,10 +1,28 @@
 package faang.school.godbless.StreamAPI_2;
 
+import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 public class Tron {
 
-    protected static void getUniquePairsOfNumbers(List<Integer> list, int num) {
+    private final List<Integer> list = List.of(2, 3, 7, 4);
 
+    private final int num = 5;
+
+    protected static void getUniquePairsOfNumbers(List<Integer> list, int num) {
+        Set<Integer> set = list.stream().collect(Collectors.toSet());
+        Set<int[]> arrSet = new HashSet<>();
+        int nums;
+        for (Integer integer : set) {
+            nums = integer;
+            int[] ints = new int[2];
+            ints[0] = nums;
+            if (ints[0] != 0 && ints[1] != 0) {
+                arrSet.add(ints);
+            }
+        }
     }
 }
