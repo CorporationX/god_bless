f27abf5aef896d4b8ac10b67896ad655a4910209
DiegaLoL <alex.eey@mail.ru>
2023-06-26 01:26:03 +0900

    bc-1451 MailSender

---
+++ src/main/java/WorkInMicrosoft/MailSender.java
@@ -0,0 +1,19 @@
+package WorkInMicrosoft;
+
+public class MailSender {
+    public static void main(String[] args) {
+        Thread firstThread = new Thread(new SenderRunnable(1,200));
+        Thread secondThread = new Thread(new SenderRunnable(201,400));
+        Thread thirdThread = new Thread(new SenderRunnable(401,600));
+        Thread forthThread = new Thread(new SenderRunnable(601,800));
+        Thread fifthThread = new Thread(new SenderRunnable(801,1000));
+
+        firstThread.start();
+        secondThread.start();
+        thirdThread.start();
+        forthThread.start();
+        fifthThread.start();
+
+
+    }
+}
