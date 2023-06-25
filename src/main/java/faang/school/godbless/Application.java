e139100364e850dcdd459644b64ff4f86b01bb84
Andrey <khalyavinandrey@gmail.com>
2023-06-25 11:29:00 +0500

    testing flipping matrix

--- src/main/java/faang/school/godbless/Application.java
+++ src/main/java/faang/school/godbless/Application.java
@@ -1,7 +1,39 @@
 package faang.school.godbless;
 
+import java.util.Arrays;
+import java.util.function.Consumer;
+
 public class Application {
     public static void main(String... args) {
-        System.out.println("Hello World!");
+        System.out.println(Arrays.deepToString(flipMatrix(new int[][]{{5, 7, 3}, {7, 0, 1}, {8, 1, 2}}, FlipDirection.VERTICAL)));
+    }
+
+    static int[][] transformMatrix(int[][] matrix, MatrixTransformer transformer) {
+        if (matrix.length == 0) {
+            return new int[0][0];
+        }
+
+        int[][] transformedMatrix = new int[matrix.length][matrix[0].length];
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[0].length; j++) {
+                Coordinates coordinates = transformer.transform(i, j);
+                transformedMatrix[coordinates.getI()][coordinates.getJ()] = matrix[i][j];
+            }
+        }
+        return transformedMatrix;
+    }
+
+    static int[][] flipMatrix(int[][] matrix, FlipDirection flipDirection) {
+        if (flipDirection == FlipDirection.HORIZONTAL) {
+            return transformMatrix(matrix, (i, j) -> {
+                j = matrix[0].length - 1 - j;
+                return new Coordinates(i, j);
+            });
+        } else {
+            return transformMatrix(matrix, (i, j) -> {
+                i = matrix.length - 1 - i;
+                return new Coordinates(i, j);
+            });
+        }
     }
 }
