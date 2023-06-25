89d83a6fdb2c356acf36c9fb5fbec19b436d97de
Irina Vereshchagina <irankamoya@gmail.com>
2023-06-25 23:05:24 +0300

    Задача - Вы работаете в Microsoft!

---
+++ src/main/java/Work_in_Microsoft/SenderRunnable.java
@@ -0,0 +1,18 @@
+package Work_in_Microsoft;
+
+public class SenderRunnable implements Runnable {
+    private int startIndex;
+    private int endIndex;
+
+    public SenderRunnable(int startIndex, int endIndex) {
+        this.startIndex = startIndex;
+        this.endIndex = endIndex;
+    }
+
+    @Override
+    public void run() {
+        for (int i = startIndex; i <= endIndex; i++) {
+            System.out.println("Sending message " + i);
+        }
+    }
+}
