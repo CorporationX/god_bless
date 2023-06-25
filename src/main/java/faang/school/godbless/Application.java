bbad95ba9a4344741a5d112a28c2fcd2f242e15f
Adam-D-Beno <wrofdt@yandex.ru>
2023-06-25 23:11:41 +0300

    Solved BC-1423. Lord of the Rings RPG

--- src/main/java/faang/school/godbless/Application.java
+++ src/main/java/faang/school/godbless/Application.java
@@ -4,7 +4,19 @@ import java.util.List;
 import java.util.Set;
 
 public class Application {
-    public static void main(String... args) {
+    public static void main(String[] args) {
+        Character frodo = new Character("frodo");
+        Item ring = new Item("The One Ring", 1000);
 
+        InventoryManager manager = new InventoryManager();
+
+        manager.addItem(frodo, ring, item -> System.out.println(item.getName() + " was added to the inventory"));
+
+        // manager.removeItem(frodo, item -> item.getName().equals("The One Ring"));
+
+        manager.updateItem(frodo, item -> item.getName().equals("The One Ring"),
+                item -> new Item(item.getName(), item.getValue() * 2));
+
+        System.out.println(frodo.getInventory().toString());
     }
 }
