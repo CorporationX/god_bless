387a47e5634ba139a0887901b562c82383afca9a
Adam-D-Beno <wrofdt@yandex.ru>
2023-06-26 00:29:50 +0300

    Solved BC-1487. Обрабатываем ошибки красиво

--- src/main/java/faang/school/godbless/Application.java
+++ src/main/java/faang/school/godbless/Application.java
@@ -2,9 +2,20 @@ package faang.school.godbless;
 
 import java.util.List;
 import java.util.Set;
+import java.util.function.Supplier;
 
 public class Application {
     public static void main(String... args) {
+       RemoteService remoteService = new RemoteService();
+       String res = withErrorHandling(() -> remoteService.call(""), Throwable::getMessage);
+       System.out.println(res);
+    }
 
+    static<T> T withErrorHandling(Supplier<T> action, ExceptionHandler<T> onError) {
+        try {
+            return  action.get();
+        } catch (Exception ex) {
+            return onError.handle(ex);
+        }
     }
 }
