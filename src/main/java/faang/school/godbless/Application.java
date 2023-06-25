ba03f7671d409816ff667350cfa1669788ea68de
Adil Ilchubaev <adil.200191@gmail.com>
2023-06-25 12:01:47 +0600

    Паараметризированыне тесты и MethodSource попробовал использовал в юнит тестах и получилось очень даже

--- src/main/java/faang/school/godbless/Application.java
+++ src/main/java/faang/school/godbless/Application.java
@@ -4,14 +4,13 @@ import java.util.List;
 public class Application {
 
     public static int calculate(List<Integer> nums, Calculator<Integer> calculator) throws IllegalArgumentException {
-        if (nums == null || calculator == null)
-            throw new IllegalArgumentException("Invalid input");
+        if (nums.isEmpty())
+            throw new IllegalArgumentException("The list cannot be empty.");
 
         int result = nums.get(0);
         for (int i = 1; i < nums.size(); i++) {
             result = calculator.calculate(result, nums.get(i));
         }
-
         return result;
     }
 
@@ -20,7 +19,7 @@ public class Application {
     }
 
     public static int sum(List<Integer> nums) {
-        return calculate(nums, (a, b) -> a + b);
+        return calculate(nums, Integer::sum);
     }
 
     public static void main(String... args) {
--- src/test/java/faang/school/godbless/CalculatorTest.java
+++ src/test/java/faang/school/godbless/CalculatorTest.java
@@ -1,16 +1,43 @@
 package faang.school.godbless;
 
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
-
 import java.util.List;
+import java.util.stream.Stream;
 
+import static faang.school.godbless.Application.product;
+import static faang.school.godbless.Application.sum;
 import static org.junit.jupiter.api.Assertions.*;
 
 class CalculatorTest {
     @ParameterizedTest
-    @MethodSource("providedTestData")
-    public void calculate_ShouldApplyOpertaionToAllElements(List<Integer> nums, Calculator<Integer> calculator, int expected){
-        int result = Calculator.calculate(nums, calculator);
+    @MethodSource("productTestDataProvider")
+    public void productTest(List<Integer> nums, int expectedProduct){
+        int actualProduct = product(nums);
+        assertEquals(expectedProduct, actualProduct);
+    }
+
+    @ParameterizedTest
+    @MethodSource("sumTestDataProvider")
+    public void sumTest(List<Integer> nums, int expectedSum){
+        int actualSum = sum(nums);
+        assertEquals(expectedSum, actualSum);
+    }
+
+
+    static Stream<Arguments> productTestDataProvider() {
+        return Stream.of(
+                Arguments.of(List.of(1, 2, 3, 4), 24),
+                Arguments.of(List.of(5, 6, 7), 210),
+                Arguments.of(List.of(10, 2, 1), 20)
+        );
+    }
+    static Stream<Arguments> sumTestDataProvider() {
+        return Stream.of(
+                Arguments.of(List.of(1, 2, 3, 4), 10),
+                Arguments.of(List.of(5, 6, 7), 18),
+                Arguments.of(List.of(10, 2, 1), 13)
+        );
     }
 }
