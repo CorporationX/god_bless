14ec19838cb6eddae1d52db617e83d0bd2e08eda
Dmitry Sorokin <dm.sorokin21@gmail.com>
2023-06-25 15:32:00 +0300

    resolved task uber

---
+++ src/main/java/faang/school/godbless/lambda/uber/FareCalculator.java
@@ -0,0 +1,9 @@
+package faang.school.godbless.lambda.uber;
+
+import java.util.function.BiFunction;
+
+public class FareCalculator {
+    public double calculateFare(double distance, double time, BiFunction<Double, Double, Double> biFunction) {
+        return biFunction.apply(distance, time);
+    }
+}
