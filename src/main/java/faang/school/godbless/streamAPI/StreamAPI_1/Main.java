8c7861e0b3c358a6ef5f77eb9b3eec1308fe6fd1
Pavel <pavlik1123.pg@gmail.com>
2023-06-25 14:26:36 +0700

    df

--- src/main/java/faang/school/godbless/streamAPI/StreamAPI_1/Main.java
+++ src/main/java/faang/school/godbless/streamAPI/StreamAPI_1/Main.java
@@ -3,9 +3,5 @@ package faang.school.godbless.streamAPI.StreamAPI_1;
 public class Main {
     public static void main(String[] args) {
 
-        String string = "Ракета";
-
-        System.out.println(string.length());
-        System.out.println(string);
     }
 }
