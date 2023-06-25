804d3d7de47dbab043c301601d64a87b4dd059e8
Alex Tarasov <tarasof.av@gmail.com>
2023-06-25 18:39:03 +0300

    task Google Maps

---
+++ src/main/task-BC/java/Location.java
@@ -0,0 +1,14 @@
+import lombok.Data;
+
+@Data
+public class Location {
+    private String name;
+    private double latitude;
+    private double longitude;
+
+    public Location(String name, double latitude, double longitude) {
+        this.name = name;
+        this.latitude = latitude;
+        this.longitude = longitude;
+    }
+}
---
+++ src/main/task-BC/java/LocationSearchEngine.java
@@ -0,0 +1,18 @@
+import java.util.List;
+import java.util.function.Consumer;
+import java.util.function.Function;
+import java.util.function.Predicate;
+
+public class LocationSearchEngine {
+    public List<Location> filterLocations(List<Location> locations, Predicate<Location> predicate) {
+        return locations.stream().filter(predicate).toList();
+    }
+
+    public void processLocations(List<Location> locations, Consumer<Location> consumer){
+        locations.stream().forEach(consumer);
+    }
+
+    public List<Double> calculateDistances(List<Location> locations, Function<Location, Double> function){
+        return locations.stream().map(function).toList();
+    }
+}
