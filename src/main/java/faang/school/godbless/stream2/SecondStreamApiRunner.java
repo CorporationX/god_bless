9d4b2611e904270f85413a724de784bbb02c946a
Никита <nikita.armeec.88@mail.ru>
2023-06-25 16:56:56 +0300

    Создан класс SecondStreamApiRunner со статическими методами, использующие StreamAPI

---
+++ src/main/java/faang/school/godbless/stream2/SecondStreamApiRunner.java
@@ -0,0 +1,80 @@
+package faang.school.godbless.stream2;
+
+import java.util.Comparator;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+public class SecondStreamApiRunner {
+
+    public static Set<Integer> searchPairOfDigits(List<Integer> numbers, int value) {
+        return numbers.stream().filter(num -> numbers.contains(value - num))
+                .collect(Collectors.toSet());
+    }
+
+    public static List<String> getCountryCapitals(Map<String, String> map) {
+        return map.entrySet().stream()
+                .sorted(Map.Entry.comparingByKey())
+                .map(Map.Entry::getValue)
+                .toList();
+    }
+
+    public static List<String> searchLinesWithChar(List<String> strings, char filterChar) {
+        return strings.stream()
+                .filter(line -> line.startsWith(String.valueOf(filterChar)))
+                .sorted(Comparator.comparing(String::length))
+                .toList();
+    }
+
+    public static Map<String, String> searchMutualFriends(Map<String, List<String>> friendsMap) {
+        Map<String, String> result = new HashMap<>();
+
+        friendsMap.forEach((human, hisFriends) -> hisFriends
+                .forEach(friend -> friendsMap.forEach((key, value) -> {
+                    if (!key.equals(human) && value.contains(friend)
+                            && !hisFriends.contains(key) && !value.contains(human)) {
+                        if (result.containsKey(key)) {
+                            if (!result.get(key).contains(human)) {
+                                result.put(human, key);
+                            }
+                        } else {
+                            result.put(human, key);
+                        }
+                    }
+                })));
+        return result;
+    }
+
+    public static Map<String, Double> getAverageSalary(List<Employee> employees) {
+        return employees.stream()
+                .collect(Collectors.groupingBy(Employee::getDepartment, Collectors.averagingDouble(Employee::getSalary)));
+    }
+
+    public static List<String> stringsFiltering(List<String> strings, String alphabet) {
+        return strings.stream()
+                .filter(line -> line.replaceAll(" ", "").toLowerCase().matches("[" + alphabet + "]+"))
+                .sorted(Comparator.comparing(String::length))
+                .collect(Collectors.toList());
+    }
+
+    public static List<String> intToBinary(List<Integer> numbers) {
+        return numbers.stream()
+                .map(Integer::toBinaryString)
+                .toList();
+    }
+
+    public static List<Integer> getPalindromes(int start, int end) {
+        return IntStream.range(start, end + 1)
+                .filter(num -> {
+                    StringBuilder stringBuilder = new StringBuilder(String.valueOf(num));
+                    String str = stringBuilder.toString();
+                    String reversed = stringBuilder.reverse().toString();
+                    return str.equals(reversed);
+                })
+                .boxed()
+                .toList();
+    }
+}
