fde3b35419e68225139b4e70c8493e622c048f55
Никита <nikita.armeec.88@mail.ru>
2023-06-25 22:25:19 +0300

    Созданы классы: PigThread, Pig1Thread, Pig2Thread, Pig3Thread, ThreeLittlePigs для запуска игры

---
+++ src/main/java/faang/school/godbless/kxnbg/normalgame/Pig1Thread.java
@@ -0,0 +1,24 @@
+package faang.school.godbless.kxnbg.normalgame;
+
+import lombok.SneakyThrows;
+
+public class Pig1Thread extends PigThread {
+
+    public Pig1Thread(String pigName, String material) {
+        super(pigName, material);
+    }
+
+    @SneakyThrows
+    @Override
+    public void run() {
+        System.out.println(this.getPigName() + " начал строительство дома из " + this.getMaterial());
+        Thread.sleep(500);
+
+        System.out.println(this.getPigName() + " заложил фундамент");
+        Thread.sleep(1000);
+
+        System.out.println(this.getPigName() + " приступил к строительству стен и крыши");
+        Thread.sleep(2000);
+        System.out.println(this.getPigName() + " построил дом");
+    }
+}
