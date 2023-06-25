7246930f56f9bca3880975cb0588948735be9e25
Eggur <eggurin2033@gmail.com>
2023-06-25 11:21:11 +0300

    Выполнил задание Insta

---
+++ src/main/java/Insta/FilterProcessor.java
@@ -0,0 +1,13 @@
+package Insta;
+
+import java.util.function.Function;
+
+public class FilterProcessor {
+
+    public Image applyFilter(Image image, Function<Image, Image> function){
+        return function.apply(image);
+    }
+    public Function<Image, Image> combineFilters(Function<Image, Image> first, Function<Image, Image> second){
+        return first.andThen(second);
+    }
+}
