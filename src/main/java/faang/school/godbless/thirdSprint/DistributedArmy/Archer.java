61aa43b86e2aa87a58b4412a30a306c5263d60db
Паша <petrov.pashapet@yandex.ru>
2023-06-26 17:37:09 +0300

    Distributed army of heroes of might and magic

---
+++ src/main/java/faang/school/godbless/thirdSprint/DistributedArmy/Archer.java
@@ -0,0 +1,7 @@
+package faang.school.godbless.thirdSprint.DistributedArmy;
+
+public class Archer extends Unit {
+    public Archer(int power) {
+        super(power);
+    }
+}
---
+++ src/main/java/faang/school/godbless/thirdSprint/DistributedArmy/Mage.java
@@ -0,0 +1,7 @@
+package faang.school.godbless.thirdSprint.DistributedArmy;
+
+public class Mage extends Unit {
+    public Mage(int power) {
+        super(power);
+    }
+}
