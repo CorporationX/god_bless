61e03729e2e3c79b94e89919491f3ca78ea83bf7
zazozhik <ya.940@mail.ru>
2023-06-26 14:36:11 +0300

    Реализовал условию задачи StreamAPI2

--- src/main/java/Sprint_3_Task17/StreamAPI2.java
+++ src/main/java/Sprint_3_Task17/StreamAPI2.java
@@ -1,10 +1,13 @@
 package Sprint_3_Task17;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Comparator;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 public class StreamAPI2 {
     public static Map<Integer, Integer> API(List<Integer> integerList) {
@@ -60,13 +63,90 @@ public class StreamAPI2 {
         });
         return map;
     }
-    public static int API5(){
+    public static Map<String,Double> API5(List<Employee> employeeList){
 //        Получаем список объектов класса Employee, у каждого из которых есть имя, зарплата и отдел.
 //        Найдите среднюю зарплату для каждого отдела. Должна получится map с именем отдела и средней зарплатой.
-        return
+
+        Map<String, Double> result = employeeList.stream()
+                .collect(Collectors.groupingBy(Employee::getDepartment, Collectors.averagingInt(Employee::getSalary)));
+        return result;
+    }
+    public static List<String> API6(String string,List<String> stringList){
+//        Дан список строк. Отфильтруйте строки, которые содержат только буквы заданного алфавита,
+//        и отсортируйте их в порядке возрастания длины строк.
+        return stringList.stream()
+                .filter(s -> s.matches("[a-zA-Z]*"))
+                .sorted(Comparator.comparingInt(String::length))
+                .toList();
+    }
+    public static List<String> API7(List<Integer> integerList){
+//        Написать метод, который преобразует список целых чисел в список строк,
+//        где каждое число записано в двоичном виде.
+        return integerList.stream()
+                .map(Integer::toBinaryString)
+                .toList();
+
+    }
+    public static List<Integer> API8(int a, int b){
+//        Написать метод, который найдет все числа-палиндромы
+//        (читающиеся одинаково слева направо и справа налево) в заданном диапазоне.
+//        На вход получаем число для начала диапазона и число для второй границы диапазона.
+        return IntStream.range(a,b+1)
+                .boxed()
+                .map(Object::toString)
+                .map(StringBuilder::new)
+                .filter(s->{
+                    String def = new String(s);
+                    s.reverse();
+                    return def.contentEquals(s);
+                }).map(StringBuilder::toString)
+                .map(Integer::valueOf)
+                .toList();
+    }
+    public static List<String> API9(String str){
+//        Доп задание: Написать метод, который найдёт все подстроки в строке, которые являются палиндромами.
+//        На вход получаем строку, а вернуть должны список строк.
+        return Arrays.stream(str.split(" "))
+                .map(StringBuilder::new)
+                .filter(s->{
+                    String def = new String(s);
+                    s.reverse();
+                    return def.contentEquals(s);
+                }).map(StringBuilder::toString)
+                .toList();
+    }
+    public static void API10(){
+//        Доп задание: Написать метод, который найдёт все совершенные числа
+//        (сумма делителей числа равна самому числу) в заданном диапазоне.
+//        На вход получаем число для начала диапазона и число для второй границы диапазона.
+
     }
 
+
     public static void main(String[] args) {
+
+        String str = "madam I'm Adam ara madam";
+        System.out.println(API9(str));
+
+
+        int a = 33;
+        int b = 88;
+        System.out.println(API8(a,b));
+
+
+        List<Integer> integerList2 = List.of(1,2,3,4,6);
+        System.out.println(API7(integerList2));
+
+        String s = "abcd";
+        List<String> stringList1 = List.of("Nikita","Vlad","Никита");
+        System.out.println(API6(s,stringList1));
+
+        List<Employee> employeeList = List.of(
+                new Employee("Nikita",100,"Moscow")
+                ,new Employee("Vlad",200,"St.Ptr.")
+                ,new Employee("Vlad2",200,"St.Ptr.")
+                ,new Employee("Inna",300,"Moscow"));
+        System.out.println(API5(employeeList));
 
         Map<String, List<String>> listMap = Map.of(
                 "Коля",List.of("Влад","Инна"),
