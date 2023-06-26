3216152414ed2d2ef1a13c5562314e46662c2cd2
svetlana.obrancova <obrancova.s@gmail.com>
2023-06-26 12:31:57 +0200

    DROIDSECR-1 Created class Droid, interface DroidMessageEncryptor

---
+++ src/main/java/faang/school/godbless/secretsOfDroids/App.java
@@ -0,0 +1,14 @@
+package faang.school.godbless.secretsOfDroids;
+
+public class App {
+    public static void main(String[] args) {
+        Droid r2d2 = new Droid();
+        Droid c3po = new Droid();
+        String message = "May the Force be with you!";
+        int encryptionKey = 3;
+
+        String encryptedMsg = r2d2.sendEncryptedMessage(message, encryptionKey);
+        c3po.receiveEncryptedMessage(encryptedMsg, encryptionKey);
+    }
+
+}
