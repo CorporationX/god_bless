73619f9eac85b23bc098e27eb38535f8c1f0179f
Zlata Galaburdina <585zlata@mail.ru>
2023-06-26 15:56:57 +0300

    Тут крайне много комментов, поскольку это первая задача, которую я делала по лямбдам)) И думала, что я давно ее уже отправила.

--- src/main/java/sprint3task2/NotificationManager.java
+++ src/main/java/sprint3task2/NotificationManager.java
@@ -1,78 +1,80 @@
 package sprint3task2;
 //класс NotificationManager содержит Map, связывающую идентификаторы оповещений
 //с их обработчиками - популярными функциональными интерфейсами
+
 import lombok.Data;
+
 import java.util.HashMap;
 import java.util.Map;
 import java.util.function.Consumer;
+
 @Data
 public class NotificationManager {
-    Map <String, Consumer<Notification>> map = new HashMap<>();
-    public void registerHandler (String notificationIdentification, Consumer<Notification> Nikita){
-        //индентификатор оповещений; функц интерфейс = обработчик оповещений
-        //тут функц интерфейс - значит тут будет лямбда.
-        //то есть тут вообще можно делать лямбду тк есть функц интерф в параметрах
-        map.put(notificationIdentification, Nikita);
+    Map<String, Consumer<Notification>> map = new HashMap<>();
+
+    public void registerHandler(String notificationIdentification, Consumer<Notification> consumer) {
+//В параметрах (Идентификатор оповещений, функциональный интерфейс (о же обработчик оповещений))
+//Тут функциональный интерфейс - значит отсюда пойдет лямбда.
+//То есть тут в принципе возможно делать лямбду, тк есть функциональный интерфейс привносится в параметрах.
+        map.put(notificationIdentification, consumer);
     }
-    public void sendNotification (Notification notification) {
-        //вызывает соответствующий обработчик оповещения из Map
-        //обработчики - функциональные интерфейсы Java
+
+    public void sendNotification(Notification notification) {
+//вызывает соответствующий обработчик оповещения из Map.
+//обработчики - функциональные интерфейсы Java.
 //        if (map.containsKey(notification.getType()) || !map.isEmpty()) {
 //            Consumer<Notification> consumer = map.get(notification.getType());
 //            consumer.accept(notification);
 //        } else throw new IllegalArgumentException("Исключение");
-        Consumer <Notification> consumer = map.get(notification.getType());
+        Consumer<Notification> consumer = map.get(notification.getType());
         consumer.accept(notification);
-        // вызывает ===значит нужен сюда метод функц интерфейса
-        //выдергивает значение из мапы, то есть функц интерфейс, и к этому объекту
-        // функц интерфейса применяем его собственный метод accept с параметром notification,
-        // из самого метода, где находимся
+//"Вызывает" ===> значит нужен сюда метод функционального интерфейса.
+//Выдергивает значение из мапы, то есть функциональный интерфейс, и к этому объекту
+//Функциональный интерфейс применяет его собственный метод accept с параметром notification,
+//из самого метода, где находимся.
 
-
-        //за счет этого метода вообще сотворяется ляба для использования ее в методе registerHandler
+//за счет этого метода вообще сотворяется лямбда для использования ее в методе registerHandler.
     }
+
     public static void main(String[] args) {
         NotificationManager notificationManager = new NotificationManager();
-
 // Регистрация обработчиков оповещений
-        //складываем в map ключи и значения
-        //в Consumer уже можем делать лямбду потому что это функци интерфейс
-        //сами пишем какой идентификатор оповещений и какое выражени в лямбде будет
-        // тк тут Consumer в методе, то прописываем как именно будет себя вести лямба ()
 
-        ////// каркас для лямбы (!!!!!!)
+//Складываем в map ключи и значения
+//С помощью Consumer уже можем делать лямбду, потому что это функциональный интерфейс.
+//Сами пишем какой идентификатор оповещений и какое выражение будет в лямбде.
+//Тк тут Consumer в методе, то прописываем как именно будет себя вести лямба ().
+
+////// каркас для лямбды (!)
         notificationManager.registerHandler("email", (notification) -> System.out.println("Отправка по электронной почте: " + notification.getMessage()));
         notificationManager.registerHandler("sms", (notification) -> System.out.println("Отправка SMS: " + notification.getMessage()));
         notificationManager.registerHandler("push", (notification) -> System.out.println("Отправка push-уведомления: " + notification.getMessage()));
 
 // Отправка оповещений
-        //сохдаем объекты класса Notification для их использования в sendNotification
-        //здесь сотворяются объекты notification, чтобы их использовать для
+        //Создаем объекты класса Notification для их использования в sendNotification.
+        //Здесь сотворяются объекты notification, чтобы их использовать для
         //сотворения лямбды в notificationManager.sendNotification(...);
 
-
-        ///// объекты для метода в котором вызов лямбы (!!!!!!!!)
+///// объекты для метода в котором вызов лямбды (!!)
         Notification emailNotification = new Notification("email", "Ваша учетная запись успешно активирована");
         Notification smsNotification = new Notification("sms", "Вы успешно изменили свой пароль");
         Notification pushNotification = new Notification("push", "Новый пост от пользователя: JohnDoe");
 
-        //вызываем соответствующи обработчик оповещения из Map с помощью метода sendNotification
-        //через объект класса делается
-        //вставляем соданные выше объекты класса
-        //это делаем чтобы был вообще объект notification
-        // для Consumer<Notification> Nikita из registerHandler
+//вызываем соответствующи обработчик оповещения из Map с помощью метода sendNotification
+//через объект класса делается
+//вставляем созданные выше объекты класса
+//это делаем чтобы был вообще объект notification
+//для Consumer<Notification> Nikita из registerHandler
 
-        //если это не выполнять - лямба не сотворяется
+//если это не выполнять - лямбда не сотворяется.
 
 
-        ///// собственно вызов лямбы (!!!!!!!)
+/////собственно вызов лямбды (!!!)
         notificationManager.sendNotification(emailNotification);
         notificationManager.sendNotification(smsNotification);
         notificationManager.sendNotification(pushNotification);
 
-
-        ///функц интерфейс в методе - это его вызов;
-        //функц интерфейс в передаваемых параметрах - это значит надо лямбу прописывать
-
+///функциональный интерфейс в методе - это его вызов;
+//функциональный интерфейс в передаваемых параметрах - значит будем лямбду прописывать.
     }
 }
