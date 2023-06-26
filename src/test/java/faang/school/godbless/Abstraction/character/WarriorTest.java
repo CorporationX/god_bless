09fd9206102926543ae538e0b34e56ebd9075d21
Pavel <pavlik1123.pg@gmail.com>
2023-06-26 20:33:03 +0700

    sdf

---
+++ src/test/java/faang/school/godbless/Abstraction/character/WarriorTest.java
@@ -0,0 +1,7 @@
+package faang.school.godbless.Abstraction.character;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class WarriorTest {
+
+}
---
+++ src/test/java/faang/school/godbless/spring_4/you_work_for_microsoft/MailSenderTest.java
@@ -0,0 +1,7 @@
+package faang.school.godbless.spring_4.you_work_for_microsoft;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MailSenderTest {
+
+}
