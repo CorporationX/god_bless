d9814d4302647c780d261a7f7cce30d24b8b7ec2
zazozhik <ya.940@mail.ru>
2023-06-26 18:43:08 +0300

    Реализовал условию задачи

---
+++ src/main/java/Sprint_4_Task1/MailSender.java
@@ -0,0 +1,18 @@
+package Sprint_4_Task1;
+
+public class MailSender {
+
+    public static void main(String[] args) throws InterruptedException {
+
+        Thread thread = null;
+        for(int i = 0; i < 5; i++) {
+            int startIndex = i * 200 + 1;
+            int endIndex = startIndex + 200;
+            thread = new Thread(new SenderRunnable(startIndex,endIndex));
+            thread.start();
+            
+        }
+        thread.join();
+        System.out.println(" Поле");
+    }
+}
