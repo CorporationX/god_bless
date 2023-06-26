03b1db736d1262447c95de42252b5b7f52a3e5c2
Matvey <matveynikishaev@gmail.com>
2023-06-26 22:28:17 +0300

    Implemented Microsoft mails task

---
+++ src/main/java/faang/school/godbless/SenderRunnable.java
@@ -0,0 +1,17 @@
+package faang.school.godbless;
+
+import lombok.AllArgsConstructor;
+
+@AllArgsConstructor
+public class SenderRunnable implements Runnable {
+    private final int startIndex;
+    private final int endIndex;
+
+    @Override
+    public void run() {
+        for (int i = startIndex; i < endIndex; i++) {
+            System.out.printf("Mail %d was sent%n", i);
+        }
+
+    }
+}
