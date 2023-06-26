66e0e1b719f3f5ea03dd3d50aa402f6ca8e02785
Zhugalev Sergei <zhugalcf@gmail.com>
2023-06-26 19:50:31 +0300

    Feed Peter Griffin BC1670

---
+++ src/main/java/faang/school/godbless/FeedPeterGriffin/GriffinsFoodDelivery.java
@@ -0,0 +1,20 @@
+package faang.school.godbless.FeedPeterGriffin;
+
+import faang.school.godbless.YouAreWorkingInMicrosoft.SenderRunnable;
+
+import java.util.Random;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+public class GriffinsFoodDelivery {
+
+
+    public static void main(String[] args) {
+        String[] characterNames = {"Peter", "Lois", "Meg", "Chris", "Stewie"};
+        ExecutorService executor = Executors.newFixedThreadPool(3);
+        for (int i = 0; i < 5; i++) {
+            executor.submit(new FoodDeliveryTask(characterNames[i], new Random().nextInt(50)));
+        }
+        executor.shutdown();
+    }
+}
