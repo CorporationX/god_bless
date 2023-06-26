089fb6125f0fa77d84c68e31dee169971cbe4c8c
Никита <nikita.armeec.88@mail.ru>
2023-06-26 18:34:11 +0300

    Изменен класс MailSender

--- src/main/java/faang/school/godbless/kxnvg/microsoft/MailSender.java
+++ src/main/java/faang/school/godbless/kxnvg/microsoft/MailSender.java
@@ -1,18 +1,26 @@
 package faang.school.godbless.kxnvg.microsoft;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class MailSender {
 
     public static void main(String[] args) throws InterruptedException {
+        List<Thread> threads = new ArrayList<>();
+
         for (int i = 0; i < 5; i++) {
             int startIndex = 200 * i + 1;
             int endIndex = startIndex + 199;
 
             SenderRunnable senderRunnable = new SenderRunnable(startIndex, endIndex);
-            Thread thread = new Thread(senderRunnable);
+            threads.add(new Thread(senderRunnable));
+            threads.get(i).start();
+        }
 
-            thread.start();
+        for (Thread thread : threads) {
             thread.join();
         }
+
         System.out.println("Все письма отпрпвлены");
     }
 }
