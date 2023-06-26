14f8aa56ec9c1b6e25d592ee9adeca6a97f60cea
Dregid <ufl852@gmail.com>
2023-06-27 01:14:45 +0600

    Исправление всех допущенных ошибок

--- src/main/java/sprint4/colleague/Main.java
+++ src/main/java/sprint4/colleague/Main.java
@@ -7,20 +7,28 @@ import java.util.concurrent.Executors;
 import java.util.concurrent.TimeUnit;
 
 public class Main {
-    static final List<Person> PEOPLE = new ArrayList<>();
+    private static final List<Person> PEOPLE = new ArrayList<>();
+
+    static {
+        fillList();
+    }
+    private static final int COUNT_THREADS = 10;
+    private static final int THREAD_ON_HUMAN = PEOPLE.size() / COUNT_THREADS;
 
     public static void main(String[] args) {
-        fillList();
-        ExecutorService service = Executors.newFixedThreadPool(10);
-        for (int i = 0; i < 10; i++) {
-            int startIndex = i * 1000;
-            int endIndex = startIndex + 1000;
-            List<Person> divided = PEOPLE.subList(startIndex, endIndex);
-            service.execute(new PersonNamePrinter(divided));
+        ExecutorService service = Executors.newFixedThreadPool(COUNT_THREADS);
+        for (int i = 0; i < COUNT_THREADS; i++) {
+            int startIndex = i * THREAD_ON_HUMAN;
+            int endIndex = startIndex + THREAD_ON_HUMAN - 1;
+
+            List<Person> peopleGroup = PEOPLE.subList(startIndex, endIndex);
+            service.execute(new PersonNamePrinter(peopleGroup));
         }
+
         service.shutdown();
         try {
             service.awaitTermination(10, TimeUnit.SECONDS);
+            service.shutdownNow();
         } catch (InterruptedException e) {
             throw new RuntimeException("Да взлетит оно все к чертям");
         }
