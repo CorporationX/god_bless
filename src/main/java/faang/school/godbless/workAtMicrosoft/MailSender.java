74cd587f8b39acd74854da638c238207b2291b80
Andrew13pol <andrew13pol@gmail.com>
2023-06-27 00:15:34 +0500

    Вы работаете в Microsoft! (стилистику привел в порядок)

--- src/main/java/faang/school/godbless/workAtMicrosoft/MailSender.java
+++ src/main/java/faang/school/godbless/workAtMicrosoft/MailSender.java
@@ -4,7 +4,7 @@ import java.util.concurrent.Executors;
 public class MailSender {
     public static void main(String[] args) throws InterruptedException {
         ExecutorService executor = Executors.newFixedThreadPool(5);
-        for(int i=0; i<=5; i++){
+        for (int i = 0; i <= 5; i++) {
             SenderRunnable sender = new SenderRunnable(1, 200, i);
             executor.submit(sender);
         }
--- src/main/java/faang/school/godbless/workAtMicrosoft/SenderRunnable.java
+++ src/main/java/faang/school/godbless/workAtMicrosoft/SenderRunnable.java
@@ -1,15 +1,15 @@
 import lombok.AllArgsConstructor;
 
 @AllArgsConstructor
-    public class SenderRunnable implements Runnable{
-        private int startIndex;
-        private int endIndex;
-        private int thread;
+public class SenderRunnable implements Runnable {
+    private int startIndex;
+    private int endIndex;
+    private int thread;
 
-        @Override
-        public void run() {
-            for (int i=startIndex; i<= endIndex; i++){
-                System.out.println("Отправлено письмо номер - "+i+" - Потока - "+thread);
-            }
+    @Override
+    public void run() {
+        for (int i = startIndex; i <= endIndex; i++) {
+            System.out.println("Отправлено письмо номер - " + i + " - Потока - " + thread);
         }
     }
+}
