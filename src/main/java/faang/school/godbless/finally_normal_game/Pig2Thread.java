406489fe1bb9d59ec412729f4600d9aaf05c53b6
Devilserk <demonserker@gmail.com>
2023-06-28 01:19:35 +0600

    Задание Наконец то нормальная игра выполнено

---
+++ src/main/java/faang/school/godbless/finally_normal_game/Pig2Thread.java
@@ -0,0 +1,7 @@
+package faang.school.godbless.finally_normal_game;
+
+public class Pig2Thread extends PigThread {
+    public Pig2Thread() {
+        super("Naf-naf", "wood");
+    }
+}
---
+++ src/main/java/faang/school/godbless/finally_normal_game/PigThread.java
@@ -0,0 +1,27 @@
+package faang.school.godbless.finally_normal_game;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@AllArgsConstructor
+@Getter
+public class PigThread extends Thread {
+    private String pigName;
+    private String material;
+
+    @Override
+    public void run() {
+        buildHouse();
+    }
+
+    private synchronized void buildHouse() {
+        System.out.printf("%s started building a house out of %s (%s)\n",
+                pigName, material, Thread.currentThread().getName());
+        try {
+            Thread.sleep(1000);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+        System.out.printf("%s finished (%s)\n", pigName, Thread.currentThread().getName());
+    }
+}
