1a3f584843b4faac80f1488373d952128274de76
Dmitry Sorokin <dm.sorokin21@gmail.com>
2023-06-27 19:22:13 +0300

    resolved task

--- src/main/java/faang/school/godbless/lambda/insta/Image.java
+++ src/main/java/faang/school/godbless/lambda/insta/Image.java
@@ -1,19 +1,9 @@
 package faang.school.godbless.lambda.insta;
 
+import lombok.Value;
+
+@Value
 public class Image {
     private String filePath;
     private String description;
-
-    public Image(String filePath, String description) {
-        this.filePath = filePath;
-        this.description = description;
-    }
-
-    public String getFilePath() {
-        return filePath;
-    }
-
-    public String getDescription() {
-        return description;
-    }
 }
