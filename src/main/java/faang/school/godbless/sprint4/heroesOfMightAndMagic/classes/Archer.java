f18cc2a2e91f510a929a027a095bab6db38df4e9
Basimov Rustam <sombra1nf@yandex.ru>
2023-06-27 19:43:48 +0500

    BC-1616. Done

---
+++ src/main/java/faang/school/godbless/sprint4/heroesOfMightAndMagic/classes/Archer.java
@@ -0,0 +1,7 @@
+package faang.school.godbless.sprint4.heroesOfMightAndMagic.classes;
+
+public class Archer extends Character{
+    protected Archer(int power) {
+        super(power, "Archer");
+    }
+}
