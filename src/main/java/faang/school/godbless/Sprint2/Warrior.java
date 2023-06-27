49f3b1e930ef4c1ac84a642acf23860808cc776b
chaykinks <chaykinks@gmail.com>
2023-06-27 19:17:36 +0500

    создал класс Notification

--- src/main/java/faang/school/godbless/Sprint2/Warrior.java
+++ src/main/java/faang/school/godbless/Sprint2/Warrior.java
@@ -1,4 +1,4 @@
-package faang.school.godbless;
+package faang.school.godbless.Sprint2;
 
 public class Warrior extends Character {
 
---
+++ src/main/java/faang/school/godbless/Sprint3/Task_2_Meta/Notification.java
@@ -0,0 +1,6 @@
+package faang.school.godbless.Sprint3.Task_2_Meta;
+
+public class Notification {
+    String type;
+    String message;
+}
