c12a53dbe985755a3cbafd5e502aefb3274e2d0d
Sergey Gerasimov <czar997@yandex.ru>
2023-06-27 17:48:54 +0300

    исправлен запуск ракет

--- src/main/java/faang/school/godbless/sprint4/multithreading_parallelism_thread/task11/Main.java
+++ src/main/java/faang/school/godbless/sprint4/multithreading_parallelism_thread/task11/Main.java
@@ -3,25 +3,19 @@ package faang.school.godbless.sprint4.multithreading_parallelism_thread.task11;
 import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
+
+import static faang.school.godbless.sprint4.multithreading_parallelism_thread.task11.RocketLaunch.planRocketLaunches;
 
 public class Main {
 
     public static void main(String[] args) {
         List<RocketLaunch> launches = new ArrayList<>();
-        launches.add(new RocketLaunch("Falcon 9", LocalDateTime.now().plusMinutes(2)));
-        launches.add(new RocketLaunch("Falcon Heavy", LocalDateTime.now().plusMinutes(5)));
-        launches.add(new RocketLaunch("Dragon", LocalDateTime.now().plusMinutes(10)));
+        launches.add(new RocketLaunch("Falcon 9", LocalDateTime.now().plusSeconds(2)));
+        launches.add(new RocketLaunch("Falcon Heavy", LocalDateTime.now().plusSeconds(5)));
+        launches.add(new RocketLaunch("Dragon", LocalDateTime.now().plusSeconds(10)));
 
         planRocketLaunches(launches);
     }
 
-    public static void planRocketLaunches(List<RocketLaunch> rocketLaunches) {
-        ExecutorService executor = Executors.newSingleThreadExecutor();
-        for (RocketLaunch rocketLaunch : rocketLaunches) {
-            executor.submit(rocketLaunch);
-        }
-        executor.shutdown();
-    }
+
 }
--- src/main/java/faang/school/godbless/sprint4/multithreading_parallelism_thread/task11/RocketLaunch.java
+++ src/main/java/faang/school/godbless/sprint4/multithreading_parallelism_thread/task11/RocketLaunch.java
@@ -3,7 +3,11 @@ package faang.school.godbless.sprint4.multithreading_parallelism_thread.task11;
 import lombok.AllArgsConstructor;
 import lombok.SneakyThrows;
 
+import java.time.Duration;
 import java.time.LocalDateTime;
+import java.util.List;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 @AllArgsConstructor
 public class RocketLaunch implements Runnable {
@@ -19,10 +23,23 @@ public class RocketLaunch implements Runnable {
     }
 
     @Override
+    @SneakyThrows
     public void run() {
+        if (startDate.isAfter(LocalDateTime.now())) {
+            Duration duration = Duration.between(LocalDateTime.now(), startDate);
+            Thread.sleep(duration.toMillis());
+        }
         long startTime = System.currentTimeMillis();
         launch();
         long endTime = System.currentTimeMillis();
         System.out.println("Затрачено "+ (endTime - startTime) / 1000 + " секунд");
+    }
+
+    public static void planRocketLaunches(List<RocketLaunch> rocketLaunches) {
+        ExecutorService executor = Executors.newSingleThreadExecutor();
+        for (RocketLaunch rocketLaunch : rocketLaunches) {
+            executor.submit(rocketLaunch);
+        }
+        executor.shutdown();
     }
 }
