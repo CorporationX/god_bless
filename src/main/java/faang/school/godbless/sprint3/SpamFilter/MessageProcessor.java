39b1e194765846a888fd0cf306ec36395782cf44
mixalight <mixalight@yandex.ru>
2023-06-27 23:25:02 +0300

    Spam filter

---
+++ src/main/java/faang/school/godbless/sprint3/SpamFilter/MessageProcessor.java
@@ -0,0 +1,13 @@
+package faang.school.godbless.sprint3.SpamFilter;
+
+import java.util.List;
+
+public class MessageProcessor {
+    public boolean processMessage(String message, List<MessageFilter> handlers) {
+        boolean answer = true;
+        for(MessageFilter handler : handlers) {
+            answer = answer & handler.filter(message);
+        }
+        return answer;
+    }
+}
