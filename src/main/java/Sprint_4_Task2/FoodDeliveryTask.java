b6eb2cfe30e1ce1a14d8886dce59522f21c29816
zazozhik <ya.940@mail.ru>
2023-06-28 00:15:13 +0300

    Реализовал условию задачи Feed Peter Griffin

--- src/main/java/Sprint_4_Task2/FoodDeliveryTask.java
+++ src/main/java/Sprint_4_Task2/FoodDeliveryTask.java
@@ -2,7 +2,7 @@ package Sprint_4_Task2;
 
 import java.util.Random;
 
-public class FoodDeliveryTask implements Runnable{
+public class FoodDeliveryTask implements Runnable {
     private String character;
     private int foodAmount;
 
@@ -10,14 +10,16 @@ public class FoodDeliveryTask implements Runnable{
         this.character = character;
         this.foodAmount = foodAmount;
     }
+
     private String getFoodType() {
         String[] foodTypes = {"pizza", "burger", "hot dog", "chicken wings", "taco"};
         return foodTypes[new Random().nextInt(foodTypes.length)];
     }
+
     @Override
     public void run() {
-        System.out.println(" Питер получает 50 куриных крылышек;");
-        System.out.println(" Получит доставку");
-        System.out.println(" Всё получил");
+        System.out.println(character + " Получает " + foodAmount + " " + getFoodType());
+        System.out.println(character + " Ждет доставку " + foodAmount + " " + getFoodType());
+        System.out.println(character + " Всё получил " + foodAmount + " " + getFoodType());
     }
 }
