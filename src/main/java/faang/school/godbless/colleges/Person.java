fe2c1d74a2b6d1b82981d7a50049ae05d809e204
Serhii Rubets <serhii.rubets@holbertonschool.com>
2023-06-27 16:05:22 -0500

    Added chunks printing

---
+++ src/main/java/faang/school/godbless/colleges/Person.java
@@ -0,0 +1,16 @@
+package faang.school.godbless.colleges;
+
+public class Person {
+  private String name;
+  private String surname;
+  private int age;
+  private String workplace;
+
+  public Person(String name) {
+    this.name = name;
+  }
+
+  public String getName() {
+    return name;
+  }
+}
