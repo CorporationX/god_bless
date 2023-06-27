7393fdbf7c511d9bf1256ed754db28275e006ff4
Sveta Zalogina <zalogina.sveta@gmail.com>
2023-06-26 20:23:20 -0400

    Implement Hogwarts task

---
+++ src/test/java/faang/school/godbless/sprint3/SpellCasterTest.java
@@ -0,0 +1,32 @@
+package faang.school.godbless.sprint3;
+
+import faang.school.godbless.sprint3.hogwarts.SpellCaster;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class SpellCasterTest {
+    @Test
+    public void castAlohomora() {
+        SpellCaster spellCaster = new SpellCaster();
+        String alohomora = "Alohomora";
+        assertEquals("The door is unlocked by Alohomora",
+                spellCaster.cast(alohomora, (spell) -> "The door is unlocked by " + spell));
+    }
+
+    @Test
+    public void castLumos() {
+        SpellCaster spellCaster = new SpellCaster();
+        String lumos = "Lumos";
+        assertEquals("A beam of light is created by Lumos",
+                spellCaster.cast(lumos, (spell) -> "A beam of light is created by " + spell));
+    }
+
+    @Test
+    public void castExpelliarmus() {
+        SpellCaster spellCaster = new SpellCaster();
+        String expelliarmus = "Expelliarmus";
+        assertEquals("The opponent is disarmed by Expelliarmus",
+                spellCaster.cast(expelliarmus, (spell) -> "The opponent is disarmed by " + spell));
+    }
+}
