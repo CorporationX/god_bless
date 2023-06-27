5f17c78cc849267147624b3ece6239454ed03d9d
Artem Afonin <art002.2000@mail.ru>
2023-06-27 07:14:53 +0300

    Выполнил задачу "Фильтры Instagram"

---
+++ src/main/java/faang/school/godbless/insta_filters/MainApplication.java
@@ -0,0 +1,23 @@
+package faang.school.godbless.insta_filters;
+
+import java.util.function.Function;
+
+public class MainApplication {
+    public static void main(String[] args) {
+
+        Image originalImage = new Image("original.jpg", "Оригинальное изображение");
+
+        FilterProcessor filterProcessor = new FilterProcessor();
+
+        Function<Image, Image> grayscaleFilter = (image) -> new Image(image.getName() + "_grayscale", "Фильтр: черно-белый");
+        Function<Image, Image> sepiaFilter = (image) -> new Image(image.getName() + "_sepia", "Фильтр: сепия");
+        Function<Image, Image> vignetteFilter = (image) -> new Image(image.getName() + "_vignette", "Фильтр: виньетка");
+
+        Image grayscaleImage = filterProcessor.applyFilter(originalImage, grayscaleFilter);
+        Image sepiaImage = filterProcessor.applyFilter(originalImage, sepiaFilter);
+        Image vignetteImage = filterProcessor.applyFilter(originalImage, vignetteFilter);
+
+        Function<Image, Image> combinedFilter = filterProcessor.combineFilters(grayscaleFilter, sepiaFilter);
+        Image combinedImage = filterProcessor.applyFilter(originalImage, combinedFilter);
+    }
+}
