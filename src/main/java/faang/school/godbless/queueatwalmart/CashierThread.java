330e448bdcc6c58df98149e07b2e9472cba9234e
Andrew13pol <andrew13pol@gmail.com>
2023-06-27 23:30:27 +0500

    Очередь в Walmart

---
+++ src/main/java/faang/school/godbless/queueatwalmart/CashierThread.java
@@ -0,0 +1,14 @@
+import lombok.AllArgsConstructor;
+
+import java.util.List;
+
+@AllArgsConstructor
+public class CashierThread  extends Thread{
+    private int cashierId;
+    private List<Integer> customerItems;
+
+    @Override
+    public void run() {
+        System.out.println("Продавец ID="+cashierId+" обработал "+customerItems.size()+" общей стоимостью "+customerItems.stream().mapToInt(Integer::intValue).sum());
+    }
+}
