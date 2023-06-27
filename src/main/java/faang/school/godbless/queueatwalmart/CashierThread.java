10d3c5a5dfc80f99364d82a733f27d9f4314a21f
Andrew13pol <andrew13pol@gmail.com>
2023-06-27 23:31:07 +0500

    Очередь в Walmart new

--- src/main/java/faang/school/godbless/queueatwalmart/CashierThread.java
+++ src/main/java/faang/school/godbless/queueatwalmart/CashierThread.java
@@ -3,12 +3,12 @@ import lombok.AllArgsConstructor;
 import java.util.List;
 
 @AllArgsConstructor
-public class CashierThread  extends Thread{
+public class CashierThread extends Thread {
     private int cashierId;
     private List<Integer> customerItems;
 
     @Override
     public void run() {
-        System.out.println("Продавец ID="+cashierId+" обработал "+customerItems.size()+" общей стоимостью "+customerItems.stream().mapToInt(Integer::intValue).sum());
+        System.out.println("Продавец ID=" + cashierId + " обработал " + customerItems.size() + " общей стоимостью " + customerItems.stream().mapToInt(Integer::intValue).sum());
     }
 }
--- src/main/java/faang/school/godbless/queueatwalmart/Main.java
+++ src/main/java/faang/school/godbless/queueatwalmart/Main.java
@@ -6,12 +6,12 @@ import java.util.List;
 public class Main {
     @SneakyThrows
     public static void main(String[] args) {
-        List<List<Integer>> customers = List.of(List.of(1,3,4,5), List.of(40,50), List.of(600,700,800));
+        List<List<Integer>> customers = List.of(List.of(1, 3, 4, 5), List.of(40, 50), List.of(600, 700, 800));
         List<Thread> cashiers = new ArrayList<>();
-        for(int i=0; i<customers.size(); i++){
+        for (int i = 0; i < customers.size(); i++) {
             cashiers.add(new Thread(new CashierThread(i, customers.get(i))));
         }
-        for (Thread cashier: cashiers) {
+        for (Thread cashier : cashiers) {
             cashier.start();
             cashier.join();
         }
