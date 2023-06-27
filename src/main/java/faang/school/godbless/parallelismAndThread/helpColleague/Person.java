19ee13cd1e292efc72b5bb295a5f87143181cfd1
Egor Selivanov <egorselivanov0@gmail.com>
2023-06-28 02:31:42 +0300

    Решена задача "Помогите коллеге"

---
+++ src/main/java/faang/school/godbless/parallelismAndThread/helpColleague/Person.java
@@ -0,0 +1,15 @@
+package faang.school.godbless.parallelismAndThread.helpColleague;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+public class Person {
+    private String name;
+    private String surname;
+    private int age;
+    private String workplace;
+}
