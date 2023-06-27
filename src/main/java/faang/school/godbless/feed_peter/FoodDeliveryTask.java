f6299c71d3a87a4a8ca3a142ed239d3423b98131
Devilserk <demonserker@gmail.com>
2023-06-27 23:29:23 +0600

    Задание Накормите Питера выполнено

---
+++ src/main/java/faang/school/godbless/feed_peter/FoodDeliveryTask.java
@@ -0,0 +1,28 @@
+package faang.school.godbless.feed_peter;
+
+import lombok.AllArgsConstructor;
+
+import java.util.Random;
+
+@AllArgsConstructor
+public class FoodDeliveryTask implements Runnable {
+    private String character;
+    private int foodAmount;
+
+    @Override
+    public void run() {
+        String food = getFoodType();
+        System.out.printf("%s ordered: %s %s (%s)\n", character, foodAmount, food, Thread.currentThread().getName());
+        try {
+            Thread.sleep(1000);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+        System.out.printf("%s eats: %s %s (%s)\n", character, foodAmount, food, Thread.currentThread().getName());
+    }
+
+    private String getFoodType() {
+        String[] foodTypes = {"pizza", "burger", "hot dog", "chicken wings", "taco"};
+        return foodTypes[new Random().nextInt(foodTypes.length)];
+    }
+}
