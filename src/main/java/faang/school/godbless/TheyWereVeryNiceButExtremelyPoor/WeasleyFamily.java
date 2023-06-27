2fa59069ad228b4ac277a8eb651984d80f5b3202
Zhugalev Sergei <zhugalcf@gmail.com>
2023-06-27 15:59:17 +0300

    Parallel They were very nice but extremly poor BC-1773

--- src/main/java/faang/school/godbless/TheyWereVeryNiceButExtremelyPoor/WeasleyFamily.java
+++ src/main/java/faang/school/godbless/TheyWereVeryNiceButExtremelyPoor/WeasleyFamily.java
@@ -7,7 +7,7 @@ public class WeasleyFamily {
     public static void main(String[] args) {
         String[] chores = {"Почистить зубы","Выполнить задачу 1","Выполнить задачу 2",
                 "Выполнить задачу 3","Выполнить задачу 4"};
-        ExecutorService service = Executors.newFixedThreadPool(2);
+        ExecutorService service = Executors.newCachedThreadPool();
         for (int i = 0; i < chores.length; i++) {
             service.submit(new Chore(chores[i]));
         }
