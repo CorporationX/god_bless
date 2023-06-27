599246870a66132d21d3b83c4c176a9194b21b79
Basimov Rustam <sombra1nf@yandex.ru>
2023-06-27 20:35:12 +0500

    BC-1618. Done

---
+++ src/main/java/faang/school/godbless/sprint4/queueWallmart/classes/CashierThread.java
@@ -0,0 +1,25 @@
+package faang.school.godbless.sprint4.queueWallmart.classes;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+import java.util.Arrays;
+
+@RequiredArgsConstructor
+@Getter
+public class CashierThread extends Thread {
+    private final int cashierId;
+    private final int[] customerItems;
+    private int totalSum;
+    private int countItems;
+
+    @Override
+    public void run() {
+
+        Arrays.stream(customerItems)
+                .forEach(item -> {
+                    totalSum += item;
+                    countItems++;
+                });
+    }
+}
---
+++ src/main/java/faang/school/godbless/sprint4/queueWallmart/classes/Main.java
@@ -0,0 +1,36 @@
+package faang.school.godbless.sprint4.queueWallmart.classes;
+
+public class Main {
+    public static void main(String[] args) {
+        int totalSum = 0;
+        int countItems = 0;
+        int[][] customerItems = {
+                {5, 7, 3, 2},
+                {1, 2, 3},
+                {6, 5, 2},
+                {10, 2, 1, 3, 5},
+        };
+
+        CashierThread[] cashierThreads = new CashierThread[customerItems.length];
+
+        for (int i = 0; i < customerItems.length; i++) {
+            cashierThreads[i] = new CashierThread(i, customerItems[i]);
+            cashierThreads[i].start();
+        }
+
+        for (CashierThread cashierThread : cashierThreads) {
+            try {
+                cashierThread.join();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+
+        for (CashierThread cashierThread : cashierThreads) {
+            totalSum += cashierThread.getTotalSum();
+            countItems += cashierThread.getCountItems();
+        }
+
+        System.out.println("Total sum: " + totalSum + ", count items: " + countItems);
+    }
+}
