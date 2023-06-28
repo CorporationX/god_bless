e1087a229f0d49af99987b71d3432aadeaab9467
Devilserk <demonserker@gmail.com>
2023-06-28 20:07:06 +0600

    Объявил классы

---
+++ src/main/java/faang/school/godbless/multithreading/queue_walmart/CashierThread.java
@@ -0,0 +1,13 @@
+package faang.school.godbless.multithreading.queue_walmart;
+
+import java.util.List;
+
+public class CashierThread extends Thread {
+    private int cashierId;
+    List<String> customerItems;
+
+    @Override
+    public void run() {
+
+    }
+}
---
+++ src/main/java/faang/school/godbless/multithreading/queue_walmart/Main.java
@@ -0,0 +1,14 @@
+package faang.school.godbless.multithreading.queue_walmart;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class Main {
+    public static void main(String[] args) {
+        Map<String, List<String>> customers = new HashMap<>();
+        List<CashierThread> cashiers = new ArrayList<>();
+
+    }
+}
