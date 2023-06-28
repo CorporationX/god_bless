c9be2b5dee45be2433a6ee5e6006221ff7bdb050
Andrey <khalyavinandrey@gmail.com>
2023-06-28 12:45:10 +0500

    mail sender

--- src/main/java/faang/school/godbless/youWorkAtMicrosoft/MailSender.java
+++ src/main/java/faang/school/godbless/youWorkAtMicrosoft/MailSender.java
@@ -1,9 +1,13 @@
 package faang.school.godbless.youWorkAtMicrosoft;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class MailSender {
     public static void main(String[] args) throws InterruptedException {
         final int QUANTITY = 1000;
         final int THREADQUANITY = 5;
+        List<Thread> threadList = new ArrayList<>();
 
         for (int i = 0; i < THREADQUANITY; i++) {
             int startIndex = (QUANTITY / THREADQUANITY) * i + 1;
@@ -13,8 +17,11 @@ public class MailSender {
                     new SenderRunnable(startIndex, endIndex)
             );
 
-            thread.start();
+            threadList.add(thread);
+        }
 
+        for (Thread thread : threadList) {
+            thread.start();
             thread.join();
         }
 
