d4b32338bbc2b99b0dc5d284fe10b66661e86d8e
Adam-D-Beno <wrofdt@yandex.ru>
2023-06-28 14:44:52 +0300

    solved BC-1915 Потренируем StreamAPI 1

--- src/main/java/faang/school/godbless/Application.java
+++ src/main/java/faang/school/godbless/Application.java
@@ -6,6 +6,12 @@ import java.util.function.Supplier;
 
 public class Application {
     public static void main(String... args) {
+        List<Integer> numbers = List.of(1, 2, 3, 4, 5);
+        List<String> stringList = List.of("Adam", "Me", "My");
+        System.out.println(UtilStream.findSumsEvenNumbers(numbers));
+        System.out.println(UtilStream.findMaxNumber(numbers));
+        System.out.println(UtilStream.findAverageNumber(numbers));
+        System.out.println(UtilStream.findNumberLinesStartingWithSpecChar(stringList, 'm'));
 
     }
 }
