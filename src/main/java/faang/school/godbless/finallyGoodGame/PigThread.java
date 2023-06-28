c38e40ddc7f7e491ede41d62b29f96324cac4dc2
Andrey <khalyavinandrey@gmail.com>
2023-06-28 18:24:14 +0500

    main thread class

---
+++ src/main/java/faang/school/godbless/finallyGoodGame/PigThread.java
@@ -0,0 +1,11 @@
+package faang.school.godbless.finallyGoodGame;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class PigThread extends Thread{
+    private String pigName;
+    private String material;
+}
