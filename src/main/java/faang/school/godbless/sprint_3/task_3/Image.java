44f8f8905cbd65b1d983a02d1c47375cce17b1db
Иван Стадник <stadivany@gmail.com>
2023-06-28 20:04:58 +0300

    solved task 2 (sprint 3) "Meta-world"
    изменил модификаторы полей

--- src/main/java/faang/school/godbless/sprint_3/task_3/Image.java
+++ src/main/java/faang/school/godbless/sprint_3/task_3/Image.java
@@ -1,13 +1,11 @@
 package faang.school.godbless.sprint_3.task_3;
 
 import lombok.Data;
-import lombok.Getter;
 
 @Data
-@Getter
 public class Image {
-    String name;
-    String descriptionFile;
+    private String name;
+    private String descriptionFile;
 
     public Image(String name, String descriptionFile) {
         this.name = name;
