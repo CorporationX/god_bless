285946464e0caad887bac4fa8b5362780c3ad9b6
Andrey <khalyavinandrey@gmail.com>
2023-06-28 21:10:17 +0500

    task class implementing Runnable

---
+++ src/main/java/faang/school/godbless/BigBangTheory/Task.java
@@ -0,0 +1,20 @@
+package faang.school.godbless.BigBangTheory;
+
+import lombok.AllArgsConstructor;
+
+@AllArgsConstructor
+public class Task implements Runnable{
+    private String name;
+    private String task;
+
+    @Override
+    public void run() {
+        System.out.println(name + " has started: " + task);
+        try {
+            Thread.sleep(3000);
+        } catch (InterruptedException e) {
+            System.out.println(task + " has been interrupted");
+        }
+        System.out.println(name + " has ended: " + task);
+    }
+}
