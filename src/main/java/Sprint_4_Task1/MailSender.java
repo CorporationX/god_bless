2516282978241316bd4c2a853b37fcb65633435c
zazozhik <ya.940@mail.ru>
2023-06-28 23:56:16 +0300

    Реализовал условию задачи Microsoft

--- src/main/java/Sprint_4_Task1/MailSender.java
+++ src/main/java/Sprint_4_Task1/MailSender.java
@@ -1,18 +1,24 @@
 package Sprint_4_Task1;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class MailSender {
 
     public static void main(String[] args) throws InterruptedException {
 
-        Thread thread = null;
+        List<Thread> threadList = new ArrayList<>();
         for(int i = 0; i < 5; i++) {
             int startIndex = i * 200 + 1;
             int endIndex = startIndex + 200;
-            thread = new Thread(new SenderRunnable(startIndex,endIndex));
-            thread.start();
-
+            threadList.add( new Thread(new SenderRunnable(startIndex,endIndex)));
         }
-        thread.join(100000);
+        for(Thread thread : threadList){
+            thread.start();
+        }
+        for (Thread thread : threadList){
+            thread.join(100);
+        }
         System.out.println(" Поле");
     }
 }
