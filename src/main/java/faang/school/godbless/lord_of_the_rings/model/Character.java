9c32eb9b7f6394127f8617f5a55fd3b0a2b1a12e
b.mukhtarkhanov <b.mukhtarkhanov>
2023-06-28 16:31:34 +0600

    BC-1024. Lord of the rings. еще изменил

--- src/main/java/faang/school/godbless/lord_of_the_rings/model/Character.java
+++ src/main/java/faang/school/godbless/lord_of_the_rings/model/Character.java
@@ -1,8 +1,11 @@
 package faang.school.godbless.lord_of_the_rings.model;
 
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
-import java.util.function.Consumer;
+import java.util.ListIterator;
+import java.util.function.Function;
+import java.util.function.Predicate;
 
 public class Character {
     private String name;
@@ -16,16 +19,40 @@ public class Character {
         inventory.add(item);
     }
 
+    public void removeItem(Predicate<Item> predicate) {
+        emptyCheck();
+        Iterator<Item> itemIterator = inventory.iterator();
+        while (itemIterator.hasNext()) {
+            Item item = itemIterator.next();
+            if (predicate.test(item)) {
+                itemIterator.remove();
+            }
+        }
+    }
+
+    public void updateItem(Predicate<Item> predicate, Function<Item, Item> function) {
+        emptyCheck();
+        ListIterator<Item> itemIterator = inventory.listIterator();
+        while (itemIterator.hasNext()) {
+            Item item = itemIterator.next();
+            if (predicate.test(item)) {
+                itemIterator.set(function.apply(item));
+            }
+        }
+    }
+
+    private void emptyCheck() {
+        if (inventory == null || inventory.isEmpty()) {
+            throw new IllegalArgumentException("inventory is empty");
+        }
+    }
+
     @Override
     public String toString() {
         return "Character{" +
                 "name='" + name + '\'' +
                 ", inventory=" + inventory.toString() +
                 '}';
-    }
-
-    public List<Item> getInventory() {
-        return inventory;
     }
 
 }
--- src/main/java/faang/school/godbless/lord_of_the_rings/model/InventoryManager.java
+++ src/main/java/faang/school/godbless/lord_of_the_rings/model/InventoryManager.java
@@ -1,7 +1,5 @@
 package faang.school.godbless.lord_of_the_rings.model;
 
-import java.util.Iterator;
-import java.util.ListIterator;
 import java.util.function.Consumer;
 import java.util.function.Function;
 import java.util.function.Predicate;
@@ -9,36 +7,16 @@ import java.util.function.Predicate;
 public class InventoryManager {
 
     public void addItem(Character character, Item item, Consumer<Item> consumer) {
-            character.addItem(item);
-            consumer.accept(item);
+        character.addItem(item);
+        consumer.accept(item);
     }
 
     public void removeItem(Character character, Predicate<Item> predicate) {
-        emptyCheck(character);
-        Iterator<Item> itemIterator = character.getInventory().iterator();
-        while (itemIterator.hasNext()) {
-            Item item = itemIterator.next();
-            if (predicate.test(item)) {
-                itemIterator.remove();
-            }
-        }
+        character.removeItem(predicate);
     }
 
     public void updateItem(Character character, Predicate<Item> predicate, Function<Item, Item> function) {
-        emptyCheck(character);
-        ListIterator<Item> itemIterator = character.getInventory().listIterator();
-        while (itemIterator.hasNext()) {
-            Item item = itemIterator.next();
-            if (predicate.test(item)) {
-                itemIterator.set(function.apply(item));
-            }
-        }
-    }
-
-    private void emptyCheck(Character character) {
-        if (character.getInventory() == null || character.getInventory().isEmpty()) {
-            throw new IllegalArgumentException("inventory is empty");
-        }
+        character.updateItem(predicate, function);
     }
 
 }
