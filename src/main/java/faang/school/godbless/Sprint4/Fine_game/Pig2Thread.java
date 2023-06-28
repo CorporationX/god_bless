6cb7b760fdcdcd3d051d3dcbda75b51be825820c
Alex <matcnev2001@mail.ru>
2023-06-28 14:45:13 +0300

    M4nk1n | Pigs - fine - game

---
+++ src/main/java/faang/school/godbless/Sprint4/Fine_game/Pig2Thread.java
@@ -0,0 +1,7 @@
+package faang.school.godbless.Sprint4.Fine_game;
+
+public class Pig2Thread extends PigThread {
+    public Pig2Thread(String pigName, int material) {
+        super(pigName, material);
+    }
+}
