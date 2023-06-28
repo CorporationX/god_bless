9de93103b55cc3c2846af4d290f7aa49c6f800ef
Viktor Ivanov <avacode78@gmail.com>
2023-06-28 14:59:28 +0700

    small fixes

--- src/main/java/work_at_microsoft/MailSender.java
+++ src/main/java/work_at_microsoft/MailSender.java
@@ -2,9 +2,10 @@ package work_at_microsoft;
 
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
 
 public class MailSender {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
         int messagesPerThread = 200;
 
         ExecutorService executorService = Executors.newFixedThreadPool(5);
@@ -17,9 +18,7 @@ public class MailSender {
 
         executorService.shutdownNow();
 
-        while (!executorService.isTerminated()) {
-            // instead join. Wait until all tasks are completed.
-        }
+        executorService.awaitTermination(1, TimeUnit.MINUTES);
 
         System.out.println("All messages sent successfully.");
     }
