d56524ab3c98f4e5ab01b21093cef040bfc6c265
Andrey <khalyavinandrey@gmail.com>
2023-06-28 13:19:44 +0500

    updated

--- src/main/java/faang/school/godbless/youWorkAtMicrosoft/MailSender.java
+++ src/main/java/faang/school/godbless/youWorkAtMicrosoft/MailSender.java
@@ -6,22 +6,22 @@ import java.util.List;
 public class MailSender {
     public static void main(String[] args) throws InterruptedException {
         final int QUANTITY = 1000;
-        final int THREADQUANITY = 5;
+        final int THREAD_QUANTITY = 5;
         List<Thread> threadList = new ArrayList<>();
 
-        for (int i = 0; i < THREADQUANITY; i++) {
-            int startIndex = (QUANTITY / THREADQUANITY) * i + 1;
-            int endIndex = (QUANTITY / THREADQUANITY) * (i + 1);
+        for (int i = 0; i < THREAD_QUANTITY; i++) {
+            int startIndex = (QUANTITY / THREAD_QUANTITY) * i + 1;
+            int endIndex = (QUANTITY / THREAD_QUANTITY) * (i + 1);
 
             Thread thread = new Thread(
                     new SenderRunnable(startIndex, endIndex)
             );
 
             threadList.add(thread);
+            thread.start();
         }
 
         for (Thread thread : threadList) {
-            thread.start();
             thread.join();
         }
 
