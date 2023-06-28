6b485db1661afe613991ec9aad2afa7318e4ff40
zazozhik <ya.940@mail.ru>
2023-06-28 23:45:18 +0300

    Реализовал условию задачи TelegramBot

--- src/main/java/Sprint_4_Task14/Main.java
+++ src/main/java/Sprint_4_Task14/Main.java
@@ -1,7 +1,21 @@
 package Sprint_4_Task14;
 
-public class Main {
-    public static void main(String[] args) {
+import lombok.SneakyThrows;
 
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
+
+public class Main {
+    @SneakyThrows
+    public static void main(String[] args) {
+        TelegramBot telegramBot = new TelegramBot(5, System.currentTimeMillis());
+        ExecutorService executorService = Executors.newFixedThreadPool(10);
+        for (int i = 0; i < 10; i++) {
+            executorService.execute(() -> telegramBot.sendMessage("Inna super!"));
+        }
+        executorService.shutdown();
+        executorService.awaitTermination(10, TimeUnit.SECONDS);
+        System.out.println("Задание завершено");
     }
 }
--- src/main/java/Sprint_4_Task14/TelegramBot.java
+++ src/main/java/Sprint_4_Task14/TelegramBot.java
@@ -1,22 +1,39 @@
 package Sprint_4_Task14;
 
+import lombok.SneakyThrows;
+
 import java.time.LocalDateTime;
 
 public class TelegramBot {
-    private int REQUEST_LIMIT ;
+    private int REQUEST_LIMIT;
     private int requestCounter;
-    private LocalDateTime lastRequestTime;
+    private long lastRequestTime;
 
-    public TelegramBot(int REQUEST_LIMIT, LocalDateTime lastRequestTime) {
+    public TelegramBot(int REQUEST_LIMIT, long lastRequestTime) {
         this.REQUEST_LIMIT = REQUEST_LIMIT;
         this.requestCounter = 0;
         this.lastRequestTime = lastRequestTime;
     }
-    public void sendMessage(String message) {
-        requestCounter++;
-        if (requestCounter > REQUEST_LIMIT) {
+
+    @SneakyThrows
+    public synchronized void sendMessage(String message) {
+        long startTime = System.currentTimeMillis();
+        long timeTaken = startTime - lastRequestTime;
+        if (timeTaken < 1000) {
+            requestCounter++;
+        } else {
             requestCounter = 0;
-            lastRequestTime = LocalDateTime.now();
-        }else if ()
+            lastRequestTime = 0;
+        }
+        System.out.println(Thread.currentThread().getName() + " requestCounter = " + requestCounter);
+        System.out.println(Thread.currentThread().getName() + " REQUEST_LIMIT = " + REQUEST_LIMIT);
+        if (requestCounter > REQUEST_LIMIT) {
+            System.out.println(Thread.currentThread().getName() + " timeTaken = " + timeTaken);
+            Thread.sleep(1000 - timeTaken);
+            this.wait();
+        } else {
+            this.notifyAll();
+        }
+        System.out.println(LocalDateTime.now() + " " + Thread.currentThread().getName() + " " + message);
     }
 }
