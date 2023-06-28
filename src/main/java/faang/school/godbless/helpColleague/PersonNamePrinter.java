31436789ce09aa067133d04baac95205ddb9d84a
Andrey <khalyavinandrey@gmail.com>
2023-06-28 17:19:33 +0500

    class implements Runnable

---
+++ src/main/java/faang/school/godbless/helpColleague/PersonNamePrinter.java
@@ -0,0 +1,17 @@
+package faang.school.godbless.helpColleague;
+
+import lombok.AllArgsConstructor;
+
+import java.util.List;
+
+@AllArgsConstructor
+public class PersonNamePrinter implements Runnable{
+    private List<Person> people;
+
+    @Override
+    public void run() {
+        people.forEach((person) ->
+                System.out.println(person.getName())
+        );
+    }
+}
