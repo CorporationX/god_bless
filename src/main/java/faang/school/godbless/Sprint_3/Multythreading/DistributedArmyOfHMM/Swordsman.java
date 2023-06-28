a00cad44a16df3101f96998c2154fce7e8a531fc
Ivan Averkov <averkovivan@gmail.com>
2023-06-28 17:24:38 +0300

    DistributedArmy

---
+++ src/main/java/faang/school/godbless/Sprint_3/Multythreading/DistributedArmyOfHMM/Swordsman.java
@@ -0,0 +1,7 @@
+package faang.school.godbless.Sprint_3.Multythreading.DistributedArmyOfHMM;
+
+public class Swordsman extends Unit {
+    public Swordsman(int power) {
+        super(power);
+    }
+}
---
+++ src/main/java/faang/school/godbless/Sprint_3/Multythreading/DistributedArmyOfHMM/Unit.java
@@ -0,0 +1,12 @@
+package faang.school.godbless.Sprint_3.Multythreading.DistributedArmyOfHMM;
+
+import lombok.Getter;
+
+@Getter
+public class Unit {
+    private int power;
+
+    public Unit(int power) {
+        this.power = power;
+    }
+}
