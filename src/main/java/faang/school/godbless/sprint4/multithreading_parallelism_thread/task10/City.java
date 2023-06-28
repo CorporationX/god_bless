6dbaece33cf5f4ac760125398e8edc1e955e32c1
Sergey Gerasimov <czar997@yandex.ru>
2023-06-28 23:12:19 +0300

    фикс: удалена мапа с City

--- src/main/java/faang/school/godbless/sprint4/multithreading_parallelism_thread/task10/City.java
+++ src/main/java/faang/school/godbless/sprint4/multithreading_parallelism_thread/task10/City.java
@@ -2,9 +2,6 @@ package faang.school.godbless.sprint4.multithreading_parallelism_thread.task10;
 
 import lombok.Getter;
 
-import java.util.HashMap;
-import java.util.Map;
-
 @Getter
 public class City {
 
@@ -12,11 +9,8 @@ public class City {
 
     private Location location;
 
-    public static Map<String, Location> durationToCity = new HashMap<>();
-
     public City(String cityName, Location location) {
         this.cityName = cityName;
         this.location = location;
-        durationToCity.put(cityName, location);
     }
 }
