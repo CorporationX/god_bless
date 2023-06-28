b4f3d42fabd63de822e37b013fa5676f52a561dc
Dregid <ufl852@gmail.com>
2023-06-28 17:05:12 +0600

    Убран модификатор в методе run

--- src/main/java/sprint4/harrypotter/Chore.java
+++ src/main/java/sprint4/harrypotter/Chore.java
@@ -3,7 +3,7 @@ package sprint4.harrypotter;
 public record Chore(String chore) implements Runnable {
 
     @Override
-    public synchronized void run() {
+    public void run() {
         System.out.printf("В потоке %s мы выполняем работу по дому: %s\n", Thread.currentThread().getName(), chore);
         try {
             Thread.sleep(2000);
