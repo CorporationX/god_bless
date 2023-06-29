5b85e62cbbbd812a631f8f859f6f2513dfac9327
DiegaLoL <alex.eey@mail.ru>
2023-06-29 18:29:45 +0900

    bc-1811 Helping refactor

--- src/main/java/faang/school/godbless/helping/Main.java
+++ src/main/java/faang/school/godbless/helping/Main.java
@@ -7,19 +7,19 @@ import java.util.concurrent.Executors;
 import java.util.concurrent.TimeUnit;
 
 public class Main {
-    public static void main(String[] args){
+    public static void main(String[] args) {
         List<Person> persons = new ArrayList<>();
         int threadCount = 8;
         int personsCount = 10000;
         int personsCountPerThread = personsCount / threadCount;
-        for (int i = 1; i<=personsCount;i++){
+        for (int i = 1; i <= personsCount; i++) {
             persons.add(new Person(i));
         }
         ExecutorService threadBuild = Executors.newFixedThreadPool(threadCount);
         for (int i = 0; i < threadCount; i++) {
             int start = i * personsCountPerThread;
             int end = start + personsCountPerThread;
-            threadBuild.submit(new PersonNamePrinter(persons.subList(start,end)));
+            threadBuild.submit(new PersonNamePrinter(persons.subList(start, end)));
         }
         threadBuild.shutdown();
         try {
--- src/main/java/faang/school/godbless/helping/PersonNamePrinter.java
+++ src/main/java/faang/school/godbless/helping/PersonNamePrinter.java
@@ -2,15 +2,15 @@ package faang.school.godbless.helping;
 
 import java.util.List;
 
-public class PersonNamePrinter implements Runnable{
+public class PersonNamePrinter implements Runnable {
     List<Person> persons;
 
-    public PersonNamePrinter(List<Person> persons){
+    public PersonNamePrinter(List<Person> persons) {
         this.persons = persons;
     }
 
     @Override
-    public void run(){
+    public void run() {
         persons.forEach(person -> System.out.println(person.getName()));
     }
 }
