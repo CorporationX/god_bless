07d3055b65727588d07703aabbfdf55eeb273e99
Kirill Soshnikov <soshnikov17@gmail.com>
2023-06-29 10:55:24 +0300

    Реализованы все методы со стримами и тесты для них

--- src/main/java/faang/school/godbless/train_stream_one/StreamMethods.java
+++ src/main/java/faang/school/godbless/train_stream_one/StreamMethods.java
@@ -10,18 +10,34 @@ public class StreamMethods {
     }
 
     public static int getMaxListValue(List<Integer> list) {
-        return list.stream().max((x, y) -> Integer.compare(x, y)).get();
+        return list.stream().max(Integer::compare).get();
     }
 
     public static int getAverageListValue(List<Integer> list) {
-        return list.stream().reduce((x, y) -> (x + y)).get()/list.size();
+        return list.stream().reduce(Integer::sum).get() / list.size();
     }
 
     public static List<String> filterListBySubstring(List<String> list) {
-        return list.stream().filter((x)->x.contains("ok")).collect(Collectors.toList());
+        return list.stream().filter((x) -> x.contains("ok")).collect(Collectors.toList());
     }
 
     public static List<String> getFilteredElementCount(List<String> list) {
-        return list.stream().filter((x)->x.startsWith("K")).collect(Collectors.toList());
+        return list.stream().filter((x) -> x.startsWith("K")).collect(Collectors.toList());
+    }
+
+    public static List<String> filterListByItemSize(List<String> list) {
+        return list.stream().sorted(Comparator.comparingInt(String::length)).collect(Collectors.toList());
+    }
+
+    public static boolean isAllItemMatch(List<String> list) {
+        return list.stream().allMatch(x -> x.length() > 2);
+    }
+
+    public static int getMinItem(List<Integer> list) {
+        return list.stream().filter((x) -> x > 5).min(Comparator.comparingInt(x -> x)).get();
+    }
+
+    public static List<Integer> getLengthList(List<String> list) {
+        return list.stream().map(String::length).toList();
     }
 }
--- src/test/java/faang/school/godbless/train_stream_one/StreamMethodsTest.java
+++ src/test/java/faang/school/godbless/train_stream_one/StreamMethodsTest.java
@@ -30,4 +30,27 @@ public class StreamMethodsTest {
     public void testFilteredElementCount() {
         Assertions.assertEquals(3, StreamMethods.getFilteredElementCount(List.of("Kirill", "Key", "Oko", "Knife")).size());
     }
+
+    @Test
+    public void testFilterListByItemSize() {
+        Assertions.assertEquals("K", StreamMethods.filterListByItemSize(List.of("Dog", "K", "Length", "Accessibility", "River")).get(0));
+        Assertions.assertEquals("Accessibility", StreamMethods.filterListByItemSize(List.of("Dog", "K", "Length", "Accessibility", "River")).get(4));
+    }
+
+    @Test
+    public void testIsAllMatch() {
+        Assertions.assertEquals(false, StreamMethods.isAllItemMatch(List.of("Dog", "K", "Length", "Accessibility", "River")));
+        Assertions.assertEquals(true, StreamMethods.isAllItemMatch(List.of("Dog", "Key", "Length", "Accessibility", "River")));
+    }
+
+    @Test
+    public void tesMinItem() {
+        Assertions.assertEquals(7, StreamMethods.getMinItem(List.of(1, 2, 35, 4, 10, 7)));
+    }
+
+    @Test
+    public void testListLength() {
+        Assertions.assertEquals(3, StreamMethods.getLengthList(List.of("Dog", "K", "Length", "Accessibility", "River")).get(0));
+        Assertions.assertEquals(6, StreamMethods.getLengthList(List.of("Dog", "K", "Length", "Accessibility", "River")).get(2));
+    }
 }
