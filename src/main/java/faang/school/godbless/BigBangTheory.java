4e2e58dd913c013eb8960a35cf08206f7d0e9170
Stepan Palkov <step.palkov@gmail.com>
2023-06-29 04:03:34 +0300

    complete task (BigBangTheory)

---
+++ src/main/java/faang/school/godbless/BigBangTheory.java
@@ -0,0 +1,32 @@
+package faang.school.godbless;
+
+import java.util.List;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
+
+public class BigBangTheory {
+    public static void main(String[] args) {
+        ExecutorService executor = Executors.newFixedThreadPool(4);
+        List<Task> tasks = List.of(
+                new Task("Шелдон", "подготовка теории"),
+                new Task("Леонард", "моделирование эксперимента"),
+                new Task("Говард", "разработка инструментов"),
+                new Task("Раджеш", "анализ данных")
+        );
+
+        for (Task task : tasks) {
+            executor.execute(task);
+        }
+        executor.shutdown();
+
+        try {
+            executor.awaitTermination(1, TimeUnit.MINUTES);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        System.out.println();
+        System.out.println("All tasks done");
+    }
+}
