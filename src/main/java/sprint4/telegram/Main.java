d16ba1b26b46b72ec9ef10ad12e6af3933c6aa75
Dregid <ufl852@gmail.com>
2023-06-29 15:23:00 +0600

    Не рабочий вариант решения задачи

--- src/main/java/sprint4/telegram/Main.java
+++ src/main/java/sprint4/telegram/Main.java
@@ -1,11 +1,20 @@
 package sprint4.telegram;
 
-import java.util.Date;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 public class Main {
     private static final int REQUEST_LIMIT = 5;
-    private static final TelegramBot BOT = new TelegramBot(REQUEST_LIMIT, new Date());
-    public static void main(String[] args) {
+    private static final int THREAD_LIMIT = 3;
+    private static final TelegramBot BOT = new TelegramBot(REQUEST_LIMIT, System.currentTimeMillis());
+    private static final String format = "Message number: %d";
 
+    public static void main(String[] args) {
+        ExecutorService service = Executors.newFixedThreadPool(THREAD_LIMIT);
+        for (int i = 0; i < 100; i++) {
+            int num = i;
+            service.execute(() -> BOT.sendMessage(String.format(format, num)));
+        }
+        service.shutdown();
     }
 }
--- src/main/java/sprint4/telegram/TelegramBot.java
+++ src/main/java/sprint4/telegram/TelegramBot.java
@@ -3,21 +3,41 @@ package sprint4.telegram;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 
-import java.util.Date;
-
 @Data
 @AllArgsConstructor
 public class TelegramBot {
+    private static final int SECOND = 1000;
     private final int requestLimit;
-    private Date lastRequestTime;
+    private long lastRequestTime;
     private int requestCounter;
 
-    public TelegramBot(int requestLimit, Date lastRequestTime) {
+    public TelegramBot(int requestLimit, long lastRequestTime) {
         this.requestLimit = requestLimit;
         this.lastRequestTime = lastRequestTime;
     }
 
-    public void sendMessage(String message) {
+    public synchronized void sendMessage(String message) {
+        waitIfLimitIsExceeded();
+        resetRequestTimeAndCounter();
+        requestCounter++;
+        System.out.printf("Message sent via Telegram API: %s\n", message);
 
+    }
+
+    private void waitIfLimitIsExceeded() {
+        if (requestCounter == requestLimit) {
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                System.out.println("Work interrupted");
+            }
+        }
+    }
+
+    private void resetRequestTimeAndCounter() {
+        if ((lastRequestTime - System.currentTimeMillis()) >= SECOND) {
+            lastRequestTime = System.currentTimeMillis();
+            requestCounter = 0;
+        }
     }
 }
