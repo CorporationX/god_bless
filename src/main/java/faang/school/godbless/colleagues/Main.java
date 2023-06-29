88a205731b163a597f9ba09bdd34d6f6a5376b4b
Anton <Agressive.cat@yandex.ru>
2023-06-29 20:25:56 +0300

    now it's correct

--- src/main/java/faang/school/godbless/colleagues/Main.java
+++ src/main/java/faang/school/godbless/colleagues/Main.java
@@ -7,26 +7,25 @@ import java.util.concurrent.Executors;
 import java.util.concurrent.TimeUnit;
 
 public class Main {
-
     public static void main(String[] args) throws InterruptedException {
         List<Person> people = new ArrayList<>();
         ExecutorService executor = Executors.newFixedThreadPool(4);
 
-        for (int i = 0; i < 10000; i++) {
+        int list = 10000;
+        for (int i = 0; i < list; i++) {
             people.add(new Person(i));
         }
 
-        List<Person> batch1 = people.subList(1, 2500);
-        List<Person> batch2 = people.subList(2500, 5000);
-        List<Person> batch3 = people.subList(5000, 7500);
-        List<Person> batch4 = people.subList(7500, 10000);
-
+        int sizePersonsInPrinter  = people.size() / 4;
+        int startIndex = 0;
+        int endIndex = sizePersonsInPrinter - 1;
         for (int i = 0; i < 4; i++) {
+            List<Person> batch1 = people.subList(1, sizePersonsInPrinter );
             executor.submit(new PersonNamePrinter(batch1));
-            executor.submit(new PersonNamePrinter(batch2));
-            executor.submit(new PersonNamePrinter(batch3));
-            executor.submit(new PersonNamePrinter(batch4));
+            startIndex += sizePersonsInPrinter;
+            endIndex += sizePersonsInPrinter;
         }
+
         executor.shutdown();
         executor.awaitTermination(10, TimeUnit.SECONDS);
         executor.shutdownNow();
