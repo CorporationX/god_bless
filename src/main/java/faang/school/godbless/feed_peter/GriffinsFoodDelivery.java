0046d474171404f6fd7592448f2f1cbc017406b8
Tamerlan <100015209+Devilserk@users.noreply.github.com>
2023-06-29 20:10:59 +0600

    Задание Накормите Питера выполнено (#1428)

---
+++ src/main/java/faang/school/godbless/feed_peter/GriffinsFoodDelivery.java
@@ -0,0 +1,17 @@
+package faang.school.godbless.feed_peter;
+
+import java.util.Random;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+public class GriffinsFoodDelivery {
+    public static void main(String[] args) {
+        ExecutorService deliveryService = Executors.newFixedThreadPool(5);
+        String[] characterNames = {"Peter", "Lois", "Meg", "Chris", "Stewie"};
+        for (String character : characterNames) {
+            int foodAmount = new Random().nextInt(10);
+            deliveryService.submit(new FoodDeliveryTask(character, foodAmount));
+        }
+        deliveryService.shutdown();
+    }
+}
