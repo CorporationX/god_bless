7b00986063fc8e190b22dd0aa865b8ec368ce426
Pashkevich <ppavel012100@gmail.com>
2023-06-29 15:39:44 +0200

    rework Griffins Food task

--- src/main/java/faang/school/godbless/sprint3/griffin/FoodDeliveryTask.java
+++ src/main/java/faang/school/godbless/sprint3/griffin/FoodDeliveryTask.java
@@ -10,17 +10,21 @@ import java.util.Random;
 public class FoodDeliveryTask implements Runnable {
     private String character;
     private int foodAmount;
+
     @Override
     public void run() {
-        System.out.println(String.format("%s got %d %s.", character, foodAmount, getFoodType()));
+        String foodType = getFoodType();
+
+        System.out.println(String.format("%s got %d %s.", character, foodAmount, foodType));
         try {
             Thread.sleep(new Random().nextInt(5000));
         } catch (InterruptedException e) {
             throw new RuntimeException(e);
         }
 
-        System.out.println(String.format("%s got food and eats %d %s.", character, foodAmount, getFoodType()));
+        System.out.println(String.format("%s got food and eats %d %s.", character, foodAmount, foodType));
     }
+
     private String getFoodType() {
         String[] foodTypes = {"pizza", "burger", "hot dog", "chicken wings", "taco"};
         return foodTypes[new Random().nextInt(foodTypes.length)];
