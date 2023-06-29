4adef847bbabfe857881db36b3625bd47b855a52
chaykinks <chaykinks@gmail.com>
2023-06-29 14:35:48 +0500

    добавил класс NotificationManager

--- src/main/java/faang/school/godbless/Sprint3/Task_2_Meta/Notification.java
+++ src/main/java/faang/school/godbless/Sprint3/Task_2_Meta/Notification.java
@@ -1,6 +1,19 @@
 package faang.school.godbless.Sprint3.Task_2_Meta;
 
 public class Notification {
-    String type;
-    String message;
+    private String type;
+    private String message;
+
+    public String getType() {
+        return type;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public Notification(String type, String message) {
+        this.type = type;
+        this.message = message;
+    }
 }
