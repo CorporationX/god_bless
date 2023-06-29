b669d7410761bfd532dfd2fc348cb66b40e6276e
M1khail <mishalion888@gmail.com>
2023-06-30 00:22:03 +0200

    work in Microsoft - description

--- src/main/java/faang/school/godbless/workInMicrosoft/MailSender.java
+++ src/main/java/faang/school/godbless/workInMicrosoft/MailSender.java
@@ -3,9 +3,10 @@ package faang.school.godbless.workInMicrosoft;
 public class MailSender {
     public static void main(String[] args) throws InterruptedException {
         int lettersForThread = 200;
-        for (int i = 0; i < 5; i++) {
+        for (int i = 0; i < 5; i++) { // запускаем 5 потоков
             var thread = new Thread(new SenderRunnable(lettersForThread * i, lettersForThread * i + lettersForThread));
-            thread.start();
+            // каждому потоку присваиваем нужное кол-во писем
+            thread.start(); // запуск потоков
             thread.join();
         }
     }
