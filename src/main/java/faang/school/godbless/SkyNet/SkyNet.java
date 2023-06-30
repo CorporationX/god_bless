68265e78812bc12016d6d377953508949666a65d
An Dima <an.dima@slexn.com>
2023-06-30 10:43:23 +0900

    added third robot

--- src/main/java/faang/school/godbless/SkyNet/SkyNet.java
+++ src/main/java/faang/school/godbless/SkyNet/SkyNet.java
@@ -4,11 +4,14 @@ public class SkyNet {
     public static void main(String[] args) {
         Robot robot1 = new Robot("Target1");
         Robot robot2 = new Robot("Target2");
+        Robot robot3 = new Robot("Target1");
 
         Thread thread1 = new Thread(robot1::attack);
         Thread thread2 = new Thread(robot2::attack);
+        Thread thread3 = new Thread(robot3::attack);
 
         thread1.start();
         thread2.start();
+        thread3.start();
     }
 }
