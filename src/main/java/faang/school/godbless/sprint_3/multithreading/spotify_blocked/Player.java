8951181c56f27066faa3e0cd6374387823b9b66d
OneWayMy <zhenya.ivashchenko.9494@gmail.com>
2023-06-30 16:50:56 -0700

    MultiThreading_Spotify

---
+++ src/main/java/faang/school/godbless/sprint_3/multithreading/spotify_blocked/Player.java
@@ -0,0 +1,37 @@
+package faang.school.godbless.sprint_3.multithreading.spotify_blocked;
+
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
+public class Player {
+    private final Lock lock = new ReentrantLock();
+    private boolean isPlaying = false;
+
+    public void play() {
+        synchronized (lock) {
+            isPlaying = true;
+            System.out.println("Музыка включена");
+        }
+    }
+
+    public void pause() {
+        synchronized (lock) {
+            isPlaying = false;
+            System.out.println("Музыка на паузе");
+        }
+    }
+
+    public void skip() {
+        synchronized (lock) {
+            isPlaying = false;
+            System.out.println("Включаю следующую музыку");
+        }
+    }
+
+    public void previous() {
+        synchronized (lock) {
+            isPlaying = false;
+            System.out.println("Предыдущая музыка");
+        }
+    }
+}
