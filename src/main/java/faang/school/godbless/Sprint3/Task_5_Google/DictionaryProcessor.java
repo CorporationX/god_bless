3f9347a3067f3f94c937605a55324ec090e74b9c
chaykinks <chaykinks@gmail.com>
2023-06-30 23:00:38 +0500

    создал класс DictionaryProcessor и метод processWord

---
+++ src/main/java/faang/school/godbless/Sprint3/Task_5_Google/DictionaryProcessor.java
@@ -0,0 +1,27 @@
+package faang.school.godbless.Sprint3.Task_5_Google;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.function.BiConsumer;
+
+public class DictionaryProcessor {
+
+    public static void processWord (String word, String translation, BiConsumer<String, String> addWordToDictionary) {
+        addWordToDictionary.accept(word, translation);
+    }
+
+    public static void main(String[] args) {
+        DictionaryProcessor dictionaryProcessor = new DictionaryProcessor();
+
+        // Создание словаря
+        Map<String, String> dictionary = new HashMap<>();
+        // Создание BiConsumer для сохранения слова и его перевода в словарь
+        BiConsumer<String, String> addWordToDictionary = (word, translation) -> dictionary.put(word, translation);
+        // Добавление слов и их переводов
+        dictionaryProcessor.processWord("привет", "hello", addWordToDictionary);
+        dictionaryProcessor.processWord("мир", "world", addWordToDictionary);
+        dictionaryProcessor.processWord("программирование", "programming", addWordToDictionary);
+
+        System.out.println("Словарь: " + dictionary);
+    }
+}
