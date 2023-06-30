b0287b7f5e7a160e6d60854cb2186cbd088a353d
M1khail <mishalion888@gmail.com>
2023-06-30 12:29:15 +0200

    finally normal game - update

--- src/main/java/faang/school/godbless/finallyNormalGame/ThreeLittlePigs.java
+++ src/main/java/faang/school/godbless/finallyNormalGame/ThreeLittlePigs.java
@@ -6,11 +6,12 @@ import java.util.List;
 public class ThreeLittlePigs {
     public static void main(String[] args) throws InterruptedException {
         List<String> namesOfPigs = Arrays.asList("Ниф-Ниф", "Нуф-Нуф", "Наф-Наф");
+        List<Integer> material = Arrays.asList(1, 2, 3);
         for (int i = 0; i < 3; i++) {
-            var thread = new Thread(new PigThread(namesOfPigs.get(i), i));
+            var thread = new Thread(new PigThread(namesOfPigs.get(i), material.get(i)));
             thread.start();
             thread.join();
-            System.out.println("дом построен для поросенка " + namesOfPigs.get(i));
+            System.out.println("дом построен для поросенка: " + namesOfPigs.get(i) + " ,из материала: " + material.get(i));
         }
         System.out.println("Игра завершена!");
     }
