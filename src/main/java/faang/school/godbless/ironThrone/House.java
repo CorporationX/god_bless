8f336a9a7f77abe1cd1c02bb2161e332545a0359
Rustam Nigmatullin <rustamnigma@mail.ru>
2023-06-30 13:00:26 +0500

    BC-2166-IronThrone

---
+++ src/main/java/faang/school/godbless/ironThrone/House.java
@@ -0,0 +1,36 @@
+package faang.school.godbless.ironThrone;
+
+import lombok.Getter;
+import lombok.NonNull;
+import lombok.Setter;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+@Getter
+@Setter
+public class House {
+    @NonNull
+    private String name;
+    private List<String> roles = new ArrayList<>();
+    private int availableRoles;
+
+    public House(@NonNull String name) {
+        this.name = name;
+    }
+
+    public void addRole(){
+        synchronized (this){
+            availableRoles--;
+            System.out.println("Количество доступных ролей : " + availableRoles);
+        }
+
+    }
+
+    public void removeRole(String name){
+        synchronized (this) {
+            availableRoles++;
+            System.out.println("Количество доступных ролей: " + availableRoles);
+        }
+    }
+}
