f68dd751101e243d1b6557a92310a0e0bef417c4
Pavel Voedilov <voedilov.p@gmail.com>
2023-06-30 05:04:29 +0200

    Implement Calculator interface, calculate method in Application.java & CalculatorTest to ensure functionality

--- src/main/java/faang/school/godbless/Application.java
+++ src/main/java/faang/school/godbless/Application.java
@@ -1,7 +1,14 @@
 package faang.school.godbless;
 
+import faang.school.godbless.calculator.Calculator;
+
+import java.util.List;
+
 public class Application {
     public static void main(String... args) {
+    }
 
+    public static int calculate(List<Integer> nums, Calculator calculator) throws IllegalArgumentException {
+        return nums.stream().reduce(nums.get(0), calculator::calculate);
     }
 }
