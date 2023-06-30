96db9e304810f9f69dace02d2768e835fa98a803
chaykinks <chaykinks@gmail.com>
2023-06-30 18:22:05 +0500

    добавил метод фильтрации и сортировки строк

--- src/main/java/faang/school/godbless/Sprint3/Task_17_StreamAPI_2/StreamAPI_2.java
+++ src/main/java/faang/school/godbless/Sprint3/Task_17_StreamAPI_2/StreamAPI_2.java
@@ -6,7 +6,7 @@ import java.util.stream.Collectors;
 
 public class StreamAPI_2 {
 
-    public static List<List<Integer>> findUniquePairs (List<Integer> numbers, int sum) {
+    public static List<List<Integer>> findUniquePairs(List<Integer> numbers, int sum) {
         List<List<Integer>> pairs = new ArrayList<>();
 
         numbers.stream()
@@ -18,16 +18,16 @@ public class StreamAPI_2 {
         return pairs;
     }
 
-    public static List<String> showCapitals (Map<String, String> countries) {
+    public static List<String> showCapitals(Map<String, String> countries) {
 
-        return  countries.entrySet().stream()
+        return countries.entrySet().stream()
                 .sorted(Map.Entry.comparingByKey())
                 .map(Map.Entry::getValue)
                 .toList();
 
     }
 
-    public static List<String> filterAndSortStrings (List<String> strings, char letter) {
+    public static List<String> filterAndSortStrings(List<String> strings, char letter) {
 
         return strings.stream()
                 .filter(string -> string.charAt(0) == letter)
@@ -35,9 +35,17 @@ public class StreamAPI_2 {
                 .toList();
     }
 
-    public static Map<String, Double> calculateAverageSalary (List<Employee> employees) {
+    public static Map<String, Double> calculateAverageSalary(List<Employee> employees) {
         return employees.stream()
                 .collect(Collectors.groupingBy(Employee::getDepartment,
                         Collectors.averagingDouble(Employee::getSalary)));
     }
+
+    public static List<String> filterAndSortStrings(List<String> strings, String alphabet) {
+        return strings.stream()
+                .filter(string -> string.chars().allMatch(character -> alphabet.contains(String.valueOf((char) character))))
+                .sorted(Comparator.comparing(String::length))
+                .toList();
+    }
 }
+
