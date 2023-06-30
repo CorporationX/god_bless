0e78111f3b3aff418ec9ba2a952e3b87240390fc
Andrey <khalyavinandrey@gmail.com>
2023-06-30 17:43:26 +0500

    added run method

--- src/main/java/faang/school/godbless/finallyGoodGame/PigThread.java
+++ src/main/java/faang/school/godbless/finallyGoodGame/PigThread.java
@@ -8,4 +8,21 @@ import lombok.Getter;
 public class PigThread extends Thread{
     private String pigName;
     private String material;
+
+    @Override
+    public void run() {
+        System.out.println(
+                this.getPigName() + " started building a house with " + this.getMaterial()
+        );
+
+        try {
+            Thread.sleep(3000);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+
+        System.out.println(
+                this.getPigName() + " has builded a house with " + this.getMaterial()
+        );
+    }
 }
