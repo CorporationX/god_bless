164531bc3eef1c76221c407397c8142cbf457ed7
OneWayMy <zhenya.ivashchenko.9494@gmail.com>
2023-07-01 13:13:03 -0700

    MultiThreading_Bro_Force_PR_changes

--- src/main/java/faang/school/godbless/sprint_3/multithreading/working_in_microsoft/MailSender.java
+++ src/main/java/faang/school/godbless/sprint_3/multithreading/working_in_microsoft/MailSender.java
@@ -1,14 +1,18 @@
 package faang.school.godbless.sprint_3.multithreading.working_in_microsoft;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class MailSender {
+    private static final Integer THREAD_CNT = 5;
+    private static final Integer BATCH_SIZE = 200;
+
     public static void main(String[] args) throws InterruptedException {
-        List<String> list = List.of("Hello World", "The battle begin!", "It's Jonny", "The password is 23355", "We expect more");
-        for (int i = 0; i < 5; i++) {
-            int startIndex = i * 200 + 1;
-            int endindex = (i + 1) * 200;
-            Thread thread = new Thread(new SenderRunnable(startIndex, endindex, list.get(i)));
+        List<String> list = new ArrayList<>();
+        for (int i = 0; i < THREAD_CNT; i++) {
+            int startIndex = i * BATCH_SIZE;
+            int endIndex = startIndex + BATCH_SIZE - 1;
+            Thread thread = new Thread(new SenderRunnable(startIndex, endIndex));
             thread.start();
             thread.join();
         }
--- src/main/java/faang/school/godbless/sprint_3/multithreading/working_in_microsoft/SenderRunnable.java
+++ src/main/java/faang/school/godbless/sprint_3/multithreading/working_in_microsoft/SenderRunnable.java
@@ -3,18 +3,16 @@ package faang.school.godbless.sprint_3.multithreading.working_in_microsoft;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 
-
 @Data
 @AllArgsConstructor
 public class SenderRunnable implements Runnable {
     private int startIndex;
     private int endIndex;
-    private String message;
 
     @Override
     public void run() {
         for (int i = startIndex; i < endIndex; i++) {
-            System.out.println(String.format(" %s -> has been sent successfully", message));
+            System.out.printf("%d message -> has been sent successfully%n", i);
         }
     }
 }
