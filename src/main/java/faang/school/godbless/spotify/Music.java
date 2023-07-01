77ede67a0b5a7693b213b943ce741a29fc8edbd6
Andrey <khalyavinandrey@gmail.com>
2023-07-01 16:23:31 +0500

    music class

---
+++ src/main/java/faang/school/godbless/spotify/Music.java
@@ -0,0 +1,18 @@
+package faang.school.godbless.spotify;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+public class Music {
+    public static void main(String[] args) {
+        Player player = new Player();
+        ExecutorService executorService = Executors.newFixedThreadPool(4);
+
+        executorService.execute(player::play);
+        executorService.execute(player::pause);
+        executorService.execute(player::skip);
+        executorService.execute(player::previous);
+
+        executorService.shutdown();
+    }
+}
