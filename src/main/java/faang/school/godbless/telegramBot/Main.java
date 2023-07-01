a6f991762b849945fd5ba1248c7ff17d9922408e
Andrey <khalyavinandrey@gmail.com>
2023-07-01 15:01:29 +0500

    updated main class with multithreading and timings

--- src/main/java/faang/school/godbless/telegramBot/Main.java
+++ src/main/java/faang/school/godbless/telegramBot/Main.java
@@ -2,18 +2,32 @@ package faang.school.godbless.telegramBot;
 
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
 
 public class Main {
     public static void main(String[] args) {
         TelegramBot telegramBot = new TelegramBot(5);
-        ExecutorService executorService = Executors.newFixedThreadPool(6);
+        ExecutorService executorService = Executors.newFixedThreadPool(8);
+
+        long start = System.currentTimeMillis();
 
         executorService.execute(() -> telegramBot.sendMessage("Hello"));
         executorService.execute(() -> telegramBot.sendMessage("World"));
         executorService.execute(() -> telegramBot.sendMessage("!"));
         executorService.execute(() -> telegramBot.sendMessage("Are"));
         executorService.execute(() -> telegramBot.sendMessage("There"));
+        executorService.execute(() -> telegramBot.sendMessage("Anyone"));
+        executorService.execute(() -> telegramBot.sendMessage("?"));
+        executorService.execute(() -> telegramBot.sendMessage("?"));
 
         executorService.shutdown();
+
+        try {
+            executorService.awaitTermination(2, TimeUnit.SECONDS);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+
+        System.out.println("Time to send messages: " + (System.currentTimeMillis() - start));
     }
 }
