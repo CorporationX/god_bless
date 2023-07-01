417e05d49a81b727606f7f41a3fdf39520f8b0b4
ZLATA <585zlata@mail.ru>
2023-07-01 13:17:22 +0300

    new commit

--- src/main/java/Sprint_3_Task_Analiz/AnalysisUsers.java
+++ src/main/java/Sprint_3_Task_Analiz/AnalysisUsers.java
@@ -5,6 +5,7 @@ import java.util.Comparator;
 import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class AnalysisUsers {
     //List <UserAction> userActions = new ArrayList<>();
@@ -27,11 +28,12 @@ public class AnalysisUsers {
         return userActions.stream()
                 .filter(t1 -> t1.getActionType().equals(ActionType.POST) || t1.getActionType().equals(ActionType.COMMENT))
                 .filter(t3 -> t3.getContent().startsWith("#"))
+                //.toList().size();
                 .collect(Collectors.groupingBy(UserAction::getContent, Collectors.counting()))
                 .entrySet()
                 .stream()
-                .limit(5)
                 .sorted(Map.Entry.comparingByValue(Comparator.reverseOrder()))
+                .limit(5)
                 .toList();
 
 
@@ -56,16 +58,14 @@ public class AnalysisUsers {
                 new UserAction(1, "user1", ActionType.POST, LocalDate.now(), "p#ost"),
                 new UserAction(1, "user1", ActionType.POST, LocalDate.now(), "#BC"),
                 new UserAction(1, "user1", ActionType.POST, LocalDate.now(), "post"),
-                new UserAction(1, "user1", ActionType.POST, LocalDate.now(), "post"),
+                new UserAction(1, "user1", ActionType.COMMENT, LocalDate.now(), "#post"),
                 new UserAction(1, "user1", ActionType.COMMENT, LocalDate.now(), "#post"),
                 new UserAction(1, "user1", ActionType.POST, LocalDate.now(), "post"),
-                new UserAction(2, "user2", ActionType.LIKE, LocalDate.now(), "#comment"),
                 new UserAction(2, "user2", ActionType.COMMENT, LocalDate.now(), "#Bc"),
                 new UserAction(3, "user3", ActionType.LIKE, LocalDate.now(), "#like"),
                 new UserAction(4, "user4", ActionType.COMMENT, LocalDate.now(), "#important"),
                 new UserAction(4, "user4", ActionType.LIKE, LocalDate.now(), "share"),
                 new UserAction(4, "user4", ActionType.SHARE, LocalDate.now(), "#share"),
-                new UserAction(4, "user4", ActionType.SHARE, LocalDate.now(), "share"),
                 new UserAction(5, "user5", ActionType.POST, LocalDate.now(), "#post"),
                 new UserAction(5, "user5", ActionType.POST, LocalDate.now(), "p #ost"),
                 new UserAction(5, "user5", ActionType.POST, LocalDate.now(), "#post"),
@@ -84,9 +84,11 @@ public class AnalysisUsers {
                 new UserAction(7, "user7", ActionType.LIKE, LocalDate.now(), "like"),
                 new UserAction(8, "user8", ActionType.SHARE, LocalDate.now(), "share"),
                 new UserAction(8, "user8", ActionType.SHARE, LocalDate.now(), "#share"),
-                new UserAction(8, "user8", ActionType.SHARE, LocalDate.now(), "#share"),
                 new UserAction(9, "user9", ActionType.POST, LocalDate.now(), "#sumer"),
-                new UserAction(10, "user10", ActionType.LIKE, LocalDate.now(), "#comment"),
+//                new UserAction(10, "user10", ActionType.LIKE, LocalDate.now(), "#comment")
+                new UserAction(10, "user10", ActionType.COMMENT, LocalDate.now(), "#IMPORTANT"),
+                new UserAction(10, "user10", ActionType.COMMENT, LocalDate.now(), "#IMPORTANT"),
+                new UserAction(10, "user10", ActionType.COMMENT, LocalDate.now(), "#IMPORTANT"),
                 new UserAction(10, "user10", ActionType.COMMENT, LocalDate.now(), "#IMPORTANT"),
                 new UserAction(11, "user11", ActionType.LIKE, LocalDate.now(), "like"),
                 new UserAction(12, "user12", ActionType.LIKE, LocalDate.now(), "share"),
