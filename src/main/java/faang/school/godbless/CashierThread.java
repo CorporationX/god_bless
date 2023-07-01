a5017a63ed9eae78cf886c97f3c3ca321aa6ec77
Adil Ilchubaev <adil.200191@gmail.com>
2023-07-01 21:11:49 +0600

    Обертка для класса thread для моделирования работы кассиров в магазине

---
+++ src/main/java/faang/school/godbless/CashierThread.java
@@ -0,0 +1,35 @@
+package faang.school.godbless;
+
+import java.util.List;
+
+public class CashierThread extends Thread{
+    private int cashierId;
+    private int[] customerItems;
+
+    public CashierThread(int cashierId, int[] customerItems)
+    {
+        this.cashierId = cashierId;
+        this.customerItems = customerItems;
+    }
+
+    @Override
+    public void run() {
+        int totalItems = 0;
+        double totalPrice = 0;
+        for(int item : customerItems)
+        {
+            totalItems++;
+            totalPrice += item;
+            System.out.println(Thread.currentThread().getName() + " thread | Cashier " + cashierId + " processed item: " + item);
+            try{
+                Thread.sleep(1000);
+            }
+            catch(InterruptedException e)
+            {
+                e.printStackTrace();
+            }
+        }
+        System.out.println(Thread.currentThread().getName() + " thread | Cashier " + cashierId + " processed " + totalItems + " items with a total price of " + totalPrice);
+    }
+
+}
