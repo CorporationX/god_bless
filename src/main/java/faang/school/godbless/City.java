696e8e88f41603971de973c888a16d08cb7007c3
Adil Ilchubaev <adil.200191@gmail.com>
2023-07-01 22:13:52 +0600

    В итоге 2 потока наиболее эффективнее, 1 чуть больше, а если более потоков нет смысла

---
+++ src/main/java/faang/school/godbless/City.java
@@ -0,0 +1,25 @@
+package faang.school.godbless;
+
+public class City {
+    private String name;
+    private Location location;
+    private int distance;
+
+    public City(String name, Location location, int distance) {
+        this.name = name;
+        this.location = location;
+        this.distance = distance;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public Location getLocation() {
+        return location;
+    }
+
+    public int getDistance() {
+        return distance;
+    }
+}
---
+++ src/main/java/faang/school/godbless/Monster.java
@@ -0,0 +1,19 @@
+package faang.school.godbless;
+
+public class Monster {
+    private String name;
+    private Location location;
+
+    public Monster(String name, Location location) {
+        this.name = name;
+        this.location = location;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public Location getLocation() {
+        return location;
+    }
+}
