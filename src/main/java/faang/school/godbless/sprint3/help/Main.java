4a24476e922c3a7f66b3a0357f6ccbd424c8db83
Pashkevich <ppavel012100@gmail.com>
2023-07-01 20:40:13 +0200

    rework Help task

--- src/main/java/faang/school/godbless/sprint3/help/Main.java
+++ src/main/java/faang/school/godbless/sprint3/help/Main.java
@@ -4,9 +4,10 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
 
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
         List<Person> personList = new ArrayList<>(10000);
         for(int i = 0; i < 10000; i++){
             String name = "Person" + i;
@@ -18,13 +19,15 @@ public class Main {
         for (int i = 0; i < 10; i++) {
             int startIndex = i;
             int endIndex = (i + 1) * 1000;
-            List<Person> batch = personList.subList(startIndex, endIndex);
             startIndex = i + 1000;
+            List<Person> batch = personList.subList(startIndex, endIndex);
 
             for (Person person : batch) {
                 executorService.submit(new PersonNamePrinter(person));
             }
         }
         executorService.shutdown();
+        executorService.awaitTermination(10, TimeUnit.SECONDS);;
+        System.out.println("All done!");
     }
 }
