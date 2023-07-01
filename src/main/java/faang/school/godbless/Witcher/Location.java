caffb0983c66446c8b16b30cddbbb6b6d259b382
Andrey <khalyavinandrey@gmail.com>
2023-07-01 11:12:42 +0500

    location class

---
+++ src/main/java/faang/school/godbless/Witcher/Location.java
@@ -0,0 +1,11 @@
+package faang.school.godbless.Witcher;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@AllArgsConstructor
+@Data
+public class Location {
+    private double x;
+    private double y;
+}
