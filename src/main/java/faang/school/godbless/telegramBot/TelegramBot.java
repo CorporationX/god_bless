a8e064e87e11c01302583c35eff11d476ae1415c
Andrey <khalyavinandrey@gmail.com>
2023-07-01 14:53:12 +0500

    telegram bot class

---
+++ src/main/java/faang/school/godbless/telegramBot/TelegramBot.java
@@ -0,0 +1,34 @@
+package faang.school.godbless.telegramBot;
+
+public class TelegramBot {
+    private final int REQUEST_LIMIT;
+    private int requestCounter;
+    private long lastRequestTime;
+
+    public TelegramBot(int REQUEST_LIMIT) {
+        this.REQUEST_LIMIT = REQUEST_LIMIT;
+        this.requestCounter = 0;
+        this.lastRequestTime = System.currentTimeMillis();
+    }
+
+    public void sendMessage(String message) {
+        long expired = System.currentTimeMillis() - lastRequestTime;
+        System.out.println(expired);
+
+        if (expired < 1000) {
+            requestCounter++;
+            if (requestCounter > REQUEST_LIMIT) {
+                try {
+                    wait(1000 - expired);
+                } catch (InterruptedException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+        } else {
+            requestCounter = 0;
+            lastRequestTime = 0;
+        }
+
+        System.out.println(message);
+    }
+}
