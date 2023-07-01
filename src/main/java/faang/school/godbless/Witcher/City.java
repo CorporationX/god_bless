e6a6ee1567b59ab1cc7c9b761c71fd2e2f64f940
Andrey <khalyavinandrey@gmail.com>
2023-07-01 11:12:54 +0500

    city class

---
+++ src/main/java/faang/school/godbless/Witcher/City.java
@@ -0,0 +1,12 @@
+package faang.school.godbless.Witcher;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@AllArgsConstructor
+@Data
+public class City {
+    private String name;
+    private Location location;
+    private double distance;
+}
