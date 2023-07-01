04065ea34ac4f5af83c5463e983fe66ef3e150c8
OneWayMy <zhenya.ivashchenko.9494@gmail.com>
2023-07-01 15:18:55 -0700

    MultiThreading_Lord_of_the_ring_PR_changes

--- src/main/java/faang/school/godbless/sprint_3/lord_of_the_rings/InventoryManager.java
+++ src/main/java/faang/school/godbless/sprint_3/lord_of_the_rings/InventoryManager.java
@@ -1,5 +1,6 @@
 package faang.school.godbless.sprint_3.lord_of_the_rings;
 
+import java.util.List;
 import java.util.function.Consumer;
 import java.util.function.Function;
 import java.util.function.Predicate;
@@ -12,16 +13,22 @@ public class InventoryManager {
     }
 
     public void removeItem(Character character, Predicate<Item> predicate) {
-        character.getInventory().removeIf(predicate);
+        List<Item> items = character.getInventory();
+        items.stream()
+                .filter(predicate)
+                .findFirst()
+                .ifPresent(items::remove);
     }
 
     public void updateItem(Character character, Predicate<Item> predicate, Function<Item, Item> function) {
         if (!character.getInventory().isEmpty()) {
-            character.getInventory().stream().filter(predicate).forEach(item -> {
-                Item newItem = function.apply(item);
-                int index = character.getInventory().indexOf(item);
-                character.getInventory().set(index, newItem);
-            });
+            character.getInventory().stream()
+                    .filter(predicate)
+                    .forEach(item -> {
+                        Item newItem = function.apply(item);
+                        int index = character.getInventory().indexOf(item);
+                        character.getInventory().set(index, newItem);
+                    });
         }
     }
 }
