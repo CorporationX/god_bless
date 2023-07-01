5f30f0325d7f43cf7efdd5fa1f5d7160120b24f1
”Ramazan” <mansurov.r13@gmail.com>
2023-07-01 19:26:55 +0300

    BC-2412-SkyNet

---
+++ src/main/java/faang/school/godbless/multithreading/sky_net/Target.java
@@ -0,0 +1,10 @@
+package faang.school.godbless.multithreading.sky_net;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@AllArgsConstructor
+@Getter
+public class Target {
+    private String name;
+}
