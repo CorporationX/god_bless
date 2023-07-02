976e00a4cd0a1568f0c0b16b6f3250776075b4fb
M1khail <mishalion888@gmail.com>
2023-07-02 23:18:48 +0200

    help colleague - fixed

--- src/main/java/faang/school/godbless/helpСolleague/Main.java
+++ src/main/java/faang/school/godbless/helpСolleague/Main.java
@@ -8,26 +8,25 @@ import java.util.concurrent.TimeUnit;
 
 public class Main {
     public static void main(String[] args) throws InterruptedException {
-        int personsForOneThread = 1000;
-        List<Person> listOfPersons = new ArrayList<>(10000); // лижат типа люди 1 = person; 2 = person ...
+        List<Person> listOfPersons = new ArrayList<>(10000);
         for (int i = 0; i < 10000; i++) {
-            String name = "Name" + i; // если бы были люди, то присвоили каждому имя,
-            String surname = "Surname" + i; // фамилию
-            String workSpace = "Work space" + i; // место работы
-            listOfPersons.add(new Person(name, surname, i, workSpace)); // доб. человека в лист
+            String name = "Name" + i;
+            String surname = "Surname" + i;
+            String workSpace = "Work space" + i;
+            listOfPersons.add(new Person(name, surname, i, workSpace));
         }
-        ExecutorService executor = Executors.newFixedThreadPool(10); // 1 thread = 1000 persons
-        for (int i = 0; i < 10; i++) { // реализуем 1 поток = 1000 человек
-            int head = i; // 1-ый человек
-            int tail = (i + 1) * 1000; // 1000-ый чел
-            List<Person> thousandOfPersons = listOfPersons.subList(head, tail); // проходимя по каждому челу от 1 до 999-го
-            head = i + 1000; // смещаем голову вправа
+        ExecutorService executor = Executors.newFixedThreadPool(10);
+        for (int i = 0; i < 10; i++) {
+            int head = i + 1;
+            int tail = (i + 1) * 1000;
+            List<Person> thousandOfPersons = listOfPersons.subList(head, tail);
+            head = i + 1000;
             for (Person person : thousandOfPersons) {
-                executor.submit(new PersonNamePrinter(person)); // применем потоки к людям
+                executor.submit(new PersonNamePrinter(person));
             }
         }
+        executor.shutdown();
         executor.awaitTermination(10, TimeUnit.SECONDS); // ожидаем завершения работы
-        executor.shutdown(); // удаляем потоки
         System.out.println("Program is done!");
     }
 }
