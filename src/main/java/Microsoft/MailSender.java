2acf619e2a61a0a1516dda63cbde16db9a34282d
Eggur <eggurin2033@gmail.com>
2023-07-02 15:45:05 +0300

    Исправил ошибки

--- src/main/java/Microsoft/MailSender.java
+++ src/main/java/Microsoft/MailSender.java
@@ -1,10 +1,13 @@
 package Microsoft;
 
 public class MailSender {
+    private static final Integer THREAD_CNT = 5;
+    private static final Integer BATCH_SIZE = 100;
+
     public static void main(String[] args) throws InterruptedException {
-        for (int i = 0; i < 5; i++){
-            int startIndex = i * 100 + 1;
-            int endIndex = i * 100 + 1;
+        for (int i = 0; i < THREAD_CNT; i++) {
+            int startIndex = i * BATCH_SIZE;
+            int endIndex = startIndex + BATCH_SIZE - 1;
             Thread thread = new Thread(new SenderRunnable(startIndex, endIndex));
 
             thread.start();
