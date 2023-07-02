9240d59aa0d981f1ded431fd2bb1227237028613
Никита <nikita.armeec.88@mail.ru>
2023-07-02 21:19:29 +0300

    Созданы классы: Robot, Main, Battle

---
+++ src/main/java/faang/school/godbless/kxnvg/starwarsarena/Robot.java
@@ -0,0 +1,13 @@
+package faang.school.godbless.kxnvg.starwarsarena;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@AllArgsConstructor
+@Getter
+public class Robot {
+
+    private String name;
+    private int attackPower;
+    private int defensePower;
+}
