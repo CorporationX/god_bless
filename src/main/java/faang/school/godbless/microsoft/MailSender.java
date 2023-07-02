ac3397ac56c0ee242bde3c67b4482698f26d30ed
Abramov Nikita <mr.abramovn@gmail.com>
2023-07-02 19:00:20 +0300

    fix bugs

--- src/main/java/faang/school/godbless/microsoft/MailSender.java
+++ src/main/java/faang/school/godbless/microsoft/MailSender.java
@@ -1,5 +1,7 @@
 package faang.school.godbless.microsoft;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.concurrent.Executor;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
@@ -7,15 +9,23 @@ import java.util.concurrent.Executors;
 public class MailSender {
     public static void main(String... args) throws InterruptedException {
 
-        ExecutorService executor = Executors.newFixedThreadPool(5);
+        List<Thread> threads = new ArrayList<>();
         for (int i = 0; i < 5; i++) {
             int startIndex = i * 200 + 1;
             int endIndex = startIndex + 200;
             Thread thread = new Thread(new SenderRunnable(startIndex, endIndex));
+            threads.add(thread);
             thread.start();
-            thread.join();
-
         }
+
+        threads.forEach(thread -> {
+            try {
+                thread.join();
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+        });
+
         System.out.println("Все сообщения отправлены");
 
     }
