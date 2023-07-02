05bd596b62c38b2bdc55681fcb05905fe2667fb3
”Ramazan” <mansurov.r13@gmail.com>
2023-07-02 20:50:31 +0300

    BC-2570-GooglePhotoSync

---
+++ src/main/java/faang/school/godbless/multithreading/google_photo/GooglePhotosAutoUploader.java
@@ -0,0 +1,63 @@
+package faang.school.godbless.multithreading.google_photo;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class GooglePhotosAutoUploader {
+    private final Object lock;
+    private final List<String> photosToUpload;
+    private final List<String> googlePhotos;
+    private boolean stop;
+
+    public GooglePhotosAutoUploader() {
+        lock = new Object();
+        photosToUpload = new ArrayList<>();
+        googlePhotos = new ArrayList<>();
+        stop = false;
+    }
+
+    public void startAutoUpload() {
+        while (!stop) {
+            synchronized (lock) {
+                if (photosToUpload.isEmpty()) {
+                    try {
+                        lock.wait();
+                    } catch (InterruptedException e) {
+                        System.err.println(e.getMessage());
+                    }
+                } else {
+                    uploadPhotos();
+                }
+            }
+        }
+    }
+
+    public void uploadPhotos() {
+        synchronized (lock) {
+            photosToUpload.forEach(photo -> {
+                System.out.println(photo + " uploaded with " + Thread.currentThread().getName());
+                googlePhotos.add(photo + " processed");
+            });
+            photosToUpload.clear();
+        }
+    }
+
+    public void onNewPhotoAdded(String photoPath) {
+        synchronized (lock) {
+            photosToUpload.add(photoPath);
+            System.out.println(photoPath + " was added with " + Thread.currentThread().getName());
+            lock.notify();
+        }
+    }
+
+    public List<String> getGooglePhotos() {
+        return googlePhotos;
+    }
+
+    public void stop() {
+        synchronized (lock) {
+            stop = true;
+            lock.notify();
+        }
+    }
+}
