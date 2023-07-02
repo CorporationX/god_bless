09d9e3f930a202ff7832de5cd00e008af8924d59
Паша <petrov.pashapet@yandex.ru>
2023-07-03 00:21:45 +0300

    Поправил синхронизацию

--- src/main/java/faang/school/godbless/thirdSprint/SkyNet/Robot.java
+++ src/main/java/faang/school/godbless/thirdSprint/SkyNet/Robot.java
@@ -6,16 +6,18 @@ import java.util.Random;
 
 @ThreadSafe
 public record Robot(String name, String target) {
-    public synchronized void attack() {
-        Random random = new Random();
+    public void attack() {
+        synchronized (target) {
+            Random random = new Random();
 
-        System.out.println("Робот " + name + " начинает атаковать цель " + target);
-        double killTime = random.nextInt(5000);
-        try {
-            Thread.sleep((long) killTime);
-        } catch (InterruptedException e) {
-            throw new RuntimeException(e);
+            System.out.println("Робот " + name + " начинает атаковать цель " + target);
+            double killTime = random.nextInt(5000);
+            try {
+                Thread.sleep((long) killTime);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+            System.out.println("Цель " + target + " была уничтожена за " + (killTime / 1000) + " секунд");
         }
-        System.out.println("Цель " + target + " была уничтожена за " + (killTime / 1000) + " секунд");
     }
 }
