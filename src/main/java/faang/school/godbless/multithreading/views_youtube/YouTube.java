06db7e604032182eef6f7688d195db44e72b8e2d
Devilserk <demonserker@gmail.com>
2023-07-02 15:33:02 +0600

    Пока оставлю так

---
+++ src/main/java/faang/school/godbless/multithreading/views_youtube/YouTube.java
@@ -0,0 +1,28 @@
+package faang.school.godbless.multithreading.views_youtube;
+
+import java.util.Map;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+public class YouTube {
+    public static void main(String[] args) throws InterruptedException {
+        final int NUM_THREADS = 100;
+        final int NUM_VIDEOS = 5;
+        Map<Integer, Integer> views = IntStream.rangeClosed(1, NUM_VIDEOS).boxed().collect(Collectors.toMap(i -> i, i -> 0));
+        VideoManager videoManager = new VideoManager(views);
+        ExecutorService service = Executors.newFixedThreadPool(NUM_THREADS);
+
+        for (int i = 1; i <= NUM_VIDEOS; i++) {
+            for (int j = 1; j < NUM_THREADS / NUM_VIDEOS; j++) {
+                final int id = i;
+                service.execute(() -> videoManager.addView(id));
+                service.execute(() -> videoManager.getViewCount(id));
+            }
+        }
+        service.shutdown();
+        service.awaitTermination(10, TimeUnit.SECONDS);
+    }
+}
