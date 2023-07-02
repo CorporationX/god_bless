051a0ba4440d628856dc62551a3aa7cb754f384a
ZLATA <585zlata@mail.ru>
2023-07-02 23:52:54 +0300

    коммит

---
+++ src/main/java/Sprint_4_Task_Eventually_Normal_Game/PigNafNaf3Thread.java
@@ -0,0 +1,7 @@
+package Sprint_4_Task_Eventually_Normal_Game;
+
+public class PigNafNaf3Thread extends PigThread{
+    public PigNafNaf3Thread() {
+        super("Наф-Наф", "кирпича");
+    }
+}
---
+++ src/main/java/Sprint_4_Task_Eventually_Normal_Game/PigNufNuf2Thread.java
@@ -0,0 +1,7 @@
+package Sprint_4_Task_Eventually_Normal_Game;
+
+public class PigNufNuf2Thread extends PigThread{
+    public PigNufNuf2Thread() {
+        super("Нуф-Нуф", "палок");
+    }
+}
