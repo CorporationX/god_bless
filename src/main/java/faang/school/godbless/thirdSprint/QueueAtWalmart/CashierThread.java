b425b085d635c49e4d31bcf2ace3a6d25cff1cd2
Паша <petrov.pashapet@yandex.ru>
2023-07-03 00:08:20 +0300

    Заменил конструктор на аннотацию @RequiredArgsConstructor

--- src/main/java/faang/school/godbless/thirdSprint/QueueAtWalmart/CashierThread.java
+++ src/main/java/faang/school/godbless/thirdSprint/QueueAtWalmart/CashierThread.java
@@ -1,18 +1,15 @@
 package faang.school.godbless.thirdSprint.QueueAtWalmart;
 
 import lombok.Getter;
+import lombok.RequiredArgsConstructor;
 
 @Getter
+@RequiredArgsConstructor
 public class CashierThread extends Thread {
     private final int cashierId;
     private final int[] customerItems;
     private int totalCost = 0;
     private int itemsCount = 0;
-
-    public CashierThread(int cashierId, int[] customerItems) {
-        this.cashierId = cashierId;
-        this.customerItems = customerItems;
-    }
 
     public void run() {
         System.out.println("Касса " + cashierId + " начинает обслуживать покупателя");
