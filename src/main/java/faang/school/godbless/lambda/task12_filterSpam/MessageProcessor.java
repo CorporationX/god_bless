370a655de942bd1615bc73556c091b65a5c4121d
Azat <azat.takhautdinov@gmail.com>
2023-07-02 16:40:17 +0500

    Сделал задачу Фильтруем спам

---
+++ src/main/java/faang/school/godbless/lambda/task12_filterSpam/MessageProcessor.java
@@ -0,0 +1,13 @@
+package faang.school.godbless.lambda.task12_filterSpam;
+
+import java.util.List;
+
+public class MessageProcessor{
+    public boolean processMessage(String message, List<MessageFilter> filters){
+        for (MessageFilter messageList: filters) {
+            messageList.filter(message);
+            return message;
+        }
+
+    }
+}
