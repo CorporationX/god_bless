a01f727253a80cb7bb1043d59f697df8b7e8806f
Azat <azat.takhautdinov@gmail.com>
2023-07-02 23:18:37 +0500

    Сделал задачу "Вы работаете в Microsoft!"

---
+++ src/main/java/faang/school/godbless/Multithreading/ParallelismAndThread/MailSender.java
@@ -0,0 +1,20 @@
+package faang.school.godbless.Multithreading.ParallelismAndThread;
+
+public class MailSender {
+    private static final int threadCount = 5;
+    private static final int packSize = 200;
+
+    public static void main(String[] args) {
+        try {
+            for (int i = 0; i < threadCount; i++) {
+                int start = i * packSize;
+                int end = start + packSize;
+                Thread thread = new Thread(new SenderRunnable(start, end));
+                thread.start();
+                thread.join();
+            }
+        } catch (InterruptedException inter) {
+            System.out.println("Ошибка запуска потока");
+        }
+    }
+}
