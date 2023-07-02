e3a437d1f585372dbac74fc3b030ed52f706e829
Dobrovolsky Artem <artdobrov@mail.ru>
2023-07-02 14:07:18 +0300

    Исправлен метод combineFilters

--- src/main/java/faang/school/godbless/Instagram/filters/FilterProcessor.java
+++ src/main/java/faang/school/godbless/Instagram/filters/FilterProcessor.java
@@ -3,13 +3,11 @@ package faang.school.godbless.Instagram.filters;
 import java.util.function.Function;
 
 public class FilterProcessor {
-    public Image applyFilter(Image image, Function<Image, Image> function) {
-        return function.apply(image);
+    public Image applyFilter(Image image, Function<Image, Image> filter) {
+        return filter.apply(image);
     }
 
     public Function<Image, Image> combineFilters(Function<Image, Image> function1, Function<Image, Image> function2) {
-        return image -> {
-            return function2.apply(function1.apply(image));
-        };
+        return function2.compose(function1);
     }
 }
