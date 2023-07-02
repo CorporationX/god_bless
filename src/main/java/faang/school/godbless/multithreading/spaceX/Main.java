e85c4822050de8c55af0a66ac7b51bad3495026a
Devilserk <demonserker@gmail.com>
2023-07-02 16:49:26 +0600

    Исправил объявление переменных в spaceX

--- src/main/java/faang/school/godbless/multithreading/spaceX/Main.java
+++ src/main/java/faang/school/godbless/multithreading/spaceX/Main.java
@@ -7,16 +7,13 @@ import java.util.concurrent.TimeUnit;
 
 public class Main {
     public static void main(String[] args) throws InterruptedException {
-        long start;
-        long end;
-
         List<RocketLaunch> launches = new ArrayList<>();
         launches.add(new RocketLaunch("Falcon 9", LocalDateTime.now().plusSeconds(10)));
         launches.add(new RocketLaunch("Falcon Heavy", LocalDateTime.now().plusSeconds(5)));
         launches.add(new RocketLaunch("Dragon", LocalDateTime.now().plusSeconds(2)));
-        start = System.currentTimeMillis();
+        long start = System.currentTimeMillis();
         RocketLaunch.planRocketLaunches(launches);
-        end = System.currentTimeMillis();
+        long end = System.currentTimeMillis();
         RocketLaunch.getService().shutdown();
         RocketLaunch.getService().awaitTermination(1, TimeUnit.MINUTES);
         System.out.println("Time taken to execute the method: 12" + (end - start));
