3da8aab46d7cb4015b33e26c00abda52ea6f3b0c
ZLATA <585zlata@mail.ru>
2023-07-03 23:25:44 +0300

    коммит

--- src/main/java/Sprint_4_Mice_are_very_nice/House.java
+++ src/main/java/Sprint_4_Mice_are_very_nice/House.java
@@ -19,33 +19,43 @@ public class House {
         this.food = food;
     }
 
-    public void collectFood(House house, Room room) {
+    public void collectFood(Room room) {
+
+//        System.out.println(Thread.currentThread().getName() + " Начался сбор " + food);
+//        food = rooms.stream()
+//                .flatMap(room -> room.getFoodForRoom().stream())
+//                .toList();
+//        food.forEach(System.out::println);
+//        System.out.println(Thread.currentThread().getName() + " Закончился сбор "+ food);
+//        rooms.forEach(room -> room.setFoodForRoom(new ArrayList<>()));
+//        System.out.println(Thread.currentThread().getName() + " Уничтожил еду " + food);
 
         for (int i = 0; i < rooms.size(); i++) {
-            for (int j = 0; j < room.getFoodForRoom().size(); i++) {
-                food.add((Food) room.getFoodForRoom(j));
 
-            }
+            food.addAll(room.getFoodForRoom());
+            System.out.println(Thread.currentThread().getName() + " Все фрукты получил "+ food);
+            room.setFoodForRoom(new ArrayList<>());
+            System.out.println(Thread.currentThread().getName() + " Фрукты собраны " + room.getFoodForRoom());
         }
+    }
+
+    public static void main(String[] args) {
+
+        List<Food> collectedFood = new ArrayList<>();
+        Room room1 = new Room("кухня",
+                List.of(new Food("фрукты"), new Food("авокадо")));
+        Room room2 = new Room("ванная",
+                List.of(new Food("черри"), new Food("овощи"), new Food("нут")));
+        House house = new House(List.of(room1, room2), collectedFood);
+
+        ScheduledExecutorService executorService = Executors.newScheduledThreadPool(5);
+
+        for (int i = 0; i < 5; i++) {
+            executorService.schedule(() -> house.collectFood(room1), 3000, TimeUnit.MILLISECONDS);
+            executorService.schedule(() -> house.collectFood(room2), 3000, TimeUnit.MILLISECONDS);
+        }
+        executorService.shutdown();
 
     }
-//    public static void main(String[] args) {
-//
-    //List<Food> collectedFood = new ArrayList<>();
-//        Room room1 = new Room("кухня",
-//                List.of(new Food("фрукты"), new Food("авокадо")));
-//        Room room2 = new Room("ванная",
-//                List.of(new Food("черри"), new Food("овощи"), new Food("нут")));
-//        House house = new House(List.of(room1, room2), collectedFood);
-//
-//        ScheduledExecutorService executorService = Executors.newScheduledThreadPool(5);
-//
-//        for (int i = 0; i < 5; i++) {
-//            executorService.schedule(() -> house.collectFood(), 3000, TimeUnit.MILLISECONDS);
-//        }
-//        executorService.shutdown();
-//
-//    }
-
 
 }
