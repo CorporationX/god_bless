83d7a9bbc4db919fec70d6a89c7782416de1b98e
Andrew13pol <andrew13pol@gmail.com>
2023-07-03 21:27:56 +0500

    WOW

---
+++ src/main/java/faang/school/godbless/wow/Player.java
@@ -0,0 +1,12 @@
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@AllArgsConstructor
+@Getter
+@Setter
+public class Player {
+    private String name;
+    private int level;
+    private int experience;
+}
