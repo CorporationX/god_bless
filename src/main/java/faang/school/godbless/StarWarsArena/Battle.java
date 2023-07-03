158d30de48850cc4c494afebc6ea5746a24b2d5e
An Dima <an.dima@slexn.com>
2023-07-03 11:00:40 +0900

    star wars arena

--- src/main/java/faang/school/godbless/StarWarsArena/Battle.java
+++ src/main/java/faang/school/godbless/StarWarsArena/Battle.java
@@ -7,6 +7,7 @@ import java.util.concurrent.Future;
 public class Battle {
     public Future<Robot> fight(Robot robot1, Robot robot2) {
         ExecutorService executorService = Executors.newSingleThreadExecutor();
+
         return executorService.submit(() -> robot1.getAttackPower() + robot1.getDefensePower()
                 > robot2.getAttackPower() + robot2.getDefensePower()
                 ? robot1 : robot2);
