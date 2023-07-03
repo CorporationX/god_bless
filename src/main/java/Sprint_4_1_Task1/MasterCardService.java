72905b3d0d7ec61df07c3e5e0d9c0e3b7f26e94e
zazozhik <ya.940@mail.ru>
2023-07-03 18:39:26 +0300

    Реализовал условию задачи Asynchrony_and_Future

---
+++ src/main/java/Sprint_4_1_Task1/MasterCardService.java
@@ -0,0 +1,23 @@
+package Sprint_4_1_Task1;
+
+public class MasterCardService {
+    public static int collectPayment() {
+        try {
+            Thread.sleep(10_000);
+            return 10_000;
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+            throw new RuntimeException();
+        }
+    }
+
+    public static int sendAnalystics() {
+        try {
+            Thread.sleep(1_000);
+            return 1_000;
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+            throw new RuntimeException();
+        }
+    }
+}
