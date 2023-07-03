2a0913f8b26854fd927771f8c50b161d85972411
An Dima <an.dima@slexn.com>
2023-07-03 11:00:02 +0900

    star wars arena

---
+++ src/main/java/faang/school/godbless/StarWarsArena/Battle.java
@@ -0,0 +1,14 @@
+package faang.school.godbless.StarWarsArena;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+
+public class Battle {
+    public Future<Robot> fight(Robot robot1, Robot robot2) {
+        ExecutorService executorService = Executors.newSingleThreadExecutor();
+        return executorService.submit(() -> robot1.getAttackPower() + robot1.getDefensePower()
+                > robot2.getAttackPower() + robot2.getDefensePower()
+                ? robot1 : robot2);
+    }
+}
---
+++ src/main/java/faang/school/godbless/StarWarsArena/Robot.java
@@ -0,0 +1,14 @@
+package faang.school.godbless.StarWarsArena;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Robot {
+    private String name;
+    private int attackPower;
+    private int defensePower;
+}
