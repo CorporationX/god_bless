5c367445ffd7bfba854ab2b0a7c162f69bca2449
Ivan Averkov <averkovivan@gmail.com>
2023-07-03 12:30:53 +0300

    bug fix

--- src/main/java/faang/school/godbless/Sprint_4/Multirhreading_Synchronization/SkyNet/Robot.java
+++ src/main/java/faang/school/godbless/Sprint_4/Multirhreading_Synchronization/SkyNet/Robot.java
@@ -4,9 +4,11 @@ import lombok.AllArgsConstructor;
 
 @AllArgsConstructor
 public class Robot {
-    private int target;
+    private final int target;
 
-    public synchronized void attack() {
-        System.out.printf("Now i'm attacking %d\n", target);
+    public void attack() {
+        synchronized ((Object) target) {
+            System.out.printf("Now i'm attacking %d\n", target);
+        }
     }
 }
