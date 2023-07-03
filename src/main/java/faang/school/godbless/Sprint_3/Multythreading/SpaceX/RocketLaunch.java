ca855c118cecbd329129fa0ca4fa4ebed26d8f48
Ivan Averkov <averkovivan@gmail.com>
2023-07-03 09:56:31 +0300

    bug fix

--- src/main/java/faang/school/godbless/Sprint_3/Multythreading/SpaceX/RocketLaunch.java
+++ src/main/java/faang/school/godbless/Sprint_3/Multythreading/SpaceX/RocketLaunch.java
@@ -4,6 +4,7 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 
 import java.time.LocalDateTime;
+import java.time.temporal.ChronoUnit;
 import java.util.Comparator;
 import java.util.List;
 import java.util.concurrent.ExecutorService;
@@ -16,29 +17,44 @@ public class RocketLaunch {
     private String name;
     private LocalDateTime time;
 
-    public void launch() throws InterruptedException {
+    public void launch() throws RuntimeException {
         try {
             Thread.sleep(1000);
+            System.out.printf("%s GO!\n", name);
         } catch (InterruptedException e) {
-            throw new RuntimeException(e);
+            System.out.println("launch error");
         }
-        System.out.println("Go!");
     }
 
     public static void planRocketLaunches(List<RocketLaunch> launches) throws RuntimeException{
+        LocalDateTime now = LocalDateTime.now();
         List<RocketLaunch> sortedLaunches = launches.stream()
                 .sorted(Comparator.comparing(RocketLaunch::getTime))
                 .toList();
         ExecutorService executor = Executors.newSingleThreadExecutor();
         for (RocketLaunch l : sortedLaunches){
+            long dif = Math.abs(ChronoUnit.MILLIS.between(l.getTime(), LocalDateTime.now()));
+            System.out.println(dif);
             executor.execute(()-> {
                 try {
+                    Thread.sleep(dif);
                     l.launch();
+                } catch (RuntimeException e) {
+                    System.out.println("planRocketLaunches Error");
                 } catch (InterruptedException e) {
-                    throw new RuntimeException(e);
+                    System.out.println("Sleep in planRocketLaunches Error");
                 }
             });
         }
-        executor.shutdown();
+        try {
+            executor.shutdown();
+            executor.awaitTermination(100000, TimeUnit.MILLISECONDS);
+            executor.shutdownNow();
+            System.out.println("Time: " + ChronoUnit.SECONDS.between(now, LocalDateTime.now()));
+        } catch (InterruptedException e) {
+            System.out.println("Wait Error");
+        }
+
+
     }
 }
