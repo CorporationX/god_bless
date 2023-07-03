ab2bd6e211182f6dbca48295607ef062835d8587
Devilserk <demonserker@gmail.com>
2023-07-03 13:39:16 +0600

    Исправил метод Main

--- src/main/java/faang/school/godbless/multithreading/witcher/Witcher.java
+++ src/main/java/faang/school/godbless/multithreading/witcher/Witcher.java
@@ -1,39 +1,47 @@
 package faang.school.godbless.multithreading.witcher;
 
-import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.TimeUnit;
 
 public class Witcher {
-    public static void main(String[] args) throws InterruptedException {
-        List<Monster> monsters = new ArrayList<>();
-        monsters.add(new Monster("Griffin", new Location(0, 80)));
-        monsters.add(new Monster("Basilisk", new Location(20, 0)));
-        monsters.add(new Monster("Cockatrice", new Location(50, 60)));
-        monsters.add(new Monster("Chort", new Location(120, 60)));
-        List<City> cities = new ArrayList<>();
-        cities.add(new City("Novigrad", new Location(0, 60), 180));
-        cities.add(new City("Oxenfurt", new Location(60, 0), 138));
-        cities.add(new City("Vizima", new Location(120, 50), 63));
-        cities.add(new City("Kaer Morhen", new Location(180, 70), 0));
-        long start;
-        long end;
+    public static void main(String[] args) {
+        List<Monster> monsters = List.of(
+                new Monster("Griffin", new Location(0, 80)),
+                new Monster("Basilisk", new Location(20, 0)),
+                new Monster("Cockatrice", new Location(50, 60)),
+                new Monster("Chort", new Location(120, 60))
+        );
+        List<City> cities = List.of(
+                new City("Novigrad", new Location(0, 60), 180),
+                new City("Oxenfurt", new Location(60, 0), 138),
+                new City("Vizima", new Location(120, 50), 63),
+                new City("Kaer Morhen", new Location(180, 70), 0)
+        );
 
-        start = System.currentTimeMillis();
+        runWithMultithreading(cities, monsters);
+        runWithWithoutMultithreading(cities, monsters);
+    }
+
+    private static void runWithMultithreading(List<City> cities, List<Monster> monsters) {
+        long start = System.currentTimeMillis();
         ExecutorService service = Executors.newFixedThreadPool(4);
         cities.forEach(city -> service.execute(new CityWorker(city, monsters)));
         service.shutdown();
-        if (!service.awaitTermination(1, TimeUnit.MINUTES)) {
-            service.shutdownNow();
+        try {
+            service.awaitTermination(1, TimeUnit.MINUTES);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
         }
-        end = System.currentTimeMillis();
+        long end = System.currentTimeMillis();
         System.out.println("With multithreading: " + (end - start));
+    }
 
-        start = System.currentTimeMillis();
+    private static void runWithWithoutMultithreading(List<City> cities, List<Monster> monsters) {
+        long start = System.currentTimeMillis();
         cities.forEach(city -> new CityWorker(city, monsters).run());
-        end = System.currentTimeMillis();
-        System.out.println("With out multithreading: " + (end - start));
+        long end = System.currentTimeMillis();
+        System.out.println("Without multithreading: " + (end - start));
     }
 }
