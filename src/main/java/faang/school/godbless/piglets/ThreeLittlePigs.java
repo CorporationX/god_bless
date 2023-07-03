5adea8d7a67379605f56c8c563ae0506933658f3
Sabina-Vishnya <Poecsta@gmail.com>
2023-07-03 23:13:48 +0300

    BC-2042 code review

--- src/main/java/faang/school/godbless/piglets/ThreeLittlePigs.java
+++ src/main/java/faang/school/godbless/piglets/ThreeLittlePigs.java
@@ -5,15 +5,18 @@ import java.util.List;
 public class ThreeLittlePigs {
 
     public static void main(String[] args) {
-        List.of(new Pig1Thread(), new Pig2Thread(), new Pig3Thread())
-            .forEach(pigThread -> {
-                pigThread.start();
-                try {
-                    pigThread.join();
-                } catch (InterruptedException e) {
-                    throw new RuntimeException(e);
-                }
-            });
+        List<PigThread> pigThreads = List.of(new Pig1Thread(), new Pig2Thread(), new Pig3Thread());
+
+        pigThreads.forEach(Thread::start);
+
+        pigThreads.forEach(pigThread -> {
+            try {
+                pigThread.join();
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+        });
+
         System.out.println("Game over!");
     }
 }
