3a9d12e2965701fbae2aa68f999b5e63954e27fe
ipavelbel <ilpavel94@gmail.com>
2023-07-03 16:11:09 +0200

    Sprint 4 Task Help a colleague implementation

---
+++ src/main/java/faang/school/godbless/help_colleague/PersonNamePrinter.java
@@ -0,0 +1,16 @@
+package faang.school.godbless.help_colleague;
+
+import lombok.AllArgsConstructor;
+
+import java.util.List;
+
+@AllArgsConstructor
+public class PersonNamePrinter implements Runnable {
+    List<Person> workers;
+    @Override
+    public void run() {
+        for (Person person : workers) {
+            System.out.println(person.getWorkplace() + " " + Thread.currentThread().getName());
+        }
+    }
+}
