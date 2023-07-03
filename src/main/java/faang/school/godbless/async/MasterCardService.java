66e59898cfca1edba18b5c23a952ca42d865fa2a
Andrey <khalyavinandrey@gmail.com>
2023-07-04 00:01:35 +0500

    master card service class

---
+++ src/main/java/faang/school/godbless/async/MasterCardService.java
@@ -0,0 +1,24 @@
+package faang.school.godbless.async;
+
+public class MasterCardService {
+
+    public int collectPayment() {
+        try {
+            Thread.sleep(10_000);
+            return 10_000;
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+            throw new RuntimeException();
+        }
+    }
+
+    public int sendAnalytics() {
+        try {
+            Thread.sleep(1_000);
+            return 1_000;
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+            throw new RuntimeException();
+        }
+    }
+}
