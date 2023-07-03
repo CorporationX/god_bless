1348dd5aa3864663c63c0ae07f46f42eb9e016ac
Tamerlan <100015209+Devilserk@users.noreply.github.com>
2023-07-03 13:41:02 +0600

    Задание They were very nice but extremely poor выполнено (#1748)
    * Задание They were very nice but extremely poor выполнено
    
    * Добавил вывод о том что все задания выполнены

---
+++ src/main/java/faang/school/godbless/multithreading/nice_poor/WeasleyFamily.java
@@ -0,0 +1,25 @@
+package faang.school.godbless.multithreading.nice_poor;
+
+import java.util.List;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
+
+public class WeasleyFamily {
+    public static void main(String[] args) throws InterruptedException {
+        List<Chore> chores = List.of(
+                new Chore("task1"),
+                new Chore("task2"),
+                new Chore("task3"),
+                new Chore("task4")
+        );
+        ExecutorService service = Executors.newCachedThreadPool();
+
+        chores.forEach(chore -> service.submit(chore));
+
+        service.shutdown();
+        service.awaitTermination(10, TimeUnit.SECONDS);
+        service.shutdownNow();
+        System.out.println("All tasks done");
+    }
+}
