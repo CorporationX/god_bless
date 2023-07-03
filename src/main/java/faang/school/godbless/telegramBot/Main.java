2bc6577c1b8e7d1dad0fd1eac551f3cfbcd69812
Rustam Nigmatullin <rustamnigma@mail.ru>
2023-07-03 19:35:10 +0500

    BC-1875-fixed TelegramBot

--- src/main/java/faang/school/godbless/telegramBot/Main.java
+++ src/main/java/faang/school/godbless/telegramBot/Main.java
@@ -1,35 +1,15 @@
 package faang.school.godbless.telegramBot;
 
-import java.util.ArrayList;
-import java.util.Date;
-import java.util.List;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
-import java.util.concurrent.TimeUnit;
 
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
         ExecutorService executor = Executors.newFixedThreadPool(2);
-        TelegramBot bot1 = new TelegramBot(System.currentTimeMillis());
-        TelegramBot bot2 = new TelegramBot(System.currentTimeMillis());
-        TelegramBot bot3 = new TelegramBot(System.currentTimeMillis());
-        TelegramBot bot4 = new TelegramBot(System.currentTimeMillis());
-        List<TelegramBot> bots = new ArrayList<>();
-        bots.add(bot1);
-        bots.add(bot2);
-        bots.add(bot3);
-        bots.add(bot4);
+        TelegramBot bot = new TelegramBot();
 
-        for (TelegramBot bot : bots) {
+        for (int i = 0; i < 20; i++) {
             executor.submit(bot::sendMessage);
-        }
-        executor.shutdown();
-        try {
-            if(executor.awaitTermination(5, TimeUnit.SECONDS)){
-                System.out.println("Задача выполнена");
-            }
-        } catch (InterruptedException e){
-            e.printStackTrace();
         }
     }
 }
--- src/main/java/faang/school/godbless/telegramBot/TelegramBot.java
+++ src/main/java/faang/school/godbless/telegramBot/TelegramBot.java
@@ -1,34 +1,40 @@
 package faang.school.godbless.telegramBot;
 
 import java.util.Date;
+import java.util.concurrent.ExecutionException;
 
 public class TelegramBot {
     private final int REQUEST_LIMIT;
     private int requestCounter = 0;
     private long lastRequestTime;
 
-    public TelegramBot(long lastRequestTime) {
+    public TelegramBot() {
         this.REQUEST_LIMIT = 5;
-        this.lastRequestTime = lastRequestTime;
     }
 
     public synchronized void sendMessage() {
         long currentTime = System.currentTimeMillis();
         long pastTime = currentTime - lastRequestTime;
-        if (pastTime > 1000) {
-            requestCounter = 0;
-        } else {
-            requestCounter++;
-        }
-        if (requestCounter > REQUEST_LIMIT) {
-            try {
-                Thread.currentThread().wait(1000 - pastTime);
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-                Thread.currentThread().interrupt();
+        if (pastTime < 1000) {
+            while (requestCounter == REQUEST_LIMIT) {
+                try {
+                    System.out.println("Необходима пауза");
+                    wait(1000 - pastTime);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                    Thread.currentThread().interrupt();
+                }
+                if (System.currentTimeMillis() - lastRequestTime > 1000) {
+                    requestCounter = 0;
+                    lastRequestTime = System.currentTimeMillis();
+                }
             }
+        } else {
+            requestCounter = 0;
+            lastRequestTime = currentTime;
         }
-        System.out.println("Сообщение отправлено");
-
+        requestCounter++;
+        System.out.println("Сообщение отправлено " + requestCounter);
+        notifyAll();
     }
 }
