545686c19c405fed97bf2ccfe7c57ba2b25ecaf5
Matvey <matveynikishaev@gmail.com>
2023-07-03 20:40:16 +0300

    Implemented tournament

---
+++ src/main/java/faang/school/Tournament.java
@@ -0,0 +1,17 @@
+package faang.school;
+
+import java.util.concurrent.CompletableFuture;
+
+public class Tournament {
+    public CompletableFuture<School> startTask(School school, Task task) {
+        return CompletableFuture.supplyAsync(() -> {
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+            school.getTeam().forEach(student -> student.updatePoints(task.getReward()));
+            return school;
+        });
+    }
+}
