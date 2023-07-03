b327ac4b6fa2c3cebf5082fb8614679196398146
Andrew13pol <andrew13pol@gmail.com>
2023-07-04 01:36:38 +0500

    Star Wars Arena

---
+++ src/main/java/faang/school/godbless/starwarsarena/Robot.java
@@ -0,0 +1,13 @@
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@AllArgsConstructor
+@Getter
+@Setter
+public class Robot {
+    private String name;
+    private int attackPower;
+    private int defensePower;
+
+}
