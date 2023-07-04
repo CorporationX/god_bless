c960edca59aa476c4ecbd376335e56f87ae8141c
ZLATA <585zlata@mail.ru>
2023-07-04 23:36:59 +0300

    коммит

--- src/main/java/Sprint_4_Help_the_Collegue/Main.java
+++ src/main/java/Sprint_4_Help_the_Collegue/Main.java
@@ -9,12 +9,12 @@ public class Main {
     public static void main(String[] args) {
 
         List<Person> people = new ArrayList<>();
-        for (int i = 0; i < 10000; i++) {
+        for (int i = 0; i < 10_000; i++) {
             people.add(new Person("name = " + i, "surname = " + i, i, "workplace = " + i));
         }
         ExecutorService executorService = Executors.newFixedThreadPool(10);
         for (int i=0; i<10; i++){
-            executorService.submit(new PersonNamePrinter(people.subList(i*10000/10, (i+1)*10000/10)));
+            executorService.submit(new PersonNamePrinter(people.subList(i*10_000/10, (i+1)*10_000/10)));
         }
         executorService.shutdown();
 
