9889bd70ef7ec78356612fe3355b19eb48e284a0
Andrew13pol <andrew13pol@gmail.com>
2023-07-04 10:07:36 +0500

    The Big Bang Theory fix

--- src/main/java/faang/school/godbless/thebigbangtheory/BigBangTheory.java
+++ src/main/java/faang/school/godbless/thebigbangtheory/BigBangTheory.java
@@ -4,14 +4,10 @@ import java.util.concurrent.Executors;
 public class BigBangTheory {
     public static void main(String[] args) {
         ExecutorService executor = Executors.newFixedThreadPool(4);
-        Task Task1 = new Task("Шелдон", "подготовка теории");
-        Task Task2 = new Task("Леонард", "моделирование эксперимента");
-        Task Task3 = new Task("Говард", "разработка инструментов");
-        Task Task4 = new Task("Раджеш", "анализ данных");
-        executor.submit(Task1);
-        executor.submit(Task2);
-        executor.submit(Task3);
-        executor.submit(Task4);
+        executor.execute(new Task("Шелдон", "подготовка теории"));
+        executor.execute(new Task("Леонард", "моделирование эксперимента"));
+        executor.execute(new Task("Говард", "разработка инструментов"));
+        executor.execute(new Task("Раджеш", "анализ данных"));
         executor.shutdown();
     }
 }
--- src/main/java/faang/school/godbless/thebigbangtheory/Task.java
+++ src/main/java/faang/school/godbless/thebigbangtheory/Task.java
@@ -9,10 +9,11 @@ public class Task implements Runnable {
     public void run() {
         System.out.println(name + " начал выполнение задания " + task);
         try {
-            Thread.sleep(10);
-            System.out.println(name + " выполненил задание " + task);
+            Thread.sleep(1000);
         } catch (InterruptedException e) {
+            System.out.println(name + " не смог выполнить задание " + task + " из-за ошибки");
             throw new RuntimeException(e);
         }
+        System.out.println(name + " выполненил задание " + task);
     }
 }
