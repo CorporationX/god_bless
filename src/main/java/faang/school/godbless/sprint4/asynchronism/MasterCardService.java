92f13addf199e540f5dada995dba7c00ed098fd5
Pashkevich <ppavel012100@gmail.com>
2023-07-04 17:01:08 +0200

    resolve asynchronous task

---
+++ src/main/java/faang/school/godbless/sprint4/asynchronism/MasterCardService.java
@@ -0,0 +1,47 @@
+package faang.school.godbless.sprint4.asynchronism;
+
+
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+
+public class MasterCardService {
+    static int collectPayment() {
+        try {
+            Thread.sleep(10_000);
+            return 10_000;
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+            throw new RuntimeException();
+        }
+    }
+
+    static int sendAnalystics() {
+        try {
+            Thread.sleep(1_000);
+            return 1_000;
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+            throw new RuntimeException();
+        }
+    }
+
+    public static void doAll() throws ExecutionException, InterruptedException {
+        ExecutorService executor = Executors.newSingleThreadExecutor();
+        Future<Integer> future =  executor.submit(MasterCardService::collectPayment);
+
+        CompletableFuture<Integer> analyticsFuture = CompletableFuture.supplyAsync(MasterCardService::sendAnalystics);
+        analyticsFuture.join();
+
+        System.out.println("Результат sendAnalytics: " + analyticsFuture.get());
+        try {
+            System.out.println("Результат collectPayment: " + future.get());
+        } catch (InterruptedException | ExecutionException e) {
+            e.printStackTrace();
+        }
+        executor.shutdown();
+    }
+
+}
