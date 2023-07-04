efa4aef89c04ee649d322e5bf9acf80bf29ddc31
chaykinks <chaykinks@gmail.com>
2023-07-04 11:20:19 +0500

    добавил классы-наследники PigThread и класс с потоками

--- src/main/java/faang/school/godbless/Sprint4/Task_4_GoodGame/PigThread.java
+++ src/main/java/faang/school/godbless/Sprint4/Task_4_GoodGame/PigThread.java
@@ -16,4 +16,29 @@ public class PigThread extends Thread {
     public int getMaterial() {
         return material;
     }
+
+    private String getMaterialAsString(int material) {
+        switch (material) {
+            case 1:
+                return "соломы";
+            case 2:
+                return "палок";
+            case 3:
+                return "кирпичей";
+            default:
+                return "неизвестного материала";
+        }
+    }
+
+    @Override
+    public void run() {
+        System.out.println(getPigName() + " начал строить дом из " + getMaterialAsString(material));
+        try {
+            Thread.sleep(getMaterial() * 2000L);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+        System.out.println(getPigName() + " построил дом из " + getMaterialAsString(material));
+    }
 }
+
