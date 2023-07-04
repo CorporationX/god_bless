b16b02bc6c860770ded09430f2b25857b968e8f6
An Dima <andmitrii94@gmail.com>
2023-07-04 21:57:34 +0900

    amazon orders

---
+++ src/main/java/faang/school/godbless/AmazonOrders/Order.java
@@ -0,0 +1,11 @@
+package faang.school.godbless.AmazonOrders;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class Order {
+    private int id;
+    private Boolean status;
+}
