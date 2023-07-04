8bd19c7960cd776ac3e90fb781c40eef8ad439df
Dobrovolsky Artem <artdobrov@mail.ru>
2023-07-04 14:55:38 +0300

    Добавлены классы MasterCardService и Application(исполняемый)

---
+++ src/main/java/faang/school/godbless/Future/Application.java
@@ -0,0 +1,42 @@
+package faang.school.godbless.Future;
+
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+
+public class Application {
+
+    public static void main(String[] args) {
+
+        ExecutorService executor = Executors.newFixedThreadPool(6);
+
+        try {
+            doAll(executor, new MasterCardService());
+        } catch (ExecutionException e) {
+            throw new RuntimeException(e);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+
+    }
+
+    public static void doAll(ExecutorService executor, MasterCardService masterCardService) throws ExecutionException, InterruptedException {
+
+        Future<Integer> payment = executor.submit(() -> masterCardService.collectPayment());
+        CompletableFuture<Integer> analytics = CompletableFuture.supplyAsync(() -> masterCardService.sendAnalytics());
+
+        while (!analytics.isDone()) {
+
+        }
+
+        System.out.println(analytics.get());
+
+        while (!payment.isDone()) {
+
+        }
+
+        System.out.println(payment.get());
+    }
+}
