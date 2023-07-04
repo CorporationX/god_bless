cfa89f27ec50d2ac0a0475fb56b8149b10846db4
svetlana.obrancova <obrancova.s@gmail.com>
2023-07-04 20:03:55 +0200

    SENDFBNOT-1 Created classes Notification, Manager, Main

---
+++ src/main/java/faang/school/godbless/sendingFbNotifications/Notification.java
@@ -0,0 +1,11 @@
+package faang.school.godbless.sendingFbNotifications;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class Notification {
+    private int id;
+    private String message;
+}
