dfcfbe50dcf76fb9a79e9264bda7bfd58f4f742c
An Dima <an.dima@slexn.com>
2023-07-04 17:19:07 +0900

    fixed

--- src/main/java/faang/school/godbless/WallmartQueue/Application.java
+++ src/main/java/faang/school/godbless/WallmartQueue/Application.java
@@ -1,20 +1,30 @@
 package faang.school.godbless.WallmartQueue;
 
+import java.util.Arrays;
+
 public class Application {
     public static void main(String[] args) throws InterruptedException {
-        String[][] customers = {{"Apple", "Banana", "Cucumber"},
-                {"Coffee", "Milk", "Tea"},
-                {"Potato", "Watermelon", "Strawberry"}};
+        int[][] customers = {{5, 7, 3, 2},
+                {1, 2, 3},
+                {6, 5, 2},
+                {10, 2, 1, 3, 5}};
+
         CashierThread[] cashierThreads = new CashierThread[3];
         for (int i = 0; i < cashierThreads.length; i++) {
             cashierThreads[i] = new CashierThread(i + 1, customers[i]);
         }
+
         for (CashierThread cashierThread : cashierThreads) {
             cashierThread.start();
+        }
+
+        for (CashierThread cashierThread : cashierThreads) {
             cashierThread.join();
         }
+
         int overallQuantity = customers.length * customers[0].length;
-        int overallPrice = overallQuantity * 300;
-        System.out.println("Products quantity: " + overallQuantity + " Products price: " + overallPrice);
+        int overallPrice=  Arrays.stream(customers).flatMapToInt(Arrays::stream).sum();
+        System.out.println("Products quantity: " + overallQuantity
+                + " Products price: " + overallPrice);
     }
 }
--- src/main/java/faang/school/godbless/WallmartQueue/CashierThread.java
+++ src/main/java/faang/school/godbless/WallmartQueue/CashierThread.java
@@ -1,18 +1,27 @@
 package faang.school.godbless.WallmartQueue;
 
+import java.util.Arrays;
+
 public class CashierThread extends Thread {
     private int cashierId;
-    private String[] customerItems;
+    private int[] customerItems;
 
-    public CashierThread(int cashierId, String[] customerItems) {
+    public CashierThread(int cashierId, int[] customerItems) {
         this.cashierId = cashierId;
         this.customerItems = customerItems;
     }
 
     @Override
     public void run() {
-        for (String item : this.customerItems) {
+        try {
+            Thread.sleep(customerItems.length);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+
+        for (int item : this.customerItems) {
             System.out.println(item + " proceeded by cashier with id " + this.cashierId);
         }
+        System.out.println("Total price of all items " + Arrays.stream(customerItems).sum());
     }
 }
