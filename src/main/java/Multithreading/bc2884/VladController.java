c67aa8d2460d33de8881976e6492edbc02bb7f8c
Alex Tarasov <tarasof.av@gmail.com>
2023-07-04 11:43:48 +0300

    fix task Тамагочи Влад

--- src/main/java/Multithreading/bc2884/VladController.java
+++ src/main/java/Multithreading/bc2884/VladController.java
@@ -4,47 +4,61 @@ import lombok.Getter;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 
 @Getter
 public class VladController {
     public static List<TamagotchiVlad> tamagotchiVladList = new ArrayList<>();
+    public static VladController vladController = new VladController();
 
     public static void main(String[] args) {
         for (int i = 1; i <= 5; i++) {
             tamagotchiVladList.add(new TamagotchiVlad(i));
         }
+        ExecutorService executorService = Executors.newFixedThreadPool(3);
+        executorService.execute(() -> {
+            vladController.sleepAll();
+            vladController.removeTamagochi(1);
+            vladController.playAll();
+            vladController.removeTamagochi(1);
+            vladController.cleanAll();
+            vladController.addTamagochi(new TamagotchiVlad(8));
+            vladController.feedAll();
+        });
 
+        executorService.shutdown();
     }
-
 
     public void addTamagochi(TamagotchiVlad tamagotchiVlad) {
         tamagotchiVladList.add(tamagotchiVlad);
     }
 
-    public void removeTamagochi(TamagotchiVlad tamagotchiVlad) {
-        tamagotchiVladList.remove(tamagotchiVlad);
+    public void removeTamagochi(int index) {
+        tamagotchiVladList.remove(index);
+        System.out.println("Delete " + index);
     }
 
-    public void feedAll() {
+    public synchronized void feedAll() {
         for (TamagotchiVlad tamagotchiVlad : tamagotchiVladList) {
             tamagotchiVlad.feed();
         }
     }
 
-    public void playAll() {
+    public synchronized void playAll() {
         for (TamagotchiVlad tamagotchiVlad : tamagotchiVladList) {
             tamagotchiVlad.play();
         }
     }
 
-    public void cleanAll() {
+    public synchronized void cleanAll() {
         for (TamagotchiVlad tamagotchiVlad : tamagotchiVladList) {
             tamagotchiVlad.clean();
         }
     }
 
-    public void sleepAll() {
+    public synchronized void sleepAll() {
         for (TamagotchiVlad tamagotchiVlad : tamagotchiVladList) {
             tamagotchiVlad.sleep();
         }
