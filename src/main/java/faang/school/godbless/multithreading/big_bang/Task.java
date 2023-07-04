719e03953da692a1f50f77365e51739a5c68304d
”Ramazan” <mansurov.r13@gmail.com>
2023-07-04 17:05:43 +0300

    BC-2033-TheBigBangTheory fixed

--- src/main/java/faang/school/godbless/multithreading/big_bang/Task.java
+++ src/main/java/faang/school/godbless/multithreading/big_bang/Task.java
@@ -1,17 +1,15 @@
 package faang.school.godbless.multithreading.big_bang;
 
 import lombok.AllArgsConstructor;
-import lombok.Getter;
 
 @AllArgsConstructor
-@Getter
 public class Task implements Runnable {
     private String name;
     private String task;
 
     @Override
     public void run() {
-        System.out.println(name + "начинает выполнять задание " + task);
+        System.out.println(name + " начинает выполнять задание " + task);
         try {
             Thread.sleep(250);
         } catch (InterruptedException e) {
