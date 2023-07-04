dfa8c4f71acb9d68a020f2edc67f44c90ae49b31
Pashkevich <ppavel012100@gmail.com>
2023-07-04 11:09:49 +0200

    resolve help animals task

---
+++ src/main/java/faang/school/godbless/sprint4/animals/Donation.java
@@ -0,0 +1,13 @@
+package faang.school.godbless.sprint4.animals;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+
+
+@Data
+@AllArgsConstructor
+public class Donation {
+    private long id;
+    private int amount;
+}
