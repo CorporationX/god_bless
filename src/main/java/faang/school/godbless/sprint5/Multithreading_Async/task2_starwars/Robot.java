b866f20d0e3335e85b1f6a0510e6ecb6eccebf49
Sergey Gerasimov <czar997@yandex.ru>
2023-07-04 21:21:46 +0300

    Star Wars Arena: done!

---
+++ src/main/java/faang/school/godbless/sprint5/Multithreading_Async/task2_starwars/Robot.java
@@ -0,0 +1,17 @@
+package faang.school.godbless.sprint5.Multithreading_Async.task2_starwars;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+public class Robot {
+    
+    private String name;
+    
+    private int attackPower;
+    
+    private int defensePower;
+}
