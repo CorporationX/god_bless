9305f137e1c0abdad2fe0024c2c215a9a2eb18c2
Andrey <khalyavinandrey@gmail.com>
2023-07-04 20:01:25 +0500

    player class

---
+++ src/main/java/faang/school/godbless/wow/Player.java
@@ -0,0 +1,13 @@
+package faang.school.godbless.wow;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@AllArgsConstructor
+@Data
+public class Player {
+
+    private String name;
+    private int level;
+    private int experience;
+}
