0c0c7feb5b7a0a3b3081275193bc5293ebff08d3
OneWayMy <zhenya.ivashchenko.9494@gmail.com>
2023-07-03 20:47:02 -0700

    MultiThreading_Async_and_Future_PR

---
+++ src/main/java/faang/school/godbless/sprint_3/multithreading_future/async_and_fututre/Main.java
@@ -0,0 +1,29 @@
+package faang.school.godbless.sprint_3.multithreading_future.async_and_fututre;
+
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+
+public class Main {
+    public static void main(String[] args) {
+        doAll();
+    }
+
+    public static void doAll() {
+        MasterCardService cardService = new MasterCardService();
+        ExecutorService service = Executors.newFixedThreadPool(1);
+        Future<Integer> first = service.submit(cardService::collectPayment);
+        CompletableFuture<Integer> second = CompletableFuture.supplyAsync(cardService::sendAnalytics);
+        int firstValue = second.join();
+        System.out.println(firstValue);
+        service.shutdown();
+        try {
+            int secondValue = first.get();
+            System.out.println(secondValue);
+        } catch (InterruptedException | ExecutionException exception) {
+            exception.printStackTrace();
+        }
+    }
+}
---
+++ src/main/java/faang/school/godbless/sprint_3/multithreading_future/async_and_fututre/MasterCardService.java
@@ -0,0 +1,23 @@
+package faang.school.godbless.sprint_3.multithreading_future.async_and_fututre;
+
+public class MasterCardService {
+    public int collectPayment() {
+        try {
+            Thread.sleep(10_000);
+            return 10_000;
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+            throw new RuntimeException();
+        }
+    }
+
+    public int sendAnalytics() {
+        try {
+            Thread.sleep(1_000);
+            return 1_000;
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+            throw new RuntimeException();
+        }
+    }
+}
