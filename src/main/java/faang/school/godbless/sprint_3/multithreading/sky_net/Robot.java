6f782506d1e4ffc473998296af4ebba0737ae205
OneWayMy <zhenya.ivashchenko.9494@gmail.com>
2023-07-04 14:37:59 -0700

    MultiThreading_Sky_net_PR_changes

--- src/main/java/faang/school/godbless/sprint_3/multithreading/sky_net/Robot.java
+++ src/main/java/faang/school/godbless/sprint_3/multithreading/sky_net/Robot.java
@@ -1,14 +1,9 @@
 package faang.school.godbless.sprint_3.multithreading.sky_net;
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-
-@AllArgsConstructor
-@Getter
-public class Robot {
-    private String target;
-
-    public synchronized void attack() {
-        System.out.printf("Target: %s are dead!", target);
+public record Robot(String target) {
+    public void attack() {
+        synchronized (target) {
+            System.out.printf("Target: %s are dead!%n", target);
+        }
     }
 }
