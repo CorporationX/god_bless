039721838a9d31716fd811b85fe591e8ab3f1134
ZLATA <585zlata@mail.ru>
2023-07-04 23:24:19 +0300

    коммит

--- src/main/java/Sprint_4_Queue_in_Walmart/CashierThread.java
+++ src/main/java/Sprint_4_Queue_in_Walmart/CashierThread.java
@@ -1,6 +1,8 @@
 package Sprint_4_Queue_in_Walmart;
+
 public class CashierThread extends Thread {
     private int cashierId;
+    //число кассиров 3, меньше, чем число покупателей (4)
     private int[] customerItems;
 
     public CashierThread(int cashierId, int[] customerItems) {
@@ -9,19 +11,26 @@ public class CashierThread extends Thread {
     }
 
     public void run() {
-        System.out.println(Thread.currentThread().getName() + ": " + "Cashier " + "пробивает такие товары:" + cashierId);
+        int sumItems = 0;
+        int sumI = 0;
+        for (int i = 0; i < customerItems.length; i++) {
+            sumItems += customerItems[i];
+            sumI +=1;
+        }
+        System.out.println("Cashier: " + cashierId + " пробивает товары покупателя: "
+                + Thread.currentThread().getName() +" сумма покупок: "+ sumItems + ", колличество покупок: " + sumI);
     }
 
     public static void main(String[] args) {
-        int[][] customers = {{5, 7, 3, 2}, {1, 2, 3}, {6, 5, 2}, {10, 2, 1, 3, 5}};
-        CashierThread[] cashiers = new CashierThread[customers.length];
+        int[][] customersBaskets = {{5, 7, 3, 2}, {1, 2, 3}, {6, 5, 2}, {10, 2, 1, 3, 5}};
+        CashierThread[] allCustomers = new CashierThread[customersBaskets.length];
 
-        for (int i = 0; i < customers.length; i++) {
-            cashiers[i] = new CashierThread(i, customers[i]);
+        for (int i = 0; i < customersBaskets.length; i++) {
+            allCustomers[i] = new CashierThread((int) (Math.random() * 3) + 1, customersBaskets[i]);
         }
 
-        for (int i = 0; i < cashiers.length; i++) {
-            cashiers[i].start();
+        for (int i = 0; i < allCustomers.length; i++) {
+            allCustomers[i].start();
         }
     }
 }
