c4ca4c9563e7f12517fc933cde7a11ee36246b1a
Anton <Agressive.cat@yandex.ru>
2023-07-05 00:18:42 +0300

    fix some issues

--- src/main/java/faang/school/godbless/the/big/bang/theory/BigBangTheory.java
+++ src/main/java/faang/school/godbless/the/big/bang/theory/BigBangTheory.java
@@ -8,20 +8,13 @@ import static java.util.concurrent.TimeUnit.SECONDS;
 public class BigBangTheory {
     public static void main(String[] args) throws InterruptedException {
         ExecutorService executor = Executors.newFixedThreadPool(4);
-        Task task1 = new Task("Шелдон", "подготовка теории");
-        Task task2 = new Task("Леонард", "моделирование эксперимента");
-        Task task3 = new Task("Говард", "разработка инструментов");
-        Task task4 = new Task("Раджеш", "анализ данных");
-
-        executor.submit(task1);
-        executor.submit(task2);
-        executor.submit(task3);
-        executor.submit(task4);
+        executor.execute(new Task("Шелдон", "подготовка теории"));
+        executor.execute(new Task("Леонард", "моделирование эксперимента"));
+        executor.execute(new Task("Говард", "разработка инструментов"));
+        executor.execute(new Task("Раджеш", "анализ данных"));
         executor.shutdown();
 
-
-        boolean isDone = executor.awaitTermination(6, SECONDS);
-        if(isDone == true) {
+        if(executor.awaitTermination(6, SECONDS)) {
             System.out.println("Задачи сделаны");
         }
     }
