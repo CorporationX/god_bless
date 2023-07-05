3bec1bb7386b3cd274cff6e5f1a990796b4af80e
Stepashkins <121392656+Stepashkins@users.noreply.github.com>
2023-07-05 15:36:55 +0300

    complete task (Army of heroes) (#1606)

--- src/main/java/faang/school/godbless/Archer.java
+++ src/main/java/faang/school/godbless/Archer.java
@@ -1,12 +1,7 @@
 package faang.school.godbless;
 
-public class Archer extends Character{
-    public Archer(String name, int power, int dexterity, int brain) {
-        super(name, 3, 10, 5);
-    }
-
-    @Override
-    public void attack(Character character) {
-        character.setHealth(character.getHealth()-getDexterity());
+public class Archer extends Hero{
+    public Archer(int power){
+        super(power);
     }
 }
