9ae27c8fdd9e95004a0c3ad5f6c443b030831e4d
Andrey <khalyavinandrey@gmail.com>
2023-07-05 22:50:51 +0500

    superhero class

---
+++ src/main/java/faang/school/godbless/superheroes/Superhero.java
@@ -0,0 +1,12 @@
+package faang.school.godbless.superheroes;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@AllArgsConstructor
+@Data
+public class Superhero {
+    private String name;
+    private int strength;
+    private int agility;
+}
