744bfb3167359d274a17464426900e51e4dfef08
zazozhik <ya.940@mail.ru>
2023-07-05 15:32:08 +0300

    Реализовал условию задачи SuperheroBattle

---
+++ src/main/java/Sprint_4_1_Task6/Pair.java
@@ -0,0 +1,11 @@
+package Sprint_4_1_Task6;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class Pair{
+    private Superhero left;
+    private Superhero right;
+}
