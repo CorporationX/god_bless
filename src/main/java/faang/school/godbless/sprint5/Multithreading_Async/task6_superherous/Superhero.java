3ba4f8596805b97dcf1c4a414a6d46e8d5c2f7a1
Sergey Gerasimov <czar997@yandex.ru>
2023-07-05 15:37:27 +0300

    Superheroes battle: выполнено основное условие задачи + доп задание

---
+++ src/main/java/faang/school/godbless/sprint5/Multithreading_Async/task6_superherous/Superhero.java
@@ -0,0 +1,17 @@
+package faang.school.godbless.sprint5.Multithreading_Async.task6_superherous;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+public class Superhero {
+
+    private String name;
+
+    private int strength;
+
+    private int agility;
+}
