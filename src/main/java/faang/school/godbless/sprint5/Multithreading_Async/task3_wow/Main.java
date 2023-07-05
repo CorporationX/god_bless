bf503fc4ec2077bc95bb47a33ecc816ff6c491b0
Sergey Gerasimov <czar997@yandex.ru>
2023-07-05 20:09:50 +0300

    фикс

--- src/main/java/faang/school/godbless/sprint5/Multithreading_Async/task3_wow/Main.java
+++ src/main/java/faang/school/godbless/sprint5/Multithreading_Async/task3_wow/Main.java
@@ -12,15 +12,19 @@ public class Main {
         Player player2 = new Player("Sylvanas", 12, 450);
 
         // Создание заданий
-        Quest quest1 = new Quest("Defeat the Lich King", 10, 150);
-        Quest quest2 = new Quest("Retrieve the Sword of Azeroth", 8, 100);
+        Quest quest1 = new Quest("Defeat the Lich King", 3, 150);
+        Quest quest2 = new Quest("Retrieve the Sword of Azeroth", 1, 100);
 
         // Запуск заданий
         CompletableFuture<Player> player1Quest = questSystem.startQuest(player1, quest1);
         CompletableFuture<Player> player2Quest = questSystem.startQuest(player2, quest2);
 
+        CompletableFuture<Void> voidCompletableFuture = CompletableFuture.allOf(player1Quest, player2Quest);
+
         // Обработка результатов заданий
         player1Quest.thenAccept(player -> System.out.println(player.getName() + " has completed the quest and now has " + player.getExperience() + " experience points."));
         player2Quest.thenAccept(player -> System.out.println(player.getName() + " has completed the quest and now has " + player.getExperience() + " experience points."));
+
+        voidCompletableFuture.join();
     }
 }
--- src/main/java/faang/school/godbless/sprint5/Multithreading_Async/task3_wow/QuestSystem.java
+++ src/main/java/faang/school/godbless/sprint5/Multithreading_Async/task3_wow/QuestSystem.java
@@ -6,6 +6,11 @@ public class QuestSystem {
 
     public CompletableFuture<Player> startQuest(Player player, Quest quest) {
         return CompletableFuture.supplyAsync(() -> {
+            try {
+                Thread.sleep(quest.getLevel() * 1000L);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
             player.setExperience(player.getExperience() + quest.getReward() * 100);
             return player;
         });
