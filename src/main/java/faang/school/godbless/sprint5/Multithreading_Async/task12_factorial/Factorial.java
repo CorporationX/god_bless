23c5927146a4c1aee0f1a520fe515ebb7cf45d30
Sergey Gerasimov <czar997@yandex.ru>
2023-07-05 14:30:55 +0300

    фикс

--- src/main/java/faang/school/godbless/sprint5/Multithreading_Async/task12_factorial/Factorial.java
+++ src/main/java/faang/school/godbless/sprint5/Multithreading_Async/task12_factorial/Factorial.java
@@ -19,26 +19,34 @@ public class Factorial {
             }
             return result;
         } else {
-            throw new IllegalArgumentException();
+            throw new IllegalArgumentException("Число " + n + " больше 12 или меньше 0");
         }
     }
 
     public static long factorialLong(int n) throws IllegalArgumentException {
-        if (n > MAX_INT_FACTORIAL && n <= MAX_LONG_FACTORIAL) {
+        if (n <= MAX_INT_FACTORIAL) {
+            return factorialInt(n);
+        }
+        if (n <= MAX_LONG_FACTORIAL) {
             long result = factorialInt(MAX_INT_FACTORIAL);
             for (int i = MAX_INT_FACTORIAL + 1; i <= n; i++) {
                 result *= i;
             }
             return result;
         } else {
-            throw new IllegalArgumentException();
+            throw new IllegalArgumentException("Число " + n + " больше 19");
         }
     }
 
     public static BigInteger factorialBig(int n) {
-        BigInteger result = BigInteger.valueOf(factorialLong(MAX_LONG_FACTORIAL));
-        for (int i = MAX_LONG_FACTORIAL + 1; i <= n; i++) {
-            result = result.multiply(BigInteger.valueOf(i));
+        BigInteger result;
+        if (n > MAX_LONG_FACTORIAL) {
+            result = BigInteger.valueOf(factorialLong(MAX_LONG_FACTORIAL));
+            for (int i = MAX_LONG_FACTORIAL + 1; i <= n; i++) {
+                result = result.multiply(BigInteger.valueOf(i));
+            }
+        } else {
+            result = BigInteger.valueOf(factorialLong(n));
         }
         return result;
     }
@@ -54,7 +62,7 @@ public class Factorial {
 
     public static void main(String[] args) {
         List<Integer> numbers = List.of(
-                50, 100, 200, 300, 400, 10, 25, 10000
+                50, 100, 200, 300, 400, 10, 25, 10000, 5
         );
         List<CompletableFuture<BigInteger>> result = factorials(numbers);
         AtomicInteger counter = new AtomicInteger(0);
