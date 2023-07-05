e7ae29855a11e16b42a49e44e0cd8ceec8165140
Irina Vereshchagina <irankamoya@gmail.com>
2023-07-06 00:24:31 +0300

    Задача - Star Wars Arena

---
+++ src/main/java/star_wars_arena/Robot.java
@@ -0,0 +1,25 @@
+package star_wars_arena;
+
+public class Robot {
+    private String name;
+    private int attackPower;
+    private int defensePower;
+
+    public Robot(String name, int attackPower, int defensePower) {
+        this.name = name;
+        this.attackPower = attackPower;
+        this.defensePower = defensePower;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getAttackPower() {
+        return attackPower;
+    }
+
+    public int getDefensePower() {
+        return defensePower;
+    }
+}
