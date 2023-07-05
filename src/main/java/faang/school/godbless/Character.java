2a98ff272360e21ef05275365a4fc87684af1baf
Stepashkins <121392656+Stepashkins@users.noreply.github.com>
2023-07-05 15:35:07 +0300

    complete task9 (Lord of the Rings RPG) (#897)

--- src/main/java/faang/school/godbless/Character.java
+++ src/main/java/faang/school/godbless/Character.java
@@ -1,27 +1,47 @@
 package faang.school.godbless;
 
-import lombok.Getter;
-import lombok.Setter;
 
-@Getter
-@Setter
-public abstract class Character {
-    private String name;
-    private int power;
-    private int dexterity;
-    private int brain;
-    private int health = 100;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.function.Predicate;
+import java.util.function.Function;
 
-    public Character(String name){
-        this.name = name;
+
+public class Character {
+    List<Item> inventory = new ArrayList<>();
+
+    public Character(String name) {
+        if(name.isBlank()){
+            throw new IllegalArgumentException("Name can't be empty.");
+        }
     }
 
-    public Character(String name, int power, int dexterity, int brain){
-        this.name = name;
-        this.power = power;
-        this.dexterity = dexterity;
-        this.brain = brain;
+    public static void main(String[] args) {
+        Character frodo = new Character("Frodo");
+        Item ring = new Item("The One Ring", 1000);
+
+        InventoryManager manager = new InventoryManager();
+
+// Добавляем предмет в инвентарь
+        manager.addItem(frodo, ring, (item) -> System.out.println(item.getName() + " was added to the inventory."));
+
+// Удаляем предмет из инвентаря
+        manager.removeItem(frodo, (item) -> item.getName().equals("The One Ring"));
+
+// Обновляем предмет в инвентаре
+        manager.updateItem(frodo, (item) -> item.getName().equals("The One Ring"), (item) -> new Item(item.getName(), item.getValue() * 2));
     }
 
-    public abstract void attack(Character character);
+    public void updateItemIf(Predicate<Item> condition, Function<Item, Item> update) {
+        int indexItem = 0;
+        for (Item item : inventory) {
+
+            if (condition.test(item)) {
+                Item updatedItem = update.apply(item);
+                inventory.set(indexItem,updatedItem);
+            }
+            indexItem++;
+        }
+    }
 }
+
