daea7ef7156719c3d82f04a96ac9c2e7d8092477
chaykinks <chaykinks@gmail.com>
2023-07-05 17:07:45 +0500

    реализовал метод sendMessage класс Main

--- src/main/java/faang/school/godbless/Sprint4/Task_14_TelegramBot/TelegramBot.java
+++ src/main/java/faang/school/godbless/Sprint4/Task_14_TelegramBot/TelegramBot.java
@@ -1,15 +1,35 @@
 package faang.school.godbless.Sprint4.Task_14_TelegramBot;
 
-import java.time.Instant;
+import java.time.Duration;
+import java.time.LocalDateTime;
 
 public class TelegramBot {
-    private final int REQUEST_LIMIT;
+    private static final int REQUEST_LIMIT = 5;
     private int requestCounter;
-    private Instant lastRequestTime;
+    private LocalDateTime lastRequestTime;
 
     public TelegramBot() {
-        this.REQUEST_LIMIT = 5;
         this.requestCounter = 0;
-        this.lastRequestTime = Instant.now();
+        this.lastRequestTime = LocalDateTime.now();
+    }
+
+    public synchronized void sendMessage(String message) {
+        long elapsedTime = Duration.between(lastRequestTime, LocalDateTime.now()).toMillis();
+
+        if (elapsedTime < 1000) {
+            requestCounter++;
+            if (requestCounter > REQUEST_LIMIT) {
+                try {
+                    Thread.sleep(1000 - elapsedTime);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                requestCounter = 0;
+            }
+        } else {
+            requestCounter = 0;
+            lastRequestTime = LocalDateTime.now();
+        }
+        System.out.println("Отправка сообщения: " + message);
     }
 }
