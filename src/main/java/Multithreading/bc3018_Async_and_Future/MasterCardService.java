195ac8ee70fb1b97f9bcad6fbbd1a2529c291312
Alex Tarasov <tarasof.av@gmail.com>
2023-07-05 11:47:31 +0300

    task Асинхронность и Future

---
+++ src/main/java/Multithreading/bc3018_Async_and_Future/MasterCardService.java
@@ -0,0 +1,44 @@
+package Multithreading.bc3018_Async_and_Future;
+
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+
+public class MasterCardService {
+
+    static int collectPayment() {
+        try {
+            Thread.sleep(10_000);
+            return 10_000;
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+            throw new RuntimeException();
+        }
+    }
+
+    static int sendAnalytics() {
+        try {
+            Thread.sleep(1_000);
+            return 1_000;
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+            throw new RuntimeException();
+        }
+    }
+
+    public void doAll() throws ExecutionException, InterruptedException {
+        ExecutorService executorService = Executors.newSingleThreadExecutor();
+        Future<Integer> collectPayment = executorService.submit(MasterCardService::collectPayment);
+        CompletableFuture<Integer> sendAnalytics = CompletableFuture
+                .supplyAsync(MasterCardService::sendAnalytics);
+        int result = sendAnalytics.get();
+        int res = collectPayment.get();
+
+        System.out.println(result);
+        System.out.println(res);
+
+        executorService.shutdown();
+    }
+}
