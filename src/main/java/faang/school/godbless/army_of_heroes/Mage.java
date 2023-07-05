aafe4e82c1cc52d292cd2c96444be57e66e7b5cd
ipavelbel <ilpavel94@gmail.com>
2023-07-05 15:51:25 +0200

    Sprint 4 Task: Army of heroes of might and magic implementation

---
+++ src/main/java/faang/school/godbless/army_of_heroes/Mage.java
@@ -0,0 +1,7 @@
+package faang.school.godbless.army_of_heroes;
+
+public class Mage extends Unit {
+    public Mage(String name, int power, int quantity) {
+        super(name, power, quantity);
+    }
+}
