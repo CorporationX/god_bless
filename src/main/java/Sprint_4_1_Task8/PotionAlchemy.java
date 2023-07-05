5cb53561cdd08866528f9944ba3a387592fc92bb
zazozhik <ya.940@mail.ru>
2023-07-05 16:16:42 +0300

    Реализовал условию задачи SuperheroBattle

--- src/main/java/Sprint_4_1_Task8/PotionAlchemy.java
+++ src/main/java/Sprint_4_1_Task8/PotionAlchemy.java
@@ -2,23 +2,51 @@ package Sprint_4_1_Task8;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
-import lombok.SneakyThrows;
 
 import java.util.List;
 import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutionException;
 import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicInteger;
 
 @Data
 @AllArgsConstructor
 public class PotionAlchemy {
     List<Potion> potionList;
-    @SneakyThrows
-    public double gatherIngredients(Potion potion){
-        TimeUnit.SECONDS.sleep(3);
-        return (Math.random()*10)+1;
+    public CompletableFuture<Integer> gatherIngredients(Potion potion){
+        CompletableFuture<Integer> future = CompletableFuture.supplyAsync(() -> {
+            try {
+                TimeUnit.SECONDS.sleep(3);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+            return potion.getRequiredIngredients();
+        });
+        return future;
     }
 
     public static void main(String[] args) {
-        CompletableFuture<PotionAlchemy> future = new CompletableFuture<>();
+        List<Potion> potionList = List.of(
+                new Potion("Potion1", 3),
+                new Potion("Potion2", 8),
+                new Potion("Potion3", 7)
+        );
+        PotionAlchemy potionAlchemy = new PotionAlchemy(potionList);
+        List<CompletableFuture<Integer>> collect = potionList.stream()
+                .map(potionAlchemy::gatherIngredients)
+                .toList();
+        AtomicInteger i = new AtomicInteger();
+        CompletableFuture<Void> voidCompletableFuture = CompletableFuture.allOf(collect.toArray(new CompletableFuture[potionList.size()]));
+        voidCompletableFuture.thenApply((x) -> {
+            collect.forEach((f) -> {
+                try {
+                    i.addAndGet(f.get());
+                } catch (InterruptedException | ExecutionException e) {
+                    throw new RuntimeException(e);
+                }
+            });
+            return null;
+        }).join();
+        System.out.println(i);
     }
 }
