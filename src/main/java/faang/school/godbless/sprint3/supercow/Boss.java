8e7c472c3676ef0c820fc464163cb52df3c28bb7
Pashkevich <ppavel012100@gmail.com>
2023-07-05 22:11:58 +0200

    rework Supercow task

--- src/main/java/faang/school/godbless/sprint3/supercow/Boss.java
+++ src/main/java/faang/school/godbless/sprint3/supercow/Boss.java
@@ -5,22 +5,23 @@ import lombok.Data;
 @Data
 public class Boss {
     private final Object lock = new Object();
-    private  final int maxPlayers = 3;
+    private final int maxPlayers = 3;
     private int currentPlayers;
 
     public void joinBattle(Player player) throws InterruptedException {
         synchronized (lock) {
             if (currentPlayers < maxPlayers) {
                 currentPlayers++;
-            }else {
+            } else {
                 lock.wait();
             }
         }
     }
+
     public void leaveBattle(Player player) {
         synchronized (lock) {
             currentPlayers--;
-            notifyAll();
+            lock.notifyAll();
         }
     }
 }
--- src/main/java/faang/school/godbless/sprint3/supercow/Main.java
+++ src/main/java/faang/school/godbless/sprint3/supercow/Main.java
@@ -3,10 +3,12 @@ package faang.school.godbless.sprint3.supercow;
 public class Main {
     public static void main(String[] args) throws InterruptedException {
         Boss boss = new Boss();
+
         Player player = new Player("Pavel");
         Player player2 = new Player("Vlad");
         Player player3 = new Player("Ivan");
         Player player4 = new Player("Anton");
+
         boss.joinBattle(player);
         boss.joinBattle(player2);
         boss.joinBattle(player3);
