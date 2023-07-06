1b9f28194037dba5152e3891acd5513315d2536b
Alex Tarasov <tarasof.av@gmail.com>
2023-07-06 12:25:05 +0300

    task Send a Raven

--- src/main/java/Multithreading/bc3101_Send_a_Raven/Kingdom.java
+++ src/main/java/Multithreading/bc3101_Send_a_Raven/Kingdom.java
@@ -4,6 +4,7 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
 
+import java.util.Random;
 import java.util.concurrent.CompletableFuture;
 
 @AllArgsConstructor
@@ -13,19 +14,21 @@ public class Kingdom {
     private String name;
     private String message;
 
+
     public String sendMessage(Kingdom kingdom) {
         return kingdom.getMessage();
-
     }
 
-    public void sendRaven(Kingdom kingdom1, Kingdom kingdom2) {
+    public CompletableFuture<String> sendRaven(Kingdom kingdom1, Kingdom kingdom2) {
+        Random random = new Random();
 
-        CompletableFuture<String> result = CompletableFuture
-                .supplyAsync(() -> kingdom2.sendMessage(kingdom1))
-                .handle(null);
-
-        String res = result.join();
-        System.out.println(res);
+        return CompletableFuture
+                .supplyAsync(() -> {
+                    if (random.nextBoolean()) {
+                        throw new RuntimeException();
+                    }
+                    return kingdom2.sendMessage(kingdom1);
+                });
     }
 }
 
