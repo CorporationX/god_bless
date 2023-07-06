55588c243eed0e150e8f5bccb9f337ca823ab204
Dobrovolsky Artem <artdobrov@mail.ru>
2023-07-06 20:53:31 +0300

    Добавлены классы Point, Application(исполняемый)

---
+++ src/main/java/faang/school/godbless/Pi/Point.java
@@ -0,0 +1,11 @@
+package faang.school.godbless.Pi;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@AllArgsConstructor
+@Getter
+public class Point {
+    private double x;
+    private double y;
+}
