1db410aa1087d8604e067d07f784543d16d5ac5f
OneWayMy <zhenya.ivashchenko.9494@gmail.com>
2023-07-06 16:02:28 -0700

    MultiThreading_facebook_notification_PR

---
+++ src/main/java/faang/school/godbless/sprint_3/multithreading_future/facebook_notifications/Notification.java
@@ -0,0 +1,11 @@
+package faang.school.godbless.sprint_3.multithreading_future.facebook_notifications;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@AllArgsConstructor
+@Getter
+public class Notification {
+    private Integer id;
+    private String message;
+}
