15de22a174eaf8dce13bae8c6e6d3954e079cc80
Adam-D-Beno <wrofdt@yandex.ru>
2023-07-06 15:43:46 +0300

    solved. Потренируем StreamAPI 2

--- src/main/java/Notification.java
+++
@@ -1,9 +0,0 @@
-import lombok.AllArgsConstructor;
-import lombok.Data;
-
-@Data
-@AllArgsConstructor
-public class Notification {
-    private String type;
-    private String message;
-}
--- src/main/java/faang/school/godbless/SpellAction.java
+++
@@ -1,5 +0,0 @@
-package faang.school.godbless;
-@FunctionalInterface
-public interface SpellAction {
-    String action(String nameOfTheSpell);
-}
