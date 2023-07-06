5b697292f887961108a079365a28eea1540558f6
Angie <angie@>
2023-07-06 14:24:25 +0300

    comments

--- src/main/java/faang/school/godbless/sprint3_lambda/error/ExceptionHandler.java
+++ src/main/java/faang/school/godbless/sprint3_lambda/error/ExceptionHandler.java
@@ -1,7 +1,8 @@
-package main.java.faang.school.godbless.sprint3_lambda.error;
+package faang.school.godbless.sprint3_lambda.error;
 
 @FunctionalInterface
-
+//объявление интерфейса ExceptionHandler, который принимает тип T в качестве параметра:
 public interface ExceptionHandler<T> {
+    // Этот интерфейс определяет метод handle,который принимает исключение Exception и возвращает значение типа T:
     T handle(Exception e);
 }
