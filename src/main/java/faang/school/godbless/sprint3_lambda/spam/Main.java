f685553720e9f3003581c84286abd1569e1ce023
Angie <angie@>
2023-07-06 15:16:40 +0300

    comments

--- src/main/java/faang/school/godbless/sprint3_lambda/spam/Main.java
+++ src/main/java/faang/school/godbless/sprint3_lambda/spam/Main.java
@@ -9,12 +9,12 @@ public class Main {
 
         MessageFilter spamFilter = message -> !message.toLowerCase().contains("спам");
         MessageFilter lengthFilter = message -> message.length() > 5;
-        MessageFilter emojiFilter = message -> !message.contains("😀");
+        MessageFilter emojiFilter = message -> !message.contains("");
         MessageFilter spammFilter = message -> !message.contains("spam");
 
         List<MessageFilter> filters = Arrays.asList(spamFilter,lengthFilter, emojiFilter, spammFilter);
 
-        String[] messages = {"Привет!", "Это Жук", "Как дела? 😀", "Длинное сообщение без спама и эмодзи"};
+        String[] messages = {"Привет!", "Это Жук", "Как дела?", "Длинное сообщение без спама и эмодзи"};
 
         for (String message : messages) {
             messageProcessor.processMessage(message, filters);
