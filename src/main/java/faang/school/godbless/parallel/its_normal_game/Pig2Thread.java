c308fa8317068bc86a2be2ed325b8acf4a8154f4
mo <nataleshen.ko@yandex.ru>
2023-07-07 17:24:20 +0300

    its_normal_game done

---
+++ src/main/java/faang/school/godbless/parallel/its_normal_game/Pig2Thread.java
@@ -0,0 +1,10 @@
+package faang.school.godbless.parallel.its_normal_game;
+
+public class Pig2Thread extends PigThread {
+    public Pig2Thread() {
+        super("naf-naf", 1);
+    }
+}
+
+
+
---
+++ src/main/java/faang/school/godbless/parallel/its_normal_game/Pig3Thread.java
@@ -0,0 +1,8 @@
+package faang.school.godbless.parallel.its_normal_game;
+
+public class Pig3Thread extends PigThread {
+   public Pig3Thread() {
+      super("nuf-nuf", 2);
+   }
+}
+
