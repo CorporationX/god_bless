e2ce2ba1d8c51eebaec2b527ef78ec3a34fa616f
RasulMurtuzaliev <ras8705@mail.ru>
2023-07-07 22:52:33 +0500

    Completed the task "The Big Bang Theory"

---
+++ src/main/java/faang/school/godbless/Sprint4/task6/BigBangTheory.java
@@ -0,0 +1,17 @@
+package faang.school.godbless.Sprint4.task6;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+public class BigBangTheory {
+    public static void main(String[] args) {
+        ExecutorService execute = Executors.newFixedThreadPool(4);
+
+        execute.execute(new Task("Sheldon", "preparation of the theory"));
+        execute.execute(new Task("Leonard", "simulation of the experiment"));
+        execute.execute(new Task("Penny", "tool development"));
+        execute.execute(new Task("Howard", "data analysis"));
+
+        execute.shutdown();
+    }
+}
