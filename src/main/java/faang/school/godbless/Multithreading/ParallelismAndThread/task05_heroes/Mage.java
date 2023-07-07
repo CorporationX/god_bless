a29cd19420b985fcc3719cd839108eecd5f68a33
Azat <azat.takhautdinov@gmail.com>
2023-07-08 02:47:34 +0500

    Сделал задачу "Распределенная армия героев меча и магии"

---
+++ src/main/java/faang/school/godbless/Multithreading/ParallelismAndThread/task05_heroes/Mage.java
@@ -0,0 +1,12 @@
+package faang.school.godbless.Multithreading.ParallelismAndThread.task05_heroes;
+
+public class Mage extends Unit{
+    public Mage(int power) {
+        super(power);
+    }
+
+    @Override
+    public boolean isMage() {
+        return true;
+    }
+}
---
+++ src/main/java/faang/school/godbless/Multithreading/ParallelismAndThread/task05_heroes/Swordsman.java
@@ -0,0 +1,12 @@
+package faang.school.godbless.Multithreading.ParallelismAndThread.task05_heroes;
+
+public class Swordsman extends Unit{
+    public Swordsman(int power) {
+        super(power);
+    }
+
+    @Override
+    public boolean isSwordsman() {
+        return true;
+    }
+}
