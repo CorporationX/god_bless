0c3a5a06826d829654500aff895838f9595df7a6
ZLATA <585zlata@mail.ru>
2023-07-07 22:47:56 +0300

    Initial commit

--- src/main/java/Sprint_4_Tamagochi_Vlad/TamagotchiVlad.java
+++ src/main/java/Sprint_4_Tamagochi_Vlad/TamagotchiVlad.java
@@ -1,5 +1,38 @@
 package Sprint_4_Tamagochi_Vlad;
 
-public class TamagotchiVlad {
+import lombok.Data;
 
+@Data
+
+public class TamagotchiVlad {
+    private String name;
+
+    public TamagotchiVlad(String name) {
+        this.name = name;
+    }
+
+    public void feed() {
+        synchronized (this) {
+            System.out.println(Thread.currentThread().getName() + ": " + getName() +" вкусно покушал");
+        }
+    }
+
+    public void play() {
+        synchronized (this) {
+            System.out.println(Thread.currentThread().getName() + ": " + getName() + " интересно поиграл");
+        }
+    }
+
+    public void clean() {
+        synchronized (this) {
+            System.out.println(Thread.currentThread().getName() + ": " + getName() + " помылся в душе");
+        }
+    }
+
+    public void sleep() {
+        synchronized (this) {
+            System.out.println(Thread.currentThread().getName() + ": " + getName() + " сладко поспал");
+
+        }
+    }
 }
