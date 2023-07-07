c23dfa9ecdea716df40ec1c87defe0a9aaf86eab
Gordeens <gordeenvs@yandex.ru>
2023-07-07 22:18:15 +0300

    BC-3281. task done, Implemented classes: Robot, SkyNet, Target.

---
+++ src/main/java/faang/school/godbless/multithreadingS4/skyNet/Target.java
@@ -0,0 +1,15 @@
+package faang.school.godbless.multithreadingS4.skyNet;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@AllArgsConstructor
+public class Target {
+    private String name;
+
+    @Setter
+    private int healthPoints;
+
+}
