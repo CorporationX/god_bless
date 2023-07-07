2b9fb10c246335ff3e56783347bdb0560edfb4a7
Andrey <khalyavinandrey@gmail.com>
2023-07-07 17:39:54 +0500

    twitter account class

---
+++ src/main/java/faang/school/godbless/twitter/TwitterAccount.java
@@ -0,0 +1,11 @@
+package faang.school.godbless.twitter;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@AllArgsConstructor
+@Data
+public class TwitterAccount {
+    private String username;
+    private int followers;
+}
