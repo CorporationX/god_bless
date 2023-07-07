b5312a639448232708c47d5aa921ed1a250ad47b
Basimov Rustam <sombra1nf@yandex.ru>
2023-07-07 19:15:22 +0500

    BC-3249. Done

---
+++ src/main/java/faang/school/godbless/sprint5/alchemestry/classes/Potion.java
@@ -0,0 +1,11 @@
+package faang.school.godbless.sprint5.alchemestry.classes;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@AllArgsConstructor
+@Getter
+public class Potion {
+    private String name;
+    private int requiredIngredients;
+}
