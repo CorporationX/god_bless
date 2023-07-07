a6fa06b2323052207dee2212b04ee8102e287154
svetlana.obrancova <obrancova.s@gmail.com>
2023-07-07 19:12:59 +0200

    TELEGBOT-2 Changed all classes

--- src/main/java/faang/school/godbless/telegramBot/Main.java
+++ src/main/java/faang/school/godbless/telegramBot/Main.java
@@ -2,28 +2,19 @@ package faang.school.godbless.telegramBot;
 
 public class Main {
     public static void main(String[] args) {
+
         TelegramBot bot = new TelegramBot();
 
-        Thread[] threads = new Thread[10];
-
-        for (int i = 0; i < 10; i++) {
-            int threadNumber = i + 1;
-            threads[i] = new Thread(() -> {
-                String message = "Thread " + threadNumber + " message";
-                bot.sendMessage(message);
-                System.out.println("Thread " + threadNumber + " sent message");
-            });
-            threads[i].start();
+        for (int i = 0; i < 100; i++) {
+            String message = "Message " + i;
+            new Thread(() -> {
+                try {
+                    bot.sendMessage(message);
+                } catch (InterruptedException e) {
+                    throw new RuntimeException(e);
+                }
+            }).start();
         }
-
-        for (Thread thread : threads) {
-            try {
-                thread.join();
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
-        }
-
-        System.out.println("Task completed");
     }
+
 }
--- src/main/java/faang/school/godbless/telegramBot/TelegramBot.java
+++ src/main/java/faang/school/godbless/telegramBot/TelegramBot.java
@@ -1,42 +1,21 @@
 package faang.school.godbless.telegramBot;
 
-import lombok.Getter;
-import lombok.Setter;
-
-@Getter
-@Setter
+import java.util.concurrent.Semaphore;
+import java.util.concurrent.TimeUnit;
 
 public class TelegramBot {
     private static final int REQUEST_LIMIT = 5;
-    private static final int SECOND_IN_MILLIS = 1000;
+    private final Semaphore semaphore = new Semaphore(REQUEST_LIMIT, true);
 
-    private int requestCounter;
-    private long lastRequestTime;
-
-    public TelegramBot() {
-        this.requestCounter = 0;
-        this.lastRequestTime = System.currentTimeMillis();
+    public void sendMessage(String message) throws InterruptedException {
+        semaphore.acquire();
+        send(message);
+        TimeUnit.SECONDS.sleep(1);
+        semaphore.release();
     }
 
-    public synchronized void sendMessage(String message) {
-        long currentTime = System.currentTimeMillis();
-        long expiredTime = currentTime - lastRequestTime;
-
-        if (expiredTime < SECOND_IN_MILLIS) {
-            requestCounter++;
-
-            if (requestCounter > REQUEST_LIMIT) {
-                try {
-                    wait(SECOND_IN_MILLIS - expiredTime);
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
-                }
-            }
-        } else {
-            requestCounter = 0;
-            lastRequestTime = currentTime;
-        }
-
-        System.out.println("Sending message: " + message);
+    private void send(String msg) {
+        System.out.println(msg + " was sent");
     }
+
 }
