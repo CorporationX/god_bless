e20d406063990891babd253d87c7f7faa5dad5a5
Alex Tarasov <tarasof.av@gmail.com>
2023-07-07 15:12:16 +0300

    fix task Fan out Fan in

--- src/main/java/Multithreading/bc3131_Fan_out_Fan_In/Application.java
+++ src/main/java/Multithreading/bc3131_Fan_out_Fan_In/Application.java
@@ -8,25 +8,25 @@ import java.util.concurrent.Executors;
 
 public class Application {
 
-    static List<SquareRequest> numbers = new ArrayList<>();
-    ResultConsumer resultConsumer = new ResultConsumer(0L);
-
-    public static void main(String[] args) {
-        launch();
-    }
+        public static void main(String[] args) {
+            launch();
+        }
 
     public static Long fanOutFanIn(List<SquareRequest> requests, ResultConsumer resultConsumer) {
         ExecutorService executorService = Executors.newFixedThreadPool(requests.size());
-        CompletableFuture<Long> res = CompletableFuture.runAsync(() -> {
-            requests.forEach(squareRequest -> squareRequest.longTimeSquare(resultConsumer));
-            return resultConsumer.getSumOfSquaredNumbers().get();
-        }, executorService);
+        List<CompletableFuture<Void>> res = requests.stream()
+                .map(squareRequest -> CompletableFuture.runAsync(() ->
+                        squareRequest.longTimeSquare(resultConsumer), executorService))
+                .toList();
+        CompletableFuture.allOf(res.toArray(new CompletableFuture[0])).join();
+        executorService.shutdown();
 
-
-        return null;
+        return resultConsumer.getSumOfSquaredNumbers().get();
     }
 
     public static void launch() {
+        List<SquareRequest> numbers = new ArrayList<>();
+        ResultConsumer resultConsumer = new ResultConsumer(0L);
         for (long i = 1; i <= 1000; i++) {
             numbers.add(new SquareRequest(i));
         }
