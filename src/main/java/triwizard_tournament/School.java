2f0468cf99df58aa48765c03fb8eb5ad5cb732cc
Irina Vereshchagina <irankamoya@gmail.com>
2023-07-07 23:53:22 +0300

    Задача - Турнир трех волшебников

---
+++ src/main/java/triwizard_tournament/School.java
@@ -0,0 +1,27 @@
+package triwizard_tournament;
+
+import java.util.List;
+
+public class School {
+    private String name;
+    private List<Student> students;
+
+    public List<Student> getStudents() {
+        return students;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public School(String name, List<Student> students) {
+        this.name = name;
+        this.students = students;
+    }
+
+    public int getTotalPoints() {
+        return students.stream()
+                .mapToInt(Student::getPoints)
+                .reduce(0, Integer::sum);
+    }
+}
