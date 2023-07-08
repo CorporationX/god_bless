d796355ae793cf3fe0f8990bdf368aa9f69c4b39
Adil Ilchubaev <adil.200191@gmail.com>
2023-07-08 17:22:15 +0600

    подправил замечания

--- src/main/java/faang/school/godbless/Main.java
+++ src/main/java/faang/school/godbless/Main.java
@@ -1,15 +1,28 @@
 package faang.school.godbless;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
         TelegramBot bot = new TelegramBot();
-
+        List<Thread> threads = new ArrayList<>();
         for (int i = 1; i <= 10; i++) {
             int threadNumber = i;
-            new Thread(() -> {
+            Thread thread = new Thread(() -> {
                 String message = "Message from thread " + threadNumber;
                 bot.sendMessage(message);
-            }).start();
+            });
+            thread.start();
+            threads.add(thread);
+        }
+
+        for (Thread thread : threads) {
+            try {
+                thread.join();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
         }
 
         System.out.println("Task completed.");
--- src/main/java/faang/school/godbless/TelegramBot.java
+++ src/main/java/faang/school/godbless/TelegramBot.java
@@ -3,37 +3,39 @@ package faang.school.godbless;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 
-import java.util.Date;
+import java.time.Instant;
 
 @Data
 @AllArgsConstructor
 public class TelegramBot {
     private static final int REQUEST_LIMIT = 5;
     private int requestCounter;
-    private Date lastRequestTime;
+    private Instant lastRequestTime;
 
     public TelegramBot() {
         this.requestCounter = 0;
-        this.lastRequestTime = new Date();
+        this.lastRequestTime = Instant.now();
     }
 
     public synchronized void sendMessage(String message) {
-        Date currentTime = new Date();
-        long timeElapsed = currentTime.getTime() - lastRequestTime.getTime();
+        Instant currentTime = Instant.now();
+        long timeElapsed = currentTime.toEpochMilli() - lastRequestTime.toEpochMilli();
 
         if (timeElapsed < 1000) {
-            requestCounter++;
             if (requestCounter > REQUEST_LIMIT) {
                 try {
                     Thread.sleep(1000 - timeElapsed);
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
+                } catch (InterruptedException ex) {
+                    System.err.println("Thread sleep interrupted again: " + ex.getMessage());
+                    ex.printStackTrace();
                 }
                 requestCounter = 1;
             }
+        } else {
+            requestCounter = 0;  // Сбрасываем счетчик при новом временном интервале
         }
         System.out.println("Send message: " + message);
-
-        lastRequestTime = new Date();
+        requestCounter++;
+        lastRequestTime = Instant.now();
     }
 }
