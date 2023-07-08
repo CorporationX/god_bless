2bde334069892007183ece5b694e8450c99d6c28
Azat <azat.takhautdinov@gmail.com>
2023-07-08 15:07:05 +0500

    Наверное сделал задачу "Carry отожрался"

---
+++ src/main/java/faang/school/godbless/Multithreading/AsynchronismFutureCompletableFuture/task14_Carry/Item.java
@@ -0,0 +1,9 @@
+package faang.school.godbless.Multithreading.AsynchronismFutureCompletableFuture.task14_Carry;
+
+import lombok.Data;
+
+@Data
+public class Item {
+    private String name;
+    private int power;
+}
