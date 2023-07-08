1483b7b24e0f00d0f2ae281c4094a289239fef01
”Ramazan” <mansurov.r13@gmail.com>
2023-07-08 10:37:17 +0300

    BC-3283-FacebookNotifications

---
+++ src/main/java/faang/school/godbless/multithreading/facebook/Notification.java
@@ -0,0 +1,13 @@
+package faang.school.godbless.multithreading.facebook;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.ToString;
+
+@AllArgsConstructor
+@Getter
+@ToString
+public class Notification {
+    private int id;
+    private String message;
+}
