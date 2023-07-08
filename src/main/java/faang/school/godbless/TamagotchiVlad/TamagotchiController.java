7c4359d096a3afd47d10faae3b9f911b3ae38d8d
An Dima <andmitrii94@gmail.com>
2023-07-08 18:34:19 +0900

    fixed

--- src/main/java/faang/school/godbless/TamagotchiVlad/TamagotchiController.java
+++ src/main/java/faang/school/godbless/TamagotchiVlad/TamagotchiController.java
@@ -8,7 +8,7 @@ import java.util.Random;
 
 @Data
 public class TamagotchiController {
-    List<TamagotchiVlad> list = new ArrayList<>();
+    private List<TamagotchiVlad> list = new ArrayList<>();
 
     public synchronized void add() {
         TamagotchiVlad tamagotchiVlad = new TamagotchiVlad(new Random(1000).nextInt());
