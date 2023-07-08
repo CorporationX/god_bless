5af4627c806dfe2fe242f88853b17089ec75d342
Kirill Soshnikov <soshnikov17@gmail.com>
2023-07-08 20:23:43 +0300

    Выполнено задание "Асинхронность и Future"

---
+++ src/main/java/faang/school/godbless/async_future/Main.java
@@ -0,0 +1,8 @@
+package faang.school.godbless.async_future;
+
+public class Main {
+    public static void main(String[] args) {
+        MasterCardService masterCardService = new MasterCardService();
+        masterCardService.doAll();
+    }
+}
