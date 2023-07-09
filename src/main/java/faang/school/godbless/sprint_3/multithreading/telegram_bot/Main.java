f3163d6da1528aefeecf042b4a192f562acc2daf
OneWayMy <zhenya.ivashchenko.9494@gmail.com>
2023-07-08 17:12:44 -0700

    MultiThreading_Tekegram_bot_PR_changes

--- src/main/java/faang/school/godbless/sprint_3/multithreading/telegram_bot/Main.java
+++ src/main/java/faang/school/godbless/sprint_3/multithreading/telegram_bot/Main.java
@@ -1,17 +1,23 @@
 package faang.school.godbless.sprint_3.multithreading.telegram_bot;
 
-import java.time.LocalDateTime;
+import lombok.SneakyThrows;
+
+import java.util.List;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
 
 public class Main {
+    @SneakyThrows
     public static void main(String[] args) {
-        TelegramBot bot = new TelegramBot(LocalDateTime.now());
+        TelegramBot bot = new TelegramBot();
         ExecutorService executorService = Executors.newFixedThreadPool(4);
+        List<String> messages = List.of("Hello", "How are you", "I am fine", "My name is Matt");
         for (int i = 0; i < 4; i++) {
-            executorService.submit(() -> bot.sendMessage("Hello"));
+            int index = i;
+            executorService.submit(() -> bot.sendMessage(messages.get(index)));
         }
-        executorService.shutdown();
+        executorService.awaitTermination(5, TimeUnit.SECONDS);
         System.out.println("Задача выполнена");
     }
 }
--- src/main/java/faang/school/godbless/sprint_3/multithreading/telegram_bot/TelegramBot.java
+++ src/main/java/faang/school/godbless/sprint_3/multithreading/telegram_bot/TelegramBot.java
@@ -1,24 +1,23 @@
 package faang.school.godbless.sprint_3.multithreading.telegram_bot;
 
 import java.time.Duration;
-import java.time.LocalDateTime;
+import java.time.Instant;
 
 public class TelegramBot {
     private final int REQUEST_LIMIT = 5;
     private Integer requestCounter;
-    private LocalDateTime lastRequestTime;
+    private Instant lastRequestTime;
 
-    public TelegramBot(LocalDateTime lastRequestTime) {
-        this.lastRequestTime = lastRequestTime;
+    public TelegramBot() {
+        this.lastRequestTime = Instant.now();
         requestCounter = 0;
     }
 
-    public void sendMessage(String message) {
-        LocalDateTime dateTimeNow = LocalDateTime.now();
+    public synchronized void sendMessage(String message) {
+        Instant dateTimeNow = Instant.now();
         Duration duration = Duration.between(lastRequestTime, dateTimeNow);
         long time = duration.getSeconds();
         if (time < 1) {
-            requestCounter++;
             if (requestCounter > REQUEST_LIMIT) {
                 try {
                     Thread.sleep(1000);
@@ -27,9 +26,10 @@ public class TelegramBot {
                 }
             } else {
                 requestCounter = 0;
-                lastRequestTime = LocalDateTime.now();
+                lastRequestTime = Instant.now();
             }
             System.out.printf("%s отправлено.%n", message);
+            requestCounter++;
         }
     }
 }
