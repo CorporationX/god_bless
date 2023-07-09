a2afbdbcb33af2bdc2621121f33cbb5a299624e6
Devilserk <demonserker@gmail.com>
2023-07-09 18:29:51 +0600

    Исправил

--- src/main/java/faang/school/godbless/multithreading/broforce/BroForce.java
+++ src/main/java/faang/school/godbless/multithreading/broforce/BroForce.java
@@ -5,9 +5,10 @@ import java.util.concurrent.Executors;
 import java.util.concurrent.TimeUnit;
 
 public class BroForce {
+    private static final int NUM_THREADS = 4;
+
     public static void main(String[] args) throws InterruptedException {
         Game game = new Game();
-        final int NUM_THREADS = 4;
         ExecutorService service = Executors.newFixedThreadPool(NUM_THREADS);
 
         for (int i = 0; i < NUM_THREADS; i++) {
@@ -15,6 +16,7 @@ public class BroForce {
         }
         service.shutdown();
         service.awaitTermination(10, TimeUnit.MINUTES);
+        game.gameOver();
         System.out.println("Final score: " + game.getScore());
         System.out.println("Remaining lives: " + game.getLives());
     }
--- src/main/java/faang/school/godbless/multithreading/broforce/Game.java
+++ src/main/java/faang/school/godbless/multithreading/broforce/Game.java
@@ -2,6 +2,7 @@ package faang.school.godbless.multithreading.broforce;
 
 import lombok.Getter;
 
+import java.util.Random;
 import java.util.concurrent.TimeUnit;
 
 @Getter
@@ -9,49 +10,47 @@ public class Game {
     private int score;
     private int lives;
     private volatile boolean isGameOver;
+    private static final int LIVES_COUNT = 5;
     private final Object scoreLock = new Object();
     private final Object livesLock = new Object();
 
     public Game() {
-        score = 0;
-        lives = 5;
-        isGameOver = false;
+        lives = LIVES_COUNT;
     }
 
     public void startGame() {
         while (!isGameOver) {
             update();
         }
-        gameOver();
     }
 
-    /* Будем считать что потоки это игроки, и у них есть общие очки и жизни.
-     * Представим что все игроки это камикадзе и они должны умирать после того как заработают 1 очко.
-     * Пока один игрок мертв, то другие могут увеличить очки.*/
+    /* Теперь игрок случайно либо умирает, либо зарабатывает очки.*/
     private void update() {
-        synchronized (scoreLock) {
-            if (lives == 0) {
-                isGameOver = true;
-                return;
+        if (new Random().nextBoolean()) {
+            synchronized (scoreLock) {
+                if (lives == 0) {
+                    isGameOver = true;
+                    return;
+                }
+                score++;
+                System.out.printf("Score: %s Lives: %s | Player: (%s)\n", score, lives, Thread.currentThread().getName());
             }
-            score++;
-            System.out.printf("Score: %s Lives: %s | Player: (%s)\n", score, lives, Thread.currentThread().getName());
-        }
-
-        synchronized (livesLock) {
-            if (lives >= 1) {
-                lives--;
-                System.out.printf("Died Player: (%s)\n", Thread.currentThread().getName());
+        } else {
+            synchronized (livesLock) {
+                if (lives >= 1) {
+                    lives--;
+                    System.out.printf("Died Player: (%s)\n", Thread.currentThread().getName());
+                }
             }
-        }
-        try {
-            TimeUnit.SECONDS.sleep(1);
-        } catch (InterruptedException e) {
-            throw new RuntimeException(e);
+            try {
+                TimeUnit.SECONDS.sleep(1);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
         }
     }
 
-    private void gameOver() {
+    public void gameOver() {
         System.out.println("Game over! | " + Thread.currentThread().getName());
     }
 }
