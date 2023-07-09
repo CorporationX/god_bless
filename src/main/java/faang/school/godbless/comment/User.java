26dc859d84bae999bd443367feea65ef51057424
Andrey <khalyavinandrey@gmail.com>
2023-07-09 13:32:51 +0500

    user class

---
+++ src/main/java/faang/school/godbless/comment/User.java
@@ -0,0 +1,26 @@
+package faang.school.godbless.comment;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Getter
+@Setter
+public class User {
+    private static int counter;
+    private int id = counter;
+    private String username;
+    private int age;
+    private List<Comment> comments = new ArrayList<>();
+    private List<Post> posts = new ArrayList<>();
+
+    public User(String username, int age) {
+        this.username = username;
+        this.age = age;
+        counter++;
+    }
+}
