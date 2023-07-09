7bbf281773ed015f5c299b944b80f70904fa4270
Никита <nikita.armeec.88@mail.ru>
2023-07-09 10:37:15 +0300

    Созданы классы: Donation, Organization, Main с методом main

---
+++ src/main/java/faang/school/godbless/kxnvg/animalsupport/Donation.java
@@ -0,0 +1,12 @@
+package faang.school.godbless.kxnvg.animalsupport;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@AllArgsConstructor
+@Getter
+public class Donation {
+
+    private int id;
+    private int amount;
+}
