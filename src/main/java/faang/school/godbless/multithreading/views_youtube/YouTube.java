3d2fb3ad607148aac5299766ba53158bc77c1669
Devilserk <demonserker@gmail.com>
2023-07-11 01:21:24 +0600

    Исправил переменные в YouTube

--- src/main/java/faang/school/godbless/multithreading/views_youtube/YouTube.java
+++ src/main/java/faang/school/godbless/multithreading/views_youtube/YouTube.java
@@ -8,13 +8,13 @@ import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
 public class YouTube {
+    private static final int NUM_THREADS = 100;
+    private static final int NUM_VIDEOS = 5;
+
     public static void main(String[] args) throws InterruptedException {
-        final int NUM_THREADS = 100;
-        final int NUM_VIDEOS = 5;
         Map<Integer, Integer> views = IntStream.rangeClosed(1, NUM_VIDEOS).boxed().collect(Collectors.toMap(i -> i, i -> 0));
         VideoManager videoManager = new VideoManager(views);
         ExecutorService service = Executors.newFixedThreadPool(NUM_THREADS);
-
         for (int i = 1; i <= NUM_VIDEOS; i++) {
             for (int j = 1; j < NUM_THREADS / NUM_VIDEOS; j++) {
                 final int id = i;
