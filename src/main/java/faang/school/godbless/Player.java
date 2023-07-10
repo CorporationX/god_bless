ff15c25fbeeaf928ab449d1441c8c89f256cfb30
Stepan Palkov <step.palkov@gmail.com>
2023-07-11 01:45:59 +0300

    complete wow

---
+++ src/main/java/faang/school/godbless/Player.java
@@ -0,0 +1,12 @@
+package faang.school.godbless;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class Player {
+    private String name;
+    private int level;
+    private int experience;
+}
---
+++ src/main/java/faang/school/godbless/Quest.java
@@ -0,0 +1,11 @@
+package faang.school.godbless;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+@AllArgsConstructor
+@Data
+public class Quest {
+    private String name;
+    private int difficulty;
+    private int reward;
+}
