858f6cddd69cf76b822f70ee9e944a79a8cd396c
zhugalcf <zhugalcf@gmail.com>
2023-07-12 11:41:36 +0300

    Make changes

--- src/main/java/faang/school/godbless/Sprint4Future/SendARaven/MessageSender.java
+++ src/main/java/faang/school/godbless/Sprint4Future/SendARaven/MessageSender.java
@@ -9,19 +9,19 @@ public class MessageSender {
         MessageSender sender = new MessageSender();
         Kingdom lanisters = new Kingdom("Lanisters");
         Kingdom starks = new Kingdom("Starks");
-        sender.sendRaven(lanisters,starks,"Hi Starks");
+        CompletableFuture<String> hiStarks = sender.sendRaven(lanisters, starks, "Hi Starks");
+        try {
+            System.out.println(hiStarks.get());
+        } catch (InterruptedException | ExecutionException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     public CompletableFuture<String> sendRaven(Kingdom kingdomFrom, Kingdom kingdomTo, String message){
         CompletableFuture<String> sendingMessage = kingdomFrom.sendMessage(kingdomTo, message);
-        try {
-            System.out.println(sendingMessage.get());
-        } catch (InterruptedException | ExecutionException e) {
-            throw new RuntimeException(e);
-        }
         return sendingMessage.handle((sentMessage, exception) -> {
-            if (message != null){
-                return message;
+            if (sentMessage != null){
+                return sentMessage;
             } else {
                 return exception.getMessage();
             }
