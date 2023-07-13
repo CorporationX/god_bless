5d2e9b5f9fddba8da059f0c669a86c2c608f385c
Devilserk <demonserker@gmail.com>
2023-07-13 22:07:46 +0600

    Исправил логику в CashierThread

--- src/main/java/faang/school/godbless/multithreading/queue_walmart/CashierThread.java
+++ src/main/java/faang/school/godbless/multithreading/queue_walmart/CashierThread.java
@@ -8,18 +8,16 @@ import java.util.Arrays;
 @AllArgsConstructor
 public class CashierThread extends Thread {
     private int cashierId;
-    private int[][] customers;
+    private int[] items;
 
     @Override
     public void run() {
-        for (int i = 0; i < customers.length; i++) {
-            int totalAmount = Arrays.stream(customers[i]).sum();
-            try {
-                Thread.sleep(1000);
-            } catch (InterruptedException e) {
-                throw new RuntimeException(e);
-            }
-            System.out.printf("CashierId: %s | CustomerId: %s | Total Amount: %s\n", cashierId, i, totalAmount);
+        int totalAmount = Arrays.stream(items).sum();
+        try {
+            Thread.sleep(1000);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
         }
+        System.out.printf("CashierId: %s | Total Amount: %s\n", cashierId, totalAmount);
     }
 }
--- src/main/java/faang/school/godbless/multithreading/queue_walmart/Walmart.java
+++ src/main/java/faang/school/godbless/multithreading/queue_walmart/Walmart.java
@@ -1,16 +1,10 @@
 package faang.school.godbless.multithreading.queue_walmart;
 
-import java.util.List;
-
 public class Walmart {
     public static void main(String[] args) {
-        int[][] customers1 = {{5, 7, 3, 2}, {1, 2, 3}, {6, 5, 2}, {10, 2, 1, 3, 5}};
-        int[][] customers2 = {{6, 3, 3, 1}, {3, 4, 3}, {8, 11, 2}, {13, 5, 1, 3, 5}};
-
-        List<CashierThread> cashiers = List.of(
-                new CashierThread(1, customers1),
-                new CashierThread(2, customers2)
-        );
-        cashiers.forEach(CashierThread::start);
+        int[][] customers = {{5, 7, 3, 2}, {1, 2, 3}, {6, 5, 2}, {10, 2, 1, 3, 5}};
+        for (int i = 0; i < customers.length; i++) {
+            new CashierThread(i, customers[i]).start();
+        }
     }
 }
