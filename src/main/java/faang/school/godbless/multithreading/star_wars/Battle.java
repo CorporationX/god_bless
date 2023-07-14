7d9c6a5f5d537fe5a09898da98fc76ec9ca231ce
”Ramazan” <mansurov.r13@gmail.com>
2023-07-14 18:46:40 +0300

    BC-3030-StarWarsArena fixes after review

--- src/main/java/faang/school/godbless/multithreading/star_wars/Battle.java
+++ src/main/java/faang/school/godbless/multithreading/star_wars/Battle.java
@@ -7,32 +7,21 @@ import java.util.concurrent.Executors;
 import java.util.concurrent.Future;
 
 public class Battle {
-    private final Random random;
-
-    public Battle() {
-        random = new Random();
-    }
 
     public Future<Robot> fight(Robot robot1, Robot robot2) {
-        ExecutorService executor = Executors.newCachedThreadPool();
+        ExecutorService executor = Executors.newFixedThreadPool(2);
 
         Future<Robot> winner = executor.submit(() -> {
-            int robot1TotalPower = robot1.getAttackPower() + robot1.getDefencePower();
-            int robot2TotalPower = robot2.getAttackPower() + robot2.getDefencePower();
+            int robot1Score = robot1.getAttackPower() - robot2.getDefencePower();
+            int robot2Score = robot2.getAttackPower() - robot1.getDefencePower();
 
             try {
-                Thread.sleep(random.nextInt(1000, 5000));
+                Thread.sleep(new Random().nextInt(1000, 5000));
             } catch (InterruptedException e) {
                 e.printStackTrace();
             }
 
-            if (robot1TotalPower > robot2TotalPower) {
-                return robot1;
-            } else if (robot2TotalPower > robot1TotalPower) {
-                return robot2;
-            } else {
-                return List.of(robot1, robot2).get(random.nextInt(2));
-            }
+            return robot1Score > robot2Score ? robot1 : robot2;
         });
 
         executor.shutdown();
