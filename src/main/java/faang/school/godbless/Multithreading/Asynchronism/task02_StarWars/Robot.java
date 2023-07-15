843b7af9d55981f4b413a0f0728419e2b9dd1f18
Azat <azat.takhautdinov@gmail.com>
2023-07-15 16:10:46 +0500

    Сделал задачу "Star Wars Arena"

---
+++ src/main/java/faang/school/godbless/Multithreading/Asynchronism/task02_StarWars/Robot.java
@@ -0,0 +1,12 @@
+package faang.school.godbless.Multithreading.Asynchronism.task02_StarWars;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@AllArgsConstructor
+@Getter
+public class Robot {
+    private String name;
+    private int attackPower;
+    private int defensePower;
+}
