8418ac5960f74c4fccd74ba1573905fc45ddb340
Sergey Gerasimov <czar997@yandex.ru>
2023-07-15 17:50:33 +0300

    fix

--- src/main/java/faang/school/godbless/sprint5/multithreading_conc/task1_comment/PostService.java
+++ src/main/java/faang/school/godbless/sprint5/multithreading_conc/task1_comment/PostService.java
@@ -5,6 +5,7 @@ import lombok.Setter;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.locks.ReentrantLock;
 
 @Getter
 @Setter
@@ -12,45 +13,54 @@ public class PostService {
 
     private volatile List<Post> posts = new ArrayList<>();
 
+    private ReentrantLock lock = new ReentrantLock();
+
     public void addComment(int id, Comment comment) {
-        synchronized (posts) {
+        lock.lock();
+        try {
             posts.get(id).getComments().add(comment);
             System.out.println("Коммент " + comment + " добавлен к посту " + id);
+        } finally {
+            lock.unlock();
         }
     }
 
     public void addPost(Post post) {
-        synchronized (posts) {
-            posts.add(post);
-            System.out.println("Пост " + post + " добавлен");
-        }
+        posts.add(post);
+        System.out.println("Пост " + post + " добавлен");
     }
 
     public Post getPostById(int id) {
-        synchronized (posts) {
-            return posts.get(id);
-        }
+        return posts.get(id);
+
     }
 
     public void deletePost(Post post, String author) {
-        synchronized (posts) {
+        lock.lock();
+        try {
             if (post.getAuthor().equals(author)) {
                 posts.remove(post);
                 System.out.println("Пост " + post + " удален пользователем " + author);
             } else {
                 System.out.println("Вы не можете удалить пост другого пользователя!");
             }
+        } finally {
+            lock.unlock();
         }
+
     }
 
     public void deleteComment(int id, Comment comment, String author) {
-        synchronized (posts) {
+        lock.lock();
+        try {
             if (comment.getAuthor().equals(author)) {
                 posts.get(id).getComments().remove(comment);
                 System.out.println("Комментарий " + comment + " удален пользователем " + author + " от поста " + id);
             } else {
                 System.out.println("Вы не можете удалить комментарий от другого пользователя!");
             }
+        } finally {
+            lock.unlock();
         }
     }
 }
