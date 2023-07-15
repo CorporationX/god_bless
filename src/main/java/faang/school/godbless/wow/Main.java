f24d8225793c98f5c52ae06267bc8ca376fea15a
Zhargal Budaev <budaev17@mail.ru>
2023-07-15 21:25:00 +0700

    перенес вывод результата в консоль из метода startQuest в метод thanAccept

--- src/main/java/faang/school/godbless/wow/Main.java
+++ src/main/java/faang/school/godbless/wow/Main.java
@@ -9,7 +9,7 @@ public class Main {
     public static void main(String[] args) {
         QuestSystem questSystem = new QuestSystem();
 
-        Player player1 = new Player("Thrall", 10, 250);
+        Player player1 = new Player("Thrall", 11, 250);
         Player player2 = new Player("Sylvanas", 12, 450);
         Player player3 = new Player("Arthas", 15, 600);
 
@@ -18,16 +18,11 @@ public class Main {
         Quest quest2 = new Quest("Retrieve the Sword of Azeroth", 8, 100);
 
 
-        CompletableFuture<Player> player1Quest = questSystem.startQuest(player1, quest1);
-        CompletableFuture<Player> player2Quest = questSystem.startQuest(player2, quest2);
-        CompletableFuture<Player> player3Quest = questSystem.startQuest(player3, quest2);
-
-        try {
-            player1Quest.get();
-            player2Quest.get();
-            player3Quest.get();
-        } catch (InterruptedException | ExecutionException e) {
-            throw new RuntimeException(e);
-        }
+        questSystem.startQuest(player1, quest1).thenAccept(player -> System.out.println(player.getName() + " has completed the quest and now has " + player.getExperience() + " experience points."))
+                .join();
+        questSystem.startQuest(player2, quest2).thenAccept(player -> System.out.println(player.getName() + " has completed the quest and now has " + player.getExperience() + " experience points."))
+                .join();
+        questSystem.startQuest(player3, quest2).thenAccept(player -> System.out.println(player.getName() + " has completed the quest and now has " + player.getExperience() + " experience points."))
+                .join();
     }
 }
--- src/main/java/faang/school/godbless/wow/QuestSystem.java
+++ src/main/java/faang/school/godbless/wow/QuestSystem.java
@@ -9,16 +9,15 @@ public class QuestSystem {
     public CompletableFuture<Player> startQuest(Player player, Quest quest) {
         CompletableFuture<Player> playerOnQuest = CompletableFuture.supplyAsync(() -> {
             Random random = new Random();
-            if(player.getLevel() <= quest.getDifficulty()) {
-                System.out.println(player.getName() + " not ready yet for this quest!");
-            } else {
+            if(player.getLevel() > quest.getDifficulty()) {
                 try {
                     Thread.sleep((random.nextInt(5)+1)*1000);
                 } catch (InterruptedException e) {
                     throw new RuntimeException(e);
                 }
                 player.setExperience(player.getExperience() + quest.getReward());
-                System.out.println(player.getName() + " has completed the quest and now has " + player.getExperience() + " experience points.");
+            } else {
+                throw new RuntimeException(player.getName() + " not ready yet for this quest!");
             }
             return player;
         });
