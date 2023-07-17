536e6819e6afe4e74eea52d8e8b32c497668c34e
”Ramazan” <mansurov.r13@gmail.com>
2023-07-17 12:50:26 +0300

    BC-3359-Twitter fixes after review

--- src/main/java/faang/school/godbless/multithreading/twitter/TwitterSubscriptionSystem.java
+++ src/main/java/faang/school/godbless/multithreading/twitter/TwitterSubscriptionSystem.java
@@ -3,7 +3,7 @@ package faang.school.godbless.multithreading.twitter;
 import java.util.concurrent.CompletableFuture;
 
 public class TwitterSubscriptionSystem {
-    public void addFollower(TwitterAccount follower) {
+    public synchronized void addFollower(TwitterAccount follower) {
         follower.getFollowers().incrementAndGet();
     }
 
