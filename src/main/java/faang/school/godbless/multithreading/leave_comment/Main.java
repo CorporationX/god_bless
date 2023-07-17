ff3da2416bc977142dcd776a38af83ba9698c1e9
”Ramazan” <mansurov.r13@gmail.com>
2023-07-17 14:01:45 +0300

    BC-3399-LeaveAComment fixes after review

--- src/main/java/faang/school/godbless/multithreading/leave_comment/Main.java
+++ src/main/java/faang/school/godbless/multithreading/leave_comment/Main.java
@@ -1,28 +1,26 @@
 package faang.school.godbless.multithreading.leave_comment;
 
-import java.util.List;
 import java.util.Random;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
 import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
 public class Main {
-    private final Random random = new Random();
     private final PostService service = new PostService();
 
-
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
         Main main = new Main();
 
         Author authorOne = new Author(1, "Author #1");
         Author authorTwo = new Author(2, "Author #2");
 
-        List<Thread> adderThreads = List.of(
-                main.getPostAdderThread(authorOne, 1, 5),
-                main.getPostAdderThread(authorTwo, 6, 10),
-                main.getCommentAdderThread(authorOne, 1, 5),
-                main.getCommentAdderThread(authorTwo, 6, 10)
-        );
-        adderThreads.forEach(Thread::start);
+        ExecutorService executor = Executors.newFixedThreadPool(4);
+        executor.execute(main.getPostAdderThread(authorOne, 1, 5));
+        executor.execute(main.getPostAdderThread(authorTwo, 6, 10));
+        executor.execute(main.getCommentAdderThread(authorOne, 1, 5));
+        executor.execute(main.getCommentAdderThread(authorTwo, 6, 10));
 
         try {
             Thread.sleep(2000);
@@ -30,19 +28,13 @@ public class Main {
             log.error(e.getMessage());
         }
 
-        List<Thread> removerThreads = List.of(
-                main.getPostRemoverThread(authorOne, 1, 5),
-                main.getPostRemoverThread(authorTwo, 6, 10),
-                main.getCommentRemoverThread(authorOne, 1, 5),
-                main.getCommentRemoverThread(authorTwo, 6, 10)
-        );
-        removerThreads.forEach(Thread::start);
+        executor.execute(main.getPostRemoverThread(authorOne, 1, 5));
+        executor.execute(main.getPostRemoverThread(authorTwo, 6, 10));
+        executor.execute(main.getCommentRemoverThread(authorOne, 1, 5));
+        executor.execute(main.getCommentRemoverThread(authorTwo, 6, 10));
 
-        try {
-            Thread.sleep(2000);
-        } catch (InterruptedException e) {
-            log.error(e.getMessage());
-        }
+        executor.shutdown();
+        executor.awaitTermination(5, TimeUnit.SECONDS);
 
         main.service.getPosts().forEach(System.out::println);
     }
@@ -54,7 +46,7 @@ public class Main {
                         service.addPost(new Post(i, "Post #" + i, "Text #" + i, author));
                     }
                     try {
-                        Thread.sleep(random.nextInt(100, 500));
+                        Thread.sleep(new Random().nextInt(100, 500));
                     } catch (InterruptedException e) {
                         log.error(e.getMessage());
                     }
@@ -70,7 +62,7 @@ public class Main {
                         service.addComment(new Comment(i, "Comment #" + i, author), i);
                     }
                     try {
-                        Thread.sleep(random.nextInt(100, 1000));
+                        Thread.sleep(new Random().nextInt(100, 1000));
                     } catch (InterruptedException e) {
                         log.error(e.getMessage());
                     }
@@ -86,7 +78,7 @@ public class Main {
                         service.removePost(author, i);
                     }
                     try {
-                        Thread.sleep(random.nextInt(100, 500));
+                        Thread.sleep(new Random().nextInt(100, 500));
                     } catch (InterruptedException e) {
                         log.error(e.getMessage());
                     }
@@ -102,7 +94,7 @@ public class Main {
                         service.removeComment(author, i);
                     }
                     try {
-                        Thread.sleep(random.nextInt(100, 500));
+                        Thread.sleep(new Random().nextInt(100, 500));
                     } catch (InterruptedException e) {
                         log.error(e.getMessage());
                     }
