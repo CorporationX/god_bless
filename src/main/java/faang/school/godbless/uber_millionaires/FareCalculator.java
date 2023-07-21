612ca972d08035271d05f3eb0149aca730570ff2
Tupik Maxim <m-tupik2006@mail.ru>
2023-07-21 20:29:59 +0700

    Миллиончики Uber https://faang-school.com/courses/jvxzj9d6/32dnx7dg

---
+++ src/main/java/faang/school/godbless/uber_millionaires/FareCalculator.java
@@ -0,0 +1,28 @@
+package faang.school.godbless.uber_millionaires;
+
+import java.util.function.BiFunction;
+
+public class FareCalculator {
+    public Double calculateFare(Double time, Double distance, BiFunction<Double, Double, Double> calculate) {
+        return calculate.apply(time, distance);
+    }
+
+    public static void main(String[] args) {
+        FareCalculator fareCalculator = new FareCalculator();
+
+        BiFunction<Double, Double, Double> economyFare = (distance, time) -> 1.0 * distance + 0.5 * time;
+        BiFunction<Double, Double, Double> comfortFare = (distance, time) -> 1.5 * distance + 0.8 * time;
+        BiFunction<Double, Double, Double> premiumFare = (distance, time) -> 2.0 * distance + 1.5 * time;
+
+        double distance = 10.0;
+        double time = 15.0;
+
+        double economyTripCost = fareCalculator.calculateFare(distance, time, economyFare);
+        double comfortTripCost = fareCalculator.calculateFare(distance, time, comfortFare);
+        double premiumTripCost = fareCalculator.calculateFare(distance, time, premiumFare);
+
+        System.out.println("Стоимость поездки на тарифе Economy: " + economyTripCost);
+        System.out.println("Стоимость поездки на тарифе Comfort: " + comfortTripCost);
+        System.out.println("Стоимость поездки на тарифе Premium: " + premiumTripCost);
+    }
+}
