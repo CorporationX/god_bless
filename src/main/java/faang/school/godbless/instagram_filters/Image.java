d4bd687a0f0bf59ce0e9aa86e4086fcf0327207f
Tupik Maxim <m-tupik2006@mail.ru>
2023-07-21 19:40:16 +0700

    Фильтры Instagram https://faang-school.com/courses/jvxzj9d6/ynz2xezg

---
+++ src/main/java/faang/school/godbless/instagram_filters/Image.java
@@ -0,0 +1,11 @@
+package faang.school.godbless.instagram_filters;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class Image {
+    private String name;
+    private String description;
+}
