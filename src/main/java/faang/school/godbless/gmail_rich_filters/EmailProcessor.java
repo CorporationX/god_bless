d80eb42198b9138e563df18f941790bc6a8d918c
Tupik Maxim <m-tupik2006@mail.ru>
2023-07-21 22:09:40 +0700

    Gmail Rich Filters https://faang-school.com/courses/jvxzj9d6/69dr9bz8

---
+++ src/main/java/faang/school/godbless/gmail_rich_filters/EmailProcessor.java
@@ -0,0 +1,36 @@
+package faang.school.godbless.gmail_rich_filters;
+
+import java.sql.SQLOutput;
+import java.util.Arrays;
+import java.util.List;
+import java.util.function.Consumer;
+import java.util.function.Function;
+import java.util.function.Predicate;
+
+public class EmailProcessor {
+    public void processEmails(List<Email> emails, Predicate<Email> importantFilter, Consumer<Email> printEmail, Function<Email, String> toUpperCase) {
+        emails.stream()
+                .filter(importantFilter)
+                .forEach(email -> {
+                    printEmail.accept(email);
+                    String uppercase = toUpperCase.apply(email);
+                    System.out.println("Переобразованное письмо: " + uppercase);
+                });
+    }
+
+    public static void main(String[] args) {
+        EmailProcessor emailProcessor = new EmailProcessor();
+
+        List<Email> emails = Arrays.asList(
+                new Email("Письмо 1", "Текст письма 1", false),
+                new Email("Письмо 2", "Текст письма 2", true),
+                new Email("Спам", "Текст спама", false)
+        );
+
+        Predicate<Email> importantFilter = Email::isImportant;
+        Consumer<Email> printEmail = email -> System.out.println("Обработано письмо: " + email.getSubject());
+        Function<Email, String> toUpperCase = email -> email.getBody().toUpperCase();
+
+        emailProcessor.processEmails(emails, importantFilter, printEmail, toUpperCase);
+    }
+}
