7fefd5bf9dbcad355b9661122ae0c9bfac241b4b
Edisolt Khatsiev <Edisolt Khatsiev>
2023-12-05 10:21:42 +0300

    Развернитесь!

---
+++ src/main/java/faang/school/godbless/BJS2_412/Example.java
@@ -0,0 +1,16 @@
+package faang.school.godbless.BJS2_412;
+
+public class Example {
+    public static void reverse (int... value) {
+
+        int length = value.length;
+
+        for (int i = 0; i < length/2; i++) {
+            int temp = value[i];
+
+            value[i] = value[length-1 - i];
+            value[length-1 - i] = temp;
+        }
+
+    }
+}
