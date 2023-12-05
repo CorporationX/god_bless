1f6b9c458fbc00e8bc5c3c3bbf69c5ef10784ff6
Vlad Meniailov <vlad.meniailov@yandex.ru>
2023-12-05 18:34:23 +0300

    test commit

---
+++ src/main/java/faang/school/godbless/collectUsers/Main.java
@@ -0,0 +1,17 @@
+package faang.school.godbless.collectUsers;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) {
+        List<String> hobbies = new ArrayList<>();
+        hobbies.add("Domino");
+        hobbies.add("Domino");
+        hobbies.add("Domino");
+        hobbies.add("Domino");
+        hobbies.add("Domino");
+
+
+    }
+}
---
+++ src/main/java/faang/school/godbless/collectUsers/User.java
@@ -0,0 +1,38 @@
+package faang.school.godbless.collectUsers;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
+@AllArgsConstructor
+@Getter
+@Setter
+public class User {
+    private int id;
+    private String name;
+    private int age;
+    Set<String> activities;
+
+    Map<User, String> findHobbyLovers(List<User> usersList, List<String> activities){
+        Map<User, String> hobbyLovers = new HashMap<>();
+        Iterator iterator;
+        iterator = usersList.iterator();
+        while (iterator.hasNext()){
+            User  user = (User) iterator.next();
+            Set<String> userActivities = user.getActivities();
+            for (String activity : userActivities){
+                if (activities.contains(activity)){
+                    hobbyLovers.put(user,activity);
+                    break;
+                }
+            }
+        }
+        return hobbyLovers;
+    }
+}
