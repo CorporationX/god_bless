c5351fa4cb456286cae9fb5c34f8327c7f4eba8b
lonovojvladimir <lonovojvladimir@gmail.com>
2023-12-05 22:42:37 +0300

    new Turn around method

--- src/main/java/faang/school/godbless/Application.java
+++ src/main/java/faang/school/godbless/Application.java
@@ -2,6 +2,8 @@ package faang.school.godbless;
 
 public class Application {
     public static void main(String... args) {
+        TurnAround turnAround = new TurnAround();
+        turnAround.sort(new int[] {5,4,3,2,1});
         System.out.println("Hello world");
     }
 }
---
+++ src/main/java/faang/school/godbless/TurnAround.java
@@ -0,0 +1,13 @@
+package faang.school.godbless;
+
+public class TurnAround {
+    public int[] sort(int[] array){
+        for(int i=0; i< array.length/2; i++) {
+            int swap = array[i];
+            array[i]=array[array.length-i-1];
+            array[array.length-i-1]=swap;
+        }
+        return array;
+    }
+
+}
