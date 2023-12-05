92ce963ca1c89fadb2ceedb3516c018d91301b63
Marina Razina <razinamv3@gmail.com>
2023-12-05 22:01:03 +0300

    add USER class + edit from code review

--- src/main/java/faang/school/godbless/group_by/User.java
+++ src/main/java/faang/school/godbless/group_by/User.java
@@ -9,14 +9,14 @@ public class User {
     private String name;
     private Integer age;
     private String work;
-    private String adress;
+    private String address;
 
 
-    public User(String name, Integer age, String work, String adress) {
+    public User(String name, Integer age, String work, String address) {
         this.name = name;
         this.age = age;
         this.work = work;
-        this.adress = adress;
+        this.address = address;
     }
 
     @Override
@@ -25,17 +25,47 @@ public class User {
                 "name='" + name + '\'' +
                 ", age=" + age +
                 ", work='" + work + '\'' +
-                ", adress='" + adress + '\'' +
+                ", address='" + address + '\'' +
                 '}';
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+    public String getWork() {
+        return work;
+    }
+
+    public void setWork(String work) {
+        this.work = work;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
     }
 
     public static Map<Integer, List<User>> groupUsers(List<User> listUser) {
 
         Map<Integer, List<User>> usersGroup = new HashMap<>();
         for (User user : listUser) {
-            if (!usersGroup.containsKey(user.age)) {
-                usersGroup.put(user.age, new ArrayList<>());
-            }
+            usersGroup.putIfAbsent(user.age, new ArrayList<>());
             usersGroup.get(user.age).add(user);
         }
         return usersGroup;
