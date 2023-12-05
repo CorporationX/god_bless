69cd9b237566a973bfb12ef49bc7cd9bf5852546
Rauan Tureyev <rauanture@gmail.com>
2023-12-05 22:15:11 +0600

    grouping by age using the static method

---
+++ src/main/java/faang/school/godbless/User.java
@@ -0,0 +1,43 @@
+package faang.school.godbless;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class User {
+    private String name;
+    private int age;
+    private String workplace;
+    private String address;
+
+    public String getAddress() {
+        return address;
+    }
+
+    public String getWorkplace() {
+        return workplace;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public static Map<Integer, List<User>> groupUsers(List<User> listOfUsers){
+        Map<Integer, List<User>> result = new HashMap<>();
+        for (User user : listOfUsers) {
+            int key = user.getAge();
+            List<User> value = result.getOrDefault(key,new ArrayList<>());
+            value.add(user);
+            result.put(key, value);
+        }
+        for (Map.Entry<Integer, List<User>> qwe : result.entrySet()) {
+            System.out.println(qwe.getKey()+": "+qwe.getValue());
+        }
+        return result;
+    }
+}
