df200c30b9f4984d0295fe8bd234905a3b5c9f59
Kamil Gasanov <gasanovkamil516@gmail.com>
2023-12-06 01:29:57 +0300

    finished "Group users"

--- src/main/java/faang/school/godbless/User.java
+++ src/main/java/faang/school/godbless/User.java
@@ -6,15 +6,25 @@ import java.util.*;
 
 @Data
 public class User {
+
     private String userName;
     private int userAge;
     private String userPlaceWork;
     private String userAddress;
 
+    public User(String userName, int userAge, String userPlaceWork, String userAddress) {
+        this.userName = userName;
+        this.userAge = userAge;
+        this.userPlaceWork = userPlaceWork;
+        this.userAddress = userAddress;
+    }
+
     public static Map<Integer, List<User>> groupUsers(List<User> users) {
         Map<Integer, List<User>> groupedUser= new HashMap<>();
 
         for (User user: users) {
+            // Метод getUserAge() создает lombock, все компилируется
+            // Я тестировал, но тест не сохронял
             int age = user.getUserAge();
 
             if(!groupedUser.containsKey(age)) {
