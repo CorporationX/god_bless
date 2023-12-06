c6cad3c32367e9d9c292974a2538cafbd074668d
Kamil Gasanov <gasanovkamil516@gmail.com>
2023-12-06 04:09:09 +0300

    fix

--- src/main/java/faang/school/godbless/Application.java
+++ src/main/java/faang/school/godbless/Application.java
@@ -5,13 +5,6 @@ import java.util.List;
 
 public class Application {
     public static void main(String... args) {
-        User user1 = new User("ivan", 15, "faang","lermontova");
-        User user2 = new User("petia", 15, "zavod","lenina");
-        User user3 = new User("lena", 16, "pecarnia","perspectivnaia");
-
-        List<User> users = List.of(user1, user2, user3);
-
-        System.out.println(User.groupUsers(users));
 
     }
 }
