9da7e71c15fc182b10223a05c339d110a81cef63
v.dyakov <v.dyakov@sbereducation.ru>
2023-12-06 10:33:25 +0300

    feature: add new class user (BJS2-439)

--- src/main/java/faang/school/godbless/groupUsers/GroupUserApp.java
+++ src/main/java/faang/school/godbless/groupUsers/GroupUserApp.java
@@ -1,2 +1,22 @@
-package faang.school.godbless.groupUsers;public class GroupUserApp {
+package faang.school.godbless.groupUsers;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static faang.school.godbless.groupUsers.User.groupUsers;
+
+public class GroupUserApp {
+    public static void main(String... args) {
+        List<User> users = new ArrayList<>();
+
+        users.add(new User("Alice", 25, "Developer", "123 Street A"));
+        users.add(new User("Bob", 30, "Designer", "456 Street B"));
+        users.add(new User("Charlie", 25, "Artist", "789 Street C"));
+        users.add(new User("David", 28, "Engineer", "321 Street D"));
+        users.add(new User("Eve", 32, "Manager", "654 Street E"));
+        users.add(new User("Frank", 40, "Teacher", "987 Street F"));
+
+
+        System.out.println(groupUsers(users).get(25));
+    }
 }
--- src/main/java/faang/school/godbless/groupUsers/User.java
+++ src/main/java/faang/school/godbless/groupUsers/User.java
@@ -1,12 +1,41 @@
-package faang.school.godbless;
+package faang.school.godbless.groupUsers;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 public class User {
-    private long id;
-    private String username;
-    private String email;
-    private int age;
+    private String name;
+    private Integer age;
+    private String job;
+    private String address;
 
-    private groupUsers() {
-        
+    public User(String name, Integer age, String job, String address) {
+        this.name = name;
+        this.age = age;
+        this.job = job;
+        this.address = address;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public static Map<Integer, List<User>> groupUsers(List<User> users) {
+        Map<Integer, List<User>> newUsersList = new HashMap();
+        for (User user: users) {
+            int key = user.getAge();
+            List<User> value = newUsersList.get(key);
+
+            if (value == null) {
+                value = new ArrayList<>();
+            }
+
+            value.add(user);
+            newUsersList.put(user.getAge(), value);
+        }
+
+        return newUsersList;
     }
 }
