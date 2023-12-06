23fd79e16e94246c9ffb37995ca09e6dec71caf0
Teo Shtanko <atraxlxxxl@gmail.com>
2023-12-06 14:39:49 +0300

    Работает

---
+++ src/main/java/faang/school/godbless/user_registration/User.java
@@ -0,0 +1,35 @@
+package faang.school.godbless.user_registration;
+
+import java.util.Set;
+
+public class User {
+    public String name;
+    public  int age;
+    public String  work_place;
+    public String address;
+    final static Set<String> VALID_JOBS =Set.of("Google","Uber","Amazon");
+    final static Set<String> VALID_ADDRESSES= Set.of("London","New York","Amsterdam");
+    final static int age1 =18;
+
+    public User(String name, int age, String work_place, String address) {
+
+        if (!name.isEmpty() && age >= age1 && VALID_JOBS.contains(work_place) && VALID_ADDRESSES.contains(address)) {
+            this.name = name;
+            this.age = age;
+            this.work_place = work_place;
+            this.address = address;
+        } else {
+            throw new IllegalArgumentException();
+        }
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "name='" + name + '\'' +
+                ", age=" + age +
+                ", work_place='" + work_place + '\'' +
+                ", address='" + address + '\'' +
+                '}';
+    }
+}
