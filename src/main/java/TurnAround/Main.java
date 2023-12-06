3d27312f5e4b579ebebefe7b77e67891534420cd
FD_Shell <dariuskpp@gmail.com>
2023-12-06 15:46:25 +0300

    Created class Example with method reverse Created class Main

---
+++ src/main/java/TurnAround/Main.java
@@ -0,0 +1,12 @@
+package TurnAround;
+
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) {
+
+        int[] nums = new int[]{1, 2, 3, 4, 5, 6};
+
+        System.out.println(Arrays.toString(Example.reverse(nums)));
+    }
+}
