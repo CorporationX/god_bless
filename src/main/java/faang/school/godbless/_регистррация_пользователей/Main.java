2c893f8a4ccf957d744b545fa0be2c4f31a9d27e
Teo Shtanko <atraxlxxxl@gmail.com>
2023-12-06 14:24:08 +0300

    Кажется норм, но не работает

---
+++ src/main/java/faang/school/godbless/_регистррация_пользователей/Main.java
@@ -0,0 +1,10 @@
+package faang.school.godbless._регистррация_пользователей;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Main {
+    public static void main (String [] args ){
+        User user=new User("Jack",19,"Google","London");
+    }
+}
--- src/main/java/faang/school/godbless/_регистррация_пользователей/User.java
+++ src/main/java/faang/school/godbless/_регистррация_пользователей/User.java
@@ -1,45 +1,23 @@
 package faang.school.godbless._регистррация_пользователей;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.HashSet;
 import java.util.Set;
 
 public class User {
     public String name;
-    public static int age;
+    public  int age;
     public String  work_place;
     public String address;
     final static Set<String> VALID_JOBS =Set.of("Google","Uber","Amazon");
     final static Set<String> VALID_ADDRESSES= Set.of("London","New York","Amsterdam");
     final static int age1 =18;
 
-    public User(String name,int age,String work_place,String address){
-        this.name=name;
-        this.age=age;
-        this.work_place=work_place;
-        this.address=address;
-        if(name.isEmpty()){
-
-        }else if( )
-
-
-    }
-
-    public static HashMap<Integer, ArrayList> groupUsers(ArrayList<User> users){
-        ArrayList<User> us = users;
-        ArrayList<User> group=new ArrayList<>();
-        HashMap<Integer,ArrayList> mapUsers = new HashMap<>();
-        mapUsers.put(age,group);
-        for(User user :us){
-            if( mapUsers.containsKey(user.age)){
-
-            }else{
-                ArrayList<User> gr = new ArrayList<>();
-                gr.add(user);
-                mapUsers.put(user.age,group );
-            }
+    public User(String name, int age, String work_place, String address) {
+        if(!name.isEmpty()&&age>=age1&&VALID_JOBS.contains(work_place)&&VALID_ADDRESSES.contains(address)){
+            this.name=name;
+            this.age=age;
+            this.work_place=work_place;
+            this.address=address;
+        }else {
+            throw new IllegalArgumentException();
         }
-        return mapUsers;
-
     }
 }
