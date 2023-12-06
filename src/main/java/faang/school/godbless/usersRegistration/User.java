804b050575293399e7bf74bff79607768d588530
Ilia Chuvatkin <ilyachuvatkin@gmail.com>
2023-12-06 13:31:48 +0300

    Added static value, message in exceptions.

--- src/main/java/faang/school/godbless/usersRegistration/User.java
+++ src/main/java/faang/school/godbless/usersRegistration/User.java
@@ -3,6 +3,7 @@ package faang.school.godbless.usersRegistration;
 import java.util.Set;
 
 public class User {
+    public static final int ADULT_AGE = 18;
     public static final Set<String> VALID_JOBS = Set.of("Google","Uber","Amazon");
     public static final Set<String> VALID_ADDRESSES = Set.of("London","New York","Amsterdam");
     private String name;
@@ -11,10 +12,18 @@ public class User {
     private String address;
 
     public User(String name, int age, String work, String address) {
-        if (name.isEmpty()) throw new IllegalArgumentException();
-        if (age<18) throw new IllegalArgumentException();
-        if (!VALID_ADDRESSES.contains(address)) throw new IllegalArgumentException();
-        if (!VALID_JOBS.contains(work)) throw new IllegalArgumentException();
+        if (name.isEmpty()) {
+            throw new IllegalArgumentException("Field name should not be empty!");
+        }
+        if (age<ADULT_AGE){
+            throw new IllegalArgumentException("Age should be more or equal 18!");
+        }
+        if (!VALID_ADDRESSES.contains(address)) {
+            throw new IllegalArgumentException("Wrong field address!");
+        }
+        if (!VALID_JOBS.contains(work)) {
+            throw new IllegalArgumentException("Wrong field work!");
+        }
         this.name = name;
         this.age = age;
         this.work = work;
