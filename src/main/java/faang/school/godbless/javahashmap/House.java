24db976de00c8aa660dbc98ad1f5a6e8970a6ff4
Kamil Gasanov <gasanovkamil516@gmail.com>
2023-12-06 16:22:19 +0300

    finished "Game of Thrones"

---
+++ src/main/java/faang/school/godbless/javahashmap/House.java
@@ -0,0 +1,11 @@
+package faang.school.godbless.javahashmap;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class House {
+    private String name;
+    private String sigil;
+}
