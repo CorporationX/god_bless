dc9c852d9161f4811d800a607d5c29adb3cfe074
Gor Arutyunyan <Rid03@yandex.ru>
2023-12-06 14:11:15 +0300

    "Группировка пользователей по возрасту"

--- src/main/java/faang/school/godbless/Application.java
+++ src/main/java/faang/school/godbless/Application.java
@@ -1,6 +1,7 @@
 package faang.school.godbless;
 
 public class Application {
+
     public static void main(String... args) {
         System.out.println("Hello world");
     }
--- src/main/java/faang/school/godbless/User.java
+++ src/main/java/faang/school/godbless/User.java
@@ -1,5 +1,29 @@
 package faang.school.godbless;
 
-public class Users {
-    
+import lombok.*;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.TreeMap;
+
+@AllArgsConstructor
+@Data
+public class User {
+    private String name;
+    private int age;
+    private String job;
+    private String adress;
+
+    public static Map<Integer, List<User>> groupUsers(List<User> users) {
+        Map<Integer, List<User>> groupUsers = new TreeMap<>();
+        for (User user : users) {
+            if (!groupUsers.containsKey(user.getAge())) {
+                groupUsers.put(user.getAge(), new ArrayList<>());
+            }
+            groupUsers.get(user.getAge()).add(user);
+        }
+        return groupUsers;
+    }
 }
