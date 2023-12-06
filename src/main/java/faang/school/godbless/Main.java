a3c9b329ac0640722b92b2f10bb33b24250027d4
Rikaf <fakir207@mail.ru>
2023-12-06 16:35:25 +0500

    created the Example and Main class, Wrote the reversal method

---
+++ src/main/java/faang/school/godbless/Main.java
@@ -0,0 +1,14 @@
+package faang.school.godbless;
+
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) {
+        int [] nums = { 1,2,3,4,5};
+
+        System.out.println(Arrays.toString(nums));
+        Example.reverse(nums);
+        System.out.println(Arrays.toString(nums));
+
+    }
+}
