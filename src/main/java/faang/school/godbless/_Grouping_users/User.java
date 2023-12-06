c987d0e999b202b4a89db5a501b196c0882a4a23
Teo Shtanko <atraxlxxxl@gmail.com>
2023-12-06 12:31:38 +0300

    Попытка миллионная, но рабочая. Сам,конечно же,не додумался.

--- src/main/java/faang/school/godbless/_Grouping_users/User.java
+++ src/main/java/faang/school/godbless/_Grouping_users/User.java
@@ -1,27 +1,33 @@
 package faang.school.godbless._Grouping_users;
 
 import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
 import java.util.HashMap;
 
 public class User {
     public String name;
-    public static int age;
+    public  int age;
     public String  work_place;
     public String address;
 
-    public static HashMap<Integer,ArrayList> groupUsers(ArrayList<User> users){
-        ArrayList<User> us = users;
-        ArrayList<User> group=new ArrayList<>();
-        HashMap<Integer,ArrayList> mapUsers = new HashMap<>();
-        mapUsers.put(age,group);
-        for(User user :us){
-            if( mapUsers.containsKey(user.age)){
+    @Override
+    public String toString() {
+        return "User{" +
+                "name='" + name + '\'' +
+                ", work_place='" + work_place + '\'' +
+                ", address='" + address + '\'' +
+                '}';
+    }
 
-            }else{
-                ArrayList<User> gr = new ArrayList<>();
-                gr.add(user);
-                mapUsers.put(user.age,group );
+    public static Map<Integer,List<User>> groupUsers(List<User> users){
+
+        Map<Integer,List<User>> mapUsers = new HashMap<>();
+        for(User user :users){
+            if( !mapUsers.containsKey(user.age)) {
+                mapUsers.put(user.age, new ArrayList<>());
             }
+            mapUsers.get(user.age).add(user);
         }
         return mapUsers;
 
