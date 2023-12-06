96377d160159d776c3885ce481e84212933c1988
Teo Shtanko <atraxlxxxl@gmail.com>
2023-12-06 23:29:21 +0300

    Ну вроде все норм.

---
+++ src/main/java/faang/school/godbless/abstraction/Main.java
@@ -0,0 +1,12 @@
+package faang.school.godbless.abstraction;
+
+public class Main {
+    public static void main(String [] args){
+        Warrior warrior = new Warrior("воин");
+        Archer archer = new Archer("лучник");
+        warrior.attack(archer);
+        archer.attack(warrior);
+        System.out.println(archer.health);
+        System.out.println(warrior.health);
+    }
+}
