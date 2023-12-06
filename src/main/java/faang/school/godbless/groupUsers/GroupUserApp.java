e2b7f7b0502b83b650c77d9cf29fd2d0fa5143b4
v.dyakov <v.dyakov@sbereducation.ru>
2023-12-06 11:31:22 +0300

    feature: use list.of (BJS2-439)

--- src/main/java/faang/school/godbless/groupUsers/GroupUserApp.java
+++ src/main/java/faang/school/godbless/groupUsers/GroupUserApp.java
@@ -7,15 +7,13 @@ import static faang.school.godbless.groupUsers.User.groupUsers;
 
 public class GroupUserApp {
     public static void main(String... args) {
-        List<User> users = new ArrayList<>();
-
-        users.add(new User("Alice", 25, "Developer", "123 Street A"));
-        users.add(new User("Bob", 30, "Designer", "456 Street B"));
-        users.add(new User("Charlie", 25, "Artist", "789 Street C"));
-        users.add(new User("David", 28, "Engineer", "321 Street D"));
-        users.add(new User("Eve", 32, "Manager", "654 Street E"));
-        users.add(new User("Frank", 40, "Teacher", "987 Street F"));
-
+        List<User> users = List.of(new User("Alice", 25, "Developer", "123 Street A"),
+                new User("Bob", 30, "Designer", "456 Street B"),
+                new User("Charlie", 25, "Artist", "789 Street C"),
+                new User("David", 28, "Engineer", "321 Street D"),
+                new User("Eve", 32, "Manager", "654 Street E"),
+                new User("Frank", 40, "Teacher", "987 Street F")
+                );
 
         System.out.println(groupUsers(users).get(25));
     }
