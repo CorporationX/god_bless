3434f152fcd9bb8b74c2612f275dd4bce4a79c61
v.dyakov <v.dyakov@sbereducation.ru>
2023-12-06 11:59:52 +0300

    fix: add local toString for class User (BJS2-439)

--- src/main/java/faang/school/godbless/groupUsers/User.java
+++ src/main/java/faang/school/godbless/groupUsers/User.java
@@ -7,11 +7,11 @@ import java.util.Map;
 
 public class User {
     private String name;
-    private Integer age;
+    private int age;
     private String job;
     private String address;
 
-    public User(String name, Integer age, String job, String address) {
+    public User(String name, int age, String job, String address) {
         this.name = name;
         this.age = age;
         this.job = job;
@@ -23,19 +23,27 @@ public class User {
     }
 
     public static Map<Integer, List<User>> groupUsers(List<User> users) {
-        Map<Integer, List<User>> newUsersList = new HashMap();
+        Map<Integer, List<User>> groupUsers = new HashMap();
         for (User user: users) {
             int key = user.getAge();
-            List<User> value = newUsersList.get(key);
+            List<User> value = groupUsers.get(key);
 
             if (value == null) {
                 value = new ArrayList<>();
             }
 
             value.add(user);
-            newUsersList.put(user.getAge(), value);
+            groupUsers.put(user.getAge(), value);
         }
 
-        return newUsersList;
+        return groupUsers;
+    }
+
+    @Override
+    public String toString() {
+        return "name='" + name + '\'' +
+                ", age=" + age +
+                ", workplace='" + job + '\'' +
+                ", adress='" + address;
     }
 }
