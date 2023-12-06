07906e432f039ee525cd95fac6dd5aae98ccdf99
Marina_Zu <bugsi-47@yandex.ru>
2023-12-06 13:29:25 +0300

    Add task "Коллекционируем пользователей"

---
+++ src/main/java/faang/school/godbless/Main.java
@@ -0,0 +1,21 @@
+package faang.school.godbless;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static faang.school.godbless.User.findHobbyLovers;
+
+public class Main {
+    public static void main(String[] args) {
+        List<User> listUser = new ArrayList<>();
+        listUser.add(new User(1, "Василий", 27, new String[]{"шахматы", "велосипед", "кулинария"}));
+        listUser.add(new User(2, "Олег", 35, new String[]{"сноуборд", "велосипед", "баскетбол"}));
+        listUser.add( new User(3, "Iрина", 35, new String[]{"рукоделие", "волейбол", "кулинария"}));
+        listUser.add(new User(4, "Егор", 18, new String[]{"туризм", "баскетбол"}));
+        listUser.add(new User(5, "Мария", 22, new String[]{"шахматы", "йога", "рисование"}));
+
+        String[] activities = {"шахматы","плавание", "баскетбол"};
+
+        System.out.println(findHobbyLovers(listUser,activities));
+    }
+}
---
+++ src/main/java/faang/school/godbless/User.java
@@ -0,0 +1,30 @@
+package faang.school.godbless;
+
+import lombok.AllArgsConstructor;
+import lombok.ToString;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@AllArgsConstructor
+@ToString
+public class User {
+    protected int id;
+    String name;
+    protected int age;
+    protected String[] userActivities;
+
+    public static Map<User, String> findHobbyLovers(List<User> users, String[] activities) {
+        Map<User, String> collectionUserByHobby = new HashMap<>();
+        for (User user : users) {
+            for (String active : user.userActivities) {
+                if (Arrays.asList(activities).contains(active)) {
+                    collectionUserByHobby.put(user, active);
+                }
+            }
+        }
+        return collectionUserByHobby;
+    }
+}
