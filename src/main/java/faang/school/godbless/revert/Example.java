b5bf06ce0d662e5fd0ed5c991d8a3068fd6adfe5
v.dyakov <v.dyakov@sbereducation.ru>
2023-12-07 11:57:23 +0300

    feture: add class with reverse method (BJS2-528)

---
+++ src/main/java/faang/school/godbless/revert/Example.java
@@ -0,0 +1,13 @@
+package faang.school.godbless.revert;
+
+public class Example {
+    public static void reverse(int[] value) {
+        int length = value.length;
+        for (int i = 0; i < length / 2; i++) {
+            int temp = value[i];
+
+            value[i] = value[length - 1 - i];
+            value[length - 1 - i] = temp;
+        }
+    }
+}
