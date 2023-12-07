790c02d2f2995e979e4085978dc8506470b61f15
Kamil Gasanov <gasanovkamil516@gmail.com>
2023-12-07 21:25:51 +0300

    finished "search google"

---
+++ src/main/java/faang/school/godbless/javahashmap/task4/WebPage.java
@@ -0,0 +1,11 @@
+package faang.school.godbless.javahashmap.task4;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+public class WebPage {
+    private String url;
+    private String title;
+    private String content;
+}
