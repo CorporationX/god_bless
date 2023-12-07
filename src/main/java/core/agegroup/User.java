67624c3857ae38e10b08467ff046e3ab7fb0c81c
javaDevArtem <artemjdev@gmail.com>
2023-12-06 22:15:29 -0500

    BJS2-522 Регистрация пользователей Реализация конструктора

--- src/main/java/core/agegroup/User.java
+++ src/main/java/core/agegroup/User.java
@@ -1,6 +1,5 @@
 package core.agegroup;
 
-import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.ToString;
 
@@ -9,13 +8,24 @@ import java.util.stream.Collectors;
 
 @Getter
 @ToString
-@AllArgsConstructor
 public class User {
 
+    private static final Set<String> VALID_JOBS = new HashSet<>(Arrays.asList("Google", "Uber", "Amazon"));
+    private static final Set<String> VALID_ADDRESSES = new HashSet<>(Arrays.asList("London", "New York", "Amsterdam"));
     private String name;
     private int age;
     private String work;
     private String address;
+
+    public User(String name, int age, String work, String address) {
+        if (name.isEmpty() || age < 18 || !VALID_JOBS.contains(work) || !VALID_ADDRESSES.contains(address)) {
+            throw new IllegalArgumentException("Incorrect data");
+        }
+        this.name = name;
+        this.age = age;
+        this.work = work;
+        this.address = address;
+    }
 
     public static Map<Integer, List<User>> groupUsers(List<User> users) {
         return users.stream()
