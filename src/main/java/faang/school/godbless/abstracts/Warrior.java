3944c2ddfd79f0a41cf69c16c6b88ca84dee9ecc
v.dyakov <v.dyakov@sbereducation.ru>
2023-12-07 10:05:10 +0300

    feature: new adstract class Character (BJS2-525)

---
+++ src/main/java/faang/school/godbless/abstracts/Warrior.java
@@ -0,0 +1,12 @@
+package faang.school.godbless.abstracts;
+
+public class Warrior extends Character {
+    public Warrior(String name) {
+        super(name, 10, 5, 3);
+    }
+
+    @Override
+    public void attack(Character character) {
+        character.health -= this.power;
+    }
+}
