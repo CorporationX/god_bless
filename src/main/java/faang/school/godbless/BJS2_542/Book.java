83f594eb25bf8ed7b05b250640a5d998b4cf072c
Edisolt Khatsiev <Edisolt Khatsiev>
2023-12-08 00:18:25 +0300

    Java HashMap: Библиотека Вестеросса

---
+++ src/main/java/faang/school/godbless/BJS2_542/Book.java
@@ -0,0 +1,26 @@
+package faang.school.godbless.BJS2_542;
+
+import lombok.EqualsAndHashCode;
+
+import java.util.Objects;
+
+//@EqualsAndHashCode
+public class Book {
+    private String title;
+    private String author;
+    private int year;
+
+
+    //there are generated equals and hashcode methods
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Book book = (Book) o;
+        return year == book.year && Objects.equals(title, book.title) && Objects.equals(author, book.author);
+    }
+    @Override
+    public int hashCode() {
+        return Objects.hash(title, author, year);
+    }
+}
