b21641439a237a6e32443e6e92e5243f3e80bd02
Ilia Chuvatkin <ilyachuvatkin@gmail.com>
2023-12-07 19:02:40 +0300

    Changed algorithm in reverse method

--- src/main/java/faang/school/godbless/turnAround/Example.java
+++ src/main/java/faang/school/godbless/turnAround/Example.java
@@ -1,14 +1,15 @@
 package faang.school.godbless.turnAround;
 
 public class Example {
-    public static int[] reverse(int[] incomingNums) {
-        int numsLength = incomingNums.length;
-        int[] outcomingNums = new int[numsLength];
-        int count = numsLength - 1;
-        for (int i = 0; i < numsLength; i++) {
-            outcomingNums[count] = incomingNums[i];
-            count--;
+    public static int[] reverse(int[] nums) {
+        int index = nums.length - 1;
+        int buble;
+        for (int i = 0; i < nums.length / 2; i++) {
+            buble = nums[i];
+            nums[i] = nums[index];
+            nums[index] = buble;
+            index--;
         }
-        return outcomingNums;
+        return nums;
     }
 }
--- src/main/java/faang/school/godbless/turnAround/Main.java
+++ src/main/java/faang/school/godbless/turnAround/Main.java
@@ -2,5 +2,9 @@ package faang.school.godbless.turnAround;
 
 public class Main {
     public static void main(String[] args) {
+        int[] numbers = Example.reverse(new int[]{1, 2, 3, 4, 5, 6, 7});
+        for (int i = 0; i < numbers.length; i++) {
+            System.out.print(" " + numbers[i]);
+        }
     }
 }
