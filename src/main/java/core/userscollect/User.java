d77b27656be408577ae6ed5f4162df40c4cbebc0
javaDevArtem <artemjdev@gmail.com>
2023-12-06 22:46:02 -0500

    BJS2-521 Collect users

---
+++ src/main/java/core/userscollect/User.java
@@ -0,0 +1,20 @@
+package core.userscollect;
+
+import java.util.Collections;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
+public class User {
+
+    private Integer id;
+    private String name;
+    private int age;
+    private Set<String> activities;
+
+    private Map<User, String> findHobbyLovers(List<User> userList, Set<String> activities) {
+
+        return Collections.emptyMap();
+    }
+
+}
