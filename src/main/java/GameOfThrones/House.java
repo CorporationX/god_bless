56f7180a7f26fb24aee09e52ccd1e465a9b22b98
FD_Shell <dariuskpp@gmail.com>
2023-12-08 11:30:23 +0300

    Add House and Main classes

---
+++ src/main/java/GameOfThrones/House.java
@@ -0,0 +1,19 @@
+package GameOfThrones;
+
+public class House {
+    private String name;
+    private String sigil;
+
+    public House(String name, String sigil) {
+        this.name = name;
+        this.sigil = sigil;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getSigil() {
+        return sigil;
+    }
+}
