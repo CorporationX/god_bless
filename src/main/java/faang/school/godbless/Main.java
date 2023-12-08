2b06d8bee77c77dbcbae2fd10c588345584170dd
Rauan Tureyev <rauanture@gmail.com>
2023-12-08 23:25:01 +0600

    create methods(add, remove, find, sort)

--- src/main/java/faang/school/godbless/Main.java
+++ src/main/java/faang/school/godbless/Main.java
@@ -1,15 +1,59 @@
 package faang.school.godbless;
 
+import java.lang.reflect.Array;
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Iterator;
 import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
-        List<User> listOfUsers = new ArrayList<>();
-        listOfUsers.add(new User(20, "Jackson", "Google", "New York"));
-        listOfUsers.add(new User(35, "Don", "Uber", "Amsterdam"));
-        listOfUsers.add(new User(18, "Rauan", "Amazon", "London"));
-        listOfUsers.add(new User(18, "Fedor", "Amazon", "Amsterdam"));
-        System.out.println(User.groupUsers(listOfUsers));
+        ArrayList<Student> listOfStudents = new ArrayList<>();
+        listOfStudents.add(new Student("Rauan", "MCM", 1));
+        listOfStudents.add(new Student("Vova", "CI", 4));
+        listOfStudents.add(new Student("Nariman", "SIS", 2));
+        addStudent(listOfStudents, new Student("Alinur", "MCM", 1));
+        deleteStudent(listOfStudents, "Rauan", "MCM", 1);
+        System.out.println(sortStudent(listOfStudents));
+        System.out.println(findStudent(listOfStudents,"CI", 4));
+
     }
+    public static HashMap<String, List<Student>> sortStudent(List<Student> listOfStudents){
+        HashMap<String, List<Student>> result = new HashMap<>();
+        String[] key = new String[2];
+        for (Student s : listOfStudents) {
+            key[0] = s.getFaculty();
+            key[1] = Integer.toString(s.getCourse());
+            String keyy = key[0]+key[1];
+            List<Student> value = result.getOrDefault(keyy, new ArrayList<>());
+            value.add(s);
+            result.put(keyy, value);
+        }
+        return result;
+    }
+    public static ArrayList<Student> addStudent(ArrayList<Student> list, Student student){
+        list.add(student);
+        return list;
+    }
+    public static ArrayList<Student> deleteStudent(ArrayList<Student> list, String name, String faculty, int course){
+        Iterator<Student> iterator = list.iterator();
+        while (iterator.hasNext()){
+            Student s = iterator.next();
+            if(s.getName().equals(name)&&s.getFaculty().equals(faculty)&&s.getCourse()==course){
+                iterator.remove();
+            }
+        }
+        return list;
+    }
+    public static String findStudent(ArrayList<Student> list, String faculty, int course){
+        for (Student s : list) {
+            if(s.getFaculty().equals(faculty)&&s.getCourse()==course){
+                return (s.getName());
+            }else{
+                return "There are no student with this faculty and course";
+            }
+        }
+       return null;
+    }
+
 }
