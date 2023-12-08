ad1c1f6cb64e4d4f70c5cb841d63521c60fa0367
Nikita <nikita.kopot@yandex.ru>
2023-12-08 14:53:35 +0300

    add validation for user input fields

--- src/main/java/faang/school/godbless/users/Main.java
+++ src/main/java/faang/school/godbless/users/Main.java
@@ -1,12 +1,13 @@
-package users;
+package faang.school.godbless.users;
 
 import java.util.ArrayList;
 import java.util.List;
 
 public class Main {
-    public static void main (String[] args) {
+    public static void main(String[] args) {
         List<User> users = new ArrayList<>();
 
+        users.add(new User("", 27, "Uber", "London"));
         User.groupUsers(users);
     }
 }
