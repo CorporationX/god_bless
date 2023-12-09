4d56ad3bd72ac73a5e604f01eb572d9e4cf38f40
Kamil Gasanov <gasanovkamil516@gmail.com>
2023-12-09 17:36:36 +0300

    add test

---
+++ src/main/java/faang/school/godbless/Main.java
@@ -0,0 +1,35 @@
+package faang.school.godbless;
+
+
+public class Main {
+    public static void main(String[] args) {
+
+        User validUser = new User("John Doe", 25, "Google", "London");
+        System.out.println("Пользователь создан успешно.");
+
+        try {
+            User invalidNameUser = new User("", 30, "Uber", "New York");
+        } catch (IllegalArgumentException e) {
+            System.out.println("Ошибка при создании пользователя: " + e.getMessage());
+        }
+
+        try {
+            User invalidAgeUser = new User("Alice", 15, "Amazon", "Amsterdam");
+        } catch (IllegalArgumentException e) {
+            System.out.println("Ошибка при создании пользователя: " + e.getMessage());
+        }
+
+        try {
+            User invalidJobUser = new User("Bob", 28, "Microsoft", "Paris");
+        } catch (IllegalArgumentException e) {
+            System.out.println("Ошибка при создании пользователя: " + e.getMessage());
+        }
+
+        try {
+            User invalidAddressUser = new User("Eve", 22, "Uber", "Tokyo");
+        } catch (IllegalArgumentException e) {
+            System.out.println("Ошибка при создании пользователя: " + e.getMessage());
+        }
+    }
+}
+
