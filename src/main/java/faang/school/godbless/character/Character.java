2b11272afe601a82be57faf4b87ec3c1dfc055fa
Nikita <nikita.kopot@yandex.ru>
2023-12-09 14:59:55 +0300

    update getters, setters and access modifiers

--- src/main/java/faang/school/godbless/character/Character.java
+++ src/main/java/faang/school/godbless/character/Character.java
@@ -1,20 +1,16 @@
 package faang.school.godbless.character;
 
+import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.Setter;
 
-@Getter
-@Setter
+@Getter(AccessLevel.PROTECTED)
 public abstract class Character {
     private String name;
     private int strength;
     private int agility;
     private int intelligence;
-    private int health = 100;
-
-    public Character (String name) {
-        this.name = name;
-    }
+    @Setter(AccessLevel.PROTECTED) private int health = 100;
 
     public Character (
             String name,
--- src/main/java/faang/school/godbless/character/Main.java
+++ src/main/java/faang/school/godbless/character/Main.java
@@ -2,8 +2,8 @@ package faang.school.godbless.character;
 
 public class Main {
     public static void main(String[] args) {
-        Warrior warrior = new Warrior("Dean");
-        Archer archer = new Archer("Sam");
+        Character warrior = new Warrior("Dean");
+        Character archer = new Archer("Sam");
 
         warrior.attack(archer);
         System.out.println(archer.getHealth());
