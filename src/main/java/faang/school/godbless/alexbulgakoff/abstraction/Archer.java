1a1b9a0b5ba94a8e563c5138637a9fa3a7238c9f
Alexander Bulgakov <asbulgakov90@gmail.com>
2023-12-09 23:16:22 +0300

    Complete task BJS2-581

---
+++ src/main/java/faang/school/godbless/alexbulgakoff/abstraction/Archer.java
@@ -0,0 +1,17 @@
+package faang.school.godbless.alexbulgakoff.abstraction;
+
+/**
+ * @author Alexander Bulgakov
+ */
+public class Archer extends Character {
+
+    public Archer(String name) {
+        super(name, 3, 10, 5);
+    }
+
+    @Override
+    void attack(Character character) {
+        character.setHealth(character.getHealth() - this.getDexterity());
+    }
+
+}
---
+++ src/main/java/faang/school/godbless/alexbulgakoff/abstraction/Main.java
@@ -0,0 +1,36 @@
+package faang.school.godbless.alexbulgakoff.abstraction;
+
+/**
+ * @author Alexander Bulgakov
+ */
+
+public class Main {
+    public static void main(String[] args) {
+        Archer archer = new Archer("Archer");
+        Warrior warrior = new Warrior("Warrior");
+
+        System.out.println("Имя: " + archer.getName() +
+                "\nСила: " + archer.getPower() +
+                "\nЛовкость: " + archer.getDexterity() +
+                "\nИнтеллект: " + archer.getIntelligence() +
+                "\nЗдоровье: " + archer.getHealth());
+
+        System.out.println();
+
+        System.out.println("Имя: " + warrior.getName() +
+                "\nСила: " + warrior.getPower() +
+                "\nЛовкость: " + warrior.getDexterity() +
+                "\nИнтеллект: " + warrior.getIntelligence() +
+                "\nЗдоровье: " + warrior.getHealth());
+
+        System.out.println();
+
+        archer.attack(warrior);
+        System.out.println("Archer attacks Warrior");
+        System.out.println("Warrior health after attack: " + warrior.getHealth());
+        System.out.println();
+        warrior.attack(archer);
+        System.out.println("Warrior attacks Archer");
+        System.out.println("Archer health after attack: " + archer.getHealth());
+    }
+}
