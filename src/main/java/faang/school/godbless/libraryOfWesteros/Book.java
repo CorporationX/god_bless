f04835f06999576a177725e8a8410649eb6c413b
Ilia Chuvatkin <ilyachuvatkin@gmail.com>
2023-12-09 17:06:23 +0300

    Java HashMap: Библиотека Вестеросса

---
+++ src/main/java/faang/school/godbless/libraryOfWesteros/Book.java
@@ -0,0 +1,35 @@
+package faang.school.godbless.libraryOfWesteros;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+import java.util.Objects;
+
+@Getter
+@AllArgsConstructor
+public class Book {
+    String title;
+    String author;
+    int year;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Book book)) return false;
+        return year == book.year && Objects.equals(title, book.title) && Objects.equals(author, book.author);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(title, author, year);
+    }
+
+    @Override
+    public String toString() {
+        return "Book{" +
+                "title='" + title + '\'' +
+                ", author='" + author + '\'' +
+                ", year=" + year +
+                '}';
+    }
+}
