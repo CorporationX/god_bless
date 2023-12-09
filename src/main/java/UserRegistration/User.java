a9abea0e239f5e0e48d5e481604b7b967396f97b
FD_Shell <dariuskpp@gmail.com>
2023-12-09 23:56:13 +0300

    Added break in cicle and constructor location;

--- src/main/java/UserRegistration/User.java
+++ src/main/java/UserRegistration/User.java
@@ -11,26 +11,6 @@ public class User {
     private int age;
     private Set<String> activities;
 
-
-    public static Map<User, String> findHobbyLovers (List<User> users, Set<String> activities){
-        //Создать мапу для финального ответа
-        Map<User, String> foundedUsers = new HashMap<>();
-
-        //ЦИКЛ Пройтись по каждому пользователю
-        for (User user : users) {
-        //  ЦИКЛ Пройтись по каждому элементу сета activites у текущего пользователя
-            for (String activity : user.activities) {
-                //      ЕСЛИ текущая активность есть в искомом списке
-                if (activities.contains(activity)) {
-                    //          Сохраняем пользователя в мапу, прерываем цикл
-                    foundedUsers.put(user, activity);
-                }
-            }
-        }
-        //Возвращаем мапу
-        return foundedUsers;
-    }
-
     public User(int id, String name, int age, Set<String> activities) {
         this.id = id;
         this.name = name;
@@ -38,35 +18,24 @@ public class User {
         this.activities = activities;
     }
 
-    public int getId() {
-        return id;
+    public static Map<User, String> findHobbyLovers(List<User> users, Set<String> activities) {
+        //Создать мапу для финального ответа
+        Map<User, String> foundedUsers = new HashMap<>();
+
+        //ЦИКЛ Пройтись по каждому пользователю
+        for (User user : users) {
+            //  ЦИКЛ Пройтись по каждому элементу сета activites у текущего пользователя
+            for (String activity : user.activities) {
+                //      ЕСЛИ текущая активность есть в искомом списке
+                if (activities.contains(activity)) {
+                    //          Сохраняем пользователя в мапу, прерываем цикл
+                    foundedUsers.put(user, activity);
+                    break;
+                }
+            }
+        }
+        //Возвращаем мапу
+        return foundedUsers;
     }
 
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public int getAge() {
-        return age;
-    }
-
-    public void setAge(int age) {
-        this.age = age;
-    }
-
-    public Set<String> getActivities() {
-        return activities;
-    }
-
-    public void setActivities(Set<String> activities) {
-        this.activities = activities;
-    }
 }
