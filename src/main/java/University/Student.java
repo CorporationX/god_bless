803d6150e13dc936280f6ec8198f18058f5675cd
FD_Shell <dariuskpp@gmail.com>
2023-12-09 18:38:39 +0300

    Create Main, Student and UniversityCourse classes

---
+++ src/main/java/University/Student.java
@@ -0,0 +1,25 @@
+package University;
+
+import lombok.Getter;
+
+@Getter
+public class Student {
+    private final String name;
+    private final String faculty;
+    private final int year;
+
+    public Student(String name, String faculty, int year) {
+        this.name = name;
+        this.faculty = faculty;
+        this.year = year;
+    }
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                "name='" + name + '\'' +
+                ", faculty='" + faculty + '\'' +
+                ", year=" + year +
+                '}';
+    }
+}
