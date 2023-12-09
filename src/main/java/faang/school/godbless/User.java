bd050f927d2c086dc7ef24a55f48eb9c48ba55b6
lonovojvladimir <lonovojvladimir@gmail.com>
2023-12-10 01:17:35 +0300

    add Student

--- src/main/java/faang/school/godbless/User.java
+++ src/main/java/faang/school/godbless/User.java
@@ -11,15 +11,15 @@ import java.util.Map;
 @Setter
 public class User {
     private Long id;
-    private String Name;
-    private Long Age;
-    private List<String> ActivityList;
+    private String name;
+    private Long age;
+    private List<String> activityList;
 
     public Map<User, String> findHobbyLovers(List<User> userList, List<String> activityList){
         Map<User, String> map = null;
         for(User u : userList){
-            if(u.ActivityList!=null) {
-                for (String a : u.ActivityList) {
+            if(u.activityList!=null) {
+                for (String a : u.activityList) {
                     if (activityList.contains(a)) {
                         map.put(u, a);
                     }
