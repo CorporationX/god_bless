416141a74b8ae45fcd374caece9fea84735e5320
Teo Shtanko <atraxlxxxl@gmail.com>
2023-12-09 18:33:27 +0300

    Проработал комментарии

--- src/main/java/faang/school/godbless/_Grouping_users/Main.java
+++ src/main/java/faang/school/godbless/_Grouping_users/Main.java
@@ -4,18 +4,11 @@ package faang.school.godbless._Grouping_users;
 import java.util.List;
 
 public class Main {
-    public static void main(String [] args){
-        User user1 = new User();
-        user1.age=35;
-        user1.name="Jack";
-        User user = new User();
-        user.age=18;
-        user.name="Jason";
+    public static void main(String[] args) {
+        User user1 = new User("Jack", 35);
+        User user = new User("Jason", 18);
 
-        List<User> users=List.of(user,user1);
-
+        List<User> users = List.of(user, user1);
         System.out.println(User.groupUsers(users));
-
-
     }
 }
--- src/main/java/faang/school/godbless/_Grouping_users/User.java
+++ src/main/java/faang/school/godbless/_Grouping_users/User.java
@@ -1,35 +1,41 @@
 package faang.school.godbless._Grouping_users;
 
+
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.HashMap;
 
 public class User {
-    public String name;
-    public  int age;
-    public String  work_place;
-    public String address;
+    private String name;
+    private int age;
+    private String workPlace;
+    private String address;
 
-    @Override
-    public String toString() {
-        return "User{" +
-                "name='" + name + '\'' +
-                ", work_place='" + work_place + '\'' +
-                ", address='" + address + '\'' +
-                '}';
+    public User(String name, int age) {
+        this.name = name;
+        this.age = age;
     }
 
-    public static Map<Integer,List<User>> groupUsers(List<User> users){
+    public static Map<Integer, List<User>> groupUsers(List<User> users) {
 
-        Map<Integer,List<User>> mapUsers = new HashMap<>();
-        for(User user :users){
-            if( !mapUsers.containsKey(user.age)) {
+        Map<Integer, List<User>> mapUsers = new HashMap<>();
+        for (User user : users) {
+            if (!mapUsers.containsKey(user.age)) {
                 mapUsers.put(user.age, new ArrayList<>());
             }
             mapUsers.get(user.age).add(user);
         }
         return mapUsers;
 
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "name='" + name + '\'' +
+                ", work_place='" + workPlace + '\'' +
+                ", address='" + address + '\'' +
+                '}';
     }
 }
