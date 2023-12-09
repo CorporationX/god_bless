f76de369f89c45f8a9e8b378022b3af7da1fef3c
Vlad Meniailov <vlad.meniailov@yandex.ru>
2023-12-09 09:54:57 +0300

    Application fixed

--- src/main/java/faang/school/godbless/Application.java
+++ src/main/java/faang/school/godbless/Application.java
@@ -3,6 +3,5 @@ package faang.school.godbless;
 public class Application {
     public static void main(String... args) {
         System.out.println("Hello world");
-        System.out.println();
     }
 }
