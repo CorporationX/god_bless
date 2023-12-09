e19b9228918229d4349fcba71b164832dba058c0
Rauan Tureyev <rauanture@gmail.com>
2023-12-09 20:08:02 +0600

    create methods(set to map, output)

--- src/main/java/faang/school/godbless/Main.java
+++ src/main/java/faang/school/godbless/Main.java
@@ -1,15 +1,36 @@
 package faang.school.godbless;
 
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Map;
+import java.util.Set;
 
 public class Main {
     public static void main(String[] args) {
-        List<User> listOfUsers = new ArrayList<>();
-        listOfUsers.add(new User(20, "Jackson", "Google", "New York"));
-        listOfUsers.add(new User(35, "Don", "Uber", "Amsterdam"));
-        listOfUsers.add(new User(18, "Rauan", "Amazon", "London"));
-        listOfUsers.add(new User(18, "Fedor", "Amazon", "Amsterdam"));
-        System.out.println(User.groupUsers(listOfUsers));
+        Set<Product> setOfProduct = new HashSet<>();
+        setOfProduct.add(new Product(1, "Banana", "Food"));
+        setOfProduct.add(new Product(2, "Apple", "Food"));
+        setOfProduct.add(new Product(43, "Energetic", "Liquid"));
+        output(sortProduct(setOfProduct));
+
+    }
+    public static HashMap<String, List<Product>> sortProduct(Set<Product> setOfProduct){
+        HashMap<String, List<Product>> result = new HashMap<>();
+        for (Product p : setOfProduct) {
+            if(!result.containsKey(p.getCategory())){
+                result.put(p.getCategory(), new ArrayList<>());
+                result.get(p.getCategory()).add(p);
+            }else{
+                result.get(p.getCategory()).add(p);
+            }
+        }
+        return result;
+    }
+    public static void output(HashMap<String, List<Product>> map){
+        for (Map.Entry<String, List<Product>> entry : map.entrySet()) {
+            System.out.println(entry);
+        }
     }
 }
