c9ead9aca63557f76f533a174d93221e61476f9d
Teo Shtanko <atraxlxxxl@gmail.com>
2023-12-10 14:47:47 +0300

    Не работает метод удаления

--- src/main/java/faang/school/godbless/counting_absenteeism/Main.java
+++ src/main/java/faang/school/godbless/counting_absenteeism/Main.java
@@ -1,18 +1,28 @@
 package faang.school.godbless.counting_absenteeism;
 
+import lombok.Data;
+
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-
+@Data
 public class Main {
-    static List<Student> students = new ArrayList<>();
 
     public static void main(String[] args) {
+        List<Student> students = List.of(
+                new Student("Вася", "Факультет1", 2),
+                new Student("Петя", "Факультет2", 1),
+                new Student("Сеня", "Факультет1", 2),
+                new Student("Коля", "Факультет2", 1));
 
-        System.out.println(sortStudents(students));
+
+        //Student.removeStudent(students,"Коля","Факультет2",1);
+        Student.infoGroupedStudents(Main.sortStudents(students));
+        System.out.println(Student.searchAllStudentsGroup(students, "Факультет2", 1));
     }
+
     public static Map<Course, List<Student>> sortStudents(List<Student> students) {
         Map<Course, List<Student>> classmates = new HashMap<>();
         for (Student student : students) {
--- src/main/java/faang/school/godbless/counting_absenteeism/Student.java
+++ src/main/java/faang/school/godbless/counting_absenteeism/Student.java
@@ -2,16 +2,49 @@ package faang.school.godbless.counting_absenteeism;
 
 import lombok.Data;
 
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Map;
+
 @Data
 public class Student {
-    private  String name;
-    private  String faculty;
-    private  int year;
+    private String name;
+    private String faculty;
+    private int year;
 
     public Student(String name, String faculty, int year) {
         this.name = name;
         this.faculty = faculty;
         this.year = year;
+    }
+
+    public static void removeStudent(List<Student> students, String name, String faculty, int year) {
+        Iterator<Student> student = students.iterator();
+        while (student.hasNext()) {
+            if (student.next().getYear() == year && student.next().getName().equals(name) && student.next().getFaculty().equals(faculty)) {
+                students.remove(student.next());
+                break;
+            }
+        }
+
+    }
+
+    public static List<Student> searchAllStudentsGroup(List<Student> students, String faculty, int year) {
+        List<Student> allStudentsGroup = new ArrayList<>();
+        for (Student student : students) {
+            if (student.getFaculty().equals(faculty) && student.getYear() == year) {
+                allStudentsGroup.add(student);
+            }
+        }
+        return allStudentsGroup;
+    }
+
+    public static void infoGroupedStudents(Map<Course, List<Student>> groupedStudents) {
+        for (Map.Entry<Course, List<Student>> entry : groupedStudents.entrySet()) {
+            System.out.println(entry.getKey() + "-->" + entry.getValue());
+        }
+
     }
 
 }
