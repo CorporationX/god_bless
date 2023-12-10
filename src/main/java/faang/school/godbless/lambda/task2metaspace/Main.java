21112eb71d597b2276800fc0f5561e568201ebad
Kamil Gasanov <gasanovkamil516@gmail.com>
2023-12-10 23:14:09 +0300

    finished

--- src/main/java/faang/school/godbless/lambda/task2metaspace/Main.java
+++ src/main/java/faang/school/godbless/lambda/task2metaspace/Main.java
@@ -8,9 +8,14 @@ public class Main {
         manager.registerHandler("sms", (notification) -> System.out.println("Отправка SMS: " + notification.getMessage()));
         manager.registerHandler("push", (notification) -> System.out.println("Отправка push-уведомления: " + notification.getMessage()));
 
-        Notification emailNotification = new Notification("email", "Ваша учетная запись успешно активирована");
-        Notification smsNotification = new Notification("sms", "Вы успешно изменили свой пароль");
-        Notification pushNotification = new Notification("push", "Новый пост от пользователя: JohnDoe");
+        manager.registerFilter((content) -> content.replaceAll("успешно", "****"));
+        manager.registerFilter((content) -> content.replaceAll("умер", "жив здоров"));
+        manager.registerFilter((content) -> content.replaceAll("ипаный", "слишком замудренный"));
+        manager.registerFilter((content) -> content.replaceAll("шатал я его все", "меня переполняют эмоции"));
+
+        Notification emailNotification = new Notification("email", "Ваша учетная запись заблакирована, шатал я его все");
+        Notification smsNotification = new Notification("sms", "Вы успешно изменили свой ипаный пароль");
+        Notification pushNotification = new Notification("push", "Новый пост от пользователя: умер");
         Notification unknownNotification = new Notification("unknown", "бесполезное сообщение");
 
         manager.sendNotification(emailNotification);
