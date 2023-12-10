fce317a777ed96600a21a85f3b52fcb84f5df719
Leilorr <148541216+Leilorr@users.noreply.github.com>
2023-12-11 00:26:57 +0300

    Создал конструктор, который принимает в качестве параметров значения для всех полей класса. В качестве статических констант в классе объявил 2 сета: VALID_JOBS и VALID_ADDRESSES. В конструкторе провел валидацию параметров по следующим правилам: -имя не может быть пустым; -возраст не может быть меньше 18; -место работы должно содержаться во множестве VALID_JOBS; -адрес должен содержаться во множестве VALID_ADDRESSES. Если хотя бы одно из условий не выполняется, то выбросываетя исключение IllegalArgumentException. Иначе сохранить переданные значения в поля.

---
+++ src/main/java/faang/school/godbless/User.java
@@ -0,0 +1,45 @@
+package faang.school.godbless;
+
+import java.util.*;
+
+public class User {
+    private final static Set<String> VALID_JOBS = Set.of("Google", "Uber", "Amazon");
+    private final static Set<String> VALID_ADDRESSES = Set.of("London", "New York", "Amsterdam");
+
+    private String name;
+    private int age;
+    private String work;
+    private String address;
+    public User(String name, int age, String work, String address){
+        if (name.isBlank())
+            throw new IllegalArgumentException ("Enter your name");
+        if (age < 18)
+            throw new IllegalArgumentException("You are still young");
+        if (!VALID_JOBS.contains(work))
+            throw new IllegalArgumentException("Invalid workplace");
+        if (!VALID_ADDRESSES.contains(address))
+            throw new IllegalArgumentException("Invalid address");
+
+
+        this.age = age;
+        this.name = name;
+        this.work = work;
+        this.address = address;
+    }
+
+    static public Map<Integer, List<User>> groupUsers (List<User> userList){
+        Map<Integer, List<User>> groupedUsers = new HashMap<>();
+        for(User user : userList){
+            int userAge = user.getAge();
+            groupedUsers.putIfAbsent(userAge, new ArrayList<>());
+            groupedUsers.get(userAge).add(user);
+        }
+        return groupedUsers;
+    }
+    public String getName() {
+        return name;
+    }
+    public int getAge() {
+        return age;
+    }
+}
