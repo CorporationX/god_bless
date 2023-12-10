7b6c39ac7489114ee70f11202def92b6bd18ac17
Leilorr <148541216+Leilorr@users.noreply.github.com>
2023-12-11 01:24:16 +0300

    Создал класс User с полями: id; имя; возраст; множество активностей (строки). Создал в этом классе метод findHobbyLovers, который принимает список пользователей и множество строк — активности. Возвращает мапу, где ключ пользователь, значение — строка. Этот метод в переданном списке пользователей находит тех, у кого в ИХ множестве активностей есть хотя бы один элемент из переданного в метод множества активностей, сохраняет этого пользователя в виде ключа в результирующую мапу, а значением ему выставляет первую совпавшую активность в двух множествах.

---
+++ src/main/java/faang/school/godbless/User.java
@@ -0,0 +1,27 @@
+package faang.school.godbless;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
+public class User {
+    private int id;
+    private String name;
+    private int age;
+    private Set<String> activities;
+    public Set<String> getActivities() {
+        return activities;
+    }
+    public Map<User, String> findHobbyLovers (List<User> users, Set<String> hobbies){
+    Map<User, String> hobbyLovers = new HashMap<>();
+    for (User user : users){
+        for (String hobby : user.getActivities()){
+            if (hobbies.contains(hobby))
+                hobbyLovers.put(user, hobby);
+            break;
+        }
+    }
+    return hobbyLovers;
+    }
+}
