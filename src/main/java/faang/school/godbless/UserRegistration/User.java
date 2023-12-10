45a43e57acb1fa5dc36d180564911153da9447a0
Marina_Zu <bugsi-47@yandex.ru>
2023-12-10 14:31:18 +0300

    Change the name verification

--- src/main/java/faang/school/godbless/UserRegistration/User.java
+++ src/main/java/faang/school/godbless/UserRegistration/User.java
@@ -15,23 +15,23 @@ public class User {
     private final Set<String> VALID_JOBS = new HashSet<>(Arrays.asList("Google", "Uber", "Amazon"));
     private final Set<String> VALID_ADDRESSES = new HashSet<>(Arrays.asList("London", "New York", "Amsterdam"));
 
-    public static Map<Integer, List<User>> groupUsers(List<User> users){
+    public static Map<Integer, List<User>> groupUsers(List<User> users) {
         Map<Integer, List<User>> usersMap = new HashMap<>();
-        for(User user: users){
+        for (User user : users) {
             int key = user.getAge();
-            if(!usersMap.containsKey(key)){
+            if (!usersMap.containsKey(key)) {
                 usersMap.put(key, new ArrayList<>());
             }
             usersMap.get(key).add(user);
         }
-        for(Map.Entry<Integer, List<User>> entry: usersMap.entrySet()){
+        for (Map.Entry<Integer, List<User>> entry : usersMap.entrySet()) {
             System.out.println("Ключ: " + entry.getKey() + " Значение: " + entry.getValue());
         }
         return usersMap;
     }
 
     public User(String name, int age, String workplace, String address) {
-        if (name.isEmpty() || name == null || age < 18 || !VALID_JOBS.contains(workplace) || !VALID_ADDRESSES.contains(address)){
+        if (name == null || name.isEmpty() || age < 18 || !VALID_JOBS.contains(workplace) || !VALID_ADDRESSES.contains(address)) {
             throw new IllegalArgumentException("Введены неверные данные");
         }
         this.name = name;
