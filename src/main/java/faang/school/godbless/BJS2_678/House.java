f92fecfa56dd7192139aac19d90bfdd3cc882e05
Nikita <soul2009.89@gmail.com>
2023-12-10 16:43:59 +0500

    BJS2-678

---
+++ src/main/java/faang/school/godbless/BJS2_678/House.java
@@ -0,0 +1,16 @@
+package faang.school.godbless.BJS2_678;
+
+import java.security.Key;
+import java.util.HashMap;
+import java.util.Map;
+
+public class House {
+    private String name;
+    private String symbol;
+
+    protected House(String name, String symbol) {
+        this.name = name;
+        this.symbol = symbol;
+    }
+
+}
