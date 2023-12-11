308ce6c32ac8308a57149eb3cb1758a48bec3798
Rikaf <fakir207@mail.ru>
2023-12-11 16:13:30 +0500

    delete

--- src/main/java/faang/school/godbless/hogvarts/SpellAction.java
+++
@@ -1,6 +0,0 @@
-package faang.school.godbless.hogvarts;
-
-@FunctionalInterface
-public interface SpellAction {
-    String cast (String spell);
-}
--- src/main/java/faang/school/godbless/hogvarts/SpellCaster.java
+++
@@ -1,7 +0,0 @@
-package faang.school.godbless.hogvarts;
-
-public class SpellCaster {
-    public void cast(String spell, SpellAction spellAction) {
-        System.out.println(spellAction.cast(spell));
-    }
-}
