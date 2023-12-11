7ab77196cdc48d16bf8bc361fadf81e20da234ff
lonovojvladimir <lonovojvladimir@gmail.com>
2023-12-11 20:56:56 +0300

    add class SpellCaster, interface SpellAction

---
+++ src/main/java/faang/school/godbless/SpellCaster.java
@@ -0,0 +1,8 @@
+package faang.school.godbless;
+
+public class SpellCaster {
+    public void cast(String spell, SpellAction spellAction){
+        System.out.println(spellAction.toSpellAction(spell));
+    }
+
+}
