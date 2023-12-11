9537b05ae2e8a25f4c6eff6ad0c85a43c3cd9277
Teo Shtanko <atraxlxxxl@gmail.com>
2023-12-11 23:16:47 +0300

    переделал revers

--- src/main/java/faang/school/godbless/Turn_around/Example.java
+++ src/main/java/faang/school/godbless/Turn_around/Example.java
@@ -1,14 +1,11 @@
 package faang.school.godbless.Turn_around;
 
 public class Example {
-    public static void reverse(int[] array){
-        int d=0;
-        for (int i = 0; i < array.length; i++) {
-            array[i]=array.length-d;
-            d++;
-        }
-        for(int a:array){
-            System.out.println(a);
+    public static void revers(int[] massive) {
+        for (int i = 0; i < massive.length / 2; i++) {
+            int tmp = massive[i];
+            massive[i] = massive[massive.length - i - 1];
+            massive[massive.length - i - 1] = tmp;
         }
     }
 }
--- src/main/java/faang/school/godbless/Turn_around/Main.java
+++ src/main/java/faang/school/godbless/Turn_around/Main.java
@@ -2,8 +2,8 @@ package faang.school.godbless.Turn_around;
 
 public class Main {
     public static void main (String [] args){
-        int[] d ={1,2,3,4,5};
-        Example.reverse(d);
+        int[] d ={6,4,3,8,1};
+        Example.revers(d);
         for(int a :d){
             System.out.println(a);
         }
