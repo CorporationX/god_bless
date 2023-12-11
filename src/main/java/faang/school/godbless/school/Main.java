c19dc6028e38739ee6588a7188bda9ab39829fa1
Ruslan Latyshev <latyshevruslan@ya.ru>
2023-12-11 22:57:10 +0300

    resolve task triangleArea with lambdas

---
+++ src/main/java/faang/school/godbless/school/Main.java
@@ -0,0 +1,29 @@
+package faang.school.godbless.school;
+
+import java.util.function.Function;
+
+public class Main {
+    Double triangleArea(double a, double b, double c) throws IllegalArgumentException {
+        Function<Double, Function<Double, Double>> sum = (x) -> y -> x + y;
+        Function<Double, Function<Double, Double>> mul = (x) -> y -> x * y;
+        Function<Double, Function<Double, Double>> sub = (x) -> y -> x - y;
+        Function<Double, Function<Double, Double>> div = (x) -> y -> x / y;
+        Function<Double, Double> sqrt = (x) -> Math.sqrt(x);
+
+        double result = 0;
+        if(a > 0 && b > 0 && c > 0) {
+            double half = div.apply(sum.apply(sum.apply(a).apply(b)).apply(c)).apply(2.0);
+            double halfMinusA = sub.apply(half).apply(a);
+            double halfMinusB = sub.apply(half).apply(b);
+            double halfMinusC = sub.apply(half).apply(c);
+            double all = mul.apply(mul.apply(half).apply(halfMinusA)).apply(mul.apply(halfMinusB).apply(halfMinusC));
+            result = sqrt.apply(all);
+        }
+        return result;
+    }
+
+    public static void main(String[] args) {
+        Main main = new Main();
+        System.out.println(main.triangleArea(30,10,10));
+    }
+}
