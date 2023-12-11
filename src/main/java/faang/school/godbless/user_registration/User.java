0474101f7f4bd09fc947538238570058fe0fadc8
Teo Shtanko <atraxlxxxl@gmail.com>
2023-12-11 21:38:56 +0300

    удаление лишнего

--- src/main/java/faang/school/godbless/user_registration/User.java
+++ src/main/java/faang/school/godbless/user_registration/User.java
@@ -3,19 +3,20 @@ package faang.school.godbless.user_registration;
 import java.util.Set;
 
 public class User {
+    private final static int MIN_AGE = 18;
     private String name;
     private int age;
-    private String work_place;
+    private String workPlace;
     private String address;
     private final static Set<String> VALID_JOBS = Set.of("Google", "Uber", "Amazon");
     private final static Set<String> VALID_ADDRESSES = Set.of("London", "New York", "Amsterdam");
-    private final static int MIN_AGE = 18;
+
 
     public User(String name, int age, String work_place, String address) {
-        if (name!=null||!name.isEmpty() && age >= MIN_AGE && VALID_JOBS.contains(work_place) && VALID_ADDRESSES.contains(address)) {
+        if (name != null || !name.isEmpty() && age >= MIN_AGE && VALID_JOBS.contains(work_place) && VALID_ADDRESSES.contains(address)) {
             this.name = name;
             this.age = age;
-            this.work_place = work_place;
+            this.workPlace = work_place;
             this.address = address;
         } else {
             throw new IllegalArgumentException("Incorrect validation");
@@ -27,7 +28,7 @@ public class User {
         return "User{" +
                 "name='" + name + '\'' +
                 ", age=" + age +
-                ", work_place='" + work_place + '\'' +
+                ", work_place='" + workPlace + '\'' +
                 ", address='" + address + '\'' +
                 '}';
     }
