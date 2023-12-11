610534bdbbaa2660258ca22e9e57cad21b0a7447
Emperor_Samedi <mexanik13970@gmail.com>
2023-12-11 20:32:15 +0300

    Fixed

--- src/main/java/JavaCore_2/User.java
+++ src/main/java/JavaCore_2/User.java
@@ -4,29 +4,29 @@ import java.util.*;
 
 public class User {
 
+    private final Set<String> VALID_JOBS = Set.of("Google", "Uber", "Amazon");
+    private final Set<String> VALID_ADDRESSES = Set.of("London", "New York", "Amsterdam");
     private String name;
     private int age;
     private String job;
     private String address;
 
-    private final Set<String> VALID_JOBS = Set.of("Google", "Uber", "Amazon");
-    private final Set<String> VALID_ADDRESSES = Set.of("London", "New York", "Amsterdam");
 
     public User(String name, int age, String job, String address) {
-        if ((name!=null) && (!name.isBlank()) && (age>=18) && (VALID_JOBS.equals(job)) && (VALID_ADDRESSES.equals(address))) {
+        if ((name != null) && (!name.isBlank()) && (age >= 18) && (VALID_JOBS.equals(job)) && (VALID_ADDRESSES.equals(address))) {
             this.name = name;
             this.age = age;
             this.job = job;
             this.address = address;
         } else {
-            throw new IllegalArgumentException();
+            throw new IllegalArgumentException("The entered data is incorrect");
         }
     }
 
-    public static Map<Integer, List<User>> groupUsers(List<User> users){
+    public static Map<Integer, List<User>> groupUsers(List<User> users) {
         Map<Integer, List<User>> usersGroups = new HashMap<>();
-        for (User user: users) {
-            if (!(usersGroups.containsKey(user.getAge()))){
+        for (User user : users) {
+            if (!(usersGroups.containsKey(user.getAge()))) {
                 usersGroups.put(user.getAge(), new ArrayList<>());
             }
             usersGroups.get(user.getAge()).add(user);
