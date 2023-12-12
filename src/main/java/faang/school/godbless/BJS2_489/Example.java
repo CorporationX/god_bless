a788958206009ef2feb051e042d1f08ffc5b0c04
Rauan Tureyev <rauanture@gmail.com>
2023-12-12 18:48:02 +0600

    create method that reverse massive

---
+++ src/main/java/faang/school/godbless/BJS2_489/Example.java
@@ -0,0 +1,19 @@
+package faang.school.godbless.BJS2_489;
+
+import java.util.Arrays;
+
+public class Example {
+    public static void main(String[] args) {
+        int[] nums = {1, 2, 3, 4, 5};
+        int[] reverseNums = reverse(nums);
+        System.out.println(Arrays.toString(reverseNums));
+    }
+    public static int[] reverse(int[] nums){
+        int[] result = new int[nums.length];
+        int lastIndex = nums.length-1;
+        for (int i = 0; i < nums.length; i++) {
+            result[i] = nums[lastIndex-i];
+        }
+        return result;
+    }
+}
