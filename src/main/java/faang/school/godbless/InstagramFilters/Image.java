76191750f68b28c62223bebc5766d4ad8e550d23
Marina_Zu <bugsi-47@yandex.ru>
2023-12-12 14:18:01 +0300

    Add task "Фильтры Instagram"

---
+++ src/main/java/faang/school/godbless/InstagramFilters/Image.java
@@ -0,0 +1,13 @@
+package faang.school.godbless.InstagramFilters;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.ToString;
+
+@AllArgsConstructor
+@Getter
+@ToString
+public class Image {
+    private String name;
+    private String description;
+}
