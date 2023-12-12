27c6b75cd8e1d265be03b207286d67d01daec856
Rikaf <fakir207@mail.ru>
2023-12-12 12:26:05 +0500

    какая то путаница с гитом у меня, Хогвартс перезалил

---
+++ src/main/java/faang/school/godbless/hogwarts/SpellCaster.java
@@ -0,0 +1,7 @@
+package faang.school.godbless.hogwarts;
+
+public class SpellCaster {
+    public void cast(String spell, SpellAction spellAction) {
+        System.out.println(spellAction.cast(spell));
+    }
+}
