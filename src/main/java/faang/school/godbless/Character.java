97fbc61bceee5214e45959071c9db5ab1fa96af8
Leilorr <148541216+Leilorr@users.noreply.github.com>
2023-12-13 02:34:35 +0300

    1. Создать абстрактный класс Character со следующими полями:
    Имя;
    
    Сила;
    
    Ловкость;
    
    Интеллект;
    
    Здоровье. У всех персонажей по умолчанию 100.

--- src/main/java/faang/school/godbless/Character.java
+++ src/main/java/faang/school/godbless/Character.java
@@ -11,27 +11,33 @@ public abstract class Character {
     protected int agility;
     protected int intellect;
     protected int health = 100;
-    public Character(String name){
+
+    public Character(String name) {
         this.name = name;
     }
-    public Character(String name, int strength, int agility, int intellect){
+
+    public Character(String name, int strength, int agility, int intellect) {
         this.name = name;
         this.strength = strength;
         this.agility = agility;
         this.intellect = intellect;
     }
-    public abstract void attack (Character target);
+
+    public abstract void attack(Character target);
 }
-class Warrior extends Character{
-    public Warrior (String name){
+
+class Warrior extends Character {
+    public Warrior(String name) {
         super(name, 10, 5, 3);
     }
+
     @Override
     public void attack(Character target) {
         target.health -= this.getStrength();
 
     }
 }
+
 class Archer extends Character {
     public Archer(String name) {
         super(name, 3, 10, 5);
