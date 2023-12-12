35e9bd7adfba2fda1081e13579e1b973b430eeba
lonovojvladimir <lonovojvladimir@gmail.com>
2023-12-12 22:37:07 +0300

    add class EmailProcessor, Email

---
+++ src/main/java/faang/school/godbless/Email.java
@@ -0,0 +1,15 @@
+package faang.school.godbless;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+public class Email {
+    private String subject;
+    private String body;
+    private boolean isImportant;
+
+}
