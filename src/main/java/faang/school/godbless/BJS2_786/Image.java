1ac6b9d16c5f60c3e5bac7ce006edc016555d37b
Edisolt Khatsiev <Edisolt Khatsiev>
2023-12-13 00:50:12 +0300

    Фильтры Instagram

---
+++ src/main/java/faang/school/godbless/BJS2_786/Image.java
@@ -0,0 +1,11 @@
+package faang.school.godbless.BJS2_786;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class Image {
+    private String name;
+    private String description;
+}
