7cd68cd11870054e3b0eb2d68366dcee4b3b3b1b
Edisolt Khatsiev <Edisolt Khatsiev>
2023-12-13 00:14:54 +0300

    Meta-вселенная?

--- src/main/java/faang/school/godbless/BJS2_748/Main.java
+++ src/main/java/faang/school/godbless/BJS2_748/Main.java
@@ -7,22 +7,24 @@ public class Main {
         NotificationManager notificationManager = new NotificationManager();
 
         // Регистрация обработчиков оповещений
-        notificationManager.registerHandler("email", (notification) -> System.out.println("Отправка по электронной почте: " + notification.getMessage()));
-        notificationManager.registerHandler("sms", (notification) -> System.out.println("Отправка SMS: " + notification.getMessage()));
-        notificationManager.registerHandler("push", (notification) -> System.out.println("Отправка push-уведомления: " + notification.getMessage()));
+        notificationManager.registerHandler("email", notification -> System.out.println("Отправка по электронной почте: " + notification.getMessage()));
+        notificationManager.registerHandler("sms", notification -> System.out.println("Отправка SMS: " + notification.getMessage()));
+        notificationManager.registerHandler("push", notification -> System.out.println("Отправка push-уведомления: " + notification.getMessage()));
 
-        notificationManager.censoredHandler(List.of("дурак", "козел", "идиот"), (notification -> {
-            notification.setMessage("Сообщение было удалено!");
+        //if message contains any bad word of list
+        notificationManager.censoredWords("sms", List.of("дурак", "идиот"));
+        notificationManager.censoredHandler("sms", (badWord, notification) -> {
+            if (notification.getMessage().contains(badWord)) {
+                return new Notification(notification.getType(), "Сообщение удалено!");
+            }
             return notification;
-        }));
+        });
 
         // Отправка оповещений
         Notification emailNotification = new Notification("email", "Ваша учетная запись успешно активирована");
-        Notification smsNotification = new Notification("sms", "Вы успешно изменили свой пароль");
+        Notification smsNotification = new Notification("sms", "Вы успешно изменили свой пароль, дурак");
         Notification pushNotification = new Notification("push", "Новый пост от пользователя: JohnDoe");
         Notification callNotification = new Notification("call", "Входящий вызов от пользователя: Vlad Mishustin");
-
-
 
         notificationManager.sendNotification(emailNotification);
         notificationManager.sendNotification(smsNotification);
--- src/main/java/faang/school/godbless/BJS2_748/NotificationManager.java
+++ src/main/java/faang/school/godbless/BJS2_748/NotificationManager.java
@@ -1,33 +1,42 @@
 package faang.school.godbless.BJS2_748;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.function.BiFunction;
 import java.util.function.Consumer;
-import java.util.function.Function;
 
 public class NotificationManager {
-    private final Map<String, Consumer<Notification>> registerHandler = new HashMap<>();
-    private final Map<String, Function<Notification, Notification>> censoredHandler = new HashMap<>();
+    private Map<String, Consumer<Notification>> registerHandler = new HashMap<>();
+    private Map<String, BiFunction<String, Notification, Notification>> censoredHandler = new HashMap<>();
+    private List<String> censoredWords = new ArrayList<>();
 
     public void registerHandler(String notificationType, Consumer<Notification> notificationAction) {
-        registerHandler.put(notificationType, notificationAction);
+        this.registerHandler.put(notificationType, notificationAction);
     }
 
-    public void censoredHandler(List<String> badWords, Function<Notification, Notification> censorAction) {
-        badWords.forEach(badWord -> censoredHandler.put(badWord, censorAction));
+    public void censoredWords(String notificationType, List<String> censoredWords) {
+        this.censoredWords.addAll(censoredWords);
+    }
+
+    public void censoredHandler(String notificationType, BiFunction<String, Notification, Notification> censorAction) {
+        this.censoredHandler.put(notificationType, censorAction);
     }
 
     public void sendNotification(Notification notification) {
-        censoredHandler.forEach((key, value) -> {
-            boolean isBadWord = notification.getMessage().contains(key);
-            if (isBadWord) {
-                value.apply(notification);
-            }
-        });
+        String notificationType = notification.getType();
 
-        registerHandler.getOrDefault(notification.getType(), (defaultNotificationAction ->
-                        System.out.println("Action not found for notification type: " + notification.getType())))
+        if (censoredHandler.containsKey(notificationType)) {
+            for (String word : censoredWords) {
+                if (notification.getMessage().contains(word)) {
+                    notification = this.censoredHandler.get(notificationType).apply(word, notification);
+                    break;
+                }
+            }
+        }
+
+        this.registerHandler.getOrDefault(notificationType, (notif) -> System.out.println("Action not found for notification type: " + notificationType))
                 .accept(notification);
     }
 }
