67372f6aa0b1057c3a67148c3866c71dc19996c7
Rid03 <90507932+Rid03@users.noreply.github.com>
2023-12-13 16:31:26 +0300

    added validate (validateConstructor)

--- src/main/java/faang/school/godbless/collectusers/User.java
+++ src/main/java/faang/school/godbless/collectusers/User.java
@@ -21,7 +21,7 @@ public class User {
     private String address;
 
     public User(String name, int age, String job, String address) {
-        validateConstructor();
+        validateConstructor(name, age, job, address);
         this.name = name;
         this.age = age;
         this.job = job;
@@ -31,30 +31,14 @@ public class User {
     public static Map<Integer, List<User>> groupUsers(List<User> users) {
         Map<Integer, List<User>> groupUsers = new TreeMap<>();
         for (User user : users) {
-            if (!groupUsers.containsKey(user.getAge())) {
-                groupUsers.put(user.getAge(), new ArrayList<>());
-            }
-            groupUsers.get(user.getAge()).add(user);
+            int usersAge = user.getAge();
+            groupUsers.putIfAbsent(usersAge, new ArrayList<>());
+            groupUsers.get(usersAge).add(user);
         }
         return groupUsers;
     }
     private void validateConstructor(String name, int age, String job, String address) {
-        validate(name);
-        validate(age);
-        validate(job);
-        validate(address);
+        if(name == null || age <= 18 || !VALID_JOBS.contains(job) || !VALID_ADDRESSES.contains(address))
+            throw new IllegalArgumentException();
     }
-    private void validateName() {
-
-    }
-    private void validateAge() {
-
-    }
-    private void validateJob() {
-
-    }
-    private void validateAddress() {
-
-    }
-
 }
