04b378c63ca05b1726eafe155dc2df17006fcf1c
Marina Razina <razinamv3@gmail.com>
2023-12-13 09:54:05 +0300

    create realization for task "Google Переводчик под нашим контролем"

---
+++ src/main/java/googleTranslate_804/DictionaryProcessor.java
@@ -0,0 +1,10 @@
+package googleTranslate_804;
+
+import java.util.function.BiConsumer;
+
+public class DictionaryProcessor {
+
+    public void processWord(String word, String translation, BiConsumer<String, String> biConsumer) {
+        biConsumer.accept(word,translation);
+    }
+}
