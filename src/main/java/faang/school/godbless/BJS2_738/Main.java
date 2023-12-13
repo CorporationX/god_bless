ef838f21c5164c90aede851bdb65ec80e0c68805
Edisolt5498 <48184500+Edisolt5498@users.noreply.github.com>
2023-12-14 00:08:43 +0300

    Update Main.java
    comments were deleted

--- src/main/java/faang/school/godbless/BJS2_738/Main.java
+++ src/main/java/faang/school/godbless/BJS2_738/Main.java
@@ -10,33 +10,26 @@ public class Main {
     public static Map<Integer, Data> cache = new HashMap<>();
 
     public static void main(String[] args) {
-        // Create some sample Data objects
         Data data1 = new Data("Sample Data 1");
         Data data2 = new Data("Sample Data 2");
         Data data3 = new Data("Sample Data 3");
 
-        // Add data to the cache
         add(data1);
         add(data2);
         add(data3);
 
-        // Show the current state of the cache
         System.out.println("Current Cache:");
         showCache();
 
-        // Retrieve data from the cache
-        int idToRetrieve = data2.getId(); // Use the ID of one of the added data
+        int idToRetrieve = data2.getId();
         Data retrievedData = getDataById(idToRetrieve);
 
-        // Show the updated state of the cache after retrieval
         System.out.println("Cache after retrieval:");
         showCache();
 
-        // Add data to the cache
         add(new Data("Sample Data 4"));
         add(new Data("Sample Data 5"));
 
-        // Show the updated state of the cache after overload
         showCache();
     }
 
