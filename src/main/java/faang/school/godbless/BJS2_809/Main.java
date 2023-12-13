82c4708a50ef32fae3e03cdf5dce81fda83c86fa
Edisolt5498 <48184500+Edisolt5498@users.noreply.github.com>
2023-12-14 00:11:18 +0300

    Update Main.java
    comments were deleted

--- src/main/java/faang/school/godbless/BJS2_809/Main.java
+++ src/main/java/faang/school/godbless/BJS2_809/Main.java
@@ -7,13 +7,10 @@ public class Main {
 
         InventoryManager manager = new InventoryManager();
 
-// Добавляем предмет в инвентарь
         manager.addItem(frodo, ring, (item) -> System.out.println(item.getName() + " was added to the inventory."));
 
-// Обновляем предмет в инвентаре
         manager.updateItem(frodo, (item) -> item.getName().equals("The One Ring"), (item) -> new Item(item.getName(), item.getValue() * 2));
 
-// Удаляем предмет из инвентаря
         manager.removeItem(frodo, (item) -> item.getName().equals("The One Ring"));
     }
 }
