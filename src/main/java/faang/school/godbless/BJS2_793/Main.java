4b6ebe033a585669c02d0d9f5c7af22de8809df8
Edisolt5498 <48184500+Edisolt5498@users.noreply.github.com>
2023-12-14 00:10:48 +0300

    Update Main.java
    comments were deleted

--- src/main/java/faang/school/godbless/BJS2_793/Main.java
+++ src/main/java/faang/school/godbless/BJS2_793/Main.java
@@ -10,19 +10,16 @@ public class Main {
     public static void main(String[] args) {
         EmailProcessor emailProcessor = new EmailProcessor();
 
-// Создание списка входящих писем
         List<Email> emails = Arrays.asList(
                 new Email("Письмо 1", "Текст письма 1", false),
                 new Email("Письмо 2", "some text", true),
                 new Email("Спам", "Текст спама", false)
         );
 
-// Создание фильтров, обработчиков и преобразователей
         Predicate<Email> importantFilter = email -> email.isImportant();
         Consumer<Email> printEmail = email -> System.out.println("Обработано письмо: " + email.getSubject());
         Function<Email, String> toUpperCase = email -> email.getBody().toUpperCase();
 
-// Обработка писем
         emailProcessor.processEmails(emails, importantFilter, printEmail, toUpperCase);
     }
 }
