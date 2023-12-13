7af9dec2471b07d7b34e8632b23da2d6f2b8938f
Kamil Gasanov <gasanovkamil516@gmail.com>
2023-12-13 18:37:20 +0300

    finished

---
+++ src/main/java/faang/school/godbless/lambda/task15rotate_matrix/Coordinates.java
@@ -0,0 +1,11 @@
+package faang.school.godbless.lambda.task15rotate_matrix;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class Coordinates {
+    private int x;
+    private int y;
+}
