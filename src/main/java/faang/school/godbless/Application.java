3d73e54e65ab036c191f6aa201c1c0f8ba408a59
lonovojvladimir <lonovojvladimir@gmail.com>
2023-12-13 23:40:17 +0300

    add class EmailProcessor, Email

--- src/main/java/faang/school/godbless/Application.java
+++ src/main/java/faang/school/godbless/Application.java
@@ -1,8 +1,36 @@
 package faang.school.godbless;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class Application {
     public static void main(String... args) {
-
+        List<Integer> nums = Arrays.asList(1, 2, 3, 4);
+        System.out.println(calculate(nums, (a,b)->b-a));
+        System.out.println(sum(nums));
+        System.out.println(product(nums));
 
     }
+
+    public static int calculate(List<Integer> nums, Calculator calculator) {
+        if (nums.isEmpty()) {
+            throw new IllegalArgumentException("empty list");
+        }
+
+        int result = nums.get(0);
+        for (int i = 1; i < nums.size(); ++i) {
+            result = calculator.calculate(result, nums.get(i));
+        }
+        return result;
+    }
+    public static int sum(List<Integer> nums) {
+        return calculate(nums, (a, b) -> a + b);
+    }
+
+    public static int product(List<Integer> nums){
+        return calculate(nums, (a, b) -> a * b);
+    }
+
+
 }
---
+++ src/main/java/faang/school/godbless/Calculator.java
@@ -0,0 +1,7 @@
+package faang.school.godbless;
+
+@FunctionalInterface
+public interface Calculator {
+    int calculate(int a, int b);
+
+}
