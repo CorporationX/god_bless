dcd595b33f366d79443975d11e27399afd498244
Edisolt Khatsiev <Edisolt Khatsiev>
2023-12-13 12:43:38 +0300

    Калькулятор

---
+++ src/main/java/faang/school/godbless/BJS2_796/Main.java
@@ -0,0 +1,27 @@
+package faang.school.godbless.BJS2_796;
+
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) {
+        List<Integer> numbers = List.of(1, 2, 3, 4, 5);
+
+        System.out.println(sum(numbers));
+        System.out.println(produce(numbers));
+    }
+
+    public static int sum(List<Integer> nums) {
+        return calculate(nums, (a, b) -> a + b);
+    }
+
+    public static int produce(List<Integer> nums) {
+        return calculate(nums, (a, b) -> a * b);
+    }
+
+    public static int calculate(List<Integer> nums, Calculator calculator) throws IllegalArgumentException {
+        if (nums == null) {
+            throw new IllegalArgumentException("List is null");
+        }
+        return nums.stream().reduce((a, b) -> calculator.operate(a, b)).orElse(0);
+    }
+}
