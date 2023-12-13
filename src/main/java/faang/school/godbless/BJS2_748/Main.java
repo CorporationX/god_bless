ee1f2570cba50043cd43ccaa7bad1e9c82f4a554
Edisolt5498 <48184500+Edisolt5498@users.noreply.github.com>
2023-12-14 00:09:54 +0300

    Update Main.java
    comments were deleted

--- src/main/java/faang/school/godbless/BJS2_748/Main.java
+++ src/main/java/faang/school/godbless/BJS2_748/Main.java
@@ -6,12 +6,10 @@ public class Main {
     public static void main(String[] args) {
         NotificationManager notificationManager = new NotificationManager();
 
-        // Регистрация обработчиков оповещений
         notificationManager.registerHandler("email", notification -> System.out.println("Отправка по электронной почте: " + notification.getMessage()));
         notificationManager.registerHandler("sms", notification -> System.out.println("Отправка SMS: " + notification.getMessage()));
         notificationManager.registerHandler("push", notification -> System.out.println("Отправка push-уведомления: " + notification.getMessage()));
 
-        //if message contains any bad word of list
         notificationManager.censoredWords("sms", List.of("дурак", "идиот"));
         notificationManager.censoredHandler("sms", (badWord, notification) -> {
             if (notification.getMessage().contains(badWord)) {
@@ -20,7 +18,6 @@ public class Main {
             return notification;
         });
 
-        // Отправка оповещений
         Notification emailNotification = new Notification("email", "Ваша учетная запись успешно активирована");
         Notification smsNotification = new Notification("sms", "Вы успешно изменили свой пароль, дурак");
         Notification pushNotification = new Notification("push", "Новый пост от пользователя: JohnDoe");
