3870fc9c174c4a9668555d0a393255adc34e72d3
Teo Shtanko <atraxlxxxl@gmail.com>
2023-12-14 06:02:39 +0300

    куча методов

--- src/main/java/faang/school/godbless/filter_spam/Main.java
+++ src/main/java/faang/school/godbless/filter_spam/Main.java
@@ -4,19 +4,19 @@ import java.util.Arrays;
 import java.util.List;
 
 public class Main {
-    public static void main(String... args){
+    public static void main(String... args) {
         MessageProcessor messageProcessor = new MessageProcessor();
         MessageFilter spamFilter = message -> !message.toLowerCase().contains("spam");
-        MessageFilter lengthFilter = message -> message.length()<20;
+        MessageFilter lengthFilter = message -> message.length() < 20;
         MessageFilter debtFilter = message -> !message.toLowerCase().contains("займешь");
 
-        List<MessageFilter> filters = Arrays.asList(spamFilter,lengthFilter,debtFilter);
+        List<MessageFilter> filters = Arrays.asList(spamFilter, lengthFilter, debtFilter);
         String[] messages = {"Привет!", "It is spam!", "Займешь 1к до зп", "Длинное сообщение без спама и займов"};
-// Интересно, но если написать "спам" по-русски и лямбде и в массиве, то будет его пропускать. ДА и вообще, дебаг русский
-// язык не воспринимает у меня
-        for(String message : messages){
-            boolean isFiltered=messageProcessor.processMessage(message,filters);
-            System.out.println("Message: "+message+" Skipped: "+isFiltered);
+        // интересно, но если написать "спам" по-русски и лямбде и в массиве, то будет его пропускать. ДА и вообще, дебаг русский
+        // язык не воспринимает у меня
+        for (String message : messages) {
+            boolean isFiltered = messageProcessor.processMessage(message, filters);
+            System.out.println("Message: " + message + " Skipped: " + isFiltered);
         }
     }
 }
--- src/main/java/faang/school/godbless/filter_spam/MessageProcessor.java
+++ src/main/java/faang/school/godbless/filter_spam/MessageProcessor.java
@@ -3,15 +3,13 @@ package faang.school.godbless.filter_spam;
 import java.util.List;
 
 public class MessageProcessor {
-    public boolean processMessage(String message, List<MessageFilter> filters){
-    boolean isFiltered=true;
-    for (MessageFilter filter : filters){
-        if (filter.apply(message) == false){
-            isFiltered=false;
-            break;
-        }
+    public boolean processMessage(String message, List<MessageFilter> filters) {
 
-    }
-    return isFiltered;
+        for (MessageFilter filter : filters) {
+            if (!filter.apply(message)) {
+                return false;
+            }
+        }
+        return true;
     }
 }
