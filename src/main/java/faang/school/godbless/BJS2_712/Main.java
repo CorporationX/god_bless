a23902a58089367146f6cbc1971b2b6adcf639a8
Edisolt Khatsiev <Edisolt Khatsiev>
2023-12-14 22:41:56 +0300

    Java HashMap: Индекс запросов ELK. Я не хочу спать теперь и сделал нормально!

--- src/main/java/faang/school/godbless/BJS2_712/Main.java
+++ src/main/java/faang/school/godbless/BJS2_712/Main.java
@@ -4,26 +4,35 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Comparator;
 
 public class Main {
     public static Map<User, List<Query>> requestsByUsers = new HashMap<>();
 
     public static void main(String[] args) {
-        User user1 = new User("John Doe");
-        User user2 = new User("Jane Doe");
+        User user1 = new User("Donald Trump");
+        User user2 = new User("Joseph Biden");
 
         Query query1 = new Query("Select * from users");
-        Query query2 = new Query("Insert into products values (1, 'Product')");
+        Query query2 = new Query("Select * from Bootcamp");
+        Query query3 = new Query("Drop database FAANG");
+        List<Query> queriesForUser1 = List.of(query1, query2, query3);
+        addQueries(user1, queriesForUser1);
 
-        addQuery(user1, query1);
-        addQuery(user2, query2);
+        Query query4 = new Query("Insert into products values (1, 'Apple')");
+        Query query5 = new Query("Insert into products values (2, 'Samsung')");
+        Query query6 = new Query("Insert into products values (3, 'SpaceX')");
+        addQuery(user2, query4);
+        addQuery(user2, query5);
+        addQuery(user2, query6);
 
-        showAllUserAndQuery();
+        printAll();
+        printAllByUser(user2);
 
         removeUserAndQuery(user1);
 
         System.out.println("\nAfter removing user1:");
-        showAllUserAndQuery();
+        printAll();
     }
 
     public static void addQueries(User user, List<Query> queries) {
@@ -31,16 +40,29 @@ public class Main {
     }
 
     public static void addQuery(User user, Query query) {
-        requestsByUsers.put(user, new ArrayList<>(List.of(query)));
+        requestsByUsers.computeIfAbsent(user, key -> new ArrayList<>()).add(query);
     }
 
     public static void removeUserAndQuery(User user) {
         requestsByUsers.remove(user);
     }
 
-    public static void showAllUserAndQuery() {
+    public static void printAll() {
         requestsByUsers.forEach((user, queries) -> {
             System.out.println(user + " Queries: " + queries);
         });
+    }
+
+    public static void printAllByUser(User user) {
+        List<Query> queries = requestsByUsers.get(user);
+        if (queries == null) {
+            System.out.println("User not found");
+            return;
+        }
+
+        queries.sort(Comparator.comparing(query -> query.getTimestamp()));
+
+        System.out.println("Queries for User " + user.getName() + ":");
+        queries.forEach(query -> System.out.println(query));
     }
 }
