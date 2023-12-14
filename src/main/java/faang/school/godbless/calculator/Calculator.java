eb6c3f78614bc62ef9f592156749f2589a3494e0
v.dyakov <v.dyakov@sbereducation.ru>
2023-12-14 20:39:57 +0300

    fix: new class Calculate (BJS2-917)

---
+++ src/main/java/faang/school/godbless/calculator/Calculator.java
@@ -0,0 +1,6 @@
+package faang.school.godbless.calculator;
+
+@FunctionalInterface
+public interface Calculator {
+    int calculate(int num1, int num2);
+}
---
+++ src/main/java/faang/school/godbless/calculator/Main.java
@@ -0,0 +1,41 @@
+package faang.school.godbless.calculator;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) {
+        List<Integer> nums = new ArrayList<>();
+
+        nums.add(1);
+        nums.add(2);
+        nums.add(3);
+        nums.add(4);
+
+        System.out.println(sum(nums));
+        System.out.println(product(nums));
+
+    }
+
+    public static int calculate(List<Integer> nums, Calculator calculator) {
+        if (nums.isEmpty()) {
+            throw new IllegalArgumentException("empty list");
+        }
+
+        int result = nums.get(0);
+        for (int i = 1; i < nums.size(); ++i) {
+            result = calculator.calculate(result, nums.get(i));
+        }
+        return result;
+    }
+
+    public static int sum(List<Integer> nums) {
+        return calculate(nums, (a, b) -> a + b);
+    }
+
+    public static int product(List<Integer> nums) {
+        return calculate(nums, (a, b) -> a * b);
+    }
+
+
+}
