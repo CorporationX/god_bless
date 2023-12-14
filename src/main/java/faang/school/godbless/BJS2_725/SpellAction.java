7bc44bb17de9c9ccc8a0c45302e7741822808b41
Rauan Tureyev <rauanture@gmail.com>
2023-12-14 19:58:19 +0600

    used own functional interface

---
+++ src/main/java/faang/school/godbless/BJS2_725/SpellAction.java
@@ -0,0 +1,6 @@
+package faang.school.godbless.BJS2_725;
+
+@FunctionalInterface
+public interface SpellAction {
+    String action(String spell);
+}
---
+++ src/main/java/faang/school/godbless/BJS2_725/SpellCaster.java
@@ -0,0 +1,7 @@
+package faang.school.godbless.BJS2_725;
+
+public class SpellCaster {
+    public void cast(String spell, SpellAction spellAction){
+        System.out.println(spellAction.action(spell));
+    }
+}
