d848f91aea44a8220feab99726c4e496849830eb
Vlad Meniailov <vlad.meniailov@yandex.ru>
2023-12-14 22:40:57 +0300

    Updated "Counting absenteeism" after Pasha's review.

--- src/main/java/faang/school/godbless/task_4/Main.java
+++ src/main/java/faang/school/godbless/task_4/Main.java
@@ -7,38 +7,44 @@ import java.util.List;
 import java.util.Map;
 
 public class Main {
-    private static List<Student> studentList = createStudents();
+    private static List<Student> studentList = new ArrayList<>(Arrays.asList(
+            new Student("Ivan Ivanov", "Economy", "Economist"),
+            new Student("Sergey Petrov", "Economy", "Economist"),
+
+            new Student("Ivan Sibirko", "Economy", "Creditor"),
+            new Student("Alexey Shelehov", "Economy", "Creditor"),
+
+            new Student("Ruslan Galimov", "Jurisprudence", "Lawyer"),
+            new Student("Anastasia Belikova", "Jurisprudence", "Lawyer"),
+
+            new Student("Nariman Usypov", "Jurisprudence", "Judge"),
+            new Student("Kirill Sobolev", "Jurisprudence", "Judge"),
+
+            new Student("Pavel Volya", "Jurisprudence", "Judge"),
+            new Student("Artur Pirojkov", "Jurisprudence", "Judge")
+    ));
 
     public static void main(String[] args) {
-
 
         System.out.println();
 
         Student newStudent = new Student("Ivan Serov", "Economy", "Economist");
-        Student searchStudent = new Student("Alexey Shelehov", "Economy", "Creditor");
 
         printStudentsByFacultyAndCourse();
+        System.out.println();
 
+        addStudent(newStudent);
 
-    }
+        printStudentsByFacultyAndCourse();
+        System.out.println();
 
-    static List<Student> createStudents() {
-        return Arrays.asList(
-                new Student("Ivan Ivanov", "Economy", "Economist"),
-                new Student("Sergey Petrov", "Economy", "Economist"),
+        removeStudent("Ruslan Galimov", "Jurisprudence", "Lawyer");
 
-                new Student("Ivan Sibirko", "Economy", "Creditor"),
-                new Student("Alexey Shelehov", "Economy", "Creditor"),
+        printStudentsByFacultyAndCourse();
+        System.out.println();
 
-                new Student("Ruslan Galimov", "Jurisprudence", "Lawyer"),
-                new Student("Anastasia Belikova", "Jurisprudence", "Lawyer"),
+        System.out.println(searchStudents("Jurisprudence", "Judge"));
 
-                new Student("Nariman Usypov", "Jurisprudence", "Judge"),
-                new Student("Kirill Sobolev", "Jurisprudence", "Judge"),
-
-                new Student("Pavel Volya", "Jurisprudence", "Judge"),
-                new Student("Artur Pirojkov", "Jurisprudence", "Judge")
-        );
     }
 
     public static void addStudent(Student student) {
@@ -46,14 +52,19 @@ public class Main {
     }
 
     public static void removeStudent(String name, String faculty, String course) {
+        Student removeStudent = null;
         for (Student student : studentList) {
             if (student.getName().equals(name) && student.getFaculty().equals(faculty) && student.getCourse().equals(course)) {
-                studentList.remove(student);
+                removeStudent = student;
+                break;
             }
+        }
+        if (removeStudent != null) {
+            studentList.remove(removeStudent);
         }
     }
 
-    public static List<Student> searchStudent(String faculty, String course) {
+    public static List<Student> searchStudents(String faculty, String course) {
         List<Student> students = new ArrayList<>();
         for (Student student : studentList) {
             if (student.getFaculty().equals(faculty) && student.getCourse().equals(course)) {
@@ -64,20 +75,27 @@ public class Main {
     }
 
     public static void printStudentsByFacultyAndCourse() {
-        getStudentsMap().forEach((stringStringEntry, studentList1) -> {
-            System.out.println(stringStringEntry.getKey() + " " + stringStringEntry.getValue() + " : '\n'" + studentList1);
+        getStudentsMap().forEach((stringStringEntry, studentList) -> {
+            System.out.println(stringStringEntry.getKey() + " " + stringStringEntry.getValue() + " : '\n'" + studentList);
         });
     }
 
     public static Map<Map.Entry<String, String>, List<Student>> getStudentsMap() {
         Map<Map.Entry<String, String>, List<Student>> mapStudentsOnCourse = new HashMap<>();
+
         List<Student> studentsOnCourse;
+
         for (Student st : studentList) {
+
             String faculty = st.getFaculty();
             String course = st.getCourse();
+
             Map.Entry<String, String> mapEntry = Map.entry(faculty, course);
+
             studentsOnCourse = mapStudentsOnCourse.getOrDefault(mapEntry, new ArrayList<>());
+
             studentsOnCourse.add((st));
+
             mapStudentsOnCourse.put(mapEntry, studentsOnCourse);
         }
         return mapStudentsOnCourse;
--- src/main/java/faang/school/godbless/task_4/Student.java
+++ src/main/java/faang/school/godbless/task_4/Student.java
@@ -1,38 +1,17 @@
 package faang.school.godbless.task_4;
 
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
 import lombok.Getter;
-
-import java.util.Objects;
+import lombok.ToString;
 
 @Getter
+@EqualsAndHashCode
+@ToString
+@AllArgsConstructor
+
 public class Student {
     private final String name;
     private final String faculty;
     private final String course;
-
-
-    public Student(String name, String faculty, String course) {
-        this.name = name;
-        this.faculty = faculty;
-        this.course = course;
-    }
-
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        Student student = (Student) o;
-        return Objects.equals(name, student.name) && Objects.equals(faculty, student.faculty) && Objects.equals(course, student.course);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(name, faculty, course);
-    }
-
-    @Override
-    public String toString() {
-        return "Student{" + "name='" + name + "}" + '\n';
-    }
 }
