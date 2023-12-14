ea4a0a502d0fa1b40b106415ad92510bd5fbef7d
Edisolt Khatsiev <Edisolt Khatsiev>
2023-12-14 09:17:25 +0300

    Java HashMap: Поисковой мини-движок Google

--- src/main/java/faang/school/godbless/BJS2_711/Main.java
+++ src/main/java/faang/school/godbless/BJS2_711/Main.java
@@ -1,25 +1,25 @@
 package faang.school.godbless.BJS2_711;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
+import java.util.stream.Collectors;
 
 public class Main {
     public static Map<String, List<WebPage>> index = new HashMap<>();
 
     public static void main(String[] args) {
-        // Creating some sample WebPages
-        WebPage page1 = new WebPage("https://example.com/page1", "FAANG", "This is a sample page.");
+        WebPage page1 = new WebPage("https://example.com/page1", "FAANG", "This is a page page page page sample page.");
         WebPage page2 = new WebPage("https://example.com/page2", "BOOTCAMP", "Another example page.");
         WebPage page3 = new WebPage("https://example.com/page3", "AMAZON", "Yet another example.");
 
-        // Adding WebPages to the index
         addToIndexByKeyWord(page1);
         addToIndexByKeyWord(page2);
         addToIndexByKeyWord(page3);
 
-        // Listing WebPages for a keyword
+        index.forEach((a, b) -> {
+            System.out.println(a);
+            System.out.println(b);
+        });
+
         String keyword = "example";
         List<WebPage> pagesForWord = listWebPage(keyword);
         System.out.println("Pages for word '" + keyword + "':");
@@ -27,11 +27,9 @@ public class Main {
             System.out.println("Page URL: " + page.getUrl());
         }
 
-        // Removing a WebPage by URL
         String urlToRemove = "https://example.com/page1";
         remove(urlToRemove);
 
-        // Listing WebPages for the same keyword after removal
         pagesForWord = listWebPage(keyword);
         System.out.println("\nPages for word '" + keyword + "' after removal:");
         for (WebPage page : pagesForWord) {
@@ -40,7 +38,7 @@ public class Main {
     }
 
     public static void addToIndexByKeyWord(WebPage webPage) {
-        String[] words = webPage.getContent().split("[^a-zA-Z']+");
+        Set<String> words = Arrays.stream(webPage.getContent().split("[^a-zA-Z']+")).collect(Collectors.toSet());
         for (String word : words) {
             index.computeIfAbsent(word, key -> new ArrayList<>()).add(webPage);
         }
