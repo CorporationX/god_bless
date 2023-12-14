02f8da4c3fb799c967e2855f7b99cc2948726912
Rauan Tureyev <rauanture@gmail.com>
2023-12-14 19:52:51 +0600

    used functional interface consumer

--- src/main/java/faang/school/godbless/SpellAction.java
+++
@@ -1,6 +0,0 @@
-package faang.school.godbless;
-
-@FunctionalInterface
-public interface SpellAction {
-    String action(String spell);
-}
--- src/main/java/faang/school/godbless/SpellCaster.java
+++
@@ -1,7 +0,0 @@
-package faang.school.godbless;
-
-public class SpellCaster {
-    public void cast(String spell, SpellAction spellAction){
-        System.out.println(spellAction.action(spell));
-    }
-}
