c4182635263a4ebae2ed5c719d92e4a6ca2d1533
Alexander Bulgakov <asbulgakov90@gmail.com>
2023-12-15 22:28:18 +0300

    Complete BJS2-916

---
+++ src/main/java/faang/school/godbless/alexbulgakoff/functionalinterfaceslambdas/hogwarts/SpellAction.java
@@ -0,0 +1,10 @@
+package faang.school.godbless.alexbulgakoff.functionalinterfaceslambdas.hogwarts;
+
+/**
+ * @author Alexander Bulgakov
+ */
+
+@FunctionalInterface
+public interface SpellAction {
+    String action(String spell);
+}
