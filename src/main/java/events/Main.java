5f8bc90d0838c3f63c4a95bd70f2291d93c4e891
Marina Razina <razinamv3@gmail.com>
2023-12-16 16:50:41 +0300

    Исправления по замечаниям

--- src/main/java/events/Main.java
+++ src/main/java/events/Main.java
@@ -7,23 +7,24 @@ import java.util.Map;
 import java.util.List;
 
 public class Main {
-    private static final Map<Integer, StreamEvent> listEvent = new HashMap<>();
-    private static final Map<String, List<StreamEvent>> typeEvent = new HashMap<>();
+    private static final Map<Integer, StreamEvent> LIST_EVENT = new HashMap<>();
+    private static final Map<String, List<StreamEvent>> TYPE_EVENT = new HashMap<>();
+    private static Integer idCount = 1;
 
     public static void main(String[] args) {
 
-        addNewEvent(01, "type 1", "ohh");
-        addNewEvent(02, "type 1", "woow");
-        addNewEvent(03, "type 2", "OMG");
-        addNewEvent(03, "type 2", "new OMG");
+        addNewEvent("type 1", "ohh");
+        addNewEvent("type 1", "woow");
+        addNewEvent("type 2", "OMG");
+        addNewEvent("type 2", "new OMG");
 
         System.out.println("All events: ");
-        for (Map.Entry entry : listEvent.entrySet()) {
+        for (Map.Entry<Integer, StreamEvent> entry : LIST_EVENT.entrySet()) {
             System.out.println("Event: " + entry.getKey() + " - " + entry.getValue());
         }
 
         System.out.println("\n" + "Type events:");
-        for (Map.Entry entry : typeEvent.entrySet()) {
+        for (Map.Entry<String, List<StreamEvent>> entry : TYPE_EVENT.entrySet()) {
             System.out.println(entry.getKey() + " - " + entry.getValue());
         }
 
@@ -32,43 +33,40 @@ public class Main {
         removeEvent(1);
 
         System.out.println("\n" + "Type events after delete:");
-        for (Map.Entry entry : typeEvent.entrySet()) {
+        for (Map.Entry<String, List<StreamEvent>> entry : TYPE_EVENT.entrySet()) {
             System.out.println(entry.getKey() + " - " + entry.getValue());
         }
     }
 
-    public static void addNewEvent(int id, String type, String data) {
-        if (listEvent.containsKey(id)) {
-            // remove from ArrayList of typeEvent old StreamEvent, this id will be update in listEvent
-            typeEvent.get(type).remove(listEvent.get(id));
-        }
+    public static void addNewEvent(String type, String data) {
 
-        StreamEvent newStream = new StreamEvent(id, type, data);
-        listEvent.put(id, newStream); //add or update (from id) new event in listEvent
-        typeEvent.putIfAbsent(type, new ArrayList<>()); // check contain key, if not - create new ArrayList
-        typeEvent.get(type).add(newStream);  // add new StreamEvent in ArrayList of typeEvent
+        StreamEvent newStream = new StreamEvent(idCount, type, data);
+        LIST_EVENT.put(idCount, newStream); //add or update (from id) new event in listEvent
+        TYPE_EVENT.putIfAbsent(type, new ArrayList<>()); // check contain key, if not - create new ArrayList
+        TYPE_EVENT.get(type).add(newStream);  // add new StreamEvent in ArrayList of typeEvent
+        idCount++;
     }
 
     public static void searchFromId(int id) {
-        System.out.println("\n" + "Search from ID: " + listEvent.get(id));
+        System.out.println("\n" + "Search from ID: " + LIST_EVENT.get(id));
     }
 
     public static void searchFromTypeEvent(String type) {
-        System.out.println("\n" + "Search from type Event: " + typeEvent.get(type));
+        System.out.println("\n" + "Search from type Event: " + TYPE_EVENT.get(type));
     }
 
     public static void removeEvent(int id) {
         final int MIN_MAP_SIZE = 1;
 
-        StreamEvent typeRemoveEvent = listEvent.get(id);
+        StreamEvent typeRemoveEvent = LIST_EVENT.get(id);
         if (typeRemoveEvent == null) {
             throw new RuntimeException("no this ID");
         }
         String typeRemoveEventType = typeRemoveEvent.getEventType();// key for Map typeEvent
-        List<StreamEvent> streamEvents = typeEvent.get(typeRemoveEventType);  // Value for key of Map typeEvent
+        List<StreamEvent> streamEvents = TYPE_EVENT.get(typeRemoveEventType);  // Value for key of Map typeEvent
 
         if (streamEvents.size() == MIN_MAP_SIZE) {
-            typeEvent.remove(typeRemoveEventType);  // if value (List) of Map typeEvent have 1 record - delete key from Map
+            TYPE_EVENT.remove(typeRemoveEventType);  // if value (List) of Map typeEvent have 1 record - delete key from Map
         } else {
             Iterator<StreamEvent> stream = streamEvents.iterator();
             while (stream.hasNext()) {
@@ -78,6 +76,6 @@ public class Main {
                 }
             }
         }
-        listEvent.remove(id);  // remove id from Map listEvent
+        LIST_EVENT.remove(id);  // remove id from Map listEvent
     }
 }
