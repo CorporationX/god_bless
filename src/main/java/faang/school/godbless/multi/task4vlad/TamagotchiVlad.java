bed3f4981986a926ecda4b4b282ea78f7f2411dd
Kamil Gasanov <gasanovkamil516@gmail.com>
2023-12-16 22:14:39 +0300

    progress

---
+++ src/main/java/faang/school/godbless/multi/task4vlad/TamagotchiVlad.java
@@ -0,0 +1,19 @@
+package faang.school.godbless.multi.task4vlad;
+
+public class TamagotchiVlad {
+    public void feed() {
+
+    }
+
+    public void play() {
+
+    }
+
+    public void clean() {
+
+    }
+
+    public void sleep() {
+
+    }
+}
