8008fd8862f673e98b57be542846f9c1255f0522
Rauan Tureyev <rauanture@gmail.com>
2023-12-16 15:17:39 +0600

    create functional interface with exception

---
+++ src/main/java/faang/school/godbless/BJS2_950/Main.java
@@ -0,0 +1,14 @@
+package faang.school.godbless.BJS2_950;
+
+import java.util.function.Supplier;
+
+public class Main {
+    public static <T> T withErrorHandling(Supplier<T> supplier, ExceptionHandler<T> exceptionHandler){
+        try {
+            return supplier.get();
+        } catch (Exception e) {
+            exceptionHandler.handle(new Exception("Не удалось получить доступ к Remote Service. Вернем default значение"));
+        }
+        return (T) "DEFAULT";
+    }
+}
