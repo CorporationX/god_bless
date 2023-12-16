df3774afe7cf36941969ab689003a0dd14dfb625
Kamil Gasanov <gasanovkamil516@gmail.com>
2023-12-16 22:19:23 +0300

    fix

--- src/main/java/faang/school/godbless/multi/task3spotify/Player.java
+++ src/main/java/faang/school/godbless/multi/task3spotify/Player.java
@@ -4,31 +4,27 @@ public class Player {
     private final Object lock = new Object();
     private boolean isPlaying = false;
 
-    public void play() {
-        synchronized (lock) {
-            if (!isPlaying) {
-                System.out.println("* Играет музыка *");
-                isPlaying = true;
-            }
+    public synchronized void play() {
+        if (!isPlaying) {
+            System.out.println("* Играет музыка *");
+            isPlaying = true;
         }
     };
 
-    public void pause() {
-        synchronized (lock) {
-            if (isPlaying) {
-                System.out.println("* Тишина *");
-                isPlaying = false;
-            }
+    public synchronized void pause() {
+        if (isPlaying) {
+            System.out.println("* Тишина *");
+            isPlaying = false;
         }
     };
 
-    public void skip() {
+    public synchronized void skip() {
         if (isPlaying) {
             System.out.println("* Играет следующая музыка *");
         }
     };
 
-    public void previous() {
+    public synchronized void previous() {
         if (isPlaying) {
             System.out.println("* Играет предыдущая музыка *");
         }
