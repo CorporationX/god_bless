2ed326a628fcb459281e142a11db0249d3baf7c4
Ilia Chuvatkin <ilyachuvatkin@gmail.com>
2023-12-17 13:16:49 +0300

    changed combineFilters method return

--- src/main/java/faang/school/godbless/instagram/FilterProcessor.java
+++ src/main/java/faang/school/godbless/instagram/FilterProcessor.java
@@ -8,6 +8,6 @@ public class FilterProcessor {
     }
 
     Function<Image, Image> combineFilters(Function<Image, Image> filter1, Function<Image, Image> filter2) {
-        return (image) -> applyFilter(applyFilter(image,filter2),filter1);
+        return filter1.compose(filter2);
     }
 }
