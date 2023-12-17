f554f033f26a15668f6cc14a7d3e5c79fba865af
lonovojvladimir <lonovojvladimir@gmail.com>
2023-12-17 23:19:15 +0300

    add class Methods

--- src/main/java/faang/school/godbless/TurnAround.java
+++ src/main/java/faang/school/godbless/TurnAround.java
@@ -1,14 +1,12 @@
 package faang.school.godbless;
 
 public class TurnAround {
-    public int[] sort(int[] array){
-        for(int i=0; i< array.length/2; i++) {
+    public int[] turnAround(int[] array) {
+        for (int i = 0; i < array.length / 2; i++) {
             int swap = array[i];
-            array[i]=array[array.length-i-1];
-            array[array.length-i-1]=swap;
+            array[i] = array[array.length - i - 1];
+            array[array.length - i - 1] = swap;
         }
-
         return array;
     }
-
 }
