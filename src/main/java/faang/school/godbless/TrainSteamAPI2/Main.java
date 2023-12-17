b362aaa64510d6b0b263cf15dbf8969ba3215dce
Marina_Zu <bugsi-47@yandex.ru>
2023-12-17 10:52:21 +0300

    Add task "Потренируем StreamAPI 2"

--- src/main/java/faang/school/godbless/TrainSteamAPI2/Main.java
+++ src/main/java/faang/school/godbless/TrainSteamAPI2/Main.java
@@ -4,15 +4,12 @@ import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
+        // Задание 1
         List<Integer> numbers = Arrays.asList(1, 2, 5, 7, 8, 9, 1, 8, 3, 10, 9, 1, 7, 7, 3);
-
-        /*numbers.stream()
-                .sorted((num1, num2) -> num2 - num1)
-                .forEach(num -> System.out.println(num));*/
-
         Set<List<Integer>> uniquePairs = StreamMethods.uniquePairs(numbers, 10);
         System.out.println("Уникальные значения: " + uniquePairs);
 
+        // Задание 2
         Map<String, String> countryCapital = new HashMap<>();
         countryCapital.put("Russia", "Mosсow");
         countryCapital.put("Austria", "Vienna");
@@ -23,10 +20,12 @@ public class Main {
         List<String> capitalsSort = StreamMethods.capitalsSort(countryCapital);
         System.out.println(capitalsSort);
 
+        // Задание 3
         List<String> words = Arrays.asList("pineapple", "banana", "orange", "pear", "avocado", "potato");
         List<String> sortStringsByLength = StreamMethods.sortStringsByLength(words, 'p');
         System.out.println(sortStringsByLength);
 
+        // Задание 4
         Map<String, List<String>> friends = new HashMap<>();
         friends.put("Alice", Arrays.asList("Daniel", "Join", "Emma"));
         friends.put("Jack", Arrays.asList("Bill", "Join", "Eva"));
@@ -36,6 +35,7 @@ public class Main {
         Set<List<String>> friendsFriends = StreamMethods.friendsFriends(friends);
         System.out.println("Друзья друзей: " + friendsFriends);
 
+        // Задание 5
         List<Employee> employees = new ArrayList<>();
         employees.add(new Employee("Сергей", 40000, "отдел работ"));
         employees.add(new Employee("Алёна", 50000, "отдел работ"));
@@ -45,5 +45,23 @@ public class Main {
 
         Map<String, Double> averageDepartmentSalary = StreamMethods.averageDepartmentSalary(employees);
         System.out.println(averageDepartmentSalary);
+
+        // Задание 6
+        List<String> strings = Arrays.asList("hello", "house", "home", "pineapple", "banana", "orange", "ho");
+        List<Character> characters = List.of('h', 'e', 'l', 'o', 'm');
+        List<String> sortStringsOfLetters = StreamMethods.sortStringsOfLetters(strings, characters);
+        System.out.println(sortStringsOfLetters);
+
+        // Задание 7
+        List<String> convertsIntToBinary = StreamMethods.convertsIntToBinary(numbers);
+        System.out.println("Задача 7: " + convertsIntToBinary);
+
+        // Задание 8
+        StreamMethods.searchIntPalindrome(1, 150).forEach(System.out::println);
+
+        // Задание  9
+        String stringWithPalindrom = "Madam has moved to the next level";
+        List<String> searchStringPalindrome = StreamMethods.searchStringPalindrome(stringWithPalindrom);
+        System.out.println("Слова-палиндромы: " + searchStringPalindrome);
     }
 }
--- src/main/java/faang/school/godbless/TrainSteamAPI2/StreamMethods.java
+++ src/main/java/faang/school/godbless/TrainSteamAPI2/StreamMethods.java
@@ -4,12 +4,13 @@ import lombok.experimental.UtilityClass;
 
 import java.util.*;
 import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 
 @UtilityClass
 public class StreamMethods {
 
-    //Найдите все уникальные пары чисел, сумма которых равна заданному числу
+    // 1. Найдите все уникальные пары чисел, сумма которых равна заданному числу
     public static Set<List<Integer>> uniquePairs(List<Integer> numbers, int sum) {
         Set<List<Integer>> pair = numbers.stream()
                 .flatMap(i -> numbers.stream()
@@ -20,7 +21,7 @@ public class StreamMethods {
     }
 
 
-    //На вход получаем мапу с названиями стран и их столицами.
+    // 2. На вход получаем мапу с названиями стран и их столицами.
     // Отсортируйте страны по алфавиту, а затем выведите названия их столиц в виде списка.
     public List<String> capitalsSort(Map<String, String> country) {
         List<String> entryStream = country.entrySet().stream()
@@ -33,18 +34,18 @@ public class StreamMethods {
     }
 
 
-    //Получаем список строк и букву в виде char.
+    // 3. Получаем список строк и букву в виде char.
     // Отфильтруйте строки, которые начинаются с заданной буквы,
     // и отсортируйте их по длине в порядке возрастания, и верните список этих строк.
     public List<String> sortStringsByLength(List<String> strings, Character character) {
         List<String> sortStrings = strings.stream()
                 .filter(string -> string.startsWith(character.toString()))
-                .sorted()
+                .sorted(Comparator.comparingInt(String::length))
                 .toList();
         return sortStrings;
     }
 
-    //Дана мапа, где ключами являются имена людей, а значениями — списки их друзей.
+    // 4. Дана мапа, где ключами являются имена людей, а значениями — списки их друзей.
     // Найдите все пары людей, которые не являются друзьями, но у них есть общие друзья
     public Set<List<String>> friendsFriends(Map<String, List<String>> friends) {
         Set<List<String>> pairFriends = friends.entrySet().stream()
@@ -63,16 +64,62 @@ public class StreamMethods {
         return pairFriends;
     }
 
-    //Получаем список объектов класса Employee, у каждого из которых есть имя, зарплата и отдел.
+    // 5. Получаем список объектов класса Employee, у каждого из которых есть имя, зарплата и отдел.
     // Найдите среднюю зарплату для каждого отдела.
     // Должна получится map с именем отдела и средней зарплатой.
-        public Map<String, Double> averageDepartmentSalary(List<Employee> employees){
-            return  employees.stream()
-                    .collect(Collectors.groupingBy(Employee::getDepartment,
-                            Collectors.averagingDouble(Employee::getSalary)));
+    public Map<String, Double> averageDepartmentSalary(List<Employee> employees) {
+        return employees.stream()
+                .collect(Collectors.groupingBy(Employee::getDepartment,
+                        Collectors.averagingDouble(Employee::getSalary)));
 
-        }
+    }
 
+    // 6. Дан список строк. Отфильтруйте строки, которые содержат только буквы заданного алфавита,
+    // и отсортируйте их в порядке возрастания длины строк.
+    public List<String> sortStringsOfLetters(List<String> strings, List<Character> characters) {
+        return strings.stream()
+                .filter(string -> string.toLowerCase().chars().allMatch(ch -> characters.contains((char) ch)))
+                .sorted(Comparator.comparingInt(String::length))
+                .toList();
+    }
+
+    // 7. Написать метод, который преобразует список целых чисел в список строк,
+    // где каждое число записано в двоичном виде.
+
+    public List<String> convertsIntToBinary(List<Integer> integers) {
+        return integers.stream()
+                .map((Integer::toBinaryString))
+                .toList();
+    }
+
+    // 8. Написать метод, который найдет все числа-палиндромы
+    // (читающиеся одинаково слева направо и справа налево) в заданном диапазоне.
+    // На вход получаем число для начала диапазона и число для второй границы диапазона.
+    public IntStream searchIntPalindrome(int start, int finish) {
+        return IntStream.rangeClosed(start, finish)
+                .filter(StreamMethods::palindromInt);
+    }
+
+    private boolean palindromInt(int num) {
+        if (num > 10)
+            return String.valueOf(num).equals(new StringBuilder(String.valueOf(num)).reverse().toString());
+        else
+            return false;
+    }
+
+    // 9. Написать метод, который найдёт все подстроки в строке, которые являются палиндромами.
+    // На вход получаем строку, а вернуть должны список строк.
+    public List<String> searchStringPalindrome(String strings) {
+        return Arrays.stream(strings.split(" "))
+                .filter(StreamMethods::palindromString)
+                .toList();
+    }
+
+    private boolean palindromString(String string) {
+        return String.valueOf(string).toLowerCase()
+                .equals(new StringBuilder(String.valueOf(string).toLowerCase()).reverse().toString());
+
+    }
 
 
 }
