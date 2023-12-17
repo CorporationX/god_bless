f498c8a864666fd2db61712430b79d8b60ee91ba
lonovojvladimir <lonovojvladimir@gmail.com>
2023-12-17 22:41:19 +0300

    add class Methods

--- src/main/java/faang/school/godbless/User.java
+++ src/main/java/faang/school/godbless/User.java
@@ -3,22 +3,21 @@ package faang.school.godbless;
 import lombok.Getter;
 import lombok.Setter;
 
-import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
 @Getter
 @Setter
 public class User {
-    private Long id;
+    private long id;
     private String name;
-    private Long age;
+    private long age;
     private List<String> activityList;
 
-    public Map<User, String> findHobbyLovers(List<User> userList, List<String> activityList){
+    public Map<User, String> findHobbyLovers(List<User> userList, List<String> activityList) {
         Map<User, String> map = null;
-        for(User u : userList){
-            if(u.activityList!=null) {
+        for (User u : userList) {
+            if (u.activityList != null) {
                 for (String a : u.activityList) {
                     if (activityList.contains(a)) {
                         map.put(u, a);
