2f052e0d2b8425c8b43de30b1df97c652c34f75f
Rauan Tureyev <rauanture@gmail.com>
2023-12-17 16:50:26 +0600

    use Stream API's methods

---
+++ src/main/java/faang/school/godbless/BJS2_960/Employee.java
@@ -0,0 +1,12 @@
+package faang.school.godbless.BJS2_960;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class Employee {
+    private String name;
+    private int salary;
+    private String department;
+}
