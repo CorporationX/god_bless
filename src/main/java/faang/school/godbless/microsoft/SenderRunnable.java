8a1a2d50c5e081c5aa02893cf2d8ff37f6889ffc
Teo Shtanko <atraxlxxxl@gmail.com>
2023-12-18 01:43:10 +0300

    Отправка писем

---
+++ src/main/java/faang/school/godbless/microsoft/SenderRunnable.java
@@ -0,0 +1,14 @@
+package faang.school.godbless.microsoft;
+
+import lombok.AllArgsConstructor;
+
+@AllArgsConstructor
+public class SenderRunnable implements Runnable {
+    private int startIndex;
+    private int endIndex;
+
+    @Override
+    public void run() {
+        System.out.println("Letters is sent");
+    }
+}
