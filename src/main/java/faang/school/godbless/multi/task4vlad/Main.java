c2c269a1aa1f00ebe330326f3b3f20c59eeed65f
Kamil Gasanov <gasanovkamil516@gmail.com>
2023-12-17 22:33:45 +0300

    fix

--- src/main/java/faang/school/godbless/multi/task4vlad/Main.java
+++ src/main/java/faang/school/godbless/multi/task4vlad/Main.java
@@ -1,8 +1,13 @@
 package faang.school.godbless.multi.task4vlad;
 
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
+
 public class Main {
     public static void main(String[] args) throws InterruptedException {
         VladController controller = new VladController();
+        ExecutorService executorService = Executors.newFixedThreadPool(3);
 
         controller.addVlad(new TamagotchiVlad("Vlad1"));
         controller.addVlad(new TamagotchiVlad("Vlad2"));
@@ -16,20 +21,11 @@ public class Main {
             controller.sleepAll();
         };
 
-        Thread thread1 = new Thread(task);
-        Thread thread2 = new Thread(task);
-        Thread thread3 = new Thread(task);
-
-        thread1.start();
-        thread2.start();
-        thread3.start();
-
-        try {
-            thread1.join();
-            thread2.join();
-            thread3.join();
-        } catch (InterruptedException e) {
-            e.printStackTrace();
+        for (int i = 0; i < 3; i++) {
+            executorService.execute(task);
         }
+
+        executorService.shutdown();
+        executorService.awaitTermination(5, TimeUnit.MINUTES);
     }
 }
