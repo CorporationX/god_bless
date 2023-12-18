0c4b2517ad6c8ac63166f9e743af46104368c2a6
Leilorr <148541216+Leilorr@users.noreply.github.com>
2023-12-18 11:42:44 +0300

    Создайте класс Book, который будет содержать поля title (название книги), author (автор книги) и year (год издания).
    Переопределите методы equals и hashCode в классе Book, чтобы учитывать название книги, автора и год издания при сравнении книг и расчёте их хэш-кода.
    
    В классе Main, создайте HashMap с ключом типа Book и значением типа String (местонахождение книги в библиотеке, например, номер полки).
    
    Добавьте информацию о нескольких книгах в ваш HashMap.
    
    Реализуйте методы для следующих действий: добавление новой книги и её местонахождения в библиотеке; удаление книги по её названию, автору и году издания; поиск книги и вывода информации о её местонахождении в библиотеке, используя название книги, автора и год издания; вывод списка всех книг и их местонахождения в библиотеке.

--- src/main/java/faang/school/godbless/Book.java
+++ src/main/java/faang/school/godbless/Book.java
@@ -29,8 +29,20 @@ public class Book {
         return Objects.equals(title, book.title) && Objects.equals(author, book.author) && year == book.year;
     }
 
+    @Override
+    public String toString() {
+        return "Book{" +
+                "title='" + title + '\'' +
+                ", author='" + author + '\'' +
+                ", year=" + year +
+                '}';
+    }
+}
+
+class Main {
+    private static Map<Book, String> books = new HashMap<>();
+
     public static void main(String[] args) {
-        Map<Book, String> books = new HashMap<>();
         Book book1 = new Book("Название книги 1", "Автор книги 1", 2000);
         Book book2 = new Book("Название книги 2", "Автор книги 2", 1990);
         Book book3 = new Book("Название книги 3", "Автор книги 3", 2010);
@@ -38,17 +50,32 @@ public class Book {
         books.put(book1, "Полка 1");
         books.put(book2, "Полка 2");
         books.put(book3, "Полка 3");
-        books.remove(book1);
+    }
 
-        // Поиск книги
+    public static void addBook(Book book, String place) {
+        books.put(book, place);
+    }
+
+    public static void removeBook(String title, String author, int year) {
+        Book book = new Book(title, author, year);
+        if (books.containsKey(book)) {
+            books.remove(book);
+            System.out.println("Книга удалена");
+        } else {
+            System.out.println("Ошибка удаления");
+        }
+    }
+
+    public static void searchBook(String title, String author, int year) {
         Book searchBook = new Book("Название книги 2", "Автор книги 2", 1990);
         String location = books.get(searchBook);
         System.out.println("Местонахождение книги: " + location);
+    }
 
-        // Вывод списка всех книг
+    public static void infoBooks() {
         for (Map.Entry<Book, String> entry : books.entrySet()) {
-            System.out.println(entry.getKey().getTitle() +
-                    " - Местонахождение: " + entry.getValue());
+            System.out.println(entry.getKey().getTitle() + " - Местонахождение: " + entry.getValue());
         }
     }
 }
+
