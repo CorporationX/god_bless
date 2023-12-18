977b5879942ee335ab3ac4d6506dc1ccbe77bc8a
Kamil Gasanov <gasanovkamil516@gmail.com>
2023-12-18 18:56:13 +0300

    fix

--- src/main/java/faang/school/godbless/stream_api/task3vacancies/JobScraper.java
+++ src/main/java/faang/school/godbless/stream_api/task3vacancies/JobScraper.java
@@ -3,16 +3,13 @@ package faang.school.godbless.stream_api.task3vacancies;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 import java.io.IOException;
-import java.util.List;
-
-import com.fasterxml.jackson.databind.JsonNode;
 
 public class JobScraper {
     private final ObjectMapper objectMapper = new ObjectMapper();
 
     public Job parseJobFromJson(String json) {
         try {
-           return objectMapper.readValue(json, Job.class);
+            return objectMapper.readValue(json, Job.class);
         } catch (IOException e) {
             return null;
         }
