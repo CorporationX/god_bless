e18be08f51dad2009efae07d68f36afced254619
Ilia Chuvatkin <ilyachuvatkin@gmail.com>
2023-12-18 23:52:40 +0300

    They were very nice but extremely poor

---
+++ src/main/java/faang/school/godbless/nicebutpoor/Chore.java
@@ -0,0 +1,21 @@
+package faang.school.godbless.nicebutpoor;
+
+import java.util.Random;
+
+public class Chore implements Runnable {
+    String chore;
+
+    public Chore(String chore) {
+        this.chore = chore;
+    }
+
+    @Override
+    public void run() {
+        System.out.printf("%s - started on: %s \n", chore, Thread.currentThread().getName());
+        try {
+            Thread.sleep(new Random().nextInt(5) * 1000);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
