09485247bec20b1d0dd4d4d31f04bd228e30e744
Alexander Bulgakov <asbulgakov90@gmail.com>
2023-12-19 19:30:01 +0300

    Update task with Exception BJS2-1015

--- src/main/java/faang/school/godbless/alexbulgakoff/functionalinterfaceslambdas/calculator/Main.java
+++ src/main/java/faang/school/godbless/alexbulgakoff/functionalinterfaceslambdas/calculator/Main.java
@@ -1,5 +1,6 @@
 package faang.school.godbless.alexbulgakoff.functionalinterfaceslambdas.calculator;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
@@ -11,9 +12,12 @@ public class Main {
     public static void main(String[] args) {
 
         List<Integer> numbers = Arrays.asList(25, 94);
+        List<Integer> numbersEmpty = new ArrayList<>();
 
         System.out.println(product(numbers));
         System.out.println(sum(numbers));
+
+        System.out.println(sum(numbersEmpty));
 
     }
 
@@ -28,10 +32,16 @@ public class Main {
     }
 
     public static int product(List<Integer> nums) {
+        if (nums.isEmpty()) {
+            throw new IllegalArgumentException("In list not arguments");
+        }
         return calculate(nums, (a, b) -> a * b);
     }
 
     public static int sum(List<Integer> nums) {
+        if (nums.isEmpty()) {
+            throw new IllegalArgumentException("In list not arguments");
+        }
         return calculate(nums, Integer::sum);
     }
 }
