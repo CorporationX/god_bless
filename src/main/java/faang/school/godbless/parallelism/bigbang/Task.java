d493f01302393d76c9bca3515f3bf3144f9b7379
starkoracia <starkoracia@gmail.com>
2023-12-19 22:39:34 +0000

    Finished task

---
+++ src/main/java/faang/school/godbless/parallelism/bigbang/Task.java
@@ -0,0 +1,22 @@
+package faang.school.godbless.parallelism.bigbang;
+
+import lombok.AllArgsConstructor;
+
+@AllArgsConstructor
+public class Task implements Runnable {
+
+    private String name;
+    private String task;
+
+    @Override
+    public void run() {
+        System.out.printf("%s started %s\n", name, task);
+        try {
+            Thread.sleep(1000);
+        } catch (InterruptedException e) {
+            Thread.currentThread().interrupted();
+            System.out.printf("%s interrupted\n", name);
+        }
+    }
+
+}
