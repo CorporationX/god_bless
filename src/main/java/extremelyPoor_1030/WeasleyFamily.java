e7611545ceaf127fff0894f7622c7c05c5389865
Marina Razina <razinamv3@gmail.com>
2023-12-19 15:34:52 +0300

    Create realization for task "They were very nice but extremely poor"

--- src/main/java/extremelyPoor_1030/WeasleyFamily.java
+++ src/main/java/extremelyPoor_1030/WeasleyFamily.java
@@ -5,8 +5,10 @@ import java.util.concurrent.Executors;
 
 public class WeasleyFamily {
     public static void main(String[] args) {
+
         String[] chores = {"Уборка", "Стирка", "Глажка", "Мытье окон"};
         ExecutorService executorService = Executors.newCachedThreadPool();
+
         for (String chore : chores) {
             executorService.execute(new Chore(chore));
         }
