5b8125e843d94b42796bb4dd16b3bbaec92479f8
FD_Shell <dariuskpp@gmail.com>
2023-12-20 18:42:25 +0300

    Create House, Food and Room classes

---
+++ src/main/java/TomAndJerry/House.java
@@ -0,0 +1,98 @@
+package TomAndJerry;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
+
+public class House implements Runnable {
+    private static final List<Food> collectedFood = new ArrayList<>();
+    private final List<Food> foodList = List.of(
+            new Food("Apple"),
+            new Food("Banana"),
+            new Food("Tomato"),
+            new Food("Orange Juice"),
+            new Food("Potato"),
+            new Food("Chicken"),
+            new Food("Potato"),
+            new Food("Cheese"),
+            new Food("Fish"),
+            new Food("Vodka"),
+            new Food("Bread"),
+            new Food("Nuts")
+    );
+    private final List<Room> roomList = List.of(
+            new Room("Bedroom"),
+            new Room("Kitchen"),
+            new Room("BathRoom"),
+            new Room("Hall")
+    );
+
+    public static void main(String[] args) throws InterruptedException {
+        House house = new House();
+        house.fillRoomsWithFood();
+
+        for (Room room : house.roomList) {
+            System.out.println(room.getFoodList());
+        }
+
+        ScheduledExecutorService executorService = Executors.newScheduledThreadPool(5);
+        for (int i = 0; i < 5; i++) {
+            executorService.schedule(house, 30, TimeUnit.SECONDS);
+        }
+        executorService.shutdown();
+
+        while (!executorService.awaitTermination(3, TimeUnit.SECONDS)) {
+        }
+
+        System.out.println(collectedFood);
+        System.out.println("Еда собрана");
+
+
+        for (Room room : house.roomList) {
+            System.out.println(room.getFoodList());
+        }
+    }
+
+    public void fillRoomsWithFood() {
+        int roomIndex = 0;
+        for (Food food : foodList) {
+            if (roomIndex == roomList.size()) {
+                roomIndex = 0;
+            }
+            roomList.get(roomIndex).addFood(food);
+            roomIndex++;
+        }
+    }
+
+    public void coollectFood() {
+        //Найти две комнаты где есть еда
+        List<Room> foundedRooms = new ArrayList<>();
+        int roomCount = 0;
+        for (Room room : roomList) {
+            if (roomCount == 2) {
+                break;
+            }
+            if (room.isAvailable()) {
+                foundedRooms.add(room);
+                room.setAvailable(false);
+                roomCount++;
+            }
+        }
+        //Собрать еду из каждой комнаты
+        //Хотел с помощью stream сделать, но не сообразил как(
+        for (Room foundedRoom : foundedRooms) {
+            for (Food food : foundedRoom.getFoodList()) {
+                collectedFood.add(food);
+            }
+            foundedRoom.getFoodList().clear();
+        }
+    }
+
+    @Override
+    public void run() {
+        coollectFood();
+    }
+}
+
---
+++ src/main/java/TomAndJerry/Room.java
@@ -0,0 +1,23 @@
+package TomAndJerry;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Getter
+@Setter
+public class Room {
+    private final String name;
+    private final List<Food> foodList = new ArrayList<>();
+    private boolean available = true;
+
+    public Room(String name) {
+        this.name = name;
+    }
+
+    public void addFood(Food food) {
+        foodList.add(food);
+    }
+}
