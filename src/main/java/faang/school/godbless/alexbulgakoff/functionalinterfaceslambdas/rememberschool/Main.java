d0693ff017d82328301de6f20d7af14d4c4f51ec
Alexander Bulgakov <asbulgakov90@gmail.com>
2023-12-20 18:32:17 +0300

    Complete task BJS2-1053

--- src/main/java/faang/school/godbless/alexbulgakoff/functionalinterfaceslambdas/rememberschool/Main.java
+++ src/main/java/faang/school/godbless/alexbulgakoff/functionalinterfaceslambdas/rememberschool/Main.java
@@ -1,5 +1,7 @@
 package faang.school.godbless.alexbulgakoff.functionalinterfaceslambdas.rememberschool;
 
+import java.util.function.Function;
+
 /**
  * @author Alexander Bulgakov
  */
@@ -11,16 +13,23 @@ public class Main {
         double b = 15.0;
         double c = 20.0;
 
-
+        System.out.println(triangleArea(a, b, c));
     }
+    //S = √(p * (p — a) * (p — b) * (p — c))
+    //p = (a + b + c) / 2
 
-    // Даны три стороны треугольника, вычислить его площадь
-//    Double triangleArea(double a, double b, double c) throws IllegalArgumentException {
-//        Function<Double, Function<Double, Double>> sum = (x) -> y -> x + y;
-//        Function<Double, Function<Double, Double>> mul = (x) -> y -> x * y;
-//        Function<Double, Function<Double, Double>> sub = (x) -> y -> x - y;
-//        Function<Double, Function<Double, Double>> div = (x) -> y -> x / y;
-//        Function<Double, Double> sqrt = (x) -> Math.sqrt(x);
-//    }
+    public static Double triangleArea(double a, double b, double c) throws IllegalArgumentException {
+        Function<Double, Function<Double, Double>> sum = (x) -> y -> x + y;
+        Function<Double, Function<Double, Double>> mul = (x) -> y -> x * y;
+        Function<Double, Function<Double, Double>> sub = (x) -> y -> x - y;
+        Function<Double, Function<Double, Double>> div = (x) -> y -> x / y;
+        Function<Double, Double> sqrt = (x) -> Math.sqrt(x);
+
+        double p = div.apply(
+                sum.apply(sum.apply(a).apply(b)).apply(c))
+                .apply(2.0);
+
+        return sqrt.apply(mul.apply(mul.apply(mul.apply(p).apply(sub.apply(p).apply(a))).apply(sub.apply(p).apply(b))).apply(sub.apply(p).apply(c)));
+    }
 
 }
