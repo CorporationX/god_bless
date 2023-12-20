7091123bb9928974930599b6a2d0db61d6c52da3
starkoracia <starkoracia@gmail.com>
2023-12-20 18:01:12 +0000

    Finished task

---
+++ src/main/java/faang/school/godbless/parallelism/nicemice/Food.java
@@ -0,0 +1,14 @@
+package faang.school.godbless.parallelism.nicemice;
+
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.ToString;
+
+@AllArgsConstructor
+@Getter
+@EqualsAndHashCode
+@ToString
+public class Food {
+    private String name;
+}
