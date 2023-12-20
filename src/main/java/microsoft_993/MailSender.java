a4a5c43bf7c55cce168ae83ded7a0c25b406f2e1
Marina Razina <razinamv3@gmail.com>
2023-12-20 08:18:24 +0300

    edit from code review

--- src/main/java/microsoft_993/MailSender.java
+++ src/main/java/microsoft_993/MailSender.java
@@ -1,17 +1,25 @@
 package microsoft_993;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class MailSender {
     public static int countThreads = 5;
     public static int countMessage = 1000;
-    public static int countMessageOfThread = countMessage/countThreads;
+    public static int countMessageOfThread = countMessage / countThreads;
 
     public static void main(String[] args) throws InterruptedException {
+
+        List<Thread> threads = new ArrayList<>();
 
         for (int i = 0; i < 5; i++) {
             int start = i * countMessageOfThread;
             int end = start + countMessageOfThread;
             Thread thread = new Thread(() -> new SenderRunnable(start, end).run());
             thread.start();
+            threads.add(thread);
+        }
+        for (Thread thread : threads) {
             thread.join();
         }
         System.out.println("Все сообщения отправлены");
