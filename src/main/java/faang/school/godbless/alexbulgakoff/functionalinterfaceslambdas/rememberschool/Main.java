44bb378b278165015528e1c882d471e32e33cdc8
Alexander Bulgakov <asbulgakov90@gmail.com>
2023-12-20 11:28:36 +0300

    Create task BJS2-1053

---
+++ src/main/java/faang/school/godbless/alexbulgakoff/functionalinterfaceslambdas/rememberschool/Main.java
@@ -0,0 +1,26 @@
+package faang.school.godbless.alexbulgakoff.functionalinterfaceslambdas.rememberschool;
+
+/**
+ * @author Alexander Bulgakov
+ */
+
+public class Main {
+
+    public static void main(String[] args) {
+        double a = 10.0;
+        double b = 15.0;
+        double c = 20.0;
+
+
+    }
+
+    // Даны три стороны треугольника, вычислить его площадь
+//    Double triangleArea(double a, double b, double c) throws IllegalArgumentException {
+//        Function<Double, Function<Double, Double>> sum = (x) -> y -> x + y;
+//        Function<Double, Function<Double, Double>> mul = (x) -> y -> x * y;
+//        Function<Double, Function<Double, Double>> sub = (x) -> y -> x - y;
+//        Function<Double, Function<Double, Double>> div = (x) -> y -> x / y;
+//        Function<Double, Double> sqrt = (x) -> Math.sqrt(x);
+//    }
+
+}
