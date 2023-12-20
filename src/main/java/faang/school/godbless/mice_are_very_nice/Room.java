40a4776e47a4826622de8369aef7f27eb8b861c8
Teo Shtanko <atraxlxxxl@gmail.com>
2023-12-20 03:57:44 +0300

    Рабочая версия мышей

---
+++ src/main/java/faang/school/godbless/mice_are_very_nice/Room.java
@@ -0,0 +1,13 @@
+package faang.school.godbless.mice_are_very_nice;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+import java.util.List;
+
+@Data
+@AllArgsConstructor
+public class Room {
+    private List<Food> foods;
+
+}
