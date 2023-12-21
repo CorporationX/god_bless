ae48327d3e652e8ab25548693071c0969c7d3600
Ilia Chuvatkin <ilyachuvatkin@gmail.com>
2023-12-21 14:00:47 +0300

    Changed join

--- src/main/java/faang/school/godbless/microsoft/MailSender.java
+++ src/main/java/faang/school/godbless/microsoft/MailSender.java
@@ -1,23 +1,29 @@
 package faang.school.godbless.microsoft;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class MailSender {
     public static void main(String[] args) throws InterruptedException {
         int totalEmail = 1000;
-        int amountThread = 5;
-        int count = 0;
-
+        int amountThread = 9;
 
         int startIndex = 1;
         int endIndex = totalEmail / amountThread;
-        for (int i = 0; i < amountThread - 1; i++) {
+        List<Thread> threads = new ArrayList<>();
+        for (int i = 0; i < amountThread; i++) {
+            if (i == amountThread - 1) {
+                endIndex = totalEmail;
+            }
             Thread thread = new Thread(new SenderRunnable(new StackMail(startIndex, endIndex)));
+            threads.add(thread);
             startIndex = endIndex + 1;
             endIndex = totalEmail / amountThread + endIndex;
             thread.start();
         }
-        Thread thread = new Thread(new SenderRunnable(new StackMail(startIndex, totalEmail)));
-        thread.start();
-        thread.join();
+        for (Thread thread : threads) {
+            thread.join();
+        }
         System.out.println("All emails were sent!");
     }
 }
