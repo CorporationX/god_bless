95459a6934405fcb3367405fd73475779bc72fdc
Nikita <nikita.kopot@yandex.ru>
2023-12-22 23:53:09 +0300

    BJS2-1118

---
+++ src/main/java/faang/school/godbless/error/handler/Main.java
@@ -0,0 +1,16 @@
+package faang.school.godbless.error.handler;
+
+public class Main {
+    public static void main(String[] args) {
+        ErrorHandler.withErrorHandling(
+                () -> {
+                    String result = "resolved";
+                    System.out.println("method called");
+                    return result;
+                },
+                (e) -> {
+                    System.out.println("An error occured");
+                    return "rejected";
+                });
+    }
+}
