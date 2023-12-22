df183811fc8aeb4fbf418b0e8cf9592c34ceb447
Edisolt Khatsiev <Edisolt Khatsiev>
2023-12-22 20:31:02 +0300

    Вы работаете в Microsoft!

--- src/main/java/faang/school/godbless/BJS2_1087/MailSender.java
+++ src/main/java/faang/school/godbless/BJS2_1087/MailSender.java
@@ -1,9 +1,14 @@
 package faang.school.godbless.BJS2_1087;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class MailSender {
     public static void main(String[] args) {
         int totalMessages = 1000;
         int messagesPerThread = 200;
+
+        List<Thread> threads = new ArrayList<>();
 
         for (int i = 0; i < totalMessages; i += messagesPerThread) {
             int startIndex = i + 1;
@@ -11,15 +16,18 @@ public class MailSender {
 
             SenderRunnable senderRunnable = new SenderRunnable(startIndex, endIndex);
             Thread thread = new Thread(senderRunnable);
-
             thread.start();
-
-            try {
-                thread.join();
-            } catch (InterruptedException e) {
-                throw new RuntimeException(e);
-            }
+            threads.add(thread);
         }
+
+        try {
+            for (Thread thread : threads) {
+                thread.join();
+            }
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+
         System.out.println("Все сообщения отправлены.");
     }
 }
