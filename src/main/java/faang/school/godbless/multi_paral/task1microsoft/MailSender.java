07e37afc50ebd08095882ee33263bf1fcbebb41d
Kamil Gasanov <gasanovkamil516@gmail.com>
2023-12-23 01:20:02 +0300

    fix

--- src/main/java/faang/school/godbless/multi_paral/task1microsoft/MailSender.java
+++ src/main/java/faang/school/godbless/multi_paral/task1microsoft/MailSender.java
@@ -1,46 +1,46 @@
 package faang.school.godbless.multi_paral.task1microsoft;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
-import java.util.concurrent.TimeUnit;
 
 public class MailSender {
-    public static void main(String[] args) throws InterruptedException {
+    public static void main(String[] args) {
         //Как рекомендовал делать Влад
         int messagesCount = 1000;
         int threadsCount = 5;
-        int messageThread = messagesCount / threadsCount;
+        int messagePerThread = messagesCount / threadsCount;
         ExecutorService executorService = Executors.newFixedThreadPool(5);
 
         for (int i = 0; i < threadsCount; i++) {
-            int startIndex = i * messageThread + 1;
-            int endIndex = (i + 1) * messageThread + 1;
+            int startIndex = i * messagePerThread + 1;
+            int endIndex = (i + 1) * messagePerThread + 1;
             executorService.execute(new SenderRunnable(startIndex, endIndex));
         }
 
         executorService.shutdown();
-        executorService.awaitTermination(5, TimeUnit.MINUTES);
 
         System.out.println("Все сообщения отправлены\n\n\n");
 
         // По условию
-        Thread thread1 = new Thread(new SenderRunnable(1, 201));
-        Thread thread2 = new Thread(new SenderRunnable(201, 401));
-        Thread thread3 = new Thread(new SenderRunnable(401, 601));
-        Thread thread4 = new Thread(new SenderRunnable(601, 801));
-        Thread thread5 = new Thread(new SenderRunnable(801, 1001));
+        List<Thread> threads = new ArrayList<>();
 
-        thread1.start();
-        thread2.start();
-        thread3.start();
-        thread4.start();
-        thread5.start();
+        for (int i = 0; i < threadsCount; i++) {
+            int startIndex = i * messagePerThread + 1;
+            int endIndex = (i + 1) * messagePerThread + 1;
+            Thread thread = new Thread(new SenderRunnable(startIndex, endIndex));
+            thread.start();
+            threads.add(thread);
+        }
 
-        thread1.join();
-        thread2.join();
-        thread3.join();
-        thread4.join();
-        thread5.join();
+        for (Thread thread : threads) {
+            try {
+                thread.join();
+            } catch (InterruptedException e) {
+                Thread.currentThread().interrupt();
+            }
+        }
 
         System.out.println("Все сообщения отправлены");
     }
