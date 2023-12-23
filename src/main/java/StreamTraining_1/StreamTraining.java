75f6bd74e4878ee792c23cdd024b58fd1db90a02
Emperor_Samedi <mexanik13970@gmail.com>
2023-12-23 16:05:01 +0300

    Fixed

--- src/main/java/StreamTraining_1/StreamTraining.java
+++ src/main/java/StreamTraining_1/StreamTraining.java
@@ -1,48 +1,67 @@
 package StreamTraining_1;
 
+import java.util.Comparator;
 import java.util.List;
+import java.util.Objects;
 import java.util.OptionalDouble;
 import java.util.function.Predicate;
 
 public class StreamTraining {
 
     public static int sumOdd(List<Integer> list) {
-        return list.stream().filter(n -> n % 2 == 0).mapToInt(Integer::intValue).sum();
+        return list.stream()
+                .filter(n -> n % 2 == 0)
+                .mapToInt(Integer::intValue)
+                .sum();
     }
 
     public static int max(List<Integer> list) {
-        return list.stream().mapToInt(Integer::intValue).max().getAsInt();
+        return list.stream()
+                .mapToInt(Integer::intValue)
+                .max()
+                .orElseThrow(()-> new IllegalArgumentException());
     }
 
     public static double average(List<Integer> list) {
-        OptionalDouble average = list.stream().mapToInt(n -> n).average();
-        return average.getAsDouble();
+        return list.stream()
+                .mapToInt(n -> n).average()
+                .orElseThrow(()->new IllegalArgumentException());
     }
 
-    public static int countString(List<String> list, char symbol) {
-        return (int) list.stream().filter(n -> n.charAt(0) == symbol).count();
+    public static long countString(List<String> list, char symbol) {
+        return (int) list.stream()
+                .filter(n -> n.startsWith(String.valueOf(symbol)))
+                .count();
     }
 
     public static List<String> stringFilter(List<String> list, String string) {
-        return list.stream().filter(n -> n.contains(string)).toList();
+        return list.stream()
+                .filter(n -> n.contains(string))
+                .toList();
     }
 
     public static List<String> stringSorter(List<String> list) {
-        return list.stream().sorted((a, b) -> {
-            if (a.length() > b.length()) return -1;
-            else return 1;
-        }).toList();
+        return list.stream()
+                .sorted(Comparator.comparing(String::length))
+                .toList();
     }
 
-    public static boolean check(List<String> list, Predicate<String> predicate) {
-        return list.stream().allMatch(predicate);
+    public static boolean check(List<String> list, Predicate<? super String> predicate) {
+        return list.stream()
+                .allMatch(predicate);
     }
 
     public static int min(List<Integer> list, int border) {
-        return list.stream().filter(n -> n > border).mapToInt(Integer::intValue).min().getAsInt();
+        return list.stream()
+                .filter(n -> n > border)
+                .mapToInt(Integer::intValue)
+                .min()
+                .orElseThrow(()->new IllegalArgumentException());
     }
 
     public static List<Integer> stringForLength(List<String> list) {
-        return list.stream().map(String::length).toList();
+        return list.stream()
+                .map(String::length)
+                .toList();
     }
 }
