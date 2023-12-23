cb1676f2f95b2e23a11dc40541857657ced1b21c
Marina_Zu <bugsi-47@yandex.ru>
2023-12-23 22:24:40 +0300

    Add task "Google Photo sync"

---
+++ src/main/java/faang/school/godbless/GooglePhotoSync/GooglePhoto.java
@@ -0,0 +1,12 @@
+package faang.school.godbless.GooglePhotoSync;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@AllArgsConstructor
+@Getter
+public class GooglePhoto {
+    private String name;
+    private String photoPath;
+
+}
