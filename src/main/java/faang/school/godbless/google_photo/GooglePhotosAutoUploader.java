695a6f84adcf3f11c97b1707440d4c5d007daaa7
Rikaf <fakir207@mail.ru>
2023-12-23 13:40:57 +0500

    Done google photo

--- src/main/java/faang/school/godbless/google_photo/GooglePhotosAutoUploader.java
+++ src/main/java/faang/school/godbless/google_photo/GooglePhotosAutoUploader.java
@@ -10,31 +10,6 @@ import java.util.concurrent.TimeUnit;
 
 @Data
 public class GooglePhotosAutoUploader {
-    public static void main(String[] args) throws InterruptedException {
-        ExecutorService executorService = Executors.newFixedThreadPool(2);
-        GooglePhotosAutoUploader googlePhotosAutoUploader = new GooglePhotosAutoUploader();
-
-        String photoPath = "www.google.com/phot/urheq123ijf";
-
-        List<String> photosToUpload = List.of("1.png", "2.png", "3.png", "4.png", "5.png", "6png",
-                "7.png", "8.png", "9.png", "10.png", "11.png", "12.png", "13.png", "14.png", "15.png");
-
-        executorService.submit(() -> googlePhotosAutoUploader.onNewPhotoAdded(photoPath));
-
-        executorService.submit(() -> {
-            try {
-                googlePhotosAutoUploader.startAutoUpload(photosToUpload);
-            } catch (InterruptedException e) {
-                throw new RuntimeException(e);
-            }
-        });
-
-        if (!executorService.awaitTermination(2, TimeUnit.SECONDS)) {
-            executorService.shutdown();
-        }
-    }
-
-
     private final Object lock = new Object();
     private List<String> photosToUpload = new ArrayList<>();
 
@@ -64,6 +39,30 @@ public class GooglePhotosAutoUploader {
             photosToUpload.add(photoPath);
             System.out.println("Фото " + photoPath + " успешно добавлено в список ...");
             lock.notify();
+        }
+    }
+
+    public static void main(String[] args) throws InterruptedException {
+        ExecutorService executorService = Executors.newFixedThreadPool(2);
+        GooglePhotosAutoUploader googlePhotosAutoUploader = new GooglePhotosAutoUploader();
+
+        String photoPath = "www.google.com/phot/urheq123ijf";
+
+        List<String> photosToUpload = List.of("1.png", "2.png", "3.png", "4.png", "5.png", "6png",
+                "7.png", "8.png", "9.png", "10.png", "11.png", "12.png", "13.png", "14.png", "15.png");
+
+        executorService.submit(() -> googlePhotosAutoUploader.onNewPhotoAdded(photoPath));
+
+        executorService.submit(() -> {
+            try {
+                googlePhotosAutoUploader.startAutoUpload(photosToUpload);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+        });
+
+        if (!executorService.awaitTermination(2, TimeUnit.SECONDS)) {
+            executorService.shutdown();
         }
     }
 }
