cbffd68813e9f20102b7d07d836133380c5afa90
Kamil Gasanov <gasanovkamil516@gmail.com>
2023-12-24 18:37:53 +0300

    finished

---
+++ src/main/java/faang/school/godbless/multi_asyn/task1asyn_future/Main.java
@@ -0,0 +1,27 @@
+package faang.school.godbless.multi_asyn.task1asyn_future;
+
+import java.util.concurrent.*;
+
+public class Main {
+    public static void main(String[] args) {
+        try {
+            doAll();
+        } catch (ExecutionException | InterruptedException e) {
+            System.out.println("Платеж прерван");
+            Thread.currentThread().interrupt();
+            e.printStackTrace();
+        }
+    }
+
+    public static void doAll() throws ExecutionException, InterruptedException {
+        MasterCardService masterCardService = new MasterCardService();
+        ExecutorService executorService = Executors.newFixedThreadPool(1);
+
+        Future<Integer> resultPayment = executorService.submit(masterCardService::collectPayment);
+        CompletableFuture<Integer> resultAnalytics = CompletableFuture.supplyAsync(masterCardService::sendAnalytics);
+
+        // В условии написно иcпользовать join, но как я понял особых различий нет
+        System.out.println("Время аналитики: " + resultAnalytics.get());
+        System.out.println("Время оплаты: " + resultPayment.get());
+    }
+}
