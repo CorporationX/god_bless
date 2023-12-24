438aee9337823f1c59a49145138626ef79c11fb2
javaDevArtem <artemjdev@gmail.com>
2023-12-24 18:40:17 -0500

    BJS2-1142 Fixes

--- src/main/java/functions/meta/NotificationManager.java
+++ src/main/java/functions/meta/NotificationManager.java
@@ -7,15 +7,15 @@ import java.util.function.Predicate;
 
 public class NotificationManager {
 
-    private final Map<String, Consumer<Notification>> notificationMap = new HashMap<>();
+    private final Map<String, Consumer<Notification>> handlers = new HashMap<>();
 
-    public void registerHandler(String id, Consumer<Notification> consumer) {
-        notificationMap.put(id, consumer);
+    public void registerHandler(String id, Consumer<Notification> handler) {
+        handlers.put(id, handler);
     }
 
     public void sendNotification(Notification notification, Predicate<Notification> predicate) {
         if (predicate.test(notification)) {
-            notificationMap.get(notification.getType()).accept(notification);
+            this.handlers.get(notification.getType()).accept(notification);
         } else {
             System.out.println("Invalid ID type");
         }
