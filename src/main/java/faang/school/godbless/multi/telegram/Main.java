dc0b6d846c25bfad0bfc1228862db10cf911ce09
Kamil Gasanov <gasanovkamil516@gmail.com>
2023-12-24 04:55:18 +0300

    fix

--- src/main/java/faang/school/godbless/multi/telegram/Main.java
+++ src/main/java/faang/school/godbless/multi/telegram/Main.java
@@ -1,6 +1,5 @@
 package faang.school.godbless.multi.telegram;
 
-
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
--- src/main/java/faang/school/godbless/multi/telegram/TelegramBot.java
+++ src/main/java/faang/school/godbless/multi/telegram/TelegramBot.java
@@ -4,6 +4,7 @@ public class TelegramBot {
     private static final int REQUEST_LIMIT = 5;
     private int requestCounter;
     private long lastRequestTime;
+    private static final long ONE_SECOND = 1000;
 
     public TelegramBot() {
         requestCounter = 0;
@@ -13,12 +14,13 @@ public class TelegramBot {
     public synchronized void sendMessage(String message) {
         lastRequestTime = System.currentTimeMillis();
 
-        if (getTimeDifference() < 1000) {
+
+        if (getTimeDifference() < ONE_SECOND) {
             requestCounter++;
             if (requestCounter > REQUEST_LIMIT) {
                 try {
                     System.out.println("------------- Отправка ограничена -----------------\n");
-                    Thread.sleep(1000 - getTimeDifference());
+                    Thread.sleep(ONE_SECOND - getTimeDifference());
                     requestCounter = 1;
                 } catch (InterruptedException e) {
                     Thread.currentThread().interrupt();
