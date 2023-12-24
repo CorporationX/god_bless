d7e6d50dcde7493dfb3ef27f9de86deb5f4bfaeb
Kamil Gasanov <gasanovkamil516@gmail.com>
2023-12-24 04:48:33 +0300

    fix

--- src/main/java/faang/school/godbless/multi/telegram/TelegramBot.java
+++ src/main/java/faang/school/godbless/multi/telegram/TelegramBot.java
@@ -1,30 +1,25 @@
 package faang.school.godbless.multi.telegram;
 
-import java.util.ArrayList;
-import java.util.List;
-
 public class TelegramBot {
     private static final int REQUEST_LIMIT = 5;
     private int requestCounter;
     private long lastRequestTime;
 
     public TelegramBot() {
-        this.requestCounter = 0;
-        this.lastRequestTime = System.currentTimeMillis();
+        requestCounter = 0;
+        lastRequestTime = System.currentTimeMillis();
     }
 
     public synchronized void sendMessage(String message) {
-        long currentTime = System.currentTimeMillis();
-        long timeDiff = currentTime - lastRequestTime;
+        lastRequestTime = System.currentTimeMillis();
 
-        if (timeDiff < 1000) {
+        if (getTimeDifference() < 1000) {
             requestCounter++;
             if (requestCounter > REQUEST_LIMIT) {
                 try {
-                    Thread.sleep(1000 - timeDiff);
-                    lastRequestTime = System.currentTimeMillis();
-                    requestCounter = 1;
                     System.out.println("------------- Отправка ограничена -----------------\n");
+                    Thread.sleep(1000 - getTimeDifference());
+                    requestCounter = 1;
                 } catch (InterruptedException e) {
                     Thread.currentThread().interrupt();
                     System.out.println("Отправка прервана");
@@ -32,35 +27,17 @@ public class TelegramBot {
             }
         } else {
             requestCounter = 1;
-            lastRequestTime = currentTime;
             System.out.println("--------------------------------------------------");
         }
 
         sendToTelegramAPI(message);
     }
 
-    private void sendToTelegramAPI(String message) {
-        System.out.println("Бот ответил на " + message);
+    private long getTimeDifference() {
+        return System.currentTimeMillis() - lastRequestTime;
     }
 
-    public static void main(String[] args) {
-        TelegramBot bot = new TelegramBot();
-        List<Thread> threads = new ArrayList<>();
-
-        for (int i = 0; i < 53; i++) {
-            int finalI = i;
-            Thread thread = new Thread(() -> bot.sendMessage("Сообщение " + finalI));
-            threads.add(thread);
-            thread.start();
-        }
-
-        for (Thread thread : threads) {
-            try {
-                thread.join();
-            } catch (InterruptedException e) {
-                Thread.currentThread().interrupt();
-                System.out.println("Отправка прервана");
-            }
-        }
+    private void sendToTelegramAPI(String message) {
+        System.out.println("Бот ответил на " + message);
     }
 }
