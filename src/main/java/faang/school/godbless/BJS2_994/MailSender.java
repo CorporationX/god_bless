e32f958b6a8f717b8afd833bdd93d4f3c63a3cb7
Rauan Tureyev <rauanture@gmail.com>
2023-12-24 11:18:03 +0600

    use class Thread and implement Functional Interface

--- src/main/java/faang/school/godbless/BJS2_994/MailSender.java
+++ src/main/java/faang/school/godbless/BJS2_994/MailSender.java
@@ -4,14 +4,18 @@ import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
 public class MailSender {
-    public static void main(String[] args) throws InterruptedException {
+    public static void main(String[] args) {
         for (int i = 0; i < 5; i++) {
             int start = i * 200 + 1;
             int end = start + 200;
             SenderRunnable senderRunnable = new SenderRunnable(start, end);
             Thread thread = new Thread(senderRunnable);
             thread.start();
-            thread.join();
+            try {
+                thread.join();
+            } catch (InterruptedException e) {
+                throw new RuntimeException("fault in thread.join");
+            }
         }
         System.out.println("All messages was sended");
     }
