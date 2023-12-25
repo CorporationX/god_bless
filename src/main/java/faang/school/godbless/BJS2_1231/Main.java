4bd0792065aa097112565b2efd0ea727d247799b
Edisolt Khatsiev <Edisolt Khatsiev>
2023-12-26 02:32:18 +0300

    Блокировка Spotify

---
+++ src/main/java/faang/school/godbless/BJS2_1231/Main.java
@@ -0,0 +1,17 @@
+package faang.school.godbless.BJS2_1231;
+
+public class Main {
+    public static void main(String[] args) {
+        Player player = new Player();
+
+        Thread playThread = new Thread(player::play);
+        Thread pauseThread = new Thread(player::pause);
+        Thread skipThread = new Thread(player::skip);
+        Thread previousThread = new Thread(player::previous);
+
+        playThread.start();
+        pauseThread.start();
+        skipThread.start();
+        previousThread.start();
+    }
+}
