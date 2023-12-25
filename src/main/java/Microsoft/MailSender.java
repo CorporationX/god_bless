42c097537686a2596fe51457a9798ba6a31f89e5
FD_Shell <dariuskpp@gmail.com>
2023-12-25 23:35:49 +0300

    Replacement threadpool on Thread

--- src/main/java/Microsoft/MailSender.java
+++ src/main/java/Microsoft/MailSender.java
@@ -1,57 +1,26 @@
 package Microsoft;
 
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
-import java.util.concurrent.TimeUnit;
-
 public class MailSender {
     public static void main(String[] args) throws InterruptedException {
 
-        int numberOfMessages = 1000;
-
         int poolSize = 200;
-        int poolCounter = 0;
-        int numberOfThreads = 5;
+        int numberOfThread = 5;
         int startIndex = 0;
         int endIndex;
 
 
-        ExecutorService executorService = Executors.newFixedThreadPool(numberOfThreads);
-        for (int i = 0; i <= numberOfMessages; i++) {
-            //Если пул сообщений == 0
-            if (poolCounter == 0) {
-                //Записываем startIndex
-                startIndex = i;
-            }
-
-
-            //Если counter == 200
-            if (poolCounter == poolSize) {
-                //Записываем endIndex
-                endIndex = i;
-                //Запускаем отправку группы писем
-                executorService.submit(new SenderRunnable(startIndex, endIndex));
-
-                //Сбрасываем counter
-                poolCounter = 0;
-            }
-
-            //Инкрементируем счетчик
-            poolCounter++;
-
-            //Если дошли до конца списка но counter != 200
-            if (i == numberOfMessages) {
-                //Отправляем оставшиеся
-                endIndex = i;
-
-                SenderRunnable senderRunnable = new SenderRunnable(startIndex, endIndex);
-                senderRunnable.run();
-
-            }
-
+        for (int i = 0; i < numberOfThread; i++) {
+            //вычислить индексы
+            endIndex = startIndex + poolSize - 1;
+            //назначить потоку задание с начальным и конечным индексами
+            Thread thread = new Thread(new SenderRunnable(startIndex, endIndex));
+            thread.start();
+            //пересчитать индексы
+            startIndex += poolSize;
+            thread.join();
         }
-        executorService.awaitTermination(2000, TimeUnit.MILLISECONDS);
-        executorService.shutdown();
+
+        System.out.println("Сообщения отправлены");
     }
 }
 
