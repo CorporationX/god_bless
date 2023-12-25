c68e5d5a320a01c10134aaf9ee8e4d40156bc667
Kamil Gasanov <gasanovkamil516@gmail.com>
2023-12-25 05:38:10 +0300

    fix

--- src/main/java/faang/school/godbless/multi/task3fan/Main.java
+++ src/main/java/faang/school/godbless/multi/task3fan/Main.java
@@ -2,6 +2,8 @@ package faang.school.godbless.multi.task3fan;
 
 import java.util.List;
 import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 import java.util.stream.Collectors;
 import java.util.stream.LongStream;
 
@@ -11,11 +13,15 @@ public class Main {
     }
 
     public static Long fanOutFanIn(List<SquareRequest> requests, ResultConsumer resultConsumer) {
+        ExecutorService executorService = Executors.newCachedThreadPool();
+
         List<CompletableFuture<Void>> listSquareFuture = requests.stream()
-                .map(request -> CompletableFuture.runAsync(() -> request.longTimeSquare(resultConsumer)))
+                .map(request -> CompletableFuture.runAsync(() -> request.longTimeSquare(resultConsumer), executorService))
                 .toList();
 
         CompletableFuture.allOf(listSquareFuture.toArray(new CompletableFuture[0])).join();
+
+        executorService.shutdown();
 
         return resultConsumer.add(0L);
     }
