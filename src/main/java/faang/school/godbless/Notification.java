17ba8209b8de05671a9e3a89ccfb6e65e9efe6fa
Leilorr <148541216+Leilorr@users.noreply.github.com>
2023-12-27 01:25:07 +0300

    Требования:
    Создайте класс Notification, который содержит поля type и message;
    
    Создайте класс NotificationManager с методами: registerHandler: принимает идентификатор оповещения и функциональный интерфейс (например, Consumer<Notification>), добавляет пару ключ-значение в Map;
    
    sendNotification: принимает объект типа Notification и вызывает соответствующий обработчик оповещения из Map.
    
    Создайте несколько обработчиков оповещений, используя лямбда-выражения и стандартные функциональные интерфейсы Java;

---
+++ src/main/java/faang/school/godbless/Notification.java
@@ -0,0 +1,11 @@
+package faang.school.godbless;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class Notification {
+    String type;
+    String message;
+}
