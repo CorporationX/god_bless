1627282541aee6f2c32c4ef741ef8de152ba0a28
Alexander Bulgakov <asbulgakov90@gmail.com>
2023-12-27 00:34:24 +0300

    Create BJS2-1226

---
+++ src/main/java/faang/school/godbless/alexbulgakoff/multithreading/parallelism/feedpitergriffin/FoodDeliveryTask.java
@@ -0,0 +1,12 @@
+package faang.school.godbless.alexbulgakoff.multithreading.parallelism.feedpitergriffin;
+
+/**
+ * @author Alexander Bulgakov
+ */
+
+public class FoodDeliveryTask implements Runnable {
+    @Override
+    public void run() {
+        
+    }
+}
