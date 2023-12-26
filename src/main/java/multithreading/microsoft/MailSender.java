1b490f6d272a2cbffe40ee915154969bf861d0b7
javaDevArtem <artemjdev@gmail.com>
2023-12-26 10:10:23 -0500

    BJS2-1225 Review fix

--- src/main/java/multithreading/microsoft/MailSender.java
+++ src/main/java/multithreading/microsoft/MailSender.java
@@ -2,15 +2,15 @@ package multithreading.microsoft;
 
 public class MailSender {
     public static void main(String[] args) {
-
+        Thread thread = null;
         for (int i = 0; i < 5; i++) {
-            Thread thread = new Thread(new SenderRunnable(i * 50, i * 50 + 50));
+            thread = new Thread(new SenderRunnable(i * 50, i * 50 + 50));
             thread.start();
-            try {
-                thread.join();
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
+        }
+        try {
+            thread.join();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
         }
         System.out.println("Success, all letters have been sent");
 
