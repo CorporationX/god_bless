ca7ece513615a5f03cb58e287292a843f6b50005
Kamil Gasanov <gasanovkamil516@gmail.com>
2023-12-26 06:22:12 +0300

    finished

---
+++ src/main/java/faang/school/godbless/multi_asyn/task5alchemistry/Potion.java
@@ -0,0 +1,11 @@
+package faang.school.godbless.multi_asyn.task5alchemistry;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class Potion {
+    private String name;
+    private int requiredIngredients;
+}
