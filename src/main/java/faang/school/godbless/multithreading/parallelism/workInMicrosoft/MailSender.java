8ef8a4ce4ad278f95019622413c47a8cd3591a08
Vlad Meniailov <vlad.meniailov@yandex.ru>
2023-12-27 15:30:23 +0300

    Updated "Work in Microsoft".

--- src/main/java/faang/school/godbless/multithreading/parallelism/workInMicrosoft/MailSender.java
+++ src/main/java/faang/school/godbless/multithreading/parallelism/workInMicrosoft/MailSender.java
@@ -12,8 +12,8 @@ public class MailSender {
         List<Thread> threads = new ArrayList<>();
 
         for (int i = 0; i < THREADS_AMOUNT; i++) {
-            int startIndex = (i == 0) ? i + 1 : i * LETTERS_FOR_ONE_THREAD + 1;
-            int endIndex = (i == 0) ? LETTERS_FOR_ONE_THREAD : (i + 1) * LETTERS_FOR_ONE_THREAD;
+            int startIndex = i * LETTERS_FOR_ONE_THREAD + 1;
+            int endIndex = (i + 1) * LETTERS_FOR_ONE_THREAD;
             Thread thread = new Thread(new SenderRunnable(startIndex, endIndex));
             threads.add(thread);
             threads.get(i).start();
