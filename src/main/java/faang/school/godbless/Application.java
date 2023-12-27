adea23fef1d1ea3a7cf70fc3662079efaf509c3b
Leilorr <148541216+Leilorr@users.noreply.github.com>
2023-12-28 02:30:29 +0300

    Требования:
    Создайте функциональный интерфейс MessageFilter с методом filter, который принимает строку (сообщение) и возвращает boolean (применяется фильтр или нет);
    
    Создайте класс MessageProcessor с методом processMessage;
    
    Метод processMessage должен принимать входящее сообщение и список объектов MessageFilter для обработки сообщения;
    
    Создайте несколько фильтров с помощью лямбда-выражений и добавьте их в список;
    
    Протестируйте вашу систему, обрабатывая несколько входящих сообщений с разными фильтрами.

--- src/main/java/faang/school/godbless/Application.java
+++ src/main/java/faang/school/godbless/Application.java
@@ -1,7 +1,25 @@
 package faang.school.godbless;
 
+import java.util.Arrays;
+import java.util.List;
+
 public class Application {
     public static void main(String... args) {
-        System.out.println("Hello world");
+        MessageProcessor messageProcessor = new MessageProcessor();
+
+// Создание фильтров
+        MessageFilter spamFilter = message -> !message.toLowerCase().contains("спам");
+        MessageFilter lengthFilter = message -> message.length() > 10;
+        MessageFilter emojiFilter = message -> !message.contains("\uD83D\uDE00");
+
+        List<MessageFilter> filters = Arrays.asList(spamFilter, lengthFilter, emojiFilter);
+
+// Обработка сообщений
+        String[] messages = {"Привет!", "Это спам!", "Как дела? \uD83D\uDE00", "Длинное сообщение без спама и эмодзи"};
+
+        for (String message : messages) {
+            boolean isFiltered = messageProcessor.processMessage(message, filters);
+            System.out.println("Сообщение: " + message + " | Пропущено: " + isFiltered);
+        }
     }
 }
---
+++ src/main/java/faang/school/godbless/MessageProcessor.java
@@ -0,0 +1,15 @@
+package faang.school.godbless;
+
+import java.util.List;
+
+public class MessageProcessor {
+    public boolean processMessage(String message, List<MessageFilter> filters){
+        for (MessageFilter filter : filters){
+            if (!filter.filter(message)){
+                return false;
+            }
+
+        }
+        return true;
+    }
+}
