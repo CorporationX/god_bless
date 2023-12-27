ab59c882c9f54ac378b79db91157d12a6c56ccdd
Leilorr <148541216+Leilorr@users.noreply.github.com>
2023-12-27 03:27:11 +0300

    Требования:
    Создайте класс Notification, который содержит поля type и message;
    
    Создайте класс NotificationManager с методами: registerHandler: принимает идентификатор оповещения и функциональный интерфейс (например, Consumer<Notification>), добавляет пару ключ-значение в Map;
    
    sendNotification: принимает объект типа Notification и вызывает соответствующий обработчик оповещения из Map.
    
    Создайте несколько обработчиков оповещений, используя лямбда-выражения и стандартные функциональные интерфейсы Java;

---
+++ src/main/java/faang/school/godbless/FilterProcessor.java
@@ -0,0 +1,17 @@
+package faang.school.godbless;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.function.Function;
+
+public class FilterProcessor {
+
+    public Image applyFilter(Image image, Function<Image, Image> filter){
+        return filter.apply(image);
+
+    }
+    public Function<Image, Image> combineFilters(Function<Image, Image> filter1, Function<Image, Image> filter2){
+       return filter1.andThen(filter2);
+
+    }
+}
