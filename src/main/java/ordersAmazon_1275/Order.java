d0b93e4b7a868b3c580be5950317f6acfeb425dd
Marina Razina <razinamv3@gmail.com>
2023-12-28 10:49:46 +0300

    create realization for task "Собираем заказы в Amazon"

---
+++ src/main/java/ordersAmazon_1275/Order.java
@@ -0,0 +1,11 @@
+package ordersAmazon_1275;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class Order {
+    private String id;
+    private String status;
+}
