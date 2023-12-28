3aad2b61f21c91a965d2b5e64f4aeea4266d1e30
Vlad Meniailov <vlad.meniailov@yandex.ru>
2023-12-28 23:23:08 +0300

    fixed Boss in supercow

--- src/main/java/faang/school/godbless/multithreading/synchronizationPractice/supercow/Boss.java
+++ src/main/java/faang/school/godbless/multithreading/synchronizationPractice/supercow/Boss.java
@@ -4,7 +4,6 @@ import lombok.Getter;
 
 @Getter
 public class Boss {
-    private final Object lock = new Object();
     private final int maxPlayers;
     private int currentPlayers;
 
@@ -12,23 +11,19 @@ public class Boss {
         this.maxPlayers = maxPlayers;
     }
 
-    public void joinBattle(Player player) throws InterruptedException {
-        synchronized (lock) {
-            while (maxPlayers <= currentPlayers) {
-                wait();
-                System.out.println(player.getName() + " waiting free slot..");
-            }
-            currentPlayers++;
-            System.out.println(player.getName() + " joined to battle!");
+    public synchronized void joinBattle(Player player) throws InterruptedException {
+        while (maxPlayers <= currentPlayers) {
+            wait();
+            System.out.println(player.getName() + " waiting free slot..");
         }
+        currentPlayers++;
+        System.out.println(player.getName() + " joined to battle!");
     }
 
-    public void endBattle(Player player) {
-        synchronized (lock) {
-            System.out.println(player.getName() + " fenitto la comedia");
-            currentPlayers--;
-            notifyAll();
-        }
+    public synchronized void endBattle(Player player) {
+        System.out.println(player.getName() + " fenitto la comedia");
+        currentPlayers--;
+        notifyAll();
     }
 
 }
