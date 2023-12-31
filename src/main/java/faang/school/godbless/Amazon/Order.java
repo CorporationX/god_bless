516c1aee59ffeddce73e612cc3be488a5cbf93ae
Teo Shtanko <atraxlxxxl@gmail.com>
2023-12-31 09:13:00 +0300

    заказы амазон

---
+++ src/main/java/faang/school/godbless/Amazon/Order.java
@@ -0,0 +1,11 @@
+package faang.school.godbless.Amazon;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class Order {
+    private String id;
+    private String status;
+}
