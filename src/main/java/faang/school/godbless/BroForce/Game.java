fd411d0b5b679d0b9b0584193185c9281fcc523d
Marina_Zu <bugsi-47@yandex.ru>
2023-12-31 11:20:15 +0300

    Не выходит

--- src/main/java/faang/school/godbless/BroForce/Game.java
+++ src/main/java/faang/school/godbless/BroForce/Game.java
@@ -2,6 +2,8 @@ package faang.school.godbless.BroForce;
 
 import lombok.Getter;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Random;
 
 @Getter
@@ -10,31 +12,33 @@ public class Game {
     private static int lives;      // количество потерянных всеми участниками жизней
     private static final Object lockScore = new Object();
     private static final Object lockLives = new Object();
-    //static List<Player> players = new ArrayList<>();
+    private static int countPlayers = 0;
+    static List<Player> players = new ArrayList<>();
 
-    public void update(Player player) {
-        if (player.isAlive()) {
-            if (player.getLive() > 0) {
-                Random random = new Random();
-                boolean alive = random.nextBoolean();
-                if (alive) {
-                    synchronized (lockScore) {
-                        if (player.isAlive()) {
-                            player.setScore(player.getScore() + 1);
-                            System.out.println("Очки " + player.getName() + ": " + player.getScore());
-                            score++;
-                        }
-                    }
-                } else {
-                    synchronized (lockLives) {
-                        player.setLive(player.getLive() - 1);
-                        System.out.println("Оставшееся количество жизней у игрока " + player.getName() + ": " + player.getLive());
-                        lives++;
-                        if (player.getLive() == 0)
-                            gameOver(player);
+    public boolean update(Player player) {
+        if (player.isAlive() && (players.size() - countPlayers) > 1) {
+            Random random = new Random();
+            boolean action = random.nextBoolean();
+            if (action) {
+                synchronized (lockScore) {
+                    if (player.isAlive()) {
+                        player.setScore(player.getScore() + 1);
+                        System.out.println("Очки " + player.getName() + ": " + player.getScore());
+                        score++;
                     }
                 }
-            }
+            } else {
+                synchronized (lockLives) {
+                    player.setLive(player.getLive() - 1);
+                    System.out.println("Оставшееся количество жизней у игрока " + player.getName() + ": " + player.getLive());
+                    lives++;
+                    if (player.getLive() == 0)
+                        gameOver(player);
+                }
+            } return true;
+        } else {
+            System.out.println(player.getName() + " победил");
+            return false;
         }
     }
 
@@ -43,6 +47,7 @@ public class Game {
         System.out.println("Потерянные жизни: " + lives);
         System.out.println("Очки за игру: " + score);
         player.setAlive(false);
+        countPlayers++;
     }
 }
 
--- src/main/java/faang/school/godbless/BroForce/Main.java
+++ src/main/java/faang/school/godbless/BroForce/Main.java
@@ -1,25 +1,27 @@
 package faang.school.godbless.BroForce;
 
-import java.util.ArrayList;
-import java.util.List;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.TimeUnit;
 
+import static faang.school.godbless.BroForce.Game.players;
+
 public class Main {
     public static void main(String[] args) {
         Game game = new Game();
-        List<Player> players = new ArrayList<>();
+        //List<Player> players = new ArrayList<>();
         players.add(new Player("Player1", 5));
         players.add(new Player("Player2", 5));
         players.add(new Player("Player3", 3));
 
         ExecutorService executorService = Executors.newFixedThreadPool(2);
         for (Player player : players) {
-            executorService.submit(() -> {
-                while (player.getLive()>0)
+            while (game.update(player)) {
+                executorService.submit(() -> {
+                    while (player.getLive() > 0)
                         game.update(player);
-                    });
+                });
+            }
         }
         executorService.shutdown();
         try {
