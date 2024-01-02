2fcad7ecc27cb566136151c3a73bf4235b8de269
FD_Shell <dariuskpp@gmail.com>
2024-01-02 22:01:56 +0300

    Complete main method

--- src/main/java/TheElderScrolls/Main.java
+++ src/main/java/TheElderScrolls/Main.java
@@ -1,36 +1,44 @@
 package TheElderScrolls;
 
-import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.ExecutionException;
-import java.util.concurrent.atomic.AtomicInteger;
 
 public class Main {
-    private static AtomicInteger totalIngredients = new AtomicInteger(0);
-    private static List<Potion> potions = List.of(
+    private static int totalIngredients = 0;
+    private static final List<Potion> potions = List.of(
             new Potion("Poison", 5),
             new Potion("Elixir of Vitality", 3),
             new Potion("Tonic of Clarity", 8),
             new Potion("Remedy of Restoration", 1),
             new Potion("Essence of Luck", 4)
-            );
+    );
 
-    public static void main(String[] args) throws InterruptedException, ExecutionException {
+    public static void main(String[] args) {
         //создать лист фучеров
-        List<CompletableFuture<Integer>> futures = new ArrayList<>();
+        List<CompletableFuture<Integer>> futures;
 
         //вызвать supplyAsync для каждого элемента
         futures = potions.stream().map(potion -> CompletableFuture.supplyAsync(() -> gatherIngredients(potion)))
                 .toList();
 
         //добавить все в allOf
-        CompletableFuture<Void> finalFuture = CompletableFuture.allOf(futures.toArray(new CompletableFuture[0]));
+        CompletableFuture<Void> allFutures = CompletableFuture.allOf(futures.toArray(new CompletableFuture[0]));
 
-        //добавить все полученные результаты в totalIngredients
+        //Дождаться выполнения всех задач
+        allFutures.join();
 
-        }
+        //сложить все полученные результаты в totalIngredients
+        totalIngredients = futures.stream().map(future -> {
+            try {
+                return future.get();
+            } catch (InterruptedException | ExecutionException e) {
+                throw new RuntimeException(e);
+            }
+        }).reduce(0, Integer::sum);
 
+        System.out.println(totalIngredients);
+    }
 
 
     public static int gatherIngredients(Potion potion) {
