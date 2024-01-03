f52c34c9c0697c98c9d5556da080bb56ec1d0954
Nariman <salikhov.nariman1994@gmail.com>
2024-01-03 17:10:56 +0600

    java module: Multithreading async, java task: Collecting orders on Amazon

---
+++ src/main/java/faang/school/godbless/multithreading_async/collecting_orders_on_amazon/OrderProcessor.java
@@ -0,0 +1,30 @@
+package faang.school.godbless.multithreading_async.collecting_orders_on_amazon;
+
+import lombok.Data;
+
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.atomic.AtomicInteger;
+
+@Data
+public class OrderProcessor {
+    private AtomicInteger totalProcessedOrders = new AtomicInteger();
+
+    private ExecutorService executorService = Executors.newFixedThreadPool(10);
+
+    public CompletableFuture<Order> processOrder(Order order) {
+
+        return CompletableFuture.supplyAsync(() -> {
+            System.out.println(Thread.currentThread().getName());
+            try {
+                Thread.sleep(3000);
+                order.setStatus(1);// 1 - Обработано
+                totalProcessedOrders.incrementAndGet();
+                return order;
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+        }, executorService);
+    }
+}
