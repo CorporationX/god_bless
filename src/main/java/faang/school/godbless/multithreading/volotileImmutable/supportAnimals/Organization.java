bd1941337b44720d4ae991c662eda5ba96d160d7
Vlad Meniailov <vlad.meniailov@yandex.ru>
2024-01-03 21:23:35 +0300

    Fixed AtomicInteger.

--- src/main/java/faang/school/godbless/multithreading/volotileImmutable/supportAnimals/Organization.java
+++ src/main/java/faang/school/godbless/multithreading/volotileImmutable/supportAnimals/Organization.java
@@ -7,7 +7,7 @@ import java.util.concurrent.atomic.AtomicInteger;
 
 @Getter
 public class Organization {
-    private volatile AtomicInteger generalSum = new AtomicInteger();
+    private AtomicInteger generalSum = new AtomicInteger();
 
     public CompletableFuture<Donation> addDonation(Donation donation) {
 
