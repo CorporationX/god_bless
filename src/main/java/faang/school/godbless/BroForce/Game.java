165b777a238b219d300a1cc0e741d27f6521e8f3
Marina_Zu <bugsi-47@yandex.ru>
2024-01-05 18:17:29 +0300

    Add task "Bro Force!"

--- src/main/java/faang/school/godbless/BroForce/Game.java
+++ src/main/java/faang/school/godbless/BroForce/Game.java
@@ -13,40 +13,47 @@ public class Game {
     private static final Object lockScore = new Object();
     private static final Object lockLives = new Object();
     private static int countPlayers = 0;
+    private static boolean gameContinues = true;
     static List<Player> players = new ArrayList<>();
 
-    public boolean update(Player player) {
-        if (player.isAlive() && (players.size() - countPlayers) > 1) {
-            Random random = new Random();
-            boolean action = random.nextBoolean();
-            if (action) {
-                synchronized (lockScore) {
-                    if (player.isAlive()) {
-                        player.setScore(player.getScore() + 1);
-                        System.out.println("Очки " + player.getName() + ": " + player.getScore());
-                        score++;
+
+    public void update() {
+        while (gameContinues) {
+            Player player = players.get(new Random().nextInt(players.size()));
+            if (player.isAlive() && (players.size() - countPlayers) > 1) {
+                Random random = new Random();
+                boolean action = random.nextBoolean();
+                if (action) {
+                    synchronized (lockScore) {
+                        if (player.isAlive()) {
+                            player.setScore(player.getScore() + 1);
+                            System.out.println("Очки " + player.getName() + ": " + player.getScore());
+                            score++;
+                        }
+                    }
+                } else {
+                    // не могу понять, почему после gameOver() все равно еще один раз заходит именно в этот поток...
+                    synchronized (lockLives) {
+                        player.setLive(player.getLive() - 1);
+                        System.out.println("Оставшееся количество жизней у игрока " + player.getName() + ": " + player.getLive());
+                        lives++;
+                        if (player.getLive() == 0) {
+                            gameOver(player);
+                        }
                     }
                 }
-            } else {
-                synchronized (lockLives) {
-                    player.setLive(player.getLive() - 1);
-                    System.out.println("Оставшееся количество жизней у игрока " + player.getName() + ": " + player.getLive());
-                    lives++;
-                    if (player.getLive() == 0)
-                        gameOver(player);
-                }
-            } return true;
-        } else {
-            System.out.println(player.getName() + " победил");
-            return false;
+            }
         }
     }
+
 
     private void gameOver(Player player) {
         System.out.println("Iгрок " + player.getName() + " умер в жестоком бою");
         System.out.println("Потерянные жизни: " + lives);
         System.out.println("Очки за игру: " + score);
+        System.out.println("Iгра окончена");
         player.setAlive(false);
+        gameContinues = false;
         countPlayers++;
     }
 }
--- src/main/java/faang/school/godbless/BroForce/Main.java
+++ src/main/java/faang/school/godbless/BroForce/Main.java
@@ -2,34 +2,19 @@ package faang.school.godbless.BroForce;
 
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
-import java.util.concurrent.TimeUnit;
 
 import static faang.school.godbless.BroForce.Game.players;
 
 public class Main {
     public static void main(String[] args) {
         Game game = new Game();
-        //List<Player> players = new ArrayList<>();
         players.add(new Player("Player1", 5));
         players.add(new Player("Player2", 5));
-        players.add(new Player("Player3", 3));
 
         ExecutorService executorService = Executors.newFixedThreadPool(2);
-        for (Player player : players) {
-            while (game.update(player)) {
-                executorService.submit(() -> {
-                    while (player.getLive() > 0)
-                        game.update(player);
-                });
-            }
+        for (int i = 0; i < players.size(); i++) {
+            executorService.submit(game::update);
         }
         executorService.shutdown();
-        try {
-            executorService.awaitTermination(5, TimeUnit.SECONDS);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
-        System.out.println("Iгра окончена");
     }
-
 }
