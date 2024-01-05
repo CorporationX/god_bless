36252224339b6194bb7dbdfbe8f0b61ee99dc454
Edisolt Khatsiev <Edisolt Khatsiev>
2024-01-05 09:05:46 +0300

    Качаем алхимию

---
+++ src/main/java/faang/school/godbless/BJS2_1341/Potion.java
@@ -0,0 +1,11 @@
+package faang.school.godbless.BJS2_1341;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class Potion {
+    private String name;
+    private int requiredIngredients;
+}
