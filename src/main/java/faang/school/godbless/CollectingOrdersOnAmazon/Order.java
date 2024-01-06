3183c50353e930a70bdb40f19d0ad65b0e5f4913
Marina_Zu <bugsi-47@yandex.ru>
2024-01-06 18:26:37 +0300

    Add task "Собираем заказы в Amazon"

---
+++ src/main/java/faang/school/godbless/CollectingOrdersOnAmazon/Order.java
@@ -0,0 +1,11 @@
+package faang.school.godbless.CollectingOrdersOnAmazon;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@AllArgsConstructor
+@Data
+public class Order {
+    private int id;
+    private String status;
+}
