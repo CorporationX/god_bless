9bc1eaa0cf03064bb503290d72982b6439a6ada8
lonovojvladimir <lonovojvladimir@gmail.com>
2024-01-06 20:50:59 +0300

    Add class AsynchronismFuture and MasterCardService

---
+++ src/main/java/faang/school/godbless/multithreadingAsync/AsynchronismFuture.java
@@ -0,0 +1,26 @@
+package faang.school.godbless.multithreadingAsync;
+
+import java.util.concurrent.*;
+
+public class AsynchronismFuture {
+
+    public static void main(String[] args) {
+        try {
+            doAll();
+        } catch (ExecutionException | InterruptedException e) {
+            System.out.println("Платеж прерван");
+            Thread.currentThread().interrupt();
+            e.printStackTrace();
+        }
+    }
+
+    public static void doAll() throws ExecutionException, InterruptedException {
+        MasterCardService masterCardService = new MasterCardService();
+        ExecutorService executorService = Executors.newSingleThreadExecutor();
+        Future<Integer> resultPayment = executorService.submit(masterCardService::collectPayment);
+        CompletableFuture<Integer> resultAnalystics = CompletableFuture.supplyAsync(masterCardService::sendAnalystics);
+        System.out.println("Result Analystics " + resultAnalystics.get());
+        System.out.println("Result Payment " + resultPayment.get());
+        executorService.shutdown();
+    }
+}
