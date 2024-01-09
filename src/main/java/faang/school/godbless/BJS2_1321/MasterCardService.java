c02a3db3dc10b119ab97b4df99ea87b78a797c47
Edisolt Khatsiev <Edisolt Khatsiev>
2024-01-10 00:33:02 +0300

    Асинхронность и Future

--- src/main/java/faang/school/godbless/BJS2_1321/MasterCardService.java
+++ src/main/java/faang/school/godbless/BJS2_1321/MasterCardService.java
@@ -1,26 +1,32 @@
 package faang.school.godbless.BJS2_1321;
 
-import java.util.concurrent.*;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeoutException;
+import java.util.concurrent.Future;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.TimeUnit;
 
 public class MasterCardService {
 
     public static void main(String[] args) {
-        doAll();
+        try {
+            doAll();
+        } catch (ExecutionException | InterruptedException | TimeoutException e) {
+            e.printStackTrace();
+        }
     }
 
-    public static void doAll() {
+    public static void doAll() throws ExecutionException, InterruptedException, TimeoutException {
         ExecutorService executor = Executors.newSingleThreadExecutor();
         Future<Integer> collectPaymentResult = executor.submit(MasterCardService::collectPayment);
         CompletableFuture<Integer> sendAnalyticsResult = CompletableFuture.supplyAsync(MasterCardService::sendAnalytics);
 
         executor.shutdown();
 
-        try {
-            System.out.println("Collect payment result: " + collectPaymentResult.get(15_000, TimeUnit.MILLISECONDS));
-        } catch (InterruptedException | TimeoutException | ExecutionException e) {
-            e.printStackTrace();
-        }
         System.out.println("Send analytics result: " + sendAnalyticsResult.join());
+        System.out.println("Collect payment result: " + collectPaymentResult.get(15_000, TimeUnit.MILLISECONDS));
     }
 
     public static int collectPayment() {
