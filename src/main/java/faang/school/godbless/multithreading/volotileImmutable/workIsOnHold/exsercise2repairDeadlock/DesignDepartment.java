eb73fc2fc009d295e063a3e7085ba5aa9635c23c
Vlad Meniailov <vlad.meniailov@yandex.ru>
2024-01-16 23:33:20 +0300

    Fixed after Pasha's review.

--- src/main/java/faang/school/godbless/multithreading/volotileImmutable/workIsOnHold/exsercise2repairDeadlock/DesignDepartment.java
+++ src/main/java/faang/school/godbless/multithreading/volotileImmutable/workIsOnHold/exsercise2repairDeadlock/DesignDepartment.java
@@ -1,10 +1,6 @@
 package faang.school.godbless.multithreading.volotileImmutable.workIsOnHold.exsercise2repairDeadlock;
 
-import java.util.concurrent.locks.ReadWriteLock;
-import java.util.concurrent.locks.ReentrantReadWriteLock;
-
 public class DesignDepartment implements Runnable {
-    private final ReadWriteLock lock = new ReentrantReadWriteLock();
     private final DesignResources designResources;
     private final MarketingResources marketingResources;
 
@@ -15,41 +11,23 @@ public class DesignDepartment implements Runnable {
 
     @Override
     public void run() {
-        //readLock marketingResources
-        lock.readLock().lock();
-        try {
-            System.out.println("Original MARKETING files called from DESIGN DEPARTMENT.class: " + marketingResources.readAllFiles());
-            System.out.println("Original DESIGN files called from DESIGN DEPARTMENT.class: " + designResources.readAllFiles());
-        } finally {
-            lock.readLock().unlock();
-        }
+        System.out.println("Original MARKETING files called from DESIGN DEPARTMENT.class: " + marketingResources.readAllFiles());
+        System.out.println("Original DESIGN files called from DESIGN DEPARTMENT.class: " + designResources.readAllFiles());
 
         try {
             Thread.sleep(100);
         } catch (InterruptedException e) {
             Thread.interrupted();
         }
+        designResources.addFile("newFile-Design1.txt");
+        System.out.println("Added new file from DESIGN DEPARTMENT.");
 
-        //writeLock designResources
-        lock.writeLock().lock();
-        try {
-            designResources.addFile("newFile-Design1.txt");
-            System.out.println("Added new file from DESIGN DEPARTMENT.");
-        } finally {
-            lock.writeLock().unlock();
-        }
         try {
             Thread.sleep(100);
         } catch (InterruptedException e) {
             Thread.interrupted();
         }
-
-        lock.readLock().lock();
-        try {
-            System.out.println("Final MARKETING files called from DESIGN DEPARTMENT.class: " + marketingResources.readAllFiles());
-            System.out.println("Final DESIGN files called from DESIGN DEPARTMENT.class: " + designResources.readAllFiles());
-        } finally {
-            lock.readLock().unlock();
-        }
+        System.out.println("Final MARKETING files called from DESIGN DEPARTMENT.class: " + marketingResources.readAllFiles());
+        System.out.println("Final DESIGN files called from DESIGN DEPARTMENT.class: " + designResources.readAllFiles());
     }
 }
--- src/main/java/faang/school/godbless/multithreading/volotileImmutable/workIsOnHold/exsercise2repairDeadlock/MarketingDepartment.java
+++ src/main/java/faang/school/godbless/multithreading/volotileImmutable/workIsOnHold/exsercise2repairDeadlock/MarketingDepartment.java
@@ -1,10 +1,6 @@
 package faang.school.godbless.multithreading.volotileImmutable.workIsOnHold.exsercise2repairDeadlock;
 
-import java.util.concurrent.locks.ReadWriteLock;
-import java.util.concurrent.locks.ReentrantReadWriteLock;
-
 public class MarketingDepartment implements Runnable {
-    private final ReadWriteLock lock = new ReentrantReadWriteLock();
     private final MarketingResources marketingResources;
     private final DesignResources designResources;
 
@@ -15,41 +11,23 @@ public class MarketingDepartment implements Runnable {
 
     @Override
     public void run() {
-        //readLock marketingResources
-        lock.readLock().lock();
-        try {
-            System.out.println("Original MARKETING files called from MARKETING DEPARTMENT.class: " + marketingResources.readAllFiles());
-            System.out.println("Original DESIGN files called from MARKETING DEPARTMENT.class: " + designResources.readAllFiles());
-        } finally {
-            lock.readLock().unlock();
-        }
-
+        System.out.println("Original MARKETING files called from MARKETING DEPARTMENT.class: " + marketingResources.readAllFiles());
+        System.out.println("Original DESIGN files called from MARKETING DEPARTMENT.class: " + designResources.readAllFiles());
         try {
             Thread.sleep(100);
         } catch (InterruptedException e) {
             Thread.interrupted();
         }
 
-        //writeLock designResources
-        lock.writeLock().lock();
-        try {
-            marketingResources.addFile("newFile-Marketing1.txt");
-            System.out.println("Added new file from MARKETING DEPARTMENT.");
-        } finally {
-            lock.writeLock().unlock();
-        }
+        marketingResources.addFile("newFile-Marketing1.txt");
+        System.out.println("Added new file from MARKETING DEPARTMENT.");
         try {
             Thread.sleep(100);
         } catch (InterruptedException e) {
             Thread.interrupted();
         }
+        System.out.println("Final MARKETING files called from MARKETING DEPARTMENT.class: " + marketingResources.readAllFiles());
+        System.out.println("Final DESIGN files called from MARKETING DEPARTMENT.class: " + designResources.readAllFiles());
 
-        lock.readLock().lock();
-        try {
-            System.out.println("Final MARKETING files called from MARKETING DEPARTMENT.class: " + marketingResources.readAllFiles());
-            System.out.println("Final DESIGN files called from MARKETING DEPARTMENT.class: " + designResources.readAllFiles());
-        } finally {
-            lock.readLock().unlock();
-        }
     }
 }
