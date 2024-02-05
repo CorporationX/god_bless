f2c93ac23eadf42a22a540c02f9f68d8de4deda4
Bukevsky | Kirill <bykaplay@gmail.com>
2024-02-05 16:50:59 +0300

    add abstract class and children

---
+++ src/main/java/faang/school/godbless/Warrior.java
@@ -0,0 +1,12 @@
+package faang.school.godbless;
+
+public class Warrior extends Character {
+    public Warrior(String name) {
+        super(name, 10, 5 ,3);
+    }
+
+    @Override
+    public void attack(Character character) {
+        character.setHealth(character.getHealth() - this.power);
+    }
+}
