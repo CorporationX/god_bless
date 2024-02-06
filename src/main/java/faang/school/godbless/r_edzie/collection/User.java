c7781fa210ee6848f3606040055444d25855fe02
Rostislav Bartenev <rostislav.bartenev@gmail.com>
2024-02-06 17:41:57 +0300

    fix commits

--- src/main/java/faang/school/godbless/r_edzie/collection/User.java
+++ src/main/java/faang/school/godbless/r_edzie/collection/User.java
@@ -1,23 +1,28 @@
 package faang.school.godbless.r_edzie.collection;
 
-import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Objects;
 import java.util.Set;
 
 public class User {
     private int id;
     private String name;
     private int age;
-    private Set<String> hobbies;
+    private Set<String> activities;
 
-    public Map<User, String> findHobbyLovers(ArrayList<User> users, Set<String> hobbies) {
+    User(String name, Set<String> activities) {
+        this.name = name;
+        this.activities = activities;
+    }
+
+    public Map<User, String> findHobbyLovers(List<User> users, Set<String> hobbies) {
         Map<User, String> collection = new HashMap<>();
 
         for (User user : users) {
             for (String hobby : hobbies) {
-                if (user.hobbies.contains(hobby)) {
+                if (user.activities.contains(hobby)) {
                     collection.put(user, hobby);
                     break;
                 }
@@ -25,5 +30,34 @@ public class User {
         }
 
         return collection;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        User user = (User) o;
+        return id == user.id && age == user.age && Objects.equals(name, user.name) && Objects.equals(activities, user.activities);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, name, age, activities);
     }
 }
