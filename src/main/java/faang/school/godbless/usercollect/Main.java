7d03ba537ba73a99ba86ed691acafdc03e68d65b
Viktor Dyukov <snailrake@mail.ru>
2024-02-06 21:00:26 +0400

    refactor: move findHobbyLovers method into User class

--- src/main/java/faang/school/godbless/usercollect/Main.java
+++ src/main/java/faang/school/godbless/usercollect/Main.java
@@ -1,7 +1,6 @@
 package faang.school.godbless.usercollect;
 
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
@@ -13,22 +12,9 @@ public class Main {
         users.add(new User(1, "John", 25, Set.of("Reading", "Running")));
         users.add(new User(2, "Alice", 30, Set.of("Swimming", "Painting")));
         users.add(new User(3, "Bob", 22, Set.of("Cycling", "Gaming")));
-        Map<User, String> hobbyLovers = findHobbyLovers(users, Set.of("Gaming", "Swimming"));
+        Map<User, String> hobbyLovers = User.findHobbyLovers(users, Set.of("Gaming", "Swimming"));
         for (var entry : hobbyLovers.entrySet()) {
             System.out.println(String.format("User: %s, Hobby: %s", entry.getKey().getName(), entry.getValue()));
         }
-    }
-
-    public static Map<User, String> findHobbyLovers(List<User> users, Set<String> hobbies) {
-        Map<User, String> hobbyLovers = new HashMap<>();
-        for (var user : users) {
-            for (var activity : user.getActivities()) {
-                if (hobbies.contains(activity)) {
-                    hobbyLovers.put(user, activity);
-                    break;
-                }
-            }
-        }
-        return hobbyLovers;
     }
 }
--- src/main/java/faang/school/godbless/usercollect/User.java
+++ src/main/java/faang/school/godbless/usercollect/User.java
@@ -3,6 +3,9 @@ package faang.school.godbless.usercollect;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 import java.util.Set;
 
 @Getter
@@ -13,4 +16,17 @@ public class User {
     private String name;
     private int age;
     private Set<String> activities;
+
+    public static Map<User, String> findHobbyLovers(List<User> users, Set<String> hobbies) {
+        Map<User, String> hobbyLovers = new HashMap<>();
+        for (var user : users) {
+            for (var activity : user.getActivities()) {
+                if (hobbies.contains(activity)) {
+                    hobbyLovers.put(user, activity);
+                    break;
+                }
+            }
+        }
+        return hobbyLovers;
+    }
 }
