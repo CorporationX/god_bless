2be336e1eb35623a07da19f52b229e81061d7295
Weltschmerz <kirill.aleynikooov@gmail.com>
2024-02-06 17:57:09 +0500

    Абстракция Абстракция

---
+++ src/main/java/faang/school/godbless/User.java
@@ -0,0 +1,23 @@
+package faang.school.godbless;
+
+public class User {
+    private int id;
+    private String name;
+    private int age;
+    private String[] aktiv;
+
+    public User(int id, String name, int age, String[] aktiv) {
+        this.id = id;
+        this.name = name;
+        this.age = age;
+        this.aktiv = aktiv;
+    }
+
+    public void findHobbyLovers(){
+
+    }
+}
+
+public static void main(String args[]){
+
+}
