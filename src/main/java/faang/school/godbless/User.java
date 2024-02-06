d5093e74dfad5b165beb46b94c90e802cad7d0ba
Weltschmerz <kirill.aleynikooov@gmail.com>
2024-02-06 18:08:26 +0500

    Абстракция Абстракция

--- src/main/java/faang/school/godbless/User.java
+++ src/main/java/faang/school/godbless/User.java
@@ -16,13 +16,20 @@ public class User {
         this.aktiv = aktiv;
     }
 
-    public void findHobbyLovers(){
+    public void findHobbyLovers(List<users>){
 
     }
 
     public static void main(String args[]){
         List<User> users = new ArrayList<>();
-        
+        String[] hobbies1 = {"football", "basketball"};
+        String[] hobbies2 = {"reading", "programming"};
+        String[] hobbies3 = {"programming", "horses"};
+        String[] hobbies4 = {"football", "horses"};
+        User person1 = new User(1,"Karl", 20, hobbies1);
+        User person2 = new User(2,"Mary", 28, hobbies1);
+        User person3 = new User(3,"Stefan", 18, hobbies1);
+        User person4 = new User(4,"Helga", 31, hobbies1);
     }
 }
 
