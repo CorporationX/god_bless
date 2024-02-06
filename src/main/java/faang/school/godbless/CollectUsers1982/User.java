423106a5dd2c0f0aedd2ad74a78a5bd462a91aed
Dmitriy Goncharov <incoom@yandex.ru>
2024-02-06 21:15:26 +0500

    BJS2-1982 - Коллекционируем пользователей

---
+++ src/main/java/faang/school/godbless/CollectUsers1982/User.java
@@ -0,0 +1,27 @@
+package faang.school.godbless.CollectUsers1982;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
+public class User {
+    private int id;
+    private String name;
+    private int age;
+    private Set<String> hobbies;
+
+    public Map<User, String> findHobbyLovers(List<User> users, Set<String> hobbies) {
+        Map<User, String> hobbyLovers = new HashMap<>();
+
+        for (User user : users) {
+            for (String hobby : hobbies) {
+                if (user.hobbies.contains(hobby)) {
+                    hobbyLovers.put(user, hobby);
+                    break;
+                }
+            }
+        }
+        return hobbyLovers;
+    }
+}
