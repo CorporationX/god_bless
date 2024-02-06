b3f498bbe8e8ef0a06007706ba20d929bbef3105
Kirill Gutorov <gutorovvvvv.k@gmail.com>
2024-02-06 21:19:39 +0700

    Abstraction

--- src/main/java/faang/school/godbless/abstraction/Character.java
+++ src/main/java/faang/school/godbless/abstraction/Character.java
@@ -1,12 +1,6 @@
 package faang.school.godbless.abstraction;
 
 public abstract class Character {
-    //Имя;
-    //Сила;
-    //Ловкость;
-    //Интеллект;
-    //Здоровье. У всех персонажей по умолчанию 100.
-
     protected String name;
     protected int power;
     protected int skill;
--- src/main/java/faang/school/godbless/abstraction/Warrior.java
+++ src/main/java/faang/school/godbless/abstraction/Warrior.java
@@ -1,9 +1,6 @@
 package faang.school.godbless.abstraction;
 
 public class Warrior extends Character{
-//для Warrior сила всегда выставляется в 10, ловкость в 5, интеллект в 3. Имя передаётся извне;
-
-
     public Warrior(String name) {
         super(name, 10, 5,3);
     }
