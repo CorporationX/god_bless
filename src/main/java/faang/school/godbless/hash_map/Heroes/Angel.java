994d9cafbd41974108e36a757cb38d4420303dff
Bukevsky | Kirill <bykaplay@gmail.com>
2024-02-08 00:47:36 +0300

    Сделал задачу | Герои Меча и Магии

---
+++ src/main/java/faang/school/godbless/hash_map/Heroes/Angel.java
@@ -0,0 +1,7 @@
+package faang.school.godbless.hash_map.Heroes;
+
+public class Angel extends Creature{
+    public Angel() {
+        super("Ангел", 5, 4, 50, 15, 9);
+    }
+}
---
+++ src/main/java/faang/school/godbless/hash_map/Heroes/Griffin.java
@@ -0,0 +1,7 @@
+package faang.school.godbless.hash_map.Heroes;
+
+public class Griffin extends Creature{
+    public Griffin() {
+        super("Магический", 10, 50, 25, 10, 3);
+    }
+}
