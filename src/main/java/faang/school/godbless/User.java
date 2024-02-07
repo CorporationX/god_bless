b5b4efa8ebae24cffeae1dac414a3f0a2377b77c
Vlad Krivonosov <krivonosov.2000@gmail.com>
2024-02-08 00:14:42 +0300

    Created a user class and a method for grouping users by age

---
+++ src/main/java/faang/school/godbless/User.java
@@ -0,0 +1,33 @@
+package faang.school.godbless;
+
+import java.util.*;
+
+public class User {
+
+    private int age;
+    private String name, placeOfWork, address;
+
+    User(String name, String placeOfWork, String address, int age) {
+        this.name = name;
+        this.placeOfWork = placeOfWork;
+        this.age = age;
+        this.address = address;
+    }
+
+    public static Map<Integer, List<User>> groupUsers(List<User> users) {
+        Map<Integer, List<User>> groupUsers = new HashMap<>();
+        Set<Integer> age = new HashSet<>();
+
+        for (User user : users) {//Вносим возраст и списки пользователй
+            List<User> ageAppropriateUser = new ArrayList<>();
+            for (User userAge : users) {//Создаем список пользователей
+                // соответствующий возрасту
+                if (userAge.age == user.age) {
+                    ageAppropriateUser.add(userAge);
+                }
+            }
+            groupUsers.put(user.age, ageAppropriateUser);
+        }
+        return groupUsers;
+    }
+}
