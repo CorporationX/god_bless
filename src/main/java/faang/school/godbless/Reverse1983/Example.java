b6e8c4fbf73963d7a40a9ee290e51cda4f610bcf
Dmitriy Goncharov <incoom@yandex.ru>
2024-02-07 12:39:44 +0500

    BJS2-1983 - Развернитесь! - Исправление.
    1. Изменил метод reverse в классе Example (сделал его static и исправил реализацию);
    2. Добавил тест в метод Main.

--- src/main/java/faang/school/godbless/Reverse1983/Example.java
+++ src/main/java/faang/school/godbless/Reverse1983/Example.java
@@ -2,13 +2,11 @@ package faang.school.godbless.Reverse1983;
 
 public class Example {
 
-    public int[] reverse(int[] nums) {
+    public static int[] reverse(int[] nums) {
         int length = nums.length;
         int[] reversed = new int[length];
-        for (int i = 0; i < length; i++) {
-            for (int j = length - 1; j >= 0; j--) {
-                reversed[i] = nums[j];
-            }
+        for (int i = 0, j = length - 1; i < length && j >= 0; i++, j--) {
+            reversed[i] = nums[j];
         }
         return reversed;
     }
