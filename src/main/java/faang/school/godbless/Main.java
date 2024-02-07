afb70582266e8b55add17c594820b179da2b9994
BehzodR <behzod.rustam@mail.ru>
2024-02-07 09:53:50 +0100

    First task: User group by age

--- src/main/java/faang/school/godbless/Main.java
+++ src/main/java/faang/school/godbless/Main.java
@@ -12,6 +12,7 @@ public class Main {
                 new UserGroup("Sasha", 25, "VK", "Moscow"),
                 new UserGroup("Behzod", 24, "MTC", "S.Petersburg"),
                 new UserGroup("Masha", 30, "Sber", "Kazan"));
+
         Map<Integer, List<UserGroup>> groupedUser=UserGroup.groupUsers(list);
 
         groupedUser.forEach((age, users)->{
