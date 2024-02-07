b772a0dd618478cb6d19ba88f37c126adf632b8a
BehzodR <behzod.rustam@mail.ru>
2024-02-07 11:03:22 +0100

    Second task: some changing in App class. If condition filling Main class changes

--- src/main/java/faang/school/godbless/Main.java
+++ src/main/java/faang/school/godbless/Main.java
@@ -7,19 +7,11 @@ import java.util.Map;
 public class Main {
     public static void main(String[] args) {
         //UserGroup.groupUsers();
-
-        List<App> list = Arrays.asList(
-                new App("Sasha", 25, "VK", "Moscow"),
-                new App("Behzod", 24, "MTC", "S.Petersburg"),
-                new App("Masha", 30, "Sber", "Kazan"));
-
-        Map<Integer, List<App>> groupedUser = App.groupUsers(list);
-
-        groupedUser.forEach((age, users) -> {
-            System.out.println(age + ":");
-            users.forEach(user -> System.out.println("Names" + user.getName()));
-            System.out.println();
-        });
+        try {
+            App user = new App("Sasha", -1, "Developer", "London");
+        } catch (IllegalArgumentException e) {
+            System.out.println("Error: " + e.getMessage());
+        }
 
     }
 }
