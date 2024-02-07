9fcb5889106f057ab23c8ceb895dbaa0007e4b4a
BehzodR <behzod.rustam@mail.ru>
2024-02-07 11:24:12 +0100

    Second task: some changing in App class. If condition filling Main class changes
    Final changes for both classes
    changes in main with comment

--- src/main/java/faang/school/godbless/Main.java
+++ src/main/java/faang/school/godbless/Main.java
@@ -2,9 +2,8 @@ package faang.school.godbless;
 
 public class Main {
     public static void main(String[] args) {
-        //UserGroup.groupUsers();
         try {
-            App user = new App("Behzod", 20, "Uber", "Moscow");
+            App user = new App("Behzod", 20, "Uber", "London"); //не понял почему кидает Error: Address is not in the VALID_ADDRESSES если London есть в Сете
         } catch (IllegalArgumentException e) {
             System.out.println("Error: " + e.getMessage());
         }
