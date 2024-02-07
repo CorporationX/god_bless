269f0a2c1da90573ddd367f725ac3ae76a7597a0
Sam Migachev <sammigachev@gmail.com>
2024-02-07 21:07:47 +0300

    Сделал задачу Группировка пользователей по возрасту

---
+++ src/main/java/Task1/Main.java
@@ -0,0 +1,23 @@
+package Task1;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+public class Main {
+    public static void main(String[] args) {
+        User sam = new User("sam",20, "tinkoff","street1");
+        User vlad = new User("vlad",20, "uber","street2");
+        User artem = new User("artem",24, "sber","street3");
+        User alex = new User("alex",24, "ozon","street4");
+        User david = new User("david",20, "alfa","street5");
+        List<User> users = new ArrayList<>();
+        users.add(sam);
+        users.add(vlad);
+        users.add(artem);
+        users.add(alex);
+        users.add(david);
+        Map<Integer, List<User>> integerListMap = User.groupUsers(users);
+        System.out.println(integerListMap);
+    }
+}
