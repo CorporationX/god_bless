7e48e590dcd0b311c351568163fd03550609a70d
BehzodR <behzod.rustam@mail.ru>
2024-02-07 10:31:49 +0100

    Second task: some changing in App class

--- src/main/java/faang/school/godbless/App.java
+++ src/main/java/faang/school/godbless/App.java
@@ -8,43 +8,52 @@ public class App {
     private int age;
     private String work;
     private String address;
-    public App(String name, int age, String work, String address){
-        this.name=name;
-        this.age=age;
-        this.work=work;
-        this.address=address;
+
+    public App(String name, int age, String work, String address) {
+        this.name = name;
+        this.age = age;
+        this.work = work;
+        this.address = address;
     }
-    /*    static final Set<String> VALID_JOBS=new HashSet<>();
-    VALID_JOBS.add("Google");
-    VALID_JOBS.add("Uber");
-    VALID_JOBS.add("Amazon");
 
-    static final Set<String> VALID_ADDRESSES=new HashSet<>();
-    VALID_JOBS.add("London");
-    VALID_JOBS.add("New York");
-    VALID_JOBS.add("Amsterdam");*/
+    public static final Set<String> VALID_JOBS;
 
-    public String getName(){
+    static {
+        VALID_JOBS = new HashSet<>();
+        VALID_JOBS.add("Google");
+        VALID_JOBS.add("Uber");
+        VALID_JOBS.add("Amazon");
+    }
 
-        if(getName().isEmpty()){
+    public static final Set<String> VALID_ADDRESSES;
 
-        }
+    static {
+        VALID_ADDRESSES = new HashSet<>();
+        VALID_JOBS.add("London");
+        VALID_JOBS.add("New York");
+        VALID_JOBS.add("Amsterdam");
+    }
+
+    public String getName() {
         return name;
     }
-    public Integer getAge(){
-        if(getAge()<18){
 
+    public Integer getAge() {
+        if (age < 18) {
+            throw new IllegalArgumentException("Age is not valid!");
         }
         return age;
     }
-    public String getWork(){
+
+    public String getWork() {
         return work;
     }
-    public String getAddress(){
-        return getAddress();
+
+    public String getAddress() {
+        return address;
     }
 
-    public static Map<Integer,  List<App>>groupUsers(List<App>users){
+    public static Map<Integer, List<App>> groupUsers(List<App> users) {
         return users.stream().collect(Collectors.groupingBy(App::getAge));
     }
 }
--- src/main/java/faang/school/godbless/Main.java
+++ src/main/java/faang/school/godbless/Main.java
@@ -8,16 +8,16 @@ public class Main {
     public static void main(String[] args) {
         //UserGroup.groupUsers();
 
-        List<App> list= Arrays.asList(
+        List<App> list = Arrays.asList(
                 new App("Sasha", 25, "VK", "Moscow"),
                 new App("Behzod", 24, "MTC", "S.Petersburg"),
                 new App("Masha", 30, "Sber", "Kazan"));
 
-        Map<Integer, List<App>> groupedUser= App.groupUsers(list);
+        Map<Integer, List<App>> groupedUser = App.groupUsers(list);
 
-        groupedUser.forEach((age, users)->{
-            System.out.println(age + ":" );
-            users.forEach(user->System.out.println("Names" + user.getName()));
+        groupedUser.forEach((age, users) -> {
+            System.out.println(age + ":");
+            users.forEach(user -> System.out.println("Names" + user.getName()));
             System.out.println();
         });
 
