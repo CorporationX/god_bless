27f785e55e1ee4099e13dd536dd23b4793df1ea1
Rostislav Bartenev <rostislav.bartenev@gmail.com>
2024-02-07 18:09:28 +0300

    Java HashMap: Ловим события

---
+++ src/main/java/faang/school/godbless/r_edzie/events/StreamEvent.java
@@ -0,0 +1,16 @@
+package faang.school.godbless.r_edzie.events;
+
+import lombok.Data;
+
+@Data
+public class StreamEvent {
+    private int id;
+    private String eventType;
+    private String data;
+
+    public StreamEvent(int id, String eventType, String data) {
+        this.id = id;
+        this.eventType = eventType;
+        this.data = data;
+    }
+}
