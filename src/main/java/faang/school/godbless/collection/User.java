1094047aa9031dbc252d3c7969e2f427e94ba6dd
Kirill Gutorov <gutorovvvvv.k@gmail.com>
2024-02-07 13:57:14 +0700

    UserCollection

--- src/main/java/faang/school/godbless/collection/User.java
+++ src/main/java/faang/school/godbless/collection/User.java
@@ -3,6 +3,7 @@ package faang.school.godbless.collection;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Objects;
 
 public class User {
     private int id;
@@ -33,5 +34,17 @@ public class User {
         }
         return map;
 
+    }
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        User user = (User) o;
+        return id == user.id && age == user.age && Objects.equals(name, user.name) && Objects.equals(userActivities, user.userActivities);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, name, age, userActivities);
     }
 }
