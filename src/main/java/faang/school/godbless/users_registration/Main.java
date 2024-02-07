dee7b9d1c0ced914d9ba3ae4c02c0b236fd33c98
Adilbek <adilbek.nurmakov@nu.edu.kz>
2024-02-08 04:38:46 +0600

    РегситÑгистрация пользователей

--- src/main/java/faang/school/godbless/users_registration/Main.java
+++ src/main/java/faang/school/godbless/users_registration/Main.java
@@ -1,2 +1,8 @@
-package faang.school.godbless.users_registration;public class Main {
+package faang.school.godbless.users_registration;
+
+public class Main {
+    public static void main(String[] args) {
+        User tom = new User( 101, "Tom", 18, "Amazon", "London" );
+        //User robert = new User( 101, "Robert", 14, "Amazon", "London" );
+    }
 }
--- src/main/java/faang/school/godbless/users_registration/User.java
+++ src/main/java/faang/school/godbless/users_registration/User.java
@@ -1,2 +1,29 @@
-package faang.school.godbless.users_registration;public class User {
+package faang.school.godbless.users_registration;
+
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+
+public class User {
+    private long id;
+    private String name;
+    private int age;
+    private String workPlace;
+    private String address;
+    private static final Set<String> VALID_JOBS = new HashSet<>( Arrays.asList( "Google","Uber", "Amazon"));
+    private static final Set<String> VALID_ADDRESSES = new HashSet<>(Arrays.asList("London", "New-York", "Amsterdam"));
+
+
+
+    public User(long id, String name, int age, String workPlace, String address) {
+        if(name.isEmpty() || age < 18 || !VALID_JOBS.contains(workPlace ) || !VALID_ADDRESSES.contains(address)){
+            throw new  IllegalArgumentException("Invalid user data");
+        }
+        this.id = id;
+        this.name = name;
+        this.age = age;
+        this.address = address;
+        this.workPlace = workPlace;
+
+    }
 }
