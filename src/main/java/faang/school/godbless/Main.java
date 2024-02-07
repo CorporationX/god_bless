83bc17e0d7c8798489e0ddff010f96f047ac10c4
BehzodR <behzod.rustam@mail.ru>
2024-02-07 09:22:43 +0100

    First task: User group by age

---
+++ src/main/java/faang/school/godbless/Main.java
@@ -0,0 +1,24 @@
+package faang.school.godbless;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
+
+public class Main {
+    public static void main(String[] args) {
+        //UserGroup.groupUsers();
+
+        List<UserGroup> list= Arrays.asList(
+                new UserGroup("Sasha", 25, "VK", "Moscow"),
+                new UserGroup("Behzod", 24, "MTC", "S.Petersburg"),
+                new UserGroup("Masha", 30, "Sber", "Kazan"));
+        Map<Integer, List<UserGroup>> groupedUser=UserGroup.groupUsers(list);
+
+        groupedUser.forEach((age, users)->{
+            System.out.println(age + ":" );
+            users.forEach(user->System.out.println("Names" + user.getName()));
+            System.out.println();
+        });
+
+    }
+}
---
+++ src/main/java/faang/school/godbless/UserGroup.java
@@ -0,0 +1,52 @@
+package faang.school.godbless;
+
+import java.util.*;
+import java.util.stream.Collectors;
+
+public class UserGroup {
+    private String name;
+    private int age;
+    private String work;
+    private String address;
+    public UserGroup(String name, int age, String work, String address){
+        this.name=name;
+        this.age=age;
+        this.work=work;
+        this.address=address;
+    }
+
+    static final Set<String> VALID_JOBS=new HashSet<>();
+    VALID_JOBS.add("Google");
+    VALID_JOBS.add("Uber");
+    VALID_JOBS.add("Amazon");
+
+    static final Set<String> VALID_ADDRESSES=new HashSet<>();
+    VALID_JOBS.add("London");
+    VALID_JOBS.add("New York");
+    VALID_JOBS.add("Amsterdam");
+
+
+    public String getName(){
+
+        if(getName().isEmpty()){
+
+        }
+        return name;
+    }
+    public Integer getAge(){
+        if(getAge()<18){
+
+        }
+        return age;
+    }
+    public String getWork(){
+        return work;
+    }
+    public String getAddress(){
+        return getAddress();
+    }
+
+    public static Map<Integer,  List<UserGroup>>groupUsers(List<UserGroup>users){
+        return users.stream().collect(Collectors.groupingBy(UserGroup::getAge));
+    }
+}
