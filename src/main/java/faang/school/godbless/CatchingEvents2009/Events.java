2a4bf45cda5c157204792cc22ecefbdf81dc1894
Dmitriy Goncharov <incoom@yandex.ru>
2024-02-08 16:01:21 +0500

    BJS2-2009 - Java HashMap: Ловим события

---
+++ src/main/java/faang/school/godbless/CatchingEvents2009/Events.java
@@ -0,0 +1,21 @@
+package faang.school.godbless.CatchingEvents2009;
+
+public class Events {
+
+    private Events() {
+    }
+
+    public static final String EVENT_1 = "Event1_Type1";
+    public static final String EVENT_2 = "Event2_Type1";
+    public static final String EVENT_3 = "Event3_Type2";
+    public static final String EVENT_4 = "Event4_Type2";
+    public static final String EVENT_5 = "Event5_Type3";
+    public static final String EVENT_6 = "Event6_Type3";
+    public static final String EVENT_7 = "Event7_Type4";
+    public static final String EVENT_8 = "Event8_Type4";
+    public static final String EVENTTYPE_1 = "Type1";
+    public static final String EVENTTYPE_2 = "Type2";
+    public static final String EVENTTYPE_3 = "Type3";
+    public static final String EVENTTYPE_4 = "Type4";
+
+}
---
+++ src/main/java/faang/school/godbless/CatchingEvents2009/Main.java
@@ -0,0 +1,102 @@
+package faang.school.godbless.CatchingEvents2009;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class Main {
+
+    static Map<Integer, StreamEvent> eventMap = new HashMap<>(); //id and Event
+    static Map<String, List<StreamEvent>> groupedEvents = new HashMap<>(); //EventType and Events
+    static int id = 1;
+
+    public static void main(String[] args) {
+        addEvent(Events.EVENT_1, Events.EVENTTYPE_1);
+        addEvent(Events.EVENT_2, Events.EVENTTYPE_1);
+        addEvent(Events.EVENT_3, Events.EVENTTYPE_2);
+
+        System.out.println("TEST 1: Added 3 event to both maps. Check if they grouped");
+        System.out.println(eventMap.toString());
+        System.out.println(groupedEvents.toString());
+        removeEvent(2);
+
+        System.out.println("TEST 2: Removed event with id=2");
+        System.out.println(eventMap.toString());
+        System.out.println(groupedEvents.toString());
+        addEvent(Events.EVENT_5, Events.EVENTTYPE_3);
+        addEvent(Events.EVENT_6, Events.EVENTTYPE_3);
+
+        System.out.println("TEST 3: Added 2 events with eventtype 3. Check if they grouped");
+        System.out.println(eventMap.toString());
+        System.out.println(groupedEvents.toString());
+
+        System.out.println("TEST 4: Test printAllEventsInfo method");
+        printAllEventsInfo();
+
+        System.out.println("TEST 5: Test findEventById method (id=4)");
+        try {
+            System.out.println(findEventById(4).toString());
+        } catch (IllegalArgumentException exception) {
+            System.out.println(exception);
+        }
+
+        System.out.println("TEST 6: Test findEventsByType method (type3)");
+        try {
+            System.out.println(findEventsByType(Events.EVENTTYPE_3).toString());
+        } catch (IllegalArgumentException exception) {
+            System.out.println(exception);
+        }
+    }
+
+    public static void addEvent(String event, String eventType) {
+        eventMap.put(id, new StreamEvent(id, eventType, event));
+        if (!(groupedEvents.containsKey(eventType))) {
+            groupedEvents.put(eventType, new ArrayList<>());
+        }
+        groupedEvents.get(eventType).add(new StreamEvent(id, eventType, event));
+        id++;
+    }
+
+    public static void removeEvent(int id) {
+        eventMap.remove(id);
+        int count = 0;
+        for (var entry : groupedEvents.entrySet()) {
+            if(entry.getValue().get(count).getId() == id) {
+                entry.getValue().remove(count);
+            }
+            count ++;
+        }
+    }
+
+    public static StreamEvent findEventById(int id) {
+        if (eventMap.get(id) != null) {
+            return eventMap.get(id);
+        } else {
+            throw new IllegalArgumentException("There's no event with id " + id);
+        }
+    }
+
+    public static List<StreamEvent> findEventsByType(String eventType) {
+        if (groupedEvents.get(eventType) != null) {
+            return groupedEvents.get(eventType);
+        } else {
+            throw new IllegalArgumentException("There's no event type " + eventType);
+        }
+    }
+
+    public static void printAllEventsInfo() {
+        int count = 1;
+        for (var entry : eventMap.entrySet()) {
+            int eventId = entry.getValue().getId();
+            String eventType = entry.getValue().getEventType();
+            String event = entry.getValue().getData();
+            System.out.printf("Event %d: id: %d, EventType: %s, Event: %s%n",
+                    count,
+                    eventId,
+                    eventType,
+                    event);
+            count++;
+        }
+    }
+}
