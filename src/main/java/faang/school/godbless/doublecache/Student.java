d176f9a49295a95bf12a836193dbbfe59fd37442
Viktor Dyukov <snailrake@mail.ru>
2024-02-08 22:59:35 +0400

    Java HashMap: Двойной кэш-кэш

---
+++ src/main/java/faang/school/godbless/doublecache/Student.java
@@ -0,0 +1,12 @@
+package faang.school.godbless.doublecache;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class Student {
+
+    private long id;
+    private String name;
+}
---
+++ src/main/java/faang/school/godbless/doublecache/Subject.java
@@ -0,0 +1,13 @@
+package faang.school.godbless.doublecache;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class Subject {
+
+    private long id;
+    private String name;
+}
