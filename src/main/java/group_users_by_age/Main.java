65a704cbd3fa5070349d1cb8edc6099866b83653
Anton Lukyanchikov <antontotenkopf@gmail.com>
2024-02-08 20:06:26 +0300

    task done

---
+++ src/main/java/group_users_by_age/Main.java
@@ -0,0 +1,10 @@
+package group_users_by_age;
+
+public class Main {
+    public static void main(String[] args) {
+        User bill = new User("Bill", 23, "Apple", "Los Angeles");
+        User anton = new User("Anton", 24, "VTB", "Moscow");
+        User marat = new User("Marat", 35, "Sber", "Moscow");
+
+    }
+}
--- src/main/java/group_users_by_age/User.java
+++ src/main/java/group_users_by_age/User.java
@@ -51,10 +51,4 @@ public class User {
         return groupUsers;
     }
 
-    public static void main(String[] args) {
-        User bill = new User("Bill", 23, "Apple", "Los Angeles");
-        User anton = new User("Anton", 24, "VTB", "Moscow");
-        User marat = new User("Marat", 35, "Sber", "Moscow");
-    }
-
 }
