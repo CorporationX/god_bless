98975a90fd9b3ed6c63ea4b874a37ffcd0629dff
Anton Lukyanchikov <antontotenkopf@gmail.com>
2024-02-08 18:54:53 +0300

    update class User. create setters

--- src/main/java/group_users_by_age/User.java
+++ src/main/java/group_users_by_age/User.java
@@ -5,18 +5,32 @@ public class User {
     private int age;
     private String workPlace;
     private String address;
+
+    public String getName(){return name;}
+    public int getAge(){return age;}
+    public String getWorkPlace() {return workPlace;}
+    public String getAddress() {return address;}
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public void setWorkPlace(String workPlace) {
+        this.workPlace = workPlace;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
     public User(String name, int age, String workPlace, String address){
         this.name = name;
         this.age = age;
         this.workPlace = workPlace;
         this.address = address;
     }
-    public String getName(){return name;}
-    public int getAge(){return age;}
-
-    public String getWorkPlace() {return workPlace;}
-
-    public String getAddress() {return address;}
-
 
 }
