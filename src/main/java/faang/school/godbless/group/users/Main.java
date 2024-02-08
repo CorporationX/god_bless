3ab56fc388942564395390480475a0b31425a2c1
Almaz Miniakhmetov <almaz.miniakhmetov@mail.ru>
2024-02-08 18:26:44 +0300

    Подправил id

--- src/main/java/faang/school/godbless/group/users/Main.java
+++ src/main/java/faang/school/godbless/group/users/Main.java
@@ -10,8 +10,8 @@ public class Main {
     public static void main(String[] args) {
         List<User> users = new ArrayList<>();
         users.add(new User(1, "Almaz", 29, Set.of("Bascet", "Programming", "Anime")));
-        users.add(new User(1, "Irina", 29, Set.of("Films", "Programming", "Music")));
-        users.add(new User(1, "Inna", 29, Set.of("Music", "Programming")));
+        users.add(new User(2, "Irina", 29, Set.of("Films", "Programming", "Music")));
+        users.add(new User(3, "Inna", 29, Set.of("Music", "Programming")));
         Map<User, String> usersWithHobby = User.createMapWithUsers(users, Set.of("Programming", "Music"));
         for (Map.Entry<User, String> user : usersWithHobby.entrySet()) {
             System.out.println(String.format("User: %s, Hobby: %s", user.getKey().getName(), user.getValue()));
