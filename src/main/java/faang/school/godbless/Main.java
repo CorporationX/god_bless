66c15603fcb25d1898d019c7db795722a513274e
DadyaVolodya <pl4stmass@yandex.ru>
2024-02-09 21:10:28 +0300

    create 2 class and 1 method

---
+++ src/main/java/faang/school/godbless/Main.java
@@ -0,0 +1,20 @@
+package src.main.java.faang.school.godbless;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+public class Main {
+    public static void main(String[] args) {
+        List<Student> studentList = new ArrayList<>();
+        studentList.add(new Student("petya", "istfak", 1));
+        studentList.add(new Student("vasya", "mexfak", 2));
+        studentList.add(new Student("sasha", "itfak", 3));
+        studentList.add(new Student("vanya", "himfak", 4));
+
+        Student ivan = new Student("ivat", "istfak", 2);
+
+
+
+    }
+}
