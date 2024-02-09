192b118661188aba9ee048b1c501caf8dbfaf319
BehzodR <behzod.rustam@mail.ru>
2024-02-09 23:37:59 +0100

    Commit, and changes in test classes

--- src/main/java/faang/school/godbless/Main.java
+++ src/main/java/faang/school/godbless/Main.java
@@ -14,23 +14,12 @@ public class Main {
         housesMap.put("Barateon", new House("Dragon Stone", "Three-headed Dragon"));
 
         //удаление
-        //
-        //
-        //  housesMap.remove("Starks");
+        housesMap.remove("Starks");
 
         //get
         House getHouse = housesMap.get("Lanister");
         System.out.println("House name received:  " + getHouse);
 
-
-        //когда удаляем Barateon house, проверка идет на дом Stark
-//        if (housesMap.containsKey("Barateon")) {
-//            System.out.println("Barateon house exist");
-//        } else if (housesMap.containsKey("Starks")) {
-//            System.out.println("Starks existed");
-//        } else {
-//            System.out.println("Lanisters");
-//        }
 
         for (Map.Entry<String, House> entry : housesMap.entrySet()) {
             System.out.println("Name: " + entry.getKey() + "\t" + " Sigil: " + entry.getValue());
---
+++ src/main/java/faang/school/godbless/TestMap.java
@@ -0,0 +1,11 @@
+package faang.school.godbless;
+
+import org.junit.jupiter.api.Test;
+
+public class TestMap {
+    @Test
+
+
+
+
+}
