b2e8be054914488e329490514b84629de89cbd5b
BehzodR <behzod.rustam@mail.ru>
2024-02-09 16:34:46 +0100

    ctrl+alt+l used

--- src/main/java/faang/school/godbless/Example.java
+++ src/main/java/faang/school/godbless/Example.java
@@ -4,13 +4,14 @@ import java.util.Arrays;
 
 public class Example {
     public static void reverse(int[] num) {
-        int l=num.length;
-        for (int i = 0; i < l/2; i++) {
-            int tmp=num[i];
-            num[i]= num[l-i-1];
-            num[l-i-1]=tmp;
+        int l = num.length;
+        for (int i = 0; i < l / 2; i++) {
+            int tmp = num[i];
+            num[i] = num[l - i - 1];
+            num[l - i - 1] = tmp;
         }
     }
+
     public static void main(String[] args) {
         int[] array = {5, 2, 3, 8, 6, 4, 54, 80, 98};
         reverse(array);
