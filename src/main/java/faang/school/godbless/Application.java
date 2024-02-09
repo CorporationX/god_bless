c5e70b12e8a6203e314fbdee7e3fd873a114836f
Vlad Krivonosov <krivonosov.2000@gmail.com>
2024-02-09 12:42:30 +0300

    The users created a class, created a method in it, and added a mini-test for the method.

--- src/main/java/faang/school/godbless/Application.java
+++ src/main/java/faang/school/godbless/Application.java
@@ -1,8 +1,25 @@
 package faang.school.godbless;
 
+import java.util.ArrayList;
+import java.util.Map;
+import java.util.Set;
+import java.util.List;
 public class Application {
     public static void main(String... args) {
         System.out.println("Hello world");
-
+        List<User> users = new ArrayList<>();
+        Set<String>activity1 = Set.of("Drawing","Dancing","Music","Sport");
+        User user1 =new User("Ivan",14,activity1);
+        Set<String>activity2 = Set.of("Dancing","Music","Drawing","Sport");
+        User user2 =new User("Sergey",20,activity2);
+        Set<String>activity3 = Set.of("Sport","Drawing","Dancing","Music");
+        User user3 =new User("Artem",35,activity3);
+        Set<String>activity4 = Set.of("Learning foreign languages","Blogging");
+        User user4 =new User("Mathew",23,activity4);
+        users.add(user1);
+        users.add(user2);
+        users.add(user3);
+        users.add(user4);
+        Map<User,String> spisUser = User.findHobbyLovers(users,activity2);
     }
 }
---
+++ src/main/java/faang/school/godbless/User.java
@@ -0,0 +1,44 @@
+package faang.school.godbless;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.*;
+
+@Setter
+@Getter
+public class User {
+    private static int idNum = 0;
+    private int id;
+    private String name;
+    private int age;
+    private Set<String> activity;
+
+    User(String name, int age, Set<String> activity) {
+        setId(idNum++);
+        setActivity(activity);
+        setAge(age);
+        setName(name);
+    }
+
+    public static Map<User, String> findHobbyLovers(List<User> users, Set<String> activity) {
+        Map<User, String> userActivity = new HashMap<>();
+
+        for (User usFor : users) {
+            for (String activUsFor : usFor.activity) {
+                boolean compite= false;
+                for (String activFor : activity) {
+                    if (activFor.equals(activUsFor)) {
+                        userActivity.put(usFor, activUsFor);
+                        compite = true;
+                        break;
+                    }
+                }
+                if(compite) {
+                    break;
+                }
+            }
+        }
+        return userActivity;
+    }
+}
