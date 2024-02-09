c9203333d9562f1da9e4fb23ae0633a815c252d8
Dmitriy Goncharov <incoom@yandex.ru>
2024-02-09 17:43:27 +0500

    BJS2-2064 - Java HashMap: Оптимизируем работу дата-центра

--- src/main/java/faang/school/godbless/OptimizeDataCenter2064/LoadBalancingOptimizationStrategy.java
+++ src/main/java/faang/school/godbless/OptimizeDataCenter2064/LoadBalancingOptimizationStrategy.java
@@ -1,7 +1,5 @@
 package faang.school.godbless.OptimizeDataCenter2064;
 
-import java.util.List;
-
 public class LoadBalancingOptimizationStrategy implements OptimizationStrategy {
     @Override
     public void optimize(DataCenter dataCenter) {
