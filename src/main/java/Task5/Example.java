2cdd06905bd44632a03a4dbc8af0c585b47e135d
Sam Migachev <sammigachev@gmail.com>
2024-02-10 02:34:30 +0300

    сделал задачу развернитесь два варианта дайте пожалуйста обратную связь правильно щел или нет

--- src/main/java/Task5/Example.java
+++ src/main/java/Task5/Example.java
@@ -2,10 +2,20 @@ package Task5;
 
 public class Example {
     public static int[] reverse(int[] nums){
-        int[] array = new int[nums.length];
-        for(int i = 0; i < nums.length; i++){
-            array[i] = nums[nums.length - i - 1];
+        /**
+         * сделал два варианта
+         */
+//        int[] array = new int[nums.length];
+//        for(int i = 0; i < nums.length; i++){
+//            array[i] = nums[nums.length - i - 1];
+//        }
+//        return array;
+
+        for(int i = 0; i < nums.length/2; i++){
+            int n = nums[i];
+            nums[i] = nums[nums.length - i -1];
+            nums[nums.length - i - 1] = n;
         }
-        return array;
+        return nums;
     }
 }
--- src/main/java/Task5/Main.java
+++ src/main/java/Task5/Main.java
@@ -2,10 +2,10 @@ package Task5;
 
 public class Main {
     public static void main(String[] args){
-        int[] nums = {5,4,3,2,1};
+        int[] nums = {5,4,3,2,1,0};
         nums = Example.reverse(nums);
-        for(Integer integer: nums){
-            System.out.print(integer);
+        for(Integer integer: nums) {
+            System.out.println(integer);
         }
     }
 }
