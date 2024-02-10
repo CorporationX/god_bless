202576f22b321d983751ccfa332bebf8494e59f2
DadyaVolodya <pl4stmass@yandex.ru>
2024-02-10 18:18:38 +0300

    create classes and methods

---
+++ src/main/java/faang/school/godbless/Main.java
@@ -0,0 +1,10 @@
+package src.main.java.faang.school.godbless;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class Main {
+    Map<Integer,StreamEvent> map1 = new HashMap<>();
+    Map<String, List<StreamEvent>> map2 = new HashMap<>();
+}
