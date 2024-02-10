f6ad6675670aee45b57400677b7e4f1fafd20147
Adilbek <adilbek.nurmakov@nu.edu.kz>
2024-02-10 20:59:12 +0600

    User resgistration

--- src/main/java/faang/school/godbless/users_registration/Main.java
+++ src/main/java/faang/school/godbless/users_registration/Main.java
@@ -2,7 +2,7 @@ package faang.school.godbless.users_registration;
 
 public class Main {
     public static void main(String[] args) {
-        User tom = new User( 101, "Tom", 18, "Amazon", "London" );
-        //User robert = new User( 101, "Robert", 14, "Amazon", "London" );
+        User tom = new User( 101, "Tom Jackson", 18, "Amazon", "London" );
+        User robert = new User( 101, "Robert Tompson", 18, "AAAAAAA", "London" );
     }
 }
--- src/main/java/faang/school/godbless/users_registration/User.java
+++ src/main/java/faang/school/godbless/users_registration/User.java
@@ -13,17 +13,27 @@ public class User {
     private static final Set<String> VALID_JOBS = new HashSet<>( Arrays.asList( "Google","Uber", "Amazon"));
     private static final Set<String> VALID_ADDRESSES = new HashSet<>(Arrays.asList("London", "New-York", "Amsterdam"));
 
-
-
     public User(long id, String name, int age, String workPlace, String address) {
-        if(name.isEmpty() || age < 18 || !VALID_JOBS.contains(workPlace ) || !VALID_ADDRESSES.contains(address)){
-            throw new  IllegalArgumentException("Invalid user data");
-        }
+        isUserValid(name, age, workPlace, address  );
         this.id = id;
         this.name = name;
         this.age = age;
         this.address = address;
         this.workPlace = workPlace;
 
+    }
+    private void isUserValid( String name, int age, String workPlace, String address){
+        if(name==null || name.isBlank() ){
+            throw new IllegalArgumentException("User name cannot be empty or null!");
+        }
+        if(age<18){
+            throw new IllegalArgumentException("User age cannot be <18");
+
+        }if( !VALID_JOBS.contains(workPlace ) ){
+            throw new IllegalArgumentException("Workplace is not valid company name");
+
+        }if(!VALID_ADDRESSES.contains(address)){
+            throw new IllegalArgumentException("Address is not valid address name");
+        }
     }
 }
