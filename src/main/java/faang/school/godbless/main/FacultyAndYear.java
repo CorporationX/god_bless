6406dbc08bb805c041a99b5b405a19ec39f4bb40
Sergey Ryabov <mre@mail.ru>
2024-02-10 19:02:14 +0800

    HashMap students

---
+++ src/main/java/faang/school/godbless/main/FacultyAndYear.java
@@ -0,0 +1,42 @@
+package faang.school.godbless.main;
+
+import java.util.Objects;
+
+public class FacultyAndYear {
+    private String faculty;
+    private int year;
+
+    public FacultyAndYear(String faculty, int year) {
+        this.faculty = faculty;
+        this.year = year;
+    }
+
+    public String getFaculty() {
+        return faculty;
+    }
+
+    public void setFaculty(String faculty) {
+        this.faculty = faculty;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        FacultyAndYear that = (FacultyAndYear) o;
+        return year == that.year && Objects.equals(faculty, that.faculty);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(faculty, year);
+    }
+}
---
+++ src/main/java/faang/school/godbless/main/Student.java
@@ -0,0 +1,40 @@
+package faang.school.godbless.main;
+
+import java.util.Objects;
+
+public class Student {
+    private String name;
+    private String faculty;
+    private int year;
+
+    public Student(String name, String faculty, int year) {
+        this.name = name;
+        this.faculty = faculty;
+        this.year = year;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getFaculty() {
+        return faculty;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Student student = (Student) o;
+        return year == student.year && Objects.equals(name, student.name) && Objects.equals(faculty, student.faculty);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, faculty, year);
+    }
+}
