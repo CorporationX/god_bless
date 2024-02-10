74e42ff3b5cd77d4c7eb746223cc1003c8e52d46
BehzodR <behzod.rustam@mail.ru>
2024-02-10 15:13:51 +0100

    Commit, and changes

--- src/main/java/faang/school/godbless/Main.java
+++ src/main/java/faang/school/godbless/Main.java
@@ -3,11 +3,17 @@ package faang.school.godbless;
 public class Main {
     public static void main(String[] args) {
         try {
-            User user = new User("Behzod", 20, "Uber", "Amsterdam");
-            System.out.println(user.getName());
-            System.out.println(user.getAge());
-            System.out.println(user.getWork());
-            System.out.println(user.getAddress());
+            User user1 = new User("Behzod",35 ,"Uber", "Amsterdam");
+            User user2 = new User("Behzod",35 ,"Uber", "Amsterdam");
+            System.out.println(user1.getName());
+            System.out.println(user1.getAge());
+            System.out.println(user1.getWork());
+            System.out.println(user1.getAddress());
+
+            System.out.println(user2.getName());
+            System.out.println(user2.getAge());
+            System.out.println(user2.getWork());
+            System.out.println(user2.getAddress());
         } catch (IllegalArgumentException e) {
             System.out.println("Error: " + e.getMessage());
         }
--- src/main/java/faang/school/godbless/User.java
+++ src/main/java/faang/school/godbless/User.java
@@ -16,36 +16,34 @@ public class User {
 
     public User(String name, int age, String work, String address) {
         userValidate();
+
         this.name = name;
         this.age = age;
         this.work = work;
         this.address = address;
     }
-    private void userValidate(){
+    private void userValidate(String name, int age, String work, String address){
         if (name!=null && !name.isEmpty()) {
             throw new IllegalArgumentException("Name can not be empty, please fill it!");
-        }
-        if (age < 18) {
+        }else if (age < 18) {
             throw new IllegalArgumentException("Age is not valid!");
         }
-        if (!VALID_JOBS.contains(work)) {
+        else if (!VALID_JOBS.contains(work)) {
             throw new IllegalArgumentException("Work is not in the VALID_JOBS");
         }
-        if (!VALID_ADDRESSES.contains(address)) {
+        else if (!VALID_ADDRESSES.contains(address)) {
             throw new IllegalArgumentException("Address is not in the VALID_ADDRESSES");
         }
     }
     public static final Set<String> VALID_JOBS;
+    public static final Set<String> VALID_ADDRESSES;
 
     static {
         VALID_JOBS = new HashSet<>();
         VALID_JOBS.add("Google");
         VALID_JOBS.add("Uber");
         VALID_JOBS.add("Amazon");
-    }
-    public static final Set<String> VALID_ADDRESSES;
 
-    static {
         VALID_ADDRESSES = new HashSet<>();
         VALID_ADDRESSES.add("London");
         VALID_ADDRESSES.add("New York");
