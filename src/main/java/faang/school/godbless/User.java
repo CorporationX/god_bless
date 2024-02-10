09fd0013fbcefa1142ec1321a7a5bf54832403d0
BehzodR <behzod.rustam@mail.ru>
2024-02-10 20:43:50 +0100

    change

--- src/main/java/faang/school/godbless/User.java
+++ src/main/java/faang/school/godbless/User.java
@@ -1,17 +1,19 @@
 package faang.school.godbless;
 
 import lombok.Getter;
+import lombok.Setter;
 
 import java.util.*;
 
+@Getter
 public class User {
-    @Getter
+    @Setter
     private int id;
-    @Getter
+    @Setter
     private String name;
-    @Getter
+    @Setter
     private int age;
-    @Getter
+    @Setter
     private Set<String> activities;
 
     public User(int id, String name, int age, Set<String> activities) {
@@ -24,10 +26,10 @@ public class User {
     public static Map<User, String> findHobbyLovers(List<User> users, Set<String> activities) {
         Map<User, String> hobbyLovers = new HashMap<>();
 
-        for(User user: users){
-        Set<String>userActivities=user.getActivities();
-            for(String activity: userActivities){
-                if(activities.contains(activity)){
+        for (User user : users) {
+            Set<String> userActivities = user.getActivities();
+            for (String activity : userActivities) {
+                if (activities.contains(activity)) {
                     hobbyLovers.put(user, activity);
                     break;
                 }
@@ -37,6 +39,23 @@ public class User {
     }
 
     public static void main(String[] args) {
-            ///main method fill left
+        Set<String> findActivities1 = Set.of("Футбол", "Лыжи", "Плавание");
+        Set<String> findActivities2 = Set.of("Хокей", "Музыка", "Программирование");
+        Set<String> findActivities3 = Set.of("Путешествия", "Фотографирование", "Бег");
+
+        List<User> user = Arrays.asList(
+                new User(1, "Behzod", 24, findActivities1),
+                new User(2, "Sasha", 35, findActivities2),
+                new User(3, "Masha", 28, findActivities3)
+        );
+
+        Set<String> searchHobbies = Set.of("Музыка", "Бег");
+
+        Map<User, String> activityLovers = User.findHobbyLovers(user, searchHobbies);
+        for (Map.Entry<User, String> entry : activityLovers.entrySet()) {
+            System.out.println("Пользователь: " + entry.getKey().getName());
+            System.out.println("Общее хобби: " + entry.getValue());
+            System.out.println();
+        }
     }
 }
---
+++ src/main/java/faang/school/godbless/testUserHashMap.java
@@ -0,0 +1,36 @@
+package faang.school.godbless;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class testUserHashMap {
+    @Test
+        public void Map(){
+        Map<User, String> userMap=new HashMap<>();
+        User user1=new User(1, "Behzod", 24, Set.of("Хокей", "Футбол", "Программирование"));
+        User user2=new User(2, "Sasha", 25, Set.of("Музыка", "Плавание", "Лыжи"));
+        User user3=new User(3, "Masha", 26, Set.of("Хокей", "Бег", "Путешествия"));
+
+        userMap.put(user1, "Бег");
+        userMap.put(user2, "Шахматы");
+
+        //remove
+        userMap.remove("Плавание");
+        assertEquals(1, userMap.size());
+        assertFalse(userMap.containsKey(user1));
+
+        //проверка eqauls()
+        assertTrue(user1.equals(user2));
+
+        //проверка hashcode
+        int hashcode1=user1.hashCode();
+        int hashcode2=user2.hashCode();
+        assertEquals(user1, user2);
+
+    }
+}
