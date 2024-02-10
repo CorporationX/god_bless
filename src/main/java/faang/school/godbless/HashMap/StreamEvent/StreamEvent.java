106a4190fc79378fe8ef61a8759e882acff2de9a
DadyaVolodya <pl4stmass@yandex.ru>
2024-02-10 19:52:27 +0300

    create classes and methods

---
+++ src/main/java/faang/school/godbless/HashMap/StreamEvent/StreamEvent.java
@@ -0,0 +1,37 @@
+package src.main.java.faang.school.godbless.HashMap.StreamEvent;
+
+public class StreamEvent {
+    int id;
+    String eventType;
+    String data;
+
+    StreamEvent(int id, String eventType, String data){
+        this.data = data;
+        this.eventType = eventType;
+        this.id = id;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getEventType() {
+        return eventType;
+    }
+
+    public void setEventType(String eventType) {
+        this.eventType = eventType;
+    }
+
+    public String getData() {
+        return data;
+    }
+
+    public void setData(String data) {
+        this.data = data;
+    }
+}
