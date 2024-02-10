126f1b0fb43d5021e56cd207d9c78da4bfc40f97
BehzodR <behzod.rustam@mail.ru>
2024-02-10 16:55:12 +0100

    change

--- src/main/java/faang/school/godbless/Book.java
+++ src/main/java/faang/school/godbless/Book.java
@@ -14,6 +14,7 @@ public class Book {
         map.remove(bookRemove);
 
     }
+
     @Getter
     @Setter
     private String title;
@@ -29,6 +30,7 @@ public class Book {
         this.author = author;
         this.year = year;
     }
+
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
@@ -41,35 +43,5 @@ public class Book {
     public int hashCode() {
         return Objects.hash(title, author, year);
     }
-
-
-    public static void main(String[] args) {
-        Map<Book, Integer> map=new HashMap<>();
-        //
-        Book book1=new Book("Маленький принц","Экзюпери", 1965);
-        Book book2=new Book("Атлант расправил плечи","Айн Ренд", 1957);
-        Book book3=new Book("Преступление и Наказание","Достоевский", 1866);
-        Book book4=new Book("Война и мир","Толстой", 1863);
-        Book book5=new Book("Раковые яйца","Булгакова", 1924);
-        //добавление
-        map.put(book1, 12);
-        map.put(book2, 65);
-        map.put(book3, 78);
-        map.put(book4, 787);
-        map.put(book5, 1224);
-        //удаление
-        map.remove("Маленький принц");
-        map.remove("Айн Ренд");
-
-        removeBook("Маленький принц","Экзюпери", 1965);
-        //получение данных по названию
-        map.get("Война и мир");
-        map.get("Атлант расправил плечи");
-
-        //
-    }
-
-
-
 
 }
--- src/main/java/faang/school/godbless/Main.java
+++ src/main/java/faang/school/godbless/Main.java
@@ -4,8 +4,39 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class Main {
+    public static void main(String[] args) {
+        Map<Book, String> map = new HashMap<>();
+        //
+        Book book1 = new Book("Маленький принц", "Экзюпери", 1965);
+        Book book2 = new Book("Атлант расправил плечи", "Айн Ренд", 1957);
+        Book book3 = new Book("Преступление и Наказание", "Достоевский", 1866);
+        Book book4 = new Book("Война и мир", "Толстой", 1863);
+        Book book5 = new Book("Раковые яйца", "Булгакова", 1924);
 
-    Map<Book, String> map=new HashMap<>();
+        //добавление
+        map.put(book1, "B");
+        map.put(book2, "A");
+        map.put(book3, "C");
+        map.put(book4, "D");
+        map.put(book5, "E");
+
+        //удаление
+        //map.remove("Маленький принц");
+        //map.remove("Айн Ренд");
+
+        removeBook("Маленький принц", "Экзюпери", 1965);
+        //получение данных по названию
+        map.get("Война и мир");
+        map.get("Атлант расправил плечи");
+
+        //
+    }
+    private static void removeBook(Map<Book, String>map, String title, String author, Integer year) {
+        Book bookRemove = new Book(title, author, year);
+
+        map.remove(bookRemove);
+
+    }
 
 
 
