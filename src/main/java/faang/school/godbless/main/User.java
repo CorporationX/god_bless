b20699b80d64eca3ac56f08901743426666df4da
Sergey Ryabov <mre@mail.ru>
2024-02-10 15:40:48 +0800

    added method validateUser for constructor

--- src/main/java/faang/school/godbless/main/User.java
+++ src/main/java/faang/school/godbless/main/User.java
@@ -1,5 +1,6 @@
 package faang.school.godbless.main;
 
+
 import java.util.*;
 
 public class User {
@@ -7,20 +8,31 @@ public class User {
     private int age;
     private String work;
     private String address;
+    private static final Set<String> VALID_JOBS = Set.of("Google", "Uber", "Amazon");
+    private static final Set<String> VALID_ADDRESSES = Set.of("London", "New York", "Amsterdam");
+    private static final int adultAge = 18;
 
-    private final Set<String> VALID_JOBS = Set.of("Google", "Uber", "Amazon");
-    private final Set<String> VALID_ADDRESSES = Set.of("London", "New York", "Amsterdam");
-    private final int adultAge = 18;
+    private static void validateUser(String name, int age, String work, String address) throws IllegalArgumentException {
+        if (name.isEmpty()) {
+            throw new IllegalArgumentException("Name can not be blank!");
+        }
+        if (age < adultAge) {
+            throw new IllegalArgumentException("User must be older then 18!");
+        }
+        if (VALID_JOBS.contains(work)) {
+            throw new IllegalArgumentException("It is not valid job!");
+        }
+        if (VALID_ADDRESSES.contains(address)) {
+            throw new IllegalArgumentException("It is not valid address!");
+        }
+    }
 
     public User(String name, int age, String work, String address) throws IllegalArgumentException {
-        if (!name.isEmpty() && age >= adultAge && VALID_JOBS.contains(work) && VALID_ADDRESSES.contains(address)) {
-            this.name = name;
-            this.age = age;
-            this.work = work;
-            this.address = address;
-        } else {
-            throw new IllegalArgumentException();
-        }
+        validateUser(name, age, work, address);
+        this.name = name;
+        this.age = age;
+        this.work = work;
+        this.address = address;
     }
 
     public static Map<Integer, List<User>> groupUsers(List<User> users) {
@@ -44,31 +56,15 @@ public class User {
         return name;
     }
 
-    public void setName(String name) {
-        this.name = name;
-    }
-
     public int getAge() {
         return age;
-    }
-
-    public void setAge(int age) {
-        this.age = age;
     }
 
     public String getWork() {
         return work;
     }
 
-    public void setWork(String work) {
-        this.work = work;
-    }
-
     public String getAddress() {
         return address;
-    }
-
-    public void setAddress(String address) {
-        this.address = address;
     }
 }
