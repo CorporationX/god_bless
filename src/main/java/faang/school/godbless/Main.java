39b620cfe9ee3f6918fb513287ed8e2b664e27df
DadyaVolodya <pl4stmass@yandex.ru>
2024-02-10 12:54:49 +0300

    create 2 class and 1 method

--- src/main/java/faang/school/godbless/Main.java
+++ src/main/java/faang/school/godbless/Main.java
@@ -1,18 +1,83 @@
 package src.main.java.faang.school.godbless;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
 public class Main {
-    public static void main(String[] args) {
-        List<Student> studentList = new ArrayList<>();
-        studentList.add(new Student("petya", "istfak", 1));
-        studentList.add(new Student("vasya", "mexfak", 2));
-        studentList.add(new Student("sasha", "itfak", 3));
-        studentList.add(new Student("vanya", "himfak", 4));
+    static List<Student> studentList = new ArrayList<>();
+    public void addStudent(Student student) {
+        studentList.add(student);
+    }
+    public void removeStudent(String name, String faculty, int year) {
+        Student studentdelite= null;
+        for (Student student : studentList) {
+            if (student.getName().equals(name) && student.getFaculty().equals(faculty) && student.getYear() == year) {
+                studentdelite = student;
+                break;
+            }
+        }
+        if (studentdelite != null) {
+            studentList.remove(studentdelite);
+        }
+    }
+    public List<Student> findStudentsByFacultyAndYear(String faculty, int year) {
+        List<Student> students = new ArrayList<>();
+        for (Student student : studentList) {
+            if (student.getFaculty().equals(faculty) && student.getYear() == year) {
+                students.add(student);
+            }
+        }
+        return students;
+    }
+    public static Map<String, List<Student>> groupStudentsByFacultyAndYear(List<Student> studentList) {
+        Map<String, List<Student>> groupedStudents = new HashMap<>();
+        for (Student student : studentList) {
+            String key = student.getFaculty() + " " + student.getYear();
+            if (groupedStudents.containsKey(key)) {
+                groupedStudents.get(key).add(student);
+            } else {
+                List<Student> studentsList = new ArrayList<>();
+                studentsList.add(student);
+                groupedStudents.put(key, studentsList);
+            }
+        }
 
-        Student ivan = new Student("ivat", "istfak", 2);
+        return groupedStudents;
+    }
+    public void printStudentsByFacultyAndYear() {
+        Map<String, List<Student>> groupedStudents = groupStudentsByFacultyAndYear(studentList);
+        for (String key : groupedStudents.keySet()) {
+            System.out.println("Факультет и курс: " + key);
+            System.out.println("Студенты:");
+            List<Student> students = groupedStudents.get(key);
+            for (Student student : students) {
+                System.out.println("Имя: " + student.getName());
+            }
+            System.out.println();
+        }
+    }
+
+    public static void main(String[] args) {
+        Main main = new Main();
+        main.addStudent(new Student("petya", "istfak", 1));
+        main.addStudent(new Student("vasya", "mexfak", 2));
+        main.addStudent(new Student("sasha", "itfak", 3));
+        main.addStudent(new Student("pasha", "itfak", 3));
+        main.addStudent(new Student("sveta", "itfak", 3));
+        main.addStudent(new Student("galya", "itfak", 3));
+        main.addStudent(new Student("vanya", "himfak", 4));
+        System.out.println("add 4 student");
+        main.removeStudent("vanya", "himfak", 4);
+        System.out.println("delete student");
+        main.findStudentsByFacultyAndYear("itfak",3);
+        System.out.println("vot_oni");
+        groupStudentsByFacultyAndYear(studentList);
+        System.out.println("group");
+        groupStudentsByFacultyAndYear(studentList);
+
+
 
 
 
--- src/main/java/faang/school/godbless/Student.java
+++ src/main/java/faang/school/godbless/Student.java
@@ -1,10 +1,4 @@
 package src.main.java.faang.school.godbless;
-
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-
 public class Student {
     String name;
     String faculty;
@@ -20,36 +14,14 @@ public class Student {
         return name;
     }
 
-    public void setName(String name) {
-        this.name = name;
-    }
-
     public String getFaculty() {
         return faculty;
-    }
-
-    public void setFaculty(String faculty) {
-        this.faculty = faculty;
     }
 
     public int getYear() {
         return year;
     }
 
-    public void setYear(int year) {
-        this.year = year;
-    }
 
-    Map<String, List<Student>> addHashmap(List<Student> studentList){
-        Map<String, List<Student>> map = new HashMap<>();
-        for (Student student : studentList){
-            String key = student.getFaculty() + " " + student.getYear();
-            if(!map.containsKey(key)){
-                map.put(key,new ArrayList<>());
-            }
-            map.get(key).add(student);
-        }
-        return map;
 
-    }
 }
