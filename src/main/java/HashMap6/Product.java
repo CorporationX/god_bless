0e115a324d2e4f30ed7a02c8ecf2fea266be878c
Sam Migachev <sammigachev@gmail.com>
2024-02-11 13:51:17 +0300

    сделал задачу Разбираем склад Amazon

---
+++ src/main/java/HashMap6/Product.java
@@ -0,0 +1,12 @@
+package HashMap6;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class Product {
+    private int id;
+    private String name;
+    private String category;
+}
