3aa2655d2041de667c1833d183b9302d03487453
Vlad Krivonosov <krivonosov.2000@gmail.com>
2024-02-11 11:45:04 +0300

    removed set and replaced the loop with a map check using the containsKey method

--- src/main/java/faang/school/godbless/Application.java
+++ src/main/java/faang/school/godbless/Application.java
@@ -1,7 +1,22 @@
 package faang.school.godbless;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+import static faang.school.godbless.User.groupUsers;
+
 public class Application {
     public static void main(String... args) {
         System.out.println("Hello world");
+
+        User user1GroupAge =new User("Vanua","work","NN",10);
+        User user2GroupAge =new User("Petua","work","NN",11);
+        User user3GroupAge =new User("Misha","work","NN",12);
+        User user4GroupAge =new User("Ivan","work","NN",10);
+        User user5GroupAge =new User("Petr","work","NN",11);
+
+        List<User> users =List.of(user1GroupAge,user2GroupAge,user3GroupAge,user4GroupAge,user5GroupAge);
+        Map<Integer, List<User>> groupAgeUsers = groupUsers(users);
     }
 }
--- src/main/java/faang/school/godbless/User.java
+++ src/main/java/faang/school/godbless/User.java
@@ -16,18 +16,23 @@ public class User {
 
     public static Map<Integer, List<User>> groupUsers(List<User> users) {
         Map<Integer, List<User>> groupUsers = new HashMap<>();
-        Set<Integer> age = new HashSet<>();
+
 
         for (User user : users) {//Вносим возраст и списки пользователй
-            List<User> ageAppropriateUser = new ArrayList<>();
-            for (User userAge : users) {//Создаем список пользователей
-                // соответствующий возрасту
-                if (userAge.age == user.age) {
-                    ageAppropriateUser.add(userAge);
-                }
+
+            if (groupUsers.containsKey(user.age)) {
+                List<User> ageAppropriateUser = groupUsers.get(user.age);
+                ageAppropriateUser.add(user);
+                groupUsers.put(user.age, ageAppropriateUser);
             }
-            groupUsers.put(user.age, ageAppropriateUser);
+            else {
+                List<User> ageAppropriateUser = new ArrayList<>();
+                ageAppropriateUser.add(user);
+                groupUsers.put(user.age, ageAppropriateUser);
+            }
+
         }
         return groupUsers;
     }
 }
+
