b53d4a9e25a354a5e668e8a5d2ad93302152884f
BehzodR <behzod.rustam@mail.ru>
2024-02-11 12:22:45 +0100

    changes

--- src/main/java/faang/school/godbless/House.java
+++ src/main/java/faang/school/godbless/House.java
@@ -3,6 +3,9 @@ package faang.school.godbless;
 import lombok.Getter;
 import lombok.Setter;
 
+import java.util.HashMap;
+import java.util.Map;
+
 @Getter
 @Setter
 public class House {
@@ -18,5 +21,40 @@ public class House {
     @Override
     public String toString() {
         return  name + " " + sigil;
+    }
+
+    private static void addHouse(HashMap<String, House> housesMap, String name, String sigil) {
+        //добавление
+        if(housesMap.containsKey(name)){
+            housesMap.put(name, new House(name, sigil));
+        }
+        System.out.println("House added: " + name);
+    }
+
+    private static void removeHouse(HashMap<String, House> housesMap, String name, String sigil) {
+        //удаление
+        if (housesMap.containsKey(name)) {
+            housesMap.remove("Starks");
+            System.out.println("House removed: " + name);
+        } else {
+            System.out.println("House not found");
+        }
+    }
+
+    private static void getHouse(HashMap<String, House> housesMap, String name, String sigil) {
+        //get
+        if (housesMap.containsKey(name)) {
+            House getHouse = housesMap.get("Lanister");
+            System.out.println("House name received:  " + getHouse);
+        } else {
+            System.out.println("House" + name + "not found:");
+        }
+    }
+
+    private static void iterateHouse(HashMap<String, House> housesMap, String name, String sigil) {
+        //printAll
+        for (Map.Entry<String, House> entry : housesMap.entrySet()) {
+            System.out.println("Name: " + entry.getKey() + "\t" + " Sigil: " + entry.getValue());
+        }
     }
 }
--- src/main/java/faang/school/godbless/Main.java
+++ src/main/java/faang/school/godbless/Main.java
@@ -12,41 +12,6 @@ public class Main {
         housesMap.put("Barateon", new House("Dragon Stone", "Three-headed Dragon"));
 
     }
-
-    private static void addHouse(HashMap<String, House> housesMap, String name, String sigil) {
-        //добавление
-        if(housesMap.containsKey(name)){
-            housesMap.put(name, new House(name, sigil));
-        }
-        System.out.println("House added: " + name);
-    }
-
-    private static void removeHouse(HashMap<String, House> housesMap, String name, String sigil) {
-        //удаление
-        if (housesMap.containsKey(name)) {
-            housesMap.remove("Starks");
-            System.out.println("House removed: " + name);
-        } else {
-            System.out.println("House not found");
-        }
-    }
-
-    private static void getHouse(HashMap<String, House> housesMap, String name, String sigil) {
-        //get
-        if (housesMap.containsKey(name)) {
-            House getHouse = housesMap.get("Lanister");
-            System.out.println("House name received:  " + getHouse);
-        } else {
-            System.out.println("House" + name + "not found:");
-        }
-    }
-
-    private static void iterateHouse(HashMap<String, House> housesMap, String name, String sigil) {
-        //printAll
-        for (Map.Entry<String, House> entry : housesMap.entrySet()) {
-            System.out.println("Name: " + entry.getKey() + "\t" + " Sigil: " + entry.getValue());
-        }
-    }
 }
 
 
