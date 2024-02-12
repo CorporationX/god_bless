f4032ed7014bee2a20082230f74d2f66c3810546
Weltschmerz <kirill.aleynikooov@gmail.com>
2024-02-12 05:50:46 +0500

    Google Переводчик под нашим контролем…

--- src/main/java/faang/school/godbless/Main.java
+++ src/main/java/faang/school/godbless/Main.java
@@ -1,5 +1,22 @@
 package faang.school.godbless;
 
-public class Main {
+import java.util.HashMap;
+import java.util.Map;
+import java.util.function.BiConsumer;
 
+public class Main {
+    public static void main(String args[]){
+        DictionaryProcessor dictionaryProcessor = new DictionaryProcessor();
+
+        Map<String, String> dictionary = new HashMap<>();
+
+        BiConsumer<String, String> addToTheDictionary = (word, translatedWord) -> dictionary.put(word, translatedWord);
+
+        dictionaryProcessor.processWord("Hello", "Привет", addToTheDictionary);
+        dictionaryProcessor.processWord("World", "Мир", addToTheDictionary);
+        dictionaryProcessor.processWord("Cat", "Кошка", addToTheDictionary);
+
+        System.out.println("Словарь: " + dictionary);
+
+    }
 }
