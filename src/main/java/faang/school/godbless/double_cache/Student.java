bc6bcac00d83be2e5c4809b5f5884514d321c615
Adilbek <adilbek.nurmakov@nu.edu.kz>
2024-02-12 23:44:25 +0600

    Double cache

---
+++ src/main/java/faang/school/godbless/double_cache/Student.java
@@ -0,0 +1,11 @@
+package faang.school.godbless.double_cache;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class Student {
+    private int id;
+    private String name;
+}
