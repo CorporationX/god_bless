1e680863a53734f7989b4e28e234822d0e37fd71
Vadim Shorokhov <shorohovvv@gmail.com>
2024-02-12 16:59:38 +0300

    BJS2-2123. Абстракция, абстракция!.

---
+++ src/main/java/bjs2_2123/Archer.java
@@ -0,0 +1,12 @@
+package bjs2_2123;
+
+public class Archer extends Character {
+    public Archer(String name) {
+        super(name, 3, 10, 5);
+    }
+
+    @Override
+    public void attack(Character character) {
+        character.health -= super.dexterity;
+    }
+}
