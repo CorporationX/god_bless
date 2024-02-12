7e15b257f885113dbc7fcd77953bd029bf6b04cf
Weltschmerz <kirill.aleynikooov@gmail.com>
2024-02-12 05:02:33 +0500

    Meta-вселенная?

---
+++ src/main/java/faang/school/godbless/Notification.java
@@ -0,0 +1,19 @@
+package faang.school.godbless;
+
+public class Notification {
+    private String type;
+    private String message;
+
+    public Notification(String type, String message) {
+        this.type = type;
+        this.message = message;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public String getType() {
+        return type;
+    }
+}
---
+++ src/main/java/faang/school/godbless/NotificationManager.java
@@ -0,0 +1,20 @@
+package faang.school.godbless;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.function.Consumer;
+
+public class NotificationManager {
+    Map<String, Consumer<Notification>> map = new HashMap<>();
+    public void registerHandler(String notificationType, Consumer<Notification> message){
+        map.put(notificationType, message);
+    }
+    public void sendNotification(Notification notification){
+        String notificationType = notification.getType();
+        Consumer<Notification> type = map.get(notificationType);
+        if (type != null) {
+            type.accept(notification);
+        }
+    }
+
+}
