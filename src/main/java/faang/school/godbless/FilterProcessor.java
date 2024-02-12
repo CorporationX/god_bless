bb97072dce24b098335ffa292cce76854531da89
Weltschmerz <kirill.aleynikooov@gmail.com>
2024-02-12 05:36:45 +0500

    Фильтры Instagram

---
+++ src/main/java/faang/school/godbless/FilterProcessor.java
@@ -0,0 +1,13 @@
+package faang.school.godbless;
+
+import java.util.function.Function;
+
+public class FilterProcessor {
+    public Image applyFilter(Image image, Function<Image, Image> filter){
+        return filter.apply(image);
+    }
+
+    public Function<Image, Image> combineFilters(Function<Image, Image> first, Function<Image, Image> second){
+        return first.andThen(second);
+    }
+}
--- src/main/java/faang/school/godbless/Main.java
+++ src/main/java/faang/school/godbless/Main.java
@@ -1,5 +1,24 @@
 package faang.school.godbless;
 
+import java.util.function.Function;
+
 public class Main {
+    public static void main(String args[]){
+        Image originalImage = new Image("original.jpg", "Оригинальное изображение");
+        FilterProcessor filterProcessor = new FilterProcessor();
+
+        Function<Image, Image> grayscaleFilter = (image) -> new Image(image.getName() + "_grayScale", "Фильтр: черно-белый");
+        Function<Image, Image> sepiaFilter = (image) -> new Image(image.getName() + "_sepia", "Фильтр: сепия");
+        Function<Image, Image> vignetteFilter = (image) -> new Image(image.getName() + "_vignette", "Фильтр: виньетка");
+
+        Image grayscaleImage = filterProcessor.applyFilter(originalImage, grayscaleFilter);
+        Image sepiaImage = filterProcessor.applyFilter(originalImage, sepiaFilter);
+        Image vignetteImage = filterProcessor.applyFilter(originalImage, vignetteFilter);
+
+        Function<Image, Image> combinedFilter = filterProcessor.combineFilters(grayscaleFilter, sepiaFilter);
+        Image combinedImage = filterProcessor.applyFilter(originalImage, combinedFilter);
+
+        System.out.println(combinedImage);
+    }
 
 }
