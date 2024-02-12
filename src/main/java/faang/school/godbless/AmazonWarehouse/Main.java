45635eacbe85cb928d01e81c14b43f651ecdbd79
Almaz Miniakhmetov <almaz.miniakhmetov@mail.ru>
2024-02-13 02:22:42 +0300

    Решение

--- src/main/java/faang/school/godbless/AmazonWarehouse/Main.java
+++ src/main/java/faang/school/godbless/AmazonWarehouse/Main.java
@@ -7,23 +7,31 @@ import java.util.List;
 import java.util.Map;
 import java.util.Set;
 
-import static jdk.internal.org.jline.utils.Colors.s;
-
 public class Main {
+    private static Set<Product> products = new HashSet<>();
     public static void main(String[] args) {
-        Set<Product> products = new HashSet<>();
+        products.add(new Product(1, "Кофе", "Чай и кофе"));
+        products.add(new Product(2, "Чай", "Чай и кофе"));
+        products.add(new Product(3, "Пепси", "Напитки"));
+        products.add(new Product(4, "Кола", "Напитки"));
+        printAll();
     }
-    public static void groupProduct (Set<Product> productSet){
+
+    public static Map<String, List<Product>> groupProduct(Set<Product> productSet) {
         Map<String, List<Product>> groupsProducts = new HashMap();
-        for(Product product : productSet){
-            groupsProducts.computeIfAbsent(product.getCategory(),k -> new ArrayList<>()).add(product);
+        for (Product product : productSet) {
+            groupsProducts.computeIfAbsent(product.getCategory(), k -> new ArrayList<>()).add(product);
         }
+        return groupsProducts;
     }
-    public static void printAll(Map<String, List<Product>> groupsProducts){
-        for(Map.Entry entry : groupsProducts.entrySet()) {
-            System.out.println(String.format("Категория: \"%s\"." ,entry.getKey()));
-            for (Product product : entry.getValue()) { // Почему нельзя без объявление переменной? Выдавает ошибку с
-                System.out.println(product.getName());
+
+    public static void printAll() {
+        Map<String, List<Product>> groupsProducts = groupProduct(products);
+        for (Map.Entry entry : groupsProducts.entrySet()) {
+            System.out.println(String.format("Категория: \"%s\".", entry.getKey()));
+            Set<Product> products123 = products;
+            for (Product product : products123) {
+                System.out.println("Продукт: " + product.getName());
             }
         }
     }
