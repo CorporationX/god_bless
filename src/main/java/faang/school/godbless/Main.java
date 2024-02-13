8a26f0e2453243b0955b48951cdf01b5d68cdf9a
Weltschmerz <kirill.aleynikooov@gmail.com>
2024-02-13 18:40:48 +0500

    fix conflicts

--- src/main/java/faang/school/godbless/Main.java
+++ src/main/java/faang/school/godbless/Main.java
@@ -1,19 +1,5 @@
 package faang.school.godbless;
 
 public class Main {
-    public static void main(String args[]){
-        
-        NotificationManager notificationManager = new NotificationManager();
-        notificationManager.registerHandler("email", (notification) -> System.out.println("Отправка по электронной почте: " + notification.getMessage()));
-        notificationManager.registerHandler("sms", (notification) -> System.out.println("Отправка SMS: " + notification.getMessage()));
-        notificationManager.registerHandler("push", (notification) -> System.out.println("Отправка push-уведомления: " + notification.getMessage()));
 
-        Notification emailNotification = new Notification("email", "Ваша учетная запись успешно активирована");
-        Notification smsNotification = new Notification("sms", "Вы успешно изменили свой пароль");
-        Notification pushNotification = new Notification("push", "Новый пост от пользователя John Doe");
-
-        notificationManager.sendNotification(emailNotification);
-        notificationManager.sendNotification(smsNotification);
-        notificationManager.sendNotification(pushNotification);
-    }
 }
