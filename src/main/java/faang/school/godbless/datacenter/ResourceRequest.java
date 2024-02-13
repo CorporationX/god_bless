4b5418b475230207f1833c4e99d490854fd0af28
Sergey Ryabov <mre@mail.ru>
2024-02-13 22:08:34 +0800

    ResourceRequest added

---
+++ src/main/java/faang/school/godbless/datacenter/ResourceRequest.java
@@ -0,0 +1,10 @@
+package faang.school.godbless.datacenter;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class ResourceRequest {
+    private double load;
+}
