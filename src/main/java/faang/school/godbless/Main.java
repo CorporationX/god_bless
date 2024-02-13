c2fc9cfbea0e3aa093967e9f768937d43acd99e2
Weltschmerz <kirill.aleynikooov@gmail.com>
2024-02-13 18:43:02 +0500

    fix conflicts

--- src/main/java/faang/school/godbless/Main.java
+++ src/main/java/faang/school/godbless/Main.java
@@ -1,22 +1,5 @@
 package faang.school.godbless;
 
-import java.util.HashMap;
-import java.util.Map;
-import java.util.function.BiConsumer;
-
 public class Main {
-    public static void main(String args[]){
-        DictionaryProcessor dictionaryProcessor = new DictionaryProcessor();
 
-        Map<String, String> dictionary = new HashMap<>();
-
-        BiConsumer<String, String> addToTheDictionary = (word, translatedWord) -> dictionary.put(word, translatedWord);
-
-        dictionaryProcessor.processWord("Hello", "Привет", addToTheDictionary);
-        dictionaryProcessor.processWord("World", "Мир", addToTheDictionary);
-        dictionaryProcessor.processWord("Cat", "Кошка", addToTheDictionary);
-
-        System.out.println("Словарь: " + dictionary);
-
-    }
 }
