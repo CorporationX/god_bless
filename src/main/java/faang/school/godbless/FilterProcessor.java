00311d99301998b43eb1c203f8a03b49f6e6e943
DadyaVolodya <pl4stmass@yandex.ru>
2024-02-13 20:13:29 +0300

    create 1

---
+++ src/main/java/faang/school/godbless/FilterProcessor.java
@@ -0,0 +1,34 @@
+package src.main.java.faang.school.godbless;
+
+import java.util.function.Function;
+
+public class FilterProcessor {
+    Image applyFilter(Image image, Function<Image,Image> filter){
+        return filter.apply(image);
+    }
+    Function<Image, Image> combineFilters(Function<Image, Image> sepiaFilter, Function<Image, Image> grayscaleFilter){
+        return sepiaFilter.andThen(grayscaleFilter);
+    }
+
+
+    public static void main(String[] args) {
+        Image originalImage = new Image("original.jpg", "Оригинальное изображение");
+
+        FilterProcessor filterProcessor = new FilterProcessor();
+
+// Создание фильтров
+        Function<Image, Image> grayscaleFilter = (image) -> new Image(image.getName() + "_grayscale", "Фильтр: черно-белый");
+        Function<Image, Image> sepiaFilter = (image) -> new Image(image.getName() + "_sepia", "Фильтр: сепия");
+        Function<Image, Image> vignetteFilter = (image) -> new Image(image.getName() + "_vignette", "Фильтр: виньетка");
+
+// Применение фильтров
+        Image grayscaleImage = filterProcessor.applyFilter(originalImage, grayscaleFilter);
+        Image sepiaImage = filterProcessor.applyFilter(originalImage, sepiaFilter);
+        Image vignetteImage = filterProcessor.applyFilter(originalImage, vignetteFilter);
+
+// Создание и применение комбинированного фильтра
+        Function<Image, Image> combinedFilter = filterProcessor.combineFilters(grayscaleFilter, sepiaFilter);
+        Image combinedImage = filterProcessor.applyFilter(originalImage, combinedFilter);
+    }
+
+}
