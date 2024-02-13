4b1e017787bb40d7d7dacd14d7d134e51052ca70
kirillgutorov <gutorovvvvv@gmail.ru>
2024-02-13 17:46:15 +0700

    Double cache Подскажи, пожалуйста, что мне еще нужно добавить?

---
+++ src/main/java/faang/school/godbless/hogwarts/SpellAction.java
@@ -0,0 +1,5 @@
+package faang.school.godbless.hogwarts;
+
+public interface SpellAction {
+    String action(String name);
+}
---
+++ src/main/java/faang/school/godbless/hogwarts/SpellCaster.java
@@ -0,0 +1,15 @@
+package faang.school.godbless.hogwarts;
+
+public class SpellCaster {
+    public void cast(String name, SpellAction spellAction){
+        System.out.println(spellAction.action(name));
+    }
+
+    public static void main(String[] args) {
+
+        SpellCaster spell = new SpellCaster();
+        spell.cast("Expelliarmus", (name -> "Whips your target's wand out of their hand"));
+        spell.cast("Alochomorus", (name -> "Open the door"));
+        spell.cast("Lumos", (name -> "Creates the light"));
+    }
+}
