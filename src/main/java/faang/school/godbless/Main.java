ef7cb16c381302c3e5c352ab826df2d251971a45
Weltschmerz <kirill.aleynikooov@gmail.com>
2024-02-13 18:47:18 +0500

    fix conflicts

--- src/main/java/faang/school/godbless/Main.java
+++ src/main/java/faang/school/godbless/Main.java
@@ -1,33 +1,5 @@
 package faang.school.godbless;
 
-import java.util.ArrayList;
-import java.util.List;
-
 public class Main {
 
-
-    public static void main(String args[]){
-        System.out.println();
-
-        List<Item> frodoInventory = new ArrayList<>();
-        Character frodo = new Character(frodoInventory);
-        InventoryManager inventoryManager = new InventoryManager();
-
-        Item ring = new Item("The One Ring", 1000);
-        Item ring2 = new Item("The Second Ring", 10);
-
-        inventoryManager.addItem(frodo, ring, (item) -> System.out.println(item.getName() + " was added"));
-        inventoryManager.addItem(frodo, ring2, (item) -> System.out.println(item.getName() + " was added"));
-
-        System.out.println(frodoInventory);
-        System.out.println();
-
-        inventoryManager.removeItem(frodo, (item) -> item.getName().equals("The One Ring"));
-
-        System.out.println();
-
-        inventoryManager.updateItem(frodo, (item) -> item.getName().equals("The Second Ring"), (item) -> new Item(item.getName(), item.getValue() * 2));
-
-        System.out.println(frodoInventory);
-    }
 }
