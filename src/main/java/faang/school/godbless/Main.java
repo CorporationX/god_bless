092bacf346c153dc87e854a3ab2f00c15afad030
Weltschmerz <kirill.aleynikooov@gmail.com>
2024-02-13 18:48:06 +0500

    fix conflicts

--- src/main/java/faang/school/godbless/Main.java
+++ src/main/java/faang/school/godbless/Main.java
@@ -2,36 +2,4 @@ package faang.school.godbless;
 
 public class Main {
 
-    @FunctionalInterface
-    interface ExceptionHandler {
-        void handleException(Exception e);
-    }
-
-    @FunctionalInterface
-    interface Action<T> {
-        T perform() throws Exception;
-    }
-
-    public static <T> T withErrorHandling(Action<T> action, ExceptionHandler exceptionHandler) {
-        try {
-            return action.perform();
-        } catch (Exception e) {
-            exceptionHandler.handleException(e);
-            return null;
-        }
-    }
-
-    public static void main(String[] args) {
-        String result = withErrorHandling(
-                () -> remoteServiceCall("param"),
-                e -> System.err.println("Не удалось получить доступ к Remote Service. Ошибка DEFAULT")
-        );
-
-        System.out.println("Результат: " + result);
-    }
-
-
-    private static String remoteServiceCall(String param) throws Exception {
-        throw new Exception("Ошибка");
-    }
 }
