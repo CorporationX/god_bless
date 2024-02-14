728d2500612ab34c98d7c030b61c21ee2a8dd57a
Almaz Miniakhmetov <almaz.miniakhmetov@mail.ru>
2024-02-15 00:52:46 +0300

    Решение

---
+++ src/main/java/faang/school/godbless/hogwarts/SpellCaster.java
@@ -0,0 +1,7 @@
+package faang.school.godbless.hogwarts;
+
+public class SpellCaster {
+    public void cast(String spell, SpellCast spellCast) {
+        System.out.println(spellCast.cast(spell));
+    }
+}
