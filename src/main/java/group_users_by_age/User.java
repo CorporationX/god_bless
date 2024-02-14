6e3d67587ee3662b43488d418c45d459287f4216
Anton Lukyanchikov <antontotenkopf@gmail.com>
2024-02-14 22:34:48 +0300

    update class User: updated constructor User

--- src/main/java/group_users_by_age/User.java
+++ src/main/java/group_users_by_age/User.java
@@ -14,6 +14,7 @@ public class User {
     private String address;
     private final Set <String> VALID_JOBS = new HashSet<>(Set.of("Google", "Uber", "Amazon"));
     private final Set <String> VALID_ADDRESSES = new HashSet<>(Set.of("London", "New York", "Amsterdam"));
+    private static final int ALLOWED_AGE = 18;
 
     public String getName() {
         return name;
@@ -31,36 +32,21 @@ public class User {
         return address;
     }
 
-    public void setName(String name) {
-        this.name = name;
-    }
+    public void setName(String name) {this.name = name;}
 
-    public void setAge(int age) {
-        this.age = age;
-    }
+    public void setAge(int age) {this.age = age;}
 
-    public void setWorkPlace(String workPlace) {
-        this.workPlace = workPlace;
-    }
+    public void setWorkPlace(String workPlace) {this.workPlace = workPlace;}
 
-    public void setAddress(String address) {
-        this.address = address;
-    }
+    public void setAddress(String address) {this.address = address;}
 
     public User(String name, int age, String workPlace, String address) {
-        if (!name.isEmpty()) {
-            this.name = name;
-            if (age >= 18) {
-                this.age = age;
-                if (VALID_JOBS.contains(workPlace)){
-                    this.workPlace = workPlace;
-                    if (VALID_ADDRESSES.contains(address)){
-                        this.address = address;
-                    }
-                }
+        if (name.isEmpty() || age < 18 || !VALID_JOBS.contains(workPlace) || !VALID_ADDRESSES.contains(address)) {
+            try {
+                throw new IllegalArgumentException();
+            } catch (IllegalArgumentException e) {
+                System.out.println("!ERROR!\nYOUR ARGUMENT IS ILLEGAL!");
             }
-        } else {
-            throw new IllegalArgumentException("your argument is not allowed");
         }
 
     }
