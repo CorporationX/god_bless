af6b86371b347662c3155f75804c934d206498d1
BehzodR <behzod.rustam@mail.ru>
2024-02-14 12:44:39 +0100

    Meta

--- src/main/java/faang/school/godbless/Main.java
+++ src/main/java/faang/school/godbless/Main.java
@@ -3,6 +3,8 @@ package faang.school.godbless;
 public class Main {
     public static void main(String[] args) {
         NotificationManager notificationManager = new NotificationManager();
+
+
     }
 
 }
--- src/main/java/faang/school/godbless/NotificationManager.java
+++ src/main/java/faang/school/godbless/NotificationManager.java
@@ -1,17 +1,33 @@
 package faang.school.godbless;
 
+import lombok.Getter;
+import lombok.Setter;
+
 import java.util.HashMap;
 import java.util.Map;
 
+@Getter
+@Setter
 public class NotificationManager {
+    public String getNotificationId() {
+        return notificationId;
+    }
 
+    private String notificationId;
+    private String message;
+
+    private Map<String, Consumer<Notification>>notificationHandlers=new HashMap<>();
     @FunctionalInterface
     public interface Consumer<Notification> {
         void accept(Notification param);
     }
 
-    public void registerHandler(){
-        Map<String, Consumer>notify=new HashMap<>();
+    public void registerHandler(String notificationId, Consumer<Notification> handler){
+        notificationHandlers.put(notificationId, handler);
+    }
+    public void setNotification(Notification notification){
+        String notificationId=notification
+
 
     }
 
