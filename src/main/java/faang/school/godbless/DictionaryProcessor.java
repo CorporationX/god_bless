3f73ba1e0e4cf627dd1f7590d561d0cb9ea3f483
Angelina112358 <angelma112358@gmail.com>
2024-02-14 20:32:21 +0300

    Made task "Google translator"

---
+++ src/main/java/faang/school/godbless/DictionaryProcessor.java
@@ -0,0 +1,8 @@
+package faang.school.godbless;
+import java.util.function.BiConsumer;
+
+public class DictionaryProcessor {
+    public void processWord(String word, String translation, BiConsumer<String, String> dictionaryHandler){
+        dictionaryHandler.accept(word, translation);
+    }
+}
