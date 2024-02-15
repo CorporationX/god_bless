799e0a5a832626c79d2da542a13152626141876d
Sergey Ryabov <mre@mail.ru>
2024-02-15 21:27:58 +0800

    rename functions

--- src/main/java/faang/school/godbless/instagram/FilterProcessor.java
+++ src/main/java/faang/school/godbless/instagram/FilterProcessor.java
@@ -3,11 +3,11 @@ package faang.school.godbless.instagram;
 import java.util.function.Function;
 
 public class FilterProcessor {
-    public Image applyFilter(Image image, Function<Image, Image> function) {
-        return  function.apply(image);
+    public Image applyFilter(Image image, Function<Image, Image> filter) {
+        return  filter.apply(image);
     }
 
-    public  Function<Image, Image> combineFilters(Function<Image, Image> function1, Function<Image, Image> function2) {
-        return  function1.andThen(function2);
+    public  Function<Image, Image> combineFilters(Function<Image, Image> filter1, Function<Image, Image> filter2) {
+        return  filter1.andThen(filter2);
     }
 }
