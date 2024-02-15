94ad0d87b2baf17f1e6dd929cce5376d7715880d
Bukevsky | Kirill <bykaplay@gmail.com>
2024-02-15 14:04:52 +0300

    обновил код после замечания

--- src/main/java/faang/school/godbless/lambda/filter_instagram/FilterProcessor.java
+++ src/main/java/faang/school/godbless/lambda/filter_instagram/FilterProcessor.java
@@ -4,11 +4,11 @@ import java.util.function.Function;
 
 public class FilterProcessor {
 
-    public Image applyFilter(Image image, Function<Image, Image> function){
-        return function.apply(image);
+    public Image applyFilter(Image image, Function<Image, Image> filter){
+        return filter.apply(image);
     }
 
-    public Function<Image, Image> combineFilters(Function<Image, Image> filter, Function<Image,Image> filter1){
-        return filter.andThen(filter1);
+    public Function<Image, Image> combineFilters(Function<Image, Image> firstFilter, Function<Image,Image> secondFilter){
+        return firstFilter.andThen(secondFilter);
     }
 }
