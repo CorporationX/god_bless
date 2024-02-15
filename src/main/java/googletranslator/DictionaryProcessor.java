dde9e5ea4201a7563f23e8cae4298115673c9a7a
Sergey Ryabov <mre@mail.ru>
2024-02-15 21:25:45 +0800

    rename biConsumer

--- src/main/java/googletranslator/DictionaryProcessor.java
+++ src/main/java/googletranslator/DictionaryProcessor.java
@@ -3,7 +3,7 @@ package googletranslator;
 import java.util.function.BiConsumer;
 
 public class DictionaryProcessor {
-    public void processWord(String word, String translation, BiConsumer<String, String> biConsumer) {
-        biConsumer.accept(word, translation);
+    public void processWord(String word, String translation, BiConsumer<String, String> dictionaryConsumer) {
+        dictionaryConsumer.accept(word, translation);
     }
 }
