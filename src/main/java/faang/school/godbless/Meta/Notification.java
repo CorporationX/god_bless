f2d10cddb2c1cf67e61c5a98c303451fc1295703
Angelina112358 <angelma112358@gmail.com>
2024-02-15 23:53:24 +0300

    Made task Meta

---
+++ src/main/java/faang/school/godbless/Meta/Notification.java
@@ -0,0 +1,16 @@
+package faang.school.godbless.Meta;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class Notification {
+    private String type;
+    private String message;
+
+    public Notification(String type, String message) {
+        this.type = type;
+        this.message = message;
+    }
+}
