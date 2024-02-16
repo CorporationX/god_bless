c55146df287846d01f86a95eb6601dba715d7cf9
DadyaVolodya <pl4stmass@yandex.ru>
2024-02-16 21:30:53 +0300

    add

--- src/main/java/faang/school/godbless/Main.java
+++ src/main/java/faang/school/godbless/Main.java
@@ -3,12 +3,10 @@ package src.main.java.faang.school.godbless;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
-import java.util.Comparator;
 import java.util.List;
 import java.util.Optional;
 import java.util.OptionalDouble;
 import java.util.stream.Collectors;
-
 import static java.util.Comparator.*;
 
 public class Main {
