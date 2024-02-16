c69f87633fc2d795f8a87991162a510d6930d948
Behzod <behzod.narzullo@gmail.com>
2024-02-16 19:47:59 +0100

    Changes in Hogwarts

--- src/main/java/faang/school/godbless/SpellCaster.java
+++ src/main/java/faang/school/godbless/SpellCaster.java
@@ -2,13 +2,14 @@ package faang.school.godbless;
 
 public class SpellCaster {
 
-    @FunctionalInterface
-    interface SpellAction {
-        String perform(String spell);
-    }
 
-    public void cast(String spell, SpellAction spellAction){
-        String result=spellAction.perform(spell);
+    public void cast(String spell, SpellAction spellAction) {
+        String result = spellAction.perform(spell);
         System.out.println(result);
     }
+}
+
+@FunctionalInterface
+interface SpellAction {
+    String perform(String spell);
 }
