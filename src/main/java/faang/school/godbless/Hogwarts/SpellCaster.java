c56d8d31ecca2319b7daafe60032a6536aec3bc8
Angelina112358 <angelma112358@gmail.com>
2024-02-16 18:24:39 +0300

    Deleted unnecessary files

--- src/main/java/faang/school/godbless/Hogwarts/SpellCaster.java
+++
@@ -1,9 +0,0 @@
-package faang.school.godbless.Hogwarts;
-
-public class SpellCaster {
-
-    public void cast(String spell, SpellAction action) {
-        String result = action.castSpell(spell);
-        System.out.println(result);
-    }
-}
