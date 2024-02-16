37e8329bd7dead14f7df665c8718c3c621e1472b
Sergey Ryabov <mre@mail.ru>
2024-02-16 22:28:13 +0800

    check for null list, cycle through indexes

--- src/main/java/faang/school/godbless/lotr/InventoryManager.java
+++ src/main/java/faang/school/godbless/lotr/InventoryManager.java
@@ -1,13 +1,16 @@
 package faang.school.godbless.lotr;
 
+import java.util.List;
 import java.util.function.Consumer;
 import java.util.function.Function;
 import java.util.function.Predicate;
 
 public class InventoryManager {
     public void addItem(Character character, Item item, Consumer<Item> itemHandler) {
-        character.getInventory().add(item);
-        itemHandler.accept(item);
+        if (character.getInventory() != null) {
+            character.getInventory().add(item);
+            itemHandler.accept(item);
+        }
     }
 
     public void removeItem(Character character, Predicate<Item> itemCondition) {
@@ -18,9 +21,11 @@ public class InventoryManager {
 
     public void updateItem(Character character, Predicate<Item> itemCondition, Function<Item, Item> updateFunction) {
         if (character.getInventory() != null) {
-            for (Item item : character.getInventory()) {
+            List<Item> items = character.getInventory();
+            for (int i = 0; i < items.size(); i++) {
+                Item item = items.get(i);
                 if (itemCondition.test(item)) {
-                    character.getInventory().set(character.getInventory().indexOf(item), updateFunction.apply(item));
+                    character.getInventory().set(i, updateFunction.apply(item));
                 }
             }
         }
