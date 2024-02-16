0fde1e73bf86b60a597c0f10ae9ee8c2a3b1d993
DadyaVolodya <pl4stmass@yandex.ru>
2024-02-16 13:17:23 +0300

    add

--- src/main/java/faang/school/godbless/Notification.java
+++ src/main/java/faang/school/godbless/Notification.java
@@ -1,8 +1,8 @@
 package src.main.java.faang.school.godbless;
 
 public class Notification {
-    private String type;
-    private String message;
+    private final String type;
+    private final String message;
     Notification(String type,String message){
         this.type = type;
         this.message = message;
@@ -13,22 +13,4 @@ public class Notification {
     public String getMessage() {
         return message;
     }
-    public void setMessage(String message) {
-        this.message = message;
-    }
-    NotificationManager notificationManager = new NotificationManager();
-
-// Регистрация обработчиков оповещений
-notificationManager.registerHandler("email", (notification) -> System.out.println("Отправка по электронной почте: " + notification.getMessage()));
-notificationManager.registerHandler("sms", (notification) -> System.out.println("Отправка SMS: " + notification.getMessage()));
-notificationManager.registerHandler("push", (notification) -> System.out.println("Отправка push-уведомления: " + notification.getMessage()));
-
-    // Отправка оповещений
-    Notification emailNotification = new Notification("email", "Ваша учетная запись успешно активирована");
-    Notification smsNotification = new Notification("sms", "Вы успешно изменили свой пароль");
-    Notification pushNotification = new Notification("push", "Новый пост от пользователя: JohnDoe");
-
-notificationManager.sendNotification(emailNotification);
-notificationManager.sendNotification(smsNotification);
-notificationManager.sendNotification(pushNotification);
 }
--- src/main/java/faang/school/godbless/NotificationManager.java
+++ src/main/java/faang/school/godbless/NotificationManager.java
@@ -1,15 +1,22 @@
 package src.main.java.faang.school.godbless;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 import java.util.function.Consumer;
+import java.util.function.Predicate;
 
 
 public class NotificationManager {
     private final Map<String, Consumer<Notification>>map = new HashMap<>();
+    private final List<Predicate<Notification>> filters = new ArrayList<>();
 
     public void registerHandler(String type,Consumer<Notification> handler){
         map.put(type,handler);
+    }
+    public void registerFilter(Predicate<Notification> filter) {
+        filters.add(filter);
     }
     public void sendNotification(Notification notification) {
         Consumer<Notification> handler = map.get(notification.getType());
@@ -21,6 +28,7 @@ public class NotificationManager {
         notificationManager.registerHandler("email", (notification) -> System.out.println("Отправка по электронной почте: " + notification.getMessage()));
         notificationManager.registerHandler("sms", (notification) -> System.out.println("Отправка SMS: " + notification.getMessage()));
         notificationManager.registerHandler("push", (notification) -> System.out.println("Отправка push-уведомления: " + notification.getMessage()));
+        notificationManager.registerFilter(notification -> !notification.getType().equals("sms"));
         Notification emailNotification = new Notification("email", "Ваша учетная запись успешно активирована");
         Notification smsNotification = new Notification("sms", "Вы успешно изменили свой пароль");
         Notification pushNotification = new Notification("push", "Новый пост от пользователя: JohnDoe");
