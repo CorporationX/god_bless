0a0d204ca7b4e0d137430ec86a2b91313929862d
Irina Vereshchagina <irankamoya@gmail.com>
2024-02-16 11:13:24 +0300

    Deleted unnecessary files

--- src/main/java/faang/school/godbless/Hogwarts/Application.java
+++ src/main/java/faang/school/godbless/Hogwarts/Application.java
@@ -2,14 +2,6 @@ package faang.school.godbless.Hogwarts;
 
 public class Application {
     public static void main(String... args) {
-        SpellCaster spellCaster = new SpellCaster();
 
-        String alohomora = "Alohomora";
-        String lumos = "Lumos";
-        String expelliarmus = "Expelliarmus";
-
-        spellCaster.cast(alohomora, (spell) -> "The door is unlocked by " + spell);
-        spellCaster.cast(lumos, (spell) -> "A beam of light is created by " + spell);
-        spellCaster.cast(expelliarmus, (spell) -> "The opponent is disarmed by " + spell);
     }
 }
--- src/main/java/faang/school/godbless/Hogwarts/SpellAction.java
+++
@@ -1,6 +0,0 @@
-package faang.school.godbless.Hogwarts;
-
-@FunctionalInterface
-public interface SpellAction {
-    String castSpell(String spell);
-}
