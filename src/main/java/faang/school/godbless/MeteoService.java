ebd6d992ea35874014545216480211df38e6613d
DadyaVolodya <pl4stmass@yandex.ru>
2024-02-16 15:31:06 +0300

    delete

--- src/main/java/faang/school/godbless/MeteoService.java
+++
@@ -1,9 +0,0 @@
-package src.main.java.faang.school.godbless;
-
-public class MeteoService {
-    public static WeatherData getWeather(String city) {
-        int temperature = (int) (Math.random() * 22);
-        int humidity = (int) (Math.random() * 22);
-        return new WeatherData(city, temperature, humidity);
-    }
-}
