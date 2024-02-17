b1b9b1dc3e1d3ce7dd660b1af4d75435f1ddcd73
DadyaVolodya <pl4stmass@yandex.ru>
2024-02-17 13:31:34 +0300

    решил 4 пункта

---
+++ src/main/java/faang/school/godbless/Main.java
@@ -0,0 +1,61 @@
+package src.main.java.faang.school.godbless;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+public class Main {
+    public static void main(String[] args) {
+        List<Integer> integerList = new ArrayList<>();
+        Collections.addAll(integerList, 1, 9, 3, 6, 4, 5);
+        int tgsum = 10;
+        Map<Integer, Integer> pairValues = integerList.stream()
+                .filter(num -> integerList.contains(tgsum - num) && num < tgsum - num)
+                .collect(Collectors.toMap(
+                        num -> num,
+                        num -> tgsum - num
+                ));
+        pairValues.forEach((key, value) -> System.out.println("[" + key + "," + value + "]"));
+        System.out.println("_________________________________________________________________");
+        Map<String, String> countryCapitalMap = new java.util.HashMap<>();
+        countryCapitalMap.put("Germany", "Berlin");
+        countryCapitalMap.put("Italy", "Rome");
+        countryCapitalMap.put("Spain", "Madrid");
+        countryCapitalMap.put("Japan", "Tokyo");
+        List<String> capitals = countryCapitalMap.entrySet().stream()
+                .sorted(Map.Entry.comparingByKey())
+                .map(Map.Entry::getValue)
+                .collect(Collectors.toList());
+        System.out.println(capitals);
+        System.out.println("_________________________________________________________________");
+        List<String> stringList = Arrays.asList("gsd","asdasf","dsasaf","asdaf","gasfg","ddhd");
+        char letter = 'd';
+        List<String> fs = stringList.stream()
+                .filter(s -> s.charAt(0) == letter)
+                .sorted(Comparator.comparingInt(String::length))
+                .toList();
+        System.out.println(fs);
+        System.out.println("_________________________________________________________________");
+        Map<String, List<String>> friendsMap = new HashMap<>();
+        friendsMap.put("Sasha", Arrays.asList("Masha", "Gena", "Vanya"));
+        friendsMap.put("Petya", Arrays.asList("Anya", "Gena"));
+        friendsMap.put("Vanya", Arrays.asList("Anya", "Sasha"));
+        friendsMap.put("Anya", Arrays.asList("Petya", "Gena"));
+        friendsMap.put("Masha", Arrays.asList("Sasha", "Petya"));
+        friendsMap.put("Gena", Arrays.asList("Masha", "Petya"));
+        Set<String> commonFriends = friendsMap.entrySet().stream()
+                .flatMap(entry -> entry.getValue().stream()
+                        .flatMap(friend -> friendsMap.get(friend).stream()
+                                .filter(f -> !entry.getValue().contains(f) && !f.equals(entry.getKey()))
+                                .map(f -> entry.getKey() + " - " + f)))
+                .collect(Collectors.toSet());
+        commonFriends.forEach(System.out::println);
+        System.out.println("_________________________________________________________________");
+    }
+}
