9f626d4e1a9b3685a7719de0403cf2b2f7fa013e
Behzod <behzod.narzullo@gmail.com>
2024-02-17 10:53:19 +0100

    Google translator in main

--- src/main/java/faang/school/godbless/gmail_filter/Main.java
+++ src/main/java/faang/school/godbless/gmail_filter/Main.java
@@ -9,19 +9,17 @@ public class Main {
     public static void main(String[] args) {
         EmailProcessor emailProcessor = new EmailProcessor();
         List<Email> emailList = List.of(
-                new Email("Mail-1", "Text of mail", true),
-                new Email("Mail-2", "Text of mail 2", false),
-                new Email("Mail-3", "Text of mail 3", true)
+                new Email("Email-1", " Something in the email-1 ", true),
+                new Email("Email-2", " Something in the email-2", true),
+                new Email("Email-3", " Something in the email-3", false)
         );
 
         Predicate<Email> importantFilter = email -> email.isImportant();
-        Consumer<Email> printEmail = email -> System.out.println("Mail is Recevived: " + email.getSubject());
-
+        Consumer<Email> printEmail = email -> System.out.println(" Processed: " + email.getSubject());
         Function<Email, Email> toUpperCase = email -> {
             email.setBody(email.getBody().toUpperCase());
             return email;
         };
-
 
         emailProcessor.processEmails(emailList, importantFilter, printEmail, toUpperCase);
 
