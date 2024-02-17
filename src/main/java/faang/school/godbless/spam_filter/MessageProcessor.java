c84794ba841f8648b3f79ae8622f27b3b9493807
Behzod <behzod.narzullo@gmail.com>
2024-02-17 21:10:36 +0100

    Spam Filter changes

--- src/main/java/faang/school/godbless/spam_filter/MessageProcessor.java
+++ src/main/java/faang/school/godbless/spam_filter/MessageProcessor.java
@@ -14,7 +14,7 @@ public class MessageProcessor {
 
     public boolean processMessage(String message, List<MessageFilter> filters) {
         for (MessageFilter filter : filters) {
-            if (!filter.filter(message)){
+            if (!filter.filter(message)) {
                 return false;
             }
         }
