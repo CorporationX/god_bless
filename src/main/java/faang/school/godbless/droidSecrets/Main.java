58d1d6d7e602c45370827e8a1adfec69c5d2f71a
Sergey Ryabov <mre@mail.ru>
2024-02-18 00:33:15 +0800

    droid secrets

---
+++ src/main/java/faang/school/godbless/droidSecrets/Main.java
@@ -0,0 +1,15 @@
+package faang.school.godbless.droidSecrets;
+
+public class Main {
+    public static void main(String... args) {
+        Droid r2d2 = new Droid();
+        Droid c3po = new Droid();
+        String message = "May the Force be with you!";
+        int encryptionKey = 3;
+
+        String encryptedMsg = r2d2.sendEncryptedMessage(message, encryptionKey);
+        System.out.println(encryptedMsg);
+        String msg = c3po.receiveEncryptedMessage(encryptedMsg, encryptionKey);
+        System.out.println(msg);
+    }
+}
