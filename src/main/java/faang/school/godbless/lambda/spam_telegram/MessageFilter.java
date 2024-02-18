434d40f25706e30dd426bbee15ce4979e656cf2e
Bukevsky | Kirill <bykaplay@gmail.com>
2024-02-18 17:24:01 +0300

    обновил после замечаний

--- src/main/java/faang/school/godbless/lambda/spam_telegram/MessageFilter.java
+++ src/main/java/faang/school/godbless/lambda/spam_telegram/MessageFilter.java
@@ -1,5 +1,5 @@
 package faang.school.godbless.lambda.spam_telegram;
 @FunctionalInterface
 public interface MessageFilter {
-    public boolean filter(String word);
+    boolean filter(String message);
 }
