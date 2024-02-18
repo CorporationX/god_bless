a8149dfc32d4f4bcaac1f4a97f237bb13d77c867
Bukevsky | Kirill <bykaplay@gmail.com>
2024-02-18 17:24:16 +0300

    обновил после замечаний

--- src/main/java/faang/school/godbless/lambda/spam_telegram/MessageProcessor.java
+++ src/main/java/faang/school/godbless/lambda/spam_telegram/MessageProcessor.java
@@ -3,9 +3,9 @@ package faang.school.godbless.lambda.spam_telegram;
 import java.util.List;
 
 public class MessageProcessor {
-    public boolean processMessage(String message, List<MessageFilter> messageFilters){
-        for(MessageFilter messageFilter : messageFilters){
-            if(!messageFilter.filter(message)){
+    public boolean processMessage(String message, List<MessageFilter> messageFilters) {
+        for (MessageFilter messageFilter : messageFilters) {
+            if (!messageFilter.filter(message)) {
                 return false;
             }
         }
