f1d6cd1a4de5404437f359c3c379c0203f4f9350
kirillgutorov <gutorovvvvv@gmail.ru>
2024-02-18 21:39:46 +0700

    Edited version of Double cache

--- src/main/java/faang/school/godbless/doublecache/Main.java
+++ src/main/java/faang/school/godbless/doublecache/Main.java
@@ -1,49 +1,72 @@
 package faang.school.godbless.doublecache;
 
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Set;
 
 public class Main {
     static Map<Student, Map<Subject, Integer>> studentsMarksBySubjects = new HashMap<>();
     static Map<Subject, List<Student>> studentByEachSubject = new HashMap<>();
-    public static void main(String[] args) {
 
+    public static void main(String[] args) {
     }
-    public static void addNewStudent(Student student, Map<Subject, Integer> marks){
+
+    public static void addNewStudent(Student student, Map<Subject, Integer> marks) {
         studentsMarksBySubjects.put(student, marks);
+        Set<Subject> subjects = marks.keySet();
+        for (Subject subject : subjects) {
+            addStudentToSubject(subject, student);
+        }
     }
-    public static void addNewSubjectForExistingStudent(Student student, Subject subject, Integer mark){
-        studentsMarksBySubjects.get(student).put(subject, mark);
+
+    public static void addNewSubjectForExistingStudent(Student student, Subject subject, Integer mark) {
+        if (studentsMarksBySubjects.containsKey(student)) {
+            studentsMarksBySubjects.get(student).put(subject, mark);
+        } else {
+            System.out.println("There is no such student in the Map");
+        }
     }
-    public static void deleteStudent(Student student){
+
+    public static void deleteStudent(Student student) {
+        for (Subject subject : studentsMarksBySubjects.get(student).keySet()) {
+            deleteStudentFromSubject(subject, student);
+        }
         studentsMarksBySubjects.remove(student);
     }
-    public static void listStudentsAndMarks(){
-        System.out.println(studentByEachSubject);
+
+    public static void listStudentsAndMarks() {
+        studentsMarksBySubjects.forEach((student, marks) -> System.out.println("Student: " + student + ", Marks: " + marks));
     }
-    public static void addSubjectAndStudents(Subject subject, List<Student> students){
+
+    public static void addSubjectAndStudents(Subject subject, List<Student> students) {
+        students.forEach(student -> addStudentToSubject(subject, student));
         studentByEachSubject.put(subject, students);
     }
-    public static void addStudentToSubject(Subject subject, Student student){
-        studentByEachSubject.get(subject).add(student);
+
+    public static void addStudentToSubject(Subject subject, Student student) {
+        if (studentByEachSubject.containsKey(subject)) {
+            studentByEachSubject.get(subject).add(student);
+        } else {
+            studentByEachSubject.put(subject, Arrays.asList(student));
+        }
     }
-    public static void deleteStudentFromSubject(Subject subject , Student student){
-        studentByEachSubject.get(subject).remove(student);
+
+    public static void deleteStudentFromSubject(Subject subject, Student student) {
+        if (studentByEachSubject.containsKey(subject)) {
+            studentByEachSubject.get(subject).remove(student);
+        } else {
+            System.out.println("There is no such subject in the Map");
+        }
     }
-    public static void listStudentsAndSubjects(){
-        System.out.println(studentByEachSubject);
+
+    public static void listStudentsAndSubjects() {
+        studentsMarksBySubjects.forEach((student, marks) -> System.out.println("Student: " + student + ", Marks: " + marks));
+        studentByEachSubject.forEach((subject, students) -> {
+            System.out.println("Subject: " + subject);
+            students.stream().forEach(student -> System.out.println("\t" + student));
+        });
     }
 
 }
-//добавление нового студента и его предметов с оценками;
-// добавление нового предмета для существующего студента с оценкой;
-// удаление студента и его предметов с оценками;
-// вывод списка всех студентов и их оценок по предметам.
-
-
-//Реализуйте методы для работы со второй HashMap: добавление нового предмета и списка студентов, изучающих его;
-// добавление студента к существующему предмету;
-// удаление студента из предмета;
-// вывод списка всех предметов и студентов, изучающих их.
-
