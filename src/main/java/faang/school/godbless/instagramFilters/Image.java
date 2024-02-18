f4ab63240b456cd35189878b4d5371247a2c2c94
Ikhsanov Nail <ikhsanovn17@gmail.com>
2024-02-18 13:49:33 +0500

    nail09875 -- BJS2-2691 (Фильтры Instagram)

---
+++ src/main/java/faang/school/godbless/instagramFilters/Image.java
@@ -0,0 +1,20 @@
+package faang.school.godbless.instagramFilters;
+
+public class Image {
+    private final String nameFile;
+    private final String descriptionFile;
+
+    public Image(String nameFile, String descriptionFile) {
+        this.nameFile = nameFile;
+        this.descriptionFile = descriptionFile;
+    }
+
+    public String getName() {
+        return nameFile;
+    }
+
+    public String getDescription() {
+        return descriptionFile;
+    }
+
+}
