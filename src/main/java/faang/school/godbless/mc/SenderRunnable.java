f0c2fe66665896ea9e44759d15a4141b5b6b3815
Adilbek <adilbek.nurmakov@nu.edu.kz>
2024-02-19 14:08:29 +0600

    Microsoft Outlook mail

---
+++ src/main/java/faang/school/godbless/mc/SenderRunnable.java
@@ -0,0 +1,17 @@
+package faang.school.godbless.mc;
+
+import lombok.AllArgsConstructor;
+
+@AllArgsConstructor
+public class SenderRunnable implements Runnable {
+
+    private int startIndex;
+    private int endIndex;
+
+    @Override
+    public void run() {
+        for (int i = startIndex; i <= endIndex; i++) {
+            System.out.printf( "Email %d is sent%n", i );
+        }
+    }
+}
