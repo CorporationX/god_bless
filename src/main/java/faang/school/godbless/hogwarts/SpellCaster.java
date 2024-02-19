36cf09eafa5b1f042b579882b51c6efff6bdf119
Ikhsanov Nail <159633442+Ikhsanov-Nail-95@users.noreply.github.com>
2024-02-19 21:13:14 +0500

    Delete src/main/java/faang/school/godbless/hogwarts directory

--- src/main/java/faang/school/godbless/hogwarts/SpellCaster.java
+++
@@ -1,8 +0,0 @@
-package faang.school.godbless.hogwarts;
-
-
-public class SpellCaster {
-    public void cast(String spellName, SpellAction spellAction) {
-        System.out.println(spellAction.spell((spellName)));
-    }
-}
