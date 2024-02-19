9d2684d4d2ac9d07500bf8c071ca65057798ede0
Bukevsky | Kirill <bykaplay@gmail.com>
2024-02-19 23:39:24 +0300

    Решил задачу | Распределенная армия героев меча и магии

---
+++ src/main/java/faang/school/godbless/multithreading_parallelism/heroes_of_might_and_magic/Character.java
@@ -0,0 +1,11 @@
+package faang.school.godbless.multithreading_parallelism.heroes_of_might_and_magic;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@AllArgsConstructor
+@Getter
+public abstract class Character {
+    private int power;
+
+}
