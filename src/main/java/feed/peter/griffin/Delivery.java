c4beac559caee2fff3f7515bb46b095c5990acff
Weltschmerz <kirill.aleynikooov@gmail.com>
2024-02-19 13:11:15 +0500

    Накормите Питера Гриффина

---
+++ src/main/java/feed/peter/griffin/Delivery.java
@@ -0,0 +1,11 @@
+package feed.peter.griffin;
+
+public class Delivery implements Runnable{
+    @Override
+    public void run(){
+
+    }
+
+    private String character;
+    private int foodAmount;
+}
