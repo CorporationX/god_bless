e5a4b0c3edb7475a07111f5583bd5bd96d74eecf
Sam Migachev <sammigachev@gmail.com>
2024-02-20 19:41:33 +0300

    внес правки

--- src/main/java/griffins/GriffinsFoodDelivery.java
+++ src/main/java/griffins/GriffinsFoodDelivery.java
@@ -3,15 +3,28 @@ package griffins;
 import java.util.Random;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
 
 public class GriffinsFoodDelivery {
+    public static final int THREADAMOUNT = 3;
+
     public static void main(String[] args) {
         Random rnd = new Random();
         String[] names = {"Peter", "Lois", "Meg", "Chris", "Stewie"};
-        ExecutorService executorService = Executors.newFixedThreadPool(3);
-        for (int i = 0; i < 3; i++) {
-            executorService.submit(new FoodDeliveryTask(names[rnd.nextInt(names.length)], rnd.nextInt(1, 50)));
+        ExecutorService executorService = Executors.newFixedThreadPool(THREADAMOUNT);
+        for (int i = 0; i < names.length; i++) {
+            executorService.submit(new FoodDeliveryTask(names[i], rnd.nextInt(1, 50)));
         }
-        executorService.close();
+        executorService.shutdown();
+        try {
+            if (!executorService.awaitTermination(10, TimeUnit.SECONDS)) {
+                System.out.println("Задачи не завершились");
+                executorService.shutdownNow();
+            } else {
+                System.out.println("Задачи завершились");
+            }
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
