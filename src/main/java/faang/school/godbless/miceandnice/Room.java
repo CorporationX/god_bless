d98306187f4beed1f63ff4562a9faafdb09f2f93
Viktor Dyukov <snailrake@mail.ru>
2024-02-20 18:52:22 +0400

    Mice are very nice!

---
+++ src/main/java/faang/school/godbless/miceandnice/Room.java
@@ -0,0 +1,15 @@
+package faang.school.godbless.miceandnice;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.List;
+
+@Setter
+@Getter
+@AllArgsConstructor
+public class Room {
+
+    private List<Food> food;
+}
