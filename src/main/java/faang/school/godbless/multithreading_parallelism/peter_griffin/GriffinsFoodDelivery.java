14779783d6e8831006ba55b885a5211061757dbe
Bukevsky | Kirill <bykaplay@gmail.com>
2024-02-20 22:15:00 +0300

    Решил задачу | Накормите Питера Гриффина

--- src/main/java/faang/school/godbless/multithreading_parallelism/peter_griffin/GriffinsFoodDelivery.java
+++ src/main/java/faang/school/godbless/multithreading_parallelism/peter_griffin/GriffinsFoodDelivery.java
@@ -6,14 +6,18 @@ import java.util.concurrent.Executors;
 import java.util.concurrent.TimeUnit;
 
 public class GriffinsFoodDelivery {
+    private static final int COUNT_THREAD = 4;
     public static void main(String[] args) throws InterruptedException {
         String[] characterNames = {"Peter", "Lois", "Meg", "Chris", "Stewie"};
-        ExecutorService executor = Executors.newFixedThreadPool(3);
+        Random random = new Random();
+        ExecutorService executor = Executors.newFixedThreadPool(COUNT_THREAD);
         for(String character: characterNames){
-            int foodAmount = new Random().nextInt(50);
+            int foodAmount = random.nextInt(50);
             FoodDeliveryTask deliveryFood = new FoodDeliveryTask(character, foodAmount);
             executor.submit(deliveryFood);
         }
         executor.shutdown();
+        executor.awaitTermination(Long.MAX_VALUE, TimeUnit.NANOSECONDS);
+        System.out.println("Все покушали");
     }
 }
