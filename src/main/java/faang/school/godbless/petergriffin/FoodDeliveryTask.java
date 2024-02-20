7c0527c6cc9b83c84ba771cec6c5b88f99767537
kirillgutorov <gutorovvvvv@gmail.ru>
2024-02-20 11:53:57 +0700

    Feed Peter Griffin

---
+++ src/main/java/faang/school/godbless/petergriffin/FoodDeliveryTask.java
@@ -0,0 +1,30 @@
+package faang.school.godbless.petergriffin;
+
+import java.util.Random;
+
+public class FoodDeliveryTask implements Runnable {
+    private String character;
+    private int foodAmount;
+
+    public FoodDeliveryTask(String character, int foodAmount) {
+        this.character = character;
+        this.foodAmount = foodAmount;
+    }
+
+    private String getFoodType() {
+        String[] foodTypes = {"pizza", "burger", "hot dog", "chicken wings", "taco"};
+        return foodTypes[new Random().nextInt(foodTypes.length)];
+    }
+
+    @Override
+    public void run() {
+        String foodType = getFoodType();
+        System.out.printf("%s is about to get %d %s\n", character, foodAmount, foodType);
+        try {
+            Thread.sleep(new Random().nextInt(5001));
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+        System.out.printf("%s eats %d %s\n", character, foodAmount, foodType);
+    }
+}
---
+++ src/main/java/faang/school/godbless/petergriffin/GriffinsFoodDelivery.java
@@ -0,0 +1,17 @@
+package faang.school.godbless.petergriffin;
+
+import java.util.Arrays;
+import java.util.Random;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+public class GriffinsFoodDelivery {
+    public static void main(String[] args) {
+        ExecutorService executors = Executors.newFixedThreadPool(3);
+        String[] characterNames = {"Peter", "Lois", "Meg", "Chris", "Stewie"};
+
+
+        Arrays.stream(characterNames).forEach((character)-> executors.submit(new FoodDeliveryTask(character,new Random().nextInt(10000))));
+        executors.shutdown();
+    }
+}
