6f6bb6a0cba285127d901705948a51cd798a9ccf
Sergey Ryabov <mre@mail.ru>
2024-02-20 22:02:58 +0800

    awaitTermination added

--- src/main/java/faang/school/godbless/griffins/GriffinsFoodDelivery.java
+++ src/main/java/faang/school/godbless/griffins/GriffinsFoodDelivery.java
@@ -3,14 +3,20 @@ package faang.school.godbless.griffins;
 import java.util.Random;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
 
 public class GriffinsFoodDelivery {
-    public static void main(String... args) {
-        ExecutorService executor = Executors.newFixedThreadPool(3);
+    final static int THREADS = 3;
+    public static void main(String... args) throws InterruptedException {
+        ExecutorService executor = Executors.newFixedThreadPool(THREADS);
         String[] characterNames = {"Peter", "Lois", "Meg", "Chris", "Stewie"};
+        Random random = new Random();
         for (String character : characterNames) {
-            executor.submit(new FoodDeliveryTask(character, new Random().nextInt(1, 50)));
+            executor.submit(new FoodDeliveryTask(character, random.nextInt(1, 50)));
         }
         executor.shutdown();
+        if (!executor.awaitTermination(60, TimeUnit.SECONDS))
+            System.err.println("Threads didn't finish in 60 seconds!");
     }
+
 }
