a7989fe79626f1c83ee1622ccf18a0c4bb056527
DadyaVolodya <pl4stmass@yandex.ru>
2024-02-20 22:01:53 +0300

    кomменты и конфликты

--- src/main/java/faang/school/godbless/Main.java
+++ src/main/java/faang/school/godbless/Main.java
@@ -1,58 +1,83 @@
-package src.main.java.faang.school.godbless;
+package faang.school.godbless;
 
-import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Collections;
 import java.util.List;
 import java.util.Optional;
 import java.util.OptionalDouble;
 import java.util.stream.Collectors;
-import static java.util.Comparator.*;
+
+import static java.util.Comparator.comparingInt;
+import static java.util.Comparator.naturalOrder;
 
 public class Main {
-    public static void main(String[] args) {
-        List<Integer> list = new ArrayList<>();
-        List<String> strings = Arrays.asList("Java", "Python", "JavaScript", "C++", "Ruby");
-        Collections.addAll(list,1,2,3,4,5,6,7,8,9);
-        int sum = list.stream()
-                .filter(numbers -> numbers %2==0)
+    public static int sumEvenNumbers(List<Integer> list) {
+        return list.stream()
+                .filter(numbers -> numbers % 2 == 0)
                 .mapToInt(Integer::intValue)
                 .sum();
-        System.out.println(sum);
+    }
 
-        int max = list.stream()
+    public static int findMax(List<Integer> list) {
+        return list.stream()
                 .max(naturalOrder())
                 .get();
-        System.out.println(max);
+    }
 
-        OptionalDouble average = list.stream()
+    public static OptionalDouble calculateAverage(List<Integer> list) {
+        return list.stream()
                 .mapToInt(Integer::intValue)
                 .average();
-        System.out.println(average);
+    }
 
-        long count = strings.stream()
+    public static long countStartsWithJ(List<String> strings) {
+        return strings.stream()
                 .filter(s -> s.startsWith("J"))
                 .count();
-        System.out.println(count);
+    }
 
-        List<String> sorted_strings = strings.stream()
+    public static List<String> sortByLength(List<String> strings) {
+        return strings.stream()
                 .sorted(comparingInt(String::length))
                 .toList();
-        System.out.println(sorted_strings);
+    }
 
-        boolean allMatch = strings.stream()
+    public static boolean allStringsLongerThanOne(List<String> strings) {
+        return strings.stream()
                 .allMatch(s -> s.length() > 1);
-        System.out.println(allMatch);
+    }
 
-        int targetNumber = 5;
-        Optional<String> string3 = strings.stream()
-                .filter(s -> s.length() > targetNumber)
+    public static Optional<String> findLongestStringGreaterThan(List<String> strings, int targetLength) {
+        return strings.stream()
+                .filter(s -> s.length() > targetLength)
                 .min(comparingInt(String::length));
-        string3.ifPresent(System.out::println);
+    }
 
-        List<Integer> lengths = strings.stream()
+    public static List<Integer> getStringLengths(List<String> strings) {
+        return strings.stream()
                 .map(String::length)
                 .collect(Collectors.toList());
-        System.out.println(lengths);
+    }
+
+    public static void main(String[] args) {
+        List<Integer> list = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9);
+        List<String> strings = Arrays.asList("Java", "Python", "JavaScript", "C++", "Ruby");
+
+        int sum = sumEvenNumbers(list);
+        int max = findMax(list);
+        OptionalDouble average = calculateAverage(list);
+        long count = countStartsWithJ(strings);
+        List<String> sortedStrings = sortByLength(strings);
+        boolean allMatch = allStringsLongerThanOne(strings);
+        Optional<String> longestString = findLongestStringGreaterThan(strings, 5);
+        List<Integer> lengths = getStringLengths(strings);
+
+        System.out.println("Sum of even numbers: " + sum);
+        System.out.println("Max number: " + max);
+        System.out.println("Average: " + average.orElse(0.0));
+        System.out.println("Count of strings starting with J: " + count);
+        System.out.println("Sorted strings: " + sortedStrings);
+        System.out.println("All strings longer than 1 character: " + allMatch);
+        System.out.println("Longest string longer than 5 characters: " + longestString.orElse("None"));
+        System.out.println("Lengths of strings: " + lengths);
     }
 }
