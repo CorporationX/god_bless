b7543afdfab0b713c9921c05a818397dda5021a0
Behzod <behzod.narzullo@gmail.com>
2024-02-21 09:32:43 +0100

    GriffinsFoodDelivery

---
+++ src/main/java/faang/school/godbless/pieter_grifin/FoodDeliveryTask.java
@@ -0,0 +1,35 @@
+package faang.school.godbless.pieter_grifin;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.Random;
+
+@Getter
+@Setter
+public class FoodDeliveryTask implements Runnable {
+    private String characterName;
+    private int foodAmount;
+
+    public FoodDeliveryTask(String characterName, int foodAmount) {
+        this.characterName = characterName;
+        this.foodAmount = foodAmount;
+    }
+
+    @Override
+    public void run() {
+        System.out.println(characterName + " receiving " + foodAmount + " " + getFoodType());
+        System.out.println();
+        try {
+            Thread.sleep(foodAmount * 1000L);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+        System.out.println("Delivering " + foodAmount + " food items to " + getCharacterName());
+        System.out.println(characterName + " eating " + foodAmount + " " +getFoodType());
+    }
+    private String getFoodType() {
+        String[] foodTypes = {"pizza", "burger", "hot dog", "chicken wings", "taco"};
+        return foodTypes[new Random().nextInt(foodTypes.length)];
+    }
+}
---
+++ src/main/java/faang/school/godbless/pieter_grifin/GriffinsFoodDelivery.java
@@ -0,0 +1,20 @@
+package faang.school.godbless.pieter_grifin;
+
+import java.util.Random;
+import java.util.concurrent.*;
+
+public class GriffinsFoodDelivery {
+    public static void main(String[] args) {
+        ExecutorService executor = Executors.newFixedThreadPool(3);
+        String[] characterNames = {"Peter", "Lois", "Meg", "Chris", "Stewie"};
+
+        for (String characterName: characterNames) {
+
+            int randomFoodAmount = new Random().nextInt(5) + 1;
+
+            Runnable foodDeliveryTask=new FoodDeliveryTask(characterName, randomFoodAmount);
+            executor.submit(foodDeliveryTask);
+        }
+        executor.shutdown();
+    }
+}
