e2d70e57a7f69d8e919c30df34dfeaa2c5bb9383
Viktor Dyukov <snailrake@mail.ru>
2024-02-21 10:43:21 +0400

    refactor

--- src/main/java/faang/school/godbless/feedgriffin/GriffinsFoodDelivery.java
+++ src/main/java/faang/school/godbless/feedgriffin/GriffinsFoodDelivery.java
@@ -3,15 +3,18 @@ package faang.school.godbless.feedgriffin;
 import java.util.Random;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
 
 public class GriffinsFoodDelivery {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
+        Random random = new Random();
         ExecutorService executorService = Executors.newFixedThreadPool(3);
         String[] characterNames = {"Peter", "Lois", "Meg", "Chris", "Stewie"};
         for (var character : characterNames) {
-            executorService.submit(new FoodDeliveryTask(character, new Random().nextInt(5) + 1));
+            executorService.submit(new FoodDeliveryTask(character, random.nextInt(5) + 1));
         }
         executorService.shutdown();
+        executorService.awaitTermination(5, TimeUnit.MINUTES);
     }
 }
