3f9f442b83438d4195eff6f2ae02f405cd46431f
Rostislav Bartenev <rostislav.bartenev@gmail.com>
2024-02-21 13:55:57 +0300

    Распределенная армия героев меча и магии

---
+++ src/main/java/faang/school/godbless/r_edzie/parallelism/army/Mage.java
@@ -0,0 +1,7 @@
+package faang.school.godbless.r_edzie.parallelism.army;
+
+public class Mage extends Unit {
+    public Mage(int power) {
+        super(power);
+    }
+}
---
+++ src/main/java/faang/school/godbless/r_edzie/parallelism/army/Swordsman.java
@@ -0,0 +1,7 @@
+package faang.school.godbless.r_edzie.parallelism.army;
+
+public class Swordsman extends Unit {
+    public Swordsman(int power) {
+        super(power);
+    }
+}
