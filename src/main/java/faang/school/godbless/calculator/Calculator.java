ec2ff8b8db9330d8f4978aa9107130ab7dffdddd
Vlad Krivonosov <krivonosov.2000@gmail.com>
2024-02-22 17:33:08 +0300

    Made changes and additions to the functionality described in the review

--- src/main/java/faang/school/godbless/calculator/Calculator.java
+++ src/main/java/faang/school/godbless/calculator/Calculator.java
@@ -1,6 +1,6 @@
 package faang.school.godbless.calculator;
-@FunctionalInterface
-public interface Calculator <T> {
 
-    public int calc (T x, T y);
+@FunctionalInterface
+public interface Calculator<T> {
+    int calc(T x, T y);
 }
--- src/main/java/faang/school/godbless/calculator/Main.java
+++ src/main/java/faang/school/godbless/calculator/Main.java
@@ -1,23 +1,25 @@
 package faang.school.godbless.calculator;
 
-import java.util.ArrayList;
 import java.util.List;
 
 public class Main {
-    public static Calculator<Integer> proizv = (firstNum, secondNum) -> firstNum * secondNum;
-    public static Calculator<Integer> sum = (firstNum, secondNum) -> firstNum + secondNum;
+    public static Calculator<Integer> product = (firstNum, secondNum) -> firstNum * secondNum;
+    public static Calculator<Integer> sum = Integer::sum;
 
     public static int calculate(List<Integer> numberList, Calculator calculator) throws IllegalArgumentException {
         int multiplication = 1;
         for (Integer number : numberList) {
             multiplication = calculator.calc(multiplication, number);
         }
-        ;
         return multiplication;
     }
 
     public static void product(List<Integer> numberList) {
-        System.out.println(calculate(numberList, proizv));
+        if (numberList.isEmpty() || numberList == null) {
+            System.out.println("The list is empty");
+            throw new IllegalArgumentException();
+        }
+        System.out.println(calculate(numberList, product));
     }
 
     public static void sum(List<Integer> numberList) {
