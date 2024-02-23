7b8b37724fb0596670355d591f6d3156a2b74354
Ikhsanov Nail <159633442+Ikhsanov-Nail-95@users.noreply.github.com>
2024-02-23 20:31:19 +0500

    Create SpellCaster.java

---
+++ src/main/java/faang/school/godbless/hogwarts/SpellCaster.java
@@ -0,0 +1,7 @@
+package faang.school.godbless.hogwarts;
+
+public class SpellCaster {
+    public void cast(String spellName, SpellAction spellAction) {
+        System.out.println(spellAction.toSpellAction(spellName));
+    }
+}
