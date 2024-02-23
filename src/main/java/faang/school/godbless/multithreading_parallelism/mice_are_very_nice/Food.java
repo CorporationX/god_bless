ff3436076615a33610fbefa93d9baa4de57809a5
Bukevsky | Kirill <bykaplay@gmail.com>
2024-02-23 18:29:49 +0300

    Решил задачу | Mice are very nice!

---
+++ src/main/java/faang/school/godbless/multithreading_parallelism/mice_are_very_nice/Food.java
@@ -0,0 +1,13 @@
+package faang.school.godbless.multithreading_parallelism.mice_are_very_nice;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.ToString;
+
+@AllArgsConstructor
+@Getter
+@ToString
+public class Food {
+    private String name;
+    private int amount;
+}
