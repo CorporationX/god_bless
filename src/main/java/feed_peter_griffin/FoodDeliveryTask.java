f08dc431a0915345dca349fde1bf63f52b9c7edf
Pankov Kirill <uralostrov6@gmail.com>
2024-02-23 16:15:31 +0500

    переписал рандом,  сделал костантные значения

--- src/main/java/feed_peter_griffin/FoodDeliveryTask.java
+++ src/main/java/feed_peter_griffin/FoodDeliveryTask.java
@@ -1,6 +1,7 @@
 package feed_peter_griffin;
 
 import lombok.AllArgsConstructor;
+import lombok.SneakyThrows;
 
 import java.util.Random;
 
@@ -9,17 +10,15 @@ public class FoodDeliveryTask implements Runnable {
     private String character;
     private int foodAmount;
 
+    @SneakyThrows
     @Override
     public void run() {
         String foodTypes = getFoodType();
-        System.out.printf(" %s ждёт дсотавку %d %s",character,foodAmount,foodTypes );
-        try {
-            Thread.sleep(1000L);
-        } catch (InterruptedException e) {
-            throw new RuntimeException(e);
-        }
-        System.out.println(String.format(" %s жадно съел %d %s",character,foodAmount,foodTypes));
+        System.out.printf(" %s ждёт дсотавку %d %s", character, foodAmount, foodTypes);
+        Thread.sleep(1000L);
+        System.out.println(String.format(" %s жадно съел %d %s", character, foodAmount, foodTypes));
     }
+
     private String getFoodType() {
         String[] foodTypes = {"pizza", "burger", "hot dog", "chicken wings", "taco"};
         return foodTypes[new Random().nextInt(foodTypes.length)];
--- src/main/java/feed_peter_griffin/GriffinsFoodDelivery.java
+++ src/main/java/feed_peter_griffin/GriffinsFoodDelivery.java
@@ -1,17 +1,25 @@
 package feed_peter_griffin;
 
+import lombok.SneakyThrows;
+
 import java.util.Arrays;
 import java.util.Random;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
 
 public class GriffinsFoodDelivery {
-    private static int randomNum = new Random().nextInt(10+1);
+    private static final int NUMBER_OF_EXECUTORS = 3;
+    private static final int LIMIT_RANDOM = 50;
+    private static Random randomNum = new Random();
+
+    @SneakyThrows
     public static void main(String[] args) {
 
         String[] characterNames = {"Peter", "Lois", "Meg", "Chris", "Stewie"};
-        ExecutorService executorService = Executors.newFixedThreadPool(3);
-        Arrays.stream(characterNames).forEach((character)-> executorService.submit(new FoodDeliveryTask(character,randomNum)));
+        ExecutorService executorService = Executors.newFixedThreadPool(NUMBER_OF_EXECUTORS);
+        Arrays.stream(characterNames).forEach((character) -> executorService.submit(new FoodDeliveryTask(character, randomNum.nextInt(LIMIT_RANDOM + 1))));
         executorService.shutdown();
+        executorService.awaitTermination(Long.MAX_VALUE, TimeUnit.SECONDS);
     }
 }
