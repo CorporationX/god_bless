ed096baa14568f71c738f346f3c5232121771797
Pankov Kirill <uralostrov6@gmail.com>
2024-02-24 22:57:44 +0500

    внес изменения в класс Player

--- src/main/java/spotify_blocking/Player.java
+++ src/main/java/spotify_blocking/Player.java
@@ -1,10 +1,12 @@
 package spotify_blocking;
 
+import lombok.SneakyThrows;
+
 public class Player {
     private final Object lock = new Object();
     private boolean isPlaying = false;
 
-    protected void play() {
+    public void play() {
         synchronized (lock) {
             if (!isPlaying) {
                 isPlaying = true;
@@ -14,60 +16,41 @@ public class Player {
                 } catch (InterruptedException e) {
                     throw new RuntimeException(e);
                 }
-                isPlaying = false;
-            }else{
+            } else {
                 System.out.println("музыка уже играет");
             }
         }
     }
-    protected void pause() {
+
+    public void pause() {
         synchronized (lock) {
-            if (!isPlaying) {
-                isPlaying = true;
+            if (isPlaying) {
+                isPlaying = false;
                 System.out.println("жесткий трек на паузе");
                 try {
                     Thread.sleep(1000L);
                 } catch (InterruptedException e) {
                     throw new RuntimeException(e);
                 }
-                isPlaying = false;
-            }else {
+            } else {
                 System.out.println("уже на паузе");
             }
         }
     }
 
-    protected void skip() {
+    @SneakyThrows
+    public void skip() {
         synchronized (lock) {
-            if (!isPlaying) {
-                isPlaying = true;
-                System.out.println("скипаем скучный трек");
-                try {
-                    Thread.sleep(1000L);
-                } catch (InterruptedException e) {
-                    throw new RuntimeException(e);
-                }
-                isPlaying = false;
-            }else {
-                System.out.println("ещё скипаем");
-            }
+            System.out.println("скипаем скучный трек");
+            Thread.sleep(1000L);
         }
     }
 
-    protected void previous() {
+    @SneakyThrows
+    public void previous() {
         synchronized (lock) {
-            if (!isPlaying) {
-                isPlaying = true;
-                System.out.println("включить предыдущий трек");
-                try {
-                    Thread.sleep(1000L);
-                } catch (InterruptedException e) {
-                    throw new RuntimeException(e);
-                }
-                isPlaying = false;
-            }else {
-                System.out.println("скипаем ещё один трек назад");
-            }
+            System.out.println("включить предыдущий трек");
+            Thread.sleep(1000L);
         }
     }
 }
