ab0bc4643df834e7d73c363da76cfb5de462e425
three-moon <avfedorenko3@yandex.com>
2024-02-24 21:43:12 +0300

    Хогвартс, Хогвартс, наш любимый Хогвартс

---
+++ src/main/java/faang/school/godbless/BJS2_2937/SpellAction.java
@@ -0,0 +1,6 @@
+package faang.school.godbless.BJS2_2937;
+
+@FunctionalInterface
+public interface SpellAction {
+    String act(String name);
+}
---
+++ src/main/java/faang/school/godbless/BJS2_2937/SpellCaster.java
@@ -0,0 +1,7 @@
+package faang.school.godbless.BJS2_2937;
+
+public class SpellCaster {
+    public void cast(String name, SpellAction action){
+        System.out.println(action.act(name));
+    }
+}
