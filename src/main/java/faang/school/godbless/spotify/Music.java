2c29608570d24c002dbe8640a80450abbe275283
Viktor Dyukov <snailrake@mail.ru>
2024-02-25 11:16:34 +0400

    refactor

--- src/main/java/faang/school/godbless/spotify/Music.java
+++ src/main/java/faang/school/godbless/spotify/Music.java
@@ -6,14 +6,16 @@ import java.util.concurrent.TimeUnit;
 
 public class Music {
 
+    private static final int THREAD_COUNT = 4;
+
     public static void main(String[] args) throws InterruptedException {
         Player player = new Player();
-        ExecutorService executor = Executors.newFixedThreadPool(4);
+        ExecutorService executor = Executors.newFixedThreadPool(THREAD_COUNT);
         executor.execute(player::play);
         executor.execute(player::pause);
         executor.execute(player::skip);
         executor.execute(player::previous);
         executor.shutdown();
-        executor.awaitTermination(Long.MAX_VALUE, TimeUnit.SECONDS);
+        executor.awaitTermination(5L, TimeUnit.MINUTES);
     }
 }
