81a5c021051544aae282477b8a44573e011aa8cd
Bukevsky | Kirill <bykaplay@gmail.com>
2024-02-25 17:46:19 +0300

    Решил задачу | Google Photo sync

---
+++ src/main/java/faang/school/godbless/multithreading_synchronization/google_photo/GooglePhoto.java
@@ -0,0 +1,11 @@
+package faang.school.godbless.multithreading_synchronization.google_photo;
+
+import lombok.Getter;
+
+import java.util.ArrayList;
+import java.util.List;
+@Getter
+public class GooglePhoto {
+    private List<String> photoStorage = new ArrayList<>();
+
+}
