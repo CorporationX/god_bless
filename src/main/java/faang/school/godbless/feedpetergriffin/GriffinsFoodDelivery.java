2c26617c6651ad80add576372fe605775cfae5ab
Vlad Krivonosov <krivonosov.2000@gmail.com>
2024-02-26 13:40:53 +0300

    made amendments that were indicated in the review

--- src/main/java/faang/school/godbless/feedpetergriffin/GriffinsFoodDelivery.java
+++ src/main/java/faang/school/godbless/feedpetergriffin/GriffinsFoodDelivery.java
@@ -4,17 +4,25 @@ import java.io.IOException;
 import java.util.Random;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
 
 public class GriffinsFoodDelivery {
-    public static void main(String... args) throws IOException {
-        final int numberOfThreads = 3;
+    private static Random random = new Random();
+    public static void main(String... args) throws IOException, InterruptedException {
+                final int numberOfThreads = 3;
         ExecutorService executorService = Executors.newFixedThreadPool(numberOfThreads);
         String[] characterNames = {"Peter", "Lois", "Meg", "Chris", "Stewie"};
-        int orderQuantity = new Random().nextInt(30);
-        int waitingTime = new Random().nextInt(5000);
+
         for (String character : characterNames) {
-            executorService.submit(new FoodDeliveryTask(character, orderQuantity, waitingTime));
+            executorService.submit(new FoodDeliveryTask(character, random.nextInt(30), random.nextInt(5000)));
         }
         executorService.shutdown();
+        final boolean done = executorService.awaitTermination(1, TimeUnit.MINUTES);
+        if(done){
+            System.out.println("Все задачи были выполнены");
+
+        }else {
+            System.out.println("Не все задачи были выполнены за отведенное время");
+        }
     }
 }
