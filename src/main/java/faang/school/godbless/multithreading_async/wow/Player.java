56b03fba3ad652d619d4effdf0e5de270d55f440
Bukevsky | Kirill <bykaplay@gmail.com>
2024-02-27 16:45:30 +0300

    Решил задачу | WOW

---
+++ src/main/java/faang/school/godbless/multithreading_async/wow/Player.java
@@ -0,0 +1,14 @@
+package faang.school.godbless.multithreading_async.wow;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@AllArgsConstructor
+@Getter
+@Setter
+public class Player {
+    private String name;
+    private int level;
+    private int experience;
+}
