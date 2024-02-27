847a89baacb6be8814500d625dc7acb93c714d66
Sam Migachev <sammigachev@gmail.com>
2024-02-28 00:47:39 +0300

    решил задачу Асинхронность и Future

--- src/main/java/multithreading_part2/async_and_future/MasterCardService.java
+++ src/main/java/multithreading_part2/async_and_future/MasterCardService.java
@@ -7,34 +7,37 @@ import java.util.concurrent.Executors;
 import java.util.concurrent.Future;
 
 public class MasterCardService {
-    static int collectPayment(){
-        try{
+    static int collectPayment() {
+        try {
             Thread.sleep(10000);
             return 10000;
-        }catch(InterruptedException e){
+        } catch (InterruptedException e) {
             e.printStackTrace();
             throw new RuntimeException();
         }
     }
 
-    static int sendAnalytics(){
-        try{
+    static int sendAnalytics() {
+        try {
             Thread.sleep(1000);
             return 1000;
-        }catch(InterruptedException e){
+        } catch (InterruptedException e) {
             e.printStackTrace();
             throw new RuntimeException();
         }
     }
+
     static void doAll() throws ExecutionException, InterruptedException {
-        ExecutorService executorService = Executors.newFixedThreadPool(1);
+        ExecutorService executorService = Executors.newSingleThreadExecutor();
         Future<Integer> futurePayment = executorService.submit(() -> MasterCardService.collectPayment());
         CompletableFuture<Integer> analyticsCompletableFuture = CompletableFuture.supplyAsync(() -> MasterCardService.collectPayment());
+
+        executorService.shutdown();
+
         Integer completableFuture = analyticsCompletableFuture.get();
         Integer future = futurePayment.get();
         System.out.println(completableFuture);
         System.out.println(future);
-        executorService.shutdown();
 
 
     }
