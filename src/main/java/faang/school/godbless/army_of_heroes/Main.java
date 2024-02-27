8fa01423be4e26c353cb26558ba1c62737eda5b7
Behzod <behzod.narzullo@gmail.com>
2024-02-27 19:34:54 +0100

    Распределенная армия героев меча и магии

--- src/main/java/faang/school/godbless/army_of_heroes/Main.java
+++ src/main/java/faang/school/godbless/army_of_heroes/Main.java
@@ -1,7 +1,7 @@
 package faang.school.godbless.army_of_heroes;
 
 public class Main {
-    public static void main(String[] args) throws InterruptedException {
+    public static void main(String[] args) {
         Army army = new Army();
         army.addUnit(new Archer(25));
         army.addUnit(new Swordsman(40));
