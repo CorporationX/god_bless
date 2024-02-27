970ec311f36e23dab74209ccef0bec4dd78bdfa6
Pankov Kirill <uralostrov6@gmail.com>
2024-02-27 12:39:31 +0500

    Fan out – Fan In

---
+++ src/main/java/fan_out__fan_In/ResultConsumer.java
@@ -0,0 +1,18 @@
+package fan_out__fan_In;
+
+import lombok.Getter;
+
+import java.util.concurrent.atomic.AtomicLong;
+
+@Getter
+public class ResultConsumer {
+    private final AtomicLong sumOfSquaredNumbers;
+
+    ResultConsumer(Long init) {
+        sumOfSquaredNumbers = new AtomicLong(init);
+    }
+
+    public Long add(final Long num) {
+        return sumOfSquaredNumbers.addAndGet(num);
+    }
+}
