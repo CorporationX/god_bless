eaa2712ac068e06f14263a9a2c873467ae8ead77
Sergey Ryabov <mre@mail.ru>
2024-02-27 21:41:32 +0800

    async and future

---
+++ src/main/java/faang/school/godbless/asyncAndFuture/Main.java
@@ -0,0 +1,8 @@
+package faang.school.godbless.asyncAndFuture;
+
+public class Main {
+    public static void main(String... args) {
+        MasterCardService masterCardService = new MasterCardService();
+        masterCardService.doAll();
+    }
+}
---
+++ src/main/java/faang/school/godbless/asyncAndFuture/MasterCardService.java
@@ -0,0 +1,46 @@
+package faang.school.godbless.asyncAndFuture;
+
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+import java.util.concurrent.TimeUnit;
+
+public class MasterCardService {
+    public int collectPayment() {
+        try {
+            Thread.sleep(10_000);
+            return 10_000;
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+            throw new RuntimeException();
+        }
+    }
+
+    public int sendAnalytics() {
+        try {
+            Thread.sleep(1_000);
+            return 1_000;
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+            throw new RuntimeException();
+        }
+    }
+
+    public void doAll() {
+        ExecutorService executor = Executors.newSingleThreadExecutor();
+        Future<Integer> payment = executor.submit(this::collectPayment);
+        CompletableFuture<Integer> analytics = CompletableFuture.supplyAsync(this::sendAnalytics, executor);
+
+        executor.shutdown();
+        try {
+            executor.awaitTermination(Long.MAX_VALUE, TimeUnit.SECONDS);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+
+        System.out.println("Payment: " + payment);
+        System.out.println("Analytica " + analytics);
+
+    }
+}
