95a11c4d9080539676635b4940dede8776aaafc4
Viktor Dyukov <snailrake@mail.ru>
2024-02-27 20:14:29 +0400

    Carry отожрался

---
+++ src/main/java/faang/school/godbless/carry/Inventory.java
@@ -0,0 +1,61 @@
+package faang.school.godbless.carry;
+
+import java.util.HashMap;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
+
+public class Inventory {
+
+    private final ExecutorService EXECUTOR_SERVICE = Executors.newCachedThreadPool();
+    private final HashMap<String, Item> ITEMS = new HashMap<>();
+
+    public void addItem(Item item) {
+        ITEMS.put(item.getName(), item);
+    }
+
+    public CompletableFuture<Item> getFromInventory(String itemName) {
+        return CompletableFuture.supplyAsync(() -> ITEMS.get(itemName), EXECUTOR_SERVICE);
+    }
+
+    public CompletableFuture<Item> getFromShop(String itemName) {
+        return CompletableFuture.supplyAsync(() -> new Item(itemName, 3), EXECUTOR_SERVICE);
+    }
+
+    public void combineItems(CompletableFuture<Item> item1, CompletableFuture<Item> item2) {
+        waitFutures(item1, item2);
+        item1.thenCombine(item2, this::mergeItems)
+                .thenCompose(this::putInInventory);
+    }
+
+    public void shutdownAndAwaitExecution(Long minutes) {
+        EXECUTOR_SERVICE.shutdown();
+        awaitExecution(minutes);
+    }
+
+    private Item mergeItems(Item i1, Item i2) {
+        return new Item(String.format("%s %s", i1.getName(), i2.getName()), i1.getPower() + i2.getPower());
+    }
+
+    private void waitFutures(CompletableFuture<?>... futures) {
+        CompletableFuture.allOf(futures).join();
+    }
+
+    private CompletableFuture<Item> putInInventory(Item item) {
+        return CompletableFuture.supplyAsync(() -> {
+            synchronized (ITEMS) {
+                ITEMS.put(item.getName(), item);
+            }
+            return item;
+        }, EXECUTOR_SERVICE);
+    }
+
+    private void awaitExecution(Long minutes) {
+        try {
+            EXECUTOR_SERVICE.awaitTermination(minutes, TimeUnit.MINUTES);
+        } catch (InterruptedException e) {
+            Thread.currentThread().interrupt();
+        }
+    }
+}
---
+++ src/main/java/faang/school/godbless/carry/Main.java
@@ -0,0 +1,34 @@
+package faang.school.godbless.carry;
+
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.Future;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.TimeoutException;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Inventory inventory = new Inventory();
+        inventory.addItem(new Item("Diadem", 2));
+
+        CompletableFuture<Item> item1 = inventory.getFromInventory("Diadem");
+        CompletableFuture<Item> item2 = inventory.getFromShop("Axe");
+        inventory.combineItems(item1, item2);
+
+        Item combinedItem = getFromFuture(inventory.getFromInventory("Diadem Axe"));
+        inventory.shutdownAndAwaitExecution(5L);
+        System.out.println("Combined item: " + combinedItem);
+    }
+
+    private static <T> T getFromFuture(Future<T> future) {
+        try {
+            return future.get(30L, TimeUnit.MINUTES);
+        } catch (InterruptedException e) {
+            Thread.currentThread().interrupt();
+            throw new RuntimeException(e);
+        } catch (ExecutionException | TimeoutException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
