1a7854b97991b6a738eb8609a7b369b67c197b57
three-moon <avfedorenko3@yandex.com>
2024-02-27 16:29:01 +0300

    Hotfix 1

--- src/main/java/faang/school/godbless/BJS2_3136/MailSender.java
+++ src/main/java/faang/school/godbless/BJS2_3136/MailSender.java
@@ -1,13 +1,28 @@
 package faang.school.godbless.BJS2_3136;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class MailSender {
     public static void main(String[] args) throws InterruptedException {
-        var messageList = Message.getList(1000);
+        var messageList = createMessages(1000);
+        var threadList = new ArrayList<Thread>(5);
         for (int i = 0; i < 1000; i += 200){
             Thread newThread = new Thread(new SenderRunnable(i, i+200, messageList));
             newThread.start();
-            newThread.join();
+            threadList.add(newThread);
+        }
+        for (var thread: threadList){
+            thread.join();
         }
         System.out.println("Main thread finished");
+    }
+
+    static List<Message> createMessages(int n) {
+        var messageList = new ArrayList<Message>(n);
+        for (int i = 0; i < n; ++i) {
+            messageList.add(new Message(i));
+        }
+        return messageList;
     }
 }
--- src/main/java/faang/school/godbless/BJS2_3136/Message.java
+++ src/main/java/faang/school/godbless/BJS2_3136/Message.java
@@ -3,22 +3,11 @@ package faang.school.godbless.BJS2_3136;
 import lombok.Data;
 import lombok.NonNull;
 
-import java.util.ArrayList;
-import java.util.List;
-
 @Data
 public class Message {
     @NonNull Integer id;
 
     void send() {
         System.out.println("Message " + id + " sent!");
-    }
-
-    static List<Message> getList(int n) {
-        var messageList = new ArrayList<Message>(n);
-        for (int i = 0; i < n; ++i) {
-            messageList.add(new Message(i));
-        }
-        return messageList;
     }
 }
