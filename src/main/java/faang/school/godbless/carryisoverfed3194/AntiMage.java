a8bae3250a90dbe3642c963b4280f334bb6eabc5
Dmitriy Goncharov <incoom@yandex.ru>
2024-02-28 21:40:25 +0500

    Немного рефакторинга, чтобы приятнее читать было.

--- src/main/java/faang/school/godbless/carryisoverfed3194/AntiMage.java
+++ src/main/java/faang/school/godbless/carryisoverfed3194/AntiMage.java
@@ -8,23 +8,29 @@ import java.util.concurrent.Executors;
 
 public class AntiMage {
     static final ExecutorService EXECUTOR = Executors.newCachedThreadPool();
+    static final Inventory INVENTORY = new Inventory(new ArrayList<>());
 
     public static void main(String[] args) {
-        Inventory inventory = new Inventory(new ArrayList<>());
-        CompletableFuture<Item> futureFirstItemAdded = inventory.addItem(Item.SKULL_BASHER, EXECUTOR);
-        CompletableFuture<Item> futureSecondItemAdded = inventory.addItem(Item.BUTTERFLY, EXECUTOR);
+        CompletableFuture<Item> futureFirstItemAdded = INVENTORY.addItem(Item.SKULL_BASHER, EXECUTOR);
+        CompletableFuture<Item> futureSecondItemAdded = INVENTORY.addItem(Item.BUTTERFLY, EXECUTOR);
+        futureSecondItemAdded.thenRun(() -> System.out.println("Items added. Inventory: " + INVENTORY.getItems()));
 
-        futureSecondItemAdded.thenRun(() -> System.out.println("Items added. Inventory: " + inventory.getItems()));
         futureFirstItemAdded.thenCombine(
-                        futureSecondItemAdded, (item1, item2) -> inventory.combineItems(item1, item2, EXECUTOR))
-                .thenCompose(item -> {
-                    try {
-                        return inventory.addItem(item.get(), EXECUTOR);
-                    } catch (InterruptedException | ExecutionException e) {
-                        throw new RuntimeException(e);
-                    }
-                })
-                .thenRun(() -> System.out.println("Items combined. Inventory: " + inventory.getItems()))
+                        futureSecondItemAdded, (item1, item2) -> INVENTORY.combineItems(item1, item2, EXECUTOR))
+                .thenCompose(item -> INVENTORY.addItem(getFutureItem(item), EXECUTOR))
+                .thenRun(() -> System.out.println("Items combined. Inventory: " + INVENTORY.getItems()))
                 .thenRun(EXECUTOR::shutdown);
+    }
+
+    private static Item getFutureItem(CompletableFuture<Item> futureItem) {
+        try {
+            return futureItem.get();
+        } catch (InterruptedException e) {
+            Thread.currentThread().interrupt();
+            throw new RuntimeException("Current thread was interrupted", e);
+        } catch (ExecutionException e) {
+            Throwable cause = e.getCause();
+            throw new RuntimeException("Error during async operation", cause);
+        }
     }
 }
