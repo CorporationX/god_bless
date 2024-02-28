98142844c5a3f9f59fa87f776192e5c78bec9d0c
Vlad Krivonosov <krivonosov.2000@gmail.com>
2024-02-28 14:01:23 +0300

    Made adjustments that were indicated in the review

--- src/main/java/faang/school/godbless/cleaningtheweasleyfamily/Chore.java
+++ src/main/java/faang/school/godbless/cleaningtheweasleyfamily/Chore.java
@@ -1,22 +1,20 @@
 package faang.school.godbless.cleaningtheweasleyfamily;
 
+import lombok.RequiredArgsConstructor;
+
 import java.util.Random;
 
-
+@RequiredArgsConstructor
 public class Chore implements Runnable {
-    private String chore;
-    int threadSleepingTime = new Random().nextInt(1_0000);
-
-    Chore(String chore) {
-        this.chore = chore;
-    }
+    private  final String chore;
+    private Random random = new Random();
 
     @Override
     public void run() {
 
         System.out.println("Задача " + chore + " Обрабатывается в " + Thread.currentThread().getName());
         try {
-            Thread.sleep(threadSleepingTime);
+            Thread.sleep(random.nextInt(1_0000));
         } catch (InterruptedException e) {
             throw new RuntimeException(e);
         }
