e3cb993f2cb6adebf0ba0f061689a78147915f10
kirillgutorov <gutorovvvvv@gmail.ru>
2024-02-29 09:58:02 +0700

    Gathering orders in Amazon(replaced parallelStream() with stream())

--- src/main/java/faang/school/godbless/amazon/Main.java
+++ src/main/java/faang/school/godbless/amazon/Main.java
@@ -10,7 +10,7 @@ public class Main {
     public static void main(String[] args) {
         List<Order> orders = generateOrders();
 
-        List<CompletableFuture<Void>> future = orders.parallelStream()
+        List<CompletableFuture<Void>> future = orders.stream()
                 .map(OrderProcessor::processOrder)
                 .toList();
 
