4ca80e4fdf95f037c614e672858b28363245b43d
Bukevsky | Kirill <bykaplay@gmail.com>
2024-02-29 23:40:43 +0300

    refactor

--- src/main/java/faang/school/godbless/multithreading_synchronization/youtube/VideoManager.java
+++ src/main/java/faang/school/godbless/multithreading_synchronization/youtube/VideoManager.java
@@ -10,15 +10,16 @@ public class VideoManager {
     private static final int NUM_THREADS = 100;
     private static final int NUM_VIDEOS = 20;
     private static final Map<String, Integer> viewsMap = new HashMap<>();
-    public synchronized void addView(String videoId){
+
+    public synchronized void addView(String videoId) {
         viewsMap.compute(videoId, (k, v) -> (v == null) ? 0 : v + 1);
     }
 
-    public synchronized int getViewCount(String videoId){
-        return viewsMap.get(videoId);
+    public synchronized int getViewCount(String videoId) {
+        return viewsMap.getOrDefault(videoId, 0);
     }
 
-    public void addVideo(String videoId){
+    public void addVideo(String videoId) {
         viewsMap.putIfAbsent(videoId, 0);
     }
 
@@ -26,17 +27,17 @@ public class VideoManager {
         ExecutorService executorService = Executors.newFixedThreadPool(NUM_THREADS);
         VideoManager videoManager = new VideoManager();
         for (int i = 1; i <= NUM_VIDEOS; i++) {
-            int finalI = i;
-            videoManager.addVideo(String.valueOf(i));
+            String videoId = String.valueOf(i);
+            videoManager.addVideo(videoId);
             for (int j = 0; j < NUM_THREADS / NUM_VIDEOS; j++) {
                 executorService.execute(() -> {
-                    videoManager.addView(String.valueOf(finalI));
-                    System.out.println(videoManager.getViewCount(String.valueOf(finalI)));
+                    videoManager.addView(videoId);
+                    System.out.println(videoManager.getViewCount(videoId));
                 });
             }
         }
         executorService.shutdown();
-        executorService.awaitTermination(Long.MAX_VALUE, TimeUnit.SECONDS);
+        executorService.awaitTermination(30, TimeUnit.SECONDS);
         System.out.println("Final results:");
         for (int i = 1; i <= NUM_VIDEOS; i++) {
             System.out.println("Video " + i + " views: " + videoManager.getViewCount(String.valueOf(i)));
