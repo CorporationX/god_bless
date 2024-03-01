71b5549f112e6d8d960bdc1937820af0f17dd35e
Sergey Ryabov <mre@mail.ru>
2024-03-01 20:17:27 +0800

    calculate Pi

---
+++ src/main/java/faang/school/godbless/picalculation/Main.java
@@ -0,0 +1,10 @@
+package faang.school.godbless.picalculation;
+
+public class Main {
+    public static void main(String[] args) {
+
+        System.out.println(new calculatePi().calculate(10000000));
+    }
+
+
+}
