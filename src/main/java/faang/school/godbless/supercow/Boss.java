8d6252074d336e61b5273c33baff767e8e8dd117
Behzod <behzod.narzullo@gmail.com>
2024-03-02 21:06:46 +0100

    Supercow-initial commit. Not finished

---
+++ src/main/java/faang/school/godbless/supercow/Boss.java
@@ -0,0 +1,41 @@
+package faang.school.godbless.supercow;
+
+import java.io.ObjectStreamException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Boss {
+    private int maxPlayers;
+    private int currentPlayers;
+    private List<Player> players;
+
+    public Boss(int maxPlayers) {
+        this.maxPlayers = maxPlayers;
+        this.currentPlayers = 0;
+        this.players = new ArrayList<>();
+    }
+
+    Object lock = new Object();
+
+    private void joinBattle(Player player) {
+        synchronized (lock) {
+            while (currentPlayers >= maxPlayers) {
+                try {
+                    lock.wait();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                currentPlayers++;
+                players.add(player);
+                System.out.println(player.getName() + " Joined the battle ");
+
+                currentPlayers--;
+                players.remove(player);
+                System.out.println(player.getName() + " Finished the battle");
+                lock.notify();
+
+            }
+        }
+
+    }
+}
---
+++ src/main/java/faang/school/godbless/supercow/Player.java
@@ -0,0 +1,14 @@
+package faang.school.godbless.supercow;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class Player {
+    private String name;
+
+    private void startBattle(Boss boss){
+
+    }
+}
