460ed2d35520cf2b5226ca1ba59a724c07f11e91
Vlad Krivonosov <krivonosov.2000@gmail.com>
2024-03-04 15:25:57 +0300

    made the changes indicated in the review

--- src/main/java/faang/school/godbless/spotify/Player.java
+++ src/main/java/faang/school/godbless/spotify/Player.java
@@ -1,37 +1,45 @@
 package faang.school.godbless.spotify;
 
 public class Player {
-    private final Object LOCK = new Object();
-    private Boolean isPlaying = true;
+    private Boolean isPlaying = false;
 
 
     public void play() {
-        synchronized (LOCK) {
-            this.isPlaying = true;
+        synchronized (isPlaying) {
+            if (!isPlaying) {
+                this.isPlaying = true;
+            }
             System.out.println("Воспроизведение");
             System.out.println(isPlaying);
         }
     }
 
     public void pause() {
-        synchronized (LOCK) {
-            this.isPlaying = false;
+        synchronized (isPlaying) {
+            if (isPlaying) {
+                this.isPlaying = false;
+            }
             System.out.println("Пауза");
             System.out.println(isPlaying);
         }
     }
 
     public void skip() {
-        synchronized (LOCK) {
-            this.isPlaying = true;
+        synchronized (isPlaying) {
+            if (isPlaying != true) {
+                this.isPlaying = true;
+            }
             System.out.println("Воспроизведение следующей песни");
             System.out.println(isPlaying);
         }
     }
 
+
     public void previous() {
-        synchronized (LOCK) {
-            this.isPlaying = true;
+        synchronized (isPlaying) {
+            if (isPlaying != true) {
+                this.isPlaying = true;
+            }
             System.out.println("Воспроизведение предыдущей песни");
             System.out.println(isPlaying);
         }
