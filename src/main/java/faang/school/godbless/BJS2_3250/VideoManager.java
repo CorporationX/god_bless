18030bd5a2968a9bd9618cba960eda12527b6f31
three-moon <avfedorenko3@yandex.com>
2024-03-04 16:24:04 +0300

    Fix synchronization locks

--- src/main/java/faang/school/godbless/BJS2_3250/VideoManager.java
+++ src/main/java/faang/school/godbless/BJS2_3250/VideoManager.java
@@ -1,16 +1,14 @@
 package faang.school.godbless.BJS2_3250;
 
-import lombok.SneakyThrows;
-
 import java.util.HashMap;
 import java.util.Map;
 
 public class VideoManager {
+    private final Object viewLock = new Object();
     private final Map<String, Integer> viewsMap = new HashMap<>();
 
-    @SneakyThrows
     public void addView(String videoId) {
-        synchronized (viewsMap) {
+        synchronized (viewLock) {
             if (viewsMap.containsKey(videoId)) {
                 viewsMap.put(videoId, viewsMap.get(videoId) + 1);
             } else {
@@ -20,7 +18,7 @@ public class VideoManager {
     }
 
     public int getViewCount(String videoId) {
-        synchronized (viewsMap) {
+        synchronized (viewLock) {
             if (viewsMap.containsKey(videoId)) {
                 return viewsMap.get(videoId);
             } else {
@@ -31,11 +29,7 @@ public class VideoManager {
 
     public void addVideo(String videoId) {
         synchronized (viewsMap) {
-            if (!viewsMap.containsKey(videoId)) {
-                viewsMap.put(videoId, 0);
-            } else {
-                throw new IllegalArgumentException("videoId");
-            }
+            viewsMap.put(videoId, 0);
         }
     }
 
