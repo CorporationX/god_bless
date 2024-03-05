b948c3c9c8c0195388ea52cce8c9953583106d9c
Ikhsanov Nail <ikhsanovn17@gmail.com>
2024-03-05 12:42:57 +0500

    nail09875--BJS2-3340

---
+++ src/main/java/faang/school/godbless/starwarsarena/Battle.java
@@ -0,0 +1,21 @@
+package faang.school.godbless.starwarsarena;
+
+import java.util.Random;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+
+public class Battle {
+    Random random = new Random();
+    public Future<Robot> fight(Robot robot1, Robot robot2) {
+        ExecutorService executorService = Executors.newFixedThreadPool(2);
+        Future<Robot> winnerFuture = executorService.submit(() -> {
+            Thread.sleep(1000 + random.nextInt(4000));
+            return robot1.getAttackPower() + robot1.getDefensePower() >
+                    robot2.getAttackPower() + robot2.getDefensePower() ? robot1 : robot2;
+        });
+
+        executorService.shutdown();
+        return  winnerFuture;
+    }
+}
