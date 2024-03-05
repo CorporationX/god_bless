a99505d199596062b297fa15d02977065806b700
Behzod <behzod.narzullo@gmail.com>
2024-03-05 11:23:11 +0100

    Асинхронность и Future задача final result

--- src/main/java/faang/school/godbless/async_and_future/Main.java
+++ src/main/java/faang/school/godbless/async_and_future/Main.java
@@ -2,7 +2,7 @@ package faang.school.godbless.async_and_future;
 
 public class Main {
     public static void main(String[] args) {
-        MasterCardService service = new MasterCardService(MasterCardService.collectPayment(),MasterCardService.sendAnalytics());
+        MasterCardService service = new MasterCardService(MasterCardService.collectPayment(), MasterCardService.sendAnalytics());
         service.doAll();
     }
 }
--- src/main/java/faang/school/godbless/async_and_future/MasterCardService.java
+++ src/main/java/faang/school/godbless/async_and_future/MasterCardService.java
@@ -1,20 +1,22 @@
 package faang.school.godbless.async_and_future;
 
-import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
 
 import java.util.concurrent.*;
 
-@AllArgsConstructor
 @Getter
 @Setter
 public class MasterCardService {
     private final int payment;
     private final int analytics;
 
-    public static int collectPayment() {
-        //System.out.println("Money sent " + payment);
+    public MasterCardService(int payment, int analytics) {
+        this.payment = payment;
+        this.analytics = analytics;
+    }
+
+    static int collectPayment() {
         try {
             Thread.sleep(10_000);
             return 10_000;
@@ -24,7 +26,7 @@ public class MasterCardService {
         }
     }
 
-    public static int sendAnalytics() {
+    static int sendAnalytics() {
         try {
             Thread.sleep(1_000);
             return 1_000;
@@ -36,23 +38,21 @@ public class MasterCardService {
 
     public void doAll() {
         ExecutorService executor = Executors.newSingleThreadExecutor();
-        Future<Integer> paymentFuture = executor.submit(() -> collectPayment());
-        CompletableFuture<Integer> analyticsFuture = CompletableFuture.supplyAsync(MasterCardService::sendAnalytics, executor);
-        try {
+        Future<Integer> paymentFuture = executor.submit(MasterCardService::collectPayment);
 
+        CompletableFuture<Integer> analyticsFuture = CompletableFuture.supplyAsync(MasterCardService::sendAnalytics);
 
-            CompletableFuture<Void> resultFutures = CompletableFuture.allOf((CompletableFuture<?>) paymentFuture, analyticsFuture);
-            resultFutures.join();
-            int paymentResult = paymentFuture.get();
-            System.out.println("Payment result: " + paymentResult);
-
-            int analyticsResult = analyticsFuture.get();
-            System.out.println("Analytics result: " + analyticsResult);
-        } catch (InterruptedException | ExecutionException e) {
-            e.printStackTrace();
-        } finally {
-            executor.shutdown();
-        }
-
+        CompletableFuture<Void> futuresResult = analyticsFuture
+                .thenCompose(analyticsResult -> CompletableFuture.supplyAsync(() -> {
+                    System.out.println("Analytics result: " + analyticsResult);
+                    try {
+                        System.out.println("Payment result: " + paymentFuture.get());
+                    } catch (ExecutionException | InterruptedException e) {
+                        throw new RuntimeException(e);
+                    }
+                    return null;
+                }));
+        futuresResult.join();
+        executor.shutdown();
     }
 }
