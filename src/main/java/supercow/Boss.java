1c78f16397d7dfaed76c62a871ae828a6fe5d345
Sam Migachev <sammigachev@gmail.com>
2024-03-05 22:40:46 +0300

    внес изменения

--- src/main/java/supercow/Boss.java
+++ src/main/java/supercow/Boss.java
@@ -12,17 +12,17 @@ public class Boss {
         this.maxPlayers = maxPlayers;
     }
     public synchronized void joinBattle(Player player) {
-        if (currentPlayers == maxPlayers) {
+        while (currentPlayers >= maxPlayers) {
             try{
                 System.out.println(player.getName() + " ожидает свою очередь");
                 wait();
             }catch (InterruptedException e){
                 e.printStackTrace();
             }
-        } else {
-            currentPlayers++;
-            System.out.println(player.getName() + " присоединился к игре с боссом");
         }
+        currentPlayers++;
+        System.out.println(player.getName() + " присоединился к игре с боссом");
+
     }
 
     public synchronized void endBattle(Player player) {
