23162a51748a528c791b528098ab7bf8a0f39782
nuraly <nuralysaparaliev2gmail.com>
2024-04-02 13:45:37 +0600

    BJS2-4186: Add reverse method

---
+++ src/main/java/faang/school/godbless/BJS2_4186/Main.java
@@ -0,0 +1,11 @@
+package faang.school.godbless.BJS2_4186;
+
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) {
+        int[] array = {1,2,3,4,5};
+        int[] reversedArray = Example.reverseArray(array);
+        System.out.println(Arrays.toString(reversedArray));
+    }
+}
