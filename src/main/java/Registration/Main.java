aad3a880a62874ac64e8ea4aaff30c804cef7114
Nikolai Gnidenko <gnidenkonn@gmail.com>
2024-04-02 17:01:11 +0300

    Название задачи: Регистрация пользователей. Добавлено 2 Set'а с валидными значениями работы и адреса. Добавлены проверки: На возраст (больше 18) Место работы (Set VALID_JOBS) Место проживания (Set VALID_ADDRESSES)
    Переписан main под данную задачу

---
+++ src/main/java/Registration/Main.java
@@ -0,0 +1,27 @@
+package Registration;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+public class Main {
+    public static void main(String[] args) throws IllegalAccessException {
+        List<User> users = new ArrayList<>();
+
+        User user1 = new User("Vlad", 24, "Uber", "Amsterdam");
+        users.add(user1);
+        User user2 = new User("Nick", 22, "Google", "New York");
+        users.add(user2);
+        User user3 = new User("Paul", 22, "Uber", "Amsterdam");
+        users.add(user3);
+        User user4 = new User("Stas", 19, "Amazon", "London");
+        users.add(user4);
+
+
+        for (Map.Entry<Integer, List<User>> entry : User.groupUser(users).entrySet()){
+            System.out.println("Age = " + entry.getKey()+
+                                "\nValue = " + entry.getValue().toString());
+
+        }
+    }
+}
--- src/main/java/Registration/User.java
+++ src/main/java/Registration/User.java
@@ -1,4 +1,4 @@
-package groupByAge;
+package Registration;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -8,19 +8,46 @@ import lombok.ToString;
 
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
+import java.util.Set;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 @Getter
 @Setter
 @NoArgsConstructor
-@AllArgsConstructor
 @ToString
 public class User {
     private String name;
     private int age;
     private String job;
     private String address;
+    private static Set<String> VALID_JOBS = Stream.of("Google",
+            "Uber", "Amazon").collect(Collectors.toCollection(HashSet::new));
+    private static Set<String> VALID_ADDRESS = Stream.of("London",
+            "New York", "Amsterdam").collect(Collectors.toCollection(HashSet::new));
+
+    public User(String name, int age, String job, String address) throws IllegalAccessException {
+        this.name = name;
+
+        if (age < 18) {
+            throw new IllegalAccessException();
+        } else {
+            this.age = age;
+        }
+        if (!VALID_JOBS.contains(job)) {
+            throw new IllegalAccessException();
+        } else {
+            this.job = job;
+        }
+        if (!VALID_ADDRESS.contains(address)) {
+            throw new IllegalAccessException();
+        } else {
+            this.address = address;
+        }
+    }
 
     public static Map<Integer, List<User>> groupUser(List<User> users) {
         Map<Integer, List<User>> usersGroupByAge = new HashMap<>();
