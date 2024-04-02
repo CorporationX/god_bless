a2d80f02680cf18e054b41f8f28c013e4dcb5589
Andrei Archakov <archandrwork@gmail.com>
2024-04-02 20:17:12 +0300

    BJS2-4123: small refactor

--- src/main/java/faang/school/godbless/BJS2_4123/Main.java
+++ src/main/java/faang/school/godbless/BJS2_4123/Main.java
@@ -3,14 +3,15 @@ package faang.school.godbless.BJS2_4123;
 import java.util.Arrays;
 import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 public class Main {
     public static void main(String[] args) {
         List<User> users = Arrays.asList(
-                new User(1, "Andrei", (byte) 22, new HashSet<>(List.of("a", "b", "c", "g", "e"))),
-                new User(2, "Andrei", (byte) 22, new HashSet<>(List.of("a", "c", "e")))
+                new User(1, "Andrei", (byte) 22, new HashSet<>(Set.of("a", "b", "c", "g", "e"))),
+                new User(2, "Andrei", (byte) 22, new HashSet<>(Set.of("a", "c", "e")))
         );
 
-        System.out.println(User.findHobbyLovers(users, new HashSet<>(List.of("b", "c", "e"))));
+        System.out.println(User.findHobbyLovers(users, new HashSet<>(Set.of("b", "c", "e"))));
     }
 }
