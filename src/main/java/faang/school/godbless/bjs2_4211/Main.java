571a2d178842c34288e9244d6f44b4ac3c89a7a8
ekoregin <evgkoregin@gmail.com>
2024-04-02 15:04:02 +0300

    BJS2-4211: Регистрация пользователя

--- src/main/java/faang/school/godbless/bjs2_4211/Main.java
+++ src/main/java/faang/school/godbless/bjs2_4211/Main.java
@@ -1,4 +1,4 @@
-package faang.school.godbless.bjs2_4218;
+package faang.school.godbless.bjs2_4211;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -8,11 +8,11 @@ public class Main {
     public static void main(String[] args) {
         List<User> users = new ArrayList<>();
 
-        users.add(new User("Ivanov", 28, "Cisco", "Los Angeles"));
-        users.add(new User("Petrov", 28, "Microsoft", "Berlin"));
-        users.add(new User("Sidorov", 38, "Yandex", "Moscow"));
+        users.add(new User("Ivanov", 28, "Google", "New York"));
+        users.add(new User("Petrov", 28, "Uber", "Amsterdam"));
+        users.add(new User("Sidorov", 38, "Amazon", "London"));
         users.add(new User("Limonov", 38, "Uber", "Amsterdam"));
-        users.add(new User("Scott", 21, "NASA", "Washington"));
+        users.add(new User("Scott", 21, "Google", "New York"));
 
         Map<Integer, List<User>> userGroups = User.groupUsers(users);
 
--- src/main/java/faang/school/godbless/bjs2_4211/User.java
+++ src/main/java/faang/school/godbless/bjs2_4211/User.java
@@ -1,25 +1,39 @@
-package faang.school.godbless.bjs2_4218;
+package faang.school.godbless.bjs2_4211;
 
-import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
+import java.util.Set;
 
 @Getter
 @Setter
 @ToString
-@AllArgsConstructor
 public class User {
+
+    public static final Set<String> VALID_JOBS = new HashSet<>(Arrays.asList("Google", "Uber", "Amazon"));
+    public static final Set<String> VALID_ADDRESSES = new HashSet<>(Arrays.asList("London", "New York", "Amsterdam"));
 
     private String name;
     private int age;
     private String jobPlace;
     private String address;
+
+    public User(String name, int age, String jobPlace, String address) {
+        if (name.isBlank() || name.isEmpty() || age < 18 || !VALID_JOBS.contains(jobPlace) || !VALID_ADDRESSES.contains(address)) {
+            throw new IllegalArgumentException();
+        }
+        this.name = name;
+        this.age = age;
+        this.jobPlace = jobPlace;
+        this.address = address;
+    }
 
     public static Map<Integer, List<User>> groupUsers(List<User> users) {
         Map<Integer, List<User>> groupMapUsers = new HashMap<>();
