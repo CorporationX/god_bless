e7964787a80c94e2e1c6f25a2067c0e47d254f53
Tem <tipokruto014@gmail.com>
2024-04-02 14:18:32 +0300

    а я все сделал

---
+++ src/main/java/faang/school/godbless/task_4/User.java
@@ -0,0 +1,30 @@
+package faang.school.godbless.task_4;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
+
+@Data
+@AllArgsConstructor
+public class User {
+    private long id;
+    private String name;
+    private int age;
+    private final Set<String> activities;
+
+    public static Map<User, String> findHobbyLovers(Set<User> users, Set<String> activities) {
+        Map<User, String> output = new HashMap<>();
+        for (User user : users) {
+            for (String activity : activities) {
+                if (user.getActivities().contains(activity)) {
+                    output.put(user, activity);
+                    break;
+                }
+            }
+        }
+        return output;
+    }
+}
