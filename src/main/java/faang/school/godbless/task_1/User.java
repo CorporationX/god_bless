e8197b17fbbce46cc90766ab6841140ecfa8e66c
Tem <tipokruto014@gmail.com>
2024-04-03 01:18:50 +0300

    style: до ревью я даже не замечал на сколько сильно люблю ифы без скобок...

--- src/main/java/faang/school/godbless/task_1/User.java
+++ src/main/java/faang/school/godbless/task_1/User.java
@@ -19,7 +19,9 @@ public class User {
     public static Map<Integer, List<User>> groupUsers(List<User> users) {
         Map<Integer, List<User>> map = new HashMap<>();
         for (User user : users) {
-            if (!map.containsKey(user.age)) map.put(user.age, new ArrayList<>());
+            if (!map.containsKey(user.age)) {
+                map.put(user.age, new ArrayList<>());
+            }
             map.get(user.age).add(user);
         }
         return map;
