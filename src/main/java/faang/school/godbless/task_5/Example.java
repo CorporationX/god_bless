a697c78d886335087b7d50b3798feaaa6dd00e98
Tem <tipokruto014@gmail.com>
2024-04-03 01:20:04 +0300

    style: до ревью я даже не замечал на сколько сильно люблю ифы без скобок...

--- src/main/java/faang/school/godbless/task_5/Example.java
+++ src/main/java/faang/school/godbless/task_5/Example.java
@@ -2,7 +2,9 @@ package faang.school.godbless.task_5;
 
 public class Example {
     public static void reverseInPlace(int[] nums) {
-        if (nums == null) return;
+        if (nums == null) {
+            return;
+        }
         int left = 0, right = nums.length - 1;
         while (left < right) {
             int temp = nums[right];
@@ -15,7 +17,9 @@ public class Example {
     }
 
     public static int[] reverse(int[] nums) {
-        if (nums == null) return null;
+        if (nums == null) {
+            return null;
+        }
         int[] output = new int[nums.length];
         for (int i = nums.length - 1; i >= 0; i--) {
             output[nums.length - i - 1] = nums[i];
