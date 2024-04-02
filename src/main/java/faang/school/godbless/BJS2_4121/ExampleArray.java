3c47d0bba7827a7ae32cbccfc8e9fb14499b146c
Andrei Archakov <archandrwork@gmail.com>
2024-04-02 13:32:11 +0300

    BJS2-4121: add all examples

---
+++ src/main/java/faang/school/godbless/BJS2_4121/ExampleArray.java
@@ -0,0 +1,17 @@
+package faang.school.godbless.BJS2_4121;
+
+public class ExampleArray {
+    static int[] reverse(int[] nums) {
+        int[] reversedArray = new int[nums.length];
+        int left = 0;
+        int right = nums.length - 1;
+
+        while (left < nums.length) {
+            reversedArray[left] = nums[right];
+            left++;
+            right--;
+        }
+
+        return reversedArray;
+    }
+}
--- src/main/java/faang/school/godbless/BJS2_4121/Main.java
+++ src/main/java/faang/school/godbless/BJS2_4121/Main.java
@@ -1,13 +1,28 @@
 package faang.school.godbless.BJS2_4121;
 
+import java.util.Arrays;
 import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
-        List<Integer> array = List.of(1, 2, 3, 4, 5);
-        List<Integer> reversedArray = Example.reverse(array);
+        List<Integer> arrayCollection = List.of(1, 2, 3, 4, 5);
+        List<Integer> reversedArrayCollection = ExampleCollection.reverse(arrayCollection);
 
-        System.out.println(array);
-        System.out.println(reversedArray);
+        System.out.println(arrayCollection);
+        System.out.println(reversedArrayCollection);
+        System.out.println(arrayCollection);
+
+        int[] array = {1, 2, 3, 4, 5};
+        int[] reversedArray = ExampleArray.reverse(array);
+
+        System.out.println(Arrays.toString(array));
+        System.out.println(Arrays.toString(reversedArray));
+        System.out.println(Arrays.toString(array));
+
+        int[] arrayIndicator = {1, 2, 3, 4, 5};
+
+        System.out.println(Arrays.toString(arrayIndicator));
+        System.out.println(Arrays.toString(ExampleIndicator.reverse(arrayIndicator)));
+        System.out.println(Arrays.toString(arrayIndicator));
     }
 }
