7a710933540fcd5849733e03dc20c1db5a755f6a
Ilya <ilya-ermakov.2003@yandex.ru>
2024-04-02 23:53:57 +0300

    Выполнение задачи BJS2_4266

--- src/main/java/faang/school/godbless/Application.java
+++ src/main/java/faang/school/godbless/Application.java
@@ -2,8 +2,6 @@ package faang.school.godbless;
 
 public class Application {
     public static void main(String[] args) {
- Aleksey-BJS2-4262
-        System.out.println("Book2 Complete");
-
+        System.out.println("hello world");
     }
 }
