6142a7a97fdf69832eae2f709174e63bff8a568c
KuvarzinAlex <fargus.wow@mail.ru>
2024-04-02 23:26:34 +0500

    Book2 Complete

--- src/main/java/faang/school/godbless/Application.java
+++ src/main/java/faang/school/godbless/Application.java
@@ -2,6 +2,6 @@ package faang.school.godbless;
 
 public class Application {
     public static void main(String[] args) {
-        System.out.println("Hello world7");
+        System.out.println("Book2 Complete");
     }
 }
