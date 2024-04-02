42fec41ae92051288de0dfe3479f43642ce2f02f
DimaMinin <91825998+DimaMinin@users.noreply.github.com>
2024-04-02 09:50:42 +0300

    Task: https://faang-school.atlassian.net/browse/BJS2-4183

---
+++ src/main/java/faang/school/godbless/reverse/Example.java
@@ -0,0 +1,13 @@
+package faang.school.godbless.reverse;
+
+import java.util.Collections;
+import java.util.List;
+
+public class Example {
+
+    public List<Integer> reverse(List<Integer> list) {
+        Collections.reverse(list);
+        return list;
+    }
+
+}
