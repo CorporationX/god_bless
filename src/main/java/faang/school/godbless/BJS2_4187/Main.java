563447b4c5d8a5efc843b7d4a8f5cda47d734aae
nuraly <nuralysaparaliev2gmail.com>
2024-04-02 15:43:58 +0600

    BJS2-4187: Add data validation for User class

---
+++ src/main/java/faang/school/godbless/BJS2_4187/Main.java
@@ -0,0 +1,16 @@
+package faang.school.godbless.BJS2_4187;
+
+public class Main {
+    public static void main(String[] args) {
+
+        User anotherUser = new User("", 19, "Google", "New York");
+        User tom = new User("Tom", 17, "Uber", "Amsterdam");
+        User john = new User("John", 23, "EPAM", "London");
+        User tilek = new User("Tilek", 30, "Amazon", "California");
+
+        System.out.println(anotherUser);
+        System.out.println(tom);
+        System.out.println(john);
+        System.out.println(tilek);
+    }
+}
