1778800840dd23e7178dbb23ce8d58e01a8a5261
Maxim Suhanov <maksimka_13.05.2004@mail.ru>
2024-04-03 21:18:24 +0400

    BJS2-4324

--- src/main/java/Turned/Example.java
+++ src/main/java/Turned/Example.java
@@ -1,13 +1,17 @@
 package Turned;
 
 public class Example {
+    protected static void swap(int[] arr, int num1, int num2) {
+        int variable = arr[num1];
+        arr[num1] = arr[num2];
+        arr[num2] = variable;
+    }
+
     public static void reverse(int[] arr) {
         int leftIter = 0;
         int rightIter = arr.length - 1;
         while (leftIter < rightIter) {
-            int variable = arr[leftIter];
-            arr[leftIter] = arr[rightIter];
-            arr[rightIter] = variable;
+            swap(arr, leftIter, rightIter);
             leftIter++;
             rightIter--;
         }
--- src/main/java/Turned/Main.java
+++ src/main/java/Turned/Main.java
@@ -2,7 +2,7 @@ package Turned;
 
 public class Main {
     public static void main(String[] args) {
-        int[] array = {1, 2, 3, 4, 5, 6};
+        int[] array = {1, 2, 3, 4, 5};
         for (int elem : array) {
             System.out.print(elem);
         }
