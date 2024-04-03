6483b1e1449b9a77a86b1ed7f01bece4271f1eed
DimaMinin <91825998+DimaMinin@users.noreply.github.com>
2024-04-03 10:07:50 +0300

    remove equals and hashcode from book

--- src/main/java/faang/school/godbless/library_vesteross/Book.java
+++ src/main/java/faang/school/godbless/library_vesteross/Book.java
@@ -3,26 +3,10 @@ package faang.school.godbless.library_vesteross;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 
-import java.util.Objects;
-
 @Data
 @AllArgsConstructor
 public class Book {
-
     private String title;
     private String author;
     private String year;
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        Book book = (Book) o;
-        return Objects.equals(title, book.title) && Objects.equals(author, book.author) && Objects.equals(year, book.year);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(title, author, year);
-    }
 }
