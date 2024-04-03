a1dfd8fe4bfed40b90a7d068b28bb270fe218e29
Demchenkov7 <andreydemchenkov@yandex.ru>
2024-04-03 18:37:20 +0300

    Задача: Игра престолов

---
+++ src/main/java/faang/school/godbless/vesterosslibrary/House.java
@@ -0,0 +1,12 @@
+package faang.school.godbless.vesterosslibrary;
+
+import lombok.Data;
+import lombok.NonNull;
+
+@Data
+public class House {
+    @NonNull
+    private String name;
+    @NonNull
+    private String sigil;
+}
