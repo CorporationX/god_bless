106643eb852779b7d04c2452da41ce1dd543525e
DimaMinin <91825998+DimaMinin@users.noreply.github.com>
2024-04-03 09:51:07 +0300

    create new list in method, and return it without modifying list which was gave

--- src/main/java/faang/school/godbless/reverse/Example.java
+++ src/main/java/faang/school/godbless/reverse/Example.java
@@ -1,13 +1,17 @@
 package faang.school.godbless.reverse;
 
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
 public class Example {
 
     public List<Integer> reverse(List<Integer> list) {
-        Collections.reverse(list);
-        return list;
+        List<Integer> ans = new ArrayList<>();
+        for (int i = list.size() - 1; i >= 0; i--) {
+            ans.add(list.get(i));
+        }
+        return ans;
     }
 
 }
