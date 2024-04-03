bfb55943cca71eaa47c7aecc14dbb6748f724bd5
nadir <den4ik.strelok@gmail.com>
2024-04-03 09:53:28 +0300

    Исправил ошибку с отсутствием equals() и hashcode()

--- src/main/java/faang/school/godbless/usersCollection/User.java
+++ src/main/java/faang/school/godbless/usersCollection/User.java
@@ -1,6 +1,7 @@
 package faang.school.godbless.usersCollection;
 
 import lombok.AllArgsConstructor;
+import lombok.Data;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
@@ -10,9 +11,7 @@ import java.util.List;
 import java.util.Map;
 import java.util.Set;
 
-@Getter
-@Setter
-@ToString
+@Data
 @AllArgsConstructor
 public class User {
     private int id;
