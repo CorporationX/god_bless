5d7d4f9f60da183b8b615b6ca0e28d6d6b1dac3a
nadir <den4ik.strelok@gmail.com>
2024-04-03 09:45:45 +0300

    Обновил валидацию через сеттеры и убрал мин. возраст в константу

--- src/main/java/faang/school/godbless/userRegistration/User.java
+++ src/main/java/faang/school/godbless/userRegistration/User.java
@@ -1,16 +1,16 @@
 package faang.school.godbless.userRegistration;
 
 import lombok.Getter;
-import lombok.Setter;
 import lombok.ToString;
+
 import java.util.Set;
 
 @Getter
-@Setter
 @ToString
 public class User {
     private final static Set<String> VALID_JOBS = Set.of("Google", "Uber", "Amazon");
     private final static Set<String> VALID_ADDRESSES = Set.of("London", "New York", "Amsterdam");
+    private final static int MIN_AGE = 18;
     private String name;
     private int age;
     private String job;
@@ -18,25 +18,42 @@ public class User {
 
 
     public User(String name, int age, String job, String address) {
-        if(name.isBlank()) {
+        this.setName(name);
+        this.setAge(age);
+        this.setJob(job);
+        this.setAddress(address);
+    }
+
+
+    public void setName(String name) {
+        if(name == null || name.isBlank()) {
             throw new IllegalArgumentException("имя не может быть пустым");
         }
 
-        if(age < 18) {
-            throw new IllegalArgumentException("возраст не может быть меньше 18");
+        this.name = name;
+    }
+
+    public void setAge(int age) {
+        if(age < MIN_AGE) {
+            throw new IllegalArgumentException("возраст не может быть меньше " + MIN_AGE);
         }
 
+        this.age = age;
+    }
+
+    public void setJob(String job) {
         if(!VALID_JOBS.contains(job)) {
             throw new IllegalArgumentException("неизвестное место работы");
         }
 
+        this.job = job;
+    }
+
+    public void setAddress(String address) {
         if(!VALID_ADDRESSES.contains(address)) {
             throw new IllegalArgumentException("неизвестный адрес");
         }
 
-        this.name = name;
-        this.age = age;
-        this.job = job;
         this.address = address;
     }
 }
