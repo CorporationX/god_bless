d673bd1cda29f227596d3af0566e3d5c33d63920
DimaMinin <91825998+DimaMinin@users.noreply.github.com>
2024-04-03 10:05:54 +0300

    remove equals and hashcode from user

--- src/main/java/faang/school/godbless/users_collection/User.java
+++ src/main/java/faang/school/godbless/users_collection/User.java
@@ -33,17 +33,4 @@ public class User {
         return map;
     }
 
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        User user = (User) o;
-        return age == user.age && Objects.equals(id, user.id) && Objects.equals(name, user.name) && Objects.equals(activities, user.activities);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(id, name, age, activities);
-    }
-
 }
