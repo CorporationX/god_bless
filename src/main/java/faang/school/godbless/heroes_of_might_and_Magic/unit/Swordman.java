40cf59112458e56e56aa01e85fd21bac9642906e
DimaMinin <91825998+DimaMinin@users.noreply.github.com>
2024-04-03 21:56:16 +0300

    Task: https://faang-school.atlassian.net/browse/BJS2-4381

---
+++ src/main/java/faang/school/godbless/heroes_of_might_and_Magic/unit/Swordman.java
@@ -0,0 +1,18 @@
+package faang.school.godbless.heroes_of_might_and_Magic.unit;
+
+public class Swordman extends Creature {
+
+    private static final int ATTACK = 1;
+    private static final int DEFENSE = 55;
+    private static final int SPEED = 3;
+
+    public Swordman(String name, int level, int count) {
+        super(name, level, ATTACK, DEFENSE, SPEED, count);
+    }
+
+    @Override
+    public int getDamage() {
+        return this.getAttack() + this.getSpeed() + this.getLevel();
+    }
+
+}
