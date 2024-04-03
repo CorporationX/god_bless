a23e682fe080bc68081886493515c6697a1ce580
Andrei Archakov <archandrwork@gmail.com>
2024-04-03 13:08:11 +0300

    BJS2-4122: make changes with review

--- src/main/java/faang/school/godbless/BJS2_4122/Main.java
+++ src/main/java/faang/school/godbless/BJS2_4122/Main.java
@@ -10,16 +10,16 @@ import java.util.List;
 public class Main {
     public static void main(String[] args) {
         List<User> users = Arrays.asList(
-                new User("Andrei", (byte) 22, Job.AMAZON, Address.AMSTERDAM),
-                new User("Victor", (byte) 20, Job.UBER, Address.NEW_YORK),
-                new User("Vlad", (byte) 21, Job.AMAZON, Address.LONDON),
-                new User("Jack", (byte) 21, Job.GOOGLE, Address.NEW_YORK),
-                new User("Nick", (byte) 23, Job.GOOGLE, Address.LONDON)
+                new User("Andrei", 22, Job.AMAZON, Address.AMSTERDAM),
+                new User("Victor", 20, Job.UBER, Address.NEW_YORK),
+                new User("Vlad", 21, Job.AMAZON, Address.LONDON),
+                new User("Jack", 21, Job.GOOGLE, Address.NEW_YORK),
+                new User("Nick", 23, Job.GOOGLE, Address.LONDON)
         );
 
-        User Jack = new User("Jack", (byte) 21, Job.UBER, Address.AMSTERDAM);
+        User Jack = new User("Jack", 21, Job.UBER, Address.AMSTERDAM);
         Jack.setJob(Job.AMAZON);
-        Jack.setAge((byte) 18);
+        Jack.setAge(18);
         Jack.setName("Andrei");
         Jack.setAddress(Address.LONDON);
 
--- src/main/java/faang/school/godbless/BJS2_4122/models/User.java
+++ src/main/java/faang/school/godbless/BJS2_4122/models/User.java
@@ -3,27 +3,25 @@ package faang.school.godbless.BJS2_4122.models;
 import faang.school.godbless.BJS2_4122.enums.Address;
 import faang.school.godbless.BJS2_4122.enums.Job;
 import lombok.Getter;
-import lombok.Setter;
 
 import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
 
 @Getter
-@Setter
 public class User {
+    private static final int AGE_MAJORITY = 18;
+    private static final Set<Job> VALID_JOBS = Set.of(Job.GOOGLE, Job.UBER, Job.AMAZON);
+    private static final Set<Address> VALID_ADDRESSES = Set.of(Address.LONDON, Address.NEW_YORK, Address.AMSTERDAM);
+
     private String name;
-    private byte age;
+    private int age;
     private Job job;
     private Address address;
 
-    private static final Set<Job> VALID_JOBS = new HashSet<>(Set.of(Job.GOOGLE, Job.UBER, Job.AMAZON));
-    private static final Set<Address> VALID_ADDRESSES = new HashSet<>(Set.of(Address.LONDON, Address.NEW_YORK, Address.AMSTERDAM));
-
-    public User(String name, byte age, Job job, Address address) {
+    public User(String name, int age, Job job, Address address) {
         this.setName(name);
         this.setAge(age);
         this.setJob(job);
@@ -46,8 +44,8 @@ public class User {
         this.address = address;
     }
 
-    public void setAge(Byte age) {
-        if (age < 18) {
+    public void setAge(int age) {
+        if (age < AGE_MAJORITY) {
             throw new IllegalArgumentException("The age must be at least 18");
         }
 
@@ -62,8 +60,8 @@ public class User {
         this.name = name;
     }
 
-    public static Map<Byte, List<User>> groupUsersByAge(List<User> users) {
-        Map<Byte, List<User>> result = new HashMap<>();
+    public static Map<Integer, List<User>> groupUsersByAge(List<User> users) {
+        Map<Integer, List<User>> result = new HashMap<>();
 
         for (User user : users) {
             result.computeIfAbsent(user.getAge(), k -> new ArrayList<>()).add(user);
