5059757be429b687198f7deb8d25a98db373c485
Andrei Archakov <archandrwork@gmail.com>
2024-04-03 13:20:35 +0300

    BJS2-4123: make changes with review

--- src/main/java/faang/school/godbless/BJS2_4123/Main.java
+++ src/main/java/faang/school/godbless/BJS2_4123/Main.java
@@ -8,8 +8,8 @@ import java.util.Set;
 public class Main {
     public static void main(String[] args) {
         List<User> users = Arrays.asList(
-                new User(1, "Andrei", (byte) 22, new HashSet<>(Set.of("a", "b", "c", "g", "e"))),
-                new User(2, "Andrei", (byte) 22, new HashSet<>(Set.of("a", "c", "e")))
+                new User(1, "Andrei", 22, new HashSet<>(Set.of("a", "b", "c", "g", "e"))),
+                new User(2, "Andrei", 22, new HashSet<>(Set.of("a", "c", "e")))
         );
 
         System.out.println(User.findHobbyLovers(users, new HashSet<>(Set.of("b", "c", "e"))));
--- src/main/java/faang/school/godbless/BJS2_4123/User.java
+++ src/main/java/faang/school/godbless/BJS2_4123/User.java
@@ -1,21 +1,19 @@
 package faang.school.godbless.BJS2_4123;
 
 import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.Setter;
+import lombok.Data;
 
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
 
-@Getter
-@Setter
+@Data
 @AllArgsConstructor
 public class User {
     private int id;
     private String name;
-    private byte age;
+    private int age;
     private Set<String> hobbies;
 
     static Map<User, String> findHobbyLovers(List<User> users, Set<String> hobbies) {
