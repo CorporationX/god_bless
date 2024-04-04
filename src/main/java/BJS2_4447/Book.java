743b0e2b0a16caa2aaf667f0d83be2b1051da3df
Ilya <ilya-ermakov.2003@yandex.ru>
2024-04-04 15:16:37 +0300

    Выполнение задачи BJS2_4447

---
+++ src/main/java/BJS2_4447/Book.java
@@ -0,0 +1,12 @@
+package BJS2_4447;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class Book {
+    private String title;
+    private String author;
+    private int year;
+}
