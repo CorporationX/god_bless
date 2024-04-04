f865682269801a2c627cd3a73e1b82aebff8e54c
Rick <RickHammerson@ya.ru>
2024-04-04 12:58:07 +0300

    Рефактор по сокращению строчек кода

--- src/main/java/faang/school/godbless/module/core/group/Main.java
+++ src/main/java/faang/school/godbless/module/core/group/Main.java
@@ -1,7 +1,6 @@
 package faang.school.godbless.module.core.group;
 
 import java.util.List;
-import java.util.Map;
 
 public class Main {
     public static void main(String[] args) {
@@ -9,7 +8,6 @@ public class Main {
         User vova = new User("Володька", 31, "Фрезировщик", "Улица Пушкина");
         User lilya = new User("Лилия", 31, "Прогер", "Улица Пушкина");
         
-        Map<Integer, List<User>> usersGroupedByAge = User.groupUsers(List.of(ivan, vova, lilya));
-        System.out.println(usersGroupedByAge);
+        System.out.println(User.groupUsers(List.of(ivan, vova, lilya)));
     }
 }
