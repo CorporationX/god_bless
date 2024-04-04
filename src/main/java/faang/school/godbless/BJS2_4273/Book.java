e84b517cbf1213526d7d237fffe335913e95ddc0
nuraly <nuralysaparaliev2gmail.com>
2024-04-04 14:56:32 +0600

    BJS2-4273: Библиотека Вестеросса

---
+++ src/main/java/faang/school/godbless/BJS2_4273/Book.java
@@ -0,0 +1,18 @@
+package faang.school.godbless.BJS2_4273;
+
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.ToString;
+
+@Getter
+@EqualsAndHashCode
+@AllArgsConstructor
+@ToString
+public class Book {
+
+    private String title;
+    private String author;
+    private int year;
+
+}
