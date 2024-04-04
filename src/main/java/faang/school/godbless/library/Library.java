d3f58fc0f15ed5d2a3c27179392f971f42c27d38
object-petit-c <konishhev96@gmail.com>
2024-04-04 11:02:38 +0300

    Задача: Библиотека Вестероса

---
+++ src/main/java/faang/school/godbless/library/Library.java
@@ -0,0 +1,43 @@
+package faang.school.godbless.library;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
+
+public class Library {
+    HashMap<Book, String> library = new HashMap<>();
+
+    // добавление новой книги и её местонахождения в библиотеке;
+    public void addBookToPlace(String bookTitle, String bookAuthor, int bookYear, String bookPlace) {
+        library.put(new Book(bookTitle, bookAuthor, bookYear), bookPlace);
+    }
+
+    // удаление книги по её названию, автору и году издания;
+    public void deleteByAttributes(String bookTitle, String bookAuthor, int bookYear) {
+        Book bookToDelete = new Book(bookTitle, bookAuthor, bookYear);
+        if (library.containsKey(bookToDelete)) {
+            library.remove(bookToDelete);
+        } else {
+            throw new IllegalArgumentException("No such book in library");
+        }
+    }
+
+    // поиск книги и вывода информации о её местонахождении в библиотеке,
+    // используя название книги, автора и год издания;
+    public void searchPlaceByAttributes(String bookTitle, String bookAuthor, int bookYear) {
+        Book desiredBook = new Book(bookTitle, bookAuthor, bookYear);
+        if (library.containsKey(desiredBook)) {
+            System.out.println("Указанная книга лежит в " + library.get(desiredBook));
+        } else {
+            throw new IllegalArgumentException("No such book in library");
+        }
+    }
+
+    // вывод списка всех книг и их местонахождения в библиотеке
+    public void displayAllBooksAndPlaces() {
+        Set<Map.Entry<Book, String>> booksAndPlaces = library.entrySet();
+        for (Map.Entry<Book, String> bookAndPlace : booksAndPlaces) {
+            System.out.println(bookAndPlace.getKey() + " lays in " + bookAndPlace.getValue());
+        }
+    }
+}
---
+++ src/main/java/faang/school/godbless/library/Main.java
@@ -0,0 +1,23 @@
+package faang.school.godbless.library;
+
+
+public class Main {
+    public static void main(String[] args) {
+        Library library = new Library();
+
+        library.addBookToPlace("War and peace", "Tolstoy", 1830, "First row");
+        library.addBookToPlace("A game of Thrones", "Martin", 1996, "Second row");
+        library.addBookToPlace("Clean code", "Martin", 2008, "Taken");
+
+        library.displayAllBooksAndPlaces();
+
+        library.deleteByAttributes("War and peace", "Tolstoy", 1830);
+
+        library.displayAllBooksAndPlaces();
+
+        library.searchPlaceByAttributes("Clean code", "Martin", 2008);
+
+
+
+    }
+}
