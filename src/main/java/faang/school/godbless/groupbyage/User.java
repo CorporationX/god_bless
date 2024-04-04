9234953a3e2e9cfe903015fb4a58a964d15b3482
Demchenkov7 <andreydemchenkov@yandex.ru>
2024-04-04 12:23:01 +0300

    Добавил недостающие поля и перенес метод groupUsers в класс User

--- src/main/java/faang/school/godbless/groupbyage/User.java
+++ src/main/java/faang/school/godbless/groupbyage/User.java
@@ -5,12 +5,35 @@ import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 @AllArgsConstructor
 @ToString
 @Getter
 @Setter
 public class User {
 
+    private String name;
     private int age;
+    private String jobPlace;
+    private String address;
+
+    public Map<Integer, List<User>> groupUsers(List<User> users) {
+        Map<Integer, List<User>> userMap = new HashMap<>();
+
+        for (User user : users) {
+
+            if (!userMap.containsKey(user.getAge())) {
+                userMap.put(user.getAge(), new ArrayList<>());
+            }
+
+            userMap.get(user.getAge()).add(user);
+        }
+
+        return userMap;
+    }
 
 }
