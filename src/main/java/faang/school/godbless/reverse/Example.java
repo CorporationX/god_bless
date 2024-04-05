91ee4cd6023136d0598bbd65779e7402856824e0
Aleksey <lekha.bolt2004@gmail.com>
2024-04-05 13:34:25 +0300

    Reverse!

---
+++ src/main/java/faang/school/godbless/reverse/Example.java
@@ -0,0 +1,10 @@
+package faang.school.godbless.reverse;
+
+import java.util.Arrays;
+import java.util.Collections;
+
+public class Example {
+    static void reverse(Integer[] arr){
+        Collections.reverse(Arrays.asList(arr));
+    }
+}
---
+++ src/main/java/faang/school/godbless/reverse/Main.java
@@ -0,0 +1,12 @@
+package faang.school.godbless.reverse;
+
+import java.util.Arrays;
+
+
+public class Main {
+    public static void main(String[] args) {
+        Integer[] arr = {1, 2, 3, 4, 5};
+        Example.reverse(arr);
+        System.out.println(Arrays.toString(arr));
+    }
+}
