224fde7379a0215d4c6ccce4a0ee39a64670e3db
Vadim Merkulov <vm.merkulov@gmail.com>
2024-04-06 00:52:14 +0300

    BJS2-4528

--- src/main/java/faang/school/godbless/third_solution/User.java
+++ src/main/java/faang/school/godbless/third_solution/User.java
@@ -1,18 +1,39 @@
 package faang.school.godbless.third_solution;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
-
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 @Data
+@AllArgsConstructor
 public class User {
     private int id;
     private String name;
     private int age;
     private List<String> activities;
 
+    public static void main(String[] args) {
+        List<User> userList = new ArrayList<>();
+        userList.add(new User(1, "Vadim", 22, Arrays.asList("run", "jump", "swim")));
+        userList.add(new User(2, "Dmitry", 56, Arrays.asList("play", "jump", "work")));
+        userList.add(new User(3, "Igor", 34, Arrays.asList("run", "work", "swim")));
+        userList.add(new User(4, "Radislav", 42, Arrays.asList("play", "cry")));
+        userList.add(new User(5, "Andrey", 33, Arrays.asList()));
 
+        List<String> possible = new ArrayList<>(Arrays.asList("work", "cry"));
 
+        findHobbyLovers(userList, possible);
 
+    }
+
+    public static void findHobbyLovers(List<User> users, List<String> possible_activities) {
+        Map<User, String> usersMap = new HashMap<>();
+        for (User user : users) {
+            for (String activity : user.getActivities()) {
+                if (possible_activities.contains(activity)) {
+                    usersMap.put(user, activity);
+                }
+            }
+        }
+    }
 }
