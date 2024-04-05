8ceffbadd65d1a2cfb8e169551f0c8f23b9f8ac5
Maxim Suhanov <maksimka_13.05.2004@mail.ru>
2024-04-05 16:18:38 +0400

    BJS2-4462-_makakcuk_-Оптимизируем работу дата-центра

--- src/main/java/Optimise/EnergyEfficencyOptimizationStrategy.java
+++ src/main/java/Optimise/EnergyEfficencyOptimizationStrategy.java
@@ -3,6 +3,6 @@ package Optimise;
 public class EnergyEfficencyOptimizationStrategy implements OptimizationStrategy{
     @Override
     public void optimize(DataCenter dataCenter) {
-        System.out.println("Оптимизация энергопотребления");//не совсем понял, как реализовать этот метод
+        System.out.println("Оптимизация энергопотребления");//не совсем понял, как реализовать этот метод иначе
     }
 }
--- src/main/java/Optimise/LoadBalancingOptimizationStrategy.java
+++ src/main/java/Optimise/LoadBalancingOptimizationStrategy.java
@@ -3,6 +3,6 @@ package Optimise;
 public class LoadBalancingOptimizationStrategy implements OptimizationStrategy{
     @Override
     public void optimize(DataCenter dataCenter) {
-        System.out.println("Оптимизация нагрузки");//не совсем понял, как реализовать этот метод
+        System.out.println("Оптимизация нагрузки");//не совсем понял, как реализовать этот метод иначе
     }
 }
