18238bb0d24158042d09c607289e792d5edafb9b
Vadim Merkulov <vm.merkulov@gmail.com>
2024-04-05 11:55:41 +0300

    -

---
+++ src/main/java/faang/school/godbless/third_solution/User.java
@@ -0,0 +1,18 @@
+package faang.school.godbless.third_solution;
+
+import lombok.Data;
+
+import java.util.List;
+import java.util.Map;
+
+@Data
+public class User {
+    private int id;
+    private String name;
+    private int age;
+    private List<String> activities;
+
+
+
+
+}
