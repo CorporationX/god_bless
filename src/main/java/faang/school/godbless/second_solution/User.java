ca0eb9d3a6ef48d30d13c654df4f70a23150d664
Vadim Merkulov <vm.merkulov@gmail.com>
2024-04-05 11:41:59 +0300

    BJS2-4516 Исправленная версия

--- src/main/java/faang/school/godbless/second_solution/User.java
+++ src/main/java/faang/school/godbless/second_solution/User.java
@@ -2,15 +2,13 @@ package faang.school.godbless.second_solution;
 
 import lombok.Data;
 import lombok.NoArgsConstructor;
-
-
 import java.util.*;
 
 @Data
 @NoArgsConstructor
 public class User {
     public static void main(String[] args) {
-
+        User user1 = new User("Vadim", 22, "Uber", "London");
     }
 
     private static final Set<String> VALID_JOBS = new HashSet<>(Arrays.asList("Google", "Uber", "Amazon"));
@@ -21,26 +19,25 @@ public class User {
     private String address;
 
     public User(String name, int age, String job, String address) {
-        if (name != null
-                && age >= 18
-                && VALID_JOBS.contains(job)
-                && VALID_ADDRESSES.contains(address)) {
-            this.name = name;
-            this.age = age;
-            this.job = job;
-            this.address = address;
-        } else {
-            throw new IllegalArgumentException();
-        }
-
+        userValid(name, age, job, address);
+        this.name = name;
+        this.age = age;
+        this.job = job;
+        this.address = address;
     }
 
-    public Map<Integer, User> groupUsers(List<User> users) {
-        Map<Integer, User> usersMap = new HashMap<>();
-        for (int i = 0; i < users.size(); i++) {
-            usersMap.put(users.get(i).getAge(), users.get(i));
+    public void userValid(String name, int age, String job, String address) {
+        if (name.isEmpty()) {
+            throw new IllegalArgumentException("Отсутствует имя");
         }
-        return usersMap;
+        if (age < 18) {
+            throw new IllegalArgumentException("Нет 18 лет");
+        }
+        if (!VALID_JOBS.contains(job)) {
+            throw new IllegalArgumentException("Места работы нет в списке");
+        }
+        if (!VALID_ADDRESSES.contains(address)) {
+            throw new IllegalArgumentException("Адреса нет в списке");
+        }
     }
-
 }
