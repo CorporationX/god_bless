79557a3da32134b610aaf08102d251b30d251699
romantarelicev <romuhut@mail.ru>
2024-04-05 19:02:36 +0300

    решил задачу Группировка пользователей по возрасту - BJS2-4556

---
+++ src/main/java/usersGrouping/User.java
@@ -0,0 +1,34 @@
+package usersGrouping;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@AllArgsConstructor
+@Getter
+@Setter
+@ToString
+public class User {
+    private String name;
+    private int age;
+    private String workAddress;
+    private String address;
+
+    public static Map<Integer, List<User>> groupUsers(List<User> users) {
+        Map<Integer, List<User>> groupedUsers = new HashMap<>();
+        for (User user : users) {
+            if (!groupedUsers.containsKey(user.getAge())) {
+                groupedUsers.put(user.getAge(), new ArrayList<>());
+            }
+            groupedUsers.get(user.getAge()).add(user);
+        }
+        return groupedUsers;
+    }
+
+}
