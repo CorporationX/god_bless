0e2840743addc3e91fae17b2f7ee18726ae3e8f2
nadir <den4ik.strelok@gmail.com>
2024-04-05 19:27:16 +0300

    исправил замечания

--- src/main/java/faang/school/godbless/javaHashMap/proguly/Main.java
+++ src/main/java/faang/school/godbless/javaHashMap/proguly/Main.java
@@ -6,10 +6,10 @@ import java.util.List;
 import java.util.Map;
 
 public class Main {
-    private List<Student> studentBase;
+    private static List<Student> studentBase;
 
-    public Main() {
-        this.studentBase = new ArrayList<>();
+    public static void main(String[] args) {
+        studentBase = new ArrayList<>();
 
         studentBase.add(new Student("Oleg", "IT", 3));
         studentBase.add(new Student("Vasya", "IT", 3));
@@ -18,42 +18,38 @@ public class Main {
         studentBase.add(new Student("Alex", "Economics", 1));
         studentBase.add(new Student("Danil", "Phys-tech", 4));
         studentBase.add(new Student("Anna", "Pharma", 4));
-    }
-
-    public static void main(String[] args) {
-        Main studentManager = new Main();
 
         System.out.println("StudentManager test.\n");
 
         System.out.println("Student base at start point:");
-        studentManager.printGroupedStudents();
+        printGroupedStudents();
 
 
         System.out.println("\nAdding new student:");
-        studentManager.addNewStudent("Denis", "IT", 4);
+        addNewStudent("Denis", "IT", 4);
 
         System.out.println("Student base after that:");
-        studentManager.printGroupedStudents();
+        printGroupedStudents();
 
 
         System.out.println("\nRemoving a student (Ruslan):");
-        studentManager.removeStudent("Ruslan", "Economics", 1);
+        removeStudent("Ruslan", "Economics", 1);
 
         System.out.println("Student base after that:");
-        studentManager.printGroupedStudents();
+        printGroupedStudents();
 
 
         System.out.println("\nStudents of IT faculty 4th year:");
-        studentManager.getStudentsByFacultyAndYear("IT", 4).forEach(System.out::println);
+        getStudentsByFacultyAndYear("IT", 4).forEach(System.out::println);
 
 
         System.out.println("Done.");
     }
 
-    public Map<StudentKey, List<Student>> groupStudents(List<Student> studentList) {
+    public static Map<StudentKey, List<Student>> groupStudents(List<Student> studentList) {
         Map<StudentKey, List<Student>> groupedStudents = new HashMap<>();
 
-        for(Student student : studentList) {
+        for (Student student : studentList) {
             StudentKey currentKey = new StudentKey(student.getFaculty(), student.getYear());
             groupedStudents.computeIfAbsent(currentKey, key -> new ArrayList<>()).add(student);
         }
@@ -61,36 +57,21 @@ public class Main {
         return groupedStudents;
     }
 
-    public void addNewStudent(String name, String faculty, int year) {
-        this.studentBase.add(new Student(name, faculty, year));
+    public static void addNewStudent(String name, String faculty, int year) {
+        studentBase.add(new Student(name, faculty, year));
     }
 
-    public void removeStudent(String name, String faculty, int year) {
-        Student studentToBeRemoved = getStudentFromBase(name, faculty, year);
+    public static void removeStudent(String name, String faculty, int year) {
+        Student studentToBeRemoved = new Student(name, faculty, year);
 
-        if(studentToBeRemoved == null) {
-            System.out.println("No such student in the StudentBase!");
-
-            return;
-        }
-
-        this.studentBase.remove(studentToBeRemoved);
+        studentBase.remove(studentToBeRemoved);
     }
 
-    public List<Student> getStudentsByFacultyAndYear(String faculty, int year) {
-        return groupStudents(this.studentBase).getOrDefault(new StudentKey(faculty, year), null);
+    public static List<Student> getStudentsByFacultyAndYear(String faculty, int year) {
+        return groupStudents(studentBase).getOrDefault(new StudentKey(faculty, year), null);
     }
 
-    public void printGroupedStudents() {
+    public static void printGroupedStudents() {
         groupStudents(studentBase).entrySet().forEach(System.out::println);
-    }
-
-    private Student getStudentFromBase(String name, String faculty, int year) {
-        for(Student student : this.studentBase) {
-            if(student.equals(new Student(name, faculty, year))) {
-                return student;
-            }
-        }
-        return null;
     }
 }
