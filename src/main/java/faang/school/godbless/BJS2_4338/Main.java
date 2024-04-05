64935496c7505552e65afc84470ae3d81581d455
Kayfral <79108276+Kayfral@users.noreply.github.com>
2024-04-05 06:57:23 +0300

    Задание: Регистрация пользователя
    Исправлено два класса:
    
    Main: Убрана логика вывода пользователей.
    
    User: Создан метод CheckUserParameters, который проверяет параметры для заполняемых полей.

--- src/main/java/faang/school/godbless/BJS2_4338/Main.java
+++ src/main/java/faang/school/godbless/BJS2_4338/Main.java
@@ -2,7 +2,7 @@ package faang.school.godbless.BJS2_4338;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Map;
+
 
 public class Main {
     public static void main(String[] args){
@@ -11,14 +11,5 @@ public class Main {
         users.add(new User("Anton", 20, "Google", "London"));
         users.add(new User("Andrey", 19, "Uber", "New York"));
         users.add(new User("Ekaterina", 20, "Amazon", "Amsterdam"));
-
-        Map<Integer, List<User>> sortedUserMap = User.groupUsers(users);
-
-        for (Map.Entry<Integer,List<User>> entry : sortedUserMap.entrySet()){
-            System.out.println("Age: " + entry.getKey());
-            for (User user: entry.getValue()){
-                System.out.println(user);
-            }
-        }
     }
 }
--- src/main/java/faang/school/godbless/BJS2_4338/User.java
+++ src/main/java/faang/school/godbless/BJS2_4338/User.java
@@ -5,41 +5,33 @@ import java.util.*;
 
 @Data
 public class User {
+    private static final Set<String> VALID_JOBS = Set.of("Google", "Uber", "Amazon");
+    private static final Set<String> VALID_ADDRESSES = Set.of("London", "New York", "Amsterdam");
     private String name;
     private int age;
     private String workPlace;
     private String address;
-    private static final Set<String> VALID_JOBS = Set.of("Google", "Uber", "Amazon");
-    private static final Set<String> VALID_ADDRESSES = Set.of("London", "New York", "Amsterdam");
-
-    public static Map<Integer, List<User>> groupUsers(List<User> usersList) {
-        Map<Integer, List<User>> userGroups = new HashMap<>();
-
-        for (User user : usersList) {
-            if (!userGroups.containsKey(user.getAge())) {
-                userGroups.put(user.getAge(), new ArrayList<>());
-            }
-            userGroups.get(user.getAge()).add(user);
-        }
-        return userGroups;
-    }
 
     public User(String name, int age, String workPlace, String address) {
+        CheckUserParameters(name, age, workPlace, address);
+
         this.name = name;
         this.age = age;
         this.workPlace = workPlace;
         this.address = address;
+    }
 
-        if (this.name.isEmpty()){
+    private void CheckUserParameters(String name, int age, String workPlace, String address){
+        if (name.isEmpty()){
             throw new IllegalArgumentException("Имя не может быть пустым");
         }
-        if (this.age < 18){
+        if (age < 18){
             throw new IllegalArgumentException("Возраст не может быть меньше 18");
         }
-        if (!VALID_JOBS.contains(this.workPlace)){
+        if (!VALID_JOBS.contains(workPlace)){
             throw new IllegalArgumentException("Место работы отсутствует во множестве");
         }
-        if (!VALID_ADDRESSES.contains(this.address)){
+        if (!VALID_ADDRESSES.contains(address)){
             throw new IllegalArgumentException("Адрес отсутствует во множестве");
         }
     }
