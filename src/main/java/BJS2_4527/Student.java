b34a39cd3ce890fe0967a96e5e50dcd17c602e95
Ilya <ilya-ermakov.2003@yandex.ru>
2024-04-05 14:31:45 +0300

    BJS2-4527: Считаем прогулы

---
+++ src/main/java/BJS2_4527/Student.java
@@ -0,0 +1,12 @@
+package BJS2_4527;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class Student {
+    private String name;
+    private String faculty;
+    private int year;
+}
