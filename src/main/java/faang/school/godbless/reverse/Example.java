e49cdd3b3e77a66c69e3cab7b8066cb92c9160e3
Th0rn-dev <solnek33@gmail.com>
2024-04-05 09:17:39 +0300

    fix check is array is not null

--- src/main/java/faang/school/godbless/reverse/Example.java
+++ src/main/java/faang/school/godbless/reverse/Example.java
@@ -1,12 +1,17 @@
 package faang.school.godbless.reverse;
 
+import java.util.Objects;
+
 class Example {
     public int[] reverse(int[] nums) {
-        int size = nums.length;
-        int[] reversed = new int[size];
-        for (int i = 0; i < size; i++) {
-            reversed[size - i -1] = nums[i];
+        if (Objects.nonNull(nums)) {
+            int size = nums.length;
+            int[] reversed = new int[size];
+            for (int i = 0; i < size; i++) {
+                reversed[size - i -1] = nums[i];
+            }
+            return reversed;
         }
-        return reversed;
+        return new int[0];
     }
 }
