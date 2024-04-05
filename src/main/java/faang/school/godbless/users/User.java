5ece1570e35975c5741117a57dc02d39ccd13fa8
Th0rn-dev <solnek33@gmail.com>
2024-04-05 20:45:08 +0300

    Added new logic for registration of users

--- src/main/java/faang/school/godbless/users/User.java
+++ src/main/java/faang/school/godbless/users/User.java
@@ -1,6 +1,5 @@
 package faang.school.godbless.users;
 
-import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -8,17 +7,65 @@ import java.util.HashMap;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
+import java.util.Objects;
 import java.util.Set;
 
 
 @Getter
 @Setter
-@AllArgsConstructor
 public class User {
+    public static Set<String> VALID_JOBS = new HashSet<>() {{
+        add("Google");
+        add("Uber");
+        add("Amazon");
+    }};
+
+    public static Set<String> VALID_ADDRESSES = new HashSet<>() {{
+        add("London");
+        add("New York");
+        add("Amsterdam");
+    }};
+
     private int id;
     private String name;
     private int age;
     private Set<String> activity;
+    private String address;
+    private String placeOfWork;
+
+
+    public User(int id, String name, int age, Set<String> activity, String address, String placeOfWork) {
+        this.id = id;
+        if (Objects.isNull(name)) {
+            throw new IllegalArgumentException("Field name must be not null!");
+        } else {
+            this.name = name;
+        }
+
+        if (age < 18) {
+            throw new IllegalArgumentException("Age must be less 18 year!");
+        } else {
+            this.age = age;
+        }
+
+        if (!VALID_ADDRESSES.contains(address)) {
+            throw new IllegalArgumentException(
+                    "Еhe address must contain one of the following cities: " +
+                            "London, New York, Amsterdam."
+            );
+        } else {
+            this.address = address;
+        }
+
+        if (!VALID_JOBS.contains(placeOfWork)) {
+            throw new IllegalArgumentException(
+                    "The place of work must be one of the companies: " +
+                            "Google, Uber, Amazon"
+            );
+        } else {
+            this.placeOfWork = placeOfWork;
+        }
+    }
 
     public static Map<User, String> findHobbyLovers(List<User> users, Set<String> activities) {
         Map<User, String> hm = new HashMap<>();
@@ -34,3 +81,4 @@ public class User {
         return hm;
     }
 }
+
