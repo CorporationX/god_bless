68c06cda46d45e880014d782b738ad5aa681d35e
Aleksey <lekha.bolt2004@gmail.com>
2024-04-05 10:24:10 +0300

    Развернитесь! Решил задачу

---
+++ src/main/java/faang/school/godbless/task_1/Main.java
@@ -0,0 +1,14 @@
+package faang.school.godbless.task_1;
+
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) {
+        Integer[] arr = {1, 2, 3, 4, 5};
+        Example.reverse(arr);
+//        for (int i: arr){
+//            System.out.println(i);
+//        }
+        System.out.println(Arrays.toString(arr));
+    }
+}
