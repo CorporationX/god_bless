525529325513a86672ac01b22425a0a071b6b11f
Th0rn-dev <solnek33@gmail.com>
2024-04-06 00:07:41 +0300

    fix problem

--- src/main/java/faang/school/godbless/users/Main.java
+++ src/main/java/faang/school/godbless/users/Main.java
@@ -2,44 +2,26 @@ package faang.school.godbless.users;
 
 import java.util.Arrays;
 import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
 
 public class Main {
     public static void main(String[] args) {
+        Set<String> activities = new HashSet<>(Arrays.asList("eat", "sleep", "kite surfing"));
 
-        User user1 = new User(
-                1,
-                "Tom",
-                19,
-                new HashSet<>(Arrays.asList("yoga", "run")),
-                "London",
-                "Google"
+        List<User> users = Arrays.asList(
+                new User(1, "Tom", 15, new HashSet<>(Arrays.asList("yoga", "run"))),
+                new User(2, "Jon", 11, new HashSet<>(Arrays.asList("velo", "kite surfing"))),
+                new User(3, "Ann", 23, new HashSet<>(Arrays.asList("travel", "moto"))),
+                new User(4, "Liza", 12, new HashSet<>(Arrays.asList("eat", "PC games"))),
+                new User(5, "Pol", 17, new HashSet<>(Arrays.asList("board game", "PC games")))
         );
-        // Кинет исключение из-за адреса
-        User user2 = new User(
-                2,
-                "Jon",
-                22,
-                new HashSet<>(Arrays.asList("velo", "kite surfing")),
-                "Oslo",
-                "Uber"
-        );
-        // Кинет исключение из-за возраста
-        User user3 = new User(
-                3,
-                "Ann",
-                17,
-                new HashSet<>(Arrays.asList("travel", "moto")),
-                "Amsterdam",
-                "Amazon"
-        );
-        // Кинет исключение из-за места работы
-        User user4 = new User(
-                4,
-                "Liza",
-                25,
-                new HashSet<>(Arrays.asList("eat", "PC games")),
-                "New York",
-                "Epic Games"
-        );
+
+        Map<User, String> usersActivity = User.findHobbyLovers(users, activities);
+
+        for (Map.Entry<User, String> en : usersActivity.entrySet()) {
+            System.out.println("User " + en.getKey().getName() + " has activity " + en.getValue());
+        }
     }
 }
