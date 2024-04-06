6a5b2424a11ef720bb521d78d0f2f251f1362a9a
s.kolbasov <s.kolbasov@bftcom.com>
2024-04-06 20:05:32 +0300

    BJS2-4626 Java HashMap: Библиотека Вестеросса

---
+++ src/main/java/faang/school/godbless/Book.java
@@ -0,0 +1,12 @@
+package faang.school.godbless;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class Book {
+  private String title;
+  private String author;
+  private String year;
+}
