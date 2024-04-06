bfb39df608227d9a1e3ed79940bc2444d72a7e63
ethiqque <danielpyld.work@gmail.com>
2024-04-06 07:40:04 +0300

    BJS2-4312 Java HashMap: Индекс запросов ELK

---
+++ src/main/java/faang/school/godbless/User.java
@@ -0,0 +1,11 @@
+package faang.school.godbless;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class User {
+    private int id;
+    private String name;
+}
