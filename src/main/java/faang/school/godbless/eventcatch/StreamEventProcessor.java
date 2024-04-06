4cec803191c456a24737b6daabea33f426731523
object-petit-c <konishhev96@gmail.com>
2024-04-06 14:41:32 +0300

    Задача: Ловим события
    исправил название переменной для потока по типу, сменил тернарный оператор на if-else в методе добавления нового события

--- src/main/java/faang/school/godbless/eventcatch/StreamEventProcessor.java
+++ src/main/java/faang/school/godbless/eventcatch/StreamEventProcessor.java
@@ -4,7 +4,7 @@ import java.util.*;
 
 public class StreamEventProcessor {
     private HashMap<Integer, StreamEvent> streamById = new HashMap<>();
-    private HashMap<String, List<StreamEvent>> streamByType = new HashMap<>();
+    private HashMap<String, List<StreamEvent>> streamsByType = new HashMap<>();
 
     // добавления нового события потока и обновления обоих HashMap;
     public void addNewEvent(StreamEvent streamEvent) {
@@ -13,10 +13,14 @@ public class StreamEventProcessor {
         }
         streamById.put(streamEvent.getId(), streamEvent);
 
-        List<StreamEvent> list = streamByType.containsKey(streamEvent.getEventType()) ?
-                streamByType.get(streamEvent.getEventType()) : new ArrayList<>();
+        List<StreamEvent> list;
+        if (streamsByType.containsKey(streamEvent.getEventType())) {
+            list = streamsByType.get(streamEvent.getEventType());
+        } else {
+            list = new ArrayList<>();
+        }
         list.add(streamEvent);
-        streamByType.put(streamEvent.getEventType(), list);
+        streamsByType.put(streamEvent.getEventType(), list);
     }
 
     // поиска события потока по его ID;
@@ -29,10 +33,10 @@ public class StreamEventProcessor {
 
     // поиска списка событий потока по типу события;
     public List<StreamEvent> findByType(String type) {
-        if (!streamByType.containsKey(type)) {
+        if (!streamsByType.containsKey(type)) {
             throw new IllegalArgumentException("Незарегистрированный тип события");
         }
-        return streamByType.get(type);
+        return streamsByType.get(type);
     }
 
     // удаления события потока по его ID и обновления обоих HashMap.
@@ -42,21 +46,21 @@ public class StreamEventProcessor {
         }
 
         StreamEvent event = streamById.get(id);
-        List<StreamEvent> listOfEvents = streamByType.get(event.getEventType());
+        List<StreamEvent> listOfEvents = streamsByType.get(event.getEventType());
 
         streamById.remove(id);
         listOfEvents.remove(event);
         if (listOfEvents.isEmpty()) {
-            streamByType.remove(event.getEventType());
+            streamsByType.remove(event.getEventType());
         } else {
-            streamByType.put(event.getEventType(), listOfEvents);
+            streamsByType.put(event.getEventType(), listOfEvents);
         }
     }
 
     //Реализуйте метод для вывода информации о всех событиях потока, используя обход массива Entry внутри HashMap.
     // Выведите ID событий, типы событий и данные событий.
     public void showAllEvents() {
-        Set<Map.Entry<String, List<StreamEvent>>> entrySet = streamByType.entrySet();
+        Set<Map.Entry<String, List<StreamEvent>>> entrySet = streamsByType.entrySet();
         entrySet.stream()
                 .map(Map.Entry::getValue)
                 .forEach(System.out::println);
