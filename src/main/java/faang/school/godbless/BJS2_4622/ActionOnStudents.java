2f5028e4fcf39be918c7cf221980523abcfc3494
Kayfral <79108276+Kayfral@users.noreply.github.com>
2024-04-06 19:54:23 +0300

    Задание: Считаем прогулы BJS2-4622
    Создан алгоритм для создания, удаления, поиска и вывода информации о студентах на различных курсах

---
+++ src/main/java/faang/school/godbless/BJS2_4622/ActionOnStudents.java
@@ -0,0 +1,62 @@
+package faang.school.godbless.BJS2_4622;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class ActionOnStudents {
+    public static Map<String, List<Student>> groupedStudents(List<Student> students){
+        Map<String, List<Student>> groupedStudents = new HashMap<>();
+
+        for (Student student : students){
+            String key = student.getFaculty() + "-" + student.getYear();
+            if(!groupedStudents.containsKey(key)){
+                groupedStudents.put(key, new ArrayList<>());
+            }
+            groupedStudents.get(key).add(student);
+        }
+
+        return groupedStudents;
+    }
+
+    public static void addNewStudent(String name, String faculty, int year, List<Student> studentsList){
+        studentsList.add((new Student(name, faculty, year)));
+    }
+
+    public static void deleteStudent(String name, String faculty, int year, List<Student> studentsList){
+        Map<String, List<Student>> groupedStudents = groupedStudents(studentsList);
+        String key = faculty + "-" + year;
+        List<Student> studentsFromFaculty = groupedStudents.get(key);
+
+        for (Student student : studentsFromFaculty){
+            if(student.getName().equals(name)){
+                studentsList.remove(student);
+            }
+        }
+    }
+
+    public static void searchAllStudents(String faculty, int year, List<Student> studentsList){
+        Map<String, List<Student>> groupedStudents = groupedStudents(studentsList);
+        String key = faculty + "-" + year;
+        List<Student> studentsFromFaculty = groupedStudents.get(key);
+
+        System.out.println("Студенты: " + faculty + " - " + year);
+        for (Student student : studentsFromFaculty){
+            System.out.println(student.getName());
+        }
+    }
+
+    public static void printAllStudents(List<Student> studentsList){
+        Map<String, List<Student>> groupedStudents = groupedStudents(studentsList);
+
+        for (Map.Entry<String,List<Student>> entry : groupedStudents.entrySet()){
+            System.out.println("Факультет: " + entry.getKey());
+
+            for (Student student: entry.getValue()){
+                System.out.println(student.getName());
+            }
+            System.out.print("\n");
+        }
+    }
+}
