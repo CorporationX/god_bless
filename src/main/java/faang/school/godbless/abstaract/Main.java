c9efa02e17d3bbe5023b8cc5babcfca787329c8b
Roman Antonov <romzesvxo@mail.ru>
2024-04-06 22:08:14 +0300

    BJS2-4647 Абстракция

---
+++ src/main/java/faang/school/godbless/abstaract/Main.java
@@ -0,0 +1,18 @@
+package faang.school.godbless.abstaract;
+
+public class Main {
+    public static void main(String[] args) {
+        Warrior warrior1 = new Warrior("W_1");
+        Archer archer1 = new Archer("A_1");
+
+        System.out.println("Здоровье до боя W_1: " + warrior1.health);
+        System.out.println("Здоровье до боя A_1: " + archer1.health);
+
+        warrior1.attack(archer1);
+        archer1.attack(warrior1);
+
+        System.out.println("Здоровье после боя W_1: " + warrior1.health);
+        System.out.println("Здоровье после боя A_1: " + archer1.health);
+
+    }
+}
---
+++ src/main/java/faang/school/godbless/abstaract/Warrior.java
@@ -0,0 +1,12 @@
+package faang.school.godbless.abstaract;
+
+public class Warrior extends Character{
+    public Warrior(String name) {
+        super(name, 10, 5, 3);
+    }
+
+    @Override
+    public void attack(Character enemy) {
+        enemy.health -= this.strength;
+    }
+}
