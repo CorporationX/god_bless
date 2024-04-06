22684f4419f5be0f8c18015ce38de0259b96b038
romantarelicev <romuhut@mail.ru>
2024-04-06 21:32:27 +0300

    Исправил решение  "Коллекционируем пользователей" - BJS2-4555

--- src/main/java/users_registartion/Main.java
+++ src/main/java/users_registartion/Main.java
@@ -6,16 +6,7 @@ import java.util.Map;
 
 public class Main {
     public static void main(String[] args) {
-        List<User> usersList = new ArrayList<>();
-        usersList.add(new User("John", 33, "Google", "New York"));
-        usersList.add(new User("Bob", 26, "Amazon", "Berlin"));
-        usersList.add(new User("Amely", 19, "Yandex", "Saint-Petersburg"));
-        usersList.add(new User("Maria", 26, "Ozon", "Moscow"));
-        usersList.add(new User("Sarah", 33, "Apple", "California"));
-        usersList.add(new User("Petr", 19, "Yandex", "Moscow"));
-
-        for (Map.Entry<Integer, List<User>> entry : User.groupUsers(usersList).entrySet()) {
-            System.out.println(entry.getKey() + " -> " + entry.getValue());
-        }
+        User john = new User("John", 33, "Google", "New York");
+        User bob = new User("Bob", 26, "Amazon", "Berlin");
     }
 }
