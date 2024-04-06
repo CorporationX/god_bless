c5eeae77f57c0d8a8c55b8e296200aa25bfc8ddf
TOPIKS <esaakyan03@gmail.com>
2024-04-06 18:50:47 +0300

    BSJ2-4297: с изменениями

--- src/main/java/faang/school/godbless/Main.java
+++ src/main/java/faang/school/godbless/Main.java
@@ -1,7 +1,6 @@
 package faang.school.godbless;
 
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
--- src/main/java/faang/school/godbless/User.java
+++ src/main/java/faang/school/godbless/User.java
@@ -1,60 +1,34 @@
 package faang.school.godbless;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+@AllArgsConstructor
+@Getter
 public class User {
     private String name;
     private int age;
     private String placeWork;
     private String address;
 
-    User(String name, int age, String placeWork, String address) {
-        this.name = name;
-        this.age = age;
-        this.placeWork = placeWork;
-        this.address = address;
-    }
 
-    public static Map<Integer, List<User>> groupUsers(List<User> userList) {
-        Map<Integer, List<User>> groupUser = new HashMap<>();
+    public static Map<Integer, List<User>> groupUsers(List<User> users) {
+        Map<Integer, List<User>> map = new HashMap<>();
 
-        for (User user : userList) {
+        for (User user : users) {
             int key = user.getAge();
-            if (!groupUser.containsKey(key)) {
-                groupUser.put(key, new ArrayList<>());
+            if (!map.containsKey(key)) {
+                map.put(key, new ArrayList<>());
             }
-            groupUser.get(key).add(user);
+            map.get(key).add(user);
         }
-        return groupUser;
+        return map;
 
 
     }
 
-    public String getName() {
-        return this.name;
-    }
-    public int getAge() {
-        return this.age;
-    }
-    public String getPlaceWork() {
-        return this.placeWork;
-    }
-    public String getAddress() {
-        return this.address;
-    }
-    public void setName(int age) {
-        this.age = age;
-    }
-    public void setAge(int age) {
-        this.age = age;
-    }
-    public void setPlaceWork(int age) {
-        this.age = age;
-    }
-    public void setAddress(int age) {
-        this.age = age;
-    }
 }
