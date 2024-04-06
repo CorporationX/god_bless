a533f733b7cd90f4994139e82817decb53f90fe2
mihanchik94 <mihanchik94@gmail.com>
2024-04-06 22:40:16 +0300

    Java HashMap: Игра престолов

---
+++ src/main/java/faang/school/godbless/hashMap/task_1/model/House.java
@@ -0,0 +1,13 @@
+package faang.school.godbless.hashMap.task_1.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.ToString;
+
+@AllArgsConstructor
+@Getter
+@ToString
+public class House {
+    private final String name;
+    private final String sigil;
+}
