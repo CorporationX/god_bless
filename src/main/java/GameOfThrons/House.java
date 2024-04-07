d56b2b34f57161ec9b285c2b89c1f09561ce197f
romantarelicev <romuhut@mail.ru>
2024-04-07 22:26:47 +0300

    Поправил задание, отправляю PR №2 Java HashMap: Игра престолов - BJS2-4704

--- src/main/java/GameOfThrons/House.java
+++ src/main/java/GameOfThrons/House.java
@@ -1,28 +1,15 @@
 package GameOfThrons;
 
 import lombok.AllArgsConstructor;
+import lombok.Data;
 import lombok.Getter;
 import lombok.ToString;
 
 import java.util.Objects;
 
+@Data
 @AllArgsConstructor
-@Getter
-@ToString
 public class House {
     private String name;
     private String sigil;
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        House house = (House) o;
-        return Objects.equals(name, house.name) && Objects.equals(sigil, house.sigil);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(name, sigil);
-    }
 }
