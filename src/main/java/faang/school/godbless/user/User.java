6f11f77b8ad7002941e0e0a13bea8614a93b8a03
egorp5119 <egorplo@gmail.com>
2024-04-07 13:25:16 +0300

    Регистрация пользователя

---
+++ src/main/java/faang/school/godbless/user/User.java
@@ -0,0 +1,65 @@
+package faang.school.godbless.user;
+
+import lombok.Data;
+import lombok.Getter;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
+@Getter
+@Data
+public class User {
+    private String name;
+    private int age;
+    private String job;
+    private String address;
+
+    private static final Set<String> VALID_JOBS = Set.of("Google", "Uber", "Amazon");
+    private static final Set<String> VALID_ADDRESSES = Set.of("London", "New York", "Amsterdam");
+
+    public void setName(String name) {
+        if (name.isBlank()) {
+            throw new IllegalArgumentException("Имя не может быть пустым");
+        } else {
+            this.name = name;
+        }
+    }
+
+    public void setAge(int age) {
+        if (age < 18) {
+            throw new IllegalArgumentException("Возраст не может быть меньше 18");
+        } else {
+            this.age = age;
+        }
+    }
+
+    public void setJob(String job) {
+        if (!VALID_JOBS.contains(job)) {
+            throw new IllegalArgumentException("Место работы должно содержаться во множестве VALID_JOBS");
+        } else {
+            this.job = job;
+        }
+    }
+
+    public void setAddress(String address) {
+        if (!VALID_ADDRESSES.contains(address)) {
+            throw new IllegalArgumentException("Адрес должен содержаться во множестве VALID_ADDRESSES");
+        } else {
+            this.address = address;
+        }
+    }
+
+    public static Map<Integer, List<User>> groupUsers(List<User> users) {
+        Map<Integer, List<User>> groupedUsers = new HashMap<>();
+        for (User user : users) {
+            if (!groupedUsers.containsKey(user.getAge())) {
+                groupedUsers.put(user.getAge(), new ArrayList<>());
+            }
+            groupedUsers.get(user.getAge()).add(user);
+        }
+        return groupedUsers;
+    }
+}
