8baf9b281fe36bc5de0baafadbdf394516c315d7
Liza Yudelevich <elizaveta.fr@gmail.com>
2024-04-07 14:42:41 +0300

    Made corrections in solution for task BJS2-4507.

--- src/main/java/faang/school/godbless/userRegistration/User.java
+++ src/main/java/faang/school/godbless/userRegistration/User.java
@@ -1,11 +1,8 @@
 package faang.school.godbless.userRegistration;
 
 import lombok.Data;
-import java.util.ArrayList;
-import java.util.HashMap;
+
 import java.util.HashSet;
-import java.util.List;
-import java.util.Map;
 import java.util.Set;
 
 @Data
@@ -18,43 +15,25 @@ public class User {
     private String address;
 
     public User(String name, int age, String job, String address) {
+        validateFields(name, age, job, address);
+        this.name = name;
+        this.age = age;
+        this.job = job;
+        this.address = address;
+    }
+
+    public static void validateFields(String name, int age, String job, String address) {
         if (name == null || name.trim().isEmpty()) {
             throw new IllegalArgumentException("Имя не может быть пустым.");
-        } else {
-            this.name = name;
         }
         if (age < 18) {
             throw new IllegalArgumentException("Возраст не может быть меньше 18.");
-        } else {
-            this.age = age;
         }
         if (!VALID_JOBS.contains(job)) {
             throw new IllegalArgumentException("Место работы должно содержаться во множестве VALID_JOBS.");
-        } else {
-            this.job = job;
         }
         if (!VALID_ADDRESSES.contains(address)) {
             throw new IllegalArgumentException("Адрес должен содержаться во множестве VALID_ADDRESSES.");
-        } else {
-            this.address = address;
         }
-    }
-
-
-    public static Map<Integer, List<User>> groupUsers(List<User> users) {
-        Map<Integer, List<User>> groupedUsers = new HashMap<>();
-        for (User user : users) {
-            int age = user.getAge();
-            if (groupedUsers.containsKey(age)) {
-                groupedUsers.get(age).add(user);
-            } else {
-                List<User> usersWithSameAge = new ArrayList<>();
-                usersWithSameAge.add(user);
-                groupedUsers.put(age, usersWithSameAge);
-            }
-        }
-        return groupedUsers;
     }
 }
-
-
