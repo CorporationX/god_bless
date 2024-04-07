0404e710155126f52aa259ca2cc7e9fd1207a513
Liza Yudelevich <elizaveta.fr@gmail.com>
2024-04-07 13:39:21 +0300

    Add solution for task BJS2-4034.

---
+++ src/main/java/faang/school/godbless/countingAbsenteeisc/Student.java
@@ -0,0 +1,11 @@
+package faang.school.godbless.countingAbsenteeisc;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class Student {
+    private String name;
+    private String faculty;
+    private int year;
+}
