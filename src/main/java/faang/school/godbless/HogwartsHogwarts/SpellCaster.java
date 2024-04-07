ea8cb192623c359a790281d4883e914b54fc83c2
Tem <tipokruto014@gmail.com>
2024-04-08 00:57:54 +0300

    а я все сделал

---
+++ src/main/java/faang/school/godbless/HogwartsHogwarts/SpellCaster.java
@@ -0,0 +1,7 @@
+package faang.school.godbless.HogwartsHogwarts;
+
+public class SpellCaster {
+    public void cast(String words, SpellAction action) {
+        System.out.println(action.cast(words));
+    }
+}
