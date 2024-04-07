938e425955a2c60e2f3480cc1082aee76d9173a3
Tamzhid Babutsyngueva <btamzhid@gmail.com>
2024-04-07 18:11:00 +0800

    Группировка пользователей по возрасту. Реализовано 2 способа (подробнее в комментарие в Jira)

---
+++ src/main/java/GroupingUsers/Main.java
@@ -0,0 +1,29 @@
+package GroupingUsers;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class Main {
+    public static void main(String[] args) {
+        User userSanje = new User("Sanje", 20);
+        User userMarina = new User("Marina", 18);
+        User userDorjy = new User("Dorjy", 16);
+        User userDashy = new User("Dashy", 16);
+        User userBayasana = new User("Bayasana", 11);
+        User userAnya = new User("Anya", 11);
+        User userDari = new User("Dari", 11);
+
+        List<User> usersList = new ArrayList<>();
+        usersList.add(userSanje);
+        usersList.add(userMarina);
+        usersList.add(userDorjy);
+        usersList.add(userDashy);
+        usersList.add(userBayasana);
+        usersList.add(userAnya);
+        usersList.add(userDari);
+
+        System.out.println(User.groupUsers(usersList));
+    }
+}
