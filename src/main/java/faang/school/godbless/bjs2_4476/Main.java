5689f959d4986e41b219752ce691b5ed8292d666
ekoregin <evgkoregin@gmail.com>
2024-04-07 12:29:32 +0300

    BJS2-4476: Fixed 1 after review

--- src/main/java/faang/school/godbless/bjs2_4476/Main.java
+++ src/main/java/faang/school/godbless/bjs2_4476/Main.java
@@ -1,28 +1,31 @@
 package faang.school.godbless.bjs2_4476;
 
+import lombok.extern.slf4j.Slf4j;
+
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Map;
+import java.util.NoSuchElementException;
 import java.util.Set;
 
+@Slf4j
 public class Main {
 
     private static final Map<String, Set<WebPage>> webPageMap = new HashMap<>();
+    private static final String CONTENT1 = "Java — строго типизированный объектно-ориентированный язык программирования общего назначения, " +
+            "разработанный компанией Sun Microsystems. Разработка ведётся сообществом, организованным через Java Community Process; " +
+            "язык и основные реализующие его технологии распространяются по лицензии GPL";
+    private static final String CONTENT2 = "Приложения Java обычно транслируются в специальный байт-код, поэтому они могут работать на любой компьютерной архитектуре";
 
     public static void main(String[] args) {
-        String content = "Java — строго типизированный объектно-ориентированный язык программирования общего назначения, " +
-                "разработанный компанией Sun Microsystems. Разработка ведётся сообществом, организованным через Java Community Process; " +
-                "язык и основные реализующие его технологии распространяются по лицензии GPL";
-        String content2 = "Приложения Java обычно транслируются в специальный байт-код, поэтому они могут работать на любой компьютерной архитектуре";
-        WebPage page = new WebPage("http://test.page", "Test page", content);
-        WebPage page2 = new WebPage("http://test2.page", "Test page 2", content2);
+        WebPage page = new WebPage("http://test.page", "Test page", CONTENT1);
+        WebPage page2 = new WebPage("http://test2.page", "Test page 2", CONTENT2);
         indexWebPage(page);
         indexWebPage(page2);
         getPagesByKeyWord("Java").forEach(System.out::println);
-
-        System.out.println("\nRemove test2.page from index");
         removePageByUrl("http://test2.page");
         getPagesByKeyWord("Java").forEach(System.out::println);
+        getPagesByKeyWord("Python");
     }
 
     public static void indexWebPage(WebPage webPage) {
@@ -36,10 +39,16 @@ public class Main {
     }
 
     public static Set<WebPage> getPagesByKeyWord(String keyWord) {
-        return webPageMap.get(keyWord);
+        Set<WebPage> foundPages = webPageMap.get(keyWord);
+        if (foundPages == null) {
+            log.info("Pages by key word: '{}' not found!", keyWord);
+            throw new NoSuchElementException("Pages by key word: '" + keyWord + "' not found!");
+        }
+        return foundPages;
     }
 
     public static void removePageByUrl(String url) {
+        log.info("Remove page: '{}' from index", url);
         webPageMap.values().forEach(set -> set.removeIf(page -> page.getUrl().equals(url)));
     }
 }
