018a009289845d361730a465d093c791edcc4c2c
Tem <tipokruto014@gmail.com>
2024-04-08 01:24:48 +0300

    а я все сделал

---
+++ src/main/java/InstagramFilters/FilterProcessor.java
@@ -0,0 +1,19 @@
+package InstagramFilters;
+
+import java.util.function.Function;
+
+public class FilterProcessor {
+    public Image applyFilter(Image image, Function<Image, Image> function) {
+        if (image == null || function == null) {
+            return null;
+        }
+        return function.apply(image);
+    }
+
+    public Function<Image, Image> combineFilters(Function<Image, Image> firstFunction, Function<Image, Image> secondFunction) {
+        if (firstFunction == null || secondFunction == null) {
+            return null;
+        }
+        return firstFunction.andThen(secondFunction);
+    }
+}
