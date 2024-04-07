ca8edcebe804f2bbb2084ba805719a78071589d6
Alex Vishnevskiy <vishgioall@gmail.com>
2024-04-07 15:57:01 +0300

    внес изменения после ревью, исправил именование переменных

--- src/main/java/faang/school/godbless/BJS2_4625/Example.java
+++ src/main/java/faang/school/godbless/BJS2_4625/Example.java
@@ -1,13 +1,12 @@
 package faang.school.godbless.BJS2_4625;
 
 public class Example {
-    static public int[] reverse(int[] integerList) {
-        for (int i = 0; i < integerList.length / 2; i++) {
-            int temp = integerList[i];
-            integerList[i] = integerList[integerList.length - i - 1];
-            integerList[integerList.length - i - 1] = temp;
+    static public int[] reverse(int[] numbers) {
+        for (int i = 0; i < numbers.length / 2; i++) {
+            int temp = numbers[i];
+            numbers[i] = numbers[numbers.length - i - 1];
+            numbers[numbers.length - i - 1] = temp;
         }
-
-        return integerList;
+        return numbers;
     }
 }
--- src/main/java/faang/school/godbless/BJS2_4625/Main.java
+++ src/main/java/faang/school/godbless/BJS2_4625/Main.java
@@ -1,16 +1,12 @@
 package faang.school.godbless.BJS2_4625;
 
+import java.util.Arrays;
+
 public class Main {
 
     public static void main(String[] args) {
 
-
         int[] arr = Example.reverse(new int[]{1, 2, 3});
-
-        for (int i : arr) {
-            System.out.println(i);
-        }
+        System.out.println(Arrays.toString(arr));
     }
-
-
 }
