1cb5518d7653b78386a4a79f13d434a65173ff34
Alex Vishnevskiy <vishgioall@gmail.com>
2024-04-07 20:19:32 +0300

    создал класс House, написал в нем методы для работы с HashMap

---
+++ src/main/java/faang/school/godbless/BJS2_4950/Main.java
@@ -0,0 +1,16 @@
+package faang.school.godbless.BJS2_4950;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Main {
+    public static void main(String[] args) {
+        Map<Book, String> books = new HashMap<>();
+
+        Book atlasShrugged = new Book("Atlas shrugged", "Ayn Rand", 1957);
+        Book toKillMockingbird = new Book("To kill mockingbird", "Harper Lee", 1960);
+        Book iRobot = new Book("I Robot", "Isaac Asimov", 1950);
+
+
+    }
+}
