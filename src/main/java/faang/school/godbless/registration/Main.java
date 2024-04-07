ea0658a17dee93e6a278e662b1b5e0935cf290ee
Roman Antonov <romzesvxo@mail.ru>
2024-04-07 15:49:33 +0300

    BJS2-4678 Исправил недочёты, кроме метода groupUsers Оно есть взадании, почему не относится к задаче не понял

--- src/main/java/faang/school/godbless/registration/Main.java
+++ src/main/java/faang/school/godbless/registration/Main.java
@@ -1,6 +1,7 @@
 package faang.school.godbless.registration;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
@@ -11,7 +12,18 @@ public class Main {
         users.add(new User("Ann", 23, "Google", "London"));
         users.add(new User("Mary", 21, "Amazon", "Amsterdam"));
         users.add(new User("Ted", 28, "Vk", "Moscow"));
-        Map<Integer, List<User>> group = User.groupUsers(users);
+        Map<Integer, List<User>> group = groupUsers(users);
         System.out.println(group);
+    }
+    public static Map<Integer, List<User>> groupUsers(List<User> users) {
+        Map<Integer, List<User>> group = new HashMap<>();
+        for (User user : users) {
+            int age = user.getAge();
+            if (!group.containsKey(age)) {
+                group.put(age, new ArrayList<>());
+            }
+            group.get(age).add(user);
+        }
+        return group;
     }
 }
--- src/main/java/faang/school/godbless/registration/User.java
+++ src/main/java/faang/school/godbless/registration/User.java
@@ -1,22 +1,34 @@
 package faang.school.godbless.registration;
 
+import lombok.ToString;
+
 import java.util.*;
 
+@ToString
 public class User {
+    private static final Set<String> VALID_JOBS =
+            new HashSet<>(Arrays.asList("Google", "Uber", "Amazon"));
+    private static final Set<String> VALID_ADDRESSES =
+            new HashSet<>(Arrays.asList("London", "New York", "Amsterdam"));
+    private static final int MIN_AGE = 18;
     private String name;
     private int age;
     private String job;
     private String address;
-    private static final List<String> VALID_JOBS =
-            new ArrayList<>(Arrays.asList("Google", "Uber", "Amazon"));
-    private static final List<String> VALID_ADDRESSES =
-            new ArrayList<>(Arrays.asList("London", "New York", "Amsterdam"));
 
     public User(String name, int age, String job, String address) {
-        if (name.isEmpty()) {
+        condition(name, age, job, address);
+        this.name = name;
+        this.age = age;
+        this.job = job;
+        this.address = address;
+    }
+
+    private void condition(String name, int age, String job, String address) {
+        if (name.isEmpty() || name == null) {
             throw new IllegalArgumentException("Name cannot be empty");
         }
-        if (age < 18) {
+        if (age < MIN_AGE) {
             throw new IllegalArgumentException("Age must be at least 18");
         }
         if (!VALID_JOBS.contains(job)) {
@@ -25,32 +37,9 @@ public class User {
         if (!VALID_ADDRESSES.contains(address)) {
             throw new IllegalArgumentException("Address is not valid");
         }
-
-//        boolean condition = name.isEmpty() || age < 18 || !VALID_JOBS.contains(job) || !VALID_ADDRESSES.contains(address);
-//        if (condition){
-//            throw new IllegalArgumentException();
-//        }
-
-        this.name = name;
-        this.age = age;
-        this.job = job;
-        this.address = address;
     }
 
-    public static Map<Integer, List<User>> groupUsers(List<User> users) {
-        Map<Integer, List<User>> group = new HashMap<>();
-        for (User user : users) {
-            int age = user.age;
-            if (!group.containsKey(age)) {
-                group.put(age, new ArrayList<>());
-            }
-            group.get(age).add(user);
-        }
-        return group;
-    }
-
-    @Override
-    public String toString() {
-        return "name: " + name;
+    public int getAge() {
+        return age;
     }
 }
