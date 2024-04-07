18bd8def4097b3401224cff3281f7d44caac6d19
Liza Yudelevich <elizaveta.fr@gmail.com>
2024-04-07 16:26:23 +0300

    Made corrections in solution for task BJS2-4639.

--- src/main/java/faang/school/godbless/countingAbsenteeism/Main.java
+++ src/main/java/faang/school/godbless/countingAbsenteeism/Main.java
@@ -2,8 +2,10 @@ package faang.school.godbless.countingAbsenteeism;
 
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
+import java.util.Objects;
 
 public class Main {
     public static void main(String[] args) {
@@ -17,29 +19,37 @@ public class Main {
         Student student1 = new Student("David", "Design", 1);
         Student student2 = new Student("Polina", "Math", 4);
         Student student3 = new Student("Kristina", "Math", 4);
+        Student student4 = new Student("Vasya", "Design", 2);
 
-        addNewStudentInList(studentList, student1);
-        addNewStudentInList(studentList, student2);
-        addNewStudentInList(studentList, student3);
+        Map<String, List<Student>> studentMap = groupByFacultyAndYear(studentList);
 
-        removeStudentFromList(studentList, "Kristina", "Math", 4);
-        removeStudentFromList(studentList, "Masha", "Math", 3);
+        addNewStudentInMap(studentMap, student1);
+        addNewStudentInMap(studentMap, student2);
+        addNewStudentInMap(studentMap, student3);
+        addNewStudentInMap(studentMap, student4);
 
-        findAllStudentOnFacultyAndCourse(studentList, "Math", 3);
 
-        Map<String, List<Student>> studentMap = listOfStudentsOnFacultyAndCourse(studentList);
+        removeStudentFromMap(studentMap, "Kristina", "Math", 4);
+        removeStudentFromMap(studentMap, "Masha", "Math", 3);
+
         for (Map.Entry<String, List<Student>> entry : studentMap.entrySet()) {
             System.out.println("Факультет, курс: " + entry.getKey() + ":");
             for (Student student : entry.getValue()) {
                 System.out.println(student.getName());
             }
         }
+
+        findAllStudentOnFacultyAndCourse(studentMap, "Math", 4);
     }
 
-    public static Map<String, List<Student>> listOfStudentsOnFacultyAndCourse(List<Student> studentList) {
+    private static String createPairOfFacultyAndYear(String faculty, int year) {
+        return faculty + ", " + year;
+    }
+
+    private static Map<String, List<Student>> groupByFacultyAndYear(List<Student> studentList) {
         Map<String, List<Student>> mapFacultyAndYear = new HashMap<>();
         for (Student student : studentList) {
-            String facultyAndYear = student.getFaculty() + ", " + student.getYear();
+            String facultyAndYear = createPairOfFacultyAndYear(student.getFaculty(), student.getYear());
             if (!mapFacultyAndYear.containsKey(facultyAndYear)) {
                 mapFacultyAndYear.put(facultyAndYear, new ArrayList<>());
             }
@@ -48,30 +58,46 @@ public class Main {
         return mapFacultyAndYear;
     }
 
-    public static void addNewStudentInList(List<Student> studentList, Student newStudent) {
-        studentList.add(newStudent);
+    private static void addNewStudentInMap(Map<String, List<Student>> studentMap, Student newStudent) {
+        String facultyAndYear = createPairOfFacultyAndYear(newStudent.getFaculty(), newStudent.getYear());
+        if (studentMap.containsKey(facultyAndYear)) {
+            studentMap.get(facultyAndYear).add(newStudent);
+        } else {
+            studentMap.put(facultyAndYear, new ArrayList<>());
+            studentMap.get(facultyAndYear).add(newStudent);
+        }
     }
 
-    public static void removeStudentFromList(List<Student> studentList, String name, String faculty, int year) {
-        Student student = new Student(name, faculty, year);
-        if (studentList.contains(student)) {
-            studentList.remove(student);
+    private static void removeStudentFromMap(Map<String, List<Student>> studentMap, String name, String faculty, int year) {
+        String facultyAndYear = createPairOfFacultyAndYear(faculty, year);
+        if (studentMap.containsKey(facultyAndYear)) {
+            Iterator<Student> iterator = studentMap.get(facultyAndYear).iterator();
+            boolean studentFound = false;
+            while (iterator.hasNext()) {
+                Student student = iterator.next();
+                if (Objects.equals(student.getName(), name)) {
+                    iterator.remove();
+                    studentFound = true;
+                }
+            }
+            if (!studentFound) {
+                System.out.println("Такого студента нет.");
+            }
         } else {
             System.out.println("Такого студента нет.");
         }
     }
 
-    public static void findAllStudentOnFacultyAndCourse(List<Student> studentList, String faculty, int year) {
-        Map<String, List<Student>> mapOfFacultyYearAndStudents = listOfStudentsOnFacultyAndCourse(studentList);
-        String neededFacultyAndYear = faculty + ", " + year;
-        if (mapOfFacultyYearAndStudents.containsKey(neededFacultyAndYear)) {
+
+    private static void findAllStudentOnFacultyAndCourse(Map<String, List<Student>> studentMap, String faculty, int year) {
+        String neededFacultyAndYear = createPairOfFacultyAndYear(faculty, year);
+        if (studentMap.containsKey(neededFacultyAndYear)) {
             List<String> namesOfNeededStudents = new ArrayList<>();
-            for (Student student : mapOfFacultyYearAndStudents.get(neededFacultyAndYear)) {
+            for (Student student : studentMap.get(neededFacultyAndYear)) {
                 String nameOfStudent = student.getName();
                 namesOfNeededStudents.add(nameOfStudent);
             }
-            System.out.println("На " + neededFacultyAndYear + " обучаются студенты "
-                    + namesOfNeededStudents);
+            System.out.println("На " + neededFacultyAndYear + " обучаются студенты " + namesOfNeededStudents);
         } else {
             System.out.println("На таком факультете и годе обучения нет ни одного студента.");
         }
