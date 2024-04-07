e46fdd142ce62b91264163edc58e1549cf6ba599
Nikolai Gnidenko <gnidenkonn@gmail.com>
2024-04-07 22:09:30 +0300

    Название задачи: Библиотека Вестеросса

---
+++ src/main/java/libraryOfVesteros/Book.java
@@ -0,0 +1,15 @@
+package libraryOfVesteros;
+
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.ToString;
+
+@EqualsAndHashCode
+@AllArgsConstructor
+@ToString
+public class Book {
+    private String title;
+    private String author;
+    private int year;
+}
+
