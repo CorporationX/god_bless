920326ea57b3f53c513bcc62818f1ed95154b171
Th0rn-dev <solnek33@gmail.com>
2024-04-07 10:42:25 +0300

    Refactored code by recommendations

--- src/main/java/faang/school/godbless/users/Main.java
+++ src/main/java/faang/school/godbless/users/Main.java
@@ -6,7 +6,7 @@ import java.util.HashSet;
 public class Main {
     public static void main(String[] args) {
 
-        User user1 = new User(
+        User user_1 = new User(
                 1,
                 "Tom",
                 19,
@@ -14,26 +14,35 @@ public class Main {
                 "London",
                 "Google"
         );
-        // Кинет исключение из-за адреса
-        User user2 = new User(
-                2,
-                "Jon",
-                22,
-                new HashSet<>(Arrays.asList("velo", "kite surfing")),
-                "Oslo",
-                "Uber"
-        );
-        // Кинет исключение из-за возраста
-        User user3 = new User(
-                3,
-                "Ann",
-                17,
-                new HashSet<>(Arrays.asList("travel", "moto")),
-                "Amsterdam",
-                "Amazon"
-        );
-        // Кинет исключение из-за места работы
-        User user4 = new User(
+        // Кинет исключение IllegalArgumentException() на пустое или не существующее имя
+//        User user_2 = new User(
+//                1,
+//                "",
+//                19,
+//                new HashSet<>(Arrays.asList("yoga", "run")),
+//                "London",
+//                "Google"
+//        );
+        // Кинет исключение IllegalArgumentException() из-за адреса
+//        User user_3 = new User(
+//                2,
+//                "Jon",
+//                22,
+//                new HashSet<>(Arrays.asList("velo", "kite surfing")),
+//                "Oslo",
+//                "Uber"
+//        );
+        // Кинет исключение IllegalArgumentException() из-за возраста
+//        User user_4 = new User(
+//                3,
+//                "Ann",
+//                17,
+//                new HashSet<>(Arrays.asList("travel", "moto")),
+//                "Amsterdam",
+//                "Amazon"
+//        );
+        // Кинет исключение IllegalArgumentException() из-за места работы
+        User user_5 = new User(
                 4,
                 "Liza",
                 25,
--- src/main/java/faang/school/godbless/users/User.java
+++ src/main/java/faang/school/godbless/users/User.java
@@ -3,10 +3,6 @@ package faang.school.godbless.users;
 import lombok.Getter;
 import lombok.Setter;
 
-import java.util.HashMap;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Map;
 import java.util.Objects;
 import java.util.Set;
 
@@ -14,18 +10,8 @@ import java.util.Set;
 @Getter
 @Setter
 public class User {
-    public static Set<String> VALID_JOBS = new HashSet<>() {{
-        add("Google");
-        add("Uber");
-        add("Amazon");
-    }};
-
-    public static Set<String> VALID_ADDRESSES = new HashSet<>() {{
-        add("London");
-        add("New York");
-        add("Amsterdam");
-    }};
-
+    public static final Set<String> VALID_JOBS = Set.of("Google", "Uber", "Amazon");
+    public static final Set<String> VALID_ADDRESSES = Set.of("London", "New York", "Amsterdam");
     private int id;
     private String name;
     private int age;
@@ -35,52 +21,32 @@ public class User {
 
 
     public User(int id, String name, int age, Set<String> activity, String address, String placeOfWork) {
+        validations(name, age, address, placeOfWork);
         this.id = id;
         this.activity = activity;
+        this.name = name;
+        this.age = age;
+        this.address = address;
+        this.placeOfWork = placeOfWork;
 
-        if (Objects.isNull(name)) {
-            throw new IllegalArgumentException("Field name must be not null!");
-        } else {
-            this.name = name;
-        }
+    }
 
-        if (age < 18) {
+    void validations(String name, int age, String address, String placeOfWork) {
+        if (Objects.isNull(name) || name.trim().isEmpty()) {
+            throw new IllegalArgumentException("Field name must be not null or empty!");
+        } else if (age < 18) {
             throw new IllegalArgumentException("Age must be less 18 year!");
-        } else {
-            this.age = age;
-        }
-
-        if (!VALID_ADDRESSES.contains(address)) {
+        } else if (!VALID_ADDRESSES.contains(address)) {
             throw new IllegalArgumentException(
-                    "Еhe address must contain one of the following cities: " +
+                    "The address must contain one of the following cities: " +
                             "London, New York, Amsterdam."
             );
-        } else {
-            this.address = address;
-        }
-
-        if (!VALID_JOBS.contains(placeOfWork)) {
+        } else if (!VALID_JOBS.contains(placeOfWork)) {
             throw new IllegalArgumentException(
                     "The place of work must be one of the companies: " +
                             "Google, Uber, Amazon"
             );
-        } else {
-            this.placeOfWork = placeOfWork;
         }
-    }
-
-    public static Map<User, String> findHobbyLovers(List<User> users, Set<String> activities) {
-        Map<User, String> hm = new HashMap<>();
-
-        for (User user : users) {
-            Set<String> userActivity = user.getActivity();
-            Set<String> intersection = new HashSet<>(activities);
-            intersection.retainAll(userActivity);
-            if (!intersection.isEmpty()) {
-                hm.put(user, intersection.stream().findFirst().get());
-            }
-        }
-        return hm;
     }
 }
 
