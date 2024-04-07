28b668e85c0a28952146dc0c9b88778874e08668
ekoregin <evgkoregin@gmail.com>
2024-04-07 16:34:53 +0300

    BJS2-4441: Fixed 1 after review

--- src/main/java/faang/school/godbless/bjs2_4441/Main.java
+++ src/main/java/faang/school/godbless/bjs2_4441/Main.java
@@ -4,12 +4,12 @@ public class Main {
     public static void main(String[] args) {
         Creature pikeman = new Pikeman();
         Creature griffin = new Griffin();
-        Hero hero1 = new Hero("Hero1", "Catchers", 20, 2);
-        hero1.addCreature(pikeman, 4);
-        Hero hero2 = new Hero("Hero2", "Stark",20, 2);
-        hero2.addCreature(griffin, 2);
+        Hero Lancelot = new Hero("Lancelot", "Catchers", 20, 2);
+        Lancelot.addCreature(pikeman, 4);
+        Hero JohnWane = new Hero("John Wane", "Stark",20, 2);
+        JohnWane.addCreature(griffin, 2);
 
-        Battlefield battlefield = new Battlefield(hero1, hero2);
+        Battlefield battlefield = new Battlefield(Lancelot, JohnWane);
         System.out.println("Battle winner: " + battlefield.battle());
     }
 }
