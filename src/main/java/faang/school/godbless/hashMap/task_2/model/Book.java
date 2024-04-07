29f9f7a8bf5d5192d3181c505933cea414885455
mihanchik94 <mihanchik94@gmail.com>
2024-04-07 09:33:24 +0300

    Java HashMap: Библиотека Вестеросса

---
+++ src/main/java/faang/school/godbless/hashMap/task_2/model/Book.java
@@ -0,0 +1,10 @@
+package faang.school.godbless.hashMap.task_2.model;
+
+import lombok.*;
+
+@Data
+public class Book {
+    private final String title;
+    private final String author;
+    private final int year;
+}
