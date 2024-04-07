499e45bf8eae105332757b7385e412ecbd67dec4
Alex Vishnevskiy <vishgioall@gmail.com>
2024-04-07 17:07:08 +0300

    внес изменения после ревью, изменил List на Set

--- src/main/java/faang/school/godbless/BJS2_4514/Main.java
+++ src/main/java/faang/school/godbless/BJS2_4514/Main.java
@@ -1,19 +1,19 @@
 package faang.school.godbless.BJS2_4514;
 
-import java.util.List;
 import java.util.Map;
+import java.util.Set;
 
 public class Main {
 
     public static void main(String[] args) {
 
-        User user1 = new User(1, "Sam", 25, List.of("Music", "Car", "Sport"));
-        User user2 = new User(2, "Nick", 45, List.of("Fish", "Car", "Hunt"));
-        User user3 = new User(3, "Marie", 30, List.of("Clothes", "Fashion", "Food"));
+        User user1 = new User(1, "Sam", 25, Set.of("Music", "Car", "Sport"));
+        User user2 = new User(2, "Nick", 45, Set.of("Fish", "Car", "Hunt"));
+        User user3 = new User(3, "Marie", 30, Set.of("Clothes", "Fashion", "Food"));
 
-        List<User> userList = List.of(user1, user2, user3);
-        List<String> listActivities = List.of("Hokey", "football", "Hunt");
-        Map<User, String> out = User.findHobbyLovers(userList, listActivities);
+        Set<User> users = Set.of(user1, user2, user3);
+        Set<String> activities = Set.of("Hokey", "football", "Hunt");
+        Map<User, String> out = User.findHobbyLovers(users, activities);
 
         for (User user : out.keySet()) {
             System.out.println(user.getName() + " " + out.get(user));
--- src/main/java/faang/school/godbless/BJS2_4514/User.java
+++ src/main/java/faang/school/godbless/BJS2_4514/User.java
@@ -4,8 +4,8 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
+import java.util.Set;
 
 @Data
 @AllArgsConstructor
@@ -13,14 +13,14 @@ public class User {
     private int id;
     private String name;
     private int age;
-    private List<String> listActivities;
+    private Set<String> activities;
 
-    public static Map<User, String> findHobbyLovers(List<User> users, List<String> listActivities) {
+    public static Map<User, String> findHobbyLovers(Set<User> users, Set<String> activities) {
 
         Map<User, String> result = new HashMap<>();
         for (User user : users) {
-            for (String activity : listActivities) {
-                if (user.getListActivities().contains(activity)) {
+            for (String activity : activities) {
+                if (user.getActivities().contains(activity)) {
                     result.put(user, activity);
                     break;
                 }
