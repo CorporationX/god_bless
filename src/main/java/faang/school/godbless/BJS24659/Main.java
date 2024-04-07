6357242a14b1356ffee53cf0487e834c759e134e
Maxim Usachev <fatherice@gmail.com>
2024-04-07 14:05:53 +0700

    BJS2-4659

--- src/main/java/faang/school/godbless/BJS24659/Main.java
+++ src/main/java/faang/school/godbless/BJS24659/Main.java
@@ -1,26 +1,36 @@
 package faang.school.godbless.BJS24659;
 
-import java.util.HashMap;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Map;
 
 public class Main {
     public static void main(String[] args) {
-        Map<String, House> houses = new HashMap<>();
+        List<Student> students = new ArrayList<>();
 
-        House.addHouse(houses, "Старк", "ГербСтарков");
-        House.addHouse(houses, "Ланнистер", "ГербЛанистеров");
-        House.addHouse(houses, "Баратеон", "ГербБаратеонов");
+        Student.addStudent(students, "Иванов", "ИТ", 1);
+        Student.addStudent(students, "Петров", "ИТ", 2);
+        Student.addStudent(students, "Сидоров", "Математика", 1);
+        Student.addStudent(students, "Смирнов", "Математика", 2);
+        Student.addStudent(students, "Козлов", "Физика", 1);
 
-        System.out.println("Информация о домах Вестероса:");
-        houses.values().forEach(System.out::println);
+        System.out.println("Информация о студентах:");
+        students.forEach(System.out::println);
 
-        House.removeHouse(houses, "Ланнистер");
+        Map<String, List<Student>> groupedStudents = Student.groupByFacultyAndYear(students);
 
-        System.out.println("\nПосле удаления дома Ланнистер:");
-        houses.values().forEach(System.out::println);
+        System.out.println("\nГруппировка студентов по факультету и курсу:");
+        Student.printStudentsByFacultyAndYear(groupedStudents);
 
-        String houseName = "Старк";
-        System.out.println("\nПоиск дома по названию \"" + houseName + "\":");
-        House.findHouse(houses, houseName);
+        Student.removeStudent(students, "Иванов", "ИТ", 1);
+
+        System.out.println("Информация о студентах после удаления:");
+        students.forEach(System.out::println);
+
+        String faculty = "Математика";
+        int year = 1;
+        System.out.println("\nСтуденты факультета " + faculty + " " + year + " курса:");
+        List<Student> foundStudents = Student.findStudentsByFacultyAndYear(students, faculty, year);
+        foundStudents.forEach(System.out::println);
     }
 }
