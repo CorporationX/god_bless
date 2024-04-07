b4ba4f9f787a9c377068a0b79601894492f84d46
Tem <tipokruto014@gmail.com>
2024-04-08 02:09:16 +0300

    а я все сделал

---
+++ src/main/java/faang/school/godbless/GmailFilters/EmailProcessor.java
@@ -0,0 +1,17 @@
+package faang.school.godbless.GmailFilters;
+
+import java.util.List;
+import java.util.function.Consumer;
+import java.util.function.Function;
+import java.util.function.Predicate;
+import java.util.stream.Collectors;
+
+public class EmailProcessor {
+    public List<String> processEmails(List<Email> emails, Predicate<Email> predicate, Consumer<Email> consumer, Function<Email, String> function) {
+        return emails.stream()
+                .filter(predicate)
+                .peek(consumer)
+                .map(function)
+                .collect(Collectors.toList());
+    }
+}
