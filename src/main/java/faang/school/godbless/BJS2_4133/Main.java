a146aed8ca15f9adb0fe5eeb17ad029369e8076e
Andrei Archakov <archandrwork@gmail.com>
2024-04-07 15:23:10 +0300

    BJS2-4133: changes with review

--- src/main/java/faang/school/godbless/BJS2_4133/Main.java
+++ src/main/java/faang/school/godbless/BJS2_4133/Main.java
@@ -2,11 +2,12 @@ package faang.school.godbless.BJS2_4133;
 
 import java.util.HashMap;
 import java.util.HashSet;
+import java.util.Map;
 import java.util.Set;
 
 public class Main {
     public static void main(String[] args) {
-        HashSet<Student> students = new HashSet<>(Set.of(
+        Set<Student> students = new HashSet<>(Set.of(
                 new Student("Andrei", "IT", 2024),
                 new Student("Victor", "IT", 2024),
                 new Student("Vlad", "NotIT", 2022),
@@ -14,15 +15,54 @@ public class Main {
                 new Student("Nick", "NotIT", 2020))
         );
 
-        HashMap<String, HashSet<Student>> groupedStudents = Student.groupStudentsByDirection(students);
+        Map<String, Set<Student>> groupedStudents = groupStudentsByDirection(students);
         System.out.println(groupedStudents);
 
-        Student.addStudentToDirection(new Student("Vlad", "IT", 2024), groupedStudents);
+        addStudentToDirection(new Student("Vlad", "IT", 2024), groupedStudents);
         System.out.println(groupedStudents);
 
-        Student.removeStudentFromDirection("Vlad", "IT", 2024, groupedStudents);
+        removeStudentFromDirection("Vlad", "IT", 2024, groupedStudents);
         System.out.println(groupedStudents);
 
-        System.out.println(Student.getStudentsByDirection("IT", 2024, groupedStudents));
+        System.out.println(getStudentsByDirection("IT", 2024, groupedStudents));
+    }
+
+    private static String getDirectionKey(Student student) {
+        return String.format("%s-%s", student.getFaculty(), student.getYear());
+    }
+
+    public static Map<String, Set<Student>> groupStudentsByDirection(Set<Student> students) {
+        Map<String, Set<Student>> result = new HashMap<>();
+
+        for (Student student : students) {
+            addStudentToDirection(student, result);
+        }
+
+        return result;
+    }
+
+    public static void addStudentToDirection(Student student, Map<String, Set<Student>> target) {
+        target.computeIfAbsent(getDirectionKey(student), k -> new HashSet<>()).add(student);
+    }
+
+    public static void removeStudentFromDirection(String name, String faculty, int year, Map<String, Set<Student>> target) {
+        Student student = new Student(name, faculty, year);
+        Set<Student> studentGroup = target.get(getDirectionKey(student));
+
+        if (studentGroup == null) {
+            throw new IllegalArgumentException(String.format("Студент \"%s\", Факультет \"%s\", Год \"%s\" не найден", name, faculty, year));
+        }
+
+        studentGroup.remove(student);
+    }
+
+    public static Set<Student> getStudentsByDirection(String faculty, int year, Map<String, Set<Student>> target) {
+        Set<Student> students = target.get(String.format("%s-%s", faculty, year));
+
+        if (students == null) {
+            return new HashSet<>();
+        }
+
+        return students;
     }
 }
--- src/main/java/faang/school/godbless/BJS2_4133/Student.java
+++ src/main/java/faang/school/godbless/BJS2_4133/Student.java
@@ -3,52 +3,10 @@ package faang.school.godbless.BJS2_4133;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 
-import java.util.HashMap;
-import java.util.HashSet;
-
 @Data
 @AllArgsConstructor
 public class Student {
     private String name;
     private String faculty;
     private int year;
-
-    private String getDirectionKey() {
-        return String.format("%s-%s", getFaculty(), getYear());
-    }
-
-    public static HashMap<String, HashSet<Student>> groupStudentsByDirection(HashSet<Student> students) {
-        HashMap<String, HashSet<Student>> result = new HashMap<>();
-
-        for (Student student : students) {
-            addStudentToDirection(student, result);
-        }
-
-        return result;
-    }
-
-    public static void addStudentToDirection(Student student, HashMap<String, HashSet<Student>> target) {
-        target.computeIfAbsent(student.getDirectionKey(), k -> new HashSet<>()).add(student);
-    }
-
-    public static void removeStudentFromDirection(String name, String faculty, int year, HashMap<String, HashSet<Student>> target) {
-        Student student = new Student(name, faculty, year);
-        HashSet<Student> studentGroup = target.get(student.getDirectionKey());
-
-        if (studentGroup == null) {
-            throw new IllegalArgumentException(String.format("Студент \"%s\", Факультет \"%s\", Год \"%s\" не найден", name, faculty, year));
-        }
-
-        studentGroup.remove(student);
-    }
-
-    public static HashSet<Student> getStudentsByDirection(String faculty, int year, HashMap<String, HashSet<Student>> target) {
-        HashSet<Student> students = target.get(String.format("%s-%s", faculty, year));
-
-        if (students == null) {
-            return new HashSet<>();
-        }
-
-        return students;
-    }
 }
