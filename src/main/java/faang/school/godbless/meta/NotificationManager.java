4c2805f32a3a747f7a11f13f36412b1a46be9976
Demchenkov7 <andreydemchenkov@yandex.ru>
2024-04-07 18:41:53 +0300

    Задача: Meta вселенная

---
+++ src/main/java/faang/school/godbless/meta/NotificationManager.java
@@ -0,0 +1,17 @@
+package faang.school.godbless.meta;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.function.Consumer;
+
+public class NotificationManager {
+    private final Map<String, Consumer<Notification>> notifications = new HashMap<>();
+
+    public void registerHandler(String type, Consumer<Notification> consumer) {
+        notifications.put(type, consumer);
+    }
+
+    public void sendNotification(Notification notification) {
+        notifications.get(notification.getType()).accept(notification);
+    }
+}
