1d606d63a897a093271b954dc5ed8a1f568a90c2
Tamzhid Babutsyngueva <btamzhid@gmail.com>
2024-04-07 22:32:48 +0800

    BJS2-4620 - Группировка пользователей по возрасту. Попытка №2

--- src/main/java/GroupingUsers/User.java
+++ src/main/java/GroupingUsers/User.java
@@ -21,33 +21,6 @@ public class User {
     public static Map<Integer, List<User>> groupUsers(List<User> usersList) {
         Map<Integer, List<User>> mapUsers = new HashMap<>();
 
-//      Способ 1
-//      List<User> filterUsersList = new ArrayList<>();
-//      while (!usersList.isEmpty()) {
-//          int currentAge = usersList.get(0).age;
-//
-//          filterUsersList = (List<User>) usersList.stream().filter(entry -> (entry.age == currentAge)).toList();
-//          mapUsers.put(currentAge, filterUsersList);
-//
-//          usersList.removeIf(user -> user.age == currentAge);
-//      }
-
-//      Способ 2
-//        while (!usersList.isEmpty()) {
-//            int currentAge = usersList.get(0).age;
-//            List<User> filterUsersList = new ArrayList<>();
-//
-//            for (int i = 0; i < usersList.size(); i++) {
-//                if (usersList.get(i).age == currentAge) {
-//                    filterUsersList.add(usersList.get(i));
-//                    usersList.remove(usersList.get(i));
-//                    i--;
-//                }
-//            }
-//            mapUsers.put(currentAge, filterUsersList);
-//        }
-
-        //Попытка 2
         List<User> filterUsersList = new ArrayList<>();
         for (User user : usersList) {
             if (mapUsers.containsKey(user.age)) {
