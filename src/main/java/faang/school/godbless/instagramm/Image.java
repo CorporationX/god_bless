7dd2b8306166d2f75febe52f62827c34e6cfd6ff
Demchenkov7 <andreydemchenkov@yandex.ru>
2024-04-07 19:33:40 +0300

    Задача: фильтр instagramm

---
+++ src/main/java/faang/school/godbless/instagramm/Image.java
@@ -0,0 +1,13 @@
+package faang.school.godbless.instagramm;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.ToString;
+
+@AllArgsConstructor
+@Getter
+@ToString
+public class Image {
+    private final String name;
+    private final String description;
+}
