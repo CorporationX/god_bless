5e1d930a80747277c6e60687ba3fb139391e800c
Tamzhid Babutsyngueva <btamzhid@gmail.com>
2024-04-08 00:08:28 +0800

    BJS2-4927 - Абстракция, абстракция!

---
+++ src/main/java/Abstraction/Character.java
@@ -0,0 +1,48 @@
+package Abstraction;
+
+public abstract class Character {
+
+    protected String name;
+    protected int power;
+    protected int dexterity;
+    protected int intelligence;
+    protected int health = 100;
+
+    public Character(String name) {
+        this.name = name;
+    }
+
+    public Character(String name, int power, int dexterity, int intelligence) {
+        this.name = name;
+        this.power = power;
+        this.dexterity = dexterity;
+        this.intelligence = intelligence;
+    }
+
+    public abstract void attack(Character opponent);
+
+}
+
+class Warrior extends Character{
+
+    public Warrior(String name) {
+        super(name, 10, 5, 3);
+    }
+
+    @Override
+    public void attack(Character opponent) {
+        opponent.health = opponent.health - this.power;
+    }
+}
+
+class Archer extends Character{
+
+    public Archer(String name) {
+        super(name, 3, 10, 5);
+    }
+
+    @Override
+    public void attack(Character opponent) {
+        opponent.health = opponent.health - this.dexterity;
+    }
+}
