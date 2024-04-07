aa863a1538b4c8a248f9e3d2e90d9bdff830b116
Tamzhid Babutsyngueva <btamzhid@gmail.com>
2024-04-08 01:19:17 +0800

    BJS2-4916 - Регистрация пользователей. Попытка №3

--- src/main/java/UserRegistration/User.java
+++ src/main/java/UserRegistration/User.java
@@ -4,8 +4,9 @@ import java.util.HashSet;
 import java.util.Set;
 
 public class User {
-    public static Set<String> VALID_JOBS = new HashSet<>(Set.of("Google", "Uber", "Amazon"));
-    public static Set<String> VALID_ADDRESSES = new HashSet<>(Set.of("London", "New York", "Amsterdam"));
+    private static final Set<String> VALID_JOBS = new HashSet<>(Set.of("Google", "Uber", "Amazon"));
+    private static final Set<String> VALID_ADDRESSES = new HashSet<>(Set.of("London", "New York", "Amsterdam"));
+    private final int minAge = 18;
 
     private String name;
     private int age;
@@ -24,8 +25,8 @@ public class User {
     private void userValidation(String name, int age, String workPlace, String adress) {
         if (name == null || name.isEmpty()) {
             throw new IllegalArgumentException("имя пользователя не должно быть пустым");
-        } else if (age < 18) {
-            throw new IllegalArgumentException("Возраст пользователя должен быть не меньше 18ти");
+        } else if (age < minAge) {
+            throw new IllegalArgumentException("Возраст пользователя должен быть не меньше, чем " + minAge);
         } else if (!VALID_JOBS.contains(workPlace)) {
             throw new IllegalArgumentException("Место работы " + workPlace + " невалидно");
         } else if (!VALID_ADDRESSES.contains(adress)) {
