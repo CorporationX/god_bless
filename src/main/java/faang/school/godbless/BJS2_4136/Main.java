2f9360c05c0d509a683ee7e9f8eeadbcf3bf4a7e
Andrei Archakov <archandrwork@gmail.com>
2024-04-07 15:26:34 +0300

    BJS2-4136: changes with review

--- src/main/java/faang/school/godbless/BJS2_4136/Main.java
+++ src/main/java/faang/school/godbless/BJS2_4136/Main.java
@@ -3,23 +3,24 @@ package faang.school.godbless.BJS2_4136;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class Main {
-    private static final HashMap<Integer, StreamEvent> eventsById = new HashMap<>();
-    private static final HashMap<String, List<StreamEvent>> eventsByType = new HashMap<>();
+    private static final Map<Integer, StreamEvent> eventsById = new HashMap<>();
+    private static final Map<String, List<StreamEvent>> eventsByType = new HashMap<>();
 
     public static void main(String[] args) {
         addEvent(new StreamEvent(1, "Тип 1", "Data 1"));
         addEvent(new StreamEvent(2, "Тип 2", "Data 2"));
         addEvent(new StreamEvent(3, "Тип 3", "Data 3"));
         addEvent(new StreamEvent(4, "Тип 2", "Data 3"));
-        StreamEvent.printAllEvents(eventsById);
+        printAllEvents(eventsById);
 
-        System.out.println(StreamEvent.findEventById(2, eventsById));
-        System.out.println(StreamEvent.findEventsByType("Тип 2", eventsByType));
+        System.out.println(findEventById(2, eventsById));
+        System.out.println(findEventsByType("Тип 2", eventsByType));
 
         removeEventById(2);
-        StreamEvent.printAllEvents(eventsById);
+        printAllEvents(eventsById);
     }
 
     public static void addEvent(StreamEvent event) {
@@ -34,6 +35,23 @@ public class Main {
                 events.removeIf(e -> e.getId() == id);
                 return events;
             });
+        }
+    }
+
+    public static StreamEvent findEventById(int id, Map<Integer, StreamEvent> target) {
+        return target.get(id);
+    }
+
+    public static List<StreamEvent> findEventsByType(String eventType, Map<String, List<StreamEvent>> target) {
+        return target.getOrDefault(eventType, new ArrayList<>());
+    }
+
+    public static void printAllEvents(Map<Integer, StreamEvent> target) {
+        System.out.println("Все события:");
+        for (Map.Entry<Integer, StreamEvent> entry : target.entrySet()) {
+            StreamEvent event = entry.getValue();
+            System.out.printf("ID=%s, Type=%s, Data=%s", event.getId(), event.getEventType(), event.getData());
+            System.out.println();
         }
     }
 }
