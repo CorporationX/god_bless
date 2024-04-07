0a0e5d3bffd293f26f274023d6898430879f33f4
Andrei Archakov <archandrwork@gmail.com>
2024-04-07 15:34:55 +0300

    BJS2-4136: changes with review

--- src/main/java/faang/school/godbless/BJS2_4136/Main.java
+++ src/main/java/faang/school/godbless/BJS2_4136/Main.java
@@ -6,32 +6,32 @@ import java.util.List;
 import java.util.Map;
 
 public class Main {
-    private static final Map<Integer, StreamEvent> eventsById = new HashMap<>();
-    private static final Map<String, List<StreamEvent>> eventsByType = new HashMap<>();
+    private static final Map<Integer, StreamEvent> EVENTS_BY_ID = new HashMap<>();
+    private static final Map<String, List<StreamEvent>> EVENTS_BY_TYPE = new HashMap<>();
 
     public static void main(String[] args) {
         addEvent(new StreamEvent(1, "Тип 1", "Data 1"));
         addEvent(new StreamEvent(2, "Тип 2", "Data 2"));
         addEvent(new StreamEvent(3, "Тип 3", "Data 3"));
         addEvent(new StreamEvent(4, "Тип 2", "Data 3"));
-        printAllEvents(eventsById);
+        printAllEvents(EVENTS_BY_ID);
 
-        System.out.println(findEventById(2, eventsById));
-        System.out.println(findEventsByType("Тип 2", eventsByType));
+        System.out.println(findEventById(2, EVENTS_BY_ID));
+        System.out.println(findEVENTS_BY_TYPE("Тип 2", EVENTS_BY_TYPE));
 
         removeEventById(2);
-        printAllEvents(eventsById);
+        printAllEvents(EVENTS_BY_ID);
     }
 
     public static void addEvent(StreamEvent event) {
-        eventsById.put(event.getId(), event);
-        eventsByType.computeIfAbsent(event.getEventType(), k -> new ArrayList<>()).add(event);
+        EVENTS_BY_ID.put(event.getId(), event);
+        EVENTS_BY_TYPE.computeIfAbsent(event.getEventType(), k -> new ArrayList<>()).add(event);
     }
 
     public static void removeEventById(int id) {
-        StreamEvent event = eventsById.remove(id);
+        StreamEvent event = EVENTS_BY_ID.remove(id);
         if (event != null) {
-            eventsByType.computeIfPresent(event.getEventType(), (type, events) -> {
+            EVENTS_BY_TYPE.computeIfPresent(event.getEventType(), (type, events) -> {
                 events.removeIf(e -> e.getId() == id);
                 return events;
             });
@@ -42,7 +42,7 @@ public class Main {
         return target.get(id);
     }
 
-    public static List<StreamEvent> findEventsByType(String eventType, Map<String, List<StreamEvent>> target) {
+    public static List<StreamEvent> findEVENTS_BY_TYPE(String eventType, Map<String, List<StreamEvent>> target) {
         return target.getOrDefault(eventType, new ArrayList<>());
     }
 
