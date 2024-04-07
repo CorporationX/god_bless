4d36be0e8dcfcb270e9686c0e8e73fba24a8daab
ekoregin <evgkoregin@gmail.com>
2024-04-07 18:50:04 +0300

    Task: https://faang-school.atlassian.net/browse/BJS2-4918

---
+++ src/main/java/faang/school/godbless/bjs2_4918/SpellCaster.java
@@ -0,0 +1,8 @@
+package faang.school.godbless.bjs2_4918;
+
+public class SpellCaster {
+
+    public void cast(String spell, SpellAction spellAction) {
+        System.out.println(spellAction.action(spell));
+    }
+}
