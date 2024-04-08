fe1edc40924049f172e2e5db971e89ddc40ff25d
Tamzhid Babutsyngueva <btamzhid@gmail.com>
2024-04-08 23:56:19 +0800

    BJS2-4951 - Коллекционируем пользователей. Попытка №3

--- src/main/java/collectingUsers/Main.java
+++ src/main/java/collectingUsers/Main.java
@@ -8,28 +8,17 @@ import java.util.Set;
 public class Main {
     public static void main(String[] args) {
 
-        Set<String> activitiesOfUserA = new HashSet<>();
-        activitiesOfUserA.add("сон");
-        activitiesOfUserA.add("поиск мемов");
-        activitiesOfUserA.add("что-то полезное");
+        Set<String> activitiesOfUserA = new HashSet<String>(Set.of("сон", "поиск мемов", "что-то полезное"));
+        Set<String> activitiesOfUserB = new HashSet<String>(Set.of("чтение", "что-то важное", "что-то бесполезное"));
 
-        Set<String> activitiesOfUserB = new HashSet<>();
-        activitiesOfUserB.add("чтение");
-        activitiesOfUserB.add("что-то важное");
-        activitiesOfUserB.add("что-то бесполезное");
-
-        User userA = new User(1, "Anna", 12, activitiesOfUserA);
-        User userB = new User(2, "Bato", 15, activitiesOfUserB);
+        User anna = new User(1, "Anna", 12, activitiesOfUserA);
+        User bato = new User(2, "Bato", 15, activitiesOfUserB);
 
         List<User> usersList = new ArrayList<User>();
-        usersList.add(userA);
-        usersList.add(userB);
+        usersList.add(anna);
+        usersList.add(bato);
 
-        Set<String> activitiesSet = new HashSet<String>();
-        activitiesSet.add("что-то бесполезное");
-        activitiesSet.add("что-то полезное");
-        activitiesSet.add("что-то важное");
-        activitiesSet.add("cон");
+        Set<String> activitiesSet = new HashSet<String>(Set.of("что-то бесполезное", "что-то полезное", "что-то важное", "cон"));
 
         System.out.println(User.findHobbyLovers(usersList, activitiesSet));
 
