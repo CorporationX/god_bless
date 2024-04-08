c999f7d5ab56b96f18c504833a34112cd516fb4b
Aleksey <lekha.bolt2004@gmail.com>
2024-04-08 12:53:21 +0300

    Lombok Data update

--- src/main/java/faang/school/godbless/grouped/User.java
+++ src/main/java/faang/school/godbless/grouped/User.java
@@ -1,48 +1,15 @@
 package faang.school.godbless.grouped;
 
-import lombok.Getter;
-import lombok.Setter;
 
-
+import lombok.Data;
 import java.util.*;
 
-@Getter
-@Setter
+@Data
 public class User {
     private String name;
     private int age;
     private String location;
     private String address;
-
-    public User(String name, int age, String location, String address) {
-        this.name = name;
-        this.age = age;
-        this.location = location;
-        this.address = address;
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        User user = (User) o;
-        return age == user.age && Objects.equals(name, user.name) && Objects.equals(location, user.location) && Objects.equals(address, user.address);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(name, age, location, address);
-    }
-
-    @Override
-    public String toString() {
-        return "User{" +
-                "name='" + name + '\'' +
-                ", age=" + age +
-                ", placeOfWork='" + location + '\'' +
-                ", address='" + address + '\'' +
-                '}';
-    }
 
     public static Map<Integer, List<User>> groupUsers(List<User> users){
         Map<Integer, List<User>> groupedUsers = new HashMap<>();
