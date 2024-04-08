91678164cc45e440507895c667563333d15e1b2f
Maxim Usachev <fatherice@gmail.com>
2024-04-08 15:08:13 +0700

    BJS2-4479 Убрал второго назойливого война. Поправил по замечанию.

--- src/main/java/faang/school/godbless/BJS24479/Main.java
+++ src/main/java/faang/school/godbless/BJS24479/Main.java
@@ -3,11 +3,10 @@ package faang.school.godbless.BJS24479;
 public class Main {
     public static void main(String[] args) {
         Warrior warrior = new Warrior("Воин");
-        Warrior warrior1 = new Warrior("Воин1");
         Archer archer = new Archer("Лучник");
 
         // Первая атака по 10
-        fight(warrior, archer, warrior1);
+        fight(warrior, archer);
 
         //Воин прокачался +1 сила
         warrior.setStrength(warrior.getStrength() + 1);
@@ -16,18 +15,16 @@ public class Main {
         archer.setDexterity(archer.getDexterity() + 2);
 
         // Вторая атака по 11, 12 и 10 соответственно
-        fight(warrior, archer, warrior1);
+        fight(warrior, archer);
     }
 
-    private static void fight(Warrior warrior, Archer archer, Warrior warrior1) {
+    private static void fight(Warrior warrior, Archer archer) {
         warrior.attack(archer);
         archer.attack(warrior);
-        warrior1.attack(warrior);
 
         // Проверка изменения здоровья
         System.out.println("Здоровье воина: " + warrior.getHealth());
         System.out.println("Здоровье лучника: " + archer.getHealth());
-        System.out.println("Здоровье воина1: " + warrior1.getHealth());
         System.out.println("-------------------------------------------");
     }
 }
