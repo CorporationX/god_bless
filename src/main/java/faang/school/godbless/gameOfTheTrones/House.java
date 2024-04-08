55bdf8e1ea4e0ac1ed386e4e269f780537661d1f
Daniyar Kabdulin <mrdocula@gmail.com>
2024-04-08 21:15:52 +0300

    все исправил.

--- src/main/java/faang/school/godbless/gameOfTheTrones/House.java
+++ src/main/java/faang/school/godbless/gameOfTheTrones/House.java
@@ -1,5 +1,8 @@
 package faang.school.godbless.gameOfTheTrones;
 
+import lombok.Data;
+
+@Data
 public class House {
     private String name;
     private String sigil;
@@ -7,29 +10,5 @@ public class House {
     public House(String name, String sigil) {
         this.name = name;
         this.sigil = sigil;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getSigil() {
-        return sigil;
-    }
-
-    public void setSigil(String sigil) {
-        this.sigil = sigil;
-    }
-
-    @Override
-    public String toString() {
-        return "House{" +
-                "name='" + name + '\'' +
-                ", sigil='" + sigil + '\'' +
-                '}';
     }
 }
--- src/main/java/faang/school/godbless/gameOfTheTrones/Main.java
+++ src/main/java/faang/school/godbless/gameOfTheTrones/Main.java
@@ -4,93 +4,45 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class Main {
+    private static Map<String, House> map = new HashMap<>();
 
     public static void main(String[] args) {
-
-        run();
-
-    }
-
-    private static void run() {
 
         House houseOfStark = new House("Stark", "Direwolf");
         House houseOfBaratheon = new House("Baratheon", "Stag");
         House houseOftLannister = new House("Lannister", "Lion");
 
-        Map<String, House> map = new HashMap<>();
         map.put("Stark", houseOfStark);
         map.put("Baratheon", houseOfBaratheon);
         map.put("Lannister", houseOftLannister);
 
-        while (true) {
-            menu();
-            System.out.println("Enter number: ");
-            int number = Util.getIntegerInput();
-            if (number == 1) {
-                printAllHouses(map);
-            } else if (number == 2) {
-                addNewHouse(map);
-            } else if (number == 3) {
-                searchTheHouse(map);
-            } else if (number == 4) {
-                removeAHouse(map);
-            } else if (number == 5) {
-                System.out.println("You are QUIT");
-                break;
-            } else {
-                System.out.println("Unknown command! Try again!");
-            }
-        }
+        printAllHouses();
+        removeAHouse("Stark");
+        System.out.println("searchTheHouse: " + searchTheHouse("Baratheon"));
+        addNewHouse("King", "Horse");
     }
 
-    private static void menu() {
-        System.out.println("***MENU***");
-        System.out.println("1 - print all Houses and Sigils");
-        System.out.println("2 - add new House");
-        System.out.println("3 - search a House");
-        System.out.println("4 - remove the House");
-        System.out.println("5 - QUIT");
+    private static void removeAHouse(String name) {
+        map.remove(name);
     }
 
-    private static void removeAHouse(Map<String, House> map) {
-        System.out.println("Enter name of remove: ");
-        String name = Util.readLine();
+    private static House searchTheHouse(String name) {
         if (map.containsKey(name)) {
-            System.out.println("The " + map.get(name) + " has been removed.");
-            map.remove(name);
-        } else {
-            System.out.println("The House: " + name + " not found!");
+            return map.get(name);
         }
+        return null;
     }
 
-    private static void searchTheHouse(Map<String, House> map) {
-        System.out.println("Enter name of search: ");
-        String houseName = Util.readLine();
-
-        if (map.containsKey(houseName)) {
-            System.out.println("The " + map.get(houseName) + " found.");
-        } else {
-            System.out.println("The House: " + houseName + " not found!");
-        }
-    }
-
-    private static void printAllHouses(Map<String, House> map) {
-        System.out.println("All Houses and Sigils from Game of the Trones:");
+    private static void printAllHouses() {
+        System.out.println("All Houses and Sigils:");
         for (House house : map.values()) {
             System.out.println(house);
         }
     }
 
-    private static void addNewHouse(Map<String, House> map) {
-
-        System.out.println("Enter new name: ");
-        String name = Util.readLine();
-        System.out.println("Enter new sigil: ");
-        String sigil = Util.readLine();
-
+    private static void addNewHouse(String name, String sigil) {
         House newHouse = new House(name, sigil);
         map.put(name, newHouse);
-
         System.out.println("You added new House: " + newHouse);
     }
 }
