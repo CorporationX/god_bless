c700647d4c16072608594a336709d0e6f6e0f121
ekoregin <evgkoregin@gmail.com>
2024-04-08 10:34:46 +0300

    Task: https://faang-school.atlassian.net/browse/BJS2-5007

---
+++ src/main/java/faang/school/godbless/bjs2_5007/Notification.java
@@ -0,0 +1,12 @@
+package faang.school.godbless.bjs2_5007;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class Notification {
+
+    private String type;
+    private String message;
+}
