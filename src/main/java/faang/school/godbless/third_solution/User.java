023e0193ebd7e91ed426235ff98040ea658a67cd
Vadim Merkulov <vm.merkulov@gmail.com>
2024-04-08 15:25:43 +0300

    BJS2-4528

--- src/main/java/faang/school/godbless/third_solution/User.java
+++ src/main/java/faang/school/godbless/third_solution/User.java
@@ -13,27 +13,29 @@ public class User {
     private List<String> activities;
 
     public static void main(String[] args) {
-        List<User> userList = new ArrayList<>();
-        userList.add(new User(1, "Vadim", 22, Arrays.asList("run", "jump", "swim")));
-        userList.add(new User(2, "Dmitry", 56, Arrays.asList("play", "jump", "work")));
-        userList.add(new User(3, "Igor", 34, Arrays.asList("run", "work", "swim")));
-        userList.add(new User(4, "Radislav", 42, Arrays.asList("play", "cry")));
-        userList.add(new User(5, "Andrey", 33, Arrays.asList()));
+        List<User> users = new ArrayList<>();
+        users.add(new User(1, "Vadim", 22, Arrays.asList("run", "jump", "swim")));
+        users.add(new User(2, "Dmitry", 56, Arrays.asList("play", "jump", "work")));
+        users.add(new User(3, "Igor", 34, Arrays.asList("run", "work", "swim")));
+        users.add(new User(4, "Radislav", 42, Arrays.asList("play", "cry")));
+        users.add(new User(5, "Andrey", 33, Arrays.asList()));
 
-        List<String> possible = new ArrayList<>(Arrays.asList("work", "cry"));
+        List<String> possible_activities = new ArrayList<>(Arrays.asList("work", "cry"));
 
-        findHobbyLovers(userList, possible);
+        findHobbyLovers(users, possible_activities);
 
     }
 
-    public static void findHobbyLovers(List<User> users, List<String> possible_activities) {
-        Map<User, String> usersMap = new HashMap<>();
+    public static Map<User, String> findHobbyLovers(List<User> users, List<String> possible_activities) {
+        Map<User, String> hobbiLovers = new HashMap<>();
         for (User user : users) {
             for (String activity : user.getActivities()) {
                 if (possible_activities.contains(activity)) {
-                    usersMap.put(user, activity);
+                    hobbiLovers.put(user, activity);
+                    break;
                 }
             }
         }
+        return hobbiLovers;
     }
 }
