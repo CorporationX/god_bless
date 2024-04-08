b059e94dd9da66e3e9868ef1ecc73e282067c224
Liza Yudelevich <elizaveta.fr@gmail.com>
2024-04-08 11:03:53 +0300

    Made corrections in solution for task BJS2-4637. https://faang-school.atlassian.net/browse/BJS2-4637

--- src/main/java/faang/school/godbless/gameOfThrones/Main.java
+++ src/main/java/faang/school/godbless/gameOfThrones/Main.java
@@ -1,37 +1,37 @@
 package faang.school.godbless.gameOfThrones;
+
 import java.util.HashMap;
 import java.util.Map;
 
 public class Main {
+    private static Map<String, House> houses = new HashMap<>();
+
     public static void main(String[] args) {
-        Map<String, House> mapHouses = new HashMap<>();
-        mapHouses.put("Stark", new House("Stark", "Wolf"));
-        mapHouses.put("Lannister", new House("Lannister", "Lion"));
-        mapHouses.put("Baratheon", new House("Baratheon", "Deer"));
 
-        // Добавим новый дом
-        addNewHouse(mapHouses, new House("Greyjoy", "Kraken"));
-        // Удалим дом
-        deleteHouse(mapHouses, "Lannister");
-        // Выведем информацию о доме
-        informationAboutSigil(mapHouses, "Stark");
-        informationAboutSigil(mapHouses, "Staaaaark");
+        houses.put("Stark", new House("Stark", "Wolf"));
+        houses.put("Lannister", new House("Lannister", "Lion"));
+        houses.put("Baratheon", new House("Baratheon", "Deer"));
 
-        // Выведем информацию о всех домах
-        displayingAllHousesAndSigils(mapHouses);
+        addNewHouse(new House("Greyjoy", "Kraken"));
+        deleteHouse("Lannister");
+
+        informationAboutSigil("Stark");
+        informationAboutSigil("Staaaaark");
+
+        displayingAllHousesAndSigils();
 
     }
 
-    public static void addNewHouse(Map<String, House> mapHouses, House house) {
-        mapHouses.put(house.getName(), house);
+    public static void addNewHouse(House house) {
+        houses.put(house.getName(), house);
     }
 
-    public static void deleteHouse(Map<String, House> mapHouses, String nameOfHouse) {
-        mapHouses.remove(nameOfHouse);
+    public static void deleteHouse(String nameOfHouse) {
+        houses.remove(nameOfHouse);
     }
 
-    public static void informationAboutSigil(Map<String, House> mapHouses, String nameOfHouse) {
-        House house = mapHouses.get(nameOfHouse);
+    public static void informationAboutSigil(String nameOfHouse) {
+        House house = houses.get(nameOfHouse);
         if (house != null) {
             System.out.println("Герб дома " + nameOfHouse + ": " + house.getSigil());
         } else {
@@ -39,10 +39,9 @@ public class Main {
         }
     }
 
-    public static void displayingAllHousesAndSigils(Map<String, House> mapHouses) {
-        for (Map.Entry<String, House> entry : mapHouses.entrySet()) {
+    public static void displayingAllHousesAndSigils() {
+        for (Map.Entry<String, House> entry : houses.entrySet()) {
             System.out.println(entry.getValue().getName() + " : " + entry.getValue().getSigil());
         }
     }
-
 }
