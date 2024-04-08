bfb9aa74ca6c1d5de1e4aeeebe1c3de97d14a315
Tem <tipokruto014@gmail.com>
2024-04-08 16:17:43 +0300

    style: изменил нейминг

--- src/main/java/faang/school/godbless/InstagramFilters/FilterProcessor.java
+++ src/main/java/faang/school/godbless/InstagramFilters/FilterProcessor.java
@@ -3,17 +3,17 @@ package faang.school.godbless.InstagramFilters;
 import java.util.function.Function;
 
 public class FilterProcessor {
-    public Image applyFilter(Image image, Function<Image, Image> function) {
-        if (image == null || function == null) {
+    public Image applyFilter(Image image, Function<Image, Image> filter) {
+        if (image == null || filter == null) {
             return null;
         }
-        return function.apply(image);
+        return filter.apply(image);
     }
 
-    public Function<Image, Image> combineFilters(Function<Image, Image> firstFunction, Function<Image, Image> secondFunction) {
-        if (firstFunction == null || secondFunction == null) {
+    public Function<Image, Image> combineFilters(Function<Image, Image> firstFilter, Function<Image, Image> secondFilter) {
+        if (firstFilter == null || secondFilter == null) {
             return null;
         }
-        return firstFunction.andThen(secondFunction);
+        return firstFilter.andThen(secondFilter);
     }
 }
