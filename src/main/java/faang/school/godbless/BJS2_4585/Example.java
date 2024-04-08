082777f0afe0fba7e8eba3bae5346257d4ce76a2
Sergei Agafonov <sergeyagafonov.work@gmail.com>
2024-04-08 21:47:43 +0700

    Resolve the issue with array reversal logic

--- src/main/java/faang/school/godbless/BJS2_4585/Example.java
+++ src/main/java/faang/school/godbless/BJS2_4585/Example.java
@@ -2,14 +2,12 @@ package faang.school.godbless.BJS2_4585;
 
 public class Example {
 
-    public static void reverse(int[] nums) {
+    public static int[] reverse(int[] nums) {
         int arrayLength = nums.length;
         int[] arrayTemp = new int[arrayLength];
         for (int i = 0; i < nums.length; i++) {
             arrayTemp[i] = nums[(arrayLength - 1) - i];
         }
-        for (int i = 0; i < nums.length; i++) {
-            nums[i] = arrayTemp[i];
-        }
+        return arrayTemp;
     }
 }
--- src/main/java/faang/school/godbless/BJS2_4585/Main.java
+++ src/main/java/faang/school/godbless/BJS2_4585/Main.java
@@ -5,9 +5,8 @@ import java.util.Arrays;
 public class Main {
 
     public static void main(String[] args) {
-        int[] nums = {1, 2, 3, 4, 5};
-        Example.reverse(nums);
-        System.out.println(Arrays.toString(nums));
+        int[] numbers = {1, 2, 3, 4, 5};
+        System.out.println(Arrays.toString(Example.reverse(numbers)));
     }
 
 }
