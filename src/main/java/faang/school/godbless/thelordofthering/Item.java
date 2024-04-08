55d2272a5b4e5672cfa10300b896815bf28d7763
Demchenkov7 <andreydemchenkov@yandex.ru>
2024-04-08 23:09:55 +0300

    Задача: Lord of the Rings RPG

---
+++ src/main/java/faang/school/godbless/thelordofthering/Item.java
@@ -0,0 +1,13 @@
+package faang.school.godbless.thelordofthering;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.ToString;
+
+@AllArgsConstructor
+@Getter
+@ToString
+public class Item {
+    private String name;
+    private int value;
+}
