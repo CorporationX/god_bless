50243b9d839c2e13c664dd7c5d2bb1fb7c82267a
Daniyar Kabdulin <mrdocula@gmail.com>
2024-04-08 20:50:49 +0300

    все исправил.

--- src/main/java/faang/school/godbless/groupAgeOfUsers/User.java
+++ src/main/java/faang/school/godbless/groupAgeOfUsers/User.java
@@ -1,59 +1,32 @@
 package faang.school.godbless.groupAgeOfUsers;
 
+import lombok.Data;
+
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-
+@Data
 public class User {
     private String name;
     private int age;
-    private String placeOfJob;
+    private String workplace;
     private String address;
 
-    public User(String name, int age, String placeOfJob, String address) {
+    public User(String name, int age, String workplace, String address) {
         this.name = name;
         this.age = age;
-        this.placeOfJob = placeOfJob;
+        this.workplace = workplace;
         this.address = address;
     }
 
-    public static Map<Integer, List<User>> groupUsers(List<User> userList) {
-        Map<Integer, List<User>> mapGroupUsers = new HashMap<>();
+    public static Map<Integer, List<User>> groupUsers(List<User> users) {
+        Map<Integer, List<User>> groupedUsers = new HashMap<>();
 
-        for (User user : userList) {
-            int userAge = user.getAge();
-            if (!mapGroupUsers.containsKey(userAge)) {
-                mapGroupUsers.put(userAge, new ArrayList<>());
-            }
-            mapGroupUsers.get(userAge).add(user);
+        for (User user : users) {
+            groupedUsers.computeIfAbsent(user.getAge(), key -> new ArrayList<>()).add(user);
         }
-        return mapGroupUsers;
-    }
 
-    public String getName() {
-        return name;
-    }
-
-    public int getAge() {
-        return age;
-    }
-
-    public String getPlaceOfJob() {
-        return placeOfJob;
-    }
-
-    public String getAddress() {
-        return address;
-    }
-
-    @Override
-    public String toString() {
-        return "User{" +
-                "name='" + this.name + '\'' +
-                ", age=" + this.age +
-                ", placeOfJob='" + this.placeOfJob + '\'' +
-                ", address='" + this.address + '\'' +
-                '}';
+        return groupedUsers;
     }
 }
