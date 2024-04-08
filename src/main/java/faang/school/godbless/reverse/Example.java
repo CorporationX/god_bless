d2a5d2de54602528c35b70e7e8bfba08d686f392
Aleksey <lekha.bolt2004@gmail.com>
2024-04-08 12:39:44 +0300

    reverse!

--- src/main/java/faang/school/godbless/reverse/Example.java
+++ src/main/java/faang/school/godbless/reverse/Example.java
@@ -1,10 +1,18 @@
 package faang.school.godbless.reverse;
 
-import java.util.Arrays;
-import java.util.Collections;
+//import java.util.Arrays;
+//import java.util.Collections;
 
 public class Example {
     static void reverse(Integer[] arr){
-        Collections.reverse(Arrays.asList(arr));
+        int start = 0;
+        int end = arr.length - 1;
+        while (start < end){
+            int temp = arr[start];
+            arr[start] = arr[end];
+            arr[end] = temp;
+            start++;
+            end--;
+        }
     }
 }
