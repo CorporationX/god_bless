5282a21c8d8cdb747c1652523a3505b7503eba88
Tamzhid Babutsyngueva <btamzhid@gmail.com>
2024-04-08 23:23:53 +0800

    BJS2-4951 - Коллекционируем пользователей. Попытка №2

--- src/main/java/collectingUsers/Main.java
+++ src/main/java/collectingUsers/Main.java
@@ -2,16 +2,18 @@ package collectingUsers;
 
 import java.util.ArrayList;
 import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
 
 public class Main {
     public static void main(String[] args) {
 
-        HashSet<String> activitiesOfUserA = new HashSet<>();
+        Set<String> activitiesOfUserA = new HashSet<>();
         activitiesOfUserA.add("сон");
         activitiesOfUserA.add("поиск мемов");
         activitiesOfUserA.add("что-то полезное");
 
-        HashSet<String> activitiesOfUserB = new HashSet<>();
+        Set<String> activitiesOfUserB = new HashSet<>();
         activitiesOfUserB.add("чтение");
         activitiesOfUserB.add("что-то важное");
         activitiesOfUserB.add("что-то бесполезное");
@@ -19,11 +21,11 @@ public class Main {
         User userA = new User(1, "Anna", 12, activitiesOfUserA);
         User userB = new User(2, "Bato", 15, activitiesOfUserB);
 
-        ArrayList<User> usersList = new ArrayList<User>();
+        List<User> usersList = new ArrayList<User>();
         usersList.add(userA);
         usersList.add(userB);
 
-        HashSet<String> activitiesSet = new HashSet<String>();
+        Set<String> activitiesSet = new HashSet<String>();
         activitiesSet.add("что-то бесполезное");
         activitiesSet.add("что-то полезное");
         activitiesSet.add("что-то важное");
--- src/main/java/collectingUsers/User.java
+++ src/main/java/collectingUsers/User.java
@@ -3,28 +3,32 @@ package collectingUsers;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
 
 public class User {
     private int id;
     private String name;
     private int age;
-    private HashSet<String> activities;
+    private Set<String> activities;
 
-    public User(int id, String name, int age, HashSet<String> activities) {
+    public User(int id, String name, int age, Set<String> activities) {
         this.id = id;
         this.name = name;
         this.age = age;
         this.activities = activities;
     }
 
-    public static HashMap<User, String> findHobbyLovers(ArrayList<User> users, HashSet<String> activitiesSet) {
+    public static Map<User, String> findHobbyLovers(List<User> users, Set<String> activitiesSet) {
 
-        HashMap<User, String> resultMap = new HashMap<User, String>();
+        Map<User, String> resultMap = new HashMap<User, String>();
 
         for (User user : users) {
             for (String activity : user.activities) {
                 if (activitiesSet.contains(activity)) {
                     resultMap.put(user, activity);
+                    break;
                 }
             }
         }
