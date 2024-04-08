b4470f7e7452be7f5fbee71c558751fb2b15ec1d
Nikolai Gnidenko <gnidenkonn@gmail.com>
2024-04-08 17:28:24 +0300

    Исправил: мапа не статическая больше

--- src/main/java/meta/NotificationManager.java
+++ src/main/java/meta/NotificationManager.java
@@ -5,13 +5,13 @@ import java.util.Map;
 import java.util.function.Consumer;
 
 public class NotificationManager {
-    private static final Map<String, Consumer<Notification>> NOTIFICATIONS = new HashMap<>();
+    private final Map<String, Consumer<Notification>> notification = new HashMap<>();
 
     public void registerHandler(String type, Consumer<Notification> handler){
-        NOTIFICATIONS.put(type, handler);
+        notification.put(type, handler);
     }
 
     public void sendNotification(Notification notification) {
-        NOTIFICATIONS.get(notification.getType()).accept(notification);
+        this.notification.get(notification.getType()).accept(notification);
     }
 }
