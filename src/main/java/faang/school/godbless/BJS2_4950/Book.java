aa0507de9c327176904880f8d943eb8712083930
Alex Vishnevskiy <vishgioall@gmail.com>
2024-04-08 19:55:39 +0300

    добавил аннотацию equals и hashcode

--- src/main/java/faang/school/godbless/BJS2_4950/Book.java
+++ src/main/java/faang/school/godbless/BJS2_4950/Book.java
@@ -2,12 +2,13 @@ package faang.school.godbless.BJS2_4950;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.EqualsAndHashCode;
 
 import java.util.Map;
-import java.util.Objects;
 
 @Data
 @AllArgsConstructor
+@EqualsAndHashCode
 public class Book {
     private String title;
     private String author;
@@ -33,18 +34,5 @@ public class Book {
             System.out.println(entry.getKey().getTitle() + " " + entry.getKey().getAuthor() + " " +
                     entry.getKey().getYear() + ": " + entry.getValue());
         }
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        Book book = (Book) o;
-        return year == book.year && Objects.equals(title, book.title) && Objects.equals(author, book.author);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(title, author, year);
     }
 }
