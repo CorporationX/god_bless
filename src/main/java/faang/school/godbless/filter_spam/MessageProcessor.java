7a2798078101615632e91e509f40fc284a2137fd
DimaMinin <91825998+DimaMinin@users.noreply.github.com>
2024-04-08 12:58:42 +0300

    Task: https://faang-school.atlassian.net/browse/BJS2-5017

---
+++ src/main/java/faang/school/godbless/filter_spam/MessageProcessor.java
@@ -0,0 +1,11 @@
+package faang.school.godbless.filter_spam;
+
+import java.util.List;
+
+public class MessageProcessor {
+
+    public boolean processMessage(String message, List<MessageFilter> messageFilters) {
+        // Не совсем понял что в каком случае возвращать. Решил так: если все фильтры вернули true - возвращаю true, иначе false
+        return messageFilters.stream().allMatch(messageFilter -> messageFilter.filter(message));
+    }
+}
