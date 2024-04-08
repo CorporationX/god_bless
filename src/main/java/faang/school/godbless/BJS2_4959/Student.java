8a93356a3a5a271c279c9a71775082bc4c84f9ad
Alex Vishnevskiy <vishgioall@gmail.com>
2024-04-08 21:32:45 +0300

    создал клас Student, написал в нем методы: группировки студентов по факультетам и курсам, добавление студента на определенный курс и факультет, удаление студента из определенного курса и факультета, поиск всех студентов по определенному курсу и факультету и вывод всех студентов

--- src/main/java/faang/school/godbless/BJS2_4959/Student.java
+++ src/main/java/faang/school/godbless/BJS2_4959/Student.java
@@ -3,6 +3,7 @@ package faang.school.godbless.BJS2_4959;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
+import lombok.ToString;
 
 import java.util.HashMap;
 import java.util.HashSet;
@@ -12,34 +13,44 @@ import java.util.Set;
 @Data
 @AllArgsConstructor
 @EqualsAndHashCode
+@ToString
 public class Student {
     private String name;
     private String faculty;
     private int year;
 
-    public static Map<Student, Integer> convertToHashMap(Set<Student> students) {
-        Map<Student, Integer> studentsConvert = new HashMap<>();
+    private static Map<String, Integer> getGroup(String faculty, int year) {
+        Map<String, Integer> result = new HashMap<>();
+        result.put(faculty, year);
+        return result;
+    }
+
+    public static Map<Map<String, Integer>, Set<Student>> addStudentsByGroups(Set<Student> students) {
+        Map<Map<String, Integer>, Set<Student>> result = new HashMap<>();
         for (Student student : students) {
-            studentsConvert.put(student, student.getYear());
+            Student.addStudent(result, student);
         }
-        return studentsConvert;
+        return result;
     }
 
-    public static void addStudent(Map<Student, Integer> students, Student student) {
-        students.put(student, student.getYear());
+    public static void addStudent(Map<Map<String, Integer>, Set<Student>> students, Student student) {
+        students.computeIfAbsent(getGroup(student.getFaculty(), student.getYear()), v -> new HashSet<>()).add(student);
     }
 
-    public static void deleteStudent(Map<Student, Integer> students, Student student) {
-        students.remove(student);
+    public static void deleteStudent(Map<Map<String, Integer>, Set<Student>> students, Student student) {
+        students.remove(getGroup(student.getFaculty(), student.getYear()));
     }
 
-    public static Set<Student> findStudentsByFaculty(Map<Student, Integer> students, String faculty, int year) {
-        Set<Student> studentsResult = new HashSet<>();
-        for (Map.Entry<Student, Integer> entry : students.entrySet()) {
-            if (entry.getKey().getFaculty().equals(faculty) && entry.getKey().getYear() == year) {
-                studentsResult.add(entry.getKey());
+    public static Set<Student> findStudentsByFaculty(Map<Map<String, Integer>, Set<Student>> students, String faculty, int year) {
+        return students.get(getGroup(faculty, year));
+    }
+
+    public static void printAllStudents(Map<Map<String, Integer>, Set<Student>> students) {
+        for (Map.Entry<Map<String, Integer>, Set<Student>> entry : students.entrySet()) {
+            System.out.println(entry.getKey().toString());
+            for (Student student : entry.getValue()) {
+                System.out.println(student.toString());
             }
         }
-        return studentsResult;
     }
 }
