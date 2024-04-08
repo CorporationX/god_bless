e93567071ceb98daadb4bd57dde99b4e143c8a38
Maxim Suhanov <maksimka_13.05.2004@mail.ru>
2024-04-08 16:45:41 +0400

    BJS2-4456-_makakcuk_

--- src/main/java/elk/Main.java
+++ src/main/java/elk/Main.java
@@ -1,4 +1,4 @@
-package ELK;
+package elk;
 
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -8,21 +8,22 @@ import java.util.Collections;
 import java.util.Comparator;
 
 public class Main {
-    private static Map<User, List<Query>> userListMap = new HashMap<>();
+    private static Map<User, List<Query>> userQueries = new HashMap<>();
+
     public static void main(String[] args) {
         User user1 = new User(1, "Ivan");
         List<Query> queryList1 = new ArrayList<>();
         queryList1.add(new Query(1, "Java", "20240502"));
         queryList1.add(new Query(2, "Python", "2023057"));
         queryList1.add(new Query(3, "Scala", "20221204"));
-        addQueryOfUser(user1, queryList1);
+        addUserWithQueries(user1, queryList1);
 
         User user2 = new User(2, "Nikita");
         List<Query> queryList2 = new ArrayList<>();
         queryList2.add(new Query(1, "Java", "19990324"));
         queryList2.add(new Query(2, "Python", "20100825"));
         queryList2.add(new Query(3, "Scala", "20150623"));
-        addQueryOfUser(user2, queryList2);
+        addUserWithQueries(user2, queryList2);
 
         printAllUserAndQueries();
 
@@ -36,30 +37,42 @@ public class Main {
 
         printHistoryOfQueriesForEachUser();
     }
-    public static void addQueryOfUser(User user, List<Query> list){
-        userListMap.put(user, list);
+    private static void addUserWithQueries(User user, List<Query> queries){
+        if(user == null){
+            System.out.println("User is null");
+            return;
+        }
+        userQueries.put(user, queries);
     }
 
-    public static void addNewQueryToUser(User user, Query query){
-        userListMap.computeIfAbsent(user, key -> new ArrayList<>()).add(query);
+    private static void addNewQueryToUser(User user, Query query){
+        if(user == null){
+            System.out.println("User is null");
+            return;
+        }
+        userQueries.computeIfAbsent(user, key -> new ArrayList<>()).add(query);
     }
 
-    public static void removeUser(User user){
-        userListMap.remove(user);
+    private static void removeUser(User user){
+        if(user == null){
+            System.out.println("User is null");
+            return;
+        }
+        userQueries.remove(user);
     }
 
-    public static void printAllUserAndQueries(){
-        for(Map.Entry<User, List<Query>> entry: userListMap.entrySet()){
+    private static void printAllUserAndQueries(){
+        for(Map.Entry<User, List<Query>> entry: userQueries.entrySet()){
             System.out.println(entry.getKey() + ": ");
             entry.getValue().forEach(System.out::println);
         }
         System.out.println();
     }
 
-    public static void printHistoryOfQueriesForEachUser(){
-        for(Map.Entry<User, List<Query>> entry: userListMap.entrySet()){
+    private static void printHistoryOfQueriesForEachUser(){
+        for(Map.Entry<User, List<Query>> entry: userQueries.entrySet()){
             System.out.println(entry.getKey() + ": ");
-            Comparator<Query> comparator = (o1, o2) -> o2.timestamp().compareTo(o1.timestamp());
+            Comparator<Query> comparator = (o1, o2) -> o2.getTimestamp().compareTo(o1.getTimestamp());
             Collections.sort(entry.getValue(), comparator);
             entry.getValue().forEach(System.out::println);
         }
