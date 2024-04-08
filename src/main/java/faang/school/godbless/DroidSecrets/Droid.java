c5670188b24ed6bd6fcb4935d01569c8a9ee55e5
Tem <tipokruto014@gmail.com>
2024-04-08 21:09:16 +0300

    refactor: вынес encoder и decoder в глобальные константы

--- src/main/java/faang/school/godbless/DroidSecrets/Droid.java
+++ src/main/java/faang/school/godbless/DroidSecrets/Droid.java
@@ -3,23 +3,18 @@ package faang.school.godbless.DroidSecrets;
 import java.util.function.BiFunction;
 
 public class Droid {
-    private final DroidMessageEncryptor encoder;
-    private final DroidMessageEncryptor decoder;
-
-    public Droid() {
-        this.encoder = getCoder((ch, key) -> (char) (ch + key));
-        this.decoder = getCoder((ch, key) -> (char) (ch - key));
-    }
+    private static final DroidMessageEncryptor ENCODER = getCoder((ch, key) -> (char) (ch + key));
+    private static final DroidMessageEncryptor DECODER = getCoder((ch, key) -> (char) (ch - key));
 
     public String sendEncryptedMessage(String message, int encryptionKey) {
-        return encoder.encrypt(message, encryptionKey);
+        return ENCODER.encrypt(message, encryptionKey);
     }
 
     public String receiveEncryptedMessage(String encryptedMessage, int encryptionKey) {
-        return decoder.encrypt(encryptedMessage, encryptionKey);
+        return DECODER.encrypt(encryptedMessage, encryptionKey);
     }
 
-    private DroidMessageEncryptor getCoder(BiFunction<Character, Integer, Character> codingLogic) {
+    private static DroidMessageEncryptor getCoder(BiFunction<Character, Integer, Character> codingLogic) {
         return (mess, key) -> {
             StringBuilder decoded = new StringBuilder();
             for (char ch : mess.toCharArray()) {
