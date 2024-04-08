4be03e6ac40ab55f96bd5b8077ed1c959bb766bd
ekoregin <evgkoregin@gmail.com>
2024-04-08 13:11:04 +0300

    Task: https://faang-school.atlassian.net/browse/BJS2-5018

---
+++ src/main/java/faang/school/godbless/bjs2_5018/Calculator.java
@@ -0,0 +1,7 @@
+package faang.school.godbless.bjs2_5018;
+
+@FunctionalInterface
+public interface Calculator<T> {
+
+    T calc(T one, T two);
+}
---
+++ src/main/java/faang/school/godbless/bjs2_5018/Main.java
@@ -0,0 +1,36 @@
+package faang.school.godbless.bjs2_5018;
+
+import lombok.extern.slf4j.Slf4j;
+
+import java.util.List;
+
+@Slf4j
+public class Main {
+
+    public static void main(String[] args) {
+        List<Integer> numbers = List.of(1, 2, 3, 4, 5);
+
+        product(numbers);
+        sum(numbers);
+    }
+
+    public static int calculate(List<Integer> nums, Calculator<Integer> calculator) throws IllegalArgumentException {
+        int result = nums.get(0);
+        for (int i = 1; i < nums.size(); i++) {
+            result = calculator.calc(nums.get(i), result);
+        }
+        return result;
+    }
+
+    public static int product(List<Integer> nums) {
+        int result = calculate(nums, (num1, num2) -> num1 * num2);
+        log.info("Product of numbers results: {}", result);
+        return result;
+    }
+
+    public static int sum(List<Integer> nums) {
+        int result =  calculate(nums, Integer::sum);
+        log.info("Sum result numbers: {}", result);
+        return result;
+    }
+}
