0e42c43cea5a405b7511a04f07ae5e627e8c6ebd
Tem <tipokruto014@gmail.com>
2024-04-08 19:33:13 +0300

    сделал рефакторинг кода

--- src/main/java/faang/school/godbless/DroidSecrets/Droid.java
+++ src/main/java/faang/school/godbless/DroidSecrets/Droid.java
@@ -1,25 +1,31 @@
 package faang.school.godbless.DroidSecrets;
 
+import java.util.function.BiFunction;
+
 public class Droid {
+    private final DroidMessageEncryptor encoder;
+    private final DroidMessageEncryptor decoder;
+
+    public Droid() {
+        this.encoder = getCoder((ch, key) -> (char) (ch + key));
+        this.decoder = getCoder((ch, key) -> (char) (ch - key));
+    }
+
     public String sendEncryptedMessage(String message, int encryptionKey) {
-        DroidMessageEncryptor encoder = (mess, key) -> {
-            StringBuilder coded = new StringBuilder();
-            for (char ch : mess.toCharArray()) {
-                coded.append(Character.valueOf((char) (ch + key)));
-            }
-            return coded.toString();
-        };
         return encoder.encrypt(message, encryptionKey);
     }
 
     public String receiveEncryptedMessage(String encryptedMessage, int encryptionKey) {
-        DroidMessageEncryptor decoder = (mess, key) -> {
+        return decoder.encrypt(encryptedMessage, encryptionKey);
+    }
+
+    private DroidMessageEncryptor getCoder(BiFunction<Character, Integer, Character> codingLogic) {
+        return (mess, key) -> {
             StringBuilder decoded = new StringBuilder();
             for (char ch : mess.toCharArray()) {
-                decoded.append(Character.valueOf((char) (ch - key)));
+                decoded.append(codingLogic.apply(ch, key));
             }
             return decoded.toString();
         };
-        return decoder.encrypt(encryptedMessage, encryptionKey);
     }
 }
