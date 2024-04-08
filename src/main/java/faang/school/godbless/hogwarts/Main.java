fb8d89975426cf7b050467f7d9b163f7d9ee5efc
Demchenkov7 <andreydemchenkov@yandex.ru>
2024-04-08 22:11:53 +0300

    micro fix

--- src/main/java/faang/school/godbless/hogwarts/Main.java
+++ src/main/java/faang/school/godbless/hogwarts/Main.java
@@ -11,6 +11,5 @@ public class Main {
         spellCaster.cast(alohamora, spell -> "The door is unlocked by " + spell);
         spellCaster.cast(lumos, spell -> "A beam of light is created by " + spell);
         spellCaster.cast(expelliarmus, spell -> "The opponent is disarmed by " + spell);
-
     }
 }
