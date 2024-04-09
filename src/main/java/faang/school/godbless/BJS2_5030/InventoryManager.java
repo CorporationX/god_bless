3165381a7eb8bef1e2dd21b243f9c2b2463b732e
TOPIKS <esaakyan03@gmail.com>
2024-04-09 10:48:16 +0300

    https://faang-school.com/courses/7ynd2xdg/n7dvx4d4

---
+++ src/main/java/faang/school/godbless/BJS2_5030/InventoryManager.java
@@ -0,0 +1,23 @@
+package faang.school.godbless.BJS2_5030;
+
+import java.util.function.Consumer;
+import java.util.function.Function;
+import java.util.function.Predicate;
+
+public class InventoryManager {
+    public void addItem(Character character, Item item, Consumer<Item> consumer) {
+        consumer.accept(item);
+    }
+
+    public void removeItem(Character character, Predicate<Item> predicate) {
+        character.getInventory().removeIf(predicate);
+    }
+
+    public void updateItem(Character character, Predicate<Item> predicate, Function<Item, Item> function) {
+        for (Item item : character.getInventory()) {
+            if (predicate.test(item)) {
+                character.getInventory().set(character.getInventory().indexOf(item), function.apply(item));
+            }
+        }
+    }
+}
