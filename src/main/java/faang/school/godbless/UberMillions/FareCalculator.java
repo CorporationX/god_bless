85d289e2d23b148587b3f6e04c4abf18cd9b1079
Tem <tipokruto014@gmail.com>
2024-04-09 12:09:49 +0300

    style: изменил нейминг переменной

--- src/main/java/faang/school/godbless/UberMillions/FareCalculator.java
+++ src/main/java/faang/school/godbless/UberMillions/FareCalculator.java
@@ -3,7 +3,7 @@ package faang.school.godbless.UberMillions;
 import java.util.function.BiFunction;
 
 public class FareCalculator {
-    public double calculateFare(double range, double time, BiFunction<Double, Double, Double> priceCalculation) {
-        return priceCalculation.apply(range, time);
+    public double calculateFare(double distance, double time, BiFunction<Double, Double, Double> priceCalculation) {
+        return priceCalculation.apply(distance, time);
     }
 }
