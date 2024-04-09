a35137cc38c0acd520e4fb0a80106020936b2293
Kayfral <79108276+Kayfral@users.noreply.github.com>
2024-04-09 20:02:11 +0300

    Задание: Meta-вселенная? BJS2-5086
    Исправление ошибок

--- src/main/java/faang/school/godbless/BJS2_5086/NotificationManager.java
+++ src/main/java/faang/school/godbless/BJS2_5086/NotificationManager.java
@@ -2,16 +2,20 @@ package faang.school.godbless.BJS2_5086;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.NoSuchElementException;
 import java.util.function.Consumer;
 
 public class NotificationManager {
-    private final Map<String, Consumer<Notification>> notificationMap = new HashMap<>();
+    private final Map<String, Consumer<Notification>> handlers = new HashMap<>();
 
-    public void registerHandler(String typeMessage, Consumer<Notification> notificationConsumer){
-        notificationMap.put(typeMessage, notificationConsumer);
+    public void registerHandler(String type, Consumer<Notification> handler){
+        handlers.put(type, handler);
     }
 
     public void sendNotification(Notification notification){
-        notificationMap.get(notification.getType()).accept(notification);
+        if (!handlers.containsKey(notification.getType())) {
+            throw new NoSuchElementException("Не удалось найти данный тип обработчика: " + notification.getType());
+        }
+        handlers.get(notification.getType()).accept(notification);
     }
 }
