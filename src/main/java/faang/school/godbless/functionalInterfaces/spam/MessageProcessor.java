4b38ddbe2f54180908bdfb96828371bb2ebf9aa3
nadir <den4ik.strelok@gmail.com>
2024-04-09 23:08:29 +0300

    Упростил processMessage, использовав stream().allMatch()

--- src/main/java/faang/school/godbless/functionalInterfaces/spam/MessageProcessor.java
+++ src/main/java/faang/school/godbless/functionalInterfaces/spam/MessageProcessor.java
@@ -1,18 +1,9 @@
 package faang.school.godbless.functionalInterfaces.spam;
 
 import java.util.List;
-import java.util.concurrent.atomic.AtomicBoolean;
 
 public class MessageProcessor {
     public boolean processMessage(String  message, List<MessageFilter> filters) {
-        AtomicBoolean isFiltered = new AtomicBoolean(true);
-
-        filters.forEach(currentFilter -> {
-            if(!currentFilter.filter(message)) {
-                isFiltered.set(false);
-            }
-        });
-
-        return isFiltered.get();
+        return filters.stream().allMatch(currentFilter -> currentFilter.filter(message));
     }
 }
