366c16da80a0eb0506dde84d914b9a0f96c8819b
Rick <RickHammerson@ya.ru>
2024-04-09 03:50:31 +0300

    Завершающий коммит

--- src/main/java/faang/school/godbless/module/core/register/Main.java
+++ src/main/java/faang/school/godbless/module/core/register/Main.java
@@ -1,24 +1,22 @@
 package faang.school.godbless.module.core.register;
 
 public class Main {
-    private static final String JOB_GOOGLE = "Google";
-    private static final String JOB_UBER = "Uber";
-    private static final String JOB_AMAZON = "Amazon";
-    private static final String ADDRESS_LONDON = "London";
-    private static final String ADDRESS_NEW_YORK = "New York";
-    private static final String ADDRESS_AMSTERDAM = "Amsterdam";
+    private static final String CORRECT_NAME = "Vova";
+    private static final int CORRECT_YEAR = 30;
+    private static final String CORRECT_JOB = "Google";
+    private static final String CORRECT_ADDRESS = "London";
     
     public static void main(String[] args) {
-        registerUser("", 20, JOB_GOOGLE, ADDRESS_LONDON);
-        registerUser(" ", 20, JOB_UBER, ADDRESS_NEW_YORK);
-        registerUser(null, 20, JOB_AMAZON, ADDRESS_AMSTERDAM);
-        registerUser("Vlad", 17, JOB_UBER, ADDRESS_LONDON);
-        registerUser("Лилия", 31, JOB_UBER, "Улица Пушкина");
-        registerUser("Володька", 31, "Фрезировщик", ADDRESS_LONDON);
-        registerUser("Димка", 31, JOB_AMAZON, ADDRESS_LONDON);
+        validateUserRegistration("", CORRECT_YEAR, CORRECT_JOB, CORRECT_ADDRESS);
+        validateUserRegistration(" ", CORRECT_YEAR, CORRECT_JOB, CORRECT_ADDRESS);
+        validateUserRegistration(null, CORRECT_YEAR, CORRECT_JOB, CORRECT_ADDRESS);
+        validateUserRegistration(CORRECT_NAME, 17, CORRECT_JOB, CORRECT_ADDRESS);
+        validateUserRegistration(CORRECT_NAME, CORRECT_YEAR, "Фрезировщик", CORRECT_ADDRESS);
+        validateUserRegistration(CORRECT_NAME, CORRECT_YEAR, CORRECT_JOB, "Улица Пушкина");
+        validateUserRegistration(CORRECT_NAME, CORRECT_YEAR, CORRECT_JOB, CORRECT_ADDRESS);
     }
     
-    public static void registerUser(String name, int year, String job, String address) {
+    public static void validateUserRegistration(String name, int year, String job, String address) {
         try {
             System.out.printf("Пользовать успешно создан: %s", new User(name, year, job, address));
         } catch (Exception e) {
