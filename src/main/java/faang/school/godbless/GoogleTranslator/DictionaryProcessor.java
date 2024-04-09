858546513695a2e5f1547f70198bcb284cce39a1
ethiqque <danielpyld.work@gmail.com>
2024-04-09 07:16:16 +0300

    BJS2-5046 Java 8: Функциональные интерфейсы, Лямбды: Google Переводчик под нашим контролем

---
+++ src/main/java/faang/school/godbless/GoogleTranslator/DictionaryProcessor.java
@@ -0,0 +1,17 @@
+package faang.school.godbless.GoogleTranslator;
+
+import java.util.function.BiConsumer;
+
+public class DictionaryProcessor {
+    public void processWord(String word, String translatedWord, BiConsumer<String, String> biConsumer) {
+        try {
+            if (biConsumer != null) {
+                biConsumer.accept(word, translatedWord);
+            } else {
+                throw new NullPointerException("BiConsumer argument is null");
+            }
+        } catch (NullPointerException e) {
+            System.out.println("Exception: " + e.getMessage());
+        }
+    }
+}
