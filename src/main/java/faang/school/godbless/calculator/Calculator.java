d58d068da4fe805ac8c2e660e757dd35599e1039
egorp5119 <egorplo@gmail.com>
2024-04-09 23:18:04 +0300

    BJS2-5151-Calculator

---
+++ src/main/java/faang/school/godbless/calculator/Calculator.java
@@ -0,0 +1,5 @@
+package faang.school.godbless.calculator;
+@FunctionalInterface
+public interface Calculator<T> {
+    T calc(T firsNumber, T secondNumber);
+}
--- src/main/java/faang/school/godbless/gameOfThrones/House.java
+++
@@ -1,9 +0,0 @@
-package faang.school.godbless.gameOfThrones;
-
-import lombok.Data;
-
-@Data
-public class House {
-    private final String name;
-    private final String sigil;
-}
