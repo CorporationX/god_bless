0a6fe500f66cd5d435d8e02beaecfaf471e58ca9
TOPIKS <esaakyan03@gmail.com>
2024-04-09 20:04:20 +0300

    итем добавляется в инвентарь, преименованы обработчики

--- src/main/java/faang/school/godbless/BJS2_5030/InventoryManager.java
+++ src/main/java/faang/school/godbless/BJS2_5030/InventoryManager.java
@@ -1,22 +1,26 @@
 package faang.school.godbless.BJS2_5030;
 
+import java.util.List;
 import java.util.function.Consumer;
 import java.util.function.Function;
 import java.util.function.Predicate;
 
 public class InventoryManager {
-    public void addItem(Character character, Item item, Consumer<Item> consumer) {
-        consumer.accept(item);
+    public void addItem(Character character, Item item, Consumer<Item> onSuccess) {
+        character.getInventory().add(item);
+        onSuccess.accept(item);
     }
 
-    public void removeItem(Character character, Predicate<Item> predicate) {
-        character.getInventory().removeIf(predicate);
+    public void removeItem(Character character, Predicate<Item> condition) {
+        character.getInventory().removeIf(condition);
     }
 
-    public void updateItem(Character character, Predicate<Item> predicate, Function<Item, Item> function) {
-        for (Item item : character.getInventory()) {
-            if (predicate.test(item)) {
-                character.getInventory().set(character.getInventory().indexOf(item), function.apply(item));
+    public void updateItem(Character character, Predicate<Item> condition, Function<Item, Item> updateFunction) {
+        List<Item> items = character.getInventory();
+        int sizeListInventory = items.size();
+        for (int i = 0; i < sizeListInventory; i++) {
+            if (condition.test(items.get(i))) {
+                items.set(i, updateFunction.apply(items.get(i)));
             }
         }
     }
