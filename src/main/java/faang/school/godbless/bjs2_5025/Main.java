1adb4267082bf3c65db9ce6d8d03da09f28bc9d4
ekoregin <evgkoregin@gmail.com>
2024-04-09 10:05:41 +0300

    BJS2-5025: Fix1 after review

--- src/main/java/faang/school/godbless/bjs2_5025/Main.java
+++ src/main/java/faang/school/godbless/bjs2_5025/Main.java
@@ -1,23 +1,22 @@
 package faang.school.godbless.bjs2_5025;
 
 import lombok.extern.slf4j.Slf4j;
-
-import java.util.concurrent.Callable;
+import java.util.function.Supplier;
 
 @Slf4j
 public class Main {
 
     public static void main(String[] args) {
-        Callable<String> task = () -> {
+        Supplier<String> task = () -> {
             System.out.println("Executing service");
             return "OK";
         };
-        withErrorHandling(task, exception -> "DEFAULT");
+        handleTaskWithError(task, exception -> "DEFAULT");
     }
 
-    public static <T> T withErrorHandling(Callable<T> function, ExceptionHandler<T> exception) {
+    public static <T> T handleTaskWithError(Supplier<T> function, ExceptionHandler<T> exception) {
         try {
-            return function.call();
+            return function.get();
         } catch (Exception e) {
             log.warn("Не удалось получить доступ к Remote Service. Вернем default значение");
             return exception.handle(e);
