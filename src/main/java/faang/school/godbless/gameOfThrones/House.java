0090634ac90702593a4b0ef1bd723bb3d4e95264
egorp5119 <egorplo@gmail.com>
2024-04-09 13:57:50 +0300

    BJS2-4955-GOT-corrected

--- src/main/java/faang/school/godbless/gameOfThrones/House.java
+++ src/main/java/faang/school/godbless/gameOfThrones/House.java
@@ -1,19 +1,9 @@
 package faang.school.godbless.gameOfThrones;
 
+import lombok.Data;
+
+@Data
 public class House {
     private final String name;
     private final String sigil;
-
-    public House(String name, String sigil) {
-        this.name = name;
-        this.sigil = sigil;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public String getSigil() {
-        return sigil;
-    }
 }
--- src/main/java/faang/school/godbless/gameOfThrones/Main.java
+++ src/main/java/faang/school/godbless/gameOfThrones/Main.java
@@ -4,7 +4,7 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class Main {
-    private static Map<String, House> map = new HashMap<>();
+    private static Map<String, House> houses = new HashMap<>();
 
     public static void main(String[] args) {
         House stark = new House("Stark", "Wolf");
@@ -13,16 +13,16 @@ public class Main {
     }
 
     public static void addHouse(String name, House house){
-        map.put(name, house);
+        houses.put(name, house);
     }
     private static void removeHouse(String name){
-        map.remove(name);
+        houses.remove(name);
     }
     private static String getHouseSigil(String name){
-        return map.get(name).getSigil();
+        return houses.get(name).getSigil();
     }
     private static void getAllHouseInfo(){
-        for(Map.Entry<String, House> entry : map.entrySet()){
+        for(Map.Entry<String, House> entry : houses.entrySet()){
             System.out.println("House of "+entry.getKey() +" the sigil is "+entry.getValue().getSigil());
         }
     }
