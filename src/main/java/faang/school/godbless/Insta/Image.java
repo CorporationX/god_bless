a1e339486887b32045a08d92db06c2a8dbc32b11
egorp5119 <egorplo@gmail.com>
2024-04-09 16:27:43 +0300

    BJS2-5119-Instagram

---
+++ src/main/java/faang/school/godbless/Insta/Image.java
@@ -0,0 +1,11 @@
+package faang.school.godbless.Insta;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class Image {
+    private String Name;
+    private String imageDescription;
+}
