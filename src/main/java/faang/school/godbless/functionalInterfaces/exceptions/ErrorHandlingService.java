925386aaea4621e8fd2509c30c7a99852c8b2adf
nadir <den4ik.strelok@gmail.com>
2024-04-10 00:43:10 +0300

    BJS2-5153 (Обрабатываем ошибки красиво)

---
+++ src/main/java/faang/school/godbless/functionalInterfaces/exceptions/ErrorHandlingService.java
@@ -0,0 +1,19 @@
+package faang.school.godbless.functionalInterfaces.exceptions;
+
+import java.util.function.Function;
+import java.util.function.Supplier;
+
+public class ErrorHandlingService {
+
+    public static <T> T withErrorHandling(Supplier<T> regularAction, Function<Exception, T> errorAction) {
+        if (regularAction == null || errorAction == null) {
+            throw new IllegalArgumentException("Both args must be non-valued!");
+        }
+
+        try {
+            return regularAction.get();
+        } catch (Exception e) {
+            return errorAction.apply(e);
+        }
+    }
+}
