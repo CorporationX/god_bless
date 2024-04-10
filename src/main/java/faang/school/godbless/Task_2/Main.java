8a3f4b32036b32f156fc2449c16966672aee756e
Vadim Shibanov <vladimirowitch.vadim@gmail.com>
2024-04-10 22:59:35 +0300

    Commit

--- src/main/java/faang/school/godbless/Task_2/Main.java
+++ src/main/java/faang/school/godbless/Task_2/Main.java
@@ -8,6 +8,7 @@ import static faang.school.godbless.Task_2.User.groupUsers;
 
 public class Main {
     public static void main(String[] args) {
+        List<User> users = new ArrayList<>();
         User user1 = new User("Vadim", 18, "Google", "London");
         User user2 = new User("Mike", 88, "Uber", "New - York");
         User user3 = new User("Jessica", 29, "Amazon", "London");
@@ -15,17 +16,16 @@ public class Main {
         User user5 = new User("Clare", 29, "Amazon", "Amsterdam");
         User user6 = new User("Vadim", 29, "Amazon", "Amsterdam");
 
-        List<User> list = new ArrayList<>();
 
-        list.add(user1);
-        list.add(user2);
-        list.add(user3);
-        list.add(user4);
-        list.add(user5);
-        list.add(user6);
+        users.add(user1);
+        users.add(user2);
+        users.add(user3);
+        users.add(user4);
+        users.add(user5);
+        users.add(user6);
 
 
-        Map<Integer, List<User>> map = groupUsers(list);
+        Map<Integer, List<User>> map = groupUsers(users);
         for (Map.Entry<Integer, List<User>> entry : map.entrySet()) {
             Integer key = entry.getKey();
             List<User> listOfUsers = entry.getValue();
