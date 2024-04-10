f6c02e9e761eecb2db05888f5aabbc263d4d70f0
Vadim Merkulov <vm.merkulov@gmail.com>
2024-04-10 21:14:39 +0300

    BJS2-5190

---
+++ src/main/java/faang/school/godbless/hashmap/third/Book.java
@@ -0,0 +1,40 @@
+package faang.school.godbless.hashmap.third;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.Objects;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class Book {
+    private String title;
+    private String author;
+    private int year;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Book book = (Book) o;
+        return year == book.year && Objects.equals(title, book.title) && Objects.equals(author, book.author);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(title, author, year);
+    }
+
+    @Override
+    public String toString() {
+        return "Book{" +
+                "title='" + title + '\'' +
+                ", author='" + author + '\'' +
+                ", year=" + year +
+                '}';
+    }
+}
