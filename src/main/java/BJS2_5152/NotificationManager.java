f78d2787c87113af183847d4770c4da034f2d88c
Ilya <ilya-ermakov.2003@yandex.ru>
2024-04-10 09:43:03 +0300

    BJS2-5152: Meta-вселенная

---
+++ src/main/java/BJS2_5152/NotificationManager.java
@@ -0,0 +1,20 @@
+package BJS2_5152;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.function.Consumer;
+
+public class NotificationManager {
+    private Map<String, Consumer<Notification>> handlers = new HashMap<>();
+
+    public void registerHandler(String type, Consumer<Notification> notificationConsumer) {
+        handlers.put(type, notificationConsumer);
+    }
+
+    public void sendNotification(Notification notification) {
+        if (handlers.get(notification.getType()) == null) {
+            throw new NullPointerException("No handler with this name");
+        }
+        handlers.get(notification.getType()).accept(notification);
+    }
+}
