43dda8c4e82769e0a39da2632c07c741a83f2ad6
Ilya <ilya-ermakov.2003@yandex.ru>
2024-04-10 18:11:11 +0300

    BJS2-5182: Gmail Rich Filters

---
+++ src/main/java/BJS2_5182/EmailProcessor.java
@@ -0,0 +1,21 @@
+package BJS2_5182;
+
+import java.util.List;
+import java.util.function.Consumer;
+import java.util.function.Function;
+import java.util.function.Predicate;
+
+public class EmailProcessor {
+
+    public void processEmails(
+            List<Email> emails,
+            Predicate<Email> filter,
+            Consumer<Email> consumer,
+            Function<Email, String> function) {
+        for (Email email : emails) {
+            System.out.println(filter.test(email));
+            consumer.accept(email);
+            System.out.println(function.apply(email));
+        }
+    }
+}
