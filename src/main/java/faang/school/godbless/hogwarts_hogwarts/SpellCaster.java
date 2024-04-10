415d9fa2f07c7054bf4b8758f826283cbc066f44
KuvarzinAlex <fargus.wow@mail.ru>
2024-04-10 20:33:50 +0500

    BJS2-5084 hogwarts_hogwarts complete(solution 2)

--- src/main/java/faang/school/godbless/hogwarts_hogwarts/SpellCaster.java
+++ src/main/java/faang/school/godbless/hogwarts_hogwarts/SpellCaster.java
@@ -2,6 +2,12 @@ package faang.school.godbless.hogwarts_hogwarts;
 
 public class SpellCaster {
     public void cast(String spellName, SpellAction spellAction) {
+        if (spellName == null) {
+            throw new NullPointerException("The value is missing");
+        }
+        if (spellName.isEmpty()) {
+            throw new IllegalArgumentException("The meaning of the spell name is empty");
+        }
         System.out.println(spellAction.action(spellName));
     }
 }
