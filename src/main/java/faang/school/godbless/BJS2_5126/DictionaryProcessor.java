baa4933b503da579a2217d84ca09b942793b8c21
s.kolbasov <s.kolbasov@bftcom.com>
2024-04-10 10:58:29 +0300

    BJS2-5008 Google Переводчик под нашим контролем…

--- src/main/java/faang/school/godbless/BJS2_5126/DictionaryProcessor.java
+++ src/main/java/faang/school/godbless/BJS2_5126/DictionaryProcessor.java
@@ -3,7 +3,7 @@ package faang.school.godbless.BJS2_5126;
 import java.util.function.BiConsumer;
 
 public class DictionaryProcessor {
-  public void processWord(String word, String translate, BiConsumer<String, String> biConsumer) {
-    biConsumer.accept(word, translate);
+  public void processWord(String word, String translation, BiConsumer<String, String> function) {
+    function.accept(word, translation);
   }
 }
