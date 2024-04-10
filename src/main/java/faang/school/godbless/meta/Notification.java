1efd64b5f92946b3391d7d5b0a3a51b5e9fdb653
Daniyar Kabdulin <mrdocula@gmail.com>
2024-04-10 13:29:46 +0300

    Meta-вселенная?

---
+++ src/main/java/faang/school/godbless/meta/Notification.java
@@ -0,0 +1,11 @@
+package faang.school.godbless.meta;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class Notification {
+    private String type;
+    private String message;
+}
---
+++ src/main/java/faang/school/godbless/meta/NotificationManager.java
@@ -0,0 +1,21 @@
+package faang.school.godbless.meta;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.function.Consumer;
+
+public class NotificationManager {
+    private Map<String, Consumer<Notification>> notifications;
+
+    public NotificationManager() {
+        this.notifications = new HashMap<>();
+    }
+
+    public void registerHandler(String notificationID, Consumer<Notification> consumer) {
+        notifications.put(notificationID, consumer);
+    }
+
+    public void sendNotification(Notification notification) {
+        notifications.get(notification.getType()).accept(notification);
+    }
+}
