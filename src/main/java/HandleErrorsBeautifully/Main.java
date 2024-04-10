1374bb49db7997695f5fd61252e3ddb6414adc19
Maxim Suhanov <maksimka_13.05.2004@mail.ru>
2024-04-10 16:27:10 +0400

    BJS2-5160-_makakcuk_Обрабатываем ошибки красиво

--- src/main/java/HandleErrorsBeautifully/Main.java
+++ src/main/java/HandleErrorsBeautifully/Main.java
@@ -5,6 +5,7 @@ import lombok.extern.slf4j.Slf4j;
 @Slf4j
 public class Main {
     public static void main(String[] args) {
+
         Supplier<String> taskWithException = () -> {
             System.out.println("Executing service");
             throw new RuntimeException() ;
