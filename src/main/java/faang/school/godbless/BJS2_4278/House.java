2fa16f849ff1897770efd4aa8238e58b0ddd1270
nuraly <nuralysaparaliev2gmail.com>
2024-04-10 21:51:13 +0600

    BJS2-4278: Игра престолов

---
+++ src/main/java/faang/school/godbless/BJS2_4278/House.java
@@ -0,0 +1,12 @@
+package faang.school.godbless.BJS2_4278;
+
+import lombok.AllArgsConstructor;
+import lombok.ToString;
+
+@ToString
+@AllArgsConstructor
+public class House {
+
+    private String name;
+    private String sigil;
+}
