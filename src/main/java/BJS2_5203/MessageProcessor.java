d2c5e533dce42687b280decd50713f5afd2886ae
Ilya <ilya-ermakov.2003@yandex.ru>
2024-04-10 23:26:46 +0300

    да я нажимаю ctrl+alt+l, но видимо не так часто

--- src/main/java/BJS2_5203/MessageProcessor.java
+++ src/main/java/BJS2_5203/MessageProcessor.java
@@ -6,7 +6,7 @@ public class MessageProcessor {
 
     public boolean processMessage(String message, List<MessageFilter> filters) {
         for (MessageFilter filter : filters) {
-            if(!filter.filter(message)) {
+            if (!filter.filter(message)) {
                 return false;
             }
         }
