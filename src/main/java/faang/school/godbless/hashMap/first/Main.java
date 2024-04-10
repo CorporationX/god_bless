38b172dd9cee401056371cff815abc219a973b1a
Vadim Merkulov <vm.merkulov@gmail.com>
2024-04-10 19:00:03 +0300

    BJS2-5087 Исправил

--- src/main/java/faang/school/godbless/hashMap/first/Main.java
+++ src/main/java/faang/school/godbless/hashMap/first/Main.java
@@ -1,52 +1,52 @@
 package faang.school.godbless.hashMap.first;
 
-import java.nio.file.attribute.UserPrincipal;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.HashMap;
-import java.util.List;
+import java.util.*;
 
 public class Main {
-    private final static List<Student> students = new ArrayList<>();
+    private static final List<Student> students = new ArrayList<>();
 
     public static void main(String[] args) {
-        add(new Student("Vadim", "IT", 1));
-        add(new Student("Igor", "History", 4));
-        add(new Student("Dmitry", "IT", 1));
-        add(new Student("Elena", "Philological", 2));
-        add(new Student("Sergey", "History", 3));
-        add(new Student("Petr", "IT", 3));
-        add(new Student("Vladimir", "Philological", 4));
-        delete("Elena", "Philological", 2);
-        distributedStudentToString(distributedStudents(students));
+        Main main = new Main();
+        main.addStudent(new Student("Vadim", "IT", 1));
+        main.addStudent(new Student("Igor", "History", 4));
+        main.addStudent(new Student("Dmitry", "IT", 1));
+        main.addStudent(new Student("Elena", "Philological", 2));
+        main.addStudent(new Student("Sergey", "History", 3));
+        main.addStudent(new Student("Petr", "IT", 3));
+        main.addStudent(new Student("Vladimir", "Philological", 4));
+        main.deleteStudent("Elena", "Philological", 2);
+        main.writeGroupedStudents(main.groupStudents(students));
     }
 
-    public static HashMap<String, List<Student>> distributedStudents(List<Student> students) {
+    public String generateKey(Student student) {
+        String key = student.getFaculty() + " " + student.getYear();
+        return key;
+    }
+
+    public HashMap<String, List<Student>> groupStudents(List<Student> students) {
         HashMap<String, List<Student>> groupedStudents = new HashMap<>();
         for (Student student : students) {
-            if (groupedStudents.containsKey(student.getFaculty() + " " + student.getYear())) {
-                groupedStudents.get(student.getFaculty() + " " + student.getYear()).add(student);
+            if (groupedStudents.containsKey(generateKey(student))) {
+                groupedStudents.get(generateKey(student)).add(student);
             } else {
-                groupedStudents.put(student.getFaculty() + " " + student.getYear(), new ArrayList<>(Arrays.asList(student)));
+                groupedStudents.put(generateKey(student), new ArrayList<>(Arrays.asList(student)));
             }
         }
         return groupedStudents;
     }
 
-    public static void distributedStudentToString(HashMap<String, List<Student>> groupedStudent) {
-        for () {
-
+    public void writeGroupedStudents(HashMap<String, List<Student>> groupedStudent) {
+        for (Map.Entry entry : groupedStudent.entrySet()) {
+            System.out.println(entry);
         }
     }
 
-    public static void add(Student student) {
+    public void addStudent(Student student) {
         students.add(student);
     }
 
-    public static void delete(String name, String faculty, int year) {
+    public void deleteStudent(String name, String faculty, int year) {
         Student student = new Student(name, faculty, year);
-        if (students.contains(student)) {
-            students.remove(student);
-        }
+        students.remove(student);
     }
 }
