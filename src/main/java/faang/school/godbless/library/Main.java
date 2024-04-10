8177744d1b76f6b71284c738f063f551be6aa2c1
Th0rn-dev <solnek33@gmail.com>
2024-04-10 20:20:00 +0300

    deleted all coments in Main class

--- src/main/java/faang/school/godbless/library/Main.java
+++ src/main/java/faang/school/godbless/library/Main.java
@@ -14,18 +14,18 @@ public class Main {
         libraryOfWesteross.put(new Book("Title 1", "Author 1", Year.of(1601)), "Stage 1");
         libraryOfWesteross.put(new Book("Title 2", "Author 1", Year.of(1745)), "Stage 1");
         libraryOfWesteross.put(new Book("Title 3", "Author 2", Year.of(1678)), "Stage 2");
-        // Добавим книгу
+
         Book book = new Book("Title 4", "Author 2", Year.of(1700));
         addBook(libraryOfWesteross, book, "Stage 3");
         showAllBooks(libraryOfWesteross);
-        //Удаляем книги с разными параметрами
-        deleteBook(libraryOfWesteross, book.getTitle(), book.getAuthor(), book.getYear());
+
+        deleteBook(libraryOfWesteross, book);
         deleteByTitle(libraryOfWesteross, "Title 3");
         deleteByTitle(libraryOfWesteross, "Author 1");
         deleteByYear(libraryOfWesteross, Year.of(1745));
-        //Покажем, что осталось в библиотеке
+
         showAllBooks(libraryOfWesteross);
-        // Поищем книгу в остатках
+
         Book book_1 = new Book("Title 5", "Author 3", Year.of(1900));
         addBook(libraryOfWesteross, book_1, "Stage 4");
         System.out.println(findBook(libraryOfWesteross, book_1));
@@ -41,30 +41,27 @@ public class Main {
     }
 
     public static Boolean deleteBook(
-            Map<Book, String> library,
-            String title,
-            String author,
-            Year year) {
-        String returnedValue = library.remove(new Book(title, author, year));
+            Map<Book, String> library, Book book) {
+        String returnedValue = library.remove(book);
         if (!Objects.isNull(returnedValue)) {
-            System.out.println("Book with title=" + title + ", author=" + author +
-                    ", year=" + year + " deleted from library");
+            System.out.println(
+                    "Book with title=" + book.getTitle() +
+                    ", author=" + book.getAuthor() +
+                    ", year=" + book.getYear() + " deleted from library"
+            );
             return true;
         }
-        System.out.println("It is not possible to delete a book with the title=" + title +
-                ", author=" + author +
-                ", year=" + year + "since it is not in the library!");
+        System.out.println(
+                "It is not possible to delete a book with the title=" + book.getTitle() +
+                ", author=" + book.getAuthor() +
+                ", year=" + book.getYear() + "since it is not in the library!");
         return false;
     }
 
-    // Удаляем все книги одинакового названия, которые могут иметь разных авторов
-    // и год издания
     public static Boolean deleteByTitle(Map<Book, String> library, String title) {
         int deleted = 0;
-        // Перепишем на итератор
         Iterator<Map.Entry<Book, String>> booksIterator = library.entrySet().iterator();
         while (booksIterator.hasNext()) {
-            // Удаляем без ConcurrentModificationException
             Map.Entry<Book, String> record = booksIterator.next();
             if (record.getKey().getTitle().equals(title)) {
                 booksIterator.remove();
@@ -80,11 +77,8 @@ public class Main {
         return false;
     }
 
-    // Удаляем все книги одного автора, которые могут иметь разные названия
-    // и год издания
     public static boolean deleteByAuthor(Map<Book, String> library, String author) {
         int deleted = 0;
-        // Удаляем без ConcurrentModificationException
         Iterator<Map.Entry<Book, String>> booksIterator = library.entrySet().iterator();
         while (booksIterator.hasNext()) {
             Map.Entry<Book, String> record = booksIterator.next();
@@ -102,11 +96,8 @@ public class Main {
         return false;
     }
 
-    // Удаляем все книги одного года выпуска, которые могут иметь разные названия
-    // и разных авторов
     public static Boolean deleteByYear(Map<Book, String> library, Year year) {
         int deleted = 0;
-        // Удаляем без ConcurrentModificationException
         Iterator<Map.Entry<Book, String>> booksIterator = library.entrySet().iterator();
         while (booksIterator.hasNext()) {
             Map.Entry<Book, String> record = booksIterator.next();
@@ -142,4 +133,3 @@ public class Main {
         }
     }
 }
-
