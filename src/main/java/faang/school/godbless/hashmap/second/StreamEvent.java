f85c39a280ddb730d0cb1576d85aac64b9235312
Vadim Merkulov <vm.merkulov@gmail.com>
2024-04-10 19:59:32 +0300

    BJS2-5110

--- src/main/java/faang/school/godbless/hashmap/second/StreamEvent.java
+++ src/main/java/faang/school/godbless/hashmap/second/StreamEvent.java
@@ -2,9 +2,11 @@ package faang.school.godbless.hashmap.second;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @AllArgsConstructor
+@NoArgsConstructor
 public class StreamEvent {
     private int id;
     private String eventType;
