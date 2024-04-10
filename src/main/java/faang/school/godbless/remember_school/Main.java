eb3468e0bc3237896f89243fd916e3e655ba616f
DimaMinin <91825998+DimaMinin@users.noreply.github.com>
2024-04-10 17:32:59 +0300

    moved validation to a separate method, add tests

--- src/main/java/faang/school/godbless/remember_school/Main.java
+++ src/main/java/faang/school/godbless/remember_school/Main.java
@@ -1,28 +1,13 @@
 package faang.school.godbless.remember_school;
 
-import lombok.extern.slf4j.Slf4j;
-
 import java.util.function.Function;
 
-@Slf4j
 public class Main {
 
-    public static void main(String[] args) {
-
-        try {
-            System.out.println(triangleArea(1, 2, 3));
-        } catch (RuntimeException e) {
-            log.error(e.getMessage());
-        }
-
-        System.out.println(triangleArea(3, 4, 5));
-    }
+    public static void main(String[] args) {}
 
     public static Double triangleArea(double a, double b, double c) throws IllegalArgumentException {
-
-        if (a >= b + c || b >= a + c || c >= a + b) {
-            throw new IllegalArgumentException("Each side must be greater than the sum of the other sides");
-        }
+        validateTriangle(a, b, c);
 
         Function<Double, Function<Double, Double>> sum = (x) -> y -> x + y;
         Function<Double, Function<Double, Double>> mul = (x) -> y -> x * y;
@@ -30,12 +15,27 @@ public class Main {
         Function<Double, Function<Double, Double>> div = (x) -> y -> x / y;
         Function<Double, Double> sqrt = Math::sqrt;
 
-        Double p = div.apply(sum.apply(sum.apply(a).apply(b)).apply(c)).apply(2.0);
+        Double p = div.apply(
+                sum.apply(
+                        sum.apply(a).apply(b)
+                ).apply(c)
+        ).apply(2.0);
+
         Double pSubtractA = sub.apply(p).apply(a);
         Double pSubtractB = sub.apply(p).apply(b);
         Double pSubtractC = sub.apply(p).apply(c);
-        Double s = sqrt.apply(mul.apply(mul.apply(mul.apply(p).apply(pSubtractA)).apply(pSubtractB)).apply(pSubtractC));
+        return sqrt.apply(
+                mul.apply(
+                        mul.apply(
+                                mul.apply(p).apply(pSubtractA)
+                        ).apply(pSubtractB)
+                ).apply(pSubtractC)
+        );
+    }
 
-        return s;
+    private static void validateTriangle(double a, double b, double c) {
+        if (a >= b + c || b >= a + c || c >= a + b) {
+            throw new IllegalArgumentException("Each side must be greater than the sum of the other sides");
+        }
     }
 }
