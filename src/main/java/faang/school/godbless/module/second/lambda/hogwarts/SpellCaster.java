5dae07bdaf7818603a7e6ef063b497642290e442
Rick <RickHammerson@ya.ru>
2024-04-12 00:06:30 +0300

    Решение задачи BJS2-5290 Хогвартс, Хогвартс, наш любимый Хогвартс

---
+++ src/main/java/faang/school/godbless/module/second/lambda/hogwarts/SpellCaster.java
@@ -0,0 +1,7 @@
+package faang.school.godbless.module.second.lambda.hogwarts;
+
+public class SpellCaster {
+    public void cast(String spellName, SpellAction spellAction) {
+        spellAction.castSpell(spellName);
+    }
+}
