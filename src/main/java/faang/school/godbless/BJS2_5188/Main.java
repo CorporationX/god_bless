f4711ac805d5d96a888a0c2dc5573731243f3063
AlexHor <apsekenesss@gmail.com>
2024-04-11 23:47:09 +0300

    Исправление ошибок

--- src/main/java/faang/school/godbless/BJS2_5188/Main.java
+++ src/main/java/faang/school/godbless/BJS2_5188/Main.java
@@ -2,10 +2,11 @@ package faang.school.godbless.BJS2_5188;
 import lombok.SneakyThrows;
 
 public class Main {
+
     @SneakyThrows
     public static void main(String[] args){
         try {
-            User user = new User("Alex", 19, "Google", "London");
+            new User("Alex", 19, "Google", "London");
             System.out.println("All fields are correct");
 
         } catch (IllegalAccessException e) {
