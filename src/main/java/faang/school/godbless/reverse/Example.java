15c8a832f2768b1637f16ddbc95234a7269e8b2e
ethiqque <danielpyld.work@gmail.com>
2024-04-11 14:08:31 +0300

    BJS2-4114 Развернитесь

--- src/main/java/faang/school/godbless/reverse/Example.java
+++ src/main/java/faang/school/godbless/reverse/Example.java
@@ -1,10 +1,10 @@
 package faang.school.godbless.reverse;
 
 public class Example {
-    public static void reverse(Integer[] nums) {
+    public static void reverse(int[] nums) {
         int length = nums.length;
         for (int i = 0; i < length / 2; i++) {
-            Integer temp = nums[i];
+            int temp = nums[i];
             nums[i] = nums[length - i - 1];
             nums[length - i - 1] = temp;
         }
