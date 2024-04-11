b0bcc686c6e989b81fe4dea6f210b3a1c850423f
Demchenkov7 <andreydemchenkov@yandex.ru>
2024-04-11 13:22:27 +0300

    second

--- src/main/java/faang/school/godbless/greatexception/Main.java
+++ src/main/java/faang/school/godbless/greatexception/Main.java
@@ -1,11 +1,17 @@
 package faang.school.godbless.greatexception;
 
+import java.util.function.Consumer;
+import java.util.function.Function;
+
 public class Main {
     public static void main(String[] args) {
-
+        int param = 1;
+        Consumer<RemoteService> consumer = remoteService -> remoteService.call(param);
     }
 
-    public void withErrorHandling() {
-
+    public void withErrorHandling(Consumer consumer, Function function) {
+//        try {
+//            consumer.accept();
+//        }
     }
 }
