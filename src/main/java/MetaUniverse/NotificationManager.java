4dc0463acc37d59f35b50f1c02ea7c5cec9ac1ba
romantarelicev <romuhut@mail.ru>
2024-04-11 18:02:55 +0300

    Исправил замечания - BJS2-5245, отправляю PR

--- src/main/java/MetaUniverse/NotificationManager.java
+++ src/main/java/MetaUniverse/NotificationManager.java
@@ -9,14 +9,16 @@ public class NotificationManager {
     public static final Map<String, Consumer<Notification>> NOTIFICATIONS = new HashMap<>();
 
     public void registerHandler(String type, Consumer<Notification> handler) {
-        if (type != null && handler != null) {
-            NOTIFICATIONS.put(type, handler);
-        } else {
+        if (type == null && handler == null) {
             throw new IllegalArgumentException("Type or handler is null.");
         }
+        NOTIFICATIONS.put(type, handler);
     }
 
     public void sendNotification(Notification notification) {
+        if (notification.getType() == null || notification.getType().isEmpty()) {
+            throw new IllegalArgumentException("Notification cannot be empty or null.");
+        }
         NOTIFICATIONS.get(notification.getType()).accept(notification);
     }
 }
