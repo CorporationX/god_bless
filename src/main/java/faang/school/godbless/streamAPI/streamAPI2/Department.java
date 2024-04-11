ba9ac75a9f2e535d80098df199eec9e238019662
nadir <den4ik.strelok@gmail.com>
2024-04-11 23:29:54 +0300

    BJS2-5227 (Потренируем StreamAPI 2)

---
+++ src/main/java/faang/school/godbless/streamAPI/streamAPI2/Department.java
@@ -0,0 +1,11 @@
+package faang.school.godbless.streamAPI.streamAPI2;
+
+public enum Department {
+    BACKEND,
+    FRONTEND,
+    QA,
+    ANALYTICS,
+    SECURITY,
+    MANAGEMENT,
+    HR
+}
---
+++ src/main/java/faang/school/godbless/streamAPI/streamAPI2/Employee.java
@@ -0,0 +1,3 @@
+package faang.school.godbless.streamAPI.streamAPI2;
+
+public record Employee (String name, int salary, Department department){}
