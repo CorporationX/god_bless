ad8ba858f5dea19556dc1adaee3e376e7624f8b9
KuvarzinAlex <fargus.wow@mail.ru>
2024-04-11 20:53:45 +0500

    BJS2-5143 meta_universe complete(solution 3)

--- src/main/java/faang/school/godbless/meta_universe/NotificationManager.java
+++ src/main/java/faang/school/godbless/meta_universe/NotificationManager.java
@@ -8,15 +8,15 @@ public class NotificationManager {
     private final Map<String, Consumer<Notification>> notifications = new HashMap<>();
 
     public void registerHandler(String notificationId, Consumer<Notification> handler) {
-        if (notificationId.isEmpty()) {
-            throw new IllegalArgumentException("The notification ID field is empty");
+        if (notificationId == null || notificationId.isEmpty()) {
+            throw new IllegalArgumentException("Параметр " + '\"' + "notificationId" + '\"' + " не может быть null или пустым");
         }
         notifications.put(notificationId, handler);
     }
 
     public void sendNotification(Notification notification) {
         if (!notifications.containsKey(notification.getType())) {
-            throw new IllegalArgumentException("There is no such handler key");
+            throw new IllegalArgumentException("Ключа " + '\"' + notification.getType() + '\"' + " не существует.");
         }
         notifications.get(notification.getType()).accept(notification);
     }
