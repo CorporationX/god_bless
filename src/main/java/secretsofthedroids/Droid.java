4700309e1da9d52489c5bd56c17ef186e4512fb4
Maxim Suhanov <maksimka_13.05.2004@mail.ru>
2024-04-11 23:36:15 +0400

    BJS2-5178-_makakcuk_

--- src/main/java/secretsofthedroids/Droid.java
+++ src/main/java/secretsofthedroids/Droid.java
@@ -3,24 +3,22 @@ package secretsofthedroids;
 public class Droid {
 
     public String sendEncryptedMessage(String message, int encryptionKey) {
-        DroidMessageEncryptor encryptor = (msg, key) -> {
-            StringBuilder builder = new StringBuilder();
-            for (char chr : msg.toCharArray()) {
-                builder.append((char) (chr + key));
-            }
-            return builder.toString();
-        };
+        DroidMessageEncryptor encryptor = getEncryptorOrDecryptor(true);
         return encryptor.encrypt(message, encryptionKey);
     }
 
     public String receiveEncryptedMessage(String message, int decryptionKey) {
-        DroidMessageEncryptor decryptor = (msg, key) -> {
+        DroidMessageEncryptor decryptor = getEncryptorOrDecryptor(false);
+        return decryptor.encrypt(message, decryptionKey);
+    }
+
+    private static DroidMessageEncryptor getEncryptorOrDecryptor(boolean cryptor) {
+        return (msg, key) -> {
             StringBuilder builder = new StringBuilder();
             for (char chr : msg.toCharArray()) {
-                builder.append((char) (chr - key));
+                builder.append((char) (cryptor ? (chr + key) : (chr - key)));
             }
             return builder.toString();
         };
-        return decryptor.encrypt(message, decryptionKey);
     }
 }
