27d7a17b7836043c3330f92c3bc6d6b464b05f13
romantarelicev <romuhut@mail.ru>
2024-04-11 19:28:01 +0300

    Калькулятор - BJS2-5255, выполнил задание отправляю PR

---
+++ src/main/java/Calculator/Main.java
@@ -0,0 +1,32 @@
+package Calculator;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) {
+        List<Integer> nums = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
+
+        System.out.println(product(nums));
+        System.out.println(sum(nums));
+    }
+
+    private static int calculate(List<Integer> nums, Calculator calculator) throws IllegalArgumentException {
+        if (nums.isEmpty()) {
+            throw new IllegalArgumentException("empty list");
+        }
+        int result = nums.get(0);
+        for (int i = 0; i < nums.size(); i++) {
+            result = calculator.calculate(result, nums.get(i));
+        }
+        return result;
+    }
+
+    private static int product(List<Integer> nums) {
+        return calculate(nums, (a, b) -> a * b);
+    }
+
+    private static int sum(List<Integer> nums) {
+        return calculate(nums, (a, b) -> a + b);
+    }
+}
