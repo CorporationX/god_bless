c3c5b803e8968e9e4faf60ce28a360d09c4106de
Maxim Suhanov <maksimka_13.05.2004@mail.ru>
2024-04-11 23:18:55 +0400

    BJS2-5160-_makakcuk_

--- src/main/java/HandleErrorsBeautifully/Main.java
+++ src/main/java/HandleErrorsBeautifully/Main.java
@@ -4,6 +4,7 @@ import java.util.function.Supplier;
 import lombok.extern.slf4j.Slf4j;
 @Slf4j
 public class Main {
+
     public static void main(String[] args) {
 
         Supplier<String> taskWithException = () -> {
