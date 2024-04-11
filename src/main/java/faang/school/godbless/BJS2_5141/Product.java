bf8d99269b438ebef41f3b54eba4edf36925a8d2
Alex Vishnevskiy <vishgioall@gmail.com>
2024-04-11 22:17:02 +0300

    убрал аннотацию ToString

--- src/main/java/faang/school/godbless/BJS2_5141/Product.java
+++ src/main/java/faang/school/godbless/BJS2_5141/Product.java
@@ -2,11 +2,9 @@ package faang.school.godbless.BJS2_5141;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
-import lombok.ToString;
 
 @Data
 @AllArgsConstructor
-@ToString
 public class Product {
     private int id;
     private String name;
