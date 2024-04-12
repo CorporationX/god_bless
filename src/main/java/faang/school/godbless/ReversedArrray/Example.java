8c92d1753e71972f18a742fb80a5f929f91d0593
ethiqque <danielpyld.work@gmail.com>
2024-04-12 11:58:24 +0300

    BJS2-5299 Развернитесь!

---
+++ src/main/java/faang/school/godbless/ReversedArrray/Example.java
@@ -0,0 +1,12 @@
+package faang.school.godbless.ReversedArrray;
+
+public class Example {
+    public static void reverse(int[] nums) {
+        int length = nums.length;
+        for (int i = 0; i < length / 2; i++) {
+            int temp = nums[i];
+            nums[i] = nums[length - i - 1];
+            nums[length - i - 1] = temp;
+        }
+    }
+}
---
+++ src/main/java/faang/school/godbless/ReversedArrray/Main.java
@@ -0,0 +1,13 @@
+package faang.school.godbless.ReversedArrray;
+
+import java.util.Arrays;
+
+import static faang.school.godbless.ReversedArrray.Example.reverse;
+
+public class Main {
+    public static void main(String[] args) {
+        int[] nums = new int[]{1, 2, 3, 4, 5};
+        reverse(nums);
+        System.out.println("Reversed list: " + Arrays.toString(nums));
+    }
+}
