cb12dcd468dcbb4134cc4d6d145a875c2d020cbb
Muhammad <dzhabrailov3.muhammad@yandex.ru>
2024-04-12 16:39:29 +0300

    Tested two classes seems like work just well

--- src/main/java/faang/school/godbless/Application.java
+++ src/main/java/faang/school/godbless/Application.java
@@ -1,7 +1,23 @@
 package faang.school.godbless;
 
+import faang.school.godbless.abstractionabstraction.Archer;
+import faang.school.godbless.abstractionabstraction.Warrior;
+
 public class Application {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Warrior myWarrior = new Warrior("Konan");
+        Archer myArcher = new Archer("Legalas");
+        System.out.println(myWarrior);
+        System.out.println(myArcher);
+        System.out.println("-----------------------------------------------------------------------------------------");
+        System.out.println("-----------------------------------------------------------------------------------------");
+        System.out.println("-----------------------------------------------------------------------------------------");
+        myWarrior.attack(myArcher);
+        System.out.println(myArcher);
+        System.out.println("-----------------------------------------------------------------------------------------");
+        System.out.println("-----------------------------------------------------------------------------------------");
+        System.out.println("-----------------------------------------------------------------------------------------");
+        myArcher.attack(myWarrior);
+        System.out.println(myWarrior);
     }
 }
