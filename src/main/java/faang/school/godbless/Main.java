091fe2f6d170aefbd3ddd4dd9e37713a4f486a48
Demchenkov7 <andreydemchenkov@yandex.ru>
2024-04-12 20:43:41 +0300

    add validation in triangleArea method

--- src/main/java/faang/school/godbless/Main.java
+++ src/main/java/faang/school/godbless/Main.java
@@ -5,9 +5,22 @@ import java.util.function.Function;
 public class Main {
     public static void main(String[] args) {
         System.out.println(triangleArea(3, 5, 7));
+        try {
+            triangleArea(1, 2, 15);
+        } catch (IllegalArgumentException e) {
+            System.out.println("неправельный треугольник");
+        }
     }
 
     public static Double triangleArea(double a, double b, double c) {
+        if (a <= 0 || b <= 0 || c <= 0) {
+            throw new IllegalArgumentException("All lengths of the sides of a triangle must be greater than zero!");
+        }
+
+        if (a + b < c || a + c < b || c + b < a) {
+            throw new IllegalArgumentException("Two lengths of the sides of a triangle cannot be greater than third!");
+        }
+
         Function<Double, Function<Double, Double>> sum = (x) -> y -> x + y;
         Function<Double, Function<Double, Double>> mul = (x) -> y -> x * y;
         Function<Double, Function<Double, Double>> sub = (x) -> y -> x - y;
