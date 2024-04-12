32203a99b6424940eccc1d0b2e5093f2c4c98886
Vadim Shibanov <vladimirowitch.vadim@gmail.com>
2024-04-12 23:03:57 +0300

    BJS2-5321

--- src/main/java/faang/school/godbless/Task_9/Main.java
+++ src/main/java/faang/school/godbless/Task_9/Main.java
@@ -1,6 +1,7 @@
 package faang.school.godbless.Task_9;
 
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 public class Main {
@@ -12,6 +13,10 @@ public class Main {
         map.put("New-York", new WeatherData("New-York", 10, 60));
 
         weatherInfo(map, "Paris");
+
+        updateWeather(map, "Moscow", 15, 30);
+
+
 
 
     }
@@ -27,5 +32,23 @@ public class Main {
                 System.out.println(map);
             }
         }
+    }
+
+    static void updateWeather(Map<String, WeatherData> map, String cityName, Integer temperature, Integer humidity) {
+        if (map.containsKey(cityName)) {
+            WeatherData weatherData = new WeatherData(cityName, temperature, humidity);
+            map.put(cityName, weatherData);
+        }
+        System.out.println(map);
+    }
+
+    static void removeInfo(Map<String, WeatherData> map, String cityName) {
+        if (cityName!=null) {
+            map.remove(cityName);
+        }
+        System.out.println(map);
+    }
+    static void allCities(Map<String, WeatherData> map) {
+        List<WeatherData> cities = map.
     }
 }
