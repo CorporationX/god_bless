b8f677a46407b149b9c0002b3e482761af656e0a
Roman Antonov <romzesvxo@mail.ru>
2024-04-12 18:46:57 +0300

    BJS2-5311 Исправил методы multiplication и sum для работы с большими числами

--- src/main/java/faang/school/godbless/calculator/Main.java
+++ src/main/java/faang/school/godbless/calculator/Main.java
@@ -1,15 +1,20 @@
 package faang.school.godbless.calculator;
 
+import java.math.BigInteger;
 import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class Main {
     public static void main(String[] args) {
         List<Integer> numbers = Arrays.asList(1, 2, 3, 4, 5);
+        List<Integer> bigNumbers = Arrays.asList(100000, 100000);
         List<Integer> numbers1 = List.of();
         System.out.println("Умножение: " + multiplication(numbers));
         System.out.println("Сумма: " + sum(numbers));
+        System.out.println("Умножение больших чисел: " + multiplication(bigNumbers));
         System.out.println("Умножение: " + multiplication(numbers1));
+
     }
     public static <T> T calculate(List<T> nums, Calculator<T> calculator) throws IllegalArgumentException {
         if (nums.isEmpty()) {
@@ -24,11 +29,15 @@ public class Main {
         return result;
     }
 
-    public static int multiplication(List<Integer> nums) {
-        return calculate(nums, (a, b) -> a * b);
+    public static BigInteger multiplication(List<Integer> nums) {
+        List<BigInteger> bigIntegers = nums.stream()
+                .map(BigInteger::valueOf)
+                .collect(Collectors.toList());
+        return calculate(bigIntegers, (a, b) -> a.multiply(b));
     }
 
-    public static int sum(List<Integer> nums) {
-        return calculate(nums, Integer::sum);
+    public static BigInteger sum(List<Integer> nums) {
+        List<BigInteger> bigIntegers = nums.stream().map(BigInteger::valueOf).collect(Collectors.toList());
+        return calculate(bigIntegers, (a, b) -> a.add(b));
     }
 }
