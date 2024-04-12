a6e8016dbb496c5529ba121295436253db372be7
Kayfral <79108276+Kayfral@users.noreply.github.com>
2024-04-12 19:13:03 +0300

    Задание: Потренируем StreamAPI 2 BJS2-5193
    Внесены изменения

--- src/main/java/faang/school/godbless/BJS2_5139/Main.java
+++ src/main/java/faang/school/godbless/BJS2_5139/Main.java
@@ -1,16 +1,22 @@
 package faang.school.godbless.BJS2_5139;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Сумма чётных чисел: " + StreamAPIService.findSumOfEvenNumbers());
-        System.out.println("Максимальное число: " + StreamAPIService.findMaxNumber());
-        System.out.println("Среднее значение: " + StreamAPIService.findAverageValueOfNumbers());
+        List<Integer> numbers = new ArrayList<>(List.of(1, 2, 3, 4, 5, 6));
+        List<String> strings = new ArrayList<>(List.of("site", "stop", "win", "away", "sit"));
+
+        System.out.println("Сумма чётных чисел: " + StreamAPIService.findSumOfEvenNumbers(numbers));
+        System.out.println("Максимальное число: " + StreamAPIService.findMaxNumber(numbers));
+        System.out.println("Среднее значение: " + StreamAPIService.findAverageValueOfNumbers(numbers));
         System.out.println("Количество строк, которые начинаются с 's' : " + StreamAPIService
-                .countStringStartingWithChar('s'));
-        System.out.println("Слова содержащие подстроку: " + StreamAPIService.filterStringThatContainSubstring("si"));
-        System.out.println("Отсортированный список: " + StreamAPIService.sortStringByLength());
-        System.out.println("Элементы удовлетворяющие условию:" + StreamAPIService.checkStringForMaxLength(3));
-        System.out.println("Минимальный элемент который больше 2: " + StreamAPIService.findMinElementThatMoreNumber(2));
-        System.out.println("Длина строк:" + StreamAPIService.convertStringToStringLength());
+                .countStringStartingWithChar('s', strings));
+        System.out.println("Слова содержащие подстроку: " + StreamAPIService.filterStringThatContainSubstring("si", strings));
+        System.out.println("Отсортированный список: " + StreamAPIService.sortStringByLength(strings));
+        System.out.println("Элементы удовлетворяющие условию:" + StreamAPIService.checkStringForMaxLength(3, strings));
+        System.out.println("Минимальный элемент который больше 2: " + StreamAPIService.findMinElementThatMoreNumber(2, numbers));
+        System.out.println("Длина строк:" + StreamAPIService.convertStringToStringLength(strings));
     }
 }
--- src/main/java/faang/school/godbless/BJS2_5139/StreamAPIService.java
+++ src/main/java/faang/school/godbless/BJS2_5139/StreamAPIService.java
@@ -1,46 +1,42 @@
 package faang.school.godbless.BJS2_5139;
 
-import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.List;
 
 public class StreamAPIService {
-    private static final List<Integer> numbers = new ArrayList<>(List.of(1, 2, 3, 4, 5, 6));
-    private static final List<String> strings = new ArrayList<>(List.of("site", "stop", "win", "away", "sit"));
-
-    public static int findSumOfEvenNumbers(){
+    public static int findSumOfEvenNumbers(List<Integer> numbers){
         return numbers.stream().filter(number -> number % 2 == 0).mapToInt(Integer::intValue).sum();
     }
 
-    public static int findMaxNumber(){
+    public static int findMaxNumber(List<Integer> numbers){
         return numbers.stream().mapToInt(Integer::intValue).max().orElse(0);
     }
 
-    public static int findAverageValueOfNumbers(){
+    public static int findAverageValueOfNumbers(List<Integer> numbers){
         return (int) numbers.stream().mapToInt(Integer::intValue).average().orElse(0);
     }
 
-    public static int countStringStartingWithChar(char element){
+    public static int countStringStartingWithChar(char element, List<String> strings){
         return (int) strings.stream().filter(str -> str.charAt(0) == element).count();
     }
 
-    public static List<String> filterStringThatContainSubstring(String subStr){
+    public static List<String> filterStringThatContainSubstring(String subStr, List<String> strings){
         return strings.stream().filter(str -> str.contains(subStr)).toList();
     }
 
-    public static List<String> sortStringByLength(){
+    public static List<String> sortStringByLength(List<String> strings){
         return strings.stream().sorted(Comparator.comparingInt(String::length)).toList();
     }
 
-    public static List<String> checkStringForMaxLength(int maxLength){
+    public static List<String> checkStringForMaxLength(int maxLength, List<String> strings){
         return strings.stream().filter(str -> str.length() <= maxLength).toList();
     }
 
-    public static int findMinElementThatMoreNumber(int inputNumber){
+    public static int findMinElementThatMoreNumber(int inputNumber, List<Integer> numbers){
         return numbers.stream().filter(number -> number > inputNumber).min(Comparator.naturalOrder()).orElse(0);
     }
 
-    public static List<Integer> convertStringToStringLength(){
+    public static List<Integer> convertStringToStringLength(List<String> strings){
         return strings.stream().map(String::length).toList();
     }
 }
