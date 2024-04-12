5857d71a2848e4873512c74c33ac829428498d6a
Rick <RickHammerson@ya.ru>
2024-04-12 18:14:19 +0300

    Решение задачи BJS2-5313 Фильтры Instagram. Не готово

---
+++ src/main/java/faang/school/godbless/module/second/lambda/instagram/FilterProcessor.java
@@ -0,0 +1,13 @@
+package faang.school.godbless.module.second.lambda.instagram;
+
+import java.util.function.Function;
+
+public class FilterProcessor {
+    public Image applyFilter(Image image, Function<Image, Image> function) {
+        return function.apply(image);
+    }
+    
+    public Function<Image,Image> combineFilters(Function<Image,Image> firstFilter,Function<Image,Image> secondFilter) {
+        return firstFilter.andThen(secondFilter).andThen(firstFilter);
+    }
+}
