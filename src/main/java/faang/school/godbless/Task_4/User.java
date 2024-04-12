b856b3cf07d416aade46feef754ba3e981419e15
Vadim Shibanov <vladimirowitch.vadim@gmail.com>
2024-04-12 13:24:34 +0300

    BJS2-5273

--- src/main/java/faang/school/godbless/Task_4/User.java
+++ src/main/java/faang/school/godbless/Task_4/User.java
@@ -1,12 +1,15 @@
 package faang.school.godbless.Task_4;
 
+import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.experimental.FieldDefaults;
 
 import java.util.*;
 
 @AllArgsConstructor
 @Data
+@FieldDefaults(level = AccessLevel.PRIVATE)
 public class User {
     int id;
     String name;
@@ -17,12 +20,8 @@ public class User {
 
         Map<User, String> result = new HashMap<>();
 
-        for (int i = 0; i < users.size(); i++) {
-            User user = users.get(i);
-
-            Iterator<String> iteratorForActivities = user.getActivities().iterator();
-            while (iteratorForActivities.hasNext()) {
-                String activity = iteratorForActivities.next();
+        for (User user : users) {
+            for (String activity : user.getActivities()) {
                 if (strings.contains(activity)) {
                     result.put(user, activity);
                     break;
