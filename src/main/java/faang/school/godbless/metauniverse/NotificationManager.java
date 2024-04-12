3f0162cf5c850ed1ea8906aeee7096902e52d649
Aleh Biahun <stones2875@gmail.com>
2024-04-12 11:55:42 +0200

    Solved a task from the section: Meta-universe.

---
+++ src/main/java/faang/school/godbless/metauniverse/NotificationManager.java
@@ -0,0 +1,21 @@
+package faang.school.godbless.metauniverse;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.function.Consumer;
+
+public class NotificationManager {
+    private final Map<String, Consumer<Notification>> handlers = new HashMap<>();
+    public void registerHandler(String notificationType, Consumer<Notification> handler) {
+        handlers.put(notificationType, handler);
+    }
+    public void sendNotification(Notification notification) {
+        String type = notification.getType();
+        Consumer<Notification> handler = handlers.get(type);
+        if(handler != null) {
+            handler.accept(notification);
+        } else {
+            System.out.println("No handler registered: " + type);
+        }
+    }
+}
