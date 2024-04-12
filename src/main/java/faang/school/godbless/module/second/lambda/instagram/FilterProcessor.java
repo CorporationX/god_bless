ff078cfe374f61f6d5b2e6bae406bd15636d2ffe
Rick <RickHammerson@ya.ru>
2024-04-12 19:58:16 +0300

    Добавил логику и решил задачу

--- src/main/java/faang/school/godbless/module/second/lambda/instagram/FilterProcessor.java
+++ src/main/java/faang/school/godbless/module/second/lambda/instagram/FilterProcessor.java
@@ -3,11 +3,17 @@ package faang.school.godbless.module.second.lambda.instagram;
 import java.util.function.Function;
 
 public class FilterProcessor {
-    public Image applyFilter(Image image, Function<Image, Image> function) {
-        return function.apply(image);
+    public Image applyFilter(Image image, Function<Image, Image> filter) {
+        if (image == null || filter == null) {
+            return null;
+        }
+        return filter.apply(image);
     }
     
     public Function<Image,Image> combineFilters(Function<Image,Image> firstFilter,Function<Image,Image> secondFilter) {
-        return firstFilter.andThen(secondFilter).andThen(firstFilter);
+        if (firstFilter == null || secondFilter == null) {
+            return null;
+        }
+        return firstFilter.andThen(secondFilter);
     }
 }
--- src/main/java/faang/school/godbless/module/second/lambda/instagram/Main.java
+++ src/main/java/faang/school/godbless/module/second/lambda/instagram/Main.java
@@ -4,14 +4,14 @@ import java.util.function.Function;
 
 public class Main {
     public static void main(String[] args) {
-        Image originalImage = new Image("original.jpg", "Оригинальное изображение");
+        Image originalImage = new Image("original", "Оригинальное изображение.");
         
         FilterProcessor filterProcessor = new FilterProcessor();
         
         // Создание фильтров
-        Function<Image, Image> grayscaleFilter = (image) -> new Image(image.getName() + "_grayscale", "Фильтр: черно-белый");
-        Function<Image, Image> sepiaFilter = (image) -> new Image(image.getName() + "_sepia", "Фильтр: сепия");
-        Function<Image, Image> vignetteFilter = (image) -> new Image(image.getName() + "_vignette", "Фильтр: виньетка");
+        Function<Image, Image> grayscaleFilter = (image) -> new Image(image.getName() + "_grayscale", image.getDescription() + " Применили фильтр: черно-белый.");
+        Function<Image, Image> sepiaFilter = (image) -> new Image(image.getName() + "_sepia", image.getDescription() + "Применили фильтр: сепия.");
+        Function<Image, Image> vignetteFilter = (image) -> new Image(image.getName() + "_vignette", image.getDescription() + "Применили фильтр: виньетка.");
         
         // Применение фильтров
         Image grayscaleImage = filterProcessor.applyFilter(originalImage, grayscaleFilter);
