b4ad41561647f09aa8cacbdf81aa25a3078a91f0
object-petit-c <konishhev96@gmail.com>
2024-04-12 20:14:43 +0300

    Gmail rich filters
    исправил метод обработки почты, теперь он проверяет список на предикат, выводит информацию об обработке каждого письма и применяет фанкшн к каждому письму. теперь метод возвращает список отфильтрованных писем

--- src/main/java/faang/school/godbless/gmail/EmailProcessor.java
+++ src/main/java/faang/school/godbless/gmail/EmailProcessor.java
@@ -1,16 +1,21 @@
 package faang.school.godbless.gmail;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.function.Consumer;
 import java.util.function.Function;
 import java.util.function.Predicate;
 
 public class EmailProcessor {
-    public void processEmails(List<Email> emails, Predicate<Email> predicate, Consumer<Email> consumer, Function<Email, String> function) {
+    public List<Email> processEmails(List<Email> emails, Predicate<Email> predicate, Consumer<Email> consumer, Function<Email, String> function) {
+        List<Email> processedEmail = new ArrayList<>();
         for (Email email : emails) {
-            predicate.test(email);
-            function.apply(email);
+            if (predicate.test(email)) {
+                function.apply(email);
+                processedEmail.add(email);
+            }
             consumer.accept(email);
         }
+        return processedEmail;
     }
 }
