e2823d7de46eb2b2d631ae8099518483a4fe58a5
Daniyar Kabdulin <mrdocula@gmail.com>
2024-04-12 11:37:36 +0300

    Lord of the Rings RPG

--- src/main/java/faang/school/godbless/lord/Character.java
+++ src/main/java/faang/school/godbless/lord/Character.java
@@ -1,12 +1,9 @@
 package faang.school.godbless.lord;
 
 import lombok.Data;
-import lombok.NoArgsConstructor;
-
 import java.util.ArrayList;
 import java.util.List;
 @Data
-@NoArgsConstructor
 public class Character {
     private String name;
     private List<Item> inventories;
--- src/main/java/faang/school/godbless/lord/Item.java
+++ src/main/java/faang/school/godbless/lord/Item.java
@@ -2,11 +2,9 @@ package faang.school.godbless.lord;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
-import lombok.NoArgsConstructor;
 
 @Data
 @AllArgsConstructor
-@NoArgsConstructor
 public class Item {
     private String name;
     private int value;
