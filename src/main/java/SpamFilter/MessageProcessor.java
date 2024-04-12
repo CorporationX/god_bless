1ab714092c4256fb897252c076fa43f7af2b4d65
romantarelicev <romuhut@mail.ru>
2024-04-12 10:53:56 +0300

    Фильтруем спам - BJS2-5267, исправил задание отправляю PR

--- src/main/java/SpamFilter/MessageProcessor.java
+++ src/main/java/SpamFilter/MessageProcessor.java
@@ -6,9 +6,9 @@ public class MessageProcessor {
     public boolean processMessage(String message, List<MessageFilter> filters) {
         for (MessageFilter filter : filters) {
             if (filter.filter(message)) {
-                return true;
+                return false;
             }
         }
-        return false;
+        return true;
     }
 }
