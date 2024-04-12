a1bed75d45c582c3a4006d9bfceb3bae2cf275bb
Muhammad <dzhabrailov3.muhammad@yandex.ru>
2024-04-12 16:51:55 +0300

    Unfortunately looking for matches in User set of hobbies and hobbies which come from argument in "findHobbyLovers" would be force worst case to run through all hobbies of each User At least contain method would not take more than O(1)

---
+++ src/main/java/faang/school/godbless/collectingusers/User.java
@@ -0,0 +1,33 @@
+package faang.school.godbless.collectingusers;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+import java.util.Map;
+import java.util.Set;
+import java.util.List;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+
+@Data
+@AllArgsConstructor
+public class User {
+    private final int id;
+    private final String name;
+    private final Set<String> hobbies;
+    private int age;
+
+    public Map<User, String> findHobbyLovers(List<User> usersList, Set<String> hobbiesSet) {
+        return usersList.stream()
+                .collect(Collectors.toMap(Function.identity(), u -> getMatch(hobbiesSet, u.getHobbies())));
+    }
+
+    private String getMatch(Set<String> hobbiesSetFromRequest, Set<String> hobbiesSetFromUser) {
+        for (String userHobby : hobbiesSetFromRequest) {
+            if (hobbiesSetFromUser.contains(userHobby)) {
+                return userHobby;
+            }
+        }
+        return "";
+    }
+}
