a0d0ceafb66f7b74bb459961f08e9a25837d31eb
AlexHor <apsekenesss@gmail.com>
2024-04-13 18:31:28 +0300

    Добавил name в конструктор, переопределил конструкторы через super

---
+++ src/main/java/faang/school/godbless/Example.java
@@ -0,0 +1,14 @@
+package faang.school.godbless;
+
+public class Example {
+
+    public int[] reverse(int[] nums) {
+        int[] result = new int[nums.length];
+        for (int i = nums.length - 1, j = 0; i >= 0; i--, j++) {
+            result[j] = nums[i];
+        }
+        return result;
+    }
+}
+
+
