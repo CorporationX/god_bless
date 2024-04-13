d80bd1c50a4bceb432f99aa559e4e739ca92296f
Demchenkov7 <andreydemchenkov@yandex.ru>
2024-04-13 18:10:17 +0300

    create private validation method and Unit Tests

--- src/main/java/faang/school/godbless/Main.java
+++ src/main/java/faang/school/godbless/Main.java
@@ -3,23 +3,9 @@ package faang.school.godbless;
 import java.util.function.Function;
 
 public class Main {
-    public static void main(String[] args) {
-        System.out.println(triangleArea(3, 5, 7));
-        try {
-            triangleArea(1, 2, 15);
-        } catch (IllegalArgumentException e) {
-            System.out.println("неправельный треугольник");
-        }
-    }
 
     public static Double triangleArea(double a, double b, double c) {
-        if (a <= 0 || b <= 0 || c <= 0) {
-            throw new IllegalArgumentException("All lengths of the sides of a triangle must be greater than zero!");
-        }
-
-        if (a + b < c || a + c < b || c + b < a) {
-            throw new IllegalArgumentException("Two lengths of the sides of a triangle cannot be greater than third!");
-        }
+        triangleValidation(a, b, c);
 
         Function<Double, Function<Double, Double>> sum = (x) -> y -> x + y;
         Function<Double, Function<Double, Double>> mul = (x) -> y -> x * y;
@@ -38,5 +24,15 @@ public class Main {
                         ).apply(sub.apply(p).apply(c))
                 ).apply(p)
         );
+    }
+
+    private static void triangleValidation(double a, double b, double c) {
+        if (a <= 0 || b <= 0 || c <= 0) {
+            throw new IllegalArgumentException("All lengths of the sides of a triangle must be greater than zero!");
+        }
+
+        if (a + b < c || a + c < b || c + b < a) {
+            throw new IllegalArgumentException("Two lengths of the sides of a triangle cannot be greater than third!");
+        }
     }
 }
