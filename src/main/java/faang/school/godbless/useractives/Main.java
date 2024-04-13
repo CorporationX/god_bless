352719afcfae1c89f00e5ad9ab6a140f439073e7
Demchenkov7 <andreydemchenkov@yandex.ru>
2024-04-13 20:22:02 +0300

    refract getTop3ByComments, getTop5MostPopularThemes, findMostActivitiesUsers. create mapToList methods

--- src/main/java/faang/school/godbless/useractives/Main.java
+++ src/main/java/faang/school/godbless/useractives/Main.java
@@ -13,6 +13,7 @@ public class Main {
                 new UserAction(1, ActionType.LIKE, LocalDateTime.now().minusDays(10)),
                 new UserAction(2, ActionType.COMMENT, LocalDateTime.now().minusDays(5), "#java"),
                 new UserAction(2, ActionType.COMMENT, LocalDateTime.now(), "#IT"),
+                new UserAction(2, ActionType.COMMENT, LocalDateTime.now(), "#IT"),
                 new UserAction(2, ActionType.POST, LocalDateTime.now(), "#IT"),
                 new UserAction(2, ActionType.SHARE, LocalDateTime.now().minusDays(3)),
                 new UserAction(3, ActionType.COMMENT, LocalDateTime.now().minusDays(3), "#IT"),
@@ -44,57 +45,28 @@ public class Main {
     }
 
     public static List<Integer> findMostActivitiesUsers(List<UserAction> userActions, ActionType actionType) {
-        return userActions.stream()
+        Map<Integer, Long> countActivities = userActions.stream()
                 .filter(userAction -> userAction.getActionType().equals(actionType))
-                .collect(Collectors.groupingBy(UserAction::getUserId))
-                .entrySet()
-                .stream()
-                .collect(Collectors.toMap(
-                        Map.Entry::getKey,
-                        entry -> entry.getValue().size()
-                ))
-                .entrySet()
-                .stream()
-                .sorted((o1, o2) -> o2.getValue() - o1.getValue())
-                .map(Map.Entry::getKey)
-                .limit(10)
-                .toList();
+                .collect(Collectors.groupingBy(UserAction::getUserId, Collectors.counting()));
+
+        return mapToList(countActivities, 10);
     }
 
     public static List<String> getTop5MostPopularThemes(List<UserAction> userActions) {
-        return userActions.stream()
+        Map<String, Long> countRefs = userActions.stream()
                 .filter(userAction -> userAction.getContent() != null)
-                .collect(Collectors.groupingBy(UserAction::getContent))
-                .entrySet()
-                .stream()
-                .collect(Collectors.toMap(
-                        Map.Entry::getKey,
-                        entry -> entry.getValue().size()
-                ))
-                .entrySet()
-                .stream()
-                .sorted((o1, o2) -> o2.getValue() - o1.getValue())
-                .map(Map.Entry::getKey)
-                .limit(5)
-                .toList();
+                .collect(Collectors.groupingBy(UserAction::getContent, Collectors.counting()));
+
+        return mapToList(countRefs, 5);
     }
 
     public static List<Integer> getTop3ByComments(List<UserAction> userActions) {
-        return userActions.stream()
+        Map<Integer, Long> countComments = userActions.stream()
                 .filter(userAction -> userAction.getActionType().equals(ActionType.COMMENT))
-                .filter(userAction -> userAction.getActionDate().isAfter(LocalDateTime.now().minusDays(30)))
-                .collect(Collectors.groupingBy(UserAction::getUserId))
-                .entrySet()
-                .stream()
-                .collect(Collectors.toMap(
-                        Map.Entry::getKey,
-                        entry -> entry.getValue().size())
-                ).entrySet()
-                .stream()
-                .sorted((o1, o2) -> o2.getValue() - o1.getValue())
-                .map(Map.Entry::getKey)
-                .limit(3)
-                .toList();
+                .filter(userAction -> userAction.getActionDate().isAfter(LocalDateTime.now().minusMonths(1)))
+                .collect(Collectors.groupingBy(UserAction::getUserId, Collectors.counting()));
+
+        return mapToList(countComments, 3);
     }
 
     public static Map<ActionType, Double> getActionTypePercent(List<UserAction> userActions) {
@@ -108,5 +80,13 @@ public class Main {
                                 entry -> Math.floor((double) entry.getValue().size() / userActions.size() * 10000) / 100
                         )
                 );
+    }
+
+    private static <T> List<T> mapToList(Map<T, Long> map, int limit) {
+        return map.entrySet().stream()
+                .sorted((o1, o2) -> (int) (o2.getValue() - o1.getValue()))
+                .map(Map.Entry::getKey)
+                .limit(limit)
+                .toList();
     }
 }
