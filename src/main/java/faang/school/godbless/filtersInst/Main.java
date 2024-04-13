4701234981c3949d4e3533c2a04dae3c34186f85
Liza Yudelevich <elizaveta.fr@gmail.com>
2024-04-13 11:29:52 +0300

    Add solution for task BJS2-5351. https://faang-school.atlassian.net/browse/BJS2-5351

---
+++ src/main/java/faang/school/godbless/filtersInst/Main.java
@@ -0,0 +1,19 @@
+package faang.school.godbless.filtersInst;
+
+import java.util.function.Function;
+
+public class Main {
+    Image originalImage = new Image("original.jpg", "Оригинальное изображение");
+    FilterProcessor filterProcessor = new FilterProcessor();
+
+    Function<Image, Image> grayscaleFilter = (image) -> new Image(image.getFileName() + "_grayscale", "Фильтр: черно-белый");
+    Function<Image, Image> sepiaFilter = (image) -> new Image(image.getFileName() + "_sepia", "Фильтр: сепия");
+    Function<Image, Image> vignetteFilter = (image) -> new Image(image.getFileName() + "_vignette", "Фильтр: виньетка");
+
+    Image grayscaleImage = filterProcessor.applyFilter(originalImage, grayscaleFilter);
+    Image sepiaImage = filterProcessor.applyFilter(originalImage, sepiaFilter);
+    Image vignetteImage = filterProcessor.applyFilter(originalImage, vignetteFilter);
+
+    Function<Image, Image> combinedFilter = filterProcessor.combineFilters(grayscaleFilter, sepiaFilter);
+    Image combinedImage = filterProcessor.applyFilter(originalImage, combinedFilter);
+}
