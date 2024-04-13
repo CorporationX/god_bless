68f12eaa62fc490ec0e36b9069b8989d222dc087
TOPIKS <esaakyan03@gmail.com>
2024-04-13 23:24:17 +0300

    https://faang-school.com/courses/7ynd2xdg/vxzj9w5d

---
+++ src/main/java/faang/school/godbless/BJS2_4668/House.java
@@ -0,0 +1,11 @@
+package faang.school.godbless.BJS2_4668;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@AllArgsConstructor
+@Getter
+public class House {
+    private String name;
+    private String sigil;
+}
