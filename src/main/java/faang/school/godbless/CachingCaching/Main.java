d7be7861241d0a67fa5583182a68e2a79561d3bd
ethiqque <danielpyld.work@gmail.com>
2024-04-13 07:29:01 +0300

    BJS2-4317 Java HashMap: Кешируем Кешируем

--- src/main/java/faang/school/godbless/CachingCaching/Main.java
+++ src/main/java/faang/school/godbless/CachingCaching/Main.java
@@ -4,39 +4,49 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class Main {
-    private static final Map<String, WeatherData> WHEATHER_DATA = new HashMap<>();
+    private static final Map<String, WeatherData> WHETHER_DATA = new HashMap<>();
+
     public static void main(String[] args) {
+
         Main app = new Main();
+
         app.update("Tallinn", new WeatherData("Tallinn", 20.0, 70.0));
         app.update("San Francisco", new WeatherData("San Francisco", 30.0, 50.0));
         app.update("Paris", new WeatherData("Paris", 18.0, 40.0));
 
-        app.get("Tallinn");
+        WeatherData data = app.get("Tallinn");
+        System.out.println("City: " + data.getCity() +
+                ", Temperature: " + data.getTemperature() +
+                ", Humidity: " + data.getHumidity());
 
         app.delete("Tallinn");
 
-        app.getAll(WHEATHER_DATA);
-    }
+        System.out.println();
 
-    public WeatherData get(String city){
-        WeatherData data = WHEATHER_DATA.computeIfAbsent(city, key -> MockService.getCache());
-        System.out.println("City: " + city + ", Temperature: " + data.getTemperature() + ", Humidity: " + data.getHumidity());
-        return data;
-    }
-    public void update(String city, WeatherData data){
-        WHEATHER_DATA.put(city, data);
-    }
-
-    public void delete (String city){
-        WHEATHER_DATA.remove(city);
-    }
-
-    public void getAll (Map<String, WeatherData> weather){
-        for (Map.Entry<String, WeatherData> entry : weather.entrySet()) {
-        String city = entry.getKey();
-        WeatherData data = entry.getValue();
-        System.out.println( "City: " + city + ", Temperature: " + data.getTemperature() + ", Humidity: " + data.getHumidity());
+        for (Map.Entry<String, WeatherData> entry : app.getAll().entrySet()) {
+            String city = entry.getKey();
+            WeatherData weatherData = entry.getValue();
+            System.out.println("City: " + city +
+                    ", Temperature: " + weatherData.getTemperature() +
+                    ", Humidity: " + weatherData.getHumidity());
         }
+    }
+
+    public WeatherData get(String city) {
+        MockService mockService = new MockService();
+        return WHETHER_DATA.computeIfAbsent(city, key -> mockService.getCache());
+    }
+
+    public void update(String city, WeatherData data) {
+        WHETHER_DATA.put(city, data);
+    }
+
+    public void delete(String city) {
+        WHETHER_DATA.remove(city);
+    }
+
+    public Map<String, WeatherData> getAll() {
+        return new HashMap<>(WHETHER_DATA);
     }
 
 
--- src/main/java/faang/school/godbless/CachingCaching/MockService.java
+++ src/main/java/faang/school/godbless/CachingCaching/MockService.java
@@ -1,7 +1,22 @@
 package faang.school.godbless.CachingCaching;
 
+import java.util.Random;
+
 public class MockService {
-    public static WeatherData getCache(){
-        return new WeatherData("city", 20.0, 30.0);
+    private static final Random RANDOM = new Random();
+
+    public WeatherData getCache() {
+        String city = "City";
+        double temperature = generateRandomTemperature();
+        double humidity = generateRandomHumidity();
+        return new WeatherData(city, temperature, humidity);
+    }
+
+    private double generateRandomTemperature() {
+        return RANDOM.nextDouble() * 40.0;
+    }
+
+    private double generateRandomHumidity() {
+        return RANDOM.nextDouble() * 100.0;
     }
 }
