1d69cdeec7890cd6bd6a51b17ace22bebb6fffda
Andrei Archakov <archandrwork@gmail.com>
2024-04-13 21:33:37 +0300

    BJS2-4130: add double cache task

---
+++ src/main/java/faang/school/godbless/BJS2_4130/Student.java
@@ -0,0 +1,11 @@
+package faang.school.godbless.BJS2_4130;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class Student {
+    private int id;
+    private String name;
+}
