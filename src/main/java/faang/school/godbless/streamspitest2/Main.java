30e89ac6597fe7a992443ba5d9c82252589491b4
Demchenkov7 <andreydemchenkov@yandex.ru>
2024-04-13 19:45:41 +0300

    refract findUniquePairs, sortStringLength, findPairPeopleWithMutualFriends methods

--- src/main/java/faang/school/godbless/streamspitest2/Main.java
+++ src/main/java/faang/school/godbless/streamspitest2/Main.java
@@ -2,10 +2,13 @@ package faang.school.godbless.streamspitest2;
 
 import java.util.Arrays;
 import java.util.Comparator;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
+import java.util.Set;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
+import java.util.stream.Stream;
 
 public class Main {
     public static void main(String[] args) {
@@ -36,8 +39,8 @@ public class Main {
         System.out.println("findPalindrome TEST, exp 101 111 121 131 141");
         System.out.println(findPalindrome(100, 150));
 
-        System.out.println("findUniquePairs TEST, exp 1,5 2,4");
-        findUniquePairs(List.of(1, 2, 3, 4, 5), 6);
+        System.out.println("findUniquePairs TEST, exp [[1, 9], [6, 4]]");
+        System.out.println(findUniquePairs(List.of(1, 9, 3, 6, 4, 5), 10));
 
         System.out.println("findPairPeopleWithMutualFriends TEST");
         Map<String, List<String>> friends = Map.of(
@@ -46,7 +49,7 @@ public class Main {
                 "3", List.of("1"),
                 "4", List.of("2")
         );
-        findPairPeopleWithMutualFriends(friends);
+        System.out.println(findPairPeopleWithMutualFriends(friends));
 
         System.out.println("filterAndSortStrings TEST");
         List<String> strings2 = List.of("apple", "banana", "123", "dog", "cat");
@@ -54,12 +57,18 @@ public class Main {
         filterAndSortStrings(strings2, alphabet).forEach(System.out::println);
     }
 
-    public static void findUniquePairs(List<Integer> nums, int target) {
+    public static Set<List<Integer>> findUniquePairs(List<Integer> nums, int target) {
+        Set<List<Integer>> pairs = new HashSet<>();
         IntStream.range(0, nums.size())
                 .forEach(elem1 -> IntStream.range(0, nums.size())
                         .filter(elem2 -> elem1 != elem2 && nums.get(elem1) + nums.get(elem2) == target)
-                        .forEach(elem2 -> System.out.println(nums.get(elem2) + ", " + nums.get(elem1)))
+                        .forEach(elem2 -> pairs.add(
+                                Stream.of(nums.get(elem1), nums.get(elem2))
+                                        .sorted(Comparator.comparingInt(Integer::intValue))
+                                        .toList()
+                        ))
                 );
+        return pairs;
     }
 
     public static void printCapitals(Map<String, String> countries) {
@@ -75,16 +84,22 @@ public class Main {
                 .toList();
     }
 
-    public static void findPairPeopleWithMutualFriends(Map<String, List<String>> friends) {
+    public static Set<List<String>> findPairPeopleWithMutualFriends(Map<String, List<String>> friends) {
+        Set<List<String>> pairs = new HashSet<>();
         friends.forEach(
                 (key, value) -> friends.entrySet().stream()
-                        .filter(entry -> !value.contains(entry.getKey()) && !key.equals(entry.getKey()))
-                        .forEach(
-                                entry -> entry.getValue().stream()
-                                        .filter(value::contains)
-                                        .forEach(name -> System.out.println(key + " - " + entry.getKey()))
-                        )
+                        .filter(entry -> !friends.get(key).contains(entry.getKey()))
+                        .filter(entry -> !friends.get(entry.getKey()).contains(key))
+                        .filter(entry -> !key.equals(entry.getKey()))
+                        .forEach(entry -> {
+                            if (key.compareTo(entry.getKey()) > 0) {
+                                pairs.add(List.of(key, entry.getKey()));
+                            } else {
+                                pairs.add(List.of(entry.getKey(), key));
+                            }
+                        })
         );
+        return pairs;
     }
 
     public static Map<String, Double> findAverageSalaryOfDepartment(List<Employee> employees) {
