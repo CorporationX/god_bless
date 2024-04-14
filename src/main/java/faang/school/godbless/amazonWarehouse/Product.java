8f8c78af9587e07fd39db79a99575411dd9755ae
Liza Yudelevich <elizaveta.fr@gmail.com>
2024-04-14 16:22:50 +0300

    add solution for task BJS2-4633. https://faang-school.atlassian.net/browse/BJS2-4633

---
+++ src/main/java/faang/school/godbless/amazonWarehouse/Product.java
@@ -0,0 +1,12 @@
+package faang.school.godbless.amazonWarehouse;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class Product {
+    int id;
+    String name;
+    String category;
+}
