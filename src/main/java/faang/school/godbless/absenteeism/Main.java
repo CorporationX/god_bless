2649f5f540ccc40e4026474c515891435393d78f
Aleksey <lekha.bolt2004@gmail.com>
2024-04-14 12:19:07 +0300

    Не очень понял "а если списка студентов не будет, что произойдёт?". Я просто переписал с условием...

--- src/main/java/faang/school/godbless/absenteeism/Main.java
+++ src/main/java/faang/school/godbless/absenteeism/Main.java
@@ -1,9 +1,8 @@
 package faang.school.godbless.absenteeism;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
+import java.util.stream.Collectors;
+
 
 public class Main {
     private static final List<Student> STUDENTS = new ArrayList<>();
@@ -16,18 +15,15 @@ public class Main {
         STUDENTS.add(new Student("Tom", "Physics", 1));
         STUDENTS.add(new Student("Kate", "Physics", 2));
 
-        Map<Course, List<Student>> studentMap = groupStudentsByFacultyAndCourse(STUDENTS);
+        Map<Course, List<Student>> studentMap = groupStudentsByCourse(STUDENTS);
         System.out.println(studentMap);
     }
 
-    public static Map<Course, List<Student>> groupStudentsByFacultyAndCourse(List<Student> students) {
-        Map<Course, List<Student>> groupedStudents = new HashMap<>();
-
-        for (Student student : students) {
-            Course course = new Course(student.getFaculty(), student.getYear());
-            groupedStudents.computeIfAbsent(course, st -> new ArrayList<>()).add(student);
+    public static Map<Course, List<Student>> groupStudentsByCourse(List<Student> students) {
+        if (students == null || students.isEmpty()) {
+            return Collections.emptyMap();
         }
-
-        return groupedStudents;
+        return students.stream().collect(Collectors.groupingBy(
+                student -> new Course(student.getFaculty(), student.getYear())));
     }
 }
