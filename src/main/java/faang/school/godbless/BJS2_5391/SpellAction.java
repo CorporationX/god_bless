6c9429fae9662519fbe71fd9fb4c01f5bc75198d
Andrei Archakov <archandrwork@gmail.com>
2024-04-14 06:07:25 +0300

    BJS2-5391: add spell caster

---
+++ src/main/java/faang/school/godbless/BJS2_5391/SpellAction.java
@@ -0,0 +1,6 @@
+package faang.school.godbless.BJS2_5391;
+
+@FunctionalInterface
+public interface SpellAction {
+    String cast(String spell);
+}
---
+++ src/main/java/faang/school/godbless/BJS2_5391/SpellCaster.java
@@ -0,0 +1,7 @@
+package faang.school.godbless.BJS2_5391;
+
+public class SpellCaster {
+    public void cast(String spell, SpellAction action) {
+        System.out.println(action.cast(spell));
+    }
+}
