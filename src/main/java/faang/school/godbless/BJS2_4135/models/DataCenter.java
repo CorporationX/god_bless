2cde449c8f75cb6d65e6074956350da7ed65240a
Andrei Archakov <archandrwork@gmail.com>
2024-04-14 17:15:57 +0300

    BJS2-4135: fix register for name of variable & add getters from lombok

--- src/main/java/faang/school/godbless/BJS2_4135/models/DataCenter.java
+++ src/main/java/faang/school/godbless/BJS2_4135/models/DataCenter.java
@@ -1,12 +1,11 @@
 package faang.school.godbless.BJS2_4135.models;
 
+import lombok.Getter;
+
 import java.util.ArrayList;
 import java.util.List;
 
+@Getter
 public class DataCenter {
-    private final List<Server> SERVERS = new ArrayList<>();
-
-    public List<Server> getServers() {
-        return SERVERS;
-    }
+    private final List<Server> servers = new ArrayList<>();
 }
