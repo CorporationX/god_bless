882ddc91215ef0fa14d3a8462d2f119600682c84
egorp5119 <egorplo@gmail.com>
2024-04-14 18:52:24 +0300

    BJS2-5421-google for check

--- src/main/java/faang/school/godbless/google/Main.java
+++ src/main/java/faang/school/godbless/google/Main.java
@@ -1,7 +1,6 @@
 package faang.school.godbless.google;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.List;
@@ -9,37 +8,39 @@ import java.util.Map;
 import java.util.Set;
 
 public class Main {
-    private static Map<String, List<WebPage>> webPages = new HashMap<>();
+    private static final Map<String, List<WebPage>> webPages = new HashMap<>();
 
     public static void main(String[] args) {
-        webPages.put("Java",new ArrayList<>());
-        WebPage java = new WebPage("java.com","Java","Get Java for desktop applications");
-        addNewWebPage(java);
+        webPages.put("Java", new ArrayList<>());
+        webPages.put("Google", new ArrayList<>());
+        addNewWebPage(new WebPage("java.com", "Java", "Get Java for desktop applications"));
+        addNewWebPage(new WebPage("google.com", "google", "Google, you can find all search for."));
         System.out.println(findByKeyWord("Java"));
-
+        System.out.println(findByKeyWord("Google"));
+        deleteByUrl("google.com");
+        System.out.println(findByKeyWord("Google"));
     }
 
 
-    public static void addNewWebPage(WebPage webPage){
+    public static void addNewWebPage(WebPage webPage) {
         Set<String> sortedContent = new HashSet<>();
-        for(String word :webPage.getContent().trim().split("\\s+|,\\s*|\\.\\s*")){
+        for (String word : webPage.getContent().trim().split("\\s+|,\\s*|\\.\\s*")) {
             sortedContent.add(word);
         }
-        for(String sortedWord : sortedContent){
-            for (String key : webPages.keySet()){
-                if(key.equals(sortedWord)){
+        for (String sortedWord : sortedContent) {
+            for (String key : webPages.keySet()) {
+                if (key.equals(sortedWord)) {
                     webPages.get(key).add(webPage);
                 }
             }
         }
     }
-    public static List<WebPage> findByKeyWord(String keyWord){
+
+    public static List<WebPage> findByKeyWord(String keyWord) {
         return webPages.get(keyWord);
     }
 
-    public static void deleteByUrl(String url){
-        for(String key : webPages.keySet()){
-            List<WebPage> webs =new ArrayList<>(webPages.get(key));
-        }
+    public static void deleteByUrl(String url) {
+        webPages.forEach((type, webPagesList) -> webPagesList.removeIf(webPage -> webPage.getUrl().equals(url)));
     }
 }
