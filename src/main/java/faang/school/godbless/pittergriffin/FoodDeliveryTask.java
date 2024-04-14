a658be73aee8b54fa4d8b18811eda1a4a987f93c
Demchenkov7 <andreydemchenkov@yandex.ru>
2024-04-14 17:10:41 +0300

    first

---
+++ src/main/java/faang/school/godbless/pittergriffin/FoodDeliveryTask.java
@@ -0,0 +1,22 @@
+package faang.school.godbless.pittergriffin;
+
+import lombok.AllArgsConstructor;
+
+import java.util.Random;
+
+@AllArgsConstructor
+public class FoodDeliveryTask implements Runnable {
+
+    private String character;
+    private int amount;
+
+    @Override
+    public void run() {
+
+    }
+
+    private String getFoodType() {
+        String[] foodTypes = {"pizza", "burger", "hot dog", "chicken wings", "taco"};
+        return foodTypes[new Random().nextInt(foodTypes.length)];
+    }
+}
