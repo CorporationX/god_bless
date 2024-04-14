a88321ed7bc16646e9ef5a25ef04389a40aae37d
Tamzhid Babutsyngueva <btamzhid@gmail.com>
2024-04-14 12:44:48 +0800

    BJS2-5350 - Java HashMap: Герои меча и магии

--- src/main/java/HeroesOfMightAndMagic/Griffin.java
+++ src/main/java/HeroesOfMightAndMagic/Griffin.java
@@ -1,6 +1,6 @@
 package HeroesOfMightAndMagic;
 
-public class Griffin extends Creature{
+public class Griffin extends Creature {
     public Griffin(String name) {
         super(name, 4, 4, 6, 9, 4);
     }
--- src/main/java/HeroesOfMightAndMagic/Pekerman.java
+++ src/main/java/HeroesOfMightAndMagic/Pekerman.java
@@ -1,6 +1,6 @@
 package HeroesOfMightAndMagic;
 
-public class Pekerman extends Creature{
+public class Pekerman extends Creature {
     public Pekerman(String name) {
         super(name, 3, 1, 3, 12, 1);
     }
