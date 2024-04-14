fcd531daa71e24da2bb9b8be569b6f100796046d
Maxim Suhanov <maksimka_13.05.2004@mail.ru>
2024-04-14 13:02:50 +0400

    BJS2-5342-_makakcuk_

---
+++ src/main/java/faang/school/godbless/gmail/EmailProcessor.java
@@ -0,0 +1,13 @@
+package faang.school.godbless.gmail;
+
+import java.util.List;
+import java.util.function.Consumer;
+import java.util.function.Function;
+import java.util.function.Predicate;
+
+public class EmailProcessor {
+
+    public void processEmails(List<Email> emails, Predicate<Email> predicate, Consumer<Email> consumer, Function<Email, String> function) {
+        emails.stream().filter(predicate).peek(email -> email.setBody(function.apply(email))).forEach(consumer);
+    }
+}
