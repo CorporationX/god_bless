fa3b69c1f44d8bd1bd490ad006a8f312f5f26b6d
Andrei Archakov <archandrwork@gmail.com>
2024-04-14 16:56:15 +0300

    BJS2-5400: fix register for name of variable

--- src/main/java/faang/school/godbless/BJS2_5400/NotificationManager.java
+++ src/main/java/faang/school/godbless/BJS2_5400/NotificationManager.java
@@ -5,14 +5,14 @@ import java.util.Map;
 import java.util.function.Consumer;
 
 public class NotificationManager {
-    private final Map<String, Consumer<Notification>> NOTIFICATIONS = new HashMap<>();
+    private final Map<String, Consumer<Notification>> notifications = new HashMap<>();
 
     public void registerHandler(String type, Consumer<Notification> action) {
-        NOTIFICATIONS.put(type, action);
+        notifications.put(type, action);
     }
 
     public void sendNotification(Notification notification) {
-        Consumer<Notification> consumer = NOTIFICATIONS.get(notification.getType());
+        Consumer<Notification> consumer = notifications.get(notification.getType());
 
         if (consumer == null) {
             throw new IllegalArgumentException("Нотификации типа " + notification.getType() +" не зарегистрирована!");
