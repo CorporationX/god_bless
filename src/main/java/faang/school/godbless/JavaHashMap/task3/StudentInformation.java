80ad309fb81407fbde3576ac93967a9f327e7560
Alexey Kazakov <kaa89537113221ka@gmail.com>
2024-04-14 14:56:01 +0300

    Java HashMap: Считаем прогулы

---
+++ src/main/java/faang/school/godbless/JavaHashMap/task3/StudentInformation.java
@@ -0,0 +1,11 @@
+package faang.school.godbless.JavaHashMap.task3;
+
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+
+@AllArgsConstructor
+@EqualsAndHashCode
+public class StudentInformation {
+    private final String faculty;
+    private final int year;
+}
