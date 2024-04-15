6cd9d825bd5f2f4d5dd0d397bc4e0009bf1a121c
ekoregin <evgkoregin@gmail.com>
2024-04-15 16:47:19 +0300

    Task: https://faang-school.atlassian.net/browse/BJS2-5461

---
+++ src/main/java/faang/school/godbless/bjs2_5461/Monster.java
@@ -0,0 +1,15 @@
+package faang.school.godbless.bjs2_5461;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+public class Monster {
+
+    private String name;
+    private String location;
+}
