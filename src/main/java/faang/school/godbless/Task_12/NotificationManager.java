38505def05e06993d4decc31cec5d3cc12c4d82f
Vadim Shibanov <vladimirowitch.vadim@gmail.com>
2024-04-15 19:43:30 +0300

    BJS2-5521

--- src/main/java/faang/school/godbless/Task_12/NotificationManager.java
+++ src/main/java/faang/school/godbless/Task_12/NotificationManager.java
@@ -7,12 +7,13 @@ import java.util.function.Consumer;
 public class NotificationManager {
     private final Map<String, Consumer<Notification>> notificationTypeAndHandlers = new HashMap<>();
 
-    void registerHandler(String type, Consumer<Notification> notification) {
-        notificationTypeAndHandlers.put(type, notification);
+    void registerHandler(String type, Consumer<Notification> handler) {
+        notificationTypeAndHandlers.put(type, handler);
     }
 
     void sendNotification(Notification notification) {
-        Consumer<Notification> consumer = notificationTypeAndHandlers.get(notification.getType());
-        consumer.accept(notification);
+        Consumer<Notification> handler = notificationTypeAndHandlers.get(notification.getType());
+        handler.accept(notification);
     }
 }
+
