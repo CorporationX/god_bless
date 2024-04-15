2abf1dabecc2155aded46fbe03c62abe290f6d61
nadir <den4ik.strelok@gmail.com>
2024-04-16 00:01:42 +0300

    BJS2-5554 (Накормите Питера Гриффина)

---
+++ src/main/java/faang/school/godbless/parallelism/familyGuy/GriffinsFoodDelivery.java
@@ -0,0 +1,38 @@
+package faang.school.godbless.parallelism.familyGuy;
+
+import lombok.extern.slf4j.Slf4j;
+
+import java.util.Random;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
+
+@Slf4j
+public class GriffinsFoodDelivery {
+
+    public static final int N_THREADS = 3;
+
+    public static void main(String[] args) {
+        ExecutorService foodDelivery = Executors.newFixedThreadPool(N_THREADS);
+
+        String[] characterNames = {"Peter", "Lois", "Meg", "Chris", "Stewie"};
+
+        log.info("Food delivery begins executing.");
+
+        for (var character : characterNames) {
+            int foodAmount = new Random().nextInt(0, 50);
+
+            log.info("Submitting food delivery task for" + character);
+
+            foodDelivery.submit(new FoolDeliveryTask(character, foodAmount));
+        }
+
+        try {
+            foodDelivery.awaitTermination(5, TimeUnit.SECONDS);
+        } catch (InterruptedException e) {
+            log.error("Exception was thorn during shutdown executor service.", e);
+        }
+
+        log.info("Food delivery finished.");
+    }
+}
