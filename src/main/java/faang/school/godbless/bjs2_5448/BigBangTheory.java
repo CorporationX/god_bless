0e21799877dc50c5605abbbef229929ad6a9e2f7
ekoregin <evgkoregin@gmail.com>
2024-04-15 12:26:13 +0300

    Task: https://faang-school.atlassian.net/browse/BJS2-5448

---
+++ src/main/java/faang/school/godbless/bjs2_5448/BigBangTheory.java
@@ -0,0 +1,36 @@
+package faang.school.godbless.bjs2_5448;
+
+import lombok.extern.slf4j.Slf4j;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
+
+@Slf4j
+public class BigBangTheory {
+
+    public static void main(String[] args) {
+        ExecutorService executorService = Executors.newFixedThreadPool(4);
+        Task sheldonTask = new Task("Sheldon", "Prepare theory");
+        Task leonardTask = new Task("Leonard", "Model experiment");
+        Task howardTask = new Task("Howard", "Tool development");
+        Task rajeshTask = new Task("Rajesh", "Data analyze");
+
+        executorService.execute(sheldonTask);
+        executorService.execute(leonardTask);
+        executorService.execute(howardTask);
+        executorService.execute(rajeshTask);
+
+        executorService.shutdown();
+
+        try {
+            if (!executorService.awaitTermination(10, TimeUnit.SECONDS)) {
+                log.error("Time out end for this task");
+                return;
+            }
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+        System.out.println("All tasks were finished!");
+    }
+}
