4df7c9c5f55b1fe3d20372ab93e0a72c2ad4a47e
Kayfral <79108276+Kayfral@users.noreply.github.com>
2024-04-15 14:25:37 +0300

    Задание: Вы работаете в Microsoft! BJS2-5479

---
+++ src/main/java/faang/school/godbless/BJS2_5479/MailSender.java
@@ -0,0 +1,25 @@
+package faang.school.godbless.BJS2_5479;
+
+public class MailSender {
+    public static void main(String[] args) {
+        SenderRunnable senderRunnable = new SenderRunnable();
+        Thread[] threads = new Thread[5];
+
+        for (int i = 0; i < threads.length; i++) {
+            int startIndex = i * 200 + 1;
+            int endIndex = (i + 1) * 200;
+            threads[i] = new Thread(() -> senderRunnable.sendMessage(startIndex, endIndex));
+            threads[i].start();
+        }
+
+        for (Thread thread : threads) {
+            try {
+                thread.join();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+
+        System.out.println("\nВсе сообщения отправлены");
+    }
+}
