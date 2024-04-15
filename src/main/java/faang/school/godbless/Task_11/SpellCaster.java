f1a2f79bbc8e00310d48dd746a6e0826ce136561
Vadim Shibanov <vladimirowitch.vadim@gmail.com>
2024-04-15 16:46:18 +0300

    BJS2-5516

---
+++ src/main/java/faang/school/godbless/Task_11/SpellCaster.java
@@ -0,0 +1,7 @@
+package faang.school.godbless.Task_11;
+
+public class SpellCaster {
+    void cast(String spellName, SpellAction spellAction) {
+        System.out.println(spellAction.action(spellName));
+    }
+}
