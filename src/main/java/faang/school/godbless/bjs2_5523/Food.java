77d9dbc6ab210943c4bba2238f3a2ef14665d3ea
ekoregin <evgkoregin@gmail.com>
2024-04-15 19:08:54 +0300

    Task: https://faang-school.atlassian.net/browse/BJS2-5523

---
+++ src/main/java/faang/school/godbless/bjs2_5523/Food.java
@@ -0,0 +1,16 @@
+package faang.school.godbless.bjs2_5523;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@ToString
+@AllArgsConstructor
+public class Food {
+
+    private String name;
+}
