e0bc9762dce4431857c4ea995341012b6dd464cb
DimaMinin <91825998+DimaMinin@users.noreply.github.com>
2024-04-15 19:08:06 +0300

    Task: https://faang-school.atlassian.net/browse/BJS2-5527

---
+++ src/main/java/faang/school/godbless/way_with_finger/Location.java
@@ -0,0 +1,11 @@
+package faang.school.godbless.way_with_finger;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class Location {
+    private int x;
+    private int y;
+}
