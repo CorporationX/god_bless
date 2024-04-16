eb78e7ddb36cbd79f0beb5611ab81bb992ba717b
mihanchik94 <mihanchik94@gmail.com>
2024-04-16 18:51:33 +0300

    Вы работаете в Microsoft!

--- src/main/java/faang/school/godbless/multithreading/task_1/MailSender.java
+++ src/main/java/faang/school/godbless/multithreading/task_1/MailSender.java
@@ -2,28 +2,24 @@ package faang.school.godbless.multithreading.task_1;
 
 import faang.school.godbless.multithreading.task_1.sender.SenderRunnable;
 
-public class MailSender {
-    public static void main(String[] args) {
-        Thread firstThread = new Thread(new SenderRunnable(1, 100));
-        Thread secondThread = new Thread(new SenderRunnable(101, 200));
-        Thread thirdThread = new Thread(new SenderRunnable(201, 300));
-        Thread fourthThread = new Thread(new SenderRunnable(301, 400));
-        Thread fifthThread = new Thread(new SenderRunnable(401, 500));
+import java.util.ArrayList;
+import java.util.List;
 
-        try {
-            firstThread.start();
-            firstThread.join();
-            secondThread.start();
-            secondThread.join();
-            thirdThread.start();
-            thirdThread.join();
-            fourthThread.start();
-            fourthThread.join();
-            fifthThread.start();
-            fifthThread.join();
-            System.out.println(Thread.currentThread().getName() + " сообщает о том, что все письма отправлены");
-        } catch (InterruptedException e) {
-            e.printStackTrace();
+public class MailSender {
+    private static final int NUMBERS_OF_SENDERS = 5;
+
+    public static void main(String[] args) throws InterruptedException {
+        List<Thread> threads = new ArrayList<>();
+        for(int i = 0; i < NUMBERS_OF_SENDERS; i++) {
+            int startIndex = i * 100 + 1;
+            int lastIndex = startIndex + 99;
+            Thread thread = new Thread(new SenderRunnable(startIndex, lastIndex));
+            thread.start();
+            threads.add(thread);
         }
+        for (Thread thread : threads) {
+            thread.join();
+        }
+        System.out.println(Thread.currentThread().getName() + " сообщает о том, что все письма отправлены");
     }
 }
