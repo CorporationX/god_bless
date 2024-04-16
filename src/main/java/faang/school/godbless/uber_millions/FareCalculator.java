bf9bcd58fcc5c6eebd1682c20a9d4097bbe6349d
Roman Antonov <romzesvxo@mail.ru>
2024-04-16 14:08:55 +0300

    BJS2-5460 Изменил название BiFunction

--- src/main/java/faang/school/godbless/uber_millions/FareCalculator.java
+++ src/main/java/faang/school/godbless/uber_millions/FareCalculator.java
@@ -3,7 +3,7 @@ package faang.school.godbless.uber_millions;
 import java.util.function.BiFunction;
 
 public class FareCalculator {
-    public double calculateFare(double distance, double time, BiFunction<Double, Double, Double> biFunction){
-        return biFunction.apply(distance, time);
+    public double calculateFare(double distance, double time, BiFunction<Double, Double, Double> fareCalculationFunction){
+        return fareCalculationFunction.apply(distance, time);
     }
 }
