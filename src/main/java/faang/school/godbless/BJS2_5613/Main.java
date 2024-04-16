9a2fce5204fd1e3a344d5e46842a640a033bbc5b
AlexHor <apsekenesss@gmail.com>
2024-04-16 21:07:37 +0300

    Meta вселенная

---
+++ src/main/java/faang/school/godbless/BJS2_5613/Main.java
@@ -0,0 +1,7 @@
+package faang.school.godbless.BJS2_5613;
+
+public class Main {
+    public static void main(String[] args) {
+        NotificationManager notificationManager = new NotificationManager();
+    }
+}
---
+++ src/main/java/faang/school/godbless/BJS2_5613/Notification.java
@@ -0,0 +1,11 @@
+package faang.school.godbless.BJS2_5613;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@AllArgsConstructor
+@Data
+public class Notification {
+    private String type;
+    private String message;
+}
