d12e8942c34fd40141b09662396a9b429fb40a9d
nuraly <nuralysaparaliev2gmail.com>
2024-04-16 13:46:23 +0600

    BJS2-5469:Хогвартс, Хогвартс, наш любимый Хогвартс

---
+++ src/main/java/faang/school/godbless/BJS2_5469/SpellCaster.java
@@ -0,0 +1,8 @@
+package faang.school.godbless.BJS2_5469;
+
+public class SpellCaster {
+
+    public void cast(String spell, SpellAction action) {
+        System.out.println(action.act(spell));
+    }
+}
