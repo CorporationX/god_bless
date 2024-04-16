4eba76f197ad7103d6e1de063cc30411b357f80f
ekoregin <evgkoregin@gmail.com>
2024-04-16 13:15:09 +0300

    Task: https://faang-school.atlassian.net/browse/BJS2-5562

---
+++ src/main/java/faang/school/godbless/bjs2_5562/Person.java
@@ -0,0 +1,14 @@
+package faang.school.godbless.bjs2_5562;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+public class Person {
+    private String name;
+    private int age;
+    private String workplace;
+}
