8acf4350ba825c5a6ad25e2da324c069d6d52617
Nikolai Gnidenko <gnidenkonn@gmail.com>
2024-04-16 16:41:00 +0300

    fix

--- src/main/java/homeChoresList/Chore.java
+++ src/main/java/homeChoresList/Chore.java
@@ -8,16 +8,17 @@ import java.util.Random;
 public class Chore implements Runnable {
 
     private String name;
+    private static final Random RANDOM = new Random();
 
     @Override
     public void run() {
-        System.out.println(String.format("Thread %s completes the task '%s'", Thread.currentThread().getName(), this.name));
+        System.out.printf("Thread %s start the task '%s'%n", Thread.currentThread().getName(), this.name);
         try {
-            Thread.sleep(new Random().nextInt(1, 10));
+            Thread.sleep(RANDOM.nextInt(1, 10));
         } catch (InterruptedException e) {
             throw new RuntimeException(e);
         }
-        System.out.println(String.format("Thread %s completed the task '%s'", Thread.currentThread().getName(), this.name));
+        System.out.printf("Thread %s completed the task '%s'%n", Thread.currentThread().getName(), this.name);
 
     }
 }
--- src/main/java/homeChoresList/WeasleyFamily.java
+++ src/main/java/homeChoresList/WeasleyFamily.java
@@ -7,16 +7,18 @@ import java.util.concurrent.Executors;
 public class WeasleyFamily {
     public static void main(String[] args) {
         List<String> chores = List.of("Cook food", "Plan and Shop",
-                "Wash things", "Cleaning", "Taking out the trash", "Do minor repairs", "Plan your budget");
+                "Wash things", "Cleaning", "Taking out the trash",
+                "Do minor repairs", "Plan your budget");
 
         ExecutorService executor = Executors.newCachedThreadPool();
-
-        for (int i = 0; i < chores.size(); i++) {
-            int j = i;
-            executor.submit(() -> new Chore(chores.get(j)).run());
-        }
-
+        chores.forEach(chore -> executor.execute(() -> new Chore(chore).run()));
         executor.shutdown();
+        try {
+            Thread.sleep(5000);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+        System.out.println("All chores is completed");
 
     }
 }
