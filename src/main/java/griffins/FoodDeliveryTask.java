9a4de73fe36191ede0408ea61d456b2ddaffdc6b
Nikolai Gnidenko <gnidenkonn@gmail.com>
2024-04-16 13:59:20 +0300

    fix

--- src/main/java/griffins/FoodDeliveryTask.java
+++ src/main/java/griffins/FoodDeliveryTask.java
@@ -1,27 +1,30 @@
 package griffins;
 
 import lombok.AllArgsConstructor;
-import lombok.SneakyThrows;
 
 import java.util.Random;
 
 @AllArgsConstructor
-public class FoodDeliveryTask implements Runnable{
+public class FoodDeliveryTask implements Runnable {
 
     private String character;
     private int foodAmount;
-    @SneakyThrows
+    private static final Random RANDOM = new Random();
+
     @Override
     public void run() {
         String food = getFoodType();
-        System.out.println(String.format("%s получает %d %s", this.character, this.foodAmount, food));
-        Thread.sleep(new Random().nextInt(1, 5));
-        System.out.println(String.format("%s ест %d %s", this.character, this.foodAmount, food));
+        System.out.printf("%s получает %d %s%n", this.character, this.foodAmount, food);
+        try {
+            Thread.sleep(RANDOM.nextInt(1, 5));
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+        System.out.printf("%s ест %d %s%n", this.character, this.foodAmount, food);
     }
 
     private String getFoodType() {
         String[] foodTypes = {"pizza", "burger", "hot dog", "chicken wings", "taco"};
-        return foodTypes[new Random().nextInt(foodTypes.length)];
+        return foodTypes[RANDOM.nextInt(foodTypes.length)];
     }
-
 }
--- src/main/java/griffins/GriffinsFoodDelivery.java
+++ src/main/java/griffins/GriffinsFoodDelivery.java
@@ -1,24 +1,30 @@
 package griffins;
 
+import java.util.List;
 import java.util.Random;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
 public class GriffinsFoodDelivery {
+    private static final Random RANDOM = new Random();
+
     public static void main(String[] args) {
         ExecutorService executor = Executors.newFixedThreadPool(3);
-        String[] characterNames = {"Peter", "Lois", "Meg", "Chris", "Stewie"};
+        List<String> characterNames = List.of("Peter", "Lois", "Meg", "Chris", "Stewie");
 
-        for (int i = 0; i < characterNames.length; i++) {
-            int j = i;
-            executor.submit(() -> {
-               FoodDeliveryTask character = new FoodDeliveryTask(characterNames[j], new Random().nextInt(1,100));
-               character.run();
+        characterNames.forEach((name -> {
+            executor.execute(() -> {
+                FoodDeliveryTask character = new FoodDeliveryTask(name, RANDOM.nextInt(1, 100));
+                character.run();
             });
+        }));
+
+        try {
+            Thread.sleep(5000);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
         }
-
-        executor.close();
-
+        executor.shutdown();
+        System.out.println("Всех накормили!");
     }
-
 }
