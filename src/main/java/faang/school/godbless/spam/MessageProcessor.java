87810212b250e94e60d1fb9169e0d74174f0af3c
Maxim Suhanov <maksimka_13.05.2004@mail.ru>
2024-04-16 14:35:19 +0400

    BJS2-5343-makakcuk

--- src/main/java/faang/school/godbless/spam/MessageProcessor.java
+++ src/main/java/faang/school/godbless/spam/MessageProcessor.java
@@ -4,7 +4,6 @@ import java.util.List;
 public class MessageProcessor {
     public boolean processMessage(String message, List<MessageFilter> filters) {
         return filters.stream()
-                .filter(msgFilter -> msgFilter.filter(message))
-                .toList().size() == filters.size();
+                .filter(msgFilter -> msgFilter.filter(message)).count() == filters.size();
     }
 }
