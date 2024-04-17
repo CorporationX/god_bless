95f61d9f08af94bb8181ed833425a583eded8228
Rick <RickHammerson@ya.ru>
2024-04-17 23:53:54 +0300

    Небольшой рефактор

--- src/main/java/faang/school/godbless/module/third/parallelism/peter/GriffinsFoodDelivery.java
+++ src/main/java/faang/school/godbless/module/third/parallelism/peter/GriffinsFoodDelivery.java
@@ -7,12 +7,10 @@ import java.util.concurrent.Executors;
 public class GriffinsFoodDelivery {
     public static void main(String[] args) {
         ExecutorService executorService = Executors.newFixedThreadPool(3);
-        String[] characterNames = {"Peter", "Lois", "Meg", "Chris", "Stewie"};
+        String[] characterNames = { "Peter", "Lois", "Meg", "Chris", "Stewie" };
         
         for (String characterName : characterNames) {
-            int foodAmount = new Random().nextInt();
-            System.out.printf("Персонаж %s получает");
-            executorService.execute(new FoodDeliveryTask(characterName, foodAmount));
+            executorService.execute(new FoodDeliveryTask(characterName, new Random().nextInt()));
         }
         
         executorService.shutdown();
