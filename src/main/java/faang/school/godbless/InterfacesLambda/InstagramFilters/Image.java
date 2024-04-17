7cd9f78b8c08986de112d5f53d1a923858764c58
Alexey Kazakov <kaa89537113221ka@gmail.com>
2024-04-17 22:30:43 +0300

    Фильтры Instagram

---
+++ src/main/java/faang/school/godbless/InterfacesLambda/InstagramFilters/Image.java
@@ -0,0 +1,11 @@
+package faang.school.godbless.InterfacesLambda.InstagramFilters;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@AllArgsConstructor
+@Getter
+public class Image {
+    private final String name;
+    private final String description;
+}
