193b3e368c36e881c090a6bc76d889f8dc7a6113
Th0rn-dev <solnek33@gmail.com>
2024-04-17 18:55:16 +0300

    refactored code by recommendations

--- src/main/java/faang/school/godbless/outlook/MailSender.java
+++ src/main/java/faang/school/godbless/outlook/MailSender.java
@@ -1,14 +1,25 @@
 package faang.school.godbless.outlook;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class MailSender {
-    public static void main(String[] args) throws InterruptedException {
+    public static void main(String[] args) {
         int i = 1;
+        List<Thread> threads = new ArrayList<>();
         while (i < 1000) {
             Thread thread = new Thread(new SenderRunnable(i, i + 199));
+            threads.add(thread);
             i += 200;
             thread.start();
-            thread.join();
         }
+        threads.forEach(t -> {
+            try {
+                t.join();
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+        });
         System.out.println("All email sends");
     }
 }
