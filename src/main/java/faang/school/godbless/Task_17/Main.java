3f22ee3fd5d90031bfcbe84cf56fb0c0ff8f01ae
Vadim Shibanov <vladimirowitch.vadim@gmail.com>
2024-04-18 01:05:28 +0300

    BJS2-5660

---
+++ src/main/java/faang/school/godbless/Task_17/Main.java
@@ -0,0 +1,27 @@
+package faang.school.godbless.Task_17;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.IntStream;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(sumOfUniquePairs(10, Arrays.asList(1, 9, 3, 6, 4, 5)));
+
+    }
+
+    static Map<Integer, Integer> sumOfUniquePairs(int sum, List<Integer> numbers) {
+        Map<Integer, Integer> pairs = new HashMap<>();
+        IntStream.range(0, numbers.size()).
+                forEach(i -> IntStream.range(0, numbers.size()).
+                        filter(j -> i != j && numbers.get(i) + numbers.get(j) == sum).
+                        forEach(j -> {
+                            if (!pairs.containsValue(numbers.get(i))) {
+                                pairs.put(numbers.get(i), numbers.get(j));
+                            }
+                        }));
+        return pairs;
+    }
+}
