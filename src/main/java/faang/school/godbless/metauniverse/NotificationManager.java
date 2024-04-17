94df21f01ed25694a84a19e4fe06c680a85c5961
Aleh Biahun <stones2875@gmail.com>
2024-04-17 16:32:34 +0200

    Поставил пробел в указанных местах

--- src/main/java/faang/school/godbless/metauniverse/NotificationManager.java
+++ src/main/java/faang/school/godbless/metauniverse/NotificationManager.java
@@ -5,8 +5,8 @@ import java.util.Map;
 import java.util.function.Consumer;
 
 public class NotificationManager {
-    private final Map<String, Consumer<Notification>> handlers = new HashMap<>();
-    public void registerHandler(String notificationType, Consumer<Notification> handler) {
+    private final Map <String, Consumer<Notification>> handlers = new HashMap<>();
+    public void registerHandler (String notificationType, Consumer<Notification> handler) {
         handlers.put(notificationType, handler);
     }
     public void sendNotification(Notification notification) {
