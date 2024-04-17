f3e1be2a3149a14fed9202d0dd3835ff62fa48d5
DimaMinin <91825998+DimaMinin@users.noreply.github.com>
2024-04-17 17:24:28 +0300

    Task: https://faang-school.atlassian.net/browse/BJS2-5642

---
+++ src/main/java/faang/school/godbless/help_a_colleague/PersonWorkPlacePrinter.java
@@ -0,0 +1,16 @@
+package faang.school.godbless.help_a_colleague;
+
+import java.util.List;
+
+public class PersonWorkPlacePrinter implements Runnable {
+    private List<Person> persons;
+
+    public PersonWorkPlacePrinter(List<Person> persons) {
+        this.persons = persons;
+    }
+
+    @Override
+    public void run() {
+        persons.forEach(person -> System.out.println(person.getWorkPlace()));
+    }
+}
