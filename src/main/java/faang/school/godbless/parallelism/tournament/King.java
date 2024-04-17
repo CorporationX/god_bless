0fa03ae33997cb68670e70690c450e0f8ffeb01e
nadir <den4ik.strelok@gmail.com>
2024-04-17 21:15:52 +0300

    Оптимизировал код

--- src/main/java/faang/school/godbless/parallelism/tournament/King.java
+++ src/main/java/faang/school/godbless/parallelism/tournament/King.java
@@ -2,37 +2,34 @@ package faang.school.godbless.parallelism.tournament;
 
 import lombok.extern.slf4j.Slf4j;
 
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
-
 @Slf4j
 public class King {
     public static void main(String[] args) {
-        Knight sirJaimeLannister = new Knight("Sir Jaime Lannister");
-        Knight sirOberynMartell = new Knight("Sir Oberyn Martell");
+        final String sirJaimeLannisterName = "Sir Jaime Lannister";
+        final String sirOberynMartellName = "Sir Oberyn Martell";
+
+        log.info("Trials begin!");
 
 
-        Trial trial1SirJaime = new Trial("Sir Jaime Lannister", "Trial of Courage");
-        Trial trial2SirJaime = new Trial("Sir Jaime Lannister", "Trial of Strategy");
+        Knight sirJaimeLannister = new Knight(sirJaimeLannisterName);
+
+        Trial trial1SirJaime = new Trial(sirJaimeLannisterName, "Trial of Courage");
+        Trial trial2SirJaime = new Trial(sirJaimeLannisterName, "Trial of Strategy");
 
         sirJaimeLannister.addTrial(trial1SirJaime);
         sirJaimeLannister.addTrial(trial2SirJaime);
 
+        sirJaimeLannister.startTrials();
 
-        Trial trial1SirOberyn = new Trial("Sir Oberyn Martell", "Trial of Agility");
-        Trial trial2SirOberyn = new Trial("Sir Oberyn Martell", "Trial of Endurance");
+
+        Knight sirOberynMartell = new Knight(sirOberynMartellName);
+
+        Trial trial1SirOberyn = new Trial(sirOberynMartellName, "Trial of Agility");
+        Trial trial2SirOberyn = new Trial(sirOberynMartellName, "Trial of Endurance");
 
         sirOberynMartell.addTrial(trial1SirOberyn);
         sirOberynMartell.addTrial(trial2SirOberyn);
 
-
-        ExecutorService trialsThreadPool = Executors.newFixedThreadPool(2);
-
-        log.info("Trials begin!");
-
-        sirJaimeLannister.startTrials(trialsThreadPool);
-        sirOberynMartell.startTrials(trialsThreadPool);
-
-        trialsThreadPool.shutdown();
+        sirOberynMartell.startTrials();
     }
 }
--- src/main/java/faang/school/godbless/parallelism/tournament/Knight.java
+++ src/main/java/faang/school/godbless/parallelism/tournament/Knight.java
@@ -3,19 +3,24 @@ package faang.school.godbless.parallelism.tournament;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 
 public class Knight {
-    private String name;
-    private List<Trial> trials;
+    private final String name;
+    private final List<Trial> trials;
 
     public Knight(String name) {
         this.name = name;
         trials = new ArrayList<>();
     }
 
-    public void startTrials(ExecutorService executorService) {
+    public void startTrials() {
+        ExecutorService executorService = Executors.newFixedThreadPool(trials.size());
+
         trials.forEach(executorService::submit);
+
+        executorService.shutdown();
     }
 
     public void addTrial(Trial trial) {
