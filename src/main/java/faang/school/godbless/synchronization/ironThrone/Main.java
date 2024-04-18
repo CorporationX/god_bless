644f5886171f6bc5bbfa9a50b3f9a77dbdddb91f
nadir <den4ik.strelok@gmail.com>
2024-04-18 23:59:23 +0300

    BJS2-5747 (Железный трон любой ценой)

---
+++ src/main/java/faang/school/godbless/synchronization/ironThrone/Main.java
@@ -0,0 +1,51 @@
+package faang.school.godbless.synchronization.ironThrone;
+
+import lombok.extern.slf4j.Slf4j;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
+
+
+@Slf4j
+public class Main {
+    public static void main(String[] args) throws InterruptedException {
+        House stark = new House("Stark");
+        ExecutorService usersThreads = Executors.newCachedThreadPool();
+
+        List<User> users = new ArrayList<>();
+
+        users.add(new User("Rob"));
+        users.add(new User("John"));
+        users.add(new User("Bran"));
+        users.add(new User("Davos"));
+        users.add(new User("Aria"));
+
+
+        log.info("Game of thrones begins!");
+
+        users.forEach(user -> usersThreads.submit(() -> user.joinHouse(stark)));
+
+        Thread.sleep(5000);
+
+        usersThreads.submit(() -> users.get(0).leaveHouse(stark));
+
+        usersState(users);
+
+        users.forEach(user -> usersThreads.submit(() -> user.leaveHouse(stark)));
+
+        usersThreads.shutdown();
+        usersThreads.awaitTermination(10, TimeUnit.SECONDS);
+
+        log.info("Game of thrones terminated.");
+
+        usersState(users);
+    }
+
+    private static void usersState(List<User> users) {
+        log.info("Users state at this point:");
+        users.forEach(user -> log.info(user.toString()));
+    }
+}
