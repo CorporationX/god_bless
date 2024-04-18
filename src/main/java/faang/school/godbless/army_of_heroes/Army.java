a6f8c51bf159a89ca62067ab53b1fa41e4cf0793
Th0rn-dev <solnek33@gmail.com>
2024-04-18 22:32:17 +0300

    BJS2-5735 Распределенная армия героев меча и магии

---
+++ src/main/java/faang/school/godbless/army_of_heroes/Army.java
@@ -0,0 +1,62 @@
+package faang.school.godbless.army_of_heroes;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.stream.IntStream;
+
+public class Army {
+    private final List<Archer> archers = new ArrayList<>();
+    private final List<Swordsman> swordsmans = new ArrayList<>();
+    private final List<Mage> mages = new ArrayList<>();
+
+    public static void main(String[] args) throws InterruptedException {
+        Army army = new Army();
+        army.addUnit(new Archer(50));
+        army.addUnit(new Swordsman(70));
+        army.addUnit(new Mage(100));
+        army.addUnit(new Mage(200));
+
+        int totalPower = army.calculateTotalPower();
+        System.out.println("Total army power: " + totalPower);
+    }
+
+    private void addUnit(Archer archer) {
+        this.archers.add(archer);
+    }
+
+    private void addUnit(Swordsman swordsman) {
+        this.swordsmans.add(swordsman);
+    }
+
+    private void addUnit(Mage mage) {
+        this.mages.add(mage);
+    }
+
+    public Integer calculateTotalPower() throws InterruptedException {
+        AtomicInteger total = new AtomicInteger(0);
+        Thread calcTotalPowersArches = new Thread(() -> {
+            total.getAndAdd(archers.stream()
+                    .flatMapToInt(archer -> IntStream.of(archer.getPower())).sum());
+        });
+        Thread calcTotalPowerSwordsman = new Thread(() -> {
+            total.getAndAdd(swordsmans.stream()
+                    .flatMapToInt(swordsman -> IntStream.of(swordsman.getPower())).sum());
+        });
+
+        Thread calcTotalPowersMage = new Thread(() -> {
+            total.getAndAdd(mages.stream()
+                    .flatMapToInt(mage -> IntStream.of(mage.getPower())).sum());
+        });
+
+        calcTotalPowersArches.start();
+        calcTotalPowerSwordsman.start();
+        calcTotalPowersMage.start();
+
+        calcTotalPowersArches.join();
+        calcTotalPowerSwordsman.join();
+        calcTotalPowersMage.join();
+
+        return total.get();
+    }
+}
