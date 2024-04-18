6005d5f7db49b78c751a452720596cc1a1975a69
Tamzhid Babutsyngueva <btamzhid@gmail.com>
2024-04-18 22:11:20 +0800

    BJS2-5435 - Meta-вселенная? Попытка №2

--- src/main/java/meta_universe/NotificationManager.java
+++ src/main/java/meta_universe/NotificationManager.java
@@ -5,14 +5,14 @@ import java.util.Map;
 import java.util.function.Consumer;
 
 public class NotificationManager {
-    private Map<String, Consumer<Notification>> notificationIdsOfHandlers = new HashMap<String, Consumer<Notification>>();
+    private Map<String, Consumer<Notification>> handlers = new HashMap<String, Consumer<Notification>>();
 
-    public void registerHandler(String type, Consumer<Notification> notification) {
-        notificationIdsOfHandlers.put(type, notification);
+    public void registerHandler(String type, Consumer<Notification> handler) {
+        handlers.put(type, handler);
     }
 
     public void sendNotification(Notification notification) {
-        Consumer consumer = notificationIdsOfHandlers.get(notification.getType());
+        Consumer consumer = handlers.get(notification.getType());
         if (consumer != null) {
             consumer.accept(notification);
         } else {
