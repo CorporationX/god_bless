f9644dcefffc3cbea01c17f16fe73116658d6dba
romantarelicev <romuhut@mail.ru>
2024-04-18 13:50:43 +0300

    Накормите Питера Гриффина - BJS2-5644 fixed

--- src/main/java/Multithreading_FeedPeteGriffin/GriffinsFoodDelivery.java
+++ src/main/java/Multithreading_FeedPeteGriffin/GriffinsFoodDelivery.java
@@ -4,21 +4,23 @@ import java.util.Arrays;
 import java.util.Random;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
 
 public class GriffinsFoodDelivery {
-    public static void main(String[] args) {
-        ExecutorService threadPool = Executors.newFixedThreadPool(3);
+    private static final int COUNT_THREADS = 3;
+
+    public static void main(String[] args) throws InterruptedException {
+        ExecutorService threadPool = Executors.newFixedThreadPool(COUNT_THREADS);
         String[] characterNames = {"Peter", "Lois", "Meg", "Chris", "Stewie"};
         Random random = new Random();
 
         Arrays.stream(characterNames)
                 .toList()
                 .forEach(characterName -> {
-                    threadPool.execute(() -> {
-                        FoodDeliveryTask character = new FoodDeliveryTask(characterName, random.nextInt(1, 100));
-                        character.run();
-                    });
+                    threadPool.execute(new FoodDeliveryTask(characterName, random.nextInt(1, 100)));
                 });
+
         threadPool.shutdown();
+        threadPool.awaitTermination(10, TimeUnit.SECONDS);
     }
 }
