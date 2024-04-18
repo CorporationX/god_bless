ae99d59d45411bdb7bb10065a98719217393a1be
ethiqque <danielpyld.work@gmail.com>
2024-04-18 14:57:18 +0300

    BJS2-5496 Multithreading:Synchronization, wait/notify: Блокировка Spotify

---
+++ src/main/java/faang/school/godbless/Spotify/Player.java
@@ -0,0 +1,40 @@
+package faang.school.godbless.Spotify;
+
+public class Player {
+    private static final Object LOCK = new Object();
+    private boolean isPlaying = false;
+
+    public void play() {
+        synchronized (LOCK) {
+            if (!isPlaying) {
+                isPlaying = true;
+                System.out.println("Play music");
+            } else {
+                System.out.println("Music is already playing.");
+            }
+        }
+    }
+
+    public void pause() {
+        synchronized (LOCK) {
+            if (isPlaying) {
+                isPlaying = false;
+                System.out.println("Pause music.");
+            } else {
+                System.out.println("Music is already paused.");
+            }
+        }
+    }
+
+    public void skip() {
+        synchronized (LOCK) {
+            System.out.println("Skip song");
+        }
+    }
+
+    public void previous() {
+        synchronized (LOCK) {
+            System.out.println("Back to previous song");
+        }
+    }
+}
