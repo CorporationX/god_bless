85996b1b1fff3362e6d11c3137b888e7034c8ae3
s.kolbasov <s.kolbasov@bftcom.com>
2024-04-18 17:37:16 +0300

    BJS2-5719 Накормите Питера Гриффина

---
+++ src/main/java/faang/school/godbless/BJS2_5719/GriffinsFoodDelivery.java
@@ -0,0 +1,18 @@
+package faang.school.godbless.BJS2_5719;
+
+import java.util.Random;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+public class GriffinsFoodDelivery {
+  public static void main(String[] args) {
+    ExecutorService executorService = Executors.newFixedThreadPool(3);
+    String[] characterNames = {"Peter", "Lois", "Meg", "Chris", "Stewie"};
+
+    for (int i = 0; i < characterNames.length; i++) {
+      executorService.execute(new FoodDeliveryTask(characterNames[i], new Random().nextInt(1, 20)));
+    }
+
+    executorService.shutdown();
+  }
+}
