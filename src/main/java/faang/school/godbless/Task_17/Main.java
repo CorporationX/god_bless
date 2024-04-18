884c35dc4ebd2b421fec75086cc94f1dce747bc4
Vadim Shibanov <vladimirowitch.vadim@gmail.com>
2024-04-19 01:13:38 +0300

    BJS2-5660

--- src/main/java/faang/school/godbless/Task_17/Main.java
+++ src/main/java/faang/school/godbless/Task_17/Main.java
@@ -1,14 +1,17 @@
 package faang.school.godbless.Task_17;
 
-import java.util.Arrays;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
+import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
 public class Main {
     public static void main(String[] args) {
         System.out.println(uniquePairs(10, Arrays.asList(1, 9, 3, 6, 4, 5)));
+        Map<String, String> countriesAndCapitals = new HashMap<>();
+        countriesAndCapitals.put("Russia", "Moscow");
+        countriesAndCapitals.put("England", "London");
+        System.out.println(capitals(countriesAndCapitals));
+        System.out.println(stringsByParticularRules(Arrays.asList("First", "Second", "Fly", "Finally"), 'F'));
 
     }
 
@@ -23,5 +26,17 @@ public class Main {
                             }
                         }));
         return pairs;
+    }
+
+    static List<String> capitals(Map<String, String> countriesAndCapitals) {
+        return countriesAndCapitals.entrySet().stream().sorted(Map.Entry.comparingByKey()).map(Map.Entry::getValue).collect(Collectors.toList());
+    }
+
+    static List<String> stringsByParticularRules(List<String> strings, char letter) {
+        return strings.stream().filter(s -> s.startsWith(String.valueOf(letter))).sorted(Comparator.comparingInt(String::length)).collect(Collectors.toList());
+    }
+
+    static void peopleWithCommonFriends(Map<String, List<String>> peopleAndFriends) {
+        peopleAndFriends.entrySet().stream().peek(entry -> entry.getKey())
     }
 }
