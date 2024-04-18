adb4efd3d4e1f060bc1bebdac86a860fa275ed6e
Nikolai Gnidenko <gnidenkonn@gmail.com>
2024-04-18 13:35:40 +0300

    fix

--- src/main/java/theBigBangTheory/BigBangTheory.java
+++ src/main/java/theBigBangTheory/BigBangTheory.java
@@ -3,10 +3,12 @@ package theBigBangTheory;
 import java.util.List;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
 
 public class BigBangTheory {
+    private static final int NUMBER_OF_THREADS = 4;
     public static void main(String[] args) {
-        ExecutorService executor = Executors.newFixedThreadPool(4);
+        ExecutorService executor = Executors.newFixedThreadPool(NUMBER_OF_THREADS);
 
         Task taskForSheldon = new Task("Sheldon", "Theory preparation");
         Task taskForLeonard = new Task("Leonard", "Experiment simulation");
@@ -16,11 +18,13 @@ public class BigBangTheory {
 
         tasks.forEach(executor::execute);
         executor.shutdown();
+
         try {
-            Thread.sleep(5000);
+            executor.awaitTermination(5, TimeUnit.SECONDS);
         } catch (InterruptedException e) {
             throw new RuntimeException(e);
         }
+
         System.out.println("All tasks is completed");
     }
 }
