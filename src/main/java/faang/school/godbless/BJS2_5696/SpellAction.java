dfe5419b3ed6d6007b771388e720f1ea59d439db
Vladimir Kriukov <vladimir@kriukov.pro>
2024-04-18 13:57:45 +0300

    BJS2-5696: Хогвартс, Хогвартс, наш любимый Хогвартс

---
+++ src/main/java/faang/school/godbless/BJS2_5696/SpellAction.java
@@ -0,0 +1,6 @@
+package faang.school.godbless.BJS2_5696;
+
+@FunctionalInterface
+public interface SpellAction {
+    String cast(String spell);
+}
---
+++ src/main/java/faang/school/godbless/BJS2_5696/SpellCaster.java
@@ -0,0 +1,7 @@
+package faang.school.godbless.BJS2_5696;
+
+public class SpellCaster {
+    public void cast(String spell, SpellAction action) {
+        System.out.println(action.cast(spell));
+    }
+}
