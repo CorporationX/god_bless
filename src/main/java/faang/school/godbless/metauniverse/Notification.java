874e634e9ab677c5061389713485127fce0a4717
Aleh Biahun <stones2875@gmail.com>
2024-04-18 11:11:26 +0200

    Подправил замечанию по удалению @NoArgsConstructor и @Setter. Но не понимаю, что делать с гетерами в main классе. Они выдают ошибку

--- src/main/java/faang/school/godbless/metauniverse/Notification.java
+++ src/main/java/faang/school/godbless/metauniverse/Notification.java
@@ -3,12 +3,10 @@ package faang.school.godbless.metauniverse;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-import lombok.Setter;
 
 @AllArgsConstructor
-@NoArgsConstructor
 @Getter
-@Setter
+
 public class Notification {
     private String type;
     private String message;
