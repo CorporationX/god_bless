5afcf4812ce96bd0b92637591a0cbc54037adcfd
mihanchik94 <mihanchik94@gmail.com>
2024-04-18 20:09:30 +0300

    Распределенная армия героев меча и магии

--- src/main/java/faang/school/godbless/multithreading/task_3/model/Army.java
+++ src/main/java/faang/school/godbless/multithreading/task_3/model/Army.java
@@ -10,14 +10,24 @@ public class Army {
 
     public int calculateTotalPower() throws InterruptedException {
         int totalPower = 0;
+        List<UnitPowerCalculator>  unitPowerCalculators = getUnitPowerCalculatorThreads();
+        for (UnitPowerCalculator unitPowerCalculator : unitPowerCalculators) {
+            unitPowerCalculator.join();
+            int unitPower = unitPowerCalculator.getPower();
+            totalPower += unitPower;
+        }
+        return totalPower;
+    }
+
+    private List<UnitPowerCalculator> getUnitPowerCalculatorThreads() {
+        List<UnitPowerCalculator>  unitPowerCalculators = new ArrayList<>();
         for (Unit unit : units) {
             UnitPowerCalculator unitPowerCalculator = new UnitPowerCalculator(unit);
             unitPowerCalculator.start();
+            unitPowerCalculators.add(unitPowerCalculator);
             System.out.println(unitPowerCalculator.getName() + " начал свою работу");
-            unitPowerCalculator.join();
-            totalPower += unitPowerCalculator.getPower();
         }
-        return totalPower;
+        return unitPowerCalculators;
     }
 
     public void addUnit(Unit unit) {
