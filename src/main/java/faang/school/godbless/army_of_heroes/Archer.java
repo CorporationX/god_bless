9fb0c0cdd6e24d469e6e64a3c31ed85bf3d76927
Th0rn-dev <solnek33@gmail.com>
2024-04-18 22:32:17 +0300

    BJS2-5735 Распределенная армия героев меча и магии

---
+++ src/main/java/faang/school/godbless/army_of_heroes/Archer.java
@@ -0,0 +1,10 @@
+package faang.school.godbless.army_of_heroes;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class Archer {
+    private int power;
+}
