b73174bf495e3e18bb61bfbc9a390f2f215a05e9
Maxim Suhanov <maksimka_13.05.2004@mail.ru>
2024-04-19 18:42:39 +0400

    BJS2-5784-makakcuk

--- src/main/java/faang/school/godbless/youtube/VideoManager.java
+++ src/main/java/faang/school/godbless/youtube/VideoManager.java
@@ -7,17 +7,14 @@ import java.util.Map;
 
 @Getter
 public class VideoManager {
+
     private final Map<String, Integer> viewsMap = new HashMap<>();
 
-    public void addView(String videoId){
-        synchronized (viewsMap) {
-            viewsMap.compute(videoId, (key, value) -> value == null ? 1 : value+1);
-        }
+    public synchronized void addView(String videoId) {
+        viewsMap.compute(videoId, (key, value) -> value == null ? 1 : value + 1);
     }
 
-    public int getViewCount(String videoId){
-        synchronized (viewsMap) {
-            return viewsMap.get(videoId);
-        }
+    public synchronized int getViewCount(String videoId) {
+        return viewsMap.get(videoId);
     }
 }
