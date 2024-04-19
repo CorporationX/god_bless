3f14594d8226e9f9c9b28e4f1fcd3ff6990387e7
ethiqque <danielpyld.work@gmail.com>
2024-04-19 15:33:24 +0300

    BJS2-5050 Java 8: Функциональные интерфейсы, Лямбды: Lord of the Rings RPG

--- src/main/java/faang/school/godbless/LordOfTheRingsRPG/InventoryManager.java
+++ src/main/java/faang/school/godbless/LordOfTheRingsRPG/InventoryManager.java
@@ -6,21 +6,16 @@ import java.util.function.Predicate;
 
 public class InventoryManager {
     void addItem(Character character, Item item, Consumer<Item> action) {
-        character.inventory().add(item);
+        character.addItem(item);
         action.accept(item);
     }
 
     void removeItem(Character character, Predicate<Item> condition) {
-        character.inventory().removeIf(condition);
+        character.removeItem(condition);
     }
 
     public void updateItem(Character character, Predicate<Item> filter, Function<Item, Item> updater) {
-        for (Item item : character.inventory()) {
-            if (filter.test(item)) {
-                character.inventory().set(character.inventory().indexOf(item), updater.apply(item));
-                break;
-            }
-        }
+        character.updateItem(filter, updater);
     }
 }
 
--- src/main/java/faang/school/godbless/LordOfTheRingsRPG/Main.java
+++ src/main/java/faang/school/godbless/LordOfTheRingsRPG/Main.java
@@ -1,25 +1,16 @@
 package faang.school.godbless.LordOfTheRingsRPG;
 
-import java.util.List;
-
 public class Main {
     public static void main(String[] args) {
+        Character frodo = new Character();
         Item ring = new Item("The One Ring", 1000);
-        Item shield = new Item("Shield", 1000);
-
-        Character frodo = new Character(List.of(ring));
 
         InventoryManager manager = new InventoryManager();
 
         manager.addItem(frodo, ring, (item) -> System.out.println(item.getName() + " was added to the inventory."));
 
-        manager.addItem(frodo, shield, (item) -> System.out.println(item.getName() + " was added to the inventory."));
-
         manager.removeItem(frodo, (item) -> item.getName().equals("The One Ring"));
 
         manager.updateItem(frodo, (item) -> item.getName().equals("The One Ring"), (item) -> new Item(item.getName(), item.getValue() * 2));
-
-        System.out.println(frodo.inventory());
-
     }
 }
