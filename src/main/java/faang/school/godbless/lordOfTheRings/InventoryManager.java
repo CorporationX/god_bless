fce614ff09115140e712971fb0de5c9866a7c3b6
egorp5119 <egorplo@gmail.com>
2024-04-20 17:44:42 +0300

    BJS2-5541-correction by comments

--- src/main/java/faang/school/godbless/lordOfTheRings/InventoryManager.java
+++ src/main/java/faang/school/godbless/lordOfTheRings/InventoryManager.java
@@ -1,13 +1,13 @@
 package faang.school.godbless.lordOfTheRings;
 
-import java.util.List;
 import java.util.function.Consumer;
 import java.util.function.Function;
 import java.util.function.Predicate;
 
 public class InventoryManager {
     public void addItem(Character character, Item item, Consumer<Item> consumer) {
-        consumer.andThen(add -> character.getInventory().add(item)).accept(item);
+        character.getInventory().add(item);
+        consumer.accept(item);
     }
 
     public void removeItem(Character character, Predicate<Item> predicate) {
@@ -16,15 +16,13 @@ public class InventoryManager {
 //        character.getInventory().removeIf(item -> predicate.test(item));
     }
 
-
     public void updateItem(Character character, Predicate<Item> predicate, Function<Item, Item> function) {
-        List<Item> inventory = character.getInventory();
-        for(Item item : inventory){
-            if (predicate.test(item)){
-                Item updatedItem = function.apply(item);
-                inventory.set(inventory.indexOf(item),updatedItem);
-                break;
-            }
-        }
+        character.getInventory().stream()
+                .filter(predicate)
+                .findFirst()
+                .ifPresent(item -> character.getInventory()
+                        .set(character.getInventory().indexOf(item),
+                                function.apply(item)));
     }
+
 }
