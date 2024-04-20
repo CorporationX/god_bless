0abf0dd1e7725e60f9441357d81a710dd746cf94
ethiqque <danielpyld.work@gmail.com>
2024-04-20 11:31:20 +0300

    bag

--- src/main/java/faang/school/godbless/Syncrhonization/BroForce/Game.java
+++ src/main/java/faang/school/godbless/Syncrhonization/BroForce/Game.java
@@ -1,4 +1,41 @@
 package faang.school.godbless.Syncrhonization.BroForce;
 
 public class Game {
+    private int score;
+    private int lives;
+    private final Object LOCK = new Object();
+
+    public Game() {
+        this.score = 0;
+        this.lives = 3;
+    }
+
+    public void update() {
+        synchronized (LOCK) {
+            lives--;
+            if (lives <= 0) {
+                gameOver();
+            }
+        }
+        synchronized (LOCK) {
+            score++;
+        }
+    }
+
+    private void gameOver() {
+        System.out.println("Game over! Total score: " + score);
+        System.exit(0);
+    }
+
+    public int getScore() {
+        synchronized (LOCK) {
+            return score;
+        }
+    }
+
+    public int getLives() {
+        synchronized (LOCK) {
+            return lives;
+        }
+    }
 }
