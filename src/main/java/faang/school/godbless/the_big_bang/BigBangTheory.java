8151a19957b9a1e99d888c41fa91b065ac2fa93a
Roman Antonov <romzesvxo@mail.ru>
2024-04-20 11:20:26 +0300

    BJS2-5792 Теперь программа будет ждать выполнения задач не более 5 секунд

--- src/main/java/faang/school/godbless/the_big_bang/BigBangTheory.java
+++ src/main/java/faang/school/godbless/the_big_bang/BigBangTheory.java
@@ -2,22 +2,30 @@ package faang.school.godbless.the_big_bang;
 
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
 
 public class BigBangTheory {
-    public static void main(String[] args) {
-        ExecutorService executorService = Executors.newFixedThreadPool(4);
-        Task sheldonTask = new Task("Шелдон", "подготовка теории");
-        Task leonardTask = new Task("Леонард", "моделирование эксперимента");
-        Task howardTask = new Task("Говард", "разработка инструментов");
-        Task rajeshTask = new Task("Раджеш", "анализ данных");
-        executorService.execute(sheldonTask);
-        executorService.execute(leonardTask);
-        executorService.execute(howardTask);
-        executorService.execute(rajeshTask);
+        public static void main(String[] args) {
+            ExecutorService executorService = Executors.newFixedThreadPool(4);
+            Task sheldonTask = new Task("Шелдон", "подготовка теории");
+            Task leonardTask = new Task("Леонард", "моделирование эксперимента");
+            Task howardTask = new Task("Говард", "разработка инструментов");
+            Task rajeshTask = new Task("Раджеш", "анализ данных");
+            executorService.execute(sheldonTask);
+            executorService.execute(leonardTask);
+            executorService.execute(howardTask);
+            executorService.execute(rajeshTask);
 
-        executorService.shutdown();
+            executorService.shutdown();
 
-        while (!executorService.isTerminated()) {}
-        System.out.println("Все задания выполнены");
+            try {
+                if (!executorService.awaitTermination(5, TimeUnit.SECONDS)) {
+                    executorService.shutdownNow();
+                }
+            } catch (InterruptedException e) {
+                executorService.shutdownNow();
+                Thread.currentThread().interrupt();
+            }
+            System.out.println("Все задания выполнены");
+        }
     }
-}
