c167406faa6b84d285f3cccaf7addd8d9952a978
Vadim Shibanov <vladimirowitch.vadim@gmail.com>
2024-04-20 23:30:57 +0300

    BJS2-5660

--- src/main/java/faang/school/godbless/Task_17/Employee.java
+++ src/main/java/faang/school/godbless/Task_17/Employee.java
@@ -5,8 +5,6 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.experimental.FieldDefaults;
 
-import javax.swing.*;
-
 @Data
 @AllArgsConstructor
 @FieldDefaults(level = AccessLevel.PRIVATE)
