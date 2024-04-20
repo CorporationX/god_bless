f581b89746af1e31d048ce958027c00303bf5984
Vadim Shibanov <vladimirowitch.vadim@gmail.com>
2024-04-20 23:28:18 +0300

    BJS2-5660

--- src/main/java/faang/school/godbless/Task_17/Main.java
+++ src/main/java/faang/school/godbless/Task_17/Main.java
@@ -34,7 +34,7 @@ public class Main {
                 'u', 'v', 'w', 'x', 'y', 'z'};
         sortStringsByAlphabet(Arrays.asList("apple", "banana", "123", "dog", "cat"), charArray);
 
-        System.out.println(ListOfIntegersToListOfStrings(Arrays.asList(1, 2, 3, 4, 5)));
+        System.out.println(ListOfIntegersToListOfStrings(Arrays.asList(0, 0, -3, 4, 5)));
 
         allNumbersLikePalindrome(1, 50);
 
@@ -45,86 +45,132 @@ public class Main {
     }
 
     static Map<Integer, Integer> uniquePairs(int sum, List<Integer> numbers) {
-        Map<Integer, Integer> pairs = new HashMap<>();
-        IntStream.range(0, numbers.size()).
-                forEach(i -> IntStream.range(0, numbers.size()).
-                        filter(j -> i != j && numbers.get(i) + numbers.get(j) == sum).
-                        forEach(j -> {
-                            if (!pairs.containsValue(numbers.get(i))) {
-                                pairs.put(numbers.get(i), numbers.get(j));
-                            }
-                        }));
-        return pairs;
+        if (numbers != null) {
+            Map<Integer, Integer> pairs = new HashMap<>();
+            IntStream.range(0, numbers.size()).
+                    forEach(i -> IntStream.range(0, numbers.size()).
+                            filter(j -> {
+                                        if (numbers.get(i) != null && numbers.get(j) != null) {
+                                            return i != j && numbers.get(i) + numbers.get(j) == sum;
+                                        } else throw new IllegalArgumentException("One of numbers is null");
+                                    }
+                            ).forEach(j -> {
+                                if (!pairs.containsValue(numbers.get(i))) {
+                                    pairs.put(numbers.get(i), numbers.get(j));
+                                }
+                            }));
+            return pairs;
+        } else throw new IllegalArgumentException("Input is null");
     }
 
     static List<String> capitals(Map<String, String> countriesAndCapitals) {
-        return countriesAndCapitals.entrySet().stream().sorted(Map.Entry.comparingByKey()).map(Map.Entry::getValue).collect(Collectors.toList());
+        if (countriesAndCapitals != null) {
+            return countriesAndCapitals.entrySet().stream().sorted((entryOne, entryAnother) -> {
+                        if (entryOne.getKey() != null && entryAnother.getKey() != null) {
+                            return entryOne.getKey().compareTo(entryAnother.getKey());
+                        } else throw new IllegalArgumentException("Key of entry is null");
+                    }
+            ).map(entry -> {
+                        if (entry.getValue() != null) {
+                            return entry.getValue();
+                        } else throw new IllegalArgumentException("Value of entry is null");
+                    }
+            ).collect(Collectors.toList());
+        } else throw new IllegalArgumentException("Input is null");
     }
 
     static List<String> stringsByParticularRules(List<String> strings, char letter) {
-        return strings.stream().filter(s -> s.startsWith(String.valueOf(letter))).sorted(Comparator.comparingInt(String::length)).collect(Collectors.toList());
+        if (strings != null) {
+            return strings.stream().filter(s -> s.startsWith(String.valueOf(letter))).sorted(Comparator.comparingInt(String::length)).collect(Collectors.toList());
+        } else throw new IllegalArgumentException("Input is null");
     }
 
     static void peopleWithCommonFriends(Map<String, List<String>> peopleAndFriends) {
-        peopleAndFriends.entrySet().stream().
-                filter(entryOne -> peopleAndFriends.entrySet().stream().
-                        anyMatch(entryAnother -> {
-                                    if (!entryOne.getKey().equals(entryAnother.getKey())) {
-                                        return entryOne.getValue().stream().
-                                                anyMatch(stringFromEntryOne -> entryAnother.getValue().stream().
-                                                        anyMatch(stringFromEntryOne::equals));
-
-                                    } else return false;
-                                }
-                        )).forEach(entry -> System.out.println(entry.getKey()));
-
+        if (peopleAndFriends != null) {
+            peopleAndFriends.entrySet().stream().
+                    filter(entryOne -> peopleAndFriends.entrySet().stream().
+                            anyMatch(entryAnother -> {
+                                        if (entryOne.getKey() != null && entryAnother.getKey() != null && entryOne.getValue() != null && entryAnother.getValue() != null) {
+                                            if (!entryOne.getKey().equals(entryAnother.getKey())) {
+                                                return entryOne.getValue().stream().
+                                                        anyMatch(stringFromEntryOne ->
+                                                                entryAnother.getValue().stream().anyMatch(stringFromEntryAnother -> {
+                                                                    if (stringFromEntryOne != null && stringFromEntryAnother != null) {
+                                                                        return stringFromEntryOne.equals(stringFromEntryAnother);
+                                                                    } else
+                                                                        throw new IllegalArgumentException("Component of value of entry is null");
+                                                                }));
+                                            } else return false;
+                                        } else throw new IllegalArgumentException("Key or Value of entry is null");
+                                    }
+                            )).forEach(entry -> System.out.println(entry.getKey()));
+        } else throw new IllegalArgumentException("Input is null");
     }
 
     static void middleSalaryByDeparture(List<Employee> employees) {
-        Map<String, Double> middleSalaryByDeparture = new HashMap<>();
-        employees.stream().collect(Collectors.groupingBy(Employee::getDeparture)).
-                forEach((key, value) -> IntStream.range(0, value.size()).forEach(i -> {
-                    if (i != 0) {
-                        middleSalaryByDeparture.put(key, (middleSalaryByDeparture.get(key) + (value.get(i).getSalary()) / value.size()));
-                    } else {
-                        middleSalaryByDeparture.put(key, (value.get(i).getSalary()) / value.size());
+        if (employees != null) {
+            Map<String, Double> middleSalaryByDeparture = new HashMap<>();
+            employees.stream().collect(Collectors.groupingBy(employee -> {
+                        if (employee.getDeparture() != null && employee.getSalary() >= 0) {
+                            return employee.getDeparture();
+                        } else throw new IllegalArgumentException("One of the component of input is null or < 0");
                     }
-                }));
-        System.out.println(middleSalaryByDeparture);
+            )).forEach((key, value) -> IntStream.range(0, value.size()).forEach(i -> {
+                        if (i != 0) {
+                            middleSalaryByDeparture.put(key, (middleSalaryByDeparture.get(key) + (value.get(i).getSalary()) / value.size()));
+                        } else {
+                            middleSalaryByDeparture.put(key, (value.get(i).getSalary()) / value.size());
+                        }
+                    }
+            ));
+            System.out.println(middleSalaryByDeparture);
+        } else throw new IllegalArgumentException("Input is null");
     }
 
     static void sortStringsByAlphabet(List<String> strings, char[] alphabet) {
-        List<String> stringsAfterActions = new ArrayList<>();
-        strings.stream().sorted(Comparator.comparingInt(String::length)).
-                forEach(string -> IntStream.range(0, alphabet.length).filter(i -> string.indexOf(alphabet[i]) != -1).forEach(i -> {
-                    if (!stringsAfterActions.contains(string)) {
-                        stringsAfterActions.add(string);
-                    }
-                }));
-        System.out.println(stringsAfterActions);
+        if (strings != null) {
+            List<String> stringsAfterActions = new ArrayList<>();
+            strings.stream().sorted((stringOne, stringAnother) -> {
+                        if (stringOne != null && stringAnother != null) {
+                            return stringOne.length() - stringAnother.length();
+                        } else throw new IllegalArgumentException("Component of Input is null");
+                    }).
+                    forEach(string -> IntStream.range(0, alphabet.length).filter(i -> string.indexOf(alphabet[i]) != -1).forEach(i -> {
+                        if (!stringsAfterActions.contains(string)) {
+                            stringsAfterActions.add(string);
+                        }
+                    }));
+            System.out.println(stringsAfterActions);
+        } else throw new IllegalArgumentException("Input is null");
     }
 
     static List<String> ListOfIntegersToListOfStrings(List<Integer> numbers) {
-        return numbers.stream().map(Integer::toBinaryString).toList();
+        if (numbers != null) {
+            return numbers.stream().map(Integer::toBinaryString).toList();
+        } else throw new IllegalArgumentException("Input is null");
     }
 
     static void allNumbersLikePalindrome(int start, int end) {
-        IntStream.range(start, end).filter(number -> Integer.toString(number).
-                contentEquals(new StringBuilder(Integer.toString(number)).reverse())).forEach(System.out::println);
+        if (start > 0 && end > 0) {
+            IntStream.range(start, end).filter(number -> Integer.toString(number).
+                    contentEquals(new StringBuilder(Integer.toString(number)).reverse())).forEach(System.out::println);
+        } else throw new IllegalArgumentException("Value of input numbers < 0");
     }
 
     static void allPalindromeSubstrings(String string) {
-        if (string!= null) {
+        if (string != null) {
             List<String> subStrings = new ArrayList<>();
             IntStream.range(0, string.length()).forEach(i ->
-                    IntStream.range(i + 1, string.length()).filter(j -> string.substring(i, j + 1).contentEquals(new StringBuilder(string.substring(i, j + 1)).reverse())).forEach(j -> subStrings.add(string.substring(i, j + 1))));
+                    IntStream.range(i + 1, string.length()).filter(j -> string.substring(i, j + 1).contentEquals(new StringBuilder(string.substring(i, j + 1)).reverse())).
+                            forEach(j -> subStrings.add(string.substring(i, j + 1))));
             System.out.println(subStrings);
         } else throw new IllegalArgumentException("Input is null");
     }
 
     static void allPerfectNumbers(int start, int end) {
         if (start > 0 && end > 0) {
-            IntStream.range(start == 1 ? start + 1 : start, end + 1).filter(number -> number == (IntStream.range(1, number).filter(i -> number % i == 0).reduce(Integer::sum)).getAsInt()).forEach(System.out::println);
+            IntStream.range(start == 1 ? start + 1 : start, end + 1).filter(number -> number == (IntStream.range(1, number).filter(i -> number % i == 0).reduce(Integer::sum)).getAsInt()).
+                    forEach(System.out::println);
         } else throw new IllegalArgumentException("Value of input numbers < 0");
     }
 }
