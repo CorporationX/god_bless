9e77cc863be28c4102828e883853d4c8e20294d0
Roman Antonov <romzesvxo@mail.ru>
2024-04-20 11:23:45 +0300

    BJS2-5793 Написал блок ожидания выполнения всех потоков

--- src/main/java/faang/school/godbless/uizly/WeasleyFamily.java
+++ src/main/java/faang/school/godbless/uizly/WeasleyFamily.java
@@ -2,6 +2,7 @@ package faang.school.godbless.uizly;
 
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
 
 public class WeasleyFamily {
     public static void main(String[] args) {
@@ -12,5 +13,10 @@ public class WeasleyFamily {
             executorService.execute(task);
         }
         executorService.shutdown();
+        try {
+            executorService.awaitTermination(Long.MAX_VALUE, TimeUnit.NANOSECONDS);
+        } catch (InterruptedException e) {
+            System.out.println("Что то пошло не так(");
+        }
     }
 }
