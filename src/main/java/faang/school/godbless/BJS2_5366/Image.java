1cdee48fd4be0b0ae1eb2a2453d59c06336fa668
Sergei Agafonov <sergeyagafonov.work@gmail.com>
2024-04-21 22:21:27 +0700

    Solution to task BJS2-5366

---
+++ src/main/java/faang/school/godbless/BJS2_5366/Image.java
@@ -0,0 +1,12 @@
+package faang.school.godbless.BJS2_5366;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+
+public class Image {
+    private String name;
+    private String description;
+}
