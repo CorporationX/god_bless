32e44aaf412d1e9e8df52366f8976401e5a59e83
Roman Antonov <romzesvxo@mail.ru>
2024-04-21 13:17:32 +0300

    BJS2-5661 Добавил зависмость в build.gradle для работы с json и поправил класс JobScraper

--- src/main/java/faang/school/godbless/vacancy_analyzer/JobScraper.java
+++ src/main/java/faang/school/godbless/vacancy_analyzer/JobScraper.java
@@ -1,26 +1,27 @@
 package faang.school.godbless.vacancy_analyzer;
 
-import org.json.JSONArray;
-import org.json.JSONObject;
+import com.google.gson.Gson;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonArray;
 
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
 import java.util.List;
-import java.util.stream.Collectors;
 
 public class JobScraper {
     public static Job parseJsonToJob(String json){
-        JSONObject jsonObject = new JSONObject(json);
-
-        String title = jsonObject.getString("title");
-        List<String> requirements = ((JSONArray) jsonObject.get("requirements"))
-                .toList()
-                .stream()
-                .map(Object::toString)
-                .collect(Collectors.toList());
-        int salary = jsonObject.getInt("salary");
-        String location = jsonObject.getString("location");
-        String dateAddedString = jsonObject.getString("dateAdded");
+        Gson gson = new Gson();
+        JsonObject jsonObject = gson.fromJson(json, JsonObject.class);
+        String title = jsonObject.get("title").getAsString();
+        List<String> requirements = new ArrayList<>();
+        JsonArray requirementsArray = jsonObject.getAsJsonArray("requirements");
+        for (int i = 0; i < requirementsArray.size(); i++) {
+            requirements.add(requirementsArray.get(i).getAsString());
+        }
+        int salary = jsonObject.get("salary").getAsInt();
+        String location = jsonObject.get("location").getAsString();
+        String dateAddedString = jsonObject.get("dateAdded").getAsString();
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
         LocalDate dateAdded = LocalDate.parse(dateAddedString, formatter);
         return new Job(title, requirements, salary, location, dateAdded);
