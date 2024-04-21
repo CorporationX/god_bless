e9a6cee3999a6255a36cf2e762880882fd357737
Vadim Shibanov <vladimirowitch.vadim@gmail.com>
2024-04-21 20:31:08 +0300

    BJS2-5660

--- src/main/java/faang/school/godbless/Task_17/Main.java
+++ src/main/java/faang/school/godbless/Task_17/Main.java
@@ -48,8 +48,7 @@ public class Main {
         if (numbers != null) {
             Map<Integer, Integer> pairs = new HashMap<>();
             IntStream.range(0, numbers.size()).
-                    forEach(i -> IntStream.range(0, numbers.size()).filter(Objects::nonNull).
-                            filter(j -> i != j && numbers.get(i) + numbers.get(j) == sum).forEach(j -> {
+                    forEach(i -> IntStream.range(0, numbers.size()).filter(Objects::nonNull).filter(j -> i != j && numbers.get(i) + numbers.get(j) == sum).forEach(j -> {
                                 if (!pairs.containsValue(numbers.get(i))) {
                                     pairs.put(numbers.get(i), numbers.get(j));
                                 }
