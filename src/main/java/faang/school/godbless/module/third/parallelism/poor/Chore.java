99e820974397640d19c40bfbef98f4bff531f2d8
Rick <RickHammerson@ya.ru>
2024-04-21 14:16:43 +0300

    Добавил ожидание конца дел - Thread.sleep

--- src/main/java/faang/school/godbless/module/third/parallelism/poor/Chore.java
+++ src/main/java/faang/school/godbless/module/third/parallelism/poor/Chore.java
@@ -9,5 +9,10 @@ public class Chore implements Runnable {
     @Override
     public void run() {
         System.out.printf("%s выполняет задание %s %n", Thread.currentThread().getName(), taskName);
+        try {
+            Thread.sleep(1000);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
