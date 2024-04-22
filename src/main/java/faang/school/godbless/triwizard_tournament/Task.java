c2b253481673e35ba00e281ce90f49ff2f041a32
DimaMinin <91825998+DimaMinin@users.noreply.github.com>
2024-04-22 12:17:09 +0300

    Task: https://faang-school.atlassian.net/browse/BJS2-5892

---
+++ src/main/java/faang/school/godbless/triwizard_tournament/Task.java
@@ -0,0 +1,12 @@
+package faang.school.godbless.triwizard_tournament;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class Task {
+    private String name;
+    private int difficulty;
+    private int reward;
+}
