6fde2d16526b449ed5aedc0313bbec6562475318
DimaMinin <91825998+DimaMinin@users.noreply.github.com>
2024-04-22 09:24:10 +0300

    fix synchronization blocks

--- src/main/java/faang/school/godbless/iron_throne/User.java
+++ src/main/java/faang/school/godbless/iron_throne/User.java
@@ -1,10 +1,8 @@
 package faang.school.godbless.iron_throne;
 
 import lombok.Data;
-import lombok.extern.slf4j.Slf4j;
 
 @Data
-@Slf4j
 public class User {
     private String name;
     private House house;
@@ -15,27 +13,10 @@ public class User {
     }
 
     public void joinHouse(House house, HouseRole houseRole) {
-        synchronized (house) {
-            while (house.getRolesMap().get(houseRole) <= 0) {
-                try {
-                    house.wait();
-                } catch (InterruptedException e) {
-                    throw new RuntimeException(e.getMessage());
-                }
-            }
-            house.addRole(houseRole);
-            this.house = house;
-            this.role = houseRole;
-            log.info("User with name {}, was joined to {}, with role {}", name, house.getName(), houseRole);
-        }
+        house.addRole(houseRole, this);
     }
 
     public void leaveHouse(House house) {
-        synchronized (house) {
-            house.removeRole(this.role);
-            log.info("User with name {}, was leaved {}, with role {}", name, house.getName(), this.role);
-            this.house = null;
-            this.role = null;
-        }
+        house.removeRole(role, this);
     }
 }
