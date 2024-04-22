f79fe84bdce077e4e08428dddd6a4bbfa58e9885
Vadim Shibanov <vladimirowitch.vadim@gmail.com>
2024-04-22 20:16:34 +0300

    BJS2-5926

---
+++ src/main/java/faang/school/godbless/Task_19/FoodDeliveryTask.java
@@ -0,0 +1,33 @@
+package faang.school.godbless.Task_19;
+
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.experimental.FieldDefaults;
+
+import java.util.Random;
+
+@Data
+@AllArgsConstructor
+@FieldDefaults(level = AccessLevel.PRIVATE)
+public class FoodDeliveryTask implements Runnable {
+    String character;
+    int foodAmount;
+
+    @Override
+    public void run() {
+        System.out.println(character + " get " + foodAmount + " of " + getFoodType());
+        try {
+            Thread.sleep(1);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+        System.out.println(character + " eat " + foodAmount + " of " + getFoodType());
+
+    }
+
+    private String getFoodType() {
+        String[] foodTypes = {"pizza", "burger", "hot dog", "chicken wings", "taco"};
+        return foodTypes[new Random().nextInt(foodTypes.length)];
+    }
+}
