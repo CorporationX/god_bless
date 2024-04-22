6bfa5263c2efff93ef421f003720b97fdead69a6
Rick <RickHammerson@ya.ru>
2024-04-23 02:36:24 +0300

    Решение задачи BJS2-5936 Счетчик просмотров на YouTube

---
+++ src/main/java/faang/school/godbless/module/third/synchronization/youtube/VideoManager.java
@@ -0,0 +1,24 @@
+package faang.school.godbless.module.third.synchronization.youtube;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class VideoManager {
+    static final Map<String, Integer> VIEWS_MAP = new HashMap<>();
+    
+    public void addView(String videoId) {
+        synchronized (VIEWS_MAP) {
+            VIEWS_MAP.compute(videoId, (key, value) -> value == null ? 1 : value + 1);
+        }
+    }
+    
+    public int getViewCount(String videoId) {
+        synchronized (VIEWS_MAP) {
+            return VIEWS_MAP.get(videoId);
+        }
+    }
+    
+    public void addVideo(String videoId) {
+        VIEWS_MAP.put(videoId, 0);
+    }
+}
