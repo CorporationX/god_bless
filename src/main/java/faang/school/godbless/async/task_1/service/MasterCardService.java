0bc23f6c794f3d9b28fe5a38523f36c512c95971
mihanchik94 <mihanchik94@gmail.com>
2024-04-22 20:16:49 +0300

    Асинхронность и Future

---
+++ src/main/java/faang/school/godbless/async/task_1/service/MasterCardService.java
@@ -0,0 +1,44 @@
+package faang.school.godbless.async.task_1.service;
+
+import lombok.extern.slf4j.Slf4j;
+
+import java.util.concurrent.*;
+
+@Slf4j
+public class MasterCardService {
+    private static final ExecutorService EXECUTOR = Executors.newCachedThreadPool();
+
+    public int collectPayment() {
+        try {
+            Thread.sleep(10_000);
+            return 10_000;
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+            throw new RuntimeException();
+        }
+    }
+
+    public int sendAnalytics() {
+        try {
+            Thread.sleep(1_000);
+            return 1_000;
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+            throw new RuntimeException();
+        }
+    }
+
+    public void doAll() {
+        Future<Integer> collectedPayment = EXECUTOR.submit(this::collectPayment);
+        CompletableFuture.supplyAsync(this::sendAnalytics, EXECUTOR)
+                .thenAccept(integer -> System.out.println(integer.intValue()));
+        try {
+            int collectedPaymentResult = collectedPayment.get();
+            System.out.println(collectedPaymentResult);
+        } catch (ExecutionException | InterruptedException e) {
+           log.error(e.getMessage());
+        }
+        EXECUTOR.shutdown();
+    }
+
+}
