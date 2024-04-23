213f78015170df1ec645408184370ce78782016b
Tem <tipokruto014@gmail.com>
2024-04-23 20:31:28 +0300

    а я все сделал

---
+++ src/main/java/faang/school/godbless/CalculatePI/Point.java
@@ -0,0 +1,11 @@
+package faang.school.godbless.CalculatePI;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class Point {
+    private double x;
+    private double y;
+}
