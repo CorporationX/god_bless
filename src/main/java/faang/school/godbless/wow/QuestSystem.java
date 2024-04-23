389952d6cd67cdb7c70cc82736b13077fd56fe96
Roman Antonov <romzesvxo@mail.ru>
2024-04-23 21:02:59 +0300

    BJS2-5985 wow

---
+++ src/main/java/faang/school/godbless/wow/QuestSystem.java
@@ -0,0 +1,19 @@
+package faang.school.godbless.wow;
+
+import java.util.concurrent.CompletableFuture;
+
+public class QuestSystem {
+
+    public CompletableFuture<Player> startQuest(Player player, Quest quest) {
+        return CompletableFuture.supplyAsync(() -> {
+            try {
+                Thread.sleep(quest.getDifficulty() * 1000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            int newExperience = player.getExperience() + quest.getReward();
+            player.setExperience(newExperience);
+            return player;
+        });
+    }
+}
