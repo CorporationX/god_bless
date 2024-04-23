b05c8d6eb53171abc7374632512fdbe024c3c082
Alex Vishnevskiy <vishgioall@gmail.com>
2024-04-23 21:29:06 +0300

    роботы дерутся, разнемити их кто нибудь уже

---
+++ src/main/java/faang/school/godbless/BJS2_5987/Robot.java
@@ -0,0 +1,12 @@
+package faang.school.godbless.BJS2_5987;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class Robot {
+    private String name;
+    private int attackPower;
+    private int defensePower;
+}
