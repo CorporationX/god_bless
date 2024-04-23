4868bf316b2f6e454385839a06fb93c724e9be4c
egorp5119 <egorplo@gmail.com>
2024-04-23 21:44:57 +0300

    BJS2-5976-Supercow! For review

---
+++ src/main/java/faang/school/godbless/supercow/Player.java
@@ -0,0 +1,20 @@
+package faang.school.godbless.supercow;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class Player {
+    private String name;
+
+    public void startBattle(Boss boss){
+        boss.joinBattle(this);
+        try {
+            Thread.sleep(5000);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+        boss.leaveBattle(this);
+    }
+}
