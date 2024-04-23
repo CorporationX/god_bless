3dcaaa2259646129a1b08dad1518382431a5a0fb
ekoregin <evgkoregin@gmail.com>
2024-04-23 09:38:31 +0300

    Task: https://faang-school.atlassian.net/browse/BJS2-5938

---
+++ src/main/java/faang/school/godbless/bjs2_5938/Item.java
@@ -0,0 +1,16 @@
+package faang.school.godbless.bjs2_5938;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@ToString
+@AllArgsConstructor
+public class Item {
+
+    private String name;
+    private int power;
+}
