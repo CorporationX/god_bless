70b70956cba98e7776ed969a62394db8538c6371
nadir <den4ik.strelok@gmail.com>
2024-04-23 21:18:06 +0300

    BJS2-5984 (Становимся celebrity в Twitter)

---
+++ src/main/java/faang/school/godbless/async/twitter/TwitterAccount.java
@@ -0,0 +1,12 @@
+package faang.school.godbless.async.twitter;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class TwitterAccount {
+    private String username;
+    private int followers;
+
+}
