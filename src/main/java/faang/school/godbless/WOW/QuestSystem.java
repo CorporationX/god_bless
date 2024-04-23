079ecfdecc02b04a58351b004c44cceb0b1b660c
Tem <tipokruto014@gmail.com>
2024-04-23 12:48:17 +0300

    а я все сделал

---
+++ src/main/java/faang/school/godbless/WOW/QuestSystem.java
@@ -0,0 +1,17 @@
+package faang.school.godbless.WOW;
+
+import java.util.concurrent.CompletableFuture;
+
+public class QuestSystem {
+    public CompletableFuture<Player> startQuest(Player player, Quest quest) {
+        return CompletableFuture.supplyAsync(() -> {
+            try {
+                Thread.sleep(quest.getDifficulty() * 1000L);
+                player.setExperience(player.getExperience() + quest.getReward());
+                return player;
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+        });
+    }
+}
