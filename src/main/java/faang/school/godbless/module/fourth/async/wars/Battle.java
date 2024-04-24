56890ae60d45c5c75930494903fda9cb53571a8d
Rick <RickHammerson@ya.ru>
2024-04-24 13:38:13 +0300

    Решение задачи BJS2-5971 Star Wars Arena

---
+++ src/main/java/faang/school/godbless/module/fourth/async/wars/Battle.java
@@ -0,0 +1,42 @@
+package faang.school.godbless.module.fourth.async.wars;
+
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+public class Battle {
+    public static void main(String[] args) {
+        Battle battle = new Battle();
+        
+        // Создание роботов
+        Robot r2d2 = new Robot("R2-D2", 5, 7);
+        Robot c3po = new Robot("C-3PO", 4, 8);
+        
+        // Запуск битвы
+        Future<Robot> winnerFuture = battle.fight(r2d2, c3po);
+        
+        // Обработка результата
+        try {
+            Robot winner = winnerFuture.get();
+            log.info("Победитель битвы: {}", winner.getName());
+        } catch (InterruptedException | ExecutionException e) {
+            e.printStackTrace();
+        }
+    }
+    
+    public Future<Robot> fight(Robot first, Robot second) {
+        ExecutorService executorService = Executors.newSingleThreadExecutor();
+        return executorService.submit(() -> {
+            log.info("Битва началась");
+            Thread.sleep(2000);
+            if (first.getAttackPower() > second.getDefensePower()) {
+                return first;
+            }
+            return second;
+        });
+    }
+}
