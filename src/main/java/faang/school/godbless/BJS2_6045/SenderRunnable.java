6577fcc96291243138cc01493ca0689502d4742b
AlexHor <apsekenesss@gmail.com>
2024-04-24 20:14:32 +0300

    Вы работаете в Microsoft

---
+++ src/main/java/faang/school/godbless/BJS2_6045/SenderRunnable.java
@@ -0,0 +1,17 @@
+package faang.school.godbless.BJS2_6045;
+
+import lombok.AllArgsConstructor;
+
+@AllArgsConstructor
+public class SenderRunnable implements Runnable {
+
+    private final int startIndex;
+    private final int endIndex;
+
+    @Override
+    public void run() {
+        for (int i = startIndex; i <= endIndex; i++) {
+            System.out.println("send " + i + " email");
+        }
+    }
+}
