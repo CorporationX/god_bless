88c8f344b77f4389eae9f8f224167e9d93f6d839
Nikolai Gnidenko <gnidenkonn@gmail.com>
2024-04-24 16:08:20 +0300

    Название задачи: Superheroes battle

---
+++ src/main/java/superHeroesBattle/Superhero.java
@@ -0,0 +1,12 @@
+package superHeroesBattle;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class Superhero {
+    private String name;
+    private int strength;
+    private int agility;
+}
