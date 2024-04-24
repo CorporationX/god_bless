9025140840fd1c32523a48cf9c5fce252f0cd73e
Th0rn-dev <solnek33@gmail.com>
2024-04-24 21:34:13 +0300

    refactored code by recommendations

--- src/main/java/faang/school/godbless/spotify/Player.java
+++ src/main/java/faang/school/godbless/spotify/Player.java
@@ -3,17 +3,16 @@ package faang.school.godbless.spotify;
 public class Player {
 
     private final Object lock = new Object();
-
     boolean isPlaying;
 
     public void play() {
-        if (!isPlaying) {
-            synchronized (lock) {
+        synchronized (lock) {
+            if (!isPlaying) {
                 this.isPlaying = true;
                 System.out.println("Player play music");
+            } else {
+                System.out.println("The music is already playing");
             }
-        } else {
-            System.out.println("The music is already playing");
         }
     }
 
@@ -26,31 +25,30 @@ public class Player {
         } else {
             System.out.println("The player does not play");
         }
-
     }
 
     public void skip() {
-        if (isPlaying) {
-            synchronized (lock) {
+        synchronized (lock) {
+            if (isPlaying) {
                 isPlaying = false;
                 System.out.println("Skipping track");
                 play();
+            } else {
+                System.out.println("The player is stopped");
             }
-        } else {
-            System.out.println("The player is stopped");
         }
-
     }
 
     public void previous() {
-        if (isPlaying) {
-            synchronized (lock) {
+        synchronized (lock) {
+            if (isPlaying) {
+
                 isPlaying = false;
                 System.out.println("Return previous track");
                 play();
+            } else {
+                System.out.println("The player is stopped");
             }
-        } else {
-            System.out.println("The player is stopped");
         }
     }
 }
