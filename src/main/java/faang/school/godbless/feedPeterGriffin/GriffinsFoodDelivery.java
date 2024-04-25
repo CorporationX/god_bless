6a503835c5133fb65100e40b0cf1c6ec62d9bb5f
Aleh Biahun <stones2875@gmail.com>
2024-04-25 20:17:00 +0200

    Количество потоков сделал константой

--- src/main/java/faang/school/godbless/feedPeterGriffin/GriffinsFoodDelivery.java
+++ src/main/java/faang/school/godbless/feedPeterGriffin/GriffinsFoodDelivery.java
@@ -5,9 +5,10 @@ import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
 public class GriffinsFoodDelivery {
+    public static final int NUMBER_OF_THREADS = 3;
     public static void main(String[] args) {
         String[] characterNames = {"Peter", "Lois", "Meg", "Chris", "Stewie"};
-        ExecutorService executorService = Executors.newFixedThreadPool(3);
+        ExecutorService executorService = Executors.newFixedThreadPool(NUMBER_OF_THREADS);
         for (String character : characterNames) {
             int foodAmount = new Random().nextInt(51);
             FoodDeliveryTask deliveryTask = new FoodDeliveryTask(character, foodAmount);
