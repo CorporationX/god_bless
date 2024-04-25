e8371498826fb95b9cc6ab07df202b31357ccb68
DimaMinin <91825998+DimaMinin@users.noreply.github.com>
2024-04-25 22:42:41 +0300

    Task: https://faang-school.atlassian.net/browse/BJS2-6105

---
+++ src/main/java/faang/school/godbless/mini_zoom/Main.java
@@ -0,0 +1,27 @@
+package faang.school.godbless.mini_zoom;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+public class Main {
+    private static final int THREAD_POOL_SIZE = 2;
+
+    public static void main(String[] args) {
+        ExecutorService executorService = Executors.newFixedThreadPool(THREAD_POOL_SIZE);
+        Conference conference = new Conference(3);
+        List<CompletableFuture<Void>> futures = new ArrayList<>();
+
+        for (int i = 0; i < conference.getTotalMembers(); i++) {
+            String name = "Test" + i;
+            futures.add(CompletableFuture.runAsync(() -> conference.addParticipant(new Participant(name)), executorService));
+        }
+
+        futures.forEach(CompletableFuture::join);
+        executorService.shutdown();
+
+        conference.endStreaming();
+    }
+}
