90d23408823cb9ddefe719517589c33c1fe2844b
ekoregin <evgkoregin@gmail.com>
2024-04-25 11:23:56 +0300

    Task: https://faang-school.atlassian.net/browse/BJS2-6104

---
+++ src/main/java/faang/school/godbless/bjs2_6104/DesignDepartment.java
@@ -0,0 +1,8 @@
+package faang.school.godbless.bjs2_6104;
+
+public class DesignDepartment extends Thread {
+
+    public DesignDepartment(Runnable target) {
+        super(target);
+    }
+}
---
+++ src/main/java/faang/school/godbless/bjs2_6104/MarketingDepartment.java
@@ -0,0 +1,8 @@
+package faang.school.godbless.bjs2_6104;
+
+public class MarketingDepartment extends Thread {
+
+    public MarketingDepartment(Runnable target) {
+        super(target);
+    }
+}
