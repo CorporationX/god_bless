37a6ea5e63d7c68fad698c53bb55613036724726
romantarelicev <romuhut@mail.ru>
2024-04-25 13:06:13 +0300

    Качаем алхимию - BJS2-6111 Выполнил задание, отправляю PR

---
+++ src/main/java/alchemy/Potion.java
@@ -0,0 +1,11 @@
+package alchemy;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class Potion {
+    private String name;
+    private int requiredIngredients;
+}
