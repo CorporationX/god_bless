62702a41d0dea95bb689563373e5a05af2b55588
Th0rn-dev <solnek33@gmail.com>
2024-04-26 08:31:46 +0300

    BJS2-6147 Star Wars Arena

---
+++ src/main/java/faang/school/godbless/arena/Battle.java
@@ -0,0 +1,16 @@
+package faang.school.godbless.arena;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+
+public class Battle {
+    public Future<Robot> fight(Robot robotOne, Robot robotTwo) {
+        ExecutorService executor = Executors.newSingleThreadExecutor();
+        return executor.submit(() -> {
+            int v = robotOne.getDefensePower() - robotTwo.getAttackPower() +  (int) (Math.random() * 10);
+            int v1 = robotTwo.getDefensePower() - robotOne.getAttackPower() + (int) (Math.random() * 10);
+            return v >= v1 ? robotOne : robotTwo ;
+        });
+    }
+}
