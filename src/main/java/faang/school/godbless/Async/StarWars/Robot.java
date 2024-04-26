2948209c65074adbe5bbc1f66b2c237523f04a25
ethiqque <danielpyld.work@gmail.com>
2024-04-26 11:35:06 +0300

    Multithreading: Async BJS2-5999 : Star Wars Arena

---
+++ src/main/java/faang/school/godbless/Async/StarWars/Robot.java
@@ -0,0 +1,12 @@
+package faang.school.godbless.Async.StarWars;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class Robot {
+    private String name;
+    private int attackPower;
+    private int defensePower;
+}
