4145fba8ad59ead444daa28091bf1b08934bfb16
nadir <den4ik.strelok@gmail.com>
2024-04-26 11:00:54 +0300

    Поправил код по комментариям

--- src/main/java/faang/school/godbless/async/triwizardTournament/Student.java
+++ src/main/java/faang/school/godbless/async/triwizardTournament/Student.java
@@ -14,31 +14,28 @@ public class Student {
         setYear(year);
     }
 
-    public Student setName(String name) {
+    public void setName(String name) {
         if (name == null || name.isBlank()) {
             throw new IllegalArgumentException("The Name cannot be null-valued or blank!");
         }
 
         this.name = name;
-        return this;
     }
 
-    public Student setYear(int year) {
+    public void setYear(int year) {
         if (year < 0) {
             throw new IllegalArgumentException("The year cannot be negative!");
         }
 
         this.year = year;
-        return this;
     }
 
-    public Student setPoints(int points) {
+    public void setPoints(int points) {
         if (points < 0) {
             throw new IllegalArgumentException("The points number cannot be negative!");
         }
 
         this.points = points;
-        return this;
     }
 
     public void gainPoints(int reward) {
--- src/main/java/faang/school/godbless/async/triwizardTournament/Tournament.java
+++ src/main/java/faang/school/godbless/async/triwizardTournament/Tournament.java
@@ -3,18 +3,14 @@ package faang.school.godbless.async.triwizardTournament;
 import lombok.extern.slf4j.Slf4j;
 
 import java.util.concurrent.CompletableFuture;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
 import java.util.concurrent.ThreadLocalRandom;
-import java.util.function.Supplier;
 
 @Slf4j
 public class Tournament {
     private static final long MIN_TIMEOUT = 3000L;
-    private static final ExecutorService THREAD_POOL = Executors.newCachedThreadPool();
 
     public CompletableFuture<School> startTask(School school, Task task) {
-        Supplier<School> taskExecutor = () -> {
+        return CompletableFuture.supplyAsync(() -> {
             var randomTimeout = ThreadLocalRandom.current().nextLong(1000);
 
             try {
@@ -23,10 +19,7 @@ public class Tournament {
                 log.error("Task (" + task.name() + ") execution was interrupted.", e);
             }
 
-            return school;
-        };
-
-        return CompletableFuture.supplyAsync(taskExecutor, THREAD_POOL)
-                .thenApply(schoolAfterTask -> schoolAfterTask.updatePoints(task.reward()));
+            return school.updatePoints(task.reward());
+        });
     }
 }
