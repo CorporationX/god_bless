d5076443d87ffc05428a6be40a5277b99e925450
Demchenkov7 <andreydemchenkov@yandex.ru>
2024-04-27 19:38:50 +0300

    Задача: Работа стоит

---
+++ src/main/java/faang/school/godbless/workisstoped/DesignDepartment.java
@@ -0,0 +1,7 @@
+package faang.school.godbless.workisstoped;
+
+public class DesignDepartment extends Thread {
+    public DesignDepartment(Runnable target) {
+        super(target);
+    }
+}
---
+++ src/main/java/faang/school/godbless/workisstoped/DesignResources.java
@@ -0,0 +1,40 @@
+package faang.school.godbless.workisstoped;
+
+import lombok.extern.slf4j.Slf4j;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.locks.ReadWriteLock;
+import java.util.concurrent.locks.ReentrantReadWriteLock;
+
+@Slf4j
+public class DesignResources {
+    private ReadWriteLock lock = new ReentrantReadWriteLock();
+    private List<String> resources = new ArrayList<>();
+
+    public void addResource(String resource) {
+        lock.writeLock().lock();
+        try {
+            Thread.sleep(1000);
+            resources.add(resource);
+            log.info("{} was added to Marketing list", resource);
+        } catch (InterruptedException e) {
+            log.error(e.getMessage());
+        } finally {
+            lock.writeLock().unlock();
+        }
+    }
+
+    public void printResources() {
+        lock.readLock().lock();
+        try {
+            log.info("Marketing resources print: ");
+            Thread.sleep(1000);
+            resources.forEach(System.out::println);
+        } catch (InterruptedException e) {
+            log.error(e.getMessage());
+        } finally {
+            lock.readLock().unlock();
+        }
+    }
+}
