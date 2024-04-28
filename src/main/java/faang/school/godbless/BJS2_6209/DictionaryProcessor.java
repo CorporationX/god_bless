4eaedffabb25c78c15595685fb71378cd02353eb
Unforgiven <mail.unforgiven@gmail.com>
2024-04-28 05:59:18 -0600

    Dictionaryhandler -> dictionaryHandler

--- src/main/java/faang/school/godbless/BJS2_6209/DictionaryProcessor.java
+++ src/main/java/faang/school/godbless/BJS2_6209/DictionaryProcessor.java
@@ -3,7 +3,7 @@ package faang.school.godbless.BJS2_6209;
 import java.util.function.BiConsumer;
 
 public class DictionaryProcessor {
-    public void processWord(String word, String translatedWord, BiConsumer<String, String> Dictionaryhandler) {
-        Dictionaryhandler.accept(word, translatedWord);
+    public void processWord(String word, String translatedWord, BiConsumer<String, String> dictionaryHandler) {
+        dictionaryHandler.accept(word, translatedWord);
     }
 }
