5f2cfe21fee3a6e37abdb4e2589830d851e77897
Alex Vishnevskiy <vishgioall@gmail.com>
2024-04-28 13:05:57 +0300

    собираем пожертвования

---
+++ src/main/java/faang/school/godbless/BJS2_6221/Donation.java
@@ -0,0 +1,11 @@
+package faang.school.godbless.BJS2_6221;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class Donation {
+    private int id;
+    private int donation;
+}
