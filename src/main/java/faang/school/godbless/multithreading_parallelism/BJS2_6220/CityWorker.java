02ce34d97415ac4d729efca37133dde7ff2768cd
Vladimir Kriukov <vladimir@kriukov.pro>
2024-04-28 12:01:31 +0300

    BJS2-6220: Путь пальцем проложи

---
+++ src/main/java/faang/school/godbless/multithreading_parallelism/BJS2_6220/CityWorker.java
@@ -0,0 +1,50 @@
+package faang.school.godbless.multithreading_parallelism.BJS2_6220;
+
+import lombok.AllArgsConstructor;
+
+import java.util.List;
+import java.util.Random;
+
+@AllArgsConstructor
+public class CityWorker implements Runnable {
+    private City city;
+    private List<Monster> monsters;
+
+    @Override
+    public void run() {
+        System.out.println("Nearest monster to city " + city.getName() + " is " + findNearestMonster().getName());
+        System.out.println("Time to kill: " + getKillTime());
+        System.out.println("Distance to the " + city.getName() + " equals " + getJourneyDistance());
+    }
+
+    public Monster findNearestMonster() {
+
+        Monster nearestMonster = null;
+        int distance = Integer.MAX_VALUE;
+
+        for (Monster monster : monsters) {
+            int newDistance = getNewDistance(city, Location.getLocationCoordinates(monster.getLocation()));
+
+            if (newDistance < distance) {
+                distance = newDistance;
+                nearestMonster = monster;
+            }
+        }
+        return nearestMonster;
+    }
+
+    private static int getNewDistance(City city, Location monsterLocation) {
+        int diffX = Math.abs(city.getLocation().getX() - monsterLocation.getX());
+        int diffY = Math.abs(city.getLocation().getY() - monsterLocation.getY());
+
+        return diffX + diffY;
+    }
+
+    public long getKillTime() {
+        return new Random().nextInt(10 - 1) + 1;
+    }
+
+    public long getJourneyDistance() {
+        return city.getDistanceToWitcher();
+    }
+}
---
+++ src/main/java/faang/school/godbless/multithreading_parallelism/BJS2_6220/Monster.java
@@ -0,0 +1,13 @@
+package faang.school.godbless.multithreading_parallelism.BJS2_6220;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+public class Monster {
+    private String name;
+    private String location;
+}
