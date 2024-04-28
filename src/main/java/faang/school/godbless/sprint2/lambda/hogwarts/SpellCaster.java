66cb87d8c7e127cec7ca47a15f548e53765ef1d6
Julia Kravchenko <js.kravchenko@gmail.com>
2024-04-28 23:51:44 +0300

    BJS2-5569: Update Hogwarts, Hogwarts, our beloved Hogwarts task based on review comments.

--- src/main/java/faang/school/godbless/sprint2/lambda/hogwarts/SpellCaster.java
+++ src/main/java/faang/school/godbless/sprint2/lambda/hogwarts/SpellCaster.java
@@ -3,7 +3,6 @@ package faang.school.godbless.sprint2.lambda.hogwarts;
 public class SpellCaster {
 
     public void cast(String spellName, SpellAction action) {
-        String result = action.use(spellName);
-        System.out.println(result);
+        System.out.println(action.use(spellName));
     }
 }
