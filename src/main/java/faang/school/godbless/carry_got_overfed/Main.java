e0b412b25721927f2b68e707cd3890fc2ce2876f
Roman Antonov <romzesvxo@mail.ru>
2024-05-05 10:59:51 +0300

    BJS2-6114 Исправил, что методы thenCombine и thenCompose идут один за другим, без создания объекта

--- src/main/java/faang/school/godbless/carry_got_overfed/Main.java
+++ src/main/java/faang/school/godbless/carry_got_overfed/Main.java
@@ -7,19 +7,16 @@ public class Main {
         CompletableFuture<Item> chestFuture = CompletableFuture.supplyAsync(() -> new Item("Меч", 10));
         CompletableFuture<Item> shopFuture = CompletableFuture.supplyAsync(() -> new Item("Модный наконечник", 5));
 
-        CompletableFuture<Item> combinedFuture = chestFuture.thenCombine(shopFuture, (item1, item2) -> {
+        chestFuture.thenCombine(shopFuture, (item1, item2) -> {
             Inventory inventory = new Inventory();
             inventory.combineItem(item1, item2);
             return inventory.getItems().get(0);
-        });
-
-        CompletableFuture<Void> addToInventoryFuture = combinedFuture.thenCompose(combinedItem ->
+        }).thenCompose(combinedItem ->
                 CompletableFuture.runAsync(() -> {
                     Inventory playerInventory = new Inventory();
                     playerInventory.addItem(combinedItem);
                     System.out.println("Добавлено модифицированное оружие " + combinedItem.getName()
                             + " в инвентарь игрока с силой " + combinedItem.getPower());
-                }));
-        addToInventoryFuture.join();
+                })).join();
     }
 }
