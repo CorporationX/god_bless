f06178ccbecbd58a0212c854cc90d35751163de2
Vladimir Kriukov <vladimir@kriukov.pro>
2024-05-10 15:26:34 +0300

    BJS2-6507: multithread game with lives

---
+++ src/main/java/faang/school/godbless/multithreading_synchronization/BJS2_6507/Player.java
@@ -0,0 +1,15 @@
+package faang.school.godbless.multithreading_synchronization.BJS2_6507;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+public class Player {
+    private String name;
+    private int score;
+    private int lives;
+    private boolean isAlive;
+}
