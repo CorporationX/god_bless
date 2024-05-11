692a78f52f88352b23696bab3aaf829bee2bf49e
Vladimir Kriukov <vladimir@kriukov.pro>
2024-05-11 13:31:45 +0300

    BJS2-6511: players pass quests in CompletableFuture.

---
+++ src/main/java/faang/school/godbless/multithreading_async/BJS2_6511/Quest.java
@@ -0,0 +1,12 @@
+package faang.school.godbless.multithreading_async.BJS2_6511;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@AllArgsConstructor
+@Getter
+public class Quest {
+    private final String name;
+    private int difficulty;
+    private int reward;
+}
