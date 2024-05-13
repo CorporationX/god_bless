6c1299d589dc5fbc3d621140652baed6fa2d6007
vlad09032 <krivonosov.2000@gmail.com>
2024-05-13 11:06:21 +0300

    correct

--- src/main/java/faang/school/godbless/wow/Main.java
+++ src/main/java/faang/school/godbless/wow/Main.java
@@ -6,16 +6,20 @@ import java.util.concurrent.CompletableFuture;
 public class Main {
     public static void main(String[] args) {
         QuestSystem questSys = new QuestSystem();
-        List<Player> players = List.of(new Player("badBoy", 1, 50), new Player("megatron", 5, 150), new Player("Prime", 10, 1000));
-        List<Quest> quests = List.of(new Quest("Find mushrooms", "easy", 15), new Quest("cooking chocolate", "easy", 10), new Quest("Kill Megalodon", "very hard", 1000000));
+        List<Player> players = List.of(new Player("badBoy", 1, 50)
+                , new Player("megaton", 5, 150), new Player("Prime", 10, 1000));
+        List<Quest> quests = List.of(new Quest("Find mushrooms", "easy", 15)
+                , new Quest("cooking chocolate", "easy", 10)
+                , new Quest("Kill Megalodon", "very hard", 1000000));
         players.forEach(player -> quests.forEach(quest -> {
-            CompletableFuture<Player> playerCompletableFuture = null;
+            CompletableFuture<Player> playerCompletableFuture;
             try {
                 playerCompletableFuture = questSys.startQuest(player, quest);
             } catch (InterruptedException e) {
                 throw new RuntimeException(e);
             }
-            playerCompletableFuture.thenAccept(playerfuture -> System.out.println("Player " + playerfuture.getName() + " completed the task " + quest.getName() + " for this the player is credited " + quest.getReward() + " points. Total points after completing this mission: " + playerfuture.getExperience()));
+            playerCompletableFuture.thenAccept(playerFuture ->
+                    System.out.printf("Player %s completed the task %s for this the player is credited %d points. Total points after completing this mission: %d%n", playerFuture.getName(), quest.getName(), quest.getReward(), playerFuture.getExperience()));
         }));
     }
 }
