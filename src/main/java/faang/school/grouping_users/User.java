bef16a5771830dd28f9a657ed84112435c553ab2
ravshanbekn <ravshanbeknabiyev.dev@gmail.com>
2024-06-03 03:59:09 +0500

    Группировка пользователей по возрасту

---
+++ src/main/java/faang/school/grouping_users/User.java
@@ -0,0 +1,31 @@
+package faang.school.grouping_users;
+
+import lombok.Data;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@Data
+public class User {
+    private String name;
+    private int age;
+    private String workingPlace;
+    private String address;
+
+    public static Map<Integer, List<User>> groupUsers(List<User> users){
+        Map<Integer, List<User>> result = new HashMap<>();
+
+        for(User user : users){
+            int age = user.age;
+            if(result.get(age) == null) {
+                result.put(age, new ArrayList<>());
+            }
+            result.get(age).add(user);
+        }
+
+        return result;
+    }
+
+}
