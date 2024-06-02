949753bb5956ce533018c76c651ebf5281ba0726
davy <vova_patrasko@mail.ru>
2024-06-03 02:11:57 +0300

    BJS-7628: Reverse array of numbers

---
+++ src/main/java/reversearray/Example.java
@@ -0,0 +1,13 @@
+package reversearray;
+
+public class Example {
+
+    public static int[] reverseArray(int[] array) {
+        for (int i = 0; i < array.length / 2; i++) {
+            int temp = array[i];
+            array[i] = array[array.length - 1 - i];
+            array[array.length - 1 - i] = temp;
+        }
+        return array;
+    }
+}
