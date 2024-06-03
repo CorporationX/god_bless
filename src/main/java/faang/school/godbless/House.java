47fde329e869e34792b2ae8b6d74783a095b1f32
Maksim Tyumentsev <experttm@gmail.com>
2024-06-03 22:14:54 +0500

    #BJS2-8088 Реализовать систему управления информацией о домах Вестероса.

---
+++ src/main/java/faang/school/godbless/House.java
@@ -0,0 +1,18 @@
+package faang.school.godbless;
+
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.ToString;
+
+/**
+ * POJO Класс "Дома Весткросса".
+ */
+@Getter
+@ToString
+@EqualsAndHashCode
+@AllArgsConstructor
+public class House {
+  private String name;
+  private String sigil;
+}
