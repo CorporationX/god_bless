ea0186f4f17ab27e63d940e8638966ab43641f38
cirill98 <100434317+cirill98@users.noreply.github.com>
2024-06-03 11:51:43 +0500

    Добавил аннотации lombok

--- src/main/java/faang/school/godbless/User.java
+++ src/main/java/faang/school/godbless/User.java
@@ -1,44 +1,24 @@
 package faang.school.godbless;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.ToString;
+
 import javax.swing.plaf.ListUI;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+@Getter
+@ToString
+@AllArgsConstructor
 public class User {
     private String name;
     private int age;
     private String workPlace;
     private String address;
 
-    public User(String name, int age, String workPlace, String address) {
-        this.name = name;
-        this.age = age;
-        this.workPlace = workPlace;
-        this.address = address;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public int getAge() {
-        return age;
-    }
-
-    public String getWorkPlace() {
-        return workPlace;
-    }
-
-    public String getAddress() {
-        return address;
-    }
-
-    @Override
-    public String toString() {
-        return name + '\'' + age + '\'' + workPlace + '\''  + address + '\'';
-    }
 
     public static Map<Integer, List<User>> groupUsers(List<User> users){
         Map<Integer, List<User>> resultGroupUsers = new HashMap<>();
