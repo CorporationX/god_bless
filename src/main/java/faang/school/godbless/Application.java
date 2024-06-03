2a9fd7422d3b8225bbc47badecf1a276d8b1ac46
cirill98 <100434317+cirill98@users.noreply.github.com>
2024-06-03 13:14:18 +0500

    Черновое решение

--- src/main/java/faang/school/godbless/Application.java
+++ src/main/java/faang/school/godbless/Application.java
@@ -1,7 +1,9 @@
 package faang.school.godbless;
 
+import java.util.Arrays;
+
 public class Application {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
     }
 }
--- src/main/java/faang/school/godbless/Example.java
+++ src/main/java/faang/school/godbless/Example.java
@@ -1,4 +1,12 @@
 package faang.school.godbless;
 
 public class Example {
+    public static int[] revers(int[] array){
+        int[] reversInt = new int[array.length];
+        int index = 0;
+        for (int i = array.length - 1; i >=0 ; i--) {
+            reversInt[index++] = array[i];
+        }
+        return reversInt;
+    }
 }
