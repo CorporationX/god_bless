7a522fba1a676a1702e21d91d82494496c96805e
Sergey Babaev <sergeybabaev@MacBook-Pro-Sergej-2.local>
2024-06-03 14:43:31 +0300

    Test commit

---
+++ src/main/java/faang/school/godbless/Task_one.java
@@ -0,0 +1,14 @@
+package faang.school.godbless;
+
+public class Task_one {
+    public static void main(String[] args) {
+        String firstname = "Sergey";
+        String lastname = "Babaev";
+        int age = 27;
+        int weight = 70;
+        int height = 179;
+
+        System.out.println(firstname + lastname + age + weight + height);
+
+    }
+}
