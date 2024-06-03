0ebbec7224c6e5b4f7d31ff46c4993bbe5397855
Stanislav Ovsyankin <cancion2able@gmail.com>
2024-06-03 10:14:11 +0500

    BJS2-7630 | Проверка на пустой массив

--- src/main/java/faang/school/godbless/reversing/Example.java
+++ src/main/java/faang/school/godbless/reversing/Example.java
@@ -1,10 +1,17 @@
 package faang.school.godbless.reversing;
 
+import lombok.extern.slf4j.Slf4j;
+
 import java.util.Arrays;
 
+@Slf4j
 public class Example {
 
     public static int[] reverse(int[] array) {
+        if (array == null || array.length == 0) {
+            log.warn("Processing denied. Array is empty.");
+            return array;
+        }
         int left = 0, right = array.length - 1;
         int[] reversedArray = Arrays.copyOf(array, array.length);
         while (left < right) {
--- src/main/java/faang/school/godbless/reversing/Main.java
+++ src/main/java/faang/school/godbless/reversing/Main.java
@@ -6,7 +6,10 @@ public class Main {
 
     public static void main(String[] args) {
         int[] array = { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+        int[] secondArray = { };
         int[] reversedArray = Example.reverse(array);
+        int[] secondReversedArray = Example.reverse(secondArray);
         System.out.println(Arrays.toString(reversedArray));
+        System.out.println(Arrays.toString(secondReversedArray));
     }
 }
