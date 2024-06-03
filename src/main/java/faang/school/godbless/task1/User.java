6cee03f3ba88bf9dc63bbc175ceddc15a7c14df8
sta1er <vlad.sokogubov@gmail.com>
2024-06-03 17:06:17 +0300

    Решение задачи "Группировка пользователей по возрасту". Создан метод который проходит по списку пользователей. В цикле мы получаем возраст каждого пользователя и смотрим не является ли он уже ключом. Далее в зависимости от результата или просто добавляем пользователя в список по его возрасту или создаем сначала новый ключ, а потом добавляем пользователя в соответствующий список.

---
+++ src/main/java/faang/school/godbless/task1/User.java
@@ -0,0 +1,31 @@
+package faang.school.godbless.task1;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.*;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+public class User {
+
+    private String name;
+    private int age;
+    private String workplace;
+    private String address;
+
+    public static Map<Integer, List<User>> groupUsers(List<User> users) {
+        Map<Integer, List<User>> groupedUsers = new HashMap<>();
+        for(User user : users) {
+            int age = user.getAge();
+            if(!groupedUsers.containsKey(age)) {
+                groupedUsers.put(age, new ArrayList<>());
+            }
+            groupedUsers.get(age).add(user);
+        }
+        return groupedUsers;
+    }
+
+}
