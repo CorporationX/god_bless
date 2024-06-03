f396eed4d908d76bcad71fa7db14b3d72f2f18c2
l0zain <nanoowood@gmail.com>
2024-06-03 09:01:14 +0300

    https://faang-school.com/courses/e63dkedm/gnzg6ebd

---
+++ src/main/java/faang/school/godbless/BJS2_7533/User.java
@@ -0,0 +1,23 @@
+package faang.school.godbless.BJS2_7533;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+@Data
+@AllArgsConstructor
+public class User {
+    private String name;
+    private int age;
+    private String placeOfWork;
+    private String address;
+
+    public static Map<Integer, List<User>> groupUsers(List<User> users) {
+        return users.stream()
+                .collect(Collectors
+                        .groupingBy(User::getAge));
+    }
+}
