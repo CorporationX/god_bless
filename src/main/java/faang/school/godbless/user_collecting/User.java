b47f4030f129a97361d7a7b553b0bd5d1e27a613
George <cradplay@gmail.com>
2024-06-02 20:15:00 -0400

    created use class, need to clarify implementation

---
+++ src/main/java/faang/school/godbless/user_collecting/User.java
@@ -0,0 +1,13 @@
+package faang.school.godbless.user_collecting;
+
+import java.util.Map;
+
+public class User {
+    private int id;
+    private String name;
+    private int age;
+    private String[] hobbies;
+
+    public static Map
+}
+
