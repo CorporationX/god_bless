e300154aa56ee49e7d632db08335620c06072ed1
Alexandra <s7125@mail.ru>
2024-06-03 16:42:25 +0500

    add method groupUsers

--- src/main/java/faang/school/godbless/User.java
+++ src/main/java/faang/school/godbless/User.java
@@ -1,8 +1,55 @@
 package faang.school.godbless;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
 public class User{
     private String name;
     private Integer year;
     private String department;
     private String adress;
+
+    public User(String name, Integer year, String department, String adress) {
+        this.name = name;
+        this.year = year;
+        this.department = department;
+        this.adress = adress;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public Integer getYear() {
+        return year;
+    }
+
+    public String getDepartment() {
+        return department;
+    }
+
+    public String getAdress() {
+        return adress;
+    }
+
+    public static HashMap<String, List<User>> groupUser(List<User> userList){
+        HashMap<String, List<User>> userMap = new HashMap<>();
+
+        userList.forEach(user -> {
+            if(user.getYear() != null){
+                if(userMap.get(user.getYear().toString()) !=null){
+                    List userNewList = userMap.get(user.getYear().toString());
+                    userNewList.add(user);
+                    userMap.put(user.getYear().toString(), userNewList);
+                }else{
+                    List userNewList = new ArrayList<>();
+                    userNewList.add(user);
+                    userMap.put(user.getYear().toString(), userNewList);
+                }
+            }
+        });
+
+        return userMap;
+    }
 }
