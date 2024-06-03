c9343faa9072908785d220da5af6228f13accb8d
s.kolbasov <s.kolbasov@bftcom.com>
2024-06-03 19:43:07 +0300

    BJS2-7815 Регистрация пользователей

--- src/main/java/faang/school/godbless/BJS2_7815/Main.java
+++ src/main/java/faang/school/godbless/BJS2_7815/Main.java
@@ -3,7 +3,7 @@ package faang.school.godbless.BJS2_7815;
 public class Main {
   public static void main(String[] args) {
     User user1 = new User("name", 19, "Google", "London");
-    User user2 = new User("name", 18, "Google", "Londo");
+    User user2 = new User("name", 18, "Google", "London");
     System.out.println(user2);
   }
 }
