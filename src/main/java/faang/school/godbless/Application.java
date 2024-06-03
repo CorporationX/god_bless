a9f2a38dae811053353d50e994a579eb449ce7fa
cirill98 <100434317+cirill98@users.noreply.github.com>
2024-06-03 15:06:20 +0500

    Использовал 3 переменные: начало, конец и промежуточную переменную

--- src/main/java/faang/school/godbless/Application.java
+++ src/main/java/faang/school/godbless/Application.java
@@ -4,6 +4,7 @@ import java.util.Arrays;
 
 public class Application {
     public static void main(String[] args) {
-
+        int[] arr = {1, 2, 3, 4, 5, 6};
+        System.out.println(Arrays.toString(Example.revers(arr)));
     }
 }
--- src/main/java/faang/school/godbless/Example.java
+++ src/main/java/faang/school/godbless/Example.java
@@ -1,12 +1,22 @@
 package faang.school.godbless;
 
+import java.util.Arrays;
+
 public class Example {
     public static int[] revers(int[] array){
-        int[] reversInt = new int[array.length];
-        int index = 0;
-        for (int i = array.length - 1; i >=0 ; i--) {
-            reversInt[index++] = array[i];
+        int start = 0;
+        int finish = array.length - 1;
+        while (start < finish){
+            int temp = array[start];
+            array[start++] = array[finish];
+            array[finish--] = temp;
         }
-        return reversInt;
+        return array;
+//        int[] reversInt = new int[array.length];
+//        int index = 0;
+//        for (int i = array.length - 1; i >=0 ; i--) {
+//            reversInt[index++] = array[i];
+//        }
+//        return reversInt;
     }
 }
