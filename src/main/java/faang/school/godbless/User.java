5e06b0cb49c2f60493caaae52837c9b2bb163ded
Rauan Kaztaev <rtishtyk@gmail.com>
2024-06-03 19:41:07 +0500

    Группировка пользователей по возрасту

---
+++ src/main/java/faang/school/godbless/User.java
@@ -0,0 +1,30 @@
+package faang.school.godbless;
+
+import lombok.Getter;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@Getter
+public class User {
+    private String name;
+    private int age;
+    private String work;
+    private String address;
+
+    public static Map<Integer, List<User>> groupUsers(List<User> users) {
+        Map<Integer, List<User>> result = new HashMap<>();
+
+        for (User user : users) {
+            if (!result.containsKey(user.age)) {
+                result.put(user.getAge(), new ArrayList<>());
+                result.get(user.getAge()).add(user);
+            } else {
+                result.get(user.getAge()).add(user);
+            }
+        }
+        return result;
+    }
+}
