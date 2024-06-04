50073649877867fe72efd8257e42e189a60715d5
cirill98 <100434317+cirill98@users.noreply.github.com>
2024-06-04 11:17:01 +0500

    Сделал 4 метода

--- src/main/java/faang/school/godbless/Application.java
+++ src/main/java/faang/school/godbless/Application.java
@@ -1,7 +1,41 @@
 package faang.school.godbless;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class Application {
+    static Map<String, House> houseAll = new HashMap<>();
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        House stark = new House("Старк", "Волк");
+        House lannister = new House("Ланистер", "Лев");
+        House barateon = new House("Баратеон", "Олень");
+
+        addHouse(stark);
+        addHouse(lannister);
+        addHouse(barateon);
+        System.out.println(houseAll);
+        searchSigil("Баратеон");
+        listAllHouseAndSigil();
     }
+
+//    добавление нового дома
+    static void addHouse(House newHouse){
+        houseAll.put(newHouse.getName(), newHouse);
+    }
+
+//    удаление дома по его названию;
+    static void removeHouse(String nameHouse){
+        houseAll.remove(nameHouse);
+    }
+
+//    поиск дома и вывод информации о гербе дома по его названию;
+    static void searchSigil(String nameHouse){
+        System.out.println(houseAll.get(nameHouse).getSigil());
+    }
+
+//    вывод списка всех домов и их гербов
+    static void listAllHouseAndSigil(){
+        System.out.println(houseAll.values());;
+    }
+
 }
