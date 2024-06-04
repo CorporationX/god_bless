6f7d5f701f3bfaccace2885e06af698fad361392
npopov_ <lkn_popov@mail.ru>
2024-06-04 14:49:40 +0300

    BJS2-7751 добавлено решение задачи

---
+++ src/main/java/faang/school/godbless/Character.java
@@ -0,0 +1,38 @@
+package faang.school.godbless;
+
+public abstract class Character {
+    protected String name;
+    protected int power;
+    protected int agility;
+    protected int intellect;
+    protected int health = 100;
+
+    public Character(String name) {
+        this.name = name;
+    }
+
+    public Character(String name, int power, int agility, int intellect) {
+        this.name = name;
+        this.power = power;
+        this.agility = agility;
+        this.intellect = intellect;
+    }
+
+    public int getHealth() {
+        return health;
+    }
+
+    public void getDamage(int healthDamage) {
+        this.health -= healthDamage;
+
+        if (!isAlive()) {
+            System.out.println("Персонаж " + name + " мертв.");
+        }
+    }
+
+    public boolean isAlive() {
+        return this.health > 0;
+    }
+
+    public abstract void attack(Character character);
+}
