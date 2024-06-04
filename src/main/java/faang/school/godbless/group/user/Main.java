e42010eb19f1acc1399779fb8d9b304b6967cf30
Enemy095 <salam.tahaev@mail.ru>
2024-06-04 20:28:24 +0300

    перенес методы в класс User, сократил код

--- src/main/java/faang/school/godbless/group/user/Main.java
+++ src/main/java/faang/school/godbless/group/user/Main.java
@@ -2,9 +2,7 @@ package faang.school.godbless.group.user;
 
 import lombok.NonNull;
 
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
 @NonNull
 public class Main {
@@ -14,27 +12,7 @@ public class Main {
         User userDiana = new User("Diana", 33, "BBC", "London");
         User userBob = new User("Bob", 22, "Sony", "London");
         List<User> listOfUser = List.of(userJoe, userDiana, userAlex, userBob);
-        groupUsers(listOfUser);
-        showUsersGroupedByAge(listOfUser);
-    }
-
-    public static Map<Integer, List<User>> groupUsers(List<User> users) {
-        Map<Integer, List<User>> usersByAge = new HashMap<>();
-        for (User user : users) {
-            int keyAge = user.getAge();
-            if (usersByAge.containsKey(keyAge)) {
-                usersByAge.get(keyAge).add(user);
-            } else {
-                List<User> listByKey = List.of(user);
-                usersByAge.put(keyAge, listByKey);
-            }
-        }
-        return usersByAge;
-    }
-
-    public static void showUsersGroupedByAge(List<User> users) {
-        for (Map.Entry<Integer, List<User>> user : groupUsers(users).entrySet()) {
-            System.out.println(user);
-        }
+        User.groupUsers(listOfUser);
+        User.showUsersGroupedByAge(listOfUser);
     }
 }
--- src/main/java/faang/school/godbless/group/user/User.java
+++ src/main/java/faang/school/godbless/group/user/User.java
@@ -4,19 +4,36 @@ import lombok.AllArgsConstructor;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.NonNull;
-import lombok.Setter;
 import lombok.ToString;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 @NonNull
-@AllArgsConstructor
-@Setter
-@Getter
 @ToString
+@Getter
 @EqualsAndHashCode
+@AllArgsConstructor
 public class User {
     private String name;
     private int age;
     private String workPlace;
     private String address;
 
+    public static Map<Integer, List<User>> groupUsers(List<User> users) {
+        Map<Integer, List<User>> usersByAge = new HashMap<>();
+        for (User user : users) {
+            int keyAge = user.getAge();
+            usersByAge.computeIfAbsent(keyAge, listBayKey -> new ArrayList<>()).add(user);
+        }
+        return usersByAge;
+    }
+
+    public static void showUsersGroupedByAge(List<User> users) {
+        for (Map.Entry<Integer, List<User>> user : groupUsers(users).entrySet()) {
+            System.out.println(user);
+        }
+    }
 }
