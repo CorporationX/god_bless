89fbb41089cab6425abf5b967ac076c3f108088b
Kilotas <Alendos87@gmail.com>
2024-06-04 13:13:13 +0500

    Solution for grouping users by ages

--- src/main/java/faang/school/godbless/Application.java
+++ src/main/java/faang/school/godbless/Application.java
@@ -1,16 +1,22 @@
 package faang.school.godbless;
-
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 import static faang.school.godbless.Example.reverse;
 
 public class Application {
     public static void main(String[] args) {
-        int nums[] = {1, 4, 6, 7};
-        int resultNums[];
+        List<User> users = new ArrayList<>();
+        users.add(new User("Draco", 15, "Wizard", "Hogwarts"));
+        users.add(new User("Snape", 35, "Wizard", "Diagon Alley"));
 
-        resultNums = reverse(nums);
-        System.out.print("Массив: ");
-        for(int i = 0; i < resultNums.length; i++){
-            System.out.print(resultNums[i] + " ");
-        }
+
+        Map<Integer, List<User>> groupedUsers = User.groupUsers(users);
+
+        groupedUsers.forEach((age, userList) -> {
+            System.out.println("Возраст: " + age);
+            userList.forEach(user -> System.out.println("  " + user.getUsername() + " " + user.getAge() + " " + user.getAddress()));
+        });
     }
 }
