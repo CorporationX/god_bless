856e0834fa735f8cbc2c1324a361bc0049ee4520
Danill Klinkov <klinkov.danya@bk.ru>
2024-06-04 19:20:50 +0300

    Changed class to record, remove lombok

--- src/main/java/faang/school/godbless/task1/User.java
+++ src/main/java/faang/school/godbless/task1/User.java
@@ -1,31 +1,19 @@
 package faang.school.godbless.task1;
 
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
-
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-@Data
-@NoArgsConstructor
-@AllArgsConstructor
-public class User {
-    private String name;
-    private int age;
-    private String workplace;
-    private String address;
-
+record User(String name, int age, String workplace, String address) {
     public static Map<Integer, List<User>> groupUsers(List<User> users) {
         Map<Integer, List<User>> groups = new HashMap<>();
         for (User user : users) {
-            if (!groups.containsKey(user.getAge())) {
-                groups.put(user.getAge(), new ArrayList<>());
-                groups.get(user.getAge()).add(user);
+            if (!groups.containsKey(user.age())) {
+                groups.put(user.age(), new ArrayList<>());
+                groups.get(user.age()).add(user);
             } else {
-                groups.get(user.getAge()).add(user);
+                groups.get(user.age()).add(user);
             }
         }
         return groups;
