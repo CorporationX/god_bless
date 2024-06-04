17ee600b63e599e6be7a9ae4c33084f01a9036b7
Kilotas <Alendos87@gmail.com>
2024-06-04 14:07:23 +0500

    Solution for grouping users by ages

--- src/main/java/faang/school/godbless/Example.java
+++ src/main/java/faang/school/godbless/Example.java
@@ -2,13 +2,5 @@ package faang.school.godbless;
 
 public class Example {
 
-    public static int[] reverse(int nums[]){
-        int numsReverse [] = new int[nums.length];
-        int k = 0;
-        for(int i = (nums.length - 1) ; i > 0; i--){
-            numsReverse[k] = nums[i];
-            k = k+1;
-        }
-        return numsReverse;
-    }
+
 }
