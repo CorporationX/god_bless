6ab2f029677066c25278a8524fd8524dcd0ce65a
pavlick2745 <Pa4aPV@yandex.ru>
2024-06-04 20:16:14 +0700

    добавил валидацию на создание юзера

--- src/main/java/faang/school/godbless/User.java
+++ src/main/java/faang/school/godbless/User.java
@@ -1,24 +1,49 @@
 package faang.school.godbless;
 
-import lombok.AllArgsConstructor;
-import lombok.Builder;
 import lombok.Data;
-import lombok.NoArgsConstructor;
 
+import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
+import java.util.Set;
 
 @Data
-@AllArgsConstructor
-@NoArgsConstructor
-@Builder
 public class User {
 
     private String name;
     private Integer age;
-    private String workPlace;
+    private String job;
     private String address;
+
+    private static final Set<String> VALID_JOBS = new HashSet<>(List.of("Google", "Uber", "Amazon"));
+    private static final Set<String> VALID_ADDRESSES = new HashSet<>(List.of("London", "New York", "Amsterdam"));
+
+    public User(String name, Integer age, String job, String address) {
+        if (!this.validate(name, age, job, address)) {
+            throw new IllegalArgumentException();
+        }
+        this.name = name;
+        this.age = age;
+        this.job = job;
+        this.address = address;
+    }
+
+    private boolean validate(String name, Integer age, String job, String address) {
+        if (name== null || name.isEmpty()) {
+            return false;
+        }
+        if (age< 18) {
+            return false;
+        }
+        if (!VALID_ADDRESSES.contains(address)) {
+            return false;
+        }
+        if (!VALID_JOBS.contains(job)) {
+            return false;
+        }
+        return true;
+    }
 
     public static Map<Integer, List<User>> groupUsers(List<User> users) {
         return users.stream().collect(Collectors.groupingBy(User::getAge));
--- src/test/java/faang/school/godbless/UserTest.java
+++ src/test/java/faang/school/godbless/UserTest.java
@@ -1,6 +1,8 @@
 package faang.school.godbless;
 
-import org.assertj.core.api.Assertions;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
 import org.junit.jupiter.api.Test;
 
 import java.util.List;
@@ -8,22 +10,45 @@ import java.util.Map;
 
 class UserTest {
 
-    private static Integer USER_AGE_18 = 18;
+    private static final Integer USER_AGE_18 = 18;
 
     @Test
     void groupUsers() {
+        final Integer userAge21 = 21;
         User user1 = getUser("user1", USER_AGE_18);
         User user2 = getUser("user2", USER_AGE_18);
-        User user3 = getUser("user3", 12);
+        User user3 = getUser("user3", userAge21);
 
         Map<Integer, List<User>> actual = User.groupUsers(List.of(user1, user2, user3));
 
-        Assertions.assertThat(actual.size()).isEqualTo(2);
-        Assertions.assertThat(actual.get(12)).contains(user3);
-        Assertions.assertThat(actual.get(18)).contains(user1, user2);
+        assertThat(actual.size()).isEqualTo(2);
+        assertThat(actual.get(userAge21)).contains(user3);
+        assertThat(actual.get(USER_AGE_18)).contains(user1, user2);
+    }
+
+    @Test
+    void throwIllegalArgumentExceptionIfMinorAge() {
+        final Integer userAgeMinor = 10;
+        assertThrows(IllegalArgumentException.class, () -> getUser("user1", userAgeMinor));
+    }
+
+    @Test
+    void throwIllegalArgumentExceptionIfEmptyName() {
+        assertThrows(IllegalArgumentException.class, () -> getUser(null, USER_AGE_18));
+        assertThrows(IllegalArgumentException.class, () -> getUser("", USER_AGE_18));
+    }
+
+    @Test
+    void throwIllegalArgumentExceptionIfNotContainJob() {
+        assertThrows(IllegalArgumentException.class, () -> new User("user1", USER_AGE_18, "fakeJob", "London"));
+    }
+
+    @Test
+    void throwIllegalArgumentExceptionIfNotContainAddress() {
+        assertThrows(IllegalArgumentException.class, () -> new User("user1", USER_AGE_18, "Google", "fakeAddress"));
     }
 
     private static User getUser(String name, Integer age) {
-        return new User(name, age, "komiac", "Новокузнецк");
+        return new User(name, age, "Google", "London");
     }
 }
