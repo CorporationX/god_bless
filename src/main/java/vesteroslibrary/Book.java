9af55075bd9be0fb416aa0ccdad9fed6a1711bb9
davy <vova_patrasko@mail.ru>
2024-06-04 21:33:31 +0300

    BJS2-8193: Fix bugs

--- src/main/java/vesteroslibrary/Book.java
+++ src/main/java/vesteroslibrary/Book.java
@@ -11,22 +11,6 @@ import java.util.Objects;
 @AllArgsConstructor
 public record Book(String title, String author, String year) {
 
-    public static void addBook(Map<Book, String> books, Book book, String location) {
-        books.put(book, location);
-    }
-
-    public static void remove(Map<Book, String> books, String title, String author, String year) {
-        books.remove(new Book(title, author, year));
-    }
-
-    public static String getBookLocation(Map<Book, String> books, String title, String author, String year) {
-        return books.get(new Book(title, author, year));
-    }
-
-    public static Map<Book, String> getAllBooks(Map<Book, String> books) {
-        return books;
-    }
-
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
