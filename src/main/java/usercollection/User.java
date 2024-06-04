abe4ba02f215a738c835fc69a13ca2e74ec75eeb
davy <vova_patrasko@mail.ru>
2024-06-04 17:01:50 +0300

    BJS2-7144: User collection

---
+++ src/main/java/usercollection/User.java
@@ -0,0 +1,34 @@
+package usercollection;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+import java.util.*;
+
+@Data
+@AllArgsConstructor
+public class User {
+
+    private int id;
+    private String name;
+    private int age;
+    private Set<String> activities;
+
+    public static Map<User, String> findHobbyLovers(List<User> users, Set<String> commonSetOfActivities) {
+        Map<User, String> usersWithActivities = new HashMap<>();
+//        users.stream()
+//                .map(user -> {
+//                    String foundActivity = user.getActivities().stream()
+//                            .filter(commonSetOfActivities::contains)
+//                            .findFirst().orElse(null);
+//                }).
+        for (User user : users) {
+            for (String activity : commonSetOfActivities) {
+                if (user.getActivities().contains(activity)) {
+                    usersWithActivities.put(user, activity);
+                }
+            }
+        }
+        return usersWithActivities;
+    }
+}
