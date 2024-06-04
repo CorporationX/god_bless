ba61dddadf5ac8f588002991548010a903c1535a
Danill Klinkov <klinkov.danya@bk.ru>
2024-06-04 21:00:52 +0300

    Task_2 done

---
+++ src/main/java/faang/school/godbless/task2/Main.java
@@ -0,0 +1,12 @@
+package faang.school.godbless.task2;
+
+public class Main {
+    public static void main(String[] args) {
+        User mihail = new User("Mihail", 21, "Amazon", "New York");
+        User daniil = new User("Daniil", 23, "Google", "NN");
+        User ivan = new User("Ivan", 43, "Vk", "London");
+        User slava = new User("Slava", 26, "Uber", "Amsterdam");
+        User igor = new User("Igor", 26, "Google", "Amsterdam");
+
+    }
+}
---
+++ src/main/java/faang/school/godbless/task2/User.java
@@ -0,0 +1,28 @@
+package faang.school.godbless.task2;
+
+import lombok.Getter;
+
+import java.util.Set;
+
+@Getter
+public class User {
+    private static final Set<String> VALID_JOBS = Set.of("Google", "Uber", "Amazon");
+    private static final Set<String> VALID_ADDRESSES = Set.of("London", "New York", "Amsterdam");
+    private static final int MIN_AGE = 18;
+    private String name;
+    private int age;
+    private String workplace;
+    private String address;
+
+    public User(String name, int age, String workplace, String address) {
+        if (name.isEmpty() || age < MIN_AGE ||
+                !VALID_JOBS.contains(workplace) || !VALID_ADDRESSES.contains(address)) {
+            throw new IllegalArgumentException();
+        } else {
+            this.name = name;
+            this.age = age;
+            this.workplace = workplace;
+            this.address = address;
+        }
+    }
+}
