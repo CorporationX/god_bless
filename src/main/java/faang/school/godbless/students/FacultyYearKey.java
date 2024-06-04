fe33b82b52c9486b0329518f2eb59cb468e616c8
Stanislav Ovsyankin <cancion2able@gmail.com>
2024-06-04 21:51:37 +0500

    BSJ2-8145 | Удаление ненужного вызова метода

--- src/main/java/faang/school/godbless/students/FacultyYearKey.java
+++ src/main/java/faang/school/godbless/students/FacultyYearKey.java
@@ -5,7 +5,7 @@ import java.util.Objects;
 public record FacultyYearKey(String faculty, int year) {
 
     public FacultyYearKey(String faculty, int year) {
-        this.faculty = Objects.requireNonNull(faculty.trim());
+        this.faculty = Objects.requireNonNull(faculty);
         this.year = year;
     }
 }
