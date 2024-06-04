c43f834963d6e91a7968dfbee4b476b09d389573
Grisha Tkach <grisatkac@mail.ru>
2024-06-04 11:50:19 +0300

    update list of integers to array of integers

--- src/main/java/faang/school/godbless/reverse/Example.java
+++ src/main/java/faang/school/godbless/reverse/Example.java
@@ -2,18 +2,17 @@ package faang.school.godbless.reverse;
 
 import lombok.NoArgsConstructor;
 
-import java.util.List;
 
 @NoArgsConstructor
 public class Example {
 
-    public void reverse(List<Integer> integerList) {
+    public void reverse(Integer[] integerList) {
         if (integerList != null) {
-            int size = integerList.size();
+            int size = integerList.length;
             for (int i = 0; i < size / 2; i++) {
-                Integer value = integerList.get(i);
-                integerList.set(i, integerList.get(size - 1 - i));
-                integerList.set(size - 1 - i, value);
+                Integer value = integerList[i];
+                integerList[i] = integerList[size - 1 - i];
+                integerList[size - 1 - i] = value;
             }
         }
     }
--- src/main/java/faang/school/godbless/reverse/Main.java
+++ src/main/java/faang/school/godbless/reverse/Main.java
@@ -1,15 +1,15 @@
 package faang.school.godbless.reverse;
 
 import java.util.Arrays;
-import java.util.List;
 
 public class Main {
 
     public static void main(String[] args) {
         Example example = new Example();
-        List<Integer> integerListForReversing = Arrays.asList(1, 2, 3, 4, 5);
-        example.reverse(integerListForReversing);
+        Integer[] integerArrayForReversing = {1, 2, 3, 4, 5};
 
-        System.out.println(integerListForReversing);
+        example.reverse(integerArrayForReversing);
+
+        System.out.println(Arrays.toString(integerArrayForReversing));
     }
 }
