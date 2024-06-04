a6fff2cc777dca6c46342968b28c77c852102986
Nikita2768 <vechkinnikita22@gmail.com>
2024-06-04 06:33:19 +0500

    Начало работы над проектом

---
+++ src/main/java/faang/school/godbless/Character.java
@@ -0,0 +1,31 @@
+package faang.school.godbless;
+
+public abstract class Character {
+    protected String name;
+    protected int force, dexterity, intelest;
+    protected int health = 100;
+
+    public Character(String name, int force, int dexterity, int intelest) {
+        this.name = name;
+        this.force = force;
+        this.dexterity = dexterity;
+        this.intelest = intelest;
+    }
+
+    public Character(String name) {
+        this.name = name;
+    }
+
+    public abstract void attack(Character character);
+
+    @Override
+    public String toString() {
+        return "Character{" +
+                "name='" + name + '\'' +
+                ", force=" + force +
+                ", dexterity=" + dexterity +
+                ", intelest=" + intelest +
+                ", health=" + health +
+                '}';
+    }
+}
