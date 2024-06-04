ed4654ba9b401ce19adff9e10ac111609c4d21cf
pavlick2745 <Pa4aPV@yandex.ru>
2024-06-04 16:05:45 +0700

    заменил реализацию на groupingBy + отредактировал имена полей

--- src/main/java/faang/school/godbless/User.java
+++ src/main/java/faang/school/godbless/User.java
@@ -5,7 +5,6 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
@@ -18,14 +17,10 @@ public class User {
 
     private String name;
     private Integer age;
-    private String placeWork;
+    private String workPlace;
     private String address;
 
     public static Map<Integer, List<User>> groupUsers(List<User> users) {
-        return users.stream()
-                .collect(Collectors.toMap(it -> it.age, it -> new ArrayList<>(List.of(it)), ((objects, objects2) -> {
-                    objects.addAll(objects2);
-                    return objects;
-                })));
+        return users.stream().collect(Collectors.groupingBy(User::getAge));
     }
 }
