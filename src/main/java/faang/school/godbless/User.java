676b7c84698f0e2226267a601fb6e9fb42df330e
Иван Шилов <ivshvl39@gmail.com>
2024-06-04 09:12:33 +0300

    Регистрация пользователей

--- src/main/java/faang/school/godbless/User.java
+++ src/main/java/faang/school/godbless/User.java
@@ -2,8 +2,7 @@ package faang.school.godbless;
 
 import lombok.Data;
 
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 import java.util.stream.Collectors;
 @Data
 public class User {
@@ -11,9 +10,20 @@ public class User {
     private int age;
     private String workAddress;
     private String address;
+    public static final Set<String> VALID_JOBS=new HashSet<>(Arrays.asList("Google", "Uber", "Amazon"));
+    public static final Set<String> VALID_ADDRESSES=new HashSet<>(Arrays.asList("London", "New York", "Amsterdam"));
 
     public static Map<Integer, List<User>> groupUsers(List<User> users){
         return users.stream().collect(Collectors.groupingBy(User::getAge));
+    }
+
+    public User(String name, int age, String workAddress, String address) {
+        if(name!=null && !name.isBlank() && age>=18 && VALID_JOBS.contains(workAddress) && VALID_ADDRESSES.contains(address)){
+            this.name = name;
+            this.age = age;
+            this.workAddress = workAddress;
+            this.address = address;
+        } else throw new IllegalArgumentException("Ошибка заполнения полей :)");
     }
 }
 
