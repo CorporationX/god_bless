6a385fc4b61e2529dbc000e2e77abf0499a9afb9
cirill98 <100434317+cirill98@users.noreply.github.com>
2024-06-04 20:46:11 +0500

    Черновик

---
+++ src/main/java/faang/school/godbless/Main.java
@@ -0,0 +1,20 @@
+package faang.school.godbless;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class Main {
+    private static Map<Integer, StreamEvent> mapIdEvent = new HashMap<>();
+    private static Map<String, List<StreamEvent>> mapListEvent = new HashMap<>();
+
+    public static void main(String[] args) {
+
+    }
+    static void addEventStream(StreamEvent streamEvent){
+
+    }
+    static int updateId(){
+
+    }
+}
---
+++ src/main/java/faang/school/godbless/StreamEvent.java
@@ -0,0 +1,12 @@
+package faang.school.godbless;
+
+import lombok.AllArgsConstructor;
+
+@AllArgsConstructor
+public class StreamEvent {
+    private long id;
+    private String eventType;
+    private String data;
+
+
+}
