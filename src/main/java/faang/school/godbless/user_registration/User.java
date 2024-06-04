4629454e918c31e9331d9c8a89e85c0788bf82c3
ravshanbekn <ravshanbeknabiyev.dev@gmail.com>
2024-06-04 13:41:00 +0500

    Регистрация пользователей Updated

--- src/main/java/faang/school/godbless/user_registration/User.java
+++ src/main/java/faang/school/godbless/user_registration/User.java
@@ -10,18 +10,20 @@ import java.util.Set;
 @Setter
 @EqualsAndHashCode
 public class User {
+    public static final Set<String> VALID_JOBS = Set.of("Google", "Uber", "Amazon");
+    public static final Set<String> VALID_ADDRESSES = Set.of("London", "New York", "Amsterdam");
+    public static final int MINIMUM_AGE = 18;
+
     private String name;
     private int age;
     private String job;
     private String address;
-    public static Set<String> VALID_JOBS = Set.of("Google", "Uber", "Amazon");
-    public static Set<String> VALID_ADDRESSES = Set.of("London", "New York", "Amsterdam");
 
     public User(String name, int age, String job, String address) {
-        if(name == null ){
+        if(name == null || name.isBlank()){
             throw new IllegalArgumentException("Имя не может быть пустым");
-        } else if(age < 18) {
-            throw new IllegalArgumentException("Возраст не должен быть меньше 18");
+        } else if(age < MINIMUM_AGE) {
+            throw new IllegalArgumentException("Возраст не должен быть меньше " + MINIMUM_AGE);
         } else if(!VALID_JOBS.contains(job)){
             throw new IllegalArgumentException("Не валидная работа");
         } else if(!VALID_ADDRESSES.contains(address)){
