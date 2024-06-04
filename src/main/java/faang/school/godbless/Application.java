086bb4b17c5a1e7070f421b430aa7964a720fc70
Max.Shundrik <mas9l175@gmail.com>
2024-06-05 01:04:02 +0300

    Implemented validation of Users Data

--- src/main/java/faang/school/godbless/Application.java
+++ src/main/java/faang/school/godbless/Application.java
@@ -1,27 +1,48 @@
 package faang.school.godbless;
 
+import lombok.extern.slf4j.Slf4j;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 
+@Slf4j
+
 public class Application {
     public static void main(String[] args) {
         List<User> users = new ArrayList<>();
-        User user1 = new User("Vlad", 17,"McDonald's","Rostov, Lenina street");
-        User user2 = new User("Gleb", 22,"Ozon","Saint Petersburg");
-        User user3 = new User("Zlata", 32,"Yandex","Voronezh");
-        User user4 = new User("Varvara", 17,"LV Store consultant","Moscow");
-        User user5 = new User("Nikolas", 24,"Suhoi Engineer","Illinois");
-        User user6 = new User("Mathew", 22,"Suhoi","Chicago");
-        User user7 = new User("Jennifer", 17,"Pizza Promoter","New York");
-        User user8 = new User("Andrew", 30,"T1 Java developer","Moscow");
-        User user9 = new User("Alice", 35,"Sber Lead Java developer","Novosibirsk");
-        User user10 = new User("Jane", 35,"Bethesda","Melbourne");
-        User user11 = new User("Merry", 24,"FlowWow","Rostov");
-        User user12 = new User("Lisa", 30,"CarMain's Mechanic","Ekaterinburg");
-        User user13 = new User("Arnold", 27,"Sber Security","Moscow");
-        User user14 = new User("Gwen", 17,"McDonald's","Odessa");
-        User user15 = new User("Michael", 35,"McDonald's","Ivanovo");
+
+        try {
+//            User user1 = new User("Vlad", 17,"McDonald's","Rostov, Lenina street");
+//            User user2 = new User("Gleb", 22,"Ozon","Saint Petersburg");
+//            User user3 = new User("Zlata", 32,"Yandex","Voronezh");
+//            User user4 = new User("Varvara", 17,"LV Store consultant","Moscow");
+//            User user5 = new User("Nikolas", 24,"Suhoi Engineer","Illinois");
+//            User user6 = new User("Mathew", 22,"Suhoi","Chicago");
+//            User user7 = new User("Jennifer", 17,"Pizza Promoter","New York");
+//            User user8 = new User("Andrew", 30,"T1 Java developer","Moscow");
+//            User user9 = new User("Alice", 35,"Sber Lead Java developer","Novosibirsk");
+//            User user10 = new User("Jane", 35,"Bethesda","Melbourne");
+//            User user11 = new User("Merry", 24,"FlowWow","Rostov");
+//            User user12 = new User("Lisa", 30,"CarMain's Mechanic","Ekaterinburg");
+//            User user13 = new User("Arnold", 27,"Sber Security","Moscow");
+//            User user14 = new User("Gwen", 17,"McDonald's","Odessa");
+//            User user15 = new User("Michael", 35,"McDonald's","Ivanovo");
+            User user1 = new User("Vlad", 18,"Google","London");
+            User user2 = new User("Gleb", 22,"Amazon","New York");
+            User user3 = new User("Zlata", 32,"Uber","Amsterdam");
+            User user4 = new User("Varvara", 18,"Google","London");
+            User user5 = new User("Nikolas", 24,"Uber","Amsterdam");
+            User user6 = new User("Mathew", 22,"Google","London");
+            User user7 = new User("Jennifer", 18,"Uber","Amsterdam");
+            User user8 = new User("Andrew", 30,"Google","New York");
+            User user9 = new User("Alice", 35,"Uber","Amsterdam");
+            User user10 = new User("Jane", 35,"Amazon","New York");
+            User user11 = new User("Merry", 24,"Amazon","New York");
+            User user12 = new User("Lisa", 30,"Google","London");
+            User user13 = new User("Arnold", 27,"Amazon","London");
+            User user14 = new User("Gwen", 18,"Amazon","New York");
+            User user15 = new User("Michael", 35,"Uber","London");
+
         users.add(user1);
         users.add(user2);
         users.add(user3);
@@ -39,5 +60,8 @@ public class Application {
         users.add(user15);
         Map<Integer,List<User>> groupedUsers = User.groupUsers(users);
         System.out.println(groupedUsers);
+        } catch (IllegalArgumentException e) {
+            log.warn("Wrong initialization of a User" + e);
+        }
     }
 }
--- src/main/java/faang/school/godbless/User.java
+++ src/main/java/faang/school/godbless/User.java
@@ -1,20 +1,27 @@
 package faang.school.godbless;
 
-import lombok.AllArgsConstructor;
 import lombok.Data;
-
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 @Data
-@AllArgsConstructor
+//@AllArgsConstructor
 public class User {
     private String name;
     private int age;
     private String workPlace;
     private String address;
+    private static final Set <String> VALID_JOBS = new HashSet(Arrays.asList("Google","Uber","Amazon"));
+    private static final Set <String> VALID_ADDRESSES = new HashSet(Arrays.asList("London","New York","Amsterdam"));
+
+    public User(String name, int age, String workPlace, String address) throws IllegalArgumentException {
+        if (name!=null && age>=18 && VALID_JOBS.contains(workPlace) && VALID_ADDRESSES.contains(address)) {
+            this.name = name;
+            this.age = age;
+            this.workPlace = workPlace;
+            this.address = address;
+        }
+        else throw new IllegalArgumentException();
+    }
 
     public static Map<Integer, List<User>> groupUsers (List<User> users){
         Map<Integer, List<User>> groupedUsersList = new HashMap<>();
