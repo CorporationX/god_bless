b60a0dc71706f83b4ad01e0461770b19a450e695
Alexandra <s7125@mail.ru>
2024-06-04 21:45:27 +0500

    BJS2-7816 add validation in class User

--- src/main/java/faang/school/godbless/User.java
+++ src/main/java/faang/school/godbless/User.java
@@ -1,16 +1,23 @@
 package faang.school.godbless;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
+import java.util.*;
 
 public class User{
+
+    public static final Set VALID_JOBS = new HashSet<>(Arrays.asList("Google", "Uber", "Amazon"));
+    public static final Set VALID_ADDRESSES = new HashSet<>(Arrays.asList("London", "New York”", "Amsterdam"));
+
     private String name;
     private Integer year;
     private String department;
     private String adress;
 
     public User(String name, Integer year, String department, String adress) {
+        if(name == null || year < 18
+                || !VALID_JOBS.contains(department)
+                || !VALID_ADDRESSES.contains(adress)){
+            throw new IllegalArgumentException("not valid");
+        }
         this.name = name;
         this.year = year;
         this.department = department;
