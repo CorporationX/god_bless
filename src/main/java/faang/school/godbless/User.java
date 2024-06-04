3dd88236e93ce97526eef82b2c8e4e86d4944000
Nikita Tolstov <nikita.tolstv@gmail.com>
2024-06-04 12:34:07 +0300

    fixed grouping users by age

--- src/main/java/faang/school/godbless/User.java
+++ src/main/java/faang/school/godbless/User.java
@@ -8,25 +8,23 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+@Getter
+@Setter
 public class User {
 
-    @Getter
-    @Setter
     private String name;
 
-    @Getter
-    @Setter
     private int age;
 
     private String placeOfWork;
 
-    private String adress;
+    private String address;
 
     public static Map<Integer, List<User>> groupUsers(List<User> users) {
         Map<Integer, List<User>> mapUser = new HashMap<>();
 
         for (User user : users) {
-            mapUser.put(user.getAge(), users);
+            mapUser.putIfAbsent(user.getAge(), List.of(user));
         }
 
         return mapUser;
