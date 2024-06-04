56cea48f573cbbbc6d673b28a3779f091c534aac
Nikita Tolstov <nikita.tolstv@gmail.com>
2024-06-04 14:14:32 +0300

    User registration

--- src/main/java/faang/school/godbless/TestUser.java
+++ src/main/java/faang/school/godbless/TestUser.java
@@ -6,9 +6,9 @@ import java.util.List;
 public class TestUser {
 
     public static void main(String[] args) {
-        User user1 = new User("Alex", 15, "Google", "A");
-        User user2 = new User("Leha", 15, "Google", "A");
-        User user3 = new User("Vlad", 18, "Google", "A");
+        User user1 = new User("Gosha", 19, "Amazon", "London");
+        User user2 = new User("Leha", 33, "Google", "New York");
+        User user3 = new User("Vlad", 18, "Uber", "Amsterdam");
         List<User> testList = List.of(user1, user2, user3);
         System.out.println(User.groupUsers(testList));
     }
