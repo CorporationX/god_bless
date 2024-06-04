aeb45c924b9ef85121b38eb1806918c7fb563771
Enemy095 <salam.tahaev@mail.ru>
2024-06-04 22:43:21 +0300

    перенес методы в класс User, сократил код

--- src/main/java/faang/school/godbless/reverse/Example.java
+++ src/main/java/faang/school/godbless/reverse/Example.java
@@ -2,17 +2,35 @@ package faang.school.godbless.reverse;
 
 import lombok.ToString;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @ToString
 public class Example {
-    public static void reverse(int[] array) {
-        int arraySize = array.length;
-        int[] reverseArrays = new int[arraySize];
-        for (int number : array) {
-            arraySize = arraySize - 1;
-            reverseArrays[arraySize] = number;
+    public static int[] reverse(int[] array) {
+        int start = 0;
+        int end = array.length-1;
+        int buf = 0;
+        while (start < end){
+            array[start] = array[end];
+            array[end] = buf;
+            buf = array[end];
+            int aa = 1;
+            int bb = 2;
+            int cc = aa+bb;
+            aa = cc;
+            bb = aa;
+            cc = ;
+
+
+
         }
-        for (int numbers : reverseArrays) {
-            System.out.print(numbers + ", ");
+        return array;
+    }
+
+    public static void showAnArray(int[] array) {
+        for (int number : array) {
+            System.out.print(number + ", ");
         }
     }
 }
--- src/main/java/faang/school/godbless/reverse/Main.java
+++ src/main/java/faang/school/godbless/reverse/Main.java
@@ -2,8 +2,8 @@ package faang.school.godbless.reverse;
 
 public class Main {
     public static void main(String[] args) {
-        int[] arrays = {1, 35, 453, 3, 35, 66, 5};
+        int[] arrays = {1, 35, 453, 3, 344, 66, 5};
         Example.reverse(arrays);
-
+        Example.showAnArray(arrays);
     }
 }
