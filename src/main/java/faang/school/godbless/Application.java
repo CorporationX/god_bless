ef24d85d8d99685800ef8f368bda3406c400e5da
cirill98 <100434317+cirill98@users.noreply.github.com>
2024-06-04 10:20:15 +0500

    Ипользовал 2 цикла

--- src/main/java/faang/school/godbless/Application.java
+++ src/main/java/faang/school/godbless/Application.java
@@ -1,7 +1,22 @@
 package faang.school.godbless;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Set;
+
 public class Application {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Set<String> setActivity = Set.of("Плавание", "Бокс", "Танцы", "Хоббихорсинг");
+        List<User> usersAll = new ArrayList<>();
+        User user1 = new User(1, "Petr", 27, Set.of("Бег", "Бокс"));
+        User user2 = new User(2, "Luda", 45, Set.of("Хоббихорсинг", "Сплетни"));
+        User user3 = new User(3, "Ivan", 27, Set.of("Бег", "Танцы"));
+        User user4 = new User(4, "Sveta", 27, Set.of("Бег", "Плавание"));
+        usersAll.add(user1);
+        usersAll.add(user2);
+        usersAll.add(user3);
+        usersAll.add(user4);
+        System.out.println(User.findHobbyLovers(usersAll, setActivity));
+
     }
 }
---
+++ src/main/java/faang/school/godbless/User.java
@@ -0,0 +1,32 @@
+package faang.school.godbless;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.ToString;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
+@Getter
+@ToString
+@AllArgsConstructor
+public class User {
+    private long id;
+    private String name;
+    private int age;
+    private Set<String> activityUser;
+
+    public static Map<User, String> findHobbyLovers(List<User> users, Set<String> activityAll) {
+        Map<User, String> foundHobby = new HashMap<>();
+        for (User user : users) {
+            for (String oneActivityUser : user.getActivityUser()) {
+                if (activityAll.contains(oneActivityUser)) {
+                    foundHobby.put(user, oneActivityUser);
+                }
+            }
+        }
+        return foundHobby;
+    }
+}
