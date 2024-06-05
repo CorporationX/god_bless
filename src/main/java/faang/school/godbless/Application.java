39f3ccd9c7f0de019fa5142e0b445709698fb7ab
Gevorg Sogomonian <a0964593@gmail.com>
2024-06-05 12:32:48 +0300

    First try

--- src/main/java/faang/school/godbless/Application.java
+++ src/main/java/faang/school/godbless/Application.java
@@ -3,26 +3,19 @@ package faang.school.godbless;
 import java.util.ArrayList;
 import java.util.List;
 
-import static faang.school.godbless.User.groupUsers;
-
 public class Application {
     public static void main(String[] args) {
         List<User> users = new ArrayList<>();
 
-        User Bob = new User();
-        User Jack = new User();
-        User Van = new User();
-        User Kate = new User();
+        User Bob = new User("Bob", 20, "Google", "Amsterdam");
+        User Jack = new User("Jack", 20, "Amazon", "New York");
+        User Van = new User("Van", 30, "Uber", "London");
+        User Kate = new User("Kate", 20, "Google", "Amsterdam");
 
         users.add(Bob);
         users.add(Jack);
         users.add(Van);
         users.add(Kate);
-
-        Bob.setAge(20);
-        Jack.setAge(30);
-        Van.setAge(20);
-        Kate.setAge(20);
 
         System.out.println(User.groupUsers(users));
     }
