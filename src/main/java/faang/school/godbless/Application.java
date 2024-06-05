75c64d35f2d5e7c49e49500969c59106d6d7000e
Nikita2768 <vechkinnikita22@gmail.com>
2024-06-05 21:35:36 +0500

    Все исправленно

--- src/main/java/faang/school/godbless/Application.java
+++ src/main/java/faang/school/godbless/Application.java
@@ -1,7 +1,5 @@
 package faang.school.godbless;
 
-import faang.school.godbless.registry.User;
-
 public class Application {
     public static void main(String[] args) {
         System.out.println("Hello world!");
