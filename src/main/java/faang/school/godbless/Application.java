7b37375dfcaea3dd1cadd7cad6992446ab70f143
Alexandra <s7125@mail.ru>
2024-06-05 12:17:30 +0500

    BJS2-8014 add class House and method add, find, del

--- src/main/java/faang/school/godbless/Application.java
+++ src/main/java/faang/school/godbless/Application.java
@@ -1,7 +1,43 @@
 package faang.school.godbless;
 
+import java.util.HashMap;
+
 public class Application {
+    private static HashMap<String, House> allHouseMap = new HashMap<>();
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        House house = new House("Старк", "герб1");
+        House house2 = new House("Ланнистер", "герб2");
+        House house3 = new House("Баратеон", "герб3");
+
+        addHouse(house.getName(), house);
+        addHouse(house2.getName(), house);
+        addHouse(house3.getName(), house);
+        printAll();
+
+        delHouse(house2.getName());
+        System.out.println("Удалили дом: " + house2.getName());
+        printAll();
+
+        System.out.println("Поиск герба:" + findHouse(house3.getName()));
+    }
+
+    public static void addHouse(String name, House house) {
+        allHouseMap.put(name, house);
+    }
+
+    public static void delHouse(String name) {
+        allHouseMap.remove(name);
+    }
+
+    public static String findHouse(String name) {
+        return allHouseMap.get(name).getSigil();
+    }
+
+    public static void printAll() {
+        allHouseMap.forEach((s, house) -> {
+            System.out.println("Дом: " + s + ", Герб: " +house.getSigil());
+        });
     }
 }
---
+++ src/main/java/faang/school/godbless/House.java
@@ -0,0 +1,22 @@
+package faang.school.godbless;
+
+public class House {
+
+    private String name;
+    private String sigil;
+
+    public House(String name, String sigil) {
+        this.name = name;
+        this.sigil = sigil;
+    }
+
+    public String getSigil() {
+        return sigil;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+
+}
