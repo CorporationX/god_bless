4bf6b8eb34db3c74418dab6508d7ef89daae419e
Alexandra <s7125@mail.ru>
2024-06-05 21:48:33 +0500

    BJS2-7636 review

--- src/main/java/faang/school/godbless/Example.java
+++ src/main/java/faang/school/godbless/Example.java
@@ -3,6 +3,7 @@ package faang.school.godbless;
 public class Example {
 
     public static int[] reverse(int nums[]){
+        //тут не пойму как без доп массива сделать использовать тотже массив?
         int numsReverse [] = new int[nums.length];
         int k = 0;
         for(int i = (nums.length - 1) ; i > 0; i--){
