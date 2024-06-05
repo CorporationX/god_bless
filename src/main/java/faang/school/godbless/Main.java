748b67e061b661b2941813b44eed479124810261
Nikita Volzhin <vlzh.nik@mail.ru>
2024-06-05 18:40:36 +0500

    Регистрация пользователя

---
+++ src/main/java/faang/school/godbless/Main.java
@@ -0,0 +1,8 @@
+package faang.school.godbless;public class Main {
+    public static void main(String[] args) {
+        User user1 = new User("Anton", 18, "Google", "London");
+        User user2 = new User("", 28, "Google", "London");
+        User user3 = new User("Nikita", 16, "d", "London");
+        User user4 = new User("Nikita", 20, "Google", "Russia");
+    }
+}
--- src/main/java/faang/school/godbless/User.java
+++ src/main/java/faang/school/godbless/User.java
@@ -4,12 +4,28 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Set;
 
 public class User {
+    private static final Set<String> VALID_JOBS = Set.of("Google", "Uber", "Amazon");
+    private static final Set<String> VALID_ADDRESSES = Set.of("London", "New York", "Amsterdam");
+
     private String name;
     private int age;
     private String workplace;
     private String address;
+
+    public User(String name, int age, String workplace, String address) {
+        checkValidityName(name);
+        checkValidityAge(age);
+        checkWorkplaceInSet(workplace);
+        checkAddressInSet(address);
+
+        this.name = name;
+        this.age = age;
+        this.workplace = workplace;
+        this.address = address;
+    }
 
     public static Map<Integer, List<User>> groupUsers(List<User> users) {
         Map<Integer, List<User>> ageGroups = new HashMap<>();
@@ -23,5 +39,29 @@ public class User {
             }
         }
         return ageGroups;
+    }
+
+    private void checkValidityName(String name) {
+        if (name.isEmpty()) {
+            throw new IllegalArgumentException("Name cannot be empty");
+        }
+    }
+
+    private void checkValidityAge(int age) {
+        if (age < 18) {
+            throw new IllegalArgumentException("The age cannot be less than 18");
+        }
+    }
+
+    private void checkWorkplaceInSet(String workplace) {
+        if (!VALID_JOBS.contains(workplace)) {
+            throw new IllegalArgumentException("Workplace is not on the set");
+        }
+    }
+
+    private void checkAddressInSet(String address) {
+        if (!VALID_ADDRESSES.contains(address)) {
+            throw new IllegalArgumentException("Address is not on the set");
+        }
     }
 }
