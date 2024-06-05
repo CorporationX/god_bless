0301787152e750074fdcd692e5bba0533d4bba67
Danill Klinkov <klinkov.danya@bk.ru>
2024-06-05 22:43:54 +0300

    Task_5 done

---
+++ src/main/java/faang/school/godbless/task5/Example.java
@@ -0,0 +1,15 @@
+package faang.school.godbless.task5;
+
+public class Example {
+    public static void reverse(int[] nums) {
+        int left = 0;
+        int right = nums.length - 1;
+        while (left < right) {
+            int tmp = nums[left];
+            nums[left] = nums[right];
+            nums[right] = tmp;
+            left++;
+            right--;
+        }
+    }
+}
---
+++ src/main/java/faang/school/godbless/task5/Main.java
@@ -0,0 +1,11 @@
+package faang.school.godbless.task5;
+
+public class Main {
+    public static void main(String[] args) {
+        int[] nums = {1, 2, 3, 4, 5};
+        Example.reverse(nums);
+        for (int num : nums) {
+            System.out.print(num);
+        }
+    }
+}
