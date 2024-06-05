a8fba1304dee8a41710dbf49586dea2e46e02f87
Nikita Volzhin <vlzh.nik@mail.ru>
2024-06-05 15:45:08 +0500

    Created a method that Groups users by age

---
+++ src/main/java/faang/school/godbless/User.java
@@ -0,0 +1,25 @@
+package faang.school.godbless;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class User {
+    private String name;
+    private int age;
+    private String workplace;
+    private String address;
+
+    public static Map<Integer, List<User>> groupUsers(List<User> users) {
+        Map<Integer, List<User>> ageGroups = new HashMap<>();
+
+        for (User user : users) {
+            if (ageGroups.containsKey(user.age)) {
+                ageGroups.get(user.age).add(user);
+            } else {
+                ageGroups.put(user.age, List.of(user));
+            }
+        }
+        return ageGroups;
+    }
+}
