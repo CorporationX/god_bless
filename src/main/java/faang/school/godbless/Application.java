fd987884cc206410321c1b7533f3d4118375450e
Gevorg Sogomonian <a0964593@gmail.com>
2024-06-06 00:04:01 +0300

    UsersByAge third try(with Application version)

---
+++ src/main/java/faang/school/godbless/Application.java
@@ -0,0 +1,27 @@
+package faang.school.godbless;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Application {
+    public static void main(String[] args) {
+        List<User> users = new ArrayList<>();
+
+        User Bob = new User();
+        User Jack = new User();
+        User Van = new User();
+        User Kate = new User();
+
+        users.add(Bob);
+        users.add(Jack);
+        users.add(Van);
+        users.add(Kate);
+
+        Bob.setAge(20);
+        Jack.setAge(30);
+        Van.setAge(20);
+        Kate.setAge(20);
+
+        System.out.println(User.groupUsers(users));
+    }
+}
