e80271ddcb091d8d93c63d1bfa5b0a5c5724a589
Alexandra <s7125@mail.ru>
2024-06-05 22:03:04 +0500

    BJS2-8217 add class

--- src/main/java/faang/school/godbless/Application.java
+++ src/main/java/faang/school/godbless/Application.java
@@ -1,7 +1,38 @@
 package faang.school.godbless;
 
+import java.util.HashMap;
+
 public class Application {
+    private static HashMap<Book,String> bookState = new HashMap<>();
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Book firstBook = new Book("firstBook" , "FirstAuthor" , 20024);
+        Book secondBook = new Book("secondBook" , "SecondAuthor" , 20024);
+        Book thirdBook = new Book("thirdBook" , "ThirdAuthor" , 20024);
+        
+        add(firstBook, "3");
+        add(secondBook, "10");
+        add(thirdBook, "30");
+
+        printAll();
+        System.out.println("Finish");
+        remove(firstBook.getTitle(), firstBook.getAuthor(), firstBook.getYear());
+        printAll();
+
+        searchBook(secondBook.getTitle(), secondBook.getAuthor(), secondBook.getYear());
+    }
+
+    private static void add(Book book, String shelf){
+        bookState.put(book, shelf);
+    }
+    private static void remove(String name, String author, Integer year){
+        bookState.remove(new Book(name, author, year));
+    }
+    private static void printAll(){
+        bookState.forEach((book, shelf) -> {
+            System.out.println("Book: " + book.getTitle() + ", shelf: " + shelf);
+        });
+    }
+    private static void searchBook(String name, String author, Integer year){
+        System.out.println("Search book: " + name + ", shelf: " + bookState.get(new Book(name, author, year)));
     }
 }
