5934de1a7a1f4265872bedcbc552da763516a638
Иван Шилов <ivshvl39@gmail.com>
2024-06-05 20:54:21 +0300

    fix

--- src/main/java/faang/school/godbless/user2/User.java
+++ src/main/java/faang/school/godbless/user2/User.java
@@ -5,7 +5,6 @@ import lombok.Data;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.stream.Collectors;
 
 @Data
 public class User {
@@ -14,12 +13,14 @@ public class User {
     private int age;
     private List<String> activities;
 
-    public static Map<User,String> findHobbyLovers(List<User> users, List<String> activities){
-        Map<User,String> result = new HashMap<>();
-        for (User user:users){
-            for(String activity:user.getActivities()){
-                if(activities.contains(activity))
-                    result.put(user,activity);
+    public static Map<User, String> findHobbyLovers(List<User> users, List<String> activities) {
+        Map<User, String> result = new HashMap<>();
+        for (User user : users) {
+            for (String activity : user.getActivities()) {
+                if (activities.contains(activity)) {
+                    result.put(user, activity);
+                    break;
+                }
             }
         }
         return result;
