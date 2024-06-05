c6a46bb50d217afa986aac95d1a0f6940f0d5b4e
cirill98 <100434317+cirill98@users.noreply.github.com>
2024-06-05 10:49:03 +0500

    g

--- src/main/java/faang/school/godbless/Application.java
+++ src/main/java/faang/school/godbless/Application.java
@@ -3,7 +3,7 @@ package faang.school.godbless;
 public class Application {
     public static void main(String[] args) {
         Warrior w = new Warrior("Wwww");
-        Archer a = new Archer("OOO");
+        Archer a = new Archer("OOOo");
         System.out.println(w);
         System.out.println(a);
     }
