c922b34f3f5542f9544d8c5e169147e7bce446f5
Max Prigonyuk <kael.smr@gmail.com>
2024-06-05 14:39:01 +0400

    Java HashMap: Игра престолов https://faang-school.atlassian.net/browse/BJS2-8017

---
+++ src/main/java/faang/school/godbless/module1/sprint1/task6/House.java
@@ -0,0 +1,16 @@
+package faang.school.godbless.module1.sprint1.task6;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.ToString;
+
+@AllArgsConstructor
+@Getter
+@ToString
+public class House {
+    private String name;
+    private String sigil;
+
+
+}
