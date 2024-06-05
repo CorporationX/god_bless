8d32d13989711cd4d1ff13a6e082164042d044bc
Rauan Kaztaev <rtishtyk@gmail.com>
2024-06-05 13:28:02 +0500

    Развернитесь: решил задачу без вспомогательного массива и проверил параметр на null

--- src/main/java/faang/school/godbless/Example.java
+++ src/main/java/faang/school/godbless/Example.java
@@ -1,14 +1,16 @@
 package faang.school.godbless;
 
+import java.util.Objects;
+
 public class Example {
 
     public int[] reverse(int[] nums) {
-        int[] result = new int[nums.length];
-
-        for (int i = 0; i < nums.length; i++) {
-            result[i] = nums[nums.length - 1 - i];
+        Objects.requireNonNull(nums, "Array must not be null");
+        for (int i = 0, j = nums.length - 1; i < j; i++, j--) {
+            int curr = nums[i];
+            nums[i] = nums[j];
+            nums[j] = curr;
         }
-
-        return result;
+        return nums;
     }
 }
