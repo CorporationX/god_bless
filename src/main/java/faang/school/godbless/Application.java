bfb6ee52a2c579903e26953c0fc751fb2feb25c2
Kilotas <Alendos87@gmail.com>
2024-06-05 22:17:13 +0500

    Решение задачи на нахождение общих хобби

--- src/main/java/faang/school/godbless/Application.java
+++ src/main/java/faang/school/godbless/Application.java
@@ -1,16 +1,28 @@
 package faang.school.godbless;
 
-import static faang.school.godbless.Example.reverse;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
+
 
 public class Application {
     public static void main(String[] args) {
-        int nums[] = {1, 4, 6, 7};
-        int resultNums[];
+        User user1 = new User(1, "Боб", 25, new HashSet<>(Arrays.asList("Бег", "Турник")));
+        User user2 = new User(2, "Bob", 30, new HashSet<>(Arrays.asList("Бег", "Отжмания")));
 
-        resultNums = reverse(nums);
-        System.out.print("Массив: ");
-        for(int i = 0; i < resultNums.length; i++){
-            System.out.print(resultNums[i] + " ");
+        List<User> users = Arrays.asList(user1, user2);
+
+        Set<String> activities = new HashSet<>(Arrays.asList("Бег", "Плаванье"));
+
+        Map<User, String> hobbyLovers = user1.findHobbyLovers(users, activities);
+
+        // Выводим результаты
+        for (Map.Entry<User, String> entry : hobbyLovers.entrySet()) {
+            System.out.println(entry.getKey().getUsername() + " любит " + entry.getValue());
         }
+
     }
 }
