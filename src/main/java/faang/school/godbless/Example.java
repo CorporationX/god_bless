ec74a182b80761d0b37800197cddec1e46cffa95
cirill98 <100434317+cirill98@users.noreply.github.com>
2024-06-05 09:15:06 +0500

    Убрал комментарии

--- src/main/java/faang/school/godbless/Example.java
+++ src/main/java/faang/school/godbless/Example.java
@@ -3,20 +3,14 @@ package faang.school.godbless;
 import java.util.Arrays;
 
 public class Example {
-    public static int[] revers(int[] array){
+    public static int[] revers(int[] array) {
         int start = 0;
         int finish = array.length - 1;
-        while (start < finish){
+        while (start < finish) {
             int temp = array[start];
             array[start++] = array[finish];
             array[finish--] = temp;
         }
         return array;
-//        int[] reversInt = new int[array.length];
-//        int index = 0;
-//        for (int i = array.length - 1; i >=0 ; i--) {
-//            reversInt[index++] = array[i];
-//        }
-//        return reversInt;
     }
 }
