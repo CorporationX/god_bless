faaa47101a68f56dbd1ad45353d13bc3dd5bbd58
Иван Шилов <ivshvl39@gmail.com>
2024-06-05 09:40:24 +0300

    добавил форматирование

--- src/main/java/faang/school/godbless/Example.java
+++ src/main/java/faang/school/godbless/Example.java
@@ -1,9 +1,9 @@
 package faang.school.godbless;
 
-public  class Example {
-    public int[] reverse(int[] arr){
+public class Example {
+    public int[] reverse(int[] arr) {
         int[] reversedArr = new int[arr.length];
-        for (int start=0,end=arr.length-1;start<=end;start++,end--) {
+        for (int start = 0, end = arr.length - 1; start <= end; start++, end--) {
             reversedArr[start] = arr[end];
             reversedArr[end] = arr[start];
         }
