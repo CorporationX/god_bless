d09274bc688da7d1e327911ad4a9e068362ff42f
cirill98 <100434317+cirill98@users.noreply.github.com>
2024-06-05 10:24:49 +0500

    Пытаюсь перезалить на гит хаб

--- src/main/java/faang/school/godbless/Application.java
+++ src/main/java/faang/school/godbless/Application.java
@@ -2,7 +2,7 @@ package faang.school.godbless;
 
 public class Application {
     public static void main(String[] args) {
-        Warrior w = new Warrior("Www");
+        Warrior w = new Warrior("Wwww");
         Archer a = new Archer("OOO");
         System.out.println(w);
         System.out.println(a);
