52c47b96bfe32ab33c1351b258d440573a50f344
Enemy095 <salam.tahaev@mail.ru>
2024-06-05 19:47:21 +0300

    refactor method reverse()

--- src/main/java/faang/school/godbless/reverse/Example.java
+++ src/main/java/faang/school/godbless/reverse/Example.java
@@ -2,21 +2,15 @@ package faang.school.godbless.reverse;
 
 import lombok.ToString;
 
-import java.util.Arrays;
-
 @ToString
 public class Example {
-    public static void reverse(int[] array) {
+    public static int[] reverse(int[] array) {
         int buffer;
         for (int i = 0; i < array.length / 2; i++) {
             buffer = array[i];
             array[i] = array[array.length - 1 - i];
             array[array.length - 1 - i] = buffer;
         }
-        showAnArrayReverse(array);
-    }
-
-    public static void showAnArrayReverse(int[] array) {
-        System.out.println(Arrays.toString(array));
+        return array;
     }
 }
--- src/main/java/faang/school/godbless/reverse/Main.java
+++ src/main/java/faang/school/godbless/reverse/Main.java
@@ -1,8 +1,11 @@
 package faang.school.godbless.reverse;
 
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
         int[] arrays = {1, 35, 453, 3, 344, 66, 5};
-        Example.reverse(arrays);
+        int[] reversedArray = Example.reverse(arrays);
+        System.out.println("reversedArray = " + Arrays.toString(reversedArray));
     }
 }
