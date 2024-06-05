edc30ed51e1ffe0e21f66ca5b6abafd7ec97ffd1
Nikita2768 <vechkinnikita22@gmail.com>
2024-06-05 20:46:18 +0500

    Внесены правки согласно недостаткам

--- src/main/java/faang/school/godbless/Application.java
+++ src/main/java/faang/school/godbless/Application.java
@@ -1,10 +1,8 @@
 package faang.school.godbless;
 
-import java.util.Arrays;
 
 public class Application {
     public static void main(String[] args) {
-        int[] array = new int[] {1, 2, 3, 4, 5};
-        Arrays.stream(Example.reverse(array)).forEach(System.out::println);
+        System.out.println("Hello world");
     }
 }
