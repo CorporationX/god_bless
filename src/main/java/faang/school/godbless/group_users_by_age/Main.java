6dab8dc3a5542530845ef0afe731a370c6c1d8a7
George <cradplay@gmail.com>
2024-06-05 00:00:05 -0400

    plain user -> List<User> to support multiple users with one age

--- src/main/java/faang/school/godbless/group_users_by_age/Main.java
+++ src/main/java/faang/school/godbless/group_users_by_age/Main.java
@@ -1,16 +1,18 @@
 package faang.school.godbless.group_users_by_age;
 
 import java.util.Arrays;
+import java.util.List;
 import java.util.Map;
 
-public class main {
+public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
 
         User user1 = new User("George", 21, "OPS", "Yonge St. 5775");
         User user2 = new User("Ethan", 22, "NoblKids", "Bergeron Center");
+        User user3 = new User("Someone else", 21, "OPS", "Yonge St. 5775");
 
-        Map<Integer, User> map = User.groupUsers(Arrays.asList(user2, user1));
+        Map<Integer, List<User>> map = User.groupUsers(Arrays.asList(user2, user1, user3));
 
         System.out.println(map);
         System.out.println(map.get(21));
--- src/main/java/faang/school/godbless/group_users_by_age/User.java
+++ src/main/java/faang/school/godbless/group_users_by_age/User.java
@@ -3,10 +3,7 @@ package faang.school.godbless.group_users_by_age;
 import lombok.AllArgsConstructor;
 import lombok.ToString;
 
-import java.util.Arrays;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 // Я надеюсь коменты можно на английском писать, у меня клава только с английской расскладкой и я прям мучабсь писать на русском :)
 
@@ -20,10 +17,12 @@ public class User {
     private String address;
 
     //Quite self-explanatory
-    public static Map<Integer, User> groupUsers(List<User> userList) {
-        Map<Integer, User> userMap = new HashMap<Integer, User>();
+    public static Map<Integer, List<User>> groupUsers(List<User> userList) {
+        Map<Integer, List<User>> userMap = new HashMap<Integer, List<User>>();
         for (User user : userList){
-            userMap.put(user.age ,user);
+//            Intellij black magic lol, extremely handy method fr, I also love how its lambda func. Js dev in me is very happy
+            List<User> users = userMap.computeIfAbsent(user.age, k -> new ArrayList<User>());
+            users.add(user);
         }
         return userMap;
     }
