5ef07bd24ca0aa202bd6feee0b51941237fe3f74
Mikhail Fokin <fokinms@gmail.com>
2024-06-05 12:12:31 +0400

    BJS2-7565 Группировка пользователей по возрасту

---
+++ src/main/java/faang/school/godbless/bjs2_7565/User.java
@@ -0,0 +1,19 @@
+package faang.school.godbless.bjs2_7565;
+
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@Getter
+@ToString
+@EqualsAndHashCode
+@AllArgsConstructor
+@NoArgsConstructor
+public class User {
+    private String name;
+    private int age;
+    private String workplace;
+    private String address;
+}
