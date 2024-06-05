d57b9641dfe733556202693827bac4da78dc6474
Andrey Kuzmin <Andreytko@mail.ru>
2024-06-06 00:57:32 +0300

    поправил решение

--- src/main/java/faang/school/godbless/Task3/User.java
+++ src/main/java/faang/school/godbless/Task3/User.java
@@ -25,28 +25,9 @@ public class User {
     public static HashMap<Integer, List<User>> groupUsers(List<User> users) {
         Map<Integer, List<User>> userGroups = new HashMap<>();
         for (User user : users) {
-            int key = user.getAge();
-            List<User> sameAge = new ArrayList<>();
-            for (User user1 : users) {
-                if (user1.getAge() == key) {
-                    sameAge.add(user1);
-                }
-            }
-            userGroups.put(key, sameAge);
+            userGroups.computeIfAbsent(user.age, k -> new ArrayList<>()).add(user);
         }
         return (HashMap<Integer, List<User>>) userGroups;
-    }
-
-    public static void main(String[] args) {
-
-        List<User> users = new ArrayList<>();
-        users.add(new User("Andrew", 26, "Moscow", "Street1"));
-        users.add(new User("John", 25, "Moscow", "Street1"));
-        users.add(new User("Jane", 25, "Moscow", "Street1"));
-        users.add(new User("Jordan", 26, "Moscow", "Street1"));
-        HashMap<Integer, List<User>> ageGroups = groupUsers(users);
-        System.out.println(ageGroups);
-
     }
 
 }
