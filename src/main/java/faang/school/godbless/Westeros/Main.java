90f30a846c13b33d7d0ae35caeeca79ed93057bf
Nikita Tolstov <nikita.tolstv@gmail.com>
2024-06-05 11:01:51 +0300

    Changed hashMap on interface Map

--- src/main/java/faang/school/godbless/Westeros/Main.java
+++ src/main/java/faang/school/godbless/Westeros/Main.java
@@ -1,24 +1,25 @@
 package faang.school.godbless.Westeros;
 
 import java.util.HashMap;
+import java.util.Map;
 
 public class Main {
 
-    public static void addNewHouse(HashMap<String, House> houses, House house) {
+    public static void addNewHouse(Map<String, House> houses, House house) {
         houses.put(house.getName(), house);
     }
 
-    public static void deleteHouse(HashMap<String, House> houses, String nameHouse) {
+    public static void deleteHouse(Map<String, House> houses, String nameHouse) {
         houses.remove(nameHouse);
     }
 
-    public static void outputListHouses(HashMap<String, House> houses) {
+    public static void outputListHouses(Map<String, House> houses) {
         if (houses.isEmpty()) {
             System.out.println("There are no entries");
         } else houses.forEach((key, value) -> System.out.println(key + ": " + value));
     }
 
-    public static void searchByName(HashMap<String, House> houses, String nameHouse) {
+    public static void searchByName(Map<String, House> houses, String nameHouse) {
         House house = houses.get(nameHouse);
 
         if (house != null) {
@@ -32,7 +33,7 @@ public class Main {
         House stark = new House("Stark", "Gray direwolf head");
         House lannister = new House("Lannister", "Golden Lion");
 
-        HashMap<String, House> houses = new HashMap<>();
+        Map<String, House> houses = new HashMap<>();
 
         addNewHouse(houses, baratheon);
         addNewHouse(houses, stark);
