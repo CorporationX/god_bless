4030837c74e931719cd5dd8ff8c54d8ce125db60
davy <vova_patrasko@mail.ru>
2024-06-06 10:11:33 +0300

    BJS2-7894: Fix bugs

--- src/main/java/heroesofswordandmagic/Creature.java
+++ src/main/java/heroesofswordandmagic/Creature.java
@@ -3,7 +3,6 @@ package heroesofswordandmagic;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 
-@Data
 @AllArgsConstructor
 public abstract class Creature {
     protected String name;
@@ -15,4 +14,43 @@ public abstract class Creature {
 
     public abstract int getDamage();
 
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getLevel() {
+        return level;
+    }
+
+    public void setLevel(int level) {
+        this.level = level;
+    }
+
+    public int getDefense() {
+        return defense;
+    }
+
+    public void setDefense(int defense) {
+        this.defense = defense;
+    }
+
+    public int getSpeed() {
+        return speed;
+    }
+
+    public void setSpeed(int speed) {
+        this.speed = speed;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
 }
