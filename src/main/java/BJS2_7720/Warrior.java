2f484e20a49601a2783e4b4fe1c7b82186fc7eba
l0zain <nanoowood@gmail.com>
2024-06-07 02:02:48 +0300

    итог

--- src/main/java/BJS2_7720/Warrior.java
+++ src/main/java/BJS2_7720/Warrior.java
@@ -7,6 +7,6 @@ public class Warrior extends Character{
 
     @Override
     public void attack(Character character) {
-        character.HEALTH -= power;
+        character.health -= power;
     }
 }
