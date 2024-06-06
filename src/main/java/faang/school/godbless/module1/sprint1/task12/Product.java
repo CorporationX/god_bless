fb1ee92eff3a950fee70f38df97b6dfa599548ff
Max Prigonyuk <kael.smr@gmail.com>
2024-06-07 02:47:55 +0400

    Java HashMap: Разбираем склад Amazon https://faang-school.atlassian.net/browse/BJS2-8296

---
+++ src/main/java/faang/school/godbless/module1/sprint1/task12/Product.java
@@ -0,0 +1,17 @@
+package faang.school.godbless.module1.sprint1.task12;
+
+
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.ToString;
+
+@Getter
+@ToString
+@AllArgsConstructor
+@EqualsAndHashCode
+public class Product {
+    private int id;
+    private String name;
+    private String category;
+}
