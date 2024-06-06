d90edd27fc58ae4eddc350dc9c2a4d85ed46e15f
Kilotas <Alendos87@gmail.com>
2024-06-06 12:34:54 +0500

    Решение задачи с лучником и войном

--- src/main/java/faang/school/godbless/Application.java
+++ src/main/java/faang/school/godbless/Application.java
@@ -7,21 +7,19 @@ import java.util.Set;
 
 public class Application {
     public static void main(String[] args) {
-        User user1 = new User(1, "Снейп", 25, new HashSet<>(Arrays.asList("Бег", "Турник")));
-        User user2 = new User(2, "Любин", 30, new HashSet<>(Arrays.asList("Отжмания", "Турник")));
-
-
+        User user1 = new User(1, "Боб", 25, new HashSet<>(Arrays.asList("Бег", "Турник")));
+        User user2 = new User(2, "Bob", 30, new HashSet<>(Arrays.asList("Бег", "Отжмания")));
 
         List<User> users = Arrays.asList(user1, user2);
 
-        Set<String> activities = new HashSet<>(Arrays.asList("Турник", "Бег"));
-
+        Set<String> activities = new HashSet<>(Arrays.asList("Бег", "Плаванье"));
 
         Map<User, String> hobbyLovers = user1.findHobbyLovers(users, activities);
 
-
+        // Выводим результаты
         for (Map.Entry<User, String> entry : hobbyLovers.entrySet()) {
-            System.out.println(entry.getKey().getUsername() + " loves " + entry.getValue());
+            System.out.println(entry.getKey().getUsername() + " любит " + entry.getValue());
         }
+
     }
 }
