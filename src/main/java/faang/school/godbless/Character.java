ed1ec465ef9170bee5c79112c3960579c923eb7e
Gevorg Sogomonian <a0964593@gmail.com>
2024-06-06 09:09:56 +0300

    in progress

---
+++ src/main/java/faang/school/godbless/Character.java
@@ -0,0 +1,39 @@
+package faang.school.godbless;
+
+public class Character {
+    private String name;
+    private int force;
+    private int dexterity;
+    private int intelligence;
+    private int health = 100;
+
+    public Character(String name) {
+        this.name = name;
+    }
+
+    public Character(String name, int force, int dexterity, int intelligence) {
+        if (!name.isEmpty()) {
+            this.name = name;
+        } else {
+            throw new IllegalArgumentException(name + " Illegal name");
+        }
+
+        if (force > 0 && force <= 100) {
+            this.force = force;
+        } else {
+            throw new IllegalArgumentException(force + " is too much");
+        }
+
+        if (dexterity > 0 && dexterity <= 100) {
+            this.dexterity = dexterity;
+        } else {
+            throw new IllegalArgumentException(dexterity + " is too much");
+        }
+
+        if (intelligence > 0 && intelligence <= 100) {
+            this.intelligence = intelligence;
+        } else {
+            throw new IllegalArgumentException(intelligence + " is too much");
+        }
+    }
+}
