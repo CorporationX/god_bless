7f4c011d22001b5053578343882c3ceb00828b59
sta1er <vlad.sokogubov@gmail.com>
2024-06-06 15:06:16 +0300

    Задание: "Java HashMap: Игра престолов"

--- src/main/java/faang/school/godbless/Application.java
+++ src/main/java/faang/school/godbless/Application.java
@@ -1,7 +1,48 @@
 package faang.school.godbless;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class Application {
+
+    private static Map<String, House> houses = new HashMap<>();
+
+    public static void addNewHouse(House house) {
+        houses.put(house.getName(), house);
+    }
+
+    public static void deleteHouseByName(String name) {
+        houses.remove(name);
+    }
+
+    public static void getInfoByName(String name) {
+        String sigil = houses.get(name).getSigil();
+        if(sigil.isEmpty()) {
+            System.err.printf("House %s don't find", name);
+        }
+        else {
+            System.out.printf("The sigil of %s is %s%n", name, sigil);
+        }
+    }
+
+    public static void printAllHouses() {
+        for (Map.Entry<String, House> entry : houses.entrySet()) {
+            System.out.println(entry.getValue());
+        }
+    }
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        addNewHouse(new House("Stark", "Direwolf"));
+        addNewHouse(new House("Lannister", "Lion"));
+        addNewHouse(new House("Baratheon", "Stag"));
+
+        printAllHouses();
+
+        getInfoByName("Stark");
+
+        deleteHouseByName("Lannister");
+
+        printAllHouses();
     }
 }
---
+++ src/main/java/faang/school/godbless/House.java
@@ -0,0 +1,13 @@
+package faang.school.godbless;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+public class House {
+    private String name;
+    private String sigil;
+}
