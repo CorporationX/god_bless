7e66e4acd4222913088aa7f809edd1d286c0ce57
ravshanbekn <ravshanbeknabiyev.dev@gmail.com>
2024-06-06 23:16:28 +0500

    Игра престолов Final Version

--- src/main/java/faang/school/godbless/game_of_thrones/House.java
+++ src/main/java/faang/school/godbless/game_of_thrones/House.java
@@ -12,30 +12,24 @@ public class House {
     private String sigil;
 
 
-    public static void addNewHouses(Map<String, House> houseMap, House ...houses){
-        for(House house : houses){
-            houseMap.put(house.getName(), house);
-        }
+    public static void addNewHouse(Map<String, House> houseMap, House house) {
+        houseMap.put(house.getName(), house);
     }
 
-    public static void deleteHousesByName(Map<String, House> houseMap, String ...names){
-        for(String name : names){
-            houseMap.remove(name);
-        }
+    public static void deleteHouseByName(Map<String, House> houseMap, String name) {
+        houseMap.remove(name);
     }
 
-    public static House getHouseByName(Map<String, House> houseMap, String name){
+    public static House getHouseByName(Map<String, House> houseMap, String name) {
         return houseMap.get(name);
     }
 
-    public static void showSigilsByName(Map<String, House> houseMap, String ...names){
-        for(String name : names){
-            System.out.println(houseMap.get(name).getSigil());
-        }
+    public static void showSigilByName(Map<String, House> houseMap, String name) {
+        System.out.println(houseMap.get(name).getSigil());
     }
 
-    public static void showAllHouses(Map<String, House> houseMap){
-        for(Map.Entry<String, House> house : houseMap.entrySet()){
+    public static void showAllHouses(Map<String, House> houseMap) {
+        for (Map.Entry<String, House> house : houseMap.entrySet()) {
             System.out.println(house);
         }
     }
--- src/main/java/faang/school/godbless/game_of_thrones/Main.java
+++ src/main/java/faang/school/godbless/game_of_thrones/Main.java
@@ -12,18 +12,21 @@ public class Main {
         House barateonHouse = new House("Баратеон", "Чёрный Коронованный Олень");
 
         System.out.println("After adding");
-        House.addNewHouses(houseMap, starkHouse, lannisterHouse, barateonHouse);
+        House.addNewHouse(houseMap, starkHouse);
+        House.addNewHouse(houseMap, lannisterHouse);
+        House.addNewHouse(houseMap, barateonHouse);
         House.showAllHouses(houseMap);
         System.out.println();
 
         System.out.println("After deleting starkHouse and lannisterHouse");
-        House.deleteHousesByName(houseMap, "Старк", "Ланнистер");
+        House.deleteHouseByName(houseMap, "Старк");
+        House.deleteHouseByName(houseMap, "Ланнистер");
         House.showAllHouses(houseMap);
         System.out.println();
 
         System.out.println("Getting barateonHouse by name: " + House.getHouseByName(houseMap, "Баратеон"));
         System.out.println();
-        System.out.println("Show sigil of barateonHouse by name: " );
-        House.showSigilsByName(houseMap, "Баратеон");
+        System.out.println("Show sigil of barateonHouse by name: ");
+        House.showSigilByName(houseMap, "Баратеон");
     }
 }
