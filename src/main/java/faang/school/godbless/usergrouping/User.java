e8fb37d672c2b965eac1c6e64c3fd8f81121b05f
Dima Dobrev <dobrev2212@gmail.com>
2024-06-06 16:52:39 +0200

    BJS2-7568 1)сделал класс record; 2)добавил реализацию в метод groupUsers, ну и потестил метод у себя)

--- src/main/java/faang/school/godbless/usergrouping/User.java
+++ src/main/java/faang/school/godbless/usergrouping/User.java
@@ -1,20 +1,14 @@
 package faang.school.godbless.usergrouping;
 
-import lombok.AllArgsConstructor;
-
 import java.util.List;
 import java.util.Map;
 
-import static java.util.Collections.emptyMap;
+import static java.util.stream.Collectors.groupingBy;
 
-@AllArgsConstructor
-public class User {
-    private final String name;
-    private final Integer age;
-    private final String placeOfWork;
-    private final String address;
-
+public record User(String name, Integer age, String placeOfWork, String address) {
     public static Map<Integer, List<User>> groupUsers(List<User> users) {
-        return emptyMap();
+
+        return users.stream()
+                .collect(groupingBy(User::age));
     }
 }
