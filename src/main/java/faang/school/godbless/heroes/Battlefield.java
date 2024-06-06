44c21aeb47a8a3f7deafa9ef31dd5d2a2a83878b
npopov_ <lkn_popov@mail.ru>
2024-06-06 23:02:00 +0300

    BJS2-7908 добавлено решение задачи

---
+++ src/main/java/faang/school/godbless/heroes/Battlefield.java
@@ -0,0 +1,36 @@
+package faang.school.godbless.heroes;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class Battlefield {
+    List<Hero> heroes;
+
+    public Battlefield(List<Hero> heroes) {
+        this.heroes = heroes;
+    }
+
+    public void battle() {
+        boolean battlefieldContinue = true;
+        int count = 0;
+        while (battlefieldContinue) {
+            System.out.println("Раунд " + ++count);
+            for (int i = 0; i < heroes.size(); i ++) {
+                Hero firstHero = heroes.get(i);
+                Hero secondHero = heroes.get(heroes.size() - 1 - i);
+                if (!firstHero.getArmy().isEmpty()) {
+                    firstHero.hit(secondHero);
+                }
+                if (secondHero.getArmy().isEmpty()) {
+                    System.out.println("Победил " + firstHero.getName());
+                    return;
+                }
+            }
+            System.out.println("Состояние армий в конце раунда");
+            heroes.get(0).showArmy();
+            heroes.get(1).showArmy();
+            System.out.println();
+        }
+    }
+}
