b3d29c9baf4e818f6709533204ec697ad8b8824e
Gevorg Sogomonian <a0964593@gmail.com>
2024-06-06 15:01:34 +0300

    First try

--- src/main/java/faang/school/godbless/Example.java
+++ src/main/java/faang/school/godbless/Example.java
@@ -3,11 +3,12 @@ package faang.school.godbless;
 public class Example {
 
     public static int[] reverse(int nums[]){
-        int numsReverse [] = new int[nums.length];
+        int numsReverse[] = new int[nums.length];
         int k = 0;
-        for(int i = (nums.length - 1) ; i > 0; i--){
+
+        for(int i = (nums.length - 1) ; i >= 0; i--) {
             numsReverse[k] = nums[i];
-            k = k+1;
+            k++;
         }
         return numsReverse;
     }
