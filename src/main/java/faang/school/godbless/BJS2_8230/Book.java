4eead00735ca5a546e4c01a7832876a4bc75d0db
Mikhail Fokin <fokinms@gmail.com>
2024-06-06 23:43:02 +0400

    BJS2-8230 Java HashMap: Библиотека Вестеросса

---
+++ src/main/java/faang/school/godbless/BJS2_8230/Book.java
@@ -0,0 +1,14 @@
+package faang.school.godbless.BJS2_8230;
+
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.ToString;
+
+@AllArgsConstructor
+@EqualsAndHashCode
+@ToString
+public class Book {
+    private String title;
+    private String author;
+    private int year;
+}
