a7d70a8092b84bae0c3fc1c8cacf280a4e9006a4
Evgenii Malkov <malk.2011@yandex.ru>
2024-06-06 17:06:07 +0300

    BJS2-8002: removed inner static class

--- src/main/java/faang/school/godbless/gameOfThrones/Main.java
+++ src/main/java/faang/school/godbless/gameOfThrones/Main.java
@@ -17,33 +17,31 @@ public class Main {
     vesterosHouses.put("Баратеон", new House("Баратеон", "Нам это дано"));
 
     String voronins = "Воронины";
-    HouseService.addHouse(voronins, new House(voronins, "Люся"));
-    HouseService.removeHouse("Баратеон");
-    log.info(String.format("Sigil info about house: %s | %s", voronins, HouseService.getSigilInfoByHouseName(voronins)));
-    HouseService.printHousesInfo();
+    addHouse(voronins, new House(voronins, "Люся"));
+    removeHouse("Баратеон");
+    log.info(String.format("Sigil info about house: %s | %s", voronins, getSigilInfoByHouseName(voronins)));
+    printHousesInfo();
   }
 
-  public static class HouseService {
-    public static void addHouse(String houseName, House house) {
-      vesterosHouses.put(houseName, house);
-      log.info(String.format("Added house: %s | %s", houseName, vesterosHouses));
-    }
+  public static void addHouse(String houseName, House house) {
+    vesterosHouses.put(houseName, house);
+    log.info(String.format("Added house: %s | %s", houseName, vesterosHouses));
+  }
 
-    public static void removeHouse(String houseName) {
-      vesterosHouses.remove(houseName);
-      log.info(String.format("Removed house: %s | %s", houseName, vesterosHouses));
-    }
+  public static void removeHouse(String houseName) {
+    vesterosHouses.remove(houseName);
+    log.info(String.format("Removed house: %s | %s", houseName, vesterosHouses));
+  }
 
-    public static String getSigilInfoByHouseName(String houseName) {
-      return vesterosHouses.values().stream()
-          .filter(house -> houseName.equals(house.getName()))
-          .findFirst()
-          .orElseThrow(() -> new IllegalArgumentException("House not found"))
-          .getSigil();
-    }
+  public static String getSigilInfoByHouseName(String houseName) {
+    return vesterosHouses.values().stream()
+        .filter(house -> houseName.equals(house.getName()))
+        .findFirst()
+        .orElseThrow(() -> new IllegalArgumentException("House not found"))
+        .getSigil();
+  }
 
-    public static void printHousesInfo() {
-      vesterosHouses.forEach(((name, house) -> log.info(String.format("%s: %s", name, house.getSigil()))));
-    }
+  public static void printHousesInfo() {
+    vesterosHouses.forEach(((name, house) -> log.info(String.format("%s: %s", name, house.getSigil()))));
   }
 }
