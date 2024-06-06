a3d3d80c71804318ac227387404233c4a8ee7185
romax <maksimovskii.roman@yandex.ru>
2024-06-06 17:18:13 +0300

    Задание BJS2-7662 "Развернитесь!" выполнено

---
+++ src/main/java/faang/school/godbless/javaCore/turnAround/Example.java
@@ -0,0 +1,13 @@
+package faang.school.godbless.javaCore.turnAround;
+
+public class Example {
+    public static int[] reverse(int[] array) {
+        int temp;
+        for (int i = 0, j = array.length - 1; i < array.length / 2; i++, j--) {
+            temp = array[j];
+            array[j] = array[i];
+            array[i] = temp;
+        }
+        return array;
+    }
+}
