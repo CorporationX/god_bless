7985097597e944c70c91072335454667c7c7a3e8
Sergey Babaev <sergeybabaev@MacBook-Pro-Sergej-2.local>
2024-06-06 14:52:20 +0300

    Test commit

---
+++ src/main/java/faang/school/godbless/BJS27524/Main.java
@@ -0,0 +1,25 @@
+package faang.school.godbless.BJS27524;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+
+public class Main {
+    public static void main(String[] args) {
+        User user1 = new User("Pavel", 30, "Builder", "London");
+        User user2 = new User("Andrei", 22, "Dev", "Moscow");
+        User user3 = new User("Alina", 27, "Ingenere", "Omsk");
+        List<User> users = new ArrayList<>();
+        users.add(user1);
+        users.add(user2);
+        users.add(user3);
+        Set<Integer> setOfAges = new HashSet<>();
+        setOfAges.add(user1.getAge());
+        setOfAges.add(user2.getAge());
+        setOfAges.add(user3.getAge());
+
+
+    }
+}
---
+++ src/main/java/faang/school/godbless/BJS27524/User.java
@@ -0,0 +1,60 @@
+package faang.school.godbless.BJS27524;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Set;
+import java.util.Map
+
+/*
+Условия задачи
+Создать класс
+
+User со следующими полями:
+
+имя;
+
+возраст;
+
+место работы;
+
+адрес.
+
+В этом же классе создать статический метод
+
+groupUsers, который принимает List из объектов User и возвращает Map, где ключом является возраст пользователя,
+а значением — список из всех пользователей в оригинальном списке, которые имеют соответствующий возраст.
+ */
+
+
+public class User {
+    private String firstname;
+    private int age;
+    private String placeofwork;
+    private String adds;
+
+    public User(String firstname, int age, String placeofwork, String adds) {
+        this.firstname = firstname;
+        this.age = age;
+        this.placeofwork = placeofwork;
+        this.adds = adds;
+    }
+
+    public static Map<Integer, List<User>> groupUser(List<User> listOfUser, Set<Integer> newSet) {
+        Map<Integer, List<User>> map1 = new HashMap<>();
+        for (Integer i : newSet) {
+            List<User> list = new ArrayList<>();
+            for (User user : listOfUser) {
+                if (newSet.contains(user.getAge())) {
+                    list.add(user);
+                    map1.put(user.getAge(), list);
+                }
+            }
+        }
+        return map1;
+    }
+}
+
+
+        
+
+
