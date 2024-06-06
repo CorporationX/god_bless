86d66e47d4d57fd6350258a8cc7b9619d1ee03c7
Nikita Volzhin <vlzh.nik@mail.ru>
2024-06-06 20:44:32 +0500

    Развернитесь!

--- src/main/java/faang/school/godbless/Application.java
+++ src/main/java/faang/school/godbless/Application.java
@@ -4,10 +4,13 @@ public class Application {
     public static void main(String[] args) {
         Archer archer = new Archer("Andrey");
         Warrior warrior = new Warrior("Oleg");
+
         System.out.println(archer);
         System.out.println(warrior);
+
         archer.attack(warrior);
         warrior.attack(archer);
+
         System.out.println(archer);
         System.out.println(warrior);
     }
