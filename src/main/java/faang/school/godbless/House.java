49b57ee69c6ec204fe82448b5f173a46fce32c5c
Mikhail Fokin <fokinms@gmail.com>
2024-06-06 16:35:35 +0400

    BJS2-8061 Java HashMap: Игра престолов

---
+++ src/main/java/faang/school/godbless/House.java
@@ -0,0 +1,17 @@
+package faang.school.godbless;
+
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@EqualsAndHashCode
+@ToString
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class House {
+    private String name;
+    private String sigil;
+}
