b992351074a070f4d5e0d6e4f4d90c8d5f9a1999
Иван Шилов <ivshvl39@gmail.com>
2024-06-06 08:51:02 +0300

    Считаем прогулы

---
+++ src/main/java/faang/school/godbless/task3/Student.java
@@ -0,0 +1,12 @@
+package faang.school.godbless.task3;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class Student {
+    private String name;
+    private String faulty;
+    private int year;
+}
