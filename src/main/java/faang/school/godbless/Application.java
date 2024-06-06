7532a3df3120a4f40581cf68f5e0108657179008
Kilotas <Alendos87@gmail.com>
2024-06-06 12:15:15 +0500

    Решение задачи с лучником и войном

--- src/main/java/faang/school/godbless/Application.java
+++ src/main/java/faang/school/godbless/Application.java
@@ -1,13 +1,27 @@
 package faang.school.godbless;
-
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
 
 public class Application {
     public static void main(String[] args) {
-        try {
-            User user = new User("Dumbledore", 120, "Google", "Amsterdam");
-            System.out.println(user);
-        } catch (IllegalArgumentException e) {
-            System.err.println(e.getMessage());
+        User user1 = new User(1, "Снейп", 25, new HashSet<>(Arrays.asList("Бег", "Турник")));
+        User user2 = new User(2, "Любин", 30, new HashSet<>(Arrays.asList("Отжмания", "Турник")));
+
+
+
+        List<User> users = Arrays.asList(user1, user2);
+
+        Set<String> activities = new HashSet<>(Arrays.asList("Турник", "Бег"));
+
+
+        Map<User, String> hobbyLovers = user1.findHobbyLovers(users, activities);
+
+
+        for (Map.Entry<User, String> entry : hobbyLovers.entrySet()) {
+            System.out.println(entry.getKey().getUsername() + " loves " + entry.getValue());
         }
     }
 }
--- src/main/java/faang/school/godbless/User.java
+++ src/main/java/faang/school/godbless/User.java
@@ -1,53 +1,42 @@
 package faang.school.godbless;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 import java.util.Set;
 
-
 public class User {
+    private int id;
     private String username;
     private int age;
-    private String job;
-    private String address;
+    private Set<String> activites;
 
-    public static final Set<String> VALID_JOBS = Set.of("Google", "Uber", "Amazon");
-    public static final Set<String> VALID_ADDRESSES = Set.of("London", "New York", "Amsterdam");
-
-    public User(String username, int age, String job, String address) {
-
-        if (username == null || username.trim().isEmpty()) {
-            throw new IllegalArgumentException("Username cannot be null or empty");
-        }
-
-        if (age < 18) {
-            throw new IllegalArgumentException("Age must be at least 18");
-        }
-
-        if (job == null || !VALID_JOBS.contains(job)){
-            throw new IllegalArgumentException("Invalid job");
-        }
-
-        if (address == null || !VALID_ADDRESSES.contains(address)){
-            throw new IllegalArgumentException("Invalid address");
-        }
-
-
+    public User(int id, String username, int age, Set<String> activites) {
+        this.id = id;
         this.username = username;
         this.age = age;
-        this.job = job;
-        this.address = address;
-
+        this.activites = activites;
     }
 
-
-    @Override
-    public String toString(){
-        return "Пользователь{" +
-                "имя='" + username + '\'' +
-                ", возраст=" + age +
-                ", работа='" + job + '\'' +
-                ", адрес='" + address + '\'' +
-                '}';
+    public Set<String> getActivites() {
+        return activites;
     }
 
+    public String getUsername() {
+        return username;
+    }
+
+    public Map<User, String> findHobbyLovers(List<User> users, Set<String> activities) {
+        Map<User, String> hobbyLovers = new HashMap<>();
+        for (User user : users) {
+            for (String activity : user.getActivites()) {
+                if (activites.contains(activity)) {
+                    hobbyLovers.put(user, activity);
+                    break;
+                }
+            }
+        }
+        return hobbyLovers;
+    }
 
 }
-
