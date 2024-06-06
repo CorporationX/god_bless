033477a572884287237ef5a007c98e77cc843940
Kilotas <Alendos87@gmail.com>
2024-06-06 20:27:05 +0500

    Solution for library of Westeros

--- src/main/java/faang/school/godbless/Application.java
+++ src/main/java/faang/school/godbless/Application.java
@@ -1,16 +1,34 @@
 package faang.school.godbless;
 
-import static faang.school.godbless.Example.reverse;
+import java.util.HashMap;
+import java.util.Map;
+
+import static faang.school.godbless.Book.addBook;
+import static faang.school.godbless.Book.findBook;
+import static faang.school.godbless.Book.removeBook;
 
 public class Application {
     public static void main(String[] args) {
-        int nums[] = {1, 4, 6, 7};
-        int resultNums[];
 
-        resultNums = reverse(nums);
-        System.out.print("Массив: ");
-        for(int i = 0; i < resultNums.length; i++){
-            System.out.print(resultNums[i] + " ");
+        Map<Book, String> library = new HashMap<>();
+
+        Book book1 = new Book("The song of ice and fire", "J. Martin", 2008);
+        Book book2 = new Book("Prisoner of Azkaban", "J. Rowling", 2005);
+
+
+        addBook(library, book1, "1");
+        addBook(library, book2, "2");
+
+
+        for (Map.Entry<Book, String> entry : library.entrySet()) {
+            System.out.println(entry.getKey() + " " + entry.getValue());
         }
+
+
+        findBook(library, "Prisoner of Azkaban", "J. Rowling", 2005);
+        removeBook(library, "Prisoner of Azkaban", "J. Rowling", 2005);
+
+
+        System.out.println(library);
     }
 }
