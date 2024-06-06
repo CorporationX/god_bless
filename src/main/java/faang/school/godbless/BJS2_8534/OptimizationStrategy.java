dbab4c82bf54cd446768d16b0be212362383865f
s.kolbasov <s.kolbasov@bftcom.com>
2024-06-06 16:33:22 +0300

    BJS2-8534 Java HashMap: Оптимизируем работу дата-центра

---
+++ src/main/java/faang/school/godbless/BJS2_8534/OptimizationStrategy.java
@@ -0,0 +1,5 @@
+package faang.school.godbless.BJS2_8534;
+
+public interface OptimizationStrategy {
+  void optimize(DataCenter dataCenter);
+}
---
+++ src/main/java/faang/school/godbless/BJS2_8534/Server.java
@@ -0,0 +1,12 @@
+package faang.school.godbless.BJS2_8534;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class Server {
+  private double load;
+  private double maxLoad;
+  private double energyConsumption;
+}
