3a6ebf10580ab6662beef7f065352cc82ebec450
Mikhail Fokin <fokinms@gmail.com>
2024-06-06 14:32:10 +0400

    BJS2-7645 Развернитесь!

---
+++ src/main/java/faang/school/godbless/Main.java
@@ -0,0 +1,11 @@
+package faang.school.godbless;
+
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) {
+        int[] arr = { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+        Example.reverse(arr);
+        System.out.println(Arrays.toString(arr));
+    }
+}
