5ba3d2e446997418f36ca197767c97d92d20074b
Danya Ashihin <danyadefault@gmail.com>
2024-06-06 12:37:29 +0300

    feature/danya9375-BJS2-7546 fixe class User

--- src/main/java/faang/school/godbless/domain/User.java
+++ src/main/java/faang/school/godbless/domain/User.java
@@ -5,28 +5,19 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-public class User {
 
-    private String name;
-    private Integer age;
-    private String work;
-    private String address;
-
-    public Integer getAge() {
-        return age;
-    }
+public record User(String name, Integer age, String work, String address) {
 
 
     public static Map<Integer, List<User>> groupUsers(List<User> users) {
         Map<Integer, List<User>> groupUsers = new HashMap<>();
         for (User user : users) {
-            if (!groupUsers.containsKey(user.getAge()))
-                groupUsers.put(user.getAge(), new ArrayList<>());
-            groupUsers.get(user.getAge()).add(user);
+            if (!groupUsers.containsKey(user.age())) {
+                groupUsers.put(user.age(), new ArrayList<>());
+                groupUsers.get(user.age()).add(user);
+            }
         }
         return groupUsers;
     }
-
-
 }
 
