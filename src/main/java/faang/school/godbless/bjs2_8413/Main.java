82ea5a2a6f976d5e34620ae4676979ce0609a51f
Maksim Tyumentsev <experttm@gmail.com>
2024-06-07 09:36:30 +0500

    #BJS2-8413 Реализовать систему.

---
+++ src/main/java/faang/school/godbless/bjs2_8413/Main.java
@@ -0,0 +1,27 @@
+package faang.school.godbless.bjs2_8413;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
+public class Main {
+
+  private static final Map<String, List<WebPage>> webPages = new HashMap<>();
+
+  public static void main(String[] args) {
+    WebPage webPage1 = new WebPage("", "", "");
+    WebPage webPage2 = new WebPage("", "", "");
+    WebPage webPage3 = new WebPage("", "", "");
+    WebPage webPage4 = new WebPage("", "", "");
+    WebPage webPage5 = new WebPage("", "", "");
+    WebPage webPage6 = new WebPage("", "", "");
+    WebPage webPage7 = new WebPage("", "", "");
+    WebPage webPage8 = new WebPage("", "", "");
+  }
+
+  private static void method(WebPage webPage) {
+//    Set<String> words = webPage.content().split()
+  }
+
+}
---
+++ src/main/java/faang/school/godbless/bjs2_8413/WebPage.java
@@ -0,0 +1,5 @@
+package faang.school.godbless.bjs2_8413;
+
+public record WebPage(String url, String title, String content) {
+
+}
