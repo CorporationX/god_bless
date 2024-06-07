87dff7cd686ae30b5cb6b9ef028261e1d9dbabbf
Garik-create <pfdfkbybujhm@yandex.ru>
2024-06-07 05:30:16 +0300

    - убрал закомиченный код - заменил класс User на рекорд - сделал статический метод публичным

--- src/main/java/faang/school/godbless/group_users_by_age/User.java
+++ src/main/java/faang/school/godbless/group_users_by_age/User.java
@@ -2,7 +2,6 @@ package faang.school.godbless.group_users_by_age;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
-//import lombok.ToString;
 
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -11,29 +10,22 @@ import java.util.List;
 import java.util.Map;
 import java.util.Set;
 
-@Data
-@AllArgsConstructor
-//@ToString
-public class User {
-    private String userName;
-    private int userAge;
-    private String companyName;
-    private String userAddress;
+public record User (String userName, int userAge, String companyName, String userAddress){
 
-    protected static Map<Integer, List<User>> groupUsers(List<User> users) {
+    public static Map<Integer, List<User>> groupUsers(List<User> users) {
 
         Map<Integer, List<User>> groupedUsers = new HashMap<>();//список сгруппированных пользователей
         Set<Integer> userAges = new HashSet<>();//список возрастов пользователей
 
         //создание набора возрастов пользователей
         for (User user : users) {
-            int age = user.getUserAge();
+            int age = user.userAge;
             userAges.add(age);
         }
         for (Integer userAge : userAges) {
             List<User> userList = new ArrayList<>();//список пользователей данного возраста
             for (User user : users) {
-                if (user.getUserAge() == userAge) {
+                if (user.userAge == userAge) {
                     userList.add(user);
                 }
                 groupedUsers.put(userAge, userList);//добавляем список пользователей данного возраста в результирующий список
