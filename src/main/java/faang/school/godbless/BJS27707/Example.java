853bf248711063e7b07ee58fc091ffbfcfa2be2b
DonMaxim <donmaxim1990@gmail.com>
2024-06-06 22:07:06 -0300

    Resolve task BJS2-7707. Implement Array reverse

---
+++ src/main/java/faang/school/godbless/BJS27707/Example.java
@@ -0,0 +1,13 @@
+package faang.school.godbless.BJS27707;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+public class Example {
+    public static void reverse(Integer[] array) {
+        Collections.reverse(Arrays.asList(array));
+        System.out.println("Array reverse complete");
+    }
+}
