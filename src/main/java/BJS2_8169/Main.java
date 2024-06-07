9a288d2d055b13c6fdc3ab0897c0745ef10eb68e
l0zain <nanoowood@gmail.com>
2024-06-07 22:46:53 +0300

    итог

--- src/main/java/BJS2_8169/Main.java
+++ src/main/java/BJS2_8169/Main.java
@@ -12,6 +12,7 @@ public class Main {
         addStudents("Alya", "IT", 1);
         addStudents("Bob", "RIT", 1);
         addStudents("Alex", "RIT", 1);
+        addStudents(" ", "RIT", 1);
 
         removeStudent("Leo", "IT", 1);
         removeStudent("Alex", "RIT", 1);
@@ -20,12 +21,12 @@ public class Main {
     }
 
     public static void addStudents(String name, String faculty, int year) {
-        students.add(new Student(name, faculty, year));
+        if (name != null && !name.trim().isEmpty()
+                && faculty != null && !faculty.trim().isEmpty() && year != 0) students.add(new Student(name, faculty, year));
     }
 
     public static void removeStudent(String name, String faculty, int year) {
-        students.removeIf(student -> student.getName().equals(name)
-                && student.getFaculty().equals(faculty) && student.getYear() == year);
+        students.remove(new Student(name, faculty,year));
     }
 
     public static Map<Key, List<Student>> findGroupStudents() {
