12ede77023d13c39a63f425ae31f38446028c5a7
Garik-create <pfdfkbybujhm@yandex.ru>
2024-06-07 22:30:16 +0300

    - заменил класс User на рекорд - переписал метод groupUsers, чтобы бал один обход списка юзеров - убрал лишние строчки

--- src/main/java/faang/school/godbless/group_users_by_age/User.java
+++ src/main/java/faang/school/godbless/group_users_by_age/User.java
@@ -1,34 +1,27 @@
 package faang.school.godbless.group_users_by_age;
 
-import lombok.AllArgsConstructor;
-import lombok.Data;
-
 import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
-import java.util.Set;
 
-public record User (String userName, int userAge, String companyName, String userAddress){
+public record User(String userName,
+                   int userAge,
+                   String companyName,
+                   String userAddress) {
 
-    public static Map<Integer, List<User>> groupUsers(List<User> users) {
-
-        Map<Integer, List<User>> groupedUsers = new HashMap<>();//список сгруппированных пользователей
-        Set<Integer> userAges = new HashSet<>();//список возрастов пользователей
-
-        //создание набора возрастов пользователей
+    public static Map<Integer, ArrayList<User>> groupUsers(List<User> users) {
+        Map<Integer, ArrayList<User>> groupedUsers
+                = new HashMap<>();//список сгруппированных пользователей
         for (User user : users) {
-            int age = user.userAge;
-            userAges.add(age);
-        }
-        for (Integer userAge : userAges) {
-            List<User> userList = new ArrayList<>();//список пользователей данного возраста
-            for (User user : users) {
-                if (user.userAge == userAge) {
-                    userList.add(user);
-                }
-                groupedUsers.put(userAge, userList);//добавляем список пользователей данного возраста в результирующий список
+            int age = user.userAge();
+            if (groupedUsers.containsKey(age)) {
+                ArrayList<User> usersByAge = groupedUsers.get(age);
+                usersByAge.add(user);
+            } else {
+                ArrayList<User> usersByAge = new ArrayList<>();
+                usersByAge.add(user);
+                groupedUsers.put(age, usersByAge);
             }
         }
         return groupedUsers;
