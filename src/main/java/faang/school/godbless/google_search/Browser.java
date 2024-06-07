40d42184292b2cc05628ad28e6f720a3e7e5a77c
ravshanbekn <ravshanbeknabiyev.dev@gmail.com>
2024-06-07 16:57:33 +0500

    Разбираем склад Amazon Final Version

--- src/main/java/faang/school/godbless/google_search/Browser.java
+++ src/main/java/faang/school/godbless/google_search/Browser.java
@@ -12,43 +12,43 @@ import java.util.Map;
 public class Browser {
     Map<String, List<WebPage>> index = new HashMap<>();
 
-    public void addToIndex(WebPage webPage){
+    public void addToIndex(WebPage webPage) {
         List<String> keyWords = processContent(webPage.getContent());
-        for(String keyWord : keyWords){
+        for (String keyWord : keyWords) {
             index.computeIfAbsent(keyWord, keyWordAsKey -> new ArrayList<>());
             index.get(keyWord).add(webPage);
         }
     }
 
-    public List<WebPage> searchWebPages(String keyWord){
+    public List<WebPage> searchWebPages(String keyWord) {
         keyWord = keyWord.toLowerCase();
-        if(index.containsKey(keyWord)){
+        if (index.containsKey(keyWord)) {
             return index.get(keyWord.toLowerCase());
         } else {
             log.warn("Could not find any page with this key work {}", keyWord);
-            return new ArrayList<>();
+            return null;
         }
     }
 
-    public void deleteWebPageByUrl(String url){
-        for(Map.Entry<String, List<WebPage>> webPageEntry : index.entrySet()){
+    public void deleteWebPageByUrl(String url) {
+        for (Map.Entry<String, List<WebPage>> webPageEntry : index.entrySet()) {
             List<WebPage> webPageList = webPageEntry.getValue();
             webPageList.removeIf(webPage -> webPage.getUrl().equals(url));
-            if(webPageList.isEmpty()){
+            if (webPageList.isEmpty()) {
                 index.remove(webPageEntry.getKey());
             }
         }
     }
 
-    public List<String> processContent(String content){
+    public List<String> processContent(String content) {
         String onlyWords = content.toLowerCase().replaceAll("[^a-z 0-9]", "");
         return Arrays.stream(onlyWords.split(" ")).filter(string -> !string.isBlank()).toList();
     }
 
-    public void showWebPages(){
-        for(Map.Entry<String, List<WebPage>> webPageEntry : index.entrySet()){
+    public void showWebPages() {
+        for (Map.Entry<String, List<WebPage>> webPageEntry : index.entrySet()) {
             System.out.println("keyWord - " + webPageEntry.getKey());
-            for(WebPage webPage : webPageEntry.getValue()){
+            for (WebPage webPage : webPageEntry.getValue()) {
                 System.out.println(webPage);
             }
         }
