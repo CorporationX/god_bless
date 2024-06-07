4a6c408a239cc9a28d6da1939c26ae377e139557
Dima Dobrev <dobrev2212@gmail.com>
2024-06-07 18:30:35 +0200

    BJS2-8238

---
+++ src/main/java/faang/school/godbless/hashmap/library/Book.java
@@ -0,0 +1,18 @@
+package faang.school.godbless.hashmap.library;
+
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.ToString;
+
+@AllArgsConstructor
+@EqualsAndHashCode
+@Getter
+@ToString
+public class Book {
+    private String title;
+    private String author;
+    private int year;
+
+}
+
