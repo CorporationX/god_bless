e56685df5b8e61717481a2a551c8bccf307e7228
Andrey Kuzmin <Andreytko@mail.ru>
2024-06-08 00:22:56 +0300

    done 4

--- src/main/java/faang/school/godbless/Task7/Main.java
+++ src/main/java/faang/school/godbless/Task7/Main.java
@@ -5,7 +5,15 @@ import java.util.Map;
 
 public class Main {
 
-    Map<Book, String> library = new HashMap<>();
+    public static void main(String[] args) {
 
+        Map<Book, String> library = new HashMap<>();
+        Book calculus = new Book("Calculus", "Zoric", 2023);
+        Book algebra = new Book("Algebra", "Kostrikin", 2000);
+        Book geometry = new Book("Geometry", "Tuzhilin", 2006);
+        library.put(calculus, "shelf1");
+        library.put(algebra, "shelf2");
+        library.put(geometry, "shelf3");
 
+    }
 }
