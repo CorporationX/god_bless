a3dd04e182aab859339b15244cb8fa946aae9b63
npopov_ <lkn_popov@mail.ru>
2024-06-07 09:33:11 +0300

    BJS2-8606 доработано решение задачи

--- src/main/java/faang/school/godbless/cashcash/Main.java
+++ src/main/java/faang/school/godbless/cashcash/Main.java
@@ -5,7 +5,8 @@ import java.util.Map;
 import java.util.Random;
 
 public class Main {
-    private static Map<String, WeatherData> cashWeather = new HashMap<>();
+    private static final Map<String, WeatherData> CASH_WEATHER = new HashMap<>();
+    private static final Random RANDOM = new Random();
 
     public static void main(String[] args) {
         System.out.println(getWeatherInfo("Москва"));
@@ -30,31 +31,30 @@ public class Main {
     }
 
     public static WeatherData getWeatherInfo(String city) {
-        if(cashWeather.containsKey(city)) {
-            return cashWeather.get(city);
-        } else {
-            WeatherData weatherData = getWeather(city);
-            updateCash(city, weatherData.getTemperature(), weatherData.getHumidity());
-            return weatherData;
+        if(CASH_WEATHER.containsKey(city)) {
+            return CASH_WEATHER.get(city);
         }
+        WeatherData weatherData = getWeather(city);
+        updateCash(city, weatherData.getTemperature(), weatherData.getHumidity());
+        return weatherData;
+
     }
 
     public static WeatherData getWeather(String city) {
-        Random random = new Random();
-        return new WeatherData(city, random.nextInt(15, 30), random.nextInt(0, 20));
+        return new WeatherData(city, RANDOM.nextInt(15, 30), RANDOM.nextInt(0, 20));
     }
 
     public static void updateCash(String city, int temperature, int humidity) {
-        cashWeather.put(city, new WeatherData(city, temperature, humidity));
+        CASH_WEATHER.put(city, new WeatherData(city, temperature, humidity));
     }
 
     public static void removeCityCash(String city) {
-        cashWeather.remove(city);
+        CASH_WEATHER.remove(city);
     }
 
     public static void showCityCashInfo() {
         System.out.println("Информации о погоде из кэша");
-        for (Map.Entry<String, WeatherData> weatherEntry : cashWeather.entrySet()) {
+        for (Map.Entry<String, WeatherData> weatherEntry : CASH_WEATHER.entrySet()) {
             System.out.println(weatherEntry.getValue());
         }
     }
