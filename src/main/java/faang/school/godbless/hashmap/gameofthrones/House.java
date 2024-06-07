664ab1272138517dff4cf6c9f74efff0bd9530fd
Dima Dobrev <dobrev2212@gmail.com>
2024-06-07 16:28:26 +0200

    BJS2-8071

---
+++ src/main/java/faang/school/godbless/hashmap/gameofthrones/House.java
@@ -0,0 +1,15 @@
+package faang.school.godbless.hashmap.gameofthrones;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.ToString;
+
+@AllArgsConstructor
+@Getter
+@ToString
+public class House {
+
+    private String name;
+    private String sigil;
+
+}
