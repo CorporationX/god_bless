0db3d00d744802b8cba18b7631cba62f853e58bc
Aleksandr Volkov <222volkov@gmail.com>
2024-06-07 14:33:01 +0300

    выполнить задачу BSJ2-7758

---
+++ src/main/java/faang/school/godbless/aged777_BSJ2_7758_abstraction/Character.java
@@ -0,0 +1,30 @@
+package faang.school.godbless.aged777_BSJ2_7758_abstraction;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@ToString
+public abstract class Character {
+    protected String name;
+    protected int strength;
+    protected int dexterity;
+    protected int intelligence;
+    protected int health = 100;
+
+    public Character (String name) {
+        this.name = name;
+    }
+
+    public Character (String name, int strength, int dexterity, int intelligence) {
+        this.name = name;
+        this.strength = strength;
+        this.dexterity = dexterity;
+        this.intelligence = intelligence;
+    }
+
+    public abstract void attack (Character character);
+
+}
