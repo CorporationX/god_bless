0668afbaf0ddce93eb778eb7810ecc68657b7e32
Aleksandr Volkov <222volkov@gmail.com>
2024-06-07 11:20:24 +0300

    задача BSJ2-7677 переписать класс User через record, добавить @Getter на поле age

--- src/main/java/faang/school/godbless/aged777_BJS2_7590_grouping_users_by_age/User.java
+++ src/main/java/faang/school/godbless/aged777_BJS2_7590_grouping_users_by_age/User.java
@@ -1,35 +1,24 @@
 package faang.school.godbless.aged777_BJS2_7590_grouping_users_by_age;
 
-import lombok.AllArgsConstructor;
 import lombok.Getter;
-import lombok.Setter;
-import lombok.ToString;
 
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-
-@Getter
-@Setter
-@AllArgsConstructor
-@ToString
-public class User {
-    private String name;
-    private int age;
-    private String placeOfWork;
-    private String address;
+record User(String name, @Getter int age, String placeOfWork, String address) {
 
     /**
      * Метод для определения пользователей нужного возраста
-     * @param users - принимаемый список пользователей
+     *
+     * @param users      - принимаемый список пользователей
      * @param desiredAge - искомый возраст пользователей
      * @return - метод возвращает коллекцию Map, в которой ключ - переданный возраст, значение - список пользователей
-     *           данного возраста, если пользователей такого возраста нет, метод выведет в консоль сообщение:
-     *           "Пользователей искомого возраста не найдено."
+     * данного возраста, если пользователей такого возраста нет, метод выведет в консоль сообщение:
+     * "Пользователей искомого возраста не найдено."
      */
-    public static Map<Integer, List<User>> groupUsers (List <User> users, int desiredAge) {
-        Map<Integer, List<User>> resultMap = new HashMap<>();
+    public static Map<Integer, List<User>> groupUsers(List<User> users, int desiredAge) {
+        Map<Integer, List<User>> result = new HashMap<>();
         List<User> usersOfDesiredAge = new ArrayList<>();
 
         for (User currentUser : users) {
@@ -40,12 +29,12 @@ public class User {
 
         if (usersOfDesiredAge.isEmpty()) {
             System.out.println("Пользователей искомого возраста не найдено.");
-            return resultMap; // Map<desiredAge, null>
+            return result; // Map<desiredAge, null>
         }
 
-        resultMap.put(desiredAge, usersOfDesiredAge);
+        result.put(desiredAge, usersOfDesiredAge);
 
-        return resultMap;
+        return result;
     }
 
 }
