afa65c4558355866b4fbedc7683b4571aa021745
pazhik <untmum01@gmail.com>
2024-06-07 22:22:55 +0300

    Решение задачи регистрации пользователей

--- src/main/java/faang/school/godbless/task2/UserTest.java
+++ src/main/java/faang/school/godbless/task2/UserTest.java
@@ -1,7 +1,6 @@
-package faang.school.godbless.task1;
+package faang.school.godbless.task2;
 
 
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import java.util.ArrayList;
@@ -9,24 +8,26 @@ import java.util.List;
 import java.util.Map;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 class UserTest {
 
     private final List<User> users = new ArrayList<>();
 
-    @BeforeEach
-    void setUp() {
-        User user1 = new User("Vova1", 23, "smth1", "smth1");
-        User user2 = new User("Vova2", 23, "smth2", "smth2");
-        User user3 = new User("Vova3", 22, "smth3", "smth3");
-        users.add(user1);
-        users.add(user2);
-        users.add(user3);
+    @Test
+    void constructorTest() {
+        assertThrows(IllegalArgumentException.class, () -> new User("Vova", 23, "", "London"));
     }
 
     @Test
     void groupUser() {
+        User user1 = new User("Vova", 23, "Google", "London");
+        User user2 = new User("Boba", 23, "Uber", "New York");
+        User user3 = new User("Biba", 22, "Amazon", "Amsterdam");
+        users.add(user1);
+        users.add(user2);
+        users.add(user3);
         Map<Integer, List<User>> usersMap = User.groupUser(users);
         assertTrue(usersMap.keySet().containsAll(List.of(22, 23)));
         assertEquals(usersMap.get(22).size(), 1);
