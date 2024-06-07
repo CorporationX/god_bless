20f16852b34de397e62bd383260cfb470f493719
cirill98 <100434317+cirill98@users.noreply.github.com>
2024-06-07 12:28:39 +0500

    Переместил проект в отдельную папку

--- src/main/java/faang/school/godbless/Application.java
+++ src/main/java/faang/school/godbless/Application.java
@@ -2,13 +2,6 @@ package faang.school.godbless;
 
 public class Application {
     public static void main(String[] args) {
-        User user = new User("AAA", 23, "Uber", "London");
-        System.out.println(user);
-        User user1 = new User("AAA", 10, "Uber", "London");
-        System.out.println(user1);
-        User user2 = new User("", 10, "Uber", "London");
-        System.out.println(user2);
-        User user3 = new User(" ", 10, "Uber", "London");
-        System.out.println(user3);
+
     }
 }
---
+++ src/main/java/faang/school/godbless/UserRegistr/Main.java
@@ -0,0 +1,10 @@
+package faang.school.godbless.UserRegistr;
+
+public class Main {
+    public static void main(String[] args) {
+        Users users1 = new Users("AAA", 23, "Uber", "London");
+        Users users2 = new Users("AAA", 10, "Uber", "London");
+        Users users3 = new Users("", 10, "Uber", "London");
+        Users users4 = new Users(" ", 10, "Uber", "London");
+    }
+}
