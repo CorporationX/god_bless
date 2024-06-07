c10998ae00e8df614fbd84c9d0eae33bd102409b
cirill98 <100434317+cirill98@users.noreply.github.com>
2024-06-08 01:06:14 +0500

    Готово

--- src/main/java/faang/school/godbless/Application.java
+++ src/main/java/faang/school/godbless/Application.java
@@ -1,9 +1,5 @@
 package faang.school.godbless;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Set;
-
 public class Application {
     public static void main(String[] args) {
         System.out.println("Hello world!");
