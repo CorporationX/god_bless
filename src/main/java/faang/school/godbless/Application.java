e26138240515f807920d178a0679608eae9344a6
Kilotas <Alendos87@gmail.com>
2024-06-07 16:35:00 +0500

    Решение задачи с лучником и войном

--- src/main/java/faang/school/godbless/Application.java
+++ src/main/java/faang/school/godbless/Application.java
@@ -1,16 +1,41 @@
 package faang.school.godbless;
 
-import static faang.school.godbless.Example.reverse;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 
 public class Application {
-    public static void main(String[] args) {
-        int nums[] = {1, 4, 6, 7};
-        int resultNums[];
 
-        resultNums = reverse(nums);
-        System.out.print("Массив: ");
-        for(int i = 0; i < resultNums.length; i++){
-            System.out.print(resultNums[i] + " ");
-        }
+
+    public static void main(String[] args) {
+        List<Student> students = new ArrayList<>();
+        Student student1 = new Student("Hermione Granger", "Gryffindor", 1);
+        Student student2 = new Student("Harry Pooter", "Gryffindor", 1);
+        Student student3 = new Student("Ron Weasley", "Gryffindor", 1);
+
+        Student student4 = new Student("Draco Malfoy", "Slytherin", 1);
+
+        Student.addStudent(students, student1);
+        Student.addStudent(students, student2);
+        Student.addStudent(students, student3);
+        Student.addStudent(students, student4);
+
+        System.out.println("Студенты");
+        System.out.println(students);
+
+
+        Student.removeStudent(students, student2);
+
+        System.out.println(students);
+
+        List<Student> searchedStudents = Student.getStudents(students, "Slytherin", 1);
+        System.out.println(searchedStudents);
+
+        Map<String, List<Student>> groupedStudents = Student.StudentsHash(students);
+        System.out.println(groupedStudents);
+
     }
+
 }
