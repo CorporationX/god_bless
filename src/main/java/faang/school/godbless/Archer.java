6b3ec145484bc697e74dd3258f482e175cb0ab77
V1nGeri|Artem Radkevich <artemka20040613@gmail.com>
2024-06-07 17:17:09 +0300

    Переписал конструктор

--- src/main/java/faang/school/godbless/Archer.java
+++ src/main/java/faang/school/godbless/Archer.java
@@ -2,12 +2,9 @@ package faang.school.godbless;
 
 
 public class Archer extends Character {
-    private static final int DEFAULT_STRENGTH = 3;
-    private static final int DEFAULT_AGILITY = 10;
-    private static final int DEFAULT_INTELLIGENCE = 5;
 
     public Archer(String name) {
-        super(name, DEFAULT_STRENGTH, DEFAULT_AGILITY, DEFAULT_INTELLIGENCE);
+        super(name, 3, 10, 5);
     }
 
     @Override
--- src/main/java/faang/school/godbless/Warrior.java
+++ src/main/java/faang/school/godbless/Warrior.java
@@ -3,13 +3,8 @@ package faang.school.godbless;
 
 public class Warrior extends Character {
 
-    private static final int DEFAULT_STRENGTH = 10;
-    private static final int DEFAULT_AGILITY = 5;
-    private static final int DEFAULT_INTELLIGENCE = 3;
-
-
     public Warrior(String name) {
-        super(name, DEFAULT_STRENGTH, DEFAULT_AGILITY, DEFAULT_INTELLIGENCE);
+        super(name, 10, 5, 3);
 
     }
 
