e25ea782d797919c55ed700d634e2f0cb952292b
Max Prigonyuk <kael.smr@gmail.com>
2024-06-08 00:09:13 +0400

    Java HashMap: Двойной кэш-кэш https://faang-school.atlassian.net/browse/BJS2-7958

---
+++ src/main/java/faang/school/godbless/Student.java
@@ -0,0 +1,16 @@
+package faang.school.godbless;
+
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.ToString;
+
+@AllArgsConstructor
+@Getter
+@ToString
+@EqualsAndHashCode
+public class Student {
+    private int id;
+    private String name;
+
+}
