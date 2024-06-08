d4a31732464a2e6b5d8670dee0cd117ea359b3c2
V1nGeri|Artem Radkevich <artemka20040613@gmail.com>
2024-06-08 13:35:39 +0300

    BJS2-8068 Java HashMap: Игра престолов

--- src/main/java/faang/school/godbless/Application.java
+++ src/main/java/faang/school/godbless/Application.java
@@ -1,16 +1,50 @@
 package faang.school.godbless;
 
-import static faang.school.godbless.Example.reverse;
+import java.util.HashMap;
+import java.util.Map;
 
 public class Application {
     public static void main(String[] args) {
-        int nums[] = {1, 4, 6, 7};
-        int resultNums[];
 
-        resultNums = reverse(nums);
-        System.out.print("Массив: ");
-        for(int i = 0; i < resultNums.length; i++){
-            System.out.print(resultNums[i] + " ");
+        Map<String, House> houses = new HashMap<>();
+        houses.put("House Stark", new House("Stark", "Wolf"));
+        houses.put("House Tully", new House("Tully", "Fish"));
+        houses.put("House Targaryen", new House("Targaryen", "Dragon"));
+
+
+        addNewHouse(houses, "Lannister", "Lion");
+        printAllHouses(houses);
+
+        System.out.println("_________________________________");
+
+        deleteHouse(houses, "House Tully");
+        printAllHouses(houses);
+
+        System.out.println("_________________________________");
+
+        printHouseByName(houses, "House Targaryen");
+    }
+
+    private static void addNewHouse(Map<String, House> houses, String name, String sigil) {
+        houses.put("House " + name, new House(name, sigil));
+
+    }
+
+    private static void deleteHouse(Map<String, House> houses, String name) {
+        houses.remove(name);
+    }
+
+    private static void printAllHouses(Map<String, House> houses) {
+        for (Map.Entry<String, House> entry : houses.entrySet()) {
+            System.out.println("House: " + entry.getValue().getName() + " | " + "Sigil: " + entry.getValue().getSigil());
+        }
+    }
+
+    private static void printHouseByName(Map<String, House> houses, String name) {
+        try {
+            System.out.println("House " + houses.get(name).getName() + " | " + "House Sigil: " + houses.get(name).getSigil());
+        } catch (Exception e) {
+            System.out.println("Exception: " + e.getMessage());
         }
     }
 }
