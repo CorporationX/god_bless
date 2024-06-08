d0fd0d957d1f001bb53e0f65c647180cff9886ed
Nikita Volzhin <vlzh.nik@mail.ru>
2024-06-08 18:21:05 +0500

    Java HashMap: Поисковый мини-движок Google

--- src/main/java/faang/school/godbless/Main.java
+++ src/main/java/faang/school/godbless/Main.java
@@ -5,10 +5,8 @@ import lombok.NonNull;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashMap;
-import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
-import java.util.Set;
 
 public class Main {
     private static final Map<String, List<WebPage>> webPagesByKeyword = new HashMap<>();
