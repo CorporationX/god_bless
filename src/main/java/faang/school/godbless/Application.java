3cd7999de7ae1d17562adc15c3bfe9dc352815b0
Danya Ashihin <danyadefault@gmail.com>
2024-06-08 10:50:10 +0300

    Merge remote-tracking branch 'origin/danya9375-BJS2-7166' into danya9375-BJS2-7166
    # Conflicts:
    #	src/main/java/faang/school/godbless/Application.java

--- src/main/java/faang/school/godbless/Application.java
+++ src/main/java/faang/school/godbless/Application.java
@@ -8,22 +8,5 @@ import java.util.Map;
 import java.util.Set;
 
 public class Application {
-    public static void main(String[] args) {
 
-    }
-
-    public Map<User, String> findHobbyLovers(List<User> users, Set<String> activitys) {
-        Map<User, String> map = new HashMap<>();
-        for (User user : users) {
-
-            for (String activity : user.getActivitys()) {
-                if (activitys.contains(activity)) {
-                    map.put(user, activity);
-                    break;
-                }
-            }
-        }
-
-        return map;
-    }
 }
