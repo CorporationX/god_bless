becd75112382e548eaf626c0be5c609df4e41336
Kilotas <Alendos87@gmail.com>
2024-06-08 12:35:19 +0500

    Решение задачи на прогулы

--- src/main/java/faang/school/godbless/Application.java
+++ src/main/java/faang/school/godbless/Application.java
@@ -1,16 +1,41 @@
 package faang.school.godbless;
 
-import static faang.school.godbless.Example.reverse;
+
+import java.util.HashMap;
 
 public class Application {
     public static void main(String[] args) {
-        int nums[] = {1, 4, 6, 7};
-        int resultNums[];
+        HashMap<String, WeatherData> weatherData = new HashMap<>();
 
-        resultNums = reverse(nums);
-        System.out.print("Массив: ");
-        for(int i = 0; i < resultNums.length; i++){
-            System.out.print(resultNums[i] + " ");
-        }
+        WeatherData data1 = new WeatherData("Almaty", 22, 70);
+        WeatherData data2 = new WeatherData("Moscow", 25, 65);
+
+
+        WeatherData.updateWeatherData(weatherData, "Berlin", data1);
+        WeatherData.updateWeatherData(weatherData, "Tokyo", data2);
+
+        System.out.println("Все города мапы");
+        WeatherData.printAllCities(weatherData);
+
+        System.out.println("Получение по getWeatherData");
+        System.out.println(WeatherData.getWeatherData(weatherData, "Berlin"));
+
+        System.out.println("Получение по неизвестному городу");
+        System.out.println(WeatherData.getWeatherData(weatherData, "Chicago"));
+
+
+        WeatherData newData = new WeatherData("New York", 20, 75);
+        WeatherData.updateWeatherData(weatherData, "New York", newData);
+
+        WeatherData.removeWeatherData(weatherData, "Los Angeles");
+
+        System.out.println("Вывод списка всех городов");
+        WeatherData.printAllCities(weatherData);
+
+        System.out.println("Информация о Берлине");
+        System.out.println(WeatherData.getWeatherData(weatherData, "Berlin"));
+
+        System.out.println("Информация Los Angeles:");
+        System.out.println(WeatherData.getWeatherData(weatherData, "Los Angeles"));
     }
 }
