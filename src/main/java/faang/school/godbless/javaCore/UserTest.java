afbd8e377f6534009f7e594b949a9e564f1e09dd
Caveri95 <ura22051995@gmail.com>
2024-06-09 01:18:45 +0300

    добавлен метод валидации в конструктор

---
+++ src/main/java/faang/school/godbless/javaCore/UserTest.java
@@ -0,0 +1,17 @@
+package faang.school.godbless.javaCore;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class UserTest {
+    @Test
+    void validateUserTest() {
+        assertAll(
+                () -> assertThrows(IllegalArgumentException.class, () -> new User("", 19, "Google", "New York")),
+                () -> assertThrows(IllegalArgumentException.class, () -> new User("Name", 10, "Uber", "London")),
+                () -> assertThrows(IllegalArgumentException.class, () -> new User("Name", 19, "Ozon", "Amsterdam")),
+                () -> assertThrows(IllegalArgumentException.class, () -> new User("Name", 19, "Amazon", "Berlin"))
+        );
+    }
+}
