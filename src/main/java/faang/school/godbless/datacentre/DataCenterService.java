330ed9da073188f8028c13c08d3ff6b51b077503
npopov_ <lkn_popov@mail.ru>
2024-06-08 17:59:52 +0300

    BJS2-8546 добавлено решение задачи

--- src/main/java/faang/school/godbless/datacentre/DataCenterService.java
+++ src/main/java/faang/school/godbless/datacentre/DataCenterService.java
@@ -1,13 +1,21 @@
 package faang.school.godbless.datacentre;
 
 
+import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
 
-public class DataCenterService {
+import static java.util.Collections.sort;
+
+public class DataCenterService implements OptimizationStrategy {
     private DataCenter dataCenter;
 
     public DataCenterService() {
         dataCenter = new DataCenter();
+    }
+
+    public DataCenter getDataCenter() {
+        return dataCenter;
     }
 
     public void addServer(Server server) {
@@ -28,27 +36,63 @@ public class DataCenterService {
 
     public void allocateResources(ResourceRequest request) {
         /*
-         Выбирается наименее загруженн сервер на момент прихода запроса и выделяются на нём ресурсы,
-         если у него их достаточно
+         Сортируется список серверов по загруженности в обратном порядке. Если первый сервер может обработать запрос -
+         запрос передаётся ему, если нет - то проверяется второй сервер и т.д.
          */
-        List<Server> servers = dataCenter.getServers();
-        Server minLoadServer = servers.get(0);
-        for (int i = 1; i < servers.size(); i++) {
-            if (servers.get(i).getLoad() < minLoadServer.getLoad() ) {
-                minLoadServer = servers.get(i);
-            }
-        }
 
-        if (request.load() + minLoadServer.getLoad() <= minLoadServer.getEnergyConsumption()) {
-            minLoadServer.increaseLoad(request.load());
-            dataCenter.getRequestServer().put(request.id(), minLoadServer);
-        } else {
-            System.out.println("Дата-центр перегружен, вернитесь позднее");
+        for (Server server : dataCenter.getSortServer()) {
+            if (request.load() + server.getLoad() <= server.getMaxLoad()) {
+                dataCenter.allocateResourcesOnServer(request, server);
+                return;
+            }
         }
     }
 
     public void releaseResources(ResourceRequest request) {
-        dataCenter.getRequestServer().get(request.id()).decreaseLoad(request.load());
-        dataCenter.getRequestServer().remove(request.id());
+        dataCenter.releaseResourcesOnServer(request, dataCenter.getRequestServer().get(request));
+    }
+
+    @Override
+    public void optimize(DataCenter dataCenter) {
+        /*
+         Оптимизация состоит в следующем - перенести как можно больше запросов на сервера с низкой макс. загрузкой,
+         чтобы  высвободить как можно больше у.е. ресурсов на серверах с большиой макс. загрузков, чтобы была
+         возможность обработать тяжелые запросы
+         */
+        List<Server> servers = dataCenter.getSortServer();
+        for (int i = 0; i < servers.size() - 1; i++) {
+            Server server = servers.get(i);
+            if (server.getLoad() < server.getMaxLoad()) {
+                Server nextServer = servers.get(i + 1);
+
+                List<ResourceRequest> requests = dataCenter.getServerRequests().get(nextServer);
+                if (requests != null) {
+                    requests.sort(new Comparator<ResourceRequest>() {
+                        @Override
+                        public int compare(ResourceRequest o1, ResourceRequest o2) {
+                            if (o1.load() > o2.load()) {
+                                return -1;
+                            } else if (o1.load() < o2.load()) {
+                                return 1;
+                            } else {
+                                return 0;
+                            }
+                        }
+                    });
+
+                    for (ResourceRequest request : requests) {
+                        if (server.getLoad() + request.load() <= server.getMaxLoad()) {
+                            dataCenter.allocateResourcesOnServer(request, server);
+                            dataCenter.releaseResourcesOnServer(request, nextServer);
+
+                            if (server.getLoad() == server.getMaxLoad()) {
+                                break;
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
     }
 }
--- src/main/java/faang/school/godbless/datacentre/Server.java
+++ src/main/java/faang/school/godbless/datacentre/Server.java
@@ -1,13 +1,15 @@
 package faang.school.godbless.datacentre;
 
 public class Server {
+    private int id;
     private double load;
     private double maxLoad;
     private double energyConsumption;
 
     public Server() {}
 
-    public Server(double load, double maxLoad, double energyConsumption) {
+    public Server(int id, double load, double maxLoad, double energyConsumption) {
+        this.id = id;
         this.load = load;
         this.maxLoad = maxLoad;
         this.energyConsumption = energyConsumption;
@@ -25,11 +27,40 @@ public class Server {
         return load;
     }
 
+    public double getMaxLoad() {
+        return maxLoad;
+    }
+
     public void setEnergyConsumption(double energyConsumption) {
         this.energyConsumption = energyConsumption;
     }
 
     public double getEnergyConsumption () {
         return energyConsumption;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Server server = (Server) o;
+
+        return id == server.id;
+    }
+
+    @Override
+    public int hashCode() {
+        return id;
+    }
+
+    @Override
+    public String toString() {
+        return "Server{" +
+                "id=" + id +
+                ", load=" + load +
+                ", maxLoad=" + maxLoad +
+                ", energyConsumption=" + energyConsumption +
+                '}';
     }
 }
