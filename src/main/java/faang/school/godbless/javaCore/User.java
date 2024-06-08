3e399b0bf29180abc2b5641428f86267ecdd5457
Caveri95 <ura22051995@gmail.com>
2024-06-09 00:22:35 +0300

    Создал класс юзер с методом группировки в мапу по возрасту

---
+++ src/main/java/faang/school/godbless/javaCore/User.java
@@ -0,0 +1,31 @@
+package faang.school.godbless.javaCore;
+
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@Getter
+@Setter
+@EqualsAndHashCode
+@ToString
+@AllArgsConstructor
+public class User {
+
+    private String name;
+    private int age;
+    private String workPlace;
+    private String address;
+
+    public static Map<Integer, List<User>> groupUsers(List<User> userList) {
+        Map<Integer, List<User>> userMap = new HashMap<>();
+        userList.forEach(user -> userMap.computeIfAbsent(user.getAge(), v -> new ArrayList<>()).add(user));
+        return userMap;
+    }
+}
