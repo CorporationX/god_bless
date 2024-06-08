e939fe87cde335d3c7cd2c8436b0441562d12229
Andrey Kuzmin <Andreytko@mail.ru>
2024-06-08 14:16:58 +0300

    corrected 3rd

--- src/main/java/faang/school/godbless/Task8/Main.java
+++ src/main/java/faang/school/godbless/Task8/Main.java
@@ -7,6 +7,12 @@ import java.util.Map;
 
 public class Main {
 
+    public static void showMap(Map<Pair, List<Student>> table) {
+        for (Map.Entry entry: table.entrySet()) {
+            System.out.println(entry.getKey() + ": " + entry.getValue());
+        }
+    }
+
     public static HashMap<Pair, List<Student>> createTable(List<Student> students) {
         Map<Pair, List<Student>> table = new HashMap<>();
         for (Student student : students) {
@@ -21,14 +27,16 @@ public class Main {
         List<Student> students = new ArrayList<>();
         Student ivan = new Student("Ivan", "Math", 1);
         Student andrey = new Student("Andrey", "Math", 2);
-        Student john = new Student("John", "Physics", 3);
-        Student jane = new Student("Jane", "physics", 2);
+        Student john = new Student("John", "Physics", 2);
+        Student jane = new Student("Jane", "Physics", 2);
+        Student jordan = new Student("Jordan", "Math", 1);
         students.add(ivan);
         students.add(andrey);
         students.add(john);
         students.add(jane);
-
-
+        students.add(jordan);
+        Map<Pair, List<Student>> tableOfStudents = createTable(students);
+        showMap(tableOfStudents);
 
     }
 
