c66cd1b038e466e12bff4b19413741cf62dad665
Danya Ashihin <danyadefault@gmail.com>
2024-06-08 11:05:22 +0300

    Merge remote-tracking branch 'origin/danya9375-BJS2-7166' into danya9375-BJS2-7166

--- src/main/java/faang/school/godbless/Application.java
+++ src/main/java/faang/school/godbless/Application.java
@@ -1,12 +1,5 @@
 package faang.school.godbless;
 
-import faang.school.godbless.domain.User;
-
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-import java.util.Set;
-
 public class Application {
 
 }
