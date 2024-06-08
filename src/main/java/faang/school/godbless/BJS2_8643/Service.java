fa5b120a53d0b2bda95c7b1886bda971977c31fb
Roman <roma.koshetov@mail.ru>
2024-06-08 11:27:06 +0300

    BJS2-8643

---
+++ src/main/java/faang/school/godbless/BJS2_8643/Service.java
@@ -0,0 +1,8 @@
+package faang.school.godbless.BJS2_8643;
+
+// mock-class
+public class Service {
+
+    // заглушка
+    public static WeatherData getInfoFromService() {return new WeatherData("Moscow", 24, 27);}
+}
