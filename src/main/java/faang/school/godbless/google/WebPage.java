86fa196528f99ed76ac79410b4618615540af4e4
npopov_ <lkn_popov@mail.ru>
2024-06-08 21:02:24 +0300

    BJS2-8435 добавлено решение задачи

---
+++ src/main/java/faang/school/godbless/google/WebPage.java
@@ -0,0 +1,25 @@
+package faang.school.godbless.google;
+
+import java.util.Objects;
+
+public record WebPage(String url, String title, String content) {
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        WebPage webPage = (WebPage) o;
+
+        return Objects.equals(url, webPage.url);
+    }
+
+    @Override
+    public int hashCode() {
+        return url != null ? url.hashCode() : 0;
+    }
+
+    public WebPage(String url) {
+        this(url, "", "");
+    }
+}
