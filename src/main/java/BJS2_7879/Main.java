6203fe8ff69c91eff7f2e50eccda13d4027dbd3c
vitalij_rusakevic <vvitalijj@gmail.com>
2024-06-08 17:57:02 +0300

    Added: User validation and Main.class

---
+++ src/main/java/BJS2_7879/Main.java
@@ -0,0 +1,9 @@
+package BJS2_7879;
+
+public class Main {
+    public static void main(String[] args) {
+        User user = new User("as", 18, "google", "london");
+
+        System.out.println(user);
+    }
+}
---
+++ src/main/java/BJS2_7879/User.java
@@ -0,0 +1,38 @@
+package BJS2_7879;
+
+import lombok.Data;
+import lombok.Getter;
+
+import java.util.Set;
+
+@Getter
+@Data
+public class User {
+    private final Set<String> VALID_JOB = Set.of("Google", "Uber", "Amazon");
+    private final Set<String> VALID_ADDRESSES = Set.of("London", "New York", "Amsterdam");
+
+    private String name;
+    private int age;
+    private String placeOfWork;
+    private String address;
+
+    public User(String name, int age, String placeOfWork, String address) {
+        if (name.isEmpty() || name.isBlank()) {
+            throw new IllegalArgumentException("Error: name is empty or null");
+        }
+        if (age < 18) {
+            throw new IllegalArgumentException("not 18 years old");
+        }
+        if (!VALID_JOB.stream().anyMatch(placeOfWork::equalsIgnoreCase)) {
+            throw new IllegalArgumentException("Doesn't work here: " + VALID_JOB);
+        }
+        if (!(VALID_ADDRESSES.stream().anyMatch(address::equalsIgnoreCase))) {
+            throw new IllegalArgumentException("Not from:  " + VALID_ADDRESSES);
+        }
+
+        this.name = name;
+        this.age = age;
+        this.placeOfWork = placeOfWork;
+        this.address = address;
+    }
+}
