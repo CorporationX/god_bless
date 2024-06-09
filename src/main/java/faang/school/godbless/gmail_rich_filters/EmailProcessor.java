46723ef1ede01661c648cf00968e2593ed71b8a9
Rauan Kaztaev <rtishtyk@gmail.com>
2024-06-09 20:16:45 +0500

    Gmail Rich Filters

---
+++ src/main/java/faang/school/godbless/gmail_rich_filters/EmailProcessor.java
@@ -0,0 +1,34 @@
+package faang.school.godbless.gmail_rich_filters;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.function.Consumer;
+import java.util.function.Function;
+import java.util.function.Predicate;
+
+public class EmailProcessor {
+
+    public void processEmails(List<Email> letters, Predicate<Email> predicate, Consumer<Email> consumer, Function<Email, String> function) {
+        for (Email email : letters) {
+            System.out.println(predicate.test(email));
+            consumer.accept(email);
+            function.apply(email);
+        }
+    }
+
+    public static void main(String[] args) {
+        EmailProcessor emailProcessor = new EmailProcessor();
+
+        List<Email> emails = Arrays.asList(
+                new Email("Письмо 1", "Текст письма 1", false),
+                new Email("Письмо 2", "Текст письма 2", true),
+                new Email("Спам", "Текст спама", false)
+        );
+
+        Predicate<Email> importantFilter = email -> email.isImportant();
+        Consumer<Email> printEmail = email -> System.out.println("Обработано письмо: " + email.getSubject());
+        Function<Email, String> toUpperCase = email -> email.getBody().toUpperCase();
+
+        emailProcessor.processEmails(emails, importantFilter, printEmail, toUpperCase);
+    }
+}
