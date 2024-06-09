f4f74630d00ab2181aaccf7b68f31db50eb5f2dc
Vladimir Haiduk <nightshade2903@gmail.com>
2024-06-09 23:30:37 +0300

    Done task for review

---
+++ src/main/java/faang/school/godbless/group_users_by_id/Main.java
@@ -0,0 +1,17 @@
+package faang.school.godbless.group_users_by_id;
+
+import java.util.List;
+
+public class Main {
+    static List<User> users = List.of(
+            new User("Charli", 25, "Policeman"),
+            new User("Marty", 20, "Student"),
+            new User("Henry", 20, "Sportsman"),
+            new User("Maks", 45, "Builder"),
+            new User("Sanny", 22, "Sailor"),
+            new User("Hailey", 25, "Actress"));
+
+    public static void main(String[] args) {
+        User.groupUsers(users).entrySet().forEach(System.out::println);
+    }
+}
---
+++ src/main/java/faang/school/godbless/group_users_by_id/User.java
@@ -0,0 +1,26 @@
+package faang.school.godbless.group_users_by_id;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+public class User {
+    private String name;
+    private int age;
+    private String job;
+
+    public static Map<Integer, List<User>> groupUsers(List<User> users){
+        Map<Integer, List<User>> groupUsers = new HashMap<>();
+        if(users != null) {
+            users.stream().forEach(user -> groupUsers.computeIfAbsent(user.getAge(), e -> new ArrayList<>()).add(user));
+        }
+        return groupUsers;
+    }
+}
