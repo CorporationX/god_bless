4f1e9ca006d7ef025ada427e8bc951c89afa0c00
Max Shundrick <mas9l175@gmail.com>
2024-06-09 19:56:14 +0300

    Done with BJS2-10022

--- src/main/java/faang/school/godbless/Application.java
+++ src/main/java/faang/school/godbless/Application.java
@@ -1,8 +1,5 @@
 package faang.school.godbless;
 
-import java.util.ArrayList;
-import java.util.List;
-
 public class Application {
 
     public static void main(String[] args) {
--- src/main/java/faang/school/godbless/NotificationManager.java
+++ src/main/java/faang/school/godbless/NotificationManager.java
@@ -5,13 +5,16 @@ import lombok.Getter;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.function.Consumer;
+
 @Getter
 public class NotificationManager {
-    private Map<String,Consumer<Notification>> msgStorage = new HashMap<>();
-    public void registerHandler(String type, Consumer<Notification> notification){
-        msgStorage.put(type,notification);
+    private Map<String, Consumer<Notification>> msgStorage = new HashMap<>();
+
+    public void registerHandler(String type, Consumer<Notification> notification) {
+        msgStorage.put(type, notification);
     }
-    public void sendNotification(Notification notification){
+
+    public void sendNotification(Notification notification) {
         msgStorage.get(notification.getType()).accept(notification);
     }
 }
