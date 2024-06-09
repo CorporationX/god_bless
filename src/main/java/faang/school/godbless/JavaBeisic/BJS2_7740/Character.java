860b28da0c495b3c9b619fc1710d7cf03fb74938
0Miha0 <140548440+0Miha0@users.noreply.github.com>
2024-06-09 12:07:03 +0500

    https://faang-school.atlassian.net/browse/BJS2-7740

---
+++ src/main/java/faang/school/godbless/JavaBeisic/BJS2_7740/Character.java
@@ -0,0 +1,24 @@
+package faang.school.godbless.JavaBeisic.BJS2_7740;
+
+import lombok.Data;
+
+@Data
+public abstract class Character {
+    protected String name;
+    protected int strength;
+    protected int agility;
+    protected int intelligence;
+    protected int health = 100;
+
+    public Character(String name) {
+        this.name = name;
+    }
+    public Character(String name, int strength, int agility, int intelligence) {
+        this.name = name;
+        this.strength = strength;
+        this.agility = agility;
+        this.intelligence = intelligence;
+    }
+    public abstract void attack(Character opponent);
+
+}
