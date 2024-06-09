d24888420bcbfc6eb49a1227d84b8ff8d92a38ac
George <cradplay@gmail.com>
2024-06-08 21:38:13 -0400

    task + test

---
+++ src/main/java/faang/school/godbless/counting_absences/Student.java
@@ -0,0 +1,12 @@
+package faang.school.godbless.counting_absences;
+
+import lombok.AllArgsConstructor;
+import lombok.ToString;
+
+@ToString
+@AllArgsConstructor
+public class Student {
+    String name;
+    String faculty;
+    int year;
+}
