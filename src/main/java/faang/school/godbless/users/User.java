87a5e2f634837527fb43c747dfefc554df03ac96
allakamen <akamenskikh@gmail.com>
2024-06-09 16:23:15 +0200

    Task Коллекционируем пользователей

---
+++ src/main/java/faang/school/godbless/users/User.java
@@ -0,0 +1,38 @@
+package faang.school.godbless.users;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.Singular;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
+@Data
+@AllArgsConstructor
+public class User {
+
+    private int id;
+    private String name;
+    private int age;
+    @Singular
+    private Set<String> activities;
+
+
+    public static Map<User, String> findHobbyLovers(List<User> users, Set<String> activities) {
+        Map<User, String> hobbyLovers = new HashMap<>();
+        for (User user: users) {
+            for (String activity : activities) {
+                if (user.getActivities().contains(activity)) {
+                    hobbyLovers.putIfAbsent(user, activity);
+                }
+            }
+        }
+
+
+
+        return hobbyLovers;
+
+    }
+}
