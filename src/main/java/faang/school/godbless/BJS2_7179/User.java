ca35613bd51df66aa159a53c6cef44d4008d9a5e
DemureZ <lopatin.g.n@yandex.ru>
2024-06-09 11:05:19 +0300

    Исправление ошибок

--- src/main/java/faang/school/godbless/BJS2_7179/User.java
+++ src/main/java/faang/school/godbless/BJS2_7179/User.java
@@ -1,27 +1,25 @@
 package faang.school.godbless.BJS2_7179;
 
 import lombok.AllArgsConstructor;
-import lombok.ToString;
-
+import lombok.Data;
 import java.util.*;
-import java.util.stream.Collectors;
 
+@Data
 @AllArgsConstructor
-@ToString
 public class User {
     private int id;
     private String name;
     private int age;
     private Set<String> activities;
 
-    public static HashMap<User, String> findHobbyLovers(List<User> users, Set<String> activities) {
-        HashMap<User, String> result = new HashMap<>();
+    public static Map<User, String> findHobbyLovers(List<User> users, Set<String> activities) {
+        Map<User, String> result = new HashMap<>();
         for (User user : users) {
-            Set<String> cross = new HashSet<>(user.activities);
-            cross.retainAll(activities);
-            var crossValue = cross.stream().findFirst();
-            if (!crossValue.isEmpty()) {
-                result.put(user, crossValue.toString());
+            for (String activity : user.activities) {
+                if (activities.contains(activity)) {
+                    result.put(user, activity);
+                    break;
+                }
             }
         }
         return result;
