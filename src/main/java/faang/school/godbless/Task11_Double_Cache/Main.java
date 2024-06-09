4bd9f9de5af6d2f4724dee348a14b36f356f8320
Andrey Kuzmin <Andreytko@mail.ru>
2024-06-09 17:28:47 +0300

    done points 1-2

---
+++ src/main/java/faang/school/godbless/Task11_Double_Cache/Main.java
@@ -0,0 +1,11 @@
+package faang.school.godbless.Task11_Double_Cache;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+
+
+    }
+
+}
---
+++ src/main/java/faang/school/godbless/Task11_Double_Cache/Student.java
@@ -0,0 +1,12 @@
+package faang.school.godbless.Task11_Double_Cache;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@AllArgsConstructor
+@Getter
+
+public class Student {
+    private int id;
+    private String name;
+}
