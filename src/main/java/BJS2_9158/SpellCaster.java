e171d15f6eb74ce74f9a0fa2c4c4d702e904aed1
vitalij_rusakevic <vvitalijj@gmail.com>
2024-06-10 00:26:52 +0300

    Added: Two class and FunctionalInterface

--- src/main/java/BJS2_9158/SpellCaster.java
+++ src/main/java/BJS2_9158/SpellCaster.java
@@ -1,9 +1,7 @@
 package BJS2_9158;
 
-import ch.qos.logback.core.joran.spi.NoAutoStartUtil;
-
 public class SpellCaster {
-    public void cast(String spells, SpellAction spellAction){
+    public void cast(String spells, SpellAction spellAction) {
         System.out.println(spellAction.cast(spells));
     }
 }
