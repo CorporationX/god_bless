9fcc0a9d7766df1c92bda16b30ee77088e39840c
pazhik <untmum01@gmail.com>
2024-06-09 13:49:43 +0300

    Решение задачи Разбираем склад Amazon

--- src/main/java/faang/school/godbless/Application.java
+++ src/main/java/faang/school/godbless/Application.java
@@ -1,7 +1,35 @@
 package faang.school.godbless;
 
+import faang.school.godbless.task11.Product;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
 public class Application {
+
+    public static HashMap<String, List<Product>> createProductsMap(Set<Product> products) {
+        HashMap<String, List<Product>> productsMap = new HashMap<>();
+        for (Product product : products) {
+            productsMap.computeIfAbsent(product.getCategory(), k -> new ArrayList<>()).add(product);
+        }
+        return productsMap;
+    }
+
+    public static void infoProducts(HashMap<String, List<Product>> products) {
+        for (Map.Entry<String, List<Product>> entry: products.entrySet()) {
+            System.out.println("Category " + entry.getKey());
+            entry.getValue().forEach(value -> System.out.println("Value " + value));
+        }
+    }
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Product product1 = new Product(1, "iphone 11", "phones");
+        Product product2 = new Product(2, "iphone 13", "phones");
+        Product product3 = new Product(3, "asus tuf 17", "laptop");
+        HashMap<String, List<Product>> products = createProductsMap(Set.of(product1, product2, product3));
+        infoProducts(products);
     }
 }
---
+++ src/main/java/faang/school/godbless/task11/Product.java
@@ -0,0 +1,12 @@
+package faang.school.godbless.task11;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class Product {
+    private Integer id;
+    private String name;
+    private String category;
+}
