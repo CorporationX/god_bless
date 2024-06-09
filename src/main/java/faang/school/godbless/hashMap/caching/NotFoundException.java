d356a8815bfaddfa43aa2decd12702ed91b0a6a7
romax <maksimovskii.roman@yandex.ru>
2024-06-09 17:37:20 +0300

    Задание BJS2-8669 "Кэшируем, кэшируем" выполнено

---
+++ src/main/java/faang/school/godbless/hashMap/caching/NotFoundException.java
@@ -0,0 +1,10 @@
+package faang.school.godbless.hashMap.caching;
+
+public class NotFoundException extends RuntimeException {
+    public NotFoundException() {
+    }
+
+    public NotFoundException(String s) {
+        super(s);
+    }
+}
