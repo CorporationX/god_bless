cb2c9c72aaf743a67c7e868fe943764b0e21ce1b
Max Shundrick <mas9l175@gmail.com>
2024-06-09 23:20:50 +0300

    Done with BJS2-9543

--- src/main/java/faang/school/godbless/Application.java
+++ src/main/java/faang/school/godbless/Application.java
@@ -1,19 +1,19 @@
 package faang.school.godbless;
 
-import java.util.function.Function;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.function.BiConsumer;
 
 public class Application {
 
     public static void main(String[] args) {
-        Image image = new Image("File.png", "Original Image");
-        ImageProcessor imageProcessor = new ImageProcessor();
-        Filter grayscaleFilter = new Filter("_grayscale", "Filter: Grayscale");
-        Filter sepiaFilter = new Filter("_sepia", "Filter: sepia");
-        Filter vignetteFilter = new Filter("_vignette", "Filter: Vignette");
-        Function<Image, Image> combineFilters = imageProcessor.combineFilters(sepiaFilter, vignetteFilter, grayscaleFilter);
-        imageProcessor.applyFilter(image, grayscaleFilter.getFilter());
-        imageProcessor.applyFilter(image, sepiaFilter.getFilter());
-        imageProcessor.applyFilter(image, vignetteFilter.getFilter());
-        imageProcessor.applyFilter(image, combineFilters);
+        DictionaryProcessor dictionaryProcessor = new DictionaryProcessor();
+        Map<String, String> dictionary = new HashMap<>();
+        BiConsumer<String, String> addWordToDictionary = dictionary::put;
+        dictionaryProcessor.processWord("Привет", "Hello", addWordToDictionary);
+        dictionaryProcessor.processWord("Небо", "Sky", addWordToDictionary);
+        dictionaryProcessor.processWord("Трава", "Grass", addWordToDictionary);
+        dictionaryProcessor.processWord("Нравится", "Like", addWordToDictionary);
+        System.out.println(dictionary);
     }
 }
