7b3eed5d11f4307fe1b5250a484b23034f20933d
Andrey Kuzmin <Andreytko@mail.ru>
2024-06-09 17:32:14 +0300

    created 2 hashMaps;

--- src/main/java/faang/school/godbless/Task11_Double_Cache/Main.java
+++ src/main/java/faang/school/godbless/Task11_Double_Cache/Main.java
@@ -1,9 +1,15 @@
 package faang.school.godbless.Task11_Double_Cache;
 
+import java.util.List;
+import java.util.Map;
+import java.util.HashMap;
+
 public class Main {
 
     public static void main(String[] args) {
 
+    Map<Student, Map<Subject, Integer>> marks = new HashMap<>();
+    Map<Subject, List<Student>> attenders = new HashMap<>();
 
 
     }
