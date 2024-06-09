d63a7b0635ab3960f3762b7e82fa448ec338f7c9
Иван Шилов <ivshvl39@gmail.com>
2024-06-09 12:08:21 +0300

    Поисковой мини-движок Google

---
+++ src/main/java/faang/school/godbless/task11/WebPage.java
@@ -0,0 +1,12 @@
+package faang.school.godbless.task11;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class WebPage {
+    private String url;
+    private String title;
+    private String content;
+}
