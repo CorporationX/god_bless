8356bd2d06ffaa0bf9a12da9c6b533d174c7aec3
ravshanbekn <ravshanbeknabiyev.dev@gmail.com>
2024-06-09 14:14:33 +0500

    Библиотека Вестеросса Fixed anti-patterns

--- src/main/java/faang/school/godbless/vesterros_library/Library.java
+++ src/main/java/faang/school/godbless/vesterros_library/Library.java
@@ -1,13 +1,17 @@
 package faang.school.godbless.vesterros_library;
 
+import lombok.Getter;
+import lombok.Setter;
 import lombok.extern.slf4j.Slf4j;
 
 import java.util.HashMap;
 import java.util.Map;
 
 @Slf4j
+@Getter
+@Setter
 public class Library {
-    Map<Book, String> bookMap;
+    private Map<Book, String> bookMap;
 
     public Library() {
         this.bookMap = new HashMap<>();
@@ -18,11 +22,20 @@ public class Library {
     }
 
     public void deleteBookFromLibrary(String title, String author, int year) {
-        Book bookToDelete = new Book(title, author, year);
-        if (bookMap.containsKey(bookToDelete)) {
-            bookMap.remove(new Book(title, author, year));
+        Book book = null;
+        for (Map.Entry<Book, String> bookPositionEntry : bookMap.entrySet()) {
+            Book bookIteration = bookPositionEntry.getKey();
+            if (bookIteration.getTitle().equals(title) &&
+                    bookIteration.getAuthor().equals(author) &&
+                    bookIteration.getYear() == year) {
+                book = bookIteration;
+                break;
+            }
+        }
+        if (book == null) {
+            log.warn("Could find and remove book");
         } else {
-            log.warn("Could not find book: {}", bookToDelete);
+            bookMap.remove(book);
         }
     }
 
