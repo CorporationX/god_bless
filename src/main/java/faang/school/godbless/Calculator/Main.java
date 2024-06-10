9b318ec57cda74642473e6d191b07bcc98412f7f
cirill98 <100434317+cirill98@users.noreply.github.com>
2024-06-10 23:29:44 +0500

    Добавил пустой список и список с нулями

--- src/main/java/faang/school/godbless/Calculator/Main.java
+++ src/main/java/faang/school/godbless/Calculator/Main.java
@@ -6,12 +6,23 @@ import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
-        List<Integer> nums = List.of(4, 3, 5, 1);
-        System.out.println(sum(nums));
-        System.out.println(product(nums));
+        List<Integer> nums1 = List.of(4, 3, 5, 1);
+        System.out.println(sum(nums1));
+        System.out.println(product(nums1));
+
+        List<Integer> nums2 = List.of();
+        System.out.println(sum(nums2));
+        System.out.println(product(nums2));
+
+        List<Integer> nums3 = List.of(0, 0, 0);
+        System.out.println(sum(nums3));
+        System.out.println(product(nums3));
     }
 
     public static int calculate(List<Integer> in, Calculator<Integer, Integer, Integer> calculator) {
+        if (in.isEmpty()) {
+            throw new IllegalArgumentException("Список пустой");
+        }
         int result = in.get(0);
         for (int i = 1; i < in.size(); i++) {
             result = calculator.workCalculator(result, in.get(i));
