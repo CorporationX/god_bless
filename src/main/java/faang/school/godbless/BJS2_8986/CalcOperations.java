f954236628cae8f14cdc27d042248015bd1a19ad
Max.Shundrik <mas9l175@gmail.com>
2024-06-11 01:20:15 +0300

    Done with BJS2-8986

--- src/main/java/faang/school/godbless/BJS2_8986/CalcOperations.java
+++ src/main/java/faang/school/godbless/BJS2_8986/CalcOperations.java
@@ -3,8 +3,8 @@ package faang.school.godbless.BJS2_8986;
 import java.util.List;
 
 public class CalcOperations {
-    private void calculate(List<Integer> nums, Calculator calculator) throws IllegalArgumentException {
-        System.out.println(nums.stream().reduce((x, y) -> (Integer) calculator.evaluate(x, y)).get());
+    private void calculate(List<Integer> nums, Calculator<Integer, Integer> calculator) throws IllegalArgumentException {
+        System.out.println(nums.stream().reduce(calculator::evaluate).get());
     }
 
     public void product(List<Integer> nums) {
@@ -13,7 +13,7 @@ public class CalcOperations {
     }
 
     public void sum(List<Integer> nums) {
-        Calculator<Integer, Integer> multiplier = (x, y) -> x + y;
+        Calculator<Integer, Integer> multiplier = Integer::sum;
         calculate(nums, multiplier);
     }
 }
