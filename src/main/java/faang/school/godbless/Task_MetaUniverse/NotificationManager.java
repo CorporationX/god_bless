ead770d0e9c776547492ecc2c84e7a3175055e01
Andrey Kuzmin <Andreytko@mail.ru>
2024-06-11 00:01:19 +0300

    done points 1 - 2

---
+++ src/main/java/faang/school/godbless/Task_MetaUniverse/NotificationManager.java
@@ -0,0 +1,23 @@
+package faang.school.godbless.Task_MetaUniverse;
+
+import lombok.NoArgsConstructor;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.function.Consumer;
+
+@NoArgsConstructor
+public class NotificationManager {
+
+    private static final Map<String, Consumer<Notification>> noteService = new HashMap<>();
+
+    public void registerHandler(String notificationType, Consumer<Notification> consumer) {
+        noteService.put(notificationType, consumer);
+    }
+
+    public void sendNotification(Notification notification) {
+        System.out.println(noteService.get(notification.getType()));
+    }
+
+
+
+}
