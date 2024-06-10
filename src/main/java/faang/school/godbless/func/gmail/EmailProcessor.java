403a04834b9afda666627ba120003b2ae00a040d
Stanislav Ovsyankin <cancion2able@gmail.com>
2024-06-10 08:00:17 +0500

    Перевести реализацию в стрим

--- src/main/java/faang/school/godbless/func/gmail/EmailProcessor.java
+++ src/main/java/faang/school/godbless/func/gmail/EmailProcessor.java
@@ -1,11 +1,11 @@
 package faang.school.godbless.func.gmail;
 
-import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.function.Consumer;
 import java.util.function.Function;
 import java.util.function.Predicate;
+import java.util.stream.Collectors;
 
 public record EmailProcessor() {
 
@@ -13,13 +13,9 @@ public record EmailProcessor() {
         if (emails == null || emails.isEmpty()) {
             throw new IllegalArgumentException("Nothing to process here.");
         }
-        Map<String, String> importantBits = new HashMap<>();
-        emails.forEach(email -> {
-            if (filter.test(email)) {
-                handler.accept(email);
-                importantBits.put(email.subject(), transformer.apply(email));
-            }
-        });
-        return importantBits;
+        return emails.stream()
+                .filter(filter)
+                .peek(handler)
+                .collect(Collectors.toMap(Email::subject, transformer));
     }
 }
