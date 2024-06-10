8d4a4b6d443f4f6a2af0b0efda6dccf2d740fb23
Kilotas <Alendos87@gmail.com>
2024-06-10 14:03:11 +0500

    Merge pull request #5947 from CorporationX/Omarov-Alen-BJS2-7825
    Solved problem with git collision

--- src/main/java/faang/school/godbless/Application.java
+++ src/main/java/faang/school/godbless/Application.java
@@ -2,9 +2,6 @@ package faang.school.godbless;
 import java.util.HashMap;
 import java.util.Map;
 
-import static faang.school.godbless.Book.addBook;
-import static faang.school.godbless.Book.findBook;
-import static faang.school.godbless.Book.removeBook;
 
 public class Application {
     public static void main(String[] args) {
