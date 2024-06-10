367de7e69ca368ce4176e5baaffff10f865be8b1
Kilotas <Alendos87@gmail.com>
2024-06-10 14:02:56 +0500

    Merge pull request #5947 from CorporationX/Omarov-Alen-BJS2-7825
    Solved problem with git collision

--- src/main/java/faang/school/godbless/Application.java
+++ src/main/java/faang/school/godbless/Application.java
@@ -9,25 +9,7 @@ import static faang.school.godbless.Book.removeBook;
 public class Application {
     public static void main(String[] args) {
 
-        Map<Book, String> library = new HashMap<>();
+        System.out.println("Hello World!");
 
-        Book book1 = new Book("The song of ice and fire", "J. Martin", 2008);
-        Book book2 = new Book("Prisoner of Azkaban", "J. Rowling", 2005);
-
-
-        addBook(library, book1, "1");
-        addBook(library, book2, "2");
-
-
-        for (Map.Entry<Book, String> entry : library.entrySet()) {
-            System.out.println(entry.getKey() + " " + entry.getValue());
-        }
-
-
-        findBook(library, "Prisoner of Azkaban", "J. Rowling", 2005);
-        removeBook(library, "Prisoner of Azkaban", "J. Rowling", 2005);
-
-
-        System.out.println(library);
     }
 }
