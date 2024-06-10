2ca33a5528ff97f2cdff409f1232debb051759f3
George <cradplay@gmail.com>
2024-06-09 21:57:24 -0400

    refactored to use LinkedHashMap

--- src/main/java/faang/school/godbless/lru_cache/Main.java
+++ src/main/java/faang/school/godbless/lru_cache/Main.java
@@ -4,96 +4,32 @@ import lombok.ToString;
 
 import java.util.*;
 
-//Node class to construct double linked list
-class Node {
-    public Node (Data data) {
-        this.data = data;
-    }
-
-    @Override
-    public String toString() {
-        return data.toString();
-    }
-
-    public Data data;
-    public Node next;
-    public Node prev;
-}
 
 public class Main {
-    int CACHE_SIZE;
 //    db or some other type of storage
     List<Data> storage = new ArrayList<>();
-    Map<Integer, Node> cache = new HashMap<>();
-    Node head, tail;
+    LinkedHashMap<Integer, Data> cache;
 
     public Main(int cache_size) {
-        CACHE_SIZE = cache_size;
+        cache = new LinkedHashMap<>(cache_size, 1, true){
+            protected boolean removeEldestEntry(Map.Entry eldest){
+                return size() > cache_size;
+            }
+        };
     }
 
     public Data get(int key) {
-        Node d = cache.get(key);
-
-        if (d != null) {
-//            node is head
-            if(head.equals(d)) {
-                return d.data;
-            }
-
-            Node oldHead = head;
-            head = d;
-
-//            node is tail
-            if(tail.equals(d)) {
-                tail = tail.next;
-            }
-
-//            node in the middle, i guess could also be run as else in tail check
-            if(d.next != null && d.prev != null) {
-                d.prev.next = d.next;
-                d.next.prev = d.prev;
-            }
-
-            oldHead.next = d;
-            d.next = null;
-            d.prev = oldHead;
-
-            return d.data;
+        if(cache.containsKey(key)) {
+            return cache.get(key);
         }
-
 //        If data not found in cache find in storage and put in cache
         for (Data data : storage) {
             if (data.id == key) {
-                Node newNode = new Node(data);
-//                assigning for return value
-                d = newNode;
-
-                Node oldHead = head;
-                head = newNode;
-
-//                runs if there is something in cache already
-                if(oldHead != null) {
-                    oldHead.next = newNode;
-                    newNode.prev = oldHead;
-                }
-
-//                if no tail, set it to tail
-                if(tail == null) {
-                    tail = newNode;
-                }
-
-                cache.put(key, head);
+                cache.put(key, data);
+                return data;
             }
         }
-
-//        is cache is full remove oldest element
-        if (cache.size() > CACHE_SIZE) {
-            cache.remove(tail.data.id);
-            tail = tail.next;
-        }
-
-        if (d == null) { return null; }
-        return d.data;
+        return null;
     }
 
 //    Since not stated otherwise put doesnt update cache
@@ -101,27 +37,9 @@ public class Main {
         storage.add(data);
     }
 
-//    Prints head and tail nodes + outputs contents of a map
-//    Returns an array of size 2+CACHE_SIZE.  First two values are head and tail and other ones are ids in asc order
     public int[] getCacheStatus() {
-//        first 2 values head and tail id's
-        int[] arrayStatus = new int[2+CACHE_SIZE];
-        Arrays.fill(arrayStatus, -1);
-        if(head != null) {
-            arrayStatus[0] = head.data.id;
-        }
-        if(tail != null ) {
-            arrayStatus[1] = tail.data.id;
-        }
-
         System.out.println("===================");
-        System.out.println("Tail " + tail + "; Head " + head);
-        int i = 2;
-        for (Map.Entry<Integer, Node> entry : cache.entrySet()) {
-            System.out.println(entry.getKey() + " " + entry.getValue());
-            arrayStatus[i] = entry.getKey();
-            i++;
-        }
-        return arrayStatus;
+        System.out.println(cache);
+        return cache.keySet().stream().mapToInt(Integer::intValue).toArray();
     }
 }
--- src/test/java/faang/school/godbless/lru_cache/MainTest.java
+++ src/test/java/faang/school/godbless/lru_cache/MainTest.java
@@ -2,6 +2,7 @@ package faang.school.godbless.lru_cache;
 
 import org.junit.jupiter.api.Test;
 
+import java.util.Arrays;
 import java.util.Date;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -23,7 +24,7 @@ class MainTest {
         Main main = new Main(1);
         main.put(new Data(2, "test", new Date()));
         assertEquals(2, main.get(2).id);
-        assertArrayEquals(new int[]{2, 2, 2}, main.getCacheStatus());
+        assertArrayEquals(new int[]{2}, main.getCacheStatus());
     }
 
     @Test
@@ -33,14 +34,13 @@ class MainTest {
         for(int i = 0; i <= 4; i++) {
             main.put(new Data(i, "test", new Date()));
         }
-        assertArrayEquals(new int[]{-1, -1, -1, -1}, main.getCacheStatus());
         main.get(2);
         main.get(3);
-        assertArrayEquals(new int[]{3, 2, 2, 3}, main.getCacheStatus());;
+        assertArrayEquals(new int[]{2, 3}, main.getCacheStatus());;
         main.get(4);
-        assertArrayEquals(new int[]{4, 3, 3, 4}, main.getCacheStatus());
+        assertArrayEquals(new int[]{3, 4}, main.getCacheStatus());
         main.get(2);
-        assertArrayEquals(new int[]{2, 4, 2, 4}, main.getCacheStatus());
+        assertArrayEquals(new int[]{4, 2}, main.getCacheStatus());
     }
 
     @Test
@@ -50,35 +50,33 @@ class MainTest {
         for(int i = 0; i <= 7; i++) {
             main.put(new Data(i, "test", new Date()));
         }
-        assertArrayEquals(new int[]{-1, -1, -1, -1, -1}, main.getCacheStatus());
         main.get(2);
         main.get(3);
         main.get(5);
-        assertArrayEquals(new int[]{5, 2, 2, 3, 5}, main.getCacheStatus());
+        assertArrayEquals(new int[]{2, 3, 5}, main.getCacheStatus());
         main.get(4);
         main.get(2);
-        assertArrayEquals(new int[]{2, 5, 2, 4, 5}, main.getCacheStatus());
+        assertArrayEquals(new int[]{5, 4, 2}, main.getCacheStatus());
     }
 
     @Test
     public void testPutsLargest() {
-        System.out.println("************************************\n Test Puts Larger");
+        System.out.println("************************************\n Test Puts Largest");
         Main main = new Main(3);
         for(int i = 0; i <= 7; i++) {
             main.put(new Data(i, "test", new Date()));
         }
-        assertArrayEquals(new int[]{-1, -1, -1, -1, -1}, main.getCacheStatus());
         main.get(2);
         main.get(3);
         main.get(5);
         main.get(1);
         main.get(6);
-        assertArrayEquals(new int[]{6, 5, 1, 5, 6}, main.getCacheStatus());
+        assertArrayEquals(new int[]{5, 1, 6}, main.getCacheStatus());
         main.get(4);
         main.get(1);
         main.get(1);
         main.get(2);
-        assertArrayEquals(new int[]{2, 4, 1, 2, 4}, main.getCacheStatus());
+        assertArrayEquals(new int[]{4, 1, 2}, main.getCacheStatus());
     }
 
 }
