e1280a33b3e0e12eb1e8622c8aa7b808045bd595
Max.Shundrik <mas9l175@gmail.com>
2024-06-10 22:23:18 +0300

    Done with BJS-9965

---
+++ src/main/java/faang/school/godbless/MessageFilter.java
@@ -0,0 +1,8 @@
+package faang.school.godbless;
+
+public class MessageFilter {
+    public boolean filter (String message){
+        boolean isApplied = false;
+        return isApplied;
+    }
+}
---
+++ src/main/java/faang/school/godbless/MessageProcessor.java
@@ -0,0 +1,11 @@
+package faang.school.godbless;
+
+import java.util.List;
+
+public class MessageProcessor {
+    public void processMessage(String message, List<MessageFilter> filters){
+        filters.forEach(filter->{
+            filter.
+        });
+    }
+}
