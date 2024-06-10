ac4fd3505d508aae4e37829a75e6cd411d988ed7
Gevorg Sogomonian <a0964593@gmail.com>
2024-06-10 14:20:52 +0300

    First try

--- src/main/java/faang/school/godbless/Application.java
+++ src/main/java/faang/school/godbless/Application.java
@@ -1,16 +1,23 @@
 package faang.school.godbless;
 
-import static faang.school.godbless.Example.reverse;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.function.BiConsumer;
 
 public class Application {
     public static void main(String[] args) {
-        int nums[] = {1, 4, 6, 7};
-        int resultNums[];
+        DictionaryProcessor dictionaryProcessor = new DictionaryProcessor();
 
-        resultNums = reverse(nums);
-        System.out.print("Массив: ");
-        for(int i = 0; i < resultNums.length; i++){
-            System.out.print(resultNums[i] + " ");
-        }
+        // Создание словаря
+        Map<String, String> dictionary = new HashMap<>();
+
+        // Создание BiConsumer для сохранения слова и его перевода в словарь
+        BiConsumer<String, String> addWordToDictionary = (word, translation) -> dictionary.put(word, translation);
+        // Добавление слов и их переводов
+        dictionaryProcessor.processWord("привет", "hello", addWordToDictionary);
+        dictionaryProcessor.processWord("мир", "world", addWordToDictionary);
+        dictionaryProcessor.processWord("программирование", "programming", addWordToDictionary);
+
+        System.out.println("Словарь: " + dictionary);
     }
 }
