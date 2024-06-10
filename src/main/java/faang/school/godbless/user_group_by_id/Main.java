368b632d9523e6a34bb1fbbd51a6a708cbdc2279
shade2903 <nighshade2903@gmail.com>
2024-06-10 10:38:56 +0300

    add address in User

--- src/main/java/faang/school/godbless/user_group_by_id/Main.java
+++ src/main/java/faang/school/godbless/user_group_by_id/Main.java
@@ -3,10 +3,12 @@ package faang.school.godbless.user_group_by_id;
 import java.util.List;
 
 public class Main {
-    static List<User> users = List.of(new User("Charli", 25, "Policeman"),
-            new User("Marty", 20, "Student"), new User("Henry", 20, "Sportsman"),
-            new User("Maks", 45, "Builder"), new User("Sanny", 22, "Sailor"),
-            new User("Hailey", 25, "Actress"));
+    static List<User> users = List.of(new User("Charli", 25, "Policeman","Chikago"),
+            new User("Marty", 20, "Student", "Warsaw"),
+            new User("Henry", 20, "Sportsman", "Moscow"),
+            new User("Maks", 45, "Builder","Washington"),
+            new User("Sanny", 22, "Sailor","Berlin"),
+            new User("Hailey", 25, "Actress","Parish"));
 
     public static void main(String[] args) {
         User.groupUsers(users).entrySet().forEach(System.out::println);
--- src/main/java/faang/school/godbless/user_group_by_id/User.java
+++ src/main/java/faang/school/godbless/user_group_by_id/User.java
@@ -15,6 +15,7 @@ public class User {
     private String name;
     private int age;
     private String job;
+    private String address;
 
     public static Map<Integer, List<User>> groupUsers(List<User> users) {
         Map<Integer, List<User>> groupUsers = new HashMap<>();
