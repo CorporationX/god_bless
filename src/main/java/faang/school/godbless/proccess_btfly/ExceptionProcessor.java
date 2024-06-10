cf942c4aba72a45808e746d783add1bd55a1e2a2
ravshanbekn <ravshanbeknabiyev.dev@gmail.com>
2024-06-10 21:02:31 +0500

    Обрабатываем ошибки красиво + Format

--- src/main/java/faang/school/godbless/proccess_btfly/ExceptionProcessor.java
+++ src/main/java/faang/school/godbless/proccess_btfly/ExceptionProcessor.java
@@ -3,7 +3,7 @@ package faang.school.godbless.proccess_btfly;
 import java.util.function.Function;
 
 public class ExceptionProcessor {
-    public static <T, R> R process(T argument, Function<T,R> firstAction, ExceptionHandler<R> exceptionHandler) {
+    public static <T, R> R process(T argument, Function<T, R> firstAction, ExceptionHandler<R> exceptionHandler) {
         try {
             return firstAction.apply(argument);
         } catch (Exception exception) {
