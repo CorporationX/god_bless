7cf73f8bed044c65b6fb4e4c62845ef1b679d2ee
Arman Bistibekov <bistibekov.a@gmail.com>
2024-06-10 11:55:27 -0400

    Created two classe: Notifacation and NotificationManager;

---
+++ src/main/java/faang/school/godbless/Notification.java
@@ -0,0 +1,11 @@
+package faang.school.godbless;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class Notification {
+    private String type;
+    private String message;
+}
---
+++ src/main/java/faang/school/godbless/NotificationManager.java
@@ -0,0 +1,20 @@
+package faang.school.godbless;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.function.Consumer;
+
+public class NotificationManager {
+    private Map<String, Consumer<Notification>> consumerMap = new HashMap<>();
+
+    public void registerHandler(String notificationId, Consumer<Notification> notification) {
+        consumerMap.put(notificationId, notification);
+    }
+
+    public void sendNotification(Notification notification) {
+        Consumer<Notification> handler = consumerMap.get(notification.getType());
+        if (handler != null) {
+            handler.accept(notification);
+        }
+    }
+}
