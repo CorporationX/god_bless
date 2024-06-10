7441dc9d871e086c29648a039b80219406ae49e1
l0zain <nanoowood@gmail.com>
2024-06-10 23:45:15 +0300

    https://faang-school.com/courses/e63dkedm/knz334jz

---
+++ src/main/java/BJS2_9315/EmailProcessor.java
@@ -0,0 +1,18 @@
+package BJS2_9315;
+
+import java.util.List;
+import java.util.function.Consumer;
+import java.util.function.Function;
+import java.util.function.Predicate;
+
+public class EmailProcessor {
+    public void processEmails(List<Email> emails, Predicate<Email> filter,
+                              Consumer<Email> handler, Function<Email, String> transformer) {
+        List<Email> filterEmails = emails.stream().filter(filter).toList();
+
+        filterEmails.forEach(email -> {
+            email.setBody(transformer.apply(email));
+            handler.accept(email);
+        });
+    }
+}
