6d6412c26c61ff9bf04ea5897e88626463031554
Danya Ashihin <danyadefault@gmail.com>
2024-06-10 14:43:42 +0300

    feature/danya9375-BJS2-8036 House class created

--- src/main/java/faang/school/godbless/Application.java
+++ src/main/java/faang/school/godbless/Application.java
@@ -4,40 +4,6 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class Application {
-    static Map<String, House> map = new HashMap<>();
 
-    public static void main(String[] args) {
-
-        map.put("Stark", new House("Ned", "Волк"));
-        map.put("Lanister", new House("Tirion", "Лев"));
-        map.put("Baration", new House("Robert", "Олень"));
-
-        House oneHouse = new House("Дейенерис ", "Дракон");
-        add("Targarien", oneHouse);
-        remove("Stark");
-        System.out.println(get("Baration"));
-        printALL();
-
-
-    }
-
-    public static void add(String name, House house) {
-        map.put(name, house);
-    }
-
-    public static void remove(String name) {
-        map.remove(name);
-    }
-
-    public static String get(String name) {
-        return map.get(name).sigil();
-    }
-
-    public static void printALL() {
-        map.forEach((key, house) -> System.out.println(key + ":" + house.sigil()));
-    }
-
-    public record House(String name, String sigil) {
-    }
 }
 
