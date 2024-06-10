a0a2b185295087020c9a256c9927f9c95cb876ac
Garik-create <pfdfkbybujhm@yandex.ru>
2024-06-10 05:10:17 +0300

    - создал метод группировки студентов по факультету и году

--- src/main/java/faang/school/godbless/count_absences/Student.java
+++ src/main/java/faang/school/godbless/count_absences/Student.java
@@ -4,8 +4,27 @@ import java.util.ArrayList;
 import java.util.HashMap;
 
 public record Student(String name, String faculty, int year) {
-    public HashMap<Key, ArrayList<Student>> groupStudentsByFacultyAndYear() {
+    public HashMap<Key, ArrayList<Student>> groupStudentsByFacultyAndYear(
+            ArrayList<Student> students) {
         HashMap<Key, ArrayList<Student>> studentsGroupedByFacultyAndYear = new HashMap<>();
+        //создаём списки студентов сгруппированных по факультету и году
+        for (Student student : students) {
+            //формируем составной ключ
+            Key keyByFacultyAndYear = new Key(student.faculty(), student.year());
+            ArrayList<Student> studentsGrouped;
+            //проверяем существование составного ключа в ХэшМапе
+            if (!studentsGroupedByFacultyAndYear.containsKey(keyByFacultyAndYear)) {
+                //создаём список сгруппированных студентов
+                studentsGrouped = new ArrayList<>();
+                studentsGrouped.add(student);//добавляем студента в список
+                //добавляем запись в ХэшМапу
+                studentsGroupedByFacultyAndYear.put(keyByFacultyAndYear, studentsGrouped);
+            } else {
+                studentsGrouped = studentsGroupedByFacultyAndYear.get(keyByFacultyAndYear);
+                studentsGrouped.add(student);
+                studentsGroupedByFacultyAndYear.put(keyByFacultyAndYear, studentsGrouped);
+            }
+        }
         return studentsGroupedByFacultyAndYear;
     }
 }
