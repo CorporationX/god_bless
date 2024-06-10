06edb330ec6b475a63c82afd386a3fe9704d3b9a
Arman Bistibekov <bistibekov.a@gmail.com>
2024-06-10 15:04:56 -0400

    implemented Image and FilterProcessor classes

---
+++ src/main/java/faang/school/godbless/Image.java
@@ -0,0 +1,13 @@
+package faang.school.godbless;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Image {
+    private String name;
+    private String description;
+}
