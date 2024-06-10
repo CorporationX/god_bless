78e5196673a4707eb3de423e06df153ade554018
Enemy095 <salam.tahaev@mail.ru>
2024-06-10 19:14:13 +0300

    love Hogvarts

--- src/main/java/faang/school/godbless/love/hogvarts/Main.java
+++ src/main/java/faang/school/godbless/love/hogvarts/Main.java
@@ -3,7 +3,7 @@ package faang.school.godbless.love.hogvarts;
 public class Main {
     public static void main(String[] args) {
         SpellCaster spellCaster = new SpellCaster("Avada Kedavra");
-        spellCaster.cast(spellCaster.getNameCast(),(s) -> "Использованно заклинание " + s);
+        spellCaster.cast(spellCaster.getNameCast(), (s) -> "Использованно заклинание " + s);
 
 
     }
--- src/main/java/faang/school/godbless/love/hogvarts/SpellAction.java
+++ src/main/java/faang/school/godbless/love/hogvarts/SpellAction.java
@@ -1,5 +1,6 @@
 package faang.school.godbless.love.hogvarts;
+
 @FunctionalInterface
-public interface SpellAction  {
+public interface SpellAction {
     String castSpell(String s);
 }
