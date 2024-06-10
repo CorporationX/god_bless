9de47e5a9a20b09815466adc989e9bce602cb571
Nikita2768 <vechkinnikita22@gmail.com>
2024-06-10 06:03:46 +0500

    finish task meta

---
+++ src/main/java/faang/school/godbless/meta/NotificationManager.java
@@ -0,0 +1,31 @@
+package faang.school.godbless.meta;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.function.Consumer;
+
+public class NotificationManager {
+    Map<String, Consumer<Notification>> managerNotification = new HashMap<>();
+
+    public void registerHandler(String notificationID, Consumer<Notification> handler) {
+        if(notificationID == null) {
+            throw new IllegalArgumentException("ID notification not be null");
+        }
+        if(handler == null) {
+            throw new IllegalArgumentException("Consumer<Notification> not be null");
+        }
+        managerNotification.put(notificationID, handler);
+    }
+
+    public void sendNotification(Notification notification) {
+        if(notification == null) {
+            throw new IllegalArgumentException("Notification not be null");
+        }
+        var notificationType = notification.getType();
+        if(managerNotification.containsKey(notificationType)) {
+            managerNotification.get(notificationType).accept(notification);
+        } else {
+            throw new IllegalArgumentException("Not find notification in struct data");
+        }
+    }
+}
