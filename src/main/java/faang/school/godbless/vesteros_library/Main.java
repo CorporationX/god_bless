f33a3c9e1cc99bbb9c77871f7a3427c1cb24c79b
Garik-create <pfdfkbybujhm@yandex.ru>
2024-06-10 05:25:31 +0300

    - в Main сделал переменные константами

--- src/main/java/faang/school/godbless/vesteros_library/Main.java
+++ src/main/java/faang/school/godbless/vesteros_library/Main.java
@@ -1,35 +1,34 @@
 package faang.school.godbless.vesteros_library;
 
 import java.util.HashMap;
-import java.util.Map;
 
 public class Main {
 
-    static HashMap<Book, String> books = new HashMap<>();
-    static Book voina_i_mir = new Book("Война и мир", "Толстой Л.Н.", 2001);
-    static Book mciry = new Book("Мцири", "Лермонтов М.Ю.", 2002);
-    static Book prest_i_nakaz = new Book("Преступление и наказание", "Достоевский Ф.",
+    private static final HashMap<Book, String> BOOKS = new HashMap<>();
+    private static final Book VOINA_I_MIR = new Book("Война и мир", "Толстой Л.Н.", 2001);
+    private static final Book MCIRY = new Book("Мцири", "Лермонтов М.Ю.", 2002);
+    private static final Book PREST_I_NAKAZ = new Book("Преступление и наказание", "Достоевский Ф.",
             2003);
-    static Book peterFirst = new Book("Пётр I", "Толсной А.Н.", 1999);
+    private static final Book PETER_FIRST = new Book("Пётр I", "Толсной А.Н.", 1999);
 
     public static void main(String[] args) {
 
-        books.put(voina_i_mir, "1");
-        books.put(mciry, "2");
-        books.put(prest_i_nakaz, "3");
+        BOOKS.put(VOINA_I_MIR, "1");
+        BOOKS.put(MCIRY, "2");
+        BOOKS.put(PREST_I_NAKAZ, "3");
 
-        Book.getAllBooksAndItsPlaces(books);
+        Book.getAllBooksAndItsPlaces(BOOKS);
         System.out.println();
-        Book.addNewBook(mciry, "4", books);
-        Book.getAllBooksAndItsPlaces(books);
+        Book.addNewBook(MCIRY, "4", BOOKS);
+        Book.getAllBooksAndItsPlaces(BOOKS);
         System.out.println();
-        Book.addNewBook(peterFirst, "4", books);
-        Book.getAllBooksAndItsPlaces(books);
+        Book.addNewBook(PETER_FIRST, "4", BOOKS);
+        Book.getAllBooksAndItsPlaces(BOOKS);
         System.out.println();
-        Book.deleteBookByItsTitleAuthorYear("Мцири", "Лермонтов М.Ю.", 2002, books);
-        Book.getAllBooksAndItsPlaces(books);
+        Book.deleteBookByItsTitleAuthorYear("Мцири", "Лермонтов М.Ю.", 2002, BOOKS);
+        Book.getAllBooksAndItsPlaces(BOOKS);
         System.out.println();
-        Book.findBookPlaceByTitleAuthorYear("Пётр I", "Толсной А.Н.", 1999, books);
+        Book.findBookPlaceByTitleAuthorYear("Пётр I", "Толсной А.Н.", 1999, BOOKS);
     }
 
 }
