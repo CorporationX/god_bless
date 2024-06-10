62ea6961761b6422ecb37dcca43e82e0b7fa056e
Stanislav Ovsyankin <cancion2able@gmail.com>
2024-06-10 23:39:02 +0500

    Удаление лишней аннотации

--- src/main/java/faang/school/godbless/func/errors/ErrorHandling.java
+++ src/main/java/faang/school/godbless/func/errors/ErrorHandling.java
@@ -1,10 +1,7 @@
 package faang.school.godbless.func.errors;
 
-import lombok.extern.slf4j.Slf4j;
-
 import java.util.function.Supplier;
 
-@Slf4j
 public class ErrorHandling {
 
     public static <T> T withErrorHandling(Supplier<T> action, ExceptionHandler<T> fallbackHandler) {
