5d0d2e7750f5785dc8f6549bd6d3c1677ef4b37d
Stanislav Ovsyankin <cancion2able@gmail.com>
2024-06-10 09:54:14 +0500

    Null check

--- src/main/java/faang/school/godbless/func/spam/MessageProcessor.java
+++ src/main/java/faang/school/godbless/func/spam/MessageProcessor.java
@@ -5,6 +5,9 @@ import java.util.List;
 public record MessageProcessor() {
 
     public boolean processMessage(String message, List<MessageFilter> filters) {
+        if (message == null || filters == null) {
+            throw new IllegalArgumentException("Filters and message cannot be null for processing");
+        }
         return filters.stream().allMatch(messageFilter -> messageFilter.filter(message));
     }
 }
