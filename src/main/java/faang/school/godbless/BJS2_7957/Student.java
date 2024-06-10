c7bcadd60bab6f59ceebb0dcab9ac637a640b976
s.kolbasov <s.kolbasov@bftcom.com>
2024-06-10 09:56:12 +0300

    BJS2-7957 Java HashMap: Двойной кэш-кэш

---
+++ src/main/java/faang/school/godbless/BJS2_7957/Student.java
@@ -0,0 +1,11 @@
+package faang.school.godbless.BJS2_7957;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class Student {
+  private long id;
+  private String name;
+}
