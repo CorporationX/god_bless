dd056053ce12f682a6ce59cb85274113c52a5a36
Artem <xotabggg@gmail.com>
2024-06-10 22:57:27 +0700

    Turn around

---
+++ src/main/java/faang/school/godbless/Example.java
@@ -0,0 +1,15 @@
+package faang.school.godbless;
+
+public class Example {
+
+    public int[] revers(int[] reverse) {
+        int[] result = new int[reverse.length];
+        for (int i = reverse.length-1; i > 0; i--) {
+            for (int j = 0; j < reverse.length; j++) {
+                result[j] = reverse[i];
+                i--;
+            }
+        }
+        return result;
+    }
+}
