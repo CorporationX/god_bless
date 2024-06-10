b9b783017324e5e30af847535cd3f8fba4d4100e
Andrey Kuzmin <Andreytko@mail.ru>
2024-06-11 00:06:09 +0300

    corrections to class NotificationManager

--- src/main/java/faang/school/godbless/Task_MetaUniverse/NotificationManager.java
+++ src/main/java/faang/school/godbless/Task_MetaUniverse/NotificationManager.java
@@ -1,14 +1,17 @@
 package faang.school.godbless.Task_MetaUniverse;
 
-import lombok.NoArgsConstructor;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.function.Consumer;
 
-@NoArgsConstructor
+
 public class NotificationManager {
 
-    private static final Map<String, Consumer<Notification>> noteService = new HashMap<>();
+    private Map<String, Consumer<Notification>> noteService;
+
+    public NotificationManager() {
+        noteService = new HashMap<>();
+    }
 
     public void registerHandler(String notificationType, Consumer<Notification> consumer) {
         noteService.put(notificationType, consumer);
@@ -17,7 +20,5 @@ public class NotificationManager {
     public void sendNotification(Notification notification) {
         System.out.println(noteService.get(notification.getType()));
     }
-
-
 
 }
