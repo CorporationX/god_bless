71940aa2eadb46048c0d761a9c887458cabfb971
Konstantyn Huzil <huzilkon@gmail.com>
2024-06-10 10:23:58 +0200

    Группировка пользователей по возрасту

---
+++ src/main/java/faang/school/godbless/group_users/User.java
@@ -0,0 +1,21 @@
+package faang.school.godbless.group_users;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+@Data
+@AllArgsConstructor
+public class User {
+    private String name;
+    private int age;
+    private String workplace;
+    private String address;
+
+    public static Map<Integer, List<User>> groupUsers(List<User> userList) {
+        return userList.stream().collect(Collectors.groupingBy(User::getAge));
+    }
+}
