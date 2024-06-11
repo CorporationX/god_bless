dae65d1dfacc71abe426eca772de8eb8e33ed7f9
Иван Шилов <ivshvl39@gmail.com>
2024-06-11 10:16:52 +0300

    Google Переводчик под нашим контролем…

---
+++ src/main/java/faang/school/godbless/task17/DictionaryProcessor.java
@@ -0,0 +1,28 @@
+package faang.school.godbless.task17;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.function.BiConsumer;
+
+public class DictionaryProcessor {
+    public static void main(String[] args) {
+        DictionaryProcessor dictionaryProcessor = new DictionaryProcessor();
+
+// Создание словаря
+        Map<String, String> dictionary = new HashMap<>();
+
+// Создание BiConsumer для сохранения слова и его перевода в словарь
+        BiConsumer<String, String> addWordToDictionary = (word, translation) -> dictionary.put(word, translation);
+
+// Добавление слов и их переводов
+        dictionaryProcessor.processWord("привет", "hello", addWordToDictionary);
+        dictionaryProcessor.processWord("мир", "world", addWordToDictionary);
+        dictionaryProcessor.processWord("программирование", "programming", addWordToDictionary);
+
+        System.out.println("Словарь: " + dictionary);
+    }
+
+    public void processWord(String word, String translate, BiConsumer<String, String> consumer) {
+        consumer.accept(word, translate);
+    }
+}
