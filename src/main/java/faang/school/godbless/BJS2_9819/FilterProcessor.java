3aa2abbb2703a0688d9e1c3663cbee6826ebe2ad
lu_av <loveavduhova@gmail.com>
2024-06-11 21:01:15 +0700

    BJS2-9819 - задача сделана

---
+++ src/main/java/faang/school/godbless/BJS2_9819/FilterProcessor.java
@@ -0,0 +1,23 @@
+package faang.school.godbless.BJS2_9819;
+
+
+/*
+Создайте класс FilterProcessor с методами:
+applyFilter: принимает объект типа Image и функцию высокого порядка для применения фильтра (например, Function<Image, Image>),
+ возвращает новый объект типа Image с применённым фильтром;
+ combineFilters: принимает две функции высокого порядка и возвращает новую функцию высокого порядка, которая последовательно применяет обе функции.
+ */
+
+import java.util.function.Function;
+
+public class FilterProcessor {
+    public Image applyFilter(Image image, Function<Image, Image> imageFunction){
+       return imageFunction.apply(image);
+    }
+
+
+    public Function<Image, Image> combineFilters(Function<Image, Image> imageFilterOne, Function<Image, Image> imageFilterTwo){
+        Function<Image, Image> function = (image) -> imageFilterTwo.apply(imageFilterOne.apply(image));
+        return function;
+    }
+}
