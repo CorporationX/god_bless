57aff8d2e55fcc6ff49ade683153fa856282e6a4
Sasha Chekalkin <crutoipatz@yandex.ru>
2024-06-11 19:10:01 +0300

    int1nglence|Sasha|BJS2-7053 - Игра престолов

--- src/main/java/faang/school/godbless/Application.java
+++ src/main/java/faang/school/godbless/Application.java
@@ -1,7 +1,26 @@
 package faang.school.godbless;
 
+import java.util.Map;
+import java.util.HashMap;
+
+import static faang.school.godbless.House.addNames;
+import static faang.school.godbless.House.foundHouse;
+import static faang.school.godbless.House.infoAllHouse;
+import static faang.school.godbless.House.removeHouse;
+
 public class Application {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        HashMap<String, House> houseMap = new HashMap<>();
+
+        addNames(houseMap, "Stark", "Black");
+        addNames(houseMap, "Lannister", "Red");
+        addNames(houseMap, "Barateon", "Blue");
+
+        removeHouse(houseMap,"Lannister");
+
+        foundHouse(houseMap,"Lannister");
+        foundHouse(houseMap,"Stark");
+
+        infoAllHouse(houseMap);
     }
-}
+}  
