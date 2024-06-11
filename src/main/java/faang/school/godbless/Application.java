bbf16e25e5eef10b6ddca3a089133a4c25f503d3
Kilotas <Alendos87@gmail.com>
2024-06-11 11:54:49 +0500

    Merge pull request #5947 from CorporationX/Omarov-Alen-BJS2-7825
    Solved problem with git collision

--- src/main/java/faang/school/godbless/Application.java
+++ src/main/java/faang/school/godbless/Application.java
@@ -5,13 +5,7 @@ import java.util.Map;
 
 public class Application {
     public static void main(String[] args) {
-        int[] numbers = {1,2,3,4,5};
 
-        Reverse.reverseArray(numbers);
-
-        for (int number: numbers){
-            System.out.println(number);
-        }
     }
 
 }
