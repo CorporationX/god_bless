f3b8bf6e681ef3d58480ce9dcdf9fa63586a0f0b
OlgaK888 <olgakurilovi4@gmail.com>
2024-06-11 18:03:12 +0300

    Added a check for null

--- src/main/java/faang/school/godbless/user_registration/User.java
+++ src/main/java/faang/school/godbless/user_registration/User.java
@@ -1,6 +1,5 @@
 package faang.school.godbless.user_registration;
 
-import java.util.HashSet;
 import java.util.Set;
 
 public class User {
@@ -12,16 +11,16 @@ public class User {
     private final String address;
 
     public User(String name, int age, String placeOfWork, String address) {
-        if (name.isBlank()) {
+        if (name == null || name.isBlank()) {
             throw new IllegalArgumentException("Name cannot be empty");
         }
         if (age < 18) {
             throw new IllegalArgumentException("Age cannot be less than 18");
         }
-        if (!VALID_JOBS.contains(placeOfWork)) {
+        if (placeOfWork == null || !VALID_JOBS.contains(placeOfWork)) {
             throw new IllegalArgumentException("Place of work is incorrect");
         }
-        if (!VALID_ADDRESSES.contains(address)) {
+        if (address == null || !VALID_ADDRESSES.contains(address)) {
             throw new IllegalArgumentException("Address is incorrect");
         }
         this.name = name;
