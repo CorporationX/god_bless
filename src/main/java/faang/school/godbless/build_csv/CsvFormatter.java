cff284af556d64963e82424fb0abf439c0d2aa38
ravshanbekn <ravshanbeknabiyev.dev@gmail.com>
2024-06-11 15:07:36 +0500

    Построим CSV Initial Commit

---
+++ src/main/java/faang/school/godbless/build_csv/CsvFormatter.java
@@ -0,0 +1,31 @@
+package faang.school.godbless.build_csv;
+
+import java.util.List;
+import java.util.function.BiFunction;
+import java.util.stream.Collectors;
+
+public class CsvFormatter {
+    public static String toCsv(List<List<String>> table) throws IllegalArgumentException {
+        BiFunction<List<String>, String, String> collector = (list, delimiter) -> {
+            if (list == null || list.isEmpty()) {
+                throw new IllegalArgumentException("Cannot process null or empty list");
+            }
+            StringBuilder result = new StringBuilder();
+            int numberOfElements = list.size();
+            for (int i = 0; i < numberOfElements; i++) {
+                result.append(list.get(i));
+                if (i != numberOfElements - 1) {
+                    result.append(delimiter);
+                }
+            }
+            return result.toString();
+        };
+
+        VectorJoiner<String> vectorJoiner = (row) -> collector.apply(row, ", ");
+
+        MatrixJoiner<String> matrixJoiner = (rows) ->
+                collector.apply(rows.stream().map(vectorJoiner::join).collect(Collectors.toList()), "\n");
+
+        return matrixJoiner.join(table);
+    }
+}
