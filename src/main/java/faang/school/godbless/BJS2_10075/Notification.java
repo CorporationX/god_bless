81ca534489130a540ef10ee170c3f67cfc89c224
lu_av <loveavduhova@gmail.com>
2024-06-11 20:46:14 +0700

    BJS2-10075 - задача сделана

---
+++ src/main/java/faang/school/godbless/BJS2_10075/Notification.java
@@ -0,0 +1,13 @@
+package faang.school.godbless.BJS2_10075;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.ToString;
+
+@AllArgsConstructor
+@Getter
+@ToString
+public class Notification {
+    private String type;
+    private String message;
+}
