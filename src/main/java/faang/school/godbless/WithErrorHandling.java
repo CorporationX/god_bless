86930bba6a11a271c286c593826fa944e51cf67b
Gevorg Sogomonian <a0964593@gmail.com>
2024-06-11 18:29:20 +0300

    First try

---
+++ src/main/java/faang/school/godbless/WithErrorHandling.java
@@ -0,0 +1,9 @@
+package faang.school.godbless;
+
+import java.util.function.Function;
+
+public class WithErrorHandling {
+    public static <T> void withErrorHandling (Function<T, String> tryToDo, Function<String, String> ifCatchSomething) {
+
+    }
+}
