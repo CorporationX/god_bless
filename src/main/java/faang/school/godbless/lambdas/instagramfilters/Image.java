3c28363525d653a3343057c0303b8db2e44b8ee3
Kirill Maltsev <opp.2211@gmail.com>
2024-06-11 15:50:09 +0500

    Фильтры Instagram

---
+++ src/main/java/faang/school/godbless/lambdas/instagramfilters/Image.java
@@ -0,0 +1,13 @@
+package faang.school.godbless.lambdas.instagramfilters;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.ToString;
+
+@AllArgsConstructor
+@Getter
+@ToString
+public class Image {
+    private String fileName;
+    private String description;
+}
---
+++ src/main/java/faang/school/godbless/lambdas/instagramfilters/Main.java
@@ -0,0 +1,31 @@
+package faang.school.godbless.lambdas.instagramfilters;
+
+import java.util.function.Function;
+
+public class Main {
+    public static void main(String[] args) {
+        Image originalImage = new Image("original.jpg", "Оригинальное изображение");
+
+        FilterProcessor filterProcessor = new FilterProcessor();
+
+        // Создание фильтров
+        Function<Image, Image> grayscaleFilter = (image) -> new Image(image.getFileName(), image.getDescription() + " " + "Фильтр: черно-белый");
+        Function<Image, Image> sepiaFilter = (image) -> new Image(image.getFileName(),image.getDescription() + " " + "Фильтр: сепия");
+        Function<Image, Image> vignetteFilter = (image) -> new Image(image.getFileName(),image.getDescription() + " " + "Фильтр: виньетка");
+
+        // Применение фильтров
+        Image grayscaleImage = filterProcessor.applyFilter(originalImage, grayscaleFilter);
+        Image sepiaImage = filterProcessor.applyFilter(originalImage, sepiaFilter);
+        Image vignetteImage = filterProcessor.applyFilter(originalImage, vignetteFilter);
+
+        System.out.println(grayscaleImage);
+        System.out.println(sepiaImage);
+        System.out.println(vignetteImage);
+
+        // Создание и применение комбинированного фильтра
+        Function<Image, Image> combinedFilter = filterProcessor.combineFilters(grayscaleFilter, sepiaFilter);
+        Image combinedImage = filterProcessor.applyFilter(originalImage, combinedFilter);
+
+        System.out.println(combinedImage);
+    }
+}
