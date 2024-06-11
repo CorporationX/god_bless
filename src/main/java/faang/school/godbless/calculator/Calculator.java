4589857b114a7b9ec4c77971c173c4f26635488c
Nikita2768 <vechkinnikita22@gmail.com>
2024-06-11 19:22:43 +0500

    finish task for all type number again, again

--- src/main/java/faang/school/godbless/calculator/Calculator.java
+++ src/main/java/faang/school/godbless/calculator/Calculator.java
@@ -1,7 +1,5 @@
 package faang.school.godbless.calculator;
 
-import java.util.List;
-
 @FunctionalInterface
 public interface Calculator<T extends Number> {
     T calculate(T oneNumber, T twoNumber);
