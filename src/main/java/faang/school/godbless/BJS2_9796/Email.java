17ce32ef1a83c17a9c7608cf0f12f91891079951
lu_av <loveavduhova@gmail.com>
2024-06-11 21:08:08 +0700

    BJS2-9796 - задача сделана

---
+++ src/main/java/faang/school/godbless/BJS2_9796/Email.java
@@ -0,0 +1,15 @@
+package faang.school.godbless.BJS2_9796;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.ToString;
+
+@Getter
+@AllArgsConstructor
+@ToString
+public class Email {
+    private String subject;
+    private String body;
+    private boolean isImportant;
+
+}
---
+++ src/main/java/faang/school/godbless/BJS2_9796/EmailProcessor.java
@@ -0,0 +1,21 @@
+package faang.school.godbless.BJS2_9796;
+
+import java.util.List;
+import java.util.function.Consumer;
+import java.util.function.Function;
+import java.util.function.Predicate;
+
+public class EmailProcessor {
+    public void processEmails(List<Email> emails, Predicate<Email> predicate, Consumer<Email> consumer, Function<Email, String> function) {
+        if (emails != null || !emails.isEmpty() ||
+                predicate != null || consumer != null ||
+                function != null) {
+            for (Email email : emails) {
+                if (predicate.test(email)) {
+                    consumer.accept(email);
+                    System.out.println(function.apply(email));
+                }
+            }
+        }
+    }
+}
