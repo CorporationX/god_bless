ef3e01da3f3cce2a337a281f99ec28fa113a97f3
Kirill Maltsev <opp.2211@gmail.com>
2024-06-11 15:22:31 +0500

    feat: добавить цензурирование сообщений перед отправкой уведомления

--- src/main/java/faang/school/godbless/lambdas/metauniverce/Main.java
+++ src/main/java/faang/school/godbless/lambdas/metauniverce/Main.java
@@ -1,9 +1,9 @@
 package faang.school.godbless.lambdas.metauniverce;
 
 public class Main {
-    public static void main(String[] args) {
-        NotificationManager notificationManager = new NotificationManager();
+    private static NotificationManager notificationManager = new NotificationManager();
 
+    public static void main(String[] args) {
         notificationManager.registerHandler("email", notification ->
                 System.out.println("Уведомление через email: " + notification.getMessage()));
         notificationManager.registerHandler("sms", notification ->
@@ -11,9 +11,26 @@ public class Main {
         notificationManager.registerHandler("push", notification ->
                 System.out.println("Push-уведомление в приложении: " + notification.getMessage()));
 
-        notificationManager.sendNotification(new Notification("email", "Сообщение 1"));
-        notificationManager.sendNotification(new Notification("email", "Сообщение 2"));
-        notificationManager.sendNotification(new Notification("sms", "Сообщение 3"));
+
+        // Регистрируем цензор для emailов, чтобы заменять звездочками нецензурные вхождения
+        notificationManager.registerCensor("email", notification -> {
+            String textToCensure = "censure";
+            String message = notification.getMessage();
+            int idx = message.toLowerCase().indexOf(textToCensure);
+            while (idx != -1) {
+                message = message.substring(0, idx)
+                        + "[" + "*".repeat(textToCensure.length()) + "]"
+                        + message.substring(idx + textToCensure.length());
+                idx = message.toLowerCase().indexOf(textToCensure);
+            }
+            notification.setMessage(message);
+            return notification;
+        });
+
+        notificationManager.sendNotification(new Notification("email", "Сообщение 1 + censure"));
+        notificationManager.sendNotification(new Notification("email", "Сообcensureщение 2 censure"));
+        notificationManager.sendNotification(new Notification("sms", "Сообщение 3 + censure"));
         notificationManager.sendNotification(new Notification("push", "Сообщение 4"));
+        notificationManager.sendNotification(new Notification("telegram", "Сообщение 5"));
     }
 }
