a4548353901267c35e6b0dc7887821e91b509c96
Иван Шилов <ivshvl39@gmail.com>
2024-06-11 10:11:30 +0300

    Фильтры Instagram

---
+++ src/main/java/faang/school/godbless/task16/Image.java
@@ -0,0 +1,12 @@
+package faang.school.godbless.task16;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class Image {
+    private String fileName;
+    private String fileDescription;
+
+}
