91dd2722c69bc9a19c2948f5050457063191b24b
Dima Dobrev <dobrev2212@gmail.com>
2024-06-11 16:34:07 +0200

    BJS2-8071 deleteHouse method is fixed; "statics" are added; public static void main with "tests" is added

--- src/main/java/faang/school/godbless/hashmap/gameofthrones/Main.java
+++ src/main/java/faang/school/godbless/hashmap/gameofthrones/Main.java
@@ -4,35 +4,54 @@ import java.util.HashMap;
 import java.util.Map;
 import java.util.NoSuchElementException;
 
+import static java.util.Optional.ofNullable;
+
 public class Main {
-    public Map<String, House> houses = new HashMap<>(Map.of(
+    private static final Map<String, House> HOUSES = new HashMap<>(Map.of(
             "Stark", new House("Stark", "Stark sigil"),
             "Lannister", new House("Lannister", "Lannister sigil"),
             "Baratheon", new House("Baratheon", "Baratheon sigil"))
     );
 
-    public House addHouse(House house){
-        return houses.put(house.getName(), house);
+    public static House addHouse(House house){
+        return HOUSES.put(house.getName(), house);
     }
 
-    public House deleteHouse(String houseName){
-        var house = getHouse(houseName);
-        return houses.remove(house);
+    public static House deleteHouse(String houseName){
+        return ofNullable(HOUSES.remove(houseName))
+                .orElseThrow(() -> new NoSuchElementException("No house with name " + houseName));
     }
 
-    public void searchHouse(String houseName){
+    public static void searchHouse(String houseName){
         var house = getHouse(houseName);
         System.out.printf("House %s has %s", houseName, house.getSigil());
     }
-    public void printHouse() {
-        houses.values().forEach(System.out::println);
+    public static void printHouses() {
+        HOUSES.values().forEach(System.out::println);
     }
 
-    private House getHouse(String houseName) {
-        var house = houses.get(houseName);
+    private static House getHouse(String houseName) {
+        var house = HOUSES.get(houseName);
         if (house == null){
             throw new NoSuchElementException("No house with name " + houseName);
         }
         return house;
+    }
+
+    public static void main(String[] args) {
+        var house = new House("Ambrose", "Ambrose sigil");
+        var houseName = house.getName();
+
+        addHouse(house);
+        System.out.printf("House %s is added.\n", houseName);
+        printHouses();
+        System.out.println("============");
+
+        searchHouse(houseName);
+        System.out.println("\n============");
+
+        deleteHouse(houseName);
+        System.out.printf("House %s is deleted.\n", houseName);
+        printHouses();
     }
 }
