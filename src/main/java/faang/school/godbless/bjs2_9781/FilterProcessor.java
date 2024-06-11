d892852927d102a47bd233e059540322fabc447f
Maksim Tyumentsev <experttm@gmail.com>
2024-06-11 12:38:56 +0500

    #BJS2-9781 Реализовать функционал для применения фильтров к изображениям.

---
+++ src/main/java/faang/school/godbless/bjs2_9781/FilterProcessor.java
@@ -0,0 +1,15 @@
+package faang.school.godbless.bjs2_9781;
+
+import java.util.function.Function;
+
+public class FilterProcessor {
+
+  public Image applyFilter(Image image, Function<Image, Image> function) {
+    return function.apply(image);
+  }
+
+  public Function<Image, Image> combineFilters(Function<Image, Image> firstFilter, Function<Image, Image> secondFilter) {
+    return firstFilter.andThen(secondFilter);
+  }
+
+}
---
+++ src/main/java/faang/school/godbless/bjs2_9781/Image.java
@@ -0,0 +1,5 @@
+package faang.school.godbless.bjs2_9781;
+
+public record Image(String name, String title) {
+
+}
