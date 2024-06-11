d85fee6f45530662b3899fbcf44053334e5bc6a8
cirill98 <100434317+cirill98@users.noreply.github.com>
2024-06-11 19:04:09 +0500

    Реализовал исключение в методе

--- src/main/java/faang/school/godbless/HandleErrorsBeautifully/Main.java
+++ src/main/java/faang/school/godbless/HandleErrorsBeautifully/Main.java
@@ -4,7 +4,9 @@ import java.util.function.Supplier;
 
 public class Main {
     public static void main(String[] args) {
-
+        Supplier<String> supplier = () -> "Подключение успешно";
+        ExceptionHandler<String> exceptionHandler = (e) -> e + " ошибка";
+        System.out.println(withErrorHandling(supplier, exceptionHandler));
     }
 
     public static <T> T withErrorHandling(Supplier<T> supplier, ExceptionHandler<T> errorHandling) {
