ec8ae7d3f8a587d29bf14f5e2fc2cf9578f3fc6f
Max Shundrick <mas9l175@gmail.com>
2024-06-12 08:31:11 +0300

    Fix BJS2-9780 after reviewing

--- src/main/java/faang/school/godbless/Application.java
+++ src/main/java/faang/school/godbless/Application.java
@@ -10,10 +10,18 @@ public class Application {
         Filter grayscaleFilter = new Filter("_grayscale", "Filter: Grayscale");
         Filter sepiaFilter = new Filter("_sepia", "Filter: sepia");
         Filter vignetteFilter = new Filter("_vignette", "Filter: Vignette");
-        Function<Image, Image> combineFilters = imageProcessor.combineFilters(sepiaFilter, vignetteFilter, grayscaleFilter);
+
+        Function<Image, Image> grayscale = img -> new Image(img.getFileName() + "_grayscale", "Filter: Grayscale");
+        Function<Image, Image> sepia = img -> new Image(img.getFileName() + "_sepia", "Filter: sepia");
+        Function<Image, Image> vignette = img -> new Image(img.getFileName() + "_vignette", "Filter: Vignette");
+
+        Function<Image, Image> combineFilters = imageProcessor.combineFilters(grayscaleFilter, sepiaFilter, vignetteFilter);
+        Function<Image, Image> sequentialFunction = imageProcessor.combineFilters(grayscale, sepia);
+
         imageProcessor.applyFilter(image, grayscaleFilter.getFilter());
         imageProcessor.applyFilter(image, sepiaFilter.getFilter());
         imageProcessor.applyFilter(image, vignetteFilter.getFilter());
         imageProcessor.applyFilter(image, combineFilters);
+        imageProcessor.applyFilter(image, sequentialFunction);
     }
 }
--- src/main/java/faang/school/godbless/ImageProcessor.java
+++ src/main/java/faang/school/godbless/ImageProcessor.java
@@ -1,10 +1,17 @@
 package faang.school.godbless;
 
+import java.util.function.BiFunction;
 import java.util.function.Function;
 
 public class ImageProcessor {
     public void applyFilter(Image image, Function<Image, Image> filter) {
         System.out.println(filter.apply(image).getFileName() + " , " + filter.apply(image).getDescription());
+    }
+
+    public Function<Image, Image> combineFilters(Function<Image, Image> function1, Function<Image, Image> function2) {
+        BiFunction<Function<Image, Image>, Function<Image, Image>, Function<Image, Image>> combinedFunction = Function::andThen;
+        Function<Image, Image> resultFunction = combinedFunction.apply(function1, function2);
+        return resultFunction;
     }
 
     public Function<Image, Image> combineFilters(Filter... filters) {
