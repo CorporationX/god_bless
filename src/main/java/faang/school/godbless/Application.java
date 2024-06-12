ebed93f794f710f467e649fa9b9760acdb2191f1
Gevorg Sogomonian <a0964593@gmail.com>
2024-06-12 11:29:15 +0300

    First try

--- src/main/java/faang/school/godbless/Application.java
+++ src/main/java/faang/school/godbless/Application.java
@@ -3,11 +3,9 @@ package faang.school.godbless;
 public class Application {
     public static void main(String[] args) {
         SpellCaster spellCaster = new SpellCaster();
-
         String alohorma = "Alohorma";
         String lumos = "Lumos";
         String expelliarmus = "Expelliarmus";
-
         spellCaster.cast(alohorma, (spell) -> "The door is unlocked by " + spell);
         spellCaster.cast(lumos, (spell) -> "A beam of light is created by " + spell);
         spellCaster.cast(expelliarmus, (spell) -> "The opponent is disarmed by " + spell);
