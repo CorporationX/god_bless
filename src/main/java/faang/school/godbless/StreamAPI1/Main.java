be90e0e7784a60888a7a9a047657627f1bf18741
cirill98 <100434317+cirill98@users.noreply.github.com>
2024-06-12 23:15:39 +0500

    Исправил название класса Добавил модификатор доступа public На счет предиката не понял, мне в параметр указывать функциональный интерфейс? если метод anyMatch в себе и так содержит предикат

--- src/main/java/faang/school/godbless/StreamAPI1/Main.java
+++ src/main/java/faang/school/godbless/StreamAPI1/Main.java
@@ -7,14 +7,14 @@ public class Main {
         List<Integer> nums = List.of(1, 2, 3, 4, 5, 6, 7, 8);
         List<String> str = List.of("Letooo", "Moreeee", "Jaraa", "Mir", "Mart", "Love");
         List<String> str2 = List.of("Лето в столице", "Лето зимой", "вперед лето", "нилето");
-        System.out.println(staticMetod.sumNumsList(nums));
-        System.out.println(staticMetod.maxNumList(nums));
-        System.out.println(staticMetod.avgNumsList(nums));
-        System.out.println(staticMetod.findNumLine(str, "L"));
-        System.out.println(staticMetod.filterList(str2, "Лето"));
-        System.out.println(staticMetod.sortListString(str));
-        System.out.println(staticMetod.anyMatchTest(str));
-        System.out.println(staticMetod.minNumListBigNum(nums, 4));
-        System.out.println(staticMetod.stringConvertLength(str));
+        System.out.println(StaticMetod.sumNumsList(nums));
+        System.out.println(StaticMetod.maxNumList(nums));
+        System.out.println(StaticMetod.avgNumsList(nums));
+        System.out.println(StaticMetod.findNumLine(str, "L"));
+        System.out.println(StaticMetod.filterList(str2, "Лето"));
+        System.out.println(StaticMetod.sortListString(str));
+        System.out.println(StaticMetod.anyMatchTest(str));
+        System.out.println(StaticMetod.minNumListBigNum(nums, 4));
+        System.out.println(StaticMetod.stringConvertLength(str));
     }
 }
--- src/main/java/faang/school/godbless/StreamAPI1/StaticMetod.java
+++ src/main/java/faang/school/godbless/StreamAPI1/StaticMetod.java
@@ -1,52 +1,50 @@
 package faang.school.godbless.StreamAPI1;
 
 import java.util.List;
-import java.util.Optional;
-import java.util.OptionalDouble;
 
-public class staticMetod {
+public class StaticMetod {
     //    Сумма четных чисел в списке
-    static int sumNumsList(List<Integer> nums) {
+    public static int sumNumsList(List<Integer> nums) {
         return nums.stream().filter(i -> i % 2 == 0).reduce(0, Integer::sum);
     }
 
     //    Максимальный элемент в списке чисел
-    static int maxNumList(List<Integer> nums) {
+    public static int maxNumList(List<Integer> nums) {
         return nums.stream().max((t, u) -> t - u).orElse(0);
     }
 
     //    Среднее значение чисел в списке
-    static double avgNumsList(List<Integer> nums) {
+    public static double avgNumsList(List<Integer> nums) {
         return nums.stream().mapToDouble(i -> i).average().orElse(0);
     }
 
     //    Количество строк, начинающихся с определённого символа в списке строк
-    static long findNumLine(List<String> str, String ch) {
+    public static long findNumLine(List<String> str, String ch) {
         return str.stream().filter(s -> s.startsWith(ch)).count();
     }
 
     //    Отфильтровать список строк и оставить только те, которые содержат определенную подстроку
-    static List<String> filterList(List<String> str, String substring) {
+    public static List<String> filterList(List<String> str, String substring) {
         return str.stream().filter(st -> st.contains(substring)).toList();
     }
 
     //    Отсортировать список строк по длине;
-    static List<String> sortListString(List<String> str) {
+    public static List<String> sortListString(List<String> str) {
         return str.stream().sorted((s1, s2) -> s1.length() - s2.length()).toList();
     }
 
     //    Все ли элементы списка удовлетворяют определённому условию
-    static boolean anyMatchTest(List<String> str) {
+    public static boolean anyMatchTest(List<String> str) {
         return str.stream().anyMatch(s -> s.length() > 2);
     }
 
     //    Наименьший элемент в списке, который больше заданного числа
-    static int minNumListBigNum(List<Integer> nums, int num) {
+    public static int minNumListBigNum(List<Integer> nums, int num) {
         return nums.stream().filter(i -> i > num).min((t, u) -> t - u).orElse(0);
     }
 
     //    Преобразовать список строк в список их длин
-    static List<Integer> stringConvertLength(List<String> str) {
+    public static List<Integer> stringConvertLength(List<String> str) {
         return str.stream().map(String::length).toList();
     }
 }
