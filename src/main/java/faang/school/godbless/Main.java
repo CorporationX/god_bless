1d627389e623f1d783e2620e9a66f82e4e545160
Gevorg <a0964593@gmail.com>
2024-06-12 14:09:09 +0300

    First try

--- src/main/java/faang/school/godbless/Main.java
+++ src/main/java/faang/school/godbless/Main.java
@@ -1,6 +1,6 @@
 package faang.school.godbless;
 
-public class Application {
+public class Main {
     public static void main(String[] args) {
         NotificationManager notificationManager = new NotificationManager();
 
