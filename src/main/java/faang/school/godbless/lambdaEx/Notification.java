995bdc048f6c8b1e1ca76b4382add18215a4ef2d
Caveri95 <ura22051995@gmail.com>
2024-06-12 11:17:59 +0300

    Реализована система, которая позволяет пользователям выбирать различные типы оповещений

---
+++ src/main/java/faang/school/godbless/lambdaEx/Notification.java
@@ -0,0 +1,12 @@
+package faang.school.godbless.lambdaEx;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class Notification {
+
+    private String type;
+    private String message;
+}
