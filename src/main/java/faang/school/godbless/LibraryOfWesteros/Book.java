c1cf2d2b43584ecfaabe84f063b0636b0b3cbcc1
Gevorg <a0964593@gmail.com>
2024-06-12 14:43:03 +0300

    First try

--- src/main/java/faang/school/godbless/LibraryOfWesteros/Book.java
+++ src/main/java/faang/school/godbless/LibraryOfWesteros/Book.java
@@ -1,4 +1,4 @@
-package faang.school.godbless;
+package faang.school.godbless.LibraryOfWesteros;
 
 import java.util.Objects;
 
--- src/main/java/faang/school/godbless/LibraryOfWesteros/Main.java
+++ src/main/java/faang/school/godbless/LibraryOfWesteros/Main.java
@@ -1,4 +1,4 @@
-package faang.school.godbless;
+package faang.school.godbless.LibraryOfWesteros;
 
 import java.util.HashMap;
 import java.util.Map;
