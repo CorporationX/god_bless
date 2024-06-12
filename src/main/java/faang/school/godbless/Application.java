1e968ab9536e194f108f8267113dd6f854172bd6
Alen <Alendos87@gmail.com>
2024-06-12 19:33:44 +0500

    Problem with spell cast

--- src/main/java/faang/school/godbless/Application.java
+++ src/main/java/faang/school/godbless/Application.java
@@ -2,6 +2,20 @@ package faang.school.godbless;
 
 public class Application {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        NotificationManager manager = new NotificationManager();
+
+        // Регистрация обработчиков
+        manager.registerHandler("email", notification1 -> System.out.println("Email notification: " + notification1.getMessage()));
+        manager.registerHandler("sms", notification1 -> System.out.println("SMS notification: " + notification1.getMessage()));
+        manager.registerHandler("facebook", notification1 -> System.out.println("Facebook notification: " + notification1.getMessage()));
+
+        Notification emailNotification = new Notification( "email", "Ваша учетная запись успешна активирована");
+        Notification smsNotification = new Notification("sms", "Вы успешно изменили свой пароль");
+        Notification pushNotification = new Notification("facebook", "Новый пост от пользователя: JohnDoe");
+
+
+        manager.sendNotification(emailNotification);
+        manager.sendNotification(smsNotification);
+        manager.sendNotification(pushNotification);
     }
 }
