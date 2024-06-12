78cf7a82ab2e63c740a87665e4091510473a7f36
Mikhail Fokin <fokinms@gmail.com>
2024-06-13 01:52:57 +0400

    BJS2-9374 Gmail Rich Filters

---
+++ src/main/java/faang/school/godbless/BJS2_9374/Email.java
@@ -0,0 +1,14 @@
+package faang.school.godbless.BJS2_9374;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.ToString;
+
+@AllArgsConstructor
+@Getter
+@ToString
+public class Email {
+    String subject;
+    String body;
+    boolean isImportant;
+}
---
+++ src/main/java/faang/school/godbless/BJS2_9374/EmailProcessor.java
@@ -0,0 +1,18 @@
+package faang.school.godbless.BJS2_9374;
+
+import java.util.List;
+import java.util.function.Consumer;
+import java.util.function.Function;
+import java.util.function.Predicate;
+
+public class EmailProcessor {
+
+    public void processEmails(List<Email> emails, Predicate<Email> filter, Consumer<Email> handler, Function<Email, String> transform) {
+        for (Email email : emails) {
+            if (filter.test(email)) {
+                handler.accept(email);
+                System.out.println(transform.apply(email));
+            }
+        }
+    }
+}
