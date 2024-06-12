ca6f33f7e3c97ad9b580be124b7d382aae8657fe
Andrey Kuzmin <Andreytko@mail.ru>
2024-06-12 20:07:05 +0300

    fixed

--- src/main/java/faang/school/godbless/Task_Lord_of_the_Rings/Main.java
+++ src/main/java/faang/school/godbless/Task_Lord_of_the_Rings/Main.java
@@ -2,7 +2,6 @@ package faang.school.godbless.Task_Lord_of_the_Rings;
 
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
