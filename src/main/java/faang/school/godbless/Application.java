9cf4fd6f4eb4e6d9d8d4907b6be2d7f863afe3b8
Sasha Chekalkin <crutoipatz@yandex.ru>
2024-06-12 15:52:18 +0300

    int1nglence|Sasha|BJS2-7053 - Игра престолов

--- src/main/java/faang/school/godbless/Application.java
+++ src/main/java/faang/school/godbless/Application.java
@@ -12,15 +12,15 @@ import static faang.school.godbless.User.findHobbyLovers;
 
 public class Application{
     public static void main(String[] args){
-    Set<String> userValues1 = new HashSet<>(Arrays.asList("Чтение","Боулинг"));
-    Set<String> userValues2 = new HashSet<>(Arrays.asList("Плавание","Рисование"));
-    Set<String> userValues3 = new HashSet<>(Arrays.asList("Английский","Баскетбол"));
-    Set<String> userValues4 = new HashSet<>(Arrays.asList("Скоропись","Футбол"));
+    Set<String> userIvan = new HashSet<>(Arrays.asList("Чтение","Боулинг"));
+    Set<String> userDima = new HashSet<>(Arrays.asList("Плавание","Рисование"));
+    Set<String> userVova = new HashSet<>(Arrays.asList("Английский","Баскетбол"));
+    Set<String> userOleg = new HashSet<>(Arrays.asList("Скоропись","Футбол"));
 
-    User Ivan = new User(1,"Ivan",20,userValues1);
-    User Dima = new User(2,"Dima",25,userValues2);
-    User Vova = new User(3,"Vova",19,userValues3);
-    User Oleg = new User(4,"Oleg",18,userValues4);
+    User Ivan = new User(1,"Ivan",20,userIvan);
+    User Dima = new User(2,"Dima",25,userDima);
+    User Vova = new User(3,"Vova",19,userVova);
+    User Oleg = new User(4,"Oleg",18,userOleg);
 
     List<User> userList = new ArrayList<>();
     userList.add(Ivan);
@@ -32,7 +32,7 @@ public class Application{
 
     Map<User , String> hobbyLovers = findHobbyLovers(userList , activity);
     for(Map.Entry<User , String> entry : hobbyLovers.entrySet()){
-        System.out.println(entry.getKey().name + " любит " + entry.getValue());
+        System.out.println(entry.getKey().name() + " любит " + entry.getValue());
     }
     }
 }
