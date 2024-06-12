eba7a4c836aaadbf1eb9d3b4e6f730f6e181f363
DemureZ <lopatin.g.n@yandex.ru>
2024-06-13 02:23:17 +0300

    Перемещение методов в отдельный класс

--- src/main/java/faang/school/godbless/BJS2_8259/Book.java
+++ src/main/java/faang/school/godbless/BJS2_8259/Book.java
@@ -1,12 +1,24 @@
 package faang.school.godbless.BJS2_8259;
 
-import lombok.AllArgsConstructor;
 import lombok.Data;
 
 @Data
-@AllArgsConstructor
 public class Book {
     private String title;
     private String author;
     private int year;
+
+    public Book(String title, String author, int year) {
+        if (title.isBlank()) {
+            System.out.println("Некорректное название книги");
+            return;
+        }
+        if (author.isBlank()) {
+            System.out.println("Некорректный автор");
+            return;
+        }
+        this.title = title;
+        this.author = author;
+        this.year = year;
+    }
 }
--- src/main/java/faang/school/godbless/BJS2_8259/Main.java
+++ src/main/java/faang/school/godbless/BJS2_8259/Main.java
@@ -1,38 +1,19 @@
 package faang.school.godbless.BJS2_8259;
 
-import java.util.HashMap;
-import java.util.Map;
-
 public class Main {
     public static void main(String[] args) {
-        Map<Book, String> library = new HashMap<>();
-        library.put(new Book("Book1", "Author1", 2000), "01");
-        library.put(new Book("Book2", "Author2", 2001), "02");
-        library.put(new Book("Book3", "Author3", 2002), "03");
-        library.put(new Book("Book4", "Author4", 2003), "04");
+        Library library = new Library();
+        library.addBook("01", new Book("Book1", "Author1", 2000));
+        library.addBook("02", new Book("Book2", "Author2", 2001));
+        library.addBook("03", new Book("Book3", "Author3", 2002));
+        library.addBook("04", new Book("Book4", "Author4", 2003));
 
-        printAllBooks(library);
+        library.printAllBooks();
         System.out.println();
-        removeBook(library, "Book3", "Author3", 2002);
-        printAllBooks(library);
+        library.removeBook("Book3", "Author3", 2002);
+        library.printAllBooks();
         System.out.println();
-        addBook(library, "03", new Book("Book3", "Author3", 2002));
-        printBookIndex(library, "Book3", "Author3", 2002);
-    }
-
-    public static void addBook(Map<Book, String> map, String index, Book book) {
-        map.put(book, index);
-    }
-
-    public static void removeBook(Map<Book, String> map, String title, String author, int year) {
-        map.remove(new Book(title, author, year));
-    }
-
-    public static void printBookIndex(Map<Book, String> map, String title, String author, int year) {
-        System.out.println(map.get(new Book(title, author, year)));
-    }
-
-    public static void printAllBooks(Map<Book, String> map) {
-        map.entrySet().forEach(System.out::println);
+        library.addBook("03", new Book("Book3", "Author3", 2002));
+        library.printBookIndex("Book3", "Author3", 2002);
     }
 }
