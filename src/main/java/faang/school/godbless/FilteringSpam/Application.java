f67965dd09a7019de7fde530edba09ec7ffe8b0b
Gevorg <a0964593@gmail.com>
2024-06-12 15:02:05 +0300

    First try

--- src/main/java/faang/school/godbless/FilteringSpam/Application.java
+++ src/main/java/faang/school/godbless/FilteringSpam/Application.java
@@ -1,4 +1,4 @@
-package faang.school.godbless;
+package faang.school.godbless.FilteringSpam;
 
 import java.util.Arrays;
 import java.util.List;
--- src/main/java/faang/school/godbless/FilteringSpam/MessageFilter.java
+++ src/main/java/faang/school/godbless/FilteringSpam/MessageFilter.java
@@ -1,4 +1,4 @@
-package faang.school.godbless;
+package faang.school.godbless.FilteringSpam;
 
 public interface MessageFilter {
     boolean filter(String message);
