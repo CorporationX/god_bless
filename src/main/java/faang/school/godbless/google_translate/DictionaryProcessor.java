45a3c77361417123cda2f5d5fe44897b36af9fa4
shade2903 <nighshade2903@gmail.com>
2024-06-12 11:29:02 +0300

    done task Google translate

---
+++ src/main/java/faang/school/godbless/google_translate/DictionaryProcessor.java
@@ -0,0 +1,9 @@
+package faang.school.godbless.google_translate;
+
+import java.util.function.BiConsumer;
+
+public class DictionaryProcessor {
+    public void processWord(String word, String translation, BiConsumer<String,String> consumer){
+         consumer.accept(word,translation);
+    }
+}
