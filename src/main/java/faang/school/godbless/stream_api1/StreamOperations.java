fac7cde24f52957924637e37e540a7bed670d26b
ravshanbekn <ravshanbeknabiyev.dev@gmail.com>
2024-06-12 21:39:02 +0500

    BJS2-10750 - Потренируем Stream API
    added static methods, processing integers and strings

---
+++ src/main/java/faang/school/godbless/stream_api1/StreamOperations.java
@@ -0,0 +1,51 @@
+package faang.school.godbless.stream_api1;
+
+import lombok.NonNull;
+
+import java.util.Comparator;
+import java.util.List;
+import java.util.Optional;
+import java.util.OptionalDouble;
+import java.util.function.Predicate;
+
+public class StreamOperations {
+    public static int evenSum(@NonNull List<Integer> list) {
+        return list.stream().filter(integer -> integer % 2 == 0).reduce(0, Integer::sum);
+    }
+
+    public static Optional<Integer> maxNumber(@NonNull List<Integer> list) {
+        return list.stream().max(Integer::compare);
+    }
+
+    public static OptionalDouble avgNumber(@NonNull List<Integer> list) {
+        return list.stream().mapToDouble(Double::valueOf).average();
+    }
+
+    public static long countStringsContaining(@NonNull List<String> list,
+                                              @NonNull String containing) {
+        return list.stream().filter(string -> string.contains(containing)).count();
+    }
+
+    public static List<String> remainContaining(@NonNull List<String> list,
+                                                @NonNull String containing) {
+        return list.stream().filter(string -> string.contains(containing)).toList();
+    }
+
+    public static List<String> sortStringsByLength(@NonNull List<String> list) {
+        return list.stream().sorted(Comparator.comparingInt(String::length)).toList();
+    }
+
+    public static boolean allMatcher(@NonNull List<String> list,
+                                     @NonNull Predicate<String> conditions) {
+        return list.stream().allMatch(conditions);
+    }
+
+    public static Optional<Integer> minGreaterThan(@NonNull List<Integer> list,
+                                                   int lowerBound) {
+        return list.stream().filter(integer -> integer > lowerBound).min(Integer::compare);
+    }
+
+    public static List<Integer> stringToItsLength(@NonNull List<String> list) {
+        return list.stream().map(String::length).toList();
+    }
+}
