4af25c0338ab320572ad20003584932dbacdb36e
Enemy095 <salam.tahaev@mail.ru>
2024-06-13 00:16:09 +0300

    cleaned up the object and fixed the logic

--- src/main/java/faang/school/godbless/instagram/FilterProcessor.java
+++ src/main/java/faang/school/godbless/instagram/FilterProcessor.java
@@ -5,17 +5,17 @@ import java.util.function.Function;
 public class FilterProcessor {
     public static void main(String[] args) {
         Image image = new Image("море", "без эффектов");
-        Function<Image, Image> function1 = i -> new Image(i.getNameF(), "применен фильтр");
-        Function<Image, Image> function2 = i -> new Image(i.getNameF(), "Наложено изображение");
+        Function<Image, Image> function1 = i -> new Image(i.getNameF() + "f1 ",  "применен фильтр");
+        Function<Image, Image> function2 = i -> new Image(i.getNameF() + "f2", "Наложено изображение");
         System.out.println(applyFilter(image, function1));
-        System.out.println(combineFilters(image,function1, function2));
+        System.out.println(applyFilter(image,combineFilters(function1, function2)));
     }
 
     public static Image applyFilter(Image image, Function<Image, Image> function) {
         return function.apply(image);
     }
 
-    public static Image combineFilters(Image image, Function<Image, Image> function1, Function<Image, Image> function2) {
-        return applyFilter(image, function1.andThen(function2));
+    public static Function<Image, Image> combineFilters(Function<Image, Image> function1, Function<Image, Image> function2) {
+        return  function1.andThen(function2);
     }
 }
