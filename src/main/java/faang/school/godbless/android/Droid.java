79960d2d281ba615da3bf2092bdc6f9b56fabf82
Nikita2768 <vechkinnikita22@gmail.com>
2024-06-12 14:00:55 +0500

    finish task droid secrets

---
+++ src/main/java/faang/school/godbless/android/Droid.java
@@ -0,0 +1,24 @@
+package faang.school.godbless.android;
+
+import lombok.Data;
+
+import java.util.stream.Collectors;
+
+public class Droid {
+
+    public String sendEncryptedMessage(Droid enotherDroid, String message, int keyEncryption) {
+        DroidMessageEncryptor encryptor = (mess, key) -> mess.chars()
+                .map(c -> c * key)
+                .mapToObj(c -> String.valueOf((char) c))
+                .collect(Collectors.joining());
+        return encryptor.encryptionMessage(message, keyEncryption);
+    }
+
+    public String receiveEncryptedMessage(String message, int keyEncryption) {
+        DroidMessageReceiver receiver = (mess, key) -> mess.chars()
+                .map(c-> c / key)
+                .mapToObj(c -> String.valueOf((char) c))
+                .collect(Collectors.joining());
+        return receiver.receiveEncryptedMessage(message, keyEncryption);
+    }
+}
