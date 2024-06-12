4fd8cf9d9db0e12d4379f3311154c5a846bd7b9a
DemureZ <lopatin.g.n@yandex.ru>
2024-06-12 13:44:09 +0300

    Создание функциолнального интерфейса и использование лямбды

---
+++ src/main/java/faang/school/godbless/BJS2_9311/SpellAction.java
@@ -0,0 +1,6 @@
+package faang.school.godbless.BJS2_9311;
+
+@FunctionalInterface
+public interface SpellAction {
+    String spell(String spellName);
+}
---
+++ src/main/java/faang/school/godbless/BJS2_9311/SpellCaster.java
@@ -0,0 +1,8 @@
+package faang.school.godbless.BJS2_9311;
+
+public class SpellCaster {
+    public void cast(String spellName, SpellAction action) {
+        System.out.println(action.spell(spellName));
+    }
+
+}
