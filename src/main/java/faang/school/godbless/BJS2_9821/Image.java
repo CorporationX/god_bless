114af305258af496cd694a475b0a258581de6bc1
Mikhail Fokin <fokinms@gmail.com>
2024-06-13 00:43:33 +0400

    BJS2-9821 Фильтры Instagram

---
+++ src/main/java/faang/school/godbless/BJS2_9821/Image.java
@@ -0,0 +1,13 @@
+package faang.school.godbless.BJS2_9821;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.ToString;
+
+@AllArgsConstructor
+@Getter
+@ToString
+public class Image {
+    String name;
+    String description;
+}
