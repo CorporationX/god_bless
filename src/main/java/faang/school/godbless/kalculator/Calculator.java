20b84f224c29b43c38a0350d14d9cdc0de4573f7
Enemy095 <salam.tahaev@mail.ru>
2024-06-12 14:05:29 +0300

    remove generics, moved the solution to a new class

--- src/main/java/faang/school/godbless/kalculator/Calculator.java
+++ src/main/java/faang/school/godbless/kalculator/Calculator.java
@@ -1,6 +1,6 @@
 package faang.school.godbless.kalculator;
 
 @FunctionalInterface
-public interface Calculator<T, K, U> {
-    U interfaceCalculate(T t, K k);
+public interface Calculator {
+    int interfaceCalculate(int I, int k);
 }
--- src/main/java/faang/school/godbless/kalculator/Main.java
+++ src/main/java/faang/school/godbless/kalculator/Main.java
@@ -5,23 +5,7 @@ import java.util.List;
 public class Main {
     public static void main(String[] args) {
         List<Integer> listNumbers = List.of(1, 3, 5);
-        System.out.println(product(listNumbers));
-        System.out.println(sum(listNumbers));
-    }
-
-    public static int calculate(List<Integer> nums, Calculator<Integer, Integer, Integer> calculator) {
-        int buffer = nums.get(0);
-        for (int i = 1; i < nums.size(); i++) {
-            buffer = calculator.interfaceCalculate(buffer, nums.get(i));
-        }
-        return buffer;
-    }
-
-    public static int product(List<Integer> nums) {
-        return calculate(nums, (s, h) -> s * h);
-    }
-
-    public static int sum(List<Integer> nums) {
-        return calculate(nums, (s, h) -> s + h);
+        System.out.println(CalculateList.product(listNumbers));
+        System.out.println(CalculateList.sum(listNumbers));
     }
 }
