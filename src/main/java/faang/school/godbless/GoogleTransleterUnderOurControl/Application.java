10c626ce5649bc7c3ae996ab00dc27845e635e85
Gevorg <a0964593@gmail.com>
2024-06-12 14:59:44 +0300

    First try

--- src/main/java/faang/school/godbless/GoogleTransleterUnderOurControl/Application.java
+++ src/main/java/faang/school/godbless/GoogleTransleterUnderOurControl/Application.java
@@ -1,4 +1,4 @@
-package faang.school.godbless;
+package faang.school.godbless.GoogleTransleterUnderOurControl;
 
 import java.util.HashMap;
 import java.util.Map;
--- src/main/java/faang/school/godbless/GoogleTransleterUnderOurControl/DictionaryProcessor.java
+++ src/main/java/faang/school/godbless/GoogleTransleterUnderOurControl/DictionaryProcessor.java
@@ -1,4 +1,4 @@
-package faang.school.godbless;
+package faang.school.godbless.GoogleTransleterUnderOurControl;
 
 import java.util.function.BiConsumer;
 
