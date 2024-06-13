ebc5cc4487edb87b68ed682f3157d6589531d758
Sergey Shkaev <shkaev.ss@mail.ru>
2024-06-13 16:23:52 +0300

    Двойной кэш. Созданы классы Student, Subject, Main (с методами по условию задачи).

---
+++ src/main/java/faang/school/godbless/task_16_double_cache/Student.java
@@ -0,0 +1,4 @@
+package faang.school.godbless.task_16_double_cache;
+
+public record Student(int id, String name) {
+}
---
+++ src/main/java/faang/school/godbless/task_16_double_cache/Subject.java
@@ -0,0 +1,4 @@
+package faang.school.godbless.task_16_double_cache;
+
+public record Subject(int id, String name) {
+}
