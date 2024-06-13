d43338951f16c113a0e669ddfb3718759a844a07
Nikita Volzhin <vlzh.nik@mail.ru>
2024-06-14 00:11:44 +0500

    BJS2-9970 -  removed the boolean filtered field from the processMessage method

--- src/main/java/faang/school/godbless/MessageProcessor.java
+++ src/main/java/faang/school/godbless/MessageProcessor.java
@@ -8,13 +8,11 @@ public class MessageProcessor {
 
     @NonNull
     public boolean processMessage(String message, List<MessageFilter> messageProcessing) {
-        boolean filtered = true;
-
         for (MessageFilter messageFilter : messageProcessing) {
             if (!messageFilter.filter(message)) {
-                filtered = false;
+                return false;
             }
         }
-        return filtered;
+        return true;
     }
 }
