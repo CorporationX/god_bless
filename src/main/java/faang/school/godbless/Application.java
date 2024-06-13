ab4a2e952a8b5de436580079ff4e8c8b39f8098c
Kilotas <Alendos87@gmail.com>
2024-06-13 19:34:16 +0500

    Merge remote-tracking branch 'origin/Omarov-Alen-BJS2-8651' into Omarov-Alen-BJS2-8651

--- src/main/java/faang/school/godbless/Application.java
+++ src/main/java/faang/school/godbless/Application.java
@@ -1,11 +1,7 @@
 package faang.school.godbless;
-import java.util.HashMap;
-import java.util.Map;
-
 
 public class Application {
     public static void main(String[] args) {
-
+        System.out.println("Hello world!");
     }
-
 }
