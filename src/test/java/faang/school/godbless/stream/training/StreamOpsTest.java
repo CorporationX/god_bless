7d2870429b63d811cd9a455e014bd3d46c2bd14e
Stanislav Ovsyankin <cancion2able@gmail.com>
2024-06-13 22:25:20 +0500

    Удалить ненужный импорт

--- src/test/java/faang/school/godbless/stream/training/StreamOpsTest.java
+++ src/test/java/faang/school/godbless/stream/training/StreamOpsTest.java
@@ -3,7 +3,6 @@ package faang.school.godbless.stream.training;
 import org.junit.jupiter.api.Test;
 
 import java.util.List;
-import java.util.stream.Stream;
 
 import static org.junit.jupiter.api.Assertions.*;
 
