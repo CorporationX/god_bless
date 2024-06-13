0ee7a21f10d463b7dcdf38817c002312b22d9ad9
Artem <xotabggg@gmail.com>
2024-06-13 22:45:32 +0700

    BJS2-7685-corrected the method name

--- src/main/java/faang/school/godbless/Example.java
+++ src/main/java/faang/school/godbless/Example.java
@@ -2,7 +2,7 @@ package faang.school.godbless;
 
 public class Example {
 
-    public int[] revers(int[] reverse) {
+    public int[] reverse(int[] reverse) {
         int[] result = new int[reverse.length];
         for (int i = reverse.length-1; i > 0; i--) {
             for (int j = 0; j < reverse.length; j++) {
