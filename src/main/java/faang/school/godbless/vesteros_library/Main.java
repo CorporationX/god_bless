b9462fc19e3e85574c2260498aa52c60f719af85
Vladimir Haiduk <nightshade2903@gmail.com>
2024-06-13 22:27:56 +0300

    BJS2-8274 LibraryOfWesteros
     в методах removeBook() и findBook() используют ключ возвращенный из мапы по полям которые приходят на вход

--- src/main/java/faang/school/godbless/vesteros_library/Main.java
+++ src/main/java/faang/school/godbless/vesteros_library/Main.java
@@ -14,7 +14,7 @@ public class Main {
         BOOKS.put(history, "1");
         BOOKS.put(romantic, "7");
 
-        addBook(new Book("Horse fighting","Unknown", 1785), "2");
+        addBook(new Book("Horse fighting", "Unknown", 1785), "2");
         showAllBooks();
         removeBook("History of Vesteros", "Alan Rickhman", 1923);
         findBook("Medecine almanac", "Robert Grey", 1995);
@@ -25,14 +25,24 @@ public class Main {
     }
 
     public static void removeBook(String title, String author, int year) {
-        BOOKS.remove(new Book(title, author, year));
+        BOOKS.remove(findBookByAllFields(title, author, year));
     }
 
     public static void findBook(String title, String author, int year) {
-        System.out.println(BOOKS.get(new Book(title, author, year)));
+        System.out.println(BOOKS.get(findBookByAllFields(title, author, year)));
     }
 
     public static void showAllBooks() {
         BOOKS.forEach((book, section) -> System.out.println(book + " section: " + section));
+    }
+
+    private static Book findBookByAllFields(String title, String author, int year) {
+        Book book = BOOKS.keySet().stream().filter(b ->
+                b.getTitle().equals(title) && b.getAuthor().equals(author) && b.getYear() == year).findFirst().orElse(null);
+        if (book == null) {
+            throw new IllegalArgumentException(String.format("Book with tittle: %s , author: %s , year: %d is not found",
+                    title, author, year));
+        }
+        return book;
     }
 }
