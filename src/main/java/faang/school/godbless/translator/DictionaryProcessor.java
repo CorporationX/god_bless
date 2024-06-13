fe97dcd026a3e1dfd6f27714a37984efc277b0b8
OlgaK888 <olgakurilovi4@gmail.com>
2024-06-13 17:34:29 +0300

    Created class Student and grouped students with using HashMap

---
+++ src/main/java/faang/school/godbless/translator/DictionaryProcessor.java
@@ -0,0 +1,11 @@
+package faang.school.godbless.translator;
+
+import java.util.function.BiConsumer;
+
+public class DictionaryProcessor {
+    public void processWord(String word, String translation,
+                            BiConsumer<String, String> PutIntoDictionary) {
+
+
+    }
+}
