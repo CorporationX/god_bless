3d13c756e406d6bac7ac618d040e60e8fb6ec220
V1nGeri|Artem Radkevich <artemka20040613@gmail.com>
2024-06-13 13:02:29 +0300

    вынес методы в класс книг

--- src/main/java/faang/school/godbless/Book.java
+++ src/main/java/faang/school/godbless/Book.java
@@ -3,6 +3,7 @@ package faang.school.godbless;
 import lombok.Getter;
 import lombok.Setter;
 
+import java.util.Map;
 import java.util.Objects;
 
 @Getter
@@ -31,4 +32,71 @@ public class Book {
     public int hashCode() {
         return Objects.hash(title, author, year);
     }
+
+    public static void addNewBook(Map<Book, String> books, String title, String author, int year, String place) {
+        books.put(new Book(title, author, year), place);
+        System.out.println("The book has been added to place " + place);
+    }
+
+    public static void deleteBookByTitle(Map<Book, String> books, String title) {
+        for (Map.Entry<Book, String> entry : books.entrySet()) {
+            if (entry.getKey().getTitle().equals(title)) {
+                books.remove(entry.getKey());
+                System.out.println("The book was successfully deleted");
+            }
+        }
+    }
+
+    public static void deleteBookByAuthor(Map<Book, String> books, String author) {
+        for (Map.Entry<Book, String> entry : books.entrySet()) {
+            if (entry.getKey().getAuthor().equals(author)) {
+                books.remove(entry.getKey());
+                System.out.println("The book was successfully deleted");
+            }
+        }
+    }
+
+    public static void deleteBookByYear(Map<Book, String> books, int year) {
+        for (Map.Entry<Book, String> entry : books.entrySet()) {
+            if (entry.getKey().getYear() == year) {
+                books.remove(entry.getKey());
+                System.out.println("The book was successfully deleted");
+            }
+        }
+    }
+
+    public static void printAllBooks(Map<Book, String> books) {
+        for (Map.Entry<Book, String> entry : books.entrySet()) {
+            System.out.println("Title: " + entry.getKey().getTitle() + " | Author: " + entry.getKey().getAuthor() +
+                    " | Year: " + entry.getKey().getYear() + " | Place: " + entry.getValue());
+        }
+    }
+
+    public static void findBookByTitle(Map<Book, String> books, String title) {
+        for (Map.Entry<Book, String> entry : books.entrySet()) {
+            if (entry.getKey().getTitle().equals(title)) {
+                System.out.println("Title: " + entry.getKey().getTitle() + " | Author: " + entry.getKey().getAuthor() +
+                        " | Year: " + entry.getKey().getYear() + " | Place: " + entry.getValue());
+            }
+        }
+    }
+
+    public static void findBookByAuthor(Map<Book, String> books, String author) {
+        for (Map.Entry<Book, String> entry : books.entrySet()) {
+            if (entry.getKey().getAuthor().equals(author)) {
+                System.out.println("Title: " + entry.getKey().getTitle() + " | Author: " + entry.getKey().getAuthor() +
+                        " | Year: " + entry.getKey().getYear() + " | Place: " + entry.getValue());
+            }
+        }
+    }
+
+    public static void findBookByYear(Map<Book, String> books, int year) {
+        for (Map.Entry<Book, String> entry : books.entrySet()) {
+            if (entry.getKey().getYear() == year) {
+                System.out.println("Title: " + entry.getKey().getTitle() + " | Author: " + entry.getKey().getAuthor() +
+                        " | Year: " + entry.getKey().getYear() + " | Place: " + entry.getValue());
+            }
+        }
+    }
+
 }
--- src/main/java/faang/school/godbless/Main.java
+++ src/main/java/faang/school/godbless/Main.java
@@ -11,94 +11,29 @@ public class Main {
         books.put(new Book("The Hedge Knight", "George R.R. Martin", 1998), "3B");
         books.put(new Book("The Sworn Sword", "George R.R. Martin", 2003), "22G");
 
-        printAllBooks(books);
+        Book.printAllBooks(books);
         System.out.println("_____________________________");
 
 
-        addNewBook(books,"The Princess and the Queen","George R.R. Martin",2013,"666B");
+        Book.addNewBook(books, "The Princess and the Queen", "George R.R. Martin", 2013, "666B");
         System.out.println("_____________________________");
 
-        findBookByTitle(books,"The Hedge Knight");
+        Book.findBookByTitle(books, "The Hedge Knight");
         System.out.println("_____________________________");
 
-        findBookByAuthor(books,"George R.R. Martin");
+        Book.findBookByAuthor(books, "George R.R. Martin");
 
         System.out.println("_____________________________");
 
-        findBookByYear(books,2003);
+        Book.findBookByYear(books, 2003);
         System.out.println("_____________________________");
 
-        deleteBookByTitle(books,"The Sworn Sword");
+        Book.deleteBookByTitle(books, "The Sworn Sword");
         System.out.println("_____________________________");
-        printAllBooks(books);
+        Book.printAllBooks(books);
 
         System.out.println("_____________________________");
     }
 
-    private static void addNewBook(Map<Book, String> books, String title, String author, int year, String place) {
-        books.put(new Book(title, author, year), place);
-        System.out.println("The book has been added to place " + place);
-    }
-
-    private static void deleteBookByTitle(Map<Book, String> books, String title) {
-        for (Map.Entry<Book, String> entry : books.entrySet()) {
-            if (entry.getKey().getTitle().equals(title)) {
-                books.remove(entry.getKey());
-                System.out.println("The book was successfully deleted");
-            }
-        }
-    }
-
-    private static void deleteBookByAuthor(Map<Book, String> books, String author) {
-        for (Map.Entry<Book, String> entry : books.entrySet()) {
-            if (entry.getKey().getAuthor().equals(author)) {
-                books.remove(entry.getKey());
-                System.out.println("The book was successfully deleted");
-            }
-        }
-    }
-
-    private static void deleteBookByYear(Map<Book, String> books, int year) {
-        for (Map.Entry<Book, String> entry : books.entrySet()) {
-            if (entry.getKey().getYear() == year) {
-                books.remove(entry.getKey());
-                System.out.println("The book was successfully deleted");
-            }
-        }
-    }
-
-    private static void printAllBooks(Map<Book, String> books) {
-        for (Map.Entry<Book, String> entry : books.entrySet()) {
-            System.out.println("Title: " + entry.getKey().getTitle() + " | Author: " + entry.getKey().getAuthor() +
-                    " | Year: " + entry.getKey().getYear() + " | Place: " + entry.getValue());
-        }
-    }
-
-    private static void findBookByTitle(Map<Book, String> books, String title) {
-        for (Map.Entry<Book, String> entry : books.entrySet()) {
-            if (entry.getKey().getTitle().equals(title)) {
-                System.out.println("Title: " + entry.getKey().getTitle() + " | Author: " + entry.getKey().getAuthor() +
-                        " | Year: " + entry.getKey().getYear() + " | Place: " + entry.getValue());
-            }
-        }
-    }
-
-    private static void findBookByAuthor(Map<Book, String> books, String author) {
-        for (Map.Entry<Book, String> entry : books.entrySet()) {
-            if (entry.getKey().getAuthor().equals(author)) {
-                System.out.println("Title: " + entry.getKey().getTitle() + " | Author: " + entry.getKey().getAuthor() +
-                        " | Year: " + entry.getKey().getYear() + " | Place: " + entry.getValue());
-            }
-        }
-    }
-
-    private static void findBookByYear(Map<Book, String> books, int year) {
-        for (Map.Entry<Book, String> entry : books.entrySet()) {
-            if (entry.getKey().getYear() == year) {
-                System.out.println("Title: " + entry.getKey().getTitle() + " | Author: " + entry.getKey().getAuthor() +
-                        " | Year: " + entry.getKey().getYear() + " | Place: " + entry.getValue());
-            }
-        }
-    }
 
 }
