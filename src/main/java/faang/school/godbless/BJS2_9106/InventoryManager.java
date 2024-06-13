14882fa672550095470192f5883b9941e5b36520
Roman <roma.koshetov@mail.ru>
2024-06-13 12:08:14 +0300

    BJS2-9106

--- src/main/java/faang/school/godbless/BJS2_9106/InventoryManager.java
+++ src/main/java/faang/school/godbless/BJS2_9106/InventoryManager.java
@@ -4,15 +4,16 @@ import java.util.List;
 import java.util.function.Consumer;
 import java.util.function.Function;
 import java.util.function.Predicate;
+import java.util.function.UnaryOperator;
 
 public class InventoryManager {
-    public void addItem(Character character, Item item, Consumer<Item> action) {
-        if (character == null || item == null || action == null) {
+    public void addItem(Character character, Item item, Consumer<Item> printStatus) {
+        if (character == null || item == null || printStatus == null) {
             throw new IllegalArgumentException("Some arguments was illegal");
         }
 
         character.getInventory().add(item);
-        action.accept(item);
+        printStatus.accept(item);
     }
 
     public void removeItem(Character character, Predicate<Item> condition) {
@@ -20,30 +21,15 @@ public class InventoryManager {
             throw new IllegalArgumentException("Some arguments was illegal");
         }
 
-        List<Item> inventory = character.getInventory();
-
-        for (var item: inventory) {
-            if (condition.test(item)) {
-                inventory.remove(item);
-                break;
-            }
-        }
+        character.getInventory().removeIf(condition);
     }
 
-    public void updateItem(Character character, Predicate<Item> condition, Function<Item, Item> func) {
-        if (character == null || condition == null || func == null) {
+    public void updateItem(Character character, Predicate<Item> condition, Function<Item, Item> updateFunction) {
+        if (character == null || condition == null || updateFunction == null) {
             throw new IllegalArgumentException("Some arguments was illegal");
         }
 
-        List<Item> inventory = character.getInventory();
-
-        for (int i = 0; i < inventory.size(); i++) {
-            Item item = inventory.get(i);
-
-            if (condition.test(item)) {
-                inventory.set(i, func.apply(item));
-                break;
-            }
-        }
+        //  применяет функцию под все подходящие условия и заменяет эти объекты в inventory
+        character.getInventory().replaceAll(item -> condition.test(item) ? updateFunction.apply(item): item);
     }
 }
