c2f3e6069b87258dc698836f585939b5c8cbc460
OlgaK888 <olgakurilovi4@gmail.com>
2024-06-13 21:09:08 +0300

    Using Function Interfaces Predicate, Consumer and Function in one method

---
+++ src/main/java/faang/school/godbless/gmail_rich_filters/EmailProcessor.java
@@ -0,0 +1,19 @@
+package faang.school.godbless.gmail_rich_filters;
+
+import java.util.List;
+import java.util.function.Consumer;
+import java.util.function.Function;
+import java.util.function.Predicate;
+
+public class EmailProcessor {
+    public void processEmails(List<Email> emails,
+                              Predicate<Email> filtration,
+                              Consumer<Email> processing,
+                              Function<Email, String> transformation) {
+        for (Email email : emails) {
+            System.out.println(filtration.test(email));
+            processing.accept(email);
+            System.out.println(transformation.apply(email));
+        }
+    }
+}
