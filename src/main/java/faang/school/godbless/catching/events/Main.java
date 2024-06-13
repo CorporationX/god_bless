a6a8ec017e061f00bcb351a6f2f948a5fb6ee04b
sta1er <vlad.sokogubov@gmail.com>
2024-06-13 17:46:14 +0300

    Вроде все изменил кроме removeIf

--- src/main/java/faang/school/godbless/catching/events/Main.java
+++ src/main/java/faang/school/godbless/catching/events/Main.java
@@ -1,60 +1,10 @@
 package faang.school.godbless.catching.events;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
+
+import static faang.school.godbless.catching.events.EventService.*;
 
 public class Main {
-
-    private static Map<Integer, StreamEvent> eventById = new HashMap<>();
-    private static Map<String, List<StreamEvent>> eventByType = new HashMap<>();
-
-    public static void addEvent(StreamEvent event) {
-        eventById.put(event.getId(), event);
-        // containsKey() не работает
-        if (!eventByType.containsKey(event.getEventType())) {
-            eventByType.put(event.getEventType(), new ArrayList<>());
-        }
-        eventByType.get(event.getEventType()).add(event);
-    }
-
-
-    public static StreamEvent findEventById(int id) {
-        if(id != 0) {
-            return eventById.get(id);
-        }
-        else {
-            System.err.println("Error!");
-            return null;
-        }
-    }
-
-    public static List<StreamEvent> findEventsByType(String eventType) {
-        if(eventType != null) {
-            return eventByType.get(eventType);
-        }
-        else {
-            System.err.println("Error!");
-            return null;
-        }
-    }
-
-    public static void removeEventById(int id) {
-        if(id != 0 && !eventById.containsKey(id)) {
-            eventById.remove(id);
-        }
-        else {
-            System.err.println("Error!");
-        }
-
-    }
-
-    public static void printAllEvents() {
-        for (Map.Entry<Integer, StreamEvent> entry : eventById.entrySet()) {
-            System.out.println(entry.toString());
-        }
-    }
 
 
     public static void main(String[] args) {
