ab1c09f033d49f22c9f03f16c9d3a37bdc51336d
cirill98 <100434317+cirill98@users.noreply.github.com>
2024-06-13 09:43:19 +0500

    Использовал стрим вместо replaceAll

--- src/main/java/faang/school/godbless/GmailRichFilters/EmailProcessor.java
+++ src/main/java/faang/school/godbless/GmailRichFilters/EmailProcessor.java
@@ -5,15 +5,17 @@ import java.util.ListIterator;
 import java.util.function.Consumer;
 import java.util.function.Function;
 import java.util.function.Predicate;
+import java.util.stream.Collectors;
 
 public class EmailProcessor {
     void processEmails(List<Email> emails, Predicate<Email> predicate, Consumer<Email> consumer, Function<Email, String> function) {
-        emails.replaceAll(email -> {
-            if (predicate.test(email)) {
-                consumer.accept(email);
-                email.setBody(function.apply(email));
-            }
-            return email;
-        });
+        emails.stream()
+                .peek(email -> {
+                    if (predicate.test(email)) {
+                        consumer.accept(email);
+                        email.setBody(function.apply(email));
+                    }
+                })
+                .toList();
     }
 }
