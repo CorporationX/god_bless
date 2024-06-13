a071a53c561c12a1ccd3c4a1a6a808441913bb5b
Rauan Kaztaev <rtishtyk@gmail.com>
2024-06-13 23:24:09 +0500

    Потренируем StreamAPI 1

---
+++ src/main/java/faang/school/godbless/training_stream_api/StreamApiTraining.java
@@ -0,0 +1,59 @@
+package faang.school.godbless.training_stream_api;
+
+import lombok.NonNull;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class StreamApiTraining {
+
+    public static void main(String[] args) {
+        List<Integer> nums = List.of(1, 2, 2443, 6, 4, 5);
+        List<String> words = List.of("Rauan", "Rasul", "Alex", "Yan", "Samanta", "");
+        System.out.println("sumEvenNumbers: " + sumEvenNumbers(nums));
+        System.out.println("findMaxNumber: " + findMaxNumber(nums));
+        System.out.println("findAverageNumber: " + findAverageNumber((nums)));
+        System.out.println("countingStringsWith: " + countStringsStartsWith(words, 'R'));
+        System.out.println("filteredBySubstring: " + filteredBySubstring(words, "a"));
+        System.out.println("sortWordsByLength: " + sortWordsByLength(words));
+        System.out.println("isMatch: " + isMatch(words));
+        System.out.println("findMinFromBiggerNums: " + findMinFromBiggerNums(nums, 5));
+        System.out.println("mapStringsByLength: " + mapStringsByLength(words));
+    }
+
+    public static int sumEvenNumbers(@NonNull List<Integer> numbers) {
+        return numbers.stream().filter(num -> num % 2 == 0).mapToInt(num -> num).sum();
+    }
+
+    public static int findMaxNumber(@NonNull List<Integer> numbers) {
+        return numbers.stream().mapToInt(num -> num).max().getAsInt();
+    }
+
+    public static double findAverageNumber(@NonNull List<Integer> numbers) {
+        return numbers.stream().mapToInt(num -> num).average().getAsDouble();
+    }
+
+    public static int countStringsStartsWith(@NonNull List<String> words, char s) {
+        return words.stream().filter(word -> word.startsWith(String.valueOf(s))).mapToInt(o -> 1).sum();
+    }
+
+    public static List<String> filteredBySubstring(@NonNull List<String> words, @NonNull String subString) {
+        return words.stream().filter(word -> word.contains(subString)).collect(Collectors.toList());
+    }
+
+    public static List<String> sortWordsByLength(@NonNull List<String> words) {
+        return words.stream().sorted((o1, o2) -> o1.length() - o2.length()).collect(Collectors.toList());
+    }
+
+    public static boolean isMatch(@NonNull List<String> words) {
+        return words.stream().allMatch(word -> !word.isBlank());
+    }
+
+    public static int findMinFromBiggerNums(@NonNull List<Integer> numbers, int source) {
+        return numbers.stream().filter(num -> num > source).mapToInt(num -> num).min().getAsInt();
+    }
+
+    public static List<Integer> mapStringsByLength(@NonNull List<String> words) {
+        return words.stream().mapToInt(word -> word.length()).boxed().toList();
+    }
+}
