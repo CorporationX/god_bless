08ec8adbf10485e2893bdb4fd0d8a44632d3a4cf
Alen <Alendos87@gmail.com>
2024-06-13 11:45:52 +0500

    Filter processor

---
+++ src/main/java/faang/school/godbless/Image.java
@@ -0,0 +1,14 @@
+package faang.school.godbless;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Image {
+    private String filename;
+    private String description;
+}
