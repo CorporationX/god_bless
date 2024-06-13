9a125564a9ee976229b48d159a2ef827fda37730
OlgaK888 <olgakurilovi4@gmail.com>
2024-06-13 17:37:01 +0300

    Created method with using Functional Interface BiConsumer

--- src/main/java/faang/school/godbless/translator/DictionaryProcessor.java
+++ src/main/java/faang/school/godbless/translator/DictionaryProcessor.java
@@ -5,7 +5,5 @@ import java.util.function.BiConsumer;
 public class DictionaryProcessor {
     public void processWord(String word, String translation,
                             BiConsumer<String, String> PutIntoDictionary) {
-
-
     }
 }
