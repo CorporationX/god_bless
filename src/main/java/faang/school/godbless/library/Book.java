a9214dcd3a0023a2140f97a326323ea584678133
allakamen <akamenskikh@gmail.com>
2024-06-13 19:24:44 +0200

    changes: record and immutable for HashMap

--- src/main/java/faang/school/godbless/library/Book.java
+++ src/main/java/faang/school/godbless/library/Book.java
@@ -2,13 +2,7 @@ package faang.school.godbless.library;
 
 import lombok.*;
 
-@Getter
-@EqualsAndHashCode(of = {"title", "author", "year"})
-@ToString
-@AllArgsConstructor
-@NoArgsConstructor
-public class Book {
-    private String title;
-    private String author;
-    private int year;
+
+public record Book (String title, String author, int year) {
+
 }
--- src/main/java/faang/school/godbless/library/Main.java
+++ src/main/java/faang/school/godbless/library/Main.java
@@ -5,7 +5,7 @@ import java.util.Map;
 
 public class Main {
 
-    public static Map<Book, String> books = new HashMap<>();
+    public static final Map<Book, String> books = new HashMap<>();
 
     public static void main(String[] args) {
 
