3f991bfd9d7bf5d20b5ba5645a6bd6d3bd0ab6e5
vitalij_rusakevic <vvitalijj@gmail.com>
2024-06-13 22:01:20 +0300

    changed to record

--- src/main/java/BJS2_7189/User.java
+++ src/main/java/BJS2_7189/User.java
@@ -1,20 +1,12 @@
 package BJS2_7189;
 
-import lombok.AllArgsConstructor;
-import lombok.ToString;
-
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
 
-@ToString
-@AllArgsConstructor
-public class User {
-    private final int id;
-    private final String name;
-    private final int age;
-    private final Set<String> hobbies;
+
+public record User(int id, String name, int age, Set<String> hobbies) {
 
     public Map<User, String> findHobbyLovers(List<User> users, Set<String> activities) {
         Map<User, String> userHobbies = new HashMap<>();
