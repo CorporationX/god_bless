c1d5fe3e0f57cb6217d033862223bd5fefd38e9e
Andrey Kuzmin <Andreytko@mail.ru>
2024-06-13 23:40:18 +0300

    done decryption method!

--- src/main/java/faang/school/godbless/Droids/Droid.java
+++ src/main/java/faang/school/godbless/Droids/Droid.java
@@ -24,4 +24,21 @@ public class Droid {
         return encryptor.encrypt(message, key);
     }
 
+    public String receiveEncryptedMessage(String message, int key) {
+        if (message.isBlank() || key<=0 || key>26) throw new IllegalArgumentException("invalid args");
+        DroidMessageEncryptor decryptor = (message1, key1) -> {
+            StringBuilder result = new StringBuilder();
+            for (int i=0; i<message1.length(); i++) {
+                for (int j=0; j<=25; j++) {
+                    if (message1.charAt(i) == ALPHABET.charAt(j))  {
+                        result.append(ALPHABET.charAt((j - key1 + 26) % 26));
+                        break;
+                    }
+                }
+            }
+            return String.valueOf(result);
+        };
+        return decryptor.encrypt(message, key);
+    }
+
 }
--- src/main/java/faang/school/godbless/Droids/Main.java
+++ src/main/java/faang/school/godbless/Droids/Main.java
@@ -3,11 +3,13 @@ package faang.school.godbless.Droids;
 public class Main {
     public static void main(String[] args) {
 
-        Droid testDroid = new Droid();
+        Droid sender = new Droid();
+        Droid receiver = new Droid();
         int key = 3;
-        String message = "hello";
-        String msg = testDroid.sendEncryptedMessage(message,3);
-        System.out.println(msg);
-
+        String message = "abcxyz";
+        String encryptedMessage = sender.sendEncryptedMessage(message,3);
+        System.out.println(encryptedMessage);
+        String decryptedMessage = receiver.receiveEncryptedMessage(encryptedMessage,3);
+        System.out.println(decryptedMessage);
     }
 }
