73b19c6dae0e7acbe6e35d50d78d9f8210844d62
kpzxpf <35826257+kpzxpf@users.noreply.github.com>
2024-06-14 00:28:47 +0500

    Update Application.java

--- src/main/java/faang/school/godbless/Application.java
+++ src/main/java/faang/school/godbless/Application.java
@@ -2,20 +2,6 @@ package faang.school.godbless;
 
 public class Application {
     public static void main(String[] args) {
-        Character frodo = new Character("Frodo");
-        Item ring = new Item("The One Ring", 1000);
-
-        InventoryManager manager = new InventoryManager();
-
-        System.out.println(frodo.toString());
-
-        manager.addItem(frodo, ring, (item) -> System.out.println(item.getName() + " was added to the inventory."));
-
-        System.out.println(frodo.toString());
-
-        manager.updateItem(frodo, (item) -> item.getName().equals("The One Ring"), (item) -> new Item(item.getName(), item.getValue() * 2));
-        manager.removeItem(frodo, (item) -> item.getName().equals("The One Ring"));
-
-        System.out.println(frodo.toString());
+        System.out.println("Hello world!");
     }
 }
