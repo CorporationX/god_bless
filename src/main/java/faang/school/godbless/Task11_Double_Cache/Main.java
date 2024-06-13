026c9f617a76f03021d4b01cf722544338d94e81
Andrey Kuzmin <Andreytko@mail.ru>
2024-06-13 21:17:10 +0300

    доработал еще метода, поправил классы

--- src/main/java/faang/school/godbless/Task11_Double_Cache/Main.java
+++ src/main/java/faang/school/godbless/Task11_Double_Cache/Main.java
@@ -16,8 +16,9 @@ public class Main {
         marks.put(student, subjectMarks);
     }
 
+    //работает, если есть есть студент, и у него нет оценки; если оценка есть, то ничего не делает
     public static void addSubjectForStudent(Subject subject, int mark, Student student) {
-        marks.compute(student, (key, value) -> value).put(subject, mark);
+        marks.computeIfPresent(student, (key, value) -> value).computeIfAbsent(subject, (val1) ->mark);
     }
 
     public static void removeStudentAndMarks(Student student) {
@@ -27,11 +28,12 @@ public class Main {
     public static void showStudentsAndMarks() {
         for (Map.Entry<Student, Map<Subject, Integer>> entry : marks.entrySet()) {
             String name = entry.getKey().getName();
-            System.out.println("Student: " + name);
+            System.out.print("Student: " + name + " ");
             for (Map.Entry<Subject, Integer> subjectMark : entry.getValue().entrySet()) {
-                System.out.println("Subject: " + subjectMark.getKey().getName() + ", mark: "
-                        + subjectMark.getValue());
+                System.out.print("Subject: " + subjectMark.getKey().getName() + ", mark: "
+                        + subjectMark.getValue() + " ");
             }
+            System.out.println();
         }
     }
 
@@ -41,11 +43,13 @@ public class Main {
     }
 
     public static void addStudentToSubject(Student student, Subject subject) {
-        attenders.compute(subject, (key, value) -> value).add(student);
+        if (attenders.get(subject)==null) throw new IllegalArgumentException("no such student");
+        if (attenders.get(subject).contains(student)) System.out.println("the subject already added");
+        else attenders.compute(subject, (key, value) -> value).add(student);
     }
 
     public static void removeStudentFromSubject(Student student, Subject subject) {
-        attenders.compute(subject, (key, value) -> value).remove(student);
+        attenders.computeIfPresent(subject, (key, value) -> value).remove(student);
         if (attenders.get(subject).isEmpty()) {
             attenders.remove(subject);
         }
@@ -64,23 +68,27 @@ public class Main {
     public static void main(String[] args) {
 
         Student ivan = new Student(1, "Ivan");
-        Map<Subject, Integer> ivanMarks = new HashMap<>();
-        Subject math = new Subject(1, "Math");
-        ivanMarks.put(math, 5);
         Student alex = new Student(2, "Alex");
-        Map<Subject, Integer> alexMarks = new HashMap<>();
+        Student john = new Student(3, "John");
+        Student mike = new Student(4, "Mike");
+
+        Subject math = new Subject(1, "Math");
         Subject phys = new Subject(2, "Physics");
+        Subject chemistry = new Subject(3, "Chemistry");
+
+
+        Map<Subject, Integer> alexMarks = new HashMap<>();
+        Map<Subject, Integer> ivanMarks = new HashMap<>();
+        Map<Subject, Integer> johnMarks = new HashMap<>();
+        ivanMarks.put(math, 5);
         alexMarks.put(phys, 4);
+        johnMarks.put(chemistry, 5);
+        johnMarks.put(math, 4);
 
         marks.put(ivan, ivanMarks);
         marks.put(alex, alexMarks);
         System.out.println(marks);
 
-        Student john = new Student(3, "John");
-        Subject chemistry = new Subject(3, "Chemistry");
-        Map<Subject, Integer> johnMarks = new HashMap<>();
-        johnMarks.put(chemistry, 5);
-        johnMarks.put(math, 4);
         addStudentAndMarks(john, johnMarks);
         System.out.println(marks);
         addSubjectForStudent(chemistry, 4, ivan);
@@ -91,20 +99,23 @@ public class Main {
         System.out.println();
 
         List<Student> mathLovers = new ArrayList<>();
+        List<Student> physicsEnjoyers = new ArrayList<>();
         mathLovers.add(ivan);
         mathLovers.add(john);
+        physicsEnjoyers.add(ivan);
+
         attenders.put(math, mathLovers);
         System.out.println(attenders);
-        List<Student> physicsEnjoyers = new ArrayList<>();
+
         physicsEnjoyers.add(alex);
         addSubjectAndStudents(phys, physicsEnjoyers);
         System.out.println(attenders);
-        Student mike = new Student(4, "Mike");
+
         addStudentToSubject(mike, math);
         System.out.println(attenders);
         removeStudentFromSubject(alex, phys);
         System.out.println(attenders);
-        physicsEnjoyers.add(ivan);
+
         addSubjectAndStudents(phys, physicsEnjoyers);
         showSubjectsAndStudents(attenders);
 
--- src/main/java/faang/school/godbless/Task11_Double_Cache/Subject.java
+++ src/main/java/faang/school/godbless/Task11_Double_Cache/Subject.java
@@ -1,14 +1,21 @@
 package faang.school.godbless.Task11_Double_Cache;
-
-import lombok.AllArgsConstructor;
 import lombok.Getter;
-import lombok.ToString;
 
-@AllArgsConstructor
 @Getter
-@ToString
-
 public class Subject {
     private int id;
     private String name;
+
+    public Subject(int id, String name) {
+        if (id<1 || name.isBlank()) throw new IllegalArgumentException("mistake!");
+        else {
+            this.id = id;
+            this.name = name;
+        }
+    }
+
+    @Override
+    public String toString() {
+        return name;
+    }
 }
