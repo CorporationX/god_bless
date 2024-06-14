46071597a68da50f64c0a826141f07ad011b2750
DemureZ <lopatin.g.n@yandex.ru>
2024-06-14 18:55:52 +0300

    создание обработчиков

---
+++ src/main/java/faang/school/godbless/BJS2_9881/Image.java
@@ -0,0 +1,11 @@
+package faang.school.godbless.BJS2_9881;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class Image {
+    private String name;
+    private String description;
+}
