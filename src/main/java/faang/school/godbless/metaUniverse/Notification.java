92e0a6f4a2e5a539459f47d8881f879892ce2d6c
Nikita Volzhin <vlzh.nik@mail.ru>
2024-06-15 00:40:26 +0500

    BJS2- 10173

---
+++ src/main/java/faang/school/godbless/metaUniverse/Notification.java
@@ -0,0 +1,24 @@
+package faang.school.godbless.metaUniverse;
+
+import lombok.Getter;
+import lombok.ToString;
+
+@Getter
+@ToString
+public class Notification {
+    private static final NotificationManager manager = new NotificationManager();
+
+    private String type;
+    private String message;
+    private boolean isSpam;
+
+    public Notification(String type, String message) {
+        this.type = type;
+        this.message = message;
+        manager.checkNotificationOnSpam(this);
+    }
+
+    public void changeIsSpam(boolean isSpam) {
+        this.isSpam = isSpam;
+    }
+}
---
+++ src/main/java/faang/school/godbless/metaUniverse/NotificationManager.java
@@ -0,0 +1,36 @@
+package faang.school.godbless.metaUniverse;
+
+import lombok.NonNull;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.function.Consumer;
+
+public class NotificationManager {
+    private static final Map<String, Consumer<Notification>> notifications = new HashMap<>();
+    private static final List<String> spamWords = List.of("spam1", "spam2", "spam3",
+            "spam4", "spam5", "spam6");
+
+    public void addSpamWord(@NonNull String word) {
+        spamWords.add(word);
+    }
+
+    @NonNull
+    public void registerHandler(
+            String identifierNotification, Consumer<Notification> notificationConsumer) {
+        notifications.put(identifierNotification, notificationConsumer);
+    }
+
+    public void sendNotification(@NonNull Notification notification) {
+        notifications.get(notification.getType()).accept(notification);
+    }
+
+    public void checkNotificationOnSpam(Notification notification) {
+        spamWords.forEach(word -> {
+            if (notification.getMessage().toLowerCase().contains(word)) {
+                notification.changeIsSpam(true);
+            }
+        });
+    }
+}
