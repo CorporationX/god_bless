0494af89d210e172530b13981b8eb61b77cbdaec
kpzxpf <35826257+kpzxpf@users.noreply.github.com>
2024-06-14 22:48:54 +0500

    Update Application.java

--- src/main/java/faang/school/godbless/Application.java
+++ src/main/java/faang/school/godbless/Application.java
@@ -1,21 +1,7 @@
 package faang.school.godbless;
 
-import java.util.HashMap;
-import java.util.Map;
-import java.util.function.BiConsumer;
-
 public class Application {
     public static void main(String[] args) {
-        DictionaryProcessor dictionaryProcessor = new DictionaryProcessor();
-
-        Map<String, String> dictionary = new HashMap<>();
-
-        BiConsumer<String, String> addWordToDictionary = dictionary::put;
-
-        dictionaryProcessor.processWord("Мяч", "Ball", addWordToDictionary);
-        dictionaryProcessor.processWord("Вода", "Water", addWordToDictionary);
-        dictionaryProcessor.processWord("Огонь", "Fire", addWordToDictionary);
-
-        System.out.println("Словарь: " + dictionary);
+        System.out.println("Hello world!");
     }
 }
