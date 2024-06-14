c5310c59db97d50e73d9434a2f3a679174f97369
ArzamaS Ilya <coldoff@inbox.ru>
2024-06-14 13:45:28 +0300

    Созданы объекты домов

---
+++ src/main/java/faang/school/godbless/hashmap/got/Main.java
@@ -0,0 +1,14 @@
+package faang.school.godbless.hashmap.got;
+
+public class Main {
+    private static final Map<String, Hose> houseMap = HashMap<>();
+
+    public static void main(String[] args) {
+
+        House stark = new House("Stark", "wolf");
+        House lannister = new House("Lannister", "lion");
+        House targarien = new House("Targarien", "dragon");
+        House barateon = new House("Barateon", "deer");
+        House tirell = new House("Tirell", "flower");
+    }
+}
