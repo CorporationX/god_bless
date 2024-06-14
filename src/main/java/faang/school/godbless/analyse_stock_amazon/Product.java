6eb691abab7453a7fb4a9e483ace5d5ef7462193
OlgaK888 <olgakurilovi4@gmail.com>
2024-06-14 19:59:47 +0300

    Worked with HashSet and HashMap

---
+++ src/main/java/faang/school/godbless/analyse_stock_amazon/Product.java
@@ -0,0 +1,12 @@
+package faang.school.godbless.analyse_stock_amazon;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class Product {
+    private int id;
+    private String name;
+    private String category;
+}
