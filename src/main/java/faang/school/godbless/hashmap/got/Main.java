887effdbd4de562bce897cd90fc44e4c654a1073
ArzamaS Ilya <coldoff@inbox.ru>
2024-06-15 00:05:57 +0300

    Созданы все методы

--- src/main/java/faang/school/godbless/hashmap/got/Main.java
+++ src/main/java/faang/school/godbless/hashmap/got/Main.java
@@ -1,7 +1,10 @@
 package faang.school.godbless.hashmap.got;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class Main {
-    private static final Map<String, Hose> houseMap = HashMap<>();
+    private static final Map<String, House> GOT_HOUSES = new HashMap<>();
 
     public static void main(String[] args) {
 
@@ -10,5 +13,29 @@ public class Main {
         House targarien = new House("Targarien", "dragon");
         House barateon = new House("Barateon", "deer");
         House tirell = new House("Tirell", "flower");
+
+        GOT_HOUSES.put(stark.getName(), stark);
+        GOT_HOUSES.put(lannister.getName(), lannister);
+        GOT_HOUSES.put(targarien.getName(), targarien);
+        GOT_HOUSES.put(barateon.getName(), barateon);
+        GOT_HOUSES.put(tirell.getName(), tirell);
+    }
+
+    private static void addNewHouse(String name, House house) {
+        GOT_HOUSES.put(name, house);
+    }
+
+    private static void deleteHouse(String name) {
+        GOT_HOUSES.remove(name);
+    }
+
+    private static void findHouse(String name) {
+        GOT_HOUSES.get(name).getSigil();
+    }
+
+    private static void printAllHouse() {
+        for (Map.Entry<String, House> pair : GOT_HOUSES.entrySet()) {
+            System.out.println("House: " + pair.getKey() + ", sigil:" + pair.getValue());
+        }
     }
 }
