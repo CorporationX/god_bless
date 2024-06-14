bd6a801eb04fd90035809d2332f683ffa12c1672
Meerim <meerim15179@gmail.com>
2024-06-14 13:28:33 +0200

    Created User class with findHobbyLovers method

---
+++ src/main/java/faang/school/godbless/collectUsers/User.java
@@ -0,0 +1,35 @@
+package faang.school.godbless.collectUsers;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
+public class User {
+    private int id;
+    private String name;
+    private int age;
+    private Set<String> activities;
+
+    public User(int id, String name, int age, Set<String> activities) {
+        this.id = id;
+        this.name = name;
+        this.age = age;
+        this.activities = activities;
+    }
+
+    public static Map<User, String> findHobbyLovers(List<User> users, Set<String> hobbies) {
+        Map<User, String> result = new HashMap<>();
+
+        for (User user : users) {
+            for (String activity : user.activities) {
+                if (hobbies.contains(activity)) {
+                    result.put(user, activity);
+                    break;
+                }
+            }
+        }
+
+        return result;
+    }
+}
