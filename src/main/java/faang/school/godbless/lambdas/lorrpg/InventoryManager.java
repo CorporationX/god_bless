b79ac83a4bc7d659ed415cd8a3c4970dc0ce7eea
Kirill Maltsev <opp.2211@gmail.com>
2024-06-15 01:31:26 +0500

    refactor: логика обновления предмета(ов)

--- src/main/java/faang/school/godbless/lambdas/lorrpg/InventoryManager.java
+++ src/main/java/faang/school/godbless/lambdas/lorrpg/InventoryManager.java
@@ -1,6 +1,5 @@
 package faang.school.godbless.lambdas.lorrpg;
 
-import java.util.List;
 import java.util.function.Consumer;
 import java.util.function.Function;
 import java.util.function.Predicate;
@@ -30,22 +29,7 @@ public class InventoryManager {
             throw new IllegalArgumentException();
         }
 
-        // Не нашел лучше решения для списка...
-        // Получается либо проход всего списка, либо отсутствие индекса которое нужно для замены итема
-        // Либо отходить от задания и менять сигнатуры
-        List<Item> inventory = character.getInventory();
-        Item item = null;
-        int itemIndex = -1;
-        for (int i = 0; i < inventory.size(); i++) {
-            if (predicate.test(inventory.get(i))) {
-                item = inventory.get(i);
-                itemIndex = i;
-                break;
-            }
-        }
-        if (itemIndex == -1) {
-            throw new IllegalStateException();
-        }
-        inventory.set(itemIndex, function.apply(item));
+        character.getInventory()
+                .replaceAll(item -> predicate.test(item) ? function.apply(item) : item);
     }
 }
