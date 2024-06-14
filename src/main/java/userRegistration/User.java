3fb809c95ad8da3f0dce8b85c8d978abda0fc41b
Bookinisto <bookinisto@mail.ru>
2024-06-15 01:33:51 +0700

    BJS2-7863 регистрация пользователей

--- src/main/java/userRegistration/User.java
+++ src/main/java/userRegistration/User.java
@@ -1,32 +1,46 @@
 package userRegistration;
+
 import java.util.HashSet;
+
 public class User {
 
-    String name, work, home;
+    String name;
+    String work;
+    String home;
     int age;
 
     public static HashSet<String> VALID_JOBS = new HashSet<>();
     public static HashSet<String> VALID_ADDRESSES = new HashSet<>();
-    User(String name, String home, String work, int age) {
-        try {
-            if (age < 18 || name.isEmpty() || VALID_JOBS.contains(work) == false ||
-                    VALID_ADDRESSES.contains(home) == false) {
-                throw new IllegalArgumentException();
-            } else {              //создал условие, с перечислением присваеваемых значний для User
-                this.name = name; // если хоть одно из уловий = true
-                this.age = age;   // то выбрасывается исключение , а если false
-                this.work = work; // то введеные значения присваиваются User-у
-                this.home = home;
-            }
+
+    public User(String name, String home, String work, int age) {
+
+        int minimumAge = 18;
+        if (age < minimumAge) {
+            throw new IllegalArgumentException("Возраст должен быть не менее 18 лет");
+        } else {
+            this.age = age;
         }
-        catch(IllegalArgumentException e){
-            e.printStackTrace();
-            System.out.println("Некорректно введена информация о пользователе");
+        if (name.isEmpty()) {
+            throw new IllegalArgumentException("Неуказанно имя, укажите его");
+        } else {
+            this.name = name;
         }
+        if (VALID_JOBS.contains(work) == false) {
+            throw new IllegalArgumentException("Укажите другое место работы : Google, Uber, Amazon ");
+        } else {
+            this.work = work;
+        }
+        if (VALID_ADDRESSES.contains(home) == false) {
+            throw new IllegalArgumentException("Укажите другой город : London, New York, Amsterdam ");
+        } else {
+            this.home = home;
+        }
+
     }
+
     public static void main(String[] args) {
 
-        VALID_JOBS.add("Google");           // доваляю значения в Сеты
+        VALID_JOBS.add("Google");
         VALID_JOBS.add("Uber");
         VALID_JOBS.add("Amazon");
 
@@ -34,13 +48,13 @@ public class User {
         VALID_ADDRESSES.add("New York");
         VALID_ADDRESSES.add("Amsterdam");
 
-        User User1 = new User("Bob","London","Uber",5);
-        System.out.println(User1.name);    //проверяю присвоенные значения для User
+        User User1 = new User("Bob", "London", "Uber", 19);
+        System.out.println(User1.name);
         System.out.println(User1.work);
         System.out.println(User1.home);
         System.out.println(User1.age);
 
-        User User2 = new User("Jack","London","Uber",45); // второй User для проверки
+        User User2 = new User("Jack", "London", "Uber", 18);
         System.out.println(User2.name);
     }
 }
