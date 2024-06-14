9458a6f63f703af161ca36481214d15362b83351
V1nGeri|Artem Radkevich <artemka20040613@gmail.com>
2024-06-14 13:17:43 +0300

    BJS2-9877 Фильтры Instagram

---
+++ src/main/java/faang/school/godbless/BJS2_9877/Image.java
@@ -0,0 +1,13 @@
+package faang.school.godbless.BJS2_9877;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+public class Image {
+    private String name;
+    private String description;
+}
