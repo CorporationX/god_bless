c888b9646fc0644d0a046e9ff22023befccc27dd
Alen <Alendos87@gmail.com>
2024-06-14 14:26:18 +0500

    Solution for instagram filtering

---
+++ src/main/java/faang/school/godbless/Notification/Application.java
@@ -0,0 +1,24 @@
+package faang.school.godbless.Notification;
+import faang.school.godbless.Notification.Notification;
+import faang.school.godbless.Notification.NotificationManager;
+
+
+public class Application {
+    public static void main(String[] args) {
+        NotificationManager manager = new NotificationManager();
+
+        // Регистрация обработчиков
+        manager.registerHandler("email", notification1 -> System.out.println("Email notification: " + notification1.getMessage()));
+        manager.registerHandler("sms", notification1 -> System.out.println("SMS notification: " + notification1.getMessage()));
+        manager.registerHandler("facebook", notification1 -> System.out.println("Facebook notification: " + notification1.getMessage()));
+
+        Notification emailNotification = new Notification("email", "Ваша учетная запись успешна активирована");
+        Notification smsNotification = new Notification("sms", "Вы успешно изменили свой пароль");
+        Notification pushNotification = new Notification("facebook", "Новый пост от пользователя: JohnDoe");
+
+
+        manager.sendNotification(emailNotification);
+        manager.sendNotification(smsNotification);
+        manager.sendNotification(pushNotification);
+    }
+}
