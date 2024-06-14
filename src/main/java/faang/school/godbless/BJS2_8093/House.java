661cc39a2112344e2c124a0baf74d79cb6a4b559
DemureZ <lopatin.g.n@yandex.ru>
2024-06-14 15:30:34 +0300

    вынес методы в отдельный класс Houses и добавил проверку полей на null

--- src/main/java/faang/school/godbless/BJS2_8093/House.java
+++ src/main/java/faang/school/godbless/BJS2_8093/House.java
@@ -3,10 +3,22 @@ package faang.school.godbless.BJS2_8093;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 
-@AllArgsConstructor
+
 @Data
 public class House {
     private String name;
     private String sigil;
 
+    public House(String name, String sigil) {
+        if (name.isBlank()){
+            System.out.println("Некорректное имя дома");
+            return;
+        }
+        if (sigil.isBlank()){
+            System.out.println("Некорректное название герба");
+            return;
+        }
+        this.name = name;
+        this.sigil = sigil;
+    }
 }
--- src/main/java/faang/school/godbless/BJS2_8093/Main.java
+++ src/main/java/faang/school/godbless/BJS2_8093/Main.java
@@ -5,32 +5,23 @@ import java.util.Map;
 
 public class Main {
     public static void main(String[] args) {
-        Map<String, House> houseSigils = new HashMap<>();
-        houseSigils.put("Stark", new House("Stark", "Grey direwolf"));
-        houseSigils.put("Lannister", new House("Lannister", "Golden lion"));
-        houseSigils.put("Baratheon", new House("Baratheon", "Crowned stag"));
-        printAllHouses(houseSigils);
-        removeHouse(houseSigils, "Lannister");
+        Houses houses = new Houses();
+
+        House stark = new House("Stark", "Grey direwolf");
+        House lannister = new House("Lannister", "Golden lion");
+        House baratheon = new House("Baratheon", "Crowned stag");
+
+        houses.add(stark);
+        houses.add(lannister);
+        houses.add(baratheon);
+        houses.printAllHouses();
+        houses.remove("Lannister");
         System.out.println();
-        printAllHouses(houseSigils);
-        addHouse(houseSigils, new House("Lannister", "Golden lion"));
+        houses.printAllHouses();
+        houses.add(new House("Lannister", "Golden lion"));
         System.out.println();
-        printHouseSigil(houseSigils, "Lannister");
+        houses.printHouseSigil("Lannister");
     }
 
-    public static void addHouse(Map<String, House> map, House house) {
-        map.put(house.getName(), house);
-    }
 
-    public static void removeHouse(Map<String, House> map, String houseName) {
-        map.remove(houseName);
-    }
-
-    public static void printHouseSigil(Map<String, House> map, String houseName) {
-        System.out.println(map.get(houseName));
-    }
-
-    public static void printAllHouses(Map<String, House> map) {
-        map.forEach((s, house) -> System.out.println(house));
-    }
 }
