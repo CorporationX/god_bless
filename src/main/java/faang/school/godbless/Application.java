98d3241a4104c909a6f93fdf038bf2a0bca057ff
Kilotas <Alendos87@gmail.com>
2024-06-14 12:53:33 +0500

    Merge remote-tracking branch 'origin/Omarov-Alen-BJS2-8651' into Omarov-Alen-BJS2-8651

--- src/main/java/faang/school/godbless/Application.java
+++ src/main/java/faang/school/godbless/Application.java
@@ -1,42 +1,12 @@
 package faang.school.godbless;
 
 
+import faang.school.godbless.Weather.WeatherData;
+
 import java.util.HashMap;
 
 public class Application {
     public static void main(String[] args) {
-        HashMap<String, WeatherData> weatherData = new HashMap<>();
-
-        WeatherData data1 = new WeatherData("Almaty", 22, 70);
-        WeatherData data2 = new WeatherData("Moscow", 25, 65);
-
-
-        WeatherData.updateWeatherData(weatherData, data1);
-        WeatherData.updateWeatherData(weatherData, data2);
-
-        System.out.println("Все города мапы");
-        WeatherData.printAllCities(weatherData);
-
-        System.out.println("Получение по getWeatherData");
-        System.out.println(WeatherData.getWeatherData(weatherData, "Berlin"));
-
-        System.out.println("Получение по неизвестному городу");
-        System.out.println(WeatherData.getWeatherData(weatherData, "Chicago"));
-
-
-        WeatherData newData = new WeatherData("New York", 20, 75);
-        WeatherData.updateWeatherData(weatherData, newData);
-
-        WeatherData.removeWeatherData(weatherData, "Los Angeles");
-
-        System.out.println("Вывод списка всех городов");
-        WeatherData.printAllCities(weatherData);
-
-        System.out.println("Информация о Берлине");
-        System.out.println(WeatherData.getWeatherData(weatherData, "Berlin"));
-
-        System.out.println("Информация Los Angeles:");
-        System.out.println(WeatherData.getWeatherData(weatherData, "Los Angeles"));
-
+        System.out.println("hello world");
   }
 }
--- src/main/java/faang/school/godbless/Weather/WeatherData.java
+++ src/main/java/faang/school/godbless/Weather/WeatherData.java
@@ -1,4 +1,4 @@
-package faang.school.godbless;
+package faang.school.godbless.Weather;
 
 import java.util.Map;
 import java.util.Random;
