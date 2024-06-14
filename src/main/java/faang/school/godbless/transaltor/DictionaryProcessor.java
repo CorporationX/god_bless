6ab5dd24dce9a0a2f4d851269bc618290a4e3bdb
Evgenii Malkov <malk.2011@yandex.ru>
2024-06-14 16:47:58 +0300

    BJS2-9556: rename var

--- src/main/java/faang/school/godbless/transaltor/DictionaryProcessor.java
+++ src/main/java/faang/school/godbless/transaltor/DictionaryProcessor.java
@@ -4,7 +4,7 @@ package faang.school.godbless.transaltor;
  * @author Evgenii Malkov
  */
 public class DictionaryProcessor {
-  public void processWord(String word, String translation, BiConsumer<String, String> biConsumer) {
-    biConsumer.doAction(word, translation);
+  public void processWord(String word, String translation, BiConsumer<String, String> translationHandler) {
+    translationHandler.doAction(word, translation);
   }
 }
