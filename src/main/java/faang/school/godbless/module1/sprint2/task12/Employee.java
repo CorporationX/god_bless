1631a634b98c8e35c4307317c3296cf30509749b
Max Prigonyuk <kael.smr@gmail.com>
2024-06-14 12:54:04 +0400

    Потренируем StreamAPI 2 https://faang-school.atlassian.net/browse/BJS2-10637

---
+++ src/main/java/faang/school/godbless/module1/sprint2/task12/Employee.java
@@ -0,0 +1,16 @@
+package faang.school.godbless.module1.sprint2.task12;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.Getter;
+
+@AllArgsConstructor
+@Data
+@Getter
+public class Employee {
+    private String name;
+    private int salary;
+    private String department;
+
+
+}
