1f85c96607d7744a3e9ee3ce7c7a1c79368aa125
Gevorg <a0964593@gmail.com>
2024-06-14 09:30:55 +0300

    BJS2-9375 - rewrite Email.class as a record

--- src/main/java/faang/school/godbless/GmailRichFilters/EmailProcessor.java
+++ src/main/java/faang/school/godbless/GmailRichFilters/EmailProcessor.java
@@ -1,16 +1,21 @@
 package faang.school.godbless.GmailRichFilters;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.function.Consumer;
 import java.util.function.Function;
 import java.util.function.Predicate;
 
 public class EmailProcessor {
+    private List<Email> allEmailsAfterFilter = new ArrayList<>();
+
     public void processEmails(List<Email> emails, Predicate<Email> isImportant, Consumer<Email> printEmail, Function<Email, String> toUpperCase) {
         for (Email email : emails) {
             if (isImportant.test(email)) {
-                printEmail.accept(email);
-                System.out.println(toUpperCase.apply(email));
+                Email emailAfterFilter = new Email(email.subject(), toUpperCase.apply(email), email.isImportant());
+                allEmailsAfterFilter.add(emailAfterFilter);
+                printEmail.accept(emailAfterFilter);
+                System.out.println(emailAfterFilter.body());
             }
         }
     }
