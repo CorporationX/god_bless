24d6414ef33c255ef63ef06e3cdf2b2e56875baf
Dmitriy Golubtsov <dima63653@gmail.com>
2024-06-15 13:37:40 +0300

    Создал HashMap и реализовал метод

--- src/main/java/faang/school/godbless/House.java
+++ src/main/java/faang/school/godbless/House.java
@@ -2,8 +2,8 @@ package faang.school.godbless;
 
 public class House {
 
-    private String name;
-    private String sigil;
+    public String name;
+    public String sigil;
 
     public House(String name, String sigil) {
         this.name = name;
