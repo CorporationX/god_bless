b17d18050242dc3c563118e18c18508bac3bbe39
Nikita2768 <vechkinnikita22@gmail.com>
2024-06-15 19:52:18 +0500

    stream api 2 5 tasks out of 10 completed

---
+++ src/main/java/faang/school/godbless/streamAPI2/Employee.java
@@ -0,0 +1,10 @@
+package faang.school.godbless.streamAPI2;
+
+import lombok.Getter;
+
+@Getter
+public class Employee {
+    private String name;
+    private int salary;
+    private String department;
+}
