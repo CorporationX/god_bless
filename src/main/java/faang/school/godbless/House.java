3d6ba703c16e430452f6949d1f424e3f13d1680e
Dmitriy Golubtsov <dima63653@gmail.com>
2024-06-15 13:17:50 +0300

    Создал HashMap и реализовал метод

---
+++ src/main/java/faang/school/godbless/House.java
@@ -0,0 +1,25 @@
+package faang.school.godbless;
+
+public class House {
+
+    private String name;
+    private String sigil;
+
+    public House(String name, String sigil) {
+        this.name = name;
+        this.sigil = sigil;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public String getSigil() {
+        return this.sigil;
+    }
+
+    public Object getSigin() {
+
+        return null;
+    }
+}
---
+++ src/main/java/faang/school/godbless/Main.java
@@ -0,0 +1,44 @@
+package faang.school.godbless;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Main {
+
+   private static final Map<String, House> houses = new HashMap<>();
+
+    public static void main(String[] args) {
+
+        House stark = new House("Старк", "sig");
+        House lannister = new House("Ланнистер", "sag");
+        House barateon = new House("Баратеон", "sig");
+
+        add(stark.getName(), stark);
+        add(lannister.getName(), lannister);
+        add(barateon.getName(), barateon);
+
+        remove(stark.getName());
+
+        find(lannister.getName());
+
+        printAll();
+    }
+
+    private static void add(String name, House house) {
+        houses.put(name, house);
+    }
+
+    private static void remove(String name) {
+        houses.remove(name);
+    }
+
+    private static String find(String name) {
+        return houses.get(name).getSigil();
+    }
+
+    private static void printAll() {
+        boolean home = false;
+        houses.forEach((name, house) -> System.out.println(home + ":" + house.getSigin()));
+    }
+
+}
