4fc8f3e715053408d41ce38776b1b89997909fb8
Andrey Kuzmin <Andreytko@mail.ru>
2024-06-15 10:30:55 +0300

    сделал реафкторинг, вынес методы в отделный класс и убрал у них static;

--- src/main/java/faang/school/godbless/Task_Calculator/Main.java
+++ src/main/java/faang/school/godbless/Task_Calculator/Main.java
@@ -4,36 +4,15 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Main {
-
-    public static double calculate(List<Double> numbers, Calculator calculator) {
-        if (numbers.isEmpty()) {
-            throw new IllegalArgumentException("empty list");
-        } else {
-            double res = numbers.get(0);
-           for (int i = 1; i<numbers.size(); i++) {
-               res = calculator.calculate(res, numbers.get(i));
-           }
-            return res;
-        }
-    }
-
-    public static double sum(List<Double> numbers) {
-        return calculate(numbers, (a, b) ->a+b);
-    }
-
-    public static double prod(List<Double> numbers) {
-        return calculate(numbers, (a,b)->a*b);
-    }
-
     public static void main(String[] args) {
-
         List<Double> numbers = new ArrayList<>();
+        Calculate calculate = new Calculate();
         numbers.add(2.);
         numbers.add(1.);
         numbers.add(3.);
         numbers.add(4.);
         numbers.add(5.);
-        System.out.println(sum(numbers));
-        System.out.println(prod(numbers));
+        System.out.println(calculate.sum(numbers));
+        System.out.println(calculate.prod(numbers));
     }
 }
