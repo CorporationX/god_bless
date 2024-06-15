39d41fcac884a8cd3a74e3237d4c47f66c37a0a6
Maksim Tyumentsev <experttm@gmail.com>
2024-06-15 10:11:48 +0500

    #BJS2-8413 Реализовать систему простого поиска информаци на сайтах.

--- src/main/java/faang/school/godbless/bjs2_8413/Main.java
+++ src/main/java/faang/school/godbless/bjs2_8413/Main.java
@@ -1,37 +1,75 @@
 package faang.school.godbless.bjs2_8413;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.regex.Pattern;
+import java.util.Optional;
 
 public class Main {
-
-  private static final Pattern WORDS_SEPARATOR_REGEX = Pattern.compile("[^a-z A-Z а-я]+", Pattern.UNICODE_CHARACTER_CLASS);
+  private static final String WORDS_SEPARATOR_REGEX = "\\W+";
+  private static final String EMPTY = "";
+  private static final String KEYWORD = "Ключевое слово - ";
+  private static final String WEBSITE = " Сайт: ";
 
   private static final Map<String, List<WebPage>> webPages = new HashMap<>();
 
   public static void main(String[] args) {
-    List<String> l = List.of("привет", "Привет");
 
-    l.forEach(s -> System.out.println(s));
-//    String[] sss = {"привет", "Привет"};
-//    System.out.println(sss);
+    WebPage firsPage = new WebPage("https://testOne.com", "testOn", "Java, String, Integer, Long");
+    WebPage secondPage = new WebPage("https://testTwo.com", "testTwo", "Interface, Class, Java, Best");
+    WebPage thirdPage = new WebPage("https://testThree.com", "testThree", "JAVA, VOID, API, Class");
+    WebPage fourthPage = new WebPage("https://testFour.com", "testFour", "Stream, Optional, API, Class");
+    WebPage fifthPage = new WebPage("https://delete.com", "delete", "delete, Stream, Class, Long");
 
+    addWebPage(firsPage);
+    addWebPage(secondPage);
+    addWebPage(thirdPage);
+    addWebPage(fourthPage);
+    addWebPage(fifthPage);
 
+    System.out.println(searchWebPage("JAVA"));
 
+    showAllWebPages();
 
+    deleteWebPage("https://delete.com");
 
-    WebPage ggg = new WebPage("https://testOne.com", "ddddd", "test2 test1 test3 test4");
+    showAllWebPages();
 
-    method(ggg);
+    addWebPage(fifthPage);
+
+    showAllWebPages();
   }
 
-  private static void method(WebPage webPage) {
-    String[] words = webPage.getContent().split(String.valueOf(WORDS_SEPARATOR_REGEX));
-    for (String s : words) {
-      System.out.println(s);
+  private static void addWebPage(WebPage webPage) {
+    var words = Arrays.stream(webPage.content().split(WORDS_SEPARATOR_REGEX))
+        .filter(str -> !str.isBlank())
+        .map(String::toLowerCase)
+        .toList();
+    words.forEach(word -> webPages.computeIfAbsent(word, webPages -> new ArrayList<>())
+          .add(webPage));
+  }
+
+  private static List<WebPage> searchWebPage(String word) {
+    return webPages.get(word.toLowerCase());
+  }
+
+  private static void deleteWebPage(String url) {
+    final String currentUrl = Optional.ofNullable(url)
+        .orElse(EMPTY);
+    final var iterator = webPages.entrySet().iterator();
+    while (iterator.hasNext()) {
+      final var webPages = iterator.next().getValue();
+      webPages.removeIf(page -> currentUrl.equals(page.url()));
+      if (webPages.isEmpty()) {
+        iterator.remove();
+      }
     }
+  }
+
+  private static void showAllWebPages() {
+    webPages.forEach((key, value) -> System.out.println(KEYWORD + key + WEBSITE + value));
   }
 
 }
--- src/main/java/faang/school/godbless/bjs2_8413/WebPage.java
+++ src/main/java/faang/school/godbless/bjs2_8413/WebPage.java
@@ -1,15 +1,5 @@
 package faang.school.godbless.bjs2_8413;
 
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.ToString;
-
-@Data
-@AllArgsConstructor
-@ToString
-public class WebPage {
-  private String url;
-  private String title;
-  private String content;
+public record WebPage(String url, String title,String content) {
 
 }
