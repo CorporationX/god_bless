cc043c1ca0b5c714c827df00cf039739bc10ddb9
Alen <Alendos87@gmail.com>
2024-06-15 20:11:08 +0500

    Убрал конструктор без аргументов

--- src/main/java/faang/school/godbless/Notification/Notification.java
+++ src/main/java/faang/school/godbless/Notification/Notification.java
@@ -3,11 +3,10 @@ package faang.school.godbless.Notification;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
-import lombok.NoArgsConstructor;
+
 
 @Data
 @AllArgsConstructor
-@NoArgsConstructor
 public class Notification {
     private String type;
     private String message;
