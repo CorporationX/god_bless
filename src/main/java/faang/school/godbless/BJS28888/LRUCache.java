05e29eb2102465cfee8a9bc0ae46b1aaa5c8bc85
DonMaxim <donmaxim1990@gmail.com>
2024-06-16 14:47:01 -0300

    Update after review Implement LinkedHashMap for cache

--- src/main/java/faang/school/godbless/BJS28888/LRUCache.java
+++ src/main/java/faang/school/godbless/BJS28888/LRUCache.java
@@ -4,19 +4,25 @@ import lombok.Getter;
 
 import java.time.Instant;
 import java.util.Comparator;
-import java.util.HashMap;
+import java.util.LinkedHashMap;
 import java.util.Map;
 
 public class LRUCache {
     private int cacheSize;
     @Getter
-    private Map<Integer, Data> cache = new HashMap<>();
+    private Map<Integer, Data> cache;
 
     public LRUCache(int cacheSize) {
         if (cacheSize <= 0) {
             throw new IllegalArgumentException("CacheSize cannot be less then 1 element!");
         } else {
             this.cacheSize = cacheSize;
+            this.cache = new LinkedHashMap<Integer, Data>(cacheSize, 0.75f, true) {
+                @Override
+                protected boolean removeEldestEntry(Map.Entry<Integer, Data> eldest) {
+                    return size() > cacheSize;
+                }
+            };
         }
     }
 
@@ -27,18 +33,8 @@ public class LRUCache {
 
     public Data addElement(Data data) {
         data.setTimeStamp(Instant.now());
-        updateCache();
         cache.put(data.getId(), data);
         return data;
-    }
-
-    private void updateCache() {
-        if (cache.size() == cacheSize) {
-            cache.remove(cache.values().stream().
-                    min(Comparator.comparing(Data::getTimeStamp)).
-                    orElse(null).
-                    getId());
-        }
     }
 
     public void printCache() {
