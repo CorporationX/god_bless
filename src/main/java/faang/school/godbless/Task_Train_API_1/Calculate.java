8f29481d973b993a39409c1c457109af2ee87cc3
Andrey Kuzmin <Andreytko@mail.ru>
2024-06-16 13:09:36 +0300

    переделал 3 задания (

--- src/main/java/faang/school/godbless/Task_Train_API_1/Calculate.java
+++ src/main/java/faang/school/godbless/Task_Train_API_1/Calculate.java
@@ -1,33 +1,33 @@
 package faang.school.godbless.Task_Train_API_1;
+
 import lombok.NoArgsConstructor;
 
 import java.util.Comparator;
 import java.util.List;
-import java.util.NoSuchElementException;
 import java.util.Optional;
 
 @NoArgsConstructor
 public class Calculate {
 
     public static int findSum(List<Integer> numbers) {
-        int res = numbers.stream()
-                .reduce(0,(sum, number)->sum+number);
-        return res;
-    }
-
-    public static int findMax(List<Integer> numbers) {
         return numbers.stream()
-                .max(Comparator.comparingInt(x -> x)).orElseThrow(NoSuchElementException::new);
+                .reduce(0, (sum, number) -> sum + number);
     }
 
-    public static int findAverage(List<Integer> numbers) {
-        if (numbers.isEmpty()) throw new IllegalArgumentException("Empty List!");
+    public static void findMax(List<Integer> numbers) {
+        Optional<Integer> result = numbers.stream()
+                .max(Comparator.comparingInt(x -> x));
+        if (result.isPresent()) System.out.println(result.get());
+        else System.out.println("something went wrong");
+    }
+
+    public static void findAvg(List<Integer> numbers) {
+        if (numbers.isEmpty()) System.out.println("the list is empty");
         else {
             int res = numbers.stream()
-                    .reduce(0, (sum, number) -> sum + number);
-                    return res/numbers.size();
+                    .reduce(0,(sum, number)->number+sum)/numbers.size();
+            System.out.println(res);
         }
     }
-
 
 }
--- src/main/java/faang/school/godbless/Task_Train_API_1/Main.java
+++ src/main/java/faang/school/godbless/Task_Train_API_1/Main.java
@@ -5,10 +5,10 @@ import java.util.List;
 public class Main {
     public static void main(String[] args) {
 
-        List<Integer> numbers = List.of(1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
+        List<Integer> numbers = List.of(1,2,3,4,5);
         System.out.println(Calculate.findSum(numbers));
-        System.out.println(Calculate.findMax(numbers));
-        System.out.println(Calculate.findAverage(numbers));
+        Calculate.findMax(numbers);
+        Calculate.findAvg(numbers);
 
     }
 }
