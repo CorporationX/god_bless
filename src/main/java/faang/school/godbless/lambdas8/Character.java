469ef1a20ef5974b5037c1b14cd00e7bf8c14d3c
Danill Klinkov <klinkov.danya@bk.ru>
2024-06-16 10:04:02 +0300

    lambdas8: Lord of the Rings (class on record and camelCase)

--- src/main/java/faang/school/godbless/lambdas8/Character.java
+++ src/main/java/faang/school/godbless/lambdas8/Character.java
@@ -1,17 +1,10 @@
 package faang.school.godbless.lambdas8;
 
-import lombok.Getter;
-
 import java.util.ArrayList;
 import java.util.List;
 
-public class Character {
-    private final String NAME;
-    @Getter
-    private final List<Item> inventory;
-
+public record Character(String name, List<Item> inventory) {
     public Character(String name) {
-        this.NAME = name;
-        this.inventory = new ArrayList<>();
+        this(name, new ArrayList<>());
     }
 }
--- src/main/java/faang/school/godbless/lambdas8/InventoryManager.java
+++ src/main/java/faang/school/godbless/lambdas8/InventoryManager.java
@@ -6,25 +6,25 @@ import java.util.function.Predicate;
 
 public class InventoryManager {
     void addItem(Character character, Item item, Consumer<Item> inventoryItem) {
-        character.getInventory().add(item);
+        character.inventory().add(item);
         inventoryItem.accept(item);
     }
 
     void removeItem(Character character, Predicate<Item> condition) {
         Item deleteItem = null;
-        for (Item item : character.getInventory()) {
+        for (Item item : character.inventory()) {
             if (condition.test(item)) {
                 deleteItem = item;
             }
         }
-        character.getInventory().remove(deleteItem);
+        character.inventory().remove(deleteItem);
     }
 
     void updateItem(Character character, Predicate<Item> condition, Function<Item, Item> renovation) {
-        for (Item item : character.getInventory()) {
+        for (Item item : character.inventory()) {
             if (condition.test(item)) {
-                character.getInventory().remove(item);
-                character.getInventory().add(renovation.apply(item));
+                character.inventory().remove(item);
+                character.inventory().add(renovation.apply(item));
             }
         }
     }
