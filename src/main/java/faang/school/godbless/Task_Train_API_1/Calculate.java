fddd89f58a68426654e8a39372f53e9006c43bcc
Andrey Kuzmin <Andreytko@mail.ru>
2024-06-16 11:27:22 +0300

    сделал 2 задание

--- src/main/java/faang/school/godbless/Task_Train_API_1/Calculate.java
+++ src/main/java/faang/school/godbless/Task_Train_API_1/Calculate.java
@@ -1,6 +1,7 @@
 package faang.school.godbless.Task_Train_API_1;
 import lombok.NoArgsConstructor;
 import java.util.List;
+import java.util.NoSuchElementException;
 
 @NoArgsConstructor
 public class Calculate {
@@ -10,6 +11,13 @@ public class Calculate {
     public static int findSum(List<Integer> numbers) {
         int res = numbers.stream()
                 .reduce(0,(sum, number)->sum+number);
+        return res;
+    }
+
+    public static int findMax(List<Integer> numbers) {
+        int res = numbers.stream()
+                .mapToInt(x->x)
+                .max().orElseThrow(NoSuchElementException::new);
         return res;
     }
 
--- src/main/java/faang/school/godbless/Task_Train_API_1/Main.java
+++ src/main/java/faang/school/godbless/Task_Train_API_1/Main.java
@@ -6,8 +6,8 @@ public class Main {
     public static void main(String[] args) {
 
         List<Integer> numbers = List.of(1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
-        Calculate calculateSum = new Calculate();
         System.out.println(Calculate.findSum(numbers));
+        System.out.println(Calculate.findMax(numbers));
 
     }
 }
