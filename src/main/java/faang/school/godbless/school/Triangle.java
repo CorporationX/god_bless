5d7bccca315c99b1235fd9caa9335bfba957ca53
Dima Dobrev <dobrev2212@gmail.com>
2024-06-16 11:13:12 +0200

    BJS2-9151 validation is added

--- src/main/java/faang/school/godbless/school/Triangle.java
+++ src/main/java/faang/school/godbless/school/Triangle.java
@@ -4,6 +4,10 @@ import java.util.function.Function;
 
 public class Triangle {
     public static Double triangleArea(double a, double b, double c) throws IllegalArgumentException {
+        if (a <= 0 || b <= 0 || c <= 0 || a + b <= c || a + c <= b || b + c <= a){
+            throw new IllegalArgumentException("Invalid arguments.");
+        }
+
         Function<Double, Function<Double, Double>> sum = (x) -> y -> x + y;
         Function<Double, Function<Double, Double>> mul = (x) -> y -> x * y;
         Function<Double, Function<Double, Double>> sub = (x) -> y -> x - y;
