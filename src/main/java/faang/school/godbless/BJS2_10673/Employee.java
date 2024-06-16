6a090fab329f9ccb914af55efc706483cb357d81
Mikhail Fokin <fokinms@gmail.com>
2024-06-16 13:55:05 +0400

    BJS2-10673 Потренируем StreamAPI 2

---
+++ src/main/java/faang/school/godbless/BJS2_10673/Employee.java
@@ -0,0 +1,16 @@
+package faang.school.godbless.BJS2_10673;
+
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.ToString;
+
+@AllArgsConstructor
+@Getter
+@EqualsAndHashCode
+@ToString
+public class Employee {
+    String name;
+    int salary;
+    String department;
+}
