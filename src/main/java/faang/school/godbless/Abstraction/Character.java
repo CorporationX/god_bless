d72d536f32ef071888622e833fe8300f4c56434c
Bookinisto <bookinisto@mail.ru>
2024-06-16 22:05:18 +0700

    BJS2-7763 Абстракция

--- src/main/java/faang/school/godbless/Abstraction/Character.java
+++ src/main/java/faang/school/godbless/Abstraction/Character.java
@@ -47,7 +47,6 @@ class Test {
         Warrior war1 = new Warrior("Knight");
         Archer archer1 = new Archer("Bowman");
 
-
         war1.attack(archer1);
         war1.attack(archer1);
         archer1.attack(war1);
