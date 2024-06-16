18157346165a5081c5176be269169b2843885a2a
Alexandra <s7125@mail.ru>
2024-06-16 20:23:42 +0500

    BJS2-9435 add class

--- src/main/java/faang/school/godbless/googlTranslatorUnderOurControl/DictionaryProcessor.java
+++
@@ -1,9 +0,0 @@
-package faang.school.godbless.googlTranslatorUnderOurControl;
-
-import java.util.function.BiConsumer;
-
-public class DictionaryProcessor {
-    void processWord(String name, String translate, BiConsumer<String, String> addWordToDictionary) {
-        addWordToDictionary.accept(name, translate);
-    }
-}
