6b874bffb6799e264d802b1bc01b26771cf773aa
lu_av <loveavduhova@gmail.com>
2024-06-16 20:05:07 +0700

    BJS2-10274 - поменяла логику

--- src/main/java/faang/school/godbless/BJS2_10274/MessageProcessor.java
+++ src/main/java/faang/school/godbless/BJS2_10274/MessageProcessor.java
@@ -6,12 +6,11 @@ import java.util.List;
 public class MessageProcessor {
     public boolean processMessage(String message, List<MessageFilter> messageFilters) {
         if (message == null || message.trim().isEmpty() || messageFilters == null) throw new IllegalArgumentException();
-        boolean result = false;
-        for (MessageFilter messageFilter : messageFilters) {
-            if (!messageFilter.filter(message)) {
-                break;
-            } else result = true;
-        }
-        return result;
+
+       return messageFilters.stream().map(p -> p.filter(message))
+               .filter(Boolean.FALSE::equals)
+               .findFirst()
+               .orElse(Boolean.TRUE);
+
     }
 }
