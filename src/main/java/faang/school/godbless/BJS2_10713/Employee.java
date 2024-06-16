9806d21d4001dc7ac2fd08293e31c9f22076b42e
Max Shundrick <mas9l175@gmail.com>
2024-06-16 21:12:28 +0300

    Done with BJS2-10713

---
+++ src/main/java/faang/school/godbless/BJS2_10713/Employee.java
@@ -0,0 +1,14 @@
+package faang.school.godbless.BJS2_10713;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.ToString;
+
+@Getter
+@AllArgsConstructor
+@ToString
+public class Employee {
+    private String name;
+    private int salary;
+    private String department;
+}
