16f6c0a5c4b4080dcbf80be50c52e66a133758ad
cirill98 <100434317+cirill98@users.noreply.github.com>
2024-06-16 23:43:21 +0500

    Поменял цикл

--- src/main/java/faang/school/godbless/WorkMicrosoft/MailSender.java
+++ src/main/java/faang/school/godbless/WorkMicrosoft/MailSender.java
@@ -3,13 +3,14 @@ package faang.school.godbless.WorkMicrosoft;
 import lombok.AllArgsConstructor;
 
 @AllArgsConstructor
-public class MailSender implements Runnable{
+public class MailSender implements Runnable {
     private int startIndex;
     private int endIndex;
+
     @Override
     public void run() {
         for (int i = startIndex; i <= endIndex; i++) {
-            System.out.println("Письмо " + i + " отправлено");
         }
+        System.out.println(Thread.currentThread().getName() + " завершил отправку писем");
     }
 }
