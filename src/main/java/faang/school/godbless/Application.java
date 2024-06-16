62527f7ed0a9a71c4f47e4568467687b3a86a197
Alen <Alendos87@gmail.com>
2024-06-16 22:29:41 +0500

    solved problem with git

--- src/main/java/faang/school/godbless/Application.java
+++ src/main/java/faang/school/godbless/Application.java
@@ -1,6 +1,4 @@
 package faang.school.godbless;
-import faang.school.godbless.Notification.Notification;
-import faang.school.godbless.Notification.NotificationManager;
 
 public class Application {
     public static void main(String[] args) {
