95152a66efb75a79274712295d5de852f31fc2a8
Bookinisto <bookinisto@mail.ru>
2024-06-17 02:36:58 +0700

    BJS2-7579 grouping users

--- src/main/java/GroupingUsersByAge/User.java
+++ src/main/java/GroupingUsersByAge/User.java
@@ -1,15 +1,10 @@
 package GroupingUsersByAge;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-
 public class User {
     protected String name;
     protected String work;
     protected String home;
-    protected int age;
+    public int age;
 
     public String toString() {
         String text = "";
@@ -27,42 +22,6 @@ public class User {
         this.age = age;
     }
 
-    // Map<Integer, List> usersMap = new HashMap<>();
-    public static Map<Integer, List<User>> grouping(List<User> users) {
-        Map<Integer, List<User>> usersMap = new HashMap<>();
-        for (User user : users) {
-            int userAge = user.age;
-            if (usersMap.containsKey(userAge)) {
-                List<User> spisok = usersMap.get(userAge);
-                spisok.add(user);
-                usersMap.put(userAge, spisok);
-            } else {
-                List<User> newUser = new ArrayList<>();
-                newUser.add(user);
-                usersMap.put(userAge, newUser);
-            }
-        }
-        return usersMap;
-    }
-
-    public static void main(String[] args) {
-
-        User user1 = new User("Bob", "police", " Lenina", 36);
-        User user2 = new User("Pit", "bilder", " Kirova", 36);
-        User user3 = new User("Met", "trener", " Sverdlova", 23);
-        User user4 = new User("Kail", "student", "Kurchatova", 23);
-        User user5 = new User("Nail", "buhgalter", "Lenina", 56);
-
-        List<User> users = new ArrayList<>();
-        users.add(user1);
-        users.add(user2);
-        users.add(user3);
-        users.add(user4);
-        users.add(user5);
-
-        Map<Integer, List<User>> groupedUsers = grouping(users);
-        System.out.println(groupedUsers);
-
-    }
 }
+
 
