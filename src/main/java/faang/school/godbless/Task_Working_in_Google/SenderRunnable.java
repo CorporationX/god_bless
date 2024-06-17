e03a5817e0e5720c61df9fcd5d82986f377a66ac
Andrei Kuzmin <Andreytko@mail.ru>
2024-06-18 01:03:09 +0300

    small fix

--- src/main/java/faang/school/godbless/Task_Working_in_Google/SenderRunnable.java
+++ src/main/java/faang/school/godbless/Task_Working_in_Google/SenderRunnable.java
@@ -16,7 +16,7 @@ public class SenderRunnable implements Runnable {
 
     @Override
     public void run() {
-        for (int i = startIndex; i < endIndex; i++) {
+        for (int i = startIndex; i <= endIndex; i++) {
             System.out.println("the message " + i + " has been sent");
         }
     }
