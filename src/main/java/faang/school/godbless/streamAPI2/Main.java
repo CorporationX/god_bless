ffb9f7865e7868ea2fba04b769f77d5ee36dcd13
Alexandra <s7125@mail.ru>
2024-06-17 10:11:06 +0500

    BJS2-10743 add class

--- src/main/java/faang/school/godbless/streamAPI2/Main.java
+++ src/main/java/faang/school/godbless/streamAPI2/Main.java
@@ -1,5 +1,6 @@
 package faang.school.godbless.streamAPI2;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.function.Predicate;
@@ -8,24 +9,22 @@ public class Main {
     public static void main(String[] args) {
         List<Integer> nums = List.of(1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
         List<String> str = List.of("Baku", "Berlin", "Valencia", "Varna", "Madrid", "Mexico");
-        HashMap<String, String> cityCapital = new HashMap<String,String>();
+        List<Character> characters = List.of('a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z');
+        HashMap<String, String> cityCapital = new HashMap<>();
         cityCapital.put("Armenia", "Yerevan");
         cityCapital.put("Australia", "Canberra");
         cityCapital.put("Azerbaijan", "Baku");
         cityCapital.put("Belarus", "Minsk");
 
+        HashMap<String, List<String>> friends = new HashMap<>();
+        friends.put("Andrey", List.of("Sasha", "Dmitry"));
+        friends.put("Ivan", List.of("Sasha", "Dmitry"));
+        friends.put("Nikolai", List.of("Oleg", "Michail"));
+        friends.put("Ilia", List.of("Oleg", "Michail"));
+
         System.out.println("Пары чисел: " + StaticMethod.pairChisel(nums, 5));
         System.out.println("Название столиц: " + StaticMethod.cityList(cityCapital));
-//        System.out.println("Максимальное значение: " + StaticMethod.maxNum(nums));
-//        System.out.println("Среднее значение: " + StaticMethod.averageNum(nums));
-//        System.out.println("Кол-во строк начинающихся с B: "+ + StaticMethod.countStr(str, "B"));
-//        System.out.println("Строки с подстрокой Va: " +  StaticMethod.filterStr(str, "Va"));
-//        System.out.println("Сортировка по длине: " +  StaticMethod.sortForLenght(str));
-//
-//        Predicate<String> predicate = s -> s.length() > 5;
-//        System.out.println("Условие: " + StaticMethod.anyMatchFind(str, predicate));
-//
-//        System.out.println("Первый мин элемент: " +  StaticMethod.minFind(nums, 4));
-//        System.out.println("Строка -> длина: " +  StaticMethod.strLenght(str));
+        System.out.println("Строки с буквы В: " + StaticMethod.wordList(str, 'B'));
+        System.out.println("Строки с буквы В: " + StaticMethod.friendsPair(friends));
     }
 }
--- src/main/java/faang/school/godbless/streamAPI2/StaticMethod.java
+++ src/main/java/faang/school/godbless/streamAPI2/StaticMethod.java
@@ -1,6 +1,7 @@
 package faang.school.godbless.streamAPI2;
 
 import java.util.*;
+import java.util.stream.Collectors;
 
 public class StaticMethod {
     public static List<List<Integer>> pairChisel(List<Integer> nums, Integer element) {
@@ -18,8 +19,36 @@ public class StaticMethod {
     }
 
     public static List<String> cityList(HashMap<String, String> cityCapital) {
-        List<String> result = new ArrayList<>();
+        return cityCapital.entrySet().stream()
+                .sorted(Map.Entry.comparingByKey())
+                .map(city -> city.getValue())
+                .collect(Collectors.toList());
+    }
 
-        return result;
+    public static List<String> wordList(List<String> str, char word) {
+        return str.stream().filter(s ->s.charAt(0) == word)
+                .sorted(Comparator.comparingInt(String::length))
+                .toList();
+    }
+
+    public static List<String> friendsPair(HashMap<String,List<String>> friends) {
+        return friends.entrySet()
+                .stream()
+                .flatMap(person1 -> friends.entrySet().stream()
+                        .filter(person2 -> !person2.getKey().equals(person1.getKey()))
+                        .filter(person2 -> !person1.getValue().contains(person2.getKey()))
+                        .filter(person2 -> person1.getValue().stream()
+                                .anyMatch(person2.getValue()::contains))
+                        .map(person2 -> {
+                            String nameFirst = person1.getKey();
+                            String nameSecond = person2.getKey();
+                            if (nameFirst.compareTo(nameSecond) < 0) {
+                                return nameFirst + " " + nameSecond;
+                            } else {
+                                return nameSecond + " " + nameFirst;
+                            }
+                        }))
+                .distinct()
+                .toList();
     }
 }
