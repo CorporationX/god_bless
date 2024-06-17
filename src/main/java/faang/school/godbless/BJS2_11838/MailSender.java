933050dd73137c0876c723dfc089bb50721fde8f
s.kolbasov <s.kolbasov@bftcom.com>
2024-06-17 15:30:43 +0300

    11838 Вы работаете в Microsoft!

---
+++ src/main/java/faang/school/godbless/BJS2_11838/MailSender.java
@@ -0,0 +1,21 @@
+package faang.school.godbless.BJS2_11838;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
+
+public class MailSender {
+  private static final int THREADS = 5;
+  public static void main(String[] args) {
+    ExecutorService executorService = Executors.newFixedThreadPool(THREADS);
+    for (int i = 0 ; i < THREADS; i++) {
+      executorService.execute(new SenderRunnable(i * 200 + 1, i * 200 + 1 + 200));
+    }
+    executorService.shutdown();
+    try {
+      executorService.awaitTermination(10, TimeUnit.SECONDS);
+    } catch (InterruptedException e) {
+      throw new RuntimeException(e);
+    }
+  }
+}
