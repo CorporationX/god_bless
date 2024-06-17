1423c0ab8fd35265b320a30c62b09fe0886978be
Sergey Shkaev <shkaev.ss@mail.ru>
2024-06-17 21:32:16 +0300

    удалил ненужные комментарии

--- src/main/java/faang/school/godbless/task_19_instagram/Main.java
+++ src/main/java/faang/school/godbless/task_19_instagram/Main.java
@@ -8,12 +8,10 @@ public class Main {
 
         FilterProcessor filterProcessor = new FilterProcessor();
 
-        // Создание фильтров
         Function<Image, Image> grayscaleFilter = (image) -> new Image(image.name() + "_grayscale", "Фильтр: черно-белый");
         Function<Image, Image> sepiaFilter = (image) -> new Image(image.name() + "_sepia", "Фильтр: сепия");
         Function<Image, Image> vignetteFilter = (image) -> new Image(image.name() + "_vignette", "Фильтр: виньетка");
 
-        // Применение фильтров
         Image grayscaleImage = filterProcessor.applyFilter(originalImage, grayscaleFilter);
         Image sepiaImage = filterProcessor.applyFilter(originalImage, sepiaFilter);
         Image vignetteImage = filterProcessor.applyFilter(originalImage, vignetteFilter);
@@ -21,7 +19,6 @@ public class Main {
         System.out.println(sepiaImage);
         System.out.println(vignetteImage);
 
-        // Создание и применение комбинированного фильтра
         Function<Image, Image> combinedFilter = filterProcessor.combineFilters(grayscaleFilter, sepiaFilter);
         Image combinedImage = filterProcessor.applyFilter(originalImage, combinedFilter);
         System.out.println(combinedImage);
