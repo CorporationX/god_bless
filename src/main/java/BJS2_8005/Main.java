a9f3ebe1f287f5a737c7fa6aba066d70f8c3499b
Ekaterina Shulenina <shulenina.em@gmail.com>
2024-06-17 20:47:27 +1000

    Задача Java HashMap: Игра престолов

--- src/main/java/BJS2_8005/Main.java
+++ src/main/java/BJS2_8005/Main.java
@@ -4,21 +4,17 @@ import java.util.HashMap;
 
 public class Main {
     public static void main(String[] args) {
-        HashMap<String, House> gameOfThrones = new HashMap<>();
-        House house01 = new House("Stark", "Wolf");
-        House house02 = new House("Lannister", "Lion");
-        House house03 = new House("Baratheon", "Deer");
-        gameOfThrones.put(house01.getName(), house01);
-        gameOfThrones.put(house02.getName(), house02);
-        gameOfThrones.put(house03.getName(), house03);
-        System.out.println(gameOfThrones);
-        gameOfThrones.remove(house01.getName());
-        if (gameOfThrones.containsKey(house01.getName())) {
-            System.out.println(gameOfThrones.get(house01.getName()).getSigil());
-        } else {
-            System.out.println("Дома нет в списке");
-        }
-        System.out.println(gameOfThrones);
+
+        House houseStark = new House("Stark", "Wolf");
+        House houseLannister = new House("Lannister", "Lion");
+        House houseBaratheon = new House("Baratheon", "Deer");
+        House.addHouse(houseStark);
+        House.addHouse(houseLannister);
+        House.addHouse(houseBaratheon);
+        House.removeByName("Stark");
+        House.sigilInfo("Lannister");
+        House.sigilInfo("Stark");
+        House.allHouses();
     }
 }
 
