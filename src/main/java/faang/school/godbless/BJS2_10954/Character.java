dac266ad20bea2e21ad6346611087c0123ff8342
s.kolbasov <s.kolbasov@bftcom.com>
2024-06-17 21:31:45 +0300

    BJS2-10954 Распределенная армия героев меча и магии

---
+++ src/main/java/faang/school/godbless/BJS2_10954/Character.java
@@ -0,0 +1,10 @@
+package faang.school.godbless.BJS2_10954;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public abstract class Character {
+  private int power;
+}
