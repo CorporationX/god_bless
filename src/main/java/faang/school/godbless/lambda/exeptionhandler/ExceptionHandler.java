3391d8ff62672ffedbfa95b76536dc266db7b5d8
Caveri95 <ura22051995@gmail.com>
2024-06-17 14:23:05 +0300

    BJS2-9559
    - задачу перенес в другой пакет

--- src/main/java/faang/school/godbless/lambda/exeptionhandler/ExceptionHandler.java
+++ src/main/java/faang/school/godbless/lambda/exeptionhandler/ExceptionHandler.java
@@ -1,4 +1,4 @@
-package faang.school.godbless.lambda;
+package faang.school.godbless.lambda.exeptionhandler;
 
 
 @FunctionalInterface
--- src/main/java/faang/school/godbless/lambda/exeptionhandler/Main.java
+++ src/main/java/faang/school/godbless/lambda/exeptionhandler/Main.java
@@ -1,4 +1,4 @@
-package faang.school.godbless.lambda;
+package faang.school.godbless.lambda.exeptionhandler;
 
 import java.util.Random;
 import java.util.function.Supplier;
