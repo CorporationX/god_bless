fda321ecb04758af05a4314f6839d678db734d73
Andrei Kuzmin <Andreytko@mail.ru>
2024-06-17 23:52:15 +0300

    переименовал задания, добавил номер

--- src/main/java/faang/school/godbless/Task_Calculator/Calculator.java
+++
@@ -1,6 +0,0 @@
-package faang.school.godbless.Task_Calculator;
-
-@FunctionalInterface
-public interface Calculator {
-    public double calculate(double x, double y);
-}
--- src/main/java/faang/school/godbless/Task_Hogwarts/SpellAction.java
+++
@@ -1,6 +0,0 @@
-package faang.school.godbless.Task_Hogwarts;
-
-@FunctionalInterface
-public interface SpellAction {
-    public String cast(String spell);
-}
