da71945969f69d15f843138109230daa12d7e80e
Andrei Kuzmin <Andreytko@mail.ru>
2024-06-17 23:44:12 +0300

    сделал отдельные методы

--- src/main/java/faang/school/godbless/Droids/Droid.java
+++ src/main/java/faang/school/godbless/Droids/Droid.java
@@ -10,16 +10,7 @@ public class Droid {
     public String sendEncryptedMessage(String message, int key) {
         if (message.isBlank() || key<=0 || key>26) throw new IllegalArgumentException("invalid args");
         DroidMessageEncryptor encryptor = (message1, key1) -> {
-            StringBuilder result = new StringBuilder();
-            for (int i=0; i<message1.length(); i++) {
-                for (int j=0; j<=25; j++) {
-                    if (message1.charAt(i) == ALPHABET.charAt(j))  {
-                        result.append(ALPHABET.charAt((j + key1) % 26));
-                        break;
-                    }
-                }
-            }
-            return String.valueOf(result);
+            return encryptMessage(message1, key1);
         };
         return encryptor.encrypt(message, key);
     }
@@ -27,18 +18,35 @@ public class Droid {
     public String receiveEncryptedMessage(String message, int key) {
         if (message.isBlank() || key<=0 || key>26) throw new IllegalArgumentException("invalid args");
         DroidMessageEncryptor decryptor = (message1, key1) -> {
-            StringBuilder result = new StringBuilder();
-            for (int i=0; i<message1.length(); i++) {
-                for (int j=0; j<=25; j++) {
-                    if (message1.charAt(i) == ALPHABET.charAt(j))  {
-                        result.append(ALPHABET.charAt((j - key1 + 26) % 26));
-                        break;
-                    }
-                }
-            }
-            return String.valueOf(result);
+            return decryptMessage(message1, key1);
         };
         return decryptor.encrypt(message, key);
+    }
+
+    public static String encryptMessage(String message, int key) {
+        StringBuilder result = new StringBuilder();
+        for (int i=0; i<message.length(); i++) {
+            for (int j=0; j<=25; j++) {
+                if (message.charAt(i) == ALPHABET.charAt(j))  {
+                    result.append(ALPHABET.charAt((j + key) % 26));
+                    break;
+                }
+            }
+        }
+        return String.valueOf(result);
+    }
+
+    public static String decryptMessage(String message, int key) {
+        StringBuilder result = new StringBuilder();
+        for (int i=0; i<message.length(); i++) {
+            for (int j=0; j<=25; j++) {
+                if (message.charAt(i) == ALPHABET.charAt(j))  {
+                    result.append(ALPHABET.charAt((j - key + 26) % 26));
+                    break;
+                }
+            }
+        }
+        return String.valueOf(result);
     }
 
 }
