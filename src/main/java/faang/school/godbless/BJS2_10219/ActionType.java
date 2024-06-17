21f233c40114c36dc36f3acd59ec05d07da997b5
Mikhail Fokin <fokinms@gmail.com>
2024-06-17 16:57:13 +0400

    BJS2-10219 Анализ активности пользователей в социальной сети

---
+++ src/main/java/faang/school/godbless/BJS2_10219/ActionType.java
@@ -0,0 +1,8 @@
+package faang.school.godbless.BJS2_10219;
+
+public enum ActionType {
+    post,
+    comment,
+    like,
+    share
+}
---
+++ src/main/java/faang/school/godbless/BJS2_10219/Main.java
@@ -0,0 +1,82 @@
+package faang.school.godbless.BJS2_10219;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+public class Main {
+
+
+    public static void main(String[] args) {
+        List<UserAction> actions = new ArrayList<>();
+        actions.add(new UserAction(1, "n1", ActionType.comment, LocalDateTime.now(), "#first какой-то коммент"));
+        actions.add(new UserAction(1, "n1", ActionType.share, LocalDateTime.now(), "c1"));
+        actions.add(new UserAction(1, "n1", ActionType.like, LocalDateTime.now(), "c1"));
+        actions.add(new UserAction(2, "n2", ActionType.comment, LocalDateTime.now(), "#first какой-то коммент"));
+        actions.add(new UserAction(2, "n2", ActionType.post, LocalDateTime.now(), "#second какой-то пост"));
+        actions.add(new UserAction(2, "n2", ActionType.like, LocalDateTime.now(), "c2"));
+        actions.add(new UserAction(3, "n3", ActionType.comment, LocalDateTime.of(2023, 5, 5, 5, 5), "#third какой-то коммент"));
+        actions.add(new UserAction(3, "n3", ActionType.share, LocalDateTime.now(), "c3"));
+        actions.add(new UserAction(3, "n3", ActionType.comment, LocalDateTime.of(2024, 2, 2, 2, 2), "какой-то коммент #fourth"));
+        actions.add(new UserAction(3, "n3", ActionType.share, LocalDateTime.now(), "c3"));
+        actions.add(new UserAction(4, "n4", ActionType.comment, LocalDateTime.now(), "какой-то коммент #fourth"));
+        actions.add(new UserAction(4, "n4", ActionType.share, LocalDateTime.now(), "c4"));
+        actions.add(new UserAction(4, "n4", ActionType.comment, LocalDateTime.now(), "какой-то #fifth коммент"));
+        actions.add(new UserAction(4, "n4", ActionType.post, LocalDateTime.now(), "какой-то #fifth пост"));
+
+        findTopTenMostActiveUsersByActionsNumber(actions);
+        findTopFiveMostPopularThemesInPostAndComments(actions);
+        findTopThreeUsersWithMaxCommentsForMonth(actions);
+        calculatePercetageOfActions(actions);
+    }
+
+    //найти топ-10 самых активных пользователей
+    public static void findTopTenMostActiveUsersByActionsNumber(List<UserAction> userActions) {
+        userActions.stream()
+                .collect(Collectors.groupingBy(UserAction::getUserId, Collectors.counting()))
+                .entrySet().stream()
+                .sorted(Map.Entry.<Integer, Long>comparingByValue().reversed())
+                .limit(10)
+                .forEach(entry -> System.out.println("Юзер с id = " + entry.getKey() + " имеет " + entry.getValue() + " действий"));
+    }
+
+    public static void findTopFiveMostPopularThemesInPostAndComments(List<UserAction> userActions) {
+        userActions.stream()
+                .filter(action -> action.getActionType() == ActionType.comment ||
+                        action.getActionType() == ActionType.post)
+                .filter(action -> action.content.contains("#"))
+                .map(action -> action.getContent().split(" "))
+                .flatMap(strings -> Arrays.stream(strings).
+                        filter(string -> string.regionMatches(0, "#", 0, 1)))
+                .collect(Collectors.groupingBy(s -> s, Collectors.counting()))
+                .entrySet().stream()
+                .sorted(Map.Entry.<String, Long>comparingByValue().reversed())
+                .limit(5)
+                .forEach(entry -> System.out.println("хэштег " + entry.getKey() + " встречается в постах и комментариях " + entry.getValue() + " раз"));
+    }
+
+    public static void findTopThreeUsersWithMaxCommentsForMonth(List<UserAction> userActions) {
+        userActions.stream()
+                .filter(action -> action.getActionType() == ActionType.comment)
+                .filter(action -> action.getActionDate().isAfter(LocalDateTime.now().minusMonths(1)))
+                .collect(Collectors.groupingBy(UserAction::getUserId, Collectors.counting()))
+                .entrySet().stream()
+                .sorted(Map.Entry.<Integer, Long>comparingByValue().reversed())
+                .limit(3)
+                .forEach(entry -> System.out.println("Юзер с id = " + entry.getKey() + " имеет " + entry.getValue() + " комментариев за последний месяц"));
+    }
+
+    public static void calculatePercetageOfActions(List<UserAction> userActions) {
+        Map<ActionType, Long> userAction = userActions.stream()
+                .collect(Collectors.groupingBy(UserAction::getActionType, Collectors.counting()));
+
+        Integer allActionsCount = userAction.values().stream()
+                .map(Long::intValue)
+                .reduce(0, Integer::sum);
+
+        userAction.forEach((key, value) -> System.out.println("действие " + key + " " + value * 100 / allActionsCount + "% из общего числа"));
+    }
+}
