9712c9193da445160e32df93f702091305e7c156
Sergey Shkaev <shkaev.ss@mail.ru>
2024-06-17 20:07:25 +0300

    BJS2-9102. Калькулятор. class Programm, tests

---
+++ src/main/java/faang/school/godbless/task_22_calculator/Calculator.java
@@ -0,0 +1,6 @@
+package faang.school.godbless.task_22_calculator;
+
+public interface Calculator {
+//    <T> T operation (T firstVariable, T secondVariable);
+    int operation(int a, int b);
+}
---
+++ src/test/java/faang/school/godbless/task_22_calculator/ProgramTest.java
@@ -0,0 +1,46 @@
+package faang.school.godbless.task_22_calculator;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+public class ProgramTest {
+
+    @Test
+    public void testCalculate_list() {
+        // Arrange
+        List<Integer> numbers = List.of(2, 4, 6);
+        int expectationSum = 12;
+        int expectationProduct = 48;
+
+        // Act
+        int actualSum = Program.sum(numbers);
+        int actualProduct = Program.product(numbers);
+
+        // Assert
+        assertEquals(expectationSum, actualSum, "суммы различаются");
+        assertEquals(expectationProduct, actualProduct, "произведения различаются");
+    }
+
+    @Test
+    public void testCalculate_throwsIllegalArgumentException() {
+        // Arrange
+        List<Integer> listEmpty = new ArrayList<>();
+
+        // Assert
+        assertThrows(IllegalArgumentException.class, () -> Program.sum(listEmpty), "При передачи пустого списка метод должен выбрасывать IllegalArgumentException");
+    }
+
+    @Test
+    public void testCalculate_NullPointerException() {
+        // Arrange
+        List<Integer> listNull = null;
+
+        // Assert
+        assertThrows(NullPointerException.class, () -> Program.sum(listNull), "При передачи null метод должен выбрасывать NullPointerException");
+    }
+}
