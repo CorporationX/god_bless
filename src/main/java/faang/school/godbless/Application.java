0313ece8890f48f431d53f6680fbaea5012fbbb9
Almas D <berser91@gmail.com>
2024-06-18 22:23:20 +0500

    created and implemented few classes

---
+++ src/main/java/faang/school/godbless/Application.java
@@ -0,0 +1,18 @@
+package faang.school.godbless;
+
+public class Application {
+    public static void main(String[] args) {
+
+        System.out.println("Hello Almas!");
+
+        Army army = new Army();
+        army.addUnit(new Archer(25));
+        army.addUnit(new Swordsman(40));
+        army.addUnit(new Swordsman(65));
+        army.addUnit(new Mage(20));
+        army.addUnit(new Mage(50));
+
+        int totalPower = army.calculateTotalPower();
+        System.out.println("Total army power: " + totalPower);
+    }
+}
