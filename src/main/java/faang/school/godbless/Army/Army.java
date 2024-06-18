4c859b04fa5285376f7d1bf3a30d9be8523ed2a0
Alen <Alendos87@gmail.com>
2024-06-18 17:25:42 +0500

    Распределение героя меча магии и лука

--- src/main/java/faang/school/godbless/Army/Army.java
+++ src/main/java/faang/school/godbless/Army/Army.java
@@ -11,90 +11,56 @@ import java.util.concurrent.Executors;
 
 public class Army {
 
-    List<Unit> mages = new ArrayList<>();
-    List<Unit> archers = new ArrayList<>();
-    List<Unit> swordsmen = new ArrayList<>();
+    List<PowerThread> swordsmenThread = new ArrayList<>();
+    List<PowerThread> archerThread = new ArrayList<>();
+    List<PowerThread> mageThreads = new ArrayList<>();
 
-    List<PowerThread> magesThreads = new ArrayList<>();
-
-    private List<Unit> units;
+    private final List<Unit> units;
 
     public Army(List<Unit> units) {
         this.units = units;
     }
 
+    public void processUnit(Unit unit, List<PowerThread> threadList){
+        PowerThread powerThread = new PowerThread(unit);
+        threadList.add(powerThread);
+        powerThread.start();
+    }
+
     int calculateTotalPower() throws InterruptedException {
+
         for (Unit unit : units) {
             if (unit.isMage()) {
-                mages.add(unit);
+                processUnit(unit, mageThreads);
             } else if (unit.isArcher()) {
-                archers.add(unit);
+                processUnit(unit, archerThread);
             } else if (unit.isSwordsMan()) {
-                swordsmen.add(unit);
+                processUnit(unit, swordsmenThread);
             }
         }
+        return getTotal();
+    }
+
+    private int getTotal() throws  InterruptedException {
+        int totalMagePower = getPowerSum(mageThreads);
+        int totalArcherPower = getPowerSum(archerThread);
+        int totalSwordsPower = getPowerSum(swordsmenThread);
 
 
-        List<PowerThread> mageThreads = new ArrayList<>();
+        System.out.println("Mage" + totalMagePower);
+        System.out.println("Archer" + totalArcherPower);
+        System.out.println("Swords" + totalSwordsPower);
 
-        for (Unit mage : mages) {
-            PowerThread powerThread = new PowerThread(mage);
-            mageThreads.add(powerThread);
-            powerThread.start();
-        }
+        return totalMagePower + totalArcherPower + totalSwordsPower;
+    }
 
-        List<PowerThread> archerThread = new ArrayList<>();
-
-        for (Unit archer : archers) {
-            PowerThread powerThread = new PowerThread(archer);
-            archerThread.add(powerThread);
-            powerThread.start();
-        }
-
-        List<PowerThread> swordsmenThread = new ArrayList<>();
-
-        for (Unit swordsman : swordsmen) {
-            PowerThread powerThread = new PowerThread(swordsman);
-            swordsmenThread.add(powerThread);
-            powerThread.start();
-        }
-
-        int totalMagePower = 0;
-        for (PowerThread mageThread : mageThreads) {
-            try {
-                mageThread.join();
-                totalMagePower += mageThread.getPower();
-            } catch (InterruptedException e) {
-                e.printStackTrace();
+    private int getPowerSum(List<PowerThread> units) throws InterruptedException {
+        int totalPower = 0;
+        for (PowerThread mageThread : units) {
+            mageThread.join();
+            totalPower += mageThread.getPower();
             }
-        }
-
-        int totalArcherPower = 0;
-        for (PowerThread archerThreads : archerThread) {
-            try {
-                archerThreads.join();
-                totalArcherPower += archerThreads.getPower();
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
-        }
-
-        int totalSwordsmanPower = 0;
-            for (PowerThread swordThread : swordsmenThread) {
-                try {
-                    swordThread.join();
-                    totalSwordsmanPower += swordThread.getPower();
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
-                }
-            }
-
-            System.out.println("Mage = " + totalMagePower);
-            System.out.println("Archer = " + totalArcherPower);
-            System.out.println("Swordsmen =" + totalSwordsmanPower);
-
-
-            return 0;
+        return totalPower;
         }
     }
 
