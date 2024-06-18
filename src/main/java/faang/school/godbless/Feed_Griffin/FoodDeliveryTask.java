eb84d51e2cdb57642b39fe9c2e017cd7f53bab09
Andrei Kuzmin <Andreytko@mail.ru>
2024-06-18 19:27:32 +0300

    в процессе...

---
+++ src/main/java/faang/school/godbless/Feed_Griffin/FoodDeliveryTask.java
@@ -0,0 +1,34 @@
+package faang.school.godbless.Feed_Griffin;
+
+import lombok.Getter;
+
+import java.util.Random;
+
+@Getter
+public class FoodDeliveryTask implements Runnable {
+
+    private String character;
+    private int foodAmount;
+
+    public FoodDeliveryTask(String character, int foodAmount) {
+        if (character.isBlank() || foodAmount < 0) throw new IllegalArgumentException();
+        else {
+            this.character = character;
+            this.foodAmount = foodAmount;
+        }
+    }
+
+    private String getFoodType() {
+        String[] foodTypes = {"pizza", "burger", "hot dog", "chicken wings", "taco"};
+        return foodTypes[new Random().nextInt(foodTypes.length)];
+    }
+
+    @Override
+    public void run() {
+        String food = this.getFoodType();
+        for (int i=1; i<=foodAmount; i++) {
+            System.out.println(food + " : portion number " + i + " was sent");
+        }
+    }
+
+}
