7de59d80ded992b228575896a4873c7e642c6803
Alen <Alendos87@gmail.com>
2024-06-18 10:48:48 +0500

    foodType получаю теперь через get, а не через рандом

--- src/main/java/faang/school/godbless/Microsoft/MailSender.java
+++ src/main/java/faang/school/godbless/Microsoft/MailSender.java
@@ -2,20 +2,16 @@ package faang.school.godbless.Microsoft;
 
 public class MailSender {
     public static void main(String[] args) {
-        for (int i = 0; i < args.length; i++) {
-            int startIndex = i * 200 + 1;
-            int endIndex = startIndex + 9;
-
-            SenderRunnable senderRunnable = new SenderRunnable(startIndex, endIndex);
-            Thread thread = new Thread(senderRunnable);
-            thread.start();
-
-            try{
-                thread.join();
-            } catch (InterruptedException e) {
-                throw new RuntimeException("Thread interrupted", e);
-            }
+        int mailThreads = 200;
+        int numThreads = 5;
+        for (int i = 0; i < numThreads; i++){
+            int startIndex = i * mailThreads + 1;
+            int endIndex = startIndex + mailThreads;
+            new Thread(new SenderRunnable(startIndex, endIndex)).start();
         }
     }
-
 }
+
+
+
+
