daf113073bd3c25b5c9c24f4186a84506b0f4567
Gevorg <a0964593@gmail.com>
2024-06-18 09:11:52 +0300

    BJS2-11011 - it works, but incorrect

--- src/main/java/faang/school/godbless/DistributedArmyOfHeroesOfMightAndMagic/Army.java
+++ src/main/java/faang/school/godbless/DistributedArmyOfHeroesOfMightAndMagic/Army.java
@@ -1,40 +1,122 @@
 package faang.school.godbless.DistributedArmyOfHeroesOfMightAndMagic;
 
+import lombok.Getter;
+
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
+import java.util.concurrent.Callable;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
+import static java.util.concurrent.TimeUnit.SECONDS;
+
+@Getter
 public class Army {
     private final Map<String, List<Units>> allUnits = new HashMap<>();
+    private final Map<String, Integer> powerOfEachType = new HashMap<>();
 
-    public int calculateTotalPower() {
+    public int calculateTotalPower() throws InterruptedException {
+        int totalPower = 0;
         Set<String> types = allUnits.keySet();
         ExecutorService threadPool = Executors.newFixedThreadPool(types.size());
 
         for (String type : types) {
-            threadPool.submit(calculatePowerOfType(type));
+            powerOfEachType.put(type, 0);
+            threadPool.submit(new calculatePowerOfType(type));
         }
-    }
 
-    public Runnable calculatePowerOfType(String type) {
-        int result = 0;
-        for (Units unit : allUnits.get(type)) {
-            result += unit.getPower();
+        try {
+            threadPool.awaitTermination(10, SECONDS);
+        } catch(InterruptedException e) {
+            throw new InterruptedException();
         }
-        return null;
+
+        for (String type : powerOfEachType.keySet()) {
+            totalPower += powerOfEachType.get(type);
+        }
+
+        threadPool.shutdown();
+        return totalPower;
     }
 
     public void addUnit(Units unit) {
-        if (!allUnits.containsKey("Archer")) {
+        if (!allUnits.containsKey(unit.getType())) {
             List<Units> list = new ArrayList<>();
             list.add(unit);
-            allUnits.put("Archer", list);
+            allUnits.put(unit.getType(), list);
         } else {
             allUnits.get(unit.getType()).add(unit);
         }
     }
+
+    private class calculatePowerOfType implements Runnable {
+        private String type;
+
+        @Override
+        public void run() {
+            System.out.println("Start counting Power of " + type);
+            for (Units unit : allUnits.get(type)) {
+                System.out.println(type + " " + powerOfEachType.get(type));
+                powerOfEachType.put(type, powerOfEachType.get(type) + unit.getPower());
+                System.out.println(type + " " + powerOfEachType.get(type));
+            }
+            System.out.println("End counting Power of " + type);
+        }
+
+        public calculatePowerOfType(String type) {
+            this.type = type;
+        }
+    }
+
+    /*
+    private final Map<String, List<Units>> allUnits = new HashMap<>();
+    private final Map<String, Integer> powerOfEachType = new HashMap<>();
+
+    public int calculateTotalPower() {
+        int totalPower = 0;
+        Set<String> types = allUnits.keySet();
+        ExecutorService threadPool = Executors.newFixedThreadPool(types.size());
+
+        for (String type : types) {
+            powerOfEachType.put(type, 0);
+            threadPool.submit(new calculatePowerOfType(type));
+        }
+
+        threadPool.shutdown();
+
+        for (String type : powerOfEachType.keySet()) {
+            totalPower += powerOfEachType.get(type);
+        }
+        return totalPower;
+    }
+
+    public void addUnit(Units unit) {
+        if (!allUnits.containsKey(unit.getType())) {
+            List<Units> list = new ArrayList<>();
+            list.add(unit);
+            allUnits.put(unit.getType(), list);
+        } else {
+            allUnits.get(unit.getType()).add(unit);
+        }
+    }
+
+    private class calculatePowerOfType implements Runnable {
+        private String type;
+
+        @Override
+        public void run() {
+            System.out.println("Start counting Power of " + type);
+            for (Units unit : allUnits.get(type)) {
+                powerOfEachType.compute(type, (k, i) -> i + unit.getPower());
+            }
+            System.out.println("End counting Power of " + type);
+        }
+
+        public calculatePowerOfType(String type) {
+            this.type = type;
+        }
+    }*/
 }
--- src/main/java/faang/school/godbless/DistributedArmyOfHeroesOfMightAndMagic/Main.java
+++ src/main/java/faang/school/godbless/DistributedArmyOfHeroesOfMightAndMagic/Main.java
@@ -4,7 +4,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
         Army army = new Army();
         army.addUnit(new Archer(25)); // считаем в потоке 1
         army.addUnit(new Swordsman(40)); // считаем в потоке 2
