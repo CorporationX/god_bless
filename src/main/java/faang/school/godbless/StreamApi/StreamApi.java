371eee56348d05d8c00e40b015718c9e606156a5
Alen <Alendos87@gmail.com>
2024-06-18 19:41:12 +0500

    StreamApi 2 решение задачи

--- src/main/java/faang/school/godbless/StreamApi/StreamApi.java
+++ src/main/java/faang/school/godbless/StreamApi/StreamApi.java
@@ -2,6 +2,7 @@ package faang.school.godbless.StreamApi;
 
 import java.util.*;
 import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 public class StreamApi {
 
@@ -56,6 +57,75 @@ public class StreamApi {
         }
     return result;
     }
+
+    public Map<String, Double> averageSalaryByDepartment(List<Employee> employees){
+        return employees.stream()
+                .collect(Collectors.groupingBy(Employee::getDepartment,
+                        Collectors.averagingDouble(Employee::getSalary)));
+    }
+
+    public List<String> filterAndSortByAlphabet(List<String> strings, char[] alphabet){
+        List<String> filterStrings = new ArrayList<>();
+        for (String string : strings) {
+            if(containsOnlyChars(string, alphabet)){
+                filterStrings.add(string);
+            }
+        }
+        filterStrings.sort((s1, s2) -> Integer.compare(s1.length(), s2.length()));
+        return filterStrings;
+    }
+
+    public static boolean containsOnlyChars(String str, char[] alphabet){
+        for (char alpha: str.toCharArray()){
+            if(!containsChar(alpha, alphabet)){
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public static boolean containsChar(char ch, char[] alphabet){
+        for (char alpha: alphabet){
+            if(ch==alpha){
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public static List<String> convertToBinary(List<Integer> numbers){
+        return numbers.stream()
+                .map(Integer::toBinaryString)
+                .collect(Collectors.toList());
+    }
+
+    public static List<Integer> findPalindromeInRange(int start, int end){
+        List<Integer> result = new ArrayList<>();
+        for (int i = start; i <= end; i++){
+            if (isPalindrome(i)) {
+                result.add(i);
+            }
+        }
+        return result;
+    }
+
+    public static boolean isPalindrome(int number){
+        String numStr = String.valueOf(number);
+        int left = 0;
+        int right = numStr.length() - 1;
+
+        while (left < right){
+            if (numStr.charAt(left) != numStr.charAt(right)){
+                return false;
+            }
+            left++;
+            right--;
+        }
+        return true;
+    }
+
+
+
 }
 
 
