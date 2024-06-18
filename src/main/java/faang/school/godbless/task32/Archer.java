642c345050552cb43adb682c71e3b6bb8a49e46a
Иван Шилов <ivshvl39@gmail.com>
2024-06-18 11:21:31 +0300

    Распределенная армия героев меча и магии

---
+++ src/main/java/faang/school/godbless/task32/Archer.java
@@ -0,0 +1,11 @@
+package faang.school.godbless.task32;
+
+public class Archer extends Unit{
+
+    public Archer(int power) {
+        super(power);
+    }
+    public boolean isArcher(){
+        return true;
+    }
+}
---
+++ src/main/java/faang/school/godbless/task32/Swordsman.java
@@ -0,0 +1,10 @@
+package faang.school.godbless.task32;
+
+public class Swordsman extends Unit{
+    public Swordsman( int power) {
+        super(power);
+    }
+    public boolean isSwordsman(){
+        return true;
+    }
+}
