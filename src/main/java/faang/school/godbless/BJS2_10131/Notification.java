faccbce1c077f4d65eea2beb04f08c2a4e844209
DemureZ <lopatin.g.n@yandex.ru>
2024-06-18 11:41:22 +0300

    проверка на null

--- src/main/java/faang/school/godbless/BJS2_10131/Notification.java
+++ src/main/java/faang/school/godbless/BJS2_10131/Notification.java
@@ -1,16 +1,12 @@
 package faang.school.godbless.BJS2_10131;
 
-import lombok.AllArgsConstructor;
 import lombok.Data;
-
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Set;
 
 @Data
 public class Notification {
     private String type;
     private String message;
+
     public Notification(String type, String message) {
         if(type.isBlank()){
             System.out.println("Используй корректный тип сообщения");
@@ -23,5 +19,4 @@ public class Notification {
         }
         this.message = message;
     }
-
 }
--- src/main/java/faang/school/godbless/BJS2_10131/NotificationManager.java
+++ src/main/java/faang/school/godbless/BJS2_10131/NotificationManager.java
@@ -6,10 +6,16 @@ import java.util.function.Consumer;
 
 public class NotificationManager {
     private Map<String,Consumer<Notification>> map = new HashMap<>();
+
     public void registerHandler(String id, Consumer<Notification> action){
         map.put(id, action);
     }
+
     public void sendNotification(Notification notification){
+        if (notification.getType() == null || notification.getMessage() == null){
+            System.out.println("Некорректное оповещение: "+notification);
+            return;
+        }
         map.get(notification.getType()).accept(notification);
     }
 }
