fb114f55049018f47ef7f9f9c58d542d0778e0cc
shade2903 <nighshade2903@gmail.com>
2024-06-19 13:18:08 +0300

    BJS2-12110 They were very nice but extremely poor
    implementation tasks using Executors.newCachedThreadPool()

--- src/main/java/faang/school/godbless/paralelism/weasley_family/WeasleyFamily.java
+++ src/main/java/faang/school/godbless/paralelism/weasley_family/WeasleyFamily.java
@@ -5,17 +5,17 @@ import java.util.concurrent.Executors;
 
 public class WeasleyFamily {
     public static void main(String[] args) {
-        Chore[] chores = {
-                new Chore("Washing windows"),
-                new Chore("Cleaning Furniture"),
-                new Chore("Washing dishes"),
-                new Chore("Sweeping the floor"),
-                new Chore("Cocking"),
-                new Chore("Fixing broken things")
+        String[] chores = {
+                "Washing windows",
+                "Cleaning Furniture",
+                "Washing dishes",
+                "Sweeping the floor",
+                "Cocking",
+                "Fixing broken things"
         };
         ExecutorService executor = Executors.newCachedThreadPool();
-        for(Chore chore : chores){
-            executor.submit(chore);
+        for (String chore : chores) {
+            executor.submit(new Chore(chore));
         }
         executor.shutdown();
     }
