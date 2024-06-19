a5901998e8b0157f04f92ca2d4df40200a630d33
Andrei Kuzmin <Andreytko@mail.ru>
2024-06-20 00:51:16 +0300

    done 3rd task;

--- src/main/java/faang/school/godbless/Task_API_2/Main.java
+++ src/main/java/faang/school/godbless/Task_API_2/Main.java
@@ -10,19 +10,21 @@ public class Main {
 
     static final List<Integer> numbers = Arrays.asList(1, 9, 3, 6, 4, 5);
     static final int target = 10;
+    static final List<String> strings = Arrays.asList("something", "some", "sad", "new", "now", "sold");
 
     public static void main(String[] args) {
-
+        //task1
         Set<Solver.Pair> task1 = Solver.findPairs(numbers, target);
         System.out.println(task1);
-
+        //task2
         Map<String, String> countriesAndCapitals = new HashMap<>();
         countriesAndCapitals.put("Russia", "Moscow");
         countriesAndCapitals.put("England", "London");
         countriesAndCapitals.put("Spain", "Madrid");
         countriesAndCapitals.put("France", "Paris");
-        System.out.println(Converter.sortCapitals(countriesAndCapitals));
-
+        System.out.println(StringService.sortCapitals(countriesAndCapitals));
+        //task3
+        System.out.println(StringService.stringFilter(strings, 's'));
     }
 
 }
--- src/main/java/faang/school/godbless/Task_API_2/StringService.java
+++ src/main/java/faang/school/godbless/Task_API_2/StringService.java
@@ -1,15 +1,23 @@
 package faang.school.godbless.Task_API_2;
 
+import java.util.Comparator;
 import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
 
-public class Converter {
+public class StringService {
 
     public static List<String> sortCapitals(Map<String, String> countries) {
         return countries.entrySet().stream()
                 .sorted(Map.Entry.comparingByKey())
                 .map(Map.Entry::getValue)
+                .collect(Collectors.toList());
+    }
+
+    public static List<String> stringFilter(List<String> strings, char s) {
+        return strings.stream()
+                .filter(str->str.startsWith(String.valueOf(s)))
+                .sorted(Comparator.comparingInt(String::length))
                 .collect(Collectors.toList());
     }
 
