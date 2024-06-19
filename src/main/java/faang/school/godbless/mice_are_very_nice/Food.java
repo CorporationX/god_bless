6884108205ce1d505d195ae7ced042e011ad026d
Rauan Kaztaev <rtishtyk@gmail.com>
2024-06-19 23:15:27 +0500

    Mice are very nice!

---
+++ src/main/java/faang/school/godbless/mice_are_very_nice/Food.java
@@ -0,0 +1,14 @@
+package faang.school.godbless.mice_are_very_nice;
+
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.ToString;
+
+@AllArgsConstructor
+@Getter
+@EqualsAndHashCode
+@ToString
+public class Food {
+    private String name;
+}
