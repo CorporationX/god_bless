f1df55aa77caed17fbbac20467e3fecd4432fa62
Alexandra <s7125@mail.ru>
2024-06-19 15:19:49 +0500

    BJS2-12118 add class

---
+++ src/main/java/faang/school/godbless/feedPeterGriffin/GriffinsFoodDelivery.java
@@ -0,0 +1,20 @@
+package faang.school.godbless.feedPeterGriffin;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+public class GriffinsFoodDelivery {
+    public static void main(String[] args) throws InterruptedException {
+        String[] characterNames = {"Peter", "Lois", "Meg", "Chris", "Stewie"};
+
+        ExecutorService executorService = Executors.newFixedThreadPool(3);
+
+        for (String character : characterNames) {
+            executorService.submit(new FoodDeliveryTask(character, new Random().nextInt(50)));
+        }
+        executorService.shutdown();
+    }
+}
