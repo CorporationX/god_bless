bfe5f898462f8adde67c595fb70041a6d108f41b
Danill Klinkov <klinkov.danya@bk.ru>
2024-06-19 22:11:55 +0300

    thread3: Распределенная армия героев меча и магии done

---
+++ src/main/java/faang/school/godbless/thread3/Unit.java
@@ -0,0 +1,10 @@
+package faang.school.godbless.thread3;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@AllArgsConstructor
+@Getter
+public abstract class Unit {
+    private int power;
+}
