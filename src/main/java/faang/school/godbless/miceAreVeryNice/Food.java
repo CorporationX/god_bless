dc5cdd45784d6ac11190e74736999648aa448037
Nikita Volzhin <vlzh.nik@mail.ru>
2024-06-19 23:24:57 +0500

    BJS2-11819 - Mice are very nice!

---
+++ src/main/java/faang/school/godbless/miceAreVeryNice/Food.java
@@ -0,0 +1,10 @@
+package faang.school.godbless.miceAreVeryNice;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@AllArgsConstructor
+@Getter
+public class Food {
+    private String description;
+}
