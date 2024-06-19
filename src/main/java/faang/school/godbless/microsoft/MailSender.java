3dc4207ecc4a504e8c4672aa76e934c3063e955a
Garik-create <pfdfkbybujhm@yandex.ru>
2024-06-19 06:24:54 +0300

    - промежуточный коммит

--- src/main/java/faang/school/godbless/microsoft/MailSender.java
+++ src/main/java/faang/school/godbless/microsoft/MailSender.java
@@ -9,7 +9,7 @@ public class MailSender {
     public static void main(String[] args) {
 
         Queue<String> inputMails = new LinkedList<>();
-        List<String> mailPool1 = new ArrayList<>();
+        ArrayList<String> mailPool1 = new ArrayList<>();
         List<String> mailPool2 = new ArrayList<>();
         List<String> mailPool3 = new ArrayList<>();
         List<String> mailPool4 = new ArrayList<>();
@@ -20,38 +20,21 @@ public class MailSender {
         }
         System.out.println("inputMails = " + inputMails);
 
-        SenderRunnable senderRunnable1 = new SenderRunnable(1, 200);
-        SenderRunnable senderRunnable2 = new SenderRunnable(201, 400);
-        SenderRunnable senderRunnable3 = new SenderRunnable(401, 600);
-        SenderRunnable senderRunnable4 = new SenderRunnable(601, 800);
-        SenderRunnable senderRunnable5 = new SenderRunnable(801, 1000);
-
-        for (int i = senderRunnable1.getStartIndex(); i <= senderRunnable1.getEndIndex() ; i++) {
-            mailPool1.add(inputMails.poll());
-        }
-        for (int i = senderRunnable2.getStartIndex(); i <= senderRunnable2.getEndIndex() ; i++) {
-            mailPool2.add(inputMails.poll());
-        }
-        for (int i = senderRunnable3.getStartIndex(); i <= senderRunnable3.getEndIndex() ; i++) {
-            mailPool3.add(inputMails.poll());
-        }
-        for (int i = senderRunnable4.getStartIndex(); i <= senderRunnable4.getEndIndex() ; i++) {
-            mailPool4.add(inputMails.poll());
-        }
-        for (int i = senderRunnable5.getStartIndex(); i <= senderRunnable5.getEndIndex() ; i++) {
-            mailPool5.add(inputMails.poll());
+        for (int i = 0; i < 201; i++) {
+            mailPool1.add(inputMails.peek());
         }
 
         System.out.println("mailPool1 = " + mailPool1);
-        System.out.println("mailPool2 = " + mailPool2);
-        System.out.println("mailPool3 = " + mailPool3);
-        System.out.println("mailPool4 = " + mailPool4);
-        System.out.println("mailPool5 = " + mailPool5);
-        System.out.println("inputMails = " + inputMails);
 
+//        SenderRunnable senderRunnable1 =
+//                new SenderRunnable(1,200, inputMails);
+
+//        System.out.println("mailPool1 = " + mailPool1);
+//        System.out.println("inputMails = " + inputMails);
+//        System.out.println("senderRunnable1 = " + senderRunnable1);
 //        for (int i = 0; i < 5; i++) {
 //
-//            Thread thread = new Thread(new SenderRunnable(), "Поток " + i);
+//            Thread thread = new Thread(senderRunnable1, "Поток " + i);
 //            thread.start();
 //        }
     }
--- src/main/java/faang/school/godbless/microsoft/SenderRunnable.java
+++ src/main/java/faang/school/godbless/microsoft/SenderRunnable.java
@@ -4,18 +4,35 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 
 import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.Queue;
 
 @Data
-@AllArgsConstructor
+//@AllArgsConstructor
 public class SenderRunnable implements Runnable {
     private int startIndex;
     private int endIndex;
-    public static void  sendMail(ArrayList<String> mailsPool) {
-        mailsPool.forEach(string -> System.out.println("Письмо %s было успешно отправлено!"));
+    private ArrayList<String> stringsPool;
+
+    public SenderRunnable(int startIndex,
+                          int endIndex, Queue<String> stringsQueue) {
+        this.startIndex = startIndex;
+        this.endIndex = endIndex;
+        for (int i = startIndex-1; i < endIndex; i++) {
+            this.stringsPool.add(stringsQueue.peek());
+        }
+//        this.stringsPool = new ArrayList<>()
+    }
+
+
+
+    public static void sendMail(ArrayList<String> mailsPool) {
+        mailsPool.forEach(string -> System.out.printf(
+                "Письмо %s было успешно отправлено!", string));
     }
 
     @Override
     public void run() {
-//SenderRunnable.sendMail(); - stoping
+        SenderRunnable.sendMail(stringsPool);
     }
 }
