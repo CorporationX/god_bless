d4dbbe26b60a0d99511583e68f44295037493185
Nikita2768 <vechkinnikita22@gmail.com>
2024-06-19 21:04:18 +0500

    Блокировка Spotify fix time sleep

--- src/main/java/faang/school/godbless/spotify/Player.java
+++ src/main/java/faang/school/godbless/spotify/Player.java
@@ -1,36 +1,46 @@
 package faang.school.godbless.spotify;
 
+import lombok.SneakyThrows;
+
 public class Player {
     private final static Object lock = new Object();
     private boolean isPlaying = false;
 
+    @SneakyThrows
     public void play() {
         synchronized (lock) {
             isPlaying = true;
+            Thread.sleep(5000);
             System.out.println("play");
             isPlaying = false;
         }
     }
 
+    @SneakyThrows
     public void pause() {
         synchronized (lock) {
             isPlaying = true;
+            Thread.sleep(5000);
             System.out.println("pause");
             isPlaying = false;
         }
     }
 
+    @SneakyThrows
     public void skip() {
         synchronized (lock) {
             isPlaying = true;
+            Thread.sleep(5000);
             System.out.println("skip");
             isPlaying = false;
         }
     }
 
+    @SneakyThrows
     public void previous() {
         synchronized (lock) {
             isPlaying = true;
+            Thread.sleep(5000);
             System.out.println("previous");
             isPlaying = false;
         }
