2899e6bf97384ee88e3e1d6746acff07fb3cbb79
allakamen <akamenskikh@gmail.com>
2024-06-19 14:43:50 +0200

    finished but it was horror

---
+++ src/main/java/faang/school/godbless/instagram/FilterProcessor.java
@@ -0,0 +1,19 @@
+package faang.school.godbless.instagram;
+
+import java.util.function.Function;
+
+public class FilterProcessor {
+    public Image applyFilter(Image image, Function<Image, Image> filters) {
+        try {
+            return filters.apply(image);
+        } catch (Exception e) {
+            throw new RuntimeException("The image is invalid", e);
+        }
+    }
+
+    public Function<Image, Image> combineFilters(Function<Image, Image> filter1, Function<Image, Image> filter2) {
+        return image -> filter1.apply(filter2.apply(image));
+
+    }
+
+}
---
+++ src/main/java/faang/school/godbless/instagram/Image.java
@@ -0,0 +1,7 @@
+package faang.school.godbless.instagram;
+
+import lombok.Getter;
+import lombok.Setter;
+
+
+public record Image(String fileName, String fileDescription){}
