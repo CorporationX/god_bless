1e2321a525dcd385158f2ff5de9b5b10b0378bb1
allakamen <akamenskikh@gmail.com>
2024-06-19 18:11:08 +0200

    finished but I really do not understand lambda

---
+++ src/main/java/faang/school/godbless/google/DictionaryProcessor.java
@@ -0,0 +1,12 @@
+package faang.school.godbless.google;
+
+import java.util.function.BiConsumer;
+
+public class DictionaryProcessor {
+
+    public void processWord(String word, String translation, BiConsumer<String, String> dictionary) {
+        dictionary.accept(word, translation);
+
+
+    }
+}
