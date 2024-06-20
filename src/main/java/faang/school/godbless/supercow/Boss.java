72d5391e0d809c7cefa024a81d2cfd5e0ff4f451
ravshanbekn <ravshanbeknabiyev.dev@gmail.com>
2024-06-20 21:22:09 +0500

    BJS2-12555 - Supercow!
    + made code more readable and elegant

--- src/main/java/faang/school/godbless/supercow/Boss.java
+++ src/main/java/faang/school/godbless/supercow/Boss.java
@@ -2,35 +2,29 @@ package faang.school.godbless.supercow;
 
 public class Boss {
     private final int maxPlayers;
-    private int currentPlayers;
-    private final Object lock = new Object();
+    private int currentPlayers = 0;
 
     public Boss(int maxPlayers) {
         this.maxPlayers = maxPlayers;
-        currentPlayers = 0;
     }
 
-    public void joinBattle(Player player) {
-        synchronized (lock) {
-            while (currentPlayers == maxPlayers) {
-                try {
-                    lock.wait();
-                } catch (InterruptedException e) {
-                    throw new RuntimeException("Thread was interrupted " + e.getMessage());
-                }
+    public synchronized void joinBattle(Player player) {
+        while (currentPlayers == maxPlayers) {
+            try {
+                this.wait();
+            } catch (InterruptedException e) {
+                throw new RuntimeException("Thread was interrupted " + e.getMessage());
             }
-
-            currentPlayers++;
-            System.out.println(player.getName() + " joined the battle");
         }
+
+        currentPlayers++;
+        System.out.println(player.getName() + " joined the battle");
     }
 
-    public void leaveBattle(Player player) {
-        synchronized (lock) {
-            currentPlayers--;
-            System.out.println(player.getName() + " left the battle");
+    public synchronized void leaveBattle(Player player) {
+        currentPlayers--;
+        System.out.println(player.getName() + " left the battle");
 
-            lock.notify();
-        }
+        this.notify();
     }
 }
