ecba329eea43ba78c2d4eca2c5f59df9c5691066
Light <mihail.rataev@gmail.com>
2024-06-20 09:14:29 +0300

    BJS2-11908

--- src/main/java/faang/school/godbless/MailSender.java
+++ src/main/java/faang/school/godbless/MailSender.java
@@ -2,12 +2,19 @@ package faang.school.godbless;
 
 public class MailSender {
     public static void main(String[] args) {
-        for(int i = 0; i < 5; i++) {
+        Thread thread = null;
+        for (int i = 0; i < 5; i++) {
             int startIndex = i * 200 + 1;
-            int endIndex = (i+1) * 200;
+            int endIndex = (i + 1) * 200;
             SenderRunnable sender = new SenderRunnable(startIndex, endIndex);
-            Thread thread = new Thread (sender);
+            thread = new Thread(sender);
             thread.start();
         }
+        try {
+            thread.join();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        System.out.println("Все потоки закончили работу, программа завершена");
     }
 }
