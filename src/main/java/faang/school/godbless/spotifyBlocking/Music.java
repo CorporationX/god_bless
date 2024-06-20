e3e002afbc99f3d0274414287487d423950a3553
Alexandra <s7125@mail.ru>
2024-06-20 12:48:35 +0500

    BJS2-12692 add class

--- src/main/java/faang/school/godbless/spotifyBlocking/Music.java
+++ src/main/java/faang/school/godbless/spotifyBlocking/Music.java
@@ -7,7 +7,7 @@ public class Music {
     public static void main(String[] args) {
         Player player = new Player();
         List<Thread> threads = List.of(new Thread(player::play), new Thread(player::pause),
-                                        new Thread(player::skip), new Thread(player::stop));
+                new Thread(player::skip), new Thread(player::stop));
         threads.forEach(Thread::start);
     }
 }
