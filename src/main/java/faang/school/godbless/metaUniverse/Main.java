c56e98cdd7d88b6615ebeadea7850507a0720391
Meerim <meerim15179@gmail.com>
2024-06-20 23:00:49 +0200

    Added predicate-based filtering to NotificationManager and updated Main to demonstrate usage.

--- src/main/java/faang/school/godbless/metaUniverse/Main.java
+++ src/main/java/faang/school/godbless/metaUniverse/Main.java
@@ -4,20 +4,15 @@ public class Main {
     public static void main(String[] args) {
         NotificationManager notificationManager = new NotificationManager();
 
-        // Регистрация фильтров
-        notificationManager.registerFilter(notification -> {
-            if (notification.message.contains("неприемлемый контент")) {
-                return new Notification(notification.type, notification.message.replace("неприемлемый контент", "***"));
-            }
-            return notification;
-        });
+        notificationManager.registerFilter(
+                notification -> notification.message.contains("неприемлемый контент"),
+                notification -> new Notification(notification.type, notification.message.replace("неприемлемый контент", "***"))
+        );
 
-        // Регистрация обработчиков оповещений
         notificationManager.registerHandler("email", notification -> System.out.println("Отправка по электронной почте: " + notification.message));
         notificationManager.registerHandler("sms", notification -> System.out.println("Отправка SMS: " + notification.message));
         notificationManager.registerHandler("push", notification -> System.out.println("Отправка push-уведомления: " + notification.message));
 
-        // Отправка оповещений
         Notification emailNotification = new Notification("email", "Ваша учетная запись успешно активирована");
         Notification smsNotification = new Notification("sms", "Вы успешно изменили свой пароль");
         Notification pushNotification = new Notification("push", "Новый пост от пользователя: JohnDoe");
