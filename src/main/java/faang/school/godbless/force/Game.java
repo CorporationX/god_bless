98f46974ed9f59bf9a91701d65e2befb87e88cff
Nikita2768 <vechkinnikita22@gmail.com>
2024-06-20 19:33:44 +0500

    Bro Force! final task

---
+++ src/main/java/faang/school/godbless/force/Game.java
@@ -0,0 +1,33 @@
+package faang.school.godbless.force;
+
+import lombok.AllArgsConstructor;
+
+@AllArgsConstructor
+public class Game {
+    public final static Object lock = new Object();
+    private int scope = 0;
+    private int lives;
+
+    public Game(int lives) {
+        if (lives == 0) {
+            throw new IllegalArgumentException("The game cannot start with zero lives!");
+        }
+        this.lives = lives;
+    }
+
+    public void update() {
+        scope++;
+        lives--;
+        synchronized (lock) {
+            if (lives == 0) {
+                gameOver();
+            }
+        }
+    }
+
+
+    private void gameOver() {
+        System.out.println("Game over, you have scored: " + scope);
+        throw new IllegalArgumentException("Game over");
+    }
+}
---
+++ src/main/java/faang/school/godbless/force/Main.java
@@ -0,0 +1,10 @@
+package faang.school.godbless.force;
+
+public class Main {
+    public static void main(String[] args) {
+        Game game = new Game(1000);
+        while(true) {
+            game.update();
+        }
+    }
+}
