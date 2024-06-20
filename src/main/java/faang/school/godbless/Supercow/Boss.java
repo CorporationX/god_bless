0ba17371b845423b4cbd9508d1c50cf0ae1053af
cirill98 <100434317+cirill98@users.noreply.github.com>
2024-06-20 16:04:28 +0500

    Сделал задание с помощью пула потоков, и синхронизировал блок.

--- src/main/java/faang/school/godbless/Supercow/Boss.java
+++ src/main/java/faang/school/godbless/Supercow/Boss.java
@@ -1,10 +1,32 @@
 package faang.school.godbless.Supercow;
 
-public class Boss {
-    private int maxPlayer;
-    private int currentPlayer;
+import java.util.*;
 
+public class Boss {
+    private final int maxPlayer;
+    private List<Player> currentPlayer;
     private Object lock = new Object();
 
+    public Boss(int maxPlayer) {
+        this.maxPlayer = maxPlayer;
+        this.currentPlayer = new ArrayList<>();
+    }
 
+    public void joinBattle(Player player) throws InterruptedException {
+        synchronized (lock) {
+            if (currentPlayer.size() >= maxPlayer) {
+                lock.wait();
+            }
+            currentPlayer.add(player);
+            System.out.println("Добавлен " + player);
+        }
+    }
+
+    public void endPlaying(Player player) {
+        synchronized (lock) {
+            currentPlayer.remove(player);
+            System.out.println("Удален " + player);
+            lock.notify();
+        }
+    }
 }
