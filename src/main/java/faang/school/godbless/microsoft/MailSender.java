3caf4af391642f0fdeb542c3ee82840bfc4ee4f0
Nikita2768 <vechkinnikita22@gmail.com>
2024-06-20 19:13:57 +0500

    Вы работаете в Microsoft! fix send emails

--- src/main/java/faang/school/godbless/microsoft/MailSender.java
+++ src/main/java/faang/school/godbless/microsoft/MailSender.java
@@ -7,16 +7,22 @@ import java.util.concurrent.Executors;
 
 public class MailSender {
     private static final int QUANTITY_THREAD = 5;
-    private static int quantityAllEmail = 100;
+    private static int quantityAllEmail = 1000;
 
     public static void main(String[] args) {
         ExecutorService executor = Executors.newFixedThreadPool(QUANTITY_THREAD);
+        List<String> allMail = new ArrayList<>();
+        for(int i = 0; i < quantityAllEmail; i++) {
+            allMail.add("some message!");
+        }
+        int numberLettersPerThread = quantityAllEmail / QUANTITY_THREAD;
         for (int i = 1; i <= QUANTITY_THREAD; i++) {
-            List<String> partListForPool = new ArrayList<>();
+            int startIndex = (i - 1) * numberLettersPerThread;
+            int endIndex = i * numberLettersPerThread - 1;
+            List<String> partListForPool = allMail.subList(startIndex, endIndex);
             for (int j = 0; j < quantityAllEmail / QUANTITY_THREAD; j++) {
-                partListForPool.add("message email: " + (i * (j + 1)));
+                executor.submit(new SenderRunnable(partListForPool));
             }
-            executor.submit(new SenderRunnable(partListForPool));
         }
         executor.shutdown();
     }
