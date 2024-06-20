acdf4b8db30f2f887dc007b247d18780c9822936
0Miha0 <140548440+0Miha0@users.noreply.github.com>
2024-06-20 13:59:15 +0500

    https://faang-school.atlassian.net/browse/BJS2-9869

---
+++ src/main/java/faang/school/godbless/Sprint_3/BJS2_9869/Image.java
@@ -0,0 +1,11 @@
+package faang.school.godbless.Sprint_3.BJS2_9869;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class Image {
+    private String name;
+    private String description;
+}
