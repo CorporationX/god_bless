b756aa05dcf28bdedaaeb8b0da0cfdfbfd8e6d19
DonMaxim <donmaxim1990@gmail.com>
2024-06-21 20:53:26 -0300

    Resolve task BJS2-10900 Implement methods using Stream Api

--- src/main/java/faang/school/godbless/BJS210900/StreamMethods.java
+++ src/main/java/faang/school/godbless/BJS210900/StreamMethods.java
@@ -6,12 +6,19 @@ import java.util.HashMap;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
+import java.util.Objects;
 import java.util.Set;
 import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 public class StreamMethods {
     public static Set<List<Integer>> getPairsWithSumOf(List<Integer> nums, int sum) {
+        if (nums == null) {
+            throw new IllegalArgumentException("List nums can't be null!");
+        }
+
         Map<Integer, Long> duplicates = nums.stream()
+                .filter(Objects::nonNull)
                 .collect(Collectors.groupingBy(n -> n, Collectors.counting()));
 
         return duplicates.keySet().stream()
@@ -31,16 +38,16 @@ public class StreamMethods {
     }
 
     public static List<String> sortCapitals(Map<String, String> countriesWithCapitals) {
-        List<String> capitals = new ArrayList<>(countriesWithCapitals.keySet().stream().toList());
-        capitals.sort(null);
-        return capitals.stream()
+        return countriesWithCapitals.keySet().stream()
+                .filter(Objects::nonNull)
+                .sorted()
                 .map(string -> countriesWithCapitals.get(string))
                 .toList();
     }
 
     public static List<String> sortFilteredStrings(List<String> strings, char filter) {
         return strings.stream()
-                .filter(string -> string.startsWith("" + filter))
+                .filter(string -> string != null && string.startsWith("" + filter))
                 .sorted(Comparator.comparing(String::length))
                 .toList();
     }
@@ -48,28 +55,71 @@ public class StreamMethods {
     public static Set<Set<String>> findStrangersWithCommonFriends(Map<String, List<String>> friends) {
         Map<String, List<String>> friendsCopy = new HashMap<>(friends);
         friends.values().stream()
-                        .flatMap(List::stream)
-                                .forEach(name -> friendsCopy.computeIfAbsent(name,
-                                        k -> friends.keySet().stream()
-                                                .filter(friend -> friends.get(friend).contains(name))
-                                                .toList()
-                                        )
-                                );
+                .filter(Objects::nonNull)
+                .flatMap(List::stream)
+                .forEach(name -> friendsCopy.computeIfAbsent(name,
+                                k -> friends.keySet().stream()
+                                        .filter(friend -> friends.get(friend).contains(name))
+                                        .toList()
+                        )
+                );
 
         Set<Set<String>> result = new HashSet<>();
 
-        friendsCopy.keySet().forEach(commonFriend -> {
-            for (int i = 0; i < friendsCopy.get(commonFriend).size(); i++) {
-                String nameForSearch = friendsCopy.get(commonFriend).get(i);
-                for (int j = i + 1; j < friendsCopy.get(commonFriend).size(); j++) {
-                    String possibleFriend = friendsCopy.get(commonFriend).get(j);
-                    if (!friendsCopy.get(nameForSearch).contains(possibleFriend)) {
-                        result.add(Set.of(nameForSearch, possibleFriend));
+        friendsCopy.keySet().stream()
+                .filter(Objects::nonNull)
+                .forEach(commonFriend -> {
+                    for (int i = 0; i < friendsCopy.get(commonFriend).size(); i++) {
+                    String nameForSearch = friendsCopy.get(commonFriend).get(i);
+                    for (int j = i + 1; j < friendsCopy.get(commonFriend).size(); j++) {
+                        String possibleFriend = friendsCopy.get(commonFriend).get(j);
+                        if (!friendsCopy.get(nameForSearch).contains(possibleFriend)) {
+                            result.add(Set.of(nameForSearch, possibleFriend));
+                        }
                     }
                 }
-            }
         });
 
         return result;
+    }
+
+    public static Map<String, Double> getDepartmentAverageSalary(List<Employee> employees) {
+        return employees.stream()
+                .filter(Objects::nonNull)
+                .collect(Collectors.groupingBy(
+                        Employee::getDepartment,
+                        Collectors.averagingDouble(Employee::getSalary)
+                ));
+    }
+
+    public static List<String> filterAndSortStringsContainsAlphabet(List<String> strings, char[] alphabet) {
+        Set<Character> alphabetSet = new String(alphabet).chars()
+                .filter(Objects::nonNull)
+                .mapToObj(c -> (char) c)
+                .collect(Collectors.toSet());
+
+        return strings.stream()
+                .filter(s -> s != null && s.chars()
+                        .mapToObj(c -> (char) c)
+                        .allMatch(alphabetSet::contains))
+                .sorted(Comparator.comparing(String::length))
+                .toList();
+    }
+
+    public static List<String> toBinaryString(List<Integer> nums) {
+        return nums.stream()
+                .filter(Objects::nonNull)
+                .map(Integer::toBinaryString)
+                .toList();
+    }
+
+    public static List<Integer> findPalindromes(int from, int to) {
+        if (from < 10 || from >= to) {
+            throw new IllegalArgumentException("Please write correct numbers (>= 10 and from < to)");
+        }
+        return IntStream.range(from, to)
+                .filter(s -> String.valueOf(s).equals(new StringBuilder(String.valueOf(s)).reverse().toString()))
+                .boxed()
+                .toList();
     }
 }
--- src/test/java/faang/school/godbless/BJS210900/StreamMethodsTest.java
+++ src/test/java/faang/school/godbless/BJS210900/StreamMethodsTest.java
@@ -2,22 +2,28 @@ package faang.school.godbless.BJS210900;
 
 import org.junit.jupiter.api.Test;
 
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 public class StreamMethodsTest {
     @Test
     void testGetPairsWithSumOf() {
-        List<Integer> nums = List.of(5, 5, 5, 6, 6, 6, 4, 4, 4, 1, 9, 8, 2, 3, 8, 2, 20, -10);
+        List<Integer> nums = Arrays.asList(5, 5, 5, 6, 6, 6, 4, 4, 4, 1, 9, 8, 2, 3, 8, 2, 20, -10, null);
         Set<List<Integer>> expected = Set.of(
                 List.of(5, 5), List.of(4, 6), List.of(1, 9), List.of(2, 8), List.of(-10, 20)
         );
 
         assertEquals(expected, StreamMethods.getPairsWithSumOf(nums, 10));
+
+        IllegalArgumentException ex = assertThrows(IllegalArgumentException.class,
+                () -> StreamMethods.getPairsWithSumOf(null, 5));
+        assertEquals("List nums can't be null!", ex.getMessage());
     }
 
     @Test
@@ -51,4 +57,63 @@ public class StreamMethodsTest {
 
         assertEquals(expected, actual);
     }
+
+    @Test
+    void testGetDepartmentAverageSalary() {
+        List<Employee> employees = List.of(
+                new Employee("Maksim", 5000, "Developer"),
+                new Employee("Gena", 4000, "Developer"),
+                new Employee("Neo", 1500, "Marketing"),
+                new Employee("Morpheus", 1600, "Marketing"),
+                new Employee("Walter", 100_000, "Chemistry"),
+                new Employee("Jessy", 10_000, "Chemistry")
+        );
+        Map<String, Double> actual = StreamMethods.getDepartmentAverageSalary(employees);
+
+        Map<String, Double> expected = new HashMap<>();
+        expected.put("Developer", 4500.);
+        expected.put("Marketing", 1550.);
+        expected.put("Chemistry", 55_000.);
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void testFilterAndSortStringsContainsAlphabet() {
+        char[] alphabet = {'a', 'b', 'c', 'd', 'r'};
+        List<String> strings = List.of(
+                "cadabra", "abra", "bar", "public"
+        );
+
+        List<String> actual = StreamMethods.filterAndSortStringsContainsAlphabet(strings, alphabet);
+        List<String> expected = List.of(
+                "bar", "abra", "cadabra"
+        );
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void testToBinaryString() {
+        List<Integer> nums = List.of(1, 2, 3, 4);
+        List<String> actual = StreamMethods.toBinaryString(nums);
+        List<String> expected = List.of("1", "10", "11", "100");
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void testFindPalindromes() {
+        int from = 10;
+        int to = 50;
+        List<Integer> actual = StreamMethods.findPalindromes(from, to);
+        List<Integer> expected = List.of(11, 22, 33, 44);
+
+        assertEquals(expected, actual);
+
+        IllegalArgumentException ex = assertThrows(IllegalArgumentException.class,
+                () -> StreamMethods.findPalindromes(-5, 5));
+        assertEquals("Please write correct numbers (>= 10 and from < to)", ex.getMessage());
+    }
+
 }
