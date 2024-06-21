032b7905e4ea11c28ea38060ade9c354378ea278
Alen <Alendos87@gmail.com>
2024-06-21 10:33:58 +0500

    Google photos

---
+++ src/main/java/faang/school/godbless/GooglePhoto/GooglePhotosAutoUploader.java
@@ -0,0 +1,20 @@
+package faang.school.godbless.GooglePhoto;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class GooglePhotosAutoUploader {
+    Object lock = new Object();
+    List<String> photosToUpload = Arrays.asList(
+            "/path/to/photo1.jpg",
+            "/path/to/photo2.jpg",
+            "/path/to/photo3.jpg"
+    );
+
+    public static void startAutoUpload(){
+
+    }
+
+
+
+}
