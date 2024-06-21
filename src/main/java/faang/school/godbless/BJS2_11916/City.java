0484601d2d4e102f63a05e0ebec9c9016793f626
Mikhail Fokin <fokinms@gmail.com>
2024-06-22 00:45:04 +0400

    BJS2-11916 Путь пальцем проложи…

---
+++ src/main/java/faang/school/godbless/BJS2_11916/City.java
@@ -0,0 +1,14 @@
+package faang.school.godbless.BJS2_11916;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.ToString;
+
+@AllArgsConstructor
+@Getter
+@ToString
+public class City {
+    private String name;
+    private Location location;
+    private int distance;
+}
