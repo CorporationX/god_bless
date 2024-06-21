84975f02e6a1bef62c2249c963e867eb8a3ecda4
Danill Klinkov <klinkov.danya@bk.ru>
2024-06-21 09:17:45 +0300

    thread4: class to record

--- src/main/java/faang/school/godbless/thread4/Task.java
+++ src/main/java/faang/school/godbless/thread4/Task.java
@@ -1,22 +1,19 @@
 package faang.school.godbless.thread4;
 
-import lombok.AllArgsConstructor;
-
-@AllArgsConstructor
-public class Task implements Runnable {
+public record Task(String name, String task) implements Runnable {
     private static final String ERROR = "Задача прервалась: ";
     private static final long TASK_COMPLETION_TIME = 1000L;
-    private String name;
-    private String task;
+    private static final String MESSAGE_TASK_STARTED = "%s: Начал выполнять задачу - %s \n";
+    private static final String MESSAGE_TASK_DONE = "%s: Задача %s выполнена \n";
 
     @Override
     public void run() {
-        System.out.format("%s: Начал выполнять задачу - %s \n", name, task);
+        System.out.format(MESSAGE_TASK_STARTED, name, task);
         try {
             Thread.sleep(TASK_COMPLETION_TIME);
         } catch (InterruptedException e) {
             throw new RuntimeException(ERROR + e.getMessage());
         }
-        System.out.format("%s: Задача %s выполнена \n", name, task);
+        System.out.format(MESSAGE_TASK_DONE, name, task);
     }
 }
