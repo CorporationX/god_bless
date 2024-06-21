cc3c4cc7f57475320fec5b6d05852c42530a25bc
lu_av <loveavduhova@gmail.com>
2024-06-21 19:18:21 +0700

    BJS2-12351 - задача выполнена

---
+++ src/main/java/faang/school/godbless/BJS2_12351/Location.java
@@ -0,0 +1,11 @@
+package faang.school.godbless.BJS2_12351;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@AllArgsConstructor
+@Getter
+public class Location {
+    private int x;
+    private int y;
+}
