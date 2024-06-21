e89e7bad9ed3a06ccc8111f6da9511276f75d7d1
cirill98 <100434317+cirill98@users.noreply.github.com>
2024-06-21 13:53:40 +0500

    Реализовал логику с wait() notify()

--- src/main/java/faang/school/godbless/CountYouTube/VideoManager.java
+++ src/main/java/faang/school/godbless/CountYouTube/VideoManager.java
@@ -4,25 +4,19 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class VideoManager {
-    private Object lock = new Object();
-
     private Map<Video, Integer> viewsMap = new HashMap<>();
 
     public synchronized void addView(Video videoId) throws InterruptedException {
-//        synchronized (viewsMap) {
-            if (viewsMap.containsKey(videoId)) {
-                viewsMap.put(videoId, viewsMap.get(videoId) + 1);
-            } else {
-                viewsMap.put(videoId, 1);
-            }
-            viewsMap.wait();
-//        }
+        if (viewsMap.containsKey(videoId)) {
+            viewsMap.put(videoId, viewsMap.get(videoId) + 1);
+        } else {
+            viewsMap.put(videoId, 1);
+        }
+        viewsMap.wait();
     }
 
     public synchronized void getViewCount(Video videoId) {
-//        synchronized (viewsMap) {
-            System.out.println(videoId + " " +  viewsMap.get(videoId));
-            viewsMap.notify();
-//        }
+        System.out.println(videoId + " " + viewsMap.get(videoId));
+        viewsMap.notify();
     }
 }
