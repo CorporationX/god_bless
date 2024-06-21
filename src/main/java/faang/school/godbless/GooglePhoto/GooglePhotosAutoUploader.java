87d79a16f0eea950a8007107873695d0931ab5a8
Alen <Alendos87@gmail.com>
2024-06-21 22:21:15 +0500

    Google Photos

--- src/main/java/faang/school/godbless/GooglePhoto/GooglePhotosAutoUploader.java
+++ src/main/java/faang/school/godbless/GooglePhoto/GooglePhotosAutoUploader.java
@@ -1,20 +1,38 @@
 package faang.school.godbless.GooglePhoto;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
-public class GooglePhotosAutoUploader {
+public class GooglePhotosAutoUploader{
     Object lock = new Object();
-    List<String> photosToUpload = Arrays.asList(
+    static List<String> photosToUpload = new ArrayList<>(List.of(
             "/path/to/photo1.jpg",
             "/path/to/photo2.jpg",
             "/path/to/photo3.jpg"
-    );
+    ));
 
-    public static void startAutoUpload(){
-
+    public void startAutoUpload() throws InterruptedException {
+        synchronized (lock) {
+            if (photosToUpload.isEmpty()) {
+                lock.wait();
+            }
+            uploadPhotos();
+            photosToUpload.clear();
+        }
     }
 
+    private static void uploadPhotos() {
+        System.out.println("Uploading photo");
+        for (String photo : photosToUpload) {
+            System.out.println(photo);
+        }
+    }
 
-
+    public void onNewPhotoAdded(String photoPath){
+        synchronized (lock){
+            photosToUpload.add(photoPath);
+            lock.notify();
+        }
+    }
 }
