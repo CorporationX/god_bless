b04b5d6eb3f6babc6270ad46c76503834e8f7ce5
Danill Klinkov <klinkov.danya@bk.ru>
2024-06-21 09:20:59 +0300

    thread2: added constants

--- src/main/java/faang/school/godbless/thread2/FoodDeliveryTask.java
+++ src/main/java/faang/school/godbless/thread2/FoodDeliveryTask.java
@@ -3,16 +3,20 @@ package faang.school.godbless.thread2;
 import java.util.Random;
 
 public record FoodDeliveryTask(String character, int foodAmount) implements Runnable {
+
+    private static final String MESSAGE_GET_FOOD = "%s получает %d %s \n";
+    private static final String MESSAGE_EAT_FOOD = "%s ест %d %s \n";
+
     @Override
     public void run() {
         String foodType = getFoodType();
-        System.out.format("%s получает %d %s \n", character, foodAmount, foodType);
+        System.out.format(MESSAGE_GET_FOOD, character, foodAmount, foodType);
         try {
             Thread.sleep(foodAmount * 1000L);
         } catch (InterruptedException e) {
             throw new RuntimeException(e);
         }
-        System.out.format("%s ест %d %s \n", character, foodAmount, foodType);
+        System.out.format(MESSAGE_EAT_FOOD, character, foodAmount, foodType);
     }
 
     private String getFoodType() {
