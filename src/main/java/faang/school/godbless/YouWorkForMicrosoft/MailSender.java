c49d847e8c78b2cd349ee0ffa2c34173cc8136d7
Nikita Volzhin <vlzh.nik@mail.ru>
2024-06-21 17:38:33 +0500

    BJS2-11872 - Changed the main method

--- src/main/java/faang/school/godbless/YouWorkForMicrosoft/MailSender.java
+++ src/main/java/faang/school/godbless/YouWorkForMicrosoft/MailSender.java
@@ -4,20 +4,17 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class MailSender {
-    private static final List<Integer> Batch = new ArrayList<>(200);
     private static final int SIZE_BATCH = 200;
     private static final List<Thread> THREADS = new ArrayList<>();
 
     public static void main(String[] args) {
-        for (int i = 0; i < 1000; i++) {
-            if (Batch.size() == 200) {
-                Thread thread = new Thread(new SenderRunnable(Batch.get(0), Batch.get(SIZE_BATCH - 1)));
-                thread.start();
-                Batch.clear();
-                THREADS.add(thread);
-            } else {
-                Batch.add(i);
-            }
+        for (int i = 1; i <= 5; i++) {
+            int startIndex = i * SIZE_BATCH;
+            int endIndex = startIndex + SIZE_BATCH;
+
+            Thread thread = new Thread(new SenderRunnable(startIndex, endIndex));
+            thread.start();
+            THREADS.add(thread);
         }
 
         for (Thread thread : THREADS) {
