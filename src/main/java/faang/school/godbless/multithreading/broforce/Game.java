f7a51d2c759323da6f42267853e855f4b03c8634
npopov_ <lkn_popov@mail.ru>
2024-06-22 18:48:28 +0300

    BJS2-13102 поправлено решение задачи

--- src/main/java/faang/school/godbless/multithreading/broforce/Game.java
+++ src/main/java/faang/school/godbless/multithreading/broforce/Game.java
@@ -1,37 +1,53 @@
 package faang.school.godbless.multithreading.broforce;
 
+import java.util.List;
 import java.util.Random;
 
 public class Game {
+    private final List<Player> players;
     private int score = 0;
     private int lives = 0;
     private final Object scoreLock = new Object();
     private final Object livesLock = new Object();
+    private volatile boolean isDeath = false;
 
-    public void update(Player player) {
+    public Game(List<Player> players) {
+        this.players = players;
+    }
+
+    public void update() {
         Random random = new Random();
+        int index = random.nextInt(players.size());
+        Player player = players.get(0);
         while (true) {
             boolean lossLive = getLossLive();
             if (lossLive) {
                 synchronized (livesLock) {
-                    player.decreaseLive();
-                    lives++;
+                    if (!isDeath) {
+                        player.decreaseLive();
+                        lives++;
 
-                    if (!player.isAlive()) {
-                        gameOver();
+                        if (!player.isAlive()) {
+                            gameOver(player);
+                            isDeath = true;
+                            return;
+                        }
+                    } else {
                         return;
                     }
                 }
             } else {
                 synchronized (scoreLock) {
-                    if (player.isAlive()) {
-                        player.increaseScore();
-                        score++;
+                    synchronized (livesLock) {
+                        if (!isDeath) {
+                            player.increaseScore();
+                            score++;
+                        } else {
+                            return;
+                        }
                     }
-
                 }
             }
-
         }
     }
 
@@ -39,7 +55,8 @@ public class Game {
         return new Random().nextInt(10) < 5;
     }
 
-    public void gameOver() {
+    public void gameOver(Player player) {
+        System.out.println("Игра завершилась на игроке: " + player.getName());
         System.out.println("Игра окончена с max score: " + score);
         System.out.println("Игра окончена с max lives: " + lives);
     }
