9a03b15f002c929310f42364db98b39597e03c77
ravshanbekn <ravshanbeknabiyev.dev@gmail.com>
2024-06-22 18:57:58 +0500

    BJS2-13000 - Matters of the heart
    + realized required functions

---
+++ src/main/java/faang/school/godbless/tinder/Chat.java
@@ -0,0 +1,43 @@
+package faang.school.godbless.tinder;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.concurrent.ThreadLocalRandom;
+
+@Getter
+@Setter
+public class Chat {
+    private ChatManager chatManager;
+    private User firstUser;
+    private User secondUser;
+
+    public Chat(ChatManager chatManager, User firstUser) {
+        this.chatManager = chatManager;
+        this.firstUser = firstUser;
+    }
+
+    public void startConversation() {
+        System.out.println(firstUser.getName() + " and " + secondUser.getName() + " are chatting.");
+    }
+
+    public void deliverMessage(){
+        int whoSends = ThreadLocalRandom.current().nextInt(2);
+        if(whoSends == 0){
+            System.out.println(firstUser.getName() + " sends message to " + secondUser.getName());
+        } else {
+            System.out.println(secondUser.getName() + " sends message to " + firstUser.getName());
+        }
+    }
+
+    public void isItTimeToCloseConversation(){
+        boolean isConversationSuccessful = ThreadLocalRandom.current().nextBoolean();
+
+        if(!isConversationSuccessful){
+            System.out.println("It is time to close conversation");
+            chatManager.endChat();
+        } else {
+            System.out.println("It is too early to close conversation");
+        }
+    }
+}
