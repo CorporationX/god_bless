8781a50e7fbfbf00fee682426318f30a6939aee9
l0zain <nanoowood@gmail.com>
2024-06-22 23:43:00 +0300

    https://faang-school.com/courses/e63dkedm/bvd6ry2d

---
+++ src/main/java/BJS2_11823/House.java
@@ -0,0 +1,16 @@
+package BJS2_11823;
+
+import java.util.List;
+
+public class House {
+    private List<Room> rooms;
+    private List<Food> collectedFood;
+
+    public static void collectFood() {
+
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
