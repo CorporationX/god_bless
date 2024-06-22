bcf4b9717ab1535eb4059bd376f470090fe04f55
Alen <Alendos87@gmail.com>
2024-06-22 13:52:37 +0500

    Cow

---
+++ src/main/java/faang/school/godbless/SuperCow/Player.java
@@ -0,0 +1,21 @@
+package faang.school.godbless.SuperCow;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@AllArgsConstructor
+@Data
+public class Player {
+    private String name;
+
+    public void startBattle(Boss boss){
+        try{
+            boss.joinBattle(this);
+            Thread.sleep(10000);
+        }
+        catch(InterruptedException e){
+            throw new RuntimeException(e);
+        }
+        boss.leaveBattle(this);
+    }
+}
