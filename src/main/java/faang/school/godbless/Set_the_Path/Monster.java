5801fed8a5dcbd3152d8ffdaf15de6e4b197f27d
Andrei Kuzmin <Andreytko@mail.ru>
2024-06-22 17:12:07 +0300

    добавил класс

---
+++ src/main/java/faang/school/godbless/Set_the_Path/Monster.java
@@ -0,0 +1,18 @@
+package faang.school.godbless.Set_the_Path;
+
+import lombok.Getter;
+
+@Getter
+public class Monster {
+
+    private String name;
+    private String location;
+
+    public Monster(String name, String location) {
+        if (name.isBlank() || location.isBlank()) throw new IllegalArgumentException("invalid arg(s)");
+        else {
+            this.name = name;
+            this.location = location;
+        }
+    }
+}
