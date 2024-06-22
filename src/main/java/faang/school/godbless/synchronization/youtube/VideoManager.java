6d00237811cf7161b5705a6ab57179422372e634
Kirill Maltsev <opp.2211@gmail.com>
2024-06-22 19:22:58 +0500

    refactor: move main into new class

--- src/main/java/faang/school/godbless/synchronization/youtube/VideoManager.java
+++ src/main/java/faang/school/godbless/synchronization/youtube/VideoManager.java
@@ -1,40 +1,11 @@
 package faang.school.godbless.synchronization.youtube;
 
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
-import java.util.concurrent.TimeUnit;
-import java.util.stream.IntStream;
 
 public class VideoManager {
 
-    private static final int NUM_THREADS = 100;
-    private static final int NUM_VIDEOS = 20;
-
     private final Map<String, Integer> viewsMap = new HashMap<>();
-
-    public static void main(String[] args) throws InterruptedException {
-        VideoManager videoManager = new VideoManager();
-
-        ExecutorService executorService = Executors.newFixedThreadPool(NUM_THREADS);
-
-        List<String> videoIds = IntStream.rangeClosed(1, NUM_VIDEOS)
-                .boxed()
-                .map(i -> "videouid" + i)
-                .toList();
-
-        videoIds.forEach(videoId ->
-                IntStream.range(0, NUM_THREADS / NUM_VIDEOS).forEach(i ->
-                        executorService.execute(() -> {
-                            videoManager.addView(videoId);
-                            System.out.println(videoId + ": " + videoManager.getViewCount(videoId));
-                        })));
-
-        executorService.shutdown();
-        executorService.awaitTermination(120, TimeUnit.SECONDS);
-    }
 
     public void addView(String videoId) {
         synchronized (viewsMap) {
