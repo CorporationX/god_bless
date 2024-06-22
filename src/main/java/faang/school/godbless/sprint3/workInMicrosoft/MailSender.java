a3cd75b56929f4da4201c93187e87985b216d2b8
Grisha Tkach <grisatkac@mail.ru>
2024-06-22 23:14:48 +0300

    add work in microsoft home task

---
+++ src/main/java/faang/school/godbless/sprint3/workInMicrosoft/MailSender.java
@@ -0,0 +1,34 @@
+package faang.school.godbless.sprint3.workInMicrosoft;
+
+
+public class MailSender {
+
+    public static void main(String[] args) {
+        try {
+            SenderRunnable senderRunnable = new SenderRunnable(0, 199);
+            Thread thread1 = new Thread(senderRunnable);
+            thread1.start();
+
+            SenderRunnable senderRunnable2 = new SenderRunnable(200, 399);
+            Thread thread2 = new Thread(senderRunnable2);
+            thread2.start();
+
+            SenderRunnable senderRunnable3 = new SenderRunnable(400, 599);
+            Thread thread3 = new Thread(senderRunnable3);
+            thread3.start();
+
+            SenderRunnable senderRunnable4 = new SenderRunnable(600, 799);
+            Thread thread4 = new Thread(senderRunnable4);
+            thread4.start();
+
+            SenderRunnable senderRunnable5 = new SenderRunnable(800, 999);
+            Thread thread5 = new Thread(senderRunnable5);
+            thread5.start();
+
+
+            thread1.join();
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
