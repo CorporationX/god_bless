2ff3c1e271517b9010c826688aa0e5e54d58bea6
OlgaK888 <olgakurilovi4@gmail.com>
2024-06-23 00:33:14 +0300

    Worked with synchronized

--- src/main/java/faang/school/godbless/spotify_blocking/Music.java
+++ src/main/java/faang/school/godbless/spotify_blocking/Music.java
@@ -1,12 +1,43 @@
 package faang.school.godbless.spotify_blocking;
 
+import java.util.List;
+
 public class Music {
     public static void main(String[] args) {
-        Player player1 = new Player();
-        Thread playThread = new Thread(player1::play);
-        Thread pauseThread = new Thread(player1::pause);
-        Thread skipThread = new Thread(player1::skip);
-        Thread previousThread = new Thread(player1::previous);
+        Player player = new Player();
+        List<String> playList = List.of("track 1", "track 2", "track 3", "track 4", "track 5");
+
+        Thread playThread = new Thread(() -> {
+            try {
+                player.play(playList.get(1));
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+        });
+
+        Thread pauseThread = new Thread(() -> {
+            try {
+                player.pause(playList.get(0));
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+        });
+
+        Thread skipThread = new Thread(() -> {
+            try {
+                player.skip(playList.get(3));
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+        });
+
+        Thread previousThread = new Thread(() -> {
+            try {
+                player.previous(playList.get(4));
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+        });
 
         playThread.start();
         pauseThread.start();
--- src/main/java/faang/school/godbless/spotify_blocking/Player.java
+++ src/main/java/faang/school/godbless/spotify_blocking/Player.java
@@ -1,23 +1,46 @@
 package faang.school.godbless.spotify_blocking;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Player {
+    private final Object lock = new Object();
     private boolean isPlaying;
+    List<String> tracks = new ArrayList<>();
 
-    public void play() {
-        System.out.println("Music plays");
+    public void play(String trackName) throws InterruptedException {
+        synchronized (lock) {
+            if (!isPlaying) {
+                isPlaying = true;
+            }
+            System.out.println("Music " + trackName + " plays");
+        }
     }
 
-    public void pause() {
-        System.out.println("Music paused");
+    public void pause(String trackName) throws InterruptedException {
+        synchronized (lock) {
+            if (isPlaying) {
+                isPlaying = false;
+            }
+            System.out.println("Music " + trackName + " paused");
+        }
     }
 
-    public void skip() {
-        System.out.println("Music skipped");
+    public void skip(String trackName) throws InterruptedException {
+        synchronized (lock) {
+            if (isPlaying) {
+                isPlaying = false;
+            }
+            System.out.println("Music " + trackName + " skipped");
+        }
     }
 
-    public void previous() {
-        System.out.println("Previous music plays");
+    public void previous(String trackName) throws InterruptedException {
+        synchronized (lock) {
+            if (isPlaying) {
+                isPlaying = false;
+            }
+            System.out.println("Previous music " + trackName + " plays");
+        }
     }
 }
