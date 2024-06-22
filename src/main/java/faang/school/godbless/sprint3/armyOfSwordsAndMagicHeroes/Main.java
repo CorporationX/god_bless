d2d7747c752c4e85f57182948c09e1f8b57dffb1
Grisha Tkach <grisatkac@mail.ru>
2024-06-23 01:04:34 +0300

    delete comments

--- src/main/java/faang/school/godbless/sprint3/armyOfSwordsAndMagicHeroes/Main.java
+++ src/main/java/faang/school/godbless/sprint3/armyOfSwordsAndMagicHeroes/Main.java
@@ -8,7 +8,6 @@ public class Main {
     public static void main(String[] args) {
         List<ArmyItem> armyItems = Arrays.asList(new Archer(25), new Swordsman(40), new Mage(50), new Mage(20));
         Army army = new Army(armyItems);
-        // считаем в потоке 4
 
         int totalPower = army.calculateTotalPower();
         System.out.println("Total army power: " + totalPower);
