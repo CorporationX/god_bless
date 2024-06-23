46836a173454ed6ee1d123cd4558297fdd53bff1
Grisha Tkach <grisatkac@mail.ru>
2024-06-24 00:08:27 +0300

    add mice are very nice home task

---
+++ src/main/java/faang/school/godbless/sprint3/miceAreVeryNice/Food.java
@@ -0,0 +1,15 @@
+package faang.school.godbless.sprint3.miceAreVeryNice;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@ToString
+public class Food {
+
+    private String name;
+}
