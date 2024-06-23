db52e2793a82a3befb434c70a154ad4c20416f19
allakamen <akamenskikh@gmail.com>
2024-06-23 10:55:31 +0200

    changed the method in Function, decided for the method "andThen"

---
+++ src/main/java/faang/school/godbless/stream_api2/Main.java
@@ -0,0 +1,27 @@
+package faang.school.godbless.stream_api2;
+
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+public class Main {
+    public static void main(String[] args) {
+
+        //Task1
+        List<Integer> inputNumbers = Arrays.asList(1, 9, 3, 6, 4, 5);
+        int targetSum = 10;
+
+
+    }
+
+    //Task1
+
+    public static List<List<Integer>> findPairs(List<Integer> inputNumbers, int targetSum) {
+
+        Set<Integer> seenNumbers = new HashSet<>();
+        Set<List<Integer>> pairs = new HashSet<>();
+
+
+    }
+}
