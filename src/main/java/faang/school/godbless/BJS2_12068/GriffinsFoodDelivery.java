842e3dc6dd770867cc903f2a3184c21434fb8ade
Max Shundrick <mas9l175@gmail.com>
2024-06-23 23:10:35 +0300

    get the random number out of the main method BJS2-12068

--- src/main/java/faang/school/godbless/BJS2_12068/GriffinsFoodDelivery.java
+++ src/main/java/faang/school/godbless/BJS2_12068/GriffinsFoodDelivery.java
@@ -6,11 +6,13 @@ import java.util.concurrent.Executors;
 import java.util.concurrent.TimeUnit;
 
 public class GriffinsFoodDelivery {
+    private static int random = new Random().nextInt(50);
+
     public static void main(String[] args) {
         ExecutorService service = Executors.newFixedThreadPool(3);
         String[] characterNames = {"Peter", "Lois", "Meg", "Chris", "Stewie"};
         for (String name : characterNames) {
-            service.submit(new FoodDeliveryTask(name, new Random().nextInt(50)));
+            service.submit(new FoodDeliveryTask(name, random));
         }
         service.shutdown();
         try {
