bc0c7383f33aadb36194952fad3fe014294d11fe
npopov_ <lkn_popov@mail.ru>
2024-06-23 11:18:06 +0300

    BJS2-12788 добавлено решение задачи

---
+++ src/main/java/faang/school/godbless/multithreading/tron/House.java
@@ -0,0 +1,28 @@
+package faang.school.godbless.multithreading.tron;
+
+import lombok.Getter;
+
+import java.util.List;
+
+public class House {
+    @Getter
+    private volatile List<String> availableRoles;
+    @Getter
+    private volatile int countAvailableRoles = 0;
+    private final Object lock = new Object();
+
+    public House(List<String> availableRoles) {
+        this.availableRoles = availableRoles;
+        this.countAvailableRoles = availableRoles.size();
+    }
+
+    public void addRole(String role) {
+        availableRoles.remove(role);
+        countAvailableRoles--;
+    }
+
+    public void removeRole(String role) {
+        availableRoles.add(role);
+        countAvailableRoles++;
+    }
+}
