42a364347f79987f12436e533e8d674d8725041e
Almas D <berser91@gmail.com>
2024-06-23 18:07:34 +0500

    created static variables instead of hardcoding

--- src/main/java/faang/school/godbless/Chore.java
+++ src/main/java/faang/school/godbless/Chore.java
@@ -4,6 +4,8 @@ import java.util.Random;
 
 public class Chore implements Runnable {
     private final String chore;
+    private final static int maxSleepTimeout = 10000;
+    private final static int minSleepTimeout = 1000;
 
     public Chore(String chore) {
         this.chore = chore.toUpperCase();
@@ -14,7 +16,7 @@ public class Chore implements Runnable {
         String threadName = Thread.currentThread().getName();
         System.out.printf("%s started %s.\n", threadName, chore);
         try {
-            Thread.sleep(new Random().nextInt(10000) + 1000);
+            Thread.sleep(new Random().nextInt(maxSleepTimeout) + minSleepTimeout);
             System.out.printf("%s finished %s.\n", threadName, chore);
         } catch (InterruptedException e) {
             throw new RuntimeException(String.format("%s was interrupted on %s.\n", threadName, chore), e);
