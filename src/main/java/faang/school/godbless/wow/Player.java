11914e98325901356da8a916caec463f5ad68170
Nikita2768 <vechkinnikita22@gmail.com>
2024-06-23 21:04:18 +0500

    WOW finish task

---
+++ src/main/java/faang/school/godbless/wow/Player.java
@@ -0,0 +1,15 @@
+package faang.school.godbless.wow;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@AllArgsConstructor
+@Getter
+@Setter
+public class Player {
+    private String name;
+    private int level;
+    private long experience;
+}
---
+++ src/main/java/faang/school/godbless/wow/Quest.java
@@ -0,0 +1,4 @@
+package faang.school.godbless.wow;
+
+public record Quest(String name, int difficulty, int reward) {
+}
