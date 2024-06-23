effd8c42585011b69e3225b0ecbed8374e2e984a
Nikita Volzhin <vlzh.nik@mail.ru>
2024-06-23 09:55:26 +0500

    BJS2-13099 - Bro Force

--- src/main/java/faang/school/godbless/broForce/Main.java
+++ src/main/java/faang/school/godbless/broForce/Main.java
@@ -1,11 +1,12 @@
 package faang.school.godbless.broForce;
 
-import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
 public class Main {
+    private static final int NUM_THREADS = 3;
+
     private static List<Bro> bros = List.of(new Bro("Oleg", 1, true),
             new Bro("Sasha", 6, false),
             new Bro("Igor", 10, true));
@@ -17,7 +18,7 @@ public class Main {
             game.addBro(bro);
         }
 
-        ExecutorService executorService = Executors.newFixedThreadPool(1);
+        ExecutorService executorService = Executors.newFixedThreadPool(NUM_THREADS);
         for (int i = 0; i < bros.size(); i++) {
             Bro bro = game.getBros().get(i);
 
