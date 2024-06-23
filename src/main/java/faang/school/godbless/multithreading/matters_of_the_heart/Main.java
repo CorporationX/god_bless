dc009eca7f456ba88cb6e6284c46b020766e66ab
npopov_ <lkn_popov@mail.ru>
2024-06-23 20:07:36 +0300

    BJS2-12871 добавлено решение задачи

---
+++ src/main/java/faang/school/godbless/multithreading/matters_of_the_heart/Main.java
@@ -0,0 +1,59 @@
+package faang.school.godbless.multithreading.matters_of_the_heart;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
+
+public class Main {
+    private static final int NUM_THREADS = 4;
+
+    public static void main(String[] args) {
+        UserList userList = new UserList(new ArrayList<>(List.of(
+                new User("Миша", true),
+                new User("Саша", true),
+                new User("Петя", true)
+        )));
+
+
+        ChatManager chatManager = new ChatManager(userList);
+        ExecutorService executor = Executors.newFixedThreadPool(NUM_THREADS);
+
+        for (User user : userList.users()) {
+            executor.execute(() -> chatManager.startChat(user));
+        }
+
+        try {
+            Thread.sleep(3000);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+
+        executor.execute(() -> {
+            User newUser = new User("Дима", true);
+            chatManager.addUser(newUser);
+        });
+
+        try {
+            Thread.sleep(3000);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+
+        executor.execute(() -> {
+            User delUser = new User("Петя", true);
+            chatManager.endChat(delUser);
+        });
+
+        executor.shutdown();
+        boolean isTerminated = false;
+        try {
+            isTerminated = executor.awaitTermination(1, TimeUnit.MINUTES);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+
+        System.out.println("Все потоки завершены: " + isTerminated);
+    }
+}
