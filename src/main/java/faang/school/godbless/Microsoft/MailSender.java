687d8f1f27726da9defbae54eabda457bc1949ec
Artem <xotabggg@gmail.com>
2024-06-23 21:46:43 +0700

    BJS2-11886:Вы работаете в Microsoft!

--- src/main/java/faang/school/godbless/Microsoft/MailSender.java
+++ src/main/java/faang/school/godbless/Microsoft/MailSender.java
@@ -5,15 +5,16 @@ import java.util.concurrent.Executors;
 import java.util.concurrent.TimeUnit;
 
 public class MailSender {
+    private static final int MAX_TREAD = 5;
 
     public static void main(String[] args) throws InterruptedException {
-        ExecutorService executor = Executors.newFixedThreadPool(5);
+        ExecutorService executor = Executors.newFixedThreadPool(MAX_TREAD);
 
         for (int i = 0; i < 5; i++) {
             executor.submit(new SenderRunnable(i * 200, (i + 1) * 200));
         }
         executor.shutdown();
-        executor.awaitTermination(Long.MAX_VALUE, TimeUnit.NANOSECONDS);
+        executor.awaitTermination(2, TimeUnit.SECONDS);
         System.out.println("All messages have been sent");
     }
 }
