833afbd8cc0c68f3060cf3204f460788a217b1b6
Danill Klinkov <klinkov.danya@bk.ru>
2024-06-24 21:59:02 +0300

    asynch2: WOW done

---
+++ src/main/java/faang/school/godbless/asynch2/Player.java
@@ -0,0 +1,4 @@
+package faang.school.godbless.asynch2;
+
+public record Player(String name, int level, int experience) {
+}
---
+++ src/main/java/faang/school/godbless/asynch2/QuestSystem.java
@@ -0,0 +1,19 @@
+package faang.school.godbless.asynch2;
+
+import java.util.concurrent.CompletableFuture;
+
+public class QuestSystem {
+    public CompletableFuture<Player> startQuest(Player player, Quest quest) {
+        try {
+            Thread.sleep(quest.difficulty() * 1000L);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+        return CompletableFuture.supplyAsync(() ->
+                new Player(player.name(), player.level(), getNewExperience(player, quest)));
+    }
+
+    private static int getNewExperience(Player player, Quest quest) {
+        return player.experience() + quest.reward();
+    }
+}
