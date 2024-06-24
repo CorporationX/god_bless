f4ac5e3641bd01a773b4262bd11de1570ed52f6d
Gevorg <a0964593@gmail.com>
2024-06-25 00:33:58 +0300

    BJS2-14099 - make Battle and Robot classes

---
+++ src/main/java/faang/school/godbless/StarWarsArena/Robot.java
@@ -0,0 +1,14 @@
+package faang.school.godbless.StarWarsArena;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+public class Robot {
+    private final String name;
+    private int attackPower;
+    private int defensePower;
+}
