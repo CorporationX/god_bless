e7b028223195c1b9454d153ba980e7e8239aa368
cirill98 <100434317+cirill98@users.noreply.github.com>
2024-06-24 10:35:12 +0500

    Исправил решение

--- src/main/java/faang/school/godbless/StarWars/Main.java
+++ src/main/java/faang/school/godbless/StarWars/Main.java
@@ -7,8 +7,8 @@ public class Main {
     public static void main(String[] args) throws ExecutionException, InterruptedException {
         Battle battle = new Battle();
         Future<Robot> result = battle.fight(
-                new Robot("Оптимус Прайм", 15, 5, 76),
-                new Robot("Не Оптимус Прайм", 13, 7, 76));
+                new Robot("Оптимус Прайм!", 15, 5, 76),
+                new Robot("Не Оптимус Прайм!", 13, 7, 76));
         Robot winner = result.get();
         System.out.println("Победил - " + winner);
         battle.getExecutorService().shutdown();
