9b4ca468bd42e416245d50403dc8a335dbed4d90
Caveri95 <ura22051995@gmail.com>
2024-06-24 18:48:52 +0300

    BJS2-
    - реализовал асинхронное выполнение прохождения квестов игроками на основе CompletableFuture

---
+++ src/main/java/faang/school/godbless/future/wow/Player.java
@@ -0,0 +1,20 @@
+package faang.school.godbless.future.wow;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+public class Player {
+
+    private String name;
+    private int level;
+    private int experience;
+
+    public Player addExperience(int experience) {
+        this.experience += experience;
+        return this;
+    }
+}
