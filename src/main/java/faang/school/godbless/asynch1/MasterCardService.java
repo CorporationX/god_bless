c9613510c6682c2f3647f2b3b360a8a0f700b6a4
Danill Klinkov <klinkov.danya@bk.ru>
2024-06-24 21:35:19 +0300

    asynch1: Асинхронность и Future done

---
+++ src/main/java/faang/school/godbless/asynch1/MasterCardService.java
@@ -0,0 +1,21 @@
+package faang.school.godbless.asynch1;
+
+public class MasterCardService {
+    public int collectPayment() {
+        try {
+            Thread.sleep(10_000);
+            return 10_000;
+        } catch (InterruptedException e) {
+            throw new RuntimeException();
+        }
+    }
+
+    public int sendAnalytics() {
+        try {
+            Thread.sleep(1_000);
+            return 1_000;
+        } catch (InterruptedException e) {
+            throw new RuntimeException();
+        }
+    }
+}
