a26cb4b9bcbe2794ff92ec4a52c9f278e9ed67b8
Rauan Kaztaev <rtishtyk@gmail.com>
2024-06-25 16:58:53 +0500

    WOW

---
+++ src/main/java/faang/school/godbless/wow/Main.java
@@ -0,0 +1,31 @@
+package faang.school.godbless.wow;
+
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutionException;
+
+public class Main {
+    public static void main(String[] args) throws ExecutionException, InterruptedException {
+        QuestSystem questSystem = new QuestSystem();
+
+        Player player1 = new Player("Thrall", 10, 250);
+        Player player2 = new Player("Sylvanas", 12, 450);
+        Player player3 = new Player("Player3", 22, 900);
+
+        Quest quest1 = new Quest("Defeat the Lich King", 10, 150);
+        Quest quest2 = new Quest("Retrieve the Sword of Azeroth", 8, 100);
+        Quest quest3 = new Quest("Do something hard", 10, 900);
+
+        CompletableFuture<Player> player1Quest = questSystem.startQuest(player1, quest1);
+        CompletableFuture<Player> player2Quest = questSystem.startQuest(player2, quest2);
+        CompletableFuture<Player> player3Quest = questSystem.startQuest(player3, quest3);
+
+
+        player1Quest.thenAccept(player -> System.out.printf("%s has completed the quest and now on %d level with %d experience points\n", player.getName(), player.getLevel(), player.getExperience()));
+        player2Quest.thenAccept(player -> System.out.printf("%s has completed the quest and now on %d level with %d experience points\n", player.getName(), player.getLevel(), player.getExperience()));
+        player3Quest.thenAccept(player -> System.out.printf("%s has completed the quest and now on %d level with %d experience points\n", player.getName(), player.getLevel(), player.getExperience()));
+
+        player1Quest.join();
+        player2Quest.join();
+        player3Quest.join();
+    }
+}
---
+++ src/main/java/faang/school/godbless/wow/QuestSystem.java
@@ -0,0 +1,28 @@
+package faang.school.godbless.wow;
+
+import java.util.concurrent.CompletableFuture;
+
+public class QuestSystem {
+
+    public CompletableFuture<Player> startQuest(Player player, Quest quest) {
+        return CompletableFuture.supplyAsync(() -> {
+            try {
+                System.out.printf("%s: %s started %s\n", Thread.currentThread().getName(), player.getName(), quest.getName());
+                Thread.sleep(quest.getDifficulty() * 1000L);
+            } catch (InterruptedException e) {
+                System.out.printf("%s: %s interrupted\n", Thread.currentThread().getName(), player.getName());
+            }
+            player.setExperience(player.getExperience() + quest.getReward());
+            updateExperienceAndLevel(player);
+
+            return player;
+        });
+    }
+
+    private void updateExperienceAndLevel(Player player) {
+        if (player.getExperience() >= 1000) {
+            player.setExperience(player.getExperience() - 1000);
+            player.setLevel(player.getLevel() + 1);
+        }
+    }
+}
