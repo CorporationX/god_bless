96aeb755b44dc3cee65afd2e91bd3dd8e3b103bc
sauce-chili <saucechilidev@gmail.com>
2024-06-26 03:40:45 +0300

    Add new method `getAllNotifications` and changed the return value of the `fetchNotification` method to `CompletableFuture<Void>`

--- src/main/java/faang/school/godbless/domain/manager/NotificationManager.java
+++ src/main/java/faang/school/godbless/domain/manager/NotificationManager.java
@@ -2,10 +2,11 @@ package faang.school.godbless.domain.manager;
 
 import faang.school.godbless.domain.model.Notification;
 
+import java.util.List;
 import java.util.concurrent.CompletableFuture;
 
 public interface NotificationManager {
-    void addNotification(Notification notification) throws InterruptedException;
-
-    CompletableFuture<Notification> fetchNotification();
+    void addNotification(Notification notification);
+    List<Notification> getAllNotifications();
+    CompletableFuture<Void> fetchNotification();
 }
