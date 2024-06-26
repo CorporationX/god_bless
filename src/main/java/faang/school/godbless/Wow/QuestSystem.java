1a6356cb09e4bde415eaef3423bf6fbea4317eaf
Alen <Alendos87@gmail.com>
2024-06-26 16:58:50 +0500

    Solved problem World Of Warcraft

--- src/main/java/faang/school/godbless/Wow/QuestSystem.java
+++ src/main/java/faang/school/godbless/Wow/QuestSystem.java
@@ -2,8 +2,7 @@ package faang.school.godbless.Wow;
 import java.util.concurrent.CompletableFuture;
 
 public class QuestSystem {
-
-    public CompletableFuture<Player> startQuest(Player player,Quest quest) {
+    public CompletableFuture<Player> startQuest(Player player, Quest quest) {
         return CompletableFuture.supplyAsync(() -> {
             try {
                 Thread.sleep(quest.getDifficulty() * 1000);
