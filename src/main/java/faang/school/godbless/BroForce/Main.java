8b08af77397044dd82eca9c21c6a6eab752362ff
Alen <Alendos87@gmail.com>
2024-06-26 13:47:21 +0500

    Исправил отступы, убрал лишний метод и поменял местами executor.execute() с циклом for

---
+++ src/main/java/faang/school/godbless/BroForce/Main.java
@@ -0,0 +1,20 @@
+package faang.school.godbless.BroForce;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+public class Main {
+    public static void main(String[] args) {
+        Game game = new Game(0, 10);
+
+        ExecutorService executor = Executors.newFixedThreadPool(10);
+        for (int i = 0; i < 10; i++) {
+            executor.execute(() -> {
+                game.update();
+                System.out.println("Результат: " + game.getScore() + ", Жизнь: " + game.getLives());
+            });
+        }
+        executor.shutdown();
+    }
+}
+
