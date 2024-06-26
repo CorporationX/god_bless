5165340f6e2b8b36b0d89c3d9cc44693da5ba8e6
Stanislav Ovsyankin <cancion2able@gmail.com>
2024-06-26 21:14:20 +0500

    Асинхронный турнир волшебников

---
+++ src/main/java/faang/school/godbless/async/wizards/Student.java
@@ -0,0 +1,16 @@
+package faang.school.godbless.async.wizards;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+public class Student {
+
+    private final String name;
+    private final int year;
+    private int points;
+
+}
