fafa84a77a9c2fc6a80cd6cbd63dec9866fcf8bb
Grisha Tkach <grisatkac@mail.ru>
2024-06-26 21:59:23 +0300

    update thread creation

--- src/main/java/faang/school/godbless/sprint3/workInMicrosoft/MailSender.java
+++ src/main/java/faang/school/godbless/sprint3/workInMicrosoft/MailSender.java
@@ -5,31 +5,19 @@ public class MailSender {
 
     public static void main(String[] args) {
         try {
-            SenderRunnable senderRunnable = new SenderRunnable(0, 199);
-            Thread thread1 = new Thread(senderRunnable);
-            thread1.start();
 
-            SenderRunnable senderRunnable2 = new SenderRunnable(200, 399);
-            Thread thread2 = new Thread(senderRunnable2);
-            thread2.start();
+            int numberOfThreads = 5;
+            int mailRangeSize = 200;
 
-            SenderRunnable senderRunnable3 = new SenderRunnable(400, 599);
-            Thread thread3 = new Thread(senderRunnable3);
-            thread3.start();
+            for (int i = 0; i < numberOfThreads; i++) {
+                int start = i * 200;
+                int end = (i + 1) * mailRangeSize - 1;
 
-            SenderRunnable senderRunnable4 = new SenderRunnable(600, 799);
-            Thread thread4 = new Thread(senderRunnable4);
-            thread4.start();
-
-            SenderRunnable senderRunnable5 = new SenderRunnable(800, 999);
-            Thread thread5 = new Thread(senderRunnable5);
-            thread5.start();
-
-            thread1.join();
-            thread2.join();
-            thread3.join();
-            thread4.join();
-            thread5.join();
+                SenderRunnable senderRunnable = new SenderRunnable(start, end);
+                Thread thread = new Thread(senderRunnable);
+                thread.start();
+                thread.join();
+            }
 
             System.out.println("All threads have done");
         } catch (InterruptedException e) {
