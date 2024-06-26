c5f8546f467bb0a77d258edb0a78bf030a257484
Evgenii Malkov <malk.2011@yandex.ru>
2024-06-26 20:24:09 +0300

    BJS2-14260: Carry отожрался

---
+++ src/main/java/faang/school/godbless/sprint2/dota2/Item.java
@@ -0,0 +1,16 @@
+package faang.school.godbless.sprint2.dota2;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.ToString;
+
+/**
+ * @author Evgenii Malkov
+ */
+@Getter
+@AllArgsConstructor
+@ToString
+public class Item {
+    private String name;
+    private int power;
+}
