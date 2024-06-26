6a9bce8a2d87beedfd9eee8164722dd0d12855ee
0Miha0 <140548440+0Miha0@users.noreply.github.com>
2024-06-26 13:54:53 +0500

    https://faang-school.atlassian.net/browse/BJS2-11971

---
+++ src/main/java/faang/school/godbless/Sprint_3/BJS2_11971/SenderRunnable.java
@@ -0,0 +1,17 @@
+package faang.school.godbless.Sprint_3.BJS2_11971;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@AllArgsConstructor
+@Data
+public class SenderRunnable implements Runnable{
+    private int startIndex;
+    private int endIndex;
+    @Override
+    public void run() {
+        for (int i = startIndex; i < endIndex; i++) {
+            System.out.println("Сообщение " + i + " отправлено");
+        }
+    }
+}
