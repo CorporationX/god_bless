077848b47161ad3aa132e845d409ae5defc35138
Stanislav Ovsyankin <cancion2able@gmail.com>
2024-06-27 15:13:50 +0500

    Рефакторинг

--- src/main/java/faang/school/godbless/async/wizards/Student.java
+++ src/main/java/faang/school/godbless/async/wizards/Student.java
@@ -1,11 +1,9 @@
 package faang.school.godbless.async.wizards;
 
 import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.Setter;
+import lombok.Data;
 
-@Getter
-@Setter
+@Data
 @AllArgsConstructor
 public class Student {
 
