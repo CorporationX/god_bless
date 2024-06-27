aacf4be54877cec1a9c0d31dc17728e0881d9483
Artem <xotabggg@gmail.com>
2024-06-27 13:32:11 +0700

    BJS2-13755: Собираем заказы в Amazon

---
+++ src/main/java/CollectOrdersOnAmazon/OrderProcessor.java
@@ -0,0 +1,26 @@
+package CollectOrdersOnAmazon;
+
+import lombok.NonNull;
+
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.atomic.AtomicInteger;
+
+public class OrderProcessor {
+    private AtomicInteger totalProcessedOrders = new AtomicInteger(0);
+
+    public CompletableFuture<Void> processOrder(@NonNull Order order) {
+        return CompletableFuture.runAsync(() -> {
+            try {
+                Thread.sleep(3000);
+                order.setStatus("Processed");
+                totalProcessedOrders.incrementAndGet();
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+        });
+    }
+
+    public int getTotalProcessedOrders() {
+        return totalProcessedOrders.get();
+    }
+}
