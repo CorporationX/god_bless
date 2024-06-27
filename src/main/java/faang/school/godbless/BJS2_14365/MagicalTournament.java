bb69c1c95532b7ea8114e8bfe7542abf9515ba88
Max.Shundrik <mas9l175@gmail.com>
2024-06-28 00:16:11 +0300

    Fixed Threads pool method BJS2-14095

--- src/main/java/faang/school/godbless/BJS2_14365/MagicalTournament.java
+++ src/main/java/faang/school/godbless/BJS2_14365/MagicalTournament.java
@@ -7,21 +7,20 @@ public class MagicalTournament {
         Tournament tournament = new Tournament();
         School hogwarts = new School("Hogwarts");
         School beauxbatons = new School("Beauxbatons");
-
-        Student s1 = new Student("Student1", 1, 2);
-        Student s2 = new Student("Student2", 2, 1);
-        Student s3 = new Student("Student3", 2, 4);
-        Student s4 = new Student("Student4", 1, 6);
-        Student s5 = new Student("Student5", 3, 9);
-        Student s6 = new Student("Student6", 2, 10);
-        Student s7 = new Student("Student7", 3, 3);
-        Student s8 = new Student("Student8", 1, 3);
-        Student s9 = new Student("Student9", 1, 5);
-        Student s10 = new Student("Student10", 3, 4);
-        Student s11 = new Student("Student11", 4, 2);
-        Student s12 = new Student("Student12", 2, 1);
-        hogwarts.addStudentsToSchool(s1, s2, s3, s7, s11, s12);
-        beauxbatons.addStudentsToSchool(s4, s5, s6, s10, s8, s9);
+        hogwarts.addStudentsToSchool(
+                new Student("Student1", 1, 2),
+                new Student("Student2", 2, 1),
+                new Student("Student3", 2, 4),
+                new Student("Student7", 3, 3),
+                new Student("Student11", 4, 2),
+                new Student("Student12", 2, 1));
+        beauxbatons.addStudentsToSchool(
+                new Student("Student4", 1, 6),
+                new Student("Student5", 3, 9),
+                new Student("Student6", 2, 10),
+                new Student("Student8", 1, 3),
+                new Student("Student9", 1, 5),
+                new Student("Student10", 3, 4));
         Task quiddich = new Task("Quiddich", 25, 200);
         Task magic = new Task("Magic", 5, 35);
 
--- src/main/java/faang/school/godbless/BJS2_14365/Tournament.java
+++ src/main/java/faang/school/godbless/BJS2_14365/Tournament.java
@@ -3,11 +3,11 @@ package faang.school.godbless.BJS2_14365;
 import java.util.concurrent.*;
 
 public class Tournament {
-    private int random;
+    ExecutorService service;
 
     public CompletableFuture<School> startTask(School school, Task task) {
         double reward = task.getReward();
-        ExecutorService service = Executors.newFixedThreadPool(school.getTeam().size());
+        service = Executors.newFixedThreadPool(school.getTeam().size());
         CompletableFuture<School> schoolFuture = CompletableFuture.supplyAsync(() -> {
             divideReward(reward, school);
             try {
@@ -24,15 +24,13 @@ public class Tournament {
     private void divideReward(double reward, School school) {
         double tempReward = reward;
         double earnedPoints;
-        int quantity = school.getTeam().size();
         int i = -1;
         while (++i < school.getTeam().size()) {
-            random = ThreadLocalRandom.current().nextInt(quantity);
+            int random = ThreadLocalRandom.current().nextInt(school.getTeam().size() - i);
             System.out.println("random is " + random);
             earnedPoints = tempReward / random;
             school.getTeam().get(i).earnPoints(earnedPoints);
             tempReward -= earnedPoints;
-            quantity--;
         }
     }
 }
