8a7ada35f7d70298eab3805881531e78fdd357c1
Alen <Alendos87@gmail.com>
2024-06-27 13:41:35 +0500

    FanOutFanIn

---
+++ src/main/java/faang/school/godbless/FanOutFanIn/Main.java
@@ -0,0 +1,29 @@
+package faang.school.godbless.FanOutFanIn;
+
+import javax.xml.transform.Result;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+public class Main {
+
+    public static void main(String[] args) {
+        List<SquareRequest> requests = launch();
+
+        ResultConsumer resultConsumer = new ResultConsumer(0L);
+
+        long result = SquareRequest.fanOutFanIn(requests, resultConsumer);
+
+        System.out.println("Sum of squares from 1 to 1000 " + result);
+    }
+
+    public static List<SquareRequest> launch() {
+        List<SquareRequest> requests = IntStream.rangeClosed(1, 1000)
+                .parallel()  // Параллельный стрим
+                .mapToObj(i -> new SquareRequest((long) i))
+                .collect(Collectors.toList());
+        return requests;
+    }
+
+}
---
+++ src/main/java/faang/school/godbless/FanOutFanIn/ResultConsumer.java
@@ -0,0 +1,21 @@
+package faang.school.godbless.FanOutFanIn;
+import java.util.List;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.atomic.AtomicLong;
+
+public class ResultConsumer {
+
+    private final AtomicLong sumOfSquaredNumbers;
+
+    public ResultConsumer(Long init) {
+        sumOfSquaredNumbers = new AtomicLong(init);
+    }
+
+    public Long add(final Long num) {
+        return sumOfSquaredNumbers.addAndGet(num);
+    }
+
+    public Long getResult(){
+        return sumOfSquaredNumbers.get();
+    }
+}
