b54d7ca723d46b6b3a6b18698e2b798786f61314
Nurlan Turemuratov <nurlant2003@gmail.com>
2024-06-28 13:50:03 +0500

    WOW

---
+++ src/main/java/faang/school/godbless/wow/Player.java
@@ -0,0 +1,15 @@
+package faang.school.godbless.wow;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@AllArgsConstructor
+public class Player {
+    private String name;
+    private int level;
+
+    @Setter
+    private int experience;
+}
