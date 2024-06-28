3ce0d637dc3d6cf70959350edf82c765bfa3516c
Andrei Kuzmin <Andreytko@mail.ru>
2024-06-29 02:21:43 +0300

    working(

---
+++ src/main/java/faang/school/godbless/Task_Leveling_Alchemy/Main.java
@@ -0,0 +1,20 @@
+package faang.school.godbless.Task_Leveling_Alchemy;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.ExecutionException;
+
+public class Main {
+    public static final List<Potion> potions = new ArrayList<>();
+
+    public static void main(String[] args) throws ExecutionException, InterruptedException {
+        Potion cola = new Potion("cola", 3);
+        PotionMaker.gatherIngredients(cola);
+    }
+
+    static {
+        potions.add(new Potion("pepsi", 2));
+        potions.add(new Potion("sprite", 3));
+        potions.add(new Potion("cola", 3));
+    }
+}
--- src/main/java/faang/school/godbless/Task_Leveling_Alchemy/PotionMaker.java
+++ src/main/java/faang/school/godbless/Task_Leveling_Alchemy/PotionMaker.java
@@ -1,27 +1,42 @@
 package faang.school.godbless.Task_Leveling_Alchemy;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutionException;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
-import java.util.
+import java.util.concurrent.atomic.AtomicInteger;
+
 
 public class PotionMaker {
 
-    public int gatherIngredients(Potion potion) {
+    public static int gatherIngredients(Potion potion) throws ExecutionException, InterruptedException {
         ExecutorService potionService = Executors.newFixedThreadPool(potion.getRequiredIngredients());
         List<CompletableFuture<Void>> futures = new ArrayList<>();
         for (int i = 0; i < potion.getRequiredIngredients(); i++) {
             int index = i;
             CompletableFuture<Void> numberOfIngredients = CompletableFuture.runAsync(() -> {
                 System.out.println("creating potion number " + index);
+                try {
+                    Thread.sleep(2000L);
+                } catch (InterruptedException e) {
+                    throw new RuntimeException("the potion was not made...");
+                }
             }, potionService);
             futures.add(numberOfIngredients);
         }
+        potionService.shutdown();
         CompletableFuture[] futureResult = futures.toArray(new CompletableFuture[futures.size()]);
         CompletableFuture<Void> combinedResult = CompletableFuture.allOf(futureResult);
+        CompletableFuture<Integer> finalResult = combinedResult.thenApply(x -> (int) futures.stream()
+                .map(CompletableFuture::join).count());
         System.out.println("All potions were prepared");
-        return potion.getRequiredIngredients();
+        return finalResult.get();
+    }
+
+    public int gatherAllIngredients(List<Potion> potions) {
+        return -1;
     }
 
 }
