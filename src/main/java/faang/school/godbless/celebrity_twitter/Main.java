38d7420282d4620cf3c1af5725d923e84b75f10a
Rauan Kaztaev <rtishtyk@gmail.com>
2024-06-28 22:49:31 +0500

    Становимся celebrity в Twitter: добавил константы, добавил allOf()

--- src/main/java/faang/school/godbless/celebrity_twitter/Main.java
+++ src/main/java/faang/school/godbless/celebrity_twitter/Main.java
@@ -7,19 +7,23 @@ import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
 public class Main {
+    private static final int THREADS = 5;
+    private static final int FOLLOWERS = 10_000;
+
     public static void main(String[] args) {
-        ExecutorService executorService = Executors.newFixedThreadPool(5);
+        ExecutorService executorService = Executors.newFixedThreadPool(THREADS);
 
         TwitterAccount account = new TwitterAccount("Rauan");
         TwitterSubscriptionSystem subscriptionSystem = new TwitterSubscriptionSystem();
 
         List<CompletableFuture<Void>> completableFutures = new ArrayList<>();
 
-        for (int i = 0; i < 100000; i++) {
+        for (int i = 0; i < FOLLOWERS; i++) {
             completableFutures.add(subscriptionSystem.followAccount(account, executorService));
         }
 
-        completableFutures.forEach(CompletableFuture::join);
+        CompletableFuture.allOf(completableFutures.toArray(CompletableFuture[]::new)).join();
+
         executorService.shutdown();
 
         System.out.printf("Total %s followers: %s", account.getName(), account.getFollowers());
