7ae988b113f728cbc01f01f7da1ea1eeff5ccf34
alibaysarov <alibajsarov353@gmail.com>
2024-06-29 21:00:02 +0400

    Качаем алхимию

---
+++ src/main/java/faang/school/godbless/alchemy/Potion.java
@@ -0,0 +1,12 @@
+package faang.school.godbless.alchemy;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class Potion {
+    private String name;
+    private Integer requiredIngredients;
+}
