23aa3ee8f59d11d8e0f8b3590eef7fdb840067dd
alibaysarov <alibajsarov353@gmail.com>
2024-06-29 21:52:49 +0400

    Собираем заказы в Amazon

---
+++ src/main/java/faang/school/godbless/amazon/Order.java
@@ -0,0 +1,14 @@
+package faang.school.godbless.amazon;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+public class Order {
+    private Integer id;
+    private String  status;
+}
