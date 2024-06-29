ac6684633f601864cab2f68595d33eb8162d5ca1
Konstantyn <gagarin2789@gmail.com>
2024-06-29 13:55:47 +0200

    BJS2-13472
    - Removed executorService from Main
    - Added executorService to PiCalculator
    - Added method shutdownExecutor

--- src/main/java/faang/school/godbless/multithreading_async/task_9/Main.java
+++ src/main/java/faang/school/godbless/multithreading_async/task_9/Main.java
@@ -2,18 +2,15 @@ package faang.school.godbless.multithreading_async.task_9;
 
 import lombok.extern.slf4j.Slf4j;
 
-import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
 @Slf4j
 public class Main {
-    private static final int THREAD_COUNT = 4;
-    private static final int N = 1000;
+    private static final int N = 100;
 
     public static void main(String[] args) {
-        PiCalculator piCalculator = new PiCalculator();
-        ExecutorService executor = Executors.newFixedThreadPool(THREAD_COUNT);
-        log.info("PI : {}", piCalculator.calculatePi(N, executor));
-        executor.shutdown();
+        PiCalculator piCalculator = new PiCalculator(Executors.newFixedThreadPool(N));
+        log.info("PI : {}", piCalculator.calculatePi(N));
+        piCalculator.shutdownExecutor();
     }
 }
--- src/main/java/faang/school/godbless/multithreading_async/task_9/PiCalculator.java
+++ src/main/java/faang/school/godbless/multithreading_async/task_9/PiCalculator.java
@@ -1,15 +1,24 @@
 package faang.school.godbless.multithreading_async.task_9;
 
+import lombok.AllArgsConstructor;
+
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.ExecutorService;
 import java.util.stream.IntStream;
 
+@AllArgsConstructor
 public class PiCalculator {
-    public double calculatePi(int n, ExecutorService executor) {
+    private final ExecutorService executor;
+
+    public double calculatePi(int n) {
         long pointInsideCount = IntStream.range(0, n).parallel()
             .mapToObj(index -> CompletableFuture.supplyAsync(Point::new, executor))
             .filter(point -> point.join().isInside())
             .count();
         return (double) 4 * pointInsideCount / n;
+    }
+
+    public void shutdownExecutor() {
+        executor.shutdown();
     }
 }
