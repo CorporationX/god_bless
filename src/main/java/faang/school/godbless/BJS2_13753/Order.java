63f9eac5b3239775cf13ae509bb3d61fbe1347b7
Mikhail Fokin <fokinms@gmail.com>
2024-06-29 21:10:24 +0400

    BJS2-13753 Собираем заказы в Amazon

---
+++ src/main/java/faang/school/godbless/BJS2_13753/Order.java
@@ -0,0 +1,14 @@
+package faang.school.godbless.BJS2_13753;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@AllArgsConstructor
+@Getter @Setter
+@ToString
+public class Order {
+    private int id;
+    private String status;
+}
