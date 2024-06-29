9a3ed2814bfa3590eb60836a0828eba2bbe4e332
sauce-chili <saucechilidev@gmail.com>
2024-06-29 03:20:21 +0300

    Completed BJS2-14289

--- src/main/java/faang/school/godbless/Application.java
+++ src/main/java/faang/school/godbless/Application.java
@@ -1,7 +1,45 @@
 package faang.school.godbless;
 
+import faang.school.godbless.model.Inventory;
+import faang.school.godbless.model.Item;
+
+import java.util.ArrayList;
+import java.util.concurrent.CompletableFuture;
+
 public class Application {
+
+    private static Item takeStoreItem() throws InterruptedException {
+        Thread.sleep(2_000);
+        return new Item("store_item", 21);
+    }
+
+    private static Item takeChestItem() throws InterruptedException {
+        Thread.sleep(4_000);
+        return new Item("chest_item", 35);
+    }
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Inventory inventory = new Inventory(new ArrayList<>());
+
+        CompletableFuture<Item> chestLootingFuture = CompletableFuture.supplyAsync(() -> {
+            try {
+                return takeChestItem();
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+        });
+
+        CompletableFuture<Item> shoppingStoreFuture = CompletableFuture.supplyAsync(() -> {
+            try {
+                return takeStoreItem();
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+        });
+
+        chestLootingFuture.thenCombine(shoppingStoreFuture,
+                inventory::combineItem).thenAccept(inventory::addItem).join();
+
+        inventory.items().forEach(System.out::println);
     }
 }
