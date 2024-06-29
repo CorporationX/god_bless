3ea9ab8f5512b27aa13551f741d23b218125ad80
alibaysarov <alibajsarov353@gmail.com>
2024-06-29 23:09:46 +0400

    Становимся celebrity в Twitter

---
+++ src/main/java/faang/school/godbless/twitter/TwitterAccount.java
@@ -0,0 +1,14 @@
+package faang.school.godbless.twitter;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+public class TwitterAccount {
+    private String username;
+    private Integer followers;
+}
