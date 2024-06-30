629dcdc50a0f2e1a4f55e3072d02d98fd1988543
Nurlan Turemuratov <nurlant2003@gmail.com>
2024-06-30 23:19:56 +0500

    исправил код

--- src/main/java/faang/school/godbless/supercow/Main.java
+++ src/main/java/faang/school/godbless/supercow/Main.java
@@ -4,16 +4,10 @@ public class Main {
     public static void main(String[] args) {
         Boss boss = new Boss(3);
 
-        Thread player1 = new Thread(new Player("Player 1", boss));
-        Thread player2 = new Thread(new Player("Player 2", boss));
-        Thread player3 = new Thread(new Player("Player 3", boss));
-        Thread player4 = new Thread(new Player("Player 4", boss));
-        Thread player5 = new Thread(new Player("Player 5", boss));
-
-        player1.start();
-        player2.start();
-        player3.start();
-        player4.start();
-        player5.start();
+        Thread[] players = new Thread[5];
+        for (int i = 0; i < players.length; ++i) {
+            players[i] = new Thread(new Player("Player " + (i + 1), boss));
+            players[i].start();
+        }
     }
 }
