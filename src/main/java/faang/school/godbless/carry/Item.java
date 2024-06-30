52c95852f350c566e268543e8e459876f00183eb
Иван Шилов <ivshvl39@gmail.com>
2024-06-30 11:56:41 +0300

    Carry отожрался

---
+++ src/main/java/faang/school/godbless/carry/Item.java
@@ -0,0 +1,14 @@
+package faang.school.godbless.carry;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Item {
+    private String name;
+    private int power;
+
+}
