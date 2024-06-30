33a0fb539fc61cbbbf54a0d73b02c838fc0d04df
Sasha Chekalkin <crutoipatz@yandex.ru>
2024-06-30 15:22:18 +0300

    int1nglence-BJS2-13392 - Асинхронность и Future

--- src/main/java/faang/school/godbless/BJS2_13889/Player.java
+++ src/main/java/faang/school/godbless/BJS2_13889/Player.java
@@ -3,4 +3,7 @@ package faang.school.godbless.BJS2_13889;
 public record Player(String name,
                      int level,
                      int experience){
+
+    public void setExperience(int experience){
+    }
 }
--- src/main/java/faang/school/godbless/BJS2_13889/QuestSystem.java
+++ src/main/java/faang/school/godbless/BJS2_13889/QuestSystem.java
@@ -1,10 +1,21 @@
 package faang.school.godbless.BJS2_13889;
 
 import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutionException;
+import java.util.function.Supplier;
 
 public class QuestSystem {
 
     public CompletableFuture<Player> startQuest(Player player , Quest quest){
-
+        return CompletableFuture.supplyAsync(() -> {
+            try{
+                System.out.println("Task is starting");
+                Thread.sleep(quest.difficulty() * 1000);
+                player.setExperience(player.experience() + quest.reward());
+            } catch (InterruptedException e){
+                e.printStackTrace();
+            }
+            return player;
+        });
     }
 }
