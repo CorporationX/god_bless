1b232d0a14e5cb0ba2a707e91ab86db20bbcca9d
Mikhail Fokin <fokinms@gmail.com>
2024-06-30 13:04:30 +0400

    BJS2-13469 Вычислим число ПИ

---
+++ src/main/java/faang/school/godbless/BJS2_13469/Point.java
@@ -0,0 +1,12 @@
+package faang.school.godbless.BJS2_13469;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@AllArgsConstructor
+@Setter @Getter
+public class Point {
+    private double x;
+    private double y;
+}
