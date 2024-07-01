263c862a28971171b4c16e5827d4d312daf1ef9b
ravshanbekn <ravshanbeknabiyev.dev@gmail.com>
2024-07-01 14:11:36 +0500

    BJS2-14150 - Рассылаем уведомления в Facebook
    + Исправил недочеты

--- src/main/java/faang/school/godbless/facebook_notifications/NotificationManager.java
+++ src/main/java/faang/school/godbless/facebook_notifications/NotificationManager.java
@@ -8,13 +8,15 @@ import java.util.List;
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
 
 @Slf4j
 @Getter
 public class NotificationManager {
+    private final long TIME_LIMIT_SECONDS = 5;
+    private final long NOTIFICATION_FETCHING_TIME = 1000L;
     private final ExecutorService executorService =
             Executors.newFixedThreadPool(Runtime.getRuntime().availableProcessors());
-    private final long NOTIFICATION_FETCHING_TIME = 1000L;
     private final List<Notification> notificationList = new ArrayList<>();
 
     public CompletableFuture<Void> fetchNotification(int id, String message) {
@@ -36,5 +38,11 @@ public class NotificationManager {
 
     public void closeNotificationManager() {
         executorService.shutdown();
+
+        try {
+            executorService.awaitTermination(TIME_LIMIT_SECONDS, TimeUnit.SECONDS);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
