be660e876080cf4204ba8846b4a5c85ad99e0dce
Gevorg <a0964593@gmail.com>
2024-07-01 10:11:01 +0300

    BJS2-14099 - add awaitTermination in fight method

--- src/main/java/faang/school/godbless/StarWarsArena/Battle.java
+++ src/main/java/faang/school/godbless/StarWarsArena/Battle.java
@@ -1,19 +1,25 @@
 package faang.school.godbless.StarWarsArena;
 
-import java.util.Optional;
+import lombok.extern.slf4j.Slf4j;
+
 import java.util.Random;
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.Future;
 
+import static java.util.concurrent.TimeUnit.SECONDS;
+
+@Slf4j
 public class Battle {
+    private static final int AWAIT_TERMINATION_TIME = 2;
     private static final Random random = new Random();
     private static final int NUM_THEADS = 3;
     private static final ExecutorService EXECUTOR = Executors.newFixedThreadPool(3);
 
     public Future<Robot> fight (Robot robotOne, Robot robotTwo) {
         attack(robotOne, robotTwo);
+
         CompletableFuture<Robot> result = CompletableFuture.supplyAsync(() -> {
             if (robotOne.getDefensePower() > 0 && robotTwo.getDefensePower() <= 0) {
                 return robotOne;
@@ -23,7 +29,15 @@ public class Battle {
             }
             return robotOne;
         }, EXECUTOR);
+
         EXECUTOR.shutdown();
+        try {
+            EXECUTOR.awaitTermination(AWAIT_TERMINATION_TIME, SECONDS);
+        } catch (InterruptedException e) {
+            log.error(".awaitTermination in fight method was interrupted while waiting");
+            throw new RuntimeException(e);
+        }
+
         return result;
     }
 
--- src/main/java/faang/school/godbless/StarWarsArena/Main.java
+++ src/main/java/faang/school/godbless/StarWarsArena/Main.java
@@ -1,6 +1,5 @@
 package faang.school.godbless.StarWarsArena;
 
-import java.util.Optional;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.Future;
 
@@ -8,14 +7,11 @@ public class Main {
     public static void main(String[] args) {
         Battle battle = new Battle();
 
-        // Создание роботов
         Robot r2d2 = new Robot("R2-D2", 5, 7);
         Robot c3po = new Robot("C-3PO", 4, 8);
 
-        // Запуск битвы
         Future<Robot> winnerFuture = battle.fight(r2d2, c3po);
 
-        // Обработка результата
         try {
             Robot winner = winnerFuture.get();
             System.out.println("Победитель битвы: " + winner.getName());
