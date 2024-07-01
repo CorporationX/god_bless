2efa05bc292dfee5b5e88046465e4afc925d443e
Rauan Kaztaev <rtishtyk@gmail.com>
2024-07-01 18:29:44 +0500

    Оставляем комментарии: добавил методы, добавил Callable

--- src/main/java/faang/school/godbless/leave_comments/Main.java
+++ src/main/java/faang/school/godbless/leave_comments/Main.java
@@ -2,10 +2,16 @@ package faang.school.godbless.leave_comments;
 
 import java.time.LocalDateTime;
 import java.util.List;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
 
 public class Main {
+    private static final int THREADS = 5;
+
     public static void main(String[] args) throws InterruptedException {
         PostService postService = new PostService();
+        ExecutorService executorService = Executors.newFixedThreadPool(THREADS);
 
         List<Comment> commentsForPost1 = List.of(
                 new Comment("Author1", "Text1", LocalDateTime.now()),
@@ -28,39 +34,16 @@ public class Main {
         Runnable addPost1 = () -> postService.addPost(new Post(1, "Author post 1", "Some title", "Some text"));
         Runnable addPost2 = () -> postService.addPost(new Post(2, "Author post 2", "Another title", "Another text"));
 
-        Thread threadAddPost1 = new Thread(addPost1);
-        Thread threadAddPost2 = new Thread(addPost2);
+        executorService.execute(addPost1);
+        executorService.execute(addPost2);
+        executorService.execute(() -> commentsForPost1.forEach(comment -> postService.addComment(comment, () -> 1)));
+        executorService.execute(() -> commentsForPost2.forEach(comment -> postService.addComment(comment, () -> 2)));
+        executorService.execute(() -> postService.deleteComment(() -> 1, new Comment("Author1", "Text1", LocalDateTime.now())));
+        executorService.execute(() -> postService.deletePost(() -> 2, "Author post 2"));
 
-        Thread threadAddCommentsPost1 = new Thread(() -> {
-            try {
-                threadAddPost1.join();
-            } catch (InterruptedException e) {
-                throw new RuntimeException(e);
-            }
-            for (Comment comment : commentsForPost1) {
-                postService.addComment(comment, 1);
-            }
-        });
-
-        Thread threadAddCommentsPost2 = new Thread(() -> {
-            try {
-                threadAddPost2.join();
-            } catch (InterruptedException e) {
-                throw new RuntimeException(e);
-            }
-            for (Comment comment : commentsForPost2) {
-                postService.addComment(comment, 2);
-            }
-        });
-
-        threadAddPost1.start();
-        threadAddPost2.start();
-        threadAddCommentsPost1.start();
-        threadAddCommentsPost2.start();
-
-        threadAddCommentsPost1.join();
-        threadAddCommentsPost2.join();
-
+        executorService.shutdown();
+        executorService.awaitTermination(15, TimeUnit.SECONDS);
+        System.out.println(postService.getComments(() -> 1));
         System.out.println(postService.getPosts());
     }
 }
