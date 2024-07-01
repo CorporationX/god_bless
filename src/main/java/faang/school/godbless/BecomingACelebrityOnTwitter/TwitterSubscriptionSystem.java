8840da2dcc68272c2adfe7cee8ca4460ed5e24a7
Gevorg <a0964593@gmail.com>
2024-07-01 08:23:22 +0300

    BJS2-14178 - add .exceptionally to CompletableFuture addFollowerFuture

--- src/main/java/faang/school/godbless/BecomingACelebrityOnTwitter/TwitterSubscriptionSystem.java
+++ src/main/java/faang/school/godbless/BecomingACelebrityOnTwitter/TwitterSubscriptionSystem.java
@@ -11,19 +11,24 @@ import java.util.concurrent.Executors;
 
 @Slf4j
 public class TwitterSubscriptionSystem {
-    private static final Random random = new Random();
+    private static final int THREAD_SLEEP = 1000;
     private static final int NUM_THREADS = 13;
     private static final ExecutorService EXECUTOR = Executors.newFixedThreadPool(NUM_THREADS);
-    private final static List<CompletableFuture<Void>> allTwitterSubSystemFuture = new ArrayList<>();
+    private static final Random random = new Random();
+    private static final List<CompletableFuture<Integer>> allTwitterSubSystemFuture = new ArrayList<>();
 
     public static void addFollower(TwitterAccount twitterAccount) {
-        CompletableFuture<Void> addFollowerFuture = CompletableFuture.runAsync(() -> {
+        CompletableFuture<Integer> addFollowerFuture = CompletableFuture.supplyAsync(() -> {
             try {
-                Thread.sleep(random.nextInt(1000));
+                Thread.sleep(random.nextInt(THREAD_SLEEP));
             } catch (InterruptedException e) {
                 throw new RuntimeException(e);
             }
             twitterAccount.getFollowers().incrementAndGet();
+            return 1;
+        }).exceptionally(ex -> {
+            log.error(".sleep in addFollower method was interrupted while waiting");
+            return -1;
         });
         allTwitterSubSystemFuture.add(addFollowerFuture);
     }
