98459b14247497211f402af0627afb5217a936ee
shade2903 <nightshade2903@gmail.com>
2024-07-01 12:10:35 +0300

    BJS2-13330 Async and Future
    Refactoring task

--- src/main/java/faang/school/godbless/async/async_and_future/Main.java
+++ src/main/java/faang/school/godbless/async/async_and_future/Main.java
@@ -5,12 +5,6 @@ import java.util.concurrent.ExecutionException;
 public class Main {
     public static void main(String[] args) {
         MasterCardService masterCardService = new MasterCardService();
-        try {
             masterCardService.doAll();
-        } catch (ExecutionException e) {
-            throw new RuntimeException(e);
-        } catch (InterruptedException e) {
-            throw new RuntimeException(e);
-        }
     }
 }
--- src/main/java/faang/school/godbless/async/async_and_future/MasterCardService.java
+++ src/main/java/faang/school/godbless/async/async_and_future/MasterCardService.java
@@ -9,36 +9,59 @@ public class MasterCardService {
     private final int POOL_SIZE = 2;
     private final ExecutorService SINGLE_EXECUTOR = Executors.newSingleThreadExecutor();
     private final ExecutorService EXECUTOR = Executors.newFixedThreadPool(POOL_SIZE);
+    private final int DURATION_COLLECTED_PAYMENT = 10_000;
+    private final int DURATION_SEND_ANALYSTICS = 1000;
 
-    public void doAll() throws ExecutionException, InterruptedException {
-
+    public void doAll() {
         Future<Integer> payment = SINGLE_EXECUTOR.submit(this::collectPayment);
         CompletableFuture<Integer> analytics = CompletableFuture.supplyAsync(this::sendAnalystics, EXECUTOR);
 
-        log.info(String.format("Result analytics %d", analytics.get()));
+        log.info(String.format("Result analytics %d", analytics.join()));
+        try {
+            log.info(String.format("Result payment %d", payment.get()));
+        } catch (InterruptedException e) {
+            log.error("Thread payment  was interrupted");
+            throw new RuntimeException("Thread payment  was interrupted", e);
+        } catch (ExecutionException e) {
+            log.error("Thread payment  was terminated with ExecutionException");
+            throw new RuntimeException("Thread payment  was terminated with ExecutionException", e);
+        }
 
-        log.info(String.format("Result payment %d", payment.get()));
+        try {
+            EXECUTOR.awaitTermination(2, TimeUnit.SECONDS);
+        } catch (InterruptedException e) {
+            log.error("Thread analytics  was interrupted when during execution await termination");
+            throw new RuntimeException("Thread analytics  was interrupted when during execution await termination", e);
+        }
+        try {
+            SINGLE_EXECUTOR.awaitTermination(2, TimeUnit.SECONDS);
+        } catch (InterruptedException e) {
+            log.error("Thread payment  was interrupted when during execution await termination");
+            throw new RuntimeException("Thread payment  was interrupted when during execution await termination", e);
+        }
         EXECUTOR.shutdown();
         SINGLE_EXECUTOR.shutdown();
     }
 
     public int collectPayment() {
         try {
-            Thread.sleep(10_000);
-            return 10_000;
+            Thread.sleep(DURATION_COLLECTED_PAYMENT);
+            return DURATION_COLLECTED_PAYMENT;
         } catch (InterruptedException e) {
             e.printStackTrace();
-            throw new RuntimeException();
+            log.error("Thread in collectPayment was interrupted");
+            throw new RuntimeException("Thread in collectPayment was interrupted", e);
         }
     }
 
     public int sendAnalystics() {
         try {
-            Thread.sleep(1_000);
-            return 1_000;
+            Thread.sleep(DURATION_SEND_ANALYSTICS);
+            return DURATION_SEND_ANALYSTICS;
         } catch (InterruptedException e) {
-            e.printStackTrace();
-            throw new RuntimeException();
+            Thread.currentThread().interrupt();
+            log.error("Thread interrupted during analytics sending");
+            throw new RuntimeException("Thread interrupted during analytics sending", e);
         }
     }
 }
