edb43c46a0505c50c0cff23d02fd3297d2209b14
Murzin34 <108129795+Murzin34@users.noreply.github.com>
2024-08-02 20:53:41 +0300

    BJS2-18815 Развернитесь

---
+++ src/main/java/faang/school/godbless/reverse/Example.java
@@ -0,0 +1,11 @@
+package faang.school.godbless.reverse;
+
+public class Example {
+    public static int[] reverse(int[] nums) {
+        int[] reverseNums = new int[nums.length];
+        for (int i = nums.length - 1, j = 0; i >= 0; i--, j++) {
+            reverseNums[j] = nums[i];
+        }
+        return reverseNums;
+    }
+}
---
+++ src/main/java/faang/school/godbless/reverse/Main.java
@@ -0,0 +1,13 @@
+package faang.school.godbless.reverse;
+
+import java.util.Arrays;
+
+import static faang.school.godbless.reverse.Example.reverse;
+
+public class Main {
+    public static void main(String[] args) {
+        int[] nums = {1, 2, 3, 4, 5};
+
+        System.out.println(Arrays.toString(reverse(nums)));
+    }
+}
