dd60d771b5a4dc9e389f7678745c17ab18d702cf
Sultan <nikowosuka@gmail.com>
2024-08-02 23:57:11 +0500

    Collecting users task resolve

---
+++ src/Test/java/faang/school/godbless/collectingUsers/UserTest.java
@@ -0,0 +1,107 @@
+package faang.school.godbless.collectingUsers;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class UserTest {
+
+    private Set<String> activitiesForMethod;
+    private User user;
+    private List<User> users;
+    private Map<User, String> expected;
+    private Map<User, String> actual;
+
+    User user1 = new User(1, "Mike", 34, new HashSet<>(Set.of("Some activities", "Some activities1", "Some activities3", "Some activities2")));
+    User user2 = new User(2, "Bob", 23, new HashSet<>(Set.of("Some activities", "Some activities1", "Gym")));
+    User user3 = new User(3, "Jack", 41, new HashSet<>(Set.of("Some activities", "Some activities1")));
+    User user4 = new User(4, "Sam", 18, new HashSet<>(Set.of("Some activities", "Playing", "Some activities1")));
+    User user5 = new User(5, "Amanda", 24, new HashSet<>(Set.of("Some activities4", "Some activities", "Some activities1")));
+    User user6 = new User(6, "Mary", 19, new HashSet<>(Set.of("Some activities", "Some activities1", "Some activities2", "Some activities3")));
+    User user7 = new User(7, "Abby", 34, new HashSet<>(Set.of("Some activities", "Some activities1", "Some activities3", "Some activities2")));
+    User user8 = new User(8, "Tom", 23, new HashSet<>(Set.of("Some activities", "Some activities1", "Gym")));
+    User user9 = new User(9, "Petter", 41, new HashSet<>(Set.of("Some activities", "Some activities1")));
+    User user10 = new User(10, "Sam", 18, new HashSet<>(Set.of("Some activities", "Cooking", "Some activities1")));
+    User user11 = new User(11, "Lucy", 24, new HashSet<>(Set.of("Some activities3", "Some activities", "Some activities1")));
+    User user12 = new User(12, "Katy", 19, new HashSet<>(Set.of("Some activities", "Some activities1", "Some activities2", "Sleeping")));
+
+    @BeforeEach
+    public void setUp() {
+        activitiesForMethod = new HashSet<>(Set.of("Gym", "Cooking", "Playing", "Sleeping"));
+        user = new User(0, "Name", 0, activitiesForMethod);
+        users = List.of(
+                user1,
+                user2,
+                user3,
+                user4,
+                user5,
+                user6,
+                user7,
+                user8,
+                user9,
+                user10,
+                user11,
+                user12
+        );
+    }
+
+    @Test
+    public void testFindHobbyLovers() {
+        expected = Map.of(
+                user12, "Sleeping",
+                user10, "Cooking",
+                user8, "Gym",
+                user4, "Playing",
+                user2, "Gym"
+        );
+
+        actual = user.findHobbyLovers(users, activitiesForMethod);
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void testFindHobbyLovers_nullUsers() {
+        expected = null;
+
+        actual = user.findHobbyLovers(null, activitiesForMethod);
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void testFindHobbyLovers_nullActivities() {
+        expected = null;
+
+        actual = user.findHobbyLovers(users, null);
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void testFindHobbyLovers_emptyActivities() {
+        expected = new HashMap<>();
+
+        actual = user.findHobbyLovers(users, new HashSet<>());
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void testFindHobbyLovers_emptyUsers() {
+        expected = new HashMap<>();
+
+        actual = user.findHobbyLovers(new ArrayList<User>(), activitiesForMethod);
+
+        assertEquals(expected, actual);
+    }
+}
---
+++ src/main/java/faang/school/godbless/collectingUsers/User.java
@@ -0,0 +1,53 @@
+package faang.school.godbless.collectingUsers;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Objects;
+import java.util.Set;
+
+@AllArgsConstructor
+public class User {
+
+    private int id;
+
+    @Getter
+    private String name;
+
+    private int age;
+
+    @Getter
+    private Set<String> activities;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        User user = (User) o;
+        return id == user.id && age == user.age && Objects.equals(name, user.name) && Objects.equals(activities, user.activities);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, name, age, activities);
+    }
+
+    public Map<User, String> findHobbyLovers(List<User> users, Set<String> activities) {
+        if(users == null || activities == null)
+            return null;
+        Map<User, String> userActivityMap = new HashMap<>();
+        users.forEach(user -> {
+            user.getActivities().forEach(activity -> {
+                if(activities.contains(activity)) {
+                    userActivityMap.put(user, activity);
+                    return;
+                }
+            });
+        });
+
+        return userActivityMap;
+    }
+}
