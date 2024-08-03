853de7a0c30565899c390cb81078e256c705fa2e
Vitaly Maslyuk <maslyuk03@bk.ru>
2024-08-04 02:01:23 +0700

    Refactoring the code

--- src/main/java/users_registration/Main.java
+++ src/main/java/users_registration/Main.java
@@ -1,8 +1,7 @@
-package faang.school.godbless.UsersRegistration;
+package users_registration;
 
 public class Main {
     public static void main(String[] args) {
-
         User correctUser = new User("Name", 20, "Google", "London");
         User incorrectUserName = new User("  ", 20, "Google", "London");
         User incorrectUserAge = new User("Name", 15, "Google", "London");
--- src/main/java/users_registration/User.java
+++ src/main/java/users_registration/User.java
@@ -1,4 +1,4 @@
-package faang.school.godbless.UsersRegistration;
+package users_registration;
 
 import lombok.Data;
 
@@ -6,7 +6,6 @@ import java.util.Set;
 
 @Data
 public class User {
-
     private static final Set<String> VALID_JOBS = Set.of("Google", "Uber", "Amazon");
     private static final Set<String> VALID_ADDRESSES = Set.of("London", "New York", "Amsterdam");
 
@@ -15,9 +14,9 @@ public class User {
     private String job;
     private String address;
 
-
     public User(String name, int age, String job, String address) {
-        if(name.trim().isEmpty() || age < 18 || !VALID_JOBS.contains(job) || !VALID_ADDRESSES.contains(address)){
+        if (name.trim().isEmpty() || age < 18
+                || !VALID_JOBS.contains(job) || !VALID_ADDRESSES.contains(address)) {
             throw new IllegalArgumentException();
         }
         this.name = name;
