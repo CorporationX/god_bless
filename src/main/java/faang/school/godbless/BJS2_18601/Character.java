a83b66c838a5c39dd8ea358bf87dd34b72cb9cc9
DenysDovgan <12qwa0@ukr.net>
2024-08-03 18:03:55 +0200

    Task BJS2-18601 completed

---
+++ src/main/java/faang/school/godbless/BJS2_18601/Character.java
@@ -0,0 +1,31 @@
+package faang.school.godbless.BJS2_18601;
+
+public abstract class Character {
+    protected String name;
+    protected int strength;
+    protected int dexterity;
+    protected int intelligence;
+    protected int health = 100;
+
+    public Character(String name) {
+        this.name = name;
+    }
+
+    public int getHealth() {
+        return health;
+    }
+
+    public void setHealth(int health) {
+        this.health = health;
+    }
+
+
+    public Character(String name, int strength, int dexterity, int intelligence) {
+        this.name = name;
+        this.strength = strength;
+        this.dexterity = dexterity;
+        this.intelligence = intelligence;
+    }
+
+    public abstract void attack(Character c);
+}
