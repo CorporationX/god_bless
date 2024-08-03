2447977d32cb3967efb6ae625af18430afdf5ca9
Maksim Kononov <07maks7@mail.ru>
2024-08-03 22:33:37 +0300

    Написал программу, которая хранит информацию о домах Вестероса и их гербах в виде HashMap, и методы для взамодействия с ними.

---
+++ src/main/java/faang/school/godbless/House.java
@@ -0,0 +1,15 @@
+package faang.school.godbless;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class House {
+    private String name;
+    private String sigil;
+}
