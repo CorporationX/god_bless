6db0653d989aca172ee789dce41c4d3dd7b00a23
Denis Alginin <AlgininDK@gmail.com>
2024-08-03 19:00:22 +0500

    Test1

---
+++ src/main/java/faang/school/godbless/Denalg/Main.java
@@ -0,0 +1,26 @@
+//package faang.school.godbless.Denalg;
+//
+//import java.util.Arrays;
+//import java.util.HashSet;
+//import java.util.Map;
+//import java.util.Set;
+//
+//public class Main {
+//    public static void main(String[] args){
+//        Set<String> activity1 = new HashSet<>();
+//        Set<String> activity2 = new HashSet<>();
+//
+//        activity1.add("football");
+//        activity1.add("voleyball");
+//
+//        activity2.add("voleyball");
+//        activity2.add("basketball");
+//
+//        User user1 = new User(1, "Beks", 19, activity1);
+//        User user2 = new User(2, "Sula", 28, activity2);
+//
+//        Map<User, String> result = User.findHobbyLovers(Arrays.asList(user1, user2), new HashSet<>(Arrays.asList("voleyball", "swimming")));
+//
+//        System.out.println(result.toString());
+//    }
+//}
