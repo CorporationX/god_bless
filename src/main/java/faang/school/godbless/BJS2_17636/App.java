236ea29b354ed4a284d120718941420ab34d5fbd
Peregruzochka <peregruzochka48@mail.ru>
2024-08-03 11:08:21 +0300

    Создание класса App и main-метода, который выводит сообщение "Открытие магазина ожидается через 2 месяца" в консоль

---
+++ src/main/java/faang/school/godbless/BJS2_17636/App.java
@@ -0,0 +1,7 @@
+package faang.school.godbless.BJS2_17636;
+
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Открытие магазина ожидается через 2 месяца");
+    }
+}
