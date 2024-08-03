1423a221559f31870f5adade6998909cf5c60ff3
Evgenii Shevchenko <evgeny.shevchenko@webware24.de>
2024-08-03 13:44:57 +0200

    add author to Clean Architecture

--- src/main/java/faang/school/godbless/bookshop/step4/App.java
+++ src/main/java/faang/school/godbless/bookshop/step4/App.java
@@ -2,5 +2,6 @@ package faang.school.godbless.bookshop.step4;
 
 public class App {
     public static void main(String[] args) {
+        System.out.println("Clean Architecture - Robert MArtin");
     }
 }
