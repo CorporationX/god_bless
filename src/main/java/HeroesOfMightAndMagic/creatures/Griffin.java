2129e1f2fdae4199fb129fcc2c3b31014e5bbacd
Vitaly Maslyuk <maslyuk03@bk.ru>
2024-08-03 23:18:30 +0700

    Solved BJS2-18840

---
+++ src/main/java/HeroesOfMightAndMagic/creatures/Griffin.java
@@ -0,0 +1,14 @@
+package HeroesOfMightAndMagic.creatures;
+
+public class Griffin extends Creature {
+    public Griffin(String name) {
+        super(name);
+        this.armor = 5;
+        this.damage = 70;
+        this.health = 300;
+        this.level = 5;
+        this.speed = 70;
+    }
+
+
+}
