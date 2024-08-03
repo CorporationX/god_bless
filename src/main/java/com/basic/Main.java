472553ea60b5d47e22c34bdcbb592a72cadde051
Evgen <fragmatik.Oo@yandex.ru>
2024-08-03 22:19:12 +0300

    done

---
+++ src/main/java/com/basic/Main.java
@@ -0,0 +1,23 @@
+package com.basic;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class Main {
+    public static void main(String[] args) {
+        List<String> allActivities = List.of("football", "tennis", "basketball", "hockey");
+
+        User user = new User();
+
+        User userOne = new User(1, "David", 28, List.of("tennis", "box", "hockey"));
+        User userTwo = new User(2, "Vlad", 22, List.of("football", "running"));
+        User userTree = new User(3, "Denis", 31, List.of("snowboarding"));
+
+        List<User> userList = List.of(userOne, userTwo, userTree);
+
+        System.out.println(user.findHobbyLovers(userList, allActivities));
+
+        }
+}
---
+++ src/main/java/com/basic/User.java
@@ -0,0 +1,37 @@
+package com.basic;
+
+import lombok.*;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@Data
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+public class User {
+    private int id;
+    private String name;
+    private int age;
+    private List<String> activities;
+
+    public Map<User, String> findHobbyLovers(List<User> users, List<String> allActivities) {
+        Map<User, String> userActivitiesMap = new HashMap<>();
+        if (allActivities != null) {
+            for (User user : users) {
+                for (String activ : allActivities) {
+                    if (user.getActivities().contains(activ)) {
+                        userActivitiesMap.put(user, activ);
+                    }
+                }
+            }
+            return userActivitiesMap;
+        }
+        return null;
+    }
+
+}
+
+
