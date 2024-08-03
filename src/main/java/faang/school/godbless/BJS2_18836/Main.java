3c956acf679df880f2c9323f793ec6c634485bd7
Andrew Romanov <r0m4n0v4v@gmail.com>
2024-08-03 17:25:51 +0300

    BJS2-18836 done

---
+++ src/main/java/faang/school/godbless/BJS2_18836/Main.java
@@ -0,0 +1,9 @@
+package faang.school.godbless.BJS2_18836;
+
+public class Main {
+
+    public static void main(String[] args) {
+        int[] array = new int[]{1, 2, 3, 4, 5};
+        Example.reverse(array);
+    }
+}
