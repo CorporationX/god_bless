fb0bf8d6d540112c41069652aa2c5c30d49cbd49
Evgenii Shevchenko <evgeny.shevchenko@webware24.de>
2024-08-03 13:46:40 +0200

    add authors to Spring in Action

---
+++ src/main/java/faang/school/godbless/bookshop/step4/App.java
@@ -0,0 +1,8 @@
+package faang.school.godbless.bookshop.step4;
+
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Clean Architecture - Robert MArtin");
+        System.out.println("Head First Java - Kathy Sierra, Bert Bates, Trisha Gee");
+    }
+}
