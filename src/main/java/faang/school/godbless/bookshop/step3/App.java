f1d87088134c2b0c73be4dc03268eb7e1e15491d
Evgenii Shevchenko <evgeny.shevchenko@webware24.de>
2024-08-03 13:31:41 +0200

    Add App.java

---
+++ src/main/java/faang/school/godbless/bookshop/step3/App.java
@@ -0,0 +1,7 @@
+package faang.school.godbless.bookshop.step3;
+
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Открытие магазина ожидается через 2 месяца");
+    }
+}
