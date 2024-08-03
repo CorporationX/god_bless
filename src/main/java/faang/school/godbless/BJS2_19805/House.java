2ad5ecb6836642d9af8398fbf1345f07a7454b4b
Denis Artamonov <dns.artamonov@gmail.com>
2024-08-03 20:44:09 +0300

    BJS2-19805 complete

---
+++ src/main/java/faang/school/godbless/BJS2_19805/House.java
@@ -0,0 +1,11 @@
+package faang.school.godbless.BJS2_19805;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@AllArgsConstructor
+@Getter
+public class House {
+    private String name;
+    private String sigil;
+}
