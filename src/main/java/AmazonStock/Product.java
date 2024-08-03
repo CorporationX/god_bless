6aa629259e19ad902fb35c7bfb9d616dd2498866
Vitaly Maslyuk <maslyuk03@bk.ru>
2024-08-03 15:02:36 +0700

    Solved BJS2-19280

---
+++ src/main/java/AmazonStock/Product.java
@@ -0,0 +1,14 @@
+package AmazonStock;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class Product {
+    private int id;
+    private String name;
+    private String category;
+
+
+}
