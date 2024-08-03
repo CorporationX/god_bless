ebc179e5f730dcef59aad8f13950e56b019fe2fe
Evgenii Shevchenko <evgeny.shevchenko@webware24.de>
2024-08-03 21:07:08 +0200

    Revert "add authors to Spring in Action"
    This reverts commit fb0bf8d6d540112c41069652aa2c5c30d49cbd49.

--- src/main/java/faang/school/godbless/bookshop/step4/App.java
+++
@@ -1,8 +0,0 @@
-package faang.school.godbless.bookshop.step4;
-
-public class App {
-    public static void main(String[] args) {
-        System.out.println("Clean Architecture - Robert MArtin");
-        System.out.println("Head First Java - Kathy Sierra, Bert Bates, Trisha Gee");
-    }
-}
