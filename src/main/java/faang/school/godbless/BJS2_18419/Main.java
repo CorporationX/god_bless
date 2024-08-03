4bc485c468b0100b5d279d64719f96289d651a2b
Denis Artamonov <dns.artamonov@gmail.com>
2024-08-03 12:34:52 +0300

    BJS2-18419 complete

--- src/main/java/faang/school/godbless/BJS2_18419/Main.java
+++
@@ -1,8 +0,0 @@
-package faang.school.godbless.BJS2_18419;
-
-public class Main {
-    public static void main(String[] args) {
-        String str=null;
-        System.out.println(str.isEmpty());
-    }
-}
