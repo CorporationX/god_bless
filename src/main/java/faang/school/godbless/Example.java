5b5d01f09e1c840a67ead21ad388d58bafb384b4
pgusev <pgusev@fil-it.ru>
2024-08-03 18:34:55 +0300

    BJS2-18854 - Добавлен класс Example и реализован метод reverse для разворачивания  массива

---
+++ src/main/java/faang/school/godbless/Example.java
@@ -0,0 +1,13 @@
+package faang.school.godbless;
+
+public class Example {
+
+    public static void reverse(int[] incomingArray) {
+
+        for (int i = 0; i < incomingArray.length / 2; i++) {
+            int temp = incomingArray[i];
+            incomingArray[i] = incomingArray[incomingArray.length - 1 - i];
+            incomingArray[incomingArray.length - 1 - i] = temp;
+        }
+    }
+}
