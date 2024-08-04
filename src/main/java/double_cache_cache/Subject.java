e2539be35b6e456d1ff06e56a877aa0b661127f6
Vitaly Maslyuk <maslyuk03@bk.ru>
2024-08-04 18:10:05 +0700

    Solved BJS2-19684

---
+++ src/main/java/double_cache_cache/Subject.java
@@ -0,0 +1,11 @@
+package double_cache_cache;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class Subject {
+    private long id;
+    private String name;
+}
