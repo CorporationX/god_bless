b400fc755d847bcd611eca6b6feca80a3512e24d
Alena Guskova <amguskova@edu.hse.ru>
2024-08-04 19:16:18 +0500

    BJS2-18454 Регистрация пользователей

---
+++ src/main/java/faang/school/godbless/registration/User.java
@@ -0,0 +1,26 @@
+package faang.school.godbless.registration;
+
+import lombok.Getter;
+
+import java.util.*;
+
+@Getter
+public class User {
+    private static final Set<String> VALID_JOBS = new HashSet<>(Arrays.asList("Google", "Uber", "Amazon"));
+    private static final Set<String> VALID_ADDRESSES = new HashSet<>(Arrays.asList("London", "New York", "Amsterdam"));
+    private String name;
+    private int age;
+    private String company;
+    private String address;
+
+    public User(String name, int age, String company, String address) throws IllegalArgumentException {
+        if (name.isEmpty() || age < 18 || !VALID_JOBS.contains(company) || !VALID_ADDRESSES.contains(address)) {
+            throw new IllegalArgumentException();
+        } else {
+            this.name = name;
+            this.age = age;
+            this.company = company;
+            this.address = address;
+        }
+    }
+}
