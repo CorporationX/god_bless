dd7babe9354d57e880b630edbd462a797027d346
Semen Plekhanov <semen-5555@mail.ru>
2024-08-04 19:57:03 +0300

    BJS2-18374 Коллекционируем пользователей правки по коментариям

--- src/main/java/collectible/users/User.java
+++ src/main/java/collectible/users/User.java
@@ -15,22 +15,22 @@ public class User {
     private final int id;
     private final String name;
     private final int age;
-    private final List<String> activity;
+    private final Set<String> activities;
 
-    public static Map<User, String> findHobbyLovers(List<User> users, List<String> activity) {
-        HashMap<User, String> findUsers = new HashMap<>();
+    public static Map<User, String> findHobbyLovers(List<User> users, Set<String> activities) {
+        Map<User, String> hobbyLovers = new HashMap<>();
 
         for (User user : users) {
-            Set<String> intersection = new HashSet<>(user.activity);
-            intersection.retainAll(activity);
+            Set<String> intersection = new HashSet<>(user.activities);
+            intersection.retainAll(activities);
 
             if (intersection.isEmpty()) {
                 continue;
             }
 
-            findUsers.put(user, intersection.iterator().next());
+            hobbyLovers.put(user, intersection.iterator().next());
         }
 
-        return findUsers;
+        return hobbyLovers;
     }
 }
