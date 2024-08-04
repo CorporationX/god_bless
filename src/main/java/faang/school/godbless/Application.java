55adab5c63b6a67538657f82f8ea65ae46ce06cd
Ilya Merkulov <imerkulov@fun.co>
2024-08-04 17:13:43 +0300

    Implement Task 1: Collecting users

--- src/main/java/faang/school/godbless/Application.java
+++ src/main/java/faang/school/godbless/Application.java
@@ -1,8 +1,6 @@
 package faang.school.godbless;
 
-import java.util.List;
-import java.util.Map;
-import java.util.Set;
+import java.util.*;
 
 public class Application {
     public static void main(String[] args) {
