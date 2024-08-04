09f70452a5ace8a56c5838f2948112133651870d
Peregruzochka <peregruzochka48@mail.ru>
2024-08-04 18:34:04 +0300

    Создание класса FacultyAndAdmissionYearPair

---
+++ src/main/java/faang/school/godbless/BJS2_19386/FacultyAndAdmissionYearPair.java
@@ -0,0 +1,12 @@
+package faang.school.godbless.BJS2_19386;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class FacultyAndAdmissionYearPair {
+    private String faculty;
+    private int admissionYear;
+}
