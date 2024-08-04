0060c420e1ca71a0ecc041de051a4bd19f5ca56e
Svirlex <aleksandrsviridov446@gmail.com>
2024-08-04 12:34:03 +0300

    Created groupUsers method

--- src/main/java/faang/school/godbless/Application.java
+++ src/main/java/faang/school/godbless/Application.java
@@ -1,7 +1,22 @@
 package faang.school.godbless;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+
 public class Application {
     public static void main(String[] args) {
-        System.out.println("God Bless!");
+        User user1 = new User("Emily", 28);
+        User user2 = new User("Michael", 35);
+        User user3 = new User("Sophia", 22);
+        User user4 = new User("James", 28);
+        User user5 = new User("Olivia", 35);
+
+        ArrayList<User> usersList = new ArrayList<>();
+        Collections.addAll(usersList, user1, user2, user3, user4, user5);
+
+        System.out.println(User.groupUsers(usersList));
+
     }
 }
