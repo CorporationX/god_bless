ddd8a5ef7c37aa249ebab2030de9801e4eddbe2a
Maksim Kononov <07maks7@mail.ru>
2024-08-04 12:28:31 +0300

    Написал программу, которая хранит информацию о библиотеке королевства, используя HashMap, и методы для работы с ней.

---
+++ src/main/java/faang/school/godbless/Book.java
@@ -0,0 +1,25 @@
+package faang.school.godbless;
+
+
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@EqualsAndHashCode
+@ToString
+public class Book {
+    private String title;
+    private String author;
+    private int year;
+
+    public Book(String title, String author, int year) {
+        this.title = title;
+        this.author = author;
+        this.year = year;
+    }
+}
