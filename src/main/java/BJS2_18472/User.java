e52feb56e05aeb67b92ce0f04bee1094ed60a609
avoevoda <voevodacode@gmail.com>
2024-08-04 22:41:05 +0300

    User registration

---
+++ src/main/java/BJS2_18472/User.java
@@ -0,0 +1,55 @@
+package BJS2_18472;
+
+import lombok.Data;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
+@Data
+public class User {
+    private static final Set<String> VALID_JOBS = Set.of("Google", "Uber", "Amazon");
+    private static final Set<String> VALID_ADDRESSES = Set.of("London", "New York", "Amsterdam");
+    private String name;
+    private int age;
+    private String job;
+    private String address;
+
+    public User(String name, int age, String job, String address) {
+        if (name == null || name.isEmpty()) {
+            throw new IllegalArgumentException("Name cannot be null or empty");
+        }
+
+        if (age < 18) {
+            throw new IllegalArgumentException("Must be at least 18 years old.");
+        }
+
+        if (!VALID_JOBS.contains(job)) {
+            throw new IllegalArgumentException("Invalid job");
+        }
+
+        if (!VALID_ADDRESSES.contains(address)) {
+            throw new IllegalArgumentException("Invalid address");
+        }
+
+        this.name = name;
+        this.age = age;
+        this.job = job;
+        this.address = address;
+    }
+
+    public static Map<Integer, List<User>> groupUsers(List<User> users) {
+        final HashMap<Integer, List<User>> groupedUsers = new HashMap<>();
+
+        for (User user : users) {
+            if (!groupedUsers.containsKey(user.getAge())) {
+                groupedUsers.put(user.getAge(), new ArrayList<>());
+            }
+            groupedUsers.get(user.getAge()).add(user);
+        }
+
+        return groupedUsers;
+    }
+}
