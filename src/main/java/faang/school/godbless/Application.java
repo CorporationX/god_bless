d04ee9a5fdf44472fe73493c02be1578b503bf53
Sergey Sklyar <sklyar1091@gmail.com>
2024-08-04 16:39:57 +0300

    Task "Развернитесь!"

--- src/main/java/faang/school/godbless/Application.java
+++ src/main/java/faang/school/godbless/Application.java
@@ -1,7 +1,10 @@
 package faang.school.godbless;
 
+import java.util.Arrays;
+
+
 public class Application {
     public static void main(String[] args) {
-        System.out.println("God Bless!");
+        System.out.println("Hi!");
     }
 }
