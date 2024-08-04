494720ff6a14d2bcf49507640c03ad2f5929f2b0
Prigozhaeva-Anastasia <nastaprigozaeva20@gmail.com>
2024-08-04 21:44:42 +0300

    Reverse

---
+++ src/main/java/faang/school/godbless/BJS2_18897/Main.java
@@ -0,0 +1,11 @@
+package faang.school.godbless.BJS2_18897;
+
+public class Main {
+    public static void main(String[] args) {
+        int[] nums = {2, 6, 4, 7, 1, 6};
+        int[] reversedNums = Example.reverse(nums);
+        for (int num : reversedNums) {
+            System.out.print(num + " ");
+        }
+    }
+}
