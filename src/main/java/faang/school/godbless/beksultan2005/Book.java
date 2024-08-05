5eb6fffa24082c2aeb30129062f8506c79973b35
aephrodita <146974539+aephrodita@users.noreply.github.com>
2024-08-05 14:03:14 +0500

    .

--- src/main/java/faang/school/godbless/beksultan2005/Book.java
+++ src/main/java/faang/school/godbless/beksultan2005/Book.java
@@ -1,7 +1,9 @@
 package faang.school.godbless.beksultan2005;
 
 import lombok.AllArgsConstructor;
+import lombok.ToString;
 
+@ToString
 @AllArgsConstructor
 public class Book {
     private String title;
--- src/main/java/faang/school/godbless/beksultan2005/Main.java
+++ src/main/java/faang/school/godbless/beksultan2005/Main.java
@@ -14,14 +14,32 @@ public class Main {
         cacheBook.remove(new Book(title, author, year));
     }
 
-    public String getLocation(String title, String author, int year){
+    public static String getLocation(String title, String author, int year){
         return cacheBook.get(new Book(title, author, year));
     }
 
-    public
+    public static void getAllCacheBook(){
+        cacheBook.forEach((key, value) -> System.out.println(key.toString() + ": " + value));
+    }
 
 
     public static void main(String[] args) {
+        Book book1 = new Book("Title1", "Author1", 2001);
+        Book book2 = new Book("Title2", "Author2", 2002);
+        Book book3 = new Book("Title3", "Author3", 2003);
 
+        addBook(book1, "Shelf1");
+        addBook(book2, "Shelf2");
+        addBook(book3, "Shelf3");
+
+        System.out.println("All books in cache:");
+        getAllCacheBook();
+
+        System.out.println("\nLocation of Title2 by Author2, 2002: " + getLocation("Title2", "Author2", 2002));
+
+        removeBook("Title1", "Author1", 2001);
+
+        System.out.println("\nAll books in cache after removing Title1:");
+        getAllCacheBook();
     }
 }
