aee4a09c90795c447e942ef260f6a19ef9135458
ifef228 <ifef228@gmail.com>
2024-08-05 18:25:00 +0300

    Абстракция, абстракция!

---
+++ src/main/java/faang/school/godbless/abstraction/Main.java
@@ -0,0 +1,10 @@
+package faang.school.godbless.abstraction;
+
+public class Main {
+    public static void main(String[] args) {
+        Warrior warrior = new Warrior("warrior");
+        Archer archer = new Archer("archer");
+        archer.attack(warrior);
+        warrior.attack(archer);
+    }
+}
