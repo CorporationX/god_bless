a9ac20e8f03fde094b937f2bf9561c768f9803ea
PLSGOTOASS <koteykaarisha@mail.ru>
2024-08-05 23:03:29 +0300

    Поправил скобки

--- src/main/java/Reverse/Example.java
+++ src/main/java/Reverse/Example.java
@@ -1,10 +1,8 @@
 package Reverse;
 
 public class Example {
-    public static void reverse(int[] inputArray)
-    {
-        for (int start = 0, end = inputArray.length - 1; start < end; start++, end--)
-        {
+    public static void reverse(int[] inputArray) {
+        for (int start = 0, end = inputArray.length - 1; start < end; start++, end--) {
             int temp = inputArray[start];
             inputArray[start] = inputArray[end];
             inputArray[end] = temp;
