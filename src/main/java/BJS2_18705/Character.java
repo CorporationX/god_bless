018a943be3943ceeee403d8e8de0cf395b46ff1b
avoevoda <voevodacode@gmail.com>
2024-08-05 16:46:27 +0300

    Character and abstraction

---
+++ src/main/java/BJS2_18705/Character.java
@@ -0,0 +1,27 @@
+package BJS2_18705;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+abstract public class Character {
+    protected String name;
+    protected int strength;
+    protected int agility;
+    protected int intelligence;
+    protected int health = 100;
+
+    public Character(String name) {
+        this.name = name;
+    }
+
+    public Character(String name, int strength, int agility, int intelligence) {
+        this.name = name;
+        this.strength = strength;
+        this.agility = agility;
+        this.intelligence = intelligence;
+    }
+
+    public abstract void attack(Character character);
+}
