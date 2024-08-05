a13948680eb0b9cbb4b505f1ad3bf7f9beb62d8f
Denis Panasenko <panasenkodenis1995@gmail.com>
2024-08-05 21:36:52 +0300

    BJS2-18868 loop fix

--- src/main/java/faang/school/godbless/BJS2_18868/Example.java
+++ src/main/java/faang/school/godbless/BJS2_18868/Example.java
@@ -5,12 +5,11 @@ import java.util.Collections;
 
 public class Example {
     public static void reverse(int[] array){
-        int size = array.length;
         for (int i = 0; i < array.length / 2; i++) {
-            int temp = array[i];
-            array[i] = array[size - 1];
-            array[size - 1] = temp;
-            size--;
+            int firstElem = array[i];
+            int lastElem = array[array.length - 1 - i];
+            array[i] = lastElem;
+            array[array.length - 1 - i] = firstElem;
         }
     }
 }
