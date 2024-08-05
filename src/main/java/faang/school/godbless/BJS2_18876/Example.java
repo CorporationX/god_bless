e253b31a0a343d2450481c44e7e3cbaee087757c
Peregruzochka <peregruzochka48@mail.ru>
2024-08-05 17:31:40 +0300

    фоматирование кода

--- src/main/java/faang/school/godbless/BJS2_18876/Example.java
+++ src/main/java/faang/school/godbless/BJS2_18876/Example.java
@@ -2,11 +2,11 @@ package faang.school.godbless.BJS2_18876;
 
 public class Example {
     public static int[] reverse(int[] array) {
-        int[] reverse_array = new int[array.length];
+        int[] reverseArray = new int[array.length];
         for (int i = 0; i < array.length; i++) {
-            reverse_array[array.length - 1 - i] = array[i];
+            reverseArray[array.length - 1 - i] = array[i];
         }
-        return reverse_array;
+        return reverseArray;
     }
 }
 
--- src/main/java/faang/school/godbless/BJS2_18876/Main.java
+++ src/main/java/faang/school/godbless/BJS2_18876/Main.java
@@ -4,7 +4,7 @@ import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) {
-        int[] myArray = new int[] {1, 2, 3, 4, 5, 6, 7, 8, 9};
+        int[] myArray = new int[]{1, 2, 3, 4, 5, 6, 7, 8, 9};
         int[] myReverseArray = Example.reverse(myArray);
         System.out.println(Arrays.toString(myReverseArray));
     }
