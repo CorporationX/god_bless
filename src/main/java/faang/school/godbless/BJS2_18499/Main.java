d65735556aeef0bcb92ebd892d8aa922184d9f75
Amirhan Almeev <almeevamirhan294@gmail.com>
2024-08-05 14:14:57 +0300

    Регистрация пользователя решение

---
+++ src/main/java/faang/school/godbless/BJS2_18499/Main.java
@@ -0,0 +1,8 @@
+package faang.school.godbless.BJS2_18499;
+
+public class Main {
+    public static void main(String[] args) {
+        User user = new User("Vlad", 19, "Google", "Moscow");
+
+    }
+}
---
+++ src/main/java/faang/school/godbless/BJS2_18499/User.java
@@ -0,0 +1,25 @@
+package faang.school.godbless.BJS2_18499;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class User {
+    private String name;
+    private int age;
+    private String placeJob;
+    private String address;
+
+    private static final Set VALID_JOBS = Set.of("Google", "Uber", "Amazon");
+    private static final Set VALID_ADDRESSES = Set.of("London", "New York", "Amsterdam");
+
+    public User(String name, int age, String placeJob, String address) {
+        if (name.isEmpty() || age < 18 || !VALID_JOBS.contains(placeJob) || !VALID_ADDRESSES.contains(address) ) {
+            throw new IllegalArgumentException("Ошибка");
+        } else {
+            this.name = name;
+            this.age = age;
+            this.placeJob = placeJob;
+            this.address = address;
+        }
+    }
+}
