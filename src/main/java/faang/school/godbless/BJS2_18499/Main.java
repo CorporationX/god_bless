9526f22851f0f87f2831bf7eab50cb7b44bec26e
Amirhan Almeev <almeevamirhan294@gmail.com>
2024-08-05 14:15:32 +0300

    Регистрация пользователя решение

--- src/main/java/faang/school/godbless/BJS2_18499/Main.java
+++ src/main/java/faang/school/godbless/BJS2_18499/Main.java
@@ -3,6 +3,5 @@ package faang.school.godbless.BJS2_18499;
 public class Main {
     public static void main(String[] args) {
         User user = new User("Vlad", 19, "Google", "Moscow");
-
     }
 }
--- src/main/java/faang/school/godbless/BJS2_18499/User.java
+++ src/main/java/faang/school/godbless/BJS2_18499/User.java
@@ -1,6 +1,5 @@
 package faang.school.godbless.BJS2_18499;
 
-import java.util.HashSet;
 import java.util.Set;
 
 public class User {
