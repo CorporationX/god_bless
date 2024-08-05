b0062c3e72d18ecd70752595dde33189c215947e
Юрий <subbotin.yuriy3@wb.ru>
2024-08-05 18:25:03 +0300

    Fix remarks

--- src/main/java/faang/school/godbless/registration_users/Main.java
+++ src/main/java/faang/school/godbless/registration_users/Main.java
@@ -7,12 +7,13 @@ import java.util.Map;
 public class Main {
     public static void main(String[] args) {
         try {
-            List<User> users = new ArrayList<>(5);
-            users.add(new User("John", 20, "Google", "London"));
-            users.add(new User("Jane", 20, "Uber", "Amsterdam"));
-            users.add(new User("Bob", 30, "Amazon", "New York"));
-            users.add(new User("Alice", 30, "Google", "Amsterdam"));
-            users.add(new User("Mike", 17, "Microsoft", "Chicago"));
+            List<User> users = new ArrayList<>(List.of(
+                    new User("John", 20, "Google", "London"),
+                    new User("Jane", 20, "Uber", "Amsterdam"),
+                    new User("Bob", 30, "Amazon", "New York"),
+                    new User("Alice", 30, "Google", "Amsterdam")
+//                    new User("Mike", 17, "Microsoft", "Chicago")
+            ));
 
             Map<Integer, List<User>> groupedUsers = User.groupUsers(users);
 
@@ -20,10 +21,7 @@ public class Main {
                 System.out.println("Age: " + entry.getKey());
                 List<User> usersByAge = entry.getValue();
                 for (User user : usersByAge) {
-                    System.out.println("Name: " + user.getName());
-                    System.out.println("Place of Work: " + user.getPlaceOfWork());
-                    System.out.println("Address: " + user.getAddress());
-                    System.out.println();
+                    System.out.println(user);
                 }
             }
         } catch (IllegalArgumentException e) {
--- src/main/java/faang/school/godbless/registration_users/User.java
+++ src/main/java/faang/school/godbless/registration_users/User.java
@@ -2,6 +2,7 @@ package faang.school.godbless.registration_users;
 
 import lombok.Getter;
 import lombok.Setter;
+import lombok.ToString;
 
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -9,16 +10,18 @@ import java.util.HashMap;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
+import java.util.Set;
 
 @Getter
 @Setter
+@ToString
 public class User {
     private String name;
     private int age;
     private String placeOfWork;
     private String address;
 
-    private final HashSet<String> VALID_JOBS = new HashSet<>(
+    private final Set<String> VALID_JOBS = new HashSet<>(
             Arrays.asList("Google", "Uber", "Amazon")
     );
     private final HashSet<String> VALID_ADDRESSES = new HashSet<>(
@@ -26,10 +29,20 @@ public class User {
     );
 
     public User(String name, int age, String placeOfWork, String address) {
-        if (name.isEmpty() || age < 18 || !VALID_JOBS.contains(placeOfWork) || !VALID_ADDRESSES.contains(address)) {
-            throw new IllegalArgumentException(
-                    "Invalid input in user: " + name + ", " + age + ", " + placeOfWork + ", " + address
-            );
+        if (name.isEmpty()) {
+            throw new IllegalArgumentException("Name cannot be empty");
+        }
+
+        if (age < 18) {
+            throw new IllegalArgumentException("Age cannot be less than 18");
+        }
+
+        if (!VALID_JOBS.contains(placeOfWork)) {
+            throw new IllegalArgumentException("Invalid place of work");
+        }
+
+        if (!VALID_ADDRESSES.contains(address)) {
+            throw new IllegalArgumentException("Invalid address");
         }
 
         this.name = name;
@@ -38,18 +51,11 @@ public class User {
         this.address = address;
     }
 
-
     public static Map<Integer, List<User>> groupUsers(List<User> users) {
         Map<Integer, List<User>> groupedUsers = new HashMap<>();
 
         for (User user : users) {
-            if (!groupedUsers.containsKey(user.getAge())) {
-                List<User> usersByAge = new ArrayList<>();
-                usersByAge.add(user);
-                groupedUsers.put(user.getAge(), usersByAge);
-            } else {
-                groupedUsers.get(user.getAge()).add(user);
-            }
+            groupedUsers.computeIfAbsent(user.getAge(), k -> new ArrayList<>()).add(user);
         }
 
         return groupedUsers;
