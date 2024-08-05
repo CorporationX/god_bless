aae3462c18c7035fe5eca604c78fc9cd91eb1206
avoevoda <voevodacode@gmail.com>
2024-08-05 16:46:27 +0300

    Character and abstraction

---
+++ src/main/java/BJS2_18705/Archer.java
@@ -0,0 +1,12 @@
+package BJS2_18705;
+
+public class Archer extends Character {
+    public Archer(String name) {
+        super(name, 3, 10, 5);
+    }
+
+    @Override
+    public void attack(Character character) {
+        character.setHealth(character.getHealth() - this.getAgility());
+    }
+}
