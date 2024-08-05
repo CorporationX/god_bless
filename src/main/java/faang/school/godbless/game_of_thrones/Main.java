63af2fd50f6159de4bae0aa11a57b31737983eae
Юрий <subbotin.yuriy3@wb.ru>
2024-08-05 12:55:11 +0300

    BJS2-19828

---
+++ src/main/java/faang/school/godbless/game_of_thrones/Main.java
@@ -0,0 +1,46 @@
+package faang.school.godbless.game_of_thrones;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Main {
+    public static void main(String[] args) {
+        Map<String, House> houses = new HashMap<>();
+        houses.put("Stark", new House("Stark", "Winter is coming"));
+
+        add(houses, "Lannister", new House("Lannister", "Hear me Roar!"));
+        add(houses, "Targaryen", new House("Targaryen", "Fire and Blood"));
+        System.out.println(houses);
+
+        remove(houses, "Targaryen");
+        System.out.println(houses);
+
+        add(houses, "Baratheon", new House("Baratheon", "Ours is the Fury"));
+        printInfoAboutSigilHouse(houses, "Baratheon");
+
+        printAllInfoAboutHouse(houses);
+    }
+
+    public static void add(Map<String, House> houses, String name, House house) {
+        houses.put(name, house);
+    }
+
+    public static void remove(Map<String, House> houses, String name) {
+        houses.remove(name);
+    }
+
+    public static House search(Map<String, House> houses, String name) {
+        return houses.get(name);
+    }
+
+    public static void printInfoAboutSigilHouse(Map<String, House> houses, String name) {
+        House house = search(houses, name);
+        System.out.println(house.getSigil());
+    }
+
+    public static void printAllInfoAboutHouse(Map<String, House> houses) {
+        for (House house : houses.values()) {
+            System.out.println("House: " + house.getName() + ", Sigil: " + house.getSigil());
+        }
+    }
+}
