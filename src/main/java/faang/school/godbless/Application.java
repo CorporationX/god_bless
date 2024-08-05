ddf4ae88c188cdbaf5c4ab7988784f32f62debef
Svirlex <aleksandrsviridov446@gmail.com>
2024-08-05 19:55:27 +0300

    BJS2-19904 Created House class

--- src/main/java/faang/school/godbless/Application.java
+++ src/main/java/faang/school/godbless/Application.java
@@ -1,12 +1,34 @@
 package faang.school.godbless;
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
+import java.util.*;
 
 
 public class Application {
     public static void main(String[] args) {
         System.out.println("God_Bless");
+
+
+    }
+
+    public static void putHouse(Map map, String key, House house){
+        map.put(key, house);
+        System.out.println("House " + house.getName() + " has been putted in the list");
+    }
+
+    public static void deleteHouse(Map map, String name){
+        map.remove(name);
+        System.out.println("House " + name + " removed");
+    }
+
+    public static void getHouse(Map map, String name){
+        House house = (House) map.get(name);
+        System.out.println("House: " + house.getName() + "; Sigil: " + house.getSigil());
+    }
+
+    public static void getHousesList(Map<String, House> houses){
+        for (Map.Entry<String, House> pair : houses.entrySet()) {
+            System.out.println("House: " + pair.getValue().getName());
+            System.out.println("|_ Sigil: " + pair.getValue().getSigil());
+        }
     }
 }
