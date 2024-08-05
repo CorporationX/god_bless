6236e2209b07254db8e5bc22014ba1f4b504de0d
Ivan <spvanym@gmail.com>
2024-08-05 19:07:07 +0300

    Регистрация пользователя SMI

--- src/main/java/GroupUsers/Main.java
+++ src/main/java/GroupUsers/Main.java
@@ -6,19 +6,7 @@ import java.util.Map;
 
 public class Main {
     public static void main(String[] args) throws Exception {
-        List<User> userList = new ArrayList<>();
-        userList.add(new User("Ivan", 19, "BootCamp", "Kazan"));
-        userList.add(new User("Kolya", 19, "BootCamp", "Kazan"));
-        userList.add(new User("Kirill", 19, "BootCamp", "Kazan"));
-        userList.add(new User("Sasha", 20, "BootCamp", "Kazan"));
-        userList.add(new User("Max", 20, "BootCamp", "Kazan"));
-        userList.add(new User("Marat", 20, "BootCamp", "Kazan"));
-
-        Map<Integer, List<User>> test = User.groupUsers(userList);
-
-        for(Map.Entry<Integer, List<User>> pair : test.entrySet()){
-            System.out.println(pair.getKey() + " : " + pair.getValue());
-        }
+        User testUser = new User("ff", 19, "Google", "London");
 
     }
 }
