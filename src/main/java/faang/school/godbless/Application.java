74ee795278f566d8d881fb9503f6de226429ec53
Владислав Воронов <iPlesk90@yandex.ru>
2024-08-05 19:52:34 +0300

    Выполнил задачу №1 HashMap

--- src/main/java/faang/school/godbless/Application.java
+++ src/main/java/faang/school/godbless/Application.java
@@ -1,7 +1,35 @@
 package faang.school.godbless;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class Application {
     public static void main(String[] args) {
         System.out.println("God Bless!");
+
+        Map<String, House> greatHouses = new HashMap<>();
+
+        House house1 = new House("Stark", "Wolf");
+        House house2 = new House("Barateon", "Deer");
+        House house3 = new House("Grayjoy", "Kraken");
+        House house4 = new House("Lanister", "Lion");
+        House house5 = new House("Tirel", "Rose");
+        House house6 = new House("Martell", "Sun");
+
+        House.addHouse(greatHouses, house1);
+        House.addHouse(greatHouses, house2);
+        House.addHouse(greatHouses, house3);
+        House.addHouse(greatHouses, house4);
+        House.addHouse(greatHouses, house5);
+        House.addHouse(greatHouses, house6);
+
+        House.getAllHouses(greatHouses);
+        System.out.println();
+
+        House.delHouse(greatHouses, "Lanister");
+
+        House.getAllHouses(greatHouses);
+
+        House.getHouse(greatHouses, "Stark");
     }
 }
