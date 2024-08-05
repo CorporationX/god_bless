03b632c56c5d79495f3afb17341c657c43b357f4
Denis Artamonov <dns.artamonov@gmail.com>
2024-08-05 18:07:18 +0300

    BJS2-18419 bug fixes

--- src/test/java/faang/school/godbless/BJS2_18419/UserTest.java
+++ src/test/java/faang/school/godbless/BJS2_18419/UserTest.java
@@ -21,36 +21,23 @@ class UserTest {
     }
 
     @Test
-    @DisplayName("Создание пользователя с пустым именем")
-    public void testInvalidName() {
+    @DisplayName("Создание не валидных пользователей")
+    public void testInvalidUsersCreation() {
         Exception exception = assertThrows(IllegalArgumentException.class,
                 () -> new User("", 19, "Google", "London"));
-
         assertEquals("Name cannot be empty", exception.getMessage());
-    }
 
-    @Test
-    @DisplayName("Создание пользователя с возрастом меньше 18 лет")
-    public void testInvalidAge() {
-        Exception exception = assertThrows(IllegalArgumentException.class,
+        exception = assertThrows(IllegalArgumentException.class,
                 () -> new User("Alice", 15, "Google", "London"));
 
         assertEquals("Age must be at least 18", exception.getMessage());
-    }
 
-    @Test
-    @DisplayName("Создание пользователя с местом работы не из требуемого набора")
-    public void testInvalidJob() {
-        Exception exception = assertThrows(IllegalArgumentException.class,
+        exception = assertThrows(IllegalArgumentException.class,
                 () -> new User("Alice", 19, "Yandex", "London"));
 
         assertEquals("Work Place is not valid", exception.getMessage());
-    }
 
-    @Test
-    @DisplayName("Создание пользователя с адресом не из требуемого набора")
-    public void testInvalidAddress() {
-        Exception exception = assertThrows(IllegalArgumentException.class,
+        exception = assertThrows(IllegalArgumentException.class,
                 () -> new User("Alice", 21, "Google", "Saint-Petersburg"));
 
         assertEquals("Address is not valid", exception.getMessage());
