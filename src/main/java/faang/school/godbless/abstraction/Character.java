38de2778111f14b121d333a6414f21ad18bbf24f
Юрий <subbotin.yuriy3@wb.ru>
2024-08-05 11:39:05 +0300

    BJS2-18511

---
+++ src/main/java/faang/school/godbless/abstraction/Character.java
@@ -0,0 +1,27 @@
+package faang.school.godbless.abstraction;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Setter
+@Getter
+abstract public class Character {
+    private String name;
+    private int power;
+    private int dexterity;
+    private int intelligence;
+    private int health = 100;
+
+    public Character(String name) {
+        this.name = name;
+    }
+
+    public Character(String name, int power, int dexterity, int intelligence) {
+        this.name = name;
+        this.power = power;
+        this.dexterity = dexterity;
+        this.intelligence = intelligence;
+    }
+
+    abstract public void attack(Character character);
+}
