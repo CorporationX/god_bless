bf2a1496ecaf21d6dd814f4eee3d868e413dfa58
ifef228 <ifef228@gmail.com>
2024-08-05 19:17:51 +0300

    Развернитесь!

--- src/main/java/faang/school/godbless/Application.java
+++ src/main/java/faang/school/godbless/Application.java
@@ -1,7 +1,10 @@
 package faang.school.godbless;
 
+import faang.school.godbless.turnAround.Main;
+
 public class Application {
     public static void main(String[] args) {
         System.out.println("God Bless!");
+        Main.main(args);
     }
 }
