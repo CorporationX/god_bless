403046d3cb94f27d356095e7f7eaf117da1ed020
Okabe <mathew.mackonoha@gmail.com>
2024-08-05 18:44:43 +0300

    Commiting "Collection of Students" task 1

---
+++ src/main/java/Mathew/Pair.java
@@ -0,0 +1,13 @@
+package Mathew;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+@EqualsAndHashCode
+@Data
+@AllArgsConstructor
+public class Pair {
+    private String faculty;
+    private int year;
+}
