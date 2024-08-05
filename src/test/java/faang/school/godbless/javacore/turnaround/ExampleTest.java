a6b159fbbf228b1971c0caf180b2deceada3635b
Aleksandr Andreev <andreev.aleksandr.spb@gmail.com>
2024-08-05 20:23:37 +0300

    BJS2-19045 Развернитесь!

---
+++ src/test/java/faang/school/godbless/javacore/turnaround/ExampleTest.java
@@ -0,0 +1,17 @@
+package faang.school.godbless.javacore.turnaround;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
+
+class ExampleTest {
+
+    @Test
+    void testReverse() {
+        int[] nums = {1, 2, 3, 4, 5};
+
+        Example.reverse(nums);
+
+        assertArrayEquals(new int[]{5, 4, 3, 2, 1}, nums);
+    }
+}
