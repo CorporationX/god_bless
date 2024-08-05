cc41c5b59ca45023f3db77e4881ab42720777a8c
Svirlex <aleksandrsviridov446@gmail.com>
2024-08-05 17:20:50 +0300

    Cleared the main branch

--- src/main/java/faang/school/godbless/Application.java
+++ src/main/java/faang/school/godbless/Application.java
@@ -7,16 +7,6 @@ import java.util.List;
 
 public class Application {
     public static void main(String[] args) {
-//        User user1 = new User("Emily", 28, "Amazon", "London");
-//        User user2 = new User("Michael", 35, "Uber", "Amsterdam");
-//        User user3 = new User("Sophia", 22);
-//        User user4 = new User("James", 28);
-//        User user5 = new User("Olivia", 35);
-//
-//        ArrayList<User> usersList = new ArrayList<>();
-//        Collections.addAll(usersList, user1, user2, user3, user4, user5);
-//
-//        System.out.println(User.groupUsers(usersList));
-
+        System.out.println("God_Bless");
     }
 }
