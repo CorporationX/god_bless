a857a1ccc721b445e90a848bd2832a58211a4ee3
Andrew Romanov <r0m4n0v4v@gmail.com>
2024-08-05 15:42:05 +0300

    Изменил поле users и исправил System.out

--- src/main/java/faang/school/godbless/groupusersbyage/Main.java
+++ src/main/java/faang/school/godbless/groupusersbyage/Main.java
@@ -1,22 +1,22 @@
 package faang.school.godbless.groupusersbyage;
 
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 
 public class Main {
 
-    public static final List<User> USER_LIST = new ArrayList<>();
-
     public static void main(String[] args) {
-        USER_LIST.add(new User("Павел", 26, "Повар", "Москва"));
-        USER_LIST.add(new User("Дмитрий", 29, "Пожарный", "Калуга"));
-        USER_LIST.add(new User("Юрий", 33, "Программист", "Воронеж"));
-        USER_LIST.add(new User("Даниил", 33, "Врач", "Сочи"));
+        List<User> users = List.of(
+                new User("Павел", 26, "Повар", "Москва"),
+                new User("Дмитрий", 29, "Пожарный", "Калуга"),
+                new User("Юрий", 33, "Программист", "Воронеж"),
+                new User("Даниил", 33, "Врач", "Сочи")
+        );
 
-        Map<Integer, List<User>> groupedUsersMap = User.groupUsers(USER_LIST);
+        Map<Integer, List<User>> groupedUsersMap = User.groupUsers(users);
+
         for (Map.Entry<Integer, List<User>> entry : groupedUsersMap.entrySet()) {
-            System.out.println(entry);
+            System.out.printf("Возраст: %s, Пользователи: %s%n", entry.getKey(), entry.getValue());
         }
     }
 }
