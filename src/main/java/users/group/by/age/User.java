0629791038bd9f8d41f06fdb92376d162a567ff3
Semen Plekhanov <semen-5555@mail.ru>
2024-08-05 11:50:27 +0300

    BJS2-18396 Регистрация пользователей правки по коментариям

--- src/main/java/users/group/by/age/User.java
+++ src/main/java/users/group/by/age/User.java
@@ -1,20 +1,20 @@
 package users.group.by.age;
 
-import java.util.Arrays;
-import java.util.List;
+import java.util.Set;
 
 
 public class User {
-    private static final List<String> VALID_JOBS = Arrays.asList("Google", "Uber", "Amazon");
-    private static final List<String> VALID_ADDRESSES = Arrays.asList("London", "New York", "Amsterdam");
+    private static final Set<String> VALID_JOBS = Set.of("Google", "Uber", "Amazon");
+    private static final Set<String> VALID_ADDRESSES = Set.of("London", "New York", "Amsterdam");
+    private static final int MIN_VALID_AGE = 18;
 
     private final String name;
     private final String workPlace;
     private final String address;
     private final int age;
 
-    public User(String name, String workPlace, String address, int age) throws IllegalArgumentException {
-        if (name.isBlank() || age < 18 || !VALID_JOBS.contains(workPlace) || !VALID_ADDRESSES.contains(address)) {
+    public User(String name, String workPlace, String address, int age) {
+        if (name.isBlank() || age < MIN_VALID_AGE || !VALID_JOBS.contains(workPlace) || !VALID_ADDRESSES.contains(address)) {
             throw new IllegalArgumentException();
         }
 
