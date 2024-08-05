5379d21254861681f63c8f8b896407d6637aa723
Svirlex <aleksandrsviridov446@gmail.com>
2024-08-05 19:55:27 +0300

    Created House class

---
+++ src/main/java/faang/school/godbless/House.java
@@ -0,0 +1,27 @@
+package faang.school.godbless;
+
+public class House {
+    private String name;
+    private String sigil;
+
+    public House(String name, String sigil) {
+        this.name = name;
+        this.sigil = sigil;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSigil() {
+        return sigil;
+    }
+
+    public void setSigil(String sigil) {
+        this.sigil = sigil;
+    }
+}
