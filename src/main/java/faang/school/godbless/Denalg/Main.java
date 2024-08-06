c9530659baf33458b995562d46395a57a5f7c012
Denis Alginin <AlgininDK@gmail.com>
2024-08-06 13:18:52 +0500

    BJS2-18402 code review

--- src/main/java/faang/school/godbless/Denalg/Main.java
+++ src/main/java/faang/school/godbless/Denalg/Main.java
@@ -7,15 +7,8 @@ import java.util.Set;
 
 public class Main {
     public static void main(String[] args) {
-        Set<String> activity1 = new HashSet<>();
-        Set<String> activity2 = new HashSet<>();
-        activity1.add("Football");
-        activity1.add("Basketball");
-        activity2.add("Tennis");
-        activity2.add("Boxing");
-
-        User user1 = new User(1, "Denis", 31, activity1);
-        User user2 = new User(2, "Ivan", 20, activity2);
+        User user1 = new User("Denis", 1, 31, Set.of("Football", "Basketball"));
+        User user2 = new User("Ivan", 2, 20, Set.of("Tennis", "Boxing"));
 
         Map<User, String> result = User.findHobbyLovers(Arrays.asList(user1, user2),
                 new HashSet<>(Arrays.asList("Football", "Shooting")));
--- src/main/java/faang/school/godbless/Denalg/User.java
+++ src/main/java/faang/school/godbless/Denalg/User.java
@@ -14,15 +14,16 @@ import java.util.Set;
 @ToString
 @AllArgsConstructor
 public class User {
-    private long id;
     private String name;
+    private long id;
     private int age;
     private Set<String> activities;
-    public static Map<User, String> findHobbyLovers(List<User> users, Set<String> activities){
+
+    public static Map<User, String> findHobbyLovers(List<User> users, Set<String> activities) {
         Map<User, String> result = new HashMap<>();
-        for(User user : users){
-            for (String activity : user.getActivities()){
-                if (activities.contains(activity)){
+        for (User user : users) {
+            for (String activity : user.getActivities()) {
+                if (activities.contains(activity)) {
                     result.put(user, activity);
                 }
             }
