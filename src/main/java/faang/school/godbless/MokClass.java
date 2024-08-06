10f7617831502adab16bf77651e49ebff6ca94e6
Владислав Воронов <iPlesk90@yandex.ru>
2024-08-06 16:19:59 +0300

    Выполнил задачу

---
+++ src/main/java/faang/school/godbless/MokClass.java
@@ -0,0 +1,8 @@
+package faang.school.godbless;
+
+public class MokClass {
+    public static WeatherDate mokMethod(String city) {
+        WeatherDate weatherDate = new WeatherDate(city,999, 99);
+        return weatherDate;
+    }
+}
