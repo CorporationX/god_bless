03f7e4907623d9e2b4a34b60151dd2ab440c363c
Ivan <spvanym@gmail.com>
2024-08-06 17:52:17 +0300

    Развернитесь! SMI - правки

--- src/main/java/Reverse/Example.java
+++ src/main/java/Reverse/Example.java
@@ -2,29 +2,22 @@ package Reverse;
 
 public class Example {
     public static void main(String[] args) {
-        int[] nums = new int[5];
-        nums[0] = 1;
-        nums[1] = 2;
-        nums[2] = 3;
-        nums[3] = 4;
-        nums[4] = 5;
-
-
+        int[] nums = {1,2,3,4,5};
         reverse(nums);
     }
 
     public static void reverse(int[] num){
         int middle; //для возможности использования любого массива, а не только заданного
-        if(num.length % 2 == 0){
+        if (num.length % 2 == 0) {
             middle = num.length / 2;
         }
-        else{
+        else {
             middle = num.length / 2 + 1;
         }
 
         for (int i = 0; i < num.length; i++) {
 
-            if( i < middle) {
+            if ( i < middle) {
                 int itterFirst = num[i];
                 int itterAnd = num.length - i - 1;
                 int andNum = num.length - i;
