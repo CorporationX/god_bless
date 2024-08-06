107f2205685c37e54cf90c9275e854566dab4c6e
avoevoda <voevodacode@gmail.com>
2024-08-06 22:33:42 +0300

    Library of Westeros

---
+++ src/main/java/BJS2_19431/Book.java
@@ -0,0 +1,12 @@
+package BJS2_19431;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@AllArgsConstructor
+@Data
+public class Book {
+    private String title;
+    private String author;
+    private int year;
+}
