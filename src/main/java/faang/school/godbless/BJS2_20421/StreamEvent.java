7307deb59218b1a686be5a64bf4784731531cb0b
Peregruzochka <peregruzochka48@mail.ru>
2024-08-06 13:08:05 +0300

    создание класса StreamEvent

---
+++ src/main/java/faang/school/godbless/BJS2_20421/StreamEvent.java
@@ -0,0 +1,14 @@
+package faang.school.godbless.BJS2_20421;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.ToString;
+
+@ToString
+@AllArgsConstructor
+@Getter
+public class StreamEvent {
+    private int id;
+    private String eventType;
+    private String data;
+}
