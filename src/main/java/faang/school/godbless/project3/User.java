c0f8d04e008eb1e645f415c41adbdf418d539bc1
Denis Alginin <AlgininDK@gmail.com>
2024-08-06 11:54:37 +0500

    BJS2-18462 code review

--- src/main/java/faang/school/godbless/project3/User.java
+++ src/main/java/faang/school/godbless/project3/User.java
@@ -1,6 +1,5 @@
 package faang.school.godbless.project3;
 
-
 import lombok.Data;
 import lombok.ToString;
 
@@ -13,36 +12,38 @@ import java.util.Set;
 @Data
 @ToString
 public class User {
+    private final static Set<String> VALID_JOBS = Set.of("Google", "Uber", "Amazon");
+    private final static Set<String> VALID_ADDRESS = Set.of("London", "New York", "Amsterdam");
+    private final int MIN_AGE = 18;
     private String name;
     private int age;
     private String placeWork;
     private String address;
-    private final static Set<String> VALID_JOBS = Set.of("Google", "Uber", "Amazon");
-    private final static Set<String> VALID_ADDRESS = Set.of("London", "New York", "Amsterdam");
 
-    public static Map<Integer, List<User>> groupedUsers(List<User> userList) {
-        Map<Integer, List<User>> result = new HashMap<>();
-            for (User user : userList){
-                int age = user.getAge();
-                if(!result.containsKey(age)){
-                    result.put(age, new ArrayList<>());
-                }
-                result.get(age).add(user);
-            }
-        return result;
-
-    }
-    public User(String name, int age, String placeWork, String address){
-        if(age > 18 && !name.isEmpty() && VALID_JOBS.contains(placeWork) && VALID_ADDRESS.contains(address)){
+    public User(String name, int age, String placeWork, String address) {
+        if (age > MIN_AGE && !name.isBlank() && VALID_JOBS.contains(placeWork) && VALID_ADDRESS.contains(address)) {
             this.age = age;
             this.name = name;
             this.placeWork = placeWork;
             this.address = address;
-        }
-        else {
-            throw new IllegalArgumentException("Argument is not valid");
+        } else {
+            throw new IllegalArgumentException("The data entered is incorrect. Maybe you are over 18? Try again.");
         }
     }
+
+    public static Map<Integer, List<User>> groupedUsers(List<User> userList) {
+        Map<Integer, List<User>> result = new HashMap<>();
+        for (User user : userList) {
+            int age = user.getAge();
+            if (!result.containsKey(age)) {
+                result.put(age, new ArrayList<>());
+            }
+            result.get(age).add(user);
+        }
+        return result;
+
+    }
+
 }
 
 
