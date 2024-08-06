aa503d0a048b683c5df77ca6b6d3e3d767704395
Okabe <mathew.mackonoha@gmail.com>
2024-08-06 12:24:07 +0300

    Commiting "Reverse" task 2

--- src/main/java/BJS218595/Example.java
+++ src/main/java/BJS218595/Example.java
@@ -1,17 +1,9 @@
 package BJS218595;
 
 public class Example {
-
-    public void reverse(int[] array) { //это вариант с For который я не использовал в Main
-        for (int i = 0; i < array.length / 2; i++) {
-            array[i] = array[i] + array[array.length - i - 1];
-            array[array.length - i - 1] = array[i] - array[array.length - i - 1];
-            array[i] = array[i] - array[array.length - i - 1];
-        }
-    }
     public static int[] reverse2(int[] array) { //это вариант с For-each который используется в main
         int index = array.length - 1;
-         int[] reversedArray = new int[array.length];
+        int[] reversedArray = new int[array.length];
         for (int element : array) {
             reversedArray[index--] = element;
         }
--- src/main/java/BJS218595/Main.java
+++ src/main/java/BJS218595/Main.java
@@ -1,12 +1,9 @@
 package BJS218595;
 
-import lombok.ToString;
-
-@ToString
-public  class Main {
+public class Main {
     public static void main(String[] args) {
-        int[] nums = {1,2,3,4,5,6,7,8,9,10};
-        for (int elements : Example.reverse2(nums)){
+        int[] nums = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+        for (int elements : Example.reverse2(nums)) {
             System.out.println(elements);
         }
     }
