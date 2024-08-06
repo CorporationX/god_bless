8efa8ee0d8acf2977d26a9ab64f7938a33fc7a22
Mikhail Tupitsyn <mikhail.tupitsyn@gmail.com>
2024-08-06 23:37:08 +0300

    Completed and ready for review

---
+++ src/test/java/faang/school/godbless/groupUsers/TestUserGroup.java
@@ -0,0 +1,48 @@
+package faang.school.godbless.groupUsers;
+
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+import static org.junit.jupiter.api.Assertions.assertAll;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+public class TestUserGroup {
+
+    @Test
+    @DisplayName("Grouping users by Age")
+    public void testGroupUsers() {
+        User user;
+        List<User> usersList = new ArrayList<>();
+
+        user = new User("Vika", 19, "Google", "London");
+        usersList.add(user);
+
+        user = new User("Dima", 19, "Facebook", "Russia");
+        usersList.add(user);
+
+        user = new User("Fedya", 20, "Instagram", "USA");
+        usersList.add(user);
+
+        user = new User("Frosya", 20, "VK", "USA");
+        usersList.add(user);
+
+        user = new User("Marusya", 20, "Yandex", "Russia");
+        usersList.add(user);
+
+        Map<Integer, List<User>> usersMap;
+        usersMap = User.groupUsers(usersList);
+
+        assertAll(
+                () -> assertNotNull(usersMap),
+                () -> assertEquals(2, usersMap.size()),
+                () -> assertEquals(2, usersMap.get(19).size()),
+                () -> assertEquals(3, usersMap.get(20).size())
+        );
+    }
+}
