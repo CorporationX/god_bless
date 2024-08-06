7278323cd268529880ef8cf04204eb58b9a54eee
ifef228 <ifef228@gmail.com>
2024-08-06 12:55:40 +0300

    Java HashMap: Игра престолов

--- src/main/java/faang/school/godbless/Application.java
+++ src/main/java/faang/school/godbless/Application.java
@@ -1,7 +1,10 @@
 package faang.school.godbless;
 
+import faang.school.godbless.game_of_prestols.Main;
+
 public class Application {
     public static void main(String[] args) {
         System.out.println("God Bless!");
+        Main.main(args);
     }
 }
---
+++ src/main/java/faang/school/godbless/game_of_prestols/Main.java
@@ -0,0 +1,41 @@
+package faang.school.godbless.game_of_prestols;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Main {
+    public static void main(String[] args) {
+        Map<String, House> map;
+        map = new HashMap<>(
+                Map.ofEntries(Map.entry("Stark" , new House("Stark", "1")),
+                        Map.entry("Lannister" , new House("Lannister", "2")),
+                        Map.entry("Barateon" , new House("Barateon", "3")),
+                        Map.entry("dom" , new House("dom", "4")))
+        );
+        printMap(map);
+        addHouse(map, new House("domina", "none"));
+        deleteHome(map, "dom");
+        House house = searchHome(map, "domina");
+    }
+    public static void printMap(Map<String, House> map){
+        for (Map.Entry<String, House> pair: map.entrySet()){
+            System.out.println(pair.getKey() + " = " + pair.getValue().getSigil());
+        }
+    }
+
+    public static void addHouse(Map<String, House> map, House house){
+        map.put(house.getName(), house);
+    }
+
+    public static void deleteHome(Map<String, House> map, String name){
+        map.remove(name);
+    }
+
+    public static House searchHome(Map<String, House> map, String name){
+        House house = map.get(name);
+        System.out.println(house.getSigil());
+        return house;
+    }
+}
+
+
