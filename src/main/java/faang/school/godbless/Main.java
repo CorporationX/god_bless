5a609011ca8da4d2796c7d11a3aab237cc997ba2
KnyazAl <cny4z3v@yandex.ru>
2024-08-07 01:14:59 +0300

    correction of comments

--- src/main/java/faang/school/godbless/Main.java
+++ src/main/java/faang/school/godbless/Main.java
@@ -8,7 +8,9 @@ import java.util.Set;
 public class Main {
     public static void main(String[] args) {
         testAddConstructor();
+        System.out.println();
         testGroupedUsers();
+        System.out.println();
         testHobbyLovers();
     }
 
@@ -60,9 +62,9 @@ public class Main {
         Set<String> hobbies2 = new HashSet<>(List.of("Painting", "Gaming", "Cooking"));
         Set<String> hobbies3 = new HashSet<>(List.of("Traveling", "Music", "Sports"));
 
-        User user1 = new User(1, "Alex", 25, hobbies1);
-        User user2 = new User(2, "Egor", 30, hobbies2);
-        User user3 = new User(3, "Dima", 28, hobbies3);
+        User user1 = new User(1, "Alex", 25,"Uber", "New York", hobbies1);
+        User user2 = new User(2, "Egor", 30,"Uber", "New York", hobbies2);
+        User user3 = new User(3, "Dima", 28,"Uber", "New York", hobbies3);
         List<User> users = List.of(user1, user2, user3);
 
         Set<String> targetHobbies = new HashSet<>(List.of("Dancing", "Music", "Coding"));
