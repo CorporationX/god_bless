ff82ea3a692cd313cb3c069f00fe3d505b14e76e
Okabe <mathew.mackonoha@gmail.com>
2024-08-06 12:13:01 +0300

    Commiting "Registration of users" task 2

--- src/main/java/BJS218427/User.java
+++ src/main/java/BJS218427/User.java
@@ -1,6 +1,7 @@
 package BJS218427;
 
 import java.util.Set;
+import java.lang.IllegalArgumentException;
 
 public class User {
     private String name;
@@ -8,21 +9,21 @@ public class User {
     private String work;
     private String adress;
 
-    static Set<String> VALID_JOBS = Set.of("Google", "Uber", "Amazon");
-    static Set<String> VALID_ADDRESSES = Set.of("London","New York","Amsterdam");
+    public static final Set<String> VALID_JOBS = Set.of("Google", "Uber", "Amazon");
+    public static final Set<String> VALID_ADDRESSES = Set.of("London", "New York", "Amsterdam");
 
     public User(String name, int age, String work, String adress) {
-            if(name == null || name.isEmpty()) {
-                throw new java.lang.IllegalArgumentException("name is empty");
-            }
-        if(age < 18){
-            throw new java.lang.IllegalArgumentException("age < 18");
+        if (name == null || name.isEmpty()) {
+            throw new IllegalArgumentException("name is empty");
         }
-        if(!VALID_JOBS.contains(work) || work.isEmpty()){
-            throw new java.lang.IllegalArgumentException("work does not contains in list of valid jobs");
+        if (age < 18) {
+            throw new IllegalArgumentException("age < 18");
         }
-        if(!VALID_ADDRESSES.contains(adress) || adress.isEmpty()){
-            throw new java.lang.IllegalArgumentException("adress does not contains in list of valid adresses");
+        if (!VALID_JOBS.contains(work) || work.isEmpty()) {
+            throw new IllegalArgumentException("work does not contains in list of valid jobs");
+        }
+        if (!VALID_ADDRESSES.contains(adress) || adress.isEmpty()) {
+            throw new IllegalArgumentException("adress does not contains in list of valid adresses");
         }
         this.name = name;
         this.age = age;
