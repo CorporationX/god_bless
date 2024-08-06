6b398c6ef89ccd952067bf747a9141223e7638b7
avoevoda <voevodacode@gmail.com>
2024-08-06 09:43:02 +0300

    Fix code style

--- src/main/java/BJS2_18472/User.java
+++ src/main/java/BJS2_18472/User.java
@@ -13,6 +13,7 @@ public class User {
     private static final Set<String> VALID_JOBS = Set.of("Google", "Uber", "Amazon");
     private static final Set<String> VALID_ADDRESSES = Set.of("London", "New York", "Amsterdam");
     private static final int LEGAL_AGE = 18;
+
     private String name;
     private int age;
     private String job;
