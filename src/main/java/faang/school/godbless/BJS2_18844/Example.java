e4c570e03b8bee327440ec9242f21ac5c1cc7024
Ilia Chudov <chudow-iluha@mail.ru>
2024-08-06 09:13:40 +0300

    Исправил метод reverse(через цикл)

--- src/main/java/faang/school/godbless/BJS2_18844/Example.java
+++ src/main/java/faang/school/godbless/BJS2_18844/Example.java
@@ -1,22 +1,15 @@
 package faang.school.godbless.BJS2_18844;
 
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.List;
-import java.util.stream.Collectors;
-
 public class Example {
     public void reverse(int[] nums) {
-        List<Integer> list = Arrays.stream(nums)
-                .boxed()
-                .collect(Collectors.toList());
-
-        Collections.reverse(list);
-
-        int[] reversed = list.stream()
-                .mapToInt(Integer::intValue)
-                .toArray();
-
-        System.arraycopy(reversed, 0, nums, 0, nums.length);
+        int left = 0;
+        int right = nums.length - 1;
+        while (left < right) {
+            int temp = nums[left];
+            nums[left] = nums[right];
+            nums[right] = temp;
+            left++;
+            right--;
+        }
     }
 }
