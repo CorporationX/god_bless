f76a340a79815a7078e8288bfbdfc7017c36cdca
Sergey Sklyar <sklyar1091@gmail.com>
2024-08-06 23:39:44 +0300

    Кэшируем!

--- src/main/java/BJS2_18987/WeatherTestAPI.java
+++ src/main/java/BJS2_18987/WeatherTestAPI.java
@@ -1,7 +1,5 @@
 package BJS2_18987;
 
-import java.util.Random;
-
 public class WeatherTestAPI {
     public static WeatherData getWeather(String city) {
         return new WeatherData(city, Math.round(Math.random() * 40), Math.round(Math.random() * 99));
