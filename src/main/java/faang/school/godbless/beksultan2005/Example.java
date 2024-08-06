95ff15c6dd0b5a4f66de946ce3ba440adf9d1d5f
aephrodita <146974539+aephrodita@users.noreply.github.com>
2024-08-06 22:05:38 +0500

    исправил пробелы

--- src/main/java/faang/school/godbless/beksultan2005/Example.java
+++ src/main/java/faang/school/godbless/beksultan2005/Example.java
@@ -1,10 +1,10 @@
 package faang.school.godbless.beksultan2005;
 
-public class Example{
-    public static int[] reverse(int[] arr){
-        for (int i = 0; i < arr.length / 2; i++){
-            arr[i] = arr[arr.length - i -1] + arr[i];
-            arr[arr.length - i - 1] = arr[i] - arr[arr.length - i -1];
+public class Example {
+    public static int[] reverse(int[] arr) {
+        for (int i = 0; i < arr.length / 2; i++) {
+            arr[i] = arr[arr.length - i - 1] + arr[i];
+            arr[arr.length - i - 1] = arr[i] - arr[arr.length - i - 1];
             arr[i] = arr[i] - arr[arr.length - i - 1];
         }
         return arr;
--- src/main/java/faang/school/godbless/beksultan2005/Main.java
+++ src/main/java/faang/school/godbless/beksultan2005/Main.java
@@ -2,10 +2,10 @@ package faang.school.godbless.beksultan2005;
 
 public class Main {
     public static void main(String[] args) {
-        int[] arr = {1,2,3,4,5,6,7};
+        int[] arr = {1, 2, 3, 4, 5, 6, 7};
         Example.reverse(arr);
 
-        for (int i = 0; i < arr.length; i++){
+        for (int i = 0; i < arr.length; i++) {
             System.out.println(arr[i]);
         }
     }
