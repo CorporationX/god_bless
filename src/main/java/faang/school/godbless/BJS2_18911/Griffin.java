8802953b85da2ae459a6173946c22d69fa02ad32
Robert Oguzov <robertoguzov2@gmail.com>
2024-08-06 08:08:51 +0300

    Реализовал йеирархию с существами

---
+++ src/main/java/faang/school/godbless/BJS2_18911/Griffin.java
@@ -0,0 +1,7 @@
+package faang.school.godbless.BJS2_18911;
+
+public class Griffin extends Creature {
+    public Griffin(int level) {
+        super("Griffin", level, 8, 3, 8, 1);
+    }
+}
---
+++ src/main/java/faang/school/godbless/BJS2_18911/Pikeman.java
@@ -0,0 +1,7 @@
+package faang.school.godbless.BJS2_18911;
+
+public class Pikeman extends Creature{
+    public Pikeman(int level) {
+        super("Pikeman", level, 3, 2, 4, 5);
+    }
+}
