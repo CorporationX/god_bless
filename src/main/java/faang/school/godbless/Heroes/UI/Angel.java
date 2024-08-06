6d046486c809e4d829831f788c7df382b341c1ee
ifef228 <ifef228@gmail.com>
2024-08-06 20:36:28 +0300

    Java HashMap: Герои Меча и Магии Полностью поменял все класс, добавил класс Battlefield.java, поменялд логику атаки у юнитов

--- src/main/java/faang/school/godbless/Heroes/UI/Angel.java
+++ src/main/java/faang/school/godbless/Heroes/UI/Angel.java
@@ -1,19 +1,17 @@
 package faang.school.godbless.Heroes.UI;
 
+import lombok.Getter;
+
 public class Angel extends Creature {
     private static final int  ATTACK_CONST = 4;
     private static final int  PROTECTION_CONST = 6;
     private static final int  SPEED_CONST = 19;
-    private static int count = 0;
 
 
-    public int getCount(){
-        return count;
-    }
 
-    public Angel(String name, int level) {
-        super(name, level, ATTACK_CONST, PROTECTION_CONST, SPEED_CONST);
-        count++;
+    public Angel(int level, int count) {
+        super(level, ATTACK_CONST, PROTECTION_CONST, SPEED_CONST, count);
+        name = "Pikeman";
     }
 
 
--- src/main/java/faang/school/godbless/Heroes/UI/Griffin.java
+++ src/main/java/faang/school/godbless/Heroes/UI/Griffin.java
@@ -1,19 +1,16 @@
 package faang.school.godbless.Heroes.UI;
 
+import lombok.Getter;
+
 public class Griffin extends Creature {
     private static final int ATTACK_CONST = 8;
     private static final int PROTECTION_CONST = 6;
     private static final int SPEED_CONST = 15;
-    private static int count = 0;
 
 
-    public int getCount(){
-        return count;
-    }
-
-    public Griffin(String name, int level) {
-        super(name, level, ATTACK_CONST, PROTECTION_CONST, SPEED_CONST);
-        count++;
+    public Griffin(int level, int count) {
+        super(level, ATTACK_CONST, PROTECTION_CONST, SPEED_CONST, count);
+        name = "Griffin";
     }
 
 
