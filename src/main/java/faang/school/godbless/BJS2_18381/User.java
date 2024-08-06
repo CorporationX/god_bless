6b864d1b877e1a664c85128f4785d57bf5e12d17
mamadjonovKO <mamadjonov_k@auca.kg>
2024-08-06 16:12:14 +0300

    fixes based on the comments

--- src/main/java/faang/school/godbless/BJS2_18381/User.java
+++ src/main/java/faang/school/godbless/BJS2_18381/User.java
@@ -12,20 +12,20 @@ import java.util.*;
 public class User {
     private int id;
     private String name;
-    Set<String> activites = new HashSet<>();
+    private Set<String> activites = new HashSet<>();
 
-    public static Map<User, String> findHobbyLovers(List<User> users, Set<String> activitesl) {
-        Map<User, String> result = new HashMap<>();
+    public static Map<User, String> findHobbyLovers(List<User> users, Set<String> generalActivities) {
+        Map<User, String> usersWithGeneralActivities = new HashMap<>();
 
         for(var user : users) {
             for(var activity : user.getActivites()) {
-                if(activitesl.contains(activity)) {
-                    result.put(user, activity);
+                if(generalActivities.contains(activity)) {
+                    usersWithGeneralActivities.put(user, activity);
                     break;
                 }
             }
         }
 
-        return result;
+        return usersWithGeneralActivities;
     }
 }
