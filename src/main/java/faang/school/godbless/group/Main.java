3c1ce50e6103f1eba56286ca88aef8c3e5ec883f
ifef228 <ifef228@gmail.com>
2024-08-07 09:33:20 +0300

    BJS2-18801

--- src/main/java/faang/school/godbless/group/Main.java
+++ src/main/java/faang/school/godbless/group/Main.java
@@ -4,7 +4,6 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Map;
-import java.util.logging.Logger;
 
 public class Main {
     public static void main(String[] args) {
