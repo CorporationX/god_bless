b702980d6d11e6bed9525eadeacec39264821e70
KnyazAl <cny4z3v@yandex.ru>
2024-08-07 23:19:29 +0300

    conflict resolution

--- src/main/java/faang/school/godbless/javacore/Example.java
+++ src/main/java/faang/school/godbless/javacore/Example.java
@@ -1,4 +1,4 @@
-package faang.school.godbless;
+package faang.school.godbless.javacore;
 
 public class Example {
     public int[] reverse(int[] arrayInt) {
--- src/main/java/faang/school/godbless/javacore/User.java
+++ src/main/java/faang/school/godbless/javacore/User.java
@@ -1,4 +1,4 @@
-package faang.school.godbless;
+package faang.school.godbless.javacore;
 
 import lombok.Getter;
 import lombok.Setter;
