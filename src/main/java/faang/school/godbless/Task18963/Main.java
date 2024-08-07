fcc9c7fe6362a45ff35f0fe468c7d1ba335f7b64
Chibix <chibisovanastena@gmail.com>
2024-08-08 00:28:42 +0300

    18963

---
+++ src/main/java/faang/school/godbless/Task18963/Main.java
@@ -0,0 +1,8 @@
+package faang.school.godbless.Task18963;
+
+public class Main {
+    public static void main(String[] args) {
+        Example example = new Example();
+        example.reverse(new int[]{1, 2, 3, 4, 5, 6 ,7});
+    }
+}
