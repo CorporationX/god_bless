cbccc269e54f9da5d59436bb19951e21c35d7ad2
Юрий <subbotin.yuriy3@wb.ru>
2024-08-07 23:29:25 +0300

    Update Main.java

--- src/main/java/faang/school/godbless/catch_event/Main.java
+++ src/main/java/faang/school/godbless/catch_event/Main.java
@@ -7,7 +7,7 @@ import java.util.Map;
 
 public class Main {
     static Map<Integer, StreamEvent> dataOfEvents = new HashMap<>();
-    static Map<String, List<StreamEvent>> listEvents = new HashMap<>();
+    static Map<String, List<StreamEvent>> eventsByType = new HashMap<>();
 
     public static void main(String[] args) {
         addNewEvent(1, "payment", "data1");
@@ -17,12 +17,13 @@ public class Main {
         printAllInfo();
 
         System.out.println("\n");
-        searchEvents(1);
+        System.out.println(searchEvents(1));
 
         System.out.println("\n");
-        searchEvents("payment");
+        System.out.println(searchEvents("payment"));;
 
-        removeEvent(2);
+        System.out.println("\n");
+        System.out.println(removeEvent(1));
         System.out.println("\n");
 
         printAllInfo();
@@ -31,30 +32,33 @@ public class Main {
     public static void addNewEvent(int id, String eventType, String data) {
         StreamEvent event = new StreamEvent(id, eventType, data);
         dataOfEvents.put(event.getId(), event);
-        listEvents.computeIfAbsent(eventType, k -> new ArrayList<>()).add(event);
+        eventsByType.computeIfAbsent(eventType, k -> new ArrayList<>()).add(event);
     }
 
-    public static void searchEvents(int id) {
+    public static StreamEvent searchEvents(int id) {
         if (dataOfEvents.containsKey(id)) {
-            System.out.println(dataOfEvents.get(id));
+            return dataOfEvents.get(id);
         }
+        return null;
     }
 
-    public static void searchEvents(String eventType) {
-        if (listEvents.containsKey(eventType)) {
-            listEvents.get(eventType).forEach(System.out::println);
+    public static List<StreamEvent> searchEvents(String eventType) {
+        if (eventsByType.containsKey(eventType)) {
+            eventsByType.get(eventType).forEach(System.out::println);
+            return eventsByType.get(eventType);
         }
+        return List.of();
     }
 
-    public static void removeEvent(int id) {
-        for (Map.Entry<String, List<StreamEvent>> entry : listEvents.entrySet()) {
+    public static boolean removeEvent(int id) {
+        for (Map.Entry<String, List<StreamEvent>> entry : eventsByType.entrySet()) {
             entry.getValue().remove(dataOfEvents.get(id));
         }
-        dataOfEvents.remove(id);
+        return dataOfEvents.remove(id) != null;
     }
 
     public static void printAllInfo() {
-        for (Map.Entry<String, List<StreamEvent>> entry : listEvents.entrySet()) {
+        for (Map.Entry<String, List<StreamEvent>> entry : eventsByType.entrySet()) {
             System.out.println(entry);
             for (StreamEvent event : entry.getValue()) {
                 System.out.println("\t" + event);
