a3cd88dc60a6944ef5b533d40139434d65acebd1
Ilia Chudov <chudow-iluha@mail.ru>
2024-08-07 17:04:35 +0300

    Добавил Класс-сущность House и класс Main с методами работы программы

---
+++ src/main/java/faang/school/godbless/BJS2_19999/House.java
@@ -0,0 +1,13 @@
+package faang.school.godbless.BJS2_19999;
+
+import lombok.Builder;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+@Data
+@EqualsAndHashCode
+@Builder
+public class House {
+    private String name;
+    private String sigil;
+}
