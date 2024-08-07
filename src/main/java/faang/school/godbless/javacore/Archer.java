7cd116b92fb3e8cc675fd96a328557d351f8e555
KnyazAl <cny4z3v@yandex.ru>
2024-08-07 23:14:15 +0300

    conflict resolution

--- src/main/java/faang/school/godbless/javacore/Archer.java
+++ src/main/java/faang/school/godbless/javacore/Archer.java
@@ -1,4 +1,4 @@
-package faang.school.godbless;
+package faang.school.godbless.javacore;
 
 public class Archer extends Character {
     public Archer(String name) {
--- src/main/java/faang/school/godbless/javacore/Warrior.java
+++ src/main/java/faang/school/godbless/javacore/Warrior.java
@@ -1,4 +1,4 @@
-package faang.school.godbless;
+package faang.school.godbless.javacore;
 
 public class Warrior extends Character {
     public Warrior(String name) {
