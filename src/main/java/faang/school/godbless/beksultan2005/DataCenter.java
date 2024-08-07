e048e046f7091ccc15fe5f2d6a72972991a4a6b1
aephrodita <146974539+aephrodita@users.noreply.github.com>
2024-08-07 22:56:03 +0500

    исправил пробелы

--- src/main/java/faang/school/godbless/beksultan2005/DataCenter.java
+++ src/main/java/faang/school/godbless/beksultan2005/DataCenter.java
@@ -2,9 +2,7 @@ package faang.school.godbless.beksultan2005;
 
 import lombok.Getter;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
+import java.util.*;
 
 @Getter
 public class DataCenter {
--- src/main/java/faang/school/godbless/beksultan2005/LoadBalancingOptimizationStrategy.java
+++ src/main/java/faang/school/godbless/beksultan2005/LoadBalancingOptimizationStrategy.java
@@ -1,8 +1,11 @@
 package faang.school.godbless.beksultan2005;
 
+import java.util.Comparator;
+import java.util.Map;
+import java.util.TreeMap;
+
 public class LoadBalancingOptimizationStrategy implements OptimizationStrategy {
     @Override
     public void optimize(DataCenter dataCenter) {
-
     }
 }
