6767b704022347644d3beb19ca29b0ba25ec5938
Юрий <subbotin.yuriy3@wb.ru>
2024-08-07 11:19:01 +0300

    Fix

--- src/main/java/faang/school/godbless/dismantling_amazon_warehouse/Main.java
+++ src/main/java/faang/school/godbless/dismantling_amazon_warehouse/Main.java
@@ -2,48 +2,42 @@ package faang.school.godbless.dismantling_amazon_warehouse;
 
 import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
 
 public class Main {
     public static void main(String[] args) {
-        Set<Product> products = new HashSet<>();
-        products.add(new Product("Laptop", "Electronics"));
-        products.add(new Product("iPhone", "Electronics"));
-        products.add(new Product("Tablet", "Electronics"));
-        products.add(new Product("Sofa", "Furniture"));
-        products.add(new Product("Chair", "Furniture"));
-        products.add(new Product("Bed", "Furniture"));
-        products.add(new Product("Shoes", "Clothing"));
-        products.add(new Product("T-Shirt", "Clothing"));
-        products.add(new Product("Sweater", "Clothing"));
+        Set<Product> products = Set.of(
+                new Product("Laptop", "Electronics"),
+                new Product("Tablet", "Electronics"),
+                new Product("Shoes", "Clothing"),
+                new Product("T-Shirt", "Clothing"),
+                new Product("Sweater", "Clothing"),
+                new Product("Sofa", "Furniture"),
+                new Product("Chair", "Furniture"),
+                new Product("Bed", "Furniture")
+        );
 
-        HashMap<String, List<Product>> categoryMap = groupByCategory(products);
+        Map<String, List<Product>> categoryMap = groupByCategory(products);
         printProducts(categoryMap);
     }
 
-    public static HashMap<String, List<Product>> groupByCategory(Set<Product> products) {
-        HashMap<String, List<Product>> categoryMap = new HashMap<>();
-        for (Product product : products) {
-            String category = product.getCategory();
-            if (categoryMap.containsKey(category)) {
-                categoryMap.get(category).add(product);
-            } else {
-                List<Product> productsList = new ArrayList<>();
-                productsList.add(product);
-                categoryMap.put(category, productsList);
-            }
-        }
+    public static Map<String, List<Product>> groupByCategory(Set<Product> products) {
+        Map<String, List<Product>> categoryMap = new HashMap<>();
+        products.forEach(
+                product -> categoryMap.computeIfAbsent(
+                        product.getCategory(), k -> new ArrayList<>()
+                ).add(product)
+        );
         return categoryMap;
     }
 
     public static void printProducts(Map<String, List<Product>> categoryMap) {
-        for (HashMap.Entry<String, List<Product>> entry : categoryMap.entrySet()) {
+        for (Map.Entry<String, List<Product>> entry : categoryMap.entrySet()) {
             System.out.println(entry.getKey());
             for (Product product : entry.getValue()) {
-                System.out.println("\t" + product.getName());
+                System.out.println("\t" + product);
 
             }
         }
--- src/main/java/faang/school/godbless/dismantling_amazon_warehouse/Product.java
+++ src/main/java/faang/school/godbless/dismantling_amazon_warehouse/Product.java
@@ -1,15 +1,16 @@
 package faang.school.godbless.dismantling_amazon_warehouse;
 
-import lombok.AllArgsConstructor;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.Setter;
+import lombok.ToString;
 
 import java.util.UUID;
 
 @Getter
 @Setter
 @EqualsAndHashCode
+@ToString
 public class Product {
     private UUID id;
     private String name;
