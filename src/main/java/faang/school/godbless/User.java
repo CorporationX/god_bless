ed818bf944cb0952ca0d168765fbcde2bfa08a56
pgusev <pgusev@fil-it.ru>
2024-08-07 23:31:32 +0300

    BJS2-18395 - Добавлены equals и hashCode в класс User

--- src/main/java/faang/school/godbless/User.java
+++ src/main/java/faang/school/godbless/User.java
@@ -22,4 +22,17 @@ public class User {
                     return !u.getActivities().isEmpty();
                 }).collect(Collectors.toMap(u -> u, u -> u.getActivities().iterator().next()));
     }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        User user = (User) o;
+        return id == user.id && age == user.age && Objects.equals(name, user.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, name, age);
+    }
 }
