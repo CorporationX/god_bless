76fe5690097ee6a20fda61ef48d0c1d5c6629f49
ifef228 <ifef228@gmail.com>
2024-08-07 09:31:54 +0300

    BJS2-18812

--- src/main/java/faang/school/godbless/abstraction/Main.java
+++ src/main/java/faang/school/godbless/abstraction/Main.java
@@ -1,6 +1,7 @@
 package faang.school.godbless.abstraction;
 
 public class Main {
+
     public static void main(String[] args) {
         Warrior warrior = new Warrior("warrior");
         Archer archer = new Archer("archer");
