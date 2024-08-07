77b0e3a929b1aae6b9e3d9558a447b51ae1e2c26
KnyazAl <cny4z3v@yandex.ru>
2024-08-08 02:25:06 +0300

    add class Student; add methods add, remove, search Student

---
+++ src/main/java/faang/school/godbless/javahashmap/task3/Student.java
@@ -0,0 +1,14 @@
+package faang.school.godbless.javahashmap.task3;
+
+import lombok.*;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@EqualsAndHashCode
+@ToString
+public class Student {
+    private String name;
+    private String faculty;
+    private Integer year;
+}
