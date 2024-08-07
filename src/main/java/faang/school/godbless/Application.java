5ef8edcd23a8d87011afd2483d9caa17bd22e764
Garib_Aghayev <garibaghayev@gmail.com>
2024-08-07 22:25:29 +0300

    User class update

--- src/main/java/faang/school/godbless/Application.java
+++ src/main/java/faang/school/godbless/Application.java
@@ -1,31 +1,29 @@
 package faang.school.godbless;
 
-import java.awt.*;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Collections;
 import java.util.HashMap;
+import java.util.Map;
 
 public class Application {
     public static void main(String[] args) {
 
-        User user = new User("Bob", 32, "Analyst", "Cypress str.");
-        User user1 = new User("Tom", 25, "Analyst", "Cypress str.");
-        User user2 = new User("Mary", 25, "Analyst", "Cypress str.");
-        User user3 = new User("Jane", 32, "Analyst", "Cypress str.");
-        User user4 = new User("Peter", 17, "Analyst", "Cypress str.");
+        User userBob = new User("Bob", 32, "Analyst", "Cypress str.");
+        User userTom = new User("Tom", 25, "Analyst", "Cypress str.");
+        User userMary = new User("Mary", 25, "Analyst", "Cypress str.");
+        User userJane = new User("Jane", 32, "Analyst", "Cypress str.");
+        User userPeter = new User("Peter", 17, "Analyst", "Cypress str.");
 
-        ArrayList<User> list = new ArrayList<>();
-        ArrayList<User> emptyList = new ArrayList<>();
-        list.add(user);
-        list.add(user1);
-        list.add(user2);
-        list.add(user3);
-        list.add(user4);
+        List<User> list = new ArrayList<>();
+        List<User> emptyList = new ArrayList<>();
+        list.add(userBob);
+        list.add(userTom);
+        list.add(userMary);
+        list.add(userJane);
+        list.add(userPeter);
 
         try {
-            HashMap<Integer, List<User>> result = User.groupUsers(list);
-            HashMap<Integer, List<User>> badResult = User.groupUsers(emptyList);
+            Map<Integer, List<User>> result = User.groupUsers(list);
             System.out.println(result.toString());
 
         } catch (Exception e) {
