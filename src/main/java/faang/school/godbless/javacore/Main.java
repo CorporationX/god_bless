39cabf1120dd2491c1cd885265eddb97226e7198
KnyazAl <cny4z3v@yandex.ru>
2024-08-07 23:21:10 +0300

    conflict resolution

--- src/main/java/faang/school/godbless/javacore/Main.java
+++ src/main/java/faang/school/godbless/javacore/Main.java
@@ -1,9 +1,6 @@
 package faang.school.godbless.javacore;
 
-import java.util.Arrays;
-import java.util.List;
-import java.util.Map;
-import java.util.Set;
+import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
