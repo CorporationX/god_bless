3458fb6ee628cafb09714e08014bce55625b1a7d
Юрий <subbotin.yuriy3@wb.ru>
2024-08-07 10:57:49 +0300

    Fix remarks

--- src/main/java/faang/school/godbless/grouping_users_by_age/User.java
+++ src/main/java/faang/school/godbless/grouping_users_by_age/User.java
@@ -4,6 +4,7 @@ package faang.school.godbless.grouping_users_by_age;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
+import lombok.ToString;
 
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -13,6 +14,7 @@ import java.util.Map;
 @Getter
 @Setter
 @AllArgsConstructor
+@ToString
 public class User {
     private String name;
     private int age;
@@ -21,17 +23,7 @@ public class User {
 
     public static Map<Integer, List<User>> groupUsers(List<User> users) {
         Map<Integer, List<User>> groupedUsers = new HashMap<>();
-
-        for (User user : users) {
-            if (groupedUsers.containsKey(user.getAge())) {
-                groupedUsers.get(user.getAge()).add(user);
-            } else {
-                List<User> usersByAge = new ArrayList<>();
-                usersByAge.add(user);
-                groupedUsers.put(user.getAge(), usersByAge);
-            }
-        }
-
+        users.forEach(user -> groupedUsers.computeIfAbsent(user.getAge(), k -> new ArrayList<>()).add(user));
         return groupedUsers;
     }
 }
