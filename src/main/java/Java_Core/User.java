211fd9f125cc7b1b288edfd24de5f8d042f40dae
Garib_Aghayev <garibaghayev@gmail.com>
2024-08-07 22:36:59 +0300

    User class update

--- src/main/java/Java_Core/User.java
+++ src/main/java/Java_Core/User.java
@@ -1,21 +1,21 @@
 package Java_Core;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.HashSet;
-import java.util.List;
+import lombok.ToString;
+
 import java.util.Set;
 
+@ToString
 public class User {
     private String name;
     private int age;
     private String job;
     private String adress;
-    public static final Set<String> VALID_JOBS =  Set.of("Google", "Uber", "Amazon");
+
+    public static final Set<String> VALID_JOBS = Set.of("Google", "Uber", "Amazon");
     public static final Set<String> VALID_ADDRESSES = Set.of("London", "New York", "Amsterdam");
 
-    public User(String name, int age, String job, String adress) throws IllegalArgumentException{
-        if(name.isEmpty()||name == null || age < 18 || !VALID_JOBS.contains(job) || !VALID_ADDRESSES.contains(adress) ){
+    public User(String name, int age, String job, String adress) throws IllegalArgumentException {
+        if (name.isEmpty() || name == null || age < 18 || !VALID_JOBS.contains(job) || !VALID_ADDRESSES.contains(adress)) {
             throw new IllegalArgumentException();
         }
         this.name = name;
@@ -25,14 +25,4 @@ public class User {
 
     }
 
-
-    @Override
-    public String toString() {
-        return "User{" +
-                "name='" + name + '\'' +
-                ", age=" + age +
-                ", job='" + job + '\'' +
-                ", adress='" + adress + '\'' +
-                '}';
-    }
 }
