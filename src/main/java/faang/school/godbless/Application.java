51b704faf3ed726f69b33021db9d54ad243ac088
ifef228 <ifef228@gmail.com>
2024-08-07 09:25:09 +0300

    BJS2-19066

--- src/main/java/faang/school/godbless/Application.java
+++ src/main/java/faang/school/godbless/Application.java
@@ -1,7 +1,11 @@
 package faang.school.godbless;
 
+import faang.school.godbless.Heroes.Main;
+
+
 public class Application {
-    public static void main(String[] args) {
+    public static void main(String[] args){
         System.out.println("God Bless!");
+        Main.main(args);
     }
 }
