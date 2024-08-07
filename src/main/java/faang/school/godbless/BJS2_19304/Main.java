048f5f4cfb90ac9e2f1dfbaba1dddbb4409890e0
Ivan Tsegelnyuk <tsegelnyuk.ivan@gmail.com>
2024-08-07 23:39:52 +0300

    Fix comments

--- src/main/java/faang/school/godbless/BJS2_19304/Main.java
+++ src/main/java/faang/school/godbless/BJS2_19304/Main.java
@@ -1,36 +1,23 @@
 package faang.school.godbless.BJS2_19304;
 
+import lombok.Getter;
+
 import java.util.*;
 
+@Getter
 public class Main {
     private final Set<Product> products = new HashSet<>();
 
-    {
-        products.add(new Product(1, "Playstation 4", "Console"));
-        products.add(new Product(2, "Xbox 360", "Console"));
-        products.add(new Product(3, "Playstation 5", "Console"));
-        products.add(new Product(4, "Samsung", "TV"));
-        products.add(new Product(5, "LG", "TV"));
-        products.add(new Product(6, "Sony", "TV"));
-        products.add(new Product(7, "Toyota", "Car"));
-        products.add(new Product(8, "Honda", "Car"));
-        products.add(new Product(9, "Subaru", "Car"));
-    }
-
-    public Map<String, List<Product>> groupProducts(Set<Product> products) {
+    public Map<String, List<Product>> groupedProducts(Set<Product> products) {
         Map<String, List<Product>> result = new HashMap<>();
         for (Product product : products) {
-            result.merge(product.getCategory(), new ArrayList<>(List.of(product)),
-                    (x, y) -> {
-                        x.addAll(y);
-                        return x;
-                    });
+            result.computeIfAbsent(product.getCategory(), k -> new ArrayList<>()).add(product);
         }
         return result;
     }
 
     public void printProducts() {
-        groupProducts(products).forEach((category, list) -> {
+        groupedProducts(products).forEach((category, list) -> {
             System.out.println("-" + category + " :");
             list.forEach(p -> System.out.println("---" + p));
         });
--- src/test/java/faang/school/godbless/BJS2_19304/MainTest.java
+++ src/test/java/faang/school/godbless/BJS2_19304/MainTest.java
@@ -6,6 +6,18 @@ class MainTest {
 
     Main main = new Main();
 
+    {
+        main.getProducts().add(new Product(1, "Playstation 4", "Console"));
+        main.getProducts().add(new Product(2, "Xbox 360", "Console"));
+        main.getProducts().add(new Product(3, "Playstation 5", "Console"));
+        main.getProducts().add(new Product(4, "Samsung", "TV"));
+        main.getProducts().add(new Product(5, "LG", "TV"));
+        main.getProducts().add(new Product(6, "Sony", "TV"));
+        main.getProducts().add(new Product(7, "Toyota", "Car"));
+        main.getProducts().add(new Product(8, "Honda", "Car"));
+        main.getProducts().add(new Product(9, "Subaru", "Car"));
+    }
+
     @Test
     void printProducts() {
         main.printProducts();
