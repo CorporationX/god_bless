18fb7f99f49ffc5112eb0db96b18d3dbd02aeee6
Sergey Sklyar <sklyar1091@gmail.com>
2024-08-08 01:39:10 +0300

    Разбираем склад Амазон

---
+++ src/main/java/BJS2_19417/Product.java
@@ -0,0 +1,39 @@
+package BJS2_19417;
+
+import lombok.ToString;
+
+import java.util.Objects;
+
+@ToString
+public class Product {
+    private int id;
+    private String name;
+    private String category;
+
+    public Product(int id, String name, String category) {
+        this.id = id;
+        this.name = name;
+        this.category = category;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Product product = (Product) o;
+        return id == product.id && Objects.equals(name, product.name) && Objects.equals(getCategory(), product.getCategory());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, name, getCategory());
+    }
+
+    public String getName() {
+        return name;
+    }
+}
