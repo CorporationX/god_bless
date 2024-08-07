4c20c5912ae9169f9a2cacdc1b6843e0c7591451
Sergey Sklyar <sklyar1091@gmail.com>
2024-08-08 00:40:40 +0300

    Правки к ревью!

--- src/main/java/BJS2_18537/User.java
+++ src/main/java/BJS2_18537/User.java
@@ -9,10 +9,11 @@ import java.util.Map;
 
 @ToString
 public class User {
-    String name;
-    int age;
-    String job;
-    String address;
+    //Поправил модификаторы на private
+    private String name;
+    private int age;
+    private String job;
+    private String address;
 
     public User(String name, int age, String job, String address) {
         this.name = name;
@@ -21,20 +22,22 @@ public class User {
         this.address = address;
     }
 
-    public static Map<Integer, List<User>> groupUsers(List<User> users){
+    //Переписал с computeIfAbsent сложнее для понимания пока . но вроде все корректно
+    //работает и кода в разы меньше! Спасибо
+    public static Map<Integer, List<User>> groupUsers(List<User> users) {
 
-        Map<Integer,List<User>> resultMap = new HashMap<>();
+        Map<Integer, List<User>> resultMap = new HashMap<>();
 
-        for(User user : users){
-            if(!resultMap.containsKey(user.age)){
-                resultMap.put(user.age, new ArrayList<>());
-                resultMap.get(user.age).add(user);
-            }
-            else{
-                resultMap.get(user.age).add(user);
-            }
+        for (User user : users) {
+            resultMap.computeIfAbsent(user.getAge(), x -> new ArrayList<>());
+            resultMap.get(user.getAge()).add(user);
         }
 
         return resultMap;
+    }
+
+    //добавил геттер по полю age
+    public int getAge() {
+        return age;
     }
 }
