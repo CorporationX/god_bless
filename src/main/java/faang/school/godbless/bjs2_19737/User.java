a9d72a79d3b73d9dbae13cb93f900c67ae016e7f
Alexander Milosh <milosh.alexandr.by@yandex.by>
2024-08-07 02:31:34 +0200

    BJS2-19737 The ELK Query Index

---
+++ src/main/java/faang/school/godbless/bjs2_19737/User.java
@@ -0,0 +1,13 @@
+package faang.school.godbless.bjs2_19737;
+
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+
+@AllArgsConstructor
+@EqualsAndHashCode
+public class User {
+    private Integer id;
+    @Getter
+    private String name;
+}
