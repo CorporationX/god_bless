c7438da48eeb2a0e624197d002f1c39bdb3e35af
Дмитрий Бахтин <dmitridorje@gmail.com>
2024-08-07 17:42:34 +0300

    Решение задачи: Ловим события

--- src/main/java/faang/school/godbless/catchingevents/StreamEvent.java
+++ src/main/java/faang/school/godbless/catchingevents/StreamEvent.java
@@ -1,25 +1,14 @@
 package faang.school.godbless.catchingevents;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.ToString;
 
 @Getter
+@AllArgsConstructor
+@ToString
 public class StreamEvent {
     private int id;
     private String eventType;
     private String data;
-
-    public StreamEvent(int id, String eventType, String data) {
-        this.id = id;
-        this.eventType = eventType;
-        this.data = data;
-    }
-
-    @Override
-    public String toString() {
-        return "StreamEvent{" +
-                "id=" + id +
-                ", eventType='" + eventType + '\'' +
-                ", data='" + data + '\'' +
-                '}';
-    }
 }
