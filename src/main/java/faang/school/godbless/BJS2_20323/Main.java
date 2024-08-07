047ab65030bba389de9dc824b749d795dd0da8b5
Ivan Tsegelnyuk <tsegelnyuk.ivan@gmail.com>
2024-08-07 22:11:37 +0300

    Fix comments

--- src/main/java/faang/school/godbless/BJS2_20323/Main.java
+++ src/main/java/faang/school/godbless/BJS2_20323/Main.java
@@ -10,29 +10,22 @@ public class Main {
 
     private final Map<String, List<StreamEvent>> eventsByTypes = new HashMap<>();
 
-    {
-        StreamEvent event1 = new StreamEvent(1, "SMS", "Списание средств: -50 р.");
-        StreamEvent event2 = new StreamEvent(2, "Email", "Проверочный код: 12345.");
-        StreamEvent event3 = new StreamEvent(3, "Post", "Повестка из военкомата");
-        StreamEvent event4 = new StreamEvent(4, "Email", "Успешная регистрация на сервисе");
-        StreamEvent event5 = new StreamEvent(5, "SMS", "Абнент 123 не в сети");
-        StreamEvent event6 = new StreamEvent(6, "Post", "Письмо от приставов");
-
-        eventsById.put(event1.getId(), event1);
-        eventsById.put(event2.getId(), event2);
-        eventsById.put(event3.getId(), event3);
-        eventsById.put(event4.getId(), event4);
-        eventsById.put(event5.getId(), event5);
-        eventsById.put(event6.getId(), event6);
-
-        eventsByTypes.put(event1.getEventType(), new ArrayList<>(Arrays.asList(event1, event5)));
-        eventsByTypes.put(event2.getEventType(), new ArrayList<>(Arrays.asList(event2, event4)));
-        eventsByTypes.put(event3.getEventType(), new ArrayList<>(Arrays.asList(event3, event6)));
-    }
-
     public void addStreamEvent(StreamEvent streamEvent) {
         addStreamEventById(streamEvent);
-        addStreamEventsByType(streamEvent);
+        addStreamEventByType(streamEvent);
+    }
+
+    private void addStreamEventById(StreamEvent streamEvent) {
+        eventsById.put(streamEvent.getId(), streamEvent);
+    }
+
+    private void addStreamEventByType(StreamEvent streamEvent) {
+        String type = streamEvent.getEventType();
+        if (!eventsByTypes.containsKey(type)) {
+            eventsByTypes.put(type, new ArrayList<>(List.of(streamEvent)));
+        } else {
+            eventsByTypes.get(type).add(streamEvent);
+        }
     }
 
     public StreamEvent getStreamEventById(Integer id) {
@@ -46,7 +39,19 @@ public class Main {
     public boolean deleteStreamEvent(Integer id) {
         StreamEvent event = deleteStreamEventById(id);
         if (event != null) {
-            return deleteStreamEventByIdFromEventsByType(event);
+            return deleteStreamEventFromEventsByType(event);
+        }
+        return false;
+    }
+
+    private StreamEvent deleteStreamEventById(Integer id) {
+        return eventsById.remove(id);
+    }
+
+    private boolean deleteStreamEventFromEventsByType(StreamEvent streamEvent) {
+        List<StreamEvent> events = eventsByTypes.get(streamEvent.getEventType());
+        if (events != null) {
+            return events.remove(streamEvent);
         }
         return false;
     }
@@ -55,35 +60,5 @@ public class Main {
         for(Map.Entry<Integer, StreamEvent> event: eventsById.entrySet()) {
             System.out.println(event);
         }
-    }
-
-    private void addStreamEventById(StreamEvent streamEvent) {
-        eventsById.put(streamEvent.getId(), streamEvent);
-    }
-
-    private void addStreamEventsByType(StreamEvent streamEvent) {
-        eventsByTypes.merge(streamEvent.getEventType(), new ArrayList<>(List.of(streamEvent)),
-                (x, y) -> {
-                    x.addAll(y);
-                    return x;
-                }
-        );
-    }
-
-    //удаление из первой мапы
-    private StreamEvent deleteStreamEventById(Integer id) {
-        return eventsById.remove(id);
-    }
-
-    //удаление из второй мапы
-    private boolean deleteStreamEventByIdFromEventsByType(StreamEvent streamEvent) {
-        List<StreamEvent> events = eventsByTypes.get(streamEvent.getEventType());
-        for (StreamEvent event : events) {
-            if (Objects.equals(event.getId(), streamEvent.getId())) {
-                events.remove(event);
-                return true;
-            }
-        }
-        return false;
     }
 }
--- src/test/java/faang/school/godbless/BJS2_20323/MainTest.java
+++ src/test/java/faang/school/godbless/BJS2_20323/MainTest.java
@@ -3,6 +3,8 @@ package faang.school.godbless.BJS2_20323;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -14,6 +16,24 @@ class MainTest {
     @BeforeEach
     void init() {
         main = new Main();
+
+        StreamEvent event1 = new StreamEvent(1, "SMS", "Списание средств: -50 р.");
+        StreamEvent event2 = new StreamEvent(2, "Email", "Проверочный код: 12345.");
+        StreamEvent event3 = new StreamEvent(3, "Post", "Повестка из военкомата");
+        StreamEvent event4 = new StreamEvent(4, "Email", "Успешная регистрация на сервисе");
+        StreamEvent event5 = new StreamEvent(5, "SMS", "Абнент 123 не в сети");
+        StreamEvent event6 = new StreamEvent(6, "Post", "Письмо от приставов");
+
+        main.getEventsById().put(event1.getId(), event1);
+        main.getEventsById().put(event2.getId(), event2);
+        main.getEventsById().put(event3.getId(), event3);
+        main.getEventsById().put(event4.getId(), event4);
+        main.getEventsById().put(event5.getId(), event5);
+        main.getEventsById().put(event6.getId(), event6);
+
+        main.getEventsByTypes().put(event1.getEventType(), new ArrayList<>(Arrays.asList(event1, event5)));
+        main.getEventsByTypes().put(event2.getEventType(), new ArrayList<>(Arrays.asList(event2, event4)));
+        main.getEventsByTypes().put(event3.getEventType(), new ArrayList<>(Arrays.asList(event3, event6)));
     }
 
     @Test
