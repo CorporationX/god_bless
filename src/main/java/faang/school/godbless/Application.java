1f931d3ea48ddfa9a0640770d846e795dbee971a
Bobiktrans <Bobicktrnas@yandex.ru>
2024-08-08 00:09:53 +0300

    BJS2-18984

--- src/main/java/faang/school/godbless/Application.java
+++ src/main/java/faang/school/godbless/Application.java
@@ -2,6 +2,14 @@ package faang.school.godbless;
 
 public class Application {
     public static void main(String[] args) {
-        System.out.println("God Bless!");
+        Example example = new Example();
+        int[] nums = {1, 2, 3, 4, 5};
+
+        example.reverse(nums);
+
+        for (int num : nums) {
+            System.out.print(num + " ");
+        }
+
     }
 }
