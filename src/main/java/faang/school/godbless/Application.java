6dd32a3c93b8d63da0682fccdbb4f7c6529dd6a3
ifef228 <ifef228@gmail.com>
2024-08-07 09:25:09 +0300

    BJS2-18947 почистил код

--- src/main/java/faang/school/godbless/Application.java
+++ src/main/java/faang/school/godbless/Application.java
@@ -2,6 +2,7 @@ package faang.school.godbless;
 
 import faang.school.godbless.Heroes.Main;
 
+
 public class Application {
     public static void main(String[] args){
         System.out.println("God Bless!");
