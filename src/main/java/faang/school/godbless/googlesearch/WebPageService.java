f5f9a8e13a8343c90e7964b6b704450ab1333fbe
Murzin34 <108129795+Murzin34@users.noreply.github.com>
2024-08-07 18:20:38 +0300

    BJS2-20185 Java HashMap: Поисковой мини-движок Google

---
+++ src/main/java/faang/school/godbless/googlesearch/WebPageService.java
@@ -0,0 +1,41 @@
+package faang.school.godbless.googlesearch;
+
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+
+public class WebPageService {
+    public void addWebPage(Map<String, Set<WebPage>> webPagesMap, WebPage webPage) {
+        var keyWords = webPage.getContent().split(",");
+
+        Arrays.stream(keyWords)
+                .map(word -> word.replaceAll("\\p{Punct}", "").trim())
+                .filter(word -> word.length() > 1)
+                .forEach(word -> webPagesMap.computeIfAbsent(word, k -> new HashSet<>()).add(webPage));
+    }
+
+    public Set<WebPage> findPagesByKeyWord(Map<String, Set<WebPage>> webPagesMap, String keyWord) {
+        var foundPages = webPagesMap.get(keyWord);
+
+        if (foundPages == null) {
+            System.out.println("Pages by key word: " + keyWord + " not found!");
+        }
+
+        return foundPages;
+    }
+
+    public void removePageByUrl(Map<String, Set<WebPage>> webPagesMap, String url) {
+        webPagesMap.values().forEach(key -> key.removeIf(value -> value.getUrl().equals(url)));
+        webPagesMap.entrySet().removeIf(entry -> entry.getValue().isEmpty());
+    }
+
+    public void webPagesInfo(Map<String, Set<WebPage>> webPagesMap) {
+        System.out.println("WebPages by key word");
+        webPagesMap.forEach((keyWord, webPages) -> {
+            System.out.println(keyWord + ":");
+            webPages.forEach(webPage -> System.out.println(" " + webPage.getTitle() + ", " + webPage.getUrl()));
+            System.out.println();
+        });
+    }
+}
