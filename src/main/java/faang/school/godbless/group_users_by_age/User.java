da4d1edfb3523e02850139f3af3b39a6fa31b66b
Nikita Korolev <nk25.dev@gmail.com>
2024-08-07 17:46:30 +0300

    Solution grouping users with unit JUnit tests

---
+++ src/main/java/faang/school/godbless/group_users_by_age/User.java
@@ -0,0 +1,39 @@
+package faang.school.godbless.group_users_by_age;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Objects;
+
+@Getter
+@AllArgsConstructor
+public class User {
+
+    private String name;
+    private int age;
+    private String workplace;
+    private String address;
+
+    public static Map<Integer, List<User>> groupUsers(List<User> users) {
+        if (Objects.equals(users, null) || users.isEmpty()) {
+            throw new IllegalArgumentException("User list is null/empty!");
+        }
+
+        Map<Integer, List<User>> ageUsersMap = new HashMap<>();
+        users.forEach(user -> {
+            ageUsersMap.computeIfAbsent(user.getAge(), age -> new ArrayList<>());
+            ageUsersMap.get(user.getAge()).add(user);
+        });
+
+        return ageUsersMap;
+    }
+
+    @Override
+    public String toString() {
+        return "User: Name[" + this.name + "], age[" + age + "]";
+    }
+}
