9420d2ba0b64ebaae0b9db8acf4a22f55f40cc01
Sultan <nikowosuka@gmail.com>
2024-08-08 11:45:20 +0500

    Event catching task refactoring

--- src/main/java/faang/school/godbless/task/event/catching/EventManager.java
+++ src/main/java/faang/school/godbless/task/event/catching/EventManager.java
@@ -1,24 +1,20 @@
 package faang.school.godbless.task.event.catching;
 
-import java.util.HashMap;
+import lombok.RequiredArgsConstructor;
+
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
-import java.util.stream.Collectors;
 
+@RequiredArgsConstructor
 public class EventManager {
-    private Map<Integer, StreamEvent> idStreamEventCache;
-    private Map<String, Map<Integer, StreamEvent>> typeStreamEventCache;
-
-    public EventManager(Map<Integer, StreamEvent> idStreamEventCache,
-                        Map<String, Map<Integer, StreamEvent>> typeStreamEventCache) {
-        this.idStreamEventCache = idStreamEventCache;
-        this.typeStreamEventCache = typeStreamEventCache;
-    }
+    private final Map<Integer, StreamEvent> idStreamEventCache;
+    private final Map<String, List<StreamEvent>> typeStreamEventCache;
 
     public void addNewStreamEvent(StreamEvent streamEvent) {
         idStreamEventCache.put(streamEvent.id(), streamEvent);
         typeStreamEventCache.computeIfAbsent(streamEvent.eventType(),
-                key -> new HashMap<>()).put(streamEvent.id(), streamEvent);
+                key -> new ArrayList<>()).add(streamEvent);
     }
 
     public StreamEvent findStreamEventById(int id) {
@@ -26,22 +22,12 @@ public class EventManager {
     }
 
     public List<StreamEvent> findStreamEventByEventType(String eventType) {
-        Map<Integer, StreamEvent> integerStreamEventMap = typeStreamEventCache.get(eventType);
-//        integerStreamEventMap.forEach((key, value) -> streamEventsList.add(value));
-//        return integerStreamEventMap.entrySet().stream().collect(Collectors.toList());
-//        return streamEventsList;
-//        return integerStreamEventMap.entrySet().stream()
-//                .map(entry -> entry.getValue()).collect(Collectors.toList());
-//        return integerStreamEventMap.entrySet().stream()
-//                .map(Map.Entry::getValue).filter(k -> k.equals(new StreamEvent(3, "df" , "df"))).collect(Collectors.toList());
-//        return integerStreamEventMap.values().stream().collect(Collectors.toList());
-//        return new ArrayList<>(integerStreamEventMap.values());
-        return integerStreamEventMap.entrySet().stream()
-                .map(Map.Entry::getValue).collect(Collectors.toList());
+        return typeStreamEventCache.get(eventType);
     }
 
     public void deleteStreamEventById(int id) {
         StreamEvent streamEvent = idStreamEventCache.remove(id);
-        typeStreamEventCache.get(streamEvent.eventType()).remove(streamEvent.id());
+        List<StreamEvent> streamEventsList = typeStreamEventCache.get(streamEvent.eventType());
+        streamEventsList.remove(streamEvent);
     }
 }
