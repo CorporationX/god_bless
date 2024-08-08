2920148aa8dae9951aa60c5c23b9e735dd4fc543
Юрий <subbotin.yuriy3@wb.ru>
2024-08-08 20:36:26 +0300

    Fix

--- src/main/java/faang/school/godbless/counting_absenteeism/Student.java
+++ src/main/java/faang/school/godbless/counting_absenteeism/Student.java
@@ -1,6 +1,5 @@
 package faang.school.godbless.counting_absenteeism;
 
-import lombok.AllArgsConstructor;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.Setter;
@@ -8,11 +7,31 @@ import lombok.ToString;
 
 @Setter
 @Getter
-@AllArgsConstructor
 @EqualsAndHashCode
 @ToString
 public class Student {
     private String name;
     private String faculty;
     private int year;
+
+    public Student(String name, String faculty, int year) {
+        if (name == null) {
+            throw new IllegalArgumentException("Name cannot be null");
+        }
+        if (name.isEmpty()) {
+            throw new IllegalArgumentException("Name cannot be empty");
+        }
+        if (faculty == null) {
+            throw new IllegalArgumentException("Faculty cannot be null");
+        }
+        if (faculty.isEmpty()) {
+            throw new IllegalArgumentException("Faculty cannot be empty");
+        }
+        if (year < 0) {
+            throw new IllegalArgumentException("Year cannot be negative");
+        }
+        this.name = name;
+        this.faculty = faculty;
+        this.year = year;
+    }
 }
