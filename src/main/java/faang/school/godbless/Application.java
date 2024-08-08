07e3eef79255d1460a634dcbb4e445a2bd80bc3b
Aleh Biahun <stones2875@gmail.com>
2024-08-08 12:50:15 +0200

    Solution task Stones-BJS2-18590

--- src/main/java/faang/school/godbless/Application.java
+++ src/main/java/faang/school/godbless/Application.java
@@ -1,7 +1,24 @@
 package faang.school.godbless;
 
+import java.util.*;
+
 public class Application {
     public static void main(String[] args) {
-        System.out.println("God Bless!");
+
+        Set<String> ivanActivities = new HashSet<>(Arrays.asList("reading", "swimming", "cycling"));
+        Set<String> petrActivities = new HashSet<>(Arrays.asList("running", "swimming", "cooking"));
+        Set<String> fedrActivities = new HashSet<>(Arrays.asList("gaming", "cycling", "hiking"));
+
+        User ivan = new User(1, "Ivan", 36, ivanActivities);
+        User petr = new User(2, "Petr", 25, petrActivities);
+        User fedr = new User(3, "Fedr", 16, fedrActivities);
+
+        List<User> users = new ArrayList<>(Arrays.asList(ivan, petr, fedr));
+        Set<String> hobbyActivities = new HashSet<>(Arrays.asList("reading", "cycling"));
+        Map<User, String> findResult = User.findHobbyLovers(users, hobbyActivities);
+
+        for (Map.Entry<User, String> entry : findResult.entrySet()) {
+            System.out.println(entry.getKey() + ", " + "his activities - " + entry.getValue());
+        }
     }
 }
