8a511bce99998921524cb0b6bf02f2da474f270c
Varvara Stepnova <stepnova.varya@yandex.ru>
2024-08-08 21:07:30 +0300

    BJS2-18426-user_registration: added method isValidData

--- src/main/java/faang/school/godbless/user_registration/User.java
+++ src/main/java/faang/school/godbless/user_registration/User.java
@@ -3,6 +3,7 @@ package faang.school.godbless.user_registration;
 import java.util.Set;
 
 public class User {
+    private static final int MIN_AGE = 18;
 
     private static final Set<String> VALID_JOBS = Set.of("Google", "Uber", "Amazon");
 
@@ -14,14 +15,21 @@ public class User {
     private String address;
 
     public User(String name, int age, String job, String address) {
-        if (name == null || name.trim().isEmpty() || age <= 18 ||
-                !VALID_JOBS.contains(job) || !VALID_ADDRESSES.contains(address)) {
-            throw new IllegalArgumentException();
+        if (name == null || name.trim().isEmpty() || age <= MIN_AGE ||
+                isValidData(job, address)) {
+            System.out.println("Поля User заполнены некорректно! " + name);
         } else {
             this.name = name;
             this.age = age;
             this.job = job;
             this.address = address;
         }
+    }
+
+    private boolean isValidData(String job, String address) {
+        if (!VALID_JOBS.contains(job) || !VALID_ADDRESSES.contains(address)) {
+            return true;
+        }
+        return false;
     }
 }
