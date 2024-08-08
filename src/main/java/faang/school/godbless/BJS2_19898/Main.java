a8e1c94529ea4ee425e5aab6bd20e952b4019e78
DenysDovgan <12qwa0@ukr.net>
2024-08-08 21:56:22 +0200

    Edited, Minor changes

--- src/main/java/faang/school/godbless/BJS2_19898/Main.java
+++ src/main/java/faang/school/godbless/BJS2_19898/Main.java
@@ -8,21 +8,21 @@ public class Main {
     private static Map<String, House> houses = new HashMap<String, House>();
 
     public static void main(String[] args) {
-        addHouse("Stark", "A grey direwolf on a white field");
-        addHouse("Lannister", "A golden lion on a crimson field");
-        addHouse("Baratheon", "A crowned black stag on a golden field");
+        addHouse(new House("Stark", "A grey direwolf on a white field"));
+        addHouse(new House("Lannister", "A golden lion on a crimson field"));
+        addHouse(new House("Baratheon", "A crowned black stag on a golden field"));
 
         printAllHouses();
 
-        findHouse("Stark");
+        findHouseAndSigil("Stark");
 
         removeHouse("Lannister");
 
         printAllHouses();
     }
 
-    private static void addHouse(String houseName, String sigil) {
-        houses.put(houseName, new House(houseName, sigil));
+    private static void addHouse(House house) {
+        houses.put(house.getName(), house);
     }
 
     public static void removeHouse(String name) {
@@ -34,10 +34,11 @@ public class Main {
         }
     }
 
-    public static void findHouse(String name) {
+    public static void findHouseAndSigil(String name) {
         House house = houses.get(name);
         if (house != null) {
-            System.out.println("Found: " + house);
+            System.out.println("Found: " + house.getName());
+            System.out.println(house.getSigil());
         } else {
             System.out.println("House not found: " + name);
         }
