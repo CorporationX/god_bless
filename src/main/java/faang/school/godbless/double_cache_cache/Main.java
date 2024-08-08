0f6ca887fc8638e40363edd4f43671eda6c439c8
Юрий <subbotin.yuriy3@wb.ru>
2024-08-08 17:02:05 +0300

    BJS2-19782

---
+++ src/main/java/faang/school/godbless/double_cache_cache/Main.java
@@ -0,0 +1,99 @@
+package faang.school.godbless.double_cache_cache;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.UUID;
+
+public class Main {
+    private static final Map<Student, Map<Subject, Integer>> students = new HashMap<>();
+
+    private static final Map<Subject, List<Student>> subjects = new HashMap<>();
+
+    public static void main(String[] args) {
+        Student student = new Student(UUID.randomUUID(), "John");
+        Map<Subject, Integer> subject = new HashMap<>() {{
+            put(new Subject(UUID.randomUUID(), "Maths"), 90);
+            put(new Subject(UUID.randomUUID(), "Science"), 80);
+        }};
+        System.out.println(addStudent(student, subject));;
+        System.out.println();
+
+        System.out.println(addSubject(List.of(student), new Subject(UUID.randomUUID(), "English")));;
+        System.out.println();
+
+        System.out.println(addSubjectForStudent(student, new Subject(UUID.randomUUID(), "English"), 80));;
+        System.out.println();
+
+        printStudents();
+
+        addStudentForSubject(new Subject(UUID.randomUUID(), "French"), student);
+        System.out.println(subjects);
+        System.out.println();
+
+        removeStudentForSubject(new Subject(UUID.randomUUID(), "English"), student);
+        System.out.println(subjects);
+        System.out.println();
+
+        System.out.println(removeStudent(student));
+        System.out.println(students);
+        System.out.println();
+
+        printSubject();
+    }
+
+    public static Map<Student, Map<Subject, Integer>> addStudent(Student student, Map<Subject, Integer> subject) {
+        students.put(student, subject);
+        return Map.of(student, students.get(student));
+    }
+
+    public static Map<Student, Map<Subject, Integer>> addSubjectForStudent(Student student, Subject subject, Integer marks) {
+        students.computeIfAbsent(student, k -> new HashMap<>()).put(subject, marks);
+        return Map.of(student, students.get(student));
+    }
+
+    public static boolean removeStudent(Student student) {
+        return students.remove(student) != null;
+    }
+
+    public static void printStudents() {
+        System.out.println();
+        for (Map.Entry<Student, Map<Subject, Integer>> entry : students.entrySet()) {
+            System.out.println(entry);
+        }
+        System.out.println();
+    }
+
+    public static Map<Subject, List<Student>> addSubject(List<Student> students, Subject subject) {
+        for (Student student : students) {
+            subjects.computeIfAbsent(subject, k -> new ArrayList<>()).add(student);
+        }
+        return Map.of(subject, subjects.get(subject));
+    }
+
+    public static Map<Subject, List<Student>> addStudentForSubject(Subject subject, Student student) {
+        subjects.computeIfAbsent(subject, k -> new ArrayList<>()).add(student);
+        return Map.of(subject, subjects.get(subject));
+    }
+
+    public static boolean removeStudentForSubject(Subject subject, Student student) {
+        if (subjects.containsKey(subject)) {
+            for (Map.Entry<Subject, List<Student>> entry : subjects.entrySet()) {
+                if (entry.getKey().equals(subject)) {
+                    entry.getValue().remove(student);
+                }
+
+            }
+        }
+        return true;
+    }
+
+    public static void printSubject() {
+        System.out.println();
+        for (Map.Entry<Subject, List<Student>> entry : subjects.entrySet()) {
+            System.out.println(entry);
+        }
+        System.out.println();
+    }
+}
---
+++ src/main/java/faang/school/godbless/double_cache_cache/Student.java
@@ -0,0 +1,19 @@
+package faang.school.godbless.double_cache_cache;
+
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+import java.util.UUID;
+
+@Setter
+@Getter
+@AllArgsConstructor
+@ToString
+@EqualsAndHashCode
+public class Student {
+    private UUID id;
+    private String name;
+}
