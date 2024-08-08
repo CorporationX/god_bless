8840746af0b296315e10ca93df750f92a26d6841
Ivan <spvanym@gmail.com>
2024-08-09 00:24:30 +0300

    Игра престолов SMI

--- src/main/java/game/of/trone/Main.java
+++ src/main/java/game/of/trone/Main.java
@@ -6,17 +6,18 @@ import java.util.Map;
 public class Main {
     public static void main(String[] args) {
         Map<String, House> test = new HashMap<>();
-        test.put("Старк", new House("Старк","СтаркГерб"));
-        test.put("Ланнистер", new House("Ланнистер","ЛаннистерГерб"));
-        test.put("Баратеон", new House("Баратеон","БаратеонГерб"));
+        test.put("Старк", new House("Старк", "СтаркГерб"));
+        test.put("Ланнистер", new House("Ланнистер", "ЛаннистерГерб"));
+        test.put("Баратеон", new House("Баратеон", "БаратеонГерб"));
         String s = "Спирьков";
-        House.addFamily(s,test);
+        House.addFamily(s, test);
 
-        for (Map.Entry<String, House> pair : test.entrySet()){
+        for (Map.Entry<String, House> pair : test.entrySet()) {
             System.out.println(pair.getKey() + " : " + pair.getValue());
         }
 
-        House.deleteFamily(s,test);
+        House.deleteFamily(s, test);
         House.searchFamily("Старк", test);
         House.allHouse(test);
+    }
 }
