c81de014ae7604b1ffe198743044e40b5b712572
Denis Artamonov <dns.artamonov@gmail.com>
2024-08-08 16:17:10 +0300

    BJS2-20211 begin work

---
+++ src/main/java/faang/school/godbless/BJS2_20211/Main.java
@@ -0,0 +1,15 @@
+package faang.school.godbless.BJS2_20211;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class Main {
+    public static void main(String[] args) {
+        Map<String, List<WebPage>> map = new HashMap<>();
+    }
+
+    public static void indexWebPage(WebPage webPage) {
+
+    }
+}
