b4be966ca46a60e841a44d7f520effb64753b840
aephrodita <146974539+aephrodita@users.noreply.github.com>
2024-08-08 18:34:54 +0500

    18420

--- src/main/java/faang/school/godbless/beksultan2005/User.java
+++ src/main/java/faang/school/godbless/beksultan2005/User.java
@@ -3,25 +3,25 @@ package faang.school.godbless.beksultan2005;
 import java.util.Set;
 
 public class User {
+    private static final Set<String> VALID_JOBS = Set.of("Google", "Uber", "Amazon");
+    private static final Set<String> VALID_ADDRESSES = Set.of("London", "New York", "Amsterdam");
     private String name;
     private int age;
     private String placeWork;
     private String address;
-    private final Set<String> VALID_JOBS = Set.of("Google", "Uber", "Amazon");
-    private final Set<String> VALID_ADDRESSES = Set.of("London", "New York", "Amsterdam");
 
     public User(String name, int age, String placeWork, String address) {
         if (name == null || name.isEmpty()) {
-            throw new java.lang.IllegalArgumentException("name is empty");
+            throw new IllegalArgumentException("name is empty");
         }
         if (age < 18) {
-            throw new java.lang.IllegalArgumentException("age small 18");
+            throw new IllegalArgumentException("age small 18");
         }
         if (!VALID_JOBS.contains(placeWork)) {
-            throw new java.lang.IllegalArgumentException("Error");
+            throw new IllegalArgumentException("Error");
         }
         if (!VALID_ADDRESSES.contains(address)) {
-            throw new java.lang.IllegalArgumentException("Error");
+            throw new IllegalArgumentException("Error");
         }
         this.name = name;
         this.age = age;
