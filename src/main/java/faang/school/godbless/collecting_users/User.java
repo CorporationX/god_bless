b27a750eab218f155b6af8c75285aaa08a3a182e
Varvara Stepnova <stepnova.varya@yandex.ru>
2024-08-08 06:37:53 +0300

    BJS2-18403-collecting_users: added equals and hashCode for User, changed method findHobbyLovers

--- src/main/java/faang/school/godbless/collecting_users/User.java
+++ src/main/java/faang/school/godbless/collecting_users/User.java
@@ -5,14 +5,28 @@ import lombok.ToString;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Objects;
 import java.util.Set;
 
 @ToString
 public class User {
-    private int id;
-    private String name;
-    private int age;
-    private Set<String> activities;
+    private final int id;
+    private final String name;
+    private final int age;
+    private final Set<String> activities;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        User user = (User) o;
+        return id == user.id && age == user.age && Objects.equals(name, user.name) && Objects.equals(activities, user.activities);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, name, age, activities);
+    }
 
     public User(int id, String name, int age, Set<String> activities) {
         this.id = id;
@@ -28,6 +42,7 @@ public class User {
             for (String activity : activitySet) {
                 if (user.activities.contains(activity)) {
                     userMap.put(user, activity);
+                    break;
                 }
             }
         }
