4a62ff3167a0cdab4c777092d2d489c9e733b4e3
aephrodita <146974539+aephrodita@users.noreply.github.com>
2024-08-08 18:53:00 +0500

    18420

--- src/main/java/faang/school/godbless/beksultan2005/Battlefield.java
+++ src/main/java/faang/school/godbless/beksultan2005/Battlefield.java
@@ -11,7 +11,7 @@ public class Battlefield {
         this.hero2 = hero2;
     }
 
-    public void battle(){
+    public void battle() {
         float power1 = 0, power2 = 0;
         for (Map.Entry<Creature, Integer> entry : hero1.getFaction().entrySet()) {
             power1 += (entry.getKey().getDamage() * entry.getValue());
@@ -19,13 +19,13 @@ public class Battlefield {
         for (Map.Entry<Creature, Integer> entry : hero2.getFaction().entrySet()) {
             power2 += (entry.getKey().getDamage() * entry.getValue());
         }
-        power1 = power1 * hero1.getExperience()/10 * hero1.getLevel();
-        power2 = power2 * hero2.getExperience()/10 * hero1.getLevel();
-        if(power1 > power2){
+        power1 = power1 * hero1.getExperience() / 10 * hero1.getLevel();
+        power2 = power2 * hero2.getExperience() / 10 * hero1.getLevel();
+        if (power1 > power2) {
             System.out.println(hero1.getName() + " win!");
-        }else if(power2 > power1){
+        } else if (power2 > power1) {
             System.out.println(hero2.getName() + " win!");
-        }else{
+        } else {
             System.out.println("There is no winner!");
         }
     }
