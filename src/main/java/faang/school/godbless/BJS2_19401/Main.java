1afe4541ee2618c46139be4227e9844eaca2312b
Amirhan Almeev <almeevamirhan294@gmail.com>
2024-08-08 13:02:31 +0300

    Разбираем склад Амазон решение

--- src/main/java/faang/school/godbless/BJS2_19401/Main.java
+++ src/main/java/faang/school/godbless/BJS2_19401/Main.java
@@ -1,12 +1,14 @@
 package faang.school.godbless.BJS2_19401;
 
 import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
 import java.util.Set;
 
 public class Main {
     public static void main(String[] args) {
         Set<Product> productSet = new HashSet<>();
-        Product bmw = new Product(1, "bmw", "car");
+        Product bmw = new Product(1, "Bmw", "car");
         Product volkswagen = new Product(2, "Volkswagen", "car");
         Product boeing747 = new Product(5, "Boeing 747", "airplane");
         Product boeing737 = new Product(6, "Boeing 737", "airplane");
@@ -15,8 +17,8 @@ public class Main {
         productSet.add(boeing747);
         productSet.add(boeing737);
 
-        Product.GroupingProductsByCategory(productSet);
-
-
+        for (Map.Entry<String, List<Product>> entry : Product.GroupingProductsByCategory(productSet).entrySet()) {
+            System.out.println(entry.getKey() + " " + entry.getValue());
+        }
     }
 }
--- src/main/java/faang/school/godbless/BJS2_19401/Product.java
+++ src/main/java/faang/school/godbless/BJS2_19401/Product.java
@@ -1,13 +1,14 @@
 package faang.school.godbless.BJS2_19401;
 
 import lombok.AllArgsConstructor;
+import lombok.ToString;
 
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
-
+@ToString
 @AllArgsConstructor
 public class Product {
     private int id;
@@ -16,15 +17,10 @@ public class Product {
 
     public static Map<String, List<Product>> GroupingProductsByCategory(Set<Product> productSet) {
         Map<String, List<Product>> productMap = new HashMap<>();
-        List<Product> productList = new ArrayList<>();
+
         for (Product product : productSet) {
-            if (productList.contains(product.category)) {
-                productList.add(product);
-                productMap.put(product.category, productList);
-            } else {
-                List<Product> newList = new ArrayList<>();
-                productMap.put(product.category, newList);
-            }
+            productMap.putIfAbsent(product.category, new ArrayList<>());
+            productMap.get(product.category).add(product);
         }
         return productMap;
     }
