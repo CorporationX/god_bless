ed0d64603319443d0ee942089adbfe2ac97f6c1b
Okabe <mathew.mackonoha@gmail.com>
2024-08-08 11:30:51 +0300

    Commiting "Vesteros Library" task 2

--- src/main/java/Mathew/Book.java
+++ src/main/java/Mathew/Book.java
@@ -2,10 +2,8 @@ package Mathew;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
-import lombok.EqualsAndHashCode;
 
 @Data
-@EqualsAndHashCode
 @AllArgsConstructor
 public class Book {
     private String title;
