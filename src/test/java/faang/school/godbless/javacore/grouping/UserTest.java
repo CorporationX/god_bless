a0822adb3b260f8fb1afcf5db15b1efa5a2e1e72
Aleksandr Andreev <andreev.aleksandr.spb@gmail.com>
2024-08-08 20:47:27 +0300

    BJS2-18548 Регистрация пользователей

--- src/test/java/faang/school/godbless/javacore/grouping/UserTest.java
+++ src/test/java/faang/school/godbless/javacore/grouping/UserTest.java
@@ -7,6 +7,8 @@ import java.util.List;
 import java.util.Map;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 class UserTest {
 
@@ -16,10 +18,10 @@ class UserTest {
         int age30 = 30;
         int age28 = 28;
 
-        User user1 = new User("Alice", age25, "ABC Corp", "123 Main St");
-        User user2 = new User("Bob", age30, "XYZ Corp", "456 Elm St");
-        User user3 = new User("Charlie", age28, "ABC Corp", "789 Oak St");
-        User user4 = new User("David", age25, "ABC Corp", "123 Main St");
+        User user1 = new User("Alice", age25, "Google", "London");
+        User user2 = new User("Bob", age30, "Google", "New York");
+        User user3 = new User("Charlie", age28, "Uber", "Amsterdam");
+        User user4 = new User("David", age25, "Amazon", "London");
         List<User> users = Arrays.asList(user1, user2, user3, user4);
 
         Map<Integer, List<User>> result = User.groupUsers(users);
@@ -35,5 +37,45 @@ class UserTest {
 
         assertEquals(1, result.get(age28).size());
         assertEquals(user2, result.get(age30).get(0));
+    }
+
+    @Test
+    void testGroupUsers_NameNotValid() {
+        IllegalArgumentException thrown = assertThrows(
+                IllegalArgumentException.class,
+                () -> new User("", 25, "Google", "London"),
+                "Expected IllegalArgumentException to throw, but it didn't"
+        );
+        assertTrue(thrown.getMessage().contains("Имя не может быть пустым"));
+    }
+
+    @Test
+    void testGroupUsers_AgeNotValid() {
+        IllegalArgumentException thrown = assertThrows(
+                IllegalArgumentException.class,
+                () -> new User("Alice", 16, "Google", "London"),
+                "Expected IllegalArgumentException to throw, but it didn't"
+        );
+        assertTrue(thrown.getMessage().contains("Возраст не может быть меньше 18"));
+    }
+
+    @Test
+    void testGroupUsers_JobNotValid() {
+        IllegalArgumentException thrown = assertThrows(
+                IllegalArgumentException.class,
+                () -> new User("Alice", 25, "KGB", "London"),
+                "Expected IllegalArgumentException to throw, but it didn't"
+        );
+        assertTrue(thrown.getMessage().contains("Место работы должно быть \"Google\", \"Uber\" или \"Amazon\""));
+    }
+
+    @Test
+    void testGroupUsers_AddressNotValid() {
+        IllegalArgumentException thrown = assertThrows(
+                IllegalArgumentException.class,
+                () -> new User("Alice", 25, "Google", "Moscow"),
+                "Expected IllegalArgumentException to throw, but it didn't"
+        );
+        assertTrue(thrown.getMessage().contains("Адрес должен быть \"London\", \"New York\" или \"Amsterdam\""));
     }
 }
