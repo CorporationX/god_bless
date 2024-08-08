75ec933aa02a64ffcb9c02a3fce3884f6bac5da3
Юрий <subbotin.yuriy3@wb.ru>
2024-08-08 20:50:08 +0300

    Fix

--- src/main/java/faang/school/godbless/googles_mini_search_engine/WebPage.java
+++ src/main/java/faang/school/godbless/googles_mini_search_engine/WebPage.java
@@ -8,11 +8,22 @@ import lombok.ToString;
 
 @Getter
 @Setter
-@AllArgsConstructor
 @ToString
 @EqualsAndHashCode
 public class WebPage {
     private String url;
     private String title;
     private String content;
+
+    public WebPage(String url, String title, String content) {
+        if (url == null || title == null || content == null) {
+            throw new IllegalArgumentException("url, title, and content cannot be null");
+        }
+        if (url.isEmpty() || title.isEmpty() || content.isEmpty()) {
+            throw new IllegalArgumentException("url, title, and content cannot be empty");
+        }
+        this.url = url;
+        this.title = title;
+        this.content = content;
+    }
 }
