5656ac8d04e16c353752bde8da2e859706445b80
Юрий <subbotin.yuriy3@wb.ru>
2024-08-08 20:48:25 +0300

    Fix

--- src/main/java/faang/school/godbless/double_cache_cache/Subject.java
+++ src/main/java/faang/school/godbless/double_cache_cache/Subject.java
@@ -1,6 +1,5 @@
 package faang.school.godbless.double_cache_cache;
 
-import lombok.AllArgsConstructor;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.Setter;
@@ -10,10 +9,26 @@ import java.util.UUID;
 
 @Setter
 @Getter
-@AllArgsConstructor
 @ToString
 @EqualsAndHashCode
 public class Subject {
     private UUID id;
     private String name;
+
+    public Subject(UUID id, String name) {
+        if (id == null) {
+            throw new IllegalArgumentException("Id cannot be null");
+        }
+        if (name == null) {
+            throw new IllegalArgumentException("Name cannot be null");
+        }
+        if (name.isEmpty()) {
+            throw new IllegalArgumentException("Name cannot be empty");
+        }
+        if (name.length() > 100) {
+            throw new IllegalArgumentException("Name cannot be more than 100 characters");
+        }
+        this.id = id;
+        this.name = name;
+    }
 }
