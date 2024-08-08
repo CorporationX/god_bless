512e435dbbc31582bb3c9a3949c480d04171caa8
Varvara Stepnova <stepnova.varya@yandex.ru>
2024-08-08 06:48:40 +0300

    BJS2-18855-turn_around: changed method reverse

--- src/main/java/faang/school/godbless/turn_around/Example.java
+++ src/main/java/faang/school/godbless/turn_around/Example.java
@@ -1,15 +1,15 @@
 package faang.school.godbless.turn_around;
 
-import java.util.Arrays;
-
 public class Example {
 
     public static void reverse(int[] numbers) {
-        int[] newArray = new int[numbers.length];
-
-        for (int i = 0; i < numbers.length; i++) {
-            newArray[numbers.length - 1 - i] = numbers[i];
+        for (int i = 0; i < numbers.length / 2; i++) {
+            int tmp = numbers[i];
+            numbers[i] = numbers[numbers.length - i - 1];
+            numbers[numbers.length - i - 1] = tmp;
         }
-        System.out.println(Arrays.toString(newArray));
+        for (int number : numbers) {
+            System.out.println(number);
+        }
     }
 }
