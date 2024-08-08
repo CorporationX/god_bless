760b85ecbdde64775fcd73028f05b223011b579f
Bobiktrans <Bobicktrnas@yandex.ru>
2024-08-08 19:56:13 +0300

    BJS2-18984

--- src/main/java/faang/school/godbless/Application.java
+++ src/main/java/faang/school/godbless/Application.java
@@ -2,6 +2,26 @@ package faang.school.godbless;
 
 public class Application {
     public static void main(String[] args) {
-        System.out.println("God Bless!");
+        Hero hero1 = new Hero("Sir Christian", "Castle");
+        Hero hero2 = new Hero("Vey", "Necropolis");
+
+        Creature pikeman = new Pikeman(50);
+        Creature griffin = new Griffin(20);
+        Creature swordman = new Swordman(30);
+        Creature angel = new Angel(5);
+
+        hero1.addCreature(pikeman, 50);
+        hero1.addCreature(griffin, 20);
+        hero1.addCreature(swordman, 30);
+        hero1.addCreature(angel, 5);
+
+        Creature skeleton = new Pikeman(100);
+        Creature vampire = new Griffin(10);
+
+        hero2.addCreature(skeleton, 100);
+        hero2.addCreature(vampire, 10);
+
+        Battlefield battlefield = new Battlefield(hero1, hero2);
+        battlefield.battle();
     }
 }
---
+++ src/main/java/faang/school/godbless/Pikeman.java
@@ -0,0 +1,7 @@
+package faang.school.godbless;
+
+public class Pikeman extends Creature {
+    public Pikeman(int quantity) {
+        super("Pikeman", 1,4,7,5, quantity);
+    }
+}
