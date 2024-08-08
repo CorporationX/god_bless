0c85585b521c519674d8ad433189656c08f2c752
Владислав Воронов <iPlesk90@yandex.ru>
2024-08-08 08:44:15 +0300

    Исправил название переменной, теперь соответствует требованиям!

--- src/main/java/faang/school/godbless/Example.java
+++ src/main/java/faang/school/godbless/Example.java
@@ -2,12 +2,12 @@ package faang.school.godbless;
 
 public class Example {
     public static int[] reverse(int[] arrayInt) {
-        int[] EmptyArrayInt = new int[arrayInt.length];
+        int[] emptyArrayInt = new int[arrayInt.length];
         int j = arrayInt.length - 1;
-        for (int i = 0; i < EmptyArrayInt.length; i++) {
-            EmptyArrayInt[j] = arrayInt[i];
+        for (int i = 0; i < emptyArrayInt.length; i++) {
+            emptyArrayInt[j] = arrayInt[i];
             j--;
         }
-        return EmptyArrayInt;
+        return emptyArrayInt;
     }
 }
