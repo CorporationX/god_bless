0abe56c74f6220e85df2c5e0fac230866523d6e7
Юрий <subbotin.yuriy3@wb.ru>
2024-08-08 20:31:40 +0300

    Fix

--- src/main/java/faang/school/godbless/library_of_westeros/Book.java
+++ src/main/java/faang/school/godbless/library_of_westeros/Book.java
@@ -1,6 +1,5 @@
 package faang.school.godbless.library_of_westeros;
 
-import lombok.AllArgsConstructor;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.Setter;
@@ -8,11 +7,26 @@ import lombok.ToString;
 
 @Getter
 @Setter
-@AllArgsConstructor
 @EqualsAndHashCode
 @ToString
 public class Book {
     private String title;
     private String author;
     private int year;
+
+    public Book(String title, String author, int year) {
+        if (title.isEmpty()) {
+            throw new IllegalArgumentException("Title cannot be empty");
+        }
+        if (author.isEmpty()) {
+            throw new IllegalArgumentException("Author: Firstname Lastname be empty");
+        }
+        if (year < 0) {
+            throw new IllegalArgumentException("Year cannot be negative");
+        }
+
+        this.title = title;
+        this.author = author;
+        this.year = year;
+    }
 }
--- src/main/java/faang/school/godbless/library_of_westeros/Main.java
+++ src/main/java/faang/school/godbless/library_of_westeros/Main.java
@@ -4,37 +4,34 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class Main {
+    private static final Map<Book, String> books = new HashMap<>();
+
     public static void main(String[] args) {
-        Map<Book, String> books = new HashMap<>();
-
-        books.put(new Book("The Hobbit", "J.R.R. Tolkien", 1937), "1");
-        books.put(new Book("The Fellowship of the Ring", "J.R.R. Tolkien", 1954), "2");
-        books.put(new Book("The Return of the King", "J.R.R. Tolkien", 1955), "3");
-
-        addBook(books, new Book("The Two Towers", "J.R.R. Tolkien", 1954), "3");
+        addBook(new Book("The Return of the King", "J.R.R. Tolkien", 1955), "1");
+        addBook(new Book("The Two Towers", "J.R.R. Tolkien", 1954), "3");
         System.out.println(books);
 
-        removeBook(books, new Book("The Two Towers", "J.R.R. Tolkien", 1954));
+        removeBook(new Book("The Two Towers", "J.R.R. Tolkien", 1954));
         System.out.println(books);
 
-        printIfoBookHerLocation(books, new Book("The Return of the King", "J.R.R. Tolkien", 1955));
+        printIfoBookHerLocation(new Book("The Return of the King", "J.R.R. Tolkien", 1955));
 
-        printInfoAllBooks(books);
+        printInfoAllBooks();
     }
 
-        public static void addBook(Map<Book, String> books, Book book, String shelf) {
+        public static void addBook(Book book, String shelf) {
             books.put(book, shelf);
         }
 
-        public static void removeBook(Map<Book, String> books, Book book) {
+        public static void removeBook(Book book) {
             books.remove(book);
         }
 
-        public static void printIfoBookHerLocation(Map<Book, String> books, Book book) {
-            System.out.println(books.get(book));
+        public static void printIfoBookHerLocation(Book book) {
+            System.out.println(books.getOrDefault(book, "Not found"));
         }
 
-        public static void printInfoAllBooks(Map<Book, String> books) {
+        public static void printInfoAllBooks() {
             for (Map.Entry<Book, String> entry : books.entrySet()) {
                 System.out.println(entry.getKey());
             }
