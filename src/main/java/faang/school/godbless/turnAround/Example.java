068ae073dc1234d478ad620ff21e519b5bf39275
ifef228 <ifef228@gmail.com>
2024-08-08 08:15:59 +0300

    поправил вопросы из ревью

--- src/main/java/faang/school/godbless/turnAround/Example.java
+++ src/main/java/faang/school/godbless/turnAround/Example.java
@@ -1,14 +1,12 @@
 package faang.school.godbless.turnAround;
 
-import java.util.Arrays;
-
 public class Example {
     public static void reverse(int[] arr){
         for (int i = 0; i < arr.length/2; i++) {
             int buf;
             buf = arr[i];
-            arr[i] = arr[arr.length-1-i];
-            arr[arr.length-1-i] = buf;
+            arr[i] = arr[arr.length - 1 - i];
+            arr[arr.length - 1 - i] = buf;
         }
     }
 }
