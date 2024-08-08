25b82b51087fc3fd53f4e7decc2d71a2c42e3bbb
pgusev <pgusev@fil-it.ru>
2024-08-08 10:56:06 +0300

    BJS2-18453 - Правки после CR

--- src/test/java/UserTest.java
+++ src/test/java/UserTest.java
@@ -1,55 +1,45 @@
 import faang.school.godbless.User;
 import org.junit.jupiter.api.Test;
 
-import java.util.*;
-
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 public class UserTest {
 
     @Test
     void creationIncorrectAgeUserTest() {
-
-        try {
-            User user = new User("BOB", 10, "Uber", "Amsterdam");
-        } catch (IllegalArgumentException ilae) {
-            assertEquals(ilae.getMessage(), "Age couldn't be less than 18");
-        }
+        assertThrows(IllegalArgumentException.class,
+                () -> new User("BOB", 10, "Uber", "Amsterdam"),
+                "Age couldn't be less than 18");
     }
 
     @Test
     void creationIncorrectNameUserTest() {
+        assertThrows(IllegalArgumentException.class,
+                () -> new User(null, 18, "Uber", "Amsterdam"),
+                "Name couldn't be empty");
 
-        try {
-            User user = new User(null, 18, "Uber", "Amsterdam");
-        } catch (IllegalArgumentException ilae) {
-            assertEquals(ilae.getMessage(), "Name couldn't be empty");
-        }
+        assertThrows(IllegalArgumentException.class,
+                () -> new User("               ", 18, "Uber", "Amsterdam"),
+                "Name couldn't be empty");
     }
 
     @Test
     void creationIncorrectJobUserTest() {
-
-        try {
-            User user = new User("BOB", 18, "Unknown", "Amsterdam");
-        } catch (IllegalArgumentException ilae) {
-            assertEquals(ilae.getMessage(), "Job should be from [Amazon, Uber, Google]");
-        }
+        assertThrows(IllegalArgumentException.class,
+                () -> new User("BOB", 18, "Unknown", "Amsterdam"),
+                "Job should be from [Amazon, Uber, Google]");
     }
 
     @Test
     void creationIncorrectAddressUserTest() {
-
-        try {
-            User user = new User("BOB", 18, "Amazon", "Russia");
-        } catch (IllegalArgumentException ilae) {
-            assertEquals(ilae.getMessage(), "Address should be from [New York, London, Amsterdam]");
-        }
+        assertThrows(IllegalArgumentException.class,
+                () -> new User("BOB", 18, "Amazon", "Russia"),
+                "Address should be from [New York, London, Amsterdam]");
     }
 
     @Test
     void creationCorrectUserTest() {
-
         User user = new User("BOB", 18, "Amazon", "London");
         assertEquals(user, new User("BOB", 18, "Amazon", "London"));
     }
