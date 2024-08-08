db62fd1e71c402eb1fdc6d409c010657adcb76ca
aephrodita <146974539+aephrodita@users.noreply.github.com>
2024-08-08 19:02:51 +0500

    18420

--- src/main/java/faang/school/godbless/beksultan2005/Book.java
+++ src/main/java/faang/school/godbless/beksultan2005/Book.java
@@ -9,5 +9,4 @@ public class Book {
     private String title;
     private String author;
     private int year;
-
 }
--- src/main/java/faang/school/godbless/beksultan2005/Main.java
+++ src/main/java/faang/school/godbless/beksultan2005/Main.java
@@ -6,19 +6,19 @@ import java.util.Map;
 public class Main {
     private static Map<Book, String> cacheBook = new HashMap<>();
 
-    public static void addBook(Book book, String location){
+    public static void addBook(Book book, String location) {
         cacheBook.put(book, location);
     }
 
-    public static void removeBook(String title, String author, int year){
+    public static void removeBook(String title, String author, int year) {
         cacheBook.remove(new Book(title, author, year));
     }
 
-    public static String getLocation(String title, String author, int year){
+    public static String getLocation(String title, String author, int year) {
         return cacheBook.get(new Book(title, author, year));
     }
 
-    public static void getAllCacheBook(){
+    public static void getAllCacheBook() {
         cacheBook.forEach((key, value) -> System.out.println(key.toString() + ": " + value));
     }
 
