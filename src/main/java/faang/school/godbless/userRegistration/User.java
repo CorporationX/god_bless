9554e8107d1942c95922f754ad394f1427b85780
TheGreatLucky <danke_villetti@mail.ru>
2024-08-09 18:32:50 +0500

    corrected comments

--- src/main/java/faang/school/godbless/userRegistration/User.java
+++ src/main/java/faang/school/godbless/userRegistration/User.java
@@ -3,6 +3,7 @@ package faang.school.godbless.userRegistration;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 import lombok.AllArgsConstructor;
+import lombok.NonNull;
 
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -12,29 +13,29 @@ import java.util.Set;
 import java.util.List;
 
 
-
 @Data
 public class User {
     private String name;
     private int age;
     private String address;
     private String placeOfWork;
+    private int minAge = 18;
+    private int maxAge = 125;
+    static final Set<String> VALID_JOBS = Set.of("Google", "Uber", "Amazon");
+    static final Set<String> VALID_ADDRESSES = Set.of("London", "New York", "Amsterdam");
 
-    static Set<String> VALID_JOBS = Set.of("Google", "Uber", "Amazon");
-    static Set<String> VALID_ADDRESSES = Set.of("London", "New York", "Amsterdam");
 
-
-    public User(String name, int age, String address, String placeOfWork) {
-        if(name.isEmpty()){
+    public User(@NonNull String name, int age, @NonNull String address, @NonNull String placeOfWork) {
+        if (name.isBlank()) {
             throw new IllegalArgumentException("name is empty");
         }
-        if (age < 18 || age >125){
-            throw new IllegalArgumentException("age less then 18 or very much");
+        if (age < minAge || age > maxAge) {
+            throw new IllegalArgumentException("age less then " + minAge + " or very much");
         }
-        if (!VALID_JOBS.contains(placeOfWork)){
+        if (!VALID_JOBS.contains(placeOfWork)) {
             throw new IllegalArgumentException("Choose correct place of work");
         }
-        if (!VALID_ADDRESSES.contains(address)){
+        if (!VALID_ADDRESSES.contains(address)) {
             throw new IllegalArgumentException("Choose correct address");
         }
         this.name = name;
