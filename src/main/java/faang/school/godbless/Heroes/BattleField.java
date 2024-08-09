3ca706edcecb993b42756791870dde51b681cab8
Svirlex <aleksandrsviridov446@gmail.com>
2024-08-09 17:56:18 +0300

    BJS2-19026 Developed a Heroes of Might and Magic III copy

--- src/main/java/faang/school/godbless/Heroes/BattleField.java
+++ src/main/java/faang/school/godbless/Heroes/BattleField.java
@@ -18,15 +18,20 @@ public class BattleField {
         System.out.println(hero1.getName() + " Total Damage: " + damageHero1);
         System.out.println(hero2.getName() + " Total Damage: " + damageHero2);
 
-        int winRate = new Random().nextInt(damageHero1 + damageHero2 + 1);
+        double winPercentage = (hero1.getTotalArmyDamage() + hero2.getTotalArmyDamage()) / 100;
+        int hero1Rate = (int) Math.floor((double) hero1.getTotalArmyDamage()/winPercentage);
+        int hero2Rate = (int) Math.ceil((double) hero2.getTotalArmyDamage()/winPercentage);
+        System.out.println("Win Rate: " + hero1Rate + "% VS " +  + hero2Rate + "%");
+
+        int winRate = new Random().nextInt(0,101);
 
         if (winRate == 0){
             System.out.println("It's a tie");
             return null;
-        }else if (winRate <= damageHero1){
+        }else if (winRate <= hero1Rate){
             System.out.println(hero1.getName() + " wins!");
             return hero1;
-        } else if (winRate > damageHero1) {
+        } else if (winRate > hero2Rate) {
             System.out.println(hero2.getName() + " wins!");
             return hero2;
         } else {
