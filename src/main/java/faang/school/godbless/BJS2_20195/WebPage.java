9943a93f055e798a37271c8907af82c8ea1be87a
Robert Oguzov <robertoguzov2@gmail.com>
2024-08-09 12:50:42 +0300

    Написал WebPage

---
+++ src/main/java/faang/school/godbless/BJS2_20195/WebPage.java
@@ -0,0 +1,14 @@
+package faang.school.godbless.BJS2_20195;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.ToString;
+
+@AllArgsConstructor
+@Getter
+@ToString
+public class WebPage {
+    private String url;
+    private String title;
+    private String content;
+}
