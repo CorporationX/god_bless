e9a40c6e462d6d86b748aeedaaeead4c9e1b1b35
Ivan <spvanym@gmail.com>
2024-08-09 19:29:07 +0300

    Герои Меча и Магии SMI

---
+++ src/main/java/heroOfSwordAndMagic/Swordman.java
@@ -0,0 +1,13 @@
+package heroOfSwordAndMagic;
+
+public class Swordman extends Creature{
+
+
+    public Swordman(String name) {
+        super(name);
+        lvl = 50;
+        damage = 250;
+        defense = 300;
+        speed = 100;
+    }
+}
