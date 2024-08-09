4d0399fa2d602d52d1697d38de71da5f0565675f
Дмитрий Бахтин <dmitridorje@gmail.com>
2024-08-09 16:08:01 +0300

    Добавлены константые поля для минимальных температуры и влажности

--- src/main/java/faang/school/godbless/caching/MockWeatherService.java
+++ src/main/java/faang/school/godbless/caching/MockWeatherService.java
@@ -3,10 +3,15 @@ package faang.school.godbless.caching;
 import java.util.Random;
 
 public class MockWeatherService {
+    private static final int MIN_TEMP = -10;
+    private static final int MAX_TEMP = 40;
+    private static final int MIN_HUMIDITY = 10;
+    private static final int MAX_HUMIDITY = 90;
+
     public WeatherData fetchWeather(String city) {
         Random random = new Random();
-        double temperature = -10 + (40 - (-10)) * random.nextDouble();
-        int humidity = random.nextInt(90 - 10 + 1) + 10;
+        double temperature = MIN_TEMP + (MAX_TEMP - (MIN_TEMP)) * random.nextDouble();
+        int humidity = random.nextInt(MAX_HUMIDITY - MIN_HUMIDITY + 1) + MIN_HUMIDITY;
         return new WeatherData(city, temperature, humidity);
     }
 }
