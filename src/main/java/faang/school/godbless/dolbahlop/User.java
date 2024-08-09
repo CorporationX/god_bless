7446e9960b2ea36d45dd57ba5780f0ef288b7230
Bogdan <bogdanrazumov125@gmail.com>
2024-08-09 15:58:27 +0300

    Выполнил задачу, написал тесты, поработал над code Style

--- src/main/java/faang/school/godbless/dolbahlop/User.java
+++ src/main/java/faang/school/godbless/dolbahlop/User.java
@@ -1,36 +1,41 @@
 package faang.school.godbless.dolbahlop;
 
 import lombok.AllArgsConstructor;
-import lombok.Data;
 
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-@Data
 @AllArgsConstructor
 public class User {
+
     private String name;
     private int age;
-    private String placeOfWork;
+    private String workplace;
     private String address;
 
-   public static Map<Integer, List<User>> groupUsers(List<User> users){
-       if (users == null){
-           return null;
-       }
-       Map<Integer, List<User>> mapOfAge = new HashMap<>();
-       for (User user : users){
-           if (mapOfAge.containsKey(user.getAge())){
-               List<User> listOfUsers = new ArrayList<>();
-               listOfUsers.add(user);
-               mapOfAge.put(user.getAge(), listOfUsers);
-           }
-           else {
-               mapOfAge.get(user.getAge()).add((user));
-           }
-       }
-       return mapOfAge;
-   }
+    // Группировки пользователей по возрасту
+    public static Map<Integer, List<User>> groupUsersByAge(List<User> users){
+        Map<Integer, List<User>> usersGroupedByAge = new HashMap<>();
+
+        for (User user : users){
+            addUserToAgeGroup(usersGroupedByAge, user);
+        }
+
+        return usersGroupedByAge;
+    }
+
+    // Добавляет пользователя в его возрастную группу
+    private static void addUserToAgeGroup(Map<Integer, List<User>> usersGroupedByAge, User user){
+        int age = user.age;
+        List<User> usersWithinTheSameAgeGroup = getUsersWithinTheSameAgeGroup(usersGroupedByAge, age);
+        usersWithinTheSameAgeGroup.add(user);
+        usersGroupedByAge.put(age, usersWithinTheSameAgeGroup);
+    }
+
+    //Получает список пользователей с таким же возрастом
+    private static List<User> getUsersWithinTheSameAgeGroup(Map<Integer, List<User>> usersGroupedByAge, int age) {
+        return usersGroupedByAge.getOrDefault(age, new ArrayList<>());
+    }
 }
