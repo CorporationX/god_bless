a62171061bfaccf3c5806008741090183a36f161
Okabe <mathew.mackonoha@gmail.com>
2024-08-09 13:24:36 +0300

    Commiting "Double Cache-Cache" task 1

---
+++ src/main/java/faang/school/godbless/Double_Cache_Chache/Student.java
@@ -0,0 +1,13 @@
+package faang.school.godbless.Double_Cache_Chache;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+public class Student {
+    private int id;
+    private String name;
+}
---
+++ src/main/java/faang/school/godbless/Double_Cache_Chache/Subject.java
@@ -0,0 +1,13 @@
+package faang.school.godbless.Double_Cache_Chache;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+public class Subject {
+    private int id;
+    private String name;
+}
