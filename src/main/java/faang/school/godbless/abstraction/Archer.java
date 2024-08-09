438b2b5f78864fdc70d6782dcbe76521bccd9666
Mikhail Tupitsyn <mikhail.tupitsyn@gmail.com>
2024-08-09 20:31:48 +0300

    review fixes

--- src/main/java/faang/school/godbless/abstraction/Archer.java
+++ src/main/java/faang/school/godbless/abstraction/Archer.java
@@ -2,15 +2,7 @@ package faang.school.godbless.abstraction;
 
 public class Archer extends Character {
     public Archer(String name) {
-        super(name);
-
-        this.strength = 3;
-        this.agility = 10;
-        this.intelligence = 5;
-    }
-
-    public Archer(String name, int strength, int agility, int intelligence, int health) {
-        super(name, strength, agility, intelligence, health);
+        super(name, 3, 10, 5, 100);
     }
 
     @Override
--- src/main/java/faang/school/godbless/abstraction/Warrior.java
+++ src/main/java/faang/school/godbless/abstraction/Warrior.java
@@ -2,15 +2,7 @@ package faang.school.godbless.abstraction;
 
 public class Warrior extends Character {
     public Warrior(String name) {
-        super(name);
-
-        this.strength = 10;
-        this.agility = 5;
-        this.intelligence = 3;
-    }
-
-    public Warrior(String name, int strength, int agility, int intelligence, int health) {
-        super(name, strength, agility, intelligence, health);
+        super(name, 10, 5, 3, 100);
     }
 
     @Override
