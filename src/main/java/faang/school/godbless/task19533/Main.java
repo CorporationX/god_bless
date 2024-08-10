c8ccbec54574937d8c8ca59966c65f171a04d20b
Chibix <chibisovanastena@gmail.com>
2024-08-10 15:29:11 +0300

    19533

--- src/main/java/faang/school/godbless/task19533/Main.java
+++ src/main/java/faang/school/godbless/task19533/Main.java
@@ -1,18 +1,22 @@
 package faang.school.godbless.task19533;
 
 import java.util.*;
+import java.util.stream.Stream;
 
 public class Main {
     public static List<Student> listOfStudents;
     public static void main(String[] args) {
         listOfStudents = new ArrayList<>();
-        Student student1 = new Student("Inna", "economic", 2017);
-        Student student2 = new Student("Igor", "math", 2017);
-        Student student3 = new Student("Nina", "economic", 2020);
-        Student student4 = new Student("Alla", "history", 2021);
-        Student student5 = new Student("Liza", "history", 2020);
+        listOfStudents.add(new Student("Inna", "economic", 2017));
+        listOfStudents.add(new Student("Igor", "math", 2017));
+        listOfStudents.add(new Student("Nina", "economic", 2020));
+        listOfStudents.add(new Student("Alla", "history", 2021));
+        listOfStudents.add(new Student("Liza", "history", 2020));
 
         System.out.println(getStudentsByYearFaculty(listOfStudents));
+        proverka();
+
+
 
     }
     public static Map<String, List<Student>> getStudentsByYearFaculty(List<Student> list){
@@ -23,21 +27,32 @@ public class Main {
         for (Student student:list) {
             yearSet.add(student.getYear());
         }
+        System.out.println(yearSet);
         for (Student student:list) {
             facultySet.add(student.getFaculty());
         }
+        System.out.println(facultySet);
         for (Student student:list) {
             facultySet.add(student.getFaculty());
-        for (Integer year:yearSet) {
-            for (String string:facultySet) {
-                String key = student.getYear() + student.getFaculty();
-                map.computeIfAbsent(key, k -> new ArrayList<>()).add(student);
-                map.get(key).add(student);
+            for (Integer year:yearSet) {
+                for (String faculty:facultySet) {
+                    String key = year + faculty;
+                    map.put(key, new ArrayList<>());
+                    if(map.get(key).equals(student.getYear()+student.getFaculty()))
+                        map.get(key).add(student);
+                }
             }
-
-        }}
+        }
         return map;
     }
+
+    public static void proverka(){
+        Map<String, List<Student>> map = getStudentsByYearFaculty(listOfStudents);
+        map.entrySet().stream()
+                .flatMap(entry -> entry.getValue().stream()) // Сплющиваем список студентов
+                .forEach(student -> System.out.println("Студент: " + student.getName() +  ", Year: " + student.getYear()));
+    }
+
 
     public static void addStudent(String name, String faculty, Integer year){
         listOfStudents.add(new Student(name, faculty, year));
