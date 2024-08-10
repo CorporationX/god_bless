d066cbc5a62529f92dfc21b366fb8413456f1ade
Robert Oguzov <robertoguzov2@gmail.com>
2024-08-10 09:44:56 +0300

    класс Student

---
+++ src/main/java/faang/school/godbless/BJS2_19913/Student.java
@@ -0,0 +1,13 @@
+package faang.school.godbless.BJS2_19913;
+
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.ToString;
+
+@ToString
+@EqualsAndHashCode
+@AllArgsConstructor
+public class Student {
+    private int id;
+    private String name;
+}
