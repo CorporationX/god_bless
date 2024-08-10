79581d6025594a1b5c5d0899afe237402a1b255d
Rashit Gareyev <gareyevrs@gmail.com>
2024-08-10 17:53:23 +0400

    nf BJS2-19952 hash hash

---
+++ src/main/java/faang/school/godbless/hash/hash/Student.java
@@ -0,0 +1,11 @@
+package faang.school.godbless.hash.hash;
+
+import lombok.EqualsAndHashCode;
+import lombok.ToString;
+
+@EqualsAndHashCode
+@ToString
+public class Student {
+    private int id;
+    private String name;
+}
