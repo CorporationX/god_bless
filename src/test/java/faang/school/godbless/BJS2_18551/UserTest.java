4efaddd0db58038cc079ce6cea2a76b25d41e4b5
Anastasia Dragomirova <nastay.dr2002@gmail.com>
2024-08-10 12:13:41 +0400

    Выполнила задачу "Коллекционируем пользователей"

---
+++ src/test/java/faang/school/godbless/BJS2_18551/UserTest.java
@@ -0,0 +1,24 @@
+package faang.school.godbless.BJS2_18551;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.Map;
+import java.util.Set;
+
+import static faang.school.godbless.BJS2_18551.Constants.*;
+import static org.junit.jupiter.api.Assertions.*;
+
+class UserTest {
+
+    private static final Map<User, String> FIND_HOBBY_LOVERS_RESULT = User.findHobbyLovers(USERS, Set.of("Swimming", "Yoga"));
+
+    @Test
+    void findHobbyLoversCorrect() {
+        assertEquals(CORRECT_USER_MAP, FIND_HOBBY_LOVERS_RESULT);
+    }
+
+    @Test
+    void findHobbyLoversIncorrect() {
+        assertNotEquals(INCORRECT_USER_MAP, FIND_HOBBY_LOVERS_RESULT);
+    }
+}
