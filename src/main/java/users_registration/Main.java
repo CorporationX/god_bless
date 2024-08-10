9635a007b78b1c5673e3dd2d6c419c38dee5ffff
Vitaly Maslyuk <maslyuk03@bk.ru>
2024-08-10 18:38:05 +0700

    Refactored User.java

--- src/main/java/users_registration/Main.java
+++ src/main/java/users_registration/Main.java
@@ -3,6 +3,7 @@ package users_registration;
 public class Main {
     public static void main(String[] args) {
         User correctUser = new User("Name", 20, "Google", "London");
+        User nullUserName = new User(null, 20, "Google", "London");
         User incorrectUserName = new User("  ", 20, "Google", "London");
         User incorrectUserAge = new User("Name", 15, "Google", "London");
         User incorrectUserJob = new User("Name", 20, "Yandex", "London");
