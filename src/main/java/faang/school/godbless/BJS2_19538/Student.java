96c58bc61f5e89b889a1d519f1085fe71e4dc520
Viktor Shpet <shpet.viktor.93@gmail.com>
2024-08-10 21:09:32 +0500

    BJS2-19538 is solved.

---
+++ src/main/java/faang/school/godbless/BJS2_19538/Student.java
@@ -0,0 +1,14 @@
+package faang.school.godbless.BJS2_19538;
+
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+
+@AllArgsConstructor
+@Getter
+@EqualsAndHashCode
+public class Student {
+    private String name;
+    private String faculty;
+    private int year;
+}
