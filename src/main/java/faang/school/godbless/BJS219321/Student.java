52518f379b4b8f13f9e6e3a36a797228ccf4eed3
aephrodita <146974539+aephrodita@users.noreply.github.com>
2024-08-10 20:23:44 +0500

    18420

---
+++ src/main/java/faang/school/godbless/BJS219321/Student.java
@@ -0,0 +1,14 @@
+package faang.school.godbless.BJS219321;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.ToString;
+
+@AllArgsConstructor
+@Getter
+@ToString
+public class Student {
+    private String name;
+    private String faculty;
+    private int year;
+}
