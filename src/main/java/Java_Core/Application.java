d0767382aa8dc9e1dddc64e8fae89f8a00c196cd
Garib_Aghayev <garibaghayev@gmail.com>
2024-08-11 01:04:35 +0300

    abstract class

---
+++ src/main/java/Java_Core/Application.java
@@ -0,0 +1,11 @@
+package Java_Core;
+
+public class Application {
+    public static void main(String[] args) {
+
+        User user = new User("Bob", 23, "Uber", "Lomndon");
+        System.out.println(user);
+
+
+    }
+}
