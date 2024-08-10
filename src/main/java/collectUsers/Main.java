7d549265393ff2e092503e5950102aa045e6a910
Abalik <aabalykov@gmail.com>
2024-08-10 14:23:37 +0300

    Задача "Регистрация пользователя". Как-то так.

--- src/main/java/collectUsers/Main.java
+++ src/main/java/collectUsers/Main.java
@@ -1,28 +1,38 @@
 package collectUsers;
 
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
 public class Main {
+    protected static List<User> users = new ArrayList<>();
     public static void main (String[] args) {
-    List<User> users = new ArrayList<>();
-    User andrey = new User("Andrey", 33,"Home", "Moscow");
-    User sabina = new User("Sabina", 18, "Sber", "Moscow");
-    User elon = new User("Elon", 53,"Tesla", "USA");
-    User noname = new User("Incognito", 33, "Null","Moscow");
 
-    users.add(andrey);
-    users.add(sabina);
-    users.add(elon);
-    users.add(noname);
+    createUser("Andrey", 33, "Home", "Moscow");
+    createUser("Sabina", 17, "Sber", "Moscow");
+    createUser("Elon", 53, "Tesla", "USA");
+    createUser("Incognito", 33, "Google", "Moscow");
+    createUser("",20,"Google", "London");
+    createUser("IdealUser", 20, "Google", "London");
+
 
     Map<Integer, List<User>> group = User.groupUsers(users);
+    System.out.println("В мапе находятся следующие пользователи:");
     for(Map.Entry<Integer, List<User>> entry: group.entrySet()){
         for(User user: entry.getValue()){
-                    System.out.println(entry.getKey()+":" + user.getFirstName()+ " " + user.getCompanyName() + " " + user.getAddress());
+                    System.out.println("Возраст: " + entry.getKey() + ". имя: " + user.getFirstName() + ". Место работы: " + user.getCompanyName() + ". Адрес: " + user.getAddress());
         }
     }
     }
+    static void createUser(String name, int age, String companyName, String address) throws IllegalArgumentException {
+        try {
+            User newUser = new User(name, age, companyName, address);
+            users.add(newUser);
+        }
+        catch (Exception e){
+            System.out.println("Создаем пользователя - имя: " + name + ". Возраст: " + age+ ". Место работы: " +companyName+ ". Адрес: " +address+ ". Ошибка валидации: " + e.getMessage());
+
+        }
+    }
+
 }
--- src/main/java/collectUsers/User.java
+++ src/main/java/collectUsers/User.java
@@ -2,23 +2,38 @@ package collectUsers;
 
 import lombok.Data;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 @Data
 public class User {
+    public static final Set<String> VALID_JOBS = Set.of("Google", "Uber", "Amazon");
+    public static final Set<String> VALID_ADDRESSES = Set.of("London", "New York", "Amsterdam");
+
     private String firstName;
     private int age;
     private String companyName;
     private String address;
 
-    public User(String firstName, int age, String companyName, String address){
-        this.firstName = firstName;
-        this.age = age;
-        this.companyName = companyName;
-        this.address = address;
+    public User(String firstName, int age, String companyName, String address) throws IllegalArgumentException{
+
+        if(firstName.length()==0) {
+            throw new IllegalArgumentException("имя не может быть пустым");
+        }
+        else if(age<18) {
+            throw new IllegalArgumentException("возраст не может быть меньше 18");
+        }
+        else if(!VALID_JOBS.contains(companyName)) {
+            throw new IllegalArgumentException("место работы должно содержаться во множестве VALID_JOBS");
+        }
+        else if(!VALID_ADDRESSES.contains(address)) {
+            throw new IllegalArgumentException("адрес должен содержаться во множестве VALID_ADDRESSES");
+        }
+        else {
+            this.firstName = firstName;
+            this.age = age;
+            this.companyName = companyName;
+            this.address = address;
+        }
     }
     public static Map<Integer, List<User>> groupUsers(List<User> users) {
         Map<Integer,List<User>> group = new HashMap<>();
