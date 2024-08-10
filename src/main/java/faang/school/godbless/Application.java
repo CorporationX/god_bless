791989781ccbff94bd075dda59bc6b8dc6cc61be
Bobiktrans <Bobicktrnas@yandex.ru>
2024-08-10 15:41:24 +0300

    BJS2-19467

--- src/main/java/faang/school/godbless/Application.java
+++ src/main/java/faang/school/godbless/Application.java
@@ -2,6 +2,20 @@ package faang.school.godbless;
 
 public class Application {
     public static void main(String[] args) {
-        System.out.println("God Bless!");
+        DataCenterService dataCenterService = new DataCenterService();
+
+        Server server1 = new Server(100, 50);
+        Server server2 = new Server(200, 70);
+        Server server3 = new Server(150, 60);
+
+        dataCenterService.addServer(server1);
+        dataCenterService.addServer(server2);
+        dataCenterService.addServer(server3);
+
+        ResourceRequest request = new ResourceRequest(50);
+        dataCenterService.allocateResources(request);
+
+        System.out.println("Total Energy Consumption: " + dataCenterService.getTotalEnergyConsumption());
+
     }
 }
