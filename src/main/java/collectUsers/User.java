2890abf499eaafcc69b922a1a6a21f4dc4644b80
Abalik <aabalykov@gmail.com>
2024-08-10 10:11:57 +0300

    Задача "Группировка пользователей по возрасту". Меня не отпускает ощущение что я тут наговнокодил :) Использовал lombok как мог (чтобы не создавать геттеры и сеттеры)

---
+++ src/main/java/collectUsers/User.java
@@ -0,0 +1,38 @@
+package collectUsers;
+
+import lombok.Data;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@Data
+public class User {
+    private String firstName;
+    private int age;
+    private String companyName;
+    private String address;
+
+    public User(String firstName, int age, String companyName, String address){
+        this.firstName = firstName;
+        this.age = age;
+        this.companyName = companyName;
+        this.address = address;
+    }
+    public static Map<Integer, List<User>> groupUsers(List<User> users) {
+        Map<Integer,List<User>> group = new HashMap<>();
+        for(User user: users){
+            if(group.get(user.getAge())==null) {
+                List<User> usersByAge = new ArrayList<>();
+                usersByAge.add(user);
+                group.put(user.getAge(),usersByAge);
+            }
+            else{
+                group.get(user.getAge()).add(user);
+            }
+        }
+        return group;
+    }
+
+}
