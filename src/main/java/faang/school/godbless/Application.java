8013febf2dcfbeec5717360e71a76833177461b1
Bobiktrans <Bobicktrnas@yandex.ru>
2024-08-10 18:43:01 +0300

    BJS2-19014

--- src/main/java/faang/school/godbless/Application.java
+++ src/main/java/faang/school/godbless/Application.java
@@ -2,6 +2,22 @@ package faang.school.godbless;
 
 public class Application {
     public static void main(String[] args) {
-        System.out.println("God Bless!");
+        Hero hero1 = new Hero("Sir Christian", "Castle", 500, 10);
+        Hero hero2 = new Hero("Vey", "Necropolis", 300, 8);
+
+        hero1.addCreature(new Pikeman(), 10);
+        hero1.addCreature(new Swordman(), 5);
+
+        hero2.addCreature(new Griffin(), 6);
+        hero2.addCreature(new Angel(), 2);
+
+        Battlefield battlefield = new Battlefield(hero1, hero2);
+        Hero winner = battlefield.battle();
+
+        if (winner != null) {
+            System.out.println("Winner: " + winner.getArmy());
+        } else {
+            System.out.println("It's a draw!");
+        }
     }
 }
