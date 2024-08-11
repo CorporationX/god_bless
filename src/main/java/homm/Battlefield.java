04b514431a371217d4a75d8969a26f04c2835f79
SharkB38 <val.wolf.ne@gmail.com>
2024-08-11 18:55:59 +0900

    fixing battle method

--- src/main/java/homm/Battlefield.java
+++ src/main/java/homm/Battlefield.java
@@ -1,6 +1,7 @@
 package homm;
 
 import homm.creatures.Creature;
+import lombok.Data;
 import lombok.NonNull;
 
 import java.util.ArrayList;
@@ -9,64 +10,24 @@ import java.util.Random;
 
 public class Battlefield {
     public static void battle(@NonNull Hero firstHero, @NonNull Hero secondHero) {
-        System.out.println(
-                "\nThe battle between Hero " + firstHero.getName() + " of " + firstHero.getFraction()
-                + " and Hero " + secondHero.getName() + " of " + secondHero.getFraction()
-        );
-        System.out.println("The battle begin!");
-        int firstHeroExp = 0;
-        int secondHeroExp = 0;
-        boolean turn = true;
-        boolean prevTurnResult = true;
-        boolean drawFlag = false;
-        while(!firstHero.getCreatures().isEmpty() && !secondHero.getCreatures().isEmpty()) {
-            if (drawFlag) {
-                break;
-            }
-            boolean currentTurnResult;
-            if (turn) {
-                int exp = doTurn(firstHero, secondHero);
-                currentTurnResult = exp > 0;
-                firstHeroExp += exp;
-                turn = false;
-            } else {
-                int exp = doTurn(secondHero, firstHero);
-                currentTurnResult = exp > 0;
-                secondHeroExp += exp;
-                turn = true;
-            }
-            if (!prevTurnResult && !currentTurnResult) {
-                drawFlag = true;
-            } else {
-                prevTurnResult = currentTurnResult;
-            }
-        }
-        if (drawFlag) {
-            System.out.println("Two armies are even! Time to retreat and grow powers!");
+        printBattleIntroduction(firstHero.getName(), secondHero.getName());
+        var result = executeBattle(firstHero, secondHero);
+        handleResult(result, firstHero, secondHero);
+    }
+
+    private static int doTurn(@NonNull Hero attacker, @NonNull Hero defender) {
+        System.out.println("\nHero " + attacker.getName() + " is attacking!");
+        System.out.println("Attack of hero: " + attacker.getDamage());
+        System.out.println("Defense of enemy: " + defender.getDefense());
+        int finalDamage = attacker.getDamage() - defender.getDefense();
+        int successAttack = 0;
+        if (finalDamage > 0) {
+            successAttack = attackOnArmy(finalDamage, defender);
         } else {
-            Hero winner;
-            if (firstHero.getCreatures().isEmpty()) {
-                winner = secondHero;
-            } else {
-                winner = firstHero;
-            }
-            System.out.println("\nThat was a tough battle!");
-            System.out.println("The winner is Hero " + winner.getName() + " of " + winner.getFraction());
+            System.out.println("Defense of the enemy is too strong!");
         }
-        if (firstHeroExp > 0) {
-            System.out.println(
-                    "\nHero " + firstHero.getName() + " of " + firstHero.getFraction()
-                    + " earned " + firstHeroExp + " xp"
-            );
-            firstHero.computeLevel(firstHeroExp);
-        }
-        if (secondHeroExp > 0) {
-            System.out.println(
-                    "\nHero " + secondHero.getName() + " of " + secondHero.getFraction()
-                            + " earned " + secondHeroExp + " xp"
-            );
-            secondHero.computeLevel(secondHeroExp);
-        }
+
+        return successAttack;
     }
 
     private static int attackOnArmy(int finalDamage, @NonNull Hero hero) {
@@ -90,18 +51,80 @@ public class Battlefield {
         }
     }
 
-    private static int doTurn(Hero attacker, @NonNull Hero defender) {
-        System.out.println("\nHero " + attacker.getName() + " is attacking!");
-        System.out.println("Attack of hero: " + attacker.getDamage());
-        System.out.println("Defense of enemy: " + defender.getDefense());
-        int finalDamage = attacker.getDamage() - defender.getDefense();
-        int successAttack = 0;
-        if (finalDamage > 0) {
-            successAttack = attackOnArmy(finalDamage, defender);
-        } else {
-            System.out.println("Defense of the enemy is too strong!");
+    private static void printBattleIntroduction(@NonNull String firstHeroName, @NonNull String secondHeroName) {
+        System.out.println(
+                "\nThe battle between Hero " + firstHeroName + " of " + firstHeroName
+                        + " and Hero " + secondHeroName + " of " + secondHeroName
+        );
+        System.out.println("The battle begin!");
+    }
+
+    private static BattleResult executeBattle(@NonNull Hero firstHero, @NonNull Hero secondHero) {
+        int firstHeroExp = 0;
+        int secondHeroExp = 0;
+        boolean turn = true;
+        boolean prevTurnResult = true;
+        boolean drawFlag = false;
+        while(!firstHero.getCreatures().isEmpty() && !secondHero.getCreatures().isEmpty()) {
+            if (drawFlag) {
+                break;
+            }
+            boolean currentTurnResult;
+            int exp;
+            if (turn) {
+                exp = doTurn(firstHero, secondHero);
+                firstHeroExp += exp;
+                turn = false;
+            } else {
+                exp = doTurn(secondHero, firstHero);
+                secondHeroExp += exp;
+                turn = true;
+            }
+            currentTurnResult = exp > 0;
+            if (!prevTurnResult && !currentTurnResult) {
+                drawFlag = true;
+            } else {
+                prevTurnResult = currentTurnResult;
+            }
         }
 
-        return successAttack;
+        return new BattleResult(firstHeroExp, secondHeroExp, drawFlag);
+    }
+
+    private static void handleResult(@NonNull BattleResult result, @NonNull Hero firstHero, @NonNull Hero secondHero) {
+        printResult(result.isDrawFlag(), firstHero, secondHero);
+        calcExperience(result.getFirstHeroExp(), firstHero);
+        calcExperience(result.getSecondHeroExp(), secondHero);
+    }
+
+    private static void calcExperience(int exp, @NonNull Hero hero) {
+        if (exp > 0) {
+            System.out.println(
+                    "\nHero " + hero.getName() + " of " + hero.getFraction()
+                            + " earned " + exp + " xp");
+            hero.computeLevel(exp);
+        }
+    }
+
+    private static void printResult(boolean drawFlag, @NonNull Hero firstHero, @NonNull Hero secondHero) {
+        if (drawFlag) {
+            System.out.println("Two armies are even! Time to retreat and grow powers!");
+        } else {
+            Hero winner;
+            if (firstHero.getCreatures().isEmpty()) {
+                winner = secondHero;
+            } else {
+                winner = firstHero;
+            }
+            System.out.println("\nThat was a tough battle!");
+            System.out.println("The winner is Hero " + winner.getName() + " of " + winner.getFraction());
+        }
+    }
+
+    @Data
+    private static class BattleResult {
+        private final int firstHeroExp;
+        private final int secondHeroExp;
+        private final boolean drawFlag;
     }
 }
