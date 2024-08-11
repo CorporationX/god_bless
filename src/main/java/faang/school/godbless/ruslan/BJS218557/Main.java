0a658b8d206f0c97d762d244639721cbead19693
Ruslan Iovlev <ruslan-iovlev@mail.ru>
2024-08-11 15:25:51 +0700

    Изменение метода для группировки по возрасту

--- src/main/java/faang/school/godbless/ruslan/BJS218557/Main.java
+++ src/main/java/faang/school/godbless/ruslan/BJS218557/Main.java
@@ -5,8 +5,15 @@ import java.util.List;
 import java.util.Map;
 
 public class Main {
+
     public static void main(String[] args) {
 
+        List<User> users = new ArrayList<>();
+        users.add(new User("Ruslan", 25, "cdek", "sovetskay"));
+        users.add(new User("Olga", 25, "cdek", "sovetskay"));
+        users.add(new User("Darina", 16, "cdek", "sovetskay"));
 
+        Map<Integer, List<User>> usersAge = User.groupUsers(users);
+        usersAge.forEach((key,value) -> System.out.println(key + " " + value));
     }
 }
--- src/main/java/faang/school/godbless/ruslan/BJS218557/User.java
+++ src/main/java/faang/school/godbless/ruslan/BJS218557/User.java
@@ -24,15 +24,7 @@ public class User {
     public static Map<Integer, List<User>> groupUsers(List<User> users) {
         Map<Integer, List<User>> groupedUsers = new HashMap<>();
         for (User user : users) {
-            int age = user.getAge();
-            if (groupedUsers.containsKey(age)) {
-                groupedUsers.get(age).add(user);
-            }
-            else {
-                List<User> userList = new ArrayList<>();
-                userList.add(user);
-                groupedUsers.put(age, userList);
-            }
+            groupedUsers.computeIfAbsent(user.getAge(), v -> new ArrayList<>()).add(user);
         }
         return groupedUsers;
     }
