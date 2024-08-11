9fc3d18a240caf6b63cb4aef4c3887347bfc8561
Denis Panasenko <panasenkodenis1995@gmail.com>
2024-08-11 14:54:27 +0300

    BJS2-20705

---
+++ src/main/java/faang/school/godbless/BJS2_20705/SpellAction.java
@@ -0,0 +1,5 @@
+package faang.school.godbless.BJS2_20705;
+
+public interface SpellAction {
+    String describeSpell(String spell);
+}
---
+++ src/main/java/faang/school/godbless/BJS2_20705/SpellCaster.java
@@ -0,0 +1,8 @@
+package faang.school.godbless.BJS2_20705;
+
+public class SpellCaster {
+
+    public void cast(String spell, SpellAction action){
+        System.out.println(action.describeSpell(spell));
+    }
+}
