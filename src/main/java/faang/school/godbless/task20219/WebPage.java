01998929654e91738eaf9436ee14f1ac511d0c09
Chibix <chibisovanastena@gmail.com>
2024-08-11 19:49:34 +0300

    BJS2-20219 - Поисковой мини-движок Google

---
+++ src/main/java/faang/school/godbless/task20219/WebPage.java
@@ -0,0 +1,19 @@
+package faang.school.godbless.task20219;
+
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.ToString;
+
+@AllArgsConstructor
+@Getter
+@ToString
+@EqualsAndHashCode
+public class WebPage {
+    private String url;
+    private String title;
+    private String content;
+
+
+
+}
