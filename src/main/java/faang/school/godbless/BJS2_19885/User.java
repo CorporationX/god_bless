d474b862c53bb6cb5a83ca1c91c7957db087861b
avoevoda <voevodacode@gmail.com>
2024-08-11 18:58:29 +0300

    Refactoring

--- src/main/java/faang/school/godbless/BJS2_19885/User.java
+++ src/main/java/faang/school/godbless/BJS2_19885/User.java
@@ -4,22 +4,18 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 
 import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
 
 @Data
 @AllArgsConstructor
 public class User {
-    public static Map<User, List<Query>> userQueryMap = new HashMap<>();
     private int id;
     private String name;
 
     public static void addUser(User user) {
-        userQueryMap.putIfAbsent(user, new ArrayList<>());
+        Main.queriesByUser.putIfAbsent(user, new ArrayList<>());
     }
 
     public static void removeUser(User user) {
-        userQueryMap.remove(user);
+        Main.queriesByUser.remove(user);
     }
 }
