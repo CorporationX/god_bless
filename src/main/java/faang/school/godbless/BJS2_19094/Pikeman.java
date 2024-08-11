a9811cc6a6f42e30ca9b933feef9cefbf52381b8
avoevoda <voevodacode@gmail.com>
2024-08-11 15:25:16 +0300

    Heroes of Might and Magic

---
+++ src/main/java/faang/school/godbless/BJS2_19094/Pikeman.java
@@ -0,0 +1,12 @@
+package faang.school.godbless.BJS2_19094;
+
+public class Pikeman extends Creature {
+    public Pikeman() {
+        super("Pikeman", 1, 4, 5, 4);
+    }
+
+    @Override
+    public int getDamage() {
+        return 3;
+    }
+}
