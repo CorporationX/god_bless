f47086397270d17b802f8824822cf1f8139a9eac
avoevoda <voevodacode@gmail.com>
2024-08-11 15:31:56 +0300

    Update groupedStudents key

---
+++ src/main/java/BJS2_19546/FacultyYearKey.java
@@ -0,0 +1,11 @@
+package BJS2_19546;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class FacultyYearKey {
+    private String faculty;
+    private int year;
+}
