1168c1e404b6d9a4368ea4ab15826bee7536e6b7
Varvara Stepnova <stepnova.varya@yandex.ru>
2024-08-11 12:22:08 +0300

    BJS2-20361: added change

--- src/main/java/faang/school/godbless/caching_events/Main.java
+++ src/main/java/faang/school/godbless/caching_events/Main.java
@@ -7,23 +7,22 @@ import java.util.Map;
 
 public class Main {
 
-    public static Map<Integer, StreamEvent> streamEventMap = new HashMap<>();
+    public static Map<Integer, StreamEvent> streamEventById = new HashMap<>();
 
-    public static Map<String, List<StreamEvent>> eventTypeMap = new HashMap<>();
+    public static Map<String, List<StreamEvent>> streamEventsByType = new HashMap<>();
 
     public static void main(String[] args) {
-
         addStreamEvent(1, "event", "data");
         addStreamEvent(2, "event2", "data2");
         addStreamEvent(3, "event", "data2");
 
         printAllStreamEvents();
 
-        findStreamEventById(1);
+        System.out.println(findStreamEventById(1));
 
-        findStreamEventByEventType("event");
+        System.out.println(findStreamEventByEventType("event"));
 
-        removeStreamEventById(1);
+        removeStreamEventById(4);
 
         printAllStreamEvents();
     }
@@ -31,32 +30,35 @@ public class Main {
     public static void addStreamEvent(int id, String eventType, String data) {
         StreamEvent streamEvent = new StreamEvent(id, eventType, data);
 
-        streamEventMap.put(streamEvent.getId(), streamEvent);
+        streamEventById.put(streamEvent.getId(), streamEvent);
 
-        if (!eventTypeMap.containsKey(streamEvent.getEventType())) {
-            eventTypeMap.put(streamEvent.getEventType(), new ArrayList<>(List.of(streamEvent)));
+        if (!streamEventsByType.containsKey(streamEvent.getEventType())) {
+            streamEventsByType.put(streamEvent.getEventType(), new ArrayList<>(List.of(streamEvent)));
         } else {
-            eventTypeMap.get(streamEvent.getEventType()).add(streamEvent);
+            streamEventsByType.get(streamEvent.getEventType()).add(streamEvent);
         }
     }
 
     public static StreamEvent findStreamEventById(int id) {
-        return streamEventMap.get(id);
+        return streamEventById.get(id);
     }
 
     public static List<StreamEvent> findStreamEventByEventType(String eventType) {
-        return eventTypeMap.get(eventType);
+        return streamEventsByType.get(eventType);
     }
 
     public static void removeStreamEventById(int id) {
-        StreamEvent streamEvent = streamEventMap.get(id);
-        streamEventMap.remove(id);
-
-        eventTypeMap.get(streamEvent.getEventType()).remove(id);
+        StreamEvent streamEvent = streamEventById.get(id);
+        streamEventById.remove(id);
+        try {
+            streamEventsByType.get(streamEvent.getEventType()).remove(id);
+        } catch (NullPointerException ex) {
+            System.out.println("StreamEvent с айди - " + id + " не найден!");
+        }
     }
 
     public static void printAllStreamEvents() {
-        for (Map.Entry<Integer, StreamEvent> entry : streamEventMap.entrySet()) {
+        for (Map.Entry<Integer, StreamEvent> entry : streamEventById.entrySet()) {
             System.out.println(entry.getKey() + " - " + entry.getValue());
         }
     }
