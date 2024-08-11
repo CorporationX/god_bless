0ae8b9cb073dc994d1e3e84bc1a5ff0355556dc4
avoevoda <voevodacode@gmail.com>
2024-08-11 15:25:16 +0300

    Heroes of Might and Magic

---
+++ src/main/java/faang/school/godbless/BJS2_19094/Swordman.java
@@ -0,0 +1,12 @@
+package faang.school.godbless.BJS2_19094;
+
+public class Swordman extends Creature {
+    public Swordman() {
+        super("Swordman", 4, 10, 12, 5);
+    }
+
+    @Override
+    public int getDamage() {
+        return 7;
+    }
+}
