0a7bb8e3aaf4c1afefd575baf9ebeb5e6a4e9b94
avoevoda <voevodacode@gmail.com>
2024-08-11 16:10:17 +0300

    refactoring

--- src/main/java/BJS2_20476/Main.java
+++ src/main/java/BJS2_20476/Main.java
@@ -2,7 +2,6 @@ package BJS2_20476;
 
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
-import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -11,16 +10,15 @@ public class Main {
     private static final DateTimeFormatter DATE_TIME_FORMATTER = DateTimeFormatter.ofPattern("HH:mm:ss dd-MM-yyyy");
 
     public static void main(String[] args) {
-        final List<StreamEvent> eventsList = new ArrayList<>();
         Map<Integer, StreamEvent> eventsById = new HashMap<>();
         Map<String, List<StreamEvent>> eventsByType = new HashMap<>();
 
-        StreamEvent.addEvent(eventsList, eventsById, eventsByType, new StreamEvent("Cache", DATE_TIME_FORMATTER.format(LocalDateTime.now())));
-        StreamEvent.addEvent(eventsList, eventsById, eventsByType, new StreamEvent("Cache", DATE_TIME_FORMATTER.format(LocalDateTime.now())));
-        StreamEvent.addEvent(eventsList, eventsById, eventsByType, new StreamEvent("Log", DATE_TIME_FORMATTER.format(LocalDateTime.now())));
-        StreamEvent.addEvent(eventsList, eventsById, eventsByType, new StreamEvent("Log", DATE_TIME_FORMATTER.format(LocalDateTime.now())));
-        StreamEvent.addEvent(eventsList, eventsById, eventsByType, new StreamEvent("Payment Service", DATE_TIME_FORMATTER.format(LocalDateTime.now())));
-        StreamEvent.addEvent(eventsList, eventsById, eventsByType, new StreamEvent("Payment Service", DATE_TIME_FORMATTER.format(LocalDateTime.now())));
+        StreamEvent.addEvent(eventsById, eventsByType, new StreamEvent("Cache", DATE_TIME_FORMATTER.format(LocalDateTime.now())));
+        StreamEvent.addEvent(eventsById, eventsByType, new StreamEvent("Cache", DATE_TIME_FORMATTER.format(LocalDateTime.now())));
+        StreamEvent.addEvent(eventsById, eventsByType, new StreamEvent("Log", DATE_TIME_FORMATTER.format(LocalDateTime.now())));
+        StreamEvent.addEvent(eventsById, eventsByType, new StreamEvent("Log", DATE_TIME_FORMATTER.format(LocalDateTime.now())));
+        StreamEvent.addEvent(eventsById, eventsByType, new StreamEvent("Payment Service", DATE_TIME_FORMATTER.format(LocalDateTime.now())));
+        StreamEvent.addEvent(eventsById, eventsByType, new StreamEvent("Payment Service", DATE_TIME_FORMATTER.format(LocalDateTime.now())));
 
         StreamEvent.printEvents(eventsById);
 
