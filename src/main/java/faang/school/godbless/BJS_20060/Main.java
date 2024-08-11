a060112ee42c577cbdae3514a0731b8d42c2d185
Denis Panasenko <panasenkodenis1995@gmail.com>
2024-08-11 13:59:11 +0300

    BJS2-20060 commentaries fix

--- src/main/java/faang/school/godbless/BJS_20060/Main.java
+++ src/main/java/faang/school/godbless/BJS_20060/Main.java
@@ -3,33 +3,29 @@ package faang.school.godbless.BJS_20060;
 import java.util.*;
 
 public class Main {
-    private static final Map<String, List<WebPage>> PAGES = new HashMap<>();
+    private static Map<String, List<WebPage>> indexes = new HashMap<>();
 
     public static void main(String[] args) {
         WebPage google = new WebPage("google.com/greetings", "Google", "This is google greeting page");
         WebPage yandex = new WebPage("yandex.ru/greetings/ru", "Yandex", "This is yandex greeting page for ru users");
         WebPage instagram = new WebPage("instagram.com/photo", "Insta", "This is for your photos");
 
-        splitUrl(google);
-        splitUrl(yandex);
-        splitUrl(instagram);
-        System.out.println(PAGES);
+        indexUrl(google);
+        indexUrl(yandex);
+        indexUrl(instagram);
+        System.out.println(indexes);
         System.out.println(findWebPagesByKeyword("This"));
         System.out.println(findWebPagesByKeyword("notexist"));
 
     }
 
-    public static void splitUrl(WebPage page){
+    public static void indexUrl(WebPage page){
         Arrays.asList(page.content().split("\\W+")).forEach(word -> {
-            PAGES.computeIfAbsent(word, k -> new ArrayList<>()).add(page);
+            indexes.computeIfAbsent(word, k -> new ArrayList<>()).add(page);
         });
     }
 
     public static List<WebPage> findWebPagesByKeyword(String keyword){
-        List<WebPage> webPages = new ArrayList<>();
-
-        Optional.ofNullable(PAGES.get(keyword)).ifPresent(webPages::addAll);
-
-        return webPages;
+        return Optional.ofNullable(indexes.get(keyword)).orElseThrow(() -> new IllegalArgumentException("Incorrect index"));
     }
 }
