c1889547c7dd1366291492204fa8e30f31106434
Denis Panasenko <panasenkodenis1995@gmail.com>
2024-08-11 22:55:41 +0300

    BJS2-21954

--- src/main/java/faang/school/godbless/BJS2_21954/Main.java
+++ src/main/java/faang/school/godbless/BJS2_21954/Main.java
@@ -1,17 +1,32 @@
 package faang.school.godbless.BJS2_21954;
 
+import java.util.List;
+import java.util.function.Function;
+import java.util.function.Predicate;
+
 public class Main {
     public static void main(String[] args) {
+        List<String> badWords = List.of("cat", "dog", "mouse");
         Notification emailNotification = new Notification("email", "Ваша учетная запись успешно активирована");
         Notification smsNotification = new Notification("sms", "Вы успешно изменили свой пароль");
         Notification pushNotification = new Notification("push", "Новый пост от пользователя: JohnDoe");
+        Notification notificationWithSwear = new Notification("email", "I have a bad dog and cat");
+        Predicate<Notification> hasSwearWords = (notification) -> {
+            return badWords.stream().anyMatch(word -> notification.getMessage().contains(word));
+        };
+        Function<Notification, Notification> replaceSwearWord = (notification) -> {
+          badWords.forEach(word -> notification.setMessage(notification.getMessage().replaceAll(word, "*".repeat(word.length()))));
+          return notification;
+        };
 
-        NotificationManager.registerHandler("email", (notification) -> System.out.println("Отправка по электронной почте: " + notification.message()));
-        NotificationManager.registerHandler("sms", (notification) -> System.out.println("Отправка SMS: " + notification.message()));
-        NotificationManager.registerHandler("push", (notification) -> System.out.println("Отправка push-уведомления: " + notification.message()));
+        NotificationManager.registerHandler("email", (notification) -> System.out.println("Отправка по электронной почте: " + notification.getMessage()));
+        NotificationManager.registerHandler("sms", (notification) -> System.out.println("Отправка SMS: " + notification.getMessage()));
+        NotificationManager.registerHandler("push", (notification) -> System.out.println("Отправка push-уведомления: " + notification.getMessage()));
+        NotificationManager.registerFilter(hasSwearWords, replaceSwearWord);
 
         NotificationManager.sendNotification(emailNotification);
         NotificationManager.sendNotification(smsNotification);
         NotificationManager.sendNotification(pushNotification);
+        NotificationManager.sendNotification(notificationWithSwear);
     }
 }
--- src/main/java/faang/school/godbless/BJS2_21954/Notification.java
+++ src/main/java/faang/school/godbless/BJS2_21954/Notification.java
@@ -1,4 +1,13 @@
 package faang.school.godbless.BJS2_21954;
 
-public record Notification(String type, String message) {
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+public class Notification {
+    private String type;
+    private String message;
 }
