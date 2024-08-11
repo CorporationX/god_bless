a430d4494d1a10df91052708e20b26af5241fa09
Varvara Stepnova <stepnova.varya@yandex.ru>
2024-08-11 12:52:39 +0300

    BJS2-18473-group_users_by_age: added change

--- src/main/java/faang/school/godbless/group_user_by_age/User.java
+++ src/main/java/faang/school/godbless/group_user_by_age/User.java
@@ -26,19 +26,8 @@ public class User {
 
     public static Map<Integer, List<User>> groupUsers(List<User> userList) {
         Map<Integer, List<User>> userMap = new HashMap<>();
-
-
         for (User user : userList) {
-            if (!userMap.containsKey(user.age)) {
-                userMap.computeIfAbsent(user.age, list -> new ArrayList<>(List.of(user)));
-            } else {
-                List<User> existedUserList = userMap.get(user.age);
-
-                List<User> updateUserList = new ArrayList<>(existedUserList);
-                updateUserList.add(user);
-
-                userMap.put(user.age, updateUserList);
-            }
+            userMap.computeIfAbsent(user.age, k -> new ArrayList<>()).add((user));
         }
 
         return userMap;
