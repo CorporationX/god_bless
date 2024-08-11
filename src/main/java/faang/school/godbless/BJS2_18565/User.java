4c528b2f16afba5fb991978505333108c7b3ab90
Anastasia Dragomirova <nastay.dr2002@gmail.com>
2024-08-11 17:14:15 +0400

    Изменила Lombok-аннотации

--- src/main/java/faang/school/godbless/BJS2_18565/User.java
+++ src/main/java/faang/school/godbless/BJS2_18565/User.java
@@ -1,22 +1,20 @@
 package faang.school.godbless.BJS2_18565;
 
 import lombok.AllArgsConstructor;
-import lombok.Data;
 
 import java.util.*;
 
-@Data
 @AllArgsConstructor
 public class User {
-    private String name;
-    private int age;
-    private String placeOfWork;
-    private String address;
+    private final String name;
+    private final int age;
+    private final String placeOfWork;
+    private final String address;
 
     public static Map<Integer, List<User>> groupUsers (List<User> users) {
         Map<Integer, List<User>> userMap = new HashMap<>();
         for (User user : users) {
-            int userAge = user.getAge();
+            int userAge = user.age;
             if (userMap.containsKey(userAge)) {
                 userMap.get(userAge).add(user);
             } else {
