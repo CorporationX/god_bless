46e77a528d7369641f50e44f01208c1b192e32f6
Rashit Gareyev <gareyevrs@gmail.com>
2024-08-11 19:02:09 +0400

    nf BJS2-20241 urlWordsMap added

--- src/main/java/faang/school/godbless/google/Main.java
+++ src/main/java/faang/school/godbless/google/Main.java
@@ -5,17 +5,19 @@ import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 public class Main {
+
     private static final Map<String, List<WebPage>> wordPagesMap = new HashMap<>();
-    private static final Map<String, List<WebPage>> urlPagesMap = new HashMap<>();
+    private static final Map<String, Set<String>> urlWordsMap = new HashMap<>();
 
     private void addToIndex(WebPage webPage) {
-
         String[] words = webPage.getContent().split("\\W+");
-
         Arrays.stream(words)
                 .forEach(word -> wordPagesMap.computeIfAbsent(word, k -> new ArrayList<>()).add(webPage));
+        urlWordsMap.put(webPage.getUrl(), Arrays.stream(words).collect(Collectors.toSet()));
     }
 
     private List<WebPage> findWebPages(String keyWord) {
@@ -23,10 +25,10 @@ public class Main {
     }
 
     private void deleteByUrl(String url) {
-        for (List<WebPage> webPages : wordPagesMap.values()) {
+        Set<String> words = urlWordsMap.get(url);
+        for (String word : words) {
+            List<WebPage> webPages = wordPagesMap.get(word);
             webPages.removeIf(webPage -> webPage.getUrl().equals(url));
         }
-
-        wordPagesMap.values().removeIf(List::isEmpty);
     }
 }
