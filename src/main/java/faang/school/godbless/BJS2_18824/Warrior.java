cb5101eb3b78bec8156ae73ae91b9b70d9d09a7c
Ruslan Iovlev <ruslan-iovlev@mail.ru>
2024-08-11 16:27:03 +0700

    исправил опечатку в классе Warrior

--- src/main/java/faang/school/godbless/BJS2_18824/Warrior.java
+++ src/main/java/faang/school/godbless/BJS2_18824/Warrior.java
@@ -2,15 +2,11 @@ package faang.school.godbless.BJS2_18824;
 
 public class Warrior extends Character {
 
-    public Warrior(String name) {
-        super(name, 10, 5, 3);
-    }
+    public Warrior(String name) {super(name, 10, 5, 3);}
 
     @Override
     public void attack(Character character) {
         int damage = this.power;
-        character.health -= power;
+        character.health -= damage;
     }
-
-
 }
