81b2d37f08ce7c67e7e44a3d64d2bb6540c19bb5
Denis Artamonov <dns.artamonov@gmail.com>
2024-08-11 23:07:14 +0300

    BJS2-21976 complete

---
+++ src/main/java/faang/school/godbless/BJS2_21976/Notification.java
@@ -0,0 +1,11 @@
+package faang.school.godbless.BJS2_21976;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class Notification {
+    private String type;
+    private String message;
+}
