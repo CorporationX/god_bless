e09ac5d24c49e5815ecb9242cb932cf0b63ae130
Ruslan Iovlev <ruslan-iovlev@mail.ru>
2024-08-11 17:32:35 +0700

    изменил на Integer

--- src/main/java/faang/school/godbless/BJS2_18974/Example.java
+++ src/main/java/faang/school/godbless/BJS2_18974/Example.java
@@ -1,20 +1,13 @@
 package faang.school.godbless.BJS2_18974;
 
-import java.util.Arrays;
 
 public class Example {
 
-     public static void reverse(int[] array) {
+     public static void reverse(Integer[] array) {
          for (int i = 0; i < array.length / 2; i++){
              int temp = array[i];
              array[i] = array[array.length - 1 - i];
              array[array.length - 1 - i] = temp;
          }
-         System.out.println(Arrays.toString(array));
      }
-
-    public static void main(String[] args) {
-        int[] bound = new int[]{1,2,3,4,5};
-        reverse(bound);
-    }
 }
