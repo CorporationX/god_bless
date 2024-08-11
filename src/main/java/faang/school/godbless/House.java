fd40df0fc7ba1d6e964093580c0ff198bc8b2413
Aleh Biahun <stones2875@gmail.com>
2024-08-11 20:02:04 +0200

    Solution task Stones-BJS2-20086

---
+++ src/main/java/faang/school/godbless/House.java
@@ -0,0 +1,13 @@
+package faang.school.godbless;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class House {
+    private String name;
+    private String sigil;
+}
