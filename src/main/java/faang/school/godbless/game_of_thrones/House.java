e1240c73d34b9195a384b528f11730348e6f1477
Varvara Stepnova <stepnova.varya@yandex.ru>
2024-08-11 10:00:11 +0300

    BJS2-19818: delete equals and hashCode

--- src/main/java/faang/school/godbless/game_of_thrones/House.java
+++ src/main/java/faang/school/godbless/game_of_thrones/House.java
@@ -4,8 +4,6 @@ import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
 
-import java.util.Objects;
-
 @Getter
 @Setter
 @ToString
@@ -17,18 +15,5 @@ public class House {
     public House(String name, String sigil) {
         this.name = name;
         this.sigil = sigil;
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        House house = (House) o;
-        return Objects.equals(name, house.name) && Objects.equals(sigil, house.sigil);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(name, sigil);
     }
 }
