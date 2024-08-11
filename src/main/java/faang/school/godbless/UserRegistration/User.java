022f62d1fd874b9e031cec8b525eff7ef74fbac6
DariaMak <49920983+DariaMak@users.noreply.github.com>
2024-08-11 09:10:53 +0300

    updated folder

---
+++ src/main/java/faang/school/godbless/UserRegistration/User.java
@@ -0,0 +1,54 @@
+package faang.school.godbless.UserRegistration;
+import java.util.*;
+
+public class User {
+    private String name;
+    private int age;
+    private String workplace;
+    private String address;
+
+    public static final Set<String> VALID_JOBS = new HashSet<>(Arrays.asList("Google", "Uber", "Amazon"));
+    public static final Set<String> VALID_ADDRESSES = new HashSet<>(Arrays.asList("London", "New York", "Amsterdam"));
+
+    public User(String name, int age, String workplace, String address) {
+        if (name == null || name.isEmpty()) {
+            throw new IllegalArgumentException("Name cannot be empty");
+        }
+        if (age < 18) {
+            throw new IllegalArgumentException("Age must be at least 18");
+        }
+        if (!VALID_JOBS.contains(workplace)) {
+            throw new IllegalArgumentException("Workplace must be one of " + VALID_JOBS);
+        }
+        if (!VALID_ADDRESSES.contains(address)) {
+            throw new IllegalArgumentException("Address must be one of " + VALID_ADDRESSES);
+        }
+        this.name = name;
+        this.age = age;
+        this.workplace = workplace;
+        this.address = address;
+    }
+
+@Override
+public String toString() {
+    return "User{" +
+            "name='" + name + '\'' +
+            ", age=" + age +
+            ", workplace='" + workplace + '\'' +
+            ", address='" + address + '\'' +
+            '}';
+}
+
+public static void main(String[] args) {
+    try {
+        User user1 = new User("Alice", 25, "Google", "London");
+        System.out.println(user1);
+
+        // Error main test
+        User user2 = new User("", 17, "Facebook", "Paris");
+        System.out.println(user2);
+    } catch (IllegalArgumentException e) {
+        System.out.println(e.getMessage());
+    }
+}
+}
