f4e36798b5c3be3c958f4d08bab22b88be621073
Denis Panasenko <panasenkodenis1995@gmail.com>
2024-08-12 21:41:23 +0300

    BJS2-20705

--- src/main/java/faang/school/godbless/BJS2_20705/SpellCaster.java
+++ src/main/java/faang/school/godbless/BJS2_20705/SpellCaster.java
@@ -3,7 +3,7 @@ package faang.school.godbless.BJS2_20705;
 public class SpellCaster {
 
     public void cast(String spell, SpellAction action){
-        if(spell.isEmpty()){
+        if(spell == null){
             throw new IllegalArgumentException("Spell is not chosen");
         }
         if(action == null){
