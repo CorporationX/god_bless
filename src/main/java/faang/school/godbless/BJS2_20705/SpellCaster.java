7329ebc224b3cbb074666fd890bd34b6ff2437ac
Denis Panasenko <panasenkodenis1995@gmail.com>
2024-08-12 14:06:11 +0300

    BJS2-20705 added exceptions

--- src/main/java/faang/school/godbless/BJS2_20705/SpellCaster.java
+++ src/main/java/faang/school/godbless/BJS2_20705/SpellCaster.java
@@ -3,6 +3,12 @@ package faang.school.godbless.BJS2_20705;
 public class SpellCaster {
 
     public void cast(String spell, SpellAction action){
+        if(spell.isEmpty()){
+            throw new IllegalArgumentException("Spell is not chosen");
+        }
+        if(action == null){
+            throw new IllegalArgumentException("Action is not chosen");
+        }
         System.out.println(action.describeSpell(spell));
     }
 }
