94bb8557761c0de30f9f22db8a28cdddd572b5b8
Robert Oguzov <robertoguzov2@gmail.com>
2024-08-12 17:29:43 +0300

    написал класс Filter

---
+++ src/main/java/faang/school/godbless/BJS2_21880/Filter.java
@@ -0,0 +1,11 @@
+package faang.school.godbless.BJS2_21880;
+
+import lombok.AllArgsConstructor;
+import lombok.ToString;
+
+@ToString
+@AllArgsConstructor
+public class Filter {
+    private String name;
+    private int value;
+}
