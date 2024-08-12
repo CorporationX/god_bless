bc95cd43dd725b288e84b546823ed3c0cf0eb634
aephrodita <146974539+aephrodita@users.noreply.github.com>
2024-08-12 17:35:04 +0500

    BJS2-22070

--- src/main/java/faang/school/godbless/BJS222070/MessageProcessor.java
+++ src/main/java/faang/school/godbless/BJS222070/MessageProcessor.java
@@ -3,9 +3,9 @@ package faang.school.godbless.BJS222070;
 import java.util.List;
 
 public class MessageProcessor {
-    public boolean processMessage(String message, List<MessageFilter> MessageFilters){
-        for (MessageFilter messageFilter : MessageFilters){
-            if(!messageFilter.filter(message)){
+    public boolean processMessage(String message, List<MessageFilter> MessageFilters) {
+        for (MessageFilter messageFilter : MessageFilters) {
+            if (!messageFilter.filter(message)) {
                 return false;
             }
         }
