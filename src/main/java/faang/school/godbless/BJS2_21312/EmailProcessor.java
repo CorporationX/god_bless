80ae80bc64741bc6fec958eb992cb324cdc33939
Denis Panasenko <panasenkodenis1995@gmail.com>
2024-08-12 18:04:23 +0300

    BJS2-21312

---
+++ src/main/java/faang/school/godbless/BJS2_21312/EmailProcessor.java
@@ -0,0 +1,23 @@
+package faang.school.godbless.BJS2_21312;
+
+import java.util.List;
+import java.util.function.Consumer;
+import java.util.function.Function;
+import java.util.function.Predicate;
+
+public class EmailProcessor {
+    public void processEmails(List<Email> emails, Predicate<Email> filtration, Consumer<Email> printerEmail,
+                              Function<Email, String> emailConvertion){
+        if(emails == null){
+            throw new IllegalArgumentException("No emails for filtering");
+        }
+
+        emails.forEach(email -> {
+            if(filtration.test(email)){
+                printerEmail.accept(email);
+                email.setBody(emailConvertion.apply(email));
+                System.out.println(email);
+            }
+        });
+    }
+}
