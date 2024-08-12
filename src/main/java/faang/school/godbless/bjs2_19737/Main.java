586e18d1b985128d9e4bd236127cc59cbdcda495
Alexander Milosh <milosh.alexandr.by@yandex.by>
2024-08-12 23:04:53 +0200

    BJS2-19737 Code review 2

--- src/main/java/faang/school/godbless/bjs2_19737/Main.java
+++ src/main/java/faang/school/godbless/bjs2_19737/Main.java
@@ -1,27 +1,44 @@
 package faang.school.godbless.bjs2_19737;
 
+import java.util.Comparator;
 import java.util.HashMap;
-import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
 
 public class Main {
-    private Map<User, LinkedList<Query>> userQueries = new HashMap<>();
+    private Map<User, List<Query>> userQueries = new HashMap<>();
 
-    public void addUserWithQueries(User user, LinkedList<Query> queries) {
+    public void addUserWithQueries(User user, List<Query> queries) {
+        queries.sort(Comparator.comparing(Query::getTimestamp).reversed());
         userQueries.put(user, queries);
     }
 
     public void addQueryToUser(Query query, User user) {
         var existedQueries = userQueries.get(user);
-        existedQueries.addFirst(query);
+        if (existedQueries != null) {
+            existedQueries.add(query);
+            int last = existedQueries.size() - 1;
+            for (int i = 0; i < last; i++) {
+                var right = existedQueries.get(last - i);
+                var left = existedQueries.get(last - i - 1);
+                var rightTimestamp = right.getTimestamp();
+                var leftTimestamp = left.getTimestamp();
+                if (rightTimestamp.isAfter(leftTimestamp)) {
+                    var temp = right;
+                    existedQueries.set(last - i, left);
+                    existedQueries.set(last - i - 1, temp);
+                } else {
+                    break;
+                }
+            }
+        }
     }
 
     public void removeUser(User user) {
         userQueries.remove(user);
     }
 
-    public void printAllUsersWithSortedQueries() {
+    public void printAllUsers() {
         userQueries.forEach((user, queries) -> {
             System.out.println("User: " + user.getName());
             printQueries(queries);
@@ -30,5 +47,20 @@ public class Main {
 
     public void printQueries(List<Query> queries) {
         queries.forEach(query -> System.out.println("    " + query.toString()));
+    }
+
+    public void printUserWithSortedQueries(User user) {
+        for (Map.Entry<User, List<Query>> entry : userQueries.entrySet()) {
+            var u = entry.getKey();
+            if (u.equals(user)) {
+                System.out.println("User: " + u.getName());
+                printQueries(entry.getValue());
+            }
+        }
+    }
+
+    // for tests
+    public Map<User, List<Query>> getUserQueries() {
+        return userQueries;
     }
 }
--- src/main/java/faang/school/godbless/bjs2_19737/Query.java
+++ src/main/java/faang/school/godbless/bjs2_19737/Query.java
@@ -6,17 +6,23 @@ import lombok.ToString;
 
 import java.time.LocalDateTime;
 
+@Getter
 @ToString
 @EqualsAndHashCode
 public class Query {
     private Integer id;
     private String content;
-    @Getter
     private LocalDateTime timestamp;
 
     public Query(Integer id, String content) {
         this.id = id;
         this.content = content;
         this.timestamp = LocalDateTime.now();
+    }
+
+    public Query(Integer id, String content, LocalDateTime timestamp) {
+        this.id = id;
+        this.content = content;
+        this.timestamp = timestamp;
     }
 }
