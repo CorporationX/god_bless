dba0da8a6747b6d9a0b98e7b68a2125d4ffbb6bb
Denis Panasenko <panasenkodenis1995@gmail.com>
2024-08-13 00:30:10 +0300

    BJS2-21417

---
+++ src/main/java/faang/school/godbless/BJS2_21417/Calculator.java
@@ -0,0 +1,7 @@
+package faang.school.godbless.BJS2_21417;
+
+import java.util.function.BiFunction;
+
+public interface Calculator {
+    int calculate(int a, int b);
+}
---
+++ src/main/java/faang/school/godbless/BJS2_21417/Main.java
@@ -0,0 +1,14 @@
+package faang.school.godbless.BJS2_21417;
+
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) {
+        List<Integer> numbers = List.of(1, 4, 6, 9, 12, 24);
+        System.out.println(calculate(numbers, Integer::sum));
+    }
+
+    public static int calculate(List<Integer> numbers, Calculator calculator) throws IllegalArgumentException {
+        return numbers.stream().reduce(0, calculator::calculate);
+    }
+}
