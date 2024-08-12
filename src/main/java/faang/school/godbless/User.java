b28d22aa7e190623db748d909057a06472b5cdc9
Adel <asdgh@gmail.com>
2024-08-13 01:13:12 +0400

    Индекс запросов ELK

---
+++ src/main/java/faang/school/godbless/User.java
@@ -0,0 +1,32 @@
+package faang.school.godbless;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+import java.util.Objects;
+
+@Getter
+@Setter
+@ToString
+@AllArgsConstructor
+public class User {
+
+    private int id;
+    private String name;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        User user = (User) o;
+        return id == user.id && name.equals(user.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, name);
+    }
+
+}
