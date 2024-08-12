a7380e71a9c89ed13e13822aedbacafc83f281dd
Ilia Chudov <chudow-iluha@mail.ru>
2024-08-12 13:40:40 +0300

    Убрал в константу "взрослость", поправил поля и импорты.

--- src/main/java/faang/school/godbless/BJS2_18400/User.java
+++ src/main/java/faang/school/godbless/BJS2_18400/User.java
@@ -1,25 +1,31 @@
 package faang.school.godbless.BJS2_18400;
 
-import lombok.Data;
+
 import lombok.Getter;
 import lombok.Setter;
 
-import java.util.*;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
 @Setter
 @Getter
 public class User {
     private String name;
     private int age;
+    private final int MATURITY = 18;
     private String job;
     private String address;
-    public static final Set<String> VALID_JOBS = new HashSet<>(Set.of("Google", "Uber", "Amazon"));
-    public static final Set<String> VALID_ADDRESSES = new HashSet<>(Set.of("London", "New York", "Amsterdam"));
+    public static final Set<String> VALID_JOBS = Set.of("Google", "Uber", "Amazon");
+    public static final Set<String> VALID_ADDRESSES = Set.of("London", "New York", "Amsterdam");
 
     public User(String name, int age, String job, String address) {
         if (name == null || name.isEmpty()) {
             throw new IllegalArgumentException("Имя не может быть пустым");
         }
-        if (age < 18) {
+        if (age < MATURITY) {
             throw new IllegalArgumentException("Возраст не может быть меньше 18");
         }
         if (!VALID_JOBS.contains(job)) {
