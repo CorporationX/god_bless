cc2601309e37b684ba7fb8a2c1d2f9c3c9c2f4cc
Chibix <chibisovanastena@gmail.com>
2024-08-12 13:19:04 +0300

    BJS2-19371 - Библиотека Вестеросса

--- src/main/java/faang/school/godbless/task19371/Main.java
+++ src/main/java/faang/school/godbless/task19371/Main.java
@@ -1,11 +1,9 @@
 package faang.school.godbless.task19371;
-
 import java.util.HashMap;
 import java.util.Map;
 
 public class Main {
-    public static Map<Book, String> library;
-
+    public static Map<Book, String> library = new HashMap<>();
 
     public static void putNewBook(Book book, String place){
         library.put(book,place);
@@ -16,15 +14,19 @@ public class Main {
         if(library.containsKey(book)){
             library.remove(book);
             return "Книга удалена";
-        } else return "Книга не найдена";
+        } else {
+            return "Книга не найдена";
+        }
     }
 
     public static String findPlaceOfBook(Book book){
         return library.get(book);
     }
-
-    public static Map<Book, String> getAllBooks(){
-        return library;
+    public static void getAllBooks(){
+        for (Map.Entry<Book, String> entry : library.entrySet()) {
+            System.out.println("Книга: " + entry.getKey().getTitle() + "; Автор: "
+                    + entry.getKey().getAuthor() + "; Год издания: " + entry.getKey().getYear());
+        }
     }
 
     public static void main(String[] args) {
@@ -35,7 +37,7 @@ public class Main {
         putNewBook(new Book("Три товарища", "Эрих Мария Ремарк", 1939), "ряд 3, полка 6");
         System.out.println(findPlaceOfBook(new Book("Цветы для Элджернона", "Дениел Киз", 1959)));
         System.out.println(removeBook("Цветы для Элджернона", "Дениел Киз", 1959));
-        System.out.println(getAllBooks());
+        getAllBooks();
 
     }
 
