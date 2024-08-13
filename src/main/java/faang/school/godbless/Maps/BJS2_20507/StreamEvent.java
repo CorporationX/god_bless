cacdd03a97d888804479da4c0d2aac6533b8c1f5
Garib_Aghayev <garibaghayev@gmail.com>
2024-08-13 06:34:23 +0300

    first release

---
+++ src/main/java/faang/school/godbless/Maps/BJS2_20507/StreamEvent.java
@@ -0,0 +1,22 @@
+package faang.school.godbless.Maps.BJS2_20507;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import lombok.ToString;
+
+import java.util.Random;
+import java.util.UUID;
+import java.util.concurrent.ThreadLocalRandom;
+
+@ToString
+@Getter
+public class StreamEvent {
+    private final int id;
+    private final String eventType;
+    private String data;
+
+    public StreamEvent(String eventType) {
+        this.id = ThreadLocalRandom.current().nextInt(1,10000);
+        this.eventType = eventType;
+    }
+}
