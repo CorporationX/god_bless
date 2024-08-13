3020fff4cca9c095408b618e8e4ec8a222d09e03
Garib_Aghayev <garibaghayev@gmail.com>
2024-08-13 22:05:48 +0300

    first release

---
+++ src/main/java/faang/school/godbless/Maps/BJS2_19505/Main.java
@@ -0,0 +1,42 @@
+package faang.school.godbless.Maps.BJS2_19505;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
+public class Main {
+    private static Set<Product> products = new HashSet<>();
+
+    public static void main(String[] args) {
+
+        products.add(new Product("Rolex", "Luxury"));
+        products.add(new Product("Ademars", "Luxury"));
+        products.add(new Product("Omega", "Mass market"));
+        products.add(new Product("Breguet", "Exclusive"));
+        products.add(new Product("Casio", "Mass market"));
+        products.add(new Product("PatekPH", "Luxury"));
+        products.add(new Product("Zenit", "Exclusive"));
+
+        printAllProds(gruopProdsMap());
+
+    }
+
+    public static Map<String, List<Product>> gruopProdsMap() {
+        Map<String, List<Product>> result = new HashMap<>();
+        for (Product prod : products) {
+            result.computeIfAbsent(prod.getCategory(), k -> new ArrayList<>());
+            result.get(prod.getCategory()).add(prod);
+        }
+        return result;
+    }
+
+    public static void printAllProds(Map<String, List<Product>> prodsMap) {
+        for (Map.Entry<String, List<Product>> entry : prodsMap.entrySet()) {
+            System.out.println(entry.getValue());
+        }
+    }
+
+}
---
+++ src/main/java/faang/school/godbless/Maps/BJS2_19505/Product.java
@@ -0,0 +1,22 @@
+package faang.school.godbless.Maps.BJS2_19505;
+
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.ToString;
+
+import java.util.concurrent.ThreadLocalRandom;
+
+@ToString
+@EqualsAndHashCode
+@Getter
+public class Product {
+    private final int id;
+    private final String name;
+    private final String category;
+
+    public Product(String name, String category) {
+        this.id = ThreadLocalRandom.current().nextInt(1,10000);
+        this.name = name;
+        this.category = category;
+    }
+}
