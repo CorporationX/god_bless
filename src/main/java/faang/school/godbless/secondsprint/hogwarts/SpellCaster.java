4717b8b31716ce73328ba2bdde11d749453cb8c6
Дмитрий Бахтин <dmitridorje@gmail.com>
2024-08-13 18:25:24 +0300

    Добавлена проверка на нулевое/пустое заклинание

--- src/main/java/faang/school/godbless/secondsprint/hogwarts/SpellCaster.java
+++ src/main/java/faang/school/godbless/secondsprint/hogwarts/SpellCaster.java
@@ -2,6 +2,10 @@ package faang.school.godbless.secondsprint.hogwarts;
 
 public class SpellCaster {
     public void cast(String spellName, SpellAction action) {
-        System.out.println(action.perform(spellName));
+        if(spellName == null || spellName.isEmpty()) {
+            System.out.println("No spell has been cast");
+        } else {
+            System.out.println(action.perform(spellName));
+        }
     }
 }
--- src/test/java/faang/school/godbless/secondsprint/hogwarts/SpellCasterTest.java
+++ src/test/java/faang/school/godbless/secondsprint/hogwarts/SpellCasterTest.java
@@ -6,30 +6,29 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import java.io.ByteArrayOutputStream;
-import java.io.OutputStream;
 import java.io.PrintStream;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class SpellCasterTest {
-    private final OutputStream outContent = new ByteArrayOutputStream();
+    private final ByteArrayOutputStream outContent = new ByteArrayOutputStream();
     private final PrintStream originalOut = System.out;
     private SpellCaster wizard;
 
     @BeforeEach
-    void setUp() {
+    public void setUp() {
         wizard = new SpellCaster();
         System.setOut(new PrintStream(outContent));
     }
 
     @AfterEach
-    void tearDown() {
+    public void tearDown() {
         System.setOut(originalOut);
     }
 
     @Test
     @DisplayName("Should print correct messages for different spells")
-    void testCast() {
+    public void testCast() {
         String alohomora = "Alohomora";
         String lumos = "Lumos";
         String expelliarmus = "Expelliarmus";
@@ -38,17 +37,20 @@ public class SpellCasterTest {
         wizard.cast(lumos, (spell) -> "A beam of light is created by " + spell);
         wizard.cast(expelliarmus, (spell) -> "The opponent is disarmed by " + spell);
 
-        String expectedOutput = "The door is unlocked by Alohomora\n" +
-                        "A beam of light is created by Lumos\n" +
-                        "The opponent is disarmed by Expelliarmus\n";
+        String expectedOutput = """
+                The door is unlocked by Alohomora
+                A beam of light is created by Lumos
+                The opponent is disarmed by Expelliarmus
+                """;
 
         assertEquals(expectedOutput, outContent.toString());
     }
 
     @Test
     @DisplayName("Should handle null spell by printing a default message")
-    void testCastNullSpell() {
-        wizard.cast(null, (spell) -> spell == null ? "No spell has been cast" : "Casting " + spell);
+    public void testCastNullSpell() {
+        String nihil = "";
+        wizard.cast(nihil, (spell) -> "Casting " + spell);
 
         assertEquals("No spell has been cast\n", outContent.toString());
     }
