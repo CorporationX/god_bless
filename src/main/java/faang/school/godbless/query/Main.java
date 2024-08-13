365eca8244150d5628a23e4e4bdcfa1675bbfb59
Alena Guskova <amguskova@edu.hse.ru>
2024-08-14 00:37:43 +0500

    BJS2-19709 Java HashMap: Индекс запросов ELK

--- src/main/java/faang/school/godbless/query/Main.java
+++ src/main/java/faang/school/godbless/query/Main.java
@@ -1,27 +1,18 @@
 package faang.school.godbless.query;
 
-import java.util.HashMap;
-import java.util.LinkedList;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 public class Main {
     private static HashMap<User, List<Query>> index = new HashMap<>();
 
     public static void indexQuery(User user, Query query) {
-        if (!index.containsKey(user)) {
-            index.put(user, new LinkedList<>(List.of(query)));
-        } else {
-            index.get(user).add(query);
-        }
+        index.computeIfAbsent(user, k -> new LinkedList<>()).add(query);
     }
 
-    //удаление пользователя и списка всех его запросов
     public static void deleteUser(User user) {
         index.remove(user);
     }
 
-    //удаление конкретного запроса у какого-то пользователя
     public static void deleteQuery(User user, Query query) {
         if (index.containsKey(user)) {
             index.get(user).remove(query);
@@ -41,7 +32,18 @@ public class Main {
 
     public static void printQueryHistory(User user) {
         if (index.containsKey(user)) {
-            for (Query query : index.get(user)) {
+            System.out.println("Query history of " + user + ":");
+            List<Query> queries = index.get(user);
+            queries.sort((o1, o2) -> {
+                if (o1.getTimestamp().after(o2.getTimestamp())) {
+                    return 1;
+                } else if (o1.getTimestamp() == o2.getTimestamp()) {
+                    return 0;
+                } else {
+                    return -1;
+                }
+            });
+            for (Query query : queries) {
                 System.out.println(query);
             }
         } else {
@@ -49,7 +51,34 @@ public class Main {
         }
     }
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
+        indexQuery(new User(1, "Mike"), new Query(1, "Weather in Moscow"));
+        Thread.sleep(1000);
+        indexQuery(new User(2, "Tom"), new Query(2, "Iphone 15 Pro price"));
+        Thread.sleep(1000);
+        indexQuery(new User(3, "Kate"), new Query(3, "How to start investing in stocks?"));
+        Thread.sleep(1000);
+        indexQuery(new User(4, "Helen"), new Query(4, "How to install Python on a Mac?"));
+        Thread.sleep(1000);
+        indexQuery(new User(1, "Mike"), new Query(5, "Tips for spending time alone"));
+        Thread.sleep(1000);
+        indexQuery(new User(2, "Tom"), new Query(6, "Where can I go cycling in Paris?"));
+        Thread.sleep(1000);
+        indexQuery(new User(2, "Tom"), new Query(7, "Which artists influenced Impressionism?"));
+        Thread.sleep(1000);
+        indexQuery(new User(4, "Helen"), new Query(8, "Best VPN"));
+        Thread.sleep(1000);
+        indexQuery(new User(1, "Mike"), new Query(9, "Top 100 TV series"));
+        Thread.sleep(1000);
+        indexQuery(new User(2, "Tom"), new Query(9, "What is artificial intelligence?"));
 
+
+        deleteUser(new User(3, "Kate"));
+
+        deleteQuery(new User(4, "Helen"), new Query(8, "Best VPN"));
+
+        printAllUsersWithQueries();
+
+        printQueryHistory(new User(1, "Mike"));
     }
 }
--- src/main/java/faang/school/godbless/query/Query.java
+++ src/main/java/faang/school/godbless/query/Query.java
@@ -1,5 +1,6 @@
 package faang.school.godbless.query;
 
+import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.ToString;
 
@@ -7,13 +8,15 @@ import java.util.Date;
 
 @Getter
 @ToString
+@EqualsAndHashCode
 public class Query {
     private int id;
     private String content;
-    private Date timestamp = new Date();
+    private Date timestamp;
 
     public Query(int id, String content) {
         this.id = id;
         this.content = content;
+        this.timestamp = new Date();
     }
 }
