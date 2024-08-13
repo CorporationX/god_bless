abdcad99e1b0adbffbd8a15093ad53bd1b90f1dd
Rashit Gareyev <gareyevrs@gmail.com>
2024-08-13 16:40:43 +0400

    nf BJS2-21463 calculator added

---
+++ src/main/java/faang/school/godbless/calculator/Calculator.java
@@ -0,0 +1,26 @@
+package faang.school.godbless.calculator;
+
+import java.util.List;
+
+public class Calculator {
+
+    public static int calculate(List<Integer> nums, Calculators calculator) throws IllegalArgumentException {
+        if (nums.isEmpty()) {
+            throw new IllegalArgumentException("List is empty");
+        }
+
+        int result = nums.get(0);
+        for (int i = 1; i < nums.size(); i++) {
+            result = calculator.calculate(result, nums.get(i));
+        }
+        return result;
+    }
+
+    public static int sum(List<Integer> nums) {
+        return calculate(nums, (a, b) -> a + b);
+    }
+
+    public static int product(List<Integer> nums) {
+        return calculate(nums, (a, b) -> a * b);
+    }
+}
---
+++ src/main/java/faang/school/godbless/calculator/Calculators.java
@@ -0,0 +1,6 @@
+package faang.school.godbless.calculator;
+
+@FunctionalInterface
+public interface Calculators {
+    int calculate(int first, int second);
+}
