75640528ddb76cc2146ff343784c51d33932e350
Okabe <mathew.mackonoha@gmail.com>
2024-08-13 13:44:50 +0300

    Commiting "Google-translator" task 1

---
+++ src/main/java/faang/school/godbless/Google/DictionaryProcessor.java
@@ -0,0 +1,20 @@
+package faang.school.godbless.Google;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.function.BiConsumer;
+
+public class DictionaryProcessor {
+    public void processWord(String word, String translation, BiConsumer<String, String> biConsumer) {
+        biConsumer.accept(word, translation);
+    }
+
+    public static void main(String[] args) {
+        Map<String, String> dictionary = new HashMap<>();
+        DictionaryProcessor dictionaryProcessor = new DictionaryProcessor();
+        BiConsumer<String, String> addWordToDictionary = (word, translation) -> dictionary.put(word, translation);
+        dictionaryProcessor.processWord("Слово", "Word", addWordToDictionary);
+        dictionaryProcessor.processWord("Жизнь", "Life", addWordToDictionary);
+        dictionaryProcessor.processWord("Смерть", "Death", addWordToDictionary);
+    }
+}
