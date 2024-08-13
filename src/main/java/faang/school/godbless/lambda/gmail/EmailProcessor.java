8e847b16a3afd5fb5f2e4c00c7e30399a398863f
Aleksandr Andreev <andreev.aleksandr.spb@gmail.com>
2024-08-13 14:07:51 +0300

    BJS2-21315 Gmail Rich Filters

---
+++ src/main/java/faang/school/godbless/lambda/gmail/EmailProcessor.java
@@ -0,0 +1,15 @@
+package faang.school.godbless.lambda.gmail;
+
+import java.util.List;
+import java.util.function.Consumer;
+import java.util.function.Function;
+import java.util.function.Predicate;
+
+public class EmailProcessor {
+    public void processEmails(List<Email> emails, Predicate<Email> filter, Consumer<Email> process, Function<Email, String> transform) {
+        emails.stream()
+                .filter(filter)
+                .peek(process)
+                .forEach(email -> System.out.println(transform.apply(email)));
+    }
+}
