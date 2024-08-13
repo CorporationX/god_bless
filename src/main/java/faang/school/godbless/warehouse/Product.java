c0264140374092c94babbdc05f961d70b293f28b
Alena Guskova <amguskova@edu.hse.ru>
2024-08-13 23:28:45 +0500

    Добавлены переопределения equals и hashcode

--- src/main/java/faang/school/godbless/warehouse/Product.java
+++ src/main/java/faang/school/godbless/warehouse/Product.java
@@ -1,10 +1,12 @@
 package faang.school.godbless.warehouse;
 
 import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
 import lombok.Getter;
 
 @AllArgsConstructor
 @Getter
+@EqualsAndHashCode
 public class Product {
     private int id;
     private String name;
