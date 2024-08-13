78f5667ba25a0f6077cd086e7583aa3eaa2c8d0c
SharkB38 <val.wolf.ne@gmail.com>
2024-08-13 20:41:24 +0900

    Little fixes to make calculate method universal

--- src/main/java/calculator/Main.java
+++ src/main/java/calculator/Main.java
@@ -10,14 +10,16 @@ public class Main {
         List<Integer> nums = Arrays.asList(1, 2, 3, 4, 5);
         System.out.println(product(nums));
         System.out.println(sum(nums));
+        System.out.println(calculate(Arrays.asList(1.2, 1.4, 1.5), Double::sum));
+        System.out.println(calculate(Arrays.asList(1.2, 1.4, 1.5), (n1, n2) -> n1 * n2));
     }
 
-    public static int calculate(@NonNull List<Integer> nums,
-                                @NonNull Calculator<Integer> calculator) throws IllegalArgumentException {
+    public static <T extends Number> T calculate(@NonNull List<T> nums,
+                                @NonNull Calculator<T> calculator) throws IllegalArgumentException {
         if (nums.isEmpty()) {
             throw new IllegalArgumentException("List of numbers is empty!");
         } else {
-            int result = nums.get(0);
+            var result = nums.get(0);
             for (int i = 1; i < nums.size(); i++) {
                 result = calculator.calculate(result, nums.get(i));
             }
