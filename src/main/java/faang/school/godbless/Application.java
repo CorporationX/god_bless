921bc77f99e0d6850009365b7739371ccb9f9344
Bobiktrans <Bobicktrnas@yandex.ru>
2024-08-13 15:09:15 +0300

    BJS2-20768

--- src/main/java/faang/school/godbless/Application.java
+++ src/main/java/faang/school/godbless/Application.java
@@ -1,7 +1,24 @@
 package faang.school.godbless;
 
+import java.util.HashMap;
+import java.util.Map;
+import java.util.function.BiConsumer;
+
 public class Application {
+    public void processWord(String word, String translation, BiConsumer<String, String> dictionaryProcessor) {
+        dictionaryProcessor.accept(word, translation);
+    }
     public static void main(String[] args) {
-        System.out.println("God Bless!");
+        Application dictionaryProcessor = new Application();
+
+        Map<String, String> dictionary = new HashMap<>();
+
+        BiConsumer<String, String> addWordToDictionary = (word, translation) -> dictionary.put(word, translation);
+
+        dictionaryProcessor.processWord("привет", "hello", addWordToDictionary);
+        dictionaryProcessor.processWord("мир", "world", addWordToDictionary);
+        dictionaryProcessor.processWord("программирование", "programming", addWordToDictionary);
+
+        System.out.println("Словарь: " + dictionary);
     }
 }
