7489f770b2b30eb1412730c2e808a0e46322a02a
Sergey Sklyar <sklyar1091@gmail.com>
2024-08-13 23:50:53 +0300

    1) name.isBlank проверит имя пустое или состоит из пробелов 2) добавил проверку на нулл для job и address 3) поставил final на поля User 4) удалил метод 5) поставил static константе MINIMUM_AGE (не было)

--- src/main/java/BJS2_18476/App.java
+++ src/main/java/BJS2_18476/App.java
@@ -5,21 +5,16 @@ import java.util.List;
 
 public class App {
     public static void main(String[] args) {
-
         List<User> userList = new ArrayList<>();
-
         try {
             userList.add(new User("Sergey", 22, "Google", "London"));
             userList.add(new User("Andrew", 25, "Uber", "New York"));
-            userList.add(new User("Misha", 25, "Google", "London"));
-            userList.add(new User("Vera", 22, "Uber", "New York"));
-            userList.add(new User("Masha", 30, "Amazon", "Amsterdam"));
-            userList.add(new User("Vlad", 30, "Amazon", "Amsterdam"));
         } catch (IllegalArgumentException e) {
             System.out.println("Error!");
         }
 
-        System.out.println(User.groupUsers(userList));
+        for (User user : userList) {
+            System.out.println(user.toString());
+        }
     }
-
 }
--- src/main/java/BJS2_18476/User.java
+++ src/main/java/BJS2_18476/User.java
@@ -2,23 +2,18 @@ package BJS2_18476;
 
 import lombok.ToString;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
 import java.util.Set;
 
 @ToString
 public class User {
-
-    private final int MINIMUM_AGE = 18;
+    private final static int MINIMUM_AGE = 18;
     final Set<String> VALID_JOBS = Set.of("Google", "Uber", "Amazon");
     final Set<String> VALID_ADDRESSES = Set.of("London", "New York", "Amsterdam");
 
-    private String name;
-    private int age;
-    private String job;
-    private String address;
+    private final String name;
+    private final int age;
+    private final String job;
+    private final String address;
 
     public User(String name, int age, String job, String address) {
         if (validateUser(name, age, job, address)) {
@@ -32,30 +27,19 @@ public class User {
     }
 
     private boolean validateUser(String name, int age, String job, String address) {
-        if (name.isEmpty() || name.isBlank()) {
+        if (name.isBlank()) {
             System.out.println("User name should not be empty!");
             return false;
         } else if (age < MINIMUM_AGE) {
             System.out.println(name + "'s age should be more than 18!");
             return false;
-        } else if (!VALID_ADDRESSES.contains(address)) {
+        } else if (address == null || !VALID_ADDRESSES.contains(address)) {
             System.out.println(name + "'s address " + address + " not valid!");
             return false;
-        } else if (!VALID_JOBS.contains(job)) {
+        } else if (job == null || !VALID_JOBS.contains(job)) {
             System.out.println(name + "'s job " + job + " not valid!");
             return false;
         }
         return true;
-    }
-
-    public static Map<Integer, List<User>> groupUsers(List<User> users) {
-
-        Map<Integer, List<User>> resultMap = new HashMap<>();
-
-        for (User user : users) {
-            resultMap.computeIfAbsent(user.age, key -> new ArrayList<>()).add(user);
-        }
-
-        return resultMap;
     }
 }
