2477eb0a616c0dbffa03f387677dae0d020ab702
Denis Panasenko <panasenkodenis1995@gmail.com>
2024-08-13 18:01:58 +0300

    BJS2-20424

---
+++ src/main/java/faang/school/godbless/BJS2_20424/Main.java
@@ -0,0 +1,24 @@
+package faang.school.godbless.BJS2_20424;
+
+import java.util.function.Function;
+
+public class Main {
+    Double triangleArea(double a, double b, double c) throws IllegalArgumentException {
+        Function<Double, Function<Double, Double>> sum = (x) -> y -> x + y;
+        Function<Double, Function<Double, Double>> mul = (x) -> y -> x * y;
+        Function<Double, Function<Double, Double>> sub = (x) -> y -> x - y;
+        Function<Double, Function<Double, Double>> div = (x) -> y -> x / y;
+        Function<Double, Double> sqrt = (x) -> Math.sqrt(x);
+        double halfPerimeter = div.apply(sum.compose(sum.apply(a)).apply(b).apply(c)).apply(2.0);
+        double aSub = sub.apply(halfPerimeter).apply(a);
+        double bSub = sub.apply(halfPerimeter).apply(b);
+        double cSub = sub.apply(halfPerimeter).apply(c);
+        double forRootNumber = mul.apply(halfPerimeter).apply(mul.apply(aSub).apply(mul.apply(bSub).apply(cSub)));
+
+        return sqrt.apply(forRootNumber);
+    }
+
+    public static void main(String[] args) {
+        System.out.println(new Main().triangleArea(4, 8, 5));;
+    }
+}
