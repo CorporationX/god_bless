307570f43221fb31ad2174659f25bc21ef0b43ef
Denis Alginin <AlgininDK@gmail.com>
2024-08-13 16:56:51 +0500

    BJS2-21871

---
+++ src/main/java/faang/school/godbless/BJS221871/Main.java
@@ -0,0 +1,26 @@
+package faang.school.godbless.BJS221871;
+
+import org.w3c.dom.ls.LSOutput;
+
+import java.util.function.Function;
+
+public class Main {
+    public static void main(String[] args) {
+        Image originalImage = new Image("original.jpg", "Оригинальное изображение");
+
+        FilterProcessor filterProcessor = new FilterProcessor();
+
+        Function<Image, Image> grayscaleFilter = image -> new Image(image.getName() + "_grayscale", "Фильтр: черно - белый");
+        Function<Image, Image> sepiaFilter = image -> new Image(image.getName() + "_sepia", "Фильтр: сепия");
+        Function<Image, Image> vignetteFilter = image -> new Image(image.getName() + "_vignetter", "Фильтр: виньетка");
+
+        Image grayscaleImage = filterProcessor.applyFilter(originalImage, grayscaleFilter);
+        Image sepiaImage = filterProcessor.applyFilter(originalImage, sepiaFilter);
+        Image vignetteImage = filterProcessor.applyFilter(originalImage, vignetteFilter);
+
+        Function<Image, Image> combinedFilter = filterProcessor.combineFilters(grayscaleFilter, sepiaFilter);
+        Image combinedImage = filterProcessor.applyFilter(originalImage, combinedFilter);
+
+        System.out.println(combinedImage);
+    }
+}
