0e1f44f370b4fdd924d617e74ee7ef69c652b742
Bobiktrans <Bobicktrnas@yandex.ru>
2024-08-13 19:57:41 +0300

    BJS2-21104

--- src/main/java/faang/school/godbless/Application.java
+++ src/main/java/faang/school/godbless/Application.java
@@ -2,6 +2,24 @@ package faang.school.godbless;
 
 public class Application {
     public static void main(String[] args) {
-        System.out.println("God Bless!");
+        String result = RemoteServiceHandler.withErrorHandling(
+                () -> {
+                    try {
+                        return remoteServiceCall();
+                    } catch (Exception e) {
+                        throw new RuntimeException(e);
+                    }
+                },
+                e -> {
+                    System.out.println("The Remote Service could not be accessed. Let's return the default value.");
+                    return "DEFAULT";
+                }
+        );
+
+        System.out.println(result);
+    }
+
+    public static String remoteServiceCall() throws Exception {
+        throw new Exception("Error");
     }
 }
