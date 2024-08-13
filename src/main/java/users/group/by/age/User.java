f7b4a5f1ad8e1008669d7780bc56cf3bcfe78de8
Semen Plekhanov <semen-5555@mail.ru>
2024-08-13 10:46:56 +0300

    фикс мержа

--- src/main/java/users/group/by/age/User.java
+++ src/main/java/users/group/by/age/User.java
@@ -6,12 +6,9 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-
-@Data
-public class User {
 import java.util.Set;
 
-
+@Data
 public class User {
     private static final Set<String> VALID_JOBS = Set.of("Google", "Uber", "Amazon");
     private static final Set<String> VALID_ADDRESSES = Set.of("London", "New York", "Amsterdam");
