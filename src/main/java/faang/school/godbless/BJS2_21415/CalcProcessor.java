259dba6c9b50bab9f36f5e855985ee75ca6e2c01
zeus <denis@fatum.ru>
2024-08-13 09:42:47 +0300

    BJS2-21415 complete

--- src/main/java/faang/school/godbless/BJS2_21415/CalcProcessor.java
+++ src/main/java/faang/school/godbless/BJS2_21415/CalcProcessor.java
@@ -1,5 +1,25 @@
 package faang.school.godbless.BJS2_21415;
 
+import java.util.List;
+
 public class CalcProcessor {
-    public
+    public static int calculate(List<Integer> nums, Calculator calculator) {
+        if (nums.isEmpty()) {
+            throw new IllegalArgumentException("empty list");
+        }
+
+        int result = nums.get(0);
+        for (int i = 1; i < nums.size(); ++i) {
+            result = calculator.calc(result, nums.get(i));
+        }
+        return result;
+    }
+
+    public static int sum(List<Integer> nums) {
+        return calculate(nums, (a, b) -> a + b);
+    }
+
+    public static int product(List<Integer> nums) {
+        return calculate(nums, (a, b) -> a * b);
+    }
 }
--- src/main/java/faang/school/godbless/BJS2_21415/Main.java
+++ src/main/java/faang/school/godbless/BJS2_21415/Main.java
@@ -1,7 +1,11 @@
 package faang.school.godbless.BJS2_21415;
 
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
-
+        List<Integer> list = List.of(4, 5, 3);
+        System.out.println(CalcProcessor.sum(list));
+        System.out.println(CalcProcessor.product(list));
     }
 }
