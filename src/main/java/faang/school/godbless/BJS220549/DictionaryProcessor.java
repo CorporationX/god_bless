02f44580c0d44061a5ee63c08d8208bd00a95334
Denis Alginin <AlgininDK@gmail.com>
2024-08-13 18:04:41 +0500

    BJS2-20549

---
+++ src/main/java/faang/school/godbless/BJS220549/DictionaryProcessor.java
@@ -0,0 +1,9 @@
+package faang.school.godbless.BJS220549;
+
+import java.util.function.BiConsumer;
+
+public class DictionaryProcessor {
+    public void processWord(String word, String translation, BiConsumer<String, String> biConsumer) {
+        biConsumer.accept(word, translation);
+    }
+}
