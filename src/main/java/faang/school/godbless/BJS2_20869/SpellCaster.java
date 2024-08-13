3e56f1bb606f3d9f1ca3ed237744de00ca1b49b1
Peregruzochka <peregruzochka48@mail.ru>
2024-08-13 20:36:54 +0300

    Реализация класса SpellCaster

---
+++ src/main/java/faang/school/godbless/BJS2_20869/SpellCaster.java
@@ -0,0 +1,7 @@
+package faang.school.godbless.BJS2_20869;
+
+public class SpellCaster {
+    public void cast(String spellName, SpellAction spellAction) {
+        System.out.println(spellAction.action(spellName));
+    }
+}
