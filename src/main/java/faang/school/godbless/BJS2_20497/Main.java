486a64c29b588e16fbdce1cffe6771a86f0b6c53
Amirhan Almeev <almeevamirhan294@gmail.com>
2024-08-14 18:43:29 +0300

    Вспомним школу решение

---
+++ src/main/java/faang/school/godbless/BJS2_20497/Main.java
@@ -0,0 +1,7 @@
+package faang.school.godbless.BJS2_20497;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(calculatingAreaTriangle.triangleArea(2,2,3));
+    }
+}
