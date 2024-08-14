b0f31d694915406f6d98d7ac817d59697b3d64bd
Varvara Stepnova <stepnova.varya@yandex.ru>
2024-08-14 22:34:33 +0300

    BJS2-22119-spam: created interface MessageFilter, class MessageProcessor with method processMessage

---
+++ src/main/java/faang/school/godbless/spam/MessageProcessor.java
@@ -0,0 +1,19 @@
+package faang.school.godbless.spam;
+
+import java.util.List;
+
+public class MessageProcessor {
+
+    public boolean processMessage(String message, List<MessageFilter> messageFilterList) {
+        if (message == null || message.isBlank()) {
+            System.out.println("Сообщение не может быть пустым!");
+        }
+
+        boolean isFiltered = false;
+        for (MessageFilter messageFilter : messageFilterList) {
+            isFiltered = messageFilter.filter(message); {
+            }
+        }
+        return isFiltered;
+    }
+}
