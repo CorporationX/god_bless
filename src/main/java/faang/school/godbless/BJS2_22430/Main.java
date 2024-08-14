f595f74e0e6191753d6881a3ecd6e956618137c3
Denis Artamonov <dns.artamonov@gmail.com>
2024-08-14 23:12:56 +0300

    BJS2-22430 continue

--- src/main/java/faang/school/godbless/BJS2_22430/Main.java
+++ src/main/java/faang/school/godbless/BJS2_22430/Main.java
@@ -5,20 +5,19 @@ import java.util.Map;
 import java.util.stream.Collectors;
 
 public class Main {
-    //    Пример: Input List: [1, 9, 3, 6, 4, 5]
-//    Target Sum: 10
-//    Pairs: [[1, 9], [6, 4]]
     public static void main(String[] args) {
-        List<Integer> inputList = List.of(1, 9, 3, 6, 4, 5);
+        List<Integer> inputList = List.of(1, 9, 3, 5, 6, 4, 5);
         int targetSum = 10;
         System.out.println(uniquePairs(inputList, targetSum));
     }
 
 
-    public static Map<String,Integer> uniquePairs(List<Integer> inputList, int targetSum) {
-         Map<String,Integer> resultMap = inputList.stream()
+    public static Map<String, Integer> uniquePairs(List<Integer> inputList, int targetSum) {
+        Map<String, Integer> resultMap = inputList.stream()
                 .flatMap(outerNum -> inputList.stream()
-                        .filter(innerNum -> !outerNum.equals(innerNum) && innerNum + outerNum == targetSum && outerNum<innerNum).map(innerNum->outerNum+","+innerNum)).collect(Collectors.toMap(val->val,val->targetSum));
+                        .filter(innerNum -> innerNum + outerNum == targetSum && (outerNum > innerNum || outerNum.equals(innerNum)))
+                        .map(innerNum -> outerNum + "," + innerNum))
+                .collect(Collectors.toMap(value -> value, value -> targetSum, (target1, target2) -> targetSum));
 
         return resultMap;
     }
