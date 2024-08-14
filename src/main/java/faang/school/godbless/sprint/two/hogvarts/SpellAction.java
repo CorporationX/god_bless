ef5995f1c800ffc5ab393de986932a983bd54723
Evgenii Shevchenko <evgeny.shevchenko@webware24.de>
2024-08-14 20:19:55 +0200

    BJS2-20924 - Хогвартс, Хогвартс, наш любимый Хогвартс

---
+++ src/main/java/faang/school/godbless/sprint/two/hogvarts/SpellAction.java
@@ -0,0 +1,6 @@
+package faang.school.godbless.sprint.two.hogvarts;
+
+@FunctionalInterface
+public interface SpellAction {
+    String createSpell(String spell);
+}
---
+++ src/main/java/faang/school/godbless/sprint/two/hogvarts/SpellCaster.java
@@ -0,0 +1,8 @@
+package faang.school.godbless.sprint.two.hogvarts;
+
+public class SpellCaster {
+
+    public void cast(String spell, SpellAction createSpell) {
+        System.out.println(createSpell.createSpell(spell));
+    }
+}
