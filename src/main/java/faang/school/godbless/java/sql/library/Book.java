4ebd009d87d59bd70ea83a1f193fca1ab2effdd1
Evgenii Shevchenko <evgeny.shevchenko@webware24.de>
2024-08-14 19:41:53 +0200

    fixed all remarks. The service class LibraryService was created , all methods from main have been moved there. Class book was cleaned from boilerplate methods (with lombok)

--- src/main/java/faang/school/godbless/java/sql/library/Book.java
+++ src/main/java/faang/school/godbless/java/sql/library/Book.java
@@ -1,40 +1,18 @@
 package faang.school.godbless.java.sql.library;
 
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
 import lombok.Getter;
+import lombok.ToString;
 
-import java.util.Objects;
 
 @Getter
+@AllArgsConstructor
+@EqualsAndHashCode
+@ToString
 public class Book {
-    private String title;
-    private String author;
-    private int year;
+    private final String title;
+    private final String author;
+    private final int year;
 
-    public Book(String title, String author, int year) {
-        this.title = title;
-        this.author = author;
-        this.year = year;
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        Book book = (Book) o;
-        return year == book.year && Objects.equals(title, book.title) && Objects.equals(author, book.author);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(title, author, year);
-    }
-
-    @Override
-    public String toString() {
-        return "{" +
-                "title='" + title + '\'' +
-                ", author='" + author + '\'' +
-                ", year=" + year +
-                '}';
-    }
 }
