a0ad218c45dbc36bbdbbe9d9bcbcd72eb561319e
Anastasia Dragomirova <nastay.dr2002@gmail.com>
2024-08-14 19:45:59 +0400

    BJS2-20850 — Хогвартс, Хогвартс, наш любимый Хогвартс

---
+++ src/main/java/faang/school/godbless/BJS2_20850/SpellCaster.java
@@ -0,0 +1,7 @@
+package faang.school.godbless.BJS2_20850;
+
+public class SpellCaster {
+    public void cast(String nameOfTheSpell, SpellAction spellAction) {
+        System.out.println(spellAction.getTheResultOfTheSpell(nameOfTheSpell));
+    }
+}
