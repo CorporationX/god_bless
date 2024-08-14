1e83a187e2140cd68018862da41d24b8d38f32b9
Svirlex <aleksandrsviridov446@gmail.com>
2024-08-14 12:22:04 +0300

    BJS2-21914 Created Image filters

---
+++ src/main/java/faang/school/godbless/instagram/Image.java
@@ -0,0 +1,15 @@
+package faang.school.godbless.instagram;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@ToString
+public class Image {
+    private String fileName;
+    private String description;
+}
