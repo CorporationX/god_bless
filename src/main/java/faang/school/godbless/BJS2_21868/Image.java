2e9812fb4ef372b91e0ee19a13062c63da6c7aca
avoevoda <voevodacode@gmail.com>
2024-08-14 19:31:14 +0300

    Meta-universe

---
+++ src/main/java/faang/school/godbless/BJS2_21868/Image.java
@@ -0,0 +1,13 @@
+package faang.school.godbless.BJS2_21868;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.ToString;
+
+@Getter
+@AllArgsConstructor
+@ToString
+public class Image {
+    private String path;
+    private String description;
+}
