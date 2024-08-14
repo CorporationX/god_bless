3dcbb40a3c86bc3681d8b5f61688a9f8b2d8b962
Amirhan Almeev <almeevamirhan294@gmail.com>
2024-08-14 11:51:56 +0300

    решаю

---
+++ src/main/java/faang/school/godbless/BJS2_22096/MessageFilter.java
@@ -0,0 +1,6 @@
+package faang.school.godbless.BJS2_22096;
+
+@FunctionalInterface
+public interface MessageFilter {
+    boolean filter(String message);
+}
---
+++ src/main/java/faang/school/godbless/BJS2_22096/MessageProcessor.java
@@ -0,0 +1,5 @@
+package faang.school.godbless.BJS2_22096;
+
+public class MessageProcessor {
+    public
+}
