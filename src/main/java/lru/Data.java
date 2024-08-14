8ec70bbe79786dac598474e92b3c8d26ba6e5eaf
Ivan <spvanym@gmail.com>
2024-08-15 00:25:00 +0300

    lru SMI

--- src/main/java/lru/Data.java
+++ src/main/java/lru/Data.java
@@ -5,9 +5,14 @@ import lombok.Getter;
 import lombok.Setter;
 
 import java.util.ArrayList;
+import java.util.Collection;
 import java.util.Collections;
+import java.util.Comparator;
+import java.util.HashMap;
+import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 @Setter
 @Getter
@@ -32,8 +37,7 @@ public class Data {
     public static void addData(Map<Integer, Data> dataByInteger, int id, Data data) {
         if (!dataByInteger.containsKey(id)) {
             dataByInteger.put(id, data);
-        }
-        else {
+        } else {
             System.out.println("ID уже принадлежит другому объекту");
         }
     }
@@ -42,8 +46,7 @@ public class Data {
         if (dataCash.containsKey(id)) {
             System.out.println(dataCash.get(id));
             dataCash.get(id).setTimestamp(+1);
-        }
-        else {
+        } else {
             Data timeData = dataByInteger.get(id);
             dataCash.put(id, timeData);
             System.out.println(dataCash.get(id));
@@ -53,6 +56,28 @@ public class Data {
 
     public static void updateTimestamp( Map<Integer, Data> dataCash) {
         if (dataCash.size() > CASH_SIZE) {
-        dataCash.remove(dataCash.get(9));
+            List<Map<Integer, Data>> mapList = new ArrayList<>((Collection) dataCash);
+
+            Collections.sort(mapList, new Comparator<Map<Integer, Data>>() {
+                @Override
+                public int compare(Map<Integer, Data> o1, Map<Integer, Data> o2) {
+                    return compare(o1,o2);
+                }
+            });
+
+            Map<Integer, Data> sortedMap = new HashMap<>((Map) mapList);
+            for (int i = 0; i < sortedMap.size(); i++) {
+                Data data1 = sortedMap.get(i);
+                for (int j = 0; j < dataCash.size(); j++) {
+                    Data data2 = dataCash.get(j);
+                    if(data1.equals(data2)) {
+                        dataCash.remove(dataCash.get(j));
+                        break;
+                    }
+
+                }
+
+            }
+        }
     }
 }
