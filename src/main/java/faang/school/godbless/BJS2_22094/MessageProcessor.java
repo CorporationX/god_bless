461347791ba38f669369c96b4b44b3f8a7a7e1f8
Denis Artamonov <dns.artamonov@gmail.com>
2024-08-14 22:29:16 +0300

    BJS2-22094 bug fixes

--- src/main/java/faang/school/godbless/BJS2_22094/MessageProcessor.java
+++ src/main/java/faang/school/godbless/BJS2_22094/MessageProcessor.java
@@ -3,9 +3,9 @@ package faang.school.godbless.BJS2_22094;
 import java.util.List;
 
 public class MessageProcessor {
-    public boolean processMessage(String str, List<MessageFilter> msgFilterList) {
-        for (MessageFilter msgFilter : msgFilterList) {
-            if (!msgFilter.filter(str)) {
+    public boolean processMessage(String message, List<MessageFilter> messageFilterList) {
+        for (MessageFilter messageFilter : messageFilterList) {
+            if (!messageFilter.filter(message)) {
                 return false;
             }
         }
