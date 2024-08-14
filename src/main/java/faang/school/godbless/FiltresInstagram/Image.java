d342e8097c12f7996b684cfbff2e9473797d4096
Владислав Воронов <iPlesk90@yandex.ru>
2024-08-14 17:57:18 +0300

    Выполнил задачу

---
+++ src/main/java/faang/school/godbless/FiltresInstagram/Image.java
@@ -0,0 +1,11 @@
+package faang.school.godbless.FiltresInstagram;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class Image {
+    private String name;
+    private String description;
+}
