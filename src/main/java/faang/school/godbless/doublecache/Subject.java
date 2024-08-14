2fdd4c284d2efa6688da3f2cda0ba609acf1f8b4
it-mikhail <vulp@yandex.ru>
2024-08-14 23:28:25 +0300

    Double cache

---
+++ src/main/java/faang/school/godbless/doublecache/Subject.java
@@ -0,0 +1,15 @@
+package faang.school.godbless.doublecache;
+
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.ToString;
+
+@Getter
+@ToString
+@EqualsAndHashCode
+@AllArgsConstructor
+public class Subject {
+    private Integer id;
+    private String name;
+}
