fb04064d7517048ebd8413a41a5b4e59ee21789c
avoevoda <voevodacode@gmail.com>
2024-08-14 11:14:11 +0300

    Meta-universe

---
+++ src/main/java/faang/school/godbless/BJS2_21980/Notification.java
@@ -0,0 +1,13 @@
+package faang.school.godbless.BJS2_21980;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+public class Notification {
+    private String type;
+    private String message;
+}
