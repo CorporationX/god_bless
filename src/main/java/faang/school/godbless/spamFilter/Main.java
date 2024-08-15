103beafb80b6acaa7387e9b3455a864ff43ae9f6
Alena Guskova <amguskova@edu.hse.ru>
2024-08-15 20:54:17 +0500

    BJS2-22106 Фильтруем спам

---
+++ src/main/java/faang/school/godbless/spamFilter/Main.java
@@ -0,0 +1,22 @@
+package faang.school.godbless.spamFilter;
+
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) {
+        MessageProcessor messageProcessor = new MessageProcessor();
+
+        MessageFilter spamFilter = message -> !message.toLowerCase().contains("спам");
+        MessageFilter lengthFilter = message -> message.length() > 10;
+        MessageFilter emojiFilter = message -> !message.contains("😀");
+
+        List<MessageFilter> filters = List.of(spamFilter, lengthFilter, emojiFilter);
+
+        String[] messages = {"Привет!", "Это спам!", "Как дела? 😀", "Подходящее сообщение!!!"};
+
+        for (String message : messages) {
+            boolean isFiltered = messageProcessor.processMessage(message, filters);
+            System.out.println("Сообщение: " + message + " | Пропущено: " + isFiltered);
+        }
+    }
+}
