ff839c026ab45c949bd154bd1b2d3d065ad11765
Sergey Sklyar <sklyar1091@gmail.com>
2024-08-15 23:10:05 +0300

    Filtering Spam!

---
+++ src/main/java/BJS2_22136_SpamFilter/MessageFilter.java
@@ -0,0 +1,5 @@
+package BJS2_22136_SpamFilter;
+
+public interface MessageFilter {
+    boolean filter(String message);
+}
---
+++ src/main/java/BJS2_22136_SpamFilter/MessageProcessor.java
@@ -0,0 +1,19 @@
+package BJS2_22136_SpamFilter;
+
+import java.util.List;
+
+public class MessageProcessor {
+    public static boolean processMessage(String message, List<MessageFilter> filterList) {
+        if (message == null) {
+            throw new IllegalArgumentException("Message can't be null!");
+        }
+        boolean isPassedFlag = true;
+        for (MessageFilter messageFilter : filterList) {
+            if (!messageFilter.filter(message)) {
+                isPassedFlag = false;
+                return isPassedFlag;
+            }
+        }
+        return isPassedFlag;
+    }
+}
