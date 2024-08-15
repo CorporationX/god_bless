af616aa204f784deabcb4e0f7d96f84e8bc92cc8
PLSGOTOASS <koteykaarisha@mail.ru>
2024-08-15 15:02:18 +0300

    Сделал класс LocationSearchEngine, который содержит методы для работы с локациями. Локация включает в себя имя, широту и долготу. В Main тесты

---
+++ src/main/java/faang/school/godbless/GoogleMaps/Location.java
@@ -0,0 +1,14 @@
+package faang.school.godbless.GoogleMaps;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@AllArgsConstructor
+@Getter
+@Setter
+public class Location {
+    private String name;
+    private double latitude;
+    private double longitude;
+}
