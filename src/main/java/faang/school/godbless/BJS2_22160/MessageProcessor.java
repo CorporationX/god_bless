4769da0f36f70a54bc3acd04543c57204c084c36
avoevoda <voevodacode@gmail.com>
2024-08-15 14:00:09 +0300

    Filtering spam

---
+++ src/main/java/faang/school/godbless/BJS2_22160/MessageProcessor.java
@@ -0,0 +1,27 @@
+package faang.school.godbless.BJS2_22160;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class MessageProcessor {
+    public boolean processMessage(String message, MessageFilter filter) {
+        return filter.filter(message);
+    }
+
+    public static void main(String[] args) {
+        MessageProcessor messageProcessor = new MessageProcessor();
+
+        MessageFilter spamFilter = message -> !message.toLowerCase().contains("спам");
+        MessageFilter lengthFilter = message -> message.length() > 10;
+        MessageFilter emojiFilter = message -> !message.contains("😀");
+
+        List<MessageFilter> filters = Arrays.asList(spamFilter, lengthFilter, emojiFilter);
+
+        String[] messages = {"Привет!", "Это спам!", "Как дела? 😀", "Длинное сообщение без спама и эмодзи"};
+
+        for (String message : messages) {
+            boolean isFiltered = messageProcessor.processMessage(message, spamFilter);
+            System.out.println("Сообщение: " + message + " | Пропущено: " + isFiltered);
+        }
+    }
+}
