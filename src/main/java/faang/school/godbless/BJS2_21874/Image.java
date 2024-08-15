bb8ac7988bd04c931b26ce6da06ae98385b30a8e
Prigozhaeva-Anastasia <nastaprigozaeva20@gmail.com>
2024-08-15 13:46:56 +0300

    Instagram filters

---
+++ src/main/java/faang/school/godbless/BJS2_21874/Image.java
@@ -0,0 +1,13 @@
+package faang.school.godbless.BJS2_21874;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.ToString;
+
+@Getter
+@ToString
+@AllArgsConstructor
+public class Image {
+    private String fileName;
+    private String description;
+}
