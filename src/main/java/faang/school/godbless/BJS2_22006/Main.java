01c557ea74aa73b5821cf0ca02970af8b8579e54
Peregruzochka <peregruzochka48@mail.ru>
2024-08-15 15:54:03 +0300

    добавил фильтрацию уведомлений

--- src/main/java/faang/school/godbless/BJS2_22006/Main.java
+++ src/main/java/faang/school/godbless/BJS2_22006/Main.java
@@ -4,18 +4,26 @@ public class Main {
     public static void main(String[] args) {
         NotificationManager notificationManager = new NotificationManager();
 
-// Регистрация обработчиков оповещений
+
         notificationManager.registerHandler("email", (notification) -> System.out.println("Sending by email: " + notification.getMessage()));
         notificationManager.registerHandler("sms", (notification) -> System.out.println("Sending SMS: " + notification.getMessage()));
         notificationManager.registerHandler("push", (notification) -> System.out.println("Sending a push notification: " + notification.getMessage()));
 
-// Отправка оповещений
-        Notification emailNotification = new Notification("email", "Your account has been successfully activated");
+        Notification emailNotificationOne = new Notification("email", "Your account has been successfully activated");
+        Notification emailNotificationTwo = new Notification("email", "");
         Notification smsNotification = new Notification("sms", "You have successfully changed your password");
         Notification pushNotification = new Notification("push", "New post from user: Frank");
 
-        notificationManager.sendNotification(emailNotification);
+        notificationManager.sendNotification(emailNotificationOne);
         notificationManager.sendNotification(smsNotification);
         notificationManager.sendNotification(pushNotification);
+
+        notificationManager.addNotificationFilter(notification -> !notification.getMessage().isBlank());
+        notificationManager.addNotificationFilter(notification -> !notification.getType().equals("sms"));
+
+        System.out.println(notificationManager.isValidNotification(emailNotificationOne));
+        System.out.println(notificationManager.isValidNotification(emailNotificationTwo));
+        System.out.println(notificationManager.isValidNotification(smsNotification));
+        System.out.println(notificationManager.isValidNotification(pushNotification));
     }
 }
