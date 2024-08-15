dc1ccf3e1019fe9605c0245e1d6c0165c7cc086d
Stanislaav Kozh <printks@yandex.ru>
2024-08-15 15:49:01 +0300

    Java HashMap: Герои меча и магии

---
+++ src/main/java/Main.java
@@ -0,0 +1,28 @@
+import entity.Angel;
+import entity.Griffin;
+import entity.Pikeman;
+import entity.Swordman;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Hero hero1 = new Hero("Sir Petr", "Castle");
+        Hero hero2 = new Hero("Lord Vania", "Necropolis");
+
+        hero1.addCreature(new Pikeman(), randomNumber(1, 78));
+        hero1.addCreature(new Griffin(), randomNumber(5, 61));
+
+        hero2.addCreature(new Swordman(), randomNumber(21, 71));
+        hero2.addCreature(new Angel(), randomNumber(3, 17));
+
+        Battlefield battlefield = new Battlefield(hero1, hero2);
+        Hero winner = battlefield.battle();
+
+        System.out.println("Win: " + winner.getName());
+    }
+
+    public static int randomNumber(int min, int max) {
+        return (int) (Math.random() * (max - min) + min);
+    }
+
+}
---
+++ src/main/java/entity/Griffin.java
@@ -0,0 +1,9 @@
+package entity;
+
+public class Griffin extends Creature {
+
+    public Griffin() {
+        super("Griffin", 3, 8, 5, 7);
+    }
+
+}
