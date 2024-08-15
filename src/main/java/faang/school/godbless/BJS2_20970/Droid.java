7149a9b954ad61aabf4b8e463104db86f1a07b2e
zeus <denis@fatum.ru>
2024-08-15 16:50:28 +0300

    BJS2-20970 bug fixes

--- src/main/java/faang/school/godbless/BJS2_20970/Droid.java
+++ src/main/java/faang/school/godbless/BJS2_20970/Droid.java
@@ -1,25 +1,67 @@
 package faang.school.godbless.BJS2_20970;
 
+
 public class Droid {
+    private final static int MAX_UPPER_CODE = 90;
+    private final static int MIN_UPPER_CODE = 65;
+    private final static int MAX_LOWER_CODE = 122;
+    private final static int MIN_LOWER_CODE = 97;
+
     public String sendEncryptedMessage(String message, int key) {
+        StringBuilder result = new StringBuilder();
         DroidMessageEncryptor droidMessageEncryptor = (msg, encryptKey) -> {
-            char[] chars = msg.toCharArray();
-            for (int i = 0; i < chars.length; i++) {
-                chars[i] = (char) (chars[i] + encryptKey);
+            for (char c : message.toCharArray()) {
+                if (Character.isAlphabetic(c)) {
+                    if (Character.isUpperCase(c)) {
+                        int code = encrypt(c, key, MIN_UPPER_CODE, MAX_UPPER_CODE);
+                        result.append((char) code);
+                    } else {
+                        int code = encrypt(c, key, MIN_LOWER_CODE, MAX_LOWER_CODE);
+                        result.append((char) code);
+                    }
+                } else {
+                    result.append(c);
+                }
             }
-            return String.valueOf(chars);
+            return result.toString();
         };
         return droidMessageEncryptor.encrypt(message, key);
     }
 
-    public String receiveEncryptedMessage(String message, int key) {
-        DroidMessageEncryptor droidMessageDecryptor = (msg, encryptKey) -> {
-            char[] chars = message.toCharArray();
-            for (int i = 0; i < chars.length; i++) {
-                chars[i] = (char) (chars[i] - key);
-            }
-            return String.valueOf(chars);
-        };
-        return droidMessageDecryptor.encrypt(message, key);
+    private static int encrypt(char c, int key, int minBorder, int maxBorder) {
+        int code = c;
+        code += key;
+        if (code > maxBorder) {
+            code = code % maxBorder + minBorder - 1;
+        }
+        return code;
     }
+
+    private static int decrypt(char c, int key, int minBorder, int maxBorder) {
+        int code = c;
+        code -= key % 26;
+        if (code < minBorder) {
+            code = maxBorder - (minBorder - code) + 1;
+        }
+        return code;
+    }
+
+    public String receiveEncryptedMessage(String message, int key) {
+        StringBuilder result = new StringBuilder();
+        for (char c : message.toCharArray()) {
+            if (Character.isAlphabetic(c)) {
+                if (Character.isUpperCase(c)) {
+                    int code = decrypt(c, key, MIN_UPPER_CODE, MAX_UPPER_CODE);
+                    result.append((char) code);
+                } else if (Character.isLowerCase(c)) {
+                    int code = decrypt(c, key, MIN_LOWER_CODE, MAX_LOWER_CODE);
+                    result.append((char) code);
+                }
+            } else {
+                result.append(c);
+            }
+        }
+        return result.toString();
+    }
+
 }
--- src/main/java/faang/school/godbless/BJS2_20970/Main.java
+++ src/main/java/faang/school/godbless/BJS2_20970/Main.java
@@ -5,8 +5,8 @@ public class Main {
         Droid r2d2 = new Droid();
         Droid c3po = new Droid();
         String message = "May the Force be with you!";
-        int encryptionKey = 5;
-
+        int encryptionKey = 1;
+        System.out.println(message);
         String encryptedMsg = r2d2.sendEncryptedMessage(message, encryptionKey);
         System.out.println(encryptedMsg);
         String msg = c3po.receiveEncryptedMessage(encryptedMsg, encryptionKey);
