3888662d7135d357e00b31b745b459666b5dd634
Ilia Chudov <chudow-iluha@mail.ru>
2024-08-15 11:23:29 +0300

    Добавил класс-модель Notification и класс NotificationManager c методом работы программы.

---
+++ src/main/java/faang/school/godbless/BJS2_22020/Notification.java
@@ -0,0 +1,13 @@
+package faang.school.godbless.BJS2_22020;
+
+import lombok.*;
+
+@Getter
+@Setter
+@ToString
+@AllArgsConstructor
+@EqualsAndHashCode
+public class Notification {
+    private String type;
+    private String message;
+}
