7cf8b969240571ca7218a3e31f3e2442fc3407c2
Chibix <chibisovanastena@gmail.com>
2024-08-15 15:48:54 +0300

    BJS2-20505 - Повернем матрицу

---
+++ src/main/java/faang/school/godbless/task20505/Coordinates.java
@@ -0,0 +1,6 @@
+package faang.school.godbless.task20505;
+
+public class Coordinates {
+    private int x;
+    private int y;
+}
---
+++ src/main/java/faang/school/godbless/task20505/MatrixTransformer.java
@@ -0,0 +1,5 @@
+package faang.school.godbless.task20505;
+
+public interface MatrixTransformer {
+    public int [][] transformMatrix(int [][] array);
+}
