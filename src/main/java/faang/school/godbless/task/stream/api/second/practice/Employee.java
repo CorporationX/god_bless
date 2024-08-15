fb1dfed6e3dc1e4627e3875bbef1d9c8adcac13a
Sultan <nikowosuka@gmail.com>
2024-08-15 23:21:35 +0500

    stream api second practice task perform

---
+++ src/main/java/faang/school/godbless/task/stream/api/second/practice/Employee.java
@@ -0,0 +1,12 @@
+package faang.school.godbless.task.stream.api.second.practice;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
+public class Employee {
+    private final String name;
+    private final int paycheck;
+    private final String division;
+}
