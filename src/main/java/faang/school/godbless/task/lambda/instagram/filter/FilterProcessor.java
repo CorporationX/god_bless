c54b72db31c3f64a2601208cba58578c6783af1a
Sultan <nikowosuka@gmail.com>
2024-08-15 10:03:15 +0500

    combineFilters method refactoring

--- src/main/java/faang/school/godbless/task/lambda/instagram/filter/FilterProcessor.java
+++ src/main/java/faang/school/godbless/task/lambda/instagram/filter/FilterProcessor.java
@@ -3,18 +3,13 @@ package faang.school.godbless.task.lambda.instagram.filter;
 import java.util.function.Function;
 
 public class FilterProcessor {
-    
+
     public Image applyFilter(Image image, Function<Image, Image> function) {
         return function.apply(image);
     }
 
     public Function<Image, Image> combineFilters(Function<Image, Image> function1,
                                                  Function<Image, Image> function2) {
-        Function<Image, Image> combineFunction = image -> {
-            image = function1.apply(image);
-            image = function2.apply(image);
-            return image;
-        };
-        return combineFunction;
+        return function1.andThen(function2);
     }
 }
