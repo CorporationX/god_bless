c7a634c8752cea321ced4fc834be75b3959dc670
Ilya Merkulov <imerkulov@fun.co>
2024-08-15 17:36:51 +0300

    Implement Task 23: Handle Errors

--- src/main/java/faang/school/godbless/HandleErrors/OperationCall.java
+++
@@ -1,6 +0,0 @@
-package faang.school.godbless.HandleErrors;
-
-@FunctionalInterface
-public interface OperationCall<T> {
-    T call() throws Exception;
-}
--- src/main/java/faang/school/godbless/HandleErrors/RemoteService.java
+++ src/main/java/faang/school/godbless/HandleErrors/RemoteService.java
@@ -1,9 +1,11 @@
 package faang.school.godbless.HandleErrors;
 
+import java.util.function.Supplier;
+
 public class RemoteService {
-    public static <T> T withErrorHandling(OperationCall<T> call, ExceptionHandler<T> handler) {
+    public static <T> T withErrorHandling(Supplier<T> action, ExceptionHandler<T> handler) {
         try {
-            return call.call();
+            return action.get();
         } catch (Exception e) {
             return handler.handle(e);
         }
