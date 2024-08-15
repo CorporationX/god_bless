69e3d8b1ec049b3faeb8866133badeae030626a6
Garib_Aghayev <garibaghayev@gmail.com>
2024-08-15 07:43:30 +0300

    first release

--- src/main/java/faang/school/godbless/User.java
+++ src/main/java/faang/school/godbless/User.java
@@ -1,38 +1,8 @@
 package faang.school.godbless;
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.ToString;
-
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-
-@Getter
-@AllArgsConstructor
-@ToString
 public class User {
-    private String name;
+    public String name;
     private int age;
-    private String job;
-    private String adress;
-
-
-    public static Map<Integer, List<User>> groupUsers(List<User> listOfUsers) throws Exception {
-        Map<Integer, List<User>> result = new HashMap<>();
-
-        if (listOfUsers.isEmpty()) {
-            throw new IllegalArgumentException();
-        }
-        for (User user : listOfUsers) {
-            if(!result.containsKey(user.getAge())){
-                result.put(user.getAge(), new ArrayList<>());
-            }
-
-            result.get(user.getAge()).add(user);
-        }
-
-        return result;
-    }
+    public String job;
+    public String adress;
 }
