961c1ef4401c65cad88f8507c206426f491854ae
Peregruzochka <peregruzochka48@mail.ru>
2024-08-15 15:56:00 +0300

    добавил фильтрацию уведомлений

--- src/main/java/faang/school/godbless/BJS2_22006/Main.java
+++ src/main/java/faang/school/godbless/BJS2_22006/Main.java
@@ -4,7 +4,6 @@ public class Main {
     public static void main(String[] args) {
         NotificationManager notificationManager = new NotificationManager();
 
-
         notificationManager.registerHandler("email", (notification) -> System.out.println("Sending by email: " + notification.getMessage()));
         notificationManager.registerHandler("sms", (notification) -> System.out.println("Sending SMS: " + notification.getMessage()));
         notificationManager.registerHandler("push", (notification) -> System.out.println("Sending a push notification: " + notification.getMessage()));
