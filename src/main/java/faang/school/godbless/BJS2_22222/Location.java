5715297aa9a9a3674f86611a4492ade4a7e5c22a
Ilia Chudov <chudow-iluha@mail.ru>
2024-08-15 12:33:05 +0300

    Добавил класс-модель Location и класс LocationSearchEngine c методом работы программы.

---
+++ src/main/java/faang/school/godbless/BJS2_22222/Location.java
@@ -0,0 +1,14 @@
+package faang.school.godbless.BJS2_22222;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+public class Location {
+    private String name;
+    private Double latitude;
+    private Double longitude;
+}
