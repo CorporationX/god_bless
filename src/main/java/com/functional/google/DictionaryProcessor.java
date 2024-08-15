7513e5daa90a8362a917ffa2c398171eef0c42bc
Evgen <fragmatik.Oo@yandex.ru>
2024-08-15 13:50:12 +0300

    Исправил

--- src/main/java/com/functional/google/DictionaryProcessor.java
+++ src/main/java/com/functional/google/DictionaryProcessor.java
@@ -1,12 +1,21 @@
 package com.functional.google;
 
+import lombok.NonNull;
+
 import java.util.function.BiConsumer;
 
 public class DictionaryProcessor {
 
     public void processWord(String word, String translation, BiConsumer<String, String> consumer) {
-        if (!word.isEmpty() && !translation.isEmpty()) {
-            consumer.accept(word, translation);
+        if (word.isEmpty() && translation.isEmpty()) {
+            return;
         }
+        if (word.isBlank() && translation.isBlank()) {
+            return;
+        }
+        if(word == null&& translation==null){
+            return;
+        }
+        consumer.accept(word, translation);
     }
 }
