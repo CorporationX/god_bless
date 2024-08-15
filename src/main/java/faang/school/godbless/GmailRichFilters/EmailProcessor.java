d10ae2e51d880c3b67eb13506adc7d6c1414c52c
Владислав Воронов <iPlesk90@yandex.ru>
2024-08-15 10:24:24 +0300

    Выполнил задачу

---
+++ src/main/java/faang/school/godbless/GmailRichFilters/EmailProcessor.java
@@ -0,0 +1,17 @@
+package faang.school.godbless.GmailRichFilters;
+
+import java.util.List;
+import java.util.function.Consumer;
+import java.util.function.Function;
+import java.util.function.Predicate;
+
+public class EmailProcessor {
+    public void processEmails(List<Email> emailList, Predicate<Email> predicate, Consumer<Email> consumer,
+                              Function<Email, String> function) {
+        for (Email email : emailList) {
+            predicate.test(email);
+            function.apply(email);
+            consumer.accept(email);
+        }
+    }
+}
