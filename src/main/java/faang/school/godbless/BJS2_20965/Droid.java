da60356de61a9d3c8260d31f3b7464fbafe51e57
Amirhan Almeev <almeevamirhan294@gmail.com>
2024-08-15 15:56:26 +0300

    Дроиды решение

--- src/main/java/faang/school/godbless/BJS2_20965/Droid.java
+++ src/main/java/faang/school/godbless/BJS2_20965/Droid.java
@@ -7,13 +7,61 @@ public class Droid {
             for (char letter : string.toCharArray()) {
                 int code = letter + key % 26;
                 if (Character.isLetter(letter)) {
-                    res.append((char) (code <= 'z' ? code : code -  ));
-                } else{
-                    res.append((char) (code <= '9' ? code : code - 10));
+                    res.append((char) getLetterCodeEncrypt(code));
+                } else if(Character.isDigit(letter)){
+                    res.append((char) getNumberCodeEncrypt(code));
+                } else {
+                    res.append(letter);
                 }
             }
             return res.toString();
         };
         return encryptor.messageEncryption(message, encryptionKey);
+    }
+
+    public static String receiveEncryptedMessage(String encryptedMessage, int encriptionKey) {
+        DroidMessageEncryptor decryptor = (string, key) -> {
+            StringBuilder res = new StringBuilder();
+            for (char letter : string.toCharArray()) {
+                int code = letter - key % 26;
+                if (Character.isLetter(letter)) {
+                    res.append((char) getLetterCodeDecrypt(code));
+                } else if(Character.isDigit(letter)){
+                    res.append((char) getNumberCodeDecrypt(code));
+                } else{
+                    res.append(letter);
+                }
+            }
+            return res.toString();
+        };
+        return decryptor.messageEncryption(encryptedMessage, encriptionKey);
+    }
+
+    private static int getLetterCodeEncrypt(int code) {
+        if ('a' <= code && code <= 'z' || 'A' <= code && code <= 'Z') {
+            return code;
+        }
+        return code - 26;
+    }
+
+    private static int getNumberCodeEncrypt(int code) {
+        if (code <= '9') {
+            return code;
+        }
+        return code - 10;
+    }
+
+    private static int getLetterCodeDecrypt(int code) {
+        if ('a' <= code && code <= 'z' || 'A' <= code && code <= 'Z') {
+            return code;
+        }
+        return code + 26;
+    }
+
+    private static int getNumberCodeDecrypt(int code) {
+        if (code >= '0') {
+            return code;
+        }
+        return code + 10;
     }
 }
--- src/main/java/faang/school/godbless/BJS2_20965/Main.java
+++ src/main/java/faang/school/godbless/BJS2_20965/Main.java
@@ -3,7 +3,10 @@ package faang.school.godbless.BJS2_20965;
 public class Main {
     public static void main(String[] args) {
         Droid droid = new Droid();
-        String message = "RobertZ";
-        System.out.println(droid.sendEncryptedMessage(message, 2));
+        String message = "Amirhan1234567890 sdfdfdf";
+        String str = droid.sendEncryptedMessage(message, 2);
+        System.out.println(str);
+        String str2 = droid.receiveEncryptedMessage(str, 2);
+        System.out.println(str2);
     }
 }
