f92ba0f5bb6819bb9fe1891dd4f5473e608d39f8
Dmitriy Varankin <dima.varankin2012@yandex.ru>
2024-08-16 16:46:11 +0300

    2 версия решения задачи: Развернитесь!

--- src/main/java/faang/school/godbless/BJS2_19159/Example.java
+++ src/main/java/faang/school/godbless/BJS2_19159/Example.java
@@ -7,13 +7,13 @@ public class Example {
         int element;
         for (int i = 0; i < array.length / 2; i++) {
             element = array[i];
-            array[i] = array.length - i;
+            array[i] = array[array.length - 1 - i];
             array[array.length - 1 - i] = element;
         }
     }
 
     public static void main(String[] args) {
-        int[] array = new int[]{1, 2, 3, 4, 5};
+        int[] array = new int[]{11, 1, 5, 3, 4, 5, 6, 0, 44, 0, -23};
         reverse(array);
         System.out.println(Arrays.toString(array));
     }
