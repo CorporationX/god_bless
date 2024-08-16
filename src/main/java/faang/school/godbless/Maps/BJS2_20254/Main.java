40fe9678b3f054d604dca88df8d5c91af872bdf7
Garib_Aghayev <garibaghayev@gmail.com>
2024-08-16 20:22:15 +0300

    first release

--- src/main/java/faang/school/godbless/Maps/BJS2_20254/Main.java
+++ src/main/java/faang/school/godbless/Maps/BJS2_20254/Main.java
@@ -1,46 +1,46 @@
 package faang.school.godbless.Maps.BJS2_20254;
 
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.net.URL;
-import java.net.URLConnection;
+import lombok.Getter;
+
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+
 public class Main {
     private static Map<String, List<WebPage>> webPageList = new HashMap<>();
+
     public static void main(String[] args) {
 
-        WebPage webPage = new WebPage("https://www.amrood.com");
-        indexWebPage(webPage);
+        WebPage webPageAmrood = new WebPage("https://www.amrood.com");
+        WebPage webPageGoogel = new WebPage("https://www.google.com");
 
-        remove("https://www.amrood.com");
+        indexWebPage(webPageAmrood);
+        indexWebPage(webPageGoogel);
+        System.out.println(webPageList);
+        remove(webPageAmrood.getUrl());
+//        remove(webPage2.getUrl());
         System.out.println(search("Good"));
         System.out.println(webPageList);
     }
 
-    public static void indexWebPage(WebPage webPage)  {
+    public static void indexWebPage(WebPage webPage) {
         String[] strings = webPage.getContent().split("\\W+");
-        for (String str : strings){
-            webPageList.computeIfAbsent(str, k -> new ArrayList<WebPage>());
-            webPageList.get(str).add(webPage);
+        for (String str : strings) {
+            webPageList.computeIfAbsent(str, k -> new ArrayList<>()).add(webPage);
         }
     }
-    public static List<WebPage> search(String string){
+
+    public static List<WebPage> search(String string) {
         return webPageList.get(string);
     }
-    public static void remove(String url){
-        for(Map.Entry<String, List<WebPage>> entry : webPageList.entrySet()){
-           for(WebPage webPage : entry.getValue()) {
-               if(webPage.getUrl().equals(url)){
-                   entry.getValue().remove(webPage);
-               }
-           }
+
+    public static void remove(String url) {
+        for (Map.Entry<String, List<WebPage>> webPageEntry : webPageList.entrySet()) {
+            List<WebPage> pages = webPageEntry.getValue();
+            pages.removeIf(page -> page.getUrl().equals(url));
         }
     }
 }
