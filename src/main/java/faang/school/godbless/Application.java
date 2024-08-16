5b27fbcff4a3ef11e1714350c0d2a3c496deb98a
Bobiktrans <Bobicktrnas@yandex.ru>
2024-08-16 13:50:22 +0300

    BJS2-21450

--- src/main/java/faang/school/godbless/Application.java
+++ src/main/java/faang/school/godbless/Application.java
@@ -1,7 +1,33 @@
 package faang.school.godbless;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Application {
     public static void main(String[] args) {
-        System.out.println("God Bless!");
+        List<Integer> nums = List.of(1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
+
+        int sumResult = sum(nums);
+        int productResult = product(nums);
+
+        System.out.println(sumResult);
+        System.out.println(productResult);
+    }
+
+    public static int calculate(List<Integer> nums, Calculator<Integer> calculator){
+
+        int result = nums.get(0);
+        for (int i = 1; i < nums.size(); i++) {
+            result = calculator.apply(result, nums.get(i));
+        }
+        return result;
+    }
+
+    public static int product(List<Integer> nums) {
+        return calculate(nums, (a, b) -> a * b);
+    }
+
+    public static int sum(List<Integer> nums) {
+        return calculate(nums, Integer::sum);
     }
 }
