721437b3a70aefa1fcc91f0b9be2bbb4307eea75
TheGreatLucky <danke_villetti@mail.ru>
2024-08-16 17:30:37 +0500

    added validations

--- src/main/java/faang/school/godbless/TriangleArea/triangleArea.java
+++ src/main/java/faang/school/godbless/TriangleArea/triangleArea.java
@@ -1,12 +1,20 @@
 package faang.school.godbless.TriangleArea;
 
+import lombok.NonNull;
+
 import java.util.function.Function;
 
 public class triangleArea {
     public static void main(String[] args) {
         System.out.println(getTriangleArea(5,4,3)); // 4*3/2 = 6.0
     }
-    public static Double getTriangleArea(double a, double b, double c) throws IllegalArgumentException {
+    public static Double getTriangleArea(@NonNull double a,@NonNull double b,@NonNull double c) throws IllegalArgumentException {
+        if (a<=0 || b<=0 || c<=0){
+            throw new IllegalArgumentException("there should not be a negative length");
+        }
+        if (a >= b+c || b>= c+a || c>= a+b){
+            throw new IllegalArgumentException("a triangle with such parameters does not exist");
+        }
         Function<Double, Function<Double, Double>> sum = (x) -> y -> x + y;
         Function<Double, Function<Double, Double>> mul = (x) -> y -> x * y;
         Function<Double, Function<Double, Double>> sub = (x) -> y -> x - y;
