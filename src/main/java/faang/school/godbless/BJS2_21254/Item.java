bf369f318a29c185fc411ab1afef7f7e0415ba45
Prigozhaeva-Anastasia <nastaprigozaeva20@gmail.com>
2024-08-16 15:39:12 +0300

    Lord of the Rings RPG

---
+++ src/main/java/faang/school/godbless/BJS2_21254/Item.java
@@ -0,0 +1,13 @@
+package faang.school.godbless.BJS2_21254;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.ToString;
+
+@Getter
+@ToString
+@AllArgsConstructor
+public class Item {
+    private String name;
+    private int value;
+}
