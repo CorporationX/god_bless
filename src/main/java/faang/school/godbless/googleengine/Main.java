918a13279d6f346e506650ac47c72cbc380ad426
it-mikhail <vulp@yandex.ru>
2024-08-16 20:51:13 +0300

    checkpoint

---
+++ src/main/java/faang/school/godbless/googleengine/Main.java
@@ -0,0 +1,31 @@
+package faang.school.godbless.googleengine;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
+
+public class Main {
+
+    public static void main(String[] args) {
+        WebPageService webPageService = new WebPageService();
+        Map<String, Set<WebPage>> webPagesMap = new HashMap<>();
+
+        webPageService.addWebPage(webPagesMap, new WebPage("yandex.ru", "Yandex", "music, marketplace, search"));
+        webPageService.addWebPage(webPagesMap, new WebPage("twitch.tv", "Twitch", "stream, game"));
+        webPageService.addWebPage(webPagesMap, new WebPage("youtube.com", "Youtube", "video, stream, game"));
+
+        webPageService.webPagesInfo(webPagesMap);
+        System.out.println();
+
+        var searchResult = webPageService.findPagesByKeyWord(webPagesMap, "stream");
+        System.out.println(searchResult);
+
+        webPageService.findPagesByKeyWord(webPagesMap, "123");
+        System.out.println();
+
+        webPageService.removePageByUrl(webPagesMap, "youtube.com");
+
+        webPageService.webPagesInfo(webPagesMap);
+    }
+
+}
---
+++ src/main/java/faang/school/godbless/googleengine/WebPage.java
@@ -0,0 +1,14 @@
+package faang.school.godbless.googleengine;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class WebPage {
+
+    private String url;
+    private String title;
+    private String content;
+
+}
