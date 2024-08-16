dd9a743cc4a2ce2b89c14e5b095fd07630824be0
Chibix <chibisovanastena@gmail.com>
2024-08-16 16:25:44 +0300

    BJS2-20977 - Cекреты дроидов

--- src/main/java/faang/school/godbless/task20977/Droid.java
+++ src/main/java/faang/school/godbless/task20977/Droid.java
@@ -2,6 +2,7 @@ package faang.school.godbless.task20977;
 
 public class Droid {
     String sendEncryptedMessage(Droid recipient, String massage, int encryption){
+        
         return ;
     }
 }
---
+++ src/main/java/faang/school/godbless/task20977/DroidMassageReceiver.java
@@ -0,0 +1,7 @@
+package faang.school.godbless.task20977;
+
+public class DroidMassageReceiver {
+ public void receiveEncryptedMessage(String encryptMassage, int encryption){
+
+ }
+}
