9c8096bb313173cbf21659bf5f0ce99acae749e9
Sergey Sklyar <sklyar1091@gmail.com>
2024-08-16 10:12:34 +0300

    Постарался переименовать без слова map/list в названии - Лью!

--- src/main/java/BJS2_19506/Main.java
+++ src/main/java/BJS2_19506/Main.java
@@ -9,42 +9,42 @@ public class Main {
     private static final String NULL_RESULT_MESSAGE = "Student or studentsList can't be null!";
 
     public static void main(String[] args) {
-        List<Student> studentList = new ArrayList<>();
+        List<Student> students = new ArrayList<>();
 
-        studentList.add(new Student("Misha", "Physics", 4));
-        studentList.add(new Student("Yana", "English", 4));
-        studentList.add(new Student("Olya", "Programming", 3));
-        studentList.add(new Student("Stas", "Mathematics", 3));
-        studentList.add(new Student("Marina", "Programming", 2));
-        studentList.add(new Student("Egor", "English", 4));
+        students.add(new Student("Misha", "Physics", 4));
+        students.add(new Student("Yana", "English", 4));
+        students.add(new Student("Olya", "Programming", 3));
+        students.add(new Student("Stas", "Mathematics", 3));
+        students.add(new Student("Marina", "Programming", 2));
+        students.add(new Student("Egor", "English", 4));
 
         Student testStudent = new Student("Georgiy", "Physics", 4);
         FacultyYearPair testFacultyYearPair = new FacultyYearPair("English", 4);
 
-        //Students Map by faculty and year
-        System.out.println(getStudentsGroupedByFacultyYear(studentList));
+        //Students by faculty and year
+        System.out.println(getStudentsGroupedByFacultyYear(students));
 
         //Add Student to List
-        addStudent(testStudent, studentList);
+        addStudent(testStudent, students);
 
         //Remove Student from list
-        removeStudent(null, studentList);
+        removeStudent(null, students);
 
         //Get student list using faculty + year pair
-        System.out.println(getStudentsByFacultyAndYear(testFacultyYearPair, studentList));
+        System.out.println(getStudentsByFacultyAndYear(testFacultyYearPair, students));
 
         //Print all students grouped by faculty and year
-        printAllStudentsByFacultyAndYear(studentList);
+        printAllStudentsByFacultyAndYear(students);
     }
 
     public static Map<FacultyYearPair, List<Student>> getStudentsGroupedByFacultyYear(List<Student> students) {
-        Map<FacultyYearPair, List<Student>> studentMap = new HashMap<>();
+        Map<FacultyYearPair, List<Student>> studentsByFacultyYear = new HashMap<>();
 
         for (Student student : students) {
             FacultyYearPair facultyYearPair = new FacultyYearPair(student.getFaculty(), student.getYear());
-            studentMap.computeIfAbsent(facultyYearPair, key -> new ArrayList<>()).add(student);
+            studentsByFacultyYear.computeIfAbsent(facultyYearPair, key -> new ArrayList<>()).add(student);
         }
-        return studentMap;
+        return studentsByFacultyYear;
     }
 
     public static void addStudent(Student student, List<Student> students) {
@@ -67,19 +67,19 @@ public class Main {
 
     public static List<Student> getStudentsByFacultyAndYear(FacultyYearPair facultyYearPair, List<Student> students) {
 
-        Map<FacultyYearPair, List<Student>> studentsMap = getStudentsGroupedByFacultyYear(students);
-        List<Student> studentList = new ArrayList<>();
+        Map<FacultyYearPair, List<Student>> studentsByFacultyYear = getStudentsGroupedByFacultyYear(students);
+        List<Student> groupedStudents = new ArrayList<>();
 
-        if (studentsMap.containsKey(facultyYearPair)) {
-            studentList.addAll(studentsMap.get(facultyYearPair));
+        if (studentsByFacultyYear.containsKey(facultyYearPair)) {
+            groupedStudents.addAll(studentsByFacultyYear.get(facultyYearPair));
         }
-        return studentList;
+        return groupedStudents;
     }
 
     public static void printAllStudentsByFacultyAndYear(List<Student> students) {
-        Map<FacultyYearPair, List<Student>> map = getStudentsGroupedByFacultyYear(students);
+        Map<FacultyYearPair, List<Student>> studentsByFacultyYear = getStudentsGroupedByFacultyYear(students);
 
-        for (Map.Entry<FacultyYearPair, List<Student>> entry : map.entrySet()) {
+        for (Map.Entry<FacultyYearPair, List<Student>> entry : studentsByFacultyYear.entrySet()) {
             System.out.println(entry.getKey().getFaculty() + " " + entry.getKey().getYear() + " " + entry.getValue());
         }
     }
