0fa5268ebcf6a51bb81b0c58beb5c63e05b02844
Sultan <nikowosuka@gmail.com>
2024-08-16 15:36:37 +0500

    delete comments

--- src/main/java/faang/school/godbless/task/lambda/meta/universe/Main.java
+++ src/main/java/faang/school/godbless/task/lambda/meta/universe/Main.java
@@ -7,12 +7,10 @@ public class Main {
     public static void main(String... args) {
         var notificationManager = new NotificationManager(new HashMap<>(), new HashMap<>());
 
-        // Registration of notification handlers
         notificationManager.registerHandler("email", notification -> System.out.println("Отправка по электронной почте: " + notification.getMassage()));
         notificationManager.registerHandler("sms", notification -> System.out.println("Отправка SMS: " + notification.getMassage()));
         notificationManager.registerHandler("push", notification -> System.out.println("Отправка push-уведомления: " + notification.getMassage()));
 
-        // Registration of notification filters
         List<String> banWords = List.of("kiwi", "apple");
         notificationManager.registerNotificationFilter(
                 notification -> {
@@ -28,15 +26,12 @@ public class Main {
                 }
         );
 
-        // Create notification
         Notification emailNotification = new Notification("email", "Ваша учетная запись успешно активирована");
         Notification smsNotification = new Notification("sms", "Вы успешно изменили свой пароль");
         Notification pushNotification = new Notification("push", "Новый пост от пользователя: JohnDoe");
 
-        // Notification with ban word
         Notification banWordNotification = new Notification("email", "kiwi better than apple!");
 
-        // Send notification
         notificationManager.sendNotification(emailNotification);
         notificationManager.sendNotification(smsNotification);
         notificationManager.sendNotification(pushNotification);
