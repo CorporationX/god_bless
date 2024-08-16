d73cd7db1d6e2ee30385fd70a17acb55c75453b3
Viktor Shpet <shpet.viktor.93@gmail.com>
2024-08-16 19:06:12 +0500

    BSJ2-21375 Фильтруем спам.

---
+++ src/main/java/faang/school/godbless/BJS2_22142/Main.java
@@ -0,0 +1,7 @@
+package faang.school.godbless.BJS2_22142;
+
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/main/java/faang/school/godbless/BJS2_22142/MessageProcessor.java
@@ -0,0 +1,13 @@
+package faang.school.godbless.BJS2_22142;
+
+import java.util.List;
+
+public class MessageProcessor {
+    public boolean processMessage(String message, List<MessageFilter> filters) {
+        for (MessageFilter filter : filters) {
+            if (!filter.filter(message))
+                return true;
+        }
+        return false;
+    }
+}
