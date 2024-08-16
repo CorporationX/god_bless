22391026852564373bb3f952dea3b2bc0d62699d
Chibix <chibisovanastena@gmail.com>
2024-08-16 12:01:20 +0300

    BJS2-20977 - Секреты дроидов

---
+++ src/main/java/faang/school/godbless/task20977/Droid.java
@@ -0,0 +1,7 @@
+package faang.school.godbless.task20977;
+
+public class Droid {
+    String sendEncryptedMessage(Droid recipient, String massage, int encryption){
+        return ;
+    }
+}
---
+++ src/main/java/faang/school/godbless/task20977/DroidMessageEncryptor.java
@@ -0,0 +1,6 @@
+package faang.school.godbless.task20977;
+
+@FunctionalInterface
+public interface DroidMessageEncryptor {
+    String encrypt(String text, int encryption);
+}
