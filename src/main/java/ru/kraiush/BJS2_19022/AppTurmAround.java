514ce8f5cac2ff5b9bcb4325802950e369efc947
Alexander Kraiushkin <akraiushkin@gmail.com>
2024-08-16 12:17:07 +0300

    BJS2-19022: update

--- src/main/java/ru/kraiush/BJS2_19022/AppTurmAround.java
+++ src/main/java/ru/kraiush/BJS2_19022/AppTurmAround.java
@@ -3,7 +3,7 @@ package ru.kraiush.BJS2_19022;
 import java.util.Arrays;
 import java.util.Collections;
 
-public class App_Example {
+public class AppTurmAround {
 
     public static void main(String[] args) {
 
