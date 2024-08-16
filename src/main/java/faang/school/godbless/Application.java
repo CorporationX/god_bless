02b0ae69cfcea0942e9d150f417c487f0b761604
Bobiktrans <Bobicktrnas@yandex.ru>
2024-08-16 17:49:02 +0300

    BJS2-21562

--- src/main/java/faang/school/godbless/Application.java
+++ src/main/java/faang/school/godbless/Application.java
@@ -1,7 +1,31 @@
 package faang.school.godbless;
 
+import java.io.FileNotFoundException;
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class Application {
     public static void main(String[] args) {
-        System.out.println("God Bless!");
+        List<List<String>> table = List.of(
+                List.of("1", "2", "3", "4", "5"),
+                List.of("6", "7", "8", "9", "10"),
+                List.of("11", "12", "13", "14", "15"),
+                List.of("16", "17", "18", "19", "20"),
+                List.of("21", "22", "23", "24", "25")
+        );
+
+        String csv = toCsv(table);
+
+        System.out.println(csv);
+    }
+
+    public static String toCsv(List<List<String>> table) throws IllegalArgumentException {
+        VectorJoiner<String> vectorJoiner = vector -> String.join(", ", vector);
+
+        MatrixJoiner <String> matrixJoiner = matrix -> matrix.stream()
+                .map(vectorJoiner::join)
+                .collect(Collectors.joining("\n"));
+
+        return matrixJoiner.join(table);
     }
 }
