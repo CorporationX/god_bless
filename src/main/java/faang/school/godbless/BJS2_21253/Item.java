f552d3a1985910950a1e9e8a9615407914881492
avoevoda <voevodacode@gmail.com>
2024-08-16 15:51:11 +0300

    Lord of the Rings RPG

---
+++ src/main/java/faang/school/godbless/BJS2_21253/Item.java
@@ -0,0 +1,16 @@
+package faang.school.godbless.BJS2_21253;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@AllArgsConstructor
+@ToString
+public class Item {
+    private String name;
+
+    @Setter
+    private int value;
+}
