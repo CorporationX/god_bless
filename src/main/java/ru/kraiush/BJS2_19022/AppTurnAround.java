9987484e223f43bd7e1fd84fe13f32778c1e4c3b
Alexander Kraiushkin <akraiushkin@gmail.com>
2024-08-16 12:25:16 +0300

    kraiush: update

--- src/main/java/ru/kraiush/BJS2_19022/AppTurnAround.java
+++ src/main/java/ru/kraiush/BJS2_19022/AppTurnAround.java
@@ -3,7 +3,7 @@ package ru.kraiush.BJS2_19022;
 import java.util.Arrays;
 import java.util.Collections;
 
-public class App_Example {
+public class AppTurnAround {
 
     public static void main(String[] args) {
 
