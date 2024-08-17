54a6184b7c5e7c6ea78b80078874f05773bd27a9
Alena Guskova <amguskova@edu.hse.ru>
2024-08-18 01:39:37 +0300

    BJS2-20459 Вспомним школу (Добавлены исправления после ревью)

--- src/main/java/faang/school/godbless/area/CountTriangleArea.java
+++ src/main/java/faang/school/godbless/area/CountTriangleArea.java
@@ -4,15 +4,15 @@ import java.util.function.Function;
 
 public class CountTriangleArea {
     Double triangleArea(double a, double b, double c) throws IllegalArgumentException {
-        Function<Double, Function<Double, Double>> sum = (x) -> y -> x + y;
-        Function<Double, Function<Double, Double>> mul = (x) -> y -> x * y;
-        Function<Double, Function<Double, Double>> sub = (x) -> y -> x - y;
-        Function<Double, Function<Double, Double>> div = (x) -> y -> x / y;
-        Function<Double, Double> sqrt = (x) -> Math.sqrt(x);
-
         if (!(a + b < c) && !(b + c < a) && !(c + a < b)) {
+            Function<Double, Function<Double, Double>> sum = (x) -> y -> x + y;
+            Function<Double, Function<Double, Double>> mul = (x) -> y -> x * y;
+            Function<Double, Function<Double, Double>> sub = (x) -> y -> x - y;
+            Function<Double, Function<Double, Double>> div = (x) -> y -> x / y;
+            Function<Double, Double> sqrt = (x) -> Math.sqrt(x);
+
             double p = div.apply(sum.apply(sum.apply(a).apply(b)).apply(c))
-                    .apply((double)2);
+                    .apply(2.0);
 
             double pa = sub.apply(p).apply(a);
             double pb = sub.apply(p).apply(b);
