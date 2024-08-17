c82fb4369b3783caf03a90b7916f610efc4db47f
mamadjonovKO <mamadjonov_k@auca.kg>
2024-08-17 19:53:42 +0300

    insta filters done

---
+++ src/main/java/faang/school/godbless/BJS2_21903/FilterProcessor.java
@@ -0,0 +1,16 @@
+package faang.school.godbless.BJS2_21903;
+
+import java.util.function.Function;
+
+public class FilterProcessor {
+    public Image applyFilter(Image image, Function<Image, Image> filterFunction) {
+        return filterFunction.apply(image);
+    }
+
+    public Function<Image, Image> combineFilters(Function<Image, Image> firstFilterFunction,
+                                Function<Image, Image> secondFilterFunction) {
+        return (image) -> { firstFilterFunction.apply(image);
+            secondFilterFunction.apply(image);
+            return image; };
+    }
+}
