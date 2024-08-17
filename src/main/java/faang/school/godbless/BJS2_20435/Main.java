4b1acc7896fc3184a90bc5e4694f59d7cbd1fb08
Denis Artamonov <dns.artamonov@gmail.com>
2024-08-17 21:14:34 +0300

    BJS2-20435 bug fixes

--- src/main/java/faang/school/godbless/BJS2_20435/Main.java
+++ src/main/java/faang/school/godbless/BJS2_20435/Main.java
@@ -4,16 +4,19 @@ import java.util.function.Function;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println(triangleArea(8, 4, 5));
         System.out.println(triangleArea(5, 5, 5));
+        System.out.println(triangleArea(8, 4, 14));
     }
 
-    public static Double triangleArea(double a, double b, double c) throws IllegalArgumentException {
+    public static Double triangleArea(double a, double b, double c) {
+        if (a + b <= c || a + c <= b || b + c <= a) {
+            throw new IllegalArgumentException("There can be no triangle with the given sides");
+        }
         Function<Double, Function<Double, Double>> sum = (x) -> y -> x + y;
         Function<Double, Function<Double, Double>> mul = (x) -> y -> x * y;
         Function<Double, Function<Double, Double>> sub = (x) -> y -> x - y;
         Function<Double, Function<Double, Double>> div = (x) -> y -> x / y;
-        Function<Double, Double> sqrt = (x) -> Math.sqrt(x);
+        Function<Double, Double> sqrt = (x)->Math.sqrt(x);
 
         double halfPerimeter = div.apply(sum.apply(sum.apply(a).apply(b)).apply(c)).apply(2.0);
 
