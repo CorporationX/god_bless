aaf2921c752ba4d04d4e74fc05d2e3c6e9c0e689
soulasphyxia <maslyuk03@bk.ru>
2024-08-17 09:52:44 +0700

    Исправил замечания по коду

--- src/main/java/ubers_billions/FareCalculator.java
+++ src/main/java/ubers_billions/FareCalculator.java
@@ -5,8 +5,8 @@ import java.util.function.BiFunction;
 public class FareCalculator {
 
     public double calculateFare(double distance,
-                              double time,
-                              BiFunction<Double, Double, Double> fare) {
+                                double time,
+                                BiFunction<Double, Double, Double> fare) {
         return fare.apply(distance, time);
     }
 }
