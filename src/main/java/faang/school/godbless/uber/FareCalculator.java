616d89b2dac27f14ef7d6af0f0012e37421ff547
Ilya Merkulov <imerkulov@fun.co>
2024-08-17 20:27:52 +0300

    Implement Task 28: Uber Millions

---
+++ src/main/java/faang/school/godbless/uber/FareCalculator.java
@@ -0,0 +1,9 @@
+package faang.school.godbless.uber;
+
+import java.util.function.BiFunction;
+
+public class FareCalculator {
+    public double calculateFare(double distance, double time, BiFunction<Double, Double, Double> fare) {
+        return fare.apply(distance, time);
+    }
+}
