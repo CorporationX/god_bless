84967c9aac23b34bb8018f8f41fb8657ddd74358
Garib_Aghayev <garibaghayev@gmail.com>
2024-08-18 00:12:36 +0300

    first release

--- src/main/java/faang/school/godbless/Maps/BJS2_20011/Main.java
+++ src/main/java/faang/school/godbless/Maps/BJS2_20011/Main.java
@@ -1,14 +1,66 @@
 package faang.school.godbless.Maps.BJS2_20011;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
 public class Main {
 
-    private static Map<User, List<Query>> queryList = new HashMap<>();
-    public static void main(String[] args) {
+    private static Map<User, List<Query>> queryMap = new HashMap<>();
 
+    public static void main(String[] args) throws InterruptedException {
+
+        Query third = new Query("third");
+        Thread.sleep(500);
+        Query first = new Query("first");
+        Thread.sleep(500);
+        Query fourth = new Query("fourth");
+        Thread.sleep(500);
+        Query second = new Query("second");
+
+        User tom = new User("Tom");
+        addQuery(tom, fourth);
+        addQuery(tom, first);
+        addQuery(tom, second);
+        addQuery(tom, third);
+
+        User bob = new User("Bob");
+        List<Query> list = new ArrayList<>();
+        list.add(first);
+        list.add(second);
+        list.add(third);
+        list.add(fourth);
+        addQuery(bob, list);
+
+        printAllUsersQueries();
     }
 
+    public static void addQuery(User user, Query query) {
+        queryMap.put(user, user.addQuery(query));
+    }
+
+    public static void addQuery(User user, List<Query> list) {
+        queryMap.put(user, user.addQuery(list));
+    }
+
+    public static void removeSubject(User user) {
+        queryMap.remove(user);
+    }
+
+    public static void printUserQueries(User user) {
+        System.out.println("#" + user.getId() + ":" + user.getName());
+        for (Query query : user.getSortedQuryList()) {
+            System.out.println(query.getId() + ":" + query.getContent() + "::" + query.getTimestamp());
+        }
+    }
+
+    public static void printAllUsersQueries() {
+        for (Map.Entry<User, List<Query>> entry : queryMap.entrySet()) {
+            System.out.println("=======");
+            System.out.println(entry.getKey().getName());
+            printUserQueries(entry.getKey());
+            System.out.println("=======");
+        }
+    }
 }
--- src/main/java/faang/school/godbless/Maps/BJS2_20011/User.java
+++ src/main/java/faang/school/godbless/Maps/BJS2_20011/User.java
@@ -1,17 +1,22 @@
 package faang.school.godbless.Maps.BJS2_20011;
 
 import lombok.EqualsAndHashCode;
+import lombok.Getter;
 
 import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
 import java.util.List;
 import java.util.concurrent.ThreadLocalRandom;
 
 @EqualsAndHashCode
+@Getter
 public class User {
     private final int id;
     private String name;
     @EqualsAndHashCode.Exclude
-    private List<Query> queries;
+    private List<Query> queries = new ArrayList<>();
 
     public User(String name) {
         this.name = name;
@@ -28,9 +33,13 @@ public class User {
         return queries;
     }
 
-    public void printQueries() {
-        for (Query query : queries) {
-            System.out.println(query.getId() + ":" + query.getContent());
-        }
+//    public void printQueries() {
+//        for (Query query : queries) {
+//            System.out.println(query.getId() + ":" + query.getContent());
+//        }
+//    }
+    public List<Query> getSortedQuryList(){
+        Collections.sort(queries, (a, b) -> a.getTimestamp().compareTo(b.getTimestamp()));
+        return queries;
     }
 }
