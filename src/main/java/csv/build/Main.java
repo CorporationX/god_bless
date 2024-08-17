8db14d1171c119eb5275fb2d63a6ecc7c4b4978b
SharkB38 <val.wolf.ne@gmail.com>
2024-08-17 17:20:21 +0900

    fix

--- src/main/java/csv/build/Main.java
+++ src/main/java/csv/build/Main.java
@@ -1,6 +1,9 @@
 package csv.build;
 
+import lombok.NonNull;
+
 import java.util.List;
+import java.util.Objects;
 import java.util.stream.Collectors;
 
 public class Main {
@@ -17,12 +20,27 @@ public class Main {
         System.out.println(result);
     }
 
-    public static String toCsv(List<List<String>> table) throws IllegalArgumentException {
+    public static String toCsv(@NonNull List<List<String>> table) throws IllegalArgumentException {
+        validateMatrix(table);
         VectorJoiner<String> vectorJoiner = (vector) -> String.join(", ", vector);
         MatrixJoiner<String> matrixJoiner = (matrix) -> matrix.stream()
                 .map(vectorJoiner::join)
                 .collect(Collectors.joining("\n"));
 
         return matrixJoiner.join(table);
+    }
+
+    private static void validateMatrix(@NonNull List<List<String>> table) {
+        if (table.isEmpty()) {
+            throw new IllegalArgumentException("Matrix is empty");
+        }
+        if (table.stream()
+                .anyMatch(Objects::isNull)) {
+            throw new NullPointerException("Row of the matrix is null");
+        }
+        if (table.stream()
+                .anyMatch(List::isEmpty)) {
+            throw new IllegalArgumentException("Row of the matrix is empty");
+        }
     }
 }
