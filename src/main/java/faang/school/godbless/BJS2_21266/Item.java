d43d35470029df345e6e3e41938d23d4fb735f53
Ruslan Iovlev <ruslan-iovlev@mail.ru>
2024-08-17 23:28:37 +0700

    Lord of the Rings RPG

---
+++ src/main/java/faang/school/godbless/BJS2_21266/Item.java
@@ -0,0 +1,13 @@
+package faang.school.godbless.BJS2_21266;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@AllArgsConstructor
+public class Item {
+    private String name;
+    @Setter
+    private int value;
+}
