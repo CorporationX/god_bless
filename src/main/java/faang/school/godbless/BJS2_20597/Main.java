e9e5b13128c66aec7de606bd463badc31199cee4
Prigozhaeva-Anastasia <nastaprigozaeva20@gmail.com>
2024-08-17 09:30:45 +0300

    Add validation

--- src/main/java/faang/school/godbless/BJS2_20597/Main.java
+++ src/main/java/faang/school/godbless/BJS2_20597/Main.java
@@ -7,10 +7,13 @@ public class Main {
         double a = 10;
         double b = 5;
         double c = 8;
-        System.out.println("Triangle (" + a + ", " + b + ", " + c + ") area is " + triangleArea(10, 5, 8));
+        System.out.println("Triangle (" + a + ", " + b + ", " + c + ") area is " + triangleArea(a, b, c));
     }
 
     public static Double triangleArea(double a, double b, double c) throws IllegalArgumentException {
+        if (a <= 0 || b <= 0 || c <= 0 || !isValidTriangle(a, b, c)) {
+            throw new IllegalArgumentException("Invalid triangle sides");
+        }
         Function<Double, Function<Double, Double>> sum = (x) -> y -> x + y;
         Function<Double, Function<Double, Double>> mul = (x) -> y -> x * y;
         Function<Double, Function<Double, Double>> sub = (x) -> y -> x - y;
@@ -28,5 +31,9 @@ public class Main {
         double pb_pcMul = mul.apply(pbSub).apply(pcSub);
         double resultMul = mul.apply(p_paMul).apply(pb_pcMul);
         return sqrt.apply(resultMul);
+    }
+
+    private static boolean isValidTriangle(double a, double b, double c) {
+        return (a + b > c) && (a + c > b) && (b + c > a);
     }
 }
