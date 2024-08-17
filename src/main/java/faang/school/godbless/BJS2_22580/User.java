e6a67cc4389674a287b7a06c47c2411cf27ad971
Robert Oguzov <robertoguzov2@gmail.com>
2024-08-17 09:31:19 +0300

    Написал класс User

---
+++ src/main/java/faang/school/godbless/BJS2_22580/User.java
@@ -0,0 +1,11 @@
+package faang.school.godbless.BJS2_22580;
+
+import lombok.AllArgsConstructor;
+import lombok.ToString;
+
+@ToString
+@AllArgsConstructor
+public class User {
+    private int id;
+    private String name;
+}
