96d2c4a8ca69c522cbbff31e6e4f89bce1c35251
Denis Artamonov <dns.artamonov@gmail.com>
2024-08-19 00:02:15 +0300

    BJS2-23930 complete

---
+++ src/main/java/faang/school/godbless/BJS2_23930/GriffinsFoodDelivery.java
@@ -0,0 +1,25 @@
+package faang.school.godbless.BJS2_23930;
+
+import java.util.Arrays;
+import java.util.Random;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
+
+public class GriffinsFoodDelivery {
+    public static void main(String[] args) {
+        String[] characterNames = {"Peter", "Lois", "Meg", "Chris", "Stewie"};
+        ExecutorService executor = Executors.newFixedThreadPool(3);
+        Arrays.stream(characterNames)
+                .forEach(character -> executor.submit(new FoodDeliveryTask(character, new Random().nextInt(1, 10))));
+        try {
+            executor.shutdown();
+            if (!executor.awaitTermination(60, TimeUnit.SECONDS)) {
+                executor.shutdownNow();
+            }
+        } catch (InterruptedException e) {
+            executor.shutdownNow();
+            Thread.currentThread().interrupt();
+        }
+    }
+}
