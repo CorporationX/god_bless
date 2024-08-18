408bcf9404c45a8148e88fc944c8520a95589dd0
Дмитрий Бахтин <dmitridorje@gmail.com>
2024-08-18 17:22:33 +0300

    Уточнён перечень случаев для исключения, добавлена проверка невалидных данных

--- src/main/java/faang/school/godbless/secondsprint/csv/CsvConverter.java
+++ src/main/java/faang/school/godbless/secondsprint/csv/CsvConverter.java
@@ -1,19 +1,18 @@
 package faang.school.godbless.secondsprint.csv;
 
+import java.util.Collection;
 import java.util.List;
 import java.util.stream.Collectors;
 
 public class CsvConverter {
     public static String toCsv(List<List<String>> table) throws IllegalArgumentException {
-        if (table == null || table.isEmpty() ||
-        table.stream().anyMatch(row -> row.isEmpty())
-        || table.stream().flatMap(row -> row.stream()).anyMatch(el -> el.isEmpty())) {
+        if (table == null || table.isEmpty() || table.stream().anyMatch(List::isEmpty)
+                || table.stream().flatMap(Collection::stream).anyMatch(String::isEmpty)) {
             throw new IllegalArgumentException("Table cannot be null or empty and" +
-                    "must not contain empty rows");
+                    " must not contain empty rows or values");
         }
 
-        VectorJoiner<String> vectorJoiner = vector -> vector.stream().collect(Collectors.joining(","));
-
+        VectorJoiner<String> vectorJoiner = vector -> String.join(",", vector);
         MatrixJoiner<String> matrixJoiner = matrix -> matrix.stream().
                 map(vectorJoiner::join).collect(Collectors.joining("\n"));
 
@@ -21,8 +20,7 @@ public class CsvConverter {
     }
 
     public static void main(String[] args) {
-
-        String test = toCsv(
+        String testValidTable = toCsv(
                 List.of(
                         List.of("1", "2", "3", "4", "5"),
                         List.of("6", "7", "8", "9", "10"),
@@ -32,6 +30,16 @@ public class CsvConverter {
                 )
         );
 
-        System.out.println(test);
+        System.out.println(testValidTable);
+
+        String testInvalidTable = toCsv(
+                List.of(
+                        List.of("1", "2", "3"),
+                        List.of("4", "5", "6"),
+                        List.of("7", "8", "")
+                )
+        );
+
+        System.out.println(testInvalidTable);
     }
 }
--- src/main/java/faang/school/godbless/secondsprint/csv/VectorJoiner.java
+++ src/main/java/faang/school/godbless/secondsprint/csv/VectorJoiner.java
@@ -4,6 +4,5 @@ import java.util.List;
 
 @FunctionalInterface
 public interface VectorJoiner<T> {
-    // Соединяет список элементов типа T в один элемент типа T
     T join(List<T> vector);
 }
