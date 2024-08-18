0f373f337f8b1576a1c49555b99db0fc8888ff5f
SharkB38 <val.wolf.ne@gmail.com>
2024-08-19 07:00:34 +0900

    fix

--- src/main/java/stream/training1/Main.java
+++ src/main/java/stream/training1/Main.java
@@ -8,20 +8,20 @@ import java.util.function.Predicate;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println(getSumOfOddNumbers(List.of(1, 2, 3, 4, 5, 6)));
+        System.out.println(getSumOfEvenNumbers(List.of(1, 2, 3, 4, 5, 6)));
         System.out.println(getMax(List.of(1, 2, 3, 4, 5, 6)));
-        System.out.println(getAverrage(List.of(1, 2, 3, 4, 5, 6)));
-        System.out.println(getStringCountOnCondition(List.of("abc", "abd", "dbf"), "ab"));
-        System.out.println(getFilteredListOfString(List.of("abc", "abd", "dbf"), "f"));
+        System.out.println(getAverage(List.of(1, 2, 3, 4, 5, 6)));
+        System.out.println(getStringsStartsWithCount(List.of("abc", "abd", "dbf"), "ab"));
+        System.out.println(getFilteredListOfStringsContainsSubstring(List.of("abc", "abd", "dbf"), "f"));
         System.out.println(getSortedByStringLengthList(List.of("abababa", "asfas", "11", "asf")));
         System.out.println(isAllElementsMatchCondition(List.of(2.5, 3.4, 5.4, 4.0), e -> e > 2));
-        System.out.println(getMinFromBiggerThanNum(List.of(1, 2, 3, 4, 5, 6), 3));
+        System.out.println(getMinElementGreaterThan(List.of(1, 2, 3, 4, 5, 6), 3));
         System.out.println(getStringLengths(List.of("abababa", "asfas", "11", "asf")));
     }
 
-    public static int getSumOfOddNumbers(@NonNull List<Integer> nums) {
+    public static int getSumOfEvenNumbers(@NonNull List<Integer> nums) {
         return nums.stream()
-                .filter(num -> num % 2 != 0)
+                .filter(num -> num % 2 == 0)
                 .mapToInt(Integer::intValue)
                 .sum();
     }
@@ -32,20 +32,20 @@ public class Main {
                 .orElseThrow();
     }
 
-    public static double getAverrage(@NonNull List<Integer> nums) {
+    public static double getAverage(@NonNull List<Integer> nums) {
         return nums.stream()
                 .mapToInt(Integer::intValue)
                 .average()
                 .orElseThrow();
     }
 
-    public static long getStringCountOnCondition(@NonNull List<String> lines, @NonNull String pattern) {
+    public static long getStringsStartsWithCount(@NonNull List<String> lines, @NonNull String pattern) {
         return lines.stream()
                 .filter(line -> line.startsWith(pattern))
                 .count();
     }
 
-    public static List<String> getFilteredListOfString(@NonNull List<String> lines, @NonNull String substring) {
+    public static List<String> getFilteredListOfStringsContainsSubstring(@NonNull List<String> lines, @NonNull String substring) {
         return lines.stream()
                 .filter(line -> line.contains(substring))
                 .toList();
@@ -62,7 +62,7 @@ public class Main {
                 .allMatch(condition);
     }
 
-    public static int getMinFromBiggerThanNum(@NonNull List<Integer> nums, int min) {
+    public static int getMinElementGreaterThan(@NonNull List<Integer> nums, int min) {
         return nums.stream()
                 .filter(num -> num > min)
                 .min(Integer::compareTo)
