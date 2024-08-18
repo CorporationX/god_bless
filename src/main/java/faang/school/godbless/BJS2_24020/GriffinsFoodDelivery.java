dd247e4d614e3f40f34ddc52dc6c262b24005c74
Robert Oguzov <robertoguzov2@gmail.com>
2024-08-18 13:41:09 +0300

    GriffinsFoodDelivery

---
+++ src/main/java/faang/school/godbless/BJS2_24020/GriffinsFoodDelivery.java
@@ -0,0 +1,17 @@
+package faang.school.godbless.BJS2_24020;
+
+import java.util.Arrays;
+import java.util.Random;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+public class GriffinsFoodDelivery {
+    public static void main(String[] args) {
+        Random random = new Random();
+        String[] characterNames = {"Peter", "Lois", "Meg", "Chris", "Stewie"};
+        ExecutorService executor = Executors.newFixedThreadPool(3);
+        Arrays.stream(characterNames)
+                .forEach(name -> executor.execute(new FoodDeliveryTask(name, random.nextInt(1, 50))));
+        executor.shutdown();
+    }
+}
