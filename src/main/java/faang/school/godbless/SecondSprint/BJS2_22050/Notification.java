ffcce6089c14127d9e132fd7be6d7a45626ea14a
Garib_Aghayev <garibaghayev@gmail.com>
2024-08-18 07:04:56 +0300

    first release

---
+++ src/main/java/faang/school/godbless/SecondSprint/BJS2_22050/Notification.java
@@ -0,0 +1,11 @@
+package faang.school.godbless.SecondSprint.BJS2_22050;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class Notification {
+    private String type;
+    private String message;
+}
