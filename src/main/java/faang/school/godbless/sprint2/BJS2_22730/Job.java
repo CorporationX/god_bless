63ba40a594d63da9df87e5629a20ee6bcd82b275
Denys Dovgan <ytcsgoatlas@gmail.com>
2024-08-18 17:34:48 +0200

    task started

---
+++ src/main/java/faang/school/godbless/sprint2/BJS2_22730/Job.java
@@ -0,0 +1,12 @@
+package faang.school.godbless.sprint2.BJS2_22730;
+
+import java.time.LocalDate;
+import java.util.List;
+
+public class Job {
+    private String jobTitle;
+    private List<String> requirements;
+    private int proposedSalary;
+    private String location;
+    private LocalDate vacancyPublishingDate;
+}
---
+++ src/main/java/faang/school/godbless/sprint2/BJS2_22730/JobScraper.java
@@ -0,0 +1,11 @@
+package faang.school.godbless.sprint2.BJS2_22730;
+
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+public class JobScraper {
+
+    public Job jsonToJobConverter(String json) {
+        
+    }
+}
