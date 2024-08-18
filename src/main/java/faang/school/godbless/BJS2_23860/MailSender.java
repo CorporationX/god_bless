f0f8baebd06904b3342047b9f7ceb8f079818aac
Robert Oguzov <robertoguzov2@gmail.com>
2024-08-18 12:31:37 +0300

    Написал класс MailSender

---
+++ src/main/java/faang/school/godbless/BJS2_23860/MailSender.java
@@ -0,0 +1,23 @@
+package faang.school.godbless.BJS2_23860;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
+
+public class MailSender {
+    public static void main(String[] args) {
+        ExecutorService executor = Executors.newFixedThreadPool(5);
+        for (int j = 0, i = 1; j < 1000; j+=200, i++) {
+            executor.execute(new SenderRunnable(i, j, j + 200));
+        }
+
+        executor.shutdown();
+        try {
+            executor.awaitTermination(1, TimeUnit.SECONDS);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+
+        System.out.println("Успешно отправлены все письма!");
+    }
+}
