2063c096123e9881a48a9d5077f62ae59acca9d7
Maksim Kononov <07maks7@mail.ru>
2024-08-18 16:27:40 +0300

    Внес изменения в код.

--- src/main/java/lru/cache/Main.java
+++ src/main/java/lru/cache/Main.java
@@ -4,24 +4,22 @@ import java.time.LocalDateTime;
 import java.util.HashMap;
 import java.util.LinkedHashMap;
 import java.util.Map;
-import java.util.Random;
 import java.util.Scanner;
 
 public class Main {
-    private static final Random RANDOM = new Random();
+    private static int ID = 1;
     private static final Scanner SCANNER = new Scanner(System.in);
-    private static Map<Integer, Data> cache = new HashMap<>();
+    private final static Map<Integer, Data> dataBase = new HashMap<>();
     private final static int CACHE_SIZE = 3;
-    private final static int MAX_RANDOM_ID_FOR_DATA = 150;
     private final static String START_MESSAGE = "Список доступных команд: \n" +
             "'print base' - вывод базы данных\n" +
             "'get data' - получить данные запроса по ID\n" +
             "'stop' - остановить работу программы.";
 
-    private static Map<Integer, Data> dataBase = new LinkedHashMap<>(CACHE_SIZE) {
+    private final static Map<Integer, Data> cache = new LinkedHashMap<>(CACHE_SIZE) {
         @Override
         protected boolean removeEldestEntry(Map.Entry<Integer, Data> eldest) {
-            if (dataBase.size() > CACHE_SIZE) {
+            if (cache.size() > CACHE_SIZE) {
                 System.out.println("Удален старый элемент");
                 return true;
             }
@@ -30,7 +28,6 @@ public class Main {
     };
 
     public static void main(String[] args) {
-        boolean flagToStop = false;
 
         System.out.println(START_MESSAGE);
 
@@ -38,14 +35,14 @@ public class Main {
             System.out.println("Введите свой запрос: ");
             String input = SCANNER.nextLine();
 
-            if (input.equals("print base")) {
-                printDataBase(dataBase);
+            if (input.equals("print cache")) {
+                printCache(cache);
                 continue;
             }
             if (input.equals("get data")) {
                 System.out.println("Введите ID");
                 int id = Integer.parseInt(SCANNER.nextLine());
-                getDataFromCache(id);
+                getData(id);
                 continue;
             }
             if (input.equals("stop")) {
@@ -61,38 +58,33 @@ public class Main {
         System.out.println("Запрос добавлен в базу данных.");
     }
 
-    public static void getDataFromCache(int id) {
+    public static void getData(int id) {
         if (cache.containsKey(id)) {
+            cache.get(id).setTimestamp(LocalDateTime.now());
             System.out.println(cache.get(id));
-            System.out.println("Данные полученны из базы и добавлены в кэш.");
-            dataBase.get(id).setTimestamp(LocalDateTime.now());
+            System.out.println("Данные полученны кэша.");
         } else {
             if (dataBase.containsKey(id)) {
+                dataBase.get(id).setTimestamp(LocalDateTime.now());
                 System.out.println("Данные полученны из базы и добавлены в кэш.");
                 cache.put(id, dataBase.get(id));
                 System.out.println(cache.get(id));
-                System.out.println("Данные полученны из базы и добавлены в кэш.");
-                dataBase.get(id).setTimestamp(LocalDateTime.now());
+                System.out.println("Данные полученны из кэша.");
+            } else {
+                throw new IllegalArgumentException("Данных нет");
             }
         }
     }
 
-    public static void printDataBase(Map<Integer, Data> dataMap) {
-        for (Map.Entry<Integer, Data> entry : dataMap.entrySet()) {
+    public static void printCache(Map<Integer, Data> cache) {
+        for (Map.Entry<Integer, Data> entry : cache.entrySet()) {
             System.out.println(entry.getKey() + ": " + entry.getValue());
         }
     }
 
-    public static int generateDataId() {
-        return RANDOM.nextInt(MAX_RANDOM_ID_FOR_DATA) + 1;
-    }
 
     public static Data createData(String value) {
-        int id = generateDataId();
-        if (dataBase.containsKey(id)) {
-            id = +generateDataId();
-        }
-        return new Data(id, value);
+        return new Data(ID++, value);
     }
 
 }
