7b9c9dfbcf48cef657dfa70e13c816b2aec499e4
Murzin34 <108129795+Murzin34@users.noreply.github.com>
2024-08-19 02:37:28 +0300

    Фикс. Поправил main.

--- src/main/java/faang/school/godbless/modul3/tournament/King.java
+++ src/main/java/faang/school/godbless/modul3/tournament/King.java
@@ -1,19 +1,25 @@
 package faang.school.godbless.modul3.tournament;
 
 import java.util.ArrayList;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 public class King {
     public static void main(String[] args) {
+        ExecutorService executorService = Executors.newFixedThreadPool(2);
+
         Knight gregorClegane = new Knight("Gregor Clegane", new ArrayList<>());
         System.out.println(gregorClegane.getName() + " trials begin!");
         gregorClegane.addTrial(new Trial(gregorClegane.getName(), "Trial of Strength"));
         gregorClegane.addTrial(new Trial(gregorClegane.getName(), "Trial of Endurance"));
-        gregorClegane.startTrials();
 
         Knight oberynMartell = new Knight("Oberyn Martell", new ArrayList<>());
-        System.out.println("\n" + oberynMartell.getName() + " trials begin!");
+        System.out.println(oberynMartell.getName() + " trials begin!");
         oberynMartell.addTrial(new Trial(oberynMartell.getName(), "Trial of Agility"));
         oberynMartell.addTrial(new Trial(oberynMartell.getName(), "Trial of Durability"));
-        oberynMartell.startTrials();
+
+        executorService.submit(gregorClegane::startTrials);
+        executorService.submit(oberynMartell::startTrials);
+        executorService.shutdown();
     }
 }
