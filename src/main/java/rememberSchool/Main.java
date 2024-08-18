64e310213e2008420b763202215db2ce0fe40772
Ivan <spvanym@gmail.com>
2024-08-18 12:49:30 +0300

    Вспомним школу SMI

---
+++ src/main/java/rememberSchool/Main.java
@@ -0,0 +1,23 @@
+package rememberSchool;
+
+import java.util.function.Function;
+
+public class Main{
+    public static void main(String[] args) {
+        System.out.println(triangleArea(8.34,3.34,7.23));
+    }
+
+    public static Double triangleArea(double a, double b, double c) throws IllegalArgumentException {
+        Function<Double, Function<Double, Double>> sum = (x) -> y -> x + y;
+        Function<Double, Function<Double, Double>> mul = (x) -> y -> x * y;
+        Function<Double, Function<Double, Double>> sub = (x) -> y -> x - y;
+        Function<Double, Function<Double, Double>> div = (x) -> y -> x / y;
+        Function<Double, Double> sqrt = (x) -> Math.sqrt(x);
+
+        double result = 0;
+        result =  mul.apply(a).apply(mul.apply(b).apply(c));
+        return result;
+    }
+
+
+}
