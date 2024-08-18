bcb2d4a2736945547c70b6ef3020c8b92aa167b4
Alexander Kraiushkin <akraiushkin@gmail.com>
2024-08-18 17:33:18 +0300

    BJS2-18571: user registration

---
+++ src/main/java/ru/kraiush/BJS2_18571/AppUserRegistration.java
@@ -0,0 +1,29 @@
+package ru.kraiush.BJS2_18571;
+
+import ru.kraiush.model.UserRegistration;
+
+import java.util.Collections;
+
+public class AppUserRegistration {
+
+    public static void main(String[] args) {
+
+        System.out.print(String.join("", Collections.nCopies(80, "-")));
+
+        try {
+            UserRegistration user = new UserRegistration(null, "cada", "bra");
+            System.out.println("\nUser registration: " + user);
+        } catch (IllegalArgumentException e) {
+            System.out.println("\nValidation error: " + e.getMessage());
+        }
+
+        System.out.print(String.join("", Collections.nCopies(80, "-")));
+
+        try {
+            UserRegistration user = new UserRegistration("spark", "\"Google", "Amsterdam");
+            System.out.println("\nUser registration: " + user);
+        } catch (IllegalArgumentException e) {
+            System.out.println("\nValidation error: " + e.getMessage());
+        }
+    }
+}
