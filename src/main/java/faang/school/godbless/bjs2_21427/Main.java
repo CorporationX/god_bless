2b0fde71898ce2a33079b880c83fbd5618528371
Alexander Milosh <milosh.alexandr.by@yandex.by>
2024-08-19 01:44:07 +0300

    BJS2-21427 Code review

--- src/main/java/faang/school/godbless/bjs2_21427/Main.java
+++ src/main/java/faang/school/godbless/bjs2_21427/Main.java
@@ -4,29 +4,20 @@ import java.util.List;
 
 public class Main {
     public static int calculate(List<Integer> nums, Calculator calculator) throws IllegalArgumentException {
-        if (nums == null || nums.size() < 2) {
-            throw new IllegalArgumentException();
+        if (nums == null) {
+            throw new IllegalArgumentException("List of numbers cannot be null");
         }
 
-        int result = nums.get(0);
-        int last = nums.size() - 1;
-        for (int i = 1; i <= last; i++) {
-            result = calculator.execute(result, nums.get(i));
-        }
-        return result;
+        return nums.stream()
+                .reduce(calculator::execute)
+                .orElseThrow(() -> new IllegalArgumentException("List of numbers cannot be empty"));
     }
 
     public static int product(List<Integer> nums) {
-        if (nums.size() == 1) {
-            return nums.get(0);
-        }
         return calculate(nums, (a, b) -> a * b);
     }
 
     public static int sum(List<Integer> nums) {
-        if (nums.size() == 1) {
-            return nums.get(0);
-        }
         return calculate(nums, Integer::sum);
     }
 }
--- src/test/java/faang/school/godbless/bjs2_21427/MainTest.java
+++ src/test/java/faang/school/godbless/bjs2_21427/MainTest.java
@@ -5,25 +5,17 @@ import org.junit.jupiter.api.Test;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 class MainTest {
     @Test
-    void testCalculate() {
-        var result1 = Main.calculate(List.of(1, 2), (a, b) -> a * b);
-        assertEquals(2, result1);
-
-        var result2 = Main.calculate(List.of(1, 2, 3), (a, b) -> a * b);
-        assertEquals(6, result2);
-
-        var result3 = Main.calculate(List.of(1, 2), Integer::sum);
-        assertEquals(3, result3);
-
-        var result4 = Main.calculate(List.of(1, 2, 3), Integer::sum);
-        assertEquals(6, result4);
-    }
-
-    @Test
     void testProduct() {
+        Exception nullException = assertThrows(IllegalArgumentException.class, () -> Main.sum(null));
+        assertEquals("List of numbers cannot be null", nullException.getMessage());
+
+        Exception emptyException = assertThrows(IllegalArgumentException.class, () -> Main.sum(List.of()));
+        assertEquals("List of numbers cannot be empty", emptyException.getMessage());
+
         var result0 = Main.product(List.of(1));
         assertEquals(1, result0);
 
@@ -36,6 +28,12 @@ class MainTest {
 
     @Test
     void testSum() {
+        Exception nullException = assertThrows(IllegalArgumentException.class, () -> Main.sum(null));
+        assertEquals("List of numbers cannot be null", nullException.getMessage());
+
+        Exception emptyException = assertThrows(IllegalArgumentException.class, () -> Main.sum(List.of()));
+        assertEquals("List of numbers cannot be empty", emptyException.getMessage());
+
         var result0 = Main.sum(List.of(1));
         assertEquals(1, result0);
 
