d29a36c9180e033aa90c8ef4e022e07fd5f281d1
Denis Panasenko <panasenkodenis1995@gmail.com>
2024-08-18 22:54:05 +0300

    BJS2-23433

---
+++ src/main/java/faang/school/godbless/BJS2_23433/Trial.java
@@ -0,0 +1,21 @@
+package faang.school.godbless.BJS2_23433;
+
+import lombok.AllArgsConstructor;
+
+@AllArgsConstructor
+public class Trial implements Runnable {
+    private String knightName;
+    private String trialName;
+
+    @Override
+    public void run() {
+
+        try {
+            System.out.printf("Trial %s has started by %s%n", trialName, knightName);
+            Thread.sleep(3000);
+            System.out.printf("Trial %s was ended by %s%n", trialName, knightName);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
