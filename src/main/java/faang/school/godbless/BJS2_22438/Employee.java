6fd554ece487768ddb7ce45e086ecc83722835d1
Peregruzochka <peregruzochka48@mail.ru>
2024-08-18 12:24:09 +0300

    тренировка stream api

---
+++ src/main/java/faang/school/godbless/BJS2_22438/Employee.java
@@ -0,0 +1,12 @@
+package faang.school.godbless.BJS2_22438;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@AllArgsConstructor
+@Getter
+public class Employee {
+    private String name;
+    private double salary;
+    private String department;
+}
