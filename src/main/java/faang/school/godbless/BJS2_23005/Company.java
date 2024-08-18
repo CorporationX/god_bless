0446afe1821ec6966e4ff5b1cfff86f6b60e47d2
Robert Oguzov <robertoguzov2@gmail.com>
2024-08-18 11:57:37 +0300

    Написал класс Company

---
+++ src/main/java/faang/school/godbless/BJS2_23005/Company.java
@@ -0,0 +1,10 @@
+package faang.school.godbless.BJS2_23005;
+
+import lombok.AllArgsConstructor;
+
+@AllArgsConstructor
+public class Company {
+    private int id;
+    private String name;
+    private int totalEmployees;
+}
