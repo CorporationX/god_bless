503cb86cfc17bd6de780586f93bcb2b8faa72d7a
Maksim Kononov <07maks7@mail.ru>
2024-08-18 22:30:29 +0300

    Убрал путаницу с true/false. true - попало под фильтр, false - прошло.

--- src/main/java/filter/spam/MessageProcessor.java
+++ src/main/java/filter/spam/MessageProcessor.java
@@ -6,9 +6,9 @@ public class MessageProcessor {
     public boolean processMessage(String message, List<MessageFilter> filterList) {
         for (MessageFilter filter : filterList) {
             if (!filter.filter(message)) {
-                return false;
+                return true;
             }
         }
-        return true;
+        return false;
     }
 }
