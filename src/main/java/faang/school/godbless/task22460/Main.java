6dc9bc1f3381f528152f2714dcb9cc962e3163f5
Chibix <chibisovanastena@gmail.com>
2024-08-18 19:14:12 +0300

    BJS2-22460 - Потренируем StreamAPI 2

---
+++ src/main/java/faang/school/godbless/task22460/Main.java
@@ -0,0 +1,24 @@
+package faang.school.godbless.task22460;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) {
+        Map<String, String> country = new HashMap<>();
+        country.put("Россия", "Москва");
+        country.put("Франция", "Париж");
+        country.put("Австралия", "Канберра");
+        Transformation.getCapital(country);
+
+        List<String> word = new ArrayList<>();
+        word.add("Австралия");
+        word.add("Алина");
+        word.add("акула");
+        word.add("мост");
+        System.out.println(Transformation.filterWordsByChar(word, 'А'));
+
+    }
+}
