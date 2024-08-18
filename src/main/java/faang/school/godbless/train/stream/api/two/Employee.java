baaa80e8c8b575c1203ac336645d3dd7a984f777
Rashit Gareyev <gareyevrs@gmail.com>
2024-08-18 13:37:17 +0400

    nf BJS2-22446 stream api 2 without additionals

---
+++ src/main/java/faang/school/godbless/train/stream/api/two/Employee.java
@@ -0,0 +1,12 @@
+package faang.school.godbless.train.stream.api.two;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@AllArgsConstructor
+@Getter
+public class Employee {
+    private String name;
+    private String department;
+    private Integer salary;
+}
