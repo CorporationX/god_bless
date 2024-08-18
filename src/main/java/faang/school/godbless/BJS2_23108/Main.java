d3d2fe7e58c841a4ca21fbf64f6b22e3a4e7779a
Ruslan Iovlev <ruslan-iovlev@mail.ru>
2024-08-18 15:28:11 +0700

    Потренируем StreamAPI 1

---
+++ src/main/java/faang/school/godbless/BJS2_23108/Main.java
@@ -0,0 +1,22 @@
+package faang.school.godbless.BJS2_23108;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class Main {
+
+    public static void main(String[] args) {
+        List<Integer> numbers = Arrays.asList(1, 2, 3, 4, 5, 6);
+        List<String> strings = Arrays.asList("apple", "banana", "apricot", "kiwi");
+
+        System.out.println("Sum of even numbers: " + StreamApiFirst.sum(numbers));
+        System.out.println("Max element: " + StreamApiFirst.findMax(numbers));
+        System.out.println("Average: " + StreamApiFirst.findAverage(numbers));
+        System.out.println("Count of strings starting with 'a': " + StreamApiFirst.findString(strings, "a"));
+        System.out.println("Filtered strings: " + StreamApiFirst.filterString(strings, "ap"));
+        System.out.println("Sorted strings by length: " + StreamApiFirst.filterStringByLength(strings));
+        System.out.println("All strings are having s: " + StreamApiFirst.checkingString(strings, "s"));
+        System.out.println("Min greater than 2: " + StreamApiFirst.findElement(numbers, 2));
+        System.out.println("Length of strings: " + StreamApiFirst.transformStringByLength(strings));
+    }
+}
