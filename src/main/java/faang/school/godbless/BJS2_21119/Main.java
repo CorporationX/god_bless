e6d3a38ad3a1ed199cdc357badd8ec607e53bdaa
Denis Alginin <AlgininDK@gmail.com>
2024-08-18 22:04:54 +0500

    BJS2-23064 review

--- src/main/java/faang/school/godbless/BJS2_21119/Main.java
+++ src/main/java/faang/school/godbless/BJS2_21119/Main.java
@@ -1,8 +1,19 @@
 package faang.school.godbless.BJS2_21119;
 
+import java.util.Arrays;
+import java.util.List;
 import java.util.function.Supplier;
 
 public class Main {
+    public static void main(String[] args) {
+        List<String> strings = Arrays.asList("one", "two", "thee");
+        String string = withErrorHandling(() -> strings.get(0), (e) -> {
+            System.out.println(e.getMessage());
+            return "Error";
+        });
+        System.out.println(string);
+    }
+
     public static <T> T withErrorHandling(Supplier<T> supplier, ExceptionHandler<T> onError) {
         try {
             return supplier.get();
@@ -10,4 +21,5 @@ public class Main {
             return onError.handle(e);
         }
     }
+
 }
