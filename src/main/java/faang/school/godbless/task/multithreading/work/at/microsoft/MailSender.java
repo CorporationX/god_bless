c148ea606f382ec302568167bd2dc582291cda15
Sultan <nikowosuka@gmail.com>
2024-08-20 01:49:07 +0500

    add @Slf4j

--- src/main/java/faang/school/godbless/task/multithreading/work/at/microsoft/MailSender.java
+++ src/main/java/faang/school/godbless/task/multithreading/work/at/microsoft/MailSender.java
@@ -1,17 +1,15 @@
 package faang.school.godbless.task.multithreading.work.at.microsoft;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import lombok.extern.slf4j.Slf4j;
 
 import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.IntStream;
 
+@Slf4j
 public class MailSender {
     public static final int THREAD_POOL_LIMIT = 5;
     public static final int MAIL_LIMIT_BY_THREAD = 200;
-
-    private static final Logger logger = LoggerFactory.getLogger(MailSender.class);
 
     public static void main(String[] args) {
         List<Thread> threads = new ArrayList<>();
@@ -19,9 +17,7 @@ public class MailSender {
         IntStream.range(0, THREAD_POOL_LIMIT).forEach(i -> {
             int startIndex = MAIL_LIMIT_BY_THREAD * i + 1;
             int endIndex = MAIL_LIMIT_BY_THREAD + startIndex;
-            Thread thread = new Thread(() -> {
-                new SenderRunnable(startIndex, endIndex).run();
-            });
+            Thread thread = new Thread(() -> new SenderRunnable(startIndex, endIndex).run());
             thread.start();
             threads.add(thread);
         });
@@ -30,10 +26,10 @@ public class MailSender {
             try {
                 thread.join();
             } catch (InterruptedException exception) {
-                logger.error("Interrupted exception: {}", exception.getMessage());
+                log.error("Interrupted exception: {}", exception.getMessage());
             }
         });
 
-        logger.info("All message was sends");
+        log.info("All message was sends");
     }
 }
