eca4a2689c72fe780b8edcdd1b0b8f9d757d4d13
zeus <denis@fatum.ru>
2024-08-19 10:00:06 +0300

    BJS2-24029 complete

--- src/main/java/faang/school/godbless/BJS2_24029/Task.java
+++ src/main/java/faang/school/godbless/BJS2_24029/Task.java
@@ -2,6 +2,8 @@ package faang.school.godbless.BJS2_24029;
 
 import lombok.AllArgsConstructor;
 
+import java.util.Random;
+
 @AllArgsConstructor
 public class Task implements Runnable {
     private String name;
@@ -9,6 +11,11 @@ public class Task implements Runnable {
 
     @Override
     public void run() {
-
+        System.out.println(name + " begin working on " + task);
+        try {
+            Thread.sleep(new Random().nextInt(1000, 5000));
+        } catch (InterruptedException e) {
+            throw new RuntimeException("Interrupt task");
+        }
     }
 }
