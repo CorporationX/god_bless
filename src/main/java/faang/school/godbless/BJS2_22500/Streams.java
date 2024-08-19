bb5fd657094c1c3971cd37e0652c1761084c6837
Robert Oguzov <robertoguzov2@gmail.com>
2024-08-20 00:43:31 +0300

    Внес правки

--- src/main/java/faang/school/godbless/BJS2_22500/Streams.java
+++ src/main/java/faang/school/godbless/BJS2_22500/Streams.java
@@ -1,27 +1,31 @@
 package faang.school.godbless.BJS2_22500;
 
 
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.Comparator;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
+import java.util.function.IntPredicate;
 import java.util.function.Predicate;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
-import java.util.stream.Stream;
 
 public class Streams {
 
-    public static List<List<Integer>> findUniquePairsSumIsEqualTarget(List<Integer> nums, int target) {
+    public static Set<List<Integer>> findUniquePairsSumIsEqualTarget(List<Integer> nums, int target) {
         return nums.stream()
                 .flatMap(i ->
                     nums.stream()
-                            .filter(j -> i + j == target)
-                            .filter(j -> !i.equals(j))
-                            .map(j -> Stream.of(i, j).sorted().toList())
+                            .filter(j -> i + j == target && !i.equals(j))
+                            .map(j -> {
+                                List<Integer> pair = new ArrayList<>(List.of(i, j));
+                                Collections.sort(pair);
+                                return pair;
+                            })
                 )
-                .distinct()
-                .toList();
+                .collect(Collectors.toSet());
     }
 
     public static void sortAndPrintCapitals(Map<String, String> countryByCapital) {
@@ -40,15 +44,23 @@ public class Streams {
     public static List<List<String>> findPairsWhoNotFriendsButHaveMutualFriends(Map<String, List<String>> nameByNamesFriends) {
         return nameByNamesFriends.entrySet().stream()
                 .flatMap(person -> nameByNamesFriends.entrySet().stream()
-                        .filter(entry -> !entry.getValue().contains(person.getKey()))
-                        .filter(entry -> !entry.getKey().equals(person.getKey()))
-                        .filter(entry -> entry.getValue()
-                                .stream()
-                                .anyMatch(friend -> person.getValue().contains(friend)))
-                        .map(entry -> Stream.of(person.getKey(), entry.getKey()).sorted().toList())
+                        .filter(isValidFriend(person))
+                        .map(entry -> {
+                            List<String> pair = new ArrayList<>(List.of(person.getKey(), entry.getKey()));
+                            Collections.sort(pair);
+                            return pair;
+                        })
                 )
                 .distinct()
                 .toList();
+    }
+
+    private static Predicate<Map.Entry<String, List<String>>> isValidFriend(Map.Entry<String, List<String>> person) {
+        return entry -> !entry.getValue().contains(person.getKey()) &&
+                !entry.getKey().equals(person.getKey()) &&
+                entry.getValue()
+                        .stream()
+                        .anyMatch(friend -> person.getValue().contains(friend));
     }
 
     public static Map<String, Double> getMapDepartmentByAverageSalary(List<Employee> employees) {
@@ -60,8 +72,7 @@ public class Streams {
     public static List<String> filterByAlphabetAndSortByLength(List<String> strings, char[] alphabet) {
         Set<Character> chars = charArrayToSet(alphabet);
         return strings.stream()
-                .filter(string -> string.chars()
-                        .allMatch(symbol -> chars.contains((char) symbol)))
+                .filter(isValidString(chars))
                 .sorted(Comparator.comparingInt(String::length))
                 .toList();
     }
@@ -70,6 +81,11 @@ public class Streams {
         return IntStream.range(0, array.length)
                 .mapToObj(i -> array[i])
                 .collect(Collectors.toSet());
+    }
+
+    private static Predicate<String> isValidString(Set<Character> chars) {
+        return string -> string.chars()
+                .allMatch(symbol -> chars.contains((char) symbol));
     }
 
     public static List<String> numbersToBinaryNumbers(List<Integer> numbers) {
@@ -104,9 +120,12 @@ public class Streams {
 
     public static int[] findPerfectNumbersInRange(int start, int end) {
         return IntStream.rangeClosed(start, end)
-                .filter(num -> num == IntStream.range(1, num)
-                        .filter(divisor -> num % divisor == 0)
-                        .sum()
-                ).toArray();
+                .filter(isPerfectNumber()).toArray();
+    }
+
+    private static IntPredicate isPerfectNumber() {
+        return num -> num == IntStream.range(1, num)
+                .filter(divisor -> num % divisor == 0)
+                .sum();
     }
 }
