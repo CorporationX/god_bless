9ccb92be91b9534f3b676cbf8b56ba9d890c0b50
Sultan <nikowosuka@gmail.com>
2024-08-19 12:38:45 +0500

    refactoring add logger

--- src/main/java/faang/school/godbless/task/multithreading/they/very/nice/Chore.java
+++ src/main/java/faang/school/godbless/task/multithreading/they/very/nice/Chore.java
@@ -1,19 +1,22 @@
 package faang.school.godbless.task.multithreading.they.very.nice;
 
 import lombok.RequiredArgsConstructor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 @RequiredArgsConstructor
 public class Chore implements Runnable {
+    private final Logger logger = LoggerFactory.getLogger(Chore.class);
     private final String chore;
 
     @Override
     public void run() {
-        System.out.println("Задача '%s' выполняется в потоке: %s"
+        logger.info("Задача '%s' выполняется в потоке: %s"
                 .formatted(chore, Thread.currentThread().getName()));
         try {
             Thread.sleep(3000);
         } catch (InterruptedException exception) {
-            exception.printStackTrace();
+            logger.error("Interrupted exception: {}", exception.getMessage());
         }
     }
 }
