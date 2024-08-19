3533e6fd622198468a93c321e0ff7d85e86d5ee1
cemeh666 <semen-5555@mail.ru>
2024-08-19 22:04:50 +0300

    BJS2-23531 Вы работаете в Microsoft!

--- src/main/java/work/in/microsoft/SenderRunnable.java
+++ src/main/java/work/in/microsoft/SenderRunnable.java
@@ -2,13 +2,24 @@ package work.in.microsoft;
 
 import lombok.AllArgsConstructor;
 
+import java.util.Random;
+
 @AllArgsConstructor
 public class SenderRunnable implements Runnable{
     private final int startIndex;
     private final int endIndex;
+    private static final int MAX_DELAY_TIME = 500;
 
     @Override
     public void run() {
-        System.out.printf("Отправлены письма с %d по %d\n", this.startIndex, this.endIndex);
+        for (int i = this.startIndex; i <= this.endIndex; i++) {
+            try {
+                Thread.sleep(new Random().nextInt(MAX_DELAY_TIME));
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+
+            System.out.printf("Отправлено %d письмо\n", i);
+        }
     }
 }
