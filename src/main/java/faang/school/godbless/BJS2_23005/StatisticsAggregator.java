5b7d93c06f78a5fc22801fcde4d67852c1b6c3bb
Robert Oguzov <robertoguzov2@gmail.com>
2024-08-19 23:32:21 +0300

    Почистил StatisticsAggregator

--- src/main/java/faang/school/godbless/BJS2_23005/StatisticsAggregator.java
+++ src/main/java/faang/school/godbless/BJS2_23005/StatisticsAggregator.java
@@ -3,7 +3,6 @@ package faang.school.godbless.BJS2_23005;
 import java.time.LocalDate;
 import java.util.List;
 import java.util.Map;
-import java.util.function.Predicate;
 import java.util.stream.Collectors;
 
 public class StatisticsAggregator {
@@ -12,17 +11,8 @@ public class StatisticsAggregator {
             LocalDate startDate, LocalDate endDate,
             List<EnvironmentalImpact> environmentalImpacts,
             TypeEnvironmentalImpact typeEnvironmentalImpact) {
-        return mapCompanyBySumImpact(startDate, endDate, environmentalImpacts, typeEnvironmentalImpact, i -> true);
-    }
-
-    public static Map<Company, Double> mapCompanyBySumImpact(
-            LocalDate startDate, LocalDate endDate,
-            List<EnvironmentalImpact> environmentalImpacts,
-            TypeEnvironmentalImpact typeEnvironmentalImpact,
-            Predicate<EnvironmentalImpact> filter) {
 
         return environmentalImpacts.stream()
-                .filter(filter)
                 .filter(impact -> impact.getFactor().equals(typeEnvironmentalImpact))
                 .filter(impact -> impact.getDate().isAfter(startDate) && impact.getDate().isBefore(endDate))
                 .collect(Collectors.groupingBy(
