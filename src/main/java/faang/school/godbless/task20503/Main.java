13451322f68c962b18707e30c4bcaa90c8426c16
Chibix <chibisovanastena@gmail.com>
2024-08-19 11:17:28 +0300

    BJS2-20503 - Вспомним школу

---
+++ src/main/java/faang/school/godbless/task20503/Main.java
@@ -0,0 +1,7 @@
+package faang.school.godbless.task20503;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(TriangleFunctions.triangleArea(4, 6, 6));
+    }
+}
