cdb817ec2746dcd7672a29922f330a522f3ffecd
Владислав Воронов <iPlesk90@yandex.ru>
2024-08-19 09:17:04 +0300

    Выполнил задачу

--- src/main/java/faang/school/godbless/Application.java
+++ src/main/java/faang/school/godbless/Application.java
@@ -1,7 +1,22 @@
 package faang.school.godbless;
 
+import java.util.function.Supplier;
+
 public class Application {
     public static void main(String[] args) {
         System.out.println("God Bless!");
+    }
+
+    @FunctionalInterface
+    public interface ExceptionHandler<T> {
+        T handle(Exception e);
+    }
+
+    public static <T> T withErrorHandling(Supplier<T> supplier, ExceptionHandler<T> onError) {
+        try {
+            return supplier.get();
+        } catch (Exception e) {
+            return onError.handle(e);
+        }
     }
 }
