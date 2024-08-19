6238e4cc4ec87d1bed8a556ee783603e59a6d0cc
Rashit Gareyev <gareyevrs@gmail.com>
2024-08-19 22:20:14 +0400

    nf BJS2-21988 logger added

--- src/main/java/faang/school/godbless/forbidden/meta/NotificationManager.java
+++ src/main/java/faang/school/godbless/forbidden/meta/NotificationManager.java
@@ -1,11 +1,16 @@
 package faang.school.godbless.forbidden.meta;
 
+import lombok.extern.log4j.Log4j;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import java.util.HashMap;
 import java.util.Map;
 import java.util.function.Consumer;
 
+@Log4j
 public class NotificationManager {
-
+    private static final Logger logger = LoggerFactory.getLogger(NotificationManager.class);
     private final Map<String, Consumer<Notification>> typeHandlerMap = new HashMap<>();
 
     public void registerHandler(String type, Consumer<Notification> consumer) {
@@ -15,6 +20,8 @@ public class NotificationManager {
     public void sendNotification(Notification notification) {
         Consumer<Notification> notificationConsumer = typeHandlerMap.get(notification.getType());
         if (notificationConsumer == null) {
+            logger.warn(String.format("Unknown notification type: %s, message: %s. Need to add a handler",
+                    notification.getType(), notification.getMessage()));
             Consumer<Notification> defaultNotificationConsumer =
                     acceptedNotification -> System.out.printf("Type of notification: %s, message: %s",
                             acceptedNotification.getType(), acceptedNotification.getMessage());
