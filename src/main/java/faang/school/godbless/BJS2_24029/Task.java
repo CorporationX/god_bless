4cde00a7580ee12bebd027ffebcb5e768bcf389a
zeus <denis@fatum.ru>
2024-08-19 09:27:38 +0300

    BJS2-24029 initial commit

---
+++ src/main/java/faang/school/godbless/BJS2_24029/Task.java
@@ -0,0 +1,14 @@
+package faang.school.godbless.BJS2_24029;
+
+import lombok.AllArgsConstructor;
+
+@AllArgsConstructor
+public class Task implements Runnable {
+    private String name;
+    private String task;
+
+    @Override
+    public void run() {
+
+    }
+}
