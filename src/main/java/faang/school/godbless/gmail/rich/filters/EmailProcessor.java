e86f9eef434bb4d1efec9d644ec241e99d3ae195
Rashit Gareyev <gareyevrs@gmail.com>
2024-08-19 14:11:37 +0400

    nf BJS2-21348 refactoring after review

--- src/main/java/faang/school/godbless/gmail/rich/filters/EmailProcessor.java
+++ src/main/java/faang/school/godbless/gmail/rich/filters/EmailProcessor.java
@@ -7,15 +7,14 @@ import java.util.function.Predicate;
 
 public class EmailProcessor {
     public void processEmails(List<Email> emails,
-                             Predicate<Email> filter,
-                             Consumer<Email> handler,
-                             Function<Email, Email> converter) {
+                              Predicate<Email> filter,
+                              Consumer<Email> handler,
+                              Function<Email, Email> converter) {
         for (Email email : emails) {
-            if (!filter.test(email)) {
-                continue;
+            if (filter.test(email)) {
+                email = converter.apply(email);
+                handler.accept(email);
             }
-            email = converter.apply(email);
-            handler.accept(email);
         }
     }
 }
