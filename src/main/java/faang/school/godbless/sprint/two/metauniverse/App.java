a06c8fb53b77c3b4b2213440e09b8b76eed3fc8b
Evgenii Shevchenko <evgeny.shevchenko@webware24.de>
2024-08-19 10:59:17 +0200

    all remarks have been fixed

--- src/main/java/faang/school/godbless/sprint/two/metauniverse/App.java
+++ src/main/java/faang/school/godbless/sprint/two/metauniverse/App.java
@@ -1,22 +1,32 @@
 package faang.school.godbless.sprint.two.metauniverse;
 
 import java.util.Arrays;
+import java.util.List;
 import java.util.Locale;
 
 public class App {
     public static void main(String... args) {
         NotificationManager notificationManager = new NotificationManager();
-        notificationManager.registeredFilters(Constants.CHECK_BAD_WORDS, (notificationMessage) -> Arrays.stream(Constants.BAD_WORDS).anyMatch(notificationMessage.toLowerCase()::contains));
+        notificationManager.registeredFilters(
+                (notificationMessage) -> Arrays.stream(Constants.BAD_WORDS)
+                        .anyMatch(notificationMessage.toLowerCase()::contains),
+                List.of("email", "push")
+        );
 
         // Регистрация обработчиков оповещений
-        notificationManager.registerHandler("email", (notification) -> System.out.println("Send via email: " + notification.getMessage()));
-
-        notificationManager.registerHandler("sms", (notification) -> System.out.println("Send SMS: " + notification.getMessage()));
-        notificationManager.registerHandler("push", (notification) -> System.out.println("Send push-notification: " + notification.getMessage()));
+        notificationManager.registerHandler("email",
+                (notification) -> System.out.println("Send via email: " + notification.getMessage())
+        );
+        notificationManager.registerHandler("sms", (notification) ->
+                System.out.println("Send SMS: " + notification.getMessage())
+        );
+        notificationManager.registerHandler("push",
+                (notification) -> System.out.println("Send push-notification: " + notification.getMessage())
+        );
 
         // Отправка оповещений
         Notification emailNotification = new Notification("email", "Your account Bastard was successfully activated");
-        Notification smsNotification = new Notification("sms", "Your password was successfully changed");
+        Notification smsNotification = new Notification("sms", "Your password fuck was successfully changed");
         Notification pushNotification = new Notification("push", "New post from: JohnDoe");
         Notification telegramNotification = new Notification("telegram", "New message from Vasia");
 
--- src/main/java/faang/school/godbless/sprint/two/metauniverse/NotificationManager.java
+++ src/main/java/faang/school/godbless/sprint/two/metauniverse/NotificationManager.java
@@ -1,13 +1,14 @@
 package faang.school.godbless.sprint.two.metauniverse;
 
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 import java.util.function.Consumer;
 import java.util.function.Function;
 
 public class NotificationManager {
     private final Map<String, Consumer<Notification>> registeredNotifications = new HashMap<>();
-    private final Map<String, Function<String, Boolean>> notificationFilters = new HashMap<>();
+    private final Map<Function<String, Boolean>, List<String>> notificationFilters = new HashMap<>();
 
     public void registerHandler(String type, Consumer<Notification> notificationConsumer) {
         this.registeredNotifications.put(type, notificationConsumer);
@@ -15,18 +16,25 @@ public class NotificationManager {
 
     public void sendNotification(Notification notification) {
         if (this.registeredNotifications.containsKey(notification.getType())) {
-            if (notificationFilters.get(Constants.CHECK_BAD_WORDS).apply(notification.getMessage())) {
+
+            boolean isInappropriateContent = notificationFilters.entrySet().stream()
+                    .anyMatch(entry -> entry.getValue().contains(notification.getType())
+                            && entry.getKey().apply(notification.getMessage()));
+
+            if (isInappropriateContent) {
                 System.out.println("Your message contains inappropriate content");
                 return;
             }
+
             this.registeredNotifications.get(notification.getType()).accept(notification);
-            return;
+        } else {
+            System.out.println("Cannot find notification type: " + notification.getType());
         }
-        System.out.println("Cannot find notification type: " + notification.getType());
+
     }
 
-    public void registeredFilters(String name, Function<String, Boolean> isValidString) {
-        this.notificationFilters.put(name, isValidString);
+    public void registeredFilters(Function<String, Boolean> isValidString, List<String> notifications) {
+        this.notificationFilters.put(isValidString, notifications);
     }
 
 }
