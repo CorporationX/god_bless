ce43f79a7d93169eff0949b41315002e69661eb6
Sultan <nikowosuka@gmail.com>
2024-08-19 12:28:49 +0500

    refactoring add logger

--- src/main/java/faang/school/godbless/task/multithreading/might/and/magic/Army.java
+++ src/main/java/faang/school/godbless/task/multithreading/might/and/magic/Army.java
@@ -1,6 +1,8 @@
 package faang.school.godbless.task.multithreading.might.and.magic;
 
 import lombok.RequiredArgsConstructor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -12,6 +14,7 @@ import java.util.stream.IntStream;
 
 @RequiredArgsConstructor
 public class Army {
+    private final Logger logger = LoggerFactory.getLogger(Army.class);
     private final List<Character> army;
 
     public void addUnit(Character unit) {
@@ -21,17 +24,18 @@ public class Army {
     public int calculateTotalPower() {
         ExecutorService executorService = Executors.newFixedThreadPool(army.size());
         List<Future<Integer>> futures = new ArrayList<>();
-        IntStream.range(0, army.size()).forEach(i -> {
-            futures.add(executorService.submit(() -> army.get(i).getPower()));
-        });
+        IntStream.range(0, army.size())
+                .forEach(i -> futures.add(executorService.submit(() -> army.get(i).getPower())));
         executorService.shutdown();
         return IntStream.range(0, futures.size())
                 .map(i -> {
                     try {
                         return futures.get(i).get();
                     } catch (InterruptedException | ExecutionException e) {
+                        logger.error("Error: {}", e.getMessage());
                         throw new RuntimeException(e);
                     }
-                }).sum();
+                })
+                .sum();
     }
 }
