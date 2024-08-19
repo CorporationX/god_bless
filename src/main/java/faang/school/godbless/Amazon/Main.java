89bd12f28e9e3ebd195d0c0c7cbc9710bc9cfd19
TheGreatLucky <danke_villetti@mail.ru>
2024-08-19 14:49:08 +0500

    Corrected comments

--- src/main/java/faang/school/godbless/Amazon/Main.java
+++ src/main/java/faang/school/godbless/Amazon/Main.java
@@ -13,43 +13,33 @@ public class Main {
     private final Map<String, Set<Product>> uniqueProduct = new HashMap<>();
 
     public static void main(String[] args) {
-        Main main = new Main();
         Product pr1 = new Product(1, "Milk", "Food");
         Product pr2 = new Product(12, "Milk", "Food");
         Product pr3 = new Product(3, "Jordans", "boots");
         Product pr4 = new Product(5, "J-Z", "boots");
         Product pr5 = new Product(4, "Wow", "wow");
         Set<Product> cat1 = new HashSet<>();
-        Set<Product> cat2 = new HashSet<>();
         cat1.add(pr1);
         cat1.add(pr5);
         cat1.add(pr2);
-        cat2.add(pr3);
-        cat2.add(pr4);
-        cat2.add(pr5);
-        main.sortingProductsByCategories("Food",cat1);
-        main.sortingProductsByCategories("boots",cat2);
-        System.out.println(main.sortingProductsByCategoriesAndGettingTemporaryMap("Food",cat1));
+        cat1.add(pr3);
+        cat1.add(pr4);
 
+        System.out.println(sortingProductsByCategoriesAndGettingTemporaryMap("Food", cat1));
+        System.out.println(sortingProductsByCategoriesAndGettingTemporaryMap("boots", cat1));
+
+        Main main = new Main();
         main.allCategoryWithProducts();
 
     }
 
-
-    public void sortingProductsByCategories(@NonNull String category, Set<Product> products) {
-        for (Product pr: products) {
-            uniqueProduct.computeIfAbsent(category, key -> new HashSet<>()).add(pr);
-        }
-    }
-
-    public Map<String, List<Product>> sortingProductsByCategoriesAndGettingTemporaryMap(@NonNull String category, Set<Product> products) {
+    public static Map<String, List<Product>> sortingProductsByCategoriesAndGettingTemporaryMap(@NonNull String category, Set<Product> products) {
         Map<String, List<Product>> productsSorted = new HashMap<>();
-        for (Product pr: products) {
+        for (Product pr : products) {
             productsSorted.computeIfAbsent(category, key -> new ArrayList<>()).add(pr);
         }
         return productsSorted;
     }
-
 
     public void allCategoryWithProducts() {
         for (Map.Entry<String, Set<Product>> entry : uniqueProduct.entrySet()) {
@@ -59,5 +49,4 @@ public class Main {
             }
         }
     }
-
 }
--- src/main/java/faang/school/godbless/Amazon/Product.java
+++ src/main/java/faang/school/godbless/Amazon/Product.java
@@ -1,11 +1,12 @@
 package faang.school.godbless.Amazon;
-
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.Getter;
+import lombok.ToString;
 
 @Getter
 @AllArgsConstructor
+@ToString
 public class Product {
     private int id;
     private String name;
