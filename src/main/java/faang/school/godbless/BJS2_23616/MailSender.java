7f973207cd5f626806b38dc16b90631795f59924
Peregruzochka <peregruzochka48@mail.ru>
2024-08-19 17:50:54 +0300

    редактирование

--- src/main/java/faang/school/godbless/BJS2_23616/MailSender.java
+++ src/main/java/faang/school/godbless/BJS2_23616/MailSender.java
@@ -1,25 +1,30 @@
 package faang.school.godbless.BJS2_23616;
 
+import java.util.Arrays;
 import java.util.List;
-import java.util.stream.IntStream;
 
 public class MailSender {
-    private static final int THREAD_AMOUNT = 5;
-    private static final int LETTER_AMOUNT = 1000;
-
     public static void main(String[] args) {
-        int amountEmailsInThread = LETTER_AMOUNT / THREAD_AMOUNT;
-        IntStream.range(0, THREAD_AMOUNT)
-                .mapToObj(num -> new SenderRunnable(num * amountEmailsInThread, (num + 1) * amountEmailsInThread))
+        List<Runnable> tasks = Arrays.asList(
+                new SenderRunnable(0, 200),
+                new SenderRunnable(200, 400),
+                new SenderRunnable(400, 600),
+                new SenderRunnable(600, 800),
+                new SenderRunnable(800, 1000)
+        );
+
+        List<Thread> treads = tasks.stream()
                 .map(Thread::new)
                 .peek(Thread::start)
-                .forEach(thread -> {
-                    try {
-                        thread.join();
-                    } catch (InterruptedException e) {
-                        throw new RuntimeException("Thread Error");
-                    }
-                });
+                .toList();
+
+        treads.forEach(thread -> {
+            try {
+                thread.join();
+            } catch (InterruptedException e) {
+                throw new RuntimeException("Thread error");
+            }
+        });
         System.out.println("All emails have been sent");
     }
 }
