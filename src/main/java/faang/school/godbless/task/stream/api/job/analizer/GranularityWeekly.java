5445bd180a7515c53d8691742aa533f6e885264e
Sultan <nikowosuka@gmail.com>
2024-08-19 12:13:34 +0500

    refactoring

--- src/main/java/faang/school/godbless/task/stream/api/job/analizer/GranularityWeekly.java
+++ src/main/java/faang/school/godbless/task/stream/api/job/analizer/GranularityWeekly.java
@@ -2,7 +2,7 @@ package faang.school.godbless.task.stream.api.job.analizer;
 
 import java.time.DayOfWeek;
 
-public class GranularityWeekly implements TrendGranularity{
+public class GranularityWeekly implements TrendGranularity {
     @Override
     public String get(Job job) {
         return job.getDateOfCreation().toLocalDate().with(DayOfWeek.MONDAY).toString();
