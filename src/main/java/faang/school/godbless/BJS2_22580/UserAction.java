bd228cc23257eade6d633fa0d71d8d18ae12eb70
Robert Oguzov <robertoguzov2@gmail.com>
2024-08-20 09:35:52 +0300

    Внес правки в UserAction

--- src/main/java/faang/school/godbless/BJS2_22580/UserAction.java
+++ src/main/java/faang/school/godbless/BJS2_22580/UserAction.java
@@ -5,8 +5,10 @@ import lombok.ToString;
 
 import java.time.LocalDateTime;
 import java.util.Arrays;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.function.Predicate;
 import java.util.stream.Collectors;
 
 @Getter
@@ -26,10 +28,10 @@ public class UserAction {
     }
 
     public static List<User> findTop10MostActiveUser(List<UserAction> userActions) {
-        return userActions.stream()
-                .collect(Collectors.groupingBy(UserAction::getUser, Collectors.counting()))
-                .entrySet()
-                .stream()
+        Map<User, Long> userByUserActionCount = userActions.stream()
+                .collect(Collectors.groupingBy(UserAction::getUser, Collectors.counting()));
+
+        return userByUserActionCount.entrySet().stream()
                 .sorted(Map.Entry.<User, Long>comparingByValue().reversed())
                 .limit(10)
                 .map(Map.Entry::getKey)
@@ -37,12 +39,12 @@ public class UserAction {
     }
 
     public static List<String> findTop5MostPopularTopics(List<UserAction> userActions) {
-        return userActions.stream()
+        Map<String, Long> hashtagByHashtagCount = userActions.stream()
                 .flatMap(userAction -> Arrays.stream(userAction.getContent().split(" "))
                         .filter(word -> word.startsWith("#"))
-                ).collect(Collectors.groupingBy(hashtag -> hashtag, Collectors.counting()))
-                .entrySet()
-                .stream()
+                ).collect(Collectors.groupingBy(hashtag -> hashtag, Collectors.counting()));
+
+        return hashtagByHashtagCount.entrySet().stream()
                 .sorted(Map.Entry.<String, Long>comparingByValue().reversed())
                 .limit(5)
                 .map(Map.Entry::getKey)
@@ -50,24 +52,28 @@ public class UserAction {
     }
 
     public static List<User> findTop3UsersWithMostCommentsLastMonth(List<UserAction> userActions) {
-        LocalDateTime oneMinuteAgo = LocalDateTime.now().minusMonths(1);
-        return userActions.stream()
-                .filter(userAction -> userAction.getActionType() == ActionType.COMMENT)
-                .filter(userAction -> userAction.getActionDate().isAfter(oneMinuteAgo))
-                .collect(Collectors.groupingBy(UserAction::getUser, Collectors.counting()))
-                .entrySet()
-                .stream()
+        Map<User, Long> userByUserActionCount = userActions.stream()
+                .filter(isValidUserAction())
+                .collect(Collectors.groupingBy(UserAction::getUser, Collectors.counting()));
+
+        return userByUserActionCount.entrySet().stream()
                 .sorted(Map.Entry.<User, Long>comparingByValue().reversed())
                 .limit(3)
                 .map(Map.Entry::getKey)
                 .toList();
     }
 
+    private static Predicate<UserAction> isValidUserAction() {
+        LocalDateTime oneMonthAgo = LocalDateTime.now().minusMonths(1);
+        return userAction -> userAction.getActionDate().isAfter(oneMonthAgo)
+                && userAction.getActionType() == ActionType.COMMENT;
+    }
+
     public static Map<ActionType, Integer> calculatePercentageActions(List<UserAction> userActions) {
-        return userActions.stream()
-                .collect(Collectors.groupingBy(UserAction::getActionType, Collectors.counting()))
-                .entrySet()
-                .stream()
+        Map<ActionType, Long> actionTypeByActionCount = userActions.stream()
+                .collect(Collectors.groupingBy(UserAction::getActionType, Collectors.counting()));
+
+        return actionTypeByActionCount.entrySet().stream()
                 .collect(Collectors.toMap(
                         Map.Entry::getKey,
                         entry -> (int) (Math.round(entry.getValue() * 100.0 / userActions.size())))
