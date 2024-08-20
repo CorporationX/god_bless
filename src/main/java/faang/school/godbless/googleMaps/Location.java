7c70f8338218a4c89e2821adfb33ebc82c45cc62
TheGreatLucky <danke_villetti@mail.ru>
2024-08-20 15:42:46 +0500

    Created Google maps Function

---
+++ src/main/java/faang/school/godbless/googleMaps/Location.java
@@ -0,0 +1,14 @@
+package faang.school.godbless.googleMaps;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.ToString;
+
+@ToString
+@Getter
+@AllArgsConstructor
+public class Location {
+    private String name;
+    private double longitude;
+    private double latitude;
+}
