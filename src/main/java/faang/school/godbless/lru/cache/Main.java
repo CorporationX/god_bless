4dd740930438185bdc2e41f46672f66f4f1d6c29
Rashit Gareyev <gareyevrs@gmail.com>
2024-08-20 11:13:32 +0400

    nf BJS2-20269 corrected work of cache

--- src/main/java/faang/school/godbless/lru/cache/Main.java
+++ src/main/java/faang/school/godbless/lru/cache/Main.java
@@ -3,6 +3,7 @@ package faang.school.godbless.lru.cache;
 import java.util.Comparator;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Optional;
 import java.util.PriorityQueue;
 
 public class Main {
@@ -20,10 +21,13 @@ public class Main {
     public static Data get(int id) {
         Data data = idDataCache.get(id);
         if (data != null) {
+            queue.remove(data);
             data.updateTimestamp();
+            queue.add(data);
             idDataCache.put(id, data);
         } else {
-            data = idDataDB.get(id);
+            data = Optional.ofNullable(idDataDB.get(id))
+                    .orElseThrow(() -> new IllegalArgumentException(String.format("Record not found for provided id: %s", id)));
             data.updateTimestamp();
 
             if (idDataCache.size() == CACHE_SIZE) {
