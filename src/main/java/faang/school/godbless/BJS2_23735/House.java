799b657edf30d96271ef169e703613d231eebe7f
Denis Panasenko <panasenkodenis1995@gmail.com>
2024-08-20 18:08:45 +0300

    BJS2-23735

--- src/main/java/faang/school/godbless/BJS2_23735/House.java
+++ src/main/java/faang/school/godbless/BJS2_23735/House.java
@@ -2,6 +2,7 @@ package faang.school.godbless.BJS2_23735;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.extern.slf4j.Slf4j;
 
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -9,30 +10,39 @@ import java.util.List;
 import java.util.concurrent.Executors;
 import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.TimeUnit;
+import java.util.stream.IntStream;
 
 @AllArgsConstructor
 @Getter
+@Slf4j
 public class House {
     private List<Room> rooms;
     private List<Food> food;
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
         House house = new House(prepareRooms(), new ArrayList<>());
         ScheduledExecutorService service = Executors.newScheduledThreadPool(5);
 
-        house
-                .getRooms()
-                .forEach(room -> service.schedule(() -> house.collectFood(room), 30, TimeUnit.SECONDS));
+        IntStream
+                .range(0, house.getRooms().size())
+                .filter(counter -> counter % 2 == 0)
+                .forEach(oddRoomNumber -> service.schedule(() -> {
+                    log.info("{} room {}", Thread.currentThread().getName(), oddRoomNumber);
+                    house.collectFood(house.getRooms().get(oddRoomNumber));
+                    if(house.getRooms().size() > oddRoomNumber + 1){
+                        house.collectFood(house.getRooms().get(oddRoomNumber + 1));
+                        log.info("{} room {}", Thread.currentThread().getName(), oddRoomNumber + 1);
+                    }
+
+                }, 30, TimeUnit.SECONDS));
 
         service.shutdown();
+        service.awaitTermination(50, TimeUnit.SECONDS);
+        log.info("Total amount of stolen food: {}", house.food);
     }
 
     public void collectFood(Room room){
         food.addAll(room.food());
-    }
-
-    public static void fillHouseWithFood(House house){
-
     }
 
     private static List<Room> prepareRooms(){
