ec68342f2e2e095629526fec5fe994a036e1c29e
Владислав Воронов <iPlesk90@yandex.ru>
2024-08-20 16:24:29 +0300

    Выполнил все пункты, кроме 4, слишком сложная для меня задача. Дополнителньые тоже не сделал

---
+++ src/main/java/faang/school/godbless/StreamAPI_2/Employee.java
@@ -0,0 +1,10 @@
+package faang.school.godbless.StreamAPI_2;
+
+import lombok.Getter;
+
+@Getter
+public class Employee {
+    private String name;
+    private String department;
+    private Double salary;
+}
