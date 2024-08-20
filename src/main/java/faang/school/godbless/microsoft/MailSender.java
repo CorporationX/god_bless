1aa53c6dca62a9f5782f8f84cebfb52262414292
pgusev <pgusev@fil-it.ru>
2024-08-20 22:08:02 +0300

    BJS2-23558 - Переделал join для потоков

--- src/main/java/faang/school/godbless/microsoft/MailSender.java
+++ src/main/java/faang/school/godbless/microsoft/MailSender.java
@@ -3,20 +3,23 @@ package faang.school.godbless.microsoft;
 public class MailSender {
 
     private static final int TWO_HUNDRED = 200;
+    private static final int THREAD_COUNT = 5;
 
-    public static void main(String[] args) {
-        for (int i = 0; i < 5; i++) {
+    static Thread[] threads = new Thread[THREAD_COUNT];
+
+    public static void main(String[] args) throws InterruptedException {
+        for (int i = 0; i < THREAD_COUNT; i++) {
             SenderRunnable sender = new SenderRunnable(i * TWO_HUNDRED + 1,
                     i * TWO_HUNDRED + TWO_HUNDRED);
-            Thread thread = new Thread(sender);
-            thread.start();
-            try {
-                thread.join();
-            } catch (InterruptedException e) {
-                throw new RuntimeException(e);
-            }
-            System.out.println("Thread " + i + " finished");
+            threads[i] = new Thread(sender);
+            threads[i].start();
         }
+
+        for (Thread thread : threads) {
+            thread.join();
+            System.out.println(thread.getName() + " finished ");
+        }
+
         System.out.println("Fin all");
     }
 }
