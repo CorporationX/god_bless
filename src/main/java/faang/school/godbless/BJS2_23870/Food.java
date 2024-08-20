30ae864d952d48b8eae062912b907195e233a2b4
Robert Oguzov <robertoguzov2@gmail.com>
2024-08-20 12:48:37 +0300

    Написал класс Food

---
+++ src/main/java/faang/school/godbless/BJS2_23870/Food.java
@@ -0,0 +1,14 @@
+package faang.school.godbless.BJS2_23870;
+
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.ToString;
+
+@Getter
+@ToString
+@EqualsAndHashCode
+@AllArgsConstructor
+public class Food {
+    private String name;
+}
