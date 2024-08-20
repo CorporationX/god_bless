c163a9a75b9d24e9e0f1e149bfdd479637203871
aephrodita <146974539+aephrodita@users.noreply.github.com>
2024-08-20 15:09:00 +0500

    BJS2-22551

--- src/main/java/faang/school/godbless/BJS222551/Main.java
+++ src/main/java/faang/school/godbless/BJS222551/Main.java
@@ -9,39 +9,41 @@ import java.util.stream.Collectors;
 
 public class Main {
     public static List<Long> findTop10ActiveUsers(List<UserAction> userActions) {
-        return userActions.stream()
-                .collect(Collectors.groupingBy(UserAction::getUserId, Collectors.counting()))
-                .entrySet().stream()
+        Map<Long, Long> userActionCounts = userActions.stream()
+                .collect(Collectors.groupingBy(UserAction::getUserId, Collectors.counting()));
+
+        return userActionCounts.entrySet().stream()
                 .sorted((e1, e2) -> Long.compare(e2.getValue(), e1.getValue()))
                 .limit(10)
-                .map(Map.Entry::getValue)
+                .map(Map.Entry::getKey)
                 .toList();
     }
 
     public static List<String> findTop5PopularTopics(List<UserAction> userActions) {
-        return userActions.stream()
+        Map<String, Long> topicCounts = userActions.stream()
                 .filter(userAction ->
                         (userAction.getActionType() == ActionType.POST || userAction.getActionType() == ActionType.COMMENT)
                                 && userAction.getContent().contains("#"))
                 .flatMap(userAction -> Arrays.stream(userAction.getContent().split("\\s+")))
                 .filter(word -> word.startsWith("#"))
-                .collect(Collectors.groupingBy(Function.identity(), Collectors.counting()))
-                .entrySet().stream()
+                .collect(Collectors.groupingBy(Function.identity(), Collectors.counting()));
+
+        return topicCounts.entrySet().stream()
                 .sorted((e1, e2) -> Long.compare(e2.getValue(), e1.getValue()))
                 .limit(5)
                 .map(Map.Entry::getKey)
                 .toList();
     }
 
-
     public static List<Long> findTop3CommentersLastMonth(List<UserAction> userActions) {
         LocalDateTime oneMonthAgo = LocalDateTime.now().minusMonths(1);
 
-        return userActions.stream()
+        Map<Long, Long> commenterCounts = userActions.stream()
                 .filter(userAction -> userAction.getActionType() == ActionType.COMMENT
                         && userAction.getActionDate().isAfter(oneMonthAgo))
-                .collect(Collectors.groupingBy(UserAction::getUserId, Collectors.counting()))
-                .entrySet().stream()
+                .collect(Collectors.groupingBy(UserAction::getUserId, Collectors.counting()));
+
+        return commenterCounts.entrySet().stream()
                 .sorted((e1, e2) -> Long.compare(e2.getValue(), e1.getValue()))
                 .limit(3)
                 .map(Map.Entry::getKey)
@@ -51,15 +53,13 @@ public class Main {
     public static Map<ActionType, Double> calculateActionTypePercentage(List<UserAction> userActions) {
         long totalActions = userActions.size();
 
-        return userActions.stream()
-                .collect(Collectors.groupingBy(
-                        UserAction::getActionType,
-                        Collectors.counting()))
-                .entrySet().stream()
+        Map<ActionType, Long> actionTypeCounts = userActions.stream()
+                .collect(Collectors.groupingBy(UserAction::getActionType, Collectors.counting()));
+
+        return actionTypeCounts.entrySet().stream()
                 .collect(Collectors.toMap(
                         Map.Entry::getKey,
                         entry -> (entry.getValue() * 100.0) / totalActions
                 ));
     }
-
 }
