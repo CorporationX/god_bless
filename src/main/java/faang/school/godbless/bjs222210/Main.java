a5906e50b7ebc597ac310a84e2ca622314f1e48f
Anton Martynenko <ant.martynenko@gmail.com>
2024-08-20 23:19:03 +0200

    Решение задачи "Фильтруем спам"

---
+++ src/main/java/faang/school/godbless/bjs222210/Main.java
@@ -0,0 +1,23 @@
+package faang.school.godbless.bjs222210;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) {
+        MessageProcessor messageProcessor = new MessageProcessor();
+
+        MessageFilter spamFilter = message -> !message.toLowerCase().contains("спам");
+        MessageFilter lengthFilter = message -> message.length() > 10;
+        MessageFilter emojiFilter = message -> !message.contains("😀");
+
+        List<MessageFilter> filters = Arrays.asList(spamFilter, lengthFilter, emojiFilter);
+
+        String[] messages = {"Привет!", "Это спам!", "Как дела? 😀", "Длинное сообщение, которое будет пропущено фильтрами"};
+
+        for (String message : messages) {
+            boolean isFiltered = messageProcessor.processMessage(message, filters);
+            System.out.println("Сообщение: " + message + " | Пропущено: " + isFiltered);
+        }
+    }
+}
