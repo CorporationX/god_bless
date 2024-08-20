cd479cd1e6c0c29e772647384f1647555bd8256b
Dmitriy Varankin <dima.varankin2012@yandex.ru>
2024-08-20 17:10:57 +0300

    1 версия решения задачи: Потренируем StreamAPI 1

---
+++ src/main/java/faang/school/godbless/StreamAPI/Stream_API1/Main.java
@@ -0,0 +1,24 @@
+package faang.school.godbless.StreamAPI.Stream_API1;
+
+import java.util.List;
+
+public class Main {
+    private static List<Integer> numers = List.of(1, 2, 3, 4, 5, 6, 7, 8, 9 , 10, 23, 65, 45);
+    private static List<String> fruits = List.of("apple", "banana", "avocado", "cherry", "apricot", "date");
+    private static List<String> fruitsA = List.of("apple", "avocado", "apricot");
+
+    public static void main(String[] args) {
+        System.out.println(Solution.outputSumEvenNumbers(numers));
+        System.out.println(Solution.outputMaxNumber(numers));
+        System.out.println(Solution.outputAverageNumber(numers));
+
+        char prefix = 'a';
+        System.out.println(Solution.countLineStartingWith(fruits, prefix));
+
+        System.out.println(Solution.filterStringWithValue(fruits, "apple"));
+        System.out.println(Solution.sortLineLength(fruits));
+        System.out.println(Solution.checkingAllElementsForCondition(fruitsA));
+        System.out.println(Solution.findSmallNumberMoreN(numers, 10));
+        System.out.println(Solution.stringToListLength(fruits));
+    }
+}
---
+++ src/main/java/faang/school/godbless/StreamAPI/Stream_API1/Solution.java
@@ -0,0 +1,64 @@
+package faang.school.godbless.StreamAPI.Stream_API1;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class Solution {
+    public static int outputSumEvenNumbers(List<Integer> numbers) {
+        return numbers.stream()
+                .filter(number -> number % 2 == 0)
+                .mapToInt(Integer::intValue)
+                .sum();
+    }
+
+    public static int outputMaxNumber(List<Integer> numbers) {
+        return numbers.stream()
+                .mapToInt(Integer::intValue)
+                .max()
+                .orElseThrow(() -> new IllegalArgumentException("Список пуст"));
+    }
+
+    public static double outputAverageNumber(List<Integer> numbers) {
+        return numbers.stream()
+                .mapToInt(Integer::intValue)
+                .average()
+                .orElseThrow(() -> new IllegalArgumentException("Список пуст"));
+    }
+
+    public static long countLineStartingWith(List<String> strings, char prefix) {
+        return strings.stream()
+                .filter(s -> s.startsWith(String.valueOf(prefix)))
+                .count();
+    }
+
+    public static List<String> filterStringWithValue (List<String> strings, String line) {
+        return strings.stream()
+                .filter(s -> s.contains(line))
+                .collect(Collectors.toList());
+    }
+
+    public static List<String> sortLineLength (List<String> strings){
+        return strings.stream()
+                .sorted((s1, s2) -> Integer.compare(s1.length(), s2.length()))
+                .toList();
+    }
+
+    public static boolean checkingAllElementsForCondition (List<String> elements){
+        return elements.stream()
+                .allMatch(element -> element.startsWith("a"));
+    }
+
+    public static int findSmallNumberMoreN (List<Integer> listInt, Integer N){
+        return listInt.stream()
+                .filter(element -> element > N)
+                .mapToInt(Integer::intValue)
+                .min()
+                .orElseThrow();
+    }
+
+    public static List<Integer> stringToListLength (List<String> strings){
+        return strings.stream()
+                .map(String::length)
+                .toList();
+    }
+}
