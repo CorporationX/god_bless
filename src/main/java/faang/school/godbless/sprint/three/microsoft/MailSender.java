fdfe1593a71af0fd089737443463d803e885cbde
Evgenii Shevchenko <evgeny.shevchenko@webware24.de>
2024-08-20 11:07:44 +0200

    fixing remarks

--- src/main/java/faang/school/godbless/sprint/three/microsoft/MailSender.java
+++ src/main/java/faang/school/godbless/sprint/three/microsoft/MailSender.java
@@ -1,24 +1,30 @@
 package faang.school.godbless.sprint.three.microsoft;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class MailSender {
     public static void main(String... args) {
         int batchMails = Constants.MAILS_IN_QUEUE / Constants.ACTIVE_THREADS;
+        List<Thread> executingThreads = new ArrayList<>();
 
-        int start = 1;
-        int end;
         for (int i = 0; i < Constants.ACTIVE_THREADS; i++) {
-            end = start + batchMails;
+            int startIndex = i * batchMails + 1;
+            int endIndex = startIndex + batchMails;
 
-            SenderRunnable senderRunnable = new SenderRunnable(start, end);
-            Thread thread = new Thread(senderRunnable);
-            try {
-                thread.start();
-                thread.join();
-            } catch (InterruptedException e) {
-                throw new RuntimeException(e);
-            }
-
-            start = end;
+            Thread thread = new Thread(new SenderRunnable(startIndex, endIndex));
+            thread.start();
+            executingThreads.add(thread);
         }
+
+        executingThreads.forEach(thread -> {
+                            try {
+                                thread.join();
+                            } catch (InterruptedException e) {
+                                throw new RuntimeException(e);
+                            }
+                        });
+
+        System.out.println("Finish");
     }
 }
