ca4bb9991a07d0ccff33b5d8191d19d3123a17b6
PLSGOTOASS <koteykaarisha@mail.ru>
2024-08-20 16:15:41 +0300

    Поправил недоработки

--- src/main/java/faang/school/godbless/AnalysisVacantion/DataAnalyzer.java
+++ src/main/java/faang/school/godbless/AnalysisVacantion/DataAnalyzer.java
@@ -7,29 +7,39 @@ import java.util.stream.Collectors;
 
 public class DataAnalyzer {
     public List<String> topFiveSkills(List<Job> jobList) {
-        return jobList.stream()
+        Map<String, Integer> m = jobList.stream()
                 .flatMap(job -> job.getRequirements().stream())
-                .collect(Collectors.groupingBy(Function.identity(), Collectors.collectingAndThen(Collectors.counting(), Long::intValue)))
-                .entrySet().stream()
+                .collect(Collectors.groupingBy(Function.identity(), Collectors.collectingAndThen(Collectors.counting(), Long::intValue)));
+
+        return m.entrySet().stream()
                 .sorted((entry1, entry2) -> entry2.getValue() - entry1.getValue())
                 .map(Map.Entry::getKey).limit(5).toList();
     }
 
     public List<String> topFiveVacantNames(List<Job> jobList) {
-        return jobList.stream().collect(Collectors.groupingBy(Job::getName, Collectors.collectingAndThen(Collectors.counting(), Long::intValue)))
-                .entrySet().stream().sorted((entry1, entry2) -> entry2.getValue() - entry1.getValue())
-                .map(Map.Entry::getKey).limit(5).toList();
+        return simpleTopFive(jobList, Job::getName);
     }
 
     public List<String> topFiveVacantOffices(List<Job> jobList) {
-        return jobList.stream().collect(Collectors.groupingBy(Job::getLocation, Collectors.collectingAndThen(Collectors.counting(), Long::intValue)))
-                .entrySet().stream().sorted((entry1, entry2) -> entry2.getValue() - entry1.getValue())
-                .map(Map.Entry::getKey).limit(5).toList();
+        return simpleTopFive(jobList, Job::getLocation);
     }
 
     public Map<String, Integer> salaryRange(List<Job> jobList) {
-        Map<String, List<Job>> jobsBySalariesRange = jobList.stream().collect(Collectors.groupingBy(Job::getSalaryRange));
+        Map<String, List<Job>> jobsBySalariesRange = jobList.stream()
+                .collect(Collectors.groupingBy(Job::getSalaryRange));
+
         return jobsBySalariesRange.entrySet().stream().collect(Collectors.toMap(Map.Entry::getKey, entry -> entry.getValue().size()));
+    }
+
+    public List<String> simpleTopFive(List<Job> jobList, Function<Job, String> function) {
+        Map<String, Integer> m = jobList.stream().
+                collect(Collectors.groupingBy(function, Collectors.collectingAndThen(Collectors.counting(), Long::intValue)));
+
+        return m.entrySet()
+                .stream()
+                .sorted((entry1, entry2) -> entry2.getValue() - entry1.getValue())
+                .map(Map.Entry::getKey)
+                .limit(5).toList();
     }
 
 }
