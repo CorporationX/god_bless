1425d0880211e9493a37fe0b60b882e479d0e5ce
Alena Guskova <amguskova@edu.hse.ru>
2024-08-20 21:35:02 +0300

    BJS2-22557 Анализ активности пользователей в социальной сети (добавлены исправления после ревью)

--- src/main/java/faang/school/godbless/activityAnalysis/ActionAnalyzer.java
+++ src/main/java/faang/school/godbless/activityAnalysis/ActionAnalyzer.java
@@ -6,53 +6,44 @@ import java.util.Map;
 import java.util.stream.Collectors;
 
 public class ActionAnalyzer {
-    public List<Integer> topTenActiveUsers (List<UserAction> userActions) {
-        return userActions
-                .stream()
-                .collect(Collectors.groupingBy(UserAction::getUserId, Collectors.counting()))
-                .entrySet()
-                .stream()
+    public List<Integer> topTenActiveUsers(List<UserAction> userActions) {
+        Map<Integer, Long> map = userActions.stream()
+                .collect(Collectors.groupingBy(UserAction::getUserId, Collectors.counting()));
+        return map.entrySet().stream()
                 .sorted(Map.Entry.<Integer, Long>comparingByValue().reversed())
                 .limit(10)
                 .map(Map.Entry::getKey)
                 .toList();
     }
 
-    public List<String> topFivePopularThemes (List<UserAction> userActions) {
-        return userActions
-                .stream()
+    public List<String> topFivePopularThemes(List<UserAction> userActions) {
+        Map<String, Long> map = userActions.stream()
                 .map(UserAction::getContent)
-                .filter(content -> content.contains("#"))
-                .collect(Collectors.groupingBy(content -> content.replace("#", ""), Collectors.counting()))
-                .entrySet()
-                .stream()
+                .filter(content -> content.startsWith("#"))
+                .collect(Collectors.groupingBy(content -> content.replace("#", ""), Collectors.counting()));
+        return map.entrySet().stream()
                 .sorted(Map.Entry.<String, Long>comparingByValue().reversed())
                 .limit(5)
                 .map(Map.Entry::getKey)
                 .toList();
     }
 
-    public List<Integer> topThreeCommenters (List<UserAction> userActions) {
+    public List<Integer> topThreeCommenters(List<UserAction> userActions) {
         LocalDate oneMonthAgo = LocalDate.now().minusMonths(1);
-
-        return userActions.stream()
-                .filter(userAction -> userAction.getActionType().equals(ActionType.COMMENT))
-                .filter(userAction -> userAction.getActionDate().isAfter(oneMonthAgo))
-                .collect(Collectors.groupingBy(UserAction::getUserId, Collectors.counting()))
-                .entrySet()
-                .stream()
+        Map<Integer, Long> map = userActions.stream()
+                .filter(userAction -> userAction.getActionType().equals(ActionType.COMMENT) && userAction.getActionDate().isAfter(oneMonthAgo))
+                .collect(Collectors.groupingBy(UserAction::getUserId, Collectors.counting()));
+        return map.entrySet().stream()
                 .sorted(Map.Entry.<Integer, Long>comparingByValue().reversed())
                 .limit(3)
                 .map(Map.Entry::getKey)
                 .toList();
     }
 
-    public Map<ActionType, Double> getActionTypePercentage (List<UserAction> userActions) {
-        return userActions
-                .stream()
-                .collect(Collectors.groupingBy(UserAction::getActionType, Collectors.counting()))
-                .entrySet()
-                .stream()
+    public Map<ActionType, Double> getActionTypePercentage(List<UserAction> userActions) {
+        Map<ActionType, Long> map = userActions.stream()
+                .collect(Collectors.groupingBy(UserAction::getActionType, Collectors.counting()));
+        return map.entrySet().stream()
                 .collect(Collectors.toMap(
                         Map.Entry::getKey,
                         entry -> (double) entry.getValue() / userActions.size() * 100
