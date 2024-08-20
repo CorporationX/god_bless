5a5967b6fa9b81e0d31299f8f470fd93e36fb39a
Robert Oguzov <robertoguzov2@gmail.com>
2024-08-20 18:46:59 +0300

    Написал класс Main для тестирования

---
+++ src/main/java/faang/school/godbless/BJS2_24450/Main.java
@@ -0,0 +1,23 @@
+package faang.school.godbless.BJS2_24450;
+
+import java.util.List;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+public class Main {
+    public static void main(String[] args) {
+        Boss boss = new Boss("Java", 2);
+        Player player1 = new Player("Robert");
+        Player player2 = new Player("Sultan");
+        Player player3 = new Player("Denis");
+        Player player4 = new Player("Vlad");
+        Player player5 = new Player("Mikhail");
+        List<Player> players = List.of(player1, player2, player3, player4, player5);
+
+        ExecutorService executor = Executors.newFixedThreadPool(boss.getMaxPlayers());
+        players.forEach(player ->
+                executor.execute(() -> player.startBattle(boss))
+        );
+        executor.shutdown();
+    }
+}
