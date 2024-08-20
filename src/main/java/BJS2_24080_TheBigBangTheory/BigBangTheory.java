6c935ea956e1eb96b6db9379737628f3500dfce7
Sergey Sklyar <sklyar1091@gmail.com>
2024-08-20 11:34:08 +0300

    The Big Bang Theory

---
+++ src/main/java/BJS2_24080_TheBigBangTheory/BigBangTheory.java
@@ -0,0 +1,19 @@
+package BJS2_24080_TheBigBangTheory;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+public class BigBangTheory {
+    public static void main(String[] args) {
+        ExecutorService executor = Executors.newFixedThreadPool(4);
+        Task task1 = new Task("Sheldon", "Theory");
+        Task task2 = new Task("Leonard", "Modelling");
+        Task task3 = new Task("Govard", "Development");
+        Task task4 = new Task("Radjesh", "Analyze");
+        executor.submit(task1);
+        executor.submit(task2);
+        executor.submit(task3);
+        executor.submit(task4);
+        executor.shutdown();
+    }
+}
