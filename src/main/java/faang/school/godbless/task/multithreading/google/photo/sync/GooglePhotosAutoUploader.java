cec4c6704cfcbfc7a3877410cdb55f229947a6ab
Sultan <nikowosuka@gmail.com>
2024-08-21 15:43:03 +0500

    refactoring, add stop auto upload by timer

--- src/main/java/faang/school/godbless/task/multithreading/google/photo/sync/GooglePhotosAutoUploader.java
+++ src/main/java/faang/school/godbless/task/multithreading/google/photo/sync/GooglePhotosAutoUploader.java
@@ -9,10 +9,12 @@ import java.util.List;
 public class GooglePhotosAutoUploader {
     private final Object lock = new Object();
     private final List<String> photosToUpload = new ArrayList<>();
+    private boolean autoUploadRun;
 
     public void startAutoUpload() {
         log.info("Начата автозагрузка фотографий в облако");
-        while (true) {
+        autoUploadRun = true;
+        while (autoUploadRun) {
             synchronized (lock) {
                 if (photosToUpload.isEmpty()) {
                     log.info("Список фотографий пуст, автозагрузка приостановлена");
@@ -20,12 +22,21 @@ public class GooglePhotosAutoUploader {
                         lock.wait();
                     } catch (InterruptedException exception) {
                         log.error("Interrupted exception: {}", exception.getMessage());
+                        Thread.currentThread().interrupt();
                     }
                 } else {
                     log.info("В списке новые фото, автозагрузка возобновлена");
                     uploadPhotos();
                 }
             }
+        }
+        log.info("Автозагрузка завершена");
+    }
+
+    public void stopAutoUpload() {
+        synchronized (lock) {
+            autoUploadRun = false;
+            lock.notifyAll();
         }
     }
 
--- src/main/java/faang/school/godbless/task/multithreading/google/photo/sync/Main.java
+++ src/main/java/faang/school/godbless/task/multithreading/google/photo/sync/Main.java
@@ -1,10 +1,20 @@
 package faang.school.godbless.task.multithreading.google.photo.sync;
 
-import java.util.List;
+import lombok.extern.slf4j.Slf4j;
 
+import java.util.List;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
+
+@Slf4j
 public class Main {
+    private static final int SCHEDULER_TIMER = 5;
+
+    private static final ScheduledExecutorService scheduler = Executors.newScheduledThreadPool(1);
+    private static final GooglePhotosAutoUploader googlePhotosAutoUploader = new GooglePhotosAutoUploader();
+
     public static void main(String[] args) {
-        var googlePhotosAutoUploader = new GooglePhotosAutoUploader();
         var pathToPhotos = List.of(
                 "/disc/images/Photo_1",
                 "/disc/images/Photo_2",
@@ -21,5 +31,21 @@ public class Main {
         });
         uploadPhoto.start();
         addPhoto.start();
+        executeStopUpload();
+        try {
+            uploadPhoto.join();
+            addPhoto.join();
+        } catch (InterruptedException e) {
+            log.error("Interrupted exception: {}", e.getMessage());
+            Thread.currentThread().interrupt();
+        }
+        log.info("Программа завершена");
+    }
+
+    private static void executeStopUpload() {
+        scheduler.schedule(() -> {
+            googlePhotosAutoUploader.stopAutoUpload();
+            scheduler.shutdown();
+        }, SCHEDULER_TIMER, TimeUnit.SECONDS);
     }
 }
