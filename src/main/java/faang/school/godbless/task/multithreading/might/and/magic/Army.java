d61331be6712b2c82c74a0d2f3182a9390aa971e
Sultan <nikowosuka@gmail.com>
2024-08-21 13:59:04 +0500

    refactoring after review

--- src/main/java/faang/school/godbless/task/multithreading/might/and/magic/Army.java
+++ src/main/java/faang/school/godbless/task/multithreading/might/and/magic/Army.java
@@ -3,36 +3,54 @@ package faang.school.godbless.task.multithreading.might.and.magic;
 import lombok.extern.slf4j.Slf4j;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
-import java.util.concurrent.Future;
-import java.util.stream.IntStream;
 
 @Slf4j
 public class Army {
-    private final List<Character> army = new ArrayList<>();
+    private final List<CompletableFuture<Integer>> futures = new ArrayList<>();
+    private final Map<Character, List<Character>> army = new HashMap<>();
+    private final ExecutorService executor = Executors.newFixedThreadPool(5);
 
-    public void addUnit(Character unit) {
-        army.add(unit);
+    public void addUnits(Character unit) {
+        List<Character> units = army.computeIfAbsent(unit, k -> new ArrayList<>());
+        units.add(unit);
     }
 
     public int calculateTotalPower() {
-        ExecutorService executorService = Executors.newFixedThreadPool(army.size());
-        List<Future<Integer>> futures = new ArrayList<>();
-        IntStream.range(0, army.size())
-                .forEach(i -> futures.add(executorService.submit(() -> army.get(i).getPower())));
-        executorService.shutdown();
-        return IntStream.range(0, futures.size())
-                .map(i -> {
-                    try {
-                        return futures.get(i).get();
-                    } catch (InterruptedException | ExecutionException e) {
-                        log.error("Error: {}", e.getMessage());
-                        throw new RuntimeException(e);
-                    }
-                })
-                .sum();
+        futures.addAll(
+                army.values()
+                        .stream()
+                        .map(this::startThread)
+                        .toList());
+        executor.shutdown();
+        return calculate();
+    }
+
+    private CompletableFuture<Integer> startThread(List<Character> units) {
+        return CompletableFuture.supplyAsync(() -> units
+                .stream()
+                .map(Character::getPower)
+                .mapToInt(Integer::intValue)
+                .sum(), executor);
+    }
+
+    private int calculate() {
+        try {
+            return CompletableFuture.allOf(futures.toArray(new CompletableFuture[futures.size()]))
+                    .thenApply(v -> futures.stream()
+                            .map(CompletableFuture::join)
+                            .mapToInt(Integer::intValue)
+                            .sum())
+                    .get();
+        } catch (InterruptedException | ExecutionException e) {
+            log.error("Error: {}", e.getMessage());
+            return 0;
+        }
     }
 }
--- src/main/java/faang/school/godbless/task/multithreading/might/and/magic/Main.java
+++ src/main/java/faang/school/godbless/task/multithreading/might/and/magic/Main.java
@@ -1,13 +1,16 @@
 package faang.school.godbless.task.multithreading.might.and.magic;
 
+import java.util.stream.IntStream;
+
 public class Main {
     public static void main(String[] args) {
         Army army = new Army();
-        army.addUnit(new Archer(25));
-        army.addUnit(new Swordsman(40));
-        army.addUnit(new Mage(50));
-        army.addUnit(new Mage(20));
-
+        IntStream.rangeClosed(1, 10)
+                .forEach(i -> army.addUnits(new Archer(25)));
+        IntStream.rangeClosed(1, 7)
+                .forEach(i -> army.addUnits(new Mage(25)));
+        IntStream.rangeClosed(1, 12)
+                .forEach(i -> army.addUnits(new Swordsman(25)));
         int totalPower = army.calculateTotalPower();
         System.out.println("Total army power: " + totalPower);
     }
