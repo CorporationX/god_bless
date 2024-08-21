a1c9a762ec85c874dbe728a4745f9708d0e0e901
aephrodita <146974539+aephrodita@users.noreply.github.com>
2024-08-22 00:40:07 +0500

    BJS2-23950

--- src/main/java/faang/school/godbless/BJS223950/GriffinsFoodDelivery.java
+++ src/main/java/faang/school/godbless/BJS223950/GriffinsFoodDelivery.java
@@ -3,6 +3,7 @@ package faang.school.godbless.BJS223950;
 import java.util.Random;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
 
 public class GriffinsFoodDelivery {
     public static void main(String[] args) {
@@ -14,6 +15,17 @@ public class GriffinsFoodDelivery {
             executor.execute(new FoodDeliveryTask(characterName, foodAmount));
         }
 
+        // Инициация безопасного завершения работы
         executor.shutdown();
+        try {
+            // Ожидание завершения всех задач в течение 5 секунд
+            if (!executor.awaitTermination(5, TimeUnit.SECONDS)) {
+                System.out.println("Принудительное завершение...");
+                executor.shutdownNow(); // Принудительное завершение, если задачи не завершены
+            }
+        } catch (InterruptedException e) {
+            System.err.println("Завершение прервано");
+            executor.shutdownNow();
+        }
     }
 }
