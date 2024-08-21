b754d59c299f345ac96ff6abd3e7f0527409edcd
Sergey Sklyar <sklyar1091@gmail.com>
2024-08-22 00:03:52 +0300

    BJS2-24139 Supercow!

---
+++ src/main/java/BJS2_24139_Supercow/Boss.java
@@ -0,0 +1,37 @@
+package BJS2_24139_Supercow;
+
+public class Boss {
+    private final int maxPlayers;
+    private int currentPlayers;
+
+    public Boss(int maxPlayers, int currentPlayers) {
+        this.maxPlayers = maxPlayers;
+        this.currentPlayers = currentPlayers;
+    }
+
+    public void joinBattle(Player player) {
+        synchronized (this) {
+            while (currentPlayers == maxPlayers) {
+                try {
+                    this.wait();
+                } catch (InterruptedException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+            currentPlayers++;
+            System.out.println(player.name() + " joined the battle! Current players are fighting boss: " +
+                    currentPlayers);
+        }
+        try {
+            Thread.sleep(4000);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+        synchronized (this) {
+            currentPlayers--;
+            System.out.println(player.name() + " has left the battle! Current open slots: " +
+                    (maxPlayers - currentPlayers));
+            this.notifyAll();
+        }
+    }
+}
