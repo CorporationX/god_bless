baad44dc6af9da3c48682350062e6a1a824746e5
Peregruzochka <peregruzochka48@mail.ru>
2024-08-21 14:27:54 +0300

    финал переделки

--- src/main/java/faang/school/godbless/BJS2_23405/Army.java
+++ src/main/java/faang/school/godbless/BJS2_23405/Army.java
@@ -13,21 +13,15 @@ public class Army {
     private final AtomicInteger totalPower = new AtomicInteger(0);
 
     public void addCharacter(Character character) {
-        //распределение героев по типам
         army.computeIfAbsent(character.getClass().getSimpleName(), key -> new ArrayList<>()).add(character);
     }
 
     public int calculateTotalPower() {
-        //создание потоков
         List<Thread> threads = army.values().stream()
                 .map(this::createTask)
                 .map(Thread::new)
                 .toList();
-
-        //одновременный запуск
         threads.forEach(Thread::start);
-
-        //ожидаем, когда все потоки выполнятся
         threads.forEach(thread -> {
             try {
                 thread.join();
@@ -35,20 +29,15 @@ public class Army {
                 throw new RuntimeException("Thread error");
             }
         });
-        //возвращаем общую силу армии
         return totalPower.get();
     }
 
     private Runnable createTask(List<Character> characters) {
         return () -> {
-            System.out.println("Start: " + Thread.currentThread().getName() + ": " + characters.get(0).getClass().getSimpleName());
-            //поток считает сумму сил его героев
             int localTotalPower = characters.stream()
                     .map(Character::getPower)
                     .reduce(Integer::sum)
                     .orElseThrow();
-            System.out.println("End: " + Thread.currentThread().getName() + ": " + characters.get(0).getClass().getSimpleName() + " -> Power: " + localTotalPower);
-            //добавляем значение к общей сумме
             totalPower.addAndGet(localTotalPower);
         };
     }
