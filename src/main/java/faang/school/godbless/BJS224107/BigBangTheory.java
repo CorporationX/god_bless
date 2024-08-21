575e2f7804beb26b2c73788592140d2e42f107da
aephrodita <146974539+aephrodita@users.noreply.github.com>
2024-08-22 01:09:06 +0500

    BJS2-24107

--- src/main/java/faang/school/godbless/BJS224107/BigBangTheory.java
+++ src/main/java/faang/school/godbless/BJS224107/BigBangTheory.java
@@ -4,6 +4,7 @@ import java.util.Arrays;
 import java.util.List;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
 
 public class BigBangTheory {
     public static void main(String[] args) {
@@ -16,12 +17,20 @@ public class BigBangTheory {
         ExecutorService executor = Executors.newFixedThreadPool(3);
 
         for (Task task : tasks) {
-            executor.submit(task);
+            executor.execute(task);
         }
 
         executor.shutdown();
 
-        while (!executor.isTerminated()) ;
+        try {
+            if (!executor.awaitTermination(5, TimeUnit.SECONDS)) {
+                System.out.println("Принудительное завершение");
+                executor.shutdownNow();
+            }
+        } catch (InterruptedException e) {
+            System.err.println("Завершение прервано");
+            executor.shutdownNow();
+        }
 
         System.out.println("All tasks have been completed.");
     }
