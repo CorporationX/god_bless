ab7beaa4ca2df0a9b9061509347b3a294f9b97b3
vestmen <alex.nemtsev95@gmail.com>
2024-08-21 11:35:26 +0300

    Распределенная армия героев меча и магии BJS2-23416 Сделал конструктор с помощью lombok в методе Unit

--- src/main/java/faang/school/godbless/sprint3/armyHeroes/Unit.java
+++ src/main/java/faang/school/godbless/sprint3/armyHeroes/Unit.java
@@ -2,16 +2,14 @@ package faang.school.godbless.sprint3.armyHeroes;
 
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
+import lombok.RequiredArgsConstructor;
 import lombok.Setter;
 
 @Getter
 @EqualsAndHashCode(exclude = "power")
+@RequiredArgsConstructor
 public abstract class Unit {
     @Setter
     private String name;
     private final int power;
-
-    public Unit(int power) {
-        this.power = power;
-    }
 }
