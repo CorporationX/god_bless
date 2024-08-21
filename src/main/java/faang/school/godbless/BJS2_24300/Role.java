4acb93d16b09f253475765d8fe9771038f39138b
Robert Oguzov <robertoguzov2@gmail.com>
2024-08-21 17:42:06 +0300

    Написал enum Role

---
+++ src/main/java/faang/school/godbless/BJS2_24300/Role.java
@@ -0,0 +1,29 @@
+package faang.school.godbless.BJS2_24300;
+
+public enum Role {
+    KNIGHT(2), MAGE(2), LORD(1), TECH_LEAD(3);
+
+    private int maxCount;
+    private int countAvailable;
+
+    Role(int maxCount) {
+        this.maxCount = maxCount;
+        this.countAvailable = maxCount;
+    }
+
+    public synchronized void takeRole() {
+        while (countAvailable == 0) {
+            try {
+                wait();
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+        }
+        countAvailable--;
+    }
+
+    public synchronized void releaseRole() {
+        countAvailable++;
+        notify();
+    }
+}
