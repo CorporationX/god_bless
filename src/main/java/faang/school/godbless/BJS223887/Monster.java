1d1bf78d57ec71afbeb8613b1cb3e997063591f0
aephrodita <146974539+aephrodita@users.noreply.github.com>
2024-08-21 16:22:49 +0500

    BJS2-23887

---
+++ src/main/java/faang/school/godbless/BJS223887/Monster.java
@@ -0,0 +1,31 @@
+package faang.school.godbless.BJS223887;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@AllArgsConstructor
+@Getter
+public class Monster {
+    private String name;
+    private String location;
+
+    public Location getLocationCoordinates() {
+        switch (this.location) {
+            case "Velen" -> {
+                return new Location(10, 20);
+            }
+            case "Toussaint" -> {
+                return new Location(40, 50);
+            }
+            case "White Orchard" -> {
+                return new Location(100, 10);
+            }
+            case "Skellige" -> {
+                return new Location(80, 120);
+            }
+            default -> {
+                return new Location(0, 0);
+            }
+        }
+    }
+}
