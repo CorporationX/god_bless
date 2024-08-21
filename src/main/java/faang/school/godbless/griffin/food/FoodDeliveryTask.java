67baf8ae234ede8dd11706cbf337d9b06722a1fd
Rashit Gareyev <gareyevrs@gmail.com>
2024-08-21 16:22:58 +0400

    nf BJS2-24165 awaitTermination added

--- src/main/java/faang/school/godbless/griffin/food/FoodDeliveryTask.java
+++ src/main/java/faang/school/godbless/griffin/food/FoodDeliveryTask.java
@@ -1,13 +1,21 @@
 package faang.school.godbless.griffin.food;
 
-import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.util.Random;
 
-@AllArgsConstructor
+@Slf4j
 public class FoodDeliveryTask implements Runnable {
-    private String character;
-    private int foodAmount;
+    private final Logger logger = LoggerFactory.getLogger(FoodDeliveryTask.class);
+    private final String character;
+    private final int foodAmount;
+
+    public FoodDeliveryTask(String character, int foodAmount) {
+        this.character = character;
+        this.foodAmount = foodAmount;
+    }
 
     @Override
     public void run() {
@@ -17,7 +25,7 @@ public class FoodDeliveryTask implements Runnable {
             int waitTime = new Random().nextInt(5) + 1;
             Thread.sleep(waitTime * 1000);
         } catch (InterruptedException exception) {
-            exception.printStackTrace();
+            logger.error(String.format("While %s was waiting for %s an error occurred", character, getFoodType()), exception);
         }
 
         System.out.printf("%s is eating food %s in amount: %d\n", character, getFoodType(), foodAmount);
--- src/main/java/faang/school/godbless/griffin/food/GriffinsFoodDelivery.java
+++ src/main/java/faang/school/godbless/griffin/food/GriffinsFoodDelivery.java
@@ -4,6 +4,7 @@ import java.util.Arrays;
 import java.util.Random;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
 
 public class GriffinsFoodDelivery {
     public static void main(String[] args) {
@@ -12,11 +13,21 @@ public class GriffinsFoodDelivery {
 
         ExecutorService executorService = Executors.newFixedThreadPool(3);
 
-        Arrays.stream(characterNames).forEach(name -> {
-            FoodDeliveryTask foodDeliveryTask = new FoodDeliveryTask(name, new Random().nextInt(49) + 1);
-            executorService.submit(foodDeliveryTask);
-        });
-
-        executorService.shutdown();
+        try {
+            Arrays.stream(characterNames).forEach(name -> {
+                FoodDeliveryTask foodDeliveryTask = new FoodDeliveryTask(name, new Random().nextInt(49) + 1);
+                executorService.submit(foodDeliveryTask);
+            });
+        } finally {
+            executorService.shutdown();
+            try {
+                if (!executorService.awaitTermination(5, TimeUnit.SECONDS)) {
+                    executorService.shutdownNow();
+                }
+            } catch (InterruptedException e) {
+                executorService.shutdownNow();
+                Thread.currentThread().interrupt();
+            }
+        }
     }
 }
