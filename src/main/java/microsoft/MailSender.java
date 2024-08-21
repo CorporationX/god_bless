51b9f178b3646b5d012922611195b51b99d40003
Ivan <spvanym@gmail.com>
2024-08-21 21:14:17 +0300

    SMI

--- src/main/java/microsoft/MailSender.java
+++ src/main/java/microsoft/MailSender.java
@@ -1,26 +1,24 @@
 package microsoft;
 
-import lombok.Data;
-import lombok.Getter;
-
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
-
-
 public class MailSender {
 
     public static void main(String[] args) {
         int startIndex = 0;
         int endindex = 200;
 
-        ExecutorService executorService = Executors.newFixedThreadPool(5);
         for (int i = 0; i < 5; i++) {
             SenderRunnable senderRunnable = new SenderRunnable(startIndex, endindex);
-            executorService.submit(senderRunnable);
+            Thread thread = new Thread(senderRunnable);
+            thread.start();
+            try {
+                thread.join();
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
             startIndex += 200;
             endindex += 200;
         }
-        executorService.shutdown();
+
     }
 
 }
