0dad9ec0c0835d15bb623ecf3821638fce740700
Robert Oguzov <robertoguzov2@gmail.com>
2024-08-21 11:14:34 +0300

    Внес правки

--- src/main/java/faang/school/godbless/BJS2_24020/GriffinsFoodDelivery.java
+++ src/main/java/faang/school/godbless/BJS2_24020/GriffinsFoodDelivery.java
@@ -4,14 +4,24 @@ import java.util.Arrays;
 import java.util.Random;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
 
 public class GriffinsFoodDelivery {
     public static void main(String[] args) {
         Random random = new Random();
         String[] characterNames = {"Peter", "Lois", "Meg", "Chris", "Stewie"};
         ExecutorService executor = Executors.newFixedThreadPool(3);
+
         Arrays.stream(characterNames)
                 .forEach(name -> executor.execute(new FoodDeliveryTask(name, random.nextInt(1, 50))));
+
         executor.shutdown();
+        try {
+            if (!executor.awaitTermination(5 * characterNames.length, TimeUnit.SECONDS)) {
+                executor.shutdownNow();
+            }
+        } catch (InterruptedException e) {
+            executor.shutdownNow();
+        }
     }
 }
