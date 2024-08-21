b6bdb1333bce5554be310e0f5f9644a46b23a7d2
Evgenii Shevchenko <evgeny.shevchenko@webware24.de>
2024-08-21 09:01:29 +0200

    BJS2-24039 - Накормите Питера Гриффина

---
+++ src/main/java/faang/school/godbless/sprint/three/griffins/Constants.java
@@ -0,0 +1,7 @@
+package faang.school.godbless.sprint.three.griffins;
+
+public class Constants {
+    public final static int THREADS_AMOUNT = 3;
+    public final static int[] DELIVERY_TIME = {1000, 2000, 3000, 4000, 5000};
+    public final static int SECOND_IN_MILLISECOND = 1000;
+ }
---
+++ src/main/java/faang/school/godbless/sprint/three/griffins/GriffinsFoodDelivery.java
@@ -0,0 +1,25 @@
+package faang.school.godbless.sprint.three.griffins;
+
+import java.util.Arrays;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+public class GriffinsFoodDelivery {
+    public static void main(String... args) {
+        ExecutorService executor = Executors.newFixedThreadPool(Constants.THREADS_AMOUNT);
+        String[] characterNames = {"Peter", "Lois", "Meg", "Chris", "Stewie"};
+
+        Arrays.stream(characterNames)
+                .forEach(character -> {
+                    executor.submit(new FoodDeliveryTask(character, getRandomFoodAmount()));
+                });
+
+        executor.shutdown();
+    }
+
+    public static int getRandomFoodAmount() {
+        final int maxFoodAmount = 7;
+        final int minFoodAmount = 1;
+        return (int) ((Math.random() * (maxFoodAmount - minFoodAmount)) + minFoodAmount);
+    }
+}
