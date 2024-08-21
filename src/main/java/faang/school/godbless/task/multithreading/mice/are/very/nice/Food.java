60433afff7bb80ca965aa8453afc4bc912635648
Sultan <nikowosuka@gmail.com>
2024-08-21 15:07:21 +0500

    refactoring, fix gatherFood method

--- src/main/java/faang/school/godbless/task/multithreading/mice/are/very/nice/Food.java
+++ src/main/java/faang/school/godbless/task/multithreading/mice/are/very/nice/Food.java
@@ -2,8 +2,10 @@ package faang.school.godbless.task.multithreading.mice.are.very.nice;
 
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
+import lombok.ToString;
 
 @Getter
+@ToString
 @RequiredArgsConstructor
 public class Food {
     private final String name;
--- src/main/java/faang/school/godbless/task/multithreading/mice/are/very/nice/House.java
+++ src/main/java/faang/school/godbless/task/multithreading/mice/are/very/nice/House.java
@@ -17,57 +17,79 @@ public class House {
     private static final int NUMBER_OF_ROOMS_PER_TIME = 2;
 
     private static final List<Room> rooms = new ArrayList<>();
-    private static final List<Food> foods = new ArrayList<>();
-
-    public static void gatherFood(int houseIndex) {
-        int limit = houseIndex + NUMBER_OF_ROOMS_PER_TIME;
-        IntStream.range(houseIndex, Math.min(limit, rooms.size()))
-                .forEach(i -> {
-                    var room = rooms.get(i);
-                    log.info("\nСбор еды из комнаты: %s".formatted(room.getName()));
-                    var roomFoods = room.getFoods();
-                    System.out.println("Собранная еда:");
-                    roomFoods.forEach(food -> System.out.println(food.getName()));
-                    foods.addAll(roomFoods);
-                    roomFoods.clear();
-                });
-    }
+    private static final List<Food> collectingFoods = new ArrayList<>();
+    private static final ScheduledExecutorService scheduler = Executors.newScheduledThreadPool(THREAD_POOL_LIMIT);
 
     public static void main(String[] args) {
-        var fds = List.of(
+        addRoomsToList();
+        printFoodsInRooms();
+        IntStream.iterate(0, i -> i < rooms.size(), i -> i += NUMBER_OF_ROOMS_PER_TIME)
+                .forEach(i -> scheduler.schedule(() ->
+                        gatherFood(getRoomsByRange(i)), i * THREAD_START_TIMER, TimeUnit.SECONDS));
+        scheduler.shutdown();
+        printResult(scheduler);
+    }
+
+    public static void gatherFood(List<Room> rooms) {
+        rooms.forEach(room -> {
+            log.info("\nСбор еды из комнаты: {}", room.getName());
+            var roomFoods = room.getFoods();
+            System.out.println("Собранная еда:");
+            roomFoods.forEach(food -> System.out.println(food.getName()));
+            collectingFoods.addAll(roomFoods);
+            roomFoods.clear();
+        });
+    }
+
+    private static List<Room> getRoomsByRange(int roomIndex) {
+        int limit = roomIndex + NUMBER_OF_ROOMS_PER_TIME;
+        return IntStream.range(roomIndex, Math.min(limit, rooms.size()))
+                .mapToObj(rooms::get)
+                .toList();
+    }
+
+    private static void printResult(ScheduledExecutorService scheduler) {
+        try {
+            if (scheduler.awaitTermination(GATHER_TIME_LIMIT, TimeUnit.MINUTES)) {
+                log.info("\nВся еда из дома собрана!");
+                printFoodsInRooms();
+            } else {
+                log.warn("Мыши не успели собрать всю едй за {} минут", GATHER_TIME_LIMIT);
+            }
+        } catch (InterruptedException exception) {
+            log.error("Interrupted exception: {}", exception.getMessage());
+        }
+    }
+
+    private static void printFoodsInRooms() {
+        rooms.forEach(room -> {
+            System.out.println("Список еды из комнаты %s: %s".formatted(room.getName(), room.getFoods()));
+        });
+    }
+
+    private static void addRoomsToList() {
+        var foods = getCollectingFoods();
+        rooms.addAll(List.of(
+                new Room("Прихожая 1", new ArrayList<>(foods)),
+                new Room("Спальня 1", new ArrayList<>(foods)),
+                new Room("Кухня 1", new ArrayList<>(foods)),
+                new Room("Ванная 1", new ArrayList<>(foods)),
+                new Room("Прихожая 2", new ArrayList<>(foods)),
+                new Room("Спальня 2", new ArrayList<>(foods)),
+                new Room("Гостиная 1", new ArrayList<>(foods)),
+                new Room("Гостиная 2", new ArrayList<>(foods)),
+                new Room("Спальня 3", new ArrayList<>(foods)),
+                new Room("Ванная 2", new ArrayList<>(foods)),
+                new Room("Кухня 2", new ArrayList<>(foods))
+        ));
+    }
+
+    private static List<Food> getCollectingFoods() {
+        return List.of(
                 new Food("Пицца"),
                 new Food("Сыр"),
                 new Food("Колбаса"),
                 new Food("Орехи")
         );
-        rooms.addAll(List.of(
-                new Room("Прихожая 1", new ArrayList<>(fds)),
-                new Room("Спальня 1", new ArrayList<>(fds)),
-                new Room("Кухня 1", new ArrayList<>(fds)),
-                new Room("Ванная 1", new ArrayList<>(fds)),
-                new Room("Прихожая 2", new ArrayList<>(fds)),
-                new Room("Спальня 2", new ArrayList<>(fds)),
-                new Room("Гостиная 1", new ArrayList<>(fds)),
-                new Room("Гостиная 2", new ArrayList<>(fds)),
-                new Room("Спальня 3", new ArrayList<>(fds)),
-                new Room("Ванная 2", new ArrayList<>(fds)),
-                new Room("Кухня 2", new ArrayList<>(fds))
-        ));
-        ScheduledExecutorService scheduler = Executors.newScheduledThreadPool(THREAD_POOL_LIMIT);
-        IntStream.iterate(0, i -> i < rooms.size(), i -> i += NUMBER_OF_ROOMS_PER_TIME)
-                .forEach(i -> {
-                    scheduler.schedule(() -> gatherFood(i), i * THREAD_START_TIMER, TimeUnit.SECONDS);
-                });
-        scheduler.shutdown();
-        try {
-            if (scheduler.awaitTermination(GATHER_TIME_LIMIT, TimeUnit.MINUTES)) {
-                log.info("\nВся еда из дома собрана!");
-                System.out.println(foods);
-            } else {
-                log.warn("Мыши не успели собрать всю едй за %s минут".formatted(GATHER_TIME_LIMIT));
-            }
-        } catch (InterruptedException exception) {
-            log.error("Interrupted exception: {}", exception.getMessage());
-        }
     }
 }
