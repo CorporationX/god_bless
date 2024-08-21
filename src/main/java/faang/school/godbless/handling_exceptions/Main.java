46da4b8281293b8a8a13a96a0df9e04ff03f9140
Varvara Stepnova <stepnova.varya@yandex.ru>
2024-08-21 06:44:09 +0300

    BJS2-21141-handling-exceptions: added method main

--- src/main/java/faang/school/godbless/handling_exceptions/Main.java
+++ src/main/java/faang/school/godbless/handling_exceptions/Main.java
@@ -4,6 +4,12 @@ import java.util.function.Supplier;
 
 public class Main {
 
+    public static void main(String[] args) {
+        String name = "Varya";
+
+        System.out.println(withErrorHandling(name::toUpperCase, Exception::getMessage));
+    }
+
     public static <T> T withErrorHandling(Supplier<T> action, ExceptionHandler<T> onError) {
         try {
             return action.get();
