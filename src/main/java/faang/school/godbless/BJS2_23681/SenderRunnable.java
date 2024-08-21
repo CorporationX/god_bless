4fd34e4eb42a8bb4ff1ee51bd244a5edb7870cb5
avoevoda <voevodacode@gmail.com>
2024-08-21 07:23:20 +0300

    Atomic loopCounter, fix MailSender

--- src/main/java/faang/school/godbless/BJS2_23681/SenderRunnable.java
+++ src/main/java/faang/school/godbless/BJS2_23681/SenderRunnable.java
@@ -3,20 +3,21 @@ package faang.school.godbless.BJS2_23681;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 
+import java.util.concurrent.atomic.AtomicInteger;
+
 @RequiredArgsConstructor
 public class SenderRunnable implements Runnable {
+    @Getter
+    private static final AtomicInteger loopCounter = new AtomicInteger(0);
     private static final String SUCCESS_MESSAGE = "Письмо под номером %s успешно отправлено.%n";
     private final int startIndex;
     private final int endIndex;
-
-    @Getter
-    private static int loopCounter = 0;
 
     @Override
     public void run() {
         for (int i = startIndex; i <= endIndex; i++) {
             System.out.printf(SUCCESS_MESSAGE, i);
-            loopCounter++;
+            loopCounter.incrementAndGet();
         }
     }
 }
