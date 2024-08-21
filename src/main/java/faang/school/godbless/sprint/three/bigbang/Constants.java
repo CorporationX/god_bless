ef95ce78adc6cc39a861a97cbc53919a5ff4548f
Evgenii Shevchenko <evgeny.shevchenko@webware24.de>
2024-08-21 19:27:18 +0200

    BJS2-24071 - The Big Bang Theory

---
+++ src/main/java/faang/school/godbless/sprint/three/bigbang/Constants.java
@@ -0,0 +1,7 @@
+package faang.school.godbless.sprint.three.bigbang;
+
+public class Constants {
+    public final static int ACTIVE_THREADS = 4;
+    public final static int PERFORM_TASK = 2000;
+    public final static int TASK_TIMEOUT = 500;
+}
