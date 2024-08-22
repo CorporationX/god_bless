5d25f41f13c2f8c4c24e84bcc8c8b629df17a21b
Nikita Korolev <nk25.dev@gmail.com>
2024-08-22 10:31:54 +0300

    Task implementation.

---
+++ src/main/java/faang/school/godbless/nice_but_poor/Chore.java
@@ -0,0 +1,20 @@
+package faang.school.godbless.nice_but_poor;
+
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
+public class Chore implements Runnable {
+    private final String chore;
+
+    @Override
+    public void run() {
+        String currentThreadName = Thread.currentThread().getName();
+        System.out.printf("%s working with chore: %s%n", currentThreadName, chore);
+        try {
+            Thread.sleep(5000);
+        } catch(InterruptedException e) {
+            System.out.printf("%s with task %s interrupted!", currentThreadName, chore);
+            e.printStackTrace();
+        }
+    }
+}
