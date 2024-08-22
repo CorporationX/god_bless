4ff75d0518d449044a9eddc5069fde7aa74eca22
pgusev <pgusev@fil-it.ru>
2024-08-22 14:17:41 +0300

    BJS2-23558 - потерял модификатор

--- src/main/java/faang/school/godbless/microsoft/MailSender.java
+++ src/main/java/faang/school/godbless/microsoft/MailSender.java
@@ -5,17 +5,17 @@ public class MailSender {
     private static final int TWO_HUNDRED = 200;
     private static final int THREAD_COUNT = 5;
 
-    static Thread[] threads = new Thread[THREAD_COUNT];
+    private static final Thread[] THREADS = new Thread[THREAD_COUNT];
 
     public static void main(String[] args) throws InterruptedException {
         for (int i = 0; i < THREAD_COUNT; i++) {
             SenderRunnable sender = new SenderRunnable(i * TWO_HUNDRED + 1,
                     i * TWO_HUNDRED + TWO_HUNDRED);
-            threads[i] = new Thread(sender);
-            threads[i].start();
+            THREADS[i] = new Thread(sender);
+            THREADS[i].start();
         }
 
-        for (Thread thread : threads) {
+        for (Thread thread : THREADS) {
             thread.join();
             System.out.println(thread.getName() + " finished ");
         }
