44df416c53bd8fddb52740ea93307a05eea6294f
Rashit Gareyev <gareyevrs@gmail.com>
2024-08-22 12:18:45 +0400

    nf BJS2-23724 refactoring

--- src/main/java/faang/school/godbless/microsoft/outlook/MainSender.java
+++ src/main/java/faang/school/godbless/microsoft/outlook/MainSender.java
@@ -1,23 +1,25 @@
 package faang.school.godbless.microsoft.outlook;
 
 import lombok.extern.slf4j.Slf4j;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 import java.util.List;
 
 @Slf4j
 public class MainSender {
-    private static final Logger logger = LoggerFactory.getLogger(MainSender.class);
+    private static final int[] RANGE_1 = {0, 200};
+    private static final int[] RANGE_2 = {200, 400};
+    private static final int[] RANGE_3 = {400, 600};
+    private static final int[] RANGE_4 = {600, 800};
+    private static final int[] RANGE_5 = {800, 1000};
 
     public static void main(String[] args) {
 
         List<Thread> threads = List.of(
-                new Thread(new SenderRunnable(0, 200)),
-                new Thread(new SenderRunnable(200, 400)),
-                new Thread(new SenderRunnable(400, 600)),
-                new Thread(new SenderRunnable(600, 800)),
-                new Thread(new SenderRunnable(800, 1000)));
+                new Thread(new SenderRunnable(RANGE_1[0], RANGE_1[1])),
+                new Thread(new SenderRunnable(RANGE_2[0], RANGE_2[1])),
+                new Thread(new SenderRunnable(RANGE_3[0], RANGE_3[1])),
+                new Thread(new SenderRunnable(RANGE_4[0], RANGE_4[1])),
+                new Thread(new SenderRunnable(RANGE_5[0], RANGE_5[1])));
 
         threads.forEach(Thread::start);
 
@@ -27,7 +29,7 @@ public class MainSender {
             }
             System.out.println("All messages have just sent");
         } catch (InterruptedException exception) {
-            logger.error("An error occurred while sending the emails", exception);
+            log.error("An error occurred while sending the emails", exception);
         }
     }
 }
