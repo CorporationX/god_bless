4c08e1de934c907dae7df2118c180dcf865c353b
Evgenii Shevchenko <evgeny.shevchenko@webware24.de>
2024-08-22 19:52:10 +0200

    BJS2-23502 - Турнир в Королевской гавани

---
+++ src/main/java/faang/school/godbless/sprint/three/tournament/Knight.java
@@ -0,0 +1,16 @@
+package faang.school.godbless.sprint.three.tournament;
+
+import lombok.NonNull;
+import lombok.RequiredArgsConstructor;
+
+import java.util.List;
+
+@RequiredArgsConstructor
+public class Knight {
+    @NonNull
+    private final String name;
+    private List<Trial> trials;
+
+
+
+}
---
+++ src/main/java/faang/school/godbless/sprint/three/tournament/Trial.java
@@ -0,0 +1,6 @@
+package faang.school.godbless.sprint.three.tournament;
+
+public class Trial {
+    private String knightName;
+    private String trialName;
+}
