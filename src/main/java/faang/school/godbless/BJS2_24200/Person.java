5dc2c3dfb2bac7f82c45c63cc444fbb98151e3d7
Robert Oguzov <robertoguzov2@gmail.com>
2024-08-22 17:09:35 +0300

    Написал class Person

---
+++ src/main/java/faang/school/godbless/BJS2_24200/Person.java
@@ -0,0 +1,13 @@
+package faang.school.godbless.BJS2_24200;
+
+import lombok.AllArgsConstructor;
+import lombok.ToString;
+
+@ToString
+@AllArgsConstructor
+public class Person {
+    private String name;
+    private String surname;
+    private int age;
+    private String workplace;
+}
