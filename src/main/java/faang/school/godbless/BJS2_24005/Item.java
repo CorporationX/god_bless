18d855eeb32b859be023545ccbdc3e260df87d27
Robert Oguzov <robertoguzov2@gmail.com>
2024-08-22 17:59:27 +0300

    Написал class Item

---
+++ src/main/java/faang/school/godbless/BJS2_24005/Item.java
@@ -0,0 +1,13 @@
+package faang.school.godbless.BJS2_24005;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.ToString;
+
+@Getter
+@ToString
+@AllArgsConstructor
+public class Item {
+    private String itemName;
+    private int price;
+}
