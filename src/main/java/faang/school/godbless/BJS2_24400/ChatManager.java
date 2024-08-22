1d747331389a3425377936d756148c29f5d54ea4
Robert Oguzov <robertoguzov2@gmail.com>
2024-08-22 12:18:20 +0300

    Написал class ChatManager

---
+++ src/main/java/faang/school/godbless/BJS2_24400/ChatManager.java
@@ -0,0 +1,65 @@
+package faang.school.godbless.BJS2_24400;
+
+import lombok.NonNull;
+
+import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.LinkedBlockingQueue;
+import java.util.concurrent.TimeUnit;
+
+public class ChatManager {
+    private BlockingQueue<User> queue = new LinkedBlockingQueue<>();
+    private static ExecutorService executor = Executors.newCachedThreadPool();
+
+    public void start() {
+        System.out.println("Начать работу");
+        while (true) {
+            try {
+                User user1 = queue.poll(10, TimeUnit.SECONDS);
+                User user2 = queue.poll(10, TimeUnit.SECONDS);
+
+                if (user1 != null && user2 != null) {
+                    createChat(user1, user2);
+                } else {
+                    stopExecutorService(user1);
+                    break;
+                }
+            } catch (InterruptedException e) {
+                Thread.currentThread().interrupt();
+                break;
+            }
+        }
+    }
+
+    private void createChat(User user1, User user2) {
+        executor.execute(() -> {
+            Chat chat = new Chat(user1, user2);
+            chat.communicate();
+        });
+    }
+
+    private void stopExecutorService(User remainingUser) {
+        System.out.println("Прекращаем работу!");
+        if (remainingUser != null) {
+            System.out.println(remainingUser + " извините, у нас обед");
+        }
+
+        executor.shutdown();
+        try {
+            if (!executor.awaitTermination(3, TimeUnit.SECONDS)) {
+                executor.shutdownNow();
+            }
+        } catch (InterruptedException e) {
+            executor.shutdownNow();
+        }
+    }
+
+    public void startChat(@NonNull User user) {
+        try {
+            queue.put(user);
+        } catch (InterruptedException e) {
+            Thread.currentThread().interrupt();
+        }
+    }
+}
