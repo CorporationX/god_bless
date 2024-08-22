18d7d5d3e8c20c97ece7bfaed971a7e49ebed209
SharkB38 <val.wolf.ne@gmail.com>
2024-08-23 05:49:17 +0900

    fix

--- src/main/java/supercow/Boss.java
+++ src/main/java/supercow/Boss.java
@@ -2,16 +2,19 @@ package supercow;
 
 import lombok.NonNull;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Random;
+import java.util.concurrent.ThreadLocalRandom;
 
+@Slf4j
 @RequiredArgsConstructor
 public class Boss {
     @NonNull
     private final Object lock;
-    private static final Random RANDOM = new Random();
+    private static final Random RANDOM = ThreadLocalRandom.current();
     private final int maxPlayers;
     @NonNull
     private final Map<Integer, Player> currentPlayers = new HashMap<>();
@@ -21,7 +24,15 @@ public class Boss {
             if (currentPlayers.size() < maxPlayers) {
                 addPlayer(player);
             } else {
-                waitForBattleSlot(player);
+                try {
+                    System.out.println("No available slot in battle, please wait...");
+                    while (maxPlayers <= currentPlayers.size()) {
+                        lock.wait();
+                    }
+                    addPlayer(player);
+                } catch (InterruptedException e) {
+                    System.out.println("Server error: " + e.getMessage());
+                }
             }
         }
         processPlayerBattle(player);
@@ -39,28 +50,16 @@ public class Boss {
         printCountOfCurrentPlayers();
     }
 
-    private void waitForBattleSlot(@NonNull Player player) {
-        synchronized (lock) {
-            try {
-                System.out.println("No available slot in battle, please wait...");
-                lock.wait();
-                addPlayer(player);
-            } catch (InterruptedException e) {
-                System.out.println("Server error: " + e.getMessage());
-            }
-        }
-    }
-
     private void processPlayerBattle(@NonNull Player player) {
         try {
             Thread.sleep(1000L * (RANDOM.nextInt(4) + 1));
             synchronized (lock) {
                 removePlayer(player);
-                lock.notify();
+                lock.notifyAll();
             }
         } catch (InterruptedException e) {
             System.out.println("Connection lost");
-            throw new RuntimeException(e);
+            Thread.currentThread().interrupt();
         }
     }
 
