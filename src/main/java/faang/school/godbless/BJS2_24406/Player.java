a877c61018baa50a90f3fe37d3e6b3cd52dcf89e
Peregruzochka <peregruzochka48@mail.ru>
2024-08-22 13:57:25 +0300

    music control

---
+++ src/main/java/faang/school/godbless/BJS2_24406/Player.java
@@ -0,0 +1,41 @@
+package faang.school.godbless.BJS2_24406;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class Player {
+    private final Object lock = new Object();
+    private boolean isPlaying;
+
+    public void play() {
+        synchronized (lock) {
+            isPlaying = true;
+            System.out.println("Play! -> isPlaying: " + isPlaying);
+        }
+    }
+
+    public void pause() {
+        synchronized (lock) {
+            isPlaying = false;
+            System.out.println("Pause -> isPlaying: " + isPlaying);
+        }
+
+    }
+
+    public void skip() {
+        synchronized (lock) {
+            isPlaying = true;
+            System.out.println("Skip -> isPlaying: " + isPlaying);
+        }
+    }
+
+    public void previous() {
+        synchronized (lock) {
+            isPlaying = true;
+            System.out.println("Previous -> isPlaying: " + isPlaying);
+        }
+    }
+
+}
