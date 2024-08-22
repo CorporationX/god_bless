f601caed1635a44d9606a45bf39fb42b65dd977f
Ivan <spvanym@gmail.com>
2024-08-23 01:15:52 +0300

    Распределенная армия героев меча и магии SMI - правки

--- src/main/java/heroAndMagic/Army.java
+++ src/main/java/heroAndMagic/Army.java
@@ -16,16 +16,26 @@ public class Army {
     public static int calculateTotalPower() {
         int result = 0;
 
+        List<Thread> threads = new ArrayList<>();
         for (int i = 0; i < 4; i++) {
             Thread thread = new Thread((Runnable) armyList.get(i));
+            threads.add(thread);
             thread.start();
+        }
+
+        for (Thread thread : threads) {
             try {
                 thread.join();
             } catch (InterruptedException e) {
                 throw new RuntimeException(e);
             }
+        }
+
+        for (int i = 0; i < 4; i++) {
             result += armyList.get(i).getTotal();
         }
+
+
         return result;
     }
 
