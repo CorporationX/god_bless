88d9f84485cdaf05f9230d16d9a2aa33242967e6
Okabe <mathew.mackonoha@gmail.com>
2024-08-23 11:30:40 +0300

    Commiting "Weasley Family" task 2

--- src/main/java/faang/school/godbless/Poor_things/WeasleyFamily.java
+++ src/main/java/faang/school/godbless/Poor_things/WeasleyFamily.java
@@ -2,6 +2,7 @@ package faang.school.godbless.Poor_things;
 
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
 
 public class WeasleyFamily {
     public static void main(String[] args) {
@@ -9,9 +10,16 @@ public class WeasleyFamily {
         ExecutorService executor = Executors.newCachedThreadPool();
         for (int i = 0; i < chores.length; i++) {
             Chore chore = new Chore(chores[i]);
-            executor.submit(chore);
+            executor.execute(chore);
             System.out.println(chores[i] + " is done");
         }
         executor.shutdown();
+        try {
+            if (executor.awaitTermination(800, TimeUnit.MILLISECONDS)) {
+                executor.shutdownNow();
+            }
+        } catch (InterruptedException e) {
+            executor.shutdownNow();
+        }
     }
 }
