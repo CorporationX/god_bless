c7bfd140e8c7c12573a81222388308ca87d16024
Bobiktrans <Bobicktrnas@yandex.ru>
2024-08-23 15:04:57 +0300

    BJS2-24137

--- src/main/java/faang/school/godbless/Application.java
+++ src/main/java/faang/school/godbless/Application.java
@@ -1,7 +1,19 @@
 package faang.school.godbless;
 
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ThreadLocalRandom;
+
 public class Application {
     public static void main(String[] args) {
-        System.out.println("God Bless!");
+        ExecutorService executorService = Executors.newFixedThreadPool(3);
+
+        String[] characterNames = {"Питер", "Лоис", "Мэг", "Крис", "Стьюи"};
+
+         for (String character : characterNames) {
+            int foodAmount = ThreadLocalRandom.current().nextInt(10, 101);
+            executorService.submit(new FoodDeliveryTask(character, foodAmount));
+        }
+        executorService.shutdown();
     }
 }
