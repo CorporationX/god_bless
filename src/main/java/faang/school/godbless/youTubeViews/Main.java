69bd33507bb65447fc082694f18c0accdd901636
Alena Guskova <amguskova@edu.hse.ru>
2024-08-23 22:35:20 +0300

    Исправления после ревью

--- src/main/java/faang/school/godbless/youTubeViews/Main.java
+++ src/main/java/faang/school/godbless/youTubeViews/Main.java
@@ -1,13 +1,12 @@
 package faang.school.godbless.youTubeViews;
 
-import java.util.Map;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.TimeUnit;
 
 public class Main {
     private static final int NUM_THREADS = 100;
-    private static final int NUM_VIDEOS = 50;
+    private static final int NUM_VIDEOS = 20;
 
     public static void main(String[] args) {
 
@@ -15,17 +14,17 @@ public class Main {
 
         ExecutorService executorService = Executors.newFixedThreadPool(NUM_THREADS);
 
-        for (int i = 1; i <= NUM_VIDEOS; i++) {
-            videoManager.addVideo(Integer.toString(i));
-        }
+        for (int i = 0; i < NUM_VIDEOS; i++) {
+            String videoId =  Integer.toString(i+1);
 
-        for (Map.Entry<String, Integer> entry : videoManager.getViewsMap().entrySet()) {
-            executorService.submit(() -> {
-                videoManager.addView(entry.getKey());
-                videoManager.getViewCount(entry.getKey()).ifPresentOrElse(
-                        (viewCount) -> System.out.println("Video with id: " + entry.getKey() + " has " + viewCount + " views"),
-                        () -> System.out.println("Video with id: " + entry.getKey() + "not found"));
-            });
+            for (int j = 0; j < NUM_THREADS / NUM_VIDEOS; j++) {
+                executorService.submit(() -> {
+                    videoManager.addView(videoId);
+                    videoManager.getViewCount(videoId).ifPresentOrElse(
+                            (viewCount) -> System.out.println("Video id: " + videoId + ", Views: " + viewCount),
+                            () -> System.out.println("Video with id: " + videoId + " not found"));
+                });
+            }
         }
 
         executorService.shutdown();
--- src/main/java/faang/school/godbless/youTubeViews/VideoManager.java
+++ src/main/java/faang/school/godbless/youTubeViews/VideoManager.java
@@ -11,23 +11,10 @@ public class VideoManager {
     private final Map<String, Integer> viewsMap = new HashMap<>();
 
     public synchronized void addView (String videoId) {
-        if (viewsMap.containsKey(videoId)) {
-            viewsMap.put(videoId, viewsMap.get(videoId) + 1);
-            System.out.println("A view added to video with id " + videoId);
-        } else {
-            System.out.println("Video with id " + videoId + " not found");
-        }
+        viewsMap.put(videoId, viewsMap.getOrDefault(videoId, 0) + 1);
     }
 
-    public Optional<Integer> getViewCount (String videoId) {
+    public synchronized Optional<Integer> getViewCount (String videoId) {
         return Optional.ofNullable(viewsMap.get(videoId));
-    }
-
-    public void addVideo (String videoId) {
-        if (!viewsMap.containsKey(videoId)) {
-            viewsMap.put(videoId, 0);
-        } else {
-            System.out.println("Video with id: " + videoId + " already exists");
-        }
     }
 }
