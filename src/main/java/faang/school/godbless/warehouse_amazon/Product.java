498ced5b7273495cd530557c5f2b863f31fb4ddc
Varvara Stepnova <stepnova.varya@yandex.ru>
2024-08-23 20:35:47 +0300

    BJS2-19359-warehouse_amazon: fix

--- src/main/java/faang/school/godbless/warehouse_amazon/Product.java
+++ src/main/java/faang/school/godbless/warehouse_amazon/Product.java
@@ -1,12 +1,14 @@
 package faang.school.godbless.warehouse_amazon;
 
 import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.ToString;
 
 @AllArgsConstructor
 @Getter
 @ToString
+@EqualsAndHashCode
 public class Product {
     private int id;
     private String name;
