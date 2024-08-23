8fcee7109e987bbd880491a93499d5ced224d800
Varvara Stepnova <stepnova.varya@yandex.ru>
2024-08-23 20:52:46 +0300

    BJS2-22119-spam: fix

--- src/main/java/faang/school/godbless/spam/MessageProcessor.java
+++ src/main/java/faang/school/godbless/spam/MessageProcessor.java
@@ -7,12 +7,12 @@ public class MessageProcessor {
     public boolean processMessage(String message, List<MessageFilter> messageFilterList) {
         if (message == null || message.isBlank()) {
             System.out.println("Сообщение не может быть пустым!");
+            throw new NullPointerException();
         }
 
         boolean isFiltered = false;
         for (MessageFilter messageFilter : messageFilterList) {
-            isFiltered = messageFilter.filter(message); {
-            }
+            isFiltered = messageFilter.filter(message);
         }
         return isFiltered;
     }
