e20d0b8d26f34c74050937e0d43cc7e36b18b279
Ruslan Iovlev <ruslan-iovlev@mail.ru>
2024-08-23 13:23:10 +0700

    фильтруем спам доработка

--- src/main/java/faang/school/godbless/BJS2_22151/MessageFilter.java
+++ src/main/java/faang/school/godbless/BJS2_22151/MessageFilter.java
@@ -3,5 +3,5 @@ package faang.school.godbless.BJS2_22151;
 @FunctionalInterface
 public interface MessageFilter {
 
-    Boolean filter(String message);
+    boolean filter(String message);
 }
--- src/main/java/faang/school/godbless/BJS2_22151/MessageProcessor.java
+++ src/main/java/faang/school/godbless/BJS2_22151/MessageProcessor.java
@@ -4,9 +4,9 @@ import java.util.List;
 
 public class MessageProcessor {
 
-    public boolean processMessage(String message, List<MessageFilter> filters){
-        for (MessageFilter messageFilter : filters){
-            if (!messageFilter.filter(message)){
+    public boolean processMessage(String message, List<MessageFilter> filters) {
+        for (MessageFilter messageFilter : filters) {
+            if (!messageFilter.filter(message)) {
                 return false;
             }
         }
