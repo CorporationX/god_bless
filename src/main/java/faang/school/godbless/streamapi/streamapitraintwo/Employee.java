6d41fbb4b6a84b7b7c70e9bd1a0bfe9dc7b2411e
it-mikhail <vulp@yandex.ru>
2024-08-24 00:29:20 +0300

    Stream Api 2

---
+++ src/main/java/faang/school/godbless/streamapi/streamapitraintwo/Employee.java
@@ -0,0 +1,13 @@
+package faang.school.godbless.streamapi.streamapitraintwo;
+
+import lombok.Data;
+
+@Data
+public class Employee {
+
+    private final String name;
+
+    private final String department;
+    private final double salary;
+
+}
