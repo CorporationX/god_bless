05630b853b582ae00a0e8b1e32bfeef67793c6be
Ilia Chudov <chudow-iluha@mail.ru>
2024-08-23 10:09:12 +0300

    Созданы классы: Task, BigBangTheory с демонстрацией работы.

---
+++ src/main/java/faang/school/BJS2_24344/Task.java
@@ -0,0 +1,25 @@
+package faang.school.BJS2_24344;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+public class Task implements Runnable {
+    private String name;
+    private String task;
+
+    @Override
+    public void run() {
+        try {
+            System.out.println(getName() + " начал выполнять задание: " + getTask());
+            Thread.sleep(2000);
+            System.out.println(getName() + " выполнил своё задание!");
+        } catch (InterruptedException e) {
+            System.out.println("Задача прервалась!");
+            Thread.currentThread().interrupt();
+        }
+    }
+}
