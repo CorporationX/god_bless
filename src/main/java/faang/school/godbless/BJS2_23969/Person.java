60d7aee00e7b30451dc969d8937e86fff6fb8a2a
Arstan Subanov <arstansubanov@gmail.com>
2024-08-23 20:59:57 +0600

    Помогите коллеге

---
+++ src/main/java/faang/school/godbless/BJS2_23969/Person.java
@@ -0,0 +1,14 @@
+package faang.school.godbless.BJS2_23969;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
+@Getter
+public class Person {
+
+    private final String name;
+    private final String surname;
+    private final int age;
+    private final String workPlace;
+}
