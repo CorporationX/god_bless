32c9a09d4865beb630542f5b05606ee8a5abd925
zeus <denis@fatum.ru>
2024-08-23 16:49:40 +0300

    BJS2-24095 complete

--- src/main/java/faang/school/godbless/BJS2_24095/Bro.java
+++ src/main/java/faang/school/godbless/BJS2_24095/Bro.java
@@ -1,14 +1,29 @@
 package faang.school.godbless.BJS2_24095;
 
-import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
 
-@AllArgsConstructor
+import java.util.Random;
+
 @Getter
 @Setter
 public class Bro {
     private int lives;
-    private boolean isAlive = true;
+    private int score = 0;
     private String name;
+    private boolean alive = true;
+
+    public Bro(int lives, String name) {
+        this.lives = lives;
+        this.name = name;
+    }
+
+    public boolean isAlive() {
+        return alive;
+    }
+
+    public boolean getAlive() {
+        Random random = new Random();
+        return random.nextBoolean();
+    }
 }
--- src/main/java/faang/school/godbless/BJS2_24095/Game.java
+++ src/main/java/faang/school/godbless/BJS2_24095/Game.java
@@ -1,25 +1,58 @@
 package faang.school.godbless.BJS2_24095;
 
+import lombok.Getter;
+
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Random;
 
 public class Game {
+    Random rand = new Random();
     private int score;
     private int lives;
-
     private Object scoreLock = new Object();
     private Object livesLock = new Object();
-    private List<Bro> bros;
-
-    public void update() {
-
-    }
+    @Getter
+    private List<Bro> bros = new ArrayList<>();
 
     public void addBro(Bro bro) {
         bros.add(bro);
     }
 
-    private void gameOver() {
-
+    public boolean update() {
+        int broIndex = rand.nextInt(bros.size());
+        Bro bro = bros.get(broIndex);
+        boolean isAlive = bro.getAlive();
+        if (!isAlive) {
+            synchronized (livesLock) {
+                bro.setLives(bro.getLives() - 1);
+                lives++;
+                System.out.println(bro.getName() + " current lives = " + bro.getLives());
+                if (bro.getLives() == 0) {
+                    bro.setAlive(false);
+                    return gameOver();
+                }
+            }
+        } else {
+            synchronized (scoreLock) {
+                bro.setScore(bro.getScore() + 1);
+                score++;
+            }
+        }
+        return false;
     }
 
+
+    private boolean gameOver() {
+        System.out.println("Game over with max score: " + score);
+        System.out.println("Lost lives: " + lives);
+        return true;
+    }
+
+    public boolean anyOneDead() {
+        long count = bros.stream()
+                .filter(bro -> !bro.isAlive())
+                .count();
+        return count > 0;
+    }
 }
