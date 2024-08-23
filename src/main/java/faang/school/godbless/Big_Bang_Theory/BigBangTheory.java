a656d1d6cb6cacffa30710b82579abd3facb17c0
Okabe <mathew.mackonoha@gmail.com>
2024-08-23 11:37:05 +0300

    Commiting "Big Bang Theory" task 2

--- src/main/java/faang/school/godbless/Big_Bang_Theory/BigBangTheory.java
+++ src/main/java/faang/school/godbless/Big_Bang_Theory/BigBangTheory.java
@@ -6,20 +6,24 @@ import java.util.concurrent.TimeUnit;
 
 public class BigBangTheory {
     public static void main(String[] args) {
+
+        ExecutorService executor = Executors.newFixedThreadPool(4);
+        Task task1 = new Task("Groceries", "Buy some products");
+        Task task2 = new Task("Gaming", "Spend 8 hour on Dark Souls");
+        Task task3 = new Task("Health care", "Buy some medicine");
+        Task task4 = new Task("Work", "Made o lot of work");
+        executor.execute(task1);
+        executor.execute(task2);
+        executor.execute(task3);
+        executor.execute(task4);
+        executor.shutdown();
         try {
-            ExecutorService executor = Executors.newFixedThreadPool(4);
-            Task task1 = new Task("Groceries", "Buy some products");
-            Task task2 = new Task("Gaming", "Spend 8 hour on Dark Souls");
-            Task task3 = new Task("Health care", "Buy some medicine");
-            Task task4 = new Task("Work", "Made o lot of work");
-            executor.submit(task1);
-            executor.submit(task2);
-            executor.submit(task3);
-            executor.submit(task4);
-            executor.shutdown();
-            executor.awaitTermination(1, TimeUnit.MINUTES);
+            if (!executor.awaitTermination(800, TimeUnit.MILLISECONDS)) {
+                executor.shutdownNow();
+            }
         } catch (InterruptedException e) {
-            e.printStackTrace();
+            executor.shutdownNow();
         }
+
     }
 }
