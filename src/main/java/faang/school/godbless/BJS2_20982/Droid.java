8db0861fe5be50a4970cf2d80e59318bd9149ee3
Ruslan Iovlev <ruslan-iovlev@mail.ru>
2024-08-23 15:31:07 +0700

    Секреты дроидов доработка

--- src/main/java/faang/school/godbless/BJS2_20982/Droid.java
+++ src/main/java/faang/school/godbless/BJS2_20982/Droid.java
@@ -8,37 +8,27 @@ public class Droid {
     private String message;
     private int key;
 
-    public String sendEncryptedMessage(String message, Integer key){
-        DroidMessageEncryptor encryptor = (msg, k) -> {
-            StringBuilder encrypted = new StringBuilder();
-            for (char ch : msg.toCharArray()) {
-                if (Character.isLetter(ch)) {
-                    char base = Character.isLowerCase(ch) ? 'a' : 'A';
-                    char encryptedChar = (char) ((ch - base + k) % 26 + base);
-                    encrypted.append(encryptedChar);
-                } else {
-                    encrypted.append(ch);
-                }
-            }
-            return encrypted.toString();
-        };
+    public String sendEncryptedMessage(String message, Integer key) {
+        DroidMessageEncryptor encryptor = this::shiftMessage;
         return encryptor.encrypt(message, key);
     }
 
-    public String receiveEncryptedMessage(String encryptedMsg, Integer key){
-        DroidMessageEncryptor decryptor = (msg, k) -> {
-            StringBuilder decrypted = new StringBuilder();
-            for (char ch : msg.toCharArray()) {
-                if (Character.isLetter(ch)) {
-                    char base = Character.isLowerCase(ch) ? 'a' : 'A';
-                    char decryptedChar = (char) ((ch - base - k + 26) % 26 + base);
-                    decrypted.append(decryptedChar);
-                } else {
-                    decrypted.append(ch);
-                }
-            }
-            return decrypted.toString();
-        };
+    public String receiveEncryptedMessage(String encryptedMsg, Integer key) {
+        DroidMessageEncryptor decryptor = (msg, k) -> shiftMessage(msg, -k);
+        ;
         return decryptor.encrypt(encryptedMsg, key);
+    }
+
+    private String shiftMessage(String message, int key) {
+        StringBuilder result = new StringBuilder();
+        for (char ch : message.toCharArray()) {
+            char shiftedChar = ch;
+            if (Character.isLetter(ch)) {
+                char base = Character.isUpperCase(ch) ? 'A' : 'a';
+                shiftedChar = (char) ((ch - base + key + 26) % 26 + base);
+            }
+            result.append(shiftedChar);
+        }
+        return result.toString();
     }
 }
