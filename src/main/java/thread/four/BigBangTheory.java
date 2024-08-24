37e872ca86bd49eb181a775fe7fb6d11b3deeaa5
Maksim Kononov <07maks7@mail.ru>
2024-08-24 12:50:57 +0300

    Решение задачи The Big Bang Theory

---
+++ src/main/java/thread/four/BigBangTheory.java
@@ -0,0 +1,35 @@
+package thread.four;
+
+import java.util.List;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
+import java.util.stream.Stream;
+
+public class BigBangTheory {
+
+    public static void main(String[] args) {
+
+        int threadCount = 4;
+        ExecutorService executor = Executors.newFixedThreadPool(threadCount);
+        List<Task> tasks = Stream.of(new Task("Шелдон", "подготовка теории"),
+                        new Task("Леонард", "моделирование эксперимента"),
+                        new Task("Говард ", "разработка инструментов"),
+                        new Task("Раджеш ", "анализ данных"))
+                .toList();
+
+        tasks.forEach(executor::execute);
+        executor.shutdown();
+
+        try {
+            if (!executor.awaitTermination(5, TimeUnit.MINUTES)) {
+                executor.shutdownNow();
+            }
+        } catch (InterruptedException e) {
+            executor.shutdownNow();
+        }
+
+        System.out.println("Все задания выполнены.");
+
+    }
+}
