3ddb0e61a0a71b6ce52a86dc50542aad99dbf3ac
Denis Panasenko <panasenkodenis1995@gmail.com>
2024-08-24 12:18:24 +0300

    BJS2-24356 commentaries fixed

--- src/main/java/faang/school/godbless/BJS2_24356/GooglePhotosAutoUploader.java
+++ src/main/java/faang/school/godbless/BJS2_24356/GooglePhotosAutoUploader.java
@@ -1,8 +1,11 @@
 package faang.school.godbless.BJS2_24356;
 
+import lombok.extern.slf4j.Slf4j;
+
 import java.util.ArrayList;
 import java.util.List;
 
+@Slf4j
 public class GooglePhotosAutoUploader {
     private final Object lock = new Object();
     private final List<String> photosToUpload = new ArrayList<>();
@@ -12,28 +15,29 @@ public class GooglePhotosAutoUploader {
            try {
                while (true){
                    if (photosToUpload.isEmpty()) {
-                       System.out.println("No photos, waiting");
+                       log.info("No photos, waiting");
                        lock.wait();
                    }
-                   System.out.println("thread is alive!");
+                   log.info("thread is alive!");
                    uploadPhotos();
                }
            } catch (InterruptedException e) {
-               System.out.println("auto loader is turned off");
+               Thread.currentThread().interrupt();
+               log.error("auto loader got an error: {}", e.getMessage());
            }
        }
     }
 
     public void uploadPhotos(){
         synchronized (lock){
-            System.out.println("Loading in Google cloud");
+            log.info("Loading in Google cloud");
             photosToUpload.clear();
         }
     }
 
     public void onNewPhotoAdded(String photoPath){
        synchronized (lock){
-           System.out.printf("adding %s to list%n", photoPath);
+          log.info("adding {} to list%n", photoPath);
            photosToUpload.add(photoPath);
            lock.notify();
        }
