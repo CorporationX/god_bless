4fb19c84d300177f8eb04585f7027a3baac547b6
TheGreatLucky <danke_villetti@mail.ru>
2024-08-24 14:13:44 +0500

    created methods

---
+++ src/main/java/faang/school/godbless/TheyWereVeryNiceButExtremelyPoor/WeasleyFamily.java
@@ -0,0 +1,25 @@
+package faang.school.godbless.TheyWereVeryNiceButExtremelyPoor;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
+import java.util.stream.Stream;
+
+public class WeasleyFamily {
+    public static void main(String[] args) {
+        String[] chores = {"run", "jump", "swim", "fly"};
+        ExecutorService executor = Executors.newCachedThreadPool();
+        Stream.of(chores).forEach((chore) -> executor.execute(new Chore(chore)));
+
+        try {
+            executor.shutdown();
+            if (!executor.awaitTermination(60, TimeUnit.SECONDS)) {
+                executor.shutdownNow();
+            }
+        } catch (InterruptedException e) {
+            executor.shutdownNow();
+            Thread.currentThread().interrupt();
+        }
+        System.out.println("All tasks have been completed");
+    }
+}
