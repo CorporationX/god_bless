192cda1fd908d276a9c7dbf410687127e7d15f62
it-mikhail <vulp@yandex.ru>
2024-08-24 16:14:51 +0300

    Uber

---
+++ src/main/java/faang/school/godbless/fi/uber/FareCalculator.java
@@ -0,0 +1,14 @@
+package faang.school.godbless.fi.uber;
+
+import java.util.function.BiFunction;
+
+public class FareCalculator {
+
+    public double calculateFare(double distance,
+                                double time,
+                                BiFunction<Double, Double, Double> fare) {
+
+        return fare.apply(distance, time);
+    }
+
+}
