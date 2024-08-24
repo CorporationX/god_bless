177b8ed73ef6198e812d72f6a21cb7ba4ee59248
SharkB38 <val.wolf.ne@gmail.com>
2024-08-24 11:03:35 +0900

    fix

--- src/main/java/youtube/VideoManager.java
+++ src/main/java/youtube/VideoManager.java
@@ -9,20 +9,33 @@ import java.util.Map;
 @RequiredArgsConstructor
 public class VideoManager {
     private final Map<Video, Integer> viewsMap = new HashMap<>();
+    private final Map<Video, Object> videoLocks = new HashMap<>();
 
     public void addView(@NonNull Video video) {
-        synchronized (video.getId()) {
+        if (!videoLocks.containsKey(video)) {
+            addLockForVideo(video);
+        }
+        synchronized (videoLocks.get(video)) {
             viewsMap.merge(video, 1, Integer::sum);
         }
     }
 
     public int getViewCount(@NonNull Video video) {
-        synchronized (video.getId()) {
+        if (!videoLocks.containsKey(video)) {
+            throw new IllegalArgumentException("Don't have lock for video with this id");
+        }
+        synchronized (videoLocks.get(video)) {
             if (viewsMap.containsKey(video)) {
                 return viewsMap.get(video);
             } else {
                 throw new IllegalArgumentException("Don't have video with this id");
             }
+        }
+    }
+
+    private synchronized void addLockForVideo(@NonNull Video video) {
+        if (!videoLocks.containsKey(video)) {
+            videoLocks.put(video, new Object());
         }
     }
 }
