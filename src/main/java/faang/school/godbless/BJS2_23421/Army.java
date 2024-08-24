5d5aef0e6690ef1618096d0b7e64285173318b31
Viktor Shpet <shpet.viktor.93@gmail.com>
2024-08-24 19:54:03 +0500

    Изменён метод calculateTotalPower. В классе UnitPowerCalculator поле unit заменено на List<Unit>.

--- src/main/java/faang/school/godbless/BJS2_23421/Army.java
+++ src/main/java/faang/school/godbless/BJS2_23421/Army.java
@@ -4,29 +4,46 @@ import lombok.Getter;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Stream;
 
 @Getter
 public class Army {
     private final List<Unit> UNITS = new ArrayList<>();
 
     public int calculateTotalPower() {
-        List<UnitPowerCalculator> unitPowerCalculators = UNITS.stream()
-                .map(UnitPowerCalculator::new)
-                .peek(UnitPowerCalculator::start)
-                .toList();
+        List<Unit> archers = new ArrayList<>();
+        List<Unit> mages = new ArrayList<>();
+        List<Unit> swordsmen = new ArrayList<>();
 
-        return unitPowerCalculators.stream()
-                .peek(calc -> {
-                    try {
-                        calc.join();
-                    } catch (InterruptedException e) {
-                        throw new RuntimeException(e);
-                    }
-                })
-                .map(UnitPowerCalculator::getPower)
-                .mapToInt(Integer::intValue)
+        for (Unit unit : UNITS) {
+            if (unit instanceof Mage)
+                mages.add(unit);
+            if (unit instanceof Archer)
+                archers.add(unit);
+            if (unit instanceof Swordsman)
+                swordsmen.add(unit);
+        }
+
+        var archersCalculator = new UnitPowerCalculator(archers);
+        var magesCalculator = new UnitPowerCalculator(mages);
+        var swordsmanCalculator = new UnitPowerCalculator(swordsmen);
+
+        archersCalculator.start();
+        magesCalculator.start();
+        swordsmanCalculator.start();
+
+        try {
+            archersCalculator.join();
+            magesCalculator.join();
+            swordsmanCalculator.join();
+        } catch (InterruptedException e) {
+            Thread.currentThread().interrupt();
+            throw new RuntimeException(e);
+        }
+
+        return Stream.of(archersCalculator, magesCalculator, swordsmanCalculator)
+                .mapToInt(UnitPowerCalculator::getPower)
                 .sum();
-
     }
 
     public void addUnit(Unit unit) {
--- src/main/java/faang/school/godbless/BJS2_23421/UnitPowerCalculator.java
+++ src/main/java/faang/school/godbless/BJS2_23421/UnitPowerCalculator.java
@@ -3,14 +3,18 @@ package faang.school.godbless.BJS2_23421;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 
+import java.util.List;
+
 @Getter
 @RequiredArgsConstructor
 public class UnitPowerCalculator extends Thread {
     private int power;
-    private final Unit UNIT;
+    private final List<Unit> UNIT;
 
     @Override
     public void run() {
-        power = UNIT.getPOWER();
+        power = UNIT.stream()
+                .mapToInt(Unit::getPOWER)
+                .sum();
     }
 }
