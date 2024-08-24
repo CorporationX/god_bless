56e5ae79a152fd44fb47baeca42c3e1df61f122f
Garib_Aghayev <garibaghayev@gmail.com>
2024-08-25 02:29:31 +0300

    first release

--- src/main/java/faang/school/godbless/SecondSprint/BJS2_23239/Manager.java
+++ src/main/java/faang/school/godbless/SecondSprint/BJS2_23239/Manager.java
@@ -4,59 +4,60 @@ import java.util.Comparator;
 import java.util.List;
 import java.util.function.Predicate;
 
-
 public class Manager {
     public static int sumOfEven(List<Integer> input) {
-        int result = input.stream().filter(n -> n % 2 == 0).mapToInt(item -> item).sum();
-
-        return result;
+        return input.stream()
+                .filter(n -> n % 2 == 0)
+                .mapToInt(item -> item)
+                .sum();
     }
 
     public static int findMax(List<Integer> input) {
-        int result = input.stream().max(Comparator.naturalOrder()).orElseThrow();
-
-        return result;
+        return input.stream()
+                .max(Comparator.naturalOrder())
+                .orElseThrow();
     }
 
     public static double average(List<Integer> input) {
-        double result = input.stream().mapToInt(k -> k).average().orElseThrow();
-
-        return result;
+        return input.stream()
+                .mapToInt(k -> k)
+                .average()
+                .orElseThrow();
     }
 
     public static long countOfStringStartWith(List<String> input, char ch) {
-        long result = input.stream().filter(s -> s.startsWith(String.valueOf(ch))).count();
-
-        return result;
+        return input.stream()
+                .filter(s -> s.startsWith(String.valueOf(ch)))
+                .count();
     }
 
     public static List<String> filterBySubstring(List<String> input, String subString) {
-        List<String> result = input.stream().filter(s -> s.contains(subString)).toList();
-
-        return result;
+        return input.stream()
+                .filter(s -> s.contains(subString))
+                .toList();
     }
 
     public static List<String> sortByLength(List<String> input) {
-        List<String> result = input.stream().sorted((a, k) -> a.length() - k.length()).toList();
-
-        return result;
+        return input.stream()
+                .sorted((a, k) -> a.length() - k.length())
+                .toList();
     }
 
     public static <T> boolean isMeetCondition(List<T> input, Predicate<T> predictor) {
-        boolean result = input.stream().allMatch(predictor);
-
-        return result;
+        return input.stream()
+                .allMatch(predictor);
     }
 
     public static int minFromGiven(List<Integer> input, int num) {
-        int result = input.stream().filter(n -> n > num).min(Comparator.naturalOrder()).orElseThrow();
-
-        return result;
+        return input.stream()
+                .filter(n -> n > num)
+                .min(Comparator.naturalOrder())
+                .orElseThrow();
     }
 
     public static List<Integer> getLenghtOfStrings(List<String> input) {
-        List<Integer> result = input.stream().map((s) -> s.length()).toList();
-
-        return result;
+        return input.stream()
+                .map((s) -> s.length())
+                .toList();
     }
 }
