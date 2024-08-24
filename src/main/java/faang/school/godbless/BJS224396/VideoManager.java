4ed8d6a1f6fdfa9897d85af81462962d5b40af5e
aephrodita <146974539+aephrodita@users.noreply.github.com>
2024-08-24 21:26:39 +0500

    BJS2-24396

--- src/main/java/faang/school/godbless/BJS224396/VideoManager.java
+++ src/main/java/faang/school/godbless/BJS224396/VideoManager.java
@@ -3,8 +3,8 @@ package faang.school.godbless.BJS224396;
 import lombok.Getter;
 import lombok.extern.slf4j.Slf4j;
 
-import java.util.concurrent.ConcurrentHashMap;
 import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
 
 @Slf4j
 @Getter
