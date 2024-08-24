05b65120d4d7ee6cd4f46fce720dc43835191553
Denys Dovgan <ytcsgoatlas@gmail.com>
2024-08-24 22:44:08 +0200

    Fixed. Logs added

--- src/main/java/faang/school/godbless/sprint3/BJS2_23976/FoodDeliveryTask.java
+++ src/main/java/faang/school/godbless/sprint3/BJS2_23976/FoodDeliveryTask.java
@@ -1,9 +1,11 @@
 package faang.school.godbless.sprint3.BJS2_23976;
 
 import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 
 import java.util.Random;
 
+@Slf4j
 @AllArgsConstructor
 public class FoodDeliveryTask implements Runnable {
     private String character;
@@ -22,7 +24,8 @@ public class FoodDeliveryTask implements Runnable {
         try {
             Thread.sleep(new Random().nextLong(1, 6) * 1000L);
         } catch (InterruptedException e) {
-            e.printStackTrace();
+            Thread.currentThread().interrupt();
+            log.error("Interrupted exception: {}", e.getMessage());
         }
 
         System.out.printf("%s is eating %d x %s %n", character, foodAmount, foodType);
--- src/main/java/faang/school/godbless/sprint3/BJS2_23976/GriffinsFoodDelivery.java
+++ src/main/java/faang/school/godbless/sprint3/BJS2_23976/GriffinsFoodDelivery.java
@@ -1,9 +1,13 @@
 package faang.school.godbless.sprint3.BJS2_23976;
 
+import lombok.extern.slf4j.Slf4j;
+
 import java.util.Random;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
 
+@Slf4j
 public class GriffinsFoodDelivery {
     public static void main(String[] args) {
         ExecutorService executorService = Executors.newFixedThreadPool(3);
@@ -14,5 +18,16 @@ public class GriffinsFoodDelivery {
             executorService.submit(foodDeliveryTask::run);
         }
         executorService.shutdown();
+
+        try {
+            if (executorService.awaitTermination(60, TimeUnit.SECONDS)) {
+                log.info("All food have been delivered");
+            } else {
+                log.info("Timeout: ExecutorService did not shut down in time.");
+            }
+        } catch(InterruptedException e) {
+            Thread.currentThread().interrupt();
+            log.error("Interrupted exception: {}", e.getMessage());
+        }
     }
 }
