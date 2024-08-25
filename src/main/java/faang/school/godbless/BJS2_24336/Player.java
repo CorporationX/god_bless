8ff782361c3df2868b5c138c3c1c2f0e2c1a46f8
Prigozhaeva-Anastasia <nastaprigozaeva20@gmail.com>
2024-08-25 20:07:45 +0300

    Supercow!

---
+++ src/main/java/faang/school/godbless/BJS2_24336/Player.java
@@ -0,0 +1,14 @@
+package faang.school.godbless.BJS2_24336;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class Player {
+    private String name;
+
+    public void startBattle(Boss boss) {
+        boss.joinBattle(this);
+    }
+}
