685f4e1774ef4b35caa4ad34938c53515e97cb36
Evgenii Shevchenko <evgeny.shevchenko@webware24.de>
2024-08-25 11:38:33 +0200

    BJS2-21892 - Фильтры Instagram

---
+++ src/main/java/faang/school/godbless/sprint/two/instagram/App.java
@@ -0,0 +1,9 @@
+package faang.school.godbless.sprint.two.instagram;
+
+import java.util.function.Function;
+
+public class App {
+    public static void main(String... args) {
+        // Result in tests
+    }
+}
---
+++ src/main/java/faang/school/godbless/sprint/two/instagram/Image.java
@@ -0,0 +1,11 @@
+package faang.school.godbless.sprint.two.instagram;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class Image {
+    private String fileName;
+    private String description;
+}
