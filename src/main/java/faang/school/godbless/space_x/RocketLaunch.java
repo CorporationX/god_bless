cf453995e257ee846be2fc3382c32e029024eb26
VladislavVoronov <vvvlad_90@mail.ru>
2024-08-25 13:44:13 +0300

    Выполнил задачу

---
+++ src/main/java/faang/school/godbless/space_x/RocketLaunch.java
@@ -0,0 +1,62 @@
+package faang.school.godbless.space_x;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
+
+@AllArgsConstructor
+@Getter
+public class RocketLaunch {
+    private String titleStart;
+    private long timeStart;
+
+
+    public void launch() {
+
+            System.out.println("Запускаем ракету %s".formatted(titleStart));
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+
+        System.out.println("Поехали!");
+    }
+
+    public static void planRocketLaunches(List<RocketLaunch> rocketLaunchList) {
+        ScheduledExecutorService poolThreads = Executors.newSingleThreadScheduledExecutor();
+
+        rocketLaunchList.sort(Comparator.comparingLong(RocketLaunch::getTimeStart));
+        long currentTime = System.currentTimeMillis();
+
+        for (RocketLaunch launch : rocketLaunchList) {
+            long delay = launch.getTimeStart() - currentTime;
+            if (delay > 0) {
+                poolThreads.schedule(launch::launch, delay, TimeUnit.MILLISECONDS);
+            }
+        }
+        poolThreads.shutdown();
+    }
+
+    public static void main(String[] args) {
+        List<RocketLaunch> launches = new ArrayList<>();
+        launches.add(new RocketLaunch("Apollo 11", System.currentTimeMillis() + 3000));
+        launches.add(new RocketLaunch("Falcon Heavy", System.currentTimeMillis() + 10000));
+        launches.add(new RocketLaunch("Soyuz MS-10", System.currentTimeMillis() + 5000));
+
+
+        long startTime = System.currentTimeMillis();
+        planRocketLaunches(launches);
+        long endTime = System.currentTimeMillis();
+
+
+        System.out.println("Time taken to plan rocket launches: " + (endTime - startTime) + " ms");
+    }
+}
