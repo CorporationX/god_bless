f02a09c1b262b2a968078e407e41793fc4c37525
TheGreatLucky <danke_villetti@mail.ru>
2024-08-25 12:00:24 +0500

    created methods

---
+++ src/main/java/faang/school/godbless/TomAndJary/Food.java
@@ -0,0 +1,13 @@
+package faang.school.godbless.TomAndJary;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.Getter;
+import lombok.ToString;
+
+@AllArgsConstructor
+@Getter
+@ToString
+public class Food {
+    private String name;
+}
