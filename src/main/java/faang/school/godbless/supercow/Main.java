4756bc74f6ef69113e18dfd74b5e24a2958fcfe1
ifef228 <ifef228@gmail.com>
2024-08-25 19:40:38 +0300

    поправил

--- src/main/java/faang/school/godbless/supercow/Main.java
+++ src/main/java/faang/school/godbless/supercow/Main.java
@@ -7,25 +7,23 @@ import java.util.concurrent.Executors;
 
 public class Main {
     public static void main(String[] args) {
-        Player p1 = new Player("p1");
-        Player p2 = new Player("p2");
+        Player player1 = new Player("player1");
+        Player player2 = new Player("player2");
 
         Boss boss = new Boss(2, new ArrayList<>(
-                List.of(p1, p2)
+                List.of(player1, player2)
         ));
-
-
+        
         ExecutorService service = Executors.newFixedThreadPool(2);
+        
+        Player player3 = new Player("player3");
+        Player player4 = new Player("player4");
 
+        service.execute(() -> player3.startBattle(boss));
+        service.execute(() -> player4.startBattle(boss));
 
-        Player p3 = new Player("p3");
-        Player p4 = new Player("p4");
-
-        service.execute(() -> p3.startBattle(boss));
-        service.execute(() -> p4.startBattle(boss));
-
-        boss.leaveBattle(p1);
-        boss.leaveBattle(p2);
+        boss.leaveBattle(player1);
+        boss.leaveBattle(player2);
 
         service.shutdown();
     }
