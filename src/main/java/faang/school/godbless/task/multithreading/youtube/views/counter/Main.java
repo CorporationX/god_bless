ee3ad58590c5c80ddd49ec0b95b8e315aa386626
Sultan <nikowosuka@gmail.com>
2024-08-25 12:47:54 +0500

    refactoring, decompose main method

--- src/main/java/faang/school/godbless/task/multithreading/youtube/views/counter/Main.java
+++ src/main/java/faang/school/godbless/task/multithreading/youtube/views/counter/Main.java
@@ -10,49 +10,57 @@ import java.util.stream.IntStream;
 
 @Slf4j
 public class Main {
-    public static final int NUM_THREADS = 100;
-    public static final int NUM_VIDEOS = 20;
-    public static final int TIME_OUT = 5;
+    private static final int TREAD_POOL_LIMIT = 100;
+    private static final int NUMBER_OF_VIDEOS = 20;
+    private static final int VIEWS_PER_VIDEO = TREAD_POOL_LIMIT / NUMBER_OF_VIDEOS;
+    private static final int TIME_OUT = 1000;
 
     private static final VideoManager videoManager = new VideoManager();
-    private static final ExecutorService executor = Executors.newFixedThreadPool(NUM_THREADS);
+    private static final ExecutorService executor = Executors.newFixedThreadPool(TREAD_POOL_LIMIT);
     private static List<String> videoId;
 
     public static void main(String[] args) {
         videoId = createVideoId();
-        videoId.forEach(id -> IntStream.range(0, NUM_THREADS / NUM_VIDEOS)
-                .forEach(i -> executor.submit(() -> watchVideo(id))));
+        videoId.forEach(Main::startWatching);
         executor.shutdown();
-        waitEnd(executor, videoId);
+        waitEnd();
     }
 
-    private static void waitEnd(ExecutorService executor, List<String> videoId) {
+    private static List<String> createVideoId() {
+        return IntStream
+                .rangeClosed(1, NUMBER_OF_VIDEOS)
+                .mapToObj(i -> "Video_" + i)
+                .toList();
+    }
+
+    private static void startWatching(String videoId) {
+        IntStream.range(0, VIEWS_PER_VIDEO)
+                .forEach(i -> executor.execute(() -> watchVideo(videoId)));
+    }
+
+    private static void watchVideo(String videoId) {
+        videoManager.addView(videoId);
+        videoManager.getViewCount(videoId)
+                .ifPresentOrElse(
+                        view -> log.info("Воспроизведено виде с id: {}, просмотров: {}", videoId, view),
+                        () -> log.info("Видео с id: {}, не найдено", videoId));
+    }
+
+    private static void waitEnd() {
         try {
-            if (executor.awaitTermination(TIME_OUT, TimeUnit.SECONDS)) {
-                printAllViews(videoId);
+            if (executor.awaitTermination(TIME_OUT, TimeUnit.MILLISECONDS)) {
+                log.info("Количество просмотров по каждому видео:");
+                printAllViews();
             } else {
-                log.info("Видео записи не успели воспроизвестись за {} секунд", TIME_OUT);
+                log.info("Видео записи не успели воспроизвестись за {} миллисекунд", TIME_OUT);
             }
         } catch (InterruptedException exception) {
             log.error("Interrupted exception: {}", exception.getMessage());
         }
     }
 
-    private static void watchVideo(String videoId) {
-        videoManager.addView(videoId);
-        videoManager.getViewCount(videoId)
-                .ifPresent(view -> log.info("Воспроизведено виде с id: {}, просмотров: {}", videoId, view));
-    }
-
-    private static List<String> createVideoId() {
-        return IntStream
-                .rangeClosed(1, NUM_VIDEOS)
-                .mapToObj(i -> "Video_" + i)
-                .toList();
-    }
-
-    private static void printAllViews(List<String> videos) {
-        videos.forEach(videoId -> videoManager.getViewCount(videoId)
-                .ifPresent(view -> log.info("id: {}, просмотров: {}", videoId, view)));
+    private static void printAllViews() {
+        videoId.forEach(video -> videoManager.getViewCount(video)
+                .ifPresent(view -> log.info("id: {}, просмотров: {}", video, view)));
     }
 }
