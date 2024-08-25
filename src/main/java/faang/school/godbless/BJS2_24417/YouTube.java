99ad1fe8a1ec13719c526e7c01ec32a54bc7a924
Viktor Shpet <shpet.viktor.93@gmail.com>
2024-08-25 19:33:55 +0500

    BJS2-24417 solved.

---
+++ src/main/java/faang/school/godbless/BJS2_24417/YouTube.java
@@ -0,0 +1,41 @@
+package faang.school.godbless.BJS2_24417;
+
+import java.util.Map;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
+
+public class YouTube {
+    public static void main(String[] args) throws InterruptedException {
+        var videoManager = new VideoManager();
+        final int NUM_THREADS = 100;
+        final int NUM_VIDEOS = 10;
+        createVideo(videoManager.getViewsMap(), NUM_VIDEOS);
+
+        var executorService = Executors.newFixedThreadPool(NUM_THREADS);
+
+        videoManager.getViewsMap().forEach(
+                (s, integer) -> executorService.execute(() -> {
+                    for (int i = NUM_THREADS / NUM_VIDEOS; i > 0; i--) {
+                        videoManager.addView(s);
+                        var videoCount = videoManager.getVideoCount(s);
+                        System.out.printf("%s has %d views\n", s, videoCount);
+                    }
+                })
+        );
+
+        executorService.shutdown();
+
+        if (executorService.awaitTermination(1, TimeUnit.MINUTES))
+            System.out.println("All tasks completed");
+        else {
+            var tasks = executorService.shutdownNow();
+            tasks.forEach(task -> System.out.printf("Task %s wasn't ended%n", task));
+        }
+    }
+
+    private static void createVideo(Map<String, Integer> videoMap, int numVideos) {
+        for (int i = 0; i < numVideos; ++i) {
+            videoMap.put("Video #" + i, 0);
+        }
+    }
+}
