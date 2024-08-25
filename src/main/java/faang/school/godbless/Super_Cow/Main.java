4975645d5d0c569be619eaad360a10d6140ce3cf
Okabe <mathew.mackonoha@gmail.com>
2024-08-25 13:25:49 +0300

    git commit -m "BJS2-24287 <Super cow>"

--- src/main/java/faang/school/godbless/Super_Cow/Main.java
+++ src/main/java/faang/school/godbless/Super_Cow/Main.java
@@ -7,9 +7,9 @@ public class Main {
     public static void main(String[] args) {
         Player player1 = new Player("Jack");
         Player player2 = new Player("Rob");
-        Player player3 = new Player("George");
-        Player player4 = new Player("Owo");
-        Player player5 = new Player("Mat");
+        //Player player3 = new Player("George");
+        //Player player4 = new Player("Owo");
+        //Player player5 = new Player("Mat");
         List<Player> currentPlayers1 = new ArrayList<>();
         Boss boss1 = new Boss(5, currentPlayers1);
         Thread thread1 = new Thread(() ->boss1.joinBattle(player1));
