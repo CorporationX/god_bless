b4e2fbdad063baf52e2df15a883652c0ac92a71d
Sultan <nikowosuka@gmail.com>
2024-08-25 19:37:02 +0500

    refactoring, getPlayers method

--- src/main/java/faang/school/godbless/task/multithreading/bro/force/Main.java
+++ src/main/java/faang/school/godbless/task/multithreading/bro/force/Main.java
@@ -5,10 +5,12 @@ import lombok.extern.slf4j.Slf4j;
 import java.util.List;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
+import java.util.stream.IntStream;
 
 @Slf4j
 public class Main {
-    private static final int THREAD_POOL_LIMIT = 4;
+    private static final int NUMBER_OF_PLAYERS = 6;
+    private static final int THREAD_POOL_LIMIT = NUMBER_OF_PLAYERS;
 
     private static final Game game = new Game();
     private static List<Player> players;
@@ -21,13 +23,9 @@ public class Main {
     }
 
     private static List<Player> getPlayers() {
-        return List.of(
-                new Player("Player 1", game),
-                new Player("Player 2", game),
-                new Player("Player 3", game),
-                new Player("Player 4", game),
-                new Player("Player 5", game),
-                new Player("Player 6", game)
-        );
+        return IntStream
+                .rangeClosed(1, NUMBER_OF_PLAYERS)
+                .mapToObj(i -> new Player("Player " + i, game))
+                .toList();
     }
 }
