c8ad44120243a5589a3be88f6247bf4b53f99309
Buukhalai Mamadaminova <buukhalai.mamadaminova.mfn@fh-zwickau.de>
2024-08-25 16:04:53 +0200

    Google Photo sync

--- src/main/java/faang/school/godbless/BJS224326/Boss.java
+++ src/main/java/faang/school/godbless/BJS224326/Boss.java
@@ -1,33 +1,59 @@
 package faang.school.godbless.BJS224326;
 
-import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 
 import java.util.ArrayList;
 import java.util.List;
 
-@AllArgsConstructor
+@RequiredArgsConstructor
 @Getter
+@Slf4j
 public class Boss {
-    private int maxPlayers;
-    private int currentPlayers;
-    List<Player> list = new ArrayList<>(maxPlayers);
+    private final int maxPlayers;
+    private int currentPlayers = 0;
+    List<Player> list = new ArrayList<>();
 
-    public void joinBattle(Player player) throws InterruptedException {
+    public void joinBattle(Player player) {
+        addPlayer(player);
+        fight(player);
+        removePlayer(player);
+    }
+
+    private void removePlayer(Player player) {
         synchronized (this) {
-            if (list.size() == maxPlayers) {
-                this.wait();
-            }
-        }
-        list.add(player);
-        Thread.sleep(2000);
-        System.out.println("    ");
-        synchronized (this){
+            log.info(" " + player.getName() + "вышел освободилось место");
+            list.remove(player);
             this.notifyAll();
         }
     }
 
+    private static void fight(Player player) {
+        try {
+            log.info(player.getName() + " сражается с босом");
+            Thread.sleep(2000);
+            log.info(player.getName() + " закончил сражается с босом");
 
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private void addPlayer(Player player) {
+        synchronized (this) {
+            if (list.size() == maxPlayers) {
+                log.info("" + player.getName() + " перещел в ожидание");
+                try {
+                    this.wait();
+                } catch (InterruptedException e) {
+                    throw new RuntimeException(e);
+                }
+                log.info("" + player.getName() + " вышел из режима ожидания");
+            }
+            list.add(player);
+        }
+    }
 
 
 }
