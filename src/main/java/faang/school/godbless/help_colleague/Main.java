fa6c65e5d6e1b5872f488991a4d5d42b469ffc3f
Nikita Korolev <nk25.dev@gmail.com>
2024-08-25 16:48:00 +0300

    Requested changes from the reviewer.

--- src/main/java/faang/school/godbless/help_colleague/Main.java
+++ src/main/java/faang/school/godbless/help_colleague/Main.java
@@ -1,7 +1,5 @@
 package faang.school.godbless.help_colleague;
 
-import lombok.extern.slf4j.Slf4j;
-
 import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.ExecutorService;
@@ -9,7 +7,6 @@ import java.util.concurrent.Executors;
 import java.util.concurrent.TimeUnit;
 import java.util.stream.IntStream;
 
-@Slf4j
 public class Main {
     private static final long TIMEOUT = 10000;
     private static final int THREADS = 100;
@@ -19,13 +16,11 @@ public class Main {
     public static void main(String[] args) {
         ExecutorService executor = Executors.newFixedThreadPool(THREADS);
         List<Person> batch = new ArrayList<>();
-        List<Person> people = IntStream.range(0, LIST_SIZE)
-                .mapToObj(n -> new Person("Name " + n, "Surname " + n, n % 10 + 18, "Address " + n))
-                .toList();
+        List<Person> people = generateData();
         people.forEach(p -> {
             batch.add(p);
             if (batch.size() % BATCH_SIZE == 0) {
-                executor.submit(new PersonNamePrinter(batch));
+                executor.submit(new PersonNamePrinter(List.copyOf(batch)));
                 batch.clear();
             }
         });
@@ -33,12 +28,12 @@ public class Main {
         executor.shutdown();
         long startTime = System.currentTimeMillis();
         try {
-            while(!executor.awaitTermination(5, TimeUnit.SECONDS)) {
+            while(!executor.awaitTermination(2, TimeUnit.SECONDS)) {
                 if (System.currentTimeMillis() - startTime > TIMEOUT) {
                     System.out.printf("Timeout reached: %d%n", TIMEOUT);
                     executor.shutdownNow();
                 } else {
-                    log.info("Tasks are currently executed...");
+                    System.out.println("--- Tasks are being processed...");
                 }
             }
         } catch(InterruptedException e) {
@@ -46,5 +41,11 @@ public class Main {
         }
         System.out.println("===========================");
         System.out.println("Finished processing records");
+    }
+
+    private static List<Person> generateData() {
+        return IntStream.range(0, LIST_SIZE)
+                .mapToObj(n -> new Person("Name " + n, "Surname " + n, n % 10 + 18, "Address " + n))
+                .toList();
     }
 }
