44eba26295322ea5ba463c0b966ba513e235d169
Okabe <mathew.mackonoha@gmail.com>
2024-08-25 10:40:24 +0300

    Commiting "Spotify" task 2

--- src/main/java/faang/school/godbless/Spotify_Cancelling/Music.java
+++ src/main/java/faang/school/godbless/Spotify_Cancelling/Music.java
@@ -4,13 +4,21 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Music {
-    public static void main(String[] args) {
-        Object lock1 = new Object();
+    public static void main(String[] args) throws InterruptedException {
         List<String> songs = new ArrayList<>();
-        Player myPlayer = new Player(lock1,"Egor Letov", songs);
+        Player myPlayer = new Player(songs);
+        myPlayer.queueAdd("My protection");
+        myPlayer.queueAdd("On sunrise");
+        myPlayer.queueAdd("Bakamitai");
+        myPlayer.queueAdd("Judgement");
+        myPlayer.queueAdd("The man who sold the world");
         Thread playThread = new Thread(() -> myPlayer.play());
         Thread pauseThread = new Thread(() -> myPlayer.pause());
-        //Thread previousThread = new Thread();
+        Thread previousThread = new Thread(() -> myPlayer.previous());
         Thread skipThread = new Thread(() -> myPlayer.skip());
+        playThread.start();
+        pauseThread.start();
+        skipThread.start();
+        previousThread.start();
     }
 }
--- src/main/java/faang/school/godbless/Spotify_Cancelling/Player.java
+++ src/main/java/faang/school/godbless/Spotify_Cancelling/Player.java
@@ -12,28 +12,45 @@ import java.util.List;
 @Setter
 @NoArgsConstructor
 public class Player {
-    private Object lock1 = new Object();
+    private final Object lock1 = new Object();
     private String isPlaying;
     private List<String> queue;
+    private int currentSong;
+
+    public Player(List<String> queue) {
+        this.queue = queue;
+    }
 
     public void play() {
         synchronized (lock1) {
-            isPlaying = queue.get(0) + " is playing";
-            System.out.println(isPlaying);
+            if (currentSong < queue.size()) {
+                isPlaying = queue.get(currentSong);
+                System.out.println(isPlaying + " is playing");
+            } else {
+                System.out.println("End of playlist");
+            }
         }
     }
 
     public void pause() {
         synchronized (lock1) {
-            isPlaying = isPlaying + " is paused";
-            System.out.println(isPlaying);
+            if (isPlaying != null) {
+                System.out.println(isPlaying + " is paused");
+            } else {
+                System.out.println("No song is playing");
+            }
         }
     }
 
     public void skip() {
         synchronized (lock1) {
-            queue.removeFirst();
-            isPlaying = queue.getFirst();
+            if (currentSong < queue.size() - 1) {
+                currentSong++;
+                isPlaying = queue.get(currentSong);
+                System.out.println(isPlaying + " is playing");
+            } else {
+                System.out.println("End of playlist");
+            }
         }
     }
 
@@ -45,7 +62,13 @@ public class Player {
 
     public void previous() {
         synchronized (lock1) {
+            if (currentSong > 0) {
+                currentSong--;
+                isPlaying = queue.get(currentSong);
+                System.out.println(isPlaying + " is playing");
+            } else {
+                System.out.println("Beginning of playlist");
+            }
         }
-
     }
 }
