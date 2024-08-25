b86e6b9e92a58b5d433b29d52127c1b5b6513d6d
Nikita Korolev <nk25.dev@gmail.com>
2024-08-25 15:41:36 +0300

    Requested changes from the reviewer.

--- src/main/java/faang/school/godbless/help_colleague/Main.java
+++ src/main/java/faang/school/godbless/help_colleague/Main.java
@@ -1,16 +1,18 @@
 package faang.school.godbless.help_colleague;
 
-import java.sql.Array;
+import lombok.extern.slf4j.Slf4j;
+
 import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.TimeUnit;
-import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
+@Slf4j
 public class Main {
-    private static final int THREADS = 10;
+    private static final long TIMEOUT = 10000;
+    private static final int THREADS = 100;
     private static final int LIST_SIZE = 10000;
     private static final int BATCH_SIZE = LIST_SIZE / THREADS;
 
@@ -18,26 +20,29 @@ public class Main {
         ExecutorService executor = Executors.newFixedThreadPool(THREADS);
         List<Person> batch = new ArrayList<>();
         List<Person> people = IntStream.range(0, LIST_SIZE)
-                .mapToObj(n -> new Person("Name " + n,
-                        "Surname " + n,
-                        n % 10 + 18,
-                        "Address " + n))
+                .mapToObj(n -> new Person("Name " + n, "Surname " + n, n % 10 + 18, "Address " + n))
                 .toList();
-
-        for(int i = 0; i < people.size(); i++) {
-            batch.add(people.get(i));
-            if ((i + 1) % BATCH_SIZE == 0) {
+        people.forEach(p -> {
+            batch.add(p);
+            if (batch.size() % BATCH_SIZE == 0) {
                 executor.submit(new PersonNamePrinter(batch));
-                batch = new ArrayList<>();
+                batch.clear();
             }
-        }
+        });
+
         executor.shutdown();
+        long startTime = System.currentTimeMillis();
         try {
             while(!executor.awaitTermination(5, TimeUnit.SECONDS)) {
-                System.out.println("Tasks being processed...");
+                if (System.currentTimeMillis() - startTime > TIMEOUT) {
+                    System.out.printf("Timeout reached: %d%n", TIMEOUT);
+                    executor.shutdownNow();
+                } else {
+                    log.info("Tasks are currently executed...");
+                }
             }
         } catch(InterruptedException e) {
-            Thread.currentThread().interrupt();
+            e.printStackTrace();
         }
         System.out.println("===========================");
         System.out.println("Finished processing records");
