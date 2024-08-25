44e8576342b2a68492842c8a0005d4a431d46bab
Buukhalai Mamadaminova <buukhalai.mamadaminova.mfn@fh-zwickau.de>
2024-08-25 14:59:36 +0200

    Google Photo sync

---
+++ src/main/java/faang/school/godbless/GooglePhoto.java
@@ -0,0 +1,45 @@
+package faang.school.godbless;
+
+import lombok.extern.slf4j.Slf4j;
+
+import java.util.List;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
+
+@Slf4j
+public class GooglePhoto {
+    public static void main(String[] args) {
+        GooglePhotosAutoUploader uploader = new GooglePhotosAutoUploader(new Object());
+
+        List<String> photos = List.of("photo1", "photo2", "photo3", "photo4", "photo5", "photo6",
+                "photo7", "photo8", "photo9", "photo10", "photo11", "photo12",
+                "photo13", "photo14", "photo15", "photo16");
+
+        ExecutorService executorService = Executors.newFixedThreadPool(2);
+        executorService.execute(uploader::startAutoUpload);
+        photos.forEach(photo -> {
+            try {
+                Thread.sleep(1000);
+                executorService.execute(() -> {
+                    uploader.onNewPhotoAdded(photo);
+                });
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+        });
+        executorService.shutdown();
+
+        try {
+           if( executorService.awaitTermination(5, TimeUnit.SECONDS)){
+               log.info("finised ok");
+           }
+           else {
+               log.info("finshed not ko");
+           }
+        } catch (InterruptedException e) {
+
+
+        }
+    }
+}
--- src/main/java/faang/school/godbless/GooglePhotosAutoUploader.java
+++ src/main/java/faang/school/godbless/GooglePhotosAutoUploader.java
@@ -2,25 +2,56 @@ package faang.school.godbless;
 
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 
+import java.util.ArrayList;
 import java.util.List;
+
 @RequiredArgsConstructor
 @Getter
+@Slf4j
 public class GooglePhotosAutoUploader {
-    private final List<String> photosToUpload ;
-    Object lock ;
+    private  List<String> photosToUpload = new ArrayList<>();
+    private final Object lock;
 
-    public void startAutoUpload(){
-        synchronized (lock){
-                if (photosToUpload.isEmpty()){
-                    try {
-                        wait();
-
-
-                    } catch (InterruptedException e) {
-                        throw new RuntimeException(e);
-                    }
+    public void startAutoUpload() {
+        synchronized (lock) {
+            if (photosToUpload.isEmpty()) {
+                try {
+                    log.info("waiting before fotos come"  );
+                    lock.wait();
+                } catch (InterruptedException e) {
+                    throw new RuntimeException(e);
                 }
+            }
+            for (int i = 0; i<photosToUpload.size();i++){
+                if (photosToUpload.size() < i){
+                    break;
+                }
+                var photo = photosToUpload.get(i);
+                try {
+                    log.info("staered uploading to server " + photo );
+                    Thread.sleep(2000);
+                    log.info(" uploaded to server " + photo );
+                    photosToUpload.remove(photo);
+                } catch (InterruptedException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+
         }
+    }
+
+    public void onNewPhotoAdded(String photoPath) {
+        synchronized (lock) {
+            log.info("onNewPhotoAdded " + photoPath);
+            photosToUpload.add(photoPath);
+            lock.notify();
+            startAutoUpload();
+        }
+    }
+
+    private void uploadPhotos() {
+
     }
 }
