ae655849c70ea2ca602eb8d0afa56ca20e4823c3
Sergey Sklyar <sklyar1091@gmail.com>
2024-08-26 23:04:03 +0300

    Async and Future

---
+++ src/main/java/BJS2_24702_AsyncAndFuture/Main.java
@@ -0,0 +1,8 @@
+package BJS2_24702_AsyncAndFuture;
+
+public class Main {
+    public static void main(String[] args) {
+        MasterCardService masterCardService = new MasterCardService();
+        masterCardService.doAll();
+    }
+}
