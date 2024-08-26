ba3db7abac835d49c8db8ed35018ff83eca24a74
Andrew Romanov <r0m4n0v4v@gmail.com>
2024-08-26 13:24:55 +0300

    BJS2-24658 done

---
+++ src/main/java/faang/school/godbless/asyncandfuture/Main.java
@@ -0,0 +1,8 @@
+package faang.school.godbless.asyncandfuture;
+
+public class Main {
+    public static void main(String[] args) {
+        MasterCardService masterCardService = new MasterCardService();
+        masterCardService.doAll();
+    }
+}
