53807b36736b53ec43e5c0c0924ad2330403272d
Robert Oguzov <robertoguzov2@gmail.com>
2024-08-26 15:16:04 +0300

    Написал class Point

---
+++ src/main/java/faang/school/godbless/BJS2_25010/Point.java
@@ -0,0 +1,11 @@
+package faang.school.godbless.BJS2_25010;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class Point {
+    private double x;
+    private double y;
+}
