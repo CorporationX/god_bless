8919fb7f19389dac658c4505a0be736f2911be37
Okabe <mathew.mackonoha@gmail.com>
2024-08-26 15:21:39 +0300

    Complete "WOW"

---
+++ src/main/java/faang/school/godbless/WOW/QuestSystem.java
@@ -0,0 +1,48 @@
+package faang.school.godbless.WOW;
+
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
+
+public class QuestSystem {
+
+    public CompletableFuture<Player> startQuest(Player player, Quest quest) {
+        CompletableFuture<Player> playerAfter = CompletableFuture.supplyAsync(() -> {
+            try {
+                Thread.sleep(1000 * quest.getDifficulty());
+                return player;
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+                return null;
+            }
+        });
+        playerAfter.join();
+        return playerAfter;
+    }
+
+    public static void main(String[] args) {
+        QuestSystem questSystem = new QuestSystem();
+        Player player1 = new Player("Rob", 1, 1);
+        Player player2 = new Player("Arthas", 99, 2000);
+
+        Quest quest1 = new Quest("Defeat the Lich King", 10, 15);
+        Quest quest2 = new Quest("Retrieve the Sword of Azeroth", 8, 10);
+
+        CompletableFuture<Player> player1Quest = questSystem.startQuest(player1, quest1);
+        CompletableFuture<Player> player2Quest = questSystem.startQuest(player2, quest2);
+        player1Quest.thenAccept(player -> System.out.println(player.getName() + " has completed quest and now has: " + player.getExperience() + " experience points"));
+        player2Quest.thenAccept(player -> System.out.println(player.getName() + " has completed quest and now has: " + player.getExperience() + " experience points"));
+        ExecutorService executorService = Executors.newCachedThreadPool();
+        executorService.submit(() -> player1Quest);
+        executorService.submit(() -> player2Quest);
+        executorService.shutdown();
+        try {
+            if (!executorService.awaitTermination(800, TimeUnit.MILLISECONDS)) {
+                executorService.shutdownNow();
+            }
+        } catch (InterruptedException e) {
+            executorService.shutdownNow();
+        }
+    }
+}
