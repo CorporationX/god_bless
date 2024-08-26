c3a076288469986d88ebd1e66d7b0148d6a2fa23
soulasphyxia <maslyuk03@bk.ru>
2024-08-26 11:51:05 +0700

    Solved BJS2-23924

---
+++ src/main/java/lay_a_path/Location.java
@@ -0,0 +1,11 @@
+package lay_a_path;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@AllArgsConstructor
+@Getter
+public class Location {
+    private final int x;
+    private final int y;
+}
---
+++ src/main/java/lay_a_path/Monster.java
@@ -0,0 +1,11 @@
+package lay_a_path;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@AllArgsConstructor
+@Getter
+public class Monster {
+    private String name;
+    private Location location;
+}
