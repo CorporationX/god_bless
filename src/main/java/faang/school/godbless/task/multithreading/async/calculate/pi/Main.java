ff89f6172a9879ab5cc6cc202ebb931b695f7ea9
Sultan <nikowosuka@gmail.com>
2024-08-27 00:01:45 +0500

    calculate pi task perform

---
+++ src/main/java/faang/school/godbless/task/multithreading/async/calculate/pi/Main.java
@@ -0,0 +1,50 @@
+package faang.school.godbless.task.multithreading.async.calculate.pi;
+
+import lombok.extern.slf4j.Slf4j;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+import java.util.concurrent.CompletableFuture;
+import java.util.stream.IntStream;
+
+@Slf4j
+public class Main {
+    private static final int NUMBER_OF_POINTS = 10_000_000;
+    private static final int THREAD_POOL_SIZE = 1000;
+
+    private static final PiFinder piFinder = new PiFinder(THREAD_POOL_SIZE);
+    private static final Random random = new Random();
+    private static final List<CompletableFuture<Void>> futures = new ArrayList<>();
+    private static List<Point> points;
+
+    public static void main(String[] args) {
+        points = getPoints();
+        futures.addAll(runTask());
+        printResult();
+        piFinder.executorShutdown();
+    }
+
+    private static List<CompletableFuture<Void>> runTask() {
+        return points
+                .stream()
+                .map(piFinder::allocatePoint)
+                .toList();
+    }
+
+    private static void printResult() {
+        CompletableFuture.allOf(futures.toArray(new CompletableFuture[0]))
+                .thenRun(() -> log.info("Pi number ~ {}", piFinder.calculatePi()));
+    }
+
+    private static List<Point> getPoints() {
+        return IntStream
+                .range(0, NUMBER_OF_POINTS)
+                .mapToObj(i -> new Point(getNumber(), getNumber()))
+                .toList();
+    }
+
+    private static double getNumber() {
+        return random.nextDouble();
+    }
+}
---
+++ src/main/java/faang/school/godbless/task/multithreading/async/calculate/pi/PiFinder.java
@@ -0,0 +1,46 @@
+package faang.school.godbless.task.multithreading.async.calculate.pi;
+
+import lombok.Getter;
+
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.atomic.AtomicInteger;
+
+@Getter
+public class PiFinder {
+    private static final int RADIOS = 1;
+
+    private final AtomicInteger all = new AtomicInteger();
+    private final AtomicInteger inside = new AtomicInteger();
+    private final ExecutorService executor;
+
+    public PiFinder(int threadPoolSize) {
+        executor = Executors.newFixedThreadPool(threadPoolSize);
+    }
+
+    public CompletableFuture<Void> allocatePoint(Point point) {
+        return CompletableFuture.runAsync(() -> allocate(point), executor);
+    }
+
+    private void allocate(Point point) {
+        if (isInside(point)) {
+            inside.incrementAndGet();
+        }
+        all.incrementAndGet();
+    }
+
+    private boolean isInside(Point point) {
+        double x = point.x();
+        double y = point.y();
+        return (x * x) + (y * y) <= RADIOS * RADIOS;
+    }
+
+    public double calculatePi() {
+        return 4.0 * inside.get() / all.get();
+    }
+
+    public void executorShutdown() {
+        executor.shutdown();
+    }
+}
