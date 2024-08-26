65d9b3db41adb4394b264850765e4167d0dd460c
Ivan Tsegelnyuk <tsegelnyuk.ivan@gmail.com>
2024-08-26 20:50:29 +0300

    WOW

---
+++ src/main/java/faang/school/godbless/BJS2_24906/Player.java
@@ -0,0 +1,13 @@
+package faang.school.godbless.BJS2_24906;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@AllArgsConstructor
+@Data
+public class Player {
+
+    private final String name;
+    private int level;
+    private int experience;
+}
