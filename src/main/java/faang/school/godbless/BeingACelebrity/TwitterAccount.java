f338b797ac73d076c0832f0a9482cab9fd162a0b
Okabe <mathew.mackonoha@gmail.com>
2024-08-27 14:35:07 +0300

    Complete "Orders"

---
+++ src/main/java/faang/school/godbless/BeingACelebrity/TwitterAccount.java
@@ -0,0 +1,13 @@
+package faang.school.godbless.BeingACelebrity;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@AllArgsConstructor
+@Setter
+@Getter
+public class TwitterAccount {
+    private String username;
+    private int followers;
+}
