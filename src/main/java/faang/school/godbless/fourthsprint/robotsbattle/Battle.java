915526ef2b4f9e005708e17984832641092f8c5d
Дмитрий Бахтин <dmitridorje@gmail.com>
2024-08-28 01:01:32 +0300

    Решение задачи: Star Wars Arena

--- src/main/java/faang/school/godbless/fourthsprint/robotsbattle/Battle.java
+++ src/main/java/faang/school/godbless/fourthsprint/robotsbattle/Battle.java
@@ -5,14 +5,15 @@ import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.Future;
 
-
 public class Battle {
-    private final ExecutorService executor = Executors.newSingleThreadExecutor();
+    private static final int NUM_BATTLES = 3;
+
+    private final ExecutorService executor = Executors.newFixedThreadPool(NUM_BATTLES);
     private final Random random = new Random();
 
     public Future<Robot> fight(Robot robot1, Robot robot2) {
         return executor.submit(() -> {
-            int comparison = Integer.compare(robot1.getPower(), robot2.getPower());
+            int comparison = robot1.getPower() - robot2.getPower();
             if (comparison == 0) {
                 return random.nextBoolean() ? robot1 : robot2;
             } else {
--- src/main/java/faang/school/godbless/fourthsprint/robotsbattle/Tournament.java
+++ src/main/java/faang/school/godbless/fourthsprint/robotsbattle/Tournament.java
@@ -1,24 +1,39 @@
 package faang.school.godbless.fourthsprint.robotsbattle;
 
+import java.util.List;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.Future;
+import java.util.stream.IntStream;
 
 public class Tournament {
     public static void main(String[] args) {
         Battle battle = new Battle();
+        List<Robot> robots = getRobots();
 
-        Robot r2d2 = new Robot("R2-D2", 5, 7);
-        Robot c3po = new Robot("C-3PO", 4, 8);
-
-        Future<Robot> winnerFuture = battle.fight(r2d2, c3po);
+        List<Future<Robot>> brawls = IntStream.range(0, robots.size() / 2)
+                .mapToObj(index -> battle.fight(robots.get(2 * index), robots.get(2 * index + 1)))
+                .toList();
 
         try {
-            Robot winner = winnerFuture.get();
-            System.out.println("Победитель битвы: " + winner.getName());
+            for (Future<Robot> brawl : brawls) {
+                Robot winner = brawl.get();
+                System.out.println("\nПобедитель битвы: " + winner.getName());
+            }
         } catch (InterruptedException | ExecutionException e) {
             e.printStackTrace();
         }
 
         battle.shutdown();
+    }
+
+    private static List<Robot> getRobots() {
+        return List.of(
+                new Robot("R2-D2", 5, 7),
+                new Robot("C-3PO", 4, 8),
+                new Robot("BB-8", 6, 6),
+                new Robot("K-2SO", 7, 5),
+                new Robot("IG-88", 8, 4),
+                new Robot("L3-37", 6, 7)
+        );
     }
 }
