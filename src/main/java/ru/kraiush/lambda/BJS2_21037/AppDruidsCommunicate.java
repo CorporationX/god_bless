72dbead24ce5b9cd6eacf889954ef753ffdbb0e7
Alexander Kraiushkin <akraiushkin@gmail.com>
2024-08-27 21:26:59 +0300

    BJS2-23817: The tournament at King's Landing

--- src/main/java/ru/kraiush/lambda/BJS2_21037/AppDruidsCommunicate.java
+++ src/main/java/ru/kraiush/lambda/BJS2_21037/AppDruidsCommunicate.java
@@ -6,20 +6,20 @@ public class AppDruidsCommunicate {
 
     public static void main(String[] args) {
 
-        String message= "learning is funny";
+        String message = "learning is funny";
         System.out.print(String.join("", Collections.nCopies(80, "-")));
         System.out.println("\nMessage to send: \n" + message);
 
         DruidProducer druidProducer = new DruidProducer();
 
-        String sended= druidProducer.sendEncryptedMessage(message, 77);
-        sended= sended.replaceAll("[\\s]{2,}", " ");
+        String sended = druidProducer.sendEncryptedMessage(message, 77);
+        sended = sended.replaceAll("[\\s]{2,}", " ");
         System.out.print(String.join("", Collections.nCopies(80, "-")));
         System.out.println("\nEncrypted sended message: \n" + sended);
 
         DruidReceiver druidReceiver = new DruidReceiver();
 
-        String  receive= druidReceiver.receiveEncryptedMessage(sended, 77);
+        String receive = druidReceiver.receiveEncryptedMessage(sended, 77);
         receive = receive.replaceAll("[\\s]{2,}", " ");
         System.out.print(String.join("", Collections.nCopies(80, "-")));
         System.out.println("\nEncrypted received message: \n" + receive);
