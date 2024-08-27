4a7aca9022b53270d3fa0a0e8e84b0bda2c73208
Dmitriy Varankin <dima.varankin2012@yandex.ru>
2024-08-27 11:28:07 +0300

    3 версия решения задачи: Вы работаете в Microsoft!

--- src/main/java/faang/school/godbless/BJS2_23689/MailSender.java
+++ src/main/java/faang/school/godbless/BJS2_23689/MailSender.java
@@ -3,17 +3,16 @@ package faang.school.godbless.BJS2_23689;
 public class MailSender {
     public static void main(String[] args) {
 
-        Thread[] arrayThread = new Thread[4];
+        Thread[] arrayThread = new Thread[5];
 
         for (int i = 0; i < arrayThread.length; i++) {
             int startIndex = i * 200;
             int endIndex = startIndex + 200;
             arrayThread[i] = new Thread(
                     () -> {
-                        SenderRunnable secondPoolEmail = new SenderRunnable(startIndex, endIndex);
-                        for (int j = secondPoolEmail.getStartIndex(); j < secondPoolEmail.getEndIndex(); j++) {
-                            secondPoolEmail.run();
-                        }
+                        System.out.println("Запуск потока для индексов: " + startIndex + " - " + endIndex);
+                        new SenderRunnable(startIndex, endIndex).run();
+                        System.out.println("Завершение потока для индексов: " + startIndex + " - " + endIndex);
                     }
             );
             arrayThread[i].start();
--- src/main/java/faang/school/godbless/BJS2_23689/SenderRunnable.java
+++ src/main/java/faang/school/godbless/BJS2_23689/SenderRunnable.java
@@ -7,11 +7,15 @@ import lombok.Data;
 @AllArgsConstructor
 public class SenderRunnable implements Runnable {
 
-    private  int startIndex;
-    private  int endIndex;
+    private int startIndex;
+    private int endIndex;
 
     @Override
     public void run() {
-        System.out.println("письмо отправленно");
+        System.out.println("Запуск SenderRunnable: startIndex=" + startIndex + ", endIndex=" + endIndex);
+        for (int j = startIndex; j < endIndex; j++) {
+            System.out.println("письмо c индексом " + j + " отправленно");
+        }
+        System.out.println("Завершение SenderRunnable: startIndex=" + startIndex + ", endIndex=" + endIndex);
     }
 }
