bc9e4630475aa5df60d4e50bf206fafc9e32d2d0
Ilia Chudov <chudow-iluha@mail.ru>
2024-08-27 13:42:24 +0300

    Поправил метод calculateTotalPower().

--- src/main/java/faang/school/godbless/BJS2_23410/Army.java
+++ src/main/java/faang/school/godbless/BJS2_23410/Army.java
@@ -5,18 +5,30 @@ import lombok.Setter;
 
 import java.util.ArrayList;
 import java.util.List;
+
 @Getter
 @Setter
 public class Army {
     private List<Unit> armyList = new ArrayList<>();
+    private int totalPower;
 
     public void addUnit(Unit unit) {
         armyList.add(unit);
     }
 
     public int calculateTotalPower() {
-        return armyList.parallelStream()
-                .mapToInt(Unit::getPower)
-                .sum();
+        List<Thread> threads = new ArrayList<>();
+        for (Unit unit : armyList) {
+            threads.add(new Thread(() -> totalPower += unit.getPower()));
+        }
+        threads.forEach(Thread::start);
+        threads.forEach(thread -> {
+            try {
+                thread.join();
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+        });
+        return totalPower;
     }
 }
