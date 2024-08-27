dfe0523a10c860df96523de0d70ec1d900d678a1
Arstan Subanov <arstansubanov@gmail.com>
2024-08-27 22:24:47 +0600

    Fan out – Fan In

---
+++ src/main/java/faang/school/godbless/BJS2_25069/Main.java
@@ -0,0 +1,59 @@
+package faang.school.godbless.BJS2_25069;
+
+import lombok.extern.slf4j.Slf4j;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
+
+@Slf4j
+public class Main {
+
+    private static final ExecutorService executorService = Executors.newFixedThreadPool(Runtime.getRuntime().availableProcessors());
+
+    public static void main(String[] args) {
+        launch();
+    }
+
+    public static Long fanOutFanIn(List<SquareRequest> requests, ResultConsumer resultConsumer) {
+
+        List<CompletableFuture<Void>> futures = new ArrayList<>();
+
+        requests.forEach(request -> {
+            CompletableFuture<Void> future = CompletableFuture.runAsync(() -> request.longTimeSquare(resultConsumer), executorService);
+            futures.add(future);
+        });
+
+        CompletableFuture.allOf(futures.toArray(new CompletableFuture[0])).join();
+
+        executorService.shutdown();
+
+        try {
+            if (executorService.awaitTermination(5, TimeUnit.MINUTES)) {
+                System.out.println("All threads are completed");
+            } else {
+                executorService.shutdownNow();
+            }
+        } catch (InterruptedException e) {
+            log.error(e.getMessage());
+            throw new RuntimeException(e);
+        }
+
+        return resultConsumer.add(0L);
+    }
+
+    public static void launch() {
+        List<SquareRequest> requests = new ArrayList<>();
+        for (long i = 1; i <= 1000; i++) {
+            requests.add(new SquareRequest(i));
+        }
+        ResultConsumer resultConsumer = new ResultConsumer(0L);
+
+        long result = fanOutFanIn(requests, resultConsumer);
+
+        System.out.println("Result: " + result);
+    }
+}
