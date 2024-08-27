6a7eaadd210a0543d6448051616d60d56325e267
DenysDovgan <ytcsgoatlas@gmail.com>
2024-08-28 00:39:21 +0200

    Updated

--- src/main/java/faang/school/godbless/sprint3/BJS2_23658/MailSender.java
+++ src/main/java/faang/school/godbless/sprint3/BJS2_23658/MailSender.java
@@ -2,14 +2,14 @@ package faang.school.godbless.sprint3.BJS2_23658;
 
 public class MailSender {
     public static void main(String[] args) {
-        int totalMails = 800;
+        int totalMails = 1000;
         int numberOfThreads = 5;
         int mailsPerThread = totalMails / numberOfThreads;
 
         Thread[] threads = new Thread[numberOfThreads];
 
         for (int i = 0; i < numberOfThreads; i++) {
-            int startIndex = i * mailsPerThread + 1;
+            int startIndex = i * (mailsPerThread + 1);
             int endIndex;
             if (i == numberOfThreads - 1) {
                 endIndex = totalMails;
