5cb937c3cfef9c51b50f841fa6fd4d5aac50b600
Dmitriy Varankin <dima.varankin2012@yandex.ru>
2024-08-27 20:24:03 +0300

    2 версия решения задачи: Блокировка Spotify

--- src/main/java/faang/school/godbless/BJS2_24491/Music.java
+++ src/main/java/faang/school/godbless/BJS2_24491/Music.java
@@ -1,19 +1,22 @@
 package faang.school.godbless.BJS2_24491;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Music {
     public static void main(String[] args) {
 
         Player player = new Player();
 
-        Thread playThread = new Thread(() -> player.play());
-        Thread pauseThread = new Thread(() -> player.pause());
-        Thread skipThread = new Thread(() -> player.skip());
-        Thread previousThread = new Thread(() -> player.previous());
+        List<Thread> threadPool = new ArrayList<>();
 
-        playThread.start();
-        pauseThread.start();
-        skipThread.start();
-        previousThread.start();
+        threadPool.add(new Thread(player::play));
+        threadPool.add(new Thread(player::pause));
+        threadPool.add(new Thread(player::skip));
+        threadPool.add(new Thread(player::previous));
 
+        for (Thread thread : threadPool){
+            thread.start();
+        }
     }
 }
--- src/main/java/faang/school/godbless/BJS2_24491/Player.java
+++ src/main/java/faang/school/godbless/BJS2_24491/Player.java
@@ -2,36 +2,52 @@ package faang.school.godbless.BJS2_24491;
 
 
 public class Player {
-    private static Object lock = new Object();
-    private static boolean isPlaying = false;
+    private Object lock = new Object();
+    private boolean isPlaying = false;
 
-    public static boolean isPlaying(){
-        return isPlaying;
-    }
 
     public void play() {
-        synchronized (lock){
-            isPlaying = true;
-            System.out.println("Заиграл лучший трек");
+        synchronized (lock) {
+            if (isPlaying) {
+                System.out.println("Музыка продолжает играть");
+            } else {
+                isPlaying = true;
+                System.out.println("Заиграл лучший трек");
+            }
         }
     }
 
     public void pause() {
-        synchronized (lock){
-            isPlaying = false;
-            System.out.println("Лучший трек поставлен на паузу");
+        synchronized (lock) {
+            if (isPlaying) {
+                isPlaying = false;
+                System.out.println("Лучший трек поставлен на паузу");
+            } else {
+                System.out.println("Лучший трек уже на паузе");
+            }
+
         }
     }
 
     public void skip() {
-        synchronized (lock){
-            isPlaying = true;
-            System.out.println("Включился следующий трек");
+        synchronized (lock) {
+            if (isPlaying) {
+                System.out.println("Включился следующий трек");
+            } else {
+                isPlaying = true;
+                System.out.println("Включился следующий трек");
+            }
         }
     }
 
     public void previous() {
-        isPlaying = true;
-        System.out.println("Включился предыдуший лучший трек");
+        synchronized (lock) {
+            if (isPlaying) {
+                System.out.println("Включился предыдуший лучший трек");
+            } else {
+                isPlaying = true;
+                System.out.println("Включился предыдуший лучший трек");
+            }
+        }
     }
 }
