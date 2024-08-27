70ba1bd130cfbb54317542b1d5f85cac2b9b8e5e
Robert Oguzov <robertoguzov2@gmail.com>
2024-08-27 15:40:47 +0300

    Создал class Donation

---
+++ src/main/java/faang/school/godbless/BJS2_25400/Donation.java
@@ -0,0 +1,11 @@
+package faang.school.godbless.BJS2_25400;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class Donation {
+    private int id;
+    private int amount;
+}
