28b8900798d351e2a728473fc50102d52544b47f
Ilia Chudov <chudow-iluha@mail.ru>
2024-08-27 21:09:02 +0300

    Созданы классы Student, School, Task, Tournament, MagicalTournament.

---
+++ src/main/java/faang/school/godbless/BJS2_25120/Task.java
@@ -0,0 +1,16 @@
+package faang.school.godbless.BJS2_25120;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Setter
+@Getter
+@ToString
+@AllArgsConstructor
+public class Task {
+    private final String name;
+    private final int difficulty;
+    private final int reward;
+}
