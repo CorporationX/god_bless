88139d6de2cbb0f70bef233b936ae38ce188dc03
Dmitriy Varankin <dima.varankin2012@yandex.ru>
2024-08-27 15:54:18 +0300

    2 версия решения задачи: Накормите Питера Гриффина

--- src/main/java/faang/school/godbless/BJS2_24177/FoodDeliveryTask.java
+++ src/main/java/faang/school/godbless/BJS2_24177/FoodDeliveryTask.java
@@ -1,33 +1,30 @@
 package faang.school.godbless.BJS2_24177;
 
 import lombok.AllArgsConstructor;
-import lombok.Data;
 
 import java.util.Random;
 import java.util.concurrent.TimeUnit;
 
-@Data
 @AllArgsConstructor
 public class FoodDeliveryTask implements Runnable {
 
     private String character;
     private int foodAmount;
-
+    static final int DELIVERY_TIME_SECOND = 5;
 
     @Override
     public void run() {
-        int deliveryTimeSecond = 5;
         String eat = getFoodType();
         System.out.format("%s получает %d %s%n", character, foodAmount, eat);
-        System.out.println("Курьер в пути. Время доставки: " + deliveryTimeSecond);
+        System.out.println("Курьер в пути. Время доставки: " + DELIVERY_TIME_SECOND);
         try {
-            TimeUnit.SECONDS.sleep(deliveryTimeSecond);
+            TimeUnit.SECONDS.sleep(DELIVERY_TIME_SECOND);
         } catch (InterruptedException e) {
             e.printStackTrace();
+            Thread.currentThread().interrupt();
         }
         System.out.println(character + " получил заказ");
-        System.out.format("%s объелся %s", character, eat);
-        System.out.println();
+        System.out.format("%s объелся %s \n", character, eat);
     }
 
     private String getFoodType() {
--- src/main/java/faang/school/godbless/BJS2_24177/GriffinsFoodDelivery.java
+++ src/main/java/faang/school/godbless/BJS2_24177/GriffinsFoodDelivery.java
@@ -1,21 +1,39 @@
 package faang.school.godbless.BJS2_24177;
 
+import java.util.List;
 import java.util.Random;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
 
 public class GriffinsFoodDelivery {
     private static final String[] characterNames = {"Peter", "Lois", "Meg", "Chris", "Stewie"};
 
     public static void main(String[] args) {
-        ExecutorService pollThread = Executors.newFixedThreadPool(3);
+        ExecutorService executorService = Executors.newFixedThreadPool(3);
         for (int i = 0; i < characterNames.length; i++) {
             int index = i;
-            pollThread.submit(() -> {
-                int randomNumber = new Random().nextInt(1, 50);
-                FoodDeliveryTask newTask = new FoodDeliveryTask(characterNames[index], randomNumber);
-                newTask.run();
+            int randomNumber = new Random().nextInt(1, 50);
+            executorService.submit(() -> {
+                new FoodDeliveryTask(characterNames[index], randomNumber).run();
             });
+        }
+
+        executorService.shutdown();
+
+        try {
+            if (!executorService.awaitTermination(30, TimeUnit.SECONDS)) {
+                System.out.println("Время вышло. инициализируем shutdownNow.");
+                List<Runnable> notExecutedTasks = executorService.shutdownNow();
+                System.out.println("Количество не выполненых задач: " + notExecutedTasks.size());
+            } else {
+                System.out.println("Все задачи в пуле потоков выполенны");
+            }
+        } catch (InterruptedException e) {
+            System.out.println("Ожидание завершения было прервано. Запускаем завершение работы прямо сейчас");
+            List<Runnable> notExecutedTasks = executorService.shutdownNow();
+            System.out.println("Количество не выполненых задач: " + notExecutedTasks.size());
+            Thread.currentThread().interrupt();
         }
     }
 }
