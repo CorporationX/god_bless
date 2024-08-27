ac606a92c9907becf2691dd172d06fde0bf52ac8
Robert Oguzov <robertoguzov2@gmail.com>
2024-08-27 16:36:00 +0300

    Создал class Participant

---
+++ src/main/java/faang/school/godbless/BJS2_25710/Participant.java
@@ -0,0 +1,10 @@
+package faang.school.godbless.BJS2_25710;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class Participant {
+    private String name;
+}
