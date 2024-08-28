9ed84d48a38979ccfec64dd91c27275ec1db2b0c
Murzin34 <108129795+Murzin34@users.noreply.github.com>
2024-08-28 12:10:12 +0300

    BJS2-25236 Поддержим животных

---
+++ src/main/java/faang/school/godbless/modul4/animals/Donation.java
@@ -0,0 +1,11 @@
+package faang.school.godbless.modul4.animals;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class Donation {
+    private long id;
+    private long donate;
+}
