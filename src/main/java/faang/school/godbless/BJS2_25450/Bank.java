43fb0136cfe2a1ab85dfc9fcc5f4ecab93c40b0e
Robert Oguzov <robertoguzov2@gmail.com>
2024-08-28 13:20:47 +0300

    Внес правки

--- src/main/java/faang/school/godbless/BJS2_25450/Bank.java
+++ src/main/java/faang/school/godbless/BJS2_25450/Bank.java
@@ -2,30 +2,36 @@ package faang.school.godbless.BJS2_25450;
 
 import lombok.Getter;
 
-import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Map;
 import java.util.Set;
+import java.util.concurrent.ConcurrentHashMap;
 
 @Getter
 public class Bank {
-    private Map<Integer, Account> accounts = new HashMap<>();
+    private Map<Integer, Account> accounts = new ConcurrentHashMap<>();
 
     public boolean transfer(Account from, Account to, int amount) {
         if (from.equals(to)) {
             return false;
         }
 
-        synchronized (from.getLock()) {
-            if (from.getBalance() >= amount) {
+        Object lock1 = from.getLock();
+        Object lock2 = to.getLock();
+        if (from.getId() < to.getId()) {
+            lock1 = to.getLock();
+            lock2 = from.getLock();
+        }
 
-                synchronized (to.getLock()) {
+        synchronized (lock1) {
+            synchronized (lock2) {
+                if (from.getBalance() >= amount) {
                     System.out.printf("Перевод! Кому: %s, От кого: %s, Сумма: %d%n", from.getName(), to.getName(), amount);
                     from.withdraw(amount);
                     to.deposit(amount);
-                }
-                return true;
 
+                    return true;
+                }
             }
         }
         return false;
