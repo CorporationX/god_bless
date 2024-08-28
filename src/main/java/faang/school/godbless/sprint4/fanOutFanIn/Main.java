9225ad3b3484ee10f7675ce1de20ebe164d341e3
vestmen <alex.nemtsev95@gmail.com>
2024-08-28 10:23:40 +0300

    BJS2-25107 Fan out – Fan In декомпозировал

--- src/main/java/faang/school/godbless/sprint4/fanOutFanIn/Main.java
+++ src/main/java/faang/school/godbless/sprint4/fanOutFanIn/Main.java
@@ -6,28 +6,37 @@ import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.stream.LongStream;
+import java.util.stream.Stream;
 
 public class Main {
+    private static final int QUANTITY_OF_REQUESTS = 1000;
     private static final List<SquareRequest> requests = new ArrayList<>();
     private static final ResultConsumer resultConsumer = new ResultConsumer(0L);
     private static ExecutorService executor;
 
     public static void main(String[] args) {
         launch();
+        executor.shutdown();
     }
 
-    public static Long fanOutFanIn() {
-        CompletableFuture.allOf(requests.stream()
-                .map(request -> CompletableFuture
-                        .runAsync(() -> request.longTimeSquare(resultConsumer), executor))
-                .toArray(CompletableFuture[]::new)).join();
+    private static Long fanOutFanIn() {
+        CompletableFuture
+                .allOf(streamOfCompletableFutures()
+                        .toArray(CompletableFuture[]::new))
+                .join();
         return resultConsumer.add(0L);
     }
 
-    public static void launch() {
-        LongStream.rangeClosed(1, 1000).forEach(number -> requests.add(new SquareRequest(number)));
+    private static Stream<CompletableFuture<Void>> streamOfCompletableFutures() {
+        return requests.stream()
+                .map(request -> CompletableFuture
+                        .runAsync(() -> request.longTimeSquare(resultConsumer), executor));
+    }
+
+    private static void launch() {
+        LongStream.rangeClosed(1, QUANTITY_OF_REQUESTS)
+                .forEach(number -> requests.add(new SquareRequest(number)));
         executor = Executors.newFixedThreadPool(requests.size());
         System.out.println(fanOutFanIn());
-        executor.shutdown();
     }
 }
