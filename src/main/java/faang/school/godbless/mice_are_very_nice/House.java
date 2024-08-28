b68412a3946df56b1d25e6a39d05129ceac537ac
Varvara Stepnova <stepnova.varya@yandex.ru>
2024-08-28 07:10:07 +0300

    BJS2-23837-mice_are_very_nice: fix

--- src/main/java/faang/school/godbless/mice_are_very_nice/House.java
+++ src/main/java/faang/school/godbless/mice_are_very_nice/House.java
@@ -12,35 +12,25 @@ public class House {
     private static List<Food> foods = new ArrayList<>();
     private static List<Food> collectedFood = new ArrayList<>();
 
-    private static final int FOOD_COUNT = 10;
+    private static final int FOOD_COUNT = 7;
     private static final int POOL_SIZE = 5;
+    private static final int WAITING_TIME = 3;
 
     public static void main(String[] args) {
-        Room kitchen = new Room(new ArrayList<>());
-        Room bedroom = new Room(new ArrayList<>());
-
-        for (int i = 0; i < FOOD_COUNT; i++) {
-            foods.add(new Food("Product" + i));
-            if (i < 5) {
-                kitchen.getFoods().add(new Food("Product" + i));
-            } else {
-                bedroom.getFoods().add(new Food("Product" + i));
-            }
-        }
-        rooms.add(kitchen);
-        rooms.add(bedroom);
+        addFoodRooms();
+        addFoodRooms();
 
         ScheduledExecutorService executor = Executors.newScheduledThreadPool(POOL_SIZE);
         int delay = 0;
         for (int i = 0; i < POOL_SIZE; i++) {
             executor.schedule(House::collectFood, delay, TimeUnit.SECONDS);
-            delay += 10;
+            delay += 30;
         }
 
         executor.shutdown();
 
         try {
-            if (executor.awaitTermination(10, TimeUnit.MINUTES)) {
+            if (executor.awaitTermination(WAITING_TIME, TimeUnit.MINUTES)) {
                 System.out.println("Еда собрана, кол-во собранных продуктов = " + collectedFood.size());
             }
         } catch (InterruptedException e) {
@@ -54,5 +44,15 @@ public class House {
             room.getFoods().remove(room.getFoods().get(0));
             System.out.println("Добавили продукт - " + collectedFood.get(collectedFood.size() - 1).getProductName());
         }
+    }
+
+    private static void addFoodRooms() {
+        Room room = new Room(new ArrayList<>());
+        for (int i = 0; i < FOOD_COUNT; i++) {
+            foods.add(new Food("Product" + i));
+            room.getFoods().add(new Food("Product" + i));
+        }
+
+        rooms.add(room);
     }
 }
