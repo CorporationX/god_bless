b5151b2999586613b36cd8465eeef99e6a231c16
Bobiktrans <Bobicktrnas@yandex.ru>
2024-08-28 12:21:25 +0300

    BJS2-23514

--- src/main/java/faang/school/godbless/Application.java
+++ src/main/java/faang/school/godbless/Application.java
@@ -1,7 +1,26 @@
 package faang.school.godbless;
 
+import faang.school.godbless.Trial.Trial;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
 public class Application {
     public static void main(String[] args) {
-        System.out.println("God Bless!");
+        Knight knight1 = new Knight("Сэр Джейме");
+        Knight knight2 = new Knight("Сэр Лорас");
+
+        knight1.addTrial(new Trial(knight1.getName(), "Бой на мечах"));
+        knight1.addTrial(new Trial(knight1.getName(), "Турнир на копьях"));
+
+        knight2.addTrial(new Trial(knight2.getName(), "Стрельба из лука"));
+        knight2.addTrial(new Trial(knight2.getName(), "Борьба"));
+
+        ExecutorService executorService = Executors.newFixedThreadPool(2);
+
+        knight1.startTrials(executorService);
+        knight2.startTrials(executorService);
+
+        executorService.shutdown();
     }
 }
---
+++ src/main/java/faang/school/godbless/Knight.java
@@ -0,0 +1,29 @@
+package faang.school.godbless;
+
+import faang.school.godbless.Trial.Trial;
+import lombok.Getter;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.ExecutorService;
+
+@Getter
+public class Knight {
+    private String name;
+    private List<Trial> trials;
+
+    public Knight(String name) {
+        this.name = name;
+        this.trials = new ArrayList<>();
+    }
+
+    public void addTrial(Trial trial) {
+        trials.add(trial);
+    }
+
+    public void startTrials(ExecutorService executorService) {
+        for (Trial trial : trials) {
+            executorService.submit(trial);
+        }
+    }
+}
