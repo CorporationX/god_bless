647d1ac95365e7bf17e5ad58d29437ee739285fb
Дмитрий Бахтин <dmitridorje@gmail.com>
2024-08-28 22:57:49 +0300

    рефакторинг метода calculatePi()

--- src/main/java/faang/school/godbless/fourthsprint/pi/PiCalculator.java
+++ src/main/java/faang/school/godbless/fourthsprint/pi/PiCalculator.java
@@ -1,24 +1,37 @@
 package faang.school.godbless.fourthsprint.pi;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutionException;
 import java.util.concurrent.ThreadLocalRandom;
 import java.util.stream.IntStream;
 
 public class PiCalculator {
     private final static int NUM_POINTS = Integer.MAX_VALUE;
+    private final static int NUM_TASKS = 8;
 
-    public double calculatePi(int n) {
-        CompletableFuture<Long> insideCircleFuture = CompletableFuture.supplyAsync(() ->
-                IntStream.range(0, n)
-                        .parallel()
-                        .mapToObj(iteration -> generateRandomPoint())
-                        .filter(Point::isInsideCircle)
-                        .count()
-        );
+    public CompletableFuture<Double> calculatePi(int n) {
+        List<CompletableFuture<Long>> futures = new ArrayList<>();
+        int pointsPerTask = n / NUM_TASKS;
 
-        long insideCircle = insideCircleFuture.join();
+        for (int i = 0; i < NUM_TASKS; i++) {
+            CompletableFuture<Long> future = CompletableFuture.supplyAsync(() ->
+                    IntStream.range(0, pointsPerTask)
+                            .mapToObj(iteration -> generateRandomPoint())
+                            .filter(Point::isInsideCircle)
+                            .count()
+            );
+            futures.add(future);
+        }
 
-        return 4.0 * insideCircle / n;
+        CompletableFuture<Long> totalInsideCircle = CompletableFuture.allOf(futures.toArray(new CompletableFuture[0]))
+                .thenApply(v -> futures.stream()
+                        .mapToLong(CompletableFuture::join)
+                        .sum()
+                );
+
+        return totalInsideCircle.thenApply(insideCircle -> 4.0 * insideCircle / n);
     }
 
     private Point generateRandomPoint() {
@@ -27,9 +40,10 @@ public class PiCalculator {
         return new Point(x, y);
     }
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws ExecutionException, InterruptedException {
         PiCalculator calculator = new PiCalculator();
-        double pi = calculator.calculatePi(NUM_POINTS);
-        System.out.println("Calculated value of Pi: " + pi);
+        calculator.calculatePi(NUM_POINTS)
+                .thenAccept(pi -> System.out.println("Calculated value of Pi: " + pi))
+                .join();
     }
 }
