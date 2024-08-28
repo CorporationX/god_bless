78cca5dfff257c72e31a43b1a01883c5d6418927
Peregruzochka <peregruzochka48@mail.ru>
2024-08-28 14:10:13 +0300

    реализовал сложный способ посчитать ингредиенты

---
+++ src/main/java/faang/school/godbless/BJS2_25114/Potion.java
@@ -0,0 +1,11 @@
+package faang.school.godbless.BJS2_25114;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@AllArgsConstructor
+public class Potion {
+    private final String name;
+    @Getter
+    private final int requiredIngredients;
+}
