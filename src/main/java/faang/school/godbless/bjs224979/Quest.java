68edee5eabb2844fc5d70e4001f0dbb1ddf5c1f4
Anton Martynenko <ant.martynenko@gmail.com>
2024-08-28 15:25:07 +0200

    Решение задачи "WOW"

---
+++ src/main/java/faang/school/godbless/bjs224979/Quest.java
@@ -0,0 +1,13 @@
+package faang.school.godbless.bjs224979;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class Quest {
+
+    private String name;
+    private int difficulty;
+    private int reward;
+}
