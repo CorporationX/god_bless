68d9efdfc35d8815d9e57422690b694ac48596b9
Robert Oguzov <robertoguzov2@gmail.com>
2024-08-28 09:48:27 +0300

    Написал class VehicleTracker

---
+++ src/main/java/faang/school/godbless/BJS2_25405/VehicleTracker.java
@@ -0,0 +1,42 @@
+package faang.school.godbless.BJS2_25405;
+
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.ThreadLocalRandom;
+import java.util.concurrent.TimeUnit;
+
+public class VehicleTracker {
+
+    private TransportManagementSystem transportManagement;
+    private ScheduledExecutorService scheduledExecutor;
+
+    public VehicleTracker(TransportManagementSystem transportManagement) {
+        this.transportManagement = transportManagement;
+        this.scheduledExecutor = Executors.newScheduledThreadPool(transportManagement.getCountVehicles());
+    }
+
+    public void startTracking() {
+        transportManagement.forEachValues((vehicle -> scheduledExecutor.scheduleWithFixedDelay(
+                () -> tracking(vehicle), 0, 2, TimeUnit.SECONDS)));
+    }
+
+    private void tracking(Vehicle vehicle) {
+        try {
+            Location location = vehicle.getLocation();
+            System.out.printf("Tracking vehicle %s. Her location: %s%n", vehicle.getId(), location);
+
+            boolean isNewStatus = ThreadLocalRandom.current().nextInt(1, 11) >= 8;
+            if (isNewStatus) {
+                vehicle.nextStatus();
+                if (vehicle.getStatus() == Status.FREE) {
+                    System.out.printf("The vehicle %d is free%n", vehicle.getId());
+                    Thread.sleep(5000L);
+                }
+            }
+
+            vehicle.setLocation(location.nextLocation());
+        } catch (InterruptedException e) {
+            Thread.currentThread().interrupt();
+        }
+    }
+}
