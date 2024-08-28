81429b9269e7d65d189726e9263eca0b2d44ea8b
Владислав Воронов <iPlesk90@yandex.ru>
2024-08-28 13:08:14 +0300

    Выполнил задачу

---
+++ src/main/java/faang/school/godbless/counter_youtube/Video.java
@@ -0,0 +1,16 @@
+package faang.school.godbless.counter_youtube;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Setter
+@Getter
+@AllArgsConstructor
+public class Video {
+    private String name;
+    private String id;
+}
