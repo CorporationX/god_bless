80462776ed434b97466bb0773c630cfb7620947c
Сураган Оразбаев <s.orazbayev@enbek.kz>
2024-08-29 01:41:46 +0500

    feat: Реализована группировка товаров по категориям с использованием HashSet и HashMap
    Создан класс Product для представления товаров с полями id, name и category. В классе Main реализованы методы для группировки товаров по категориям и вывода информации о них. Товары добавляются в HashSet и группируются по категориям в HashMap. Программа успешно протестирована и корректно группирует товары по категориям, выводя результаты на экран.

---
+++ src/main/java/faang/school/godbless/BJS2_19488/Product.java
@@ -0,0 +1,14 @@
+package faang.school.godbless.BJS2_19488;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.ToString;
+
+@Getter
+@AllArgsConstructor
+@ToString
+public class Product {
+    private int id;
+    private String name;
+    private String category;
+}
