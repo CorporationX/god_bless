ecdbe7df112c27dcb6067ed42255fbe90668b9a3
Alena Guskova <amguskova@edu.hse.ru>
2024-08-28 14:58:28 +0300

    BJS2-25329 Работа стоит

--- src/main/java/faang/school/godbless/deadlock/DesignDepartment.java
+++ src/main/java/faang/school/godbless/deadlock/DesignDepartment.java
@@ -2,20 +2,28 @@ package faang.school.godbless.deadlock;
 
 import lombok.AllArgsConstructor;
 
+import java.util.concurrent.locks.ReadWriteLock;
+import java.util.concurrent.locks.ReentrantReadWriteLock;
+
 @AllArgsConstructor
 public class DesignDepartment extends Thread {
-    private final MarketingResources marketingResources;
-    private final DesignResources designResources;
+    private MarketingResources marketingResources;
+    private DesignResources designResources;
+    private final ReadWriteLock lock = new ReentrantReadWriteLock();
 
     private void work() {
-        synchronized (designResources) {
-            designResources.writeFiles("Design department started working");
-            synchronized (marketingResources) {
-                marketingResources.readFiles();
-            }
-            designResources.writeFiles("Design department adding files" +
-                    " after looking at marketing resources");
-        }
+        lock.writeLock().lock();
+        designResources.writeFiles("Design department started working");
+        lock.writeLock().unlock();
+
+        lock.readLock().lock();
+        marketingResources.readFiles();
+        lock.readLock().unlock();
+
+        lock.writeLock().lock();
+        designResources.writeFiles("Design department adding files" +
+                " after looking at marketing resources");
+        lock.writeLock().unlock();
     }
 
     @Override
--- src/main/java/faang/school/godbless/deadlock/MarketingDepartment.java
+++ src/main/java/faang/school/godbless/deadlock/MarketingDepartment.java
@@ -2,20 +2,28 @@ package faang.school.godbless.deadlock;
 
 import lombok.AllArgsConstructor;
 
+import java.util.concurrent.locks.ReadWriteLock;
+import java.util.concurrent.locks.ReentrantReadWriteLock;
+
 @AllArgsConstructor
 public class MarketingDepartment extends Thread {
-    private final MarketingResources marketingResources;
-    private final DesignResources designResources;
+    private MarketingResources marketingResources;
+    private DesignResources designResources;
+    private final ReadWriteLock lock = new ReentrantReadWriteLock();
 
     private void work() {
-        synchronized (marketingResources) {
-            marketingResources.writeFiles("Marketing department starting working");
-            synchronized (designResources) {
-                designResources.readFiles();
-            }
-            marketingResources.writeFiles("Marketing department adding files" +
-                    " after looking at design resources");
-        }
+        lock.writeLock().lock();
+        marketingResources.writeFiles("Marketing department starting working");
+        lock.writeLock().unlock();
+
+        lock.readLock().lock();
+        designResources.readFiles();
+        lock.readLock().unlock();
+
+        lock.writeLock().lock();
+        marketingResources.writeFiles("Marketing department adding files" +
+                " after looking at design resources");
+        lock.writeLock().unlock();
     }
 
     @Override
