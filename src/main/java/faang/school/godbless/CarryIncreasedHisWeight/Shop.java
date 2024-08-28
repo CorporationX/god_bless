649791b15ade0814dd9bb264b41f3b106f2502b2
Okabe <mathew.mackonoha@gmail.com>
2024-08-28 13:54:49 +0300

    Сделан класс Shop

---
+++ src/main/java/faang/school/godbless/CarryIncreasedHisWeight/Shop.java
@@ -0,0 +1,13 @@
+package faang.school.godbless.CarryIncreasedHisWeight;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.List;
+@AllArgsConstructor
+@Setter
+@Getter
+public class Shop {
+    private List<Item> items;
+}
