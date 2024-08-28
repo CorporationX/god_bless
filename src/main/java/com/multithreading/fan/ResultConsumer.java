3e20862a84b6b9594182aa49bceba172c1825d69
Evgen <fragmatik.Oo@yandex.ru>
2024-08-28 18:07:36 +0300

    Fan out – Fan In

---
+++ src/main/java/com/multithreading/fan/ResultConsumer.java
@@ -0,0 +1,18 @@
+package com.multithreading.fan;
+
+import lombok.Getter;
+
+import java.util.concurrent.atomic.AtomicLong;
+
+public class ResultConsumer {
+    @Getter
+    private final AtomicLong sumOfSquaredNumbers;
+
+    ResultConsumer(Long init) {
+        sumOfSquaredNumbers = new AtomicLong(init);
+    }
+
+    public Long add(final Long num) {
+        return sumOfSquaredNumbers.addAndGet(num);
+    }
+}
