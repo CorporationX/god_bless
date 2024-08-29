da6e64fc85eb57f089b1aa81c3ede11d39f1a430
SharkB38 <val.wolf.ne@gmail.com>
2024-08-29 22:53:55 +0900

    not a fix(((

--- src/main/java/bot/TelegramBot.java
+++ src/main/java/bot/TelegramBot.java
@@ -3,44 +3,46 @@ package bot;
 import lombok.NonNull;
 import lombok.extern.slf4j.Slf4j;
 
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.concurrent.atomic.AtomicLong;
+
 @Slf4j
 public class TelegramBot {
     private static final int REQUEST_LIMIT = 10;
     private static final long TIME_LIMIT = 1000;
-    private volatile int requestCounter;
-    private long lastRequestTime;
+    private AtomicInteger requestCounter;
+    private AtomicLong lastRequestTime;
 
     public TelegramBot() {
-        requestCounter = 0;
-        lastRequestTime = System.currentTimeMillis();
+        requestCounter = new AtomicInteger(0);
+        lastRequestTime = new AtomicLong(System.currentTimeMillis());
     }
 
-    public synchronized void sendMessage(@NonNull String message) {
-        long deltaTime = System.currentTimeMillis() - lastRequestTime;
-        if (deltaTime >= TIME_LIMIT) {
-            lastRequestTime = System.currentTimeMillis();
-            requestCounter = 0;
-            notifyAll();
-        } else {
-            if (requestCounter >= REQUEST_LIMIT) {
+    public void sendMessage(@NonNull String message) {
+        long deltaTime = System.currentTimeMillis() - lastRequestTime.get();
+        if (deltaTime < TIME_LIMIT) {
+            requestCounter.getAndIncrement();
+            while (requestCounter.get() >= REQUEST_LIMIT) {
+                if (System.currentTimeMillis() - lastRequestTime.get() >= TIME_LIMIT) {
+                    break;
+                }
                 try {
-                    log.info("Waiting for send..." + (TIME_LIMIT - deltaTime));
-                    Thread.sleep(TIME_LIMIT - deltaTime);
+                    synchronized (this) {
+                        wait(TIME_LIMIT - deltaTime);
+                    }
                 } catch (InterruptedException e) {
                     Thread.currentThread().interrupt();
                     throw new RuntimeException(e);
                 }
             }
-            while (requestCounter >= REQUEST_LIMIT) {
-                try {
-                    wait();
-                } catch (InterruptedException e) {
-                    Thread.currentThread().interrupt();
-                    throw new RuntimeException(e);
-                }
+        } else {
+            lastRequestTime.set(System.currentTimeMillis());
+            requestCounter.set(0);
+            synchronized (this) {
+                notifyAll();
+                log.info("----");
             }
         }
-        requestCounter++;
         sendMessageWithAPI(message);
     }
 
