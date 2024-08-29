13102542ac6907e4dcb9a5f5a7b3eb01c59cfa40
Ruslan Iovlev <ruslan-iovlev@mail.ru>
2024-08-29 13:13:07 +0700

    Асинхронность и Future

---
+++ src/main/java/faang/school/godbless/BJS2_24861/Main.java
@@ -0,0 +1,22 @@
+package faang.school.godbless.BJS2_24861;
+
+import java.util.concurrent.*;
+
+public class Main {
+
+    public static void main(String[] args) throws ExecutionException, InterruptedException {
+        Main.doAll();
+    }
+
+    public static void doAll() throws ExecutionException, InterruptedException {
+        ExecutorService executor = Executors.newSingleThreadExecutor();
+        Future<Integer> future = executor.submit(MasterCardService::collectPayment);
+        CompletableFuture<Integer> completableFuture = CompletableFuture.supplyAsync(MasterCardService::sendAnalytics);
+        int completableFutureResult = completableFuture.get();
+        System.out.println(completableFutureResult);
+        int futureResult = future.get();
+        System.out.println(futureResult);
+        executor.shutdown();
+        completableFuture.isCancelled();
+    }
+}
