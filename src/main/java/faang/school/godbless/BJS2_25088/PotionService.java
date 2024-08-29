a1abe5f0f05a00b18c22f751ad46250ff8ea40d9
Arstan Subanov <arstansubanov@gmail.com>
2024-08-29 20:48:11 +0600

    Качаем алхимию Refactoring

--- src/main/java/faang/school/godbless/BJS2_25088/PotionService.java
+++ src/main/java/faang/school/godbless/BJS2_25088/PotionService.java
@@ -5,8 +5,6 @@ import lombok.extern.slf4j.Slf4j;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.CompletableFuture;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
 import java.util.concurrent.TimeUnit;
 import java.util.concurrent.atomic.AtomicInteger;
 
@@ -14,8 +12,6 @@ import java.util.concurrent.atomic.AtomicInteger;
 public class PotionService {
 
     private static final List<Potion> potions = new ArrayList<>();
-    private static final int THREAD_POOL_SIZE = 5;
-    private static final ExecutorService EXECUTOR_SERVICE = Executors.newFixedThreadPool(THREAD_POOL_SIZE);
     private static final AtomicInteger totalIngredients = new AtomicInteger(0);
 
     public static void main(String[] args) {
@@ -25,31 +21,32 @@ public class PotionService {
                 new Potion("Felix Felicis", 12),
                 new Potion("Memory Potion", 4)));
 
-        CompletableFuture[] futures = potions.stream()
-                .map(potion -> CompletableFuture.supplyAsync(() -> gatherIngredients(potion), EXECUTOR_SERVICE)
-                        .thenApply(ingredient -> totalIngredients.accumulateAndGet(ingredient, Integer::sum)))
-                .toArray(CompletableFuture[]::new);
+        List<CompletableFuture<Integer>> futures = potions.stream()
+                .map(PotionService::gatherIngredients)
+                .toList();
 
-        CompletableFuture.allOf(futures).join();
-        
-        EXECUTOR_SERVICE.shutdown();
+        CompletableFuture.allOf(futures.toArray(CompletableFuture[]::new)).join();
+
 
         System.out.println("Total " + totalIngredients + " potions");
     }
 
-    public static int gatherIngredients(Potion potion) {
+    public static CompletableFuture<Integer> gatherIngredients(Potion potion) {
 
-        System.out.println("Gathering ingredients for " + potion.getName());
+        return CompletableFuture.supplyAsync(() -> {
+                    System.out.println("Gathering ingredients for " + potion.getName());
 
-        try {
-            TimeUnit.SECONDS.sleep(potion.getRequiredIngredients());
-        } catch (InterruptedException e) {
-            log.error(e.getMessage());
-            throw new RuntimeException(e);
-        }
+                    try {
+                        TimeUnit.SECONDS.sleep(potion.getRequiredIngredients());
+                    } catch (InterruptedException e) {
+                        log.error(e.getMessage());
+                        throw new RuntimeException(e);
+                    }
 
-        System.out.println("Ingredients for " + potion.getName() + " are gathered");
+                    System.out.println("Ingredients for " + potion.getName() + " are gathered");
 
-        return potion.getRequiredIngredients();
+                    return potion.getRequiredIngredients();
+                })
+                .thenApply(ingredient -> totalIngredients.accumulateAndGet(ingredient, Integer::sum));
     }
 }
