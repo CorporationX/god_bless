2bb101b4da61c326dd05a4f6f8beba6f6ba48cbf
Robert Oguzov <robertoguzov2@gmail.com>
2024-08-29 13:20:57 +0300

    Написал класс Main для тестирования

---
+++ src/main/java/faang/school/godbless/BJS2_24670/Main.java
@@ -0,0 +1,37 @@
+package faang.school.godbless.BJS2_24670;
+
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+import java.util.concurrent.TimeUnit;
+
+public class Main {
+    public static void main(String[] args) {
+        ExecutorService executor = Executors.newFixedThreadPool(5);
+        Battle battle = new Battle(executor);
+
+        Robot r2d2 = new Robot("R2-D2", 5, 7);
+        Robot c3po = new Robot("C-3PO", 4, 8);
+
+        Future<Robot> winnerFuture = battle.fight(r2d2, c3po);
+
+        try {
+            Robot winner = winnerFuture.get();
+            System.out.println("Победитель битвы: " + winner.getName());
+        } catch (InterruptedException | ExecutionException e) {
+            e.printStackTrace();
+        }
+
+        executor.shutdown();
+        try {
+            if (!executor.awaitTermination(1, TimeUnit.MINUTES)) {
+                executor.shutdownNow();
+            }
+        } catch (InterruptedException e) {
+            executor.shutdownNow();
+        }
+
+        System.out.println("Game over");
+    }
+}
