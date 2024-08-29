2961479067c5ffcf89fe9d5459ebbe467519814e
Viktor Shpet <shpet.viktor.93@gmail.com>
2024-08-29 18:11:18 +0500

    BJS2-25234 solved.

---
+++ src/main/java/faang/school/godbless/BJS2_25234/Donation.java
@@ -0,0 +1,11 @@
+package faang.school.godbless.BJS2_25234;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class Donation {
+    private int id;
+    private int amount;
+}
