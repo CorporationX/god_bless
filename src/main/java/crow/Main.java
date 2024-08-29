f394a0334b009a994390b115045d9f13a6b23af8
SharkB38 <val.wolf.ne@gmail.com>
2024-08-29 13:23:32 +0900

    fix

--- src/main/java/crow/Main.java
+++ src/main/java/crow/Main.java
@@ -3,52 +3,43 @@ package crow;
 import lombok.NonNull;
 import lombok.extern.slf4j.Slf4j;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.CompletableFuture;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
-import java.util.concurrent.TimeUnit;
-import java.util.stream.Stream;
 
 @Slf4j
 public class Main {
-    private static final int POOL_SIZE = 20;
 
-    public static void main(String[] args) throws InterruptedException {
+    public static void main(String[] args) {
         var kingdoms = initKingdoms();
-        try (ExecutorService executor = Executors.newFixedThreadPool(POOL_SIZE)) {
-            var futures = kingdoms.stream()
-                    .flatMap(k -> Stream.of(k)
-                            .flatMap(i -> kingdoms.stream()
-                                    .filter(o -> !o.equals(k)))
-                            .map(o -> CompletableFuture.runAsync(
-                                            () -> sendRaven(k, o), executor)
-                                    .handle((r, e) -> {
-                                        if (e != null) {
-                                            log.error(e.getMessage());
-                                            return false;
-                                        } else {
-                                            log.info(String.format(
-                                                    "Crow successfully delivered message from %s to %s",
-                                                    k.getName(), o.getName()));
-                                            return true;
-                                        }
-                                    })))
-                    .toList();
-            futures.forEach(CompletableFuture::join);
-            executor.shutdown();
-            if (executor.awaitTermination(1, TimeUnit.MINUTES)) {
-                log.info("Finished");
-            } else {
-                log.error("Time out");
+        List<CompletableFuture<Boolean>> futures = new ArrayList<>();
+        for (int i = 0; i < kingdoms.size(); i++) {
+            for (int j = 0; j < kingdoms.size(); j++) {
+                if (!kingdoms.get(i).equals(kingdoms.get(j))) {
+                    var kingdomFrom = kingdoms.get(i);
+                    var kingdomTo = kingdoms.get(j);
+                    futures.add(sendRaven(kingdomFrom, kingdomTo)
+                            .handle((result, ex) -> {
+                                if (ex != null) {
+                                    log.error(ex.getMessage());
+                                    return false;
+                                } else {
+                                    log.info(String.format(
+                                            "Crow successfully delivered message from %s to %s",
+                                            kingdomFrom, kingdomTo));
+                                    return true;
+                                }
+                            }));
+                }
             }
         }
+        futures.forEach(CompletableFuture::join);
     }
 
-    private static void sendRaven(
+    private static CompletableFuture<Void> sendRaven(
             @NonNull Kingdom kingdomFrom, @NonNull Kingdom kingdomTo
     ) throws CrowFailedException {
-        kingdomFrom.sendMessage(kingdomTo);
+        return CompletableFuture.runAsync(() -> kingdomFrom.sendMessage(kingdomTo));
     }
 
     private static List<Kingdom> initKingdoms() {
