3c82e59147bde2e86fc97160ea548987272fc125
Robert Oguzov <robertoguzov2@gmail.com>
2024-08-29 17:20:39 +0300

    Написал класс Notification

---
+++ src/main/java/faang/school/godbless/BJS2_24920/Notification.java
@@ -0,0 +1,11 @@
+package faang.school.godbless.BJS2_24920;
+
+import lombok.AllArgsConstructor;
+import lombok.ToString;
+
+@ToString
+@AllArgsConstructor
+public class Notification {
+    private int id;
+    private String message;
+}
