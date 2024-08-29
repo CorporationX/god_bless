bfc70f19be65d8bddd5e6227e03bce9d10c49586
Anton Martynenko <ant.martynenko@gmail.com>
2024-08-29 17:05:56 +0200

    Решение задачи "Fan out – Fan In"

---
+++ src/main/java/faang/school/godbless/bjs225150/Main.java
@@ -0,0 +1,8 @@
+package faang.school.godbless.bjs225150;
+
+public class Main {
+    public static void main(String[] args) {
+        SquareSumCalculator squareSumCalculator = new SquareSumCalculator();
+        squareSumCalculator.launch(1000);
+    }
+}
