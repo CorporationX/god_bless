6d136e67805f30cce439080402ec1712f4a5ad2a
Dmitriy Varankin <dima.varankin2012@yandex.ru>
2024-08-30 18:39:28 +0300

    3 версия решения задачи: Накормите Питера Гриффина

--- src/main/java/faang/school/godbless/BJS2_24177/GriffinsFoodDelivery.java
+++ src/main/java/faang/school/godbless/BJS2_24177/GriffinsFoodDelivery.java
@@ -1,7 +1,6 @@
 package faang.school.godbless.BJS2_24177;
 
 import java.util.List;
-import java.util.Random;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.ThreadLocalRandom;
