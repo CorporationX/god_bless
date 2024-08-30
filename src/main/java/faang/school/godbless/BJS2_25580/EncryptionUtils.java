4fa620fbb73382ec7734ca804fcbc50b339f3b39
Robert Oguzov <robertoguzov2@gmail.com>
2024-08-30 11:53:39 +0300

    Написал class EncryptionUtils

---
+++ src/main/java/faang/school/godbless/BJS2_25580/EncryptionUtils.java
@@ -0,0 +1,42 @@
+package faang.school.godbless.BJS2_25580;
+
+import lombok.SneakyThrows;
+
+import javax.crypto.Cipher;
+import javax.crypto.KeyGenerator;
+import javax.crypto.SecretKey;
+import java.security.NoSuchAlgorithmException;
+import java.util.Base64;
+
+public class EncryptionUtils {
+    private static final String algorithm = "AES";
+    private static final SecretKey key;
+
+    static {
+        try {
+            KeyGenerator aes = KeyGenerator.getInstance(algorithm);
+            key = aes.generateKey();
+        } catch (NoSuchAlgorithmException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    @SneakyThrows
+    public static String encrypt(String text) {
+        Cipher cipher = Cipher.getInstance(algorithm);
+        cipher.init(Cipher.ENCRYPT_MODE, key);
+        byte[] encryptedBytes = cipher.doFinal(text.getBytes());
+
+        return Base64.getEncoder().encodeToString(encryptedBytes);
+    }
+
+    @SneakyThrows
+    public static String decrypt(String text) {
+        Cipher cipher = Cipher.getInstance(algorithm);
+        cipher.init(Cipher.DECRYPT_MODE, key);
+        byte[] decodedBytes = Base64.getDecoder().decode(text);
+        byte[] decryptedBytes = cipher.doFinal(decodedBytes);
+
+        return new String(decryptedBytes);
+    }
+}
