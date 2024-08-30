a493bf4152c64f4fb35d24120aac1b90daf4bfaa
SharkB38 <val.wolf.ne@gmail.com>
2024-08-30 17:57:46 +0900

    final final fix?

--- src/main/java/bot/TelegramBot.java
+++ src/main/java/bot/TelegramBot.java
@@ -3,9 +3,6 @@ package bot;
 import lombok.NonNull;
 import lombok.extern.slf4j.Slf4j;
 
-import java.util.concurrent.atomic.AtomicInteger;
-import java.util.concurrent.atomic.AtomicLong;
-
 @Slf4j
 public class TelegramBot {
     private static final int REQUEST_LIMIT = 10;
