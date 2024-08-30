c72c1ad10560cf1752634f882a4780a49455f443
Robert Oguzov <robertoguzov2@gmail.com>
2024-08-30 14:13:20 +0300

    Написал class MilitaryBase

---
+++ src/main/java/faang/school/godbless/BJS2_25700/MilitaryBase.java
@@ -0,0 +1,37 @@
+package faang.school.godbless.BJS2_25700;
+
+import lombok.SneakyThrows;
+
+import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.LinkedBlockingQueue;
+
+public class MilitaryBase implements Runnable {
+    private static BlockingQueue<String> inbox = new LinkedBlockingQueue<>();
+    private volatile boolean running = true;
+
+    @Override
+    @SneakyThrows
+    public void run() {
+        while (running) {
+            Thread.sleep(1000L);
+            String encryptMessage = inbox.poll();
+
+            if (encryptMessage != null) {
+                String decryptMessage = EncryptionUtils.decrypt(encryptMessage);
+                if (running) {
+                    System.out.printf("Base received a message: %s%n", decryptMessage);
+                }
+            }
+        }
+    }
+
+    public static void sendMessage(String message) {
+        String encryptMessage = EncryptionUtils.encrypt(message);
+        inbox.offer(encryptMessage);
+    }
+
+    public void stop() {
+        System.out.println("Stopping Military Base");
+        running = false;
+    }
+}
