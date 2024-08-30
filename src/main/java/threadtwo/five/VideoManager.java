ee1b78091081274f7b1dff99d4968cc7669dbac5
Maksim Kononov <07maks7@mail.ru>
2024-08-30 22:13:48 +0300

    refactoring after review

--- src/main/java/threadtwo/five/VideoManager.java
+++ src/main/java/threadtwo/five/VideoManager.java
@@ -11,22 +11,23 @@ import java.util.concurrent.TimeUnit;
 public class VideoManager {
     private static final int NUM_THREADS = 100;
     private static final int NUM_VIDEOS = 10;
-    private static final Map<String, Video> VIDEOS = new HashMap<>();
+    private final Map<String, Video> videos = new HashMap<>();
     private static final ExecutorService EXECUTOR = Executors.newFixedThreadPool(NUM_THREADS);
 
     public static void main(String[] args) {
+        VideoManager videoManager = new VideoManager();
 
         for (int i = 0; i < NUM_VIDEOS; i++) {
             String videoId = String.valueOf(i);
-            VIDEOS.put(videoId, new Video(videoId, 0));
+            videoManager.videos.put(videoId, new Video(videoId, 0));
             System.out.println("Video created with ID: " + videoId);
 
             for (int j = 0; j < NUM_THREADS / NUM_VIDEOS; j++) {
                 EXECUTOR.execute(() -> {
                     System.out.println("Thread " + Thread.currentThread().getId() +
                             " started for video index: " + videoId);
-                    addView(videoId);
-                    int view = getViewCount(videoId);
+                    videoManager.addView(videoId);
+                    int view = videoManager.getViewCount(videoId);
                     System.out.println("Thread " + Thread.currentThread().getId() +
                             " processed video index: " + videoId + ", view count: " + view);
                 });
@@ -43,18 +44,17 @@ public class VideoManager {
         }
     }
 
-    public static synchronized void addView(@NonNull String videoId) {
-        Video video = VIDEOS.get(videoId);
+    public synchronized void addView(@NonNull String videoId) {
+        Video video = videos.get(videoId);
         if (video == null) {
             throw new IllegalArgumentException("Video not found: " + videoId);
         }
         video.setView(video.getView() + 1);
-
     }
 
-    public static synchronized int getViewCount(@NonNull String videoId) {
-        if (VIDEOS.containsKey(videoId)) {
-            return VIDEOS.get(videoId).getView();
+    public synchronized int getViewCount(@NonNull String videoId) {
+        if (videos.containsKey(videoId)) {
+            return videos.get(videoId).getView();
         } else {
             throw new IllegalArgumentException("Video ID " + videoId + " does not exist");
         }
