553cb007b2df2d41ca1d6bf5d7e7bfebe4d48fc2
Arstan Subanov <arstansubanov@gmail.com>
2024-08-30 21:24:00 +0600

    Superheroes battle

---
+++ src/main/java/faang/school/godbless/BJS2_24615/Pair.java
@@ -0,0 +1,9 @@
+package faang.school.godbless.BJS2_24615;
+
+import lombok.Data;
+
+@Data
+public class Pair<K, V> {
+    private final K key;
+    private final V value;
+}
---
+++ src/main/java/faang/school/godbless/BJS2_24615/SuperheroBattle.java
@@ -0,0 +1,84 @@
+package faang.school.godbless.BJS2_24615;
+
+import lombok.extern.slf4j.Slf4j;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+import java.util.concurrent.TimeUnit;
+
+@Slf4j
+public class SuperheroBattle {
+
+    private final ExecutorService executor = Executors.newFixedThreadPool(2);
+
+    public List<Future<Superhero>> runCompetitions(List<Pair<Superhero, Superhero>> heroPairs) {
+        return heroPairs.stream()
+                .map(pair -> executor.submit(() -> pair.getKey().getTotalPower() > pair.getValue().getTotalPower() ? pair.getKey() : pair.getValue()))
+                .toList();
+    }
+
+    public Future<Superhero> getStrongestSuperhero(List<Pair<Superhero, Superhero>> heroPairs) {
+
+        List<Future<Superhero>> futures = runCompetitions(heroPairs);
+
+        System.out.println(futures.size());
+
+        List<Superhero> winners = new ArrayList<>();
+
+        futures.forEach(future -> {
+            try {
+                Superhero hero = future.get();
+                winners.add(hero);
+                System.out.println("Победитель раунда" + ": " + hero.getName());
+            } catch (InterruptedException | ExecutionException e) {
+                log.error(e.getMessage(), e);
+                throw new RuntimeException(e);
+            }
+        });
+
+        if (futures.size() == 1) {
+
+            return futures.get(0);
+        } else {
+            return getStrongestSuperhero(splitIntoPairs(winners));
+        }
+    }
+
+    private List<Pair<Superhero, Superhero>> splitIntoPairs(List<Superhero> heroPairs) {
+        int left = 0;
+        int right = heroPairs.size() - 1;
+
+        List<Pair<Superhero, Superhero>> pairs = new ArrayList<>();
+
+        while (right - left > 1) {
+            pairs.add(new Pair<>(heroPairs.get(left), heroPairs.get(right)));
+            left++;
+            right--;
+        }
+
+        if (left == right || right - left == 1) {
+            pairs.add(new Pair<>(heroPairs.get(left), heroPairs.get(right)));
+        }
+
+        return pairs;
+    }
+
+    public void shutdown() {
+        executor.shutdown();
+
+        try {
+            if (executor.awaitTermination(5, TimeUnit.MINUTES)) {
+                System.out.println("All threads terminated");
+            } else {
+                executor.shutdownNow();
+            }
+        } catch (InterruptedException e) {
+            log.error(e.getMessage());
+            throw new RuntimeException(e);
+        }
+    }
+}
