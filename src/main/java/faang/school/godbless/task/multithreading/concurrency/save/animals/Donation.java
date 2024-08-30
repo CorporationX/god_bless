c7faf59d608955d9e4a8ad372f3b58643b4225c6
Sultan <nikowosuka@gmail.com>
2024-08-30 13:12:57 +0500

    save animals task perform

--- src/main/java/faang/school/godbless/task/multithreading/concurrency/save/animals/Donation.java
+++ src/main/java/faang/school/godbless/task/multithreading/concurrency/save/animals/Donation.java
@@ -1,12 +1,4 @@
 package faang.school.godbless.task.multithreading.concurrency.save.animals;
 
-import lombok.RequiredArgsConstructor;
-
-import java.util.concurrent.atomic.AtomicLong;
-
-@RequiredArgsConstructor
-public class Donation {
-    private final AtomicLong idCounter;
-    private final long id = idCounter.incrementAndGet();
-    private final long donationAmount;
+public record Donation(long id, long donationAmount) {
 }
