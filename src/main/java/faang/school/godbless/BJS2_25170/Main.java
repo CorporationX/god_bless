b211fc8734629c02ba6bf3fc47913212958c2cd0
Lexus <lexus__74@mail.ru>
2024-08-31 15:16:54 +0500

    Исправление замечаний к задаче Fan out – Fan In

--- src/main/java/faang/school/godbless/BJS2_25170/Main.java
+++ src/main/java/faang/school/godbless/BJS2_25170/Main.java
@@ -1,6 +1,5 @@
 package faang.school.godbless.BJS2_25170;
 
-import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.ExecutorService;
@@ -15,9 +14,9 @@ public class Main {
     }
 
     static void launch() {
-        List<SquareRequest> requests = new ArrayList<>();
-        LongStream.rangeClosed(1, SQUARE_REQUEST_SIZE)
-                .forEach(index -> requests.add(new SquareRequest(index)));
+        List<SquareRequest> requests = LongStream.rangeClosed(1, SQUARE_REQUEST_SIZE)
+                .mapToObj(SquareRequest::new)
+                .toList();
 
         ResultConsumer consumer = new ResultConsumer(0L);
         long result = fanOutFanIn(requests, consumer);
@@ -25,13 +24,12 @@ public class Main {
     }
 
     static Long fanOutFanIn(List<SquareRequest> requests, ResultConsumer resultConsumer) {
-
         ExecutorService service = Executors.newFixedThreadPool(requests.size());
         List<CompletableFuture<Void>> futures = requests.stream()
                 .map(request -> CompletableFuture.runAsync(() -> request.longTimeSquare(resultConsumer), service))
                 .toList();
 
-        CompletableFuture.allOf(futures.get(0)).join();
+        CompletableFuture.allOf(futures.toArray(new CompletableFuture[0])).join();
 
         service.shutdown();
         return resultConsumer.getResult();
