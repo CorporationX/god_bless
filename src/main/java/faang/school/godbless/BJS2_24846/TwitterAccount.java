02161fe8eed6bf47f06209887a2082cd14719830
avoevoda <voevodacode@gmail.com>
2024-08-31 23:42:55 +0300

    refactor

--- src/main/java/faang/school/godbless/BJS2_24846/TwitterAccount.java
+++ src/main/java/faang/school/godbless/BJS2_24846/TwitterAccount.java
@@ -1,15 +1,13 @@
 package faang.school.godbless.BJS2_24846;
 
 import lombok.Getter;
+import lombok.RequiredArgsConstructor;
 
 @Getter
+@RequiredArgsConstructor
 public class TwitterAccount {
     private final String username;
     private int followers;
-
-    public TwitterAccount(String username) {
-        this.username = username;
-    }
 
     public synchronized int getFollowers() {
         return followers;
