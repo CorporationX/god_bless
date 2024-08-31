d933f5798de6829d6836a9225d0ea63582106734
Robert Oguzov <robertoguzov2@gmail.com>
2024-08-31 11:11:35 +0300

    Написал class SalesData

---
+++ src/main/java/faang/school/godbless/BJS2_25280/SalesData.java
@@ -0,0 +1,13 @@
+package faang.school.godbless.BJS2_25280;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+import java.util.List;
+
+@Getter
+@AllArgsConstructor
+public class SalesData {
+    private int storeId;
+    private List<Sales> sales;
+}
