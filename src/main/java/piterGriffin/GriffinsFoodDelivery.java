8e61a1e763e8ce60720610c33c16c23a21bc506f
Ivan <spvanym@gmail.com>
2024-08-31 15:28:14 +0300

    Правки SMI

--- src/main/java/piterGriffin/GriffinsFoodDelivery.java
+++ src/main/java/piterGriffin/GriffinsFoodDelivery.java
@@ -3,6 +3,7 @@ package piterGriffin;
 import java.util.Random;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
 
 public class GriffinsFoodDelivery {
     public static void main(String[] args) {
@@ -10,8 +11,16 @@ public class GriffinsFoodDelivery {
         ExecutorService execute = Executors.newFixedThreadPool(3);
         for (int i = 0; i < characterNames.length; i++) {
             FoodDeliveryTask foodDeliveryTask = new FoodDeliveryTask(characterNames[i], 50);
-            execute.submit(foodDeliveryTask);
+            execute.execute(foodDeliveryTask);
         }
         execute.shutdown();
+        try {
+            if (!execute.awaitTermination(800, TimeUnit.MILLISECONDS)) {
+                execute.shutdownNow();
+            }
+        } catch (InterruptedException e) {
+            execute.shutdownNow();
+            throw new RuntimeException(e);
+        }
     }
 }
