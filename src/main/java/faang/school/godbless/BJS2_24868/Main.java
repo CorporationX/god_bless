394e7a29879b2be611c41e7dddf330bd297555c4
Ruslan Iovlev <ruslan-iovlev@mail.ru>
2024-08-31 12:56:00 +0700

    Становимся celebrity в Twitter

---
+++ src/main/java/faang/school/godbless/BJS2_24868/Main.java
@@ -0,0 +1,20 @@
+package faang.school.godbless.BJS2_24868;
+
+import java.util.List;
+import java.util.concurrent.CompletableFuture;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        TwitterAccount twitterAccount = new TwitterAccount("Ilon Mask", 10);
+
+        TwitterSubscriptionSystem twitterSubscriptionSystem = new TwitterSubscriptionSystem();
+
+        List<Integer> folowersCounts = List.of(1,2,3,4,5,6,7,8,9,10);
+
+        CompletableFuture<Void> future = CompletableFuture.allOf(folowersCounts.stream().map(integer -> twitterSubscriptionSystem.followAccount(twitterAccount)).toArray(CompletableFuture[]::new));
+        future.join();
+        System.out.println(twitterAccount.getFollowers());
+    }
+}
