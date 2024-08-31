896598edb03541e5ec6459f509ca2ba86de1a118
natalia-dch <natalia.a.doskach@gmail.com>
2024-08-31 11:32:52 +0300

    for loop to streams

--- src/main/java/faang/school/godbless/microsoft/MailSender.java
+++ src/main/java/faang/school/godbless/microsoft/MailSender.java
@@ -2,23 +2,26 @@ package faang.school.godbless.microsoft;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
+
+import lombok.SneakyThrows;
 
 public class MailSender {
-    public static void main(String[] args) throws InterruptedException {
-        List<String> messages = new ArrayList<>();
-        List<Thread> threads = new ArrayList<>();
-        for (int i = 0; i < 1000; i++) {
-            messages.add("message" + i);
-        }
-        for (int i = 0; i < 5; i++) {
-            threads.add(new Thread(new SenderRunnable(i * 200, i * 200 + 199, i, messages)));
-        }
-        for (var thread : threads) {
-            thread.start();
-        }
-        for (var thread : threads) {
-            thread.join();
-        }
+    @SneakyThrows
+    public static void main(String[] args) {
+        List<String> messages = IntStream.range(0, 1000).mapToObj(i -> "message" + i).toList();
+        List<Thread> threads = IntStream.range(0, 5).mapToObj(i -> new Thread(new SenderRunnable(i * 200,
+                i * 200 + 199, i, messages))).toList();
+
+        threads.forEach(Thread::start);
+        threads.forEach(thread -> {
+            try {
+                thread.join();
+            } catch (InterruptedException e) {
+                System.out.println(e);
+            }
+        });
         System.out.println("all messages processed");
     }
 }
