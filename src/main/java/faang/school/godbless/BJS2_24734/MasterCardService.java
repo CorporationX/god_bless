c858c39ce8be4fe97d5cf22056592576df179b95
avoevoda <voevodacode@gmail.com>
2024-08-31 12:54:01 +0300

    MasterCardService

---
+++ src/main/java/faang/school/godbless/BJS2_24734/MasterCardService.java
@@ -0,0 +1,51 @@
+package faang.school.godbless.BJS2_24734;
+
+import lombok.extern.slf4j.Slf4j;
+
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+
+@Slf4j
+public class MasterCardService {
+    public static int collectPayment() {
+        try {
+            Thread.sleep(10_000);
+            return 10_000;
+        } catch (InterruptedException e) {
+            log.error(e.getMessage());
+            throw new RuntimeException();
+        }
+    }
+
+    public static int sendAnalytics() {
+        try {
+            Thread.sleep(1_000);    // Имитация задержки в 1 секунду
+            return 1_000;            // Возврат результата аналитики
+        } catch (InterruptedException e) {
+            log.error(e.getMessage());
+            throw new RuntimeException();
+        }
+    }
+
+    public void doAll() {
+        ExecutorService executor = Executors.newSingleThreadExecutor();
+        Future<Integer> paymentFuture = executor.submit(MasterCardService::collectPayment);
+
+        CompletableFuture<Integer> analyticsFuture = CompletableFuture.supplyAsync(MasterCardService::sendAnalytics);
+
+        try {
+            Integer analyticsResult = analyticsFuture.get();
+            System.out.println("Analytics result: " + analyticsResult);
+
+            Integer paymentResult = paymentFuture.get();
+            System.out.println("Payment result: " + paymentResult);
+        } catch (InterruptedException | ExecutionException e) {
+            log.error(e.getMessage());
+        } finally {
+            executor.shutdown();
+        }
+    }
+}
