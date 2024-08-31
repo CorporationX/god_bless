80262e8b0803d9b9ca74209587af8e276ee35fe5
Ilya Merkulov <imerkulov@fun.co>
2024-08-31 19:35:16 +0300

    BJS2-26039 WOW

---
+++ src/main/java/faang/school/godbless/WOW/QuestSystem.java
@@ -0,0 +1,29 @@
+package faang.school.godbless.WOW;
+
+import lombok.extern.slf4j.Slf4j;
+
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+@Slf4j
+public class QuestSystem {
+    private final ExecutorService executor = Executors.newFixedThreadPool(1);
+
+    public CompletableFuture<Player> startQuest(Player player, Quest quest) {
+        return CompletableFuture.supplyAsync(() -> {
+            try {
+                Thread.sleep(quest.getDifficulty() * 1000L);
+                return player;
+            } catch (InterruptedException e) {
+                Thread.currentThread().interrupt();
+                log.error(e.getMessage());
+                return null;
+            }
+        }, executor);
+    }
+
+    public void shutdown() {
+        executor.shutdown();
+    }
+}
