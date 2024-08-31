eb0eca7df23cf982a1c8ba684399ca68710cf0cd
ifef228 <ifef228@gmail.com>
2024-08-31 08:07:56 +0300

    поправил

--- src/main/java/faang/school/godbless/WOW/Player.java
+++ src/main/java/faang/school/godbless/WOW/Player.java
@@ -11,4 +11,17 @@ public class Player {
     private String name;
     private int level;
     private int experience;
+
+    public Player start(Quest quest) {
+        System.out.println(name + " начал выполнение квеста " + quest.getName());
+        try {
+            Thread.sleep(1000L * quest.getDifficulty());
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+            throw new RuntimeException();
+        }
+        System.out.println(name + " закончил выполнение квеста " + quest.getName());
+        experience += quest.getReward();
+        return this;
+    }
 }
--- src/main/java/faang/school/godbless/WOW/QuestSystem.java
+++ src/main/java/faang/school/godbless/WOW/QuestSystem.java
@@ -1,16 +1,9 @@
 package faang.school.godbless.WOW;
 
 import java.util.concurrent.CompletableFuture;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
 
 public class QuestSystem {
     public CompletableFuture<Player> startQuest(Player player, Quest quest) {
-        ExecutorService service = Executors.newSingleThreadExecutor();
-        return CompletableFuture.supplyAsync(() ->{
-            Player player1 = quest.start(player);
-            service.shutdown();
-            return player1;
-        }, service );
+        return CompletableFuture.supplyAsync(() -> player.start(quest));
     }
 }
