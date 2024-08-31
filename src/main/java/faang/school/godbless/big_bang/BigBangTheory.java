4d0f035a39b6bca23d03eaa81f6082020915b8b5
natalia-dch <natalia.a.doskach@gmail.com>
2024-08-31 11:07:44 +0300

    fixes

--- src/main/java/faang/school/godbless/big_bang/BigBangTheory.java
+++ src/main/java/faang/school/godbless/big_bang/BigBangTheory.java
@@ -1,17 +1,23 @@
 package faang.school.godbless.big_bang;
 
+import java.util.concurrent.ExecutionException;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
 
 public class BigBangTheory {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws ExecutionException, InterruptedException {
         ExecutorService executor = Executors.newFixedThreadPool(4);
 
-        executor.submit(new Task("a", "taskA"));
-        executor.submit(new Task("b", "taskB"));
-        executor.submit(new Task("c", "taskC"));
-        executor.submit(new Task("d", "taskD"));
-        //no new tasks will be accepted
+        Future<?> t1 = executor.submit(new Task("a", "taskA"));
+        Future<?> t2 = executor.submit(new Task("b", "taskB"));
+        Future<?> t3 = executor.submit(new Task("c", "taskC"));
+        Future<?> t4 = executor.submit(new Task("d", "taskD"));
+        t1.get();
+        t2.get();
+        t3.get();
+        t4.get();
+        System.out.println("tasks are finished");
         executor.shutdown();
     }
 }
