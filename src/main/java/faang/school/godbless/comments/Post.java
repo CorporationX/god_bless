c26842d3c77b74c93a0df1d7e359cdb0af714f08
ifef228 <ifef228@gmail.com>
2024-08-31 11:01:41 +0300

    gogo

---
+++ src/main/java/faang/school/godbless/comments/Post.java
@@ -0,0 +1,37 @@
+package faang.school.godbless.comments;
+
+import lombok.Getter;
+
+import java.util.List;
+import java.util.concurrent.CopyOnWriteArrayList;
+
+@Getter
+public class Post implements Comparable {
+    private static int CUR_ID = 0;
+    private final Integer id;
+    private final String title;
+    private final String body;
+    private final String author;
+    private final List<Comment> comments;
+
+    public Post(String title, String body) {
+        id = ++CUR_ID;
+        this.title = title;
+        this.body = body;
+        this.author = Thread.currentThread().getName();
+        this.comments = new CopyOnWriteArrayList<>();
+    }
+
+    public void addComment(Comment comment) {
+        comments.add(comment);
+    }
+
+    public void deleteComment(Comment comment) {
+        comments.remove(comment);
+    }
+
+    @Override
+    public int compareTo(Object o) {
+        return this.id - ((Post) o).id;
+    }
+}
