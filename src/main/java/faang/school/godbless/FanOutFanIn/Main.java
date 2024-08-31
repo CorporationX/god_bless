6d93c11164147e430e08e11adfc57d1b5653fd70
Ilya Merkulov <imerkulov@fun.co>
2024-08-31 20:38:29 +0300

    BJS2-25191 Fan out – Fan In

---
+++ src/main/java/faang/school/godbless/FanOutFanIn/Main.java
@@ -0,0 +1,9 @@
+package faang.school.godbless.FanOutFanIn;
+
+public class Main {
+    public static void main(String[] args) {
+        Calculator calculator = new Calculator();
+
+        calculator.launch();
+    }
+}
---
+++ src/main/java/faang/school/godbless/FanOutFanIn/SquareRequest.java
@@ -0,0 +1,28 @@
+package faang.school.godbless.FanOutFanIn;
+
+import lombok.extern.slf4j.Slf4j;
+
+import java.util.concurrent.ThreadLocalRandom;
+
+@Slf4j
+public class SquareRequest {
+    private static final long MIN_TIMEOUT = 3000L;
+    private final Long number;
+
+    public SquareRequest(Long number) {
+        this.number = number;
+    }
+
+    public void longTimeSquare(ResultConsumer resultConsumer) {
+        var randomTimeout = ThreadLocalRandom.current().nextInt(1000);
+
+        try {
+            Thread.sleep(MIN_TIMEOUT + randomTimeout);
+        } catch (InterruptedException e) {
+            Thread.currentThread().interrupt();
+            log.error(e.getMessage());
+        } finally {
+            resultConsumer.add(number * number);
+        }
+    }
+}
