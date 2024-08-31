42905a22faae603e584dffc759044fa87e060ec1
Владислав Воронов <iPlesk90@yandex.ru>
2024-08-31 09:34:36 +0300

    Выполнил задание

--- src/main/java/faang/school/godbless/orders_amazon/Order.java
+++ src/main/java/faang/school/godbless/orders_amazon/Order.java
@@ -1,7 +1,6 @@
 package faang.school.godbless.orders_amazon;
 
 import lombok.AllArgsConstructor;
-import lombok.Data;
 import lombok.Getter;
 import lombok.Setter;
 
--- src/main/java/faang/school/godbless/orders_amazon/OrderProcessor.java
+++ src/main/java/faang/school/godbless/orders_amazon/OrderProcessor.java
@@ -3,9 +3,6 @@ package faang.school.godbless.orders_amazon;
 import lombok.extern.slf4j.Slf4j;
 
 import java.util.concurrent.CompletableFuture;
-import java.util.concurrent.Executor;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
 import java.util.concurrent.atomic.AtomicInteger;
 
 @Slf4j
