8fcba9cec792bee55af6e8c1557bfcb2f4ddac42
mamadjonovKO <mamadjonov_k@auca.kg>
2024-09-01 10:38:03 +0600

    potions is done

---
+++ src/main/java/faang/school/godbless/BJS2_25181/Potion.java
@@ -0,0 +1,13 @@
+package faang.school.godbless.BJS2_25181;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+import java.util.concurrent.CompletableFuture;
+
+@Getter
+@AllArgsConstructor
+public class Potion {
+    private String name;
+    private int requiredIngredients;
+}
