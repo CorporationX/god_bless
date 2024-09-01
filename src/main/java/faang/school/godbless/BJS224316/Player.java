c77748d85e88281caa5dad5ab0c8bbf85055362a
aephrodita <146974539+aephrodita@users.noreply.github.com>
2024-09-01 11:21:25 +0500

    BJS2-24316

--- src/main/java/faang/school/godbless/BJS224316/Player.java
+++ src/main/java/faang/school/godbless/BJS224316/Player.java
@@ -13,14 +13,12 @@ public class Player {
     private String name;
 
     public void startBattle(Boss boss) throws InterruptedException {
-            boss.joinBattle(this);
-            try {
-                Thread.sleep((long) (Math.random() * 5000));
-            } catch (InterruptedException e) {
-                log.error(e.getMessage());
-            }
-            boss.leaveBattle(this);
-
-
+        boss.joinBattle(this);
+        try {
+            Thread.sleep((long) (Math.random() * 5000));
+        } catch (InterruptedException e) {
+            log.error(e.getMessage());
+        }
+        boss.leaveBattle(this);
     }
 }
