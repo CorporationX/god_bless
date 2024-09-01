5871b21031d32d67bcee45b834e8dbb5da0b5970
Denis Panasenko <panasenkodenis1995@gmail.com>
2024-09-01 17:36:26 +0300

    BJS2_25304

--- src/main/java/faang/school/godbless/BJS2_25304/PostService.java
+++ src/main/java/faang/school/godbless/BJS2_25304/PostService.java
@@ -10,26 +10,20 @@ import java.util.Optional;
 @Slf4j
 @Getter
 public class PostService {
-    private volatile List<Post> posts;
-    private static final Object LOCKER_COMMENTS = new Object();
+    private volatile List<Post> posts = new ArrayList<>();
     private static final Object LOCKER_POSTS = new Object();
 
-    public PostService() {
-        this.posts = new ArrayList<>();
-    }
-
     public void addComment(Comment comment, int id) {
-        synchronized (LOCKER_COMMENTS) {
-            posts
-                    .stream()
-                    .filter(postFromList -> postFromList.id() == id)
-                    .findFirst()
-                    .map(postFromList -> {
-                        postFromList.addComment(comment);
-                        return postFromList;
-                    })
-                    .orElseThrow(() -> new IllegalArgumentException("Post with id " + id + " doesn't exist"));
+        synchronized (LOCKER_POSTS) {
+            Post post = findPostById(id);
 
+            if (post == null) {
+                throw new IllegalArgumentException("Post with id " + id + " doesn't exist");
+            } else {
+                synchronized (post) {
+                    post.addComment(comment);
+                }
+            }
         }
     }
 
@@ -40,29 +34,48 @@ public class PostService {
     }
 
     public void deletePost(int id) {
-        posts.removeIf(post -> {
-            if (post.id() == id && post.author().equals(Thread.currentThread().getName())) {
-                log.info("Post written by {} with id {} was deleted by {}", post.author(), id, Thread.currentThread().getName());
-                return true;
-            } else {
-                log.info("{} is not author of id {}, need {}", Thread.currentThread().getName(), id, post.author());
-                return false;
-            }
-        });
+        synchronized (LOCKER_POSTS) {
+            posts.removeIf(post -> {
+                if (post.id() == id && post.author().equals(Thread.currentThread().getName())) {
+                    log.info("Post written by {} with id {} was deleted by {}", post.author(), id, Thread.currentThread().getName());
+                    return true;
+                } else {
+                    log.info("{} is not author of id {}, need {}", Thread.currentThread().getName(), id, post.author());
+                    return false;
+                }
+            });
+        }
     }
 
-    public synchronized void deleteComments(int postId) {
-        Optional
-                .ofNullable(posts.get(postId))
-                .ifPresentOrElse((post) -> {
-                    post.comments()
-                            .stream()
-                            .filter(comment -> comment.author().equals(Thread.currentThread().getName()))
-                            .findFirst()
-                            .ifPresent(comment -> {
-                                log.info("Comment with author {} was deleted by author {}", comment.author(), Thread.currentThread().getName());
-                                post.comments().remove(comment);
-                            });
-                }, () -> log.error("Post with id {} does not exist", postId));
+    public void deleteComments(int postId) {
+        Post post;
+        synchronized (LOCKER_POSTS) {
+            post = findPostById(postId);
+        }
+
+        if (post == null) {
+            log.error("Post with id {} does not exist", postId);
+            return;
+        }
+
+        synchronized (post) {
+            Optional<Comment> commentToDelete = post.comments()
+                    .stream()
+                    .filter(comment -> comment.author().equals(Thread.currentThread().getName()))
+                    .findFirst();
+
+            if (commentToDelete.isPresent()) {
+                log.info("Comment with author {} was deleted by author {}", commentToDelete.get().author(),
+                        Thread.currentThread().getName());
+                post.comments().remove(commentToDelete.get());
+            }
+        }
+    }
+
+    private Post findPostById(int id) {
+        return posts.stream()
+                .filter(post -> post.id() == id)
+                .findFirst()
+                .orElse(null);
     }
 }
