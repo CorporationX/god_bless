081074e833a07402e21333d7b6b82347ff030881
Garib_Aghayev <garibaghayev@gmail.com>
2024-09-01 18:35:47 +0300

    first release

--- src/main/java/faang/school/godbless/ThirdSprint/BJS2_23871/House.java
+++ src/main/java/faang/school/godbless/ThirdSprint/BJS2_23871/House.java
@@ -1,14 +1,16 @@
 package faang.school.godbless.ThirdSprint.BJS2_23871;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.concurrent.Executors;
 import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.ThreadLocalRandom;
 import java.util.concurrent.TimeUnit;
 
 public class House {
-    private List<Room> roomList = new ArrayList<>();
+    private Map<Integer, List<Room>> roommap = new HashMap<>();
     private List<Food> collectedFood = new ArrayList<>();
 
     public static void main(String[] args) throws InterruptedException {
@@ -18,24 +20,24 @@ public class House {
         ScheduledExecutorService service = Executors.newScheduledThreadPool(5);
 
         for (int i = 0; i < 5; i++) {
+            int finalI = i;
             service.schedule(() -> {
                 System.out.println(Thread.currentThread().getName());
-                house.collectFood();
-            },30, TimeUnit.SECONDS);
+                house.roommap.get(finalI).stream().forEach(r -> house.collectFood(r));
+            }, 5 * (i + 1), TimeUnit.SECONDS);
         }
 
         service.shutdown();
         while(!service.awaitTermination(30, TimeUnit.SECONDS)){}
 
-        System.out.println(house.roomList);
+        System.out.println(house.roommap);
         System.out.println(house.collectedFood);
-        System.out.println("Done");
     }
 
     public void initialize() {
         for (int i = 0; i < 5; i++){
-        roomList.add(new Room());
-        roomList.add(new Room());
+            roommap.computeIfAbsent(i, (a) -> new ArrayList<>()).add(new Room());
+            roommap.get(i).add(new Room());
         }
         Food[] foods = new Food[]{new Food("cheese"),
                 new Food("meet"),
@@ -43,19 +45,16 @@ public class House {
                 new Food("bread"),
                 new Food("apple")};
         for (int i = 0; i < 5; i++) {
-            roomList.stream()
+            roommap.get(i).stream()
                     .forEach(u -> u.getFoodList()
                             .add(foods[ThreadLocalRandom.current().nextInt(0, 4)]));
         }
     }
 
-    public synchronized void collectFood() {
-            roomList.stream()
-                    .sorted((a, b) -> b.getFoodList().size() - a.getFoodList().size())
-                    .limit(2)
-                    .forEach(u -> {
-                        collectedFood.addAll(u.getFoodList());
-                        u.getFoodList().clear();
-                    });
+    public void collectFood(Room room) {
+        synchronized (collectedFood) {
+            collectedFood.addAll(room.getFoodList());
+            room.getFoodList().clear();
+        }
     }
 }
--- src/main/java/faang/school/godbless/ThirdSprint/BJS2_23871/Room.java
+++ src/main/java/faang/school/godbless/ThirdSprint/BJS2_23871/Room.java
@@ -10,5 +10,5 @@ import java.util.List;
 @ToString
 
 public class Room {
-    private List<Food> foodList = new ArrayList<>();
+    private final List<Food> foodList = new ArrayList<>();
 }
