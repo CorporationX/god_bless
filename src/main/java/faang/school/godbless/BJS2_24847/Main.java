888825eeabf41de33fe5a7fe9cd5c5d3f2bd15e8
Ruslan Iovlev <ruslan-iovlev@mail.ru>
2024-09-01 11:16:44 +0700

    Собираем заказы в Amazon доработка

--- src/main/java/faang/school/godbless/BJS2_24847/Main.java
+++ src/main/java/faang/school/godbless/BJS2_24847/Main.java
@@ -2,20 +2,24 @@ package faang.school.godbless.BJS2_24847;
 
 import java.util.List;
 import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutionException;
 
 public class Main {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws ExecutionException, InterruptedException {
 
-        List<Order> orders = List.of(new Order(1,"Требует обработки"),
-                new Order(2,"Требует обработки"),
-                new Order(3,"Требует обработки"),
-                new Order(4,"Требует обработки"));
+        List<Order> orders = List.of(new Order(1, "Требует обработки"),
+                new Order(2, "Требует обработки"),
+                new Order(3, "Требует обработки"),
+                new Order(4, "Требует обработки"));
 
         OrderProcessor orderProcessor = new OrderProcessor();
 
-        CompletableFuture<Void> future = CompletableFuture.allOf(orders.stream().map(orderProcessor::processOrder).toArray(CompletableFuture[]::new));
-        future.join();
+        CompletableFuture<Void> future = CompletableFuture.allOf(
+                orders.stream()
+                        .map(orderProcessor::processOrder)
+                        .toArray(CompletableFuture[]::new));
+        future.get();
         System.out.println(orderProcessor.getTotalProcessedOrders().get());
     }
 }
