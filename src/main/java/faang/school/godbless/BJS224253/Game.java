0516531ec32cc81e883433d670d6b120a7e4e325
aephrodita <146974539+aephrodita@users.noreply.github.com>
2024-09-01 12:07:44 +0500

    BJS2-24253

--- src/main/java/faang/school/godbless/BJS224253/Game.java
+++ src/main/java/faang/school/godbless/BJS224253/Game.java
@@ -1,33 +1,68 @@
 package faang.school.godbless.BJS224253;
 
+import lombok.Getter;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
+@Getter
 public class Game {
     private int scores;
     private int lives;
     private final Object scoreLock = new Object();
     private final Object liveLock = new Object();
+    private final List<Player> bros = new ArrayList<>();
+    private final Random random = new Random();
+    private int activePlayersCounter = 0;
+    private boolean isGameOver = false;
 
-    public Game(int scores, int lives) {
-        this.scores = scores;
-        this.lives = lives;
+    public void addPlayer(Player player) {
+        bros.add(player);
+        activePlayersCounter++;
     }
 
-    public void update(Player attacker, Player defender) {
-        boolean damageWillBeTakenByAttacker = Math.random() < 0.5;
-        Player.getDamage(damageWillBeTakenByAttacker ? attacker : defender);
-        synchronized (scoreLock) {
-            scores++;
+    public void update() {
+        if (isGameOver) {
+            return;
         }
-        if (attacker.getLives() == 0 || defender.getLives() == 0) {
-            gameOver();
-        } else {
-            synchronized (liveLock) {
+        Player player = getRandomPlayer();
+        increaseLives(player);
+        increaseScore(player);
+    }
+
+    private void increaseLives(Player player) {
+        synchronized (liveLock) {
+            if (player.isAlive() && !isGameOver) {
+                player.setLives(player.getLives() - 1);
                 lives++;
+                System.out.println(player.getName() + " lives: " + player.getLives());
+                if (player.getLives() == 0 && player.isAlive()) {
+                    player.setAlive(false);
+                    activePlayersCounter--;
+                    gameOver();
+                }
+            }
+        }
+    }
+
+    private void increaseScore(Player player) {
+        synchronized (scoreLock) {
+            if (player.isAlive() && !isGameOver) {
+                player.setScore(player.getScore() + 1);
+                scores++;
+                System.out.println(player.getName() + " earned score: " + player.getScore());
             }
         }
     }
 
     private void gameOver() {
-        System.out.println("The game is over");
-        System.out.printf("Was spent %s lives. The result score is %s%n", this.lives, this.scores);
+        isGameOver = true;
+        System.out.printf("Game over with max score %d\n", scores);
+        System.out.printf("Lost lives %d\n", lives);
+    }
+
+    private Player getRandomPlayer() {
+        return bros.get(random.nextInt(bros.size()));
     }
 }
--- src/main/java/faang/school/godbless/BJS224253/Main.java
+++ src/main/java/faang/school/godbless/BJS224253/Main.java
@@ -1,23 +1,21 @@
 package faang.school.godbless.BJS224253;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
 public class Main {
     public static void main(String[] args) {
-        Game broForce = new Game(0, 0);
-        Player attacker = new Player("Attacker", 10);
-        Player defender = new Player("Defender", 6);
-
-        ExecutorService service = Executors.newFixedThreadPool(2);
-
-        for (int i = 0; i < 2; i++) {
-            service.submit(() -> {
-                while(attacker.getLives() > 0 && defender.getLives() > 0)
-                    broForce.update(attacker, defender);
-            });
+        Game game = new Game();
+        Player beks = new Player("beks", 10);
+        Player aru = new Player("aru", 12);
+        List<Player> bros = new ArrayList<>(List.of(beks, aru));
+        bros.forEach(game::addPlayer);
+        ExecutorService executorService = Executors.newFixedThreadPool(2);
+        while (game.getActivePlayersCounter() > 0 && !game.isGameOver()) {
+            executorService.submit(game::update);
         }
-
-        service.shutdown();
+        executorService.shutdown();
     }
 }
