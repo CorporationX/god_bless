11b7f374f3f43e54b15750c2dceb0891d890b646
SharkB38 <val.wolf.ne@gmail.com>
2024-09-02 23:24:25 +0900

    fix

--- src/main/java/bank/game/Account.java
+++ src/main/java/bank/game/Account.java
@@ -6,24 +6,39 @@ import java.util.concurrent.atomic.AtomicInteger;
 import java.util.concurrent.locks.Lock;
 import java.util.concurrent.locks.ReentrantLock;
 
-@Getter
 public class Account {
     private static final double DEFAULT_BALANCE = 500.0;
     private static final AtomicInteger idCount = new AtomicInteger(0);
 
+    @Getter
     private final int id = idCount.getAndIncrement();
     private double balance = DEFAULT_BALANCE;
     private final Lock lock = new ReentrantLock();
 
     public void deposit(double amountToDeposit) {
         lock.lock();
-        balance += amountToDeposit;
-        lock.unlock();
+        try {
+            balance += amountToDeposit;
+        } finally {
+            lock.unlock();
+        }
     }
 
     public void withdraw(double amountToWithdraw) {
         lock.lock();
-        balance -= amountToWithdraw;
-        lock.unlock();
+        try {
+            balance -= amountToWithdraw;
+        } finally {
+            lock.unlock();
+        }
+    }
+
+    public double getBalance() {
+        lock.lock();
+        try {
+            return balance;
+        } finally {
+            lock.unlock();
+        }
     }
 }
