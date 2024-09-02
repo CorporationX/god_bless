f761f53292b8aff9732e2037193df7fd9ef05e92
aephrodita <146974539+aephrodita@users.noreply.github.com>
2024-09-02 17:39:39 +0500

    BJS2-25092

---
+++ src/main/java/faang/school/godbless/BJS225092/School.java
@@ -0,0 +1,23 @@
+package faang.school.godbless.BJS225092;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+import java.util.List;
+
+@Getter
+@AllArgsConstructor
+public class School {
+    private String name;
+    private List<Student> team;
+
+    public int getTotalPoints() {
+        return team.stream()
+                .mapToInt(Student::getPoints)
+                .sum();
+    }
+
+    public void addPointsToTeam(int points) {
+        team.forEach(student -> student.addPoints(points));
+    }
+}
