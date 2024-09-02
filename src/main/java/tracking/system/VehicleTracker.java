05d8f8df5cfd9341f828862c114ebef968b36c28
cemeh666 <semen-5555@mail.ru>
2024-09-02 21:15:15 +0300

    BJS2-25397 Система трекинга доделки

--- src/main/java/tracking/system/VehicleTracker.java
+++ src/main/java/tracking/system/VehicleTracker.java
@@ -8,23 +8,23 @@ import java.util.concurrent.TimeUnit;
 
 @Getter
 public class VehicleTracker {
-    private final int THREAD_NUM = 1;
     private final int UPDATE_PERIOD = 3;
     private final int DELAY = 30;
 
     private final TransportManagementSystem transportManagementSystem;
-    private final ScheduledExecutorService executorService;
+    private final ScheduledExecutorService executorService = Executors.newScheduledThreadPool(
+        DELAY / UPDATE_PERIOD
+    );
 
     VehicleTracker(TransportManagementSystem transportManagementSystem) {
         this.transportManagementSystem = transportManagementSystem;
-        this.executorService = Executors.newScheduledThreadPool(transportManagementSystem.getVehicleMap().size());
     }
 
     public void startTracking() {
         this.executorService.scheduleWithFixedDelay(() -> {
             transportManagementSystem.getVehicleMap()
-                    .values()
-                    .forEach(transportManagementSystem::updateVehicle);
+                .values()
+                .forEach(transportManagementSystem::updateVehicle);
 
             System.out.println(transportManagementSystem.getVehicleMap());
         }, 0, UPDATE_PERIOD, TimeUnit.SECONDS);
