6cc8513273b80e82d536808b79b634ef64fab4c7
SharkB38 <val.wolf.ne@gmail.com>
2024-09-03 00:44:47 +0900

    fix

--- src/main/java/bank/game/Main.java
+++ src/main/java/bank/game/Main.java
@@ -1,5 +1,6 @@
 package bank.game;
 
+import lombok.NonNull;
 import lombok.extern.slf4j.Slf4j;
 
 import java.util.List;
@@ -14,33 +15,46 @@ public class Main {
     public static void main(String[] args) {
         var accounts = initAccount();
         Bank bank = new Bank();
+        addAccounts(accounts, bank);
+        proceedTransfers(bank);
+    }
+
+    private static void addAccounts(@NonNull List<Account> accounts, @NonNull Bank bank) {
         var futures = accounts.stream()
                 .map(account -> CompletableFuture.runAsync(() -> bank.openAccount(account)))
                 .toList();
         CompletableFuture.allOf(futures.toArray(new CompletableFuture[0])).join();
+    }
+
+    private static void proceedTransfers(@NonNull Bank bank) {
         var transferFutures = IntStream.range(0, ACCOUNTS_COUNT)
-                .mapToObj(i -> CompletableFuture.supplyAsync(() -> {
-                    var ids = bank.getAllIds();
-                    int idFrom = ids.get(ThreadLocalRandom.current().nextInt(ids.size()));
-                    int idTo = ids.get(ThreadLocalRandom.current().nextInt(ids.size()));
-                    log.info("{} {}", idFrom, idTo);
-                    if (idFrom != idTo) {
-                        return bank.transfer(idFrom, idTo,
-                                ThreadLocalRandom.current().nextDouble() * 100.0 + 1.0);
-                    }
-                    log.error("Same id");
-                    return false;
-                }))
+                .mapToObj(i -> CompletableFuture.supplyAsync(() -> sendTransfer(bank)))
                 .toList();
+        CompletableFuture.allOf(transferFutures.toArray(new CompletableFuture[0])).join();
         transferFutures.stream()
                 .map(CompletableFuture::join)
-                .forEach(result -> {
-                    if (result) {
-                        log.info("Transaction successful");
-                    } else {
-                        log.error("Transaction failed");
-                    }
-                });
+                .forEach(Main::handleResult);
+    }
+
+    private static boolean sendTransfer(@NonNull Bank bank) {
+        var ids = bank.getAllIds();
+        int idFrom = ids.get(ThreadLocalRandom.current().nextInt(ids.size()));
+        int idTo = ids.get(ThreadLocalRandom.current().nextInt(ids.size()));
+        log.info("{} {}", idFrom, idTo);
+        if (idFrom != idTo) {
+            return bank.transfer(idFrom, idTo,
+                    ThreadLocalRandom.current().nextDouble() * 200.0 + 1.0);
+        }
+        log.error("Same id");
+        return false;
+    }
+
+    private static void handleResult(boolean result) {
+        if (result) {
+            log.info("Transaction successful");
+        } else {
+            log.error("Transaction failed");
+        }
     }
 
     private static List<Account> initAccount() {
