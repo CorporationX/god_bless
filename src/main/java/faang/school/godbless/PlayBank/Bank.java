6e172ecd6a5203ca90ae2eb634d6372e09b5572d
PLSGOTOASS <koteykaarisha@mail.ru>
2024-09-02 22:36:26 +0300

    выполнил задание

--- src/main/java/faang/school/godbless/PlayBank/Bank.java
+++ src/main/java/faang/school/godbless/PlayBank/Bank.java
@@ -10,6 +10,25 @@ import java.util.Map;
 public class Bank {
     private Map<Integer, Account> accounts = new HashMap<>();
 
+    public void transfer(int fromId, int toId, int amount) {
+        Account from = accounts.get(fromId);
+        Account to = accounts.get(toId);
+        if (fromId < toId) {
+            Account temp = from;
+            from = to;
+            to = temp;
+        }
+        synchronized (from) {
+            synchronized (to) {
+                executeTransfer(from, to, amount);
+            }
+        }
+    }
+
+    public void addAccount(Account account) {
+        accounts.put(account.getId(), account);
+    }
+
     private boolean executeTransfer(Account from, Account to, int amount) {
         synchronized (from) {
             synchronized (to) {
@@ -22,29 +41,6 @@ public class Bank {
         }
         System.out.println(from.getId() + " не смог перевести " + amount + " на аккаунт " + to.getId());
         return false;
-    }
-
-    public void transfer(int fromId, int toId, int amount) {
-        Account from = accounts.get(fromId);
-        Account to = accounts.get(toId);
-        if (fromId < toId) {
-            synchronized (from) {
-                synchronized (to) {
-                    executeTransfer(from, to, amount);
-                }
-            }
-        }
-        else {
-            synchronized (to) {
-                synchronized (from) {
-                    executeTransfer(from, to, amount);
-                }
-            }
-        }
-    }
-
-    public void addAccount(Account account) {
-        accounts.put(account.getId(), account);
     }
 }
 
