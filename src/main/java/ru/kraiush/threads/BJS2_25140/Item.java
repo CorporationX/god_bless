3a43973764bc4a2b40689059532b0e65c7581cc0
Alexander Kraiushkin <akraiushkin@gmail.com>
2024-09-10 22:13:06 +0300

    BJS2-25140: GarryAteTooMuch .V.01

---
+++ src/main/java/ru/kraiush/threads/BJS2_25140/Item.java
@@ -0,0 +1,17 @@
+package ru.kraiush.threads.BJS2_25140;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@ToString
+@AllArgsConstructor
+public class Item {
+
+    private String name;
+    private int power;
+
+}
