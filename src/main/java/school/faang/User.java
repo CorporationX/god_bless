61e549953467cb1020186c28cca4bbd679771d2d
M1rroxxx <nikita.sidorov.7@icloud.com>
2024-10-02 02:21:08 +0300

    Grouping users by age

---
+++ src/main/java/school/faang/User.java
@@ -0,0 +1,29 @@
+package school.faang;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+
+@AllArgsConstructor
+@Data
+public class User {
+    private String name;
+    private int age;
+    private String placeOfWork;
+    private String address;
+
+    public static Map<Integer, List<User>> groupUsers(List<User> users) {
+        Map<Integer, List<User>> groupUsers = new HashMap<>();
+
+        for (User user : users) {
+            groupUsers.computeIfAbsent(user.getAge(), k -> new ArrayList<>()).add(user);
+        }
+
+        return groupUsers;
+    }
+}
