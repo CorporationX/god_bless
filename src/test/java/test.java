665c83aa48cad52d288c2e5eeb9f31a15a847d65
Aliyar <aliartalybov@gmail.com>
2024-10-01 16:22:12 +0300

    Commit 2 est

--- src/test/java/test.java
+++ src/test/java/test.java
@@ -4,6 +4,6 @@ import lombok.Getter;
 public class test {
 
     public static void main(String[] args) {
-        System.out.println("Hello World222");
+        System.out.println("Hello World22222");
     }
 }
