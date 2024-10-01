4e3960e82e90b128928700d34c28c45b6c0c0e33
Igor <asachiyigor@gmail.com>
2024-10-02 02:35:24 +0300

    BJS2-31332
    updated commit
    
    add new files from main folder

--- src/main/java/school/faang/Main.java
+++ src/main/java/school/faang/Main.java
@@ -1,4 +1,24 @@
 package school.faang;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
 public class Main {
+
+    private static final List<User> USERS = new ArrayList<>();
+
+    public static void main(String[] args) {
+        USERS.add(new User("Egor", 18, "it", "Spb"));
+        USERS.add(new User("Masha", 23, "it", "Msk"));
+        USERS.add(new User("Pasha", 24, "it", "Ekb"));
+        USERS.add(new User("Sasha", 23, "it", "Oms"));
+
+        Map<Integer, List<User>> result = User.groupUsers(USERS);
+
+        for (Map.Entry<Integer, List<User>> entry : result.entrySet()) {
+            System.out.println("Age: " + entry);
+        }
+        System.out.println();
+    }
 }
