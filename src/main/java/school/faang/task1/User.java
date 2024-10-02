e2c1d597bb0b64153886b610c0090c535c0e0eef
Olga Karaskova <okalughina@gmail.com>
2024-10-02 22:33:26 +0200

    Implemented task2 - User Registration

--- src/main/java/school/faang/task1/User.java
+++ src/main/java/school/faang/task1/User.java
@@ -1,16 +1,16 @@
 package school.faang.task1;
 
-import lombok.AllArgsConstructor;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.ToString;
 
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
+import java.util.Set;
 import java.util.TreeMap;
 
-@AllArgsConstructor
 @Getter
 @ToString
 @EqualsAndHashCode
@@ -19,6 +19,53 @@ public class User {
     private int age;
     private String workPlace;
     private String address;
+
+    public static final Set<String> VALID_JOBS = new HashSet<String>() {{
+        add("Google");
+        add("Uber");
+        add("Amazon");
+    }};
+
+    public static final Set<String> VALID_ADDRESSES = new HashSet<String>() {{
+        add("London");
+        add("New York");
+        add("Amsterdam");
+    }};
+
+    public User(String name, int age, String workPlace, String address) {
+        setName(name);
+        setAge(age);
+        setWorkPlace(workPlace);
+        setAddress(address);
+    }
+
+    public void setName(String name) {
+        if (name == null || name.isEmpty()) {
+            throw new IllegalArgumentException("Name cannot be null or empty");
+        }
+        this.name = name;
+    }
+
+    public void setAge(int age) {
+        if (age < 18) {
+            throw new IllegalArgumentException("Age should not be less than 18");
+        }
+        this.age = age;
+    }
+
+    public void setWorkPlace(String workPlace) {
+        if (!VALID_JOBS.contains(workPlace)) {
+            throw new IllegalArgumentException("Job is invalid. Valid jobs are: " + VALID_JOBS.toString());
+        }
+        this.workPlace = workPlace;
+    }
+
+    public void setAddress(String address) {
+        if (!VALID_ADDRESSES.contains(address)) {
+            throw new IllegalArgumentException("Address is invalid. Valid address in: " + VALID_ADDRESSES);
+        }
+        this.address = address;
+    }
 
     public static void addUserToList(List<User> users, User user) {
         users.add(user);
