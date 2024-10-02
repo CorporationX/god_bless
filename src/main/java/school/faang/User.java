ba13e3d66382b6d87f9945d3582889d30bda69c9
Alexander Litovkin <litovkinalexan@gmail.com>
2024-10-02 23:10:14 +0700

    refactor code style

--- src/main/java/school/faang/User.java
+++ src/main/java/school/faang/User.java
@@ -7,17 +7,12 @@ import java.util.Set;
 public class User {
 
     private static final Set<String> VALID_JOBS = Set.of("Google", "Uber", "Amazon");
-
     private static final Set<String> VALID_ADDRESSES = Set.of("London", "New York", "Amsterdam");
-
     private static final int VALID_AGE = 18;
 
     private String name;
-
     private int age;
-
     private String job;
-
     private String address;
 
     public User(String name, int age, String job, String address) {
