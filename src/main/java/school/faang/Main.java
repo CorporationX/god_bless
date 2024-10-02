939b9517b390da3ae039a2e46c610ee2f5c61267
AlekseyB86nb <tigrxam@gmail.com>
2024-10-02 22:41:16 +0300

    Абстракция, абстракция

--- src/main/java/school/faang/Main.java
+++ src/main/java/school/faang/Main.java
@@ -1,4 +1,13 @@
 package school.faang;
 
 public class Main {
+
+    public static void main(String[] args) {
+        Warrior warrior = new Warrior("Vlad", 12, 10, 3);
+        Archer archer = new Archer("Petya", 8, 5, 10);
+
+        warrior.attack(archer);
+        archer.attack(warrior);
+    }
+
 }
