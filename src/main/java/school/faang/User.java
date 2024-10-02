ac840e22404f012080c178abddf12468d14b7254
Alexander Litovkin <litovkinalexan@gmail.com>
2024-10-02 21:54:44 +0700

    refactor code style

--- src/main/java/school/faang/User.java
+++ src/main/java/school/faang/User.java
@@ -13,11 +13,8 @@ import java.util.Map;
 public class User {
 
     private String name;
-
     private int age;
-
     private String job;
-
     private String address;
 
     public static Map<Integer, List<User>> groupUsers(List<User> users) {
