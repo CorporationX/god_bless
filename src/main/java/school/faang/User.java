2759aeef75bc629ad9cfac7e4b05de83f8210872
heckfy86 <marlinco263@gmail.com>
2024-10-02 17:26:28 +0200

    BSJ2-31580

--- src/main/java/school/faang/User.java
+++ src/main/java/school/faang/User.java
@@ -7,6 +7,7 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Set;
 
 @Getter
 @Setter
@@ -16,7 +17,23 @@ public class User {
     private String workingPlace;
     private String address;
 
+    private static final Set<String> VALID_JOBS = Set.of("Google", "Uber", "Amazon");
+    private static final Set<String> VALID_ADDRESSES = Set.of("London", "New York", "Amsterdam");
     public User(String name, Integer age, String workingPlace, String address) {
+        if (name == null || name.trim().isEmpty()) {
+            throw new IllegalArgumentException("The name cannot be empty.");
+        }
+        if (age < 18) {
+            throw new IllegalArgumentException("Age cannot be less than 18.");
+        }
+        if (!VALID_JOBS.contains(workingPlace))
+        {
+            throw new IllegalArgumentException("Uncorrected workingPlace");
+        }
+        if (!VALID_ADDRESSES.contains(address))
+        {
+            throw new IllegalArgumentException("Uncorrected address");
+        }
         this.name = name;
         this.age = age;
         this.workingPlace = workingPlace;
