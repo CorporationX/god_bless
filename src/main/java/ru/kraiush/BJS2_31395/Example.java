7668ca83a2c8d8f2126330950c3a25a429cca863
Ekaterina Badratdinova <ekaterinafeshch@gmail.com>
2024-10-01 18:42:41 -0700

    Развернитесь!

---
+++ src/main/java/ru/kraiush/BJS2_31395/Example.java
@@ -0,0 +1,13 @@
+package ru.kraiush.BJS2_31395;
+
+public class Example {
+
+    public static int[] reverse(int [] nums) {
+        for (int i = 0; i < nums.length / 2; i++) {
+            int temp = nums[i];
+            nums[i] = nums[nums.length - 1 - i];
+            nums[nums.length - 1 - i] = temp;
+        }
+        return nums;
+    }
+}
