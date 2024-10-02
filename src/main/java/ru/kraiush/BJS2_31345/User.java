a564dbe04e6dc5d0f68c5e416b5673e7bafc8a31
Ekaterina Badratdinova <ekaterinafeshch@gmail.com>
2024-10-01 23:06:23 -0700

    Регистрация пользователей BSJ2-31506: refactoring

--- src/main/java/ru/kraiush/BJS2_31345/User.java
+++ src/main/java/ru/kraiush/BJS2_31345/User.java
@@ -20,20 +20,22 @@ public class User {
     private String address;
 
     public User(String name, int age, String company, String address) {
-        if (name.isBlank() || age < VALID_AGE || !VALID_JOBS.contains(company) || !VALID_ADDRESSES.contains(address)) {
-            throw new IllegalArgumentException("""
-                    Invalid data:
-                    Name should not be blank. Age should be equal or greater than  18.
-                    Company should be from the list: Google, Uber or Amazon.
-                    Address should be from the list: London, New York, "Amsterdam.
-                    """
-                    );
-        } else {
-            this.name = name;
-            this.age = age;
-            this.company = company;
-            this.address = address;
+        if (name.isBlank()) {
+            throw new IllegalArgumentException("Name should not be blank");
         }
+        if (age < VALID_AGE) {
+            throw new IllegalArgumentException("Age should be equal or greater than 18");
+        }
+        if (!VALID_JOBS.contains(company)) {
+            throw new IllegalArgumentException("Company should be from the list: Google, Uber or Amazon");
+        }
+        if (!VALID_ADDRESSES.contains(address)) {
+            throw new IllegalArgumentException("Address should be from the list: London, New York, Amsterdam");
+        }
+        this.name = name;
+        this.age = age;
+        this.company = company;
+        this.address = address;
     }
 
     public static Map<Integer, List<User>> groupUsers(List<User> users) {
