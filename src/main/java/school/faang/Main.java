d240f99afffb87267b5f6e33bb07d34c65099b85
Dmitrii Pazinich <pazinich.d@yandex.ru>
2024-10-02 15:46:36 +0300

    Task "Регистрация пользователя" solution.

--- src/main/java/school/faang/Main.java
+++ src/main/java/school/faang/Main.java
@@ -7,20 +7,60 @@ public class Main {
 
     public static void main(String[] args) {
 
-        // Проверим работу метода на списке пользователей.
-        List<User> usersList = List.of(
-                new User("John", 25, "Google",
-                        "America"),
-                new User("God", 2024, "Heaven",
-                        "World"),
-                new User("Satan", 2024, "Hell",
-                        "World"),
-                new User("Piper", 25, "Orchestra",
-                        "China"),
-                new User("Bobby", 25, "Google",
-                        "UK")
-        );
+        List<User> usersList = null;
 
+        // Проверим создание корректных пользователей
+        System.out.println("Проверим создание корректных пользователей:");
+
+        try {
+            usersList = List.of(
+                    new User("John", 28, "Google", "New York"),
+                    new User("Bobby", 28, "Uber", "New York"),
+                    new User("Robert", 33, "Amazon", "Amsterdam"),
+                    new User("Dylan", 25, "Google", "London"),
+                    new User("Pepe", 33, "Google", "London")
+            );
+            System.out.println("Пользователи созданы успешно.");
+            System.out.println();
+            System.out.println("~".repeat(30));
+            System.out.println();
+        } catch (IllegalArgumentException e) {
+            System.out.println(e.getMessage());
+        }
+
+        // Проверим группировку пользователей
+        System.out.println("Проверим группировку пользователей:");
         System.out.println(User.groupUsers(usersList));
+        System.out.println();
+        System.out.println("~".repeat(30));
+        System.out.println();
+
+        // Проверим создание невалидных пользователей
+        System.out.println("Проверка случаев создания невалидных пользователей:");
+        System.out.println();
+
+        try {
+            new User("", 18, "Google", "London");
+        } catch (IllegalArgumentException e) {
+            System.out.println("Проверка невалидного имени: " + e.getMessage());
+        }
+
+        try {
+            new User("Daniel", 15, "Google", "London");
+        } catch (IllegalArgumentException e) {
+            System.out.println("Проверка невалидного возраста: " + e.getMessage());
+        }
+
+        try {
+            new User("Daniel", 18, "WrongGoogle", "London");
+        } catch (IllegalArgumentException e) {
+            System.out.println("Проверка невалидного места работы: " + e.getMessage());
+        }
+
+        try {
+            new User("Daniel", 15, "Google", "WrongLondon");
+        } catch (IllegalArgumentException e) {
+            System.out.println("Проверка невалидного места жительства: " + e.getMessage());
+        }
     }
 }
