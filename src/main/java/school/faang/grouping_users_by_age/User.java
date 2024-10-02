4896d5af7843e3c8613c1264ff26c81cfb2f2d76
Dmitry Shakhaev <dmtry.shahaev@gmail.com>
2024-10-02 16:44:50 +0300

    Task: Grouping users by age, added User class, Main class (for test)

---
+++ src/main/java/school/faang/grouping_users_by_age/User.java
@@ -0,0 +1,55 @@
+package school.faang.grouping_users_by_age;
+
+import lombok.Getter;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Set;
+
+@Getter
+public class User {
+    private static final Set<String> PLACE_OF_WORK = Set.of("Yandex", "Sber", "Aston", "VK");
+    private static final Set<String> ADDRESS = Set.of("Moscow", "Saint-Petersburg", "Kazan");
+    private String userName;
+    private int userAge;
+    private String placeOfWork;
+    private String address;
+
+    public User(String userName, int userAge, String placeOfWork, String address) {
+        if (userName == null || userName.isBlank()) {
+            throw new IllegalArgumentException("Имя пользователя не может быть пустым");
+        }
+        this.userName = userName;
+
+        if (userAge <= 0) {
+            throw new IllegalArgumentException("Возраст не может быть нулевым или отрицательным");
+        }
+        this.userAge = userAge;
+
+        if (!PLACE_OF_WORK.contains(placeOfWork)) {
+            throw new IllegalArgumentException("Неверное рабочее место");
+        }
+        this.placeOfWork = placeOfWork;
+
+        if (!ADDRESS.contains(address)) {
+            throw new IllegalArgumentException("Неверный адрес");
+        }
+        this.address = address;
+    }
+
+    public static HashMap<Integer, ArrayList<User>> groupUsers(List<User> users) {
+        HashMap<Integer, ArrayList<User>> gropedUsers = new HashMap<>();
+        for (User user : users) {
+            gropedUsers.computeIfAbsent(user.getUserAge(), age -> new ArrayList<>()).add(user);
+        }
+        return gropedUsers;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "userName='" + userName + '\'' +
+                '}';
+    }
+}
