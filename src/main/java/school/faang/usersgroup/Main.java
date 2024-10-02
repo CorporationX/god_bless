6920f22610008eddd51a74dde28643dcb61f6bfb
Ilya Ishanov <ilia.detroit2016@yandex.ru>
2024-10-03 00:32:38 +0500

    Вторая попытка

--- src/main/java/school/faang/usersgroup/Main.java
+++ src/main/java/school/faang/usersgroup/Main.java
@@ -7,15 +7,15 @@ import java.util.HashMap;
 
 public class Main {
     public static void main(String[] args) {
-        List<Users> students = new ArrayList<>();
-        students.add(new Users("Den", 22, "Amazon", "Manshen street"));
-        students.add(new Users("Ben", 23, "Sber", "Lenin street"));
-        students.add(new Users("Ivan", 21, "Okko", "Degin street"));
-        students.add(new Users("Petr", 18, "Uber", "Mira street"));
-        students.add(new Users("Nadia", 26, "Diksi", "Akademik street"));
-        students.add(new Users("Masha", 17, "Mebel", "Korolev street"));
-        students.add(new Users("Daria", 17, "Google", "Baiker street"));
-        students.add(new Users("Саша", 26, "EPAM", "Gorki street"));
+        List<Users> students = List.of(
+        new Users("Den", 22, "Amazon", "Manshen street"),
+        new Users("Ben", 23, "Sber", "Lenin street"),
+        new Users("Ivan", 21, "Okko", "Degin street"),
+        new Users("Petr", 18, "Uber", "Mira street"),
+        new Users("Nadia", 26, "Diksi", "Akademik street"),
+        new Users("Masha", 17, "Mebel", "Korolev street"),
+        new Users("Daria", 17, "Google", "Baiker street"),
+        new Users("Саша", 26, "EPAM", "Gorki street"));
 
         Map<Integer, ArrayList<Users>> studentsHasMap = Users.groupUsers(students);
         for (Map.Entry<Integer, ArrayList<Users>> student : studentsHasMap.entrySet()) {
--- src/main/java/school/faang/usersgroup/Users.java
+++ src/main/java/school/faang/usersgroup/Users.java
@@ -1,48 +1,20 @@
 package school.faang.usersgroup;
 
+import lombok.Getter;
+import lombok.Setter;
+
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-
+@Getter
+@Setter
 public class Users {
     private String name;
     private Integer age;
     private String workPlace;
     private String address;
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public Integer getAge() {
-        return age;
-    }
-
-    public void setAge(Integer age) {
-        this.age = age;
-    }
-
-    public String getWorkPlace() {
-        return workPlace;
-    }
-
-    public void setWorkPlace(String workPlace) {
-        this.workPlace = workPlace;
-    }
-
-    public String getAddress() {
-        return address;
-    }
-
-    public void setAddress(String address) {
-        this.address = address;
-    }
 
     public Users(String name, int age, String workPlace, String address) {
         this.name = name;
@@ -63,17 +35,9 @@ public class Users {
 
     public static Map<Integer,ArrayList<Users>> groupUsers(List<Users> list) {
         Map<Integer, ArrayList<Users>> usersHashMap = new HashMap<>();
-        for (Users usersList: list) {
-            List<Users> usersArrayList = new ArrayList<>();
-            usersArrayList.add(new Users(usersList.getName(), usersList.getAge(), usersList.getWorkPlace(), usersList.getAddress()));
-
-                for (Users secondList: list) { //не знал как по другому сравнить возраст если есть студенты с одинаковым возрастом, сделал цикл
-                    if (usersList.getAge().equals(secondList.getAge()) && (!usersList.name.equals(secondList.getName()))) {
-                        usersArrayList.add(new Users(secondList.getName(), secondList.getAge(), secondList.getWorkPlace(), secondList.getAddress()));
-                    }
-                }
-            usersHashMap.put(usersList.getAge(), (ArrayList<Users>) usersArrayList);
-        }
+            for (Users usersList: list) {
+                usersHashMap.computeIfAbsent(usersList.getAge(), user -> new ArrayList<>()).add(usersList);
+            }
         return usersHashMap;
     }
 }
