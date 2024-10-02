8317fb535dcb6f3ff79464a7cfedb777f3f1d613
AnnaAplavina <59871896+AnnaAplavina@users.noreply.github.com>
2024-10-02 17:38:01 +0300

    formatting and computeIfAbsent

--- src/main/java/school/faang/godbless/bjs2_28996/Main.java
+++ src/main/java/school/faang/godbless/bjs2_28996/Main.java
@@ -7,8 +7,7 @@ public class Main {
 
   public static void main(String[] args) {
     List<User> users = List.of(
-        new User("Homelander", 32, "The Seven, leader",
-            "29 Washington Pl, New York, NY 10003, USA"),
+        new User("Homelander", 32, "The Seven, leader", "29 Washington Pl, New York, NY 10003, USA"),
         new User("Geralt of Rivia", 94, "Kaer Morhen, witcher", "12 Witcher PL, Rivia"),
         new User("Dexter Morgan", 32, "Miami Police Department, Forensics Specialist",
             "Miami, Florida(FL), 33162")
--- src/main/java/school/faang/godbless/bjs2_28996/User.java
+++ src/main/java/school/faang/godbless/bjs2_28996/User.java
@@ -19,9 +19,7 @@ public class User {
   public static Map<Integer, List<User>> groupUsers(List<User> users) {
     Map<Integer, List<User>> usersMap = new HashMap<>();
     for (User user : users) {
-      if (!usersMap.containsKey(user.age)) {
-        usersMap.put(user.age, new ArrayList<>());
-      }
+      usersMap.computeIfAbsent(user.age, k -> new ArrayList<>());
       List<User> usersList = usersMap.get(user.age);
       usersList.add(user);
     }
