7b39aa44f432c3355b74686a31b99e33a6df5052
Alexey Zimin <zimin.alexey@gmail.com>
2024-10-03 02:26:25 +0300

    [BJS2-31601] - User registration

--- src/main/java/school/faang/Main.java
+++ src/main/java/school/faang/Main.java
@@ -1,4 +1,14 @@
 package school.faang;
 
 public class Main {
+    public static void main(String[] args) {
+
+        try {
+            new User("Alex", 20, "Google", "London");
+            System.out.println("It's OK");
+
+        } catch (IllegalArgumentException e) {
+            System.out.println(e.getMessage());
+        }
+    }
 }
