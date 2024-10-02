c2f1d5669ea35774a3cd7b2f330bcfdebb59e359
Dmytro Yevsieienko <youdima70@gmail.com>
2024-10-02 06:21:16 +0200

    Добавлен импорт assertThrows в тест

--- src/test/java/dima/evseenko/group/UserTest.java
+++ src/test/java/dima/evseenko/group/UserTest.java
@@ -6,6 +6,7 @@ import java.util.Arrays;
 import java.util.List;
 import java.util.Map;
 
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 
