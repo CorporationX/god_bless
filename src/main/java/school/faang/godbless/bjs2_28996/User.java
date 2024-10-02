9ec91bbf675835b114b8b8d02874defb1bcb3eaa
AnnaAplavina <59871896+AnnaAplavina@users.noreply.github.com>
2024-10-02 16:46:08 +0300

    completed the task

---
+++ src/main/java/school/faang/godbless/bjs2_28996/User.java
@@ -0,0 +1,30 @@
+package school.faang.godbless.bjs2_28996;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class User {
+
+  private String name;
+  private int age;
+  private String job;
+  private String address;
+
+  public static Map<Integer, List<User>> groupUsers(List<User> users) {
+    Map<Integer, List<User>> usersMap = new HashMap<>();
+    for (User user : users) {
+      if (!usersMap.containsKey(user.age)) {
+        usersMap.put(user.age, new ArrayList<>());
+      }
+      List<User> usersList = usersMap.get(user.age);
+      usersList.add(user);
+    }
+    return usersMap;
+  }
+}
