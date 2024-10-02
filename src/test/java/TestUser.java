2cae14021048708b241fb170ff5f5e5235b8bc2d
Vakarash <max.vackarash@gmail.com>
2024-10-02 23:23:31 +0300

    BJS2-31488

--- src/test/java/TestUser.java
+++ src/test/java/TestUser.java
@@ -1,35 +1,30 @@
-import school.faang.User;
+import org.junit.jupiter.api.Test;
+import school.faang.collectUsers.User;
 
 import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.Set;
 
-import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class TestUser {
 
     @Test
-    public void testIfGroupedByAge() {
+    public void testIfActivitiesAreFoundForUser() {
         // arrange
-        User user1 = new User("Maksim", 20, "No job", "Moscow");
-        User user2 = new User("Aleksandr", 30, "Factory", "Novgorod");
-        User user3 = new User("Katya", 20, "Student", "Tver");
-        User user4 = new User("John", 30, "Businessman", "Klin");
-        User user5 = new User("Shrek", 25, "Saving the world", "Swamp");
+        User user1 = new User(1, "Sanchez", 20, Set.of("Swimming", "Basketball", "Football", "Video games"));
+        User user2 = new User(2, "Oleg", 40, Set.of("Sleeping", "Eating", "Dota 2"));
+        List<User> usersList = List.of(user1, user2);
+        Set<String> activityList = Set.of("Crypto", "Running", "Basketball", "Video games", "Eating burgers");
 
-        List<User> usersList = List.of(user1, user2, user3, user4, user5);
+        Map<User, String> expected = new HashMap<>();
 
         // act
-        HashMap<Integer, List<User>> actual = User.groupUsers(usersList);
+        expected.put(user1, "Basketball");
+        Map<User, String> actual = User.findHobbyLovers(usersList, activityList);
 
         // assert
-        assertEquals(2, actual.get(20).size());
-        assertEquals(2, actual.get(30).size());
-        assertEquals(1, actual.get(25).size());
-
-        assertEquals("Maksim", actual.get(20).get(0).getName());
-        assertEquals("Katya", actual.get(20).get(1).getName());
-        assertEquals("Aleksandr", actual.get(30).get(0).getName());
-        assertEquals("John", actual.get(30).get(1).getName());
+        assertEquals(expected, actual, "Must return a map with a key user1 and only a value of Basketball");
     }
 }
