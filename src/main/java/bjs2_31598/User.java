a4ca1aae9e11d7c57843b1bd153d1e323b77db0f
light <estarosa@list.ru>
2024-10-03 00:16:53 +0300

    Правки в задачу: "Регистрация пользователя"

--- src/main/java/bjs2_31598/User.java
+++ src/main/java/bjs2_31598/User.java
@@ -2,18 +2,13 @@ package bjs2_31598;
 
 import lombok.Data;
 
-import java.util.HashSet;
-import java.util.List;
 import java.util.Set;
 
 @Data
 public class User {
-    public static final Set<String> VALID_JOBS = new HashSet<>(
-            List.of("Google", "Uber", "Amazon")
-    );
-    public static final Set<String> VALID_ADDRESSES = new HashSet<>(
-            List.of("London", "New York", "Amsterdam")
-    );
+    public static final Set<String> VALID_JOBS = Set.of("Google", "Uber", "Amazon");
+    public static final Set<String> VALID_ADDRESSES = Set.of("London", "New York", "Amsterdam");
+    public static final Integer MIN_USER_AGE = 18;
 
     private String name;
     private Integer age;
@@ -44,7 +39,7 @@ public class User {
     }
 
     private static boolean isUserAgeValid(Integer age) {
-        if (age >= 18) {
+        if (age >= MIN_USER_AGE) {
             return true;
         }
         throw new IllegalArgumentException("User is under 18 years old");
