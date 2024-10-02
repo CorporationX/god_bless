b7f1eed3e3b7d7e3da54371078a048fb3213bda5
Aliyar <aliartalybov@gmail.com>
2024-10-02 18:58:56 +0300

    решение задаиа https://faang-school.atlassian.net/browse/BJS2-28997

---
+++ src/main/java/school/faang/Character.java
@@ -0,0 +1,22 @@
+package school.faang;
+import lombok.Getter;
+import lombok.Setter;
+
+
+abstract public class Character {
+    String name;
+    int strength;
+    int dexterity;
+    int intelligence;
+    int health;
+
+    public Character(String name, int strength, int dexterity, int intelligence) {
+        this.name = name;
+        this.strength = strength;
+        this.dexterity = dexterity;
+        this.intelligence = intelligence;
+        this.health = 100; // Устанавливаем здоровье по умолчанию
+    }
+
+    abstract public void attack(Character enemy);
+}
