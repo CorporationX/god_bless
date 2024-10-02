af6ea77208b759ef3f68905338c4a6be1ca51fcf
Alexander Litovkin <litovkinalexan@gmail.com>
2024-10-02 18:53:23 +0700

    add execution example into main

--- src/main/java/school/faang/Main.java
+++ src/main/java/school/faang/Main.java
@@ -1,4 +1,28 @@
 package school.faang;
 
 public class Main {
+
+    public static void main(String[] args) {
+        User user = new User("Alex", 21, "Google", "New York");
+        try {
+            user = new User("", 20, "Google", "New York");
+        } catch (IllegalArgumentException e) {
+            System.out.println(e.getMessage());
+        }
+        try {
+            user = new User("Alex", 10, "Google", "New York");
+        } catch (IllegalArgumentException e) {
+            System.out.println(e.getMessage());
+        }
+        try {
+            user = new User("Alex", 20, "", "New York");
+        } catch (IllegalArgumentException e) {
+            System.out.println(e.getMessage());
+        }
+        try {
+            user = new User("Alex", 20, "Google", "");
+        } catch (IllegalArgumentException e) {
+            System.out.println(e.getMessage());
+        }
+    }
 }
