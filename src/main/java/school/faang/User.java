103dff1989cb52bdca51f2f974c64d9c3fce07d6
Vakarash <max.vackarash@gmail.com>
2024-10-02 17:27:59 +0300

    BJS2-31529

--- src/main/java/school/faang/User.java
+++ src/main/java/school/faang/User.java
@@ -11,22 +11,45 @@ import java.util.Set;
 public class User {
     private static final Set<String> VALID_JOBS = Set.of("Google", "Uber", "Amazon");
     private static final Set<String> VALID_ADDRESSES = Set.of("London", "New York", "Amsterdam");
+    private static final int VALID_AGE = 18;
     private String name;
     private int age;
     private String job;
     private String address;
 
     public User(String name, int age, String job, String address) {
-        if (name == null || name.isBlank()
-                || age < 18
-                || !VALID_JOBS.contains(job)
-                || !VALID_ADDRESSES.contains(address)) {
-            throw new IllegalArgumentException("Invalid inputs");
-        } else {
-            this.name = name;
-            this.age = age;
-            this.job = job;
-            this.address = address;
+        checkName(name);
+        checkAge(age);
+        checkJob(job);
+        checkAddress(address);
+
+        this.name = name;
+        this.age = age;
+        this.job = job;
+        this.address = address;
+    }
+
+    private void checkName(String name) {
+        if (name == null || name.isBlank()) {
+            throw new IllegalArgumentException("Illegal name - user's name can't be null or blank.");
+        }
+    }
+
+    private void checkAge(int age) {
+        if (age < VALID_AGE) {
+            throw new IllegalArgumentException("Illegal age - user can't be a minor.");
+        }
+    }
+
+    private void checkJob(String job) {
+        if (!VALID_JOBS.contains(job)) {
+            throw new IllegalArgumentException("Illegal job - user must work in Google, Uber or Amazon");
+        }
+    }
+
+    private void checkAddress(String address) {
+        if (!VALID_ADDRESSES.contains(address)) {
+            throw new IllegalArgumentException("Illegal address - user must be in London, New York or Amsterdam");
         }
     }
 
