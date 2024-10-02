540f724ab998aaba6715dbf9c488a27cd39d4a8b
Alina44444 <alina.valiyeva.s@mail.ru>
2024-10-02 14:50:04 +0500

    Задача Развернитесь!

---
+++ src/main/java/faang/school/godbless/Example.java
@@ -0,0 +1,18 @@
+package faang.school.godbless;
+
+public class Example {
+    int[] reverse(int e[]) {
+        int e1[] = new int[e.length];
+        for (int i = e.length - 1, i1 = 0; i >= 0; i--, i1++) {
+            e1[i1] = e[i];
+        }
+        for (int i : e1)
+            System.out.println(i);
+        return e1;
+    }
+
+    public static void main(String[] args) {
+        Example d = new Example();
+        System.out.println(d.reverse(new int[]{1, 2, 3, 4, 5}));
+    }
+}
