485b51f211ae1a2719b5e12670a748d461252967
Юрий <subbotin.yuriy3@wb.ru>
2024-10-02 23:50:11 +0300

    Update User.java

--- src/main/java/grouping_users_by_age/User.java
+++ src/main/java/grouping_users_by_age/User.java
@@ -8,7 +8,6 @@ import java.util.List;
 import java.util.Map;
 import java.util.Set;
 
-@AllArgsConstructor
 @Data
 public class User {
     private static final Set<String> VALID_JOBS = Set.of("Google", "Uber", "Amazon");
