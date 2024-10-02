6e16cbdfb2cc8757246a94ef00d77db4199e47ef
Dmytro Yevsieienko <youdima70@gmail.com>
2024-10-02 10:34:46 +0200

    Развернитесь!

---
+++ src/test/java/dima/evseenko/reverse/ExampleTest.java
@@ -0,0 +1,24 @@
+package dima.evseenko.reverse;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+class ExampleTest {
+
+    @Test
+    void reverseValidArray(){
+        assertArrayEquals(new int[]{8,7,6,5,4,3,2,1}, Example.reverse(new int[]{1,2,3,4,5,6,7,8}));
+    }
+
+    @Test
+    void reverseEmptyArray(){
+        assertArrayEquals(new int[]{}, Example.reverse(new int[]{}));
+    }
+
+    @Test
+    void reverseNull() {
+        assertThrows(IllegalArgumentException.class, () -> Example.reverse(null));
+    }
+}
