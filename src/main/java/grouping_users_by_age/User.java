706c433ce46eabe38366ef351dd525887b28c881
Юрий <subbotin.yuriy3@wb.ru>
2024-10-02 14:12:09 +0300

    Update User.java

--- src/main/java/grouping_users_by_age/User.java
+++ src/main/java/grouping_users_by_age/User.java
@@ -1,6 +1,7 @@
 package grouping_users_by_age;
 
 import lombok.AllArgsConstructor;
+import lombok.Data;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
@@ -11,9 +12,7 @@ import java.util.List;
 import java.util.Map;
 
 @AllArgsConstructor
-@Getter
-@Setter
-@ToString
+@Data
 public class User {
     private String name;
     private int age;
@@ -22,15 +21,9 @@ public class User {
 
     public static Map<Integer, List<User>> groupUsers(List<User> users) {
         Map<Integer, List<User>> usersByAge = new HashMap<>();
-
         for (User user : users) {
-            if (usersByAge.get(user.getAge()) != null) {
-                usersByAge.get(user.getAge()).add(user);
-            } else {
-                usersByAge.put(user.getAge(), new ArrayList<>(List.of(user)));
-            }
+            usersByAge.computeIfAbsent(user.getAge(), k -> new ArrayList<>()).add(user);
         }
-
         return usersByAge;
     }
 }
