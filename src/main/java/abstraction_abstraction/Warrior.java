0270601dff59957f7aa54e6779bd41b017c0ec8f
Юрий <subbotin.yuriy3@wb.ru>
2024-10-02 22:35:09 +0300

    BJS2-31278

---
+++ src/main/java/abstraction_abstraction/Warrior.java
@@ -0,0 +1,12 @@
+package abstraction_abstraction;
+
+public class Warrior extends Character {
+    public Warrior(String name) {
+        super(name, 10, 5, 3);
+    }
+
+    @Override
+    public void attack(Character target) {
+        target.setHealth(target.getHealth() - this.getPower());
+    }
+}
