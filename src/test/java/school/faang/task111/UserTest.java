90c5d0d2859274b66d489f7f9644efee6bbf160b
Svyatoslav Kostrov <kost-2014@mail.ru>
2024-10-02 17:45:01 +0300

    Rebuild solution for first task

--- src/test/java/school/faang/task111/UserTest.java
+++ src/test/java/school/faang/task111/UserTest.java
@@ -1,6 +1,5 @@
 package school.faang.task111;
 
-import org.assertj.core.api.Assert;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
@@ -17,25 +16,25 @@ class UserTest {
     @Test
     void groupUsers() {
         List<User> testList = new ArrayList<>();
-        testList.add(new User("Вася",14,"Самокат","Г.Москва"));
-        testList.add(new User("Илья",14,"Яндекс","Г.Адлер"));
-        testList.add(new User("masha",15,"ММК","Г.Магнитогорск"));
+        User user1=new User("Вася",14,"Самокат","Г.Москва");
+        User user2=new User("Илья",14,"Яндекс","Г.Адлер");
+        User user3=new User("masha",15,"ММК","Г.Магнитогорск");
+
+        testList=List.of(user1,user2,user3);
 
         Map<Integer,List<User>> testMap = new HashMap<>();
-
-        List<User> listForMap = new ArrayList<>();
-        listForMap.add(new User("Вася",14,"Самокат","Г.Москва"));
-        listForMap.add(new User("Илья",14,"Яндекс","Г.Адлер"));
-
+        List<User> listForMap=List.of(user1,user2);
         testMap.put(14,listForMap);
 
         List<User> secondListForMap = new ArrayList<>();
-        secondListForMap.add(new User("masha",15,"ММК","Г.Магнитогорск"));
-
+        secondListForMap.add(user3);
         testMap.put(15,secondListForMap);
 
         Map<Integer,List<User>> actualMap= User.groupUsers(testList);
 
-        Assertions.assertEquals(testMap,actualMap);
+        Assertions.assertEquals(new HashMap<>(),actualMap);
+        Assertions.assertEquals(2,actualMap.get(14).size());
+        Assertions.assertEquals(113231322,actualMap.get(15).size());
+        Assertions.assertEquals(user1,actualMap.get(14).get(1));
     }
 }
