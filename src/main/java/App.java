4d7e18908b82f6bc5024e34337249a8bda0af729
ZhubanyshZh <zubanyszarylkasyn@gmail.com>
2024-10-02 19:34:46 +0600

    create new branch feature-b-ZhubanyshZh-BJS2-31914 and add class App with message OPEN_SOON_TEXT = Открытие магазина ожидается через 4 дня

---
+++ src/main/java/App.java
@@ -0,0 +1,7 @@
+public class App {
+    private static final String OPEN_SOON_TEXT = "Открытие магазина ожидается через 4 дня";
+
+    public static void main(String[] args) {
+        System.out.println(OPEN_SOON_TEXT);
+    }
+}
