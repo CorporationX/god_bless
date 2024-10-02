7671181c1db6ccf7d31ca69c7e3e3bf1114223cc
Alexander Litovkin <litovkinalexan@gmail.com>
2024-10-02 19:38:48 +0700

    add execution example into main

--- src/main/java/school/faang/Main.java
+++ src/main/java/school/faang/Main.java
@@ -1,4 +1,19 @@
 package school.faang;
 
 public class Main {
+
+    public static void main(String[] args) {
+        Character warrior = new Warrior("Alex");
+        Character archer = new Archer("Ivan");
+        System.out.println(
+                "warrior health: " + warrior.getHealth() +
+                "; archer health: " + archer.getHealth()
+        );
+        warrior.attack(archer);
+        archer.attack(warrior);
+        System.out.println(
+                "warrior health: " + warrior.getHealth() +
+                "; archer health: " + archer.getHealth()
+        );
+    }
 }
