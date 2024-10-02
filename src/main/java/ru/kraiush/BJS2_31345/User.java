5bb125a80795cca097cb0bdabe7fda7926ac1dbc
Ekaterina Badratdinova <ekaterinafeshch@gmail.com>
2024-10-01 20:13:34 -0700

    Регистрация пользователей BSJ2-31506

--- src/main/java/ru/kraiush/BJS2_31345/User.java
+++ src/main/java/ru/kraiush/BJS2_31345/User.java
@@ -1,21 +1,34 @@
 package ru.kraiush.BJS2_31345;
 
-import lombok.AllArgsConstructor;
 import lombok.Data;
 
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.stream.Collectors;
+import java.util.Set;
 
 @Data
-@AllArgsConstructor
 public class User {
+    private static Set<String> VALID_JOBS = Set.of("Google", "Uber", "Amazon");
+    private static Set<String> VALID_ADDRESSES = Set.of("London", "New York", "Amsterdam");
+    private static int VALID_AGE = 18;
+
     private String name;
     private int age;
     private String company;
     private String address;
+
+    public User(String name, int age, String company, String address) {
+        if (name.isBlank() || age < VALID_AGE || !VALID_JOBS.contains(company) || !VALID_ADDRESSES.contains(address)) {
+            throw new IllegalArgumentException("");
+        } else {
+            this.name = name;
+            this.age = age;
+            this.company = company;
+            this.address = address;
+        }
+    }
 
     public static Map<Integer, List<User>> groupUsers(List<User> users) {
         Map<Integer, List<User>> groupedUsersByAge = new HashMap<>();
