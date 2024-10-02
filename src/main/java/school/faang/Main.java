6213eea0a84a0ea5e675796202b54d28ac2cba31
Gadzhiev Gadzhi <Gadzhiev3222@gmail.com>
2024-10-02 08:22:03 +0300

    Group users commit

--- src/main/java/school/faang/Main.java
+++ src/main/java/school/faang/Main.java
@@ -1,4 +1,19 @@
 package school.faang;
 
+import java.util.Arrays;
+import java.util.List;
+
 public class Main {
+    public static void main(String[] args) {
+        List<User> users = Arrays.asList(
+                new User("Gadzhi", 20, "google", "address"),
+                new User("Ali", 20, "microsoft", "address"),
+                new User("Maks", 20, "sber", "address"),
+                new User("Anastasya", 13, "amazon", "address"),
+                new User("Vlad", 13, "uber", "address"),
+                new User("Andrey", 33, "meta", "address")
+        );
+
+        System.out.println(User.groupUsers(users));
+    }
 }
