59847016a4d9b267e5f8f6c98f72863ef8e31b69
Alexander Litovkin <litovkinalexan@gmail.com>
2024-10-02 19:37:43 +0700

    add Warrior and Archer classes

---
+++ src/main/java/school/faang/Warrior.java
@@ -0,0 +1,19 @@
+package school.faang;
+
+public class Warrior extends Character {
+
+    private static final int BASE_STRENGTH = 10;
+
+    private static final int BASE_AGILITY = 5;
+
+    private static final int BASE_INTELLIGENCE = 3;
+
+    public Warrior(String name) {
+        super(name, BASE_STRENGTH, BASE_AGILITY, BASE_INTELLIGENCE);
+    }
+
+    @Override
+    public void attack(Character character) {
+        character.setHealth(character.getHealth() - this.strength);
+    }
+}
