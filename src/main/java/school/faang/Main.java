bf14cfb9b00add9abd6394eb9293c3ae4e10f520
Memoreks <akifyevalex1@gmail.com>
2024-10-02 21:36:35 +0700

    Создан класс User и реализован метод groupUsers

--- src/main/java/school/faang/Main.java
+++ src/main/java/school/faang/Main.java
@@ -1,4 +1,27 @@
 package school.faang;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 public class Main {
+    public class User {
+        private String name;
+        private Integer age;
+        private String job;
+        private String address;
+
+        static Map<Integer, List<User>> groupUsers(List<User> usersList) {
+            Map<Integer, List<User>> usersMap = new HashMap<>();
+
+            for(User user : usersList) {
+                if(!usersMap.containsKey(user.age)) {
+                    usersMap.put(user.age, new ArrayList<>());
+                }
+                usersMap.get(user.age).add(user);
+            }
+            return usersMap;
+        }
+    }
 }
