63cb7fdea670ee1cb95625a182b543b2248716b4
Dmytro Yevsieienko <youdima70@gmail.com>
2024-10-02 05:27:01 +0200

    Добавлена группировка пользователей по возрасту

---
+++ src/main/java/dima/evseenko/group/Main.java
@@ -0,0 +1,26 @@
+package dima.evseenko.group;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) {
+        new Main().start();
+    }
+
+    private void start(){
+        System.out.println(User.groupUsers(getUsers()));
+    }
+
+    private List<User> getUsers(){
+        return Arrays.asList(
+                new User("Dima", 22, "Amazon", "Poland"),
+                new User("Kolya", 20, "Randstad", "Poland"),
+                new User("Olya", 22, "Google", "England"),
+                new User("Vasya", 21, "DNS", "Russia"),
+                new User("Vladislav", 19, "Uber", "Amsterdam"),
+                new User("Polina", 23, "Amazon", "Poland"),
+                new User("Igor", 20, "Amazon", "Poland")
+        );
+    }
+}
---
+++ src/main/java/dima/evseenko/group/User.java
@@ -0,0 +1,32 @@
+package dima.evseenko.group;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+import java.util.*;
+
+@Data
+@AllArgsConstructor
+public class User {
+    private String name;
+    private int age;
+    private String job;
+    private String address;
+
+    public static Map<Integer, List<User>> groupUsers(List<User> users) {
+        Map<Integer, List<User>> groupedUsersMap = new HashMap<Integer, List<User>>();
+
+        users.forEach(u -> {
+            if(groupedUsersMap.containsKey(u.getAge())) {
+                if(!groupedUsersMap.get(u.getAge()).contains(u)) {
+                    groupedUsersMap.get(u.getAge()).add(u);
+                }
+            }else {
+                List<User> userList = new ArrayList<User>();
+                userList.add(u);
+                groupedUsersMap.put(u.getAge(), userList);
+            }
+        });
+        return groupedUsersMap;
+    }
+}
