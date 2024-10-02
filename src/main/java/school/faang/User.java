d44a45b2b1385eb71346b8f6a3d9c41444e6fb54
Alexander Litovkin <litovkinalexan@gmail.com>
2024-10-02 17:45:04 +0700

    implement User class

---
+++ src/main/java/school/faang/User.java
@@ -0,0 +1,29 @@
+package school.faang;
+
+import lombok.Data;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@Data
+public class User {
+
+    private String name;
+
+    private int age;
+
+    private String job;
+
+    private String address;
+
+    public static Map<Integer, List<User>> groupUsers(List<User> users) {
+        Map<Integer, List<User>> ageToUserList = new HashMap<>();
+        for (User user : users) {
+            ageToUserList.computeIfAbsent(user.getAge(), k -> new ArrayList<>()).add(user);
+        }
+
+        return ageToUserList;
+    }
+}
