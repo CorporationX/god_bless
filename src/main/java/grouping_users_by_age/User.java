5bc2ea8bb5dcbb7227b3bef70deea223e74d37fd
Юрий <subbotin.yuriy3@wb.ru>
2024-10-02 23:34:59 +0300

    Fix

--- src/main/java/grouping_users_by_age/User.java
+++ src/main/java/grouping_users_by_age/User.java
@@ -14,15 +14,14 @@ import java.util.Set;
 
 @Data
 public class User {
+    private static final Set<String> VALID_JOBS = Set.of("Google", "Uber", "Amazon");
+    private static final Set<String> VALID_ADDRESSES = Set.of("London", "New York", "Amsterdam");
+    private static final int MIN_AGE = 18;
+
     private String name;
     private int age;
     private String placeOfWorking;
     private String address;
-
-    private static Set<String> VALID_JOBS = Set.of("Google", "Uber", "Amazon");
-    private static Set<String> VALID_ADDRESSES = Set.of("London", "New York", "Amsterdam");
-
-    private static final int MIN_AGE = 18;
 
     public User(String name, int age, String placeOfWorking, String address) {
         if (name.isEmpty()) {
