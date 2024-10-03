beab46db73304b0906270a3bea8f802ce5c2fe7e
Alexey Zimin <zimin.alexey@gmail.com>
2024-10-04 02:23:21 +0300

    [BJS2-33081] - library

--- src/main/java/school/faang/Main.java
+++ src/main/java/school/faang/Main.java
@@ -1,30 +1,11 @@
 package school.faang;
 
-import java.util.List;
+import school.faang.library.TestLibrary;
 
 public class Main {
 
     public static void main(String[] args) {
 
-        List<User> userList = List.of(
-                new User("Alex", 30, "Sberbank", "Moscow"),
-                new User("Sergey", 27, "Alfa Bank", "Saint Petersburg"),
-                new User("Alina", 27, "T-bank", "Moscow"),
-                new User("Alex", 25, "VTB", "Saint Petersburg"),
-                new User("Kseniya", 31, "VTB", "Samara"),
-                new User("Kate", 35, "Sberbank", "Kazan"),
-                new User("Irina", 45, "T-bank", "Yekaterinburg"),
-                new User("Mary", 45, "Sberbank", "Челябинск"),
-                new User("Pete", 27, "Alfa Bank", "Chelyabinsk")
-        );
-
-        var groups = User.groupUsers(userList);
-        var keys = groups.keySet().stream().sorted(Integer::compareTo).toList();
-        for (Integer key : keys) {
-            for (User user : groups.get(key)) {
-                System.out.println(user);
-            }
-        }
-
+        new TestLibrary().doTest();
     }
 }
---
+++ src/main/java/school/faang/library/TestLibrary.java
@@ -0,0 +1,78 @@
+package school.faang.library;
+
+import java.util.Comparator;
+import java.util.HashMap;
+import java.util.Optional;
+
+public class TestLibrary {
+
+    public void doTest() {
+
+        var booksMap = new HashMap<Book, String>();
+        booksMap.put(new Book("Rich Dad", "Kiyosaki", 2010), "A150");
+        booksMap.put(new Book("Harry Potter", "Rowling", 2005), "B012");
+        booksMap.put(new Book("The Snow Queen", "Andersen", 2000), "F001");
+        booksMap.put(new Book("A Game of Thrones", "Martin", 2003), "K100");
+        booksMap.put(new Book("Peter Pan", "Barrie", 2003), "H123");
+        print(booksMap);
+
+        System.out.println();
+
+        System.out.println("Book added");
+        add(booksMap, new Book("The Little Prince", "Saint-Exupery", 2003), "H345");
+        print(booksMap);
+
+        var bookName = "Harry Potter";
+        var bookAuthor = "Rowling";
+        var bookYear = 2005;
+
+        System.out.println();
+        System.out.println("Book removed");
+        remove(booksMap, bookName, bookAuthor, bookYear);
+        print(booksMap);
+
+        System.out.println();
+        System.out.println("Book place found");
+        var bookNameForFind = "Rich Dad";
+        var bookAuthorForFind = "Kiyosaki";
+        var bookYearForFind = 2010;
+        var findResultOptional = find(booksMap, bookNameForFind, bookAuthorForFind, bookYearForFind);
+        findResultOptional.ifPresent(place -> {
+            System.out.print(new Book(bookNameForFind, bookAuthorForFind, bookYearForFind));
+            System.out.print(" - ");
+            System.out.println(place);
+        });
+
+        System.out.println();
+        System.out.println("Book place changed");
+        add(booksMap, new Book("A Game of Thrones", "Martin", 2003), "W333");
+        print(booksMap);
+    }
+
+    private void add(HashMap<Book, String> booksMap, Book newBook, String place) {
+        booksMap.put(newBook, place);
+    }
+
+    private void remove(HashMap<Book, String> booksMap, String title, String author, int year) {
+        var book = new Book(title, author, year);
+        booksMap.remove(book);
+    }
+
+    private Optional<String> find(HashMap<Book, String> booksMap, String title, String author, int year) {
+        var book = new Book(title, author, year);
+
+        if (booksMap.containsKey(book)) {
+            return Optional.of(booksMap.get(book));
+        }
+        return Optional.empty();
+    }
+
+    private void print(HashMap<Book, String> booksMap) {
+        var keys = booksMap.keySet().stream().sorted(Comparator.comparing(Book::getYear)).toList();
+        for (Book key : keys) {
+            System.out.print(key);
+            System.out.print(" - ");
+            System.out.println(booksMap.get(key));
+        }
+    }
+}
