27f4cb604e44edee88474dc1930546c2e8e2eac6
bogda <bogdanskrip404@gmail.com>
2024-10-03 21:00:33 +0500

    Абстракция, абстракция!

--- src/main/java/school/faang/Main.java
+++ src/main/java/school/faang/Main.java
@@ -1,4 +1,13 @@
 package school.faang;
 
 public class Main {
+    public static void main(String[] args) {
+        Character robin = new Archer("Robin");
+        Character leonid = new Warrior("Leonid");
+
+        System.out.println(robin.getHealth());
+        leonid.attack(robin);
+        System.out.println("Leonid attacked Robin");
+        System.out.println(robin.getHealth());
+    }
 }
