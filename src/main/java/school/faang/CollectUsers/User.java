09c61bab04c5e30c704c55d1f92f5715f367725c
Doniyor Kurbanov <doniyor.kurbanov.24@gmail.com>
2024-10-03 21:35:01 +0500

    Коллекционируем пользователей

---
+++ src/main/java/school/faang/CollectUsers/User.java
@@ -0,0 +1,27 @@
+package school.faang.CollectUsers;
+
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
+
+public class User {
+    private int id;
+    private String name;
+    private int userAge;
+    private Set<String> activities = new HashSet<>(); //множество активностей
+
+    private Map<User, String> findHobbyLovers(List<User> users, HashSet<String> strings) {
+        Map<User, String> map = new HashMap<>();
+        for (User user : users) {
+            Set<String> userActivities = new HashSet<>(user.activities);
+            userActivities.retainAll(strings);
+            if (!userActivities.isEmpty()) {
+                map.put(user, userActivities.iterator().next());
+            }
+        }
+        return map;
+    }
+}
