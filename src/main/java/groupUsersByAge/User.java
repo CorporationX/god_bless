6d48e558635863a0dfecf2d289d8e621f49a6abb
gimadeev <6962009@gmail.com>
2024-10-04 00:54:29 +0400

    фиксы: 1. использован putIfAbsent 2. валидатор вынесен в отдельный метод

--- src/main/java/groupUsersByAge/User.java
+++ src/main/java/groupUsersByAge/User.java
@@ -11,9 +11,7 @@ import java.util.List;
 import java.util.Map;
 
 @Getter
-@Setter
 @AllArgsConstructor
-@ToString
 public class User {
     private String name;
     private int age;
@@ -23,17 +21,16 @@ public class User {
     public static Map<Integer, List<User>> groupUsersByAge(List<User> users) {
         Map<Integer, List<User>> groupedUsers = new HashMap<>();
         for (User user : users) {
-            if (user.getAge() < 1) {
-                throw new IllegalArgumentException("Incorrect age value");
-            }
-            if (groupedUsers.containsKey(user.getAge())) {
-                groupedUsers.get(user.getAge()).add(user);
-            } else {
-                List<User> userList = new ArrayList<>();
-                userList.add(user);
-                groupedUsers.put(user.getAge(), userList);
-            }
+            validateAgeValue(user);
+            groupedUsers.putIfAbsent(user.getAge(), new ArrayList<>());
+            groupedUsers.get(user.getAge()).add(user);
         }
         return groupedUsers;
+    }
+
+    private static void validateAgeValue(User user) {
+        if (user.getAge() < 1) {
+            throw new IllegalArgumentException("Incorrect age value");
+        }
     }
 }
