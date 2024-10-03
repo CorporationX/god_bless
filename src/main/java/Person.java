29ec0912fb0950d7eaa37dc5bfada688033add28
ZhubanyshZh <zubanyszarylkasyn@gmail.com>
2024-10-03 19:01:27 +0600

    instead of create User class I create Person because User class already exits and add to Person class static method findHobbyLovers which find Users with given activity list

---
+++ src/main/java/Person.java
@@ -0,0 +1,48 @@
+import lombok.Data;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Objects;
+
+@Data
+public class Person {
+    private long id = 0;
+    private String name;
+    private int age;
+    private List<String> activities;
+
+    public Person(String name, int age, List<String> activities) {
+        ++this.id;
+        this.name = name;
+        this.age = age;
+        this.activities = activities;
+    }
+
+    public static Map<Person, String> findHobbyLovers(List<Person> personList, List<String> activities){
+        var result = new HashMap<Person, String>();
+
+        for(Person person: personList){
+            for(String activity: activities){
+                if(person.getActivities().contains(activity)){
+                    result.put(person, activity);
+                    break;
+                }
+            }
+        }
+
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Person person)) return false;
+        return getId() == person.getId() && getAge() == person.getAge() && Objects.equals(getName(), person.getName()) && Objects.equals(getActivities(), person.getActivities());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getId(), getName(), getAge(), getActivities());
+    }
+}
