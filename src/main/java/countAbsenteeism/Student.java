7cb552a6dcf32bfd1593c631ed48f958cc01f148
AlekseyB86nb <tigrxam@gmail.com>
2024-10-03 20:31:24 +0300

    add class Student.java

---
+++ src/main/java/countAbsenteeism/Student.java
@@ -0,0 +1,17 @@
+package countAbsenteeism;
+
+
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.ToString;
+
+@Getter
+@AllArgsConstructor
+@EqualsAndHashCode
+@ToString
+public class Student {
+    private String name;
+    private String faculty;
+    private int year;
+}
