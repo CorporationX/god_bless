dcd97ceb77d0cb0848a27c7480cb0f325a4267c4
AlekseyB86nb <tigrxam@gmail.com>
2024-10-03 23:59:29 +0300

    добавил класс House.java

---
+++ src/main/java/gameOfThrones/House.java
@@ -0,0 +1,13 @@
+package gameOfThrones;
+
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+@EqualsAndHashCode
+public class House {
+    private String name;
+    private String sigil;
+}
