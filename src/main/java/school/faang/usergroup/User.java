05e6e6c35521d6847127bd5d8a671b1300fbddde
gejako <gejako@ttu.ee>
2024-10-04 00:42:43 +0300

    first commit

--- src/main/java/school/faang/usergroup/User.java
+++ src/main/java/school/faang/usergroup/User.java
@@ -6,17 +6,40 @@ import lombok.Getter;
 
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
+import java.util.Set;
 
 @Getter
 @EqualsAndHashCode
-@AllArgsConstructor
+
 public class User {
+    private static final Set<String> VALID_JOBS = Set.of("Google", "Uber", "Amazon");
+    private static final Set<String> VALID_ADDRESSES = Set.of("London", "New York", "Amsterdam");
+
     private String name;
     private int age;
     private String job;
     private String address;
+
+    public User(String name, int age, String job, String address) {
+        if (name.isEmpty()){
+            System.out.println("Name cannot be empty");
+            throw new IllegalArgumentException("Name cannot be empty");
+        } else if (age < 18) {
+            throw new IllegalArgumentException("Age cannot be less than 18");
+        } else if (!VALID_JOBS.contains(job)) {
+            throw new IllegalArgumentException("Invalid job");
+        } else if (!VALID_ADDRESSES.contains(address)) {
+            throw new IllegalArgumentException("Invalid address");
+        }
+
+        this.name = name;
+        this.age = age;
+        this.job = job;
+        this.address = address;
+    }
 
 
     public static Map<Integer, List<User>> groupUsers(List<User> users) {
