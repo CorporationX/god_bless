75b5444e134ff1b8315ea369ce95ec512d26099a
Alexey Zimin <zimin.alexey@gmail.com>
2024-10-04 00:29:17 +0300

    [BJS2-31448] - reverse array

--- src/main/java/school/faang/Main.java
+++ src/main/java/school/faang/Main.java
@@ -1,30 +1,14 @@
 package school.faang;
 
-import java.util.List;
+import school.faang.reversearray.Example;
+
+import java.util.Arrays;
 
 public class Main {
 
     public static void main(String[] args) {
-
-        List<User> userList = List.of(
-                new User("Alex", 30, "Sberbank", "Moscow"),
-                new User("Sergey", 27, "Alfa Bank", "Saint Petersburg"),
-                new User("Alina", 27, "T-bank", "Moscow"),
-                new User("Alex", 25, "VTB", "Saint Petersburg"),
-                new User("Kseniya", 31, "VTB", "Samara"),
-                new User("Kate", 35, "Sberbank", "Kazan"),
-                new User("Irina", 45, "T-bank", "Yekaterinburg"),
-                new User("Mary", 45, "Sberbank", "Челябинск"),
-                new User("Pete", 27, "Alfa Bank", "Chelyabinsk")
-        );
-
-        var groups = User.groupUsers(userList);
-        var keys = groups.keySet().stream().sorted(Integer::compareTo).toList();
-        for (Integer key : keys) {
-            for (User user : groups.get(key)) {
-                System.out.println(user);
-            }
-        }
-
+        var reverser = new Example();
+        var result = reverser.reverse(new int[]{1, 2, 3, 4, 5});
+        System.out.println(Arrays.toString(result));
     }
 }
