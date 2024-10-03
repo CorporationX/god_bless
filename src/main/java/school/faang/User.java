effb741ca75b6ab33364f9184486a13493e56275
MunirIsmoilov <ismoilov.munir@gmail.com>
2024-10-03 21:31:52 +0300

    Commit Grouping users by age

--- src/main/java/school/faang/User.java
+++ src/main/java/school/faang/User.java
@@ -1,4 +1,34 @@
 package school.faang;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@Getter
+@Setter
+@AllArgsConstructor
 public class User {
-}
+        private String name;
+        private int age;
+        private String workplace;
+        private String address;
+
+        public static Map<Integer, List<User>> groupUsers(List<User> users) {
+            Map<Integer, List<User>> userMap = new HashMap<>();
+            for (User user : users) {
+                int age = user.getAge();
+                if (!userMap.containsKey(age)) {
+                    userMap.put(age, new ArrayList<>());
+
+                }
+                userMap.get(age).add(user);
+            }
+            return userMap;
+        }
+
+    }
