06c1a20be8742a78616dbe655a408f9c51db283a
AlekseyB86nb <tigrxam@gmail.com>
2024-10-03 23:00:27 +0300

    добавил метод для группировки студентов по курсам

--- src/main/java/countAbsenteeism/Main.java
+++ src/main/java/countAbsenteeism/Main.java
@@ -12,21 +12,37 @@ public class Main {
     public static void main(String[] args) {
 
         Student student1 = new Student("Petya", "IT", 2000);
-        Student student2 = new Student("Masha", "OEP", 2000);
-        Student student3 = new Student("Kolya", "OEP", 2002);
-        Student student4 = new Student("Vasya", "RL", 2002);
-        Student student5 = new Student("Irina", "IT", 2010);
+        Student student2 = new Student("Masha", "IT", 2000);
+        Student student3 = new Student("Masha", "OEP", 2000);
+        Student student4 = new Student("Kolya", "OEP", 2002);
+        Student student5 = new Student("Vasya", "RL", 2002);
+        Student student6 = new Student("Vasilisa", "RL", 2002);
+        Student student7 = new Student("Irina", "IT", 2010);
 
         addStudent(student1);
         addStudent(student2);
         addStudent(student3);
         addStudent(student4);
         addStudent(student5);
+        addStudent(student6);
+        addStudent(student7);
 
+        STUDENTS.forEach(System.out::println);
+        System.out.println();
+
+        groupStudentsByCourse(STUDENTS);
+        COURSES.forEach((k, v) -> System.out.println("course: " + k.getFaculty() + "-" + k.getYear() + " students: " + v));
     }
 
     public static void addStudent(Student student) {
         STUDENTS.add(student);
+    }
+
+    public static void groupStudentsByCourse(List<Student> students) {
+        for (Student student : students) {
+            Course course = new Course(student.getFaculty(), student.getYear());
+            COURSES.computeIfAbsent(course, k -> new ArrayList<>()).add(student);
+        }
     }
 
 }
