bc9bdb5828d111ae195eaa31f6d4a6bbb1294a35
Gadzhiev Gadzhi <Gadzhiev3222@gmail.com>
2024-10-03 06:52:26 +0300

    Group users commit

--- src/main/java/school/faang/Main.java
+++ src/main/java/school/faang/Main.java
@@ -1,4 +1,25 @@
 package school.faang;
 
+import java.util.List;
+import java.util.Set;
+
 public class Main {
+    public static void main(String[] args) {
+        List<User> users = List.of(
+                new User(1, "Gadzhi", 18, "HobbyHorsing"),
+                new User(2, "Vlad", 30, "Prog"),
+                new User(3, "Ali", 22, "Volleyball"),
+                new User(4, "Vanya", 24, "Box")
+        );
+
+        Set<String> activity = Set.of(
+                new String("game"),
+                new String("football"),
+                new String("Box"),
+                new String("HobbyHorsing")
+        );
+
+        System.out.println(User.findHobbyLovers(users, activity));
+
+    }
 }
---
+++ src/main/java/school/faang/User.java
@@ -0,0 +1,29 @@
+package school.faang;
+
+import lombok.AllArgsConstructor;
+import lombok.ToString;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
+@ToString
+@AllArgsConstructor
+public class User {
+    private int id;
+    private String name;
+    private int age;
+    private String activity;
+
+    public static Map<User, String> findHobbyLovers(List<User> users, Set<String> activity) {
+        Map<User, String> usersGroup = new HashMap<>();
+
+        for (User user : users) {
+            if (activity.contains(user.activity)) {
+                usersGroup.put(user, user.activity);
+            }
+        }
+        return usersGroup;
+    }
+}
