c1d7b3ff7a7a12155110a2fc5ffba1ef51918aef
Aliyar <aliartalybov@gmail.com>
2024-10-03 17:57:49 +0300

    Решения задачи Регистрация пользователей! BJS2-31713

--- src/main/java/school/faang/collcetUsers/Main.java
+++ src/main/java/school/faang/collcetUsers/Main.java
@@ -8,11 +8,11 @@ import java.util.Set;
 
 public class Main {
     public static void main(String[] args) {
-
         Set<String> activities1 = new HashSet<>(Arrays.asList("Бег","Ходьба"));
 
         List<String> activitiesList1 = new ArrayList<>(activities1);
-        User user1 = new User(1, "Алиса", 25, activitiesList1);
+        User user1 = new User(1, "Alisa", 25, activitiesList1, "Google", "London");
+
         System.out.println(user1);
     }
 }
--- src/main/java/school/faang/collcetUsers/User.java
+++ src/main/java/school/faang/collcetUsers/User.java
@@ -1,5 +1,6 @@
 package school.faang.collcetUsers;
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
@@ -14,13 +15,51 @@ public class User {
     private String name;
     private int age;
     private List<String> activities;
+    private String job;
+    private String address;
 
-    public User(int id, String name, int age, List<String> activities) {
+    public User(int id, String name, int age, List<String> activities, String job, String address) {
         this.id = id;
         this.name = name;
         this.age = age;
         this.activities = activities;
+        this.job = job;
+        this.address = address;
     }
+
+
+    public static final Set<String> VALID_JOBS = new HashSet<>(){ {
+        add("Google");
+        add("Uber");
+        add("Amazone");
+
+    }};
+
+    public static final Set<String> VALID_ADDRESSES = new HashSet<>(){ {
+        add("London");
+        add("New York");
+        add("Amsterdam");
+
+    }};
+
+    public User(String name, int age, String job, String address) {
+        if (name == null || name.isEmpty()) {
+            throw new IllegalArgumentException("Имя не может быть пустым.");
+        }
+        if (age < 18) {
+            throw new IllegalArgumentException("Возраст не может быть меньше 18.");
+        }
+        if (!VALID_JOBS.contains(job)) {
+            throw new IllegalArgumentException("Недопустимая профессия. Допустимые профессии: " + VALID_JOBS);
+        }
+        if (!VALID_ADDRESSES.contains(address)) {
+            throw new IllegalArgumentException("Недопустимый адрес. Допустимые адреса: " + VALID_ADDRESSES);
+        }
+
+        this.job = job;
+        this.address = address;
+    }
+
 
     public static Map<User, String> findHobbyLovers(List<User> users, Set<String> hobbies) {
         Map<User, String> hobbyLovers = new HashMap<>();
@@ -43,8 +82,8 @@ public class User {
                 ", name='" + name + '\'' +
                 ", age=" + age +
                 ", activities=" + activities +
+                ", job='" + job + '\'' +
+                ", address='" + address + '\'' +
                 '}';
     }
-
-
 }
