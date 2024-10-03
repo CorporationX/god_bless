28dda1f27723bfb028d9088ee858cf0560668709
Ilya Ishanov <ilia.detroit2016@yandex.ru>
2024-10-04 02:01:39 +0500

    Третья задача исправления

--- src/main/java/school/faang/userregistration/Users.java
+++ src/main/java/school/faang/userregistration/Users.java
@@ -6,14 +6,18 @@ import java.util.Set;
 public class Users {
     private String name;
     private Integer age;
+    private String workPlace;
+    private String address;
+    private final Integer MIN_AGE = 18;
 
-    private final Set<String> VALID_JOBS = Set.of("Google", "Uber", "Amazon");
+    private static final Set<String> VALID_JOBS = Set.of("Google", "Uber", "Amazon");
     private static final Set<String> VALID_ADDRESSES = Set.of("London", "New-York", "Amsterdam");
 
-    public Users(String name, int age) {
+    public Users(String name, int age, String workPlace, String address) {
         this.name = nameValidation(name);
         this.age = ageValidation(age);
-        setValidation();
+        this.workPlace = workPlaceValidation(workPlace);
+        this.address = addressValidation(address);
     }
 
     private String nameValidation(String name) {
@@ -25,20 +29,36 @@ public class Users {
     }
 
     private Integer ageValidation(Integer age) {
-        if (age < 18) {
+        if (age < MIN_AGE) {
             throw new IllegalArgumentException("Возраст не может быть меньше 18");
         } else {
            return this.age = age;
         }
     }
 
-    private void setValidation() {
-        if (VALID_JOBS.isEmpty()) {
-            throw new IllegalArgumentException("Set VALID_JOBS пустой");
+    private String workPlaceValidation(String workPlace) {
+        boolean logicValue = false;
+        for (String element : VALID_JOBS) {
+            if (element.contains(workPlace)) {
+                logicValue = true;
+            }
         }
+        if (!logicValue) {
+            throw new IllegalArgumentException("Этого элемента workPlace нет в сэте");
+        }
+        return workPlace;
+    }
 
-        if (VALID_ADDRESSES.isEmpty()) {
-            throw new IllegalArgumentException("Set VALID_ADDRESSES пустой");
+    private String addressValidation(String address) {
+        boolean logicValue = false;
+        for (String element : VALID_ADDRESSES) {
+            if (element.contains(address)) {
+                logicValue = true;
+            }
         }
+        if (!logicValue) {
+            throw new IllegalArgumentException("Этого элемента address нет в сэте");
+        }
+        return address;
     }
 }
