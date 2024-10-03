4d34644e36a6205e55e315726b20d95d41665c32
Aleksi Basilashvili <lucheidobra@mail.ru>
2024-10-03 21:38:57 +0300

    Развернитесь! BJS2-31444 Изменена логика

--- src/main/java/school/faang/turnaround/Example.java
+++ src/main/java/school/faang/turnaround/Example.java
@@ -1,20 +1,29 @@
 package school.faang.turnaround;
 
-import java.util.ArrayList;
-import java.util.List;
-
 public class Example {
     public static void main(String[] args) {
-        List<Integer> list = new ArrayList<>(List.of(1, 2, 3, 4, 5));
-        System.out.println(turnAround(list));
+        int[] array = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16};
+        for (int i = 0; i < array.length; i++) {
+            System.out.print(array[i] + " ");
+        }
+        turnAround(array);
+        System.out.println("");
+        for (int i = 0; i < array.length; i++) {
+            System.out.print(array[i] + " ");
+        }
 
     }
 
-    public static List<Integer> turnAround(List<Integer> numbers) {
-        List<Integer> result = new ArrayList<>();
-        for (int i = numbers.size() - 1; i >= 0; i--) {
-            result.add(numbers.get(i));
+    public static void turnAround(int[] array) {
+        int temp = 0;
+        int temp2 = array.length - 1;
+        int temp3;
+        for (int i = 0; i < array.length/2; i++) {
+            temp3 = array[temp];
+            array[temp] = array[temp2];
+            array[temp2] = temp3;
+            temp++;
+            temp2--;
         }
-        return result;
     }
 }
