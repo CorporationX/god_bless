0290bfdad4458a21780e7389273da4c56f040d6f
AlekseyB86nb <tigrxam@gmail.com>
2024-10-03 19:55:24 +0300

    add method removeBook

--- src/main/java/westerosLibrary/Main.java
+++ src/main/java/westerosLibrary/Main.java
@@ -4,20 +4,27 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class Main {
-    private static final Map<Book, String> library = new HashMap<>();
+    private static final Map<Book, String> LIBRARY = new HashMap<>();
 
     public static void main(String[] args) {
 
-        add(new Book("Мёртвые души", "Гоголь Н.В.", 1842), "0001");
-        add(new Book("Вий", "Гоголь Н.В.", 1835), "0001");
-        add(new Book("Мастер и Маргарита", "Булгаков М.А.", 1966), "0002");
-        add(new Book("Бич времён", "Головачев В.В.", 2008), "0003");
+        addBook(new Book("Мёртвые души", "Гоголь Н.В.", 1842), "0001");
+        addBook(new Book("Вий", "Гоголь Н.В.", 1835), "0001");
+        addBook(new Book("Мастер и Маргарита", "Булгаков М.А.", 1966), "0002");
+        addBook(new Book("Бич времён", "Головачев В.В.", 2008), "0003");
+        LIBRARY.forEach((book, shelf) -> System.out.println(book + " - " + shelf));
 
-        library.forEach((book, shelf) -> System.out.println(book + " - " + shelf));
+        System.out.println();
+        removeBook("Мёртвые души", "Гоголь Н.В.", 1842);
+        LIBRARY.forEach((book, shelf) -> System.out.println(book + " - " + shelf));
 
     }
 
-    public static void add(Book book, String shelf) {
-        library.put(book, shelf);
+    public static void addBook(Book book, String shelf) {
+        LIBRARY.put(book, shelf);
+    }
+
+    public static void removeBook(String title, String author, int year) {
+        LIBRARY.remove(new Book(title, author, year));
     }
 }
