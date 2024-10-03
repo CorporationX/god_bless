bc9ade8c076721b3df8e36e9db99402571494df0
AlekseyB86nb <tigrxam@gmail.com>
2024-10-03 19:26:15 +0300

    add class Book

---
+++ src/main/java/westerosLibrary/Book.java
@@ -0,0 +1,15 @@
+package westerosLibrary;
+
+
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+
+
+@EqualsAndHashCode
+@AllArgsConstructor
+public class Book {
+    private String title;
+    private String author;
+    private int year;
+
+}
