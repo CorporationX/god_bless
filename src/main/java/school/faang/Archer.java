b8f44aca402b030f2202c9483296c0aa4cbc775d
AlekseyB86nb <tigrxam@gmail.com>
2024-10-03 13:57:19 +0300

    изменения в файле Archer.java

--- src/main/java/school/faang/Archer.java
+++ src/main/java/school/faang/Archer.java
@@ -1,4 +1,17 @@
 package school.faang;
 
-public class Archer {
+public class Archer extends Character{
+
+    public Archer(String name) {
+        super(name, 3, 10, 5);
+    }
+
+    public Archer(String name, int power, int dexterity, int intelligence) {
+        super(name, power, dexterity, intelligence);
+    }
+
+    @Override
+    public void attack(Character character) {
+        character.setHealth(character.getHealth() - this.getDexterity());
+    }
 }
