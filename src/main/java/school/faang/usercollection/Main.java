21a6ed419aae8f74e6ccdac0222ccd33133c08f4
Aleksi Basilashvili <lucheidobra@mail.ru>
2024-10-03 22:07:15 +0300

    Коллекционируем пользователей BJS2-31526 Надеюсь правильно понял замечания, и внес правильные изменения.

--- src/main/java/school/faang/usercollection/Main.java
+++ src/main/java/school/faang/usercollection/Main.java
@@ -1,6 +1,5 @@
 package school.faang.usercollection;
 
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
@@ -8,19 +7,20 @@ import java.util.Set;
 public class Main {
     private static final Set<String> HOBBIES = Set.of("Programming", "Skiing",
             "Gaming", "Drifting", "Hiking", "Watching Movies");
-    public static void main(String[] args) {
-        List<User> users = new ArrayList<>();
 
-        users.add(new User(1, "Alex", 25,
-                Set.of("Programming", "Gaming", "Watching Movies")));
-        users.add(new User(2, "Vlad", 25,
-                Set.of("Skiing", "Gaming", "Watching Movies")));
-        users.add(new User(3, "Kirill", 25,
-                Set.of("Drifting", "Gaming", "Watching Movies")));
-        users.add(new User(4, "Mifodiy", 25,
-                Set.of("Gaming", "Hiking", "Watching Movies")));
-        users.add(new User(5, "Sauron", 25,
-                Set.of("Creating Rings", "Manipulating", "Giant eyes")));
+    public static void main(String[] args) {
+        List<User> users = List.of(
+                new User(1, "Alex", 25,
+                        Set.of("Programming", "Gaming", "Watching Movies")),
+                new User(2, "Vlad", 25,
+                        Set.of("Skiing", "Gaming", "Watching Movies")),
+                new User(3, "Kirill", 25,
+                        Set.of("Drifting", "Gaming", "Watching Movies")),
+                new User(4, "Mifodiy", 25,
+                        Set.of("Gaming", "Hiking", "Watching Movies")),
+                new User(5, "Sauron", 25,
+                        Set.of("Creating Rings", "Manipulating", "Giant eyes"))
+        );
 
         Map<User, String> hobbyLovers = User.findHobbyLovers(users, HOBBIES);
 
--- src/main/java/school/faang/usercollection/User.java
+++ src/main/java/school/faang/usercollection/User.java
@@ -3,9 +3,7 @@ package school.faang.usercollection;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 
-import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
@@ -20,16 +18,14 @@ public class User {
     private int age;
     private Set<String> hobby;
 
-
-    public static Map<User, String> findHobbyLovers(List<User> users,
-                                              Set<String> hobbies) {
+    public static Map<User, String> findHobbyLovers(List<User> users, Set<String> hobbies) {
         Map<User, String> hobbyLovers = new HashMap<>();
         for (User user : users) {
-            Set<String> commonHobbies = new HashSet<>(user.getHobby());
-            commonHobbies.retainAll(hobbies);
-            if (!commonHobbies.isEmpty()) {
-                String firstCommonHobby = commonHobbies.iterator().next();
-                hobbyLovers.put(user, firstCommonHobby);
+            for (String hobby : user.getHobby()){
+                if (hobbies.contains(hobby)){
+                    hobbyLovers.put(user, hobby);
+                    break;
+                }
             }
         }
         return hobbyLovers;
