2965bd020a3730761c3c9ae81d6f1b57c095d248
Andrew Balashov <balashov83@gmail.com>
2024-10-03 17:58:37 +0300

    BJS2-32790 Web Search Engine task is done

---
+++ src/main/java/school/faang/bjs2_32790/Main.java
@@ -0,0 +1,51 @@
+package school.faang.bjs2_32790;
+
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+
+public class Main {
+    static final Map<String, Set<WebPage>> wordIndex = new HashMap<>();
+    static final Map<String, WebPage> urlIndex = new HashMap<>();
+
+    public static void main(String[] args) {
+
+        indexWebPage(new WebPage("yandex.ru", "News", "Hello world! мир! :)"));
+        indexWebPage(new WebPage("google.com", "Recent", "Привет, Мир!"));
+        indexWebPage(new WebPage("faang-school.net", "Java Bootcamp", "Hello Mentor, spare me please!"));
+        indexWebPage(new WebPage("school.ru", "Stupid camp", "Hello I love world!"));
+        indexWebPage(new WebPage("youtube.com", "Puppies video", "Hello cuties"));
+
+        System.out.println(wordIndex);
+
+        System.out.println(searchByIndex("HELLO"));
+
+        deleteByUrl("faang-school.net");
+        System.out.println(wordIndex);
+
+    }
+
+    static void indexWebPage(WebPage webPage) {
+        String url = webPage.getUrl();
+        urlIndex.put(url, webPage);
+
+        String content = webPage.getContent();
+        String[] words = content.toLowerCase().split("[(\s,.\"!?:';)]+");
+        for (String word : words) {
+            wordIndex.computeIfAbsent(word, key -> new HashSet<>()).add(webPage);
+        }
+    }
+
+    static Set<WebPage> searchByIndex(String word) {
+        return wordIndex.get(word.toLowerCase());
+    }
+
+    static void deleteByUrl(String url) {
+        WebPage targetPage = urlIndex.get(url);
+        for (var entry : wordIndex.entrySet()) {
+            Set<WebPage> webPageSet = entry.getValue();
+            webPageSet.remove(targetPage);
+        }
+    }
+}
