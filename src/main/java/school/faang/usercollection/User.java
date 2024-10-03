3d7aa0d80495ab8223c4e8ba0cfd790fc6743a1c
Olga Karaskova <okalughina@gmail.com>
2024-10-03 22:24:22 +0200

    Fixes for review comments

--- src/main/java/school/faang/usercollection/User.java
+++ src/main/java/school/faang/usercollection/User.java
@@ -1,12 +1,13 @@
 package school.faang.usercollection;
 
 import lombok.EqualsAndHashCode;
+import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 
-import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Set;
 
 @RequiredArgsConstructor
 @EqualsAndHashCode
@@ -14,16 +15,16 @@ public class User {
     private final long id;
     private final String name;
     private final int age;
-    private final List<String> activities;
+    @Getter
+    private final Set<String> activities;
 
-    public static Map<User, String> findHobbyLovers(List<User> users, List<String> activities) {
+    public static Map<User, String> findHobbyLovers(List<User> users, Set<String> hobbies) {
         HashMap<User, String> hobbyLovers = new HashMap<>();
         for (User user : users) {
-            List<String> commonHobbies = new ArrayList<>(user.activities);
-            commonHobbies.retainAll(activities);
-            if (!commonHobbies.isEmpty()) {
-                hobbyLovers.put(user, commonHobbies.get(0));
-            }
+            for (String activity : user.getActivities())
+                if (hobbies.contains(activity)) {
+                    hobbyLovers.put(user, activity);
+                }
         }
         return hobbyLovers;
     }
--- src/test/java/school/faang/usercollection/UserCollectionTest.java
+++ src/test/java/school/faang/usercollection/UserCollectionTest.java
@@ -4,8 +4,10 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
+import java.util.Set;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static school.faang.usercollection.User.findHobbyLovers;
@@ -16,47 +18,47 @@ public class UserCollectionTest {
     @DisplayName("Each user has 1 matching activity")
     public void testUserSingleHobbyMatch() {
         List<User> users = List.of(
-                new User(1, "User1", 25, new ArrayList<>(List.of("Running", "Swimming", "Cycling", "Hiking", "Yoga"))),
-                new User(2, "User2", 15, new ArrayList<>(List.of("Reading", "Writing", "Painting", "Drawing", "Sculpting"))),
-                new User(3, "User3", 35, new ArrayList<>(List.of("Cooking", "Reading", "Grilling", "Roasting", "Frying")))
+                new User(1, "User1", 25, new HashSet<>(Set.of("Running", "Swimming", "Cycling", "Hiking", "Yoga"))),
+                new User(2, "User2", 15, new HashSet<>(Set.of("Reading", "Writing", "Painting", "Drawing", "Sculpting"))),
+                new User(3, "User3", 35, new HashSet<>(Set.of("Cooking", "Reading", "Grilling", "Roasting", "Frying")))
         );
 
-        List<String> hobbies = new ArrayList<>(List.of("Writing", "Running", "Roasting"));
+        Set<String> hobbies = new HashSet<>(Set.of("Writing", "Running", "Roasting"));
 
         Map<User, String> hobbyLovers = findHobbyLovers(users, hobbies);
         assertEquals(3, hobbyLovers.size());
-        assertEquals("Running", hobbyLovers.get(new User(1, "User1", 25, new ArrayList<>(List.of("Running", "Swimming", "Cycling", "Hiking", "Yoga")))));
-        assertEquals("Writing", hobbyLovers.get(new User(2, "User2", 15, new ArrayList<>(List.of("Reading", "Writing", "Painting", "Drawing", "Sculpting")))));
-        assertEquals("Roasting", hobbyLovers.get(new User(3, "User3", 35, new ArrayList<>(List.of("Cooking", "Reading", "Grilling", "Roasting", "Frying")))));
+        assertEquals("Running", hobbyLovers.get(new User(1, "User1", 25, new HashSet<>(Set.of("Running", "Swimming", "Cycling", "Hiking", "Yoga")))));
+        assertEquals("Writing", hobbyLovers.get(new User(2, "User2", 15, new HashSet<>(Set.of("Reading", "Writing", "Painting", "Drawing", "Sculpting")))));
+        assertEquals("Roasting", hobbyLovers.get(new User(3, "User3", 35, new HashSet<>(Set.of("Cooking", "Reading", "Grilling", "Roasting", "Frying")))));
     }
 
     @Test
     @DisplayName("Each user has several matching activity")
     public void testUserMultipleHobbyMatch() {
         List<User> users = List.of(
-                new User(1, "User1", 25, new ArrayList<>(List.of("Running", "Swimming", "Cycling", "Hiking", "Yoga"))),
-                new User(2, "User2", 15, new ArrayList<>(List.of("Reading", "Writing", "Painting", "Drawing", "Sculpting"))),
-                new User(3, "User3", 35, new ArrayList<>(List.of("Cooking", "Reading", "Grilling", "Roasting", "Frying")))
+                new User(1, "User1", 25, new HashSet<>(Set.of("Running", "Swimming", "Cycling", "Hiking", "Yoga"))),
+                new User(2, "User2", 15, new HashSet<>(Set.of("Reading", "Writing", "Painting", "Drawing", "Sculpting"))),
+                new User(3, "User3", 35, new HashSet<>(Set.of("Cooking", "Reading", "Grilling", "Roasting", "Frying")))
         );
 
-        List<String> hobbies = new ArrayList<>(List.of("Drawing", "Running", "Writing", "Swimming", "Cycling", "Hiking"));
+        Set<String> hobbies = new HashSet<>(Set.of("Drawing", "Running", "Writing", "Swimming", "Cycling", "Hiking"));
 
         Map<User, String> hobbyLovers = findHobbyLovers(users, hobbies);
         assertEquals(2, hobbyLovers.size());
-        assertEquals("Running", hobbyLovers.get(new User(1, "User1", 25, new ArrayList<>(List.of("Running", "Swimming", "Cycling", "Hiking", "Yoga")))));
-        assertEquals("Writing", hobbyLovers.get(new User(2, "User2", 15, new ArrayList<>(List.of("Reading", "Writing", "Painting", "Drawing", "Sculpting")))));
+        assertEquals("Hiking", hobbyLovers.get(new User(1, "User1", 25, new HashSet<>(Set.of("Running", "Swimming", "Cycling", "Hiking", "Yoga")))));
+        assertEquals("Writing", hobbyLovers.get(new User(2, "User2", 15, new HashSet<>(Set.of("Reading", "Writing", "Painting", "Drawing", "Sculpting")))));
     }
 
     @Test
     @DisplayName("Users have no matching activity")
     public void testUserNoHobbyMatch() {
         List<User> users = List.of(
-                new User(1, "User1", 25, new ArrayList<>(List.of("Running", "Swimming", "Cycling", "Hiking", "Yoga"))),
-                new User(2, "User2", 15, new ArrayList<>(List.of("Reading", "Writing", "Painting", "Drawing", "Sculpting"))),
-                new User(3, "User3", 35, new ArrayList<>(List.of("Cooking", "Reading", "Grilling", "Roasting", "Frying")))
+                new User(1, "User1", 25, new HashSet<>(Set.of("Running", "Swimming", "Cycling", "Hiking", "Yoga"))),
+                new User(2, "User2", 15, new HashSet<>(Set.of("Reading", "Writing", "Painting", "Drawing", "Sculpting"))),
+                new User(3, "User3", 35, new HashSet<>(Set.of("Cooking", "Reading", "Grilling", "Roasting", "Frying")))
         );
 
-        List<String> hobbies = new ArrayList<>(List.of("Exercising", "Weightlifting", "Cardio", "Stretching", "Pilates"));
+        Set<String> hobbies = new HashSet<>(Set.of("Exercising", "Weightlifting", "Cardio", "Stretching", "Pilates"));
 
         Map<User, String> hobbyLovers = findHobbyLovers(users, hobbies);
         assertEquals(0, hobbyLovers.size());
@@ -66,12 +68,12 @@ public class UserCollectionTest {
     @DisplayName("Empty Hobby list provided")
     public void testUserEmptyHobbyList() {
         List<User> users = List.of(
-                new User(1, "User1", 25, new ArrayList<>(List.of("Running", "Swimming", "Cycling", "Hiking", "Yoga"))),
-                new User(2, "User2", 15, new ArrayList<>(List.of("Reading", "Writing", "Painting", "Drawing", "Sculpting"))),
-                new User(3, "User3", 35, new ArrayList<>(List.of("Cooking", "Reading", "Grilling", "Roasting", "Frying")))
+                new User(1, "User1", 25, new HashSet<>(Set.of("Running", "Swimming", "Cycling", "Hiking", "Yoga"))),
+                new User(2, "User2", 15, new HashSet<>(Set.of("Reading", "Writing", "Painting", "Drawing", "Sculpting"))),
+                new User(3, "User3", 35, new HashSet<>(Set.of("Cooking", "Reading", "Grilling", "Roasting", "Frying")))
         );
 
-        List<String> hobbies = new ArrayList<>();
+        Set<String> hobbies = new HashSet<>();
 
         Map<User, String> hobbyLovers = findHobbyLovers(users, hobbies);
         assertEquals(0, hobbyLovers.size());
@@ -81,26 +83,26 @@ public class UserCollectionTest {
     @DisplayName("Users have empty Activity list")
     public void testUserEmptyUserActivityLIst() {
         List<User> users = List.of(
-                new User(1, "User1", 25, new ArrayList<>()),
-                new User(2, "User2", 15, new ArrayList<>(List.of("Reading", "Writing", "Painting", "Drawing", "Sculpting"))),
-                new User(3, "User3", 35, new ArrayList<>(List.of("Cooking", "Reading", "Grilling", "Roasting", "Frying")))
+                new User(1, "User1", 25, new HashSet<>()),
+                new User(2, "User2", 15, new HashSet<>(Set.of("Reading", "Writing", "Painting", "Drawing", "Sculpting"))),
+                new User(3, "User3", 35, new HashSet<>(Set.of("Cooking", "Reading", "Grilling", "Roasting", "Frying")))
         );
 
-        List<String> hobbies = new ArrayList<>(List.of("Drawing", "Running", "Testing"));
+        Set<String> hobbies = new HashSet<>(Set.of("Drawing", "Running", "Testing"));
 
         Map<User, String> hobbyLovers = findHobbyLovers(users, hobbies);
         assertEquals(1, hobbyLovers.size());
-        assertEquals("Drawing", hobbyLovers.get(new User(2, "User2", 15, new ArrayList<>(List.of("Reading", "Writing", "Painting", "Drawing", "Sculpting")))));
+        assertEquals("Drawing", hobbyLovers.get(new User(2, "User2", 15, new HashSet<>(Set.of("Reading", "Writing", "Painting", "Drawing", "Sculpting")))));
     }
 
     @Test
     @DisplayName("Empty Activity list and Hobby list")
     public void testUserEmptyLists() {
         List<User> users = List.of(
-                new User(1, "User1", 25, new ArrayList<>())
+                new User(1, "User1", 25, new HashSet<>())
         );
 
-        List<String> hobbies = new ArrayList<>();
+        Set<String> hobbies = new HashSet<>();
 
         Map<User, String> hobbyLovers = findHobbyLovers(users, hobbies);
         assertEquals(0, hobbyLovers.size());
@@ -111,7 +113,7 @@ public class UserCollectionTest {
     public void testUserEmptyUserList() {
         List<User> users = new ArrayList<>();
 
-        List<String> hobbies = new ArrayList<>(List.of("Drawing", "Running", "Testing"));
+        Set<String> hobbies = new HashSet<>(Set.of("Drawing", "Running", "Testing"));
 
         Map<User, String> hobbyLovers = findHobbyLovers(users, hobbies);
         assertEquals(0, hobbyLovers.size());
