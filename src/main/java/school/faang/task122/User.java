ad8faa5b643b8e1f5cf8c2be1a4150ae23b15070
Svyatoslav Kostrov <kost-2014@mail.ru>
2024-10-03 12:32:26 +0300

    Solution for second task (with changed constructor)

--- src/main/java/school/faang/task122/User.java
+++ src/main/java/school/faang/task122/User.java
@@ -16,22 +16,26 @@ public class User {
 
     public User(String name, int age, String workPlace, String address) {
 
-        if(name!=null){
-            if(age>=legalAge) {
-                if (VALID_JOBS.contains(workPlace)) {
-                    if (VALID_ADDRESSES.contains(address)) {
-                        this.name = name;
-                        this.age = age;
-                        this.workPlace = workPlace;
-                        this.address = address;
-                    }
-                    else throw new IllegalArgumentException("Не валидный адрес работы ");
-                }
-                else throw new IllegalArgumentException("Не валидное место работы");
-            }
-            else throw new IllegalArgumentException("Возраст меньше 18");
+        if (name == null) {
+            throw new IllegalArgumentException("Пустое имя");
         }
-        else throw new IllegalArgumentException("Пустое имя");
+
+        if (age < legalAge) {
+            throw new IllegalArgumentException("Возраст меньше 18");
+        }
+
+        if (!VALID_JOBS.contains(workPlace)) {
+            throw new IllegalArgumentException("Не валидное место работы");
+        }
+
+        if (!VALID_ADDRESSES.contains(address)) {
+            throw new IllegalArgumentException("Не валидный адрес работы");
+        }
+
+        this.name = name;
+        this.age = age;
+        this.workPlace = workPlace;
+        this.address = address;
     }
 
     public int getAge() {
