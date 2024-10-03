79c59809a312056605ae2bae87310a0c3279934e
Andrew Balashov <balashov83@gmail.com>
2024-10-03 18:13:15 +0300

    BJS2-32454 Abscence Count second commit after review: access modificators fixed, comments deleted

--- src/main/java/school/faang/bsj2_32454_abscenceCount/Main.java
+++ src/main/java/school/faang/bsj2_32454_abscenceCount/Main.java
@@ -9,7 +9,7 @@ import java.util.List;
 import java.util.Map;
 
 public class Main {
-    public static List<Student> studentList = new ArrayList<>();
+    private static List<Student> studentList = new ArrayList<>();
 
     public static void main(String[] args) {
         studentList.addAll(List.of(
@@ -22,18 +22,14 @@ public class Main {
         System.out.println(studentList);
         System.out.println(sortByFacultyYear(studentList));
 
-        //Adding ned student with addStudent method
         addStudent(new Student("Skywalker", "Dark Force", 15));
         System.out.println(sortByFacultyYear(studentList));
 
-        //Removing student with deleteStudent method
         deleteStudent("Skywalker", "Dark Force", 15);
         System.out.println(sortByFacultyYear(studentList));
 
-        //Searching faculties with searchStudents method
         System.out.println(searchStudents("Management", 3));
 
-        //Showing all students with displayStudents method
         displayStudents();
     }
 
@@ -44,7 +40,7 @@ public class Main {
         private int year;
     }
 
-    public static Map<FacultyYearKey, List<Student>> sortByFacultyYear(List<Student> studentList) {
+    private static Map<FacultyYearKey, List<Student>> sortByFacultyYear(List<Student> studentList) {
         Map<FacultyYearKey, List<Student>> groupedMap = new HashMap<>();
 
         for (Student student : studentList) {
@@ -57,19 +53,19 @@ public class Main {
         return groupedMap;
     }
 
-    static void addStudent(Student student) {
+    private static void addStudent(Student student) {
         studentList.add(student);
     }
 
-    static void deleteStudent(String name, String faculty, int year) {
+    private static void deleteStudent(String name, String faculty, int year) {
         studentList.remove(new Student(name, faculty, year));
     }
 
-    static List<Student> searchStudents(String faculty, int year) {
+    private static List<Student> searchStudents(String faculty, int year) {
         return sortByFacultyYear(studentList).get(new FacultyYearKey(faculty, year));
     }
 
-    static void displayStudents() {
+    private static void displayStudents() {
         for (Map.Entry<FacultyYearKey, List<Student>> entry : sortByFacultyYear(studentList).entrySet()) {
             String faculty = entry.getKey().getFaculty();
             int year = entry.getKey().getYear();
