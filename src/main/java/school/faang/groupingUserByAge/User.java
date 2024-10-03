f61a78dfb3296c44819b49f662b8954e35e4483e
Sergey Zhitenev <zhitenev@lanit.ru>
2024-10-03 12:27:36 +0300

    user registration task

--- src/main/java/school/faang/groupingUserByAge/User.java
+++ src/main/java/school/faang/groupingUserByAge/User.java
@@ -3,10 +3,7 @@ package school.faang.groupingUserByAge;
 import lombok.Builder;
 import lombok.Data;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 @Data
 @Builder
@@ -15,6 +12,19 @@ public class User {
     private int age;
     private String workplace;
     private String address;
+    private final static Set<String> VALID_JOBS = Set.of("Google", "Uber", "Amazon");
+    private final static Set<String> VALID_ADDRESSES = Set.of("London", "New York", "Amsterdam");
+
+    public User(String name, int age, String workplace, String address) {
+        if (!name.isEmpty() && age >= 18 && VALID_JOBS.contains(workplace) && VALID_ADDRESSES.contains(address)) {
+            this.name = name;
+            this.age = age;
+            this.workplace = workplace;
+            this.address = address;
+        } else {
+            throw new IllegalArgumentException("Invalid user");
+        }
+    }
 
     public static Map<Integer, List<User>> groupUsers(List<User> userList) {
         Map<Integer, List<User>> userGroupMap = new HashMap<>();
