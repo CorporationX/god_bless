ea89aae8a26bd541eca1468b0dce5fe48a70d815
Maks Russkikh <15maksim@mail.ru>
2024-10-03 22:27:22 +0300

    clear Main

--- src/main/java/school/faang/Main.java
+++ src/main/java/school/faang/Main.java
@@ -1,20 +1,5 @@
 package school.faang;
 
 public class Main {
-    public static void main(String[] args) {
-        try {
-            Character warrior = new Warrior("Cyklop");
-            Character archer = new Archer("Taracan");
-            for (int i = 0; i < 11; i++) {
-                warrior.attack(archer);
-                archer.attack(warrior);
-            }
 
-            System.out.println("Здоровье Warrior равно: " + warrior.getHitPoints());
-            System.out.println("Здоровье Archer равно: " + archer.getHitPoints());
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-
-    }
 }
