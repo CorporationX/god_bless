cce2799a467c23941bc7421c2579cb9d1a1ab8c8
Alena Shmelkova <AlenaShmelkova@gmail.com>
2024-10-03 13:19:53 -0400

    git commit -m "BJS2-31484 <changed add() to Set.of>"

--- src/main/java/school/faang/user_hobby_collection/Main.java
+++ src/main/java/school/faang/user_hobby_collection/Main.java
@@ -1,7 +1,6 @@
 package school.faang.user_hobby_collection;
 
 import java.util.ArrayList;
-import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 
@@ -9,24 +8,24 @@ import static school.faang.user_hobby_collection.User.findHobbyLovers;
 
 public class Main {
     public static void main(String[] args) {
-        Set<String> cheburaskaHobbies = new HashSet<>();
-        cheburaskaHobbies.add("Running");
-        cheburaskaHobbies.add("Swimming");
-        cheburaskaHobbies.add("Play the garmoshka");
-        cheburaskaHobbies.add("Crocodile hunting");
+        Set<String> cheburaskaHobbies = Set.of(
+                "Running",
+                "Swimming",
+                "Play the garmoshka",
+                "Crocodile hunting");
 
-        Set<String> garfieldHobbies = new HashSet<>();
-        garfieldHobbies.add("Eating");
-        garfieldHobbies.add("Watching the TV");
+        Set<String> garfieldHobbies = Set.of(
+                "Eating",
+                "Watching the TV");
 
         User cheburaska = new User(1, "Cheburashka", 57, cheburaskaHobbies);
         User garfield = new User(2, "Garfield", 46, garfieldHobbies);
 
-        Set<String> hobbies = new HashSet<>();
-        hobbies.add("Cooking");
-        hobbies.add("Play the garmoshka");
-        hobbies.add("Eating");
-        hobbies.add("Crocodile hunting");
+        Set<String> hobbies = Set.of(
+                "Cooking",
+                "Play the garmoshka",
+                "Eating",
+                "Crocodile hunting");
 
         List<User> users = new ArrayList<>();
         users.add(cheburaska);
