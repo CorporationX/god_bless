7b3c4d67cda87e6f112a42b7d40a48ce142bc439
Svyatoslav Kostrov <kost-2014@mail.ru>
2024-10-03 13:26:46 +0300

    Solution for fourth task

---
+++ src/main/java/school/faang/task124/User.java
@@ -0,0 +1,36 @@
+package school.faang.task124;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.*;
+import java.util.stream.Stream;
+
+@AllArgsConstructor
+public class User {
+
+    private int id;
+    @Getter
+    private String name;
+
+    private int age ;
+    @Getter
+    private Set<String> activity;
+
+    public static Map<User,String> findHobbyLovers (List<User> users,Set<String> activity){
+        Map<User,String> answer = new HashMap<>();
+        outer:
+        for(User user : users){
+            for(String userActivity : user.getActivity()){
+                if(activity.contains(userActivity)){
+                    answer.put(user,userActivity);
+                    continue outer;
+                }
+            }
+        }
+        return answer;
+    }
+
+
+}
