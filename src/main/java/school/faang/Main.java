437ebededa5e3a3e9b98a7e6dbd92e30d2cac92f
Slava <leeteso97@gmail.com>
2024-10-03 22:49:12 +0500

    Внес тестовые данные для проверки метода

--- src/main/java/school/faang/Main.java
+++ src/main/java/school/faang/Main.java
@@ -1,24 +1,17 @@
 package school.faang;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Map;
-
-import static school.faang.User.groupUsers;
+import school.faang.Abstract.Archer;
+import school.faang.Abstract.Character;
+import school.faang.Abstract.Warrior;
 
 public class Main {
     public static void main(String[] args) {
-        List<User> users = new ArrayList<>();
-        users.add(new User("Аслан", "Пушкина 51", "Менеджер", 35));
-        users.add(new User("Арман", "Абылайхана 1", "Маркетолог", 28));
-        users.add(new User("Станислав", "Шевченко 11", "Разработчик", 23));
-        users.add(new User("Таисия", "Абая 103", "Бухгалтер", 29));
-        users.add(new User("Батырхан", "село Абай", "Курьер", 23));
+        Character arthas = new Warrior("Arthas");
+        Character silvana = new Archer("Silvana");
 
-        for (Map.Entry<Integer, List<User>> result : groupUsers(users).entrySet()) {
-            if (result.getKey() == 29) {
-                System.out.println(result.getValue());
-            }
-        }
+        arthas.attack(silvana);
+        System.out.println(silvana.getHealth());
+        silvana.attack(arthas);
+        System.out.println(arthas.getHealth());
     }
 }
