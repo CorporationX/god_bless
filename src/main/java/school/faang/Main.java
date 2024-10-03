929e91fb54d7750777987a86e196189749fe61ff
Gadzhiev Gadzhi <Gadzhiev3222@gmail.com>
2024-10-03 11:45:38 +0300

    .

--- src/main/java/school/faang/Main.java
+++ src/main/java/school/faang/Main.java
@@ -1,5 +1,6 @@
 package school.faang;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -8,9 +9,48 @@ public class Main {
     private static Map<Integer, StreamEvent> streamEvents = new HashMap<>();
     private static Map<String, List<StreamEvent>> groupStreamEvent = new HashMap<>();
 
-    public void addEvent(StreamEvent event) {
-        streamEvents.put(event.getId(), event);
+    public static void main(String[] args) {
+        StreamEvent firstEvent = new StreamEvent(1, "sport", "train");
+        StreamEvent secondEvent = new StreamEvent(2, "study", "learn");
+        StreamEvent thirdEvent = new StreamEvent(3, "chill", "chilling");
 
-        if ()
+        addEvent(firstEvent);
+        addEvent(secondEvent);
+        addEvent(thirdEvent);
+
+        deleteeEvent(secondEvent);
+
+        System.out.println(getEventById(1));
+        System.out.println(getEventsByEventType("chill"));
+
+        printAllEvents();
+    }
+
+    public static void addEvent(StreamEvent event) {
+        streamEvents.put(event.getId(), event);
+        groupStreamEvent.computeIfAbsent(event.getEventType(), k -> new ArrayList<>()).add(event);
+    }
+
+    public static StreamEvent getEventById(int id) {
+        return streamEvents.get(id);
+    }
+
+    public static List<StreamEvent> getEventsByEventType(String eventType) {
+        return groupStreamEvent.get(eventType);
+    }
+
+    public static void deleteeEvent(StreamEvent event) {
+        streamEvents.remove(event.getId());
+        if (groupStreamEvent.containsKey(event.getEventType())) {
+            groupStreamEvent.remove(event.getEventType()).remove(event);
+        }
+    }
+
+    public static void printAllEvents() {
+        for(Map.Entry<Integer, StreamEvent> entry : streamEvents.entrySet()) {
+            StreamEvent event = entry.getValue();
+            System.out.println("Тип события: " + event.getEventType() +
+                    " id события: " + event.getId() + " данные события: " + event.getData());
+        }
     }
 }
--- src/main/java/school/faang/StreamEvent.java
+++ src/main/java/school/faang/StreamEvent.java
@@ -2,9 +2,11 @@ package school.faang;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.ToString;
 
 @AllArgsConstructor
 @Getter
+@ToString
 public class StreamEvent {
     private int id;
     private String eventType;
