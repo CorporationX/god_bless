626cf4b5fca0a3db8ce2bffff73cb78101e95b60
Alexander Litovkin <litovkinalexan@gmail.com>
2024-10-03 15:53:26 +0700

    add methods for library interaction

--- src/main/java/school/faang/Main.java
+++ src/main/java/school/faang/Main.java
@@ -1,28 +1,47 @@
 package school.faang;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class Main {
 
+    private static final Map<Book, String> BOOK_TO_SHELF = new HashMap<>();
+
+    static {
+        BOOK_TO_SHELF.put(new Book("Book1", "Alex", 2000), "shelf_1");
+        BOOK_TO_SHELF.put(new Book("Book2", "Ivan", 1900), "shelf_2");
+    }
+
     public static void main(String[] args) {
-        User user = new User("Alex", 21, "Google", "New York");
-        try {
-            user = new User("", 20, "Google", "New York");
-        } catch (IllegalArgumentException e) {
-            System.out.println(e.getMessage());
-        }
-        try {
-            user = new User("Alex", 10, "Google", "New York");
-        } catch (IllegalArgumentException e) {
-            System.out.println(e.getMessage());
-        }
-        try {
-            user = new User("Alex", 20, "", "New York");
-        } catch (IllegalArgumentException e) {
-            System.out.println(e.getMessage());
-        }
-        try {
-            user = new User("Alex", 20, "Google", "");
-        } catch (IllegalArgumentException e) {
-            System.out.println(e.getMessage());
+        String title = "Test Book";
+        String author = "The Best Author";
+        int year = 1000;
+        String shelf = "shelf_2";
+
+        addBookToShelf(new Book(title, author, year), shelf);
+        String foundShelf = findBookShelf(title, author, year);
+        System.out.printf("%s shelf: %s%n", title, foundShelf);
+        removeBook(title, author, year);
+        printLibraryContents();
+    }
+
+    public static void addBookToShelf(Book book, String shelf) {
+        BOOK_TO_SHELF.put(book, shelf);
+    }
+
+    public static void removeBook(String title, String author, int year) {
+        Book book = new Book(title, author, year);
+        BOOK_TO_SHELF.remove(book);
+    }
+
+    public static String findBookShelf(String title, String author, int year) {
+        Book book = new Book(title, author, year);
+        return BOOK_TO_SHELF.get(book);
+    }
+
+    public static void printLibraryContents() {
+        for (Map.Entry<Book, String> entry : BOOK_TO_SHELF.entrySet()) {
+            System.out.println(entry.getKey() + " " + entry.getValue());
         }
     }
 }
