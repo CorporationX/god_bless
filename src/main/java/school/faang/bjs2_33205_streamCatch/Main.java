e560fc3190ed2e6f09129774485fca070674af5f
Andrew Balashov <balashov83@gmail.com>
2024-10-03 18:27:42 +0300

    BJS2-33205 StreamCatch second commit: access modificators fixed, comments deleted

--- src/main/java/school/faang/bjs2_33205_streamCatch/Main.java
+++ src/main/java/school/faang/bjs2_33205_streamCatch/Main.java
@@ -6,8 +6,8 @@ import java.util.List;
 import java.util.Map;
 
 public class Main {
-    static Map<Integer, StreamEvent> idEventMap = new HashMap<>();
-    static Map<String, List<StreamEvent>> typeGroupMap = new HashMap<>();
+    private static Map<Integer, StreamEvent> idEventMap = new HashMap<>();
+    private static Map<String, List<StreamEvent>> typeGroupMap = new HashMap<>();
 
     public static void main(String[] args) {
         addEvent(new StreamEvent(1, "Message", "Message sent"));
@@ -19,34 +19,30 @@ public class Main {
         displayEvents();
         System.out.println(typeGroupMap);
 
-        //Search event by id
         System.out.println(searchEventById(3));
 
-        //Search events ty type
         System.out.println(searchEventsByType("Message"));
 
-        //Deleting events
         removeEvent(5);
         System.out.println(idEventMap);
         System.out.println(typeGroupMap);
         displayEvents();
-
     }
 
-    static void addEvent(StreamEvent streamEvent) {
+    private static void addEvent(StreamEvent streamEvent) {
         idEventMap.put(streamEvent.getId(), streamEvent);
         typeGroupMap.computeIfAbsent(streamEvent.getEventType(), k -> new ArrayList<>()).add(streamEvent);
     }
 
-    static StreamEvent searchEventById(int id) {
+    private static StreamEvent searchEventById(int id) {
         return idEventMap.get(id);
     }
 
-    static List<StreamEvent> searchEventsByType(String type) {
+    private static List<StreamEvent> searchEventsByType(String type) {
         return typeGroupMap.get(type);
     }
 
-    static void removeEvent(int id) {
+    private static void removeEvent(int id) {
         if (idEventMap.containsKey(id)) {
             StreamEvent event = idEventMap.remove(id);
             typeGroupMap.remove(event.getEventType());
@@ -55,7 +51,7 @@ public class Main {
         }
     }
 
-    static void displayEvents() {
+    private static void displayEvents() {
         for (Map.Entry<Integer, StreamEvent> entry : idEventMap.entrySet()) {
             int id = entry.getKey();
             String type = entry.getValue().getEventType();
