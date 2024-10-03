8d62f97c48fcac8a072c636dfc1408a749cd7aec
Gadzhiev Gadzhi <Gadzhiev3222@gmail.com>
2024-10-03 06:18:12 +0300

    Group users commit

--- src/main/java/school/faang/Main.java
+++ src/main/java/school/faang/Main.java
@@ -1,5 +1,11 @@
 package school.faang;
 
-public class Main {
+import java.util.Arrays;
 
+public class Main {
+    public static void main(String[] args) {
+        int[] mas = {1, 2, 3, 4 , 5, -1};
+
+        System.out.println(Arrays.toString(Example.reverse(mas)));
+    }
 }
