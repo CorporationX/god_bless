8070645ded772dbfad66a922d36be0fdc4f46d36
AlekseyB86nb <tigrxam@gmail.com>
2024-10-03 19:58:33 +0300

    add method getShelf

--- src/main/java/westerosLibrary/Main.java
+++ src/main/java/westerosLibrary/Main.java
@@ -8,15 +8,24 @@ public class Main {
 
     public static void main(String[] args) {
 
-        addBook(new Book("Мёртвые души", "Гоголь Н.В.", 1842), "0001");
-        addBook(new Book("Вий", "Гоголь Н.В.", 1835), "0001");
-        addBook(new Book("Мастер и Маргарита", "Булгаков М.А.", 1966), "0002");
-        addBook(new Book("Бич времён", "Головачев В.В.", 2008), "0003");
+        Book book1 = new Book("Мёртвые души", "Гоголь Н.В.", 1842);
+        Book book2 = new Book("Вий", "Гоголь Н.В.", 1835);
+        Book book3 = new Book("Мастер и Маргарита", "Булгаков М.А.", 1966);
+        Book book4 = new Book("Бич времён", "Головачев В.В.", 2008);
+        addBook(book1, "0001");
+        addBook(book2, "0001");
+        addBook(book3, "0002");
+        addBook(book4, "0003");
         LIBRARY.forEach((book, shelf) -> System.out.println(book + " - " + shelf));
+
 
         System.out.println();
         removeBook("Мёртвые души", "Гоголь Н.В.", 1842);
         LIBRARY.forEach((book, shelf) -> System.out.println(book + " - " + shelf));
+
+        System.out.println();
+        System.out.println(getShelf(book1));
+        System.out.println(getShelf(book4));
 
     }
 
@@ -26,5 +35,9 @@ public class Main {
 
     public static void removeBook(String title, String author, int year) {
         LIBRARY.remove(new Book(title, author, year));
+    }
+
+    public static String getShelf(Book book) {
+        return LIBRARY.get(book);
     }
 }
