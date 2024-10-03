28164cf998edfa49b8cbde963bf5b7a523d798ae
Memoreks <akifyevalex1@gmail.com>
2024-10-03 19:25:55 +0700

    Рефакторинг

--- src/main/java/school/faang/Main.java
+++ src/main/java/school/faang/Main.java
@@ -1,21 +1,25 @@
 package school.faang;
 
 import school.faang.heroes.Archer;
+import school.faang.heroes.Character;
 import school.faang.heroes.Warrior;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
         Warrior warrior = new Warrior("Ivan");
         Archer archer = new Archer("Sergey");
+        List<Character> heroes = List.of(warrior, archer);
+
 
         System.out.println("До боя:");
-        warrior.showHealth();
-        archer.showHealth();
+        heroes.forEach(Character::showHealth);
         System.out.println("Бой:");
         warrior.attack(archer);
         archer.attack(warrior);
         System.out.println("После боя:");
-        warrior.showHealth();
-        archer.showHealth();
+        heroes.forEach(Character::showHealth);
     }
 }
