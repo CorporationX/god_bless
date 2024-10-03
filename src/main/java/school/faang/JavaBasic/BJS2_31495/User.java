0fdedc923d7855b750fa1433a59df0c69f6ba5c8
0Miha0 <140548440+0Miha0@users.noreply.github.com>
2024-10-03 13:08:36 +0500

    https://faang-school.atlassian.net/browse/BJS2-31495

---
+++ src/main/java/school/faang/JavaBasic/BJS2_31495/User.java
@@ -0,0 +1,29 @@
+package school.faang.JavaBasic.BJS2_31495;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+import java.util.*;
+
+@Data
+@AllArgsConstructor
+public class User {
+    private int id;
+    private String name;
+    private int age;
+    private Set<String> activities;
+
+
+    public static Map<User, String> findHobbyLovers(List<User> userList, Set<String> activities){
+        Map<User, String> groupedUsers = new HashMap<>();
+        for(User user : userList){
+            for(String set : activities){
+                if(user.getActivities().contains(set)){
+                    groupedUsers.put(user, set);
+                    break;
+                }
+            }
+        }
+        return groupedUsers;
+    }
+}
