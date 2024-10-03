3d05b70cf3905177b8323b713e10f7277159709b
Блюдёнов Александр Вадимович <ABlyudenov@alfabank.ru>
2024-10-03 21:19:19 +1000

    added StreamEventStorage

---
+++ src/main/java/school/faang/StreamEventStorage.java
@@ -0,0 +1,49 @@
+package school.faang;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class StreamEventStorage {
+    private Map<Integer, StreamEvent> streamEventById;
+    private Map<String, List<StreamEvent>> streamEventByType;
+
+    public StreamEventStorage() {
+        this.streamEventById = new HashMap<>();
+        this.streamEventByType = new HashMap<>();
+    }
+
+    public void add(StreamEvent streamEvent) {
+        streamEventById.put(streamEvent.getId(), streamEvent);
+        streamEventByType.computeIfAbsent(streamEvent.getEventType(),
+                eventType -> new ArrayList<>()).add(streamEvent);
+    }
+
+    public StreamEvent findById(Integer id) {
+        return streamEventById.get(id);
+    }
+
+    public List<StreamEvent> findByEventType(String eventType) {
+        return streamEventByType.get(eventType);
+    }
+
+    public void deleteById(Integer id) {
+        StreamEvent streamEvent = streamEventById.get(id);
+        streamEventById.remove(id);
+        List<StreamEvent> streamEvents = streamEventByType.get(streamEvent.getEventType());
+        streamEvents.remove(streamEvent);
+        if (streamEvents.isEmpty()) {
+            streamEventByType.remove(streamEvent.getEventType());
+        }
+    }
+
+    public void printAllStreamEvent() {
+        for (Map.Entry<Integer, StreamEvent> entry : streamEventById.entrySet()) {
+            System.out.printf("Id: %d, eventType: %s, data: %s%n",
+                    entry.getKey(),
+                    entry.getValue().getEventType(),
+                    entry.getValue().getData());
+        }
+    }
+}
---
+++ src/test/java/school/faang/StreamEventStorageTest.java
@@ -0,0 +1,61 @@
+package school.faang;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class StreamEventStorageTest {
+
+    private StreamEventStorage streamEventStorage;
+
+    private StreamEvent streamEvent1;
+    private StreamEvent streamEvent2;
+    private StreamEvent streamEvent3;
+
+    @BeforeEach
+    public void setUp() {
+        streamEventStorage = new StreamEventStorage();
+
+        streamEvent1 = new StreamEvent(1, "eventType1", "data1");
+        streamEvent2 = new StreamEvent(2, "eventType1", "data2");
+        streamEvent3 = new StreamEvent(3, "eventType2", "data3");
+
+        streamEventStorage.add(streamEvent1);
+        streamEventStorage.add(streamEvent2);
+        streamEventStorage.add(streamEvent3);
+    }
+
+    @Test
+    public void addAndFindByIdTest() {
+        StreamEvent streamEventById = streamEventStorage.findById(streamEvent1.getId());
+        assertEquals(streamEventById, streamEvent1);
+    }
+
+    @Test
+    public void addAndFindByEventTypeTest() {
+        List<StreamEvent> streamEvents = streamEventStorage.findByEventType("eventType1");
+        assertEquals(2, streamEvents.size());
+    }
+
+    @Test
+    public void deleteByIdTest() {
+        streamEventStorage.deleteById(1);
+        List<StreamEvent> streamEvents = streamEventStorage.findByEventType("eventType1");
+        assertEquals(1, streamEvents.size());
+    }
+
+    @Test
+    public void deleteByIdLastStreamEventTest() {
+        streamEventStorage.deleteById(3);
+        List<StreamEvent> streamEvents = streamEventStorage.findByEventType("eventType2");
+        assertNull(streamEvents);
+    }
+
+    @Test
+    public void printAllStreamEventsTest() {
+        streamEventStorage.printAllStreamEvent();
+    }
+}
