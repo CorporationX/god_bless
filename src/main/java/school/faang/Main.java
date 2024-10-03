799dc8bed04c271f8c3ca99113b2e5c8b24db463
Alexander Litovkin <litovkinalexan@gmail.com>
2024-10-03 10:28:47 +0700

    remove code duplication

--- src/main/java/school/faang/Main.java
+++ src/main/java/school/faang/Main.java
@@ -5,15 +5,20 @@ public class Main {
     public static void main(String[] args) {
         Character warrior = new Warrior("Alex");
         Character archer = new Archer("Ivan");
-        System.out.println(
-                "warrior health: " + warrior.getHealth() +
-                "; archer health: " + archer.getHealth()
-        );
+        printStatistics(warrior, archer);
+
         warrior.attack(archer);
         archer.attack(warrior);
-        System.out.println(
-                "warrior health: " + warrior.getHealth() +
-                "; archer health: " + archer.getHealth()
-        );
+        printStatistics(warrior, archer);
+    }
+
+    private static void printStatistics(Character... characters) {
+        for (Character character : characters) {
+            System.out.printf(
+                    "%s health: %d%n",
+                    character.getClass().getSimpleName(),
+                    character.getHealth()
+            );
+        }
     }
 }
