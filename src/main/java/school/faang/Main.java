62fa14ce520e25efeb378f976332a2c9f0691827
Alexey Zimin <zimin.alexey@gmail.com>
2024-10-03 18:47:45 +0300

    [BJS2-31318] - Abstraction fix

--- src/main/java/school/faang/Main.java
+++ src/main/java/school/faang/Main.java
@@ -1,4 +1,26 @@
 package school.faang;
 
+import school.faang.abstractclasses.Archer;
+import school.faang.abstractclasses.Character;
+import school.faang.abstractclasses.Warrior;
+
 public class Main {
+    public static void main(String[] args) {
+
+        Character warrior = new Warrior("Warrior");
+        Character archer = new Archer("Archer");
+
+        System.out.println(warrior);
+        System.out.println(archer);
+
+        System.out.println("Warrior attack");
+        warrior.attack(archer);
+        System.out.println(warrior);
+        System.out.println(archer);
+
+        System.out.println("Archer attack");
+        archer.attack(warrior);
+        System.out.println(warrior);
+        System.out.println(archer);
+    }
 }
