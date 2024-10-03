ba8c4ef504b162d85b5d89dba65713483e68fb01
ZhubanyshZh <zubanyszarylkasyn@gmail.com>
2024-10-03 17:23:51 +0600

    added abstract class Character with attributes name, power, dexterity, intelligence and health and with abstract method attack which drop health from another character which transferred into parameter
    And added classes which extends from Character class there are Warrior and Archer

---
+++ src/main/java/Archer.java
@@ -0,0 +1,10 @@
+public class Archer extends Character {
+    Archer(String name) {
+        super(name, 3, 10, 5);
+    }
+
+    @Override
+    public void attack(Character character) {
+        character.setHealth(character.health - this.getPower());
+    }
+}
