815038fd5238308a09f3344109fc74665239ad11
Dmitrii Pazinich <pazinich.d@yandex.ru>
2024-10-05 00:05:39 +0300

    testSearchByIndex изменен. Внесены исправления в соответствии с замечаниями в ревью.

--- src/main/java/school/faang/bjs2_32814/Main.java
+++ src/main/java/school/faang/bjs2_32814/Main.java
@@ -6,27 +6,28 @@ import java.util.Map;
 import java.util.Set;
 
 public class Main {
-    protected static final Map<String, Set<WebPage>> wordIndex = new HashMap<>();
-    protected static final Map<String, WebPage> urlIndex = new HashMap<>();
+    static final Map<String, Set<WebPage>> WORD_INDEX = new HashMap<>();
+    static final Map<String, WebPage> URL_INDEX = new HashMap<>();
+    static final String SPLIT_RULE = "[( ,.\"!?:';)]+";
 
     static void indexWebPage(WebPage webPage) {
         String url = webPage.getUrl();
         String content = webPage.getContent();
-        String[] words = content.toLowerCase().split("[( ,.\"!?:';)]+");
-        urlIndex.put(url, webPage);
+        String[] words = content.toLowerCase().split(SPLIT_RULE);
+        URL_INDEX.put(url, webPage);
 
         for (String word : words) {
-            wordIndex.computeIfAbsent(word, key -> new HashSet<>()).add(webPage);
+            WORD_INDEX.computeIfAbsent(word, key -> new HashSet<>()).add(webPage);
         }
     }
 
     static Set<WebPage> searchByIndex(String word) {
-        return wordIndex.get(word.toLowerCase());
+        return WORD_INDEX.get(word.toLowerCase());
     }
 
     static void deleteByUrl(String url) {
-        WebPage targetPage = urlIndex.remove(url);
-        for (var entry : wordIndex.entrySet()) {
+        WebPage targetPage = URL_INDEX.remove(url);
+        for (var entry : WORD_INDEX.entrySet()) {
             Set<WebPage> webPageSet = entry.getValue();
             webPageSet.remove(targetPage);
         }
--- src/test/java/school/faang/bjs2_32814/MainTest.java
+++ src/test/java/school/faang/bjs2_32814/MainTest.java
@@ -5,47 +5,58 @@ import org.junit.jupiter.api.Test;
 
 import java.util.Set;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
+import static school.faang.bjs2_32814.Main.SPLIT_RULE;
+import static school.faang.bjs2_32814.Main.URL_INDEX;
+import static school.faang.bjs2_32814.Main.WORD_INDEX;
+import static school.faang.bjs2_32814.Main.indexWebPage;
+import static school.faang.bjs2_32814.Main.searchByIndex;
 
 public class MainTest {
 
     @BeforeEach
     public void setUp() {
-        Main.wordIndex.clear();
-        Main.urlIndex.clear();
+        WORD_INDEX.clear();
+        URL_INDEX.clear();
     }
 
     @Test
     void testIndexWebPage() {
         WebPage faangSchool = new WebPage("faang.school", "Java Bootcamp", "Учи Java, лодырь!");
-        Main.indexWebPage(faangSchool);
+        indexWebPage(faangSchool);
 
-        assertTrue(Main.wordIndex.containsKey("учи"));
-        assertTrue(Main.wordIndex.containsKey("java"));
-        assertTrue(Main.wordIndex.containsKey("лодырь"));
+        assertTrue(WORD_INDEX.containsKey("учи"));
+        assertTrue(WORD_INDEX.containsKey("java"));
+        assertTrue(WORD_INDEX.containsKey("лодырь"));
     }
 
     @Test
     void testSearchByIndex() {
         WebPage faangSchool = new WebPage("faang.school", "Java Bootcamp", "Учи Java, лодырь!");
         WebPage school = new WebPage("school.ru", "Алгебра", "Учи алгебру, лодырь!");
-        Main.indexWebPage(faangSchool);
-        Main.indexWebPage(school);
+        indexWebPage(faangSchool);
+        indexWebPage(school);
+        String[] faangSchoolWords = faangSchool.getContent().split(SPLIT_RULE);
+        String[] schoolWords = school.getContent().split(SPLIT_RULE);
 
-        assertEquals(2, Main.searchByIndex("учи").size());
-        assertEquals(1, Main.searchByIndex("java").size());
+        for (String word : faangSchoolWords) {
+            assertTrue(searchByIndex(word).contains(faangSchool));
+        }
+
+        for (String word : schoolWords) {
+            assertTrue(searchByIndex(word).contains(school));
+        }
     }
 
     @Test
     void testDeleteByUrl() {
         WebPage faangSchool = new WebPage("faang.school", "Java Bootcamp", "Учи Java, лодырь!");
-        Main.indexWebPage(faangSchool);
+        indexWebPage(faangSchool);
         Main.deleteByUrl("faang.school");
         Set<WebPage> result = Main.searchByIndex("java");
 
         assertTrue(result == null || !result.contains(faangSchool));
-        assertFalse(Main.urlIndex.containsKey("faang.school"));
+        assertFalse(URL_INDEX.containsKey("faang.school"));
     }
 }
