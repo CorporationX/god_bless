d2bd25d24f05d1a8aea7b892c43c3ecd17657cf2
Svyatoslav Kostrov <kost-2014@mail.ru>
2024-10-04 21:08:21 +0300

    some corrections

--- src/main/java/school/faang/task133/Main.java
+++ src/main/java/school/faang/task133/Main.java
@@ -7,7 +7,8 @@ import java.util.Map;
 
 public class Main {
 
-    private final List<Student> students= new ArrayList<>();
+    private static final List<Student> STUDENTS = new ArrayList<>();
+
     {
         Student student1 = new Student("Ilya", "Гриффиндор", 4);
         Student student2 = new Student("Artem", "Слизерин", 4);
@@ -15,53 +16,60 @@ public class Main {
         Student student4 = new Student("Svyatoslav", "Пуффендуй", 4);
         Student student5 = new Student("ivan", "Слизерин", 3);
 
-        students.add(student1);
-        students.add(student2);
-        students.add(student3);
-        students.add(student4);
-        students.add(student5);
+        STUDENTS.add(student1);
+        STUDENTS.add(student2);
+        STUDENTS.add(student3);
+        STUDENTS.add(student4);
+        STUDENTS.add(student5);
     }
-    public HashMap<String,List<Student>> studentGroup (List<Student> students){
+
+    public HashMap<String, List<Student>> studentGroup(List<Student> students) {
         HashMap<String, List<Student>> answer = new HashMap<>();
-        for(Student student : students){
-            answer.computeIfAbsent(facultyAndYear(student), k->new ArrayList<>()).add(student);
+        for (Student student : students) {
+            answer.computeIfAbsent(facultyAndYear(student), k -> new ArrayList<>()).add(student);
         }
         return answer;
     }
-    private String facultyAndYear(Student student){
+
+    private String facultyAndYear(Student student) {
         return student.getFaculty() + " year=" + student.getYear();
     }
-    public void addStudent(Student student){
-        students.add(student);
+
+    public void addStudent(Student student) {
+        STUDENTS.add(student);
     }
-    public void removeStudent(String name, String faculty, int year){
+
+    public void removeStudent(String name, String faculty, int year) {
         Student student = new Student(name, faculty, year);
-        students.remove(student);
+        STUDENTS.remove(student);
     }
-    public List<Student> findStudentsByFacultyAndYear(String faculty, int year){
+
+    public List<Student> findStudentsByFacultyAndYear(String faculty, int year) {
         List<Student> answer = new ArrayList<>();
-        for(Student student : students){
-            if(student.getYear()==year && student.getFaculty()==faculty){
+        for (Student student : STUDENTS) {
+            if (student.getYear() == year && student.getFaculty().equals(faculty)) {
                 answer.add(student);
             }
         }
         return answer;
     }
-    public void printAllGroupedStudents(){
-        HashMap<String, List<Student>> allStudentsGrouped = studentGroup(students);
-        for(Map.Entry<String, List<Student>> entry : allStudentsGrouped.entrySet()){
-            for(Student student : entry.getValue()){
+
+    public void printAllGroupedStudents() {
+        HashMap<String, List<Student>> allStudentsGrouped = studentGroup(STUDENTS);
+        for (Map.Entry<String, List<Student>> entry : allStudentsGrouped.entrySet()) {
+            for (Student student : entry.getValue()) {
                 System.out.println(entry.getKey() + " " + student);
             }
             System.out.println("================================================================");
         }
     }
+
     public static void main(String[] args) {
         Main main = new Main();
         main.printAllGroupedStudents();
         System.out.println();
 
-        Student student = new Student("Вася", "Пуффендуй" , 4);
+        Student student = new Student("Вася", "Пуффендуй", 4);
         main.addStudent(student);
         main.printAllGroupedStudents();
         System.out.println();
@@ -70,8 +78,8 @@ public class Main {
         main.printAllGroupedStudents();
         System.out.println();
 
-        List<Student> studentList =main.findStudentsByFacultyAndYear("Пуффендуй" , 4);
-        for(Student student1 : studentList){
+        List<Student> studentList = main.findStudentsByFacultyAndYear("Пуффендуй", 4);
+        for (Student student1 : studentList) {
             System.out.println(student1);
         }
     }
