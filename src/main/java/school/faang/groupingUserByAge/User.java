821bc19c002423162029f575c6c744ad4349ec91
Sergey Zhitenev <zhitenev@lanit.ru>
2024-10-04 14:52:07 +0300

    changing the validation of constructor fields, fix imports

--- src/main/java/school/faang/groupingUserByAge/User.java
+++ src/main/java/school/faang/groupingUserByAge/User.java
@@ -3,7 +3,12 @@ package school.faang.groupingUserByAge;
 import lombok.Builder;
 import lombok.Data;
 
-import java.util.*;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
 
 @Data
 @Builder
@@ -18,26 +23,24 @@ public class User {
     private String address;
 
     public User(String name, int age, String workplace, String address) {
-        if (!name.isEmpty()) {
-            this.name = name;
-        } else throw new IllegalArgumentException("The name field cannot be empty");
+        fieldValidation(name, age, workplace, address);
 
-        if (age >= MIN_AGE) {
-            this.age = age;
-        } else throw new IllegalArgumentException("The minimum age is 18");
-
-        if (VALID_JOBS.contains(workplace)) {
-            this.workplace = workplace;
-        } else throw new IllegalArgumentException("Invalid workplace: " + workplace);
-
-        if (VALID_ADDRESSES.contains(address)) {
-            this.address = address;
-        } else throw new IllegalArgumentException("Invalid address: " + address);
+        this.name = name;
+        this.age = age;
+        this.workplace = workplace;
+        this.address = address;
     }
 
     public static Map<Integer, List<User>> groupUsers(List<User> userList) {
         Map<Integer, List<User>> userGroupMap = new HashMap<>();
         userList.forEach(user -> userGroupMap.computeIfAbsent(user.getAge(), age -> new ArrayList<>()).add(user));
         return userGroupMap;
+    }
+
+    private static void fieldValidation(String name, int age, String workplace, String address) {
+        if (name.isEmpty()) throw new IllegalArgumentException("The name field cannot be empty");
+        if (age < MIN_AGE) throw new IllegalArgumentException("The minimum age is 18");
+        if (!VALID_JOBS.contains(workplace)) throw new IllegalArgumentException("Invalid workplace: " + workplace);
+        if (!VALID_ADDRESSES.contains(address)) throw new IllegalArgumentException("Invalid address: " + address);
     }
 }
