7168236218e204672ba9e85a3ac23fd58bf26d51
Denis Levkhov <levkhovde@yandex.ru>
2024-10-05 00:51:39 +0300

    Solving task "Counting Absenteeism"

---
+++ src/main/java/countingAbsenteeism/LearningStage.java
@@ -0,0 +1,11 @@
+package countingAbsenteeism;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class LearningStage {
+    private String faculty;
+    private int year;
+}
