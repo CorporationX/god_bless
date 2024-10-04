1243b762f09cb3625f20c12b7e0edfd63a69c0e0
Maks Russkikh <15maksim@mail.ru>
2024-10-04 06:22:04 +0300

    Main clear

--- src/main/java/school/faang/Main.java
+++ src/main/java/school/faang/Main.java
@@ -1,16 +1,6 @@
 package school.faang;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Map;
-
 public class Main {
-    public static void main(String[] args) {
-        List<User> users = new ArrayList<>();
-        users.add(new User("Igor", 32, "Olenov", "Moscow"));
-        users.add(new User("Oleg", 32, "Urin", "Tula"));
-        users.add(new User("Evgen", 30, "Policai", "Orenburg"));
-        Map<Integer, List<User>>  resultMapUsers = User.groupUsers(users);
-    }
+
 }
 
