6ed036d94804c3992708bd66ac83485feabf1dfe
AVB <abdddev@gmail.com>
2024-10-04 15:39:11 +1000

    added two cache for school

---
+++ src/main/java/school/faang/two/cache/Student.java
@@ -0,0 +1,11 @@
+package school.faang.two.cache;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class Student {
+    private Integer id;
+    private String name;
+}
