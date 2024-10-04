a183a8caa2d7ab9ee48c07efc7d3d990af4238a8
lomuserg <sergeymu2015@yandex.ru>
2024-10-04 23:21:15 +0300

    Create User.java

---
+++ src/main/java/school/faang/User.java
@@ -0,0 +1,42 @@
+package school.faang;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+@Getter
+public class User {
+    private String name;
+    private String age;
+    private String job;
+    private String address;
+
+    public User() {
+
+    }
+
+    public User(String name, String age, String job, String address) {
+        this.name = name;
+        this.age = age;
+        this.job = job;
+        this.address = address;
+    }
+
+    public Map<String, List<User>> groupUsers(List<User> users) {
+        Map<String, List<User>> mapByAge = new HashMap<>();
+        List<User> listByAge = new ArrayList<>();
+        for (User user : users) {
+            if (user.age == this.age) {
+                listByAge.add(user);
+                System.out.println("Закинул " + user.name + "\n");
+            }
+        }
+        mapByAge.put(this.age,listByAge);
+        return  mapByAge;
+
+    }
+}
+
