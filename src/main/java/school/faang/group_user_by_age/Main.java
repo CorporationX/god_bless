9f3d9da5b9556229f84b2123cd8c9541db6dcb25
Yevgeniy <yevgengeodakyan@gmail.com>
2024-10-04 10:02:47 -0400

    user registry, constants added , users check added

--- src/main/java/school/faang/group_user_by_age/Main.java
+++ src/main/java/school/faang/group_user_by_age/Main.java
@@ -4,15 +4,15 @@ import java.util.List;
 
 public class Main {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IllegalAccessException {
 
         var users = List.of(
-                new User("Sam", 24, "Google", "Washington DC"),
-                new User("Ban", 24, "Amazon", "Miami"),
-                new User("Ken", 21, "Amazon", "Chicago"),
-                new User("Keanu", 21, "Netflix", "California"),
-                new User("Man", 32, "Netflix", "New York"),
-                new User("John", 32, "Microsoft", "Texas")
+                new User("Sam", 24, "Google", "London"),
+                new User("Ban", 24, "Amazon", "London"),
+                new User("Ken", 21, "Amazon", "New York"),
+                new User("Keanu", 21, "Uber", "Amsterdam"),
+                new User("Man", 32, "Uber", "New York"),
+                new User("John", 32, "Uber", "Amsterdam")
         );
 
         User.groupUsers(users).entrySet().forEach(System.out::println);
--- src/main/java/school/faang/group_user_by_age/User.java
+++ src/main/java/school/faang/group_user_by_age/User.java
@@ -1,20 +1,29 @@
 package school.faang.group_user_by_age;
 
-import lombok.AllArgsConstructor;
 import lombok.Data;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 @Data
-@AllArgsConstructor
 public class User {
+    public static final Set<String> VALID_JOBS = Set.of("Google", "Uber", "Amazon");
+    public static final Set<String> VALID_ADDRESSES = Set.of("London", "New York", "Amsterdam");
     private final String name;
     private int age;
     private String company;
     private String address;
+
+    public User(String name, int age, String company, String address) throws IllegalAccessException {
+        if (name.isEmpty() || name.isBlank()) throw new IllegalAccessException("Name cannot be empty");
+        if (age < 18) throw new IllegalAccessException("Age cannot be less than 18");
+        if (!VALID_JOBS.contains(company)) throw new IllegalAccessException("Company not exists");
+        if (!VALID_ADDRESSES.contains(address)) throw new IllegalAccessException("Address not exists");
+
+        this.name = name;
+        this.age = age;
+        this.company = company;
+        this.address = address;
+    }
 
     static Map<Integer, List<User>> groupUsers(List<User> users) {
         var groupedUsers = new HashMap<Integer, List<User>>();
