89a81760d25bfd0b9114b00588a99d701ed8288a
Lexsandr <lexsandr@inbox.ru>
2024-10-04 17:46:42 +0300

    BJS2_31377 : Changed the architecture app Delete class BJS2_31377 and BJS2_31323, Create package BJS2_31377 and BJS2_31323,

--- src/main/java/school/faang/Main.java
+++ src/main/java/school/faang/Main.java
@@ -1,8 +1,6 @@
 package school.faang;
 
 public class Main {
-	public static void main(String[] args) {
-		//new Tasks.BJS2_31377().run(); //Группировка пользователей по возрасту
-		new Tasks.BJS3_31323().run(); //Абстракция, абстракция!
-	}
+    public static void main(String[] args) {
+    }
 }
