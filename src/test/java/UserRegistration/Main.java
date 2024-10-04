a0d97a73053b7fd8d90d7a56d303624fd7775460
Irina Krasnova <arisha.17@mail.ru>
2024-10-04 21:43:32 +0300

    валидация полей вынесена в отдельный метод

--- src/test/java/UserRegistration/Main.java
+++ src/test/java/UserRegistration/Main.java
@@ -6,12 +6,12 @@ import java.util.Map;
 
 public class Main {
     public static void main(String[] args) {
-        User olya = new User("olya", 24, "Uber", "London");
-        User kolya = new User("kolya", 21, "Uber", "London");
-        User molya = new User("molya", 21, "Uber", "London");
-        User tolya = new User("tolya", 24, "Uber", "London");
+        List<User> users = Arrays.asList(
+                new User("olya", 17, "Uber", "London"),
+                new User("kolya", 21, "Uber", "London"),
+                new User("molya", 21, "Uber", "London"),
+                new User("tolya", 24, "Uber", "London"));
 
-        List<User> users = Arrays.asList(olya, kolya, tolya, molya);
         users.forEach(s -> System.out.print(s + " "));
     }
 }
