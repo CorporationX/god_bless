a2736551557f3a654564acb790b16f1d1556e48d
Irina Krasnova <arisha.17@mail.ru>
2024-10-04 20:53:45 +0300

    задача Ловим события

---
+++ src/test/java/StreamEventPak/StreamEvent.java
@@ -0,0 +1,49 @@
+package StreamEventPak;
+
+import java.util.Objects;
+
+public class StreamEvent {
+    private int id;
+    private String eventType;
+    private String data;
+
+    public StreamEvent(int id, String eventType, String data) {
+        this.id = id;
+        this.eventType = eventType;
+        this.data = data;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getEventType() {
+        return eventType;
+    }
+
+    public String getData() {
+        return data;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        StreamEvent that = (StreamEvent) o;
+        return id == that.id && Objects.equals(eventType, that.eventType) && Objects.equals(data, that.data);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, eventType, data);
+    }
+
+    @Override
+    public String toString() {
+        return "StreamEvent{" +
+                "id=" + id +
+                ", eventType='" + eventType + '\'' +
+                ", data='" + data + '\'' +
+                '}';
+    }
+}
