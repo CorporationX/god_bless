257a19912dcf0651e3202aee6e28818dacd579f2
Aleksi Basilashvili <lucheidobra@mail.ru>
2024-10-04 17:16:52 +0300

    Считаем прогулы BJS2-32443

--- src/main/java/school/faang/misseddays/FacultyAndYear.java
+++ src/main/java/school/faang/misseddays/FacultyAndYear.java
@@ -3,11 +3,9 @@ package school.faang.misseddays;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 
-@AllArgsConstructor
 @Data
-public class Student {
-    private String name;
+@AllArgsConstructor
+public class FacultyAndYear{
     private String faculty;
     private int year;
 }
-
