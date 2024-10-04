ceac33eed31e043e0a9149d63da8ecca506289fd
Gadzhiev Gadzhi <Gadzhiev3222@gmail.com>
2024-10-04 06:13:10 +0300

    .

--- src/main/java/school/faang/collect_users/Main.java
+++ src/main/java/school/faang/collect_users/Main.java
@@ -1,4 +1,4 @@
-package school.faang;
+package school.faang.collect_users;
 
 import java.util.List;
 import java.util.Set;
--- src/main/java/school/faang/collect_users/User.java
+++ src/main/java/school/faang/collect_users/User.java
@@ -1,4 +1,4 @@
-package school.faang;
+package school.faang.collect_users;
 
 import lombok.AllArgsConstructor;
 import lombok.ToString;
