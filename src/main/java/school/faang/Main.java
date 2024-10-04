c007a08c290b38e7fb690c7c1b1556da4723d955
heckfy86 <marlinco263@gmail.com>
2024-10-04 21:47:46 +0200

    BJS2-33242

--- src/main/java/school/faang/Main.java
+++ src/main/java/school/faang/Main.java
@@ -6,23 +6,38 @@ import java.util.List;
 import java.util.Map;
 
 public class Main {
-    Map<Integer, StreamEvent> idMap = new HashMap<>();
-    Map<String, List<StreamEvent>> typeMap = new HashMap<>();
+    static Map<Integer, StreamEvent> idMap = new HashMap<>();
+    static Map<String, List<StreamEvent>> typeMap = new HashMap<>();
 
-    public void addStreamEvent(StreamEvent streamEvent) {
+    public static void main(String[] args) {
+
+
+        addStreamEvent(new StreamEvent(1, "Warning", "adfadfasd"));
+        addStreamEvent(new StreamEvent(2, "Warning", "fdsfsdf"));
+        addStreamEvent(new StreamEvent(3, "Warning", "123213"));
+        addStreamEvent(new StreamEvent(4, "Info", "gfgfsg"));
+        addStreamEvent(new StreamEvent(5, "Info", "weqrert"));
+        addStreamEvent(new StreamEvent(6, "Info", "1454325"));
+
+        printStreamEvents();
+        removeStreamEvent(1);
+        System.out.println("After remove");
+        printStreamEvents();
+    }
+    public static void addStreamEvent(StreamEvent streamEvent) {
         idMap.put(streamEvent.getId(), streamEvent);
         typeMap.computeIfAbsent(streamEvent.getEventType(), se -> new ArrayList<>()).add(streamEvent);
     }
 
-    public StreamEvent findStreamEventById(int id) {
+    public static StreamEvent findStreamEventById(int id) {
         return idMap.get(id);
     }
 
-    public List<StreamEvent> findStreamEventByType(String type) {
+    public static List<StreamEvent> findStreamEventByType(String type) {
         return typeMap.get(type);
     }
 
-    public Boolean removeStreamEvent(int id) {
+    public static Boolean removeStreamEvent(int id) {
         StreamEvent streamEvent = idMap.remove(id);
         if (streamEvent != null) {
             return typeMap.get(streamEvent.getEventType()).remove(streamEvent);
@@ -30,7 +45,7 @@ public class Main {
         return false;
     }
 
-    public void printStreamEvents() {
+    public static void printStreamEvents() {
         for (Map.Entry<Integer, StreamEvent> entry : idMap.entrySet()) {
             System.out.println("id: " + entry.getKey()
                     + "; Event Type: " + entry.getValue().getEventType()
@@ -38,19 +53,4 @@ public class Main {
         }
     }
 
-    public static void main(String[] args) {
-
-        Main main = new Main();
-        main.addStreamEvent(new StreamEvent(1, "Warning", "adfadfasd"));
-        main.addStreamEvent(new StreamEvent(2, "Warning", "fdsfsdf"));
-        main.addStreamEvent(new StreamEvent(3, "Warning", "123213"));
-        main.addStreamEvent(new StreamEvent(4, "Info", "gfgfsg"));
-        main.addStreamEvent(new StreamEvent(5, "Info", "weqrert"));
-        main.addStreamEvent(new StreamEvent(6, "Info", "1454325"));
-
-        main.printStreamEvents();
-        main.removeStreamEvent(1);
-        System.out.println("After remove");
-        main.printStreamEvents();
-    }
 }
