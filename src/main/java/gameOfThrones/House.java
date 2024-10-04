7516eb96dcdb2a022fde56eea3e47fa01a0a90f3
AlekseyB86nb <tigrxam@gmail.com>
2024-10-04 19:02:28 +0300

    поправил название класса House, и поправил код в файле Main

--- src/main/java/gameOfThrones/House.java
+++ src/main/java/gameOfThrones/House.java
@@ -7,7 +7,7 @@ import lombok.Getter;
 @Getter
 @AllArgsConstructor
 @EqualsAndHashCode
-public class addHouse {
+public class House {
     private String name;
     private String sigil;
 }
--- src/main/java/gameOfThrones/Main.java
+++ src/main/java/gameOfThrones/Main.java
@@ -4,25 +4,24 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class Main {
-    private static final Map<String, addHouse> HOUSES = new HashMap<>();
+    private static final Map<String, House> HOUSES = new HashMap<>();
 
 
     public static void main(String[] args) {
 
-        System.out.println("Houses: ");
         printAllHouses();
 
-        HOUSES.computeIfAbsent("Stark", key -> new addHouse("Stark of Winterfell", "SOW"));
-        HOUSES.computeIfAbsent("Lannister", key -> new addHouse("Lannister of Casterly Rock", "LOCR"));
-        HOUSES.computeIfAbsent("Barateon", key -> new addHouse("Barateon of Storm's End", "BOSE"));
+        HOUSES.computeIfAbsent("Stark", key -> new House("Stark", "SOW"));
+        HOUSES.computeIfAbsent("Lannister", key -> new House("Lannister", "LOCR"));
+        HOUSES.computeIfAbsent("Barateon", key -> new House("Barateon", "BOSE"));
         printAllHouses();
 
-        addHouse("Targaryen of King's Landing", "TOKL");
-        addHouse("Arryn of the Eyrie", "Arr");
-        addHouse("Tully of Riverrun", "TOR");
-        addHouse("Greyjoy of Pyke", "GOP");
-        addHouse("Tyrell of Highgarden", "TOH");
-        addHouse("Martell of Sunspear", "MOS");
+        addHouse("Targaryen", "TOKL");
+        addHouse("Arryn", "Arr");
+        addHouse("Tully", "TOR");
+        addHouse("Greyjoy", "GOP");
+        addHouse("Tyrell", "TOH");
+        addHouse("Martell", "MOS");
 
         System.out.println();
         printAllHouses();
@@ -41,8 +40,7 @@ public class Main {
     }
 
     public static void addHouse(String name, String sigil) {
-        String key = name.split(" ")[0];
-        HOUSES.computeIfAbsent(key, (k) -> new addHouse(name, sigil));
+        HOUSES.computeIfAbsent(name, (k) -> new House(name, sigil));
     }
 
     public static void removeHouse(String name) {
