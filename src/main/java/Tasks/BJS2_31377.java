fb447f125d0db8dba46028263918f15746c2501f
Lexsandr <lexsandr@inbox.ru>
2024-10-04 14:25:19 +0300

    BJS2_31377 : Returned 120 character strings, fix Method camelCase CodeStyle

--- src/main/java/Tasks/BJS2_31377.java
+++ src/main/java/Tasks/BJS2_31377.java
@@ -7,16 +7,14 @@ import java.util.TreeMap;
 
 public class BJS2_31377 {
 	public void run() {
-		List<User> users = User.GetUsersList();
+		List<User> users = User.getUsersList();
 		Map<Integer, List<User>> groupUser;
 		
-		System.out.println(
-				"Не отсортированный список пользователей");
-		User.ShowUsers(users);
+		System.out.println("Не отсортированный список пользователей");
+		User.showUsers(users);
 		groupUser = User.groupUser(users);
-		System.out.println(
-				"Коллекции Map состоящий из ключа " + "возраста пользоватей и списка пользователей");
-		User.ShowUsersSortedByAge(groupUser);
+		System.out.println("Коллекции Map состоящий из ключа " + "возраста пользователей и списка пользователей");
+		User.showUsersSortedByAge(groupUser);
 	}
 }
 
@@ -27,8 +25,7 @@ class User {
 	private String address;
 	
 	public User(
-			String name, int age, String workPlace,
-			String address
+			String name, int age, String workPlace, String address
 	) {
 		this.name = name;
 		this.age = age;
@@ -41,13 +38,10 @@ class User {
 	}
 	
 	public String getInformation() {
-		return String.format(
-				"Я %s. Мой возраст : %d. Мой адрес : %s. Работаю в : %s",
-				name, age, address, workPlace
-		);
+		return String.format("Я %s. Мой возраст : %d. Мой адрес : %s. Работаю в : %s", name, age, address, workPlace);
 	}
 	
-	public static void ShowUsers(List<User> users) {
+	public static void showUsers(List<User> users) {
 		for (User user : users) {
 			System.out.println(user.getInformation());
 		}
@@ -55,21 +49,18 @@ class User {
 		System.out.println();
 	}
 	
-	public static void ShowUsersSortedByAge(
+	public static void showUsersSortedByAge(
 			Map<Integer, List<User>> users
 	) {
 		for (Integer age : users.keySet()) {
-			System.out.println(String.format(
-					"Пользователи чей возраст : %d", age));
-			ShowUsers(users.get(age));
+			System.out.println(String.format("Пользователи чей возраст : %d", age));
+			showUsers(users.get(age));
 		}
 		
 		System.out.println();
 	}
 	
-	public static Map<Integer, List<User>> groupUser(
-			List<User> users
-	) {
+	public static Map<Integer, List<User>> groupUser(List<User> users) {
 		Map<Integer, List<User>> groupedUsersByAge = new TreeMap<>();
 		
 		for (User user : users) {
@@ -80,27 +71,18 @@ class User {
 		return groupedUsersByAge;
 	}
 	
-	public static List<User> GetUsersList() {
-		return List.of(new User("Алексей", 30, "Яндекс",
-		                        "г. Москва, ул. Ленина, д. 10"
-		), new User("Мария", 30, "Сбербанк",
-		            "г. Санкт-Петербург, Невский пр., д. 25"
-		), new User("Дмитрий", 18, "Роснефть",
-		            "г. Казань, ул. Баумана, д. 12"
-		), new User("Екатерина", 22, "Лукойл",
-		            "г. Новосибирск, ул. Ленина, д. 5"
-		), new User("Иван", 32, "Газпром",
-		            "г. Екатеринбург, проспект Ленина, д. 20"
-		), new User("Ольга", 30, "РЖД",
-		            "г. Нижний Новгород, ул. Горького, д. 8"
-		), new User("Сергей", 19, "МТС",
-		            "г. Самара, ул. Куйбышева, д. 18"
-		), new User("Анна", 55, "Билайн",
-		            "г. Ростов-на-Дону, ул. Советская, д. 11"
-		), new User("Николай", 20, "Мегафон",
-		            "г. Челябинск, проспект Победы, д. 4"
-		), new User("Вера", 32, "Тинькофф",
-		            "г. Омск, ул. Гагарина, д. 15"
-		));
+	public static List<User> getUsersList() {
+		return List.of(
+				new User("Алексей", 30, "Яндекс", "г. Москва, ул. Ленина, д. 10"),
+				new User("Дмитрий", 18, "Роснефть", "г. Казань, ул. Баумана, д. 12"),
+				new User("Мария", 30, "Сбербанк", "г. Санкт-Петербург, Невский пр., д. 25"),
+				new User("Екатерина", 22, "Лукойл", "г. Новосибирск, ул. Ленина, д. 5"),
+				new User("Иван", 32, "Газпром", "г. Екатеринбург, проспект Ленина, д. 20"),
+				new User("Ольга", 30, "РЖД", "г. Нижний Новгород, ул. Горького, д. 8"),
+				new User("Сергей", 19, "МТС", "г. Самара, ул. Куйбышева, д. 18"),
+				new User("Анна", 55, "Билайн", "г. Ростов-на-Дону, ул. Советская, д. 11"),
+				new User("Николай", 20, "Мегафон", "г. Челябинск, проспект Победы, д. 4"),
+				new User("Вера", 32, "Тинькофф", "г. Омск, ул. Гагарина, д. 15")
+		);
 	}
 }
