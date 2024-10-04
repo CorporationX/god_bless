be7c9d885acf3e2c5a6b593c3bbbca511bb28be5
Pizdja <yakitayka@gmail.com>
2024-10-04 10:17:33 +0300

    I added two static constants and introduced parameter checking into the constructor

--- src/main/java/groupUsers/Main.java
+++ src/main/java/groupUsers/Main.java
@@ -6,14 +6,14 @@ import java.util.List;
 public class Main {
     public static void main(String[] args) {
         List<User> users = new ArrayList<User>();
-        users.add(new User("Коля", 15, "Amazon", "USA"));
-        users.add(new User("Толя", 13, "Amazon", "USA"));
-        users.add(new User("Моля", 13, "Amazon", "USA"));
-        users.add(new User("Ноля", 15, "Amazon", "USA"));
-        users.add(new User("Боля", 15, "Amazon", "USA"));
-        users.add(new User("Оля", 11, "Amazon", "USA"));
-        users.add(new User("Зоя", 25, "Amazon", "USA"));
-        users.add(new User("Поля", 104, "Amazon", "USA"));
+        users.add(new User("Коля", 150, "Google", "London"));
+        users.add(new User("Толя", 130, "Google", "London"));
+        users.add(new User("Моля", 130, "Google", "London"));
+        users.add(new User("Ноля", 150, "Google", "London"));
+        users.add(new User("Боля", 150, "Google", "London"));
+        users.add(new User("Оля", 110, "Google", "London"));
+        users.add(new User("Зоя", 250, "Google", "London"));
+        users.add(new User("Поля", 104, "Google", "London"));
 
         User.groupUsers(users);
     }
--- src/main/java/groupUsers/User.java
+++ src/main/java/groupUsers/User.java
@@ -1,32 +1,52 @@
 package groupUsers;
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.Setter;
+import java.util.*;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-
-@Getter
-@Setter
-@AllArgsConstructor
 public class User {
+    private final static Set<String> VALID_JOBS = new HashSet<String>() {{
+        add("Google");
+        add("Uber");
+        add("Amazon");
+    }};
+    private final static Set<String> VALID_ADDRESSES = new HashSet<String>() {{
+        add("London");
+        add("New York");
+        add("Amsterdam");
+    }};
+
     private String name;
     private int age;
     private String workPlace;
     private String address;
 
+    public User(String name, int age, String workPlace, String address) {
+        if(name == null || name.isEmpty()) {
+            throw new IllegalArgumentException("Имя не может быть пустым");
+        }
+        if (age < 18) {
+            throw new IllegalArgumentException("Возраст не может быть меньше 18");
+        }
+        if (!VALID_JOBS.contains(workPlace)) {
+            throw new IllegalArgumentException("Место работы должно содержаться во множестве VALID_JOBS");
+        }
+        if (!VALID_ADDRESSES.contains(address)) {
+            throw new IllegalArgumentException("Адрес должен содержаться во множестве VALID_ADDRESSES");
+        }
+        this.name = name;
+        this.age = age;
+        this.workPlace = workPlace;
+        this.address = address;
+    }
+
     public static Map<Integer, List<User>> groupUsers(List<User> users) {
         Map<Integer, List<User>> userGroups = new HashMap<>();
 
         for (User user : users) {
-            if (userGroups.containsKey(user.getAge())) {
-                userGroups.get(user.getAge()).add(user);
+            if (userGroups.containsKey(user.age)) {
+                userGroups.get(user.age).add(user);
             } else {
-                userGroups.put(user.getAge(), new ArrayList<User>());
-                userGroups.get(user.getAge()).add(user);
+                userGroups.put(user.age, new ArrayList<User>());
+                userGroups.get(user.age).add(user);
             }
         }
         return userGroups;
