d4c69198b6960f950b3d2595019735583d1adc61
Aliyar <aliartalybov@gmail.com>
2024-10-04 16:01:16 +0300

    https://github.com/CorporationX/god_bless/pull/11055 доработки согласно замечаниям

--- src/main/java/school/faang/collcetUsers/Main.java
+++ src/main/java/school/faang/collcetUsers/Main.java
@@ -4,15 +4,23 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashSet;
 import java.util.List;
+import java.util.Map;
 import java.util.Set;
 
 public class Main {
     public static void main(String[] args) {
+        User user1 = new User(1, "Alice", 30, new HashSet<>(Arrays.asList("Reading", "Swimming")));
+        User user2 = new User(2, "Bob", 25, new HashSet<>(Arrays.asList("Running", "Cycling")));
+        User user3 = new User(3, "Charlie", 28, new HashSet<>(Arrays.asList("Swimming", "Cooking")));
 
-        Set<String> activities1 = new HashSet<>(Arrays.asList("Бег","Ходьба"));
+        List<User> users = List.of(user1, user2, user3);
+        Set<String> hobbies = new HashSet<>(Arrays.asList("Swimming", "Dancing"));
 
-        List<String> activitiesList1 = new ArrayList<>(activities1);
-        User user1 = new User(1, "Алиса", 25, activitiesList1);
-        System.out.println(user1);
+        Map<User, String> hobbyLovers = User.findHobbyLovers(users, hobbies);
+
+        // Вывод результата
+        for (Map.Entry<User, String> entry : hobbyLovers.entrySet()) {
+            System.out.println(entry.getKey().getName() + " "+ entry.getValue());
+        }
     }
 }
--- src/main/java/school/faang/collcetUsers/User.java
+++ src/main/java/school/faang/collcetUsers/User.java
@@ -1,4 +1,5 @@
 package school.faang.collcetUsers;
+
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -6,30 +7,31 @@ import java.util.Set;
 import lombok.Getter;
 import lombok.Setter;
 
-
 @Getter
 @Setter
 public class User {
     private int id;
     private String name;
     private int age;
-    private List<String> activities;
+    private Set<String> activities; // Множество активностей
 
-    public User(int id, String name, int age, List<String> activities) {
+    // Конструктор
+    public User(int id, String name, int age, Set<String> activities) {
         this.id = id;
         this.name = name;
         this.age = age;
         this.activities = activities;
     }
 
+    // Метод для нахождения пользователей с совпадающими активностями
     public static Map<User, String> findHobbyLovers(List<User> users, Set<String> hobbies) {
         Map<User, String> hobbyLovers = new HashMap<>();
 
         for (User user : users) {
             for (String hobby : hobbies) {
                 if (user.getActivities().contains(hobby)) {
-                    hobbyLovers.put(user, user.getName() + " увлекается " + hobby);
-                    break;
+                    hobbyLovers.put(user, hobby); // Сохраняем пользователя и первую совпавшую активность
+                    break; // Прекращаем поиск для данного пользователя
                 }
             }
         }
@@ -45,6 +47,4 @@ public class User {
                 ", activities=" + activities +
                 '}';
     }
-
-
 }
