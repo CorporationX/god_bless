8683b4c01ab35bde8770fd93dd025d099f04fed1
m1rroxxx <Nikita.sidorov.7@icloud.com>
2024-10-04 05:23:01 +0300

    убрал возможность добавлять повторные ивенты, начал пытаться сделать удаление через свой LinkedList

--- src/main/java/school/faang/searchEngine/Main.java
+++ src/main/java/school/faang/searchEngine/Main.java
@@ -6,8 +6,8 @@ public class Main {
     WebPage webPage1 = new WebPage("url1", "page1", "123 asd zxc qwe");
     WebPage webPage2 = new WebPage("url2", "page2", "1233 asdd zxcz qwee");
     WebPage webPage3 = new WebPage("url3", "page3", "1234 asdw zxcz qwez");
-    WebPage webPage4 = new WebPage("url3", "page3", "qwe asdw zxcz 123");
-    WebPage webPage5 = new WebPage("url3", "page3", "1234 asdw 123 qwez");
+    WebPage webPage4 = new WebPage("url4", "page4", "qwe asdw zxcz 123");
+    WebPage webPage5 = new WebPage("url5", "page5", "1234 asdw 123 qwez");
 
     WebPageRepository webPageRepository = new WebPageRepository();
 
@@ -16,13 +16,17 @@ public class Main {
     webPageRepository.indexingPage(webPage3);
     webPageRepository.indexingPage(webPage4);
     webPageRepository.indexingPage(webPage5);
+    webPageRepository.indexingPage(webPage5);
+    webPageRepository.indexingPage(webPage5);
+
+    System.out.println(webPageRepository.getNodesByWords());
 
     System.out.println(webPageRepository.searchPages("123"));
     System.out.println(webPageRepository.searchPages("zxc"));
     System.out.println(webPageRepository.searchPages("qwez"));
 
     webPageRepository.removePage("url2");
-    System.out.println(webPageRepository.searchPages("1233"));
+    System.out.println(webPageRepository.searchPages("1234"));
 
     System.out.println(webPageRepository.getWebPages());
 
--- src/main/java/school/faang/searchEngine/WebPageRepository.java
+++ src/main/java/school/faang/searchEngine/WebPageRepository.java
@@ -1,30 +1,39 @@
 package school.faang.searchEngine;
 
 import lombok.Getter;
+import school.faang.searchEngine.nodes.Node;
 
+import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.LinkedList;
+import java.util.HashSet;
+
+import java.util.List;
 import java.util.Map;
 
 @Getter
 public class WebPageRepository {
 
-  private final Map<String, LinkedList<WebPage>> webPages = new HashMap<>();
+  private final Map<String, List<WebPage>> webPages = new HashMap<>();
+  private final Map<String, List<Node>> nodesByWords = new HashMap<>();
+  private final HashSet<WebPage> noRepetitionsWebPages = new HashSet<>();
 
   public void indexingPage(WebPage page) {
     String[] words = page.getContent().split(" ");
 
     for(String word : words) {
-      webPages.computeIfAbsent(word, k -> new LinkedList<>()).add(page);
+      if(!noRepetitionsWebPages.contains(page)) {
+        webPages.computeIfAbsent(word, k -> new ArrayList<>()).add(page);
+      }
     }
+    noRepetitionsWebPages.add(page);
   }
 
-  public LinkedList<WebPage> searchPages(String keyWord) {
+  public List<WebPage> searchPages(String keyWord) {
     return webPages.get(keyWord);
   }
 
   public void removePage(String url) {
-    for(Map.Entry<String, LinkedList<WebPage>> entry : webPages.entrySet()) {
+    for(Map.Entry<String, List<WebPage>> entry : webPages.entrySet()) {
       entry.getValue().removeIf(page -> page.getUrl().equals(url));
     }
   }
