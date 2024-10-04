b8b1acb35cace23f4eef3ead1841a52172edb4a0
Andrew Balashov <balashov83@gmail.com>
2024-10-04 12:57:44 +0300

    BJS2-32790 Search Engine: fixed namings of constant variables; set regex as constant

--- src/main/java/school/faang/bjs2_32790/Main.java
+++ src/main/java/school/faang/bjs2_32790/Main.java
@@ -6,8 +6,9 @@ import java.util.Map;
 import java.util.Set;
 
 public class Main {
-    private static final Map<String, Set<WebPage>> wordIndex = new HashMap<>();
-    private static final Map<String, WebPage> urlIndex = new HashMap<>();
+    private static final Map<String, Set<WebPage>> WORD_INDEX = new HashMap<>();
+    private static final Map<String, WebPage> URL_INDEX = new HashMap<>();
+    private static final String WORD_SPLITTER_REGEX = "[(\s,.\"!?:';)]+";
 
     public static void main(String[] args) {
 
@@ -17,32 +18,32 @@ public class Main {
         indexWebPage(new WebPage("school.ru", "Stupid camp", "Hello I love world!"));
         indexWebPage(new WebPage("youtube.com", "Puppies video", "Hello cuties"));
 
-        System.out.println(wordIndex);
+        System.out.println(WORD_INDEX);
 
         System.out.println(searchByIndex("HELLO"));
 
         deleteByUrl("faang-school.net");
-        System.out.println(wordIndex);
+        System.out.println(WORD_INDEX);
     }
 
     private static void indexWebPage(WebPage webPage) {
         String url = webPage.getUrl();
-        urlIndex.put(url, webPage);
+        URL_INDEX.put(url, webPage);
 
         String content = webPage.getContent();
-        String[] words = content.toLowerCase().split("[(\s,.\"!?:';)]+");
+        String[] words = content.toLowerCase().split(WORD_SPLITTER_REGEX);
         for (String word : words) {
-            wordIndex.computeIfAbsent(word, key -> new HashSet<>()).add(webPage);
+            WORD_INDEX.computeIfAbsent(word, key -> new HashSet<>()).add(webPage);
         }
     }
 
     private static Set<WebPage> searchByIndex(String word) {
-        return wordIndex.get(word.toLowerCase());
+        return WORD_INDEX.get(word.toLowerCase());
     }
 
     private static void deleteByUrl(String url) {
-        WebPage targetPage = urlIndex.remove(url);
-        for (var entry : wordIndex.entrySet()) {
+        WebPage targetPage = URL_INDEX.remove(url);
+        for (var entry : WORD_INDEX.entrySet()) {
             Set<WebPage> webPageSet = entry.getValue();
             webPageSet.remove(targetPage);
         }
