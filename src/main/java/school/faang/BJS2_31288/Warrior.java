ada568f12a3ac0197eccbf3914293b01dcafbfb8
Sergey Kalinin <serukalinin@gmail.com>
2024-10-04 17:30:37 +0300

    Removed student's code

---
+++ src/main/java/school/faang/BJS2_31288/Warrior.java
@@ -0,0 +1,17 @@
+package school.faang.BJS2_31288;
+
+public class Warrior extends Character{
+
+    public Warrior(String name){
+        super(name,
+            10,
+            5,
+            3);
+    }
+
+    @Override
+    public void attack(Character opponent){
+        System.out.println(name + " атакует " + opponent.name + " с силой " + strength);
+        opponent.takeDamage(strength);
+    }
+}
