cf704a87a551576fc77831c4f5b6cba893de8e04
Dmytro Yevsieienko <youdima70@gmail.com>
2024-10-04 12:37:22 +0200

    Рефакторинг

--- src/test/java/dima/evseenko/weather/WeatherServiceTest.java
+++ src/test/java/dima/evseenko/weather/WeatherServiceTest.java
@@ -2,7 +2,10 @@ package dima.evseenko.weather;
 
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertNull;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 class WeatherServiceTest {
 
