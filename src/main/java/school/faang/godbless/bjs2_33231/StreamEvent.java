9c957cf75f766452255aaf1653a867736357e802
AnnaAplavina <59871896+AnnaAplavina@users.noreply.github.com>
2024-10-04 17:14:43 +0300

    stream events

---
+++ src/main/java/school/faang/godbless/bjs2_33231/StreamEvent.java
@@ -0,0 +1,13 @@
+package school.faang.godbless.bjs2_33231;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class StreamEvent {
+
+  private int id;
+  private String eventType;
+  private String data;
+}
---
+++ src/main/java/school/faang/godbless/bjs2_33231/StreamEventsSystem.java
@@ -0,0 +1,46 @@
+package school.faang.godbless.bjs2_33231;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Map.Entry;
+import java.util.Optional;
+
+public class StreamEventsSystem {
+
+  private final Map<Integer, StreamEvent> idEventMap = new HashMap<>();
+  private final Map<String, List<StreamEvent>> typeEventsListMap = new HashMap<>();
+
+  public void addStreamEvent(StreamEvent event) {
+    idEventMap.put(event.getId(), event);
+    typeEventsListMap.computeIfAbsent(event.getEventType(), key -> new ArrayList<>());
+    typeEventsListMap.get(event.getEventType()).add(event);
+  }
+
+  public Optional<StreamEvent> getById(int id) {
+    return idEventMap.containsKey(id) ? Optional.of(idEventMap.get(id)) : Optional.empty();
+  }
+
+  public Optional<List<StreamEvent>> getByType(String type) {
+    return typeEventsListMap.containsKey(type) ? Optional.of(typeEventsListMap.get(type)) : Optional.empty();
+  }
+
+  public boolean removeById(int id) {
+    StreamEvent eventToDelete = idEventMap.get(id);
+    if (eventToDelete == null) {
+      return false;
+    }
+    idEventMap.remove(id);
+    return typeEventsListMap.get(eventToDelete.getEventType()).remove(eventToDelete);
+  }
+
+  public String getEventsAsString() {
+    StringBuilder sb = new StringBuilder();
+    //idEventMap.forEach((key, event) -> sb.append(event));
+    for (Entry<Integer, StreamEvent> entry : idEventMap.entrySet()) {
+      sb.append(entry.getValue());
+    }
+    return sb.toString();
+  }
+}
