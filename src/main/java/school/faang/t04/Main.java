9f5c85f6c73cd5bf111168acbf5c15786a4209dd
Igor <asachiyigor@gmail.com>
2024-10-04 17:25:12 +0300

    BJS2-33014
    refactoring code

--- src/main/java/school/faang/t04/Main.java
+++ src/main/java/school/faang/t04/Main.java
@@ -6,6 +6,22 @@ import java.util.Map;
 public class Main {
     private static final HashMap<Book, String> BOOKS = new HashMap<>();
 
+    public static void main(String[] args) {
+        Book bookOne = new Book("Philosopher's Stone", "J.K. Rowling", 2015);
+        Book bookTwo = new Book("The Wonder of Nature", "Natural History Museum", 2020);
+        Book bookThree = new Book("Fantastic Beasts", "J.K. Rowling, Newt Scamander", 2017);
+
+        add(bookOne, "1 полка 2 ряд");
+        add(bookTwo, "3 полка 1 ряд");
+        add(bookThree, "5 полка 4 ряд");
+
+        System.out.println("After add : " + BOOKS);
+        remove("Philosopher's Stone", "J.K. Rowling", 2015);
+        System.out.println("After remove : " + BOOKS);
+        findAndGetInfo("The Wonder of Nature", "Natural History Museum", 2020);
+        printAll();
+    }
+
     private static void add(Book book, String place) {
         BOOKS.put(book, place);
     }
@@ -22,21 +38,5 @@ public class Main {
         for (Map.Entry<Book, String> entry : BOOKS.entrySet()) {
             System.out.println(entry.getKey() + ": " + entry.getValue());
         }
-    }
-
-    public static void main(String[] args) {
-        Book bookOne = new Book("Philosopher's Stone", "J.K. Rowling", 2015);
-        Book bookTwo = new Book("The Wonder of Nature", "Natural History Museum", 2020);
-        Book bookThree = new Book("Fantastic Beasts", "J.K. Rowling, Newt Scamander", 2017);
-
-        add(bookOne, "1 полка 2 ряд");
-        add(bookTwo, "3 полка 1 ряд");
-        add(bookThree, "5 полка 4 ряд");
-
-        System.out.println("After add : " + BOOKS);
-        remove("Philosopher's Stone", "J.K. Rowling", 2015);
-        System.out.println("After remove : " + BOOKS);
-        findAndGetInfo("The Wonder of Nature", "Natural History Museum", 2020);
-        printAll();
     }
 }
