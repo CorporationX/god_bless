749b6ae92570b30c62baa04f28338b5efd6c277b
Doniyor Kurbanov <doniyor.kurbanov.24@gmail.com>
2024-10-04 09:29:18 +0500

    Готово, проверьте пожалуйста

--- src/main/java/school/faang/solution/userSignUp/User.java
+++ src/main/java/school/faang/solution/userSignUp/User.java
@@ -1,18 +1,16 @@
 package school.faang.solution.userSignUp;
 
-
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
 import java.util.stream.Collectors;
 
 public class User {
+    private static final int ELIGIBILITY_AGE = 18;
     private String name;
     private int userAge;
     private String company;
     private String address;
-    private static final int ELIGIBILITY_AGE = 18;
-
     private static final Set<String> VALID_JOBS = Set.of("Google", "Uber", "Amazon");
     private static final Set<String> VALID_ADDRESS = Set.of("London", "New York", "Amsterdam");
 
@@ -20,31 +18,32 @@ public class User {
     public User(String name, int userAge, String company, String address) {
         if (name.isEmpty() || name.isBlank()) {
             throw new IllegalArgumentException("Name can not be empty");
-        } else {
-            this.name = name;
         }
 
         if (userAge < ELIGIBILITY_AGE) {
             throw new IllegalArgumentException("Age can not be less than 18");
-        } else {
-            this.userAge = userAge;
         }
 
+        boolean validCompany = false;
         for (String job : VALID_JOBS) {
-            if (!company.equalsIgnoreCase(job)) {
-                throw new IllegalArgumentException("Invalid job");
-            } else {
-                this.company = company;
+            if (company.equalsIgnoreCase(job)) {
+                validCompany = true;
+                break;
             }
+        }
+
+        if (!validCompany) {
+            throw new IllegalArgumentException("Invalid job");
         }
 
         if (!VALID_ADDRESS.contains(address)) {
             throw new IllegalArgumentException("Invalid address");
-        } else {
-            this.address = address;
         }
 
-
+        this.name = name;
+        this.userAge = userAge;
+        this.company = company;
+        this.address = address;
     }
 
     public int getAge() {
