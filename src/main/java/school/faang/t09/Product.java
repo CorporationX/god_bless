212e1f4bc324c6d9e21d60e2c01e75cdbd220184
Igor <asachiyigor@gmail.com>
2024-10-04 16:16:41 +0300

    BJS2-32281
    refactoring code

--- src/main/java/school/faang/t09/Product.java
+++ src/main/java/school/faang/t09/Product.java
@@ -1,8 +1,10 @@
 package school.faang.t09;
 
-@lombok.Data
-@lombok.AllArgsConstructor
+import lombok.AllArgsConstructor;
+import lombok.Data;
 
+@Data
+@AllArgsConstructor
 public class Product {
     private int id;
     private String name;
