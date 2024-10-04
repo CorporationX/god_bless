838c99ce60f4f8777e9343178c8e3013f4c5f6a4
m1rroxxx <Nikita.sidorov.7@icloud.com>
2024-10-04 04:31:14 +0300

    add repository stream events

--- src/main/java/school/faang/catchingEvents/Main.java
+++ src/main/java/school/faang/catchingEvents/Main.java
@@ -1,34 +1,30 @@
 package school.faang.catchingEvents;
 
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-
 public class Main {
   public static void main(String[] args) {
-    Map<Integer, StreamEvent> events = new HashMap<>();
-    Map<String, List<StreamEvent>> groupEvents = new HashMap<>();
 
-    StreamEvent.addStreamEvent(new StreamEvent(0, "2", "12312"), events, groupEvents);
-    StreamEvent.addStreamEvent(new StreamEvent(1, "2", "12321313"), events, groupEvents);
-    StreamEvent.addStreamEvent(new StreamEvent(2, "23", "12321313"), events, groupEvents);
-    StreamEvent.addStreamEvent(new StreamEvent(3, "23", "1111"), events, groupEvents);
+    StreamEventRepository streamEventRepository = new StreamEventRepository();
 
-    System.out.println(events);
-    System.out.println(groupEvents);
+    streamEventRepository.addStreamEvent(new StreamEvent(0, "2", "12312"));
+    streamEventRepository.addStreamEvent(new StreamEvent(1, "2", "12321313"));
+    streamEventRepository.addStreamEvent(new StreamEvent(2, "23", "12321313"));
+    streamEventRepository.addStreamEvent(new StreamEvent(3, "23", "1111"));
 
-    System.out.println("=============");
-
-    System.out.println(StreamEvent.findStreamEventById(3, events));
-    System.out.println(StreamEvent.findArraysStreamEventByEventType("23", groupEvents));
-
-    StreamEvent.deleteStreamEvent(2, events, groupEvents);
-    StreamEvent.deleteStreamEvent(0, events, groupEvents);
+    System.out.println(streamEventRepository.getEvents());
+    System.out.println(streamEventRepository.getGroupEvents());
 
     System.out.println("=============");
 
-    System.out.println(events);
-    System.out.println(groupEvents);
+    System.out.println(streamEventRepository.findStreamEventById(3));
+    System.out.println(streamEventRepository.findArraysStreamEventByEventType("23"));
+
+    streamEventRepository.deleteStreamEvent(2);
+    streamEventRepository.deleteStreamEvent(0);
+
+    System.out.println("=============");
+
+    System.out.println(streamEventRepository.getEvents());
+    System.out.println(streamEventRepository.getGroupEvents());
 
 
 
