724e4250a34444dc094332bd8e4452c67ee80217
bogda <bogdanskrip404@gmail.com>
2024-10-04 14:45:05 +0500

    Библиотека Вестероса

---
+++ src/main/java/school/faang/Book.java
@@ -0,0 +1,56 @@
+package school.faang;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+import java.util.Map;
+import java.util.Objects;
+
+@Data
+@AllArgsConstructor
+public class Book {
+    private String title;
+    private String author;
+    private int year;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Book book = (Book) o;
+        return year == book.year && Objects.equals(title, book.title) && Objects.equals(author, book.author);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(title, author, year);
+    }
+
+    // 1.Добавление новой книги
+    public static void addBook(Book newBook, String place, Map<Book, String> library){
+        library.put(newBook, place);
+    }
+
+    // 2.Удаление по названию, автору и году
+    public static void deleteBook(String title, String author, int year, Map<Book, String> library){
+        library.remove(new Book(title, author, year));
+    }
+
+    // поиск книги и вывод её местонахождения по названию, автору и году
+    public static void searchBook(String title, String author, int year, Map<Book, String> library){
+        Book book = new Book(title, author, year);
+        if(library.containsKey(book)){
+            System.out.println(book.getTitle() + " находится на полке №" + library.get(book));
+        } else {
+            System.out.println("Книга: " + book.getTitle() + " не найдена.");
+        }
+    }
+
+    //Вывод списка всех книг и местонахождения
+    public static void allBooksInfo(Map<Book, String> library){
+        for (Map.Entry<Book, String> entry: library.entrySet()){
+            searchBook(entry.getKey().getTitle(), entry.getKey().getAuthor(),  entry.getKey().getYear(),
+                    library);
+        }
+    }
+}
--- src/main/java/school/faang/Main.java
+++ src/main/java/school/faang/Main.java
@@ -1,4 +1,25 @@
 package school.faang;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class Main {
+    public static void main(String[] args) {
+        Map<Book, String> library = new HashMap<>();
+        library.put(new Book("book1", "author1", 1975), "1");
+        library.put(new Book("book2", "author2", 2005), "2");
+        library.put(new Book("book3", "author3", 2024), "3");
+
+        // 1.Добавление новой книги
+        Book.addBook(new Book("book4", "author4", 2000), "4", library);
+        Book.allBooksInfo(library);
+        System.out.println("\nПоиск book4:");
+        Book.searchBook("book4", "author4", 2000, library);
+
+        // 2.Удаление по названию, автору и году
+        Book.deleteBook("book4", "author4", 2000, library);
+        System.out.println("\nПоиск удаленной book4:");
+        Book.searchBook("book4", "author4", 2000, library);
+        Book.allBooksInfo(library);
+    }
 }
