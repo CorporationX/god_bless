ad5cbe66ae67ed865c520cfb201a7b0a96f40b1b
Roman Nifontov <romannif@mail.ru>
2024-10-04 19:02:48 +0500

    Сделал изменения к задаче BJS2-29000

--- src/main/java/school/BJS2_29000/User.java
+++ src/main/java/school/BJS2_29000/User.java
@@ -6,21 +6,14 @@ import java.util.*;
 @Getter
 public class User {
 
+    public final static String[] VALID_JOBS = { "Google", "Uber", "Amazon"};
+    public final static String[] VALID_ADDRESSES = { "London", "New York", "Amsterdam"};
+    public final static int minAvailableAgeForUser = 18;
+
     private String name;
     private int age;
     private String job;
     private String address;
-
-    public final static String[] VALID_JOBS = { "Google", "Uber", "Amazon"};
-    public final static String[] VALID_ADDRESSES = { "London", "New York", "Amsterdam"};
-
-    public static Map<Integer, List<User>> groupUsers(List<User> users) {
-        Map<Integer, List<User>> userGroups = new HashMap<>();
-        for (User user : users) {
-            userGroups.computeIfAbsent(user.age, k -> new ArrayList<>()).add(user);
-        }
-        return userGroups;
-    }
 
     /* имя не может быть пустым;
        возраст не может быть меньше 18;
@@ -28,13 +21,29 @@ public class User {
        адрес должен содержаться во множестве VALID_ADDRESSES.*/
     public User(String name, String address, String job, int age) {
         if(name == null) throw new IllegalArgumentException("name is null");
-        else this.name = name;
-        if(age < 18) throw new IllegalArgumentException("age is smaller than 18");
-        else this.age = age;
-        if(Arrays.stream(VALID_ADDRESSES).noneMatch(a -> a.equals(address))) throw new IllegalArgumentException("address is invalid");
-        else this.address = address;
-        if(Arrays.stream(VALID_JOBS).noneMatch(j -> j.equals(job))) throw new IllegalArgumentException("job is invalid");
-        else this.job = job;
+        else if(age < minAvailableAgeForUser) throw new IllegalArgumentException("age is smaller than 18");
+        else if(Arrays.stream(VALID_ADDRESSES).noneMatch(a -> a.equals(address))) throw new IllegalArgumentException("address is invalid");
+        else if(Arrays.stream(VALID_JOBS).noneMatch(j -> j.equals(job))) throw new IllegalArgumentException("job is invalid");
+        //Можно сделать так, но будет обобщенный Exception
+        /*
+        if (name == null ||
+             age < minAvailableAgeForUser ||
+                 Arrays.stream(VALID_ADDRESSES).noneMatch(a -> a.equals(address)) ||
+                    Arrays.stream(VALID_JOBS).noneMatch(j -> j.equals(job))) throw new IllegalArgumentException("data is invalid");
+         */
+        else {
+        this.address = address;
+        this.age = age;
+        this.name = name;
+        this.job = job; }
+    }
+
+    public static Map<Integer, List<User>> groupUsers(List<User> users) {
+        Map<Integer, List<User>> userGroups = new HashMap<>();
+        for (User user : users) {
+            userGroups.computeIfAbsent(user.age, k -> new ArrayList<>()).add(user);
+        }
+        return userGroups;
     }
 
     @Override
