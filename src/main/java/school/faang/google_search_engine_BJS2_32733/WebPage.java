72cf13e45353d575ddbdfc6a2835cccd98aa5c5a
VovaBuzivskyi <VovaBuzivskyi@gmail.com>
2024-10-04 21:30:15 +0700

    BJS2-32733 google search engine

---
+++ src/main/java/school/faang/google_search_engine_BJS2_32733/WebPage.java
@@ -0,0 +1,12 @@
+package school.faang.google_search_engine_BJS2_32733;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class WebPage {
+    private String url;
+    private String title;
+    private String content;
+}
