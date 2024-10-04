bbeba2606c7b26ae2060321b0c7886bf2abd5431
Slava <leeteso97@gmail.com>
2024-10-04 22:25:04 +0500

    Внес корректировки в классы и конструкторы

--- src/main/java/school/faang/Abstract/Archer.java
+++ src/main/java/school/faang/Abstract/Archer.java
@@ -2,14 +2,11 @@ package school.faang.Abstract;
 
 public class Archer extends Character {
     public Archer(String name) {
-        super(name);
-        strength = 3;
-        agility = 10;
-        intelligence = 5;
+        super(name, 3, 10, 5);
     }
 
     @Override
     public void attack(Character enemy) {
-        enemy.setHealth(enemy.getHealth() - agility);
+        enemy.health -= agility;
     }
 }
--- src/main/java/school/faang/Abstract/Warrior.java
+++ src/main/java/school/faang/Abstract/Warrior.java
@@ -2,14 +2,11 @@ package school.faang.Abstract;
 
 public class Warrior extends Character {
     public Warrior(String name) {
-        super(name);
-        strength = 10;
-        agility = 5;
-        intelligence = 3;
+        super(name, 10, 5, 3);
     }
 
     @Override
     public void attack(Character enemy) {
-        enemy.setHealth(enemy.getHealth() - strength);
+        enemy.health -= strength;
     }
 }
