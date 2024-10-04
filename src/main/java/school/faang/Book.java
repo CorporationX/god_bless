464081634d941e5fc191732b8613d586f89b0b2f
Matvey <mashptz@gmail.com>
2024-10-04 17:07:02 +0300

    create a new class and hashmap and wrote and implemented methods to work with them

---
+++ src/main/java/school/faang/Book.java
@@ -0,0 +1,61 @@
+package school.faang;
+import java.util.Objects;
+
+public class Book {
+    private String title;
+    private String author;
+    private int year;
+
+
+
+    public Book(String author, int year, String title) {
+        this.author = author;
+        this.year = year;
+        this.title = title;
+    }
+
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+
+    @Override
+    public int hashCode(){
+        return Objects.hash(author, title, year);
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null || getClass() != obj.getClass()) {
+            return false;
+        }
+        Book book = (Book) obj;
+        return year == book.year &&
+                Objects.equals(author, book.author) &&
+                Objects.equals(title, book.title);
+
+    }
+}
--- src/main/java/school/faang/Main.java
+++ src/main/java/school/faang/Main.java
@@ -1,4 +1,47 @@
 package school.faang;
 
+import java.util.Map;
+import java.util.HashMap;
+
 public class Main {
+    public static void main(String[] args) {
+        String bookshelf;
+
+        Map<Book, String> bookMap = new HashMap<>();
+        bookMap.put(new Book("Rowling", 1997, "harryPotter"), "5");
+        bookMap.put(new Book("Pushkin", 1833, "theQueenOfSpades"), "1");
+        bookMap.put(new Book("Dahl", 1964, "theChocolateFactory"), "3");
+
+        addNewBook(bookMap, "5");
+
+        deleteBook(bookMap, "Pushkin", 1833, "theQueenOfSpades");
+
+        searchBook(bookMap, "Dahl", 1964, "theChocolateFactory");
+
+        getAllBooks(bookMap);
+
+    }
+
+    public static void addNewBook(Map<Book, String> bookMap, String bookshelf) {
+        bookMap.put(new Book("Gosling", 2049, "gostRunner"), "5");
+    }
+
+    public static void deleteBook(Map<Book, String> bookMap, String author,
+                                  int year, String title) {
+        Book bookToDelete = new Book(author, year, title);
+        bookMap.remove(bookToDelete);
+    }
+
+    public static void searchBook(Map<Book, String> bookMap, String author,
+                                  int year, String title) {
+        Book bookToSearch = new Book(author, year, title);
+        System.out.println(bookMap.get(bookMap.containsKey(bookToSearch)));
+    }
+
+    public static void getAllBooks(Map<Book, String> bookMap) {
+        for (Map.Entry<Book, String> entry : bookMap.entrySet()) {
+            System.out.println(entry.getKey() + ":" + entry.getValue());
+        }
+    }
+
 }
