c9f2b1c932f9d872186acaf2495ffedfce25c69b
AlekseyB86nb <tigrxam@gmail.com>
2024-10-04 13:06:49 +0300

    реализация добавления события и вывод всех событий

--- src/main/java/catchEvents/Main.java
+++ src/main/java/catchEvents/Main.java
@@ -1,8 +1,51 @@
 package catchEvents;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 public class Main {
+    private static final Map<Integer, StreamEvent> EVENTS = new HashMap<>();
+    private static final Map<String, List<StreamEvent>> EVENTS_BY_TYPE = new HashMap<>();
 
     public static void main(String[] args) {
 
+        addEvent("event1", "data1");
+        addEvent("event2", "data2");
+        addEvent("event1", "data3");
+        addEvent("event3", "data4");
+        addEvent("event3", "data5");
+
+        printAllStreamEvents();
+
+
     }
+
+    public static void addEvent(String eventType, String data) {
+        StreamEvent event = new StreamEvent(eventType, data);
+        EVENTS.putIfAbsent(event.getId(), event);
+        EVENTS_BY_TYPE.computeIfAbsent(eventType, k -> new ArrayList<>()).add(event);
+    }
+
+    public static StreamEvent findEventByID(int id) {
+        return null;
+    }
+
+    public static List<StreamEvent> findEventsByType(String eventType) {
+        return null;
+    }
+
+    public static void removeEvent(int id) {
+    }
+
+    public static void printAllStreamEvents(){
+        for(Map.Entry<Integer, StreamEvent> entry : EVENTS.entrySet()) {
+            System.out.println(
+                    "ID: " + entry.getKey() +
+                    ", тип события: " + entry.getValue().getEventType() +
+                    ", данные: " + entry.getValue().getData());
+        }
+    }
+
 }
