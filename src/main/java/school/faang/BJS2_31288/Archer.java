1314bf9c6597db0449ca2fa71035d36732076b10
Sergey Kalinin <serukalinin@gmail.com>
2024-10-04 18:01:51 +0300

    Removed student's code

---
+++ src/main/java/school/faang/BJS2_31288/Archer.java
@@ -0,0 +1,17 @@
+package school.faang.BJS2_31288;
+
+public class Archer extends Character{
+
+    public Archer(String name) {
+        super(name,
+            3,
+            10,
+            5);
+    }
+
+    @Override
+    public void attack(Character opponent) {
+        System.out.println(name + " атакует " + opponent.name + " с ловкостью" + agility);
+        opponent.takeDamage(agility);
+    }
+}
