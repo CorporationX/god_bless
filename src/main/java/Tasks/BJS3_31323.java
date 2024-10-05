68a97391589f0b98f8d9b833b5d83bc5fa278299
Lexsandr <lexsandr@inbox.ru>
2024-10-05 14:09:08 +0300

    Revert "BJS2_31377 : Changed the architecture app"
    This reverts commit 89a81760d25bfd0b9114b00588a99d701ed8288a.

---
+++ src/main/java/Tasks/BJS3_31323.java
@@ -0,0 +1,81 @@
+package Tasks;
+
+public class BJS3_31323
+{
+	public void run()
+	{
+		Warrior warrior = new Warrior("Грек-Хак");
+		Archer archer = new Archer("Джелу");
+		
+		warrior.attack(archer);
+		archer.attack(warrior);
+	}
+}
+
+interface Defensible {
+	String getName();
+	void takeDamage(int damage);
+}
+
+abstract class Character implements Defensible {
+	protected String name;
+	protected int strength;
+	protected int agility;
+	private int intellect;
+	private int health;
+	
+	private static final int DEFAULT_HEALTH = 100;
+	
+	public Character(String name, int strength, int agility, int intellect) {
+		this.name = name;
+		this.strength = strength;
+		this.agility = agility;
+		this.intellect = intellect;
+		this.health = DEFAULT_HEALTH;
+	}
+	
+	public Character(String name) {
+		this.name = name;
+		this.health = DEFAULT_HEALTH;
+	}
+	
+	public String getName() {
+		return name;
+	}
+	
+	public abstract void attack(Defensible defensible);
+	
+	public void takeDamage(int damage) {
+		this.health -= damage;
+		System.out.println(this.name + " получил " + damage + " урона. Оставшееся здоровье: " + this.health);
+	}
+	
+	protected void showAttackInformation(String enemyName, int damage)
+	{
+		System.out.println(String.format("%s атакует %s нанося урон : %s",this.name, enemyName, damage));
+	}
+}
+
+class Warrior extends Character {
+	public Warrior(String name) {
+		super(name, 10, 5, 3);
+	}
+	
+	@Override
+	public void attack(Defensible defensible) {
+		showAttackInformation(defensible.getName(), this.strength);
+		defensible.takeDamage(this.strength);
+	}
+}
+
+class Archer extends Character {
+	public Archer(String name) {
+		super(name, 3, 11, 5);
+	}
+	
+	@Override
+	public void attack(Defensible defensible) {
+		showAttackInformation(defensible.getName(), this.agility);
+		defensible.takeDamage(this.agility);
+	}
+}
--- src/main/java/school/faang/Main.java
+++ src/main/java/school/faang/Main.java
@@ -1,6 +1,8 @@
 package school.faang;
 
 public class Main {
-    public static void main(String[] args) {
-    }
+	public static void main(String[] args) {
+		//new Tasks.BJS2_31377().run(); //Группировка пользователей по возрасту
+		new Tasks.BJS3_31323().run(); //Абстракция, абстракция!
+	}
 }
