35c6a038ff9c8645efbe79590c380ee801a2a27c
AnnaAplavina <59871896+AnnaAplavina@users.noreply.github.com>
2024-10-05 12:24:38 +0300

    students

---
+++ src/main/java/school/faang/godbless/bjs2_32485/FacultyYearKey.java
@@ -0,0 +1,12 @@
+package school.faang.godbless.bjs2_32485;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class FacultyYearKey {
+
+  private String faculty;
+  private int year;
+}
