f1bd1940c50818ddd986089a430d2f11d10d9720
Aleksandr Dainiak <a.dainiak@arammeem.com>
2024-10-05 19:24:51 +0200

    BJS2-31510 Развернитесь!

--- src/main/java/school/faang/Main.java
+++ src/main/java/school/faang/Main.java
@@ -1,4 +1,14 @@
 package school.faang;
 
+import school.faang.thirdTask.Example;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
+    public static void main(String[] args) {
+        List<Integer> numbers = new ArrayList<>(List.of(1, 2, 3, 4, 5));
+        List result = Example.reverse(numbers);
+        System.out.println("ArrayList reversed : " + result);
+    }
 }
