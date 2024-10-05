18415632a681281e62a43fc32e2c76c5191299a3
Alexander Litovkin <litovkinalexan@gmail.com>
2024-10-05 22:02:34 +0700

    refactor WebPage

--- src/main/java/school/faang/searchengine/WebPage.java
+++ src/main/java/school/faang/searchengine/WebPage.java
@@ -3,25 +3,11 @@ package school.faang.searchengine;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 
-import java.util.Objects;
-
 @Data
 @AllArgsConstructor
 public class WebPage {
 
-    private final String url;
+    private String url;
     private String title;
     private String content;
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (!(o instanceof WebPage webPage)) return false;
-        return Objects.equals(url, webPage.url);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hashCode(url);
-    }
 }
