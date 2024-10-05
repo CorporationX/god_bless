87f4f805815a63240cdef1223d9482072a1e1da6
Doniyor Kurbanov <doniyor.kurbanov.24@gmail.com>
2024-10-05 18:17:56 +0500

    Проверьте пожалуйста. Подправил так как вы сказали

--- src/main/java/school/faang/CollectUsers/BJS231489/User.java
+++ src/main/java/school/faang/CollectUsers/BJS231489/User.java
@@ -1,24 +1,29 @@
 package school.faang.CollectUsers.BJS231489;
 
+import lombok.Data;
+
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
 
+@Data
 public class User {
     private int userId;
     private String userName;
     private int userAge;
     private Set<String> activities = new HashSet<>();
 
-    private Map<User, String> findHobbyLovers(List<User> users, HashSet<String> strings) {
+    private Map<User, String> findHobbyLovers(List<User> users, HashSet<String> hobbies) {
         Map<User, String> map = new HashMap<>();
+
         for (User user : users) {
-            Set<String> userActivities = new HashSet<>(user.activities);
-            userActivities.retainAll(strings);
-            if (!userActivities.isEmpty()) {
-                map.put(user, userActivities.iterator().next());
+            for (String activity: user.getActivities()) {
+                if (hobbies.contains(activity)) {
+                    map.put(user, activity);
+                    break;
+                }
             }
         }
         return map;
