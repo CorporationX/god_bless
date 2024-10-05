bf9726bd68750f9277dc1fb8fad80c9bde5e7871
VovaBuzivskyi <VovaBuzivskyi@gmail.com>
2024-10-06 02:37:06 +0700

    BJS2-32584 cache cache

---
+++ src/main/java/school/faang/cache_cache_BJS2_32584/WeatherData.java
@@ -0,0 +1,12 @@
+package school.faang.cache_cache_BJS2_32584;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class WeatherData {
+    private String city;
+    private double temperature;
+    private double humidity;
+}
---
+++ src/main/java/school/faang/cache_cache_BJS2_32584/WeatherSource.java
@@ -0,0 +1,8 @@
+package school.faang.cache_cache_BJS2_32584;
+
+public class WeatherSource {
+
+    public WeatherData fetchWeather(String city){
+       return new WeatherData(city,24,47);
+    }
+}
