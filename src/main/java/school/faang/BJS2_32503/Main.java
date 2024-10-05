4afaca2a2ff758cec6e3f9edd614bd529925a5aa
Maxim Gorobets <gorobets.maks@bk.ru>
2024-10-06 02:16:59 +1000

    new Student class

---
+++ src/main/java/school/faang/BJS2_32503/Main.java
@@ -0,0 +1,4 @@
+package school.faang.BJS2_32503;
+
+public class Main {
+}
---
+++ src/main/java/school/faang/BJS2_32503/Student.java
@@ -0,0 +1,14 @@
+package school.faang.BJS2_32503;
+
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+
+@EqualsAndHashCode
+@Getter
+@AllArgsConstructor
+public class Student {
+    private String name;
+    private String faculty;
+    private int year;
+}
