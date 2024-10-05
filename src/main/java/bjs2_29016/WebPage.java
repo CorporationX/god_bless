933cbfd030d96142e1f63a40490f762d0359f4c8
Sanasar1 <almasyansb@gmail.com>
2024-10-05 14:29:29 +0300

    BJS2-32829: Поисковый мини-движок Google

---
+++ src/main/java/bjs2_29016/WebPage.java
@@ -0,0 +1,14 @@
+package bjs2_29016;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.ToString;
+
+@Getter
+@AllArgsConstructor
+@ToString
+public class WebPage {
+    private String url;
+    private String title;
+    private String content;
+}
