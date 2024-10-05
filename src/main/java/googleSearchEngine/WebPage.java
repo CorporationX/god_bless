8a9f3454d755aeba004b1a939d72276ad8637311
Denis Levkhov <levkhovde@yandex.ru>
2024-10-05 20:13:27 +0300

    Google search engine

---
+++ src/main/java/googleSearchEngine/WebPage.java
@@ -0,0 +1,13 @@
+package googleSearchEngine;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class WebPage {
+    private String url;
+    private String title;
+    private String content;
+}
+
