29468cb61ee6fd9a1e9c307325628b0f1a477256
sars33 <sarsenyev@hotmail.com>
2024-10-05 08:06:36 -0700

    Completed a new taask BJS2-31324 and ready for review

--- src/main/java/school/faang/Main.java
+++ src/main/java/school/faang/Main.java
@@ -23,5 +23,13 @@ public class Main {
         );
 
         System.out.println(User.groupUsers(userList).get(40));
+
+        Warrior warrior = new Warrior("Peter", 0, 0, 0);
+        Archer archer = new Archer("John", 0, 0, 0);
+        warrior.attack(archer);
+        archer.attack(warrior);
+        System.out.println(warrior.name + " is attacked you and you have  " + archer.health + " % left");
+        System.out.println(archer.name + " is attacked you and you have  " + warrior.health + " % left");
     }
 }
+
