c29db451611f5c9f5673a0e0f301fedc295499d0
lomuserg <sergeymu2015@yandex.ru>
2024-10-05 16:28:24 +0300

    Validation for: name, age, job, address

--- src/main/java/school/faang/Main.java
+++ src/main/java/school/faang/Main.java
@@ -1,27 +1,23 @@
 package school.faang;
 
 import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.Scanner;
 
 public class Main {
     public static List<User> usersList = new ArrayList<>();
 
     public static void main(String[] args) {
-        User user = new User("Sergey", "21", "Student", "Moscow");
-        User user2 = new User("Dmitry", "21", "Programmer", "Moscow");
-        User user3 = new User("Yakov", "20", "Student", "Moscow");
-        User user4 = new User("Michail", "21", "Programmer", "Moscow");
+        User user = new User("", "21", "Uber", "Amsterdam");//incorrect name
+        User user2 = new User("Dmitry", "12", "Google", "London");//incorrect age
+        User user3 = new User("Yakov", "20", "Amazon", "Moscow");//incorrect address
+        User user4 = new User("Michail", "21", "Programmer", "New York");//incorrect job
         usersList.add(user);
         usersList.add(user2);
         usersList.add(user3);
         usersList.add(user4);
         Map<String, List<User>> mapByAge = user.groupUsers(usersList);
         System.out.println(mapByAge.get("21"));
-
     }
 
 }
--- src/main/java/school/faang/User.java
+++ src/main/java/school/faang/User.java
@@ -12,20 +12,33 @@ import java.util.Set;
 
 @Data
 public class User {
-    private static Set <String> VALID_JOBS = new HashSet<>(Arrays.asList("Google", "Uber", "Amazon"));
+    private static Set<String> VALID_JOBS = new HashSet<>(Arrays.asList("Google", "Uber", "Amazon"));
     private static Set<String> VALID_ADDRESSES = new HashSet<>(Arrays.asList("London", "New York", "Amsterdam"));
     private String name;
     private String age;
     private String job;
     private String address;
 
-    // private void jobValidation()throws jobValidationException{}
+    private void Validation(String name, String age, String job, String address) throws IllegalArgumentException {
+        if (name.equals("")) throw new IllegalArgumentException("Uncorrected name");
+        if ((Integer.parseInt(age) < 18) || (Integer.parseInt(age) > 100))
+            throw new IllegalArgumentException("Uncorrected age");
+        if (!VALID_JOBS.contains(job))
+            throw new IllegalArgumentException("Uncorrected job");
+        if (!VALID_ADDRESSES.contains(address))
+            throw new IllegalArgumentException("Uncorrected address");
+    }
 
     public User(String name, String age, String job, String address) {
-        this.name = name;
-        this.age = age;
-        this.job = job;
-        this.address = address;
+        try {
+            Validation(name, age, job, address);
+            this.name = name;
+            this.age = age;
+            this.job = job;
+            this.address = address;
+        } catch (IllegalArgumentException e) {
+            System.out.println(e);
+        }
     }
 
     public static Map<String, List<User>> groupUsers(List<User> users) {
@@ -34,7 +47,6 @@ public class User {
             mapByAge.computeIfAbsent(user.age, k -> new ArrayList<>()).add(user);
         }
         return mapByAge;
-
     }
 
 }
