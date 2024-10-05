7e2d16e594743614dd637e12dca925fc7cde78f1
Lexsandr <lexsandr@inbox.ru>
2024-10-05 14:21:36 +0300

    BJS2_31323 : return BJS2_31377 and Changed the architecture BJS2_31323

--- src/main/java/school/faang/Main.java
+++ src/main/java/school/faang/Main.java
@@ -2,7 +2,5 @@ package school.faang;
 
 public class Main {
 	public static void main(String[] args) {
-		//new Tasks.BJS2_31377().run(); //Группировка пользователей по возрасту
-		new Tasks.BJS3_31323().run(); //Абстракция, абстракция!
 	}
 }
