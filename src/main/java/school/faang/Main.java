5550f8c06646cd49e4f21f2e235f7010a9a60d34
Denis Ignatenko <d.i.ignatenko@gmail.com>
2024-10-05 16:44:58 +0300

    preparation to next task

--- src/main/java/school/faang/Main.java
+++ src/main/java/school/faang/Main.java
@@ -2,15 +2,6 @@ package school.faang;
 
 public class Main {
     public static void main(String[] args) {
-        Warrior warrior = new Warrior("Гимли");
-        Archer archer = new Archer("Леголас");
 
-        System.out.println("\nБитва начинается!");
-        warrior.attack(archer);
-        archer.attack(warrior);
-
-        System.out.println("\nСостояние персонажей после атаки:");
-        System.out.println(warrior.getName() + ": " + warrior.getHealth() + " здоровья");
-        System.out.println(archer.getName() + ": " + archer.getHealth() + " здоровья");
     }
 }
