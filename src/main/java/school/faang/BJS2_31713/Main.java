7f0a1d54c8ce5a12c29e95ab911c1180e08cc53e
Aliyar <aliartalybov@gmail.com>
2024-10-05 13:24:40 +0300

    Исправленный коммит, задача BJS2-31713

---
+++ src/main/java/school/faang/BJS2_31713/Main.java
@@ -0,0 +1,18 @@
+package school.faang.BJS2_31713;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+public class Main {
+    public static void main(String[] args) {
+        Set<String> activities1 = new HashSet<>(Arrays.asList("Бег","Ходьба"));
+
+        List<String> activitiesList1 = new ArrayList<>(activities1);
+        User user1 = new User(1, "Alisa", 25, activitiesList1, "Google", "London");
+
+        System.out.println(user1);
+    }
+}
