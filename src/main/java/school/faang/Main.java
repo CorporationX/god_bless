27a07ef8fe2cececb7dae1b5b8efbb7d070e7c73
lomuserg <sergeymu2015@yandex.ru>
2024-10-06 00:40:45 +0300

    init Book.java with equals() and hashCode() main methods: addBook(), getBookFromKey(), deleteBookFromKey(), getMap()

--- src/main/java/school/faang/Main.java
+++ src/main/java/school/faang/Main.java
@@ -1,7 +1,41 @@
 package school.faang;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class Main {
-    public static void main(String[] args){
-        System.out.println("");
+    public static void main(String[] args) {
+        Map<Book, String> hashMap = new HashMap<>();
+        Book book = new Book("Rt", "Sergey", "2024");
+        addBook(hashMap, "Остров сокровищ", "Rt", "Sergey", "2024");
+        addBook(hashMap, "Приключения Тома Сойера", "Ts", "Dima", "2003");
+        System.out.println(getBookFromKey(hashMap, book));
+        getMap(hashMap);
+    }
+
+    public static void addBook(Map<Book, String> map, String value, String title, String author, String year) {
+        Book book = new Book();
+        book.setAuthor(author);
+        book.setYear(year);
+        book.setTitle(title);
+        map.put(book, value);
+    }
+
+    public static String getBookFromKey(Map<Book, String> map, Book key) {
+        return map.get(key);
+    }
+
+    public static void deleteBookFromKey(Map<Book, String> map, String title, String author, String year) {
+        Book book = new Book(title, author, year);
+        map.remove(getBookFromKey(map, book));
+
+    }
+
+    public static void getMap(Map<Book, String> map) {
+        String books = "";
+        for (Map.Entry<Book, String> entry : map.entrySet()) {
+            books += getBookFromKey(map, entry.getKey()) + "\n";
+        }
+        System.out.println("Books:\n" + books);
     }
 }
