d67470cb51dbdf164f6cebf9740b57b733482862
Aleksandr Dainiak <a.dainiak@arammeem.com>
2024-10-05 17:00:41 +0200

    BJS2-31393 Группировка пользователей по возрасту. COde review fixes

--- src/main/java/school/faang/firsttask/User.java
+++ src/main/java/school/faang/firsttask/User.java
@@ -1,16 +1,19 @@
 package school.faang.firsttask;
+
 import java.util.List;
 import java.util.Map;
 import java.util.HashMap;
 import java.util.ArrayList;
 
+import lombok.Getter;
+
+@Getter
 public class User {
     private String name;
     private int age;
     private String workplace;
     private String address;
 
-    // Конструктор
     public User(String name, int age, String workplace, String address) {
         this.name = name;
         this.age = age;
@@ -18,25 +21,7 @@ public class User {
         this.address = address;
     }
 
-    // Геттеры
-    public String getName() {
-        return name;
-    }
-
-    public int getAge() {
-        return age;
-    }
-
-    public String getWorkplace() {
-        return workplace;
-    }
-
-    public String getAddress() {
-        return address;
-    }
-
     /**
-     *
      * @param users - List of users
      * @return Map of users of the same age
      */
@@ -44,9 +29,7 @@ public class User {
         Map<Integer, List<User>> groupedUsers = new HashMap<>();
         for (User user : users) {
             int age = user.getAge();
-            if (!groupedUsers.containsKey(age)) {
-                groupedUsers.put(age, new ArrayList<>());
-            }
+            groupedUsers.putIfAbsent(age, new ArrayList<>());
             groupedUsers.get(age).add(user);
         }
         return groupedUsers;
