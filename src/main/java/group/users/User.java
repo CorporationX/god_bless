834df6aa866a528cadfa3c5d331b89f66260c25f
Viktor Krivetskiy <1woracc@gmail.com>
2024-10-05 11:25:57 +0300

    Correcting defects

--- src/main/java/group/users/User.java
+++ src/main/java/group/users/User.java
@@ -11,6 +11,13 @@ public class User {
     private String workplace;
     private String address;
 
+    public User(String name, int age, String workplace, String address) {
+        this.name = name;
+        this.age = age;
+        this.workplace = workplace;
+        this.address = address;
+    }
+
     public String getName() {
         return name;
     }
@@ -19,20 +26,11 @@ public class User {
         return age;
     }
 
-    public User(String name, int age, String workplace, String address) {
-        this.name = name;
-        this.age = age;
-        this.workplace = workplace;
-        this.address = address;
-    }
-
     public static Map<Integer, List<User>> groupUsers(List<User> users) {
         Map<Integer, List<User>> groupedUsers = new HashMap<>();
         for (User user : users) {
-            if (!groupedUsers.containsKey(user.getAge())) {
-                List<User> usersList = new ArrayList<>();
-                groupedUsers.put(user.getAge(), usersList);
-            }
+            List<User> usersList = new ArrayList<>();
+            List<User> value = groupedUsers.putIfAbsent(user.getAge(), usersList);
             groupedUsers.get(user.getAge()).add(user);
         }
         return groupedUsers;
