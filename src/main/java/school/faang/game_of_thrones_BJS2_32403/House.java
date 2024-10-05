1c7190885c39979f824670d6778d38672fdb379b
VovaBuzivskyi <VovaBuzivskyi@gmail.com>
2024-10-05 20:20:19 +0700

    BJS2-32403 Game of thrones

---
+++ src/main/java/school/faang/game_of_thrones_BJS2_32403/House.java
@@ -0,0 +1,11 @@
+package school.faang.game_of_thrones_BJS2_32403;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@AllArgsConstructor
+@Data
+public class House {
+    String name;
+    String blazon;
+}
