a018c6c4a4233899e6423e4f93987beb017cf4d9
Aleksandr Kuznetsov <adeskmath@gmail.com>
2024-10-06 00:28:59 +0300

    BJS2-31779 corrected constructor

--- src/main/java/school/faang/user/registration/User.java
+++ src/main/java/school/faang/user/registration/User.java
@@ -1,7 +1,5 @@
 package school.faang.user.registration;
 
-import java.util.Arrays;
-import java.util.HashSet;
 import java.util.Set;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
@@ -12,25 +10,40 @@ import lombok.NoArgsConstructor;
 @EqualsAndHashCode
 public class User {
 
+  private static final int VALID_AGE = 18;
+  private static final Set<String> VALID_JOBS = Set.of("Google", "Uber", "Amazon");
+  private static final Set<String> VALID_ADDRESSES = Set.of("London", "New York", "Amsterdam");
+
   private String name;
   private int age;
   private String workplace;
   private String address;
 
-  private static final Set<String> VALID_JOBS = new HashSet<>(
-      Arrays.asList("Google", "Uber", "Amazon"));
-  private static final Set<String> VALID_ADDRESSES = new HashSet<>(
-      Arrays.asList("London", "New York", "Amsterdam"));
-
   public User(String name, int age, String workplace, String address) {
-    if (name.isEmpty() || age < 18 || !VALID_JOBS.contains(workplace)
-        || !VALID_ADDRESSES.contains(address)) {
-      throw new IllegalArgumentException();
+    String incorrectData = invalidData(name, age, workplace, address);
+    if (!incorrectData.isEmpty()) {
+      throw new IllegalArgumentException(incorrectData);
     }
     this.name = name;
     this.age = age;
     this.workplace = workplace;
     this.address = address;
+  }
+
+  private static String invalidData(String name, int age, String workplace, String address) {
+    if (name.isEmpty()) {
+      return "Missing name";
+    }
+    if (age < VALID_AGE) {
+      return "incorrect age: " + age;
+    }
+    if (!VALID_JOBS.contains(workplace)) {
+      return "incorrect workplace: " + workplace;
+    }
+    if (!VALID_ADDRESSES.contains(address)) {
+      return "incorrect address: " + workplace;
+    }
+    return "";
   }
 
 }
