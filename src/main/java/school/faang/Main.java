7b9330d7c0778506d98f0b53f9c54309594db55d
bogda <bogdanskrip404@gmail.com>
2024-10-05 14:01:47 +0500

    Вернул в исходное состояние

--- src/main/java/school/faang/Main.java
+++ src/main/java/school/faang/Main.java
@@ -1,14 +1,8 @@
 package school.faang;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Map;
 
 public class Main {
     public static void main(String[] args) {
-        User alexander = new User("Alexander", 18, "Amazon", "Amsterdam");
-        User timur = new User("Timur", 25, "Google", "London");
-        User denis = new User("Denis", 20, "Amazon", "New York");
-        User kirill = new User("Kirill", 17, "Uber", "New York");
+    
     }
 }
