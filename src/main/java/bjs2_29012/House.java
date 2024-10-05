77c9bb984ff7595d7821060ee0070cd2bb8e0de1
Sanasar1 <almasyansb@gmail.com>
2024-10-06 01:27:26 +0300

    BJS2-32439

---
+++ src/main/java/bjs2_29012/House.java
@@ -0,0 +1,13 @@
+package bjs2_29012;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.ToString;
+
+@AllArgsConstructor
+@Getter
+@ToString
+public class House {
+    private String name;
+    private String sigil;
+}
