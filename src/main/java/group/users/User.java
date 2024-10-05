72137ab54fc71297a8aed09da615c00545d78801
Viktor Krivetskiy <1woracc@gmail.com>
2024-10-05 17:55:17 +0300

    Correcting defects 3

--- src/main/java/group/users/User.java
+++ src/main/java/group/users/User.java
@@ -1,13 +1,11 @@
 package group.users;
 
-import lombok.val;
-
 import java.util.List;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.ArrayList;
 
-public class User {
+public class User extends Main {
     private String name;
     private int age;
     private String workplace;
@@ -32,9 +30,7 @@ public class User {
         Map<Integer, List<User>> groupedUsers = new HashMap<>();
         for (User user : users) {
             List<User> value = groupedUsers.putIfAbsent(user.getAge(), new ArrayList<>());
-            if (value == null) {
-                value = groupedUsers.get(user.getAge());
-            }
+            value = groupedUsers.get(user.getAge());
             value.add(user);
         }
         return groupedUsers;
@@ -46,16 +42,5 @@ public class User {
                 System.out.println("Возраст: " + entry.getKey() + " лет" + "\n" + "Имя: " + user.getName() + "\n");
             }
         }
-    }
-
-    public static void main(String[] args) {
-        List<User> users = new ArrayList<>();
-        users.add(new User("Иван", 21, "Сбер", "ул. Веры Хоружей"));
-        users.add(new User("Коля", 20, "Яндекс", "пр-т. Победы"));
-        users.add(new User("Максим", 24, "Aston", "ул. Каховская"));
-        users.add(new User("Андрей", 23, "Тиньков", "бульвар. Есенина"));
-
-        Map<Integer, List<User>> grouped = User.groupUsers(users);
-        printResult(grouped);
     }
 }
