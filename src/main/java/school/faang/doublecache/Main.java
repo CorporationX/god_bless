e16cbc83defd2d85d636e4adc02aafc9687294fe
Aleksey <aleksei.zuk86@gmail.com>
2024-10-05 18:24:19 +0300

    Двойной кэш-кэш

--- src/main/java/school/faang/doublecache/Main.java
+++ src/main/java/school/faang/doublecache/Main.java
@@ -7,8 +7,9 @@ import java.util.Map;
 import java.util.Set;
 
 public class Main {
-    static Map<Student, Map<Subject, Integer>> studentWithSubjects = new HashMap<>();
-    static Map<Subject, Set<Student>> studentWithThisSubject = new HashMap<>();
+    private static final Map<Student, Map<Subject, Integer>> subjectGradesOfStudent =
+            new HashMap<>();
+    private static final Map<Subject, Set<Student>> subjectStudents = new HashMap<>();
 
     public static void main(String[] args) {
         Student student1 = new Student(1, "Ivan");
@@ -18,33 +19,33 @@ public class Main {
         Subject it = new Subject(5, "IT");
 
         addNewStudent(student1, math, 5);
-        System.out.println("studentWithSubjects after adding new student: " + studentWithSubjects);
+        System.out.println("studentWithSubjects after adding new student: " + subjectGradesOfStudent);
         System.out.println("----------------");
         addNewStudent(student1, physics, 4);
-        System.out.println("studentWithSubjects after adding existing student with new subject: " + studentWithSubjects);
+        System.out.println("studentWithSubjects after adding existing student with new subject: " + subjectGradesOfStudent);
         System.out.println("----------------");
         addNewStudent(student2, math, 3);
         addNewStudent(student2, physics, 5);
-        System.out.println("studentWithSubjects after adding new student with multiple subjects: " + studentWithSubjects);
+        System.out.println("studentWithSubjects after adding new student with multiple subjects: " + subjectGradesOfStudent);
         System.out.println("----------------");
         addNewSubjectForStudent(student1, it, 6);
-        System.out.println("studentWithSubjects after adding new subject for existing student: " + studentWithSubjects);
+        System.out.println("studentWithSubjects after adding new subject for existing student: " + subjectGradesOfStudent);
         System.out.println("----------------");
         printAllStudents();
         System.out.println("----------------");
         removeStudent(student1);
-        System.out.println("studentWithSubjects after removing student: " + studentWithSubjects);
+        System.out.println("studentWithSubjects after removing student: " + subjectGradesOfStudent);
         System.out.println("----------------");
         printAllStudents();
         System.out.println("----------------");
         addSubjectWithStudents(math, List.of(student1, student2));
-        System.out.println("studentWithThisSubject after adding subject with students: " + studentWithThisSubject);
+        System.out.println("studentWithThisSubject after adding subject with students: " + subjectStudents);
         System.out.println("----------------");
         addStudentToSubject(student1, physics);
-        System.out.println("studentWithThisSubject after adding student to subject: " + studentWithThisSubject);
+        System.out.println("studentWithThisSubject after adding student to subject: " + subjectStudents);
         System.out.println("----------------");
         removeStudentFromSubject(student1, math);
-        System.out.println("studentWithThisSubject after removing student from subject: " + studentWithThisSubject);
+        System.out.println("studentWithThisSubject after removing student from subject: " + subjectStudents);
         System.out.println("----------------");
         printAllSubjects();
     }
@@ -52,30 +53,30 @@ public class Main {
     public static void addNewStudent(Student student,
                                      Subject subject,
                                      Integer grade) {
-        studentWithSubjects.computeIfAbsent(student, v -> new HashMap<>())
+        subjectGradesOfStudent.computeIfAbsent(student, v -> new HashMap<>())
                 .put(subject, grade);
-        studentWithThisSubject.computeIfAbsent(subject, v -> new HashSet<>())
+        subjectStudents.computeIfAbsent(subject, v -> new HashSet<>())
                 .add(student);
     }
 
     public static void addNewSubjectForStudent(Student student,
                                                Subject subject,
                                                Integer grade) {
-        studentWithSubjects.computeIfPresent(student, (k, v) -> {
+        subjectGradesOfStudent.computeIfPresent(student, (k, v) -> {
             v.put(subject, grade);
-            studentWithThisSubject
+            subjectStudents
                     .computeIfAbsent(subject, s -> new HashSet<>()).add(student);
             return v;
         });
     }
 
     public static void removeStudent(Student student) {
-        studentWithSubjects.remove(student);
-        studentWithThisSubject.values().forEach(students -> students.remove(student));
+        subjectGradesOfStudent.remove(student);
+        subjectStudents.values().forEach(students -> students.remove(student));
     }
 
     public static void printAllStudents() {
-        studentWithSubjects.forEach((student, subjects) ->
+        subjectGradesOfStudent.forEach((student, subjects) ->
                 System.out.println(student + " " + subjects));
     }
 
@@ -88,15 +89,15 @@ public class Main {
     }
 
     public static void removeStudentFromSubject(Student student, Subject subject) {
-        studentWithSubjects.computeIfPresent(student, (k, v) -> {
+        subjectGradesOfStudent.computeIfPresent(student, (k, v) -> {
             v.remove(subject);
             return v;
         });
-        studentWithThisSubject.get(subject).remove(student);
+        subjectStudents.get(subject).remove(student);
     }
 
     public static void printAllSubjects() {
-        studentWithThisSubject.forEach((subject, students) ->
+        subjectStudents.forEach((subject, students) ->
                 System.out.println(subject + " " + students));
     }
 }
