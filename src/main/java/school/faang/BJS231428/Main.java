b85b1ec8be3cc2510965f9a686636bcc4d16aeb6
Slava <leeteso97@gmail.com>
2024-10-05 15:55:58 +0500

    Добавил отдельный пакет для задач и перенес классы и методы

---
+++ src/main/java/school/faang/BJS231428/Main.java
@@ -0,0 +1,10 @@
+package school.faang.BJS231428;
+
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) {
+        int[] test = {1, 2, 3, 4, 5};
+        System.out.println(Arrays.toString(Example.reverse(test)));
+    }
+}
--- src/main/java/school/faang/Main.java
+++ src/main/java/school/faang/Main.java
@@ -1,10 +1,8 @@
 package school.faang;
 
+import school.faang.BJS231428.Example;
+
 import java.util.Arrays;
 
 public class Main {
-    public static void main(String[] args) {
-        int[] test = {1, 2, 3, 4};
-        System.out.println(Arrays.toString(Example.reverse(test)));
-    }
 }
