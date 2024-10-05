0d46bc11de51856847fdcafdd3dfa90bfb93317c
bogda <bogdanskrip404@gmail.com>
2024-10-05 14:11:11 +0500

    Вернул в исходное состояние

--- src/main/java/school/faang/Main.java
+++ src/main/java/school/faang/Main.java
@@ -1,8 +1,6 @@
 package school.faang;
 
-import school.faang.Abstraction.Archer;
-import school.faang.Abstraction.Character;
-import school.faang.Abstraction.Warrior;
+
 
 public class Main {
     public static void main(String[] args) {
