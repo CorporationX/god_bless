5a48fcd9b20035a354b2e77d1ad1bef61da9eab8
lomuserg <sergeymu2015@yandex.ru>
2024-10-05 22:59:17 +0300

    Character.java

---
+++ src/main/java/school/faang/Character.java
@@ -0,0 +1,16 @@
+package school.faang;
+
+public abstract class Character {
+    private String name;
+    private String strength;
+    private String dexterity;
+    private String intelligence;
+    private String health;
+
+    public Character(String name){
+        this.name = name;
+    }
+    public Character(String name, String){
+        this.name = name;
+    }
+}
