24ea0b3e5c45df41cba94ceb355ad4baaa428251
Sanasar1 <almasyansb@gmail.com>
2024-10-05 13:23:44 +0300

    Changed removeStudent func to delete by name

--- src/main/java/bjs2_29013/Main.java
+++ src/main/java/bjs2_29013/Main.java
@@ -17,7 +17,7 @@ public class Main {
         addStudent(students, new Student("Jacob", "Economy", 2));
         addStudent(students, new Student("Jonathon", "Psychology", 4));
 
-        removeStudent(students, new Student("Jack", "Computer Science", 3));
+        removeStudent(students, "Jack");
 
         Map<AcademicProgram, List<Student>> groupedStudents = groupStudents(students);
 
@@ -43,8 +43,8 @@ public class Main {
         students.add(student);
     }
 
-    public static void removeStudent(List<Student> students, Student student) {
-        students.remove(student);
+    public static void removeStudent(List<Student> students, String name) {
+        students.removeIf(n -> n.getName().equals(name));
     }
 
     public static List<Student> searchAcademicProgramStudents(Map<AcademicProgram, List<Student>> groupedStudents,
