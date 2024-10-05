a3026783a90af5c6b1bba1f1b568711f5c8670ee
Gadzhiev Gadzhi <Gadzhiev3222@gmail.com>
2024-10-05 11:41:51 +0300

    .

---
+++ src/main/java/school/faang/double_cash/Student.java
@@ -0,0 +1,12 @@
+package school.faang.double_cash;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@AllArgsConstructor
+@Data
+
+public class Student {
+    private int id;
+    private String name;
+}
