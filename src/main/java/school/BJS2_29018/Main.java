5bd7ac549c8d68945b101b3dc7aca255885eb891
Roman Nifontov <romannif@mail.ru>
2024-10-05 13:01:00 +0500

    Сделал изменения в задаче Библиотека Вестеросса

--- src/main/java/school/BJS2_29018/Main.java
+++ src/main/java/school/BJS2_29018/Main.java
@@ -5,17 +5,17 @@ import java.util.Map;
 
 public class Main {
 
-    private static final Map<Book,String> mappa = new HashMap<>();
+    private static final Map<Book, String> mappa = new HashMap<>();
 
     public static void main(String[] args) {
-        addBook(new Book("Война и мир", "Толстой", 1873),"Полка номер 10");
-        addBook(new Book("Преступление и наказание", "Достоевский", 1866),"Полка номер 6");
-        addBook(new Book("Самый успешный блогер на деревне", "Милохин", 2024),"Полка номер 5");
-        System.out.println(getLine("Милохин","Самый успешный блогер на деревне", 2024));
+        addBook(new Book("Война и мир", "Толстой", 1873), "Полка номер 10");
+        addBook(new Book("Преступление и наказание", "Достоевский", 1866), "Полка номер 6");
+        addBook(new Book("Самый успешный блогер на деревне", "Милохин", 2024), "Полка номер 5");
+        System.out.println(getLine("Милохин", "Самый успешный блогер на деревне", 2024));
         System.out.println("---------------------------------------------");
         printLibrary();
         System.out.println("---------------------------------------------");
-        removeBook("Милохин","Самый успешный блогер на деревне", 2024);
+        removeBook("Милохин", "Самый успешный блогер на деревне", 2024);
         printLibrary();
     }
 
@@ -26,18 +26,14 @@ public class Main {
 
     //удаление книги по её названию, автору и году издания
     private static void removeBook(String author, String title, int year) {
-        Map<Book, String> clone = new HashMap<>(mappa);
-        for (Map.Entry<Book, String> entry : clone.entrySet()) {
-            if(checkBook(author, title, year, entry.getKey())) {mappa.remove(entry.getKey());}
-        }
+        Book book = new Book(title, author, year);
+        mappa.remove(book);
     }
 
     //поиск книги и вывода информации о её местонахождении в библиотеке
     private static String getLine(String author, String title, int year) {
-        for (Map.Entry<Book, String> entry : mappa.entrySet()) {
-            if(checkBook(author, title, year, entry.getKey())) return entry.getValue();
-        }
-        return null;
+        Book book = new Book(title, author, year);
+        return mappa.get(book);
     }
 
     //вывод списка всех книг и их местонахождения в библиотеке.
