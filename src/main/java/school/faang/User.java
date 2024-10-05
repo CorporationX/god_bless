4a4838a25efa9d7e532b2d7e36672c3e8004d506
lomuserg <sergeymu2015@yandex.ru>
2024-10-05 14:31:17 +0300

    fix groupUsers

--- src/main/java/school/faang/User.java
+++ src/main/java/school/faang/User.java
@@ -1,22 +1,18 @@
 package school.faang;
 
 import lombok.Data;
-import lombok.Getter;
 
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+
 @Data
 public class User {
     private String name;
     private String age;
     private String job;
     private String address;
-
-    public User() {
-
-    }
 
     public User(String name, String age, String job, String address) {
         this.name = name;
@@ -25,17 +21,12 @@ public class User {
         this.address = address;
     }
 
-    public Map<String, List<User>> groupUsers(List<User> users) {
+    public static Map<String, List<User>> groupUsers(List<User> users) {
         Map<String, List<User>> mapByAge = new HashMap<>();
-        List<User> listByAge = new ArrayList<>();
         for (User user : users) {
-            if (user.age.equals(this.age)) {
-                listByAge.add(user);
-                System.out.println("Закинул " + user.name + "\n");
-            }
+            mapByAge.computeIfAbsent(user.age, k -> new ArrayList<>()).add(user);
         }
-        mapByAge.put(this.age,listByAge);
-        return  mapByAge;
+        return mapByAge;
 
     }
 
