a4240e4f1012df84e305fdd7e4fecf6e07af93c7
Aleksandr Dainiak <a.dainiak@arammeem.com>
2024-10-06 12:02:08 +0200

    BJS2-31510 Развернитесь! Refactoring

--- src/main/java/school/faang/Main.java
+++ src/main/java/school/faang/Main.java
@@ -7,10 +7,14 @@ import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
+        arrayReverse();
+    }
+
+    private static void arrayReverse() {
         int[] nums = {1, 2, 3, 4, 5};
         Example.reverse(nums);
 
-        System.out.println("ArrayList reversed : ");
+        System.out.println("Array reversed : ");
         for (int num : nums) {
             System.out.print(num + " ");
         }
