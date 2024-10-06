9c07df479fc4240bcb940d0833e08a849439c8ae
Memoreks <akifyevalex1@gmail.com>
2024-10-06 21:17:19 +0700

    Фильтры Instagram

---
+++ src/main/java/school/faang/instagram/FilterProcessor.java
@@ -0,0 +1,14 @@
+package school.faang.instagram;
+
+import java.util.function.Function;
+
+public class FilterProcessor {
+
+    public static Image applyFilter(Image image, Function<Image, Image> function) {
+        return function.apply(image);
+    }
+
+    public static Function<Image, Image> combineFilters(Function<Image, Image> function1, Function<Image, Image> function2) {
+        return function1.compose(function2);
+    }
+}
