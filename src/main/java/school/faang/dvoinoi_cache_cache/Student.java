4d22cf054331d98fc557ee1a3f8fdeb38669ed9c
Alena Shmelkova <AlenaShmelkova@gmail.com>
2024-10-06 01:39:35 -0400

    git commit -m "BJS2-32406 <Dvoinoi cache cache initial commit>"

---
+++ src/main/java/school/faang/dvoinoi_cache_cache/Student.java
@@ -0,0 +1,13 @@
+package school.faang.dvoinoi_cache_cache;
+
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.ToString;
+
+@EqualsAndHashCode
+@AllArgsConstructor
+@ToString
+public class Student {
+    private int id;
+    private String name;
+}
---
+++ src/main/java/school/faang/dvoinoi_cache_cache/Subject.java
@@ -0,0 +1,13 @@
+package school.faang.dvoinoi_cache_cache;
+
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.ToString;
+
+@AllArgsConstructor
+@EqualsAndHashCode
+@ToString
+public class Subject {
+    private int id;
+    private String name;
+}
