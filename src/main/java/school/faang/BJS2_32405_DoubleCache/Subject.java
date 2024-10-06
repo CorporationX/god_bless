0106b4ae361877e53c5276e9305cbd7de784fa45
Maxim Gorobets <gorobets.maks@bk.ru>
2024-10-06 14:12:03 +1000

    new Subject class

---
+++ src/main/java/school/faang/BJS2_32405_DoubleCache/Subject.java
@@ -0,0 +1,11 @@
+package school.faang.BJS2_32405_DoubleCache;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class Subject {
+    private int id;
+    private String name;
+}
