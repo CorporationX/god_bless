526f9f427d130cbfc0725120b9ef13096524f563
Roman Nifontov <romannif@mail.ru>
2024-10-06 18:53:34 +0500

    Сделал задачу BJS2-34611

--- src/main/java/school/BJS2_34611/Main.java
+++ src/main/java/school/BJS2_34611/Main.java
@@ -1,6 +1,15 @@
 package school.BJS2_34611;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
 public class Main {
+
+    static final Set<String> set = new HashSet<>(Set.of("email","sms","push"));
+
     public static void main(String[] args) {
 
         NotificationManager notificationManager = new NotificationManager();
@@ -18,14 +27,20 @@ public class Main {
                 (notification) -> System.out.println("Отправка push-уведомления: " + notification.getMessage())
         );
 
+        notificationManager.registerHandler2(set, (notification) -> {
+            List<String> contains18Plus = new ArrayList<>(Arrays.asList(notification.getMessage().split(" ")));
+        return contains18Plus.contains("18+");});
+
 // Отправка оповещений
-        Notification emailNotification = new Notification("email", "Ваша учетная запись успешно активирована");
+        Notification emailNotification = new Notification("email", "18+ контент. Что-то на некультурном");
         Notification smsNotification = new Notification("sms", "Вы успешно изменили свой пароль");
         Notification pushNotification = new Notification("push", "Новый пост от пользователя: JohnDoe");
 
-        notificationManager.sendNotification(emailNotification);
-        notificationManager.sendNotification(smsNotification);
-        notificationManager.sendNotification(pushNotification);
+        notificationManager.sendNotification(set,emailNotification);
+        notificationManager.sendNotification(set,smsNotification);
+        notificationManager.sendNotification(set,pushNotification);
+
+
 
 
 
--- src/main/java/school/BJS2_34611/NotificationManager.java
+++ src/main/java/school/BJS2_34611/NotificationManager.java
@@ -2,19 +2,32 @@ package school.BJS2_34611;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Set;
 import java.util.function.Consumer;
+import java.util.function.Predicate;
 
 public class NotificationManager {
 
     private Map<String, Consumer<Notification>> listeners = new HashMap<>();
 
+    private Map<Set<String>, Predicate<Notification>> listenersWithPredicate = new HashMap<>();
+
+    public void registerHandler2(Set<String> set, Predicate<Notification> predicate) {
+        listenersWithPredicate.put(set,predicate);
+    }
+
     public void registerHandler(String type, Consumer<Notification> handler) {
         listeners.put(type, handler);
     }
-    public void sendNotification(Notification notification) {
-        String type = notification.getType();
-        listeners.get(type).accept(notification);
 
+    public void sendNotification(Set<String> set, Notification notification) {
+        String type = notification.getType();
+        if (!listenersWithPredicate.get(set).test(notification)) {
+            listeners.get(type).accept(notification);
+        } else {
+            System.out.printf("Отправка %s заблокировна, есть контент 18+", type);
+            System.out.println();
+        }
     }
 
 
