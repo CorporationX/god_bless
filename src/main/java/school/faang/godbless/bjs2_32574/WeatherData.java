61795da60372ecad5e21477f5bc81e82091746db
AnnaAplavina <59871896+AnnaAplavina@users.noreply.github.com>
2024-10-06 17:54:24 +0300

    weather cache

---
+++ src/main/java/school/faang/godbless/bjs2_32574/WeatherData.java
@@ -0,0 +1,13 @@
+package school.faang.godbless.bjs2_32574;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class WeatherData {
+
+  private String city;
+  private int temperature;
+  private int humidity;
+}
