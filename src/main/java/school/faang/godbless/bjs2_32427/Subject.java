ccc0c3ff1da04b653fb0e83035a826400b5658a5
AnnaAplavina <59871896+AnnaAplavina@users.noreply.github.com>
2024-10-06 10:50:04 +0300

    students subjects and grades

---
+++ src/main/java/school/faang/godbless/bjs2_32427/Subject.java
@@ -0,0 +1,12 @@
+package school.faang.godbless.bjs2_32427;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class Subject {
+
+  private int id;
+  private String name;
+}
