f86bc34de795548d02dabd7f3e3d4928ff931964
Egor Semenovskii <gaoqiao5217@gmail.com>
2024-10-06 17:31:54 +0300

    Game of Thrones

---
+++ src/main/java/school/faang/GameOfThrones/Main.java
@@ -0,0 +1,48 @@
+package school.faang.GameOfThrones;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Main {
+    private static final Map<String, House> HOUSES = new HashMap<>();
+
+    public static void main(String[] args) {
+
+        House stark = new House("Stark", "Grey Wolf");
+        House lannister = new House("Lannister", "Golden Lion");
+        addHouse(stark);
+        addHouse(lannister);
+        addHouse("Baratheon", "Black Stag");
+
+        printHouses();
+        System.out.println();
+
+        removeHouseByName("Lannister");
+        printHouses();
+        System.out.println();
+
+        printSigilByName("Stark");
+
+    }
+
+    public static void addHouse(House house) {
+        HOUSES.put(house.getName(), house);
+    }
+
+    public static void addHouse(String name, String sigil) {
+        HOUSES.put(name, new House(name, sigil));
+    }
+
+    public static void removeHouseByName(String name) {
+        HOUSES.remove(name);
+    }
+
+    public static House printSigilByName(String name) {
+        System.out.println(HOUSES.get(name).getSigil());
+        return HOUSES.get(name);
+    }
+
+    public static void printHouses() {
+        HOUSES.forEach((name, house) -> {System.out.println(house);});
+    }
+}
