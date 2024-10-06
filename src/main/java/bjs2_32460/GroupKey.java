dfbab678b153a92ce1ace0b62f74b6b0b7f51578
light <estarosa@list.ru>
2024-10-06 12:18:16 +0300

    правки к задаче "Считаем прогулы" 4

--- src/main/java/bjs2_32460/GroupKey.java
+++
@@ -1,4 +0,0 @@
-package bjs2_32460;
-
-public record GroupKey(String faculty, Integer year) {
-}
--- src/main/java/bjs2_32460/Main.java
+++ src/main/java/bjs2_32460/Main.java
@@ -28,12 +28,12 @@ public class Main {
         printAllGroups();
     }
 
-    public static Map<GroupKey, List<Student>> getGroups(List<Student> students) {
+    public static Map<FacultyYear, List<Student>> getGroups(List<Student> students) {
         return students
                 .stream()
-                .collect(Collectors.groupingBy(student -> {
-                    return new GroupKey(student.getFaculty(), student.getYear());
-                }));
+                .collect(Collectors.groupingBy(student ->
+                        new FacultyYear(student.getFaculty(), student.getYear())
+                ));
     }
 
     public static void addStudent(Student student) {
@@ -45,7 +45,7 @@ public class Main {
     }
 
     public static List<Student> findStudentsByFacultyAndYear(String faculty, Integer year) {
-        return getGroups(students).get(new GroupKey(faculty, year));
+        return getGroups(students).get(new FacultyYear(faculty, year));
     }
 
     public static void printAllGroups() {
