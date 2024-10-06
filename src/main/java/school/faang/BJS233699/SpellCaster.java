e0df08233f93a94915652a1df8d8b13d79a1307d
Slava <leeteso97@gmail.com>
2024-10-06 19:49:51 +0500

    Добавил классы согласно ТЗ

---
+++ src/main/java/school/faang/BJS233699/SpellCaster.java
@@ -0,0 +1,7 @@
+package school.faang.BJS233699;
+
+public class SpellCaster {
+    public void cast(String spellName, SpellAction spellAction) {
+        System.out.println(spellAction.castSpell(spellName));
+    }
+}
