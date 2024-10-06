fe79fb71f222bb269df0fc9abcbff9ec9f3fd5dd
yestdmitriikirichenko <yakitayka@gmail.com>
2024-10-07 00:57:37 +0300

    countingAbsenteeism

--- src/main/java/countingAbsenteeism/Main.java
+++ src/main/java/countingAbsenteeism/Main.java
@@ -1,7 +1,6 @@
 package countingAbsenteeism;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -9,12 +8,18 @@ import java.util.Map;
 public class Main {
     static final List<Student> STUDENTS = new ArrayList<>();
 
+    private static String generateKey(String faculty, int year) {
+        return "%s-%d".formatted(faculty, year);
+    }
+
     public static Map<String, List<Student>> studentGroups(List<Student> students) {
         Map<String, List<Student>> studentGroups = new HashMap<>();
 
         students.forEach(student -> {
-            studentGroups.putIfAbsent("%s-%d".formatted(student.getFaculty(), student.getYear()), new ArrayList<>());
-            studentGroups.get("%s-%d".formatted(student.getFaculty(), student.getYear())).add(student);
+            studentGroups.computeIfAbsent(
+                    generateKey(student.getFaculty(), student.getYear()),
+                    k -> new ArrayList<>()
+            ).add(student);
         });
 
         return studentGroups;
@@ -29,11 +34,14 @@ public class Main {
     }
 
     public static List<Student> facultyGroup(String faculty, int year) {
-        return studentGroups(STUDENTS).get("%s-%d".formatted(faculty, year));
+        return studentGroups(STUDENTS).get(generateKey(faculty, year));
     }
 
     public static void printStudentsGroup() {
-        System.out.println(Arrays.toString(STUDENTS.toArray()));
+        studentGroups(STUDENTS).forEach((key, value) -> {
+            System.out.printf("%s - %s%n", key, value);
+        });
+
     }
 
     public static void main(String[] args) {
@@ -47,7 +55,9 @@ public class Main {
         printStudentsGroup();
         deleteStudent("Alice", "Software Engineering", 5);
         deleteStudent("Bob", "Software Engineering", 2);
+        System.out.println();
         printStudentsGroup();
+        System.out.println();
         System.out.println(facultyGroup("Mathematics", 2));
         System.out.println(facultyGroup("Computer Science", 1));
     }
