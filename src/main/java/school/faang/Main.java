1cbbe07486cd94717e8388cac7083ece4834a2ba
Aleksandr Dainiak <a.dainiak@arammeem.com>
2024-10-06 13:41:32 +0200

    BJS2-33067 Java HashMap: Поисковой мини-движок Google #11450

--- src/main/java/school/faang/Main.java
+++ src/main/java/school/faang/Main.java
@@ -1,16 +1,32 @@
 package school.faang;
 
+import school.faang.firsttask.User;
 import school.faang.forthTask.SearchEngine;
 import school.faang.forthTask.WebPage;
 
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
 public class Main {
     public static void main(String[] args) {
-       googleSearchEngine();
+        usersGrouping();
+        googleSearchEngine();
+    }
+
+    public static void usersGrouping() {
+        List<User> users = new ArrayList<>();
+        users.add(new User("Ivan", 30, "Company 1", "Address 1"));
+        users.add(new User("Alex", 25, "Company 2", "Address 2"));
+        users.add(new User("Stepan", 30, "Company 3", "Address 3"));
+
+        Map<Integer, List<User>> grouped = User.groupUsers(users);
+        for (Map.Entry<Integer, List<User>> entry : grouped.entrySet()) {
+            System.out.println("Age: " + entry.getKey());
+            for (User user : entry.getValue()) {
+                System.out.println(" - " + user.getName());
+            }
+        }
     }
 
     public static void googleSearchEngine() {
