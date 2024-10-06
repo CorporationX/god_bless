95ba86d5353272ef1530a9c9dc60aa8495f80977
Gadzhiev Gadzhi <Gadzhiev3222@gmail.com>
2024-10-06 07:40:37 +0300

    .

---
+++ src/main/java/school/faang/optimize_amazon/Main.java
@@ -0,0 +1,39 @@
+package school.faang.optimize_amazon;
+
+import school.faang.main_code.Product;
+
+import java.util.*;
+
+public class Main {
+    private static Set<Product> productsSet = new HashSet<>();
+    private Map<String, List<Product>> productsMap = new HashMap<>();
+
+    public static void main(String[] args) {
+        Main main = new Main();
+
+        Product firstProduct = new Product(1, "milk", "1");
+        Product secondProduct = new Product(2, "water", "2");
+        Product thirdProduct = new Product(3, "energydrink", "2");
+
+        productsSet.add(firstProduct);
+        productsSet.add(secondProduct);
+        productsSet.add(thirdProduct);
+
+        main.groupedProduct(productsSet);
+
+        main.printProductsByCategory();
+    }
+
+    public Map<String, List<Product>> groupedProduct(Set<Product> products) {
+        for (Product product : products) {
+            productsMap.computeIfAbsent(product.getCategory(), k -> new ArrayList<>()).add(product);
+        }
+        return productsMap;
+    }
+
+    public void printProductsByCategory() {
+        for(Map.Entry<String, List<Product>> entry : productsMap.entrySet()) {
+            System.out.println(entry.getKey() + "-" + entry.getValue());
+        }
+    }
+}
