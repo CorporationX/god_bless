2ce5bf24126b2ddb05e9c593f28dbab4f33fa170
Maxim Gorobets <gorobets.maks@bk.ru>
2024-10-06 17:21:15 +1000

    new House class

---
+++ src/main/java/school/faang/BJS2_32407_ThronesGames/House.java
@@ -0,0 +1,11 @@
+package school.faang.BJS2_32407_ThronesGames;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@AllArgsConstructor
+@Data
+public class House {
+    String name;
+    String sigil;
+}
