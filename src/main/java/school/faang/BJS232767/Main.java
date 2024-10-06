687a85c85a8eb307982ba83123b33b1c180841f3
Slava <leeteso97@gmail.com>
2024-10-06 20:59:29 +0500

    отрефакторил код согласно комментариям

--- src/main/java/school/faang/BJS232767/Main.java
+++ src/main/java/school/faang/BJS232767/Main.java
@@ -6,30 +6,30 @@ import java.util.List;
 import java.util.Map;
 
 public class Main {
-    static Map<String, List<WebPage>> webMap = new HashMap<>();
+    static Map<String, List<WebPage>> webPageMap = new HashMap<>();
 
     static void addPage(WebPage page) {
         String[] words = page.getContent().split("\\W+");
         for (String word : words) {
-            webMap.computeIfAbsent(word, w -> new ArrayList<>()).add(page);
+            webPageMap.computeIfAbsent(word, w -> new ArrayList<>()).add(page);
         }
     }
 
     static List<WebPage> getWebPagesByKeyWord(String keyWord) {
-        return webMap.get(keyWord);
+        return webPageMap.get(keyWord);
     }
 
     static void deleteWebPageByURL(String url) {
         List<String> keysToDelete = new ArrayList<>();
-        for (Map.Entry<String, List<WebPage>> entry : webMap.entrySet()) {
+        for (Map.Entry<String, List<WebPage>> entry : webPageMap.entrySet()) {
             List<WebPage> webPages = entry.getValue();
             webPages.removeIf(page -> page.getUrl().equals(url));
             if (webPages.isEmpty()) {
                 keysToDelete.add(entry.getKey());
             }
         }
-        for (String s : keysToDelete) {
-            webMap.remove(s);
+        for (String key : keysToDelete) {
+            webPageMap.remove(key);
         }
     }
 
