bfe988d8d686abdd8ed57dbcc7cef0f04143ff2f
Oleksii Babaikin <a.babaykin@gmail.com>
2024-10-06 17:52:43 +0200

    BJS2-32451 Java HashMap: Двойной кэш-кэш! 1) Classes Student, Subject, Main 2) Tests: add, delete, print

---
+++ src/main/java/school/faang/Student.java
@@ -0,0 +1,11 @@
+package school.faang;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class Student {
+    private int id;
+    private String name;
+}
