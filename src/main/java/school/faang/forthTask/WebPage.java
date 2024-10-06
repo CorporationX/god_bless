ad5ca639179628e1fe38338ec18127ed30f83644
Aleksandr Dainiak <a.dainiak@arammeem.com>
2024-10-06 13:18:41 +0200

    BJS2-33067 Java HashMap: Поисковой мини-движок Google

---
+++ src/main/java/school/faang/forthTask/WebPage.java
@@ -0,0 +1,11 @@
+package school.faang.forthTask;
+
+import lombok.Data;
+import java.util.*;
+
+@Data
+public class WebPage {
+    final private String url;
+    final private String title;
+    final private String content;
+}
