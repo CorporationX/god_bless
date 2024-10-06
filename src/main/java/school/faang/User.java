50713534c23c0bd6b3a55924cb717780a13ac26e
zlomilk95 <kenobi_95@mail.ru>
2024-10-06 12:07:31 +0300

    edits

--- src/main/java/school/faang/User.java
+++ src/main/java/school/faang/User.java
@@ -9,31 +9,34 @@ import java.util.Set;
 public class User {
     private static final Set<String> VALID_JOBS = new HashSet<>(Set.of("Google", "Uber", "Amazon"));
     private static final Set<String> VALID_ADDRESSES = new HashSet<>(Set.of("London", "New York", "Amsterdam"));
+    private static final int MINIMUM_AGE = 18;
+
     private final String name;
     private final int age;
     private final String job;
     private final String address;
 
     public User(String name, int age, String job, String address) {
+        checkVariables();
+
+        this.name = name;
+        this.age = age;
+        this.job = job;
+        this.address = address;
+    }
+
+    private void checkVariables() {
         if (name.isEmpty()) {
             throw new IllegalArgumentException("Имя не может быть пустым");
-        } else {
-            this.name = name;
         }
-        if (age < 18) {
+        if (age < MINIMUM_AGE) {
             throw new IllegalArgumentException("Возраст не может быть меньше 18");
-        } else {
-            this.age = age;
         }
-        if (VALID_JOBS.stream().noneMatch(j -> j.equals(job))) {
+        if (!VALID_JOBS.contains(job)) {
             throw new IllegalArgumentException("Место работы должно содержаться во множестве VALID_JOBS");
-        } else {
-            this.job = job;
         }
-        if (VALID_ADDRESSES.stream().noneMatch(a -> a.equals(address))) {
+        if (!VALID_ADDRESSES.contains(address)) {
             throw new IllegalArgumentException("Адрес должен содержаться во множестве VALID_ADDRESSES");
-        } else {
-            this.address = address;
         }
     }
 }
