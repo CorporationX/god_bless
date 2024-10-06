b3ae2d0021b2c6c1211bdc7d1e836357db0ffeb2
Anatoly Galay <galay007@mail.ru>
2024-10-06 13:19:47 +0300

    Applied feedback comments

---
+++ src/main/java/school/faang/registr_user/User.java
@@ -0,0 +1,11 @@
+package school.faang.registr_user;
+
+import lombok.AllArgsConstructor;
+
+@AllArgsConstructor
+public class User {
+    private String name;
+    private int age;
+    private String placeWork;
+    private String address;
+}
