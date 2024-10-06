ba4aca34789a13c8b970e75bf4e18fa4bae2ea96
Aleksandr Kuznetsov <adeskmath@gmail.com>
2024-10-06 15:54:41 +0300

    BJS2-32700

---
+++ src/main/java/school/faang/student/Course.java
@@ -0,0 +1,14 @@
+package school.faang.student;
+
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode
+public class Course {
+  private String faculty;
+  private int year;
+
+}
