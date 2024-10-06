316fd362ba9bc94096933e7784bd48c976a84d39
Sergey Kalinin <serukalinin@gmail.com>
2024-10-07 00:18:59 +0300

    Removed student's code

--- src/main/java/school/faang/BJS2_31758/User.java
+++ src/main/java/school/faang/BJS2_31758/User.java
@@ -12,6 +12,7 @@ public class User {
 
     private static final Set<String> VALID_JOBS = new HashSet<>(Arrays.asList("Google", "Uber", "Amazon"));
     private static final Set<String> VALID_ADDRESS = new HashSet<>(Arrays.asList("London", "New York", "Amsterdam"));
+    private static final  int MIN_AGE = 18;
     private int id;
     private String name;
     private int age;
@@ -20,12 +21,24 @@ public class User {
     private String address;
 
     public User(int id, String name, int age, Set<String> activities, String job, String address) {
+
+        this.id = id;
+        this.name = name;
+        this.age = age;
+        this.activities = activities;
+        this.job = job;
+        this.address = address;
+
+        validate();
+    }
+
+    private void validate() {
         if (name == null || name.trim().isEmpty()) {
             throw new IllegalArgumentException("Name cannot be empty or null");
         }
 
-        if (age < 18) {
-            throw new IllegalArgumentException("Age cannot be less than 18.");
+        if (age < MIN_AGE) {
+            throw new IllegalArgumentException("Age cannot be less than" + MIN_AGE + ".");
         }
 
         if (!VALID_JOBS.contains(job)) {
@@ -35,13 +48,6 @@ public class User {
         if (!VALID_ADDRESS.contains(address)) {
             throw new IllegalArgumentException("Invalid address. Addres must be one of " + VALID_ADDRESS);
         }
-
-        this.id = id;
-        this.name = name;
-        this.age = age;
-        this.activities = activities;
-        this.job = job;
-        this.address = address;
     }
 
     @Override
