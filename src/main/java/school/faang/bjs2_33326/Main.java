c74c7811af954deb79115938668204b4953fbd47
Shap|Misha <maxim.ivanov.jr@gmail.com>
2024-10-06 16:24:23 +0300

    added method: removeEventById and printAllEvents for StreamEventUtils,testRemoveEventById, testPrintAllEvents and generateEventsAddMap for Main

--- src/main/java/school/faang/bjs2_33326/Main.java
+++ src/main/java/school/faang/bjs2_33326/Main.java
@@ -11,15 +11,17 @@ public class Main {
         testAddStreamEventUpdateBothMaps(idToStreamEventMap, eventTypeToStreamEventsMap);
         testFindEventById(idToStreamEventMap);
         testFindEventsByType(eventTypeToStreamEventsMap);
+        testRemoveEventById(idToStreamEventMap, eventTypeToStreamEventsMap);
+        testPrintAllEvents(idToStreamEventMap);
     }
 
-    public static void testAddStreamEventUpdateBothMaps(Map<Integer, StreamEvent> idToStreamEventMap,
+    private static void testAddStreamEventUpdateBothMaps(Map<Integer, StreamEvent> idToStreamEventMap,
                                                         Map<String, List<StreamEvent>> eventTypeToStreamEventsMap) {
         System.out.println("testAddStreamEventUpdateBothMaps\n");
 
         System.out.println("idToStreamEventMap before: " + idToStreamEventMap);
         System.out.println("eventTypeToStreamEventsMap before: " + eventTypeToStreamEventsMap);
-        StreamEvent streamEventForAdd = new StreamEvent(5, "Start", "Data1");
+        StreamEvent streamEventForAdd = new StreamEvent(5, "Start", "Stream start");
         System.out.println("\nadd: " + streamEventForAdd + "\n");
 
         StreamEventUtils.addStreamEvent(streamEventForAdd, idToStreamEventMap, eventTypeToStreamEventsMap);
@@ -27,7 +29,7 @@ public class Main {
         System.out.println("eventTypeToStreamEventsMap after: " + eventTypeToStreamEventsMap);
     }
 
-    public static void testFindEventById(Map<Integer, StreamEvent> idToStreamEventMap) {
+    private static void testFindEventById(Map<Integer, StreamEvent> idToStreamEventMap) {
         System.out.println("\ntestFindEventById\n");
 
         int idEvent = 5;
@@ -37,7 +39,7 @@ public class Main {
         System.out.println("event by Id: " + eventById);
     }
 
-    public static void testFindEventsByType(Map<String, List<StreamEvent>> eventTypeToStreamEventsMap) {
+    private static void testFindEventsByType(Map<String, List<StreamEvent>> eventTypeToStreamEventsMap) {
         System.out.println("\ntestFindEventsByType\n");
 
         String eventType = "Start";
@@ -45,5 +47,30 @@ public class Main {
 
         List<StreamEvent> eventsByType = StreamEventUtils.findEventsByType(eventType, eventTypeToStreamEventsMap);
         System.out.println("events by type" + eventsByType);
+    }
+
+    private static void testRemoveEventById(Map<Integer, StreamEvent> idToStreamEventMap,
+                                           Map<String, List<StreamEvent>> eventTypeToStreamEventsMap) {
+        System.out.println("\ntestRemoveEventById\n");
+
+        System.out.println("idToStreamEventMap before: " + idToStreamEventMap);
+        System.out.println("eventTypeToStreamEventsMap before: " + eventTypeToStreamEventsMap);
+        int idEventForRemove = 5;
+        System.out.println("\nid event for remove: " + idEventForRemove + "\n");
+
+        StreamEventUtils.removeEventById(5, idToStreamEventMap, eventTypeToStreamEventsMap);
+        System.out.println("idToStreamEventMap after: " + idToStreamEventMap);
+        System.out.println("eventTypeToStreamEventsMap after: " + eventTypeToStreamEventsMap);
+    }
+
+    private static void testPrintAllEvents(Map<Integer, StreamEvent> idToStreamEventMap){
+        System.out.println("\ntestPrintAllEvents\n");
+        generateEventsAddMap(idToStreamEventMap);
+        StreamEventUtils.printAllEvents(idToStreamEventMap);
+    }
+    private static void generateEventsAddMap(Map<Integer, StreamEvent> idToStreamEventMap){
+        idToStreamEventMap.put(1, new StreamEvent(1, "START", "Stream started"));
+        idToStreamEventMap.put(2, new StreamEvent(2, "PAUSE", "Stream paused"));
+        idToStreamEventMap.put(3, new StreamEvent(3, "RESUME", "Stream resumed"));
     }
 }
--- src/main/java/school/faang/bjs2_33326/StreamEventUtils.java
+++ src/main/java/school/faang/bjs2_33326/StreamEventUtils.java
@@ -19,12 +19,34 @@ public class StreamEventUtils {
         streamEvents.add(event);
     }
 
-    public static StreamEvent findEventById(int id, Map<Integer, StreamEvent> idToStreamEventMap) {
-        return idToStreamEventMap.get(id);
+    public static StreamEvent findEventById(int idEvent, Map<Integer, StreamEvent> idToStreamEventMap) {
+        return idToStreamEventMap.get(idEvent);
     }
 
     public static List<StreamEvent> findEventsByType(String eventType,
                                                      Map<String, List<StreamEvent>> eventTypeToStreamEventsMap) {
         return eventTypeToStreamEventsMap.get(eventType);
+    }
+
+    public static void removeEventById(int idEvent, Map<Integer, StreamEvent> idToStreamEventMap,
+                                       Map<String, List<StreamEvent>> eventTypeToStreamEventsMap) {
+        StreamEvent streamEvent = idToStreamEventMap.get(idEvent);
+        String eventType = streamEvent.getEventType();
+        idToStreamEventMap.remove(idEvent);
+
+        List<StreamEvent> streamEvents = eventTypeToStreamEventsMap.get(eventType);
+        if (streamEvents == null) {
+            return;
+        }
+        streamEvents.remove(streamEvent);
+
+    }
+
+    public static void printAllEvents(Map<Integer, StreamEvent> idToStreamEventMap) {
+        for (Map.Entry<Integer, StreamEvent> integerStreamEventEntry : idToStreamEventMap.entrySet()) {
+            StreamEvent eventValueEntry = integerStreamEventEntry.getValue();
+            System.out.println(eventValueEntry.getId() + " " + eventValueEntry.getEventType() + " " + eventValueEntry.getData());
+
+        }
     }
 }
