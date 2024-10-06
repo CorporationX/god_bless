1a1758482464f07d86f0ab212ff00ec2264fd33e
Alena Shmelkova <AlenaShmelkova@gmail.com>
2024-10-06 14:35:01 -0400

    git commit -m "BJS2-32415 <Igra Prestolov initial commit>"

---
+++ src/main/java/school/faang/igra_prestolov/House.java
@@ -0,0 +1,13 @@
+package school.faang.igra_prestolov;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.ToString;
+
+@AllArgsConstructor
+@Getter
+@ToString
+public class House {
+    private String name;
+    private String sigil;
+}
