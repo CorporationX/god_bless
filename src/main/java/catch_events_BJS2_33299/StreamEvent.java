e7a7dad8d04043e780343673b33c267d4ad2cd55
IvanMatiko <matiko1993@gmail.com>
2024-10-06 18:07:34 +0200

    Made task Catch Events BJS2-33299

--- src/main/java/catch_events_BJS2_33299/StreamEvent.java
+++ src/main/java/catch_events_BJS2_33299/StreamEvent.java
@@ -5,11 +5,6 @@ import lombok.Setter;
 import lombok.AllArgsConstructor;
 import lombok.ToString;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-
 @Getter
 @Setter
 @AllArgsConstructor
