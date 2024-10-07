f7926cc3ee99d8d465f180f5265d057d5c445d7d
Aleksandr Dainiak <a.dainiak@arammeem.com>
2024-10-07 18:44:39 +0200

    BJS2-33135 Java HashMap: Библиотека Вестеросс. Code review fixes

--- src/main/java/school/faang/secondTask/Book.java
+++ src/main/java/school/faang/secondTask/Book.java
@@ -1,11 +1,10 @@
 package school.faang.secondTask;
 
 import lombok.Data;
-import java.util.Objects;
 
 @Data
 public class Book {
-    final private String title;
-    final private String author;
-    final private int year;
+    private final String title;
+    private final String author;
+    private final int year;
 }
