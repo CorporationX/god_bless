d9c460bfbdf0348024b1cf53e56fe89a4d2205d2
Igor <asachiyigor@gmail.com>
2024-10-07 17:25:16 +0300

    BJS2-32387
    refactoring code - record

--- src/main/java/school/faang/t14/Student.java
+++ src/main/java/school/faang/t14/Student.java
@@ -1,11 +1,4 @@
 package school.faang.t14;
 
-import lombok.AllArgsConstructor;
-import lombok.Data;
-
-@Data
-@AllArgsConstructor
-public class Student {
-    private int id;
-    private String name;
+public record Student(int id, String name) {
 }
--- src/main/java/school/faang/t14/Subject.java
+++ src/main/java/school/faang/t14/Subject.java
@@ -1,11 +1,4 @@
 package school.faang.t14;
 
-import lombok.AllArgsConstructor;
-import lombok.Data;
-
-@Data
-@AllArgsConstructor
-public class Subject {
-    private int id;
-    private String name;
+public record Subject(int id, String name) {
 }
