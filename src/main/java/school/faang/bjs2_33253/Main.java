56801f5d7144f13794e5208eb106baed8c852b97
zlomilk95 <kenobi_95@mail.ru>
2024-10-07 21:35:34 +0300

    edits

--- src/main/java/school/faang/bjs2_33253/Main.java
+++ src/main/java/school/faang/bjs2_33253/Main.java
@@ -1,78 +1,87 @@
 package school.faang.bjs2_33253;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import lombok.extern.slf4j.Slf4j;
 
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+@Slf4j
 public class Main {
-    private static final Logger LOG = LoggerFactory.getLogger(Main.class);
-    private static Map<Integer, StreamEvent> map1 = new HashMap<>();
-    private static Map<String, List<StreamEvent>> map2 = new HashMap<>();
+    private static final Map<Integer, StreamEvent> SORTED_EVENTS_BY_ID = new HashMap<>();
+    private static final Map<String, List<StreamEvent>> SORTED_EVENTS_BY_TYPE = new HashMap<>();
 
     private Main() {
     }
 
     public static void main(String[] args) {
-        StreamEvent streamEvent1 = new StreamEvent(1, "ERROR", "У пользователя возникла ошибка");
-        StreamEvent streamEvent2 = new StreamEvent(2, "INFO", "Отправка данных в бд");
-        StreamEvent streamEvent3 = new StreamEvent(3, "WARNING", "Предупреждение о том, что память заканчивается");
-        StreamEvent streamEvent4 = new StreamEvent(4, "ERROR", "Закончилась память");
+        StreamEvent userErrorEvent = new StreamEvent(2, "ERROR", "У пользователя возникла ошибка");
+        StreamEvent sendingDataEvent = new StreamEvent(2, "INFO", "Отправка данных в бд");
+        StreamEvent memoryRunningOutEvent = new StreamEvent(5, "WARNING", "Предупреждение о том, что память заканчивается");
+        StreamEvent memoryEndingEvent = new StreamEvent(5, "ERROR", "Закончилась память");
 
-        addStreamEvent(streamEvent1);
-        addStreamEvent(streamEvent2);
-        addStreamEvent(streamEvent3);
-        addStreamEvent(streamEvent4);
+        addStreamEvent(userErrorEvent);
+        addStreamEvent(sendingDataEvent);
+        addStreamEvent(memoryRunningOutEvent);
+        addStreamEvent(memoryEndingEvent);
 
-        StreamEvent streamEvent5 = searchStreamEventById(5);
-        System.out.println(streamEvent5);
+        StreamEvent newEvent = searchStreamEventById(5);
+        System.out.println(newEvent);
 
         List <StreamEvent> streamEvents = searchStreamEventByType("ERROR");
         System.out.println(streamEvents);
 
         deleteStreamEventById(2);
-        deleteStreamEventById(5);
+
+        System.out.println(SORTED_EVENTS_BY_ID);
+        System.out.println(SORTED_EVENTS_BY_TYPE);
+
     }
     public static void addStreamEvent(StreamEvent streamEvent) {
-       map1.putIfAbsent(streamEvent.getId(), streamEvent);
-       LOG.info("Событие добавлено в мапу 1");
+       SORTED_EVENTS_BY_ID.putIfAbsent(streamEvent.getId(), streamEvent);
+       log.info("Событие c id " + streamEvent.getId() +  " добавлено в мапу 1");
 
-       map2.putIfAbsent(streamEvent.getEventType(), new ArrayList<>());
-       map2.get(streamEvent.getEventType()).add(streamEvent);
-        LOG.info("Событие добавлено в мапу 2");
+       SORTED_EVENTS_BY_TYPE.putIfAbsent(streamEvent.getEventType(), new ArrayList<>());
+       SORTED_EVENTS_BY_TYPE.get(streamEvent.getEventType()).add(streamEvent);
+        log.info("Событие c id " + streamEvent.getId() +  " добавлено в мапу 2");
     }
 
     public static StreamEvent searchStreamEventById(int id) {
-        if (map1.containsKey(id)) {
-            LOG.info("Возвращаю событие с id " + id);
-            return map1.get(id);
+        if (SORTED_EVENTS_BY_ID.containsKey(id)) {
+            log.info("Возвращаю событие с id " + id);
+            return SORTED_EVENTS_BY_ID.get(id);
         } else {
-            LOG.error("События с id " + id + " не существует");
+            log.error("События с id " + id + " не существует");
             return null;
         }
     }
 
     public static List<StreamEvent> searchStreamEventByType(String eventType) {
-        if (map2.containsKey(eventType)) {
-            LOG.info("Возвращаю события с типом " + eventType);
-            return map2.get(eventType);
+        if (SORTED_EVENTS_BY_TYPE.containsKey(eventType)) {
+            log.info("Возвращаю события с типом " + eventType);
+            return SORTED_EVENTS_BY_TYPE.get(eventType);
         } else {
-            LOG.error("Событий с типом " + eventType + " не найдено");
-            return null;
+            log.error("Событий с типом " + eventType + " не найдено");
+            return new ArrayList<>();
         }
     }
 
     public static void deleteStreamEventById(int id) {
-        if (!map1.containsKey(id)) {
-            LOG.error("События c id " + id + " не существует");
+        if (!SORTED_EVENTS_BY_ID.containsKey(id)) {
+            log.error("События c id " + id + " не существует в мапе");
         } else {
-            StreamEvent streamEventToDelete = map1.get(id);
-            map1.remove(id);
-            map2.remove(streamEventToDelete.getEventType());
-            LOG.info("Событие с id " + id +  " удалено");
+            SORTED_EVENTS_BY_ID.remove(id);
+            log.info("Событие с id " + id + " удалено из памы 1");
+
+            for (Map.Entry<String, List<StreamEvent>> event : SORTED_EVENTS_BY_TYPE.entrySet()) {
+                for (StreamEvent streamEvent : event.getValue()) {
+                   if (streamEvent.getId() == id) {
+                       event.getValue().remove(streamEvent);
+                       log.info("События с id " + id + " удалено из памы 2");
+                   }
+                }
+            }
         }
     }
 }
--- src/main/java/school/faang/bjs2_33253/StreamEvent.java
+++ src/main/java/school/faang/bjs2_33253/StreamEvent.java
@@ -1,32 +1,10 @@
 package school.faang.bjs2_33253;
 
-import lombok.Getter;
-import lombok.RequiredArgsConstructor;
+import lombok.Data;
 
-import java.util.Objects;
-
-@Getter
-@RequiredArgsConstructor
+@Data
 public class StreamEvent {
     private final int id;
     private final String eventType;
     private final String data;
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        StreamEvent that = (StreamEvent) o;
-        return id == that.id && Objects.equals(eventType, that.eventType) && Objects.equals(data, that.data);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(id, eventType, data);
-    }
-
-    @Override
-    public String toString() {
-        return "{id: " + id + ", Тип события: " + eventType + ", Данные: " + data + "}";
-    }
 }
