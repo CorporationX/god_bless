45b4c16a848e2382fd255dca92614e3fdc8359d8
Maxim Gorobets <gorobets.maks@bk.ru>
2024-10-08 00:44:48 +1000

    new Battlefield class

---
+++ src/main/java/school/faang/BJS2_32861_HeroesOfMightAndMagic/Battlefield.java
@@ -0,0 +1,24 @@
+package school.faang.BJS2_32861_HeroesOfMightAndMagic;
+
+public class Battlefield {
+    public Hero battle(Hero firstHero, Hero secondHero) {
+        int firstHeroTotalDamage = getHeroTotalDamage(firstHero) + firstHero.getExperience();
+        int secondHeroTotalDamage = getHeroTotalDamage(secondHero) + secondHero.getExperience();
+
+        System.out.println("*Strange sounds of the battlefield*");
+        System.out.printf(
+                "Total damage of %s: %d, total damage of %s: %d\n",
+                firstHero.getName(), firstHeroTotalDamage, secondHero.getName(), secondHeroTotalDamage
+        );
+
+        return firstHeroTotalDamage > secondHeroTotalDamage ? firstHero : secondHero;
+    }
+
+    private int getHeroTotalDamage(Hero hero) {
+        return hero.getArmy()
+                .entrySet()
+                .stream()
+                .mapToInt(entry -> entry.getKey().getDamage() * entry.getValue())
+                .sum();
+    }
+}
