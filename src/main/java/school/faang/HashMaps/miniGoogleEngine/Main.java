6aa24c8b39702141a17d5523af13c85db627cb0d
Sergey Zhitenev <zhitenev@lanit.ru>
2024-10-07 13:58:11 +0300

    change searchPages and deletePageFromIndex methods

--- src/main/java/school/faang/HashMaps/miniGoogleEngine/Main.java
+++ src/main/java/school/faang/HashMaps/miniGoogleEngine/Main.java
@@ -10,9 +10,9 @@ public class Main {
     private static Map<String, List<WebPage>> index = new HashMap<>();
 
     public static void main(String[] args) {
-        index(new WebPage("faang.com", "faang", "Вы решили создать простой поисковой движок"));
+        index(new WebPage("faang.com", "faang", "Вы создать простой поисковой движок"));
         index(new WebPage("yandex.ru", "yandex", "какой-то текст"));
-        index(new WebPage("dzen.ru", "dzen", "Вы решили"));
+        index(new WebPage("dzen.ru", "dzen", "решили"));
         deletePageFromIndex("dzen.ru");
         System.out.println(index);
         System.out.println(searchPages("Вы"));
@@ -24,10 +24,11 @@ public class Main {
     }
 
     public static List<WebPage> searchPages(String word) {
-        return index.get(word);
+        return index.containsKey(word) ? index.get(word) : new ArrayList<>();
     }
 
     public static void deletePageFromIndex(String url) {
         index.forEach((k, v) -> v.removeIf(x -> x.getUrl().equals(url)));
+        index.values().removeIf(List::isEmpty);
     }
 }
