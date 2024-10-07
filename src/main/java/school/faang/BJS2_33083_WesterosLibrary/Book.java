027fbb8b5a228d0a037d8195816cab432c105fa3
Maxim Gorobets <gorobets.maks@bk.ru>
2024-10-07 18:38:44 +1000

    methods equals, hashCode, toString without using annotations

--- src/main/java/school/faang/BJS2_33083_WesterosLibrary/Book.java
+++ src/main/java/school/faang/BJS2_33083_WesterosLibrary/Book.java
@@ -3,8 +3,9 @@ package school.faang.BJS2_33083_WesterosLibrary;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
 
+import java.util.Objects;
+
 @Getter
-@EqualsAndHashCode
 public class Book {
     private final String title;
     private final String author;
@@ -20,6 +21,24 @@ public class Book {
         this.year = year;
     }
 
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Book book = (Book) o;
+        return year == book.year && Objects.equals(title, book.title) && Objects.equals(author, book.author);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(title, author, year);
+    }
+
+    @Override
+    public String toString() {
+        return String.format("Book with title: '%s', its author: %s, year of publication: %d", title, author, year);
+    }
+
     private void validateString(String string) {
         if (string == null || string.isBlank())
             throw new IllegalArgumentException("Invalid info.");
@@ -31,8 +50,4 @@ public class Book {
         }
     }
 
-    @Override
-    public String toString() {
-        return String.format("Book title: %s, author: %s, year: %s", title, author, year);
-    }
 }
