36d7d3a7aa6a777d792a42748ebad5acf5bfb5e1
Slava <leeteso97@gmail.com>
2024-10-07 21:25:33 +0500

    исправил методы и перенес в сервисы

--- src/main/java/school/faang/BJS233103/Main.java
+++ src/main/java/school/faang/BJS233103/Main.java
@@ -1,47 +1,11 @@
 package school.faang.BJS233103;
 
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
 public class Main {
 
-    public static Map<Book, String> BOOKS = new HashMap<>();
-
-    public static void addBook(String title, String author, int year) {
-        String place = generatePlace();
-        Book newBook = new Book(title, author, year);
-        BOOKS.put(newBook, place);
-        System.out.println("ADDED " + newBook);
-    }
-
-    public static void deleteBookByTitle(String title, String author, int year) {
-        Book removeBook = new Book(title, author, year);
-        BOOKS.remove(removeBook);
-        System.out.println("DELETED " + removeBook);
-    }
-
-    public static void findBook(String title, String author, int year) {
-        Book findBook = new Book(title, author, year);
-        for (Book book : BOOKS.keySet()) {
-            if (book.equals(findBook)) {
-                System.out.println("FOUNDED " + book + " Place: " + BOOKS.get(book));
-            }
-        }
-    }
-
-    public static void getBooks() {
-        for (Map.Entry<Book, String> entry : BOOKS.entrySet()) {
-            System.out.println(entry.getKey() + " Place: " + entry.getValue());
-        }
-    }
-
-    public static String generatePlace() {
-        char letter = (char) ((Math.random() * 25) + 65);
-        int number = (int) (Math.random() * 20);
-        return String.valueOf(letter) + number;
-    }
+    public static BookService bookService = new BookServiceImpl();
 
     public static void main(String[] args) {
         List<Book> books = new ArrayList<>();
@@ -51,13 +15,15 @@ public class Main {
         books.add(new Book("Светила", "Элеанор Каттон", 2020));
 
         for (Book book : books) {
-            BOOKS.put(book, generatePlace());
+
+            bookService.getBooks().put(book, bookService.generatePlace());
         }
-        getBooks();
-        addBook("New Age", "Christian Something", 2019);
-        deleteBookByTitle("Светила", "Элеанор Каттон", 2020);
-        findBook("Думай и богатей", "Наполеон Хилл", 1937);
-        getBooks();
+
+        bookService.printBooks();
+        bookService.addBook("New Age", "Christian Something", 2019);
+        bookService.deleteBookByTitle("Светила", "Элеанор Каттон", 2020);
+        bookService.findBook("Думай и богатей", "Наполеон Хилл", 1937);
+        bookService.printBooks();
     }
 
 }
