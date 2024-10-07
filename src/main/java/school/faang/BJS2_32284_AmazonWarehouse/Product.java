c7a502da34729fead86c32c7b330a7bb26989679
Maxim Gorobets <gorobets.maks@bk.ru>
2024-10-07 20:41:14 +1000

    new Product class

---
+++ src/main/java/school/faang/BJS2_32284_AmazonWarehouse/Product.java
@@ -0,0 +1,17 @@
+package school.faang.BJS2_32284_AmazonWarehouse;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@AllArgsConstructor
+@Getter
+public class Product {
+    int id;
+    String name;
+    String category;
+
+    @Override
+    public String toString() {
+        return String.format("Product with id: %d, category: %s, name: %s", id, category, name);
+    }
+}
