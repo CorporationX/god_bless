36c25c72f949f786f871c11944394e7b884487ed
Svyatoslav Kostrov <kost-2014@mail.ru>
2024-10-08 00:39:10 +0300

    186

--- src/main/java/school/faang/task1310/Server.java
+++ src/main/java/school/faang/task1310/Server.java
@@ -2,13 +2,9 @@ package school.faang.task1310;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
-import lombok.EqualsAndHashCode;
-import lombok.ToString;
 
 @Data
-@EqualsAndHashCode
 @AllArgsConstructor
-@ToString
 public class Server {
 
     private double load;
