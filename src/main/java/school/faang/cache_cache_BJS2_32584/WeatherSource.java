e568760c3389712263b62cbd49b0de061ccc2670
VovaBuzivskyi <VovaBuzivskyi@gmail.com>
2024-10-08 03:10:05 +0700

    BJS2-32584 cache cache (added constants for random generator)

--- src/main/java/school/faang/cache_cache_BJS2_32584/WeatherSource.java
+++ src/main/java/school/faang/cache_cache_BJS2_32584/WeatherSource.java
@@ -2,7 +2,11 @@ package school.faang.cache_cache_BJS2_32584;
 
 public class WeatherSource {
 
+    private static final int MAX_TEMPERATURE = 40;
+    private static final int MAX_HUMIDITY = 90;
+
     public WeatherData fetchWeather(String city) {
-        return new WeatherData(city, Math.round(Math.random() * 40), Math.round(Math.random() * 90));
+        return new WeatherData(city, Math.round(Math.random() * MAX_TEMPERATURE)
+                , Math.round(Math.random() * MAX_HUMIDITY));
     }
 }
