5d9e2f93e0cdcb0f52c8f74b92b66b7b23f2b325
Nikolay Orlenko <norlenko2801@gmail.com>
2024-10-07 22:03:31 +0300

    Исправил название переменных в методе main

--- src/main/java/school/faang/catching/events/Main.java
+++ src/main/java/school/faang/catching/events/Main.java
@@ -4,18 +4,18 @@ import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
-        Event event1 = new Event(1, "Работа", "Созвон насчет фичи с разработчиками Агента");
-        Event event2 = new Event(2, "Работа", "Ретро с командой");
-        Event event3 = new Event(3, "Спорт", "Поход в зал");
-        Event event4 = new Event(4, "Спорт", "Бег");
-        Event event5 = new Event(5, "Личная жизнь", "Увидеться с друзьями");
+        Event teamCall = new Event(1, "Работа", "Созвон насчет фичи с разработчиками Агента");
+        Event retro = new Event(2, "Работа", "Ретро с командой");
+        Event gym = new Event(3, "Спорт", "Поход в зал");
+        Event running = new Event(4, "Спорт", "Бег");
+        Event meeting = new Event(5, "Личная жизнь", "Увидеться с друзьями");
 
         StreamEvent eventStream = new StreamEvent();
-        eventStream.add(event1);
-        eventStream.add(event2);
-        eventStream.add(event3);
-        eventStream.add(event4);
-        eventStream.add(event5);
+        eventStream.add(teamCall);
+        eventStream.add(retro);
+        eventStream.add(gym);
+        eventStream.add(running);
+        eventStream.add(meeting);
         eventStream.printEvents();
 
         System.out.println("Событие с id 1: " + eventStream.get(1));
