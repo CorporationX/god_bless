80595d2380464c1440aad4b30ba973f1bafee0f8
Nikolay Orlenko <norlenko2801@gmail.com>
2024-10-07 22:08:23 +0300

    1. Убрал лишние пробелы 2. Изменил названия гербов 3. Дал более осмысленные названия переменным в методе main 4. Заменил аннотацию Data на Getter

--- src/main/java/school/faang/game/of/thrones/House.java
+++ src/main/java/school/faang/game/of/thrones/House.java
@@ -1,9 +1,9 @@
 package school.faang.game.of.thrones;
 
 import lombok.AllArgsConstructor;
-import lombok.Data;
+import lombok.Getter;
 
-@Data
+@Getter
 @AllArgsConstructor
 public class House {
     private String name;
