170013e310d5d5ef9a094ed33db03da70851c32f
lomuserg <sergeymu2015@yandex.ru>
2024-10-07 22:20:10 +0300

    init SpellAction.java, SpellCaster.java,  and method cast

--- src/main/java/school/faang/Main.java
+++ src/main/java/school/faang/Main.java
@@ -1,27 +1,17 @@
 package school.faang;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-import java.util.Scanner;
 
 public class Main {
-    public static List<User> usersList = new ArrayList<>();
 
     public static void main(String[] args) {
-        User user = new User("Sergey", "21", "Student", "Moscow");
-        User user2 = new User("Dmitry", "21", "Programmer", "Moscow");
-        User user3 = new User("Yakov", "20", "Student", "Moscow");
-        User user4 = new User("Michail", "21", "Programmer", "Moscow");
-        usersList.add(user);
-        usersList.add(user2);
-        usersList.add(user3);
-        usersList.add(user4);
-        System.out.println("age of user = "+user.getAge()+"\n");
-        Map<String, List<User>> mapByAge = user.groupUsers(usersList);
-        System.out.println(mapByAge.get("21"));
+        SpellCaster spellCaster = new SpellCaster();
+        String alohomora = "Alohomora";
+        String lumos = "Lumos";
+        String expelliarmus = "Expelliarmus";
+
+        spellCaster.cast(alohomora, (spell) -> "The door is unlocked by " + spell);
+        spellCaster.cast(lumos, (spell) -> "A beam of light is created by " + spell);
+        spellCaster.cast(expelliarmus, (spell) -> "The opponent is disarmed by " + spell);
 
     }
 
