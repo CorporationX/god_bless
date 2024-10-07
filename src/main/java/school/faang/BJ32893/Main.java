f8cc0ff9b7ce043529b2f9e62751fab5d81a1401
Doniyor Kurbanov <doniyor.kurbanov.24@gmail.com>
2024-10-07 23:31:46 +0500

    BJS2-32893. Жду фитбека. Спасибо большое

--- src/main/java/school/faang/BJ32893/Main.java
+++ src/main/java/school/faang/BJ32893/Main.java
@@ -8,41 +8,53 @@ import java.util.Map;
 
 public class Main {
     private static final Map<String, List<WebPage>> INDEX = new HashMap<>();
+    private static final Map<String, List<String>> urlToWords = new HashMap<>();
 
-    void indexWebPage(WebPage webPage) {
+    public static void indexWebPage(WebPage webPage) {
         String content = webPage.getContent();
         String[] words = content.split("\\s+");
+        String url = webPage.getUrl();
+        List<String> listOfWords = new ArrayList<>();
 
         for (String keyword : words) {
             keyword = keyword.toLowerCase();
             INDEX.putIfAbsent(keyword, new ArrayList<>());
             INDEX.get(keyword).add(webPage);
+            listOfWords.add(keyword);
         }
+        urlToWords.put(url, listOfWords);
     }
 
-    List<WebPage> searchWebPage(String keyword) {
+    public static List<WebPage> searchWebPage(String keyword) {
         List<WebPage> listOfPages = INDEX.get(keyword);
         if (listOfPages == null) {
-            throw new IllegalArgumentException("404 not found");
+            throw new IllegalArgumentException("404 not found " + keyword);
         }
         return listOfPages;
     }
 
-    List<WebPage> deletePageByUrl(String url) {
+    public static List<WebPage> deletePageByUrl(String url) {
         List<WebPage> listPages = new ArrayList<>();
+        List<String> keywords = urlToWords.get(url);
 
-        for (Map.Entry<String, List<WebPage>> entry : INDEX.entrySet()) {
-            List<WebPage> webPages = entry.getValue();
-            Iterator<WebPage> iterator = webPages.iterator();
-            while (iterator.hasNext()) {
-                WebPage webPage = iterator.next();
-                if (webPage.getUrl().equals(url)) {
-                    listPages.add(webPage);
-                    iterator.remove();
+        if (keywords == null) {
+            return listPages;
+        }
+
+        for (String keyword : keywords) {
+            List<WebPage> webPages = INDEX.get(keyword);
+            if (webPages != null) {
+                Iterator<WebPage> iterator = webPages.iterator();
+                while (iterator.hasNext()) {
+                    WebPage webPage = iterator.next();
+                    if (webPage.getUrl().equals(url)) {
+                        listPages.add(webPage);
+                        iterator.remove();
+                    }
                 }
-            }
-            if (webPages.isEmpty()) {
-                iterator.remove();
+                if (webPages.isEmpty()) {
+                    INDEX.remove(keyword);
+                }
             }
         }
         return listPages;
