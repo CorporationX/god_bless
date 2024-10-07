c8a1465a63bbd193594edc15f36780230bdf22e7
Aleksandr Dainiak <a.dainiak@arammeem.com>
2024-10-07 18:16:52 +0200

    BJS2-33135 Java HashMap: Библиотека Вестеросс. Code review fixes

--- src/main/java/school/faang/secondTask/Book.java
+++ src/main/java/school/faang/secondTask/Book.java
@@ -8,19 +8,4 @@ public class Book {
     final private String title;
     final private String author;
     final private int year;
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        Book book = (Book) o;
-        return year == book.year &&
-                Objects.equals(title, book.title) &&
-                Objects.equals(author, book.author);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(title, author, year);
-    }
 }
