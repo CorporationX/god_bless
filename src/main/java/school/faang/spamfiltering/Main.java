7acc44219c32d47a17eb12a43bcca81ef668be9f
Aleksey <aleksei.zuk86@gmail.com>
2024-10-07 18:25:02 +0300

    Google Переводчик под нашим контролем…

---
+++ src/main/java/school/faang/spamfiltering/Main.java
@@ -0,0 +1,25 @@
+package school.faang.spamfiltering;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) {
+        MessageProcessor messageProcessor = new MessageProcessor();
+
+        MessageFilter spamFilter =
+                message -> !message.toLowerCase().contains("spam");
+        MessageFilter lengthFilter = message -> message.length() > 10;
+        MessageFilter emojiFilter = message -> !message.contains("😀");
+
+        List<MessageFilter> filters = Arrays.asList(spamFilter, lengthFilter, emojiFilter);
+
+        String[] messages = {"Hello!", "This is spam!", "How are you? 😀",
+                "Длинное сообщение без спама и эмодзи"};
+
+        for (String message : messages) {
+            boolean isFiltered = messageProcessor.processMessage(message, filters);
+            System.out.println("Сообщение: " + message + " | Пропущено: " + isFiltered);
+        }
+    }
+}
---
+++ src/main/java/school/faang/spamfiltering/MessageFilter.java
@@ -0,0 +1,5 @@
+package school.faang.spamfiltering;
+
+public interface MessageFilter {
+    boolean filter(String message);
+}
