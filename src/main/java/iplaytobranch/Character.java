9f8e835fc455f5ffd15b5b5554981a312116eded
DavidT <davidx5@bk.ru>
2024-10-07 20:27:37 +0300

    rezalt

---
+++ src/main/java/iplaytobranch/Character.java
@@ -0,0 +1,22 @@
+package iplaytobranch;
+
+public abstract class Character {
+    protected String name;
+    protected int power;
+    protected int skil;
+    protected int intelligence;
+    protected int health = 100;
+
+    public Character(String name) {
+        this.name = name;
+    }
+
+    public Character(int power, int intelligence, int skil,String name) {
+        this.power = power;
+        this.intelligence = intelligence;
+        this.skil = skil;
+        this.name = name;
+    }
+
+    public abstract void attack(Character character);
+}
