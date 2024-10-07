4345f70337f3d83975052a0b7f4632aceb54a963
Igor <asachiyigor@gmail.com>
2024-10-07 21:13:36 +0300

    BJS2-34655
    initial commit

---
+++ src/main/java/school/faangSprint2/t2/Notification.java
@@ -0,0 +1,11 @@
+package school.faangSprint2.t2;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@AllArgsConstructor
+@Getter
+public class Notification {
+    private String type;
+    private String message;
+}
