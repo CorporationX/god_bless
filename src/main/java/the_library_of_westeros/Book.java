401da98e22856e8ccc3114bb19dd3983cb0c7773
Юрий <angelbes777@gmail.com>
2024-10-07 10:17:08 +0300

    Fix by MR the_library_of_westeros

--- src/main/java/the_library_of_westeros/Book.java
+++ src/main/java/the_library_of_westeros/Book.java
@@ -1,4 +1,12 @@
 package the_library_of_westeros;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
 public class Book {
+    private String title;
+    private String author;
+    private int year;
 }
