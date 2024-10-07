0b759baf157b3fdb306b7044c1cf3515881c9c2f
Doniyor Kurbanov <doniyor.kurbanov.24@gmail.com>
2024-10-08 00:41:38 +0500

    BJS2-32492. Жду фитбека. Спасибо большое

---
+++ src/main/java/school/faang/BS232492/House.java
@@ -0,0 +1,11 @@
+package school.faang.BS232492;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class House {
+    private final String name;
+    private final String sigil;
+}
