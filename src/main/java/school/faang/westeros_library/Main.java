5292ea385d4cac137c0dbb4a6e6cdb5ddc86842f
Yevgeniy <yevgengeodakyan@gmail.com>
2024-10-07 07:46:44 -0400

    one more try

--- src/main/java/school/faang/westeros_library/Main.java
+++ src/main/java/school/faang/westeros_library/Main.java
@@ -2,20 +2,21 @@ package school.faang.westeros_library;
 
 import java.util.HashMap;
 import java.util.Map;
-import java.util.NoSuchElementException;
 import java.util.Optional;
 
 public class Main {
 
     public static void main(String[] args) {
+
+        var book = new Book("The Catcher in the Rye", "J.D.Salinger", 1951);
         var library = new HashMap<Book, String>();
         library.put(new Book("To Kill a Mockingbird", "Harper Lee", 1960), "SP1");
         library.put(new Book("1984", "George Orwell", 1949), "SP2");
         library.put(new Book("The Great Gatsby", "F.Scott Fitzgerald", 1925), "SP3");
 
-        System.out.println("Add book: " + addBook(library, new Book("The Catcher in the Rye", "J.D.Salinger", 1951), "SP5"));
-        System.out.println("Book to remove: " + removeBook(library, "1984", "George Orwell", 1949));
-        System.out.println("SearchBook: " + searchBook(library, "The Great Gatsby", "F.Scott Fitzgerald", 1925));
+        System.out.println("Add book: " + addBook(library, book, "SP5"));
+        System.out.println("Book to remove: " + removeBook(library, book));
+        System.out.println("SearchBook: " + searchBook(library, book));
         System.out.println("Printed books:");
         printBooks(library);
     }
@@ -25,31 +26,14 @@ public class Main {
         return book;
     }
 
-    private static Book removeBook(Map<Book, String> library, String title, String author, int year)
-            throws NoSuchElementException {
-        Book bookToRemove = null;
-        for (var entry : library.entrySet()) {
-            Book book = entry.getKey();
-            if (book.title().equalsIgnoreCase(title) && book.author().equalsIgnoreCase(author) && book.year() == year) {
-                bookToRemove = book;
-                library.remove(bookToRemove);
-            }
-        }
-        return Optional.ofNullable(bookToRemove).orElseThrow(() ->
-                new NoSuchElementException("Book can not be removed, cause: not exits"));
+    private static Optional<Book> removeBook(Map<Book, String> library, Book book) {
+        library.remove(book);
+        return Optional.of(book);
     }
 
-    private static Book searchBook(Map<Book, String> library, String title, String author, int year)
-            throws NoSuchElementException {
-        Book resultBook = null;
-        for (var entry : library.entrySet()) {
-            Book book = entry.getKey();
-            if (book.title().equalsIgnoreCase(title) && book.author().equalsIgnoreCase(author) && book.year() == year) {
-                resultBook = entry.getKey();
-            }
-        }
-        return Optional.ofNullable(resultBook).orElseThrow(() ->
-                new NoSuchElementException("Couldn't find particular book"));
+    private static Optional<Book> searchBook(Map<Book, String> library, Book book) {
+        library.get(book);
+        return Optional.of(book);
     }
 
     private static void printBooks(Map<Book, String> library) {
