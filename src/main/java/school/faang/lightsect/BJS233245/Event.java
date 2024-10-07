1b66a3fd7c2c2e0b3630e171b6bd87977caafbe7
Doniyor Kurbanov <doniyor.kurbanov.24@gmail.com>
2024-10-07 23:57:55 +0500

    BJS2-33245. Жду фитбека. Спасибо большое

--- src/main/java/school/faang/lightsect/BJS233245/Event.java
+++ src/main/java/school/faang/lightsect/BJS233245/Event.java
@@ -6,33 +6,28 @@ import java.util.List;
 import java.util.Map;
 
 public class Event {
-    static final Map<Integer, StreamEvent> STREAM_EVENT = new HashMap<>();
+    private static final Map<Integer, StreamEvent> STREAM_EVENT_BY_ID = new HashMap<>();
     static final Map<String, List<StreamEvent>> LIST_OF_EVENT_WITH_TYPE = new HashMap<>();
 
-    void addEvent(StreamEvent newEvent) {
-        STREAM_EVENT.put(newEvent.getId(), newEvent);
+    public static void addEvent(StreamEvent newEvent) {
+        STREAM_EVENT_BY_ID.put(newEvent.getId(), newEvent);
         LIST_OF_EVENT_WITH_TYPE.computeIfAbsent(newEvent.getEventType(), key -> new ArrayList<>()).add(newEvent);
     }
 
-    StreamEvent getEventById(int id) {
-        return STREAM_EVENT.get(id);
+    public static StreamEvent getEventById(int id) {
+        return STREAM_EVENT_BY_ID.get(id);
     }
 
-    List<StreamEvent> searchStreamEvenByType(String type) {
-        return LIST_OF_EVENT_WITH_TYPE.get(type);
+    public static List<StreamEvent> searchStreamEvenByType(String event) {
+        return LIST_OF_EVENT_WITH_TYPE.get(event);
     }
 
-    void deleteStreamById(int id) {
-        StreamEvent event = STREAM_EVENT.remove(id);
-        if (event != null) {
-            LIST_OF_EVENT_WITH_TYPE.get(event.getEventType()).removeIf(e -> e.getId() == id);
-        }
-
-        STREAM_EVENT.remove(id);
+    public static void deleteStreamById(int id) {
+        STREAM_EVENT_BY_ID.remove(id);
     }
 
-    void printAllEvents() {
-        STREAM_EVENT.forEach((key, value) -> System.out.printf("id: %d, type: %s, data: %s\n",
+    public static void printAllEvents() {
+        STREAM_EVENT_BY_ID.forEach((key, value) -> System.out.printf("id: %d, type: %s, data: %s\n",
                 key, value.getEventType(), value.getData()));
     }
 
