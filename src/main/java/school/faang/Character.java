8d156c6b4415b2079ba9ac45ca6db07be7e41c9c
abzalkhan00 <abzalkhanondakanov@gmail.com>
2024-10-08 12:31:42 +0500

    Solved abstraction task

---
+++ src/main/java/school/faang/Character.java
@@ -0,0 +1,28 @@
+package school.faang;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public abstract class Character {
+    private String name;
+    private int power;
+    private int dexterity;
+    private int intelligence;
+    private int health = 100;
+
+    public Character(String name) {
+        this.name = name;
+    }
+
+    public Character(String name, int power, int dexterity, int intelligence) {
+        this.name = name;
+        this.power = power;
+        this.dexterity = dexterity;
+        this.intelligence = intelligence;
+    }
+
+    public abstract void attack(Character character);
+
+}
--- src/main/java/school/faang/Main.java
+++ src/main/java/school/faang/Main.java
@@ -1,4 +1,16 @@
 package school.faang;
 
 public class Main {
+    public static void main(String[] args) {
+        Warrior warrior = new Warrior("Abzalkhan");
+        Archer archer = new Archer("Olzhas");
+        warrior.attack(archer);
+        warrior.attack(archer);
+        warrior.attack(archer);
+        System.out.println(archer.getHealth());
+        archer.attack(warrior);
+        archer.attack(warrior);
+        archer.attack(warrior);
+        System.out.println(warrior.getHealth());
+    }
 }
