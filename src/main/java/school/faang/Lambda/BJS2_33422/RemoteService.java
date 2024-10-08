f8bbf90f4bf3d4914a1e6dfa09a37831c505577b
0Miha0 <140548440+0Miha0@users.noreply.github.com>
2024-10-08 17:59:45 +0500

    https://faang-school.atlassian.net/browse/BJS2-33422

---
+++ src/main/java/school/faang/Lambda/BJS2_33422/RemoteService.java
@@ -0,0 +1,7 @@
+package school.faang.Lambda.BJS2_33422;
+
+public class RemoteService {
+    public static String call(String param) throws Exception {
+        throw new Exception("Сервис недоступен");
+    }
+}
