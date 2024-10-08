6d810010fd465945a324ebc7d15050fb320ffcae
Maxim Gorobets <gorobets.maks@bk.ru>
2024-10-09 00:46:33 +1000

    fixed SpellAction method modifier

--- src/main/java/school/faang/BJS2_33819_Hogwarts/SpellAction.java
+++ src/main/java/school/faang/BJS2_33819_Hogwarts/SpellAction.java
@@ -2,5 +2,5 @@ package school.faang.BJS2_33819_Hogwarts;
 
 @FunctionalInterface
 public interface SpellAction {
-    public String getSpellResult(String spell);
+    String getSpellResult(String spell);
 }
