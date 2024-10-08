d88352a523ae7b778c1af4960e68697921c4750c
Gadzhiev Gadzhi <Gadzhiev3222@gmail.com>
2024-10-08 17:58:35 +0300

    .

--- src/main/java/school/faang/gmailrichfilter/main/Main.java
+++ src/main/java/school/faang/gmailrichfilter/main/Main.java
@@ -6,7 +6,6 @@ import school.faang.gmailrichfilter.maincode.EmailProcessor;
 
 import java.util.Arrays;
 import java.util.List;
-
 import java.util.function.Consumer;
 import java.util.function.Function;
 import java.util.function.Predicate;
