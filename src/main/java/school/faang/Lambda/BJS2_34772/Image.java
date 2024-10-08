2ca30000e71fec9d750445b2a2a0885cf4ab13a6
0Miha0 <140548440+0Miha0@users.noreply.github.com>
2024-10-08 12:16:10 +0500

    https://faang-school.atlassian.net/browse/BJS2-34772

---
+++ src/main/java/school/faang/Lambda/BJS2_34772/Image.java
@@ -0,0 +1,11 @@
+package school.faang.Lambda.BJS2_34772;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class Image {
+    private String name;
+    private String description;
+}
