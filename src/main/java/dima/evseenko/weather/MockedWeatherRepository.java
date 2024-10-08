3c82fdced240262d378b8572d573a41650c6c653
Dmytro Yevsieienko <youdima70@gmail.com>
2024-10-08 12:54:01 +0200

    Рефакторинг

--- src/main/java/dima/evseenko/weather/MockedWeatherRepository.java
+++ src/main/java/dima/evseenko/weather/MockedWeatherRepository.java
@@ -8,15 +8,17 @@ import static org.mockito.Mockito.when;
 public class MockedWeatherRepository implements WeatherRepository {
     private final Map<String, WeatherData> weatherDataList = mock(Map.class);
 
-    @Override
-    public WeatherData getWeather(String city) {
+    public MockedWeatherRepository() {
         when(weatherDataList.get("Санкт-Петербург")).thenReturn(new WeatherData("Санкт-Петербург", 9, 91));
         when(weatherDataList.get("Москва")).thenReturn(new WeatherData("Москва", 12, 79));
         when(weatherDataList.get("Екатеринбург")).thenReturn(new WeatherData("Екатеринбург", 16, 25));
         when(weatherDataList.get("Ростов-на-Дону")).thenReturn(new WeatherData("Ростов-на-Дону", 24, 25));
         when(weatherDataList.get("Краснодар")).thenReturn(new WeatherData("Краснодар", 25, 34));
         when(weatherDataList.get("Воронеж")).thenReturn(new WeatherData("Воронеж", 22, 23));
+    }
 
+    @Override
+    public WeatherData getWeather(String city) {
         return weatherDataList.get(city);
     }
 }
