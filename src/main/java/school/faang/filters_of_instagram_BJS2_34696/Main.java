9cbd5d401e108ec5644c484e2d273ce59750b094
VovaBuzivskyi <VovaBuzivskyi@gmail.com>
2024-10-08 15:37:39 +0700

    BJS2-34696 filters of instagram

---
+++ src/main/java/school/faang/filters_of_instagram_BJS2_34696/Main.java
@@ -0,0 +1,30 @@
+package school.faang.filters_of_instagram_BJS2_34696;
+
+import java.util.function.Function;
+
+public class Main {
+    public static void main(String[] args) {
+        Image originalImage = new Image("original.jpg", "Оригинальное изображение");
+        FilterProcessor filterProcessor = new FilterProcessor();
+
+        Function<Image, Image> grayscaleFilter = (image) -> new Image(image.getName()
+                , image.getDescription() + " | Фильтр: черно-белый");
+        Function<Image, Image> sepiaFilter = (image) -> new Image(image.getName()
+                , image.getDescription() + " | Фильтр: сепия");
+        Function<Image, Image> highContrast = (image) -> new Image(image.getName()
+                , image.getDescription() + " | Фильтр: высокий контраст");
+        Function<Image, Image> sharpenFilter = (image) -> new Image(image.getName()
+                , image.getDescription() + " | Фильтр: резкость");
+
+        Image grayscaleImage = filterProcessor.applyFilter(originalImage, grayscaleFilter);
+        System.out.println(grayscaleImage.getDescription());
+
+        Image sepiaImage = filterProcessor.applyFilter(grayscaleImage, sepiaFilter);
+        System.out.println(sepiaImage.getDescription());
+
+        Function<Image, Image> combinedFilter = filterProcessor.combineFilters(sharpenFilter, highContrast);
+        Image combinedImage = filterProcessor.applyFilter(originalImage, combinedFilter);
+        System.out.println(combinedImage.getDescription());
+
+    }
+}
