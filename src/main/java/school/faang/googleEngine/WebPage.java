6c42b009dba26f43f7e3402d74b0102a817cf848
SiarheiHaurylchuk <sggavrilchik@gmail.com>
2024-10-08 17:34:17 +0300

    engine

---
+++ src/main/java/school/faang/googleEngine/WebPage.java
@@ -0,0 +1,49 @@
+package school.faang.googleEngine;
+
+import java.util.Objects;
+
+public class WebPage {
+    private String url;
+    private String title;
+    private String content;
+
+    public WebPage(String url, String title, String content) {
+        this.url = url;
+        this.title = title;
+        this.content = content;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        if (this == object) return true;
+        if (object == null || getClass() != object.getClass()) return false;
+        WebPage webPage = (WebPage) object;
+        return Objects.equals(url, webPage.url) && Objects.equals(title, webPage.title) && Objects.equals(content, webPage.content);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(url, title, content);
+    }
+
+    @Override
+    public String toString() {
+        return "WebPage{" +
+                "url='" + url + '\'' +
+                ", title='" + title + '\'' +
+                ", content='" + content + '\'' +
+                '}';
+    }
+}
