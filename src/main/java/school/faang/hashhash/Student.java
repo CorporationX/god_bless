b199a3e8f568dc1d2a84df6701311bc108737941
Alexander Litovkin <litovkinalexan@gmail.com>
2024-10-08 12:03:35 +0700

    refactor student and subject classes

--- src/main/java/school/faang/hashhash/Student.java
+++ src/main/java/school/faang/hashhash/Student.java
@@ -1,12 +1,26 @@
 package school.faang.hashhash;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
-import lombok.RequiredArgsConstructor;
+
+import java.util.Objects;
 
 @Data
-@RequiredArgsConstructor
+@AllArgsConstructor
 public class Student {
 
     private final int id;
-    private final String name;
+    private String name;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Student student)) return false;
+        return id == student.id;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(id);
+    }
 }
--- src/main/java/school/faang/hashhash/Subject.java
+++ src/main/java/school/faang/hashhash/Subject.java
@@ -1,12 +1,26 @@
 package school.faang.hashhash;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
-import lombok.RequiredArgsConstructor;
+
+import java.util.Objects;
 
 @Data
-@RequiredArgsConstructor
+@AllArgsConstructor
 public class Subject {
 
     private final int id;
-    private final String name;
+    private String name;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Subject subject)) return false;
+        return id == subject.id;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(id);
+    }
 }
