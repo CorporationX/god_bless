2c95f0c930f528a76a908e48610542da68b6b2cd
light <estarosa@list.ru>
2024-10-08 11:16:05 +0300

    Решение задачи: Meta-вселенная

---
+++ src/main/java/bjs2_34724/Notification.java
@@ -0,0 +1,11 @@
+package bjs2_34724;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class Notification {
+    private String type;
+    private String message;
+}
