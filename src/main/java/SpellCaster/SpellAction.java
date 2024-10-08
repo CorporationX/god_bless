d19f5e41dc6feacbfc7743518b75fbc3e866ecdd
DavidT <davidx5@bk.ru>
2024-10-09 00:11:04 +0300

    rezalt

---
+++ src/main/java/SpellCaster/SpellAction.java
@@ -0,0 +1,6 @@
+package SpellCaster;
+
+@FunctionalInterface
+public interface SpellAction {
+    String castSpell(String spellName);
+}
---
+++ src/main/java/SpellCaster/SpellCaster.java
@@ -0,0 +1,9 @@
+package SpellCaster;
+
+public class SpellCaster {
+
+    public void cast(String spellName, SpellAction action) {
+        String result = action.castSpell(spellName);
+        System.out.println("Результат заклинания: " + result);
+    }
+}
