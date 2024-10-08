140104a08f95a6d0c8adf133365477f99a3adc23
AnnaAplavina <59871896+AnnaAplavina@users.noreply.github.com>
2024-10-08 12:00:12 +0300

    no data annotation and formatting

--- src/main/java/school/faang/godbless/bjs2_32408/House.java
+++ src/main/java/school/faang/godbless/bjs2_32408/House.java
@@ -2,11 +2,11 @@ package school.faang.godbless.bjs2_32408;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.Getter;
 
-@Data
+@Getter
 @AllArgsConstructor
 public class House {
-
-  private String name;
-  private String sigil;
+    private String name;
+    private String sigil;
 }
--- src/main/java/school/faang/godbless/bjs2_32408/Main.java
+++ src/main/java/school/faang/godbless/bjs2_32408/Main.java
@@ -4,40 +4,39 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class Main {
+    private static final Map<String, House> housesMap = new HashMap<>();
 
-  private static final Map<String, House> housesMap = new HashMap<>();
+    public static void main(String[] args) {
+        House starkHouse = new House("Stark", "Wolf");
+        addHouse(starkHouse);
+        House baratheonHouse = new House("Baratheon", "Deer");
+        addHouse(baratheonHouse);
+        House lanisterHouse = new House("Lanister", "Lion");
+        addHouse(lanisterHouse);
+        printHouses();
 
-  public static void main(String[] args) {
-    House starkHouse = new House("Stark", "Wolf");
-    addHouse(starkHouse);
-    House baratheonHouse = new House("Baratheon", "Deer");
-    addHouse(baratheonHouse);
-    House lanisterHouse = new House("Lanister", "Lion");
-    addHouse(lanisterHouse);
-    printHouses();
+        removeByHouseName("Lanister");
+        printHouses();
 
-    removeByHouseName("Lanister");
-    printHouses();
-
-    printSigil("Stark");
-  }
-
-  private static void addHouse(House house) {
-    housesMap.put(house.getName(), house);
-  }
-
-  private static void removeByHouseName(String houseName) {
-    housesMap.remove(houseName);
-  }
-
-  private static void printSigil(String houseName) {
-    if (!housesMap.containsKey(houseName)) {
-      throw new IllegalArgumentException("No such house");
+        printSigil("Stark");
     }
-    System.out.println(housesMap.get(houseName).getSigil());
-  }
 
-  private static void printHouses() {
-    System.out.println(housesMap);
-  }
+    private static void addHouse(House house) {
+        housesMap.put(house.getName(), house);
+    }
+
+    private static void removeByHouseName(String houseName) {
+        housesMap.remove(houseName);
+    }
+
+    private static void printSigil(String houseName) {
+        if (!housesMap.containsKey(houseName)) {
+            throw new IllegalArgumentException("No such house");
+        }
+        System.out.println(housesMap.get(houseName).getSigil());
+    }
+
+    private static void printHouses() {
+        System.out.println(housesMap);
+    }
 }
