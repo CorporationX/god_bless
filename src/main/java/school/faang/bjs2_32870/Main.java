2f5ee5ab91cace2cb3b6e6af298235c850b51627
zlomilk95 <kenobi_95@mail.ru>
2024-10-08 20:23:37 +0300

    edits

--- src/main/java/school/faang/bjs2_32870/Main.java
+++ src/main/java/school/faang/bjs2_32870/Main.java
@@ -1,6 +1,5 @@
 package school.faang.bjs2_32870;
 
-import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -12,34 +11,27 @@ public class Main {
     }
 
     public static void main(String[] args) {
-        WebPage webPage1 = new WebPage("https://apteka.ru", "Круглосуточная аптека \"АптекаRu\"", "Аптека работает 24/7. Есть все лекарства");
-        WebPage webPage2 = new WebPage("https://astro.com", "Астрономия простым языком", "Галактика, Млечный путь, Комета. О чем вам говорят эти слова?");
-        WebPage webPage3 = new WebPage("https://Eapteka.ru", "ЕАптека", "То же работает 24/7. То же есть все лекарства");
+        WebPage webPage1 = new WebPage("https://apteka.ru", "24-hour pharmacy \"AptekaRu\"", "The pharmacy is open 24/7. All medicines are available");
+        WebPage webPage2 = new WebPage("https://astro.com", "Astronomy in simple terms", "Galaxy, Milky Way, Comet. What do these words tell you?");
+        WebPage webPage3 = new WebPage("https://Eapteka.ru", "EApteka", "The same works 24/7. The same goes for all the medicines");
 
         indexingWebPage(webPage1);
         indexingWebPage(webPage2);
         indexingWebPage(webPage3);
 
-        System.out.println(searchByKeyword("24/7"));
-        System.out.println(searchByKeyword("Комета"));
+        System.out.println(searchPagesByKeyword("24/7"));
+        System.out.println(searchPagesByKeyword("Milky Way"));
     }
 
     private static void indexingWebPage(WebPage webPage) {
         List<String> keywords = List.of(webPage.getContent().split("[\\s.]+"));
 
         for (String keyword : keywords) {
-            SEARCH_ENGINE.putIfAbsent(keyword, new ArrayList<>());
-            SEARCH_ENGINE.get(keyword).add(webPage);
+            SEARCH_ENGINE.putIfAbsent(keyword, List.of(webPage));
         }
     }
 
-    private static List<WebPage> searchByKeyword(String keyword) {
-        List<WebPage> sortedPages = new ArrayList<>();
-        for (Map.Entry<String, List<WebPage>> page : SEARCH_ENGINE.entrySet()) {
-            if (page.getKey().equals(keyword)) {
-                sortedPages = page.getValue();
-            }
-        }
-        return sortedPages;
+    private static List<WebPage> searchPagesByKeyword(String keyword) {
+       return SEARCH_ENGINE.get(keyword);
     }
 }
