c953bdadda3e1c4597b111c5df9689759f1eb5ea
Roman Nifontov <romannif@mail.ru>
2024-10-08 19:15:07 +0500

    Сделал изменения в задаче BJS2-33687

--- src/main/java/school/BJS2_33687/SpellCaster.java
+++ src/main/java/school/BJS2_33687/SpellCaster.java
@@ -2,7 +2,8 @@ package school.BJS2_33687;
 
 public class SpellCaster {
 
-    static void cast(String spell, SpellAction spellAction) {
+    //Не знаю почему он стал стачным)) видимо я был не в себе)
+    public void cast(String spell, SpellAction spellAction) {
         System.out.println(spellAction.action(spell));
     }
 }
