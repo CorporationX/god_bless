2dcd6dff4ac2647b83d963d84a44e6c735bb6aa5
Denis Levkhov <levkhovde@yandex.ru>
2024-10-08 22:53:12 +0300

    Changing access

--- src/main/java/filtersInstagram/FilterProcessor.java
+++ src/main/java/filtersInstagram/FilterProcessor.java
@@ -4,11 +4,11 @@ import java.util.function.Function;
 
 public class FilterProcessor {
 
-    Image applyFilter(Image image, Function<Image, Image> filter) {
+    public Image applyFilter(Image image, Function<Image, Image> filter) {
         return filter.apply(image);
     }
 
-    Function<Image, Image> combineFilters(Function<Image, Image> firstFilter, Function<Image, Image> secondFilter) {
+    public Function<Image, Image> combineFilters(Function<Image, Image> firstFilter, Function<Image, Image> secondFilter) {
         return firstFilter.andThen(secondFilter);
     }
 }
