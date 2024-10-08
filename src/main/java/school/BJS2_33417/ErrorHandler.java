109f462ddc7b26160ffaae8d20b9966cfa2777eb
Roman Nifontov <romannif@mail.ru>
2024-10-08 06:27:53 +0500

    Сделал изменения в задаче BJS2-33417

--- src/main/java/school/BJS2_33417/ErrorHandler.java
+++ src/main/java/school/BJS2_33417/ErrorHandler.java
@@ -4,11 +4,11 @@ import java.util.function.Function;
 
 public class ErrorHandler {
 
-    public static <T> T withErrorHandling (InterfaceWithException<T> methodCanThrowException, Function<Exception,T> function){
+    public static <T> T withErrorHandling(InterfaceWithException<T> methodCanThrowException, Function<Exception, T> function) {
         try {
             return methodCanThrowException.get();
         } catch (Exception e) {
-          return function.apply(e);
+            return function.apply(e);
         }
     }
 }
