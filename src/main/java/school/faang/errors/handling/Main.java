ae9e4d34e65f9c8b0c0c3d3a4251d14b63db1cb5
Nikolay Orlenko <norlenko2801@gmail.com>
2024-10-08 13:26:28 +0300

    Обрабатываем ошибки красиво Для первого действия создал аналог Supplier, который умеет теперь прокидывать наверх исключение

---
+++ src/main/java/school/faang/errors/handling/Main.java
@@ -0,0 +1,14 @@
+package school.faang.errors.handling;
+
+public class Main {
+    public static void main(String[] args) {
+        String result = ErrorHandler.withErrorHandling(
+                () -> RemoteService.call("someParam"),
+                e -> {
+                    System.out.println("Ошибка при вызове сервиса, возвращаем дефолтное значение");
+                    return "DEFAULT";
+                }
+        );
+        System.out.println(result);
+    }
+}
