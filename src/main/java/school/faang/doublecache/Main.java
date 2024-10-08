abde7fe36f9fcae82086b4b8036a826bed906f88
Алексей Беляев <belyaevayur@intech.rshb.ru>
2024-10-08 13:45:48 +0300

    BJS2-32666 Двойной кэш-кэш

---
+++ src/main/java/school/faang/doublecache/Main.java
@@ -0,0 +1,109 @@
+package school.faang.doublecache;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class Main {
+    private static final Map<Student, Map<Subject, Integer>> SUBJECT_GRADES_OF_STUDENT = new HashMap<>();
+    private static final Map<Subject, List<Student>> STUDENTS_OF_SUBJECT = new HashMap<>();
+
+    public static void main(String[] args) {
+        Student alex = new Student(1L, "Alex");
+        Student ann = new Student(2L, "Ann");
+        Student lisa = new Student(3L, "Lisa");
+        Subject math = new Subject(1L, "Math");
+        Subject english = new Subject(2L, "English");
+        Subject history = new Subject(3L, "History");
+        Subject chemistry = new Subject(4L, "Chemistry");
+        Map<Subject, Integer> alexGrades = new HashMap<>() {{
+            put(math, 5);
+            put(english, 5);
+        }};
+        Map<Subject, Integer> annGrades = new HashMap<>() {{
+            put(math, 4);
+            put(history, 4);
+        }};
+        Map<Subject, Integer> lisaGrades = new HashMap<>() {{
+            put(english, 3);
+            put(history, 4);
+        }};
+
+        addStudent(alex, alexGrades);
+        addStudent(ann, annGrades);
+        addStudent(lisa, lisaGrades);
+        printAllStudentsWithGrades();
+        deleteStudent(lisa);
+        printAllStudentsWithGrades();
+        addSubjectWithStudents(chemistry, List.of(ann, alex));
+        printAllSubjectsWithStudents();
+        addStudent(lisa, lisaGrades);
+        printAllStudentsWithGrades();
+        addStudentToSubject(lisa, chemistry);
+        printAllSubjectsWithStudents();
+        removeStudentFromSubject(ann, math);
+        removeStudentFromSubject(ann, english);
+        printAllSubjectsWithStudents();
+    }
+
+    public static void addStudent(Student student, Map<Subject, Integer> subjectGrades) {
+        if (!SUBJECT_GRADES_OF_STUDENT.containsKey(student)) {
+            SUBJECT_GRADES_OF_STUDENT.put(student, subjectGrades);
+            for (Subject subject : subjectGrades.keySet()) {
+                STUDENTS_OF_SUBJECT.computeIfAbsent(subject, k -> new ArrayList<>()).add(student);
+            }
+        }
+    }
+
+    public static void addSubjectWithGrade(Student student, Subject subject, Integer grade) {
+        SUBJECT_GRADES_OF_STUDENT.computeIfPresent(student, (k, v) -> {
+            v.put(subject, grade);
+            STUDENTS_OF_SUBJECT.computeIfAbsent(subject, s -> new ArrayList<>()).add(student);
+            return v;
+        });
+    }
+
+    public static void deleteStudent(Student student) {
+        SUBJECT_GRADES_OF_STUDENT.remove(student);
+        STUDENTS_OF_SUBJECT.values().forEach(students -> students.remove(student));
+    }
+
+    public static void printAllStudentsWithGrades() {
+        System.out.println(SUBJECT_GRADES_OF_STUDENT);
+    }
+
+    public static void addSubjectWithStudents(Subject subject, List<Student> students) {
+        if (!STUDENTS_OF_SUBJECT.containsKey(subject)) {
+            for (Student student : students) {
+                addSubjectWithGrade(student, subject, 0);
+            }
+        }
+    }
+
+    public static void addStudentToSubject(Student student, Subject subject) {
+        SUBJECT_GRADES_OF_STUDENT.computeIfPresent(student, (k, v) -> {
+            v.put(subject, 0);
+            STUDENTS_OF_SUBJECT.computeIfPresent(subject, (z, x) -> {
+                x.add(student);
+                return x;
+            });
+            return v;
+        });
+    }
+
+    public static void removeStudentFromSubject(Student student, Subject subject) {
+        SUBJECT_GRADES_OF_STUDENT.computeIfPresent(student, (k, v) -> {
+            v.remove(subject);
+            STUDENTS_OF_SUBJECT.computeIfPresent(subject, (z, x) -> {
+                x.remove(student);
+                return x;
+            });
+            return v;
+        });
+    }
+
+    public static void printAllSubjectsWithStudents() {
+        System.out.println(STUDENTS_OF_SUBJECT);
+    }
+}
---
+++ src/main/java/school/faang/doublecache/Subject.java
@@ -0,0 +1,11 @@
+package school.faang.doublecache;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class Subject {
+    private Long id;
+    private String name;
+}
