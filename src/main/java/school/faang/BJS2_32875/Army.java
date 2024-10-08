4fd0cdcb10e4431537020f2310a0fdf5f3e24e52
UrmatBolotbek <urmar9797@gmail.com>
2024-10-08 13:44:16 +0200

    completed the task "Герои Меча и Магии" BJS2-32875

---
+++ src/main/java/school/faang/BJS2_32875/Army.java
@@ -0,0 +1,7 @@
+package school.faang.BJS2_32875;
+
+public class Army extends Creature{
+    public Army(String name, int level, int damage, int protection, int speed, int hp) {
+        super(name, level, damage, protection, speed, hp);
+    }
+}
