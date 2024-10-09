afaa8746d404dde4dfc1ae69ef6dc8c084a18f9d
Sanasar1 <almasyansb@gmail.com>
2024-10-09 15:21:12 +0300

    BJS2-33744: Lord of the Rings RPG

---
+++ src/main/java/bjs2_33388/Character.java
@@ -0,0 +1,12 @@
+package bjs2_33388;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+import java.util.List;
+
+@Getter
+@AllArgsConstructor
+public class Character {
+    private List<Item> inventory;
+}
---
+++ src/main/java/bjs2_33388/Item.java
@@ -0,0 +1,13 @@
+package bjs2_33388;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+public class Item {
+    private String name;
+    private int value;
+}
