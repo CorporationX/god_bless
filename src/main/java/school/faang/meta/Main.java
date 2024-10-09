5eb8a6300726c49968720d6f900268f6869fd92d
daurenberdibekov <berder08@gmail.com>
2024-10-09 15:46:07 +0200

    Исправления

--- src/main/java/school/faang/meta/Main.java
+++ src/main/java/school/faang/meta/Main.java
@@ -1,19 +1,25 @@
 package school.faang.meta;
 
+import java.util.Map;
+import java.util.function.Consumer;
+
 public class Main {
 
     public static void main(String[] args) {
         NotificationManager notificationManager = new NotificationManager();
+        Map<String, Consumer<Notification>> notificationConsumers = notificationManager.getNotificationConsumers();
 
         notificationManager.registerHandler("email", notification -> System.out.println(notification.getMessage()));
         notificationManager.registerHandler("sms", notification -> System.out.println(notification.getMessage()));
         notificationManager.registerHandler("missed call", notification -> System.out.println(notification.getMessage()));
 
-        System.out.println(NotificationManager.notificationConsumer);
-
         notificationManager.sendNotification(new Notification("email", "You have a new email"));
         notificationManager.sendNotification(new Notification("sms", "You have a new sms"));
         notificationManager.sendNotification(new Notification("missed call", "You have a missed call"));
+
+        for(Map.Entry<String, Consumer<Notification>> entry: notificationConsumers.entrySet()){
+            System.out.println("Notification: " + entry.getKey() + ", Action: " + entry.getValue());
+        }
 
     }
 }
--- src/main/java/school/faang/meta/NotificationManager.java
+++ src/main/java/school/faang/meta/NotificationManager.java
@@ -1,19 +1,22 @@
 package school.faang.meta;
 
+import lombok.Data;
+
 import java.util.HashMap;
 import java.util.Map;
 import java.util.function.Consumer;
 
+@Data
 public class NotificationManager {
 
-    protected static final Map<String, Consumer<Notification>> notificationConsumer = new HashMap<>();
+    private final Map<String, Consumer<Notification>> notificationConsumers = new HashMap<>();
 
     public void registerHandler(String type, Consumer<Notification> consumer){
-        notificationConsumer.putIfAbsent(type, consumer);
+        notificationConsumers.putIfAbsent(type, consumer);
     }
 
     public void sendNotification(Notification notification){
-        Consumer<Notification> handler = notificationConsumer.get(notification.getType());
+        Consumer<Notification> handler = notificationConsumers.get(notification.getType());
 
         if(handler != null){
             handler.accept(notification);
