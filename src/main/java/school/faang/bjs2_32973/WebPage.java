3392a411ba3bedc6caa35198f21742b74b9903a0
Shap|Misha <maxim.ivanov.jr@gmail.com>
2024-10-10 01:48:52 +0300

    add @Data for WebPage; add validate arguments for methods class WebPageIndexer

--- src/main/java/school/faang/bjs2_32973/WebPage.java
+++ src/main/java/school/faang/bjs2_32973/WebPage.java
@@ -1,14 +1,11 @@
 package school.faang.bjs2_32973;
 
 import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.Setter;
-import lombok.ToString;
+import lombok.Data;
+
 
 @AllArgsConstructor
-@Getter
-@Setter
-@ToString
+@Data
 public class WebPage {
     private String url;
     private String title;
--- src/main/java/school/faang/bjs2_32973/WebPageIndexer.java
+++ src/main/java/school/faang/bjs2_32973/WebPageIndexer.java
@@ -10,6 +10,12 @@ public class WebPageIndexer {
 
 
     public void indexWebPage(WebPage webPage, Map<String, List<WebPage>> webPageKeywordIndex) {
+        if (webPage == null) {
+            throw new IllegalArgumentException("web page is null");
+        }
+        if (webPageKeywordIndex == null) {
+            throw new IllegalArgumentException("webPageKeywordIndex is null");
+        }
         String content = webPage.getContent();
 
         String[] contentWords = divideByWords(content);
@@ -32,30 +38,39 @@ public class WebPageIndexer {
     }
 
     private static String[] divideByWords(String content) {
+        if (content == null || content.isEmpty() || content.isBlank()) {
+            throw new NullPointerException("сontent is empty");
+        }
         String nonLetterOrDigitRegex = "[^\\p{L}\\p{N}']+";
         return content.split(nonLetterOrDigitRegex);
     }
 
     public List<WebPage> getWebPagesByWord(String keyWord, Map<String, List<WebPage>> webPageKeywordIndex) {
+        if(webPageKeywordIndex == null){
+            throw new IllegalArgumentException("webPageKeywordIndex is null");
+        }
         String contentWordLowerCase = keyWord.toLowerCase();
         return webPageKeywordIndex.get(contentWordLowerCase);
     }
 
     public void removeFromIndexByUrl(String urlForRemove, Map<String, List<WebPage>> webPageKeywordIndex) {
+        if(webPageKeywordIndex == null){
+            throw new IllegalArgumentException("webPageKeywordIndex is null");
+        }
         String[] keyWordsWebPageToRemove = getKeyWordsCurrentWebPageByUrl(urlForRemove, webPageKeywordIndex);
-        if(keyWordsWebPageToRemove == null){
+        if (keyWordsWebPageToRemove == null) {
             throw new NoSuchElementException("there is no web page with this url in the index");
         }
         for (String keyWordForWebPageToRemove : keyWordsWebPageToRemove) {
-            List<WebPage> webPages = getWebPagesByWord(keyWordForWebPageToRemove,webPageKeywordIndex);
+            List<WebPage> webPages = getWebPagesByWord(keyWordForWebPageToRemove, webPageKeywordIndex);
 
             for (WebPage webPage : webPages) {
                 String urlCurrentWebPage = webPage.getUrl();
 
-                if(Objects.equals(urlCurrentWebPage,urlForRemove)){
+                if (Objects.equals(urlCurrentWebPage, urlForRemove)) {
                     webPages.remove(webPage);
 
-                    if(webPages.isEmpty()){
+                    if (webPages.isEmpty()) {
                         webPageKeywordIndex.remove(keyWordForWebPageToRemove);
                     }
                     break;
