09cd655fc3e3660f5489ca51b341549f65cd7092
IvanMatiko <matiko1993@gmail.com>
2024-10-09 20:22:47 +0200

    Made task Hogwarts

---
+++ src/main/java/Hogwarts_BJS2_33875/SpellCaster.java
@@ -0,0 +1,7 @@
+package Hogwarts_BJS2_33875;
+
+public class SpellCaster {
+    public void cast(String spellName, SpellAction action) {
+        System.out.println(action.spell(spellName));
+    }
+}
