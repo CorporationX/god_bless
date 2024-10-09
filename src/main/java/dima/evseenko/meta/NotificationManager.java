30c5e9c1b56785fff8eb4f238c15d0e4c20b0cc9
Dmytro Yevsieienko <youdima70@gmail.com>
2024-10-09 14:01:10 +0200

    Рефакторинг

--- src/main/java/dima/evseenko/meta/NotificationManager.java
+++ src/main/java/dima/evseenko/meta/NotificationManager.java
@@ -1,23 +1,23 @@
 package dima.evseenko.meta;
 
+import org.jetbrains.annotations.NotNull;
+
 import java.util.HashMap;
 import java.util.Map;
-import java.util.Objects;
 import java.util.Set;
 import java.util.function.Consumer;
 
 public class NotificationManager {
-    private final Map<NotificationType, Consumer<Notification>> notificationHandlers = new HashMap<>();
-    private final Set<String> forbiddenWords = Set.of(
+    public static final Set<String> forbiddenWords = Set.of(
             "fuck",
             "dick",
             "pussy"
     );
 
-    public void registerHandler(NotificationType type, Consumer<Notification> handler) {
-        if (Objects.nonNull(type) && Objects.nonNull(handler)) {
-            notificationHandlers.put(type, handler);
-        }
+    private final Map<NotificationType, Consumer<Notification>> notificationHandlers = new HashMap<>();
+
+    public void registerHandler(@NotNull NotificationType type, @NotNull Consumer<Notification> handler) {
+        notificationHandlers.put(type, handler);
     }
 
     public void sendNotification(Notification notification) {
@@ -32,13 +32,11 @@ public class NotificationManager {
         return notificationHandlers.get(type);
     }
 
-    private void validateNotificationMassage(Notification notification) {
-        if (Objects.nonNull(notification)) {
-            forbiddenWords.forEach((word) -> {
-                if (notification.getMessage().toLowerCase().contains(word.toLowerCase())) {
-                    notification.setMessage(notification.getMessage().replace(word.toLowerCase(), "*"));
-                }
-            });
-        }
+    private void validateNotificationMassage(@NotNull Notification notification) {
+        forbiddenWords.forEach((word) -> {
+            if (notification.getMessage().toLowerCase().contains(word.toLowerCase())) {
+                notification.setMessage(notification.getMessage().replace(word.toLowerCase(), "*"));
+            }
+        });
     }
 }
