e5a992c0c589fa8df7b5889f34e3147302b0b5f2
Memoreks <akifyevalex1@gmail.com>
2024-10-10 06:52:02 +0700

    Внес изменения согласно комментариям

--- src/main/java/school/faang/truancy/Main.java
+++ src/main/java/school/faang/truancy/Main.java
@@ -8,11 +8,11 @@ import java.util.List;
 import java.util.Map;
 
 public class Main {
-    public static Map<String, List<Student>> studentsMap;
+    public static List<Student> students = new ArrayList<>();
 
-    public static Map<String, List<Student>> listStudentsToMap(List<Student> studentList) {
+    public static Map<String, List<Student>> listStudentsToMap() {
         Map<String, List<Student>> studentMap = new HashMap<>();
-        for(Student student : studentList) {
+        for(Student student : students) {
             String key = student.getFaculty() + student.getYear();
             studentMap.computeIfAbsent(key, k -> new ArrayList<>()).add(student);
         }
@@ -20,36 +20,32 @@ public class Main {
     }
 
     public static void addNewStudent(Student student) {
-        String key = student.getFaculty() + student.getYear();
-        studentsMap.computeIfAbsent(key, k -> new ArrayList<>()).add(student);
+        students.add(student);
     }
 
     public static void deleteStudent(String name, String faculty, Integer year) {
         Student deleteStudent = new Student(name, faculty, year);
-        String key = faculty + year;
-        studentsMap.get(key).remove(deleteStudent);
+        students.remove(deleteStudent);
     }
 
     public static void findAllStudentsByFacultyAndYear(String faculty, Integer year) {
         String key = faculty + year;
-        System.out.println(studentsMap.get(key));
+        System.out.println(listStudentsToMap().get(key));
     }
 
     public static void displayAllStudentsGroupByFacultyAndYear() {
-        for(Map.Entry<String, List<Student>> entry : studentsMap.entrySet()) {
+        for(Map.Entry<String, List<Student>> entry : listStudentsToMap().entrySet()) {
             System.out.println(entry.getKey() + " " + entry.getValue());
         }
     }
     public static void main(String[] args) {
+        students.add(new Student("Sergey", "RP", 1));
+        students.add(new Student("Kolia", "RP", 1));
+        students.add(new Student("Ivan", "RP", 3));
+        students.add(new Student("Ura", "GEO", 2));
+        students.add(new Student("Dasha", "MATH", 4));
 
-        List<Student> studentList = new ArrayList<>();
-
-        studentList.add(new Student("Sergey", "RP", 1));
-        studentList.add(new Student("Kolia", "RP", 1));
-        studentList.add(new Student("Ivan", "RP", 3));
-        studentList.add(new Student("Ura", "GEO", 2));
-        studentList.add(new Student("Dasha", "MATH", 4));
-        studentsMap = listStudentsToMap(studentList);
+        System.out.println(listStudentsToMap());
         addNewStudent(new Student("Dima", "RP", 4));
         addNewStudent(new Student("Anna", "RP", 1));
         deleteStudent("Ivan", "RP", 3);
