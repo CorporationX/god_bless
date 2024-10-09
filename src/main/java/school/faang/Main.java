074006771a3f16574cd545448914ed84208433e7
Matvey <mashptz@gmail.com>
2024-10-09 20:27:44 +0300

    Исправление недочетов

--- src/main/java/school/faang/Main.java
+++ src/main/java/school/faang/Main.java
@@ -2,16 +2,13 @@ package school.faang;
 
 import school.faang.university.Student;
 import school.faang.university.Subject;
-
-import java.util.ArrayList;
+import school.faang.university.UniversityServices;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
 public class Main {
-    private static Map<Student, Map<Subject, Integer>> studentMap = new HashMap<>();
-    private static Map<Subject, List<Student>> subjectMap = new HashMap<>();
-
+    static UniversityServices universityServices = new UniversityServices();
     public static void main(String[] args) {
         Student firstStudent = new Student(12, "John");
         Student secondStudent = new Student(208, "Mark");
@@ -21,133 +18,30 @@ public class Main {
         Subject pe = new Subject(15, "PE");
         Subject maths = new Subject(26, "Maths");
 
-        addNewStudent(firstStudent, new HashMap<>(Map.of(chemistry, 8, pe, 12, maths, 3)));
-        addNewStudent(secondStudent, new HashMap<>(Map.of(pe, 10, chemistry, 7)));
-        addNewStudent(thirdStudent, new HashMap<>(Map.of(maths, 12)));
+        universityServices.addNewStudent(firstStudent, new HashMap<>(Map.of(chemistry, 8, pe, 12, maths, 3)));
+        universityServices.addNewStudent(secondStudent, new HashMap<>(Map.of(pe, 10, chemistry, 7)));
+        universityServices.addNewStudent(thirdStudent, new HashMap<>(Map.of(maths, 12)));
 
-        addNewSubjectToStudent(thirdStudent, pe, 6);
-        addNewSubjectToStudent(firstStudent, chemistry, 7);
+        universityServices.printAllStudSubj();
+        System.out.println("==============================================");
 
-        deleteStudent(secondStudent);
+        universityServices.addNewSubjectToStudent(thirdStudent, pe, 6);
 
-        printAllStudSubj();
+        universityServices.addNewSubjectToStudent(firstStudent, chemistry, 7);
 
-        addNewSubject(
+        universityServices.deleteStudent(secondStudent);
+
+        universityServices.printAllStudSubj();
+
+        universityServices.addNewSubject(
                 new Subject(12, "Physics"),
                 List.of(firstStudent, thirdStudent)
         );
-        addStudentToASubject(chemistry, thirdStudent);
+        universityServices.addStudentToASubject(chemistry, thirdStudent);
 
-        detachStudFromSubj(chemistry, firstStudent);
+        universityServices.detachStudFromSubj(chemistry, firstStudent);
+        System.out.println("=========================================");
 
-        printAllSubjects();
+        universityServices.printAllSubjects();
     }
-
-    public static void addNewStudent(
-            Student student,
-            Map<Subject, Integer> grade
-    ) {
-        if (!studentMap.containsKey(student)) {
-            studentMap.put(student, grade);
-        }
-        for (Map.Entry<Subject, Integer> entry : grade.entrySet()) {
-            Subject keySubject = entry.getKey();
-            if (subjectMap.containsKey(keySubject)) {
-                if (subjectMap.get(keySubject) != null) {
-                    subjectMap.get(keySubject).add(student);
-                } else {
-                    List<Student> students = new ArrayList<>();
-                    students.add(student);
-                    subjectMap.put(keySubject, students);
-                }
-            }
-        }
-    }
-
-    public static void addNewSubjectToStudent(
-            Student student,
-            Subject subject,
-            Integer grade
-    ) {
-        if (studentMap.containsKey(student)) {
-            if (!studentMap.get(student).containsKey(subject)) {
-                studentMap.get(student).put(subject, grade);
-                if (
-                        subjectMap.containsKey(subject)
-                        && subjectMap.get(subject) != null
-                ) {
-                    subjectMap.get(subject).add(student);
-                } else {
-                    List<Student> students = new ArrayList<>();
-                    students.add(student);
-                    subjectMap.put(subject, students);
-                }
-            }
-        }
-    }
-
-    public static void deleteStudent(Student student) {
-        if (studentMap.containsKey(student)) {
-            studentMap.remove(student);
-
-        }
-    }
-
-    public static void printAllStudSubj() {
-        for (Map.Entry<Student, Map<Subject, Integer>> entry : studentMap.entrySet()) {
-            System.out.print("Student: " + entry.getKey());
-            System.out.println(" subjects: " + entry.getValue());
-        }
-    }
-
-    public static void addNewSubject(Subject subject, List<Student> students) {
-        if (!subjectMap.containsKey(subject)) {
-            subjectMap.put(subject, students);
-            for (Student student : students) {
-                if (studentMap.containsKey(student)) {
-                    studentMap.get(student).put(subject, null);
-                } else {
-                    studentMap.put(student, new HashMap<>(Map.of(subject, null)));
-                }
-            }
-        }
-    }
-
-    public static void addStudentToASubject(Subject subject, Student student) {
-        if (subjectMap.containsKey(subject)) {
-            if (subjectMap.get(subject) == null) {
-                List<Student> students = new ArrayList<>();
-                students.add(student);
-                subjectMap.put(subject, students);
-            } else {
-                subjectMap.get(subject).add(student);
-            }
-            if(!subjectMap.get(student).contains(subject)){
-                studentMap.put(student, new HashMap<>(Map.of(subject, null)));
-            }
-        }
-    }
-
-    public static void detachStudFromSubj(Subject subject, Student student) {
-        if (subjectMap.containsKey(subject)) {
-            List<Student> students = subjectMap.get(subject);
-            if (students.contains(student) && students != null) {
-                if (students.size() == 1) {
-                    subjectMap.put(subject, null);
-                } else {
-                    subjectMap.get(subject).remove(student);
-                }
-                studentMap.get(student).remove(subject);
-            }
-        }
-
-    }
-
-    public static void printAllSubjects() {
-        for (Map.Entry<Subject, List<Student>> entry : subjectMap.entrySet()) {
-            System.out.print("Student: " + entry.getKey());
-            System.out.println(" grade:" + entry.getValue());
-        }
-    }
-
 }
--- src/main/java/school/faang/university/Subject.java
+++ src/main/java/school/faang/university/Subject.java
@@ -8,5 +8,4 @@ import lombok.Data;
 public class Subject {
     private int id;
     private String name;
-
 }
