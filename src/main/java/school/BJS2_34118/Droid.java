3d1d9a0c6c8cc407a81d05d44bee80327400f277
Roman Nifontov <romannif@mail.ru>
2024-10-09 19:55:32 +0500

    Сделал изменения в задаче BJS2-34118

--- src/main/java/school/BJS2_34118/Droid.java
+++ src/main/java/school/BJS2_34118/Droid.java
@@ -7,20 +7,22 @@ import lombok.Getter;
 @Getter
 public class Droid {
 
+    private final Integer ALPHABET_SIZE = 26;
+
     private String name;
 
     public String encryptMessage(String line, int number) {
-        DroidMessageEncryptor droidMessage = (message, count) -> caesarCipher(message,count);
+        DroidMessageEncryptor droidMessage = (message, count) -> caesarCipher(message, count);
         return droidMessage.method(line, number);
     }
 
     public String decryptMessage(String line, int number) {
-        DroidMessageEncryptor droidMessage = (message, count) ->  decrypt(message,count);
+        DroidMessageEncryptor droidMessage = (message, count) -> decrypt(message, count);
         return droidMessage.method(line, number);
     }
 
     public void sendMessage(Droid droid, String text, int number) {
-        String shifr = encryptMessage(text,number);
+        String shifr = encryptMessage(text, number);
         System.out.println(name + " отправил зашифрованное сообщение: " + shifr);
         String normText = droid.receiveMessage(shifr, number);
         System.out.println(droid.getName() + " получил расшифрованное сообщение: " + normText);
@@ -28,18 +30,18 @@ public class Droid {
     }
 
     private String receiveMessage(String text, int number) {
-        String normText = decryptMessage(text,number);
+        String normText = decryptMessage(text, number);
         return normText;
     }
 
     private String caesarCipher(String text, int shift) {
         StringBuilder result = new StringBuilder();
 
-        for(char c : text.toCharArray()) {
-            if(Character.isLetter(c)) {
-                char encrypted = (char) (c +shift);
+        for (char c : text.toCharArray()) {
+            if (Character.isLetter(c)) {
+                char encrypted = (char) (c + shift);
                 if ((Character.isLowerCase(c) && encrypted > 'z') || (Character.isUpperCase(c) && encrypted > 'Z')) {
-                    result.append((char) (c-(26-shift)));
+                    result.append((char) (c - (ALPHABET_SIZE - shift)));
                 } else {
                     result.append(encrypted);
                 }
@@ -51,7 +53,7 @@ public class Droid {
     }
 
     public String decrypt(String text, int shift) {
-        return caesarCipher(text,26 - shift);
+        return caesarCipher(text, ALPHABET_SIZE - shift);
     }
 
 
