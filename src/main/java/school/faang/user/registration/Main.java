1b04491f2871ad2afa9d9ea404b8ad4f6b6f1f29
Andrey <maandr@internet.ru>
2024-10-10 00:50:03 +0300

    BJS2-31823 User registration

---
+++ src/main/java/school/faang/user/registration/Main.java
@@ -0,0 +1,11 @@
+package school.faang.user.registration;
+
+public class Main {
+    public static void main(String[] args) {
+        User user = new User("Vlad", 26, "Uber", "Amsterdam");
+        User user2 = new User("", 15, "Tech Innovations LLC", "456 Oak Avenue");
+        User user3 = new User("Emily Rodriguez", 17,  "Global Enterprises", "789 Pine Road");
+        User user4 = new User("Michael Lee", 35, "Financial Insights Group", "321 Birch Lane");
+        User user5 = new User("Jessica Kim", 28, "Google", "654 Cedar Drive");
+    }
+}
