3fb0abf468750f29380472308c181526051eaf34
Anatoly Galay <galay007@mail.ru>
2024-10-09 08:28:49 +0300

    validation moved to calss User in its constructor

--- src/main/java/school/faang/registr_user/Main.java
+++ src/main/java/school/faang/registr_user/Main.java
@@ -4,68 +4,36 @@ import java.util.Scanner;
 import java.util.Set;
 
 public class Main {
-    private final Set<String> VALID_JOBS = Set.of("Google", "Uber", "Amazon");
-    private final Set<String> VALID_ADDRESSES = Set.of("London", "New York", "Amsterdam");
-    private final int AGE_START = 18;
 
-    static boolean created = false;
+
     private String name;
     private int age;
     private String placeWork;
     private String address;
 
     public static void main(String[] args) {
+
         Main main = new Main();
 
-        while (!created) {
+        Scanner scanner = new Scanner(System.in);
 
-            Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter Name: ");
+        main.name = scanner.next();
 
-            System.out.print("Enter Name: ");
-            main.name = scanner.next();
-
-            System.out.print("Enter Age: ");
-            if (scanner.hasNextInt()) {
-                main.age = scanner.nextInt();
-            } else {
-                scanner.next();
-                main.age = 0;
-            }
-
-            System.out.print("Enter place of work: ");
-            main.placeWork = scanner.next();
-
-            System.out.print("Enter address: ");
-            main.address = scanner.next();
-
-            if (main.validate()) {
-                System.out.println("User successfully created");
-                created = true;
-            }
+        System.out.print("Enter Age: ");
+        if (scanner.hasNextInt()) {
+            main.age = scanner.nextInt();
+        } else {
+            scanner.next();
+            main.age = 0;
         }
-    }
 
-    private boolean validate() {
-        boolean validation = false;
+        System.out.print("Enter place of work: ");
+        main.placeWork = scanner.next();
 
-        try {
-            if (name == null || name.isEmpty() || name.isBlank()) {
-                throw new IllegalArgumentException("ERROR: not valid name");
-            } else if (age < AGE_START) {
-                throw new IllegalArgumentException("ERROR: not valid age");
-            } else if (placeWork == null || placeWork.isEmpty() || placeWork.isBlank() || !VALID_JOBS.contains(placeWork)) {
-                throw new IllegalArgumentException("ERROR: not valid place of work");
-            } else if (address == null || address.isEmpty() || address.isBlank() || !VALID_ADDRESSES.contains(address)) {
-                throw new IllegalArgumentException("ERROR: not valid address");
-            }
-            validation = true;
-        } catch (IllegalArgumentException e) {
-            System.out.println(e.getMessage() + "\nYour params should follow the rules:\n" +
-                    "- Name not to be empty\n" +
-                    "- Age must be above 18\n" +
-                    "- Work on Google, Uber or Amazon\n" +
-                    "- Address London, New York or Amsterdam\n");
-        }
-        return validation;
+        System.out.print("Enter address: ");
+        main.address = scanner.next();
+
+        User user = new User(main.name, main.age, main.placeWork, main.address);
     }
 }
--- src/main/java/school/faang/registr_user/User.java
+++ src/main/java/school/faang/registr_user/User.java
@@ -1,11 +1,38 @@
 package school.faang.registr_user;
 
-import lombok.AllArgsConstructor;
+import java.util.Set;
 
-@AllArgsConstructor
 public class User {
+    private static final Set<String> VALID_JOBS = Set.of("Google", "Uber", "Amazon");
+    private static final Set<String> VALID_ADDRESSES = Set.of("London", "New York", "Amsterdam");
+    private static final int AGE_START = 18;
+
     private String name;
     private int age;
     private String placeWork;
     private String address;
+
+    public User(String name, int age, String placeWork, String address) {
+        try {
+            if (name == null || name.isEmpty() || name.isBlank()) {
+                throw new IllegalArgumentException("ERROR: not valid name " + name);
+            } else if (age < AGE_START) {
+                throw new IllegalArgumentException("ERROR: not valid age " + age);
+            } else if (placeWork == null || placeWork.isEmpty() || placeWork.isBlank() || !VALID_JOBS.contains(placeWork)) {
+                throw new IllegalArgumentException("ERROR: not valid place of work" + placeWork);
+            } else if (address == null || address.isEmpty() || address.isBlank() || !VALID_ADDRESSES.contains(address)) {
+                throw new IllegalArgumentException("ERROR: not valid address" + address);
+            }
+            this.name = name;
+            this.age = age;
+            this.placeWork = placeWork;
+            this.address = address;
+        } catch (IllegalArgumentException e) {
+            System.out.println(e.getMessage() + "\nYour params should follow the rules:\n" +
+                    "- Name not to be empty\n" +
+                    "- Age must be above 18\n" +
+                    "- Work on Google, Uber or Amazon\n" +
+                    "- Address London, New York or Amsterdam\n");
+        }
+    }
 }
