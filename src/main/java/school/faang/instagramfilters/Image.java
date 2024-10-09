ec5c4b3cc3009d16473018cffb4bd6b555cccaf3
Alexander Litovkin <litovkinalexan@gmail.com>
2024-10-09 16:32:34 +0700

    complete task

---
+++ src/main/java/school/faang/instagramfilters/Image.java
@@ -0,0 +1,14 @@
+package school.faang.instagramfilters;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+public class Image {
+
+    private String name;
+    private String description;
+}
