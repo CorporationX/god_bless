d976727d65677826901b70acb65f383604abbfb5
Egor Semenovskii <gaoqiao5217@gmail.com>
2024-10-09 19:58:31 +0300

    Service added

--- src/main/java/school/faang/ELK/Main.java
+++ src/main/java/school/faang/ELK/Main.java
@@ -2,6 +2,8 @@ package school.faang.ELK;
 
 import java.util.*;
 
+import static school.faang.ELK.queryService.*;
+
 public class Main {
     private static final Map<User, List<Query>> users = new HashMap<>();
 
@@ -14,35 +16,15 @@ public class Main {
         Query query2 = new Query(2, "Some Content");
         Query query3 = new Query(3, "Some Content");
 
-        addUser(user1, new ArrayList<>(List.of(query1, query2)));
-        addUser(user2, new ArrayList<>(List.of(query2, query3)));
-        addUser(user3, new ArrayList<>(List.of(query1, query3)));
+        addUser(user1, new ArrayList<>(List.of(query1, query2)), users);
+        addUser(user2, new ArrayList<>(List.of(query2, query3)), users);
+        addUser(user3, new ArrayList<>(List.of(query1, query3)), users);
 
-        printUsers();
+        printUsers(users);
 
-        addQueryToUser(user1, query3);
-        removeUser(user2);
+        addQueryToUser(user1, query3, users);
+        removeUser(user2, users);
 
-        printUsers();
-    }
-
-    public static void addUser(User user, List<Query> queries) {
-        users.put(user, queries);
-    }
-
-    public static void addQueryToUser(User user, Query query) {
-        users.get(user).add(query);
-    }
-
-    public static void removeUser(User user) {
-        users.remove(user);
-    }
-
-    public static void printUsers() {
-        users.forEach((user, queries) -> {
-            System.out.println(user + ": ");
-            queries.stream().sorted(Comparator.comparing(Query::getTimestamp)).
-                    forEach(System.out::println);
-        });
+        printUsers(users);
     }
 }
