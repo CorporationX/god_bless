0efe0e69751d537ab43bd5d26b91cc1bd77ebe69
Alena Shmelkova <AlenaShmelkova@gmail.com>
2024-10-09 13:40:18 -0400

    git commit -m "BJS2-34171 <DroidSecrets changed magic number to final variable NUMBER_OF_LETTERS_IN_THE_ALPHABET, changed encryption logic to follow DRY principle>"

--- src/main/java/school/faang/droids_secrets/Droid.java
+++ src/main/java/school/faang/droids_secrets/Droid.java
@@ -7,37 +7,32 @@ import lombok.Getter;
 @Getter
 public class Droid {
     private String name;
+    private static final int NUMBER_OF_LETTERS_IN_THE_ALPHABET = 26;
 
-    public String encryptMessage(String message, int key) {
+    private String shiftMessage(String message, int key) {
         DroidMessageEncryptor encryptor = (msg, encryptionKey) -> {
-            StringBuilder encryptedMessage = new StringBuilder();
-            for (char ch : msg.toCharArray()) {
+            StringBuilder shiftedMessage = new StringBuilder();
+            for (char ch : message.toCharArray()) {
                 if (Character.isLetter(ch)) {
                     char base = Character.isLowerCase(ch) ? 'a' : 'A';
-                    encryptedMessage.append((char) ((ch - base + encryptionKey) % 26 + base));
+                    int effectiveKey = (key % NUMBER_OF_LETTERS_IN_THE_ALPHABET + NUMBER_OF_LETTERS_IN_THE_ALPHABET)
+                            % NUMBER_OF_LETTERS_IN_THE_ALPHABET;
+                    shiftedMessage.append((char) ((ch - base + effectiveKey) % NUMBER_OF_LETTERS_IN_THE_ALPHABET + base));
                 } else {
-                    encryptedMessage.append(ch);
+                    shiftedMessage.append(ch);
                 }
             }
-            return encryptedMessage.toString();
+            return shiftedMessage.toString();
         };
         return encryptor.encrypt(message, key);
     }
 
-    public String decryptMessage(String encryptMessage, int key) {
-        DroidMessageEncryptor decryptor = (msg, decryptionKey) -> {
-            StringBuilder decryptedMessage = new StringBuilder();
-            for (char ch : msg.toCharArray()) {
-                if (Character.isLetter(ch)) {
-                    char base = Character.isLowerCase(ch) ? 'a' : 'A';
-                    decryptedMessage.append((char) ((ch - base - decryptionKey + 26) % 26 + base));
-                } else {
-                    decryptedMessage.append(ch);
-                }
-            }
-            return decryptedMessage.toString();
-        };
-        return decryptor.encrypt(encryptMessage, key);
+    public String encryptMessage(String message, int key) {
+        return shiftMessage(message, key);
+    }
+
+    public String decryptMessage(String encryptedMessage, int key) {
+        return shiftMessage(encryptedMessage, -key);
     }
 
     public void sendMessage(Droid droidReceiver, String message, int key) {
