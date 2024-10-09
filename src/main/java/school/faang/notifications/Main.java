b39bf4399a501476d8764394e989a6fbf5b496ef
nikitasidorov <Nikita.sidorov.7@icloud.com>
2024-10-09 07:18:50 -0700

    добавил изменение увеломления если не проходит по фильтру

--- src/main/java/school/faang/notifications/Main.java
+++ src/main/java/school/faang/notifications/Main.java
@@ -5,24 +5,37 @@ public class Main {
         NotificationManager notificationManager = new NotificationManager();
 
 // Регистрация обработчиков оповещений
-        notificationManager.registerNotification("email",
+        notificationManager.registerNotificationHandler("email",
                 (notification) -> System.out.println("Отправка по электронной почте: " + notification.getMessage())
         );
 
-        notificationManager.registerNotification("sms",
+        notificationManager.registerNotificationHandler("sms",
                 (notification) -> System.out.println("Отправка SMS: " + notification.getMessage())
         );
 
-        notificationManager.registerNotification("push",
+        notificationManager.registerNotificationHandler("push",
                 (notification) -> System.out.println("Отправка push-уведомления: " + notification.getMessage())
         );
 
-        notificationManager.registerNotification("warring",
+        notificationManager.registerNotificationHandler("warring",
                 (notification) -> System.out.println("Отправка предупреждения: " + notification.getMessage())
         );
 
         notificationManager.registerFilter("warring",
-                (message) -> message.contains("error"));
+                (message) -> message.contains("error")
+        );
+
+        notificationManager.registerFilter("error",
+                (message) -> message.contains("info")
+        );
+
+        notificationManager.registerCorrectiveNotificationsHandlers("warring",
+                (notification) -> {
+                    String correctedMessage = notification.getMessage()
+                            .toLowerCase()
+                            .replaceAll("error", "warring");
+                    return new Notification(notification.getType(), correctedMessage);
+        });
 
 // Отправка оповещений
         Notification emailNotification = new Notification("email", "Ваша учетная запись успешно активирована");
@@ -30,11 +43,13 @@ public class Main {
         Notification pushNotification = new Notification("push", "Новый пост от пользователя: JohnDoe");
         Notification warringNotificationRussia = new Notification("warring", "Пожалуйса подтвердите почту");
         Notification warringNotificationTEnglish = new Notification("warring", "Error -> confirm email");
+        Notification errorNotification = new Notification("error", "info -> невыерный пароль");
 
         notificationManager.sendNotification(emailNotification);
         notificationManager.sendNotification(smsNotification);
         notificationManager.sendNotification(pushNotification);
         notificationManager.sendNotification(warringNotificationRussia);
         notificationManager.sendNotification(warringNotificationTEnglish);
+        notificationManager.sendNotification(errorNotification);
     }
 }
--- src/main/java/school/faang/notifications/NotificationManager.java
+++ src/main/java/school/faang/notifications/NotificationManager.java
@@ -3,30 +3,44 @@ package school.faang.notifications;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.function.Consumer;
+import java.util.function.Function;
 import java.util.function.Predicate;
 
 public class NotificationManager {
-    private final Map<String, Consumer<Notification>> notifications = new HashMap<>();
+    private final Map<String, Consumer<Notification>> notificationsHandlers = new HashMap<>();
     private final Map<String, Predicate<String>> filters = new HashMap<>();
+    private final Map<String, Function<Notification, Notification>> correctiveNotificationsHandlers = new HashMap<>();
 
-    public void registerNotification(String type, Consumer<Notification> consumer) {
-        notifications.put(type, consumer);
+    public void registerNotificationHandler(String type, Consumer<Notification> consumer) {
+        notificationsHandlers.put(type, consumer);
     }
 
     public void registerFilter(String type, Predicate<String> predicate) {
         filters.put(type, predicate);
     }
 
+    public void registerCorrectiveNotificationsHandlers(String type, Function<Notification, Notification> function) {
+        correctiveNotificationsHandlers.put(type, function);
+    }
+
     public void sendNotification(Notification notification) {
         Predicate<String> filter = filters.get(notification.getType());
 
-        if (filter != null && filter.test(notification.getMessage().toLowerCase())) {
-            System.out.println(
-                    "уведмеление типа " + notification.getType() + " не прошло проверку на неприемлемый контент");
-            return;
+        if(filter != null && filter.test(notification.getMessage().toLowerCase())) {
+            var correctiveNotificationHandler =  correctiveNotificationsHandlers.get(notification.getType());
+
+            if(correctiveNotificationHandler != null) {
+                notification = correctiveNotificationHandler.apply(notification);
+            }else {
+                System.out.println("уведмеление типа " + notification.getType() + " не прошло проверку на неприемлемый контент");
+                return;
+            }
         }
-        notifications.get(notification.getType()).accept(notification);
+
+        Consumer<Notification> handler = notificationsHandlers.get(notification.getType());
+
+        if(handler != null) {
+            handler.accept(notification);
+        }
     }
-
-
 }
