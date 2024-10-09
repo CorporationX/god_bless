6a22384e8aaa0f21eb47476ea518c9b8cfd09eda
Egor Semenovskii <gaoqiao5217@gmail.com>
2024-10-09 17:57:20 +0300

    Solved

--- src/main/java/school/faang/weather/Main.java
+++ src/main/java/school/faang/weather/Main.java
@@ -3,60 +3,29 @@ package school.faang.weather;
 import java.util.HashMap;
 import java.util.Map;
 
-import static school.faang.weather.WeatherService.provideData;
+import static school.faang.weather.WeatherService.*;
 
 public class Main {
-    private static final Map<String, WeatherData> WEATHER_CACHE = new HashMap<>();
+    private static final Map<String, WeatherData> weatherCache = new HashMap<>();
 
     public static void main(String[] args) {
         WeatherData weatherData = new WeatherData("Moscow", 36.6, 87);
-        addWeather(weatherData);
-        addWeather("Colorado");
-        addWeather("Alabama");
+        addWeather(weatherData, weatherCache);
+        addWeather("Colorado", weatherCache);
+        addWeather("Alabama", weatherCache);
 
-        System.out.println(getWeatherData("Chicago"));
+        System.out.println(getWeatherData("Chicago", weatherCache));
         System.out.println();
 
-        printWeatherData();
+        printWeatherData(weatherCache);
         System.out.println();
 
-        updateWeatherData("Moscow");
+        updateWeatherData("Moscow", weatherCache);
 
-        removeWeatherData("Colorado");
+        removeWeatherData("Colorado", weatherCache);
 
-        printWeatherData();
+        printWeatherData(weatherCache);
         System.out.println();
 
     }
-
-    public static void addWeather(WeatherData weatherData) {
-        WEATHER_CACHE.put(weatherData.getCity(), weatherData);
-    }
-
-    public static void addWeather(String city) {
-        WEATHER_CACHE.put(city, provideData(city));
-    }
-
-    public static WeatherData getWeatherData(String city) {
-        if (!WEATHER_CACHE.containsKey(city)) {
-            WEATHER_CACHE.put(city, provideData(city));
-        }
-        return WEATHER_CACHE.get(city);
-    }
-
-    public static void updateWeatherData(String city) {
-        WEATHER_CACHE.put(city, provideData(city));
-    }
-
-    public static void removeWeatherData(String city) {
-        WEATHER_CACHE.remove(city);
-    }
-
-    public static void printWeatherData() {
-        WEATHER_CACHE.forEach((k, v) -> {
-            System.out.printf("In %s city %.2f degrees and %.2f humidity\n", k, v.getTemp(), v.getHumidity());
-        });
-    }
-
-
 }
--- src/main/java/school/faang/weather/WeatherService.java
+++ src/main/java/school/faang/weather/WeatherService.java
@@ -1,9 +1,40 @@
 package school.faang.weather;
 
+import java.util.Map;
+
 public class WeatherService {
     public static WeatherData provideData(String city) {
         double temperature = Math.random() * 30;
         double humidity = Math.random() * 100;
         return new WeatherData(city, temperature, humidity);
+    }
+
+    public static void addWeather(WeatherData weatherData, Map<String, WeatherData> weatherCache) {
+        weatherCache.put(weatherData.getCity(), weatherData);
+    }
+
+    public static void addWeather(String city, Map<String, WeatherData> weatherCache) {
+        weatherCache.put(city, provideData(city));
+    }
+
+    public static WeatherData getWeatherData(String city, Map<String, WeatherData> weatherCache) {
+        if (!weatherCache.containsKey(city)) {
+            weatherCache.put(city, provideData(city));
+        }
+        return weatherCache.get(city);
+    }
+
+    public static void updateWeatherData(String city, Map<String, WeatherData> weatherCache) {
+        weatherCache.put(city, provideData(city));
+    }
+
+    public static void removeWeatherData(String city, Map<String, WeatherData> weatherCache) {
+        weatherCache.remove(city);
+    }
+
+    public static void printWeatherData(Map<String, WeatherData> weatherCache) {
+        weatherCache.forEach((k, v) -> {
+            System.out.printf("In %s city %.2f degrees and %.2f humidity\n", k, v.getTemp(), v.getHumidity());
+        });
     }
 }
