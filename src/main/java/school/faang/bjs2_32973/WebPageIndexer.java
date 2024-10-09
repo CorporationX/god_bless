0c8164820de1c6f1eba862593a73756e83467c48
Shap|Misha <maxim.ivanov.jr@gmail.com>
2024-10-09 18:55:56 +0300

    add methods: removeFromIndexByUrl for WebPageIndexer, testRemoveFromIndexByUrl,printIndex for Main

--- src/main/java/school/faang/bjs2_32973/WebPageIndexer.java
+++ src/main/java/school/faang/bjs2_32973/WebPageIndexer.java
@@ -3,26 +3,27 @@ package school.faang.bjs2_32973;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
+import java.util.NoSuchElementException;
+import java.util.Objects;
 
 public class WebPageIndexer {
 
 
-    public void indexWebPage(WebPage webPage, Map<String, List<WebPage>> webPageKeywordIndex){
+    public void indexWebPage(WebPage webPage, Map<String, List<WebPage>> webPageKeywordIndex) {
         String content = webPage.getContent();
 
-        String nonLetterOrDigitRegex = "[^\\p{L}\\p{N}]+";
-        String[] contentWords = content.split(nonLetterOrDigitRegex);
+        String[] contentWords = divideByWords(content);
 
         for (String contentWord : contentWords) {
             String contentWordLowerCase = contentWord.toLowerCase();
 
-            if(!(webPageKeywordIndex.containsKey(contentWordLowerCase))){
+            if (!(webPageKeywordIndex.containsKey(contentWordLowerCase))) {
                 List<WebPage> webPagesToWord = new ArrayList<>();
                 webPagesToWord.add(webPage);
-                webPageKeywordIndex.put(contentWordLowerCase,webPagesToWord);
-            }else {
+                webPageKeywordIndex.put(contentWordLowerCase, webPagesToWord);
+            } else {
                 List<WebPage> webPages = webPageKeywordIndex.get(contentWordLowerCase);
-                if(!(webPages.contains(webPage))){
+                if (!(webPages.contains(webPage))) {
                     webPages.add(webPage);
                 }
 
@@ -30,8 +31,50 @@ public class WebPageIndexer {
         }
     }
 
-    public List<WebPage> getWebPagesByWord(String keyWord,Map<String, List<WebPage>> webPageKeywordIndex){
+    private static String[] divideByWords(String content) {
+        String nonLetterOrDigitRegex = "[^\\p{L}\\p{N}']+";
+        return content.split(nonLetterOrDigitRegex);
+    }
+
+    public List<WebPage> getWebPagesByWord(String keyWord, Map<String, List<WebPage>> webPageKeywordIndex) {
         String contentWordLowerCase = keyWord.toLowerCase();
-        return  webPageKeywordIndex.get(contentWordLowerCase);
+        return webPageKeywordIndex.get(contentWordLowerCase);
+    }
+
+    public void removeFromIndexByUrl(String urlForRemove, Map<String, List<WebPage>> webPageKeywordIndex) {
+        String[] keyWordsWebPageToRemove = getKeyWordsCurrentWebPageByUrl(urlForRemove, webPageKeywordIndex);
+        if(keyWordsWebPageToRemove == null){
+            throw new NoSuchElementException("there is no web page with this url in the index");
+        }
+        for (String keyWordForWebPageToRemove : keyWordsWebPageToRemove) {
+            List<WebPage> webPages = webPageKeywordIndex.get(keyWordForWebPageToRemove);
+
+            for (WebPage webPage : webPages) {
+                String urlCurrentWebPage = webPage.getUrl();
+
+                if(Objects.equals(urlCurrentWebPage,urlForRemove)){
+                    webPages.remove(webPage);
+
+                    if(webPages.isEmpty()){
+                        webPageKeywordIndex.remove(keyWordForWebPageToRemove);
+                    }
+                    break;
+                }
+            }
+        }
+    }
+
+    private static String[] getKeyWordsCurrentWebPageByUrl(String urlForRemove, Map<String, List<WebPage>> webPageKeywordIndex) {
+        for (Map.Entry<String, List<WebPage>> stringListEntry : webPageKeywordIndex.entrySet()) {
+            List<WebPage> webPages = stringListEntry.getValue();
+            for (WebPage webPage : webPages) {
+                String urlCurrentWebPage = webPage.getUrl();
+                if (Objects.equals(urlCurrentWebPage, urlForRemove)) {
+                    String contentCurrentWebPage = webPage.getContent();
+                    return divideByWords(contentCurrentWebPage);
+                }
+            }
+        }
+        return null;
     }
 }
