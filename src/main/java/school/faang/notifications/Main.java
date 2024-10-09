c8428215932343411f97a02aaa0a644679a26f81
nikitasidorov <Nikita.sidorov.7@icloud.com>
2024-10-09 05:33:02 -0700

    сделал фильт на проверку контета сообщения нотификации

--- src/main/java/school/faang/notifications/Main.java
+++ src/main/java/school/faang/notifications/Main.java
@@ -5,25 +5,36 @@ public class Main {
         NotificationManager notificationManager = new NotificationManager();
 
 // Регистрация обработчиков оповещений
-        notificationManager.registerHandler("email",
+        notificationManager.registerNotification("email",
                 (notification) -> System.out.println("Отправка по электронной почте: " + notification.getMessage())
         );
 
-        notificationManager.registerHandler("sms",
+        notificationManager.registerNotification("sms",
                 (notification) -> System.out.println("Отправка SMS: " + notification.getMessage())
         );
 
-        notificationManager.registerHandler("push",
+        notificationManager.registerNotification("push",
                 (notification) -> System.out.println("Отправка push-уведомления: " + notification.getMessage())
         );
+
+        notificationManager.registerNotification("warring",
+                (notification) -> System.out.println("Отправка предупреждения: " + notification.getMessage())
+        );
+
+        notificationManager.registerFilter("warring",
+                (message) -> message.contains("error"));
 
 // Отправка оповещений
         Notification emailNotification = new Notification("email", "Ваша учетная запись успешно активирована");
         Notification smsNotification = new Notification("sms", "Вы успешно изменили свой пароль");
         Notification pushNotification = new Notification("push", "Новый пост от пользователя: JohnDoe");
+        Notification warringNotificationRussia = new Notification("warring", "Пожалуйса подтвердите почту");
+        Notification warringNotificationTEnglish = new Notification("warring", "Error -> confirm email");
 
         notificationManager.sendNotification(emailNotification);
         notificationManager.sendNotification(smsNotification);
         notificationManager.sendNotification(pushNotification);
+        notificationManager.sendNotification(warringNotificationRussia);
+        notificationManager.sendNotification(warringNotificationTEnglish);
     }
 }
--- src/main/java/school/faang/notifications/NotificationManager.java
+++ src/main/java/school/faang/notifications/NotificationManager.java
@@ -3,15 +3,30 @@ package school.faang.notifications;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.function.Consumer;
+import java.util.function.Predicate;
 
 public class NotificationManager {
     private final Map<String, Consumer<Notification>> notifications = new HashMap<>();
+    private final Map<String, Predicate<String>> filters = new HashMap<>();
 
-    public void registerHandler(String type, Consumer<Notification> consumer) {
+    public void registerNotification(String type, Consumer<Notification> consumer) {
         notifications.put(type, consumer);
     }
 
-    public void sendNotification(Notification notification) {
-            notifications.get(notification.getType()).accept(notification);
+    public void registerFilter(String type, Predicate<String> predicate) {
+        filters.put(type, predicate);
     }
+
+    public void sendNotification(Notification notification) {
+        Predicate<String> filter = filters.get(notification.getType());
+
+        if (filter != null && filter.test(notification.getMessage().toLowerCase())) {
+            System.out.println(
+                    "уведмеление типа " + notification.getType() + " не прошло проверку на неприемлемый контент");
+            return;
+        }
+        notifications.get(notification.getType()).accept(notification);
+    }
+
+
 }
