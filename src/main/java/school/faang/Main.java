d1d210dd682428a3237520ebc7e8660166f51585
Matvey <mashptz@gmail.com>
2024-10-09 21:26:34 +0300

    Выполнение условия задачи Использование примера из условия Дополнение своими примерами

--- src/main/java/school/faang/Main.java
+++ src/main/java/school/faang/Main.java
@@ -1,4 +1,28 @@
 package school.faang;
 
+import school.faang.spell.services.SpellCaster;
+
 public class Main {
+    public static void main(String[] args) {
+        SpellCaster spellCaster = new SpellCaster();
+
+        String alohomora = "Alohomora";
+        String lumos = "Lumos";
+        String expelliarmus = "Expelliarmus";
+
+        String fly = "Fly";
+        String grow = "Grow";
+
+        System.out.println("Пример из самой задачи: ");
+
+        spellCaster.cast(alohomora, (spell) -> "The door is unlocked by " + spell);
+        spellCaster.cast(lumos, (spell) -> "A beam of light is created by " + spell);
+        spellCaster.cast(expelliarmus, (spell) -> "The opponent is disarmed by " + spell);
+
+        System.out.println("===============================");
+
+        spellCaster.cast(fly, (spell) -> "The apple is flyed by " + spell);
+        spellCaster.cast(grow,(spell) -> "The tree is grown by " + spell);
+
+    }
 }
