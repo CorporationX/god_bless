ff8a2b25a785de0f886c811f093c50a2bc4d41ed
Ivan Semikov <semikov.exf@gmail.com>
2024-10-09 19:52:13 +0300

    BJS2-32458 Fix mutable fields

--- src/main/java/faang/school/godbless/cache/Student.java
+++ src/main/java/faang/school/godbless/cache/Student.java
@@ -1,6 +1,30 @@
 package faang.school.godbless.cache;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
 import lombok.NonNull;
 
-public record Student(@NonNull Long id, @NonNull String name) {
+import java.util.Objects;
+
+
+@Data
+@AllArgsConstructor
+public class Student {
+    @NonNull
+    private final Long id;
+    @NonNull
+    private String name;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Student student = (Student) o;
+        return Objects.equals(id, student.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
 }
--- src/main/java/faang/school/godbless/cache/Subject.java
+++ src/main/java/faang/school/godbless/cache/Subject.java
@@ -1,6 +1,29 @@
 package faang.school.godbless.cache;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
 import lombok.NonNull;
 
-public record Subject(@NonNull Long id, @NonNull String name) {
+import java.util.Objects;
+
+@Data
+@AllArgsConstructor
+public class Subject {
+    @NonNull
+    private final Long id;
+    @NonNull
+    private String name;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Subject subject = (Subject) o;
+        return Objects.equals(id, subject.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
 }
