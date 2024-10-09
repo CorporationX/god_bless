22025081ffb114039438f078bb642e7b3c6d070d
gimadeev <6962009@gmail.com>
2024-10-09 11:37:29 +0300

    HogwardsHogwards task

---
+++ src/main/java/hogwarts/hogwarts/SpellCaster.java
@@ -0,0 +1,8 @@
+package hogwarts.hogwarts;
+
+public class SpellCaster {
+    public void cast(String spellName, SpellAction spellAction) {
+        String result = spellAction.doSpell(spellName);
+        System.out.println(result);
+    }
+}
