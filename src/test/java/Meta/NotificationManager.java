95de9b7e28a8bbca8a8cd597bce4a9d9fa08e636
Irina Krasnova <arisha.17@mail.ru>
2024-10-09 17:20:57 +0300

    исправлено

--- src/test/java/Meta/NotificationManager.java
+++ src/test/java/Meta/NotificationManager.java
@@ -6,24 +6,18 @@ import java.util.function.Consumer;
 import java.util.function.Function;
 
 public class NotificationManager {
-    private static Map<String, Consumer<Notification>> registerForTypeMap = new HashMap<>();
+    private Map<String, Consumer<Notification>> notificationHendlerMap = new HashMap<>();
 
-    public static void registerHandler(String type, Consumer<Notification> handler) {
-        registerForTypeMap.put(type, handler);
+    public void registerHandler(String type, Consumer<Notification> handler) {
+        notificationHendlerMap.put(type, handler);
     }
 
-    public static void sendNotification(Notification notification) {
+    public void sendNotification(Notification notification) {
         String type = notification.getType();
-        registerForTypeMap.get(type).accept(notification);
+        notificationHendlerMap.get(type).accept(notification);
     }
 
-
-
-    public static void filter(String message, Function<String, String> filter) {
+    public void filter(String message, Function<String, String> filter) {
         System.out.println(filter.apply(message));
-
-
-
-
     }
 }
