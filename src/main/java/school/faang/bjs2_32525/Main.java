7ebf384503267a95f71939106bba614379f9618e
Shap|Misha <maxim.ivanov.jr@gmail.com>
2024-10-10 02:39:50 +0300

    add classes: Student,Subject. add 2 hashmap for Main

---
+++ src/main/java/school/faang/bjs2_32525/Main.java
@@ -0,0 +1,12 @@
+package school.faang.bjs2_32525;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class Main {
+    public static void main(String[] args) {
+        Map<Student, Map<Subject, Integer>> studentGradesBySubject = new HashMap<>();
+        Map<Subject, List<Student>> studentsBySubject = new HashMap<>();
+    }
+}
---
+++ src/main/java/school/faang/bjs2_32525/Subject.java
@@ -0,0 +1,11 @@
+package school.faang.bjs2_32525;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@AllArgsConstructor
+@Data
+public class Subject {
+    private int id;
+    private String name;
+}
