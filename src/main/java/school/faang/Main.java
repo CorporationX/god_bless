0fd376ee006cc97fc2d5a5da99d0e4afd323623b
Matvey <mashptz@gmail.com>
2024-10-09 18:25:40 +0300

    Исправление недочетов

--- src/main/java/school/faang/Main.java
+++ src/main/java/school/faang/Main.java
@@ -1,11 +1,4 @@
 package school.faang;
-
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.LinkedList;
-import java.util.List;
-import java.util.Map;
-
 public class Main {
     public static void main(String[] args) {
         WebPageService webPageService = new WebPageService();
