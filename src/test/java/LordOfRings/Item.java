a466fdc862f8cfb212e6a21c83e7245fa9398abb
Irina Krasnova <arisha.17@mail.ru>
2024-10-09 12:20:04 +0300

    LordOfTheRings

---
+++ src/test/java/LordOfRings/Item.java
@@ -0,0 +1,42 @@
+package LordOfRings;
+
+import java.util.Objects;
+
+public class Item {
+    private String name;
+    private int value;
+
+    public Item(String name, int value) {
+        this.name = name;
+        this.value = value;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getValue() {
+        return value;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Item item = (Item) o;
+        return value == item.value && Objects.equals(name, item.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, value);
+    }
+
+    @Override
+    public String toString() {
+        return "Item{" +
+                "name='" + name + '\'' +
+                ", value=" + value +
+                '}';
+    }
+}
---
+++ src/test/java/LordOfRings/Main.java
@@ -0,0 +1,28 @@
+package LordOfRings;
+
+import java.util.ArrayList;
+import java.util.function.Consumer;
+import java.util.function.Function;
+import java.util.function.Predicate;
+
+public class Main {
+    public static void main(String[] args) {
+        Item sword = new Item("Sword", 1000);
+        Item axe = new Item("Axe", 700);
+        Item pick = new Item("Pisk", 500);
+        Character bilbo = new Character("Bilbo", new ArrayList<>());
+        InventoryManager manager = new InventoryManager();
+
+        Consumer<Item> addItemConsumer = item -> System.out.println(item.getName() + " add to inventory");
+        Predicate<Item> filterPredicate = item -> item.equals(sword);
+
+        manager.addItem(bilbo, sword, item -> System.out.println(item.getName() + " add to inventory"));
+        manager.addItem(bilbo, axe, item -> System.out.println(item.getName() + " add to inventory"));
+        manager.addItem(bilbo, sword, addItemConsumer);
+        manager.addItem(bilbo, pick, addItemConsumer);
+        manager.removeItem(sword, bilbo, item -> item.equals(sword));
+        manager.updateItem(bilbo, filterPredicate, x -> x * 2);
+
+
+    }
+}
