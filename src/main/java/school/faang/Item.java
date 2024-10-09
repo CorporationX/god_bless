96a7c8c1d8c20d10fdd81642a82a65c899d52585
Andrey Kotlyarov <weiss14@yandex.ru>
2024-10-10 00:43:12 +0300

    Here we have a code for the task BJS2-33689

---
+++ src/main/java/school/faang/Item.java
@@ -0,0 +1,20 @@
+package school.faang;
+
+public class Item {
+    private String name;
+    private int value;
+
+    public Item(String name, int value) {
+        this.name = name;
+        this.value = value;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getValue() {
+        return value;
+    }
+
+}
