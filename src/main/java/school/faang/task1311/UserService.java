e09442d3719dd76507879be2a9703560bcf87992
Svyatoslav Kostrov <kost-2014@mail.ru>
2024-10-09 13:53:57 +0300

    fixed addQuery method

--- src/main/java/school/faang/task1311/UserService.java
+++ src/main/java/school/faang/task1311/UserService.java
@@ -8,29 +8,31 @@ import java.util.Map;
 
 public class UserService {
 
-    public static final HashMap<User, List<Query>> USERS_CONTENT = new HashMap<>();
+    public static final HashMap<User, List<Query>> usersContent = new HashMap<>();
 
     public static void addUser(User user, ArrayList<Query> query) {
-        USERS_CONTENT.put(user, query);
+        usersContent.put(user, query);
     }
 
     public static void addQuery(User user, Query query) {
-        USERS_CONTENT.get(user).add(query);
+        if (usersContent.containsKey(user)) {
+            usersContent.get(user).add(query);
+        }
     }
 
     public static void removeUser(User user) {
-        USERS_CONTENT.remove(user);
+        usersContent.remove(user);
     }
 
     public static void printAll() {
-        for (Map.Entry<User, List<Query>> entry : USERS_CONTENT.entrySet()) {
+        for (Map.Entry<User, List<Query>> entry : usersContent.entrySet()) {
             System.out.println(entry.getKey() + " " + entry.getValue());
         }
     }
 
     public static void printAllUserQuery(User user) {
-        ArrayList<Query> listToPrint = (ArrayList<Query>) USERS_CONTENT.get(user);
-        Collections.sort(listToPrint, ((o1, o2) -> o1.getTimestamp() - o2.getTimestamp()));
+        ArrayList<Query> listToPrint = (ArrayList<Query>) usersContent.get(user);
+        listToPrint.sort(((o1, o2) -> o1.getTimestamp() - o2.getTimestamp()));
 
         for (Query query : listToPrint) {
             System.out.println(query);
