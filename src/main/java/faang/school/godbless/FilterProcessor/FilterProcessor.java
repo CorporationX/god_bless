98f7c05d4243bea8673132e828d93cf5cb2aafa4
Alina44444 <alina.valiyeva.s@mail.ru>
2024-10-09 19:10:25 +0500

    Задача Фильтры Instagram

---
+++ src/main/java/faang/school/godbless/FilterProcessor/FilterProcessor.java
@@ -0,0 +1,32 @@
+package faang.school.godbless.FilterProcessor;
+
+import java.util.function.Function;
+
+public class FilterProcessor {
+    public Image applyFilter(Image image, Function<Image, Image> filter) {
+        return filter.apply(image);
+    }
+
+    public Function<Image, Image> combineFilters(Function<Image, Image> firstFilter, Function<Image, Image> secondFilter) {
+        return firstFilter.andThen(secondFilter);
+    }
+
+    public static void main(String[] args) {
+        Image originalImage = new Image("original.jpg", "Оригинальное изображение");
+
+        FilterProcessor filterProcessor = new FilterProcessor();
+
+        Function<Image, Image> grayscaleFilter = (image) -> new Image(image.getName(), image.getDescription() + " | Фильтр: черно-белый");
+        Function<Image, Image> sepiaFilter = (image) -> new Image(image.getName(), image.getDescription() + " | Фильтр: сепия");
+
+        Image grayscaleImage = filterProcessor.applyFilter(originalImage, grayscaleFilter);
+        System.out.println(grayscaleImage.getDescription());
+
+        Image sepiaImage = filterProcessor.applyFilter(grayscaleImage, sepiaFilter);
+        System.out.println(sepiaImage.getDescription());
+
+        Function<Image, Image> combinedFilter = filterProcessor.combineFilters(grayscaleFilter, sepiaFilter);
+        Image combinedImage = filterProcessor.applyFilter(originalImage, combinedFilter);
+        System.out.println(combinedImage.getDescription());
+    }
+}
