89651ea7b302eaf8729cfa6ed318c7d5f9061753
Maxim Gorobets <gorobets.maks@bk.ru>
2024-10-10 00:23:57 +1000

    testing filters of NotificationManager in main method

--- src/main/java/school/faang/BJS2_34731_MetaUniverse/Main.java
+++ src/main/java/school/faang/BJS2_34731_MetaUniverse/Main.java
@@ -1,5 +1,7 @@
 package school.faang.BJS2_34731_MetaUniverse;
 
+import java.util.List;
+import java.util.Set;
 import java.util.function.Consumer;
 
 public class Main {
@@ -16,12 +18,31 @@ public class Main {
                 (notification) -> System.out.println("Sending push notification...\n" + notification.getMessage())
         );
 
-        Notification emailNotification = new Notification("email", "Your order has been shipped and is on its way!");
-        Notification smsNotification = new Notification("sms", "Reminder: Your appointment is scheduled for tomorrow at 10 AM.");
+        notificationManager.registerFilter("Alcohol filter", notification -> {
+            Set<String> alcoholNames = Set.of("beer", "rum", "vodka");
+            if (alcoholNames.stream().anyMatch(alcoholName -> notification.getMessage().toLowerCase().contains(alcoholName))) {
+                System.out.println("Notification message text contains alcohol name!");
+                return true;
+            }
+            return false;
+        });
+
+        notificationManager.registerFilter("Spam filter", notification -> {
+            if (notification.getMessage().toLowerCase().contains("spam")) {
+                System.out.println("Spam has been detected in notification message text!");
+                return true;
+            }
+            return false;
+        });
+
+        Notification emailNotification = new Notification("email", "Check out our new vodka!");
+        Notification unknownTypeNotification = new Notification("unknown", "Hello! My name is Giovanni Giorgio");
+        Notification smsNotification = new Notification("sms", "This is a spam message!");
         Notification pushNotification = new Notification("push", "Congratulations! You have reached a new level in the game.");
 
         notificationManager.sendNotification(emailNotification);
         notificationManager.sendNotification(smsNotification);
+        notificationManager.sendNotification(unknownTypeNotification);
         notificationManager.sendNotification(pushNotification);
     }
 }
