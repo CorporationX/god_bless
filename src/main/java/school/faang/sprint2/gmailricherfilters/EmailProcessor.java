2ecb342afadde280d312ebe2c25e07a50f09a63e
MunirIsmoilov <ismoilov.munir@gmail.com>
2024-10-09 22:25:45 +0300

    commit gmail rich filters

---
+++ src/main/java/school/faang/sprint2/gmailricherfilters/EmailProcessor.java
@@ -0,0 +1,17 @@
+package school.faang.sprint2.gmailricherfilters;
+
+import java.util.List;
+import java.util.function.Consumer;
+import java.util.function.Function;
+import java.util.function.Predicate;
+
+public class EmailProcessor {
+    public void processEmails(List<Email> emails, Predicate<Email> importantFilter, Consumer<Email> printEmail, Function<Email, String> toUpperCase) {
+        for (Email email : emails) {
+            if (importantFilter.test(email)) {
+                email.setBody(toUpperCase.apply(email));
+                printEmail.accept(email);
+            }
+        }
+    }
+}
