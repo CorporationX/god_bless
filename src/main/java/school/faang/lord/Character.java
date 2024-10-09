0d5d748c608882e9e0f0f89379f472db47a670d2
Aleksandr Kuznetsov <adeskmath@gmail.com>
2024-10-10 00:20:01 +0300

    BJS2-33655

---
+++ src/main/java/school/faang/lord/Character.java
@@ -0,0 +1,19 @@
+package school.faang.lord;
+
+import java.util.ArrayList;
+import java.util.List;
+import lombok.Getter;
+import lombok.Setter;
+
+
+@Setter
+@Getter
+public class Character {
+
+  private String name;
+  private List<Item> inventory = new ArrayList<>();
+
+  public Character(String name) {
+    this.name = name;
+  }
+}
---
+++ src/main/java/school/faang/lord/Item.java
@@ -0,0 +1,15 @@
+package school.faang.lord;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@AllArgsConstructor
+@Getter
+@Setter
+public class Item {
+
+  private String name;
+  private int value;
+
+}
