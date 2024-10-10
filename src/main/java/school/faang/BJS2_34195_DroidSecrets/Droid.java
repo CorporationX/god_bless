aa58a83115ba48b10ffe93ad16be3606ce4433d3
Maxim Gorobets <gorobets.maks@bk.ru>
2024-10-10 16:27:37 +1000

    new Droid class + encrypt/decrypt methods

---
+++ src/main/java/school/faang/BJS2_34195_DroidSecrets/Droid.java
@@ -0,0 +1,67 @@
+package school.faang.BJS2_34195_DroidSecrets;
+
+public class Droid {
+    // БЕЗ Ё
+    private static final int RUSSIAN_ALPHABET_LENGTH = 32;
+    private static final int ENGLISH_ALPHABET_LENGTH = 26;
+    private static final char RUSSIAN_ALPHABET_BASE_SMALL = 'а';
+    private static final char RUSSIAN_ALPHABET_BASE_BIG = 'А';
+    private static final char ENGLISH_ALPHABET_BASE_SMALL = 'a';
+    private static final char ENGLISH_ALPHABET_BASE_BIG = 'A';
+
+    public String encryptMessage(String message, int key) {
+        validateMessage(message);
+        DroidMessageEncryptor encryptLogic = (messageToEncrypt, encryptKey) -> {
+            char[] messageChars = messageToEncrypt.toCharArray();
+            for (int i = 0; i < messageChars.length; i++) {
+                char currentChar = messageChars[i];
+                if (isRussianChar(currentChar)) {
+                    char base = Character.isLowerCase(currentChar) ? RUSSIAN_ALPHABET_BASE_SMALL : RUSSIAN_ALPHABET_BASE_BIG;
+                    messageChars[i] = (char)(base + (currentChar + encryptKey - base) % RUSSIAN_ALPHABET_LENGTH);
+                } else if (isEnglishChar(currentChar)) {
+                    char base = Character.isLowerCase(currentChar) ? ENGLISH_ALPHABET_BASE_SMALL : ENGLISH_ALPHABET_BASE_BIG;
+                    messageChars[i] = (char)(base + (currentChar + encryptKey - base) % ENGLISH_ALPHABET_LENGTH);
+                }
+            }
+            return new String(messageChars);
+        };
+        return encryptLogic.encrypt(message, key);
+    }
+
+    public String decryptMessage(String message, int key) {
+        validateMessage(message);
+        DroidMessageEncryptor encryptLogic = (messageToEncrypt, encryptKey) -> {
+            char[] messageChars = messageToEncrypt.toCharArray();
+            for (int i = 0; i < messageChars.length; i++) {
+                char currentChar = messageChars[i];
+                if (isRussianChar(currentChar)) {
+                    char base = Character.isLowerCase(currentChar) ? RUSSIAN_ALPHABET_BASE_SMALL : RUSSIAN_ALPHABET_BASE_BIG;
+                    int shiftValue = (currentChar - encryptKey - base) % RUSSIAN_ALPHABET_LENGTH;
+                    shiftValue = shiftValue > 0 ? shiftValue : RUSSIAN_ALPHABET_LENGTH + shiftValue;
+                    messageChars[i] = (char)(base + shiftValue);
+                } else if (isEnglishChar(currentChar)) {
+                    char base = Character.isLowerCase(currentChar) ? ENGLISH_ALPHABET_BASE_SMALL : ENGLISH_ALPHABET_BASE_BIG;
+                    int shiftValue = (currentChar - encryptKey - base) % ENGLISH_ALPHABET_LENGTH;
+                    shiftValue = shiftValue > 0 ? shiftValue : ENGLISH_ALPHABET_LENGTH + shiftValue;
+                    messageChars[i] = (char)(base + shiftValue);
+                }
+            }
+            return new String(messageChars);
+        };
+        return encryptLogic.encrypt(message, key);
+    }
+
+    private void validateMessage(String message) {
+        if (message == null || message.isBlank()) {
+            throw new IllegalArgumentException("Invalid message");
+        }
+    }
+
+    private boolean isRussianChar(char ch) {
+        return ('а' < ch && ch < 'я') || ('А' < ch && ch < 'Я');
+    }
+
+    private boolean isEnglishChar(char ch) {
+        return ('a' <= ch && ch <= 'z') || ('A' <= ch && ch <= 'Z');
+    }
+}
