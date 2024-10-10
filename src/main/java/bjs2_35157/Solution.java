6c7eb96cfa8a97390f2018cb60a84e511386bee7
light <estarosa@list.ru>
2024-10-10 13:22:46 +0300

    правки: Потренируем StreamAPI 2

--- src/main/java/bjs2_35157/Solution.java
+++ src/main/java/bjs2_35157/Solution.java
@@ -5,59 +5,45 @@ import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
+import java.util.stream.Stream;
 
 public class Solution {
     public static Set<List<Integer>> findPairs(List<Integer> list, int target) {
         Set<List<Integer>> pairs = new HashSet<>();
-        List<Integer> sortedList = list.stream().sorted().toList();
+        Set<Integer> diff = new HashSet<>();
 
-        int left = 0;
-        int right = sortedList.size() - 1;
+        list.stream()
+                .peek(num -> diff.add(target - num))
+                .filter(diff::contains)
+                .forEach(num -> pairs.add(
+                        Stream.of(num, target - num).sorted().toList()
+                ));
 
-        while (left < right) {
-            int sum = sortedList.get(left) + sortedList.get(right);
-
-            if (sum == target) {
-                pairs.add(List.of(sortedList.get(left), sortedList.get(right)));
-                left++;
-                right--;
-            } else if (sum < target) {
-                left++;
-            } else {
-                right--;
-            }
-
-        }
         return pairs;
     }
 
     public static List<String> getCapitals(Map<String, String> countries) {
-        return countries
-                .keySet()
-                .stream()
+        return countries.keySet().stream()
                 .sorted(String::compareTo)
                 .map(countries::get)
                 .toList();
     }
 
     public static List<String> filterAndSortStrings(List<String> strings, char symbol) {
-        return strings
-                .stream()
+        return strings.stream()
                 .filter(string -> string.startsWith(Character.toString(symbol)))
                 .sorted(Comparator.comparingInt(String::length))
                 .toList();
     }
 
     public static List<String> integerToBinaryString(List<Integer> nums) {
-        return nums
-                .stream()
+        return nums.stream()
                 .map(Integer::toBinaryString)
                 .toList();
     }
 
     public static List<String> filterAndSortByAlphabet(List<String> strings, String alphabet) {
-        return strings
-                .stream()
+        return strings.stream()
                 .filter(string -> string.chars()
                         .allMatch(symbol -> alphabet.contains(Character.toString(symbol)))
                 )
