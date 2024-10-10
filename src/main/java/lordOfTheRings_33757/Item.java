3edef921f72eaf79dd8730aee45fdd30a2cfb50d
AlekseyB86nb <tigrxam@gmail.com>
2024-10-10 17:31:48 +0300

    реализация добавления, удаления  предметов в инвентарь и обновление инвентаря

--- src/main/java/lordOfTheRings_33757/Item.java
+++ src/main/java/lordOfTheRings_33757/Item.java
@@ -1,4 +1,11 @@
 package lordOfTheRings_33757;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
 public class Item {
+    private final String name;
+    private int value;
 }
