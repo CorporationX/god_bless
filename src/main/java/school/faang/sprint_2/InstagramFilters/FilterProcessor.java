cb2479e5731ee4765685779615d963c4a7928772
Naku <polevaya.nastena@internet.ru>
2024-10-10 13:54:38 +0300

    Naku-BJS2-34789 Фильтры Instagram v_2

--- src/main/java/school/faang/sprint_2/InstagramFilters/FilterProcessor.java
+++ src/main/java/school/faang/sprint_2/InstagramFilters/FilterProcessor.java
@@ -4,12 +4,11 @@ import java.util.function.Function;
 
 public class FilterProcessor {
 
-    public Image applyFilter(Image image, Function<Image, Image> newFilter) {
-        return newFilter.apply(image);
+    public Image applyFilter(Image image, Function<Image, Image> filter) {
+        return filter.apply(image);
     }
 
-    public Function<Image, Image> combineFilters(Function<Image, Image> firstFilter, Function<Image, Image> secondFilter) {
-        return secondFilter.compose(firstFilter);
-//        return firstFilter.andThen(secondFilter);
+    public Function<Image, Image> combineFilters(Function<Image, Image> filter1, Function<Image, Image> filter2) {
+        return filter1.andThen(filter2);
     }
 }
