9853548b87d7045fe25e04714ac5ade57c91386b
AVB <abdddev@gmail.com>
2024-10-10 10:05:23 +1000

    refactor after review

--- src/main/java/school/faang/filters/instagram/FilterProcessor.java
+++ src/main/java/school/faang/filters/instagram/FilterProcessor.java
@@ -4,11 +4,11 @@ import java.util.function.Function;
 
 public class FilterProcessor {
 
-    public static Image applyFilter(Image image, Function<Image, Image> function) {
-        return function.apply(image);
+    public static Image applyFilter(Image image, Function<Image, Image> filter) {
+        return filter.apply(image);
     }
 
-    public static Image combineFilters(Image image, Function<Image, Image> function1, Function<Image, Image> function2) {
-        return function1.andThen(function2).apply(image);
+    public static Image combineFilters(Image image, Function<Image, Image> filter1, Function<Image, Image> filter2) {
+        return filter1.andThen(filter2).apply(image);
     }
 }
