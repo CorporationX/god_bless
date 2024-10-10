2409a231b3eb6be330a25d23c2d9c44ad8eab499
Roman Nifontov <romannif@mail.ru>
2024-10-10 20:28:05 +0500

    Сделал изменения в задаче BJS2-34473

--- src/main/java/school/BJS2_34473/LocationSearchEngine.java
+++ src/main/java/school/BJS2_34473/LocationSearchEngine.java
@@ -1,6 +1,5 @@
 package school.BJS2_34473;
 
-import java.util.ArrayList;
 import java.util.List;
 import java.util.function.Consumer;
 import java.util.function.Function;
@@ -9,9 +8,7 @@ import java.util.function.Predicate;
 public class LocationSearchEngine {
 
     public List<Location> filterLocations(List<Location> list, Predicate<Location> filter) {
-        List<Location> result = new ArrayList<>();
-        list.stream().filter(filter).forEach(result::add);
-        return result;
+        return list.stream().filter(filter).toList();
     }
 
     public void processLocations(List<Location> list, Consumer<Location> consumer) {
@@ -19,12 +16,7 @@ public class LocationSearchEngine {
     }
 
     public List<Double> calculateDistances(List<Location> list, Function<Location, Double> distanceFunction) {
-        List<Double> result = new ArrayList<>();
-        for (Location location : list) {
-            double distance = distanceFunction.apply(location);
-            result.add(distance);
-        }
-        return result;
+        return list.stream().map(distanceFunction::apply).toList();
     }
 
 }
