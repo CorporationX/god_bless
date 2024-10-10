8af4520c86379bf7d96de96118059920f643945a
AlekseyB86nb <tigrxam@gmail.com>
2024-10-10 21:24:04 +0300

    init task Google переводчик

---
+++ src/main/java/google_translate_33542/Main.java
@@ -0,0 +1,20 @@
+package google_translate_33542;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.function.BiConsumer;
+
+public class Main {
+    public static void main(String[] args) {
+        DictionaryProcessor dictionaryProcessor = new DictionaryProcessor();
+        Map<String,String> dictionary = new HashMap<>();
+
+        BiConsumer<String, String> addWordToDictionary = dictionary::put;
+
+        dictionaryProcessor.processWord("Hello", "Привет", addWordToDictionary);
+        dictionaryProcessor.processWord("world", "мир", addWordToDictionary);
+        dictionaryProcessor.processWord("code", "код", addWordToDictionary);
+
+        System.out.println("Словарь: " + dictionary);
+    }
+}
