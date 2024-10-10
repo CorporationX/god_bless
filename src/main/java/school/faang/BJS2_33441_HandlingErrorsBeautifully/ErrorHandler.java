337e0d16c02a59087c1c655c47fb9761d5403342
Maxim Gorobets <gorobets.maks@bk.ru>
2024-10-10 15:02:18 +1000

    validating lambdas for null

--- src/main/java/school/faang/BJS2_33441_HandlingErrorsBeautifully/ErrorHandler.java
+++ src/main/java/school/faang/BJS2_33441_HandlingErrorsBeautifully/ErrorHandler.java
@@ -1,7 +1,14 @@
 package school.faang.BJS2_33441_HandlingErrorsBeautifully;
 
+import com.sun.jdi.ObjectCollectedException;
+
+import java.util.Objects;
+
 public class ErrorHandler {
     public static <T> T withErrorHandling(ThrowableAction<T> action, ExceptionHandler<T> exceptionHandler) {
+        if (action == null || exceptionHandler == null) {
+            throw new IllegalArgumentException("Action and exception handler can't be null!");
+        }
         try {
             return action.get();
         } catch (Exception exception) {
