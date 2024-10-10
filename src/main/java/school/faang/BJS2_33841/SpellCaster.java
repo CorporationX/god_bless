846ecff7f8b69a7964d6b2d87c68dfef6186c88c
SerUKalinin <serukalinin@gmail.com>
2024-10-10 19:46:54 +0300

    Removed student's code

--- src/main/java/school/faang/BJS2_33841/SpellCaster.java
+++ src/main/java/school/faang/BJS2_33841/SpellCaster.java
@@ -1,7 +1,8 @@
 package school.faang.BJS2_33841;
 
 public class SpellCaster {
-    public void cast(String spellName, String action) {
-        String result =
+    public void cast(String spellName, SpellAction action){
+        String result = action.castSpell(spellName);
+        System.out.println(result);
     }
 }
