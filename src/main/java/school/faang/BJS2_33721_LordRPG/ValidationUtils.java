5ea1100657fbb7260ff88e634c8b09129447268a
Maxim Gorobets <gorobets.maks@bk.ru>
2024-10-10 20:16:36 +1000

    validations for lambdas

--- src/main/java/school/faang/BJS2_33721_LordRPG/ValidationUtils.java
+++ src/main/java/school/faang/BJS2_33721_LordRPG/ValidationUtils.java
@@ -1,5 +1,9 @@
 package school.faang.BJS2_33721_LordRPG;
 
+import java.util.function.Consumer;
+import java.util.function.Function;
+import java.util.function.Predicate;
+
 public class ValidationUtils {
     public static void validateString(String info, String message) {
         if (info == null || info.isBlank()) {
@@ -22,6 +26,24 @@ public class ValidationUtils {
     public static void validateCharacter(Character character) {
         if (character == null) {
             throw new IllegalArgumentException("Character can't be null!");
+        }
+    }
+
+    public static void validateConsumer(Consumer<?> consumer, String message) {
+        if (consumer == null) {
+            throw new IllegalArgumentException(message);
+        }
+    }
+
+    public static void validatePredicate(Predicate<?> predicate, String message) {
+        if (predicate == null) {
+            throw new IllegalArgumentException(message);
+        }
+    }
+
+    public static void validateFunction(Function<?, ?> function, String message) {
+        if (function == null) {
+            throw new IllegalArgumentException(message);
         }
     }
 }
