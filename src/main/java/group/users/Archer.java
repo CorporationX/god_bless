504396a6cff10c04fcb3f982229dea7c3e985788
Viktor Krivetskiy <1woracc@gmail.com>
2024-10-10 23:04:42 +0300

    Solve problems

--- src/main/java/group/users/Archer.java
+++ src/main/java/group/users/Archer.java
@@ -1,13 +1,12 @@
 package group.users;
 
 public class Archer extends Character {
-
-    protected Archer(String name, int strength, int dexterity, int intelligence) {
+    public Archer(String name, int strength, int dexterity, int intelligence) {
         super(name, 3, 10, 5);
     }
 
     @Override
-    protected void attack(Character character) {
+    public void attack(Character character) {
         character.setHealth(character.getHealth() - character.getDexterity());
         System.out.println("\n" + "Лучник нанес " + character.getDexterity() + " урона противнику " + character.getName());
         System.out.println("У противника осталось " + character.getHealth() + " здоровья");
--- src/main/java/group/users/Character.java
+++ src/main/java/group/users/Character.java
@@ -5,7 +5,6 @@ import lombok.Setter;
 
 @Getter
 @Setter
-
 public abstract class Character {
     private int health = 100;
     private String name;
@@ -13,16 +12,16 @@ public abstract class Character {
     private int dexterity;
     private int intelligence;
 
-    protected Character(String name) {
+    public Character(String name) {
         this.name = name;
     }
 
-    protected Character(String name, int strength, int dexterity, int intelligence) {
+    public Character(String name, int strength, int dexterity, int intelligence) {
         this.name = name;
         this.strength = strength;
         this.dexterity = dexterity;
         this.intelligence = intelligence;
     }
 
-    protected abstract void attack(Character character);
+    public abstract void attack(Character character);
 }
