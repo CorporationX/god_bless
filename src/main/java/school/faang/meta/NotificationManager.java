12a801539b9d18cc050f3b9acb1d681b5428d8fc
AVB <abdddev@gmail.com>
2024-10-10 12:36:02 +1000

    refactor after review

--- src/main/java/school/faang/meta/NotificationManager.java
+++ src/main/java/school/faang/meta/NotificationManager.java
@@ -1,21 +1,24 @@
 package school.faang.meta;
 
+import lombok.Getter;
+
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 import java.util.function.Consumer;
 import java.util.function.Function;
 import java.util.function.Predicate;
 
 public class NotificationManager {
-    private Map<String, Consumer<Notification>> notifications;
-    private Map<String, Predicate<Notification>> censures;
-    private Map<String, Function<Notification, Notification>> correctors;
+    private static final String DEFAULT_MESSAGE = "Отправка данным типом не возможна: ";
+    private static final String CENSORED_MESSAGE = "Отправка данного сообщения не возможна. Цензура не пройдена.";
 
-    public NotificationManager() {
-        this.notifications = new HashMap<>();
-        this.censures = new HashMap<>();
-        this.correctors = new HashMap<>();
-    }
+    @Getter
+    private List<String> sentMessages = new ArrayList<>();
+    private Map<String, Consumer<Notification>> notifications = new HashMap<>();
+    private Map<String, Predicate<Notification>> censures = new HashMap<>();
+    private Map<String, Function<Notification, Notification>> correctors = new HashMap<>();
 
     public void registerHandler(String name, Consumer<Notification> consumer) {
         notifications.put(name, consumer);
@@ -39,12 +42,20 @@ public class NotificationManager {
 
     public void sendNotification(Notification notification) {
         if (isValidMessage(notification)) {
-            correctors.forEach((name, f) -> f.apply(notification));
-            notifications.getOrDefault(notification.getType(),
-                    (notificationDefault) -> System.out.println("Отправка данным типом не возможна: " + notificationDefault.getType())
-            ).accept(notification);
+            for (Function<Notification, Notification> corrector : correctors.values()) {
+                notification = corrector.apply(notification);
+            }
+            Consumer<Notification> notificationAction = notifications.get(notification.getType());
+            if (notificationAction != null) {
+                notificationAction.accept(notification);
+            } else {
+                String defaultMessage = DEFAULT_MESSAGE + notification.getType();
+                sentMessages.add(defaultMessage);
+                System.out.println(defaultMessage);
+            }
         } else {
-            System.out.println("Отправка данного сообщения не возможна. Цензура не пройдена.");
+            sentMessages.add(CENSORED_MESSAGE);
+            System.out.println(CENSORED_MESSAGE);
         }
     }
 
