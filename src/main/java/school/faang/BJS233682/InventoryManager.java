b636433d818d27067fe0b7d0a4f66bb48c0d37a9
Doniyor Kurbanov <doniyor.kurbanov.24@gmail.com>
2024-10-10 22:50:50 +0500

    BJS2-33682. Жду фитбека. Спасибо большое

---
+++ src/main/java/school/faang/BJS233682/InventoryManager.java
@@ -0,0 +1,46 @@
+package school.faang.BJS233682;
+
+import java.util.function.Consumer;
+import java.util.function.Function;
+import java.util.function.Predicate;
+
+public class InventoryManager {
+
+    public void addItem(Character character, Item item, Consumer<Item> consumer) {
+        character.getInventory().add(item);
+        consumer.accept(item);
+    }
+
+    public void removeItem(Character character, Predicate<Item> predicate) {
+        character.getInventory().removeIf(predicate);
+    }
+
+    public void updateItem(Character character, Predicate<Item> predicate, Function<Item, Item> updater) {
+        for (Item item : character.getInventory()) {
+            if (predicate.test(item)) {
+                updater.apply(item);
+            }
+        }
+    }
+
+
+    public static void main(String[] args) {
+        Character frodo = new Character("Frodo");
+        Item ring = new Item("The One Ring", 1000);
+
+        InventoryManager manager = new InventoryManager();
+
+// Добавляем предмет в инвентарь
+        manager.addItem(frodo, ring, (item) -> System.out.println(item.getName() + " был добавлен в инвентарь."));
+
+// Удаляем предмет из инвентаря
+        manager.removeItem(frodo, (item) -> item.getName().equals("The One Ring"));
+
+// Добавляем обратно предмет и обновляем его стоимость
+        manager.addItem(frodo, ring, (item) -> System.out.println(item.getName() + " снова добавлен."));
+        manager.updateItem(frodo, (item) -> item.getName().equals("The One Ring"), (item) -> new Item(item.getName(), item.getValue() * 2));
+
+// Проверяем результат
+        frodo.getInventory().forEach(item -> System.out.println(item.getName() + ": " + item.getValue()));
+    }
+}
