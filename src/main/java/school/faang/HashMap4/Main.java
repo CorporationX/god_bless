98b34d5f2cf0b6a11cac536c1651b380be2108a8
bogda <bogdanskrip404@gmail.com>
2024-10-10 13:03:14 +0500

    вынес методы в отдельный класс, доработал разделение строки не только на пробелы, но и знаки препинания. Выровнял код

--- src/main/java/school/faang/HashMap4/Main.java
+++ src/main/java/school/faang/HashMap4/Main.java
@@ -3,21 +3,20 @@ package school.faang.HashMap4;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-
-import static school.faang.HashMap4.WebPage.deletePage;
-import static school.faang.HashMap4.WebPage.index;
-import static school.faang.HashMap4.WebPage.printMap;
-import static school.faang.HashMap4.WebPage.printWebPageList;
-import static school.faang.HashMap4.WebPage.searchKeyWord;
+import static school.faang.HashMap4.WebPageOperator.deletePage;
+import static school.faang.HashMap4.WebPageOperator.index;
+import static school.faang.HashMap4.WebPageOperator.printMap;
+import static school.faang.HashMap4.WebPageOperator.printWebPageList;
+import static school.faang.HashMap4.WebPageOperator.searchKeyWord;
 
 public class Main {
     public static void main(String[] args) {
         Map<String, List<WebPage>> webPageMap = new HashMap<>();
 
-        index(new WebPage("vk.com", "VK", "Hello my friends"), webPageMap);
+        index(new WebPage("vk.com", "VK", "Hello my friends."), webPageMap);
         index(new WebPage("telegram.org", "Tg", "Hello world"), webPageMap);
         index(new WebPage("x.com", "X", "The best friends"), webPageMap);
-        index(new WebPage("youtu.be", "YouTube", "Say hello to the best friend"), webPageMap);
+        index(new WebPage("youtu.be", "YouTube", "Say hello, the best friend"), webPageMap);
         printMap(webPageMap);
         System.out.println("Searching word \"Hello\"");
         printWebPageList(searchKeyWord("Hello", webPageMap));
--- src/main/java/school/faang/HashMap4/WebPage.java
+++ src/main/java/school/faang/HashMap4/WebPage.java
@@ -31,46 +31,4 @@ public class WebPage {
     public WebPage(String url) {
         this.url = url;
     }
-
-    public static void index(WebPage webPage, Map<String, List<WebPage>> webPageMap){
-        String[] words = webPage.getContent().toLowerCase().split(" ");
-        for (String word : words){
-            webPageMap.computeIfAbsent(word, k -> new ArrayList<>()).add(webPage);
-        }
-    }
-
-    public static List<WebPage> searchKeyWord(String keyWord, Map<String, List<WebPage>> webPageMap){
-        if (webPageMap.get(keyWord.toLowerCase()) != null){
-            return webPageMap.get(keyWord.toLowerCase());
-            } else {
-            return null;
-        }
-    }
-
-    public static void deletePage (String url, Map<String, List<WebPage>> webPageMap){
-        WebPage page = new WebPage(url);
-        for (var entry : webPageMap.entrySet()){
-            if(entry.getValue().contains(page)){
-                entry.getValue().remove(page);
-            }
-        }
-    }
-
-    public static void printMap(Map<String, List<WebPage>> webPageMap){
-        for (var entry: webPageMap.entrySet()){
-            System.out.println("Word: " + entry.getKey() + "\nPages: ");
-            printWebPageList(entry.getValue());
-        }
-    }
-
-    public static void printWebPageList(List<WebPage> webPageList) {
-        if (webPageList != null) {
-            for (WebPage page : webPageList) {
-                System.out.println(page.getTitle());
-            }
-        } else {
-            System.out.println("not found");
-        }
-        System.out.println();
-    }
 }
