dfe2b3a86b5eb5e65bfa6887ab53a70433e07a3d
Egor Semenovskii <gaoqiao5217@gmail.com>
2024-10-10 18:52:42 +0300

    -

--- src/main/java/school/faang/Errors/ErrorHandler.java
+++ src/main/java/school/faang/Errors/ErrorHandler.java
@@ -1,7 +1,6 @@
 package school.faang.Errors;
 
 import java.util.function.Function;
-import java.util.function.Supplier;
 
 public class ErrorHandler {
     public static <T> T withErrorHandling(ErrorSupplier<T> supplier, Function<Exception, T> function) {
--- src/main/java/school/faang/Errors/ErrorSupplier.java
+++ src/main/java/school/faang/Errors/ErrorSupplier.java
@@ -1,7 +1,5 @@
 package school.faang.Errors;
 
-import java.util.function.Supplier;
-
 @FunctionalInterface
 public interface ErrorSupplier<T>{
     T get() throws Exception;
