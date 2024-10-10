1c5581c1fc0f62e1e11a0ec9d82177d5ae3c7669
Sergey <sergey@Marks-MacBook-Pro.local>
2024-10-10 18:22:38 +0600

    BJS2-33635 Add Gmail filters

---
+++ src/main/java/gmail/EmailProcessor.java
@@ -0,0 +1,20 @@
+package gmail;
+
+import java.util.List;
+import java.util.function.Consumer;
+import java.util.function.Function;
+import java.util.function.Predicate;
+
+public class EmailProcessor {
+    public void processEmails(
+            List<Email> emails,
+            Predicate<Email> filter,
+            Function<Email, String> bodyTransformer,
+            Consumer<Email> action
+    ) {
+        emails.stream()
+            .filter(filter)
+            .peek((email) -> email.setBody(bodyTransformer.apply(email)))
+            .forEach(action);
+    }
+}
