081e25321f520467e9d035e1c60c953ee6bb7719
Igor <asachiyigor@gmail.com>
2024-10-10 09:47:15 +0300

    BJS2-34563
    initial commit

---
+++ src/main/java/school/faangSprint2/t12/MessageProcessor.java
@@ -0,0 +1,15 @@
+package school.faangSprint2.t12;
+
+import java.util.List;
+
+public class MessageProcessor {
+    public boolean processMessage(String message, List<MessageFilter> filters) {
+        for (MessageFilter filter : filters) {
+            if (!filter.filter(message)) {
+                return false;
+            }
+
+        }
+        return true;
+    }
+}
