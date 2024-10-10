409a08c4f3117cfdc10e13b4ffd8f546749af734
Denis Levkhov <levkhovde@yandex.ru>
2024-10-11 00:00:25 +0300

    Adding constant variables

--- src/main/java/secretsOfDroids/Droid.java
+++ src/main/java/secretsOfDroids/Droid.java
@@ -5,37 +5,40 @@ import lombok.AllArgsConstructor;
 @AllArgsConstructor
 public class Droid {
     private String name;
+    private static final int ENG_ALPHABET_SIZE = 26;
+
+    private static final DroidMessageEncryptor ENCRYPTOR = (msg, encryptKey) -> {
+        StringBuilder encryptedMessage = new StringBuilder();
+        for (char ch : msg.toCharArray()) {
+            if (Character.isLetter(ch)) {
+                char base = Character.isLowerCase(ch) ? 'a' : 'A';
+                encryptedMessage.append((char) ((ch - base + encryptKey) % ENG_ALPHABET_SIZE + base));
+            } else {
+                encryptedMessage.append(ch);
+            }
+        }
+        return encryptedMessage.toString();
+    };
+
+    private static final DroidMessageEncryptor DECRYPTOR = (msg, decryptionKey) -> {
+        StringBuilder decryptedMessage = new StringBuilder();
+        for (char ch : msg.toCharArray()) {
+            if (Character.isLetter(ch)) {
+                char base = Character.isLowerCase(ch) ? 'a' : 'A';
+                decryptedMessage.append((char) ((ch - base - decryptionKey + ENG_ALPHABET_SIZE) % ENG_ALPHABET_SIZE + base));
+            } else {
+                decryptedMessage.append(ch);
+            }
+        }
+        return decryptedMessage.toString();
+    };
 
     private String encryptMessage(String message, int key) {
-        DroidMessageEncryptor encryptor = (msg, encryptKey) -> {
-            StringBuilder encryptedMessage = new StringBuilder();
-            for (char ch : msg.toCharArray()) {
-                if (Character.isLetter(ch)) {
-                    char base = Character.isLowerCase(ch) ? 'a' : 'A';
-                    encryptedMessage.append((char) ((ch - base + encryptKey) % 26 + base));
-                } else {
-                    encryptedMessage.append(ch);
-                }
-            }
-            return encryptedMessage.toString();
-        };
-        return encryptor.encrypt(message, key);
+        return ENCRYPTOR.encrypt(message, key);
     }
     
     private String decryptMessage(String message, int key) {
-        DroidMessageEncryptor decryptor = (msg, decryptionKey) -> {
-            StringBuilder decryptedMessage = new StringBuilder();
-            for (char ch : msg.toCharArray()) {
-                if (Character.isLetter(ch)) {
-                    char base = Character.isLowerCase(ch) ? 'a' : 'A';
-                    decryptedMessage.append((char) ((ch - base - decryptionKey + 26) % 26 + base));
-                } else {
-                    decryptedMessage.append(ch);
-                }
-            }
-            return decryptedMessage.toString();
-        };
-        return decryptor.encrypt(message, key);
+        return DECRYPTOR.encrypt(message, key);
     }
 
     public void sendMessage(Droid droid, String message, int encryptionKey) {
