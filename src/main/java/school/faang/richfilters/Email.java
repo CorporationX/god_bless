5dc2b3745af5d06b6512740ba9afa187456f2820
Алексей Беляев <belyaevayur@intech.rshb.ru>
2024-10-10 19:32:06 +0300

    BJS2-33820 Gmail Rich Filters

---
+++ src/main/java/school/faang/richfilters/Email.java
@@ -0,0 +1,12 @@
+package school.faang.richfilters;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class Email {
+    private String subject;
+    private String body;
+    private boolean isImportant;
+}
