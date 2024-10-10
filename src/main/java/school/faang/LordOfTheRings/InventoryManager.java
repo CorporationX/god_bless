14f50adf400a46306c6afeea83d1d1b706e5b8e2
Egor Semenovskii <gaoqiao5217@gmail.com>
2024-10-10 18:26:39 +0300

    Lord of the Rings RPG

---
+++ src/main/java/school/faang/LordOfTheRings/InventoryManager.java
@@ -0,0 +1,22 @@
+package school.faang.LordOfTheRings;
+
+import java.util.function.Consumer;
+import java.util.function.Function;
+import java.util.function.Predicate;
+import java.util.stream.Collectors;
+
+public class InventoryManager {
+
+    public void addItem(Character character, Item item, Consumer<Item> consumer) {
+        character.getInventory().add(item);
+        consumer.accept(item);
+    }
+
+    public void removeItem(Character character, Predicate<Item> predicate) {
+        character.getInventory().removeIf(predicate);
+    }
+
+    public void updateItem(Character character, Predicate<Item> predicate, Function<Item, Item> function) {
+        character.getInventory().stream().filter(predicate).forEach(function::apply);
+    }
+}
