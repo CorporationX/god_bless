1e04d9a5ff0ec4ee828b0991398bb3655d6f3201
MunirIsmoilov <ismoilov.munir@gmail.com>
2024-10-10 13:43:13 +0300

    commit Lord of the Rings RPG 1

--- src/main/java/school/faang/sprint2/loardoftheeings/Main.java
+++ src/main/java/school/faang/sprint2/loardoftheeings/Main.java
@@ -4,6 +4,7 @@ public class Main {
     public static void main(String[] args) {
         Character frodo = new Character("Frodo");
         Item ring = new Item("The One Ring", 1000);
+
         InventoryManager manager = new InventoryManager();
         manager.addItem(frodo, ring, (item) -> System.out.println(item.getName() + " был добавлен в инвентарь."));
         manager.removeItem(frodo, (item) -> item.getName().equals("The One Ring"));
