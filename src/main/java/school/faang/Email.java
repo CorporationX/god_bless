0e44bf340d61d0b2ace0e90d5a852fd863e8c0e5
lomuserg <sergeymu2015@yandex.ru>
2024-10-10 12:37:20 +0300

    Email.java

--- src/main/java/school/faang/Email.java
+++ src/main/java/school/faang/Email.java
@@ -1,10 +1,11 @@
 package school.faang;
 
 public class Email {
-   private String subject;
-   private String body;
-   public boolean isImportant;
-    public Email(String subject, String body, boolean isImportant){
+    private String subject;
+    private String body;
+    public boolean isImportant;
+
+    public Email(String subject, String body, boolean isImportant) {
         this.subject = subject;
         this.body = body;
         this.isImportant = isImportant;
