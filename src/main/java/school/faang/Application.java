c563925512cfb5c5a78ca3ff74b6729c313755d4
Stanislaav Kozh <printks@yandex.ru>
2024-10-11 22:11:16 +0300

    Абстракция, абстракция!

---
+++ src/main/java/school/faang/Application.java
@@ -0,0 +1,7 @@
+package school.faang;
+
+public class Application {
+    public static void main(String[] args) {
+        System.out.println("God Bless!");
+    }
+}
--- src/main/java/school/faang/Main.java
+++ src/main/java/school/faang/Main.java
@@ -1,4 +1,18 @@
 package school.faang;
 
 public class Main {
+
+    public static void main(String[] args) {
+
+        Warrior warrior = new Warrior("Mark");
+        Archer archer = new Archer("Andrei");
+
+        warrior.attack(archer);
+        archer.attack(warrior);
+
+        System.out.println(archer.getName() + " to have " + archer.getHealth() + " health.");
+        System.out.println(warrior.getName() + " to have " + warrior.getHealth() + " health.");
+    }
+
 }
+
