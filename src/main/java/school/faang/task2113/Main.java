21c5170df311834cefa084abec97a69393d518fa
Svyatoslav Kostrov <kost-2014@mail.ru>
2024-10-11 22:33:45 +0300

    Solution for triangle task

---
+++ src/main/java/school/faang/task2113/Main.java
@@ -0,0 +1,16 @@
+package school.faang.task2113;
+
+import school.faang.task2113.triangle.TriangleAreaCalculator;
+
+public class Main {
+    public static void main(String[] args) {
+        TriangleAreaCalculator calculator = new TriangleAreaCalculator();
+
+        try {
+            double area = calculator.calculateTriangleArea(3, 4, 5);
+            System.out.println("Площадь треугольника: " + area);
+        } catch (IllegalArgumentException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+}
---
+++ src/main/java/school/faang/task2113/triangle/TriangleAreaCalculator.java
@@ -0,0 +1,27 @@
+package school.faang.task2113.triangle;
+
+import java.util.function.Function;
+
+public class TriangleAreaCalculator {
+    Function<Double, Function<Double, Double>> add = (x) -> (y) -> x + y;
+    Function<Double, Function<Double, Double>> multiply = (x) -> (y) -> x * y;
+    Function<Double, Function<Double, Double>> subtract = (x) -> (y) -> x - y;
+    Function<Double, Function<Double, Double>> divide = (x) -> (y) -> x / y;
+    Function<Double, Double> squareRoot = (x) -> Math.sqrt(x);
+
+    public Double calculateTriangleArea(double a, double b, double c) throws IllegalArgumentException {
+        if (a > add.apply(b).apply(c) || c > add.apply(a).apply(b) || b > add.apply(c).apply(a)) {
+            throw new IllegalArgumentException();
+        }
+        double halfPerimeter = divide.apply(add.apply(a).apply(add.apply(b).apply(c))).apply(2.0);
+        double result= squareRoot.apply(
+                multiply.apply(
+                        multiply.apply(
+                                multiply.apply(halfPerimeter)
+                                        .apply(subtract.apply(halfPerimeter).apply(a)))
+                                .apply(subtract.apply(halfPerimeter).apply(b))).
+                        apply(subtract.apply(halfPerimeter).apply(c))
+        );
+        return result;
+    }
+}
