7409871de1bc754b51c89a7d59954076c04319e7
VovaBuzivskyi <VovaBuzivskyi@gmail.com>
2024-10-12 02:37:07 +0700

    BJS2-34485 google maps

---
+++ src/main/java/school/faang/google_maps_BJS2_34485/Location.java
@@ -0,0 +1,12 @@
+package school.faang.google_maps_BJS2_34485;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class Location {
+    private String name;
+    private double latitude;
+    private double longitude;
+}
