73636b451140974fc4410544e1cff3eaa979812f
AlekseyB86nb <tigrxam@gmail.com>
2024-10-12 01:42:33 +0300

    init task Анализ активности пользователей в социальной сети

---
+++ src/main/java/analysisOfUserActivity_35491/UserAction.java
@@ -0,0 +1,19 @@
+package analysisOfUserActivity_35491;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+import java.time.LocalDate;
+
+
+@Data
+@AllArgsConstructor
+public class UserAction {
+    private int id;
+    private String name;
+    private String actionType;
+    private LocalDate actionDate;
+    private String content;
+
+}
---
+++ src/main/java/analysisOfUserActivity_35491/UserActionAnalyzer.java
@@ -0,0 +1,31 @@
+package analysisOfUserActivity_35491;
+
+import java.util.List;
+import java.util.Map;
+
+public class UserActionAnalyzer {
+
+//    new UserAction(1, "Alice", "post",LocalDate.of(2024, 9, 1), "Check out this amazing #newfeature!"),
+//    new UserAction(2, "Bob", "comment", LocalDate.of(2024, 9, 2), "I totally agree with #newfeature."),
+//    new UserAction(1, "Alice", "like", LocalDate.of(2024, 9, 3), ""),
+//    new UserAction(3, "Charlie", "share", LocalDate.of(2024, 9, 4), ""),
+//    new UserAction(4, "Dave", "post", LocalDate.of(2024, 9, 5), "#Java is awesome!"),
+//    new UserAction(2, "Bob", "like", LocalDate.of(2024, 9, 6), ""),
+
+
+    public static List<String> topActiveUsers(List<UserAction> actions, int limit) {
+        return null;
+    }
+
+    public static List<String> topPopularHashtags(List<UserAction> actions, int limit) {
+        return null;
+    }
+
+    public static List<String> topCommentersLastMonth(List<UserAction> actions, int limit) {
+        return null;
+    }
+
+    public static Map<String, Double> actionTypePercentages(List<UserAction> actions) {
+        return null;
+    }
+}
