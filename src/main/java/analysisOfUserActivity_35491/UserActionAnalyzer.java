7f480b5a7256e23c933186af077fbe3b48949113
AlekseyB86nb <tigrxam@gmail.com>
2024-10-12 02:55:51 +0300

    реализация методов топ-5 хештегов, топ-3 юзера по коментам, процент каждого типа действий

--- src/main/java/analysisOfUserActivity_35491/UserActionAnalyzer.java
+++ src/main/java/analysisOfUserActivity_35491/UserActionAnalyzer.java
@@ -1,9 +1,11 @@
 package analysisOfUserActivity_35491;
 
+import java.time.LocalDate;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 import java.util.Map;
+import java.util.Optional;
 import java.util.stream.Collector;
 import java.util.stream.Collectors;
 
@@ -28,14 +30,34 @@ public class UserActionAnalyzer {
     }
 
     public static List<String> topPopularHashtags(List<UserAction> actions, int limit) {
-        return null;
+        return actions.stream()
+                .collect(Collectors.groupingBy(UserAction::getContent, Collectors.counting()))
+                .entrySet().stream()
+                .filter(entry -> entry.getKey().startsWith("#"))
+                .sorted(Collections.reverseOrder(Map.Entry.comparingByValue()))
+                .limit(limit)
+                .map(Map.Entry::getKey)
+                .collect(Collectors.toList());
     }
 
     public static List<String> topCommentersLastMonth(List<UserAction> actions, int limit) {
-        return null;
+        int lastMonth = LocalDate.now().minusMonths(1).getMonthValue();
+
+        return actions.stream()
+                .filter(action -> action.getActionDate().getMonthValue() == lastMonth
+                        && action.getActionType().equals("comment"))
+                .collect(Collectors.groupingBy(UserAction::getName, Collectors.counting()))
+                .entrySet().stream()
+                .sorted(Collections.reverseOrder(Map.Entry.comparingByValue()))
+                .limit(limit)
+                .map(Map.Entry::getKey)
+                .collect(Collectors.toList());
     }
 
     public static Map<String, Double> actionTypePercentages(List<UserAction> actions) {
-        return null;
+        return actions.stream()
+                .collect(Collectors.groupingBy(UserAction::getActionType, Collectors.counting()))
+                .entrySet().stream()
+                .collect(Collectors.toMap(Map.Entry::getKey, entry -> entry.getValue() * 100.0 / actions.size()));
     }
 }
