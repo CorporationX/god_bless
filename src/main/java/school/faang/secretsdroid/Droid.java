01d7a46729be2fee1ae64735b2301ac26655a388
Memoreks <akifyevalex1@gmail.com>
2024-10-11 22:04:00 +0700

    Внесены правки согласно комментариям

--- src/main/java/school/faang/secretsdroid/Droid.java
+++ src/main/java/school/faang/secretsdroid/Droid.java
@@ -1,40 +1,43 @@
 package school.faang.secretsdroid;
 
 public class Droid {
+    private static final Integer SIZE_ALPHABET = 26;
+
     private String name;
 
     public Droid(String name) {
         this.name = name;
     }
 
-    private DroidMessageEncryptor encryptor = (m, k) -> {
-        String newStr = "";
-        for (int i = 0; i < m.length(); i++) {
-            if(m.charAt(i) != ' '){
-                newStr += (char) (m.charAt(i) + k);
+    private DroidMessageEncryptor encryptor = (message, key) -> {
+        String encryptedMessage = "";
+        for (int i = 0; i < message.length(); i++) {
+            if(Character.isLetter(message.charAt(i))){
+                char startCharacter = Character.isUpperCase(message.charAt(i)) ? 'A' : 'a';
+                encryptedMessage += (char) ((message.charAt(i) - startCharacter + key) % SIZE_ALPHABET + startCharacter);
             } else {
-                newStr += ' ';
+                encryptedMessage += message.charAt(i);
             }
         }
-        return newStr;
+        return encryptedMessage;
     };
     public String encryptMessage(String message, Integer key){
         return encryptor.crypto(message, key);
     }
 
     public String decryptMessage(String message, Integer key) {
-        return encryptor.crypto(message, -key);
+        return encryptor.crypto(message, SIZE_ALPHABET-key);
     }
 
     public void sendMessage(Droid droid, String message, Integer key) {
-        String crypMessage = encryptMessage(message, key);
-        System.out.println(this.name + " отправил зашифрованное сообщение: " + crypMessage);
-        droid.receiveMessage(crypMessage, key);
+        String cryptedMessage = encryptMessage(message, key);
+        System.out.println(this.name + " отправил зашифрованное сообщение: " + cryptedMessage);
+        droid.receiveMessage(cryptedMessage, key);
     }
 
     public void receiveMessage(String message, Integer key) {
-        String decryptMessage = decryptMessage(message, key);
-        System.out.println(this.name + " получил расшифрованное сообщение: " + decryptMessage);
+        String decryptedMessage = decryptMessage(message, key);
+        System.out.println(this.name + " получил расшифрованное сообщение: " + decryptedMessage);
     }
 
 }
