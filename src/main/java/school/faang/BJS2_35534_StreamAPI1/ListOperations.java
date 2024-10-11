5d4930c19bb416a60324bfb9ef4a7e489b9d9601
Maxim Gorobets <gorobets.maks@bk.ru>
2024-10-11 21:24:29 +1000

    renaming

--- src/main/java/school/faang/BJS2_35534_StreamAPI1/ListOperations.java
+++ src/main/java/school/faang/BJS2_35534_StreamAPI1/ListOperations.java
@@ -31,27 +31,27 @@ public class ListOperations {
                 .orElseThrow(() -> new IllegalStateException("Unable to calculate average list value!"));
     }
 
-    public static int countStringsStartingWith(List<String> list, char ch) {
-        ValidationUtils.validateNotNull(list);
+    public static int countStringsStartingWith(List<String> wordsList, char firstChar) {
+        ValidationUtils.validateNotNull(wordsList);
 
-        return (int)list.stream()
-                .filter(string -> string.startsWith(String.valueOf(ch)))
+        return (int)wordsList.stream()
+                .filter(string -> string.startsWith(String.valueOf(firstChar)))
                 .count();
     }
 
-    public static List<String> stringsContainingSubstring(List<String> list, String substring) {
-        ValidationUtils.validateNotNull(list);
+    public static List<String> stringsContainingSubstring(List<String> wordsList, String substring) {
+        ValidationUtils.validateNotNull(wordsList);
         ValidationUtils.validateString(substring, "Substring can't be null or blank!");
 
-        return list.stream()
+        return wordsList.stream()
                 .filter(string -> string.contains(substring))
                 .toList();
     }
 
-    public static List<String> sortByLength(List<String> list) {
-        ValidationUtils.validateNotNull(list);
+    public static List<String> sortByLength(List<String> wordsList) {
+        ValidationUtils.validateNotNull(wordsList);
 
-        return list.stream()
+        return wordsList.stream()
                 .sorted(Comparator.comparingInt(String::length))
                 .toList();
     }
@@ -63,19 +63,19 @@ public class ListOperations {
                 .allMatch(condition);
     }
 
-    public static int findMinGreaterThan(List<Integer> numbers, int ceiling) {
+    public static int findMinGreaterThan(List<Integer> numbers, int lowerBound) {
         ValidationUtils.validateNotNullOrEmpty(numbers);
 
         return numbers.stream()
-                .filter(number -> number > ceiling)
+                .filter(number -> number > lowerBound)
                 .min(Comparator.naturalOrder())
                 .orElseThrow(() -> new NoSuchElementException("List does not contain any valid value for given condition"));
     }
 
-    public static List<Integer> getStringsLengths(List<String> list) {
-        ValidationUtils.validateNotNull(list);
+    public static List<Integer> getStringsLengths(List<String> wordsList) {
+        ValidationUtils.validateNotNull(wordsList);
 
-        return list.stream()
+        return wordsList.stream()
                 .map(String::length)
                 .toList();
     }
