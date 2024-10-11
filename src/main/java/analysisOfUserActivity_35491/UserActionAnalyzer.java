0d3539a2a7e754b85414872d8be12a1846208027
AlekseyB86nb <tigrxam@gmail.com>
2024-10-12 02:57:15 +0300

    код стайл

--- src/main/java/analysisOfUserActivity_35491/UserActionAnalyzer.java
+++ src/main/java/analysisOfUserActivity_35491/UserActionAnalyzer.java
@@ -1,24 +1,12 @@
 package analysisOfUserActivity_35491;
 
 import java.time.LocalDate;
-import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 import java.util.Map;
-import java.util.Optional;
-import java.util.stream.Collector;
 import java.util.stream.Collectors;
 
 public class UserActionAnalyzer {
-
-//    new UserAction(1, "Alice", "post",LocalDate.of(2024, 9, 1), "Check out this amazing #newfeature!"),
-//    new UserAction(2, "Bob", "comment", LocalDate.of(2024, 9, 2), "I totally agree with #newfeature."),
-//    new UserAction(1, "Alice", "like", LocalDate.of(2024, 9, 3), ""),
-//    new UserAction(3, "Charlie", "share", LocalDate.of(2024, 9, 4), ""),
-//    new UserAction(4, "Dave", "post", LocalDate.of(2024, 9, 5), "#Java is awesome!"),
-//    new UserAction(2, "Bob", "like", LocalDate.of(2024, 9, 6), ""),
-
-
     public static List<String> topActiveUsers(List<UserAction> actions, int limit) {
         return actions.stream()
                 .collect(Collectors.groupingBy(UserAction::getName, Collectors.counting()))
