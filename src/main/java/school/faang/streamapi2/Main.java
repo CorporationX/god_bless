7376bb3142a3de3941863607ed12c1b51ecdf7c6
heckfy86 <marlinco263@gmail.com>
2024-10-11 09:48:18 +0200

    BJS2-35164

--- src/main/java/school/faang/streamapi2/Main.java
+++ src/main/java/school/faang/streamapi2/Main.java
@@ -8,17 +8,22 @@ import java.util.Map;
 public class Main {
     public static void main(String[] args) {
         List<Integer> numbers = Arrays.asList(1, 2, 3, 4, 5, 6);
+        List<String> stringList1 = Arrays.asList("apple", "banana", "avocado", "apricot");
+        List<String> stringList2 = Arrays.asList("apple", "banana", "cherry", "date", "fig", "grape");
 
-        Map<String, String> map = new HashMap<>();
-        map.put("Russia", "Moscow");
-        map.put("USA", "Washington");
-        map.put("Germany", "Berlin");
-
+        Map<String, String> mapCapital = new HashMap<>();
+        mapCapital.put("Russia", "Moscow");
+        mapCapital.put("USA", "Washington");
+        mapCapital.put("Germany", "Berlin");
 
 
         StreamApi.findUniqPairs(numbers, 7).forEach(pair -> System.out.print(Arrays.toString(pair)));
         System.out.println();
+        System.out.println(StreamApi.sortMapByStream(mapCapital));
+        System.out.println(StreamApi.sortAndFilter(stringList1, 'a'));
         System.out.println(StreamApi.dexToBinary(numbers));
+        System.out.println(StreamApi.checkString(stringList2, "abcdefghijklmnopqrstuvwxyz"));
+
 
     }
 }
