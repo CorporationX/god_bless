51a73ee4005e0d4be24f70821321029b90d54456
UrmatBolotbek <urmar9797@gmail.com>
2024-10-11 21:26:42 +0200

    complete the task "Gmail Rich Filters"  BJS2-33663

---
+++ src/main/java/school/faang/BJS2_33663/EmailProcessor.java
@@ -0,0 +1,16 @@
+package school.faang.BJS2_33663;
+
+import java.util.List;
+import java.util.function.Consumer;
+import java.util.function.Function;
+import java.util.function.Predicate;
+
+public class EmailProcessor {
+    void processEmails(List<Email> emails, Predicate<Email> filter, Consumer<Email> handler, Function<Email, String> transformer) {
+        emails.stream().filter(filter).forEach(email -> {
+            String transformedBody = transformer.apply(email);
+            email.setBody(transformedBody);
+            handler.accept(email);
+        });
+    }
+}
