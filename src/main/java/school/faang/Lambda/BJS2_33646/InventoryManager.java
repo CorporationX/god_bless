467f6884902f04acec5661ccc69f5c69b7e3f4d9
0Miha0 <140548440+0Miha0@users.noreply.github.com>
2024-10-11 10:35:52 +0500

    Исправил косяки, улучшил метод updateItem https://faang-school.atlassian.net/browse/BJS2-35486

--- src/main/java/school/faang/Lambda/BJS2_33646/InventoryManager.java
+++ src/main/java/school/faang/Lambda/BJS2_33646/InventoryManager.java
@@ -4,28 +4,18 @@ import java.util.function.Consumer;
 import java.util.function.Predicate;
 
 public class InventoryManager {
-    private Character character;
-
     public void addItem(Character character, Item item, Consumer<Item> consumer) {
         character.getInventory().add(item);
         consumer.accept(item);
     }
 
     public void removeItem(Character character, Predicate<Item> predicate) {
-        character.getInventory().removeIf(item -> {
-            if (predicate.test(item)) {
-                System.out.println(item.getName() + " выброшен");
-                return true;
-            }
-            return false;
-        });
+        character.getInventory().removeIf(predicate);
     }
 
     public void updateItem(Character character, Predicate<Item> predicate, Consumer<Item> updater) {
-        for (Item item : character.getInventory()) {
-            if (predicate.test(item)) {
-                updater.accept(item);
-            }
-        }
+        character.getInventory().stream()
+                .filter(predicate)
+                .forEach(updater);
     }
 }
--- src/main/java/school/faang/Lambda/BJS2_33646/Main.java
+++ src/main/java/school/faang/Lambda/BJS2_33646/Main.java
@@ -1,40 +1,34 @@
 package school.faang.Lambda.BJS2_33646;
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.function.Consumer;
+import java.util.function.Predicate;
+
 public class Main {
     public static void main(String[] args) {
         InventoryManager inventoryManager = new InventoryManager();
+
         Character frodo = new Character("Frodo");
-        Item sword = new Item("Sword", 100);
-        Item shield = new Item("Shield", 150);
-        Item bow = new Item("Bow", 120);
-        Item stone = new Item("Stone", 0);
-        Item cloak = new Item("Elven Cloak", 200);
-        Item stick = new Item("Stick", 0);
+        List<Item> items = Arrays.asList(
+                new Item("Sword", 100),
+                new Item("Shield", 150),
+                new Item("Bow", 120),
+                new Item("Stone", 0),
+                new Item("Elven Cloak", 200),
+                new Item("Stick", 0)
+        );
 
-        inventoryManager.addItem(frodo, sword,
-                (item) -> System.out.println(item.getName() + " был добавлен в инвентарь."));
-        inventoryManager.addItem(frodo, shield,
-                (item) -> System.out.println(item.getName() + " был добавлен в инвентарь."));
-        inventoryManager.addItem(frodo, bow,
-                (item) -> System.out.println(item.getName() + " был добавлен в инвентарь."));
-        inventoryManager.addItem(frodo, stone,
-                (item) -> System.out.println(item.getName() + " был добавлен в инвентарь."));
-        inventoryManager.addItem(frodo, cloak,
-                (item) -> System.out.println(item.getName() + " был добавлен в инвентарь."));
-        inventoryManager.addItem(frodo, stick,
-                (item) -> System.out.println(item.getName() + " был добавлен в инвентарь."));
+        Consumer<Item> addItemConsumer = item -> System.out.println(item.getName() + " был добавлен в инвентарь.");
+        items.forEach(item -> inventoryManager.addItem(frodo, item, addItemConsumer));
 
-        inventoryManager.removeItem(frodo,
-                (item) -> item.getValue() == 0);
-        inventoryManager.removeItem(frodo,
-                (item) -> item.getValue() == 0);
-        inventoryManager.removeItem(frodo,
-                (item) -> item.getName().equals("Bow"));
+        Predicate<Item> zeroValuePredicate = item -> item.getValue() == 0;
+        Predicate<Item> bowPredicate = item -> "Bow".equals(item.getName());
+        inventoryManager.removeItem(frodo, zeroValuePredicate.or(bowPredicate));
 
         inventoryManager.updateItem(frodo,
-                (item) -> item.getName().equals("Shield"),
-                (item) -> item.setValue(item.getValue() * 2));
-
+                item -> "Shield".equals(item.getName()),
+                item -> item.setValue(item.getValue() * 2));
 
         frodo.getInventory().forEach(item -> System.out.println(item.getName() + ": " + item.getValue()));
     }
