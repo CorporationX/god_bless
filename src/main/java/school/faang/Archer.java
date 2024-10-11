f2f11f19d96fbc4c0a2a957625163f0dba6409e6
Stanislaav Kozh <printks@yandex.ru>
2024-10-11 22:05:09 +0300

    Абстракция, абстракция!

---
+++ src/main/java/school/faang/Archer.java
@@ -0,0 +1,29 @@
+package school.faang;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class Archer extends Character {
+
+    public Archer(String name) {
+        super(name, 3, 10, 5);
+    }
+
+    public Archer(String name, int power, int agility, int intelligence) {
+        super(name, power, agility, intelligence);
+    }
+
+    @Override
+    public void attack(Character enemy) {
+        int health = enemy.getHealth()- this.getAgility();
+
+        enemy.setHealth(Math.max(health, 0));
+
+        enemy.setHealth(enemy.getHealth() - this.getAgility());
+        System.out.println(this.getName() + " attacks " + enemy.getName() + " and deals " + this.getAgility() + " damage.");
+    }
+
+}
+
