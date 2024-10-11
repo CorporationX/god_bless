6e19638b602f6d5456494c9d76bc47fb632d4621
abzalkhan00 <abzalkhanondakanov@gmail.com>
2024-10-11 12:36:15 +0500

    created two classes for Google search task

---
+++ src/main/java/school/faang/googleSearch/Main.java
@@ -0,0 +1,14 @@
+package school.faang.googleSearch;
+
+import java.util.HashMap;
+import java.util.List;
+
+public class Main {
+    private static final HashMap<String, List<WebPage>> SIMILAR_PAGES = new HashMap<>();
+
+
+
+    public static void main(String[] args) {
+
+    }
+}
