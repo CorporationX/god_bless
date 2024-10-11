052cde065d4e8f2c79a428cbc3013c6b4e27444f
AlekseyB86nb <tigrxam@gmail.com>
2024-10-11 22:06:37 +0300

    тренеровка Stream API 1

--- src/main/java/practiceStreamAPI1_35624/ListOperations.java
+++ src/main/java/practiceStreamAPI1_35624/ListOperations.java
@@ -5,7 +5,6 @@ import lombok.NonNull;
 import java.util.Comparator;
 import java.util.List;
 import java.util.NoSuchElementException;
-import java.util.Optional;
 import java.util.function.Predicate;
 
 public class ListOperations {
