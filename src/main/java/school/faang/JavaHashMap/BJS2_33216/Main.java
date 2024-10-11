16919ead1324166fbe2ea3f6df7dfa04be2486c9
0Miha0 <140548440+0Miha0@users.noreply.github.com>
2024-10-11 13:23:24 +0500

    Исправил косяки https://faang-school.atlassian.net/browse/BJS2-33466

--- src/main/java/school/faang/JavaHashMap/BJS2_33216/Main.java
+++ src/main/java/school/faang/JavaHashMap/BJS2_33216/Main.java
@@ -8,6 +8,7 @@ import java.util.Map;
 public class Main {
     static Map<Integer, StreamEvent> events = new HashMap<>();
     static Map<String, List<StreamEvent>> eventData = new HashMap<>();
+
     public static void main(String[] args) {
         addEvent(1, "Регистрация", "Пользователь зарегистрировался на курсе по Java.");
         addEvent(2, "Начало урока", "Пользователь начал урок по объектно-ориентированному программированию.");
@@ -47,17 +48,13 @@ public class Main {
         System.out.println("\n----------------------------------------------------------------------------------------------");
 
         allEvents();
-
-
     }
 
     private static void addEvent(int id, String eventType, String data) {
         StreamEvent event = new StreamEvent(id, eventType, data);
         events.put(id, event);
-        eventData.putIfAbsent(eventType, new ArrayList<>());
-        eventData.get(eventType).add(event);
+        eventData.putIfAbsent(eventType, new ArrayList<>()).add(event);
     }
-
 
     private static void searchEventById(int id) {
         StreamEvent event = events.get(id);
@@ -65,21 +62,20 @@ public class Main {
             System.out.println("\n События с таким id нет :" + id);
         } else {
             System.out.println("\n Id события: " + event.getId() +
-                               "\n Тип события: " + event.getEventType() +
-                               "\n Данные события: " + event.getData());
+                    "\n Тип события: " + event.getEventType() +
+                    "\n Данные события: " + event.getData());
         }
     }
 
-
-    private static void searchEventByEventType(String eventType){
+    private static void searchEventByEventType(String eventType) {
         List<StreamEvent> streamEventList = eventData.get(eventType);
         if (streamEventList == null || streamEventList.isEmpty()) {
             System.out.println("\n Списка событий с таким типом нет: " + eventType);
         } else {
-            System.out.println(" Список событий событий с типом: " +eventType);
+            System.out.println(" Список событий событий с типом: " + eventType);
             for (StreamEvent streamEvent : streamEventList) {
                 System.out.println("\n Id события: " + streamEvent.getId() +
-                                   "\n Данные события: " + streamEvent.getData());
+                        "\n Данные события: " + streamEvent.getData());
             }
         }
     }
@@ -92,10 +88,10 @@ public class Main {
             List<StreamEvent> streamEventList = eventData.get(eventType);
             if (streamEventList != null) {
                 streamEventList.remove(event);
-                System.out.println("Событие: " + eventType + " с id: " + id + " Удалено"  );
+                System.out.println("Событие: " + eventType + " с id: " + id + " Удалено");
                 if (streamEventList.isEmpty()) {
                     eventData.remove(eventType);
-                    System.out.println("Список событий: " + eventType + " Удален"  );
+                    System.out.println("Список событий: " + eventType + " Удален");
                 }
             }
         } else {
@@ -103,8 +99,7 @@ public class Main {
         }
     }
 
-
-    private static void allEvents(){
+    private static void allEvents() {
         for (Map.Entry<String, List<StreamEvent>> entry : eventData.entrySet()) {
             System.out.println("\nТип события: " + entry.getKey());
             for (StreamEvent event : entry.getValue()) {
@@ -113,5 +108,4 @@ public class Main {
             }
         }
     }
-
 }
