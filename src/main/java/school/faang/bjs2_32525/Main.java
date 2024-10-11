cb6c9fcda9608936a49e541847631244980b5558
Shap|Misha <maxim.ivanov.jr@gmail.com>
2024-10-12 00:11:13 +0300

    added method: removeStudentWithGradesBySubject and validateRemoveStudentWithGradesBySubject for StudentManager,testRemoveStudentWithGradesBySubject for Main

--- src/main/java/school/faang/bjs2_32525/Main.java
+++ src/main/java/school/faang/bjs2_32525/Main.java
@@ -11,9 +11,12 @@ public class Main {
     public static void main(String[] args) {
         Map<Student, Map<Subject, Integer>> studentsSubjectsGrades = new HashMap<>();
         Map<Subject, List<Student>> studentsBySubject = new HashMap<>();
+
         Student student = new Student("Alex");
         testAddStudentWithGradesBySubjects(student, studentsSubjectsGrades);
         testAddSubjectWithGradesToStudent(student, studentsSubjectsGrades);
+        testRemoveStudentWithGradesBySubject(student,studentsSubjectsGrades);
+
     }
 
     private static void testAddStudentWithGradesBySubjects(Student student, Map<Student, Map<Subject, Integer>> studentsWithGradesBySubjects) {
@@ -49,6 +52,19 @@ public class Main {
 
         System.out.println("add a student Subject - " + physics.getName() + ", grade - " + gradePhysicalEducation);
         studentManager.addSubjectWithGradesToStudent(student, physics, gradePhysicalEducation, studentsWithGradesBySubjects);
+
+        System.out.println("\nstudents with grades by subjects after:\n");
+        printStudentsWithGradesBySubjects(studentsWithGradesBySubjects);
+    }
+
+    private static void testRemoveStudentWithGradesBySubject(Student student, Map<Student, Map<Subject, Integer>> studentsWithGradesBySubjects) {
+        System.out.println("\ntestRemoveStudentWithGradesBySubject\n");
+
+        System.out.println("students with grades by subjects before:\n");
+        printStudentsWithGradesBySubjects(studentsWithGradesBySubjects);
+
+        System.out.println("remove student with grades by subject: " + student);
+        studentManager.removeStudentWithGradesBySubject(student, studentsWithGradesBySubjects);
 
         System.out.println("\nstudents with grades by subjects after:\n");
         printStudentsWithGradesBySubjects(studentsWithGradesBySubjects);
--- src/main/java/school/faang/bjs2_32525/StudentManager.java
+++ src/main/java/school/faang/bjs2_32525/StudentManager.java
@@ -20,11 +20,29 @@ public class StudentManager {
         subjectIntegerMap.put(subject, grade);
     }
 
+    public void removeStudentWithGradesBySubject(Student student, Map<Student, Map<Subject, Integer>> studentsWithGradesBySubjects) {
+        validateRemoveStudentWithGradesBySubject(student, studentsWithGradesBySubjects);
+        studentsWithGradesBySubjects.remove(student);
+    }
+
+    private static void validateRemoveStudentWithGradesBySubject(Student student, Map<Student, Map<Subject, Integer>> studentsWithGradesBySubjects) {
+        if (student == null) {
+            throw new IllegalArgumentException("student is null");
+        }
+        if (studentsWithGradesBySubjects == null) {
+            throw new IllegalArgumentException("students with grades by subjects map is null");
+        }
+
+    }
+
     private static void validateAddSubjectWithGradesToStudent(Student student, Subject subject, int grade, Map<Student, Map<Subject, Integer>> studentsWithGradesBySubjects) {
         if (student == null) {
             throw new IllegalArgumentException("student is null");
         }
-        if(!(studentsWithGradesBySubjects.containsKey(student))){
+        if (studentsWithGradesBySubjects == null) {
+            throw new IllegalArgumentException("students with grades by subjects map is null");
+        }
+        if (!(studentsWithGradesBySubjects.containsKey(student))) {
             throw new NoSuchElementException("there is no such student in the map yet");
         }
         if (subject == null) {
@@ -38,9 +56,7 @@ public class StudentManager {
         if (grade > maxGrade) {
             throw new IllegalArgumentException("grade cannot be higher than the maximum score = " + maxGrade);
         }
-        if (studentsWithGradesBySubjects == null) {
-            throw new IllegalArgumentException("students with grades by subjects map is null");
-        }
+
     }
 
     private static void validateArgsMethodAddStudentWithGradesBySubjects(Student student, Map<Subject, Integer> gradesBySubjects, Map<Student, Map<Subject, Integer>> studentsWithGradesBySubjects) {
