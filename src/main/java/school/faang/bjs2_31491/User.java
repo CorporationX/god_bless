5b59deceb86efa46d26faf7e9812dc5b8f98b556
“Andrey <aiegorovceo@yandex.ru>
2024-10-11 15:35:57 +0300

    Changes indents

--- src/main/java/school/faang/bjs2_31491/User.java
+++ src/main/java/school/faang/bjs2_31491/User.java
@@ -9,18 +9,17 @@ import java.util.Map;
 
 @Getter
 class User {
-	private String name;
-	private int age;
-	private String workplace;
-	private String address;
+    private String name;
+    private int age;
+    private String workplace;
+    private String address;
 
-	public User(String name, int age, String workplace, String address) {
-		this.name = name;
-		this.age = age;
-		this.workplace = workplace;
-		this.address = address;
+    public User(String name, int age, String workplace, String address) {
+        this.name = name;
+        this.age = age;
+        this.workplace = workplace;
+        this.address = address;
 	}
-
 	public static Map<Integer, List<User>> groupUsers(List<User> users) {
 		Map<Integer, List<User>> groupedUsers = new HashMap<>();
 		for (User user : users) {
