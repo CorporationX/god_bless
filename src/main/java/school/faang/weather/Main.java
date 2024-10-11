093ecb6f514639da386ef1787c796be3840a3df0
Memoreks <akifyevalex1@gmail.com>
2024-10-11 21:29:04 +0700

    Внесены правки согласно комментариям

--- src/main/java/school/faang/weather/Main.java
+++ src/main/java/school/faang/weather/Main.java
@@ -1,26 +1,26 @@
 package school.faang.weather;
 
 import java.util.HashMap;
+import java.util.Random;
 
 public class Main {
     private static HashMap<String, WeatherData> weatherInfo = new HashMap<>();
+    private static final Integer MAX_TEMP = 50;
+    private static final Integer MAX_HUMIDITY = 100;
 
-    public static WeatherData weatherInCity(String city) {
-        if(weatherInfo.containsKey(city)) {
-            return weatherInfo.get(city);
-        } else {
-            return queryInDb(city);
-        }
+    public static WeatherData getWeatherInCity(String city) {
+        return weatherInfo.containsKey(city) ? weatherInfo.get(city) : queryInDb(city);
     }
 
     public static WeatherData queryInDb(String city) {
-        WeatherData weatherData = new WeatherData(city, (double) Math.random() * 10, (double) Math.random()*100);
+        Random rnd = new Random();
+        WeatherData weatherData = new WeatherData(city, rnd.nextDouble(MAX_TEMP), rnd.nextDouble(MAX_HUMIDITY));
         weatherInfo.put(city, weatherData);
         return weatherData;
     }
 
     public static void updateWeatherInfo(String city, Double temp, Double humidity) {
-        WeatherData weather = weatherInCity(city);
+        WeatherData weather = getWeatherInCity(city);
         weather.setTemperature(temp);
         weather.setHumidity(humidity);
         weatherInfo.remove(city);
@@ -28,21 +28,19 @@ public class Main {
     }
 
     public static void deleteWeatherInfo(String city) {
-        if(weatherInfo.containsKey(city)) {
-            weatherInfo.remove(city);
-        }
+        weatherInfo.remove(city);
     }
 
     public static void displayWeatherInfoForAllCity() {
         weatherInfo.values().forEach(System.out::println);
     }
     public static void main(String[] args) {
-        System.out.println(weatherInCity("Tomsk"));
+        System.out.println(getWeatherInCity("Tomsk"));
         updateWeatherInfo("Tomsk", 20.0, 40.5);
-        System.out.println(weatherInCity("Omsk"));
-        System.out.println(weatherInCity("Novosibirsk"));
-        System.out.println(weatherInCity("Moscow"));
-        System.out.println(weatherInCity("Tomsk"));
+        System.out.println(getWeatherInCity("Omsk"));
+        System.out.println(getWeatherInCity("Novosibirsk"));
+        System.out.println(getWeatherInCity("Moscow"));
+        System.out.println(getWeatherInCity("Tomsk"));
         deleteWeatherInfo("Moscow");
         displayWeatherInfoForAllCity();
     }
