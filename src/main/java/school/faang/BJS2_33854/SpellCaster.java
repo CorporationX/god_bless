8d0c0f84a66283e9ae81268c63ba0c55365138d7
UrmatBolotbek <urmar9797@gmail.com>
2024-10-11 13:31:51 +0200

    complete the task "Хогвартс, Хогвартс, наш любимый Хогвартс"  BJS2-33854

---
+++ src/main/java/school/faang/BJS2_33854/SpellCaster.java
@@ -0,0 +1,7 @@
+package school.faang.BJS2_33854;
+
+public class SpellCaster {
+    void cast(String spell, SpellAction spellAction) {
+        System.out.println(spellAction.castSpell(spell));
+    }
+}
