9c4528ad27b83a24dff7ab628432b717a2c5a588
Gadzhiev Gadzhi <Gadzhiev3222@gmail.com>
2024-10-12 12:38:06 +0300

    Анализ активности пользователей в социальной сети

---
+++ src/main/java/school/faang/analisisuseractivity/maincode/UserAction.java
@@ -0,0 +1,17 @@
+package school.faang.analisisuseractivity.maincode;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+import java.time.LocalDate;
+
+@Data
+@AllArgsConstructor
+public class UserAction {
+    private int id;
+    private String name;
+    private String actionType;
+    private LocalDate actionDate;
+    private String content;
+}
