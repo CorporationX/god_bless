1e1edc5f7739b313a1cac315bc665b53a9f86508
Gadzhiev Gadzhi <Gadzhiev3222@gmail.com>
2024-10-12 08:14:40 +0300

    train stream api 2

--- src/main/java/school/faang/trainstreamapi2/maincode/StreamApi.java
+++ src/main/java/school/faang/trainstreamapi2/maincode/StreamApi.java
@@ -1,6 +1,46 @@
 package school.faang.trainstreamapi2.maincode;
 
+import java.util.*;
+import java.util.stream.Collectors;
 
 public class StreamApi {
+    public static Set<List<Integer>> findPairs(List<Integer> numbers, int sum) {
+        Set<List<Integer>> pairs = new HashSet<>();
+        Set<Integer> set = new HashSet<>(numbers);
 
+        pairs = numbers.stream()
+                .filter(num -> set.contains(sum - num))
+                .map(num -> Arrays.asList(num, sum - num))
+                .peek(Collections::sort)
+                .collect(Collectors.toSet());
+        return pairs;
+    }
+
+    public static List<String> getSortedCapitails(Map<String, String> countries) {
+        return countries.entrySet().stream()
+                .sorted(Map.Entry.comparingByKey())
+                .map(Map.Entry::getValue)
+                .toList();
+    }
+
+    public static List<String> sortedAndFilter(List<String> lines, char symbol) {
+        return lines.stream()
+                .filter(s -> s.charAt(0) == symbol)
+                .sorted(Comparator.comparingInt(String::length))
+                .toList();
+    }
+
+    public static List<String> mapDecimalToBinary(List<Integer> numbers) {
+        return numbers.stream()
+                .map(Integer::toBinaryString)
+                .toList();
+    }
+
+    public static List<String> filterAndSortByLength(List<String> lines, String alphabet) {
+        String regex = "[" + alphabet + "]+";
+        return lines.stream()
+                .filter(s -> s.matches(regex))
+                .sorted(Comparator.comparingInt(String::length))
+                .toList();
+    }
 }
