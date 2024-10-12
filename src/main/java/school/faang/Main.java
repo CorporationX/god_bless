d4713fef9579bd883b5703f548d8ecd4ed520882
Matvey <mashptz@gmail.com>
2024-10-12 11:10:06 +0300

    Создание метода withErrorHandling
    Вместо Supllier создал новый функциональный интерфейс ThrowingSupplier
    , так как Supllier не выбрасывал проверяемое исключение

--- src/main/java/school/faang/Main.java
+++ src/main/java/school/faang/Main.java
@@ -1,4 +1,23 @@
 package school.faang;
 
 public class Main {
+    public static void main(String[] args) {
+        String result = ErrorHandler.withErrorHandling(
+                () -> RemoteService.call("someParam"),
+                e -> {  // Обработка ошибки
+                    System.out.println("Ошибка при вызове сервиса, возвращаем дефолтное значение");
+                    return "DEFAULT";
+                }
+        );
+
+        System.out.println(result);
+    }
+
+    public static class RemoteService {
+        public static String call(String param) throws Exception {
+            throw new Exception("Сервис недоступен");
+        }
+    }
 }
+
+
