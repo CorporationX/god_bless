66c5c07749a664970a63aaa77199fda7e8d997e6
Anatoly Galay <galay007@mail.ru>
2024-10-12 12:45:22 +0300

    need help with remove if

--- src/main/java/school/faang/google/Main.java
+++ src/main/java/school/faang/google/Main.java
@@ -1,10 +1,10 @@
 package school.faang.google;
 
 import java.util.*;
+import java.util.function.Predicate;
 
 public class Main {
-    static Map<String, List<WebPage>> indexWebPagesMap = new HashMap<>();
-    static Map<String, WebPage> urlMap = new HashMap<>();
+    static Map<String, List<WebPage>> indexWebPages = new HashMap<>();
 
     public static void main(String[] args) {
 
@@ -12,38 +12,33 @@ public class Main {
         addWebPageToIndex(new WebPage("travel.ru", "travel", "Hello Mr"));
         addWebPageToIndex(new WebPage("google.com", "test", "Hello World"));
         addWebPageToIndex(new WebPage("instagram.ru", "test", "Hello Again"));
-        deleteIndexByUrl("travel.ru");
-        deleteIndexByUrl("instagram.ru");
+        delete("travel.ru");
+        delete("instagram.ru");
     }
 
     public static void addWebPageToIndex(WebPage webPage) {
 
-        String[] contentList = webPage.getContent().split("\\s");
+        String[] contentList = webPage.content().split("\\s");
 
         for (String word : contentList) {
-            List<WebPage> indexedWebPages = indexWebPagesMap.computeIfAbsent(word, x -> new LinkedList<WebPage>());
+            List<WebPage> indexedWebPages = indexWebPages.computeIfAbsent(word.toLowerCase(), x -> new LinkedList<WebPage>());
 
             if (!indexedWebPages.contains(webPage)) {
                 indexedWebPages.add(webPage);
-
-                urlMap.computeIfAbsent(webPage.getUrl(), x -> new WebPage(webPage.getUrl(), webPage.getTitle(),webPage.getContent()));
             }
         }
     }
 
-    public static List<WebPage> searchByIndex(String index) {
-        return indexWebPagesMap.get(index);
+    public static List<WebPage> search(String index) {
+        return indexWebPages.get(index.toLowerCase());
     }
 
-    public static void deleteIndexByUrl(String url) {
-        WebPage toDeleteWP = urlMap.get(url);
-        String[] toDeleteWordsContent = toDeleteWP.getContent().split("\\s");
+    public static void delete(String url) {
+        Predicate<WebPage> condition = web -> web.url().contains(url);
 
-        for (String word : toDeleteWordsContent) {
-            if (indexWebPagesMap.get(word).contains(toDeleteWP)) {
-                indexWebPagesMap.get(word).remove(toDeleteWP);
-            }
+        for (Map.Entry<String, List<WebPage>> entry : indexWebPages.entrySet()) {
+            entry.getValue().removeIf(condition);
         }
-        urlMap.remove(url);
     }
+
 }
--- src/main/java/school/faang/google/WebPage.java
+++ src/main/java/school/faang/google/WebPage.java
@@ -1,19 +1,5 @@
 package school.faang.google;
 
 
-import lombok.EqualsAndHashCode;
-import lombok.Getter;
 
-@Getter
-@EqualsAndHashCode
-public class WebPage {
-    private String url;
-    private String title;
-    private String content;
-
-    public WebPage(String url, String title, String content) {
-        this.url = url;
-        this.title = title;
-        this.content = content;
-    }
-}
+public record WebPage(String url, String title, String content) {}
