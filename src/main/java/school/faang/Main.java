f18eed02197eb91e0d85948b19a6ea39b8685135
lomuserg <sergeymu2015@yandex.ru>
2024-10-12 14:03:39 +0300

    init

--- src/main/java/school/faang/Main.java
+++ src/main/java/school/faang/Main.java
@@ -1,6 +1,12 @@
 package school.faang;
 
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 public class Main {
+
+    private static Map<String, List<WebPage>> wabPagesWithKey = new HashMap<>();
 
     public static void main(String[] args) {
 
---
+++ src/main/java/school/faang/WebPage.java
@@ -0,0 +1,8 @@
+package school.faang;
+
+public class WebPage {
+    private String url;
+    private String title;
+    private String content;
+
+}
