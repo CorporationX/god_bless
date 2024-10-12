1a086df4660c5ce12f936d824f3b57ab8fbd1455
ZhubanyshZh <zubanyszarylkasyn@gmail.com>
2024-10-13 01:14:06 +0500

    fix(method): clean code and fix method encrypt and decrypt
    separated similar lines of method code into a separate method to maintain DRY

--- src/main/java/org/example/model/droidSecrets/Droid.java
+++ src/main/java/org/example/model/droidSecrets/Droid.java
@@ -13,36 +13,36 @@ public class Droid {
     private static final int sizeOfTheEnglishLetter = 26;
     private String name;
 
-    public static String encryptMessage(String message, Integer key) {
+    private static String processMessage(String message, Integer key, boolean isEncrypting) {
         String result = "";
-        for(char messagePart : message.toCharArray()){
-            if(Character.isLetter(messagePart)){
-                if(messagePart >= startOfTheUpperCaseLetter && messagePart <= endOfTheUpperCaseLatter){
-                    result += (char) ((messagePart - startOfTheUpperCaseLetter + key) % sizeOfTheEnglishLetter + startOfTheUpperCaseLetter);
-                }else{
-                    result += (char) ((messagePart - startOfTheLowerCaseLetter + key) % sizeOfTheEnglishLetter + startOfTheLowerCaseLetter);
+        for (char messagePart : message.toCharArray()) {
+            if (Character.isLetter(messagePart)) {
+                if (messagePart >= startOfTheUpperCaseLetter && messagePart <= endOfTheUpperCaseLatter) {
+                    if (isEncrypting) {
+                        result += (char) ((messagePart - startOfTheUpperCaseLetter + key) % sizeOfTheEnglishLetter + startOfTheUpperCaseLetter);
+                    } else {
+                        result += (char) (endOfTheUpperCaseLatter - (endOfTheUpperCaseLatter - messagePart + key) % sizeOfTheEnglishLetter);
+                    }
+                } else if (messagePart >= startOfTheLowerCaseLetter && messagePart <= endOfTheLowerCaseLetter) {
+                    if (isEncrypting) {
+                        result += (char) ((messagePart - startOfTheLowerCaseLetter + key) % sizeOfTheEnglishLetter + startOfTheLowerCaseLetter);
+                    } else {
+                        result += (char) (endOfTheLowerCaseLetter - (endOfTheLowerCaseLetter - messagePart + key) % sizeOfTheEnglishLetter);
+                    }
                 }
-            }else{
+            } else {
                 result += messagePart;
             }
         }
         return result;
     }
 
+    public static String encryptMessage(String message, Integer key) {
+        return processMessage(message, key, true);
+    }
+
     public static String decryptMessage(String message, Integer key) {
-        String result = "";
-        for(char messagePart : message.toCharArray()){
-            if(Character.isLetter(messagePart)){
-                if(messagePart >= startOfTheUpperCaseLetter && messagePart <= endOfTheUpperCaseLatter){
-                    result += (char) (endOfTheUpperCaseLatter - (endOfTheUpperCaseLatter - messagePart + key) % sizeOfTheEnglishLetter);
-                }else{
-                    result += (char) (endOfTheLowerCaseLetter - (endOfTheLowerCaseLetter - messagePart + key) % sizeOfTheEnglishLetter);
-                }
-            }else{
-                result += messagePart;
-            }
-        }
-        return result;
+        return processMessage(message, key, false);
     }
 
     public void sendMessage(Droid droid, String message, Integer key, CaesarCipher encryptor) {
@@ -52,7 +52,7 @@ public class Droid {
         droid.receiveMessage(encryptedMessage, key, Droid::decryptMessage);
     }
 
-    public void receiveMessage(String encryptedMessage, Integer key,  CaesarCipher decryptor) {
+    public void receiveMessage(String encryptedMessage, Integer key, CaesarCipher decryptor) {
         String decryptedMessage = decryptor.applyCipher(encryptedMessage, key);
         System.out.println(this.getName() + " получил расшифрованное сообщение: " + decryptedMessage);
     }
