11cc55096d3389e3d084d06292fcd9246220b78e
AndreyGustilev <gustilev@rambler.ru>
2024-10-12 20:11:25 +0300

    done

---
+++ src/main/java/school/faang/BJS2_34904/Image.java
@@ -0,0 +1,13 @@
+package school.faang.BJS2_34904;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+public class Image {
+    private String Name;
+    private String Description;
+}
