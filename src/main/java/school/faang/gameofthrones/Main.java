c2f965e5e9db0797dbf4201493c4e43ea157b2cf
MunirIsmoilov <ismoilov.munir@gmail.com>
2024-10-12 14:39:24 +0300

    commit Game of thrones

---
+++ src/main/java/school/faang/gameofthrones/Main.java
@@ -0,0 +1,53 @@
+package school.faang.gameofthrones;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Main {
+    Map<String, House> houseMap = new HashMap<>();
+
+    public void addHouse(String name, String sigil) {
+        houseMap.put(name, new House(name, sigil));
+    }
+
+    public void removeHouse(String name) {
+        houseMap.remove(name);
+    }
+
+    public void findHouse(String name) {
+        House house = houseMap.get(name);
+        if (house != null) {
+            System.out.println(house);
+        } else
+            System.out.println("Дом не найден: " + name);
+    }
+
+    public void listHose() {
+        if (houseMap.isEmpty()) {
+            System.out.println("Нет доступных домов");
+        } else {
+            for (Map.Entry<String, House> entry : houseMap.entrySet()) {
+                System.out.println(entry.getValue());
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        Main main = new Main();
+        main.addHouse("Старк", "черный");
+        main.addHouse("Ланнистер", "красный");
+        main.addHouse("Баратеон", "белый");
+
+        System.out.println("Cписок всех домов");
+        main.listHose();
+
+        System.out.println("\nПоиск дом 'Старк' :");
+        main.findHouse("Старк");
+
+        System.out.println("\nУдаление дома 'Ланнистер'");
+        main.removeHouse("Ланнистер");
+
+        System.out.println("\nСписок всех дома после удаления");
+        main.listHose();
+    }
+}
