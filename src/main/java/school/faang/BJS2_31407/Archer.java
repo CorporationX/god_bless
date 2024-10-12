2aadb9aa5f65d63ff7f46c03b5786bad9fd6475d
Stanislaav Kozh <printks@yandex.ru>
2024-10-12 12:20:31 +0300

    fix  Абстракция, абстракция!

--- src/main/java/school/faang/BJS2_31407/Archer.java
+++ src/main/java/school/faang/BJS2_31407/Archer.java
@@ -1,4 +1,4 @@
-package school.faang;
+package school.faang.BJS2_31407;
 
 import lombok.Getter;
 import lombok.Setter;
@@ -11,17 +11,12 @@ public class Archer extends Character {
         super(name, 3, 10, 5);
     }
 
-    public Archer(String name, int power, int agility, int intelligence) {
-        super(name, power, agility, intelligence);
-    }
-
     @Override
     public void attack(Character enemy) {
         int health = enemy.getHealth()- this.getAgility();
 
         enemy.setHealth(Math.max(health, 0));
 
-        enemy.setHealth(enemy.getHealth() - this.getAgility());
         System.out.println(this.getName() + " attacks " + enemy.getName() + " and deals " + this.getAgility() + " damage.");
     }
 
--- src/main/java/school/faang/BJS2_31407/Warrior.java
+++ src/main/java/school/faang/BJS2_31407/Warrior.java
@@ -1,4 +1,4 @@
-package school.faang;
+package school.faang.BJS2_31407;
 
 import lombok.Getter;
 import lombok.Setter;
@@ -11,17 +11,12 @@ public class Warrior extends Character {
         super(name, 10, 5, 3);
     }
 
-    public Warrior(String name, int power, int agility, int intelligence) {
-        super(name, power, agility, intelligence);
-    }
-
     @Override
     public void attack(Character enemy) {
         int health = enemy.getHealth() - this.getPower();
 
         enemy.setHealth(Math.max(health, 0));
 
-        enemy.setHealth(enemy.getHealth() - this.getPower());
         System.out.println(this.getName() + " attacks " + enemy.getName() + " and deals " + this.getPower() + " damage.");
     }
 
