b52c0038ab50e8323358488802f570d4f8489c5b
Matvey <mashptz@gmail.com>
2024-10-12 09:34:30 +0300

    Создание класса InventoryMa Реализация методов добавления, удаления, апгрейда предметов.

--- src/main/java/school/faang/Main.java
+++ src/main/java/school/faang/Main.java
@@ -1,4 +1,47 @@
 package school.faang;
 
+import school.faang.invetory.Character;
+import school.faang.invetory.InventoryManager;
+import school.faang.invetory.Item;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
 public class Main {
+    public static void main(String[] args) {
+        Item soulOfAChampion = new Item("Soul of a Champion", 2500);
+        Item gotthardTwinswords = new Item("Gotthard Twinswords", 10000);
+        Item hollowslayerGreatsword = new Item("Hollowslayer Greatsword", 15000);
+
+        Character anri  = new Character("Anri", new ArrayList<>());
+        Character siegward = new Character("Siegward", new ArrayList<>(Arrays.asList(hollowslayerGreatsword)));
+        InventoryManager manager = new InventoryManager();
+
+
+        manager.addItem(
+                anri ,
+                soulOfAChampion,
+                (item) -> System.out.println(anri.getName() + " подобрал " + item.getName())
+        );
+        manager.addItem(
+                siegward ,
+                gotthardTwinswords,
+                (item) -> System.out.println(siegward.getName() + " подобрал " + item.getName())
+        );
+
+        manager.removeItem(
+                siegward ,
+                (item) -> item.getName().equals("Hollowslayer Greatsword")
+        );
+
+        manager.updateItem(
+                siegward ,
+                (item) -> item.getName().equals("Gotthard Twinswords"),
+                (item) -> new Item(item.getName(), item.getValue() * 5)
+        );
+
+        anri .getInventory().forEach(item -> System.out.println(anri.getName() + ":" + item.getName() + " : " + item.getValue()));
+        siegward .getInventory().forEach(item -> System.out.println(siegward.getName() + ":" + item.getName() + " : " + item.getValue()));
+    }
 }
+
---
+++ src/main/java/school/faang/invetory/Item.java
@@ -0,0 +1,14 @@
+package school.faang.invetory;
+
+import lombok.Data;
+
+@Data
+public class Item {
+    private String name;
+    private int value;
+
+    public Item(String name, int value) {
+        this.name = name;
+        this.value = value;
+    }
+}
