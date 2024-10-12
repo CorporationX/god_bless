cab2876a4e4026ea6c229779ec2d89a8779928a8
m1rroxxx <Nikita.sidorov.7@icloud.com>
2024-10-12 20:35:16 +0300

    заменил конкатинацию на форматирование строк в задаче тренируем stream api 1

--- src/main/java/school/faang/practiceTheStreamAPI1/Main.java
+++ src/main/java/school/faang/practiceTheStreamAPI1/Main.java
@@ -8,15 +8,14 @@ public class Main {
     List<Integer> numbers = Arrays.asList(1, 2, 3, 4, 5, 6);
     List<String> strings = Arrays.asList("apple", "banana", "cherry", "date");
 
-    // Пример использования методов:
-    System.out.println("Сумма четных чисел: " + ListOperations.sumOfEvenNumbers(numbers));
-    ListOperations.findMax(numbers).ifPresent(max -> System.out.println("Максимальное число: " + max));
-    System.out.println("Среднее: " + ListOperations.findAverage(numbers));
-    System.out.println("Количество строк, начинающихся на 'a': " + ListOperations.countStringsStartingWith(strings, 'a'));
-    System.out.println("Строки, содержащие 'an': " + ListOperations.filterStringsContainingSubstring(strings, "an"));
-    System.out.println("Отсортированные по длине строки: " + ListOperations.sortByLength(strings));
-    System.out.println("Все ли числа чётные? " + ListOperations.allMatchCondition(numbers, n -> n % 2 == 0));
-    ListOperations.findMinGreaterThan(numbers, 4).ifPresent(min -> System.out.println("Наименьшее число больше 4: " + min));
-    System.out.println("Длины строк: " + ListOperations.convertToLengths(strings));
+    System.out.printf("Сумма четных чисел: %d\n", ListOperations.sumOfEvenNumbers(numbers));
+    ListOperations.findMax(numbers).ifPresent(max -> System.out.printf("Максимальное число: %d\n", max));
+    System.out.printf("Среднее: %.1f\n", ListOperations.findAverage(numbers));
+    System.out.printf("Количество строк, начинающихся на 'a': %d\n", ListOperations.countStringsStartingWith(strings, 'a'));
+    System.out.printf("Строки, содержащие 'an': %s\n", ListOperations.filterStringsContainingSubstring(strings, "an"));
+    System.out.printf("Отсортированные по длине строки: %s\n", ListOperations.sortByLength(strings));
+    System.out.printf("Все ли числа чётные? %s\n", ListOperations.allMatchCondition(numbers, n -> n % 2 == 0));
+    ListOperations.findMinGreaterThan(numbers, 4).ifPresent(min -> System.out.printf("Наименьшее число больше 4: %d\n", min));
+    System.out.printf("Длины строк: %s\n", ListOperations.convertToLengths(strings));
   }
 }
