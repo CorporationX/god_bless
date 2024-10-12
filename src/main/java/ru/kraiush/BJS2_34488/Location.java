eee93df8e19c4eda315cd9fb1d7b9774bdd2caf7
Ekaterina Badratdinova <ekaterinafeshch@gmail.com>
2024-10-11 21:09:32 -0700

    Google Maps

---
+++ src/main/java/ru/kraiush/BJS2_34488/Location.java
@@ -0,0 +1,12 @@
+package ru.kraiush.BJS2_34488;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class Location {
+    private String name;
+    private double latitude;
+    private double longitude;
+}
