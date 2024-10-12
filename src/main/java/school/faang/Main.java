5f7bf6ad28122117f55c83e663e53468e15d15df
Matvey <mashptz@gmail.com>
2024-10-12 13:02:31 +0300

    Создание функционального интерфейса DroidMessageEncryptor Реализация методов для класса Droid

--- src/main/java/school/faang/Main.java
+++ src/main/java/school/faang/Main.java
@@ -1,7 +1,15 @@
 package school.faang;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
+        User firstUser = new User("John",22,"Bells","Google");
+        User secondUser = new User("John",22,"Bells","Google");
+        User thiedUser = new User("John",22,"Bells","Google");
+        User firstUser = new User("John",22,"Bells","Google");
 
+        List<User> users = new ArrayList<>();
     }
 }
