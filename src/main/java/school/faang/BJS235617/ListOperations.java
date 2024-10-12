fb14046a9499014c15f163e6557c5a22dcf79df8
Slava <leeteso97@gmail.com>
2024-10-12 19:38:04 +0500

    Взял с примера код и реализовал согласно ТЗ

---
+++ src/main/java/school/faang/BJS235617/ListOperations.java
@@ -0,0 +1,66 @@
+package school.faang.BJS235617;
+
+import java.util.Comparator;
+import java.util.List;
+import java.util.function.Predicate;
+
+public class ListOperations {
+    public static int sumOfEvenNumbers(List<Integer> numbers) {
+        return numbers.stream()
+                .filter(n -> n % 2 == 0)
+                .mapToInt(Integer::intValue)
+                .sum();
+    }
+
+    public static int findMax(List<Integer> numbers) {
+        return numbers.stream()
+                .max(Comparator.naturalOrder())
+                .orElse(0);
+    }
+
+    public static double findAverage(List<Integer> numbers) {
+        return numbers.stream()
+                .mapToInt(Integer::intValue)
+                .summaryStatistics()
+                .getAverage();
+    }
+
+    public static long countStringsStartingWith(List<String> strings, char letter) {
+        return strings.stream()
+                .filter(string -> string.startsWith(String.valueOf(letter)))
+                .count();
+
+    }
+
+    public static List<String> filterStringsContainingSubstring(List<String> strings, String substring) {
+        return strings.stream()
+                .filter(string -> string.contains(String.valueOf(substring)))
+                .toList();
+    }
+
+    public static List<String> sortByLength(List<String> strings) {
+        return strings.stream()
+                .sorted(Comparator.comparingInt(String::length))
+                .toList();
+
+    }
+
+    public static boolean allMatchCondition(List<Integer> numbers, Predicate<Integer> condition) {
+        return numbers.stream()
+                .allMatch(condition);
+
+    }
+
+    public static int findMinGreaterThan(List<Integer> numbers, int i) {
+        return numbers.stream()
+                .filter(s -> s > i)
+                .min(Comparator.naturalOrder())
+                .get();
+    }
+
+    public static List<Integer> convertToLengths(List<String> strings) {
+        return strings.stream()
+                .map(String::length)
+                .toList();
+    }
+}
