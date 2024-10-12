922b80d5afff04998b7b78a626d05202d5b286f3
Alexander Litovkin <litovkinalexan@gmail.com>
2024-10-12 14:32:46 +0700

    implement static methods

---
+++ src/main/java/school/faang/streamapione/StreamUtils.java
@@ -0,0 +1,68 @@
+package school.faang.streamapione;
+
+import lombok.experimental.UtilityClass;
+
+import java.util.Comparator;
+import java.util.List;
+import java.util.Objects;
+import java.util.function.Predicate;
+
+@UtilityClass
+public class StreamUtils {
+
+    public int calculateEvenNumbersSum(List<Integer> numbers) {
+        Objects.requireNonNull(numbers, "numbers cannot be null");
+
+        return numbers.stream().filter(number -> number % 2 == 0).reduce(0, Integer::sum);
+    }
+
+    public int findMaxNumber(List<Integer> numbers) {
+        Objects.requireNonNull(numbers, "numbers cannot be null");
+
+        return numbers.stream().max(Integer::compare).orElse(0);
+    }
+
+    public double calculateAverageValue(List<Integer> numbers) {
+        Objects.requireNonNull(numbers, "numbers cannot be null");
+
+        return numbers.stream().mapToInt((number) -> number).average().orElse(0);
+    }
+
+    public int countStringsStartingWith(List<String> strings, char startingWith) {
+        Objects.requireNonNull(strings, "strings cannot be null");
+
+        return (int) strings.stream().filter((string) -> string.charAt(0) == startingWith).count();
+    }
+
+    public List<String> filterStringsContainingSubstring(List<String> strings, String substring) {
+        Objects.requireNonNull(strings, "strings cannot be null");
+        Objects.requireNonNull(substring, "substring cannot be null");
+
+        return strings.stream().filter((string) -> string.contains(substring)).toList();
+    }
+
+    public List<String> sortStringsByLength(List<String> strings) {
+        Objects.requireNonNull(strings, "strings cannot be null");
+
+        return strings.stream().sorted(Comparator.comparingInt(String::length)).toList();
+    }
+
+    public boolean checkNumbers(List<Integer> numbers, Predicate<Integer> predicate) {
+        Objects.requireNonNull(numbers, "numbers cannot be null");
+        Objects.requireNonNull(predicate, "predicate cannot be null");
+
+        return numbers.stream().allMatch(predicate);
+    }
+
+    public int findMinGreaterThan(List<Integer> numbers, int threshold) {
+        Objects.requireNonNull(numbers, "numbers cannot be null");
+
+        return numbers.stream().filter(number -> number > threshold).min(Integer::compare).orElse(threshold);
+    }
+
+    public List<Integer> convertStringsToLengths(List<String> strings) {
+        Objects.requireNonNull(strings, "strings cannot be null");
+
+        return strings.stream().map(String::length).toList();
+    }
+}
