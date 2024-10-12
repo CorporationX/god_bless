e02b055d75cba1875a0a905f92ccab30bdfea0ff
SiarheiHaurylchuk <sggavrilchik@gmail.com>
2024-10-12 16:59:53 +0300

    errorHandling

--- src/main/java/school/faang/errorHandling/ErrorHandler.java
+++ src/main/java/school/faang/errorHandling/ErrorHandler.java
@@ -2,11 +2,11 @@ package school.faang.errorHandling;
 
 
 public class ErrorHandler {
-    public static <T> T withErrorHandling(MainActionI<T> main, ErrorActionI<T> error){
+    public static <T> T withErrorHandling(MainActionI<T> main, ErrorActionI<T> error) {
         try {
             return main.get();
-        }catch (Exception e){
-          return  error.handleError(e);
+        } catch (Exception e) {
+            return error.handleError(e);
         }
     }
 }
