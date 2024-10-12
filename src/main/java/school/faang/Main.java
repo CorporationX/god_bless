7a06b930bb96f0de80e64dc416954fd010c3c211
Matvey <mashptz@gmail.com>
2024-10-12 19:24:16 +0300

    Реализация статических методов Использование примера для проверки работоспособности

--- src/main/java/school/faang/Main.java
+++ src/main/java/school/faang/Main.java
@@ -1,4 +1,44 @@
 package school.faang;
 
+import school.faang.methods.Method;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 public class Main {
+    public static void main(String[] args) {
+        Method method = new Method();
+
+        List<Integer> numbers= new ArrayList<>(Arrays.asList(1,2,3,4,5,6,7));
+        int point = 7;
+        System.out.println("Вход: " + numbers + " целевое число: " + point);
+        System.out.println("Выход: " + method.searchPairs(numbers, point));
+
+        Map<String, String> coutry = new HashMap<>(Map.of(
+                "Russia", "Moscow",
+                "USA", "Washington",
+                "Germany","Berlin")
+        );
+
+        System.out.println("Вход: " + coutry);
+        System.out.println("Выход: " + method.sortCapitals(coutry));
+
+
+        List<String> words = new ArrayList<>(Arrays.asList("apple", "banana", "avocado", "apricot"));
+        char ch = 'a';
+        System.out.println("Вход: " + words + " символ: " + ch);
+        System.out.println("Выход: " + method.filterAndSort(words, ch));
+
+
+        System.out.println("Вход: " + numbers );
+        System.out.println("Выход: " + method.convertBinary(numbers));
+
+        List<String> words2 = new ArrayList<>(Arrays.asList("apple", "banana", "cherry", "date", "fig", "grape"));
+        String alphabet = "abcdefghijklmnopqrstuvwxyz";
+        System.out.println("Вход: " + words2 + " aлфавит: " + alphabet);
+        System.out.println("Выход: " + method.sortByLength(words2, alphabet));
+    }
 }
---
+++ src/main/java/school/faang/methods/Method.java
@@ -0,0 +1,54 @@
+package school.faang.methods;
+
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+public class Method {
+    public static Set<List<Integer>> searchPairs(List<Integer> numbers, int point) {
+        Set<List<Integer>> pairs = new HashSet<>();
+        Set<Integer> seen = new HashSet<>();
+        for (int num : numbers) {
+            int complement = point - num;
+            if (seen.contains(complement)) {
+                List<Integer> pair = Arrays.asList(num, complement);
+                pair.sort(Integer::compareTo);
+                pairs.add(pair);
+            }
+            seen.add(num);
+        }
+        return pairs;
+    }
+
+    public List<String> sortCapitals(Map<String, String> countries) {
+        return countries.entrySet().stream()
+                .sorted(Map.Entry.comparingByKey())
+                .map(Map.Entry::getValue)
+                .collect(Collectors.toList());
+    }
+
+    public List<String> filterAndSort(List<String> strings, char letter) {
+        return strings.stream()
+                .filter(s -> s.startsWith(String.valueOf(letter)))
+                .sorted(Comparator.comparingInt(String::length))
+                .collect(Collectors.toList());
+    }
+
+    public List<String> convertBinary(List<Integer> numbers) {
+        return numbers.stream()
+                .map(Integer::toBinaryString)
+                .collect(Collectors.toList());
+    }
+
+    public List<String> sortByLength(List<String> strings, String alphabet) {
+        String regex = "[" + alphabet + "]+";
+        return strings.stream()
+                .filter(s -> s.matches(regex))
+                .sorted(Comparator.comparingInt(String::length))
+                .collect(Collectors.toList());
+    }
+}
