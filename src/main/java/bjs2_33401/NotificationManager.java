582325291c7c74e1cd4f9f9fa2a6363385154cb1
Sanasar1 <almasyansb@gmail.com>
2024-10-12 15:30:03 +0300

    fix: changed blueUnacceptableWords

--- src/main/java/bjs2_33401/NotificationManager.java
+++ src/main/java/bjs2_33401/NotificationManager.java
@@ -1,28 +1,37 @@
 package bjs2_33401;
 
+import lombok.Getter;
+
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Set;
 import java.util.function.Consumer;
+import java.util.function.Function;
 
+@Getter
 public class NotificationManager {
-    public static Map<String, Consumer<Notification>> notificationTypeResponse = new HashMap<>();
-    public static Set<String> unacceptableWords = Set.of("drugs", "alcohol");
+    private Map<String, Consumer<Notification>> responseByNotificationType = new HashMap<>();
+    private Map<String, Function<String, String>> filterByNotificationType = new HashMap<>();
+    private static final Set<String> UNACCAPTABLE_WORDS = Set.of("drugs", "alcohol");
 
     public void registerHandler(String type, Consumer<Notification> consumer) {
-        notificationTypeResponse.put(type, consumer);
+        responseByNotificationType.put(type, consumer);
+    }
+
+    public void registerFilter(String type, Function<String, String> function) {
+        filterByNotificationType.put(type, function);
     }
 
     public void sendNotification(Notification notification) {
         preprocessNotificationMessage(notification, 20);
-        notificationTypeResponse.get(notification.getType()).accept(notification);
+        responseByNotificationType.get(notification.getType()).accept(notification);
     }
 
     public void preprocessNotificationMessage(Notification notification, int maxWords) {
         if (isLongMessage(notification, maxWords)) {
             shortenNotificationMessage(notification, maxWords);
         }
-        blurUnacceptableWords(notification);
+        blurUnacceptableWords(notification, filterByNotificationType.get(notification.getType()));
     }
 
     public boolean isLongMessage(Notification notification, int maxWords) {
@@ -41,13 +50,17 @@ public class NotificationManager {
         notification.setMessage(stringBuilder.toString().trim());
     }
 
-    public void blurUnacceptableWords(Notification notification) {
+    public void blurUnacceptableWords(Notification notification,
+                                      Function<String, String> messageFilter) {
         String message = notification.getMessage();
 
-        for (String word : unacceptableWords) {
-            // меняем слова без учета регистра
-            message = message.replaceAll("(?i)" + word, "***");
+        for (String word : UNACCAPTABLE_WORDS) {
+            message = messageFilter.apply(message);
         }
         notification.setMessage(message);
+    }
+
+    public static Set<String> getUnaccaptableWords() {
+        return UNACCAPTABLE_WORDS;
     }
 }
