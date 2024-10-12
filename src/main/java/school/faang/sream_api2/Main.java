39d581b721aa00eec4267ec2eafc09bee86cdafb
Maks Russkikh <15maksim@mail.ru>
2024-10-12 16:37:12 +0300

    убрал ненужное

--- src/main/java/school/faang/sream_api2/Main.java
+++ src/main/java/school/faang/sream_api2/Main.java
@@ -1,15 +1,10 @@
 package school.faang.sream_api2;
 
-import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.Comparator;
 import java.util.HashMap;
-import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
-import java.util.Set;
 import java.util.function.Predicate;
-import java.util.stream.Stream;
 
 public class Main {
     public static void main(String[] args) {
