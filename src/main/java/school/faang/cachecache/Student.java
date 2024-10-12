daebddcda508c9fbaf463038ef7666ef07f9918f
“Andrey <aiegorovceo@yandex.ru>
2024-10-13 01:20:01 +0300

    Initial commit: Add Student and Subject classes with fields and constructor

---
+++ src/main/java/school/faang/cachecache/Student.java
@@ -0,0 +1,12 @@
+package school.faang.cachecache;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class Student {
+    private int id;
+    private String name;
+}
+
