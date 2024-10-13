fa3488fff7c565c329f442ae2e0290f1ec842f9c
AlekseyB86nb <tigrxam@gmail.com>
2024-10-13 16:41:01 +0300

    реализация вывод на печать

--- src/main/java/electronicDiary_35235/Main.java
+++ src/main/java/electronicDiary_35235/Main.java
@@ -2,22 +2,29 @@ package electronicDiary_35235;
 
 
 import electronicDiary_35235.entity.School;
+import electronicDiary_35235.entity.Student;
+
+import java.util.List;
 
 public class Main {
 
     public static void main(String[] args) {
 
-        School school = new School();
-        school.addStudentsFromJSON("D:\\javaprojects\\faang_school\\src\\main\\java\\electronicDiary_35235\\students_test_data.json");
-        school.calculateAverageScore(school.getStudents())
+        School hogvarts = new School("Hogvarts");
+        List<Student> studentsHogvarts = hogvarts.getStudents();
+
+        hogvarts.addStudentsFromJSON("D:\\javaprojects\\faang_school\\src\\main\\java\\electronicDiary_35235\\students_test_data.json");
+        hogvarts.calculateAverageScore(studentsHogvarts)
                 .forEach((k, v) -> System.out.println("Предмет: " + k + " " + Math.round(v)));
 
         System.out.println("\nИтоговые оценки студента: Алексей Петрова ");
-        school.getTotalScoreStudent(school.getStudents(), "Алексей", "Петрова").ifPresent(map -> {
-            map.forEach((k, v) -> System.out.println(k + "-" + v));
+        hogvarts.getTotalScoreStudent(studentsHogvarts, "Алексей", "Петрова").ifPresent(map -> {
+            map.forEach((k, v) -> System.out.println(k + "-" + Math.round(v)));
         });
 
-        System.out.println("Самый сложный предмет: " + school.calculateHardSubject(school.getStudents()));
+        System.out.println("\nСамый сложный предмет: " + hogvarts.calculateHardSubject(studentsHogvarts));
+
+        hogvarts.printTotalScoreStudents(studentsHogvarts);
 
     }
 }
--- src/main/java/electronicDiary_35235/entity/School.java
+++ src/main/java/electronicDiary_35235/entity/School.java
@@ -8,14 +8,15 @@ import lombok.NonNull;
 
 import java.util.*;
 import java.util.stream.Collectors;
-import java.util.stream.Stream;
 
 @Getter
 @EqualsAndHashCode
 public class School {
+    private final String name;
     private final List<Student> students;
 
-    public School() {
+    public School(String name) {
+        this.name = name;
         this.students = new ArrayList<>();
     }
 
@@ -48,16 +49,19 @@ public class School {
                 ));
     }
 
-    public Optional<Map<String, Integer>> getTotalScoreStudent(@NonNull List<Student> students, String firstname, String lastname) {
+    public Optional<Map<String, Double>> getTotalScoreStudent(@NonNull List<Student> students, String firstname, String lastname) {
         Optional<Student> student = students.stream()
                 .filter(item -> item.getFirstName().equals(firstname) && item.getLastName().equals(lastname))
                 .findFirst();
 
         if (student.isPresent()) {
             Map<String, List<Integer>> subjectScores = student.get().getSubjects();
-            Map<String, Integer> result = subjectScores.entrySet().stream()
+            Map<String, Double> result = subjectScores.entrySet().stream()
                     .collect(Collectors.toMap(Map.Entry::getKey,
-                            entry -> entry.getValue().stream().mapToInt(i -> i).sum() / entry.getValue().size()));
+                            entry -> entry.getValue().stream()
+                                    .mapToDouble(i -> i)
+                                    .average()
+                                    .orElse(0.0)));
             return Optional.of(result);
         }
         return Optional.empty();
@@ -72,4 +76,39 @@ public class School {
                 .orElse(null);
     }
 
+    public void printTotalScoreStudents(@NonNull List<Student> students) {
+        System.out.printf("%-20s ", "ФИО");
+
+        List<String> subjects = calculateAverageScore(students).keySet().stream().toList();
+
+        int maxLength = subjects.stream().mapToInt(String::length).max().orElse(0);
+        subjects.forEach((subject) -> System.out.printf("| %" + maxLength + "s ", subject));
+        System.out.printf("| %" + maxLength + "s | %" + maxLength + "s%n", "%", "Итоговая оценка");
+
+        for (Student student : students) {
+            System.out.printf("%-20s ", student.getFirstName() + " " + student.getLastName());
+
+            List<Double> scores = new ArrayList<>();
+            for (String subject : subjects) {
+                if (student.getSubjects().containsKey(subject)) {
+                    double score = student.getSubjects().get(subject).stream()
+                            .mapToDouble(i -> i)
+                            .average()
+                            .orElse(0.0);
+                    System.out.printf("| %" + maxLength + ".1f ", score);
+                    scores.add(score);
+                } else {
+                    System.out.printf("| %" + maxLength + "s ", "");
+                }
+            }
+
+            double avgScore = scores.stream()
+                    .mapToDouble(Double::doubleValue)
+                    .average()
+                    .orElse(0.0);
+
+            System.out.printf("| %" + maxLength + ".1f ", avgScore / 5.0 * 100);
+            System.out.printf("| %" + maxLength + ".1f%n", avgScore);
+        }
+    }
 }
