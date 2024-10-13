caad3520c73a54faedb99d8b668ca33418c468c7
AlekseyB86nb <tigrxam@gmail.com>
2024-10-13 14:38:43 +0300

    реализация метода получения самого сложного предмета, правка метода получения средней оценки по всем предметам

--- src/main/java/electronicDiary_35235/Main.java
+++ src/main/java/electronicDiary_35235/Main.java
@@ -8,8 +8,16 @@ public class Main {
     public static void main(String[] args) {
 
         School school = new School();
-        school.addStudentsFromJSON("C:\\students_test_data.json");
+        school.addStudentsFromJSON("D:\\javaprojects\\faang_school\\src\\main\\java\\electronicDiary_35235\\students_test_data.json");
         school.calculateAverageScore(school.getStudents())
-                .forEach((k, v) -> System.out.println("Предмет: " + k + " " + v));
+                .forEach((k, v) -> System.out.println("Предмет: " + k + " " + Math.round(v)));
+
+        System.out.println("\nИтоговые оценки студента: Алексей Петрова ");
+        school.getTotalScoreStudent(school.getStudents(), "Алексей", "Петрова").ifPresent(map -> {
+            map.forEach((k, v) -> System.out.println(k + "-" + v));
+        });
+
+        System.out.println("Самый сложный предмет: " + school.calculateHardSubject(school.getStudents()));
+
     }
 }
--- src/main/java/electronicDiary_35235/entity/School.java
+++ src/main/java/electronicDiary_35235/entity/School.java
@@ -19,25 +19,8 @@ public class School {
         this.students = new ArrayList<>();
     }
 
-
-    public Map<String, Integer> calculateAverageScore(@NonNull List<Student> students) {
-        Map<String, List<Integer>> subjectScores = new HashMap<>();
-
-        for (Student student : students) {
-            for (Map.Entry<String, List<Integer>> entry : student.getSubjects().entrySet()) {
-                subjectScores.computeIfAbsent(entry.getKey(), k -> new ArrayList<>()).addAll(entry.getValue());
-            }
-        }
-
-        Map<String, Integer> result = subjectScores.entrySet().stream()
-                .collect(Collectors.toMap(
-                        Map.Entry::getKey,
-                        entry -> (int) entry.getValue().stream()
-                                .mapToInt(i -> i)
-                                .average()
-                                .orElse(0)
-                ));
-        return result;
+    public void addStudents(List<Student> newStudents) {
+        this.students.addAll(newStudents);
     }
 
     public void addStudentsFromJSON(@NonNull String filePath) {
@@ -46,8 +29,47 @@ public class School {
         addStudents(newStudents);
     }
 
-    public void addStudents(List<Student> newStudents) {
-        this.students.addAll(newStudents);
+    public Map<String, Double> calculateAverageScore(@NonNull List<Student> students) {
+        Map<String, List<Integer>> subjectScores = new HashMap<>();
+
+        for (Student student : students) {
+            for (Map.Entry<String, List<Integer>> entry : student.getSubjects().entrySet()) {
+                subjectScores.computeIfAbsent(entry.getKey(), k -> new ArrayList<>()).addAll(entry.getValue());
+            }
+        }
+
+        return subjectScores.entrySet().stream()
+                .collect(Collectors.toMap(
+                        Map.Entry::getKey,
+                        entry -> entry.getValue().stream()
+                                .mapToDouble(i -> i)
+                                .average()
+                                .orElse(0.0)
+                ));
+    }
+
+    public Optional<Map<String, Integer>> getTotalScoreStudent(@NonNull List<Student> students, String firstname, String lastname) {
+        Optional<Student> student = students.stream()
+                .filter(item -> item.getFirstName().equals(firstname) && item.getLastName().equals(lastname))
+                .findFirst();
+
+        if (student.isPresent()) {
+            Map<String, List<Integer>> subjectScores = student.get().getSubjects();
+            Map<String, Integer> result = subjectScores.entrySet().stream()
+                    .collect(Collectors.toMap(Map.Entry::getKey,
+                            entry -> entry.getValue().stream().mapToInt(i -> i).sum() / entry.getValue().size()));
+            return Optional.of(result);
+        }
+        return Optional.empty();
+
+    }
+
+    public String calculateHardSubject(@NonNull List<Student> students) {
+        Map<String, Double> subjectsScore = calculateAverageScore(students);
+        return subjectsScore.entrySet().stream()
+                .min(Map.Entry.comparingByValue())
+                .map(Map.Entry::getKey)
+                .orElse(null);
     }
 
 }
