980e72b6206491c8adfb9660e4a40bd66abaa94a
Ekaterina Badratdinova <ekaterinafeshch@gmail.com>
2024-10-12 19:19:56 -0700

    Анализ активности пользователей в социальной сети

---
+++ src/main/java/ru/kraiush/BJS2_35499/UserActionAnalyzer.java
@@ -0,0 +1,58 @@
+package ru.kraiush.BJS2_35499;
+
+import java.time.LocalDate;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.List;
+import java.util.Map;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+
+public class UserActionAnalyzer {
+
+    public static List<String> topActiveUsers(List<UserAction> actions) {
+        return actions.stream()
+                .collect(Collectors.groupingBy(UserAction::getName, Collectors.counting()))
+                .entrySet().stream()
+                .sorted(Map.Entry.<String, Long>comparingByValue().reversed())
+                .limit(10)
+                .map(Map.Entry::getKey)
+                .toList();
+    }
+
+    public static List<String> topPopularHashtags(List<UserAction> actions) {
+        return actions.stream()
+                .filter(action -> action.getContent() != null && action.getActionType().equals("post") || action.getActionType().equals("comment"))
+                .flatMap(action -> Arrays.stream(action.getContent().replaceAll("[\\p{Punct}&&[^#]]+$", "").split("\\s+")))
+                .filter(word -> word.startsWith("#"))
+                .collect(Collectors.groupingBy(Function.identity(), Collectors.counting()))
+                .entrySet().stream()
+                .sorted(Map.Entry.<String, Long> comparingByValue().reversed())
+                .limit(5)
+                .map(Map.Entry::getKey)
+                .toList();
+    }
+
+    public static List<String> topCommentersLastMonth(List<UserAction> actions) {
+        LocalDate oneMonthAgo = LocalDate.now().minusMonths(1);
+        return actions.stream()
+                .filter(action -> action.getActionType().equals("comment") && action.getActionDate().isAfter(oneMonthAgo))
+                .collect(Collectors.groupingBy(UserAction::getName, Collectors.counting()))
+                .entrySet().stream()
+                .sorted(Map.Entry.<String, Long> comparingByValue().reversed())
+                .limit(3)
+                .map(Map.Entry::getKey)
+                .toList();
+    }
+
+    public static Map<String, Double> actionTypePercentages(List<UserAction> actions) {
+        int amountOfAllActions = actions.size();
+        return actions.stream()
+                .collect(Collectors.groupingBy(UserAction::getActionType, Collectors.counting()))
+                .entrySet().stream()
+                .collect(Collectors.toMap(
+                        Map.Entry::getKey,
+                        entry -> (entry.getValue() * 100.0) / amountOfAllActions
+                ));
+    }
+}
