6a4a0668b02313ae1ac5a57b0a41c579c0d7a024
zlomilk95 <kenobi_95@mail.ru>
2024-10-13 16:30:04 +0300

    edits

--- src/main/java/school/faang/bjs2_34768/Notification.java
+++ src/main/java/school/faang/bjs2_34768/Notification.java
@@ -1,5 +1,15 @@
 package school.faang.bjs2_34768;
 
-public class Notification {
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
 
+@Getter
+@RequiredArgsConstructor
+public class Notification {
+    private final TypeOfNotification type;
+    private final String message;
+
+    public enum TypeOfNotification {
+     SMS, EMAIL, PUSH, RING
+ }
 }
--- src/main/java/school/faang/bjs2_34768/NotificationManager.java
+++ src/main/java/school/faang/bjs2_34768/NotificationManager.java
@@ -1,5 +1,36 @@
 package school.faang.bjs2_34768;
 
-public class NotificationManager {
+import lombok.Getter;
+import lombok.extern.java.Log;
 
+import java.util.HashMap;
+import java.util.Map;
+import java.util.function.Consumer;
+import java.util.function.Predicate;
+
+@Log
+@Getter
+public class NotificationManager {
+    private final Map<Notification.TypeOfNotification, Consumer<Notification>> notificationHandler = new HashMap<>();
+    private final Map<Notification.TypeOfNotification, Predicate<Notification>> notificationFilter = new HashMap<>();
+
+    public void registerHandler(Notification.TypeOfNotification type, Consumer<Notification> notification) {
+        notificationHandler.put(type, notification);
+    }
+
+    public void sendNotification(Notification notification) {
+       notificationHandler.get(notification.getType()).accept(notification);
+    }
+
+    public void registerFilter(Notification.TypeOfNotification type, Predicate<Notification> notification) {
+        notificationFilter.put(type, notification);
+    }
+    public void isNotificationContainsCensorship(Notification notification) {
+        boolean isCensor = notificationFilter.get(notification.getType()).test(notification);
+        if (isCensor) {
+            log.warning("Notification contains censorship. Sending prohibited!");
+        } else {
+            log.info("The notice does not contain censorship. Let's pass");
+        }
+    }
 }
