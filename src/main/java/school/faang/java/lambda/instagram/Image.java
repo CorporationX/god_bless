dc1f5b904da1ebf3101e81db22b46b1f898045b7
Oleksii Babaikin <a.babaykin@gmail.com>
2024-10-13 19:23:22 +0200

    BJS2-34899 Фильтры Instagram 1) основная часть Main, FilterProcessor, Image

---
+++ src/main/java/school/faang/java/lambda/instagram/Image.java
@@ -0,0 +1,12 @@
+package school.faang.java.lambda.instagram;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+
+public class Image {
+    private String name;
+    private String description;
+}
