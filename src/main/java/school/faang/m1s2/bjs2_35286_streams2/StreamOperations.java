f76d4c9585d6185ee0c3bd2894408c3ebd939eec
Andrew Balashov <balashov83@gmail.com>
2024-10-13 19:56:59 +0300

    BJS2-35286 Stream ops 2: fixes

--- src/main/java/school/faang/m1s2/bjs2_35286_streams2/StreamOperations.java
+++ src/main/java/school/faang/m1s2/bjs2_35286_streams2/StreamOperations.java
@@ -1,22 +1,23 @@
 package school.faang.m1s2.bjs2_35286_streams2;
 
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.Comparator;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
 import java.util.stream.Collectors;
 
-import static java.util.Arrays.asList;
-
 public class StreamOperations {
 
     public static Set<List<Integer>> findUniquePairs(List<Integer> list, int target) {
+
+        Set<Integer> set = new HashSet<>(list);
         return list.stream()
-                .flatMap(i -> list.stream()
-                        .filter(j -> i + j == target)
-                        .map(j -> asList(Math.min(i, j), Math.max(i, j)))
-                )
+                .filter(num -> set.contains(target - num))
+                .map(num -> Arrays.asList(num, target - num))
+                .peek(Collections::sort)
                 .collect(Collectors.toSet());
     }
 
@@ -41,11 +42,10 @@ public class StreamOperations {
     }
 
     public static List<String> filterSortByAlphabet(List<String> list, String alphabet) {
+        Set<String> lettersSet = Set.of(alphabet.split(""));
         return list.stream()
-                .filter(s -> s.length() ==
-                        Arrays.stream(s.split(""))
-                                .filter(alphabet::contains)
-                                .count())
+                .filter(s -> Arrays.stream(s.split(""))
+                        .allMatch(lettersSet::contains))
                 .sorted(Comparator.comparingInt(String::length))
                 .toList();
     }
