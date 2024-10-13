776b4e2eaff01c57c9f5ca8fd94e69e69afe2dde
heckfy86 <marlinco263@gmail.com>
2024-10-13 15:19:58 +0200

    BJS2-35476 completed

--- src/main/java/school/faang/socialnetwork/UserActionAnalyzer.java
+++ src/main/java/school/faang/socialnetwork/UserActionAnalyzer.java
@@ -2,7 +2,6 @@ package school.faang.socialnetwork;
 
 import java.time.LocalDate;
 import java.util.ArrayList;
-import java.util.Comparator;
 import java.util.List;
 import java.util.Map;
 import java.util.regex.Matcher;
