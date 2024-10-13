ad32f4145b94b0e52afc74df64b03ec837802cc3
nikitasidorov <Nikita.sidorov.7@icloud.com>
2024-10-13 14:22:28 +0300

    Секреты дроидов

---
+++ src/main/java/school/faang/droidsSecrets/Main.java
@@ -0,0 +1,13 @@
+package school.faang.droidsSecrets;
+
+public class Main {
+  public static void main(String[] args) {
+    Droid r2d2 = new Droid("R2D2");
+    Droid c3po = new Droid("C3PO");
+    Droid bb8 = new Droid("BB-8");
+
+    r2d2.sendMessage(c3po, "AB", 1, null);
+    c3po.sendMessage(r2d2, "qwe", 3, null);
+    bb8.sendMessage(c3po, "The mission is complete.", 7, null);
+  }
+}
