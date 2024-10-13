93603941c7273eacd42075c6c9c87be228dda64c
IvanMatiko <matiko1993@gmail.com>
2024-10-13 13:54:17 +0200

    Made task Meta Universe BJS2-2389

---
+++ src/main/java/MetaUniverse_BJS2_34890/Notification.java
@@ -0,0 +1,12 @@
+package MetaUniverse_BJS2_34890;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.AllArgsConstructor;
+
+@Setter
+@Getter
+@AllArgsConstructor
+public class Notification {
+    private String type;
+    private String message;
+}
