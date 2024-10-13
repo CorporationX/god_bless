0ac66fd9b9b9b044e98fc3d3eb2e9b6a57f67938
SerUKalinin <serukalinin@gmail.com>
2024-10-13 13:34:47 +0300

    Removed student's code

--- src/main/java/school/faang/BJS2_32549/Student.java
+++ src/main/java/school/faang/BJS2_32549/Student.java
@@ -1,18 +1,17 @@
 package school.faang.BJS2_32549;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.EqualsAndHashCode;
 import lombok.Getter;
+import lombok.ToString;
 
-@Data
+@AllArgsConstructor
+@EqualsAndHashCode
+@ToString
 @Getter
 public class Student {
     private String name;
     private String faculty;
     private int year;
-
-    public Student(String name, String faculty, int year) {
-        this.name = name;
-        this.faculty = faculty;
-        this.year = year;
-    }
 }
