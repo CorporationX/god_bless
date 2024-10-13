2350fde1d2a05402c193a81e5fd36cf58c371433
Aleksey <aleksei.zuk86@gmail.com>
2024-10-13 15:25:57 +0300

    немного переписал

--- src/main/java/school/faang/heroes/Army.java
+++ src/main/java/school/faang/heroes/Army.java
@@ -2,32 +2,34 @@ package school.faang.heroes;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.Callable;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.Future;
 import java.util.concurrent.TimeUnit;
 
+
 public class Army {
     List<Fighter> fighters = new ArrayList<>();
     ExecutorService service = Executors.newFixedThreadPool(4);
+    int totalPower = 0;
 
     public void addUnit(Fighter fighter) {
         fighters.add(fighter);
     }
 
     public int calculateTotalPower() throws InterruptedException {
-        List<Future<Integer>> futures = new ArrayList<>();
-        int totalPower = 0;
+        List<Callable<Integer>> tasks = fighters.stream()
+                .map(f -> (Callable<Integer>) f::getPower)
+                .toList();
 
-        for (Fighter fighter : fighters) {
-            futures.add(service.submit(fighter::getPower));
-        }
+        List<Future<Integer>> futures = service.invokeAll(tasks);
 
         for (Future<Integer> future : futures) {
             try {
                 totalPower += future.get();
-            } catch (InterruptedException | ExecutionException e) {
+            } catch (ExecutionException e) {
                 throw new RuntimeException(e);
             }
         }
@@ -35,10 +37,12 @@ public class Army {
         service.shutdown();
 
         if (!service.awaitTermination(10, TimeUnit.SECONDS)) {
-            System.out.println("Пришла неожиданная фигня");
+            System.out.println("Беда подкралась!");
             service.shutdownNow();
         }
 
         return totalPower;
     }
 }
+
+
