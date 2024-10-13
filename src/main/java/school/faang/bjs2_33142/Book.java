1bf68b5a12035ea08b8847cc88d48748a1eaf1c2
Mark Janovskij <markyanovskiy@gmail.com>
2024-10-13 15:36:59 +0200

    finish library

--- src/main/java/school/faang/bjs2_33142/Book.java
+++ src/main/java/school/faang/bjs2_33142/Book.java
@@ -1,5 +1,7 @@
 package school.faang.bjs2_33142;
+
 import java.util.*;
+
 public class Book {
 
 
@@ -7,11 +9,42 @@ public class Book {
     private String author;
     private int year;
 
-    public  void Book(String title,String author,int year){
+    public  Book(String title, String author, int year) {
         this.title = title;
         this.author = author;
         this.year = year;
     }
-    Map<String, Integer> mapBook = new HashMap<>();
 
+//    Map<String, Integer> mapBook = new HashMap<>();
+    @Override
+    public boolean equals(Object o){
+        if (this == o) return true;
+        if(o == null|| getClass()!= o.getClass())return false;
+        Book bookOfLibreryVesteros = (Book) o;
+        return year == bookOfLibreryVesteros.year &&
+                Objects.equals(title,bookOfLibreryVesteros.title) &&
+                Objects.equals(author,bookOfLibreryVesteros.author);
+}
+    @Override
+    public  int hashCode(){
+        return Objects.hash(title,author,year);
+    }
+    public String getTitle(){
+        return title;
+    }
+    public void setTitle(String title){
+        this.title = title;
+    }
+    public String getAuthor(){
+        return author;
+    }
+    public void setAuthor(String author){
+        this.title = author;
+    }
+    public int getYear(){
+        return year;
+    }
+    public void setYear(int year){
+        this.year = year;
+    }
 }
