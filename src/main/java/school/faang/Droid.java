98b55ae453f54fa2684ee45eb9a63258decf9805
lomuserg <sergeymu2015@yandex.ru>
2024-10-13 16:30:25 +0300

    sendMessage receiveMessage

--- src/main/java/school/faang/Droid.java
+++ src/main/java/school/faang/Droid.java
@@ -13,20 +13,23 @@ public class Droid {
     private String name;
 
     public void sendMessage(Droid droid, String message, int shift) {
-        encryptMessage(message, droid, (message, shift) -> {} );
-
+        message = encryptMessage(message, shift, (u, v) -> caesarCipher(u, v));
+        System.out.println(this.getName() + " send " + message + " to: " + droid.getName());
+        droid.receiveMessage(message, shift);
     }
 
-    public static void receiveMessage() {
-
+    public void receiveMessage(String message, int shift) {
+        message = decryptMessage(message, shift, (u, v) -> caesarCipher(u, v));
+        System.out.println(this.getName() + " decrypt to: " + message);
     }
 
-    private static void encryptMessage(String message, int shift,
-                                      DroidMessageEncryptor<String, Integer, String> messageEncryptor) {
-        message = caesarCipher(message, shift);
+    private static String encryptMessage(String message, int shift,
+                                         DroidMessageEncryptor<String, Integer, String> messageEncryptor) {
+        return messageEncryptor.apply(message, shift);
     }
 
-    private static void decryptMessage(String message, DroidMessageEncryptor<String, Integer, String> messageEncryptor) {
-
+    private static String decryptMessage(String message, int shift,
+                                         DroidMessageEncryptor<String, Integer, String> messageEncryptor) {
+        return messageEncryptor.apply(message, 0 - shift);
     }
 }
