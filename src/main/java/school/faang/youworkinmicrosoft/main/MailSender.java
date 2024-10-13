b3844d012079740dda78abec74a345acd057f393
Gadzhiev Gadzhi <Gadzhiev3222@gmail.com>
2024-10-13 09:01:49 +0300

    вы работаете в Microsoft

---
+++ src/main/java/school/faang/youworkinmicrosoft/main/MailSender.java
@@ -0,0 +1,24 @@
+package school.faang.youworkinmicrosoft.main;
+
+import school.faang.youworkinmicrosoft.maincode.SenderRunnable;
+
+public class MailSender {
+    public static void main(String[] args) throws InterruptedException {
+        int totalCount = 1000;
+        int threadCount = 5;
+        int batchSize = totalCount / threadCount;
+
+        Thread[] threads = new Thread[threadCount];
+
+        for (int i = 0; i < threadCount; i++) {
+            int startIndex = i * batchSize;
+            int endIndex = (i + 1) * batchSize;
+
+            threads[i] = new Thread(new SenderRunnable(startIndex, endIndex));
+            threads[i].start();
+            threads[i].join();
+        }
+
+        System.out.println("Все письма отправлены");
+    }
+}
