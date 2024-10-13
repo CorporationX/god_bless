a842648e150b7ffb3801ae309926dfded2dfa19a
heckfy86 <marlinco263@gmail.com>
2024-10-13 21:54:03 +0200

    BJS2-37766 completed

--- src/main/java/school/faang/microsoftmail/MailSender.java
+++ src/main/java/school/faang/microsoftmail/MailSender.java
@@ -2,8 +2,6 @@ package school.faang.microsoftmail;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
 
 public class MailSender {
     public static void main(String[] args) {
