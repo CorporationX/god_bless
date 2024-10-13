7d4553fc5472b754820cc45f8a362aae2216ec77
Alina44444 <alina.valiyeva.s@mail.ru>
2024-10-13 19:23:45 +0500

    Задача Lord of the Rings RPG

---
+++ src/main/java/faang/school/godbless/InventoryManager/Character.java
@@ -0,0 +1,8 @@
+package faang.school.godbless.InventoryManager;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Character {
+    private List<Item> inventory = new ArrayList<>();
+}
---
+++ src/main/java/faang/school/godbless/InventoryManager/Item.java
@@ -0,0 +1,6 @@
+package faang.school.godbless.InventoryManager;
+
+public class Item {
+    private String name;
+    private int value;
+}
