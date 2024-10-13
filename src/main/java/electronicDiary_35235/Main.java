f724793f735367ba52d4bb34a45dbc9a13ee28a3
AlekseyB86nb <tigrxam@gmail.com>
2024-10-13 11:49:22 +0300

    изменил путь к файлу с данными

--- src/main/java/electronicDiary_35235/Main.java
+++ src/main/java/electronicDiary_35235/Main.java
@@ -8,7 +8,7 @@ public class Main {
     public static void main(String[] args) {
 
         School school = new School();
-        school.addStudentsFromJSON("Z:\\Downloads\\students_test_data.json");
+        school.addStudentsFromJSON("C:\\students_test_data.json");
         school.calculateAverageScore(school.getStudents())
                 .forEach((k, v) -> System.out.println("Предмет: " + k + " " + v));
     }
