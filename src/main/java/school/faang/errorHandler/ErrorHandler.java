5b87f775aa3010a6102ba8612cdaaecb7eec2e7c
m1rroxxx <Nikita.sidorov.7@icloud.com>
2024-10-13 12:51:08 +0300

    fixed

--- src/main/java/school/faang/errorHandler/ErrorHandler.java
+++ src/main/java/school/faang/errorHandler/ErrorHandler.java
@@ -3,7 +3,10 @@ package school.faang.errorHandler;
 import java.util.function.Function;
 
 public class ErrorHandler {
-  public static <T> T withErrorHandling(CheckedSupplier<T> successfulAction, Function<Exception, T> anSuccessfulAction) {
+  public static <T> T withErrorHandling(
+      CheckedSupplier<T> successfulAction,
+      Function<Exception, T> anSuccessfulAction) {
+
     try {
       return successfulAction.get();
     } catch (Exception e) {
