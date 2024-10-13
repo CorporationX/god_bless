646ad4a1378006048feee13c8c649711dff90b1b
lomuserg <sergeymu2015@yandex.ru>
2024-10-13 11:56:44 +0300

    applyFilter combineFilters

--- src/main/java/school/faang/FilterProcessor.java
+++ src/main/java/school/faang/FilterProcessor.java
@@ -1,6 +1,13 @@
 package school.faang;
 
+import java.util.function.Function;
+
 public class FilterProcessor {
-    public static void applyFilter(){}
-    public static void combineFilters(){}
+    public static Image applyFilter(Image originalImage, Function<Image, Image> filter) {
+        return filter.apply(originalImage);
+    }
+
+    public static Function<Image, Image> combineFilters(Function<Image, Image> filter1, Function<Image, Image> filter2) {
+        return filter1.andThen(filter2);
+    }
 }
--- src/main/java/school/faang/Main.java
+++ src/main/java/school/faang/Main.java
@@ -5,25 +5,29 @@ import java.util.function.Function;
 public class Main {
 
     public static void main(String[] args) {
-        Image originalImage = new Image("original.jpg", "Оригинальное изображение");
+        Image originalImage = new Image("original.jpg", "Orig");
 
         FilterProcessor filterProcessor = new FilterProcessor();
 
-// Фильтры
-        Function<Image, Image> grayscaleFilter = (image) -> new Image(image.getName(), image.getDescription() + " | Фильтр: черно-белый");
-        Function<Image, Image> sepiaFilter = (image) -> new Image(image.getName(), image.getDescription() + " | Фильтр: сепия");
+        Function<Image, Image> grayscaleFilter = (image) ->
+                new Image(image.getName(), image.getDescription() + " | Filter: grayscale");
+        Function<Image, Image> sepiaFilter = (image) ->
+                new Image(image.getName(), image.getDescription() + " | Filter: sepia");
+        Function<Image, Image> vignetteFilter = (image) ->
+                new Image(image.getName(), image.getDescription() + " | Filter: vignette");
 
-// Применение фильтров
         Image grayscaleImage = filterProcessor.applyFilter(originalImage, grayscaleFilter);
-        System.out.println(grayscaleImage.getDescription()); // Вывод: Оригинальное изображение | Фильтр: черно-белый
+        System.out.println(grayscaleImage.getDescription());
 
         Image sepiaImage = filterProcessor.applyFilter(grayscaleImage, sepiaFilter);
-        System.out.println(sepiaImage.getDescription()); // Вывод: Оригинальное изображение | Фильтр: черно-белый | Фильтр: сепия
+        System.out.println(sepiaImage.getDescription());
 
-// Комбинирование фильтров
-        Function<Image, Image> combinedFilter = filterProcessor.combineFilters(grayscaleFilter, sepiaFilter);
+        Image vignetteImage = filterProcessor.applyFilter(sepiaImage, vignetteFilter);
+        System.out.println(vignetteImage.getDescription());
+
+        Function<Image, Image> combinedFilter = filterProcessor.combineFilters(grayscaleFilter, vignetteFilter);
         Image combinedImage = filterProcessor.applyFilter(originalImage, combinedFilter);
-        System.out.println(combinedImage.getDescription()); // Вывод: Оригинальное изображение | Фильтр: черно-белый | Фильтр: сепия
+        System.out.println(combinedImage.getDescription());
     }
 
 
