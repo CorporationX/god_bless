f4bcdab5b3fe46adf6fd0fad8bc92d1d1e7eb81b
UrmatBolotbek <urmar9797@gmail.com>
2024-10-14 03:00:26 +0200

    complete the task "Электронный дневник BJS2-34845

---
+++ src/main/java/school/faang/BJS2_35230/Student.java
@@ -0,0 +1,19 @@
+package school.faang.BJS2_35230;
+
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+import java.util.Map;
+
+@Getter
+@NoArgsConstructor
+public class Student {
+    private String firstName;
+    private String lastName;
+    private Map<String, List<Integer>> subjects;
+    public String getFullName() {
+        return this.firstName + " " + this.lastName;
+    }
+}
