91787c81e839db7e2a5838d6d12f1b57ec1f8b8b
Doniyor Kurbanov <doniyor.kurbanov.24@gmail.com>
2024-10-14 21:51:05 +0500

    BJS2-33924. Жду от вас фитбека спасибо

--- src/main/java/school/faang/BJS233924/Calculations.java
+++ src/main/java/school/faang/BJS233924/Calculations.java
@@ -3,14 +3,7 @@ package school.faang.BJS233924;
 import java.util.List;
 
 public class Calculations {
-    private static void validate(List<Integer> nums) {
-        if (nums == null) {
-            throw new IllegalArgumentException("Are you stupid? Where is your f*cking nums?");
-        }
-    }
-
     public static int calculate(List<Integer> nums, Calculator calculator) throws IllegalArgumentException {
-        validate(nums);
         int result = (calculator.operation(0, 1) == 1) ? 0 : 1;
         for (Integer num : nums) {
             result = calculator.operation(result, num);
@@ -19,12 +12,10 @@ public class Calculations {
     }
 
     public int product(List<Integer> nums) throws IllegalArgumentException {
-        validate(nums);
         return calculate(nums, (num1, num2) -> num1 * num2);
     }
 
     public int sum(List<Integer> nums) throws IllegalArgumentException {
-        validate(nums);
         return calculate(nums, Integer::sum);
     }
 
