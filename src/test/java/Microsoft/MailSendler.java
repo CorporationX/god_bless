d069925f2e989b79f874d36fce5a88b1ff8b5bd0
Irina Krasnova <arisha.17@mail.ru>
2024-10-14 11:46:49 +0300

    Вы работаеье в Microsoft. Исправлено

--- src/test/java/Microsoft/MailSendler.java
+++ src/test/java/Microsoft/MailSendler.java
@@ -5,11 +5,23 @@ import java.util.concurrent.Executors;
 
 public class MailSendler {
     public static void main(String[] args) {
-        SanderRunnable sanderRunnable = new SanderRunnable(0,200);
-        ExecutorService executor = Executors.newFixedThreadPool(5);
-        for(int i=sanderRunnable.getStartindex(); i< sanderRunnable.getEndindex(); i++){
-            executor.submit(()->sanderRunnable.run());
+        int totalMessage = 1000;
+        int threadsCount = 5;
+        int batchSize = totalMessage / threadsCount;
+        Thread[] threads = new Thread[threadsCount];
+        for (int i = 0; i < threadsCount; i++) {
+            int start = i * batchSize;
+            int end = (i + 1) * batchSize;
+            threads [i] =new Thread(new SanderRunnable(start,end));
+            threads[i].start();
         }
-executor.shutdown();
+        for(Thread thread :threads){
+            try {
+                thread.join();
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+        }
+        System.out.println("All letters have been sent");
     }
 }
