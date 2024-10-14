2dda45211ee5fabe3f256a4649aede7ff288976e
Aliyar <aliartalybov@gmail.com>
2024-10-14 16:46:48 +0300

    Поменял main в соответствии с примером в задаче

--- src/main/java/Sprint_2/BJS2_34857/Main.java
+++ src/main/java/Sprint_2/BJS2_34857/Main.java
@@ -1,40 +1,29 @@
 package Sprint_2.BJS2_34857;
 
-import java.util.function.Function;
-import java.util.function.Predicate;
-
 public class Main {
-    public static void main(String[] args) {
-        NotificationManager notificationManager = new NotificationManager();
+        public static void main(String[] args) {
+            NotificationManager notificationManager = new NotificationManager();
 
-        notificationManager.registerHandler("Info", notification -> System.out.println("processing info: " + notification.getMessage()));
+            notificationManager.registerHandler("email",
+                    (notification) -> System.out.println("Отправка по электронной почте: " + notification.getMessage())
+            );
 
-        notificationManager.registerHandler("Warning", notification -> System.out.println("processing warning: " + notification.getMessage()));
+            notificationManager.registerHandler("sms",
+                    (notification) -> System.out.println("Отправка SMS: " + notification.getMessage())
+            );
 
-        notificationManager.registerHandler("Error", notification -> System.out.println("processing warning: " + notification.getMessage()));
+            notificationManager.registerHandler("push",
+                    (notification) -> System.out.println("Отправка push-уведомления: " + notification.getMessage())
+            );
 
+            // оповещения
+            Notification emailNotification = new Notification("email", "Ваша учетная запись успешно активирована");
+            Notification smsNotification = new Notification("sms", "Вы успешно изменили свой пароль");
+            Notification pushNotification = new Notification("push", "Новый пост от пользователя: JohnDoe");
 
-        Notification infoNotification = new Notification("Info", "This is an info message.");
-        Notification warningNotification = new Notification("Warning", "This is an warning message.");
-        Notification errorNotification = new Notification("Error", "This is an error message.");
-
-
-        notificationManager.sendNotification(infoNotification);
-        notificationManager.sendNotification(warningNotification);
-        notificationManager.sendNotification(errorNotification);
-
-
-        Predicate<Notification> containsBadWord = notification -> !notification.getMessage().toLowerCase().contains("bad");
-        Function<Notification, Notification> correctMessage = notification -> {
-            notification.setMessage(notification.getMessage().replace("bad", "[censored]"));
-            return notification;
-        };
-
-        notificationManager.registerFilteredHandler("FILTERED", containsBadWord, correctMessage);
-
-        Notification filteredNotification = new Notification("FILTERED", "This is a bad message.");
-        notificationManager.sendNotification(filteredNotification);
+            notificationManager.sendNotification(emailNotification);
+            notificationManager.sendNotification(smsNotification);
+            notificationManager.sendNotification(pushNotification);
+        }
     }
-}
-
 
