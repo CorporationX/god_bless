d42aedf287e518cfc865f36b26ba02b1622dcc98
Maxim Gorobets <gorobets.maks@bk.ru>
2024-10-14 19:51:54 +1000

    new flipMatrix method

--- src/main/java/school/faang/BJS2_33968_RotateMatrix/MatrixOperations.java
+++ src/main/java/school/faang/BJS2_33968_RotateMatrix/MatrixOperations.java
@@ -1,7 +1,7 @@
 package school.faang.BJS2_33968_RotateMatrix;
 
 public class MatrixOperations {
-    static int[][] transformMatrix(int[][] matrix, MatrixTransformer transformer) {
+    public static int[][] transformMatrix(int[][] matrix, MatrixTransformer transformer) {
         int[][] transformedMatrix = new int[matrix.length][matrix[0].length];
         for (int i = 0; i < matrix.length; i++)
             for (int j = 0; j < matrix[i].length; j++) {
@@ -11,5 +11,14 @@ public class MatrixOperations {
                 transformedMatrix[newX][newY] = matrix[i][j];
             }
         return transformedMatrix;
+    }
+
+    public static int[][] flipMatrix(int[][] matrix, FlipDirection flipDirection) {
+        if (flipDirection == FlipDirection.HORIZONTAL) {
+            return transformMatrix(matrix, (x, y) -> new Coordinates(x, matrix[x].length - y - 1));
+        } else if (flipDirection == FlipDirection.VERTICAL) {
+            return transformMatrix(matrix, (x, y) -> new Coordinates(matrix.length - x - 1 ,y));
+        }
+        return null;
     }
 }
