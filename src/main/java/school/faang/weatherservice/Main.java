562f3d6425153b1c9290f2fbfeeea453ea37f2ae
“Andrey <aiegorovceo@yandex.ru>
2024-10-15 00:05:24 +0300

    Implement method to get weather data from cache or fetch from external source

--- src/main/java/school/faang/weatherservice/Main.java
+++ src/main/java/school/faang/weatherservice/Main.java
@@ -8,4 +8,20 @@ public class Main {
     // Create a HashMap to act as a cache for weather data
     private static final Map<String, WeatherData> weatherCache = new HashMap<>();
     private static final WeatherService weatherService = new WeatherService();
+
+    // Method to get weather data for a city, using cache
+    public static WeatherData getWeather(String city) {
+        WeatherData weatherData = weatherCache.get(city);
+        if (weatherData != null) {
+            System.out.println("Weather for " + city + " (from cache): " + weatherData);
+            return weatherData;
+        } else {
+            System.out.println("Fetching weather for " + city + " from external source...");
+            weatherData = weatherService.fetchWeatherFromExternalSource(city);
+            weatherCache.put(city, weatherData);
+            System.out.println("Weather for " + city + ": " + weatherData);
+            return weatherData;
+        }
+    }
+
 }
