da0952d0bd211a7c3b9042196a981d4f252dcbe4
m1rroxxx <Nikita.sidorov.7@icloud.com>
2024-10-14 17:27:25 +0300

    Вы работаете в Microsoft!

---
+++ src/main/java/school/faang/workInTheMicrosoft/Message.java
@@ -0,0 +1,11 @@
+package school.faang.workInTheMicrosoft;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class Message {
+  private String title;
+  private String content;
+}
