9a46cbe30f3a5bd490b2a5b8b914eed0dbc2d47a
“Andrey <aiegorovceo@yandex.ru>
2024-10-15 02:04:11 +0300

    Create ResourceRequest class to manage incoming resource allocation requests

---
+++ src/main/java/school/faang/optimizingdc/ResourceRequest.java
@@ -0,0 +1,11 @@
+package school.faang.optimizingdc;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class ResourceRequest {
+    private double load; // Requested load for allocation
+}
+
