180bcb01d891bbb23fbd23f26c3b0a4743844cf6
Gadzhiev Gadzhi <Gadzhiev3222@gmail.com>
2024-10-14 19:33:21 +0300

    Счетчик просмотров на YouTube

---
+++ src/main/java/school/faang/theirontronatanycost/maincode/House.java
@@ -0,0 +1,12 @@
+package school.faang.theirontronatanycost.maincode;
+
+import lombok.AllArgsConstructor;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@AllArgsConstructor
+public class House {
+    private List<String> roles = new ArrayList<>();
+    private int rolesCount;
+}
---
+++ src/main/java/school/faang/theirontronatanycost/maincode/User.java
@@ -0,0 +1,11 @@
+package school.faang.theirontronatanycost.maincode;
+
+public class User {
+    private String name;
+    private String house;
+    private String role;
+
+    public void joinHouse(House house) {
+
+    }
+}
