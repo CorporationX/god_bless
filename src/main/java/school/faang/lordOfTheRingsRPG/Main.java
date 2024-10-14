15486f866f44d8095a0197231359ce658d9e2e32
m1rroxxx <Nikita.sidorov.7@icloud.com>
2024-10-14 04:05:04 +0300

    fixed

--- src/main/java/school/faang/lordOfTheRingsRPG/Main.java
+++ src/main/java/school/faang/lordOfTheRingsRPG/Main.java
@@ -9,21 +9,17 @@ public class Main {
 
     InventoryManager manager = new InventoryManager();
 
-// Добавляем предмет в инвентарь
     manager.addItem(frodo, ring, (item) -> System.out.println(item.getName() + " был добавлен в инвентарь."));
     manager.addItem(frodo, armor, (item) -> System.out.println(item.getName() + " был добавлен в инвентарь."));
     manager.addItem(frodo, sword, (item) -> System.out.println(item.getName() + " был добавлен в инвентарь."));
 
     frodo.getInventory().forEach(item -> System.out.println(item.getName() + ": " + item.getPrice()));
 
-// Удаляем предмет из инвентаря
     manager.removeItem(frodo, (item) -> item.getName().equals("ring"));
 
-// Добавляем обратно предмет и обновляем его стоимость
     manager.addItem(frodo, ring, (item) -> System.out.println(item.getName() + " снова добавлен."));
     manager.updateItem(frodo, (item) -> item.getName().equals("ring"), (item) -> new Item(item.getName(), item.getPrice() * 2));
 
-// Проверяем результат
     frodo.getInventory().forEach(item -> System.out.println(item.getName() + ": " + item.getPrice()));
   }
 }
