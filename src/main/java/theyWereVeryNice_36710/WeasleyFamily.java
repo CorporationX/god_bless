c1f2078b419e8b9a328ec397dfe53fa4d174b28c
AlekseyB86nb <tigrxam@gmail.com>
2024-10-14 23:21:18 +0300

    кодстайл

--- src/main/java/theyWereVeryNice_36710/WeasleyFamily.java
+++ src/main/java/theyWereVeryNice_36710/WeasleyFamily.java
@@ -7,12 +7,12 @@ import java.util.concurrent.Executors;
 import java.util.concurrent.TimeUnit;
 
 public class WeasleyFamily {
-    private static final List<String> chores = Arrays.asList("помыть посуду", "подмести пол", "приготовить ужин");
+    private static final List<String> CHORES = Arrays.asList("помыть посуду", "подмести пол", "приготовить ужин");
 
     public static void main(String[] args) {
         ExecutorService executor = Executors.newCachedThreadPool();
 
-        for (String chore : chores) {
+        for (String chore : CHORES) {
             executor.execute(new Chore(chore));
         }
         executor.shutdown();
