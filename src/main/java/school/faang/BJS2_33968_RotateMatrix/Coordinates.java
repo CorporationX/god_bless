1a26cfdb72c63572a6a3b84c1fd0c121107bd309
Maxim Gorobets <gorobets.maks@bk.ru>
2024-10-14 19:20:06 +1000

    new class Coordinates

---
+++ src/main/java/school/faang/BJS2_33968_RotateMatrix/Coordinates.java
@@ -0,0 +1,11 @@
+package school.faang.BJS2_33968_RotateMatrix;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class Coordinates {
+    private int x;
+    private int y;
+}
