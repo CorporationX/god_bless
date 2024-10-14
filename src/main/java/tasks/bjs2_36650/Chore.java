da04f65906fe5cc214182025b5656cdf26db13d2
Lexsandr <lexsandr@inbox.ru>
2024-10-14 18:48:48 +0300

    bjs2-36650 : They were very nice but extremely poor Создан класс Chore для хранения задачь Создан класс WeasleyFamily и метод main для тестирования функоционала.

---
+++ src/main/java/tasks/bjs2_36650/Chore.java
@@ -0,0 +1,22 @@
+package tasks.bjs2_36650;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class Chore implements Runnable {
+    private String chore;
+
+    @Override
+    public void run() {
+        try {
+            System.out.println("Задача : " + chore);
+            System.out.println(Thread.currentThread().getName() + ": Выполняется");
+            Thread.sleep(2000);
+            System.out.println(Thread.currentThread().getName() + ": Выполнено");
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
