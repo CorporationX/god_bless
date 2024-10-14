c4e734d8b43062707ee38618fe728a9d130e7ef4
kristina <ksafyanik@mail.ru>
2024-10-14 17:20:50 +0300

    Выполнение оставшихся заданий

--- src/main/java/school/faang/BJS235121/Main.java
+++ src/main/java/school/faang/BJS235121/Main.java
@@ -2,14 +2,17 @@ package school.faang.BJS235121;
 
 import java.util.Arrays;
 import java.util.Collections;
+import java.util.Comparator;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Objects;
 import java.util.Set;
 import java.util.stream.Collectors;
 
 public class Main {
     public static void main(String[] args) {
+
         Set<List<Integer>> result = getUniquePairsIntegers(List.of(1, 2, 3, 4, 5, 6), 7);
         System.out.println(result);
         Set<List<Integer>> result2 = getUniquePairsIntegers(List.of(1, 3, 4, 5), 11);
@@ -18,6 +21,12 @@ public class Main {
         Map<String, String> resultMap = new HashMap<>(
                 Map.of("Russia", "Moscow", "USA", "Washington", "Germany", "Berlin"));
         System.out.println(getCitiesFromCountries(resultMap));
+
+        System.out.println(getFilterSortStringList(List.of("apple", "banana", "avocado", "apricot"), 'a'));
+        System.out.println(toBinaryIntegerList(List.of(1, 2, 3, 4, 5, 6, 7, 8, 9, 10)));
+        System.out.println(getFilterAlfavitStringList(
+                List.of("apple", "banana", "cherry", "date", "fig", "grape"),
+                "abcdefghijklmnopqrstuvwxyz"));
     }
     public static Set<List<Integer>> getUniquePairsIntegers(List<Integer> integerList, int integersSum) {
          return integerList.stream()
@@ -31,6 +40,30 @@ public class Main {
         return contriesMap.values().stream()
                 .sorted()
                 .collect(Collectors.toList());
+    }
+
+    public static List<String> getFilterSortStringList(List<String> stringList, char symbol) {
+        return stringList.stream()
+                .filter(s -> Objects.nonNull(s) && s.charAt(0) == symbol)
+                .sorted()
+                .toList();
+    }
+
+    public static List<String> toBinaryIntegerList(List<Integer> integerList) {
+        return integerList.stream()
+                .map(Integer::toBinaryString)
+                .toList();
+    }
+
+    public static List<String> getFilterAlfavitStringList(List<String> stringList, String alfavit) {
+        Set<Character> characterSet = alfavit.chars()
+                .mapToObj(c -> (char)c)
+                .collect(Collectors.toSet());
+        return stringList.stream()
+                .filter(s -> s.chars()
+                        .allMatch(c -> characterSet.contains((char) c)))
+                .sorted(Comparator.comparingInt(String::length))
+                .toList();
     }
 }
 
