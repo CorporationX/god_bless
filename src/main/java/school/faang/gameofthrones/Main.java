cf950d21332e7e4287bbe007696d8eb177cdc57a
“Andrey <aiegorovceo@yandex.ru>
2024-10-14 23:34:19 +0300

    Implement basic structure for Main class with HashMap to store houses

---
+++ src/main/java/school/faang/gameofthrones/Main.java
@@ -0,0 +1,15 @@
+package school.faang.gameofthrones;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        // Create a HashMap to store houses and their sigils
+        Map<String, House> houses = new HashMap<>();
+
+        // Add some initial houses
+        houses.put("Stark", new House("Stark", "A grey direwolf on a white field"));
+        houses.put("Lannister", new House("Lannister", "A golden lion on a crimson field"));
+        houses.put("Baratheon", new House("Baratheon", "A crowned black stag on a gold field"));
+        houses.put("Targaryen", new House("Targaryen", "A red three-headed dragon on a black field"));
