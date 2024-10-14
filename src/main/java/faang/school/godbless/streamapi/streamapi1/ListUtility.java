5b23e0932af9f18499c7d9a7f8ea74fbf19bf5ca
Ivan Semikov <semikov.exf@gmail.com>
2024-10-14 19:00:37 +0300

    BJS2-35613 fix orElse(), orElseThrow()

--- src/main/java/faang/school/godbless/streamapi/streamapi1/ListUtility.java
+++ src/main/java/faang/school/godbless/streamapi/streamapi1/ListUtility.java
@@ -4,6 +4,7 @@ import lombok.NonNull;
 
 import java.util.Comparator;
 import java.util.List;
+import java.util.NoSuchElementException;
 import java.util.function.Predicate;
 
 public class ListUtility {
@@ -11,38 +12,57 @@ public class ListUtility {
     }
 
     public static int sumEvenNumbers(@NonNull List<Integer> numbers) {
-        return numbers.stream().filter(n -> n != null && n % 2 == 0).reduce(0, Integer::sum);
+        return numbers.stream()
+                .filter(n -> n != null && n % 2 == 0)
+                .reduce(0, Integer::sum);
     }
 
     public static int findMax(@NonNull List<Integer> numbers) {
-        return numbers.stream().max(Integer::compareTo).orElseThrow();
+        return numbers.stream()
+                .max(Integer::compareTo)
+                .orElseThrow(() -> new NoSuchElementException("List is empty"));
     }
 
     public static double findAvg(@NonNull List<Integer> numbers) {
-        return numbers.stream().mapToDouble(Integer::doubleValue).average().orElse(0);
+        return numbers.stream()
+                .mapToDouble(Integer::doubleValue)
+                .average()
+                .orElseThrow(() -> new NoSuchElementException("List is empty"));
     }
 
     public static int countStringStartWith(@NonNull List<String> strings, char symbol) {
-        return (int) strings.stream().filter(s -> s != null && s.length() > 1 && s.charAt(0) == symbol).count();
+        return (int) strings.stream()
+                .filter(s -> s != null && s.length() > 1 && s.charAt(0) == symbol)
+                .count();
     }
 
     public static List<String> filterStringsContainsSubstring(@NonNull List<String> strings, @NonNull String substring) {
-        return strings.stream().filter(s -> s != null && s.contains(substring)).toList();
+        return strings.stream()
+                .filter(s -> s != null && s.contains(substring))
+                .toList();
     }
 
     public static List<String> sortByLength(@NonNull List<String> strings) {
-        return strings.stream().sorted(Comparator.comparingInt(s -> s != null ? s.length() : -1)).toList();
+        return strings.stream()
+                .sorted(Comparator.comparingInt(s -> s != null ? s.length() : -1))
+                .toList();
     }
 
     public static boolean allMatchCondition(@NonNull List<Integer> numbers, Predicate<Integer> condition) {
-        return numbers.stream().allMatch(condition);
+        return numbers.stream()
+                .allMatch(condition);
     }
 
     public static int findMinNumberGreaterThan(@NonNull List<Integer> numbers, int borderNumber) {
-        return numbers.stream().filter(n -> n != null && n > borderNumber).min(Integer::compareTo).orElseThrow();
+        return numbers.stream()
+                .filter(n -> n != null && n > borderNumber)
+                .min(Integer::compareTo)
+                .orElseThrow(() -> new NoSuchElementException("Element not found by filter condition"));
     }
 
     public static List<Integer> stringsToLengths(@NonNull List<String> strings) {
-        return strings.stream().map(s -> s == null ? null : s.length()).toList();
+        return strings.stream()
+                .map(s -> s == null ? null : s.length())
+                .toList();
     }
 }
