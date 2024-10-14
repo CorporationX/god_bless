69fff4cfb3925ef2e1844e8a1e028b4b254a91c4
Maxim Gorobets <gorobets.maks@bk.ru>
2024-10-14 21:30:52 +1000

    +@EqualsAndHashCode

--- src/main/java/school/faang/BJS2_35306_RecomendationSystem/UserProfile.java
+++ src/main/java/school/faang/BJS2_35306_RecomendationSystem/UserProfile.java
@@ -1,10 +1,12 @@
 package school.faang.BJS2_35306_RecomendationSystem;
 
 import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
 import lombok.Getter;
 
 import java.util.List;
 
+@EqualsAndHashCode
 @Getter
 @AllArgsConstructor
 public class UserProfile {
