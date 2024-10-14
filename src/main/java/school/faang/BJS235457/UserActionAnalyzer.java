e0f19754d9e540f994dc8a9630cdf74948fd81f7
Slava <leeteso97@gmail.com>
2024-10-14 11:24:24 +0500

    Добавил  оставшиеся методы

--- src/main/java/school/faang/BJS235457/UserActionAnalyzer.java
+++ src/main/java/school/faang/BJS235457/UserActionAnalyzer.java
@@ -1,26 +1,58 @@
 package school.faang.BJS235457;
 
+import java.time.YearMonth;
+import java.util.Arrays;
 import java.util.List;
 import java.util.Map;
+import java.util.function.Function;
 import java.util.stream.Collectors;
 
 public class UserActionAnalyzer {
     public static List<String> topActiveUsers(List<UserAction> actions) {
+        int limitSize = 10;
         return actions.stream()
                 .collect(Collectors.groupingBy(UserAction::getName, Collectors.counting()))
                 .entrySet().stream()
                 .sorted(Map.Entry.<String, Long>comparingByValue().reversed())
-                .limit(10)
+                .limit(limitSize)
                 .map(Map.Entry::getKey)
                 .collect(Collectors.toList());
     }
 
     public static List<String> topPopularHashtags(List<UserAction> actions) {
+        int limitSize = 5;
+        return actions.stream()
+                .filter(a -> a.getContent().contains("#"))
+                .flatMap(a -> Arrays.stream(a.getContent().split("\\s+")))
+                .filter(a -> a.startsWith("#"))
+                .collect(Collectors.groupingBy(Function.identity(), Collectors.counting()))
+                .entrySet().stream()
+                .sorted(Map.Entry.<String, Long>comparingByValue().reversed())
+                .limit(limitSize)
+                .map(Map.Entry::getKey)
+                .collect(Collectors.toList());
     }
 
     public static List<String> topCommentersLastMonth(List<UserAction> actions) {
+        int limitSize = 3;
+        return actions.stream()
+                .filter(a -> a.getActionType().equals("comment") && YearMonth.now().isAfter(YearMonth.from(a.getDate())))
+                .collect(Collectors.groupingBy(UserAction::getName, Collectors.counting()))
+                .entrySet().stream()
+                .sorted(Map.Entry.<String, Long>comparingByValue().reversed())
+                .limit(limitSize)
+                .map(Map.Entry::getKey)
+                .collect(Collectors.toList());
     }
 
     public static Map<String, Double> actionTypePercentages(List<UserAction> actions) {
+        double percent = 100;
+        return actions.stream()
+                .collect(Collectors.groupingBy(UserAction::getActionType, Collectors.counting()))
+                .entrySet().stream()
+                .collect(Collectors.toMap(
+                        Map.Entry::getKey,
+                        entry -> (entry.getValue() * percent) / actions.size()
+                ));
     }
 }
