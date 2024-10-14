5cfa129f473e6245f9e43be4a440e09020bb9b0d
Andrew Balashov <balashov83@gmail.com>
2024-10-14 17:12:49 +0300

    BJS2-35867 Heroes of M&M: task done

---
+++ src/main/java/school/faang/m1s3/bjs2_35867_homm/Creature.java
@@ -0,0 +1,10 @@
+package school.faang.m1s3.bjs2_35867_homm;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@AllArgsConstructor
+@Data
+public abstract class Creature {
+    private int power;
+}
---
+++ src/main/java/school/faang/m1s3/bjs2_35867_homm/Swordsman.java
@@ -0,0 +1,9 @@
+package school.faang.m1s3.bjs2_35867_homm;
+
+
+public class Swordsman extends Creature{
+
+    public Swordsman(int power) {
+        super(power);
+    }
+}
