a2e7e4bfb709556139c876dab66c249f698c7a52
Nikolay Orlenko <norlenko2801@gmail.com>
2024-10-15 18:39:30 +0300

    Вы работаете в Microsoft!

---
+++ src/main/java/school/faang/microsoft/outlook/MailSender.java
@@ -0,0 +1,22 @@
+package school.faang.microsoft.outlook;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MailSender {
+    private static final int BATCH_SIZE = 200;
+
+    public static void main(String[] args) throws InterruptedException {
+        List<Thread> threads = new ArrayList<>();
+        for (int i = 0; i < 1000; i += BATCH_SIZE) {
+            Thread thread = new Thread(new SenderRunnable(i, i + BATCH_SIZE));
+            thread.start();
+            threads.add(thread);
+        }
+
+        for (Thread thread : threads) {
+            thread.join();
+        }
+        System.out.println("Отправка писем завершена успешно!");
+    }
+}
