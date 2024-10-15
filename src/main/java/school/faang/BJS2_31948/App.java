a8e8a6caf8e576a6b920675a767bdd1cb227bd40
UrmatBolotbek <urmar9797@gmail.com>
2024-10-15 21:40:12 +0200

    change the folder hierarchy

--- src/main/java/school/faang/BJS2_31948/App.java
+++
@@ -1,7 +0,0 @@
-package school.faang.BJS2_31948;
-
-public class App {
-    public static void main(String[] args) {
-        System.out.println("Открытие магазина ожидается через 4 дня");
-    }
-}
