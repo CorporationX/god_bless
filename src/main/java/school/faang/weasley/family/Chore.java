a5d633732816e3e800732532dba7e268e3b672e6
Nikolay Orlenko <norlenko2801@gmail.com>
2024-10-15 21:14:26 +0300

    They were very nice but extremely poor

---
+++ src/main/java/school/faang/weasley/family/Chore.java
@@ -0,0 +1,21 @@
+package school.faang.weasley.family;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class Chore implements Runnable {
+    private String chore;
+
+    @Override
+    public void run() {
+        System.out.println(Thread.currentThread().getName() + ": " + chore);
+        try {
+            Thread.sleep(1000);
+        } catch (InterruptedException e) {
+            System.out.println("Записываем сообщение в логи" + e.getMessage());
+        }
+
+    }
+}
