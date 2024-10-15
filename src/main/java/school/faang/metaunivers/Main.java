0999d7eeb0c7a015726c0e41dc757261b3c88c0e
“Andrey <aiegorovceo@yandex.ru>
2024-10-15 19:51:23 +0300

    Create Main class to demonstrate the usage of NotificationManager

---
+++ src/main/java/school/faang/metaunivers/Main.java
@@ -0,0 +1,8 @@
+package school.faang.metaunivers;
+
+public class Main {
+    public static void main(String[] args) {
+        NotificationManager notificationManager = new NotificationManager();
+    }
+}
+
