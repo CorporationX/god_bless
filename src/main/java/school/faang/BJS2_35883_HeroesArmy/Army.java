ef0442dae6aec3ab33c3cc878e4010e79439eb39
Maxim Gorobets <gorobets.maks@bk.ru>
2024-10-15 23:34:03 +1000

    new Army class to calculate total power of army

---
+++ src/main/java/school/faang/BJS2_35883_HeroesArmy/Army.java
@@ -0,0 +1,38 @@
+package school.faang.BJS2_35883_HeroesArmy;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.atomic.AtomicInteger;
+
+public class Army {
+    private List<Hero> heroes;
+
+    public Army() {
+        heroes = new ArrayList<>();
+    }
+
+    public void addUnit(Hero hero) {
+        heroes.add(hero);
+    }
+
+    public int calculateTotalPower() {
+        AtomicInteger totalPower = new AtomicInteger(0);
+        List<Thread> startedThreads = new ArrayList<>();
+
+        heroes.forEach(hero -> {
+            Thread threadForHero = new Thread(new PowerCalculator(hero, totalPower));
+            startedThreads.add(threadForHero);
+            threadForHero.start();
+        });
+
+        startedThreads.forEach(thread -> {
+            try {
+                thread.join();
+            } catch (InterruptedException e) {
+                System.out.println(e.getMessage());
+            }
+        });
+
+        return totalPower.get();
+    }
+}
