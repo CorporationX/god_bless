bd68a16129aa173f74602240b796bbf5f321196a
Oleksii Babaikin <a.babaykin@gmail.com>
2024-10-16 01:03:35 +0200

    BJS2-35678 Потренируем StreamAPI 1 Создан класс Main

--- src/main/java/school/faang/lambda/SpellCaster.java
+++
@@ -1,7 +0,0 @@
-package school.faang.lambda;
-
-public class SpellCaster {
-    public String cast(String phraseSC, SpellAction spellAction){
-        return spellAction.transferPhraseToAction(phraseSC);
-    }
-}
---
+++ src/main/java/school/faang/lambda/streamapi1/Main.java
@@ -0,0 +1,4 @@
+package school.faang.lambda.streamapi1;
+
+public class Main {
+}
