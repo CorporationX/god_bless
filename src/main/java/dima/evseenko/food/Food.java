5b25288aea13ae369c1f7efb09e8bb273d9c0526
Dmytro Yevsieienko <youdima70@gmail.com>
2024-10-15 19:42:45 +0200

    Mice are very nice!

---
+++ src/main/java/dima/evseenko/food/Food.java
@@ -0,0 +1,10 @@
+package dima.evseenko.food;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class Food {
+    private String name;
+}
---
+++ src/main/java/dima/evseenko/food/Main.java
@@ -0,0 +1,64 @@
+package dima.evseenko.food;
+
+import java.util.List;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
+
+public class Main {
+    public static void main(String[] args) {
+        House house = new House();
+        house.getRooms().addAll(getRooms());
+
+        ScheduledExecutorService executorService = Executors.newScheduledThreadPool(5);
+
+        for (int i = 0; i < 5; i++) {
+            executorService.scheduleAtFixedRate(house::collectFood, 0, 30, TimeUnit.SECONDS);
+        }
+
+        executorService.shutdown();
+
+        while (true) {
+            if (executorService.isTerminated()) {
+                System.out.println("Еда в доме собрана!");
+                break;
+            }
+        }
+    }
+
+    private static List<Room> getRooms() {
+        Room room1 = new Room();
+        room1.getFoods().addAll(List.of(
+                new Food("Сыр"),
+                new Food("Колбаса"),
+                new Food("Майонез"),
+                new Food("Хлеб")
+        ));
+
+        Room room2 = new Room();
+        room2.getFoods().addAll(List.of(
+                new Food("Чай"),
+                new Food("Лосось"),
+                new Food("Творог"),
+                new Food("Арбуз")
+        ));
+
+        Room room3 = new Room();
+        room3.getFoods().addAll(List.of(
+                new Food("Шаурма"),
+                new Food("Молоко"),
+                new Food("Лук"),
+                new Food("Чеснок")
+        ));
+
+        Room room4 = new Room();
+        room4.getFoods().addAll(List.of(
+                new Food("Торт"),
+                new Food("Шоколад"),
+                new Food("Кофе"),
+                new Food("Тунец")
+        ));
+
+        return List.of(room1, room2, room3);
+    }
+}
