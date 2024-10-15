51ed2b02840ef087757b52c9dd2e6d341e162920
Matvey <mashptz@gmail.com>
2024-10-15 19:55:34 +0300

    Создание потоков Распределение нагрузки

---
+++ src/main/java/school/faang/sender/SenderRunnable.java
@@ -0,0 +1,19 @@
+package school.faang.sender;
+
+public class SenderRunnable implements Runnable {
+    private int startIndex;
+    private int endIndex;
+
+    public SenderRunnable(int startIndex, int endIndex) {
+        this.startIndex = startIndex;
+        this.endIndex = endIndex;
+    }
+
+    @Override
+    public void run() {
+        for (int i = startIndex; i < endIndex; i ++) {
+            System.out.println("Письмо отправлено: " + i);
+        }
+    }
+}
+
