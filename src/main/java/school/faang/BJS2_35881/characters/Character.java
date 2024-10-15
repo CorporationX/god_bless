544e646f77e85d93ab8a99e3c16c9329caed6718
UrmatBolotbek <urmar9797@gmail.com>
2024-10-15 16:21:04 +0200

    complete the task "Распределенная армия героев меча и магии" " BJS2-35881

---
+++ src/main/java/school/faang/BJS2_35881/characters/Character.java
@@ -0,0 +1,11 @@
+package school.faang.BJS2_35881.characters;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public abstract class Character {
+    private int power;
+    private String type;
+}
