aac7dad6eadcc4a3c75f3375717d0fbcff8e7860
Gadzhiev Gadzhi <Gadzhiev3222@gmail.com>
2024-10-15 08:33:49 +0300

    Железный трон любой ценой

--- src/main/java/school/faang/theirontronatanycost/maincode/House.java
+++ src/main/java/school/faang/theirontronatanycost/maincode/House.java
@@ -1,12 +1,28 @@
 package school.faang.theirontronatanycost.maincode;
 
-import lombok.AllArgsConstructor;
+import lombok.Getter;
 
 import java.util.ArrayList;
 import java.util.List;
 
-@AllArgsConstructor
+@Getter
 public class House {
-    private List<String> roles = new ArrayList<>();
+    private List<String> roles;
     private int rolesCount;
+
+    public House(List<String> roles) {
+        this.roles = new ArrayList<>(roles);
+        this.rolesCount = roles.size();
+    }
+
+    public synchronized void addRole(String role) {
+        roles.remove(role);
+        rolesCount--;
+    }
+
+    public synchronized void removeRole(String role) {
+        roles.add(role);
+        rolesCount++;
+        this.notifyAll();
+    }
 }
--- src/main/java/school/faang/theirontronatanycost/maincode/User.java
+++ src/main/java/school/faang/theirontronatanycost/maincode/User.java
@@ -2,10 +2,32 @@ package school.faang.theirontronatanycost.maincode;
 
 public class User {
     private String name;
-    private String house;
     private String role;
 
-    public void joinHouse(House house) {
+    public User(String name, String role) {
+        this.name = name;
+        this.role = role;
+    }
 
+    public void joinHouse(House house) {
+        synchronized (house) {
+            try {
+                while (house.getRolesCount() == 0) {
+                    System.out.println(name + "ждет роль");
+                    house.wait();
+                }
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            house.addRole(this.role);
+            System.out.println(name + " занял роль " + role);
+        }
+    }
+
+    public void leaveHouse(House house) {
+        synchronized (house) {
+            house.removeRole(this.role);
+            System.out.println(name + "освободил роль " + role);
+        }
     }
 }
