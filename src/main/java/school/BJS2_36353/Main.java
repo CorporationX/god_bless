7b33d9d4f5e8ac6f680724d98a6e075a57f27381
Roman Nifontov <romannif@mail.ru>
2024-10-15 20:13:49 +0500

    Сделал изменения в задаче BJS2-36353

--- src/main/java/school/BJS2_36353/Main.java
+++ src/main/java/school/BJS2_36353/Main.java
@@ -4,17 +4,17 @@ public class Main {
 
     public static void main(String[] args) throws InterruptedException {
 
-        Pig1Thread Nif_Nif = new Pig1Thread("Ниф-Ниф","Солома");
-        Pig2Thread Naf_Naf = new Pig2Thread("Наф-Наф","Кирпичи");
-        Pig3Thread Nuf_Nuf = new Pig3Thread("Нуф-Нуф","Палки");
+        Pig1Thread nif_Nif = new Pig1Thread("Ниф-Ниф","Солома");
+        Pig2Thread naf_Naf = new Pig2Thread("Наф-Наф","Кирпичи");
+        Pig3Thread nuf_Nuf = new Pig3Thread("Нуф-Нуф","Палки");
 
-        Nif_Nif.start();
-        Nuf_Nuf.start();
-        Naf_Naf.start();
+        nif_Nif.start();
+        nuf_Nuf.start();
+        naf_Naf.start();
 
-        Nif_Nif.join();
-        Naf_Naf.join();
-        Nuf_Nuf.join();
+        nif_Nif.join();
+        naf_Naf.join();
+        nuf_Nuf.join();
         System.out.println("Игра завершена");
     }
 }
