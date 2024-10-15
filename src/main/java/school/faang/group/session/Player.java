917d449c94583bdc46a8a86aeda971b188cf4991
Matvey <mashptz@gmail.com>
2024-10-15 19:27:54 +0300

    ...

---
+++ src/main/java/school/faang/group/session/Player.java
@@ -0,0 +1,30 @@
+package school.faang.group.session;
+
+import lombok.Data;
+
+import java.security.PublicKey;
+
+@Data
+public class Player {
+    private boolean lock;
+    private boolean isPlaying;
+
+    public Player() {
+    }
+
+    public void play(){
+
+    }
+
+    public void pause(){
+
+    }
+
+    public void skip(){
+
+    }
+
+    public void previous(){
+
+    }
+}
