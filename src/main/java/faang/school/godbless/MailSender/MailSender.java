5e257f82d25471c716e2558e0ad3e944a51a531f
Alina44444 <alina.valiyeva.s@mail.ru>
2024-10-16 01:19:45 +0500

    Задача Вы работаете в Microsoft!

--- src/main/java/faang/school/godbless/MailSender/MailSender.java
+++ src/main/java/faang/school/godbless/MailSender/MailSender.java
@@ -7,11 +7,11 @@ import java.util.List;
 public class MailSender {
     public static void main(String[] args) {
         List<Thread> listThread = new ArrayList<>(Arrays.asList(
-                new Thread(new SenderRunnable(1, 100)),
-                new Thread(new SenderRunnable(100, 200)),
-                new Thread(new SenderRunnable(200, 300)),
-                new Thread(new SenderRunnable(300, 400)),
-                new Thread(new SenderRunnable(500, 600)))
+                new Thread(new SenderRunnable(1, 200)),
+                new Thread(new SenderRunnable(200, 400)),
+                new Thread(new SenderRunnable(400, 600)),
+                new Thread(new SenderRunnable(600, 800)),
+                new Thread(new SenderRunnable(800, 1000)))
         );
         for (Thread thread : listThread) {
             thread.start();
