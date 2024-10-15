c8073826a0f3d8d778846b26ed1555f3564f7762
Sanasar1 <almasyansb@gmail.com>
2024-10-16 00:44:33 +0300

    fix: minor fixes

--- src/main/java/bjs2_35730/MailSender.java
+++ src/main/java/bjs2_35730/MailSender.java
@@ -1,26 +1,31 @@
 package bjs2_35730;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class MailSender {
 
     public static void main(String[] args) throws InterruptedException {
-        Thread[] threads = new Thread[5];
+        List<Thread> threads = new ArrayList<>();
         int currThread = 0;
+        int totalMessages = 1000;
+        int messagesPerThread = 200;
 
-        for (int i = 1; i <= 1000; i++) {
-            if (i % 200 == 0) {
+        for (int i = 1; i <= totalMessages; i++) {
+            if (i % messagesPerThread == 0) {
                 int endIndex = i;
                 int startIndex = i - 199;
 
                 SenderRunnable senderRunnable = new SenderRunnable(startIndex, endIndex);
                 Thread thread = new Thread(senderRunnable);
-                threads[currThread] = thread;
-                threads[currThread].start();
+                threads.add(thread);
+                threads.get(currThread).start();
                 currThread++;
             }
         }
 
-        for (int i = 0; i < 5; i++) {
-            threads[i].join();
+        for (int i = 0; i < threads.size(); i++) {
+            threads.get(i).join();
         }
 
         System.out.println("All mails were successfully delivered!");
--- src/main/java/bjs2_35730/SenderRunnable.java
+++ src/main/java/bjs2_35730/SenderRunnable.java
@@ -4,8 +4,8 @@ import lombok.AllArgsConstructor;
 
 @AllArgsConstructor
 public class SenderRunnable implements Runnable {
-    int startIndex;
-    int endIndex;
+    private int startIndex;
+    private int endIndex;
 
     @Override
     public void run() {
