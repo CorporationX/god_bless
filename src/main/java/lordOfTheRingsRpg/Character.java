92bf57a8fd56ffbd30bd85fdbb351ca85338d67a
Pizdja <yakitayka@gmail.com>
2024-10-15 13:18:12 +0300

    lordOfTheRingsRpg

--- src/main/java/lordOfTheRingsRpg/Character.java
+++ src/main/java/lordOfTheRingsRpg/Character.java
@@ -13,9 +13,4 @@ public class Character {
     public void addItem(Item item) {
         inventory.add(item);
     }
-
-    public void removeItem(Item item) {
-        inventory.remove(item);
-    }
-
 }
--- src/main/java/lordOfTheRingsRpg/InventoryManager.java
+++ src/main/java/lordOfTheRingsRpg/InventoryManager.java
@@ -1,7 +1,5 @@
 package lordOfTheRingsRpg;
 
-import java.util.ArrayList;
-import java.util.List;
 import java.util.function.Consumer;
 import java.util.function.Function;
 import java.util.function.Predicate;
@@ -9,25 +7,18 @@ import java.util.function.Predicate;
 public class InventoryManager {
     public void addItem(Character character, Item item, Consumer<Item> action) {
         character.addItem(item);
+        action.accept(item);
     }
 
     public void removeItem(Character character,Predicate<Item> filter) {
-        List<Item> itemsForRemove = new ArrayList<>();
-
-        character.getInventory().forEach(item -> {
-            if(filter.test(item)) {
-                itemsForRemove.add(item);
-            }
-        });
-
-        character.getInventory().removeAll(itemsForRemove);
+        if(character.getInventory().stream().anyMatch(filter)) {
+            character.getInventory().removeIf(filter);
+        }
     }
 
     public void updateItem(Character character, Predicate<Item> filter, Function<Item, Item> action) {
-        character.getInventory().forEach(item -> {
-           if (filter.test(item)) {
-               action.apply(item);
-           }
-        });
+        character.getInventory().stream()
+                .filter(filter)
+                .map(action);
     }
 }
