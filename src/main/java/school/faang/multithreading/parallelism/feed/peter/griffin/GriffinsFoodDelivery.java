dd8a881a4aafcbeaef8a3a2b92fd681d7188a676
AVB <abdddev@gmail.com>
2024-10-15 15:29:27 +1000

    Накормите Питера Гриффина

---
+++ src/main/java/school/faang/multithreading/parallelism/feed/peter/griffin/GriffinsFoodDelivery.java
@@ -0,0 +1,21 @@
+package school.faang.multithreading.parallelism.feed.peter.griffin;
+
+import java.util.Random;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
+
+public class GriffinsFoodDelivery {
+    private final static int THREAD_POOL_SIZE = 3;
+
+    public static void main(String[] args) throws InterruptedException {
+        ExecutorService executor = Executors.newFixedThreadPool(THREAD_POOL_SIZE);
+        String[] characterNames = {"Peter", "Lois", "Meg", "Chris", "Stewie"};
+        for (String characterName : characterNames) {
+            int foodAmount = new Random().nextInt(100) + 1;
+            executor.submit(new FoodDeliveryTask(characterName, foodAmount));
+        }
+        executor.shutdown();
+        executor.awaitTermination(2, TimeUnit.MINUTES);
+    }
+}
