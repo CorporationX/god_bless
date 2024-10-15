c81aee480089d33974e63636a9ad4212d094aae2
“Andrey <aiegorovceo@yandex.ru>
2024-10-15 19:50:12 +0300

    Add registerHandler method to map notification types to handlers Add sendNotification method to process and dispatch notifications

--- src/main/java/school/faang/metaunivers/NotificationManager.java
+++ src/main/java/school/faang/metaunivers/NotificationManager.java
@@ -6,4 +6,17 @@ import java.util.function.Consumer;
 
 public class NotificationManager {
     private Map<String, Consumer<Notification>> handlers = new HashMap<>();
+
+    public void registerHandler(String type, Consumer<Notification> handler) {
+        handlers.put(type, handler);
+    }
+
+    public void sendNotification(Notification notification) {
+        Consumer<Notification> handler = handlers.get(notification.getType());
+        if (handler != null) {
+            handler.accept(notification);
+        } else {
+            System.out.println("No handler registered for notification type: " + notification.getType());
+        }
+    }
 }
