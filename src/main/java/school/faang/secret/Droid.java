4992a9d5adea0836cfd084159ecc2bc0ccc929e3
abzalkhan00 <abzalkhanondakanov@gmail.com>
2024-10-15 18:32:10 +0500

    refactored methods

--- src/main/java/school/faang/secret/Droid.java
+++ src/main/java/school/faang/secret/Droid.java
@@ -8,35 +8,28 @@ import lombok.Getter;
 public class Droid {
     private String name;
 
-    public String encryptMessage(String message, Integer key) {
-        DroidMessageEncryptor encryptor = (msg, encryptionKey) -> {
-            StringBuilder encryptedMessage = new StringBuilder();
-            for (Character c : msg.toCharArray()) {
-                if (Character.isLetter(c)) {
-                    char base = Character.isLowerCase(c) ? 'a' : 'A';
-                    encryptedMessage.append((char) ((c - base + encryptionKey) % 26 + base));
-                } else {
-                    encryptedMessage.append(c);
-                }
-            }
-            return encryptedMessage.toString();
-        };
-        return encryptor.encrypt(message, key);
+    private final int NUMBER_OF_ENGLISH_LETTERS = 26;
 
+    public String encryptMessage(String message, Integer key) {
+        return processMessage(message, key);
     }
 
     public String decryptMessage(String message, Integer key) {
-        DroidMessageEncryptor encryptor = (msg, decryptionKey) -> {
-            StringBuilder encryptedMessage = new StringBuilder();
+        return processMessage(message, -key);
+    }
+
+    private String processMessage(String message, Integer key) {
+        DroidMessageEncryptor encryptor = (msg, encryptionKey) -> {
+            StringBuilder result = new StringBuilder();
             for (Character c : msg.toCharArray()) {
                 if (Character.isLetter(c)) {
                     char base = Character.isLowerCase(c) ? 'a' : 'A';
-                    encryptedMessage.append((char) ((c - base - decryptionKey + 26) % 26 + base));
+                    result.append((char) ((c - base + encryptionKey + NUMBER_OF_ENGLISH_LETTERS) % NUMBER_OF_ENGLISH_LETTERS + base));
                 } else {
-                    encryptedMessage.append(c);
+                    result.append(c);
                 }
             }
-            return encryptedMessage.toString();
+            return result.toString();
         };
         return encryptor.encrypt(message, key);
     }
