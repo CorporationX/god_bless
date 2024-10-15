7d578b7b8d1b52916f0cd41d1b4b9a8ca2eaa403
Roman Shipilov <roman.shipilov@gmail.com>
2024-10-16 01:08:40 +0300

    task 2102: add message filtering

--- src/main/java/school/faang/task2102/Main.java
+++ src/main/java/school/faang/task2102/Main.java
@@ -7,30 +7,37 @@ public class Main {
     private static final String FAX_NOTIFICATION = "fax";
 
     public static void main(String[] args) {
-        NotificationManager notificationManager = createAndRegisterNotificationManager();
-
+        NotificationManager notificationManager = initializeNotificationManager();
         sendNotifications(notificationManager);
     }
 
-    private static NotificationManager createAndRegisterNotificationManager() {
+    private static NotificationManager initializeNotificationManager() {
         NotificationManager notificationManager = new NotificationManager();
-
-        notificationManager.registerHandler(EMAIL_NOTIFICATION,
-                notification -> System.out.println("Sending email notification: " + notification.getMessage()));
-
-        notificationManager.registerHandler(SMS_NOTIFICATION,
-                notification -> System.out.println("Sending SMS notification: " + notification.getMessage()));
-
-        notificationManager.registerHandler(PUSH_NOTIFICATION,
-                notification -> System.out.println("Sending push notification: " + notification.getMessage()));
-
+        registerHandlers(notificationManager);
+        registerFilters(notificationManager);
         return notificationManager;
     }
 
+    private static void registerHandlers(NotificationManager notificationManager) {
+        //TODO норм не ставить () в lambda, если параметр только один?
+        notificationManager.registerHandler(EMAIL_NOTIFICATION,
+                (notification) -> System.out.println("Sending email notification: " + notification.getMessage()));
+        notificationManager.registerHandler(SMS_NOTIFICATION,
+                notification -> System.out.println("Sending SMS notification: " + notification.getMessage()));
+        notificationManager.registerHandler(PUSH_NOTIFICATION,
+                notification -> System.out.println("Sending push notification: " + notification.getMessage()));
+    }
+
+    private static void registerFilters(NotificationManager notificationManager) {
+        notificationManager.registerFilter(EMAIL_NOTIFICATION, Filter::filterMessageContent);
+        notificationManager.registerFilter(SMS_NOTIFICATION, Filter::filterMessageContent);
+        notificationManager.registerFilter(PUSH_NOTIFICATION, Filter::filterMessageContent);
+    }
+
     private static void sendNotifications(NotificationManager notificationManager) {
-        notificationManager.sendNotification(new Notification(EMAIL_NOTIFICATION, "You've got a new email."));
-        notificationManager.sendNotification(new Notification(SMS_NOTIFICATION, "Your package has been delivered."));
-        notificationManager.sendNotification(new Notification(PUSH_NOTIFICATION, "New update available."));
+        notificationManager.sendNotification(new Notification(EMAIL_NOTIFICATION, "Hello, your password: mysecret123."));
+        notificationManager.sendNotification(new Notification(SMS_NOTIFICATION, "This is a bad word: niger."));
+        notificationManager.sendNotification(new Notification(PUSH_NOTIFICATION, "User used inappropriate word: shit."));
         notificationManager.sendNotification(new Notification(FAX_NOTIFICATION, "New fax"));
     }
 }
--- src/main/java/school/faang/task2102/NotificationManager.java
+++ src/main/java/school/faang/task2102/NotificationManager.java
@@ -3,21 +3,33 @@ package school.faang.task2102;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.function.Consumer;
+import java.util.function.Function;
 
 public class NotificationManager {
     private final Map<String, Consumer<Notification>> handlers = new HashMap<>();
+    private final Map<String, Function<Notification,Notification>> filters = new HashMap<>();
 
     public void registerHandler(String notificationType, Consumer<Notification> handler){
         handlers.put(notificationType, handler);
     }
+
+    public void registerFilter(String notificationType, Function<Notification,Notification> filter){
+        filters.put(notificationType, filter);
+    }
+
     public void sendNotification(Notification notification){
         if (notification == null) {
             System.out.println("Notification cannot be null");
             return;
         }
 
-        Consumer<Notification> handler = handlers.get(notification.getType());
+        //TODO: IDEA ругается "Reassigned parameter" на notification. Как делать правильно?
+        Function<Notification, Notification> filter = filters.get(notification.getType());
+        if (filter != null) {
+            notification = filter.apply(notification);
+        }
 
+        Consumer<Notification> handler = handlers.get(notification.getType());
         if (handler != null) {
             handler.accept(notification);
         } else {
