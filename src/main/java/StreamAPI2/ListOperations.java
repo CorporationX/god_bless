3fd30c9f153a46964827e42b59faa0ce44581396
infani <abrormukimov@gmail.com>
2024-10-15 14:37:00 +0500

    Create ListOperations Class

---
+++ src/main/java/StreamAPI2/ListOperations.java
@@ -0,0 +1,8 @@
+package StreamAPI2;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ListOperations {
+
+}
---
+++ src/main/java/StreamAPI2/Main.java
@@ -0,0 +1,19 @@
+package StreamAPI2;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) {
+        List<Integer> numbers = new ArrayList<>();
+        numbers.add(1);
+        numbers.add(2);
+        numbers.add(3);
+        numbers.add(4);
+        numbers.add(5);
+        numbers.add(6);
+
+        System.out.println((ListOperations.getPairs(numbers, 7)));
+    }
+}
