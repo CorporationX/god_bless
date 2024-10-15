8d150f9c355afe40c8cda8a13b63ed9d37fe0a9c
Nikolay Orlenko <norlenko2801@gmail.com>
2024-10-15 18:40:59 +0300

    Вынес количество писем в константу

--- src/main/java/school/faang/microsoft/outlook/MailSender.java
+++ src/main/java/school/faang/microsoft/outlook/MailSender.java
@@ -4,11 +4,12 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class MailSender {
+    private static final int LETTERS_COUNT = 1000;
     private static final int BATCH_SIZE = 200;
 
     public static void main(String[] args) throws InterruptedException {
         List<Thread> threads = new ArrayList<>();
-        for (int i = 0; i < 1000; i += BATCH_SIZE) {
+        for (int i = 0; i < LETTERS_COUNT; i += BATCH_SIZE) {
             Thread thread = new Thread(new SenderRunnable(i, i + BATCH_SIZE));
             thread.start();
             threads.add(thread);
