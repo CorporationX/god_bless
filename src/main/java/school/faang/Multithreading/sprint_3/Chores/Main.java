475e0c1aff3ec6a8e075fcc4e3bbb6e98dd98620
Naku <polevaya.nastena@internet.ru>
2024-10-15 16:38:56 +0300

    Naku-BJS2-36667 They were very nice but extremely poor

---
+++ src/main/java/school/faang/Multithreading/sprint_3/Chores/Main.java
@@ -0,0 +1,8 @@
+package school.faang.Multithreading.sprint_3.Chores;
+
+public class Main {
+    public static void main(String[] args) throws InterruptedException {
+        WeasleyFamily family = new WeasleyFamily();
+        family.doChore();
+    }
+}
