1b8d3ed765590020e38ab6b88ef1850b4c5ae631
Stanislaav Kozh <printks@yandex.ru>
2024-10-15 16:42:55 +0300

    Фильтры Instagram

---
+++ src/main/java/school/faang/BJS2_34892/Image.java
@@ -0,0 +1,14 @@
+package school.faang.BJS2_34892;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+class Image {
+
+    private String name;
+    private String description;
+}
