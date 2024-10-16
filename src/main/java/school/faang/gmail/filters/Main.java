d24dc899de2fe78f77e572ecb93c5ed9a31794f7
Andrey <maandr@internet.ru>
2024-10-16 22:26:20 +0300

    BJS2-33691 Gmail Rich Filters

---
+++ src/main/java/school/faang/gmail/filters/Main.java
@@ -0,0 +1,32 @@
+package school.faang.gmail.filters;
+
+import java.util.List;
+import java.util.Arrays;
+import java.util.function.Consumer;
+import java.util.function.Function;
+import java.util.function.Predicate;
+
+public class Main {
+    public static void main(String[] args) {
+        EmailProcessor emailProcessor = new EmailProcessor();
+
+        List<Email> emails = Arrays.asList(
+                new Email("Письмо 1", "Текст письма 1", false),
+                new Email("Письмо 2", "Текст письма 2", true),
+                new Email("Спам", "Текст спама", false)
+        );
+
+        Predicate<Email> importantFilter = email -> email.isImportant();
+        Predicate<Email> notImportantFilter = email -> !email.isImportant();
+
+        Consumer<Email> printEmail = email -> System.out.println("Обработано письмо: " + email.getSubject());
+
+        Function<Email, String> toUpperCase = email -> email.getBody().toUpperCase();
+        Function<Email, String> toLowerCase = email -> email.getBody().toLowerCase();
+
+        emailProcessor.processEmails(emails, importantFilter, printEmail, toUpperCase);
+        emailProcessor.processEmails(emails, notImportantFilter, printEmail, toLowerCase);
+
+        emails.forEach(email -> System.out.println("Тема: " + email.getSubject() + ", Тело письма: " + email.getBody()));
+    }
+}
