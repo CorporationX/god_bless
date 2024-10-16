bf7ae3a2ed65eaf87f42c5cc19901cd16faf90a7
Memoreks <akifyevalex1@gmail.com>
2024-10-16 15:19:16 +0700

    Внесенны изменения, согласно комментариям

--- src/main/java/school/faang/microsoft/MailSender.java
+++ src/main/java/school/faang/microsoft/MailSender.java
@@ -1,23 +1,27 @@
 package school.faang.microsoft;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class MailSender {
     public static void main(String[] args) {
         int totalMessages = 1000;
         int threadsCount = 5;
         int batchSize = totalMessages / threadsCount;
 
-        Thread[] arrayThread = new Thread[threadsCount];
+        List<Thread> threads = new ArrayList<>();
         for(int i = 0; i < threadsCount; i++) {
             Integer startIndex = batchSize * i;
             Integer endIndex = batchSize * (1 + i);
-            arrayThread[i] = new Thread(new SenderRunnable(startIndex, endIndex));
-            arrayThread[i].start();
+            threads.add(new Thread(new SenderRunnable(startIndex, endIndex)));
+            threads.get(i).start();
         }
 
-        for(Thread thread : arrayThread) {
+        for(Thread thread : threads) {
             try {
                 thread.join();
             } catch (InterruptedException e) {
-                throw new RuntimeException(e);
+                throw new UnsupportedOperationException("The main thread has stopped working", e);
             }
         }
 
