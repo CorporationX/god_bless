11eac15d006b144029675c20b5e02c09fcf97660
Viktor Krivetskiy <1woracc@gmail.com>
2024-10-17 00:00:53 +0300

    Распределенная армия героев меча и магии

--- src/main/java/heroes/army/Army.java
+++ src/main/java/heroes/army/Army.java
@@ -4,17 +4,10 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Army {
-    private List<Unit> unitList = new ArrayList<>();
-    List<Unit> archer = new ArrayList<>();
-    List<Unit> mag = new ArrayList<>();
-    List<Unit> swordman = new ArrayList<>();
+    private List<Unit> unitList;
 
     public Army(List<Unit> unitList) {
         this.unitList = unitList;
-    }
-
-    public void addUnit(Unit unit) {
-        unitList.add(unit);
     }
 
     public int calculateTotalPower() throws InterruptedException {
