1b63de974f950d705afec7406eb002bc2acb8711
UrmatBolotbek <urmar9797@gmail.com>
2024-10-17 00:08:37 +0200

    correct the code in accordance with the mentor's comments BJS2-37787

--- src/main/java/school/faang/BJS2_37787/MailSender.java
+++ src/main/java/school/faang/BJS2_37787/MailSender.java
@@ -1,40 +1,29 @@
 package school.faang.BJS2_37787;
 
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
-import java.util.concurrent.TimeUnit;
-
 public class MailSender {
     public static void main(String[] args) {
         int messages = 1000;
         int threadsCount = 5;
         int threadSize = messages / threadsCount;
 
-        ExecutorService executor = Executors.newFixedThreadPool(threadsCount);
+        Thread[] threads = new Thread[threadsCount];
 
-        try {
-            for (int i = 0; i < threadsCount; i++) {
-                int startIndex = i * threadSize;
-                int endIndex = (i == threadsCount - 1) ? messages : (i + 1) * threadSize;
+        for (int i = 0; i < threadsCount; i++) {
+            int startIndex = i * threadSize;
+            int endIndex = Math.min((i + 1) * threadSize, messages);
 
-                executor.execute(new SenderRunnable(startIndex, endIndex));
-            }
+            threads[i] = new Thread(new SenderRunnable(startIndex, endIndex));
+            threads[i].start();
+        }
 
-            executor.shutdown();
-            boolean terminated = executor.awaitTermination(Long.MAX_VALUE, TimeUnit.NANOSECONDS);
-
-            if (terminated) {
-                System.out.println("Все письма отправлены");
-            }
-
-        } catch (InterruptedException e) {
-            System.err.println("Произошло прерывание ожидания завершения потоков");
-            e.printStackTrace();
-        } finally {
-            if (!executor.isTerminated()) {
-                System.err.println("Принудительное завершение пула потоков");
-                executor.shutdownNow();
+        for (Thread thread : threads) {
+            try {
+                thread.join();
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
             }
         }
+
+        System.out.println("Все письма отправлены");
     }
 }
