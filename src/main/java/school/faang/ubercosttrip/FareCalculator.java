9b23e01c87fd727e255d7e89a4e6ab6b227bb389
“Andrey <aiegorovceo@yandex.ru>
2024-10-16 13:00:04 +0300

    refactor: Add Lombok @NoArgsConstructor to FareCalculator

--- src/main/java/school/faang/ubercosttrip/FareCalculator.java
+++ src/main/java/school/faang/ubercosttrip/FareCalculator.java
@@ -1,7 +1,10 @@
 package school.faang.ubercosttrip;
 
+import lombok.NoArgsConstructor;
+
 import java.util.function.BiFunction;
 
+@NoArgsConstructor
 public class FareCalculator {
     public double calculateFare(double distance, double time, BiFunction<Double, Double, Double> fareCalculation) {
         return fareCalculation.apply(distance, time);
