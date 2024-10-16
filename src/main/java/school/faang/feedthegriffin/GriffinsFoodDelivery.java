cf28b4534ab7ac1961ad34998c0e9e6870f0a7a0
Maks Russkikh <15maksim@mail.ru>
2024-10-16 21:30:52 +0300

    correction

--- src/main/java/school/faang/feedthegriffin/GriffinsFoodDelivery.java
+++ src/main/java/school/faang/feedthegriffin/GriffinsFoodDelivery.java
@@ -3,14 +3,20 @@ package school.faang.feedthegriffin;
 import java.util.Random;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
 
 public class GriffinsFoodDelivery {
     public static void main(String[] args) {
-        ExecutorService service = Executors.newFixedThreadPool(3);
-        String[] charactersName = {"Peter", "Lois", "Meg", "Chris", "Stewie"};
-        for (String name : charactersName) {
-            service.submit(new FoodDeliveryTask(name, new Random().nextInt(100)));
+        try {
+            ExecutorService service = Executors.newFixedThreadPool(3);
+            String[] charactersName = {"Peter", "Lois", "Meg", "Chris", "Stewie"};
+            for (String name : charactersName) {
+                service.submit(new FoodDeliveryTask(name, new Random().nextInt(100)));
+            }
+            service.awaitTermination(5, TimeUnit.SECONDS);
+            service.shutdown();
+        } catch (InterruptedException e) {
+            throw new IllegalStateException("Прервался метод main, в классе GriffinsFoodDelivery", e);
         }
-        service.shutdown();
     }
 }
