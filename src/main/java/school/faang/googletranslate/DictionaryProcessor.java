e583cddec854bf9ff233a83764e2656b049c6943
Memoreks <akifyevalex1@gmail.com>
2024-10-16 15:45:34 +0700

    Google Переводчик под нашим контролем…

---
+++ src/main/java/school/faang/googletranslate/DictionaryProcessor.java
@@ -0,0 +1,26 @@
+package school.faang.googletranslate;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.function.BiConsumer;
+
+public class DictionaryProcessor {
+
+    public void processWord(String word, String translation, BiConsumer<String, String> consumer) {
+        consumer.accept(word, translation);
+    }
+
+    public static void main(String[] args) {
+        DictionaryProcessor dictionaryProcessor = new DictionaryProcessor();
+
+        Map<String, String> dictionary = new HashMap<>();
+
+        BiConsumer<String, String> addWordToDictionary = (word, translation) -> dictionary.put(word, translation);
+
+        dictionaryProcessor.processWord("привет", "hello", addWordToDictionary);
+        dictionaryProcessor.processWord("мир", "world", addWordToDictionary);
+        dictionaryProcessor.processWord("программирование", "programming", addWordToDictionary);
+
+        System.out.println("Словарь: " + dictionary);
+    }
+}
