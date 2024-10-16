c2302fd80c533e3f19c4377794d97803a8534ba6
AlekseyB86nb <tigrxam@gmail.com>
2024-10-16 19:00:51 +0300

    реализация планирования запуска ракет в однопоточке

--- src/main/java/spaceX_36290/RocketLaunch.java
+++ src/main/java/spaceX_36290/RocketLaunch.java
@@ -2,6 +2,7 @@ package spaceX_36290;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.time.LocalDateTime;
 import java.util.List;
@@ -9,44 +10,54 @@ import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.TimeUnit;
 
-@Data
-@AllArgsConstructor
+@NoArgsConstructor
 public class RocketLaunch {
-    private final String name;
-    private final LocalDateTime launchTime;
+    private String name;
+    private int launchTime;
 
-    public static void main(String[] args) {
-
-        List<RocketLaunch> launches = List.of(
-                new RocketLaunch("Falcon 1", LocalDateTime.now().plusSeconds(2)),
-                new RocketLaunch("Falcon 9", LocalDateTime.now().plusSeconds(5)),
-                new RocketLaunch("Falcon Heavy", LocalDateTime.now().plusSeconds(10))
-        );
-
-        planRocketLaunches(launches);
+    public RocketLaunch(String name, int launchTime) {
+        this.name = name;
+        this.launchTime = launchTime;
     }
 
-    public Runnable launch() {
+
+    public void launch() {
         try {
-            int delay = 1000;
-            Thread.sleep(delay);
+            Thread.sleep(1000);
             System.out.println("Рокета " + name + " запускается!");
         } catch (InterruptedException e) {
             throw new RuntimeException(e);
         }
-        return null;
     }
 
-    public static void planRocketLaunches(List<RocketLaunch> launches){
+    public void planRocketLaunches(List<RocketLaunch> launches){
+        long start = System.currentTimeMillis();
         ExecutorService executorService = Executors.newSingleThreadExecutor();
-        for(RocketLaunch launch : launches){
-            try {
-                executorService.execute(launch.launch());
-                executorService.awaitTermination(launch.getLaunchTime().getSecond(), TimeUnit.SECONDS);
-            } catch (InterruptedException e) {
-                throw new RuntimeException(e);
-            }
+        for(RocketLaunch rocket : launches){
+                executorService.execute(() -> {
+                    try {
+                        Thread.sleep(rocket.launchTime);
+                    } catch (InterruptedException e) {
+                        throw new RuntimeException(e);
+                    }
+                    rocket.launch();
+                });
         }
+        executorService.shutdown();
+        try {
+            if(!executorService.awaitTermination(1, TimeUnit.MINUTES)){
+                System.out.println("Ракеты не все запустились!");
+                executorService.shutdownNow();
+            }
+        } catch (InterruptedException e) {
+            executorService.shutdownNow();
+            throw new RuntimeException(e);
+        }
+        long end = System.currentTimeMillis();
+        System.out.println("Время работы программы: " + (end - start) + " мс");
+
+
 
     }
 }
+
