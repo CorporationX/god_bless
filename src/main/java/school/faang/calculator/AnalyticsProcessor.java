c086c57ddc37b46ea3e0f1e5babc7f1856e8217e
“Andrey <aiegorovceo@yandex.ru>
2024-10-16 13:46:01 +0300

    feat: Implement calculate method in AnalyticsProcessor

---
+++ src/main/java/school/faang/calculator/AnalyticsProcessor.java
@@ -0,0 +1,20 @@
+package school.faang.calculator;
+
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@NoArgsConstructor
+public class AnalyticsProcessor {
+    public static int calculate(List<Integer> nums, Calculator calculator) throws IllegalArgumentException {
+        if (nums == null || nums.isEmpty()) {
+            throw new IllegalArgumentException("The list of numbers cannot be null or empty.");
+        }
+
+        int result = nums.get(0);
+        for (int i = 1; i < nums.size(); i++) {
+            result = calculator.operate(result, nums.get(i));
+        }
+        return result;
+    }
+}
