691355a65653f3b20cc678527438af22c21d4cae
Maks Russkikh <15maksim@mail.ru>
2024-10-16 21:04:14 +0300

    correction

--- src/main/java/school/faang/goodandbad/Chore.java
+++ src/main/java/school/faang/goodandbad/Chore.java
@@ -6,7 +6,7 @@ public record Chore(String chore) implements Runnable {
         try {
             Thread.sleep(1000);
         } catch (InterruptedException e) {
-            throw new RuntimeException(e);
+            throw new IllegalStateException("ошибка при вызове Thread.sleep(1000)", e);
         }
         System.out.println("Задача выполнена потоком: " + Thread.currentThread().getName());
     }
