e511b92713eecec3f3b05318c6d099ab289491be
“Andrey <aiegorovceo@yandex.ru>
2024-10-17 00:32:56 +0300

    feat: Add lambda functions for basic arithmetic operations

---
+++ src/main/java/school/faang/rememberschool/TriangleAreaCalculator.java
@@ -0,0 +1,15 @@
+package school.faang.rememberschool;
+
+import lombok.NoArgsConstructor;
+
+import java.util.function.Function;
+
+@NoArgsConstructor
+public class TriangleAreaCalculator {
+
+    Function<Double, Function<Double, Double>> add = (x) -> (y) -> x + y;
+    Function<Double, Function<Double, Double>> multiply = (x) -> (y) -> x * y;
+    Function<Double, Function<Double, Double>> subtract = (x) -> (y) -> x - y;
+    Function<Double, Function<Double, Double>> divide = (x) -> (y) -> x / y;
+    Function<Double, Double> squareRoot = (x) -> Math.sqrt(x);
+}
