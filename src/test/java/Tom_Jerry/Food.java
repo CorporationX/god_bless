cb12c8ee9d5c8db3e5bead13232e18e41c41e66a
Irina Krasnova <arisha.17@mail.ru>
2024-10-16 16:39:07 +0300

    Уборка комнат. Не смогла сделать правильно(  убирает только в 2х комнатах

---
+++ src/test/java/Tom_Jerry/Food.java
@@ -0,0 +1,20 @@
+package Tom_Jerry;
+
+public class Food {
+    private String food;
+
+    public Food(String food) {
+        this.food = food;
+    }
+
+    public String getFood() {
+        return food;
+    }
+
+    @Override
+    public String toString() {
+        return "Food{" +
+                "food='" + food + '\'' +
+                '}';
+    }
+}
