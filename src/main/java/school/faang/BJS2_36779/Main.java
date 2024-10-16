9041e21f0fbe3b0714082fe4ac738aa8712cd185
Matvey <mashptz@gmail.com>
2024-10-16 18:49:55 +0300

    .

--- src/main/java/school/faang/BJS2_36779/Main.java
+++ src/main/java/school/faang/BJS2_36779/Main.java
@@ -9,7 +9,7 @@ public class Main {
         chores.add(new Chore("Помыть пол", 25));
         chores.add(new Chore("Забрать палочку из магазина", 10));
         chores.add(new Chore("Приготовить обед", 5));
-        chores.add(new Chore("Найти коросту", 5));
+        chores.add(new Chore("Найти Коросту", 5));
 
         WeasleyFamily weasleyFamily = new WeasleyFamily(chores);
     }
