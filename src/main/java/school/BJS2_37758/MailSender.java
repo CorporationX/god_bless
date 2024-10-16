ea5c2920dac487535894fd06d0d4080edc741edd
Roman Nifontov <romannif@mail.ru>
2024-10-16 07:11:13 +0500

    Сделал изменения в задаче BJS2-37758

--- src/main/java/school/BJS2_37758/MailSender.java
+++ src/main/java/school/BJS2_37758/MailSender.java
@@ -8,11 +8,18 @@ public class MailSender {
         int countOfThreads = 5;
         int countOfEmailsPerThread = countOfEmails / countOfThreads;
 
+        Thread[] threads = new Thread[countOfThreads];
+
         for (int i = 0; i < countOfThreads; i++) {
             int start = i * countOfEmailsPerThread;
             int end = countOfEmailsPerThread * (i + 1);
-            Thread thread = new Thread(new SenderRunnable(start, end));
+            threads[i] = new Thread(new SenderRunnable(start, end));
+        }
+
+        for (Thread thread : threads) {
             thread.start();
+        }
+        for (Thread thread : threads) {
             thread.join();
         }
         System.out.println("Все письма отправлены");
