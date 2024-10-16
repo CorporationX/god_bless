e29b51f3dd4f5d3258c7763fc3c1014c8fc2a449
Slava <leeteso97@gmail.com>
2024-10-16 10:48:25 +0500

    Отрефакторил

--- src/main/java/school/faang/BJS237757/Main.java
+++ src/main/java/school/faang/BJS237757/Main.java
@@ -1,23 +1,20 @@
 package school.faang.BJS237757;
 
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
-import java.util.concurrent.TimeUnit;
-
 public class Main {
     public static void main(String[] args) throws InterruptedException {
         int totalMessages = 1000;
         int threadPools = 5;
         int batchSize = totalMessages / threadPools;
-        ExecutorService executorService = Executors.newFixedThreadPool(threadPools);
+        Thread[] threads = new Thread[threadPools];
         for (int i = 0; i < threadPools; i++) {
             int start = i * batchSize;
             int end = (i + 1) * batchSize;
-            executorService.submit(() -> new SenderRunnable(start, end).run());
+            threads[i] = new Thread(new SenderRunnable(start, end));
+            threads[i].start();
         }
-        if (!executorService.awaitTermination(threadPools, TimeUnit.SECONDS)) {
-            System.out.println("Все задачи выполнены!");
-            executorService.shutdown();
+        for (Thread thread : threads) {
+            thread.join();
         }
+        System.out.println("Все задачи выполнены!");
     }
 }
