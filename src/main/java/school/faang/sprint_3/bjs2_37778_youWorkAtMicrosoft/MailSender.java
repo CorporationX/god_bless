d59fb95d373c68fdbd6547f91002583d7b50c6c5
Dmitrii Pazinich <pazinich.d@yandex.ru>
2024-10-16 10:49:10 +0300

    Task "Вы работаете в Microsoft!" solution. Changes have been made according to the reviewer's comments.

--- src/main/java/school/faang/sprint_3/bjs2_37778_youWorkAtMicrosoft/MailSender.java
+++ src/main/java/school/faang/sprint_3/bjs2_37778_youWorkAtMicrosoft/MailSender.java
@@ -1,33 +1,29 @@
 package school.faang.sprint_3.bjs2_37778_youWorkAtMicrosoft;
 
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
-import java.util.concurrent.TimeUnit;
-
 public class MailSender {
     private static final int THREADS_COUNT = 5;
 
     public static void main(String[] args) {
-        int totalMessages = 1000000;
+        int totalMessages = 1000;
+
         int batchSize = totalMessages / THREADS_COUNT;
 
-        ExecutorService executor = Executors.newFixedThreadPool(THREADS_COUNT);
+        Thread[] threads = new Thread[THREADS_COUNT];
 
         for (int i = 0; i < THREADS_COUNT; i++) {
             int start = i * batchSize;
             int end = (i + 1) * batchSize;
-            executor.execute(new SenderRunnable(start, end));
+            threads[i] = new Thread(new SenderRunnable(start, end));
+            threads[i].start();
         }
 
-        executor.shutdown();
-
-        try {
-            if (!executor.awaitTermination(5, TimeUnit.SECONDS)) {
-                System.out.println("Программа завершена принудительно!");
-                executor.shutdownNow();
+        for (Thread thread : threads) {
+            try {
+                thread.join();
+            } catch (InterruptedException e) {
+                System.out.println("Поток был прерван во время ожидания завершения " + e.getMessage());
+                thread.interrupt();
             }
-        } catch (InterruptedException e) {
-            throw new RuntimeException(e);
         }
 
         System.out.println("Все письма отправлены!");
