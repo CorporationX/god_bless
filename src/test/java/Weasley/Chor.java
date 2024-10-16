d74c64747e5a593c0e77c60bed954ada3a867628
Irina Krasnova <arisha.17@mail.ru>
2024-10-16 09:22:08 +0300

    WeasleyFamili

---
+++ src/test/java/Weasley/Chor.java
@@ -0,0 +1,20 @@
+package Weasley;
+
+public class Chor implements Runnable {
+    private String task;
+
+    public Chor(String task) {
+        this.task = task;
+    }
+
+    public void run() {
+        try {
+            System.out.println(Thread.currentThread().getName() + " completes the task " + task);
+            Thread.sleep(5000);
+        } catch (InterruptedException e) {
+            System.out.println("the task " + task + " was interrupted");
+        }
+    }
+
+}
+
