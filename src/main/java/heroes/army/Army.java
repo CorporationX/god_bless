479a56ddab25a034a6917957ae1a4b93ad96478a
Viktor Krivetskiy <1woracc@gmail.com>
2024-10-16 23:58:06 +0300

    Распределенная армия героев меча и магии

--- src/main/java/heroes/army/Army.java
+++ src/main/java/heroes/army/Army.java
@@ -5,9 +5,9 @@ import java.util.List;
 
 public class Army {
     private List<Unit> unitList = new ArrayList<>();
-    List<Unit> archer  = new ArrayList<>();
-    List<Unit> mag  = new ArrayList<>();
-    List<Unit> swordman  = new ArrayList<>();
+    List<Unit> archer = new ArrayList<>();
+    List<Unit> mag = new ArrayList<>();
+    List<Unit> swordman = new ArrayList<>();
 
     public Army(List<Unit> unitList) {
         this.unitList = unitList;
@@ -17,12 +17,22 @@ public class Army {
         unitList.add(unit);
     }
 
-    public Integer calculateTotalPower(List<Unit> unitList) throws InterruptedException{
+    public int calculateTotalPower() throws InterruptedException {
+        int totalPower = 0;
         List<Thread> threadList = new ArrayList<>();
         List<PowerCalculator> powerCalculatorList = new ArrayList<>();
-        int totalPower = 0;
         for (Unit unit : unitList) {
-            if(Unit.)
+            PowerCalculator powerCalculator = new PowerCalculator(unit);
+            powerCalculatorList.add(powerCalculator);
+            Thread thread = new Thread(powerCalculator);
+            threadList.add(thread);
+            thread.start();
+        }
+        for (Thread thread : threadList) {
+            thread.join();
+        }
+        for (PowerCalculator powerCalculator : powerCalculatorList) {
+            totalPower += powerCalculator.getPower();
         }
         return totalPower;
     }
--- src/main/java/heroes/army/Main.java
+++ src/main/java/heroes/army/Main.java
@@ -1,16 +1,13 @@
 package heroes.army;
 
-import java.util.ArrayList;
 import java.util.List;
 
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
         List<Unit> unitList = List.of(new Archer(25), new Swordsman(40), new Mage(50),
                 new Archer(25), new Swordsman(40), new Mage(50),
                 new Archer(25), new Swordsman(40), new Mage(50));
         Army army = new Army(unitList);
-        System.out.println(army.calculateTotalPower(unitList));
-
-
+        System.out.println(army.calculateTotalPower());
     }
 }
