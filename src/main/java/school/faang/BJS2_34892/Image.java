fdf2e7c86d80f61e60c1f8e975cc48a1ed22fa2d
Stanislaav Kozh <printks@yandex.ru>
2024-10-16 10:54:38 +0300

    fix Фильтры Instagram

--- src/main/java/school/faang/BJS2_34892/Image.java
+++ src/main/java/school/faang/BJS2_34892/Image.java
@@ -1,14 +1,3 @@
 package school.faang.BJS2_34892;
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.Setter;
-
-@Getter
-@Setter
-@AllArgsConstructor
-class Image {
-
-    private String name;
-    private String description;
-}
+record Image(String name, String description) {}
