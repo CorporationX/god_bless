0133e716beb1fda1f22e8c2a1976b54fcd92ce44
Aleksandr Kuznetsov <adeskmath@gmail.com>
2024-10-17 01:32:37 +0300

    BJS2-36799

--- src/main/java/school/faang/multithreading/supercow/Player.java
+++ src/main/java/school/faang/multithreading/supercow/Player.java
@@ -1,5 +1,17 @@
 package school.faang.multithreading.supercow;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@AllArgsConstructor
+@Getter
 public class Player {
 
+  private final String name;
+
+  public void startBattle(Boss boss) throws InterruptedException {
+    boss.joinBattle(this);
+    Thread.sleep(2000);
+    boss.fightAndLeave(this);
+  }
 }
