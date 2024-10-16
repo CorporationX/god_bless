db76883ac3d10e190416715e5859c625d03d3f40
Dmytro Yevsieienko <youdima70@gmail.com>
2024-10-16 16:30:20 +0200

    Рефакторинг

--- src/main/java/dima/evseenko/tbbt/Task.java
+++ src/main/java/dima/evseenko/tbbt/Task.java
@@ -22,6 +22,7 @@ public class Task implements Runnable {
             }
         } catch (Exception e) {
             System.out.println(e.getMessage());
+            e.printStackTrace();
         }
     }
 }
