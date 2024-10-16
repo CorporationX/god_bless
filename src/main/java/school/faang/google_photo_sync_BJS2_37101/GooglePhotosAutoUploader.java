72af47f304621a9aef29bdf697a5d068d1b34240
VovaBuzivskyi <VovaBuzivskyi@gmail.com>
2024-10-16 19:09:41 +0700

    BJS2-37101 Google Photo sync

---
+++ src/main/java/school/faang/google_photo_sync_BJS2_37101/GooglePhotosAutoUploader.java
@@ -0,0 +1,36 @@
+package school.faang.google_photo_sync_BJS2_37101;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class GooglePhotosAutoUploader {
+    private final String lock = "lock";
+    private List<String> photosToUpload = new ArrayList<>();
+
+    public void startAutoUpload() {
+        synchronized (lock) {
+            while (photosToUpload.isEmpty()) {
+                try {
+                    System.out.println(Thread.currentThread().getName() + ": is waiting");
+                    lock.wait();
+                } catch (InterruptedException e) {
+                    System.out.println(e.getMessage());
+                }
+            }
+            uploadPhotos();
+        }
+    }
+
+    public void oneNewPhotoAdded(String photoPath) {
+        synchronized (lock) {
+            photosToUpload.add(photoPath);
+            lock.notify();
+            System.out.println(Thread.currentThread().getName() + ": added " + photoPath);
+        }
+    }
+
+    public void uploadPhotos() {
+        photosToUpload.forEach(s -> System.out.println(s + " has been uploaded"));
+        photosToUpload.clear();
+    }
+}
