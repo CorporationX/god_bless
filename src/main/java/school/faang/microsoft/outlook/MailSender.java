7afe9a154a10b93bae723ca375e17228572ed39a
Nikolay Orlenko <norlenko2801@gmail.com>
2024-10-17 00:43:19 +0300

    Правки к ревью - поправил логику обработки писем, теперь обработанные письма повторно не отправляются - добавил обработку исключения прямо в методе - теперь создаю список с изначально заданным размером, равному кол-ву потоков

--- src/main/java/school/faang/microsoft/outlook/MailSender.java
+++ src/main/java/school/faang/microsoft/outlook/MailSender.java
@@ -1,23 +1,32 @@
 package school.faang.microsoft.outlook;
 
+import lombok.extern.slf4j.Slf4j;
+
 import java.util.ArrayList;
 import java.util.List;
 
+@Slf4j
 public class MailSender {
     private static final int LETTERS_COUNT = 1000;
     private static final int BATCH_SIZE = 200;
 
-    public static void main(String[] args) throws InterruptedException {
-        List<Thread> threads = new ArrayList<>();
+    public static void main(String[] args) {
+        int threadCount = LETTERS_COUNT / BATCH_SIZE;
+        List<Thread> threads = new ArrayList<>(threadCount);
         for (int i = 0; i < LETTERS_COUNT; i += BATCH_SIZE) {
-            Thread thread = new Thread(new SenderRunnable(i, i + BATCH_SIZE));
+            Thread thread = new Thread(new SenderRunnable(i, i + BATCH_SIZE - 1));
             thread.start();
             threads.add(thread);
         }
 
-        for (Thread thread : threads) {
-            thread.join();
+        try {
+            for (Thread thread : threads) {
+                thread.join();
+            }
+            System.out.println("Отправка писем завершена успешно!");
+        } catch (InterruptedException e) {
+            log.error(e.getMessage());
+            e.printStackTrace();
         }
-        System.out.println("Отправка писем завершена успешно!");
     }
 }
