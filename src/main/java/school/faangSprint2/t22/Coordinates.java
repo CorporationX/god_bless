b986c1f4350d1da6958e99ebe02991cdb0255518
Igor <asachiyigor@gmail.com>
2024-10-16 23:11:06 +0300

    BJS2-33976
    initial commit

---
+++ src/main/java/school/faangSprint2/t22/Coordinates.java
@@ -0,0 +1,11 @@
+package school.faangSprint2.t22;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class Coordinates {
+    private int x;
+    private int y;
+}
---
+++ src/main/java/school/faangSprint2/t22/FlipDirection.java
@@ -0,0 +1,5 @@
+package school.faangSprint2.t22;
+
+public enum FlipDirection {
+    HORIZONTAL, VERTICAL
+}
