5dae4057a556d314dba8a8c26881ed2ad8b244d0
Ilya Ishanov <ilia.detroit2016@yandex.ru>
2024-10-17 02:01:42 +0500

    правки

--- src/main/java/school/faang/microsoft/MailSender.java
+++ src/main/java/school/faang/microsoft/MailSender.java
@@ -1,5 +1,7 @@
 package school.faang.microsoft;
 
+import java.util.Arrays;
+import java.util.List;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
@@ -8,24 +10,22 @@ public class MailSender {
         ExecutorService executorService = Executors.newFixedThreadPool(5);
         executorService.execute(new SenderRunnable(1, 200));
 
-        Thread firstThread = new Thread(new SenderRunnable(1, 200));
-        Thread secondThread = new Thread(new SenderRunnable(1, 200));
-        Thread thirdThread = new Thread(new SenderRunnable(1, 200));
-        Thread fourthThread = new Thread(new SenderRunnable(1, 200));
-        Thread fifthThread = new Thread(new SenderRunnable(1, 200));
+        List<Thread> threads = Arrays.asList(
+            new Thread(new SenderRunnable(1, 200)),
+            new Thread(new SenderRunnable(1, 200)),
+            new Thread(new SenderRunnable(1, 200)),
+            new Thread(new SenderRunnable(1, 200)),
+            new Thread(new SenderRunnable(1, 200))
+            );
 
-        firstThread.start();
-        secondThread.start();
-        thirdThread.start();
-        fourthThread.start();
-        fifthThread.start();
+        for (Thread thread : threads) {
+            thread.start();
+        }
 
         try {
-            firstThread.join();
-            secondThread.join();
-            thirdThread.join();
-            fourthThread.join();
-            fifthThread.join();
+            for (Thread thread : threads) {
+                thread.join();
+            }
             System.out.println("Все письма отправлены");
         } catch (InterruptedException e) {
             throw new RuntimeException(e);
