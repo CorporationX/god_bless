665c336165a489bcfd025221f538d52ebc3e7ddc
Andrey Kotlyarov <weiss14@yandex.ru>
2024-10-16 15:29:17 +0300

    Here we have a code for the task BJS2-37055

---
+++ src/main/java/school/faang/House.java
@@ -0,0 +1,35 @@
+package school.faang;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.w3c.dom.ls.LSOutput;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Getter
+@Setter
+public class House {
+    private String name;
+    private List<String> accessibleRoles;
+    private int quantityOfAccessibleRoles;
+
+    public House(String name, List<String> accessibleRoles) {
+        this.name = name;
+        this.accessibleRoles = new ArrayList<>(accessibleRoles);
+        quantityOfAccessibleRoles = accessibleRoles.size();
+    }
+
+    public void addRole(User user) {
+        this.quantityOfAccessibleRoles --;
+        this.accessibleRoles.remove(user.getPreferedRole());
+    }
+
+    public void removeRole(User user) {
+        quantityOfAccessibleRoles ++;
+        accessibleRoles.add(user.getTakedRole());
+        System.out.println("игрок "  + user.getName() + " освобождает роль " +
+                                     user.getTakedRole() + " в доме " + name);
+    }
+
+}
