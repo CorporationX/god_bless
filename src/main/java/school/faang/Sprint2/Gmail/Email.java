c13a9a24c2e7800be4588e48975d7007223b1a2c
Anatoly Galay <galay007@mail.ru>
2024-10-16 18:28:31 +0300

    task is done

---
+++ src/main/java/school/faang/Sprint2/Gmail/Email.java
@@ -0,0 +1,14 @@
+package school.faang.Sprint2.Gmail;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@AllArgsConstructor
+@Getter
+@Setter
+public class Email {
+    private String subject;
+    private String body;
+    boolean isImportant;
+}
