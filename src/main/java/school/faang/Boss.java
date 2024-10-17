899cf711827c0cc74361df283e26fc06c59db9e8
Andrey Kotlyarov <weiss14@yandex.ru>
2024-10-17 13:47:50 +0300

    Here we have a code for the task BJS2-36760

--- src/main/java/school/faang/Boss.java
+++ src/main/java/school/faang/Boss.java
@@ -8,30 +8,31 @@ import lombok.Setter;
 public class Boss {
     private int maxPlayers = 5;
     private int currentPlayers;
-
-    private int timeBetweenAttempts = 50;
+    private int timeBetweenAttempts = 150;
 
     public synchronized void joinBattle(Player player)
                                                  throws InterruptedException {
-
-        //вызывая этот метод, игрок пытается агрить на себя босса
-        //если слоты заняты, то поток того игрока, который вызывает этот метод,
-        //останавливается на заданное время и разблокирует доступ к боссу,
-        //чтобы потоки других игроков могли тоже встать в очередь
-        if (!(currentPlayers < maxPlayers)) {
-            System.out.println("Слышь, " + player.getName() +
-                    ", против босса и так сражается целая пати, " +
-                    "подожди, пока он кого-то грохнет");
-            this.wait(timeBetweenAttempts);
-
+        if (currentPlayers >= maxPlayers) {
+            while (currentPlayers >= maxPlayers) {
+                System.out.println("Слышь, " + player.getName() +
+                        ", против босса и так сражается целая пати, " +
+                        "подожди, пока он кого-то грохнет");
+                wait(timeBetweenAttempts);
+                System.out.println(player.getName() +
+                        " снова пытается агрить босса");
+            }
             System.out.println(player.getName() + " все-таки заагрил босса");
-        }
 
-        //а если слот есть, то поток игрока занимает его и выполняется дальше
-        else {
+        } else {
             currentPlayers ++;
             System.out.println(player.getName() + " заагрил босса и вступил в битву");
         }
+    }
+
+    public synchronized void leaveBattle(Player player) {
+        System.out.println(player.getName() + " расчленен и выпотрошен");
+        currentPlayers --;
+        this.notifyAll();
     }
 
 }
--- src/main/java/school/faang/Main.java
+++ src/main/java/school/faang/Main.java
@@ -5,33 +5,31 @@ import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
-        //создаем босса, создаем клан и задаем количество игроков,
-        // которые будут приняты в клан и отправлены на зачистку данжа
         Boss boss = new Boss();
         List<Player> clan = new ArrayList<>();
         int quantityOfPlayers = 30;
 
-        //создаем каждого игрока и принимаем в клан
         for (int i = 1; i <= quantityOfPlayers; i++) {
-            Player player = new Player("игрок номер " + i);
-            clan.add(player);
+            clan.add(new Player("игрок номер " + i));
         }
 
-        //запускаем игроков в данж, каждого в своем потоке
-        for (Player player : clan) {
-            Thread thread = new Thread(
-                    () -> {
-                        try {
-                            System.out.println(player.getName() +
-                                                              " зашел в данж");
-                            player.startBattle(boss);
-                        } catch (InterruptedException e) {
-                            throw new RuntimeException(e);
-                        }
-                    }
+        clan.stream()
+            .forEach(
+                x -> {
+                    Thread thread = new Thread(
+                            () -> {
+                                try {
+                                    System.out.println(x.getName() +
+                                            " зашел в данж");
+                                    x.startBattle(boss);
+                                } catch (InterruptedException e) {
+                                    throw new RuntimeException(e);
+                                }
+                            }
+                    );
+                    thread.start();
+                }
             );
-            thread.start();
-        }
     }
 
 }
