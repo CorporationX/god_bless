864f6a112eee15a38ed6d8528326859e25b49a21
Aleksandr Kuznetsov <adeskmath@gmail.com>
2024-10-17 10:43:14 +0300

    BJS2-37799 moved exception handling inside main method

--- src/main/java/school/faang/multithreading/microsoft/MailSender.java
+++ src/main/java/school/faang/multithreading/microsoft/MailSender.java
@@ -1,8 +1,11 @@
 package school.faang.multithreading.microsoft;
 
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
 public class MailSender {
 
-  public static void main(String[] args) throws InterruptedException {
+  public static void main(String[] args) {
 
     int totalMessages = 1000;
     int threadsCounts = 5;
@@ -18,7 +21,11 @@ public class MailSender {
     }
 
     for (Thread thread : threads) {
-      thread.join();
+      try {
+        thread.join();
+      } catch (InterruptedException | RuntimeException e) {
+        log.error("Caught exception args: {}", args, e);
+      }
     }
 
     System.out.println("All messages were sent");
