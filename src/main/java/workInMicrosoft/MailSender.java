e648b2d912bdbeac4e759000635c55930a77433d
Denis Levkhov <levkhovde@yandex.ru>
2024-10-17 16:53:55 +0300

    adding exception catch block

--- src/main/java/workInMicrosoft/MailSender.java
+++ src/main/java/workInMicrosoft/MailSender.java
@@ -5,17 +5,21 @@ public class MailSender {
     private static int threadAmount = 5;
     private static int batchSize = allMessage / threadAmount;
 
-    public static void main(String[] args) throws InterruptedException {
+    public static void main(String[] args) {
         Thread[] threads = new Thread[threadAmount];
         for (int i = 0; i < threadAmount; i++) {
             Thread thread = new Thread(new SenderRunnable((i * batchSize) + 1, ((i + 1) * batchSize)));
             threads[i] = thread;
             thread.start();
         }
-
-        for (Thread thread : threads) {
-            thread.join();
+        try {
+            for (Thread thread : threads) {
+                thread.join();
+            }
+        } catch (InterruptedException e) {
+            throw new IllegalStateException("join() method has not completed his work");
         }
+
         System.out.println("Все письма отправлены!");
     }
 }
