46127fac1254ed8f737b22f560c766f58554d690
Igor <asachiyigor@gmail.com>
2024-10-17 10:40:55 +0300

    BJS2-34868
    initial commit

---
+++ src/main/java/school/faangSprint2/t23/Employee.java
@@ -0,0 +1,10 @@
+package school.faangSprint2.t23;
+
+import lombok.AllArgsConstructor;
+
+@AllArgsConstructor
+public class Employee {
+    String name;
+    double salary;
+    String department;
+}
