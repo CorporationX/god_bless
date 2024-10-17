6a99c912e0225cb82ff718f53c7f0e25890faeaa
Dmitry Shakhaev <dmtry.shahaev@gmail.com>
2024-10-17 16:12:23 +0300

    Задача: Распределенная армия героев меча и магии. Доработка блока try-catch в классе Army.

--- src/main/java/school/faang/distributedarmy/Army.java
+++ src/main/java/school/faang/distributedarmy/Army.java
@@ -1,9 +1,13 @@
 package school.faang.distributedarmy;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import java.util.ArrayList;
 import java.util.List;
 
 public class Army {
+    private static final Logger log = LoggerFactory.getLogger(Army.class);
     private List<Unit> fighters = new ArrayList<>();
     private int resultPower = 0;
     private List<Calculator> calculators = new ArrayList<>();
@@ -22,7 +26,9 @@ public class Army {
             try {
                 calculator.join();
             } catch (InterruptedException e) {
-                throw new RuntimeException(e);
+                log.error("Caught exception: ", e);
+                e.printStackTrace();
+                throw new IllegalStateException("Thread was interrupted while waiting for calculator to finish.");
             }
             resultPower += calculator.getTotalPower();
         }
