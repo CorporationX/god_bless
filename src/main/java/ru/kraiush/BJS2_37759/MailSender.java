979b34c9a1d61602e84a7c5a3b170dd35c84c2f3
Ekaterina Badratdinova <ekaterinafeshch@gmail.com>
2024-10-17 15:41:09 -0700

    Вы работаете в Microsoft!: refactoring

--- src/main/java/ru/kraiush/BJS2_37759/MailSender.java
+++ src/main/java/ru/kraiush/BJS2_37759/MailSender.java
@@ -1,29 +1,23 @@
 package ru.kraiush.BJS2_37759;
 
 public class MailSender {
-
     public static void main(String[] args) {
+        int mailsQuantity = 1000;
+        int threadsQuantity = 5;
+        int batchSize = mailsQuantity / threadsQuantity;
 
-        Thread thread1 = new Thread(new SenderRunnable(1, 200));
-        Thread thread2 = new Thread(new SenderRunnable(201, 400));
-        Thread thread3 = new Thread(new SenderRunnable(401, 600));
-        Thread thread4 = new Thread(new SenderRunnable(601, 800));
-        Thread thread5 = new Thread(new SenderRunnable(801, 1000));
+        Thread[] threads = new Thread[threadsQuantity];
 
-        thread1.start();
-        thread2.start();
-        thread3.start();
-        thread4.start();
-        thread5.start();
-
-        try {
-            thread1.join();
-            thread2.join();
-            thread3.join();
-            thread4.join();
-            thread5.join();
-        } catch (InterruptedException e) {
-            e.printStackTrace();
+        for (int i = 0; i < threadsQuantity; ++i) {
+            int startIndex = i * batchSize;
+            int endIndex = (i + 1) * batchSize;
+            threads[i] = new Thread(new SenderRunnable(startIndex, endIndex));
+            threads[i].start();
+            try {
+                threads[i].join();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
         }
 
         System.out.println("All emails were sent!");
--- src/main/java/ru/kraiush/BJS2_37759/SenderRunnable.java
+++ src/main/java/ru/kraiush/BJS2_37759/SenderRunnable.java
@@ -10,7 +10,7 @@ public class SenderRunnable implements Runnable {
     private int endIndex;
     @Override
     public void run() {
-        for (int i = startIndex; i <= endIndex; i++) {
+        for (int i = startIndex; i < endIndex; i++) {
             System.out.println("The email #" + i + " was sent");
         }
     }
