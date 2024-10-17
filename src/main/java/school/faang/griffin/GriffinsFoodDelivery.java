76ab20623f7aed7daeb616fb95f59ee245120e0a
Olga Karaskova <okalughina@gmail.com>
2024-10-17 21:27:36 +0200

    Fixes for review comments

--- src/main/java/school/faang/griffin/GriffinsFoodDelivery.java
+++ src/main/java/school/faang/griffin/GriffinsFoodDelivery.java
@@ -9,9 +9,10 @@ public class GriffinsFoodDelivery {
         final int THREAD_PULL_COUNT = 3;
         String[] characterNames = {"Peter", "Lois", "Meg", "Chris", "Stewie"};
         ExecutorService executor = Executors.newFixedThreadPool(THREAD_PULL_COUNT);
+        Random random = new Random();
 
         for (String characterName : characterNames) {
-            FoodDeliveryTask foodTask = new FoodDeliveryTask(characterName, new Random().nextInt(19) + 1);
+            FoodDeliveryTask foodTask = new FoodDeliveryTask(characterName, random.nextInt(19) + 1);
             executor.submit(foodTask);
         }
         executor.shutdown();
