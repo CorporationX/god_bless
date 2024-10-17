e31b71fb98d14317ada2bf5873ac1fea6847bff5
ZhubanyshZh <zubanyszarylkasyn@gmail.com>
2024-10-17 14:57:59 +0500

    fix(you work in microsoft): optimized work in main
    added static methods optimizeSendingNotifications and startThreadList to in the will more comfortable

--- src/main/java/org/example/model/microsoft/SenderRunnable.java
+++ src/main/java/org/example/model/microsoft/SenderRunnable.java
@@ -2,6 +2,9 @@ package org.example.model.microsoft;
 
 import lombok.AllArgsConstructor;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @AllArgsConstructor
 public class SenderRunnable implements Runnable {
     private int startIndex;
@@ -12,5 +15,29 @@ public class SenderRunnable implements Runnable {
         for (int i = this.startIndex; i < endIndex; i++) {
             System.out.println(String.format("success sent %d!", i + 1));
         }
+    }
+
+    public static List<Thread> optimizeSendingNotifications(int notificationNumber, int threadsNumber) {
+        int numberOfMessageForOneThread = notificationNumber / threadsNumber;
+        List<Thread> threads = new ArrayList<>();
+
+        for (int i = 0; i < threadsNumber; i++) {
+            threads.add(new Thread(new SenderRunnable(numberOfMessageForOneThread * i, (i + 1) * numberOfMessageForOneThread)));
+        }
+
+        return threads;
+    }
+
+    public static void startThreadList(List<Thread> threads) {
+        threads.forEach(thread -> {
+            thread.start();
+            try {
+                thread.join();
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+        });
+
+        System.out.println("Success sent all mail notifications!");
     }
 }
--- src/main/java/org/example/service/microsoft/MailSender.java
+++ src/main/java/org/example/service/microsoft/MailSender.java
@@ -2,33 +2,11 @@ package org.example.service.microsoft;
 
 import org.example.model.microsoft.SenderRunnable;
 
+import java.util.List;
+
 public class MailSender {
     public static void main(String[] args) {
-        Thread firstThread = new Thread(new SenderRunnable(0, 200));
-        Thread secondThread = new Thread(new SenderRunnable(200, 400));
-        Thread thirdThread = new Thread(new SenderRunnable(400, 600));
-        Thread fourthThread = new Thread(new SenderRunnable(600, 800));
-        Thread fifthThread = new Thread(new SenderRunnable(800, 1000));
-
-        try {
-            firstThread.start();
-            firstThread.join();
-
-            secondThread.start();
-            secondThread.join();
-
-            thirdThread.start();
-            thirdThread.join();
-
-            fourthThread.start();
-            fourthThread.join();
-
-            fifthThread.start();
-            fifthThread.join();
-
-            System.out.println("Success sent all mail notifications!");
-        } catch (InterruptedException e) {
-            throw new RuntimeException(e);
-        }
+        List<Thread> threads = SenderRunnable.optimizeSendingNotifications(1000, 5);
+        SenderRunnable.startThreadList(threads);
     }
 }
