216bdf8bec71b6ee6c61f6ac55b01e7b902397fa
Memoreks <akifyevalex1@gmail.com>
2024-10-17 11:23:42 +0700

    Внесены изменения, согласно комментариям

--- src/main/java/school/faang/pitergrifin/GriffinsFoodDelivery.java
+++ src/main/java/school/faang/pitergrifin/GriffinsFoodDelivery.java
@@ -3,10 +3,12 @@ package school.faang.pitergrifin;
 import java.util.Random;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
 
 public class GriffinsFoodDelivery {
     private static final int THREAD_POOL_SIZE = 3;
     private static final int MAX_FOOD_COUNT = 50;
+    private static final int MAX_TIME_WAIT = 100;
 
     private static String[] characterNames = {"Peter", "Lois", "Meg", "Chris", "Stewie"};
 
@@ -18,5 +20,13 @@ public class GriffinsFoodDelivery {
         }
 
         executor.shutdown();
+
+        try {
+            if (executor.awaitTermination(MAX_TIME_WAIT, TimeUnit.SECONDS)) {
+                System.out.println("Работа программы завершена");
+            }
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
