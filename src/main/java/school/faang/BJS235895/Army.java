71060f1c94d3b0d41242db98278a834d163eb6ba
Doniyor Kurbanov <doniyor.kurbanov.24@gmail.com>
2024-10-17 21:34:43 +0500

    BJS2-35895. Жду от вас фитбека спасибо

--- src/main/java/school/faang/BJS235895/Army.java
+++ src/main/java/school/faang/BJS235895/Army.java
@@ -5,7 +5,7 @@ import java.util.List;
 import java.util.concurrent.atomic.AtomicInteger;
 
 public class Army {
-    public List<Warrior> units = new ArrayList<>();
+    private final List<Warrior> units = new ArrayList<>();
 
     public void addUnit(Warrior warrior) {
         units.add(warrior);
@@ -16,9 +16,9 @@ public class Army {
         units.forEach(warrior -> threads.add(new MyThreads(warrior)));
 
         AtomicInteger totalPower = new AtomicInteger();
+        threads.forEach(Thread::start);
         threads.forEach(thread -> {
             try {
-                thread.start();
                 thread.join();
                 totalPower.addAndGet(thread.getTotalPower());
             } catch (InterruptedException e) {
--- src/main/java/school/faang/BJS235895/Runner.java
+++ src/main/java/school/faang/BJS235895/Runner.java
@@ -5,10 +5,10 @@ import java.util.concurrent.atomic.AtomicInteger;
 public class Runner {
     public static void main(String[] args) {
         Army army = new Army();
-        army.addUnit(new Archer(25));   // считаем в потоке 1
-        army.addUnit(new Swordsman(40)); // считаем в потоке 2
-        army.addUnit(new Mage(50));     // считаем в потоке 3
-        army.addUnit(new Mage(20));     // считаем в потоке 4
+        army.addUnit(new Archer(25));
+        army.addUnit(new Swordsman(40));
+        army.addUnit(new Mage(50));
+        army.addUnit(new Mage(20));
 
         AtomicInteger totalPower = army.calculateTotalPower();
         System.out.println("Общая сила армии: " + totalPower.toString());
