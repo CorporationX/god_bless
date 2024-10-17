b980f04d294d8760c205ad71a4d8459860ea7692
“Andrey <aiegorovceo@yandex.ru>
2024-10-17 20:33:18 +0300

    feat: Define functional interface MatrixTransformer feat: Add Coordinates class with row and column attributes

---
+++ src/main/java/school/faang/rotatematrix/Coordinates.java
@@ -0,0 +1,11 @@
+package school.faang.rotatematrix;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class Coordinates {
+    private int x; // Row index
+    private int y; // Column index
+}
