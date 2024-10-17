1ad6ff3075e1f9999b31bdb37ab8f3d0e496b9f0
daurenberdibekov <berder08@gmail.com>
2024-10-17 12:41:27 +0200

    Еще исправления

--- src/main/java/school/faang/multithread/MailSender.java
+++ src/main/java/school/faang/multithread/MailSender.java
@@ -1,11 +1,15 @@
 package school.faang.multithread;
 
 public class MailSender {
-    private static final Integer NUMBER_OF_THREADS = 5;
+    public static final int MESSAGES_PER_THREAD = 200;
+    public static final int NUMBER_OF_THREADS = 5;
 
     public static void main(String[] args) {
         for (int i = 0; i < NUMBER_OF_THREADS; i++){
-            Thread thread = new Thread(new SenderRunnable(1, 200));
+            int startMessage = i * MESSAGES_PER_THREAD + 1;
+            int endMessage = startMessage + MESSAGES_PER_THREAD - 1;
+
+            Thread thread = new Thread(new SenderRunnable(startMessage, endMessage));
             thread.start();
             try {
                 thread.join();
@@ -13,7 +17,6 @@ public class MailSender {
                 throw new IllegalStateException("Ошибка в работе потока: " + e.getMessage());
             }
         }
-
 
         System.out.println("Все письма отправлены.");
     }
