9118650a0cc664baf74979fc332e111a3fe2dc4c
Ekaterina Badratdinova <ekaterinafeshch@gmail.com>
2024-10-17 12:01:08 -0700

    Mice are very nice!

---
+++ src/main/java/ru/kraiush/BJS2_36122/Room.java
@@ -0,0 +1,14 @@
+package ru.kraiush.BJS2_36122;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.List;
+
+@Getter
+@Setter
+@AllArgsConstructor
+public class Room {
+    private List<Food> food;
+}
