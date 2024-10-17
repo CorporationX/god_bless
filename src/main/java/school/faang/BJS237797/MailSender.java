92c23b2a8f66d4c043f95b589ebbaaf19cf9bc01
Doniyor Kurbanov <doniyor.kurbanov.24@gmail.com>
2024-10-17 08:54:51 +0500

    BJS2-37797. Жду от вас фитбека спасибо

--- src/main/java/school/faang/BJS237797/MailSender.java
+++ src/main/java/school/faang/BJS237797/MailSender.java
@@ -1,24 +1,27 @@
 package school.faang.BJS237797;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class MailSender {
-    public static void main(String[] args) throws InterruptedException {
-        Thread thread1 = new Thread(new SenderRunnable(1, 200));
-        Thread thread2 = new Thread(new SenderRunnable(201, 400));
-        Thread thread3 = new Thread(new SenderRunnable(401, 600));
-        Thread thread4 = new Thread(new SenderRunnable(601, 800));
-        Thread thread5 = new Thread(new SenderRunnable(801, 1001));
+    public static void main(String[] args) {
+        List<Thread> threads = new ArrayList<>();
 
-            thread1.start();
-            thread2.start();
-            thread3.start();
-            thread4.start();
-            thread5.start();
+        for (int i = 0; i < 5; i ++) {
+            int startIndex = i * 200;
+            int endIndex = (i + 1) * 200;
+            Thread thread = new Thread(new SenderRunnable(startIndex, endIndex));
+            threads.add(thread);
+        }
 
-            thread1.join();
-            thread2.join();
-            thread3.join();
-            thread4.join();
-            thread5.join();
+        threads.forEach(Thread::start);
+        threads.forEach(thread -> {
+            try {
+                thread.join();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        });
 
         System.out.println("Все сообщения отправлены");
     }
