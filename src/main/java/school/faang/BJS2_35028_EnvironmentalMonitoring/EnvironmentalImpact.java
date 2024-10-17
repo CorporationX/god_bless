3303b7f8c5d51738eaded118ffe6a54da4052f5e
Maxim Gorobets <gorobets.maks@bk.ru>
2024-10-17 12:33:53 +1000

    added required annotations for csv parser correct work

--- src/main/java/school/faang/BJS2_35028_EnvironmentalMonitoring/EnvironmentalImpact.java
+++ src/main/java/school/faang/BJS2_35028_EnvironmentalMonitoring/EnvironmentalImpact.java
@@ -1,16 +1,30 @@
 package school.faang.BJS2_35028_EnvironmentalMonitoring;
 
+import com.opencsv.bean.CsvBindByName;
+import com.opencsv.bean.CsvDate;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 import java.time.LocalDate;
 
 @Getter
 @AllArgsConstructor
+@NoArgsConstructor
 public class EnvironmentalImpact {
+    @CsvBindByName(column = "id")
     private int id;
+
+    @CsvBindByName(column = "companyId")
     private int companyId;
+
+    @CsvBindByName(column = "volume")
     private double volume;
+
+    @CsvBindByName(column = "date")
+    @CsvDate("yyyy.MM.dd")
     private LocalDate date;
+
+    @CsvBindByName(column = "type")
     private String type;
 }
