58b948d7327b4b4c2fd8bb12ec63a334593778a2
Sergey Zhitenev <zhitenev@lanit.ru>
2024-10-17 19:24:01 +0300

    feature: Mice are very nice!

---
+++ src/main/java/school/faang/multithreading/miceAreVeryNice/Food.java
@@ -0,0 +1,10 @@
+package school.faang.multithreading.miceAreVeryNice;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@AllArgsConstructor
+@Getter
+public class Food {
+    private String name;
+}
