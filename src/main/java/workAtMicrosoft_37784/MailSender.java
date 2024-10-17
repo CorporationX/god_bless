ba355ef70d3f348eb249d6c99ba4245c8cb43f73
AlekseyB86nb <tigrxam@gmail.com>
2024-10-17 14:29:32 +0300

    fix потоки работают параллельно

--- src/main/java/workAtMicrosoft_37784/MailSender.java
+++ src/main/java/workAtMicrosoft_37784/MailSender.java
@@ -1,19 +1,25 @@
 package workAtMicrosoft_37784;
 
 public class MailSender {
-    public static void main(String[] args) throws InterruptedException {
+    public static void main(String[] args) {
         int totalMessages = 1000;
         int countThreads = 5;
         int sizeBatch = totalMessages / countThreads;
+        Thread[] threads = new Thread[countThreads];
 
         for (int i = 0; i < countThreads; i++) {
             int start = sizeBatch * i;
             int end = sizeBatch * (i + 1);
-            Thread thread = new Thread(new SenderRunnable(start, end));
-            thread.start();
-            thread.join();
+            threads[i] = new Thread(new SenderRunnable(start, end));
+            threads[i].start();
         }
-
+        for (Thread thread : threads) {
+            try {
+                thread.join();
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+        }
         System.out.println("Все письма отправлены");
     }
 }
