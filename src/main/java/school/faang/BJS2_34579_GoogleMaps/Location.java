58e9c968af6d761c448a816fd34367f912a89d21
Maxim Gorobets <gorobets.maks@bk.ru>
2024-10-17 20:08:55 +1000

    new Location class

---
+++ src/main/java/school/faang/BJS2_34579_GoogleMaps/Location.java
@@ -0,0 +1,12 @@
+package school.faang.BJS2_34579_GoogleMaps;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@AllArgsConstructor
+@Getter
+public class Location {
+    private String name;
+    private double latitude;
+    private double longitude;
+}
