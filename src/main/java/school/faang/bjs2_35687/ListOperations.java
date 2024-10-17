6362dd2f637e977ef83980cd413d89854563b516
Shap|Misha <maxim.ivanov.jr@gmail.com>
2024-10-17 20:19:07 +0300

    done method for ListOperations: countStringsStartingWith,filterStringsContaining,sortStringsByLength,allMatchCondition,findMinGreaterThan,mapStringsToLengths. added Class:ListOperationsTest. added method:sumEvenNumbers,findMaxElement,findAverageOfNumbers,countStringsStartingWith,filterStringsContaining,sortStringsByLength,allMatchCondition,findMinGreaterThan,mapStringsToLengths for ListOperationsTest

--- src/main/java/school/faang/bjs2_35687/ListOperations.java
+++ src/main/java/school/faang/bjs2_35687/ListOperations.java
@@ -1,33 +1,66 @@
 package school.faang.bjs2_35687;
 
+import lombok.NonNull;
+
+import java.util.Comparator;
 import java.util.List;
 import java.util.NoSuchElementException;
-import java.util.Optional;
+import java.util.function.Predicate;
 
 public class ListOperations {
-    public static int sumEvenNumbers(List<Integer> numbers) {
+    public static int sumEvenNumbers(@NonNull List<Integer> numbers) {
         return numbers.stream()
                 .filter(integer -> (integer % 2 == 0))
                 .reduce(0, Integer::sum);
     }
 
-    public static int findMaxElement(List<Integer> numbers) {
-        Optional<Integer> max = numbers.stream().max(Integer::compareTo);
-        if (max.isEmpty()) {
-            throw new NoSuchElementException("no elements found");
-        }
-        return max.get();
+    public static int findMaxElement(@NonNull List<Integer> numbers) {
+        return numbers.stream()
+                .max(Integer::compareTo)
+                .orElseThrow(() -> new NoSuchElementException("list is empty"));
     }
 
-    public static double findAverageOfNumbers(List<Integer> numbers) {
-        double quantity = numbers.size();
-        double sumNumbers = numbers.stream().reduce(0, Integer::sum);
-        return sumNumbers / quantity;
+    public static double findAverageOfNumbers(@NonNull List<Integer> numbers) {
+        return numbers.stream()
+                .mapToInt(Integer::intValue)
+                .average()
+                .orElseThrow(() -> new NoSuchElementException("list is empty"));
     }
 
-    public static int countStringsStartingWith(List<String> strings, char startSymbol) {
+    public static int countStringsStartingWith(@NonNull List<String> strings, char startSymbol) {
         return (int) strings.stream()
                 .filter(s -> s.startsWith(String.valueOf(startSymbol)))
                 .count();
+    }
+
+    public static List<String> filterStringsContaining(@NonNull List<String> strings, @NonNull String substring) {
+        return strings.stream()
+                .filter(s -> s.contains(substring))
+                .toList();
+    }
+
+    public static List<String> sortStringsByLength(@NonNull List<String> strings) {
+        return strings.stream()
+                .sorted(Comparator.comparingInt(String::length))
+                .toList();
+    }
+
+    public static boolean allMatchCondition(@NonNull List<Integer> numbers, @NonNull Predicate<Integer> condition) {
+        return numbers.stream()
+                .allMatch(condition);
+    }
+
+    public static int findMinGreaterThan(@NonNull List<Integer> numbers, int threshold) {
+        return numbers.stream()
+                .filter(integer -> integer > threshold)
+                .min(Integer::compareTo)
+                .orElseThrow(() -> new NoSuchElementException("list is empty"));
+
+    }
+
+    public static List<Integer> mapStringsToLengths(@NonNull List<String> strings) {
+        return strings.stream()
+                .map(String::length)
+                .toList();
     }
 }
--- src/main/java/school/faang/bjs2_35687/Main.java
+++ src/main/java/school/faang/bjs2_35687/Main.java
@@ -5,10 +5,17 @@ import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
-        List<Integer> numbers = Arrays.asList(1, 2, 3, 4, 5, 6);
+        List<Integer> numbers = Arrays.asList(10, 5, 20, 15, 3, 7);
+        List<String> strings = Arrays.asList("apple", "apricot", "banana", "avocado", "berry");
 
         System.out.println(ListOperations.sumEvenNumbers(numbers));
         System.out.println(ListOperations.findMaxElement(numbers));
         System.out.println(ListOperations.findAverageOfNumbers(numbers));
+        System.out.println(ListOperations.countStringsStartingWith(strings, 'a'));
+        System.out.println(ListOperations.filterStringsContaining(strings, "ap"));
+        System.out.println(ListOperations.sortStringsByLength(strings));
+        System.out.println(ListOperations.allMatchCondition(numbers, integer -> integer > 0));
+        System.out.println(ListOperations.findMinGreaterThan(numbers, 6));
+        System.out.println(ListOperations.mapStringsToLengths(strings));
     }
 }
