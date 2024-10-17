53bcdfdd770f63c79ad744cedac5fc816d78324d
Egor Semenovskii <gaoqiao5217@gmail.com>
2024-10-17 21:21:02 +0300

    Commit message

--- src/main/java/school/faang/Microsoft/MailSender.java
+++ src/main/java/school/faang/Microsoft/MailSender.java
@@ -2,13 +2,14 @@ package school.faang.Microsoft;
 
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
 
 public class MailSender {
 
     private static final int TOTAL_MESSAGES = 1000;
     private static final int THREADS_COUNT = 5;
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
 
         ExecutorService executorService = Executors.newFixedThreadPool(THREADS_COUNT);
         int batchSize = TOTAL_MESSAGES / THREADS_COUNT;
@@ -21,7 +22,10 @@ public class MailSender {
 
         executorService.shutdown();
 
-        while (!executorService.isTerminated()) {}
+        if (!executorService.awaitTermination(10, TimeUnit.SECONDS)){
+            System.out.println("Something went wrong");
+            executorService.shutdownNow();
+        }
         System.out.println("Done!!!");
 
     }
