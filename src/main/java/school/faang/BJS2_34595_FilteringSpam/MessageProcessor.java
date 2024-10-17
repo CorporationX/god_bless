78c66990dbd1847a4926d55e3b1332eb6c4e1336
Maxim Gorobets <gorobets.maks@bk.ru>
2024-10-17 21:58:44 +1000

    testing MessageProcessor in main method

--- src/main/java/school/faang/BJS2_34595_FilteringSpam/MessageProcessor.java
+++ src/main/java/school/faang/BJS2_34595_FilteringSpam/MessageProcessor.java
@@ -1,10 +1,31 @@
 package school.faang.BJS2_34595_FilteringSpam;
 
 import java.util.List;
+import java.util.Arrays;
 
 public class MessageProcessor {
     public boolean processMessage(String message, List<MessageFilter> messageFilters) {
         return messageFilters.stream()
                 .allMatch(filter -> filter.filter(message));
+    }
+
+    public static void main(String[] args) {
+        MessageProcessor messageProcessor = new MessageProcessor();
+
+        MessageFilter spamFilter = message -> !message.toLowerCase().contains("спам");
+        MessageFilter lengthFilter = message -> message.length() > 10;
+        MessageFilter emojiFilter = message -> !message.contains("😀");
+
+        List<MessageFilter> filters = Arrays.asList(spamFilter, lengthFilter, emojiFilter);
+
+        String[] messages = {
+                "Привет!", "Это спам!", "Как дела? 😀",
+                "Длинное сообщение без спама и эмодзи", "ЭТо точно не спаm!!!!!!"
+        };
+
+        for (String message : messages) {
+            boolean isFiltered = messageProcessor.processMessage(message, filters);
+            System.out.println("Сообщение: " + message + " | Пропущено: " + isFiltered);
+        }
     }
 }
