253d416a05082b1cc363393a11c3c1df3aad3603
“Andrey <aiegorovceo@yandex.ru>
2024-10-18 00:17:07 +0300

    feat: Add method to find the top-5 popular hashtags

--- src/main/java/school/faang/analysuseractivsocialnet/UserActionAnalyzer.java
+++ src/main/java/school/faang/analysuseractivsocialnet/UserActionAnalyzer.java
@@ -2,6 +2,7 @@ package school.faang.analysuseractivsocialnet;
 
 import java.time.LocalDate;
 import java.time.temporal.ChronoUnit;
+import java.util.Arrays;
 import java.util.List;
 import java.util.Map;
 import java.util.function.Function;
@@ -15,6 +16,18 @@ public class UserActionAnalyzer {
                 .entrySet().stream()
                 .sorted((a, b) -> Long.compare(b.getValue(), a.getValue()))
                 .limit(10)
+                .map(Map.Entry::getKey)
+                .collect(Collectors.toList());
+    }
+
+    public static List<String> topPopularHashtags(List<UserAction> actions) {
+        return actions.stream()
+                .flatMap(action -> Arrays.stream(action.getContent().split("\\s+")))
+                .filter(word -> word.startsWith("#"))
+                .collect(Collectors.groupingBy(Function.identity(), Collectors.counting()))
+                .entrySet().stream()
+                .sorted((a, b) -> Long.compare(b.getValue(), a.getValue()))
+                .limit(5)
                 .map(Map.Entry::getKey)
                 .collect(Collectors.toList());
     }
