dd797daca021749692b45869c6aa137d47948c2c
Memoreks <akifyevalex1@gmail.com>
2024-10-18 16:04:43 +0700

    Внесены правки согласно комментариям

--- src/main/java/school/faang/tamagotchi/Main.java
+++ src/main/java/school/faang/tamagotchi/Main.java
@@ -2,7 +2,6 @@ package school.faang.tamagotchi;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.function.Consumer;
 
 public class Main {
     public static void main(String[] args){
