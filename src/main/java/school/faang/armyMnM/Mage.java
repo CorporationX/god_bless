7b243efdb639b6c2da145da23cb338cd445932ee
bogda <bogdanskrip404@gmail.com>
2024-10-18 16:04:59 +0500

    .

---
+++ src/main/java/school/faang/armyMnM/Mage.java
@@ -0,0 +1,8 @@
+package school.faang.armyMnM;
+
+public class Mage extends Unit{
+
+    public Mage(int power) {
+        super(power);
+    }
+}
---
+++ src/main/java/school/faang/armyMnM/Swordsman.java
@@ -0,0 +1,8 @@
+package school.faang.armyMnM;
+
+public class Swordsman extends Unit{
+
+    public Swordsman(int power) {
+        super(power);
+    }
+}
