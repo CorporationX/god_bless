bf86df904693202bbaf155330e9914ab7d0f028a
zlomilk95 <kenobi_95@mail.ru>
2024-10-18 22:37:07 +0300

    edits

--- src/main/java/school/faang/bjs2_35254/Main.java
+++ src/main/java/school/faang/bjs2_35254/Main.java
@@ -2,13 +2,19 @@ package school.faang.bjs2_35254;
 
 import java.util.Arrays;
 import java.util.Comparator;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
+import java.util.Set;
+import java.util.regex.Pattern;
+import java.util.stream.Collectors;
 
 public class Main {
 
     public static void main(String[] args) {
-        List<Integer> listOfNumbers = Arrays.asList(1, 2, 3, 4, 5, 6);
+        List<Integer> listOfNumbers = Arrays.asList(2, 5, 3, 6, 4, 1);
+
+
         System.out.println(getUniquePairs(listOfNumbers, 7));
 
         Map<String, String> countriesAndCapitals = Map.of(
@@ -26,22 +32,20 @@ public class Main {
         System.out.println(convertNumbersToBinaryFormat(listToConvert));
 
         List<String> listStringsToSort = Arrays.asList("apple", "banana", "яблоко", "date", "fig", "grape");
-        System.out.println(filterStringsAlphabeticallyAndSortByLenght(listStringsToSort, "abcdefghijklmnopqrstuvwxyz"));
+        System.out.println(filterStringsAlphabeticallyAndSortByLength(listStringsToSort, "abcdefghijklmnopqrstuvwxyz"));
 
         System.out.println(Arrays.toString("fsf".chars().toArray()));
     }
 
-    public static List<String> getUniquePairs(List<Integer> list, int number) {
+    public static Set<Set<Integer>> getUniquePairs(List<Integer> list, int number) {
        return list.stream()
-               .filter(n -> n < number)
-               .flatMap(a -> list.stream()
-                       .filter(b -> a + b == number)
-                       .map(b -> "(" + a + ", " + b + ")"))
-               .toList();
+               .filter(element ->  list.contains(number - element))
+               .map(element -> Set.of(element, number - element))
+               .collect(Collectors.toSet());
     }
 
     public static List<String> getListOfCapital(Map<String, String> mapper) {
-      return  mapper.values().stream().toList();
+        return mapper.values().stream().sorted().toList();
     }
 
     public static List<String> filterAndSortRows(List<String> list, char symbol) {
@@ -52,18 +56,12 @@ public class Main {
     }
 
     public static List<String> convertNumbersToBinaryFormat(List<Integer> list) {
-       return list.stream().map(Integer::toBinaryString).toList();
+        return list.stream().map(Integer::toBinaryString).toList();
     }
 
-    public static List<String> filterStringsAlphabeticallyAndSortByLenght(List<String> list, String alphabet) {
-
-        List<Character> validChars = alphabet.chars().mapToObj(c -> (char) c).toList();
-
-        return list.stream()
-                .filter(s -> s.chars()
-                        .allMatch(c -> validChars.contains((char) c)))
-                .sorted(Comparator.comparing(String::length))
-                .toList();
+    public static List<String> filterStringsAlphabeticallyAndSortByLength(List<String> list, String alphabet) {
+       return list.stream()
+                .filter(s -> Pattern.matches("[" + alphabet + "]+", s))
+                .sorted(Comparator.comparing(String::length)).toList();
     }
-
 }
