bc827ab08c2d8cc5ec621adde78f453023f9c67b
Roman Nifontov <romannif@mail.ru>
2024-10-18 20:18:58 +0500

    Сделал изменения в задаче BJS2-37529

--- src/main/java/school/BJS2_37529/VideoManager.java
+++ src/main/java/school/BJS2_37529/VideoManager.java
@@ -5,23 +5,24 @@ import java.util.Map;
 
 public class VideoManager {
 
+
     private Map<String, Integer> viewsMap = new HashMap<>();
 
     public void addView(String videoId) {
         synchronized (viewsMap) {
             viewsMap.put(videoId, viewsMap.getOrDefault(videoId, 0) + 1);
-            viewsMap.notify();
         }
     }
 
     public int getViewCount(String videoId) throws InterruptedException {
         synchronized (viewsMap) {
-            if (!viewsMap.containsKey(videoId)) {
-                viewsMap.wait();
-            }
             System.out.printf("Количество просмотров видео %s = %d", videoId, viewsMap.get(videoId));
             System.out.println();
             return viewsMap.getOrDefault(videoId,0);
         }
+    }
+
+    public Map<String, Integer> getViewsMap() {
+        return viewsMap;
     }
 }
