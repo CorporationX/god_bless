3b2f88a10b5d85d90dcfb580f345fb8f9a8ac174
Irina Krasnova <arisha.17@mail.ru>
2024-10-18 14:41:46 +0300

    удаление файлов

--- src/test/java/Book.java
+++
@@ -1,8 +0,0 @@
-public class Book {
-    public static void main(String[] args) {
-        System.out.println("New book");
-        System.out.println("Read book");
-        System.out.println("Reed book again");
-        System.out.println("Continue Reed book ");
-    }
-}
