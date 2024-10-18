0ab2653301f730e1c1be2142c92b4bfaa21f1a79
Dmytro Yevsieienko <youdima70@gmail.com>
2024-10-18 20:14:34 +0300

    Рефакторинг

--- src/main/java/dima/evseenko/spotify/Main.java
+++ src/main/java/dima/evseenko/spotify/Main.java
@@ -1,14 +1,11 @@
 package dima.evseenko.spotify;
 
-import lombok.SneakyThrows;
-
 import java.util.ArrayList;
 import java.util.List;
 
 public class Main {
 
-    @SneakyThrows
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
         Player player = new Player();
 
         List<Thread> threads = new ArrayList<>();
