bffb8f03e4ecac3ddf14df30df48ee61ce666841
“Andrey <aiegorovceo@yandex.ru>
2024-10-18 14:56:56 +0300

    Update UniquePairsFinder with Stream API

--- src/main/java/school/faang/streamapitwo/UniquePairsFinder.java
+++ src/main/java/school/faang/streamapitwo/UniquePairsFinder.java
@@ -1,24 +1,27 @@
 package school.faang.streamapitwo;
 
-import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
+import java.util.stream.Collectors;
 
 public class UniquePairsFinder {
 
     public static List<Pair> findUniquePairs(List<Integer> numbers, int targetSum) {
-        Set<Pair> pairs = new HashSet<>();
         Set<Integer> seen = new HashSet<>();
 
-        for (Integer number : numbers) {
-            int complement = targetSum - number;
-            if (seen.contains(complement)) {
-                pairs.add(new Pair(Math.min(number, complement), Math.max(number, complement)));
-            }
-            seen.add(number);
-        }
-
-        return new ArrayList<>(pairs);
+        return numbers.stream()
+                .filter(number -> {
+                    int complement = targetSum - number;
+                    boolean isPair = seen.contains(complement);
+                    seen.add(number);
+                    return isPair;
+                })
+                .map(number -> {
+                    int complement = targetSum - number;
+                    return new Pair(Math.min(number, complement), Math.max(number, complement));
+                })
+                .distinct()
+                .collect(Collectors.toList());
     }
 }
