49be14d60e7ef9a7bcdd1dbae64f00050440b279
Ekaterina Badratdinova <ekaterinafeshch@gmail.com>
2024-10-17 21:14:32 -0700

    Путь пальцем проложи…

---
+++ src/main/java/ru/kraiush/BJS2_36315/Location.java
@@ -0,0 +1,11 @@
+package ru.kraiush.BJS2_36315;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class Location {
+    private int x;
+    private int y;
+}
