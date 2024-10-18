6f984518fc0b6f25d0f90224aac868c3489d6001
Irina Krasnova <arisha.17@mail.ru>
2024-10-18 12:16:43 +0300

    Подсчет видео utub

---
+++ src/test/java/utub/VideoManager.java
@@ -0,0 +1,21 @@
+package utub;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class VideoManager {
+    private Map<String, Integer> viewsMap = new HashMap<>();
+
+    public Map<String, Integer> getViewsMap() {
+        return viewsMap;
+    }
+
+    public synchronized void addView(String videoid) {
+        viewsMap.put(videoid, viewsMap.getOrDefault(videoid, 50) + 1);
+        ;
+    }
+
+    public synchronized int getViewCount(String viewid) {
+        return viewsMap.getOrDefault(viewid, 0);
+    }
+}
