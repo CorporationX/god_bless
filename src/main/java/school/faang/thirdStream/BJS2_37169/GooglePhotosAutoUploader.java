9e5be8837d5b78bbd61c9d255a99c19350671723
UrmatBolotbek <urmar9797@gmail.com>
2024-10-18 20:52:36 +0200

    complete the task "Google Photo sync" BJS2-37169

---
+++ src/main/java/school/faang/thirdStream/BJS2_37169/GooglePhotosAutoUploader.java
@@ -0,0 +1,45 @@
+package school.faang.thirdStream.BJS2_37169;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class GooglePhotosAutoUploader {
+    private final Object lock;
+    private final List<String> photosToUpload;
+
+    public GooglePhotosAutoUploader() {
+        lock = new Object();
+        photosToUpload = new ArrayList<>();
+    }
+
+    public void startAutoUpload() {
+        synchronized (lock) {
+            while (photosToUpload.isEmpty()) {
+                System.out.println("Нет новых фотографий для загрузки. Переход в режим ожидания...");
+                try {
+                    lock.wait();
+                    uploadPhotos();
+                } catch (InterruptedException e) {
+                    System.out.println("Работа загрузчика была прервана");
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+
+    public void onNewPhotoAdded(String photoPath) {
+        synchronized (lock) {
+            photosToUpload.add(photoPath);
+            System.out.println("Добавлена новая фотография: " + photoPath);
+            lock.notify();
+        }
+    }
+
+    private void uploadPhotos() {
+        for (String photo : photosToUpload) {
+            System.out.printf("Загрузка фотографии %s на сервер%n", photo);
+        }
+        photosToUpload.clear();
+        System.out.println("Загрузка фотографий завершена.\n");
+    }
+}
