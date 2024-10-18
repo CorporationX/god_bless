f7a0f6f76d7f1295b3a44ef3b3bbd7040e3868b3
“Andrey <aiegorovceo@yandex.ru>
2024-10-18 15:09:49 +0300

    Update PalindromeSubstringFinder with Stream API

--- src/main/java/school/faang/streamapithree/PalindromeSubstringFinder.java
+++ src/main/java/school/faang/streamapithree/PalindromeSubstringFinder.java
@@ -2,25 +2,29 @@ package school.faang.streamapithree;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
 
 public class PalindromeSubstringFinder {
 
     public static List<String> findPalindromicSubstrings(String input) {
-        List<String> palindromicSubstrings = new ArrayList<>();
-
-        for (int i = 0; i < input.length(); i++) {
-            findPalindromes(input, i, i, palindromicSubstrings);
-            findPalindromes(input, i, i + 1, palindromicSubstrings);
-        }
-
-        return palindromicSubstrings;
+        return IntStream.range(0, input.length())
+                .boxed()
+                .flatMap(i -> Stream.concat(
+                        findPalindromes(input, i, i).stream(),
+                        findPalindromes(input, i, i + 1).stream()
+                ))
+                .collect(Collectors.toList());
     }
 
-    private static void findPalindromes(String str, int left, int right, List<String> result) {
+    private static List<String> findPalindromes(String str, int left, int right) {
+        List<String> result = new ArrayList<>();
         while (left >= 0 && right < str.length() && str.charAt(left) == str.charAt(right)) {
             result.add(str.substring(left, right + 1));
             left--;
             right++;
         }
+        return result;
     }
 }
