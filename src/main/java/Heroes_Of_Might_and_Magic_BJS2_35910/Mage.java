c769984978fca77a5b4ff496d474042033fdc3d2
IvanMatiko <matiko1993@gmail.com>
2024-10-18 23:17:56 +0200

    Made task Heroes_Of_Might_and_Magic_BJS2_35910

---
+++ src/main/java/Heroes_Of_Might_and_Magic_BJS2_35910/Mage.java
@@ -0,0 +1,10 @@
+package Heroes_Of_Might_and_Magic_BJS2_35910;
+
+import lombok.Data;
+import lombok.AllArgsConstructor;
+
+@AllArgsConstructor
+@Data
+public class Mage {
+    private int power;
+}
---
+++ src/main/java/Heroes_Of_Might_and_Magic_BJS2_35910/Swordsman.java
@@ -0,0 +1,11 @@
+package Heroes_Of_Might_and_Magic_BJS2_35910;
+
+import lombok.Data;
+import lombok.AllArgsConstructor;
+
+@AllArgsConstructor
+@Data
+public class Swordsman {
+    private int power;
+
+}
