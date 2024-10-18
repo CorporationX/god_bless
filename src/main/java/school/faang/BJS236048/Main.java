a55de7215c017387e6c23e31d381c41d3c28937b
Doniyor Kurbanov <doniyor.kurbanov.24@gmail.com>
2024-10-18 19:42:18 +0500

    BJS2-36048. Жду от вас фитбека спасибо

--- src/main/java/school/faang/BJS236048/Main.java
+++ src/main/java/school/faang/BJS236048/Main.java
@@ -8,39 +8,35 @@ import java.util.concurrent.Executors;
 import java.util.concurrent.TimeUnit;
 
 public class Main {
+    private static final int FROM_AGE = 18;
+    private static final int TO_AGE = 65;
+    private static final int THREAD_COUNT = 5;
+
     public static void main(String[] args) {
         List<Person> personList = new ArrayList<>();
         String[] workPlace = {"Google", "Amazon", "Uber", "Nvidia"};
         Random random = new Random(workPlace.length);
 
-        for (int i = 0; i < 999; i ++) {
+        for (int i = 0; i < 999; i++) {
             String name = "Name" + i;
             String surname = "Surname" + i;
-            int age = random.nextInt(60) + 18;
+            int age = random.nextInt(FROM_AGE, TO_AGE);
             String company = workPlace[random.nextInt(workPlace.length)];
             personList.add(new Person(name, surname, age, company));
         }
 
-        ExecutorService executorService = Executors.newFixedThreadPool(5);
+        ExecutorService executorService = Executors.newFixedThreadPool(THREAD_COUNT);
 
-        int threadsCount = 5;
+        int threadsCount = THREAD_COUNT;
         int subListSize = personList.size() / threadsCount;
         for (int i = 0; i < threadsCount; i++) {
             int fromIndex = i * subListSize;
             int toIndex = (i == threadsCount - 1) ? personList.size() : fromIndex + subListSize;
             List<Person> subList = personList.subList(fromIndex, toIndex);
-            executorService.execute(() -> {
-                subList.forEach(person -> {
-                    try {
-                        Thread.sleep(500);
-                        System.out.println(person.getName() + " " + person.getSurname() + " " + person.getAge() + " " + person.getWorkPlace());
-                    } catch (InterruptedException e) {
-                        e.printStackTrace();
-                    }
-                });
-            });
+            executorService.execute(new PersonInfoPrinter(subList));
         }
         executorService.shutdown();
+
         try {
             executorService.awaitTermination(2, TimeUnit.MINUTES);
         } catch (InterruptedException e) {
