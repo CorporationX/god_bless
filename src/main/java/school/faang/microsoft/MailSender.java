ea78153120153df24a84fb3864f51972842f0b6e
Alexander Litovkin <litovkinalexan@gmail.com>
2024-10-18 10:57:18 +0700

    add exception catch

--- src/main/java/school/faang/microsoft/MailSender.java
+++ src/main/java/school/faang/microsoft/MailSender.java
@@ -8,7 +8,7 @@ public class MailSender {
     private static final int MAIL_COUNT = 1000;
     private static final int THREADS_COUNT = 5;
 
-    public static void main(String[] args) throws InterruptedException {
+    public static void main(String[] args) {
         int batchSize = MAIL_COUNT / THREADS_COUNT;
         List<Thread> threads = new ArrayList<>();
         for (int i = 0; i < THREADS_COUNT; i++) {
@@ -19,7 +19,11 @@ public class MailSender {
             thread.start();
         }
         for (Thread thread : threads) {
-            thread.join();
+            try {
+                thread.join();
+            } catch (InterruptedException e) {
+                throw new IllegalStateException("Thread was interrupted", e);
+            }
         }
         System.out.println("All mails were sent");
     }
