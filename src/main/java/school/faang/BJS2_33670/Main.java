875c4e06727f861e4df65948875b9eb6722aa776
SerUKalinin <serukalinin@gmail.com>
2024-10-18 19:58:50 +0300

    Removed student's code

--- src/main/java/school/faang/BJS2_33670/Main.java
+++ src/main/java/school/faang/BJS2_33670/Main.java
@@ -10,29 +10,23 @@ public class Main {
     public static void main(String[] args) {
         EmailProcessor emailProcessor = new EmailProcessor();
 
-// Список входящих писем
         List<Email> emails = Arrays.asList(
             new Email("Письмо 1", "Текст письма 1", false),
             new Email("Письмо 2", "Текст письма 2", true),
             new Email("Спам", "Текст спама", false)
         );
 
-// Фильтр, который пропускает только важные письма
         Predicate<Email> importantFilter = email -> email.isImportant();
 
-// Обработчик, который выводит тему письма в консоль
         Consumer<Email> printEmail = email -> System.out.println("Обработано письмо: " + email.getSubject());
 
-// Преобразователь, который переводит текст письма в верхний регистр и сохраняет изменения
         Function<Email, String> toUpperCase = email -> {
             email.setBody(email.getBody());
-            return email.getBody();  // Возвращает преобразованный текст
+            return email.getBody();
         };
 
-// Обработка писем
         emailProcessor.processEmails(emails, importantFilter, toUpperCase, printEmail);
 
-// Выводим обновленные письма, чтобы убедиться, что изменения сохранились
         emails.forEach(email -> System.out.println("Тема: " + email.getSubject() + ", Тело письма: " + email.getBody()));
     }
 }
