0fd9c0c661e54de7042ebddf00c9a44994b446d7
Alexander Litovkin <litovkinalexan@gmail.com>
2024-10-18 20:18:16 +0700

    add executor service

--- src/main/java/school/faang/googlephoto/Main.java
+++ src/main/java/school/faang/googlephoto/Main.java
@@ -2,22 +2,31 @@ package school.faang.googlephoto;
 
 import lombok.extern.slf4j.Slf4j;
 
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
+
 @Slf4j
 public class Main {
 
+    private static final int THREAD_COUNT = 2;
+    private static final int UPLOAD_DELAY = 5;
+
     public static void main(String[] args) {
         GooglePhotoAutoUploader uploader = new GooglePhotoAutoUploader();
-        Thread producer = new Thread(() -> uploader.onNewPhotoAdded("C://new_photo.png"), "Producer");
-        Thread consumer = new Thread(uploader::startAutoUpload, "Consumer");
-        consumer.start();
-        producer.start();
+        ScheduledExecutorService executor = Executors.newScheduledThreadPool(THREAD_COUNT);
+        executor.scheduleAtFixedRate(uploader::startAutoUpload, 0, UPLOAD_DELAY, TimeUnit.SECONDS);
+        executor.scheduleAtFixedRate(
+                () -> uploader.onNewPhotoAdded("C://new_photo.png"), 0, 1, TimeUnit.SECONDS
+        );
+        executor.schedule(executor::shutdown, 20, TimeUnit.SECONDS);
 
         try {
-            consumer.join();
-            producer.join();
+            if (!executor.awaitTermination(1, TimeUnit.MINUTES)) {
+                executor.shutdownNow();
+            }
         } catch (InterruptedException e) {
-            log.error("Caught exception", e);
-            throw new IllegalStateException("Thread interrupted", e);
+            executor.shutdownNow();
         }
     }
 }
