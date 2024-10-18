7292ccf7e40363a7584fbb91584942ccbbd5c37e
Gadzhiev Gadzhi <Gadzhiev3222@gmail.com>
2024-10-18 09:18:05 +0300

    stream api 3

---
+++ src/main/java/school/faang/trainstreamapi3/maincode/StreamApi3.java
@@ -0,0 +1,11 @@
+package school.faang.trainstreamapi3.maincode;
+
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
+public class StreamApi3 {
+    public static Set<List<String>> findMutualFriends(Map<String, List<String>> friends) {
+
+    }
+}
