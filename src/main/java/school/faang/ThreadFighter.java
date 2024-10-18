92121897cb919af510156b687fcee57dfd63b5cc
lomuserg <sergeymu2015@yandex.ru>
2024-10-18 17:02:01 +0300

    method calculateTotalPower

--- src/main/java/school/faang/ThreadFighter.java
+++ src/main/java/school/faang/ThreadFighter.java
@@ -4,16 +4,16 @@ import lombok.Getter;
 import school.faang.Fighter.Fighter;
 
 @Getter
-public class ThreadFighter extends Thread{
+public class ThreadFighter extends Thread {
     private Fighter fighter;
     private int result;
 
-    public ThreadFighter(Fighter fighter){
+    public ThreadFighter(Fighter fighter) {
         this.fighter = fighter;
     }
 
     @Override
-    public void run(){
+    public void run() {
         result = fighter.getPower();
     }
 
