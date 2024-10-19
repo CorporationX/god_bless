34dc46c8a1ce08e7d964c978d5360cabdf29e87d
lomuserg <sergeymu2015@yandex.ru>
2024-10-19 17:33:01 +0300

    init Player

--- src/main/java/school/faang/Main.java
+++ src/main/java/school/faang/Main.java
@@ -1,32 +1,8 @@
 package school.faang;
 
-import java.util.Arrays;
-import java.util.List;
-import java.util.function.Consumer;
-import java.util.function.Function;
-import java.util.function.Predicate;
-
 public class Main {
     public static void main(String[] args) {
-        EmailProcessor emailProcessor = new EmailProcessor();
 
-        List<Email> emails = Arrays.asList(
-                new Email("Letter 1", "Dear diary.... ", true),
-                new Email("Letter 2", "Rock and stone!!!!! ", true),
-                new Email("Spam", "Spam text", false)
-        );
-
-        Predicate<Email> importantFilter = email -> email.isImportant();
-
-        Consumer<Email> printEmail = email -> System.out.println("Обработано письмо: " + email.getSubject());
-
-        Function<Email, String> toUpperCase = email -> {
-            email.setBody(email.getBody().toUpperCase());
-            return email.getBody();  // Возвращает преобразованный текст
-        };
-
-        emailProcessor.processEmails(emails, importantFilter, printEmail, toUpperCase);
-        emails.forEach(email -> System.out.println("Тема: " + email.getSubject() + ", Тело письма: " + email.getBody()));
     }
 
 }
---
+++ src/main/java/school/faang/Player.java
@@ -0,0 +1,21 @@
+package school.faang;
+
+public class Player {
+    private Object lock = new Object();
+
+    public void play(){
+
+    }
+
+    public void pause(){
+
+    }
+
+    public void skip(){
+
+    }
+
+    public void previous(){
+
+    }
+}
