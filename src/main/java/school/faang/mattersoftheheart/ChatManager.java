c4f882c41508d76fed2a8f6cdfd801f981b01432
Алексей Беляев <belyaevayur@intech.rshb.ru>
2024-10-19 19:48:15 +0300

    BJS2-37737 Matters of the heart

---
+++ src/main/java/school/faang/mattersoftheheart/ChatManager.java
@@ -0,0 +1,54 @@
+package school.faang.mattersoftheheart;
+
+import lombok.Data;
+import lombok.extern.slf4j.Slf4j;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+@Data
+@Slf4j
+public class ChatManager {
+    private final UserList userList;
+    private final List<Chat> chats = new ArrayList<>();
+
+    public synchronized void startChat(User user) {
+        if (user.isLookingForChat()) {
+            Optional<User> companionOptional = userList.findCompanionForUser(user);
+            if (companionOptional.isPresent()) {
+                log.info("{} and {} start chatting", user.getName(), companionOptional.get().getName());
+                Chat chat = new Chat(user, companionOptional.get());
+                user.setLookingForChat(false);
+                companionOptional.get().setLookingForChat(false);
+                chats.add(chat);
+            } else {
+                waitForChat(user);
+            }
+        }
+    }
+
+    public synchronized void endChat(User user) {
+        Optional<Chat> chatOptional = chats.stream().filter(chat -> chat.user1().equals(user)).findAny();
+        if (chatOptional.isPresent()) {
+            chats.remove(chatOptional.get());
+            chatOptional.get().user1().setLookingForChat(true);
+            chatOptional.get().user2().setLookingForChat(true);
+            log.info("{} and {} end chatting", user.getName(), chatOptional.get().user2().getName());
+            notifyAll();
+        }
+    }
+
+    private synchronized void waitForChat(User user) {
+        while (userList.findCompanionForUser(user).isEmpty()) {
+            try {
+                log.info("{} waiting for companion", user.getName());
+                wait();
+            } catch (InterruptedException e) {
+                log.error(e.getMessage());
+                Thread.currentThread().interrupt();
+            }
+        }
+        startChat(user);
+    }
+}
---
+++ src/main/java/school/faang/mattersoftheheart/UserList.java
@@ -0,0 +1,27 @@
+package school.faang.mattersoftheheart;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+public class UserList {
+    private final List<User> users = new ArrayList<>();
+
+    public synchronized Optional<User> findCompanionForUser(User user) {
+        return getOnlineUsers().stream()
+                .filter(companion -> !companion.equals(user))
+                .findAny();
+    }
+
+    public List<User> getOnlineUsers() {
+        return users.stream().filter(user -> user.isOnline() && user.isLookingForChat()).toList();
+    }
+
+    public void addUser(User user) {
+        users.add(user);
+    }
+
+    public void removeUser(User user) {
+        users.remove(user);
+    }
+}
