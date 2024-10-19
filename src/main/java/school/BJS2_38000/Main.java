af21699195688e142daa9d3f9c4a51ccef070d0c
Roman Nifontov <romannif@mail.ru>
2024-10-19 23:03:31 +0500

    Сделал задачу BJS2-38000

---
+++ src/main/java/school/BJS2_38000/Main.java
@@ -0,0 +1,9 @@
+package school.BJS2_38000;
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+        MasterCardService service = new MasterCardService();
+        service.doAll();
+    }
+}
