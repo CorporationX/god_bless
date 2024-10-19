a1b3a0b64b21174c3734425a9aab33d57a5b5a66
0Miha0 <140548440+0Miha0@users.noreply.github.com>
2024-10-19 21:00:01 +0500

    https://faang-school.atlassian.net/browse/BJS2-35951 Забыл класс лишний удалить, удалил

--- src/main/java/school/faang/Mnogopotochka/ParallelismThread/BJS2_35951/Workplace.java
+++
@@ -1,8 +0,0 @@
-package school.faang.Mnogopotochka.ParallelismThread.BJS2_35951;
-
-public class Workplace {
-    private static final String[] workplaces = {
-            "GOOGLE", "MICROSOFT", "APPLE", "AMAZON", "FACEBOOK",
-            "IBM", "INTEL", "ORACLE", "SAP", "TESLA"
-    };
-}
