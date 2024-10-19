b69f66a04c9839ba83810f779f9e9edeb0e0e8d1
Matvey <mashptz@gmail.com>
2024-10-19 13:31:51 +0300

    Реализация wait() и notify()

--- src/main/java/school/faang/Main.java
+++ src/main/java/school/faang/Main.java
@@ -1,4 +1,20 @@
 package school.faang;
 
+import school.faang.BJS2_37235.GooglePhotosAutoUpload;
+
 public class Main {
+    public static void main(String[] args) {
+        GooglePhotosAutoUpload uploader = new GooglePhotosAutoUpload();
+        Thread adderThread = new Thread(() -> {
+            uploader.onNewPhotoAdded("The feast.jpg");
+            uploader.onNewPhotoAdded("Children.jpg");
+            uploader.onNewPhotoAdded("Cars.jpg");
+            uploader.onNewPhotoAdded("Selfie.jpg");
+        });
+
+        Thread uploadThread = new Thread(uploader ::startAutoUpload);
+
+        adderThread.start();
+        uploadThread.start();
+    }
 }
