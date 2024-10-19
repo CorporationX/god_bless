a0a6e2054e1bb1898641d6bfd90b4606104fed9d
Matvey <mashptz@gmail.com>
2024-10-19 13:36:49 +0300

    Исправление обработки исключения

--- src/main/java/school/faang/sender/MailSender.java
+++ src/main/java/school/faang/sender/MailSender.java
@@ -4,30 +4,33 @@ import java.util.logging.Level;
 import java.util.logging.Logger;
 
 public class MailSender {
-    private static final Logger LOGGER = Logger.getLogger(MailSender.class.getName());
+    private static final Logger logger = Logger.getLogger(MailSender.class.getName());
 
-    public static void main(String[] args) throws InterruptedException {
-        try {
-            int allMessage = 1000;
-            int threadNumber = 5;
-            int size = allMessage / threadNumber;
-            Thread[] threads = new Thread[threadNumber];
+    public static void main(String[] args) {
+        int allMessage = 1000;
+        int threadNumber = 5;
+        int size = allMessage / threadNumber;
+        Thread[] threads = new Thread[threadNumber];
 
-            for (int i = 0; i < threadNumber; i++) {
-                threads[i] = new Thread(new SenderRunnable(i * size + 1, (i + 1) * size));
-                threads[i].start();
-            }
+        for (int i = 0; i < threadNumber; i++) {
+            threads[i] = new Thread(new SenderRunnable(i * size + 1, (i + 1) * size));
+            threads[i].start();
+        }
 
-            for (Thread thread : threads) {
-                if (thread != null) {
+        for (Thread thread : threads) {
+            if (thread != null) {
+                try {
                     thread.join(1000);
+                } catch (InterruptedException e) {
+                    logger.log(
+                            Level.SEVERE,
+                            "Ошибка при ожидании завершения потока" +
+                                    thread.getName(), e
+                    );
                 }
             }
-            System.out.println("Все письма отправлены");
-
-        } catch (InterruptedException e) {
-            LOGGER.log(Level.SEVERE, "Ошибка во время выполнения потока", e);
-            throw e;
         }
+        System.out.println("Все письма отправлены");
+
     }
 }
