46f3f3d6ae3ae24afd2a9d5fe493ab5bf780c87b
Matvey <mashptz@gmail.com>
2024-10-19 10:40:03 +0300

    Добавление синхронизации в методе calculateTotalPower
    Добавление логирования и стектрейса

--- src/main/java/school/faang/BJS2_35894/Main.java
+++ src/main/java/school/faang/BJS2_35894/Main.java
@@ -7,17 +7,27 @@ import school.faang.BJS2_35894.wars.Mage;
 import school.faang.BJS2_35894.wars.Swordsman;
 import school.faang.BJS2_35894.wars.Werewolf;
 
-public class Main {
-    public static void main(String[] args) throws InterruptedException {
-        Army army = new Army();
-        army.addUnit(new Archer(25));
-        army.addUnit(new Swordsman(40));
-        army.addUnit(new Mage(50));
-        army.addUnit(new Mage(20));
-        army.addUnit(new Dwarf(100));
-        army.addUnit(new Werewolf(900));
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
-        int totalPower = army.calculateTotalPower();
-        System.out.println("Общая сила армии: " + totalPower);
+public class Main {
+    private static final Logger LOGGER = Logger.getLogger(Main.class.getName());
+
+    public static void main(String[] args) throws InterruptedException {
+        try {
+            Army army = new Army();
+            army.addUnit(new Archer(25));
+            army.addUnit(new Swordsman(40));
+            army.addUnit(new Mage(50));
+            army.addUnit(new Mage(20));
+            army.addUnit(new Dwarf(100));
+            army.addUnit(new Werewolf(900));
+
+            int totalPower = army.calculateTotalPower();
+            System.out.println("Общая сила армии: " + totalPower);
+        } catch (InterruptedException e) {
+            LOGGER.log(Level.SEVERE, "Ошибка в главном потоке программы", e);
+            throw e;
+        }
     }
 }
