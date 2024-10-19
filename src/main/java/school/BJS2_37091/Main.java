47ea7842223ef39dcfbc823b227d5b244e49bcdc
Roman Nifontov <romannif@mail.ru>
2024-10-19 22:24:44 +0500

    коммит

--- src/main/java/school/BJS2_37091/Main.java
+++ src/main/java/school/BJS2_37091/Main.java
@@ -2,12 +2,12 @@ package school.BJS2_37091;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.Callable;
 import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.Executors;
 import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.ScheduledFuture;
 import java.util.concurrent.TimeUnit;
-import java.util.concurrent.atomic.AtomicBoolean;
 
 public class Main {
 
