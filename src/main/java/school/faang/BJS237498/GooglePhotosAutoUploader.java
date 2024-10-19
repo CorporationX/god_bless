5ff7a2ce963776b1e34e9cd132d03cc8bae8ec47
Doniyor Kurbanov <doniyor.kurbanov.24@gmail.com>
2024-10-19 17:44:39 +0500

    BJS2-37498. Жду от вас фитбека спасибо

---
+++ src/main/java/school/faang/BJS237498/GooglePhotosAutoUploader.java
@@ -0,0 +1,39 @@
+package school.faang.BJS237498;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class GooglePhotosAutoUploader {
+    private static final Object lock = new Object();
+    private static final List<String> photosToUpload = new ArrayList<>();
+
+    public void startAutoUpload() {
+        while (true) {
+            synchronized (lock) {
+                if (photosToUpload.isEmpty()) {
+                    try {
+                        System.out.println("Фоток нет");
+                        lock.wait();
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
+                uploadPhotos();
+            }
+        }
+    }
+
+    public void onNewPhotoAdded(String photoPath) {
+         synchronized (lock) {
+             photosToUpload.add(photoPath);
+             System.out.println("Путь добавлен");
+             lock.notify();
+         }
+    }
+
+    public void uploadPhotos() {
+        photosToUpload.forEach(photo -> System.out.println("Загружаю фотографию " + photo));
+        System.out.println("Удаляю фотографии");
+        photosToUpload.clear();
+    }
+}
