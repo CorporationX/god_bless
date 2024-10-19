cdbc57e9863eab0c7708926526715da990660668
“Andrey <aiegorovceo@yandex.ru>
2024-10-19 23:31:57 +0300

    feat: Create Chore class implementing Runnable

---
+++ src/main/java/school/faang/theywerevnicebutextrpoor/Chore.java
@@ -0,0 +1,21 @@
+package school.faang.theywerevnicebutextrpoor;
+
+import lombok.AllArgsConstructor;
+
+@AllArgsConstructor
+public class Chore implements Runnable {
+    private String chore;
+
+    @Override
+    public void run() {
+        try {
+            System.out.println(Thread.currentThread().getName() + " is performing the task: " + chore);
+            // Simulate task execution
+            Thread.sleep(2000); // Delay of 2 seconds to show task execution
+            System.out.println(Thread.currentThread().getName() + " has completed the task: " + chore);
+        } catch (InterruptedException e) {
+            System.err.println("The task was interrupted: " + chore);
+            Thread.currentThread().interrupt();
+        }
+    }
+}
