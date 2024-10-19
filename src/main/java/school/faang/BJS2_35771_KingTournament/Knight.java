aa389a1212d90196c0100e644e2832065d2acc7a
Maxim Gorobets <gorobets.maks@bk.ru>
2024-10-20 01:32:19 +1000

    new Knight class

---
+++ src/main/java/school/faang/BJS2_35771_KingTournament/Knight.java
@@ -0,0 +1,38 @@
+package school.faang.BJS2_35771_KingTournament;
+
+import lombok.Getter;
+import lombok.SneakyThrows;
+
+import java.util.List;
+import java.util.ArrayList;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
+
+@Getter
+public class Knight {
+    private static final int TERMINATION_WAIT_SECONDS = 15;
+
+    private String name;
+    private List<Trial> trials;
+
+    public Knight(String name) {
+        this.name = name;
+        trials = new ArrayList<>();
+    }
+
+    public void addTrial(Trial trial) {
+        trials.add(trial);
+    }
+
+    @SneakyThrows
+    public void startTrials() {
+        int trialsCount = trials.size();
+        ExecutorService service = Executors.newFixedThreadPool(trialsCount);
+        trials.forEach(service::submit);
+        service.shutdown();
+        if (service.awaitTermination(TERMINATION_WAIT_SECONDS, TimeUnit.SECONDS)) {
+            service.shutdownNow();
+        }
+    }
+}
