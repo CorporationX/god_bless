c96291171aa45bfe14a206b5863426ab46626587
Shap|Misha <maxim.ivanov.jr@gmail.com>
2024-10-19 06:09:40 +0300

    added classes: UserActionAnalyzerTest
    added method: testActionTypePercentages,testTopCommentersLastMonth,testTopPopularHashtags,testTopActiveUsers for UserActionAnalyzerTest

--- src/main/java/school/faang/bjs2_35616/service/UserActionAnalyzer.java
+++ src/main/java/school/faang/bjs2_35616/service/UserActionAnalyzer.java
@@ -1,5 +1,6 @@
 package school.faang.bjs2_35616.service;
 
+import lombok.NonNull;
 import school.faang.bjs2_35616.model.ActionType;
 import school.faang.bjs2_35616.model.Pair;
 import school.faang.bjs2_35616.model.UserAction;
@@ -13,7 +14,7 @@ import java.util.Map;
 import java.util.stream.Collectors;
 
 public class UserActionAnalyzer {
-    public static List<String> topActiveUsers(List<UserAction> actions) {
+    public static List<String> topActiveUsers(@NonNull List<UserAction> actions) {
         return actions.stream()
                 .map(originalUserAction -> {
                     Long countActionByUser = actions.stream()
@@ -28,7 +29,7 @@ public class UserActionAnalyzer {
                 .toList();
     }
 
-    public static List<String> topPopularHashtags(List<UserAction> actions) {
+    public static List<String> topPopularHashtags(@NonNull List<UserAction> actions) {
         return actions.stream()
                 .filter(userAction -> userAction.getActionType().equals(ActionType.POST) || userAction.getActionType().equals(ActionType.COMMENT))
                 .filter(userAction -> !(userAction.getContent().isEmpty()))
@@ -52,7 +53,7 @@ public class UserActionAnalyzer {
                 .toList();
     }
 
-    public static List<String> topCommentersLastMonth(List<UserAction> actions) {
+    public static List<String> topCommentersLastMonth(@NonNull List<UserAction> actions) {
         return actions.stream()
                 .filter(userAction -> userAction.getActionType().equals(ActionType.COMMENT))
                 .filter(userAction -> ChronoUnit.DAYS.between(userAction.getActionDate(), LocalDate.now()) >= 0 &&
@@ -73,12 +74,12 @@ public class UserActionAnalyzer {
                 .toList();
     }
 
-    public static Map<String, Double> actionTypePercentages(List<UserAction> actions) {
+    public static Map<String, Double> actionTypePercentages(@NonNull List<UserAction> actions) {
         return actions.stream()
                 .collect(Collectors.groupingBy(UserAction::getActionType))
                 .entrySet().stream()
                 .collect(Collectors.toMap(actionTypeListEntry -> actionTypeListEntry.getKey().name(),
-                        actionTypeListEntry -> Math.round((((double) actionTypeListEntry.getValue().size() / actions.size()) * 100) * 10) / 10.0
+                        actionTypeListEntry -> Math.round(((double) actionTypeListEntry.getValue().size() / actions.size()) * 100 * 10) / 10.0
                 ));
     }
 }
