18429a9ab45e45715403376ba5750d84a365263c
Matvey <mashptz@gmail.com>
2024-10-19 10:39:59 +0300

    Добавление синхронизации в методе calculateTotalPower
    Добавление логирования и стектрейса

--- src/main/java/school/faang/BJS2_35894/wars/Army.java
+++ src/main/java/school/faang/BJS2_35894/wars/Army.java
@@ -2,8 +2,11 @@ package school.faang.BJS2_35894.wars;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 public class Army {
+    private static final Logger LOGGER = Logger.getLogger(Army.class.getName());
     private List<Unit> army = new ArrayList<>();
     private int armyDamage = 0;
 
@@ -12,18 +15,28 @@ public class Army {
     }
 
     public int calculateTotalPower() throws InterruptedException {
-        List<Thread> threads = new ArrayList<>();
-        for (Unit unit : army) {
-            int power = unit.getPower();
-            Thread thread = new Thread(() -> armyDamage = armyDamage + power);
-            threads.add(thread);
-            thread.start();
-        }
+        try {
+            List<Thread> threads = new ArrayList<>();
+            for (Unit unit : army) {
+                int power = unit.getPower();
+                Thread thread = new Thread(() -> {
+                    synchronized (this) {
+                        armyDamage += power;
+                    }
+                });
+                threads.add(thread);
+                thread.start();
+            }
 
-        for (Thread thread : threads) {
-            thread.join();
-        }
+            for (Thread thread : threads) {
+                thread.join();
+            }
 
-        return armyDamage;
+            return armyDamage;
+
+        } catch (InterruptedException e) {
+            LOGGER.log(Level.SEVERE, "Ошибка при вычислении общей силы армии", e);
+            throw e;
+        }
     }
 }
