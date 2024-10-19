2fe5aba44d7308e3828b581a4a0ff9c99023ee82
Nikolay Orlenko <norlenko2801@gmail.com>
2024-10-19 13:57:03 +0300

    кол-во потоков в пуле вынес в константу

--- src/main/java/school/faang/bro/force/Main.java
+++ src/main/java/school/faang/bro/force/Main.java
@@ -4,10 +4,11 @@ import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
 public class Main {
+    private static final int THREAD_COUNT = 3;
 
     public static void main(String[] args) {
         Game game = new Game(3);
-        ExecutorService executor = Executors.newFixedThreadPool(3);
+        ExecutorService executor = Executors.newFixedThreadPool(THREAD_COUNT);
         for (int i = 0; i < 4; i++) {
             executor.submit(() -> game.update(new Update(true, true)));
         }
