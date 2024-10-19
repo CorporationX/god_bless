b7759014d2bc4caf6ea0835b33cad6dfe7652e81
SerUKalinin <serukalinin@gmail.com>
2024-10-19 18:52:24 +0300

    Removed student's code

--- src/main/java/school/faang/BJS2_31451/User.java
+++ src/main/java/school/faang/BJS2_31451/User.java
@@ -1,25 +1,21 @@
 package school.faang.BJS2_31451;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.Getter;
+import lombok.ToString;
 
 import java.util.*;
 import java.util.stream.Collectors;
 
-@Data
+@ToString
 @Getter
+@AllArgsConstructor
 public class User {
     private String name;
     private int age;
     private String workplace;
     private String address;
-
-    public User(String name, int age, String workplace, String address) {
-        this.name = name;
-        this.age = age;
-        this.workplace = workplace;
-        this.address = address;
-    }
 
     public static Map<Integer, List<User>> groupUsers(List<User> users) {
         return users.stream().collect(Collectors.groupingBy(User::getAge));
