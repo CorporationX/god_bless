732fcddeb770546f4b46a0b6707760e73bc73fa5
“Andrey <aiegorovceo@yandex.ru>
2024-10-19 23:35:37 +0300

    Update

--- src/main/java/school/faang/theywerevnicebutextrpoor/Chore.java
+++ src/main/java/school/faang/theywerevnicebutextrpoor/Chore.java
@@ -10,7 +10,7 @@ public class Chore implements Runnable {
     public void run() {
         try {
             System.out.println(Thread.currentThread().getName() + " is performing the task: " + chore);
-            Thread.sleep(2000); // Delay of 2 seconds to show task execution
+            Thread.sleep(2000);
             System.out.println(Thread.currentThread().getName() + " has completed the task: " + chore);
         } catch (InterruptedException e) {
             System.err.println("The task was interrupted: " + chore);
