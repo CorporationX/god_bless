8dd13c03b77c778b52908e8548794c07d3872fa4
Maxim Gorobets <gorobets.maks@bk.ru>
2024-10-20 02:29:23 +1000

    new Person class

---
+++ src/main/java/school/faang/BJS2_35972_HelpColleague/Person.java
@@ -0,0 +1,13 @@
+package school.faang.BJS2_35972_HelpColleague;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class Person {
+    private String name;
+    private String surname;
+    private int age;
+    private String workplace;
+}
