fb0adeb17c3e6e0a2891b649ad92dbec8d156fbd
Dmitrii Pazinich <pazinich.d@yandex.ru>
2024-10-19 19:41:55 +0300

    Task "Telegram bot" solution.

---
+++ src/main/java/school/faang/sprint_3/bjs2_37266_telegramBot/Main.java
@@ -0,0 +1,36 @@
+package school.faang.sprint_3.bjs2_37266_telegramBot;
+
+import lombok.extern.slf4j.Slf4j;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
+import java.util.stream.IntStream;
+
+@Slf4j
+public class Main {
+    public static final int THREADS_COUNT = 5;
+
+    public static void main(String[] args) {
+        TelegramBot bot = new TelegramBot();
+        ExecutorService executor = Executors.newFixedThreadPool(THREADS_COUNT);
+
+        IntStream.range(0, 81)
+                .forEach(i -> executor.submit(
+                        () -> bot.sendMessage("Message " + i))
+                );
+
+        executor.shutdown();
+        try {
+            if (!executor.awaitTermination(3, TimeUnit.MINUTES)) {
+                log.info("Something went wrong, shutdown now!");
+                executor.shutdownNow();
+            } else {
+                log.info("Task completed!");
+            }
+        } catch (InterruptedException e) {
+            log.error("An error occurred while threads shutting down", e);
+            throw new RuntimeException(e);
+        }
+    }
+}
---
+++ src/main/java/school/faang/sprint_3/bjs2_37266_telegramBot/TelegramBot.java
@@ -0,0 +1,40 @@
+package school.faang.sprint_3.bjs2_37266_telegramBot;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+public class TelegramBot {
+    public static final int REQUEST_LIMIT = 5;
+    private int requestCounter;
+    private long lastRequestTime;
+
+    public TelegramBot() {
+        requestCounter = 0;
+        lastRequestTime = System.currentTimeMillis();
+    }
+
+    public synchronized void sendMessage(String message) {
+        long currentTime = System.currentTimeMillis();
+        long timeDelta = currentTime - lastRequestTime;
+
+        if (timeDelta < 1000) {
+            requestCounter++;
+            log.info("{}", requestCounter);
+            if (requestCounter >= REQUEST_LIMIT) {
+                log.info("Request counter is over the limit, waiting for {} ms", 1000 - timeDelta);
+                try {
+                    Thread.sleep(1000 - timeDelta);
+                } catch (InterruptedException e) {
+                    log.error("An error occurred while sleeping", e);
+                    throw new RuntimeException(e);
+                }
+                requestCounter = 0;
+            }
+        } else {
+            requestCounter = 1;
+            lastRequestTime = System.currentTimeMillis();
+        }
+
+        log.info("SENDING MESSAGE {}", message);
+    }
+}
