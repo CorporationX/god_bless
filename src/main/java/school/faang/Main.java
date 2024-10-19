0ab1a1bd2b953f9b16485c0942ed13e8077bc28e
lomuserg <sergeymu2015@yandex.ru>
2024-10-19 21:00:31 +0300

    realize task

--- src/main/java/school/faang/Main.java
+++ src/main/java/school/faang/Main.java
@@ -1,8 +1,21 @@
 package school.faang;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
-
+        Player player = new Player();
+        List<Thread> threads = new ArrayList<>(List.of(
+                new Thread(player::play),
+                new Thread(player::play),
+                new Thread(player::pause),
+                new Thread(player::skip),
+                new Thread(player::pause),
+                new Thread(player::skip),
+                new Thread(player::skip),
+                new Thread(player::play)
+        ));
+       threads.forEach(Thread::start);
     }
-
 }
--- src/main/java/school/faang/Player.java
+++ src/main/java/school/faang/Player.java
@@ -1,21 +1,46 @@
 package school.faang;
 
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
 public class Player {
-    private Object lock = new Object();
+    private  Object lock = new Object();
+    private  boolean isPlayer;
 
-    public void play(){
-
+    public void play() {
+        synchronized (lock) {
+            if (!isPlayer) {
+                isPlayer = true;
+                System.out.println("Play music");
+            } else {
+                System.out.println("music playing");
+            }
+        }
     }
 
-    public void pause(){
-
+    public void pause() {
+        synchronized (lock) {
+            if (isPlayer) {
+                isPlayer = false;
+                System.out.println("Pause music");
+            } else {
+                System.out.println("music paused");
+            }
+        }
     }
 
-    public void skip(){
-
+    public void skip() {
+        synchronized (lock) {
+            System.out.println("skip");
+        }
     }
 
-    public void previous(){
-
+    public void previous() {
+        synchronized (lock) {
+            System.out.println("previous");
+        }
     }
+
 }
