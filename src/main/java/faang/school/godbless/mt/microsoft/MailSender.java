2da7db65d1e27ba2442a45beedcb0b2724223b01
Ivan Semikov <semikov.exf@gmail.com>
2024-10-19 12:24:31 +0300

    BJS2-37775 Рефакторинг

--- src/main/java/faang/school/godbless/mt/microsoft/MailSender.java
+++ src/main/java/faang/school/godbless/mt/microsoft/MailSender.java
@@ -4,29 +4,28 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class MailSender {
+    private final static int THREAD_COUNT = 5;
 
     public static void main(String[] args) {
         final int messageCount = 1000;
-        final int threadsCount = 5;
 
-        sendMessages(messageCount, threadsCount);
+        new MailSender().sendMessages(messageCount);
     }
 
-    public static void sendMessages(int messageCount, int threadCount) {
-        final int batchSize = messageCount / threadCount;
+    public void sendMessages(int messageCount) {
+        final int batchSize = messageCount / THREAD_COUNT;
         List<Thread> threads = new ArrayList<>();
-        for (int i = 0; i < threadCount; i++) {
+        for (int i = 0; i < THREAD_COUNT; i++) {
             var start = i * batchSize;
             var end = (i + 1) * batchSize;
             var thread = new Thread(new SenderRunnable(start, end));
             threads.add(thread);
             thread.start();
         }
-
         handleEndSending(threads);
     }
 
-    private static void handleEndSending(List<Thread> threads) {
+    private void handleEndSending(List<Thread> threads) {
         try {
             for (var thread : threads) {
                 thread.join();
