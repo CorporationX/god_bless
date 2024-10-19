299186be5eace9b75f399c707b16612bcf069e53
Maxim Gorobets <gorobets.maks@bk.ru>
2024-10-20 02:38:52 +1000

    added annotations for csv parsing

--- src/main/java/school/faang/BJS2_35972_HelpColleague/Person.java
+++ src/main/java/school/faang/BJS2_35972_HelpColleague/Person.java
@@ -1,13 +1,21 @@
 package school.faang.BJS2_35972_HelpColleague;
 
+import com.opencsv.bean.CsvBindByName;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
 @Getter
 @AllArgsConstructor
 public class Person {
+    @CsvBindByName(column = "name")
     private String name;
+
+    @CsvBindByName(column = "surname")
     private String surname;
+
+    @CsvBindByName(column = "age")
     private int age;
+
+    @CsvBindByName(column = "workplace")
     private String workplace;
 }
