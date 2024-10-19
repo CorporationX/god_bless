40ac8948612d70abc15e6f907dcb1f377e1ea678
“Andrey <aiegorovceo@yandex.ru>
2024-10-20 00:15:17 +0300

    feat: Create GriffinsFoodDelivery class to manage and launch tasks feat: Use ExecutorService with a fixed pool to handle concurrent deliveries

---
+++ src/main/java/school/faang/feedpetergriffin/GriffinsFoodDelivery.java
@@ -0,0 +1,13 @@
+package school.faang.feedpetergriffin;
+
+import java.util.Random;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+public class GriffinsFoodDelivery {
+    public static void main(String[] args) {
+        ExecutorService executorService = Executors.newFixedThreadPool(3);
+
+        String[] characterNames = {"Peter", "Lois", "Meg", "Chris", "Stewie"};
+    }
+}
