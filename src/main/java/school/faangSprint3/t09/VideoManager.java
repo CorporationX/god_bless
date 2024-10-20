67bfb84075dfea2ff7523cbec887a044e41ea472
Igor <asachiyigor@gmail.com>
2024-10-20 16:52:36 +0300

    BJS2-37647
    refactoring code

--- src/main/java/school/faangSprint3/t09/VideoManager.java
+++ src/main/java/school/faangSprint3/t09/VideoManager.java
@@ -6,11 +6,11 @@ import java.util.concurrent.ConcurrentHashMap;
 public class VideoManager {
     private final Map<String, Integer> viewsMap = new ConcurrentHashMap<>();
 
-    public void addView(String videoId) {
+    public synchronized void addView(String videoId) {
         viewsMap.merge(videoId, 1, Integer::sum);
     }
 
-    public int getViewCount(String videoId) {
+    public synchronized int getViewCount(String videoId) {
         return viewsMap.getOrDefault(videoId, 0);
     }
 }
