b432822bbd75f4f0157797dfef392517cfe0958f
Home Alome <yevgengeodakyan@gmail.com>
2024-10-20 10:00:34 -0400

    updates

--- src/main/java/school/faang_sprint_3/you_working_in_microsoft/MailSender.java
+++ src/main/java/school/faang_sprint_3/you_working_in_microsoft/MailSender.java
@@ -2,22 +2,26 @@ package school.faang_sprint_3.you_working_in_microsoft;
 
 public class MailSender {
 
-    public static void main(String[] args) throws InterruptedException {
+    public static void main(String[] args) {
 
         var lettersAmount = 1000;
         var threadsAmount = 5;
         var partition = lettersAmount / threadsAmount;
 
-        Thread[] threads = new Thread[threadsAmount];
+        var threads = new Thread[threadsAmount];
         for (int i = 0; i < threads.length; i++) {
-            int begin = i * partition;
-            int end = (i + 1) * partition;
+            var begin = i * partition;
+            var end = (i + 1) * partition;
             threads[i] = new Thread(new SenderRunnable(begin, end));
             threads[i].start();
         }
 
         for (var thread : threads) {
-            thread.join();
+            try {
+                thread.join();
+            } catch (InterruptedException e) {
+                throw new ThreadInterruptException(Thread.currentThread().getName() + " interrupted");
+            }
         }
         System.out.println("Successfully completed");
     }
---
+++ src/main/java/school/faang_sprint_3/you_working_in_microsoft/ThreadInterruptException.java
@@ -0,0 +1,7 @@
+package school.faang_sprint_3.you_working_in_microsoft;
+
+public class ThreadInterruptException extends RuntimeException {
+    public ThreadInterruptException(String message) {
+        super(message);
+    }
+}
