0692dbb0ffd46fc75188fb064a1dca7d31be455d
yestdmitriikirichenko <yakitayka@gmail.com>
2024-10-20 10:44:58 +0300

    youWorkMicrosoft

--- src/main/java/youWorkMicrosoft/MailSender.java
+++ src/main/java/youWorkMicrosoft/MailSender.java
@@ -2,12 +2,11 @@ package youWorkMicrosoft;
 
 public class MailSender {
     public static void main(String[] args) throws InterruptedException {
-        Thread thread1 = new Thread(new SenderRunnable(1,200));
-        Thread thread2 = new Thread(new SenderRunnable(201,400));
-        Thread thread3 = new Thread(new SenderRunnable(401,600));
-        Thread thread4 = new Thread(new SenderRunnable(601,800));
-        Thread thread5 = new Thread(new SenderRunnable(801,1000));
-
+        Thread thread1 = new Thread(new SenderRunnable(1, 200));
+        Thread thread2 = new Thread(new SenderRunnable(201, 400));
+        Thread thread3 = new Thread(new SenderRunnable(401, 600));
+        Thread thread4 = new Thread(new SenderRunnable(601, 800));
+        Thread thread5 = new Thread(new SenderRunnable(801, 1000));
 
         thread1.start();
         thread2.start();
