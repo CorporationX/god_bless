a5e85f50b86b2ffbb7cb21c9a086517016cb5467
m1rroxxx <Nikita.sidorov.7@icloud.com>
2024-10-21 00:59:56 +0300

    поменял do while на while упросив логику и небольшие поправки

--- src/main/java/school/faang/gameOfThrones/House.java
+++ src/main/java/school/faang/gameOfThrones/House.java
@@ -7,21 +7,21 @@ import java.util.List;
 import java.util.Set;
 
 public class House {
-  @Getter
-  private final List<String> roles;
-  private final Set<String> availableRoles;
+    @Getter
+    private final List<String> roles;
+    private final Set<String> availableRoles;
 
-  House(List<String> roles) {
-    this.roles = roles;
-    availableRoles = new HashSet<>(roles);
-  }
+    House(List<String> roles) {
+        this.roles = roles;
+        availableRoles = new HashSet<>(roles);
+    }
 
-  public void addAvailableRole(String role) {
-    availableRoles.add(role);
-    notifyAll();
-  }
+    public void addAvailableRole(String role) {
+        availableRoles.add(role);
+        notifyAll();
+    }
 
-  public boolean giveAvailableRole(String role) {
-    return availableRoles.remove(role);
-  }
+    public boolean giveAvailableRole(String role) {
+        return availableRoles.remove(role);
+    }
 }
--- src/main/java/school/faang/gameOfThrones/Main.java
+++ src/main/java/school/faang/gameOfThrones/Main.java
@@ -7,52 +7,52 @@ import java.util.concurrent.Executors;
 import java.util.concurrent.TimeUnit;
 
 public class Main {
-  private static final int THREAD_POOL_SIZE = 10;
+    private static final int THREAD_POOL_SIZE = 10;
 
-  public static void main(String[] args) {
-    List<String> roles = new ArrayList<>();
-    roles.add("Lord");
-    roles.add("knight");
-    roles.add("mage");
+    public static void main(String[] args) {
+        List<String> roles = new ArrayList<>();
+        roles.add("Lord");
+        roles.add("knight");
+        roles.add("mage");
 
-    House house = new House(roles);
+        House house = new House(roles);
 
-    User DaenerysTargaryen = new User("Daenerys Targaryen");
-    User TyrionLannister = new User("Tyrion Lannister");
-    User AryaStark = new User("Arya Stark");
-    User JonSnow = new User("Jon Snow");
-    User CerseiLannister = new User("Cersei Lannister");
+        User DaenerysTargaryen = new User("Daenerys Targaryen");
+        User TyrionLannister = new User("Tyrion Lannister");
+        User AryaStark = new User("Arya Stark");
+        User JonSnow = new User("Jon Snow");
+        User CerseiLannister = new User("Cersei Lannister");
 
-    ExecutorService executorService = Executors.newFixedThreadPool(THREAD_POOL_SIZE);
-    executorService.submit(() -> DaenerysTargaryen.joinHouse(house, house.getRoles().get(0)));
-    executorService.submit(() -> TyrionLannister.joinHouse(house, house.getRoles().get(1)));
-    executorService.submit(() -> AryaStark.joinHouse(house, house.getRoles().get(2)));
+        ExecutorService executorService = Executors.newFixedThreadPool(THREAD_POOL_SIZE);
+        executorService.submit(() -> DaenerysTargaryen.joinHouse(house, house.getRoles().get(0)));
+        executorService.submit(() -> TyrionLannister.joinHouse(house, house.getRoles().get(1)));
+        executorService.submit(() -> AryaStark.joinHouse(house, house.getRoles().get(2)));
 
-    try {
-      Thread.sleep(1000);
-    } catch (InterruptedException e) {
-      throw new IllegalStateException(e.getMessage(), e);
+        try {
+            Thread.sleep(1000);
+        } catch (InterruptedException e) {
+            throw new IllegalStateException("поток был прерван " + e.getMessage(), e);
+        }
+
+        System.out.println("==================================================================");
+
+        executorService.submit(() -> JonSnow.joinHouse(house, house.getRoles().get(0)));
+        executorService.submit(() -> CerseiLannister.joinHouse(house, house.getRoles().get(2)));
+
+        executorService.submit(AryaStark::leaveHouse);
+        executorService.submit(DaenerysTargaryen::leaveHouse);
+
+        executorService.shutdown();
+
+        try {
+            if (executorService.awaitTermination(60, TimeUnit.SECONDS)) {
+                System.out.printf("поток %s дождался завершения других потоков\n", Thread.currentThread().getName());
+            } else {
+                executorService.shutdownNow();
+            }
+        } catch (InterruptedException e) {
+            executorService.shutdownNow();
+            throw new IllegalStateException("поток был прерван при ожидании завершения других потоков" + e.getMessage(), e);
+        }
     }
-
-    System.out.println("==================================================================");
-
-    executorService.submit(() -> JonSnow.joinHouse(house, house.getRoles().get(0)));
-    executorService.submit(() -> CerseiLannister.joinHouse(house, house.getRoles().get(2)));
-
-    executorService.submit(AryaStark::leaveHouse);
-    executorService.submit(DaenerysTargaryen::leaveHouse);
-
-    executorService.shutdown();
-
-    try {
-      if(executorService.awaitTermination(60, TimeUnit.SECONDS)) {
-        System.out.printf("поток %s дождался завершения других потоков\n", Thread.currentThread().getName());
-      }else {
-        executorService.shutdownNow();
-      }
-    } catch (InterruptedException e) {
-      executorService.shutdownNow();
-      throw new IllegalStateException(e.getMessage(), e);
-    }
-  }
 }
