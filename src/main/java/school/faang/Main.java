d389807124b471734abaa3991057af7858c6add4
lomuserg <sergeymu2015@yandex.ru>
2024-10-20 12:33:47 +0300

    realize all methods

--- src/main/java/school/faang/Main.java
+++ src/main/java/school/faang/Main.java
@@ -1,8 +1,21 @@
 package school.faang;
 
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
+        Boss boss = new Boss();
+
+        List<Thread> threads = new ArrayList<>();
+
+        for (int i = 0; i < 10; i++) {
+            int playerId = i;
+            threads.add(new Thread(() -> new Player("player" + playerId).startBattle(boss)));
+        }
+        threads.forEach(Thread::start);
+
 
     }
 
