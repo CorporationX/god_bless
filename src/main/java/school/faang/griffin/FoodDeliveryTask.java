ed7c37e2a419ab8441988ed288171f5a4e300632
abzalkhan00 <abzalkhanondakanov@gmail.com>
2024-10-20 15:37:44 +0500

    solved "Накормите Питера Гриффина" task

--- src/main/java/school/faang/griffin/FoodDeliveryTask.java
+++ src/main/java/school/faang/griffin/FoodDeliveryTask.java
@@ -8,6 +8,7 @@ import java.util.Random;
 public class FoodDeliveryTask implements Runnable {
     private String character;
     private int foodAmount;
+    private static final int MILLISECONDS_TO_SECONDS = 1000;
 
     private String getFoodType() {
         String[] foodTypes = {"pizza", "burger", "hot dog", "chicken wings", "taco"};
@@ -16,6 +17,17 @@ public class FoodDeliveryTask implements Runnable {
 
     @Override
     public void run() {
-        System.out.println("Peter gets 50 : " + getFoodType());
+        String foodType = getFoodType();
+
+        try {
+            System.out.println(character + " takes " + foodAmount + " " + foodType);
+            Random random = new Random();
+            int randomSeconds = random.nextInt(5) + 1;
+            Thread.sleep(randomSeconds * MILLISECONDS_TO_SECONDS);
+            System.out.println(character + " is eating " + foodAmount + " " + foodType);
+        } catch (InterruptedException e) {
+            System.out.println(character + " didn't take " + foodAmount + foodType);
+            e.printStackTrace();
+        }
     }
 }
