f1a13473aaa7d1aef04fa0671a68c80a6c99eb2b
AVB <abdddev@gmail.com>
2024-10-20 15:09:06 +1000

    WOW

--- src/main/java/school/faang/multithreading/asynchronism/wow/QuestSystem.java
+++ src/main/java/school/faang/multithreading/asynchronism/wow/QuestSystem.java
@@ -3,11 +3,12 @@ package school.faang.multithreading.asynchronism.wow;
 import lombok.extern.slf4j.Slf4j;
 
 import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutorService;
 
 @Slf4j
 public class QuestSystem {
 
-    public CompletableFuture<Player> startQuest(Player player, Quest quest) {
+    public CompletableFuture<Player> startQuest(Player player, Quest quest, ExecutorService executorService) {
         return CompletableFuture.supplyAsync(() -> {
                     log.info("Игрок: {}, начинает асинхронное выполнение квеста, его опыт: {}", player.getName(), player.getExperience());
                     log.info("Игрок: {}, начинает выполнять квест: {}, награда: {}", player.getName(), quest.getName(), quest.getReward());
@@ -19,7 +20,7 @@ public class QuestSystem {
                     }
                     player.addExperience(quest.getReward());
                     return player;
-                })
+                }, executorService)
                 .thenApply(playerResult -> {
                     log.info("Игрок: {}, получает опыт: {}", playerResult.getName(), quest.getReward());
                     log.info("Игрок: {}, стал сильнее, его опыт: {}", playerResult.getName(), playerResult.getExperience());
--- src/main/java/school/faang/multithreading/asynchronism/wow/WorldOfWarcraft.java
+++ src/main/java/school/faang/multithreading/asynchronism/wow/WorldOfWarcraft.java
@@ -3,23 +3,34 @@ package school.faang.multithreading.asynchronism.wow;
 import lombok.extern.slf4j.Slf4j;
 
 import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
 
 @Slf4j
 public class WorldOfWarcraft {
+    private static final int MAX_TIME_WORK = 5;
+
     public static void main(String[] args) {
         QuestSystem questSystem = new QuestSystem();
-
         Player player1 = new Player("Thrall", 10, 250);
         Player player2 = new Player("Sylvanas", 12, 450);
         Quest quest1 = new Quest("Defeat the Lich King", 10, 150);
         Quest quest2 = new Quest("Retrieve the Sword of Azeroth", 8, 100);
 
-        CompletableFuture<Player> player1Quest = questSystem.startQuest(player1, quest1);
-        CompletableFuture<Player> player2Quest = questSystem.startQuest(player2, quest2);
+        ExecutorService executorService = Executors.newFixedThreadPool(2);
+
+        CompletableFuture<Player> player1Quest = questSystem.startQuest(player1, quest1, executorService);
+        CompletableFuture<Player> player2Quest = questSystem.startQuest(player2, quest2, executorService);
 
         player1Quest.thenAccept(player -> log.info("Игрок: {} успешно завершил квест", player.getName()));
         player2Quest.thenAccept(player -> log.info("Игрок: {} успешно завершил квест", player.getName()));
 
-        CompletableFuture.allOf(player1Quest, player2Quest).join();
+        executorService.shutdown();
+        try {
+            executorService.awaitTermination(MAX_TIME_WORK, TimeUnit.SECONDS);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
