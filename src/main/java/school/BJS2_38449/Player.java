acfcc219acdf4480d3459ef7e6fdac8d2fd42107
Roman Nifontov <romannif@mail.ru>
2024-10-20 09:46:48 +0500

    Сделал задачу BJS2-38449

---
+++ src/main/java/school/BJS2_38449/Player.java
@@ -0,0 +1,16 @@
+package school.BJS2_38449;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@AllArgsConstructor
+@Getter
+@Setter
+public class Player {
+
+    private String name;
+    private int level;
+    private int experience;
+
+}
