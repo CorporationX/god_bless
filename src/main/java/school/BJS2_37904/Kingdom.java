0ae8d8bf81d9d645c3733be4bec50a290a950e8a
Roman Nifontov <romannif@mail.ru>
2024-10-20 17:37:57 +0500

    Сделал задачу BJS2-37904

---
+++ src/main/java/school/BJS2_37904/Kingdom.java
@@ -0,0 +1,24 @@
+package school.BJS2_37904;
+
+import lombok.Getter;
+import java.util.Random;
+
+@Getter
+public class Kingdom {
+
+    private String name;
+    private Random rand = new Random();
+
+    public Kingdom(String name) {
+        this.name = name;
+    }
+
+    public String sendMessage(Kingdom receiver, String message) throws Exception {
+        if (rand.nextBoolean()) {
+            return "Сообщение \"" + message + "\" было отправлено из королевства: " + name;
+        } else {
+            throw new Exception();
+        }
+    }
+
+}
