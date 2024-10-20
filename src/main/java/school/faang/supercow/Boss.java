2adfe0654dce211eaeafb482890f0f4f04eeb411
bogda <bogdanskrip404@gmail.com>
2024-10-20 20:31:56 +0500

    Supercow!

---
+++ src/main/java/school/faang/supercow/Boss.java
@@ -0,0 +1,39 @@
+package school.faang.supercow;
+
+import lombok.Data;
+
+@Data
+public class Boss {
+    private String name;
+    private int maxPlayers;
+    private int currentPlayers;
+
+    public void printSlots(){
+        System.out.println("Слоты: " + getCurrentPlayers() + "/" + getMaxPlayers());
+        System.out.println();
+    }
+
+    public Boss(String name, int maxPlayers) {
+        this.name = name;
+        this.maxPlayers = maxPlayers;
+    }
+
+    public synchronized void joinBattle(Player player) throws InterruptedException {
+        {
+            if (maxPlayers <= currentPlayers) {
+                System.out.println(player.getName() + " ждет свободного слота.");
+                wait();
+            }
+            currentPlayers++;
+            System.out.println(player.getName() + " присоединился к бивте с " + getName());
+            printSlots();
+        }
+    }
+
+    public synchronized void leaveBattle(Player player) {
+        currentPlayers--;
+        System.out.println("Игрок: " + player.getName() + " покинул бой.");
+        notify();
+        printSlots();
+    }
+}
