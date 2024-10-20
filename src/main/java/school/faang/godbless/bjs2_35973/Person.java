4e00443321b7b769ac63580556fe42bea56e6cd7
AnnaAplavina <59871896+AnnaAplavina@users.noreply.github.com>
2024-10-20 12:01:16 +0300

    printing workers

---
+++ src/main/java/school/faang/godbless/bjs2_35973/Person.java
@@ -0,0 +1,13 @@
+package school.faang.godbless.bjs2_35973;
+
+import lombok.AllArgsConstructor;
+import lombok.ToString;
+
+@ToString
+@AllArgsConstructor
+public class Person {
+    private String name;
+    private String surname;
+    private int age;
+    private String workplace;
+}
