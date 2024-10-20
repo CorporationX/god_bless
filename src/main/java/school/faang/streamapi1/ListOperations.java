f217eb87b9e2b82dcb4df93202261483312f007e
Andrey <maandr@internet.ru>
2024-10-20 17:03:15 +0300

    BJS2-35665 Сorrected comments from code review

--- src/main/java/school/faang/streamapi1/ListOperations.java
+++ src/main/java/school/faang/streamapi1/ListOperations.java
@@ -6,30 +6,59 @@ import java.util.function.Predicate;
 
 public class ListOperations {
     public static int sumOfEvenNumbers(List<Integer> numbers) {
-        return numbers.stream().mapToInt(Integer::intValue).sum();//filter(n -> n % 2 == 0).reduce(0, Integer::sum);
+        return numbers.stream()
+                .mapToInt(Integer::intValue)
+                .sum();
     }
+
     public static int findMax(List<Integer> numbers) {
-        return numbers.stream().mapToInt(Integer::intValue).max().orElse(0);
+        return numbers.stream()
+                .mapToInt(Integer::intValue)
+                .max()
+                .orElseThrow(() -> new IllegalArgumentException("Список не может быть пустым"));
     }
+
     public static double findAverage(List<Integer> numbers) {
-        return numbers.stream().mapToInt(Integer::intValue).average().orElse(0);
+        return numbers.stream()
+                .mapToInt(Integer::intValue)
+                .average()
+                .orElse(0);
     }
+
     public static int countStringsStartingWith(List<String> strings, char firstChar) {
-        return strings.stream().filter(str -> str.charAt(0) == firstChar).mapToInt(e -> 1).sum();
+        return strings.stream()
+                .filter(str -> str.charAt(0) == firstChar)
+                .mapToInt(e -> 1)
+                .sum();
     }
+
     public static List<String> filterStringsContainingSubstring(List<String> strings, String substr) {
-        return strings.stream().filter(str -> str.contains(substr)).toList();
+        return strings.stream()
+                .filter(str -> str.contains(substr))
+                .toList();
     }
+
     public static List<String> sortByLength(List<String> strings) {
-        return strings.stream().sorted(Comparator.comparingInt(String::length)).toList();
+        return strings.stream()
+                .sorted(Comparator.comparingInt(String::length))
+                .toList();
     }
+
     public static boolean allMatchCondition(List<Integer> numbers, Predicate<Integer> condition) {
-        return numbers.stream().allMatch(condition);
+        return numbers.stream()
+                .allMatch(condition);
     }
+
     public static int findMinGreaterThan(List<Integer> numbers, int num) {
-        return numbers.stream().filter(n -> n > num).findFirst().orElse(0);
+        return numbers.stream()
+                .filter(n -> n > num)
+                .findFirst()
+                .orElseThrow(() -> new IllegalArgumentException("Список не может быть пустым"));
     }
+
     public static List<Integer> convertToLengths(List<String> strings) {
-        return strings.stream().map(String::length).toList();
+        return strings.stream()
+                .map(String::length)
+                .toList();
     }
 }
