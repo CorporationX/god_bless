f95d06e8e172eacda08b641c53b178709c9e9c14
Gadzhiev Gadzhi <Gadzhiev3222@gmail.com>
2024-10-21 17:23:29 +0300

    Качаем алхимию

--- src/main/java/school/faang/train_alchemic/main/Main.java
+++ src/main/java/school/faang/train_alchemic/main/Main.java
@@ -1,7 +1,20 @@
 package school.faang.train_alchemic.main;
 
+import school.faang.train_alchemic.main_code.Potion;
+import school.faang.train_alchemic.main_code.PotionService;
+
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
+        PotionService potionService = new PotionService();
 
+        List<Potion> potions = List.of(
+                new Potion("Healing Potion", 5),
+                new Potion("Mana Potion", 3),
+                new Potion("Stamina Potion", 4)
+        );
+
+        potionService.getAllIngredients(potions);
     }
 }
--- src/main/java/school/faang/train_alchemic/main_code/PotionService.java
+++ src/main/java/school/faang/train_alchemic/main_code/PotionService.java
@@ -2,17 +2,25 @@ package school.faang.train_alchemic.main_code;
 
 import java.util.List;
 import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.atomic.AtomicInteger;
 import java.util.stream.Collectors;
 
 public class PotionService {
-    private List<Potion> potions;
-
-    public void getAllIngredients() {
+    public void getAllIngredients(List<Potion> potions) {
         List<CompletableFuture<Integer>> futures = potions.stream()
-                .map(potion -> gatherIntredients(potion))
+                .map(this::gatherIntredients)
                 .collect(Collectors.toList());
 
-        CompletableFuture<Void> allIngredients = CompletableFuture.allOf();
+        CompletableFuture<Void> allIngredients =
+                CompletableFuture.allOf(futures.toArray(new CompletableFuture[0]));
+
+        allIngredients.thenRun(() -> {
+            AtomicInteger totalIngredients = new AtomicInteger(0);
+            futures.forEach(future -> totalIngredients.addAndGet(future.join()));
+            System.out.println("Общее количество собранных ингредиентов " + totalIngredients);
+        });
+
+        allIngredients.join();
     }
 
     private CompletableFuture<Integer> gatherIntredients(Potion potion) {
@@ -20,11 +28,12 @@ public class PotionService {
             try {
                 Thread.sleep(1000);
             } catch (InterruptedException e) {
-                throw new RuntimeException(e);
+                throw new RuntimeException("Ошибка при попытке получить инредиент " + e);
             }
 
             return potion.getRequiredIngredients();
         });
+
         return future;
     }
 }
