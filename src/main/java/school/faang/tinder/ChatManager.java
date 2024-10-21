24243f56afd4f8f473571e720bd445e66b709285
Nikolay Orlenko <norlenko2801@gmail.com>
2024-10-21 23:01:58 +0300

    Провел рефакторинг кода.

--- src/main/java/school/faang/tinder/ChatManager.java
+++ src/main/java/school/faang/tinder/ChatManager.java
@@ -6,12 +6,8 @@ import school.faang.tinder.exceptions.UserAlreadyInChatException;
 
 import java.util.Arrays;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
-import java.util.Random;
-import java.util.Set;
-import java.util.stream.Collectors;
-import java.util.stream.Stream;
+import java.util.Optional;
 
 @AllArgsConstructor
 @Slf4j
@@ -21,52 +17,45 @@ public class ChatManager {
 
     public synchronized int startChat(User user) throws UserAlreadyInChatException {
         validateUser(user);
-        Random random = new Random();
-        List<User> interlocutorCandidates = getInterlocutorCandidates(user);
-        while (interlocutorCandidates.isEmpty()) {
+        Optional<User> interlocutorOptional = userList.getInterlocutorFor(user);
+        while (interlocutorOptional.isEmpty()) {
             try {
-                System.out.println("На данный момент нет доступных собеседников");
+                System.out.println("На данный момент нет доступных собеседников: " + user.getName());
                 this.wait();
             } catch (InterruptedException e) {
                 log.error(e.getMessage());
                 log.error(Arrays.toString(e.getStackTrace()));
             }
-            interlocutorCandidates = getInterlocutorCandidates(user);
+            System.out.println(user.getName() + " пытается найти чат");
+            interlocutorOptional = userList.getInterlocutorFor(user);
         }
-        User interlocutor = interlocutorCandidates.get(random.nextInt(interlocutorCandidates.size()));
+        User interlocutor = interlocutorOptional.orElseThrow(() ->
+                new IllegalArgumentException("Собеседник не может быть null"));
         Chat chat = new Chat(user, interlocutor);
         chatIdToChat.put(chat.getChatId(), chat);
-        switchChatSearchStatusForIntelocutors(user, interlocutor);
+        switchChatSearchStatusForInterlocutors(user, interlocutor);
         System.out.println("Чат " + chat + " ,был запущен!");
         return chat.getChatId();
-    }
-
-    private static void switchChatSearchStatusForIntelocutors(User user1, User user2) {
-        user1.setLookingForChat(!user1.isLookingForChat());
-        user2.setLookingForChat(!user2.isLookingForChat());
     }
 
     public synchronized void endChat(int chatId) {
         if (!chatIdToChat.containsKey(chatId)) {
             System.out.println("Чат " + chatId + " был удален");
+            return;
         }
         Chat removedChat = chatIdToChat.remove(chatId);
-        switchChatSearchStatusForIntelocutors(removedChat.getUser1(), removedChat.getUser2());
+        switchChatSearchStatusForInterlocutors(removedChat.getUser1(), removedChat.getUser2());
         System.out.println("Чат " + removedChat + " был завершен");
-        userList.notifyAll();
+        this.notifyAll();
+    }
+
+    private synchronized void switchChatSearchStatusForInterlocutors(User user1, User user2) {
+        user1.switchLookingForChat();
+        user2.switchLookingForChat();
     }
 
     public Chat getChat(int chatId) {
         return chatIdToChat.get(chatId);
-    }
-
-    private List<User> getInterlocutorCandidates(User user) {
-        Set<User> userInChats = chatIdToChat.values().stream()
-                .flatMap(chat -> Stream.of(chat.getUser1(), chat.getUser2()))
-                .collect(Collectors.toSet());
-        return userList.getOnlineUsers().stream()
-                .filter(currUser -> !user.equals(currUser) && !userInChats.contains(currUser))
-                .toList();
     }
 
     private static void validateUser(User user) throws UserAlreadyInChatException {
--- src/main/java/school/faang/tinder/exceptions/UserAlreadyInChatException.java
+++ src/main/java/school/faang/tinder/exceptions/UserAlreadyInChatException.java
@@ -1,6 +1,7 @@
 package school.faang.tinder.exceptions;
 
 public class UserAlreadyInChatException extends Exception {
+
     public UserAlreadyInChatException(String message) {
         super(message);
     }
