df15d014bd662c1cfe41b89d20449f4af851e62c
gimadeev <6962009@gmail.com>
2024-10-21 23:58:49 +0300

    PR fix: added "addUnits" method for unit creation and adding to unit list

--- src/main/java/distributed/army/homm/Main.java
+++ src/main/java/distributed/army/homm/Main.java
@@ -6,16 +6,23 @@ import java.util.List;
 public class Main {
     public static void main(String[] args) throws InterruptedException {
         List<Unit> units = new ArrayList<>();
-        units.add(new Archer(25));
-        units.add(new Archer(25));
-        units.add(new Swordsman(50));
-        units.add(new Swordsman(50));
-        units.add(new Mage(75));
-        units.add(new Mage(75));
-        units.add(new Mage(75));
+
+        addUnits(units, Archer.class, 25, 2);
+        addUnits(units, Swordsman.class, 50, 3);
+        addUnits(units, Mage.class, 75, 1);
 
         Army army = new Army(units);
         int totalPower = army.calculateTotalPower();
         System.out.println(totalPower);
+    }
+
+    private static void addUnits(List<Unit> targetList, Class<? extends Unit> unitClass, int power, int count) {
+        for (int i = 0; i < count; i++) {
+            try {
+                targetList.add(unitClass.getDeclaredConstructor(int.class).newInstance(power));
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
     }
 }
