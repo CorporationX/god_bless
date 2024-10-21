f663e111a5327404d8296942685f1ad38364e0a8
Viktor Krivetskiy <1woracc@gmail.com>
2024-10-22 00:32:22 +0300

    Mice are very nice - fix 2

--- src/main/java/nice/mice/House.java
+++ src/main/java/nice/mice/House.java
@@ -19,10 +19,12 @@ public class House {
         Room room1 = roomList.get(random.nextInt(roomList.size()));
         Room room2 = roomList.get(random.nextInt(roomList.size()));
         if ((room1.hasFood() && room2.hasFood())) {
-            collectedFood.addAll(room1.getFoodList());
-            room1.removeAllFood();
-            collectedFood.addAll(room2.getFoodList());
-            room2.removeAllFood();
+            synchronized (collectedFood) {
+                collectedFood.addAll(room1.getFoodList());
+                room1.removeAllFood();
+                collectedFood.addAll(room2.getFoodList());
+                room2.removeAllFood();
+            }
         }
     }
 
