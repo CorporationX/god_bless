61b1919c11a2027f55755eebd7b4bf87c7fa45a0
Slava <leeteso97@gmail.com>
2024-10-21 23:19:35 +0500

    Добавил нужный класс и методы

--- src/main/java/school/faang/BJS237051/User.java
+++ src/main/java/school/faang/BJS237051/User.java
@@ -10,7 +10,34 @@ public class User {
     private House house;
     private String role;
 
-    public void joinHouse(){
+    public User(String name) {
+        this.name = name;
+    }
 
+    public synchronized void joinHouse(House newHouse) {
+        synchronized (newHouse) {
+            try {
+                while (newHouse.getAvailableRoleCount() < 1) {
+                    System.out.println(name + " подожди пока найдут для тебя подходящую роль!");
+                    newHouse.wait();
+                }
+                house = newHouse;
+                role = newHouse.addRole();
+                System.out.println(name + " занимает роль " + role);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+
+            }
+        }
+    }
+
+    public synchronized void leaveHouse() {
+        synchronized (house) {
+            System.out.println(name + " отрекается от дома и роли " + role);
+            house.removeRole(role);
+            house.notifyAll();
+            house = new House();
+            role = "";
+        }
     }
 }
