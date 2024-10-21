2d720ba5d69c2ca8fc5e24bec6b66fc56f85c28d
“Andrey <aiegorovceo@yandex.ru>
2024-10-21 20:37:22 +0300

    feat:  Created VideoManagerTest class to simulate multiple threads concurrently adding views to videos

---
+++ src/main/java/school/faang/youtubeviewcounter/VideoManagerTest.java
@@ -0,0 +1,27 @@
+package school.faang.youtubeviewcounter;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
+
+public class VideoManagerTest {
+    private static final int NUM_THREADS = 100;
+    private static final int NUM_VIDEOS = 10;
+
+    public static void main(String[] args) {
+        VideoManager videoManager = new VideoManager();
+
+        ExecutorService executorService = Executors.newFixedThreadPool(NUM_THREADS);
+
+        for (int i = 0; i < NUM_VIDEOS; i++) {
+            String videoId = "video_" + i;
+            for (int j = 0; j < NUM_THREADS / NUM_VIDEOS; j++) {
+                executorService.submit(() -> {
+                    videoManager.addView(videoId);
+                    int viewCount = videoManager.getViewCount(videoId);
+                    System.out.println("Video: " + videoId + ", Current Views: " + viewCount);
+                });
+            }
+        }
+    }
+}
