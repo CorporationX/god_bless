8ee77e4e3bdbb2e629f9b687a1247e4ea07f2704
heckfy86 <marlinco263@gmail.com>
2024-10-21 21:51:11 +0200

    BJS2-37154 update started in a few flows

--- src/main/java/school/faang/broforce/Main.java
+++ src/main/java/school/faang/broforce/Main.java
@@ -1,17 +1,25 @@
 package school.faang.broforce;
 
 public class Main {
-    public static void main(String[] args) {
-        Game game = new Game(3);
-        while (!game.getIsGameOver()) {
-            game.update();
 
-            try {
-                Thread.sleep(500);
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
+    private final static int LIVES = 3;
+    private final static int COUNT_THREADS = 8;
+    public static void main(String[] args) {
+        Game game = new Game(LIVES);
+
+        Thread[] threads = new Thread[COUNT_THREADS];
+        for (int i = 0; i < COUNT_THREADS; i++) {
+            threads[i] = new Thread(() -> {
+                while (game.getLives() > 0) {
+                    game.update();
+                    try {
+                        Thread.sleep(1000);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
+            });
+            threads[i].start();
         }
-        System.out.println("Game finished. Final score: " + game.getScore());
     }
 }
