040dc0a8af64a6c4e8d31b16ebf9bbeb0ef91f6c
Viktor Krivetskiy <1woracc@gmail.com>
2024-10-21 23:23:40 +0300

    They were very nice but extremely poor - fix

--- src/main/java/extremely/poor/WeasleyFamily.java
+++ src/main/java/extremely/poor/WeasleyFamily.java
@@ -7,13 +7,9 @@ public class WeasleyFamily {
     public static void main(String[] args) {
         String[] chores = {"Помыть посуду", "Подмести пол", "Протереть пыль", "Выкинуть мусор", "Приготовить ужин"};
         ExecutorService executors = Executors.newCachedThreadPool();
-        try {
-            for (String chore : chores) {
-                executors.execute(new Chore(chore));
-            }
-            executors.shutdown();
-        } catch (OutOfMemoryError e) {
-            System.out.println("Недостаточно памяти" + e.getMessage());
+        for (String chore : chores) {
+            executors.execute(new Chore(chore));
         }
+        executors.shutdown();
     }
 }
