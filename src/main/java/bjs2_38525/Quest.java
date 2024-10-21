c517a0512d0dd42f87a29331affaa7afb26c47cb
light <estarosa@list.ru>
2024-10-21 17:20:34 +0300

    Решение: WOW

---
+++ src/main/java/bjs2_38525/Quest.java
@@ -0,0 +1,12 @@
+package bjs2_38525;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class Quest {
+    private String name;
+    private int difficulty;
+    private int reward;
+}
