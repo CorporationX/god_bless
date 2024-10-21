d64ae52960b0bde69900b8198342da0c1222062c
Andrew Balashov <balashov83@gmail.com>
2024-10-21 20:27:31 +0300

    BJS2-38009 Mastercard: task done

---
+++ src/main/java/school/faang/m1s4/bjs2_38009_mastercard/Main.java
@@ -0,0 +1,8 @@
+package school.faang.m1s4.bjs2_38009_mastercard;
+
+public class Main {
+    public static void main(String[] args) {
+        MasterCardService masterCardService = new MasterCardService();
+        masterCardService.doAll();
+    }
+}
