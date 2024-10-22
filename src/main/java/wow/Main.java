b8d3173f770b14db9b95c688b84fb022f603e677
Юрий <angelbes777@gmail.com>
2024-10-22 14:13:26 +0300

    BJS2-38486

---
+++ src/main/java/wow/Main.java
@@ -0,0 +1,28 @@
+package wow;
+
+import java.util.concurrent.CompletableFuture;
+
+public class Main {
+    /**
+     * The main entry point of the application. Initializes a quest system and two players,
+     * assigns quests to the players, and starts the quests asynchronously.
+     * Once a quest is completed, the player's name and updated experience points are printed.
+     *
+     * @param args Command line arguments (not used).
+     */
+    public static void main(String[] args) {
+        QuestSystem questSystem = new QuestSystem();
+
+        Player player1 = new Player("Thrall", 10, 250);
+        Player player2 = new Player("Sylvanas", 12, 450);
+
+        Quest quest1 = new Quest("Defeat the Lich King", 10, 150);
+        Quest quest2 = new Quest("Retrieve the Sword of Azeroth", 8, 100);
+
+        CompletableFuture<Player> player1Quest = questSystem.startQuest(player1, quest1);
+        CompletableFuture<Player> player2Quest = questSystem.startQuest(player2, quest2);
+
+        player1Quest.thenAccept(player -> System.out.println(player.getName() + " has completed the quest and now has " + player.getExperience() + " experience points."));
+        player2Quest.thenAccept(player -> System.out.println(player.getName() + " has completed the quest and now has " + player.getExperience() + " experience points."));
+    }
+}
---
+++ src/main/java/wow/QuestSystem.java
@@ -0,0 +1,19 @@
+package wow;
+
+import java.util.concurrent.CompletableFuture;
+
+public class QuestSystem {
+    /**
+     * Starts a quest for the player by updating the player's experience based on the quest reward.
+     *
+     * @param player the player starting the quest
+     * @param quest the quest to be started
+     * @return a CompletableFuture representing the player's updated information after completing the quest
+     */
+    public CompletableFuture<Player> startQuest(Player player, Quest quest) {
+        return CompletableFuture.supplyAsync(() -> {
+            player.setExperience(player.getExperience() + quest.getReward());
+            return player;
+        });
+    }
+}
