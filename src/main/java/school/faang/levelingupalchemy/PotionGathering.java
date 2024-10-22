b8c28c43ff6fe63a90f9c673ce965c4466158cad
“Andrey <aiegorovceo@yandex.ru>
2024-10-22 16:16:17 +0300

    feat: Add main class PotionGathering for async ingredient collection

---
+++ src/main/java/school/faang/levelingupalchemy/PotionGathering.java
@@ -0,0 +1,21 @@
+package school.faang.levelingupalchemy;
+
+import java.util.List;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.stream.Collectors;
+
+public class PotionGathering {
+    public static void main(String[] args) {
+        List<Potion> potions = List.of(
+                new Potion("Healing Potion", 5),
+                new Potion("Mana Potion", 3),
+                new Potion("Stamina Potion", 4)
+        );
+
+        // Асинхронный сбор ингредиентов
+        gatherAllIngredients(potions);
+    }
+
+
+}
