b45901eb128104fa4a867b069eb14dc382ec3f24
Andrew Balashov <balashov83@gmail.com>
2024-10-22 14:51:12 +0300

    BJS2-38998 Three Wizards: task done

---
+++ src/main/java/school/faang/m1s4/bjs2_38998_threeWizards/Task.java
@@ -0,0 +1,10 @@
+package school.faang.m1s4.bjs2_38998_threeWizards;
+
+import lombok.Data;
+
+@Data
+public class Task {
+    private final String name;
+    private final int difficulty;
+    private final int reward;
+}
