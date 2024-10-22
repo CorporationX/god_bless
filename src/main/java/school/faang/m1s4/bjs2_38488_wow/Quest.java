5c88962558a02d35d91447e3e11d73b70bd5b750
Andrew Balashov <balashov83@gmail.com>
2024-10-22 09:31:52 +0300

    BJS2-38488 WoW: task done

---
+++ src/main/java/school/faang/m1s4/bjs2_38488_wow/Quest.java
@@ -0,0 +1,10 @@
+package school.faang.m1s4.bjs2_38488_wow;
+
+import lombok.Data;
+
+@Data
+public class Quest {
+    private final String name;
+    private final int difficulty;
+    private final int reward;
+}
