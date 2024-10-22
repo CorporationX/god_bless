b075fae82dbd01172a630fe4b4602cdb531efb74
AlekseyB86nb <tigrxam@gmail.com>
2024-10-22 23:30:36 +0300

    completed task

---
+++ src/main/java/sprint_4/sprint_4_1_async_future/collectOrdersAmazon_38107/OrderProcessor.java
@@ -0,0 +1,48 @@
+package sprint_4.sprint_4_1_async_future.collectOrdersAmazon_38107;
+
+import lombok.extern.slf4j.Slf4j;
+
+import java.util.List;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.atomic.AtomicInteger;
+
+import static sprint_4.sprint_4_1_async_future.collectOrdersAmazon_38107.OrderStatus.*;
+
+@Slf4j
+public class OrderProcessor {
+    private static final int TIME_SLEEP_THREAD = 1000;
+
+    private final AtomicInteger totalProcessedOrders;
+
+    public OrderProcessor() {
+        totalProcessedOrders = new AtomicInteger(0);
+    }
+
+    public void processAllOrders(List<Order> orders) {
+        List<CompletableFuture<Void>> futures = orders.stream()
+                .map(this::processOrder)
+                .toList();
+
+        CompletableFuture.allOf(futures.toArray(new CompletableFuture[0]))
+                .thenRun(() -> log.info("Processed orders: {}", totalProcessedOrders.get()))
+                .join();
+    }
+
+    public CompletableFuture<Void> processOrder(Order order) {
+        return CompletableFuture.runAsync(() -> {
+            sleepThread();
+            if (order.getStatus() != CANCELED) {
+                order.setStatus(PROCESSED);
+                totalProcessedOrders.incrementAndGet();
+            }
+        });
+    }
+
+    private void sleepThread() {
+        try {
+            Thread.sleep(TIME_SLEEP_THREAD);
+        } catch (InterruptedException e) {
+            log.error("Error: {}", e.getMessage());
+        }
+    }
+}
