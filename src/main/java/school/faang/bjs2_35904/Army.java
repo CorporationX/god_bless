6009c59f01d233a3c776fd88b60dfc7843a872b3
zlomilk95 <kenobi_95@mail.ru>
2024-10-22 15:28:24 +0300

    BJS2-35904

---
+++ src/main/java/school/faang/bjs2_35904/Army.java
@@ -0,0 +1,37 @@
+package school.faang.bjs2_35904;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Army {
+    private final List<Unit> units = new ArrayList<>();
+
+    public void addUnit(Unit unit) {
+        units.add(unit);
+    }
+
+    public int calculateTotalPower() throws InterruptedException {
+        int totalPower = 0;
+        List<Thread> threads = new ArrayList<>();
+        List<PowerCalculator> tasks = new ArrayList<>();
+
+        for (Unit unit : units) {
+            PowerCalculator powerCalculator = new PowerCalculator(unit);
+            tasks.add(powerCalculator);
+
+            Thread thread = new Thread(powerCalculator);
+            threads.add(thread);
+            thread.start();
+
+            thread.join();
+
+        }
+
+        for (PowerCalculator task : tasks) {
+            totalPower += task.getPower();
+        }
+
+        return totalPower;
+    }
+
+}
---
+++ src/main/java/school/faang/bjs2_35904/Swordsman.java
@@ -0,0 +1,8 @@
+package school.faang.bjs2_35904;
+
+public class Swordsman extends Unit {
+
+    public Swordsman(int power) {
+        super(power);
+    }
+}
