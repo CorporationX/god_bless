4ec056aaaf4d4493adce2b5a3329799f3b507492
Home Alome <yevgengeodakyan@gmail.com>
2024-10-22 12:12:41 -0400

    fix

--- src/main/java/school/faang_sprint_3/bro_force/Main.java
+++ src/main/java/school/faang_sprint_3/bro_force/Main.java
@@ -3,7 +3,6 @@ package school.faang_sprint_3.bro_force;
 import java.util.Random;
 
 public class Main {
-
     public static void main(String[] args) {
         var random = new Random();
         var game = new Game();
