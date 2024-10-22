d820b28296301094dd39e87a68873b136d754007
Ilya Ishanov <ilia.detroit2016@yandex.ru>
2024-10-23 02:33:18 +0500

    Асинхронность и Future

---
+++ src/main/java/school/faang/asynchandfuture/Main.java
@@ -0,0 +1,8 @@
+package school.faang.asynchandfuture;
+
+public class Main {
+    public static void main(String[] args) {
+        MasterCardService cardService = new MasterCardService();
+        cardService.doAll();
+    }
+}
---
+++ src/main/java/school/faang/asynchandfuture/MasterCardService.java
@@ -0,0 +1,47 @@
+package school.faang.asynchandfuture;
+
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+
+public class MasterCardService {
+    static int collectPayment() {
+        try {
+            Thread.sleep(10_000);
+            return 10_000;
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+            throw new RuntimeException();
+        }
+    }
+
+    static int sendAnalytics() {
+        try {
+            Thread.sleep(1_000);
+            return 1_000;
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+            throw new RuntimeException();
+        }
+    }
+
+    public void doAll() {
+        ExecutorService executor = Executors.newSingleThreadExecutor();
+        Future<Integer> futureResult = executor.submit(() -> collectPayment());
+
+        CompletableFuture<Void> analyticsFuture = CompletableFuture.supplyAsync(() -> sendAnalytics())
+                .thenAccept(result -> System.out.println("Аналитика отправлена: " + result));
+
+        try {
+            analyticsFuture.join();
+
+            int result = futureResult.get();
+            System.out.println("Платеж выполнен: " + result);
+        } catch (InterruptedException | ExecutionException e) {
+            throw new RuntimeException(e);
+        }
+        executor.shutdown();
+    }
+}
