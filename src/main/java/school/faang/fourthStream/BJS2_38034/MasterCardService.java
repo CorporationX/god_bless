33674ffb75b60a1b1fbb4c9584a37ac45002e556
UrmatBolotbek <urmar9797@gmail.com>
2024-10-22 15:40:19 +0200

    complete the task "Асинхронность и Future" BJS2-38034

---
+++ src/main/java/school/faang/fourthStream/BJS2_38034/MasterCardService.java
@@ -0,0 +1,66 @@
+package school.faang.fourthStream.BJS2_38034;
+
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+import java.util.concurrent.TimeUnit;
+
+public class MasterCardService {
+    private static final int THREADS_COUNT = 2;
+    private final ExecutorService executor = Executors.newFixedThreadPool(THREADS_COUNT);
+
+    public void doAll() {
+        CompletableFuture.supplyAsync(MasterCardService::sendAnalytics, executor)
+                .thenAccept(analytics ->
+                        System.out.println("Аналитика отправлена: " + analytics))
+                .exceptionally(e -> {
+                    System.err.println("Не удалось отправить аналитику: " + e.getMessage());
+                    return null;
+                })
+                .join();
+
+        Future<Integer> payment = executor.submit(MasterCardService::collectPayment);
+
+        try {
+            int resultPayment = payment.get();
+            System.out.println("Платеж выполнен: " + resultPayment);
+        } catch (InterruptedException | ExecutionException e) {
+            throw new IllegalStateException("Не удалось провести платеж: +" +
+                    e.getMessage());
+        }
+
+        executor.shutdown();
+
+        try {
+            if (!executor.awaitTermination(15, TimeUnit.SECONDS)) {
+                executor.shutdownNow();
+                System.out.println("Время ожидания истекло!");
+            }
+        } catch (InterruptedException e) {
+            Thread.currentThread().interrupt();
+            System.err.println("Поток прерван: " + e.getMessage());
+        }
+    }
+
+    private static int collectPayment() {
+        try {
+            Thread.sleep(10_000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+            throw new RuntimeException();
+        }
+        return 10_000;
+    }
+
+    private static int sendAnalytics() {
+        try {
+            Thread.sleep(1_000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+            throw new RuntimeException();
+        }
+        return 1_000;
+    }
+}
