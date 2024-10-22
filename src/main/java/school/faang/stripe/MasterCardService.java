a4a2f16c593e2d445305a7705d31f4fbd5a7070e
heckfy86 <marlinco263@gmail.com>
2024-10-22 21:21:43 +0200

    BJS2-38056 completed

---
+++ src/main/java/school/faang/stripe/MasterCardService.java
@@ -0,0 +1,43 @@
+package school.faang.stripe;
+
+import java.util.concurrent.*;
+
+public class MasterCardService {
+
+
+    static int collectPayment() {
+        try {
+            Thread.sleep(10_000);
+            return 10_000;
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+            throw new RuntimeException();
+        }
+    }
+
+    static int sendAnalytics() {
+        try {
+            Thread.sleep(1_000);
+            return 1_000;
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+            throw new RuntimeException();
+        }
+    }
+
+    public void doAll() {
+
+        ExecutorService executor = Executors.newFixedThreadPool(2);
+        Future<Integer> result = executor.submit(MasterCardService::collectPayment);
+        CompletableFuture<Integer> comFuture = CompletableFuture.supplyAsync(MasterCardService::sendAnalytics);
+
+        try {
+            System.out.println("Analytics result: " + comFuture.get());
+            System.out.println("Payment result: " + result.get());
+        } catch (InterruptedException | ExecutionException e) {
+            e.printStackTrace();
+        } finally {
+            executor.shutdown();
+        }
+    }
+}
