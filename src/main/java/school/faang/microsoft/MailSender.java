87b2dcc06bd7e8219b9823914f08d77527761892
Damba <mdamba2020@yandex.ru>
2024-10-22 21:38:55 +0300

    mailSenderEdit

--- src/main/java/school/faang/microsoft/MailSender.java
+++ src/main/java/school/faang/microsoft/MailSender.java
@@ -4,14 +4,14 @@ import java.util.Arrays;
 import java.util.List;
 
 public class MailSender {
-    public static final String stream1 = "Genesis";
-    public static final String stream2 = "First";
-    public static final String stream3 = "Second";
-    public static final String stream4 = "Third";
-    public static final String stream5 = "Fourth";
+    public static final String stream0 = "Genesis";
+    public static final String stream1 = "First";
+    public static final String stream2 = "Second";
+    public static final String stream3 = "Third";
+    public static final String stream4 = "Fourth";
 
     public static void main(String[] args) {
-        List<String> stream = Arrays.asList(stream1, stream2, stream3, stream4, stream5);
+        List<String> stream = Arrays.asList(stream0, stream1, stream2, stream3, stream4);
         Thread[] threads = new Thread[5];
         for (int i = 0; i < 5; i++) {
             int startIndex = i * 200 + 1;
