d8b26558adb9b6d7dd4adb366004a00bf9421eb8
Dmitry Shakhaev <dmtry.shahaev@gmail.com>
2024-10-22 19:21:30 +0300

    Задача: Асинхронность и Future. Добавление констант.

--- src/main/java/school/faang/mastercard/MasterCardService.java
+++ src/main/java/school/faang/mastercard/MasterCardService.java
@@ -8,6 +8,8 @@ import java.util.concurrent.Future;
 
 public class MasterCardService {
     public static final int NUM_THREADS = 2;
+    public static final int PAYMENT_DURATION = 10000;
+    public static final int DURATION_OF_ANALYTICS_COLLECTION = 1000;
 
     public void doAll() throws ExecutionException, InterruptedException {
         ExecutorService executor = Executors.newFixedThreadPool(NUM_THREADS);
@@ -22,8 +24,8 @@ public class MasterCardService {
 
     static int collectPayment() {
         try {
-            Thread.sleep(10_000);
-            return 10_000;
+            Thread.sleep(PAYMENT_DURATION);
+            return 10000;
         } catch (InterruptedException e) {
             e.printStackTrace();
             throw new RuntimeException();
@@ -32,8 +34,8 @@ public class MasterCardService {
 
     static int sendAnalytics() {
         try {
-            Thread.sleep(1_000);
-            return 1_000;
+            Thread.sleep(DURATION_OF_ANALYTICS_COLLECTION);
+            return 1000;
         } catch (InterruptedException e) {
             e.printStackTrace();
             throw new RuntimeException();
