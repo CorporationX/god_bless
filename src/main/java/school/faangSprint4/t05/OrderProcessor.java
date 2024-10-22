82873fef466b8703f024cf4348e3d7c13c286d35
Igor <asachiyigor@gmail.com>
2024-10-22 17:29:09 +0300

    BJS2-38064
    initial commit

---
+++ src/main/java/school/faangSprint4/t05/OrderProcessor.java
@@ -0,0 +1,39 @@
+package school.faangSprint4.t05;
+
+import java.util.List;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.atomic.AtomicInteger;
+
+public class OrderProcessor {
+    private final AtomicInteger totalProcessedOrders;
+
+    public OrderProcessor() {
+        this.totalProcessedOrders = new AtomicInteger(0);
+    }
+
+    public CompletableFuture<Void> processOrder(Order order) {
+        return CompletableFuture.runAsync(() -> {
+            try {
+                Thread.sleep(2000);
+
+                order.setStatus(OrderStatus.PROCESSED);
+
+                totalProcessedOrders.incrementAndGet();
+
+                System.out.println("Заказ " + order.getId() + " обработан");
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        });
+    }
+
+    public void processAllOrders(List<Order> orders) {
+        List<CompletableFuture<Void>> futures = orders.stream()
+                .map(this::processOrder)
+                .toList();
+
+        CompletableFuture.allOf(futures.toArray(new CompletableFuture[0])).join();
+
+        System.out.println("Обработано заказов: " + totalProcessedOrders.get());
+    }
+}
