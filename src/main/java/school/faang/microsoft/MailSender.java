d7c59fac8816d5622959c4a84019b1c1743824d7
Damba <mdamba2020@yandex.ru>
2024-10-22 21:33:53 +0300

    mailSender

---
+++ src/main/java/school/faang/microsoft/MailSender.java
@@ -0,0 +1,34 @@
+package school.faang.microsoft;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class MailSender {
+    public static final String stream1 = "Genesis";
+    public static final String stream2 = "First";
+    public static final String stream3 = "Second";
+    public static final String stream4 = "Third";
+    public static final String stream5 = "Fourth";
+
+    public static void main(String[] args) {
+        List<String> stream = Arrays.asList(stream1, stream2, stream3, stream4, stream5);
+        Thread[] threads = new Thread[5];
+        for (int i = 0; i < 5; i++) {
+            int startIndex = i * 200 + 1;
+            int endIndex = (i + 1) * 200;
+            threads[i] = new Thread(new SenderRunnable(startIndex, endIndex, stream.get(i)));
+            threads[i].start();
+        }
+
+        for (Thread thread : threads) {
+            try {
+                thread.join();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+        System.out.println("shutdown /r");
+    }
+
+
+}
