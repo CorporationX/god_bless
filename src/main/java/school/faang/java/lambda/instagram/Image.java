26d419869529d002c56aff1f10e038e6bd8fff3f
Oleksii Babaikin <a.babaykin@gmail.com>
2024-10-22 18:30:56 +0200

    ver 1

--- src/main/java/school/faang/java/lambda/instagram/Image.java
+++
@@ -1,9 +0,0 @@
-package school.faang.java.lambda.instagram;
-
-import lombok.Data;
-
-@Data
-public class Image {
-    private final String name;
-    private final String description;
-}
--- src/main/java/school/faang/java/lambda/meta/Notification.java
+++
@@ -1,9 +0,0 @@
-package school.faang.java.lambda.meta;
-
-import lombok.Data;
-
-@Data
-public class Notification {
-    private final String type;
-    private final String message;
-}
