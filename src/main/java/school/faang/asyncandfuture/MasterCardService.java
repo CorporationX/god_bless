5b54b0e45438365a7327b23483bad725f63663a6
daurenberdibekov <berder08@gmail.com>
2024-10-22 16:21:11 +0200

    Залогировал исключения и вынес повторяющиеся значения в константы

--- src/main/java/school/faang/asyncandfuture/MasterCardService.java
+++ src/main/java/school/faang/asyncandfuture/MasterCardService.java
@@ -1,19 +1,25 @@
 package school.faang.asyncandfuture;
 
 import lombok.Data;
+import lombok.extern.slf4j.Slf4j;
 
 import java.util.concurrent.*;
 
 @Data
+@Slf4j
 public class MasterCardService {
+    private static final Integer PAYMENT_SLEEP_TIME = 10000;
+    private static final Integer ANALYTIC_SLEEP_TIME = 1000;
+    private static final Integer TIMEOUT_TIME = 1000;
+
     private final ExecutorService executorService = Executors.newFixedThreadPool(3);
 
     public void doAll() throws ExecutionException, InterruptedException, TimeoutException {
         Future<Integer> payment = executorService.submit(MasterCardService::collectPayment);
         CompletableFuture<Integer> analytic = CompletableFuture.supplyAsync(MasterCardService::sendAnalytics, executorService);
 
-        int resultPayment = payment.get(20000, TimeUnit.SECONDS);
-        int resultAnalytic = analytic.get(20000, TimeUnit.SECONDS);
+        int resultPayment = payment.get(TIMEOUT_TIME, TimeUnit.SECONDS);
+        int resultAnalytic = analytic.get(TIMEOUT_TIME, TimeUnit.SECONDS);
 
         executorService.shutdown();
 
@@ -23,18 +29,20 @@ public class MasterCardService {
 
     static int collectPayment() {
         try {
-            Thread.sleep(10000);
-            return 10000;
+            Thread.sleep(PAYMENT_SLEEP_TIME);
+            return PAYMENT_SLEEP_TIME;
         } catch (InterruptedException e) {
+            log.error(e.getMessage());
             throw new IllegalStateException("Ошибка в потоке" + e.getMessage());
         }
     }
 
     static int sendAnalytics() {
         try {
-            Thread.sleep(1000);
-            return 1000;
+            Thread.sleep(ANALYTIC_SLEEP_TIME);
+            return ANALYTIC_SLEEP_TIME;
         } catch (InterruptedException e) {
+            log.error(e.getMessage());
             throw new IllegalStateException("Ошибка в потоке" + e.getMessage());
         }
     }
