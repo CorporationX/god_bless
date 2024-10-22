d56e9edef34dd2e4f2a7d361a607c68ea9e546ed
Maxim Gorobets <gorobets.maks@bk.ru>
2024-10-22 19:24:13 +1000

    simplification of the program

--- src/main/java/school/faang/BJS2_37144_BroForce/Game.java
+++ src/main/java/school/faang/BJS2_37144_BroForce/Game.java
@@ -2,32 +2,29 @@ package school.faang.BJS2_37144_BroForce;
 
 import lombok.extern.slf4j.Slf4j;
 
-import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
-import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.ThreadLocalRandom;
 import java.util.concurrent.TimeUnit;
 
 @Slf4j
 public class Game {
+    private static final int GAME_STEPS = 100;
     private static final int DEFAULT_LIVES_AMOUNT = 10;
     private static final int THREAD_POOL_SIZE = 4;
-    private static final int UPDATE_RATE = 1;
+    private static final int AWAIT_TERMINATION = 10;
 
     private int score;
     private int lives;
-    private volatile boolean isGameOver;
     private final Object scoreLock;
     private final Object livesLock;
-    private ScheduledExecutorService service;
-    private CountDownLatch latch;
+    private ExecutorService service;
 
     public Game() {
         lives = DEFAULT_LIVES_AMOUNT;
         scoreLock = new Object();
         livesLock = new Object();
-        service = Executors.newScheduledThreadPool(THREAD_POOL_SIZE);
-        latch = new CountDownLatch(1);
+        service = Executors.newFixedThreadPool(THREAD_POOL_SIZE);
     }
 
     public static void main(String[] args) {
@@ -36,37 +33,38 @@ public class Game {
     }
 
     public void start() {
-        isGameOver = false;
-        for (int i = 0; i < THREAD_POOL_SIZE; i++) {
-            service.scheduleAtFixedRate(() -> {
-                boolean isGotScore = ThreadLocalRandom.current().nextBoolean();
-                boolean isLostLife = ThreadLocalRandom.current().nextBoolean();
-                update(isGotScore, isLostLife);
-            }, 0, UPDATE_RATE, TimeUnit.SECONDS);
-        }
-
-        try {
-            latch.await();
-        } catch (InterruptedException e) {
-            log.error("Main thread was interrupted while waiting", e);
+        lives = DEFAULT_LIVES_AMOUNT;
+        for (int i = 0; i < GAME_STEPS; i++) {
+            if (lives <= 0) {
+                break;
+            }
+            boolean isGotScore = ThreadLocalRandom.current().nextBoolean();
+            boolean isLostLife = ThreadLocalRandom.current().nextBoolean();
+            service.submit(() -> update(isGotScore, isLostLife));
         }
         service.shutdown();
+        try {
+            if (service.awaitTermination(AWAIT_TERMINATION, TimeUnit.SECONDS)) {
+                log.info("The game ended correctly!");
+            } else {
+                log.warn("The game didn't end correctly on time! Force shutting down executor!");
+            }
+        } catch (InterruptedException e) {
+            log.error("Main thread was interrupted while waiting for the game to finish!", e);
+        }
     }
 
     private void update(boolean isGotScore, boolean isLostLife) {
         synchronized (scoreLock) {
-            synchronized (livesLock) {
-                if (isGameOver) {
-                    return;
-                }
-                if (isGotScore) {
-                    log.info("The score has been increased by 1!");
-                    score++;
-                }
-                if (isLostLife) {
-                    log.info("The lives amount was decreased by 1!");
-                    lives--;
-                }
+            if (isGotScore) {
+                log.info("The score has been increased by 1!");
+                score++;
+            }
+        }
+        synchronized (livesLock) {
+            if (isLostLife && lives > 0) {
+                log.info("The lives amount was decreased by 1!");
+                lives--;
                 if (lives <= 0) {
                     gameOver();
                 }
@@ -75,12 +73,6 @@ public class Game {
     }
 
     private void gameOver() {
-        synchronized (livesLock) {
-            log.info("GAME OVER! Total score - {}!", score);
-            isGameOver = true;
-            score = 0;
-            lives = DEFAULT_LIVES_AMOUNT;
-            latch.countDown();
-        }
+        log.info("GAME OVER! Total score - {}!", score);
     }
 }
