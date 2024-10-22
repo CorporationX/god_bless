49a47927cdee8dc2b6fcb24e160e03daa3ebdf6d
Sanasar1 <almasyansb@gmail.com>
2024-10-22 19:22:11 +0300

    feat: added gatherAllIngredients

---
+++ src/main/java/bjs2_37881/Potion.java
@@ -0,0 +1,11 @@
+package bjs2_37881;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@AllArgsConstructor
+@Getter
+public class Potion {
+    private String name;
+    private int requiredIngredients;
+}
