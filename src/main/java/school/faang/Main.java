fb520003d516a2975b822c5e6da10a6255cb9199
lomuserg <sergeymu2015@yandex.ru>
2024-10-22 23:15:50 +0300

    Packages for sprints)

--- src/main/java/school/faang/Main.java
+++ src/main/java/school/faang/Main.java
@@ -1,20 +1,8 @@
 package school.faang;
 
-import school.faang.Fighter.Archer;
-import school.faang.Fighter.Mage;
-import school.faang.Fighter.Swordsman;
-
 public class Main {
     public static void main(String[] args){
-        Army army = new Army();
-        for (int i = 0; i < 2; i++) {
-            army.addUnit(new Mage());
-            army.addUnit(new Archer());
-        }
-        army.addUnit(new Swordsman());
-
-        int totalPower = army.calculateTotalPower();
-        System.out.println("Army strength " + totalPower);
+        System.out.println("");
     }
 
 }
