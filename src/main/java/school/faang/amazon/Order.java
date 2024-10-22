fb0ba092deea9ea97ee0675c8bf38e5ff42d73b2
Aleksey <aleksei.zuk86@gmail.com>
2024-10-22 22:05:07 +0300

    Собираем заказы в Amazon

---
+++ src/main/java/school/faang/amazon/Order.java
@@ -0,0 +1,13 @@
+package school.faang.amazon;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+public class Order {
+    private int id;
+    private OrderStatus status;
+}
