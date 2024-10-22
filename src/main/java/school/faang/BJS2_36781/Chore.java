1937c18c0ee8800a8315ae9adb24b0acc22849ba
Stanislaav Kozh <printks@yandex.ru>
2024-10-22 16:59:55 +0300

    They were very nice but extremely poor

---
+++ src/main/java/school/faang/BJS2_36781/Chore.java
@@ -0,0 +1,20 @@
+package school.faang.BJS2_36781;
+
+class Chore implements Runnable {
+    private final String chore;
+
+    public Chore(String chore) {
+        this.chore = chore;
+    }
+
+    @Override
+    public void run() {
+        try {
+            System.out.println(Thread.currentThread().getName() + " выполняет задачу: " + chore);
+            Thread.sleep(1000);
+            System.out.println(Thread.currentThread().getName() + " завершил задачу: " + chore);
+        } catch (InterruptedException e) {
+            System.out.println("Задача " + chore + " была прервана");
+        }
+    }
+}
---
+++ src/main/java/school/faang/BJS2_36781/WeasleyFamily.java
@@ -0,0 +1,19 @@
+package school.faang.BJS2_36781;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+public class WeasleyFamily {
+    public static void main(String[] args) {
+
+        String[] chores = {"помыть посуду", "подмести пол", "приготовить ужин"};
+
+        ExecutorService executorService = Executors.newCachedThreadPool();
+
+        for (String chore : chores) {
+            executorService.submit(new Chore(chore));
+        }
+
+        executorService.shutdown();
+    }
+}
