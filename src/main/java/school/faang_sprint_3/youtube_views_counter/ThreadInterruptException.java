11b7d06972146c379eecb84704ab8473bf10fbbd
Home Alome <yevgengeodakyan@gmail.com>
2024-10-22 10:52:42 -0400

    YouTube views counter

---
+++ src/main/java/school/faang_sprint_3/youtube_views_counter/ThreadInterruptException.java
@@ -0,0 +1,7 @@
+package school.faang_sprint_3.youtube_views_counter;
+
+public class ThreadInterruptException extends RuntimeException {
+    public ThreadInterruptException(String message) {
+        super(message);
+    }
+}
