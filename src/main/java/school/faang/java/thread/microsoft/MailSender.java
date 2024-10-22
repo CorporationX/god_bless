7f211bc4666ef0242dc724f3ba4119c2cb33f67c
Oleksii Babaikin <a.babaykin@gmail.com>
2024-10-22 12:07:54 +0200

    Созданы классы:  MailSender  SenderRunnable

---
+++ src/main/java/school/faang/java/thread/microsoft/MailSender.java
@@ -0,0 +1,33 @@
+package school.faang.java.thread.microsoft;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MailSender {
+    public static List<String> listEmails;
+
+    static {
+        listEmails = new ArrayList<>();
+        for (int counter = 0; counter < 1000; counter++) {
+            listEmails.add("Email #" + counter);
+        }
+    }
+
+    public static void main(String[] args) {
+        List<Thread> threadList = new ArrayList<>();
+        for (int counter = 0; counter < 5; counter++) {
+            threadList.add(new Thread(new SenderRunnable(200 * counter, 200 * (counter + 1) - 1)));
+            threadList.get(counter).start();
+        }
+
+        threadList.forEach(thread -> {
+            try {
+                thread.join();
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+        });
+
+        System.out.println("\nAll 1000 Emails send");
+    }
+}
