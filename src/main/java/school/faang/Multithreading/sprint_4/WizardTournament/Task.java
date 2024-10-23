7c7b398f1a48bc305e58d005dfcfed34b46934d5
Naku <polevaya.nastena@internet.ru>
2024-10-23 23:04:40 +0300

    Naku-BJS2-39062 Турнир трех волшебников final v_1

---
+++ src/main/java/school/faang/Multithreading/sprint_4/WizardTournament/Task.java
@@ -0,0 +1,12 @@
+package school.faang.Multithreading.sprint_4.WizardTournament;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class Task {
+    private String name;
+    private int difficulty;
+    private int reward;
+}
