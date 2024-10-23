0206293a5010345fab00da75c42318046f2d9ef7
Maxim Gorobets <gorobets.maks@bk.ru>
2024-10-24 01:41:35 +1000

    new PigThread subclasses for different pigs

---
+++ src/main/java/school/faang/BJS2_36396_NormalGamePigs/Pig1Thread.java
@@ -0,0 +1,7 @@
+package school.faang.BJS2_36396_NormalGamePigs;
+
+public class Pig1Thread extends PigThread {
+    public Pig1Thread() {
+        super("Nif-Nif", "Straw");
+    }
+}
---
+++ src/main/java/school/faang/BJS2_36396_NormalGamePigs/Pig3Thread.java
@@ -0,0 +1,7 @@
+package school.faang.BJS2_36396_NormalGamePigs;
+
+public class Pig3Thread extends PigThread {
+    public Pig3Thread() {
+        super("Nuf-Nuf", "Bricks");
+    }
+}
