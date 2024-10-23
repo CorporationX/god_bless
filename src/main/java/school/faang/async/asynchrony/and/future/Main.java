9f7ffc9819ce78579e97c16dd37f923416bdea3f
Nikolay Orlenko <norlenko2801@gmail.com>
2024-10-23 20:45:19 +0300

    Асинхронность и Future

---
+++ src/main/java/school/faang/async/asynchrony/and/future/Main.java
@@ -0,0 +1,8 @@
+package school.faang.async.asynchrony.and.future;
+
+public class Main {
+    public static void main(String[] args) {
+        MasterCardService masterCardService = new MasterCardService();
+        masterCardService.doAll();
+    }
+}
