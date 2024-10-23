101491b6be56ee38873a734679ad03268e5a169c
Egor Semenovskii <gaoqiao5217@gmail.com>
2024-10-23 17:23:15 +0300

    MasterCardService created

---
+++ src/main/java/school/faang/AsyncFuture/MasterCardService.java
@@ -0,0 +1,34 @@
+package school.faang.AsyncFuture;
+import java.util.concurrent.CompletableFuture;
+
+public class MasterCardService {
+    public static void main(String[] args) throws InterruptedException {
+        MasterCardService service = new MasterCardService();
+        service.doAll();
+    }
+
+    static int collectPayment() {
+        try {
+            Thread.sleep(10_000);
+            return 10_000;
+        } catch (InterruptedException e) {
+            throw new IllegalStateException("Something went wrong while collecting", e);
+        }
+    }
+
+    static int sendAnalytics() {
+        try {
+            Thread.sleep(1_000);
+            return 1_000;
+        } catch (InterruptedException e) {
+            throw new IllegalStateException("Something went wrong while sending", e);
+        }
+    }
+
+    public void doAll(){
+        CompletableFuture.supplyAsync(MasterCardService::sendAnalytics)
+                .thenAccept(e -> System.out.println("Analytics sent: " + e)).join();
+        CompletableFuture.supplyAsync(MasterCardService::collectPayment)
+                .thenAccept(e -> System.out.println("Payment collected: " + e)).join();
+    }
+}
