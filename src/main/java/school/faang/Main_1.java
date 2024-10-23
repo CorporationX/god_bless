6c80ea0f03ede25a200e090fb11ff0d1a7be8b3a
lomuserg <sergeymu2015@yandex.ru>
2024-10-23 11:53:28 +0300

    Packages for sprint 35902

--- src/main/java/school/faang/Main_1.java
+++ src/main/java/school/faang/Main_1.java
@@ -4,7 +4,7 @@ import school.faang.Fighter.Archer;
 import school.faang.Fighter.Mage;
 import school.faang.Fighter.Swordsman;
 
-public class Main {
+public class Main_1 {
     public static void main(String[] args){
         Army army = new Army();
         for (int i = 0; i < 2; i++) {
