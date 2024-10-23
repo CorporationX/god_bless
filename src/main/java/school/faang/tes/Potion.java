d5cc32c76ebc0550b7f0d5a77ee08abf89517a2f
Alexander Litovkin <litovkinalexan@gmail.com>
2024-10-23 22:28:00 +0700

    implement potion class

---
+++ src/main/java/school/faang/tes/Potion.java
@@ -0,0 +1,14 @@
+package school.faang.tes;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+public class Potion {
+
+    private String name;
+    private int requiredIngredients;
+}
