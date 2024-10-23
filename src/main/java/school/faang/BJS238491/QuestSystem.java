943b8b6aca49ca096c1c1f485aee0666204d8a39
Doniyor Kurbanov <doniyor.kurbanov.24@gmail.com>
2024-10-23 22:15:51 +0500

    BJS2-38491. Жду от вас фитбека спасибо

--- src/main/java/school/faang/BJS238491/QuestSystem.java
+++ src/main/java/school/faang/BJS238491/QuestSystem.java
@@ -1,20 +1,34 @@
 package school.faang.BJS238491;
 
+import lombok.extern.slf4j.Slf4j;
+
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
+@Slf4j
 public class QuestSystem {
+    private static final int THREADS = 2;
+    private static final ExecutorService EXECUTOR_SERVICE = Executors.newFixedThreadPool(THREADS);
     public CompletableFuture<Player> startQuest(Player player, Quest quest) {
-        ExecutorService executorService = Executors.newFixedThreadPool(2);
-        CompletableFuture<Player> questCompletableFuture = CompletableFuture.supplyAsync(() -> {
-            int reward = quest.getReward();
-            player.setExperience(reward);
-            return player;
-        });
-        questCompletableFuture.thenRun(executorService::shutdown);
+        CompletableFuture<Player> questCompletableFuture = CompletableFuture.supplyAsync(() -> player, EXECUTOR_SERVICE);
+        {
+            questCompletableFuture.thenApply(q -> {
+                try {
+                    Thread.sleep(5000);
+                } catch (InterruptedException e) {
+                    log.error("Something went wrong " + e);
+                }
+                int reward = quest.getReward();
+                player.setExperience(reward);
+                return player;
+            });
+            return questCompletableFuture;
+        }
+    }
 
-        return questCompletableFuture;
+    public void shutDown() {
+        EXECUTOR_SERVICE.shutdown();
     }
 
     public static void main(String[] args) {
@@ -28,6 +42,9 @@ public class QuestSystem {
 
         CompletableFuture<Player> player1Quest = questSystem.startQuest(player1, quest1);
         CompletableFuture<Player> player2Quest = questSystem.startQuest(player2, quest2);
+        player1Quest.join();
+        player2Quest.join();
+        questSystem.shutDown();
 
         player1Quest.thenAccept(player -> System.out.println(player.getName() + " has completed the quest and now has " + player.getExperience() + " experience points."));
         player2Quest.thenAccept(player -> System.out.println(player.getName() + " has completed the quest and now has " + player.getExperience() + " experience points."));
