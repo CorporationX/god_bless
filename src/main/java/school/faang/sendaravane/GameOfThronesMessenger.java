ee56b68e4289d7ca9d5bef18d242dc4401600904
“Andrey <aiegorovceo@yandex.ru>
2024-10-23 13:21:49 +0300

    feat: Create GameOfThronesMessenger main class for sending and processing messages

---
+++ src/main/java/school/faang/sendaravane/GameOfThronesMessenger.java
@@ -0,0 +1,11 @@
+package school.faang.sendaravane;
+
+public class GameOfThronesMessenger {
+    public static void main(String[] args) {
+        Kingdom winterfell = new Kingdom("Winterfell");
+        Kingdom kingsLanding = new Kingdom("King's Landing");
+        Kingdom dorne = new Kingdom("Dorne");
+
+        RavenService ravenService = new RavenService();
+    }
+}
