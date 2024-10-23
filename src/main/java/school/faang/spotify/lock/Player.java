6a505c8464ae1e6c838711fd59cc1bd4b26af849
Andrey <maandr@internet.ru>
2024-10-23 18:03:32 +0300

    BJS2-37405 Spotify lock

---
+++ src/main/java/school/faang/spotify/lock/Player.java
@@ -0,0 +1,36 @@
+package school.faang.spotify.lock;
+
+public class Player {
+    private String lock = "lock object";
+    boolean isPlaying = false;
+
+    public void play() {
+        synchronized(lock) {
+            isPlaying = true;
+            System.out.println(Thread.currentThread().getName() + " uses \"play\"");
+        }
+    }
+
+    public void pause() {
+        synchronized(lock) {
+            isPlaying = false;
+            System.out.println(Thread.currentThread().getName() + " uses \"pause\"");
+        }
+    }
+
+    public void skip() {
+        synchronized(lock) {
+            isPlaying = false;
+            System.out.println(Thread.currentThread().getName() + " uses \"skip\"");
+            play();
+        }
+    }
+
+    public void previous() {
+        synchronized(lock) {
+            isPlaying = false;
+            System.out.println(Thread.currentThread().getName() + " uses \"previous\"");
+            play();
+        }
+    }
+}
