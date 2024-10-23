f787fb66ce193fcba72118157648914f8baf74d2
Shap|Misha <maxim.ivanov.jr@gmail.com>
2024-10-23 13:08:10 +0300

    automated the creation of objects in main

--- src/main/java/school/faang/bjs2_37834/MailSender.java
+++ src/main/java/school/faang/bjs2_37834/MailSender.java
@@ -1,19 +1,21 @@
 package school.faang.bjs2_37834;
 
-import java.util.Arrays;
+import java.util.ArrayList;
 import java.util.List;
 
 public class MailSender {
     public static void main(String[] args) {
-        List<Thread> list = Arrays.asList(
-                new Thread(new SenderRunnable(1, 200)),
-                new Thread(new SenderRunnable(201, 400)),
-                new Thread(new SenderRunnable(401, 600)),
-                new Thread(new SenderRunnable(601, 800)),
-                new Thread(new SenderRunnable(801, 1000))
-        );
-        list.forEach(Thread::start);
-        list.forEach(thread -> {
+        List<Thread> threads = new ArrayList<>();
+        int startIndex = 1;
+        int endIndex = 200;
+        for (int i = 0; i < 5; i++) {
+             threads.add(new Thread(new SenderRunnable(startIndex, endIndex)));
+            startIndex += 200;
+            endIndex += 200;
+
+        }
+        threads.forEach(Thread::start);
+        threads.forEach(thread -> {
             try {
                 thread.join();
             } catch (InterruptedException e) {
