36e83482177e35b0f86e8b30a4f67d328a82ecc6
Maxim Gorobets <gorobets.maks@bk.ru>
2024-10-24 02:21:42 +1000

    new Location class

---
+++ src/main/java/school/faang/BJS2_36332_FingerPath/Location.java
@@ -0,0 +1,11 @@
+package school.faang.BJS2_36332_FingerPath;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class Location {
+    private double x;
+    private double y;
+}
