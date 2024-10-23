41a666474baad2fe137016ef0c002b82f98f63ff
Irina Krasnova <arisha.17@mail.ru>
2024-10-23 17:49:00 +0300

    исправлено

--- src/test/java/theEldeкScrolls/Potion.java
+++ src/test/java/theEldeкScrolls/Potion.java
@@ -5,14 +5,14 @@ import java.util.Random;
 public class Potion {
     private String name;
     private int requiredIngredients;
-    Random random = new Random();
+    private Random random = new Random();
 
     public Potion(String name, int requiredIngredients) {
         this.name = name;
         this.requiredIngredients = requiredIngredients;
     }
 
-    public synchronized int getRequiredIngredients() {
+    public int getRequiredIngredients() {
         return requiredIngredients;
     }
 }
