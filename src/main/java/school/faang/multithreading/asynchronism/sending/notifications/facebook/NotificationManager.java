7deafe1cdc2ab3264d35f46850c6c5f9c5876448
AVB <abdddev@gmail.com>
2024-10-23 22:31:56 +1000

    Рассылаем уведомления в Facebook

---
+++ src/main/java/school/faang/multithreading/asynchronism/sending/notifications/facebook/NotificationManager.java
@@ -0,0 +1,34 @@
+package school.faang.multithreading.asynchronism.sending.notifications.facebook;
+
+import lombok.extern.slf4j.Slf4j;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+@Slf4j
+public class NotificationManager {
+    private static final int THREADS_COUNT = 5;
+    private final ExecutorService executor = Executors.newFixedThreadPool(THREADS_COUNT);
+
+    private List<Notification> notifications = new ArrayList<>();
+
+    public CompletableFuture<Void> fetchNotification(Integer id, String message) {
+        return CompletableFuture.runAsync(
+                () -> addNotification(new Notification(id, message)), executor);
+    }
+
+    public void shutdown() {
+        executor.shutdown();
+    }
+
+    public void printNotifications() {
+        notifications.forEach(notification -> log.info("Уведомление: №{}, сообщение: {}", notification.getId(), notification.getMessage()));
+    }
+
+    private synchronized void addNotification(Notification notification) {
+        notifications.add(notification);
+    }
+}
