4fcc68a47096efb6ea48a2152dc50a4e039f5c92
“Andrey <aiegorovceo@yandex.ru>
2024-10-23 13:25:09 +0300

    fix: Handle exceptions using handle method of CompletableFuture refactor: Extract sending logic into RavenService for better modularity chore: Add console output for successful and failed message deliveries

--- src/main/java/school/faang/sendaravane/GameOfThronesMessenger.java
+++ src/main/java/school/faang/sendaravane/GameOfThronesMessenger.java
@@ -7,5 +7,23 @@ public class GameOfThronesMessenger {
         Kingdom dorne = new Kingdom("Dorne");
 
         RavenService ravenService = new RavenService();
+
+        CompletableFuture<String> future1 = ravenService.sendRaven(winterfell, kingsLanding)
+                .handle((result, ex) -> {
+                    if (ex != null) {
+                        return "Failed to send message: " + ex.getMessage();
+                    } else {
+                        return result;
+                    }
+                });
+        
+        CompletableFuture<String> future2 = ravenService.sendRaven(winterfell, dorne)
+                .handle((result, ex) -> {
+                    if (ex != null) {
+                        return "Failed to send message: " + ex.getMessage();
+                    } else {
+                        return result;
+                    }
+                });
     }
 }
