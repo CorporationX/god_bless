e5de5f4b77d1ebbded5741ff8e62a81da86d4305
AVB <abdddev@gmail.com>
2024-10-23 20:44:05 +1000

    Superheroes battle

---
+++ src/main/java/school/faang/multithreading/asynchronism/superheroes/bettle/Superhero.java
@@ -0,0 +1,12 @@
+package school.faang.multithreading.asynchronism.superheroes.bettle;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class Superhero {
+    private String name;
+    private int strength;
+    private int agility;
+}
