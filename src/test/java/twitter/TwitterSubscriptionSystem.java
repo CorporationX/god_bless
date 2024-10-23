c89d540bb87c659983dcec0b1db8a6224196d2e7
Irina Krasnova <arisha.17@mail.ru>
2024-10-23 14:58:08 +0300

    Становимся celebrity в Twitter

---
+++ src/test/java/twitter/TwitterSubscriptionSystem.java
@@ -0,0 +1,16 @@
+package twitter;
+
+import java.util.concurrent.CompletableFuture;
+
+public class TwitterSubscriptionSystem {
+
+    private synchronized void addFollower(TwitterAccount account) {
+        account.setFollowers(account.getFollowers() + 1);
+    }
+
+    public CompletableFuture<Void> followAccount(TwitterAccount account) {
+        return CompletableFuture.runAsync(() -> {
+            addFollower(account);
+        });
+    }
+}
