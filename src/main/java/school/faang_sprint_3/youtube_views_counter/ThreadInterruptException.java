d101dccccd9f16296da6a27013da0dde663c8124
Home Alome <yevgengeodakyan@gmail.com>
2024-10-23 11:27:44 -0400

    exception removed

--- src/main/java/school/faang_sprint_3/youtube_views_counter/ThreadInterruptException.java
+++
@@ -1,7 +0,0 @@
-package school.faang_sprint_3.youtube_views_counter;
-
-public class ThreadInterruptException extends RuntimeException {
-    public ThreadInterruptException(String message) {
-        super(message);
-    }
-}
