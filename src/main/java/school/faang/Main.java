be5f86480d3f5cdfa56911dba15ac17de9f4b5bf
lomuserg <sergeymu2015@yandex.ru>
2024-10-23 19:33:28 +0300

    Packages for sprint 36796

--- src/main/java/school/faang/Main.java
+++ src/main/java/school/faang/Main.java
@@ -1,20 +1,8 @@
 package school.faang;
 
-<<<<<<< HEAD
-import java.util.ArrayList;
-import java.util.List;
-
-public class Main {
-
-   private static List<String> chores = new ArrayList<>();
-
-    public static void main(String[] args) {
-
-=======
 public class Main {
     public static void main(String[] args){
         System.out.println("Hello_Word");
->>>>>>> Smurno
     }
 
 }
