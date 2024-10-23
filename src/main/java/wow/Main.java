011f46021d1e6f9bbfbb1bdb8beb21017fae8496
Юрий <angelbes777@gmail.com>
2024-10-23 15:03:41 +0300

    Fix remarks

--- src/main/java/wow/Main.java
+++ src/main/java/wow/Main.java
@@ -1,8 +1,13 @@
 package wow;
 
 import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
 
 public class Main {
+    private static final int MAX_QUESTS = 2;
+    private static final int AWAIT_SECONDS = 30;
     /**
      * The main entry point of the application. Initializes a quest system and two players,
      * assigns quests to the players, and starts the quests asynchronously.
@@ -10,7 +15,8 @@ public class Main {
      *
      * @param args Command line arguments (not used).
      */
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
+        ExecutorService executorService = Executors.newFixedThreadPool(MAX_QUESTS);
         QuestSystem questSystem = new QuestSystem();
 
         Player player1 = new Player("Thrall", 10, 250);
@@ -19,10 +25,16 @@ public class Main {
         Quest quest1 = new Quest("Defeat the Lich King", 10, 150);
         Quest quest2 = new Quest("Retrieve the Sword of Azeroth", 8, 100);
 
-        CompletableFuture<Player> player1Quest = questSystem.startQuest(player1, quest1);
-        CompletableFuture<Player> player2Quest = questSystem.startQuest(player2, quest2);
+        CompletableFuture<Player> player1Quest = questSystem.startQuest(player1, quest1, executorService);
+        CompletableFuture<Player> player2Quest = questSystem.startQuest(player2, quest2, executorService);
 
         player1Quest.thenAccept(player -> System.out.println(player.getName() + " has completed the quest and now has " + player.getExperience() + " experience points."));
         player2Quest.thenAccept(player -> System.out.println(player.getName() + " has completed the quest and now has " + player.getExperience() + " experience points."));
+
+        executorService.shutdown();
+
+        if (!executorService.awaitTermination(AWAIT_SECONDS, TimeUnit.SECONDS)) {
+            executorService.shutdownNow();
+        }
     }
 }
--- src/main/java/wow/QuestSystem.java
+++ src/main/java/wow/QuestSystem.java
@@ -1,6 +1,7 @@
 package wow;
 
 import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.Executor;
 
 public class QuestSystem {
     /**
@@ -10,10 +11,10 @@ public class QuestSystem {
      * @param quest the quest to be started
      * @return a CompletableFuture representing the player's updated information after completing the quest
      */
-    public CompletableFuture<Player> startQuest(Player player, Quest quest) {
+    public CompletableFuture<Player> startQuest(Player player, Quest quest, Executor executor) {
         return CompletableFuture.supplyAsync(() -> {
             player.setExperience(player.getExperience() + quest.getReward());
             return player;
-        });
+        }, executor);
     }
 }
