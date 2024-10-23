17e04e25a0c7891b2fde58642dd76fe855bec161
abzalkhan00 <abzalkhanondakanov@gmail.com>
2024-10-23 20:31:25 +0500

    solved "Качаем алхимию" task

---
+++ src/main/java/school/faang/asynchrony/alchemy/Potion.java
@@ -0,0 +1,12 @@
+package school.faang.asynchrony.alchemy;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class Potion {
+    private String name;
+    private Integer requiredIngredients;
+
+}
