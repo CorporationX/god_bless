07ff01abd5f1f74ac2f30cefd46d28ccaa1380f6
0Miha0 <140548440+0Miha0@users.noreply.github.com>
2024-10-23 18:36:25 +0500

    https://faang-school.atlassian.net/browse/BJS2-36782

--- src/main/java/school/faang/Mnogopotochka/Synhronaized/BJS2_36782/Main.java
+++ src/main/java/school/faang/Mnogopotochka/Synhronaized/BJS2_36782/Main.java
@@ -1,31 +1,26 @@
 package school.faang.Mnogopotochka.Synhronaized.BJS2_36782;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
         Boss dragon = new Boss(3);
 
-        Player dima = new Player("Dima", dragon);
-        Player alex = new Player("Alex", dragon);
-        Player maria = new Player("Maria", dragon);
-        Player john = new Player("John", dragon);
-        Player sophia = new Player("Sophia", dragon);
-        Player michael = new Player("Michael", dragon);
-        Player emma = new Player("Emma", dragon);
-        Player daniel = new Player("Daniel", dragon);
-        Player olivia = new Player("Olivia", dragon);
-        Player james = new Player("James", dragon);
-        Player isabella = new Player("Isabella", dragon);
+        List<Player> players = new ArrayList<>(List.of(
+                new Player("Dima", dragon),
+                new Player("Alex", dragon),
+                new Player("Maria", dragon),
+                new Player("John", dragon),
+                new Player("Sophia", dragon),
+                new Player("Michael", dragon),
+                new Player("Emma", dragon),
+                new Player("Daniel", dragon),
+                new Player("Olivia", dragon),
+                new Player("James", dragon),
+                new Player("Isabella", dragon)
+        ));
 
-        dima.start();
-        alex.start();
-        maria.start();
-        john.start();
-        sophia.start();
-        michael.start();
-        emma.start();
-        daniel.start();
-        olivia.start();
-        james.start();
-        isabella.start();
+        players.forEach(Thread::start);
     }
 }
