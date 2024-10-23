05292ab994038711f36842d1c12383dfd58c74dd
Dmitrii Pazinich <pazinich.d@yandex.ru>
2024-10-23 12:12:50 +0300

    Task "Качаем алхимию" solution.

---
+++ src/main/java/school/faang/sprint_4/bjs2_38883_alchemyLeveling/Potion.java
@@ -0,0 +1,11 @@
+package school.faang.sprint_4.bjs2_38883_alchemyLeveling;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class Potion {
+    private String name;
+    private int ingredientsCount;
+}
