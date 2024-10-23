b740c7b81d1b1072a20fd2c235e86586cda1eec4
AnnaAplavina <59871896+AnnaAplavina@users.noreply.github.com>
2024-10-23 19:49:17 +0300

    amazon orders

---
+++ src/main/java/school/faang/godbless/bjs2_38286/Order.java
@@ -0,0 +1,13 @@
+package school.faang.godbless.bjs2_38286;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@AllArgsConstructor
+@Getter
+@Setter
+public class Order {
+    private int id;
+    private String status;
+}
