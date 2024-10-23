f2e62964e43fdac2307f0c7bdb283103dd79230d
Aleksey <aleksei.zuk86@gmail.com>
2024-10-23 17:41:35 +0300

    Star Wars Arena

---
+++ src/main/java/school/faang/starWarsArena/StarWarsArena.java
@@ -0,0 +1,27 @@
+package school.faang.starWarsArena;
+
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.Future;
+
+public class StarWarsArena {
+    public static void main(String[] args) {
+        Battle battle = new Battle();
+
+        Robot r2d2 = new Robot("R2-D2", 5, 7);
+        Robot c3po = new Robot("C-3PO", 4, 8);
+        Robot bb8 = new Robot("BB-8", 6, 6);
+
+        Future<Robot> winnerFuture = battle.fight(r2d2, bb8);
+
+        try {
+            Robot winner = winnerFuture.get();
+            System.out.println("Победитель битвы: " + winner.getName());
+        } catch (InterruptedException | ExecutionException e) {
+            e.printStackTrace();
+        }
+
+        battle.serviceShutdown();
+    }
+}
+
+
