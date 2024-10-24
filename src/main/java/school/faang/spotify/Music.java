50f8883fec707e43100def175ce678a9a2bade7b
Damba <mdamba2020@yandex.ru>
2024-10-24 21:51:48 +0300

    spotify

---
+++ src/main/java/school/faang/spotify/Music.java
@@ -0,0 +1,20 @@
+package school.faang.spotify;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class Music {
+    public static void main(String[] args) {
+        Player player = new Player();
+        Thread playThread = new Thread(player::play);
+        Thread pauseThread = new Thread(player::pause);
+        Thread skipThread = new Thread(player::skip);
+        Thread previousThread = new Thread(player::previous);
+
+        List<Thread> threads = Arrays.asList(playThread, pauseThread, skipThread, previousThread);
+
+        for (Thread thread : threads) {
+            thread.start();
+        }
+    }
+}
