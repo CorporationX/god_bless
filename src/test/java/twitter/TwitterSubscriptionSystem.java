42f24bc3e45b4ef1fb7c0155ae4c1d5b0898d166
Irina Krasnova <arisha.17@mail.ru>
2024-10-24 21:54:39 +0300

    добавлен свой пул потоков

--- src/test/java/twitter/TwitterSubscriptionSystem.java
+++ src/test/java/twitter/TwitterSubscriptionSystem.java
@@ -1,8 +1,12 @@
 package twitter;
 
 import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 public class TwitterSubscriptionSystem {
+    private final int THREAD_COUNT = 3;
+    private ExecutorService executorService = Executors.newFixedThreadPool(THREAD_COUNT);
 
     private synchronized void addFollower(TwitterAccount account) {
         account.setFollowers(account.getFollowers() + 1);
@@ -11,6 +15,6 @@ public class TwitterSubscriptionSystem {
     public CompletableFuture<Void> followAccount(TwitterAccount account) {
         return CompletableFuture.runAsync(() -> {
             addFollower(account);
-        });
+        },executorService);
     }
 }
