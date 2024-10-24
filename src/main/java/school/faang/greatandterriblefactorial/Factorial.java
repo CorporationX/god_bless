1c3a1602df422e5818d83e0badbfdf5e8e5feb39
“Andrey <aiegorovceo@yandex.ru>
2024-10-24 12:19:54 +0300

    feat: Create Factorial class with methods for calculating int, long, and BigInteger factorials

---
+++ src/main/java/school/faang/greatandterriblefactorial/Factorial.java
@@ -0,0 +1,44 @@
+package school.faang.greatandterriblefactorial;
+
+import java.math.BigInteger;
+
+public class Factorial {
+    private static final int MAX_INT_FACTORIAL = 12;
+    private static final int MAX_LONG_FACTORIAL = 19;
+
+    public static int factorialInt(int n) throws IllegalArgumentException {
+        if (n < 0 || n > MAX_INT_FACTORIAL) {
+            throw new IllegalArgumentException("Value out of range for int factorial calculation.");
+        }
+        int result = 1;
+        for (int i = 2; i <= n; i++) {
+            result *= i;
+        }
+        return result;
+    }
+
+    public static long factorialLong(int n) throws IllegalArgumentException {
+        if (n <= MAX_INT_FACTORIAL) {
+            return factorialInt(n);
+        }
+        if (n > MAX_LONG_FACTORIAL) {
+            throw new IllegalArgumentException("Value out of range for long factorial calculation.");
+        }
+        long result = 1L;
+        for (int i = 2; i <= n; i++) {
+            result *= i;
+        }
+        return result;
+    }
+
+    public static BigInteger factorialBig(int n) {
+        if (n <= MAX_LONG_FACTORIAL) {
+            return BigInteger.valueOf(factorialLong(n));
+        }
+        BigInteger result = BigInteger.ONE;
+        for (int i = 2; i <= n; i++) {
+            result = result.multiply(BigInteger.valueOf(i));
+        }
+        return result;
+    }
+}
