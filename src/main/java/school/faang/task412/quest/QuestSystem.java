f569bfc62261bc4f7d28ae49d8f1bb53b04d6896
Svyatoslav Kostrov <kost-2014@mail.ru>
2024-10-24 17:56:13 +0300

    Solution for WOW task reworked

--- src/main/java/school/faang/task412/quest/QuestSystem.java
+++ src/main/java/school/faang/task412/quest/QuestSystem.java
@@ -3,8 +3,12 @@ package school.faang.task412.quest;
 import school.faang.task412.player.Player;
 
 import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
 
 public class QuestSystem {
+    private ExecutorService service = Executors.newFixedThreadPool(3);
     public CompletableFuture<Player> startQuest(Player player, Quest quest) {
         return CompletableFuture.supplyAsync(() -> {
             try {
@@ -14,6 +18,17 @@ public class QuestSystem {
             } catch (InterruptedException e) {
                 throw new RuntimeException(e.getMessage());
             }
-        });
+        },service);
+    }
+    public void shutdown(){
+        service.shutdown();
+        try {
+            if(!service.awaitTermination(1, TimeUnit.MINUTES)){
+                System.out.println("Ошибка");
+                service.shutdownNow();
+            }
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
