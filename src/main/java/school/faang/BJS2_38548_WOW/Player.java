880c6d3689519f0842e8fe47af5a91d3e83e8935
Maxim Gorobets <gorobets.maks@bk.ru>
2024-10-24 14:28:44 +1000

    new Player class

---
+++ src/main/java/school/faang/BJS2_38548_WOW/Player.java
@@ -0,0 +1,12 @@
+package school.faang.BJS2_38548_WOW;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class Player {
+    private String name;
+    private int level;
+    private int experience;
+}
