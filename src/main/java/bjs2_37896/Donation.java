955bc5c3c7c2318a9b120bb543605827345c5d25
Sanasar1 <almasyansb@gmail.com>
2024-10-24 16:41:37 +0300

    feat: added Organization

---
+++ src/main/java/bjs2_37896/Donation.java
@@ -0,0 +1,13 @@
+package bjs2_37896;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+public class Donation {
+    private final int id;
+    private double amount;
+}
