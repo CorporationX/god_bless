6e7fef6824b2cccf7bce012637ae5f821a3b96a4
VovaBuzivskyi <VovaBuzivskyi@gmail.com>
2024-10-24 21:37:10 +0700

    BJS2-38409 superheroes battle(Amended)

--- src/main/java/school/faang/superheroes_battle_BJS2_38409/SuperheroBattle.java
+++ src/main/java/school/faang/superheroes_battle_BJS2_38409/SuperheroBattle.java
@@ -4,15 +4,16 @@ import lombok.Getter;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.ExecutionException;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.Future;
 
 @Getter
 public class SuperheroBattle {
-    private ExecutorService executor = Executors.newCachedThreadPool();
+    private final ExecutorService executor = Executors.newCachedThreadPool();
 
-    public List<Future<Superhero>> runCompetitions(List<Pair> competitions) {
+    public Future<Superhero> runCompetitions(List<Pair> competitions) {
         List<Future<Superhero>> futures = new ArrayList<>();
 
         competitions.forEach(pair -> futures.add(executor.submit(() -> {
@@ -20,6 +21,46 @@ public class SuperheroBattle {
             int totalPower2 = (pair.getSecondSuperhero().getAgility() + pair.getSecondSuperhero().getStrength());
             return totalPower1 >= totalPower2 ? pair.getFistSuperhero() : pair.getSecondSuperhero();
         })));
-        return futures;
+
+        if (isSizeOne(futures.size())) {
+            final int FIRST_INDEX = 0;
+            return futures.get(FIRST_INDEX);
+        }
+        competitions.clear();
+        List<Pair> nextRoundPairs = new ArrayList<>();
+
+        if (isEvenNumber(futures.size())) {
+            repeatBattle(nextRoundPairs, futures, futures.size());
+        } else {
+            repeatBattle(nextRoundPairs, futures, deleteLastOddIndex(futures.size()));
+        }
+
+        return runCompetitions(nextRoundPairs);
+    }
+
+    private void repeatBattle(List<Pair> superheroPairs,
+                              List<Future<Superhero>> winnersFutures, int listSize) {
+        final int ONE_INDEX = 1;
+        final int DOUBLE_INCREMENT = 2;
+        for (int i = 0; i < listSize; i += DOUBLE_INCREMENT) {
+            try {
+                superheroPairs.add(new Pair(winnersFutures.get(i).get(),
+                        winnersFutures.get(i + ONE_INDEX).get()));
+            } catch (InterruptedException | ExecutionException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    private boolean isEvenNumber(int number) {
+        return number % 2 == 0;
+    }
+
+    private boolean isSizeOne(int number) {
+        return number == 1;
+    }
+
+    private int deleteLastOddIndex(int number) {
+        return number - 1;
     }
 }
