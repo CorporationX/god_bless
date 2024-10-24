042af9b80cca89f5662d9afde52ccf5c0c7b5a42
Igor <asachiyigor@gmail.com>
2024-10-24 20:46:16 +0300

    BJS2-36745
    initial commit

---
+++ src/main/java/school/faangSprint3/t20/TamagotchiVlad.java
@@ -0,0 +1,55 @@
+package school.faangSprint3.t20;
+
+public class TamagotchiVlad {
+    private final String name;
+    private String state = "нейтральное";
+
+    public TamagotchiVlad(String name) {
+        this.name = name;
+    }
+
+    public synchronized void feed() {
+        state = "сыт";
+        System.out.println(name + " ест. Его состояние: " + state);
+        try {
+            Thread.sleep(1000); // Имитация времени выполнения действия
+        } catch (InterruptedException e) {
+            Thread.currentThread().interrupt();
+        }
+    }
+
+    public synchronized void play() {
+        state = "весел";
+        System.out.println(name + " играет. Его состояние: " + state);
+        try {
+            Thread.sleep(1000);
+        } catch (InterruptedException e) {
+            Thread.currentThread().interrupt();
+        }
+    }
+
+    public synchronized void clean() {
+        state = "чист";
+        System.out.println(name + " чистится. Его состояние: " + state);
+        try {
+            Thread.sleep(1000);
+        } catch (InterruptedException e) {
+            Thread.currentThread().interrupt();
+        }
+    }
+
+    public synchronized void sleep() {
+        state = "спит";
+        System.out.println(name + " спит. Его состояние: " + state);
+        try {
+            Thread.sleep(1000);
+        } catch (InterruptedException e) {
+            Thread.currentThread().interrupt();
+        }
+    }
+
+    @Override
+    public String toString() {
+        return name;
+    }
+}
