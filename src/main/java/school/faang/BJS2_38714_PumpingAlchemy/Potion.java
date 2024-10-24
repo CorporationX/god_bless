a76f065b3cd6325894458c892656e7e58986f9a1
Maxim Gorobets <gorobets.maks@bk.ru>
2024-10-24 18:01:59 +1000

    new Potion class

---
+++ src/main/java/school/faang/BJS2_38714_PumpingAlchemy/Potion.java
@@ -0,0 +1,11 @@
+package school.faang.BJS2_38714_PumpingAlchemy;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class Potion {
+    private String name;
+    private int requiredIngredients;
+}
