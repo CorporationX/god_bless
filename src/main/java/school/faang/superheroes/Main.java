4c40e9c189dbf7802941b2b8156f95a8e27ca8e0
Memoreks <akifyevalex1@gmail.com>
2024-10-24 17:11:44 +0700

    Добавлено использование логов и переделан метод определения победителя боя

--- src/main/java/school/faang/superheroes/Main.java
+++ src/main/java/school/faang/superheroes/Main.java
@@ -1,46 +1,40 @@
 package school.faang.superheroes;
 
+import lombok.extern.slf4j.Slf4j;
+
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
+import java.util.Random;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.Future;
 
+@Slf4j
 public class Main {
+    private static final int MAXIMUM_SKILL = 100;
+
     public static void main(String[] args) {
         SuperheroBattle superheroBattle = new SuperheroBattle();
+        Random random = new Random();
 
-        Superhero ironMan = new Superhero("Iron Man", 9, 6);
-        Superhero captainAmerica = new Superhero("Captain America", 8, 8);
-        Superhero thor = new Superhero("Thor", 10, 7);
-        Superhero hulk = new Superhero("Hulk", 10, 4);
+        List<String> names = List.of("Iron Man", "Captain America", "Thor", "Hulk",
+                "Spider-Man", "Vision", "Ultron", "Thanos", "Ant-Man");
 
-        List<Pair<Superhero, Superhero>> superheroPairs = Arrays.asList(
-                new Pair<>(ironMan, captainAmerica),
-                new Pair<>(thor, hulk)
-        );
+        List<Superhero> superheroes = new ArrayList<>();
+        for (String name : names) {
+            int strength = random.nextInt(MAXIMUM_SKILL);
+            int agility = random.nextInt(MAXIMUM_SKILL);
+            superheroes.add(new Superhero(name, strength, agility));
+        }
+
+        List<Pair<Superhero, Superhero>> superheroPairs = superheroBattle.toPairs(superheroes);
 
         List<Future<Superhero>> winnersFutures = superheroBattle.runCompetitions(superheroPairs);
 
-        while (winnersFutures.size() > 1) {
-            superheroPairs = new ArrayList<>();
-            for (int i = 0; i < winnersFutures.size(); i += 2) {
-                try {
-                    Superhero winner = winnersFutures.get(i).get();
-                    Superhero winner2 = winnersFutures.get(i + 1).get();
-                    superheroPairs.add(new Pair<>(winner, winner2));
-                } catch (InterruptedException | ExecutionException e) {
-                    e.printStackTrace();
-                }
-            }
-            winnersFutures = superheroBattle.runCompetitions(superheroPairs);
-        }
-
         try {
             Superhero winner = winnersFutures.get(0).get();
-            System.out.println("Победитель финального боя: " + winner.getName());
+            log.info("Победитель финального боя: " + winner.getName());
         } catch (InterruptedException | ExecutionException e) {
-            e.printStackTrace();
+            log.error(e.getMessage());
         }
 
         superheroBattle.shutdown();
--- src/main/java/school/faang/superheroes/SuperheroBattle.java
+++ src/main/java/school/faang/superheroes/SuperheroBattle.java
@@ -1,24 +1,70 @@
 package school.faang.superheroes;
 
+import lombok.extern.slf4j.Slf4j;
+
 import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.ExecutionException;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.Future;
 
+@Slf4j
 public class SuperheroBattle {
-    private ExecutorService executor = Executors.newFixedThreadPool(8);
+    private static final int THREAD_POOL_SIZE = 8;
+    private static final int TIME_TO_WORK = 1000;
+
+    private ExecutorService executor = Executors.newFixedThreadPool(THREAD_POOL_SIZE);
 
     public List<Future<Superhero>> runCompetitions(List<Pair<Superhero, Superhero>> pairs) {
         List<Future<Superhero>> winners = new ArrayList<>();
         for (Pair<Superhero, Superhero> pair : pairs) {
             winners.add(executor.submit(() -> {
-                int powerAttackFirst = pair.getL().getAgility() + pair.getL().getStrength();
-                int powerAttackSecond = pair.getR().getAgility() + pair.getR().getStrength();
+                Thread.sleep(TIME_TO_WORK);
+                if (pair.getL() == null) {
+                    log.info(String.format("Прошел без боя в следующий этап %s", pair.getR().getName()));
+                    return pair.getR();
+                }
+                if (pair.getR() == null) {
+                    log.info(String.format("Прошел без боя в следующий этап %s", pair.getL().getName()));
+                    return pair.getL();
+                }
+                Superhero firstHeroes = pair.getL();
+                Superhero secondHeroes = pair.getR();
+                int powerAttackFirst = firstHeroes.getAgility() + firstHeroes.getStrength();
+                int powerAttackSecond = secondHeroes.getAgility() + secondHeroes.getStrength();
+                log.info(String.format("Бои между %s (%d) и %s (%d)",
+                        firstHeroes.getName(), powerAttackFirst, secondHeroes.getName(), powerAttackSecond));
                 return powerAttackFirst > powerAttackSecond ? pair.getL() : pair.getR();
             }));
         }
+
+        if (winners.size() > 1) {
+            List<Superhero> heroes = winners.stream()
+                    .map(winner -> {
+                        try {
+                            return winner.get();
+                        } catch (InterruptedException | ExecutionException e) {
+                            throw new RuntimeException(e);
+                        }
+                    })
+                    .toList();
+            List<Pair<Superhero, Superhero>> superheroPairs = toPairs(heroes);
+            log.info("--------Следующий этап боев---------");
+            winners = runCompetitions(superheroPairs);
+        }
+
         return winners;
+    }
+
+    public List<Pair<Superhero, Superhero>> toPairs(List<Superhero> superheroes) {
+        List<Pair<Superhero, Superhero>> superheroPairs = new ArrayList<>();
+        for (int i = 0; i < superheroes.size(); i += 2) {
+            Superhero winner = superheroes.get(i);
+            Superhero winner2 = superheroes.size() > (i + 1) ? superheroes.get(i + 1) : null;
+            superheroPairs.add(new Pair<>(winner, winner2));
+        }
+        return superheroPairs;
     }
 
     public void shutdown() {
