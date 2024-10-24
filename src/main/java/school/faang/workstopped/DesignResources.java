b103a9f4d64ee0e8e7719de29a81467eef144e1f
“Andrey <aiegorovceo@yandex.ru>
2024-10-24 19:04:26 +0300

    fix: Replace synchronized with ReentrantReadWriteLock to avoid deadlock

--- src/main/java/school/faang/workstopped/DesignResources.java
+++ src/main/java/school/faang/workstopped/DesignResources.java
@@ -2,17 +2,29 @@ package school.faang.workstopped;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.locks.ReentrantReadWriteLock;
 
 public class DesignResources {
     private final List<String> files = new ArrayList<>();
+    private final ReentrantReadWriteLock lock = new ReentrantReadWriteLock();
 
-    public synchronized List<String> readFiles() {
-        System.out.println("Reading files from Design Resources");
-        return new ArrayList<>(files);
+    public List<String> readFiles() {
+        lock.readLock().lock();
+        try {
+            System.out.println("Reading files from Design Resources");
+            return new ArrayList<>(files);
+        } finally {
+            lock.readLock().unlock();
+        }
     }
 
-    public synchronized void addFile(String fileName) {
-        System.out.println("Adding file to Design Resources: " + fileName);
-        files.add(fileName);
+    public void addFile(String fileName) {
+        lock.writeLock().lock();
+        try {
+            System.out.println("Adding file to Design Resources: " + fileName);
+            files.add(fileName);
+        } finally {
+            lock.writeLock().unlock();
+        }
     }
 }
