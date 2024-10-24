cd1ea9277b909beac49e87880b6cf12abc87e8ff
ZhubanyshZh <zubanyszarylkasyn@gmail.com>
2024-10-25 00:51:33 +0500

    feat(BJS2-38821): notification FaceBook
    added functional which async add the notifications to list and compliance true access to list

---
+++ src/main/java/org/example/model/facebookNotification/Notification.java
@@ -0,0 +1,13 @@
+package org.example.model.facebookNotification;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+public class Notification {
+    private int id;
+    private String message;
+}
