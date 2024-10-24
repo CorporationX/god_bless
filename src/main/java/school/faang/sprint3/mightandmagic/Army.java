61267656823720cfd1a4030afc32e63d9cebf68b
Anatoly Galay <galay007@mail.ru>
2024-10-24 22:00:29 +0300

    refactor after review

--- src/main/java/school/faang/sprint3/mightandmagic/Army.java
+++ src/main/java/school/faang/sprint3/mightandmagic/Army.java
@@ -4,44 +4,27 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Army {
-    List<Thread> threads;
-
-    List<Archer> archers = new ArrayList<>();
-    List<Mage> mages25 = new ArrayList<>();
-    List<Mage> mages50 = new ArrayList<>();
-    List<Swordsman> swordsmans = new ArrayList<>();
-
+    List<Integer> powers = new ArrayList<>();
     Integer totalPower = 0;
 
-    public void addUnit(Object obj) {
+    public <T> void addUnit(T obj) {
+        int power = 0;
         if (obj instanceof Archer) {
-            archers.add((Archer) obj);
-        } else if (obj instanceof Mage && ((Mage) obj).getPower() == 25) {
-            mages25.add((Mage) obj);
-        } else if (obj instanceof Mage && ((Mage) obj).getPower() == 50) {
-            mages50.add((Mage) obj);
+            powers.add(((Archer)obj).getPower());
+        } else if (obj instanceof Mage) {
+            powers.add(((Mage)obj).getPower());
         } else if (obj instanceof Swordsman) {
-            swordsmans.add((Swordsman) obj);
+            powers.add(((Swordsman)obj).getPower());
         }
     }
 
     public int calculateTotalPower() throws InterruptedException {
-        Thread one = new Thread(() -> totalPower += archers.stream().map(Archer::getPower).reduce(Integer::sum).orElse(0));
-        threads.add(one);
 
-        Thread two = new Thread(() -> totalPower += mages25.stream().map(Mage::getPower).reduce(Integer::sum).orElse(0));
-        threads.add(two);
-
-        Thread three = new Thread(() -> totalPower += mages50.stream().map(Mage::getPower).reduce(Integer::sum).orElse(0));
-        threads.add(three);
-
-        Thread four = new Thread(() -> totalPower += swordsmans.stream().map(Swordsman::getPower).reduce(Integer::sum).orElse(0));
-        threads.add(four);
-
-        for (Thread thread : threads) {
+        for (int i = 0; i < powers.size(); i++) {
             try {
+                final int a = i;
+                Thread thread = new Thread(() -> totalPower += powers.get(a));
                 thread.start();
-
                 thread.join();
             } catch (Exception e) {
                 System.out.println("Invalid process");
