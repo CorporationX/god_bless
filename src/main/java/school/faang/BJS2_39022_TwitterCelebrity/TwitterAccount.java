4058a3811c5a94af24971fc912e0c2aa77684059
Maxim Gorobets <gorobets.maks@bk.ru>
2024-10-24 20:01:39 +1000

    logging

--- src/main/java/school/faang/BJS2_39022_TwitterCelebrity/TwitterAccount.java
+++ src/main/java/school/faang/BJS2_39022_TwitterCelebrity/TwitterAccount.java
@@ -1,9 +1,11 @@
 package school.faang.BJS2_39022_TwitterCelebrity;
 
 import lombok.Getter;
+import lombok.extern.slf4j.Slf4j;
 
 import java.util.concurrent.atomic.AtomicInteger;
 
+@Slf4j
 @Getter
 public class TwitterAccount {
     private final String username;
@@ -16,5 +18,6 @@ public class TwitterAccount {
 
     public void addFollower() {
         followers.incrementAndGet();
+        log.info("Added new follower to {}'s account", username);
     }
 }
