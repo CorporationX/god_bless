178b39bd5d076cd15f3223316a12ab53367efa41
Naku <polevaya.nastena@internet.ru>
2024-10-24 23:00:16 +0300

    Naku-BJS2-37915 Пошлите ворона! v_1

--- src/main/java/school/faang/Multithreading/sprint_4/Amazon/OrderProcessor.java
+++ src/main/java/school/faang/Multithreading/sprint_4/Amazon/OrderProcessor.java
@@ -2,9 +2,6 @@ package school.faang.Multithreading.sprint_4.Amazon;
 
 import lombok.extern.slf4j.Slf4j;
 
-import java.util.List;
-import java.util.Vector;
-import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.atomic.AtomicInteger;
 
 @Slf4j
@@ -12,30 +9,9 @@ public class OrderProcessor {
     public final static long TIME_PROCESS = 3_000;
     private AtomicInteger totalProcessedOrders;
 
-    public void processAllOrders (List<Order> orders){
-        List<CompletableFuture<Void>> futures = orders.stream()
-                .map(order -> processOrder(order)) // Асинхронная обработка каждого заказа
-                .collect(Collectors.toList());
-
-
-        CompletableFuture.allOf(futureTotalsList.toArray(CompletableFuture[0]));
-
-        System.out.println("Обработано заказов: " + totalProcessedOrders);
+    public void  processOrder(Order order){
+        
     }
 
-    private void processOrder(Order order){
-        CompletableFuture<Void> futureOrder = CompletableFuture.runAsync(() -> {
-            try {
-                Thread.sleep(TIME_PROCESS);
-                order.setStatus(OrderStatus.PROCESSED);
-                totalProcessedOrders.incrementAndGet();
-            } catch (InterruptedException e) {
-                log.error("Операция была прервана", e);
-                throw new RuntimeException(e);
-            }
-        });
-
-
-
-    }
 }
+
