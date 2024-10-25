d61504810ddca3c6aa42d335418d4736e1ae6548
Naku <polevaya.nastena@internet.ru>
2024-10-26 02:02:34 +0300

    Naku-BJS2-38809 Dota v_2

--- src/main/java/school/faang/Multithreading/sprint_4/CurryEat/DistributionItems.java
+++ src/main/java/school/faang/Multithreading/sprint_4/CurryEat/DistributionItems.java
@@ -3,7 +3,6 @@ package school.faang.Multithreading.sprint_4.CurryEat;
 import lombok.extern.slf4j.Slf4j;
 
 import java.util.concurrent.CompletableFuture;
-import java.util.concurrent.ThreadLocalRandom;
 
 @Slf4j
 public class DistributionItems {
