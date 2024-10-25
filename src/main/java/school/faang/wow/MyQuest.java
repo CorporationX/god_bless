1b22c974696da9f89113de2c0315e75be71c03eb
Maks Russkikh <15maksim@mail.ru>
2024-10-25 15:50:59 +0300

    i don-t understand

--- src/main/java/school/faang/wow/MyQuest.java
+++ src/main/java/school/faang/wow/MyQuest.java
@@ -1,16 +1,15 @@
 package school.faang.wow;
 
-import java.util.List;
 import java.util.concurrent.CompletableFuture;
 
 public class MyQuest {
     public static void main(String[] args) {
         QuestSystem system = new QuestSystem();
-        List<CompletableFuture<Player>> allCompletableFuture = List.of(
-                system.startQuest(new Player("Maks", 1), new Quest("Warships", 5, 100)),
-                system.startQuest(new Player("Rusik", 2), new Quest("World of Tanks", 3, 150)));
-        allCompletableFuture.stream()
-                .map(CompletableFuture::join)
-                .forEach(Player::showPlayer);
+        CompletableFuture<Player> future1 = system.startQuest(new Player("Maks", 1), new Quest("Warships", 5, 100));
+        CompletableFuture<Player> future2 = system.startQuest(new Player("Rusik", 2), new Quest("World of Tanks", 3, 150));
+        future1.join();
+        future2.join();
+        future1.thenAccept(Player::showPlayer);
+        future2.thenAccept(Player::showPlayer);
     }
 }
