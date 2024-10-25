e655fa6f77f9515f60f5a383bfc0164b0759c89b
Damba <mdamba2020@yandex.ru>
2024-10-26 02:22:27 +0300

    spotifyEdit

--- src/main/java/school/faang/spotify/Music.java
+++ src/main/java/school/faang/spotify/Music.java
@@ -4,7 +4,7 @@ import java.util.Arrays;
 import java.util.List;
 
 public class Music {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
         Player player = new Player();
         Thread playThread = new Thread(player::play);
         Thread pauseThread = new Thread(player::pause);
@@ -16,5 +16,14 @@ public class Music {
         for (Thread thread : threads) {
             thread.start();
         }
+
+        for (Thread thread : threads) {
+            thread.join();
+        }
+
+        System.out.println("Выполнено");
     }
+
 }
+
+
