ea2f41fc5f03ee426d33daa15774f939112d142e
“Andrey <aiegorovceo@yandex.ru>
2024-10-25 10:40:41 +0300

    feat: Added BankDemo class for demonstrating multi-threaded bank transfers refactor: Optimized balance calculation using Stream API and improved locking order

---
+++ src/main/java/school/faang/playbank/BankDemo.java
@@ -0,0 +1,66 @@
+package school.faang.playbank;
+
+import java.util.Random;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
+
+public class BankDemo {
+    public static void main(String[] args) {
+        Bank bank = new Bank();
+//          Как лучше создавать объекты?
+        double[] initialBalances = {1000.0, 1500.0, 2000.0};
+        for (int i = 0; i < initialBalances.length; i++) {
+            bank.addAccount(new Account(i + 1, initialBalances[i]));
+        }
+
+//        bank.addAccount(new Account(1, 1000.0));
+//        bank.addAccount(new Account(2, 1500.0));
+//        bank.addAccount(new Account(3, 2000.0));
+
+//        Account account1 = new Account(1, 1000);
+//        Account account2 = new Account(2, 2000);
+//        Account account3 = new Account(3, 3000);
+//        bank.addAccount(account1);
+//        bank.addAccount(account2);
+//        bank.addAccount(account3);
+
+        ExecutorService executor = Executors.newFixedThreadPool(3);
+        Random random = new Random();
+
+        for (int i = 0; i < 10; i++) {
+            executor.submit(() -> {
+                int fromId = random.nextInt(3) + 1;
+                int toId = random.nextInt(3) + 1;
+                double amount = random.nextInt(500) + 100;
+
+                performTransfer(bank, fromId, toId, amount);
+            });
+        }
+
+        executor.shutdown();
+        try {
+            if (!executor.awaitTermination(1, TimeUnit.MINUTES)) {
+                executor.shutdownNow();
+            }
+        } catch (InterruptedException e) {
+            executor.shutdownNow();
+        }
+
+        System.out.println("Total bank balance: " + bank.getTotalBalance());
+    }
+
+    private static void performTransfer(Bank bank, int fromId, int toId, double amount) {
+        if (fromId != toId) {
+            boolean success = bank.transfer(fromId, toId, amount);
+            if (success) {
+                System.out.println("Transfer successful: $" + amount + " from Account " + fromId + " to Account " + toId);
+            } else {
+                System.out.println("Transfer failed: Insufficient funds on Account " + fromId);
+            }
+        } else {
+            System.out.println("Transfer skipped: Source and destination accounts are the same.");
+        }
+    }
+}
+
