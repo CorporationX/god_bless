07553f4ff2e0f851d1a58f0edf0aadab3d578247
Egor Semenovskii <gaoqiao5217@gmail.com>
2024-10-25 17:49:48 +0300

    Synchronized list

--- src/main/java/school/faang/FacebookNotifications/NotificationManager.java
+++ src/main/java/school/faang/FacebookNotifications/NotificationManager.java
@@ -3,15 +3,14 @@ package school.faang.FacebookNotifications;
 import lombok.extern.slf4j.Slf4j;
 
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
 import java.util.concurrent.CompletableFuture;
 
 @Slf4j
 public class NotificationManager {
-    private final List<Notification> notifications = Collections.synchronizedList(new ArrayList<>());
+    private final List<Notification> notifications = new ArrayList<>();
 
-    public void addNotification(Notification notification) {
+    public synchronized void addNotification(Notification notification) {
         notifications.add(notification);
     }
 
