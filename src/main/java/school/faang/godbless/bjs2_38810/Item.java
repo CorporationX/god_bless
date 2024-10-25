6b7e9d96a429f2432a379cc1ea22e4ff5d67febd
AnnaAplavina <59871896+AnnaAplavina@users.noreply.github.com>
2024-10-25 13:17:14 +0300

    BJS2-38810-Carry отожрался

---
+++ src/main/java/school/faang/godbless/bjs2_38810/Item.java
@@ -0,0 +1,15 @@
+package school.faang.godbless.bjs2_38810;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@AllArgsConstructor
+@Getter
+@Setter
+@ToString
+public class Item {
+    private String name;
+    private int power;
+}
