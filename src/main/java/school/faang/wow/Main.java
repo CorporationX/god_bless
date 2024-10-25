62d6787c8a0af70f79f2d2ea2dafaae8a36888d0
Ilya Ishanov <ilia.detroit2016@yandex.ru>
2024-10-26 03:27:47 +0500

    правки

--- src/main/java/school/faang/wow/Main.java
+++ src/main/java/school/faang/wow/Main.java
@@ -1,5 +1,7 @@
 package school.faang.wow;
 
+import java.util.Arrays;
+import java.util.List;
 import java.util.concurrent.CompletableFuture;
 
 public class Main {
@@ -11,10 +13,11 @@ public class Main {
         Quest quest1 = new Quest("Defeat the Lich King", 10, 150);
         Quest quest2 = new Quest("Retrieve the Sword of Azeroth", 8, 100);
 
-        CompletableFuture<Player> playerQuest1 = questSystem.startQuest(player1, quest1);
-        CompletableFuture<Player> playerQuest2 = questSystem.startQuest(player2, quest2);
+        List<CompletableFuture<Player>> playersQuest = Arrays.asList(
+                questSystem.startQuest(player1, quest1), questSystem.startQuest(player2, quest2));
 
-        playerQuest1.thenAccept(player -> System.out.println(player.getName() + " has completed the quest and now has " + player.getExperience() + " experience points.")).join();
-        playerQuest2.thenAccept(player -> System.out.println(player.getName() + " has completed the quest and now has " + player.getExperience() + " experience points.")).join();
+        for (CompletableFuture<Player> completableFuture : playersQuest) {
+            completableFuture.thenAccept((player) -> System.out.println(player.getName() + " has completed the quest and now has " + player.getExperience() + " experience points.")).join();
+        }
     }
 }
