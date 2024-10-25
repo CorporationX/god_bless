e7d7cb814f9d772a5c6f344690633b4a859e2973
Igor <asachiyigor@gmail.com>
2024-10-25 17:10:40 +0300

    BJS2-40115
    initial commit

---
+++ src/main/java/school/faangSprint4/t12/DonationSystem.java
@@ -0,0 +1,39 @@
+package school.faangSprint4.t12;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
+
+public class DonationSystem {
+    public static void main(String[] args) {
+        Organization organization = new Organization();
+        int numberOfThreads = 5;
+        int donationsPerThread = 100;
+
+        ExecutorService executorService = Executors.newFixedThreadPool(numberOfThreads);
+
+        for (int i = 0; i < numberOfThreads; i++) {
+            final int threadNumber = i;
+            executorService.submit(() -> {
+                for (int j = 0; j < donationsPerThread; j++) {
+                    int donationId = threadNumber * donationsPerThread + j;
+                    double amount = 1 + Math.random() * 99;
+                    Donation donation = new Donation(donationId, amount);
+                    organization.addDonation(donation);
+
+                    System.out.printf("Thread %d: Added donation #%d with amount %.2f%n",
+                            threadNumber, donationId, amount);
+                }
+            });
+        }
+
+        executorService.shutdown();
+        try {
+            executorService.awaitTermination(10, TimeUnit.SECONDS);
+        } catch (InterruptedException e) {
+            System.err.println("Thread execution was interrupted");
+        }
+
+        System.out.printf("%nFinal balance: %.2f%n", organization.getBalance());
+    }
+}
