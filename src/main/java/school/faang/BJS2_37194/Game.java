b4c685a3b9932bb777f4993c5fe55825b0713dcc
Stanislaav Kozh <printks@yandex.ru>
2024-10-25 21:24:08 +0300

    fix Bro Force!

--- src/main/java/school/faang/BJS2_37194/Game.java
+++ src/main/java/school/faang/BJS2_37194/Game.java
@@ -1,22 +1,30 @@
 package school.faang.BJS2_37194;
 
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
 public class Game {
     private int score = 0;
     private int lives = 10;
+    private boolean isGameOver = false;
 
     private final Object scoreLock = new Object();
     private final Object livesLock = new Object();
 
     public void update(boolean scored, boolean lostLife) {
-        synchronized (scoreLock) {
-            if (scored) {
+        if (isGameOver) {
+            return;
+        }
+
+        if (scored) {
+            synchronized (scoreLock) {
                 score++;
                 System.out.println("Очки увеличены. Текущий счёт: " + score);
             }
         }
 
-        synchronized (livesLock) {
-            if (lostLife) {
+        if (lostLife) {
+            synchronized (livesLock) {
                 lives--;
                 System.out.println("Жизнь потеряна. Осталось жизней: " + lives);
                 if (lives <= 0) {
@@ -27,27 +35,35 @@ public class Game {
     }
 
     private void gameOver() {
-        System.out.println("Игра окончена. Итоговый счёт: " + score);
+        isGameOver = true;
+        System.out.println("Game Over. Score: " + score);
+    }
+
+    public boolean isGameOver() {
+        return isGameOver;
     }
 
     public void startGame() {
-        for (int i = 0; i < 100; i++) {
-            boolean scored = Math.random() > 0.5;
-            boolean lostLife = Math.random() > 0.7;
+        ExecutorService executorService = Executors.newFixedThreadPool(3);
 
-            update(scored, lostLife);
+        for (int i = 0; i < 10; i++) {
+            executorService.execute(() -> {
+                while (!isGameOver()) {
+                    boolean scored = Math.random() > 0.5;
+                    boolean lifeLost = Math.random() > 0.7;
+                    update(scored, lifeLost);
 
-            try {
-                Thread.sleep(100);
-            } catch (InterruptedException e) {
-                System.out.println("Игровой цикл был прерван.");
-                e.printStackTrace();
-            }
-
-            if (lives <= 0) {
-                break;
-            }
+                    try {
+                        Thread.sleep(100);
+                    } catch (InterruptedException e) {
+                        System.out.println("Game interrupted.");
+                        e.printStackTrace();
+                    }
+                }
+            });
         }
+
+        executorService.shutdown();
     }
 
     public static void main(String[] args) {
