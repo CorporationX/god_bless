49cba8c16e0da1b1eeb29fd770c78adb76d8cf7c
Doniyor Kurbanov <doniyor.kurbanov.24@gmail.com>
2024-10-25 21:17:19 +0500

    BJS2-39060. Становимся celebrity в Twitter. Немного схитрил с конструктором и переменной. Можно ли так делать ?

--- src/main/java/school/faang/BJS239060/TwitterAccount.java
+++ src/main/java/school/faang/BJS239060/TwitterAccount.java
@@ -3,9 +3,16 @@ package school.faang.BJS239060;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 
+import java.util.concurrent.atomic.AtomicInteger;
+
 @AllArgsConstructor
 @Data
 public class TwitterAccount {
-    private String username;
-    private int followers;
+    private final String username;
+    private final AtomicInteger followers;
+
+    public TwitterAccount(String username, int initialFollowers) {
+        this.username = username;
+        this.followers = new AtomicInteger(initialFollowers);
+    }
 }
--- src/main/java/school/faang/BJS239060/TwitterSubscriptionSystem.java
+++ src/main/java/school/faang/BJS239060/TwitterSubscriptionSystem.java
@@ -5,29 +5,25 @@ import lombok.extern.slf4j.Slf4j;
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
-import java.util.concurrent.atomic.AtomicInteger;
 
 @Slf4j
 public class TwitterSubscriptionSystem {
     private static final int THREADS = 2;
     private final ExecutorService executorService = Executors.newFixedThreadPool(THREADS);
-    private final AtomicInteger followers = new AtomicInteger(0);
 
     public synchronized void addFollower(TwitterAccount account) {
             log.info("Увеличиваем количество подписчиков, текущее количество " + account.getFollowers());
             try {
                 Thread.sleep(3000);
             } catch (InterruptedException e) {
-                throw new RuntimeException(e);
+                log.error("Что то пошло не так с добавлением подписчиков ", e);
             }
-            int total = account.getFollowers();
-            followers.addAndGet(total);
+            account.getFollowers().incrementAndGet();
     }
 
     public void followAccount(TwitterAccount account) {
-        CompletableFuture<TwitterAccount> future = CompletableFuture.supplyAsync(() -> account, executorService);
-        future.thenRunAsync(() -> addFollower(account), executorService)
-                .thenRun(() -> log.info("Общее количество подписчиков: " + followers))
+        CompletableFuture.runAsync(() -> addFollower(account), executorService)
+                .thenRun(() -> log.info("Общее количество подписчиков: " + account.getFollowers()))
                 .join();
     }
 
