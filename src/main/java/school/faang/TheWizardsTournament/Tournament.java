ed99fb59579df2456e5d19a4a407e895fcc10410
m1rroxxx <Nikita.sidorov.7@icloud.com>
2024-10-26 00:40:03 +0300

    вместо переменной exceptionHandler сделал метол handlerException

--- src/main/java/school/faang/TheWizardsTournament/Tournament.java
+++ src/main/java/school/faang/TheWizardsTournament/Tournament.java
@@ -5,7 +5,6 @@ import lombok.extern.slf4j.Slf4j;
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
-import java.util.function.Function;
 import java.util.function.Supplier;
 
 @Slf4j
@@ -24,18 +23,18 @@ public class Tournament {
             return school;
         };
 
-        Function<Throwable, School> exceptionHandler = (e -> {
-            executorService.shutdownNow();
-            log.error(e.getMessage());
-            throw new IllegalStateException("Task interrupted " + e.getMessage(), e);
-        });
-
         return CompletableFuture.supplyAsync(completingTask, executorService)
                 .thenApply(schoolInstance -> schoolInstance.earnPoints(task.getReward()))
-                .exceptionally(exceptionHandler);
+                .exceptionally(this::handlerException);
     }
 
     public void shutdown() {
         executorService.shutdown();
+    }
+
+    private School handlerException(Throwable e) {
+        executorService.shutdownNow();
+        log.error(e.getMessage());
+        throw new IllegalStateException("Task interrupted " + e.getMessage(), e);
     }
 }
