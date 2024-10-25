cacadb7207a1a98fd64d5b238fc4d6f58b8f3865
bogda <bogdanskrip404@gmail.com>
2024-10-25 12:25:25 +0500

    Асинхронность и Future

---
+++ src/main/java/school/faang/asyncFuture/MasterCardService.java
@@ -0,0 +1,52 @@
+package school.faang.asyncFuture;
+
+import lombok.extern.slf4j.Slf4j;
+
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+import java.util.concurrent.TimeUnit;
+
+@Slf4j
+public class MasterCardService {
+    private ExecutorService executor = Executors.newSingleThreadExecutor();
+
+    static int collectPayment(){
+        try {
+            Thread.sleep(10_000);
+            return 10_000;
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+            log.error("Сбор оплаты не выполнен.");
+            throw new RuntimeException();
+        }
+    }
+
+    static int sendAnalytics(){
+        try {
+            Thread.sleep(1_000);
+            return 1_000;
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+            log.error("Отправка аналитики не выполнена.");
+            throw new RuntimeException();
+        }
+    }
+
+    public void doAll() throws ExecutionException, InterruptedException {
+        CompletableFuture<Integer> analyticsFuture =
+                CompletableFuture.supplyAsync(MasterCardService::sendAnalytics);
+        Future<Integer> paymentFuture = executor.submit(MasterCardService::collectPayment);
+
+        Integer analyticsResult = analyticsFuture.join();
+        Integer paymentResult = paymentFuture.get();
+
+        System.out.println("Аналитика отправлена: " + analyticsResult);
+        System.out.println("Платёж выполнен: " + paymentResult);
+
+        executor.shutdown();
+        executor.awaitTermination(30, TimeUnit.SECONDS);
+    }
+}
