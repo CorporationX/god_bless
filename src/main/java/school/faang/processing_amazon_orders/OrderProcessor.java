24771ce23656a9d78958a70f657ff5478ee8f9c5
Alena Shmelkova <AlenaShmelkova@gmail.com>
2024-10-25 12:38:32 -0400

    BJS2-38137-ProcessingAmazonOrders <Changed the name of totalProcessedOrders to camelCase, got rid of throwing RuntimeException inside InterruptedException>

--- src/main/java/school/faang/processing_amazon_orders/OrderProcessor.java
+++ src/main/java/school/faang/processing_amazon_orders/OrderProcessor.java
@@ -11,7 +11,7 @@ import java.util.concurrent.atomic.AtomicInteger;
 
 @Slf4j
 public class OrderProcessor {
-    private static final AtomicInteger TOTAL_PROCESSED_ORDERS = new AtomicInteger(0);
+    private static final AtomicInteger totalProcessedOrders = new AtomicInteger(0);
     private final ExecutorService executor;
 
     public OrderProcessor(int poolSize) {
@@ -23,13 +23,12 @@ public class OrderProcessor {
             log.info("Starting processing order: " + order.getId());
             Thread.sleep(3000);
             order.setStatus(Status.PROCESSED);
-            int processedCount = TOTAL_PROCESSED_ORDERS.incrementAndGet();
+            int processedCount = totalProcessedOrders.incrementAndGet();
             log.info("Finished processing order " + order.getId() + ". Status is now " + order.getStatus() +
                     ". Total processed orders: " + processedCount);
         } catch (InterruptedException e) {
             log.error("Thread had been interrupted during processing order: " + order.getId(), e);
             Thread.currentThread().interrupt();
-            throw new RuntimeException("Order processing was interrupted", e);
         }
     }
 
@@ -46,7 +45,7 @@ public class OrderProcessor {
                 .toList();
         CompletableFuture<Void> allOf = CompletableFuture.allOf(orderProcesses.toArray(new CompletableFuture[0]));
         allOf.thenRun(() -> {
-            log.info("All orders have been processed. Total processed orders: " + TOTAL_PROCESSED_ORDERS.get());
+            log.info("All orders have been processed. Total processed orders: " + totalProcessedOrders.get());
         }).exceptionally(ex -> {
             log.error("An error occurred while processing orders", ex);
             return null;
