ee5f9d65644369687e817b0b8d860f545d96d54f
Andrey <maandr@internet.ru>
2024-10-25 21:44:40 +0300

    BJS2-38500 WOW

---
+++ src/main/java/school/faang/QuestSystem.java
@@ -0,0 +1,21 @@
+package school.faang;
+
+import java.util.concurrent.CompletableFuture;
+
+public class QuestSystem {
+    public CompletableFuture<Player> startQuest(Player player, Quest quest) {
+        return CompletableFuture.supplyAsync(() -> {
+            long timeInProgress = (long) 1000 * quest.getDifficulty();
+            int questReward = quest.getReward() + player.getExperience();
+
+            try {
+                Thread.sleep(timeInProgress);
+                player.setExperience(questReward);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+
+            return player;
+        });
+    }
+}
