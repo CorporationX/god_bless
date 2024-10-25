2c98168ed9f839ec0edab6d1d03534264cc557f6
abzalkhan00 <abzalkhanondakanov@gmail.com>
2024-10-25 20:38:24 +0500

    refactored processOrder and  processAllOrders methods

--- src/main/java/school/faang/asynchrony/amazon/OrderProcessor.java
+++ src/main/java/school/faang/asynchrony/amazon/OrderProcessor.java
@@ -2,17 +2,18 @@ package school.faang.asynchrony.amazon;
 
 import lombok.Getter;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.atomic.AtomicInteger;
 
 @Getter
 public class OrderProcessor {
-    private AtomicInteger totalProcessedOrders = new AtomicInteger(0);
+    private final AtomicInteger totalProcessedOrders = new AtomicInteger(0);
     private final static int THREAD_SLEEP_TIME = 1000;
 
-    public void processOrder(Order order) {
-        CompletableFuture<Void> future = CompletableFuture.runAsync(() -> {
+    public CompletableFuture<Void> processOrder(Order order) {
+        return CompletableFuture.runAsync(() -> {
             try {
                 Thread.sleep(THREAD_SLEEP_TIME);
             } catch (InterruptedException e) {
@@ -21,13 +22,14 @@ public class OrderProcessor {
             order.setStatus(OrderStatus.PROCESSED);
             totalProcessedOrders.incrementAndGet();
         });
-        future.join();
     }
 
     public void processAllOrders(List<Order> orders) {
-        orders.forEach(this::processOrder);
-        System.out.println("Total processed orders are : " + totalProcessedOrders);
+        List<CompletableFuture<Void>> futures = new ArrayList<>();
+        orders.forEach(order -> futures.add(processOrder(order)));
+
+        CompletableFuture.allOf(futures.toArray(new CompletableFuture[0]))
+                .thenRun(() -> System.out.println("Total processed orders are : " + totalProcessedOrders))
+                .join();
     }
-
-
 }
