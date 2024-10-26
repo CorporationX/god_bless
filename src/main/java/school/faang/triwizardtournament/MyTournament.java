c6000833a8000f850a296a6c39b0f19b22a193b8
Maks Russkikh <15maksim@mail.ru>
2024-10-26 07:44:14 +0300

    save

--- src/main/java/school/faang/triwizardtournament/MyTournament.java
+++ src/main/java/school/faang/triwizardtournament/MyTournament.java
@@ -7,7 +7,7 @@ public class MyTournament {
     public static void main(String[] args) {
         School schoolOne = new School("Hogwards", Arrays.asList(new Student("Evgen", 1978, 500),
                 new Student("Paha", 2005, 345), new Student("Olya", 1995, 300)));
-        School  schoolTwo = new School("Gimnaz", Arrays.asList(new Student("Dima", 1995, 340),
+        School schoolTwo = new School("Gimnaz", Arrays.asList(new Student("Dima", 1995, 340),
                 new Student("Uljyana", 2000, 650), new Student("Lena", 2005, 750)));
         Task taskOne = new Task("Proga", 3, 600);
         Task taskTwo = new Task("Fire", 3, 900);
