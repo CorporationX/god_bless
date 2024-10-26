bfebd6b3fe93bdc71e6e5ff7cbb4fa2747aa27e5
“Andrey <aiegorovceo@yandex.ru>
2024-10-26 14:56:38 +0300

    feat: Create MilitaryBase class for secure message exchange

--- src/main/java/school/faang/fieldbasecomein/EncryptionUtils.java
+++ src/main/java/school/faang/fieldbasecomein/EncryptionUtils.java
@@ -3,7 +3,6 @@ package school.faang.fieldbasecomein;
 import javax.crypto.Cipher;
 import javax.crypto.KeyGenerator;
 import javax.crypto.SecretKey;
-import javax.crypto.spec.SecretKeySpec;
 import java.util.Base64;
 
 public class EncryptionUtils {
