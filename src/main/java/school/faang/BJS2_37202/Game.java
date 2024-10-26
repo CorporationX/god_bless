88aea97d090e79a4b016b05945b498aeaffc1a8d
SerUKalinin <serukalinin@gmail.com>
2024-10-26 17:22:49 +0300

    Removed student's code

--- src/main/java/school/faang/BJS2_37202/Game.java
+++ src/main/java/school/faang/BJS2_37202/Game.java
@@ -1,4 +1,32 @@
 package school.faang.BJS2_37202;
 
 public class Game {
+    private int score = 0;
+    private int lives = 9;
+
+    private final Object lockScore= new Object();
+    private final Object lockLives= new Object();
+
+    public void update(boolean earnedPoint, boolean loseLife) {
+        synchronized (lockScore) {
+            if (earnedPoint) {
+                score++;
+                System.out.println("Очки увеличены, текущий счет: " + score);
+            }
+        }
+        synchronized (lockLives) {
+            if (loseLife) {
+                lives--;
+                System.out.println("Жизни уменьшены, текущее количество жизней: " + lives);
+                if (lives <= 0) {
+                    gameOver();
+                }
+            }
+        }
+    }
+
+    public void gameOver() {
+        System.out.println("Game Over!. Вы проиграли!");
+        System.out.println("Финальный счет: " + score);
+    }
 }
--- src/main/java/school/faang/BJS2_37202/Main.java
+++ src/main/java/school/faang/BJS2_37202/Main.java
@@ -1,4 +1,28 @@
 package school.faang.BJS2_37202;
 
 public class Main {
+    public static void main(String[] args) {
+        Game game = new Game();
+
+        Unit player1 = new Unit("Гарри", 8, game);
+        Unit player2 = new Unit("Том", 5, game);
+
+        for (int i = 0; i < 10; i++) {
+            boolean earnedPoint = Math.random() < 0.5;
+            boolean lostLife = Math.random() < 0.7;
+            if (earnedPoint) {
+                player1.earnPoint();
+            }
+
+            if (lostLife) {
+                player2.loseLife();
+            }
+
+            try {
+                Thread.sleep(100);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
 }
