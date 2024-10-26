131f3cac08b41b0fd84b4b7fd815969b87f4f00a
Irina Krasnova <arisha.17@mail.ru>
2024-10-26 23:36:01 +0300

    доработано

--- src/test/java/facebook/Main.java
+++ src/test/java/facebook/Main.java
@@ -12,33 +12,33 @@ public class Main {
     public static void main(String[] args) {
         PostServis postServis = new PostServis();
         ExecutorService executorService = Executors.newFixedThreadPool(THREAD_COUNT);
-        Post post1 = new Post(1, "Java Multithreading", "This is a post about multithreading in Java.", "Alice");
-        Post post2 = new Post(2, "Introduction to Spring", "This post covers the basics of Spring framework.", "Bob");
-        Post post3 = new Post(3, "Java", "This is a post", "Olya");
-//        String[] titles = {"Java Multithreading", "Introduction to Spring", "Java"};
-//        String[] contents = {
-//                "This is a post about multithreading in Java.",
-//                "This post covers the basics of Spring framework.",
-//                "This is a post"
-//        };
-//        String[] authors = {"Alice", "Bob", "Olya"};
-//        for (int i = 0; i < titles.length; i++) {
-//            Post post = new Post (i + 1, titles[i], contents[i], authors[i]);
-//            CompletableFuture<Void> futurePost = CompletableFuture.runAsync(() -> postServis.addPost(post), executorService);
-//        }
-
-        CompletableFuture<Void> futurePost1 = CompletableFuture.runAsync(() -> postServis.addPost(post1), executorService);
-        CompletableFuture<Void> futurePost2 = CompletableFuture.runAsync(() -> postServis.addPost(post2), executorService);
-        CompletableFuture<Void> futurePost3 = CompletableFuture.runAsync(() -> postServis.addPost(post3), executorService);
-
-        Comment comment1 = new Comment("Great post!", "Tolya");
-        Comment comment2 = new Comment("I found this very helpful.", "Kolya");
-        Comment comment3 = new Comment("Good introduction.", "Nick");
-
-        CompletableFuture<Void> future1Comment1 = CompletableFuture.runAsync(() -> postServis.addComment(1, comment1), executorService);
-        CompletableFuture<Void> future1Comment2 = CompletableFuture.runAsync(() -> postServis.addComment(1, comment2), executorService);
-        CompletableFuture<Void> future2Comment3 = CompletableFuture.runAsync(() -> postServis.addComment(2, comment3), executorService);
-        CompletableFuture<Void> futureShowPost = CompletableFuture.allOf(futurePost1, futurePost2, futurePost3, future1Comment2, future1Comment1, future2Comment3)
+        String[] titles = {"Java Multithreading", "Introduction to Spring", "Java"};
+        String[] contents = {
+                "This is a post about multithreading in Java.",
+                "This post covers he basics of Spring framework.",
+                "This is a post"
+        };
+        String[] authors = {"Alice", "Bob", "Olya"};
+        List<Post> posts = new ArrayList<>();
+        for (int i = 0; i < titles.length; i++) {
+            posts.add(new Post(i + 1, titles[i], contents[i], authors[i]));
+        }
+        List<CompletableFuture<Void>> postFutures = posts.stream()
+                .map(post -> CompletableFuture.runAsync(() -> postServis.addPost(post), executorService))
+                .toList();
+        Comment[] comments = {
+                new Comment("Great post!", "Tolya"),
+                new Comment("I found this very helpful.", "Kolya"),
+                new Comment("Good introduction.", "Nick")
+        };
+        int[] postIds = {1, 1, 2};
+        List<CompletableFuture<Void>> commentFutures = new ArrayList<>();
+        for (int i = 0; i < comments.length; i++) {
+            int postId = postIds[i];
+            int index = i;
+            commentFutures.add(CompletableFuture.runAsync(() -> postServis.addComment(postId, comments[index]), executorService));
+        }
+        CompletableFuture<Void> futureShowPost = CompletableFuture.allOf(commentFutures.toArray(new CompletableFuture[0]))
                 .thenRun(() -> {
                     System.out.println("All posts and comments:");
                     postServis.showPosts();
