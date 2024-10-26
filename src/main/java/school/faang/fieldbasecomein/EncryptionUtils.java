7263110e4590b87626d10ad47e066fee1c6bd357
“Andrey <aiegorovceo@yandex.ru>
2024-10-26 14:38:47 +0300

    feat: Create EncryptionUtils class for AES encryption and decryption

---
+++ src/main/java/school/faang/fieldbasecomein/EncryptionUtils.java
@@ -0,0 +1,37 @@
+package school.faang.fieldbasecomein;
+
+import javax.crypto.Cipher;
+import javax.crypto.KeyGenerator;
+import javax.crypto.SecretKey;
+import javax.crypto.spec.SecretKeySpec;
+import java.util.Base64;
+
+public class EncryptionUtils {
+    private static final String ALGORITHM = "AES";
+    private static final SecretKey KEY = generateKey();
+
+    public static String encrypt(String message) throws Exception {
+        Cipher cipher = Cipher.getInstance(ALGORITHM);
+        cipher.init(Cipher.ENCRYPT_MODE, KEY);
+        byte[] encryptedBytes = cipher.doFinal(message.getBytes());
+        return Base64.getEncoder().encodeToString(encryptedBytes);
+    }
+
+    public static String decrypt(String encryptedMessage) throws Exception {
+        Cipher cipher = Cipher.getInstance(ALGORITHM);
+        cipher.init(Cipher.DECRYPT_MODE, KEY);
+        byte[] decryptedBytes = cipher.doFinal(Base64.getDecoder().decode(encryptedMessage));
+        return new String(decryptedBytes);
+    }
+
+    private static SecretKey generateKey() {
+        try {
+            KeyGenerator keyGenerator = KeyGenerator.getInstance(ALGORITHM);
+            keyGenerator.init(128);
+            return keyGenerator.generateKey();
+        } catch (Exception e) {
+            throw new RuntimeException("Error generating encryption key", e);
+        }
+    }
+}
+
