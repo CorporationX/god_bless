8ec0fccb9ed8eaf9d6f0db57ea27df89f2c40cf0
“Andrey <aiegorovceo@yandex.ru>
2024-10-26 08:38:17 +0300

    feat: add immutable Location class with latitude and longitude fields

---
+++ src/main/java/school/faang/trackingsystem/Location.java
@@ -0,0 +1,12 @@
+package school.faang.trackingsystem;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public final class Location {
+    private final double latitude;
+    private final double longitude;
+}
+
