ec81ca86fa8b4c62b41df163ff2feed4bab21d5f
Matvey <mashptz@gmail.com>
2024-10-26 21:37:21 +0300

    Шаблон

---
+++ src/main/java/school/faang/BJS2_39059/TwitterSubscriptionSystem.java
@@ -0,0 +1,20 @@
+package school.faang.BJS2_39059;
+
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.atomic.AtomicInteger;
+
+public class TwitterSubscriptionSystem {
+    private AtomicInteger totalFollowers;
+
+    public TwitterSubscriptionSystem() {
+        this.totalFollowers = new AtomicInteger(0);
+    }
+
+    public void addFollower(TwitterAccount account){
+
+    }
+
+    public void followAccount(TwitterAccount account){
+
+    }
+}
