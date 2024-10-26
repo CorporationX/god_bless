864b7720bc6ebc71f1a76ca2495afd577e79c986
Irina Krasnova <arisha.17@mail.ru>
2024-10-26 14:11:46 +0300

    исправлено

--- src/test/java/animals/Main.java
+++ src/test/java/animals/Main.java
@@ -1,21 +1,24 @@
 package animals;
 
+import java.util.Random;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.TimeUnit;
 
 public class Main {
     private static final int TREAD_COUNT = 2;
+    private static Random random = new Random();
 
     public static void main(String[] args) {
-        Donation donation1 = new Donation(1, 34.5);
-        Donation donation2 = new Donation(2, 21.4);
-        Donation donation3 = new Donation(3, 78.6);
         Organization organization = new Organization();
         ExecutorService executorService = Executors.newFixedThreadPool(TREAD_COUNT);
-        executorService.submit(() -> organization.addDonation(donation1));
-        executorService.submit(() -> organization.addDonation(donation2));
-        executorService.submit(() -> organization.addDonation(donation3));
+        for (int i = 0; i < TREAD_COUNT; i++) {
+            for (int j = 1; j < 4; j++) {
+                Donation donation = new Donation(i, random.nextInt(100));
+                executorService.submit(() -> organization.addDonation(donation));
+            }
+        }
+
         executorService.shutdown();
         try {
             if (executorService.awaitTermination(30, TimeUnit.SECONDS)) {
