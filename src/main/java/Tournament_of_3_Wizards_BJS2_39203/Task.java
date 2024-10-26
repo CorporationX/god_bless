b9cde2cc85e8158153cbfed3b4792762ac42d0c1
IvanMatiko <matiko1993@gmail.com>
2024-10-26 18:07:34 +0200

    Made task 39203 Tournament of 2 wizards

---
+++ src/main/java/Tournament_of_3_Wizards_BJS2_39203/Task.java
@@ -0,0 +1,12 @@
+package Tournament_of_3_Wizards_BJS2_39203;
+
+import lombok.Getter;
+import lombok.AllArgsConstructor;
+
+@Getter
+@AllArgsConstructor
+public class Task {
+    private String name;
+    private int difficulty;
+    private int reward;
+}
