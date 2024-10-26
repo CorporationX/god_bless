5da9a9ef21c16cf4484be042123a87c3228c716d
bogda <bogdanskrip404@gmail.com>
2024-10-26 22:06:12 +0500

    доработал

--- src/main/java/school/faang/threeWizards/Tournament.java
+++ src/main/java/school/faang/threeWizards/Tournament.java
@@ -3,13 +3,18 @@ package school.faang.threeWizards;
 import lombok.extern.slf4j.Slf4j;
 
 import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 @Slf4j
 public class Tournament {
+    final int THREAD_POOL = 5;
+    ExecutorService executor = Executors.newFixedThreadPool(THREAD_POOL);
     public CompletableFuture<School> startTask(School school, Task task) {
         return CompletableFuture.supplyAsync(() -> {
             try {
                 Thread.sleep(task.getDifficulty() * 1000L);
+                executor.shutdown();
             } catch (InterruptedException e) {
                 log.error("Начало задания было прервано.");
                 throw new RuntimeException(e);
