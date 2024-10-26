3d5adcbedc317eb00d168ac6e66c7dadf47e6827
Vakarash <max.vackarash@gmail.com>
2024-10-26 13:24:02 +0300

    "Поддержим животных fixed"

--- src/main/java/school/faang/helpAnimals/Main.java
+++ src/main/java/school/faang/helpAnimals/Main.java
@@ -4,8 +4,10 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
 import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.atomic.AtomicInteger;
 
 public class Main {
+    private static final AtomicInteger donationId = new AtomicInteger(0);
     private static final int NUMBER_OF_DONATIONS = 100;
     private static final Random random = new Random();
 
@@ -13,10 +15,10 @@ public class Main {
         Organization organization = new Organization();
         List<CompletableFuture<Void>> futures = new ArrayList<>();
         for (int i = 0; i < NUMBER_OF_DONATIONS; i++) {
-            int copyI = i;
             futures.add(CompletableFuture.runAsync(() -> {
-                organization.addDonation(new Donation(copyI, random.nextDouble(10.0, 1000.0)));
-                System.out.println("Donation " + copyI + " added. Balance: " + organization.getBalance());
+                int id = donationId.getAndIncrement();
+                organization.addDonation(new Donation(id, random.nextDouble(10.0, 1000.0)));
+                System.out.println("Donation " + id + " added. Balance: " + organization.getBalance());
             }));
         }
         CompletableFuture.allOf(futures.toArray(new CompletableFuture[0]))
