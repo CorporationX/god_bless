f05ce669328d8a9b4a38d589de10abf1a1c13ce2
Igor <asachiyigor@gmail.com>
2024-10-26 18:32:48 +0300

    BJS2-38310
    refactoring code

--- src/main/java/school/faangSprint4/t07/Battle.java
+++ src/main/java/school/faangSprint4/t07/Battle.java
@@ -6,22 +6,25 @@ import java.util.concurrent.Future;
 
 public class Battle {
     private final ExecutorService executor;
+    private static final int THREADS = 3;
+    private static final int SLEEP_TIME = 1000;
+    private static final double PERCENTS = 0.5;
 
     public Battle() {
-        this.executor = Executors.newFixedThreadPool(3);
+        this.executor = Executors.newFixedThreadPool(THREADS);
     }
 
     public Future<Robot> fight(Robot robot1, Robot robot2) {
         return executor.submit(() -> {
             int power1 = robot1.attackPower() + robot1.defensePower();
             int power2 = robot2.attackPower() + robot2.defensePower();
-            Thread.sleep(1000);
+            Thread.sleep(SLEEP_TIME);
             if (power1 > power2) {
                 return robot1;
             } else if (power2 > power1) {
                 return robot2;
             } else {
-                return Math.random() > 0.5 ? robot1 : robot2;
+                return Math.random() > PERCENTS ? robot1 : robot2;
             }
         });
     }
