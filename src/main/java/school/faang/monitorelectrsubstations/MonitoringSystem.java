8428557830612e9caa3f14268e3c56d9dbfd895e
“Andrey <aiegorovceo@yandex.ru>
2024-10-26 08:03:54 +0300

    feat: Update MonitoringSystem to manage data from substations

--- src/main/java/school/faang/monitorelectrsubstations/MonitoringSystem.java
+++ src/main/java/school/faang/monitorelectrsubstations/MonitoringSystem.java
@@ -1,17 +1,16 @@
 package school.faang.monitorelectrsubstations;
 
+import lombok.Getter;
 import java.util.concurrent.ConcurrentHashMap;
 
+@Getter
 public class MonitoringSystem {
-    private final ConcurrentHashMap<Integer, Double> substationData;
-
-    public MonitoringSystem() {
-        this.substationData = new ConcurrentHashMap<>();
-    }
+    private final ConcurrentHashMap<Integer, Double> substationData = new ConcurrentHashMap<>();
 
     public void updateData(int substationId, double averageData) {
         substationData.put(substationId, averageData);
         System.out.println("Substation " + substationId + " average: " + averageData);
     }
 }
+
 
