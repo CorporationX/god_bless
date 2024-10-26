9229cb95e9957892763c77b1fb8a6530ed16c348
ZhubanyshZh <zubanyszarylkasyn@gmail.com>
2024-10-26 08:36:31 +0500

    fix(BJS2-39081): twitter subscribe system
    fixed some moments

--- src/main/java/org/example/service/twitterSubscirbe/TwitterSubscriptionSystem.java
+++ src/main/java/org/example/service/twitterSubscirbe/TwitterSubscriptionSystem.java
@@ -13,17 +13,12 @@ import java.util.concurrent.Executors;
 public class TwitterSubscriptionSystem {
     private final ExecutorService executorService = Executors.newFixedThreadPool(5);
 
-    public synchronized void addFollower(TwitterAccount account) {
+    public void addFollower(TwitterAccount account) {
         account.getFollowers().incrementAndGet();
     }
 
     public CompletableFuture<Void> followAccount(TwitterAccount account) {
         return CompletableFuture.runAsync(() -> {
-            try {
-                Thread.sleep(2000);
-            } catch (InterruptedException e) {
-                log.error(e.getMessage(), e);
-            }
             addFollower(account);
         }, executorService);
     }
