5762eb63568df6652906f5c9d6e37b03c4a81db9
Aleksandr Kuznetsov <adeskmath@gmail.com>
2024-10-26 19:42:41 +0300

    BJS2-38665

---
+++ src/main/java/school/faang/async/alchemist/Potion.java
@@ -0,0 +1,13 @@
+package school.faang.async.alchemist;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@AllArgsConstructor
+@Getter
+public class Potion {
+
+  private String name;
+  private int requiredIngredients;
+
+}
