6fe470f605a6f7910fd1f830e7af920c5737d2c5
Dmitry Shakhaev <dmtry.shahaev@gmail.com>
2024-10-26 21:49:49 +0300

    Задача: Fan out – Fan In. Исправление синтаксиса

--- src/main/java/school/faang/fanoutfanin/FanOutFanInSystem.java
+++ src/main/java/school/faang/fanoutfanin/FanOutFanInSystem.java
@@ -4,7 +4,6 @@ import lombok.extern.slf4j.Slf4j;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Random;
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
