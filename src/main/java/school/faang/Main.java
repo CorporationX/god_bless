9748703bb100cf917f5bf21662525f900743db60
Doniyor Kurbanov <doniyor.kurbanov.24@gmail.com>
2024-10-26 21:15:35 +0500

    Все настроил

--- src/main/java/school/faang/Main.java
+++ src/main/java/school/faang/Main.java
@@ -1,15 +1,4 @@
 package school.faang;
 
-import school.faang.reverse.Example;
-
 public class Main {
-    public static void main(String[] args) {
-        Example example = new Example();
-        int[] array = {1, 2, 3, 4, 5};
-        example.reverse(array);
-
-        for (int num : array) {
-            System.out.print(num + " ");
-        }
-    }
 }
