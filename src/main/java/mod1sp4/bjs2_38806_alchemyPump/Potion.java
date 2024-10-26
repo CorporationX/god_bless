2c5c32529e5599c71e2f1a0c6f55c4910b88e660
Denis Levkhov <levkhovde@yandex.ru>
2024-10-26 17:53:20 +0300

    BJS2-38806 Alchemy pump: adding executorService, adding logs & refactoring

--- src/main/java/mod1sp4/bjs2_38806_alchemyPump/Potion.java
+++ src/main/java/mod1sp4/bjs2_38806_alchemyPump/Potion.java
@@ -6,6 +6,9 @@ import lombok.extern.slf4j.Slf4j;
 
 import java.util.Random;
 import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
 
 @Slf4j
 @Data
@@ -13,19 +16,32 @@ import java.util.concurrent.CompletableFuture;
 public class Potion {
     private String name;
     private int requiredIngredients;
+    private static final Random RANDOMIZER = new Random();
+    private static final ExecutorService executor = Executors.newCachedThreadPool();
 
     public CompletableFuture<Integer> gatherIngredients(Potion potion) {
         return CompletableFuture.supplyAsync(() -> {
-            long gatheringTimeoutByAmountIngredients = new Random().nextLong(potion.getRequiredIngredients());
+            long gatheringTimeoutByAmountIngredients = RANDOMIZER.nextLong(potion.getRequiredIngredients());
             try {
                 Thread.sleep(gatheringTimeoutByAmountIngredients);
             } catch (InterruptedException e) {
                 log.error("\nThread was interrupted while he was sleeping");
                 throw new IllegalStateException(e);
             }
-            System.out.printf("\nIngredients for %s was gather", potion.getName());
+            log.info("\nIngredients for {} was gather", potion.getName());
             return potion.getRequiredIngredients();
-        });
+        }, executor);
     }
 
+    public static void shuttingDownExecutor() {
+        executor.shutdown();
+        try {
+            if (executor.awaitTermination(2, TimeUnit.MINUTES)) {
+                executor.shutdownNow();
+            }
+        } catch (InterruptedException e) {
+            log.error("Error shutting down executor: {}", e.getMessage());
+            throw new IllegalStateException(e);
+        }
+    }
 }
--- src/main/java/mod1sp4/bjs2_38806_alchemyPump/PotionGathering.java
+++ src/main/java/mod1sp4/bjs2_38806_alchemyPump/PotionGathering.java
@@ -1,9 +1,12 @@
 package mod1sp4.bjs2_38806_alchemyPump;
 
+import lombok.extern.slf4j.Slf4j;
+
 import java.util.List;
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.atomic.AtomicInteger;
 
+@Slf4j
 public class PotionGathering {
 
     private static final AtomicInteger ALL_GATHERING_INGREDIENTS = new AtomicInteger(0);
@@ -15,6 +18,7 @@ public class PotionGathering {
                 new Potion("Stamina Potion", 4)
         );
         gatherAllIngredients(potions);
+        Potion.shuttingDownExecutor();
     }
 
     public static void gatherAllIngredients(List<Potion> potions) {
@@ -23,6 +27,6 @@ public class PotionGathering {
                 .toList();
 
         CompletableFuture.allOf(completableFutureElementsInt.toArray(new CompletableFuture[0])).join();
-        System.out.printf("\nОбщее количество собранных ингредиентов: %d", ALL_GATHERING_INGREDIENTS.get());
+        log.info("\nОбщее количество собранных ингредиентов: {}", ALL_GATHERING_INGREDIENTS.get());
     }
 }
