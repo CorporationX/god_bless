109552e40bcc7d28383950d246de8c2c67fb7e08
Amanyaz Bayramdurdyyev <amanyaz9090@gmail.com>
2024-10-25 22:00:27 -0500

    Thread

---
+++ src/main/java/school/faang/thread4/Food.java
@@ -0,0 +1,10 @@
+package school.faang.thread4;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class Food {
+    private final String foodDescription;
+}
