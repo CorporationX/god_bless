50d3b7800d029482cf9ed922f7238db14912dee2
Andrew Balashov <balashov83@gmail.com>
2024-10-26 20:25:53 +0300

    BJS2-38347 Pi: Task done

---
+++ src/main/java/school/faang/m1s4/bjs2_38347_pi/Main.java
@@ -0,0 +1,9 @@
+package school.faang.m1s4.bjs2_38347_pi;
+
+public class Main {
+    public static void main(String[] args) {
+        Calculator calculator = new Calculator();
+
+        calculator.calculatePi();
+    }
+}
