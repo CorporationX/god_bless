de45a8d016a673d996806355511ee05838c1fb0c
kitamuro <leeteso97@gmail.com>
2024-10-26 23:23:04 +0500

    Отрефакторил

--- src/main/java/school/faang/BJS238927/Main.java
+++ src/main/java/school/faang/BJS238927/Main.java
@@ -1,19 +1,18 @@
 package school.faang.BJS238927;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.concurrent.CompletableFuture;
 
 public class Main {
     public static void main(String[] args) {
         TwitterAccount account = new TwitterAccount("Elon Musk", 0);
         TwitterSubscriptionSystem subscriptionSystem = new TwitterSubscriptionSystem();
-
-        CompletableFuture<Void> future1 = subscriptionSystem.followAccount(account);
-        CompletableFuture<Void> future2 = subscriptionSystem.followAccount(account);
-        CompletableFuture<Void> future3 = subscriptionSystem.followAccount(account);
-        CompletableFuture<Void> future4 = subscriptionSystem.followAccount(account);
-        CompletableFuture<Void> future5 = subscriptionSystem.followAccount(account);
-
-        CompletableFuture<Void> allFutures = CompletableFuture.allOf(future1, future2, future3, future4, future5)
+        List<CompletableFuture<Void>> futures = new ArrayList<>();
+        for (int i = 0; i < 6; i++) {
+            futures.add(subscriptionSystem.followAccount(account));
+        }
+        CompletableFuture<Void> allFutures = CompletableFuture.allOf(futures.toArray(new CompletableFuture[0]))
                 .thenRun(() -> System.out.println("Amount of followers: " + account.getUsername() + " " + account.getFollowers()));
         allFutures.join();
     }
