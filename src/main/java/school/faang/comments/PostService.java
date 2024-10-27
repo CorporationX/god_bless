1d4d70c0254cc1e823fe5e8e23f33e4fd0b055a5
heckfy86 <marlinco263@gmail.com>
2024-10-27 17:23:42 +0100

    BJS2-39334 completed

---
+++ src/main/java/school/faang/comments/PostService.java
@@ -0,0 +1,66 @@
+package school.faang.comments;
+
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.concurrent.locks.ReentrantLock;
+
+public class PostService {
+
+    private final ReentrantLock lock = new ReentrantLock();
+    private final Map<Integer, Post> posts = new LinkedHashMap<>();
+
+
+    public void addPost(Post post) {
+        lock.lock();
+        try {
+            posts.put(post.id(), post);
+        } finally {
+            lock.unlock();
+        }
+    }
+
+    public void addComment(int postId, Comment comment) {
+        lock.lock();
+        try {
+            posts.put(postId, posts.get(postId).updateComment(comment));
+        } finally {
+            lock.unlock();
+        }
+    }
+
+    public Post readPost(int idPost) {
+        return posts.get(idPost);
+    }
+
+    public List<Comment> readComments(int idPost) {
+        if (posts.containsKey(idPost)) {
+            return posts.get(idPost).listComments();
+        }
+        return null;
+    }
+
+    public Post deletePost(int idPost) {
+        lock.lock();
+        Post deletedPost = null;
+        try {
+            if (posts.containsKey(idPost))
+                deletedPost = posts.remove(idPost);
+        } finally {
+            lock.unlock();
+        }
+        return deletedPost;
+    }
+
+    public void deleteComment(int idPost, String author) {
+        lock.lock();
+        try {
+            posts.get(idPost)
+                    .listComments().removeIf(comment1 -> comment1.author().equals(author));
+        } catch (IndexOutOfBoundsException e) {
+            System.out.println("Post with ID " + idPost + " not found.");
+        } finally {
+            lock.unlock();
+        }
+    }
+}
