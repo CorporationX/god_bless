304fe132e8ce05faf0a696b763e7b81a2a503a98
Doniyor Kurbanov <doniyor.kurbanov.24@gmail.com>
2024-10-27 23:59:42 +0500

    BJS2-38853. Пошлите ворона. Рассылаем уведомления в Facebook

--- src/main/java/school/faang/BJS238853/NotificationManager.java
+++ src/main/java/school/faang/BJS238853/NotificationManager.java
@@ -5,11 +5,15 @@ import lombok.extern.slf4j.Slf4j;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 import java.util.concurrent.TimeUnit;
 
 @Slf4j
 public class NotificationManager {
     private final List<Notification> notificationList = new ArrayList<>();
+    private static final int THREADS = 3;
+    private final ExecutorService executorService = Executors.newFixedThreadPool(THREADS);
 
     public synchronized void addNotification(Notification notification) {
         notificationList.add(notification);
@@ -20,11 +24,11 @@ public class NotificationManager {
             try {
                 TimeUnit.SECONDS.sleep(3);
             } catch (InterruptedException e) {
-               log.error("Проблема с добавлением уведомления {}", e.getMessage());
+               log.error("Проблема с добавлением уведомления ", e);
             }
             Notification notification = new Notification(id, message);
             addNotification(notification);
-        });
+        },executorService);
     }
 
     public void printNotifications() {
