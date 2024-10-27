3ca96ffc1db2307050cdf7c9d3ac256cb10abd8e
Doniyor Kurbanov <doniyor.kurbanov.24@gmail.com>
2024-10-28 00:16:39 +0500

    BJS2-39151. Carry отожрался

--- src/main/java/school/faang/BJS239151/Inventory.java
+++ src/main/java/school/faang/BJS239151/Inventory.java
@@ -5,21 +5,30 @@ import lombok.extern.slf4j.Slf4j;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 @Slf4j
 public class Inventory {
     private List<Item> itemList = new ArrayList<>();
+    private static final int THREADS = 2;
+    private final ExecutorService executorService = Executors.newFixedThreadPool(THREADS);
 
     public void addItem(Item item) {
         itemList.add(item);
     }
 
-    public CompletableFuture<Item> combineItems(Item item1, Item item2) {
-        return CompletableFuture.supplyAsync(() -> {
+    public CompletableFuture<Void> combineAndAddItems() {
+        CompletableFuture<Item> chestItemFuture = getItemFromChest();
+        CompletableFuture<Item> shopItemFuture = getItemFromShop();
+
+        CompletableFuture<Item> combinedItemFuture = chestItemFuture.thenCombine(shopItemFuture, (item1, item2) -> {
             Item combinedItem = new Item(item1.getName() + "-" + item2.getName(), item1.getPower() + item2.getPower());
             System.out.println("Скомбинированный предмет: " + combinedItem.getName());
             return combinedItem;
         });
+
+        return combinedItemFuture.thenCompose(this::addItemToInventoryAsync);
     }
 
     public CompletableFuture<Item> getItemFromChest() {
@@ -27,10 +36,10 @@ public class Inventory {
             try {
                 Thread.sleep(1000);
             } catch (InterruptedException e) {
-                log.error("Что-то пошло не так {}", e.getMessage());
+                log.error("Что-то пошло не так ", e);
             }
             return new Item("Chest Item", 10);
-        });
+        },executorService);
     }
 
     public CompletableFuture<Item> getItemFromShop() {
@@ -38,10 +47,10 @@ public class Inventory {
             try {
                 Thread.sleep(1000);
             } catch (InterruptedException e) {
-                log.error("Что-то пошло не так {}", e.getMessage());
+                log.error("Что-то пошло не так ", e);
             }
             return new Item("Shop Item", 20);
-        });
+        }, executorService);
     }
 
     public CompletableFuture<Void> addItemToInventoryAsync(Item item) {
