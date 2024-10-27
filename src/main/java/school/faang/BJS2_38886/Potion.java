5a443dfbff3fda93df4a8ee69a6f790c7b815f8e
SerUKalinin <serukalinin@gmail.com>
2024-10-27 10:40:22 +0300

    Removed student's code

---
+++ src/main/java/school/faang/BJS2_38886/Potion.java
@@ -0,0 +1,11 @@
+package school.faang.BJS2_38886;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@AllArgsConstructor
+@Getter
+public class Potion {
+    private String name;
+    private final int requiredIngredient;
+}
