71262d4aec2a923c7e8321643e4bb8a696daf642
Nikolay Orlenko <norlenko2801@gmail.com>
2024-10-27 23:27:04 +0300

    Правки к ревью

--- src/main/java/school/faang/leave/comments/Main.java
+++ src/main/java/school/faang/leave/comments/Main.java
@@ -7,14 +7,14 @@ import java.util.concurrent.ThreadLocalRandom;
 import java.util.stream.IntStream;
 
 public class Main {
+    public static final int NUM_OF_POSTS = 3;
     private static final int NUM_OF_USERS = 5;
     private static final int MAX_NUM_OF_COMMENTS = 100;
 
     public static void main(String[] args) {
-        Post cats = new Post(1, "Котики", "Много котиков", "kokin");
-        Post dogs = new Post(2, "Собаки", "Много собак", "kostya");
-        Post eat = new Post(3, "Еда", "Много еды", "nazagold");
-        List<Post> posts = List.of(cats, dogs, eat);
+        List<Post> posts = IntStream.rangeClosed(1, NUM_OF_POSTS)
+                .mapToObj(i -> new Post(i, "Post" + i, "content" + i, "user" + i))
+                .toList();
 
         PostService postService = new PostService();
         List<CompletableFuture<Void>> futures = posts.stream()
@@ -40,17 +40,18 @@ public class Main {
                 .thenRun(() -> System.out.println("Все комменты добавлены!"))
                 .join();
 
-        System.out.println("Получаем комменты под постом про котиков...");
-        List<Comment> comments = postService.getComments(cats.getId());
+        Post post = posts.get(0);
+        System.out.println("Получаем комменты под первым постом...");
+        List<Comment> comments = postService.getComments(post.getId());
         comments.forEach(System.out::println);
 
         System.out.println("Удаляем первый комментарий под постом с котиками.");
         Comment firstComment = comments.get(0);
-        postService.removeComment(cats.getId(), firstComment.id(), firstComment.author());
-        postService.getComments(cats.getId()).forEach(System.out::println);
+        postService.removeComment(post.getId(), firstComment.id(), firstComment.author());
+        postService.getComments(post.getId()).forEach(System.out::println);
 
         System.out.println("Удаляем пост про котиков");
-        postService.removePost(cats.getId(), cats.getAuthor());
+        postService.removePost(post.getId(), post.getAuthor());
         postService.getPosts().forEach(System.out::println);
     }
 }
