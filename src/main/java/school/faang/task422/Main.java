fc234be901959f279f68dbe3b2b2b4c5aecefb05
Svyatoslav Kostrov <kost-2014@mail.ru>
2024-10-27 15:52:33 +0300

    Reworked

--- src/main/java/school/faang/task422/Main.java
+++ src/main/java/school/faang/task422/Main.java
@@ -13,12 +13,15 @@ import java.util.concurrent.Executors;
 import java.util.concurrent.TimeUnit;
 
 public class Main {
+    private static final int THREAD_COUNT = 10;
+    private static final int ONE_MINUTES = 1;
+
     public static void main(String[] args) {
         ExecutorService service = Executors.newFixedThreadPool(6);
         Organization organization = new Organization();
         List<CompletableFuture<Void>> futures = new ArrayList<>();
 
-        for (int i = 0; i < 10; i++) {
+        for (int i = 0; i < THREAD_COUNT; i++) {
             int finalI = i;
             futures.add(CompletableFuture.runAsync(() -> {
                 Donation donation = new Donation(finalI, new BigDecimal(new Random().nextDouble(99)));
@@ -32,7 +35,7 @@ public class Main {
 
         service.shutdown();
         try {
-            if (!service.awaitTermination(1, TimeUnit.MINUTES)) {
+            if (!service.awaitTermination(ONE_MINUTES, TimeUnit.MINUTES)) {
                 System.out.println("Ошибка, деньги утеряны");
                 service.shutdownNow();
             }
