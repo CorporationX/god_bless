3ca28e56c4d61fa8f5627eef2f30a9d69aeb5265
Aleksi Basilashvili <lucheidobra@mail.ru>
2024-10-27 13:47:15 +0300

    Рассылаем уведомления в Facebook
    BJS2-38831
    Внес изменения

--- src/main/java/school/faang/sendingfbnotifications/Main.java
+++ src/main/java/school/faang/sendingfbnotifications/Main.java
@@ -9,17 +9,15 @@ public class Main {
         NotificationManager manager = new NotificationManager();
         List<CompletableFuture<Void>> futures = new ArrayList<>();
 
-        // Создаем и добавляем уведомления асинхронно
         for (int i = 1; i <= 10; i++) {
             CompletableFuture<Void> future = manager.fetchNotification(i, "Уведомление #" + i);
             futures.add(future);
         }
 
-        // Ожидание завершения всех асинхронных задач
         CompletableFuture.allOf(futures.toArray(new CompletableFuture[0])).join();
 
-        // Вывод всех уведомлений
         System.out.println("\nВсе уведомления:");
         manager.displayNotifications();
+        manager.shutdown();
     }
 }
--- src/main/java/school/faang/sendingfbnotifications/NotificationManager.java
+++ src/main/java/school/faang/sendingfbnotifications/NotificationManager.java
@@ -3,11 +3,14 @@ package school.faang.sendingfbnotifications;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 import java.util.concurrent.ThreadLocalRandom;
 
 public class NotificationManager {
 
-    List<Notification> notifications = new ArrayList<Notification>();
+    private final List<Notification> notifications = new ArrayList<>();
+    private final ExecutorService executorService = Executors.newFixedThreadPool(4);
 
     public synchronized void addNotification(Notification notification) {
         notifications.add(notification);
@@ -19,14 +22,14 @@ public class NotificationManager {
                 Thread.sleep(ThreadLocalRandom.current().nextInt(500, 1500));
             } catch (InterruptedException e) {
                 Thread.currentThread().interrupt();
-                e.printStackTrace();
+                throw new IllegalStateException("Поток был прерван во время выполнения задачи", e);
             }
 
             Notification notification = new Notification(id, message);
 
             addNotification(notification);
             System.out.println("Уведомление добавлено: " + notification.getMessage());
-        });
+        }, executorService);
     }
 
     public void displayNotifications() {
@@ -35,5 +38,9 @@ public class NotificationManager {
                     System.out.println("ID: " + notification.getId() + ", Message: " + notification.getMessage())
             );
         }
+    }
+
+    public void shutdown() {
+        executorService.shutdown();
     }
 }
