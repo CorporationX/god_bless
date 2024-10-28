1b06c3bb3d48b090ce191a0d0cdfa08dd0498201
Vakarash <max.vackarash@gmail.com>
2024-10-28 20:54:49 +0300

    "Работа стоит fixed 2"

--- src/main/java/school/faang/workSuspended/Deadlock/MarketingDepartment.java
+++ src/main/java/school/faang/workSuspended/Deadlock/MarketingDepartment.java
@@ -3,28 +3,33 @@ package school.faang.workSuspended.Deadlock;
 import lombok.AllArgsConstructor;
 
 import java.util.List;
+import java.util.concurrent.CountDownLatch;
 
 @AllArgsConstructor
-public class MarketingDepartment implements Runnable{
+public class MarketingDepartment implements Runnable {
     private final MarketingResources marketingResources;
     private final DesignResources designResources;
+    private final CountDownLatch designLatch;
+    private final CountDownLatch marketingLatch;
 
     @Override
     public void run() {
-        synchronized (designResources) {
-            System.out.println("Marketing Department is reading design files...");
-            List<String> designFiles = designResources.getDesignFiles();
-            try {
-                Thread.sleep(100);
-            } catch (InterruptedException e) {
-                throw new IllegalStateException("Task interrupted", e);
-            }
-            synchronized (marketingResources) {
-                System.out.println("Marketing Department is adding design files to marketing files...");
-                for (String file : designFiles) {
-                    marketingResources.addFile(file);
+        try {
+            designLatch.await();
+            synchronized (designResources) {
+                System.out.println("Marketing Department is reading design files...");
+                List<String> designFiles = designResources.getDesignFiles();
+                synchronized (marketingResources) {
+                    System.out.println("Marketing Department is adding design files to marketing files...");
+                    for (String file : designFiles) {
+                        marketingResources.addFile(file);
+                    }
                 }
             }
+            marketingLatch.countDown();
+        } catch (Exception e) {
+            e.printStackTrace();
+            throw new IllegalStateException("Marketing Department is interrupted", e);
         }
     }
 }
