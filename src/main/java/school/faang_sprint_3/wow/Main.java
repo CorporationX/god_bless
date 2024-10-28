14b960cfbbb4aba86e9a8ad26c3189e13d205ed4
Home Alone <yevgengeodakyan@gmail.com>
2024-10-27 22:12:57 -0400

    race condition fixe

--- src/main/java/school/faang_sprint_3/wow/Main.java
+++ src/main/java/school/faang_sprint_3/wow/Main.java
@@ -10,12 +10,18 @@ public class Main {
         var quest1 = new Quest("Defeat the Lich King", Difficulty.HARD, 150);
         var quest2 = new Quest("Retrieve the sword of Azeroth", Difficulty.MEDIUM, 100);
 
-        var player1Quest = questSystem.startQuest(player1, quest1);
-        var player2Quest = questSystem.startQuest(player2, quest2);
+        var player1Quest1 = questSystem.startQuest(player1, quest1);
+        var player2Quest1 = questSystem.startQuest(player2, quest1);
+        var player1Quest2 = questSystem.startQuest(player1, quest2);
+        var player2Quest2 = questSystem.startQuest(player2, quest2);
 
-        player1Quest.thenAccept(player -> System.out.println(player.getName() + " has completed the quest and now has "
+        player1Quest1.thenAccept(player -> System.out.println(player.getName() + " has completed the quest and now has "
                 + player.getExperience() + " experience points."));
-        player2Quest.thenAccept(player -> System.out.println(player.getName() + " has completed the quest and now has "
+        player2Quest1.thenAccept(player -> System.out.println(player.getName() + " has completed the quest and now has "
+                + player.getExperience() + " experience points."));
+        player1Quest2.thenAccept(player -> System.out.println(player.getName() + " has completed the quest and now has "
+                + player.getExperience() + " experience points."));
+        player2Quest2.thenAccept(player -> System.out.println(player.getName() + " has completed the quest and now has "
                 + player.getExperience() + " experience points."));
     }
 }
--- src/main/java/school/faang_sprint_3/wow/QuestSystem.java
+++ src/main/java/school/faang_sprint_3/wow/QuestSystem.java
@@ -3,19 +3,21 @@ package school.faang_sprint_3.wow;
 import lombok.extern.slf4j.Slf4j;
 
 import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
 @Slf4j
 public class QuestSystem {
+    ExecutorService threadPool = Executors.newFixedThreadPool(5);
+
     public CompletableFuture<Player> startQuest(Player player, Quest quest) {
-        var threadPool = Executors.newFixedThreadPool(5);
-        var futurePlayer = CompletableFuture.supplyAsync(() -> {
+        return CompletableFuture.supplyAsync(() -> {
             timeForQuest(player, quest, quest.difficulty());
-            player.addExp(quest.reward());
+            synchronized (player) {
+                player.addExp(quest.reward());
+            }
             return player;
         }, threadPool);
-        threadPool.shutdown();
-        return futurePlayer;
     }
 
     private void timeForQuest(Player player, Quest quest, Difficulty difficulty) {
@@ -38,5 +40,6 @@ public class QuestSystem {
             log.error("An error occurred while waiting for quest to be completed", e);
             throw new IllegalStateException(e);
         }
+        threadPool.shutdown();
     }
 }
