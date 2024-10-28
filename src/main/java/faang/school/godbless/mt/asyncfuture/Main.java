b627b847940f29fc19ea163c6ef49947e9a85cc9
Ivan Semikov <semikov.exf@gmail.com>
2024-10-28 20:35:37 +0300

    BJS2-38096 fixes

--- src/main/java/faang/school/godbless/mt/asyncfuture/Main.java
+++ src/main/java/faang/school/godbless/mt/asyncfuture/Main.java
@@ -1,33 +1,8 @@
 package faang.school.godbless.mt.asyncfuture;
 
-import java.util.concurrent.CompletableFuture;
-import java.util.concurrent.ExecutionException;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
-import java.util.concurrent.Future;
-
 public class Main {
     public static void main(String[] args) {
-        doAll();
-    }
-
-    public static void doAll() {
-        try {
-            ExecutorService executor = Executors.newSingleThreadExecutor();
-            Future<Integer> paymentFuture = executor.submit(MasterCardService::collectPayment);
-            executor.shutdown();
-
-            CompletableFuture<Integer> analyticsFuture = CompletableFuture.supplyAsync(MasterCardService::sendAnalytics);
-
-            Integer analyticsResult = analyticsFuture.join();
-            System.out.println("Аналитика отправлена: " + analyticsResult);
-
-            Integer paymentResult = paymentFuture.get();
-            System.out.println("Платеж выполнен: " + paymentResult);
-
-        } catch (InterruptedException | ExecutionException e) {
-            e.printStackTrace();
-            throw new RuntimeException(e);
-        }
+        MasterCardService service = new MasterCardService();
+        service.doAll();
     }
 }
--- src/main/java/faang/school/godbless/mt/asyncfuture/MasterCardService.java
+++ src/main/java/faang/school/godbless/mt/asyncfuture/MasterCardService.java
@@ -1,19 +1,27 @@
 package faang.school.godbless.mt.asyncfuture;
 
+import lombok.extern.slf4j.Slf4j;
+
 import java.util.concurrent.CompletableFuture;
-import java.util.concurrent.ExecutionException;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.Future;
 
+@Slf4j
 public class MasterCardService {
-    static int collectPayment() {
+    private static final int THREAD_COUNT = 10;
+    private final ExecutorService executor;
+
+    public MasterCardService() {
+        this.executor = Executors.newFixedThreadPool(THREAD_COUNT);
+    }
+
+    public int collectPayment() {
         try {
             Thread.sleep(10_000);
             return 10_000;
         } catch (InterruptedException e) {
-            e.printStackTrace();
-            throw new RuntimeException(e);
+            throw new IllegalStateException("Thread interrupted", e);
         }
     }
 
@@ -22,8 +30,24 @@ public class MasterCardService {
             Thread.sleep(1_000);
             return 1_000;
         } catch (InterruptedException e) {
-            e.printStackTrace();
-            throw new RuntimeException(e);
+            throw new IllegalStateException("Thread interrupted", e);
         }
     }
+
+    public void doAll() {
+        try {
+            Future<Integer> paymentFuture = executor.submit(this::collectPayment);
+            CompletableFuture<Integer> analyticsFuture = CompletableFuture.supplyAsync(MasterCardService::sendAnalytics, executor);
+
+            Integer analyticsResult = analyticsFuture.join();
+            log.info("Analytics sent: {}", analyticsResult);
+
+            Integer paymentResult = paymentFuture.get();
+            log.info("Payment collected: {}", paymentResult);
+
+        } catch (Exception e) {
+            log.error("Execution service exception", e);
+        }
+    }
+
 }
