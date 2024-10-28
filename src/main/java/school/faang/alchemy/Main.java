eb036778846e9715c7a52f5969b2eacf5829a81c
heckfy86 <marlinco263@gmail.com>
2024-10-28 18:50:40 +0100

    BJS2-39103 fixed comments

--- src/main/java/school/faang/alchemy/Main.java
+++ src/main/java/school/faang/alchemy/Main.java
@@ -2,8 +2,10 @@ package school.faang.alchemy;
 
 import java.util.List;
 import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.atomic.AtomicInteger;
 
 public class Main {
+    private static final AtomicInteger totalIngredients = new AtomicInteger(0);
     public static void main(String[] args) {
         List<Potion> potions = List.of(
                 new Potion("Healing Potion", 5),
@@ -13,17 +15,25 @@ public class Main {
         gatherAllIngredients(potions);
     }
 
-    public static void gatherAllIngredients(List<Potion> potions) {
+    private static void gatherAllIngredients(List<Potion> potions) {
         List<CompletableFuture<Integer>> completableFutureList = potions
                 .stream()
-                .map(potion -> CompletableFuture.supplyAsync(() -> potion.gatherIngredients(potion)))
+                .map(potion -> CompletableFuture.supplyAsync(() -> gatherIngredients(potion)))
                 .toList();
         CompletableFuture<Void> allFutures =
                 CompletableFuture.allOf(completableFutureList.toArray(new CompletableFuture[0]));
 
         allFutures.thenRun(() -> {
-            completableFutureList.forEach(future -> Potion.totalIngredients.addAndGet(future.join()));
-            System.out.println("Total amount of ingredients: " + Potion.totalIngredients);
+            completableFutureList.forEach(future -> totalIngredients.addAndGet(future.join()));
+            System.out.println("Total amount of ingredients: " + totalIngredients);
         }).join();
+    }
+    private static int gatherIngredients(Potion potion) {
+        try {
+            Thread.sleep(1000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        return potion.requiredIngredients();
     }
 }
--- src/main/java/school/faang/alchemy/Potion.java
+++ src/main/java/school/faang/alchemy/Potion.java
@@ -4,14 +4,4 @@ import java.util.concurrent.atomic.AtomicInteger;
 
 public record Potion(String name,
                      int requiredIngredients) {
-    public static final AtomicInteger totalIngredients = new AtomicInteger(0);
-
-    public int gatherIngredients(Potion potion) {
-        try {
-            Thread.sleep(1000);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
-        return potion.requiredIngredients;
-    }
 }
