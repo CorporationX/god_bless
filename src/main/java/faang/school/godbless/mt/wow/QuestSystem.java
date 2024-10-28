83fe60fff787ff0619e725e31109b11951786392
Ivan Semikov <semikov.exf@gmail.com>
2024-10-28 20:39:16 +0300

    BJS2-38663 fixes

--- src/main/java/faang/school/godbless/mt/wow/QuestSystem.java
+++ src/main/java/faang/school/godbless/mt/wow/QuestSystem.java
@@ -3,10 +3,15 @@ package faang.school.godbless.mt.wow;
 import lombok.extern.slf4j.Slf4j;
 
 import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 @Slf4j
 public class QuestSystem {
+    private static final int THREAD_COUNT = 10;
     private static final long TIME_FOR_DIFFICULT = 10000;
+
+    private final ExecutorService executor = Executors.newFixedThreadPool(THREAD_COUNT);
     public CompletableFuture<Player> startQuest(Player player, Quest quest) {
         return CompletableFuture.supplyAsync(() -> {
             try {
@@ -14,10 +19,9 @@ public class QuestSystem {
                 player.addExperience(quest.getReward());
                 return player;
             } catch (InterruptedException e) {
-                Thread.currentThread().interrupt();
                 log.error("Thread interrupted", e);
-                throw new RuntimeException(e);
+                throw new IllegalStateException("Thread interrupted", e);
             }
-        });
+        }, executor);
     }
 }
