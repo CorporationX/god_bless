b13f596947798b4f7da6d93bcf5754ef2b5418cd
infani <abrormukimov@gmail.com>
2024-10-28 09:32:15 +0500

    Update Mastercard Service class

--- src/main/java/MultithreadingAsyncFuture/MasterCardService.java
+++ src/main/java/MultithreadingAsyncFuture/MasterCardService.java
@@ -1,45 +1,47 @@
 package MultithreadingAsyncFuture;
 
+import lombok.extern.slf4j.Slf4j;
+
 import java.util.concurrent.*;
 
+@Slf4j
 public class MasterCardService {
     private static final int THREAD_POOL_SIZE = 2;
+    private static final int TIMER = 10000;
+    private static final int MIN_TIMER = 1000;
 
     public static int collectPayment() {
         try {
-            Thread.sleep(10000);
-            return 10000;
+            Thread.sleep(TIMER);
         } catch (InterruptedException e) {
-            e.printStackTrace();
-            throw new RuntimeException(e);
+            log.debug(e.getMessage());
         }
+        return 10000;
     }
 
     public static int sendAnalytics() {
         try {
-            Thread.sleep(1000);
-            return 1000;
+            Thread.sleep(MIN_TIMER);
         } catch (InterruptedException e) {
-            e.printStackTrace();
-            throw new RuntimeException();
+            log.debug(e.getMessage());
         }
+        return 1000;
     }
 
     public void doAll() {
         ExecutorService ex = Executors.newFixedThreadPool(THREAD_POOL_SIZE);
         Future<Integer> futureAnalyticsAmount = ex.submit(() -> sendAnalytics());
+        CompletableFuture<Integer> futurePaymentAmount = CompletableFuture.supplyAsync(() -> collectPayment());
+
         try {
             futureAnalyticsAmount.get();
             if (futureAnalyticsAmount.isDone()) {
                 System.out.println(futureAnalyticsAmount.get());
             }
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        } catch (ExecutionException e) {
-            e.printStackTrace();
+        } catch (InterruptedException | ExecutionException e) {
+            log.debug(e.getMessage());
         }
 
-        CompletableFuture<Integer> futurePaymentAmount = CompletableFuture.supplyAsync(() -> collectPayment());
         int futurePaymentResult = futurePaymentAmount.join();
         System.out.println(futurePaymentResult);
 
