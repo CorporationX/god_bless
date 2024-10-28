fcae645fe7c0e94bce33486e85ac50723fdfd1d6
AlexLogino <alexeyjob3@gmail.com>
2024-10-28 11:43:16 +0300

    BJS2-37826 fix naming

--- src/main/java/school/faang/sprint_3/microsoft/MailSender.java
+++ src/main/java/school/faang/sprint_3/microsoft/MailSender.java
@@ -3,23 +3,23 @@ package school.faang.sprint_3.microsoft;
 public class MailSender {
 
     public static void main(String[] args) throws InterruptedException {
-        Thread thread_1 = new Thread(new SenderRunnable(1, 200));
-        Thread thread_2 = new Thread(new SenderRunnable(201, 400));
-        Thread thread_3 = new Thread(new SenderRunnable(401, 600));
-        Thread thread_4 = new Thread(new SenderRunnable(601, 800));
-        Thread thread_5 = new Thread(new SenderRunnable(801, 1000));
+        Thread firstThread = new Thread(new SenderRunnable(1, 200));
+        Thread secondThread = new Thread(new SenderRunnable(201, 400));
+        Thread thirdThread = new Thread(new SenderRunnable(401, 600));
+        Thread fourthThread = new Thread(new SenderRunnable(601, 800));
+        Thread fifthThread = new Thread(new SenderRunnable(801, 1000));
 
-        thread_1.start();
-        thread_2.start();
-        thread_3.start();
-        thread_4.start();
-        thread_5.start();
+        firstThread.start();
+        secondThread.start();
+        thirdThread.start();
+        fourthThread.start();
+        fifthThread.start();
 
-        thread_1.join();
-        thread_2.join();
-        thread_3.join();
-        thread_4.join();
-        thread_5.join();
+        firstThread.join();
+        secondThread.join();
+        thirdThread.join();
+        fourthThread.join();
+        fifthThread.join();
 
         System.out.println("Отправка писем завершена");
     }
