ec3dd13d773844243193fdfb2679a8b5391e9b74
Irina Krasnova <arisha.17@mail.ru>
2024-11-01 09:45:23 +0300

    доработано

--- src/test/java/stop_work/DesignResources.java
+++ src/test/java/stop_work/DesignResources.java
@@ -6,32 +6,9 @@ import java.util.concurrent.locks.Lock;
 import java.util.concurrent.locks.ReadWriteLock;
 import java.util.concurrent.locks.ReentrantReadWriteLock;
 
-public class DesignResources {
-    private final List<String> dFilesNames;
-    private final ReadWriteLock lock = new ReentrantReadWriteLock();
-    private final Lock readLock = lock.readLock();
-    private final Lock writeLock = lock.writeLock();
-
-    public DesignResources() {
-        this.dFilesNames = new ArrayList<>();
-    }
-
-    public void printFilesNames() {
-        readLock.lock();
-        try {
-            System.out.println("DesignResources files: " + dFilesNames);
-        } finally {
-            readLock.unlock();
-        }
-    }
-
-    public void addFilesNames(String fileName) {
-        writeLock.lock();
-        try {
-            dFilesNames.add(fileName);
-            System.out.println("Added file to DesignResources " + fileName);
-        } finally {
-            writeLock.unlock();
-        }
+public class DesignResources extends ResourseManager {
+    @Override
+    public String getResourceType() {
+        return "DesignResources";
     }
 }
--- src/test/java/stop_work/MarketingResources.java
+++ src/test/java/stop_work/MarketingResources.java
@@ -6,32 +6,9 @@ import java.util.concurrent.locks.Lock;
 import java.util.concurrent.locks.ReadWriteLock;
 import java.util.concurrent.locks.ReentrantReadWriteLock;
 
-public class MarketingResources {
-    private List<String> mFilesNames;
-    private final ReadWriteLock lock = new ReentrantReadWriteLock();
-    private final Lock readLock = lock.readLock();
-    private final Lock writeLock = lock.writeLock();
-
-    public MarketingResources() {
-        this.mFilesNames = new ArrayList<>();
-    }
-
-    public void printFilesNames() {
-        readLock.lock();
-        try {
-            System.out.println("MarcetingResourses files: " + mFilesNames);
-        } finally {
-            readLock.unlock();
-        }
-    }
-
-    public void addFilesNames(String fileName) {
-        writeLock.lock();
-        try {
-            mFilesNames.add(fileName);
-            System.out.println("Added file to DesignResources: " + fileName);
-        } finally {
-            writeLock.unlock();
-        }
+public class MarketingResources extends ResourseManager {
+    @Override
+    public String getResourceType() {
+        return "MarketingResources";
     }
 }
