af21674ba86bfc7f8d8ee7837eb7e2b073f25435
Maria Sahno <sahnomyu@m2.ru>
2024-11-05 01:49:08 +0500

    test commit

--- src/main/java/school/faang/Main.java
+++ src/main/java/school/faang/Main.java
@@ -1,4 +1,8 @@
 package school.faang;
 
 public class Main {
+    public static void main(String[] args) {
+        int i=123;
+System.out.println(""+i);
+    }
 }
---
+++ src/test/java/UnitTest.java
@@ -0,0 +1,11 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class UnitTest {
+    @Test
+    public void test(){
+assertEquals(1, 2)
+        ;
+    }
+}
