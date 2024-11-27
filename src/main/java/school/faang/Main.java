d2fbc5049fbba1901e7028007cb55813cfaf3a8f
Maria Sahno <sahnomyu@m2.ru>
2024-11-28 00:05:01 +0500

    add hello world

--- src/main/java/school/faang/Main.java
+++ src/main/java/school/faang/Main.java
@@ -1,4 +1,12 @@
 package school.faang;
 
 public class Main {
+    public static void main(String[] args){
+  System.out.println("Hello, world");
+    }
+    private void My_emptyFunction(){
+int VAR =100;
+        for (int i= 0; i< VAR; i++){ System.out.println("number is "+i);
+   }
+}
 }
