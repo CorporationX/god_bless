282cf241b5bfe352b8b2ac8da8013a6ac346951e
Maria Sahno <sahnomyu@m2.ru>
2024-11-28 00:07:57 +0500

    add sum method

--- src/main/java/school/faang/Main.java
+++ src/main/java/school/faang/Main.java
@@ -1,4 +1,11 @@
 package school.faang;
 
 public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello, world!");
+    }
+
+    public static int sum(int a1, int a2) {
+        return a1 + a2;
+    }
 }
---
+++ src/test/java/school/faang/SumTest.java
@@ -0,0 +1,14 @@
+package school.faang;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class SumTest {
+
+    @Test
+    public void sumTest() {
+        int res = Main.sum(5, 2);
+        assertEquals(8, res);
+    }
+}
