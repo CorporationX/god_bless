8c08a45df8819905dcb33557dc3d9164bda8e983
Anatoly Kushnarev <makartih@Anatolys-MacBook-Pro.local>
2024-12-02 01:08:37 +0200

    the BJS2-43597 is completed

---
+++ src/main/java/school/faang/BJS243597/Example.java
@@ -0,0 +1,18 @@
+package school.faang.BJS243597;
+
+public class Example {
+
+    public static void reverse(int [] inputArray) {
+        int [] arrayWithReversedNumber = new int [inputArray.length];
+
+        int counter = 0;
+        for (int i = inputArray.length - 1; i >= 0; i--) {
+            arrayWithReversedNumber[counter] = inputArray[i];
+            counter++;
+        }
+
+        for (int i = 0; i < arrayWithReversedNumber.length; i++){
+            System.out.print(arrayWithReversedNumber[i] + " ");
+        }
+    }
+}
---
+++ src/main/java/school/faang/BJS243597/Main.java
@@ -0,0 +1,10 @@
+package school.faang.BJS243597;
+
+public class Main {
+    public static void main(String[] args) {
+
+        int [] arrayToReverse = {1,2,3,4,5};
+
+        Example.reverse(arrayToReverse);
+    }
+}
