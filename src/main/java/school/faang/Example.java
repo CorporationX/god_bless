0fd1c4ceecfc14a7477cae27676dfd91be1fbe88
rostex <rostex2@yandex.ru>
2024-12-01 20:37:48 +0300

    Add Example.java with reverse int array method

---
+++ src/main/java/school/faang/Example.java
@@ -0,0 +1,13 @@
+package school.faang;
+
+public class Example {
+    private static int[] reverse(int[] nums) {
+        for (int i = 0; i < nums.length / 2; i++) {
+            int temp = nums[i];
+            nums[i] = nums[nums.length - 1 - i];
+            nums[nums.length - 1 - i] = temp;
+        }
+        return nums;
+    }
+}
+
