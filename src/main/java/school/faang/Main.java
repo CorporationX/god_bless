90acc2e314e3518f6ddcfca5e1a9e9f0beb44ec6
maks <fhbfds@fb>
2024-12-01 22:51:12 +0300

    Revers array 3.0

--- src/main/java/school/faang/Main.java
+++ src/main/java/school/faang/Main.java
@@ -7,7 +7,7 @@ import static school.faang.Example.reverse;
 public class Main {
     public static void main(String[] args) {
 
-        int[] nums = {1,2,3,4,5};
+        int[] nums = {1, 2, 3, 4, 5};
 
         System.out.println("Old nums is: " + Arrays.toString(nums));
         reverse(nums);
