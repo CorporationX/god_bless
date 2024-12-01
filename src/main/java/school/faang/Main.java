83cbaa678937e97c5e190ebe6c3de4473ac88768
rafissabitov <rafsabqa@gmail.com>
2024-12-01 13:21:39 -0600

    Ловим события

--- src/main/java/school/faang/Main.java
+++ src/main/java/school/faang/Main.java
@@ -1,4 +1,20 @@
 package school.faang;
 
+import school.faang.catch_event.HogwartsSpells;
+
 public class Main {
+    public static void main(String[] args) {
+        HogwartsSpells hogwartsSpells = new HogwartsSpells();
+        hogwartsSpells.addSpellEvent(1, "magic", "fireball");
+        hogwartsSpells.addSpellEvent(2, "magic", "frieze");
+        hogwartsSpells.printAllSpellEvents();
+
+        System.out.println(hogwartsSpells.getSpellEventById(1));
+
+        System.out.println(hogwartsSpells.getSpellEventsByType("magic"));
+
+        hogwartsSpells.deleteSpellEvent(2);
+
+        hogwartsSpells.printAllSpellEvents();
+    }
 }
---
+++ src/main/java/school/faang/catch_event/HogwartsSpells.java
@@ -0,0 +1,48 @@
+package school.faang.catch_event;
+
+import java.util.*;
+
+public class HogwartsSpells {
+    private final Map<Integer, SpellEvent> spellById = new HashMap<>();
+    private final Map<String, List<SpellEvent>> spellsByType = new HashMap<>();
+    private List<SpellEvent> spellEventList = new ArrayList<>();
+
+    public void addSpellEvent(int id, String eventType, String actionDescription) {
+        if (id < 0 && eventType == null && eventType.isBlank() && actionDescription == null && actionDescription.isBlank()) {
+            throw new IllegalArgumentException();
+        }
+        SpellEvent spellEvent = new SpellEvent(id, eventType, actionDescription);
+        spellById.put(id, spellEvent);
+        spellEventList.add(spellEvent);
+
+        spellsByType.put(eventType, spellEventList);
+    }
+
+    public SpellEvent getSpellEventById(int id) {
+        if (id < 0) {
+            throw new IllegalArgumentException();
+        }
+        return spellById.get(id);
+    }
+
+    public List<SpellEvent> getSpellEventsByType(String eventType) {
+        if (eventType.isBlank() && eventType == null) {
+            throw new IllegalArgumentException();
+        }
+        return spellsByType.get(eventType);
+    }
+
+    public void deleteSpellEvent(int id) {
+        if (id < 0) {
+            throw new IllegalArgumentException();
+        }
+        spellById.remove(id);
+    }
+
+    public void printAllSpellEvents() {
+        for (Map.Entry<Integer, SpellEvent> entry : spellById.entrySet()) {
+            System.out.println("Spell Event: id " + entry.getValue().getId() + " type " + entry.getValue().getEventType()
+                    + " action " + entry.getValue().getAction());
+        }
+    }
+}
