22c4a3616498ad9e0ebe9c2c2bbd7f8642894d54
Maria Bortsova <bortsovamariia@gmail.com>
2024-12-01 22:00:52 +0300

    add create user

--- src/main/java/school/faang/Main.java
+++ src/main/java/school/faang/Main.java
@@ -1,4 +1,30 @@
 package school.faang;
 
 public class Main {
+    public static void main(String[] args) {
+        createUser();
+//        createUserWithWrongAge();
+//        createUserInvalidJob();
+//        createUserInvalidAddress();
+    }
+
+    private static void createUser() {
+        User user = new User("Vasya", 19, "Uber", "New York");
+        System.out.println(user);
+    }
+
+    private static void createUserWithWrongAge() {
+        User user = new User("Peter", 17, "Google", "New York");
+        System.out.println(user);
+    }
+
+    private static void createUserInvalidJob() {
+        User user = new User("Vasya", 19, "Yandex", "New York");
+        System.out.println(user);
+    }
+
+    private static void createUserInvalidAddress() {
+        User user = new User("Vasya", 19, "Google", "Moscow");
+        System.out.println(user);
+    }
 }
