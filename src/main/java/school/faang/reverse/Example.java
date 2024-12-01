a629e26d8a091e6b0914e7fec4338ebba8022cff
lehaps <awda.96@bk.ru>
2024-12-01 21:15:16 +0300

    сделал проверку на пустой массив

--- src/main/java/school/faang/reverse/Example.java
+++ src/main/java/school/faang/reverse/Example.java
@@ -1,12 +1,21 @@
 package school.faang.reverse;
 
+import java.util.Arrays;
+
 public class Example {
 
     public static void reverse(int[] nums) {
-        for (int i = 0; i < nums.length / 2; i++) {
-            int temp = nums[i];
-            nums[i] = nums[nums.length - i - 1];
-            nums[nums.length - i - 1] = temp;
+        if (nums == null || nums.length == 0) {
+            System.out.println("Массив пуст");
+        } else {
+            for (int i = 0; i < nums.length / 2; i++) {
+                int temp = nums[i];
+                nums[i] = nums[nums.length - i - 1];
+                nums[nums.length - i - 1] = temp;
+            }
+
+            System.out.println("nums = "
+                    + Arrays.toString(nums));
         }
     }
 }
--- src/main/java/school/faang/reverse/Main.java
+++ src/main/java/school/faang/reverse/Main.java
@@ -1,14 +1,9 @@
 package school.faang.reverse;
 
-import java.util.Arrays;
-
 public class Main {
     public static void main(String[] args) {
         int[] nums = new int[]{1, 2, 3, 4, 5};
 
         Example.reverse(nums);
-
-        System.out.println("nums = "
-                + Arrays.toString(nums));
     }
 }
