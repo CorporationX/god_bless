5a14aca6bc20d55e687b9c768e09863673b32e35
salikhdev <salikhdev@gmail.com>
2024-12-02 00:50:30 +0500

    Task number : BJS2_43610 Added Class Reverse array

---
+++ src/main/java/school/faang/BJS2_43610/Main.java
@@ -0,0 +1,18 @@
+package school.faang.BJS2_43610;
+
+
+import java.util.Arrays;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Reverse reverse = new Reverse();
+
+        int[] nums = {1, 2, 3, 4};
+
+        int[] result = reverse.reverse(nums);
+        System.out.println(Arrays.toString(result));
+
+    }
+}
---
+++ src/main/java/school/faang/BJS2_43610/Reverse.java
@@ -0,0 +1,21 @@
+package school.faang.BJS2_43610;
+
+public class Reverse {
+
+    public int[] reverse(int[] nums) {
+
+        int indexFirst = 0;
+        int indexLast = nums.length - 1;
+
+        for (int i = 0; i < nums.length / 2; i++) {
+            int temp = nums[indexFirst];
+            nums[indexFirst] = nums[indexLast];
+            nums[indexLast] = temp;
+            indexFirst++;
+            indexLast--;
+        }
+
+        return nums;
+    }
+
+}
