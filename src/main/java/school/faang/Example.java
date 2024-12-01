5a1cb87dd09799a13f990b1062f85018058cea65
maks <fhbfds@fb>
2024-12-01 20:22:10 +0300

    Revers array

--- src/main/java/school/faang/Example.java
+++ src/main/java/school/faang/Example.java
@@ -1,10 +1,40 @@
 package school.faang;
 
+import java.util.Arrays;
+
+
 public class Example {
+    public static void main(String[] args) {
+
+        int[] nums = {1, 2, 3, 4, 5};
+
+        System.out.println("Old nums is: " + Arrays.toString(nums));
+        reverse(nums);
+        System.out.println("Reverse nums is: " + Arrays.toString(nums));
+
+    }
+
 
     public static void reverse(int[] nums) {
-        for (int i = 0; i < nums.length; i++) {
-            nums[i] = nums.length - i;
+        int left = 0;
+        int right = nums.length - 1;
+        int temp;
+
+        while (left < right) {
+
+            temp = nums[left];
+            nums[left] = nums[right];
+            nums[right] = temp;
+
+            left++;
+            right--;
         }
+
     }
+
 }
+
+
+
+
+
