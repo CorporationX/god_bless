f4296772804277aff9754f6fc925d63af51855e2
rostex <rostex2@yandex.ru>
2024-12-01 21:17:52 +0300

    Update Example.java
    Make void reverse method
    Add empty array check
    Add main method as entry point

--- src/main/java/school/faang/Example.java
+++ src/main/java/school/faang/Example.java
@@ -1,13 +1,21 @@
 package school.faang;
 
+import java.util.Arrays;
+
 public class Example {
-    private static int[] reverse(int[] nums) {
+    public static void main(String[] args) {
+        int[] nums = new int[]{1, 2, 3, 4, 5,};
+        reverse(nums);
+        System.out.println(Arrays.toString(nums));
+
+    }
+    private static void reverse(int[] nums) {
+        if (nums.length == 0) return;
         for (int i = 0; i < nums.length / 2; i++) {
             int temp = nums[i];
             nums[i] = nums[nums.length - 1 - i];
             nums[nums.length - 1 - i] = temp;
         }
-        return nums;
     }
 }
 
