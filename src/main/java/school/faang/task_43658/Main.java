9b0f7b6b1b9acb6330f594f7728bce10cd21c037
rostex <rostex2@yandex.ru>
2024-12-01 23:33:10 +0300

    Create User.java
    Add User class with validation rules and exception message

---
+++ src/main/java/school/faang/task_43658/Main.java
@@ -0,0 +1,7 @@
+package school.faang.task_43658;
+
+public class Main {
+    public static void main(String[] args) {
+        User user = new User("Alex", 18, "Google", "London");
+    }
+}
---
+++ src/main/java/school/faang/task_43658/User.java
@@ -0,0 +1,52 @@
+package school.faang.task_43658;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class User {
+    private static final List<String> VALID_JOBS = new ArrayList<>(Arrays.asList("Google", "Uber", "Amazon"));
+    private static final List<String> VALID_ADDRESSES = new ArrayList<>(Arrays.asList(
+            "London", "New York", "Amsterdam"));
+    private static final Integer VALID_AGE = 18;
+
+    private String name;
+    private int age;
+    private String job;
+    private String address;
+
+    public User(String name, int age, String job, String address) {
+        setName(name);
+        setAge(age);
+        setJob(job);
+        setAddress(address);
+    }
+
+    public void setName(String name) {
+        if (name == null) {
+            throw new IllegalArgumentException("Имя не может быть пустым");
+        }
+        this.name = name;
+    }
+
+    public void setAge(int age) {
+        if (!(age >= VALID_AGE)) {
+            throw new IllegalArgumentException(String.format("Возраст не может быть меньше %d", VALID_AGE));
+        }
+        this.age = age;
+    }
+
+    public void setJob(String job) {
+        if (!VALID_JOBS.contains(job)) {
+            throw new IllegalArgumentException(String.format("Недопустимое место работы. Доступно : %s", VALID_JOBS));
+        }
+        this.job = job;
+    }
+
+    public void setAddress(String address) {
+        if (!VALID_ADDRESSES.contains(address)) {
+            throw new IllegalArgumentException(String.format("Недопустимый адрес. Доступно : %s", VALID_ADDRESSES));
+        }
+        this.address = address;
+    }
+}
