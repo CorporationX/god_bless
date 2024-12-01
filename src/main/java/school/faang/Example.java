a43ebffadaf316355034de2600709a997db0893b
Anatoly Kushnarev <makartih@Anatolys-MacBook-Pro.local>
2024-12-01 19:17:51 +0200

    the BJS2-43597 is completed

--- src/main/java/school/faang/Example.java
+++ src/main/java/school/faang/Example.java
@@ -1,11 +1,12 @@
 package school.faang;
 
 import java.util.ArrayList;
+import java.util.List;
 
 public class Example {
 
-    public static ArrayList<Integer> reverse(ArrayList<Integer> inputArray) {
-        ArrayList<Integer> arrayWithReversedNumber = new ArrayList<>();
+    public static List<Integer> reverse(List<Integer> inputArray) {
+        List<Integer> arrayWithReversedNumber = new ArrayList<>();
 
         for (int i = inputArray.size() - 1; i >= 0; i--) {
             arrayWithReversedNumber.add(inputArray.get(i));
--- src/main/java/school/faang/Main.java
+++ src/main/java/school/faang/Main.java
@@ -2,12 +2,13 @@ package school.faang;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
-        ArrayList<Integer> listToReverse = new ArrayList<>(Arrays.asList(1,2,3,4,5,6,7,8,9,10));
+        List<Integer> listToReverse = new ArrayList<>(Arrays.asList(1,2,3,4,5,6,7,8,9,10));
 
-        ArrayList<Integer> reversedList = Example.reverse(listToReverse);
+        List<Integer> reversedList = Example.reverse(listToReverse);
 
         System.out.println(" the result is :: " + reversedList.toString());
     }
