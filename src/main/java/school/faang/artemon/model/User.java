9adfd32271ab32ef960e3a8b0f11a93544b3bd8c
Artemon <artemon.kos@yandex.ru>
2024-12-01 17:19:07 +0100

    Error: eckstyle исправил v.2

--- src/main/java/school/faang/artemon/model/User.java
+++ src/main/java/school/faang/artemon/model/User.java
@@ -7,14 +7,14 @@ import java.util.stream.Collectors;
 
 /**
  * \Создать класс User с полями:
- *     name — имя пользователя;
- *     age — возраст пользователя;
- *     workplace — место работы пользователя;
- *     address — адрес пользователя.
+ * name — имя пользователя;
+ * age — возраст пользователя;
+ * workplace — место работы пользователя;
+ * address — адрес пользователя.
  */
 public class User {
-    private String name;
-    private int age;
+    private final String name;
+    private final int age;
     private String workplace;
     private String address;
 
@@ -23,20 +23,20 @@ public class User {
         this.age = age;
     }
 
-    public int getAge() {
-        return age;
-    }
-
     /**
      * Добавить в класс User статический метод groupUsers(List‹User›), который:
-     *     Принимает список объектов класса User.
-     *     Возвращает Map‹Integer, List‹User››, где: ключ — возраст пользователя; значение — список всех пользователей
-     *     из исходного списка с этим возрастом.
+     * Принимает список объектов класса User.
+     * Возвращает Map‹Integer, List‹User››, где: ключ — возраст пользователя; значение — список всех пользователей
+     * из исходного списка с этим возрастом.
      */
-    public static Map<Integer, List<User>> groupUsers(List<User> users){
+    public static Map<Integer, List<User>> groupUsers(List<User> users) {
         Map<Integer, List<User>> result;
         result = users.stream().collect(Collectors.groupingBy(User::getAge));
         return result;
+    }
+
+    public int getAge() {
+        return age;
     }
 
     @Override
