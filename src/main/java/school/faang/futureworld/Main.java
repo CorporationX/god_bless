5ed3c7c116a9692e2c860f877e2c2cd608ef1bdb
lehaps <awda.96@bk.ru>
2024-12-01 23:44:52 +0300

    изменил стиль кода

--- src/main/java/school/faang/futureworld/Main.java
+++ src/main/java/school/faang/futureworld/Main.java
@@ -2,14 +2,15 @@ package school.faang.futureworld;
 
 public class Main {
     public static void main(String[] args) {
-        try{
+        try {
             User user1 = new User("Alexey", 23, "Google", "Amsterdam");
             System.out.println("Пользователь создан: " + user1);
             User user2 = new User("Bobby", 25, "Facebook", "Russia");
             System.out.println("Пользователь создан: " + user2);
             User user3 = new User("Bobby", 26, null, "New York");
             System.out.println("Пользователь создан: " + user3);
-        }catch (IllegalArgumentException e){
+
+        } catch (IllegalArgumentException e) {
             System.out.println(e.getMessage());
         }
 
