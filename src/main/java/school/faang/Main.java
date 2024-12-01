13ab9f21d3d48a32b9961a949550b8da75e639a7
ivans <ivan_semenov@bk.ru>
2024-12-02 00:30:54 +0300

    BJS2-43609 [Развернись!] Исправление замечаний: - убран неоптимальный метод - метод reverse преобразован в void - создал свой класс Main для задачи

--- src/main/java/school/faang/Main.java
+++ src/main/java/school/faang/Main.java
@@ -1,13 +1,5 @@
 package school.faang;
 
-import school.faang.moduleone.turnaround.Example;
-
-import java.util.Arrays;
-
 public class Main {
-    public static void main(String[] args) {
-        int[] source = {3, 10, 15, 8, 12};
-        System.out.println(Arrays.toString(Example.reverse(source)));
-        System.out.println(Arrays.toString(Example.reverseWithoutAdditionalArray(source)));
-    }
+
 }
---
+++ src/main/java/school/faang/moduleone/task_43500/Main.java
@@ -0,0 +1,11 @@
+package school.faang.moduleone.task_43500;
+
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) {
+        int[] source = {3, 10, 15, 8, 12};
+        Example.reverse(source);
+        System.out.println(Arrays.toString(source));
+    }
+}
