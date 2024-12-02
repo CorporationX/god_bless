3feadf04235ab076cc7279b04ff3b1f42f3546d2
Mihail Svistunov <egzermr@gmail.com>
2024-12-02 20:50:23 +0800

    BJS2-43660 done

--- src/main/java/school/faang/task_43660/User.java
+++ src/main/java/school/faang/task_43660/User.java
@@ -10,16 +10,18 @@ import java.util.Set;
 public class User {
     static final Set<String> VALID_JOBS = new HashSet<>(Arrays.asList("Google", "Uber", "Amazon"));
     static final Set<String> VALID_ADDRESSES = new HashSet<>(Arrays.asList("London", "New York", "Amsterdam"));
+
     private String name;
     private int age;
     private String job;
     private String address;
+    static final int ADULTHOOD = 18;
 
     public User(String name, int age, String job, String address) throws IllegalArgumentException {
         if (name.isEmpty()) {
             throw new IllegalArgumentException("name is empty");
         }
-        if (age < 18) {
+        if (age < ADULTHOOD) {
             throw new IllegalArgumentException("Must be over 18 years of age");
         }
         if (!VALID_JOBS.contains(job)) {
