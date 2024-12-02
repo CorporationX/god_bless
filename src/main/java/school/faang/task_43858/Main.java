f4b281bc0910056a3d85ce463b84d17484b613a3
Sergey Senin <04sersen@gmail.com>
2024-12-03 00:08:21 +0300

    Добавил группировку пользователей по возрасту с помощью HashMap.

--- src/main/java/school/faang/task_43858/Main.java
+++ src/main/java/school/faang/task_43858/Main.java
@@ -6,11 +6,11 @@ import java.util.List;
 public class Main {
     public static void main(String[] args) {
         List<User> users = new ArrayList<>();
-        users.add(new User("A", 25, "", ""));
-        users.add(new User("B", 30, "", ""));
-        users.add(new User("C", 30, "", ""));
-        users.add(new User("D", 30, "", ""));
-        users.add(new User("E", 25, "", ""));
+        users.add(new User("A", 25, "A", "A"));
+        users.add(new User("B", 30, "B", "B"));
+        users.add(new User("C", 30, "C", "C"));
+        users.add(new User("D", 30, "D", "D"));
+        users.add(new User("E", 25, "E", "E"));
 
         User.groupUsers(users).forEach((age, userList) -> {
             System.out.println("Age: " + age);
