418672b9c0d151e305322ba0a4c155dc52e9ee76
TFdevQA <46944859+TFdevQA@users.noreply.github.com>
2024-12-01 20:47:47 -0800

    edit code style

--- src/main/java/school/faang/m1bjs243617/Example.java
+++ src/main/java/school/faang/m1bjs243617/Example.java
@@ -2,13 +2,13 @@ package school.faang.m1bjs243617;
 
 public class Example {
 
-    public static void reverse (int[] array){
+    public static void reverse(int[] array) {
         if ( array.length != 0 ) {
-           for (int i = 0; i < array.length/2; i++) {
-               int temp = array[i];
-               array[i] = array[array.length - i - 1];
-               array[array.length - i - 1] = temp;
-           }
+            for (int i = 0; i < array.length / 2; i++) {
+                int temp = array[i];
+                array[i] = array[array.length - i - 1];
+                array[array.length - i - 1] = temp;
+            }
         }
     }
 }
--- src/main/java/school/faang/m1bjs243617/Main.java
+++ src/main/java/school/faang/m1bjs243617/Main.java
@@ -3,9 +3,8 @@ package school.faang.m1bjs243617;
 public class Main {
     public static void main(String[] args) {
         int[] array = new int[]{0, 1, 2, 3, 4, 5};
-        Example.reverse(array);
 
-        for (int i = 0; i < array.length; i++)
-        System.out.print(array[i] + ", ");
+        Example.reverse(array);
+        for (int element : array) System.out.print(element + ", ");
     }
 }
