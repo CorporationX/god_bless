fa67dd06056738aa8ef9ed601c5f0358895f8903
KsushaTarasova <ksutaras543@mail.ru>
2024-12-02 09:48:28 +0300

    fix checkstyle error

--- src/main/java/school/faang/task_43501/User.java
+++ src/main/java/school/faang/task_43501/User.java
@@ -7,9 +7,9 @@ public class User {
     private int age;
     private String job;
     private String address;
-    private final static int VALID_AGE = 18;
-    private final static Set<String> VALID_JOBS = Set.of("Google", "Uber", "Amazon");
-    private final static Set<String> VALID_ADDRESSES = Set.of("London", "New York", "Amsterdam");
+    private static final int VALID_AGE = 18;
+    private static final Set<String> VALID_JOBS = Set.of("Google", "Uber", "Amazon");
+    private static final Set<String> VALID_ADDRESSES = Set.of("London", "New York", "Amsterdam");
 
     public User(String name, int age, String job, String address) {
         setName(name);
