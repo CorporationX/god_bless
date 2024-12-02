c31ffff279344e994b017421fb74b39348763529
Eugene <@evgeniy_kim>
2024-12-02 14:05:02 +0500

    task BJS2-43621, evgeniy_kim1993

---
+++ src/main/java/school/faang/BJS2_43621/Main.java
@@ -0,0 +1,22 @@
+package school.faang.BJS2_43621;
+
+public class Main {
+    public static void main(String[] args) {
+        var main = new Main();
+        int[] nums = {1, 2, 3, 4, 5};
+        main.reverse(nums);
+    }
+
+    public void reverse(int[] nums) {
+        int firstIndex = 0;
+        int lastIndex = nums.length - 1;
+        while (firstIndex < lastIndex) {
+            int value = nums[firstIndex];
+            nums[firstIndex] = nums[lastIndex];
+            nums[lastIndex] = value;
+
+            firstIndex++;
+            lastIndex--;
+        }
+    }
+}
