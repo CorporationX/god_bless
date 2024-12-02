04c5da45c2d39b81fc35e95793f8e5f7745bfe67
EugeneT1024 <EugeneT1024@yandex.ru>
2024-12-02 17:04:55 +0300

    [BJS2-43623] сделал класс main отдельно для задачи

--- src/main/java/school/faang/Main.java
+++ src/main/java/school/faang/Main.java
@@ -1,15 +1,4 @@
 package school.faang;
 
-import school.faang.task_1.Example;
-
-import java.util.Arrays;
-
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("BJS2-43623 (Развернитесь!)---------------------------------------------------------------");
-        System.out.println("null array: " + Arrays.toString(Example.reverse(null)));
-        System.out.println("one element array: " + Arrays.toString(Example.reverse(new int[]{999})));
-        System.out.println("many elements array: " + Arrays.toString(Example.reverse(new int[]{0, 1, 2, 3, 4})));
-        System.out.println("-----------------------------------------------------------------------------------------");
-    }
 }
