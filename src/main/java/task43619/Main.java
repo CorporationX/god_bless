0394e1e93e0b1a0141465fd7a5e74c0b846790f2
Timofey Khutoryanskiy <tiroxxxin@gmail.com>
2024-12-02 14:03:09 +0500

    Развернитесь! BJS2-43619 (ver.1)

--- src/main/java/task43619/Main.java
+++ src/main/java/task43619/Main.java
@@ -1,8 +1,8 @@
-package task_43619;
+package task43619;
 
 import java.util.Arrays;
 
-import static task_43619.Example.reverse;
+import static task43619.Example.reverse;
 
 public class Main {
     public static void main(String[] args) {
