a164b3b5156595f3c8e22b06e6d6575cd43a1de8
TFdevQA <46944859+TFdevQA@users.noreply.github.com>
2024-12-01 21:05:50 -0800

    edit code style in main

--- src/main/java/school/faang/m1bjs243617/Main.java
+++ src/main/java/school/faang/m1bjs243617/Main.java
@@ -5,6 +5,9 @@ public class Main {
         int[] array = new int[]{0, 1, 2, 3, 4, 5};
 
         Example.reverse(array);
-        for (int element : array) System.out.print(element + ", ");
+
+        for (int element : array) {
+            System.out.print(element + ", ");
+        }
     }
 }
