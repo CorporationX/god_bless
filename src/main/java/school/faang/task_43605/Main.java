4ba50768fa78cb5fb3b6c5ef7e14cd76b1287715
Leonid Naumov <leonidnaumov34@gmail.com>
2024-12-02 23:52:15 +0300

    task completed

---
+++ src/main/java/school/faang/task_43605/Main.java
@@ -0,0 +1,8 @@
+package school.faang.task_43605;
+
+public class Main {
+    public static void main() {
+        int[] nums = {1,2,3,4,5};
+        System.out.println(Example.reverse(nums));
+    }
+}
