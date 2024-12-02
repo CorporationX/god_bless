b2d339685a878a0d52c710bd5a33bfc2723a5c2e
Timofey Khutoryanskiy <tiroxxxin@gmail.com>
2024-12-02 14:18:40 +0500

    Развернитесь! BJS2-43619 (ver.1)

--- src/main/java/task_43619/Main.java
+++ src/main/java/task_43619/Main.java
@@ -1,13 +1,13 @@
-package task43619;
+package task_43619;
 
 import java.util.Arrays;
 
-import static task43619.Example.reverse;
+
 
 public class Main {
     public static void main(String[] args) {
         int[] nums = {1, 2, 3, 4, 5};
-        reverse(nums);
+        Example.reverse(nums);
         System.out.println(Arrays.toString(nums));
     }
 }
