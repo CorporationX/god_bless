2929f88872bd16bc85313e5601fbb2bafbe0a784
Vadim Ovcharenko <vadimovcharenko12@gmail.com>
2024-12-02 23:49:31 +0300

    Fix checkstyle and deleted toString() in User.class

--- src/main/java/school/faang/task_43690/User.java
+++ src/main/java/school/faang/task_43690/User.java
@@ -12,31 +12,21 @@ public class User {
     private static final List<String> VALID_ADDRESSES = List.of("London", "New York", "Amsterdam");
 
     public User(String name, int age, String job, String address) throws IllegalArgumentException {
-        if(name.isEmpty())
+        if (name.isEmpty()) {
             throw new IllegalArgumentException("Name cannot be empty");
-        else if(age < 18)
+        } else if (age < 18) {
             throw new IllegalArgumentException("Age cannot be less than 18");
-        else if(!VALID_JOBS.contains(job))
+        } else if (!VALID_JOBS.contains(job)) {
             throw new IllegalArgumentException("This work is not on the list of allowed");
-        else if(!VALID_ADDRESSES.contains(address))
+        } else if (!VALID_ADDRESSES.contains(address)) {
             throw new IllegalArgumentException("The address is not listed in the allowed list");
-        else {
-            this.name=name;
-            this.age=age;
-            this.job=job;
-            this.address=address;
+        } else {
+            this.name = name;
+            this.age = age;
+            this.job = job;
+            this.address = address;
         }
 
 
-    }
-
-    @Override
-    public String toString() {
-        return "User{" +
-                "name='" + name + '\'' +
-                ", age=" + age +
-                ", job='" + job + '\'' +
-                ", address='" + address + '\'' +
-                '}';
     }
 }
