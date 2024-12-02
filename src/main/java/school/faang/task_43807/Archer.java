d659ec7ac6db936c4a8c2eeea4c6f57c5c0483ef
Lesha Kozyrev <alekseykoz@gmail.com>
2024-12-02 20:24:14 +0300

    BJS2-43807 was completed. Code style was fixed.

--- src/main/java/school/faang/task_43807/Archer.java
+++ src/main/java/school/faang/task_43807/Archer.java
@@ -4,6 +4,7 @@ public class Archer extends Character {
     public Archer(String name) {
         super(name, 3, 10, 5);
     }
+
     @Override
     public void attack(Character other) {
         System.out.println(other + " attack Archer");
--- src/main/java/school/faang/task_43807/Warrior.java
+++ src/main/java/school/faang/task_43807/Warrior.java
@@ -4,6 +4,7 @@ public class Warrior extends Character {
     public Warrior(String name) {
         super(name, 10, 5, 3);
     }
+
     @Override
     public void attack(Character other) {
         System.out.println(other + " attack Warrior");
