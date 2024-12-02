fd72b71cebc632a8bf28ba9b6889c5b711285ecb
elnur-elnur <tahirzadee@gmail.com>
2024-12-02 11:00:49 +0400

    Refactoring Method

--- src/main/java/school/faang/spr1task1/ReverseClass.java
+++ src/main/java/school/faang/spr1task1/ReverseClass.java
@@ -3,24 +3,17 @@ package school.faang.spr1task1;
 import java.util.Arrays;
 
 public class ReverseClass {
-
-
-     public static void reverse(int[] args) {
-
+    public static void reverse(int[] args) {
         int temp;
-
-        for(int i = 0; i < args.length / 2; i++) {
+        for (int i = 0; i < args.length / 2; i++) {
             temp = args[i];
             args[i] = args[args.length - 1 - i];
             args[args.length - 1 - i] = temp;
-
         }
-
     }
 
     public static void main(String[] args) {
-        int[] nums = {1,2,3,4,5};
-
+        int[] nums = {1, 2, 3, 4, 5};
         reverse(nums);
         System.out.println("Reversed arr: " + Arrays.toString(nums));
     }
