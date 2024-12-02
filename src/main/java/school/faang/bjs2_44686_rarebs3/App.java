84a2de56c1358e8666e30d08794400f42ef0d1f3
vvz3zzv <vladislav.v.zhurbin@gmail.com>
2024-12-02 19:45:24 +0300

    vvz3zzv-BJS2-44354-raraBS3-featureB
    - добавлены класс App

--- src/main/java/school/faang/bjs2_44686_rarebs3/App.java
+++ src/main/java/school/faang/bjs2_44686_rarebs3/App.java
@@ -2,6 +2,6 @@ package school.faang.bjs2_44686_rarebs3;
 
 public class App {
     public static void main(String[] args) {
-        System.out.println("«Открытие магазина ожидается через 4 дня»");
+        System.out.println("«Открытие магазина ожидается через 5 дня»");
     }
 }
