6151d1ed896ddafd004abe270ee1f4b3b5b4d987
Alexey Gorokhov <almigor@mail.ru>
2024-12-02 21:32:02 +0300

    Учет замечаний. пакет не получается без символов создать, сделал вида taskNNNNN

--- src/main/java/school/faang/task44028/Main.java
+++ src/main/java/school/faang/task44028/Main.java
@@ -1,30 +1,13 @@
-package school.faang.bjs2_44028;
+package school.faang.task44028;
 
-import lombok.AllArgsConstructor;
-import lombok.Data;
 
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-@Data
-@AllArgsConstructor
-public class User {
-    private String name;
-    private int age;
-    private String workplace;
-    private String address;
+public class Main {
 
     private static final List<User> users = new ArrayList<>();
-
-    public static Map<Integer, List<User>> groupUsers(List<User> users) {
-        Map<Integer, List<User>> groupedUsers = new HashMap<>();
-        for (User user : users) {
-            groupedUsers.computeIfAbsent(user.getAge(), initWith -> new ArrayList<>()).add(user);
-        }
-        return groupedUsers;
-    }
 
     public static void main(String[] args) {
         users.add(new User("Ann", 30, "MoscowCity", "Moscow"));
