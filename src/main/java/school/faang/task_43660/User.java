3aaef2dd1dd9f20bd5acb7afbcc5a38da2ac3d3b
Mihail Svistunov <egzermr@gmail.com>
2024-12-02 21:19:15 +0800

    BJS2-43660 done

--- src/main/java/school/faang/task_43660/User.java
+++ src/main/java/school/faang/task_43660/User.java
@@ -10,19 +10,19 @@ import java.util.Set;
 public class User {
     static final Set<String> VALID_JOBS = Set.of("Google", "Uber", "Amazon");
     static final Set<String> VALID_ADDRESSES = Set.of("London", "New York", "Amsterdam");
+    static final int ADULTHOOD = 18;
 
     private String name;
     private int age;
     private String job;
     private String address;
-    static final int ADULTHOOD = 18;
 
     public User(String name, int age, String job, String address) throws IllegalArgumentException {
         if (name.isEmpty()) {
             throw new IllegalArgumentException("name is empty");
         }
         if (age < ADULTHOOD) {
-            throw new IllegalArgumentException("Must be over 18 years of age");
+            throw new IllegalArgumentException("Must be over"+ ADULTHOOD +"years of age");
         }
         if (!VALID_JOBS.contains(job)) {
             throw new IllegalArgumentException("The place of work is indicated incorrectly");
