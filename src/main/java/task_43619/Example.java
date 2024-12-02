61dafd347978acecf75dfb303543db6f256a1f56
Timofey Khutoryanskiy <tiroxxxin@gmail.com>
2024-12-02 13:58:47 +0500

    Развернитесь! BJS2-43619 (ver.1)

--- src/main/java/task_43619/Example.java
+++ src/main/java/task_43619/Example.java
@@ -1,15 +1,14 @@
 package task_43619;
 
 public class Example {
-    public static void reverse (int [] nums){
-        if (nums.length == 0){
+    public static void reverse(int[] nums) {
+        if (nums.length == 0) {
             System.out.println("Массив пустой");
-        }
-        else {
+        } else {
             int startArray = 0;
             int endArray = nums.length - 1;
 
-            while (startArray < endArray){
+            while (startArray < endArray) {
                 int temp = nums[startArray];
                 nums[startArray] = nums[endArray];
                 nums[endArray] = temp;
--- src/main/java/task_43619/Main.java
+++ src/main/java/task_43619/Main.java
@@ -6,7 +6,7 @@ import static task_43619.Example.reverse;
 
 public class Main {
     public static void main(String[] args) {
-        int [] nums = {1,2,3,4,5};
+        int[] nums = {1, 2, 3, 4, 5};
         reverse(nums);
         System.out.println(Arrays.toString(nums));
     }
