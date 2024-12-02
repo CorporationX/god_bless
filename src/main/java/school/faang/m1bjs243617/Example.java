9b90b603b277190554bdf83a37d9a9733ff285d5
TFdevQA <46944859+TFdevQA@users.noreply.github.com>
2024-12-01 21:02:17 -0800

    additional edit code style

--- src/main/java/school/faang/m1bjs243617/Example.java
+++ src/main/java/school/faang/m1bjs243617/Example.java
@@ -3,7 +3,7 @@ package school.faang.m1bjs243617;
 public class Example {
 
     public static void reverse(int[] array) {
-        if ( array.length != 0 ) {
+        if (array.length != 0) {
             for (int i = 0; i < array.length / 2; i++) {
                 int temp = array[i];
                 array[i] = array[array.length - i - 1];
