b2fe1d4e79190ee64614a091e50965ba942791bf
Mihail Svistunov <egzermr@gmail.com>
2024-12-02 18:57:36 +0800

    BJS2-43660 done

---
+++ src/main/java/task_43660/Main.java
@@ -0,0 +1,29 @@
+package task_43660;
+
+public class Main {
+    public static void main(String[] args) {
+
+        try {
+            User user1 = new User("", 19, "Google", "London");
+        } catch (IllegalArgumentException e) {
+            System.out.println(e.getMessage());
+        }
+        try {
+            User user1 = new User("some name1", 16, "Google", "London");
+        } catch (IllegalArgumentException e) {
+            System.out.println(e.getMessage());
+        }
+        try {
+            User user1 = new User("some name2", 19, "Googl", "London");
+        } catch (IllegalArgumentException e) {
+            System.out.println(e.getMessage());
+        }
+        try {
+            User user1 = new User("some name3", 19, "Uber", "Londn");
+        } catch (IllegalArgumentException e) {
+            System.out.println(e.getMessage());
+        }
+        System.out.println(new User("some name", 18, "Amazon", "New York"));
+    }
+
+}
---
+++ src/main/java/task_43660/User.java
@@ -0,0 +1,36 @@
+package task_43660;
+
+import lombok.ToString;
+
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+
+@ToString
+public class User {
+    static final Set<String> VALID_JOBS = new HashSet<>(Arrays.asList("Google", "Uber", "Amazon"));
+    static final Set<String> VALID_ADDRESSES = new HashSet<>(Arrays.asList("London", "New York", "Amsterdam"));
+    private String name;
+    private int age;
+    private String job;
+    private String address;
+
+    public User(String name, int age, String job, String address) throws IllegalArgumentException {
+        if (name.isEmpty()) {
+            throw new IllegalArgumentException("name is empty");
+        }
+        if (age < 18) {
+            throw new IllegalArgumentException("Must be over 18 years of age");
+        }
+        if (!VALID_JOBS.contains(job)) {
+            throw new IllegalArgumentException("The place of work is indicated incorrectly");
+        }
+        if (!VALID_ADDRESSES.contains(address)) {
+            throw new IllegalArgumentException("The address is incorrect");
+        }
+        this.name = name;
+        this.age = age;
+        this.job = job;
+        this.address = address;
+    }
+}
