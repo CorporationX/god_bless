6a01449de6a9f2ab4bf5469156187357ae1f87a8
az3l1t <eternity_cr9p@mail.ru>
2024-12-03 08:42:13 +0300

    BJS2-43676 Решение переделано по рекомендациям!

--- src/main/java/school/faang/task_43676/Main.java
+++ src/main/java/school/faang/task_43676/Main.java
@@ -2,32 +2,17 @@ package school.faang.task_43676;
 
 public class Main {
     public static void main(String[] args) {
-        try {
-            User user = new User("NoName", 12, "Google", "London");
-        } catch (IllegalArgumentException e) {
-            System.out.println(e.getMessage());
-        }
+        tryToCreateUser("NoName", 12, "Google", "New York");
+        tryToCreateUser("", 20, "Google", "New York");
+        tryToCreateUser("NoName", 20, "What?", "New York");
+        tryToCreateUser("NoName", 20, "Google", "What?");
+        tryToCreateUser("NoName", 20, "Google", "New York");
+    }
 
-        try {
-            User user = new User("", 20, "Google", "London");
-        } catch (IllegalArgumentException e) {
-            System.out.println(e.getMessage());
-        }
-
-        try {
-            User user = new User("NoName", 20, "T1", "London");
-        } catch (IllegalArgumentException e) {
-            System.out.println(e.getMessage());
-        }
-
-        try {
-            User user = new User("NoName", 20, "Google", "Moscow");
-        } catch (IllegalArgumentException e) {
-            System.out.println(e.getMessage());
-        }
-
-        try {
-            User user = new User("NoName", 20, "Google", "London");
+    public static void tryToCreateUser(String name, Integer age, String job, String address) {
+        try{
+            User user = new User(name, age, job, address);
+            System.out.printf("User was created, his name is: %s", name);
         } catch (IllegalArgumentException e) {
             System.out.println(e.getMessage());
         }
