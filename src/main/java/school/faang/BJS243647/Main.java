ce9b53c1adad04862a483e68ef3567917ed08e03
Dmitriy <dda20040609@gmail.com>
2024-12-03 15:00:43 +0300

    commit 1 Register User

---
+++ src/main/java/school/faang/BJS243647/Main.java
@@ -0,0 +1,16 @@
+package school.faang.BJS243647;
+
+public class Main {
+    public static void main(String[] args) {
+        try {
+            User user = new User("Dima", 20, "Google", "London");
+            System.out.println(user);
+            User user1 = new User("Ivan", 30, "Uber", "Amsterdam");
+            System.out.println(user1);
+        } catch (IllegalArgumentException e) {
+            System.out.println("Ошибка: " + e.getMessage());
+        }
+
+    }
+
+}
