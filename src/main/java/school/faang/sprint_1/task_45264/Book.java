6b3ba60952eb642528f684c2a6d5211e53b2e26d
Stanislav Bugaev <stasikbygaev@gmail.com>
2024-12-03 21:52:53 +0500

    Добавил {} для if в equals в Book

--- src/main/java/school/faang/sprint_1/task_45264/Book.java
+++ src/main/java/school/faang/sprint_1/task_45264/Book.java
@@ -17,8 +17,12 @@ public class Book {
 
     @Override
     public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
         Book book = (Book) o;
         return Objects.equals(title, book.title)
                 && Objects.equals(author, book.author) && Objects.equals(year, book.year);
