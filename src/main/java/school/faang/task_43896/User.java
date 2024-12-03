abea30971812bc53e58433d80ce780faac01249c
Mihail Ermolaev <fitis_rus21@mail.ru>
2024-12-03 23:39:05 +0300

    Добавлен класс запускатор, улучшена читаемость кода

--- src/main/java/school/faang/task_43896/User.java
+++ src/main/java/school/faang/task_43896/User.java
@@ -7,10 +7,10 @@ import java.util.HashMap;
 
 @Data
 public class User {
-    String name;
-    int age;
-    String workplace;
-    String address;
+    private String name;
+    private int age;
+    private String workplace;
+    private String address;
 
     public User(String name, int age, String workplace, String address) {
         this.name = name;
@@ -25,32 +25,13 @@ public class User {
         for (User user : users) {
             if (!usersMap.containsKey(user.getAge())) {
                 ArrayList<User> usersList = new ArrayList<>();
-
-                usersList.add(user);
                 usersMap.put(user.getAge(), usersList);
-            } else {
-                ArrayList<User> userArrayList = usersMap.get(user.getAge());
-
-                userArrayList.add(user);
             }
+
+            ArrayList<User> userArrayList = usersMap.get(user.getAge());
+            userArrayList.add(user);
         }
 
         return usersMap;
-    }
-
-    public static void main(String[] args) {
-        User user1 = new User("Миша", 36, "Библиотека", "Москва");
-        User user2 = new User("Влад", 20, "Завод", "Самара");
-        User user3 = new User("Саша", 20, "Фабрика", "Тула");
-        User user4 = new User("Вася", 50, "Библиотека", "Тверь");
-
-        ArrayList<User> users = new ArrayList<>();
-
-        users.add(user1);
-        users.add(user2);
-        users.add(user3);
-        users.add(user4);
-
-        System.out.println(groupUsers(users));
     }
 }
