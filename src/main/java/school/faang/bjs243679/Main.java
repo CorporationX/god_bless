2403e492a18f753e43e5f4a7583409b0d1a8a454
Pavel <45390423+rsticks@users.noreply.github.com>
2024-12-03 19:37:38 +0300

    BJS2-43679 Регистрация пользователей

--- src/main/java/school/faang/bjs243679/Main.java
+++ src/main/java/school/faang/bjs243679/Main.java
@@ -2,35 +2,18 @@ package school.faang.bjs243679;
 
 public class Main {
     public static void main(String[] args) {
-        // Invalid name
+        tryCreateUser("", 18, "Google", "Amsterdam");
+        tryCreateUser("Pavel", 17, "Google", "Amsterdam");
+        tryCreateUser("Pavel", 18, "Gooooooooogle", "Amsterdam");
+        tryCreateUser("Pavel", 18, "Google", "Moscow");
+        tryCreateUser("Pavel", 18, "Google", "Amsterdam");
+    }
+
+    private static void tryCreateUser(String name, int age, String job, String address) {
         try {
-            new User("", 18, "Google", "Amsterdam");
+            new User(name, age, job, address);
         } catch (Exception e) {
             System.out.println(e.getMessage());
         }
-
-        // Invalid age
-        try {
-            new User("Pavel", 17, "Google", "Amsterdam");
-        } catch (Exception e) {
-            System.out.println(e.getMessage());
-        }
-
-        // Invalid job
-        try {
-            new User("Pavel", 18, "Gooooooooogle", "Amsterdam");
-        } catch (Exception e) {
-            System.out.println(e.getMessage());
-        }
-
-        // Invalid address
-        try {
-            new User("Pavel", 18, "Google", "Moscow");
-        } catch (Exception e) {
-            System.out.println(e.getMessage());
-        }
-
-        // Success
-        new User("Pavel", 18, "Google", "Amsterdam");
     }
 }
