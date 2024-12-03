0f0ee198b053e3ce8138c9f850e9f694c995a0f1
Stepan Evangulov <stevangulov@gmail.com>
2024-12-03 08:51:45 +0200

    Done

--- src/main/java/school/faang/task_43503/User.java
+++ src/main/java/school/faang/task_43503/User.java
@@ -18,17 +18,4 @@ public class User {
     public static Map<Integer, List<User>> groupUsers(List<User> users) {
         return users.stream().collect(Collectors.groupingBy(User::getAge));
     }
-
-    public static void main(String[] args) {
-        List<User> users = List.of(
-                new User("Tom", 25, "Corp 1", "Adress A"),
-                new User("Sofia", 18, "Corp 2", "Adress B"),
-                new User("Bob", 36, "Corp 3", "Adress C"),
-                new User("Jerry", 52, "Corp 4", "Adress D")
-        );
-
-        for (Map.Entry<Integer, List<User>> usersEntry : User.groupUsers(users).entrySet()) {
-            System.out.println(usersEntry);
-        }
-    }
 }
