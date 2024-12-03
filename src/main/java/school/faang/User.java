9cc872c5c6da97beafe40e7c5c41ffd08855a387
Ilya Vysokolyan <rd2d@list.ru>
2024-12-03 19:18:17 +0300

    Поправил последние замечания

--- src/main/java/school/faang/User.java
+++ src/main/java/school/faang/User.java
@@ -1,7 +1,7 @@
 package school.faang;
 
 public class User {
-    private int MINIMUM_AGE = 18;
+    private static final int minimumAge = 18;
 
     private String name;
     private int age;
@@ -17,7 +17,7 @@ public class User {
         if (name.isEmpty() || name == null) {
             throw new IllegalArgumentException("Имя должно быть заполнено");
         }
-        if (age < MINIMUM_AGE) {
+        if (age < minimumAge) {
             throw new IllegalArgumentException("Минимальный возраст 18 лет");
         }
         if (job == null || address == null) {
