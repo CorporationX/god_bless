10e686a879902a8daac4195289d995d02b655ec8
askar0103 <ananaspineapple2048@gmail.com>
2024-12-03 11:37:01 +0300

    Solution to task BJS2-43696

---
+++ src/main/java/school/faang/task_43696/User.java
@@ -0,0 +1,43 @@
+package school.faang.task_43696;
+
+import lombok.ToString;
+
+import java.util.Set;
+
+@ToString
+public class User {
+    private static final Set<String> VALID_JOBS = Set.of("Google", "Uber", "Amazon");
+    private static final Set<String> VALID_ADDRESSES = Set.of("London", "New York", "Amsterdam");
+
+    private final String name;
+    private  final int age;
+    private final String job;
+    private final String address;
+
+    public User(String name, int age, String job, String address) {
+        if (!name.isBlank()) {
+            this.name = name;
+        } else {
+            throw new IllegalArgumentException("Name cannot be blank");
+        }
+
+        if (age >= 18) {
+            this.age = age;
+        } else {
+            throw new IllegalArgumentException("Age must be at least 18");
+        }
+
+        if (!job.isBlank() && VALID_JOBS.contains(job.trim())) {
+            this.job = job;
+        } else {
+            throw new IllegalArgumentException("Job cannot be blank and must be one of: " + String.join(", ", VALID_JOBS));
+        }
+
+        if (!address.isBlank() && VALID_ADDRESSES.contains(address.trim())) {
+            this.address = address;
+        } else {
+            throw new IllegalArgumentException("Address cannot be blank and must be one of: " + String.join(", ", VALID_ADDRESSES));
+        }
+    }
+
+}
