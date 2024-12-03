762c9ca421d1a00c600df95b94ea58b5796ba1e9
Kir34 <kirnovikof1@gmail.com>
2024-12-03 16:19:19 +0300

    задание с группировкой юзеров по возрасту

---
+++ src/main/java/task_BJS2_43948/User.java
@@ -0,0 +1,28 @@
+package task_BJS2_43948;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+import java.util.*;
+
+@Data
+@AllArgsConstructor
+public class User {
+    private String name;
+    private int age;
+    private String workplace;
+    private int address;
+
+    public static Map<Integer, List<User>> groupUsers(List<User> users) {
+        Map<Integer, List<User>> groupUsersResult = new HashMap<>();
+        for (User user : users) {
+            if (!groupUsersResult.containsKey(user.age)) {
+                groupUsersResult.put(user.age, new ArrayList<>());
+                groupUsersResult.get(user.age).add(user);
+            } else {
+                groupUsersResult.get(user.age).add(user);
+            }
+        }
+        return groupUsersResult;
+    }
+}
