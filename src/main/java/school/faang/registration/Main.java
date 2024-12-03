1835b91f623972793c95a39a2b25c2613604f271
Daria <dashok.sok@gmail.com>
2024-12-03 09:47:40 +0300

    Задача "Регистрация пользователя"

---
+++ src/main/java/school/faang/registration/Main.java
@@ -0,0 +1,14 @@
+package school.faang.registration;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) {
+//        User masha = new User("Masha", 5, "Google", "London");
+//        User dasha = new User("Dasha", 22, "Uber", "Minsk");
+//        User noName = new User("", 33, "Google", "New York");
+        User pasha = new User("Pasha", 32, "Amazon", "Amsterdam");
+//        User sasha = new User("Sasha", 22, "Sber", "New York");
+    }
+}
