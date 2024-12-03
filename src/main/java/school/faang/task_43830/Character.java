df59b02fa7a6d979b2f16129e11d8b2056c1de51
Anatoly Kushnarev <makartih@Anatolys-MacBook-Pro.local>
2024-12-03 18:43:13 +0200

    the task BJS2-43830 is completed

---
+++ src/main/java/school/faang/task_43830/Character.java
@@ -0,0 +1,31 @@
+package school.faang.task_43830;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@ToString
+public abstract class Character {
+    private String name;
+    private int power;
+    private int agility;
+    private int intelligence;
+    private int health;
+    private static final int HEALTH = 100;
+
+    public Character(String name){
+        this.name = name;
+    }
+
+    public Character(String name, int power, int agility, int intelligence){
+        this.name = name;
+        this.power = power;
+        this.agility = agility;
+        this.intelligence = intelligence;
+        this.health = HEALTH;
+    }
+
+    public abstract void attack(Character player);
+}
---
+++ src/main/java/school/faang/task_43830/Main.java
@@ -0,0 +1,24 @@
+package school.faang.task_43830;
+
+public class Main {
+
+    public static void main(String [] args){
+
+        Archer myArcher = new Archer("Anatolijs");
+        Warrior myWarrior = new Warrior("Dmitrijs");
+
+        System.out.println("Health :: " + myWarrior.getHealth());
+        myArcher.attack(myWarrior);
+        System.out.println("Health :: " + myWarrior.getHealth());
+        myArcher.attack(myWarrior);
+        System.out.println("Health :: " + myWarrior.getHealth());
+        System.out.println(" ----------------- ");
+        System.out.println("Health :: " + myArcher.getHealth());
+        myWarrior.attack(myArcher);
+        System.out.println("Health :: " + myArcher.getHealth());
+        myArcher.attack(myArcher);
+        System.out.println("Health :: " + myArcher.getHealth());
+        myArcher.attack(myArcher);
+        System.out.println("Health :: " + myArcher.getHealth());
+    }
+}
