34f583e392d6bc25291c32b4c63edc5e732b8ec0
NiveshOne <Shishov.44@mail.ru>
2024-12-04 00:42:03 +0300

    Fix compilation error

--- src/main/java/school/faang/task_43976/Main.java
+++ src/main/java/school/faang/task_43976/Main.java
@@ -9,12 +9,18 @@ public class Main {
 
         List<User> listOfUsers = new ArrayList<>();
 
-        listOfUsers.add(new User("Ivan", 19, "Yandex", "123 Red Square, Moscow, 101000, Russia"));
-        listOfUsers.add(new User("Dmitry", 38, "Sber", "45 Nevsky Prospekt, Saint Petersburg, 191011, Russia"));
-        listOfUsers.add(new User("Alex", 25, "Uber", "22 Komsomolskaya Street, Kazan, 420012, Russia"));
-        listOfUsers.add(new User("Nick", 33, "VTB", "78 Tverskaya Street, Moscow, 125009, Russia"));
-        listOfUsers.add(new User("Jon", 19, "Creative Call Project", "101 Pushkin Street, Nizhny Novgorod, 603000, Russia"));
-        listOfUsers.add(new User("Sergey", 38, "Amazon", "56 Sovetskaya Street, Novosibirsk, 630000, Russia"));
+        listOfUsers.add(new User("Ivan", 19, "Yandex",
+                "123 Red Square, Moscow, 101000, Russia"));
+        listOfUsers.add(new User("Dmitry", 38, "Sber",
+                "45 Nevsky Prospekt, Saint Petersburg, 191011, Russia"));
+        listOfUsers.add(new User("Alex", 25, "Uber",
+                "22 Komsomolskaya Street, Kazan, 420012, Russia"));
+        listOfUsers.add(new User("Nick", 33, "VTB",
+                "78 Tverskaya Street, Moscow, 125009, Russia"));
+        listOfUsers.add(new User("Jon", 19, "Creative Call Project",
+                "101 Pushkin Street, Nizhny Novgorod, 603000, Russia"));
+        listOfUsers.add(new User("Sergey", 38, "Amazon",
+                "56 Sovetskaya Street, Novosibirsk, 630000, Russia"));
 
         Map<Integer, List<User>> gropedUsers = User.groupUsers(listOfUsers);
 
