3df8ab3cad372d5e770a3336b302baf08283f613
Eugene <@evgeniy_kim>
2024-12-03 20:48:53 +0500

    task BJS2-43693

--- src/main/java/school/faang/task_43693/Main.java
+++ src/main/java/school/faang/task_43693/Main.java
@@ -2,6 +2,7 @@ package school.faang.task_43693;
 
 public class Main {
     public static void main(String[] args) {
-        User user = new User("Sergey Brin", 51, "Google", "California");
+        User invalidUser = new User("Sergey Brin", 51, "Google", "California");
+        User validUser = new User("Jeff Bezos", 60, "Amazon", "New York");
     }
 }
