015f990fc097be639b720ab66f87e84cbf49245d
Aleksandr Kozlov <vismut-888@yandex.ru>
2024-12-03 19:39:55 +0300

    Задача: BJS2-43977 Коллекционируем пользователей

---
+++ src/main/java/school/faang/sprint_1/task_43977/Main.java
@@ -0,0 +1,49 @@
+package school.faang.sprint_1.task_43977;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
+import static school.faang.sprint_1.task_43977.User.findHobbyLovers;
+
+public class Main {
+    public static void main(String[] args) {
+        Set<String> hobbyIvan = new HashSet<>();
+        hobbyIvan.add("Dance");
+        hobbyIvan.add("Painting");
+        hobbyIvan.add("Cooking");
+        hobbyIvan.add("Fishing");
+
+        Set<String> hobbyMax = new HashSet<>();
+        hobbyMax.add("Dance");
+        hobbyMax.add("Cooking");
+        hobbyMax.add("Fishing");
+        hobbyMax.add("Bowling");
+
+        Set<String> hobbyAlex = new HashSet<>();
+        hobbyAlex.add("Golf");
+        hobbyAlex.add("Cooking");
+        hobbyAlex.add("Knitting");
+        hobbyAlex.add("Gardening");
+
+        Set<String> test = new HashSet<>();
+        test.add("Cooking");
+        test.add("Fishing");
+        test.add("Painting");
+
+        User user1 = new User(1, "Ivan", 24, hobbyIvan);
+        User user2 = new User(2, "Max", 34, hobbyMax);
+        User user3 = new User(3, "Alex", 33, hobbyAlex);
+
+        List<User> users = new ArrayList<>();
+        users.add(user1);
+        users.add(user2);
+        users.add(user3);
+
+        for (Map.Entry<User, String> entry : findHobbyLovers(users, test).entrySet()) {
+            System.out.println(entry.getKey() + " - " + entry.getValue());
+        }
+    }
+}
---
+++ src/main/java/school/faang/sprint_1/task_43977/User.java
@@ -0,0 +1,43 @@
+package school.faang.sprint_1.task_43977;
+
+import lombok.Getter;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
+@Getter
+public class User {
+    private int id;
+    private String name;
+    private int age;
+    private Set<String> hobby;
+
+    public User(int id, String name, int age, Set<String> hobby) {
+        this.id = id;
+        this.name = name;
+        this.age = age;
+        this.hobby = hobby;
+    }
+
+    public static Map<User, String> findHobbyLovers(List<User> users, Set<String> hobbies) {
+        Map<User, String> hobbyLovers = new HashMap<>();
+        for (User user : users) {
+            for (String hobby : hobbies) {
+                if (user.getHobby().contains(hobby)) {
+                    hobbyLovers.put(user, hobby);
+                }
+            }
+        }
+        return hobbyLovers;
+    }
+
+    @Override
+    public String toString() {
+        return "User name: '" + name
+                + "', age: " + age
+                + ", id: " + id
+                + ", hobby: " + hobby;
+    }
+}
