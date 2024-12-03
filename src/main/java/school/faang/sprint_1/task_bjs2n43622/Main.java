71b5fdaca07cad2e7913f872b3b2f6b097c54a07
alexey_obraztsov <alexey.autotester86@gmail.com>
2024-12-03 14:30:37 +0300

    reverse() method implemented with fix checkstyle whitespaces

--- src/main/java/school/faang/sprint_1/task_bjs2n43622/Main.java
+++ src/main/java/school/faang/sprint_1/task_bjs2n43622/Main.java
@@ -4,16 +4,16 @@ public class Main {
     public static void main(String[] args) {
         Example example = new Example();
 
-        int[] arrayEven = new int[]{1,2,3,4,5,6,7,8};
-        int[] arrayOdd = new int[]{1,2,3,4,5,6,7,8,9};
-        example.reverse( arrayEven);
-        example.reverse( arrayOdd);
+        int[] arrayEven = new int[]{1, 2, 3, 4, 5, 6, 7, 8};
+        int[] arrayOdd = new int[]{1, 2, 3, 4, 5, 6, 7, 8, 9};
+        example.reverse(arrayEven);
+        example.reverse(arrayOdd);
 
-        for(int num: arrayEven) {
+        for (int num : arrayEven) {
             System.out.print(num + " ");
         }
         System.out.println();
-        for(int num: arrayOdd) {
+        for (int num : arrayOdd) {
             System.out.print(num + " ");
         }
     }
