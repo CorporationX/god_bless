bb86e112289e6a95cf37d3069487a9d0c3a1422b
Eugene <@evgeniy_kim>
2024-12-03 10:42:00 +0500

    task BJS2-43921, evgeniy_kim1993

---
+++ src/main/java/school/faang/task_43921/Main.java
@@ -0,0 +1,20 @@
+package school.faang.task_43921;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) {
+        List<User> users = new ArrayList<>();
+        users.add(new User("Petya", 10, "Yandex", "Moscow"));
+        users.add(new User("Vasya", 10, "Alfa-Bank", "Moscow"));
+        users.add(new User("Kolya", 9, "T-Bank", "Moscow"));
+        users.add(new User("Igor", 9, "Alexander and Сo", "St Petersburg"));
+
+        var groupedUSers = User.groupUsers(users);
+        groupedUSers.forEach((age, list) -> {
+            System.out.println("Age: " + age);
+            list.forEach(user -> System.out.println(" " + user.toString()));
+        });
+    }
+}
