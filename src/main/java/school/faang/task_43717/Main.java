41eb7352de48c88a76541d3aa74fcab44f04b816
porchy21 <ogogo.mt@gmail.com>
2024-12-03 22:17:39 +0500

    Вынес валидацию в отдельный метод, устранил дублирование кода.

--- src/main/java/school/faang/task_43717/Main.java
+++ src/main/java/school/faang/task_43717/Main.java
@@ -2,41 +2,19 @@ package school.faang.task_43717;
 
 public class Main {
     public static void main(String[] args) {
-        // Пример 1: Пустое имя
-        try {
-            User user1 = new User("", 25, "Google", "Amsterdam");
-        } catch (IllegalArgumentException ex) {
-            System.out.println(ex.getMessage()); // "Имя не может быть пустым"
-        }
+        createUser("", 25, "Google", "Amsterdam");
+        createUser("Алексей", 17, "Uber", "London");
+        createUser("Мария", 30, "ООО Рога и копыта", "New York");
+        createUser("Дмитрий", 40, "Amazon", "Калуга");
+        createUser("Ольга", 29, "Uber", "Amsterdam");
+    }
 
-        // Пример 2: Возраст меньше 18
+    private static void createUser(String name, int age, String job, String address) {
         try {
-            User user2 = new User("Алексей", 17, "Uber", "London");
-        } catch (IllegalArgumentException ex) {
-            System.out.println(ex.getMessage()); // "Возраст не может быть меньше 18"
-        }
-
-        // Пример 3: Неверная работа
-        try {
-            User user3 = new User("Мария", 30, "ООО Рога и копыта", "New York");
-        } catch (IllegalArgumentException ex) {
-            System.out.println(ex.getMessage()); // "Место работы должно быть содержится в наборе VALID_JOBS"
-        }
-
-        // Пример 4: Неверный адрес
-        try {
-            User user4 = new User("Дмитрий", 40, "Amazon", "Калуга");
-        } catch (IllegalArgumentException ex) {
-            System.out.println(ex.getMessage()); // "Адрес должен быть содержится в наборе VALID_ADDRESSES"
-        }
-
-        // Пример 5: Все корректно
-        try {
-            User user5 = new User("Ольга", 29, "Uber", "Amsterdam");
+            User user = new User(name, age, job, address);
             System.out.println("Пользователь успешно создан");
         } catch (IllegalArgumentException ex) {
             System.out.println(ex.getMessage());
         }
-
     }
 }
