61a1d877f73035d44a7f1d00517591d6ffc92a00
Aleksandr Kozlov <vismut-888@yandex.ru>
2024-12-03 12:26:58 +0300

    Задача: BJS2-43688 (Регистрация пользователей)

---
+++ src/main/java/school/faang/sprint_1/task_43688/Main.java
@@ -0,0 +1,10 @@
+package school.faang.sprint_1.task_43688;
+
+public class Main {
+    public static void main(String[] args) {
+        User goodUser = new User("Ivan", 22, "Google", "London");
+        System.out.println(goodUser);
+
+        User badUser = new User("Ivan", 22, "MacDonald", "London");
+    }
+}
---
+++ src/main/java/school/faang/sprint_1/task_43688/User.java
@@ -0,0 +1,34 @@
+package school.faang.sprint_1.task_43688;
+
+import java.util.Arrays;
+
+public class User {
+    private static final String[] VALID_JOBS = {"Google", "Uber", "Amazon"};
+    private static final String[] VALID_ADDRESSES = {"London", "New York", "Amsterdam"};
+
+    private String name;
+    private int age;
+    private String job;
+    private String address;
+
+    public User(String name, int age, String job, String address) {
+        if (name.isEmpty()) throw new IllegalArgumentException("Name must not be empty");
+        else this.name = name;
+        if (age < 18) throw new IllegalArgumentException("Age cannot be less than 18");
+        else this.age = age;
+        if (!Arrays.asList(VALID_JOBS).contains(job))
+            throw new IllegalArgumentException("Place of work must contain: " + Arrays.toString(VALID_JOBS));
+        else this.job = job;
+        if (!Arrays.asList(VALID_ADDRESSES).contains(address))
+            throw new IllegalArgumentException("Work address must contain: " + Arrays.toString(VALID_ADDRESSES));
+        else this.address = address;
+    }
+
+    @Override
+    public String toString() {
+        return "User name: '" + name + '\''
+                + ", age: " + age
+                + ", job: '" + job + '\''
+                + ", address: '" + address + '\'';
+    }
+}
