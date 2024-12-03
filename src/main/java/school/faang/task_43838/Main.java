fb4efa22f51d8c120260de6b2232eb04ffa21bfb
narol01 <kmingogod@gmail.com>
2024-12-03 19:36:11 +0100

    BJS2-43838

--- src/main/java/school/faang/task_43838/Main.java
+++ src/main/java/school/faang/task_43838/Main.java
@@ -6,16 +6,16 @@ import school.faang.task_43838.entity.Warrior;
 
 public class Main {
     public static void main(String[] args) {
-        Character Archer = new Archer("Assassin");
-        Character Warrior = new Warrior("Axe");
+        Character archer = new Archer("Assassin");
+        Character warrior = new Warrior("Axe");
 
-        System.out.println(Archer);
-        System.out.println(Warrior);
+        System.out.println(archer);
+        System.out.println(warrior);
 
-        Warrior.attack(Archer);
-        Archer.attack(Warrior);
+        warrior.attack(archer);
+        archer.attack(warrior);
 
-        System.out.println(Archer);
-        System.out.println(Warrior);
+        System.out.println(archer);
+        System.out.println(warrior);
     }
 }
