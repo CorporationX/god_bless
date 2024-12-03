a9649aaaaa30b90f2036dc60fe14dc284a29cf08
az3l1t <eternity_cr9p@mail.ru>
2024-12-03 23:01:39 +0300

    BJS2-44473 Ловим события Исправил по рекомендациям!

--- src/main/java/school/faang/task_43822/Archer.java
+++ src/main/java/school/faang/task_43822/Archer.java
@@ -2,18 +2,15 @@ package school.faang.task_43822;
 
 public class Archer extends Character {
     public Archer(String name) {
-        super(name);
-        setPower(3);
-        setStamina(10);
-        setIq(5);
+        super(name, 3, 10, 5);
     }
 
     @Override
     public void attack(Character c) {
-        if (c.getHealth() < 0) {
-            throw new IllegalArgumentException("The character you try to attack already dead :(");
-        } else {
+        if (c.getHealth() > 0) {
             c.setHealth(c.getHealth() - getStamina());
+        } else {
+            throw new IllegalArgumentException("The character you try to attack already dead :(");
         }
     }
 }
--- src/main/java/school/faang/task_43822/Warrior.java
+++ src/main/java/school/faang/task_43822/Warrior.java
@@ -2,18 +2,15 @@ package school.faang.task_43822;
 
 public class Warrior extends Character {
     public Warrior(String name) {
-        super(name);
-        setPower(10);
-        setStamina(5);
-        setIq(3);
+        super(name, 10, 5, 3);
     }
 
     @Override
     public void attack(Character c) {
-        if (c.getHealth() < 0) {
-            throw new IllegalArgumentException("The character you try to attack already dead :(");
-        } else {
+        if (c.getHealth() > 0) {
             c.setHealth(c.getHealth() - getPower());
+        } else {
+            throw new IllegalArgumentException("The character you try to attack already dead :(");
         }
     }
 }
