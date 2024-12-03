20b18b4fe455ea24c6e7490b13dd7d2d0c56802a
alexey_obraztsov <alexey.autotester86@gmail.com>
2024-12-03 14:08:39 +0300

    reverse() method implemented 2

--- src/main/java/school/faang/sprint_1/task_bjs2n43622/Main.java
+++ src/main/java/school/faang/sprint_1/task_bjs2n43622/Main.java
@@ -2,7 +2,6 @@ package school.faang.sprint_1.task_bjs2n43622;
 
 public class Main {
     public static void main(String[] args) {
-
         Example example = new Example();
 
         int[] arrayEven = new int[]{1,2,3,4,5,6,7,8};
