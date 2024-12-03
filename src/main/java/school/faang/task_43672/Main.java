75c5821ad0c53fafdeb4ce7faa578a5135af31bc
Leonid Naumov <leonidnaumov34@gmail.com>
2024-12-03 23:29:20 +0300

    task completed

--- src/main/java/school/faang/task_43672/Main.java
+++ src/main/java/school/faang/task_43672/Main.java
@@ -2,6 +2,6 @@ package school.faang.task_43672;
 
 public class Main {
     public static void main(String[] args) {
-        User user = new User("Vasya",19,"Google","Amsterdam");
+        User user = new User("Vasya", 19, "Google", "Amsterdam");
     }
 }
