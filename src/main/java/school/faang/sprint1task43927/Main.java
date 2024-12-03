27b13b30dd8ada856b5659ee61da8361de49238b
elnur-elnur <tahirzadee@gmail.com>
2024-12-04 01:24:15 +0400

    Refactoring v1 Группировка пользователей по возрасту

--- src/main/java/school/faang/sprint1task43927/Main.java
+++ src/main/java/school/faang/sprint1task43927/Main.java
@@ -5,9 +5,9 @@ import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
-        User user1 =  new User("testUser1", 22, "Google", "USA");
-        User user2 =  new User("testUser2", 33, "Uber", "Moscow");
-        User user3 =  new User("testUser3", 33, "Amazon", "Dubai");
+        User user1 = new User("testUser1", 22, "Google", "USA");
+        User user2 = new User("testUser2", 33, "Uber", "Moscow");
+        User user3 = new User("testUser3", 33, "Amazon", "Dubai");
         List<User> list = new ArrayList<>();
         list.add(user1);
         list.add(user2);
