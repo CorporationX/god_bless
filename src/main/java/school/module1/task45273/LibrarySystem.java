649e30643eb9dfe124fa732f3c36cd7de73cd598
Semyon Postnikov <postnikov.simon@gmail.com>
2024-12-03 19:11:43 +0300

    added Library of Westeros

---
+++ src/main/java/school/module1/task45273/LibrarySystem.java
@@ -0,0 +1,29 @@
+package school.module1.task45273;
+import java.util.Map;
+
+import static school.module1.task45273.Main.bookLocation;
+
+public class LibrarySystem {
+
+    public void addBook(String title, String author, int year, String location) {
+        Book book = new Book(title, author, year);
+        bookLocation.put(book, location);
+    }
+
+    public void removeBook(String title, String author, int year) {
+        Book book = new Book(title, author, year);
+        bookLocation.remove(book);
+    }
+
+    public String findBook(String title, String author, int year) {
+        Book book = new Book(title, author, year);
+        System.out.println(bookLocation.getOrDefault(book, "Нет книги"));
+        return bookLocation.getOrDefault(book, "Нет книги");
+    }
+
+    public void printAllBooks() {
+        for (Map.Entry<Book, String> entry : bookLocation.entrySet()) {
+            System.out.println(entry.getKey() + " - местоположение " + entry.getValue());
+        }
+    }
+}
---
+++ src/main/java/school/module1/task45273/Main.java
@@ -0,0 +1,29 @@
+package school.module1.task45273;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Main {
+    static LibrarySystem librarySystem = new LibrarySystem();
+    static Map<Book, String> bookLocation = new HashMap<>();
+
+    public static void main(String[] args) {
+
+        Book book1 = new Book("Harry Potter and the Prisoner of Azkaban", "JK Rowling", 2000);
+        Book book2 = new Book("The Green Mile", "Stephen King", 1993);
+        Book book3 = new Book("The Lord of the Rings: The Return of the King", "John R. R. Tolkien", 1950);
+
+        bookLocation.put(book1, "shelf1");
+        bookLocation.put(book2, "shelf2");
+        bookLocation.put(book3, "shelf1");
+
+        librarySystem.printAllBooks();
+        librarySystem.removeBook("Harry Potter and the Prisoner of Azkaban", "JK Rowling", 2000);
+        librarySystem.printAllBooks();
+        librarySystem.addBook("The Count of Monte Cristo", "Alexandre Dumas", 1700, "shelf4");
+        librarySystem.printAllBooks();
+        librarySystem.findBook("The Lord of the Rings: The Return of the King", "John R. R. Tolkien", 1950);
+    }
+
+
+}
