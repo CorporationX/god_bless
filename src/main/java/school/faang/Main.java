0762880e888a2d65d52e3d071569878d45a3d6c2
Stepan Evangulov <stevangulov@gmail.com>
2024-12-03 10:22:26 +0200

    Done

--- src/main/java/school/faang/Main.java
+++ src/main/java/school/faang/Main.java
@@ -1,4 +1,13 @@
 package school.faang;
 
 public class Main {
+    public static void main(String[] args) {
+        try {
+            User user = new User("Tom", 18, "Google", "London");
+            System.out.println(user);
+        } catch (Exception e) {
+            e.printStackTrace();
+            System.out.println(e.getMessage());
+        }
+    }
 }
