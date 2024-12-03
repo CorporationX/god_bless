a646988cb86653e5a70f90fd9398741f70e1417b
Eugene <@evgeniy_kim>
2024-12-03 20:39:33 +0500

    task BJS2-45250

---
+++ src/main/java/school/faang/task_43693/Main.java
@@ -0,0 +1,7 @@
+package school.faang.task_43693;
+
+public class Main {
+    public static void main(String[] args) {
+        User user = new User("Sergey Brin", 51, "Google", "California");
+    }
+}
