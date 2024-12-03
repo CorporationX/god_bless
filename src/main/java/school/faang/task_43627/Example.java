98193cf42144415d50fff0174ce095c86b28a1fb
senty <senty1844@gmail.com>
2024-12-03 10:10:47 +0300

    Task-BJS2-43627 Changing logic to save memory

--- src/main/java/school/faang/task_43627/Example.java
+++ src/main/java/school/faang/task_43627/Example.java
@@ -2,16 +2,14 @@ package school.faang.task_43627;
 
 public class Example {
 
-    public static int[] reverse(int[] normalArray) {
-        int[] reversedArray = new int[normalArray.length];
+    public static void reverse(int[] array) {
 
-        for (int normalArrayIndex = 0,
-             reversedArrayIndex = normalArray.length - 1;
-             normalArrayIndex < normalArray.length;
-             normalArrayIndex++, reversedArrayIndex--) {
-            reversedArray[reversedArrayIndex] = normalArray[normalArrayIndex];
-
+        for (int fromBegin = 0, fromEnd = array.length - 1;
+             fromBegin < array.length / 2;
+             fromBegin++, fromEnd--) {
+            int tempInt = array[fromBegin];
+            array[fromBegin] = array[fromEnd];
+            array[fromEnd] = tempInt;
         }
-        return reversedArray;
     }
 }
--- src/main/java/school/faang/task_43627/Main.java
+++ src/main/java/school/faang/task_43627/Main.java
@@ -4,13 +4,14 @@ public class Main {
 
     public static void main(String[] args) {
         int[] evenExample = {5, 4, 3, 2, 1};
-        int[] oddExample = {4, 3, 2, 1};
-
         printArray(evenExample);
-        printArray(Example.reverse(evenExample));
+        Example.reverse(evenExample);
+        printArray(evenExample);
 
+        int[] oddExample = {4, 3, 2, 1};
         printArray(oddExample);
-        printArray(Example.reverse(oddExample));
+        Example.reverse(oddExample);
+        printArray(oddExample);
     }
 
     private static void printArray(int[] reverse) {
