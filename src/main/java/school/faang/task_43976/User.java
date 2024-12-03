0bef7e25894285f19c6b38d251d05d53eb0b0044
NiveshOne <Shishov.44@mail.ru>
2024-12-04 00:08:56 +0300

    Создан класс User с необходимыми полями Реализован метод groupUsers, возвращающий интерфейс "возраст - список пользователей" Реализован вывод в консоль

---
+++ src/main/java/school/faang/task_43976/User.java
@@ -0,0 +1,33 @@
+package school.faang.task_43976;
+
+import lombok.*;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+
+@Getter
+@AllArgsConstructor
+@ToString
+public class User {
+
+    private final String name;
+    private final int age;
+    private final String workplace;
+    private final String address;
+
+    public static Map<Integer, List<User>> groupUsers(List<User> listOfUsers){
+        Map<Integer, List<User>> groupingUsers = new HashMap<>();
+
+        for(User user : listOfUsers){
+            int ageUser = user.getAge();
+            if (!groupingUsers.containsKey(ageUser)) {
+                groupingUsers.put(ageUser, new ArrayList<>());
+            }
+            groupingUsers.get(ageUser).add(user);
+        }
+
+        return groupingUsers;
+    }
+}
