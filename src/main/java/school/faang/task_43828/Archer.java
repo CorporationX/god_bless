b9f6e4f2cd7d6ad61bd072fd2a4ec11ee22ea2bd
Eugene <@evgeniy_kim>
2024-12-03 12:14:27 +0500

    task BJS2-43693

---
+++ src/main/java/school/faang/task_43828/Archer.java
@@ -0,0 +1,12 @@
+package school.faang.task_43828;
+
+public class Archer extends Character {
+    public Archer(String name) {
+        super(name, 3, 10, 5);
+    }
+
+    @Override
+    public void attack(Character opponent) {
+        opponent.setHealth(opponent.getHealth() - this.agility);
+    }
+}
