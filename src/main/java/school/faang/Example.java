7bdbb6a341d46dfb73a386a1c78cd870faf2c92d
Artur Berdnikov <prikolovichp@inbox.ru>
2024-12-03 20:10:52 +0300

    task-43606

--- src/main/java/school/faang/Example.java
+++ src/main/java/school/faang/Example.java
@@ -11,11 +11,11 @@ public class Example {
         reverse(nums);
     }
 
-    public static void reverse(int[] nums){
-        if(nums.length == 0){
-                    System.out.println("Массив пуст");
+    public static void reverse(int[] nums) {
+        if (nums.length == 0) {
+            System.out.println("Массив пуст");
         } else {
-            for(int i = 0; i < nums.length / 2; i++){
+            for (int i = 0; i < nums.length / 2; i++) {
                 int temp = nums[i];
                 nums[i] = nums[nums.length - i - 1];
                 nums[nums.length - i - 1] = temp;
