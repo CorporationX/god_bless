91f4b6bfa56ac48241df223e36af51613a59f12f
Dmitriy <dda20040609@gmail.com>
2024-12-03 22:58:20 +0300

    commit 3 Register User rename package and operator

--- src/main/java/school/faang/task43636reverse/Example.java
+++ src/main/java/school/faang/task43636reverse/Example.java
@@ -3,18 +3,15 @@ package school.faang.task43636reverse;
 public class Example {
 
     public static void reverse(int[] nums) {
-        if (nums.length == 0) {
+        if (nums == null || nums.length == 0) {
             System.out.println("Пустой массив");
             return;
         }
         int temp;
-
         for (int i = 0; i < nums.length / 2; i++) {
             int tmp = nums[i];
             nums[i] = nums[nums.length - i - 1];
             nums[nums.length - i - 1] = tmp;
-
         }
-
     }
 }
