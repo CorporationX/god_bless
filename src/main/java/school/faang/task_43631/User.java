f0cfd8f55528e67e7f95b124148b24669ec9001d
ArzamaS Ilya <coldoff@inbox.ru>
2024-12-05 00:51:29 +0300

    Вроде исправил структуру веток в Git :)

--- src/main/java/school/faang/task_43631/User.java
+++ src/main/java/school/faang/task_43631/User.java
@@ -20,12 +20,12 @@ public class User {
 
     public static Map<Integer, List<User>> groupUsers(List<User> userList) {
         Map<Integer, List<User>> result = new HashMap<>();
-            for (User user : userList){
-                if (!result.containsKey(user.getAge())) {
-                    result.put(user.getAge(), new ArrayList<>());
-                }
-                result.get(user.getAge()).add(user);
+        for (User user : userList) {
+            if (!result.containsKey(user.getAge())) {
+                result.put(user.getAge(), new ArrayList<>());
             }
+            result.get(user.getAge()).add(user);
+        }
         return result;
     }
 }
