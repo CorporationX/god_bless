34fdf2a4907c4c10e4a4ea42438f466799478c48
Maksim-VV <laulmv11@gmail.com>
2024-12-04 16:58:45 +0300

    user registration - after the checkstyle

--- src/main/java/school/faang/Main.java
+++ src/main/java/school/faang/Main.java
@@ -3,8 +3,9 @@ package school.faang;
 public class Main {
     public static void main(String[] args) {
 
-//  for test after checking it, you can delete it
+//      for test after checking it, you can delete it
         new User("Agnetha", 20, "Google", "London");
+
 //        new User("Bob", 25, "", "London");
 //        new User("Tom", 25, null, "London");
 //        new User("Roman", 24, "Google", "");
