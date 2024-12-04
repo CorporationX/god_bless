986a370aa9d60dab6267862e43f76e764ed0df1e
Maksim-VV <laulmv11@gmail.com>
2024-12-04 16:45:15 +0300

    user registration - alpha release

--- src/main/java/school/faang/Main.java
+++ src/main/java/school/faang/Main.java
@@ -1,6 +1,19 @@
 package school.faang;
 
 public class Main {
+    public static void main(String[] args) {
+
+//  for test after checking it, you can delete it
+        new User("Agnetha", 20, "Google", "London");
+//        new User("Bob", 25, "", "London");
+//        new User("Tom", 25, null, "London");
+//        new User("Roman", 24, "Google", "");
+//        new User("Anni", 23, "Google", null);
+//        new User("Bill", 5, "Google", "London");
+//        new User(null, 22, "Google", "London");
+
+        System.out.println("Finish!");
+    }
 
 
 }
---
+++ src/main/java/school/faang/User.java
@@ -0,0 +1,66 @@
+package school.faang;
+
+
+import lombok.Data;
+import java.util.Set;
+
+@Data
+public class User {
+
+    private String name;
+    private int age;
+    private String job;
+    private String address;
+
+    public static final Set<String> VALID_JOBS = Set.of(
+            "Google",
+            "Uber",
+            "Amazon"
+    );
+
+    public static final Set<String> VALID_ADDRESSES  = Set.of(
+            "London",
+            "New York",
+            "Amsterdam"
+    );
+
+    public User(String name, int age, String job, String address) {
+        validateName(name);
+        validateAge(age);
+        validateJob(job);
+        validateAddress(address);
+
+        this.name = name;
+        this.age = age;
+        this.job = job;
+        this.address = address;
+    }
+
+    private void validateName(String name) {
+        if (name == null || name.isBlank()) {
+            throw new IllegalArgumentException("Name cannot be null or empty!");
+        }
+    }
+
+    private void validateAge(int age) {
+        if (age < 18) {
+            throw new IllegalArgumentException("The age must be over 18!");
+        }
+    }
+
+    private void validateJob(String job) {
+        if (job == null || !isPropertyExists(job, VALID_JOBS)) {
+            throw new IllegalArgumentException("Invalid job!");
+        }
+    }
+
+    private void validateAddress(String address) {
+        if (address == null || !isPropertyExists(address, VALID_ADDRESSES)) {
+            throw new IllegalArgumentException("Invalid address!");
+        }
+    }
+
+    private boolean isPropertyExists(String value, Set<String> list) {
+        return list.contains(value);
+    }
+}
