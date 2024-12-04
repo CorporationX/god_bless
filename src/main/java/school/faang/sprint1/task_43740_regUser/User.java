e3cf4718c7bc6685f70700ad22dfe244ba63fcf3
Maxim Maslov <vint_vin@mail.ru>
2024-12-04 21:19:07 +1000

    Создание конструктора для пользователя

--- src/main/java/school/faang/sprint1/task_43740_regUser/User.java
+++ src/main/java/school/faang/sprint1/task_43740_regUser/User.java
@@ -1,4 +1,25 @@
 package school.faang.sprint1.task_43740_regUser;
 
+import java.util.Arrays;
+import java.util.List;
+
 public class User {
+    private String name;
+    private Integer age;
+    private String job;
+    private String address;
+
+    public final List<String> VALID_JOBS = Arrays.asList("Google", "Uber", "Amazon");
+    public final List<String> VALID_ADDRESSES = Arrays.asList("London", "New York", "Amsterdam");
+
+    public User(String name, Integer age, String job, String address) {
+        if (name != null && age >= 18 && VALID_JOBS.contains(job) && VALID_ADDRESSES.contains(address)) {
+            this.name = name;
+            this.age = age;
+            this.job = job;
+            this.address = address;
+        } else {
+            throw new IllegalArgumentException();
+        }
+    }
 }
