ab04ace6aebc2fb730dc10173dbf355dbe147273
VladimirGontarenko <mister.gontarenko@yandex.ru>
2024-12-04 12:47:10 +0400

    task_BJS2-43635 проверка на null и пустоту массива

--- src/main/java/school/faang/Example.java
+++ src/main/java/school/faang/Example.java
@@ -3,10 +3,16 @@ package school.faang;
 public class Example {
     public static void revers(int[] nums) {
         int temp;
-        for (int i = 0; i < nums.length / 2; i++) {
-            temp = nums[i];
-            nums[i] = nums[nums.length - 1 - i];
-            nums[nums.length - 1 - i ] = temp;
+        if (nums == null) {
+            System.out.println(" nums is null .");
+        } else if (nums.length == 0) {
+            System.out.println("nums is empty .");
+        } else {
+            for (int i = 0; i < nums.length / 2; i++) {
+                temp = nums[i];
+                nums[i] = nums[nums.length - 1 - i];
+                nums[nums.length - 1 - i] = temp;
+            }
         }
     }
 }
