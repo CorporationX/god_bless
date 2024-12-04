8d57842d53194b4ba594059a57a8991ae8d487b4
askar0103 <ananaspineapple2048@gmail.com>
2024-12-04 15:31:48 +0300

    Solution to task BJS2-43933

--- src/main/java/school/faang/task_43933/User.java
+++ src/main/java/school/faang/task_43933/User.java
@@ -10,11 +10,7 @@ public record User(String name, int age, String workplace, String address) {
     public static Map<Integer, List<User>> groupUsers(List<User> users) {
         Map<Integer, List<User>> map = new HashMap<>();
         for (User user : users) {
-            if (map.get(user.age()) != null) {
-                map.get(user.age()).add(user);
-            } else {
-                map.put(user.age(), new ArrayList<>(List.of(user)));
-            }
+            map.computeIfAbsent(user.age, key -> new ArrayList<>()).add(user);
         }
 
         return map;
