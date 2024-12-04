846f840f4e6631f5f986e337f4120eda0863475d
stepanevangulov <167680047+stepanevangulov@users.noreply.github.com>
2024-12-04 08:46:34 +0200

    User.java

--- src/main/java/school/faang/task_43503/User.java
+++ src/main/java/school/faang/task_43503/User.java
@@ -1,25 +1,4 @@
-package school.faang.task_43503;
-
-import lombok.AllArgsConstructor;
-import lombok.Data;
-
-import java.util.List;
-import java.util.Map;
-import java.util.stream.Collectors;
-
-@Data
-@AllArgsConstructor
-public class User {
-    private String name;
-    private int age;
-    private String workplace;
-    private String address;
-
-    public static Map<Integer, List<User>> groupUsers(List<User> users) {
-        return users.stream().collect(Collectors.groupingBy(User::getAge));
-    }
-
-    public static void main(String[] args) {
+public static void main(String[] args) {
         List<User> users = List.of(
                 new User("Tom", 25, "Corp 1", "Adress A"),
                 new User("Sofia", 18, "Corp 2", "Adress B"),
