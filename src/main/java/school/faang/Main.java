6965b73b2c0bdcad09caca1f501e149823b3a584
VladimirGontarenko <mister.gontarenko@yandex.ru>
2024-12-04 15:23:32 +0400

    task_BJS2-43726

--- src/main/java/school/faang/Main.java
+++ src/main/java/school/faang/Main.java
@@ -1,4 +1,12 @@
 package school.faang;
 
 public class Main {
+    public static void main(String[] args) {
+        try {
+            User user = new User("Ivan", 18, "Google", "London");
+            user.setAge(5);
+        } catch (Exception e){
+            System.out.println(e.getMessage());
+        }
+    }
 }
---
+++ src/main/java/school/faang/User.java
@@ -0,0 +1,55 @@
+package school.faang;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.Set;
+
+@Getter
+public class User {
+    private static final Set<String> VALID_JOBS = Set.of("Google", "Uber", "Amazon");
+    private static final Set<String> VALID_ADDRESSES = Set.of("London", "New York", "Amsterdam");
+
+    private static final int MIN_AGE = 18;
+    private String name;
+    private int age;
+    private String job;
+    private String address;
+
+    public void setName(String name) {
+        if (name.isEmpty()) {
+            throw new IllegalArgumentException("name cannot be empty ");
+        }
+        this.name = name;
+    }
+
+    public void setAge(int age) {
+        if (age < MIN_AGE) {
+            throw new IllegalArgumentException("age cannot be less " + MIN_AGE);
+        }
+        this.age = age;
+    }
+
+    public void setJob(String job) {
+        if (VALID_JOBS.contains(job) == false) {
+            throw new IllegalArgumentException("job is not valid ");
+        }
+        this.job = job;
+    }
+
+    public void setAddress(String address) {
+        if (VALID_ADDRESSES.contains(address) == false) {
+            throw new IllegalArgumentException("address is not valid");
+        }
+        this.address = address;
+    }
+
+    public User(String name, int age, String job, String address) {
+
+        this.setName(name);
+        this.setAge(age);
+        this.setJob(job);
+        this.setAddress(address);
+
+    }
+}
