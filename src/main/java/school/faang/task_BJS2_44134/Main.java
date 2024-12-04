428aaeeac3a281fb51f69a24dda9cc0e19af600f
Maxim Dorofeev <maxim.dp.dorofeev@gmail.com>
2024-12-04 03:28:54 +0300

    Fix problem commit

--- src/main/java/school/faang/task_BJS2_44134/Main.java
+++
@@ -1,7 +0,0 @@
-package school.faang.task_BJS2_44134;
-
-public class Main {
-    public static void main(String[] args) {
-
-    }
-}
---
+++ src/main/java/school/faang/task_bjs2_44134/Main.java
@@ -0,0 +1,7 @@
+package school.faang.task_bjs2_44134;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+}
