59657ee1ca4c673d9cf61a8f88a6f04f1167c77b
Vadim Ovcharenko <vadimovcharenko12@gmail.com>
2024-12-04 17:55:12 +0300

    else if заменен на if в классе User. В классе Main написаны блоки try-catch для проверки

--- src/main/java/school/faang/task_43690/Main.java
+++ src/main/java/school/faang/task_43690/Main.java
@@ -3,20 +3,48 @@ package school.faang.task_43690;
 public class Main {
     public static void main(String[] args) {
 
-        // Корректно
-        User user1 = new User("Vadim", 18, "Uber", "Amsterdam");
+        try {
+            // Корректно
+            User user1 = new User("Vadim", 18, "Uber", "Amsterdam");
+        } catch (IllegalArgumentException e) {
+            System.out.println("Error: " + e.getMessage());
+        }
 
-        // Получим value exception: Name cannot be empty
-        User user21 = new User("", 18, "Uber", "Amsterdam");
-        User user22 = new User(null, 18, "Uber", "Amsterdam");
+        try {
+            // Получим value exception: Name cannot be empty
+            User userWithEmptyName = new User("", 18, "Uber", "Amsterdam");
+        } catch (IllegalArgumentException e) {
+            System.out.println("Error: " + e.getMessage());
+        }
 
-        // Получим value exception: Age cannot be less than 18
-        User user3 = new User("Vadim", 0, "Uber", "Amsterdam");
+        try {
+            User userNullInAge = new User(null, 18, "Uber", "Amsterdam");
+        } catch (IllegalArgumentException e) {
+            System.out.println("Error: " + e.getMessage());
+        }
 
-        // Получим value exception: This work is not on the list of allowed
-        User user4 = new User("Vadim", 18, "KFC", "Amsterdam");
+        try {
+            User userAgeLessThanEighteen = new User("Vadim", 0, "Uber", "Amsterdam");
 
-        // Получим value exception: The address is not listed in the allowed list
-        User user5 = new User("Vadim", 18, "Uber", "Krasnodar");
+        } catch (IllegalArgumentException e) {
+            System.out.println("Error: " + e.getMessage());
+        }
+
+        try {
+
+            // Получим value exception: This work is not on the list of allowed
+            User userWithTheRightJob = new User("Vadim", 18, "KFC", "Amsterdam");
+
+        } catch (IllegalArgumentException e) {
+            System.out.println("Error: " + e.getMessage());
+        }
+
+        try {
+            // Получим value exception: The address is not listed in the allowed list
+            User userWithTheWrongAddress = new User("Vadim", 18, "Uber", "Krasnodar");
+        } catch (IllegalArgumentException e) {
+            System.out.println("Error: " + e.getMessage());
+        }
+
     }
 }
--- src/main/java/school/faang/task_43690/User.java
+++ src/main/java/school/faang/task_43690/User.java
@@ -14,19 +14,19 @@ public class User {
     public User(String name, int age, String job, String address) throws IllegalArgumentException {
         if (name == null || name.isEmpty()) {
             throw new IllegalArgumentException("Name cannot be empty");
-        } else if (age < 18) {
-            throw new IllegalArgumentException("Age cannot be less than 18");
-        } else if (!VALID_JOBS.contains(job)) {
-            throw new IllegalArgumentException("This work is not on the list of allowed");
-        } else if (!VALID_ADDRESSES.contains(address)) {
-            throw new IllegalArgumentException("The address is not listed in the allowed list");
-        } else {
-            this.name = name;
-            this.age = age;
-            this.job = job;
-            this.address = address;
         }
-
-
+        if (age < 18) {
+            throw new IllegalArgumentException("Age cannot be less than 18");
+        }
+        if (!VALID_JOBS.contains(job)) {
+            throw new IllegalArgumentException("This work is not on the list of allowed");
+        }
+        if (!VALID_ADDRESSES.contains(address)) {
+            throw new IllegalArgumentException("The address is not listed in the allowed list");
+        }
+        this.name = name;
+        this.age = age;
+        this.job = job;
+        this.address = address;
     }
 }
