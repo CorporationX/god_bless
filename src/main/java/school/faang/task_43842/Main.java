44b137e7dd65161155c83c05d2a517c8775bd297
ANaydyonov <alexander.naydenov@23software.net>
2024-12-05 00:41:52 +0100

    created separated files

--- src/main/java/school/faang/task_43842/Main.java
+++ src/main/java/school/faang/task_43842/Main.java
@@ -1,31 +1,5 @@
 package school.faang.task_43842;
 
-class Warrior extends Character {
-  public Warrior(String name) {
-    super(name, 10, 5, 3);
-  }
-
-  @Override
-  public void attack(Character target) {
-    int damage = getStrength();
-    target.setHealth(target.getHealth() - damage);
-    System.out.println(getName() + " (Warrior) attacked " + target.getName() + " and inflicts " + damage + " damage");
-  }
-}
-
-class Archer extends Character {
-  public Archer(String name) {
-    super(name, 3, 10, 5);
-  }
-
-  @Override
-  public void attack(Character target) {
-    int damage = getAgility();
-    target.setHealth(target.getHealth() - damage);
-    System.out.println(getName() + " (Archer) attacked " + target.getName() + " and inflicts " + damage + " damage");
-  }
-}
-
 
 public class Main {
   public static void main(String[] args) {
