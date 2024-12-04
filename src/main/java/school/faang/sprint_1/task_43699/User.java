d15d2796493d233fdc4c7625b29c3ab4e7905ef2
tatgricenko <tattgricenko@gmail.com>
2024-12-04 16:24:58 +0300

    Add const MAJORUTY_AGE and func validateUser()

--- src/main/java/school/faang/sprint_1/task_43699/User.java
+++ src/main/java/school/faang/sprint_1/task_43699/User.java
@@ -6,6 +6,7 @@ import java.util.Set;
 
 @ToString
 public class User {
+    private static final int MAJORITY_AGE = 18;
     private static final Set<String> VALID_JOBS = Set.of("Google", "Uber", "Amazon");
     private static final Set<String> VALID_ADDRESSES = Set.of("London", "New York", "Amsterdam");
 
@@ -15,25 +16,26 @@ public class User {
     private String address;
 
     public User(String name, int age, String job, String address) {
-        if (name.isEmpty()) {
-            throw new IllegalArgumentException("The user name cannot be empty");
-        }
-
-        if (age < 18) {
-            throw new IllegalArgumentException("The user's age cannot be less than 18");
-        }
-
-        if (!VALID_JOBS.contains(job)) {
-            throw new IllegalArgumentException("Invalid job");
-        }
-
-        if (!VALID_ADDRESSES.contains(address)) {
-            throw new IllegalArgumentException("Invalid address");
-        }
+        validateUser(name, age, job, address);
 
         this.name = name;
         this.age = age;
         this.job = job;
         this.address = address;
+    }
+
+    private void validateUser(String name, int age, String job, String address) {
+        if (name.isEmpty()) {
+            throw new IllegalArgumentException("The user name cannot be empty");
+        }
+        if (age < MAJORITY_AGE) {
+            throw new IllegalArgumentException("The user's age cannot be less than " + MAJORITY_AGE);
+        }
+        if (!VALID_JOBS.contains(job)) {
+            throw new IllegalArgumentException("Invalid job");
+        }
+        if (!VALID_ADDRESSES.contains(address)) {
+            throw new IllegalArgumentException("Invalid address");
+        }
     }
 }
