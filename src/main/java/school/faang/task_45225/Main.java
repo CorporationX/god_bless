38f72f83757014022fe67f57e8e6b23196912263
Maria Bortsova <bortsovamariia@gmail.com>
2024-12-04 15:54:26 +0300

    fix checkstyle

--- src/main/java/school/faang/task_45225/Main.java
+++ src/main/java/school/faang/task_45225/Main.java
@@ -38,64 +38,56 @@ public class Main {
 
     private static Map<Student, Map<Subject, Integer>> initSubjectsAndGradeByStudent() {
         return new HashMap<>() {{
-            put(new Student(1, "Vasya"), new HashMap<>() {
-                {
-                    put(new Subject(1, "Math"), 5);
-                    put(new Subject(2, "Physics"), 4);
-                }
-            });
-            put(new Student(2, "Petya"), new HashMap<>() {
-                {
-                    put(new Subject(1, "Math"), 3);
-                    put(new Subject(3, "Russian"), 4);
-                    put(new Subject(4, "Literature"), 4);
-                }
-            });
-            put(new Student(3, "Alina"), new HashMap<>() {
-                {
-                    put(new Subject(1, "Math"), 5);
-                    put(new Subject(2, "Physics"), 5);
-                    put(new Subject(5, "Technology"), 5);
-                }
-            });
-        }};
+                put(new Student(1, "Vasya"), new HashMap<>() {{
+                        put(new Subject(1, "Math"), 5);
+                        put(new Subject(2, "Physics"), 4);
+                    }
+                });
+                put(new Student(2, "Petya"), new HashMap<>() {{
+                        put(new Subject(1, "Math"), 3);
+                        put(new Subject(3, "Russian"), 4);
+                        put(new Subject(4, "Literature"), 4);
+                    }
+                });
+                put(new Student(3, "Alina"), new HashMap<>() {{
+                        put(new Subject(1, "Math"), 5);
+                        put(new Subject(2, "Physics"), 5);
+                        put(new Subject(5, "Technology"), 5);
+                    }
+                });
+            }};
     }
 
     private static Map<Subject, List<Student>> initStudentsBySubject() {
         return new HashMap<>() {{
-            put(new Subject(1, "Math"),
-                    new ArrayList<>() {
-                        {
+                put(new Subject(1, "Math"),
+                        new ArrayList<>() {{
                             add(new Student(1, "Vasya"));
                             add(new Student(2, "Petya"));
                             add(new Student(3, "Alina"));
                         }
                     });
-            put(new Subject(2, "Physics"),
-                    new ArrayList<>() {
-                        {
+                put(new Subject(2, "Physics"),
+                        new ArrayList<>() {{
                             add(new Student(1, "Vasya"));
                             add(new Student(3, "Alina"));
                         }
                     });
-            put(new Subject(3, "Russian"),
-                    new ArrayList<>() {
-                        {
+                put(new Subject(3, "Russian"),
+                        new ArrayList<>() {{
                             add(new Student(2, "Petya"));
                         }
                     });
-            put(new Subject(4, "Literature"),
-                    new ArrayList<>() {
-                        {
+                put(new Subject(4, "Literature"),
+                        new ArrayList<>() {{
                             add(new Student(2, "Petya"));
                         }
                     });
-            put(new Subject(5, "Technology"),
-                    new ArrayList<>() {
-                        {
+                put(new Subject(5, "Technology"),
+                        new ArrayList<>() {{
                             add(new Student(3, "Alina"));
                         }
                     });
-        }};
+            }};
     }
 }
