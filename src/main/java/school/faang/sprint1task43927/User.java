59041151d5fed1baa8807734ceee749e117eb94d
elnur-elnur <tahirzadee@gmail.com>
2024-12-05 00:19:59 +0400

    Refactoring v4 after code review

--- src/main/java/school/faang/sprint1task43927/User.java
+++ src/main/java/school/faang/sprint1task43927/User.java
@@ -1,21 +1,21 @@
 package school.faang.sprint1task43927;
 
 import lombok.Getter;
-import lombok.Setter;
+import lombok.ToString;
 
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+@Getter
+@ToString
 public class User {
+
     private String name;
-    @Setter
-    @Getter
     private int age;
     private String workplace;
     private String address;
-    private static final List<String> user = new ArrayList<>();
 
     public User(String name, int age, String workplace, String address) {
         this.name = name;
@@ -26,32 +26,14 @@ public class User {
 
     public static Map<Integer, List<User>> groupUsers(List<User> users) {
         Map<Integer, List<User>> map = new HashMap<>();
-        for (User user : users) {
-            if (map.containsKey(user.getAge())) {
-                List<User> previous = map.get(user.getAge());
-                previous.add(user);
-                map.put(user.getAge(), previous);
-            } else {
-                List<User> newList = new ArrayList<>();
-                newList.add(user);
-                map.put(user.getAge(), newList);
+        for (User currentUser : users) {
+            int age = currentUser.getAge();
+            if (!map.containsKey(currentUser.getAge())) {
+                map.put(age, new ArrayList<>());
             }
+            map.get(age).add(currentUser);
         }
         return map;
     }
 
-    @Override
-    public String toString() {
-        return "User{"
-                +
-                "name='" + name + '\''
-                +
-                ", age=" + age
-                +
-                ", workplace='" + workplace + '\''
-                +
-                ", address='" + address + '\''
-                +
-                '}';
-    }
 }
