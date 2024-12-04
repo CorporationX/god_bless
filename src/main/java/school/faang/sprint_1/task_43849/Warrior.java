ad88ed7d615dd359232d3d8378eddb84dfe974a3
tatgricenko <tattgricenko@gmail.com>
2024-12-04 22:54:16 +0300

    Fix checkstyle error

--- src/main/java/school/faang/sprint_1/task_43849/Warrior.java
+++ src/main/java/school/faang/sprint_1/task_43849/Warrior.java
@@ -15,6 +15,6 @@ public class Warrior extends Character {
 
     @Override
     public void attack(Character character) {
-       character.health -= strength;
+        character.health -= strength;
     }
 }
