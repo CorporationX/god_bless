14157527d4af541f2f078ca5a48dd8a5ab147837
maxdrattcev <adratcev@mail.ru>
2024-12-04 16:34:31 +0300

    BJS2-45283:Библиотека Вестероса

---
+++ src/main/java/school/faang/task_45283/LibrarySystem.java
@@ -0,0 +1,23 @@
+package school.faang.task_45283;
+
+import java.util.Map;
+
+public class LibrarySystem {
+    public static void addBook(String title, String author, int year, String location) {
+        Main.getBooks().put(new Book(title,author,year), location);
+    }
+    public static boolean removeBook(String title, String author, int year){
+        Book book = new Book(title, author, year);
+        return Main.getBooks().remove(book) != null;
+    }
+    public static String findBook (String title, String author, int year){
+        Book book = new Book(title, author, year);
+        return Main.getBooks().getOrDefault(book, "Данная книга отсутсвует");
+    }
+    public static void printAllBooks(){
+        for (Map.Entry<Book, String> entry : Main.getBooks().entrySet()){
+            System.out.println("Книга " + entry.getKey().getTitle() + ", автор " + entry.getKey().getAuthor() +
+                    ", год издания " + entry.getKey().getYear() + " - лежит на полке №" + entry.getValue());
+        }
+    }
+}
---
+++ src/main/java/school/faang/task_45283/Main.java
@@ -0,0 +1,24 @@
+package school.faang.task_45283;
+
+import lombok.Getter;
+import java.util.HashMap;
+import java.util.Map;
+
+public class Main {
+    @Getter
+    private static Map<Book, String> books = new HashMap<>();
+
+    public static void main(String[] args) {
+        books.put(new Book("Тихий дон", "Шелохов", 1932), "1");
+        books.put(new Book("Треумфальная арка", "Ремарк", 1939), "2");
+        books.put(new Book("Война и Мир", "Толстой", 1869), "3");
+        books.put(new Book("Мастер и Маргарита", "Булгаков", 1928), "4");
+        books.put(new Book("Капитанская дочка", "Пушкин", 1830), "5");
+        books.put(new Book("Мертвые души", "Гоголь", 1842), "6");
+
+        LibrarySystem.addBook("Вечера на хуторе", "Гоголь", 1863, "7");
+        LibrarySystem.removeBook("Треумфальная арка", "Ремарк", 1939);
+        System.out.println(LibrarySystem.findBook("Мастер и Маргарита", "Булгаков", 1928));
+        LibrarySystem.printAllBooks();
+    }
+}
