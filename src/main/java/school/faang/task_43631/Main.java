97f6340497a0f166cbe9a81b1be37f2d33e18e37
ArzamaS Ilya <coldoff@inbox.ru>
2024-12-05 00:37:21 +0300

    Реализовал метод группировки

--- src/main/java/school/faang/task_43631/Main.java
+++ src/main/java/school/faang/task_43631/Main.java
@@ -2,9 +2,7 @@ package school.faang.task_43631;
 
 import java.util.ArrayList;
 import java.util.Collections;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
 import static school.faang.task_43631.User.groupUsers;
 
@@ -14,18 +12,12 @@ public class Main {
         User secondUser = new User("Samanta", 15, "Google", "Florida");
         User thirdUser = new User("Arnold", 44, "Tik-Tok", "Home");
         User fourthUser = new User("Silvester", 56, "VK", "Derevnya");
-        User fivesUser = new User("Laqvanda", 25, "Yandex", "Zelepupinsk");
+        User fivesUser = new User("Laqvanda", 44, "Yandex", "Zelepupinsk");
 
         List<User> userList = new ArrayList<>();
         Collections.addAll(userList, firstUser, secondUser, thirdUser, fourthUser, fivesUser);
 
-        groupUsers(userList);
-
-        HashMap<Integer, List<User>> hashMapUser = User.groupUsers(userList);
-
-        for (Map.Entry<Integer, List<User>> entry : hashMapUser.entrySet()) {
-            System.out.println(entry.getKey());
-            System.out.println(entry.getValue());
-        }
+        System.out.println(groupUsers(userList));
 
     }
+}
--- src/main/java/school/faang/task_43631/User.java
+++ src/main/java/school/faang/task_43631/User.java
@@ -1,25 +1,33 @@
 package school.faang.task_43631;
 
 import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.ToString;
 
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
 @AllArgsConstructor
+@Getter
+@ToString
 public class User {
     private String name;
     private int age;
     private String workplace;
     private String addres;
 
-    public static Map<Integer, List<User>> groupUsers(List<User> userList){
-
+    public static Map<Integer, List<User>> groupUsers(List<User> userList) {
+        Map<Integer, List<User>> result = new HashMap<>();
+            for (User user : userList){
+                if (!result.containsKey(user.getAge())) {
+                    result.put(user.getAge(), new ArrayList<>());
+                }
+                result.get(user.getAge()).add(user);
+            }
+        return result;
     }
-    @Override
-    public String toString() {
-        return "User{"
-                + "name='" + name + '\''
-                + ", age=" + age + '\''
-                + ", workplace='" + workplace + '\''
-                + ", address='" + address + '\'' + '}';
 }
+
+
