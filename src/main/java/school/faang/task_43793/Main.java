90648869b4b13ee61315011f1549e4baa093cecb
vvz3zzv <vladislav.v.zhurbin@gmail.com>
2024-12-04 19:18:50 +0300

    BJS2-43793: Регистрация пользователя
    - в классе User заданы сеттеры, валидирующие входные параметры

---
+++ src/main/java/school/faang/task_43793/Main.java
@@ -0,0 +1,8 @@
+package school.faang.task_43793;
+
+public class Main {
+    public static void main(String[] args) {
+        User john = new User("John", 18, "Uber", "Amsterdam");
+        System.out.println(john);
+    }
+}
