4a394c1e62f9dfe4f66a512cb206d3fe1cb6c18b
az3l1t <eternity_cr9p@mail.ru>
2024-12-04 19:55:45 +0300

    BJS2-45228 Двойной кэш-кэш Переделал по рекомендациям!

--- src/main/java/school/faang/task_43822/Character.java
+++ src/main/java/school/faang/task_43822/Character.java
@@ -4,17 +4,23 @@ import lombok.Data;
 
 @Data
 public abstract class Character {
-    private String name;
-    private Integer power;
-    private Integer stamina;
-    private Integer iq;
-    private Integer health = 100;
+    protected String name;
+    protected Integer power;
+    protected Integer stamina;
+    protected Integer iq;
+    protected Integer health = 100;
 
     public Character(String name) {
+        if (name == null) {
+            throw new IllegalArgumentException("Name can`t be null");
+        }
         this.name = name;
     }
 
     public Character(String name, Integer power, Integer stamina, Integer iq) {
+        if (name == null) {
+            throw new IllegalArgumentException("Name can`t be null");
+        }
         this.name = name;
         this.power = power;
         this.stamina = stamina;
@@ -23,15 +29,4 @@ public abstract class Character {
 
     public abstract void attack(Character c);
 
-
-    @Override
-    public String toString() {
-        return "Character{"
-                + "name='" + name + '\''
-                + ", power=" + power
-                + ", stamina=" + stamina
-                + ", iq=" + iq
-                + ", health=" + health
-                + '}';
-    }
 }
