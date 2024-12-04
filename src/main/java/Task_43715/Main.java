c25f6731395fcbf0c1dd87e658f90d98f76b5ed1
Artur Berdnikov <prikolovichp@inbox.ru>
2024-12-04 20:01:08 +0300

    task-43606

---
+++ src/main/java/Task_43715/Main.java
@@ -0,0 +1,16 @@
+package Task_43715;
+
+public class Main {
+    public static void main(String[] args){
+        try {
+            User user1 = new User("Artur", 25, "Uber","London");
+            System.out.println(user1);
+
+            User user2 = new User("Ivan", 30, "Amazon", "New York");
+            System.out.println(user2);
+        } catch (IllegalArgumentException e) {
+            System.out.println("Error: " + e.getMessage());
+        }
+
+    }
+}
