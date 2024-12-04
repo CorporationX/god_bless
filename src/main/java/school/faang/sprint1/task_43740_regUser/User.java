e2e0b6bafee085e5e6134094fe627e2bc2803f3f
Maxim Maslov <vint_vin@mail.ru>
2024-12-04 20:58:08 +1000

    Готово

---
+++ src/main/java/school/faang/sprint1/task_43740_regUser/User.java
@@ -0,0 +1,10 @@
+package school.faang.sprint1.task_43740_regUser;
+
+public class User {
+    private String name;
+    private Integer age;
+    private String job;
+    private String address;
+
+
+}
