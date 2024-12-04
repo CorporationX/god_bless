35268d906a1b9074cc67ba4345772565a20484b6
vvz3zzv <vladislav.v.zhurbin@gmail.com>
2024-12-04 19:32:51 +0300

    BJS2-43793: Регистрация пользователя
    - добавлены {} для исправления chekstyle блоков if? написанных в одну строку

--- src/main/java/school/faang/task_43793/User.java
+++ src/main/java/school/faang/task_43793/User.java
@@ -31,17 +31,23 @@ public class User {
     }
 
     public void setAge(int age) {
-        if (age < 18) throw new IllegalArgumentException("Age can't be less than 18!");
+        if (age < 18) {
+            throw new IllegalArgumentException("Age can't be less than 18!");
+        }
         this.age = age;
     }
 
     public void setJob(String job) {
-        if (!VALID_JOBS.contains(job)) throw new IllegalArgumentException();
+        if (!VALID_JOBS.contains(job)) {
+            throw new IllegalArgumentException("Your workplace is not suitable for registration");
+        }
         this.job = job;
     }
 
     public void setAddress(String address) {
-        if (!VALID_ADDRESSES.contains((address))) throw new IllegalArgumentException();
+        if (!VALID_ADDRESSES.contains((address))) {
+            throw new IllegalArgumentException("The address is wrong");
+        }
         this.address = address;
     }
 }
