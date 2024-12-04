c772237af62c75b6b41110138aeae6eb9b820edf
ANaydyonov <alexander.naydenov@23software.net>
2024-12-04 01:59:17 +0100

    added class Main, separated responsibility

--- src/main/java/school/faang/task_43599/Example.java
+++ src/main/java/school/faang/task_43599/Example.java
@@ -18,28 +18,5 @@ public class Example {
             right--;
         }
     }
-
-    private static void printArray(int[] nums) {
-        for (int i = 0; i < nums.length; i++) {
-            System.out.print(nums[i]);
-            if (i < nums.length - 1) {
-                System.out.print(", ");
-            }
-        }
-    }
-
-    public static void main(String[] args) {
-        int[] nums = {1, 2, 3, 4, 5};
-
-        System.out.print("nums=[");
-        printArray(nums);
-        System.out.println("]");
-
-        reverse(nums);
-        System.out.print("reversed nums=[");
-        printArray(nums);
-        System.out.println("]");
-
-    }
 }
 
