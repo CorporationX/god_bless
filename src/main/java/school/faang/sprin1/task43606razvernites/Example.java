27c85d6820b9dbfd11ed073861d67692af52e229
Maxim Maslov <vint_vin@mail.ru>
2024-12-04 22:49:43 +1000

    Код стайл

--- src/main/java/school/faang/sprin1/task43606razvernites/Example.java
+++ src/main/java/school/faang/sprin1/task43606razvernites/Example.java
@@ -18,9 +18,9 @@ public class Example {
 
     public static void printArray(Object array) {
         if (Array.getLength(array) == 0) {
-            System.out.println("Массив не может быть пустым");
+            System.out.println("Массив не может быть пустым!");
         } else if (array == null) {
-            System.out.println("Пустая ссылка");
+            System.out.println("Пустая ссылка!");
         } else {
             System.out.print(array.getClass() + " ");
             int length = Array.getLength(array);
