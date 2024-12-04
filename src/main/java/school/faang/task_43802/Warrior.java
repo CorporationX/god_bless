0d5485b257ed4f63deed18091d31a860587e9374
Timofey Khutoryanskiy <tiroxxxin@gmail.com>
2024-12-04 21:03:09 +0500

    tiroxin BJS2-43802 (инкапсулирована логика атаки в клас Character, переопределена в каждом классе с дополнением, добавлена проверка на то что жив ли атакующий и атакуемый)

--- src/main/java/school/faang/task_43802/Warrior.java
+++ src/main/java/school/faang/task_43802/Warrior.java
@@ -4,6 +4,7 @@ public class Warrior extends Character {
     private static final int DEFAULT_STRENGTH = 10;
     private static final int DEFAULT_AGILITY = 5;
     private static final int DEFAULT_INTELLIGENCE = 3;
+
     public Warrior(String name) {
         super(name, DEFAULT_STRENGTH, DEFAULT_AGILITY, DEFAULT_INTELLIGENCE);
     }
