4707ae6eac9715c3e060f42400410601fa2899c0
lehaps <awda.96@bk.ru>
2024-12-04 12:33:02 +0300

    Добавил несколько исключений Проверил работу программы У Student делается проверка только по id без name

--- src/main/java/school/faang/task45219/Student.java
+++ src/main/java/school/faang/task45219/Student.java
@@ -1,19 +1,36 @@
 package school.faang.task45219;
 
 import lombok.AllArgsConstructor;
-import lombok.EqualsAndHashCode;
 import lombok.Getter;
+
+import java.util.Objects;
 
 @AllArgsConstructor
 @Getter
-@EqualsAndHashCode
 public class Student {
     private int id;
     private String name;
 
     @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        Student student = (Student) o;
+        return id == student.id;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+
+    @Override
     public String toString() {
-        return "Студент: "
+        return "Student: "
                 + "id=" + id
                 + ", name='" + name + '\'';
     }
