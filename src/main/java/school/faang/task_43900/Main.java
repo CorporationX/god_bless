98ca09a004bee0373f72fdd75e3c91b456b5178e
Leonid Naumov <leonidnaumov34@gmail.com>
2024-12-04 23:28:05 +0300

    add user data generator

--- src/main/java/school/faang/task_43900/Main.java
+++ src/main/java/school/faang/task_43900/Main.java
@@ -4,14 +4,19 @@ import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
-        List<User> users = Arrays.asList(
-                new User("Alisa", 22, "Amazon", "Amsterdam"),
-                new User("Cheburek", 27, "Google", "Chebarkul"),
-                new User("Alabama", 22, "Uber", "Cherepovets"),
-                new User("Matroskin", 18, "SpaceX", "Anapa"),
-                new User("How", 27, "Rostics", "Moscow"),
-                new User("Cow", 18, "Farm", "Moon")
-        );
+        String[] testNames = {"Alisa", "Chebarkul", "Cheburek", "Alabama", "Matroskin", "Cow"};
+        String[] testJobs = {"Amazon", "Google", "Uber", "Farm", "SpaceX", "Rostics"};
+        String[] testAddress = {"Amsterdam", "Chebarkul", "Cherepovets", "Anapa", "Moscow", "Moon"};
+
+        Random rand = new Random();
+        List<User> users = new ArrayList<User>();
+        for (int i = 0; i <= 1000; i++) {
+            String name = testNames[rand.nextInt(testNames.length)];
+            String job = testJobs[rand.nextInt(testJobs.length)];
+            String address = testAddress[rand.nextInt(testAddress.length)];
+            int age = rand.nextInt(18,100);
+            users.add(new User(name, age, job, address));
+        }
 
         Map<Integer, List<User>> groupUsersByAge = User.groupUsers(users);
 
