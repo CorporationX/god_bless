684124fce97d649077548033a5f7c4feec26a5c5
Daria <dashok.sok@gmail.com>
2024-12-04 12:11:27 +0300

    Добавлена функциональность класса Book, переопределены equals и hashCode, реализованы методы LibrarySystem

--- src/main/java/school/faang/westeroslibrary/LibrarySystem.java
+++ src/main/java/school/faang/westeroslibrary/LibrarySystem.java
@@ -4,20 +4,23 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class LibrarySystem {
-    private Map<Book, String> books = new HashMap<>();
+    Map<Book, String> books = new HashMap<>();
 
     public void addBook(String title, String author, int year, String location) {
         Book book = new Book(title, author, year);
         books.put(book, location);
     }
+
     public void removeBook(String title, String author, int year) {
         Book book = new Book(title, author, year);
         books.remove(book);
     }
+
     public String findBook(String title, String author, int year) {
         Book book = new Book(title, author, year);
         return books.getOrDefault(book, "Такой книги нет в библиотеке");
     }
+
     public void printAllBooks() {
         for (Map.Entry<Book, String> book : books.entrySet()) {
             System.out.println("Книга " + book.getKey().getTitle() + " находится " + book.getValue());
