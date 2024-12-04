69cb38f9aa2e9c5671be0a4e1efd1cf45f7eddd0
User Novozhilova <nov2303@mail,ru>
2024-12-04 11:11:32 +0500

    BJS2-43829 update after review

--- src/main/java/school/faang/task_43829/Main.java
+++ src/main/java/school/faang/task_43829/Main.java
@@ -2,26 +2,26 @@ package school.faang.task_43829;
 
 public class Main {
     public static void main(String[] args) {
-        Archer archer = new Archer("Archer");
-        Warrior warrior = new Warrior("Warrior");
+        Archer archer = new Archer("Oleg");
+        Warrior warrior = new Warrior("Tim");
 
         while (warrior.health > 0 && archer.health > 0) {
             archer.attack(warrior);
-            System.out.println(String.format("Heath %s after attack %s: %s", warrior.getName(),archer.getName(), warrior.health));
+            System.out.println(String.format("Heath %s after attack: %s", warrior.getName(), warrior.health));
             warrior.attack(archer);
-            if (isDead(archer)){
+            if (isDead(archer)) {
                 break;
             }
-            System.out.println(String.format("Heath %s after attack %s: %s", archer.getName(), warrior.getName(), archer.health));
-            if (isDead(archer)){
-               break;
+            System.out.println(String.format("Heath %s after attack: %s", archer.getName(), archer.health));
+            if (isDead(archer)) {
+                break;
             }
 
         }
     }
 
     private static boolean isDead(Character character) {
-        if (character.isDead()){
+        if (character.isDead()) {
             System.out.println(String.format("%s is dead.", character.getName()));
             return true;
         }
