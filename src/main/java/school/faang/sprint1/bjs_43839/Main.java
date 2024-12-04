99ce440f4ffd746f2a86f10b28e2a7fbe8f4396e
TFdevQA <46944859+TFdevQA@users.noreply.github.com>
2024-12-03 23:35:34 -0800

    task: abstract fighters (checkstyle)

--- src/main/java/school/faang/sprint1/bjs_43839/Main.java
+++ src/main/java/school/faang/sprint1/bjs_43839/Main.java
@@ -1,7 +1,7 @@
 package school.faang.sprint1.bjs_43839;
 
 public class Main {
-    public static void main(String[] args){
+    public static void main(String[] args) {
         Character gamerW = new Warrior("Warrior gamer");
         Character gamerA = new Archer("Archer gamer");
 
