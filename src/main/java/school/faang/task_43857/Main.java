0db339a9dadfe592338b36a86e430dc41f38d767
Leysan Pirkina <missleysan1609@gmail.com>
2024-12-04 23:12:22 +0300

    fixed checkstyle

--- src/main/java/school/faang/task_43857/Main.java
+++ src/main/java/school/faang/task_43857/Main.java
@@ -5,13 +5,7 @@ public class Main {
         Character warrior = new Warrior("alex");
         Character archer = new Archer("alexa");
 
-        Warrior warrior2 = new Warrior("ggg", 2,4,6);
-        Archer archer2 = new Archer("mmm", 3,5,8);
-
         warrior.attack(archer);
         archer.attack(warrior);
-
-        warrior2.attack(archer2);
-        archer2.attack(warrior2);
     }
 }
