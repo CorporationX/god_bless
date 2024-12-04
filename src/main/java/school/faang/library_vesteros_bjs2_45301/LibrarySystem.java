4d6ecebc6bced855abe02115f0457151fffe4c18
Ilya <thomas.moody@mail.ru>
2024-12-04 18:32:46 +0300

    создал метод takeDamage

---
+++ src/main/java/school/faang/library_vesteros_bjs2_45301/LibrarySystem.java
@@ -0,0 +1,36 @@
+package school.faang.library_vesteros_bjs2_45301;
+
+import java.util.Map;
+
+public class LibrarySystem {
+    public static void addBook(String title, String author, int year, String location) {
+        Main.numberShelf.put(new Book(title, author, year), location);
+        System.out.println("Book " + title + " add at shelf: " + location);
+    }
+
+    public static void removeBook(String title, String author, int year) {
+        if (Main.numberShelf.remove(new Book(title, author, year)) != null) {
+            System.out.println("Book " + title + " delete");
+        } else {
+            System.out.println("Book don't find");
+        }
+    }
+
+    public static void findBook(String title, String author, int year) {
+        if (Main.numberShelf.get(new Book(title, author, year)) != null) {
+            System.out.println("Book " + title + " find");
+        } else {
+            System.out.println("Book don't find");
+        }
+    }
+
+    public static void printAllBooks() {
+        if (Main.numberShelf.isEmpty()) {
+            System.out.println("Library is empty");
+        } else {
+            for (Map.Entry<Book, String> entry : Main.numberShelf.entrySet()) {
+                System.out.println(entry.getKey() + " find in " + entry.getValue());
+            }
+        }
+    }
+}
---
+++ src/main/java/school/faang/library_vesteros_bjs2_45301/Main.java
@@ -0,0 +1,24 @@
+package school.faang.library_vesteros_bjs2_45301;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Main {
+    protected static Map<Book, String> numberShelf = new HashMap<>();
+
+    public static void main(String[] args) {
+
+        LibrarySystem.addBook("Annals of the Black Centaur", "Yorkwen", 12, "Shelf A1");
+        LibrarySystem.addBook("The Red Book", "Elkin", 79, "Shelf A2");
+        LibrarySystem.addBook("Flora of the Valyrian Freehold", "Mikellion", 36, "Shelf A3");
+
+        LibrarySystem.printAllBooks();
+
+        LibrarySystem.findBook("The Red Book", "Elkin", 79);
+
+        LibrarySystem.removeBook("The Red Book", "Elkin", 79);
+
+        LibrarySystem.printAllBooks();
+
+    }
+}
