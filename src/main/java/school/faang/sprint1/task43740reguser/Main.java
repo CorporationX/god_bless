bccaa9ca888396aa03708d3609044cc3bb66cdd6
Maxim Maslov <vint_vin@mail.ru>
2024-12-04 23:39:45 +1000

    Исправлено3

---
+++ src/main/java/school/faang/sprint1/task43740reguser/Main.java
@@ -0,0 +1,8 @@
+package school.faang.sprint1.task43740reguser;
+
+public class Main {
+    public static void main(String[] args) throws IllegalArgumentException {
+        User user = new User("Maxim", 19, "Google", "London");
+        System.out.println(user.getName());
+    }
+}
--- src/main/java/school/faang/sprint1/task43740reguser/User.java
+++ src/main/java/school/faang/sprint1/task43740reguser/User.java
@@ -3,20 +3,18 @@ package school.faang.sprint1.task43740reguser;
 import java.util.Arrays;
 import java.util.List;
 
+import lombok.Getter;
+
+@Getter
 public class User {
-    private String name;
-    private Integer age;
-    private String job;
-    private String address;
+    private final String name;
+    private final Integer age;
+    private final String job;
+    private final String address;
 
     public final List<String> validJobs = Arrays.asList("Google", "Uber", "Amazon");
     public final List<String> validAddresses = Arrays.asList("London", "New York", "Amsterdam");
 
-
-    public static void main(String[] args) throws IllegalArgumentException {
-        User user = new User("Maxim", 19, "Google", "London");
-
-    }
 
     public User(String name, Integer age, String job, String address) {
         if (name != null && age >= 18 && validJobs.contains(job) && validAddresses.contains(address)) {
