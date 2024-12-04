7c4e20c5d3cad196205b454df827e4e605bd0b21
salikhdev <salikhdev@gmail.com>
2024-12-04 16:30:39 +0500

    The required classes have been created and tested in the main class.

--- src/main/java/school/faang/bjs243845/Main.java
+++ src/main/java/school/faang/bjs243845/Main.java
@@ -3,7 +3,7 @@ package school.faang.bjs243845;
 public class Main {
     public static void main(String[] args) {
         Archer archer = new Archer("John");
-        Warrior warrior  =new Warrior("Steve");
+        Warrior warrior = new Warrior("Steve");
 
         archer.attack(warrior);
         archer.attack(warrior);
