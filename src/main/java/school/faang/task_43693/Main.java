09503e45fedfd4ab32b50a6562201eb80f1ef5d6
Eugene <@evgeniy_kim>
2024-12-04 09:50:20 +0500

    update

--- src/main/java/school/faang/task_43693/Main.java
+++ src/main/java/school/faang/task_43693/Main.java
@@ -2,7 +2,11 @@ package school.faang.task_43693;
 
 public class Main {
     public static void main(String[] args) {
-        User invalidUser = new User("Sergey Brin", 51, "Google", "California");
-        User validUser = new User("Jeff Bezos", 60, "Amazon", "New York");
+        try {
+            User validUser = new User("Jeff Bezos", 60, "Amazon", "New York");
+            User invalidUser = new User("Sergey Brin", 51, "Google", "California");
+        } catch(IllegalArgumentException e) {
+            throw new IllegalArgumentException(e.getMessage());
+        }
     }
 }
