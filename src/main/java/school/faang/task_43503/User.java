3b03d801a9ecba605922ae284a89005f37b505b1
Stepan Evangulov <stevangulov@gmail.com>
2024-12-04 13:25:35 +0200

    Done

--- src/main/java/school/faang/task_43503/User.java
+++ src/main/java/school/faang/task_43503/User.java
@@ -1,13 +1,13 @@
 package school.faang.task_43503;
 
 import lombok.AllArgsConstructor;
-import lombok.Data;
+import lombok.Getter;
 
 import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
 
-@Data
+@Getter
 @AllArgsConstructor
 public class User {
     private String name;
