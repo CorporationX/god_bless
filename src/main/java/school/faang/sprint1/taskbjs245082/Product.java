5553526e851a7bf69bf8156c1c6af37ed3cfd105
rafissabitov <rafsabqa@gmail.com>
2024-12-04 16:12:35 -0600

    BJS2-45082 disassemble Amazon warehouse

---
+++ src/main/java/school/faang/sprint1/taskbjs245082/Product.java
@@ -0,0 +1,12 @@
+package school.faang.sprint1.taskbjs245082;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@AllArgsConstructor
+@Data
+public class Product {
+    private int id;
+    private String name;
+    private String category;
+}
