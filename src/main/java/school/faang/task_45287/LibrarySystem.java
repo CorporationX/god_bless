bb5a9371665e3f1126b757b076733f7fa577d3c0
triplek <weribel@mail.ru>
2024-12-05 15:31:19 +0300

    Библиотека Вестеросса

--- src/main/java/school/faang/task_45287/LibrarySystem.java
+++ src/main/java/school/faang/task_45287/LibrarySystem.java
@@ -15,12 +15,10 @@ public class LibrarySystem {
         lib.remove(findBook);
         System.out.println("Book " + title + " " + author + " " + year + " if successful removed");
     }
-
     public static void findPolka(String title, String author, int year, HashMap<Book, String> lib) {
         Book findBook = new Book(title, author, year);
         System.out.println("This book is on " + lib.get(findBook));
     }
-
     public static void booksList(HashMap<Book, String> lib) {
         for (Map.Entry<Book, String> el : lib.entrySet()) {
             System.out.println("Book " + el.getKey() + " " + " is on " + el.getValue());
