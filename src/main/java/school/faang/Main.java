0c32bbe4ccb5fe4418403e61f76038f54faa142f
Leonid Zhuravel <zhuravel_leonid@yahoo.com>
2024-12-05 14:00:42 +0200

    Constraints in constructor exercise

--- src/main/java/school/faang/Main.java
+++ src/main/java/school/faang/Main.java
@@ -1,4 +1,33 @@
 package school.faang;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
 public class Main {
+    public static void main(String[] args) {
+
+        List<User> users = new ArrayList<>() {
+            {
+                add(new User(1, "User1", 19,
+                        Stream.of("Sports", "Music", "Reading")
+                                .collect(Collectors.toList())));
+
+
+                add(new User(2, "User2", 21,
+                        Stream.of("Gaming", "Gambling").toList()));
+
+                add(new User(3, "User3", 23,
+                        Stream.of("Gaming", "Music").toList()));
+
+                add(new User(4, "User4", 31,
+                        Stream.of("Hiking", "Gambling").toList()));
+            }
+        };
+
+        List<String> activities = Stream.of("Gaming", "Music").toList();
+
+        User.findHobbyLovers(users, activities);
+    }
 }
