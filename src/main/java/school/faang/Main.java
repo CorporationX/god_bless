98dd43e518b3aeae7c0af1e1d31000b2444bfb47
Anton Graf <ant0ngraf@yandex.ru>
2024-12-05 12:49:47 +0600

    Добавляет класс Main с проверкой персонажей

--- src/main/java/school/faang/Main.java
+++ src/main/java/school/faang/Main.java
@@ -1,4 +1,27 @@
 package school.faang;
 
+import school.faang.bsj_43843.Archer;
+import school.faang.bsj_43843.Character;
+import school.faang.bsj_43843.Warrior;
+
+import java.util.List;
+
 public class Main {
+
+    public static void main(String[] args) {
+        List<Character> characters = List.of(new Warrior("Warrior_1"), new Archer("Archer_1"));
+        System.out.println("Состояния персонажей перед боем:");
+        System.out.println(characters);
+
+        attackCharacters(characters.get(0), characters.get(1));
+        attackCharacters(characters.get(1), characters.get(0));
+    }
+
+    private static void attackCharacters(Character forward, Character defender) {
+        System.out.println("Атакует " + forward.getName());
+        forward.attack(defender);
+        System.out.println("Состояние персонажей после раунда:");
+        System.out.println("Нападающий: " + forward);
+        System.out.println("Защитник: " + defender);
+    }
 }
