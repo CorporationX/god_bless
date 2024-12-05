da441483edc64465a621b013846a3da1765ebc2a
Stepan Evangulov <stevangulov@gmail.com>
2024-12-05 14:00:51 +0200

    Good  bless

--- src/main/java/school/faang/bjs45307/Main.java
+++ src/main/java/school/faang/bjs45307/Main.java
@@ -7,14 +7,14 @@ public class Main {
     static Map<Book, String> library = new HashMap<>();
 
     public static void main(String[] args) {
-    
-    LibrarySystem.addBook("Дориан Грей", "Оскар Уальд", 1965,"0001");
-    LibrarySystem.addBook("Гарри Потер", "Джоан Ролинг", 1998,"0002");
-    LibrarySystem.addBook("Винни Пух", "Алан Александр Милн", 1978,"0003");
-    LibrarySystem.addBook("Пинокио", "Карло Коллоди", 1971,"0004");
-    LibrarySystem.printAllBooks();
-    LibrarySystem.removeBook("Винни Пух", "Алан Александр Милн", 1978);
-    LibrarySystem.printAllBooks();
-    LibrarySystem.findBook("Гарри Потер", "Джоан Ролинг", 1998);
+
+        LibrarySystem.addBook("Дориан Грей", "Оскар Уальд", 1965,"0001");
+        LibrarySystem.addBook("Гарри Потер", "Джоан Ролинг", 1998,"0002");
+        LibrarySystem.addBook("Винни Пух", "Алан Александр Милн", 1978,"0003");
+        LibrarySystem.addBook("Пинокио", "Карло Коллоди", 1971,"0004");
+        LibrarySystem.printAllBooks();
+        LibrarySystem.removeBook("Винни Пух", "Алан Александр Милн", 1978);
+        LibrarySystem.printAllBooks();
+        LibrarySystem.findBook("Гарри Потер", "Джоан Ролинг", 1998);
     }
 }
