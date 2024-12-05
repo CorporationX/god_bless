6cf46d78ca1db5452d3d75504753126cff7217c8
elnur-elnur <tahirzadee@gmail.com>
2024-12-05 23:04:35 +0400

    Done: Библиотека Вестеросса

---
+++ src/main/java/school/faang/sprint1taks45324/LibrarySystem.java
@@ -0,0 +1,39 @@
+package school.faang.sprint1taks45324;
+
+import java.util.HashMap;
+
+public class LibrarySystem {
+    HashMap<Book, String> books = new HashMap<>();
+
+    public LibrarySystem(HashMap<Book, String> books) {
+        this.books = books;
+    }
+
+    public void addBook(String title, String author, int year, String location) {
+        books.put(new Book(title, author, year), location);
+    }
+
+    public void removeBook(String title, String author, int year) {
+        Book newBook = new Book(title, author, year);
+        if (!books.containsKey(newBook)) {
+            throw new IllegalArgumentException("Книга не найдена");
+        }
+        books.remove(new Book(title, author, year));
+    }
+
+    public void findBook(String title, String author, int year) {
+        Book newBook = new Book(title, author, year);
+
+        if (!books.containsKey(newBook)) {
+            throw new IllegalArgumentException("книга не найдена");
+        }
+
+        String location = books.get(newBook);
+        System.out.println(location);
+    }
+
+    public void printAllBooks() {
+        System.out.println(books);
+    }
+
+}
---
+++ src/main/java/school/faang/sprint1taks45324/Main.java
@@ -0,0 +1,24 @@
+package school.faang.sprint1taks45324;
+
+import java.util.HashMap;
+
+public class Main {
+    public static void main(String[] args) {
+        Book book1 = new Book("Sandora", "Masa", 2000);
+        Book book2 = new Book("Apple", "Misa", 2001);
+        Book book3 = new Book("X", "Musa", 2002);
+        Book book4 = new Book("Mercedec", "Mata", 2003);
+
+        HashMap<Book, String> book = new HashMap<>() {{
+            put(book1, "22");
+            put(book2, "33");
+            put(book3, "44");
+            put(book4, "55");
+        }};
+
+        LibrarySystem libSys = new LibrarySystem(book);
+        libSys.addBook("hoohoho", "Santa", 2025, "77");
+        libSys.findBook("1231323321", "Santa", 2025);
+
+    }
+}
