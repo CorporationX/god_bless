86abecb3e3f04091bfd3844829cef867768486ad
porchy21 <ogogo.mt@gmail.com>
2024-12-06 01:27:56 +0500

    fix checkstyle errors

--- src/main/java/school/faang/task_43853/FightManager.java
+++ src/main/java/school/faang/task_43853/FightManager.java
@@ -3,22 +3,20 @@ package school.faang.task_43853;
 import lombok.NoArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 
-import java.util.InputMismatchException;
-
 @Slf4j
 @NoArgsConstructor
 public class FightManager {
 
     public void fight(Character character1, Character character2) {
         do {
-                character1.attack(character2);
-                if (!character2.isAlive()) {
-                    break;
-                }
-                character2.attack(character1);
-                if (!character1.isAlive()) {
-                    break;
-                }
+            character1.attack(character2);
+            if (!character2.isAlive()) {
+                break;
+            }
+            character2.attack(character1);
+            if (!character1.isAlive()) {
+                break;
+            }
         } while (true);
 
         log.info("The winner is {}", character1.isAlive() ? character1.getName() : character2.getName());
