2b5516ae101a39cc038222e4d5c185674af1c90e
andruxaUZB <andruxa0989@yandex.ru>
2024-12-06 01:41:43 +0300

    Done

---
+++ src/main/java/school/faang/BJS2_43729/Main.java
@@ -0,0 +1,15 @@
+package school.faang.BJS2_43729;
+
+
+
+public class Main {
+
+    public static void main(String[] args) throws IllegalArgumentException {
+
+    User user = new User("Wow",18,"Google","Amsterdam");
+    System.out.println(user);
+
+    }
+
+
+}
