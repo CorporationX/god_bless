a7e3565f672261fbf8ba9552fa8d70053f1f77c5
romenskd <romenskyid@gmail.com>
2024-12-06 00:54:39 +0100

    Добавлен main в директорию с таском. Улучшен код с учетом комментариев.

--- src/main/java/school/faang/task_43733/User.java
+++ src/main/java/school/faang/task_43733/User.java
@@ -8,19 +8,29 @@ import lombok.Data;
 public class User {
     private static final Set<String> VALID_JOBS = Set.of("Google", "Uber", "Amazon");
     private static final Set<String> VALID_ADDRESSES = Set.of("London", "New York", "Amsterdam");
-    
+    private static final int AGE = 18;
     private String name; 
     private int age;
     private String job;  
     private String address;
 
     public User(String name, int age, String job, String address) {
+        this.name = name; 
+        this.age = age; 
+        this.job = job; 
+        this.address = address;
+
+        validate();
+    }
+
+    // Validates all parameters of the User object and throws an exception if any validation fails
+    public void validate() {
         if (name == null || name.isBlank()) {
             throw new IllegalArgumentException("Name cant be empty");
         }
 
-        if (age < 18) {
-            throw new IllegalArgumentException("Age must be over 18"); 
+        if (age < AGE) {
+            throw new IllegalArgumentException("Age must be over 18");
         }
 
         if (!VALID_JOBS.contains(job)) {
@@ -30,11 +40,5 @@ public class User {
         if (!VALID_ADDRESSES.contains(address)) {
             throw new IllegalArgumentException("The address must be contained in the VALID_ADDRESSES set");
         }
-
-        this.name = name; 
-        this.age = age; 
-        this.job = job; 
-        this.address = address; 
     }
-
 }
