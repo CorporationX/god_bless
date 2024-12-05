0e49e770dbecfcc8958fce2cde6a23545f9d4aaa
Maksim-VV <laulmv11@gmail.com>
2024-12-05 18:49:41 +0300

    The Library of Westeros - alpha release

---
+++ src/main/java/school/faang/BJS245398/Book.java
@@ -0,0 +1,22 @@
+package school.faang.BJS245398;
+
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NonNull;
+import lombok.Setter;
+import lombok.ToString;
+
+@AllArgsConstructor
+@EqualsAndHashCode
+@Getter
+@Setter
+@ToString
+public class Book {
+    @NonNull
+    private String title;
+    @NonNull
+    private String author;
+    @NonNull
+    private Integer year;
+}
--- src/main/java/school/faang/Main.java
+++ src/main/java/school/faang/Main.java
@@ -1,12 +1,5 @@
 package school.faang;
 
-
-import school.faang.BJS2_43968.User;
-
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Map;
-
 public class Main {
     public static void main(String[] args) {
 
