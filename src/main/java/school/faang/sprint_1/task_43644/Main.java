21082fc568e5d8383d09af229e8f508bbb729759
Aleksandr Kozlov <vismut-888@yandex.ru>
2024-12-05 17:38:14 +0300

    Исправлены комментарии после ревью

--- src/main/java/school/faang/sprint_1/task_43644/Main.java
+++ src/main/java/school/faang/sprint_1/task_43644/Main.java
@@ -1,13 +1,20 @@
 package school.faang.sprint_1.task_43644;
 
+import lombok.extern.slf4j.Slf4j;
+
 import static school.faang.sprint_1.task_43644.Example.revers;
 
+@Slf4j
 public class Main {
     public static void main(String[] args) {
-        int[] example = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
-        revers(example);
-        for (Integer e : example) {
-            System.out.println(e);
+        int[] array = {1, 2, 3, 4, 5, 6, 7, 8, 9};
+        try {
+            revers(array);
+            for (Integer e : array) {
+                System.out.println(e);
+            }
+        } catch (Exception e) {
+            log.error(e.getMessage());
         }
     }
 }
