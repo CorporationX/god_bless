2da71a7aae24ce772445cbe0f00dac24acc10700
sergeybabaev96 <166114022+sergeybabaev96@users.noreply.github.com>
2024-12-05 13:06:16 +0300

    Поместил создание объектов User сразу в коллекцию лист и скорректировал название переменных

--- src/main/java/school/faang/bjs243981/Main.java
+++ src/main/java/school/faang/bjs243981/Main.java
@@ -7,22 +7,17 @@ import java.util.Set;
 
 public class Main {
     public static void main(String[] args) {
-        User user1 = new User(123, "Nikolay", 25, List.of("Football", "Chess", "Book", "Math"));
-        User user2 = new User(332, "Anna", 24, List.of("Swimming", "Drawing", "Book", "Volleyball"));
-        User user3 = new User(132, "Dmitriy", 22, List.of("Volleyball", "Football", "Computer game", "Math"));
-        User user4 = new User(913, "Elizaveta", 19, List.of("Drawing", "Volleyball"));
-
-        List<User> users = new ArrayList<>();
-        users.add(user1);
-        users.add(user2);
-        users.add(user3);
-        users.add(user4);
+        List<User> users = List.of(
+                new User(123, "Nikolay", 25, List.of("Football", "Chess", "Book", "Math")),
+                new User(332, "Anna", 24, List.of("Swimming", "Drawing", "Book", "Volleyball")),
+                new User(132, "Dmitriy", 22, List.of("Volleyball", "Football", "Computer game", "Math")),
+                new User(913, "Elizaveta", 19, List.of("Drawing", "Volleyball")));
 
         Set<String> setHobby = Set.of("Football", "Chess", "Book", "Math");
 
         Map<User, String> containsUserHobby = User.findHobbyLovers(users, setHobby);
-        containsUserHobby.forEach((user, string) -> {
-            System.out.println(user + " : " + string);
+        containsUserHobby.forEach((user, hobby) -> {
+            System.out.println(user + " : " + hobby);
         });
     }
 }
