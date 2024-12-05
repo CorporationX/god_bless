c10c947b00dea56e85375a427fac151bc644ad82
Mipusatina <mipspam2@mail.ru>
2024-12-05 20:02:54 +0100

    Task04. Rare Books Store. Step 3. Feature-B

--- src/main/java/derschrank/task04/s3/bjstwo_44395/App.java
+++ src/main/java/derschrank/task04/s3/bjstwo_44395/App.java
@@ -2,6 +2,6 @@ package derschrank.task04.s3.bjstwo_44395;
 
 public class App {
     public static void main(String[] args) {
-
+        System.out.println("The shop will is open in 4 days");
     }
 }
