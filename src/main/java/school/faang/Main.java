b61a5902e2292a045250a4cd3b2a2241c840c1f5
Azamat71 <azamat.beisembay@gmail.com>
2024-12-05 23:35:30 +0500

    Reverse int array method

--- src/main/java/school/faang/Main.java
+++ src/main/java/school/faang/Main.java
@@ -1,4 +1,20 @@
 package school.faang;
 
+import java.util.Arrays;
+
 public class Main {
+    public static void main(String[] args) {
+        int[] nums = {1,2,3,4,5,6};
+        System.out.println(Arrays.toString(reverse(nums)));
+    }
+
+    public static int[] reverse(int[] nums) {
+        int[] reversed = new int[nums.length];
+        int i = 0;
+        for (int j = nums.length-1; j >= 0; j--) {
+            reversed[i] = nums[j];
+            i++;
+        }
+        return reversed;
+    }
 }
