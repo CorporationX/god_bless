4309d5ba48750ee63fe4e2ea6511ee5cbb71fecc
VladimirGontarenko <mister.gontarenko@yandex.ru>
2024-12-06 00:52:49 +0400

    task_BJS2-43726 Группировка пользователей по возрасту.

---
+++ src/main/java/school/faang/User.java
@@ -0,0 +1,27 @@
+package school.faang;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@Data
+@AllArgsConstructor
+public class User {
+    private String name;
+    private int age;
+    private String workPlace;
+    private String address;
+
+    public static Map<Integer, List<User>> groupUsers(List<User> users) {
+        Map<Integer, List<User>> groupedUsersByAge = new HashMap<>();
+        for (User user : users) {
+            groupedUsersByAge.computeIfAbsent(user.getAge(), key -> new ArrayList<>()).add(user);
+        }
+        return groupedUsersByAge;
+    }
+}
+
