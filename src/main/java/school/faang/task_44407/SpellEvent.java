42a0c5066145fd32788e723dc1b7d430b697f8d9
Timofey Khutoryanskiy <103549514+tiroxin@users.noreply.github.com>
2024-12-05 22:07:19 +0500

    tiroxin BJS2_44407 (#14522)

---
+++ src/main/java/school/faang/task_44407/SpellEvent.java
@@ -0,0 +1,14 @@
+package school.faang.task_44407;
+
+import lombok.*;
+
+@EqualsAndHashCode
+@AllArgsConstructor
+@Getter
+@Setter
+@ToString
+public class SpellEvent {
+    private int id;
+    private String eventType;
+    private String action;
+}
