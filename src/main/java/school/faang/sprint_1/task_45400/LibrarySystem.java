07ef9b615d4b300e20fe2191dafdbd6c3f302d12
Aleksandr Kozlov <vismut-888@yandex.ru>
2024-12-05 23:32:17 +0300

    Исправлены комментарии после ревью

--- src/main/java/school/faang/sprint_1/task_45400/LibrarySystem.java
+++ src/main/java/school/faang/sprint_1/task_45400/LibrarySystem.java
@@ -4,15 +4,15 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class LibrarySystem {
-    private static Map<Book, String> library = new HashMap<>();
+    private Map<Book, String> library = new HashMap<>();
 
-    public static void addBook(String title, String author, int year, String location) {
+    public void addBook(String title, String author, int year, String location) {
         Book book = new Book(title, author, year);
         library.put(book, location);
         System.out.println("Book added: " + book + ", her location: " + location);
     }
 
-    public static void removeBook(String title, String author, int year) {
+    public void removeBook(String title, String author, int year) {
         Book book = new Book(title, author, year);
         if (library.remove(book) != null) {
             System.out.println("Book: " + book + ", removed");
@@ -21,7 +21,7 @@ public class LibrarySystem {
         }
     }
 
-    public static void findBook(String title, String author, int year) {
+    public void findBook(String title, String author, int year) {
         Book book = new Book(title, author, year);
         if (library.containsKey(book)) {
             System.out.println("Book: " + book + ", found and is located: " + library.get(book));
@@ -30,7 +30,7 @@ public class LibrarySystem {
         }
     }
 
-    public static void printAllBooks() {
+    public void printAllBooks() {
         for (var entry : library.entrySet()) {
             System.out.println("Book: " + entry.getKey() + ", her location: " + entry.getValue());
         }
--- src/main/java/school/faang/sprint_1/task_45400/Main.java
+++ src/main/java/school/faang/sprint_1/task_45400/Main.java
@@ -2,28 +2,23 @@ package school.faang.sprint_1.task_45400;
 
 import lombok.extern.slf4j.Slf4j;
 
-import java.util.HashMap;
-import java.util.Map;
-
-import static school.faang.sprint_1.task_45400.LibrarySystem.addBook;
-import static school.faang.sprint_1.task_45400.LibrarySystem.findBook;
-import static school.faang.sprint_1.task_45400.LibrarySystem.printAllBooks;
-import static school.faang.sprint_1.task_45400.LibrarySystem.removeBook;
-
 @Slf4j
 public class Main {
     public static void main(String[] args) {
         try {
-            addBook("War and Peace", "Tolstoy", 1873, "Bookshelf 3");
-            addBook("Anna Karenina", "Tolstoy", 1877, "Bookshelf 1");
-            addBook("The Overcoat", "Gogol", 1842, "Bookshelf 5");
-            addBook("Dead Souls", "Gogol", 1842, "Bookshelf 4");
+            LibrarySystem librarySystem = new LibrarySystem();
 
-            removeBook("Shining", "King", 1977);
+            librarySystem.addBook("War and Peace", "Tolstoy", 1873, "Bookshelf 3");
+            librarySystem.addBook("Anna Karenina", "Tolstoy", 1877, "Bookshelf 1");
+            librarySystem.addBook("The Overcoat", "Gogol", 1842, "Bookshelf 5");
+            librarySystem.addBook("Dead Souls", "Gogol", 1842, "Bookshelf 4");
 
-            findBook("War and Peace", "Tolstoy", 1873);
+            librarySystem.removeBook("Shining", "King", 1977);
 
-            printAllBooks();
+            librarySystem.findBook("War and Peace", "Tolstoy", 1873);
+
+            librarySystem.printAllBooks();
+
         } catch (Exception e) {
             log.error(e.getMessage());
         }
