47b2f9a0227a86fc335eda6f0d7865f493fb0945
tatgricenko <tattgricenko@gmail.com>
2024-12-05 17:06:00 +0300

    Delete second constructor for Warrior and Archer

--- src/main/java/school/faang/sprint_1/task_43849/Archer.java
+++ src/main/java/school/faang/sprint_1/task_43849/Archer.java
@@ -2,13 +2,6 @@ package school.faang.sprint_1.task_43849;
 
 public class Archer extends Character {
     public Archer(String name) {
-        super(name);
-        strength = 3;
-        dexterity = 10;
-        intelligence = 5;
-    }
-
-    public Archer(String name, int strength, int dexterity, int intelligence) {
         super(name, 3, 10, 5);
     }
 
--- src/main/java/school/faang/sprint_1/task_43849/Warrior.java
+++ src/main/java/school/faang/sprint_1/task_43849/Warrior.java
@@ -3,13 +3,6 @@ package school.faang.sprint_1.task_43849;
 public class Warrior extends Character {
 
     public Warrior(String name) {
-        super(name);
-        strength = 10;
-        dexterity = 5;
-        intelligence = 3;
-    }
-
-    public Warrior(String name, int strength, int dexterity, int intelligence) {
         super(name, 10, 5, 3);
     }
 
