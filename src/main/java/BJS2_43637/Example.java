9315590ff4431d79a17c0ef3dde030f5155b65f9
RGZ_TTT <rgz.ttt@mail.ru>
2024-12-05 18:41:06 +0700

    Added reverse-method

---
+++ src/main/java/BJS2_43637/Example.java
@@ -0,0 +1,12 @@
+package BJS2_43637;
+
+public class Example {
+    static void reverse(int [] array){
+
+        for (int  i= array.length-1; i>=0; i--){
+            System.out.println(array[i]);
+
+        }
+
+    }
+}
