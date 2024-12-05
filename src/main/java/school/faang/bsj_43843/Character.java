4bb9df24943fab051e83fc387914890a4072b92a
Anton Graf <ant0ngraf@yandex.ru>
2024-12-05 11:12:50 +0600

    Добавляет классы персонажей

---
+++ src/main/java/school/faang/bsj_43843/Character.java
@@ -0,0 +1,32 @@
+package school.faang.bsj_43843;
+
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@ToString
+@EqualsAndHashCode
+public abstract class Character {
+    protected String name;
+    protected int strength;
+    protected int agility;
+    protected int intelligence;
+    protected int health;
+
+    public Character(String name) {
+        this.name = name;
+    }
+
+    public Character(String name, int strength, int agility, int intelligence) {
+        this.name = name;
+        this.strength = strength;
+        this.agility = agility;
+        this.intelligence = intelligence;
+        this.health = 100;
+    }
+
+    public abstract void attack(Character character);
+}
