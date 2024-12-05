c402c671f681f1e09c7a16496453a5ee7c31db38
TFdevQA <46944859+TFdevQA@users.noreply.github.com>
2024-12-04 20:03:52 -0800

    task: user's hobby

---
+++ src/main/java/school/faang/sprint1/bjs_43985/Main.java
@@ -0,0 +1,21 @@
+package school.faang.sprint1.bjs_43985;
+
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
+public class Main {
+    public static void main(String[] args) {
+        List<User> users = List.of(
+                new User(20, "Mike", 24, Set.of("sky", "read")),
+                new User(43, "Nike", 19, Set.of("swim", "read")),
+                new User (21, "Nino", 22, Set.of("dance", "sing"))
+        );
+        Set<String> hobbies = Set.of("read", "dance", "sing", "swim");
+
+        Map<User, String> resultList = User.findHobbyLovers(users, hobbies);
+        resultList.forEach((user, hobby) ->
+                System.out.println(user + " -> " + hobby)
+        );
+    }
+}
---
+++ src/main/java/school/faang/sprint1/bjs_43985/User.java
@@ -0,0 +1,38 @@
+package school.faang.sprint1.bjs_43985;
+
+import lombok.Getter;
+
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+public class User {
+    private final int id;
+    private final String name;
+    private final int age;
+    @Getter
+    private final Set<String> activities;
+
+    public User(int id, String name, int age, Set<String> activities) {
+        this.id = id;
+        this.name = name;
+        this.age = age;
+        this.activities = activities;
+    }
+
+    @Override
+    public String toString() {
+        return "User{id=%d, name='%s', age=%d}".formatted(id, name, age);
+    }
+
+    public static Map<User, String> findHobbyLovers(List<User> users, Set<String> hobbies) {
+        Map<User, String> activeUsers = users.stream()
+                .flatMap(user -> user.getActivities().stream()
+                        .filter(hobbies::contains)
+                        .map(hobby -> Map.entry(user, hobby)))
+                .collect(Collectors.toMap(entry -> entry.getKey(), entry -> entry.getValue(),
+                        (existing, replacement) -> existing));
+        return activeUsers;
+    }
+}
