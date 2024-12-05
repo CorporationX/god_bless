4c4589b74ffc8909449d4c76ac10488581667fef
Mihail Ermolaev <fitis_rus21@mail.ru>
2024-12-05 14:55:45 +0300

    checkstyle

--- src/main/java/school/faang/task_43876/Main.java
+++ src/main/java/school/faang/task_43876/Main.java
@@ -5,16 +5,20 @@ public class Main {
         Warrior warrior = new Warrior("Warr");
         Archer archer = new Archer("Arch");
 
+        System.out.println("Health " + archer.getName() + " before attack " + warrior.getName() + " - "
+                + archer.getHealth());
+        warrior.attack(archer);
+        System.out.println("Health " + archer.getName() + " after attack - " + warrior.getName() + " - "
+                + archer.getHealth());
+
         Warrior warrior2 = new Warrior("Warr2", 100, 0, 0);
         Archer archer2 = new Archer("Arch2", 100, 3, 3);
 
-        System.out.println("Health " + archer.getName() + " before attack " + warrior.getName() + " - " + archer.getHealth());
-        warrior.attack(archer);
-        System.out.println("Health " + archer.getName() + " after attack - " + warrior.getName() + " - " + archer.getHealth());
-
-        System.out.println("Health " + warrior2.getName() + " before attack - " + archer2.getName() + " - " + warrior2.getHealth());
+        System.out.println("Health " + warrior2.getName() + " before attack - " + archer2.getName() + " - "
+                + warrior2.getHealth());
         archer2.attack(warrior2);
-        System.out.println("Health " + warrior2.getName() + " after attack - " + archer2.getName() + " - " + warrior2.getHealth());
+        System.out.println("Health " + warrior2.getName() + " after attack - " + archer2.getName() + " - "
+                + warrior2.getHealth());
 
 
     }
