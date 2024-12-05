0648860316ab76f92ba0dc9cbeb11d3bcfb91701
Sergey Senin <04sersen@gmail.com>
2024-12-05 17:51:24 +0300

    Рефакторинг классов Archer и Warrior:

--- src/main/java/school/faang/task_43841/Archer.java
+++ src/main/java/school/faang/task_43841/Archer.java
@@ -2,14 +2,17 @@ package school.faang.task_43841;
 
 public class Archer extends Character {
     public Archer(String name) {
-        super(name, 3, 10, 5); // Сила = 3, Ловкость = 10, Интеллект = 5
+        super(name, 3, 10, 5);
     }
 
     @Override
     public void attack(Character target) {
-        int damage = this.agility;
-        System.out.println(this.name + " shoots at " + target.getName() + " with dexterity " + damage);
-        target.health -= damage;
-        target.checkHealth();
+        if (target.getHealth() > 0) {
+            System.out.println(this.name + " shoots at " + target.getName() + " with dexterity " + this.agility);
+            target.health -= this.agility;
+            target.checkHealth();
+        } else {
+            System.out.println(this.name + " cannot attack " + target.getName() + " because he is already defeated.");
+        }
     }
 }
--- src/main/java/school/faang/task_43841/Warrior.java
+++ src/main/java/school/faang/task_43841/Warrior.java
@@ -2,14 +2,17 @@ package school.faang.task_43841;
 
 public class Warrior extends Character {
     public Warrior(String name) {
-        super(name, 10, 5, 3); // Сила = 10, Ловкость = 5, Интеллект = 3
+        super(name, 10, 5, 3);
     }
 
     @Override
     public void attack(Character target) {
-        int damage = this.strength;
-        System.out.println(this.name + " attacks " + target.getName() + " by force " + damage);
-        target.health -= damage;
-        target.checkHealth();
+        if (target.getHealth() > 0) {
+            System.out.println(this.name + " attacks " + target.getName() + " by force " + this.strength);
+            target.health -= this.strength;
+            target.checkHealth();
+        } else {
+            System.out.println(this.name + " cannot attack " + target.getName() + " because he is already defeated.");
+        }
     }
 }
