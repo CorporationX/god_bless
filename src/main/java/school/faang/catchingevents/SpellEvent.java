d0d82a127412e2a2f5094cf228dd198ac10c9a1e
AesaQ <ilyaovchinnikov2095@gmail.com>
2024-12-05 20:15:44 +0500

    refactoring

--- src/main/java/school/faang/catchingevents/SpellEvent.java
+++ src/main/java/school/faang/catchingevents/SpellEvent.java
@@ -7,10 +7,4 @@ public class SpellEvent {
     private final int id;
     private final String eventType;
     private final String action;
-
-    public SpellEvent(int id, String eventType, String action) {
-        this.id = id;
-        this.eventType = eventType;
-        this.action = action;
-    }
 }
