413936f5bae5d11d985466a41c8185680b5691a8
Maksim-VV <laulmv11@gmail.com>
2024-12-05 12:03:15 +0300

    Abstraction, abstraction - added Main,the code has been moved to the package

---
+++ src/main/java/school/faang/BJS2_43751/Main.java
@@ -0,0 +1,9 @@
+package school.faang.BJS2_43751;
+
+public class Main {
+    public static void main(String[] args) {
+        new User("Bob", 25, "Google", "London");
+        new User("Bob", 25, "Google", "");
+    }
+
+}
--- src/main/java/school/faang/BJS2_43751/User.java
+++ src/main/java/school/faang/BJS2_43751/User.java
@@ -1,4 +1,4 @@
-package school.faang;
+package school.faang.BJS2_43751;
 
 
 import lombok.Data;
