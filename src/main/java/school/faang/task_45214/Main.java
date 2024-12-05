591a76d08f46a15019fc0bfe61521d601576c0c5
therealadik <vladerm2000@yandex.ru>
2024-12-05 21:58:08 +0500

    fix style

--- src/main/java/school/faang/task_45214/Main.java
+++ src/main/java/school/faang/task_45214/Main.java
@@ -11,37 +11,32 @@ import java.util.Map;
 
 public class Main {
     public static void main(String[] args) {
-        // Создаем студентов
-        Student student1 = new Student(1, "Harry Potter");
-        Student student2 = new Student(2, "Hermione Granger");
-        Student student3 = new Student(3, "Ron Weasley");
-
-        // Создаем предметы
-        Subject subject1 = new Subject(1, "Potions");
-        Subject subject2 = new Subject(2, "Defense Against the Dark Arts");
-        Subject subject3 = new Subject(3, "Herbology");
-
-        // Создаем базу данных студентов
         Map<Student, Map<Subject, Integer>> studentMap = new HashMap<>();
         Map<Subject, List<Student>> subjectMap = new HashMap<>();
         StudentDatabase database = new StudentDatabase(studentMap, subjectMap);
 
-        // Тест 1: Добавление студента с предметами
+        Subject subject1 = new Subject(1, "Potions");
+        Subject subject2 = new Subject(2, "Defense Against the Dark Arts");
+
         Map<Subject, Integer> gradesForHarry = new HashMap<>();
         gradesForHarry.put(subject1, 85);
         gradesForHarry.put(subject2, 90);
+        Student student1 = new Student(1, "Harry Potter");
         database.addStudentWithSubjects(student1, gradesForHarry);
 
         System.out.println("=== После добавления Harry ===");
         database.printAllStudentsWithSubjectsAndScores();
 
         // Тест 2: Добавление предмета к существующему студенту
+        Subject subject3 = new Subject(3, "Herbology");
         database.addSubjectWithStudent(student1, subject3, 88);
 
         System.out.println("\n=== После добавления Herbology для Harry ===");
         database.printAllStudentsWithSubjectsAndScores();
 
         // Тест 3: Добавление предмета с несколькими студентами
+        Student student2 = new Student(2, "Hermione Granger");
+        Student student3 = new Student(3, "Ron Weasley");
         database.addSubjectWithStudents(subject3, Arrays.asList(student2, student3));
 
         System.out.println("\n=== После добавления Herbology для Hermione и Ron ===");
