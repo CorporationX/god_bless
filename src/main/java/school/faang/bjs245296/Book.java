eeefa9fcbe83925ff6d43c71fff5108470396646
Parvin Etibarli <etibarliparvin@gmail.com>
2024-12-05 13:35:24 +0400

    BJS2-45296 is done

---
+++ src/main/java/school/faang/bjs245296/Book.java
@@ -0,0 +1,16 @@
+package school.faang.bjs245296;
+
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.ToString;
+
+@ToString
+@EqualsAndHashCode
+@AllArgsConstructor
+public class Book {
+
+    private String title;
+    private String author;
+    private int year;
+
+}
