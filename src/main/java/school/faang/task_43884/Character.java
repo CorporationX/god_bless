1b2554dfd2f39d8fc4d76df4dcade58a319edf78
Natalya Vasileva <boryantheone228@yandex.ru>
2024-12-05 23:32:29 +0300

    BJS2-43884 Создание абстрактного класса Character

---
+++ src/main/java/school/faang/task_43884/Character.java
@@ -0,0 +1,22 @@
+package school.faang.task_43884;
+
+abstract class Character {
+    protected String name;
+    protected int strength;
+    protected int agility;
+    protected int intelligence;
+    protected int health = 100;
+
+    public Character(String name) {
+        this.name = name;
+    }
+
+    public Character(String name, int strength, int agility, int intelligence) {
+        this.name = name;
+        this.strength = strength;
+        this.agility = agility;
+        this.intelligence = intelligence;
+    }
+
+    protected abstract void attack(Character character);
+}
