1465b9e8fe4e5b3920c87eaa579f0d723f4b24a0
Daniil Kovalev <daniil_kovalyov@mail.ru>
2024-12-05 21:59:18 +0300

    delete constructor

--- src/main/java/school/faang/task_43869/Archer.java
+++ src/main/java/school/faang/task_43869/Archer.java
@@ -5,10 +5,6 @@ public class Archer extends Character {
         super(name, 3, 10, 5);
     }
 
-    public Archer(String name, Integer strength, Integer agility, Integer intelligence) {
-        super(name, strength, agility, intelligence);
-    }
-
     @Override
     public void attack(Character target) {
         target.setHealth(target.getHealth() - this.agility);
--- src/main/java/school/faang/task_43869/Warrior.java
+++ src/main/java/school/faang/task_43869/Warrior.java
@@ -5,10 +5,6 @@ public class Warrior extends Character {
         super(name, 10, 5, 3);
     }
 
-    public Warrior(String name, Integer strength, Integer agility, Integer intelligence) {
-        super(name, strength, agility, intelligence);
-    }
-
     @Override
     public void attack(Character target) {
         target.setHealth(target.getHealth() - this.strength);
