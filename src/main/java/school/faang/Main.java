d21fabb24a46fe8345bd66602e995e0b71a2702f
Azamat71 <azamat.beisembay@gmail.com>
2024-12-06 00:18:42 +0500

    BJS2-43646 Без использование дополнительного массива в методе reverse

--- src/main/java/school/faang/Main.java
+++ src/main/java/school/faang/Main.java
@@ -4,17 +4,18 @@ import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) {
-        int[] nums = {1,2,3,4,5,6};
+        int[] nums = {1,2,3,4,5,6,7,8,9};
         System.out.println(Arrays.toString(reverse(nums)));
     }
 
     public static int[] reverse(int[] nums) {
-        int[] reversed = new int[nums.length];
         int i = 0;
-        for (int j = nums.length-1; j >= 0; j--) {
-            reversed[i] = nums[j];
+        for (int j = nums.length-1; j >= nums.length/2; j--) {
+            int temp = nums[i];
+            nums[i] = nums[j];
+            nums[nums.length-1-i] = temp;
             i++;
         }
-        return reversed;
+        return nums;
     }
 }
