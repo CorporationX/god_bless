2c674792865b87e0ae13568b5d7b94ab0f80786d
ArzamaS Ilya <coldoff@inbox.ru>
2024-12-05 23:32:01 +0300

    Переписал создание списка

--- src/main/java/school/faang/task_43631/Main.java
+++ src/main/java/school/faang/task_43631/Main.java
@@ -8,14 +8,13 @@ import static school.faang.task_43631.User.groupUsers;
 
 public class Main {
     public static void main(String[] args) {
-        User firstUser = new User("John", 30, "Amazon", "Canada");
-        User secondUser = new User("Samanta", 15, "Google", "Florida");
-        User thirdUser = new User("Arnold", 44, "Tik-Tok", "Home");
-        User fourthUser = new User("Silvester", 56, "VK", "Derevnya");
-        User fivesUser = new User("Laqvanda", 44, "Yandex", "Zelepupinsk");
 
-        List<User> userList = new ArrayList<>();
-        Collections.addAll(userList, firstUser, secondUser, thirdUser, fourthUser, fivesUser);
+
+        List<User> userList = List.of(new User("John", 30, "Amazon", "Canada"),
+                new User("Samanta", 15, "Google", "Florida"),
+                new User("Silvester", 56, "VK", "Derevnya"),
+                new User("Arnold", 44, "Tik-Tok", "Home"),
+                new User("Laqvanda", 44, "Yandex", "Zelepupinsk"));
 
         System.out.println(groupUsers(userList));
 
