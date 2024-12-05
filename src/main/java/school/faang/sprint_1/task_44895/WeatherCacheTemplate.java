33e61a99f800f668ad9ecb82fe4974febf733108
rostex <rostex2@yandex.ru>
2024-12-05 23:28:34 +0300

    Создан интерфейс WeatherProvider и абстрактный класс WeatherCacheTemplate
    Создан объект WeatherData
    Создан класс WeatherService реализующий интерфейс WeatherProvider с логикой получения данных для объекта WeatherData
    Созданы вложенные классы StandardWeatherCache FrequentUpdateWeatherCache наследующие абстрактный класс WeatherCacheTemplate с логикой проверки актуальности данных
    Создан класс Main с проверкой работы методов классов.

---
+++ src/main/java/school/faang/sprint_1/task_44895/WeatherCacheTemplate.java
@@ -0,0 +1,39 @@
+package school.faang.sprint_1.task_44895;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public abstract class WeatherCacheTemplate {
+    public static Map<String, WeatherData> cache = new HashMap<>();
+    private static final WeatherService weatherService = new WeatherService();
+
+    public WeatherData getWeatherData(String city, long maxCacheAgeMillis) {
+        WeatherData currentData = cache.get(city);
+        WeatherData updatedData = weatherService.fetchWeatherData(city);
+
+        if (currentData == null || isCacheExpired(currentData, maxCacheAgeMillis)) {
+            cache.put(city, updatedData);
+        }
+        return updatedData;
+    }
+
+    public void forceUpdateWeather(String city) {
+        WeatherData data = weatherService.fetchWeatherData(city);
+        cache.put(city, data);
+        System.out.printf("Weather data to %s force updated\n", city);
+    }
+
+    public void clearExpiredCache(long maxCacheAgeMillis) {
+        StandardWeatherCache standardWeatherCache = new StandardWeatherCache();
+
+        for (Map.Entry<String, WeatherData> entry : cache.entrySet()) {
+            WeatherData data = entry.getValue();
+            if (standardWeatherCache.isCacheExpired(data, maxCacheAgeMillis)) {
+                cache.remove(data.getCity());
+            }
+        }
+    }
+
+    protected abstract boolean isCacheExpired(WeatherData data, long maxCacheAgeMillis);
+
+}
---
+++ src/main/java/school/faang/sprint_1/task_44895/WeatherData.java
@@ -0,0 +1,12 @@
+package school.faang.sprint_1.task_44895;
+
+import lombok.Value;
+
+@Value
+public class WeatherData {
+    String city;
+    double temperature;
+    double humidity;
+    long timestamp;
+}
+
