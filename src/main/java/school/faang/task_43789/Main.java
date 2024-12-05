d6f37bd69162b3f3de62da6ff7c6ed2cbb7cc460
IgnatovEI <ignatovei96@mail.ru>
2024-12-05 09:24:29 +0300

    fix checkstyle

--- src/main/java/school/faang/task_43789/Main.java
+++ src/main/java/school/faang/task_43789/Main.java
@@ -3,7 +3,7 @@ package school.faang.task_43789;
 public class Main {
     public static void main(String[] args) {
         User user = new User("John", 23, "Amazon", "London");
-        System.out.println(user.getName() + " " + user.getAge() +
-                " " + user.getJob() + " " + user.getAddress());
+        System.out.println(user.getName() + " " + user.getAge()
+                + " " + user.getJob() + " " + user.getAddress());
     }
 }
