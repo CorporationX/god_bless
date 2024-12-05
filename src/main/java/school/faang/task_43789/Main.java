3f4b45db7ccf307fc730d5d6079e8cf8054fac7c
IgnatovEI <ignatovei96@mail.ru>
2024-12-05 09:21:06 +0300

    task_43789 solution

---
+++ src/main/java/school/faang/task_43789/Main.java
@@ -0,0 +1,9 @@
+package school.faang.task_43789;
+
+public class Main {
+    public static void main(String[] args) {
+        User user = new User("John", 23, "Amazon", "London");
+        System.out.println(user.getName() + " " + user.getAge() +
+                " " + user.getJob() + " " + user.getAddress());
+    }
+}
---
+++ src/main/java/school/faang/task_43789/User.java
@@ -0,0 +1,39 @@
+package school.faang.task_43789;
+
+import lombok.Getter;
+
+import java.util.Set;
+
+@Getter
+public class User {
+    private String name;
+    private int age;
+    private String job;
+    private String address;
+
+    public static final Set<String> VALID_JOBS = Set.of("Google", "Uber", "Amazon");
+    public static final Set<String> VALID_ADDRESSES = Set.of("London", "New York", "Amsterdam");
+
+    public User(String name, int age, String job, String address) {
+
+        if (name.isBlank()) {
+            throw new IllegalArgumentException("Имя не может быть пустым.");
+        }
+        if (age < 18) {
+            throw new IllegalArgumentException("Возраст не может быть меньше 18.");
+        }
+        if (!VALID_JOBS.contains(job)) {
+            throw new IllegalArgumentException("Место работы должно быть содержится в наборе VALID_JOBS.");
+        }
+        if (!VALID_ADDRESSES.contains(address)) {
+            throw new IllegalArgumentException("Адрес должен быть содержится в наборе VALID_ADDRESSES.");
+        }
+
+        this.name = name;
+        this.age = age;
+        this.job = job;
+        this.address = address;
+
+    }
+}
+
