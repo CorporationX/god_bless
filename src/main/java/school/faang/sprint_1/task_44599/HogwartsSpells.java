568bd49ade79b3eb4bba4b133aae6e02beb7e622
Aleksandr Kozlov <vismut-888@yandex.ru>
2024-12-06 12:55:29 +0300

    BJS2-44599: Ловим события

---
+++ src/main/java/school/faang/sprint_1/task_44599/HogwartsSpells.java
@@ -0,0 +1,49 @@
+package school.faang.sprint_1.task_44599;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class HogwartsSpells {
+    private Map<Integer, SpellEvent> spellById = new HashMap<>();
+    private Map<String, List<SpellEvent>> spellsByType = new HashMap<>();
+
+    public void addSpellEvent(int id, String eventType, String actionDescription) {
+        SpellEvent spellEvent = new SpellEvent(id, eventType, actionDescription);
+        spellById.put(id, spellEvent);
+        spellsByType.putIfAbsent(eventType, new ArrayList<>());
+        spellsByType.get(eventType).add(spellEvent);
+    }
+
+    public SpellEvent getSpellEventById(int id) {
+        return spellById.get(id);
+    }
+
+    public List<SpellEvent> getSpellEventsByType(String eventType) {
+        if (eventType == null || eventType.isEmpty()) {
+            throw new IllegalArgumentException("EventType cannot be null or empty");
+        }
+        return spellsByType.getOrDefault(eventType, new ArrayList<>());
+    }
+
+    public void deleteSpellEvent(int id) {
+        SpellEvent spellEvent = spellById.remove(id);
+        if (spellEvent != null) {
+            List<SpellEvent> events = spellsByType.get(spellEvent.getEventType());
+            if (events != null) {
+                events.remove(spellEvent);
+                System.out.println("SpellEvent with id: " + id + ", removed");
+                if (events.isEmpty()) {
+                    spellsByType.remove(spellEvent.getEventType());
+                }
+            }
+        } else System.out.println("SpellEvent with id: " + id + ", not exist");
+    }
+
+    public void printAllSpellEvents() {
+        for (var entry : spellById.entrySet()) {
+            System.out.println(entry.getValue());
+        }
+    }
+}
---
+++ src/main/java/school/faang/sprint_1/task_44599/SpellEvent.java
@@ -0,0 +1,48 @@
+package school.faang.sprint_1.task_44599;
+
+import lombok.Getter;
+
+import java.util.Objects;
+
+@Getter
+public class SpellEvent {
+    private int id;
+    private String eventType;
+    private String action;
+
+    public SpellEvent(int id, String eventType, String action) {
+        this.id = id;
+        if (eventType.isEmpty()) {
+            throw new IllegalArgumentException("EventType cannot be null or empty");
+        }
+        this.eventType = eventType;
+        if (action.isEmpty()) {
+            throw new IllegalArgumentException("Action cannot be null or empty");
+        }
+        this.action = action;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        SpellEvent that = (SpellEvent) o;
+        return id == that.id && Objects.equals(eventType, that.eventType) && Objects.equals(action, that.action);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, eventType, action);
+    }
+
+    @Override
+    public String toString() {
+        return "SpellEvent id: " + id +
+                ", eventType: '" + eventType
+                + "', action: '" + action + "'";
+    }
+}
