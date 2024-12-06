6f161530ec62e706fb393eab563f723b9403e2e3
alexey_obraztsov <alexey.autotester86@gmail.com>
2024-12-06 15:32:02 +0300

    Done. Initial commit - Коллекционируем пользователей

---
+++ src/main/java/school/faang/sprint_1/task_collectusersbjs2n44013/Main.java
@@ -0,0 +1,23 @@
+package school.faang.sprint_1.task_collectusersbjs2n44013;
+
+
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
+public class Main {
+    public static void main(String[] args) {
+        List<User> usersList = List.of(
+                new User(1, "Tom", 24, Set.of("tennis", "football")),
+                new User(2, "Ann", 25, Set.of("carting", "football")),
+                new User(3, "John", 26, Set.of("hockey", "literball"))
+        );
+
+        Set<String> set = Set.of("tennis", "football", "carting");
+        Map<User, String> map = User.findHobbyLovers(usersList, set);
+
+        for (Map.Entry<User, String> user : map.entrySet()) {
+            System.out.printf("%s - with activity -  %s \n", user.getKey().getName(), user.getValue());
+        }
+    }
+}
---
+++ src/main/java/school/faang/sprint_1/task_collectusersbjs2n44013/User.java
@@ -0,0 +1,37 @@
+package school.faang.sprint_1.task_collectusersbjs2n44013;
+
+import lombok.Getter;
+
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import java.util.TreeMap;
+
+@Getter
+public class User {
+    private final int id;
+    private String name;
+    private int age;
+    private final Set<String> activities;
+
+    public User(int id, String name, int age, Set<String> activities) {
+        this.id = id;
+        this.name = name;
+        this.age = age;
+        this.activities = activities;
+    }
+
+    public static Map<User, String> findHobbyLovers(List<User> userList, Set<String> activitiesSet) {
+        Map<User, String> map = new HashMap<>();
+        for (User user : userList) {
+            for (String act : user.activities) {
+                if (activitiesSet.contains(act)) {
+                    map.put(user, act);
+                }
+            }
+        }
+        return map;
+    }
+}
