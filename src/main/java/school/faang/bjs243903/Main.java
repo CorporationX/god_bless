4792044a94ea1a93e2fc41fdecff82aede5be22d
Leonid Zhuravel <zhuravel_leonid@yahoo.com>
2024-12-06 23:03:16 +0200

    moved case to project

--- src/main/java/school/faang/bjs243903/Main.java
+++ src/main/java/school/faang/bjs243903/Main.java
@@ -1,4 +1,4 @@
-package school.faang;
+package school.faang.bjs243903;
 
 import java.util.ArrayList;
 import java.util.List;
--- src/main/java/school/faang/bjs243903/User.java
+++ src/main/java/school/faang/bjs243903/User.java
@@ -1,9 +1,7 @@
-package school.faang;
+package school.faang.bjs243903;
 
 import lombok.Getter;
 
-import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
