2155d3d98b31447b721b599d1c6251b055956abd
Pavel Schelkin <rhanan1233@gmail.com>
2024-12-06 14:35:51 +0300

    Абстракция Абстрауция

---
+++ src/main/java/school/faang/sprint1/task_43881/Archer.java
@@ -0,0 +1,15 @@
+package school.faang.sprint1.task_43881;
+
+public class Archer extends Character {
+    public Archer(String name) {
+        super(name);
+        this.power = 3;
+        this.dexterity = 10;
+        this.intellect = 5;
+    }
+
+    @Override
+    public void attac(Character character) {
+        character.health -= this.dexterity;
+    }
+}
