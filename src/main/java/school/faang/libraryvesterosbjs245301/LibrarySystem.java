8453ec4269132da8c7ab7a6c4c1211a25cc53eb9
Ilya <thomas.moody@mail.ru>
2024-12-06 11:50:35 +0300

    перекинул hashMap в класс Library system и в этом же классе убрал статику

--- src/main/java/school/faang/libraryvesterosbjs245301/LibrarySystem.java
+++ src/main/java/school/faang/libraryvesterosbjs245301/LibrarySystem.java
@@ -1,34 +1,38 @@
 package school.faang.libraryvesterosbjs245301;
 
+import java.util.HashMap;
 import java.util.Map;
 
+
 public class LibrarySystem {
-    public static void addBook(String title, String author, int year, String location) {
-        Main.numberShelf.put(new Book(title, author, year), location);
+    private Map<Book, String> numberShelf = new HashMap<>();
+
+    public void addBook(String title, String author, int year, String location) {
+        numberShelf.put(new Book(title, author, year), location);
         System.out.println("Book " + title + " add at shelf: " + location);
     }
 
-    public static void removeBook(String title, String author, int year) {
-        if (Main.numberShelf.remove(new Book(title, author, year)) != null) {
+    public void removeBook(String title, String author, int year) {
+        if (numberShelf.remove(new Book(title, author, year)) != null) {
             System.out.println("Book " + title + " delete");
         } else {
             System.out.println("Book don't find");
         }
     }
 
-    public static void findBook(String title, String author, int year) {
-        if (Main.numberShelf.get(new Book(title, author, year)) != null) {
+    public void findBook(String title, String author, int year) {
+        if (numberShelf.get(new Book(title, author, year)) != null) {
             System.out.println("Book " + title + " find");
         } else {
             System.out.println("Book don't find");
         }
     }
 
-    public static void printAllBooks() {
-        if (Main.numberShelf.isEmpty()) {
+    public void printAllBooks() {
+        if (numberShelf.isEmpty()) {
             System.out.println("Library is empty");
         } else {
-            for (Map.Entry<Book, String> entry : Main.numberShelf.entrySet()) {
+            for (Map.Entry<Book, String> entry : numberShelf.entrySet()) {
                 System.out.println(entry.getKey() + " find in " + entry.getValue());
             }
         }
--- src/main/java/school/faang/libraryvesterosbjs245301/Main.java
+++ src/main/java/school/faang/libraryvesterosbjs245301/Main.java
@@ -4,18 +4,18 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class Main {
-    protected static Map<Book, String> numberShelf = new HashMap<>();
 
     public static void main(String[] args) {
+        LibrarySystem library = new LibrarySystem();
 
-        LibrarySystem.addBook("Annals of the Black Centaur", "Yorkwen", 12, "Shelf A1");
-        LibrarySystem.addBook("The Red Book", "Elkin", 79, "Shelf A2");
-        LibrarySystem.addBook("Flora of the Valyrian Freehold", "Mikellion", 36, "Shelf A3");
+        library.addBook("Annals of the Black Centaur", "Yorkwen", 12, "Shelf A1");
+        library.addBook("The Red Book", "Elkin", 79, "Shelf A2");
+        library.addBook("Flora of the Valyrian Freehold", "Mikellion", 36, "Shelf A3");
 
-        LibrarySystem.printAllBooks();
-        LibrarySystem.findBook("The Red Book", "Elkin", 79);
-        LibrarySystem.removeBook("The Red Book", "Elkin", 79);
-        LibrarySystem.printAllBooks();
+        library.printAllBooks();
+        library.findBook("The Red Book", "Elkin", 79);
+        library.removeBook("The Red Book", "Elkin", 79);
+        library.printAllBooks();
 
     }
 }
