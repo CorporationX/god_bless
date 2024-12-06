e55f9d9637e0c9b6f01357071264c69c28979956
romenskd <romenskyid@gmail.com>
2024-12-06 16:03:11 +0100

    Ловим события

--- src/main/java/school/faang/task_43878/Archer.java
+++ src/main/java/school/faang/task_43878/Archer.java
@@ -2,13 +2,17 @@ package school.faang.task_43878;
 
 public class Archer extends Character {
 
+    private static final int STRENGTH = 3;
+    private static final int DEXTERITY = 10;
+    private static final int INTELLIGENCE = 5;
+
     public Archer(String name) {
-        super(name, 3, 10,  5);
+        super(name, STRENGTH, DEXTERITY, INTELLIGENCE);
     }
 
     @Override
     public void attack(Character character) {
-        int damage = getDexterity();
+        int damage = getDexterity();  // Ловкость используется для расчета урона
         System.out.println(getName() + " attacking " + character.getName() + "\ndamage " + damage);
         character.setHealth(character.getHealth() - damage);
     }
--- src/main/java/school/faang/task_43878/Warrior.java
+++ src/main/java/school/faang/task_43878/Warrior.java
@@ -2,16 +2,18 @@ package school.faang.task_43878;
 
 public class Warrior extends Character {
 
+    private static final int STRENGTH = 10;
+    private static final int DEXTERITY = 5;
+    private static final int INTELLIGENCE = 3;
+
     public Warrior(String name) {
-        super(name, 10, 5, 3);
+        super(name, STRENGTH, DEXTERITY, INTELLIGENCE);
     }
 
     @Override
     public void attack(Character character) {
-        int damage = getStrength();
-        System.out.println(getName() + " атакует " + character.getName() + "\ndamage " + getStrength());
+        int damage = getStrength();  // Сила используется для расчета урона
+        System.out.println(getName() + " атакует " + character.getName() + "\ndamage " + damage);
         character.setHealth(character.getHealth() - damage);
     }
-
 }
-
