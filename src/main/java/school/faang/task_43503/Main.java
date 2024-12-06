7fe62ead65021f907d455702b7a23776147dc5c0
Stepan Evangulov <stevangulov@gmail.com>
2024-12-06 09:22:26 +0200

    Now

--- src/main/java/school/faang/task_43503/Main.java
+++ src/main/java/school/faang/task_43503/Main.java
@@ -6,7 +6,7 @@ import java.util.Map;
 public class Main {
     public static void main(String[] args) {
         List<User> users = List.of(
-                new User("Tom", 23, "Corp 1", "Adress A"),
+                new User("Tom", 25, "Corp 1", "Adress A"),
                 new User("Sofia", 18, "Corp 2", "Adress B"),
                 new User("Bob", 36, "Corp 3", "Adress C"),
                 new User("Jerry", 52, "Corp 4", "Adress D")
