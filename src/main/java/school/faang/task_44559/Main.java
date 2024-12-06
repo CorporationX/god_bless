6176d4d1a729d5e4d541ee640c9426b8c5c922e6
narol01 <kmingogod@gmail.com>
2024-12-06 17:29:39 +0100

    BJS2-44559 code after review (не очень понимаю что ты просил , мне нужно удалить из двух колекций и во второй колекции ключ SpellType  а достать я его могу из первой колекци по id , если можешь обьясни пожалуйста как точно ты просил это реализовать, discord: ita69chi. )

--- src/main/java/school/faang/task_44559/Main.java
+++ src/main/java/school/faang/task_44559/Main.java
@@ -7,22 +7,22 @@ public class Main {
     public static void main(String[] args) {
         HogwartsSpells hogwartsSpells = new HogwartsSpells();
 
-        SpellEvent wandLightingSpell = new SpellEvent
-                (1, SpellType.CHARM, "Lights up the tip of the wand");
-        SpellEvent objectTransformationSpell = new SpellEvent
-                (2, SpellType.TRANSFIGURATION, "Transforms an object into another object");
-        SpellEvent magicalShieldSpell = new SpellEvent
-                (3, SpellType.PROTECTION, "Creates a magical shield against attacks");
-        SpellEvent itemSummonSpell = new SpellEvent
-                (4, SpellType.CHARM, "Summons an item to the caster");
-        SpellEvent animalToObjectSpell = new SpellEvent
-                (5, SpellType.TRANSFIGURATION, "Turns an animal into an object");
-        SpellEvent spellNeutralizationSpell = new SpellEvent
-                (6, SpellType.PROTECTION, "Neutralizes an opponent's spells");
-        SpellEvent voiceAmplificationSpell = new SpellEvent
-                (7, SpellType.CHARM, "Increases the volume of the voice");
-        SpellEvent objectDuplicationSpell = new SpellEvent
-                (8, SpellType.TRANSFIGURATION, "Creates a duplicate of an object");
+        SpellEvent wandLightingSpell = new SpellEvent(1, SpellType.CHARM,
+                "Lights up the tip of the wand");
+        SpellEvent objectTransformationSpell = new SpellEvent(2, SpellType.TRANSFIGURATION,
+                "Transforms an object into another object");
+        SpellEvent magicalShieldSpell = new SpellEvent(3, SpellType.PROTECTION,
+                "Creates a magical shield against attacks");
+        SpellEvent itemSummonSpell = new SpellEvent(4, SpellType.CHARM,
+                "Summons an item to the caster");
+        SpellEvent animalToObjectSpell = new SpellEvent(5, SpellType.TRANSFIGURATION,
+                "Turns an animal into an object");
+        SpellEvent spellNeutralizationSpell = new SpellEvent(6, SpellType.PROTECTION,
+                "Neutralizes an opponent's spells");
+        SpellEvent voiceAmplificationSpell = new SpellEvent(7, SpellType.CHARM,
+                "Increases the volume of the voice");
+        SpellEvent objectDuplicationSpell = new SpellEvent(8, SpellType.TRANSFIGURATION,
+                "Creates a duplicate of an object");
 
         hogwartsSpells.addSpellEvent(wandLightingSpell);
         hogwartsSpells.addSpellEvent(objectTransformationSpell);
