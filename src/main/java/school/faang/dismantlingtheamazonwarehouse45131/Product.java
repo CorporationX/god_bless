1f888b998e3a78094b08861989c7ca0fed862fa7
Ilya <thomas.moody@mail.ru>
2024-12-06 11:38:19 +0300

    Установил Lombok на свою IDE и реализовал его

--- src/main/java/school/faang/dismantlingtheamazonwarehouse45131/Product.java
+++ src/main/java/school/faang/dismantlingtheamazonwarehouse45131/Product.java
@@ -2,52 +2,18 @@ package school.faang.dismantlingtheamazonwarehouse45131;
 
 import java.util.Objects;
 
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.ToString;
+
+@Getter
+@ToString
+@EqualsAndHashCode
+@AllArgsConstructor
+
 public class Product {
     private int id;
     private String name;
     private String category;
-
-    public Product(int id, String name, String category) {
-        this.id = id;
-        this.name = name;
-        this.category = category;
-    }
-
-    public int getId() {
-        return id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public String getCategory() {
-        return category;
-    }
-
-    @Override
-    public boolean equals(Object object) {
-        if (this == object) {
-            return true;
-        }
-        if (object == null || getClass() != object.getClass()) {
-            return false;
-        }
-        Product product = (Product) object;
-        return id == product.id && Objects.equals(name, product.name) && Objects.equals(category, product.category);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(id, name, category);
-    }
-
-    @Override
-    public String toString() {
-        return "Product{"
-                + "id=" + id
-                + ", name='" + name + '\''
-                + ", category='" + category + '\''
-                + '}';
-    }
 }
