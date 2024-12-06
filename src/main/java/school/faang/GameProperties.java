6eda3616effc7a2dd769e324f9176973bcd4772f
Stepan Evangulov <stevangulov@gmail.com>
2024-12-06 12:37:54 +0200

    CI ругается, поэтому изменил название констант

--- src/main/java/school/faang/GameProperties.java
+++ src/main/java/school/faang/GameProperties.java
@@ -7,5 +7,5 @@ public class GameProperties {
     public static final int Strength_Default = 3;
     public static final int Agility_Default = 10;
     public static final int Intelligence_Default = 5;
-    public int Default_Health = 100;
+    public static final int Default_Health = 100;
 }
