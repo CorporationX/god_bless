a6b7335efd934f76c8a2f4a3897d7eccf6fbef9a
DmitryQueen <276246@student.pwr.edu.pl>
2024-12-06 04:54:35 +0100

    code is refactored

--- src/main/java/school/faang/task_45291/LibrarySystem.java
+++ src/main/java/school/faang/task_45291/LibrarySystem.java
@@ -1,36 +1,39 @@
 package school.faang.task_45291;
 
+import lombok.extern.slf4j.Slf4j;
+
 import java.util.HashMap;
 import java.util.Map;
 
+@Slf4j
 public class LibrarySystem {
-    private static Map<Book, String> books = new HashMap<>();
+    private final Map<Book, String> books = new HashMap<>();
 
-    public static void addBook(String title, String author, int year, String location) {
+    public void addBook(String title, String author, int year, String location) {
         Book book = new Book(title, author, year);
         books.put(book, location);
-        System.out.println("Book was added");
+        log.info(title + " was added");
     }
 
-    public static void removeBook(String title, String author, int year) {
+    public void removeBook(String title, String author, int year) {
         Book book = new Book(title, author, year);
-        if (books.remove(new Book(title, author, year)) == null) {
-            System.out.println("There is no such book in library");
+        if (books.remove(book) == null) {
+            log.info("There is no book with this title " + title);
         } else {
-            System.out.println("Book was succesfully deleted");
+            log.info("Book with title: " + title + " was succesfully deleted");
         }
     }
 
-    public static void findBook(String title, String author, int year) {
+    public void findBook(String title, String author, int year) {
         String book = books.get(new Book(title, author, year));
         if (book != null) {
-            System.out.println("Book was found. Position is " + book);
+            log.info("Book was found. Position is " + book);
         } else {
-            System.out.println("Book is not found");
+            log.info("Book with title: " + title + " was not found");
         }
     }
 
-    public static void printAllBooks() {
+    public void printAllBooks() {
         for (var book : books.entrySet()) {
             System.out.println(book.getKey() + " is on position: " + book.getValue());
         }
