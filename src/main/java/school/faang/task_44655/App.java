af98ce605f364389ea717b697aad253ac25a72de
Roman Kondratev <romankondratev.com@gmail.com>
2024-12-05 17:41:30 -0800

    BJS2-44655 feat: add App, Main - Added App.java for core logic implementation - Added Main.java for application entry point

---
+++ src/main/java/school/faang/task_44655/App.java
@@ -0,0 +1,7 @@
+package school.faang.task_44655;
+
+public class App {
+    public void printMessage(String message) {
+        System.out.println(message);
+    }
+}
---
+++ src/main/java/school/faang/task_44655/Main.java
@@ -0,0 +1,8 @@
+package school.faang.task_44655;
+
+public class Main {
+    public static void main(String[] args) {
+        App app = new App();
+        app.printMessage("Открытие магазина ожидается через 2 месяца");
+    }
+}
