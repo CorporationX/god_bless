1d4995530143ac7c6ab0d536867ff5bdd0a93944
Dias Kamza <kamzadias31@gmail.com>
2024-12-06 17:10:44 +0500

    fixed style

--- src/main/java/school/faang/Archer.java
+++ src/main/java/school/faang/Archer.java
@@ -5,7 +5,7 @@ import lombok.Setter;
 
 @Setter
 @Getter
-public class Archer extends Character{
+public class Archer extends Character {
     public Archer(String name) {
         super(name, 3, 10, 5);
     }
