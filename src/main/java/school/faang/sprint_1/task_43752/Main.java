8431d533d1d3c0aebba555bbba2983084491cbe8
our ame <you@example.com>
2024-12-06 15:06:08 +0500

    Выполнил задачу "Группировка пользователей по возрасту"

--- src/main/java/school/faang/sprint_1/task_43752/Main.java
+++ src/main/java/school/faang/sprint_1/task_43752/Main.java
@@ -2,11 +2,16 @@ package school.faang.sprint_1.task_43752;
 
 public class Main {
     public static void main(String[] args) {
+        checkUsersData("Ilya", 18, "Google", "London");
+        checkUsersData("Kevin", 17, "Uber", "New York");
+        checkUsersData("Ilya", 34, "Amazon", "Amsterdam");
+        checkUsersData("", 22, "Google", "London");
+
+    }
+
+    private static void checkUsersData(String name, Integer age, String job, String address) {
         try {
-            User firstUser = new User("Nina", 18, "Google", "London");
-            User secondUser = new User("Sergey", 17, "Uber", "New York");
-            System.out.println(firstUser);
-            System.out.println(secondUser);
+            new User(name, age, job, address);
         } catch (IllegalArgumentException e) {
             System.out.println(e.getMessage());
         }
