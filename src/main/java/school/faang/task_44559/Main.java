9eb06d261d04a5a74790204812aa9430b94c97c6
narol01 <kmingogod@gmail.com>
2024-12-06 04:00:42 +0100

    BJS2-44559 code after review

--- src/main/java/school/faang/task_44559/Main.java
+++ src/main/java/school/faang/task_44559/Main.java
@@ -7,8 +7,10 @@ public class Main {
     public static void main(String[] args) {
         HogwartsSpells hogwartsSpells = new HogwartsSpells();
         hogwartsSpells.addSpellEvent(new SpellEvent(1, SpellType.CHARM, "Lights up the tip of the wand"));
-        hogwartsSpells.addSpellEvent(new SpellEvent(2, SpellType.TRANSFIGURATION, "Transforms an object into another object"));
-        hogwartsSpells.addSpellEvent(new SpellEvent(3, SpellType.PROTECTION, "Creates a magical shield against attacks"));
+        hogwartsSpells.addSpellEvent(
+                new SpellEvent(2, SpellType.TRANSFIGURATION, "Transforms an object into another object"));
+        hogwartsSpells.addSpellEvent(
+                new SpellEvent(3, SpellType.PROTECTION, "Creates a magical shield against attacks"));
         hogwartsSpells.addSpellEvent(new SpellEvent(4, SpellType.CHARM, "Summons an item to the caster"));
         hogwartsSpells.addSpellEvent(new SpellEvent(5, SpellType.TRANSFIGURATION, "Turns an animal into an object"));
         hogwartsSpells.addSpellEvent(new SpellEvent(6, SpellType.PROTECTION, "Neutralizes an opponent's spells"));
