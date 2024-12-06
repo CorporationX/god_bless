ec87373c0327532dd91aeaf512592cb931a2591a
therealadik <vladerm2000@yandex.ru>
2024-12-06 12:40:08 +0500

    init

---
+++ src/main/java/school/faang/task_43522/Main.java
@@ -0,0 +1,11 @@
+package school.faang.task_43522;
+
+public class Main {
+    public static void main(String[] args) {
+        SpellCaster spellCaster = new SpellCaster();
+
+        spellCaster.cast(Spell.ALOHOMORA, (spell) -> "The door is unlocked by " + spell);
+        spellCaster.cast(Spell.LUMOS, (spell) -> "A beam of light is created by " + spell);
+        spellCaster.cast(Spell.EXPELLIARMUS, (spell) -> "The opponent is disarmed by " + spell);
+    }
+}
