9e87b874312d16364593fe2e49fdd4ab60d7fddd
alexey_obraztsov <alexey.autotester86@gmail.com>
2024-12-06 16:03:00 +0300

    adjusted checkstyle v2. Abstractions

--- src/main/java/school/faang/sprint_1/task_abstractionbsj2n43974/Warrior.java
+++ src/main/java/school/faang/sprint_1/task_abstractionbsj2n43974/Warrior.java
@@ -1,15 +1,12 @@
 package school.faang.sprint_1.task_abstractionbsj2n43974;
 
 public class Warrior extends Character {
-    private final static int WARRIOR_STRENGTH = 10;
-    private final static int WARRIOR_AGILITY = 5;
-    private final static int WARRIOR_INTELLECT = 3;
 
     public Warrior(String name) {
         super(name);
-        this.strength = WARRIOR_STRENGTH;
-        this.agility = WARRIOR_AGILITY;
-        this.intellect = WARRIOR_INTELLECT;
+        this.strength = 10;
+        this.agility = 5;
+        this.intellect = 3;
     }
 
     public Warrior(String name, int strength, int agility, int intellect) {
