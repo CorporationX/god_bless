cbc18e904bc23566171849c8a00cebbf3274d1ff
Leonid Zhuravel <zhuravel_leonid@yahoo.com>
2024-12-06 17:35:14 +0200

    fixing code style

--- src/main/java/school/faang/bjs243962/Main.java
+++ src/main/java/school/faang/bjs243962/Main.java
@@ -2,7 +2,6 @@ package school.faang.bjs243962;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 public class Main {
@@ -11,22 +10,17 @@ public class Main {
         List<User> users = new ArrayList<>() {
             {
                 add(new User(1, "User1", 19,
-                        Stream.of("Sports", "Music", "Reading")
-                                .collect(Collectors.toList())));
-
-
+                        Stream.of("Sports", "Music", "Reading").toList()));
                 add(new User(2, "User2", 21,
                         Stream.of("Gaming", "Gambling").toList()));
-
                 add(new User(3, "User3", 23,
                         Stream.of("Gaming", "Music").toList()));
-
                 add(new User(4, "User4", 31,
                         Stream.of("Hiking", "Gambling").toList()));
             }
         };
 
-        List<String> activities = Stream.of("Gaming", "Music").toList();
+        List<String> activities = List.of("Gaming", "Music");
 
         User.findHobbyLovers(users, activities);
     }
--- src/main/java/school/faang/bjs243962/User.java
+++ src/main/java/school/faang/bjs243962/User.java
@@ -5,7 +5,6 @@ import lombok.AllArgsConstructor;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.stream.Collector;
 import java.util.stream.Collectors;
 
 @AllArgsConstructor
@@ -21,19 +20,24 @@ public class User {
         for (User user : users) {
             for (String activity : user.activities) {
                 if (activitiesToCheck.contains(activity)) {
-                    //System.out.printf("For user: %s, the following activity match found: %s \r\n",
-                    // user.name, activity);
+                    System.out.printf("For user: %s, the following activity match found: %s \r\n",
+                            user.name, activity);
                     result.put(user, activity);
                 }
             }
         }
 
-        //  Map<User, String> result = users.stream()
-        //      .flatMap(user -> user.activities.stream()
-        //          .filter(activitiesToCheck::contains)
-        //          .map(activity -> Map.entry(user, activity)))
-        //       .collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue, (a, b) -> a, HashMap::new));
-
         return result;
+    }
+
+    public static Map<User, String> findHobbyLovers(List<User> users, List<String> activitiesToCheck,
+                                                    boolean useCollections) {
+
+        return users.stream()
+                .flatMap(user -> user.activities.stream()
+                        .filter(activitiesToCheck::contains)
+                        .map(activity -> Map.entry(user, activity)))
+                .collect(Collectors.toMap(
+                        Map.Entry::getKey, Map.Entry::getValue, (a, b) -> a, HashMap::new));
     }
 }
