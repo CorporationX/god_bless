6fe436078ffc54f497a8d1da0b1a990277cb5665
Leonid Zhuravel <zhuravel_leonid@yahoo.com>
2024-12-06 17:24:10 +0200

    PR fixes

--- src/main/java/school/faang/bjs243678/User.java
+++ src/main/java/school/faang/bjs243678/User.java
@@ -1,12 +1,15 @@
 package school.faang.bjs243678;
 
 import lombok.Getter;
+import java.util.List;
 import java.util.StringJoiner;
 import java.util.stream.Collectors;
-import static school.faang.bjs243678.Constants.VALID_JOBS;
-import static school.faang.bjs243678.Constants.VALID_ADDRESSES;
 
 public class User {
+
+    public static final int MIN_AGE = 18;
+    public static final List<String> VALID_JOBS = List.of("google", "uber", "amazon");
+    public static final List<String> VALID_ADDRESSES = List.of("london", "new york", "amsterdam");
 
     private String name;
     @Getter
@@ -16,35 +19,10 @@ public class User {
 
     public User(String name, int age, String workplace, String address) {
 
-        if (name.isEmpty()) {
-            System.out.println("Name cannot be empty");
-            return;
-        }
-
-        if (age < 18) {
-            System.out.println("Age cannot be less than 18");
-            return;
-        }
-
-        if (!VALID_JOBS.contains(workplace.toLowerCase())) {
-            StringJoiner joiner = new StringJoiner(" ,");
-            VALID_JOBS.forEach(item -> joiner.add(item));
-            System.out.println("Workplace entered: " + workplace + ", but should be one of the following values: "
-                    + joiner);
-            return;
-        }
-
-        if (!VALID_ADDRESSES.contains(address.toLowerCase())) {
-            System.out.printf("Address entered: %s, but should be one of the following values: %s%n",
-                    address, String.join(" ,", VALID_JOBS));
-            return;
-        }
-
-        //  if (!VALID_ADDRESSES.contains(address.toLowerCase())) {
-        //      System.out.printf("Address entered: %s, but should be one of the following values: %s%n",
-        //          address, VALID_ADDRESSES.stream().collect(Collectors.joining(" ,")));
-        //      return;
-        //  }
+        validateName();
+        validateAge();
+        validateJob();
+        validateAddress();
 
         this.name = name;
         this.age = age;
@@ -52,4 +30,40 @@ public class User {
         this.address = address;
     }
 
+    private void validateName() {
+        if (this.name.isEmpty()) {
+            throw new IllegalArgumentException("Name cannot be empty");
+        }
+    }
+
+    private void validateAge() {
+        if (this.age < MIN_AGE) {
+            throw new IllegalArgumentException("Age cannot be less than 18");
+        }
+    }
+
+    private void validateJob() {
+        if (!VALID_JOBS.contains(workplace.toLowerCase())) {
+            StringJoiner joiner = new StringJoiner(" ,");
+            VALID_JOBS.forEach(joiner::add);
+            throw new IllegalArgumentException("Workplace entered: " + workplace
+                    + ", but should be one of the following values: " + joiner);
+        }
+    }
+
+    private void validateAddress() {
+        if (!VALID_ADDRESSES.contains(this.address.toLowerCase())) {
+            throw new IllegalArgumentException(String.join(
+                    "Address entered: %s, but should be one of the following values: %s%n",
+                    this.address, String.join(" ,", VALID_JOBS)));
+        }
+    }
+
+    private void validateAddress(boolean usingCollection) {
+        if (!VALID_ADDRESSES.contains(this.address.toLowerCase())) {
+            throw new IllegalArgumentException(String.join(
+                    "Address entered: %s, but should be one of the following values: %s%n",
+                    this.address, VALID_ADDRESSES.stream().collect(Collectors.joining(" ,"))));
+        }
+    }
 }
