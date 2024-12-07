a7a532c9fb15aee4a5134b1aa67120871d08615e
Anastasia Dragomirova <nastay.dr2002@gmail.com>
2024-12-07 11:20:19 +0400

    BJS2-44521 — Ловим события

---
+++ src/main/java/faang/school/godbless/BJS2_44521/HogwartsSpells.java
@@ -0,0 +1,68 @@
+package faang.school.godbless.BJS2_44521;
+
+import lombok.Getter;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@Getter
+public class HogwartsSpells {
+    private final Map<Integer, SpellEvent> spellById = new HashMap<>();
+    private final Map<String, List<SpellEvent>> spellsByType = new HashMap<>();
+
+    public void addSpellEvent(int id, String eventType, String actionDescription) {
+        SpellEvent spellEvent = new SpellEvent(id, eventType, actionDescription);
+        spellById.put(id, spellEvent);
+        addSpellEventByType(spellEvent, eventType);
+    }
+
+    public SpellEvent getSpellEventById(int id) {
+        return spellById.get(id);
+    }
+
+    public List<SpellEvent> getSpellEventsByType(String eventType) {
+        return spellsByType.get(eventType);
+    }
+
+    public void deleteSpellEvent(int id) {
+        SpellEvent deletedSpellEvent = spellById.get(id);
+        if (deletedSpellEvent != null) {
+            deleteSpellEventByType(deletedSpellEvent);
+            spellById.remove(id);
+        }
+    }
+
+    public void printAllSpellEvents() {
+        for (Map.Entry<Integer, SpellEvent> spellEvent : spellById.entrySet()) {
+            System.out.println(spellEvent.getKey() + " — " + spellEvent.getValue());
+        }
+    }
+
+    private void addSpellEventByType(SpellEvent spellEvent, String eventType) {
+        List<SpellEvent> spellEvents;
+        if (spellsByType.containsKey(eventType)) {
+            spellEvents = spellsByType.get(eventType);
+            spellEvents.add(spellEvent);
+        } else {
+            spellEvents = new ArrayList<>();
+            spellEvents.add(spellEvent);
+            spellsByType.put(eventType, spellEvents);
+        }
+    }
+
+    private void deleteSpellEventByType(SpellEvent deletedSpellEvent) {
+        List<SpellEvent> spellEventsWithSpecificType = spellsByType.get(deletedSpellEvent.getEventType());
+        if (spellEventsWithSpecificType != null) {
+            int spellEventIndex = 0;
+            for (SpellEvent spellEvent : spellEventsWithSpecificType) {
+                if (spellEvent.equals(deletedSpellEvent)) {
+                    break;
+                }
+                spellEventIndex++;
+            }
+            spellEventsWithSpecificType.remove(spellEventIndex);
+        }
+    }
+}
---
+++ src/main/java/faang/school/godbless/BJS2_44521/Main.java
@@ -0,0 +1,23 @@
+package faang.school.godbless.BJS2_44521;
+
+public class Main {
+
+    public static final SpellEvent SPELL_EVENT_1 = new SpellEvent(1, "Защита", "Защищает от атак");
+    public static final SpellEvent SPELL_EVENT_2 = new SpellEvent(2, "Чары", "Изменяет цвет предмета");
+    public static final SpellEvent SPELL_EVENT_3 = new SpellEvent(3, "Трансфигурация", "Изменяет цвет волос");
+
+    public static final SpellEvent SPELL_EVENT_4 = new SpellEvent(4, "Чары", "Заставляет объект левитировать");
+    public static final SpellEvent SPELL_EVENT_5 = new SpellEvent(5, "Трансфигурация", "Делает старше");
+
+    public static final HogwartsSpells HOGWARTS_SPELLS = new HogwartsSpells();
+
+    public static void main(String[] args) {
+        HOGWARTS_SPELLS.addSpellEvent(SPELL_EVENT_1.getId(), SPELL_EVENT_1.getEventType(), SPELL_EVENT_1.getAction());
+        HOGWARTS_SPELLS.addSpellEvent(SPELL_EVENT_2.getId(), SPELL_EVENT_2.getEventType(), SPELL_EVENT_2.getAction());
+        HOGWARTS_SPELLS.addSpellEvent(SPELL_EVENT_3.getId(), SPELL_EVENT_3.getEventType(), SPELL_EVENT_3.getAction());
+        HOGWARTS_SPELLS.addSpellEvent(SPELL_EVENT_4.getId(), SPELL_EVENT_4.getEventType(), SPELL_EVENT_4.getAction());
+        HOGWARTS_SPELLS.addSpellEvent(SPELL_EVENT_5.getId(), SPELL_EVENT_5.getEventType(), SPELL_EVENT_5.getAction());
+
+        HOGWARTS_SPELLS.printAllSpellEvents();
+    }
+}
