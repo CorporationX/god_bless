5f581c7c732befd91bca1ab7a7c20a79f4153cc4
muslim <xammuslim06@gmail.com>
2024-12-07 19:54:50 +0300

    created a method for grouping users by age

---
+++ src/main/java/school/faang/Task_groupingUsersByAge/Main.java
@@ -0,0 +1,23 @@
+package school.faang.Task_groupingUsersByAge;
+
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+public class Main {
+    public static void main(String[] args) {
+        List<User> usersList = new ArrayList<>();
+        usersList.add(new User("Alex", 23, "Epam", "Lenina 21"));
+        usersList.add(new User("Viktor", 44, "T-bank", "Lenina 23"));
+        usersList.add(new User("Max", 14, "Sber", "Lenina 22"));
+        usersList.add(new User("Serega", 61, "Sber", "Lenina 24"));
+        usersList.add(new User("Egor", 32, "Yandex", "Lenina 25"));
+
+        Map<Integer, List<User>> groupedUsers = User.groupUsers(usersList);
+        for (Map.Entry<Integer, List<User>> currentUser : groupedUsers.entrySet()) {
+            System.out.println("Age: " + currentUser.getKey() + ", Users: " + currentUser.getValue());
+        }
+    }
+}
+
