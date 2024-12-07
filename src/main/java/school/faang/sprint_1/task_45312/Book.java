aa5f1e63463c0eb098221e985421ab69faa591ff
tatgricenko <tattgricenko@gmail.com>
2024-12-07 14:05:24 +0300

    Библиотека Вестероса

---
+++ src/main/java/school/faang/sprint_1/task_45312/Book.java
@@ -0,0 +1,33 @@
+package school.faang.sprint_1.task_45312;
+
+import java.util.Objects;
+
+public class Book {
+    private String title;
+    private String author;
+    int year;
+
+    public Book(String title, String author, int year) {
+        this.title = title;
+        this.author = author;
+        this.year = year;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Book book = (Book) o;
+        return year == book.year && Objects.equals(title, book.title) && Objects.equals(author, book.author);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(title, author, year);
+    }
+
+    @Override
+    public String toString() {
+        return String.format("\"%s\" %s %d", title, author, year);
+    }
+}
