6ed89af1f68eb4018d1ecc455b32ac1694d6123b
KsushaTarasova <ksutaras543@mail.ru>
2024-12-07 20:24:06 +0300

    Create records instead classes

--- src/main/java/school/faang/task_43519/Student.java
+++ src/main/java/school/faang/task_43519/Student.java
@@ -1,11 +1,4 @@
 package school.faang.task_43519;
 
-import lombok.AllArgsConstructor;
-import lombok.Data;
-
-@Data
-@AllArgsConstructor
-public class Student {
-    private int id;
-    private String name;
+public record Student(int id, String name) {
 }
--- src/main/java/school/faang/task_43519/Subject.java
+++ src/main/java/school/faang/task_43519/Subject.java
@@ -1,11 +1,4 @@
 package school.faang.task_43519;
 
-import lombok.AllArgsConstructor;
-import lombok.Data;
-
-@Data
-@AllArgsConstructor
-public class Subject {
-    private int id;
-    private String name;
+public record Subject(int id, String name) {
 }
