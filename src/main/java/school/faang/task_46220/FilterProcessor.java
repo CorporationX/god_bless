4183017ec647f0bee9c9c1dad082a7da96867b16
Alfir Rakhimov <exprnc7@gmail.com>
2024-12-07 19:50:59 +0500

    Фильтры Instagram

---
+++ src/main/java/school/faang/task_46220/FilterProcessor.java
@@ -0,0 +1,15 @@
+package school.faang.task_46220;
+
+import school.faang.task_46220.model.Image;
+
+import java.util.function.Function;
+
+public class FilterProcessor {
+    public Image applyFilter(Image image, Function<Image, Image> imageFilter) {
+        return imageFilter.apply(image);
+    }
+
+    public Function<Image, Image> combineFilters(Function<Image, Image> firstFilter, Function<Image, Image> secondFilter) {
+        return firstFilter.andThen(secondFilter);
+    }
+}
---
+++ src/main/java/school/faang/task_46220/model/Image.java
@@ -0,0 +1,15 @@
+package school.faang.task_46220.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@ToString
+@AllArgsConstructor
+public class Image {
+    private final String name;
+    private final String description;
+}
