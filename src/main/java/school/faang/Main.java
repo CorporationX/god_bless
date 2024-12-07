523ccceb4fdd840f87b7a168639a2935688607df
Azamat Beisembay <azamat.beisembay@gmail.com>
2024-12-07 23:34:54 +0500

    azamat71-BJS2-43760 Исправил названия переменных в Main.

--- src/main/java/school/faang/Main.java
+++ src/main/java/school/faang/Main.java
@@ -8,9 +8,9 @@ public class Main {
 
         // https://faang-school.atlassian.net/browse/BJS2-43760
 
-        User Anny = new User("Anny", 25, "Google", "Amsterdam");
-        User Kenny = new User("Kenny", 29, "Uber", "London");
-        User Louis = new User("Louis", 32, "Amazon", "New York");
+        User anny = new User("Anny", 25, "Google", "Amsterdam");
+        User kenny = new User("Kenny", 29, "Uber", "London");
+        User louis = new User("Louis", 32, "Amazon", "New York");
 
     }
 }
