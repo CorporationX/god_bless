68a0dc7c038f5bae2d2299d988b4e2128d4adc17
Leonid Zhuravel <zhuravel_leonid@yahoo.com>
2024-12-07 07:33:43 +0200

    removed arrays of class updated code to restore functionality added most efficient ways to work with arrays added null array check

--- src/main/java/school/faang/bjs243596/Example.java
+++ src/main/java/school/faang/bjs243596/Example.java
@@ -1,39 +1,59 @@
 package school.faang.bjs243596;
 
 import java.util.Arrays;
-import java.util.Collection;
 import java.util.Collections;
 
 public class Example {
-    public static Integer[] reverseArray(Integer[] inputArray) {
+    public static int[] reverseArray(int[] inputArray) {
         validateEmptyArray(inputArray);
 
         Collections.reverse(Arrays.asList(inputArray));
         return inputArray;
     }
 
-    public static int[] reverseArray(Integer[] inputArray, boolean useIteration) {
+    public static int[] reverseArray(int[] inputArray, String isMostEfficient) {
+        validateEmptyArray(inputArray);
+
+        int first = 0;
+        int last = inputArray.length - 1;
+
+        while (first < last) {
+            int oldRightValue = inputArray[first];
+            inputArray[first] = inputArray[last];
+            inputArray[last] = oldRightValue;
+            first++;
+            last--;
+        }
+
+        return inputArray;
+    }
+
+    public static int[] reverseArray(int[] inputArray, boolean useIteration) {
         validateEmptyArray(inputArray);
 
         int arrayLength = inputArray.length;
-        int[] result = new int[arrayLength];
 
-        for (int i = 0; i < arrayLength; i++) {
-            result[i] = inputArray[arrayLength - i - 1];
+        for (int i = 0; i < arrayLength / 2; i++) {
+            int oldValue = inputArray[i];
+            inputArray[i] = inputArray[arrayLength - i - 1];
+            inputArray[arrayLength - i - 1] = oldValue;
         }
 
-        return result;
+        return inputArray;
     }
 
-    public static int[] reverseArray(Integer[] inputArray, int useStream) {
+    public static int[] reverseArray(int[] inputArray, int useStream) {
         validateEmptyArray(inputArray);
 
-        Collections.reverse(Arrays.asList(inputArray));
-        return Arrays.stream(inputArray).mapToInt(Integer::intValue).toArray();
+        //Collections.reverse(Arrays.asList(inputArray));
+        //return Arrays.stream(inputArray).mapToInt(Integer::intValue).toArray();
+        return Arrays.stream(inputArray)
+                .boxed().sorted(Collections.reverseOrder())
+                .mapToInt(Integer::intValue).toArray();
     }
 
-    private static void validateEmptyArray(Integer[] inputArray) {
-        if (inputArray.length < 1) {
+    private static void validateEmptyArray(int[] inputArray) {
+        if (inputArray == null || inputArray.length < 1) {
             throw new IllegalArgumentException("Input array cannot be empty!");
         }
     }
--- src/main/java/school/faang/bjs243596/Main.java
+++ src/main/java/school/faang/bjs243596/Main.java
@@ -6,7 +6,7 @@ public class Main {
 
     public static void main(String[] args) {
 
-        Integer [] array = {1, 2, 3, 4, 5, 6};
-        System.out.println(Arrays.toString(Example.reverseArray(array)));
+        int[] array = {1, 2, 3, 4, 5, 6};
+        System.out.println(Arrays.toString(Example.reverseArray(array, "isMostEfficient")));
     }
 }
