22749fa0cc036f1dd8b8796a1e5c34011fb0bae1
Maria Bortsova <bortsovamariia@gmail.com>
2024-12-07 22:15:26 +0300

    add optimize data center

---
+++ src/test/java/school/faang/task_45018/EnergyEfficiencyOptimizationStrategyTest.java
@@ -0,0 +1,21 @@
+package school.faang.task_45018;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static school.faang.task_45018.DataInitializerUtils.initDataCenter;
+
+class EnergyEfficiencyOptimizationStrategyTest {
+    private final DataCenter dataCenter = new DataCenter(initDataCenter());
+    private final DataCenterService dataCenterService = new DataCenterService(dataCenter,
+            new EnergyEfficiencyOptimizationStrategy());
+
+    @Test
+    void shouldOptimize() {
+        dataCenterService.optimize();
+
+        double expectedEnergyConsumption = 4;
+        double actualEnergyConsumption = dataCenter.getDataCenters().get(0).getEnergyConsumption();
+        assertEquals(expectedEnergyConsumption, actualEnergyConsumption);
+    }
+}
