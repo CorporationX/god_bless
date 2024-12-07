475de91e71de0460ca0248bb4a4a94541283f327
RGZ_TTT <rgz.ttt@mail.ru>
2024-12-07 14:36:48 +0700

    Added validation on class User

---
+++ src/main/java/school/faang/task_43770/ValidAddresses.java
@@ -0,0 +1,7 @@
+package school.faang.task_43770;
+
+import java.util.Set;
+
+public class ValidAddresses {
+    public static final Set<String> VALID_ADDRESSES = Set.of("London", "New-York", "Amsterdam");
+}
