9ce7e3768af0694ee276413c56b746060e92cf2e
Kir34 <kirnovikof1@gmail.com>
2024-12-07 11:14:13 +0300

    задание с группировкой юзеров по возрасту

---
+++ src/main/java/school/faang/task_BJS2_43736/Main.java
@@ -0,0 +1,9 @@
+package school.faang.task_BJS2_43736;
+
+public class Main {
+    public static void main(String[] args){
+        User user = new User("Igor", 22, "Google", "Amsterdam");
+        System.out.println(user);
+
+    }
+}
