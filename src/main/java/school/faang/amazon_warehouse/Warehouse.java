c59b7a5de4237a06f55c2aed1bdd9f850a5d0f9d
Ivan Baev <ibai86@yandex.ru>
2024-12-07 19:17:55 +0300

    в процессе 2

--- src/main/java/school/faang/amazon_warehouse/Warehouse.java
+++ src/main/java/school/faang/amazon_warehouse/Warehouse.java
@@ -1,20 +1,16 @@
 package school.faang.amazon_warehouse;
 
 import java.util.HashSet;
-import java.util.Iterator;
 import java.util.Set;
 
 public class Warehouse {
     private final Set<Product> products = new HashSet<>();
 
     public void addItem(String category, String name) {
-        products.add(new Product(name, category));
+
     }
 
     public void printAllItems() {
-        Iterator<Product> iterator = products.iterator();
-        while (iterator.hasNext()) {
-            System.out.println(iterator.next());
-        }
+        products.forEach(System.out::println);
     }
 }
