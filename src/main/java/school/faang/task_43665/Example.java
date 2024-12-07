5d261eef89a21b495c524a52d783acaf3e5f0e8e
Maria Bortsova <bortsovamariia@gmail.com>
2024-12-07 22:24:18 +0300

    fix after review

--- src/main/java/school/faang/task_43665/Example.java
+++ src/main/java/school/faang/task_43665/Example.java
@@ -1,27 +1,11 @@
 package school.faang.task_43665;
 
-import java.util.Arrays;
-
 public class Example {
-    public static void main(String[] args) {
-        int[] reverse = reverse(new int[]{1, 2, 3, 4, 5});
-        System.out.println(Arrays.toString(reverse));
-
-        int[] reverseSecond = reverseInPlace(new int[]{1, 2, 3, 4, 5});
-        System.out.println(Arrays.toString(reverseSecond));
-    }
 
     public static int[] reverse(int[] nums) {
-        int[] result = new int[nums.length];
-
-        for (int i = 0; i < nums.length; i++) {
-            result[i] = nums[nums.length - 1 - i];
+        if (nums == null || nums.length == 0) {
+            throw new IllegalArgumentException("Array doesn't exist or empty");
         }
-
-        return result;
-    }
-
-    public static int[] reverseInPlace(int[] nums) {
         for (int i = 0; i < nums.length / 2; i++) {
             int temp = nums[i];
             nums[i] = nums[nums.length - 1 - i];
---
+++ src/main/java/school/faang/task_43665/Main.java
@@ -0,0 +1,11 @@
+package school.faang.task_43665;
+
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) {
+
+        int[] reverseSecond = Example.reverse(new int[]{1, 2, 3, 4, 5});
+        System.out.println(Arrays.toString(reverseSecond));
+    }
+}
