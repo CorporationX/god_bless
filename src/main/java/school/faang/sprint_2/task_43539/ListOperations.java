191900ab3944dd8f7bc2475da905d839c7e1dce4
KsushaTarasova <ksutaras543@mail.ru>
2024-12-07 19:59:39 +0300

    Training stream

---
+++ src/main/java/school/faang/sprint_2/task_43539/ListOperations.java
@@ -0,0 +1,43 @@
+package school.faang.sprint_2.task_43539;
+
+import java.util.Comparator;
+import java.util.List;
+import java.util.function.Predicate;
+
+public class ListOperations {
+    public static int sumOfEvenNumbers(List<Integer> numbers) {
+        return numbers.stream().mapToInt(Integer::intValue).filter(n -> n % 2 == 0).sum();
+    }
+
+    public static int max(List<Integer> numbers) {
+        return numbers.stream().mapToInt(Integer::intValue).max().orElse(0);
+    }
+
+    public static double average(List<Integer> numbers) {
+        return numbers.stream().mapToInt(Integer::intValue).average().orElse(0);
+    }
+
+    public static int countStringsStartingWith(List<String> strings, char ch) {
+        return (int) strings.stream().filter(s -> s.startsWith(String.valueOf(ch))).count();
+    }
+
+    public static List<String> filterStringContainingSubstring(List<String> strings, String substring) {
+        return strings.stream().filter(s -> s.contains(substring)).toList();
+    }
+
+    public static List<String> sortByLength(List<String> strings) {
+        return strings.stream().sorted(Comparator.comparingInt(String::length)).toList();
+    }
+
+    public static boolean allMatchCondition(List<Integer> numbers, Predicate<Integer> condition) {
+        return numbers.stream().allMatch(condition);
+    }
+
+    public static int findMinGreaterThan(List<Integer> numbers, int lowerLimit) {
+        return numbers.stream().filter(num -> num > lowerLimit).min(Integer::compareTo).orElse(0);
+    }
+
+    public static List<Integer> convertToLengths(List<String> strings){
+        return strings.stream().map(String::length).toList();
+    }
+}
