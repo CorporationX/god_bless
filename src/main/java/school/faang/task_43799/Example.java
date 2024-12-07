8b6ac9f71a118b917e802ff52bd75134b598cc1e
vvz3zzv <vladislav.v.zhurbin@gmail.com>
2024-12-07 19:30:29 +0300

    BJS2-43799: Развернитесь!

---
+++ src/main/java/school/faang/task_43799/Example.java
@@ -0,0 +1,12 @@
+package school.faang.task_43799;
+
+public class Example {
+    public static int[] reversArray(int[] array) {
+        for (int i = 0; i < (array.length - 1) / 2; i++) {
+            int first = array[array.length - 1 - i];
+            array[array.length - 1 - i] = array[i];
+            array[i] = first;
+        }
+        return array;
+    }
+}
---
+++ src/main/java/school/faang/task_43799/Main.java
@@ -0,0 +1,12 @@
+package school.faang.task_43799;
+
+import java.util.Arrays;
+
+import static school.faang.task_43799.Example.reversArray;
+
+public class Main {
+    public static void main(String[] args) {
+        int[] newArray = {1, 2, 3, 4, 5};
+        System.out.println(Arrays.toString(reversArray(newArray)));
+    }
+}
