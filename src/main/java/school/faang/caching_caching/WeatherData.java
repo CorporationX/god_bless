aa1a0e012074a6294466da9f7f42c96f71710f07
AesaQ <ilyaovchinnikov2095@gmail.com>
2024-12-07 17:30:48 +0500

    bugfix

--- src/main/java/school/faang/caching_caching/WeatherData.java
+++ src/main/java/school/faang/caching_caching/WeatherData.java
@@ -1,7 +1,4 @@
 package school.faang.caching_caching;
 
-import lombok.EqualsAndHashCode;
-
-@EqualsAndHashCode(callSuper = true)
 public record WeatherData(String city, double temperature, double humidity, long timestamp) {
 }
