2ebda8e6c64198936bc636cdcffdc7b7a4a745e3
Leysan Pirkina <missleysan1609@gmail.com>
2024-12-08 00:05:37 +0300

    Абстракция,Абстракция, исправлены ошибки

--- src/main/java/school/faang/task_43857/Archer.java
+++ src/main/java/school/faang/task_43857/Archer.java
@@ -1,20 +1,18 @@
 package school.faang.task_43857;
 
-public class Archer extends Character {
-    public Archer(String name) {
-        super(name);
-        setPower(3);
-        setAgility(10);
-        setIntellect(5);
-    }
+import static school.faang.task_43857.Properties.ARCHER_AGILITY;
+import static school.faang.task_43857.Properties.ARCHER_INTELLECT;
+import static school.faang.task_43857.Properties.ARCHER_POWER;
 
-    public Archer(String name, int power, int agility, int intellect) {
-        super(name, power, agility, intellect);
+public class Archer extends Character {
+
+    public Archer(String name) {
+        super(name, ARCHER_POWER, ARCHER_AGILITY, ARCHER_INTELLECT);
     }
 
     @Override
     protected void attack(Character character) {
         character.setHealth(character.getHealth() - this.getAgility());
-        System.out.println(character.getHealth());
+        System.out.println(character.getName() + " has the health " + character.getHealth());
     }
 }
--- src/main/java/school/faang/task_43857/Character.java
+++ src/main/java/school/faang/task_43857/Character.java
@@ -1,19 +1,27 @@
 package school.faang.task_43857;
 
+import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.Setter;
 
-@Getter
-@Setter
+import static school.faang.task_43857.Properties.AGILITY;
+import static school.faang.task_43857.Properties.INTELLECT;
+import static school.faang.task_43857.Properties.POWER;
+
+@Getter(AccessLevel.PROTECTED)
+@Setter(AccessLevel.PROTECTED)
 public abstract class Character {
-    protected String name;
-    protected int power;
-    protected int agility;
-    protected int intellect;
-    protected int health = 100;
+    private final String name;
+    private final int power;
+    private final int agility;
+    private final int intellect;
+    private int health = 100;
 
     public Character(String name) {
         this.name = name;
+        this.power = POWER;
+        this.agility = AGILITY;
+        this.intellect = INTELLECT;
     }
 
     public Character(String name, int power, int agility, int intellect) {
@@ -24,5 +32,4 @@ public abstract class Character {
     }
 
     protected abstract void attack(Character character);
-
 }
