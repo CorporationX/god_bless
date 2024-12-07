9beba826e401f2374f3e5ce0765cb27cbb331c1e
muslim <xammuslim06@gmail.com>
2024-12-07 21:04:43 +0300

    fixed errors code style

--- src/main/java/school/faang/doublecachecache/Main.java
+++ src/main/java/school/faang/doublecachecache/Main.java
@@ -7,27 +7,22 @@ public class Main {
 
     public static void main(String[] args) {
         StudentDatabase database = new StudentDatabase();
-
-        Student student1 = new Student(1, "Misha");
-        Student student2 = new Student(2, "Max");
         Subject subject1 = new Subject(1, "Математика");
         Subject subject2 = new Subject(2, "Физика");
 
         Map<Subject, Integer> grades2 = new HashMap<>();
+        Student student2 = new Student(2, "Max");
         grades2.put(subject1, 3);
         database.addStudent(student2, grades2);
 
         Map<Subject, Integer> grades1 = new HashMap<>();
         grades1.put(subject1, 5);
         grades1.put(subject2, 4);
+        Student student1 = new Student(1, "Misha");
         database.addStudent(student1, grades1);
-
-
-
 
         System.out.println("Список всех студентов и их оценки: ");
         database.printAllStudents();
-
         database.addSubjectForStudent(student1, subject2, 5);
         System.out.println("\nОбновленный список студентов и их оценки: ");
         database.printAllStudents();
