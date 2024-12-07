8803cbd620a9a6f949b1e252f5b90b2db236bec0
Maksim-VV <laulmv11@gmail.com>
2024-12-07 13:31:36 +0300

    User registration format try cacth

--- src/main/java/school/faang/bjs243751/Main.java
+++ src/main/java/school/faang/bjs243751/Main.java
@@ -3,12 +3,10 @@ package school.faang.bjs243751;
 public class Main {
     public static void main(String[] args) {
 
-        try{
+        try {
             new User("Bob", 25, "Google", "London");
             new User("Bob", 25, "Google", "");
-        }
-        catch (IllegalArgumentException ex)
-        {
+        } catch (IllegalArgumentException ex) {
             System.out.println("Error!" + ex);
         }
     }
