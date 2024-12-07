9d3fee0557e4fbba0d71d6f92a76fb8dfc20370c
Alfir Rakhimov <exprnc7@gmail.com>
2024-12-07 16:53:53 +0500

    Поправил code style

--- src/main/java/school/faang/task_45442/Main.java
+++ src/main/java/school/faang/task_45442/Main.java
@@ -5,6 +5,9 @@ import school.faang.task_45442.model.Spell;
 public class Main {
     public static void main(String[] args) {
         SpellCaster spellCaster = new SpellCaster();
-        spellCaster.cast(Spell.WINGARDIUM_LEVIOSA, (spell) -> "I cast a " + spell.name() + " and I started levitating!");
+        spellCaster.cast(
+                Spell.WINGARDIUM_LEVIOSA,
+                (spell) -> "I cast a " + spell.name() + " and I started levitating!"
+        );
     }
 }
