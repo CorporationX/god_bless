caf2ef2d7a5cff88df3748e39edee8515d1134ce
Maksim-VV <laulmv11@gmail.com>
2024-12-07 13:15:11 +0300

    User registration add try catch

--- src/main/java/school/faang/bjs243751/Main.java
+++ src/main/java/school/faang/bjs243751/Main.java
@@ -2,8 +2,14 @@ package school.faang.bjs243751;
 
 public class Main {
     public static void main(String[] args) {
-        new User("Bob", 25, "Google", "London");
-        new User("Bob", 25, "Google", "");
-    }
 
+        try{
+            new User("Bob", 25, "Google", "London");
+            new User("Bob", 25, "Google", "");
+        }
+        catch (IllegalArgumentException ex)
+        {
+            System.out.println("Error!" + ex);
+        }
+    }
 }
