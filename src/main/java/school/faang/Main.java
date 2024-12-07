2de13bf56a2a34415bd560c972c8769629c95b45
Azamat Beisembay <azamat.beisembay@gmail.com>
2024-12-07 23:28:39 +0500

    azamat71-BJS2-43760 Создал класс User для пользователей, в конструкторе добавил валидации для каждого элемента класса.

--- src/main/java/school/faang/Main.java
+++ src/main/java/school/faang/Main.java
@@ -1,8 +1,16 @@
 package school.faang;
 
-import java.util.Arrays;
+
+import school.faang.userregister.User;
 
 public class Main {
     public static void main(String[] args) {
+
+        // https://faang-school.atlassian.net/browse/BJS2-43760
+
+        User Anny = new User("Anny", 25, "Google", "Amsterdam");
+        User Kenny = new User("Kenny", 29, "Uber", "London");
+        User Louis = new User("Louis", 32, "Amazon", "New York");
+
     }
 }
