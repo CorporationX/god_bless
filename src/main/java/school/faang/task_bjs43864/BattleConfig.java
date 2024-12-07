31394a8c31298b6dfbac31f84aa4ab6eb48df84d
mixa <mikle.kuznecov@yandex.ru>
2024-12-07 20:49:20 +0300

    refactor part 2

--- src/main/java/school/faang/task_bjs43864/BattleConfig.java
+++ src/main/java/school/faang/task_bjs43864/BattleConfig.java
@@ -1,6 +1,10 @@
 package school.faang.task_bjs43864;
 
 public class BattleConfig {
+    public static final int DEFAULT_POWER = 5;
+    public static final int DEFAULT_AGILITY = 5;
+    public static final int DEFAULT_INTELLIGENCE = 5;
+
     public static final int WARRIOR_POWER = 10;
     public static final int WARRIOR_AGILITY = 5;
     public static final int WARRIOR_INTELLIGENCE = 3;
--- src/main/java/school/faang/task_bjs43864/Character.java
+++ src/main/java/school/faang/task_bjs43864/Character.java
@@ -1,5 +1,9 @@
 package school.faang.task_bjs43864;
 
+import static school.faang.task_bjs43864.BattleConfig.DEFAULT_AGILITY;
+import static school.faang.task_bjs43864.BattleConfig.DEFAULT_INTELLIGENCE;
+import static school.faang.task_bjs43864.BattleConfig.DEFAULT_POWER;
+
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.Setter;
@@ -7,16 +11,23 @@ import lombok.Setter;
 @Getter(AccessLevel.PROTECTED)
 @Setter(AccessLevel.PROTECTED)
 public abstract class Character {
-    private String name;
+    private final String name;
     private final int power;
     private final int agility;
     private final int intelligence;
     private int health = 100;
 
-    public Character(String name, int power, int dexterity, int intelligence) {
+    public Character(String name) {
+        this.name = name;
+        this.power = DEFAULT_POWER;
+        this.agility = DEFAULT_AGILITY;
+        this.intelligence = DEFAULT_INTELLIGENCE;
+    }
+
+    public Character(String name, int power, int agility, int intelligence) {
         this.name = name;
         this.power = power;
-        this.agility = dexterity;
+        this.agility = agility;
         this.intelligence = intelligence;
     }
 
