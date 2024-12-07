ead33b228617abd9ab999cb1dd406f665c579960
narol01 <kmingogod@gmail.com>
2024-12-07 18:34:50 +0100

    BJS2-44845

--- src/main/java/school/faang/task_44845/Main.java
+++ src/main/java/school/faang/task_44845/Main.java
@@ -6,7 +6,6 @@ public class Main {
     public static void main(String[] args) {
         WeatherProvider provider = new WeatherService();
 
-
         WeatherCacheTemplate standardCache = new StandardWeatherCache(provider);
 
         String city = "New York";
@@ -15,7 +14,8 @@ public class Main {
         WeatherData standardData1 = standardCache.getWeatherData(city, maxCacheAgeMillis);
         System.out.println("1st fetch: " + standardData1.getHumidity() + " " + standardData1.getTemperature());
         WeatherData standardData2 = standardCache.getWeatherData(city, maxCacheAgeMillis);
-        System.out.println("2nd fetch (from cache): " + standardData2.getHumidity() + " " + standardData2.getTemperature());
+        System.out.println(
+                "2nd fetch (from cache): " + standardData2.getHumidity() + " " + standardData2.getTemperature());
 
         try {
             Thread.sleep(6000);
@@ -24,7 +24,8 @@ public class Main {
         }
 
         WeatherData standardData3 = standardCache.getWeatherData(city, maxCacheAgeMillis);
-        System.out.println("3rd fetch (fresh data):" + standardData3.getHumidity() + " " + standardData3.getTemperature());
+        System.out.println(
+                "3rd fetch (fresh data):" + standardData3.getHumidity() + " " + standardData3.getTemperature());
 
         WeatherCacheTemplate frequentCache = new FrequentUpdateWeatherCache(provider);
 
@@ -32,9 +33,8 @@ public class Main {
         WeatherData frequentData1 = frequentCache.getWeatherData(city, maxCacheAgeMillis);
         System.out.println("1st fetch: " + frequentData1.getHumidity() + " " + frequentData1.getTemperature());
         WeatherData frequentData2 = frequentCache.getWeatherData(city, maxCacheAgeMillis);
-        System.out.println("2nd fetch (always fresh): " + frequentData2.getHumidity() + " " + frequentData2.getTemperature());
-
-
+        System.out.println(
+                "2nd fetch (always fresh): " + frequentData2.getHumidity() + " " + frequentData2.getTemperature());
     }
 
 }
