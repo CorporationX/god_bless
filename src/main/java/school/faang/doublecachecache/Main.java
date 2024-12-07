083911f4bd7275fd698559e9832817cd1f9a82e8
muslim <xammuslim06@gmail.com>
2024-12-07 20:58:56 +0300

    fixed errors code style

--- src/main/java/school/faang/doublecachecache/Main.java
+++ src/main/java/school/faang/doublecachecache/Main.java
@@ -10,18 +10,19 @@ public class Main {
 
         Student student1 = new Student(1, "Misha");
         Student student2 = new Student(2, "Max");
-
         Subject subject1 = new Subject(1, "Математика");
         Subject subject2 = new Subject(2, "Физика");
+
+        Map<Subject, Integer> grades2 = new HashMap<>();
+        grades2.put(subject1, 3);
+        database.addStudent(student2, grades2);
 
         Map<Subject, Integer> grades1 = new HashMap<>();
         grades1.put(subject1, 5);
         grades1.put(subject2, 4);
         database.addStudent(student1, grades1);
 
-        Map<Subject, Integer> grades2 = new HashMap<>();
-        grades2.put(subject1, 3);
-        database.addStudent(student2, grades2);
+
 
 
         System.out.println("Список всех студентов и их оценки: ");
--- src/main/java/school/faang/doublecachecache/Student.java
+++ src/main/java/school/faang/doublecachecache/Student.java
@@ -17,8 +17,12 @@ public class Student {
 
     @Override
     public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
         Student student = (Student) o;
         return id == student.id && Objects.equals(name, student.name);
     }
