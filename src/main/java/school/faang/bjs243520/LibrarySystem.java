03a2468594fd73fb3b917e43bed3859dbb6a6a54
Leonid Zhuravel <zhuravel_leonid@yahoo.com>
2024-12-07 11:34:23 +0200

    null handler added

--- src/main/java/school/faang/bjs243520/LibrarySystem.java
+++ src/main/java/school/faang/bjs243520/LibrarySystem.java
@@ -1,31 +1,35 @@
 package school.faang.bjs243520;
 
-import lombok.experimental.UtilityClass;
-
 import java.util.HashMap;
 import java.util.Map;
 
 public class LibrarySystem {
 
-    private static Map<Book, String> booksLibrary = new HashMap<>();
+    private static final Map<Book, String> booksLibrary = new HashMap<>();
 
     public void addBook(String title, String author, int year, String location) {
         Book book = new Book(title, author, year);
-        booksLibrary.put(book, location);
-        System.out.println(book + " added to Library: " + location);
+        String locationExist = booksLibrary.putIfAbsent(book, location);
+        if (locationExist == null) {
+            System.out.println(book + " added to Library: " + location);
+        } else {
+            System.out.println(book + " was already found in Library: " + locationExist);
+        }
     }
 
     public void removeBook(String title, String author, int year) {
         Book book = new Book(title, author, year);
         booksLibrary.remove(book);
         System.out.println(book + " removed");
-
     }
 
     public void findBook(String title, String author, int year) {
         Book book = new Book(title, author, year);
-        String location = booksLibrary.get(book);
-        System.out.println(book + " located in Library: " + location);
+        if (booksLibrary.containsKey(book)) {
+            System.out.println(book + " located in Library: " + booksLibrary.get(book));
+        } else {
+            System.out.println(book + " was not found!");
+        }
     }
 
     public void printAllBooks() {
