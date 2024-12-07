c8e6afe7db19982425e2e8f2e143e9acfd9f8651
vvz3zzv <vladislav.v.zhurbin@gmail.com>
2024-12-07 21:26:54 +0300

    BJS2-43799: Развернитесь!
    -исправления checkstyle

--- src/main/java/school/faang/task_43799/Example.java
+++ src/main/java/school/faang/task_43799/Example.java
@@ -4,7 +4,7 @@ public class Example {
     public static int[] reversArray(int[] array) {
         if (array == null) {
             throw new IllegalArgumentException("Input array cannot be null.");
-        } else if(array.length == 0) {
+        } else if (array.length == 0) {
             return array;
         } else {
             for (int i = 0; i < (array.length - 1) / 2; i++) {
