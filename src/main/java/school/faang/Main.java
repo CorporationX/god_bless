f987e765b1a4223c59f3ed6f734cdba1ecbc5daa
Maksim-VV <laulmv11@gmail.com>
2024-12-07 23:27:41 +0300

    Grouping users by age - edit main like master

--- src/main/java/school/faang/Main.java
+++ src/main/java/school/faang/Main.java
@@ -1,12 +1,5 @@
 package school.faang;
 
-
-import school.faang.bjs243968.User;
-
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Map;
-
 public class Main {
     public static void main(String[] args) {
 
