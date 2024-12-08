5f5a0e87a01b0aeedaa927feae3bec04486b4750
Anton Graf <ant0ngraf@yandex.ru>
2024-12-08 15:36:04 +0600

    Добавляет модификаторы доступы, убирает Equals&HashCode, добавляет генератор Id

--- src/main/java/school/faang/bjs_45147/Product.java
+++ src/main/java/school/faang/bjs_45147/Product.java
@@ -1,41 +1,22 @@
 package school.faang.bjs_45147;
 
-import lombok.*;
-import lombok.experimental.FieldDefaults;
-
-import java.util.Objects;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
 
 @Getter
 @Setter
 @AllArgsConstructor
 @ToString
-@FieldDefaults(level = AccessLevel.PRIVATE)
 public class Product {
-    final String name;
-    final String category;
-    int id;
+    private final String name;
+    private final String category;
+    private final int id;
 
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) {
-            return true;
-        }
-        if (o == null || getClass() != o.getClass()) {
-            return false;
-        }
-
-        Product product = (Product) o;
-
-        if (!Objects.equals(name, product.name)) {
-            return false;
-        }
-        return Objects.equals(category, product.category);
-    }
-
-    @Override
-    public int hashCode() {
-        int result = name != null ? name.hashCode() : 0;
-        result = 31 * result + (category != null ? category.hashCode() : 0);
-        return result;
+    public Product(String name, String category) {
+        this.name = name;
+        this.category = category;
+        this.id = GeneratorId.createProductId();
     }
 }
