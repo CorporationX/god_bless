c325d3d128a6846a87f75d30c6b7a71bb35e3388
Stepan Evangulov <stevangulov@gmail.com>
2024-12-08 09:40:37 +0200

    god bless

--- src/main/java/school/faang/task_45116/Category.java
+++ src/main/java/school/faang/task_45116/Category.java
@@ -1,10 +1,5 @@
 package school.faang.task_45116;
 
 public enum Category {
-    ELECTRONICS, CLOTHING, FOOD, BOOKS;
-
-    @Override
-    public String toString() {
-        return name().charAt(0) + name().substring(1).toLowerCase();
-    }
+    ELECTRONICS, CLOTHING, FOOD, BOOKS
 }
