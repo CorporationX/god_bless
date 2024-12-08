f594f026fa1997be0f00741e3fdb8fb0004b30d9
Maria Bortsova <bortsovamariia@gmail.com>
2024-12-08 19:32:35 +0300

    task filters instagram impl

---
+++ src/main/java/school/faang/task_46234/Image.java
@@ -0,0 +1,11 @@
+package school.faang.task_46234;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
+@Getter
+public class Image {
+    private final String name;
+    private final String description;
+}
