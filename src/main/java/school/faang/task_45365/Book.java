fd8091e4455318523bc8f2f7175b100051353101
shamshaev <an.shamshaev@gmail.com>
2024-12-09 01:45:25 +0300

    Solve task_45365

---
+++ src/main/java/school/faang/task_45365/Book.java
@@ -0,0 +1,32 @@
+package school.faang.task_45365;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+import java.util.Objects;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@ToString
+public class Book {
+    private String title;
+    private String author;
+    private int year;
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        Book book = (Book) o;
+        return year == book.year && Objects.equals(title, book.title) && Objects.equals(author, book.author);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(title, author, year);
+    }
+}
