20a03e4c107aad021d5dafd31b7b737847a9d361
Alexey Gorokhov <almigor@mail.ru>
2024-12-08 17:37:51 +0300

    Секреты дроидов

--- src/main/java/school/faang/sprint2/task46400/Main.java
+++ src/main/java/school/faang/sprint2/task46400/Main.java
@@ -6,22 +6,21 @@ public class Main {
 
     public static void main(String[] args) {
 
-        int encryptionKey1 = 3;
-        int encryptionKey2 = 4;
-        int encryptionKey3 = 1;
-
         Droid r2d2 = new Droid("R2D2");
         Droid c3po = new Droid("C3PO");
         Droid bb8 = new Droid("BB-8");
 
         String message1 = "Join the Rebellion!";
+        int encryptionKey1 = 3;
         r2d2.sendMessage(c3po, message1, encryptionKey1);
         System.out.println("------");
 
         String message2 = "The Death Star is operational!";
+        int encryptionKey2 = 4;
         c3po.sendMessage(r2d2, message2, encryptionKey2);
         System.out.println("------");
 
+        int encryptionKey3 = 1;
         bb8.sendMessage(c3po, "The mission is complete.", encryptionKey3);
         System.out.println("------");
 
