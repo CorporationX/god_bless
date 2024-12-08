4a432059261b514288ec589a55ea0dbd179ab1c0
az3l1t <eternity_cr9p@mail.ru>
2024-12-08 14:46:57 +0300

    BJS2-46233 Фильтры Instagram

---
+++ src/main/java/school/faang/task_46233/FilterProcessor.java
@@ -0,0 +1,19 @@
+package school.faang.task_46233;
+
+import org.apache.commons.lang3.StringUtils;
+
+import java.util.function.Function;
+
+public class FilterProcessor {
+    public Image applyFilter(Image image, Function<Image, Image> filter) {
+        if (StringUtils.isBlank(image.name()) || StringUtils.isBlank(image.description())) {
+            throw new IllegalArgumentException("Image is not in good shape!");
+        }
+        image = filter.apply(image);
+        return image;
+    }
+
+    public Function<Image, Image> combineFilters(Function<Image, Image> filterFirst, Function<Image, Image> filterSecond) {
+        return image -> filterSecond.apply(filterFirst.apply(image));
+    }
+}
---
+++ src/main/java/school/faang/task_46233/Main.java
@@ -0,0 +1,24 @@
+package school.faang.task_46233;
+
+import java.util.function.Function;
+
+public class Main {
+    public static void main(String[] args) {
+        Image origImage = new Image("meWithBro.png", "Just image with bro");
+        FilterProcessor filterProcessor = new FilterProcessor();
+
+        Function<Image, Image> firstFilter = (image -> new Image(origImage.name(), image.description() + " -> with filter : X"));
+        Function<Image, Image> secondFilter = (image -> new Image(origImage.name(), image.description() + " -> with filter : Y"));
+
+        Image imageWithX = filterProcessor.applyFilter(origImage, firstFilter);
+        Image imageWithY = filterProcessor.applyFilter(origImage, secondFilter);
+
+        System.out.println(imageWithX.description());
+        System.out.println(imageWithY.description());
+
+        Function<Image, Image> combinedImage = filterProcessor.combineFilters(firstFilter, secondFilter);
+        Image imageWithXY = filterProcessor.applyFilter(origImage, combinedImage);
+
+        System.out.println(imageWithXY.description());
+    }
+}
