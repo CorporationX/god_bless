2d19459f4f996573e2757654c6500ab1e7971671
Mihail Ermolaev <fitis_rus21@mail.ru>
2024-12-08 10:36:00 +0300

    Исправлены замечания

--- src/main/java/school/faang/task_45300/LibrarySystem.java
+++ src/main/java/school/faang/task_45300/LibrarySystem.java
@@ -7,34 +7,36 @@ import java.util.Map;
 
 @Data
 public class LibrarySystem {
+    HashMap<Book, String> booksMap = new HashMap<>();
+
     public void addBook(String title, String author, int year, String location) {
         Book book = new Book(title, author, year);
-        HashMap<Book, String> booksMap = new HashMap<>();
 
         booksMap.put(book, location);
     }
 
     public void removeBook(String title, String author, int year) {
         Book book = new Book(title, author, year);
-        HashMap<Book, String> booksMap = new HashMap<>();
-
-        booksMap.remove(book);
-    }
-
-    public String findBook(String title, String author, int year) {
-        Book book = new Book(title, author, year);
-        HashMap<Book, String> booksMap = new HashMap<>();
 
         if (booksMap.containsKey(book)) {
-            return booksMap.get(book);
+            booksMap.remove(book);
+            System.out.println("Book: " + title + " " + author + " " + year + " is remove");
         } else {
-            return "Книга не найдена!";
+            System.out.println("Book: " + title + " " + author + " " + year + " for remove not found");
+        }
+    }
+
+    public void findBook(String title, String author, int year) {
+        Book book = new Book(title, author, year);
+
+        if (booksMap.containsKey(book)) {
+            System.out.println("Book: " + title + " " + author + " " + year + " location on " + booksMap.get(book));
+        } else {
+            System.out.println("Book: " + title + " " + author + " " + year + " not found");
         }
     }
 
     public void printAllBooks() {
-        HashMap<Book, String> booksMap = new HashMap<>();
-
         for (Map.Entry<Book, String> entry : booksMap.entrySet()) {
             Book key = entry.getKey();
             String value = entry.getValue();
--- src/main/java/school/faang/task_45300/Main.java
+++ src/main/java/school/faang/task_45300/Main.java
@@ -4,23 +4,16 @@ import java.util.HashMap;
 
 public class Main {
     public static void main(String[] args) {
-        HashMap<Book, String> booksMap = new HashMap<>();
-        Book book1 = new Book("Война и Мир", "Толстой", 2000);
-        Book book2 = new Book("Евгений Онегин", "Пушкин", 2002);
-        Book book3 = new Book("Отцы и дети", "Достоевский", 2000);
-        Book book4 = new Book("Вий", "Горький", 2001);
 
-        booksMap.put(book1, "15");
-        booksMap.put(book2, "10");
-        booksMap.put(book3, "12");
-        booksMap.put(book4, "12");
+        LibrarySystem librarySystem = new LibrarySystem();
+        librarySystem.addBook("Война и Мир", "Толстой", 2000, "15");
+        librarySystem.addBook("Евгений Онегин", "Пушкин", 2002, "10");
+        librarySystem.addBook("Отцы и дети", "Достоевский", 2000, "12");
+        librarySystem.addBook("Вий", "Горький", 2001, "12");
 
-        Book bookForSearch = new Book("Евгений Онегин", "Пушкин", 2002);
-
-        System.out.println("Книга находится на полке: " + booksMap.get(bookForSearch));
-
-        System.out.println(booksMap);
-        booksMap.replace(book4, "22");
-        System.out.println(booksMap);
+        librarySystem.findBook("Евгений Онегин", "Пушкин", 2002);
+        librarySystem.printAllBooks();
+        librarySystem.removeBook("Вий1", "Горький", 2001);
+        librarySystem.printAllBooks();
     }
 }
