31a889f3eb862f99584ab5d7b9b92d71343422dd
Anastasia Dragomirova <nastay.dr2002@gmail.com>
2024-12-08 15:33:19 +0400

    Внесла изменения по замечанию

--- src/main/java/faang/school/godbless/sprint_1/task_43686/Example.java
+++ src/main/java/faang/school/godbless/sprint_1/task_43686/Example.java
@@ -2,6 +2,16 @@ package faang.school.godbless.sprint_1.task_43686;
 
 public class Example {
     public void revers(int[] numbers) {
+        if (numbers == null) {
+            System.out.println("Массив не должен быть null!");
+            return;
+        }
+
+        if (numbers.length < 1) {
+            System.out.println("Массив не должен быть пустым!");
+            return;
+        }
+
         for (int i = 0, j = numbers.length - 1; i < j; i++, j--) {
             int number = numbers[i];
             numbers[i] = numbers[j];
