efd65e553e7747d22e200df54a1422742199d8a7
Alexey Gorokhov <almigor@mail.ru>
2024-12-08 17:30:12 +0300

    Секреты дроидов

---
+++ src/main/java/school/faang/sprint2/task46400/Main.java
@@ -0,0 +1,36 @@
+package school.faang.sprint2.task46400;
+
+import school.faang.sprint2.task46400.model.Droid;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Droid r2d2 = new Droid("R2D2");
+        Droid c3po = new Droid("C3PO");
+        Droid bb8 = new Droid("BB-8");
+
+        String message1 = "Join the Rebellion!";
+        String message2 = "The Death Star is operational!";
+        String message3 = "May the force be with you!";
+
+        int encryptionKey1 = 3;
+        int encryptionKey2 = 4;
+        int encryptionKey3 = 1;
+
+        r2d2.sendMessage(c3po, message1, encryptionKey1);
+        System.out.println("------");
+
+        c3po.sendMessage(r2d2, message2, encryptionKey2);
+        System.out.println("------");
+
+        bb8.sendMessage(c3po, "The mission is complete.", encryptionKey3);
+        System.out.println("------");
+
+        r2d2.sendMessage(r2d2, message3, encryptionKey3);
+        System.out.println("------");
+
+    }
+
+
+}
---
+++ src/main/java/school/faang/sprint2/task46400/model/Droid.java
@@ -0,0 +1,55 @@
+package school.faang.sprint2.task46400.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@AllArgsConstructor
+@Getter
+public class Droid {
+    private final String name;
+
+    public String encryptMessage(String originalMessage, int encryptionKey) {
+        DroidMessageEncryptor encryptor = (message, key) -> {
+            StringBuilder encryptedMessage = new StringBuilder();
+            for (char ch : message.toCharArray()) {
+                if (Character.isLetter(ch)) {
+                    char base = Character.isLowerCase(ch) ? 'a' : 'A';
+                    encryptedMessage.append((char) ((ch - base + key) % 26 + base));
+                } else {
+                    encryptedMessage.append(ch);
+                }
+            }
+            return encryptedMessage.toString();
+        };
+        return encryptor.encryptDecrypt(originalMessage, encryptionKey);
+    }
+
+    public String decryptMessage(String encriptedMessage, int decryptionKey) {
+        DroidMessageEncryptor decryptor = (message, key) -> {
+            StringBuilder decryptedMessage = new StringBuilder();
+            for (char ch : message.toCharArray()) {
+                if (Character.isLetter(ch)) {
+                    char base = Character.isLowerCase(ch) ? 'a' : 'A';
+                    decryptedMessage.append((char) ((ch - base - key) % 26 + base));
+                } else {
+                    decryptedMessage.append(ch);
+                }
+            }
+            return decryptedMessage.toString();
+        };
+        return decryptor.encryptDecrypt(encriptedMessage, decryptionKey);
+    }
+
+    public void sendMessage(Droid droid, String message, int key) {
+        String encryptedMessage = this.encryptMessage(message, key);
+        System.out.println("Droid " + this.name +
+                " send encrypted message to droid " + droid.getName() +
+                ": " + encryptedMessage);
+        droid.receiveMessage(encryptedMessage, key);
+    }
+
+    public void receiveMessage(String message, int key) {
+        String decryptedMessage = this.decryptMessage(message, key);
+        System.out.println("Droid " + this.name + " recieve message and decrypt it: " + decryptedMessage);
+    }
+}
