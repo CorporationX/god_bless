2eac9ef9744aff35b0fb364a2d5489806738a939
Anton Graf <ant0ngraf@yandex.ru>
2024-12-08 17:07:17 +0600

    Разворачивает импорт

--- src/main/java/school/faang/bjs_44565/HogwartsSpells.java
+++ src/main/java/school/faang/bjs_44565/HogwartsSpells.java
@@ -1,10 +1,12 @@
 package school.faang.bjs_44565;
 
-import lombok.AccessLevel;
-import lombok.experimental.FieldDefaults;
-import lombok.extern.java.Log;
+import lombok.extern.slf4j.Slf4j;
 
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 @Log
 @FieldDefaults(level = AccessLevel.PRIVATE)
