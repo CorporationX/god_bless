38c59fce31e2266586a6476a2323a8cbbb47cbeb
Lesha Kozyrev <alekseykoz@gmail.com>
2024-12-08 20:25:56 +0300

    BJS2-46677 was completed.

---
+++ src/main/java/school/faang/task_46677/Notification.java
@@ -0,0 +1,11 @@
+package school.faang.task_46677;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@AllArgsConstructor
+@Getter
+public class Notification {
+    String type;
+    String message;
+}
