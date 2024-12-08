c29cee910fd3eeb274231f1709061cbf10ecdaa8
mixa <mikle.kuznecov@yandex.ru>
2024-12-08 16:40:51 +0300

    refactoring

--- src/main/java/school/faang/sprint_1/task_bjs43999/Main.java
+++ src/main/java/school/faang/sprint_1/task_bjs43999/Main.java
@@ -8,15 +8,17 @@ import java.util.Random;
 public class Main {
     public static void main(String[] args) {
         List<User> userList = new ArrayList<>();
+        Random randomAge = new Random();
+
         for (int i = 0; i < 10; i++) {
             userList.add(
-                new User("Mixa" + new Random().nextInt(100), new Random().nextInt(2),
-                    "Google" + new Random().nextInt(100),
-                    "City" + new Random().nextInt(100)));
+                new User("Mixa", randomAge.nextInt(20, 24),
+                    "Google",
+                    "City"));
         }
 
         Map<Integer, List<User>> mapMain = User.groupUsers(userList);
-        for (var entry : mapMain.entrySet()) {
+        for (Map.Entry<Integer, List<User>> entry : mapMain.entrySet()) {
             System.out.println("\n Key= " + entry.getKey() + "/ values = " + entry.getValue());
         }
     }
