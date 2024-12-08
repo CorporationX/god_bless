b1473ddf8ceed1e758bb3caa8482582baf9698c8
AesaQ <ilyaovchinnikov2095@gmail.com>
2024-12-08 14:31:21 +0500

    fix checkstyle

--- src/main/java/school/faang/double_cache/Main.java
+++ src/main/java/school/faang/double_cache/Main.java
@@ -7,19 +7,12 @@ public class Main {
     public static void main(String[] args) {
         StudentDatabase studentDatabase = new StudentDatabase();
 
+        studentDatabase.printAllStudentsWithGrades();
+
+        Student egor = new Student(1, "Egor");
         Subject math = new Subject(1, "Math");
         Subject geography = new Subject(2, "Geography");
         Subject russian = new Subject(3, "Russian Language");
-        Subject sport = new Subject(5, "Sport");
-        Subject physic = new Subject(6, "Physic");
-        Subject literature = new Subject(7, "Literature");
-
-        Student egor = new Student(1, "Egor");
-        Student ruslan = new Student(2, "Ruslan");
-        Student ilya = new Student(3, "Ilya");
-        Student vlad = new Student(4, "Vlad");
-
-        studentDatabase.printAllStudentsWithGrades();
 
         HashMap<Subject, Integer> egorSubjectsAndGrades = new HashMap<>();
         egorSubjectsAndGrades.put(math, 5);
@@ -27,17 +20,21 @@ public class Main {
         egorSubjectsAndGrades.put(russian, 3);
         studentDatabase.addStudent(egor, egorSubjectsAndGrades);
 
+        Student ruslan = new Student(2, "Ruslan");
         HashMap<Subject, Integer> ruslanSubjectsAndGrades = new HashMap<>();
         ruslanSubjectsAndGrades.put(math, 3);
         ruslanSubjectsAndGrades.put(russian, 4);
+        Subject sport = new Subject(5, "Sport");
         ruslanSubjectsAndGrades.put(sport, 5);
         studentDatabase.addStudent(ruslan, ruslanSubjectsAndGrades);
 
+        Student ilya = new Student(3, "Ilya");
         HashMap<Subject, Integer> ilyaSubjectsAndGrades = new HashMap<>();
         ilyaSubjectsAndGrades.put(math, 4);
         ilyaSubjectsAndGrades.put(russian, 5);
         ilyaSubjectsAndGrades.put(geography, 5);
         ilyaSubjectsAndGrades.put(sport, 5);
+        Subject physic = new Subject(6, "Physic");
         ilyaSubjectsAndGrades.put(physic, 3);
         studentDatabase.addStudent(ilya, ilyaSubjectsAndGrades);
 
@@ -56,11 +53,13 @@ public class Main {
         ArrayList<Student> literatureStudents = new ArrayList<>();
         literatureStudents.add(ilya);
         literatureStudents.add(egor);
+        Subject literature = new Subject(7, "Literature");
         studentDatabase.addSubject(literature, literatureStudents);
 
         studentDatabase.printAllStudentsWithGrades();
 
         System.out.println();
+        Student vlad = new Student(4, "Vlad");
         studentDatabase.addStudentToSubject(vlad, sport, 5);
 
         studentDatabase.printAllStudentsWithGrades();
