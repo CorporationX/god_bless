126417b2ad1dea19be4cd675383930e785253453
az3l1t <eternity_cr9p@mail.ru>
2024-12-08 23:05:21 +0300

    BJS2-45562 Повернём матрицу

--- src/main/java/school/faang/task_45562/Main.java
+++ src/main/java/school/faang/task_45562/Main.java
@@ -20,7 +20,7 @@ public class Main {
         for (int i = 0; i < matrix.length; i++) {
             for (int j = 0; j < matrix.length; j++) {
                 Coordinates coordinates = matrixTransformer.changeIndexes(i, j);
-                result[coordinates.getX()][coordinates.getY()] = matrix[i][j];
+                result[coordinates.getCoordinateX()][coordinates.getCoordinateY()] = matrix[i][j];
             }
         }
 
@@ -31,11 +31,11 @@ public class Main {
         MatrixTransformer matrixTransformer;
 
         switch (flipDirection) {
-            case VERTICAL -> matrixTransformer =
+          case VERTICAL -> matrixTransformer =
                     (i, j) -> new Coordinates(i, matrix.length - j - 1);
-            case HORIZONTAL -> matrixTransformer =
+          case HORIZONTAL -> matrixTransformer =
                     (i, j) -> new Coordinates(matrix.length - i - 1, j);
-            default -> throw new IllegalArgumentException("There is no side!");
+          default -> throw new IllegalArgumentException("There is no side!");
         }
 
         return transformMatrix(matrix, matrixTransformer);
