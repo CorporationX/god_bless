e58130447b4828611b0bb1f3e03992e6a24cf89d
Maria DB <mbashkaeva@yandex.ru>
2024-12-08 13:21:58 +0300

    BJS2-44016 - the task "Grouping users by age"

--- src/main/java/school/faang/Main.java
+++ src/main/java/school/faang/Main.java
@@ -1,4 +1,29 @@
 package school.faang;
 
+import school.faang.task_44016.User;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+import static school.faang.task_44016.User.groupUsers;
+
 public class Main {
+    public static void main(String[] args) {
+        List<User> users = new ArrayList<>();
+        User alisa = new User("Alisa", 12, " - ", "USA");
+        User maria = new User("Maria", 15, " - ", "USA");
+        User alex = new User("Alex", 12, " - ", "USA");
+        User tom = new User("Tom", 15, " - ", "USA");
+        User dima = new User("Dima", 15, " - ", "USA");
+
+        users.add(alisa);
+        users.add(maria);
+        users.add(alex);
+        users.add(tom);
+        users.add(dima);
+
+        Map<Integer, List<User>> sameAge = groupUsers(users);
+        System.out.println(sameAge);
+    }
 }
---
+++ src/main/java/school/faang/task_44016/User.java
@@ -0,0 +1,83 @@
+package school.faang.task_44016;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Objects;
+
+@Getter
+@Setter
+public class User {
+    private String name;
+    private int age;
+    private String workplace;
+    private String address;
+
+    public User(String name, int age, String workplace, String address) {
+        this.name = name;
+        this.age = age;
+        this.workplace = workplace;
+        this.address = address;
+    }
+
+    public static Map<Integer, List<User>> groupUsers(List<User> users){
+        Map<Integer, List<User>> sameAge = new HashMap<>();
+        for (int cycleForMap = 0; cycleForMap<users.size(); cycleForMap++){
+            List<User> userSameAge = new ArrayList<>();
+
+            for(int cycleForList = 0; cycleForList<users.size(); cycleForList++){
+                if(users.get(cycleForList).age == users.get(cycleForMap).age){
+                    userSameAge.add(users.get(cycleForList));
+                }
+            }
+            sameAge.put(users.get(cycleForMap).age, userSameAge);
+        }
+        return sameAge;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        User user = (User) o;
+        return age == user.age && Objects.equals(name, user.name) && Objects.equals(workplace, user.workplace) && Objects.equals(address, user.address);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, age, workplace, address);
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "name='" + name + '\'' +
+                ", AGE=" + age +
+                ", workplace='" + workplace + '\'' +
+                ", address='" + address + '\'' +
+                '}';
+    }
+}
+
+// На всякий случай сохраняю тут main. Не разобралась, где миенно его надо было реализовать
+//        public static void main(String[] args) {
+//        List<User> users = new ArrayList<>();
+//        User alisa = new User("Alisa", 12, " - ", "USA");
+//        User maria = new User("Maria", 15, " - ", "USA");
+//        User alex = new User("Alex", 12, " - ", "USA");
+//        User tom = new User("Tom", 15, " - ", "USA");
+//        User dima = new User("Dima", 15, " - ", "USA");
+//
+//        users.add(alisa);
+//        users.add(maria);
+//        users.add(alex);
+//        users.add(tom);
+//        users.add(dima);
+//
+//        Map<Integer, List<User>> sameAge = groupUsers(users);
+//        System.out.println(sameAge);
+//    }
