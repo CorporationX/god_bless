759f75193532e7e8e47bce7007a8180c0546620e
Anatoly Kushnarev <makartih@Anatolys-MacBook-Pro.local>
2024-12-08 09:05:17 +0200

    the task BJS2-45447 - our lovely Hogvards - is ready

--- src/main/java/school/faang/task_45447/Main.java
+++ src/main/java/school/faang/task_45447/Main.java
@@ -1,7 +1,15 @@
 package school.faang.task_45447;
 
 public class Main {
-    public static void main(String [] args){
-        System.out.println("");
+    public static void main(String[] args) {
+        SpellCaster spellCaster = new SpellCaster();
+
+        SpellAction spellActionOne = (String spell) -> Spell.SPELL.get(spell) + "  IS FULFILLED BY " + spell;
+
+        for (int i = 0; i < 5; i++) {
+            System.out.println(spellCaster.cast(Spell.getRandomSpell().get("eventType"), spellActionOne));
+        }
     }
 }
+
+
