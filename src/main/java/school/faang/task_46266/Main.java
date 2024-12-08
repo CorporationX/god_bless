a103b7144a6f140314df4bed4ee3dbe40cb9f993
triplek <weribel@mail.ru>
2024-12-08 23:05:28 +0300

    Meta-вселенная

--- src/main/java/school/faang/task_46266/Main.java
+++ src/main/java/school/faang/task_46266/Main.java
@@ -4,7 +4,6 @@ public class Main {
     public static void main(String[] args) {
         NotificationManager notificationManager = new NotificationManager();
 
-// Регистрация обработчиков оповещений
         notificationManager.registerHandler("email",
                 (notification) -> System.out.println("Отправка по электронной почте: " + notification.getMessage())
         );
@@ -17,7 +16,6 @@ public class Main {
                 (notification) -> System.out.println("Отправка push-уведомления: " + notification.getMessage())
         );
 
-// Отправка оповещений
         Notification emailNotification = new Notification("email", "Ваша учетная запись успешно активирована");
         Notification smsNotification = new Notification("sms", "Вы успешно изменили свой пароль");
         Notification pushNotification = new Notification("push", "Новый пост от пользователя: JohnDoe");
