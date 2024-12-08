bc0a2b3e73aebccdb05768972b3634dee0d04366
Alexey Gorokhov <almigor@mail.ru>
2024-12-08 17:40:10 +0300

    Секреты дроидов

--- src/main/java/school/faang/sprint2/task46400/Main.java
+++ src/main/java/school/faang/sprint2/task46400/Main.java
@@ -8,8 +8,6 @@ public class Main {
 
         Droid r2d2 = new Droid("R2D2");
         Droid c3po = new Droid("C3PO");
-        Droid bb8 = new Droid("BB-8");
-
         String message1 = "Join the Rebellion!";
         int encryptionKey1 = 3;
         r2d2.sendMessage(c3po, message1, encryptionKey1);
@@ -20,6 +18,7 @@ public class Main {
         c3po.sendMessage(r2d2, message2, encryptionKey2);
         System.out.println("------");
 
+        Droid bb8 = new Droid("BB-8");
         int encryptionKey3 = 1;
         bb8.sendMessage(c3po, "The mission is complete.", encryptionKey3);
         System.out.println("------");
