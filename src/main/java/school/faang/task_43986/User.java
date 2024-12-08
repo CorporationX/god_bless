21e62b0fba9e445efea0064379b5ac9ace04a504
ANaydyonov <alexander.naydenov@23software.net>
2024-12-08 22:48:50 +0100

    added User class. added faker util.

---
+++ src/main/java/school/faang/task_43986/User.java
@@ -0,0 +1,32 @@
+package school.faang.task_43986;
+
+import lombok.Getter;
+
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+@Getter
+public class User {
+    private String name;
+    private int age;
+    private String workplace;
+    private String address;
+
+    public User(String name, int age, String workplace, String address) {
+        this.name = name;
+        this. age = age;
+        this.workplace = workplace;
+        this.address = address;
+    }
+
+    public static Map<Integer, List<User>> groupUsers(List<User> users) {
+        return users.stream().collect(Collectors.groupingBy(User::getAge));
+    }
+
+    @Override
+    public String toString() {
+        return "User {name='" + name + "', age=" + age + ", workplace='"
+                + workplace + "', address='" + address + "'}";
+    }
+}
