c697037deb7a0b677c4362f5a6f18a64365122ed
Daria <dashok.sok@gmail.com>
2024-12-09 18:28:58 +0300

    Добавлен класс EmailProcessor, который применяет Predicate для фильтрации писем, Consumer для их обработки и Function для преобразования

---
+++ src/main/java/school/faang/gmailfilters/Email.java
@@ -0,0 +1,14 @@
+package school.faang.gmailfilters;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+public class Email {
+    private String subject;
+    private String body;
+    private boolean isImportant;
+}
