635e4367861d4db88c602c0559fe61a5c6591992
Vadim Ovcharenko <vadimovcharenko12@gmail.com>
2024-12-09 20:55:03 +0300

    Создал основыне классы для выполнения задачи, без реализации методов

---
+++ src/main/java/school/faang/task_45496/Character.java
@@ -0,0 +1,16 @@
+package school.faang.task_45496;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+import java.util.List;
+
+@AllArgsConstructor
+@Getter
+@Setter
+@ToString
+public class Character {
+    private List<Item> inventory;
+}
---
+++ src/main/java/school/faang/task_45496/Item.java
@@ -0,0 +1,11 @@
+package school.faang.task_45496;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@AllArgsConstructor
+@Getter
+public class Item {
+    private String name;
+    private int value;
+}
