ccc41639ac5dacbd2b02501d4abc89c0c9addbf7
KsushaTarasova <ksutaras543@mail.ru>
2024-12-09 10:57:13 +0300

    Use AtomicInteger for generate id

--- src/main/java/school/faang/task_43518/Product.java
+++ src/main/java/school/faang/task_43518/Product.java
@@ -3,10 +3,19 @@ package school.faang.task_43518;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 
+import java.util.concurrent.atomic.AtomicInteger;
+
 @Data
 @AllArgsConstructor
 public class Product {
+    private static final AtomicInteger idGenerator = new AtomicInteger(0);
     private int id;
     private String category;
     private String name;
+
+    public Product(String category, String name) {
+        this.id = idGenerator.incrementAndGet();
+        this.category = category;
+        this.name = name;
+    }
 }
