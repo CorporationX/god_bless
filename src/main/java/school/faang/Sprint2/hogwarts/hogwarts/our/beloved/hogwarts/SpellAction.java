7d89e3a0ac6b83e2a2ceefc2a02e629177c80e11
muslim <xammuslim06@gmail.com>
2024-12-10 00:55:52 +0300

    Hogwarts, Hogwarts, our beloved Hogwarts

---
+++ src/main/java/school/faang/Sprint2/hogwarts/hogwarts/our/beloved/hogwarts/SpellAction.java
@@ -0,0 +1,8 @@
+package school.faang.Sprint2.hogwarts.hogwarts.our.beloved.hogwarts;
+
+import java.util.SplittableRandom;
+
+@FunctionalInterface
+public interface SpellAction {
+   String spell(String spellName);
+}
---
+++ src/main/java/school/faang/Sprint2/hogwarts/hogwarts/our/beloved/hogwarts/SpellCaster.java
@@ -0,0 +1,9 @@
+package school.faang.Sprint2.hogwarts.hogwarts.our.beloved.hogwarts;
+
+public class SpellCaster {
+        void cast(String spellName, SpellAction spellAction){
+            String result = spellAction.spell(spellName);
+            System.out.println(result);
+        }
+    }
+
