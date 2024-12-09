bc8ffa43209fffea8c04f41cce72311a402124ce
TFdevQA <46944859+TFdevQA@users.noreply.github.com>
2024-12-08 18:27:10 -0800

    task_45265: receiveDamage method added

--- src/main/java/school/faang/sprint1/bjs_43839/Archer.java
+++ src/main/java/school/faang/sprint1/bjs_43839/Archer.java
@@ -1,9 +1,9 @@
 package school.faang.sprint1.bjs_43839;
 
 public class Archer extends Character {
-    public static final int STRENGTH = 3;
-    public static final int AGILITY = 10;
-    public static final int INTELLIGENCE = 5;
+    private static final int STRENGTH = 3;
+    private static final int AGILITY = 7;
+    private static final int INTELLIGENCE = 5;
 
     public Archer(String name) {
         super(name, STRENGTH, AGILITY, INTELLIGENCE);
@@ -11,6 +11,6 @@ public class Archer extends Character {
 
     @Override
     public void attack(Character character) {
-        character.health -= AGILITY;
+        character.receiveDamage(AGILITY);
     }
 }
