8911ba282b8819fccb5551e5c7d2f915fc49abde
Мария Борцова <bortsovamm@intech.rshb.ru>
2024-12-09 11:02:41 +0300

    refactor after review

--- src/main/java/school/faang/task_44825/WeatherCacheTemplate.java
+++ src/main/java/school/faang/task_44825/WeatherCacheTemplate.java
@@ -10,6 +10,8 @@ public abstract class WeatherCacheTemplate {
     private final Map<String, WeatherData> data;
     private final WeatherService weatherService;
 
+    abstract boolean isCacheExpired(WeatherData data, long maxCacheAgeMillis);
+
     public WeatherData getWeatherData(String city, long maxCacheAgeMillis) {
         if (city == null || city.isBlank()) {
             throw new IllegalArgumentException("City field is required");
@@ -20,17 +22,15 @@ public abstract class WeatherCacheTemplate {
         return data.get(city);
     }
 
+    public void clearExpiredCache(long maxCacheAgeMillis) {
+        data.entrySet().removeIf(entry -> isCacheExpired(entry.getValue(), maxCacheAgeMillis));
+    }
+
     private void updateCache(String city) {
         WeatherData actualWeatherData = weatherService.fetchWeatherData(city);
         if (actualWeatherData == null) {
             throw new NoSuchElementException("Weather data for current city is absent");
         }
         data.put(city, actualWeatherData);
-    }
-
-    abstract boolean isCacheExpired(WeatherData data, long maxCacheAgeMillis);
-
-    public void clearExpiredCache(long maxCacheAgeMillis) {
-        data.entrySet().removeIf(entry -> isCacheExpired(entry.getValue(), maxCacheAgeMillis));
     }
 }
--- src/main/java/school/faang/task_44825/WeatherService.java
+++ src/main/java/school/faang/task_44825/WeatherService.java
@@ -3,12 +3,15 @@ package school.faang.task_44825;
 import java.util.Random;
 
 public class WeatherService implements WeatherProvider {
+    private final Random random = new Random();
+
     @Override
     public WeatherData fetchWeatherData(String city) {
         if (city == null) {
             throw new IllegalArgumentException("City doesn't exist");
         }
-        Random random = new Random();
-        return new WeatherData(city, random.nextDouble(), random.nextDouble(), System.currentTimeMillis());
+        double temperature = random.nextDouble(((50 + 50 + 1) - 50)) * (random.nextBoolean() ? 1 : -1);
+        double humidity = random.nextDouble(100 + 1);
+        return new WeatherData(city, temperature, humidity, System.currentTimeMillis());
     }
 }
