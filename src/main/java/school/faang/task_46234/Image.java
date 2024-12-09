f4f745d58d110fa988af938479ea5fc5c96bda90
Мария Борцова <bortsovamm@intech.rshb.ru>
2024-12-09 12:20:31 +0300

    refactor after review

--- src/main/java/school/faang/task_46234/Image.java
+++ src/main/java/school/faang/task_46234/Image.java
@@ -1,11 +1,4 @@
 package school.faang.task_46234;
 
-import lombok.Getter;
-import lombok.RequiredArgsConstructor;
-
-@RequiredArgsConstructor
-@Getter
-public class Image {
-    private final String name;
-    private final String description;
+public record Image(String name, String description) {
 }
--- src/main/java/school/faang/task_46234/Main.java
+++ src/main/java/school/faang/task_46234/Main.java
@@ -9,21 +9,21 @@ public class Main {
         FilterProcessor filterProcessor = new FilterProcessor();
 
         // Фильтры
-        Function<Image, Image> grayscaleFilter = (image) -> new Image(image.getName(), image.getDescription()
+        Function<Image, Image> grayscaleFilter = (image) -> new Image(image.name(), image.description()
                 + " | Фильтр: черно-белый");
-        Function<Image, Image> sepiaFilter = (image) -> new Image(image.getName(), image.getDescription()
+        Function<Image, Image> sepiaFilter = (image) -> new Image(image.name(), image.description()
                 + " | Фильтр: сепия");
 
         // Применение фильтров
         Image grayscaleImage = filterProcessor.applyFilter(originalImage, grayscaleFilter);
-        System.out.println(grayscaleImage.getDescription());
+        System.out.println(grayscaleImage.description());
 
         Image sepiaImage = filterProcessor.applyFilter(grayscaleImage, sepiaFilter);
-        System.out.println(sepiaImage.getDescription());
+        System.out.println(sepiaImage.description());
 
         // Комбинирование фильтров
         Function<Image, Image> combinedFilter = filterProcessor.combineFilters(grayscaleFilter, sepiaFilter);
         Image combinedImage = filterProcessor.applyFilter(originalImage, combinedFilter);
-        System.out.println(combinedImage.getDescription());
+        System.out.println(combinedImage.description());
     }
 }
