021b588a008ac7152fd4dc41f1f048dc9d5570d0
ZhannaD <zhanna.davidov@gmail.com>
2024-12-09 13:43:46 +0200

    User registration fix checkstyle

--- src/main/java/school/faang/Main.java
+++ src/main/java/school/faang/Main.java
@@ -2,11 +2,11 @@ package school.faang;
 
 public class Main {
     public static void main(String[] args) {
-        User validUser=new User("Zhanna",48,"Google","London");
+        User validUser = new User("Zhanna", 48, "Google", "London");
         System.out.println(validUser);
-        User notValidName=new User(" ",48,"Google","London");
-        User notValidAge=new User("Zhanna",8,"Google","London");
-        User notValidJob=new User("Zhanna",8,"OneZero","London");
-        User notValidAddress=new User("Zhanna",8,"Google","France");
+        User notValidName = new User(" ", 48, "Google", "London");
+        User notValidAge = new User("Zhanna", 8, "Google", "London");
+        User notValidJob = new User("Zhanna", 8, "OneZero", "London");
+        User notValidAddress = new User("Zhanna", 8, "Google", "France");
     }
 }
