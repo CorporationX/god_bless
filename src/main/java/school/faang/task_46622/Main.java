849d89da0431c1b7c8e9f39b19f0f971391aaa3c
narol01 <kmingogod@gmail.com>
2024-12-09 01:37:31 +0100

    BJS2-46622

---
+++ src/main/java/school/faang/task_46622/Main.java
@@ -0,0 +1,30 @@
+package school.faang.task_46622;
+
+import school.faang.task_46622.entity.Notification;
+import school.faang.task_46622.entity.TypeNotification;
+
+public class Main {
+    public static void main(String[] args) {
+        NotificationManager notificationManager = new NotificationManager();
+
+        notificationManager.registerHandler(TypeNotification.EMAIL,
+                (notification) -> System.out.println("Отправка по электронной почте: " + notification.getMessage())
+        );
+
+        notificationManager.registerHandler(TypeNotification.SMS,
+                (notification) -> System.out.println("Отправка SMS: " + notification.getMessage())
+        );
+
+        notificationManager.registerHandler(TypeNotification.PUSH,
+                (notification) -> System.out.println("Отправка push-уведомления: " + notification.getMessage())
+        );
+
+        Notification emailNotification = new Notification(TypeNotification.EMAIL, "Ваша учетная запись успешно активирована");
+        Notification smsNotification = new Notification(TypeNotification.SMS, "Вы успешно изменили свой пароль");
+        Notification pushNotification = new Notification(TypeNotification.PUSH, "Новый пост от пользователя: JohnDoe");
+
+        notificationManager.sendNotification(emailNotification);
+        notificationManager.sendNotification(smsNotification);
+        notificationManager.sendNotification(pushNotification);
+    }
+}
---
+++ src/main/java/school/faang/task_46622/entity/TypeNotification.java
@@ -0,0 +1,7 @@
+package school.faang.task_46622.entity;
+
+public enum TypeNotification {
+    EMAIL,
+    SMS,
+    PUSH
+}
