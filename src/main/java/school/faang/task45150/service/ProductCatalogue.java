0385b1d404b690d693aafebad8cd8a768bbcbf08
Alexey Gorokhov <almigor@mail.ru>
2024-12-10 00:13:05 +0300

    рефакторинг по код-ревью

--- src/main/java/school/faang/task45150/service/ProductCatalogue.java
+++ src/main/java/school/faang/task45150/service/ProductCatalogue.java
@@ -6,11 +6,11 @@ import java.util.*;
 
 public class ProductCatalogue {
     private static int productId = 1;
-    private final Map<String, List<Product>> groupedByCategoryProducts = new HashMap<>();
+    private final Map<String, List<Product>> productsByCategory = new HashMap<>();
 
     public void addItem(String category, String name) {
         Product product = new Product(productId++, category, name);
-        groupedByCategoryProducts.computeIfAbsent(category, k -> new ArrayList<>()).add(product);
+        productsByCategory.computeIfAbsent(category, k -> new ArrayList<>()).add(product);
     }
 
     public void removeItem(String category, String name) {
@@ -21,19 +21,19 @@ public class ProductCatalogue {
             throw new IllegalArgumentException("Name is empty");
         }
 
-        List<Product> productList = groupedByCategoryProducts.get(category);
+        List<Product> productList = productsByCategory.get(category);
         if (productList != null) {
             productList.removeIf(product -> product.getName().equals(name));
         }
     }
 
     public List<Product> findItemsByCategory(String category) {
-        return groupedByCategoryProducts.get(category);
+        return productsByCategory.get(category);
     }
 
     public void printAllItems() {
         System.out.println("ALL PRODUCT LIST: ");
-        for (Map.Entry<String, List<Product>> entry : groupedByCategoryProducts.entrySet()) {
+        for (Map.Entry<String, List<Product>> entry : productsByCategory.entrySet()) {
             System.out.println(">>> " + entry.getKey());
             for (Product product : entry.getValue()) {
                 System.out.println(product);
