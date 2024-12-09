43a9cedb07f56878ae04a0f6315040b491aabfa5
therealadik <vladerm2000@yandex.ru>
2024-12-09 15:47:43 +0500

    fix

--- src/main/java/school/faang/task_45689/Main.java
+++ src/main/java/school/faang/task_45689/Main.java
@@ -1,5 +1,8 @@
 package school.faang.task_45689;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
         NotificationManager notificationManager = new NotificationManager();
@@ -16,13 +19,11 @@ public class Main {
                 (notification) -> System.out.println("Отправка push-уведомления: " + notification.message())
         );
 
-        // Отправка оповещений
-        Notification emailNotification = new Notification(NotifyType.EMAIL, "Ваша учетная запись успешно активирована");
-        Notification smsNotification = new Notification(NotifyType.SMS, "Вы успешно изменили свой пароль");
-        Notification pushNotification = new Notification(NotifyType.PUSH, "Новый пост от пользователя: JohnDoe");
+        List<Notification> notifications = new ArrayList<>();
+        notifications.add(new Notification(NotifyType.EMAIL, "Ваша учетная запись успешно активирована"));
+        notifications.add(new Notification(NotifyType.SMS, "Вы успешно изменили свой пароль"));
+        notifications.add(new Notification(NotifyType.PUSH, "Новый пост от пользователя: JohnDoe"));
 
-        notificationManager.sendNotification(emailNotification);
-        notificationManager.sendNotification(smsNotification);
-        notificationManager.sendNotification(pushNotification);
+        notifications.forEach(notificationManager::sendNotification);
     }
 }
