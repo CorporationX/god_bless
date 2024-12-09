37ae1e732350620e8043a25a2e1e34795508b51c
viktor <vikt.ovod@outlook.com>
2024-12-10 00:37:14 +0300

    выполнил задачу, вроде всё работает

---
+++ src/main/java/school/faang/User.java
@@ -0,0 +1,27 @@
+package school.faang;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+@Getter
+@AllArgsConstructor
+public class User {
+    private String name;
+    private int age;
+    private String workplace;
+    private String address;
+
+
+    @Override
+    public String toString() {
+        return name + ", работает в '" + workplace + "', живет в городе " + address + '.';
+    }
+
+    public static Map<Integer, List<User>> groupUsers(List<User> users) {
+        return users.stream().collect(Collectors.groupingBy(User::getAge));
+    }
+}
