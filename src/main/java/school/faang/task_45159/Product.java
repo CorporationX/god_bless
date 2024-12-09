277e6e1888084a62a562b70ed24b275093f13163
maks <fhbfds@fb>
2024-12-09 09:24:02 +0300

    - Добавлена аннотация @Getter

--- src/main/java/school/faang/task_45159/Product.java
+++ src/main/java/school/faang/task_45159/Product.java
@@ -1,14 +1,15 @@
 package school.faang.task_45159;
 
 import lombok.EqualsAndHashCode;
+import lombok.Getter;
 
+
+@Getter
 @EqualsAndHashCode
 public class Product {
 
     private int id;
-
     private String name;
-
     private String category;
 
     public Product(int id, String name, String category) {
@@ -17,22 +18,8 @@ public class Product {
         this.category = category;
     }
 
-    public int getId() {
-        return id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public String getCategory() {
-        return category;
-    }
-
     @Override
     public String toString() {
         return String.format("Product{id=%d, name='%s', category='%s'}", id, name, category);
     }
-
-
 }
