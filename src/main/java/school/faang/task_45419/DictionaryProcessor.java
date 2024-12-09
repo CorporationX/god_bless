13573e631c809c1e06c3f7e24efb7bd49772e405
Ilya Vysokolyan <rd2d@list.ru>
2024-12-09 18:59:54 +0300

    BJS2-45419: Поправил видимость метода `processWord`

--- src/main/java/school/faang/task_45419/DictionaryProcessor.java
+++ src/main/java/school/faang/task_45419/DictionaryProcessor.java
@@ -7,7 +7,7 @@ import java.util.function.BiConsumer;
 public class DictionaryProcessor {
     private Map<String, String> dictionary = new HashMap<>();
 
-    public void processWord(String word, String translation, BiConsumer<String, String> biConsumer) {
+    private void processWord(String word, String translation, BiConsumer<String, String> biConsumer) {
         biConsumer.accept(word, translation);
     }
 
