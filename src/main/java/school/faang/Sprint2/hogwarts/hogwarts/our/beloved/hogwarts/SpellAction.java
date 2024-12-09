7ceb67e165525c227eadb9ae060456b4ad603a92
muslim <xammuslim06@gmail.com>
2024-12-10 01:01:21 +0300

    Hogwarts, Hogwarts, our beloved Hogwarts, fixed code style

--- src/main/java/school/faang/Sprint2/hogwarts/hogwarts/our/beloved/hogwarts/SpellAction.java
+++ src/main/java/school/faang/Sprint2/hogwarts/hogwarts/our/beloved/hogwarts/SpellAction.java
@@ -1,8 +1,6 @@
 package school.faang.Sprint2.hogwarts.hogwarts.our.beloved.hogwarts;
 
-import java.util.SplittableRandom;
-
 @FunctionalInterface
 public interface SpellAction {
-   String spell(String spellName);
+    String spell(String spellName);
 }
