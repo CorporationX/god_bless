639b1eee7c9795b6c1b143381004e0816a0b1be4
etibarliparvin <73303441+etibarliparvin@users.noreply.github.com>
2024-12-09 22:37:01 +0400

    bjs2-45818 (#14894)
    * Lord of the Rings RPG is finished

---
+++ src/main/java/school/faang/bjs245818/Item.java
@@ -0,0 +1,15 @@
+package school.faang.bjs245818;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+public class Item {
+
+    private String name;
+    private Integer value;
+
+}
