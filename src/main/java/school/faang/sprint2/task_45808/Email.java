f76fca24383ec901bb8a849de01973528aad84ca
DmitryQueen <276246@student.pwr.edu.pl>
2024-12-09 12:56:34 +0100

    solved task_45808 (Gmail Rich Filters)

---
+++ src/main/java/school/faang/sprint2/task_45808/Email.java
@@ -0,0 +1,20 @@
+package school.faang.sprint2.task_45808;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NonNull;
+import lombok.Setter;
+import lombok.ToString;
+
+@AllArgsConstructor
+@Getter
+@Setter
+@ToString
+public class Email {
+    @NonNull
+    private final String subject;
+    @NonNull
+    private String body;
+    @NonNull
+    private final boolean isImportant;
+}
