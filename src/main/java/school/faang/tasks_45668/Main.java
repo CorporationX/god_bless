3f06386acf71ed94db0f3870fc8030aefd3859b8
alterality <rordufe@gmail.com>
2024-12-09 23:59:54 +0600

    Калькулятор

--- src/main/java/school/faang/tasks_45668/Main.java
+++ src/main/java/school/faang/tasks_45668/Main.java
@@ -6,7 +6,7 @@ import java.util.List;
 public class Main {
     public static void main(String[] args) {
         List<Integer> nums = Arrays.asList(5, 3);
-//        nums.set(0, null);
+        nums.set(0, null);
         int sum = calculate(nums, (a, b) -> a + b);
         System.out.println(sum);
         int product = calculate(nums, (a, b) -> a * b);
@@ -14,7 +14,7 @@ public class Main {
     }
     public static int calculate(List<Integer> nums, Calculator calculator) {
         return nums.stream()
-//                .filter(num -> num != null)
+                .filter(num -> num != null)
                 .reduce((a, b) -> calculator.calc(a, b))
                 .orElseThrow(() -> new IllegalArgumentException("список пуст"));
     }
