446c20c815d5df1c04e8a08a5a3618adbdf87e37
Oxana <nov2303@mail.ru>
2024-12-09 21:12:40 +0500

    BJS2-45482

--- src/main/java/school/faang/task_45482/SpellAction.java
+++ src/main/java/school/faang/task_45482/SpellAction.java
@@ -2,5 +2,5 @@ package school.faang.task_45482;
 
 @FunctionalInterface
 public interface SpellAction {
-   String castSpell(String spellName);
+    String castSpell(String spellName);
 }
--- src/main/java/school/faang/task_45482/SpellCaster.java
+++ src/main/java/school/faang/task_45482/SpellCaster.java
@@ -1,7 +1,7 @@
 package school.faang.task_45482;
 
 public class SpellCaster {
-    public void cast(String spell, SpellAction spellAction){
+    public void cast(String spell, SpellAction spellAction) {
         String result = spellAction.castSpell(spell);
         System.out.println(result);
     }
