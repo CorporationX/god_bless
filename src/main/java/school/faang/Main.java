5779453443e334a742a25e1fe41b3d7971d0db0f
Azamat Beisembay <azamat.beisembay@gmail.com>
2024-12-09 09:33:43 +0500

    Создал отдельный класс Main в пакете, исправил по комментам.

--- src/main/java/school/faang/Main.java
+++ src/main/java/school/faang/Main.java
@@ -1,16 +1,7 @@
 package school.faang;
 
 
-import school.faang.userregister.User;
-
 public class Main {
     public static void main(String[] args) {
-
-        // https://faang-school.atlassian.net/browse/BJS2-43760
-
-        User anny = new User("Anny", 25, "Google", "Amsterdam");
-        User kenny = new User("Kenny", 29, "Uber", "London");
-        User louis = new User("Louis", 32, "Amazon", "New York");
-
     }
 }
