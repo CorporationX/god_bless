1e3d902cb8b825718bcbb8eff37ebb9f680765ae
salikhdev <salikhdev@gmail.com>
2024-12-09 22:05:28 +0500

    Meta-вселенная.

---
+++ src/main/java/school/faang/bjs246668/Notification.java
@@ -0,0 +1,11 @@
+package school.faang.bjs246668;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class Notification {
+    private String type;
+    private String message;
+}
