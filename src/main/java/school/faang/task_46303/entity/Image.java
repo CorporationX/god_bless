e962a902383a02cceddd60a39355ae0db9d0a66f
narol01 <kmingogod@gmail.com>
2024-12-09 11:44:49 +0100

    BJS2-46303

--- src/main/java/school/faang/task_46303/entity/Image.java
+++ src/main/java/school/faang/task_46303/entity/Image.java
@@ -1,15 +1,21 @@
 package school.faang.task_46303.entity;
 
-import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
 
 @Getter
 @Setter
-@AllArgsConstructor
 @ToString
 public class Image {
     private String name;
     private String description;
+
+    public Image(String name, String description) {
+        if (name == null || name.isEmpty() || description == null || description.isEmpty()) {
+            throw new IllegalArgumentException("Image name and description cannot be empty");
+        }
+        this.name = name;
+        this.description = description;
+    }
 }
