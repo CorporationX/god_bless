6a3832d1b080abdb67adfcb7f7df1eb49a8c1645
Sergey Chirin <zopks2@gmail.com>
2024-12-10 01:36:20 +0300

    Solved my first task at this bootcamp. Maybe you will not approve it because I used Set for checking contains or not this user group age before

---
+++ src/main/java/school/faang/BJS2_44022/User.java
@@ -0,0 +1,71 @@
+package school.faang.BJS2_44022;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+
+public class User {
+    private String name;
+    private int age;
+    private String workplace;
+    private String address;
+
+    public User(String name, int age, String workplace, String address) {
+        this.name = name;
+        this.age = age;
+        this.workplace = workplace;
+        this.address = address;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getWorkplace() {
+        return workplace;
+    }
+
+    public void setWorkplace(String workplace) {
+        this.workplace = workplace;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public static Map<Integer, ArrayList<User>> groupUsers(ArrayList<User> users) {
+        Map<Integer, ArrayList<User>> userMap = new HashMap<Integer, ArrayList<User>>();
+        Set<Integer> userAgeSet = new HashSet<Integer>();
+        for (User user : users){
+            if (!(userAgeSet.contains(user.age))){
+                ArrayList<User> usersAgeGroup = new ArrayList<User>();
+                usersAgeGroup.add(user);
+                userAgeSet.add(user.age);
+                userMap.put(user.age, usersAgeGroup);
+            } else {
+                ArrayList<User> usersAgeGroup = userMap.get(user.age);
+                usersAgeGroup.add(user);
+            }
+        }
+        return userMap;
+    }
+
+}
--- src/main/java/school/faang/Main.java
+++ src/main/java/school/faang/Main.java
@@ -1,4 +1,29 @@
 package school.faang;
 
+import school.faang.BJS2_44022.User;
+
+import java.util.ArrayList;
+import java.util.Map;
+
 public class Main {
+    public static void main(String[] args) {
+        ArrayList<User> users = new ArrayList<User>();
+        int j = 0;
+        for (int i = 0; i < 10; i++) {
+            User temp_user = new User("Sergey_" + i, j, "FAANG_" + i, "_" + i);
+            users.add(temp_user);
+            if (i % 2 == 0) {
+                j += 1;
+            }
+        }
+
+        Map<Integer, ArrayList<User>> ansMap = User.groupUsers(users);
+
+        for (Map.Entry<Integer, ArrayList<User>> entry : ansMap.entrySet()) {
+            System.out.println("Возраст: " + entry.getKey());
+            for (User userTemp : entry.getValue()) {
+                System.out.println(userTemp.getName());
+            }
+        }
+    }
 }
