e17a0c6315cf491552fd11b8b071f04312183db1
Stepan Evangulov <stevangulov@gmail.com>
2024-12-09 11:02:04 +0200

    Проверочка

---
+++ src/main/java/school/faang/task_46618/Notification.java
@@ -0,0 +1,13 @@
+package school.faang.task_46618;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.ToString;
+
+@Getter
+@ToString
+@AllArgsConstructor
+public class Notification {
+    private final String type;
+    private final String message;
+}
