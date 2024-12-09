0b89911a75b76201c42ddc756c330c65291cb1f2
Artur Suleymanov <suleymanov.usa@gmail.com>
2024-12-09 11:20:38 -0800

    Add logging to HogwartsSpells class and fix variable name in getSpellEventsByType method

---
+++ src/main/java/school/faang/bjs244699/SpellEvent.java
@@ -0,0 +1,54 @@
+package school.faang.bjs244699;
+
+import lombok.Getter;
+
+@Getter
+public class SpellEvent {
+
+    private int id;
+    private String eventType;
+    private String action;
+
+    public SpellEvent(int id, String eventType, String action) {
+        this.id = id;
+        this.eventType = eventType;
+        this.action = action;
+    }
+
+    public SpellEvent(int id, String eventType) {
+        this(id, eventType, "Неопределенное действие");
+    }
+
+    public SpellEvent(int id) {
+        this(id, "Неизвестный тип", "Неопределенное действие");
+    }
+
+    public void setId(int id) {
+        if (id > 0) {
+            this.id = id;
+        } else {
+            throw new IllegalArgumentException("ID должен быть положительным числом.");
+        }
+    }
+
+    public void setEventType(String eventType) {
+        if (eventType != null || eventType.trim().isEmpty()) {
+            this.eventType = eventType;
+        } else {
+            throw new IllegalArgumentException("Тип события не может быть пустым.");
+        }
+    }
+
+    public void setAction(String action) {
+        if (action != null || action.trim().isEmpty()) {
+            this.action = action;
+        } else {
+            throw new IllegalArgumentException("Тип события не может быть пустым.");
+        }
+    }
+
+    @Override
+    public String toString() {
+        return String.format("SpellEvent{id=%d, eventType='%s', action='%s'}", id, eventType, action);
+    }
+}
