25c1e731861614afc0a140168b5fa3362f3a4a44
Shakhzod Kamolov <shakhzod.kamolov99@gmail.com>
2024-12-10 01:17:13 +0500

    feature(BJS2-43761) Registration of user's added with validation

---
+++ src/main/java/school/faang/Constants.java
@@ -0,0 +1,8 @@
+package school.faang;
+
+import java.util.Set;
+
+public class Constants {
+    public static Set<String> VALID_JOBS = Set.of("Google", "Uber", "Amazon");
+    public static Set<String> VALID_ADDRESSES = Set.of("London", "New York", "Amsterdam");
+}
--- src/main/java/school/faang/Main.java
+++ src/main/java/school/faang/Main.java
@@ -1,4 +1,11 @@
 package school.faang;
 
 public class Main {
+    public static void main(String[] args) {
+        User john = new User("john", 25, "Uber", "Amsterdam");
+        System.out.println(john);
+
+        User anna = new User("anna", 23, "Java", "Tashkent");
+        System.out.println(anna);
+    }
 }
