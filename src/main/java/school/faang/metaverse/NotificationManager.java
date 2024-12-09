6aa5ffb3ed210b43eefd252602f4421bc6bce2f1
AesaQ <ilyaovchinnikov2095@gmail.com>
2024-12-09 20:38:08 +0500

    task solution

---
+++ src/main/java/school/faang/metaverse/NotificationManager.java
@@ -0,0 +1,16 @@
+package school.faang.metaverse;
+
+import java.util.HashMap;
+import java.util.function.Consumer;
+
+public class NotificationManager {
+    HashMap<String, Consumer<Notification>> handlers = new HashMap<>();
+
+    public void registerHandler(String notificationType, Consumer<Notification> handler) {
+        handlers.put(notificationType, handler);
+    }
+    public void sendNotification(Notification notification) {
+        Consumer<Notification> handler = handlers.get(notification.type());
+        handler.accept(notification);
+    }
+}
