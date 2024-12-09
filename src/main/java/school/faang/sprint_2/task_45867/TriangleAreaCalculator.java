a4fe10298b23d11cf5cc08999487e5adb1b32236
Stanislav Bugaev <stasikbygaev@gmail.com>
2024-12-09 21:50:37 +0500

    Добавил тесты и отрефакторил код

--- src/main/java/school/faang/sprint_2/task_45867/TriangleAreaCalculator.java
+++ src/main/java/school/faang/sprint_2/task_45867/TriangleAreaCalculator.java
@@ -4,40 +4,32 @@ import java.util.function.Function;
 
 public class TriangleAreaCalculator {
 
-    Function<Double, Function<Double, Double>> add = (x) -> (y) -> x + y;
-    Function<Double, Function<Double, Double>> multiply = (x) -> (y) -> x * y;
-    Function<Double, Function<Double, Double>> subtract = (x) -> (y) -> x - y;
-    Function<Double, Function<Double, Double>> divide = (x) -> (y) -> x / y;
-    Function<Double, Double> squareRoot = (x) -> Math.sqrt(x);
+    private Function<Double, Function<Double, Double>> multiply = (x) -> (y) -> x * y;
+    private Function<Double, Function<Double, Double>> add = (x) -> (y) -> x + y;
+    private Function<Double, Function<Double, Double>> subtract = (x) -> (y) -> x - y;
+    private Function<Double, Function<Double, Double>> divide = (x) -> (y) -> x / y;
+    private Function<Double, Double> squareRoot = (x) -> Math.sqrt(x);
 
     public Double calculateTriangleArea(double a, double b, double c) throws IllegalArgumentException {
+        if (a <= 0 || b <= 0 || c <= 0) {
+            throw new IllegalArgumentException("Треугольник должен иметь положительные стороны");
+        }
         if (add.apply(a).apply(b) <= c || add.apply(a).apply(c) <= b || add.apply(c).apply(b) <= a) {
             throw new IllegalArgumentException("Треугольник с такими сторонами не существует");
         }
+
         double halfPerimeter = divide.apply(add.apply(add.apply(a).apply(b)).apply(c)).apply(2.0);
+
         double subtractWithFirstSide = subtract.apply(halfPerimeter).apply(a);
         double subtractWithSecondSide = subtract.apply(halfPerimeter).apply(b);
         double subtractWithThirdSide = subtract.apply(halfPerimeter).apply(c);
-        double underRoot =
-                multiply.apply(
-                        multiply.apply(
-                                multiply.apply(halfPerimeter).apply(subtractWithFirstSide)
-                                )
-                        .apply(subtractWithSecondSide)
-                ).apply(subtractWithThirdSide);
+
+        double multipleHalfPerimeterAndFirstSubtract = multiply.apply(halfPerimeter).apply(subtractWithFirstSide);
+        double multipleFirstProductAndSecondSubtract =
+                multiply.apply(multipleHalfPerimeterAndFirstSubtract).apply(subtractWithSecondSide);
+        double underRoot = multiply.apply(multipleFirstProductAndSecondSubtract).apply(subtractWithThirdSide);
+
         return squareRoot.apply(underRoot);
-    }
-
-    public static void main(String[] args) {
-        TriangleAreaCalculator calculator = new TriangleAreaCalculator();
-
-        try {
-            double area = calculator.calculateTriangleArea(3, 4, 5);
-            System.out.println("Площадь треугольника: " + area);
-            calculator.calculateTriangleArea(1, 2, 5);
-        } catch (IllegalArgumentException e) {
-            System.out.println(e.getMessage());
-        }
     }
 }
 
