96f3e77b0c5ddb7c96bd397cd115af91a57a366f
Daria <dashok.sok@gmail.com>
2024-12-10 16:56:43 +0300

    Исправлены комментарии после ревью

--- src/main/java/school/faang/lordoftherings/InventoryManager.java
+++ src/main/java/school/faang/lordoftherings/InventoryManager.java
@@ -8,32 +8,44 @@ import java.util.function.Predicate;
 public class InventoryManager {
 
     public void addItem(Character character, Item item, Consumer<Item> action) {
-        if (character == null || action == null) {
-            throw new IllegalArgumentException("Какой-то параметр равен null");
-        } else {
-            character.getInventory().add(item);
-            action.accept(item);
+        if (character == null) {
+            throw new IllegalArgumentException("Character can't be null");
         }
+        if (item == null) {
+            throw new IllegalArgumentException("Item can't be null");
+        }
+        if (action == null) {
+            throw new IllegalArgumentException("Action can't be null");
+        }
+        character.getInventory().add(item);
+        action.accept(item);
     }
 
     public void removeItem(Character character, Predicate<Item> filter) {
-        if (character == null || filter == null) {
-            throw new IllegalArgumentException("Какой-то параметр равен null");
-        } else {
-            character.getInventory().removeIf(filter);
+        if (character == null) {
+            throw new IllegalArgumentException("Character can't be null");
         }
+        if (filter == null) {
+            throw new IllegalArgumentException("Filter can't be null");
+        }
+        character.getInventory().removeIf(filter);
     }
 
     public void updateItem(Character character, Predicate<Item> filter, Function<Item, Item> transformer) {
-        if (character == null || filter == null || transformer == null) {
-            throw new IllegalArgumentException("Какой-то параметр равен null");
-        } else {
-            List<Item> inventory = character.getInventory();
-            for (int i = 0; i < inventory.size(); i++) {
-                Item item = inventory.get(i);
-                if (filter.test(item)) {
-                    inventory.set(i, transformer.apply(item));
-                }
+        if (character == null) {
+            throw new IllegalArgumentException("Character can't be null");
+        }
+        if (filter == null) {
+            throw new IllegalArgumentException("Filter can't be null");
+        }
+        if (transformer == null) {
+            throw new IllegalArgumentException("Transformer can't be null");
+        }
+        List<Item> inventory = character.getInventory();
+        for (int i = 0; i < inventory.size(); i++) {
+            Item item = inventory.get(i);
+            if (filter.test(item)) {
+                inventory.set(i, transformer.apply(item));
             }
         }
     }
