22a9d735d92db9fbb15ba1a02aa24b97a848ec0f
Arseni Zaytsau <zarseniy280@gmail.com>
2024-12-10 18:47:37 +0300

    ztsv-BJS2-44019 - created User class, tested in Main

---
+++ src/main/java/school/faang/BJS2_44019/User.java
@@ -0,0 +1,38 @@
+package school.faang.BJS2_44019;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@ToString
+public class User {
+    private String name;
+    private int age;
+    private String workplace;
+    private String address;
+
+    public static Map<Integer, List<User>> groupUsers(List<User> users) {
+        var ageGroups = new HashMap<Integer, List<User>>();
+        for (User user : users) {
+            int age = user.age;
+            if (ageGroups.containsKey(age)) {
+                ageGroups.get(age).add(user);
+            }
+            else {
+                var initAge = new ArrayList<User>();
+                initAge.add(user);
+                ageGroups.put(age, initAge);
+            }
+        }
+        return ageGroups;
+    }
+}
