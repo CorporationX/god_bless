a1b0d4a31afd361f3a4dcc40dce39c6bb772d7e0
Natalya Vasileva <boryantheone228@yandex.ru>
2024-12-10 21:56:50 +0300

    BJS2-45560 Добавление системы управления инвентарем и тесты

---
+++ src/test/java/school/faang/task_45560/InventoryManagerTest.java
@@ -0,0 +1,90 @@
+package school.faang.task_45560;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class InventoryManagerTest {
+    private InventoryManager manager;
+    private Character frodo;
+    private Item ring;
+    private Item necklace;
+
+    @BeforeEach
+    void setUp() {
+        manager = new InventoryManager();
+        frodo = new Character("Frodo");
+        ring = new Item("The One Ring", 1000);
+        necklace = new Item("The Necklace", 1340);
+    }
+
+    @Test
+    void testSuccessAddItem() {
+        manager.addItem(frodo, ring, (item) -> System.out.println(item.getName() + " был добавлен в инвентарь."));
+
+        assertTrue(frodo.getInventory().contains(ring));
+    }
+
+    @Test
+    void testSuccessRemoveItem() {
+        manager.addItem(frodo, ring, (item) -> System.out.println(item.getName() + " был добавлен в инвентарь."));
+        manager.addItem(frodo, necklace, (item) -> System.out.println(item.getName() + " был добавлен в инвентарь."));
+
+        manager.removeItem(frodo, (item) -> item.getName().equals("The One Ring"));
+
+        assertTrue(frodo.getInventory().contains(necklace));
+        assertFalse(frodo.getInventory().contains(ring));
+    }
+
+    @Test
+    void testRemoveItemIfItemDoesNotPassThroughFilter() {
+        manager.addItem(frodo, ring, (item) -> System.out.println(item.getName() + " был добавлен в инвентарь."));
+        manager.addItem(frodo, necklace, (item) -> System.out.println(item.getName() + " был добавлен в инвентарь."));
+
+        manager.removeItem(frodo, (item) -> item.getName().equals("wrong name"));
+
+        assertTrue(frodo.getInventory().contains(necklace));
+        assertTrue(frodo.getInventory().contains(ring));
+    }
+
+    @Test
+    void testRemoveItemIfInventoryIsEmpty() {
+        manager.removeItem(frodo, (item) -> item.getName().equals("wrong name"));
+
+        assertTrue(frodo.getInventory().isEmpty());
+    }
+
+    @Test
+    void testSuccessUpdateItem() {
+        manager.addItem(frodo, ring, (item) -> System.out.println(item.getName() + " был добавлен в инвентарь."));
+        manager.updateItem(frodo,
+            (item) -> item.getName().equals("The One Ring"),
+            (item) -> new Item(item.getName(), item.getValue() * 2)
+        );
+
+        assertTrue(frodo.getInventory().contains(ring));
+        assertEquals(2000, frodo.getInventory().get(0).getValue());
+    }
+
+    @Test
+    void testUpdateItemIfInventoryIsEmpty() {
+        manager.updateItem(frodo,
+            (item) -> item.getName().equals("The One Ring"),
+            (item) -> new Item(item.getName(), item.getValue() * 2)
+        );
+
+        assertTrue(frodo.getInventory().isEmpty());
+    }
+
+    @Test
+    void testUpdateItemIfItemDoesNotPassThroughFilter() {
+        manager.addItem(frodo, ring, (item) -> System.out.println(item.getName() + " был добавлен в инвентарь."));
+        manager.updateItem(frodo,
+            (item) -> item.getName().equals("The"),
+            (item) -> new Item(item.getName(), item.getValue() * 2)
+        );
+
+        assertEquals(ring.getValue(), frodo.getInventory().get(0).getValue());
+    }
+}
