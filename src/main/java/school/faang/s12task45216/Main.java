2cda48de49f57769bee2f7796123f368cd1432d4
lcodio <lcodio@temp.com>
2024-12-10 16:28:35 +0300

    sprint 1.3 task BJS2-45216 solved

---
+++ src/main/java/school/faang/s12task45216/Main.java
@@ -0,0 +1,80 @@
+package school.faang.s12task45216;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class Main {
+    private static StudentDatabase sd;
+
+    public static void main(String[] args) {
+        HashMap<Student, Map<Subject, Integer>> subjectsWithGrades = new HashMap<>();
+        HashMap<Subject, List<Student>> subjectWithStudents = new HashMap<>();
+        sd = new StudentDatabase(subjectsWithGrades, subjectWithStudents);
+
+        Student student1 = new Student(1, "Sam Kan");
+        Student student2 = new Student(2, "Kim Pack");
+        List<Student> students = new ArrayList<>();
+        students.add(student1);
+        students.add(student2);
+        Subject subject1 = new Subject(3, "Math");
+        Subject subject2 = new Subject(4, "Physics");
+        Map<Subject, Integer> grades1 = new HashMap<>();
+        grades1.put(subject1, 4);
+        grades1.put(subject2, 4);
+        Map<Subject, Integer> grades2 = new HashMap<>();
+        grades2.put(subject1, 5);
+        grades2.put(subject2, 5);
+
+        testAddStudentWithSubjectsAndGrades(student1, grades1);
+        testAddStudentWithSubjectsAndGrades(student2, grades2);
+        // testAddStudentWithSubjectsAndGrades(null, grades1); // some tests for null
+        // testAddStudentWithSubjectsAndGrades(student1, null);
+        testAddSubjectForStudent(student1, subject1, 3);
+        testAddSubjectForStudent(student1, subject2, 3);
+        // testAddSubjectForStudent(null, subject2, 3);
+        // testAddSubjectForStudent(student2, null, 4);
+        testRemoveStudentWithSubjects(student1);
+        testPrintAllStudentsWithSubjectsAndGrades();
+
+        testAddSubjectWithStudents(subject1, students);
+        testAddSubjectWithStudents(subject2, students);
+        tesAddStudentToSubject(student1, subject1);
+        tesAddStudentToSubject(student2, subject1);
+        testRemoveStudentFromSubject(student2, subject1);
+        testPrintAllSubjectsWithStudents();
+    }
+
+    public static void testAddStudentWithSubjectsAndGrades(Student student, Map<Subject, Integer> grades) {
+        sd.addStudentWithSubjectsAndGrades(student, grades);
+    }
+
+    public static void testAddSubjectForStudent(Student student, Subject subject, int grade) {
+        sd.addSubjectForStudent(student, subject, grade);
+    }
+
+    public static void testRemoveStudentWithSubjects(Student student) {
+        sd.removeStudentWithSubjects(student);
+    }
+
+    public static void testPrintAllStudentsWithSubjectsAndGrades() {
+        sd.printAllStudentsWithSubjectsAndGrades();
+    }
+
+    public static void testAddSubjectWithStudents(Subject subject, List<Student> students) {
+        sd.addSubjectWithStudents(subject, students);
+    }
+
+    public static void tesAddStudentToSubject(Student student, Subject subject) {
+        sd.addStudentToSubject(student, subject);
+    }
+
+    public static void testRemoveStudentFromSubject(Student student, Subject subject) {
+        sd.removeStudentFromSubject(student, subject);
+    }
+
+    public static void testPrintAllSubjectsWithStudents() {
+        sd.printAllSubjectsWithStudents();
+    }
+}
---
+++ src/main/java/school/faang/s12task45216/StudentDatabase.java
@@ -0,0 +1,71 @@
+package school.faang.s12task45216;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.extern.slf4j.Slf4j;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+
+@Slf4j
+@Getter
+@AllArgsConstructor
+public class StudentDatabase {
+    HashMap<Student, Map<Subject, Integer>> subjectsWithGrades;
+    HashMap<Subject, List<Student>> subjectWithStudents;
+
+    public void addStudentWithSubjectsAndGrades(Student student, Map<Subject, Integer> grades) {
+        Optional.ofNullable(student).ifPresent(s -> subjectsWithGrades.put(student, grades));
+        log.info("The Student with Subjects and Grades have added");
+    }
+
+    public void addSubjectForStudent(Student student, Subject subject, int grade) {
+/*        Optional.ofNullable(subjectsWithGrades.get(student))
+                .ifPresent(subjects -> subjects.putIfAbsent(subject, grade));*/
+
+        // думаю, что по логике задачи решение с computeIfAbsent лучше, чем с Optional.ofNullable
+        subjectsWithGrades.computeIfAbsent(student, s -> new HashMap<>()).put(subject, grade);
+        log.info("The Subject for Student have added");
+    }
+
+    public void removeStudentWithSubjects(Student student) {
+        Optional.ofNullable(student).ifPresent(s -> subjectsWithGrades.remove(s));
+        log.info("The Student have removed");
+    }
+
+    public void printAllStudentsWithSubjectsAndGrades() {
+        subjectsWithGrades.forEach((student, subjects) -> {
+            log.info("{}", student);
+            subjects.forEach((subject, grade) -> log.info("{}, {}", subject, grade));
+        });
+    }
+
+    public void addSubjectWithStudents(Subject subject, List<Student> students) {
+        Optional.ofNullable(subject).ifPresent(s -> subjectWithStudents.put(subject, students));
+        log.info("The Subject with Students have added");
+    }
+
+    public void addStudentToSubject(Student student, Subject subject) {
+        subjectWithStudents.computeIfAbsent(subject, s -> new ArrayList<>()).add(student);
+        log.info("The Student to the Subject have added");
+    }
+
+    public void removeStudentFromSubject(Student student, Subject subject) {
+        Optional.ofNullable(subject).ifPresent(s -> subjectWithStudents.
+                computeIfPresent(s, (k, v) -> {
+                    v.removeIf(st -> st.equals(student));
+                    return v;
+                }));
+        log.info("The Student from Subject have removed");
+    }
+
+    public void printAllSubjectsWithStudents() {
+        subjectWithStudents.forEach(((subject, students) -> {
+            log.info("{}", subject);
+            students.forEach((student -> log.info("{}", student)));
+        }));
+    }
+}
