d1a4c654d093faafa65b5b46bd5a3990b610c04a
Anton Graf <ant0ngraf@yandex.ru>
2024-12-10 14:26:39 +0600

    Добавляет тесты фильтров сообщений

--- src/test/java/school/faang/bjs_46652/NotificationManagerTest.java
+++ src/test/java/school/faang/bjs_46652/NotificationManagerTest.java
@@ -7,6 +7,7 @@ import org.junit.jupiter.params.provider.MethodSource;
 
 import java.util.List;
 import java.util.function.Consumer;
+import java.util.function.Predicate;
 import java.util.stream.Stream;
 
 class NotificationManagerTest {
@@ -30,20 +31,36 @@ class NotificationManagerTest {
         Assertions.assertTrue(actual);
     }
 
+    @ParameterizedTest
+    @MethodSource("getNotifications")
+    public void sendNotificationWithFilters(Notification notification) {
+        registerDefaultHandlers();
+        registerDefaultFilters();
+        boolean actual = manager.sendNotification(notification);
+        Assertions.assertFalse(actual);
+    }
+
+    @ParameterizedTest
+    @MethodSource("registerFilters")
+    public void registerFilter(String filterName, Predicate<String> filter) {
+        List<String> beforeRegister = manager.getRegisteredFilters();
+        manager.registerFilter(filterName, filter);
+        List<String> afterRegister = manager.getRegisteredFilters();
+        Assertions.assertTrue(afterRegister.containsAll(beforeRegister));
+        Assertions.assertEquals(afterRegister.size(), beforeRegister.size() + 1);
+    }
+
     public static Stream<Arguments> registerParameters() {
         return Stream.of(
                 Arguments.of(
-                        "email", (Consumer<Notification>) (notification) -> {
-                            System.out.println("Отправка по электронной почте: " + notification.getMessage());
-                        }),
+                        "email", (Consumer<Notification>) (notification) ->
+                                System.out.println("Отправка по электронной почте: " + notification.getMessage())),
                 Arguments.of(
-                        "sms", (Consumer<Notification>) (notification) -> {
-                            System.out.println("Отправка SMS: " + notification.getMessage());
-                        }),
+                        "sms", (Consumer<Notification>) (notification) ->
+                                System.out.println("Отправка SMS: " + notification.getMessage())),
                 Arguments.of(
-                        "push", (Consumer<Notification>) (notification) -> {
-                            System.out.println("Отправка push-уведомления: " + notification.getMessage());
-                        }));
+                        "push", (Consumer<Notification>) (notification) ->
+                                System.out.println("Отправка push-уведомления: " + notification.getMessage())));
     }
 
     public static Stream<Arguments> getNotifications() {
@@ -62,6 +79,28 @@ class NotificationManagerTest {
                 )
 
         );
+    }
+
+    public static Stream<Arguments> registerFilters() {
+        return Stream.of(
+                Arguments.of(
+                        "Message maximum length", (Predicate<String>) (message) ->
+                                message.length() <= 35),
+                Arguments.of(
+                        "Message minimum length", (Predicate<String>) (message) ->
+                                message.length() > 31),
+                Arguments.of(
+                        "Contains JohnDoe", (Predicate<String>) (message) ->
+                                !message.contains("JohnDoe"))
+                );
+
+    }
+
+    private void registerDefaultFilters() {
+        manager.unregisterAllFilters();
+        manager.registerFilter("Message maximum length", message -> message.length() <= 35);
+        manager.registerFilter("Message minimum length", message -> message.length() > 31);
+        manager.registerFilter("Contains JohnDoe", message -> !message.contains("JohnDoe"));
     }
 
     private void registerDefaultHandlers() {
