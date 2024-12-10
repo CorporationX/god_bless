73c9602c1f8c0012dc95c6e45212df8e1246967c
AesaQ <ilyaovchinnikov2095@gmail.com>
2024-12-10 11:56:01 +0500

    checkstyle fix

--- src/main/java/school/faang/instagram_filters/FilterProcessor.java
+++ src/main/java/school/faang/instagram_filters/FilterProcessor.java
@@ -6,6 +6,7 @@ public class FilterProcessor {
     public Image applyFilter(Image image, Function<Image, Image> filter) {
         return filter.apply(image);
     }
+
     public Function<Image, Image> combineFilters(Function<Image, Image> firstFilter,
                                                   Function<Image, Image> secondFilter) {
         return image -> secondFilter.apply(
