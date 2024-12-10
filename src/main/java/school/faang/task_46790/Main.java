ab224b0ee86e445db8ae104b2d2456fcec7357ed
Alfir Rakhimov <exprnc7@gmail.com>
2024-12-10 20:00:52 +0500

    Потренируем StreamAPI 1

---
+++ src/main/java/school/faang/task_46790/Main.java
@@ -0,0 +1,24 @@
+package school.faang.task_46790;
+
+import lombok.extern.slf4j.Slf4j;
+
+import java.util.Arrays;
+import java.util.List;
+
+@Slf4j
+public class Main {
+    public static void main(String[] args) {
+        List<Integer> numbers = Arrays.asList(1, 2, 3, 4, 5, 6);
+        List<String> strings = Arrays.asList("apple", "banana", "cherry", "date");
+
+        log.info("Sum of even numbers: {}", ListOperations.sumOfEvenNumbers(numbers));
+        log.info("Maximum: {}", ListOperations.findMax(numbers));
+        log.info("Average: {}", ListOperations.findAverage(numbers));
+        log.info("Count of strings starting with 'a': {}", ListOperations.countStringsStartingWith(strings, 'a'));
+        log.info("Strings containing 'an': {}", ListOperations.filterStringsContainingSubstring("an", strings));
+        log.info("Strings sorted by length: {}", ListOperations.sortByLength(strings));
+        log.info("Are all numbers even? {}", ListOperations.allMatchCondition(numbers, n -> n % 2 == 0));
+        log.info("Smallest number greater than 4: {}", ListOperations.findMinGreaterThan(numbers, 4));
+        log.info("Lengths of strings: {}", ListOperations.convertToLengths(strings));
+    }
+}
