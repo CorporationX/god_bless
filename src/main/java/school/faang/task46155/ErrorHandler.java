b475629a17056927d70be6885b04bf8a2c85e6e3
lehaps <awda.96@bk.ru>
2024-12-10 18:19:09 +0300

    Поменял на интерфейс Callable (так как писало ошибку) Добавил логгирование и аннотации

--- src/main/java/school/faang/task46155/ErrorHandler.java
+++ src/main/java/school/faang/task46155/ErrorHandler.java
@@ -1,26 +1,22 @@
 package school.faang.task46155;
 
-import java.util.function.Supplier;
+import lombok.NonNull;
+import org.slf4j.LoggerFactory;
+import org.slf4j.Logger;
+
+import java.util.concurrent.Callable;
 
 public class ErrorHandler {
-    public static <T> T withErrorHandling(Supplier<T> serviceSupplier,
-                                          ErrorHandling<T> errorHandling) {
-        validate(serviceSupplier, errorHandling);
 
+    private static final Logger logger = LoggerFactory.getLogger(ErrorHandler.class);
+
+    public static <T> T withErrorHandling(@NonNull Callable<T> serviceSupplier,
+                                          @NonNull ErrorHandling<T> errorHandling) {
         try {
-            return serviceSupplier.get();
+            return serviceSupplier.call();
         } catch (Exception e) {
+            logger.error(e.getMessage(), e);
             return errorHandling.handleError(e);
-        }
-    }
-
-    private static <T> void validate(Supplier<T> serviceSupplier,
-                                     ErrorHandling<T> errorHandling) {
-        if (serviceSupplier == null) {
-            throw new IllegalArgumentException("serviceSupplier is null");
-        }
-        if (errorHandling == null) {
-            throw new IllegalArgumentException("errorHandling is null");
         }
     }
 }
--- src/main/java/school/faang/task46155/Main.java
+++ src/main/java/school/faang/task46155/Main.java
@@ -3,13 +3,7 @@ package school.faang.task46155;
 public class Main {
     public static void main(String[] args) {
         String result = ErrorHandler.withErrorHandling(
-                () -> {
-                    try {
-                        return RemoteService.call("param");
-                    } catch (Exception e) {
-                        throw new RuntimeException(e);
-                    }
-                },
+                () -> RemoteService.call("param"),
                 exception -> {
                     System.out.println("Ошибка при вызове сервиса, возвращаем дефолтное значение");
                     return "DEFAULT";
