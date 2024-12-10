f154dba51ad4ceabd379f84c4733d50ab1e143f0
ZhannaD <zhanna.davidov@gmail.com>
2024-12-10 09:51:28 +0200

    collecting users

---
+++ src/main/java/school/faang/collectingusers/User.java
@@ -0,0 +1,35 @@
+package school.faang.collectingusers;
+
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.ToString;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
+
+@Getter
+@AllArgsConstructor
+@ToString
+@EqualsAndHashCode
+public class User {
+    Long id;
+    String name;
+    Integer age;
+    Set<String> activities;
+
+    public static Map<Long, String> findHobbyLovers(Set<User> users, Set<String> activities) {
+        Map<Long, String> returnUsers = new HashMap<>();
+        for (User user : users) {
+            Set<String> userActivities = user.getActivities();
+            for (String activity : activities) {
+                if (userActivities.contains(activity)) {
+                    returnUsers.put(user.id, activity);
+                }
+            }
+        }
+        return returnUsers;
+
+    }
+}
