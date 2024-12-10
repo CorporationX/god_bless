51715573e07c5e7779d8c108dff385ca191adcda
Gromov Ilya <109763246+DMCHMB@users.noreply.github.com>
2024-12-10 23:30:29 +0300

    BJS2-46345 Фильтры Instagram
    Провел небольшое исследование на тему валидации, логгера и null. Если в метод прилетает null - стандартно достать метод можно через рефлексию, но это часто запрещено.
    
    Еще 2 основных варианта
    - Thread.currentThread().getStackTrace() - тащит полностью весь стек вызовов, потом переводит в массив, что очень дорого
    - Stack-Walking API - рекомендуется с Java 9+, его и использовал.

--- src/main/java/school/faang/sprint_2/task_bjs246345/FilterProcessor.java
+++ src/main/java/school/faang/sprint_2/task_bjs246345/FilterProcessor.java
@@ -3,27 +3,44 @@ package school.faang.sprint_2.task_bjs246345;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.lang.StackWalker.StackFrame;
+import java.util.Optional;
 import java.util.function.UnaryOperator;
 
 public class FilterProcessor {
     private final Logger log = LoggerFactory.getLogger(FilterProcessor.class);
 
     public Image applyFilter(Image image, UnaryOperator<Image> filter) {
-        checkValidInput("applyFilter", image, filter);
+        checkValidInput(image, filter);
         return filter.apply(image);
     }
 
     public UnaryOperator<Image> combineFilters(
-            UnaryOperator<Image> firstFilter, UnaryOperator<Image> secondFilter
-    ) {
-        checkValidInput("combineFilters", firstFilter, secondFilter);
-        return image -> secondFilter.apply(firstFilter.apply(image));
+            UnaryOperator<Image> firstFilter, UnaryOperator<Image> secondFilter) {
+        checkValidInput(firstFilter, secondFilter);
+        return image -> firstFilter.andThen(secondFilter).apply(image);
     }
 
-    private <T, U> void checkValidInput(String methodName, T first, U second) {
+    private <T, U> void checkValidInput(T first, U second) {
         if (first == null || second == null) {
-            log.warn("get null input in method {}", methodName);
-            throw new IllegalArgumentException("Input in method " + methodName + " cannot be null");
+            Optional<StackFrame> frameOptional = StackWalker.getInstance()
+                    .walk(frames -> frames
+                            .skip(2)
+                            .findFirst());
+
+            String callingMethod;
+            if (frameOptional.isPresent()) {
+                StackFrame frame = frameOptional.get();
+                callingMethod = "Class: " + frame.getClassName()
+                        + " method: " + frame.getMethodName()
+                        + " line " + frame.getLineNumber();
+                log.warn("Null input in method {}", callingMethod);
+            } else {
+                callingMethod = "Unknown method";
+                log.warn("Unable to get calling method");
+            }
+
+            throw new IllegalArgumentException("Input in " + callingMethod + " cannot be null");
         }
     }
 }
