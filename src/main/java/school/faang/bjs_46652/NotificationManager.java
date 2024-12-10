7b03a08dc2fdf4ca171a28196f449f07717d2ba7
Anton Graf <ant0ngraf@yandex.ru>
2024-12-10 14:26:21 +0600

    Добавляет фильтры для сообщений

--- src/main/java/school/faang/bjs_46652/NotificationManager.java
+++ src/main/java/school/faang/bjs_46652/NotificationManager.java
@@ -6,10 +6,12 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.function.Consumer;
+import java.util.function.Predicate;
 
 @Slf4j
 public class NotificationManager {
     private final Map<String, Consumer<Notification>> handlers = new HashMap<>();
+    private final Map<String, Predicate<String>> filters = new HashMap<>();
 
     public void registerHandler(String type, Consumer<Notification> handler) {
         try {
@@ -20,16 +22,29 @@ public class NotificationManager {
         handlers.put(type, handler);
     }
 
+    public void registerFilter(String name, Predicate<String> filter) {
+        filters.put(name, filter);
+    }
+
     public boolean sendNotification(Notification notification) {
         String notificationType = notification.getNotificationType().name().toLowerCase();
         Consumer<Notification> consumer = handlers.get(notificationType);
 
-        if (consumer == null) {
+        boolean valid = validateMessage(notification.getMessage());
+        if (consumer == null || !valid) {
             return false;
         }
 
         consumer.accept(notification);
         return true;
+    }
+
+    public boolean validateMessage(String message) {
+        return filters.values().stream()
+                .map(filter -> filter.test(message))
+                .filter(filter -> !filter)
+                .findAny()
+                .orElse(true);
     }
 
     public List<String> getRegisteredHandlers() {
@@ -39,4 +54,14 @@ public class NotificationManager {
     public void unregisterAllHandlers() {
         handlers.clear();
     }
+
+    public List<String> getRegisteredFilters() {
+        return List.copyOf(filters.keySet());
+    }
+
+    public void unregisterAllFilters() {
+        filters.clear();
+    }
+
+
 }
