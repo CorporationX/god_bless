ab8acba6b780396f9d1e37190e5e02d63a757f83
TFdevQA <46944859+TFdevQA@users.noreply.github.com>
2024-12-09 18:02:04 -0800

    task 45265: added 2 methods, resolved comments

--- src/main/java/school/faang/sprint1/bjs_45265/StudentDatabase.java
+++ src/main/java/school/faang/sprint1/bjs_45265/StudentDatabase.java
@@ -9,58 +9,77 @@ public class StudentDatabase {
     private final HashMap<Student, Map<Subject, Integer>> studentReport = new HashMap<>();
     private final HashMap<Subject, List<Student>> subjectEnrollment = new HashMap<>();
 
-    public void addStudentRecords(Student student, Map<Subject, Integer> record) {
+    public void addStudentWithSubjectsAndGrades(Student student, Map<Subject, Integer> record) {
         if (student != null) {
             studentReport.put(student, record);
             for (Subject subject : record.keySet()) {
-                subjectEnrollment.putIfAbsent(subject, new ArrayList<>());
-                subjectEnrollment.get(subject).add(student);
+                subjectEnrollment.computeIfAbsent(subject, key -> new ArrayList<>())
+                        .add(student);
             }
         }
     }
 
     public void addNewSubjectForStudent(Student student, Subject subject, Integer grade) {
-        Map<Subject, Integer> studentRecord = studentReport.get(student);
-        if (studentRecord != null) {
-            studentRecord.put(subject, grade);
-            subjectEnrollment.putIfAbsent(subject, new ArrayList<>());
-            subjectEnrollment.get(subject).add(student);
+        if (student != null && subject != null) {
+            studentReport.computeIfAbsent(student, key -> new HashMap<>())
+                    .put(subject, grade);
+            subjectEnrollment.computeIfAbsent(subject, key -> new ArrayList<>())
+                    .add(student);
         }
     }
 
     public void deleteStudentRecords(Student student) {
-        Map<Subject, Integer> recordForRemove = studentReport.remove(student);
-        if (recordForRemove != null) {
-            for (Subject subj : recordForRemove.keySet()) {
-                List<Student> studentsList = subjectEnrollment.get(subj);
-                studentsList.remove(student);
+        if (studentReport.containsKey(student)) {
+            Map<Subject, Integer> recordForRemove = studentReport.remove(student);
+            if (recordForRemove != null) {
+                for (Subject subj : recordForRemove.keySet()) {
+                    List<Student> studentsList = subjectEnrollment.get(subj);
+                    studentsList.remove(student);
+                }
             }
         }
     }
 
+    public void deleteStudentFromSubjectList(Student student, Subject subject) {
+        if (student != null && subject != null) {
+            if (subjectEnrollment.containsKey(subject)) {
+                subjectEnrollment.get(subject).remove(student);
+            }
+        }
+    }
+
+
     public void addNewSubjectRecord(Subject subject, List<Student> studentsList) {
-        subjectEnrollment.put(subject, studentsList);
-        for (Student student : studentsList) {
+        if (subject != null) {
+            subjectEnrollment.put(subject, studentsList);
+            for (Student student : studentsList) {
+                studentReport.computeIfAbsent(student, key -> new HashMap<>()).put(subject, 0);
+            }
+        }
+    }
+
+    public void addStudentEnrollment(Student student, Subject subject) {
+        if (subject != null) {
+            subjectEnrollment.get(subject).add(student);
             studentReport.putIfAbsent(student, new HashMap<>());
             studentReport.get(student).put(subject, 0);
         }
     }
 
-    public void addStudentEnrollment(Student student, Subject subject) {
-        subjectEnrollment.get(subject).add(student);
-        studentReport.putIfAbsent(student, new HashMap<>());
-        studentReport.get(student).put(subject, 0);
+    public void printAllStudents() {
+        studentReport.forEach((student, record) -> {
+            System.out.print("Student: " + student + " >> ");
+            record.forEach((subj, grade) -> {
+                System.out.println("{ Subject: " + subj + "; grade=" + grade + " }");
+            });
+        });
     }
 
-    public void printAllStudents() {
-        for (var pair : studentReport.entrySet()) {
-            String name = pair.getKey().getStudentName();
-            System.out.print("Student name: " + name + " -> ");
-            Map<Subject, Integer> record = pair.getValue();
-            for (var entry : record.entrySet()) {
-                System.out.println(" { Subject: " + entry.getKey().getSubjectName()
-                        + " ; grade=" + entry.getValue() + " }");
-            }
-        }
+    public void printAllSubjects() {
+        subjectEnrollment.forEach((subj, studs) -> {
+            System.out.print("Subject :" + subj + "-> Students: ");
+            studs.forEach(System.out::print);
+            System.out.println();
+        });
     }
 }
--- src/main/java/school/faang/sprint1/bjs_45265/Subject.java
+++ src/main/java/school/faang/sprint1/bjs_45265/Subject.java
@@ -2,6 +2,8 @@ package school.faang.sprint1.bjs_45265;
 
 import lombok.Getter;
 
+import java.util.Objects;
+
 public class Subject {
     private final int subjectId;
     @Getter
@@ -10,5 +12,22 @@ public class Subject {
     public Subject(int subjectId, String subjectName) {
         this.subjectId = subjectId;
         this.subjectName = subjectName;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Subject subject)) return false;
+        return subjectId == subject.subjectId && Objects.equals(subjectName, subject.subjectName);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(subjectId, subjectName);
+    }
+
+    @Override
+    public String toString() {
+        return " " + subjectName + " ";
     }
 }
