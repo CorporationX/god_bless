7b05acd5a59eeb4960ce84914f5c3e83f184c7ff
Maksim-VV <laulmv11@gmail.com>
2024-12-10 18:49:12 +0300

    BJS2_45280 Double Cache-Cache refactoring format

--- src/main/java/school/faang/bjs245280/Main.java
+++ src/main/java/school/faang/bjs245280/Main.java
@@ -40,7 +40,6 @@ public class Main {
 
         System.out.println("----------------------------");
 
-
         studentDatabase.printStudentSubject();
         studentDatabase.deleteStudentSubject(getStudentByName("Bob"));
     }
