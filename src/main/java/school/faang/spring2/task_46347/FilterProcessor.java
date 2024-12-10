cf5300a372e2ae2b29e14ff540d8b3cc15e2ba64
Timka-cloud1 <amanzholtimka@gmail.com>
2024-12-10 13:43:36 +0500

    Фильтры Instagram

---
+++ src/main/java/school/faang/spring2/task_46347/FilterProcessor.java
@@ -0,0 +1,13 @@
+package school.faang.spring2.task_46347;
+
+import java.util.function.Function;
+
+public class FilterProcessor {
+    public Image applyFilter(Image image, Function<Image, Image> function) {
+        return function.apply(image);
+    }
+
+    public Function<Image, Image> combineFilters(Function<Image, Image> function1, Function<Image, Image> function2) {
+        return function1.andThen(function2);
+    }
+}
