257cd23a4b8c8e2b99b25b74e02e248c79c96250
rostex <rostex2@yandex.ru>
2024-12-10 21:16:47 +0300

    Создан класс ListOperations с методами, которые выполняют операции с помощью Stream API

---
+++ src/main/java/school/faang/sprint_2/task_47161/Main.java
@@ -0,0 +1,26 @@
+package school.faang.sprint_2.task_47161;
+
+import java.util.List;
+import java.util.Map;
+
+public class Main {
+    public static void main(String[] args) {
+        List<Integer> numbers = List.of(1, 2, 3, 4, 5, 6);
+        Map<String, String> countriesCity = Map.of("Russia","Moscow",
+                "USA","Washington",
+                "Germany","Berlin");
+        List<String> strings = List.of("apple", "banana", "avocado", "apricot");
+        List<String> anotherStrings = List.of("apple", "banana", "cherry", "date", "fig", "grape");
+        String alphabet = "abcdefghijklmnopqrstuvwxyz";
+
+        System.out.println(ListOperations.findPairOfNumbers(numbers, 7));
+        System.out.println(ListOperations.sortCapitals(countriesCity));
+        System.out.println(ListOperations.filterAndSortStrings(strings, 'a'));
+        System.out.println(ListOperations.convertToBinary(numbers));
+        System.out.println(ListOperations.filterAndSortByLength(anotherStrings, alphabet));
+    }
+
+
+
+
+}
