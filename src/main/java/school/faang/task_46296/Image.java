9e7d6a386183b2b1e94ed735066c7422924977a0
Natalya Vasileva <boryantheone228@yandex.ru>
2024-12-10 18:14:11 +0300

    BJS2-46296 Правки по результатам ревью

--- src/main/java/school/faang/task_46296/Image.java
+++ src/main/java/school/faang/task_46296/Image.java
@@ -8,20 +8,20 @@ public class Image {
     private final String description;
 
     public Image(String name, String description) {
-        validName(name);
-        validDescription(description);
+        validateName(name);
+        validateDescription(description);
 
         this.name = name;
         this.description = description;
     }
 
-    private static void validDescription(String description) {
+    private static void validateDescription(String description) {
         if (description == null || description.isBlank()) {
             throw new IllegalArgumentException("Description don't be null or blank!");
         }
     }
 
-    private static void validName(String name) {
+    private static void validateName(String name) {
         if (name == null || name.isBlank()) {
             throw new IllegalArgumentException("Name don't be null or blank!");
         }
