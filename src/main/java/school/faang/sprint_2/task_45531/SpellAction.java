65fb1806ce0bc929f1f144dbb566384fb27b72fd
tatgricenko <tattgricenko@gmail.com>
2024-12-10 22:05:40 +0300

    Хогвартс, Хогвартс, наш любимый Хогвартс

---
+++ src/main/java/school/faang/sprint_2/task_45531/SpellAction.java
@@ -0,0 +1,6 @@
+package school.faang.sprint_2.task_45531;
+
+@FunctionalInterface
+public interface SpellAction {
+    String act(String spell);
+}
---
+++ src/main/java/school/faang/sprint_2/task_45531/SpellCaster.java
@@ -0,0 +1,7 @@
+package school.faang.sprint_2.task_45531;
+
+public class SpellCaster {
+    public void cast(String spell, SpellAction spellAction) {
+        System.out.println(spellAction.act(spell));
+    }
+}
