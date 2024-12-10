9e46ca4f0405713bda81683110ae18747f96959d
alexey_obraztsov <alexey.autotester86@gmail.com>
2024-12-10 20:43:02 +0300

    BJS2-46747 Мета. initial

---
+++ src/main/java/school/faang/sprint_1/task_metauniversebjs2n46747/NotificationManager.java
@@ -0,0 +1,18 @@
+package school.faang.sprint_1.task_metauniversebjs2n46747;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.function.Consumer;
+
+public class NotificationManager {
+    private final Map<String, Consumer<Notification>> mapNotification = new HashMap<>();
+
+    public void registerHandler(String type, Consumer<Notification> consumer) {
+        mapNotification.put(type, consumer);
+    }
+
+    public void sendNotification(Notification notification) {
+        Consumer<Notification> consumer = mapNotification.get(notification.getType());
+        consumer.accept(notification);
+    }
+}
