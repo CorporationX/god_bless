9f871469a42d84b8a43d7f9461aeb1b34670a4ca
CendalJoe <sova333sova@mail.ru>
2024-12-10 20:40:51 +0300

    first commit

---
+++ src/main/java/CendalJoe_BJS2_45727/SpellAction.java
@@ -0,0 +1,5 @@
+package CendalJoe_BJS2_45727;
+
+public interface SpellAction {
+     String showSpell ( String spell );
+}
---
+++ src/main/java/CendalJoe_BJS2_45727/SpellCaster.java
@@ -0,0 +1,12 @@
+package CendalJoe_BJS2_45727;
+
+public class SpellCaster {
+
+    public void viewCast(String spellName, SpellAction action) {
+       action.showSpell(spellName);{
+           System.out.println(spellName);
+        }
+    }
+
+}
+
