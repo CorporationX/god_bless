3b46149aaa7705d7dfd886259375bd7de774d82b
RGZ_TTT <rgz.ttt@mail.ru>
2024-12-11 05:07:39 +0700

    Added LibrarySystem and functions-methods for library class

---
+++ src/main/java/school/faang/task_45357/Main.java
@@ -0,0 +1,32 @@
+package school.faang.task_45357;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Main {
+    public static void main(String[] args) {
+
+        LibrarySystem library = new LibrarySystem();
+
+        library.addBook("TestTitleOne", "TestAuthorOne", 1999,"Верхняя полка");
+        library.addBook("TestTitleTwo","TestAuthorTwo", 1438,"Нижняя полка");
+        library.addBook("TestTitleThree","TestAuthorThree", 1933,"Средняя полка");
+        library.addBook("TestTitleFour","TestAuthorFour", 1982,"Нижняя полка");
+        library.addBook("TestTitleFive","TestAuthorFive", 1933,"Верхняя полка");
+
+        library.printAllBooks();
+
+        library.removeBook("TestTitleFive","TestAuthorFive", 1933);
+
+        library.printAllBooks();
+
+        library.removeBook("TestTitleFour","TestAuthorFour", 1981);
+
+        library.findBook("TestTitleOne", "TestAuthorOne", 1999);
+
+        library.printAllBooks();
+
+
+
+    }
+}
