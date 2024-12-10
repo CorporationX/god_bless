ed5a65cb4185f0d70fd0fa46420d33e86b1ca7a9
askar0103 <ananaspineapple2048@gmail.com>
2024-12-10 13:45:54 +0300

    slf4j

--- src/main/java/school/faang/Main.java
+++ src/main/java/school/faang/Main.java
@@ -1,5 +1,12 @@
 package school.faang;
 
-public class Main {
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
+public class Main {
+    private static final Logger logger  = LoggerFactory.getLogger(Main.class);
+
+    public static void main(String[] args) {
+        logger.info("slf4j");
+    }
 }
