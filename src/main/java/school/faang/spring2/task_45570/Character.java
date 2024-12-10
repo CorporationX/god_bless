0c618bef9ba4b0c442e707f6a15e82d958082899
Timka-cloud1 <amanzholtimka@gmail.com>
2024-12-10 23:52:11 +0500

    Lord of the Rings RPG

---
+++ src/main/java/school/faang/spring2/task_45570/Character.java
@@ -0,0 +1,18 @@
+package school.faang.spring2.task_45570;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Getter
+public class Character {
+    private String name;
+    private List<Item> inventory;
+
+    public Character(String name) {
+        this.name = name;
+        this.inventory = new ArrayList<>();
+    }
+}
