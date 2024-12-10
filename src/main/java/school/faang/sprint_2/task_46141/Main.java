801868b353660b83d21c14787ac055c468192aad
Stanislav Bugaev <stasikbygaev@gmail.com>
2024-12-10 22:23:45 +0500

    Решение задачи

---
+++ src/main/java/school/faang/sprint_2/task_46141/Main.java
@@ -0,0 +1,48 @@
+package school.faang.sprint_2.task_46141;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(toCsv(
+                List.of(
+                        List.of("1", "2", "3", "4", "5"),
+                        List.of("6", "7", "8", "9", "10"),
+                        List.of("11", "12", "13", "14", "15"),
+                        List.of("16", "17", "18", "19", "20"),
+                        List.of("21", "22", "23", "24", "25")
+                )
+        ));
+
+        try {
+            toCsv(List.of(List.of(), List.of("6", "7", "8", "9", "10")));
+        } catch (IllegalArgumentException e) {
+            System.out.println("Vector illegal");
+        }
+
+        try {
+            toCsv(List.of());
+        } catch (IllegalArgumentException e) {
+            System.out.println("Matrix illegal");
+        }
+    }
+
+    public static String toCsv(List<List<String>> table) {
+        VectorJoiner<String> vectorJoiner = (vector) -> {
+            if (vector.isEmpty()) {
+                throw new IllegalArgumentException("Vector can`t be empty");
+            }
+            return String.join(", ", vector);
+        };
+
+        MatrixJoiner<String> matrixJoiner = (matrix) -> {
+            if (matrix.isEmpty()) {
+                throw new IllegalArgumentException("Matrix can`t be empty");
+            }
+            return matrix.stream().map(vectorJoiner::join).collect(Collectors.joining("\n"));
+        };
+
+        return matrixJoiner.join(table);
+    }
+}
