8d5b348a4a31deffe53d2c52967726e96cb1e25e
Arseni Zaytsau <zarseniy280@gmail.com>
2024-12-10 22:11:59 +0300

    ztsv-BJS2-43913 - added one more whitespace

--- src/main/java/school/faang/bsj43913/Archer.java
+++ src/main/java/school/faang/bsj43913/Archer.java
@@ -2,7 +2,7 @@ package school.faang.bsj43913;
 
 public class Archer extends Character {
     public Archer(String name) {
-        super(name, 3, 10,5);
+        super(name, 3, 10, 5);
     }
 
     @Override
