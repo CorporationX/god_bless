cf66ef946e1999659600a5541428d74ffb9d6e90
Dias Kamza <kamzadias31@gmail.com>
2024-12-10 16:47:47 +0500

    Fixed according to comments

---
+++ src/main/java/school/faang/BJS2_43757/Main.java
@@ -0,0 +1,14 @@
+package school.faang.BJS2_43757;
+
+public class Main {
+    public static void main(String[] args) {
+        try {
+            User user1 = new User("Dias", 25, "Google", "London");
+            System.out.println(user1);
+
+            User user2 = new User("Arman", 11, "Microsoft", "Paris");
+        } catch (IllegalArgumentException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+}
--- src/main/java/school/faang/BJS2_43757/User.java
+++ src/main/java/school/faang/BJS2_43757/User.java
@@ -1,4 +1,4 @@
-package school.faang;
+package school.faang.BJS2_43757;
 
 import lombok.Getter;
 import lombok.Setter;
