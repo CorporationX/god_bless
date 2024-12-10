59a71a0593af09972136a7eda5159f7bd614b5db
MuslimKhamkhoev2022 <159486820+MuslimKhamkhoev2022@users.noreply.github.com>
2024-12-10 22:49:42 +0300

    Revert "Mountain man bjs2 45463"

--- src/main/java/school/faang/hogwarts/hogwarts/our/beloved/hogwarts/SpellAction.java
+++
@@ -1,6 +0,0 @@
-package school.faang.hogwarts.hogwarts.our.beloved.hogwarts;
-
-@FunctionalInterface
-public interface SpellAction {
-    public String spell(String spellName);
-}
--- src/main/java/school/faang/hogwarts/hogwarts/our/beloved/hogwarts/SpellCaster.java
+++
@@ -1,9 +0,0 @@
-package school.faang.hogwarts.hogwarts.our.beloved.hogwarts;
-
-public class SpellCaster {
-    public void cast(String spellName, SpellAction spellAction) {
-        String result = spellAction.spell(spellName);
-        System.out.println(result);
-    }
-}
-
