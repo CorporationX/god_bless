b45ed3e15f602355102cc8d7a1e42ea4e5709c26
senty <senty1844@gmail.com>
2024-12-10 19:19:57 +0300

    Task-BJS2-46693 add nullPointer check

--- src/main/java/school/faang/sprint_2/task_46693/NotificationManager.java
+++ src/main/java/school/faang/sprint_2/task_46693/NotificationManager.java
@@ -2,13 +2,13 @@ package school.faang.sprint_2.task_46693;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.NoSuchElementException;
 import java.util.function.Consumer;
 
 public class NotificationManager {
     private final Map<NotificationType, Consumer<Notification>> handlers = new HashMap<>();
 
     public void registerHandler(NotificationType notificationType, Consumer<Notification> handler) {
-        //не уверен нужна ли эта проверка
         if (notificationType == null || handler == null) {
             throw new IllegalArgumentException("Invalid params");
         }
@@ -19,6 +19,11 @@ public class NotificationManager {
         if (notification == null) {
             throw new IllegalArgumentException("notification is null");
         }
-        handlers.get(notification.type()).accept(notification);
+
+        try {
+            handlers.get(notification.type()).accept(notification);
+        } catch (NullPointerException e) {
+            throw new NoSuchElementException();
+        }
     }
 }
