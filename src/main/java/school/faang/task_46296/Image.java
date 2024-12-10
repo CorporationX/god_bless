f88cdd8a745141c5cc6cf83e68a427d15ca77d9a
Natalya Vasileva <boryantheone228@yandex.ru>
2024-12-10 11:04:38 +0300

    BJS2-46296 Добавление проверки свойств при создании Image

--- src/main/java/school/faang/task_46296/Image.java
+++ src/main/java/school/faang/task_46296/Image.java
@@ -1,11 +1,29 @@
 package school.faang.task_46296;
 
-import lombok.AllArgsConstructor;
 import lombok.Getter;
 
 @Getter
-@AllArgsConstructor
 public class Image {
     private final String name;
     private final String description;
+
+    public Image(String name, String description) {
+        validName(name);
+        validDescription(description);
+
+        this.name = name;
+        this.description = description;
+    }
+
+    private static void validDescription(String description) {
+        if (description == null || description.isBlank()) {
+            throw new IllegalArgumentException("Description don't be null or blank!");
+        }
+    }
+
+    private static void validName(String name) {
+        if (name == null || name.isBlank()) {
+            throw new IllegalArgumentException("Name don't be null or blank!");
+        }
+    }
 }
