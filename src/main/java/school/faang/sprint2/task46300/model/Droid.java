58ea82e417c46b58d789f35add9be3d17eb8b104
Alexey Gorokhov <almigor@mail.ru>
2024-12-11 00:46:52 +0300

    исправления согласно ревью

--- src/main/java/school/faang/sprint2/task46300/model/Droid.java
+++ src/main/java/school/faang/sprint2/task46300/model/Droid.java
@@ -10,19 +10,13 @@ public class Droid {
 
     private final String name;
 
-    private String encryptMessage(String originalMessage, int encryptionKey, EncryptType encryptType) {
+    private String encryptMessage(String originalMessage, int encryptionKey) {
         DroidMessageEncryptor encryptor = (message, key) -> {
             StringBuilder encryptedMessage = new StringBuilder();
             for (char ch : message.toCharArray()) {
                 if (Character.isLetter(ch)) {
                     char base = Character.isLowerCase(ch) ? 'a' : 'A';
-                    int chr;
-                    if (encryptType == EncryptType.ENCRYPT) {
-                        chr =  (ch - base + key) % ALPHABET_SIZE + base;
-                    } else {
-                        chr =  (ch - base - key + ALPHABET_SIZE) % ALPHABET_SIZE + base;
-                    }
-                    encryptedMessage.append((char) chr);
+                    encryptedMessage.append((char)((ch - base + key) % ALPHABET_SIZE + base));
                 } else {
                     encryptedMessage.append(ch);
                 }
@@ -33,7 +27,7 @@ public class Droid {
     }
 
     public void sendMessage(Droid droid, String message, int key) {
-        String encryptedMessage = this.encryptMessage(message, key, EncryptType.ENCRYPT);
+        String encryptedMessage = this.encryptMessage(message, key);
         System.out.println("Droid " + this.name
                 + " send encrypted message to droid " + droid.getName()
                 + ": " + encryptedMessage);
@@ -41,7 +35,7 @@ public class Droid {
     }
 
     public void receiveMessage(String message, int key) {
-        String decryptedMessage = this.encryptMessage(message, key, EncryptType.DECRYPT);
+        String decryptedMessage = this.encryptMessage(message, ALPHABET_SIZE - key);
         System.out.println("Droid " + this.name + " got a message and decrypt it: " + decryptedMessage);
     }
 }
--- src/main/java/school/faang/sprint2/task46300/model/EncryptType.java
+++
@@ -1,6 +0,0 @@
-package school.faang.sprint2.task46300.model;
-
-public enum EncryptType {
-    ENCRYPT,
-    DECRYPT
-}
