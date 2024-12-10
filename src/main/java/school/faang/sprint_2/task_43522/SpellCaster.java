e3d4642c93871a8275160d38e055c0be03bdedbe
KsushaTarasova <ksutaras543@mail.ru>
2024-12-10 12:07:14 +0300

    Cast spells

---
+++ src/main/java/school/faang/sprint_2/task_43522/SpellCaster.java
@@ -0,0 +1,7 @@
+package school.faang.sprint_2.task_43522;
+
+public class SpellCaster {
+    public void cast(String spellName, SpellAction spellAction) {
+        System.out.println(spellAction.printSpellAction(spellName));
+    }
+}
