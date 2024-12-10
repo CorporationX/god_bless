ba7df3ff10fb433bb58650684536e95791ab21c4
ArzamaS Ilya <coldoff@inbox.ru>
2024-12-10 22:53:37 +0300

    Created class and interface.

---
+++ src/main/java/school/faang/task_45541/SpellAction.java
@@ -0,0 +1,5 @@
+package school.faang.task_45541;
+@FunctionalInterface
+public interface SpellAction {
+    String act(String act);
+}
---
+++ src/main/java/school/faang/task_45541/SpellCaster.java
@@ -0,0 +1,7 @@
+package school.faang.task_45541;
+
+public class SpellCaster implements SpellAction{
+    public void cast(String spell, SpellAction action){
+
+    }
+}
