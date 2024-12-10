273f409b0bee567925683d896343ed447cde1eec
Maria Bortsova <bortsovamariia@gmail.com>
2024-12-10 09:47:31 +0300

    refactor after review

--- src/main/java/school/faang/task_44825/WeatherService.java
+++ src/main/java/school/faang/task_44825/WeatherService.java
@@ -3,15 +3,21 @@ package school.faang.task_44825;
 import java.util.Random;
 
 public class WeatherService implements WeatherProvider {
-    private final Random random = new Random();
+    private static final Random RANDOM = new Random();
+    private static final double MAX_TEMPERATURE = 50;
+    private static final double MIN_TEMPERATURE = -50;
+    private static final int NUMBER_SIGN = RANDOM.nextBoolean() ? 1 : -1;
+    private static final int MIN_HUMIDITY = 0;
+    private static final int MAX_HUMIDITY = 100;
 
     @Override
     public WeatherData fetchWeatherData(String city) {
         if (city == null) {
             throw new IllegalArgumentException("City doesn't exist");
         }
-        double temperature = random.nextDouble(((50 + 50 + 1) - 50)) * (random.nextBoolean() ? 1 : -1);
-        double humidity = random.nextDouble(100 + 1);
+        double temperature = RANDOM.nextDouble(((MAX_TEMPERATURE - MIN_TEMPERATURE + 1) + MIN_TEMPERATURE))
+                * NUMBER_SIGN;
+        double humidity = RANDOM.nextDouble((MAX_HUMIDITY - MIN_HUMIDITY) + 1);
         return new WeatherData(city, temperature, humidity, System.currentTimeMillis());
     }
 }
