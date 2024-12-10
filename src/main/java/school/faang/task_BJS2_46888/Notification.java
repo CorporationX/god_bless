822c260d9a7eb54dc1679842d031a3a0ce7c3830
VladimirGontarenko <mister.gontarenko@yandex.ru>
2024-12-11 02:00:21 +0400

    BJS2-46888 Meta вселенная.

---
+++ src/main/java/school/faang/task_BJS2_46888/Notification.java
@@ -0,0 +1,15 @@
+package school.faang.task_BJS2_46888;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@AllArgsConstructor
+@Getter
+@Setter
+@ToString
+public class Notification {
+    private String type;
+    private String message;
+}
---
+++ src/main/java/school/faang/task_BJS2_46888/NotificationManager.java
@@ -0,0 +1,25 @@
+package school.faang.task_BJS2_46888;
+
+
+
+import lombok.extern.slf4j.Slf4j;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.function.Consumer;
+
+@Slf4j
+public class NotificationManager {
+    private final Map<String, Consumer<Notification>> notifications = new HashMap<>();
+
+    public void registerHandler(String type, Consumer<Notification> consumer){
+        notifications.put(type, consumer);
+        log.info("The type has been added .");
+    }
+    public void sendNotification(Notification notification){
+        Consumer<Notification> typeNotification = notifications.get(notification.getType());
+        typeNotification.accept(notification);
+        log.info("The notification has been send .");
+    }
+
+}
