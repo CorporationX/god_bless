2aa01c93f1c044d68e913faaf66134a571d9be21
Alexey Gorokhov <almigor@mail.ru>
2024-12-11 00:29:48 +0300

    Оптимизируем работу дата-центра

--- src/main/java/school/faang/task45030/strategy/EnergyEfficiencyOptimizationStrategy.java
+++ src/main/java/school/faang/task45030/strategy/EnergyEfficiencyOptimizationStrategy.java
@@ -6,11 +6,9 @@ import school.faang.task45030.model.Server;
 public class EnergyEfficiencyOptimizationStrategy implements OptimizationStrategy {
     @Override
     public void optimize(DataCenter dataCenter) {
-        {
-            for (Server server : dataCenter.getServerList()) {
-                if (server.getLoad() == 0) {
-                    server.setEnergyConsumption(0);
-                }
+        for (Server server : dataCenter.getServerList()) {
+            if (server.getLoad() == 0) {
+                server.setEnergyConsumption(0);
             }
         }
     }
