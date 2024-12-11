63c5280fcccbb959fcdeb2afea1bf563c5877b00
Anton Graf <ant0ngraf@yandex.ru>
2024-12-11 09:04:01 +0600

    Добавляет класс Email и обработчика писем

---
+++ src/main/java/school/faang/bjs_45873/EmailProcessor.java
@@ -0,0 +1,26 @@
+package school.faang.bjs_45873;
+
+import java.util.List;
+import java.util.function.Consumer;
+import java.util.function.Function;
+import java.util.function.Predicate;
+
+public class EmailProcessor {
+    public List<Email> processEmails(List<Email> emails, Predicate<Email> filter, Function<Email, String> converter,
+                              Consumer<Email> processor) {
+        if (emails == null || filter == null || converter == null || processor == null) {
+            throw new IllegalArgumentException();
+        }
+
+        List<Email> convertedEmails = emails.stream()
+                .filter(filter)
+                .toList();
+
+        convertedEmails.forEach(email -> {
+            email.setBody(converter.apply(email));
+            processor.accept(email);
+        });
+
+        return convertedEmails;
+    }
+}
