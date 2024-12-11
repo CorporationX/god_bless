311b13cfcdbaf934fc0f901b33c0e49ccf0287ef
ArzamaS Ilya <coldoff@inbox.ru>
2024-12-11 10:49:40 +0300

    Created lyambda and test program.

--- src/main/java/school/faang/task_45541/SpellCaster.java
+++ src/main/java/school/faang/task_45541/SpellCaster.java
@@ -1,7 +1,8 @@
 package school.faang.task_45541;
 
-public class SpellCaster implements SpellAction{
+public class SpellCaster{
     public void cast(String spell, SpellAction action){
-
+        String result = action.act(spell);
+        System.out.println(result);
     }
 }
