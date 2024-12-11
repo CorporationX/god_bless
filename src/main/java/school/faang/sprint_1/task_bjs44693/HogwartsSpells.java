88ba038f81166bb478e33057b1b276840a8a3cc2
mixa <mikle.kuznecov@yandex.ru>
2024-12-11 23:51:13 +0300

    add delete spellsByType

--- src/main/java/school/faang/sprint_1/task_bjs44693/HogwartsSpells.java
+++ src/main/java/school/faang/sprint_1/task_bjs44693/HogwartsSpells.java
@@ -5,17 +5,22 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import lombok.Getter;
+import lombok.extern.slf4j.Slf4j;
 
 @Getter
+@Slf4j
 public class HogwartsSpells {
     private final Map<Integer, SpellEvent> spellById = new HashMap<>();
     private final Map<SpellType, List<SpellEvent>> spellsByType = new HashMap<>();
 
     public void addSpellEvent(SpellEvent spellEvent) {
-        spellById.put(spellEvent.getId(), spellEvent);
-        spellsByType.putIfAbsent(spellEvent.getSpellType(), new ArrayList<>());
-        spellsByType.get(spellEvent.getSpellType())
-            .add(spellEvent);
+        if (spellById.containsKey(spellEvent.getId())) {
+            log.info("spellEvent exist ID = {}", spellEvent.getId());
+        } else {
+            spellById.put(spellEvent.getId(), spellEvent);
+            spellsByType.putIfAbsent(spellEvent.getSpellType(), new ArrayList<>());
+            spellsByType.get(spellEvent.getSpellType()).add(spellEvent);
+        }
     }
 
     public SpellEvent getSpellEventById(int id) {
@@ -27,8 +32,14 @@ public class HogwartsSpells {
     }
 
     public void deleteSpellEvent(int id) {
-        spellsByType.remove(getSpellEventById(id).getSpellType());
-        spellById.remove(id);
+        SpellEvent removedSpell = spellById.remove(id);
+        if (removedSpell == null) {
+            log.info("SpellEvent not exist by Id= {}", id);
+        } else {
+            SpellType spellType = removedSpell.getSpellType();
+            List<SpellEvent> spellEventList = spellsByType.get(spellType);
+            spellEventList.remove(removedSpell);
+        }
     }
 
     public void printAllSpellEvents() {
--- src/main/java/school/faang/sprint_1/task_bjs44693/Main.java
+++ src/main/java/school/faang/sprint_1/task_bjs44693/Main.java
@@ -2,21 +2,21 @@ package school.faang.sprint_1.task_bjs44693;
 
 public class Main {
     public static void main(String[] args) {
-        SpellEvent spellEvent = new SpellEvent(1, SpellType.ATTACK);
-        SpellEvent spellEvent2 = new SpellEvent(2, SpellType.DEFENCE);
-        SpellEvent spellEvent3 = new SpellEvent(3, SpellType.DEFENCE);
+        SpellEvent spellEvent = new SpellEvent(1, SpellType.DEFENCE);
+        SpellEvent spellEvent2 = new SpellEvent(2, SpellType.ATTACK);
+        SpellEvent spellEvent3 = new SpellEvent(2, SpellType.DEFENCE);
 
         HogwartsSpells hogwartsSpells = new HogwartsSpells();
         hogwartsSpells.addSpellEvent(spellEvent);
         hogwartsSpells.addSpellEvent(spellEvent2);
         hogwartsSpells.addSpellEvent(spellEvent3);
-
-        hogwartsSpells.printAllSpellEvents();
-        System.out.println(hogwartsSpells.getSpellEventById(2));
-        System.out.println(hogwartsSpells.getSpellEventsByType(SpellType.ATTACK));
-
-        hogwartsSpells.deleteSpellEvent(2);
+        System.out.println("=======BEFORE DELETE=======");
         hogwartsSpells.printAllSpellEvents();
 
+        hogwartsSpells.deleteSpellEvent(4);
+        System.out.println();
+        System.out.println();
+        System.out.println("=======AFTER DELETE=======");
+        hogwartsSpells.printAllSpellEvents();
     }
 }
