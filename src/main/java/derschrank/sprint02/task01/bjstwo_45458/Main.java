b6cd61fe382612372b85eed16e06a4f057e125d2
Mipusatina <mipspam2@mail.ru>
2024-12-11 23:29:48 +0100

    Sprint02. Task01. SpellAction and SpellCaster. Fix Checkstyle"

--- src/main/java/derschrank/sprint02/task01/bjstwo_45458/Main.java
+++ src/main/java/derschrank/sprint02/task01/bjstwo_45458/Main.java
@@ -2,13 +2,13 @@ package derschrank.sprint02.task01.bjstwo_45458;
 
 public class Main {
     public static void main(String[] args) {
+        System.out.println("Do Magic:");
         SpellCaster spellCaster = new SpellCaster();
 
         String alohomora = "Alohomora";
         String lumos = "Lumos";
         String expelliarmus = "Expelliarmus";
 
-        System.out.println("Magic:");
         spellCaster.cast(alohomora, (spell) -> "The door is unlocked by " + spell);
         spellCaster.cast(lumos, (spell) -> "A beam of light is created by " + spell);
         spellCaster.cast(expelliarmus, (spell) -> "The opponent is disarmed by " + spell);
