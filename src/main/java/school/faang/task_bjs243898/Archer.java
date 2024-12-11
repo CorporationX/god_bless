5104110da81f7c70f70fecfa131aacc52c480381
Kir34 <kirnovikof1@gmail.com>
2024-12-12 01:31:06 +0300

    задание с наследованием и абстракцией

--- src/main/java/school/faang/task_bjs243898/Archer.java
+++ src/main/java/school/faang/task_bjs243898/Archer.java
@@ -3,9 +3,6 @@ package school.faang.task_bjs243898;
 public class Archer extends Character {
     public Archer(String name) {
         super(name, 3, 10, 5);
-        power = 3;
-        dexterity = 10;
-        intelligence = 5;
     }
 
     @Override
