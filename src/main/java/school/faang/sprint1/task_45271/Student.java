dd1c2fc3f7bbdcfe76efb81336cff3e506603490
Pavel Schelkin <rhanan1233@gmail.com>
2024-12-11 14:22:01 +0300

    Двойной кеш кеш

---
+++ src/main/java/school/faang/sprint1/task_45271/Student.java
@@ -0,0 +1,45 @@
+package school.faang.sprint1.task_45271;
+
+import java.util.Objects;
+
+public class Student {
+    private int id;
+    private String name;
+
+    public Student(int id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        Student student = (Student) o;
+        return id == student.id && Objects.equals(name, student.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, name);
+    }
+
+    @Override
+    public String toString() {
+        return "Student{id=" + id + ", name='" + name + "'}";
+    }
+}
+
+
