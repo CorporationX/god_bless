ce9eb49940e6df632e1942dec8d3f065a3a6f6f3
VladimirGontarenko <mister.gontarenko@yandex.ru>
2024-12-11 13:22:08 +0400

    BJS2-45558 Хогвардс

---
+++ src/main/java/school/faang/SpellCaster.java
@@ -0,0 +1,8 @@
+package school.faang;
+
+public class SpellCaster {
+    public void cast(String spellName, SpellAction action) {
+        String result = action.castSpell(spellName);
+        System.out.println("Результат заклинания : " + result);
+    }
+}
