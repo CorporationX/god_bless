48095bf6b3ef3cb1e3a3afc038ada87ca8726810
tatgricenko <tattgricenko@gmail.com>
2024-12-11 12:53:30 +0300

    Gmail Rich Filters

---
+++ src/main/java/school/faang/sprint_2/task_45853/Main.java
@@ -0,0 +1,35 @@
+package school.faang.sprint_2.task_45853;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.function.Consumer;
+import java.util.function.Function;
+import java.util.function.Predicate;
+
+public class Main {
+    public static void main(String[] args) {
+        EmailProcessor emailProcessor = new EmailProcessor();
+
+        // Список входящих писем
+        List<Email> emails = Arrays.asList(
+                new Email("Letter 1", "Text letter 1", false),
+                new Email("Letter 2", "Text letter 2", true),
+                new Email("Spam", "Text spam", false)
+        );
+
+        // Фильтр, который пропускает только важные письма
+        Predicate<Email> importantFilter = email -> email.isImportant();
+
+        // Преобразователь, который переводит текст письма в верхний регистр
+        Function<Email, String> toUpperCase = email -> email.getBody().toUpperCase();
+
+        // Обработчик, который выводит тему письма в консоль
+        Consumer<Email> printEmail = email -> System.out.println("Обработано письмо: " + email.getSubject());
+
+        // Обработка писем
+        emailProcessor.processEmails(emails, importantFilter, toUpperCase, printEmail);
+
+        // Выводим обновленные письма, чтобы убедиться, что изменения сохранились
+        emails.forEach(email -> System.out.println("Тема: " + email.getSubject() + ", Тело письма: " + email.getBody()));
+    }
+}
