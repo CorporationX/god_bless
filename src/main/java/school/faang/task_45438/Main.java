21a05de71f5695f5473ea03b59f9af838bedb192
Dmitriy <dda20040609@gmail.com>
2024-12-11 13:04:25 +0300

    commit 2 Хогвартс

--- src/main/java/school/faang/task_45438/Main.java
+++ src/main/java/school/faang/task_45438/Main.java
@@ -7,5 +7,9 @@ public class Main {
         spellCaster.cast(Spell.ALOHOMORA, (spell) -> "The door is unlocked by " + spell);
         spellCaster.cast(Spell.LUMOS, (spell) -> "A beam of light is created by " + spell);
         spellCaster.cast(Spell.EXPELLIARMUS, (spell) -> "The opponent is disarmed by " + spell);
+        spellCaster.cast(Spell.LUMOS, null);
+        spellCaster.cast(Spell.EXPELLIARMUS, (spell) -> {
+            throw new RuntimeException("Casting failed");
+        });
     }
 }
--- src/main/java/school/faang/task_45438/SpellCaster.java
+++ src/main/java/school/faang/task_45438/SpellCaster.java
@@ -2,7 +2,11 @@ package school.faang.task_45438;
 
 public class SpellCaster {
     public void cast(Spell spell, SpellAction action) {
-        String result = action.cast(spell);
-        System.out.println(result);
+        try {
+            String result = action.cast(spell);
+            System.out.println(result);
+        } catch (Exception e) {
+            System.err.println(e.getMessage());
+        }
     }
 }
