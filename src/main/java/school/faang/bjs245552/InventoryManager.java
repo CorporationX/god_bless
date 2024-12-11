d07d138ebcbd1d73625293674f455ebe162eb693
Leonid Zhuravel <zhuravel_leonid@yahoo.com>
2024-12-11 11:44:43 +0200

    before PR with commented out other implementation sample

--- src/main/java/school/faang/bjs245552/InventoryManager.java
+++ src/main/java/school/faang/bjs245552/InventoryManager.java
@@ -1,18 +1,71 @@
 package school.faang.bjs245552;
 
+import lombok.Getter;
+
+import java.util.HashMap;
+import java.util.Map;
 import java.util.function.Consumer;
 import java.util.function.Function;
+import java.util.function.Predicate;
 
+@Getter
 public class InventoryManager {
+    private final Map<String, Character> characterMap;
+
+    public InventoryManager() {
+        characterMap = new HashMap<>();
+    }
+
     public void addItem(Character character, Item item, Consumer<Item> consumer) {
+        if (character == null) {
+            throw new IllegalArgumentException("Character was not specified in request");
+        }
+        if (item == null) {
+            throw new IllegalArgumentException("Invalid item operation specified for character inventory");
+        }
+        if (consumer == null) {
+            throw new IllegalArgumentException("Invalid item operation specified for character inventory");
+        }
 
+        character.getInventory().add(item);
+        consumer.accept(item);
     }
 
-    public void removeItem(Character character, Consumer<Item> consumer) {
+    public void removeItem(Character character, Predicate<Item> predicate) {
+        if (character == null) {
+            throw new IllegalArgumentException("Character was not specified in request");
+        }
+        if (predicate == null) {
+            throw new IllegalArgumentException("Invalid item check operation specified for character inventory");
+        }
 
+        boolean removed = character.getInventory().removeIf(predicate);
+        if (!removed) {
+            throw new IllegalArgumentException("Item was not found in character's inventory");
+        } else {
+            System.out.println("Item was removed from character's inventory");
+        }
     }
 
-    public void updateItem(Character character, Consumer<Item> consumer, Function<Item, Item> function) {
+    public void updateItem(Character character, Predicate<Item> predicate, Function<Item, Item> function) {
+        if (character == null) {
+            throw new IllegalArgumentException("Character was not specified in request");
+        }
+        if (predicate == null) {
+            throw new IllegalArgumentException("Invalid item check operation specified for character inventory");
+        }
+        if (function == null) {
+            throw new IllegalArgumentException("Invalid item update operation specified for character inventory");
+        }
 
+        for (int i = 0; i < character.getInventory().size(); i++) {
+            Item item = character.getInventory().get(i);
+            if (predicate.test(item)) {
+                character.getInventory().set(i, function.apply(item));
+            }
+        }
+
+        //not as effective as updates whole the list
+        //character.getInventory().replaceAll(item -> predicate.test(item) ? function.apply(item) : item);
     }
 }
--- src/main/java/school/faang/bjs245552/Main.java
+++ src/main/java/school/faang/bjs245552/Main.java
@@ -2,6 +2,7 @@ package school.faang.bjs245552;
 
 public class Main {
     public static void main(String[] args) {
-
+        SampleData sampleData = new SampleData();
+        sampleData.test();
     }
 }
