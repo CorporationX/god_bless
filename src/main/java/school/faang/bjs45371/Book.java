ecb3ddbdfaeff7bf9aa8ab460ffc99e049ccb78c
Arseni Zaytsau <zarseniy280@gmail.com>
2024-12-11 15:46:08 +0300

    ztsv-BJS2-45371 - created LibrarySystem + Book classes according to task, tested in Main

---
+++ src/main/java/school/faang/bjs45371/Book.java
@@ -0,0 +1,30 @@
+package school.faang.bjs45371;
+
+import lombok.AllArgsConstructor;
+import lombok.ToString;
+
+@AllArgsConstructor
+@ToString
+public class Book {
+    private String title;
+    private String author;
+    private int year;
+
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Book book = (Book) o;
+        return year == book.year && title.equals(book.title) && author.equals(book.author);
+    }
+
+    @Override
+    public int hashCode() {
+        int result = title.hashCode();
+        result = 31 * result + author.hashCode();
+        result = 31 * result + year;
+        return result;
+    }
+}
