5f9967e96c0782f4ba1be09d10751b7ccce5c65d
Мария Борцова <bortsovamm@intech.rshb.ru>
2024-12-11 12:19:58 +0300

    refactor after review

--- src/main/java/school/faang/task_46158/ErrorHandler.java
+++ src/main/java/school/faang/task_46158/ErrorHandler.java
@@ -1,14 +1,13 @@
 package school.faang.task_46158;
 
 import java.util.concurrent.Callable;
-import java.util.function.Function;
 
 public class ErrorHandler {
-    public static <T> T withErrorHandling(Callable<T> action, Function<Exception, T> errorHandler) {
+    public static <T> T withErrorHandling(Callable<T> action, ExceptionHandler<T> errorHandler) {
         try {
             return action.call();
         } catch (Exception e) {
-            return errorHandler.apply(e);
+            return errorHandler.handle(e);
         }
     }
 }
--- src/main/java/school/faang/task_46158/Main.java
+++ src/main/java/school/faang/task_46158/Main.java
@@ -2,23 +2,36 @@ package school.faang.task_46158;
 
 public class Main {
     public static void main(String[] args) {
+        ExceptionHandler<String> exceptionHandler = e -> {
+            System.out.println("Ошибка при вызове сервиса, возвращаем дефолтное значение");
+            return "DEFAULT";
+        };
+
         // Пример с вызовом удалённого сервиса
-        String result = ErrorHandler.withErrorHandling(
-                () -> RemoteService.call("someParam"),  // Основное действие
-                e -> {  // Обработка ошибки
-                    System.out.println("Ошибка при вызове сервиса, возвращаем дефолтное значение");
-                    return "DEFAULT";
-                }
+        String errorResult = ErrorHandler.withErrorHandling(
+                () -> RemoteService.callWithException("someParam"),  // Основное действие
+                exceptionHandler
         );
 
-        System.out.println(result);  // Выведет результат: "DEFAULT" при ошибке
+        System.out.println(errorResult);
+
+        String successResult = ErrorHandler.withErrorHandling(
+                () -> RemoteService.callSuccess("Ho-ho-ho"),
+                exceptionHandler
+        );
+
+        System.out.println(successResult);
     }
 
     // Пример метода для удалённого сервиса
     public static class RemoteService {
-        public static String call(String param) throws Exception {
+        public static String callWithException(String param) throws Exception {
             // Здесь возникнет ошибка для демонстрации
             throw new Exception("Сервис недоступен");
+        }
+
+        public static String callSuccess(String param) {
+            return "Call success with param = " + param;
         }
     }
 }
