491127b802ab5603377e05dac73bda0e81f0b7ac
sergeybabaev96 <166114022+sergeybabaev96@users.noreply.github.com>
2024-12-11 23:17:54 +0300

    Задача двойной кэш-кэш

---
+++ src/main/java/school/faang/bjs245251/StudentDataBase.java
@@ -0,0 +1,71 @@
+package school.faang.bjs245251;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+
+public class StudentDataBase {
+    private HashMap<Student, Map<Subject, Integer>> students = new HashMap<>();
+    private HashMap<Subject, List<Student>> subjectsStudents = new HashMap<>();
+
+    public void addStudentWithGrades(Student student, Map<Subject, Integer> studentGrade) {
+        if (student == null || studentGrade == null) {
+            throw new IllegalArgumentException("Student or students grade can not be null");
+        }
+        students.put(student, studentGrade);
+        for (Subject subject : studentGrade.keySet()) {
+            subjectsStudents.computeIfAbsent(subject, subject1 -> new ArrayList<>()).add(student);
+        }
+    }
+
+    public void addSubjectForStudent(Student student, Subject subject, int grade) {
+        Map<Subject, Integer> grades = students.get(student);
+        if (grades != null) {
+            grades.put(subject, grade);
+            subjectsStudents.computeIfAbsent(subject, subject1 -> new ArrayList<>()).add(student);
+        }
+    }
+
+    public void removeStudent(Student student) {
+        Map<Subject, Integer> studentGrade = students.remove(student);
+        if (studentGrade != null) {
+            for (Subject subject : subjectsStudents.keySet()) {
+                List<Student> studentList = subjectsStudents.get(student);
+                if (studentList != null) {
+                    studentList.remove(student);
+                }
+            }
+        }
+    }
+
+    public void printAllSubjectsWithStudents() {
+        subjectsStudents.forEach((subject, students1) -> System.out.println(subject + " : " + students1));
+    }
+
+    public void addSubjectWithStudents(Subject subject, List<Student> studentsList) {
+        subjectsStudents.put(subject, studentsList);
+        for (Student student : studentsList) {
+            students.putIfAbsent(student, new HashMap<>());
+            students.get(student).put(subject, null);
+        }
+    }
+
+    public void addStudentToSubject(Student student, Subject subject) {
+        subjectsStudents.putIfAbsent(subject, new ArrayList<>());
+        subjectsStudents.get(subject).add(student);
+        students.putIfAbsent(student, new HashMap<>());
+        students.get(student).put(subject, null);
+    }
+
+    public void removeStudentFromSubject(Student student, Subject subject) {
+        subjectsStudents.getOrDefault(subject, new ArrayList<>())
+                .removeIf(currentStudent -> currentStudent.equals(student));
+    }
+
+    public void printAllStudents() {
+        students.forEach((student, subjectIntegerMap) ->
+                System.out.println(student + " : " + subjectIntegerMap));
+    }
+}
