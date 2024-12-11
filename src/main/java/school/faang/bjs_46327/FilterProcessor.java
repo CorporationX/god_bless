7d3d9f7bfdd5c2983ace8919516dd18ee513c051
Anton Graf <ant0ngraf@yandex.ru>
2024-12-11 07:31:02 +0600

    Добавляет тесты системы фильтрации

--- src/main/java/school/faang/bjs_46327/FilterProcessor.java
+++ src/main/java/school/faang/bjs_46327/FilterProcessor.java
@@ -14,7 +14,7 @@ public class FilterProcessor {
         return filter.apply(image);
     }
 
-    public Function<Image,Image> combineFilters(Function<Image, Image> filter1, Function<Image, Image> filter2) {
+    public Function<Image, Image> combineFilters(Function<Image, Image> filter1, Function<Image, Image> filter2) {
         if (filter1 == null || filter2 == null) {
             throw new IllegalArgumentException("filter is null");
         }
