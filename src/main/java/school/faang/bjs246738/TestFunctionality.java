50fb3f89b06992e271ba779aac4103e3fca8a8a9
Leonid Zhuravel <zhuravel_leonid@yahoo.com>
2024-12-11 13:23:39 +0200

    PR ready single solution with checkstyle pass

--- src/main/java/school/faang/bjs246738/TestFunctionality.java
+++ src/main/java/school/faang/bjs246738/TestFunctionality.java
@@ -5,20 +5,26 @@ public class TestFunctionality {
         NotificationManager notificationManager = new NotificationManager();
 
         notificationManager.registerHandler(Enums.NotificationType.EMAIL,
-                (notification) -> System.out.println("Отправка по электронной почте: " + notification.getMessage())
+                (notification) -> System.out.println("Отправка по электронной почте: "
+                        + notification.getMessage())
         );
 
         notificationManager.registerHandler(Enums.NotificationType.SMS,
-                (notification) -> System.out.println("Отправка SMS: " + notification.getMessage())
+                (notification) -> System.out.println("Отправка SMS: "
+                        + notification.getMessage())
         );
 
         notificationManager.registerHandler(Enums.NotificationType.PUSH,
-                (notification) -> System.out.println("Отправка push-уведомления: " + notification.getMessage())
+                (notification) -> System.out.println("Отправка push-уведомления: "
+                        + notification.getMessage())
         );
 
-        Notification emailNotification = new Notification(Enums.NotificationType.EMAIL, "Ваша учетная запись успешно активирована");
-        Notification smsNotification = new Notification(Enums.NotificationType.SMS, "Вы успешно изменили свой пароль");
-        Notification pushNotification = new Notification(Enums.NotificationType.PUSH, "Новый пост от пользователя: JohnDoe");
+        Notification emailNotification = new Notification(Enums.NotificationType.EMAIL,
+                "Ваша учетная запись успешно активирована");
+        Notification smsNotification = new Notification(Enums.NotificationType.SMS,
+                "Вы успешно изменили свой пароль");
+        Notification pushNotification = new Notification(Enums.NotificationType.PUSH,
+                "Новый пост от пользователя: JohnDoe");
 
         notificationManager.sendNotification(emailNotification);
         notificationManager.sendNotification(smsNotification);
