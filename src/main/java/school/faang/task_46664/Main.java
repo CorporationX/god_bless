08a4fdd1e41eb8f95b4a277baf6e32d98ac05117
Mihail Ermolaev <fitis_rus21@mail.ru>
2024-12-11 20:03:46 +0300

    chekstyle

--- src/main/java/school/faang/task_46664/Main.java
+++ src/main/java/school/faang/task_46664/Main.java
@@ -8,23 +8,19 @@ public class Main {
 
         FilterProcessor filterProcessor = new FilterProcessor();
 
-// Фильтры
         Function<Image, Image> grayscaleFilter = (image) -> new Image(image.getName(), image.getDescription()
                 + " | Фильтр: черно-белый");
         Function<Image, Image> sepiaFilter = (image) -> new Image(image.getName(), image.getDescription()
                 + " | Фильтр: сепия");
 
-// Применение фильтров
         Image grayscaleImage = filterProcessor.applyFilter(originalImage, grayscaleFilter);
         System.out.println(grayscaleImage.getDescription());
 
         Image sepiaImage = filterProcessor.applyFilter(grayscaleImage, sepiaFilter);
         System.out.println(sepiaImage.getDescription());
 
-// Комбинирование фильтров
         Function<Image, Image> combinedFilter = filterProcessor.combineFilters(grayscaleFilter, sepiaFilter);
         Image combinedImage = filterProcessor.applyFilter(originalImage, combinedFilter);
         System.out.println(combinedImage.getDescription());
-
     }
 }
