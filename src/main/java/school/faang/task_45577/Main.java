21c705a89bc89734703d4767a2a3e9d7eec8f238
triplek <weribel@mail.ru>
2024-12-11 22:40:06 +0300

    Gmail Rich Filters

---
+++ src/main/java/school/faang/task_45577/Main.java
@@ -0,0 +1,22 @@
+package school.faang.task_45577;
+
+public class Main {
+    public static void main(String[] args) {
+        Character frodo = new Character("Frodo");
+        Item ring = new Item("The One Ring", 1000);
+
+        InventoryManager manager = new InventoryManager();
+
+        manager.addItem(frodo, ring, item -> System.out.println(item.getName()
+                + " успешно был добавлен в иневтарь персонажа"));
+
+        manager.removeItem(frodo, item -> item.getName().equals("The One Ring"));
+
+        manager.addItem(frodo, ring, (item) -> System.out.println(item.getName() + " снова добавлен."));
+
+        manager.updateItem(frodo, (item) -> item.getName().equals("The One Ring"), (item) -> new Item(item.getName(), item.getValue() * 2));
+
+        // Проверяем результат
+        frodo.getInventory().forEach(item -> System.out.println(item.getName() + ": " + item.getValue()));
+    }
+}
