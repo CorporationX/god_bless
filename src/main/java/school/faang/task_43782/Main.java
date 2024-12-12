01bd801378d669bd9f1e71f3156aefb6e72f511d
Sergey Chirin <zopks2@gmail.com>
2024-12-13 00:27:01 +0300

    added Exception output

--- src/main/java/school/faang/task_43782/Main.java
+++ src/main/java/school/faang/task_43782/Main.java
@@ -2,12 +2,25 @@ package school.faang.task_43782;
 
 public class Main {
     public static void main(String[] args) {
-        /*
-        User user1 = new User("Ivan", 17, "Google", "London");
-        User user2 = new User("Ivan", 18, "Yandex", "London");
-        User user3 = new User("Ivan", 18, "Google", "Moscow");
-        */
-        User user4 = new User("Ivan", 40, "Google", "London");
+        try {
+            User userWithExceptionAge = new User("Sergey", 17, "Google", "London");
+        } catch (IllegalArgumentException exception) {
+            System.out.println(exception + " - this exception was found");
+        }
+
+        try {
+            User userWithExceptionWorkplace = new User("Sergey", 18, "Yandex", "London");
+        } catch (IllegalArgumentException exception) {
+            System.out.println(exception + " - this exception was found");
+        }
+
+        try {
+            User userWithExceptionAddress = new User("Sergey", 17, "Google", "Moscow");
+        } catch (IllegalArgumentException exception) {
+            System.out.println(exception + " - this exception was found");
+        }
+
+        User userWithoutException = new User("Ivan", 40, "Google", "London");
 
     }
 }
