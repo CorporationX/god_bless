8392bb71a15cc347b6ca25f364e7d79e63e1f3d4
sanctusgermanus72362 <sanctus-germanus7@yandex.ru>
2024-12-12 19:15:32 +0300

    Исправления

--- src/main/java/ru/maks1979/task_46776/Main.java
+++ src/main/java/ru/maks1979/task_46776/Main.java
@@ -3,11 +3,14 @@ package ru.maks1979.task_46776;
 public class Main {
     public static void main(String[] args) {
         NotificationManager notificationManager = new NotificationManager();
-        notificationManager.registerHandler("discord", (notification) -> System.out.println("Sending discord-message" +
-                notification.getMessage()));
-        notificationManager.registerHandler("telegram", (notification) -> System.out.println("Sending telegram-message" +
-                notification.getMessage()));
-        notificationManager.registerHandler("watsapp", (notification) -> System.out.println("Sending watsapp-message" +
-                notification.getMessage()));
+        notificationManager.registerHandler("discord", (notification)
+                -> System.out.println("Sending discord-message"
+                + notification.getMessage()));
+        notificationManager.registerHandler("telegram", (notification)
+                -> System.out.println("Sending telegram-message"
+                + notification.getMessage()));
+        notificationManager.registerHandler("watsapp", (notification)
+                -> System.out.println("Sending watsapp-message"
+                + notification.getMessage()));
     }
 }
