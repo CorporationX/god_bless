6603862943c54f89b8e3b4355adfbd8125fab7f0
Ivan Baev <ibai86@yandex.ru>
2024-12-12 04:37:47 +0300

    in process

--- src/main/java/school/faang/lordof_therings/InventoryManager.java
+++ src/main/java/school/faang/lordof_therings/InventoryManager.java
@@ -5,7 +5,7 @@ import school.faang.meta_universe.Consumer;
 import java.util.List;
 import java.util.function.Function;
 import java.util.function.Predicate;
-import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class InventoryManager {
 
