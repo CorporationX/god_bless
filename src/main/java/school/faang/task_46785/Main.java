508a704fccba38ad75fc246bff4b0afd01cc0172
NiveshOne <Shishov.44@mail.ru>
2024-12-12 21:58:13 +0300

    Реализован enum NotificationType Добавлены проверки на null параметров методов registerHandler и sendNotification

--- src/main/java/school/faang/task_46785/Main.java
+++ src/main/java/school/faang/task_46785/Main.java
@@ -7,17 +7,18 @@ public class Main {
         NotificationManager notificationManager = new NotificationManager();
 
         // Регистрация обработчиков оповещений
-        notificationManager.registerHandler("email",
+        notificationManager.registerHandler(NotificationType.EMAIL.getNonificationType(),
                 (notification) -> System.out.println("Отправка по электронной почте: " + notification.getMessage())
         );
 
-        notificationManager.registerHandler("sms",
+        notificationManager.registerHandler(NotificationType.SMS.getNonificationType(),
                 (notification) -> System.out.println("Отправка SMS: " + notification.getMessage())
         );
 
-        notificationManager.registerHandler("push",
+        notificationManager.registerHandler(NotificationType.PUSH.getNonificationType(),
                 (notification) -> System.out.println("Отправка push-уведомления: " + notification.getMessage())
         );
+        notificationManager.registerHandler(null, null);
 
         Function<Notification, Notification> modifier = n -> {
             String newMessage = n.getMessage().replace("плохой", "самый лучший");
@@ -27,11 +28,16 @@ public class Main {
 
 
         // Отправка оповещений
-        Notification emailNotification = new Notification("email", "Ваша учетная запись успешно активирована");
-        Notification smsNotification = new Notification("sms", "Вы успешно изменили свой пароль");
-        Notification pushNotification = new Notification("push", "Новый пост от пользователя: JohnDoe");
-        Notification noNameNotification = new Notification("noName", "Новый пост от пользователя: JohnDoe");
-        Notification modifierNotification = new Notification("push", "Java плохой язык!!!");
+        Notification emailNotification = new Notification(NotificationType.EMAIL.getNonificationType(),
+                "Ваша учетная запись успешно активирована");
+        Notification smsNotification = new Notification(NotificationType.SMS.getNonificationType(),
+                "Вы успешно изменили свой пароль");
+        Notification pushNotification = new Notification(NotificationType.PUSH.getNonificationType(),
+                "Новый пост от пользователя: JohnDoe");
+        Notification noNameNotification = new Notification("No name",
+                "Новый пост от пользователя: JohnDoe");
+        Notification modifierNotification = new Notification(NotificationType.PUSH.getNonificationType(),
+                "Java плохой язык!!!");
 
         notificationManager.sendNotification(emailNotification, modifier);
         notificationManager.sendNotification(smsNotification, modifier);
--- src/main/java/school/faang/task_46785/NotificationManager.java
+++ src/main/java/school/faang/task_46785/NotificationManager.java
@@ -9,17 +9,24 @@ public class NotificationManager {
     private final Map<String, Consumer<Notification>> consumerMap = new HashMap<>();
 
     public void registerHandler(String notification, Consumer<Notification> consumer) {
-        consumerMap.put(notification, consumer);
+        if ((notification != null) && (consumer != null)) {
+            consumerMap.put(notification, consumer);
+        }
     }
 
     public void sendNotification(Notification notification, Function<Notification, Notification> modifier) {
-        Notification modifierNotification = modifier.apply(notification);
+        if ((notification != null) && modifier != null) {
+            Notification modifierNotification = modifier.apply(notification);
 
-        Consumer<Notification> consumer = consumerMap.get(modifierNotification.getType());
-        if (consumer != null) {
-            consumer.accept(modifierNotification);
+            Consumer<Notification> consumer = consumerMap.get(modifierNotification.getType());
+            if (consumer != null) {
+                consumer.accept(modifierNotification);
+            } else {
+                System.out.println("Нет обработчика для типа оповещения: " + modifierNotification.getType());
+            }
         } else {
-            System.out.println("Нет обработчика для типа оповещения: " + modifierNotification.getType());
+            System.out.println("Данные введены некорректно!!!");
         }
+
     }
 }
