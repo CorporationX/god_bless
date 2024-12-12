3b9408afeb70a945cb6c7c7b00bcef3becd069b7
etibarliparvin <73303441+etibarliparvin@users.noreply.github.com>
2024-12-12 19:30:44 +0400

    bjs2-47079 (#15140)
    * bjs2-47079
    
    * Анализ активности пользователей в социальной сети is finished
    
    * bjs2-47079
    
    * Анализ активности пользователей в социальной сети is finished

---
+++ src/main/java/school/faang/bjs247547/ActionType.java
@@ -0,0 +1,10 @@
+package school.faang.bjs247547;
+
+public enum ActionType {
+
+    POST,
+    COMMENT,
+    LIKE,
+    SHARE
+
+}
---
+++ src/main/java/school/faang/bjs247547/UserActionAnalyzer.java
@@ -0,0 +1,67 @@
+package school.faang.bjs247547;
+
+import java.time.LocalDate;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
+import java.util.function.Predicate;
+import java.util.stream.Collectors;
+
+import static school.faang.bjs247547.ActionType.COMMENT;
+import static school.faang.bjs247547.ActionType.POST;
+
+public class UserActionAnalyzer {
+
+    public static List<String> topActiveUsers(List<UserAction> actions) {
+        return actions.stream()
+                .collect(Collectors.groupingBy(UserAction::getName, Collectors.counting()))
+                .entrySet().stream()
+                .sorted(Map.Entry.<String, Long>comparingByValue().reversed())
+                .limit(10)
+                .map(Map.Entry::getKey)
+                .toList();
+    }
+
+    public static List<String> topPopularHashtags(List<UserAction> actions) {
+        return actions.stream()
+                .filter(predicate)
+                .flatMap(el -> Arrays.stream(el.getContent().split("\\s+")))
+                .filter(el -> el.startsWith("#"))
+                .collect(Collectors.groupingBy(el -> el, Collectors.counting()))
+                .entrySet()
+                .stream()
+                .sorted(Map.Entry.<String, Long>comparingByValue().reversed())
+                .limit(5)
+                .map(Map.Entry::getKey)
+                .toList();
+    }
+
+    public static List<String> topCommentersLastMonth(List<UserAction> actions) {
+        return actions.stream()
+                .filter(el -> el.getActionType() == COMMENT
+                        && el.getActionDate().isAfter(LocalDate.now().minusMonths(1))
+                        && el.getActionDate().isBefore(LocalDate.now()))
+                .collect(Collectors.groupingBy(UserAction::getName, Collectors.counting()))
+                .entrySet().stream()
+                .sorted(Map.Entry.<String, Long>comparingByValue().reversed())
+                .limit(3)
+                .map(Map.Entry::getKey)
+                .toList();
+    }
+
+    public static Map<String, Double> actionTypePercentages(List<UserAction> actions) {
+        return actions.stream()
+                .collect(Collectors.groupingBy(UserAction::getActionType, Collectors.counting()))
+                .entrySet()
+                .stream()
+                .collect(Collectors.toMap(
+                        el -> el.getKey().name(),
+                        el -> (double) (el.getValue() * 100) / actions.size()
+                ));
+    }
+
+    private static final Predicate<UserAction> predicate = userAction -> userAction != null
+            && (userAction.getActionType() == POST
+            || userAction.getActionType() == COMMENT);
+
+}
