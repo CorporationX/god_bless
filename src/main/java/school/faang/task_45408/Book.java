46410f1c7d9560b34ac1bcca4ce003506dbd6f48
Sergey Chirin <zopks2@gmail.com>
2024-12-13 01:47:49 +0300

    fixed &&, added them after split at new line

--- src/main/java/school/faang/task_45408/Book.java
+++ src/main/java/school/faang/task_45408/Book.java
@@ -24,8 +24,8 @@ public class Book {
             return false;
         }
         Book book = (Book) o;
-        return Objects.equals(title, book.title) && Objects.equals(author, book.author) &&
-                Objects.equals(year, book.year);
+        return Objects.equals(title, book.title) && Objects.equals(author, book.author)
+                && Objects.equals(year, book.year);
     }
 
     @Override
