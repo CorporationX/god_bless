273abe71d13727a616551d1c3c3c1e88e8ea1bc2
Mihail Ermolaev <fitis_rus21@mail.ru>
2024-12-12 11:57:27 +0300

    наименование объектов приведены к говорящему названию

--- src/main/java/school/faang/task_46664/FilterProcessor.java
+++ src/main/java/school/faang/task_46664/FilterProcessor.java
@@ -4,11 +4,11 @@ import java.util.function.Function;
 
 public class FilterProcessor {
 
-    public final Image applyFilter(Image image, Function<Image, Image> f) {
-        return f.apply(image);
+    public final Image applyFilter(Image image, Function<Image, Image> filter) {
+        return filter.apply(image);
     }
 
-    public Function<Image, Image> combineFilters(Function<Image, Image> f1, Function<Image, Image> f2) {
-        return f1.andThen(f2);
+    public Function<Image, Image> combineFilters(Function<Image, Image> filter1, Function<Image, Image> filter2) {
+        return filter1.andThen(filter2);
     }
 }
--- src/main/java/school/faang/task_46664/Image.java
+++ src/main/java/school/faang/task_46664/Image.java
@@ -2,12 +2,10 @@ package school.faang.task_46664;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
-import lombok.NoArgsConstructor;
 
 @Getter
 @AllArgsConstructor
-@NoArgsConstructor
 public class Image {
-    private String name;
-    private String description;
+    private final String name;
+    private final String description;
 }
