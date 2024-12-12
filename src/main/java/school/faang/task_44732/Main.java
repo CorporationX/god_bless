dccdfb89949a07662999c5ffa93f393c93e8e9e2
ANaydyonov <alexander.naydenov@23software.net>
2024-12-12 03:52:37 +0100

    remove unused method

--- src/main/java/school/faang/task_44732/Main.java
+++ src/main/java/school/faang/task_44732/Main.java
@@ -1,12 +1,9 @@
 package school.faang.task_44732;
 
-import com.github.javafaker.Faker;
-
 import java.util.Set;
 
 public class Main {
     public static void main(String[] args) {
-        Faker faker = new Faker();
         BookingSystem system = new BookingSystem();
 
         system.addObserver(new BookingObserverImpl());
