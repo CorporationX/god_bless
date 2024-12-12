420211bb0e28431380b0a9de8b777c9a3a341e0c
romenskd <romenskyid@gmail.com>
2024-12-12 16:38:20 +0100

    S2 фильтры инстаграм

---
+++ src/main/java/school/faang/task_46394/FilterProcessor.java
@@ -0,0 +1,13 @@
+package school.faang.task_46394;
+
+import java.util.function.Function;
+
+public class FilterProcessor {
+    public Image applyFilter(Image image, Function<Image, Image> filter) {
+        return filter.apply(image);
+    }
+
+    public Function<Image, Image> combineFilters(Function<Image, Image> f1, Function<Image, Image> f2) {
+        return f1.andThen(f2);
+    }
+}
