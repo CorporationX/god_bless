8cc2d8837532c1f8f6df3d8d4e4ea0a44e5b4f4e
EugeneT1024 <EugeneT1024@yandex.ru>
2024-12-12 21:25:49 +0300

    [BJS2-45679] Обновил по замечаниям

--- src/main/java/school/faang/sprint_2/task_bjs245426/DictionaryProcessor.java
+++ src/main/java/school/faang/sprint_2/task_bjs245426/DictionaryProcessor.java
@@ -3,7 +3,7 @@ package school.faang.sprint_2.task_bjs245426;
 import java.util.function.BiConsumer;
 
 public class DictionaryProcessor {
-    void processWord(String word, String translation, BiConsumer<String, String> consumer) {
+    public void processWord(String word, String translation, BiConsumer<String, String> consumer) {
         consumer.accept(word, translation);
     }
 }
