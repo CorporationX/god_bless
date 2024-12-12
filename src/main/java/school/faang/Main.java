b27d71ac75ec2277d859f4b913ac8e3734c6245e
Roman Shevchuk <reacount1393@gmail.com>
2024-12-12 15:07:36 +0300

    Создание юзеров работает, валидация по полям работает

--- src/main/java/school/faang/Main.java
+++ src/main/java/school/faang/Main.java
@@ -1,4 +1,18 @@
 package school.faang;
 
 public class Main {
+    public static void main(String[] args) {
+        try {
+            User user1 = new User("John", 20, "Google", "London");
+            System.out.println(user1);
+        } catch (IllegalArgumentException e) {
+            System.out.println(e.getMessage());
+        }
+        try {
+            User user2 = new User("Jack", 17, "Facebook", "Moscow");
+            System.out.println(user2);
+        } catch (IllegalArgumentException e) {
+            System.out.println(e.getMessage());
+        }
+    }
 }
