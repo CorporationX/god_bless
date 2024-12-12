f4812631b4cf4f7c0367fc71396bdc6e7bdf6e62
Stepan Evangulov <stevangulov@gmail.com>
2024-12-12 12:42:04 +0200

    practice Stream API 1

--- src/main/java/school/faang/task_46829/Main.java
+++ src/main/java/school/faang/task_46829/Main.java
@@ -5,7 +5,7 @@ import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
-        List<Integer> numbers = Arrays.asList(1, 2, 3, 4, 5, 6);
+        List<Integer> numbers = Arrays.asList(1, 2, 3, 4, 5, 7);
         List<String> strings = Arrays.asList("apple", "banana", "cherry", "date");
 
         System.out.println("Сумма четных чисел: " + ListOperations.sumOfEvenNumbers(numbers));
