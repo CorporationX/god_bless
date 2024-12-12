d77d1bd9b04e62d774e4346b8f8eb5e226c3ae70
maks <fhbfds@fb>
2024-12-12 17:38:02 +0300

    BJS2-46500 Фильтры Instagram Переименовал объект Image images на Image image

--- src/main/java/school/faang/task_46500/Main.java
+++ src/main/java/school/faang/task_46500/Main.java
@@ -5,23 +5,23 @@ import java.util.function.Function;
 public class Main {
     public static void main(String[] args) {
 
-        Image images = new Image("Summer", "Original Image");
+        Image image = new Image("Summer", "Original Image");
         FilterProcessor filterProcessor = new FilterProcessor();
-        System.out.println(images);
+        System.out.println(image);
 
-        Function<Image, Image> blackWhiteFilter = (image) ->
+        Function<Image, Image> blackWhiteFilter = (inputImage) ->
                 new Image(image.getName(), image.getDescription() + " | Фильтр: черно-белый");
-        Function<Image, Image> sepiaFilter = (image) ->
+        Function<Image, Image> sepiaFilter = (inputImage) ->
                 new Image(image.getName(), image.getDescription() + " | Фильтр: сепия");
 
-        Image originalImage = filterProcessor.applyFilter(images, blackWhiteFilter);
+        Image originalImage = filterProcessor.applyFilter(image, blackWhiteFilter);
         System.out.println(originalImage.getDescription());
 
         Image sepiaImage = filterProcessor.applyFilter(originalImage, sepiaFilter);
         System.out.println(sepiaImage.getDescription());
 
         Function<Image, Image> combinedFilter = filterProcessor.combineFilters(blackWhiteFilter, sepiaFilter);
-        Image combinedImage = filterProcessor.applyFilter(images, combinedFilter);
+        Image combinedImage = filterProcessor.applyFilter(image, combinedFilter);
         System.out.println(combinedImage.getDescription());
 
     }
