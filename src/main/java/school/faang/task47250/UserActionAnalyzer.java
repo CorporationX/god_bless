968f74c410a76cbf846204d4ecf2d6fa39b3bdd8
lehaps <awda.96@bk.ru>
2024-12-12 13:53:36 +0300

    Сделал в отдельные константы

--- src/main/java/school/faang/task47250/UserActionAnalyzer.java
+++ src/main/java/school/faang/task47250/UserActionAnalyzer.java
@@ -7,26 +7,32 @@ import java.util.stream.Collectors;
 
 public class UserActionAnalyzer {
 
-    private static void validate(List<UserAction> actions) {
+    private static final int TOP_ACTIVE_USERS = 10;
+    private static final int TOP_POPULAR_TOPICS = 5;
+    private static final int TOP_USERS_MOST_COMMENTS = 3;
+    private static final Pattern HASHTAG_SEARH_PATTERN = Pattern.compile("#\\w+.");
+    private static final String REGEX_REMOVING_PUNCTUATION = "[.,;!?]*$";
+
+    private static void validateActions(List<UserAction> actions) {
         if (actions == null) {
             throw new IllegalArgumentException("actions can`t be null");
         }
     }
 
     public static List<String> topActiveUsers(List<UserAction> actions) {
-        validate(actions);
+        validateActions(actions);
 
         return actions.stream()
                 .collect(Collectors.groupingBy(UserAction::getName, Collectors.counting()))
                 .entrySet().stream()
                 .sorted(Map.Entry.<String, Long>comparingByValue().reversed())
-                .limit(10)
+                .limit(TOP_ACTIVE_USERS)
                 .map(Map.Entry::getKey)
                 .toList();
     }
 
     public static List<String> topCommentersLastMonth(List<UserAction> actions) {
-        validate(actions);
+        validateActions(actions);
 
         int maxMonth = actions.stream()
                 .map(UserAction::getActionDate)
@@ -52,34 +58,33 @@ public class UserActionAnalyzer {
 
         return commentsByUser.entrySet().stream()
                 .sorted(Map.Entry.comparingByValue((list1, list2) -> list2.size() - list1.size()))
-                .limit(3)
+                .limit(TOP_USERS_MOST_COMMENTS)
                 .map(entry -> namesId.get(entry.getKey())).toList();
 
     }
 
     public static List<String> topPopularHashtags(List<UserAction> actions) {
-        validate(actions);
+        validateActions(actions);
 
-        Pattern pattern = Pattern.compile("#\\w+.");
 
         return actions.stream()
                 .map(UserAction::getContent)
                 .filter(content -> content.contains("#"))
                 .flatMap(string -> Arrays.stream(string.split(" ")))
-                .filter(string -> pattern.matcher(string).matches())
+                .filter(string -> HASHTAG_SEARH_PATTERN.matcher(string).matches())
                 .collect(Collectors.toMap(
-                        string -> string.replaceAll("[.,;!?]*$", ""),
+                        string -> string.replaceAll(REGEX_REMOVING_PUNCTUATION, ""),
                         count -> 1,
                         (existing, replacement) -> existing + 1))
                 .entrySet()
                 .stream()
                 .sorted(Map.Entry.<String, Integer>comparingByValue().reversed())
-                .limit(5)
+                .limit(TOP_POPULAR_TOPICS)
                 .map(Map.Entry::getKey).toList();
     }
 
     public static Map<ActionType, Double> actionTypePercentages(List<UserAction> actions) {
-        validate(actions);
+        validateActions(actions);
 
         return actions.stream()
                 .collect(Collectors.groupingBy(UserAction::getActionType, Collectors.counting()))
