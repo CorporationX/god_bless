935b1718c76bdc5db10dc3fa560ecb4147665e8d
Maria Bortsova <bortsovamariia@gmail.com>
2024-12-12 11:33:28 +0300

    refactor after review

--- src/main/java/school/faang/task_46277/Main.java
+++ src/main/java/school/faang/task_46277/Main.java
@@ -4,12 +4,16 @@ import java.util.Arrays;
 import java.util.List;
 
 public class Main {
+    private static final String SPAM = "spam";
+    private static final int MIN_LENGTH = 10;
+    private static final String EMOJI = ";)";
+
     public static void main(String[] args) {
         MessageProcessor messageProcessor = new MessageProcessor();
 
-        MessageFilter spamFilter = message -> !message.toLowerCase().contains("spam");
-        MessageFilter lengthFilter = message -> message.length() > 10;
-        MessageFilter emojiFilter = message -> !message.contains(";)");
+        MessageFilter spamFilter = message -> !message.toLowerCase().contains(SPAM);
+        MessageFilter lengthFilter = message -> message.length() > MIN_LENGTH;
+        MessageFilter emojiFilter = message -> !message.contains(EMOJI);
 
         List<MessageFilter> filters = Arrays.asList(spamFilter, lengthFilter, emojiFilter);
 
@@ -20,7 +24,7 @@ public class Main {
 
         for (String message : messages) {
             boolean isFiltered = messageProcessor.processMessage(message, filters);
-            System.out.println("Сообщение: " + message + " | Пропущено: " + isFiltered);
+            System.out.printf("Сообщение: %s | Пропущено: %s%n", message, isFiltered);
         }
     }
 }
