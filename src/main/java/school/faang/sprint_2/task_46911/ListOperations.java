0c5cdfae5682d3287a3612842c7d519f955991b1
tatgricenko <tattgricenko@gmail.com>
2024-12-12 17:28:22 +0300

    Make template

---
+++ src/main/java/school/faang/sprint_2/task_46911/ListOperations.java
@@ -0,0 +1,51 @@
+package school.faang.sprint_2.task_46911;
+
+import java.util.List;
+
+public class ListOperations {
+    // сумма чётных чисел
+    public static void sumOfEvenNumbers() {
+
+    }
+
+
+    // максимальный элемент в списке
+    public static void findMax() {
+
+    }
+
+    // среднее значение в списке
+    public static void findAverage() {
+
+    }
+
+    // количество строк, начинающихся с определённого символа
+    public static void countStringsStartingWith() {
+
+    }
+
+    // отфильтровать список строк и оставить только те, которые содержат определённую строку
+    public static void filterStringsContainingSubstring() {
+
+    }
+
+    // отсортировать список строк по длине
+    public static void sortByLength() {
+
+    }
+
+    // проверить, все ли элементы списка удовлетворяют определённому условию
+    public static void allMatchCondition() {
+
+    }
+
+    // найти наименьший элемент в списке, который больше заданного числа
+    public static void findMinGreaterThan() {
+
+    }
+
+    // преобразовать список строк в список их длин
+    public static void convertToLengths() {
+
+    }
+}
---
+++ src/main/java/school/faang/sprint_2/task_46911/Main.java
@@ -0,0 +1,7 @@
+package school.faang.sprint_2.task_46911;
+
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
