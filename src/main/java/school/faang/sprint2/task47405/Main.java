ff538373c79635e9fc44af0ddc76143ed24db9aa
Alexey Gorokhov <almigor@mail.ru>
2024-12-12 23:51:54 +0300

    Stream API 2

---
+++ src/main/java/school/faang/sprint2/task47405/Main.java
@@ -0,0 +1,37 @@
+package school.faang.sprint2.task47405;
+
+import java.util.Map;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Set;
+
+public class Main {
+    public static void main(String[] args) {
+
+        List<Integer> numbers = List.of(11, 12, 13, 14, 1, 2, 3, 4, 5, 8, 4, 5, 6, 7, 8, 9);
+
+        Set<List<Integer>> pairs = StreamFunc.getUniquePairs(numbers, 10);
+        System.out.println(pairs);
+
+        Map<String, String> capitals = new HashMap<>();
+        capitals.put("Russia", "Moscow");
+        capitals.put("USA", "Washington");
+        capitals.put("Germany", "Berlin");
+
+        List<String> cities = StreamFunc.getSortedCities(capitals);
+        System.out.println(cities);
+
+        List<String> strings = List.of("apple", "banana", "avocado", "apricot");
+        List<String> sortedStrings = StreamFunc.getSelectedStrings(strings, 'a');
+        System.out.println(sortedStrings);
+
+        List<String> binaryNumbers = StreamFunc.numberToBinaryList(numbers);
+        System.out.println(binaryNumbers);
+
+        List<String> someStrings = List.of("apple", "banana", "cherry", "date", "fig", "grape", "Orange", "Peach");
+        List<String> selectedStrings = StreamFunc.sortStringsByAlphabet(someStrings, "abcdefghijklmnopqrstuvwxyz");
+        System.out.println(selectedStrings);
+
+
+    }
+}
---
+++ src/main/java/school/faang/sprint2/task47405/StreamFunc.java
@@ -0,0 +1,47 @@
+package school.faang.sprint2.task47405;
+
+import java.util.*;
+import java.util.stream.Collectors;
+
+public class StreamFunc {
+
+    public static Set<List<Integer>> getUniquePairs(List<Integer> numbers, int sum) {
+        return numbers.stream()
+                .filter(n -> n < sum)
+                .map(n -> Arrays.asList(n, sum - n))
+                .peek(Collections::sort)
+                .collect(Collectors.toSet());
+    }
+
+    public static List<String> getSortedCities(Map<String, String> cities) {
+        return cities.keySet()
+                .stream()
+                .sorted()
+                .map(cities::get)
+                .toList();
+    }
+
+    public static List<String> getSelectedStrings(List<String> strings, char ch) {
+        return strings.stream()
+                .filter(s -> s.startsWith(String.valueOf(ch)))
+                .sorted()
+                .toList();
+    }
+
+    public static List<String> numberToBinaryList(List<Integer> numbers) {
+        return numbers.stream()
+                .map(Integer::toBinaryString)
+                .toList();
+
+    }
+
+    public static List<String> sortStringsByAlphabet(List<String> strings, String alphabet) {
+        String regex = "[" + alphabet + "]+";
+        return strings.stream()
+                .filter(s -> s.matches(regex))
+                .sorted(Comparator.comparing(String::length))
+                .toList();
+    }
+
+
+}
