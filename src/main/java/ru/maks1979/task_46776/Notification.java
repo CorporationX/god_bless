168b13f56a4ecbc1cd69d005f40dfbd5876e572e
sanctusgermanus72362 <sanctus-germanus7@yandex.ru>
2024-12-12 19:10:00 +0300

    Исправления

---
+++ src/main/java/ru/maks1979/task_46776/Notification.java
@@ -0,0 +1,14 @@
+package ru.maks1979.task_46776;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+public class Notification {
+    @Setter
+    private String type;
+    @Setter
+    private String message;
+
+
+}
---
+++ src/main/java/ru/maks1979/task_46776/NotificationManager.java
@@ -0,0 +1,17 @@
+package ru.maks1979.task_46776;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.function.Consumer;
+
+public class NotificationManager {
+    Map<String, Consumer<Notification>> notificationCache = new HashMap<>();
+
+    public void registerHandler(String notificationId, Consumer<Notification> consumer) {
+        notificationCache.put(notificationId, consumer);
+    }
+
+    public void sendNotification(Notification notification) {
+        notificationCache.get(notification.getType());
+    }
+}
