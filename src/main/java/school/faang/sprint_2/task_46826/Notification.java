fd9d318c4c34f132edfb7307f85f24add7d52a1f
mixa <mikle.kuznecov@yandex.ru>
2024-12-13 01:11:54 +0300

    add task 46826

---
+++ src/main/java/school/faang/sprint_2/task_46826/Notification.java
@@ -0,0 +1,11 @@
+package school.faang.sprint_2.task_46826;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@AllArgsConstructor
+@Getter
+public class Notification {
+    private String type;
+    private String message;
+}
