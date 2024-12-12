f6af2e98860108e4cc6cbd20c7a76cf2c9767128
Arseni Zaytsau <zarseniy280@gmail.com>
2024-12-12 12:55:34 +0300

    ztsv-BJS2-45371 - changed hashcode and equals

--- src/main/java/school/faang/bjs45371/Book.java
+++ src/main/java/school/faang/bjs45371/Book.java
@@ -3,6 +3,8 @@ package school.faang.bjs45371;
 import lombok.AllArgsConstructor;
 import lombok.ToString;
 
+import java.util.Objects;
+
 @AllArgsConstructor
 @ToString
 public class Book {
@@ -10,25 +12,16 @@ public class Book {
     private String author;
     private int year;
 
-
     @Override
     public boolean equals(Object o) {
-        if (this == o) {
-            return true;
-        }
-        if (o == null || getClass() != o.getClass()) {
-            return false;
-        }
-
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
         Book book = (Book) o;
-        return year == book.year && title.equals(book.title) && author.equals(book.author);
+        return year == book.year && Objects.equals(title, book.title) && Objects.equals(author, book.author);
     }
 
     @Override
     public int hashCode() {
-        int result = title.hashCode();
-        result = 31 * result + author.hashCode();
-        result = 31 * result + year;
-        return result;
+        return Objects.hash(title, author, year);
     }
 }
