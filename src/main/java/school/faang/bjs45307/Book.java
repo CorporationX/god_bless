772a55bbf564ab1b96172f3c1fcdca3ca0c4002d
Stepan Evangulov <stevangulov@gmail.com>
2024-12-12 10:30:16 +0200

    Droid

--- src/main/java/school/faang/bjs45307/Book.java
+++ src/main/java/school/faang/bjs45307/Book.java
@@ -3,13 +3,12 @@ package school.faang.bjs45307;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import lombok.Setter;
-import lombok.ToString;
 
 @RequiredArgsConstructor
 @Getter
 @Setter
 public class Book {
-    private String title;
-    private String author;
-    private int year;
+    private final String title;
+    private final String author;
+    private final int year;
 }
