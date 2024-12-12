8c0536deac830ccf306ccbd6502a3bbdbcd31849
Roman Shevchuk <reacount1393@gmail.com>
2024-12-12 14:32:59 +0300

    Разворот работает, чекстайл не ругается

--- src/main/java/school/faang/Main.java
+++ src/main/java/school/faang/Main.java
@@ -1,4 +1,8 @@
 package school.faang;
 
 public class Main {
+    public static void main(String[] args) {
+        Example example = new Example();
+        example.reverse(new int[]{1, 2, 3, 4, 5});
+    }
 }
