2020ad5c48a6d70e2dbd236f0a172649330b3d66
Mihail Ermolaev <fitis_rus21@mail.ru>
2024-12-12 14:40:07 +0300

    Реализована задача Gmail Rich Filters

--- src/main/java/school/faang/task_45999/EmailProcessor.java
+++ src/main/java/school/faang/task_45999/EmailProcessor.java
@@ -1,7 +1,5 @@
 package school.faang.task_45999;
 
-import javax.swing.text.html.HTMLDocument;
-import java.util.Iterator;
 import java.util.List;
 import java.util.function.Consumer;
 import java.util.function.Function;
@@ -9,19 +7,14 @@ import java.util.function.Predicate;
 
 public class EmailProcessor {
 
-    EmailProcessor emailProcessor = new EmailProcessor();
-    public void processEmails(List<Email> emails, Predicate<Email> predicate, Consumer<Email> consumer, Function<Email, String> function) {
-
+    public void processEmails(List<Email> emails, Predicate<Email> checkImportant, Consumer<Email> printEmail, Function<Email, String> changeEmail) {
 
         for (Email email : emails) {
-            if (predicate.test(email)){
-                String transBody = function.apply(email);
-                email.setBody(transBody);
-                consumer.accept(email);
-
-
+            if (checkImportant.test(email)) {
+                String changeBody = changeEmail.apply(email);
+                email.setBody(changeBody);
+                printEmail.accept(email);
             }
         }
-
     }
 }
