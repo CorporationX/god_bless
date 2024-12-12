df303b1a61884ab6ca6a7a1385beb4b6e8f12e27
Artem Kopanev <artemkopanev757@gmail.com>
2024-12-12 23:40:32 +0300

    BJS2-46525

---
+++ src/main/java/school/faang/sprint2/bjs_46525/Image.java
@@ -0,0 +1,11 @@
+package school.faang.sprint2.bjs_46525;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@AllArgsConstructor
+@Getter
+public class Image {
+    private String name;
+    private String description;
+}
