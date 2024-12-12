fe7a541b51e85582f3cf066177c260113b133951
Oxana <nov2303@mail.ru>
2024-12-12 21:20:38 +0500

    BJS2-46190

--- src/main/java/school/faang/task_46190/Droid.java
+++ src/main/java/school/faang/task_46190/Droid.java
@@ -2,24 +2,56 @@ package school.faang.task_46190;
 
 import lombok.AllArgsConstructor;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.StringReader;
+
 @AllArgsConstructor
 public class Droid {
     private String name;
 
-    private void encryptMessage() {
-
+    private String encryptMessage(String message, int encryptionKey) throws IOException {
+        DroidMessageEncryptor encryptor = (msg, key) -> {
+            StringBuilder encryptedMessage = new StringBuilder();
+            for (char ch : msg.toCharArray()) {
+                if (Character.isLetter(ch)) {
+                    char base = Character.isLowerCase(ch) ? 'a' : 'A';
+                    encryptedMessage.append((char) ((ch - base + key) % 26 + base));
+                } else {
+                    encryptedMessage.append(ch);
+                }
+            }
+            return encryptedMessage.toString();
+        };
+        return encryptor.encrypt(message, encryptionKey);
     }
 
-    private void decryptMessage() {
 
+    private String decryptMessage(String message, int encryptionKey) throws IOException {
+        DroidMessageEncryptor decryptor = (msg, key) -> {
+            StringBuilder decryptedMessage = new StringBuilder();
+            for (char ch : msg.toCharArray()) {
+                if (Character.isLetter(ch)) {
+                    char base = Character.isLowerCase(ch) ? 'a' : 'A';
+                    decryptedMessage.append((char) ((ch - base - key + 26) % 26 + base));
+                } else {
+                    decryptedMessage.append(ch);
+                }
+            }
+            return decryptedMessage.toString();
+        };
+        return decryptor.encrypt(message, encryptionKey);
     }
 
-    public void sendMessage(Droid droid, String message, int encryptionKey){
-
+    public void sendMessage(Droid droid, String message, int encryptionKey) throws IOException {
+        String encryptResult = encryptMessage(message, encryptionKey);
+        System.out.println(String.format("%s send a encrypted message: %s", this.name, encryptResult));
+        System.out.println(String.format("%s received a decrypted message: %s", this.name, receiveMessage(encryptResult,
+                encryptionKey)));
     }
 
-    private void receiveMessage() {
-
+    private String receiveMessage(String message, int decryptionKey) throws IOException {
+        return decryptMessage(message, decryptionKey);
     }
 
 }
