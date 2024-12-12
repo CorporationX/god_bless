089ed3c5179dd79222b50a30de5d6ce9b3ba000c
RustBad <rustambad1993@gmail.com>
2024-12-12 16:02:32 +0500

    Task_43677

---
+++ src/main/java/school/faang/task_43677/Main.java
@@ -0,0 +1,11 @@
+package school.faang.task_43677;
+
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) {
+        int[] nums = {1, 2, 3, 4, 5};
+        int[] results = Example.reverse(nums);
+        System.out.println(Arrays.toString(results));
+    }
+}
