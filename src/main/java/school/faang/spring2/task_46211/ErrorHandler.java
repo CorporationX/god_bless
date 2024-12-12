63a2d668a8a96f3c25a0fae1f0dec2f78f74a102
Timka-cloud1 <amanzholtimka@gmail.com>
2024-12-12 14:16:47 +0500

    Обрабатываем ошибки красиво

--- src/main/java/school/faang/spring2/task_46211/ErrorHandler.java
+++ src/main/java/school/faang/spring2/task_46211/ErrorHandler.java
@@ -1,12 +1,10 @@
 package school.faang.spring2.task_46211;
 
-import java.util.function.Function;
 import java.util.function.Supplier;
 
 public class ErrorHandler {
 
-    public static <T> T withErrorHandling(Supplier<T> supplier, ExceptionHandle<T> handle) {
-
+    public static <T> T withErrorHandling(Supplier<T> supplier, ExceptionHandler<T> handle) {
         try {
             return supplier.get();
         } catch (Exception e) {
--- src/main/java/school/faang/spring2/task_46211/ExceptionHandler.java
+++ src/main/java/school/faang/spring2/task_46211/ExceptionHandler.java
@@ -1,6 +1,6 @@
 package school.faang.spring2.task_46211;
 
 @FunctionalInterface
-public interface ExceptionHandle<T> {
+public interface ExceptionHandler<T> {
     T handle(Exception e);
 }
