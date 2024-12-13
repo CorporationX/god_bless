ad66edec3cc740092fc051808e07990eda0eef69
porchy21 <ogogo.mt@gmail.com>
2024-12-14 00:02:48 +0500

    modified solution

--- src/main/java/school/faang/task_46217/Action.java
+++ src/main/java/school/faang/task_46217/Action.java
@@ -1,14 +1,15 @@
 package school.faang.task_46217;
 
 import java.util.function.Function;
+import java.util.function.Supplier;
 
 @FunctionalInterface
 public interface Action<T> {
     T execute() throws Exception;
 
-    static <T> T withErrorHandling(Action<T> mainFunction, Function<Exception, T> handlingFunction) {
+    static <T> T withErrorHandling(Supplier<T> mainFunction, Function<Exception, T> handlingFunction) {
         try {
-            return mainFunction.execute();
+            return mainFunction.get();
         } catch (Exception e) {
             return handlingFunction.apply(e);
         }
--- src/main/java/school/faang/task_46217/Main.java
+++ src/main/java/school/faang/task_46217/Main.java
@@ -3,7 +3,13 @@ package school.faang.task_46217;
 public class Main {
     public static void main(String[] args) {
         String result = Action.withErrorHandling(
-                () -> RemoteService.call("Conect"),
+                () -> {
+                    try {
+                        return RemoteService.call("Conect");
+                    } catch (Exception e) {
+                        throw new RuntimeException(e);
+                    }
+                },
                 e -> {
                     System.out.println("Ошибка при вызове сервиса: " + e.getMessage());
                     return "DEFAULT";
