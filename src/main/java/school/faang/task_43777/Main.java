7ff1dfc15e5e9f5b55c15bc55b95460ec597081e
Roman Kondratev <romankondratev.com@gmail.com>
2024-12-13 08:00:53 -0800

    BJS2-43777 refactor: Main

--- src/main/java/school/faang/task_43777/Main.java
+++ src/main/java/school/faang/task_43777/Main.java
@@ -3,5 +3,11 @@ package school.faang.task_43777;
 public class Main {
     public static void main(String[] args) {
         UserFactory.createUsers().forEach(System.out::println);
+
+        try {
+            new User("Alice", 17, "Google", "London");
+        } catch (IllegalArgumentException e) {
+            System.err.println("Ошибка создания пользователя: " + e.getMessage());
+        }
     }
 }
