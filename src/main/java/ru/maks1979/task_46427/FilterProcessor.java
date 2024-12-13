b70f72a6f91d1b7b0ca728ab8185efd6f6646efb
sanctusgermanus72362 <sanctus-germanus7@yandex.ru>
2024-12-13 22:27:25 +0300

    Исправления

--- src/main/java/ru/maks1979/task_46427/FilterProcessor.java
+++ src/main/java/ru/maks1979/task_46427/FilterProcessor.java
@@ -3,12 +3,12 @@ package ru.maks1979.task_46427;
 import java.util.function.Function;
 
 public class FilterProcessor {
-    public Image applyFilter(Image image, Function<Image,Image> function) {
+    public Image applyFilter(Image image, Function<Image, Image> function) {
 
         return function.apply(image);
     }
 
-    public Function <Image, Image> combineFilters(Function<Image,Image> firstStep, Function<Image,Image> secondStep) {
-return firstStep.andThen(secondStep);
+    public Function<Image, Image> combineFilters(Function<Image, Image> firstStep, Function<Image, Image> secondStep) {
+        return firstStep.andThen(secondStep);
     }
 }
