0ea273af8d73c97f32e86f40af4403f3c359d0f7
CendalJoe <sova333sova@mail.ru>
2024-12-13 10:03:15 +0300

    Insta filters

---
+++ src/main/java/school/faang/cendaljoe_instagramf_ilters_bjs2_46403/Image.java
@@ -0,0 +1,27 @@
+package school.faang.cendaljoe_instagramf_ilters_bjs2_46403;
+
+public class Image {
+    private String name;
+    private String description;
+
+    public Image(String name, String description) {
+        this.name = name;
+        this.description = description;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
