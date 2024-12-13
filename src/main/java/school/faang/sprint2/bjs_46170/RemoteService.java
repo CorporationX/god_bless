5e00a8b74cee1cd29377e3d028a7c4c7dbc9811d
TFdevQA <46944859+TFdevQA@users.noreply.github.com>
2024-12-13 14:28:34 -0800

    task:46170 changed RemoteService

--- src/main/java/school/faang/sprint2/bjs_46170/RemoteService.java
+++ src/main/java/school/faang/sprint2/bjs_46170/RemoteService.java
@@ -1,8 +1,18 @@
 package school.faang.sprint2.bjs_46170;
 
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
 public class RemoteService {
-    public static String call(String param) throws Exception {
-        System.out.println(param);
-        throw new Exception("Сервис недоступен");
+    public static String exceptionCall(String param) {
+        log.error(param);
+        throw new RuntimeException("Сервис недоступен");
+    }
+
+    public static String call(String string) {
+        log.info("no error call");
+        return string;
     }
 }
+
+
