6671bf20536591b4954bade07bab68247219ebfe
Максим <vint_vin@mail.ru>
2024-12-13 16:46:55 +1000

    redy2

--- src/main/java/school/faang/sprint2/task46437instagram/Main.java
+++ src/main/java/school/faang/sprint2/task46437instagram/Main.java
@@ -9,20 +9,20 @@ public class Main {
 
         FilterProcessor filterProcessor = new FilterProcessor();
 
-// Фильтры
+        // Фильтры
         Function<Image, Image> grayscaleFilter = (image) -> new Image(image.getName(),
                 image.getDescription() + " | Фильтр: черно-белый");
         Function<Image, Image> sepiaFilter = (image) -> new Image(image.getName(),
                 image.getDescription() + " | Фильтр: сепия");
 
-// Применение фильтров
+        // Применение фильтров
         Image grayscaleImage = filterProcessor.applyFilter(originalImage, grayscaleFilter);
         System.out.println(grayscaleImage.getDescription());
 
         Image sepiaImage = filterProcessor.applyFilter(grayscaleImage, sepiaFilter);
         System.out.println(sepiaImage.getDescription());
 
-// Комбинирование фильтров
+        // Комбинирование фильтров
         Function<Image, Image> combinedFilter = filterProcessor.combineFilters(grayscaleFilter, sepiaFilter);
         Image combinedImage = filterProcessor.applyFilter(originalImage, combinedFilter);
         System.out.println(combinedImage.getDescription());
