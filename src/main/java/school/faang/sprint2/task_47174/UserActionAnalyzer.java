3df874a1d0be26c0d2e9a91be0b6fd387c144842
DmitryQueen <276246@student.pwr.edu.pl>
2024-12-13 23:59:47 +0100

    removed Maps

--- src/main/java/school/faang/sprint2/task_47174/UserActionAnalyzer.java
+++ src/main/java/school/faang/sprint2/task_47174/UserActionAnalyzer.java
@@ -1,71 +1,58 @@
 package school.faang.sprint2.task_47174;
 
 import java.time.LocalDate;
+import java.util.Arrays;
 import java.util.Comparator;
-import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
+import java.util.stream.Collectors;
 
 public class UserActionAnalyzer {
 
     public List<String> topActiveUsers(List<UserAction> actions) {
-        Map<String, Integer> nameToActions = new HashMap<>();
-        actions.stream()
-                .forEach((action) -> nameToActions.merge(action.getName(), 1, Integer::sum));
-
-        return nameToActions.entrySet().stream()
+        return actions.stream()
+                .collect(Collectors.groupingBy(UserAction::getName, Collectors.counting()))
+                .entrySet().stream()
                 .sorted(Map.Entry.comparingByValue(Comparator.reverseOrder()))
                 .limit(10)
-                .map((user) -> user.getKey())
+                .map(Map.Entry::getKey)
                 .toList();
     }
 
     public List<String> topPopularHashtags(List<UserAction> actions) {
-        Map<String, Integer> hashtags = new HashMap<>();
-        Pattern hashtagPattern = Pattern.compile("#\\w+");
-
-        actions.stream()
-                .forEach((action) -> {
-                    Matcher matcher = hashtagPattern.matcher(action.getText());
-                    if (matcher.find()) {
-                        String hashtag = matcher.group();
-                        hashtags.merge(hashtag, 1, Integer::sum);
-                    }
-                });
-
-        return hashtags.entrySet().stream()
+        return actions.stream()
+                .filter((action) -> action.getActionType() == ActionType.POST
+                        || action.getActionType() == ActionType.COMMENT)
+                .flatMap(action -> Arrays.stream(action.getText().split(" ")))
+                .filter((text) -> text.startsWith("#") && text.length() > 1)
+                .collect(Collectors.groupingBy(String::valueOf, Collectors.counting()))
+                .entrySet().stream()
                 .sorted(Map.Entry.comparingByValue(Comparator.reverseOrder()))
                 .limit(5)
-                .map((tag) -> tag.getKey())
+                .map(Map.Entry::getKey)
                 .toList();
     }
 
     public List<String> topCommentersLastMonth(List<UserAction> actions) {
-        Map<String, Integer> nameToComments = new HashMap<>();
-
-        actions.stream()
-                .filter((action) -> action.getDate().isAfter(LocalDate.now().minusMonths(1)))
-                .forEach((action) -> nameToComments.merge(action.getName(), 1, Integer::sum));
-
-        return nameToComments.entrySet().stream()
+        return actions.stream()
+                .filter((action) -> action.getActionType() == ActionType.COMMENT
+                        && action.getDate().isAfter(LocalDate.now().minusMonths(1)))
+                .collect(Collectors.groupingBy(UserAction::getName, Collectors.counting()))
+                .entrySet().stream()
                 .sorted(Map.Entry.comparingByValue(Comparator.reverseOrder()))
                 .limit(3)
-                .map((user) -> user.getKey())
+                .map(Map.Entry::getKey)
                 .toList();
     }
 
-    public Map<String, Double> actionTypePercentages(List<UserAction> actions) {
-        Map<String, Double> typePercentages = new HashMap<>();
-
-        actions.stream()
-                .forEach((action) -> typePercentages.merge(action.getActionType(), 1.00, Double::sum));
-
+    public Map<ActionType, Double> actionTypePercentages(List<UserAction> actions) {
         int actionsNumber = actions.size();
-        typePercentages.entrySet().stream()
-                .forEach((type) -> typePercentages.put(type.getKey(), (type.getValue() / actionsNumber) * 100));
-
-        return typePercentages;
+        return actions.stream()
+                .collect(Collectors.groupingBy(UserAction::getActionType, Collectors.counting()))
+                .entrySet().stream()
+                .collect(Collectors.toMap(
+                        Map.Entry::getKey,
+                        (entry) -> (entry.getValue() * 100.0) / actionsNumber
+                ));
     }
 }
