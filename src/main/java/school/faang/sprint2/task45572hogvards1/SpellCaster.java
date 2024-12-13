22604dddd6de209a8e375c3ca0ca4d3a82017c90
Максим <vint_vin@mail.ru>
2024-12-13 17:01:43 +1000

    redy2

--- src/main/java/school/faang/sprint2/task45572hogvards1/SpellCaster.java
+++ src/main/java/school/faang/sprint2/task45572hogvards1/SpellCaster.java
@@ -2,8 +2,9 @@ package school.faang.sprint2.task45572hogvards1;
 
 public class SpellCaster {
     void cast(String spell, SpellAction action) {
-        if (spell == null || action == null)
+        if (spell == null || action == null) {
             throw new IllegalArgumentException("Неверные параметры");
+        }
         System.out.println(action.act(spell));
     }
 }
