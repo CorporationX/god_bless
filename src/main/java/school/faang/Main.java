036115cc066659e0393fe479c85342198c57b408
Kairat <snowmango.official@gmail.com>
2024-12-14 00:49:46 +0500

    delete comments

--- src/main/java/school/faang/Main.java
+++ src/main/java/school/faang/Main.java
@@ -4,8 +4,5 @@ public class Main {
     public static void main(String[] args) {
         User user1 = new User("Alice", 25, "Google", "London");
         System.out.println(user1);
-        // with error
-//        User user2 = new User("Alice", 25, "GoogleCloud", "London");
-//        System.out.println(user2);
     }
 }
