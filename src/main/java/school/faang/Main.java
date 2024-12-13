4ffca092850a36ea46177fb5e4d0a8146e40f22d
olegmankirov <oleg_mankirov@stimul.app>
2024-12-13 23:55:55 +0300

    bjs2-44044 grouping users by age

--- src/main/java/school/faang/Main.java
+++ src/main/java/school/faang/Main.java
@@ -1,5 +1,11 @@
 package school.faang;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+import static school.faang.User.groupUsers;
+
 public class Main {
     public static void main(String[] args) {
         try {
@@ -26,6 +32,16 @@ public class Main {
             new User("Oleg", 35, "Amazon", "Moscow");
         } catch (IllegalArgumentException e) {
             System.out.println("Catch exception " + e.getMessage());
+        }
+        List<User> users = new ArrayList<>();
+        users.add(new User("Roman", 35, "Zavod", "Vyhino"));
+        users.add(new User("Vasily", 65, "DSK", "Novokosino"));
+        users.add(new User("Eugene", 25, "Office", "Seligerskaya"));
+        users.add(new User("Oleg", 35, "Yandex", "Park Kultury"));
+
+        Map<Integer, List<User>> usersByAge = groupUsers(users);
+        for(Map.Entry<Integer, List<User>> entry : usersByAge.entrySet()) {
+            System.out.println(entry.getKey() + " : " + entry.getValue());
         }
     }
 }
--- src/main/java/school/faang/User.java
+++ src/main/java/school/faang/User.java
@@ -1,7 +1,18 @@
 package school.faang;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.ToString;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 import java.util.Set;
 
+@Getter
+@AllArgsConstructor
+@ToString
 public class User {
     private String name;
     private int age;
@@ -37,5 +48,12 @@ public class User {
         if (!VALID_ADDRESSES.contains(address)) {
             throw new IllegalArgumentException("address is not valid");
         }
+
+    public static Map<Integer, List<User>> groupUsers(List<User> users) {
+        Map<Integer, List<User>> usersByAge = new HashMap<>();
+        for (User user : users) {
+            usersByAge.computeIfAbsent(user.age, key -> new ArrayList<>()).add(user);
+        }
+        return usersByAge;
     }
 }
