ac67a10764ae0310f94d5dce797048e8ceca0925
our ame <you@example.com>
2024-12-13 14:01:24 +0500

    Сделал изменения в задаче "Ловим события"

--- src/main/java/school/faang/sprint_1/task_45389/LibrarySystem.java
+++ src/main/java/school/faang/sprint_1/task_45389/LibrarySystem.java
@@ -1,24 +1,27 @@
 package school.faang.sprint_1.task_45389;
 
+import java.util.HashMap;
 import java.util.Map;
 
-import static school.faang.sprint_1.task_45389.Main.books;
+//import static school.faang.sprint_1.task_45389.Main.books;
 
 public class LibrarySystem {
-    public static void addBook(String title, String author, int year, String location) {
+    private Map<Book, String> books = new HashMap<>();
+
+    public void addBook(String title, String author, int year, String location) {
         Book book = new Book(title, author, year);
         books.put(book, location);
         System.out.println("Книга добавлена: " + book + " местонахождение: " + location);
         System.out.println();
     }
 
-    public static void removeBook(String title, String author, int year) {
+    public void removeBook(String title, String author, int year) {
         Book book = new Book(title, author, year);
         books.remove(book);
         System.out.println("Книга " + book + " удалена ");
     }
 
-    public static void findBook(String title, String author, int year) {
+    public void findBook(String title, String author, int year) {
         Book book = new Book(title, author, year);
         if (books.containsKey(book)) {
             String location = books.get(book);
@@ -28,7 +31,7 @@ public class LibrarySystem {
         }
     }
 
-    public static void printAllBooks() {
+    public void printAllBooks() {
         for (Map.Entry<Book, String> entry : books.entrySet()) {
             System.out.println("имя: " + entry.getKey() + " находится на полке: " + entry.getValue());
         }
--- src/main/java/school/faang/sprint_1/task_45389/Main.java
+++ src/main/java/school/faang/sprint_1/task_45389/Main.java
@@ -4,19 +4,18 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class Main {
-    public static Map<Book, String> books = new HashMap<>();
-
     public static void main(String[] args) {
-        books.put(new Book("The Lord of the Rings", "J.Tolkien", 1954), "l-11");
-        books.put(new Book("Harry Potter and the Philosopher's Stone", "J.Rowling", 1997), "h-13");
-        books.put(new Book("A Game of Thrones", "R.Martin", 1996), "g-32");
-        books.put(new Book("War and Piece", "L.Tolstoy", 1867), "l-54");
-        books.put(new Book("The Three Musketeers", "A.Dumas", 1844), "d-11");
+        LibrarySystem librarySystem = new LibrarySystem();
 
-        LibrarySystem.addBook("An American Tragedy", "T.Dreiser", 1925, "d-25");
-        LibrarySystem.removeBook("Harry Potter and the Philosopher's Stone", "J.Rowling", 1997);
-        LibrarySystem.findBook("Harry Potter and the Chamber of Secrets", "J.Rowling", 1999);
-        LibrarySystem.findBook("War and Piece", "L.Tolstoy", 1867);
-        LibrarySystem.printAllBooks();
+        librarySystem.addBook("The Lord of the Rings", "J.Tolkien", 1954, "l-11");
+        librarySystem.addBook("Harry Potter and the Philosopher's Stone", "J.Rowling", 1997, "h-13");
+        librarySystem.addBook("A Game of Thrones", "R.Martin", 1996, "g-32");
+        librarySystem.addBook("War and Piece", "L.Tolstoy", 1867, "l-54");
+        librarySystem.addBook("The Three Musketeers", "A.Dumas", 1844, "d-11");
+        librarySystem.addBook("An American Tragedy", "T.Dreiser", 1925, "d-25");
+        librarySystem.removeBook("Harry Potter and the Philosopher's Stone", "J.Rowling", 1997);
+        librarySystem.findBook("Harry Potter and the Philosopher's Stone", "J.Rowling", 1999);
+        librarySystem.findBook("War and Piece", "L.Tolstoy", 1867);
+        librarySystem.printAllBooks();
     }
 }
