1498767e31ce6ea7add9f8513804f80e742392b1
maks <fhbfds@fb>
2024-12-13 12:53:05 +0300

    BJS2-47260 API 2ч

---
+++ src/main/java/school/faang/task_47260/Data.java
@@ -0,0 +1,48 @@
+package school.faang.task_47260;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+public class Data {
+
+    public static Set<List<Integer>> findPairs(List<Integer> numbers, int sum) {
+        Set<List<Integer>> pairs = new HashSet<>();
+        Set<Integer> set = new HashSet<>();
+        set.addAll(numbers);
+        pairs = numbers.stream()
+                .filter(num -> set.contains(sum - num))
+                .map(num -> Arrays.asList(num, sum - num))
+                .peek(Collections::sort)
+                .collect(Collectors.toSet());
+        return pairs;
+    }
+
+    public static List<String> getSortedCapitals(Map<String, String> countries) {
+        return countries.entrySet().stream()
+                .sorted(Map.Entry.comparingByKey())
+                .map(Map.Entry::getValue)
+                .collect(Collectors.toList());
+    }
+
+    public static List<String> filterAndSort(List<String> strings, char letter) {
+        return strings.stream().filter(str -> str.startsWith(String.valueOf(letter)))
+                .sorted(Comparator.comparingInt(String::length))
+                .collect(Collectors.toList());
+    }
+
+    public static List<String> listStringBinary(List<Integer> ints) {
+        return ints.stream().map(integer -> Integer.toBinaryString(integer)).collect(Collectors.toList());
+    }
+
+    public static List<String> listStringFilter(List<String> words, String alphabet) {
+        String regex = "[" + alphabet + "]+";
+        return words.stream().filter(str -> str.matches(regex)).sorted(Comparator.comparingInt(String::length))
+                .collect(Collectors.toList());
+    }
+}
