fe49d4de7f351f3c043c55a4ffb071bccc6aa2ac
CendalJoe <sova333sova@mail.ru>
2024-12-13 10:23:16 +0300

    checkstyle fix

--- src/main/java/cendaljoe_bjs2_45727/Main.java
+++ src/main/java/cendaljoe_bjs2_45727/Main.java
@@ -3,14 +3,14 @@ package cendaljoe_bjs2_45727;
 public class Main {
 
     public static void main(String[] args) {
-        SpellCaster CasterWatch = new SpellCaster();
+        SpellCaster casterWatch = new SpellCaster();
 
         String armagedon = "Armagedon";
         String chainLightning = "ChainLightning";
         String slowingDown = "SlowingDown";
 
-        CasterWatch.viewCast(armagedon, spell -> "Armagedonus exterminato - " + spell);
-        CasterWatch.viewCast(chainLightning, spell -> "Amenos chain - " + spell);
-        CasterWatch.viewCast(slowingDown, spell -> "Hardo walkus - " + spell);
+        casterWatch.viewCast(armagedon, spell -> "Armagedonus exterminato - " + spell);
+        casterWatch.viewCast(chainLightning, spell -> "Amenos chain - " + spell);
+        casterWatch.viewCast(slowingDown, spell -> "Hardo walkus - " + spell);
     }
 }
