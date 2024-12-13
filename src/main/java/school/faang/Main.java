89780c98c03e6a297ee18064d3eeddc78be21d6e
Kairat <snowmango.official@gmail.com>
2024-12-14 00:43:08 +0500

    add user and test in main class

--- src/main/java/school/faang/Main.java
+++ src/main/java/school/faang/Main.java
@@ -1,4 +1,11 @@
 package school.faang;
 
 public class Main {
+    public static void main(String[] args) {
+        User user1 = new User("Alice", 25, "Google", "London");
+        System.out.println(user1);
+        // with error
+//        User user2 = new User("Alice", 25, "GoogleCloud", "London");
+//        System.out.println(user2);
+    }
 }
---
+++ src/main/java/school/faang/User.java
@@ -0,0 +1,26 @@
+package school.faang;
+
+import lombok.ToString;
+
+import java.util.*;
+
+@ToString
+public class User {
+
+    private String name;
+    private int age;
+    private String workPlace;
+    private String address;
+
+    private static final List<String> VALID_JOBS = Arrays.asList("Google", "Uber", "Amazon");
+    private static final List<String> VALID_ADDRESSES = Arrays.asList( "London", "New York", "Amsterdam");
+
+    public User(String name, int age, String workPlace, String address) {
+        this.name = Optional.ofNullable(name).orElseThrow(IllegalArgumentException::new);
+        this.age = Optional.ofNullable(age > 18 ? age : null).orElseThrow(IllegalArgumentException::new);
+        this.workPlace = Optional.ofNullable(VALID_JOBS.contains(workPlace) ? workPlace : null)
+                .orElseThrow(IllegalArgumentException::new);
+        this.address = Optional.ofNullable(VALID_ADDRESSES.contains(address) ? address : null)
+                .orElseThrow(IllegalArgumentException::new);
+    }
+}
