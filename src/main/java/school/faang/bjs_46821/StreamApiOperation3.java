1b611daf0de3becf8715d417688149b60294ce88
Anton Graf <ant0ngraf@yandex.ru>
2024-12-13 13:51:13 +0600

    Добавляет поиск людей с общими друзьями

--- src/main/java/school/faang/bjs_46821/StreamApiOperation3.java
+++ src/main/java/school/faang/bjs_46821/StreamApiOperation3.java
@@ -8,13 +8,9 @@ public class StreamApiOperation3 {
     public static Map<String, String> noFriendsPair(Map<String, List<String>> users) {
         Map<String, String> noFriendsPair = new HashMap<>();
 
-        users.forEach((user, friendList) -> {
-            friendList.forEach(friend -> {
-                users.get(friend).stream()
-                        .filter(f -> !f.equals(user) && !friendList.contains(f) && !noFriendsPair.containsKey(f))
-                        .forEach(f -> noFriendsPair.put(user, f));
-            });
-        });
+        users.forEach((user, friendList) -> friendList.forEach(friend -> users.get(friend).stream()
+                .filter(f -> !f.equals(user) && !friendList.contains(f) && !noFriendsPair.containsKey(f))
+                .forEach(f -> noFriendsPair.put(user, f))));
 
         return noFriendsPair;
     }
