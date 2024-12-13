6e1132d139600858eb309c1e41beba4817caeb04
Ivan Baev <ibai86@yandex.ru>
2024-12-13 03:10:04 +0300

    added second method

--- src/main/java/school/faang/streamapi_one/ListOperations.java
+++ src/main/java/school/faang/streamapi_one/ListOperations.java
@@ -1,6 +1,8 @@
 package school.faang.streamapi_one;
 
 import java.util.List;
+import java.util.NoSuchElementException;
+import java.util.Optional;
 
 public class ListOperations {
     public static int sumOfEvenNumbers(List<Integer> list) {
@@ -9,5 +11,11 @@ public class ListOperations {
                 .mapToInt(Integer::intValue)
                 .sum();
         return sum;
+    }
+
+    public static int findMax(List<Integer> list) {
+        return list.stream()
+                .max(Integer::compareTo)
+                .orElseThrow(() -> new NoSuchElementException("LIst is empty"));
     }
 }
