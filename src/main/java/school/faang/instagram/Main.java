e67734b96848428d5504972b13a6afe68538a090
muslim <xammuslim06@gmail.com>
2024-12-13 16:33:46 +0300

    Instagram Filters, fixed code style

--- src/main/java/school/faang/instagram/Main.java
+++ src/main/java/school/faang/instagram/Main.java
@@ -8,8 +8,10 @@ public class Main {
 
         FilterProcessor filterProcessor = new FilterProcessor();
 
-        Function<Image, Image> grayscaleFilter = (image) -> new Image(image.getName(), image.getDescription() + " | Фильтр: черно-белый");
-        Function<Image, Image> sepiaFilter = (image) -> new Image(image.getName(), image.getDescription() + " | Фильтр: сепия");
+        Function<Image, Image> grayscaleFilter = (image) -> new Image(image.getName(), image.getDescription()
+                + " | Фильтр: черно-белый");
+        Function<Image, Image> sepiaFilter = (image) -> new Image(image.getName(), image.getDescription() +
+                " | Фильтр: сепия");
 
         Image grayscaleImage = filterProcessor.applyFilter(originalImage, grayscaleFilter);
         System.out.println(grayscaleImage.getDescription());
