9f13e848babc9c090739245139b21dcc23eebde2
lcodio <lcodio@temp.com>
2024-12-14 09:53:19 +0300

    task BJS2-44546 fix

--- src/main/java/school/faang/s13task44546/SpellEvent.java
+++ src/main/java/school/faang/s13task44546/SpellEvent.java
@@ -1,12 +1,10 @@
 package school.faang.s13task44546;
 
-import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import lombok.ToString;
 
 @Getter
-@EqualsAndHashCode
 @ToString
 @RequiredArgsConstructor
 public class SpellEvent {
