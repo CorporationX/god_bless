3d934dea1a06f1af57b80304b8b92551f9cf0c2f
Maria Bortsova <bortsovamariia@gmail.com>
2024-12-14 22:44:15 +0300

    refactor after review

--- src/main/java/school/faang/task_46528/Droid.java
+++ src/main/java/school/faang/task_46528/Droid.java
@@ -3,9 +3,12 @@ package school.faang.task_46528;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 
+import java.util.function.BiFunction;
+
 @RequiredArgsConstructor
 @Getter
 public class Droid {
+    private static final int COUNT_LETTERS_ALPHABET = 26;
     private final String name;
 
     public void sendMessage(Droid droid, String message, int key) {
@@ -15,36 +18,30 @@ public class Droid {
         receiveMessage(decryptedMessage, key, droid);
     }
 
-    public void receiveMessage(String message, int decryptKey, Droid droid) {
-        String decryptedMessage = droid.decryptMessage(message, decryptKey);
+    public void receiveMessage(String message, int key, Droid droid) {
+        String decryptedMessage = droid.decryptMessage(message, key);
         System.out.println(droid.getName() + " получил расшифрованное сообщение: "
                 + decryptedMessage);
     }
 
-    private String encryptMessage(String message, int encryptKey) {
-        StringBuilder builder = new StringBuilder();
-        DroidMessageEncryptor droidMessageEncryptor = (m, k) -> {
-            for (char ch : m.toCharArray()) {
-                if (Character.isLetter(ch)) {
-                    char base = Character.isLowerCase(ch) ? 'a' : 'A';
-                    builder.append((char) ((ch - base + encryptKey) % 26 + base));
-                } else {
-                    builder.append(ch);
-                }
-            }
-            return builder.toString();
-        };
-
-        return droidMessageEncryptor.encrypt(message, encryptKey);
+    private String encryptMessage(String message, int key) {
+        return getMessageAfterCaesarCipher(message, key,
+                (ch, base) -> (char) ((ch - base + key) % COUNT_LETTERS_ALPHABET + base));
     }
 
-    private String decryptMessage(String message, int decryptKey) {
+    private String decryptMessage(String message, int key) {
+        return getMessageAfterCaesarCipher(message, key,
+                (ch, base) -> (char) ((ch - base - key + COUNT_LETTERS_ALPHABET) % COUNT_LETTERS_ALPHABET + base));
+    }
+
+    private String getMessageAfterCaesarCipher(String message, int key,
+                                               BiFunction<Character, Character, Character> cipher) {
         StringBuilder builder = new StringBuilder();
         DroidMessageEncryptor droidMessageEncryptor = (m, k) -> {
             for (char ch : m.toCharArray()) {
                 if (Character.isLetter(ch)) {
                     char base = Character.isLowerCase(ch) ? 'a' : 'A';
-                    builder.append((char) ((ch - base - decryptKey + 26) % 26 + base));
+                    builder.append(cipher.apply(ch, base));
                 } else {
                     builder.append(ch);
                 }
@@ -52,6 +49,6 @@ public class Droid {
             return builder.toString();
         };
 
-        return droidMessageEncryptor.encrypt(message, decryptKey);
+        return droidMessageEncryptor.encrypt(message, key);
     }
 }
