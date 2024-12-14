6cff0dfa50491b2d863884181a01477009465a21
Viktor <vikt.ovod@outlook.com>
2024-12-15 00:32:00 +0300

    уменьшил длину некоторых строк, чтобы GitHub пропустил этот проект на PR

--- src/main/java/school/faang/sprint2/bjs246456/FilterSystem.java
+++ src/main/java/school/faang/sprint2/bjs246456/FilterSystem.java
@@ -7,14 +7,21 @@ public class FilterSystem {
         Image originalImage = new Image("original.jpg", "Оригинальное изображение");
         FilterProcessor filterProcessor = new FilterProcessor();
 
-        Function<Image, Image> blackAndWhiteFilter = (image) -> new Image(image.getName(), image.getDescription() + " ⇢ Фильтр: черно-белый");
-        Function<Image, Image> sepiaFilter = (image) -> new Image(image.getName(), image.getDescription() + " ⇢ Фильтр: сепия");
-        Function<Image, Image> vignetteFilter = (image) -> new Image(image.getName(), image.getDescription() + " ⇢ Фильтр: виньетка");
+        Function<Image, Image> blackAndWhiteFilter = (image) ->
+                new Image(image.getName(), image.getDescription() + " ⇢ Фильтр: черно-белый");
+
+        Function<Image, Image> sepiaFilter = (image) ->
+                new Image(image.getName(), image.getDescription() + " ⇢ Фильтр: сепия");
+
+        Function<Image, Image> vignetteFilter = (image) ->
+                new Image(image.getName(), image.getDescription() + " ⇢ Фильтр: виньетка");
 
         Image blackAndWhiteImage = filterProcessor.applyFilter(originalImage, blackAndWhiteFilter);
         System.out.println(blackAndWhiteImage.getDescription());
+
         Image sepiaImage = filterProcessor.applyFilter(originalImage, sepiaFilter);
         System.out.println(sepiaImage.getDescription());
+
         Image vignetteImage = filterProcessor.applyFilter(originalImage, vignetteFilter);
         System.out.println(vignetteImage.getDescription());
 
