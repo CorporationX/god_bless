4bb0f6c96c8c61fb3852e7ca433e84787c7577e0
alterality <rordufe@gmail.com>
2024-12-14 14:11:45 +0600

    RPG rings

---
+++ src/main/java/school/faang/sprint_2/task_45665/Character.java
@@ -0,0 +1,23 @@
+package school.faang.sprint_2.task_45665;
+
+import lombok.Getter;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Getter
+public class Character {
+    private final List<Item> inventory;
+
+    public Character(String name) {
+        this.inventory = new ArrayList<>();
+    }
+
+    public void addItem(Item item) {
+        inventory.add(item);
+    }
+
+    public void removeItem(Item item) {
+        inventory.remove(item);
+    }
+}
