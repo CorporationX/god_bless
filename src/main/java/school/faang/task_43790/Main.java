f8a3b7eb3cf74a072f375bd1e63def452587dce0
Roman Kondratev <romankondratev.com@gmail.com>
2024-12-13 22:59:20 -0800

    BJS2-43790 task: reverse - https://faang-school.com/courses/4jnzmndg/gnzgrbyd

--- src/main/java/school/faang/task_43790/Main.java
+++ src/main/java/school/faang/task_43790/Main.java
@@ -4,7 +4,6 @@ import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) {
-
         int[] numbers = {1, 2, 3, 4, 5};
 
         System.out.println("Original Array: " + Arrays.toString(numbers));
