3d7f569caf46a187327c571ab6a987851dcc8940
Artur Suleymanov <suleymanov.usa@gmail.com>
2024-12-14 15:31:54 -0800

    Lord of the Rings RPG

---
+++ src/main/java/school/faang/bjs245635/Item.java
@@ -0,0 +1,24 @@
+package school.faang.bjs245635;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+@Getter
+public class Item {
+    private final String name;
+    @Setter
+    private int value;
+
+    private final static Logger logger = LoggerFactory.getLogger(Item.class);
+
+    public Item(String name, int value) {
+        if (name == null || name.trim().isEmpty() || value < 0) {
+            logger.error("Invalid item name or value: {}, {} ", name, value);
+            throw new IllegalArgumentException("Invalid item name or value");
+        }
+        this.name = name;
+        this.value = value;
+    }
+}
