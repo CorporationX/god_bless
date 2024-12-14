79ac6c0cea2b04ebb9a46f975294a155d3a51f8c
ANaydyonov <alexander.naydenov@23software.net>
2024-12-14 22:55:02 +0100

    fixed comments

--- src/main/java/school/faang/task_45638/SpellAction.java
+++ src/main/java/school/faang/task_45638/SpellAction.java
@@ -1,5 +1,6 @@
 package school.faang.task_45638;
 
+@FunctionalInterface
 interface SpellAction {
     public String execute(String spell);
 }
--- src/main/java/school/faang/task_45638/SpellCaster.java
+++ src/main/java/school/faang/task_45638/SpellCaster.java
@@ -2,6 +2,12 @@ package school.faang.task_45638;
 
 public class SpellCaster {
     public void cast(String spell, SpellAction action) {
+        if (spell.trim().isEmpty()) {
+            throw new IllegalArgumentException("Spell cannot be empty");
+        }
+        if (action == null) {
+            throw new IllegalArgumentException("Spell handler cannot be NULL");
+        }
         String result = action.execute(spell);
         System.out.println(result);
     }
