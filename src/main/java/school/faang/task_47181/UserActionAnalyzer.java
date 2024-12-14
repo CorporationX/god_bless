a06d4915709ff486ea2b94dfa0ed7174cec2049a
Alfir Rakhimov <exprnc7@gmail.com>
2024-12-14 14:51:37 +0500

    Поправил

--- src/main/java/school/faang/task_47181/UserActionAnalyzer.java
+++ src/main/java/school/faang/task_47181/UserActionAnalyzer.java
@@ -4,9 +4,11 @@ import school.faang.task_47181.model.ActionType;
 import school.faang.task_47181.model.UserAction;
 
 import java.time.LocalDate;
-import java.util.Arrays;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 import java.util.stream.Collectors;
 
 public class UserActionAnalyzer {
@@ -21,11 +23,18 @@ public class UserActionAnalyzer {
     }
 
     public static List<String> topHashtags(List<UserAction> actions) {
+        Pattern hashtagPattern = Pattern.compile("#\\w+");
+
         return actions.stream()
                 .filter(action -> !action.getContent().isBlank())
-                .flatMap(action -> Arrays.stream(action.getContent().split(" ")))
-                .map(line -> line.replaceAll("[^a-zA-Z#]", ""))
-                .filter(line -> line.startsWith("#"))
+                .flatMap(action -> {
+                    Matcher matcher = hashtagPattern.matcher(action.getContent());
+                    List<String> hashtags = new ArrayList<>();
+                    while (matcher.find()) {
+                        hashtags.add(matcher.group());
+                    }
+                    return hashtags.stream();
+                })
                 .collect(Collectors.groupingBy(item -> item, Collectors.counting()))
                 .entrySet().stream()
                 .sorted(Map.Entry.<String, Long>comparingByValue().reversed())
@@ -36,11 +45,11 @@ public class UserActionAnalyzer {
 
     public static List<String> topCommentersLastMonth(List<UserAction> actions) {
         LocalDate now = LocalDate.now();
-        LocalDate oneMonthAgo = now.minusMonths(1);
+        LocalDate oneMonthAgo = now.minusMonths(1).minusDays(1);
 
         return actions.stream()
                 .filter(action -> action.getActionType() == ActionType.COMMENT
-                        && !action.getActionDate().isBefore(oneMonthAgo))
+                        && action.getActionDate().isAfter(oneMonthAgo))
                 .collect(Collectors.groupingBy(UserAction::getUserName, Collectors.counting()))
                 .entrySet().stream()
                 .sorted(Map.Entry.<String, Long>comparingByValue().reversed())
@@ -53,12 +62,10 @@ public class UserActionAnalyzer {
         Map<ActionType, Long> groupedActions = actions.stream()
                 .collect(Collectors.groupingBy(UserAction::getActionType, Collectors.counting()));
 
-        long actionsSize = groupedActions.values().stream().reduce(0L, Long::sum);
-
         return groupedActions.entrySet().stream()
                 .collect(Collectors.toMap(
                         Map.Entry::getKey,
-                        entry -> (double) (entry.getValue() * 100) / actionsSize
+                        entry -> (double) (entry.getValue() * 100) / actions.size()
                 ));
     }
 }
