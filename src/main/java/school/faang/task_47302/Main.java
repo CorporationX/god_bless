1a015b7f4c7e24f7b98dd3db49bd409a1ad16cc7
triplek <weribel@mail.ru>
2024-12-14 19:18:43 +0300

    Потренируем stream API 2

--- src/main/java/school/faang/task_47302/Main.java
+++ src/main/java/school/faang/task_47302/Main.java
@@ -9,25 +9,24 @@ public class Main {
     public static void main(String[] args) {
         TaskManager taskManager = new TaskManager();
         List<Integer> task1 = Arrays.asList(1, 2, 3, 4, 5, 6);
-        int targetNumber_task1 = 7;
+        int targetNumberTask1 = 7;
+        System.out.println(taskManager.findPairs(task1, targetNumberTask1));
 
         Map<String, String> task2 = new HashMap<>();
         task2.put("Russia", "Moscow");
         task2.put("USA", "Washington");
         task2.put("Germany", "Berlin");
+        System.out.println(taskManager.onlyCapitals(task2));
 
         List<String> task3 = Arrays.asList("apple", "banana", "avocado", "apricot");
-        char symbol_task3 = 'a';
+        char symbolTask3 = 'a';
+        System.out.println(taskManager.sortedByLengthAndSymbol(task3, symbolTask3));
 
         List<Integer> task4 = Arrays.asList(1, 2, 3, 4);
+        System.out.println(taskManager.numbersToBinaryFormat(task4));
 
         List<String> task5 = Arrays.asList("apple", "banana", "cherry", "date", "fig", "grape");
-        String alphabet_task5 = "abcdefghijklmnopqrstuvwxyz";
-
-        System.out.println(taskManager.findPairs(task1, targetNumber_task1));
-        System.out.println(taskManager.onlyCapitals(task2));
-        System.out.println(taskManager.sortedByLengthAndSymbol(task3, symbol_task3));
-        System.out.println(taskManager.numbersToBinaryFormat(task4));
-        System.out.println(taskManager.filterAndSortByLength(task5, alphabet_task5));
+        String alphabetTask5 = "abcdefghijklmnopqrstuvwxyz";
+        System.out.println(taskManager.filterAndSortByLength(task5, alphabetTask5));
     }
 }
