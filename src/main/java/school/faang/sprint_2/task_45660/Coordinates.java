3e6bc8fda89a066e579a7e4ecdbd6d16e60f85a5
alterality <rordufe@gmail.com>
2024-12-14 17:25:28 +0600

    Matrix transform

---
+++ src/main/java/school/faang/sprint_2/task_45660/Coordinates.java
@@ -0,0 +1,15 @@
+package school.faang.sprint_2.task_45660;
+
+import lombok.Getter;
+
+@Getter
+public class Coordinates {
+    int x;
+    int y;
+
+    public Coordinates(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+}
