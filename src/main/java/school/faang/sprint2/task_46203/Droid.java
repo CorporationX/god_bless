cef3bb7eac83290d7253a3a987d0c80f498926c2
DmitryQueen <276246@student.pwr.edu.pl>
2024-12-14 01:50:49 +0100

    added streams to encryption, removed duplicated code

--- src/main/java/school/faang/sprint2/task_46203/Droid.java
+++ src/main/java/school/faang/sprint2/task_46203/Droid.java
@@ -7,49 +7,19 @@ import lombok.extern.slf4j.Slf4j;
 @Slf4j
 @AllArgsConstructor
 public class Droid {
+    private static final DroidMessageService droidMessageService = new DroidMessageService();
+
     @NonNull
     private final String title;
 
-    public void sendMessage(Droid droid, @NonNull String message, int key) {
-        String encrypted = encryptMessage(message, key);
+    public void sendMessage(@NonNull Droid droid, @NonNull String message, int key) {
+        String encrypted = droidMessageService.encryptMessage(message, key);
         log.info("Droid: " + title + ". Sending encrypted message: " + encrypted);
-        droid.recieveMessage(encrypted, key);
+        droid.receiveMessage(encrypted, key);
     }
 
-    public void recieveMessage(@NonNull String message, int key) {
-        String decrypted = decryptMessage(message, key);
-        log.info("Droid: "  + title + ". Recieved decrypted message: " + decrypted);
-    }
-
-    private String encryptMessage(@NonNull String message, int key) {
-        DroidMessageEncryptor messageEncryptor = (m, k) -> {
-            StringBuilder encryptedMessage = new StringBuilder();
-            for (char c : m.toCharArray()) {
-                if (Character.isLetter(c)) {
-                    char base = Character.isLowerCase(c) ? 'a' : 'A';
-                    encryptedMessage.append((char) ((c - base + k) % 26 + base));
-                } else {
-                    encryptedMessage.append(c);
-                }
-            }
-            return encryptedMessage.toString();
-        };
-        return messageEncryptor.cryptor(message, key);
-    }
-
-    private String decryptMessage(@NonNull String message, int key) {
-        DroidMessageEncryptor messageEncryptor = (m, k) -> {
-            StringBuilder decryptedMessage = new StringBuilder();
-            for (char c : m.toCharArray()) {
-                if (Character.isLetter(c)) {
-                    char base = Character.isLowerCase(c) ? 'a' : 'A';
-                    decryptedMessage.append((char) ((c - base - k + 26) % 26 + base));
-                } else {
-                    decryptedMessage.append(c);
-                }
-            }
-            return decryptedMessage.toString();
-        };
-        return messageEncryptor.cryptor(message, key);
+    private void receiveMessage(String message, int key) {
+        String decrypted = droidMessageService.decryptMessage(message, key);
+        log.info("Droid: " + title + ". Received decrypted message: " + decrypted);
     }
 }
--- src/main/java/school/faang/sprint2/task_46203/Main.java
+++ src/main/java/school/faang/sprint2/task_46203/Main.java
@@ -2,19 +2,22 @@ package school.faang.sprint2.task_46203;
 
 public class Main {
     public static void main(String[] args) {
+        DroidMessageService messageService = new DroidMessageService();
+
         Droid r2d2 = new Droid("R2D2");
         Droid c3po = new Droid("C3PO");
         Droid bb8 = new Droid("BB-8");
 
         String message1 = "Join the Rebellion!";
         String message2 = "The Death Star is operational!";
+        String message3 = "The mission is complete.";
         int encryptionKey1 = 3;
         int encryptionKey2 = 7;
 
         r2d2.sendMessage(c3po, message1, encryptionKey1);
-
         c3po.sendMessage(r2d2, message2, encryptionKey2);
-
-        bb8.sendMessage(c3po, "The mission is complete.", encryptionKey1);
+        bb8.sendMessage(c3po, message3, encryptionKey1);
     }
+
 }
+
