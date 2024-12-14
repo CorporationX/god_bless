cdbca9e6c8c202ea8cc95ffc9c4a42cc3a6cdde6
Ilya Vysokolyan <rd2d@list.ru>
2024-12-15 01:27:16 +0300

    BJS2-47484: Потренируем StreamAPI 3

---
+++ src/main/java/school/faang/task_47484/Employee.java
@@ -0,0 +1,14 @@
+package school.faang.task_47484;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class Employee {
+
+    private String name;
+    private int salary;
+    private String department;
+
+}
