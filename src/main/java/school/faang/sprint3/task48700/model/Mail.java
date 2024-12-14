8ddd46ba4f33d1bd34fbd3699af824ca95d39f37
Alexey Gorokhov <almigor@mail.ru>
2024-12-14 18:34:12 +0300

    Вы работаете в Microsoft!

---
+++ src/main/java/school/faang/sprint3/task48700/model/Mail.java
@@ -0,0 +1,16 @@
+package school.faang.sprint3.task48700.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.ToString;
+
+@AllArgsConstructor
+@Getter
+@ToString
+public class Mail {
+    private final String sendTo;
+    private final String sendFrom;
+    private final String Subject;
+    private final String Body;
+
+}
