fa0ca525be3c663dbf1ceb8c5b1ee7ed723698f8
Maria Bortsova <bortsovamariia@gmail.com>
2024-12-14 22:46:05 +0300

    refactor after review

--- src/main/java/school/faang/task_46528/DroidMessageEncryptor.java
+++ src/main/java/school/faang/task_46528/DroidMessageEncryptor.java
@@ -2,5 +2,5 @@ package school.faang.task_46528;
 
 @FunctionalInterface
 public interface DroidMessageEncryptor {
-    String encrypt(String message, int keyEncrypt);
+    String encrypt(String message, int key);
 }
