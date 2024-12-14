b04eac7e1f654673a924a7333622d762e3b4ace2
Ivan Baev <ibai86@yandex.ru>
2024-12-14 15:12:08 +0300

    ready to review

--- src/main/java/school/faang/streamapi_one/ListOperations.java
+++ src/main/java/school/faang/streamapi_one/ListOperations.java
@@ -1,21 +1,64 @@
 package school.faang.streamapi_one;
 
+import java.util.Comparator;
 import java.util.List;
 import java.util.NoSuchElementException;
-import java.util.Optional;
+import java.util.function.Predicate;
 
 public class ListOperations {
     public static int sumOfEvenNumbers(List<Integer> list) {
-        int sum = list.stream()
+        return list.stream()
                 .filter(num -> num % 2 == 0)
                 .mapToInt(Integer::intValue)
                 .sum();
-        return sum;
     }
 
     public static int findMax(List<Integer> list) {
         return list.stream()
                 .max(Integer::compareTo)
-                .orElseThrow(() -> new NoSuchElementException("LIst is empty"));
+                .orElseThrow(() -> new NoSuchElementException("List is empty"));
+    }
+
+    public static double findAverage(List<Integer> list) {
+        return list.stream()
+                .mapToInt(Integer::intValue)
+                .average()
+                .orElse(0.0);
+    }
+
+    public static int countStringsStartingWith(List<String> list, char symbol) {
+        return Math.toIntExact(list.stream()
+                .filter(str -> str.charAt(0) == symbol)
+                .count());
+    }
+
+    public static List<String> filterStringsContainingSubstring(List<String> list, String targetString) {
+        return list.stream()
+                .filter(s -> s.contains(targetString))
+                .toList();
+    }
+
+    public static List<String> sortByLength(List<String> list) {
+        return list.stream()
+                .sorted(Comparator.comparingInt(String::length))
+                .toList();
+    }
+
+    public static boolean allMatchCondition(List<Integer> list, Predicate<Integer> condition) {
+        return list.stream()
+                .allMatch(condition);
+    }
+
+    public static int findMinGreaterThan(List<Integer> list, int thresholdValue) {
+        return list.stream()
+                .filter(n -> n > thresholdValue)
+                .min(Integer::compareTo)
+                .orElseThrow(() -> new NoSuchElementException("No elements by condition"));
+    }
+
+    public static List<Integer> convertToLengths(List<String> list) {
+        return list.stream()
+                .map(String::length)
+                .toList();
     }
 }
--- src/main/java/school/faang/streamapi_one/Main.java
+++ src/main/java/school/faang/streamapi_one/Main.java
@@ -6,9 +6,18 @@ import java.util.List;
 public class Main {
     public static void main(String[] args) {
         List<Integer> numbers = Arrays.asList(1, 2, 3, 4, 5, 6);
-        List<String> strings = Arrays.asList("apple", "banana", "cherry", "date");
+        List<String> strings = Arrays.asList("apple", "banana", "cherry", "date", "airplane", "bug");
 
-        // Пример использования методов:
-        System.out.println("Сумма четных чисел: " + ListOperations.sumOfEvenNumbers(numbers));
+        System.out.println("Sum of even numbers: " + ListOperations.sumOfEvenNumbers(numbers));
+        System.out.println("Maximum: " + ListOperations.findMax(numbers));
+        System.out.println("Average: " + ListOperations.findAverage(numbers));
+        System.out.println("Amount of string beginning from 'a': "
+                            + ListOperations.countStringsStartingWith(strings, 'a'));
+        System.out.println("Only strings including 'pl': "
+                            + ListOperations.filterStringsContainingSubstring(strings, "pl"));
+        System.out.println("Strings sorted by length: " + ListOperations.sortByLength(strings));
+        System.out.println("Are all numbers is even? " + ListOperations.allMatchCondition(numbers, n -> n % 2 == 0));
+        System.out.println("Minimum number is greater than 4: " + ListOperations.findMinGreaterThan(numbers, 4));
+        System.out.println("Length of strings: " + ListOperations.convertToLengths(strings));
     }
 }
