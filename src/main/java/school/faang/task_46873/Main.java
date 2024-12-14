058a8cff44a1086d0454cd3a456a46a3216cf140
Vadim Ovcharenko <vadimovcharenko12@gmail.com>
2024-12-14 14:29:57 +0300

    Выполнил задачу BJS2-46873

---
+++ src/main/java/school/faang/task_46873/Main.java
@@ -0,0 +1,39 @@
+package school.faang.task_46873;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) {
+        List<Integer> numbers = Arrays.asList(1, 2, 3, 4, 5, 6);
+        List<String> strings = Arrays.asList("apple", "banana", "cherry", "date");
+
+        // Пример использования методов:
+        System.out.println("Сумма четных чисел: "
+                + WorkingWithStream.sumOfEvenNumbers(numbers));
+
+        System.out.println("Максимум: "
+                + WorkingWithStream.findMax(numbers));
+
+        System.out.println("Среднее: "
+                + WorkingWithStream.findAverage(numbers));
+
+        System.out.println("Количество строк, начинающихся на 'a': "
+                + WorkingWithStream.countStringsStartingWith(strings, 'a'));
+
+        System.out.println("Строки, содержащие 'an': "
+                + WorkingWithStream.filterStringsContainingSubstring(strings, "an"));
+
+        System.out.println("Отсортированные по длине строки: "
+                + WorkingWithStream.sortByLength(strings));
+
+        System.out.println("Все ли числа чётные? "
+                + WorkingWithStream.allMatchCondition(numbers, n -> n % 2 == 0));
+
+        System.out.println("Наименьшее число больше 4: "
+                + WorkingWithStream.findMinGreaterThan(numbers, 4));
+
+        System.out.println("Длины строк: "
+                + WorkingWithStream.convertToLengths(strings));
+    }
+}
