ca1e29377cd24f1ec80829b2b780da5d522db706
Ivan Baev <ibai86@yandex.ru>
2024-12-14 22:22:42 +0300

    add second method

--- src/main/java/school/faang/streamapi_two/Main.java
+++ src/main/java/school/faang/streamapi_two/Main.java
@@ -2,12 +2,17 @@ package school.faang.streamapi_two;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.Map;
 
 public class Main {
     public static void main(String[] args) {
         List<Integer> numbers = Arrays.asList(1, 2, 3, 4, 2, 3, 5, 6);
+        Map<String, String> countries = Map.of("Russia", "Moscow",
+                                                "USA", "Washington",
+                                                "Germany","Berlin");
 
         SolutionMethods solutionMethods = new SolutionMethods();
         System.out.println(solutionMethods.findUniquePairs(numbers, 7));
+        System.out.println(solutionMethods.sortCapitalByAlphabet(countries));
     }
 }
--- src/main/java/school/faang/streamapi_two/SolutionMethods.java
+++ src/main/java/school/faang/streamapi_two/SolutionMethods.java
@@ -1,25 +1,34 @@
 package school.faang.streamapi_two;
 
 import java.util.Arrays;
-import java.util.Collection;
 import java.util.Collections;
 import java.util.HashSet;
 import java.util.List;
+import java.util.Map;
 import java.util.Set;
 import java.util.stream.Collectors;
 
 public class SolutionMethods {
     public Set<List<Integer>> findUniquePairs(List<Integer> list, int sumOfPair) {
-        Set<List<Integer>> foundPairs;
+        Set<List<Integer>> foundPairs = new HashSet<>();
         Set<Integer> inputList = new HashSet<>();
 
         inputList.addAll(list);
         foundPairs = inputList.stream()
                 .filter(number -> inputList.contains(sumOfPair - number))
-                .peek(System.out::println)
                 .map(number -> Arrays.asList(number, sumOfPair - number))
                 .peek(Collections::sort)
                 .collect(Collectors.toSet());
         return foundPairs;
     }
+
+    public List<String> sortCapitalByAlphabet(Map<String, String> countries) {
+        return countries.entrySet().stream()
+                .sorted(Map.Entry.comparingByKey())
+                .map(Map.Entry::getValue)
+                .toList();
+    }
+
+
+
 }
