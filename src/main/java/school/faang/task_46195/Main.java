7067b04cfd0f5acfcf4d0138b9e7fe09ea65484f
Sergey Senin <04sersen@gmail.com>
2024-12-15 05:10:12 +0300

    Убрал двойную обработку ошибок в errorHandler.

--- src/main/java/school/faang/task_46195/Main.java
+++ src/main/java/school/faang/task_46195/Main.java
@@ -3,25 +3,19 @@ package school.faang.task_46195;
 public class Main {
     public static void main(String[] args) {
         String result = ErrorHandler.withErrorHandling(
-                () -> {
-                    try {
-                        return RemoteService.call("someParam");
-                    } catch (Exception e) {
-                        throw new RuntimeException(e);
-                    }
-                },
-                e -> {
-                    System.out.println("Error occurred during the remote service call, returning default value.");
+                () -> RemoteService.call("someParam"),
+                exception -> {
+                    System.out.println("Error occurred: " + exception.getMessage());
                     return "DEFAULT";
                 }
         );
 
         System.out.println(result);
     }
+}
 
-    public static class RemoteService {
-        public static String call(String param) throws Exception {
-            throw new Exception("Service is unavailable");
-        }
+class RemoteService {
+    public static String call(String param) {
+        throw new RuntimeException("Service is unavailable");
     }
 }
