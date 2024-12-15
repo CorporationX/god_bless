cd60e6d401f3a4db4642484085d36731ac29fa68
Naidonov <alexkid2001@rambler.ru>
2024-12-15 14:44:34 -0330

    fixed check

--- src/main/java/school/faang/tast_46611/Main.java
+++ src/main/java/school/faang/tast_46611/Main.java
@@ -8,18 +8,20 @@ public class Main {
 
         FilterProcessor filterProcessor = new FilterProcessor();
 
-// Фильтры
-        Function<Image, Image> grayscaleFilter = (image) -> new Image(image.getName(), image.getDescription() + " | Фильтр: черно-белый");
-        Function<Image, Image> sepiaFilter = (image) -> new Image(image.getName(), image.getDescription() + " | Фильтр: сепия");
+        // Фильтры
+        Function<Image, Image> grayscaleFilter = (image) -> new Image(image.getName(),
+                image.getDescription() + " | Фильтр: черно-белый");
+        Function<Image, Image> sepiaFilter = (image) -> new Image(image.getName(),
+                image.getDescription() + " | Фильтр: сепия");
 
-// Применение фильтров
+        // Применение фильтров
         Image grayscaleImage = filterProcessor.applyFilter(originalImage, grayscaleFilter);
         System.out.println(grayscaleImage.getDescription()); // Вывод: Оригинальное изображение | Фильтр: черно-белый
 
         Image sepiaImage = filterProcessor.applyFilter(grayscaleImage, sepiaFilter);
         System.out.println(sepiaImage.getDescription()); // Вывод: Оригинальное изображение | Фильтр: черно-белый | Фильтр: сепия
 
-// Комбинирование фильтров
+        // Комбинирование фильтров
         Function<Image, Image> combinedFilter = filterProcessor.combineFilters(grayscaleFilter, sepiaFilter);
         Image combinedImage = filterProcessor.applyFilter(originalImage, combinedFilter);
         System.out.println(combinedImage.getDescription()); // Вывод: Оригинальное изображение | Фильтр: черно-белый | Фильтр: сепия
