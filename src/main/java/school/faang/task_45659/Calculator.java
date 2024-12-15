79527916cd938dcd44751cbca8051b895ce4da0f
Ilya Vysokolyan <rd2d@list.ru>
2024-12-16 00:30:03 +0300

    BJS2-45659: Калькулятор

---
+++ src/main/java/school/faang/task_45659/Calculator.java
@@ -0,0 +1,5 @@
+package school.faang.task_45659;
+
+public interface Calculator {
+    int operate(int a, int b);
+}
---
+++ src/main/java/school/faang/task_45659/CalculatorUtils.java
@@ -0,0 +1,27 @@
+package school.faang.task_45659;
+
+import java.util.List;
+
+public class CalculatorUtils {
+    public static int calculate(List<Integer> nums, Calculator calculator) throws IllegalAccessException {
+        if (nums == null || nums.isEmpty()) {
+            throw new IllegalArgumentException("Список не должен быть пустым");
+        }
+        int result = nums.get(0);
+
+        for (int i = 1; i < nums.size(); i++) {
+            result = calculator.operate(result, nums.get(i));
+        }
+        return result;
+    }
+
+    public static int product(List<Integer> nums) throws IllegalAccessException {
+        return calculate(nums, (a, b) -> a * b);
+    }
+
+    public static int sum(List<Integer> nums) throws IllegalAccessException {
+        return calculate(nums, (a, b) -> a + b);
+    }
+
+
+}
