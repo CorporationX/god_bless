2fc9a5ab1f5c5fc825b83d9db9f2a9c83e34dd14
Pavel <45390423+rsticks@users.noreply.github.com>
2024-12-16 01:16:31 +0300

    BJS2-47027 Потренируем StreamAPI 1

--- src/main/java/school/faang/bjs247027/Utilities.java
+++ src/main/java/school/faang/bjs247027/Utilities.java
@@ -1,22 +1,64 @@
 package school.faang.bjs247027;
 
-import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
-import java.util.Optional;
+import java.util.NoSuchElementException;
+import java.util.function.Predicate;
+import java.util.stream.Collectors;
+
 
 public class Utilities {
     public static int sumOfEvenNumbers(List<Integer> numbers) {
-        if (numbers == null) {
-            return 0;
-        }
-
-        return numbers.stream().filter(number -> number % 2 == 0).reduce(0, Integer::sum);
+        return numbers.stream()
+                .filter(number -> number % 2 == 0)
+                .reduce(0, Integer::sum);
     }
 
-    public static int findMax(List<Integer> numbers) {
-        if (numbers == null) {
-            return 0;
-        }
-        return numbers.stream().max(Integer::compare);
+    public static Integer findMax(List<Integer> numbers) {
+        return numbers.stream()
+                .max(Integer::compare)
+                .orElseThrow(NoSuchElementException::new);
+    }
+
+    public static Double findAverage(List<Integer> numbers) {
+        return numbers.stream()
+                .mapToInt(number -> number)
+                .average()
+                .orElseThrow(NoSuchElementException::new);
+    }
+
+    public static long countStringsStartingWith(List<String> strings, char ch) {
+        return strings.stream()
+                .filter(string -> string.startsWith(String.valueOf(ch)))
+                .count();
+    }
+
+    public static List<String> filterStringsContainingSubstring(List<String> strings, String substring) {
+        return strings.stream()
+                .filter(string -> string.contains(substring))
+                .toList();
+    }
+
+    public static List<String> sortByLength(List<String> strings) {
+        return strings.stream()
+                .sorted(Comparator.comparingInt(String::length))
+                .toList();
+    }
+
+    public static boolean allMatchCondition(List<Integer> integers, Predicate<Integer> predicate) {
+        return integers.stream().allMatch(predicate);
+    }
+
+    public static int findMinGreaterThan(List<Integer> integers, int target) {
+        return integers.stream()
+                .filter(integer -> integer > target)
+                .findFirst()
+                .orElseThrow(NoSuchElementException::new);
+    }
+
+    public static List<Integer> convertToLengths(List<String> strings) {
+        return strings.stream()
+                .map(String::length)
+                .collect(Collectors.toList());
     }
 }
