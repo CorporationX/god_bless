d05427101fa352ee9abea9ee76e5c0949cbad89f
rostex <rostex2@yandex.ru>
2024-12-16 00:09:50 +0300

    Создан сервис расчета силы армии героев в отдельных потоках

---
+++ src/main/java/school/faang/sprint_3/task_47707/Archer.java
@@ -0,0 +1,7 @@
+package school.faang.sprint_3.task_47707;
+
+public class Archer extends Unit {
+    public Archer(int power) {
+        super(power);
+    }
+}
---
+++ src/main/java/school/faang/sprint_3/task_47707/Mage.java
@@ -0,0 +1,7 @@
+package school.faang.sprint_3.task_47707;
+
+public class Mage extends Unit {
+    public Mage(int power) {
+        super(power);
+    }
+}
