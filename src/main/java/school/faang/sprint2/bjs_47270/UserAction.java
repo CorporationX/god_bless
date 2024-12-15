4a1ba70cda6559f30d4d855cf5a97134301a61cc
Artem Kopanev <artemkopanev757@gmail.com>
2024-12-15 20:44:27 +0300

    BJS2-47270

--- src/main/java/school/faang/sprint2/bjs_47270/UserAction.java
+++ src/main/java/school/faang/sprint2/bjs_47270/UserAction.java
@@ -1,5 +1,5 @@
 package school.faang.sprint2.bjs_47270;
 
-import java.util.Date;
+import java.time.LocalDate;
 
-public record UserAction(int id, String name, String actionType, Date actiondDate, String content) {}
+public record UserAction(int id, String name, String actionType, LocalDate actionDate, String content) {}
--- src/main/java/school/faang/sprint2/bjs_47270/UserActionAnalyzer.java
+++ src/main/java/school/faang/sprint2/bjs_47270/UserActionAnalyzer.java
@@ -1,8 +1,10 @@
 package school.faang.sprint2.bjs_47270;
 
-import java.util.Comparator;
+import java.time.LocalDate;
+import java.util.Arrays;
 import java.util.List;
 import java.util.Map;
+import java.util.function.Function;
 import java.util.stream.Collectors;
 
 public class UserActionAnalyzer {
@@ -17,7 +19,46 @@ public class UserActionAnalyzer {
                 .collect(Collectors.toList());
     }
 
-    public List<String> topPopularHashtags(List<UserAction> userActions) {
-        return userActions.stream();
+    public static List<String> topPopularHashtags(List<UserAction> userActions) {
+        Map<String, Long> hashtags = userActions.stream()
+                .filter(x -> x.content() != null && x.actionType().equals("post") || x.actionType().equals("comment"))
+                .flatMap(x -> Arrays.stream(x.content().split("[ ,.;!]+")))
+                .filter(x -> x.startsWith("#"))
+                .collect(Collectors.groupingBy(Function.identity(), Collectors.counting()));
+
+        return hashtags.entrySet().stream()
+                .sorted(Map.Entry.<String, Long>comparingByValue().reversed())
+                .limit(5)
+                .map(Map.Entry::getKey)
+                .collect(Collectors.toList());
+    }
+
+    public static List<String> topCommentersLastMonth(List<UserAction> userActions) {
+        LocalDate lastMonth = LocalDate.now().minusMonths(1);
+
+        Map<String, Long> users = userActions.stream()
+                .filter(x -> x.actionType().equals("comment") && x.actionDate().isAfter(lastMonth))
+                .collect(Collectors.groupingBy(UserAction::name, Collectors.counting()));
+
+        return users.entrySet().stream()
+                .sorted(Map.Entry.<String, Long>comparingByValue().reversed())
+                .limit(3)
+                .map(Map.Entry::getKey)
+                .collect(Collectors.toList());
+    }
+
+    public static Map<String, Double> actionTypePercentages(List<UserAction> userActions) {
+        Map<String, Long> actionTypes = userActions.stream()
+                .collect(Collectors.groupingBy(UserAction::actionType, Collectors.counting()));
+
+        int generalSumActionTypes = actionTypes.values().stream()
+                .mapToInt(Long::intValue)
+                .sum();
+
+        return actionTypes.entrySet().stream()
+                .collect(Collectors.toMap(
+                        k -> k.getKey(),
+                        v -> (v.getValue() * 100.0 / generalSumActionTypes)
+                ));
     }
 }
