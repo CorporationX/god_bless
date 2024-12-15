48477124578862d6d7475c183617082ec3b2c881
Roman Shevchuk <reacount1393@gmail.com>
2024-12-15 15:01:51 +0300

    Комбинация фильтров работает

---
+++ src/main/java/school/faang/sprint2/task_46463/Image.java
@@ -0,0 +1,13 @@
+package school.faang.sprint2.task_46463;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@Setter
+@Getter
+@AllArgsConstructor
+public class Image {
+    private String name;
+    private String description;
+}
