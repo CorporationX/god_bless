43bdc7d1d6e46b798ad48a10a0d13cad3efcb120
lehaps <awda.96@bk.ru>
2024-12-15 22:20:52 +0300

    Исправил замечания

--- src/main/java/school/faang/task47756/FoodDeliveryTask.java
+++ src/main/java/school/faang/task47756/FoodDeliveryTask.java
@@ -1,12 +1,16 @@
 package school.faang.task47756;
 
 import lombok.AllArgsConstructor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.util.Random;
 import java.util.concurrent.TimeUnit;
 
 @AllArgsConstructor
 public class FoodDeliveryTask implements Runnable {
+    private final Logger logger = LoggerFactory.getLogger(FoodDeliveryTask.class);
+
     private static final int TIME_SLEEP = 10;
 
     private final String character;
@@ -22,11 +26,14 @@ public class FoodDeliveryTask implements Runnable {
         String food = getFoodType();
 
         System.out.printf("%s receives %d %s%n", character, foodAmount, food);
+
         try {
             TimeUnit.SECONDS.sleep(TIME_SLEEP);
         } catch (InterruptedException e) {
-            throw new RuntimeException(e);
+            logger.error(e.getMessage());
+            return;
         }
+
         System.out.printf("%s eat %d %s%n", character, foodAmount, food);
     }
 }
--- src/main/java/school/faang/task47756/GriffinFoodDelivery.java
+++ src/main/java/school/faang/task47756/GriffinFoodDelivery.java
@@ -1,8 +1,9 @@
 package school.faang.task47756;
 
-import java.util.ArrayList;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import java.util.Arrays;
-import java.util.List;
 import java.util.Random;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
@@ -10,6 +11,7 @@ import java.util.concurrent.TimeUnit;
 
 public class GriffinFoodDelivery {
     private static final int THREAD_POOL_SIZE = 3;
+    private static final Logger LOGGER = LoggerFactory.getLogger(GriffinFoodDelivery.class);
 
     public static void main(String[] args) {
         ExecutorService executorService = Executors.newFixedThreadPool(THREAD_POOL_SIZE);
@@ -26,8 +28,7 @@ public class GriffinFoodDelivery {
             executorService.awaitTermination(Long.MAX_VALUE, TimeUnit.SECONDS);
             System.out.println("All eats");
         } catch (InterruptedException e) {
-            throw new RuntimeException(e);
+            LOGGER.error(e.getMessage());
         }
-
     }
 }
