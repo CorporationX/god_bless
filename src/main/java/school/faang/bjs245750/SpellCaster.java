d545f986e0ad89087fc1f24578e6161967d0cf4e
Maksim-VV <laulmv11@gmail.com>
2024-12-15 14:53:54 +0300

    BJS2-46944, Meta-universe - added a null check

--- src/main/java/school/faang/bjs245750/SpellCaster.java
+++ src/main/java/school/faang/bjs245750/SpellCaster.java
@@ -2,7 +2,11 @@ package school.faang.bjs245750;
 
 public class SpellCaster {
     public void cast(String spellName, SpellAction action) {
-        String result = action.castSpell(spellName);
-        System.out.println("Результат заклинания: " + result);
+        try {
+            String result = action.castSpell(spellName);
+            System.out.println("Результат заклинания: " + result);
+        } catch (NullPointerException ex) {
+            System.out.println("The action cannot be null!!!");
+        }
     }
 }
