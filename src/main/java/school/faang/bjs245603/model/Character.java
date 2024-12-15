523c2702178869947ea69cd6fa66560d3aadfb28
Artemon <artemon.kos@yandex.ru>
2024-12-15 16:49:04 +0100

    v2

--- src/main/java/school/faang/bjs245603/model/Character.java
+++ src/main/java/school/faang/bjs245603/model/Character.java
@@ -1,20 +1,16 @@
 package school.faang.bjs245603.model;
 
-import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
-import lombok.Setter;
 import lombok.ToString;
 
 import java.util.ArrayList;
 import java.util.List;
 
 @RequiredArgsConstructor
-@EqualsAndHashCode
 @ToString
 @Getter
-@Setter
 public class Character {
     private final String name;
-    private List<Item> inventory = new ArrayList<>();
+    private final List<Item> inventory = new ArrayList<>();
 }
--- src/main/java/school/faang/bjs245603/service/InventoryManager.java
+++ src/main/java/school/faang/bjs245603/service/InventoryManager.java
@@ -8,6 +8,7 @@ import java.util.List;
 import java.util.function.Consumer;
 import java.util.function.Function;
 import java.util.function.Predicate;
+import java.util.stream.Stream;
 
 @AllArgsConstructor
 public class InventoryManager {
@@ -21,10 +22,10 @@ public class InventoryManager {
     }
 
     public static void update(Character character, Predicate<Item> filter, Function<Item, Item> function) {
-        List<Item> inventory = character.getInventory().stream()
-                .filter(filter)
-                .map(function)
-                .toList();
-        character.setInventory(inventory);
+        List<Item> inventory = character.getInventory();
+        Stream<Item> changedItems = inventory.stream().filter(filter).map(function);
+        Stream<Item> otherItems = inventory.stream().filter(filter.negate());
+        inventory.clear();
+        inventory.addAll(Stream.concat(changedItems, otherItems).toList());
     }
 }
