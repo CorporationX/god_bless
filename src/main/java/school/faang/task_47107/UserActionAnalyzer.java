259ff9e0a066f04e1836c2590e23b433847cc163
Eugene <@evgeniy_kim>
2024-12-15 12:04:20 +0500

    update

--- src/main/java/school/faang/task_47107/UserActionAnalyzer.java
+++ src/main/java/school/faang/task_47107/UserActionAnalyzer.java
@@ -1,9 +1,14 @@
 package school.faang.task_47107;
 
+import school.faang.exception.CheckException;
+
 import java.util.List;
 
 public class UserActionAnalyzer {
-    public static List<String> topActiveUsers(List<UserAction> axtions) {
-
+    public static List<String> topActiveUsers(List<UserAction> actions) {
+        if (actions == null) {
+            throw new CheckException("actions");
+        }
+        return actions.stream().
     }
 }
