919db737d960c460d5b31bfd2581ba18532e447d
narol01 <kmingogod@gmail.com>
2024-12-15 16:28:55 +0100

    BJS2-47775

---
+++ src/main/java/school/faang/task_47775/Warrior.java
@@ -0,0 +1,12 @@
+package school.faang.task_47775;
+
+import lombok.Getter;
+
+@Getter
+public class Warrior {
+    private final int power;
+
+    public Warrior(int power) {
+        this.power = power;
+    }
+}
