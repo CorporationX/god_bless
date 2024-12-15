b078b93638a6284c0a3f6ec79483767becfbbd53
Arseni Zaytsau <zarseniy280@gmail.com>
2024-12-15 12:26:18 +0300

    ztsv-BJS2-45629 - handled null-value action

--- src/main/java/school/faang/bjs45629/SpellCaster.java
+++ src/main/java/school/faang/bjs45629/SpellCaster.java
@@ -2,6 +2,9 @@ package school.faang.bjs45629;
 
 public class SpellCaster {
     public void cast(String name, SpellAction action) {
+        if (action == null) {
+            return;
+        }
         System.out.println(action.castSpell(name));
     }
 }
