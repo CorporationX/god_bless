1a7692086bb2ff3bcc011db663d7ba1e64df3bf7
alterality <rordufe@gmail.com>
2024-12-15 22:11:18 +0600

    update code

--- src/main/java/school/faang/sprint_2/task_45935/EmailProcessor.java
+++ src/main/java/school/faang/sprint_2/task_45935/EmailProcessor.java
@@ -4,21 +4,21 @@ import java.util.List;
 import java.util.function.Consumer;
 import java.util.function.Function;
 import java.util.function.Predicate;
+import java.util.Objects;
 
 public class EmailProcessor {
     public void processEmails(List<Email> emails,
                               Predicate<Email> filter,
                               Consumer<Email> processor,
                               Function<Email, String> transformer) {
-        for (Email email : emails) {
+        Objects.requireNonNull(emails, "Emails dont null");
+        Objects.requireNonNull(filter, "Filter dont null");
+        Objects.requireNonNull(processor, "Processor dont null");
+        Objects.requireNonNull(transformer, "Transformer dont null");
 
-            if (filter.test(email)) {
-
-                String transformedBody = transformer.apply(email);
-                email.setBody(transformedBody);
-
-                processor.accept(email);
-            }
-        }
+        emails.stream()
+                .filter(filter)
+                .peek(email -> email.setBody(transformer.apply(email)))
+                .forEach(processor);
     }
 }
