cfac751badbffc812c77122b1ea15e4e2c470e9e
Arseni Zaytsau <zarseniy280@gmail.com>
2024-12-15 12:12:27 +0300

    ztsv-BJS2-46858 - created classes according to the task

--- src/main/java/school/faang/Main.java
+++ src/main/java/school/faang/Main.java
@@ -1,4 +1,22 @@
 package school.faang;
 
+import school.faang.bjs46858.Notification;
+import school.faang.bjs46858.NotificationManager;
+
 public class Main {
+    public static void main(String[] args) {
+        NotificationManager notificationManager = new NotificationManager();
+        notificationManager.addValidation((notification) -> !notification.getMessage().contains("негр"));
+
+        notificationManager.registerHandler("Phone notification",
+                notification -> System.out.println("Message \"" + notification.getMessage() + "\" is sent to phone"));
+
+        Notification notification = new Notification("Phone notification", "Прив негр");
+        try {
+            notificationManager.sendNotification(notification);
+        } catch (IllegalArgumentException e) {
+            notification.setMessage("Прив афроамериканец");
+            notificationManager.sendNotification(notification);
+        }
+    }
 }
