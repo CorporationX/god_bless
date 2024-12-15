63ef1f5e62eca0f54ff5a5b9b6fca3acc6b114a9
Roman Shevchuk <reacount1393@gmail.com>
2024-12-15 20:58:38 +0300

    Чекстайл меня однажды доведет :)

--- src/main/java/school/faang/sprint2/task_3/FilterProcessor.java
+++ src/main/java/school/faang/sprint2/task_3/FilterProcessor.java
@@ -6,10 +6,12 @@ import java.util.function.Function;
 
 
 public class FilterProcessor {
+    
     @NonNull
     public Image applyFilter(Image image, Function<Image, Image> filter) {
         return filter.apply(image);
     }
+
     @NonNull
     public Function<Image, Image> combineFilters(Function<Image, Image> filter1, Function<Image, Image> filter2) {
         return filter1.andThen(filter2);
