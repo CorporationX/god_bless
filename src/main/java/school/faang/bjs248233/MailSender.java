63025d1b52a009a5f55d01ab5eedb708e7fd6bec
Leonid Zhuravel <zhuravel_leonid@yahoo.com>
2024-12-16 20:56:14 +0200

    PR-ready

---
+++ src/main/java/school/faang/bjs248233/MailSender.java
@@ -0,0 +1,20 @@
+package school.faang.bjs248233;
+
+public class MailSender {
+    public static void main(String[] args) {
+
+        Thread[] threads = new Thread[5];
+        for (int i = 0; i < 5; i++) {
+            threads[i] = new Thread(new SenderRunnable(i * 200, (i + 1) * 200));
+            threads[i].start();
+        }
+
+        for (Thread thread : threads) {
+            try {
+                thread.join();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
