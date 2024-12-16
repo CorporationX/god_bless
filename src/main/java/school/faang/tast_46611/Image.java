b5798767598542348e5018f644d83442d9a6eb9c
Naidonov <alexkid2001@rambler.ru>
2024-12-16 17:58:37 -0330

    used record class, used andThen

--- src/main/java/school/faang/tast_46611/Image.java
+++ src/main/java/school/faang/tast_46611/Image.java
@@ -2,15 +2,4 @@ package school.faang.tast_46611;
 
 import lombok.Getter;
 
-public class Image {
-    @Getter private String name;
-    @Getter private String description;
-
-    public Image(String name, String description) {
-        if (name.trim().isEmpty() || description.trim().isEmpty()) {
-            System.out.println("Name and Description cannot be empty");
-        }
-        this.name = name;
-        this.description = description;
-    }
-}
+public record Image(String name, String description) {}
--- src/main/java/school/faang/tast_46611/Main.java
+++ src/main/java/school/faang/tast_46611/Main.java
@@ -8,19 +8,19 @@ public class Main {
 
         FilterProcessor filterProcessor = new FilterProcessor();
 
-        Function<Image, Image> grayscaleFilter = (image) -> new Image(image.getName(),
-                image.getDescription() + " | Фильтр: черно-белый");
-        Function<Image, Image> sepiaFilter = (image) -> new Image(image.getName(),
-                image.getDescription() + " | Фильтр: сепия");
+        Function<Image, Image> grayscaleFilter = (image) -> new Image(image.name(),
+                image.description() + " | Фильтр: черно-белый");
+        Function<Image, Image> sepiaFilter = (image) -> new Image(image.name(),
+                image.description() + " | Фильтр: сепия");
 
         Image grayscaleImage = filterProcessor.applyFilter(originalImage, grayscaleFilter);
-        System.out.println(grayscaleImage.getDescription());
+        System.out.println(grayscaleImage.description());
 
         Image sepiaImage = filterProcessor.applyFilter(grayscaleImage, sepiaFilter);
-        System.out.println(sepiaImage.getDescription());
+        System.out.println(sepiaImage.description());
 
         Function<Image, Image> combinedFilter = filterProcessor.combineFilters(grayscaleFilter, sepiaFilter);
         Image combinedImage = filterProcessor.applyFilter(originalImage, combinedFilter);
-        System.out.println(combinedImage.getDescription());
+        System.out.println(combinedImage.description());
     }
 }
