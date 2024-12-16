8a6e30c96b018cc4a15b1dd23b08b483b5fddfef
AesaQ <ilyaovchinnikov2095@gmail.com>
2024-12-16 21:05:47 +0500

    checkstyle fix

--- src/main/java/school/faang/extremely_poor/WeasleyFamily.java
+++ src/main/java/school/faang/extremely_poor/WeasleyFamily.java
@@ -7,13 +7,16 @@ import java.util.concurrent.Executors;
 
 public class WeasleyFamily {
     private static final List<Chore> chores = new ArrayList<>();
+
     public static void main(String[] args) {
-        for(int i = 0; i < 10; i++) {
+        for (int i = 0; i < 10; i++) {
             chores.add(new Chore("Washing " + i));
             chores.add(new Chore("Clearing " + i));
             chores.add(new Chore("Drying " + i));
         }
+
         ExecutorService threadPool = Executors.newCachedThreadPool();
+
         for (Chore chore : chores) {
             threadPool.submit(chore);
         }
