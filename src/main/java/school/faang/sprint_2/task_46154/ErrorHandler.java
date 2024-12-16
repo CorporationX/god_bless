52069d8776cb1b79d396c72f4701de7d275a1062
Timofey Khutoryanskiy <tiroxxxin@gmail.com>
2024-12-16 18:12:02 +0500

    tiroxin BJS2_46154 Смена Supplier на Callable

--- src/main/java/school/faang/sprint_2/task_46154/ErrorHandler.java
+++ src/main/java/school/faang/sprint_2/task_46154/ErrorHandler.java
@@ -1,12 +1,11 @@
 package school.faang.sprint_2.task_46154;
 
-import java.util.function.Supplier;
+import java.util.concurrent.Callable;
 
 public class ErrorHandler {
-
-    public static <T> T withErrorHandling(Supplier<T> action, ExceptionHandler<T> errorHandler) {
+    public static <T> T withErrorHandling(Callable<T> action, ExceptionHandler<T> errorHandler) {
         try {
-            return action.get();
+            return action.call();
         } catch (Exception e) {
             return errorHandler.handle(e);
         }
--- src/main/java/school/faang/sprint_2/task_46154/RemoteService.java
+++ src/main/java/school/faang/sprint_2/task_46154/RemoteService.java
@@ -2,11 +2,7 @@ package school.faang.sprint_2.task_46154;
 
 public class RemoteService {
 
-    public static String call(String param) {
-        try {
-            throw new Exception("Сервис недоступен");
-        } catch (Exception e) {
-            throw new RuntimeException(e);
-        }
+    public static String call(String param) throws Exception {
+        throw new Exception("Сервис недоступен");
     }
 }
