f915c6ded49fd5382adda5b697764339ef913c9d
rostex <rostex2@yandex.ru>
2024-12-16 20:55:47 +0300

    Внесены изменения после ревью

--- src/main/java/school/faang/sprint_3/task_47707/Army.java
+++ src/main/java/school/faang/sprint_3/task_47707/Army.java
@@ -7,13 +7,22 @@ public class Army {
     static List<Unit> units = new ArrayList<>();
 
     public void addUnit(Unit unit) {
+        if (unit == null) {
+            throw new IllegalArgumentException("Герой не может быть пустым (null)");
+        }
+        if (units == null) {
+            throw new NullPointerException("Список не может быть null");
+        }
         units.add(unit);
     }
 
     public int calculateTotalPower() throws InterruptedException {
-        int totalPower = 0;
         List<Thread> threads = new ArrayList<>();
         List<PowerCalculator> tasks = new ArrayList<>();
+
+        if (units == null) {
+            throw new NullPointerException("Список не может быть null");
+        }
 
         for (Unit unit : units) {
             PowerCalculator calculator = new PowerCalculator(unit);
@@ -24,9 +33,15 @@ public class Army {
         }
 
         for (Thread thread : threads) {
-            thread.join();
+            try {
+                thread.join();
+            } catch (InterruptedException e) {
+                Thread.currentThread().interrupt();
+                throw new RuntimeException("Прерывание вложенного потока при ожидании завершения" + e.getMessage());
+            }
         }
 
+        int totalPower = 0;
         for (PowerCalculator task : tasks) {
             totalPower += task.getPower();
         }
