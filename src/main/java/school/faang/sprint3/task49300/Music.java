99434b6002a19533f51a171e4e2d5a54e1f3a405
Alexey Gorokhov <almigor@mail.ru>
2024-12-16 23:27:05 +0300

    Блокировка Spotify

--- src/main/java/school/faang/sprint3/task49300/Music.java
+++ src/main/java/school/faang/sprint3/task49300/Music.java
@@ -6,25 +6,24 @@ public class Music {
         Player player = new Player();
 
         Thread thread1 = new Thread(player::play);
-        Thread thread2 = new Thread(player::pause);
-        Thread thread3 = new Thread(player::skip);
-        Thread thread4 = new Thread(player::previous);
-        Thread thread5 = new Thread(player::play);
-        Thread thread6 = new Thread(player::pause);
-        Thread thread7 = new Thread(player::skip);
-        Thread thread8 = new Thread(player::previous);
-        Thread thread9 = new Thread(player::play);
-        Thread thread10 = new Thread(player::play);
-
         thread1.start();
+        Thread thread2 = new Thread(player::pause);
         thread2.start();
+        Thread thread3 = new Thread(player::skip);
         thread3.start();
+        Thread thread4 = new Thread(player::previous);
         thread4.start();
+        Thread thread5 = new Thread(player::play);
         thread5.start();
+        Thread thread6 = new Thread(player::pause);
         thread6.start();
+        Thread thread7 = new Thread(player::skip);
         thread7.start();
+        Thread thread8 = new Thread(player::previous);
         thread8.start();
+        Thread thread9 = new Thread(player::play);
         thread9.start();
+        Thread thread10 = new Thread(player::play);
         thread10.start();
     }
 }
