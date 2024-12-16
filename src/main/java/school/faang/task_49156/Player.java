e57385f7282c7143169494641dbfc466139494ee
therealadik <vladerm2000@yandex.ru>
2024-12-16 10:50:58 +0300

    Блокировка Spotify

---
+++ src/main/java/school/faang/task_49156/Player.java
@@ -0,0 +1,32 @@
+package school.faang.task_49156;
+
+public class Player {
+    private boolean isPlaying;
+    private final Object lock = new Object();
+
+    public void play(){
+        synchronized (lock){
+            isPlaying = true;
+            System.out.println("isPlaying: " + isPlaying);
+        }
+    }
+
+    public void pause(){
+        synchronized (lock){
+            isPlaying = false;
+            System.out.println("isPlaying: " + isPlaying);
+        }
+    }
+
+    public void skip(){
+        synchronized (lock){
+            System.out.println("Track skipped");
+        }
+    }
+
+    public void previous(){
+        synchronized (lock){
+            System.out.println("Player previous");
+        }
+    }
+}
