683e78bba3266a1e2cbb27f666d14bc81729fb66
Artem Kopanev <artemkopanev757@gmail.com>
2024-12-16 17:40:18 +0300

    Убрал ненужное создание объекта Set Сделал названия входных данных методов более читаемыми

--- src/main/java/school/faang/sprint2/bjs_47335/ListOperations.java
+++ src/main/java/school/faang/sprint2/bjs_47335/ListOperations.java
@@ -10,11 +10,9 @@ import java.util.Set;
 import java.util.stream.Collectors;
 
 public class ListOperations {
-    public static Set<List<Integer>> sumDistinctPairs(List<Integer> list, int sum) {
-        Set<Integer> numbers = new HashSet<>(list);
-
-        return numbers.stream()
-                .filter(x -> numbers.contains(sum - x))
+    public static Set<List<Integer>> sumDistinctPairs(List<Integer> digits, int sum) {
+        return digits.stream()
+                .filter(x -> digits.contains(sum - x))
                 .map((a) -> Arrays.asList(a, sum - a))
                 .peek(Collections::sort)
                 .collect(Collectors.toSet());
@@ -27,23 +25,23 @@ public class ListOperations {
                 .collect(Collectors.toList());
     }
 
-    public static List<String> filteredBySymbol(List<String> list, char symbol) {
-        return list.stream()
+    public static List<String> filteredBySymbol(List<String> words, char symbol) {
+        return words.stream()
                 .filter(x -> x.startsWith(String.valueOf(symbol)))
                 .sorted(String::compareTo)
                 .collect(Collectors.toList());
     }
 
-    public static List<String> convertToBinary(List<Integer> list) {
-        return list.stream()
+    public static List<String> convertToBinary(List<Integer> digits) {
+        return digits.stream()
                 .map(Integer::toBinaryString)
                 .collect(Collectors.toList());
     }
 
-    public static List<String> filteredByString(List<String> list, String alphabet) {
+    public static List<String> filteredByString(List<String> words, String alphabet) {
         String regex = "[" + alphabet + "]+";
 
-        return list.stream()
+        return words.stream()
                 .filter(x -> x.matches(regex))
                 .sorted(Comparator.comparingInt(String::length))
                 .collect(Collectors.toList());
--- src/main/java/school/faang/sprint2/bjs_47335/Main.java
+++ src/main/java/school/faang/sprint2/bjs_47335/Main.java
@@ -7,29 +7,24 @@ import java.util.Set;
 
 public class Main {
     public static void main(String[] args) {
-        // тестирование 1 метода
         List<Integer> list = Arrays.asList(1, 2, 3, 4, 5, 6);
         Set<List<Integer>> result = ListOperations.sumDistinctPairs(list, 7);
         System.out.println(result);
 
-        // тестирование 2 метода
         Map<String, String> countries = Map.of("Russia", "Moscow",
                 "USA", "Washington",
                 "Germany", "Berlin");
         List<String> capitals = ListOperations.sortedCapitals(countries);
         System.out.println(capitals);
 
-        // тестирование 3 метода
         List<String> fruits = Arrays.asList("apple", "banana", "avocado", "apricot");
         List<String> sortedFruits = ListOperations.filteredBySymbol(fruits, 'a');
         System.out.println(sortedFruits);
 
-        // тестирование 4 метода
         List<Integer> digits = Arrays.asList(1, 2, 3, 4);
         List<String> binaryDigits = ListOperations.convertToBinary(digits);
         System.out.println(binaryDigits);
 
-        // тестирование 5 метода
         String alphabet = "abcdefghijklmnopqrstuvwxyz";
         fruits = Arrays.asList("apple", "banana", "cherry", "date", "fig", "grape");
         sortedFruits = ListOperations.filteredByString(fruits, alphabet);
