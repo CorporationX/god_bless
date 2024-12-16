af3ee6dc01b5796f369a439b509b056e321aa3fe
Arseni Zaytsau <zarseniy280@gmail.com>
2024-12-16 22:35:24 +0300

    ztsv-BJS2-47149 - Потренируем Stream API

---
+++ src/main/java/school/faang/bjs47149/Processor.java
@@ -0,0 +1,53 @@
+package school.faang.bjs47149;
+
+import java.util.Comparator;
+import java.util.List;
+import java.util.Optional;
+import java.util.function.Predicate;
+
+public class Processor {
+    public static int sumEven(List<Integer> numbers) {
+        return numbers.stream().filter(number -> number % 2 == 0)
+                .reduce(0, Integer::sum);
+    }
+
+    public static int max(List<Integer> numbers) {
+        return numbers.stream().max(Integer::compare).orElse(Integer.MIN_VALUE);
+    }
+
+    public static double avg(List<Integer> numbers) {
+        return numbers.stream().mapToInt(Integer::intValue).average().orElse(0);
+    }
+
+    public static long countStartingWithChar(List<String> strings, char a) {
+        return strings.stream().filter(string -> string.startsWith(String.valueOf(a))).count();
+    }
+
+    public static List<String> filterStrings(List<String> strings, String substring) {
+        return strings.stream()
+                .filter(string -> string.contains(substring))
+                .toList();
+    }
+
+    public static List<String> sortByLength(List<String> strings) {
+        return strings.stream()
+                .sorted(Comparator.comparingInt(String::length))
+                .toList();
+    }
+
+    public static boolean checkAllMatch(List<Integer> integers, Predicate<Integer> condition) {
+        return integers.stream().allMatch(condition);
+    }
+
+    public static Optional<Integer> minimalMore(List<Integer> numbers, int base) {
+        return numbers.stream()
+                .filter(number -> number > base)
+                .min(Integer::compareTo);
+    }
+
+    public static List<Integer> mapStringsToLength(List<String> strings) {
+        return strings.stream()
+                .map(String::length)
+                .toList();
+    }
+}
