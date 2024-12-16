3f5f6b53e8e89e2d8f6236ec99fe42ca0c272f33
Artur Suleymanov <suleymanov.usa@gmail.com>
2024-12-16 06:11:35 -0800

    Fixed the problem with code duplication

--- src/main/java/school/faang/bjs245985/Main.java
+++ src/main/java/school/faang/bjs245985/Main.java
@@ -1,5 +1,6 @@
 package school.faang.bjs245985;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.function.Consumer;
@@ -11,18 +12,10 @@ public class Main {
     public static void main(String[] args) {
         EmailPrcessor emailPrcessor = new EmailPrcessor();
 
-        List<Email> emails = Arrays.asList(
-                new Email("Subject1", "Body1", true),
-                new Email("Subject2", "Body2", false),
-                new Email("Subject3", "Body3", true),
-                new Email("Subject4", "Body4", false),
-                new Email("Subject5", "Body5", true),
-                new Email("Subject6", "Body6", false),
-                new Email("Subject7", "Body7", true),
-                new Email("Subject8", "Body8", false),
-                new Email("Subject9", "Body9", true),
-                new Email("Subject10", "Body10", false)
-        );
+        List<Email> emails = new ArrayList<>();
+        for (int i = 1; i <= 10; i++) {
+            emails.add(new Email("Subject" + i, "Body" + i, i % 2 == 1));
+        }
 
         Predicate<Email> emailFilterIsImportant = Email::isImportant;
         Function<Email, String> emailTransformerToLowerCase = email -> email.getBody().toLowerCase();
