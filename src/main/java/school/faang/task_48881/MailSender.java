c2e51531442a7bc4d7db26fea051c00c9a511efc
maks <fhbfds@fb>
2024-12-16 18:09:15 +0300

    BJS2-48881 Вы работаете в Microsoft! Обработка исключения try-catch

--- src/main/java/school/faang/task_48881/MailSender.java
+++ src/main/java/school/faang/task_48881/MailSender.java
@@ -7,24 +7,30 @@ public class MailSender {
     private static final int ONE_THREAD = 200;
     private static final int COUNT_THREAD = 5;
 
-    public static void main(String[] args)  {
+    public static void main(String[] args) throws InterruptedException {
 
         List<Thread> threads = new ArrayList<>();
 
         for (int i = 0; i < COUNT_THREAD; i++) {
             int start = i * ONE_THREAD + 1;
             int end = (i + 1) * ONE_THREAD;
-            Thread thread = new Thread(new SenderRunnable(start, end));
+            Thread thread = null;
+            try {
+                thread = new Thread(new SenderRunnable(start, end));
+            } catch (Exception e) {
+                System.out.println("Ошибка в потоке: " + e.getMessage());
+                e.printStackTrace();
+            }
             threads.add(thread);
             thread.start();
         }
+
         for (Thread thread : threads) {
             try {
                 thread.join();
             } catch (InterruptedException e) {
-                throw new RuntimeException(e);
+                System.out.println("Поток был прерван: " + thread.getName());
             }
-        }
-        System.out.println("Все письма успешно отправлены!");
+        }System.out.println("Все письма успешно отправлены!");
     }
 }
