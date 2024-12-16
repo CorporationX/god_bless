d29a5a7b90d582274e30ac515f0668ad2629ff69
Alfir Rakhimov <exprnc7@gmail.com>
2024-12-16 21:14:19 +0500

    Поправил

--- src/main/java/school/faang/task_48189/Chore.java
+++ src/main/java/school/faang/task_48189/Chore.java
@@ -10,16 +10,19 @@ import java.util.Random;
 @Slf4j
 @RequiredArgsConstructor
 public class Chore implements Runnable {
+    private static final int MIN_SLEEP_TIME = 2000;
+    private static final int MAX_SLEEP_TIME = 10000;
+
     private final String chore;
 
     @Override
     public void run() {
+        log.info("{} started to solve the problem of {}", Thread.currentThread().getName(), getChore());
         try {
-            log.info("{} started to solve the problem of {}", Thread.currentThread().getName(), getChore());
-            Thread.sleep(new Random().nextInt(2000, 10000));
-            log.info("The {} solved the problem of {}", Thread.currentThread().getName(), getChore());
+            Thread.sleep(new Random().nextInt(MIN_SLEEP_TIME, MAX_SLEEP_TIME));
         } catch (Exception e) {
             log.info(e.getMessage());
         }
+        log.info("The {} solved the problem of {}", Thread.currentThread().getName(), getChore());
     }
 }
