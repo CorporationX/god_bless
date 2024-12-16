ae46f8a27d5fd97b102f3a36f22cc65f529ed13c
Arseni Zaytsau <zarseniy280@gmail.com>
2024-12-16 10:07:07 +0300

    ztsv-BJS2-45943 - checkstyle errors

--- src/main/java/school/faang/bjs45943/EmailProcessor.java
+++ src/main/java/school/faang/bjs45943/EmailProcessor.java
@@ -6,7 +6,8 @@ import java.util.function.Function;
 import java.util.function.Predicate;
 
 public class EmailProcessor {
-    public void processEmails(List<Email> emails, Predicate<Email> filter, Function<Email, String> modifier, Consumer<Email> action) {
+    public void processEmails(List<Email> emails, Predicate<Email> filter,
+                              Function<Email, String> modifier, Consumer<Email> action) {
         emails.forEach(email -> {
             if (filter.test(email)) {
                 email.setBody(modifier.apply(email));
