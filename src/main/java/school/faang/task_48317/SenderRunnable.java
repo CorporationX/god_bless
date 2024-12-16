776eb76814737245fb230566c364efb489240d3d
Oxana <nov2303@mail.ru>
2024-12-16 20:34:28 +0500

    BJS2-48317

--- src/main/java/school/faang/task_48317/SenderRunnable.java
+++ src/main/java/school/faang/task_48317/SenderRunnable.java
@@ -3,9 +3,10 @@ package school.faang.task_48317;
 import lombok.AllArgsConstructor;
 
 @AllArgsConstructor
-public class SenderRunnable implements Runnable{
+public class SenderRunnable implements Runnable {
     private int startIndex;
     private int endIndex;
+
     @Override
     public void run() {
         System.out.println("Email sent.");
