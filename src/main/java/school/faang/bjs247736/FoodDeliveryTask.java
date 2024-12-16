15411bbe86c7a2624c8433c40c9aa5c275d302c0
lcodio <lcodio@temp.com>
2024-12-16 23:12:51 +0300

    bjs2-47736 added static final and corrected exceptions

--- src/main/java/school/faang/bjs247736/FoodDeliveryTask.java
+++ src/main/java/school/faang/bjs247736/FoodDeliveryTask.java
@@ -6,10 +6,10 @@ public class FoodDeliveryTask implements Runnable {
 
     private final String character;
     private final int foodAmount;
-    private final String[] foodTypes = {"pizza", "burger", "hot dog", "chicken wings", "taco"};
+    private static final String[] FOOD_TYPES = {"pizza", "burger", "hot dog", "chicken wings", "taco"};
 
     private static final Random random = new Random();
-    int randomSeconds = random.nextInt(4001) + 1000;
+    private static final int RANDOM_SECONDS = random.nextInt(4001) + 1000;
 
     public FoodDeliveryTask(String character, int foodAmount) {
         this.character = character;
@@ -17,7 +17,7 @@ public class FoodDeliveryTask implements Runnable {
     }
 
     private String getFoodType() {
-        return foodTypes[new Random().nextInt(foodTypes.length)];
+        return FOOD_TYPES[new Random().nextInt(FOOD_TYPES.length)];
     }
 
     @Override
@@ -25,10 +25,12 @@ public class FoodDeliveryTask implements Runnable {
         String foodType = getFoodType();
         try {
             System.out.println(character + " gets " + foodAmount + " " + foodType);
-            Thread.sleep(randomSeconds);
+            Thread.sleep(RANDOM_SECONDS);
             System.out.println(character + " eats " + foodAmount + " " + foodType);
         } catch (InterruptedException e) {
-            throw new RuntimeException(e);
+            System.err.println("Thread was interrupted: " + e.getMessage());
+        } catch (Exception e) {
+            System.err.println("Unexpected error: " + e.getMessage());
         }
     }
 }
--- src/main/java/school/faang/bjs247736/GriffinsFoodDelivery.java
+++ src/main/java/school/faang/bjs247736/GriffinsFoodDelivery.java
@@ -9,24 +9,26 @@ public class GriffinsFoodDelivery {
 
     private static final Random random = new Random();
     private static final int THREAD_POOL_AMOUNT = 3;
+    private static final int RANDOM_FOOD_AMOUNT = random.nextInt(50) + 1;
+    private static final String[] CHARACTER_NAMES = {"Peter", "Lois", "Meg", "Chris", "Stewie"};
 
     public static void main(String[] args) {
 
-        String[] characterNames = {"Peter", "Lois", "Meg", "Chris", "Stewie"};
-        int randomfoodAmount = random.nextInt(50) + 1;
 
         ExecutorService executorService = Executors.newFixedThreadPool(THREAD_POOL_AMOUNT);
 
-        for (String character : characterNames) {
-            FoodDeliveryTask deliveryTask = new FoodDeliveryTask(character, randomfoodAmount);
+        for (String character : CHARACTER_NAMES) {
+            FoodDeliveryTask deliveryTask = new FoodDeliveryTask(character, RANDOM_FOOD_AMOUNT);
             executorService.execute(deliveryTask);
         }
 
         executorService.shutdown();
+
         try {
             executorService.awaitTermination(5, TimeUnit.SECONDS);
         } catch (InterruptedException e) {
-            throw new RuntimeException(e);
+            System.err.println("The thread was interrupted while waiting for ExecutorService to terminate: "
+                    + e.getMessage());
         }
 
         try {
@@ -36,7 +38,8 @@ public class GriffinsFoodDelivery {
                 executorService.shutdownNow();
             }
         } catch (InterruptedException e) {
-            System.out.println("The main thread was interrupted while waiting, we forcefully stop the ThreadPool");
+            System.out.println("The main thread was interrupted while waiting, we forcefully stop the ThreadPool"
+                    + e.getMessage());
             executorService.shutdownNow();
         }
     }
