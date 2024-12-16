f95bac28703c6ef83daf84d311c3afeb463d608e
askar0103 <ananaspineapple2048@gmail.com>
2024-12-16 16:56:20 +0300

    Solution to task BJS2-48186

---
+++ src/main/java/school/faang/task_48186/MailSender.java
@@ -0,0 +1,24 @@
+package school.faang.task_48186;
+
+public class MailSender {
+    public static void main(String[] args) throws InterruptedException {
+        final int totalMessages = 1000;
+        final int threadCount = 5;
+        final int batchSize = totalMessages / threadCount;
+
+        Thread[] threads = new Thread[threadCount];
+
+        for (int i = 0; i < threadCount; i++) {
+            int startIndex = i * batchSize;
+            int endIndex = startIndex + batchSize;
+            threads[i] = new Thread(new SenderRunnable(startIndex, endIndex));
+            threads[i].start();
+        }
+
+        for (Thread thread : threads) {
+            thread.join();
+        }
+
+        System.out.println("Все письма отправлены!");
+    }
+}
---
+++ src/main/java/school/faang/task_48186/Main.java
@@ -0,0 +1,4 @@
+package school.faang.task_48186;
+
+public class Main {
+}
