2131d7697afd86249ee5e24055050c52bef3fde4
KsushaTarasova <ksutaras543@mail.ru>
2024-12-16 22:19:56 +0300

    Add constants

--- src/main/java/school/faang/sprint_3/task_43554/MailSender.java
+++ src/main/java/school/faang/sprint_3/task_43554/MailSender.java
@@ -1,15 +1,16 @@
 package school.faang.sprint_3.task_43554;
 
 public class MailSender {
-    public static void main(String[] args) throws InterruptedException {
-        int messagesNumber = 1000;
-        int threadsNumber = 5;
-        int batchNumber = messagesNumber / threadsNumber;
+    public static final int MESSAGES_NUMBER = 1000;
+    public static final int THREADS_NUMBER = 5;
+    public static final int BATCH_NUMBER = MESSAGES_NUMBER / THREADS_NUMBER;
+
+    public static void main(String[] args) {
         Thread[] threads = new Thread[5];
 
-        for (int i = 0; i < threadsNumber; i++) {
-            int start = i * batchNumber;
-            int end = (i + 1) * batchNumber;
+        for (int i = 0; i < THREADS_NUMBER; i++) {
+            int start = i * BATCH_NUMBER;
+            int end = (i + 1) * BATCH_NUMBER;
             threads[i] = new Thread(new SenderRunnable(start, end));
             threads[i].start();
         }
