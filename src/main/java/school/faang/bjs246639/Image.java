aa8dd51f7bb93a35dd0964637d04e3823f3fcc6f
Maksim-VV <laulmv11@gmail.com>
2024-12-16 19:44:48 +0300

    BJS2-46639, Instagram Filters - alpha release

---
+++ src/main/java/school/faang/bjs246639/Image.java
@@ -0,0 +1,12 @@
+package school.faang.bjs246639;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class Image {
+    private String name;
+    private String description;
+}
