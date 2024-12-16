5e4a5a8c650e58c6de5ce513fae54b055ffa36e2
mixa <mikle.kuznecov@yandex.ru>
2024-12-17 00:47:09 +0300

    task stream api2

---
+++ src/main/java/school/faang/sprint_2/task_47298/ListOperations.java
@@ -0,0 +1,47 @@
+package school.faang.sprint_2.task_47298;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import java.util.regex.Pattern;
+import java.util.stream.Collectors;
+
+public class ListOperations {
+    static Set<List<Integer>> findPairs(List<Integer> numbers, int sum) {
+        return numbers.stream()
+            .distinct()
+            .map(num -> Arrays.asList(num, sum - num))
+            .peek(Collections::sort)
+            .collect(Collectors.toSet());
+    }
+
+    static List<String> getCountry(Map<String, String> country) {
+        return country.entrySet().stream()
+            .sorted(Map.Entry.comparingByKey())
+            .map(Map.Entry::getValue)
+            .toList();
+    }
+
+    static List<String> filterAndSort(List<String> strings, char letter) {
+        return strings.stream()
+            .filter(str -> str.startsWith(String.valueOf(letter)))
+            .sorted(Comparator.comparingInt(String::length))
+            .toList();
+    }
+
+    static List<String> convertToBinary(List<Integer> numbers) {
+        return numbers.stream()
+            .map(Integer::toBinaryString)
+            .toList();
+    }
+
+    static List<String> filterAndSortByLength(List<String> strings, String regex) {
+        return strings.stream()
+            .filter(str -> Pattern.compile(regex).matcher(str).matches())
+            .sorted(Comparator.comparingInt(String::length))
+            .toList();
+    }
+}
---
+++ src/main/java/school/faang/sprint_2/task_47298/Main.java
@@ -0,0 +1,22 @@
+package school.faang.sprint_2.task_47298;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
+
+public class Main {
+    public static void main(String[] args) {
+        List<Integer> numbers = Arrays.asList(1, 2, 3, 4, 5, 6);
+        Map<String, String>
+            countries = Map.of("Russia", "Moscow", "USA", "Washington", "Germany", "Berlin");
+        List<String> strings = Arrays.asList("apple", "banana", "avocado", "apricot");
+        List<String> listOfStrings =
+            Arrays.asList("apple", "banana", "cherry", "date", "fig", "grape");
+
+        ListOperations.findPairs(numbers, 7).forEach(System.out::println);
+        ListOperations.getCountry(countries).forEach(System.out::println);
+        ListOperations.filterAndSort(strings, 'a').forEach(System.out::println);
+        ListOperations.convertToBinary(numbers).forEach(System.out::println);
+        ListOperations.filterAndSortByLength(listOfStrings, "[a-z]+").forEach(System.out::println);
+    }
+}
