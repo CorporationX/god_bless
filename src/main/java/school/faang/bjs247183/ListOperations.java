c499a60c443fa66517b17d287cb42ac8a92a2312
salikhdev <salikhdev@gmail.com>
2024-12-16 11:18:05 +0500

    Потренируем StreamAPI 2. Update

--- src/main/java/school/faang/bjs247183/ListOperations.java
+++ src/main/java/school/faang/bjs247183/ListOperations.java
@@ -6,19 +6,20 @@ import java.util.stream.Collectors;
 public class ListOperations {
 
     public static Set<List<Integer>> findPairs(List<Integer> nums, int sum) {
-        validate(nums);
-        Set<List<Integer>> pairs;
+        if (nums == null) {
+            throw new NullPointerException("Args must be not null");
+        }
         Set<Integer> set = new HashSet<>(nums);
-        pairs = nums.stream()
+        return nums.stream()
                 .filter(num -> set.contains(sum - num))
                 .map(num -> Arrays.asList(num, sum - num))
-                .peek(Collections::sort)
-                .collect(Collectors.toSet());
-        return pairs;
+                .peek(Collections::sort).collect(Collectors.toSet());
     }
 
     public static Set<String> filterCountry(Map<String, String> countries) {
-        validate(countries);
+        if (countries == null) {
+            throw new NullPointerException("Args must be not null");
+        }
         return countries.entrySet().stream()
                 .sorted(Map.Entry.comparingByKey())
                 .map(Map.Entry::getValue)
@@ -26,7 +27,9 @@ public class ListOperations {
     }
 
     public static List<String> filterAndSortWords(List<String> words, char symbol) {
-        validate(words);
+        if (words == null) {
+            throw new NullPointerException("Args must be not null");
+        }
         return words.stream()
                 .filter(word -> word.startsWith(String.valueOf(symbol)))
                 .sorted(Comparator.comparingInt(String::length))
@@ -34,25 +37,22 @@ public class ListOperations {
     }
 
     public static List<String> toBinary(List<Integer> nums) {
-        validate(nums);
+        if (nums == null) {
+            throw new NullPointerException("Args must be not null");
+        }
         return nums.stream()
                 .map(num -> Integer.toString(num, 2))
                 .toList();
     }
 
     public static List<String> filterAndSortByLength(List<String> words, String alphabet) {
-        validate(words);
-        validate(alphabet);
+        if (words == null || alphabet == null) {
+            throw new NullPointerException("Args must be not null");
+        }
         String regex = "[" + alphabet + "]+";
         return words.stream()
                 .filter(word -> word.matches(regex))
                 .sorted(Comparator.comparingInt(String::length))
                 .toList();
-    }
-
-    private static void validate(Object object) {
-        if (object == null) {
-            throw new NullPointerException("Argument must be not null");
-        }
     }
 }
