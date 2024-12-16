379fae24db2f685717a3069ae3ea39b077f2d1e1
lehaps <awda.96@bk.ru>
2024-12-16 17:32:09 +0300

    Убрал sout сделал логгирование

--- src/main/java/school/faang/task49620/Boss.java
+++ src/main/java/school/faang/task49620/Boss.java
@@ -1,15 +1,20 @@
 package school.faang.task49620;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import java.util.ArrayList;
 import java.util.List;
 
 public class Boss {
+    private final Logger logger = LoggerFactory.getLogger(Boss.class);
+
     private static final int MAX_PLAYERS = 3;
     private final List<Player> currentPlayers = new ArrayList<>();
 
     public void joinBattle(Player player) {
         synchronized (this) {
-            if (currentPlayers.size() >= MAX_PLAYERS) {
+            while (currentPlayers.size() >= MAX_PLAYERS) {
                 try {
                     this.wait();
                 } catch (InterruptedException e) {
@@ -17,7 +22,7 @@ public class Boss {
                 }
             }
             currentPlayers.add(player);
-            System.out.printf("Игрок: %s врывается в сражение%n", player.name());
+            logger.info(String.format("Игрок: %s врывается в сражение%n", player.name()));
         }
     }
 
@@ -26,12 +31,12 @@ public class Boss {
             throw new IllegalArgumentException("player can`t be null");
         }
         if (!currentPlayers.contains(player)) {
-            System.out.println("Такого игрока нет в сражении.");
+            logger.error("Такого игрока нет в сражении.");
             return false;
         }
 
         synchronized (this) {
-            System.out.printf("Игрок %s выходит из сражения%n", player.name());
+            logger.info(String.format("Игрок %s выходит из сражения%n", player.name()));
             currentPlayers.remove(player);
             this.notify();
         }
