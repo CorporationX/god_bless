2ed6a94637de4d7cb42a61acb4127032865d2f22
NiveshOne <Shishov.44@mail.ru>
2024-12-17 00:39:18 +0300

    Все методы работы со стримами вынесены в класс CollectionStreamProcessor Методы переименованы в более читаемые названия и доработаны, исключив лишние переменные

---
+++ src/main/java/school/faang/task_47312/CollectionStreamProcessor.java
@@ -0,0 +1,46 @@
+package school.faang.task_47312;
+
+import java.util.*;
+import java.util.stream.Collectors;
+
+public class CollectionStreamProcessor {
+    public Set<List<Integer>> getNumberPairs(List<Integer> numbers, int sum) {
+        Set<Integer> number = new HashSet<>(numbers);
+
+        return numbers.stream()
+                .filter(num -> number.contains(sum - num))
+                .map(num -> Arrays.asList(num, sum - num))
+                .peek(Collections::sort)
+                .collect(Collectors.toSet());
+    }
+
+    public List<String> getSortingCapitals(Map<String, String> capitalsAndCountries) {
+        return capitalsAndCountries.entrySet().stream()
+                .sorted(Map.Entry.comparingByKey())
+                .map(Map.Entry::getValue)
+                .toList();
+    }
+
+    public List<String> getSortingList(List<String> list, char symbol) {
+        return list.stream()
+                .filter(string -> string.indexOf(symbol) == 0)
+                .sorted(Comparator.comparingInt(String::length))
+                .toList();
+    }
+
+    public List<String> convertToBinary(List<Integer> numbers) {
+        return numbers.stream()
+                .map(Integer::toBinaryString)
+                .toList();
+    }
+
+    public List<String> getStringFiltering(List<String> strings) {
+        String alf = "abcdefghijklmnopqrstuvwxyz";
+        String regex = "[" + alf + "]+";
+
+        return strings.stream()
+                .filter(str -> str.matches(regex))
+                .sorted(Comparator.comparingInt(String::length))
+                .toList();
+    }
+}
--- src/main/java/school/faang/task_47312/Main.java
+++ src/main/java/school/faang/task_47312/Main.java
@@ -1,78 +1,25 @@
 package school.faang.task_47312;
 
 import java.util.*;
-import java.util.stream.Collectors;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println(findPair(new ArrayList<>(List.of(1, 2, 3, 4, 5, 6)), 7));
+        CollectionStreamProcessor collectionStreamProcessor = new CollectionStreamProcessor();
+        System.out.println(collectionStreamProcessor.getNumberPairs(new ArrayList<>(List.of(1, 2, 3, 4, 5, 6)), 7));
 
         Map<String, String> capitalsAndCountries = new HashMap<>();
         capitalsAndCountries.put("Russia", "Moscow");
         capitalsAndCountries.put("USA", "Washington");
         capitalsAndCountries.put("Germany", "Berlin");
-        System.out.println(sortingCapitals(capitalsAndCountries));
+        System.out.println(collectionStreamProcessor.getSortingCapitals(capitalsAndCountries));
 
-        System.out.println(sortingList(new ArrayList<>(List.of("banana", "avocado",
+        System.out.println(collectionStreamProcessor.getSortingList(new ArrayList<>(List.of("banana", "avocado",
                 "apricot", "apple")), 'a'));
 
-        System.out.println(convertToBinary(new ArrayList<>(List.of(1, 2, 3, 4))));
+        System.out.println(collectionStreamProcessor.convertToBinary(new ArrayList<>(List.of(1, 2, 3, 4))));
 
-        System.out.println(stringFiltering(new ArrayList<>(List.of("apple", "banana",
+        System.out.println(collectionStreamProcessor.getStringFiltering(new ArrayList<>(List.of("apple", "banana",
                 "cherry", "date", "fig", "grape"))));
-    }
-
-    public static Set<List<Integer>> findPair(List<Integer> numbers, int sum) {
-        Set<List<Integer>> pairNumbers;
-        Set<Integer> number = new HashSet<>(numbers);
-
-        pairNumbers = numbers.stream()
-                .filter(num -> number.contains(sum - num))
-                .map(num -> Arrays.asList(num, sum - num))
-                .peek(Collections::sort)
-                .collect(Collectors.toSet());
-        return pairNumbers;
-    }
-
-    public static List<String> sortingCapitals(Map<String, String> capitalsAndCountries) {
-        List<String> sortedCapitals;
-
-        sortedCapitals = capitalsAndCountries.entrySet().stream()
-                .sorted(Map.Entry.comparingByKey())
-                .map(Map.Entry::getValue)
-                .toList();
-        return sortedCapitals;
-    }
-
-    public static List<String> sortingList(List<String> list, char symbol) {
-        List<String> sortedList;
-
-        sortedList = list.stream()
-                .filter(string -> string.indexOf(symbol) == 0)
-                .sorted(Comparator.comparingInt(String::length))
-                .toList();
-
-        return sortedList;
-    }
-
-    public static List<String> convertToBinary(List<Integer> numbers) {
-        List<String> binaryNumbers;
-
-        binaryNumbers = numbers.stream()
-                .map(Integer::toBinaryString)
-                .toList();
-
-        return binaryNumbers;
-    }
-
-    public static List<String> stringFiltering(List<String> strings) {
-        String alf = "abcdefghijklmnopqrstuvwxyz";
-        String regex = "[" + alf + "]+";
-
-        return strings.stream()
-                .filter(str -> str.matches(regex))
-                .sorted(Comparator.comparingInt(String::length))
-                .toList();
     }
 
 }
