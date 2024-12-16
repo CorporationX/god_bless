2aba288a1cada02df1d2072cfd3286c56f470196
rostex <rostex2@yandex.ru>
2024-12-17 00:58:12 +0300

    Реализован сервис сбора еды в доме через многопоточность

---
+++ src/main/java/school/faang/sprint_3/task_48860/FoodService.java
@@ -0,0 +1,25 @@
+package school.faang.sprint_3.task_48860;
+
+import java.util.concurrent.ScheduledExecutorService;
+
+public class FoodService implements Runnable {
+    private final House house;
+    private final ScheduledExecutorService executor;
+
+    public FoodService(House house, ScheduledExecutorService executor) {
+        this.house = house;
+        this.executor = executor;
+    }
+
+    @Override
+    public void run() {
+        if(house == null) {
+            System.out.println("Дом не может быть null");
+        }
+        house.collectFoods();
+        if (house.allFoodCollected()) {
+            executor.shutdown();
+            System.out.println("Вся еда собрана!");
+        }
+    }
+}
