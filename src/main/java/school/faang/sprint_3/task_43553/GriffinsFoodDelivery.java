07985c4129376424776c147d3e610b058fcc4d36
KsushaTarasova <ksutaras543@mail.ru>
2024-12-17 12:31:03 +0300

    Add constants for food amount

--- src/main/java/school/faang/sprint_3/task_43553/GriffinsFoodDelivery.java
+++ src/main/java/school/faang/sprint_3/task_43553/GriffinsFoodDelivery.java
@@ -6,13 +6,16 @@ import java.util.concurrent.Executors;
 
 public class GriffinsFoodDelivery {
     private static final int POOL_SIZE = 3;
+    private static final int RANDOM_FOOD_AMOUNT = 50;
+    private static final int BASE_FOOD_AMOUNT = 1;
 
     public static void main(String[] args) {
         ExecutorService executor = Executors.newFixedThreadPool(POOL_SIZE);
         String[] characterNames = {"Peter", "Lois", "Meg", "Chris", "Stewie"};
 
         for (String name : characterNames) {
-            executor.execute(new FoodDeliveryTask(name, new Random().nextInt(50) + 1));
+            executor.execute(new FoodDeliveryTask(name,
+                    new Random().nextInt(RANDOM_FOOD_AMOUNT) + BASE_FOOD_AMOUNT));
         }
 
         executor.shutdown();
