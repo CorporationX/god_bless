35ced716c429e6b9e376bcde74e6c27bb1643605
Arseni Zaytsau <zarseniy280@gmail.com>
2024-12-17 23:39:43 +0300

    ztsv-BJS2-47149 - Stream API 1

--- src/main/java/school/faang/bjs47149/Processor.java
+++ src/main/java/school/faang/bjs47149/Processor.java
@@ -7,20 +7,28 @@ import java.util.function.Predicate;
 
 public class Processor {
     public static int sumEven(List<Integer> numbers) {
-        return numbers.stream().filter(number -> number % 2 == 0)
+        return numbers.stream()
+                .filter(number -> number % 2 == 0)
                 .reduce(0, Integer::sum);
     }
 
     public static int max(List<Integer> numbers) {
-        return numbers.stream().max(Integer::compare).orElse(Integer.MIN_VALUE);
+        return numbers.stream()
+                .max(Integer::compare)
+                .orElse(Integer.MIN_VALUE);
     }
 
     public static double avg(List<Integer> numbers) {
-        return numbers.stream().mapToInt(Integer::intValue).average().orElseThrow(() -> new RuntimeException("no numbers"));
+        return numbers.stream()
+                .mapToInt(Integer::intValue)
+                .average()
+                .orElseThrow(() -> new RuntimeException("no numbers"));
     }
 
     public static long countStartingWithChar(List<String> strings, char a) {
-        return strings.stream().filter(string -> string.startsWith(String.valueOf(a))).count();
+        return strings.stream()
+                .filter(string -> string.startsWith(String.valueOf(a)))
+                .count();
     }
 
     public static List<String> filterStrings(List<String> strings, String substring) {
@@ -36,7 +44,8 @@ public class Processor {
     }
 
     public static boolean checkAllMatch(List<Integer> integers, Predicate<Integer> condition) {
-        return integers.stream().allMatch(condition);
+        return integers.stream()
+                .allMatch(condition);
     }
 
     public static Optional<Integer> minimalMore(List<Integer> numbers, int base) {
