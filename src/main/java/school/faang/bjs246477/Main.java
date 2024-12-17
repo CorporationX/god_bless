b752b2ef56ea3a561cfb1c1355adbeb3cadb86bb
Maksim-VV <laulmv11@gmail.com>
2024-12-17 21:35:27 +0300

    BJS2-46477, We handle errors beautifully  - edits after edits based on comments from the first request

--- src/main/java/school/faang/bjs246477/Main.java
+++ src/main/java/school/faang/bjs246477/Main.java
@@ -2,22 +2,17 @@ package school.faang.bjs246477;
 
 public class Main {
     public static void main(String[] args) {
-
-        String result = ErrorHandler.withErrorHandling(
-                () -> RemoteService.call("someParam"),
-                e -> {
-                    System.out.println("Ошибка при вызове сервиса, возвращаем дефолтное значение");
-                    return "DEFAULT";
-                }
-        );
-
+        String result = ErrorHandler.withErrorHandling(() -> RemoteService.call("someParam", 0), e -> {
+            System.out.println("Ошибка при вызове сервиса, возвращаем дефолтное значение");
+            return "DEFAULT";
+        });
         System.out.println(result);
     }
 
     public static class RemoteService {
-        public static String call(String param) {
-            int r = 5 / 0;
-            return param;
+        public static String call(String param, int divisible) {
+            int result = 5 / divisible;
+            return "result: " + result;
         }
     }
 }
