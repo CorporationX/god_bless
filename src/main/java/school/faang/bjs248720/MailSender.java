5c98f660d86b04e813f75f4b1afedf5e7c4cdc50
Pavel <45390423+rsticks@users.noreply.github.com>
2024-12-17 23:06:51 +0300

    BJS2-48720 Вы работаете в Microsoft!

--- src/main/java/school/faang/bjs248720/MailSender.java
+++ src/main/java/school/faang/bjs248720/MailSender.java
@@ -2,7 +2,7 @@ package school.faang.bjs248720;
 
 public class MailSender {
     public static void main(String[] args) {
-        for (int i = 0; i < 1000; i+=200) {
+        for (int i = 0; i < 1000; i += 200) {
             new Thread(new SenderRunnable(i + 1, i + 200)).start();
         }
     }
