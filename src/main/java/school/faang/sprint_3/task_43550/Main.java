e51f640170aae9ac716273a72f3c652823fe949f
KsushaTarasova <ksutaras543@mail.ru>
2024-12-17 11:20:19 +0300

    Remove throws from method signature

--- src/main/java/school/faang/sprint_3/task_43550/Main.java
+++ src/main/java/school/faang/sprint_3/task_43550/Main.java
@@ -1,7 +1,7 @@
 package school.faang.sprint_3.task_43550;
 
 public class Main {
-    public static void main(String[] args) throws InterruptedException {
+    public static void main(String[] args) {
         Army army = new Army();
 
         army.addUnit(new Archer());
