f59a61458fc3623993475c6f93d18dfd2702f33b
az3l1t <eternity_cr9p@mail.ru>
2024-12-17 10:46:39 +0300

    Az3l1t bjs2 47709

--- src/main/java/school/faang/task_47709/Main.java
+++ src/main/java/school/faang/task_47709/Main.java
@@ -5,7 +5,7 @@ import java.util.Random;
 public class Main {
     private static final int NUM_OF_CASHIERS = 3;
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
         Customer[] customers = new Customer[] {
                 new Customer(new double[]{10.5, 20.3, 15.0, 7.99}),
                 new Customer(new double[]{5.99, 8.5, 12.0, 4.75}),
@@ -25,6 +25,10 @@ public class Main {
 
         for (CashierThread cashierThread : cashierThreads) {
             cashierThread.start();
+        }
+
+        for (CashierThread cashierThread : cashierThreads) {
+            cashierThread.join();
         }
     }
 }
