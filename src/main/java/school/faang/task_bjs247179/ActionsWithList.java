a3c51431b578338ddd4b50a833591cd6ab98eaaf
Kir34 <kirnovikof1@gmail.com>
2024-12-17 22:26:39 +0300

    задание с стрим апи 1

---
+++ src/main/java/school/faang/task_bjs247179/ActionsWithList.java
@@ -0,0 +1,37 @@
+package school.faang.task_bjs247179;
+
+import java.util.Comparator;
+import java.util.List;
+import java.util.NoSuchElementException;
+
+public class ActionsWithList {
+    public static int findSumOfEven(List<Integer> list){
+        return list.stream()
+                .filter(x -> x % 2 == 0)
+                .reduce(0, (sum, number) -> sum += number);
+    }
+
+    public static int findMax(List<Integer> list){
+        return list.stream()
+                .max(Comparator.naturalOrder())
+                .orElseThrow(() -> new NoSuchElementException("Список пуст"));
+    }
+
+    public static double findAvg(List<Integer> list){
+        return list.stream()
+                .mapToInt(i -> i)
+                .average()
+                .orElse(0.0);
+    }
+
+    public static int countFilteredStrings(List<String> list, char ch){
+        return (int)list.stream()
+                .filter(string -> string.startsWith(String.valueOf(ch)))
+                .count();
+    }
+
+    public static List<String> filterStrings(List<String> list, String str){
+        return list.stream()
+                .filter(string -> string.contains(str)).toList();
+    }
+}
---
+++ src/main/java/school/faang/task_bjs247179/Main.java
@@ -0,0 +1,28 @@
+package school.faang.task_bjs247179;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) {
+        List<Integer> list1 = new ArrayList<>();
+        list1.add(1);
+        list1.add(2);
+        list1.add(4);
+        list1.add(6);
+        list1.add(7);
+        list1.add(8);
+        list1.add(9);
+        System.out.println(ActionsWithList.findSumOfEven(list1));
+        System.out.println(ActionsWithList.findMax(list1));
+        System.out.println(ActionsWithList.findAvg(list1));
+
+        List<String> list2 = new ArrayList<>();
+        list2.add("");
+        list2.add("rrr");
+        list2.add("round");
+        list2.add("mouse");
+        System.out.println(ActionsWithList.countFilteredStrings(list2, 'r'));
+        System.out.println(ActionsWithList.filterStrings(list2, "ou"));
+    }
+}
