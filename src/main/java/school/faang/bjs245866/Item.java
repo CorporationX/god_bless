10eaafb2992eb6e4013ce97acfb2f05df1235874
Maksim-VV <laulmv11@gmail.com>
2024-12-17 15:39:46 +0300

    BJS2-45866, Lord of the Rings RPG  - alpha release

---
+++ src/main/java/school/faang/bjs245866/Item.java
@@ -0,0 +1,14 @@
+package school.faang.bjs245866;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@AllArgsConstructor
+@Getter
+@Setter
+public class Item {
+    private String name;
+    private int value;
+}
