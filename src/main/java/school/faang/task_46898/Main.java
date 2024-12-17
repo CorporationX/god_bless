d6dd4dff712e00562b978146b74aac5282b6e936
davidmiro <david_1_4@hotmail.com>
2024-12-17 00:27:57 -0500

    Fixed checkstyle errors.

--- src/main/java/school/faang/task_46898/Main.java
+++ src/main/java/school/faang/task_46898/Main.java
@@ -10,9 +10,21 @@ public class Main {
                 System.out.println("ERROR Notification received: " + notification.getMessage()));
         manager.registerHandler("SUCCESS", notification ->
                 System.out.println("SUCCESS " + notification.getMessage()));
+        manager.registerHandler("WARNING", notification ->
+                System.out.println("WARNING Notification received: " + notification.getMessage()));
+
+        manager.registerFilter("WARNING", notification ->
+                !notification.getMessage().toLowerCase().contains("ban"));
+        manager.registerModifier("WARNING", notification -> {
+            String sanitizedMessage = notification.getMessage()
+                    .replaceAll("(?i)ban", "***");
+            return new Notification(notification.getType(), sanitizedMessage);
+        });
 
         manager.sendNotification(new Notification("INFO", "Update complete"));
         manager.sendNotification(new Notification("ERROR", "Database connection error"));
         manager.sendNotification(new Notification("SUCCESS", "Backup complete"));
+        manager.sendNotification(new Notification("WARNING", "This is a ban message."));
+        manager.sendNotification(new Notification("WARNING", "Disk space is low."));
     }
 }
