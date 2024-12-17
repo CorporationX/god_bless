51c43947bd7a503638dc6c027f51c609fd8503ec
rostex <rostex2@yandex.ru>
2024-12-17 21:32:51 +0300

    Внесены изменения после ревью

--- src/main/java/school/faang/sprint_3/task_48855/Chore.java
+++ src/main/java/school/faang/sprint_3/task_48855/Chore.java
@@ -7,20 +7,20 @@ import lombok.Getter;
 @AllArgsConstructor
 public class Chore implements Runnable {
     private static final int FIVE_SECONDS = 5000;
-    String chore;
+    private String chore;
 
     @Override
     public void run() {
         if (chore == null) {
             throw new IllegalStateException("Задание не может быть пустым (null)");
         }
+        System.out.println(Thread.currentThread().getName() + " выполняется задачу " + chore);
         try {
-            System.out.println(Thread.currentThread().getName() + " выполняется задачу " + chore);
             Thread.sleep(FIVE_SECONDS);
             System.out.println("Задача " + chore + " выполнена");
         } catch (InterruptedException e) {
             Thread.currentThread().interrupt();
-            throw new RuntimeException("Задача " + chore + "была прервана: " + e.getMessage());
+            System.out.println("Задача " + chore + "была прервана: " + e.getMessage());
         }
     }
 }
