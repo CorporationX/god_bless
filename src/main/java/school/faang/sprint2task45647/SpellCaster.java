74be6f32b4c6364455b50b97360419fa12ffe366
elnur-elnur <tahirzadee@gmail.com>
2024-12-17 10:16:13 +0400

    Task: BJS2-45647 "Хогвартс, Хогвартс, наш любимый Хогвартс"
    Refactoring:
    - Added condition to method for null

--- src/main/java/school/faang/sprint2task45647/SpellCaster.java
+++ src/main/java/school/faang/sprint2task45647/SpellCaster.java
@@ -2,6 +2,11 @@ package school.faang.sprint2task45647;
 
 public class SpellCaster {
     public void cast(String spellName, SpellAction spellAct) {
+        if (spellName == null || spellAct == null) {
+            System.out.println("Ошибка: Заклинание или действие не может быть null");
+            return;
+        }
+
         String result = spellAct.castSpell(spellName);
         System.out.println("Результат заклинания: " + result);
     }
