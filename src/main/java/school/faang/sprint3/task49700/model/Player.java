761b32780204c9d7c1e08bfb269a95a4adc6525f
Alexey Gorokhov <almigor@mail.ru>
2024-12-17 22:50:02 +0300

    Supercow!

---
+++ src/main/java/school/faang/sprint3/task49700/model/Player.java
@@ -0,0 +1,25 @@
+package school.faang.sprint3.task49700.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.ToString;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@Getter
+@AllArgsConstructor
+@ToString
+public class Player {
+    private final String Name;
+
+    public void startBattle(Boss boss) {
+
+        try {
+            boss.joinBattle(this);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+
+    }
+
+}
