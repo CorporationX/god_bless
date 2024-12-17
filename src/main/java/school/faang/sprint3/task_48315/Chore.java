2fefc8be2b2cdd67c2c30558a24643f70c1b6bad
DmitryQueen <276246@student.pwr.edu.pl>
2024-12-18 00:39:53 +0100

    added const

--- src/main/java/school/faang/sprint3/task_48315/Chore.java
+++ src/main/java/school/faang/sprint3/task_48315/Chore.java
@@ -5,13 +5,14 @@ import lombok.SneakyThrows;
 
 @RequiredArgsConstructor
 public class Chore implements Runnable {
+    private static final int COMPLETING_TIME = 5000;
     private final String description;
 
     @SneakyThrows
     @Override
     public void run() {
         System.out.println(Thread.currentThread().getName() + " starts doing " + description);
-        Thread.sleep(5000);
+        Thread.sleep(COMPLETING_TIME);
         System.out.println(Thread.currentThread().getName() + " ended doing " + description);
     }
 }
