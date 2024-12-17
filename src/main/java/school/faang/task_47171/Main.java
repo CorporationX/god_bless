5ebb50b65bb17ecb40e7c8af9a15433b83f3cfca
Ilya Vysokolyan <rd2d@list.ru>
2024-12-17 18:57:57 +0300

    BJS2-47171: Потренируем StreamAPI 2(Добавил пустую строку)

--- src/main/java/school/faang/task_47171/Main.java
+++ src/main/java/school/faang/task_47171/Main.java
@@ -8,6 +8,7 @@ import java.util.Set;
 import java.util.stream.Collectors;
 
 public class Main {
+
     public static void main(String[] args) {
 
         List<Integer> numbers = List.of(1, 2, 3, 4, 5, 6);
