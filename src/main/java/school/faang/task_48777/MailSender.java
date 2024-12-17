33445db21f666065543369ef709930fa06036785
Mihail Ermolaev <fitis_rus21@mail.ru>
2024-12-17 20:07:44 +0300

    checkstyle + dry

--- src/main/java/school/faang/task_48777/MailSender.java
+++ src/main/java/school/faang/task_48777/MailSender.java
@@ -6,29 +6,19 @@ public class MailSender {
     @SneakyThrows
     public static void main(String[] args) {
 
-        SenderRunnable senderRunnable1 = new SenderRunnable(1, 200);
-        SenderRunnable senderRunnable2 = new SenderRunnable(201, 400);
-        SenderRunnable senderRunnable3 = new SenderRunnable(401, 600);
-        SenderRunnable senderRunnable4 = new SenderRunnable(601, 800);
-        SenderRunnable senderRunnable5 = new SenderRunnable(801, 1000);
+        final int countThread = 5;
+        final int batchSize = 200;
 
-        Thread thread1 = new Thread(senderRunnable1);
-        Thread thread2 = new Thread(senderRunnable2);
-        Thread thread3 = new Thread(senderRunnable3);
-        Thread thread4 = new Thread(senderRunnable4);
-        Thread thread5 = new Thread(senderRunnable5);
+        Thread[] threads = new Thread[countThread];
 
-        thread1.start();
-        thread2.start();
-        thread3.start();
-        thread4.start();
-        thread5.start();
+        for (int i = 0; i < countThread; i++) {
+            threads[i] = new Thread(new SenderRunnable(i * batchSize + 1, (i + 1) * batchSize));
+            threads[i].start();
+        }
 
-        thread1.join();
-        thread2.join();
-        thread3.join();
-        thread4.join();
-        thread5.join();
+        for (Thread thread : threads) {
+            thread.join();
+        }
 
         System.out.println("Все письма успешно отправлены!");
     }
