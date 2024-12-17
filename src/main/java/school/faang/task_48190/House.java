907801d40a1d5f845e5644c44e3e7e0c11c3082c
az3l1t <eternity_cr9p@mail.ru>
2024-12-17 10:54:56 +0300

    Az3l1t bjs2 48190

--- src/main/java/school/faang/task_48190/House.java
+++ src/main/java/school/faang/task_48190/House.java
@@ -3,12 +3,8 @@ package school.faang.task_48190;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
-import java.util.concurrent.Executors;
-import java.util.concurrent.ScheduledExecutorService;
-import java.util.concurrent.TimeUnit;
 import java.util.stream.Stream;
 
 @Data
@@ -16,47 +12,6 @@ import java.util.stream.Stream;
 public class House {
     private final List<Room> rooms;
     private List<Food> collectedFoods;
-    private static final int BUFFER_SIZE = 5;
-
-    public static void main(String[] args) throws InterruptedException {
-        List<Food> foods = List.of(
-                new Food("apple"), new Food("banana"), new Food("meat"), new Food("cheese"),
-                new Food("fish"), new Food("bread"), new Food("milk"), new Food("eggs"),
-                new Food("chocolate"), new Food("butter")
-        );
-
-        List<Room> rooms = List.of(
-                new Room(new ArrayList<>(List.of(foods.get(0), foods.get(1), foods.get(8)))),
-                new Room(new ArrayList<>(List.of(foods.get(2), foods.get(3), foods.get(4)))),
-                new Room(new ArrayList<>(List.of(foods.get(5), foods.get(6), foods.get(7)))),
-                new Room(new ArrayList<>(List.of(foods.get(9), foods.get(8), foods.get(0)))),
-                new Room(new ArrayList<>(List.of(foods.get(3), foods.get(5), foods.get(6), foods.get(7))))
-        );
-
-        House house = new House(rooms, new ArrayList<>());
-        ScheduledExecutorService executorService = Executors.newScheduledThreadPool(BUFFER_SIZE);
-
-        executorService.scheduleWithFixedDelay(
-                () -> {
-                    synchronized (house) {
-                        house.collectFood();
-                        if (house.allFoodCollected()) {
-                            executorService.shutdown();
-                            System.out.println("All food was collected");
-                        }
-                    }
-                }, 0, 5000, TimeUnit.MILLISECONDS);
-
-        try {
-            if (!executorService.awaitTermination(10, TimeUnit.SECONDS)) {
-                System.out.println("Forcing shutdown...");
-                executorService.shutdownNow();
-            }
-        } catch (InterruptedException e) {
-            System.out.println("Interrupted while waiting for termination");
-            executorService.shutdownNow();
-        }
-    }
 
     public void collectFood() {
         if (rooms.size() < 2) {
@@ -68,30 +23,37 @@ public class House {
         int firstIndex;
         while (true) {
             firstIndex = randomIndex.nextInt(rooms.size());
-            if (!rooms.get(firstIndex).getFoods().isEmpty()) {
+            if (!rooms.get(firstIndex).foods().isEmpty()) {
                 break;
             }
         }
+        Room firstRoom = rooms.get(firstIndex);
 
         int secondIndex;
         while (true) {
             secondIndex = randomIndex.nextInt(rooms.size());
-            if (firstIndex != secondIndex && !rooms.get(secondIndex).getFoods().isEmpty()) {
+            if (firstIndex != secondIndex && !rooms.get(secondIndex).foods().isEmpty()) {
                 break;
             }
         }
+        Room secondRoom = rooms.get(secondIndex);
 
-        Stream<Food> firstRandomFood = rooms.get(firstIndex).getFoods().stream();
-        Stream<Food> secondRandomFood = rooms.get(secondIndex).getFoods().stream();
+        if (firstRoom.foods().isEmpty() || secondRoom.foods().isEmpty()) {
+            System.out.println("One of the rooms does not have food!");
+            return;
+        }
+
+        Stream<Food> firstRandomFood = firstRoom.foods().stream();
+        Stream<Food> secondRandomFood = secondRoom.foods().stream();
         collectedFoods.addAll(Stream.concat(firstRandomFood, secondRandomFood).toList());
 
-        rooms.get(firstIndex).getFoods().clear();
-        rooms.get(secondIndex).getFoods().clear();
+        rooms.get(firstIndex).foods().clear();
+        rooms.get(secondIndex).foods().clear();
 
         System.out.println(collectedFoods);
     }
 
     public boolean allFoodCollected() {
-        return rooms.stream().allMatch(room -> room.getFoods().isEmpty());
+        return rooms.stream().allMatch(room -> room.foods().isEmpty());
     }
 }
--- src/main/java/school/faang/task_48190/Room.java
+++ src/main/java/school/faang/task_48190/Room.java
@@ -1,12 +1,5 @@
 package school.faang.task_48190;
 
-import lombok.AllArgsConstructor;
-import lombok.Data;
-
 import java.util.List;
 
-@Data
-@AllArgsConstructor
-public class Room {
-    private List<Food> foods;
-}
+public record Room(List<Food> foods) {}
