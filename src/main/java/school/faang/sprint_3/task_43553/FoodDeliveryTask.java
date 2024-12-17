8d0cd4b794f556086ba7af690e5b400988899aa8
KsushaTarasova <ksutaras543@mail.ru>
2024-12-17 12:28:04 +0300

    Add constants

--- src/main/java/school/faang/sprint_3/task_43553/FoodDeliveryTask.java
+++ src/main/java/school/faang/sprint_3/task_43553/FoodDeliveryTask.java
@@ -3,13 +3,14 @@ package school.faang.sprint_3.task_43553;
 import java.util.Random;
 
 public record FoodDeliveryTask(String character, int foodAmount) implements Runnable {
+    private static final int RANDOM_SLEEP_TIME = 5000;
+    private static final int BASE_SLEEP_TIME = 1000;
+
     @Override
     public void run() {
         String foodType = getFoodType();
         try {
-            System.out.println(character + " получает " + foodAmount + " " + foodType);
-            Thread.sleep(new Random().nextInt(5000) + 1000);
-            System.out.println(character + " ест " + foodAmount + " " + foodType);
+            Thread.sleep(new Random().nextInt(RANDOM_SLEEP_TIME) + BASE_SLEEP_TIME);
         } catch (InterruptedException e) {
             System.out.println(character + " не смог получить " + foodType + " из-за прерывания.");
         }
