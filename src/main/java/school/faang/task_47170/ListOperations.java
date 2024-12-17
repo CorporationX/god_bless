78170ccf5b49db6eb2a3b55d4201c62d7dfe6df0
Dias Kamza <kamzadias31@gmail.com>
2024-12-17 18:00:40 +0500

    Added ListOperations class and usage in Main

---
+++ src/main/java/school/faang/task_47170/ListOperations.java
@@ -0,0 +1,65 @@
+package school.faang.task_47170;
+
+import java.util.Comparator;
+import java.util.List;
+import java.util.NoSuchElementException;
+import java.util.function.Predicate;
+import java.util.stream.Collectors;
+
+public class ListOperations {
+    protected static int sumOfEvenNumbers(List<Integer> numbers) {
+        return numbers.stream()
+                .filter(x -> x % 2 == 0)
+                .mapToInt(Integer::intValue)
+                .sum();
+    }
+
+    protected static int findMax(List<Integer> numbers) {
+        return numbers.stream()
+                .max(Integer::compareTo)
+                .orElseThrow(() -> new NoSuchElementException("Empty list"));
+    }
+
+    protected static double findAverage(List<Integer> numbers) {
+        return numbers.stream()
+                .mapToInt(Integer::intValue)
+                .average()
+                .orElse(0.0);
+    }
+
+    protected static int countStringsStartingWith(List<String> strings, char prefix) {
+        return (int) strings.stream()
+                .filter(s -> !s.isEmpty() && s.charAt(0) == prefix)
+                .count();
+    }
+
+    protected static List<String> filterStringsContainingSubstring(List<String> strings, String substring) {
+        return strings.stream()
+                .filter(s -> !s.isEmpty() && s.contains(substring))
+                .collect(Collectors.toList());
+    }
+
+    protected static List<String> sortByLength(List<String> strings) {
+        return strings.stream()
+                .sorted(Comparator.comparingInt(String::length))
+                .collect(Collectors.toList());
+    }
+
+    protected static boolean allMatchCondition(List<Integer> numbers, Predicate<Integer> condition) {
+        return numbers.stream()
+                .allMatch(condition); // Проверяем условие для всех элементов
+    }
+
+    public static int findMinGreaterThan(List<Integer> numbers, int threshold) {
+        return numbers.stream()
+                .filter(n -> n > threshold)
+                .min(Integer::compareTo)
+                .orElseThrow(() -> new NoSuchElementException("Empty list"));
+    }
+
+    public static List<Integer> convertToLengths(List<String> strings) {
+        return strings.stream()
+                .map(String::length)
+                .collect(Collectors.toList());
+    }
+}
