4bd13e12c58f6f34a1d873935b1aeb5a1fb53d2b
alterality <rordufe@gmail.com>
2024-12-17 15:31:45 +0600

    update code

--- src/main/java/school/faang/sprint_2/task_45970/TriangleAreaCalculator.java
+++ src/main/java/school/faang/sprint_2/task_45970/TriangleAreaCalculator.java
@@ -1,15 +1,14 @@
 package school.faang.sprint_2.task_45970;
 
-import java.util.function.Function;
+import java.util.function.BiFunction;
 import java.util.stream.Stream;
 
 public class TriangleAreaCalculator {
 
-    Function<Double, Function<Double, Double>> add = (x) -> (y) -> x + y;
-    Function<Double, Function<Double, Double>> multiply = (x) -> (y) -> x * y;
-    Function<Double, Function<Double, Double>> subtract = (x) -> (y) -> x - y;
-    Function<Double, Function<Double, Double>> divide = (x) -> (y) -> x / y;
-    Function<Double, Double> squareRoot = Math::sqrt;
+    BiFunction<Double, Double, Double> add = Double::sum;
+    BiFunction<Double, Double, Double> multiply = (x, y) -> x * y;
+    BiFunction<Double, Double, Double> subtract = (x, y) -> x - y;
+    BiFunction<Double, Double, Double> divide = (x, y) -> x / y;
 
     public Double calculateTriangleArea(double sideA, double sideB, double sideC) throws IllegalArgumentException {
         if (!isValidTriangle(sideA, sideB, sideC)) {
@@ -17,20 +16,20 @@ public class TriangleAreaCalculator {
         }
         Double semiPerimeter = divide.apply(
                 Stream.of(sideA, sideB, sideC)
-                        .reduce(0.0, (acc, side) -> add.apply(acc).apply(side))).apply(2.0);
+                        .reduce(0.0, add::apply), 2.0);
 
-        return squareRoot.apply(
+        return Math.sqrt(
                 Stream.of(sideA, sideB, sideC)
-                        .map(side -> subtract.apply(semiPerimeter).apply(side))
-                        .reduce(semiPerimeter, (acc, value) -> multiply.apply(acc).apply(value))
+                        .map(side -> subtract.apply(semiPerimeter, side))
+                        .reduce(semiPerimeter, multiply::apply)
         );
     }
 
     private boolean isValidTriangle(double sideA, double sideB, double sideC) {
         return sideA > 0 && sideB > 0 && sideC > 0
-                && sideA < add.apply(sideB).apply(sideC)
-                && sideB < add.apply(sideA).apply(sideC)
-                && sideC < add.apply(sideA).apply(sideB);
+                && sideA < add.apply(sideB, sideC)
+                && sideB < add.apply(sideA, sideC)
+                && sideC < add.apply(sideA, sideB);
     }
 
     public static void main(String[] args) {
