019d91df3cb15621548d03b298e0416600a8039a
Максим <vint_vin@mail.ru>
2024-12-17 21:16:02 +1000

    Gotovo

--- src/main/java/school/faang/sprint2/task47290analizusers/Main.java
+++ src/main/java/school/faang/sprint2/task47290analizusers/Main.java
@@ -9,10 +9,6 @@ public class Main {
     public static void main(String[] args) {
         List<UserAction> actions;
         actions = Arrays.asList(
-                new UserAction(1, "Alice", "post", LocalDate.of(2024, 9, 1),
-                        "Check out this amazing #newfeature!"),
-                new UserAction(2, "Bob", "comment", LocalDate.of(2024, 9, 2),
-                        "I totally agree with #newfeature."),
                 new UserAction(1, "Alice", "like", LocalDate.of(2024, 9, 3),
                         ""),
                 new UserAction(3, "Charlie", "share",
