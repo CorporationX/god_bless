30a0fcdc7749caf30f7688a3994e5682ab8233cc
Dias Kamza <kamzadias31@gmail.com>
2024-12-17 12:49:00 +0500

    Added ErrorHandler, ThrowingSupplier and usage in Main

---
+++ src/main/java/school/faang/task_46284/ErrorHandler.java
@@ -0,0 +1,13 @@
+package school.faang.task_46284;
+
+import java.util.function.Function;
+
+public class ErrorHandler {
+    public static <T> T withErrorHandling(ThrowingSupplier<T> action, Function<Exception, T> errorHandler) {
+        try {
+            return action.get();
+        } catch (Exception e) {
+            return errorHandler.apply(e);
+        }
+    }
+}
