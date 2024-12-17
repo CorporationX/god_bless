269f7b11e9a9b3cefda00bf5d759f08b410f3e57
IgnatovEI <ignatovei96@mail.ru>
2024-12-17 07:19:38 +0300

    task_46908 solution

---
+++ src/main/java/school/faang/task_46908/Notification.java
@@ -0,0 +1,14 @@
+package school.faang.task_46908;
+
+import lombok.Getter;
+
+@Getter
+public class Notification {
+    private String type;
+    private String message;
+
+    public Notification(String type, String message) {
+        this.type = type;
+        this.message = message;
+    }
+}
