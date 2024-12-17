158df37ce8e1cb44fc6f687588045efe14a41a49
Maria Bortsova <bortsovamariia@gmail.com>
2024-12-17 16:58:52 +0300

    task army impl

---
+++ src/main/java/school/faang/task_47613/Unit.java
@@ -0,0 +1,12 @@
+package school.faang.task_47613;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import lombok.ToString;
+
+@RequiredArgsConstructor
+@Getter
+@ToString
+public class Unit {
+    private final int power;
+}
