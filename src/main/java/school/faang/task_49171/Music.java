246ac0d6a99a154241a9fba5fed0426ee8754e0f
triplek <weribel@mail.ru>
2024-12-18 11:55:13 +0300

    Google Photo sync(исправленно)

--- src/main/java/school/faang/task_49171/Music.java
+++ src/main/java/school/faang/task_49171/Music.java
@@ -1,16 +1,17 @@
 package school.faang.task_49171;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Music {
     public static void main(String[] args) {
         Player player = new Player();
-        Thread playThread = new Thread(() -> player.play());
-        Thread pauseThread = new Thread(() -> player.pause());
-        Thread skipThread = new Thread(() -> player.skip());
-        final Thread previousThread = new Thread(() -> player.previous());
+        List<Thread> threadsList = new ArrayList<>(List.of(new Thread(() -> player.play()),
+                new Thread(() -> player.pause()),
+                new Thread(() -> player.skip()), new Thread(() -> player.previous())));
 
-        playThread.start();
-        pauseThread.start();
-        skipThread.start();
-        previousThread.start();
+        for (Thread thread : threadsList) {
+            thread.start();
+        }
     }
 }
