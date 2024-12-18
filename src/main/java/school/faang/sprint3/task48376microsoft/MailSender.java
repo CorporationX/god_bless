5e6a9d75ec8ad111dfd730b52be70c5d8307d8dc
Максим <vint_vin@mail.ru>
2024-12-18 23:56:47 +1000

    Исправленно

--- src/main/java/school/faang/sprint3/task48376microsoft/MailSender.java
+++ src/main/java/school/faang/sprint3/task48376microsoft/MailSender.java
@@ -1,8 +1,11 @@
 package school.faang.sprint3.task48376microsoft;
 
 public class MailSender {
-    public static void main(String[] args) throws InterruptedException {
-        SenderRunnable runnable = new SenderRunnable(0, 200);
+    public static void main(String[] args) {
+        final int START = 0;
+        final int END = 200;
+
+        SenderRunnable runnable = new SenderRunnable(START, END);
         Thread thread = new Thread(runnable);
         int createdThreads = 0;
         for (int i = 0; i < 5; i++) {
@@ -10,7 +13,12 @@ public class MailSender {
             createdThreads++;
         }
 
-        thread.join();
+        try {
+            thread.join();
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+
         System.out.println("Все письма отправлены");
         System.out.println("Всего создано потоков: " + createdThreads);
     }
