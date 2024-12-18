6314be9a86a4d997a1c137baf0e5db415964768c
alexey_obraztsov <alexey.autotester86@gmail.com>
2024-12-18 15:22:13 +0300

    BJS2-48398. They were very nice but extremely poor. Threads. initial

---
+++ src/main/java/school/faang/sprint_1/task_theverynicebjs2n48398/Chore.java
@@ -0,0 +1,17 @@
+package school.faang.sprint_1.task_theverynicebjs2n48398;
+
+import lombok.AllArgsConstructor;
+import lombok.NonNull;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@AllArgsConstructor
+public class Chore implements Runnable {
+    @NonNull
+    private String chore;
+
+    @Override
+    public void run() {
+        log.info("{} выполняет задачу \"{}\"", Thread.currentThread().getName(), chore);
+    }
+}
