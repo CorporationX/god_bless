21a784fe775c98ad86d658a01ad69901aa6c301f
Mihail Ermolaev <fitis_rus21@mail.ru>
2024-12-18 22:20:05 +0300

    начало They were very nice but extremely poor

--- src/main/java/school/faang/task_48899/Chore.java
+++ src/main/java/school/faang/task_48899/Chore.java
@@ -9,6 +9,12 @@ public class Chore implements Runnable{
 
     @Override
     public void run() {
+        Thread.currentThread().getName();
+        try {
+            Thread.sleep(1000);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
 
     }
 }
---
+++ src/main/java/school/faang/task_48899/WeasleyFamily.java
@@ -0,0 +1,11 @@
+package school.faang.task_48899;
+
+public class WeasleyFamily {
+
+    Chore chore1 = new Chore("помыть посуду");
+    Chore chore2 = new Chore("подмести пол");
+    Chore chore3 = new Chore("приготовить ужин");
+
+    Chore[] chores = new Chore[] {chore1,chore2, chore3};
+
+}
