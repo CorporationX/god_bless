b2a72835862ac7839ecb1766dfc0901a5713fc73
Kairat <snowmango.official@gmail.com>
2024-12-18 13:58:19 +0500

    replace constants and add age threshold constant

--- src/main/java/school/faang/task_43780/User.java
+++ src/main/java/school/faang/task_43780/User.java
@@ -7,17 +7,17 @@ import java.util.*;
 @ToString
 public class User {
 
+    private final int AGE_THRESHOLD = 18;
+    private static final List<String> VALID_JOBS = Arrays.asList("Google", "Uber", "Amazon");
+    private static final List<String> VALID_ADDRESSES = Arrays.asList("London", "New York", "Amsterdam");
     private String name;
     private int age;
     private String workPlace;
     private String address;
 
-    private static final List<String> VALID_JOBS = Arrays.asList("Google", "Uber", "Amazon");
-    private static final List<String> VALID_ADDRESSES = Arrays.asList("London", "New York", "Amsterdam");
-
     public User(String name, int age, String workPlace, String address) {
         this.name = Optional.ofNullable(name).orElseThrow(IllegalArgumentException::new);
-        this.age = Optional.ofNullable(age > 18 ? age : null).orElseThrow(IllegalArgumentException::new);
+        this.age = Optional.ofNullable(age > AGE_THRESHOLD ? age : null).orElseThrow(IllegalArgumentException::new);
         this.workPlace = Optional.ofNullable(VALID_JOBS.contains(workPlace) ? workPlace : null)
                 .orElseThrow(IllegalArgumentException::new);
         this.address = Optional.ofNullable(VALID_ADDRESSES.contains(address) ? address : null)
