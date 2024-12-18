5eba5ca62e21351392436a63df156fdd9c917a6c
Kairat <snowmango.official@gmail.com>
2024-12-18 16:00:31 +0500

    add inventory manager which can add remove and update

---
+++ src/main/java/school/faang/task_45888/Item.java
@@ -0,0 +1,14 @@
+package school.faang.task_45888;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@Setter
+@Getter
+@AllArgsConstructor
+public class Item {
+
+    private String name;
+    private int value;
+}
