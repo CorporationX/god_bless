d5068c0faf9bd058a1ed9c2c6245c57b54459411
Mihail Ermolaev <fitis_rus21@mail.ru>
2024-12-18 21:15:54 +0300

    начало They were very nice but extremely poor

---
+++ src/main/java/school/faang/task_48899/Chore.java
@@ -0,0 +1,14 @@
+package school.faang.task_48899;
+
+public class Chore implements Runnable{
+    private String chore;
+
+    public Chore(String chore) {
+        this.chore = chore;
+    }
+
+    @Override
+    public void run() {
+
+    }
+}
