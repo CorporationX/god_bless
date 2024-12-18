1467b51b7fc2dba7049d66bae7bd428fd59b1375
sergeybabaev96 <166114022+sergeybabaev96@users.noreply.github.com>
2024-12-18 20:19:56 +0300

    Исправил работу метода sorted. Добавил reference вместо реализации лямбда выражения

--- src/main/java/school/faang/bjs247293/StreamOperations.java
+++ src/main/java/school/faang/bjs247293/StreamOperations.java
@@ -2,6 +2,7 @@ package school.faang.bjs247293;
 
 import java.util.Arrays;
 import java.util.Collections;
+import java.util.Comparator;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
@@ -11,15 +12,13 @@ import java.util.stream.Collectors;
 
 public class StreamOperations {
     public static Set<List<Integer>> uniquePairNumbers(List<Integer> numbers, int sumPairNumbers) {
-        Set<List<Integer>> uniquePairs = new HashSet<>();
         Set<Integer> number = new HashSet<>();
         number.addAll(numbers);
-        uniquePairs = numbers.stream()
+        return numbers.stream()
                 .filter(num -> number.contains(sumPairNumbers - num))
                 .map(num -> Arrays.asList(num, sumPairNumbers - num))
                 .peek(list -> Collections.sort(list))
                 .collect(Collectors.toSet());
-        return uniquePairs;
     }
 
     public static List<String> sortingCountry(Map<String, String> cityCountry) {
@@ -32,20 +31,20 @@ public class StreamOperations {
     public static List<String> filterBySymbolAndSorting(List<String> strings, char symbol) {
         return strings.stream()
                 .filter(word -> Objects.equals(Character.toLowerCase(word.charAt(0)), Character.toLowerCase(symbol)))
-                .sorted(((o1, o2) -> o1.length() - o2.length()))
+                .sorted(Comparator.comparingInt(String::length))
                 .toList();
     }
 
     public static List<String> mappingNumInWordDivided(List<Integer> numbers) {
         return numbers.stream()
-                .map(integer -> Integer.toBinaryString(integer))
+                .map(Integer::toBinaryString)
                 .toList();
     }
 
     public static List<String> filterWordByAlphabet(List<String> words, String alphabet) {
         return words.stream()
                 .filter(word -> word.matches("[" + alphabet + "]+"))
-                .sorted((o1, o2) -> o1.length() - o2.length())
+                .sorted(Comparator.comparingInt(String::length))
                 .toList();
     }
 }
