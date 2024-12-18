55e40118c2cd0760324c0e8026eb5f7b12ade481
therealadik <vladerm2000@yandex.ru>
2024-12-18 13:37:01 +0500

    fix

--- src/main/java/school/faang/task_49197/TelegramBot.java
+++ src/main/java/school/faang/task_49197/TelegramBot.java
@@ -8,7 +8,7 @@ import java.time.LocalDateTime;
 @Data
 public class TelegramBot {
     public static final int REQUEST_LIMIT = 5;
-    private volatile int requestCounter = 0;
+    private int requestCounter = 0;
     private LocalDateTime lastRequest = LocalDateTime.now();
 
     public synchronized void sendMessage(String message) {
@@ -26,7 +26,7 @@ public class TelegramBot {
                     wait(1000);
                     continue;
                 } catch (InterruptedException e) {
-                    System.out.println("Ошибка выполнения потока");
+                    System.out.println("Ошибка выполнения потока: " + Thread.currentThread());
                 }
             }
 
