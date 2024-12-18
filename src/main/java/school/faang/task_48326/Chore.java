3230ee42f8953937b68ffc1e3fa607e058d9bc51
Мария Борцова <bortsovamm@intech.rshb.ru>
2024-12-18 14:39:30 +0300

    refactor after review

--- src/main/java/school/faang/task_48326/Chore.java
+++ src/main/java/school/faang/task_48326/Chore.java
@@ -5,13 +5,14 @@ import lombok.SneakyThrows;
 
 @RequiredArgsConstructor
 public class Chore implements Runnable {
+    public static final int ONE_SECOND = 1000;
     private final String chore;
 
     @SneakyThrows
     @Override
     public void run() {
         System.out.printf("Выполняется задача = %s, в потоке = %s%n", chore, Thread.currentThread().getName());
-        Thread.sleep(1000);
+        Thread.sleep(ONE_SECOND);
         System.out.printf("Выполнилась задача = %s, в потоке = %s%n", chore, Thread.currentThread().getName());
     }
 }
