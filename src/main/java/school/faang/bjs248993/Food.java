e6706c5308bad03aab043bb48d56d7a6ac7d0aac
Parvin Etibarli <etibarliparvin@gmail.com>
2024-12-18 13:10:21 +0400

    bjs2-48993
    * Mice are very nice! is finished

---
+++ src/main/java/school/faang/bjs248993/Food.java
@@ -0,0 +1,16 @@
+package school.faang.bjs248993;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Setter
+@Getter
+@ToString
+@AllArgsConstructor
+public class Food {
+
+    private String name;
+
+}
---
+++ src/main/java/school/faang/bjs248993/Room.java
@@ -0,0 +1,16 @@
+package school.faang.bjs248993;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.List;
+
+@Setter
+@Getter
+@AllArgsConstructor
+public class Room {
+
+    private List<Food> foods;
+
+}
