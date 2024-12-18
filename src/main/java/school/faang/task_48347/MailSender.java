c528e5982a94c7b208057afbb6649e4dd888ea62
shamshaev <an.shamshaev@gmail.com>
2024-12-19 00:15:11 +0300

    Add exception logging

--- src/main/java/school/faang/task_48347/MailSender.java
+++ src/main/java/school/faang/task_48347/MailSender.java
@@ -1,8 +1,12 @@
 package school.faang.task_48347;
 
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 public class MailSender {
     private static final int BATCH_SIZE = 200;
     private static final int TOTAL_SIZE = 1000;
+    private static final Logger LOGGER = Logger.getLogger(MailSender.class.getName());
 
     public static void main(String[] args) {
         int threadsCount = TOTAL_SIZE / BATCH_SIZE;
@@ -17,7 +21,7 @@ public class MailSender {
             try {
                 mailSenderThread.join();
             } catch (InterruptedException e) {
-                throw new RuntimeException(e.getMessage());
+                LOGGER.log(Level.WARNING, "Thread was interrupted", e);
             }
         }
 
