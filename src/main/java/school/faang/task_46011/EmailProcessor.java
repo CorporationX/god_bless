fe6d9a12839fd9c22fc780a191894133e6d572f4
Kairat <snowmango.official@gmail.com>
2024-12-18 15:24:41 +0500

    move each parameter in new line

--- src/main/java/school/faang/task_46011/EmailProcessor.java
+++ src/main/java/school/faang/task_46011/EmailProcessor.java
@@ -7,7 +7,10 @@ import java.util.function.Predicate;
 
 public class EmailProcessor {
 
-    public void processEmails(List<Email> emails, Predicate<Email> filter, Function<Email, String> converter, Consumer<Email> consumer) {
+    public void processEmails(List<Email> emails,
+                              Predicate<Email> filter,
+                              Function<Email, String> converter,
+                              Consumer<Email> consumer) {
         for (Email email : emails) {
             if (filter.test(email)) {
                 String converted = converter.apply(email);
