64f014c78a8c487a0d28b8bcfd68acc71874695f
Daria <dashok.sok@gmail.com>
2024-12-18 22:14:14 +0300

    Исправлены комментарии после ревью

--- src/main/java/school/faang/nicebutpoor/Chore.java
+++ src/main/java/school/faang/nicebutpoor/Chore.java
@@ -6,13 +6,15 @@ import lombok.Getter;
 @Getter
 @AllArgsConstructor
 public class Chore implements Runnable {
+    private static final int SLEEP_TIME = 5000;
+
     private final String chore;
 
     @Override
     public void run() {
         System.out.println(Thread.currentThread().getName() + " выполняет задачу " + chore);
         try {
-            Thread.sleep(5000);
+            Thread.sleep(SLEEP_TIME);
             System.out.println(chore + " выполнена!");
         } catch (InterruptedException e) {
             System.out.println(chore + " была прервана :(");
