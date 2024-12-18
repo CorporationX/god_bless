6ceeb449e2310a3c82f6badb613e95581137bcb0
Pavel <45390423+rsticks@users.noreply.github.com>
2024-12-19 01:47:39 +0300

    BJS2-48720 Вы работаете в Microsoft!

--- src/main/java/school/faang/bjs248720/MailSender.java
+++ src/main/java/school/faang/bjs248720/MailSender.java
@@ -1,9 +1,23 @@
 package school.faang.bjs248720;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 public class MailSender {
+    private static final int FULL_BATCH = 1000;
+    private static final int BATCH = 200;
+    private static final Logger LOGGER = LoggerFactory.getLogger(MailSender.class);
+
     public static void main(String[] args) {
-        for (int i = 0; i < 1000; i += 200) {
-            new Thread(new SenderRunnable(i + 1, i + 200)).start();
+        for (int i = 0; i < FULL_BATCH; i += BATCH) {
+            Thread thread = new Thread(new SenderRunnable(i + 1, i + 200));
+            thread.start();
+            try {
+                thread.join();
+            } catch (InterruptedException e) {
+                LOGGER.error("Thread = {} interrupted", thread.getName());
+            }
         }
+        LOGGER.info("All threads finished!");
     }
 }
--- src/main/java/school/faang/bjs248720/SenderRunnable.java
+++ src/main/java/school/faang/bjs248720/SenderRunnable.java
@@ -1,8 +1,12 @@
 package school.faang.bjs248720;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 public class SenderRunnable implements Runnable {
     private final int startIndex;
     private final int endIndex;
+    private static final Logger LOGGER = LoggerFactory.getLogger(SenderRunnable.class);
 
     public SenderRunnable(int startIndex, int endIndex) {
         this.startIndex = startIndex;
@@ -12,7 +16,7 @@ public class SenderRunnable implements Runnable {
     @Override
     public void run() {
         for (int i = startIndex; i <= endIndex; i++) {
-            System.out.printf("Письмо номер=%d отправлено\n", i);
+            LOGGER.info("Письмо номер={} отправлено", i);
         }
     }
 }
