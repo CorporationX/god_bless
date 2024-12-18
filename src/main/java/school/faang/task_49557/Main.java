df00e3e73704c1fb2f6867ac3ae4cb6a8eaf3751
therealadik <vladerm2000@yandex.ru>
2024-12-18 13:32:47 +0500

    fix

--- src/main/java/school/faang/task_49557/Main.java
+++ src/main/java/school/faang/task_49557/Main.java
@@ -9,10 +9,15 @@ import java.util.concurrent.TimeUnit;
 import java.util.function.Consumer;
 
 public class Main {
+    private static final int TASK_COUNT = 10;
+    private static final int THREAD_COUNT = 5;
+    private static final int VLADS_COUNT = 10;
+    private static final int TIMEOUT_MINUTES = 5;
+
     public static void main(String[] args) {
         VladController controller = new VladController();
 
-        for (int i = 0; i < 5; i++) {
+        for (int i = 0; i < VLADS_COUNT; i++) {
             controller.addVlad();
         }
 
@@ -22,17 +27,17 @@ public class Main {
         methods.add(VladController::sleepAll);
         methods.add(VladController::cleanAll);
 
-        ExecutorService executorService = Executors.newFixedThreadPool(5);
+        ExecutorService executorService = Executors.newFixedThreadPool(THREAD_COUNT);
         Random random = new Random();
 
-        for (int i = 0; i < 10; i++) {
+        for (int i = 0; i < TASK_COUNT; i++) {
             executorService.submit(() -> methods.get(random.nextInt(0, methods.size())).accept(controller));
         }
 
         executorService.shutdown();
 
         try {
-            if (executorService.awaitTermination(5, TimeUnit.MINUTES)) {
+            if (executorService.awaitTermination(TIMEOUT_MINUTES, TimeUnit.MINUTES)) {
                 System.out.println("Задачи завершены. Влад свободен");
             } else {
                 System.out.println("Времени не хватило.");
