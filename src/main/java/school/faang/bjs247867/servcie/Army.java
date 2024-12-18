e79c1043ab6ee82bd31873ed475b2d7027fa77ae
salikhdev <salikhdev@gmail.com>
2024-12-18 14:11:39 +0500

    Распределенная армия героев меча и магии

--- src/main/java/school/faang/bjs247867/servcie/Army.java
+++ src/main/java/school/faang/bjs247867/servcie/Army.java
@@ -8,21 +8,27 @@ import java.util.List;
 public class Army {
     private final List<Character> characters = new ArrayList<>();
 
-    public int calculateTotalPower() throws InterruptedException {
+    public int calculateTotalPower() {
         final List<Thread> threads = new ArrayList<>();
         final List<PowerCalculator> tasks = new ArrayList<>();
 
         int totalPower = 0;
+        PowerCalculator calculator;
+        Thread thread;
         for (Character character : characters) {
-            PowerCalculator calculator = new PowerCalculator(character);
+            calculator = new PowerCalculator(character);
             tasks.add(calculator);
-            Thread thread = new Thread(calculator);
+            thread = new Thread(calculator);
             threads.add(thread);
             thread.start();
         }
 
-        for (Thread thread : threads) {
-            thread.join();
+        for (Thread myThread : threads) {
+            try {
+                myThread.join();
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
         }
 
         for (PowerCalculator task : tasks) {
