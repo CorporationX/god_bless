a4adc9b39744faeb3ebada784d881c769fc93c82
AesaQ <ilyaovchinnikov2095@gmail.com>
2024-12-18 17:04:44 +0500

    bugfix

--- src/main/java/school/faang/streamapi_1/ListOperations.java
+++ src/main/java/school/faang/streamapi_1/ListOperations.java
@@ -5,87 +5,78 @@ import java.util.function.Predicate;
 
 public class ListOperations {
 
-    public static int sumOfEvenNumbers(List<Integer> numbers) {
+    public static int sumOfEvenNumbers(List<Integer> numbers) throws Exception {
         if (numbers.isEmpty()) {
-            System.out.println("List is empty");
-            return 0;
+            throw new Exception("List is empty");
         }
         return numbers.stream()
                 .filter(number -> number % 2 == 0)
                 .mapToInt(number -> number).sum();
     }
 
-    public static OptionalInt findMax(List<Integer> numbers) {
+    public static OptionalInt findMax(List<Integer> numbers) throws Exception {
         if (numbers.isEmpty()) {
-            System.out.println("List is empty");
-            return OptionalInt.of(0);
+            throw new Exception("List is empty");
         }
         return numbers.stream()
                 .mapToInt(number -> number).max();
     }
 
-    public static OptionalDouble findAverage(List<Integer> numbers) {
+    public static OptionalDouble findAverage(List<Integer> numbers) throws Exception {
         if (numbers.isEmpty()) {
-            System.out.println("List is empty");
-            return OptionalDouble.of(0);
+            throw new Exception("List is empty");
         }
         return numbers.stream()
                 .mapToInt(number -> number).average();
     }
 
-    public static long countStringsStartingWith(List<String> strings, char symbol) {
+    public static long countStringsStartingWith(List<String> strings, char symbol) throws Exception {
         if (strings.isEmpty()) {
-            System.out.println("List is empty");
-            return 0;
+            throw new Exception("List is empty");
         }
         return strings.stream()
                 .filter(s -> s.startsWith(String.valueOf(symbol)))
                 .count();
     }
     
-    public static long filterStringsContainingSubstring(List<String> strings, String prefix) {
+    public static long filterStringsContainingSubstring(List<String> strings, String prefix) throws Exception {
         if (strings.isEmpty()) {
-            System.out.println("List is empty");
-            return 0;
+            throw new Exception("List is empty");
         }
         return strings.stream()
                 .filter(s -> s.contains(prefix))
                 .count();
     }
 
-    public static List<String> sortByLength(List<String> strings) {
+    public static List<String> sortByLength(List<String> strings) throws Exception {
         if (strings.isEmpty()) {
-            System.out.println("List is empty");
-            return new ArrayList<>();
+            throw new Exception("List is empty");
         }
         return strings.stream()
                 .sorted(Comparator.comparingInt(String::length))
                 .toList();
     }
 
-    public static boolean allMatchCondition(List<Integer> numbers, Predicate<Integer> predicate) {
+    public static boolean allMatchCondition(List<Integer> numbers, Predicate<Integer> predicate) throws Exception {
         if (numbers.isEmpty()) {
-            System.out.println("List is empty");
-            return false;
+            throw new Exception("List is empty");
         }
         return numbers.stream()
                 .allMatch(predicate);
     }
 
-    public static OptionalInt findMinGreaterThan(List<Integer> numbers, Integer limit) {
+    public static OptionalInt findMinGreaterThan(List<Integer> numbers, Integer limit) throws Exception {
         if (numbers.isEmpty()) {
-            System.out.println("List is empty");
-            return OptionalInt.empty();
+            throw new Exception("List is empty");
         }
         return numbers.stream()
                 .filter(number -> number > limit)
                 .mapToInt(number -> number).min();
     }
 
-    public static List<Integer> convertToLengths(List<String> numbers) {
+    public static List<Integer> convertToLengths(List<String> numbers) throws Exception {
         if (numbers.isEmpty()) {
-            System.out.println("List is empty");
-            return new ArrayList<>();
+            throw new Exception("List is empty");
         }
         return numbers.stream().map(String::length).toList();
     }
