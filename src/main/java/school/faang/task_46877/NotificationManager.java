de85a24b6096dbff2fad7ecb6db67490bde7a99f
Shakhzod Kamolov <shaxzod.kamolov@keysoft.uz>
2024-12-18 16:19:40 +0500

    feature(BJS2-46877) Add register and send notification

---
+++ src/main/java/school/faang/task_46877/NotificationManager.java
@@ -0,0 +1,27 @@
+package school.faang.task_46877;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.function.Consumer;
+
+@Getter
+@Setter
+public class NotificationManager {
+    Map<String, Consumer<Notification>> consumerMap = new HashMap<>();
+
+    public void registerHandler(String type, Consumer<Notification> notificationConsumer) {
+        consumerMap.putIfAbsent(type, notificationConsumer);
+    }
+
+    public void sendNotification(Notification notification) {
+        Consumer<Notification> consumer = consumerMap.get(notification.getType());
+        if (consumer != null) {
+            consumer.accept(notification);
+        } else {
+            System.out.printf("Обработчик для типа '%s' не зарегистрирован.%n", notification.getType());
+        }
+    }
+}
