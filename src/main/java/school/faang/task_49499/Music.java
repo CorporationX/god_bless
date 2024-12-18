f70c7940446771e4e405ee34bd3e45b86a158fd2
narol01 <kmingogod@gmail.com>
2024-12-19 00:43:19 +0100

    BJS2-49499

---
+++ src/main/java/school/faang/task_49499/Music.java
@@ -0,0 +1,33 @@
+package school.faang.task_49499;
+
+public class Music {
+    public static void main(String[] args) {
+        Playlist playlist = new Playlist();
+        playlist.addSong("Song 1");
+        playlist.addSong("Song 2");
+        playlist.addSong("Song 3");
+
+
+        Player player = new Player(playlist);
+        Thread playThread = new Thread(player::play);
+        Thread pauseThread = new Thread(player::pause);
+        Thread skipThread = new Thread(player::skip);
+        Thread previousThread = new Thread(player::previous);
+
+        playThread.start();
+        pauseThread.start();
+        skipThread.start();
+        previousThread.start();
+
+        try {
+            playThread.join();
+            pauseThread.join();
+            skipThread.join();
+            previousThread.join();
+        } catch (InterruptedException e) {
+            Thread.currentThread().interrupt();
+        }
+
+        System.out.println("Simulation finished.");
+    }
+}
