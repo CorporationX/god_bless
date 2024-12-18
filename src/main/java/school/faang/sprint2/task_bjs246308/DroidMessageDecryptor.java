58ad3992236b471b873611318a23dd302327b9d4
Denis Zhagot <82223141+zhagot@users.noreply.github.com>
2024-12-18 13:08:12 +0300

    BJS2-46308 Droid secrets (#15551)
    * BJS2-46308 Droid secrets
    
    * BJS2-46308 fix after review

---
+++ src/main/java/school/faang/sprint2/task_bjs246308/DroidMessageDecryptor.java
@@ -0,0 +1,6 @@
+package school.faang.sprint2.task_bjs246308;
+
+@FunctionalInterface
+public interface DroidMessageDecryptor {
+    String decrypt(String message, int keyEncrypt);
+}
---
+++ src/main/java/school/faang/sprint2/task_bjs246308/DroidMessageEncryptor.java
@@ -0,0 +1,6 @@
+package school.faang.sprint2.task_bjs246308;
+
+@FunctionalInterface
+public interface DroidMessageEncryptor {
+    String encrypt(String message, int keyEncrypt);
+}
