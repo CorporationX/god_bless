6a41973bc227d745fd8306bab3fd10f6f426729a
Dmitriy <dda20040609@gmail.com>
2024-12-18 15:32:19 +0300

    commit 1 Вы работаете в Microsoft!

---
+++ src/main/java/school/faang/task_48396/MailSender.java
@@ -0,0 +1,29 @@
+package school.faang.task_48396;
+
+public class MailSender {
+
+    public static void main(String[] args) {
+        final int emailsThread = 200;
+        final int numbersOfThread = 5;
+        Thread[] theards = new Thread[numbersOfThread];
+
+        for (int i = 0; i < numbersOfThread; i++) {
+            int startIndex = i * emailsThread + 1;
+            int endIndex = startIndex + emailsThread - 1;
+
+            theards[i] = new Thread(new SenderRunnable(startIndex, endIndex));
+            theards[i].start();
+        }
+
+        for (Thread thread : theards) {
+            try {
+                thread.join();
+            } catch (InterruptedException e) {
+                System.out.println("Поток прерван " + thread.getName());
+            }
+        }
+        System.out.println("Все письма отправлены!");
+    }
+
+
+}
