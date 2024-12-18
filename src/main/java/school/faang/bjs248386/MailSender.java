c2cf7cd79cd3f38e6a2daa499a7fa7f4eca56385
salikhdev <salikhdev@gmail.com>
2024-12-18 14:19:34 +0500

    Вы работаете в Microsoft! Update

--- src/main/java/school/faang/bjs248386/MailSender.java
+++ src/main/java/school/faang/bjs248386/MailSender.java
@@ -1,23 +1,28 @@
 package school.faang.bjs248386;
 
 public class MailSender {
-    public static void main(String[] args) throws InterruptedException {
+    public static void main(String[] args) {
 
         int totalMessage = 1000;
         int threadCount = 5;
         int batchSize = totalMessage / threadCount;
 
         Thread[] threads = new Thread[threadCount];
-
+        int start;
+        int end;
         for (int i = 0; i < threads.length; i++) {
-            int start = i * batchSize;
-            int end = (i + 1) * batchSize;
+            start = i * batchSize;
+            end = (i + 1) * batchSize;
             threads[i] = new Thread(new SenderRunnable(start, end));
             threads[i].start();
         }
 
         for (Thread thread : threads) {
-            thread.join();
+            try {
+                thread.join();
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
         }
 
         System.out.println("Ishlar tugadi");
