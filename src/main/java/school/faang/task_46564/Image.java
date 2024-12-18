86f60b5c4c87d65ccb11d9b33e80a852fb0bd323
Sergey Chirin <zopks2@gmail.com>
2024-12-19 00:43:14 +0300

    solved

---
+++ src/main/java/school/faang/task_46564/Image.java
@@ -0,0 +1,11 @@
+package school.faang.task_46564;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@AllArgsConstructor
+@Getter
+public class Image {
+    private String Name;
+    private String Description;
+}
