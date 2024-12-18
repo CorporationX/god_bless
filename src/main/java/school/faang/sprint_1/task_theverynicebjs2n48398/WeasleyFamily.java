b6fd138be92e8190bfe3dabd9fade286e41eaf9f
alexey_obraztsov <alexey.autotester86@gmail.com>
2024-12-18 15:25:43 +0300

    BJS2-48398. They were very nice but extremely poor. Threads. blank line deleted

--- src/main/java/school/faang/sprint_1/task_theverynicebjs2n48398/WeasleyFamily.java
+++ src/main/java/school/faang/sprint_1/task_theverynicebjs2n48398/WeasleyFamily.java
@@ -3,7 +3,6 @@ package school.faang.sprint_1.task_theverynicebjs2n48398;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NonNull;
-
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
