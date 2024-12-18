419844d3d6d3063d838c4abca07b310667440e70
triplek <weribel@mail.ru>
2024-12-18 13:20:19 +0300

    Supercow!

---
+++ src/main/java/school/faang/task_49639/Boss.java
@@ -0,0 +1,36 @@
+package school.faang.task_49639;
+
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+public class Boss {
+    private int maxPlayers;
+    private List<Player> currentBattleGroup = new ArrayList<>(maxPlayers);
+
+
+    public Boss(int maxPlayers) {
+        this.maxPlayers = maxPlayers;
+    }
+
+    public synchronized void joinBattle(Player player) {
+        if (currentBattleGroup.size() < maxPlayers) {
+            System.out.println("игрок " + player.name + " присоединяется к битве...");
+            currentBattleGroup.add(player);
+        } else {
+            try {
+                System.out.println("Все места в битве пока заняты, ожидайте...");
+                wait();
+            } catch (InterruptedException e) {
+                System.out.println("sdfsf" + e.getMessage());
+            }
+        }
+    }
+
+    public synchronized void leaveBattle(Player player) {
+        currentBattleGroup.remove(player);
+        System.out.println("игрок" + player.name + " покинул битву");
+        notify();
+    }
+}
