d125fedfd87609e57363cb9ab45b22e2dd885d59
Stanislav <stasbugaev2006@gmail.com>
2024-12-19 00:04:21 +0500

    Рефакторинг

--- src/main/java/school/faang/sprint_3/task_47765/Archer.java
+++ src/main/java/school/faang/sprint_3/task_47765/Archer.java
@@ -1,6 +1,7 @@
 package school.faang.sprint_3.task_47765;
 
-public class Archer extends Hero{
+public class Archer extends Hero {
+
     public Archer(int power) {
         super(power);
     }
--- src/main/java/school/faang/sprint_3/task_47765/Swordsman.java
+++ src/main/java/school/faang/sprint_3/task_47765/Swordsman.java
@@ -1,6 +1,7 @@
 package school.faang.sprint_3.task_47765;
 
-public class Swordsman extends Hero{
+public class Swordsman extends Hero {
+
     public Swordsman(int power) {
         super(power);
     }
