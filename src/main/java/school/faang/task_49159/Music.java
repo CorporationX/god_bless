9899fee8d1be3f15a01cc9156101cac249566743
askar0103 <ananaspineapple2048@gmail.com>
2024-12-18 10:31:55 +0300

    Solution to task BJS2-49159. Improved code quality based on review comments

--- src/main/java/school/faang/task_49159/Music.java
+++ src/main/java/school/faang/task_49159/Music.java
@@ -1,17 +1,17 @@
 package school.faang.task_49159;
 
+import java.util.stream.Stream;
+
 public class Music {
     private static final Player PLAYER = new Player();
 
-    private static final Thread PLAY_THREAD = new Thread(PLAYER::play);
-    private static final Thread PAUSE_THREAD = new Thread(PLAYER::pause);
-    private static final Thread SKIP_THREAD = new Thread(PLAYER::skip);
-    private static final Thread PREVIOUS_THREAD = new Thread(PLAYER::previous);
-
     public static void main(String[] args) {
-        PLAY_THREAD.start();
-        PAUSE_THREAD.start();
-        SKIP_THREAD.start();
-        PREVIOUS_THREAD.start();
+        Stream.of(
+                new Thread(PLAYER::play),
+                new Thread(PLAYER::pause),
+                new Thread(PLAYER::skip),
+                new Thread(PLAYER::previous)
+        ).forEach(Thread::start);
     }
+
 }
