35b3cff450472cd598a4fa34392f61cc5834900c
Stanislav <stasbugaev2006@gmail.com>
2024-12-18 09:35:59 +0500

    Вынес в константы время

--- src/main/java/school/faang/sprint_3/task_48297/Chore.java
+++ src/main/java/school/faang/sprint_3/task_48297/Chore.java
@@ -8,12 +8,14 @@ import java.util.Random;
 @AllArgsConstructor
 public class Chore implements Runnable {
     private String chore;
+    private static final int MAX_TIME_FOR_WORK = 10;
+    private static final int MIN_TIME_FOR_WORK = 10;
 
     @SneakyThrows
     @Override
     public void run() {
         Random random = new Random();
-        int seconds = random.nextInt(1, 10) * 1000;
+        int seconds = random.nextInt(MIN_TIME_FOR_WORK, MAX_TIME_FOR_WORK) * 1000;
         Thread.sleep(seconds);
         System.out.printf(
                 "Работа \"%s\" сделана потоком %s за %d секунд\n",
