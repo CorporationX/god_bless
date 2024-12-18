32056f001366715e37397e25d937af617b3c223c
MykhailoShpakov07 <sotenkomihajlo@gmail.com>
2024-12-18 19:55:56 +0100

    Fixed task BJS2-47280 as suggested by mentor

--- src/main/java/school/faang/sprint2/task_47280/UserActionAnalyzer.java
+++ src/main/java/school/faang/sprint2/task_47280/UserActionAnalyzer.java
@@ -5,6 +5,7 @@ import java.util.List;
 import java.util.Map;
 import java.util.Set;
 import java.util.regex.MatchResult;
+import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 import java.util.stream.Collectors;
 
@@ -18,66 +19,71 @@ public class UserActionAnalyzer {
 
     public static List<String> findTopActiveUsers(List<UserAction> userActions) {
 
-        Map<String, Long> numOfActionsByUserName = userActions.stream().collect(Collectors.groupingBy(
-                UserAction::name, Collectors.counting())
-        );
-        return numOfActionsByUserName.entrySet().stream().sorted(
-                (entry1, entry2) -> {
-                    int countActionsComparison = Long.compare(entry2.getValue(), entry1.getValue());
-                    if (countActionsComparison == 0) {
-                        return entry1.getKey().compareTo(entry2.getKey());
-                    }
-                    return countActionsComparison;
-                }
-        ).map(Map.Entry::getKey).limit(MAX_ACTIVE_USERS).collect(Collectors.toList());
+        Map<String, Long> numOfActionsByUserName = userActions.stream()
+                .collect(Collectors.groupingBy(
+                        UserAction::name, Collectors.counting())
+                );
+        return numOfActionsByUserName.entrySet().stream()
+                .sorted(
+                        Map.Entry.<String, Long>comparingByValue().reversed()
+                                .thenComparing(Map.Entry.comparingByKey())
+                )
+                .limit(MAX_ACTIVE_USERS)
+                .map(Map.Entry::getKey)
+                .collect(Collectors.toList());
     }
 
     public static List<String> topPopularHashtags(List<UserAction> userActions) {
         Pattern pattern = Pattern.compile("(#\\w+\\b)(?!;)");
 
-        List<String> allHashtags = userActions.stream().filter(action -> Set.of(POST, COMMENT)
-                        .contains(action.actionType())).map(UserAction::content)
-                        .flatMap(content -> pattern.matcher(content).results()).map(MatchResult::group)
-                        .map(String::toUpperCase).toList();
-
-        Map<String, Long> numOfOccurrencesByHashtag = allHashtags.stream().collect(Collectors.groupingBy(
+        Map<String, Long> numOfOccurrencesByHashtag = userActions.stream()
+                .filter(action -> Set.of(POST, COMMENT)
+                        .contains(action.actionType()))
+                .map(UserAction::content)
+                .map(pattern::matcher)
+                .flatMap(Matcher::results)
+                .map(MatchResult::group)
+                .map(String::toUpperCase)
+                .collect(Collectors.groupingBy(
                         s -> s, Collectors.counting()
                 ));
 
-        return numOfOccurrencesByHashtag.entrySet().stream().sorted(
-                (entry1, entry2) -> {
-                    int countComparison = Long.compare(entry2.getValue(), entry1.getValue());
-                    if (countComparison == 0) {
-                        return entry1.getKey().compareTo(entry2.getKey());
-                    }
-                    return countComparison;
-                }
-        ).map(Map.Entry::getKey).limit(MAX_POPULAR_TOPICS).collect(Collectors.toList());
+        return numOfOccurrencesByHashtag.entrySet().stream()
+                .sorted(
+                        Map.Entry.<String, Long>comparingByValue().reversed()
+                                .thenComparing(Map.Entry.comparingByKey())
+                )
+                .limit(MAX_POPULAR_TOPICS)
+                .map(Map.Entry::getKey)
+                .collect(Collectors.toList());
     }
 
     public static List<String> topCommentersForTheLastMonth(List<UserAction> userActions) {
 
-        Map<String, Long> numOfCommentsByUser = userActions.stream().filter(action -> action.actionType()
-                .equals(COMMENT)).filter(action -> action.actionDate().isAfter(LocalDate.now().minusMonths(1)))
+        Map<String, Long> numOfCommentsByUser = userActions.stream()
+                .filter(action -> action.actionType()
+                        .equals(COMMENT))
+                .filter(action -> action.actionDate().isAfter(LocalDate.now().minusMonths(1)))
                 .collect(Collectors.groupingBy(UserAction::name, Collectors.counting()));
 
-        return numOfCommentsByUser.entrySet().stream().sorted(
-                (entry1, entry2) -> {
-                    int countActionsComparison = Long.compare(entry2.getValue(), entry1.getValue());
-                    if (countActionsComparison == 0) {
-                        return entry1.getKey().compareTo(entry2.getKey());
-                    }
-                    return countActionsComparison;
-                }
-        ).limit(MAX_ACTIVE_COMMENTERS).map(Map.Entry::getKey).collect(Collectors.toList());
+        return numOfCommentsByUser.entrySet().stream()
+                .sorted(
+                        Map.Entry.<String, Long>comparingByValue().reversed()
+                                .thenComparing(Map.Entry.comparingByKey())
+                )
+                .limit(MAX_ACTIVE_COMMENTERS)
+                .map(Map.Entry::getKey)
+                .collect(Collectors.toList());
     }
 
     public static Map<String, Double> actionTypePercentages(List<UserAction> userActions) {
-        return userActions.stream().collect(Collectors.groupingBy(
-                UserAction::actionType, Collectors.counting())
-        ).entrySet().stream().collect(Collectors.toMap(
-                entry -> entry.getKey().name(),
-                entry -> Math.round((entry.getValue().doubleValue() / userActions.size()) * 10000) / 100.0)
-        );
+        return userActions.stream()
+                .collect(Collectors.groupingBy(
+                        UserAction::actionType, Collectors.counting())
+                ).entrySet().stream()
+                .collect(Collectors.toMap(
+                        entry -> entry.getKey().name(),
+                        entry -> Double.valueOf(String.format("%.2f", (entry.getValue().doubleValue() / userActions.size()) * 100)))
+                );
     }
 }
