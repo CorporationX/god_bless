f7f3eab66f0bc1289719f6729f6297728c602ce0
triplek <weribel@mail.ru>
2024-12-18 07:54:13 +0300

    Блокировка Spotify

--- src/main/java/school/faang/task_49171/Music.java
+++ src/main/java/school/faang/task_49171/Music.java
@@ -6,7 +6,8 @@ public class Music {
         Thread playThread = new Thread(() -> player.play());
         Thread pauseThread = new Thread(() -> player.pause());
         Thread skipThread = new Thread(() -> player.skip());
-        Thread previousThread = new Thread(() -> player.previous());
+        final Thread previousThread = new Thread(() -> player.previous());
+
         playThread.start();
         pauseThread.start();
         skipThread.start();
