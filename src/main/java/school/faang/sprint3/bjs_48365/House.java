3b566987b3f0d17ec656400d5ff13c09d757434d
Artem Kopanev <artemkopanev757@gmail.com>
2024-12-18 22:06:13 +0300

    В методе initialize изменил логику инициализации объекта класса House

--- src/main/java/school/faang/sprint3/bjs_48365/House.java
+++ src/main/java/school/faang/sprint3/bjs_48365/House.java
@@ -39,7 +39,7 @@ public class House {
     public House(List<Room> rooms) {
         this.rooms = rooms;
         this.excludedIndex = new HashSet<>(rooms.size());
-        this.latch = new CountDownLatch(rooms.stream().mapToInt(r -> r.getFood().size()).sum());
+        this.latch = new CountDownLatch(rooms.stream().mapToInt(r -> r.food().size()).sum());
     }
 
     public static void main(String[] args) {
@@ -49,19 +49,13 @@ public class House {
 
         try {
             house.getLatch().await();
-
             executorService.shutdown();
-            if (!executorService.awaitTermination(TIMEOUT, TimeUnit.SECONDS)) {
-                executorService.shutdownNow();
-            }
         } catch (InterruptedException e) {
             System.out.println("Поток был прерван!");
             Thread.currentThread().interrupt();
             executorService.shutdownNow();
         }
 
-        System.out.println(house.totalFood);
-        System.out.println(house.rooms.get(0).getFood());
         System.out.println("Вся еда в доме собрана!");
     }
 
@@ -73,14 +67,9 @@ public class House {
         List<Food> firstRoomFood = getFoodFromRoom(firstRandomRoomIndex);
         List<Food> secondRoomFood = getFoodFromRoom(secondRandomRoomIndex);
 
-        System.out.println("Собрана еда из комнаты " + firstRandomRoomIndex + ": " + firstRoomFood);
-        System.out.println("Собрана еда из комнаты " + secondRandomRoomIndex + ": " + secondRoomFood);
-
-        // Добавляем еду в общий список
         totalFood.addAll(firstRoomFood);
         totalFood.addAll(secondRoomFood);
 
-        // Уменьшаем счетчик для каждого предмета еды
         firstRoomFood.forEach(food -> {
             latch.countDown();
             System.out.println("Еда собрана: " + food.name() + ". Осталось собрать: " + latch.getCount());
@@ -90,15 +79,8 @@ public class House {
             System.out.println("Еда собрана: " + food.name() + ". Осталось собрать: " + latch.getCount());
         });
 
-//        rooms.get(firstRandomRoomIndex).setFood(new ArrayList<>());
-//        rooms.get(secondRandomRoomIndex).setFood(new ArrayList<>());
-
         firstRoomFood.clear();
         secondRoomFood.clear();
-
-        System.out.println(totalFood);
-        System.out.println(rooms.get(firstRandomRoomIndex).getFood());
-        System.out.println(rooms.get(secondRandomRoomIndex).getFood());
     }
 
     private int takeRandomRoomIndex(IntPredicate condition) {
@@ -118,31 +100,27 @@ public class House {
     }
 
     private List<Food> getFoodFromRoom(int roomIndex) {
-        return rooms.get(roomIndex).getFood();
+        return rooms.get(roomIndex).food();
     }
 
     private static House initialize() {
         List<Food> foodList = new ArrayList<>();
+        List<Room> roomList = new ArrayList<>();
 
         for (String food : FOODS) {
             foodList.add(new Food(food));
         }
 
-        List<Room> roomList = List.of(
-                new Room(List.of(foodList.get(0), foodList.get(1))),
-                new Room(List.of(foodList.get(2), foodList.get(3), foodList.get(4))),
-                new Room(List.of(foodList.get(5))),
-                new Room(List.of(foodList.get(6), foodList.get(7))),
-                new Room(List.of(foodList.get(8))),
-                new Room(List.of(foodList.get(9), foodList.get(10), foodList.get(11))),
-                new Room(List.of(foodList.get(12), foodList.get(13))),
-                new Room(List.of(foodList.get(14)))
-        );
+        for (int i = 0; i < 10; i++) {
+            List<Food> food = new ArrayList<>();
+
+            food.add(foodList.get(RANDOM.nextInt(foodList.size())));
+            food.add(foodList.get(RANDOM.nextInt(foodList.size())));
+            food.add(foodList.get(RANDOM.nextInt(foodList.size())));
+
+            roomList.add(new Room(food));
+        }
 
         return new House(roomList);
-    }
-
-    private static boolean isAllFoodCollected(House house) {
-        return FOODS.length == house.totalFood.size();
     }
 }
--- src/main/java/school/faang/sprint3/bjs_48365/Room.java
+++ src/main/java/school/faang/sprint3/bjs_48365/Room.java
@@ -1,14 +1,6 @@
 package school.faang.sprint3.bjs_48365;
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.Setter;
-
 import java.util.List;
 
-@AllArgsConstructor
-@Getter
-@Setter
-public class Room {
-    private List<Food> food;
+public record Room(List<Food> food) {
 }
