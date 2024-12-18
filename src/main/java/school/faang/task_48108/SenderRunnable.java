27d7fe289b71067de18e7696558aab2dcf6b18ee
Lesha Kozyrev <alekseykoz@gmail.com>
2024-12-18 23:49:51 +0300

    BJS2-48108 was completed.

---
+++ src/main/java/school/faang/task_48108/SenderRunnable.java
@@ -0,0 +1,23 @@
+package school.faang.task_48108;
+
+import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@AllArgsConstructor
+public class SenderRunnable implements Runnable {
+    private final int startIndex;
+    private final int endIndex;
+
+    @Override
+    public void run() {
+        for (int count = startIndex; count < endIndex; count++) {
+            try {
+                log.info("Письмо {} отправлено.", count);
+                Thread.sleep(100);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
