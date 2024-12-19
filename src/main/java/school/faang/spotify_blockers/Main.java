1ffb2602f861adfd8dd9e8907f3d5e4ecc790a76
Ivan Baev <ibai86@yandex.ru>
2024-12-19 22:14:06 +0300

    add classes

---
+++ src/main/java/school/faang/spotify_blockers/Main.java
@@ -0,0 +1,21 @@
+package school.faang.spotify_blockers;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) {
+        Player player = new Player();
+        List<Thread> threads = Arrays.asList(
+                new Thread(player::play),
+                new Thread(player::stop),
+                new Thread(player::skip),
+                new Thread(player::previous)
+        );
+
+        threads.forEach(Thread::start);
+
+    }
+
+}
