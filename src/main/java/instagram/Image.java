12c01d06e8bc57afe37a5c5deaf66dba62b59bcd
Artur Berdnikov <prikolovichp@inbox.ru>
2024-12-19 22:34:09 +0300

    task45563 Lord Of The Rings

--- src/main/java/instagram/Image.java
+++ src/main/java/instagram/Image.java
@@ -1,21 +1,7 @@
 package instagram;
 
-import lombok.Getter;
-import lombok.Setter;
+public record Image(String name, String description) {
 
-@Setter
-@Getter
-public class Image {
-    private String name;
-    private String description;
-
-    public Image(String name, String description) {
-        this.name = name;
-        this.description = description;
-    }
-
-    @Override
-    public String toString() {
-        return "Image{name='" + name + "', description='" + description + "'}";
-    }
 }
+
+
