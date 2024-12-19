fd778d848ccfe59ac91fc236145adf61a1837612
Kir34 <kirnovikof1@gmail.com>
2024-12-19 03:17:54 +0300

    задание с стрим апи 1

--- src/main/java/school/faang/task_bjs247179/Main.java
+++ src/main/java/school/faang/task_bjs247179/Main.java
@@ -16,13 +16,18 @@ public class Main {
         System.out.println(ActionsWithList.findSumOfEven(list1));
         System.out.println(ActionsWithList.findMax(list1));
         System.out.println(ActionsWithList.findAvg(list1));
+        System.out.println(ActionsWithList.predicateByCondition(list1, (integer -> integer > 0)));
+        System.out.println(ActionsWithList.findMinIntMoreThis(list1, 4));
 
         List<String> list2 = new ArrayList<>();
         list2.add("");
         list2.add("rrr");
+        list2.add(("ab ab ab ab"));
         list2.add("round");
         list2.add("mouse");
         System.out.println(ActionsWithList.countFilteredStrings(list2, 'r'));
         System.out.println(ActionsWithList.filterStrings(list2, "ou"));
+        System.out.println(ActionsWithList.sortStringsByLength(list2));
+        System.out.println(ActionsWithList.mapStringsToInt(list2));
     }
 }
