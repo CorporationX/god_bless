9de81470e739b048e60f8385c570f6608573c58f
narol01 <kmingogod@gmail.com>
2024-12-19 05:21:46 +0100

    BJS2-49005

--- src/main/java/school/faang/task_49005/MainApp.java
+++ src/main/java/school/faang/task_49005/MainApp.java
@@ -1,30 +1,37 @@
 package school.faang.task_49005;
 
 public class MainApp {
+    private static final int ADD_PHOTOS_DELAY_MS = 2000;
+
     public static void main(String[] args) {
         final GooglePhotosAutoUploader uploader = new GooglePhotosAutoUploader();
         Thread uploaderThread = new Thread(uploader::startAutoUpload);
 
         Thread addPhotosThread = new Thread(() -> {
             for (int i = 0; i < 7; i++) {
-                uploader.onNewPhotoAdded("Photo" + i);
+                uploader.onNewPhotoAdded("Photo" + i + 1);
                 try {
-                    Thread.sleep(2000);
+                    Thread.sleep(ADD_PHOTOS_DELAY_MS);
                 } catch (InterruptedException e) {
                     throw new RuntimeException(e);
                 }
             }
         });
-        Thread stopUploader = new Thread(uploader::stopUploader);
+
         uploaderThread.start();
         addPhotosThread.start();
         try {
-            Thread.sleep(18000);
+            addPhotosThread.join();
         } catch (InterruptedException e) {
             throw new RuntimeException(e);
         }
-        if (!addPhotosThread.isAlive()) {
-            stopUploader.start();
+
+        uploader.stopUploader();
+        try {
+            uploaderThread.join();
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
         }
+        System.out.println("Application finished.");
     }
 }
