771c1e85886a77aed83ca6a63352b65f9a200ff6
Ilya <thomas.moody@mail.ru>
2024-12-19 21:57:30 +0300

    - Вынес аргументы на отдельную строку - заменил цикл for на итератор

--- src/main/java/school/faang/lordoftheringsrpg45704/InventoryManager.java
+++ src/main/java/school/faang/lordoftheringsrpg45704/InventoryManager.java
@@ -1,25 +1,32 @@
 package school.faang.lordoftheringsrpg45704;
 
+import java.util.Iterator;
 import java.util.function.Consumer;
 import java.util.function.Function;
 import java.util.function.Predicate;
 
 public class InventoryManager {
-    public void addItem(Character character, Item item, Consumer<Item> action) {
+    public void addItem(Character character,
+                        Item item,
+                        Consumer<Item> action) {
         character.addItem(item);
         action.accept(item);
     }
 
-    public void removeItem(Character character, Predicate<Item> condition) {
+    public void removeItem(Character character,
+                           Predicate<Item> condition) {
         character.getInventory().removeIf(condition);
     }
 
-    public void updateItem(Character character, Predicate<Item> condition,
+    public void updateItem(Character character,
+                           Predicate<Item> condition,
                            Function<Item, Item> updater) {
-        for (int i = 0; i < character.getInventory().size(); i++) {
-            Item item = character.getInventory().get(i);
+        Iterator<Item> iterator = character.getInventory().iterator();
+
+        while (iterator.hasNext()) {
+            Item item = iterator.next();
             if (condition.test(item)) {
-                character.removeItem(item);
+                iterator.remove();
                 character.addItem(updater.apply(item));
             }
         }
