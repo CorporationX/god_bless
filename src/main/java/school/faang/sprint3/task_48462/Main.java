6828647ae781532eacf4caaab81075c166ce3ece
Mihail Svistunov <egzermr@gmail.com>
2024-12-19 09:12:44 +0800

    BJS2-48462 checkstyle fix

--- src/main/java/school/faang/sprint3/task_48462/Main.java
+++ src/main/java/school/faang/sprint3/task_48462/Main.java
@@ -2,11 +2,11 @@ package school.faang.sprint3.task_48462;
 
 public class Main {
     public static void main(String[] args) {
-        Thread thread1 = new Thread(new SenderRunnable(1, 200));
-        Thread thread2 = new Thread(new SenderRunnable(201, 400));
-        Thread thread3 = new Thread(new SenderRunnable(401, 600));
-        Thread thread4 = new Thread(new SenderRunnable(601, 800));
-        Thread thread5 = new Thread(new SenderRunnable(801, 1000));
+        final Thread thread1 = new Thread(new SenderRunnable(1, 200));
+        final Thread thread2 = new Thread(new SenderRunnable(201, 400));
+        final Thread thread3 = new Thread(new SenderRunnable(401, 600));
+        final Thread thread4 = new Thread(new SenderRunnable(601, 800));
+        final Thread thread5 = new Thread(new SenderRunnable(801, 1000));
 
         thread1.start();
         thread2.start();
