efd2dd75647846b0e897e5d2f2949c2336d8bb53
Shakhzod Kamolov <shaxzod.kamolov@keysoft.uz>
2024-12-19 15:22:05 +0500

    fix(BJS2-47172) Fix issues with adding throw exception

--- src/main/java/school/faang/task_47172/ListOperations.java
+++ src/main/java/school/faang/task_47172/ListOperations.java
@@ -2,6 +2,7 @@ package school.faang.task_47172;
 
 import java.util.Comparator;
 import java.util.List;
+import java.util.NoSuchElementException;
 import java.util.function.Predicate;
 
 public class ListOperations {
@@ -10,20 +11,20 @@ public class ListOperations {
         return integerList.stream()
                 .filter(intNum -> intNum % 2 == 0)
                 .reduce(Integer::sum)
-                .orElse(0);
+                .orElseThrow(() -> new NoSuchElementException("No even numbers found"));
     }
 
     public static int findMax(List<Integer> integerList) {
         return integerList.stream()
                 .max(Integer::compareTo)
-                .orElse(0);
+                .orElseThrow(() -> new NoSuchElementException("List is empty"));
     }
 
     public static double findAverage(List<Integer> integerList) {
         return integerList.stream()
                 .mapToInt(Integer::intValue)
                 .average()
-                .orElse(0);
+                .orElseThrow(() -> new NoSuchElementException("List is empty"));
     }
 
     public static int countStringsStartingWith(List<String> stringList, char symbol) {
@@ -34,17 +35,20 @@ public class ListOperations {
     }
 
     public static List<String> filterStringsContainingSubstring(List<String> strings, String substring) {
-        return strings.stream().filter(
-                string -> string.contains(substring)
-        ).toList();
+        return strings.stream()
+                .filter(string -> string.contains(substring))
+                .toList();
     }
 
     public static List<String> sortByLength(List<String> unsortedStrings) {
-        return unsortedStrings.stream().sorted(Comparator.comparing(String::length)).toList();
+        return unsortedStrings.stream()
+                .sorted(Comparator.comparing(String::length))
+                .toList();
     }
 
     public static boolean allMatchCondition(List<Integer> integers, Predicate<Integer> predicate) {
-        return integers.stream().allMatch(predicate);
+        return integers.stream()
+                .allMatch(predicate);
     }
 
     public static int findMinGreaterThan(List<Integer> integers, int comparingInt) {
@@ -52,7 +56,7 @@ public class ListOperations {
                 .sorted(Comparator.naturalOrder())
                 .filter(integer -> integer > comparingInt)
                 .findFirst()
-                .orElse(0);
+                .orElseThrow(() -> new NoSuchElementException("No element greater than " + comparingInt));
     }
 
     public static List<Integer> convertToLengths(List<String> strings) {
