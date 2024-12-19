c98ca217e35cf140b528b3db0c7a9454d456309b
our ame <you@example.com>
2024-12-19 13:19:46 +0500

    BJS2-46918

---
+++ src/main/java/school/faang/sprint_2/task_46918/Notification.java
@@ -0,0 +1,11 @@
+package school.faang.sprint_2.task_46918;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class Notification {
+    private String type;
+    private String message;
+}
---
+++ src/main/java/school/faang/sprint_2/task_46918/NotificationManager.java
@@ -0,0 +1,28 @@
+package school.faang.sprint_2.task_46918;
+
+import lombok.ToString;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.function.Consumer;
+
+@ToString
+public class NotificationManager {
+    Map<String, Consumer<Notification>> stringConsumerMap = new HashMap<>();
+
+    public void registerHandler(String type, Consumer<Notification> consumer) {
+        if (type != null && consumer != null) {
+            stringConsumerMap.put(type, consumer);
+        } else {
+            System.out.println("Тип оповещения или само оповещение не должно быть Null");
+        }
+    }
+
+    public void sendNotification(Notification notification) {
+        if (stringConsumerMap.containsKey(notification.getType())) {
+            stringConsumerMap.get(notification.getType()).accept(notification);
+        } else {
+            System.out.println("Такого типа оповещения нет");
+        }
+    }
+}
