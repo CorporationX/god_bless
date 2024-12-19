456817d9d6359e02c629c454b67d2ecbbcf25ee0
Mihail Svistunov <egzermr@gmail.com>
2024-12-19 08:50:50 +0800

    BJS2-47269 Исправление ошибок

--- src/main/java/school/faang/sprint2/task_46340/Droid.java
+++ src/main/java/school/faang/sprint2/task_46340/Droid.java
@@ -8,23 +8,46 @@ import lombok.Getter;
 public class Droid {
     private String name;
 
-    private String encryptMessage(String message, int key, DroidMessageEncryptor encryptor) {
-        return encryptor.encrypt(message, key);
-    }
-
-    private String decryptMessage(String message, int key, DroidMessageEncryptor encryptor) {
-        key = -key + 26;
-        return encryptor.encrypt(message, key);
-    }
-
-    private void receiveMessage(String message, int key, DroidMessageEncryptor encryptor) {
-        message = decryptMessage(message, key, encryptor);
-        System.out.println(name + " получил расшифрованное сообщение: " + message);
-    }
-
-    public void sendMessage(Droid receiver, String message, int key, DroidMessageEncryptor encryptor) {
-        message = encryptMessage(message, key, encryptor);
+    public void sendMessage(Droid receiver, String message, int key) {
+        message = encryptMessage(message, key);
         System.out.println(name + " отправил зашифрованное сообщение: " + message);
-        receiver.receiveMessage(message, key, encryptor);
+        receiver.receiveMessage(message, key);
+    }
+
+    private String encryptMessage(String message, int key) {
+        DroidMessageEncryptor encryptor = (msg, encryptionKey) -> {
+            StringBuilder encryptedMessage = new StringBuilder();
+            for (char ch : msg.toCharArray()) {
+                if (Character.isLetter(ch)) {
+                    char base = Character.isLowerCase(ch) ? 'a' : 'A';
+                    encryptedMessage.append((char) ((ch - base + encryptionKey) % 26 + base));
+                } else {
+                    encryptedMessage.append(ch);
+                }
+            }
+            return encryptedMessage.toString();
+        };
+        return encryptor.encrypt(message, key);
+    }
+
+    private String decryptMessage(String encryptedMessage, int key) {
+        DroidMessageDecryptor decryptor = (msg, decryptionKey) -> {
+            StringBuilder decryptedMessage = new StringBuilder();
+            for (char ch : msg.toCharArray()) {
+                if (Character.isLetter(ch)) {
+                    char base = Character.isLowerCase(ch) ? 'a' : 'A';
+                    decryptedMessage.append((char) ((ch - base - decryptionKey + 26) % 26 + base));
+                } else {
+                    decryptedMessage.append(ch);
+                }
+            }
+            return decryptedMessage.toString();
+        };
+        return decryptor.decrypt(encryptedMessage, key);  // Расшифровываем сообщение
+    }
+
+    private void receiveMessage(String message, int key) {
+        message = decryptMessage(message, key);
+        System.out.println(name + " получил расшифрованное сообщение: " + message);
     }
 }
--- src/main/java/school/faang/sprint2/task_46340/Main.java
+++ src/main/java/school/faang/sprint2/task_46340/Main.java
@@ -2,25 +2,6 @@ package school.faang.sprint2.task_46340;
 
 public class Main {
     public static void main(String[] args) {
-        DroidMessageEncryptor encryptor = (message, key) -> {
-            String newStr = "";
-            int charCode = 0;
-
-            for (char character : message.toCharArray()) {
-                if (Character.isLetter(character)) {
-                    charCode = Character.toUpperCase(character);
-                    charCode = (charCode - 65 + key) % 26 + 65;
-                    if (Character.isUpperCase(character)) {
-                        newStr += (char) charCode;
-                    } else {
-                        newStr += Character.toLowerCase((char) charCode);
-                    }
-                } else {
-                    newStr += character;
-                }
-            }
-            return newStr;
-        };
 
         // Создаём трёх дроидов
         Droid r2d2 = new Droid("R2D2");
@@ -30,16 +11,16 @@ public class Main {
         // Оригинальные сообщения
         String message1 = "Join the Rebellion!";
         String message2 = "The Death Star is operational!";
-        int encryptionKey1 = 6; // Ключ шифрования для первого сообщения
-        int encryptionKey2 = 8; // Ключ шифрования для второго сообщения
+        int encryptionKey1 = 3; // Ключ шифрования для первого сообщения
+        int encryptionKey2 = 7; // Ключ шифрования для второго сообщения
 
         // R2D2 отправляет сообщение C3PO
-        r2d2.sendMessage(c3po, message1, encryptionKey1, encryptor);
+        r2d2.sendMessage(c3po, message1, encryptionKey1);
 
         // C3PO отвечает R2D2
-        c3po.sendMessage(r2d2, message2, encryptionKey2, encryptor);
+        c3po.sendMessage(r2d2, message2, encryptionKey2);
 
         // BB-8 отправляет сообщение C3PO
-        bb8.sendMessage(c3po, "The mission is complete.", encryptionKey1, encryptor);
+        bb8.sendMessage(c3po, "The mission is complete.", encryptionKey1);
     }
 }
