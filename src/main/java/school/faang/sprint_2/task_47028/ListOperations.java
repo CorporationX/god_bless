c1aeb3b37f1558d4cdfa6a52125b335c8643b623
Timofey Khutoryanskiy <103549514+tiroxin@users.noreply.github.com>
2024-12-19 21:51:04 +0500

    tiroxin BJS2-47028 (#15817)
    * tiroxin BJS2_43543
    
    * tiroxin BJS2_47028
    
    * tiroxin BJS2_47028 (RegEx)

---
+++ src/main/java/school/faang/sprint_2/task_47028/ListOperations.java
@@ -0,0 +1,43 @@
+package school.faang.sprint_2.task_47028;
+
+import java.util.*;
+import java.util.stream.Collectors;
+
+public class ListOperations {
+    public static Set<List<Integer>> findUniquePairsWithSum(List<Integer> integers, int sum) {
+        Set<Integer> set = new HashSet<>();
+        return integers.stream()
+                .filter(num -> set.add(num) || set.contains(sum - num))
+                .filter(num -> set.contains(sum - num))
+                .map(num -> Arrays.asList(Math.min(num, sum - num), Math.max(num, sum - num)))
+                .collect(Collectors.toSet());
+    }
+
+    public static List<String> getCapitalsSortedByCountry(Map<String, String> countries) {
+        return countries.entrySet().stream()
+                .sorted(Map.Entry.comparingByKey())
+                .map(Map.Entry::getValue)
+                .collect(Collectors.toList());
+    }
+
+    public static List<String> filterAndSortByLength(List<String> strings, char symbol) {
+        return strings.stream()
+                .filter(string -> string.startsWith(String.valueOf(symbol)))
+                .sorted(Comparator.comparingInt(String::length))
+                .collect(Collectors.toList());
+    }
+
+    public static List<String> convertIntegersToBinaryStrings(List<Integer> integers) {
+        return integers.stream()
+                .map(Integer::toBinaryString)
+                .collect(Collectors.toList());
+    }
+
+    public static List<String> filterAndSortByLengthAlphabet(List<String> strings, String alphabet) {
+        String regex = "[" + alphabet + "]+$";
+        return strings.stream()
+                .filter(string -> string.matches(regex))
+                .sorted(Comparator.comparingInt(String::length))
+                .collect(Collectors.toList());
+    }
+}
---
+++ src/main/java/school/faang/sprint_2/task_47028/Main.java
@@ -0,0 +1,35 @@
+package school.faang.sprint_2.task_47028;
+
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        List<Integer> integers = Arrays.asList(1, 2, 3, 4, 5, 6);
+        int sum = 7;
+        Set<List<Integer>> pairs = ListOperations.findUniquePairsWithSum(integers, sum);
+        System.out.println("Уникальные пары с суммой " + sum + ": " + pairs);
+
+        Map<String, String> countries = new HashMap<>();
+        countries.put("Франция", "Париж");
+        countries.put("Германия", "Берлин");
+        countries.put("Италия", "Рим");
+        countries.put("Испания", "Мадрид");
+        List<String> capitals = ListOperations.getCapitalsSortedByCountry(countries);
+        System.out.println("Столицы, отсортированные по странам: " + capitals);
+
+        List<String> strings = Arrays.asList("яблоко", "банан", "вишня", "абрикос", "авокадо");
+        char symbol = 'а';
+        List<String> filteredAndSorted = ListOperations.filterAndSortByLength(strings, symbol);
+        System.out.println("Строки, начинающиеся с '" + symbol + "', отсортированные по длине: " + filteredAndSorted);
+
+        List<Integer> numbers = Arrays.asList(5, 8, 13, 255);
+        List<String> binaryStrings = ListOperations.convertIntegersToBinaryStrings(numbers);
+        System.out.println("Двоичное представление целых чисел: " + binaryStrings);
+
+        List<String> alphabetStrings = Arrays.asList("яблоко", "банан", "вишня", "киви", "манго");
+        String alphabet = "абвгдеёжзийклмнопрстуфхцчшщыэюя"; // Все буквы русского алфавита
+        List<String> filteredAlphabetStrings = ListOperations.filterAndSortByLengthAlphabet(alphabetStrings, alphabet);
+        System.out.println("Строки, содержащие только буквы алфавита, отсортированные по длине: "
+                + filteredAlphabetStrings);
+    }
+}
