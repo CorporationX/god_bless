be4dbf64d504e3470413600aee8d295342ec0e5e
Mihail Svistunov <egzermr@gmail.com>
2024-12-20 05:19:26 +0800

    BJS2-48462 Исправления

--- src/main/java/school/faang/sprint3/task_48462/Main.java
+++ src/main/java/school/faang/sprint3/task_48462/Main.java
@@ -1,17 +1,14 @@
 package school.faang.sprint3.task_48462;
 
 public class Main {
-    public static void main(String[] args) {
-        final Thread thread1 = new Thread(new SenderRunnable(1, 200));
-        final Thread thread2 = new Thread(new SenderRunnable(201, 400));
-        final Thread thread3 = new Thread(new SenderRunnable(401, 600));
-        final Thread thread4 = new Thread(new SenderRunnable(601, 800));
-        final Thread thread5 = new Thread(new SenderRunnable(801, 1000));
+    private static Thread thread1;
 
-        thread1.start();
-        thread2.start();
-        thread3.start();
-        thread4.start();
-        thread5.start();
+    public static void main(String[] args) {
+        final int numberOfLetters = 1000;
+        final int numberOfThreads = 5;
+        for (int i = 0; i < numberOfLetters; i += numberOfLetters/numberOfThreads) {
+            final Thread thread = new Thread(new SenderRunnable(i + 1, i + numberOfLetters/numberOfThreads));
+            thread.start();
+        }
     }
 }
