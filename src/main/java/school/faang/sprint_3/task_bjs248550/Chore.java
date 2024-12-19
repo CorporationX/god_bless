da4a3a415fe0c95b81cb2c03d77a237a558bdfdc
Gromov Ilya <109763246+DMCHMB@users.noreply.github.com>
2024-12-19 06:19:19 +0300

    fix constants BJS2-48550 They were very nice but extremely poor

--- src/main/java/school/faang/sprint_3/task_bjs248550/Chore.java
+++ src/main/java/school/faang/sprint_3/task_bjs248550/Chore.java
@@ -6,6 +6,7 @@ import lombok.extern.slf4j.Slf4j;
 @Slf4j
 @AllArgsConstructor
 public class Chore implements Runnable {
+    private static final int EMULATE_THREAD_WORK_MILLIS = 2000;
     private final String chore;
 
     @Override
@@ -13,7 +14,7 @@ public class Chore implements Runnable {
         String threadName = Thread.currentThread().getName();
         log.info("Thread {} is working on {}", threadName, chore);
         try {
-            Thread.sleep(2000);
+            Thread.sleep(EMULATE_THREAD_WORK_MILLIS);
         } catch (InterruptedException e) {
             log.warn("Thread {} was interrupted", threadName);
             Thread.currentThread().interrupt();
