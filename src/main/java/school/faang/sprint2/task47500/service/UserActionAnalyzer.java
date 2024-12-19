54892207057bac9736e0998057f17b7edf7a4d18
Alexey Gorokhov <almigor@mail.ru>
2024-12-19 22:34:30 +0300

    Рефакторинг по код-ревью

--- src/main/java/school/faang/sprint2/task47500/service/UserActionAnalyzer.java
+++ src/main/java/school/faang/sprint2/task47500/service/UserActionAnalyzer.java
@@ -1,5 +1,6 @@
 package school.faang.sprint2.task47500.service;
 
+import school.faang.sprint2.task47500.model.ActionType;
 import school.faang.sprint2.task47500.model.UserAction;
 
 import java.time.LocalDate;
@@ -10,55 +11,55 @@ import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 public class UserActionAnalyzer {
-    public static List<String> topActiveUsers(List<UserAction> userActions) {
+    final static String WORD_REGEX = "[,.!\\s]+";
+    final static String HASHTAG_SIGN = "#";
+
+    public static List<String> getTopActiveUsers(List<UserAction> userActions, int topSize) {
         return userActions.stream()
                 .collect(Collectors.groupingBy(UserAction::getName, Collectors.counting()))
                 .entrySet()
                 .stream()
                 .sorted(Map.Entry.<String, Long>comparingByValue().reversed())
-                .limit(10)
+                .limit(topSize)
                 .map(Map.Entry::getKey)
                 .toList();
     }
 
 
-    public static List<String> topPopularHashtags(List<UserAction> userActions) {
+    public static List<String> getTopPopularHashtags(List<UserAction> userActions, int topSize) {
+
         return userActions.stream()
-                .filter(a -> ((a.getActionType().equals("post")
-                        || a.getActionType().equals("comment"))
-                        && !a.getContent().isEmpty()))
-                .flatMap(a -> Stream.of(a.getContent().split("[,.!\\s]+")))
-                .filter(w -> w.startsWith("#"))
+                .filter(a -> a.getActionType().equals(ActionType.COMMENT) && !a.getContent().isEmpty())
+                .flatMap(a -> Stream.of(a.getContent().split(WORD_REGEX)))
+                .filter(w -> w.startsWith(HASHTAG_SIGN))
                 .collect(Collectors.groupingBy(Function.identity(), Collectors.counting()))
                 .entrySet().stream()
                 .sorted(Map.Entry.<String, Long>comparingByValue().reversed())
-                .limit(5)
+                .limit(topSize)
                 .map(Map.Entry::getKey)
                 .toList();
-
     }
 
-    public static List<String> topCommentersLastMonth(List<UserAction> userActions) {
+    public static List<String> getTopCommentersLastMonth(List<UserAction> userActions, int topSize) {
         return userActions.stream()
-                .filter(a -> (a.getActionType().equals("comment")
+                .filter(a -> (a.getActionType().equals(ActionType.COMMENT)
                         && a.getActionDate().isAfter(LocalDate.now().minusDays(365))))
                 .collect(Collectors.groupingBy(UserAction::getName, Collectors.counting()))
                 .entrySet().stream()
                 .sorted(Map.Entry.<String, Long>comparingByValue().reversed())
                 .map(Map.Entry::getKey)
-                .limit(3)
+                .limit(topSize)
                 .toList();
     }
 
-    public static Map<String, Double> actionTypePercentages(List<UserAction> userActions) {
-        double userActionsQty = userActions.size();
-        System.out.println(userActionsQty);
+    public static Map<ActionType, Double> actionTypePercentages(List<UserAction> userActions) {
+        double userActionsQuantity = userActions.size();
         return userActions.stream()
                 .collect(Collectors.groupingBy(UserAction::getActionType, Collectors.counting()))
                 .entrySet().stream()
                 .collect(Collectors.toMap(
                         Map.Entry::getKey,
-                        entry -> (entry.getValue() * 100.0 / userActionsQty))
+                        entry -> (entry.getValue() * 100.0 / userActionsQuantity))
                 );
     }
 
