ac8d184f0a57a9d1ec21941dc9d6e922305404a0
Ilya <thomas.moody@mail.ru>
2024-12-19 21:44:25 +0300

    добавил аннотацию @FunctionalInterface + изменил название в методе интерфейса на process + вывел число 26 в константу класса

--- src/main/java/school/faang/secretdroidsbjs46370/Droid.java
+++ src/main/java/school/faang/secretdroidsbjs46370/Droid.java
@@ -4,23 +4,24 @@ import lombok.AllArgsConstructor;
 
 @AllArgsConstructor
 public class Droid {
+    private static final int ALPHABET_LENGTH = 26;
     private String name;
 
     public String encryptMessage(String message, int key) {
-        return shiftMessage(message, key);
+        return processMessage(message, key);
     }
 
     public String decryptMessage(String message, int key) {
-        return shiftMessage(message, -key);
+        return processMessage(message, -key);
     }
 
-    private String shiftMessage(String message, int key) {
+    private String processMessage(String message, int key) {
         StringBuilder result = new StringBuilder();
 
         for (char ch : message.toCharArray()) {
             if (Character.isLetter(ch)) {
                 char base = Character.isLowerCase(ch) ? 'a' : 'A';
-                result.append((char) (((ch - base + key + 26) % 26) + base));
+                result.append((char) (((ch - base + key + ALPHABET_LENGTH) % ALPHABET_LENGTH) + base));
             } else {
                 result.append(ch);
             }
