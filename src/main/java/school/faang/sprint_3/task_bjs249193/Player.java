88f3e75e1c99b979e6ee09cedf7a034b0cfb6566
Gromov Ilya <109763246+DMCHMB@users.noreply.github.com>
2024-12-19 17:32:41 +0300

    BJS2-49193 Блокировка Spotify

---
+++ src/main/java/school/faang/sprint_3/task_bjs249193/Player.java
@@ -0,0 +1,49 @@
+package school.faang.sprint_3.task_bjs249193;
+
+import lombok.Getter;
+import lombok.extern.slf4j.Slf4j;
+
+import java.util.concurrent.atomic.AtomicInteger;
+
+@Slf4j
+public class Player {
+    private final Object lock = new Object();
+    private boolean isPlaying = false;
+    @Getter
+    private final AtomicInteger countThreads = new AtomicInteger();
+
+    public void play() {
+        synchronized (lock) {
+            countThreads.incrementAndGet();
+            if (isPlaying) {
+                log.info("Music is already playing");
+            } else {
+                isPlaying = true;
+                log.info("Music start play");
+            }
+        }
+    }
+
+    public void pause() {
+        synchronized (lock) {
+            countThreads.incrementAndGet();
+            if (isPlaying) {
+                log.info("Music paused");
+            }
+        }
+    }
+
+    public void skip() {
+        synchronized (lock) {
+            countThreads.incrementAndGet();
+            log.info("Next track");
+        }
+    }
+
+    public void previous() {
+        synchronized (lock) {
+            countThreads.incrementAndGet();
+            log.info("Previous track");
+        }
+    }
+}
