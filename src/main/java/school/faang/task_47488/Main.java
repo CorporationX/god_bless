eb43dceac71da16f401b0924159139a444c7b87a
Kairat <snowmango.official@gmail.com>
2024-12-20 00:07:38 +0500

    change distance between first usage

--- src/main/java/school/faang/task_47488/Main.java
+++ src/main/java/school/faang/task_47488/Main.java
@@ -8,14 +8,15 @@ import java.util.Map;
 public class Main {
     public static void main(String[] args) {
         List<Integer> numbers = Arrays.asList(1, 2, 3, 4, 5, 6);
-        List<String> strings = Arrays.asList("apple", "banana", "avocado", "apricot");
+        System.out.println(ListOperations.uniqueCoupleNumbers(numbers, 7));
+
         Map<String, String> countries = new HashMap<>();
         countries.put("Russia", "Moscow");
         countries.put("USA", "Washington");
         countries.put("Germany", "Berlin");
-
-        System.out.println(ListOperations.uniqueCoupleNumbers(numbers, 7));
         System.out.println(ListOperations.countrySort(countries));
+
+        List<String> strings = Arrays.asList("apple", "banana", "avocado", "apricot");
         System.out.println(ListOperations.filterAndSorting(strings, 'a'));
         System.out.println(ListOperations.convertToBinary(Arrays.asList(1, 2, 3, 4)));
         System.out.println(ListOperations.filterByAlphabetAndSorting(
