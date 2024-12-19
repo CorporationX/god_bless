a926f17eada0c43de86b0d06b3c35375af2a07f8
Mihail Svistunov <egzermr@gmail.com>
2024-12-19 09:08:29 +0800

    BJS2-48462 done

--- src/main/java/school/faang/sprint3/task_48462/Main.java
+++ src/main/java/school/faang/sprint3/task_48462/Main.java
@@ -3,10 +3,10 @@ package school.faang.sprint3.task_48462;
 public class Main {
     public static void main(String[] args) {
         Thread thread1 = new Thread(new SenderRunnable(1, 200));
-        Thread thread2 = new Thread(new SenderRunnable(200, 400));
-        Thread thread3 = new Thread(new SenderRunnable(400, 600));
-        Thread thread4 = new Thread(new SenderRunnable(600, 800));
-        Thread thread5 = new Thread(new SenderRunnable(800, 1000));
+        Thread thread2 = new Thread(new SenderRunnable(201, 400));
+        Thread thread3 = new Thread(new SenderRunnable(401, 600));
+        Thread thread4 = new Thread(new SenderRunnable(601, 800));
+        Thread thread5 = new Thread(new SenderRunnable(801, 1000));
 
         thread1.start();
         thread2.start();
--- src/main/java/school/faang/sprint3/task_48462/SenderRunnable.java
+++ src/main/java/school/faang/sprint3/task_48462/SenderRunnable.java
@@ -10,7 +10,7 @@ public class SenderRunnable implements Runnable {
 
     @Override
     public void run() {
-        for (int i = startIndex; i < endIndex; i++) {
+        for (int i = startIndex; i <= endIndex; i++) {
             System.out.printf("Сообщение %d отправлено \n", i);
         }
     }
