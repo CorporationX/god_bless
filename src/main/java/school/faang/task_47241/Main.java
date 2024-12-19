343c535dea8c8b47cf103e5ab57fc6b07b67922a
andruxaUZB <andruxa0989@yandex.ru>
2024-12-19 20:01:05 +0300

    Start

--- src/main/java/school/faang/task_47241/Main.java
+++ src/main/java/school/faang/task_47241/Main.java
@@ -1,38 +1,30 @@
 package school.faang.task_47241;
 
-
-
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
-
 
 public class Main {
+    public static void main(String[] args) throws JsonProcessingException {
+        List<Integer> numbers = Arrays.asList(1, 2, 3, 4, 5, 6);
+        List<Integer> numbersTwo = Arrays.asList(543, 52344, 4324235, 3524, 232);
+        String alphabet = "abcdefghijklmnopqrstuvwxyz";
+        List<String> strings = Arrays.asList("apple", "banana", "avocado", "apricot");
+        List<String> stringsTwo = Arrays.asList("apple", "banana", "cherry", "date", "fig", "grape");
 
-	public static void main(String[] args) throws JsonProcessingException {
+        String jsonString =
+            "{\"Russia\": \"Moscow\", \"USA\": \"Washington\", \"Germany\": \"Berlin\"}";
 
-		List<Integer> numbers = Arrays.asList(1, 2, 3, 4, 5, 6);
+        ObjectMapper objectMapper = new ObjectMapper();
+        HashMap<String, String> countryCapitalMap = objectMapper.readValue(jsonString, HashMap.class);
 
-		List<Integer> numbersTwo = Arrays.asList(543,52344,4324235,3524,232);
-
-		String alphabet = "abcdefghijklmnopqrstuvwxyz";
-
-		List<String> strings = Arrays.asList("apple", "banana", "avocado", "apricot");
-		List<String> strings_Two = Arrays.asList("apple", "banana", "cherry", "date", "fig", "grape");
-
-		String jsonString = "{\"Russia\": \"Moscow\", \"USA\": \"Washington\", \"Germany\": \"Berlin\"}";
-
-		ObjectMapper objectMapper = new ObjectMapper();
-		HashMap<String, String> countryCapitalMap = objectMapper.readValue(jsonString, HashMap.class);
-
-		System.out.println(SteamApi2Testing.uniquePairsNumbers(numbers,7));
-		System.out.println(SteamApi2Testing.sortedCapitals(countryCapitalMap));
-		System.out.println(SteamApi2Testing.filtredWords(strings,"a"));
-		System.out.println(SteamApi2Testing.convertNumberBinary(numbersTwo));
-		System.out.println(SteamApi2Testing.filterAlphabeticallySortLength(strings_Two,alphabet));
-	}
+        System.out.println(SteamApi2Testing.uniquePairsNumbers(numbers, 7));
+        System.out.println(SteamApi2Testing.sortedCapitals(countryCapitalMap));
+        System.out.println(SteamApi2Testing.filtredWords(strings, "a"));
+        System.out.println(SteamApi2Testing.convertNumberBinary(numbersTwo));
+        System.out.println(SteamApi2Testing.filterAlphabeticallySortLength(stringsTwo, alphabet));
+    }
 }
--- src/main/java/school/faang/task_47241/SteamApi2Testing.java
+++ src/main/java/school/faang/task_47241/SteamApi2Testing.java
@@ -11,37 +11,41 @@ import java.util.stream.Collectors;
 
 public class SteamApi2Testing {
 
-	public static Set<List<Integer>> uniquePairsNumbers(List<Integer> numbers, int targetSum) {
-		Set<Integer> set = new HashSet<>(numbers);
-		return numbers.stream()
-				.filter(num -> set.contains(targetSum - num))
-				.map(num -> Arrays.asList(num, targetSum - num))
-				.peek(Collections::sort)
-				.collect(Collectors.toSet());
-	}
+    public static Set<List<Integer>> uniquePairsNumbers(List<Integer> numbers, int targetSum) {
 
-	public static List<String> sortedCapitals(Map<String, String> countries) {
-		return countries.entrySet().stream()
-				.sorted(Map.Entry.comparingByKey())
-				.map(Map.Entry::getValue)
-				.collect(Collectors.toList());
-	}
-	public static List<String> filtredWords(List<String> words, String w) {
-		return words.stream()
-				.filter(word ->word.startsWith(w))
-				.sorted(Comparator.comparing(String::length))
-				.toList();
+        Set<Integer> set = new HashSet<>(numbers);
+        return numbers.stream()
+          .filter(num -> set.contains(targetSum - num))
+          .map(num -> Arrays.asList(num, targetSum - num))
+          .peek(Collections::sort)
+          .collect(Collectors.toSet());
+    }
 
-	}
+    public static List<String> sortedCapitals(Map<String, String> countries) {
+        return countries.entrySet().stream()
+        .sorted(Map.Entry.comparingByKey())
+        .map(Map.Entry::getValue)
+        .collect(Collectors.toList());
+    }
 
-	public static List<String> convertNumberBinary(List<Integer> numbersTwo) {
-		return  numbersTwo.stream()
-				.map(Integer::toBinaryString)
-				.toList();
-	}
+    public static List<String> filtredWords(List<String> words, String w) {
+        return words.stream()
+        .filter(word -> word.startsWith(w))
+        .sorted(Comparator.comparing(String::length))
+        .toList();
+    }
 
-	public static String filterAlphabeticallySortLength(List<String> list, String alphabet){
-		return null;
-	}
+    public static List<String> convertNumberBinary(List<Integer> numbersTwo) {
+        return numbersTwo.stream().map(Integer::toBinaryString).toList();
+    }
+
+    public static List<String> filterAlphabeticallySortLength(List<String> list, String alphabet) {
+        return list.stream()
+          .filter(
+              x -> {
+                  return x.equals(alphabet);
+              })
+          .sorted(Comparator.comparing(String::length))
+          .collect(Collectors.toList());
+    }
 }
-;
