510f366684341d655378dcf4dfa54a3b02dff6d1
alexey_obraztsov <alexey.autotester86@gmail.com>
2024-12-19 22:55:33 +0300

    BJS2-48398. They were very nice but extremely poor. temp

--- src/main/java/school/faang/sprint_1/task_theverynicebjs2n48398/Chore.java
+++ src/main/java/school/faang/sprint_1/task_theverynicebjs2n48398/Chore.java
@@ -4,6 +4,9 @@ import lombok.AllArgsConstructor;
 import lombok.NonNull;
 import lombok.extern.slf4j.Slf4j;
 
+import java.sql.Time;
+import java.util.concurrent.TimeUnit;
+
 @Slf4j
 @AllArgsConstructor
 public class Chore implements Runnable {
@@ -12,6 +15,11 @@ public class Chore implements Runnable {
 
     @Override
     public void run() {
-        log.info("{} выполняет задачу \"{}\"", Thread.currentThread().getName(), chore);
+        try {
+            log.info("{} выполняет задачу \"{}\"", Thread.currentThread().getName(), chore);
+            TimeUnit.SECONDS.sleep(500);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
     }
 }
