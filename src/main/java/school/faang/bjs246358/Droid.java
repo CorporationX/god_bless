51fc5f5b5ce7cc378682bc8dfce96f1f0aae527e
Artur Suleymanov <suleymanov.usa@gmail.com>
2024-12-18 22:29:45 -0800

    Исправление ошибок компиляции

--- src/main/java/school/faang/bjs246358/Droid.java
+++ src/main/java/school/faang/bjs246358/Droid.java
@@ -4,7 +4,6 @@ import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
 public class Droid {
-
     private final String droidName;
 
     public Droid(String name) {
--- src/main/java/school/faang/bjs246358/DroidMessageEncryptor.java
+++ src/main/java/school/faang/bjs246358/DroidMessageEncryptor.java
@@ -3,4 +3,5 @@ package school.faang.bjs246358;
 @FunctionalInterface
 public interface DroidMessageEncryptor {
     String encryptOrDecrypt(String message, int key);
+
 }
