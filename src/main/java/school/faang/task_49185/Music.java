467787d8efd4bd02f53063b77e15ac2ad0489328
Oxana <nov2303@mail.ru>
2024-12-19 21:12:28 +0500

    BJS2-49185

--- src/main/java/school/faang/task_49185/Music.java
+++ src/main/java/school/faang/task_49185/Music.java
@@ -1,17 +1,20 @@
 package school.faang.task_49185;
 
+import java.util.List;
+
 public class Music {
     public static void main(String[] args) {
         Player player = new Player();
         Thread threadPlay = new Thread(() -> player.play());
-        Thread threadPause = new Thread(() -> player.pause());
-        Thread threadSkip = new Thread(() -> player.skip());
-        Thread threadPrevious = new Thread(() -> player.previous());
-
         threadPlay.start();
-        threadPause.start();
+
+        Thread threadSkip = new Thread(() -> player.skip());
         threadSkip.start();
+
+        Thread threadPrevious = new Thread(() -> player.previous());
         threadPrevious.start();
 
+        Thread threadPause = new Thread(() -> player.pause());
+        threadPause.start();
     }
 }
