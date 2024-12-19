97dd049f7df90eb71a9ad2910b829a3871e43656
Sergey Senin <04sersen@gmail.com>
2024-12-19 16:56:03 +0300

    Реализован расчет общей силы армии с использованием потоков для параллельной обработки бойцов.

---
+++ src/main/java/school/faang/task_47827/Main.java
@@ -0,0 +1,16 @@
+package school.faang.task_47827;
+
+public class Main {
+    public static void main(String[] args) throws InterruptedException {
+        Army army = new Army();
+
+        army.addUnit(new Archer(25));
+        army.addUnit(new Swordsman(40));
+        army.addUnit(new Mage(50));
+        army.addUnit(new Mage(20));
+
+        int totalPower = army.calculateTotalPower();
+
+        System.out.println("Total army strength: " + totalPower);
+    }
+}
---
+++ src/main/java/school/faang/task_47827/Unit.java
@@ -0,0 +1,12 @@
+package school.faang.task_47827;
+
+import lombok.Getter;
+
+@Getter
+public class Unit {
+    protected int power;
+
+    public Unit(int power) {
+        this.power = power;
+    }
+}
