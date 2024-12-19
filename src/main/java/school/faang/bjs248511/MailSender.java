9a3d2b5859472a450afcd972e294bacdecb9ab9a
Maksim-VV <laulmv11@gmail.com>
2024-12-19 21:46:16 +0300

    BJS2-48511, Do you work for Microsoft - variable edits, exception handling

--- src/main/java/school/faang/bjs248511/MailSender.java
+++ src/main/java/school/faang/bjs248511/MailSender.java
@@ -3,16 +3,18 @@ package school.faang.bjs248511;
 import lombok.val;
 
 public class MailSender {
-    public static void main(String[] args) throws InterruptedException {
+    public static void main(String[] args) {
         int totalMessages = 1000;
         int threadsCount = 5;
         int batchSize = totalMessages / threadsCount;
 
         Thread[] threads = new Thread[threadsCount];
+        int start;
+        int end;
 
         for (int i = 0; i < threadsCount; i++) {
-            int start = i * batchSize;
-            int end = (i + 1) * batchSize;
+            start = i * batchSize;
+            end = (i + 1) * batchSize;
             val senderRunnable = new SenderRunnable(start, end);
             threads[i] = new Thread(senderRunnable);
             senderRunnable.setThreadName(threads[i].getName());
@@ -20,9 +22,12 @@ public class MailSender {
         }
 
         for (Thread thread : threads) {
-            thread.join();
+            try {
+                thread.join();
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
         }
-
         System.out.println("Все письма отправлены!");
     }
 }
