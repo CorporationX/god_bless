390f4f21cb637de8c7917f92ccec684c30aea241
Timka-cloud1 <amanzholtimka@gmail.com>
2024-12-19 11:48:27 +0500

    added log

--- src/main/java/school/faang/spring3/task_48694/MailSender.java
+++ src/main/java/school/faang/spring3/task_48694/MailSender.java
@@ -1,8 +1,13 @@
 package school.faang.spring3.task_48694;
 
+import lombok.extern.log4j.Log4j;
+import lombok.extern.log4j.Log4j2;
+import lombok.extern.slf4j.Slf4j;
+
 import java.util.Arrays;
 import java.util.stream.IntStream;
 
+@Slf4j
 public class MailSender {
     public static void main(String[] args) {
         int totalMessages = 1000;
@@ -23,7 +28,7 @@ public class MailSender {
             try {
                 e.join();
             } catch (InterruptedException ex) {
-                throw new RuntimeException(ex);
+                log.warn("interrupted " + ex);
             }
         });
 
