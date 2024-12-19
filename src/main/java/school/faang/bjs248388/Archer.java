0cdc2a7692947bb2a4a42cd578e8d0fafe42bf09
Maksim-VV <laulmv11@gmail.com>
2024-12-19 19:18:51 +0300

    BJS2-48388, A distributed army of heroes of might and magic - alpha release

---
+++ src/main/java/school/faang/bjs248388/Archer.java
@@ -0,0 +1,10 @@
+package school.faang.bjs248388;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@AllArgsConstructor
+@Getter
+public class Archer implements Unit {
+    private int power;
+}
