8b28a610572c68445d55d2cc579e32913d1ba81f
Maria Bortsova <bortsovamariia@gmail.com>
2024-12-19 14:23:58 +0300

    supercow impl v2

--- src/main/java/school/faang/task_49640/Main.java
+++ src/main/java/school/faang/task_49640/Main.java
@@ -3,21 +3,18 @@ package school.faang.task_49640;
 public class Main {
     public static void main(String[] args) {
         Boss boss = new Boss(4);
-        
-        Player player = new Player("player 1");
-        Player player2 = new Player("player 2");
-        Player player3 = new Player("player 3");
-        Player player4 = new Player("player 4");
-        Player player5 = new Player("player 5");
-        Player player6 = new Player("player 6");
-        Player player7 = new Player("player 7");
 
-        player.startBattle(boss);
-        player2.startBattle(boss);
-        player3.startBattle(boss);
-        player4.startBattle(boss);
-        player5.startBattle(boss);
-        player6.startBattle(boss);
-        player7.startBattle(boss);
+        createPlayerAndStartBattle("dodge", boss);
+        createPlayerAndStartBattle("fox", boss);
+        createPlayerAndStartBattle("crig", boss);
+        createPlayerAndStartBattle("kolin", boss);
+        createPlayerAndStartBattle("sam", boss);
+        createPlayerAndStartBattle("manshin", boss);
+        createPlayerAndStartBattle("kris", boss);
+        createPlayerAndStartBattle("abrakadabra", boss);
+    }
+
+    private static void createPlayerAndStartBattle(String playerName, Boss boss) {
+        new Player(playerName).startBattle(boss);
     }
 }
