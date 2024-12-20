cf4045806ba784313c1d970f44b1b9d660a218c5
Kairat <snowmango.official@gmail.com>
2024-12-21 02:03:35 +0500

    change logic

--- src/main/java/school/faang/task_46441/Droid.java
+++ src/main/java/school/faang/task_46441/Droid.java
@@ -2,7 +2,6 @@ package school.faang.task_46441;
 
 public class Droid {
 
-    private static final String ALPHABET = "abcdefghijklmnopqrstuvwxyz";
     private String name;
 
     public Droid(String name) {
@@ -22,54 +21,33 @@ public class Droid {
 
     private String encryptMessage(String msg, int key) {
         DroidMessageEncryptor anigma = (message, shift) -> {
-            var encryptedMsg = new StringBuilder();
-            String lowerCaseMsg = message.toLowerCase();
-            for (int i = 0; i < lowerCaseMsg.length(); i++) {
-                boolean isUpperCase = Character.isUpperCase(message.charAt(i));
-                if (ALPHABET.indexOf(lowerCaseMsg.charAt(i)) != -1) {
-                    int index = ALPHABET.indexOf(lowerCaseMsg.charAt(i));
-                    int keyId = index + shift;
-                    if (keyId > ALPHABET.length() - 1) {
-                        keyId = keyId - ALPHABET.length();
-                    }
-                    if (isUpperCase) {
-                        encryptedMsg.append(String.valueOf(ALPHABET.charAt(keyId)).toUpperCase());
-                    } else {
-                        encryptedMsg.append(ALPHABET.charAt(keyId));
-                    }
+            StringBuilder encryptedMessage = new StringBuilder();
+            for (char ch : msg.toCharArray()) {
+                if (Character.isLetter(ch)) {
+                    char base = Character.isLowerCase(ch) ? 'a' : 'A';
+                    encryptedMessage.append((char) ((ch - base + key) % 26 + base));
                 } else {
-                    encryptedMsg.append(lowerCaseMsg.charAt(i));
+                    encryptedMessage.append(ch);
                 }
             }
-            return encryptedMsg.toString();
+            return encryptedMessage.toString();
         };
         return anigma.shift(msg, key);
     }
 
     private String decryptMessage(String msg, int key) {
         DroidMessageEncryptor alanTuring = (message, shift) -> {
-            var encryptedMsg = new StringBuilder();
-            String lowerCaseMsg = msg.toLowerCase();
-            for (int i = 0; i < lowerCaseMsg.length(); i++) {
-                boolean isUpperCase = Character.isUpperCase(message.charAt(i));
-                if (ALPHABET.indexOf(lowerCaseMsg.charAt(i)) != -1) {
-                    int index = ALPHABET.indexOf(lowerCaseMsg.charAt(i));
-                    int keyId = index - key;
-                    if (keyId < 0) {
-                        keyId = ALPHABET.length() + keyId;
-                    }
-                    if (isUpperCase) {
-                        encryptedMsg.append(String.valueOf(ALPHABET.charAt(keyId)).toUpperCase());
-                    } else {
-                        encryptedMsg.append(ALPHABET.charAt(keyId));
-                    }
+            StringBuilder decryptedMessage = new StringBuilder();
+            for (char ch : msg.toCharArray()) {
+                if (Character.isLetter(ch)) {
+                    char base = Character.isLowerCase(ch) ? 'a' : 'A';
+                    decryptedMessage.append((char) ((ch - base - key + 26) % 26 + base));
                 } else {
-                    encryptedMsg.append(lowerCaseMsg.charAt(i));
+                    decryptedMessage.append(ch);
                 }
             }
-            return encryptedMsg.toString();
+            return decryptedMessage.toString();
         };
-
         return alanTuring.shift(msg, key);
     }
 }
