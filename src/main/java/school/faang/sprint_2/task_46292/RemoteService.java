22da06ed759d5d37e3d2ffb955387c01de1c73b1
Aleksandr Kozlov <vismut-888@yandex.ru>
2024-12-20 19:35:18 +0300

    BJS2-46292: Обрабатываем ошибки красиво

---
+++ src/main/java/school/faang/sprint_2/task_46292/RemoteService.java
@@ -0,0 +1,7 @@
+package school.faang.sprint_2.task_46292;
+
+public class RemoteService {
+    public static String call(String param) throws Exception {
+        throw new Exception("Service unavailable");
+    }
+}
