ec233b987a73a889a4a91ea4ad89e48cd44b6ea7
Sergey Senin <04sersen@gmail.com>
2024-12-20 04:31:22 +0300

    Добавлен корректный расчёт `batchSize` с использованием `Math.ceil`; реализована проверка конца диапазона с помощью `Math.min`; Переменные `start` и `end` объявлены как `final`.

--- src/main/java/school/faang/task_48367/MailSender.java
+++ src/main/java/school/faang/task_48367/MailSender.java
@@ -2,15 +2,15 @@ package school.faang.task_48367;
 
 public class MailSender {
     public static void main(String[] args) throws InterruptedException {
-        int totalMessages = 1000;
+        int totalMessages = 999;
         int threadsCount = 5;
-        int batchSize = totalMessages / threadsCount;
+        int batchSize = (int) Math.ceil((double) totalMessages / threadsCount);
 
         Thread[] threads = new Thread[threadsCount];
 
         for (int i = 0; i < threadsCount; i++) {
-            int start = i * batchSize;
-            int end = (i + 1) * batchSize;
+            final int start = i * batchSize;
+            final int end = Math.min(totalMessages, (i + 1) * batchSize);
 
             threads[i] = new Thread(new SenderRunnable(start, end));
             threads[i].start();
