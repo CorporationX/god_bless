9f7ad2f1a3a460e78b6328d1d84b8b8cbf6102ee
RustBad <rustambad1993@gmail.com>
2024-12-20 12:21:18 +0500

    task_46207

---
+++ src/main/java/school/faang/task_46207/Main.java
@@ -0,0 +1,27 @@
+package school.faang.task_46207;
+
+public class Main {
+    public static void main(String[] args) {
+        String result = ErrorHandler.withErrorHandling(
+                () -> {
+                    try {
+                        return RemoteService.call("someParam");
+                    } catch (Exception e) {
+                        throw new RuntimeException(e);
+                    }
+                },
+                e -> {
+                    System.out.println("Ошибка про вызове сервиса, возвращаем дефолтное заначение");
+                    return "DEFAULT";
+                }
+        );
+
+        System.out.println(result);
+    }
+
+    public static class RemoteService {
+        public static String call(String param) throws Exception {
+            throw new Exception("Сервис недоступен");
+        }
+    }
+}
