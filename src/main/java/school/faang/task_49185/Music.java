4a29ecee02787bf58f3e5452efbcf0f80296fca6
User Novozhilova <nov2303@mail,ru>
2024-12-20 09:04:31 +0500

    BJS2-49185

--- src/main/java/school/faang/task_49185/Music.java
+++ src/main/java/school/faang/task_49185/Music.java
@@ -1,20 +1,24 @@
 package school.faang.task_49185;
 
-import java.util.List;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
 
 public class Music {
     public static void main(String[] args) {
         Player player = new Player();
-        Thread threadPlay = new Thread(() -> player.play());
-        threadPlay.start();
+        ExecutorService executor = Executors.newCachedThreadPool();
 
-        Thread threadSkip = new Thread(() -> player.skip());
-        threadSkip.start();
+        executor.submit(() -> player.play());
+        executor.submit(() -> player.skip());
+        executor.submit(() -> player.previous());
+        executor.submit(() -> player.pause());
 
-        Thread threadPrevious = new Thread(() -> player.previous());
-        threadPrevious.start();
-
-        Thread threadPause = new Thread(() -> player.pause());
-        threadPause.start();
+        executor.shutdown();
+        try {
+            executor.awaitTermination(1, TimeUnit.MINUTES);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
     }
 }
