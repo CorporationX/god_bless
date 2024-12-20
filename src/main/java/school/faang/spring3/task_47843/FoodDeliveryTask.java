26a4655c29d3e8b47d4cc36880b50ae84845e103
Timka-cloud1 <amanzholtimka@gmail.com>
2024-12-20 13:38:59 +0500

    Накормите Питера Гриффина

--- src/main/java/school/faang/spring3/task_47843/FoodDeliveryTask.java
+++ src/main/java/school/faang/spring3/task_47843/FoodDeliveryTask.java
@@ -8,17 +8,18 @@ import java.util.Random;
 @AllArgsConstructor
 @Slf4j
 public class FoodDeliveryTask implements Runnable {
+    private static final int MIN_SLEEP = 1000;
+    private static final int MAX_SLEEP = 5000;
+
     private String character;
-
     private int foodAmount;
-
 
     @Override
     public void run() {
         String foodType = getFoodType();
         System.out.println(character + " gets " + foodAmount + " " + foodType);
         try {
-            Thread.sleep(new Random().nextInt(1000, 5000));
+            Thread.sleep(new Random().nextInt(MIN_SLEEP, MAX_SLEEP));
         } catch (InterruptedException e) {
             log.error("problem with method sleep " + e.getMessage());
         }
