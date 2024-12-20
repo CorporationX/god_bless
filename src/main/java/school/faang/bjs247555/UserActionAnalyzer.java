f37f45c79bca9a2ae93bb8e6ff052f80fc007a22
Maksim-VV <laulmv11@gmail.com>
2024-12-20 22:33:13 +0300

    BJS2-47555, Analysis of user activity on a social network - second edits of the arrows

--- src/main/java/school/faang/bjs247555/UserActionAnalyzer.java
+++ src/main/java/school/faang/bjs247555/UserActionAnalyzer.java
@@ -1,6 +1,7 @@
 package school.faang.bjs247555;
 
 import lombok.NonNull;
+import lombok.val;
 
 import java.time.LocalDate;
 import java.util.Arrays;
@@ -11,58 +12,60 @@ import java.util.stream.Collectors;
 
 public class UserActionAnalyzer {
     public static List<String> topActiveUsers(List<UserAction> actions) {
-        List<String> topActive = actions.stream()
-                .collect(Collectors.groupingBy(UserAction::getName, Collectors.counting()))
-                .entrySet().stream()
+        var topActive = actions.stream()
+                .collect(Collectors.groupingBy(UserAction::getName, Collectors.counting()));
+
+        return topActive.entrySet().stream()
                 .sorted(Map.Entry.<String, Long>comparingByValue().reversed())
                 .limit(10)
                 .map(Map.Entry::getKey)
                 .collect(Collectors.toList());
-        return topActive;
     }
 
     public static List<String> topPopularHashtags(@NonNull List<UserAction> actions) {
-        List<String> topPopular = actions.stream()
+        val topPopular = actions.stream()
                 .filter(UserActionAnalyzer::isFit)
                 .flatMap(action -> Arrays.stream(action.getContent().split("\\s+")))
                 .filter(word -> word.startsWith("#"))
-                .collect(Collectors.groupingBy(Function.identity(), Collectors.counting()))
-                .entrySet().stream()
+                .collect(Collectors.groupingBy(Function.identity(), Collectors.counting()));
+
+        return topPopular.entrySet().stream()
                 .sorted(Map.Entry.<String, Long>comparingByValue().reversed())
                 .limit(5)
                 .map(Map.Entry::getKey)
                 .collect(Collectors.toList());
-        return topPopular;
     }
 
     private static boolean isFit(UserAction action) {
-        return action.getContent() != null
-                && action.getActionType().equals("post") || action.getActionType().equals("comment");
+        return (action.getContent() != null && action.getActionType().equals("post"))
+                || action.getActionType().equals("comment");
     }
 
     public static List<String> topCommentersLastMonth(@NonNull List<UserAction> actions) {
         LocalDate oneMonthAgo = LocalDate.now().minusMonths(1);
-        List<String> topCommenters = actions.stream()
+
+        val topCommenters = actions.stream()
                 .filter(action -> action.getActionType().equals("comment")
                         && action.getActionDate().isAfter(oneMonthAgo))
-                .collect(Collectors.groupingBy(UserAction::getName, Collectors.counting()))
-                .entrySet().stream()
+                .collect(Collectors.groupingBy(UserAction::getName, Collectors.counting()));
+
+        return topCommenters.entrySet().stream()
                 .sorted(Map.Entry.<String, Long>comparingByValue().reversed())
                 .limit(3)
                 .map(Map.Entry::getKey)
                 .collect(Collectors.toList());
-        return topCommenters;
     }
 
     public static Map<String, Double> actionTypePercentages(@NonNull List<UserAction> actions) {
         long totalActions = actions.size();
-        Map<String, Double> actionTypePercent = actions.stream()
-                .collect(Collectors.groupingBy(UserAction::getActionType, Collectors.counting()))
-                .entrySet().stream()
+
+        val actionTypePercent = actions.stream()
+                .collect(Collectors.groupingBy(UserAction::getActionType, Collectors.counting()));
+
+        return actionTypePercent.entrySet().stream()
                 .collect(Collectors.toMap(
                         Map.Entry::getKey,
                         entry -> (entry.getValue() * 100.0) / totalActions
                 ));
-        return actionTypePercent;
     }
 }
