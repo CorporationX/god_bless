94f9b268c728fc231e8cf0f933b6493ce510cf95
Максим <vint_vin@mail.ru>
2024-12-20 20:48:35 +1000

    Готово

--- src/main/java/school/faang/sprint3/task48474theywerenice/WeasleyFamily.java
+++ src/main/java/school/faang/sprint3/task48474theywerenice/WeasleyFamily.java
@@ -6,19 +6,23 @@ import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
 public class WeasleyFamily {
-    private static List<String> tasks = new ArrayList<>() {{
+    private static List<String> tasks = new ArrayList<String>() {{
         add("Помыть посуду");
         add("Подмести пол");
         add("Приготовить ужин");
     }};
 
     public static void main(String[] args) {
-        List<Chore> chores = tasks.stream().map(Chore::new).toList();
-        ExecutorService executor = Executors.newCachedThreadPool();
+        List<Chore> chores = tasks.stream()
+                .map(Chore::new)
+                .toList();
+
+        ExecutorService executorService = Executors.newCachedThreadPool();
 
         for (Chore chore : chores) {
-            executor.execute(chore);
+            executorService.execute(chore);
         }
-        executor.shutdown();
+
+        executorService.shutdown();
     }
 }
