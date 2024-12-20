646d55ae450407ffa7dc72b98295b18cb30ae871
elnur-elnur <tahirzadee@gmail.com>
2024-12-20 11:50:03 +0400

    Task: BJS2-47237 "Анализ активности пользователей в социальной сети"
    Done - Анализ активности пользователей в социальной сети

--- src/main/java/school/faang/sprint2task47237/UserActionAnalyzer.java
+++ src/main/java/school/faang/sprint2task47237/UserActionAnalyzer.java
@@ -1,37 +1,59 @@
 package school.faang.sprint2task47237;
 
+import java.time.LocalDate;
+import java.util.Arrays;
 import java.util.List;
 import java.util.Map;
+import java.util.function.Function;
 import java.util.stream.Collectors;
-import java.util.stream.Stream;
 
 public class UserActionAnalyzer {
     public static List<String> topActiveUsers(List<UserAction> actions) {
-
-        //return actions.stream()
-        //                  .collect(Collectors.groupingBy(UserAction::getName, Collectors.counting()))  // Группировка по имени
-        //                  .entrySet().stream()
-        //                  .sorted(Map.Entry.<String, Long>comparingByValue().reversed())  // Сортировка по количеству действий
-        //                  .limit(10)
-        //                  .map(Map.Entry::getKey)  // Получаем имена пользователей
-        //                  .collect(Collectors.toList());  // Сохраняем результат в список
-
-        Map<String, Long> collect = actions.stream().collect(
-                Collectors.groupingBy(UserAction::getName, Collectors.counting())
-        );
-//
-//        System.out.println(collect);
-
-        return collect.entrySet().stream()
-                .sorted(Map.Entry.<String, Long>comparingByValue()
-                        .reversed()).limit(3)
+        return actions.stream().collect(
+                        Collectors.groupingBy(UserAction::getName, Collectors.counting())
+                )
+                .entrySet().stream()
+                .sorted(Map.Entry.<String, Long>comparingByValue().reversed()).limit(10)
                 .map(Map.Entry::getKey)
                 .collect(Collectors.toList());
+    }
 
+    public static List<String> topPopularHashtags(List<UserAction> actions) {
+        return actions.stream()
+                .filter(k ->
+                        k.getContent() != null && !k.getActionType().equals("like") && k.getContent().contains("#"))
+                .flatMap(k -> Arrays.stream(k.getContent().split("\\\\\\\\\\\\\\s+")))
+                .filter(k -> k.startsWith("#"))
+                .collect(Collectors.groupingBy(Function.identity(), Collectors.counting()))
+                .entrySet()
+                .stream()
+                .sorted(Map.Entry.<String, Long>comparingByValue().reversed())
+                .limit(5)
+                .map(Map.Entry::getKey)
+                .collect(Collectors.toList());
+    }
 
+    public static List<String> topCommentersLastMonth(List<UserAction> actions) {
+        LocalDate oneMonthAgo = LocalDate.now().minusMonths(1);
+        return actions.stream()
+                .filter(action ->
+                        action.getActionType().equals("comment") && action.getActionDate().isAfter(oneMonthAgo))
+                .collect(Collectors.groupingBy(UserAction::getName, Collectors.counting()))
+                .entrySet()
+                .stream()
+                .sorted(Map.Entry.<String, Long>comparingByValue().reversed())
+                .limit(3)
+                .map(Map.Entry::getKey)
+                .collect(Collectors.toList());
+    }
 
-
-//        System.out.println(String);
-//        return null;
+    public static Map<String, Double> actionTypePercentages(List<UserAction> actions) {
+        long totalActions = actions.size();
+        return actions.stream()
+                .collect(Collectors.groupingBy(UserAction::getActionType, Collectors.counting()))
+                .entrySet()
+                .stream()
+                .collect(Collectors.toMap(Map.Entry::getKey,
+                        k -> Math.round((k.getValue() * 100.0 / totalActions) * 100.0) / 100.0));
     }
 }
