f85aaaec38a5a2ce27828a49d1138df52d1d0a82
rode09 <theashawnta@gmail.com>
2024-12-20 13:07:30 +0300

    сделал класс Image record и переименовал переменные

--- src/main/java/school/faang/task_46576/Image.java
+++ src/main/java/school/faang/task_46576/Image.java
@@ -1,16 +1,5 @@
 package school.faang.task_46576;
 
-import lombok.Getter;
-
-@Getter
-public class Image {
-
-    private String name;
-    private String description;
-
-    public Image(String name, String description) {
-        this.name = name;
-        this.description = description;
-    }
+public record Image(String name, String description) {
 
 }
--- src/main/java/school/faang/task_46576/Main.java
+++ src/main/java/school/faang/task_46576/Main.java
@@ -8,24 +8,24 @@ public class Main {
 
         FilterProcessor filterProcessor = new FilterProcessor();
 
-        Function<Image, Image> grayscaleFilter = (image) -> new Image(image.getName(),
-                image.getDescription() + " | Фильтр: черно-белый");
-        Function<Image, Image> sepiaFilter = (image) -> new Image(image.getName(),
-                image.getDescription() + " | Фильтр: сепия");
-        Function<Image, Image> vignetteFilter = (image) -> new Image(image.getName(),
-                image.getDescription() + " | Фильтр: виньетка");
+        Function<Image, Image> grayscaleFilter = (image) -> new Image(image.name(),
+                image.description() + " | Фильтр: черно-белый");
+        Function<Image, Image> sepiaFilter = (image) -> new Image(image.name(),
+                image.description() + " | Фильтр: сепия");
+        Function<Image, Image> vignetteFilter = (image) -> new Image(image.name(),
+                image.description() + " | Фильтр: виньетка");
 
         Image grayscaleImage = filterProcessor.applyFilter(originalImage, grayscaleFilter);
-        System.out.println(grayscaleImage.getDescription());
+        System.out.println(grayscaleImage.description());
 
         Image sepiaImage = filterProcessor.applyFilter(originalImage, sepiaFilter);
-        System.out.println(sepiaImage.getDescription());
+        System.out.println(sepiaImage.description());
 
         Image vignetteImage = filterProcessor.applyFilter(originalImage, sepiaFilter);
-        System.out.println(vignetteImage.getDescription());
+        System.out.println(vignetteImage.description());
 
         Function<Image, Image> combinedFilter = filterProcessor.combineFilters(grayscaleFilter, vignetteFilter);
         Image combinedImage = filterProcessor.applyFilter(originalImage, combinedFilter);
-        System.out.println(combinedImage.getDescription());
+        System.out.println(combinedImage.description());
     }
 }
