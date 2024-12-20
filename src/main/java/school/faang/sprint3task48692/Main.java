04e8f5945f3b63ca2495eafbcd3b647ba8ad22eb
elnur-elnur <tahirzadee@gmail.com>
2024-12-20 21:29:49 +0400

    Started Task: BJS2-48692

---
+++ src/main/java/school/faang/sprint3task48692/Main.java
@@ -0,0 +1,7 @@
+package school.faang.sprint3task48692;
+
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/main/java/school/faang/sprint3task48692/SenderRunnable.java
@@ -0,0 +1,6 @@
+package school.faang.sprint3task48692;
+
+public class SenderRunnable {
+    private int startIndex;
+    private int endIndex;
+}
