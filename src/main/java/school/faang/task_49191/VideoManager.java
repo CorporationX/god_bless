2074c2241ec98b8da9b36927cdbf9d50696bcde1
Stepan Evangulov <stevangulov@gmail.com>
2024-12-20 10:54:17 +0200

    YouTube View Count

--- src/main/java/school/faang/task_49191/VideoManager.java
+++ src/main/java/school/faang/task_49191/VideoManager.java
@@ -1,11 +1,11 @@
 package school.faang.task_49191;
 
+import java.util.HashMap;
 import java.util.Map;
-import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.atomic.AtomicInteger;
 
 public class VideoManager {
-    private final Map<String, AtomicInteger> viewsMap = new ConcurrentHashMap<>();
+    private final Map<String, AtomicInteger> viewsMap = new HashMap<>();
 
     public void addView(String videoId) {
         viewsMap.computeIfAbsent(videoId, key -> new AtomicInteger(0)).incrementAndGet();
