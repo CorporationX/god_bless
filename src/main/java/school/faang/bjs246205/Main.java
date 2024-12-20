10f6ee6b427d8191ec096100a616f57268210f12
lcodio <lcodio@temp.com>
2024-12-20 20:53:57 +0300

    bjs2-46205 with RuntimeException

--- src/main/java/school/faang/bjs246205/Main.java
+++ src/main/java/school/faang/bjs246205/Main.java
@@ -5,13 +5,7 @@ public class Main {
     public static void main(String[] args) {
 
         String result = ErrorHandler.withErrorHandling(
-                () -> {
-                    try {
-                        return FileUpload.uploadFile("C:\\data.txt");
-                    } catch (Exception e) {
-                        throw new RuntimeException(e);
-                    }
-                },
+                () -> FileUpload.uploadFile("C:\\data.txt"),
                 e -> {
                     System.err.println("Error during file uploading, return default value");
                     return "DEFAULT";
