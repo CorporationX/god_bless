4b2e593cb6061b3eba86627cafa12295875f1a59
Maria DB <mbashkaeva@yandex.ru>
2024-12-20 18:11:07 +0300

    BJS2-46328 - fix

--- src/main/java/school/faang/task_46328/Droid.java
+++ src/main/java/school/faang/task_46328/Droid.java
@@ -4,7 +4,9 @@ import lombok.Data;
 
 @Data
 public class Droid {
-    String name;
+
+    private static final int CONSTANT_26 = 26;
+    private final String name;
 
     public Droid(String name) {
         this.name = name;
@@ -16,7 +18,7 @@ public class Droid {
             for (char ch : messageString.toCharArray()) {
                 if (Character.isLetter(ch)) {
                     char base = Character.isLowerCase(ch) ? 'a' : 'A';
-                    messageEncryptor.append((char) ((ch - base + keyEncryptor) % 26 + base));
+                    messageEncryptor.append((char) ((ch - base + keyEncryptor) % CONSTANT_26 + base));
                 } else {
                     messageEncryptor.append(ch);
                 }
@@ -32,7 +34,7 @@ public class Droid {
             for (char ch : messageString.toCharArray()) {
                 if (Character.isLetter(ch)) {
                     char base = Character.isLowerCase(ch) ? 'a' : 'A';
-                    messageEncryptor.append((char) ((ch - base - keyEncryptor) % 26 + base));
+                    messageEncryptor.append((char) ((ch - base - keyEncryptor) % CONSTANT_26 + base));
                 } else {
                     messageEncryptor.append(ch);
                 }
@@ -44,11 +46,11 @@ public class Droid {
 
     public void sendMessage(String message, int key, Droid droidRecipient) {
         String messageForRecipient = encryptMessage(message, key);
-        System.out.println("Сообщение " + messageForRecipient + " отправлено Дроиду " + droidRecipient.getName());
+        System.out.printf("Сообщение %s отправлено Дроиду %s\n", messageForRecipient, droidRecipient.getName());
         receiveMessage(messageForRecipient, key);
     }
 
     public void receiveMessage(String message, int key) {
-        System.out.println("Получено расшифрованное сообщение: " + decryptMessage(message, key));
+        System.out.printf("Получено расшифрованное сообщение %s\n", decryptMessage(message, key));
     }
 }
