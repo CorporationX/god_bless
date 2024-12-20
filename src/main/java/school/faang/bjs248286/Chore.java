b60cdc29bf28002e4787acaa23ea92db077356e7
Leonid Zhuravel <zhuravel_leonid@yahoo.com>
2024-12-20 13:57:55 +0200

    PR-ready

--- src/main/java/school/faang/bjs248286/Chore.java
+++ src/main/java/school/faang/bjs248286/Chore.java
@@ -1,15 +1,17 @@
 package school.faang.bjs248286;
 
 import lombok.AllArgsConstructor;
+import lombok.SneakyThrows;
 
 @AllArgsConstructor
-public class Chore implements Runnable{
+public class Chore implements Runnable {
     private final String chore;
 
+    @SneakyThrows
     @Override
     public void run() {
-        System.out.println(Thread.currentThread().getName());
-        Thread.sleep(1000);
-
+        System.out.println(Thread.currentThread().getName() + " started");
+        Thread.sleep(3000);
+        System.out.println(chore + " completed");
     }
 }
--- src/main/java/school/faang/bjs248286/WeasleyFamily.java
+++ src/main/java/school/faang/bjs248286/WeasleyFamily.java
@@ -1,15 +1,28 @@
 package school.faang.bjs248286;
 
-import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
 
 public class WeasleyFamily {
-    List<Chore> chores = new ArrayList<>();
+    static List<String> tasks = List.of("помыть посуду", "подмести пол", "приготовить ужин", "станцевать");
 
     public static void main(String[] args) {
         ExecutorService executors = Executors.newCachedThreadPool();
-        //executors.
+        tasks.forEach(task -> {
+            Chore chore = new Chore(task);
+            executors.execute(chore);
+        });
+        executors.shutdown();
+        try {
+            if (!executors.awaitTermination(5, TimeUnit.SECONDS)) {
+                System.out.println("Executors processing...");
+            }
+        } catch (InterruptedException e) {
+            System.out.println("Process crashed: " + e.getMessage());
+            executors.shutdownNow();
+        }
+        System.out.println("Main thread complete");
     }
 }
