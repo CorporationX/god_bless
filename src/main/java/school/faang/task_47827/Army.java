27990deb7f6637f7769fdd84b89e6f483a18b4e6
Sergey Senin <04sersen@gmail.com>
2024-12-20 04:49:28 +0300

    Исключения `InterruptedException` обрабатываются внутри метода; переменные `calculator` и `thread` вынесены за пределы цикла; добавлено использование `Thread.currentThread().interrupt()`.

--- src/main/java/school/faang/task_47827/Army.java
+++ src/main/java/school/faang/task_47827/Army.java
@@ -15,16 +15,24 @@ public class Army {
         List<Thread> threads = new ArrayList<>();
         List<PowerCalculator> tasks = new ArrayList<>();
 
+        PowerCalculator calculator;
+        Thread thread;
+
         for (Unit unit : units) {
-            PowerCalculator calculator = new PowerCalculator(unit);
+            calculator = new PowerCalculator(unit);
             tasks.add(calculator);
-            Thread thread = new Thread(calculator);
+            thread = new Thread(calculator);
             threads.add(thread);
             thread.start();
         }
 
-        for (Thread thread : threads) {
-            thread.join();
+        for (Thread t : threads) {
+            try {
+                t.join();
+            } catch (InterruptedException e) {
+                System.err.println("Thread execution was interrupted: " + e.getMessage());
+                Thread.currentThread().interrupt();
+            }
         }
 
         for (PowerCalculator task : tasks) {
