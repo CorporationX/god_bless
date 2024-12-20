e5a92a86d36dbe89551efb5697269447b060a996
narol01 <kmingogod@gmail.com>
2024-12-20 18:03:22 +0100

    BJS2-48885

--- src/main/java/school/faang/task_48885/entity/House.java
+++ src/main/java/school/faang/task_48885/entity/House.java
@@ -1,76 +1,61 @@
 package school.faang.task_48885.entity;
 
+import lombok.Getter;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import java.util.ArrayList;
-import java.util.HashSet;
+import java.util.Collections;
 import java.util.List;
 import java.util.Random;
-import java.util.Set;
-import java.util.concurrent.Executors;
-import java.util.concurrent.ScheduledExecutorService;
-import java.util.concurrent.TimeUnit;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
 
+@Getter
 public class House {
-    private static final List<Room> roomInHouse = new ArrayList<>();
     private static final Random rand = new Random();
-    private static final Set<Food> collectedFood = new HashSet<>();
+    private static final Lock lock = new ReentrantLock();
+    private static final List<Food> collectedFood = Collections.synchronizedList(new ArrayList<>());
+    private static final Logger log = LoggerFactory.getLogger(House.class);
 
-    public static void collectFood(List<Room> rooms) {
-        synchronized (collectedFood) {
-            Room room = rooms.get(rand.nextInt(rooms.size()));
-            if (!room.getFoodList().isEmpty()) {
-                collectedFood.addAll(room.getFoodList());
-                room.getFoodList().clear();
-            }
+    public void collectFood(List<Room> rooms) {
+        Room room1;
+        Room room2;
+        lock.lock();
+        List<Room> availableRooms = rooms.stream().filter(Room::isAvailable).toList();
+        if (availableRooms.size() < 2) {
+            System.out.println("All rooms are clear!");
+            lock.unlock();
+            return;
+        }
+        room1 = availableRooms.get(rand.nextInt(availableRooms.size()));
+        do {
+            room2 = availableRooms.get(rand.nextInt(availableRooms.size()));
+        } while (room2 == room1);
+        lock.unlock();
 
-            Room room2;
-            do {
-                room2 = rooms.get(rand.nextInt(rooms.size()));
-            } while (room2 == room);
-            if (!room2.getFoodList().isEmpty()) {
-                collectedFood.addAll(room2.getFoodList());
-                room2.getFoodList().clear();
-            }
+        room1.startCleaning();
+        clearedRoom(room1);
+        room1.finishCleared();
+
+        room2.startCleaning();
+        clearedRoom(room2);
+        room2.finishCleared();
+
+    }
+
+    private static void clearedRoom(Room room) {
+        collectedFood.addAll(room.getFoodList());
+        log.info("Collected food: {} from {}", collectedFood, room);
+        room.getFoodList().clear();
+        try {
+            Thread.sleep(10000);
+        } catch (InterruptedException e) {
+            Thread.currentThread().interrupt();
         }
     }
 
-    public static void main(String[] args) {
-        Room kitchen = new Room();
-        Room livingRoom = new Room();
-        Room bedroom = new Room();
-        kitchen.addFood(new Food("Apple"));
-        livingRoom.addFood(new Food("Sandwich"));
-        bedroom.addFood(new Food("Chocolate"));
-        kitchen.addFood(new Food("Banana"));
-        livingRoom.addFood(new Food("Juice"));
-        bedroom.addFood(new Food("Cookies"));
-
-        roomInHouse.add(kitchen);
-        roomInHouse.add(livingRoom);
-        roomInHouse.add(bedroom);
-
-        System.out.println("Before food collected:");
-        System.out.println(roomInHouse);
-        Runnable task = (() -> {
-            synchronized (roomInHouse) {
-                collectFood(roomInHouse);
-            }
-        });
-        ScheduledExecutorService scheduler = Executors.newScheduledThreadPool(5);
-        for (int i = 0; i < 5; i++) {
-            scheduler.scheduleAtFixedRate(task, 1, 30, TimeUnit.SECONDS);
-        }
-
-        try {
-            if (scheduler.awaitTermination(15, TimeUnit.SECONDS)) {
-                scheduler.shutdown();
-            }
-        } catch (InterruptedException e) {
-            throw new RuntimeException(e);
-        }
-        System.out.println("\nAll collected food:");
-        System.out.println(collectedFood);
-        System.out.println("\nAfter food collected:");
-        System.out.println(roomInHouse);
-        System.out.println("All food collected");
+    public List<Food> getCollectedFood() {
+        return collectedFood;
     }
 }
