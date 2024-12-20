068c296ac5faaa713f6e7d7c6d7c73e990e4c1fd
Максим <vint_vin@mail.ru>
2024-12-20 20:39:15 +1000

    Готово

---
+++ src/main/java/school/faang/sprint3/task48474theywerenice/WeasleyFamily.java
@@ -0,0 +1,26 @@
+package school.faang.sprint3.task48474theywerenice;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+public class WeasleyFamily {
+    private static List<String> tasks = new ArrayList<>() {{
+        add("Помыть посуду");
+        add("Подмести пол");
+        add("Приготовить ужин");
+    }};
+
+    public static void main(String[] args) {
+        List<Chore> chores = tasks.stream()
+                .map(Chore::new)
+                .toList();
+        ExecutorService executor = Executors.newCachedThreadPool();
+
+        for (Chore chore : chores) {
+            executor.execute(chore);
+        }
+        executor.shutdown();
+    }
+}
