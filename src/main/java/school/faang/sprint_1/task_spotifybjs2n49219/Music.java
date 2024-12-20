6458be13fdbaad5037265dcad49571c9a12f4db5
alexey_obraztsov <alexey.autotester86@gmail.com>
2024-12-20 13:58:17 +0300

    alexeyrnd-BJS2-49219. Блокировка Spotify. initial. structure only

---
+++ src/main/java/school/faang/sprint_1/task_spotifybjs2n49219/Music.java
@@ -0,0 +1,5 @@
+package school.faang.sprint_1.task_spotifybjs2n49219;
+
+public class Music {
+
+}
---
+++ src/main/java/school/faang/sprint_1/task_spotifybjs2n49219/Player.java
@@ -0,0 +1,19 @@
+package school.faang.sprint_1.task_spotifybjs2n49219;
+
+public class Player {
+    private Object lock;
+    private boolean isPlaying;
+
+    public void play() {
+
+    }
+    public void pause() {
+
+    }
+    public void skip() {
+
+    }
+    public void previous() {
+
+    }
+}
