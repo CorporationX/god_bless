fc75ed58714d8adad6a2cd6b24cdafa5c02aa32a
rauanzhanabergen <rauan.zhanabergen@guavapay.com>
2024-12-20 23:11:57 +0500

    BJS2-45694 done

---
+++ src/main/java/school/faang/sprint2/task_45694/SpellCaster.java
@@ -0,0 +1,8 @@
+package school.faang.sprint2.task_45694;
+
+public class SpellCaster {
+
+    void cast(String spell, SpellAction spellAction) {
+        System.out.println(spellAction.action(spell));
+    }
+}
