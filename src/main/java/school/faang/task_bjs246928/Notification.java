abe4d76b6121815b341f2881ca0133ffbaa27e96
Kir34 <kirnovikof1@gmail.com>
2024-12-20 03:55:51 +0300

    задание с типами сообщений

---
+++ src/main/java/school/faang/task_bjs246928/Notification.java
@@ -0,0 +1,11 @@
+package school.faang.task_bjs246928;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class Notification {
+    private String type;
+    private String message;
+}
