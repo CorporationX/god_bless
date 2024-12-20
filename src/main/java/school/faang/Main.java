d4f3c7d6a7bc1e236b863d6a49db6252952827f4
olegmankirov <oleg_mankirov@stimul.app>
2024-12-20 22:32:54 +0300

    bjs2-45702-hogwarts

--- src/main/java/school/faang/Main.java
+++ src/main/java/school/faang/Main.java
@@ -2,30 +2,40 @@ package school.faang;
 
 public class Main {
     public static void main(String[] args) {
-        try {
-            new User("Oleg", 35, "Amazon", "London");
-        } catch (IllegalArgumentException e) {
-            System.out.println("Catch exception " + e.getMessage());
-        }
-        try {
-            new User("", 35, "Amazon", "London");
-        } catch (IllegalArgumentException e) {
-            System.out.println("Catch exception " + e.getMessage());
-        }
-        try {
-            new User("Oleg", 17, "Amazon", "London");
-        } catch (IllegalArgumentException e) {
-            System.out.println("Catch exception " + e.getMessage());
-        }
-        try {
-            new User("Oleg", 35, "VK", "London");
-        } catch (IllegalArgumentException e) {
-            System.out.println("Catch exception " + e.getMessage());
-        }
-        try {
-            new User("Oleg", 35, "Amazon", "Moscow");
-        } catch (IllegalArgumentException e) {
-            System.out.println("Catch exception " + e.getMessage());
-        }
+        SpellCaster spellCaster = new SpellCaster();
+
+        String alohomora = "Alohomora";
+        String lumos = "Lumos";
+        String expelliarmus = "Expelliarmus";
+
+        spellCaster.cast(alohomora, (spell) -> "The door is unlocked by " + spell);
+        spellCaster.cast(lumos, (spell) -> "A beam of light is created by " + spell);
+        spellCaster.cast(expelliarmus, (spell) -> "The opponent is disarmed by " + spell);
+
+//        try {
+//            new User("Oleg", 35, "Amazon", "London");
+//        } catch (IllegalArgumentException e) {
+//            System.out.println("Catch exception " + e.getMessage());
+//        }
+//        try {
+//            new User("", 35, "Amazon", "London");
+//        } catch (IllegalArgumentException e) {
+//            System.out.println("Catch exception " + e.getMessage());
+//        }
+//        try {
+//            new User("Oleg", 17, "Amazon", "London");
+//        } catch (IllegalArgumentException e) {
+//            System.out.println("Catch exception " + e.getMessage());
+//        }
+//        try {
+//            new User("Oleg", 35, "VK", "London");
+//        } catch (IllegalArgumentException e) {
+//            System.out.println("Catch exception " + e.getMessage());
+//        }
+//        try {
+//            new User("Oleg", 35, "Amazon", "Moscow");
+//        } catch (IllegalArgumentException e) {
+//            System.out.println("Catch exception " + e.getMessage());
+//        }
     }
 }
