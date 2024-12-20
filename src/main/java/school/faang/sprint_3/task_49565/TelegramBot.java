f61cb286df58bff176ee5cfcdfca97341b3850a0
rostex <rostex2@yandex.ru>
2024-12-20 19:06:36 +0300

    Внесены изменения после ревью

--- src/main/java/school/faang/sprint_3/task_49565/TelegramBot.java
+++ src/main/java/school/faang/sprint_3/task_49565/TelegramBot.java
@@ -1,14 +1,17 @@
 package school.faang.sprint_3.task_49565;
 
-import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 
-import java.time.LocalDateTime;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.locks.Condition;
+import java.util.concurrent.locks.ReentrantLock;
 
 @Slf4j
 public class TelegramBot {
     private static final int REQUEST_LIMIT = 5;
     public static final long REQUEST_DELAY = 1000;
+    private final ReentrantLock lock = new ReentrantLock();
+    private final Condition condition = lock.newCondition();
     private int requestCounter;
     private long lastRequestTime;
 
@@ -18,30 +21,35 @@ public class TelegramBot {
         this.lastRequestTime = System.currentTimeMillis();
     }
 
-    public synchronized void sendMessage(String message) {
+    public void sendMessage(String message) {
         long currentTime = System.currentTimeMillis();
         long timeElapsed = currentTime - lastRequestTime;
         long waitTime = REQUEST_DELAY - timeElapsed;
 
-        if (timeElapsed < REQUEST_DELAY) {
-            log.info("Запрос №{}", requestCounter);
-            requestCounter++;
-            if (requestCounter > REQUEST_LIMIT) {
-                try {
-                    log.info("Ожидаю окончание срока приостановки запроса. Осталось {} мс", waitTime);
-                    Thread.sleep(waitTime);
-                    requestCounter = 0;
-                } catch (InterruptedException e) {
-                    Thread.currentThread().interrupt();
-                    log.error("Поток прерван во время ожидания приостановки запроса");
+        lock.lock();
+        try {
+            if (timeElapsed < REQUEST_DELAY) {
+                log.info("Запрос №{}", requestCounter);
+                requestCounter++;
+                if (requestCounter > REQUEST_LIMIT) {
+                    try {
+                        log.info("Ожидаю окончание срока приостановки запроса. Осталось {} мс", waitTime);
+                        condition.await(waitTime, TimeUnit.MILLISECONDS);
+                        requestCounter = 0;
+                    } catch (InterruptedException e) {
+                        Thread.currentThread().interrupt();
+                        log.error("Поток прерван во время ожидания приостановки запроса");
+                    }
                 }
+            } else {
+                requestCounter = 0;
+                lastRequestTime = currentTime;
+                condition.signalAll();
             }
-        } else {
-            requestCounter = 0;
-            lastRequestTime = currentTime;
-            notifyAll();
+            System.out.println("Отправлено сообщение: " + message);
+        } finally {
+            lock.unlock();
         }
-        System.out.println("Отправлено сообщение: " + message);
 
     }
 }
