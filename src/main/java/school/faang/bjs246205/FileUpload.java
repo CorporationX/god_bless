858f866ed885332493be3c4c6db983a3f85a1cf7
lcodio <lcodio@temp.com>
2024-12-20 18:35:08 +0300

    bjs2-46205 questions in a comment

--- src/main/java/school/faang/bjs246205/FileUpload.java
+++ src/main/java/school/faang/bjs246205/FileUpload.java
@@ -2,7 +2,7 @@ package school.faang.bjs246205;
 
 public class FileUpload {
 
-    public static String uploadFile(String pathFile) throws Exception {
-        throw new RuntimeException("This file already exists " + pathFile);
+    public static String uploadFile(String pathFile) {
+        throw new RuntimeException(pathFile);
     }
 }
--- src/main/java/school/faang/bjs246205/Main.java
+++ src/main/java/school/faang/bjs246205/Main.java
@@ -2,15 +2,21 @@ package school.faang.bjs246205;
 
 public class Main {
 
-/*    public static void main(String[] args) {
+    public static void main(String[] args) {
 
         String result = ErrorHandler.withErrorHandling(
-                () -> FileUpload.uploadFile("C:\\data.txt"),
+                () -> {
+                    try {
+                        return FileUpload.uploadFile("C:\\data.txt");
+                    } catch (Exception e) {
+                        throw new RuntimeException(e);
+                    }
+                },
                 e -> {
                     System.err.println("Error during file uploading, return default value");
                     return "DEFAULT";
                 }
         );
         System.out.println(result);
-    }*/
+    }
 }
