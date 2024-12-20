2da7f3ea2c79604cf163d890a0c9c7c0e670624b
Artur Suleymanov <suleymanov.usa@gmail.com>
2024-12-20 11:07:44 -0800

    Анализ активности пользователей в социальной сети

---
+++ src/main/java/school/faang/bjs247340/UserAction.java
@@ -0,0 +1,9 @@
+package school.faang.bjs247340;
+
+import lombok.extern.slf4j.Slf4j;
+
+import java.time.LocalDate;
+
+@Slf4j
+public record UserAction(int id, String name, String actionType, LocalDate actionDate, String content) {
+}
