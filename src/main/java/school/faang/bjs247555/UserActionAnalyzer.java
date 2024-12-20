d92d56848c7cb87e84d250dde12dd9c7f56adfc4
Maksim-VV <laulmv11@gmail.com>
2024-12-20 15:41:51 +0300

    BJS2-47555, Analysis of user activity on a social network - after the first release

--- src/main/java/school/faang/bjs247555/UserActionAnalyzer.java
+++ src/main/java/school/faang/bjs247555/UserActionAnalyzer.java
@@ -11,20 +11,20 @@ import java.util.stream.Collectors;
 
 public class UserActionAnalyzer {
     public static List<String> topActiveUsers(List<UserAction> actions) {
-        return actions.stream()
+        List<String> topActive = actions.stream()
                 .collect(Collectors.groupingBy(UserAction::getName, Collectors.counting()))
                 .entrySet().stream()
                 .sorted(Map.Entry.<String, Long>comparingByValue().reversed())
                 .limit(10)
                 .map(Map.Entry::getKey)
                 .collect(Collectors.toList());
+        return topActive;
     }
 
     public static List<String> topPopularHashtags(@NonNull List<UserAction> actions) {
-        return actions.stream()
-                .filter(action -> action.getContent() != null
-                        && action.getActionType().equals("post") || action.getActionType().equals("comment"))
-                .flatMap(action -> Arrays.stream(action.getContent().split("\\\\\\\\s+")))
+        List<String> topPopular = actions.stream()
+                .filter(UserActionAnalyzer::isFit)
+                .flatMap(action -> Arrays.stream(action.getContent().split("\\s+")))
                 .filter(word -> word.startsWith("#"))
                 .collect(Collectors.groupingBy(Function.identity(), Collectors.counting()))
                 .entrySet().stream()
@@ -32,11 +32,17 @@ public class UserActionAnalyzer {
                 .limit(5)
                 .map(Map.Entry::getKey)
                 .collect(Collectors.toList());
+        return topPopular;
+    }
+
+    private static boolean isFit(UserAction action) {
+        return action.getContent() != null
+                && action.getActionType().equals("post") || action.getActionType().equals("comment");
     }
 
     public static List<String> topCommentersLastMonth(@NonNull List<UserAction> actions) {
         LocalDate oneMonthAgo = LocalDate.now().minusMonths(1);
-        return actions.stream()
+        List<String> topCommenters = actions.stream()
                 .filter(action -> action.getActionType().equals("comment")
                         && action.getActionDate().isAfter(oneMonthAgo))
                 .collect(Collectors.groupingBy(UserAction::getName, Collectors.counting()))
@@ -45,16 +51,18 @@ public class UserActionAnalyzer {
                 .limit(3)
                 .map(Map.Entry::getKey)
                 .collect(Collectors.toList());
+        return topCommenters;
     }
 
     public static Map<String, Double> actionTypePercentages(@NonNull List<UserAction> actions) {
         long totalActions = actions.size();
-        return actions.stream()
+        Map<String, Double> actionTypePercent = actions.stream()
                 .collect(Collectors.groupingBy(UserAction::getActionType, Collectors.counting()))
                 .entrySet().stream()
                 .collect(Collectors.toMap(
                         Map.Entry::getKey,
                         entry -> (entry.getValue() * 100.0) / totalActions
                 ));
+        return actionTypePercent;
     }
 }
