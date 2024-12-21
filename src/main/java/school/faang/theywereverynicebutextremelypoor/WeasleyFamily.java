c578aa5314ebb504d314ff6e7cf595591ebaac55
muslim <xammuslim06@gmail.com>
2024-12-21 23:32:59 +0300

    They were very nice but extremely poor

---
+++ src/main/java/school/faang/theywereverynicebutextremelypoor/WeasleyFamily.java
@@ -0,0 +1,33 @@
+package school.faang.theywereverynicebutextremelypoor;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
+
+public class WeasleyFamily {
+
+    public static void main(String[] args) {
+        String[] chores = {"помыть посуду",
+                "подмести пол",
+                "приготовить ужин",
+                "погладить вещи",
+                "вынести мусор"
+        };
+
+        ExecutorService executor = Executors.newCachedThreadPool();
+
+        for (String task : chores) {
+            Chore chore = new Chore(task);
+            executor.execute(chore);
+        }
+
+        try {
+            if (!executor.awaitTermination(1, TimeUnit.MINUTES)) {
+                System.out.println("Задачи не выполнились за 1 минуту, принудительно останавливаем...");
+                executor.shutdownNow();
+            }
+        } catch (InterruptedException e) {
+            executor.shutdownNow();
+        }
+    }
+}
