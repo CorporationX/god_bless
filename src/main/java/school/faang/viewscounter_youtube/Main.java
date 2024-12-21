f901d7e84f74c1b4cb9a7628edde56d78d7cff88
AesaQ <ilyaovchinnikov2095@gmail.com>
2024-12-21 18:35:05 +0500

    checkstyle fix

--- src/main/java/school/faang/viewscounter_youtube/Main.java
+++ src/main/java/school/faang/viewscounter_youtube/Main.java
@@ -6,17 +6,17 @@ import java.util.concurrent.TimeUnit;
 
 public class Main {
     public static void main(String[] args) throws InterruptedException {
-        final int NUM_THREADS = 100;
-        final int NUM_VIDEOS = 10;
+        final int numThreads = 100;
+        final int numVideos = 10;
 
-        ExecutorService executor = Executors.newFixedThreadPool(NUM_THREADS);
+        ExecutorService executor = Executors.newFixedThreadPool(numThreads);
         VideoManager videoManager = new VideoManager();
 
-        for (int i = 1; i <= NUM_VIDEOS; i++) {
+        for (int i = 1; i <= numVideos; i++) {
             videoManager.addNewVideo("Video " + i);
         }
         videoManager.getViewsMap().forEach((video, views) -> {
-            for (int i = 1; i <= NUM_THREADS / NUM_VIDEOS; i++) {
+            for (int i = 1; i <= numThreads / numVideos; i++) {
                 executor.submit(() -> {
                     videoManager.addView(video);
                     System.out.println(video + " has " + videoManager.getViewCount(video) + " views");
