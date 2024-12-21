aaeb4ff76e0ca2860a2bb50af6dd2c02e6bd50c2
Eugene <@evgeniy_kim>
2024-12-21 11:17:48 +0500

    update

--- src/main/java/school/faang/task_49231/Game.java
+++ src/main/java/school/faang/task_49231/Game.java
@@ -1,26 +1,22 @@
 package school.faang.task_49231;
 
+import java.util.concurrent.atomic.AtomicInteger;
+
 public class Game {
-    private int score = 0;
-    private int lives = 10;
-    private final Object scoreLock = new Object();
-    private final Object livesLock = new Object();
+    private final AtomicInteger score = new AtomicInteger(0);
+    private final AtomicInteger lives = new AtomicInteger(10);
 
     public void update(boolean earnedPoints, boolean lostLife) {
-        synchronized (scoreLock) {
-            if (earnedPoints) {
-                score++;
-                System.out.println("Очки увеличены, текущий счёт: " + score);
-            }
+        if (earnedPoints) {
+            score.incrementAndGet();
+            System.out.println("Очки увеличены, текущий счёт: " + score);
         }
 
-        synchronized (livesLock) {
-            if (lostLife) {
-                lives--;
-                System.out.println("Жизни уменьшены, оставшиеся жизни: " + lives);
-                if (lives <= 0) {
-                    gameOver();
-                }
+        if (lostLife) {
+            lives.decrementAndGet();
+            System.out.println("Жизни уменьшены, оставшиеся жизни: " + lives);
+            if (lives.get() <= 0) {
+                gameOver();
             }
         }
     }
