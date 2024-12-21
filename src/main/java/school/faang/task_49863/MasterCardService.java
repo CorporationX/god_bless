ca551286aad472a58da74536affe12d2e5a141f1
az3l1t <eternity_cr9p@mail.ru>
2024-12-21 11:47:48 +0300

    Асинхронность и Future

---
+++ src/main/java/school/faang/task_49863/MasterCardService.java
@@ -0,0 +1,12 @@
+package school.faang.task_49863;
+
+public class MasterCardService {
+    public int collectPayment() throws InterruptedException {
+        Thread.sleep(10000);
+        return 10000;
+    }
+    public int sendAnalytics() throws InterruptedException {
+        Thread.sleep(1000);
+        return 1000;
+    }
+}
