042126ffbb38253213b1b10b46ed980bff42e7cb
VladimirGontarenko <mister.gontarenko@yandex.ru>
2024-12-22 00:20:06 +0400

    task48100 Помогите коллеге

---
+++ src/main/java/school/faang/task48100/Person.java
@@ -0,0 +1,16 @@
+package school.faang.task48100;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.ToString;
+
+@ToString
+@Getter
+@AllArgsConstructor
+public class Person {
+    private String name;
+    private String surname;
+    private int age;
+    private String workplace;
+}
