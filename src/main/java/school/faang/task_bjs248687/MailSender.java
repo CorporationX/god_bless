1be3032517d6c84be2246f44f6c43d59877f1b5a
Kir34 <kirnovikof1@gmail.com>
2024-12-22 00:07:42 +0300

    задание с оправкой писем

--- src/main/java/school/faang/task_bjs248687/MailSender.java
+++ src/main/java/school/faang/task_bjs248687/MailSender.java
@@ -1,22 +1,28 @@
 package school.faang.task_bjs248687;
 
 public class MailSender {
-    public static void main(String[] args) throws InterruptedException {
+    public static void main(String[] args) {
         int totalMessages = 1000;
         int threadsCount = 5;
         int batchSize = totalMessages / threadsCount;
+        int start;
+        int end;
 
         Thread[] threads = new Thread[threadsCount];
 
         for (int i = 0; i < threadsCount; i++) {
-            int start = i * batchSize;
-            int end = (i + 1) * batchSize;
+            start = i * batchSize;
+            end = (i + 1) * batchSize;
             threads[i] = new Thread(new SenderRunnable(start, end));
             threads[i].start();
         }
 
         for (Thread thread : threads) {
-            thread.join();
+            try {
+                thread.join();
+            } catch (InterruptedException e) {
+                System.out.println("Ошибка");
+            }
         }
 
         System.out.println("Все письма отправлены!");
