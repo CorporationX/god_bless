e18693e738187d1ae6ca50fbf172dc05f9f6cadf
Leonid Zhuravel <zhuravel_leonid@yahoo.com>
2024-12-21 17:10:25 +0200

    PR-fixes

--- src/main/java/school/faang/bjs249220/Music.java
+++ src/main/java/school/faang/bjs249220/Music.java
@@ -1,17 +1,17 @@
 package school.faang.bjs249220;
 
+import java.util.List;
+
 public class Music {
     public static void main(String[] args) {
         Player player = new Player(false);
+        final List<Thread> threads = List.of(
+                new Thread(player::play),
+                new Thread(player::pause),
+                new Thread(player::skip),
+                new Thread(player::previous)
+        );
 
-        final Thread playbackThread = new Thread(player::play);
-        final Thread pauseThread = new Thread(player::pause);
-        final Thread skipThread = new Thread(player::skip);
-        final Thread previousThread = new Thread(player::previous);
-
-        playbackThread.start();
-        pauseThread.start();
-        skipThread.start();
-        previousThread.start();
+        threads.forEach(Thread::start);;
     }
 }
