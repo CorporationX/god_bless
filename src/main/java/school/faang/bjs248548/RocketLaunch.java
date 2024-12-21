e67cd0c53973c11ed3cca0bde18eb1344001ffec
Pavel <45390423+rsticks@users.noreply.github.com>
2024-12-22 01:36:48 +0300

    BJS2-48548 Космическая программа SpaceX

---
+++ src/main/java/school/faang/bjs248548/RocketLaunch.java
@@ -0,0 +1,69 @@
+package school.faang.bjs248548;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.extern.slf4j.Slf4j;
+
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.util.List;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
+
+@Slf4j
+@AllArgsConstructor
+@Getter
+public class RocketLaunch {
+    private static final int LAUNCH_DELAY = 1000;
+    private final String name;
+    private final LocalDateTime launchTime;
+
+    public Long getLaunchTimeMillis() {
+        return launchTime.atZone(ZoneId.systemDefault()).toInstant().toEpochMilli();
+    }
+
+    public void launch() {
+        sleep(String.format("Launch %s", name));
+        log.info("Launching {}, is finished", name);
+    }
+
+    public static void planRocketLaunches(List<RocketLaunch> launches) {
+        long start = System.currentTimeMillis();
+        ExecutorService executor = Executors.newSingleThreadExecutor();
+        launches.forEach(rocket -> {
+            long delay = rocket.getLaunchTimeMillis() - System.currentTimeMillis();
+            try {
+                if (delay > 0) {
+                    Thread.sleep(delay);
+                }
+                executor.submit(rocket::launch);
+                log.info("Rocket {} launch planed in {} ms", rocket.name, delay);
+            } catch (InterruptedException e) {
+                log.error(e.getMessage());
+            }
+        });
+        shutdown(executor);
+
+        log.info("Launch time: {} ms", System.currentTimeMillis() - start);
+    }
+
+    private static void shutdown(ExecutorService executor) {
+        executor.shutdown();
+        try {
+            if (!executor.awaitTermination(2, TimeUnit.SECONDS)) {
+                executor.shutdownNow();
+            }
+        } catch (InterruptedException e) {
+            log.error(e.getMessage());
+        }
+    }
+
+    private static void sleep(String messagePrefix) {
+        try {
+            Thread.sleep(LAUNCH_DELAY);
+        } catch (InterruptedException e) {
+            log.error("{} terminated, error: {}", messagePrefix, e.getMessage());
+        }
+    }
+}
---
+++ src/test/java/school/faang/bjs248548/RocketLaunchTest.java
@@ -0,0 +1,26 @@
+package school.faang.bjs248548;
+
+import org.junit.jupiter.api.Test;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+
+class RocketLaunchTest {
+    @Test
+    void launch() throws InterruptedException {
+        List<RocketLaunch> launches = List.of(
+                new RocketLaunch("falcon 9", LocalDateTime.now().plusSeconds(1)),
+                new RocketLaunch("falcon heavy", LocalDateTime.now().plusSeconds(5)),
+                new RocketLaunch("falcon 9 (2)", LocalDateTime.now().plusSeconds(10)),
+                new RocketLaunch("falcon 9 lite", LocalDateTime.now().plusSeconds(10)),
+                new RocketLaunch("star ship 1", LocalDateTime.now().plusSeconds(11)),
+                new RocketLaunch("star ship 2", LocalDateTime.now().plusSeconds(11)),
+                new RocketLaunch("star ship 3", LocalDateTime.now().plusSeconds(11))
+        );
+
+        RocketLaunch.planRocketLaunches(launches);
+
+        Thread.sleep(20000);
+    }
+}
