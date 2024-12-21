bd591fe4408201732ada3b7b493df52433b51c6a
az3l1t <eternity_cr9p@mail.ru>
2024-12-21 19:37:05 +0300

    Турнир трех волшебников

--- src/main/java/school/faang/task_50708/Tournament.java
+++ src/main/java/school/faang/task_50708/Tournament.java
@@ -11,51 +11,39 @@ import java.util.concurrent.ExecutorService;
 @AllArgsConstructor
 public class Tournament {
     private static final Random RANDOM = new Random();
-    private static final int EASY_TIME = 2999;
-    private static final int HARD_TIME = 6999;
+    private final int EASY_TIME = 2999;
+    private final int HARD_TIME = 6999;
 
     public CompletableFuture<School> startTask(School school, Task task, ExecutorService executorService) {
         int baseChanceEasyLevel = RANDOM.nextInt(50, 101);
         int baseChanceHardLevel = 20;
 
         return switch (task.getDifficulty()) {
-          case 0, 1, 2 -> CompletableFuture.supplyAsync(() -> {
-              int chance = RANDOM.nextInt(0, 101);
-              school.setStudents(school.getStudents().stream()
-                        .peek(student -> {
-                            if (chance <= baseChanceEasyLevel) {
-                                student.setPoints(student.getPoints() + task.getReward());
-                            }
-                        })
-                        .toList());
-
-              try {
-                  Thread.sleep(EASY_TIME);
-              } catch (InterruptedException e) {
-                  throw new RuntimeException(e);
-              }
-              return school;
-          }, executorService);
-
-          case 3, 4, 5 -> CompletableFuture.supplyAsync(() -> {
-              int chance = RANDOM.nextInt(0, 101);
-              school.setStudents(school.getStudents().stream()
-                        .peek(student -> {
-                            if (chance <= baseChanceHardLevel) {
-                                student.setPoints(student.getPoints() + task.getReward());
-                            }
-                        })
-                        .toList());
-
-              try {
-                  Thread.sleep(HARD_TIME);
-              } catch (InterruptedException e) {
-                  throw new RuntimeException(e);
-              }
-              return school;
-          }, executorService);
-
+          case 0, 1, 2 -> makingSomeTasks(school, task, baseChanceEasyLevel, EASY_TIME)
+                    .thenApplyAsync(s -> s, executorService);
+          case 3, 4, 5 -> makingSomeTasks(school, task, baseChanceHardLevel, HARD_TIME)
+                    .thenApplyAsync(s -> s, executorService);
           default -> throw new IllegalArgumentException("Something went wrong!");
         };
+    }
+
+    public CompletableFuture<School> makingSomeTasks(School school, Task task, int baseChance, int time) {
+        return CompletableFuture.supplyAsync(() -> {
+            int chance = RANDOM.nextInt(0, 101);
+            school.setStudents(school.getStudents().stream()
+                    .peek(student -> {
+                        if (chance <= baseChance) {
+                            student.setPoints(student.getPoints() + task.getReward());
+                        }
+                    })
+                    .toList());
+
+            try {
+                Thread.sleep(time);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+            return school;
+        });
     }
 }
