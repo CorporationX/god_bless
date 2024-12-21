5197afdaea5ba706f42756eaaa532168c1ab27cb
ivans <ivan_semenov@bk.ru>
2024-12-21 20:03:10 +0300

    BJS2-48045 [Распределенная армия героев меча и магии]

---
+++ src/main/java/school/faang/moduleone/sprintthree/task_43550/Swordsman.java
@@ -0,0 +1,8 @@
+package school.faang.moduleone.sprintthree.task_43550;
+
+public class Swordsman extends Unit {
+
+    public Swordsman(int power) {
+        super(power);
+    }
+}
---
+++ src/main/java/school/faang/moduleone/sprintthree/task_43550/Unit.java
@@ -0,0 +1,10 @@
+package school.faang.moduleone.sprintthree.task_43550;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public abstract class Unit {
+    private int power;
+}
