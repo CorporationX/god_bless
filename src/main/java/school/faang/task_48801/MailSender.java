c4b617c75a37aa21fe980f613d84f1c39c4733b4
Alexkid <alexkid2001@rambler.ru>
2024-12-21 14:36:39 -0330

    task 48801 solution

---
+++ src/main/java/school/faang/task_48801/MailSender.java
@@ -0,0 +1,31 @@
+package school.faang.task_48801;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+public class MailSender {
+    public static void main(String[] args) {
+        int numberOfMails = 1000;
+        int numberOfThread = 5;
+        int mailsPerThread = numberOfMails / numberOfThread;
+
+        Thread[] threads = new Thread[numberOfThread];
+
+        for (int i = 0; i < numberOfThread; i ++) {
+            int startIndex = i * mailsPerThread + 1;
+            int endIndex = startIndex + mailsPerThread - 1;
+            threads[i] = new Thread(new SenderRunnable(startIndex, endIndex));
+            threads[i].start();
+        }
+
+        for (Thread thread : threads) {
+            try {
+                thread.join();
+            } catch (InterruptedException e) {
+                log.error(String.valueOf(e));
+            }
+        }
+
+        log.info("All mails sent successfully");
+    }
+}
