ddf2436ea23053fc3c1d03e2d8a00b041e3cf709
Ivan Baev <ibai86@yandex.ru>
2024-12-21 17:40:28 +0300

    ready to review

---
+++ src/main/java/school/faang/youtube_counter/DataSet.java
@@ -0,0 +1,9 @@
+package school.faang.youtube_counter;
+
+import lombok.experimental.UtilityClass;
+
+@UtilityClass
+public class DataSet {
+    public static final int NUM_THREADS = 10;
+    public static final int NUM_VIDEOS = 2;
+}
