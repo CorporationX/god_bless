b8851cd13fdfd854b69f71b996a386cb373a8176
ArzamaS Ilya <coldoff@inbox.ru>
2024-12-21 13:20:41 +0300

    Created classes and threads and locker.

---
+++ src/main/java/school/faang/task_49491/Music.java
@@ -0,0 +1,15 @@
+package school.faang.task_49491;
+
+public class Music {
+    public static void main(String[] args) {
+        Player player = new Player();
+        var play = new Thread(player::play);
+        var pause = new Thread(player::pause);
+        var skip = new Thread(player::skip);
+        var previous = new Thread(player::previous);
+        play.start();
+        pause.start();
+        skip.start();
+        previous.start();
+    }
+}
