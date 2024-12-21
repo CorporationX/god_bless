b9b81ba3e3ffeb6ae786d403c1478497a78abefe
Dmitriy <dda20040609@gmail.com>
2024-12-21 20:28:30 +0300

    commit 1 They were very nice but extremely poor

---
+++ src/main/java/school/faang/task_48531/WeasleyFamily.java
@@ -0,0 +1,21 @@
+package school.faang.task_48531;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+public class WeasleyFamily {
+    public static void main(String[] args) {
+        String[] chores = {"помыть посуду", "подмести пол", "приготовить ужин"};
+
+        ExecutorService threadPool = Executors.newCachedThreadPool();
+        for (String task : chores) {
+            Chore chore = new Chore(task);
+            threadPool.execute(chore);
+        }
+
+        threadPool.shutdown();
+    }
+}
+
+
+
