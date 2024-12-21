66b583da103a5efa740526f8f2b1649c99566f42
Alexey Gorokhov <almigor@mail.ru>
2024-12-21 14:27:56 +0300

    Асинхронность и Future

--- src/main/java/school/faang/sprint4/task50070/PaymentApp.java
+++ src/main/java/school/faang/sprint4/task50070/PaymentApp.java
@@ -2,7 +2,7 @@ package school.faang.sprint4.task50070;
 
 import school.faang.sprint4.task50070.service.MasterCardService;
 
-public class paymentApp {
+public class PaymentApp {
     public static void main(String[] args) {
         MasterCardService paymentService = new MasterCardService();
         paymentService.doAll();
