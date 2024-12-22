155ca148f1b5f0e2b4b3349e8b29be22f4776457
Anton Graf <ant0ngraf@yandex.ru>
2024-12-22 09:54:39 +0600

    Добавляет тест для запуска Music

--- src/test/java/school/faang/bjs_49249/MusicTest.java
+++ src/test/java/school/faang/bjs_49249/MusicTest.java
@@ -3,8 +3,6 @@ package school.faang.bjs_49249;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
-
 class MusicTest {
 
     @Test
