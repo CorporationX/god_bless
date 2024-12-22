b0278a7645a574703b7152b0f88313b52c7d51c7
Roman Shevchuk <reacount1393@gmail.com>
2024-12-22 04:02:29 +0300

    Чекстайл чет фигню начал вытворять

---
+++ src/main/java/school/faang/sprint4/task1/Main.java
@@ -0,0 +1,19 @@
+package school.faang.sprint4.task1;
+
+public class Main {
+    public static void main(String[] args) {
+        Player player = new Player();
+
+        Thread playThread = new Thread(player::play);
+        playThread.start();
+
+        Thread pauseThread = new Thread(player::pause);
+        pauseThread.start();
+
+        Thread skipThread = new Thread(player::skip);
+        skipThread.start();
+
+        Thread previousThread = new Thread(player::previous);
+        previousThread.start();
+    }
+}
