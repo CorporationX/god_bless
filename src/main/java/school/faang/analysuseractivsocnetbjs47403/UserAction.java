8141405c13fb2d98a788f813c86f0839df105cc1
Ilya <thomas.moody@mail.ru>
2024-12-22 12:53:07 +0300

    - убрал лишнюю строку - ActionType сделал enum - убрал магические числа в константы - вместо пробела написал "\s+" - В классе UserAction оставил только поля, а все методы вынес в отдельный класс

--- src/main/java/school/faang/analysuseractivsocnetbjs47403/UserAction.java
+++ src/main/java/school/faang/analysuseractivsocnetbjs47403/UserAction.java
@@ -14,63 +14,10 @@ import school.faang.userregistration.User;
 
 @AllArgsConstructor
 @Getter
-
 public class UserAction {
     private final int id;
     private final String name;
-    private final String actionType;
+    private final ActionType actionType;
     private final LocalDate actionDate;
-    private final String content;
+    private final String content;}
 
-    public static List<String> topActiveUsers(List<UserAction> actions) {
-        return actions.stream()
-                .collect(Collectors.groupingBy(UserAction::getName,
-                        Collectors.counting()))
-                .entrySet().stream()
-                .sorted(Map.Entry.<String, Long>comparingByValue()
-                        .reversed())
-                .limit(10)
-                .map(Map.Entry::getKey)
-                .collect(Collectors.toList());
-    }
-
-    public static List<String> topPopularHashtags(List<UserAction> actions) {
-        return actions.stream()
-                .flatMap(action -> Arrays.stream(action.getContent()
-                        .split(" ")))
-                .filter(word -> word.startsWith("#"))
-                .collect(Collectors.groupingBy(Function.identity(),
-                        Collectors.counting()))
-                .entrySet().stream()
-                .sorted(Map.Entry.<String, Long>comparingByValue().reversed())
-                .limit(5)
-                .map(Map.Entry::getKey)
-                .collect(Collectors.toList());
-    }
-
-    public static List<String> topCommentersLastMonth(List<UserAction> actions) {
-        LocalDate oneMonthAgo = LocalDate.now().minusMonths(1);
-        return actions.stream()
-                .filter(action -> action.getActionType().equals("comment")
-                        && action.getActionDate().isAfter(oneMonthAgo))
-                .collect(Collectors.groupingBy(UserAction::getName,
-                        Collectors.counting()))
-                .entrySet().stream()
-                .sorted(Map.Entry.<String, Long>comparingByValue().reversed())
-                .limit(3)
-                .map(Map.Entry::getKey)
-                .collect(Collectors.toList());
-    }
-
-    public static Map<String, Double> actionTypePercentages(List<UserAction> actions) {
-        long totalActions = actions.size();
-        return actions.stream()
-                .collect(Collectors.groupingBy(UserAction::getActionType,
-                        Collectors.counting()))
-                .entrySet().stream()
-                .collect(Collectors.toMap(
-                        Map.Entry::getKey,
-                        entry -> (entry.getValue() * 100.0) / totalActions
-                ));
-    }
-}
