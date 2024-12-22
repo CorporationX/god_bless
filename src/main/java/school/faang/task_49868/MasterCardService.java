4b6d3b463d02e83ccb9b31136859ac9565ec922d
triplek <weribel@mail.ru>
2024-12-22 22:44:58 +0300

    Асинхронность и Future (исправленно)

--- src/main/java/school/faang/task_49868/MasterCardService.java
+++ src/main/java/school/faang/task_49868/MasterCardService.java
@@ -3,10 +3,10 @@ package school.faang.task_49868;
 
 public class MasterCardService {
     static int collectPayment() {
-        final int COLLECTPAYMENT = 10000;
+        final int COLLECT = 10000;
         try {
-            Thread.sleep(COLLECTPAYMENT);
-            return COLLECTPAYMENT;
+            Thread.sleep(COLLECT);
+            return COLLECT;
         } catch (InterruptedException e) {
             e.printStackTrace();
             throw new RuntimeException();
@@ -14,10 +14,10 @@ public class MasterCardService {
     }
 
     static int sendAnalytics() {
-        final int SENDANALYTICS = 1000;
+        final int SEND = 1000;
         try {
-            Thread.sleep(SENDANALYTICS);
-            return SENDANALYTICS;
+            Thread.sleep(SEND);
+            return SEND;
         } catch (InterruptedException e) {
             e.printStackTrace();
             throw new RuntimeException();
