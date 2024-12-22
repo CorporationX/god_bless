616a4bcc665b56591cf16507790f686f76d45f0a
alexey_obraztsov <alexey.autotester86@gmail.com>
2024-12-22 11:29:33 +0300

    alexeyrnd-BJS2-48944. with wait() and notify() notify doesn't work. looks like iternal lock 2

--- src/main/java/school/faang/sprint_1/task_googlephotosyncbjs2n48944/GooglePhotosAutoUploader.java
+++ src/main/java/school/faang/sprint_1/task_googlephotosyncbjs2n48944/GooglePhotosAutoUploader.java
@@ -2,52 +2,44 @@ package school.faang.sprint_1.task_googlephotosyncbjs2n48944;
 
 import lombok.NonNull;
 import lombok.extern.slf4j.Slf4j;
-import java.time.LocalDateTime;
-import java.time.format.DateTimeFormatter;
+
 import java.util.List;
 
 @Slf4j
 public class GooglePhotosAutoUploader {
     private final Object lock = new Object();
     private final List<String> photosToUpload;
+    private long startTime;
 
     public GooglePhotosAutoUploader(@NonNull List<String> photosToUpload) {
         this.photosToUpload = photosToUpload;
+        startTime = System.currentTimeMillis();
     }
-
     public synchronized void startAutoUpload() {
         synchronized (lock) {
-            long startTime = System.currentTimeMillis();
             do {
-                while (photosToUpload.isEmpty()) {
+                if (photosToUpload.isEmpty()) {
                     try {
                         log.info("Waiting for new photos to come");
                         lock.wait();
                     } catch (InterruptedException e) {
                         e.printStackTrace();
                     }
+                } else {
+                    uploadPhotos();
                 }
-                uploadPhotos();
-            } while (System.currentTimeMillis() - startTime <= 10_000);
+            } while (System.currentTimeMillis() - startTime < 10_000);
         }
     }
-
     public synchronized void onNewPhotoAdded(String photoPath) {
         synchronized (lock) {
-            try {
-                Thread.sleep(1000);
-            } catch (InterruptedException e) {
-                throw new RuntimeException(e);
-            }
+            log.info("adding new photo ....");
             photosToUpload.add(photoPath);
             lock.notify();
         }
     }
-
     private void uploadPhotos() {
-        for(String photo: photosToUpload) {
-            log.info("{} is uploaded to server", photo);
-        }
+        for (String photo : photosToUpload) {log.info("{} is uploaded to server", photo);}
         photosToUpload.clear();
     }
 }
@@ -74,4 +66,10 @@ public class GooglePhotosAutoUploader {
 //    onNewPhotoAdded("Photo_" + LocalDateTime.now()
 //            .format(DateTimeFormatter.ofPattern("mm_ss.SSS")));
 //    log.info("new photo added");
-}
+
+//   try {
+//                Thread.sleep(1000);
+//            } catch (InterruptedException e) {
+//                throw new RuntimeException(e);
+//            }
+
--- src/main/java/school/faang/sprint_1/task_googlephotosyncbjs2n48944/UploaderMain.java
+++ src/main/java/school/faang/sprint_1/task_googlephotosyncbjs2n48944/UploaderMain.java
@@ -1,42 +1,28 @@
 package school.faang.sprint_1.task_googlephotosyncbjs2n48944;
 
-import lombok.SneakyThrows;
-
 import java.util.ArrayList;
 import java.util.List;
 
 public class UploaderMain {
-    @SneakyThrows
+
     public static void main(String[] args) {
         List<String> pathsToPhotos = new ArrayList<>(List.of(
                 "C:\\Photos\\Photo1.png",
                 "C:\\Photos\\Photo2.png",
-                "C:\\Photos\\Photo3.png",
-                "C:\\Photos\\Photo4.png",
-                "C:\\Photos\\Photo5.png",
-                "C:\\Photos\\Photo6.png"
+                "C:\\Photos\\Photo3.png"
         ));
         GooglePhotosAutoUploader googlePhotosAutoUploader = new GooglePhotosAutoUploader(pathsToPhotos);
-        Runnable addNewPhotoToUploader = () -> {
+        Thread uploadThread = new Thread(() -> googlePhotosAutoUploader.startAutoUpload());
+        Thread photoAdderThread = new Thread(() -> {
             googlePhotosAutoUploader.onNewPhotoAdded("Photo_001");
             googlePhotosAutoUploader.onNewPhotoAdded("Photo_002");
             googlePhotosAutoUploader.onNewPhotoAdded("Photo_003");
             googlePhotosAutoUploader.onNewPhotoAdded("Photo_004");
-        };
-        Runnable autoUploadToServer = () -> googlePhotosAutoUploader.startAutoUpload();
-
-
-//        ScheduledExecutorService scheduledExecutor = Executors.newScheduledThreadPool(2);
-//        scheduledExecutor.scheduleAtFixedRate(autoUploadToServer,0, 1, TimeUnit.SECONDS);
-//        scheduledExecutor.scheduleAtFixedRate(addNewPhotoToUploader,0, 3, TimeUnit.SECONDS);
-
-
-        Thread uploadThread = new Thread(autoUploadToServer);
-        Thread photoAdderThread = new Thread(addNewPhotoToUploader);
+        });
         uploadThread.start();
         photoAdderThread.start();
-//        new Thread(addNewPhotoToUploader).start();
-//        new Thread(autoUploadToServer).start();
-
     }
 }
+
+//Runnable autoUploadToServer = () -> googlePhotosAutoUploader.startAutoUpload();
+//Runnable addNewPhotoToUploader =
