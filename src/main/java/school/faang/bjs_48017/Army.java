6c581c08fe6a1e606cab51f13de829ec35478075
Anton Graf <ant0ngraf@yandex.ru>
2024-12-22 08:29:37 +0600

    Исправляет ошибки

--- src/main/java/school/faang/bjs_48017/Army.java
+++ src/main/java/school/faang/bjs_48017/Army.java
@@ -1,8 +1,11 @@
 package school.faang.bjs_48017;
 
+import lombok.extern.slf4j.Slf4j;
+
 import java.util.ArrayList;
 import java.util.List;
 
+@Slf4j
 public class Army {
     private final List<Character> characters = new ArrayList<>();
 
@@ -21,7 +24,7 @@ public class Army {
             try {
                 thread.join();
             } catch (InterruptedException e) {
-                throw new RuntimeException(e);
+                log.error("Thread was interrupted", e);
             }
         });
 
