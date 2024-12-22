02394ee79061c3ce4124c0e226045a2e81afbc74
Parvin Etibarli <etibarliparvin@gmail.com>
2024-12-22 15:21:20 +0400

    bjs2-49591
    * corrected as per comment

--- src/main/java/school/faang/bjs249591/VideoManager.java
+++ src/main/java/school/faang/bjs249591/VideoManager.java
@@ -10,23 +10,19 @@ public class VideoManager {
     @Getter
     private Map<String, Integer> viewsMap = new HashMap<>();
 
-    public void addView(String videoId) {
-        synchronized (this) {
-            System.out.printf("Video with : %s is uploading...\n", videoId);
-            try {
-                Thread.sleep(2000);
-            } catch (Exception e) {
-                e.printStackTrace();
-            }
-            viewsMap.compute(videoId, (key, value) -> (value == null) ? 1 : value + 1);
-            System.out.printf("Video with : %s uploaded\n", videoId);
+    public synchronized void addView(String videoId) {
+        System.out.printf("Video with : %s is uploading...\n", videoId);
+        try {
+            Thread.sleep(2000);
+        } catch (Exception e) {
+            e.printStackTrace();
         }
+        viewsMap.compute(videoId, (key, value) -> (value == null) ? 1 : value + 1);
+        System.out.printf("Video with : %s uploaded\n", videoId);
     }
 
-    public Integer getViewCount(String videoId) {
-        synchronized (this) {
-            return viewsMap.getOrDefault(videoId, 0);
-        }
+    public synchronized Integer getViewCount(String videoId) {
+        return viewsMap.getOrDefault(videoId, 0);
     }
 
 }
