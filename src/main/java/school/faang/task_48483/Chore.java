20fd8df278f745fab9c1b969e1d334079f760650
Ilya Vysokolyan <rd2d@list.ru>
2024-12-22 14:45:01 +0300

    BJS2-48483: Поправил замечания

--- src/main/java/school/faang/task_48483/Chore.java
+++ src/main/java/school/faang/task_48483/Chore.java
@@ -2,10 +2,15 @@ package school.faang.task_48483;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.extern.slf4j.Slf4j;
 
 @Getter
 @AllArgsConstructor
+@Slf4j
 public class Chore implements Runnable {
+
+    private static final int SLEEP_TIME = 1000;
+
 
     private String chore;
 
@@ -13,10 +18,10 @@ public class Chore implements Runnable {
     public void run() {
         try {
             System.out.printf("Поток: %s\n Выполняет задачу: %s ", Thread.currentThread().getName(), chore);
-            Thread.sleep(1000);
+            Thread.sleep(SLEEP_TIME);
             System.out.println(chore + " - задача выполнена!");
         } catch (InterruptedException e) {
-            throw new RuntimeException(e);
+            log.info("Ошибка при выполнении задачи: " + e.getMessage());
         }
     }
 }
