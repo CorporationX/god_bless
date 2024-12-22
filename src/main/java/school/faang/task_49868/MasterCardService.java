1266d16b31d435d5f79febd6bedfcd1fe99f5151
triplek <weribel@mail.ru>
2024-12-22 14:20:47 +0300

    Асинхронность и Future

---
+++ src/main/java/school/faang/task_49868/MasterCardService.java
@@ -0,0 +1,24 @@
+package school.faang.task_49868;
+
+public class MasterCardService {
+    static int collectPayment() {
+        try {
+            Thread.sleep(10_000);
+            return 10_000;
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+            throw new RuntimeException();
+        }
+    }
+
+    static int sendAnalytics() {
+        try {
+            Thread.sleep(1_000);
+            return 1_000;
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+            throw new RuntimeException();
+        }
+    }
+
+}
