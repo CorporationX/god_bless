f2a834c19b6b18ff55b147eb5f31ebb4cce9b04f
Dias Kamza <kamzadias31@gmail.com>
2024-12-23 17:11:28 +0500

    Created Chore class and usage in WeasleyFamily

---
+++ src/main/java/school/faang/task_48729/WeasleyFamily.java
@@ -0,0 +1,25 @@
+package school.faang.task_48729;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+public class WeasleyFamily {
+    public static void main(String[] args) {
+        String[] chores = {
+                "помыть посуду",
+                "подмести пол",
+                "приготовить ужин",
+                "вытереть пыль",
+                "выгулить собаку"
+        };
+
+        ExecutorService executorService = Executors.newCachedThreadPool();
+
+        for(String chore : chores) {
+            Chore task = new Chore(chore);
+            executorService.execute(task);
+        }
+
+        executorService.shutdown();
+    }
+}
