685aed2d75c81999c50966823469135d82873b8f
tatgricenko <tattgricenko@gmail.com>
2024-12-23 13:10:36 +0300

    Add const

--- src/main/java/school/faang/sprint_3/task_48514/Chore.java
+++ src/main/java/school/faang/sprint_3/task_48514/Chore.java
@@ -1,6 +1,8 @@
 package school.faang.sprint_3.task_48514;
 
 public class Chore implements Runnable {
+    private final int SLEEP_MILLISECONDS = 2000;
+
     private final String chore;
 
     public Chore(String chore) {
@@ -12,7 +14,7 @@ public class Chore implements Runnable {
         String threadName = Thread.currentThread().getName();
         try {
             System.out.println("Поток " + threadName + " выполняет задачу: " + chore);
-            Thread.sleep(2000);
+            Thread.sleep(SLEEP_MILLISECONDS);
             System.out.println("Поток " + threadName + " закончил задачу: " + chore);
         } catch (InterruptedException e) {
             System.out.println("У потока " + threadName + " была прервана задача " + chore);
