4b921bb300855486b3cd18562a7e5976925b35e7
Daria <dashok.sok@gmail.com>
2024-12-23 12:20:17 +0300

    Добавлен класс Player, который управляет воспроизведением музыки

---
+++ src/main/java/school/faang/spotifyblocking/Music.java
@@ -0,0 +1,16 @@
+package school.faang.spotifyblocking;
+
+public class Music {
+    public static void main(String[] args) {
+        Player player = new Player();
+
+        Thread playThread = new Thread(player::play);
+        playThread.start();
+        Thread pauseThread = new Thread(player::pause);
+        pauseThread.start();
+        Thread skipThread = new Thread(player::skip);
+        skipThread.start();
+        Thread previousThread = new Thread(player::previous);
+        previousThread.start();
+    }
+}
