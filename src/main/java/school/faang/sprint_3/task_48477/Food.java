65c98688d302eb305514312bcd25a702307d5210
Aleksandr Kozlov <vismut-888@yandex.ru>
2024-12-23 14:06:59 +0300

    BJS2-48477: Mice are very nice!

---
+++ src/main/java/school/faang/sprint_3/task_48477/Food.java
@@ -0,0 +1,12 @@
+package school.faang.sprint_3.task_48477;
+
+import lombok.Getter;
+
+@Getter
+public class Food {
+    private String name;
+
+    public Food(String name) {
+        this.name = name;
+    }
+}
---
+++ src/main/java/school/faang/sprint_3/task_48477/Main.java
@@ -0,0 +1,76 @@
+package school.faang.sprint_3.task_48477;
+
+import lombok.extern.slf4j.Slf4j;
+
+import java.util.List;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
+
+@Slf4j
+public class Main {
+    private static final int THREAD_POOL_SIZE = 5;
+    private static final int DELAY = 0;
+    private static final int PERIOD = 30;
+
+    public static void main(String[] args) {
+
+        List<Food> foods1 = List.of(
+                new Food("bacon"),
+                new Food("cookies"),
+                new Food("pelmeni"),
+                new Food("borsch")
+        );
+
+        List<Food> foods2 = List.of(
+                new Food("bacon"),
+                new Food("chips"),
+                new Food("snacks")
+        );
+
+        List<Food> foods3 = List.of(
+                new Food("snacks"),
+                new Food("cookies"),
+                new Food("nuts")
+        );
+
+        List<Food> foods4 = List.of(
+                new Food("nuts")
+        );
+
+        House house = new House("Tom House");
+
+        Room kitchen = new Room("Kitchen");
+        Room livingRoom = new Room("Living room");
+        Room bedroom = new Room("Bedroom");
+        Room bathroom = new Room("Bathroom");
+
+        kitchen.addFood(foods1);
+        livingRoom.addFood(foods2);
+        bedroom.addFood(foods3);
+        bathroom.addFood(foods4);
+
+        house.addRoom(kitchen);
+        house.addRoom(livingRoom);
+        house.addRoom(bedroom);
+        house.addRoom(bathroom);
+
+        CountDownLatch count = new CountDownLatch(house.sizeHouse());
+        house.setCount(count);
+
+        try {
+            ScheduledExecutorService executor = Executors.newScheduledThreadPool(THREAD_POOL_SIZE);
+            for (int i = 0; i < THREAD_POOL_SIZE; i++) {
+                executor.scheduleAtFixedRate(house::collectFood, DELAY, PERIOD, TimeUnit.SECONDS);
+            }
+
+            count.await();
+            executor.shutdown();
+            System.out.println("All the food in the house is collected!");
+
+        } catch (Exception e) {
+            log.error(e.getMessage());
+        }
+    }
+}
