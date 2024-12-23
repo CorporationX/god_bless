aa127d2918cccbfd825ca3d8e0180d46e845019b
AesaQ <ilyaovchinnikov2095@gmail.com>
2024-12-23 16:40:36 +0500

    refactoring

--- src/main/java/school/faang/viewscounter_youtube/VideoManager.java
+++ src/main/java/school/faang/viewscounter_youtube/VideoManager.java
@@ -2,35 +2,27 @@ package school.faang.viewscounter_youtube;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
 
 public class VideoManager {
-    private final Map<String, Integer> viewsMap = new HashMap<>();
-    private final Object viewsMapLock = new Object();
+    private final ConcurrentHashMap<String, Integer> viewsMap = new ConcurrentHashMap<>();
 
     public void addView(String video) {
-        synchronized (viewsMapLock) {
-            if (!viewsMap.containsKey(video)) {
-                viewsMap.put(video, 0);
-            }
-            viewsMap.put(video, viewsMap.get(video) + 1);
+        if (!viewsMap.containsKey(video)) {
+            viewsMap.put(video, 0);
         }
+        viewsMap.put(video, viewsMap.get(video) + 1);
     }
 
     public int getViewCount(String video) {
-        synchronized (viewsMapLock) {
-            return viewsMap.get(video);
-        }
+        return viewsMap.get(video);
     }
 
     public void addNewVideo(String video) {
-        synchronized (viewsMapLock) {
-            viewsMap.put(video, 0);
-        }
+        viewsMap.put(video, 0);
     }
 
     public Map<String, Integer> getViewsMap() {
-        synchronized (viewsMapLock) {
-            return viewsMap;
-        }
+        return viewsMap;
     }
 }
