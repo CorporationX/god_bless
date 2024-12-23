852d57f7438b57f822fc3d8bf51f3cebfba63d41
Ivan Baev <ibai86@yandex.ru>
2024-12-24 02:41:37 +0300

    ready to review

--- src/main/java/school/faang/users_activity/UserActionAnalyzer.java
+++ src/main/java/school/faang/users_activity/UserActionAnalyzer.java
@@ -7,7 +7,7 @@ import java.util.Map;
 import java.util.stream.Collectors;
 
 public class UserActionAnalyzer {
-    public static List<String> getTopTenActiveUsers(List<UserAction> actions) {
+    public static List<String> topTenActiveUsers(List<UserAction> actions) {
         return actions.stream()
                 .collect(Collectors.groupingBy(UserAction::getName, Collectors.counting()))
                 .entrySet().stream()
@@ -17,10 +17,11 @@ public class UserActionAnalyzer {
                 .toList();
     }
 
-    public static List<String> getTopFivePopularHashtags(List<UserAction> actions) {
+    public static List<String> topFivePopularHashtags(List<UserAction> actions) {
         return actions.stream()
                 .filter(act -> !act.getActionType().isEmpty()
-                        && (act.getActionType().equals("post") || act.getActionType().equals("comment")))
+                        || act.getActionType().equals("post")
+                        || act.getActionType().equals("comment"))
                 .flatMap(act -> Arrays.stream(act.getContent().split("[,.! ]")))
                 .filter(str -> str.startsWith("#"))
                 .collect(Collectors.groupingBy(tag -> tag, Collectors.counting()))
@@ -31,26 +32,15 @@ public class UserActionAnalyzer {
                 .toList();
     }
 
-    public static List<String> getTopThreeCommentersLastMonth(List<UserAction> actions) {
-        LocalDate threshold = LocalDate.now().minusMonths(1);
+    public static List<String> topThreeCommentersLastMonth(List<UserAction> actions) {
         return actions.stream()
                 .filter(act -> act.getActionType().equals("comment")
-                        && act.getActionDate().isAfter(threshold))
-                .collect(Collectors.groupingBy(act -> act.getName(), Collectors.counting()))
+                        && act.getActionDate().isAfter(LocalDate.now().minusMonths(1)))
+                .collect(Collectors.groupingBy(user -> user.getName(), Collectors.counting()))
                 .entrySet().stream()
                 .sorted(Map.Entry.<String, Long>comparingByValue().reversed())
                 .limit(3)
                 .map(Map.Entry::getKey)
                 .toList();
-    }
-
-    public static Map<String, Double> getActionTypePercentages(List<UserAction> actions) {
-        return actions.stream()
-                .collect(Collectors.groupingBy(UserAction::getActionType, Collectors.counting()))
-                .entrySet().stream()
-                .collect(Collectors.toMap(
-                        Map.Entry::getKey,
-                        type -> Math.round(type.getValue() * 10e3 / actions.size()) / 100.0
-                ));
     }
 }
