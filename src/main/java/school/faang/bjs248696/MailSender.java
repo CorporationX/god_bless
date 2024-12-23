34be71923c017d04852ff1ca67ccabc2fe369cd0
Artur Suleymanov <suleymanov.usa@gmail.com>
2024-12-22 17:17:27 -0800

    Implement parallel mail sending using multiple threads for faster processing

---
+++ src/main/java/school/faang/bjs248696/MailSender.java
@@ -0,0 +1,22 @@
+package school.faang.bjs248696;
+
+public class MailSender {
+
+    public static void main(String[] args) throws InterruptedException {
+        int mails = 1000;
+        int mailGroups = 5;
+        int mailsInOneGroup = mails / mailGroups;
+
+        Thread[] threads = new Thread[mailGroups];
+        for (int i = 0; i < mailGroups; i++) {
+            int start = i * mailsInOneGroup;
+            int end = (i + 1) * mailsInOneGroup;
+            threads[i] = new Thread(new SenderRunnable(start, end));
+            threads[i].start();
+        }
+        for (Thread thread:threads){
+            thread.join();
+        }
+
+    }
+}
