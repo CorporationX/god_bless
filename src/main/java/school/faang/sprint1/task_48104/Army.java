2f3e9f28f4a5c184bdd57709f811cb4cc85913a2
Pavel Schelkin <rhanan1233@gmail.com>
2024-12-23 22:20:57 +0300

    Распределенная армия героев меча и магии

--- src/main/java/school/faang/sprint1/task_48104/Army.java
+++ src/main/java/school/faang/sprint1/task_48104/Army.java
@@ -4,30 +4,34 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Army {
-    List<Unit> units = new ArrayList<>();
+    private List<Unit> units = new ArrayList<>();
 
     public void addUnit(Unit unit) {
         units.add(unit);
     }
 
-    int totalPower = 0;
+    private int totalPower = 0;
 
     public int calculateTotalPower() throws InterruptedException {
         List<Thread> threads = new ArrayList<>();
         List<PowerCalculator> tasks = new ArrayList<>();
+        try {
 
-        for (Unit unit : units) {
-            PowerCalculator calculator = new PowerCalculator(unit);
-            tasks.add(calculator);
-            Thread thread = new Thread(calculator);
-            threads.add(thread);
-            thread.start();
-        }
-        for (Thread thread : threads) {
-            thread.join();
-        }
-        for (PowerCalculator task : tasks) {
-            totalPower += task.getPower();
+            for (Unit unit : units) {
+                PowerCalculator calculator = new PowerCalculator(unit);
+                tasks.add(calculator);
+                Thread thread = new Thread(calculator);
+                threads.add(thread);
+                thread.start();
+            }
+            for (Thread thread : threads) {
+                thread.join();
+            }
+            for (PowerCalculator task : tasks) {
+                totalPower += task.getPower();
+            }
+        } catch (InterruptedException e) {
+            System.out.println("поток прирвался");
         }
         return totalPower;
     }
