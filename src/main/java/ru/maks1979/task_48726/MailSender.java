6c81362b67736d86f70bda15bf458fe3882933ab
sanctusgermanus72362 <sanctus-germanus7@yandex.ru>
2024-12-23 22:46:34 +0300

    Stream api2

--- src/main/java/ru/maks1979/task_48726/MailSender.java
+++ src/main/java/ru/maks1979/task_48726/MailSender.java
@@ -3,19 +3,18 @@ package ru.maks1979.task_48726;
 public class MailSender {
     public static void main(String[] args) {
         SenderRunnable firstSender = new SenderRunnable(1, 200);
-        SenderRunnable secondSender = new SenderRunnable(201, 400);
-        SenderRunnable thirdSender = new SenderRunnable(401, 600);
-        SenderRunnable fourthSender = new SenderRunnable(601, 800);
-        SenderRunnable fifthSender = new SenderRunnable(801, 1000);
-
         Thread thread1 = new Thread(firstSender);
         thread1.start();
+        SenderRunnable secondSender = new SenderRunnable(201, 400);
         Thread thread2 = new Thread(secondSender);
         thread2.start();
+        SenderRunnable thirdSender = new SenderRunnable(401, 600);
         Thread thread3 = new Thread(thirdSender);
         thread3.start();
+        SenderRunnable fourthSender = new SenderRunnable(601, 800);
         Thread thread4 = new Thread(fourthSender);
         thread4.start();
+        SenderRunnable fifthSender = new SenderRunnable(801, 1000);
         Thread thread5 = new Thread(fifthSender);
         thread5.start();
         try {
