ce9f37c208aa0fa95f8d0b2a18d53038462d2e17
Aleksandr Kozlov <vismut-888@yandex.ru>
2024-12-23 17:25:29 +0300

    Исправил checkstyle ошибку

--- src/main/java/school/faang/sprint_3/task_49288/Music.java
+++ src/main/java/school/faang/sprint_3/task_49288/Music.java
@@ -5,13 +5,15 @@ public class Music {
         Player player = new Player();
 
         Thread thread1 = new Thread(player::play);
-        Thread thread2 = new Thread(player::pause);
-        Thread thread3 = new Thread(player::skip);
-        Thread thread4 = new Thread(player::previous);
-
         thread1.start();
+
+        Thread thread2 = new Thread(player::pause);
         thread2.start();
+
+        Thread thread3 = new Thread(player::skip);
         thread3.start();
+
+        Thread thread4 = new Thread(player::previous);
         thread4.start();
     }
 }
