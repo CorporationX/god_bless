7292c7ecb1458ccfbf590f860c0ad0ff32828188
Timofey Khutoryanskiy <tiroxxxin@gmail.com>
2024-12-23 12:11:34 +0300

    tiroxin BJS2_48443

---
+++ src/main/java/school/faang/sprint_3/task_48443/MailSender.java
@@ -0,0 +1,7 @@
+package school.faang.sprint_3.task_48443;
+
+public class MailSender {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/main/java/school/faang/sprint_3/task_48443/SenderRunnable.java
@@ -0,0 +1,9 @@
+package school.faang.sprint_3.task_48443;
+
+public class SenderRunnable implements Runnable {
+
+    @Override
+    public void run(int startIndex, int endIndex) {
+
+    }
+}
