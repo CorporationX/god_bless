da4ce9ef12431f1a97e0edcd215dbcd5f89d4798
tatgricenko <tattgricenko@gmail.com>
2024-12-23 13:42:52 +0300

    Fix checkstyle error

--- src/main/java/school/faang/sprint_3/task_48027/Main.java
+++ src/main/java/school/faang/sprint_3/task_48027/Main.java
@@ -4,7 +4,7 @@ import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
-        List<Unit> unitList= List.of(
+        List<Unit> unitList = List.of(
                 new Archer(25), new Swordsman(40), new Mage(50),
                 new Mage(20), new Archer(25), new Swordsman(40),
                 new Mage(50), new Archer(25), new Swordsman(40)
