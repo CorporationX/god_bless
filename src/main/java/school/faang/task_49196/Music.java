2ed6a8be6f160bcbb35f07a0fe9c2779e9950dd4
Leysan Pirkina <missleysan1609@gmail.com>
2024-12-23 09:38:59 +0300

    задача spotify fixed

--- src/main/java/school/faang/task_49196/Music.java
+++ src/main/java/school/faang/task_49196/Music.java
@@ -1,5 +1,7 @@
 package school.faang.task_49196;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
@@ -7,13 +9,16 @@ public class Music {
     public static void main(String[] args) {
         Player player = new Player(true);
 
-        ExecutorService executorService = Executors.newFixedThreadPool(4);
+        List<Runnable> playerList = new ArrayList<>();
+        playerList.add(player::play);
+        playerList.add(player::pause);
+        playerList.add(player::skip);
+        playerList.add(player::previous);
 
-        executorService.submit(player::play);
-        executorService.submit(player::pause);
-        executorService.submit(player::skip);
-        executorService.submit(player::previous);
+        ExecutorService executorService = Executors.newFixedThreadPool(4);
+        playerList.forEach(executorService::submit);
 
         executorService.shutdown();
+
     }
 }
