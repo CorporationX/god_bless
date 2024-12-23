fb9345a52a08903103b276a39801c33003505e52
lehaps <awda.96@bk.ru>
2024-12-23 19:16:52 +0300

    Исправил замечания

--- src/main/java/school/faang/task50610/Kingdom.java
+++ src/main/java/school/faang/task50610/Kingdom.java
@@ -4,10 +4,12 @@ import lombok.extern.slf4j.Slf4j;
 
 import java.util.Objects;
 import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ThreadLocalRandom;
 
 @Slf4j
 public record Kingdom(String name) {
     private static final String SUCCESS_MESSAGE = "Сообщение успешно доставлено";
+    private static final double FAILURE_PROBABILITY = 0.4;
 
     private void sendMessage(Kingdom receiver) {
         log.info("{} отправил сообщение {}", this, receiver);
@@ -15,8 +17,8 @@ public record Kingdom(String name) {
 
     public CompletableFuture<String> sendRaven(Kingdom sender, Kingdom receiver) {
         return CompletableFuture.runAsync(() -> {
-            if (Math.random() < 0.4) {
-                throw new IllegalArgumentException("sender не смог отправить сообщение");
+            if (ThreadLocalRandom.current().nextDouble() < FAILURE_PROBABILITY) {
+                throw new RavenDeliveryException("sender не смог отправить сообщение");
             }
             sender.sendMessage(receiver);
         }).handle((result, ex) -> {
