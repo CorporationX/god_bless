d878773bd652381273d19d0bd032c1145155bf48
Pavel Schelkin <rhanan1233@gmail.com>
2024-12-23 18:32:06 +0300

    They were very nice but extremely poor

---
+++ src/main/java/school/faang/sprint1/task_48717/Chore.java
@@ -0,0 +1,22 @@
+package school.faang.sprint1.task_48717;
+
+public class Chore implements Runnable {
+    private String chore;
+
+    public Chore(String chore) {
+        this.chore = chore;
+    }
+
+    @Override
+    public void run() {
+        try {
+            System.out.println(Thread.currentThread().getName() + " Событие");
+            Thread.sleep(5000);
+            System.out.println(chore + " Ready");
+        }
+       catch (InterruptedException e) {
+           System.out.println("Задача " + chore + " прервана");
+       }
+
+    }
+}
---
+++ src/main/java/school/faang/sprint1/task_48717/WeasleyFamily.java
@@ -0,0 +1,26 @@
+package school.faang.sprint1.task_48717;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+public class WeasleyFamily {
+    public static void main(String[] args) {
+        ExecutorService threadPool = Executors.newCachedThreadPool();
+        String[] chores = {"помыть посуду",
+                "подмести пол",
+                "приготовить ужин",
+                "вытереть пыль",
+                "постирать бельё"};
+        for (String task : chores) {
+            Chore chore = new Chore(task);
+            threadPool.submit(chore);
+        }
+        try {
+            Thread.sleep(5000);
+        }catch (InterruptedException e) {
+            System.out.println("Поток прерван");
+        }
+        threadPool.shutdown();
+    }
+
+}
