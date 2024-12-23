ce4d8e53d9a149e44bba1fa50790d4fd0ca10c4a
Eugene <@evgeniy_kim>
2024-12-24 00:17:40 +0500

    update

--- src/main/java/school/faang/task_49971/QuestSystem.java
+++ src/main/java/school/faang/task_49971/QuestSystem.java
@@ -1,17 +1,22 @@
 package school.faang.task_49971;
 
+import lombok.extern.slf4j.Slf4j;
+
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.ExecutorService;
 
+@Slf4j
 public class QuestSystem {
+    private static final long MILLIS = 1000L;
+
     CompletableFuture<Player> startQuest(Player player,
                                          Quest quest,
                                          ExecutorService executor) {
         return CompletableFuture.supplyAsync(() -> {
             try {
-                Thread.sleep(quest.getDifficulty() + 1000);
+                Thread.sleep(quest.getDifficulty() * MILLIS);
             } catch (InterruptedException e) {
-                throw new RuntimeException(e.getMessage());
+                log.error(e.getMessage());
             }
             player.addExperience(quest.getReward());
             return player;
