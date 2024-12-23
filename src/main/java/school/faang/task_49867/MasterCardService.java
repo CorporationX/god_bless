dbc73c0fee885bf08c4c57fac2dc6d5bd8695d6e
Eugene <@evgeniy_kim>
2024-12-24 00:20:47 +0500

    update

--- src/main/java/school/faang/task_49867/MasterCardService.java
+++ src/main/java/school/faang/task_49867/MasterCardService.java
@@ -1,9 +1,11 @@
 package school.faang.task_49867;
 
 public class MasterCardService {
+    private static final int PAYMENT_SLEEP_DURATION = 1000;
+    private static final int ANALYTICS_SLEEP_DURATION = 10000;
     public int collectPayment() {
         try {
-            Thread.sleep(1000);
+            Thread.sleep(PAYMENT_SLEEP_DURATION);
             return 1000;
         } catch (InterruptedException e) {
             throw new RuntimeException(e.getMessage());
@@ -12,7 +14,7 @@ public class MasterCardService {
 
     public int sendAnalytics() {
         try {
-            Thread.sleep(10000);
+            Thread.sleep(ANALYTICS_SLEEP_DURATION);
             return 10000;
         } catch (InterruptedException e) {
             throw new RuntimeException(e.getMessage());
