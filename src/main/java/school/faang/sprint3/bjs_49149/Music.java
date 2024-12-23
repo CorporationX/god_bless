c4da1e56e1156d60c8df56d2e137ccbec4423986
TFdevQA <46944859+TFdevQA@users.noreply.github.com>
2024-12-22 19:07:47 -0800

    task 49149: checkstyle again

--- src/main/java/school/faang/sprint3/bjs_49149/Music.java
+++ src/main/java/school/faang/sprint3/bjs_49149/Music.java
@@ -16,21 +16,22 @@ public class Music {
         Random random = new Random();
         Player myPlayer = new Player();
         ExecutorService executor = Executors.newCachedThreadPool();
+
         for (int i = 0; i < totalActions; i++) {
             int action = random.nextInt(4);
-            switch (action) {
-                case 0:
-                    executor.submit(myPlayer::play);
-                    break;
-                case 1:
-                    executor.submit(myPlayer::pause);
-                    break;
-                case 2:
-                    executor.submit(myPlayer::skip);
-                    break;
-                default :
-                    executor.submit(myPlayer::previous);
-                    break;
+            switch(action) {
+            case 0:
+                executor.submit(myPlayer::play);
+                break;
+            case 1:
+                executor.submit(myPlayer::pause);
+                break;
+            case 2:
+                executor.submit(myPlayer::skip);
+                break;
+            default:
+                executor.submit(myPlayer::previous);
+                break;
             }
         }
         executor.shutdown();
