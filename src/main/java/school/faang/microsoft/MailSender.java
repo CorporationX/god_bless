c7d8e01b0243258f30cf9d3f491621c9f8d874c4
Azamat Beisembay <azamat.beisembay@gmail.com>
2024-12-24 13:55:50 +0500

    azamat71-BJS2-48760 Вы работаете в Microsoft!

--- src/main/java/school/faang/microsoft/MailSender.java
+++ src/main/java/school/faang/microsoft/MailSender.java
@@ -2,17 +2,17 @@ package school.faang.microsoft;
 
 public class MailSender {
     public static void main(String[] args) {
-        System.out.println("Начало отправки");
         int threadsCount = 5;
         int messagesCount = 1000;
         int startIndex = 1;
-        int endIndex = messagesCount/threadsCount;
+        int endIndex = messagesCount / threadsCount;
         Thread[] threads = new Thread[threadsCount];
 
+        System.out.println("Начало отправки");
         for (Thread thread : threads) {
             SenderRunnable senderRunnable = new SenderRunnable(startIndex, endIndex);
             startIndex = endIndex + 1;
-            endIndex = endIndex + messagesCount/threadsCount;
+            endIndex = endIndex + messagesCount / threadsCount;
             thread = new Thread(senderRunnable);
             thread.start();
         }
