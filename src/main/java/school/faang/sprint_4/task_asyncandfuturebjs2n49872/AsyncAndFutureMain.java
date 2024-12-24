34aa9d8f34bbbd9636d4c45fdc07a6ccae9fbe16
alexey_obraztsov <alexey.autotester86@gmail.com>
2024-12-24 14:08:41 +0300

    alexeyrnd-BJS2-49872. Async and Future. initial

---
+++ src/main/java/school/faang/sprint_4/task_asyncandfuturebjs2n49872/AsyncAndFutureMain.java
@@ -0,0 +1,8 @@
+package school.faang.sprint_4.task_asyncandfuturebjs2n49872;
+
+public class AsyncAndFutureMain {
+    public static void main(String[] args) {
+        MasterCardService service = new MasterCardService();
+        service.doAll();
+    }
+}
---
+++ src/main/java/school/faang/sprint_4/task_asyncandfuturebjs2n49872/MasterCardService.java
@@ -0,0 +1,27 @@
+package school.faang.sprint_4.task_asyncandfuturebjs2n49872;
+
+public class MasterCardService {
+    public int collectPayment() {
+        try {
+            Thread.sleep(10_000);
+            return 10_000;
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+            throw new RuntimeException();
+        }
+    }
+
+    public int sendAnalytics() {
+        try {
+            Thread.sleep(1_000);
+            return 1_000;
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+            throw new RuntimeException();
+        }
+    }
+
+    public void doAll() {
+
+    }
+}
