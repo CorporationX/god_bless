6cfe181c39731792cb0387e0ddb76094e2bf6571
therealadik <vladerm2000@yandex.ru>
2024-12-25 01:59:27 +0500

    Carry отожрался

---
+++ src/main/java/school/faang/task_50724/Item.java
@@ -0,0 +1,6 @@
+package school.faang.task_50724;
+
+public record Item(String name,
+                   int power
+) {
+}
---
+++ src/main/java/school/faang/task_50724/Main.java
@@ -0,0 +1,16 @@
+package school.faang.task_50724;
+
+import lombok.extern.slf4j.Slf4j;
+
+import java.util.ArrayList;
+
+@Slf4j
+public class Main {
+    public static void main(String[] args) {
+        Inventory inventory = new Inventory(new ArrayList<>());
+
+        inventory.combiningItems().join();
+
+        inventory.getItems().forEach(System.out::println);
+    }
+}
