c1af967db8a9cfdd903d26deed409784dfeb36b5
Aleksandr Kozlov <vismut-888@yandex.ru>
2024-12-24 17:41:03 +0300

    Исправлены комментарии после ревью

--- src/main/java/school/faang/sprint_3/task_49332/VideoManager.java
+++ src/main/java/school/faang/sprint_3/task_49332/VideoManager.java
@@ -10,11 +10,11 @@ public class VideoManager {
         viewsMap = new ConcurrentHashMap<>();
     }
 
-    public synchronized void addView(String videoId) {
+    public void addView(String videoId) {
         viewsMap.put(videoId, viewsMap.getOrDefault(videoId, 0) + 1);
     }
 
-    public synchronized int getViewCount(String videoId) {
+    public int getViewCount(String videoId) {
         return viewsMap.getOrDefault(videoId, 0);
     }
 }
