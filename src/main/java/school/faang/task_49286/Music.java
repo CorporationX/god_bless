3f6ec6e9d67df394cd2e2cda5870dc7d4864ff43
Dias Kamza <kamzadias31@gmail.com>
2024-12-24 12:08:38 +0500

    fix styles

--- src/main/java/school/faang/task_49286/Music.java
+++ src/main/java/school/faang/task_49286/Music.java
@@ -5,16 +5,15 @@ public class Music {
         Player player = new Player();
 
         Thread user1 = new Thread(player::play, "Пользователь 1");
+        user1.start();
 
         Thread user2 = new Thread(player::pause, "Пользователь 2");
+        user2.start();
 
         Thread user3 = new Thread(player::skip, "Пользователь 3");
+        user3.start();
 
         Thread user4 = new Thread(player::previous, "Пользователь 4");
-
-        user1.start();
-        user2.start();
-        user3.start();
         user4.start();
     }
 }
