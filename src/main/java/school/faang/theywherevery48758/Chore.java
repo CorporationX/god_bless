bd343db23c681c41895ae0c39589e9d430807dc8
Ilya <thomas.moody@mail.ru>
2024-12-24 22:25:18 +0300

    - вывел в константу время ожидания

--- src/main/java/school/faang/theywherevery48758/Chore.java
+++ src/main/java/school/faang/theywherevery48758/Chore.java
@@ -5,13 +5,14 @@ import lombok.AllArgsConstructor;
 @AllArgsConstructor
 public class Chore implements Runnable {
     private String chore;
+    private static final int SLEEP_TIME_MS = 1000;
 
     @Override
     public void run() {
         try {
             System.out.println(Thread.currentThread().getName()
                     + " performs the task: " + chore);
-            Thread.sleep(1000);
+            Thread.sleep(SLEEP_TIME_MS);
             System.out.println(chore + " completed!");
         } catch (InterruptedException e) {
             System.out.println("the task" + chore
