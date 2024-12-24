e5119c7f00d33b5dbce6450563d442167b86eb04
Anton Graf <ant0ngraf@yandex.ru>
2024-12-24 08:35:06 +0600

    Добавляет список видео в параметры startService, подсчет суммы просмотров для указанного списка видео

--- src/main/java/school/faang/bjs_49275/VideoManagerService.java
+++ src/main/java/school/faang/bjs_49275/VideoManagerService.java
@@ -2,6 +2,7 @@ package school.faang.bjs_49275;
 
 import lombok.extern.slf4j.Slf4j;
 
+import java.util.List;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.TimeUnit;
@@ -10,29 +11,36 @@ import java.util.stream.IntStream;
 @Slf4j
 public class VideoManagerService {
     private static final int NUM_THREADS = 100;
-    private static final int NUM_VIDEOS = 10;
-    private static final int TERMINATION_TIMEOUT = 10;
+    private static final int TERMINATION_TIMEOUT = 60;
 
-    public void startService() {
+    private final VideoManager videoManager = new VideoManager();
+
+    public void startService(List<String> videos) {
         ExecutorService executor = Executors.newFixedThreadPool(NUM_THREADS);
-        VideoManager videoManager = new VideoManager();
 
-        IntStream.range(0, NUM_VIDEOS)
-                .forEach(i -> IntStream.range(0, NUM_THREADS / NUM_VIDEOS)
-                        .forEach(j -> executor.submit(() -> {
-                            String videoName = "Video #%d".formatted(i);
-                            videoManager.addView(videoName);
-                            int viewsCount = videoManager.getViewCount(videoName);
-                            log.info("ViewsCount for Video {}:{}", videoName,
-                                    viewsCount);
-                        })));
+        videos.forEach(video -> IntStream.range(0, NUM_THREADS / videos.size())
+                .forEach(i -> executor.submit(() -> {
+                    videoManager.addView(video);
+                    int viewsCount = videoManager.getViewCount(video);
+                    log.info("ViewsCount for Video {}:{}", video,
+                            viewsCount);
+                }))
+        );
 
         executor.shutdown();
 
         try {
-            executor.awaitTermination(TERMINATION_TIMEOUT, TimeUnit.SECONDS);
+            if (!executor.awaitTermination(TERMINATION_TIMEOUT, TimeUnit.SECONDS)) {
+                executor.shutdownNow();
+            }
         } catch (InterruptedException e) {
             log.error("Interrupted while waiting for termination", e);
         }
+    }
+
+    public int getViewCount(List<String> videos) {
+        return videos.stream()
+                .map(videoManager::getViewCount)
+                .reduce(0, Integer::sum);
     }
 }
