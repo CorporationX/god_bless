e438ec5836009138eba8d9ff15901b6098d0b798
OxanaNovozhilova <nov2303@mail.ru>
2024-12-24 15:17:21 +0500

    BJS2-49648 (#16123)
    * BJS2-49648
    
    * BJS2-49648 update after review
    
    ---------
    
    Co-authored-by: User Novozhilova <nov2303@mail,ru>

---
+++ src/main/java/school/faang/task_49648/Boss.java
@@ -0,0 +1,43 @@
+package school.faang.task_49648;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Boss {
+    private final int maxPlayers;
+    private static int currentPlayers;
+    private Object lock = new Object();
+    private List<Player> players = new ArrayList<>();
+
+    public Boss(int maxPlayers) {
+        this.maxPlayers = maxPlayers;
+    }
+
+    public void joinBattle(Player player) {
+        synchronized (lock) {
+            while (currentPlayers >= maxPlayers) {
+                try {
+                    lock.wait();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+            if (!players.contains(player)) {
+                players.add(player);
+                currentPlayers++;
+                System.out.println(String.format("Player %s has joined to battle", player.getName()));
+            }
+        }
+    }
+
+    public void finishBattle(Player player) {
+        synchronized (lock) {
+            if (players.contains(player)) {
+                players.remove(player);
+                currentPlayers--;
+                System.out.println(String.format("Player %s has finished to battle", player.getName()));
+                lock.notify();
+            }
+        }
+    }
+}
