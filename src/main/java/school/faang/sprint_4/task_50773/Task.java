17e5b68175c605e20515c7502a738f4b72697956
Stanislav <stasbugaev2006@gmail.com>
2024-12-24 21:50:33 +0500

    Решение задачи

---
+++ src/main/java/school/faang/sprint_4/task_50773/Task.java
@@ -0,0 +1,12 @@
+package school.faang.sprint_4.task_50773;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@AllArgsConstructor
+@Getter
+public class Task {
+    private String name;
+    private int difficult;
+    private int reward;
+}
