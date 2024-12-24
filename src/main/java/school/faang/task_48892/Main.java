5da34d0340000f79ba27a5822773054c45ef4bbd
User Novozhilova <nov2303@mail,ru>
2024-12-24 14:35:02 +0500

    BJS2-48892 update after review

--- src/main/java/school/faang/task_48892/Main.java
+++ src/main/java/school/faang/task_48892/Main.java
@@ -5,18 +5,20 @@ import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.Future;
 import java.util.concurrent.TimeUnit;
-import java.util.concurrent.TimeoutException;
 
 public class Main {
-    public static void main(String[] args) throws ExecutionException, InterruptedException, TimeoutException {
+    public static void main(String[] args) throws ExecutionException, InterruptedException {
         GooglePhotosAutoUploader googlePhotosAutoUploader = new GooglePhotosAutoUploader();
         ExecutorService executor = Executors.newFixedThreadPool(2);
         for (int i = 0; i < 10; i++) {
             int photoNumber = i;
             executor.submit(() -> googlePhotosAutoUploader.onNewPhotoAdded(String.format("Photo_%d.jpg", photoNumber)));
         }
-        Future result = (Future) executor.submit(() -> googlePhotosAutoUploader.startAutoUpload())
-                .get(1, TimeUnit.MINUTES);
+        Future result = executor.submit(() -> googlePhotosAutoUploader.startAutoUpload());
+        while (!result.isDone()) {
+            System.out.println("Loading... ");
+            Thread.sleep(1000);
+        }
         executor.shutdown();
         try {
             executor.awaitTermination(1, TimeUnit.MINUTES);
