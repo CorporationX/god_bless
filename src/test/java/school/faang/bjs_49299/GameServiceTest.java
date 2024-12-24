3c9f21debb41fd898a4c8d850e48152a3dea72c2
Anton Graf <ant0ngraf@yandex.ru>
2024-12-24 09:47:44 +0600

    Добавляет проверки количества жизней и очков до и после игры, проверку о том, что игра не запущена

--- src/test/java/school/faang/bjs_49299/GameServiceTest.java
+++ src/test/java/school/faang/bjs_49299/GameServiceTest.java
@@ -7,7 +7,18 @@ class GameServiceTest {
 
     @Test
     void gameStart() {
-        GameService gameService = new GameService();
-        Assertions.assertDoesNotThrow(gameService::gameStart);
+        int startScore = 0;
+        int startLives = 50;
+        Game game = new Game(startScore, startLives);
+        GameService gameService = new GameService(game);
+
+        Assertions.assertEquals(0, game.getScore());
+        Assertions.assertEquals(50, game.getLives());
+
+        gameService.gameStart();
+
+        Assertions.assertFalse(game.isRunning());
+        Assertions.assertEquals(0, game.getLives());
+        Assertions.assertTrue(game.getScore() > 0);
     }
 }
