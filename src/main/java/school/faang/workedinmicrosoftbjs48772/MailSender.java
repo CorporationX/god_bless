ec25d7d3a3661b4087315fb3d9bb8a4bf15f8e6f
Ilya <thomas.moody@mail.ru>
2024-12-24 22:20:48 +0300

    - залогировал InterruptedException

--- src/main/java/school/faang/workedinmicrosoftbjs48772/MailSender.java
+++ src/main/java/school/faang/workedinmicrosoftbjs48772/MailSender.java
@@ -1,9 +1,13 @@
 package school.faang.workedinmicrosoftbjs48772;
 
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 public class MailSender {
     private static final int THREADSCOUNT = 5;
     private static final int TOTALMESSAGE = 1000;
     private static final int BATCHSIZE = TOTALMESSAGE / THREADSCOUNT;
+    private static final Logger logger = Logger.getLogger(MailSender.class.getName());
 
     public static void main(String[] args) {
         Thread[] threads = new Thread[THREADSCOUNT];
@@ -21,7 +25,8 @@ public class MailSender {
             try {
                 threads[i].join();
             } catch (InterruptedException e) {
-                throw new RuntimeException(e);
+                logger.log(Level.SEVERE, "Thread was interrupted", e);
+                Thread.currentThread().interrupt();
             }
         }
 
