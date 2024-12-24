818c75b2643b54fc4a9a468c11c65e783f60985f
alexey_obraztsov <alexey.autotester86@gmail.com>
2024-12-24 18:51:08 +0300

    alexeyrnd-BJS2-49219. Spotify. Multithread. review adjustments

--- src/main/java/school/faang/sprint_1/task_spotifybjs2n49219/Music.java
+++ src/main/java/school/faang/sprint_1/task_spotifybjs2n49219/Music.java
@@ -11,36 +11,24 @@ import java.util.concurrent.TimeUnit;
 @Slf4j
 public class Music {
     private static int NUMBER_OF_THREADS = 4;
-    private static int PLAY_PRESS_PERIOD = 1;
-    private static int PAUSE_PRESS_PERIOD = 4;
-    private static int SKIP_PRESS_PERIOD = 2;
-    private static int PRIVIOUS_PRESS_PERIOD = 2;
+    private static int PRESS_PERIOD = 1;
     private static int TIME_TO_PLAY_MILISECONDS = 7000;
 
     public void startSpotify() {
         Player player = new Player();
         ScheduledExecutorService scheduledMusicPlay = Executors.newScheduledThreadPool(NUMBER_OF_THREADS);
 
-        Runnable runPlay = player::play;
-        Runnable runPause = player::pause;
-        Runnable runSkip = player::skip;
-        Runnable runPrevious = player::previous;
+        List<Runnable> runActions = List.of(
+                player::play,
+                player::pause,
+                player::skip,
+                player::previous
+        );
 
-//        List<Runnable> actions = new ArrayList<>(List.of(
-//                () -> player.play(),
-//                () -> player.pause(),
-//                new Thread(runSkip),
-//                new Thread(runPrevious)
-//        ));
-//
-//        for(Thread thread : threads) {
-//            thread.start();
-//        }
+        for (Runnable runnable : runActions) {
+            scheduledMusicPlay.scheduleAtFixedRate(runnable, 0, PRESS_PERIOD++, TimeUnit.SECONDS);
+        }
 
-        scheduledMusicPlay.scheduleAtFixedRate(runPlay, 0, PLAY_PRESS_PERIOD, TimeUnit.SECONDS);
-        scheduledMusicPlay.scheduleAtFixedRate(runPause, 0, PAUSE_PRESS_PERIOD, TimeUnit.SECONDS);
-        scheduledMusicPlay.scheduleAtFixedRate(runSkip, 0, SKIP_PRESS_PERIOD, TimeUnit.SECONDS);
-        scheduledMusicPlay.scheduleAtFixedRate(runPrevious, 0, PRIVIOUS_PRESS_PERIOD, TimeUnit.SECONDS);
 
         try {
             Thread.sleep(TIME_TO_PLAY_MILISECONDS);
