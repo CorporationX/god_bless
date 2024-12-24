37e63d0e0b94b0a0eaba875e249a2fcbb87c6ac4
Anton Graf <ant0ngraf@yandex.ru>
2024-12-24 08:36:48 +0600

    Добавляет проверку суммы просмотров для указанного списка видео

--- src/test/java/school/faang/bjs_49275/VideoManagerServiceTest.java
+++ src/test/java/school/faang/bjs_49275/VideoManagerServiceTest.java
@@ -3,11 +3,28 @@ package school.faang.bjs_49275;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+import java.util.List;
+
 class VideoManagerServiceTest {
 
     @Test
     void startService() {
         VideoManagerService service = new VideoManagerService();
-        Assertions.assertDoesNotThrow(service::startService);
+        List<String> videos = List.of(
+                "Video #1",
+                "Video #2",
+                "Video #3",
+                "Video #4",
+                "Video #5",
+                "Video #6",
+                "Video #7",
+                "Video #8",
+                "Video #9",
+                "Video #10"
+        );
+        service.startService(videos);
+        int actual = service.getViewCount(videos);
+        int expected = 100;
+        Assertions.assertEquals(expected, actual);
     }
 }
