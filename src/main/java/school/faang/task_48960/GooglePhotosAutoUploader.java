4ab21c125ab4b410b6958f1918750ab3c1f5caf6
Natalya Vasileva <boryantheone228@yandex.ru>
2024-12-24 13:12:32 +0300

    BJS2-48960 Добавление решения задачи Google Photo sync

---
+++ src/main/java/school/faang/task_48960/GooglePhotosAutoUploader.java
@@ -0,0 +1,57 @@
+package school.faang.task_48960;
+
+import lombok.Getter;
+import lombok.extern.slf4j.Slf4j;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Slf4j
+public class GooglePhotosAutoUploader {
+    private final Object lock;
+    private final List<String> photosToUpload;
+    @Getter
+    private boolean shouldWork;
+
+    public GooglePhotosAutoUploader() {
+        this.photosToUpload = new ArrayList<>();
+        this.lock = new Object();
+        this.shouldWork = true;
+    }
+
+    public void startAutoUpload() {
+        synchronized (lock) {
+            while (photosToUpload.isEmpty()) {
+                try {
+                    lock.wait();
+                    log.info("Ожидание фото для загрузки...");
+                } catch (InterruptedException e) {
+                    log.error("Произошло исключение при выполнении метода wait()", e);
+                }
+            }
+            uploadPhotos();
+        }
+    }
+
+    public void onNewPhotoAdded(String photoPath) {
+        if (photoPath == null || photoPath.isEmpty()) {
+            log.info("Фото не может быть загружено, так как указан некорректный путь к нему");
+            return;
+        }
+
+        synchronized (lock) {
+            photosToUpload.add(photoPath);
+            log.info("Фото {} добавлено в список для загрузки", photoPath);
+            lock.notifyAll();
+        }
+    }
+
+    private void uploadPhotos() {
+        for (String photo : photosToUpload) {
+            log.info("Загружаем на сервер фото {}...", photo);
+        }
+        photosToUpload.clear();
+        log.info("Фотографии загружены на сервер!");
+        lock.notify();
+    }
+}
---
+++ src/main/java/school/faang/task_48960/Main.java
@@ -0,0 +1,23 @@
+package school.faang.task_48960;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+public class Main {
+
+    public static void main(String[] args) {
+        GooglePhotosAutoUploader googleUploader = new GooglePhotosAutoUploader();
+
+        Thread uploaderThread = new Thread(googleUploader::startAutoUpload);
+        Thread adderThread = new Thread(() -> {
+            for (int i = 0; i < 5; i++) {
+                googleUploader.onNewPhotoAdded("C://journey/photo" + i);
+            }
+        });
+
+        Thread[] threads = new Thread[]{uploaderThread, adderThread};
+        for (Thread thread : threads) {
+            thread.start();
+        }
+    }
+}
