1499a98011c06745ca3d0b4ede2263378f3950f6
Pavel Schelkin <rhanan1233@gmail.com>
2024-12-24 16:15:28 +0300

    The Big Bang Theory

---
+++ src/main/java/school/faang/sprint1/task_48261/BigBangTheory.java
@@ -0,0 +1,31 @@
+package school.faang.sprint1.task_48261;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
+
+public class BigBangTheory {
+    public static void main(String[] args) {
+        ExecutorService executor = Executors.newFixedThreadPool(4);
+        Task sheldon = new Task("Sheldon", "Find topic");
+        Task leonard = new Task("Leonard", "Create project");
+        Task govard = new Task("Govard", "Check the project");
+        Task redjer = new Task("Redjer", "Present the project");
+        executor.execute(sheldon);
+        executor.execute(leonard);
+        executor.execute(govard);
+        executor.execute(redjer);
+        executor.shutdown();
+        try {
+            if (!executor.awaitTermination(1, TimeUnit.MINUTES)) {
+                System.out.println("Не все задачи выполнены в установленный срок");
+                executor.shutdownNow();
+            }
+        } catch (InterruptedException e) {
+            System.out.println("Главный поток прерван");
+            executor.shutdownNow();
+        }
+        System.out.println("Задача завершена");
+
+    }
+}
