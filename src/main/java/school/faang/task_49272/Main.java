0d31d0e9daafb3c4ac19a679abd43308a20099a4
triplek <weribel@mail.ru>
2024-12-24 22:43:51 +0300

    Bro Force! (исправлено)

--- src/main/java/school/faang/task_49272/Main.java
+++ src/main/java/school/faang/task_49272/Main.java
@@ -8,34 +8,36 @@ import java.util.concurrent.TimeUnit;
 public class Main {
     public static void main(String[] args) {
         Game game = new Game();
-
         ExecutorService executorService = Executors.newFixedThreadPool(4);
 
-        for (int i = 0; i < 100; i++) {
-            executorService.submit(() -> {
-                boolean earnedPoints = Math.random() > 0.5;
-                boolean lostLife = Math.random() > 0.7;
+        try {
+            for (int i = 0; i < 100; i++) {
+                executorService.submit(() -> {
+                    boolean earnedPoints = Math.random() > 0.5;
+                    boolean lostLife = Math.random() > 0.7;
 
-                game.update(earnedPoints, lostLife);
+                    if (!game.update(earnedPoints, lostLife)) {
+                        executorService.shutdownNow();
+                    }
 
-                if (!game.update(earnedPoints, lostLife)) {
-                    executorService.shutdown();
                     try {
-                        if (!executorService.awaitTermination(1, TimeUnit.MINUTES)) {
-                            System.err.println("Потоки не завершились вовремя");
-                        }
+                        Thread.sleep(100);
                     } catch (InterruptedException e) {
                         Thread.currentThread().interrupt();
-                        System.err.println("Ошибка ожидания завершения потоков: " + e.getMessage());
+                        System.err.println("Поток был прерван: " + e.getMessage());
                     }
+                });
+            }
+        } finally {
+            executorService.shutdown(); // Завершение ExecutorService
+            try {
+                if (!executorService.awaitTermination(1, TimeUnit.MINUTES)) {
+                    System.err.println("Потоки не завершились вовремя");
                 }
-                try {
-                    Thread.sleep(100);
-                } catch (InterruptedException e) {
-                    Thread.currentThread().interrupt();
-                    e.printStackTrace();
-                }
-            });
+            } catch (InterruptedException e) {
+                Thread.currentThread().interrupt();
+                System.err.println("Ошибка ожидания завершения потоков: " + e.getMessage());
+            }
         }
     }
 }
