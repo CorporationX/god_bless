db8bf6dd77b43b9167f6c565a1216fd64fc2e876
ArzamaS Ilya <coldoff@inbox.ru>
2024-12-25 11:20:25 +0300

    Solved task.

---
+++ src/main/java/school/faang/task_48828/Food.java
@@ -0,0 +1,10 @@
+package school.faang.task_48828;
+
+import lombok.Data;
+import lombok.RequiredArgsConstructor;
+
+@Data
+@RequiredArgsConstructor
+public class Food {
+    private final String food;
+}
