7d887f1ba5b7e16e09939048e41f24dc4f8f5c96
Artemon <artemon.kos@yandex.ru>
2024-12-25 16:10:24 +0100

    init + complete

---
+++ src/main/java/school/faang/bjs249899/service/MasterCardService.java
@@ -0,0 +1,43 @@
+package school.faang.bjs249899.service;
+
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+
+public class MasterCardService implements Runnable{
+    public String collectPayment() {
+        try {
+            Thread.sleep(10000);
+            return "Payment was successfully collected " + this;
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public String sendAnalytics() {
+        try {
+            Thread.sleep(1000);
+            return "Analytics was sent " + this;
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+
+    @Override
+    public void run() {
+        ExecutorService threadPool = Executors.newFixedThreadPool(2);
+        Future<String> paymentResult = threadPool.submit(this::collectPayment);
+        CompletableFuture.supplyAsync(this::sendAnalytics)
+                .thenAccept(System.out::println);
+        try {
+            System.out.println(paymentResult.get());
+        } catch (InterruptedException | ExecutionException e) {
+            throw new RuntimeException(e);
+        } finally {
+            threadPool.shutdown();
+        }
+    }
+}
