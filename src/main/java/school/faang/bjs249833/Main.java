e8228e74a56f193fe77c6286833af9bcd914453c
Maksim-VV <laulmv11@gmail.com>
2024-12-25 10:23:33 +0300

    BJS2-49833, Supercow - post-review edits

--- src/main/java/school/faang/bjs249833/Main.java
+++ src/main/java/school/faang/bjs249833/Main.java
@@ -3,6 +3,9 @@ package school.faang.bjs249833;
 
 import lombok.extern.slf4j.Slf4j;
 
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
 @Slf4j
 public class Main {
 
@@ -11,37 +14,16 @@ public class Main {
     public static void main(String[] args) {
 
         Boss boss = new Boss(MAX_PLAYERS);
+        ExecutorService threadPool = Executors.newFixedThreadPool(3);
 
-        Player player1 = new Player("Player 1");
-        Player player2 = new Player("Player 2");
-        Player player3 = new Player("Player 3");
+        for (int i = 1; i <= 3; i++) {
+            int numPlayer = i;
 
-        Thread thread1 = new Thread(() -> {
-            try {
+            threadPool.submit(() -> {
+                Player player1 = new Player("Player " + numPlayer);
                 player1.startBattle(boss);
-            } catch (InterruptedException ex) {
-                log.error("Battle start error, player1!");
-            }
-        });
-
-        Thread thread2 = new Thread(() -> {
-            try {
-                player2.startBattle(boss);
-            } catch (InterruptedException e) {
-                log.error("Battle start error, player2!");
-            }
-        });
-
-        Thread thread3 = new Thread(() -> {
-            try {
-                player3.startBattle(boss);
-            } catch (InterruptedException e) {
-                log.error("Battle start error, player3!");
-            }
-        });
-
-        thread1.start();
-        thread2.start();
-        thread3.start();
+            });
+        }
+        threadPool.shutdown();
     }
 }
--- src/main/java/school/faang/bjs249833/Player.java
+++ src/main/java/school/faang/bjs249833/Player.java
@@ -3,16 +3,23 @@ package school.faang.bjs249833;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NonNull;
+import lombok.extern.slf4j.Slf4j;
 
+@Slf4j
 @Getter
 @AllArgsConstructor
 public class Player {
     public static final int TIME_FIGHT = 2000;
     private String name;
 
-    public void startBattle(Boss boss) throws InterruptedException {
-        boss.joinBattle(this);
-        Thread.sleep(TIME_FIGHT);
+    public void startBattle(@NonNull Boss boss)  {
+        try {
+            boss.joinBattle(this);
+            Thread.sleep(TIME_FIGHT);
+        } catch (InterruptedException ex) {
+            log.error("Battle start error, player!");
+        }
         boss.leaveBattle(this);
     }
 }
