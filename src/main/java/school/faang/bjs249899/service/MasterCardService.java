0ef0a8274c79ae932ea9efc41465c2e09c3e5356
Artemon <artemon.kos@yandex.ru>
2024-12-25 16:12:36 +0100

    codestyle

--- src/main/java/school/faang/bjs249899/service/MasterCardService.java
+++ src/main/java/school/faang/bjs249899/service/MasterCardService.java
@@ -6,7 +6,7 @@ import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.Future;
 
-public class MasterCardService implements Runnable{
+public class MasterCardService implements Runnable {
     public String collectPayment() {
         try {
             Thread.sleep(10000);
