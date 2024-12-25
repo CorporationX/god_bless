32a446dfb3a159d7f3fcbc1e94c3ca61bc013e46
Ilya <thomas.moody@mail.ru>
2024-12-25 23:42:58 +0300

    - создал класс Player и реализовал в нем залоченные методы для выполнения действий - в классе Music создал несколько потоков, которые имитирует действия разных пользователей

--- src/main/java/school/faang/blockingsptifybjs49377/Music.java
+++ src/main/java/school/faang/blockingsptifybjs49377/Music.java
@@ -3,10 +3,11 @@ package school.faang.blockingsptifybjs49377;
 public class Music {
     public static void main(String[] args) {
         Player player = new Player();
-        Thread thread1 = new Thread(() -> player.play());
-        Thread thread2 = new Thread(() -> player.pause());
-        Thread thread3 = new Thread(() -> player.skip());
-        Thread thread4 = new Thread(() -> player.previous());
+
+        final Thread thread1 = new Thread(() -> player.play());
+        final Thread thread2 = new Thread(() -> player.pause());
+        final Thread thread3 = new Thread(() -> player.skip());
+        final Thread thread4 = new Thread(() -> player.previous());
 
         thread1.start();
         thread2.start();
