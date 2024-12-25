234d81976c9a5ffc2d3e4aab8dae8251521bf72e
Maksim-VV <laulmv11@gmail.com>
2024-12-25 14:04:23 +0300

    BJS2-49133, Google Photo sync - post-review edits

--- src/main/java/school/faang/bjs249133/GooglePhotosAutoUploader.java
+++ src/main/java/school/faang/bjs249133/GooglePhotosAutoUploader.java
@@ -1,35 +1,45 @@
 package school.faang.bjs249133;
 
+import lombok.Getter;
+import lombok.Setter;
+
 import java.util.ArrayList;
 import java.util.List;
 
 public class GooglePhotosAutoUploader {
 
-    private final Object lock = new Object();
+    private final Object lock;
     private final List<String> photosToUpload;
+    @Setter
+    private boolean isServiceTurnOn;
 
     public GooglePhotosAutoUploader() {
         this.photosToUpload = new ArrayList<>();
+        this.lock = new Object();
+        this.isServiceTurnOn = true;
     }
 
     public void startAutoUpload() {
-        synchronized (lock) {
-            while (photosToUpload.isEmpty()) {
-                try {
-                    lock.wait();
-                    System.out.println("Ждем, пока не будут добавлены фотографии");
-                } catch (InterruptedException e) {
-                    throw new RuntimeException(e);
+        while (isServiceTurnOn) {
+            synchronized (lock) {
+                while (photosToUpload.isEmpty()) {
+                    try {
+                        lock.wait();
+                        System.out.println("Ждем, пока не будут добавлены фотографии");
+                    } catch (InterruptedException e) {
+                        Thread.currentThread().interrupt();
+                        throw new RuntimeException(e);
+                    }
                 }
+                uploadPhotos();
             }
-            uploadPhotos();
         }
     }
 
     public void onNewPhotoAdded(String photoPath) {
         synchronized (lock) {
-            photosToUpload.add(photoPath); // Добавляем путь к фотографии в список
-            lock.notify(); // Уведомляем поток, что появились новые фотографии
+            photosToUpload.add(photoPath);
+            lock.notify();
         }
     }
 
