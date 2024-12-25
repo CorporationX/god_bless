992b601bec49738a5b79d30effa1c409d687daaf
Ilya Vysokolyan <rd2d@list.ru>
2024-12-25 18:35:22 +0300

    BJS2-48576: поправил замечания

--- src/main/java/school/faang/task_48576/Food.java
+++ src/main/java/school/faang/task_48576/Food.java
@@ -1,12 +1,5 @@
 package school.faang.task_48576;
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
 
-
-@Getter
-@AllArgsConstructor
-public class Food {
-
-    private String name;
+public record Food(String name) {
 }
