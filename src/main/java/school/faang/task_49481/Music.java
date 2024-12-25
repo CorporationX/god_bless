2bed0058c9cf3207d17f9681cb02bf4465cfe3c4
Shakhzod Kamolov <shaxzod.kamolov@keysoft.uz>
2024-12-25 15:11:56 +0500

    fix(BJS2-49481) Checkstyle fix

--- src/main/java/school/faang/task_49481/Music.java
+++ src/main/java/school/faang/task_49481/Music.java
@@ -3,14 +3,15 @@ package school.faang.task_49481;
 public class Music {
     public static void main(String[] args) {
         Player player = new Player();
-        Thread playThread = new Thread(player::play);
-        Thread pauseThread = new Thread(player::pause);
-        Thread skipThread = new Thread(player::skip);
-        Thread previousThread = new Thread(player::previous);
+
+        final Thread playThread = new Thread(player::play);
+        final Thread pauseThread = new Thread(player::pause);
+        final Thread skipThread = new Thread(player::skip);
+        final Thread previousThread = new Thread(player::previous);
 
         playThread.start();
         pauseThread.start();
-        skipThread.start();
         previousThread.start();
+        skipThread.start();
     }
 }
