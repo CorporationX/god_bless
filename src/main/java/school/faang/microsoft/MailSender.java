89b6b07767b703c195b8b8a1f5215caec04660c7
Azamat Beisembay <azamat.beisembay@gmail.com>
2024-12-25 23:49:45 +0500

    azamat71-BJS2-48760 Вы работаете в Microsoft!

--- src/main/java/school/faang/microsoft/MailSender.java
+++ src/main/java/school/faang/microsoft/MailSender.java
@@ -1,20 +1,24 @@
 package school.faang.microsoft;
 
 public class MailSender {
-    public static void main(String[] args) {
-        int threadsCount = 5;
+    public static void main(String[] args) throws InterruptedException {
+        int threadsCount = 13;
         int messagesCount = 1000;
-        int startIndex = 1;
-        int endIndex = messagesCount / threadsCount;
+        int batchSize = messagesCount / threadsCount;
         Thread[] threads = new Thread[threadsCount];
 
         System.out.println("Начало отправки");
-        for (Thread thread : threads) {
-            SenderRunnable senderRunnable = new SenderRunnable(startIndex, endIndex);
-            startIndex = endIndex + 1;
-            endIndex = endIndex + messagesCount / threadsCount;
-            thread = new Thread(senderRunnable);
-            thread.start();
+        for (int i = 0; i < threadsCount; i++) {
+            int startIndex = i * batchSize;
+            int endIndex = (i + 1) * batchSize;
+            threads[i] = new Thread(new SenderRunnable(startIndex, endIndex));
+            threads[i].start();
         }
+
+        for (Thread thread : threads) {
+            thread.join();
+        }
+
+        System.out.println("Все письма отправлены!");
     }
 }
