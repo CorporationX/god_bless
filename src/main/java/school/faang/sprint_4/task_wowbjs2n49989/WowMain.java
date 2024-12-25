d9007a3fdccc1525c9aa94b5ffbb384fa7c98473
alexey_obraztsov <alexey.autotester86@gmail.com>
2024-12-25 10:36:41 +0300

    alexeyrnd-BJS2-49989. WOW. works. main() adjusted

--- src/main/java/school/faang/sprint_4/task_wowbjs2n49989/WowMain.java
+++ src/main/java/school/faang/sprint_4/task_wowbjs2n49989/WowMain.java
@@ -1,7 +1,8 @@
 package school.faang.sprint_4.task_wowbjs2n49989;
 
 import lombok.extern.slf4j.Slf4j;
-import java.util.Arrays;
+
+import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.CompletableFuture;
 
@@ -12,28 +13,24 @@ public class WowMain {
     public static void main(String[] args) {
         QuestSystem questSystem = new QuestSystem();
 
-        Player player1 = new Player("Thrall", 10, 250);
-        Player player2 = new Player("Sylvanas", 12, 450);
         List<Player> players = List.of(
                 new Player("Thrall", 10, 250),
-                new Player("Sylvanas", 12, 450),
-                new Player("Sylvanas", 12, 580)
+                new Player("Sylvanas", 12, 450)
         );
 
         List<Quest> quests = List.of(
                 new Quest("Defeat the Lich King", 10, 150),
                 new Quest("Retrieve the Sword of Azeroth", 8, 100)
         );
-        Quest quest1 = new Quest("Defeat the Lich King", 10, 150);
-        Quest quest2 = new Quest("Retrieve the Sword of Azeroth", 8, 100);
 
+        List<CompletableFuture<Player>> playerQuestsFutures = new ArrayList<>();
+        for (int i = 0; i < players.size(); i++) {
+            playerQuestsFutures.add(questSystem.startQuest(players.get(i), quests.get(i)));
+        }
 
-        CompletableFuture<Player> player1Quest = questSystem.startQuest(player1, quest1);
-        CompletableFuture<Player> player2Quest = questSystem.startQuest(player2, quest2);
-
-        for (CompletableFuture<Player> playerCompletableFuture : Arrays.asList(player1Quest, player2Quest)) {
+        for (CompletableFuture<Player> playerCompletableFuture : playerQuestsFutures) {
             playerCompletableFuture.thenAccept(player ->
-                    log.info("{} has completed the quest and now has {} experience points.",
+                    log.info("'{}' has completed the quest and now has {} experience points.",
                             player.getName(), player.getExperience()));
         }
 
