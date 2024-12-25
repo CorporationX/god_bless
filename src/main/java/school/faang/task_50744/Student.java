24d58dfab94d7584cc39a3d5b4907da9c1e87e6b
Eugene <@evgeniy_kim>
2024-12-25 21:34:07 +0500

    update

--- src/main/java/school/faang/task_50744/Student.java
+++ src/main/java/school/faang/task_50744/Student.java
@@ -3,11 +3,13 @@ package school.faang.task_50744;
 import lombok.Getter;
 import school.faang.exception.CheckException;
 
+import java.util.concurrent.atomic.AtomicInteger;
+
 @Getter
 public class Student {
     private final String name;
     private final int year;
-    private int points;
+    private final AtomicInteger points;
 
     public Student(String name, int year, int points) {
         if (name == null || name.isEmpty()) {
@@ -15,11 +17,11 @@ public class Student {
         }
         this.name = name;
         this.year = year;
-        this.points = points;
+        this.points = new AtomicInteger(points);
     }
 
     public void addPoints(int points) {
         System.out.printf("%s добавлено %s очков%n", this.name, points);
-        this.points += points;
+        this.points.addAndGet(points);
     }
 }
