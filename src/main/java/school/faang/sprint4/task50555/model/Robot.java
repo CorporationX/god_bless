be665d74b5938e7197416d3135ffb08a1e61c802
Alexey Gorokhov <almigor@mail.ru>
2024-12-25 19:41:00 +0300

    Star Wars Arena

---
+++ src/main/java/school/faang/sprint4/task50555/model/Robot.java
@@ -0,0 +1,13 @@
+package school.faang.sprint4.task50555.model;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
+public class Robot {
+    private final String name;
+    private final int attackPower;
+    private final int defensePower;
+
+}
