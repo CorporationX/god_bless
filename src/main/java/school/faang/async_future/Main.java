82b896facabdf67e8709468a656b04896dda7407
Ivan Baev <ibai86@yandex.ru>
2024-12-25 09:28:02 +0300

    add second service

--- src/main/java/school/faang/async_future/Main.java
+++ src/main/java/school/faang/async_future/Main.java
@@ -1,27 +1,32 @@
 package school.faang.async_future;
 
-import lombok.SneakyThrows;
 import lombok.extern.slf4j.Slf4j;
 
+import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.Future;
 
 @Slf4j
-
 public class Main {
     public static void main(String[] args) {
         MasterCardService paymentService = new MasterCardService();
+
         ExecutorService executor = Executors.newSingleThreadExecutor();
         Future<Integer> paymentResult = executor.submit(paymentService::collectPayment);
+
+        CompletableFuture<Integer> analyticsResult = CompletableFuture.supplyAsync(paymentService::sendAnalytics);
+
         try {
             log.info("Payment passed: {}", paymentResult.get());
         } catch (InterruptedException e) {
-            throw new RuntimeException(e);
+            log.error("Payment process was interrupted");
         } catch (ExecutionException e) {
-            throw new RuntimeException(e);
+            log.info("Payment cancelled");
         }
+
+        log.info("Payment info: {}", analyticsResult.join());
         executor.shutdown();
     }
 }
