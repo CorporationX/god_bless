d801ec3ac49c301ca66c1de8fde25603bac6c54b
Sergey Senin <04sersen@gmail.com>
2024-12-26 19:24:11 +0300

    Реализована система управления доступом игроков к сражению с боссом с поддержкой многопоточности.

--- src/main/java/school/faang/task_49630/Main.java
+++ src/main/java/school/faang/task_49630/Main.java
@@ -7,7 +7,6 @@ public class Main {
         Player player1 = new Player("Player 1");
         Player player2 = new Player("Player 2");
         Player player3 = new Player("Player 3");
-        Player player4 = new Player("Player 4");
 
         Thread thread1 = new Thread(() -> {
             try {
@@ -33,24 +32,14 @@ public class Main {
             }
         });
 
-        Thread thread4 = new Thread(() -> {
-            try {
-                player4.startBattle(boss);
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
-        });
-
         thread1.start();
         thread2.start();
         thread3.start();
-        thread4.start();
 
         try {
             thread1.join();
             thread2.join();
             thread3.join();
-            thread4.join();
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
