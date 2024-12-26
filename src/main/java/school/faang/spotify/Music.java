9aca64a8043f71b5ff4830e48ea3bb49664d81ce
ZhannaD <zhanna.davidov@gmail.com>
2024-12-26 21:33:25 +0200

    fix spotify - task

--- src/main/java/school/faang/spotify/Music.java
+++ src/main/java/school/faang/spotify/Music.java
@@ -3,18 +3,16 @@ package school.faang.spotify;
 public class Music {
     public static void main(String[] args) {
         Player player = new Player();
-        Thread thread1 = new Thread(player::play);
-        thread1.start();
-        Thread thread2 = new Thread(player::pause);
-        thread2.start();
-        Thread thread3 = new Thread(player::play);
-        thread3.start();
-        Thread thread4 = new Thread(player::skip);
-        thread4.start();
-        Thread thread5 = new Thread(player::previous);
-        thread5.start();
-        Thread thread6 = new Thread(player::play);
-        thread6.start();
+        Thread playThread = new Thread(player::play);
+        playThread.start();
 
+        Thread pauseThread = new Thread(player::pause);
+        pauseThread.start();
+
+        Thread skipThread = new Thread(player::skip);
+        skipThread.start();
+
+        Thread previousThread = new Thread(player::previous);
+        previousThread.start();
     }
 }
