60fe8a5d3caa17c72644c7e87a0344913a153ce6
Anton Graf <ant0ngraf@yandex.ru>
2024-12-26 13:24:22 +0600

    Решение задачи "WOW"

---
+++ src/main/java/school/faang/bjs_50017/Player.java
@@ -0,0 +1,16 @@
+package school.faang.bjs_50017;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@AllArgsConstructor
+@Getter
+public class Player {
+    private final String name;
+    private final int level;
+    @Setter
+    private int experience;
+
+
+}
---
+++ src/main/java/school/faang/bjs_50017/QuestSystem.java
@@ -0,0 +1,31 @@
+package school.faang.bjs_50017;
+
+import lombok.extern.slf4j.Slf4j;
+
+import java.util.concurrent.CompletableFuture;
+
+@Slf4j
+public class QuestSystem {
+
+    public CompletableFuture<Player> startQuest(Player player, Quest quest) {
+        return CompletableFuture.supplyAsync(() -> {
+            log.info("{} starting quest {}", player.getName(), quest.getName());
+            return updatePlayer(player, quest);
+        }).thenApply(p -> {
+            log.info("Quest {} is running", quest.getName());
+            try {
+                Thread.sleep(quest.getDifficulty());
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+            log.info("Quest {} is finished", quest.getName());
+            return p;
+        });
+
+    }
+
+    public Player updatePlayer(Player player, Quest quest) {
+        player.setExperience(player.getExperience() + quest.getReward());
+        return player;
+    }
+}
