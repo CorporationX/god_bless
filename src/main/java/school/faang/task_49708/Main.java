da4de030dc24ad5a7d49b0a7cf42d158328ff01e
Dias Kamza <kamzadias31@gmail.com>
2024-12-26 16:12:43 +0500

    fix styles

--- src/main/java/school/faang/task_49708/Main.java
+++ src/main/java/school/faang/task_49708/Main.java
@@ -1,35 +1,29 @@
 package school.faang.task_49708;
 
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
+
 public class Main {
     public static void main(String[] args) {
         Boss boss = new Boss(3);
 
-        Player player1 = new Player("Игрок 1");
-        Thread t1 = new Thread(() -> player1.startBattle(boss));
-        t1.start();
+        ExecutorService executor = Executors.newFixedThreadPool(5);
 
-        Player player2 = new Player("Игрок 2");
-        Thread t2 = new Thread(() -> player2.startBattle(boss));
-        t2.start();
+        for (int i = 1; i <= 5; i++) {
+            final int playerNumber = i;
+            executor.submit(() -> {
+                Player player = new Player("Игрок " + playerNumber);
+                player.startBattle(boss);
+            });
+        }
 
-        Player player3 = new Player("Игрок 3");
-        Thread t3 = new Thread(() -> player3.startBattle(boss));
-        t3.start();
-
-        Player player4 = new Player("Игрок 4");
-        Thread t4 = new Thread(() -> player4.startBattle(boss));
-        t4.start();
-
-        Player player5 = new Player("Игрок 5");
-        Thread t5 = new Thread(() -> player5.startBattle(boss));
-        t5.start();
+        executor.shutdown();
 
         try {
-            t1.join();
-            t2.join();
-            t3.join();
-            t4.join();
-            t5.join();
+            if (!executor.awaitTermination(1, TimeUnit.MINUTES)) {
+                System.out.println("Не все игроки успели завершить сражение с боссом за отведенное время.");
+            }
         } catch (InterruptedException e) {
             System.out.println("Главный поток был прерван.");
             Thread.currentThread().interrupt();
