c672e56f152b66b53dbdae12f2c3689d7ab142ec
lehaps <awda.96@bk.ru>
2024-12-26 21:12:12 +0300

    Реализовал функции Кажется что 1 лок лишний

---
+++ src/main/java/school/faang/task51037/Account.java
@@ -0,0 +1,41 @@
+package school.faang.task51037;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import lombok.ToString;
+import lombok.extern.slf4j.Slf4j;
+
+import java.util.concurrent.locks.ReentrantLock;
+
+@Slf4j
+@RequiredArgsConstructor
+@ToString
+@Getter
+public class Account {
+    private final int id;
+    private double balance;
+    private final ReentrantLock lock = new ReentrantLock();
+
+    private void lockBalance(Runnable runnable) {
+        lock.lock();
+
+        try {
+            runnable.run();
+            if (balance <= 0) {
+                log.error("{} не может быть меньше либо равен 0", balance);
+                balance = 0;
+            }
+        } finally {
+            lock.unlock();
+        }
+    }
+
+    public void deposit(double amount) {
+        lockBalance(() -> balance += amount);
+        log.info("Был выполнен депозит {}", balance);
+    }
+
+    public void withdraw(double amount) {
+        lockBalance(() -> balance -= amount);
+    }
+}
