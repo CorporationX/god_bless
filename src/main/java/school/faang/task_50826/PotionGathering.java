4bb2bc33469e697fa6b83ff9a62793154d542d08
User Novozhilova <nov2303@mail,ru>
2024-12-26 16:28:29 +0500

    BJS2-50826

---
+++ src/main/java/school/faang/task_50826/PotionGathering.java
@@ -0,0 +1,39 @@
+package school.faang.task_50826;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.atomic.AtomicInteger;
+
+public class PotionGathering {
+    public static void main(String[] args) {
+        List<Potion> potions = List.of(
+                new Potion("Healing Potion", 5),
+                new Potion("Mana Potion", 3),
+                new Potion("Stamina Potion", 4)
+        );
+        gatherAllIngredients(potions);
+    }
+
+    private static void gatherAllIngredients(List<Potion> potions) {
+        List<CompletableFuture<Potion>> futureList = new ArrayList<>();
+        potions.forEach((potion) -> {
+            CompletableFuture<Potion> futurePotion = CompletableFuture.supplyAsync(() -> {
+                try {
+                    System.out.println(String.format("Begin of collecting ingredients for the potion %s...",
+                            potion.getName()));
+                    Thread.sleep(1000 * potion.getRequiredIngredients());
+                    System.out.println(String.format("Ingredients for the potion %s is collected.",
+                            potion.getName()));
+                } catch (InterruptedException e) {
+                    throw new RuntimeException(e);
+                }
+                return potion;
+            });
+            futureList.add(futurePotion);
+        });
+        AtomicInteger totalQuantity = new AtomicInteger(0);
+        futureList.forEach(future -> totalQuantity.addAndGet(future.join().getRequiredIngredients()));
+        System.out.println(String.format("Total amount of ingredients collected: %d", totalQuantity.get()));
+    }
+}
