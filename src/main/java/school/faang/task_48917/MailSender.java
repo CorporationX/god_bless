c3d5bbaafaca3b2159f2abad19ac31f33f5992ce
vvz3zzv <vladislav.v.zhurbin@gmail.com>
2024-12-26 22:54:23 +0300

    BJS2-48917: Вы работаете в Microsoft!
    - Обработал исключение

--- src/main/java/school/faang/task_48917/MailSender.java
+++ src/main/java/school/faang/task_48917/MailSender.java
@@ -1,11 +1,14 @@
 package school.faang.task_48917;
 
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
 public class MailSender {
     public static final int THREADS_QUANTITY = 5;
     public static final int MAILS_QUANTITY = 1000;
     public static final int MAILS_PER_THREAD = MAILS_QUANTITY / THREADS_QUANTITY;
 
-    public static void main(String[] args) throws InterruptedException {
+    public static void main(String[] args) {
         Thread[] threads = new Thread[THREADS_QUANTITY];
         int[] threadIndexes = new int[THREADS_QUANTITY];
         for (int i = 0; i < THREADS_QUANTITY; i++) {
@@ -17,10 +20,14 @@ public class MailSender {
         }
 
         int counter = 0;
-        for (Thread thread : threads) {
-            thread.join(3000);
-            System.out.printf("Mails from thread %d had sent!\n\n", threadIndexes[counter]);
-            counter++;
+        try {
+            for (Thread thread : threads) {
+                thread.join(3000);
+                System.out.printf("Mails from thread %d had sent!\n\n", threadIndexes[counter]);
+                counter++;
+            }
+        } catch (InterruptedException e) {
+            log.info("Unknown case exception: {}", String.valueOf(e));
         }
         System.out.println("All mails had sent!");
     }
