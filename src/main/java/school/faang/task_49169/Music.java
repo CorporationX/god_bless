0887438ef71407a0fc72b1e85758232cc860d843
Sergey Senin <04sersen@gmail.com>
2024-12-26 10:38:00 +0300

    Реализована многопоточная система управления музыкальным плеером с синхронизацией потоков.

---
+++ src/main/java/school/faang/task_49169/Music.java
@@ -0,0 +1,32 @@
+package school.faang.task_49169;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+public class Music {
+    public static void main(String[] args) {
+        Player player = new Player();
+
+        try (ExecutorService executor = Executors.newFixedThreadPool(4)) {
+
+        executor.execute(() -> safeExecute(player::play));
+        executor.execute(() -> safeExecute(player::pause));
+        executor.execute(() -> safeExecute(player::skip));
+        executor.execute(() -> safeExecute(player::previous));
+
+        executor.execute(() -> safeExecute(player::play));
+        executor.execute(() -> safeExecute(player::pause));
+
+        } catch (Exception e) { // Обработка возможных ошибок
+            System.out.println("Error while working with threads: " + e.getMessage());
+        }
+    }
+
+    private static void safeExecute(Runnable task) {
+        try {
+            task.run();
+        } catch (Exception e) {
+            System.out.println("Error in the stream: " + e.getMessage());
+        }
+    }
+}
