f0c15d7c79dbadece228fae423821a2d67d4ed42
Artemon <artemon.kos@yandex.ru>
2024-12-26 19:28:14 +0100

    checkstyle

--- src/main/java/school/faang/bjs248281/util/Constants.java
+++ src/main/java/school/faang/bjs248281/util/Constants.java
@@ -3,5 +3,5 @@ package school.faang.bjs248281.util;
 import school.faang.bjs248281.model.Location;
 
 public class Constants {
-    public static final Location WITCHER_CASTLE = new Location(0,0);
+    public static final Location WITCHER_CASTLE = new Location(0, 0);
 }
