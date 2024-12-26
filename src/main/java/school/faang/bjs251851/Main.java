3a9cc24708545c35d57417a4ab611358c5b6df5a
Parvin Etibarli <etibarliparvin@gmail.com>
2024-12-26 11:16:18 +0400

    bjs2-51851
    * Качаем алхимию is done

---
+++ src/main/java/school/faang/bjs251851/Main.java
@@ -0,0 +1,39 @@
+package school.faang.bjs251851;
+
+import java.util.List;
+import java.util.concurrent.CompletableFuture;
+
+public class Main {
+
+    public static void main(String[] args) {
+        final long start = System.currentTimeMillis();
+        List<Potion> potions = List.of(
+                new Potion("Healing Potion", 5),
+                new Potion("Mana Potion", 3),
+                new Potion("Stamina Potion", 4)
+        );
+
+        List<CompletableFuture<Integer>> futures = potions.stream()
+                .map(potion -> CompletableFuture.supplyAsync(() -> gatherIngredients(potion)))
+                .toList();
+
+        Integer totalIngredients = CompletableFuture.allOf(futures.toArray(new CompletableFuture[0]))
+                .thenApply(v -> futures.stream()
+                        .map(CompletableFuture::join)
+                        .reduce(0, Integer::sum))
+                .join();
+        System.out.println("Total ingredients: " + totalIngredients);
+        long end = System.currentTimeMillis();
+        System.out.println("time elapsed: " + (end - start));
+    }
+
+    public static int gatherIngredients(Potion potion) {
+        try {
+            Thread.sleep(potion.getRequiredIngredients() * 1000L);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return potion.getRequiredIngredients();
+    }
+
+}
---
+++ src/main/java/school/faang/bjs251851/Potion.java
@@ -0,0 +1,13 @@
+package school.faang.bjs251851;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class Potion {
+
+    private String name;
+    private int requiredIngredients;
+
+}
