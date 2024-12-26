14b3080f72168e72856320eada0ddf796dd0d97e
shamshaev <an.shamshaev@gmail.com>
2024-12-26 14:55:13 +0300

    Add requested changes

--- src/main/java/school/faang/task_49290/Main.java
+++ src/main/java/school/faang/task_49290/Main.java
@@ -5,11 +5,13 @@ import lombok.extern.slf4j.Slf4j;
 import java.util.Random;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
 
 @Slf4j
 public class Main {
     private static final int THREAD_COUNT = 2;
     private static final int DELAY = 2000;
+    private static final Random RANDOM = new Random();
 
     public static void main(String[] args) {
         Player john = new Player("john", 1);
@@ -26,20 +28,27 @@ public class Main {
 
         while (game.getPlayers().size() > 1) {
             Player[] currentPlayers = game.getPlayers().toArray(new Player[0]);
-            Random random = new Random();
-            int playerIndex = random.nextInt(currentPlayers.length);
+            int playerIndex = RANDOM.nextInt(currentPlayers.length);
             Player player = currentPlayers[playerIndex];
-            player.setAlive(random.nextBoolean());
+            player.setAlive(RANDOM.nextBoolean());
 
             try {
                 Thread.sleep(DELAY);
             } catch (InterruptedException e) {
                 log.warn("Main thread was interrupted", e);
+                System.exit(0);
             }
 
             service.submit(() -> game.update(player));
         }
 
         service.shutdown();
+        try {
+            if (!service.awaitTermination(DELAY, TimeUnit.MILLISECONDS)) {
+                service.shutdownNow();
+            }
+        } catch (InterruptedException e) {
+            service.shutdownNow();
+        }
     }
 }
