fa29a99216d8b41ab6dec6860c357645fde3b90d
Rafis <rafissabitov@gmail.com>
2024-12-26 10:45:15 -0600

    BJS2-48755 You work in Microsoft

---
+++ src/main/java/school/faang/sprint3/workmicrosoft/MailSender.java
@@ -0,0 +1,24 @@
+package school.faang.sprint3.workmicrosoft;
+
+public class MailSender {
+    public static void main(String[] args) throws InterruptedException {
+        int totalMessages = 1000;
+        int threadsCount = 5;
+        int batchSize = totalMessages / threadsCount;
+
+        Thread[] threads = new Thread[threadsCount];
+
+        for (int i = 0; i < threadsCount; i++) {
+            int start = i * batchSize;
+            int end = (i + 1) * batchSize;
+            threads[i] = new Thread(new SenderRunnable(start, end));
+            threads[i].start();
+        }
+
+        for (Thread thread : threads) {
+            thread.join();
+        }
+
+        System.out.println("All emails have been sent successfully!");
+    }
+}
---
+++ src/main/java/school/faang/sprint3/workmicrosoft/SenderRunnable.java
@@ -0,0 +1,19 @@
+package school.faang.sprint3.workmicrosoft;
+
+public class SenderRunnable implements Runnable {
+    private final int startIndex;
+    private final int endIndex;
+
+    public SenderRunnable(int startIndex, int endIndex) {
+        this.startIndex = startIndex;
+        this.endIndex = endIndex;
+    }
+
+    @Override
+    public void run() {
+        for (int i = startIndex; i < endIndex; i++) {
+            System.out.println("Email " + i  + " successfully sent");
+        }
+
+    }
+}
