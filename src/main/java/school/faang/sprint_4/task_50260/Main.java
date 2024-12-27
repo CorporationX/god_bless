659f44dce22f1b1d1a8c420182a37d2e68d57b71
rostex <rostex2@yandex.ru>
2024-12-27 18:34:29 +0300

    Внесены изменения после ревью

--- src/main/java/school/faang/sprint_4/task_50260/Main.java
+++ src/main/java/school/faang/sprint_4/task_50260/Main.java
@@ -17,14 +17,14 @@ public class Main {
         List<Integer> numbers = List.of(11, 12, 13, 18, 19, 20, 25);
         List<CompletableFuture<BigInteger>> futureList = Factorial.factorials(numbers);
         AtomicInteger counter = new AtomicInteger(0);
-        List<CompletableFuture<Void>> tasks = new ArrayList<>();
+        List<CompletableFuture<Void>> futures = new ArrayList<>();
 
         log.info("Количество доступных процессоров при расчете факториалов: {}", THREADS_SIZE);
         ExecutorService executor = Executors.newFixedThreadPool(THREADS_SIZE);
 
         for (int i = 0; i < numbers.size(); i++) {
             int index = i;
-            CompletableFuture<Void> task = CompletableFuture.supplyAsync(() -> {
+            CompletableFuture<Void> future = CompletableFuture.supplyAsync(() -> {
                 try {
                     return futureList.get(index).join();
                 } catch (CompletionException e) {
@@ -35,16 +35,17 @@ public class Main {
                 log.info("Факториал числа {} это {}", numbers.get(index), factorial);
                 counter.incrementAndGet();
             });
-            tasks.add(task);
+            futures.add(future);
         }
 
-        CompletableFuture<Void> allOfTasks = CompletableFuture.allOf(tasks.toArray(new CompletableFuture[0]));
-        try {
-            allOfTasks.join();
-            log.info("Все асинхронные задачи завершены. Итераций ожидания: {}", counter.get());
-            executor.shutdown();
-        } catch (CompletionException e) {
-            log.error("Ошибка при ожидании завершения всех задач");
-        }
+        CompletableFuture.allOf(futures.toArray(new CompletableFuture[0]))
+                .whenComplete((result, throwable) -> {
+                    if (throwable == null) {
+                        log.info("Все асинхронные задачи завершены. Итераций ожидания: {}", counter.get());
+                        executor.shutdown();
+                    } else {
+                        log.error("Возникла ошибка при выполнении задач: {}", throwable.getMessage());
+                    }
+                }).join();
     }
 }
