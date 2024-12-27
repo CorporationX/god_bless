968989a3dd1680792a960d8085f9816f6b5fa674
Alexey Gorokhov <almigor@mail.ru>
2024-12-27 17:00:35 +0300

    Правки после code-review

--- src/main/java/school/faang/sprint4/task50970/Main.java
+++ src/main/java/school/faang/sprint4/task50970/Main.java
@@ -2,6 +2,9 @@ package school.faang.sprint4.task50970;
 
 import lombok.extern.slf4j.Slf4j;
 import school.faang.sprint4.task50970.model.Kingdom;
+
+import java.util.ArrayList;
+import java.util.List;
 import java.util.concurrent.CompletableFuture;
 
 @Slf4j
@@ -12,20 +15,25 @@ public class Main {
         Kingdom kingdom1 = new Kingdom("kingdom of crooked mirrors");
         Kingdom kingdom2 = new Kingdom("Monako");
 
+        List<CompletableFuture<String>> ravenResults = new ArrayList<>();
+
         for (int i = 0; i < MAX_RAVENS; i++) {
-            sendRaven(kingdom1, kingdom2)
+            CompletableFuture<String> ravenResult = sendRaven(kingdom1, kingdom2)
                     .handle((result, exception) -> {
                         if (exception != null) {
-                            log.info("Ворон не смог доставить сообщение из замка {} в замок {}",
+                            log.error("Ворон не смог доставить сообщение из замка {} в замок {}",
                                     kingdom1.getName(), kingdom2.getName());
                         } else {
                             log.info("Ворон доставил сообщение из замка {} в замок {}",
                                     kingdom1.getName(), kingdom2.getName());
                         }
                         return result;
-                    })
-                    .join();
+                    });
+            ravenResults.add(ravenResult);
         }
+
+        CompletableFuture.allOf(ravenResults.toArray(new CompletableFuture[ravenResults.size()])).join();
+
     }
 
     public static CompletableFuture<String> sendRaven(Kingdom sender, Kingdom receiver) {
