993b6197ca6b709931194dfcd574cfb1281447a9
sergeybabaev96 <166114022+sergeybabaev96@users.noreply.github.com>
2024-12-27 12:15:55 +0300

    Изменил логику выполнения, название переменных и убрал дублирование кода. Теперь программа работает через пул потоков, а действия пользователей находятся в списке и вызываются рандомно

--- src/main/java/school/faang/bjs249347/Music.java
+++ src/main/java/school/faang/bjs249347/Music.java
@@ -1,19 +1,36 @@
 package school.faang.bjs249347;
 
+import java.util.List;
+import java.util.Random;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
+import java.util.function.Consumer;
+
 public class Music {
+
     public static void main(String[] args) {
         Player player = new Player();
+        List<Consumer<Player>> userActions = List.of(Player::play, Player::pause, Player::previous, Player::skip);
 
-        Thread user1 = new Thread(player::play, "Sergey");
-        user1.start();
+        Random random = new Random();
+        ScheduledExecutorService service = Executors.newScheduledThreadPool(10);
 
-        Thread user2 = new Thread(player::pause, "Sasha");
-        user2.start();
+        service.scheduleAtFixedRate(() -> {
+            int randomAction = random.nextInt(0, userActions.size());
+            Consumer<Player> userAction = userActions.get(randomAction);
+            userAction.accept(player);
+        }, 0, 1, TimeUnit.SECONDS);
 
-        Thread user3 = new Thread(player::skip, "Ruslan");
-        user3.start();
-
-        Thread user4 = new Thread(player::previous, "Elena");
-        user4.start();
+        try {
+            if (service.awaitTermination(10, TimeUnit.SECONDS)) {
+                System.out.println("All tasks completed");
+            } else {
+                service.shutdown();
+                System.out.println("Forced termination");
+            }
+        } catch (InterruptedException e) {
+            System.out.println("Not all tasks were completed");
+        }
     }
 }
