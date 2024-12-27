b7877d0dbfaa30c21bf34a771503c4a5b26c9934
muslim <xammuslim06@gmail.com>
2024-12-27 20:41:29 +0300

    Wow,fixed code style

--- src/main/java/school/faang/wow/QuestSystem.java
+++ src/main/java/school/faang/wow/QuestSystem.java
@@ -10,7 +10,8 @@ public class QuestSystem {
                 System.out.println(player.getName() + " начинает квест: " + quest.getName());
                 Thread.sleep(quest.getDifficulty() * 1000);
                 player.increaseExperience(quest.getReward());
-                System.out.println(player.getName() + " завершил квест: " + quest.getName() + "  и получил " + quest.getReward() + " опыта.");
+                System.out.println(player.getName() + " завершил квест: " + quest.getName() + "  и получил " + quest
+                        .getReward() + " опыта.");
             } catch (InterruptedException e) {
                 Thread.currentThread().interrupt();
                 System.out.println(" Квест был прерван для игрока: " + player.getName());
