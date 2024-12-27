9ee50ca3b0dd31a94f6df32782eb5bd1b9e475cf
Alfir Rakhimov <exprnc7@gmail.com>
2024-12-27 17:34:23 +0500

    Поправил

--- src/main/java/school/faang/task_50273/Factorial.java
+++ src/main/java/school/faang/task_50273/Factorial.java
@@ -5,59 +5,58 @@ import lombok.extern.slf4j.Slf4j;
 import java.math.BigInteger;
 import java.util.List;
 import java.util.concurrent.CompletableFuture;
+import java.util.stream.IntStream;
 
 @Slf4j
 public class Factorial {
     private static final int MAX_INT_FACTORIAL = 12;
     private static final int MAX_LONG_FACTORIAL = 19;
 
-    public static int factorialInt(int n) {
-        if (n < 0 || n > MAX_INT_FACTORIAL) {
-            throw new IllegalArgumentException("Your number must be greater than "
-                    + "0 and less than " + MAX_INT_FACTORIAL);
-        }
-        int factorial = n;
-        for (int i = 2; i < n; i++) {
-            factorial *= i;
+    private static BigInteger calculateFactorial(int n) {
+        BigInteger factorial = BigInteger.ONE;
+        for (int i = 1; i <= n; i++) {
+            factorial = factorial.multiply(BigInteger.valueOf(i));
         }
         return factorial;
+    }
+
+    public static int factorialInt(int n) {
+        if (n < 0 || n > MAX_INT_FACTORIAL) {
+            throw new IllegalArgumentException("Your number must be greater than -1 and less than "
+                    + MAX_INT_FACTORIAL + 1);
+        }
+        return IntStream.rangeClosed(1, n)
+                .reduce(1, (a, b) -> a * b);
     }
 
     public static long factorialLong(int n) {
         if (n >= 0 && n <= MAX_INT_FACTORIAL) {
             return factorialInt(n);
         } else if (n < 0 || n > MAX_LONG_FACTORIAL) {
-            throw new IllegalArgumentException("Your number must be greater than "
-                    + "0 and less than " + MAX_LONG_FACTORIAL);
+            throw new IllegalArgumentException("Your number must be greater than -1 and less than "
+                    + MAX_LONG_FACTORIAL + 1);
         }
-        long factorial = n;
-        for (int i = 2; i < n; i++) {
-            factorial *= i;
-        }
-        return factorial;
+        return IntStream.rangeClosed(1, n)
+                .mapToLong(number -> number)
+                .reduce(1L, (a, b) -> a * b);
     }
 
     public static BigInteger factorialBig(int n) {
         if (n < 0) {
-            throw new IllegalArgumentException("Your number must be greater than 0");
+            throw new IllegalArgumentException("Your number must be greater than -1");
         } else if (n <= MAX_LONG_FACTORIAL) {
             return BigInteger.valueOf(factorialLong(n));
         }
-
-        BigInteger factorial = BigInteger.valueOf(n);
-        for (int i = 2; i < n; i++) {
-            factorial = factorial.multiply(BigInteger.valueOf(i));
-        }
-        return factorial;
+        return calculateFactorial(n);
     }
 
     public static List<CompletableFuture<BigInteger>> factorials(List<Integer> numbers) {
-        return numbers.stream().map(number -> {
-            return CompletableFuture.supplyAsync(() -> {
-                BigInteger factorial = factorialBig(number);
-                log.info("Factorial of {}: {}", number, factorial);
-                return factorial;
-            });
-        }).toList();
+        return numbers.stream()
+                .map(number -> CompletableFuture.supplyAsync(() -> {
+                    BigInteger factorial = factorialBig(number);
+                    log.info("Factorial of {}: {}", number, factorial);
+                    return factorial;
+                }))
+                .toList();
     }
 }
--- src/main/java/school/faang/task_50273/Main.java
+++ src/main/java/school/faang/task_50273/Main.java
@@ -9,7 +9,7 @@ import java.util.concurrent.CompletableFuture;
 @Slf4j
 public class Main {
     public static void main(String[] args) {
-        List<Integer> numbers = List.of(12, 5, 23, 99999, 3, 11);
+        List<Integer> numbers = List.of(12, 5, 23, 99999, 3, 11, 0);
         List<CompletableFuture<BigInteger>> futureFactorials = Factorial.factorials(numbers);
 
         CompletableFuture<Void> factorialsResult = CompletableFuture.allOf(
