5fc006f4e367fa245ad7a6f5911a09e38ae52b90
lehaps <awda.96@bk.ru>
2024-12-27 10:01:35 +0300

    исправил замечания

--- src/main/java/school/faang/task49885/FindingPi.java
+++ src/main/java/school/faang/task49885/FindingPi.java
@@ -4,7 +4,6 @@ import lombok.extern.slf4j.Slf4j;
 
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.ThreadLocalRandom;
-import java.util.concurrent.atomic.AtomicInteger;
 import java.util.stream.IntStream;
 
 @Slf4j
@@ -12,25 +11,24 @@ public class FindingPi {
     private static final int SIDE_LENGTH_SQUARE = 1;
     private static final int MULTIPLIER_FOR_PI = 4;
 
-    private final AtomicInteger allPoints = new AtomicInteger(0);
-    private final AtomicInteger insidePoints = new AtomicInteger(0);
-
-    public double calculatePi(int n) {
-        if (n < 1) {
+    public double calculatePi(int countPoints) {
+        if (countPoints < 1) {
             throw new IllegalArgumentException("Число точек не может быть меньше 1");
         }
 
-        CompletableFuture.runAsync(() -> IntStream.range(0, n)
-                .forEach(i -> {
+        CompletableFuture<Long> countInside = CompletableFuture.supplyAsync(() -> IntStream.range(0, countPoints)
+                .filter(i -> {
                     Point point = new Point(ThreadLocalRandom.current().nextDouble(SIDE_LENGTH_SQUARE),
                             ThreadLocalRandom.current().nextDouble(SIDE_LENGTH_SQUARE));
-                    if (point.isInsideCircle()) {
-                        insidePoints.getAndIncrement();
-                    }
-                    allPoints.getAndIncrement();
-                })
-        ).join();
 
-        return (double) (MULTIPLIER_FOR_PI * insidePoints.intValue()) / allPoints.intValue();
+                    return isInsideCircle(point);
+                })
+                .count());
+
+        return (double) (MULTIPLIER_FOR_PI * countInside.join()) / countPoints;
+    }
+
+    private boolean isInsideCircle(Point point) {
+        return Math.pow(point.x(), 2) + Math.pow(point.y(), 2) <= 1;
     }
 }
--- src/main/java/school/faang/task49885/Point.java
+++ src/main/java/school/faang/task49885/Point.java
@@ -2,8 +2,4 @@ package school.faang.task49885;
 
 public record Point(double x,
                     double y) {
-
-    public boolean isInsideCircle() {
-        return x * x + y * y <= 1;
-    }
 }
