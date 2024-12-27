283418e086b5150e1fc37fe28c14fa1f238dc975
Kir34 <kirnovikof1@gmail.com>
2024-12-27 23:25:27 +0300

    задание с плеером спотифай

--- src/main/java/school/faang/task_bjs249257/Music.java
+++ src/main/java/school/faang/task_bjs249257/Music.java
@@ -1,19 +1,24 @@
 package school.faang.task_bjs249257;
 
+import java.util.Arrays;
+import java.util.List;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
 public class Music {
     private static final int POOL_SIZE = 4;
+    private static final int TEST_CYCLES_COUNT = 8;
 
     public static void main(String[] args) {
         Player player = new Player();
         ExecutorService executorService = Executors.newFixedThreadPool(POOL_SIZE);
-        for (int i = 0; i < 8; i++) {
-            executorService.submit(player::pause);
-            executorService.submit(player::play);
-            executorService.submit(player::skip);
-            executorService.submit(player::previous);
+        List<Runnable> actions = Arrays.asList(player::pause, player::play,
+                player::skip, player::previous);
+
+        for (int i = 0; i < TEST_CYCLES_COUNT; i++) {
+            for (Runnable action : actions) {
+                executorService.submit(action);
+            }
         }
         executorService.shutdown();
     }
