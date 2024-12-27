31366b731d7599a85ceefa4f73865cef10e39415
Alexkid <alexkid2001@rambler.ru>
2024-12-27 00:07:00 -0330

    test commit

---
+++ src/main/java/school/faang/task_49801/Boss.java
@@ -0,0 +1,15 @@
+package school.faang.task_49801;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+public class Boss {
+    private final int maxPlayers = 5;
+    private int currentPlayers = 0;
+
+    public void joinBattle(Player player) {
+        if (currentPlayers < maxPlayers) {
+            log
+        }
+    }
+}
