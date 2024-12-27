f4483a4c236ef4e7956ff07b78e8b4a29a1b933c
Kirill Pankov <uralostrov6@gmail.com>
2024-12-27 22:05:50 +0500

    Gmail Rich Filters fix getter setter

--- src/main/java/gmail_rich_filters/Email.java
+++ src/main/java/gmail_rich_filters/Email.java
@@ -13,28 +13,4 @@ public class Email {
         this.body = body;
         this.important = important;
     }
-
-    public String getSubject() {
-        return subject;
-    }
-
-    public void setSubject(String subject) {
-        this.subject = subject;
-    }
-
-    public String getBody() {
-        return body;
-    }
-
-    public void setBody(String body) {
-        this.body = body;
-    }
-
-    public boolean isImportant() {
-        return important;
-    }
-
-    public void setImportant(boolean important) {
-        this.important = important;
-    }
 }
