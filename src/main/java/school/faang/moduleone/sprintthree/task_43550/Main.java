746a9351723a9d42c7e195f38d7fff4baa6f709b
ivans <ivan_semenov@bk.ru>
2024-12-27 20:00:48 +0300

    BJS2-48045 [Распределенная армия героев меча и магии] Исправление замечаний

--- src/main/java/school/faang/moduleone/sprintthree/task_43550/Main.java
+++ src/main/java/school/faang/moduleone/sprintthree/task_43550/Main.java
@@ -3,11 +3,11 @@ package school.faang.moduleone.sprintthree.task_43550;
 public class Main {
     public static void main(String[] args) throws InterruptedException {
         Army army = new Army();
-        army.addUnit(new Archer(25));   // считаем в потоке 1
-        army.addUnit(new Swordsman(40)); // считаем в потоке 2
-        army.addUnit(new Mage(50));     // считаем в потоке 3
-        army.addUnit(new Mage(20));     // считаем в потоке 4
-
+        army.addUnit(new Archer(25));
+        army.addUnit(new Swordsman(40));
+        army.addUnit(new Mage(50));
+        army.addUnit(new Mage(20));
+        
         int totalPower = army.calculateTotalPower();
         System.out.println("Общая сила армии: " + totalPower);
     }
