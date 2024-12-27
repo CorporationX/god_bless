82067749fdc1d7b08b868367972911e92084c385
askar0103 <ananaspineapple2048@gmail.com>
2024-12-27 16:19:28 +0300

    Solution to task BJS2-49903

---
+++ src/main/java/school/faang/task_49903/Main.java
@@ -0,0 +1,8 @@
+package school.faang.task_49903;
+
+public class Main {
+    public static void main(String[] args) {
+        MasterCardService service = new MasterCardService();
+        service.doAll();
+    }
+}
