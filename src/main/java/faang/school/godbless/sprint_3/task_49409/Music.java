651462c00e2e467c62475000ecadb3d876d20e1a
Anastasia Dragomirova <nastay.dr2002@gmail.com>
2024-12-27 21:59:52 +0400

    Внесла изменения по замечаниям

--- src/main/java/faang/school/godbless/sprint_3/task_49409/Music.java
+++ src/main/java/faang/school/godbless/sprint_3/task_49409/Music.java
@@ -1,21 +1,37 @@
 package faang.school.godbless.sprint_3.task_49409;
 
+import java.util.Random;
+
 public class Music {
 
     private static final int NUMBER_OF_THREADS = 7;
 
+    private static final int NUMBER_OF_PLAYER_METHODS = 4;
+
     public static void main(String[] args) throws InterruptedException {
         Player player = new Player();
 
+        Random random = new Random();
+
         Thread[] threads = new Thread[NUMBER_OF_THREADS];
 
-        threads[0] = new Thread(player::play);
-        threads[1] = new Thread(player::previous);
-        threads[2] = new Thread(player::skip);
-        threads[3] = new Thread(player::pause);
-        threads[4] = new Thread(player::skip);
-        threads[5] = new Thread(player::play);
-        threads[6] = new Thread(player::previous);
+        for (int i = 0; i < threads.length; i++) {
+            int methodNumber = random.nextInt(NUMBER_OF_PLAYER_METHODS);
+            switch (methodNumber) {
+                case (0):
+                    threads[i] = new Thread(player::play);
+                    break;
+                case (1):
+                    threads[i] = new Thread(player::pause);
+                    break;
+                case (2):
+                    threads[i] = new Thread(player::skip);
+                    break;
+                case (3):
+                    threads[i] = new Thread(player::previous);
+                    break;
+            }
+        }
 
         for (Thread thread : threads) {
             thread.start();
