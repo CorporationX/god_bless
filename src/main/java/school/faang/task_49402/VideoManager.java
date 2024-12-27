68db77f30ba36e8ca737b654f525d4eef72d3936
Dmitriy <dda20040609@gmail.com>
2024-12-27 17:39:14 +0300

    commit 1 Счетчик просмотров на YouTube

---
+++ src/main/java/school/faang/task_49402/VideoManager.java
@@ -0,0 +1,16 @@
+package school.faang.task_49402;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class VideoManager {
+    private final Map<String, Integer> viewsMap = new HashMap<>();
+
+    public synchronized void addView(String videoId) {
+        viewsMap.put(videoId, viewsMap.getOrDefault(videoId, 0) + 1);
+    }
+
+    public synchronized int getViewCount(String videoId) {
+        return viewsMap.getOrDefault(videoId, 0);
+    }
+}
