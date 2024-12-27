b550dd8f2b827af4ea4c40a0b72bdf3c6dad8dc0
shamshaev <an.shamshaev@gmail.com>
2024-12-27 23:40:40 +0300

    Solve task_50037

---
+++ src/main/java/school/faang/task_50037/Player.java
@@ -0,0 +1,14 @@
+package school.faang.task_50037;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@AllArgsConstructor
+@Getter
+@Setter
+public class Player {
+    private final String name;
+    private int level;
+    private int experience;
+}
