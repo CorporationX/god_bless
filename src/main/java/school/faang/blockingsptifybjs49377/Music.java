447bf968770520c8edebc6174fac5a25dbc508e6
Ilya <thomas.moody@mail.ru>
2024-12-27 17:32:47 +0300

    автоматизировал работу с потоками

--- src/main/java/school/faang/blockingsptifybjs49377/Music.java
+++ src/main/java/school/faang/blockingsptifybjs49377/Music.java
@@ -2,27 +2,28 @@ package school.faang.blockingsptifybjs49377;
 
 public class Music {
     public static void main(String[] args) {
-        Player player = new Player();
+        Player musicPlayer = new Player();
 
-        final Thread thread1 = new Thread(() -> player.play());
-        final Thread thread2 = new Thread(() -> player.pause());
-        final Thread thread3 = new Thread(() -> player.skip());
-        final Thread thread4 = new Thread(() -> player.previous());
+        Runnable[] actions = {
+                musicPlayer::play,
+                musicPlayer::pause,
+                musicPlayer::skip,
+                musicPlayer::previous
+        };
 
-        thread1.start();
-        thread2.start();
-        thread3.start();
-        thread4.start();
-
-        try {
-            thread1.join();
-            thread2.join();
-            thread3.join();
-            thread4.join();
-        } catch (InterruptedException e) {
-            e.printStackTrace();
+        Thread[] actionThreads = new Thread[actions.length];
+        for (int i = 0; i < actions.length; i++) {
+            actionThreads[i] = new Thread(actions[i]);
+            actionThreads[i].start();
         }
 
+        for (Thread actionThread : actionThreads) {
+            try {
+                actionThread.join();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
         System.out.println("All action completed");
     }
 }
