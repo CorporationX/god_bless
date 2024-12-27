de5647f6be2a5a09a170ec9319fdf14e378835df
RustBad <rustambad1993@gmail.com>
2024-12-27 16:59:10 +0500

    task_48741

---
+++ src/main/java/school/faang/task_48741/MailSender.java
@@ -0,0 +1,23 @@
+package school.faang.task_48741;
+
+public class MailSender {
+    public static void main(String[] args) throws InterruptedException {
+        int totalMessages = 1000;
+        int threadCount = 5;
+        int batchSize = totalMessages / threadCount;
+
+        Thread[] threads = new Thread[threadCount];
+        for (int i = 0; i < threadCount; i++) {
+            int start = i * batchSize;
+            int end = (i + 1) * batchSize;
+            threads[i] = new Thread(new SenderRunnable(start, end));
+            threads[i].start();
+        }
+
+        for (Thread thread : threads) {
+            thread.join();
+        }
+        System.out.println("Все письма отправлены!");
+    }
+
+}
