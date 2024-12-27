502428e715b86c605818c9a72851e5d0044be65e
IgnatovEI <ignatovei96@mail.ru>
2024-12-27 15:29:06 +0300

    добавил константу

--- src/main/java/school/faang/task_48889/Chore.java
+++ src/main/java/school/faang/task_48889/Chore.java
@@ -1,6 +1,7 @@
 package school.faang.task_48889;
 
 public class Chore implements Runnable {
+    private static final int SLEEP_TIME_MS = 1000;
     private final String chore;
 
     public Chore(String chore) {
@@ -11,7 +12,7 @@ public class Chore implements Runnable {
     public void run() {
         try {
             System.out.println(Thread.currentThread().getName() + " выполняет задачу " + chore);
-            Thread.sleep(1000);
+            Thread.sleep(SLEEP_TIME_MS);
             System.out.println(Thread.currentThread().getName() + " завершил задачу " + chore);
         } catch (InterruptedException e) {
             System.err.println("Задача была прервана: " + chore);
