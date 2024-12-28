2f756f129da6aadde1da8c9865c787b4a16d2ae7
Anastasia Dragomirova <nastay.dr2002@gmail.com>
2024-12-28 18:46:50 +0400

    BJS2-48271 — Путь пальцем проложи… (#16666)

---
+++ src/main/java/faang/school/godbless/sprint_3/task_48271/City.java
@@ -0,0 +1,11 @@
+package faang.school.godbless.sprint_3.task_48271;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@AllArgsConstructor
+@Getter
+public class City {
+    private final String name;
+    private final Location location;
+}
