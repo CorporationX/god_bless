caccac8716cde431d419158bb37b17ffc60e2b3b
OxanaNovozhilova <nov2303@mail.ru>
2024-12-28 06:27:50 +0500

    BJS2-50826 Качаем алхимию (#16545)
    * BJS2-50826
    
    * BJS2-50826 update after review
    
    ---------
    
    Co-authored-by: User Novozhilova <nov2303@mail,ru>

---
+++ src/main/java/school/faang/task_50826/Potion.java
@@ -0,0 +1,11 @@
+package school.faang.task_50826;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@AllArgsConstructor
+@Getter
+public class Potion {
+    private String name;
+    private int requiredIngredients;
+}
