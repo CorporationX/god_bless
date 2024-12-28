60cad16f219378809c6e37e07b0c601e1970db64
Viktor <vikt.ovod@outlook.com>
2024-12-29 02:03:57 +0300

    использовал log из @Slf4j и упростил создание изменяемого списка

--- src/main/java/school/faang/sprint3/bjs249018/GooglePhotoAutoUploader.java
+++ src/main/java/school/faang/sprint3/bjs249018/GooglePhotoAutoUploader.java
@@ -1,13 +1,14 @@
 package school.faang.sprint3.bjs249018;
 
-import java.util.List;
-import java.util.stream.Collectors;
-import java.util.stream.Stream;
+import lombok.extern.slf4j.Slf4j;
 
+import java.util.ArrayList;
+import java.util.List;
+
+@Slf4j
 public class GooglePhotoAutoUploader {
     private final Object lock = new Object();
-    private final List<String> photosToUpload = Stream.of("C:/photos/001.jpg", "C:/photos/002.jpg")
-            .collect(Collectors.toList());
+    private final List<String> photosToUpload = new ArrayList<>(List.of("C:/photos/001.jpg", "C:/photos/002.jpg"));
 
     public void startAutoUpload() {
         synchronized (lock) {
@@ -15,9 +16,9 @@ public class GooglePhotoAutoUploader {
                 try {
                     lock.wait();
                 } catch (InterruptedException e) {
-                    System.err.println("Ошибка при ожидании потока: " + e.getMessage());
+                    log.error("Ошибка при ожидании потока: ", e);
                 } catch (Exception e) {
-                    System.err.println("Непредвиденная ошибка: " + e.getMessage());
+                    log.error("Непредвиденная ошибка: ", e);
                 }
             }
             uploadPhotos();
@@ -27,7 +28,7 @@ public class GooglePhotoAutoUploader {
     public void onNewPhotoAdded(String photoPath) {
         synchronized (lock) {
             photosToUpload.add(photoPath);
-            System.out.println("Фото '" + photoPath + "' было добавлено на устройство.");
+            log.info("Фото '{}' было добавлено на устройство.", photoPath);
             lock.notify();
         }
     }
@@ -35,11 +36,11 @@ public class GooglePhotoAutoUploader {
     public void uploadPhotos() {
         synchronized (lock) {
             for (String photo : photosToUpload) {
-                System.out.println("Фото '" + photo + "' загружено.");
+                log.info("Фото '{}' загружено.", photo);
             }
-            System.out.println("Все фотографии успешно загружены на сервер.");
+            log.info("Все фотографии успешно загружены на сервер.");
             photosToUpload.clear();
-            System.out.println("Фотографии удалены с устройства.\n");
+            log.info("Фотографии удалены с устройства.\n");
         }
     }
 }
