ccd9dd5ee68e8a9f0e8dc9a21ecdd8ef2d288628
Natalya Vasileva <boryantheone228@yandex.ru>
2024-12-28 22:29:39 +0300

    BJS2-50768 Добавление решение задачи Carry отожрался

---
+++ src/main/java/school/faang/task_50768/Inventory.java
@@ -0,0 +1,52 @@
+package school.faang.task_50768;
+
+import lombok.extern.slf4j.Slf4j;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.CompletableFuture;
+
+@Slf4j
+public class Inventory {
+    private static final long PICK_UP_TIME = 1000;
+    private List<Item> items;
+
+    public Inventory() {
+        this.items = new ArrayList<>();
+    }
+
+    public void addItem(Item item) {
+        if (item == null) {
+            throw new IllegalArgumentException("Некорретное значение для предмета!");
+        }
+
+        items.add(item);
+    }
+
+    public Item combineItems(Item firstItem, Item secondItem) {
+        return new Item(firstItem.name() + secondItem.name(), firstItem.power() + secondItem.power());
+    }
+
+    public CompletableFuture<Item> getItemFromChest() {
+        return CompletableFuture.supplyAsync(() -> {
+            getItem();
+            return new Item("Item from chest", 100);
+        });
+    }
+
+    public CompletableFuture<Item> getItemFromStore() {
+        return CompletableFuture.supplyAsync(() -> {
+            getItem();
+            return new Item("Item from store", 150);
+        });
+    }
+
+    private static void getItem() {
+        try {
+            Thread.sleep(PICK_UP_TIME);
+        } catch (InterruptedException e) {
+            Thread.currentThread().interrupt();
+            log.error("Поток был прерван при выполнении метода sleep()", e);
+        }
+    }
+}
---
+++ src/main/java/school/faang/task_50768/InventoryManager.java
@@ -0,0 +1,27 @@
+package school.faang.task_50768;
+
+import lombok.extern.slf4j.Slf4j;
+
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutionException;
+
+@Slf4j
+public class InventoryManager {
+
+    public static void main(String[] args) {
+        Inventory inventory = new Inventory();
+
+        CompletableFuture<Void> result = inventory.getItemFromStore()
+                .thenCombine(inventory.getItemFromChest(), inventory::combineItems)
+                .thenCompose(item -> CompletableFuture.runAsync(() -> {
+                    inventory.addItem(item);
+                    log.info("Скомбинированный предмет {} положен в инвентарь", item.toString());
+                }));
+
+        try {
+            result.get();
+        } catch (InterruptedException | ExecutionException e) {
+            log.error("Не удалось получить результат асинхронного комбинирования предметов", e);
+        }
+    }
+}
