cfdb7573fcc1f536b01f2bcdd069322f8ef9488e
Parvin Etibarli <etibarliparvin@gmail.com>
2024-12-28 13:39:58 +0400

    bjs2-49926
    * Fan out – Fan In is done

---
+++ src/main/java/school/faang/bjs249926/ResultConsumer.java
@@ -0,0 +1,21 @@
+package school.faang.bjs249926;
+
+import java.util.concurrent.atomic.AtomicLong;
+
+public class ResultConsumer {
+
+    private final AtomicLong sumOfSquaredNumbers;
+
+    ResultConsumer(Long init) {
+        sumOfSquaredNumbers = new AtomicLong(init);
+    }
+
+    public Long add(final Long num) {
+        return sumOfSquaredNumbers.addAndGet(num);
+    }
+
+    public Long getSum() {
+        return sumOfSquaredNumbers.get();
+    }
+
+}
