2cf395014a58bce40bb90c967e513837e1bb7cf7
Artem Kopanev <artemkopanev757@gmail.com>
2024-12-28 15:54:29 +0300

    BJS2-50575

---
+++ src/main/java/school/faang/sprint4/bjs_50575/Notification.java
@@ -0,0 +1,11 @@
+package school.faang.sprint4.bjs_50575;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@AllArgsConstructor
+@Getter
+public class Notification {
+    private final int id;
+    public final String message;
+}
