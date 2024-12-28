1a20fa898b994c0052c460bcfea682f1c090f749
Parvin Etibarli <etibarliparvin@gmail.com>
2024-12-28 15:04:13 +0400

    bjs2-50591
    * Superheroes battle is done

---
+++ src/main/java/school/faang/bjs250591/Pair.java
@@ -0,0 +1,13 @@
+package school.faang.bjs250591;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class Pair<F, S> {
+
+    public final F first;
+    public final S second;
+
+}
