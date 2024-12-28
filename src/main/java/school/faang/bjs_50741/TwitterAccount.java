98edc599c504f35d47e9db6a1ebf212ec6b41e11
Anton Graf <ant0ngraf@yandex.ru>
2024-12-28 11:03:36 +0600

    Добавляет класс твиттер-аккаунта

---
+++ src/main/java/school/faang/bjs_50741/TwitterAccount.java
@@ -0,0 +1,13 @@
+package school.faang.bjs_50741;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import lombok.Setter;
+
+@RequiredArgsConstructor
+@Getter
+@Setter
+public class TwitterAccount {
+    private final String userName;
+    private int followers;
+}
