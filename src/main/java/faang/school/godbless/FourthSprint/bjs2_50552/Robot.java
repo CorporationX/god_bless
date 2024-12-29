bd1aba2321efc49e21b2508cebf971ca3aff2ca4
Garib_Aghayev <garibaghayev@gmail.com>
2024-12-29 05:52:40 +0300

    Star Wars Arena first release

---
+++ src/main/java/faang/school/godbless/FourthSprint/bjs2_50552/Robot.java
@@ -0,0 +1,15 @@
+package faang.school.godbless.FourthSprint.bjs2_50552;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+import java.util.concurrent.atomic.AtomicInteger;
+
+@AllArgsConstructor
+@Getter
+public class Robot {
+    private String name;
+    private int attackPower;
+    private int defencePower;
+
+}
