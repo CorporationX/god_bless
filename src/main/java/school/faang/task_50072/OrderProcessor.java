fff5e0414e09f8f7a3a974582de1a91d46fc0651
OxanaNovozhilova <nov2303@mail.ru>
2024-12-29 13:50:56 +0500

    BJS2-50072 Собираем заказы в Amazon (#16612)
    * BJS2-50072
    
    * BJS2-50072 update after review
    
    ---------
    
    Co-authored-by: User Novozhilova <nov2303@mail,ru>

---
+++ src/main/java/school/faang/task_50072/OrderProcessor.java
@@ -0,0 +1,39 @@
+package school.faang.task_50072;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.atomic.AtomicInteger;
+
+public class OrderProcessor {
+    private static final long TIME_SLEEP = 1000;
+    private static AtomicInteger totalProcessedOrders = new AtomicInteger();
+
+    public void processAllOrders(List<Order> orders) {
+        List<CompletableFuture<Order>> futureList = new ArrayList<>();
+        orders.stream().map(this::processOrder).forEach(future -> futureList.add(future));
+        futureList.forEach(future -> future.join());
+        System.out.println(String.format("Total amount of processed orders: %d", totalProcessedOrders.get()));
+    }
+
+    private CompletableFuture<Order> processOrder(Order order) {
+        return CompletableFuture.supplyAsync(() -> {
+            try {
+                System.out.println(String.format("Begin of processing order %d, status %s...",
+                        order.getId(), order.getStatus()));
+                Thread.sleep(TIME_SLEEP * order.getId());
+                order.setStatus(Status.PROCESSED.getTitle());
+                addTotalProcessedOrders();
+                System.out.println(String.format("End of processing order %d, status %s.",
+                        order.getId(), order.getStatus()));
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+            return order;
+        });
+    }
+
+    private void addTotalProcessedOrders() {
+        totalProcessedOrders.incrementAndGet();
+    }
+}
