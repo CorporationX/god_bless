9e2615c7fca78449a10b2a81ab7ebe32fd32a2af
Максим <vint_vin@mail.ru>
2024-12-29 22:29:55 +1000

    Исправил

--- src/main/java/school/faang/sprint4/task50103wow/Main.java
+++ src/main/java/school/faang/sprint4/task50103wow/Main.java
@@ -19,7 +19,7 @@ public class Main {
                 + " has completed the quest and now has " + player.getExperience() + " experience points."));
 
         player2Quest.thenAccept(player -> System.out.println(player.getName()
-                + " has completed the quest and now has " + player.getExperience() + " experience points."));
+                + " has completed the quest and now has " + player.getExperience() + " experience points"));
 
         player1Quest.join();
         player2Quest.join();
