4f9cef108a669b7a745ebf7211a4c5e9703cb293
Parvin Etibarli <etibarliparvin@gmail.com>
2024-12-29 11:56:24 +0400

    bjs2-50772
    * Fixed as per comment

--- src/main/java/school/faang/bjs251814/Student.java
+++ src/main/java/school/faang/bjs251814/Student.java
@@ -1,20 +1,26 @@
 package school.faang.bjs251814;
 
-import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
 
+import java.util.concurrent.atomic.AtomicInteger;
+
 @Getter
 @Setter
-@AllArgsConstructor
 public class Student {
 
     private String name;
     private int year;
-    private int points;
+    private AtomicInteger points;
+
+    public Student(String name, int year, int points) {
+        this.name = name;
+        this.year = year;
+        this.points = new AtomicInteger(points);
+    }
 
     public void addPoints(int points) {
-        this.points += points;
+        this.points.addAndGet(points);
     }
 
 }
