296913734db2f4945c9f3a6586f9880e0f0b2007
oxana <nov2303@mail.ru>
2024-12-29 13:06:14 +0500

    BJS2-50072 update after review

--- src/main/java/school/faang/task_50072/OrderProcessor.java
+++ src/main/java/school/faang/task_50072/OrderProcessor.java
@@ -9,8 +9,11 @@ public class OrderProcessor {
     private static final long TIME_SLEEP = 1000;
     private static AtomicInteger totalProcessedOrders = new AtomicInteger();
 
-    private void addTotalProcessedOrders() {
-        totalProcessedOrders.incrementAndGet();
+    public void processAllOrders(List<Order> orders) {
+        List<CompletableFuture<Order>> futureList = new ArrayList<>();
+        orders.stream().map(this::processOrder).forEach(future -> futureList.add(future));
+        futureList.forEach(future -> future.join());
+        System.out.println(String.format("Total amount of processed orders: %d", totalProcessedOrders.get()));
     }
 
     private CompletableFuture<Order> processOrder(Order order) {
@@ -19,7 +22,7 @@ public class OrderProcessor {
                 System.out.println(String.format("Begin of processing order %d, status %s...",
                         order.getId(), order.getStatus()));
                 Thread.sleep(TIME_SLEEP * order.getId());
-                order.setStatus(Status.PROCESSED.getTitle().toString());
+                order.setStatus(Status.PROCESSED.getTitle());
                 addTotalProcessedOrders();
                 System.out.println(String.format("End of processing order %d, status %s.",
                         order.getId(), order.getStatus()));
@@ -30,10 +33,7 @@ public class OrderProcessor {
         });
     }
 
-    public void processAllOrders(List<Order> orders) {
-        List<CompletableFuture<Order>> futureList = new ArrayList<>();
-        orders.stream().map(order -> processOrder(order)).forEach(future -> futureList.add(future));
-        futureList.forEach(future -> future.join());
-        System.out.println(String.format("Total amount of processed orders: %d", totalProcessedOrders.get()));
+    private void addTotalProcessedOrders() {
+        totalProcessedOrders.incrementAndGet();
     }
 }
