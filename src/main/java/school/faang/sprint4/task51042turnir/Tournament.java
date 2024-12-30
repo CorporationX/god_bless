e7c5e94efe1d742e6f053c5db8a5ebd26f3992f3
Максим <vint_vin@mail.ru>
2024-12-30 12:50:37 +1000

    Готово

--- src/main/java/school/faang/sprint4/task51042turnir/Tournament.java
+++ src/main/java/school/faang/sprint4/task51042turnir/Tournament.java
@@ -1,12 +1,10 @@
 package school.faang.sprint4.task51042turnir;
 
-import java.util.List;
 import java.util.concurrent.CompletableFuture;
-import java.util.stream.Collectors;
 
 public class Tournament {
     public CompletableFuture<School> startTask(School school, Task task) {
-        CompletableFuture<School> taskFuture = CompletableFuture.supplyAsync(() -> {
+        return CompletableFuture.supplyAsync(() -> {
             try {
                 Thread.sleep(1000 * task.getDifficult());
             } catch (InterruptedException e) {
@@ -17,6 +15,5 @@ public class Tournament {
             }
             return school;
         });
-        return taskFuture;
     }
 }
