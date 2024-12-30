7f6ad586d06a387a7c5b270ad724f0537e32b15f
Максим <vint_vin@mail.ru>
2024-12-30 12:49:18 +1000

    Готово

--- src/main/java/school/faang/sprint4/task51042turnir/Tournament.java
+++ src/main/java/school/faang/sprint4/task51042turnir/Tournament.java
@@ -7,17 +7,16 @@ import java.util.stream.Collectors;
 public class Tournament {
     public CompletableFuture<School> startTask(School school, Task task) {
         CompletableFuture<School> taskFuture = CompletableFuture.supplyAsync(() -> {
-                    try {
-                        Thread.sleep(1000 * task.getDifficult());
-                    } catch (InterruptedException e) {
-                        throw new RuntimeException(e);
-                    }
-                    for (Student student : school.getTeam()) {
-                        student.addPoints(task.getReward());
-                    }
-                    return school;
-                }
-        );
+            try {
+                Thread.sleep(1000 * task.getDifficult());
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+            for (Student student : school.getTeam()) {
+                student.addPoints(task.getReward());
+            }
+            return school;
+        });
         return taskFuture;
     }
 }
