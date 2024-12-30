bfdf2521bfd8535da9a0be5cdfdd6bee7ebe72fc
sergeybabaev96 <166114022+sergeybabaev96@users.noreply.github.com>
2024-12-30 22:02:38 +0300

    Исправил checkStyle

--- src/main/java/school/faang/bjs250160/AmazonProcessingOrder.java
+++ src/main/java/school/faang/bjs250160/AmazonProcessingOrder.java
@@ -5,12 +5,11 @@ import java.util.List;
 public class AmazonProcessingOrder {
     public static void main(String[] args) {
         OrderProcessor orderProcessor = new OrderProcessor();
-
         List<Order> orders = List.of(new Order(1, OrderStatus.NEW),
                 new Order(2, OrderStatus.NEW),
                 new Order(3, OrderStatus.NEW));
 
-       orders.forEach(orderProcessor::processOrder);
-       orderProcessor.totalProcess();
+        orders.forEach(orderProcessor::processOrder);
+        orderProcessor.totalProcess();
     }
 }
