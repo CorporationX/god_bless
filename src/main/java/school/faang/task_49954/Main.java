ebbba53ca23a855da3a1fc6c1b531a1cdc3eefb7
Leysan Pirkina <missleysan1609@gmail.com>
2024-12-30 22:23:38 +0300

    задача асинхронность и future

---
+++ src/main/java/school/faang/task_49954/Main.java
@@ -0,0 +1,9 @@
+package school.faang.task_49954;
+
+public class Main {
+
+    public static void main(String[] args) {
+        MasterCardService service = new MasterCardService();
+        service.doAll();
+    }
+}
