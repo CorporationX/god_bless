9f7e20bd9864fa3c482eb979b82adacf2bf8e2f3
Ivan Baev <ibai86@yandex.ru>
2024-12-31 16:35:54 +0300

    initial commit

---
+++ src/main/java/school/faang/send_raven/RavenPostService.java
@@ -0,0 +1,18 @@
+package school.faang.send_raven;
+
+import lombok.extern.slf4j.Slf4j;
+
+import java.util.concurrent.CompletableFuture;
+
+@Slf4j
+public class RavenPostService {
+    public CompletableFuture<String> sendRaven(Kingdom sender, Kingdom receiver) {
+        return CompletableFuture.supplyAsync(() -> {
+            try{
+                return sender.sendMessage(receiver);
+            } catch (Exception e) {
+                throw new RuntimeException(e.getMessage());
+            }
+        });
+    }
+}
