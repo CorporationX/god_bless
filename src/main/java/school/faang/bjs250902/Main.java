e48e75fd093b131c85ac057c399d616efea49385
lcodio <lcodio@temp.com>
2024-12-31 23:37:41 +0300

    bjs2-50902 fix

--- src/main/java/school/faang/bjs250902/Main.java
+++ src/main/java/school/faang/bjs250902/Main.java
@@ -9,20 +9,20 @@ import java.util.concurrent.atomic.AtomicInteger;
 @Slf4j
 public class Main {
     private static final int INIT_NUMBER = 0;
+    private static final AtomicInteger totalNumberOfIngredients = new AtomicInteger(INIT_NUMBER);
 
     public static void main(String[] args) {
         Gathering gathering = new Gathering();
         List<Potion> potions = List.of(
                 new Potion("Healing Potion", 9),
                 new Potion("Big Potion", 20),
-                new Potion("Stamina Potion", 5)
+                new Potion("Stamina Potion", 5),
+                new Potion("Kix Potion", 5)
         );
 
         List<CompletableFuture<Integer>> completableFutures = potions.stream()
                 .map(gathering::gatherIngredients)
                 .toList();
-
-        AtomicInteger totalNumberOfIngredients = new AtomicInteger(INIT_NUMBER);
 
         completableFutures.forEach(f -> totalNumberOfIngredients.addAndGet(f.join()));
 
