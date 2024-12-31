3bff20734bd49e2ae6b4435cef962a564026fe6b
Timofey Khutoryanskiy <tiroxxxin@gmail.com>
2024-12-31 17:14:44 +0300

    tiroxin BJS2-50889

---
+++ src/main/java/school/faang/sprint_4/task_50889/Student.java
@@ -0,0 +1,17 @@
+package school.faang.sprint_4.task_50889;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@AllArgsConstructor
+@Getter
+@Setter
+@ToString
+public class Student {
+    private String name;
+    private int year;
+    private int points;
+
+}
