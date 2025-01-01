1ed2c63a81d20aed08133ee5c97b4b1eb85eb415
TFdevQA <46944859+TFdevQA@users.noreply.github.com>
2024-12-31 16:31:54 -0800

    task 50713: Twitter followers

---
+++ src/main/java/school/faang/sprint4/bjs_50713/TwitterAccount.java
@@ -0,0 +1,13 @@
+package school.faang.sprint4.bjs_50713;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+public class TwitterAccount {
+    private String userName;
+    private int followers;
+}
---
+++ src/main/java/school/faang/sprint4/bjs_50713/TwitterSubscriptionSystem.java
@@ -0,0 +1,26 @@
+package school.faang.sprint4.bjs_50713;
+
+import lombok.extern.slf4j.Slf4j;
+
+import java.util.concurrent.CompletableFuture;
+
+@Slf4j
+public class TwitterSubscriptionSystem {
+    private static final int SLEEP_TIME = 1000;
+
+    public CompletableFuture<Void> followAccount(TwitterAccount account) {
+        return CompletableFuture.runAsync(() -> {
+            try {
+                Thread.sleep(SLEEP_TIME);
+                addFollower(account);
+            } catch (InterruptedException e) {
+                Thread.currentThread().interrupt();
+                log.error("Process was interrupted");
+            }
+        });
+    }
+
+    private synchronized void addFollower(TwitterAccount account) {
+        account.setFollowers(account.getFollowers() + 1);
+    }
+}
