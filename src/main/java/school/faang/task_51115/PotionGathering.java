55eeb035deb11cd920a1504ddc42cb60f82965b9
narol01 <kmingogod@gmail.com>
2025-01-01 09:57:25 +0100

    BJS2-51115 code after review

--- src/main/java/school/faang/task_51115/PotionGathering.java
+++ src/main/java/school/faang/task_51115/PotionGathering.java
@@ -4,13 +4,15 @@ import lombok.extern.slf4j.Slf4j;
 
 import java.util.List;
 import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.atomic.AtomicInteger;
 
 @Slf4j
 public class PotionGathering {
     private static final long SLEEP_TIME = 1000;
+    private final AtomicInteger totalIngredients = new AtomicInteger(0);
 
     public int gatherIngredients(List<Potion> potions) {
-        return potions.stream()
+        List<CompletableFuture<Void>> listOfPotion = potions.stream()
                 .map(potion -> CompletableFuture.supplyAsync(() -> {
                     try {
                         Thread.sleep(potion.getRequiredIngredients() * PotionGathering.SLEEP_TIME);
@@ -18,9 +20,14 @@ public class PotionGathering {
                         throw new RuntimeException(e);
                     }
                     return potion.getRequiredIngredients();
-                }))
-                .mapToInt(CompletableFuture::join)
-                .sum();
+                }).thenAccept(totalIngredients::addAndGet))
+                .toList();
+
+        CompletableFuture<Void> allOf = CompletableFuture.allOf(listOfPotion.toArray(new CompletableFuture[0]));
+        allOf.join();
+
+        return totalIngredients.get();
+
     }
 
 }
