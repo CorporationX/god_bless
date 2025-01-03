a5935c88841f02e5421ae2808a11948da554b0bc
Sergey Senin <04sersen@gmail.com>
2025-01-03 20:37:07 +0300

    Добавлена асинхронная обработка платежей и отправка аналитики с использованием Future и CompletableFuture.

---
+++ src/main/java/school/faang/task_49919/Main.java
@@ -0,0 +1,9 @@
+package school.faang.task_49919;
+
+public class Main {
+    public static void main(String[] args) {
+        MasterCardService service = new MasterCardService();
+
+        service.doAll();
+    }
+}
