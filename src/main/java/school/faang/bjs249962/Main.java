178818ffeb7715c174c775e432f5a2503623e4d7
Muhammadsolih <salikhdev@gmail.com>
2025-01-03 17:43:23 +0500

    Асинхронность и Future. Update

--- src/main/java/school/faang/bjs249962/Main.java
+++ src/main/java/school/faang/bjs249962/Main.java
@@ -2,10 +2,7 @@ package school.faang.bjs249962;
 
 public class Main {
     public static void main(String[] args) {
-
         MasterCardService service = new MasterCardService();
-
         service.doAll();
-
     }
 }
