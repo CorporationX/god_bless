d672bf497de46c28e4188b75550bae9deebeaf72
Leonid Zhuravel <zhuravel_leonid@yahoo.com>
2025-01-03 20:43:39 +0200

    updated user class to use optionals

--- src/main/java/school/faang/bjs248887/User.java
+++ src/main/java/school/faang/bjs248887/User.java
@@ -3,38 +3,41 @@ package school.faang.bjs248887;
 import lombok.RequiredArgsConstructor;
 import lombok.SneakyThrows;
 
+import java.util.Optional;
+
 @RequiredArgsConstructor
 public class User {
     private final String name;
-    private String role;
-    private House house;
+    private Optional<String> role;
+    private Optional<House> house;
+    private final Object lock = new Object();
 
     @SneakyThrows
     public void joinHouse(House house) {
-        synchronized (house) {
+        synchronized (lock) {
             while (house.getRolesCounter() < 1) {
                 System.out.printf("%s is in queue. %s has no free roles, "
                         + "waiting for other user to leave...\n", this.name, house.getName());
                 house.wait();
             }
 
-            this.house = house;
-            this.role = house.getRoles().get(0);
-            house.removeRole(this.role);
+            this.house = Optional.of(house);
+            this.role = Optional.ofNullable(house.getRoles().get(0));
+            house.removeRole(this.role.get());
             System.out.printf("%s has entered the : %s, and took role of: %s\n",
                     this.name, house.getName(), this.role);
         }
     }
 
-    public synchronized void leaveHouse() {
-        synchronized (house) {
-            if (this.house != null) {
+    public void leaveHouse() {
+        synchronized (lock) {
+            if (!this.house.isEmpty()) {
                 System.out.printf("%s has left %s, notifying waiters about free role: %s\n",
-                        this.name, house.getName(), this.role);
-                house.addRole(this.role);
-                this.role = null;
+                        this.name, house.get().getName(), this.role);
+                house.get().addRole(this.role.get());
+                this.role =  Optional.empty();
                 house.notifyAll();
-                this.house = null;
+                this.house =  Optional.empty();
             }
         }
     }
