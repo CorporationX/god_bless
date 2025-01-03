42d4c0390725038c551175bf3420808d8221230a
Kairat <snowmango.official@gmail.com>
2025-01-04 00:55:39 +0500

    move to new line

--- src/main/java/school/faang/task_50321/Main.java
+++ src/main/java/school/faang/task_50321/Main.java
@@ -18,10 +18,14 @@ public class Main {
 
         player1Quest.thenAccept(
                 player ->
-                        System.out.println(player.getName() + " has completed the quest and now has " + player.getExperience() + " experience points."));
+                        System.out.println(player.getName()
+                                + " has completed the quest and now has "
+                                + player.getExperience() + " experience points."));
         player2Quest.thenAccept(
                 player ->
-                        System.out.println(player.getName() + " has completed the quest and now has " + player.getExperience() + " experience points."));
+                        System.out.println(player.getName()
+                                + " has completed the quest and now has "
+                                + player.getExperience() + " experience points."));
 
         player2Quest.join();
         player1Quest.join();
