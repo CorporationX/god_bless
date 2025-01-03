33e92af5637d62344641daca8d195e0dd03e4f8c
NiveshOne <Shishov.44@mail.ru>
2025-01-03 19:29:41 +0300

    BJS2-51642: Турнир трех волшебников

--- src/main/java/school/faang/task_51642/School.java
+++ src/main/java/school/faang/task_51642/School.java
@@ -11,7 +11,7 @@ public class School {
     private final String name;
     private List<Student> team;
 
-    public int getTotalPoints(){
+    public int getTotalPoints() {
         return team.stream().mapToInt(Student::getPoints).sum();
     }
 }
