a1e0d61ddfed1e2894da7356b458a10b60702b89
Leonid Zhuravel <zhuravel_leonid@yahoo.com>
2025-01-04 11:19:31 +0200

    PR-ready
    What to do with:
    PotionGathering.java uses unchecked or unsafe operations.
    Note: Recompile with -Xlint:unchecked for details.

--- src/main/java/school/faang/bjs250979/PotionGathering.java
+++ src/main/java/school/faang/bjs250979/PotionGathering.java
@@ -1,8 +1,8 @@
 package school.faang.bjs250979;
 
-import java.lang.reflect.Array;
 import java.util.List;
 import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.atomic.AtomicInteger;
 
 public class PotionGathering {
     public static void main(String[] args) {
@@ -16,15 +16,23 @@ public class PotionGathering {
     }
 
     private static void gatherAllIngredients(List<Potion> potions) {
-        CompletableFuture<Void>[] potionsFutures= potions.stream()
-                .map(potion -> {
-                    return new CompletableFuture<Void>(() -> {
+        AtomicInteger count = new AtomicInteger(0);
 
-                    });
-                })
-                .toArray();
+        CompletableFuture<Void>[] potionsFutures = potions.stream()
+                .map(potion -> CompletableFuture.runAsync(() -> {
+                    try {
+                        Thread.sleep(potion.getRequiredIngredients() * 1000L);
+                        System.out.println("It Took: " + potion.getRequiredIngredients()
+                                + " seconds to gather all ingredients for " + potion.getName());
+                        count.addAndGet(potion.getRequiredIngredients());
+                    } catch (InterruptedException e) {
+                        throw new RuntimeException(e);
+                    }
+                }))
+                .toArray(CompletableFuture[]::new);
 
-        //CompletableFuture[] potionsFutures = new CompletableFuture[potions.size()];
-        CompletableFuture.allOf(potionsFutures);
+        CompletableFuture.allOf(potionsFutures).join();
+
+        System.out.println("Общее количество собранных ингредиентов: " + count);
     }
 }
