466f8126df0a96b25810c7470720d6dfb200c46b
CendalJoe <sova333sova@mail.ru>
2025-01-04 18:32:51 +0300

    tHREADS

---
+++ src/main/java/school/faang/AsyncFuture/Main.java
@@ -0,0 +1,8 @@
+package school.faang.AsyncFuture;
+
+public class Main {
+    public static void main(String[] args) {
+        MasterCardService masterCard = new MasterCardService();
+        masterCard.doAll();
+    }
+}
