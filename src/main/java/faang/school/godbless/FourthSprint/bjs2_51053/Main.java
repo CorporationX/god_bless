01030382cc75eaeb725d8f2c01da8a6415f2851a
Garib_Aghayev <garibaghayev@gmail.com>
2025-01-04 15:45:59 +0300

    Становимся celebrity в Twitter

---
+++ src/main/java/faang/school/godbless/FourthSprint/bjs2_51053/Main.java
@@ -0,0 +1,21 @@
+package faang.school.godbless.FourthSprint.bjs2_51053;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.CompletableFuture;
+
+public class Main {
+    public static void main(String[] args) {
+        TwitterSubscriptionSystem system = new TwitterSubscriptionSystem();
+        TwitterAccount mainAccount = new TwitterAccount("John");
+
+        List<CompletableFuture<Void>> futures = new ArrayList<>();
+
+        for (int i = 0; i < 15; i++) {
+            futures.add(system.followAccount(mainAccount));
+        }
+
+        CompletableFuture.allOf(futures.toArray(new CompletableFuture[0])).join();
+        system.executorShutdown();
+    }
+}
