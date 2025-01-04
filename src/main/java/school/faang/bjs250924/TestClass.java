bc2c0209b3726a78db8f3c6b9e1621eddfb87a05
Leonid Zhuravel <zhuravel_leonid@yahoo.com>
2025-01-04 13:10:20 +0200

    PR-Ready (thread manager added)

--- src/main/java/school/faang/bjs250924/TestClass.java
+++ src/main/java/school/faang/bjs250924/TestClass.java
@@ -7,20 +7,20 @@ import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
 public class TestClass {
-    ExecutorService service = Executors.newFixedThreadPool(4);
+    private static final ExecutorService service = Executors.newFixedThreadPool(4);
 
     public static void main(String[] args) {
         runTest();
+        service.shutdown();
     }
 
     private static void runTest() {
         List<TwitterAccount> accounts = new ArrayList<>();
-        for (int accauntId = 1;  accauntId < 20; accauntId++)
-        {
+        for (int accauntId = 1;  accauntId < 20; accauntId++) {
             accounts.add(new TwitterAccount("user" + accauntId));
         }
 
-        TwitterSubscriptionSystem system = new TwitterSubscriptionSystem();
+        TwitterSubscriptionSystem system = new TwitterSubscriptionSystem(service);
 
         List<CompletableFuture<Void>> futuresList = accounts.stream()
                 .map(account -> system.followAccount(accounts.get(0)))
--- src/main/java/school/faang/bjs250924/TwitterSubscriptionSystem.java
+++ src/main/java/school/faang/bjs250924/TwitterSubscriptionSystem.java
@@ -1,22 +1,28 @@
 package school.faang.bjs250924;
 
 import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutorService;
 import java.util.concurrent.TimeUnit;
 
 public class TwitterSubscriptionSystem {
+    private final ExecutorService executorService;
+
+    public TwitterSubscriptionSystem(ExecutorService executorService) {
+        this.executorService = executorService;
+    }
 
     private void addFollower(TwitterAccount account) {
         account.updateFollowersCount(1);
     }
 
     public CompletableFuture<Void> followAccount(TwitterAccount account) {
-        return CompletableFuture.runAsync(() ->{
+        return CompletableFuture.runAsync(() -> {
             addFollower(account);
             try {
                 TimeUnit.SECONDS.sleep(1);
             } catch (InterruptedException e) {
                 throw new RuntimeException(e);
             }
-        });
+        }, executorService);
     }
 }
