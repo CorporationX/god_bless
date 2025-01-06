cbea49eb4896df78c6462787145577557aa4858d
KsushaTarasova <ksutaras543@mail.ru>
2025-01-06 14:11:38 +0300

    Rename const

--- src/main/java/school/faang/sprint_4/task_43573/QuestSystem.java
+++ src/main/java/school/faang/sprint_4/task_43573/QuestSystem.java
@@ -3,13 +3,13 @@ package school.faang.sprint_4.task_43573;
 import java.util.concurrent.CompletableFuture;
 
 public class QuestSystem {
-    private static final long THREAD_SLEEP = 1_000;
+    private static final long BASE_QUEST_TIME = 1_000;
 
     public CompletableFuture<Player> startQuest(Player player, Quest quest) {
         return CompletableFuture.supplyAsync(() -> {
             System.out.println(player.getName() + " начинает квест " + quest.name());
             try {
-                Thread.sleep(THREAD_SLEEP * quest.difficulty());
+                Thread.sleep(BASE_QUEST_TIME * quest.difficulty());
             } catch (InterruptedException e) {
                 System.out.println("Игрок прервал выполнение квеста!");
             }
