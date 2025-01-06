f274a7b1cc54afc279782a6571d621a70013364d
romenskd <romenskyid@gmail.com>
2025-01-06 01:04:16 +0100

    BJS2-51065 улучшения

--- src/main/java/school/faang/task_51065/Potion.java
+++ src/main/java/school/faang/task_51065/Potion.java
@@ -1,20 +1,24 @@
 package school.faang.task_51065;
 
 import lombok.AllArgsConstructor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 @AllArgsConstructor
 public class Potion {
+    private static final Logger logger = LoggerFactory.getLogger(Potion.class);
     private final String name;
     private final int requiredIngredients;
 
     public int gatherIngredients() {
         try {
-            System.out.println("Collecting ingredients for the potion" + name);
+            logger.info("Collecting ingredients for the potion: {}", name);
             Thread.sleep(requiredIngredients * 1000L);
             return requiredIngredients;
         } catch (InterruptedException e) {
             Thread.currentThread().interrupt();
-            throw new RuntimeException("Произошло прерываение потока ", e);
+            logger.error("Thread was interrupted while gathering ingredients for potion: {}", name, e);
+            throw new RuntimeException("Thread was interrupted", e);
         }
     }
 }
