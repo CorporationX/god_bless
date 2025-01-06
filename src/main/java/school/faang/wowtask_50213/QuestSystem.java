d1f749820176e6b7512ce73852d9542ad1dfb87d
CendalJoe <sova333sova@mail.ru>
2025-01-06 17:23:36 +0300

    quests

--- src/main/java/school/faang/wowtask_50213/QuestSystem.java
+++ src/main/java/school/faang/wowtask_50213/QuestSystem.java
@@ -1,14 +1,19 @@
 package school.faang.wowtask_50213;
 
 import java.util.concurrent.CompletableFuture;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 public class QuestSystem {
+    private static final Logger LOG = Logger.getLogger(QuestSystem.class.getName());
+
     public CompletableFuture<Player> startQuest(Player player, Quest quest) {
         CompletableFuture<Player> thisQuest = CompletableFuture.supplyAsync(() -> {
             try {
                 Thread.sleep(quest.getDifficulty() * 500);
             } catch (InterruptedException e) {
-                throw new RuntimeException(e);
+                LOG.info("Исключение: " + e);
+                Thread.currentThread().interrupt();
             }
             player.setExperience(player.getExperience() + quest.getReward());
             player.setLevel(quest.getDifficulty() / 10);
