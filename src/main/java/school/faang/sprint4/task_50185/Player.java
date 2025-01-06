7ae55f2a1d05683b623634c37f23fb8e649a2986
MykhailoShpakov07 <sotenkomihajlo@gmail.com>
2025-01-06 13:15:21 +0100

    Solved task BJS2-50185 WOW

---
+++ src/main/java/school/faang/sprint4/task_50185/Player.java
@@ -0,0 +1,13 @@
+package school.faang.sprint4.task_50185;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class Player {
+
+    private final String name;
+    private int level;
+    private int experience;
+}
