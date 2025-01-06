f6097938482e2ec39c76f47135dc34d698b8c552
Timofey Khutoryanskiy <103549514+tiroxin@users.noreply.github.com>
2025-01-06 10:18:36 +0300

    tiroxin BJS2-50872 (#16828)

---
+++ src/main/java/school/faang/sprint_4/task_50872/Potion.java
@@ -0,0 +1,12 @@
+package school.faang.sprint_4.task_50872;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class Potion {
+    private String name;
+    private int requiredIngredients;
+
+}
