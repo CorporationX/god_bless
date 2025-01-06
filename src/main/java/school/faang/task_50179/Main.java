2573f624084b03733c056e3d836991648259eb92
Maria DB <mbashkaeva@yandex.ru>
2025-01-06 14:43:39 +0300

    BJS2-50179 - the task "We collect orders on Amazon" fix

--- src/main/java/school/faang/task_50179/Main.java
+++ src/main/java/school/faang/task_50179/Main.java
@@ -4,6 +4,7 @@ import lombok.extern.slf4j.Slf4j;
 
 import java.util.List;
 import java.util.concurrent.CompletableFuture;
+
 @Slf4j
 public class Main {
     public static void main(String[] args) {
