1ede1f5058c6dbbf2bb8c0aea2227f5263e90f78
ivans <ivan_semenov@bk.ru>
2025-01-07 14:22:22 +0300

    BJS2-50094 [WOW] Исправление замечаний

--- src/main/java/school/faang/moduleone/sprintfour/task_43573/Main.java
+++ src/main/java/school/faang/moduleone/sprintfour/task_43573/Main.java
@@ -25,12 +25,12 @@ public class Main {
         CompletableFuture<Player> player1Quest = questSystem.startQuest(player1, quest1);
         CompletableFuture<Player> player2Quest = questSystem.startQuest(player2, quest2);
 
-        player1Quest.thenAccept(player -> System.out.println(player.getName() + " has completed the quest and now has "
-                        + player.getExperience() + " experience points."))
-                .join();
-        player2Quest.thenAccept(player -> System.out.println(player.getName() + " has completed the quest and now has "
-                        + player.getExperience() + " experience points."))
-                .join();
+        CompletableFuture.allOf(
+                player1Quest.thenAccept(player -> System.out.println(player.getName()
+                        + " has completed the quest and now has " + player.getExperience() + " experience points.")),
+                player2Quest.thenAccept(player -> System.out.println(player.getName()
+                        + " has completed the quest and now has " + player.getExperience() + " experience points."))
+        ).join();
 
         shutdownGracefully(executor);
     }
