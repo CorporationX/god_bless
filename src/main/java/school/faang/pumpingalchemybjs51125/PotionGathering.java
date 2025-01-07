add0677cd9e435f9b01caedf77ac1693dcd71223
Ilya <thomas.moody@mail.ru>
2025-01-07 16:06:07 +0300

    исправил ingridients на ingredients - использовал thenAccept вместо .join - добавил задержку в main, чтобы дать время для завершения всех асинхронных задач

--- src/main/java/school/faang/pumpingalchemybjs51125/PotionGathering.java
+++ src/main/java/school/faang/pumpingalchemybjs51125/PotionGathering.java
@@ -10,7 +10,7 @@ import java.util.concurrent.atomic.AtomicInteger;
 
 public class PotionGathering {
     private static final Logger logger = LoggerFactory.getLogger(PotionGathering.class);
-    private static final int COLLECTION_TIME_INGRIDIENTS = 1000;
+    private static final int COLLECTION_TIME_INGREDIENTS = 1000;
 
     public static final int HEALING_POTION = 5;
     public static final int MANA_POTION = 3;
@@ -19,17 +19,17 @@ public class PotionGathering {
     public static final int INITIAL_TOTAL_INGREDIENTS = 0;
     public static final int COMPLETABLE_FUTURE_ARRAY_SIZE = 0;
 
-    public static CompletableFuture<Integer> gatherIngridients(Potion potion) {
+    public static CompletableFuture<Integer> gatherIngredients(Potion potion) {
         return CompletableFuture.supplyAsync(() -> {
             try {
                 logger.info("The collection of ingredients for: " + potion.getName());
-                Thread.sleep(COLLECTION_TIME_INGRIDIENTS);
+                Thread.sleep(COLLECTION_TIME_INGREDIENTS);
             } catch (InterruptedException e) {
                 Thread.currentThread().interrupt();
                 logger.error("The collection of ingredients has been interrupted", e);
                 throw new RuntimeException(e);
             }
-            logger.info("Collected {} ingridients for: {}", potion.getRequiredIngredients(),
+            logger.info("Collected {} ingredients for: {}", potion.getRequiredIngredients(),
                     potion.getName());
             return potion.getRequiredIngredients();
         });
@@ -37,24 +37,24 @@ public class PotionGathering {
 
     public static void gatherAllIngredients(List<Potion> potions) {
         List<CompletableFuture<Integer>> futures = potions.stream()
-                .map(PotionGathering::gatherIngridients)
+                .map(PotionGathering::gatherIngredients)
                 .toList();
 
         CompletableFuture<Void> allOf = CompletableFuture.allOf(futures.toArray(
                 new CompletableFuture[COMPLETABLE_FUTURE_ARRAY_SIZE]));
 
-        allOf.thenRun(() -> {
-            AtomicInteger totalIngridients = new AtomicInteger(INITIAL_TOTAL_INGREDIENTS);
+        allOf.thenAccept(v -> {
+            AtomicInteger totalIngredients = new AtomicInteger(INITIAL_TOTAL_INGREDIENTS);
             futures.forEach(future -> {
                 try {
-                    totalIngridients.addAndGet(future.get());
+                    totalIngredients.addAndGet(future.get());
                 } catch (InterruptedException | ExecutionException e) {
                     Thread.currentThread().interrupt();
                     logger.error("Error when getting the result of collecting ingredients", e);
                 }
             });
-            logger.info("Total number of ingredients collected: {}", totalIngridients.get());
-        }).join();
+            logger.info("Total number of ingredients collected: {}", totalIngredients.get());
+        });
     }
 
     public static void main(String[] args) {
@@ -64,5 +64,12 @@ public class PotionGathering {
                 new Potion("Stamina potion", STAMINA_POTION)
         );
         gatherAllIngredients(potions);
+
+        try {
+            Thread.sleep(5000);
+        } catch (InterruptedException e) {
+            Thread.currentThread().interrupt();
+            logger.error("Main thread interrupted", e);
+        }
     }
 }
