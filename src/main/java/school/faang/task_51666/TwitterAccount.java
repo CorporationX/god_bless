9de91de32c87339aef2af63a4ad9b7fb6eb66bd1
Alexkid <alexkid2001@rambler.ru>
2025-01-06 21:58:58 -0330

    implemented AtomicInteger

--- src/main/java/school/faang/task_51666/TwitterAccount.java
+++ src/main/java/school/faang/task_51666/TwitterAccount.java
@@ -5,21 +5,24 @@ import lombok.Getter;
 import lombok.ToString;
 import lombok.extern.slf4j.Slf4j;
 
+import java.util.concurrent.atomic.AtomicInteger;
+
 @Slf4j
 @Getter
 @ToString
 public class TwitterAccount {
     private String userName;
-    private int followers = 0;
+    private final AtomicInteger followers;
 
     public TwitterAccount(String userName) {
         if (userName.isEmpty()) {
             throw new IllegalArgumentException("UserName cannot be empty");
         }
         this.userName = userName;
+        this.followers = new AtomicInteger(0);
     }
 
     public void addFollower() {
-        this.followers++;
+        followers.incrementAndGet();
     }
 }
