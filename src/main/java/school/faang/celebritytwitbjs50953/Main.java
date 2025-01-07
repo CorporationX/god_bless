d6880d2ed2dea457d25bfc8817ba2b9e35f9970e
Ilya <thomas.moody@mail.ru>
2025-01-07 16:11:55 +0300

    заменил цикл for на Stream API для создания списка CompletableFuture

--- src/main/java/school/faang/celebritytwitbjs50953/Main.java
+++ src/main/java/school/faang/celebritytwitbjs50953/Main.java
@@ -3,17 +3,17 @@ package school.faang.celebritytwitbjs50953;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.CompletableFuture;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 public class Main {
     public static void main(String[] args) {
         TwitterAccount account = new TwitterAccount("example_user");
         TwitterSubscriptionSystem subscriptionSystem = new TwitterSubscriptionSystem();
 
-        List<CompletableFuture<Void>> futures = new ArrayList<>();
-
-        for (int i = 0; i < 10; i++) {
-            futures.add(subscriptionSystem.followAccount(account));
-        }
+        List<CompletableFuture<Void>> futures = IntStream.range(0, 10)
+                .mapToObj(i -> subscriptionSystem.followAccount(account))
+                .collect(Collectors.toList());
 
         CompletableFuture<Void> allOf = CompletableFuture.allOf(futures.toArray(new
                 CompletableFuture[0]));
