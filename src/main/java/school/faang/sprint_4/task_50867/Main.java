f078583670620cbdd07f7ded0755b3d42ce6747c
tatgricenko <tattgricenko@gmail.com>
2025-01-08 02:11:47 +0300

    Make review changes

--- src/main/java/school/faang/sprint_4/task_50867/Main.java
+++ src/main/java/school/faang/sprint_4/task_50867/Main.java
@@ -1,26 +1,33 @@
 package school.faang.sprint_4.task_50867;
 
+import lombok.extern.slf4j.Slf4j;
+
+import java.util.ArrayList;
+import java.util.List;
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.ExecutionException;
+import java.util.stream.IntStream;
 
+@Slf4j
 public class Main {
-    public static void main(String[] args) throws ExecutionException, InterruptedException {
+    public static void main(String[] args){
         TwitterAccount account = new TwitterAccount("user", 15);
 
         TwitterSubscriptionSystem system = new TwitterSubscriptionSystem();
 
-        CompletableFuture<Void> future1 = CompletableFuture.runAsync(
-                () -> system.followAccount(account)
-        );
-        CompletableFuture<Void> future2 = CompletableFuture.runAsync(
-                () -> system.followAccount(account)
-        );
-        CompletableFuture<Void> future3 = CompletableFuture.runAsync(
-                () -> system.followAccount(account)
+        int numberOfFollowersToAdd = 5;
+
+        List<CompletableFuture<Void>> tasks = new ArrayList<>();
+
+        IntStream.range(0, numberOfFollowersToAdd).forEach(i -> tasks.add(
+                CompletableFuture.runAsync(() -> system.followAccount(account)))
         );
 
-        CompletableFuture<Void> allFutures = CompletableFuture.allOf(future1, future2, future3);
-        allFutures.get();
+        try {
+            CompletableFuture.allOf(tasks.toArray(new CompletableFuture[0])).get();
+        } catch (InterruptedException | ExecutionException e) {
+            log.error("Один из потоков прервал работу");
+        }
 
         System.out.println(account);
     }
