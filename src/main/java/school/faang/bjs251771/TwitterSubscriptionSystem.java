e3df4e3ff94dd4a43988e5e6c45e9eb95ec067ff
Maksim-VV <laulmv11@gmail.com>
2025-01-08 22:04:29 +0300

    BJS2-51771, Becoming a celebrity on Twitter  - alpha release - post-review edits, del slf4j

--- src/main/java/school/faang/bjs251771/TwitterSubscriptionSystem.java
+++ src/main/java/school/faang/bjs251771/TwitterSubscriptionSystem.java
@@ -1,12 +1,9 @@
 package school.faang.bjs251771;
 
-import lombok.extern.slf4j.Slf4j;
-
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.ExecutorService;
 
 
-@Slf4j
 public class TwitterSubscriptionSystem {
     private synchronized void addFollower(TwitterAccount account) {
         account.setFollowers(account.getFollowers() + 1);
