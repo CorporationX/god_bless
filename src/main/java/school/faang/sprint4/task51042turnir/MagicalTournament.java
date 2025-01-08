9f07ef1653f9fd6902db8406c9ef196cb81f435f
Максим <vint_vin@mail.ru>
2025-01-08 10:04:06 +1000

    Исправлено

--- src/main/java/school/faang/sprint4/task51042turnir/MagicalTournament.java
+++ src/main/java/school/faang/sprint4/task51042turnir/MagicalTournament.java
@@ -10,7 +10,7 @@ public class MagicalTournament {
     public static void main(String[] args) {
         Tournament tournament = new Tournament();
 
-        List<Student> hogwartsTeam = List.of(new Student("Harry", 5, 0),
+        List<Student> hogwartsTeam = List.of(new Student("Harry", 4, 0),
                 new Student("Hermione", 5, 0));
         List<Student> beauxbatonsTeam = List.of(new Student("Fleur", 6, 0),
                 new Student("Gabrielle", 6, 0));
