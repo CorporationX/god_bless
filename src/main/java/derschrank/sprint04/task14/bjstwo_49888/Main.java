f0bf45e2c24bb286c64674cfbcfd9742a549b17f
Mipusatina <mipspam@mail.ru>
2025-01-08 20:48:04 +0100

    Sprint04. Task14. CompletableFuture. ThreadLocalRandom. Pi. Fix Review #1

--- src/main/java/derschrank/sprint04/task14/bjstwo_49888/Main.java
+++ src/main/java/derschrank/sprint04/task14/bjstwo_49888/Main.java
@@ -1,7 +1,7 @@
 package derschrank.sprint04.task14.bjstwo_49888;
 
 public class Main {
-    private static final int POINTS_FOR_CALCULATING_PI = 1000000;
+    private static final int POINTS_FOR_CALCULATING_PI = 10000;
 
     public static void main(String[] args) {
         Pi pi = new Pi();
--- src/main/java/derschrank/sprint04/task14/bjstwo_49888/Pi.java
+++ src/main/java/derschrank/sprint04/task14/bjstwo_49888/Pi.java
@@ -2,33 +2,28 @@ package derschrank.sprint04.task14.bjstwo_49888;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Map;
 import java.util.Random;
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.ThreadLocalRandom;
-import java.util.concurrent.atomic.AtomicLong;
 
 public class Pi {
-    long countOfPointsInside = 0;
-    long countOfAllPoints = 0;
+    private volatile long countOfPointsInside = 0;
+    private volatile long countOfAllPoints = 0;
 
     public double calculatePi(int n) {
-        List<CompletableFuture<Point>> futures = new ArrayList<>();
+        List<CompletableFuture<Void>> futures = new ArrayList<>();
         for (int i = 0; i < n; i++) {
-            futures.add(CompletableFuture.supplyAsync(this::gereneratPoint));
+            futures.add(
+                    CompletableFuture
+                            .supplyAsync(this::gereneratPoint)
+                            .thenAcceptAsync(this::checkPoint)
+                            .thenRunAsync(this::displayInfo)
+            );
         }
 
-        futures.forEach(future -> {
-            Point point = future.join();
-            countOfAllPoints++;
-            if (point.isInsideCircle()) {
-                countOfPointsInside++;
-            }
+        futures.forEach(CompletableFuture::join);
 
-            System.out.printf("[%d:%d] %f%n", countOfPointsInside, countOfAllPoints,
-                    calculatePi(countOfPointsInside, countOfAllPoints));
-        });
-
+        displayInfo();
         return calculatePi(countOfPointsInside, countOfAllPoints);
     }
 
@@ -41,5 +36,31 @@ public class Pi {
         return new Point(rnd.nextDouble(), rnd.nextDouble());
     }
 
+    private void checkPoint(Point point) {
+        incrementCountOfAllPoints();
+        if (point.isInsideCircle()) {
+            incrementCountOfPointsInside();
+        }
+    }
 
+    private void displayInfo() {
+        long[] counts = getValueOfCounts();
+        countOfPointsInside = counts[0];
+        countOfAllPoints = counts[1];
+
+        System.out.printf("[%d:%d] %f%n", countOfPointsInside, countOfAllPoints,
+                calculatePi(countOfPointsInside, countOfAllPoints));
+    }
+
+    private synchronized void incrementCountOfAllPoints() {
+        countOfAllPoints++;
+    }
+
+    private synchronized void incrementCountOfPointsInside() {
+        countOfPointsInside++;
+    }
+
+    private synchronized long[] getValueOfCounts() {
+        return new long[]{countOfPointsInside, countOfAllPoints};
+    }
 }
