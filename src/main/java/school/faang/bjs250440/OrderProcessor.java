b68a972663752263e8acb9c8e3040f71c7ca12c5
Maksim-VV <laulmv11@gmail.com>
2025-01-08 15:19:02 +0300

    BJS2-50440, We collect orders on Amazon - alpha release - post-review edits

--- src/main/java/school/faang/bjs250440/OrderProcessor.java
+++ src/main/java/school/faang/bjs250440/OrderProcessor.java
@@ -3,32 +3,41 @@ package school.faang.bjs250440;
 import lombok.NonNull;
 import lombok.extern.slf4j.Slf4j;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.atomic.AtomicInteger;
 
 @Slf4j
 public class OrderProcessor {
-    public static final int NUMBER_OPERATION_PAYMENTS = 1_000;
-    public static final int BAD_ORDER = 2;
+    public static final int TIME_FOR_ONE_OPERATION = 1_000;
+    public static final int TYPE_OF_ORDERS_TO_CANCEL = 2;
     private final AtomicInteger totalProcessedOrders = new AtomicInteger(0);
 
     public int processAllOrders(@NonNull List<Order> orders) {
-        orders.stream()
-                .map(this::processOrder)
-                .forEach(CompletableFuture::join);
+
+        List<CompletableFuture<Void>> futuresList = new ArrayList<>();
+        orders.forEach(order -> futuresList.add(processOrder(order)));
+
+        CompletableFuture<?>[] futuresArray = futuresList.toArray(new CompletableFuture[0]);
+        CompletableFuture<Void> allTasks = CompletableFuture.allOf(futuresArray);
+
+        allTasks.thenRun(() -> System.out.println("All orders are processed!"));
+        allTasks.join();
+
         return totalProcessedOrders.get();
     }
 
     private CompletableFuture<Void> processOrder(Order order) {
         return CompletableFuture.runAsync(() -> {
             try {
-                Thread.sleep(NUMBER_OPERATION_PAYMENTS);
-                if (order.getId() % BAD_ORDER == 0) {
+                Thread.sleep(TIME_FOR_ONE_OPERATION);
+                if (order.getId() % TYPE_OF_ORDERS_TO_CANCEL == 0) {
                     order.setStatus(OrderStatus.CANCELED);
                     System.out.println("Order id = " + totalProcessedOrders.intValue() + ", was cancel!");
+                } else {
+                    order.setStatus(OrderStatus.PROCESSED);
                 }
-                order.setStatus(OrderStatus.PROCESSED);
                 totalProcessedOrders.incrementAndGet();
             } catch (InterruptedException ex) {
                 log.error("The task has been aborted!{}", String.valueOf(ex));
