76a7e6cbf85172060530aee456ec1b87f6818e52
Leysan Pirkina <missleysan1609@gmail.com>
2025-01-09 11:38:07 +0300

    задача качаем алхимию исправление

--- src/main/java/school/faang/task_51135/GatheringSystem.java
+++ src/main/java/school/faang/task_51135/GatheringSystem.java
@@ -2,39 +2,32 @@ package school.faang.task_51135;
 
 import java.util.List;
 import java.util.concurrent.CompletableFuture;
-import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 import java.util.concurrent.atomic.AtomicInteger;
 
 public class GatheringSystem {
     private final AtomicInteger totalIngredients = new AtomicInteger(0);
-
-    public CompletableFuture<Integer> gatherIngredients(Potion potion) {
-        return CompletableFuture.supplyAsync(() -> {
-            try {
-                Thread.sleep(2000);
-            } catch (InterruptedException e) {
-                Thread.currentThread().interrupt();
-            }
-            return potion.getRequiredIngredients();
-        });
-    }
+    final ExecutorService executorService = Executors.newFixedThreadPool(3);
 
     public void gatherAllIngredients(List<Potion> potions) {
         List<CompletableFuture<Integer>> futures = potions.stream()
-                .map(this::gatherIngredients)
+                .map(potion -> CompletableFuture.supplyAsync(() -> gatherIngredients(potion), executorService)
+                        .thenApply(totalIngredients::addAndGet))
                 .toList();
 
-        CompletableFuture.allOf(futures.toArray(new CompletableFuture[0]))
-                .thenRun(() -> {
-                    futures.forEach(future -> {
-                        try {
-                            totalIngredients.addAndGet(future.get());
-                        } catch (InterruptedException | ExecutionException e) {
-                            Thread.currentThread().interrupt();
-                        }
-                    });
-                    System.out.println("Total amount of gathered ingredients: "
-                            + totalIngredients.get());
-                }).join();
+        CompletableFuture<Void> all = CompletableFuture.allOf(futures.toArray(new CompletableFuture[0]));
+        all.join();
+        System.out.println("Total amount of gathered ingredients: " + totalIngredients.get());
+        executorService.shutdown();
+    }
+
+    private int gatherIngredients(Potion potion) {
+        try {
+            Thread.sleep(2000);
+        } catch (InterruptedException e) {
+            Thread.currentThread().interrupt();
+        }
+        return potion.requiredIngredients();
     }
 }
--- src/main/java/school/faang/task_51135/Potion.java
+++ src/main/java/school/faang/task_51135/Potion.java
@@ -1,11 +1,4 @@
 package school.faang.task_51135;
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-
-@AllArgsConstructor
-@Getter
-public class Potion {
-    private String name;
-    private int requiredIngredients;
+public record Potion(String name, int requiredIngredients) {
 }
