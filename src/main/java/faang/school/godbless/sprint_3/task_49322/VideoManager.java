08b0e7d463ef86684ae411324e3cf6c9c24afa48
Anastasia Dragomirova <nastay.dr2002@gmail.com>
2025-01-09 21:09:50 +0400

    Внесла изменения по замечаниям

--- src/main/java/faang/school/godbless/sprint_3/task_49322/VideoManager.java
+++ src/main/java/faang/school/godbless/sprint_3/task_49322/VideoManager.java
@@ -10,21 +10,17 @@ public class VideoManager {
 
     private final Map<String, Integer> viewsMap = new HashMap<>();
 
-    public void addView(String videoId) {
-        synchronized (viewsMap) {
-            checkVideoId(videoId);
-            viewsMap.put(videoId, viewsMap.getOrDefault(videoId, 0) + 1);
-        }
+    public synchronized void addView(String videoId) {
+        checkVideoId(videoId);
+        viewsMap.put(videoId, viewsMap.getOrDefault(videoId, 0) + 1);
     }
 
-    public int getViewCount(String videoId) {
-        synchronized (viewsMap) {
-            checkVideoId(videoId);
-            if (!viewsMap.containsKey(videoId)) {
-                throw new IllegalArgumentException("Video ID doesn't exist");
-            }
-            return viewsMap.get(videoId);
+    public synchronized int getViewCount(String videoId) {
+        checkVideoId(videoId);
+        if (!viewsMap.containsKey(videoId)) {
+            throw new IllegalArgumentException("Video ID doesn't exist");
         }
+        return viewsMap.get(videoId);
     }
 
     private void checkVideoId(String videoId) {
