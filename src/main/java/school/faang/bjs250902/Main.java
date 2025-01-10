32aa2e28d1f19f0056652fb0d3c1fbf84150aeb0
lcodio <lcodio@temp.com>
2025-01-10 21:00:46 +0300

    bjs2-50902 fixed

--- src/main/java/school/faang/bjs250902/Main.java
+++ src/main/java/school/faang/bjs250902/Main.java
@@ -4,15 +4,12 @@ import lombok.extern.slf4j.Slf4j;
 
 import java.util.List;
 import java.util.concurrent.CompletableFuture;
-import java.util.concurrent.atomic.AtomicInteger;
 
 @Slf4j
 public class Main {
-    private static final int INIT_NUMBER = 0;
-    private static final AtomicInteger totalNumberOfIngredients = new AtomicInteger(INIT_NUMBER);
-
     public static void main(String[] args) {
         Gathering gathering = new Gathering();
+
         List<Potion> potions = List.of(
                 new Potion("Healing Potion", 9),
                 new Potion("Big Potion", 20),
@@ -21,12 +18,18 @@ public class Main {
         );
 
         List<CompletableFuture<Integer>> completableFutures = potions.stream()
-                .map(gathering::gatherIngredients)
+                .map(potion -> gathering.gatherIngredients(potion)
+                        .thenApply(ingredients -> {
+                            gathering.getTotalNumberOfIngredients().addAndGet(ingredients);
+                            return ingredients;
+                        }))
                 .toList();
 
-        completableFutures.forEach(f -> totalNumberOfIngredients.addAndGet(f.join()));
+        CompletableFuture.allOf(completableFutures.toArray(new CompletableFuture[0])).join();
 
-        int totalNumber = totalNumberOfIngredients.get();
+        int totalNumber = gathering.getTotalNumberOfIngredients().get();
         log.info("Total amount of ingredients collected: {}", totalNumber);
+
+        gathering.shutdownExecutorService();
     }
 }
