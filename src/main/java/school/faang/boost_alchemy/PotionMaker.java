f1eeac1f19fb9d892b1e5c098a79637f01765f29
Ivan Baev <ibai86@yandex.ru>
2025-01-10 22:46:36 +0300

    commit

--- src/main/java/school/faang/boost_alchemy/PotionMaker.java
+++ src/main/java/school/faang/boost_alchemy/PotionMaker.java
@@ -5,6 +5,8 @@ import lombok.extern.slf4j.Slf4j;
 
 import java.util.List;
 import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 import java.util.concurrent.atomic.AtomicInteger;
 
 @Slf4j
@@ -12,23 +14,29 @@ public class PotionMaker {
     private static final long THREAD_SLEEP_TIME = 1000L;
     @Getter
     private final AtomicInteger totalIngredients = new AtomicInteger(0);
-
-    private CompletableFuture<Integer> gatherIngredients(Potion potion) {
-        return CompletableFuture.supplyAsync(() -> {
-            try {
-                Thread.sleep((long) (THREAD_SLEEP_TIME * (Math.random() + 0.1)));
-            } catch (InterruptedException e) {
-                Thread.currentThread().interrupt();
-                log.error("Thread {} error", Thread.currentThread().getName());
-            }
-            log.info("Potion {} counted", potion.getName());
-            return potion.getRequiredIngredients();
-        });
-    }
+    private final ExecutorService executor = Executors.newFixedThreadPool(5);
 
     public void countAllIngredients(List<Potion> potions) {
         potions.stream()
                 .map(this::gatherIngredients)
+                .peek()
                 .forEach(ingredients -> totalIngredients.addAndGet(ingredients.join()));
+    }
+
+    private CompletableFuture<Integer> gatherIngredients(Potion potion) {
+        return CompletableFuture.supplyAsync(() -> {
+            takeTimeForGathering();
+            log.info("Potion {} counted", potion.getName());
+            return potion.getRequiredIngredients();
+        }, executor);
+    }
+
+    private void takeTimeForGathering() {
+        try {
+            Thread.sleep((long) (THREAD_SLEEP_TIME * (Math.random() + 0.1)));
+        } catch (InterruptedException e) {
+            Thread.currentThread().interrupt();
+            log.error("Thread {} error", Thread.currentThread().getName());
+        }
     }
 }
