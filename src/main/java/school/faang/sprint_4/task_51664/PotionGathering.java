53de4d89f5f4507ab72ec0dd02bb0d2a7e5494f6
our ame <you@example.com>
2025-01-10 16:54:26 +0500

    BJS2-51664

---
+++ src/main/java/school/faang/sprint_4/task_51664/PotionGathering.java
@@ -0,0 +1,40 @@
+package school.faang.sprint_4.task_51664;
+
+import java.util.List;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.stream.Collectors;
+
+public class PotionGathering {
+    public static void main(String[] args) {
+        List<Potion> potions = List.of(
+                new Potion("Healing Potion", 5),
+                new Potion("Mana Potion", 3),
+                new Potion("Stamina Potion", 4)
+        );
+        gatherAllIngredients(potions);
+    }
+
+    public static CompletableFuture<Integer> gatherIngredients(Potion potion) {
+        return CompletableFuture.supplyAsync(() -> {
+            try {
+                Thread.sleep(2000);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+            return potion.getRequiredIngredients();
+        });
+    }
+
+    public static void gatherAllIngredients(List<Potion> potions) {
+        AtomicInteger totalIngredients = new AtomicInteger(0);
+
+        List<CompletableFuture<Void>> futures = potions.stream()
+                .map(potion -> gatherIngredients(potion)
+                        .thenAccept(totalIngredients::addAndGet))
+                .collect(Collectors.toList());
+
+        CompletableFuture.allOf(futures.toArray(new CompletableFuture[0])).join();
+        System.out.println("Общее количество собранных ингредиентов: " + totalIngredients.get());
+    }
+}
