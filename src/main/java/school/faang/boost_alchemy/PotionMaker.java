8de4a4ba8f274b19eee8cc37b1fb6307b05064d1
Ivan Baev <ibai86@yandex.ru>
2025-01-10 23:47:58 +0300

    ready to review

--- src/main/java/school/faang/boost_alchemy/PotionMaker.java
+++ src/main/java/school/faang/boost_alchemy/PotionMaker.java
@@ -7,20 +7,26 @@ import java.util.List;
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
 import java.util.concurrent.atomic.AtomicInteger;
 
 @Slf4j
 public class PotionMaker {
     private static final long THREAD_SLEEP_TIME = 1000L;
+    private static final int THREAD_POOL_SIZE = 5;
     @Getter
     private final AtomicInteger totalIngredients = new AtomicInteger(0);
-    private final ExecutorService executor = Executors.newFixedThreadPool(5);
+    private final ExecutorService executor = Executors.newFixedThreadPool(THREAD_POOL_SIZE);
 
     public void countAllIngredients(List<Potion> potions) {
         potions.stream()
-                .map(this::gatherIngredients)
-                .peek()
-                .forEach(ingredients -> totalIngredients.addAndGet(ingredients.join()));
+                .map(potion -> gatherIngredients(potion)
+                        .thenApply(countedIngredients -> {
+                            totalIngredients.addAndGet(countedIngredients);
+                            return countedIngredients;
+                        }))
+                .forEach(CompletableFuture::join);
+        shutdownThreadPool(executor);
     }
 
     private CompletableFuture<Integer> gatherIngredients(Potion potion) {
@@ -37,6 +43,20 @@ public class PotionMaker {
         } catch (InterruptedException e) {
             Thread.currentThread().interrupt();
             log.error("Thread {} error", Thread.currentThread().getName());
+        }
+    }
+
+    private void shutdownThreadPool(ExecutorService executor) {
+        executor.shutdown();
+        try {
+            if (executor.awaitTermination(10, TimeUnit.SECONDS)) {
+                log.info("All tasks completed without errors");
+            } else {
+                executor.shutdownNow();
+            }
+        } catch (InterruptedException e) {
+            log.error("Tasks completed with errors {}", e.getMessage());
+            Thread.currentThread().interrupt();
         }
     }
 }
