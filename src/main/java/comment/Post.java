7f42e731bf2b5cdab339c711ac26ad0acc948880
Artur Berdnikov <prikolovichp@inbox.ru>
2025-01-11 00:06:51 +0300

    task51303 "Оставляем комментарий"

---
+++ src/main/java/comment/Post.java
@@ -0,0 +1,23 @@
+package comment;
+
+import lombok.Getter;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Getter
+public final class Post {
+    private final int id;
+    private final String title;
+    private final String content;
+    private final String author;
+    private final List<Comment> comments;
+
+    public Post(int id, String title, String content, String author) {
+        this.id = id;
+        this.title = title;
+        this.content = content;
+        this.author = author;
+        this.comments = new ArrayList<>();
+    }
+}
