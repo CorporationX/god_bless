cb6b3e4c86cedb4e92cfc239cf2e1cefa8ac6fa8
Kirill Pankov <uralostrov6@gmail.com>
2025-01-12 21:05:23 +0500

    YouTube_view_counter

---
+++ src/main/java/YouTube_view_counter/VideoManager.java
@@ -0,0 +1,19 @@
+package YouTube_view_counter;
+
+import lombok.SneakyThrows;
+
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
+public class VideoManager {
+    private final Map<String, Integer> viewsMap = new ConcurrentHashMap<>();
+
+    @SneakyThrows
+    public void addView(String videoId) {
+        viewsMap.merge(videoId, 1, Integer::sum);
+    }
+
+    public int getViewCount(String videoId) {
+        return viewsMap.getOrDefault(videoId, 0);
+    }
+}
