c93b53d09d1bf191a62e75c0e3aec85830008b62
Dias Kamza <kamzadias31@gmail.com>
2025-01-13 16:16:54 +0500

    Potion and PotionGathering

---
+++ src/main/java/school/faang/task_51653/Potion.java
@@ -0,0 +1,11 @@
+package school.faang.task_51653;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class Potion {
+    private String name;
+    private int requiredIngredients;
+}
