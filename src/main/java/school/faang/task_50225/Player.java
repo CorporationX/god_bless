f5b8f61e3e1a14a51aabe69f2e38865fdf30b632
Dias Kamza <kamzadias31@gmail.com>
2025-01-13 12:06:15 +0500

    WOW

---
+++ src/main/java/school/faang/task_50225/Player.java
@@ -0,0 +1,16 @@
+package school.faang.task_50225;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class Player {
+    private String name;
+    private int level;
+    private int experience;
+
+    public void addExperience(int experience) {
+        this.experience += experience;
+    }
+}
