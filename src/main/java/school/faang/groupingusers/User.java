31ea22b4ef454478d4fb5a24f43f06afa1da44b4
aleksey.kazakov <alxej.kazakov@gmail.ru>
2025-02-02 23:44:14 +0300

    BJS2-56306 Grouping users

--- src/main/java/school/faang/groupingusers/User.java
+++ src/main/java/school/faang/groupingusers/User.java
@@ -4,7 +4,6 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
