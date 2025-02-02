0dd174e45ab884a2bb6ed6b2328179d126be662f
Yaroslav Olkin <shrelok.pupkin@yandex.ru>
2025-02-03 00:41:11 +0300

    исправил отпечатку, добавил lombok, написал проверку имени и возраста, добавил метод toString

--- src/main/java/school/faang/task_56308/Main.java
+++ src/main/java/school/faang/task_56308/Main.java
@@ -8,11 +8,15 @@ import java.util.Map;
 public class Main {
     public static void main(String[] args) {
         List<User> userList = new ArrayList<>();
-        userList.add(new User("Андрей", 19, "Работа 1", "andr.com"));
-        userList.add(new User("Артур", 19, "Работа 2", "art.com"));
-        userList.add(new User("Толя", 33, "Работа-3", "tol.com"));
-        userList.add(new User("Оксана", 33, "Работа 4", "oks.com"));
-        userList.add(new User("Арина", 19, "Работа 5", "ari.com"));
+        try {
+            userList.add(new User("Андрей", 19, "Работа 1", "andr.com"));
+            userList.add(new User("Артур", 19, "Работа 2", "art.com"));
+            userList.add(new User("Толя", 33, "Работа-3", "tol.com"));
+            userList.add(new User("Оксана", 33, "Работа 4", "oks.com"));
+            userList.add(new User("Арина", 19, "Работа 5", "ari.com"));
+        } catch (IllegalAccessError e) {
+            System.out.println("Mistake: " + e.getMessage());
+        }
 
         Map<Integer, List<User>> map = User.groupUsers(userList);
         for (Map.Entry<Integer, List<User>> entry : map.entrySet()) {
