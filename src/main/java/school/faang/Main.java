793d38fc3caa1784fd22227c4db2784a4deb3f02
Sergey Ermoshenko <ermoshenko.work@gmail.com>
2025-02-02 22:34:48 +0300

    initial commit

--- src/main/java/school/faang/Main.java
+++ src/main/java/school/faang/Main.java
@@ -1,20 +1,4 @@
 package school.faang;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Map;
-
 public class Main {
-    public static void main(String[] args) {
-        List<User> users = new ArrayList<>();
-        users.add(new User("Anton", 24, "Yandex", "Kazan"));
-        users.add(new User("Alice", 26, "Google", "Prague"));
-        users.add(new User("James", 24, "Amazon", "LA"));
-
-        Map<Integer, List<User>> groupedUsers = User.groupUsers(users);
-
-        for (var entry : groupedUsers.entrySet()) {
-            System.out.printf("Age %d: %s%n", entry.getKey(), entry.getValue());
-        }
-    }
 }
