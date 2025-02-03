1a6ad9526b7715b5407165b16cea30348ae7cde5
Sergey <bigwave2915@gmail.com>
2025-02-04 00:13:53 +0500

    Добавил класс User and

--- src/main/java/school/faang/group_users_by_age/User.java
+++ src/main/java/school/faang/group_users_by_age/User.java
@@ -1,4 +1,15 @@
 package school.faang.group_users_by_age;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+
 public class User {
+    private final String name;
+    private final int age;
+    private final String workplace;
+    private final String address;
+
 }
