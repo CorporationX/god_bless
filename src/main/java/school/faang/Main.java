b9a4a151616aa64252f806d74384e24b3c3993e7
Ekaterina Ramakoti <zubkova-92@mail.ru>
2025-02-03 22:55:03 +0300

    BJS2-56296

--- src/main/java/school/faang/Main.java
+++ src/main/java/school/faang/Main.java
@@ -1,16 +1,25 @@
 package school.faang;
 
+import java.util.HashSet;
 import java.util.List;
+import java.util.Map;
+import java.util.Set;
 
 public class Main {
-    public static void main(String[] args) {
-        List<User> users = List.of(new User("Павел", 31, "yandex", "Moscow"),
-                new User("Семен", 12, "yandex", "Orlov"),
-                new User("Антон", 75, "yandex", "Krasnodar"),
-                new User("Иван", 12, "yandex", "Adler"),
-                new User("Николай", 57, "yandex", "Moscow"));
-
-        System.out.println(User.groupUsers(users));
-
+  public static void main(String[] args) {
+      List<User> users = List.of(
+                new User(21, "Ivan", 33,
+                        new HashSet<>(Set.of("Чтение", "Футбол"))),
+                new User(21, "Sergey", 33,
+                        new HashSet<>(Set.of("Футбол"))),
+                new User(21, "Sergey", 33,
+                        new HashSet<>(Set.of("Бег", "Футбол"))),
+                new User(21, "Sergey", 33,
+                        new HashSet<>(Set.of("Чтение", "Футбол")))
+        );
+        Set<String> hobbies = new HashSet<>(List.of("Чтение", "Футбол"));
+        Map<User, String> hobby = User.findHobbyLovers(users, hobbies);
+        System.out.println(hobby);
     }
-}
+    }
+
