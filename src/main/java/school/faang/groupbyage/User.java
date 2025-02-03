0324c4d71db636f5841c2519c70f19ec899db05d
Anton Kapshukov <kapshukov.project@gmail.com>
2025-02-03 18:11:11 +0300

    Группировка пользователей по возрасту

---
+++ src/main/java/school/faang/groupbyage/User.java
@@ -0,0 +1,74 @@
+package school.faang.groupbyage;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class User {
+    private String name;
+    private int age;
+    private String workplace;
+    private String address;
+
+    public User(String name, int age, String workplace, String address) {
+        this.name = name;
+        this.address = address;
+        this.workplace = workplace;
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getWorkplace() {
+        return workplace;
+    }
+
+    public void setWorkplace(String workplace) {
+        this.workplace = workplace;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "name='" + name + '\'' +
+                ", age=" + age +
+                ", workplace='" + workplace + '\'' +
+                ", address='" + address + '\'' +
+                '}';
+    }
+
+    public static Map<Integer, List<User>> qroupUsers(List<User> users) {
+        Map<Integer, List<User>> result = new HashMap<>();
+        for (User user : users) {
+            int age = user.getAge();
+            if (!result.containsKey(age)) {
+                result.put(age, new ArrayList<User>());
+            }
+            result.get(age).add(user);
+        }
+        return result;
+    }
+}
