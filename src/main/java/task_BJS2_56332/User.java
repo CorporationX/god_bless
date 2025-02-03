cf597fffc5727dd2ff7b118218a6cd609080257c
Anton Lukyanchikov <antontotenkopf@gmail.com>
2025-02-03 18:13:12 +0300

    Created class User

---
+++ src/main/java/task_BJS2_56332/User.java
@@ -0,0 +1,33 @@
+package task_BJS2_56332;
+
+import java.util.Set;
+
+public class User {
+    private static final Set<String> VALID_JOBS = Set.of("Google", "Uber", "Amazon");
+    private static final Set<String> VALID_ADDRESSES = Set.of("London", "New York", "Amsterdam");
+    private static final int VALID_AGE = 18;
+
+    private String name;
+    private int age;
+    private String job;
+    private String address;
+
+
+    public User(String name, int age, String job, String address) {
+        if (name.isEmpty()) {
+            throw new IllegalArgumentException("The name cannot be empty");
+        }
+        if (age < VALID_AGE) {
+            throw new IllegalArgumentException("Age cannot be less than 18");
+        }
+        if (!VALID_JOBS.contains(job)) {
+            throw new IllegalArgumentException("The place of work is not correct");
+        }
+        if (!VALID_ADDRESSES.contains(address)) {
+            throw new IllegalArgumentException("The location of work is not correct");
+        }
+        this.age = age;
+        this.job = job;
+        this.address = address;
+    }
+}
