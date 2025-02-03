6b47d2cce7e1a6c54ee2dde90f488f8326de1c6c
Yaroslav Olkin <shrelok.pupkin@yandex.ru>
2025-02-03 19:26:50 +0300

    улучшил код

--- src/main/java/school/faang/task_56341/User.java
+++ src/main/java/school/faang/task_56341/User.java
@@ -8,27 +8,46 @@ import java.util.List;
 @ToString
 @Getter
 public class User {
-    private String name;
-    private int age;
-    private String job;
-    private String address;
-    private final List<String> VALID_JOBS = List.of("Google", "Uber", "Amazon");
-    private final List<String> VALID_ADDRESSES = List.of("London", "New York", "Amsterdam");
+    private final String NAME;
+    private final int AGE;
+    private final String JOB;
+    private final String ADDRESS;
+    private static final int MINIMUM_AGE = 18;
+    private static final List<String> VALID_JOBS = List.of("Google", "Uber", "Amazon");
+    private static final List<String> VALID_ADDRESSES = List.of("London", "New York", "Amsterdam");
 
     public User(String name, int age, String job, String address) {
-        if (name == null || name.trim().isEmpty()) {
-            throw new IllegalArgumentException("Name cant be empty");
-        } else if (age < 18) {
-            throw new IllegalArgumentException("The age must not be less than 18");
-        } else if (!VALID_JOBS.contains(job)) {
-            throw new IllegalArgumentException("The place of work must be included in the VALID_JOBS set.");
-        } else if (!VALID_ADDRESSES.contains(address)) {
-            throw new IllegalArgumentException("The address must be contained in the VALID_ADDRESSES set.");
-        }
+        this.NAME = validateName(name);
+        this.AGE = validateAge(age);
+        this.JOB = validateJob(job);
+        this.ADDRESS = validateAddress(address);
+    }
 
-        this.name = name;
-        this.age = age;
-        this.job = job;
-        this.address = address;
+    private String validateName(String name) {
+        if (name == null || name.isBlank()) {
+            throw new IllegalArgumentException("Name cant be empty --> " + name);
+        }
+        return name;
+    }
+
+    private int validateAge(int age) {
+        if (age < MINIMUM_AGE) {
+            throw new IllegalArgumentException("The age must not be less than 18 --> " + age);
+        }
+        return age;
+    }
+
+    private String validateJob(String job) {
+        if (!VALID_JOBS.contains(job)) {
+            throw new IllegalArgumentException("The place of work must be included in the VALID_JOBS set --> " + job);
+        }
+        return job;
+    }
+
+    private String validateAddress(String address) {
+        if (!VALID_ADDRESSES.contains(address)) {
+            throw new IllegalArgumentException("The address must be contained in the VALID_ADDRESSES set --> " + address);
+        }
+        return address;
     }
 }
