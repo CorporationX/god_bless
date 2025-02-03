ad847c96afef61e496a0e7bb5d61242fdd50ba37
Anton Lukyanchikov <antontotenkopf@gmail.com>
2025-02-03 20:13:34 +0300

    refactor task BJS2-56332

--- src/main/java/task_BJS2_56332/Main.java
+++ src/main/java/task_BJS2_56332/Main.java
@@ -4,5 +4,29 @@ public class Main {
     public static void main(String[] args) {
         User correctUser = new User("Anton", 20, "Google", "New York");
         System.out.println(correctUser);
+
+        try {
+            new User(" ", 18, "Uber", "Amsterdam");
+        } catch (IllegalArgumentException nameException) {
+            System.out.println(nameException);
+        }
+
+        try {
+            new User("Anton", 17, "Uber", "Amsterdam");
+        } catch (IllegalArgumentException ageException) {
+            System.out.println(ageException);
+        }
+
+        try {
+            new User("Anton", 18, "Sber", "Amsterdam");
+        } catch (IllegalArgumentException jobPlaceException) {
+            System.out.println(jobPlaceException);
+        }
+
+        try {
+            new User("Anton", 18, "Uber", "Moscow");
+        } catch (IllegalArgumentException jobAddressException) {
+            System.out.println(jobAddressException);
+        }
     }
 }
--- src/main/java/task_BJS2_56332/User.java
+++ src/main/java/task_BJS2_56332/User.java
@@ -1,33 +1,38 @@
 package task_BJS2_56332;
 
+import lombok.Getter;
+import lombok.NonNull;
 import lombok.ToString;
 
 import java.util.Set;
 
+@Getter
 @ToString
 public class User {
     private static final Set<String> VALID_JOBS = Set.of("Google", "Uber", "Amazon");
     private static final Set<String> VALID_ADDRESSES = Set.of("London", "New York", "Amsterdam");
-    private static final int VALID_AGE = 18;
+    private static final int MIN_LEGAL_AGE = 18;
 
-    private String name;
-    private int age;
-    private String job;
-    private String address;
+    private final String name;
+    private final int age;
+    private final String job;
+    private final String address;
 
 
-    public User(String name, int age, String job, String address) {
-        if (name.isEmpty()) {
+    public User(@NonNull String name, int age, @NonNull String job, @NonNull String address) {
+        if (name.isBlank()) {
             throw new IllegalArgumentException("The name cannot be empty");
         }
-        if (age < VALID_AGE) {
-            throw new IllegalArgumentException("Age cannot be less than 18");
+        if (age < MIN_LEGAL_AGE) {
+            throw new IllegalArgumentException(("Age cannot be less than %d").formatted(MIN_LEGAL_AGE));
         }
         if (!VALID_JOBS.contains(job)) {
-            throw new IllegalArgumentException("The place of work is not correct");
+            throw new IllegalArgumentException(("Job '%s' is not " +
+                    "in the list of valid jobs: %s").formatted(job, VALID_JOBS));
         }
         if (!VALID_ADDRESSES.contains(address)) {
-            throw new IllegalArgumentException("The location of work is not correct");
+            throw new IllegalArgumentException(("The location of work '%s' is not " +
+                    "in the list of valid addresses: %s").formatted(address, VALID_ADDRESSES));
         }
         this.name = name;
         this.age = age;
