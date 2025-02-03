0b25cea26a86c642ffa33cbbf22aaaf14ad98599
Savchenko <savchenko.ra@yandex.ru>
2025-02-03 20:43:44 +0300

    -Добавил импорт: import static java.util.Set.of -Добавил модификатор "final" к полям класса -Сделал более информативными сообщения об ошибках.

--- src/main/java/school/faang/bjs2_56348/User.java
+++ src/main/java/school/faang/bjs2_56348/User.java
@@ -2,17 +2,21 @@ package school.faang.bjs2_56348;
 
 import lombok.Data;
 
+
+//import java.util.Set;
+
 import java.util.Set;
+import static java.util.Set.of;
 
 @Data
 public class User {
-    private String name;
-    private int age;
-    private String job;
-    private String address;
+    private final String name;
+    private final int age;
+    private final String job;
+    private final String address;
 
-    private static final Set<String> VALID_JOBS = Set.of("Google", "Uber", "Amazon");
-    private static final Set<String> VALID_ADDRESSES = Set.of("London", "New York", "Amsterdam");
+    private static final Set<String> VALID_JOBS = of("Google", "Uber", "Amazon");
+    private static final Set<String> VALID_ADDRESSES = of("London", "New York", "Amsterdam");
 
     public User(String name, int age, String job, String address) {
         if (name == null || name.trim().isEmpty()) {
@@ -22,10 +26,10 @@ public class User {
             throw new IllegalArgumentException("Age cannot be less than 18");
         }
         if (!VALID_JOBS.contains(job)) {
-            throw new IllegalArgumentException("Job cannot be null or empty");
+            throw new IllegalArgumentException("Job must be one of: Google, Uber, Amazon");
         }
         if (!VALID_ADDRESSES.contains(address)) {
-            throw new IllegalArgumentException("Address cannot be null or empty");
+            throw new IllegalArgumentException("The address must be one of: London, New York, Amsterdam");
         }
 
         this.name = name;
