20fb1e933ccb3cbd02983daceea2bd080aebe368
Laura Lomberg <lomberglaura@gmail.com>
2025-02-03 11:10:39 +0300

    Add class User and class Main

---
+++ src/main/java/users/groups/Main.java
@@ -0,0 +1,21 @@
+package users.groups;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+public class Main {
+    public static void main(String[] args) {
+        List<User> users = new ArrayList<>();
+        users.add(new User("Lily", 25, "Google", "New York"));
+        users.add(new User("Tom", 30, "Facebook", "San Francisco"));
+        users.add(new User("Harry", 25, "Amazon", "Seattle"));
+        users.add(new User("David", 30, "Apple", "los Angeles"));
+        users.add(new User("John", 35, "Microsoft", "Portland"));
+
+        Map<Integer, List<User>> groupedUsers = User.groupUsers(users);
+        for (Map.Entry<Integer, List<User>> entry : groupedUsers.entrySet()) {
+            System.out.println(entry.getKey() + ":" + entry.getValue());
+        }
+    }
+}
