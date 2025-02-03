e34e056bce4db60d260992e4ca05f0970d6c56d1
aleksey.kazakov <alxej.kazakov@gmail.ru>
2025-02-03 13:12:32 +0300

    BJS2-56306 Grouping users

--- src/main/java/school/faang/groupingusers/Main.java
+++ src/main/java/school/faang/groupingusers/Main.java
@@ -1,18 +1,18 @@
 package school.faang.groupingusers;
 
-import java.util.Arrays;
 import java.util.List;
 import java.util.Map;
 
 public class Main {
     public static void main(String[] args) {
-        User user1 = new User("Natalya", 25, "Office", "g.Minsk");
-        User user2 = new User("Petr", 25, "Office", "g.Moscow");
-        User user3 = new User("Alex", 35, "Office", "London");
+        List<User> users = List.of(
+                new User("Aleksey", 25, "Google", "London"),
+                new User("Mary", 30, "Yandex", "Moscow"),
+                new User("Ivan", 25, "Facebook", "California"),
+                new User("Olga", 30, "Amazon", "Washington")
+        );
 
-        List<User> users = Arrays.asList(user1, user2, user3);
-
-        Map<Integer, List<User>> groupedUsers = User.groupUsers(users);
+       Map<Integer, List<User>> groupedUsers = User.groupUsers(users);
 
         for (Map.Entry<Integer, List<User>> entry : groupedUsers.entrySet()) {
             System.out.println("Age: " + entry.getKey());
--- src/main/java/school/faang/groupingusers/User.java
+++ src/main/java/school/faang/groupingusers/User.java
@@ -1,22 +1,38 @@
 package school.faang.groupingusers;
 
 import lombok.AllArgsConstructor;
-import lombok.Data;
+import lombok.Getter;
+import lombok.ToString;
 
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-@Data
+@Getter
 @AllArgsConstructor
+@ToString
 public class User {
     private String name;
     private int age;
-    private String placeWork;
+    private String workplace;
     private String address;
 
+    /**
+     * Группирует список пользователей по возрасту.
+     * <p>
+     * Метод принимает список пользователей и возвращает карту, где ключом является возраст,
+     * а значением — список пользователей этого возраста.
+     * </p>
+     * <p>
+     * Если входной список равен {@code null} или пуст, метод возвращает пустую карту.
+     * </p>
+     */
     public static Map<Integer, List<User>> groupUsers(List<User> users) {
+        if (users == null || users.isEmpty()) {
+            return new HashMap<>();
+        }
+
         Map<Integer, List<User>> groupedUsers = new HashMap<>();
 
         for (User user : users) {
@@ -32,5 +48,3 @@ public class User {
         return groupedUsers;
     }
 }
-
-
