8d7e577b19eef516e32e7af3ba718e072a868aa5
Yaroslav Olkin <shrelok.pupkin@yandex.ru>
2025-02-03 21:21:10 +0300

    .

--- src/main/java/school/faang/task_56329/Archer.java
+++ src/main/java/school/faang/task_56329/Archer.java
@@ -1,6 +1,6 @@
 package school.faang.task_56329;
 
-public class Archer extends Character{
+public class Archer extends Character {
     public Archer(String name) {
         super(name);
         strength = 3;
@@ -11,7 +11,7 @@ public class Archer extends Character{
     @Override
     public void attack(Character opponent) {
         System.out.println(this.name + " attacks " + opponent.getName()
-        + " for " + this.agility + " damage");
+                + " for " + this.agility + " damage");
         opponent.health -= this.agility;
     }
 }
