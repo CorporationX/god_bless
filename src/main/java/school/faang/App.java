8de60bd662a67b5028a241b97945bcb290bc17ca
Anton Kapshukov <kapshukov.project@gmail.com>
2025-02-03 14:43:43 +0300

    Выполнение задания "Магазин редких книг. Часть 3" (создал файл App.java и добавил в него команду вывода)

---
+++ src/main/java/school/faang/App.java
@@ -0,0 +1,9 @@
+package school.faang;
+
+import java.sql.SQLOutput;
+
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Открытие магазина ожидается через 2 месяца");
+    }
+}
