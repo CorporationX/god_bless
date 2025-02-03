fc81526e01c3f6108b80ec7881ba1551f680e3d9
pstratonni <79229338+pstratonni@users.noreply.github.com>
2025-02-03 18:27:04 +0100

    The task BSJ2-56358 has been corrected

--- src/main/java/school/faang/auth/Main.java
+++ src/main/java/school/faang/auth/Main.java
@@ -3,41 +3,22 @@ package school.faang.auth;
 public class Main {
 
     public static void main(String[] args) {
+        addAndPrintUser("Petr", 1984, 6, 12, "Hamburg", "Amazon");
+        addAndPrintUser("Ivan", 1900, 6, 12, "London", "Uber");
+        addAndPrintUser("Ivan", 1984, 6, 12, "London", "Uber");
+        addAndPrintUser("Ivan", 1984, 6, 12, " ", "Uber");
+        addAndPrintUser("Ivan", 2020, 6, 12, "London", "Uber");
+        addAndPrintUser("Lisa", 1984, 6, 12, "Berlin", "Uber");
+        addAndPrintUser("Lisa", null, 6, 12, "Hamburg", "Uber");
+        addAndPrintUser(null, 1984, 6, 12, "Hamburg", "Uber");
+    }
+
+    private static void addAndPrintUser(String name, Integer year, Integer month, Integer day,
+                                        String address, String job) {
         try {
-            System.out.println(new User("Petr", 1984, 6,
-                    12, "Hamburg", "Amazon"));
+            System.out.println(new User(name, year, month, day, address, job));
         } catch (IllegalArgumentException e) {
-            System.out.println(e);
-        }
-        try {
-            System.out.println(new User("Ivan", 1984, 6,
-                    12, "London", "Uber"));
-        } catch (IllegalArgumentException e) {
-            System.out.println(e);
-        }
-        try {
-            System.out.println(new User("Lisa", 1984, 6,
-                    12, "Berlin", "Uber"));
-        } catch (IllegalArgumentException e) {
-            System.out.println(e);
-        }
-        try {
-            System.out.println(new User("Katy", 1984, 6,
-                    12, "Hamburg", "Beiersdorf"));
-        } catch (IllegalArgumentException e) {
-            System.out.println(e);
-        }
-        try {
-            System.out.println(new User("Lisa", null, 6,
-                    12, "Hamburg", "Uber"));
-        } catch (IllegalArgumentException e) {
-            System.out.println(e);
-        }
-        try {
-            System.out.println(new User(null, 1984, 6,
-                    12, "Hamburg", "Uber"));
-        } catch (IllegalArgumentException e) {
-            System.out.println(e);
+            System.out.println(e.getMessage());
         }
     }
 }
--- src/main/java/school/faang/auth/User.java
+++ src/main/java/school/faang/auth/User.java
@@ -4,14 +4,20 @@ import java.time.LocalDate;
 import java.util.Set;
 
 public class User {
-    private String name;
-    private LocalDate birthDay;
-    private String address;
-    private String job;
+    private final String name;
+    private final LocalDate birthDay;
+    private final String address;
+    private final String job;
     private static final Set<String> VALID_JOBS = Set.of("Google", "Uber", "Amazon");
     private static final Set<String> VALID_ADDRESSES = Set.of("London", "New York", "Amsterdam", "Hamburg");
 
     public User(String name, Integer year, Integer month, Integer day, String address, String job) {
+        if (address.isBlank()) {
+            throw new IllegalArgumentException("Address cannot be blank.");
+        }
+        if (job.isBlank()) {
+            throw new IllegalArgumentException("Job cannot be blank.");
+        }
         if (!VALID_ADDRESSES.contains(address)) {
             throw new IllegalArgumentException("Invalid address: " + address);
         }
@@ -21,6 +27,12 @@ public class User {
         if (name == null || year == null || month == null || day == null) {
             throw new IllegalArgumentException("Invalid input. Name or date of birth is missing");
         }
+        if (LocalDate.of(year, month, day).isAfter(LocalDate.now().minusYears(18))) {
+            throw new IllegalArgumentException("Invalid input. User is too young");
+        }
+        if (LocalDate.of(year, month, day).isBefore(LocalDate.now().minusYears(120))) {
+            throw new IllegalArgumentException("Invalid input. User is already retired");
+        }
         this.name = name;
         this.birthDay = LocalDate.of(year, month, day);
         this.address = address;
@@ -29,7 +41,11 @@ public class User {
 
     @Override
     public String toString() {
-        return String.format("Name: %s, job: %s, date of birth %s, address: %s",
-                name, job, birthDay, address);
+        return String.format("""
+                     Name: %s
+                     job: %s
+                     date of birth %s
+                     address: %s
+                     """, name, job, birthDay, address);
     }
 }
