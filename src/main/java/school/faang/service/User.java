c1a1fec1aab2e8cdcd0576369047b33fa0001588
Dmitry Rogulkin <rogulkind@yandex.ru>
2025-02-04 12:29:10 +0500

    Group users by age. https://faang-school.atlassian.net/browse/BJS2-56324

--- src/main/java/school/faang/service/User.java
+++ src/main/java/school/faang/service/User.java
@@ -18,14 +18,15 @@ public class User {
         this.address = address;
     }
 
-    //принимает List из объектов User и возвращает Map, где ключом является возраст пользователя,
-// а значением — список из всех пользователей в оригинальном списке, которые имеют соответствующий возраст.
-    public static Map<Integer, List> groupUsers(List<User> users) {
-        Map<Integer, List> userAge = new HashMap<>();
-        List<User> ageUser = new ArrayList<>();
-        for (int i = 0; i < users.size(); i++) {
-            users.get(i).getAge();
+    public static Map<Integer, List<User>> groupUsers(List<User> users) {
+        Map<Integer, List<User>> userAge = new HashMap<>();
+        for (User user : users) {
+            userAge.computeIfAbsent(user.getAge(), k -> new ArrayList<>()).add(user);
         }
+        for (Map.Entry<Integer, List<User>> value : userAge.entrySet())
+            for (int i = 0; i < value.getValue().size(); i++) {
+                System.out.println(value.getKey() + " " + value.getValue().get(i).getName());
+            }
         return userAge;
     }
 
