9601123f7830aede9cee9d172b9eb381f277583e
Hayotbek Farhodov <hayotbekfarhodov020@gmail.com>
2025-02-04 12:51:13 +0500

    Групировка пользователей

--- src/main/java/school/faang/Main.java
+++ src/main/java/school/faang/Main.java
@@ -1,4 +1,42 @@
 package school.faang;
 
+import java.util.List;
+import java.util.ArrayList;
+import java.util.Map;
+
 public class Main {
+    public static void main(String[] args) {
+        User user1 = new User("Artur", 15, null, "ijrefijrife");
+        User user2 = new User("Andrey", 15, null, "ijrefijrife");
+        User user3 = new User("Leha", 15, null, "ijrefijrife");
+        User user4 = new User("Misha", 17, null, "ijrefijrife");
+        User user5 = new User("Katya", 15, null, "ijrefijrife");
+        User user6 = new User("Artem", 16, null, "ijrefijrife");
+        User user7 = new User("Artur", 20, null, "ijrefijrife");
+        User user8 = new User("Fedya", 20, null, "ijrefijrife");
+        User user9 = new User("Alice", 21, null, "ijrefijrife");
+
+        List<User> users = new ArrayList<>();
+        users.add(user1);
+        users.add(user2);
+        users.add(user3);
+        users.add(user4);
+        users.add(user5);
+        users.add(user6);
+        users.add(user7);
+        users.add(user8);
+        users.add(user9);
+        printUsers(users);
+
+    }
+
+    public static void printUsers(List<User> users) {
+        Map<Integer, List<User>> map = User.groupUsers(users);
+
+        for (Map.Entry<Integer, List<User>> entry : map.entrySet()) {
+            System.out.println(entry.getKey() + " " + entry.getValue());
+        }
+
+    }
+
 }
---
+++ src/main/java/school/faang/User.java
@@ -0,0 +1,47 @@
+package school.faang;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.ToString;
+
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.List;
+
+
+@Getter
+@AllArgsConstructor
+@EqualsAndHashCode
+@ToString
+public class User {
+
+    private final String name;
+    private final int age;
+    private final String workplace;
+    private final String address;
+
+    public static Map<Integer, List<User>> groupUsers(List<User> users) {
+        Map<Integer, List<User>> resultMap = new HashMap<>();
+
+        if (users.size() == 0) {
+            return null;
+        }
+
+        for (User user : users) {
+            Integer key = user.getAge();
+            if (!resultMap.containsKey(key)) {
+                resultMap.put(key, new ArrayList<>());
+            } else {
+                resultMap.get(key).add(user);
+            }
+        }
+
+        return resultMap;
+    }
+
+
+}
