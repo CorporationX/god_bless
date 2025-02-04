e660fc2014777ef489cbc87c9bdf8e8bdc610151
Ilyas Azhdar <azhdar902@gmail.com>
2025-02-04 10:48:39 +0500

    changed the structure of the fields

--- src/main/java/school/faang/usergrouping/User.java
+++ src/main/java/school/faang/usergrouping/User.java
@@ -9,30 +9,25 @@ import java.util.Map;
 
 @Getter
 public class User {
-    private static final List<User> USERS = new ArrayList<>();
-
     private String name;
     private int age;
-    private String organization;
+    private String workplace;
     private String address;
 
-    public User(String name, int age, String organization, String address) {
+    public User(String name, int age, String workplace, String address) {
         this.name = name;
         this.age = age;
-        this.organization = organization;
+        this.workplace = workplace;
         this.address = address;
     }
 
     public static void main(String[] args) {
-        User kolya = new User("Kolya", 25, "Google", "California");
-        User vasya = new User("Vasya", 30, "Facebook", "California");
-        User petya = new User("Petya", 25, "Google", "California");
+        List<User> users = List.of(
+                new User("Kolya", 25, "Google", "California"),
+                new User("Vasya", 30, "Facebook", "California"),
+                new User("Petya", 25, "Google", "California"));
 
-        USERS.add(kolya);
-        USERS.add(vasya);
-        USERS.add(petya);
-
-        System.out.println(groupUsers(USERS));
+        System.out.println(groupUsers(users));
     }
 
     private static Map<Integer, List<User>> groupUsers(List<User> users) {
@@ -47,11 +42,7 @@ public class User {
 
     @Override
     public String toString() {
-        return "User{" +
-                "name='" + name + '\'' +
-                ", age=" + age +
-                ", organization='" + organization + '\'' +
-                ", address='" + address + '\'' +
-                '}';
+        return String.format("User{name='%s', age=%d, organization='%s', address='%s'}",
+                name, age, workplace, address);
     }
 }
