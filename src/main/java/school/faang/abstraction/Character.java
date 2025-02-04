45f202756ebd4461fc68078e904df7bc466018e8
Anton Kapshukov <kapshukov.project@gmail.com>
2025-02-04 18:08:40 +0300

    Абстракция, абстракция! (с учетом всех комментариев)

--- src/main/java/school/faang/abstraction/Character.java
+++ src/main/java/school/faang/abstraction/Character.java
@@ -8,32 +8,17 @@ import lombok.Setter;
 public abstract class Character {
     private static final int DEFAULT_HEALTH = 100;
     private static final int DEFAULT_VALUE = 5;
-    protected final String name;
-    protected int strength;
-    protected int agility;
-    protected int intelligence;
-    protected int health;
+    private final String name;
+    private int strength;
+    private int agility;
+    private int intelligence;
+    private int health = DEFAULT_HEALTH;
 
     protected Character(String name) {
-        validateName(name);
-
-        this.name = name;
-        this.health = DEFAULT_HEALTH;
-        this.agility = DEFAULT_VALUE;
-        this.strength = DEFAULT_VALUE;
-        this.intelligence = DEFAULT_VALUE;
+        this(name, DEFAULT_VALUE, DEFAULT_VALUE, DEFAULT_VALUE);
     }
 
     public Character(String name, int strength, int agility, int intelligence) {
-        validateName(name);
-
-        this.name = name;
-        this.strength = strength;
-        this.agility = agility;
-        this.intelligence = intelligence;
-    }
-
-    protected void validateName(String name) {
         if (name == null) {
             throw new NullPointerException("Значение имени пользователя не может быть null");
         }
@@ -41,6 +26,11 @@ public abstract class Character {
         if (name.isEmpty()) {
             throw new IllegalArgumentException("Имя не может быть пустым");
         }
+
+        this.name = name;
+        this.strength = strength;
+        this.agility = agility;
+        this.intelligence = intelligence;
     }
 
     public abstract void attack(Character opponent);
