f3abf4beb3f15b31d18214f6ebf40940d58ea898
vtroshin <vtroshin@rvision.ru>
2025-02-04 22:09:22 +0300

    BJS2-56330 Группировка пользователей по возрасту

--- src/main/java/school/faang/Main.java
+++ src/main/java/school/faang/Main.java
@@ -1,4 +1,19 @@
 package school.faang;
 
+import school.faang.model.User;
+
+import java.util.List;
+
 public class Main {
+    public static void main(String[] args) {
+        List<User> userList = List.of(
+                new User("Вася", "Поваренок и К.О.", "ул. Пушкина", 11),
+                new User("Петя", "Брауберг", "ул. Старушкина", 11),
+                new User("Жора", "Кандибоберы", "аллея Петрушкина", 44),
+                new User("Евгений", "Цирк", "мкр. 1", 65),
+                new User("Ярик", "Казино", "мкр. 2", 9)
+        );
+
+        System.out.println(User.groupUsers(userList));
+    }
 }
---
+++ src/main/java/school/faang/model/User.java
@@ -0,0 +1,30 @@
+package school.faang.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class User {
+    private String name;
+    private String workplace;
+    private String address;
+    private int age;
+
+    public static Map<Integer, List<User>> groupUsers(List<User> users) {
+        Map<Integer, List<User>> resultMap = new HashMap<>();
+
+        for (var user : users) {
+            resultMap.computeIfAbsent(user.getAge(), u -> new ArrayList<>()).add(user);
+        }
+
+        return resultMap;
+    }
+}
