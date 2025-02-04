87e5edabcd1809c290d9dd94fa4a2071a4719a87
Dmitry Rogulkin <rogulkind@yandex.ru>
2025-02-04 16:24:44 +0500

    Turn around. https://faang-school.atlassian.net/browse/BJS2-56359

--- src/main/java/school/faang/Main.java
+++ src/main/java/school/faang/Main.java
@@ -1,7 +1,12 @@
 package school.faang;
 
+import school.faang.service.Example;
+
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
-
+        int[] nums = new int[]{1, 2, 3, 4, 5};
+        System.out.println(Arrays.toString(Example.reverse(nums)));
     }
 }
