d9ffa0caf1d607b8f12e9e254c76a9ac267f156a
Laura Lomberg <lomberglaura@gmail.com>
2025-02-04 07:53:52 +0300

    Refactor code

--- src/main/java/users/groups/Main.java
+++ src/main/java/users/groups/Main.java
@@ -1,17 +1,17 @@
 package users.groups;
 
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 
 public class Main {
     public static void main(String[] args) {
-        List<User> users = new ArrayList<>();
-        users.add(new User("Lily", 25, "Google", "New York"));
-        users.add(new User("Tom", 30, "Facebook", "San Francisco"));
-        users.add(new User("Harry", 25, "Amazon", "Seattle"));
-        users.add(new User("David", 30, "Apple", "los Angeles"));
-        users.add(new User("John", 35, "Microsoft", "Portland"));
+        List<User> users = List.of(
+                new User("Lily", 25, "Google", "New York"),
+                new User("Tom", 30, "Facebook", "San Francisco"),
+                new User("Harry", 25, "Amazon", "Seattle"),
+                new User("David", 30, "Apple", "Los Angeles"),
+                new User("John", 35, "Microsoft", "Portland")
+        );
 
         Map<Integer, List<User>> groupedUsers = User.groupUsers(users);
         for (Map.Entry<Integer, List<User>> entry : groupedUsers.entrySet()) {
--- src/main/java/users/groups/User.java
+++ src/main/java/users/groups/User.java
@@ -1,35 +1,25 @@
 package users.groups;
 
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+@Getter
+@AllArgsConstructor
 public class User {
     private String name;
     private int age;
     private String workplace;
     private String address;
 
-    public User(String name, int age, String workplace, String address) {
-        this.name = name;
-        this.age = age;
-        this.workplace = workplace;
-        this.address = address;
-    }
-
-    public int getAge() {
-        return age;
-    }
-
     @Override
     public String toString() {
-        return "User: " +
-                "name: '" + name + '\'' +
-                ", age: " + age +
-                ", workplace: '" + workplace + '\'' +
-                ", address: '" + address + '\'';
+        return String.format("User: name: %s, age: %d, workplace: %s, address: %s", name, age, workplace, address);
     }
 
     public static Map<Integer, List<User>> groupUsers(List<User> users) {
