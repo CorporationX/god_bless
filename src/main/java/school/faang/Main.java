a86408386144921e7aa171075edcaffee4ba9b4d
Dmitry Rogulkin <rogulkind@yandex.ru>
2025-02-04 14:02:23 +0500

    Register user task. https://faang-school.atlassian.net/browse/BJS2-56383

--- src/main/java/school/faang/Main.java
+++ src/main/java/school/faang/Main.java
@@ -1,7 +1,13 @@
 package school.faang;
 
+import school.faang.services.User;
+
 public class Main {
     public static void main(String[] args) {
-
+        try {
+            User testUser = new User("Вася",25,"Uber","Amsterdam");
+        } catch (IllegalArgumentException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
--- src/main/java/school/faang/services/User.java
+++ src/main/java/school/faang/services/User.java
@@ -1,4 +1,33 @@
 package school.faang.services;
 
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
 public class User {
+    private String name;
+    private int age;
+    private String job;
+    private String address;
+
+    private final Set<String> VALID_JOBS = new HashSet<>(List.of("Google", "Uber", "Amazon"));
+    private final Set<String> VALID_ADDRESSES = new HashSet<>(List.of("London", "New York", "Amsterdam"));
+
+    public User(String name, int age, String job, String address) {
+        if (name.isEmpty())
+            throw new IllegalArgumentException("Имя не может быть пустым.");
+        this.name = name;
+
+        if (age < 18)
+            throw new IllegalArgumentException("Вам меньше 18 лет.");
+        this.age = age;
+
+        if (!VALID_JOBS.contains(job))
+            throw new IllegalArgumentException("Указанная работа не соответствует критерию.");
+        this.job = job;
+
+        if (!VALID_ADDRESSES.contains(address))
+            throw new IllegalArgumentException("Указанный адрес не соответствует критерию.");
+        this.address = address;
+    }
 }
