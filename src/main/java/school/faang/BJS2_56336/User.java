7f616c75130fac3e6835e151ea5702189408ce7c
Ekaterina Ramakoti <zubkova-92@mail.ru>
2025-02-04 20:16:10 +0300

    task BJS2-56336 после исправления замечаний от ментора

--- src/main/java/school/faang/BJS2_56336/User.java
+++ src/main/java/school/faang/BJS2_56336/User.java
@@ -1,29 +1,11 @@
 package school.faang.BJS2_56336;
 
+import lombok.NonNull;
+
 import java.util.Set;
 
+
 public class User {
-    private String name;
-    private int age;
-    private String job;
-    private String address;
-
-    public User(String name, int age, String job, String address) {
-        if(name.isEmpty()) {
-            throw new IllegalArgumentException("Имя не может быть пустым.");
-        } if(age<18) {
-            throw new IllegalArgumentException("Возраст не может быть меньше 18.");
-        } if(!VALID_JOBS.contains(job)) {
-            throw new IllegalArgumentException("Место работы должно содержаться в наборе VALID_JOBS.");
-        } if(!VALID_ADDRESSES.contains(address)) {
-            throw new IllegalArgumentException("Адрес должен содержаться в наборе VALID_ADDRESSES.");
-        }
-        this.name = name;
-        this.age = age;
-        this.job = job;
-        this.address = address;
-    }
-
     public static final Set<String> VALID_JOBS = Set.of(
             "Google",
             "Uber",
@@ -34,4 +16,34 @@ public class User {
             "New York",
             "Amsterdam"
     );
+    public static final int MIN_LEGAL_AGE = 18;
+
+    private String name;
+    private int age;
+    private String job;
+    private String address;
+
+    @NonNull
+    public User(String name, int age, String job, String address) {
+        valid();
+        this.name = name;
+        this.age = age;
+        this.job = job;
+        this.address = address;
+    }
+
+    private void valid() {
+        if (name.isBlank()) {
+            throw new IllegalArgumentException("Имя не может быть пустым.");
+        }
+        if (age < MIN_LEGAL_AGE) {
+            throw new IllegalArgumentException("Возраст не может быть меньше 18.");
+        }
+        if (!VALID_JOBS.contains(job)) {
+            throw new IllegalArgumentException("Место работы должно содержаться в наборе VALID_JOBS.");
+        }
+        if (!VALID_ADDRESSES.contains(address)) {
+            throw new IllegalArgumentException("Адрес должен содержаться в наборе VALID_ADDRESSES.");
+        }
+    }
 }
