280fa9da93a4814cb57210cc4d7f662b2e34c623
RamilNazmiev192 <nazmiev633@gmail.com>
2025-02-04 10:46:08 +0300

    use user

--- src/main/java/school/faang/Main.java
+++ src/main/java/school/faang/Main.java
@@ -1,4 +1,22 @@
 package school.faang;
 
+import school.faang.user.User;
+import java.util.*;
+
 public class Main {
+    public static void main(String[] args) {
+        List<User> users = Arrays.asList(
+                new User(1, "Alice", 25, new HashSet<>(Arrays.asList("Football", "Chess", "Reading"))),
+                new User(2, "Bob", 30, new HashSet<>(Arrays.asList("Gaming", "Cooking"))),
+                new User(3, "Charlie", 22, new HashSet<>(Arrays.asList("Football", "Swimming"))),
+                new User(4, "David", 28, new HashSet<>(Arrays.asList("Painting", "Reading")))
+        );
+        Set<String> activities = new HashSet<>(Arrays.asList("Reading", "Gaming"));
+
+        Map<User, String> result = User.findHobbyLovers(users, activities);
+
+        for (Map.Entry<User, String> entry : result.entrySet()) {
+            System.out.println(entry.getKey().getName() + " " + entry.getValue());
+        }
+    }
 }
