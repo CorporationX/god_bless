9e10c7c1fc96c6437d66f4fc3d34a75a335cf67a
Sergey <bigwave2915@gmail.com>
2025-02-04 14:07:05 +0500

    Исправил по коментариям,то что понимал как сделать))

--- src/main/java/school/faang/Main.java
+++ src/main/java/school/faang/Main.java
@@ -8,11 +8,16 @@ public class Main {
     public static void main(String[] args) {
 
         List<User> users = List.of(
+
                 new User("Pitr", 23, "Gazprom", "Mos"),
                 new User("Lama", 23, "Lucoil", "Minsk"),
                 new User("Kisha", 22, "Lucoil", "Pirm"),
                 new User("Kuzi", 33, "Magnit", "Minsk"));
+        if (users.isEmpty()) {
+            System.out.println("Нет Пользователей");
+            return;
 
+        }
         System.out.print(User.groupUsers(users));
     }
 }
--- src/main/java/school/faang/group_users_by_age/User.java
+++ src/main/java/school/faang/group_users_by_age/User.java
@@ -6,9 +6,9 @@ import lombok.Getter;
 import lombok.ToString;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.TreeMap;
 
 @Getter
 @AllArgsConstructor
@@ -21,21 +21,18 @@ public class User {
     private final String workplace;
     private final String address;
 
-
     public static Map<Integer, List<User>> groupUsers(List<User> users) {
-        Map<Integer, List<User>> mapSort = new TreeMap<>();
+        Map<Integer, List<User>> mapSort = new HashMap<>();
         if (users.size() == 0) {
-            return null;
-        } else {
-            for (User user : users) {
-                if (!mapSort.containsKey(user.getAge())) {
-                    mapSort.put(user.getAge(),
-                            new ArrayList<>());
-                }
-                mapSort.get(user.getAge()).add(user);
-            }
+            return mapSort;
+        }
+        for (User user : users) {
+            mapSort.putIfAbsent(user.getAge(), new ArrayList<>());
+            mapSort.get(user.age).add(user);
         }
         return mapSort;
     }
 }
+
+
 
