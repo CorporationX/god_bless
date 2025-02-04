9b1bc929488c19ae5960c34ac46e7483208dd91b
Dmitry Rogulkin <rogulkind@yandex.ru>
2025-02-04 14:42:51 +0500

    Abstract,Abstract. https://faang-school.atlassian.net/browse/BJS2-56356

--- src/main/java/school/faang/models/Character.java
+++ src/main/java/school/faang/models/Character.java
@@ -1,4 +1,23 @@
 package school.faang.models;
 
-public class Character {
+public abstract class Character {
+    private String name;
+    private int power;
+    private int agility;
+    private int intellect;
+    private int health = 100;
+
+    public Character(String name) {
+        this.name = name;
+        power = 5;
+        agility = 5;
+        intellect = 5;
+    }
+
+    public Character(String name, int power, int agility, int intellect) {
+        this.name = name;
+        this.power = power;
+        this.agility = agility;
+        this.intellect = intellect;
+    }
 }
