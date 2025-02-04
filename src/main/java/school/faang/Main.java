b4ab22f37e97dde0fb3f7929dfb4f3e9a1d5ba34
Sergey <bigwave2915@gmail.com>
2025-02-04 12:38:36 +0500

    Сделал задачу Группировка пользователей по возрасту

--- src/main/java/school/faang/Main.java
+++ src/main/java/school/faang/Main.java
@@ -1,7 +1,19 @@
 package school.faang;
 
+import school.faang.group_users_by_age.User;
+
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.printf("");
+
+        List<User> users = List.of(
+                new User("Pitr", 23, "Gazprom", "Mos"),
+                new User("Lama", 23, "Lucoil", "Minsk"),
+                new User("Kisha", 22, "Lucoil", "Pirm"),
+                new User("Kuzi", 33, "Magnit", "Minsk"));
+
+        System.out.print(User.groupUsers(users));
     }
 }
+
--- src/main/java/school/faang/group_users_by_age/User.java
+++ src/main/java/school/faang/group_users_by_age/User.java
@@ -1,10 +1,19 @@
 package school.faang.group_users_by_age;
 
 import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
 import lombok.Getter;
+import lombok.ToString;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.TreeMap;
 
 @Getter
 @AllArgsConstructor
+@ToString
+@EqualsAndHashCode
 
 public class User {
     private final String name;
@@ -12,4 +21,21 @@ public class User {
     private final String workplace;
     private final String address;
 
+
+    public static Map<Integer, List<User>> groupUsers(List<User> users) {
+        Map<Integer, List<User>> mapSort = new TreeMap<>();
+        if (users.size() == 0) {
+            return null;
+        } else {
+            for (User user : users) {
+                if (!mapSort.containsKey(user.getAge())) {
+                    mapSort.put(user.getAge(),
+                            new ArrayList<>());
+                }
+                mapSort.get(user.getAge()).add(user);
+            }
+        }
+        return mapSort;
+    }
 }
+
