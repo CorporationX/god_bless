d263482568f44f5e00236cf39de0d6102da74a0b
Dmitry Rogulkin <rogulkind@yandex.ru>
2025-02-04 16:25:48 +0500

    Turn around. https://faang-school.atlassian.net/browse/BJS2-56359

--- src/main/java/school/faang/service/Example.java
+++ src/main/java/school/faang/service/Example.java
@@ -1,7 +1,6 @@
 package school.faang.service;
 
 public class Example {
-
     public static int[] reverse(int[] nums) {
         int[] reverseNum = new int[nums.length];
         int i = reverseNum.length - 1;
