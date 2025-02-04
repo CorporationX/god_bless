e23d12f955f3b56c9dbdc1c63d3a46db571a5c43
aleksey.kazakov <alxej.kazakov@gmail.ru>
2025-02-05 00:53:32 +0300

    BJS2-56964

---
+++ src/main/java/school/faang/HogwartsSpells.java
@@ -0,0 +1,51 @@
+package school.faang;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * A class for managing spell events at Hogwarts.
+ * Allows you to add, delete, search, and output spell events.
+ */
+
+public class HogwartsSpells {
+    private final Map<Integer, SpellEvent> spellById  = new HashMap<>();
+    private final Map<String, List<SpellEvent>> spellsByType = new HashMap<>();
+    private int nextId = 1;
+
+    public void addSpellEvent(String eventType, String actionDescription) {
+        SpellEvent spellEvent = new SpellEvent(nextId, eventType, actionDescription);
+        spellById.put(nextId, spellEvent);
+
+        spellsByType.computeIfAbsent(eventType, k -> new ArrayList<>()).add(spellEvent);
+        nextId++;
+    }
+
+    public SpellEvent getSpellEventById(int id) {
+        return spellById.get(id);
+    }
+
+    public List<SpellEvent> getSpellsByType(String eventType) {
+        return spellsByType.getOrDefault(eventType, Collections.emptyList());
+    }
+
+    public void deleteSpellEventById(int id) {
+        SpellEvent spellEvent = spellById.remove(id);
+        if (spellEvent != null) {
+            spellsByType.get(spellEvent.getEventType()).remove(spellEvent);
+        }
+    }
+
+    public void printAllSpellEvents() {
+        for (Map.Entry<Integer, SpellEvent> entry : spellById.entrySet()) {
+            SpellEvent spellEvent = entry.getValue();
+            System.out.printf("ID: %d, Type: %s, Action: %s%n",
+                    spellEvent.getId(),
+                    spellEvent.getEventType(),
+                    spellEvent.getAction());
+        }
+    }
+}
---
+++ src/main/java/school/faang/SpellEvent.java
@@ -0,0 +1,34 @@
+package school.faang;
+
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.ToString;
+
+@Getter
+@ToString
+@EqualsAndHashCode
+public class SpellEvent {
+    private final int id;
+    private final String eventType;
+    private final String action;
+
+    public SpellEvent(int id, String eventType, String action) {
+        validateAction(action);
+        validateEventType(eventType);
+        this.id = id;
+        this.eventType = eventType;
+        this.action = action;
+    }
+
+    public void validateAction(String action) {
+        if (action == null || action.isEmpty()) {
+            throw new IllegalArgumentException("Action cannot be null or blank!");
+        }
+    }
+
+    public void validateEventType(String eventType) {
+        if (eventType == null || eventType.isBlank()) {
+            throw new IllegalArgumentException("EventType cannot be null or blank!");
+        }
+    }
+}
