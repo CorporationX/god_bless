74f9142aff8a9aaca7245d314d08b757ab158f61
Dmitry Rogulkin <rogulkind@yandex.ru>
2025-02-04 15:12:02 +0500

    Abstract,Abstract. https://faang-school.atlassian.net/browse/BJS2-56356

--- src/main/java/school/faang/Main.java
+++ src/main/java/school/faang/Main.java
@@ -1,7 +1,10 @@
 package school.faang;
 
+import school.faang.models.Character;
+import school.faang.models.Warrior;
+
 public class Main {
     public static void main(String[] args) {
-
+        Character barbarian = new Warrior("Конан варвар",15,5,2);
     }
 }
--- src/main/java/school/faang/models/Character.java
+++ src/main/java/school/faang/models/Character.java
@@ -2,22 +2,56 @@ package school.faang.models;
 
 public abstract class Character {
     private String name;
-    private int power;
+    private int strength;
     private int agility;
     private int intellect;
     private int health = 100;
 
     public Character(String name) {
         this.name = name;
-        power = 5;
+        strength = 5;
         agility = 5;
         intellect = 5;
     }
 
-    public Character(String name, int power, int agility, int intellect) {
+    public Character(String name, int strength, int agility, int intellect) {
         this.name = name;
-        this.power = power;
+        this.strength = strength;
         this.agility = agility;
         this.intellect = intellect;
     }
+
+    public int getStrength() {
+        return strength;
+    }
+
+    public void setStrength(int strength) {
+        this.strength = strength;
+    }
+
+    public int getAgility() {
+        return agility;
+    }
+
+    public void setAgility(int agility) {
+        this.agility = agility;
+    }
+
+    public int getIntellect() {
+        return intellect;
+    }
+
+    public void setIntellect(int intellect) {
+        this.intellect = intellect;
+    }
+
+    public int getHealth() {
+        return health;
+    }
+
+    public void setHealth(int health) {
+        this.health = health;
+    }
+
+    public abstract void attack(Character opponent);
 }
