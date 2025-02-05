231aeb5dfcfd537425821649060017c44097f18b
Artpyak <art.pyak@yandex.ru>
2025-02-05 09:25:37 +0300

    BJS2-56339 1. Добавленна папка с названием задачи для файлов используемых в задаче. 2. Изменил реализацию toString используя StringBuilder. 3. В методе equals объединены условия в одну строку. 4. Обновление реализации метода groupUsers с помощью Map.computeIfAbsent 5. В методе main был использован List.of() вместо множественных add().

--- src/main/java/school/faang/grouping_users_by_age/BJS2_56339/BJS2_56339/User.java
+++ src/main/java/school/faang/grouping_users_by_age/BJS2_56339/BJS2_56339/User.java
@@ -1,4 +1,4 @@
-package school.faang;
+package school.faang.grouping_users_by_age.BJS2_56339.BJS2_56339;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -12,6 +12,7 @@ import java.util.Objects;
 @AllArgsConstructor
 @Getter
 public class User {
+
     private final String name;
     private final int age;
     private final String workplace;
@@ -21,25 +22,18 @@ public class User {
         Map<Integer, List<User>> userGroups = new HashMap<>();
 
         for (User user : users) {
-            Integer key = user.getAge();
-            if (!userGroups.containsKey(key)) {
-                userGroups.put(key, new ArrayList<>());
-            }
-            userGroups.get(key).add(user);
+            userGroups.computeIfAbsent(user.getAge(), k -> new ArrayList<>()).add(user);
         }
         return userGroups;
     }
 
     @Override
     public boolean equals(Object o) {
-        if (o == null || getClass() != o.getClass()) {
-            return false;
-        }
-        User user = (User) o;
-        return age == user.age
+        return this == o || (o instanceof User user
+                && Objects.equals(age, user.age)
                 && Objects.equals(name, user.name)
                 && Objects.equals(workplace, user.workplace)
-                && Objects.equals(address, user.address);
+                && Objects.equals(address, user.address));
     }
 
     @Override
@@ -49,11 +43,14 @@ public class User {
 
     @Override
     public String toString() {
-        return String.format(
-                "User{name='%s', age=%d, workplace='%s', address='%s'}",
-                name, age, workplace, address
-        );
+        StringBuilder stringBuilder = new StringBuilder();
+        stringBuilder
+                .append("User{")
+                .append("name=").append(name)
+                .append(", age=").append(age)
+                .append(", workplace=").append(workplace)
+                .append(", address=").append(address)
+                .append("}");
+        return stringBuilder.toString();
     }
-
-
 }
