dc85bb3afabed93c366b80117e3402ac6054ee48
Dmitry Rogulkin <rogulkind@yandex.ru>
2025-02-05 14:43:02 +0500

    https://faang-school.atlassian.net/browse/BJS2-56935

---
+++ src/main/java/school/faang/service/Book.java
@@ -0,0 +1,28 @@
+package school.faang.service;
+
+import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.Objects;
+
+//@Data
+@Getter @Setter
+public class Book {
+    private String title;
+    private String author;
+    private int year;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Book book = (Book) o;
+        return year == book.year && Objects.equals(title, book.title) && Objects.equals(author, book.author);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(title, author, year);
+    }
+}
