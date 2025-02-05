928b4aa2d16cf217717de6325d8ec3c5ceb8172e
Dmitry Rogulkin <rogulkind@yandex.ru>
2025-02-05 13:33:56 +0500

    https://faang-school.atlassian.net/browse/BJS2-56356

--- src/main/java/school/faang/Main.java
+++ src/main/java/school/faang/Main.java
@@ -10,7 +10,7 @@ public class Main {
         Character elf = new Archer("Леголас");
         Character ork = new Archer("Черный орк (лучник)");
 
-        System.out.println("У "+ ork.getName()+" было здоровья - "+ork.getHealth()+". А после удара, осталось здоровья " +barbarian.attack(ork));
-        System.out.println("Сила удара у "+barbarian.getName()+ " составляет "+ barbarian.getStrength());
+        System.out.println("У " + ork.getName() + " было здоровья - " + ork.getHealth() + ". А после удара, осталось здоровья " + barbarian.attack(ork));
+        System.out.println("Сила удара у " + barbarian.getName() + " составляет " + barbarian.getStrength());
     }
 }
