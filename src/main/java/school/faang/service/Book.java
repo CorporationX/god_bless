6dfdc326c0d35f90a281a373a925d308983845a2
Dmitry Rogulkin <rogulkind@yandex.ru>
2025-02-05 15:38:21 +0500

    https://faang-school.atlassian.net/browse/BJS2-56935

--- src/main/java/school/faang/service/Book.java
+++ src/main/java/school/faang/service/Book.java
@@ -1,17 +1,22 @@
 package school.faang.service;
 
-import lombok.Data;
 import lombok.Getter;
 import lombok.Setter;
 
 import java.util.Objects;
 
-//@Data
-@Getter @Setter
+@Getter
+@Setter
 public class Book {
     private String title;
     private String author;
     private int year;
+
+    public Book(String title, String author, int year) {
+        this.title = title;
+        this.author = author;
+        this.year = year;
+    }
 
     @Override
     public boolean equals(Object o) {
--- src/main/java/school/faang/service/LibrarySystem.java
+++ src/main/java/school/faang/service/LibrarySystem.java
@@ -1,4 +1,29 @@
 package school.faang.service;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class LibrarySystem {
+    Map<Book, String> placeBook = new HashMap<>();
+
+    public void addBook(String title, String author, int year, String location) {
+        placeBook.put(new Book(title, author, year), location);
+    }
+
+    public void removeBook(String title, String author, int year) {
+        placeBook.remove(new Book(title, author, year));
+    }
+
+    public String findBook(String title, String author, int year) {
+        Book searchBook = new Book(title, author, year);
+        return placeBook.getOrDefault(searchBook, "Книга не найдена");
+    }
+
+    public void printAllBooks() {
+        for (Map.Entry<Book, String> entry : placeBook.entrySet()) {
+            System.out.print("название книги: \"" + entry.getKey().getTitle() +
+                    "\" \nавтор: " + entry.getKey().getAuthor() +
+                    " \nгод: " + entry.getKey().getYear() + "\nместо: " + entry.getValue() + "\n===============================\n");
+        }
+    }
 }
