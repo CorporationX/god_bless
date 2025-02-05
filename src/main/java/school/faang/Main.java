09f87c7c39d7444602bd9c46e6a33b56dcb192b6
Dmitry Rogulkin <rogulkind@yandex.ru>
2025-02-05 14:24:25 +0500

    https://faang-school.atlassian.net/browse/BJS2-56359

--- src/main/java/school/faang/Main.java
+++ src/main/java/school/faang/Main.java
@@ -6,7 +6,11 @@ import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) {
-        int[] nums = new int[]{1, 2, 3, 4, 5};
-        System.out.println(Arrays.toString(Example.reverse(nums)));
+        try {
+            int[] nums = new int[]{1, 2, 3, 4, 5};
+            System.out.println(Arrays.toString(Example.reverse(nums)));
+        } catch (IllegalArgumentException e) {
+            throw new IllegalArgumentException(e);
+        }
     }
 }
