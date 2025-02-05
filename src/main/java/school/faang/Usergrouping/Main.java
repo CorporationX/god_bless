3aa3a5161f6826b3ea59ea8437e89098e3e9ad3c
Marsel <mars1212@inbox.ru>
2025-02-05 21:23:16 +0300

    Группировка пользователей по возрасту

---
+++ src/main/java/school/faang/Usergrouping/Main.java
@@ -0,0 +1,24 @@
+package school.faang.Usergrouping;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+import static school.faang.Usergrouping.User.groupUsers;
+
+public class Main {
+    public static void main(String[] args) {
+        User user1 = new User("Nikolay", 18,"ya","Street1");
+        User user2 = new User("Sergay", 18,"T-bank","Street2");
+        User user3 = new User("Alex", 20,"ya","Street3");
+        User user4 = new User("Zaur", 19,"ya","Street4");
+        List<User> users = new ArrayList<User>();
+        users.add(user1);
+        users.add(user2);
+        users.add(user3);
+        users.add(user4);
+
+        Map<Integer, List<User>> userGroupings = groupUsers(users);
+        System.out.println(userGroupings);
+    }
+}
---
+++ src/main/java/school/faang/Usergrouping/User.java
@@ -0,0 +1,33 @@
+package school.faang.Usergrouping;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@Data
+@AllArgsConstructor
+public class User {
+    private String name;
+    private int age;
+    private String workplace;
+    private String adress;
+
+    public static Map<Integer, List<User>> groupUsers(List<User> users) {
+        Map<Integer, List<User>> userGroups = new HashMap<>();
+        for(User user : users) {
+            int userAge = user.getAge();
+            if(!userGroups.containsKey(userAge)) {
+                userGroups.put(userAge, new ArrayList<>());
+            }
+            userGroups.get(userAge).add(user);
+
+        }
+        return userGroups;
+
+    }
+
+}
