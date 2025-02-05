76a96297dd83b87574f0a941c12d74814a3fc6dd
Sergey <bigwave2915@gmail.com>
2025-02-05 16:42:08 +0500

    Сделал ошибки в ветках. ..Испраил

--- src/main/java/school/faang/library_of_westeros/LibrarySystem.java
+++ src/main/java/school/faang/library_of_westeros/LibrarySystem.java
@@ -1,6 +1,7 @@
 package school.faang.library_of_westeros;
 
 import lombok.EqualsAndHashCode;
+import lombok.Getter;
 import lombok.ToString;
 import lombok.extern.slf4j.Slf4j;
 
@@ -10,21 +11,51 @@ import java.util.Map;
 @EqualsAndHashCode
 @Slf4j
 @ToString
+@Getter
 
 public class LibrarySystem {
 
-    Map<Book, String> locationBook = new HashMap<>();
+    private Map<Book, String> locationBook = new HashMap<>();
+    private Map<String, Book> booksName = new HashMap<>();
 
     public void addBook(String title, String author, int year, String location) {
-
-        Book book = new Book(title, author, year);
-        if (locationBook.containsKey(book)) {
-            log.error("Ошибка,книга уже существует.");
-            throw new IllegalArgumentException("Книга уже существует в библиотеке.");
+        if (title == null || author == null || location == null || title.isEmpty()
+                || author.isEmpty() || location.isEmpty()) {
+            throw new IllegalArgumentException("title, author или location " +
+                    " не могут быть null или пустыми");
+        }
+        if (booksName == null || locationBook == null) {
+            throw new IllegalStateException("booksName или locationBook не используются");
+        }
+        Book book;
+        String key = title + author + year;
+        if (booksName.containsKey(key)) {
+            book = booksName.get(key);
+            log.info("книга ({}) изменила локацию на ({})", book, location);
+            return;
         } else {
-            locationBook.put(book, location);
+            book = new Book(title, author, year);
+            log.info("добавлена новая книга: ({}) с локацией ({})", book, location);
+        }
+        locationBook.put(book, location);
+        booksName.put(key, book);
+    }
+
+    public void removeBook(String title, String author, int year) {
+        Book book = new Book(title, author, year);
+
+        // Проверка на пустоту коллекции
+        if (locationBook.isEmpty()) {
+            System.out.println("В библиотеке нет книг");
+            return; // Выход из метода, если коллекция пуста
         }
 
-
+        // Проверка наличия книги
+        if (!locationBook.containsKey(book)) {
+            System.out.println("Такой книги нет");
+        } else {
+            locationBook.remove(book); // Удаление книги
+            System.out.println("Книга удалена");
+        }
     }
 }
--- src/main/java/school/faang/library_of_westeros/Main.java
+++ src/main/java/school/faang/library_of_westeros/Main.java
@@ -2,5 +2,12 @@ package school.faang.library_of_westeros;
 
 public class Main {
     public static void main(String[] args) {
+
+        LibrarySystem novay = new LibrarySystem();
+        novay.addBook("ew", "gff", 45, "N1");
+        novay.addBook("ew23", "gff", 45, "N3");
+        novay.removeBook("ew24545", "gff", 45);
+
+
     }
 }
