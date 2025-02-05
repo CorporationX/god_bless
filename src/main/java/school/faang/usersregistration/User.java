fba0ff44c5b410595efbeb9d4bc2d40d0871e4c8
phtanya <112714509+phtanya@users.noreply.github.com>
2025-02-05 18:41:21 +0100

    BJS2-56406

--- src/main/java/school/faang/usersregistration/User.java
+++ src/main/java/school/faang/usersregistration/User.java
@@ -1,4 +1,18 @@
 package school.faang.usersregistration;
 
+import java.util.HashSet;
+import java.util.Set;
+
 public class User {
+    private String name;
+    private int age;
+    private String job;
+    private String address;
+    static final Set<String> VALID_JOBS = Set.of("Google", "Uber", "Amazon");
+    static final Set<String> VALID_ADDRESSES = Set.of("London", "New York", "Amsterdam");
+
+    public User(String name, int age, String job, String address){
+        if (this.name
+
+    }
 }
