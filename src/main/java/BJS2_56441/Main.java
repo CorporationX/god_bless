71d6a175e156364a036edbeb3a937c2af0df6f78
saroyangor <dr.saroyan98@gmail.com>
2025-02-05 15:21:27 +0400

    Регистрация пользователей

---
+++ src/main/java/BJS2_56441/Main.java
@@ -0,0 +1,12 @@
+package BJS2_56441;
+
+public class Main {
+    @SuppressWarnings("checkstyle:CommentsIndentation")
+    public static void main(String[] args) {
+        User gor = new User("", 25, "Google", "London");
+        User ani = new User("Ani", 15, "Google", "London");
+        User lilith = new User("Lilith", 35, "Tumo", "London");
+        User sasha = new User("Sasha", 33, "Amazon", "Moscow");
+        User vlad = new User("Vlad", 23, "Uber", "Amsterdam");
+    }
+}
---
+++ src/main/java/BJS2_56441/User.java
@@ -0,0 +1,33 @@
+package BJS2_56441;
+
+import java.util.Set;
+
+public class User {
+    private final String name;
+    private final int age;
+    private final String job;
+    private final String address;
+
+    public static final Set<String> VALID_JOBS = Set.of("Google", "Uber", "Amazon");
+    public static final Set<String> VALID_ADDRESSES = Set.of("London", "New York", "Amsterdam");
+
+    public User(String name, int age, String job, String address) {
+        if (name.isBlank()) {
+            throw new IllegalArgumentException("Имя не может быть пустым.");
+        }
+        if (age < 18) {
+            throw new IllegalArgumentException("Возраст не может быть меньше 18.");
+        }
+        if (!VALID_JOBS.contains(job)) {
+            throw new IllegalArgumentException("Место работы должно содержаться в наборе VALID_JOBS.");
+        }
+        if (!VALID_ADDRESSES.contains(address)) {
+            throw new IllegalArgumentException("Адрес должен содержаться в наборе VALID_ADDRESSES.");
+        }
+
+        this.name = name;
+        this.age = age;
+        this.job = job;
+        this.address = address;
+    }
+}
