1c1dd1442fa5ae29d3e1abe025b844dd474c4972
Yaroslav Olkin <shrelok.pupkin@yandex.ru>
2025-02-05 18:02:01 +0300

    .

---
+++ src/main/java/school/faang/task_57076/HogwartsSpells.java
@@ -0,0 +1,16 @@
+package school.faang.task_57076;
+
+import com.fasterxml.jackson.annotation.JsonTypeInfo;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class HogwartsSpells {
+    private static Map<Integer, SpellEvent> spellById = new HashMap<>();
+    private static Map<String, List<SpellEvent>> spellsByType = new HashMap<>();
+
+    public void addSpellEvent() {
+
+    }
+}
---
+++ src/main/java/school/faang/task_57076/SpellEvent.java
@@ -0,0 +1,12 @@
+package school.faang.task_57076;
+
+import lombok.Getter;
+import lombok.AllArgsConstructor;
+
+@Getter
+@AllArgsConstructor
+public class SpellEvent {
+    private int id;
+    private String eventType;
+    private String action;
+}
