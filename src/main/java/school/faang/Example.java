05a09d058083fa815415e51b58984674d3d40500
Evgeny Morzeev <morzeevevg@gmail.com>
2025-02-05 22:35:55 +0300

    BJS2-56395, task completed

---
+++ src/main/java/school/faang/Example.java
@@ -0,0 +1,17 @@
+package school.faang;
+
+public class Example {
+    public static int[] reverse(int[] nums) {
+        int left = 0;
+        int right = nums.length - 1;
+
+        while (left < right) {
+            int temp = nums[left];
+            nums[left] = nums[right];
+            nums[right] = temp;
+            left++;
+            right--;
+        }
+        return nums;
+    }
+}
--- src/main/java/school/faang/Main.java
+++ src/main/java/school/faang/Main.java
@@ -1,4 +1,10 @@
 package school.faang;
 
+import java.util.Arrays;
+
 public class Main {
+    public static void main(String[] args) {
+        int[] nums = {2, 55, -3, 4, 19, -7, 0, 1};
+        System.out.println(Arrays.toString(Example.reverse(nums)));
+    }
 }
