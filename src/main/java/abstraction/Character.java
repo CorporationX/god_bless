116d1b6a46c54321cd922931ee24497acf05fb39
Laura Lomberg <lomberglaura@gmail.com>
2025-02-05 15:28:13 +0300

    Refactor code

--- src/main/java/abstraction/Character.java
+++ src/main/java/abstraction/Character.java
@@ -2,9 +2,11 @@ package abstraction;
 
 import lombok.Getter;
 import lombok.Setter;
+import lombok.ToString;
 
 @Getter
 @Setter
+@ToString
 public abstract class Character {
     private String name;
     private int strength;
@@ -13,11 +15,7 @@ public abstract class Character {
     private int health;
 
     public Character(String name) {
-        this.name = name;
-        this.strength = 5;
-        this.agility = 5;
-        this.intelligence = 5;
-        this.health = 100;
+        this(name, 5, 5, 5);
     }
 
     public Character(String name, int strength, int agility, int intelligence) {
@@ -32,11 +30,5 @@ public abstract class Character {
 
     protected void reduceHealth(int damage) {
         health = Math.max(0, health - damage);
-    }
-
-    @Override
-    public String toString() {
-        return String.format("%s: strength = %d, agility = %d, intelligence = %d, health = %d",
-                name, strength, agility, intelligence, health);
     }
 }
