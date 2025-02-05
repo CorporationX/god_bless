cad75c517bfac78fa6653b8eccd6a2f2c71135a6
Max Goloviznin <roxet59rus@mail.ru>
2025-02-05 19:45:00 +0500

    Разбираем склад Амазон

--- src/main/java/school/faang/amazonstorage/Product.java
+++ src/main/java/school/faang/amazonstorage/Product.java
@@ -1,4 +1,24 @@
 package school.faang.amazonstorage;
 
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@EqualsAndHashCode
+@ToString
+@Getter
+@Setter
 public class Product {
+    private static int idCounter = 100;
+
+    private int id;
+    private String name;
+    private Category category;
+
+    public Product(String name, Category category) {
+        this.id = idCounter++;
+        this.name = name;
+        this.category = category;
+    }
 }
--- src/main/java/school/faang/amazonstorage/ProductManager.java
+++ src/main/java/school/faang/amazonstorage/ProductManager.java
@@ -1,4 +1,74 @@
 package school.faang.amazonstorage;
 
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
+@Getter
+@Setter
+@ToString
 public class ProductManager {
+    private static final String PRINT_SET_PRODUCTS_FORMAT = "Id: %d, category: %s, name: %s\n";
+    private static final String PRINT_CATEGORY_FORMAT = "Category: %s\n";
+    private static final String PRINT_PRODUCT_FORMAT = "- %s\n";
+
+    private Set<Product> products = new HashSet<>();
+    private Map<Category, List<Product>> categoryMap = new HashMap<>();
+
+    public void addProduct(Category category, String name) {
+        Product product = new Product(name, category);
+        products.add(product);
+        groupProductsByCategory();
+    }
+
+    public void removeProduct(Category category, String name) {
+        categoryMap.get(category).removeIf(product -> product.getName().equals(name));
+        products.removeIf(product -> product.getName().equals(name)
+                && product.getCategory().equals(category));
+    }
+
+    public void findProductsByCategory(Category category) {
+        if (categoryMap.containsKey(category)) {
+            printProduct(category);
+        } else {
+            System.out.println("There are no products in the category " + category);
+        }
+    }
+
+    private void groupProductsByCategory() {
+        categoryMap.clear();
+        products.forEach(product -> {
+            if (categoryMap.containsKey(product.getCategory())) {
+                categoryMap.get(product.getCategory()).add(product);
+            } else {
+                categoryMap.computeIfAbsent(product.getCategory(), key ->
+                        new ArrayList<>()).add(product);
+            }
+        });
+    }
+
+    public void printAllProducts() {
+        categoryMap.forEach((category, products) -> printProduct(category));
+    }
+
+    public void printAllProductsFromSet() {
+        products.forEach(product -> System.out.printf(PRINT_SET_PRODUCTS_FORMAT,
+                product.getId(), product.getCategory(), product.getName()));
+        System.out.println();
+    }
+
+    private void printProduct(Category category) {
+        System.out.printf(PRINT_CATEGORY_FORMAT, category);
+        System.out.println("Products:");
+        categoryMap.get(category).forEach(product ->
+                System.out.printf(PRINT_PRODUCT_FORMAT, product.getName()));
+        System.out.println();
+    }
 }
