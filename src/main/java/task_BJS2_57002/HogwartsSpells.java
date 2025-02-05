c03610619ff1ed3030ae8595155575a7100206e5
Anton Lukyanchikov <antontotenkopf@gmail.com>
2025-02-05 17:32:36 +0300

    created classes SpellEvent and HogwartsSpells

---
+++ src/main/java/task_BJS2_57002/HogwartsSpells.java
@@ -0,0 +1,11 @@
+package task_BJS2_57002;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class HogwartsSpells {
+    private Map<Integer, SpellEvent> spellById = new HashMap<>();
+    private Map<String, List<SpellEvent>> spellsByType = new HashMap<>();
+
+}
