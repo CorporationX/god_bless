e70a05806384d0ea85eca16b3ef831e5505ba94a
Evgeny Morzeev <morzeevevg@gmail.com>
2025-02-05 22:57:47 +0300

    BJS2-57055, task completed

---
+++ src/main/java/school/faang/LibrarySystem.java
@@ -0,0 +1,40 @@
+package school.faang;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class LibrarySystem {
+    Map<Book, String> library = new HashMap<>();
+
+    public void addBook(String title, String author, int year, String location) {
+        Book book = new Book(title, author, year);
+        library.put(book, location);
+        System.out.println(book + " added at location " + location);
+    }
+
+    public void removeBook(String title, String author, int year) {
+        Book book = new Book(title, author, year);
+        if (library.containsKey(book)) {
+            library.remove(book);
+            System.out.println(book + " successfully removed!");
+        } else {
+            System.out.println(book + " doesn't exist!");
+        }
+    }
+
+    public void findBook(String title, String author, int year) {
+        Book book = new Book(title, author, year);
+        if (library.containsKey(book)) {
+            System.out.println(book + " found at " + library.get(book));
+        } else {
+            System.out.println(book + " not found!");
+        }
+    }
+
+    public void printAllBooks() {
+        System.out.println("\nLibrary contains the following books: ");
+        for (var entry : library.entrySet()) {
+            System.out.println(entry.getKey() + " is located at " + entry.getValue());
+        }
+    }
+}
--- src/main/java/school/faang/Main.java
+++ src/main/java/school/faang/Main.java
@@ -1,4 +1,18 @@
 package school.faang;
 
 public class Main {
+    public static void main(String[] args) {
+        LibrarySystem library = new LibrarySystem();
+
+        library.addBook("The Ingenious Gentleman Don Quixote of La Mancha", "Miguel de Cervantes", 1605, "F.1");
+        library.addBook("Autumn of the Patriarch", "Gabriel Garcia Marquez", 1975, "B.1");
+        library.addBook("Flotsam", "Erich Maria Remarque", 1939, "L.2");
+
+        library.findBook("Don Quixote", "Miguel de Cervantes", 1605);
+        library.findBook("The Count of Monte Cristo", " Alexandre Dumas", 1844);  // Книги нет в библиотеке
+
+        library.removeBook("Flotsam", "Erich Maria Remarque", 1939);
+
+        library.printAllBooks();
+    }
 }
