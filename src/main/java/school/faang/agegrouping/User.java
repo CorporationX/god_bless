58b622825884b9dd64f59fa48bc5e522f96f7f20
phtanya <112714509+phtanya@users.noreply.github.com>
2025-02-05 17:09:40 +0100

    BJS2-56347

---
+++ src/main/java/school/faang/agegrouping/User.java
@@ -0,0 +1,30 @@
+package school.faang.agegrouping;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
+public class User {
+    private String name;
+    private int age;
+    private String workPlace;
+    private String address;
+
+    public User(String name, int age, String workPlace, String address){
+        this.name = name;
+        this.age = age;
+        this.workPlace = workPlace;
+        this.address = address;
+    }
+
+    public static HashMap<Integer, List<User>> groupingByAge(List<User> users){
+        HashMap<Integer, List<User>> result = new HashMap<>();
+        for(User user : users){
+            if(!result.containsKey(user.age)){
+                result.put(user.age, new ArrayList<>());
+            }
+            result.get(user.age).add(user);
+        }
+        return result;
+    }
+}
