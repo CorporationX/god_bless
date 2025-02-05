1a1e87aa61626821eabbf7d113611eb8f7989328
Dmitry Rogulkin <rogulkind@yandex.ru>
2025-02-05 17:44:26 +0500

    https://faang-school.atlassian.net/browse/BJS2-57058

--- src/main/java/school/faang/service/HogwartsSpells.java
+++ src/main/java/school/faang/service/HogwartsSpells.java
@@ -1,20 +1,30 @@
 package school.faang.service;
 
+import lombok.Data;
+
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+@Data
 public class HogwartsSpells {
-    Map<Integer, SpellEvent> spellById = new HashMap<>();
-    Map<String, List<SpellEvent>> spellsByType = new HashMap<>();
+    private final Map<Integer, SpellEvent> spellById = new HashMap<>();
+    private final Map<String, List<SpellEvent>> spellsByType = new HashMap<>();
+    private int currentId = 1;
 
-    /**
-     * Создает объект класса SpellEvent, автоматически генерирует уникальный ID, добавляет событие в обе HashMap;
-     * @param eventType - тип заклинания (например, “Чар”, “Трансфигурация”, “Защита”);
-     * @param actionDescription - строка, которая описывает действие заклинания (например, "Защищает от атак" или "Преобразует предмет").
-     */
     public void addSpellEvent(String eventType, String actionDescription) {
+        int id = currentId++;
+        SpellEvent spellEvent = new SpellEvent(id, eventType, actionDescription);
+        spellById.put(id, spellEvent);
 
+        spellsByType.computeIfAbsent(eventType, k -> new ArrayList<>()).add(spellEvent);
+    }
+
+    public void printAllSpellEvents() {
+        for (Map.Entry<Integer, SpellEvent> entry : spellById.entrySet()) {
+            System.out.println(entry.getKey() + " " + entry.getValue());
+        }
     }
 
 }
