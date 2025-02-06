2dcab3683c1a6fe9e227abd4338768350b630eba
Yaroslav Olkin <shrelok.pupkin@yandex.ru>
2025-02-06 18:43:36 +0300

    complete task

--- src/main/java/school/faang/task_57076/HogwartsSpells.java
+++ src/main/java/school/faang/task_57076/HogwartsSpells.java
@@ -1,16 +1,58 @@
 package school.faang.task_57076;
 
-import com.fasterxml.jackson.annotation.JsonTypeInfo;
-
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.security.SecureRandom;
+import java.util.*;
 
 public class HogwartsSpells {
-    private static Map<Integer, SpellEvent> spellById = new HashMap<>();
-    private static Map<String, List<SpellEvent>> spellsByType = new HashMap<>();
+    private Map<Integer, SpellEvent> spellById = new HashMap<>();
+    private Map<String, List<SpellEvent>> spellsByType = new HashMap<>();
 
-    public void addSpellEvent() {
+    public void addSpellEvent(String eventType, String actionDescription) {
+        int id = generateId();
+        SpellEvent spellEvent = new SpellEvent(id, eventType, actionDescription);
 
+        spellById.put(id, spellEvent);
+        spellsByType.putIfAbsent(eventType, new ArrayList<>());
+        spellsByType.get(eventType).add(spellEvent);
+    }
+
+    public SpellEvent getSpellEventById(int id) {
+        return spellById.get(id);
+    }
+
+    public List<SpellEvent> getSpellByType(String eventType) {
+        return spellsByType.getOrDefault(eventType, new ArrayList<>());
+    }
+
+    public void deleteSpellEvent(int id) {
+        SpellEvent spellEvent = spellById.remove(id);
+        if (spellEvent != null) {
+            List<SpellEvent> events = spellsByType.get(spellEvent.getEventType());
+            if (events != null) {
+                events.remove(spellEvent);
+            }
+        }
+    }
+
+    public void printAllSpellEvents() {
+        for (var entry : spellById.entrySet()) {
+            var value = entry.getValue();
+            System.out.printf("Id: %d \nEvent Type: %s \nAction: %s\n\n",
+                    value.getId(), value.getEventType(), value.getAction());
+        }
+    }
+
+    public int generateId() {
+        SecureRandom random = new SecureRandom();
+
+        int length = 6;
+        StringBuilder builder = new StringBuilder(length);
+
+        for (int i = 0; i < length; i++) {
+            int digit = random.nextInt(10);
+            builder.append(digit);
+        }
+
+        return Integer.parseInt(builder.toString());
     }
 }
--- src/main/java/school/faang/task_57076/SpellEvent.java
+++ src/main/java/school/faang/task_57076/SpellEvent.java
@@ -2,9 +2,11 @@ package school.faang.task_57076;
 
 import lombok.Getter;
 import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
 
 @Getter
 @AllArgsConstructor
+@EqualsAndHashCode
 public class SpellEvent {
     private int id;
     private String eventType;
