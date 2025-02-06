00afa7abe50e20e8240b560ccde82360cc495019
Dmitry Rogulkin <rogulkind@yandex.ru>
2025-02-06 12:22:17 +0500

    https://faang-school.atlassian.net/browse/BJS2-56900

--- src/main/java/school/faang/service/Category.java
+++ src/main/java/school/faang/service/Category.java
@@ -1,4 +1,8 @@
 package school.faang.service;
 
 public enum Category {
+    FOOD,
+    ELECTRONICS,
+    CLOTHING,
+    OTHER
 }
--- src/main/java/school/faang/service/Product.java
+++ src/main/java/school/faang/service/Product.java
@@ -1,8 +1,20 @@
 package school.faang.service;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
 
+@Data
+@AllArgsConstructor
 public class Product {
+    private static int uniqueId = 1;
     private int id;
     private String name;
     private Category category;
+
+    public Product(String name, Category category) {
+        this.id = uniqueId++;
+        this.name = name;
+        this.category = category;
+    }
 }
+
