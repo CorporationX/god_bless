6a32580e3cc26bbc44c833f87fe6b180f02fa410
Evgeny Sledkov <elsledkov@gmail.com>
2025-02-06 16:55:30 +0300

    elsledkov: user registration - add class Main

---
+++ src/main/java/school/faang/spring1/basicjava/userregistration/Main.java
@@ -0,0 +1,17 @@
+package school.faang.spring1.basicjava.userregistration;
+
+public class Main {
+    public static void main(String[] args) {
+        try {
+            User user1 = new User("Petr", 20, "Amazon", "London");
+            System.out.println(user1);
+            User user2 = new User("Olga", 30, "Google", "Moscow");
+            System.out.println(user2);
+        } catch (IllegalArgumentException e) {
+            System.out.println("Ошибка при создании пользователя: " + e.getMessage());
+        }
+
+
+
+    }
+}
