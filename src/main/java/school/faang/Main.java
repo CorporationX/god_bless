6f2cfbf58c96c892898f79de8ff87e3edba83f14
naught_wo <catrinesaxon@yandex.ru>
2025-02-06 22:48:51 +0300

    Задание "Библиотека Вестеросса"

--- src/main/java/school/faang/Main.java
+++ src/main/java/school/faang/Main.java
@@ -1,8 +1,5 @@
 package school.faang;
 
-import java.util.HashMap;
-import java.util.Map;
-
 public class Main {
     public static void main(String[] args) {
         LibrarySystem librarySystem = new LibrarySystem();
