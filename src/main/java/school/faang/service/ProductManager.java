3bd46f9b45c1d3f992462f14c4de6df5df8d0c78
Dmitry Rogulkin <rogulkind@yandex.ru>
2025-02-06 14:49:54 +0500

    https://faang-school.atlassian.net/browse/BJS2-56900

--- src/main/java/school/faang/service/ProductManager.java
+++ src/main/java/school/faang/service/ProductManager.java
@@ -3,7 +3,6 @@ package school.faang.service;
 import lombok.Data;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.List;
@@ -11,7 +10,6 @@ import java.util.Map;
 import java.util.Set;
 
 @Data
-
 public class ProductManager {
     private Set<Product> products = new HashSet<>();
     private Map<Category, List<String>> categoryMap = new HashMap<>();
@@ -33,8 +31,6 @@ public class ProductManager {
         }
     }
 
-    //    findProductsByCategory(Category category): возвращает список
-//    всех продуктов в указанной категории. Если категория отсутствует, метод возвращает пустой список.
     public Set<Product> findProductsByCategory(Category category) {
         Set<Product> result = new HashSet<>();
         for (Product value : products) {
@@ -45,12 +41,29 @@ public class ProductManager {
         return result;
     }
 
+    public Map<Category, List<Product>> groupProductsByCategory(Set<Product> products) {
+        Map<Category, List<Product>> productMap = new HashMap<>();
+        for (Product product : products) {
+            if (!productMap.containsKey(product.getCategory())) {
+                productMap.put(product.getCategory(), new ArrayList<>());
+            }
+            productMap.get(product.getCategory()).add(product);
+        }
+        return productMap;
+    }
+
+    public static void printProductsByCategory(Map<Category, List<Product>> groupedProducts) {
+        for (Map.Entry<Category, List<Product>> entry : groupedProducts.entrySet()) {
+            System.out.println("Категория: " + entry.getKey());
+            for (Product product : entry.getValue()) {
+                System.out.println("  - " + product.getName());
+            }
+        }
+    }
+
     public void printAllProducts() {
         for (Product value : products) {
-            System.out.println(value.getId() + " " + value.getCategory() + " " + value.getName());
+            System.out.println("Категория: " + value.getCategory() + ", Продукт: " + value.getName());
         }
-        System.out.println("------------------");
-        System.out.println(categoryMap);
-        System.out.println();
     }
 }
