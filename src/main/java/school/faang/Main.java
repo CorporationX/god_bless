0ce24d33280843a62c90dcf684934b7dce8f1712
naught_wo <catrinesaxon@yandex.ru>
2025-02-06 21:39:17 +0300

    Задание "Развернитесь!"

--- src/main/java/school/faang/Main.java
+++ src/main/java/school/faang/Main.java
@@ -1,4 +1,10 @@
 package school.faang;
 
+import java.util.Arrays;
+
 public class Main {
+    public static void main(String[] args) {
+        int[] nums = {10, 8, 9, 7, 6, 4, 5, 3, 2, 0, 1};
+        System.out.println(Arrays.toString(nums) + " rewerse: " + Arrays.toString(Example.reverse(nums)));
+    }
 }
