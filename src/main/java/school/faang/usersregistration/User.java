c3c6ffcf8d2ab27ecd347472ac9d86a1bcf0359b
phtanya <112714509+phtanya@users.noreply.github.com>
2025-02-06 21:32:10 +0100

    BJS2-56406

--- src/main/java/school/faang/usersregistration/User.java
+++ src/main/java/school/faang/usersregistration/User.java
@@ -1,29 +1,31 @@
 package school.faang.usersregistration;
 
+import static java.util.Set.of;
 import java.util.Set;
 
 public class User {
-    private String name;
-    private int age;
-    private String job;
-    private String address;
-    static final Set<String> VALID_JOBS = Set.of("Google", "Uber", "Amazon");
-    static final Set<String> VALID_ADDRESSES = Set.of("London", "New York", "Amsterdam");
-    final int validAge = 18;
+    private final String name;
+    private final int age;
+    private final String job;
+    private final String address;
+    static final Set<String> VALID_JOBS = of("Google", "Uber", "Amazon");
+    static final Set<String> VALID_ADDRESSES = of("London", "New York", "Amsterdam");
+    static final int MINIMUM_AGE = 18;
 
     public User(String name, int age, String job, String address) {
-        if (name.isEmpty()) {
+        if (name == null || name.isBlank()) {
             throw new IllegalArgumentException("Name cannot be empty");
-        } else if (age < validAge) {
+        } else if (age < MINIMUM_AGE) {
             throw new IllegalArgumentException("Age must be over 18");
         } else if (!VALID_JOBS.contains(job)) {
-            throw new IllegalArgumentException("Job is not valid");
+            throw new IllegalArgumentException("Job must be one of: Google, Uber, Amazon");
         } else if (!VALID_ADDRESSES.contains(address)) {
-            throw new IllegalArgumentException("Address is not valid");
+            throw new IllegalArgumentException("Address must be one of: London, New York, Amsterdam");
         }
+
+        this.name = name;
         this.age = age;
         this.job = job;
-        this.name = name;
         this.address = address;
     }
 }
