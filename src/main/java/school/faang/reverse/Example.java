7e2847c6cec88704d382edfa07ceed42a9b0c5b0
Sergey <bigwave2915@gmail.com>
2025-02-06 14:38:39 +0500

    Решение Задачи

--- src/main/java/school/faang/reverse/Example.java
+++ src/main/java/school/faang/reverse/Example.java
@@ -1,7 +1,7 @@
 package school.faang.reverse;
 
 public class Example {
-    public void reverse(int[] array) {
+    public static void reverse(int[] array) {
         if (array == null) {
             throw new IllegalArgumentException("Массив не должен быть null");
         }
@@ -17,7 +17,24 @@ public class Example {
             array[i] = array[length - i];
             array[length - i] = temp;
         }
-        Number [] numbers = new Number [] {34, 45, 67, 444, -435};
+    }
 
+    public static void main(String[] args) {
+        int[] array = {1, 2, 3, 4, 5};
+        // Выводим исходный массив
+        System.out.println("Исходный массив:");
+        for (int i : array) {
+            System.out.print(i + " ");
+        }
+        System.out.println();
+
+        // Вызываем метод reverse
+        reverse(array);
+
+        // Выводим измененный массив
+        System.out.println("Массив после reverse:");
+        for (int i : array) {
+            System.out.print(i + " ");
+        }
     }
 }
--- src/main/java/school/faang/reverse/Main.java
+++
@@ -1,6 +0,0 @@
-package school.faang.reverse;
-
-public class Main {
-    Number [] numbers = new Number [] {34, 45, 67, 444, -435};
-
-}
