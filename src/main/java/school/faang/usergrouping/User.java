d4523a013ee1d6b63d3a2f15ad22ab50549fc3c3
amanzholtanat <amanzholtanat@gmail.com>
2025-02-06 21:42:50 +0500

    Группировка пользователей по возрасту - BJS2-56403. Учтены комментарий

--- src/main/java/school/faang/usergrouping/User.java
+++ src/main/java/school/faang/usergrouping/User.java
@@ -22,40 +22,45 @@ public class User {
         return age;
     }
 
-    public static Map<Integer, List<User>> groupUsers(List<User> users){
-        Map<Integer, List<User>> groupedUsers = new HashMap<>();
-        for(User user : users){
-            if(!groupedUsers.containsKey(user.getAge())){
-                groupedUsers.put(user.getAge(), new ArrayList<>());
-            }
-            groupedUsers.get(user.getAge()).add(user);
+    public String getName() {
+        return name;
+    }
 
+    public String getWorkplace() {
+        return workplace;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public static Map<Integer, List<User>> groupUsers(List<User> users) {
+        Map<Integer, List<User>> groupedUsers = new HashMap<>();
+        if (users != null || !users.isEmpty()) {
+            for (User user : users) {
+                if (!groupedUsers.containsKey(user.getAge())) {
+                    groupedUsers.put(user.getAge(), new ArrayList<>());
+                }
+                groupedUsers.get(user.getAge()).add(user);
+            }
         }
         return groupedUsers;
     }
 
     @Override
     public String toString() {
-        return "User{" +
-                "name='" + name + '\'' +
-                ", age=" + age +
-                ", workplace='" + workplace + '\'' +
-                ", address='" + address + '\'' +
-                '}';
+        String newToString = String.format("Name: %s, worlplace: %s, address: %s",
+                name, workplace, address);
+        return newToString;
     }
 
     public static void main(String[] args) {
-        User user1 = new User("Alex", 21, "Uber", "1st Avenue");
+        User user1 = new User("Oleg", 21, "Uber", "1st Avenue");
         User user2 = new User("Misha", 22, "Google", "2nd Avenue");
         User user3 = new User("Jenya", 21, "Facebook", "2nd Avenue");
         User user4 = new User("Olya", 23, "Amazon", "3nd Avenue");
         User user5 = new User("Anya", 23, "Uber", "1st Avenue");
-        List<User> users = new ArrayList<>();
-        users.add(user1);
-        users.add(user2);
-        users.add(user3);
-        users.add(user4);
-        users.add(user5);
+        List<User> users = List.of(user1, user2, user3, user4, user5);
         System.out.println(groupUsers(users));
     }
 }
