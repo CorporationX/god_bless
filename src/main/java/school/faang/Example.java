1da4b77d1b60d112f6261b986bd134999d3177fe
naught_wo <catrinesaxon@yandex.ru>
2025-02-06 21:44:24 +0300

    Исправленное

--- src/main/java/school/faang/Example.java
+++ src/main/java/school/faang/Example.java
@@ -3,7 +3,7 @@ package school.faang;
 public class Example {
     public static int[] reverse(int[] nums) {
         int[] reverseNums = new int[nums.length];
-        for (int i=0; i < nums.length; i++) {
+        for (int i = 0; i < nums.length; i++) {
             reverseNums[i] = nums[nums.length - 1 - i];
         }
         nums = reverseNums;
