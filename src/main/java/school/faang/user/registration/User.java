fd64ac5aef00029488c44e352b9bf09f7ed8d2a4
amanzholtanat <amanzholtanat@gmail.com>
2025-02-06 08:37:20 +0500

    Регистрация пользователей - BJS2-56466. Учтены комментарий по ревью

--- src/main/java/school/faang/user/registration/User.java
+++ src/main/java/school/faang/user/registration/User.java
@@ -1,27 +1,34 @@
 package school.faang.user.registration;
 
 import java.util.Arrays;
+import java.util.Set;
 
 public class User {
-    public static final String[] VALID_JOBS = {"Google", "Uber", "Amazon"};
-    public static final String[] VALID_ADDRESSES = {"London", "New York", "Amsterdam"};
-    private String name;
-    private int age;
-    private String job;
-    private String address;
+    public static final Set<String> VALID_JOBS = Set.of("Uber", "Google", "Amazon");
+    public static final Set<String> VALID_ADDRESSES = Set.of("London", "New York", "Amsterdam");
+    private final String name;
+    private final int age;
+    private final String job;
+    private final String address;
+    public static final int MIN_AGE = 18
 
     public User(String name, int age, String job, String address) {
-        if(name.equals("")){
-            throw new IllegalArgumentException("Enter your name correctly");
+        if(name == null || job == null || address == null){
+            throw new IllegalArgumentException("Заполните все поля");
         }
-        if(age < 18){
-            throw new IllegalArgumentException("You are too little");
+        if(!name.isBlank()){
+            throw new IllegalArgumentException("Введите ваше имя корректно");
+        }
+        if(age < MIN_AGE){
+            throw new IllegalArgumentException("Регистрация доступна с 18 лет");
         }
         if(!Arrays.asList(VALID_JOBS).contains(job)){
-            throw new IllegalArgumentException("You are in a wrong job");
+            throw new IllegalArgumentException("Неверное значение в поле работы. " +
+                    "Работа должна быть одна из следующих: Google, Uber, Amazon");
         }
         if(!Arrays.asList(VALID_ADDRESSES).contains(address)){
-            throw new IllegalArgumentException("You are in a wrong address");
+            throw new IllegalArgumentException("Неверное значение в поле адреса. " +
+                    "Адрес должен быть один из следующих: London, New York, Amsterdam");
         }
         this.name = name;
         this.age = age;
