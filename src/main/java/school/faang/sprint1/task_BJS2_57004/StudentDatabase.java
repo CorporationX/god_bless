b699e407d504c7d0901394922955fdb2e951876e
Ilhan Mustafa <ilhnmst@gmail.com>
2025-02-06 19:55:25 +0300

    init Classes

---
+++ src/main/java/school/faang/sprint1/task_BJS2_57004/StudentDatabase.java
@@ -0,0 +1,41 @@
+package school.faang.sprint1.task_BJS2_57004;
+
+import java.util.List;
+import java.util.Map;
+
+public class StudentDatabase {
+    private Map<Student, Map<Subject, Integer>> studentSubjects;
+    private Map<Subject, List<Student>> subjectStudents;
+
+    public void addStudent(String name, Map<Subject, Integer> subjectsWithRatings) {
+
+    }
+
+    public void addSubjectToStudent(Subject subject, Student student, int rating) {
+
+    }
+
+    public void removeStudent(Student student) {
+
+    }
+
+    public void printStudentInfo() {
+
+    }
+
+    public void addNewSubject(Subject subject, List<Student> students) {
+
+    }
+
+    public void addStudentToSubject(Subject subject, Student student) {
+
+    }
+
+    public void removeStudentFromSubject(Subject subject, Student student) {
+
+    }
+
+    public void printSubjectsWithStudents() {
+
+    }
+}
---
+++ src/main/java/school/faang/sprint1/task_BJS2_57004/Subject.java
@@ -0,0 +1,5 @@
+package school.faang.sprint1.task_BJS2_57004;
+
+public class Subject {
+    private String name;
+}
