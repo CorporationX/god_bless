2cc9de4e85a67845e2e2710c14126c143dfbfee6
Ilhan Mustafa <ilhnmst@gmail.com>
2025-02-06 23:53:02 +0300

    Add JavaDoc to reverse Change value tempValue to temp Change logic cycle

--- src/main/java/school/faang/sprint1/task_BJS2_56355/Example.java
+++ src/main/java/school/faang/sprint1/task_BJS2_56355/Example.java
@@ -3,9 +3,18 @@ package school.faang.sprint1.task_BJS2_56355;
 /**
  * File for test:
  * test/java/school/faang/sprint1/task_BJS2_56355/ExampleTest
+ *
  */
 
 public class Example {
+    /**
+     * Реверсирует порядок элементов в переданном массиве целых чисел.
+     * Массив изменяется "на месте", то есть исходный массив будет модифицирован.
+     *
+     * @param array Массив целых чисел, который необходимо реверсировать.
+     *             Не может быть {@code null}.
+     * @throws IllegalArgumentException Если переданный массив равен {@code null}.
+     */
     public void reverse(int[] array) {
         if (array == null) {
             throw new IllegalArgumentException("Массив не должен быть null");
@@ -14,13 +23,13 @@ public class Example {
             return;
         }
 
-        int tempValue;
-        int length = array.length - 1;
+        int temp;
+        int length = array.length;
 
-        for (int i = 0; i <= length / 2; i++) {
-            tempValue = array[i];
-            array[i] = array[length - i];
-            array[length - i] = tempValue;
+        for (int i = 0; i < length / 2; i++) {
+            temp = array[i];
+            array[i] = array[length - i - 1];
+            array[length - i - 1] = temp;
         }
     }
 }
