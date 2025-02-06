cfc6e760a142c9b3c18a0b16b380f61c33b04c38
Sergei Ananev <sa.ananyev@yandex.ru>
2025-02-07 00:24:21 +0300

    BJS2-56456 created abstract class Character and successor classes (Warrior and Archer)

---
+++ src/main/java/school/faang/task_56456/Archer.java
@@ -0,0 +1,17 @@
+package school.faang.task_56456;
+
+public class Archer extends Character {
+    private static final int BASIC_STRENGTH_VALUE = 3;
+    private static final int BASIC_AGILITY_VALUE = 10;
+    private static final int BASIC_INTELLIGENCE_VALUE = 5;
+
+    public Archer(String name) {
+        super(name, BASIC_STRENGTH_VALUE, BASIC_AGILITY_VALUE, BASIC_INTELLIGENCE_VALUE);
+    }
+
+    @Override
+    void attack(Character opponent) {
+        opponent.setHealth(opponent.getHealth() - this.getAgility());
+        opponent.checkNegativeHealth();
+    }
+}
