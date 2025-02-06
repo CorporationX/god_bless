c8817f8787ead055b574fcea80e76de1714668b6
RamilNazmiev192 <nazmiev633@gmail.com>
2025-02-06 09:17:10 +0300

    fix: improve input validation and error handling

--- src/main/java/school/faang/BJS2_56426/User.java
+++ src/main/java/school/faang/BJS2_56426/User.java
@@ -15,16 +15,7 @@ public class User {
     private static final Set<String> VALID_ADDRESSES = Set.of("London", "New York", "Amsterdam");
 
     public User(String name, int age, String workPlace, String address) {
-        List<String> errors = new ArrayList<>();
-
-        validName(name, errors);
-        validAge(age, errors);
-        validWorkPlace(workPlace, errors);
-        validAddress(address, errors);
-
-        if (!errors.isEmpty()) {
-            throw new IllegalArgumentException("Ошибки валидации: \n" + String.join("\n", errors));
-        }
+        validate(name, age, workPlace, address);
 
         this.name = name;
         this.age = age;
@@ -32,46 +23,42 @@ public class User {
         this.address = address;
     }
 
-    private static String validName(String name, List<String> errors) {
+    private static void validate(String name, int age, String workPlace, String address) {
+        List<String> errors = new ArrayList<>();
+
         if (name == null || name.trim().isEmpty()) {
             errors.add("Имя не может быть пустым");
         }
-        return name;
-    }
 
-    private static int validAge(int age, List<String> errors) {
         if (age < 18) {
             errors.add("Возраст не может быть меньше 18");
         }
-        return age;
-    }
 
-    private static String validWorkPlace(String workPlace, List<String> errors) {
         if (!VALID_JOBS.contains(workPlace)) {
             errors.add("Место работы должно содержаться в наборе VALID_JOBS");
         }
-        return workPlace;
-    }
 
-    private static String validAddress(String address, List<String> errors) {
         if (!VALID_ADDRESSES.contains(address)) {
             errors.add("Адрес должен содержаться в наборе VALID_ADDRESSES.");
         }
-        return address;
+
+        if (!errors.isEmpty()) {
+            throw new IllegalArgumentException("Ошибки валидации: \n" + String.join("\n", errors));
+        }
     }
 
     @Override
     public String toString() {
         return String.format(
                 """
-                ---------------------------
-                User Information:
-                Name     : %s
-                Age      : %d
-                WorkPlace: %s
-                Address  : %s
-                ---------------------------
-                """,
+                        ---------------------------
+                        User Information:
+                        Name     : %s
+                        Age      : %d
+                        WorkPlace: %s
+                        Address  : %s
+                        ---------------------------
+                        """,
                 name, age, workPlace, address
         );
     }
