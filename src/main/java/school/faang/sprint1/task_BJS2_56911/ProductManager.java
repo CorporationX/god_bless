75dbbcfeb08f21e9ecda4c763795f2c1e4a18e0d
Ilhan Mustafa <ilhnmst@gmail.com>
2025-02-06 16:12:09 +0300

    Create tests

--- src/main/java/school/faang/sprint1/task_BJS2_56911/ProductManager.java
+++ src/main/java/school/faang/sprint1/task_BJS2_56911/ProductManager.java
@@ -1,5 +1,6 @@
 package school.faang.sprint1.task_BJS2_56911;
 
+import lombok.Getter;
 import lombok.NonNull;
 
 import java.util.ArrayList;
@@ -8,37 +9,39 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+// for test
+@Getter
 public class ProductManager {
     private final Map<Category, List<Product>> categoryMap = new EnumMap<>(Category.class);
-    private final Map<String, Product> productOnKey = new HashMap<>();
+    private final Map<String, Product> productsOnKey = new HashMap<>();
     private String productKey;
     private Product product;
+
 
     public void addProduct(Category category, @NonNull String name) {
         if (name.isEmpty()) {
             throw new IllegalArgumentException("Имя не может быть пустым");
         }
-        productKey = getProductKey(category, name);
+        productKey = createProductKey(category, name);
 
-        if (!productOnKey.containsKey(productKey)) {
+        if (!productsOnKey.containsKey(productKey)) {
             product = new Product(category, name);
             categoryMap.computeIfAbsent(category, k -> new ArrayList<>());
             categoryMap.get(category).add(product);
-            productOnKey.put(productKey, product);
+            productsOnKey.put(productKey, product);
         }
-
     }
 
     public void removeProduct(Category category, String name) {
         if (name.isEmpty()) {
             throw new IllegalArgumentException("Имя не может быть пустым");
         }
-        productKey = getProductKey(category, name);
+        productKey = createProductKey(category, name);
 
-        if (productOnKey.containsKey(productKey)) {
-            product = productOnKey.get(productKey);
+        if (productsOnKey.containsKey(productKey)) {
+            product = productsOnKey.get(productKey);
             categoryMap.get(category).remove(product);
-            productOnKey.remove(productKey);
+            productsOnKey.remove(productKey);
         }
     }
 
@@ -49,17 +52,21 @@ public class ProductManager {
     public void groupProductsByCategory() {
         categoryMap.clear();
 
-        productOnKey.values().forEach(product -> {
+        productsOnKey.values().forEach(product -> {
             categoryMap.computeIfAbsent(product.getCategory(), k -> new ArrayList<>());
             categoryMap.get(product.getCategory()).add(product);
         });
     }
 
     public void printAllProducts() {
-        productOnKey.values().forEach(System.out::println);
+        categoryMap.forEach((category, products) -> {
+            System.out.printf("Категория: %s\nПродукты:\n", category);
+            products.forEach(product -> System.out.println("- " + product.getName()));
+            System.out.println();
+        });
     }
 
-    private String getProductKey(Category category, String name) {
-        return String.format("%s-%s", category, name);
+    private String createProductKey(Category category, String name) {
+        return String.format("%s_%s", category, name);
     }
 }
