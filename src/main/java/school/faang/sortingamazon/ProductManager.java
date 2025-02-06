2204605c5467f1bed5ed371eaabb0a984a436c9c
Anton Kapshukov <kapshukov.project@gmail.com>
2025-02-06 20:59:34 +0300

    Двойной кэш-кэш (с учетом комментариев)

--- src/main/java/school/faang/sortingamazon/ProductManager.java
+++ src/main/java/school/faang/sortingamazon/ProductManager.java
@@ -1,6 +1,7 @@
 package school.faang.sortingamazon;
 
 import lombok.AllArgsConstructor;
+import lombok.RequiredArgsConstructor;
 
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -8,15 +9,17 @@ import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
+import java.util.stream.Collectors;
 
 @AllArgsConstructor
+@RequiredArgsConstructor
 public class ProductManager {
     private static int idCounter = 1;
     private final Set<Product> products = new HashSet<>();
-    private final Map<Category, List<Product>> categoryMap = new HashMap<>();
+    private Map<Category, List<Product>> categoryMap = new HashMap<>();
 
     public void addProduct(Category category, String name) {
-        int id = idCounter;
+        int id = idCounter++;
         Product product = new Product(id, name, category);
         if (products.contains(product)) {
             System.out.println("Продукт уже существует.");
@@ -30,29 +33,36 @@ public class ProductManager {
     }
 
     public void removeProduct(Category category, String name) {
-        int id = idCounter;
-        Product product = new Product(id, name, category);
-        if (!products.contains(product)) {
+        Product productToRemove = null;
+        for (Product product : products) {
+            if (product.getName().equals(name) && product.getCategory().equals(category)) {
+                productToRemove = product;
+                break;
+            }
+        }
+
+        if (productToRemove == null) {
             System.out.println("Продукт не существует.");
             return;
         }
 
-        categoryMap.get(category).remove(product);
-        products.remove(product);
-        System.out.println("Продукт успешно удален: " + product.toString());
+        categoryMap.get(category).remove(productToRemove);
+        products.remove(productToRemove);
+        System.out.println("Продукт успешно удален: " + productToRemove.toString());
     }
 
     public List<Product> findProductsByCategory(Category category) {
-        return categoryMap.get(category).stream().toList();
+        List<Product> products = categoryMap.get(category);
+        if (products != null) {
+            return products;
+        } else {
+            return List.of();
+        }
     }
 
     public void groupProductsByCategory() {
-        categoryMap.clear();
-        for (Product product : products) {
-            Category category = product.getCategory();
-            categoryMap.putIfAbsent(category, new ArrayList<Product>());
-            categoryMap.get(category).add(product);
-        }
+        categoryMap = products.stream()
+                .collect(Collectors.groupingBy(Product::getCategory));
     }
 
     public void printAllProducts() {
