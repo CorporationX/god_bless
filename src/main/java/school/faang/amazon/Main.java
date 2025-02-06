32d1a4fa96185f3f55fbda8a00ab71eddb9c6d26
Petr Stratonnikov <pstratonni@gmail.com>
2025-02-06 09:05:30 +0100

    The task BJS2-56923 is done

--- src/main/java/school/faang/amazon/Main.java
+++ src/main/java/school/faang/amazon/Main.java
@@ -2,7 +2,29 @@ package school.faang.amazon;
 
 public class Main {
     public static void main(String[] args) {
-        Category category = Category.FOOD;
-        System.out.println(category);
+
+        ProductManager productManager = new ProductManager();
+        try {
+            productManager.addProduct(Category.FOOD, "Apple");
+            productManager.addProduct(Category.FOOD, "Pear");
+            productManager.addProduct(Category.FOOD, "Plum");
+            productManager.addProduct(Category.ELECTRONICS, "TV");
+            productManager.addProduct(Category.ELECTRONICS, "Laptop");
+            productManager.addProduct(Category.ELECTRONICS, "Smartphone");
+            productManager.addProduct(Category.ELECTRONICS, "Tablet");
+            productManager.addProduct(Category.CLOTHING, "Dress");
+            productManager.addProduct(Category.CLOTHING, "Trousers");
+            productManager.addProduct(Category.CLOTHING, "Underpants");
+            productManager.addProduct(Category.OTHER, "Fork");
+            productManager.addProduct(Category.OTHER, "Spoon");
+        } catch (IllegalArgumentException e) {
+            System.out.println(e.getMessage());
+        }
+        productManager.printAllProducts();
+
+        productManager.removeProduct(Category.CLOTHING, "Trousers");
+        productManager.removeProduct(Category.CLOTHING, "Fok");
+        productManager.groupProductsByCategory();
+        productManager.printAllProducts();
     }
 }
--- src/main/java/school/faang/amazon/ProductManager.java
+++ src/main/java/school/faang/amazon/ProductManager.java
@@ -11,12 +11,20 @@ public class ProductManager {
     private final Set<Product> products = new HashSet<>();
     private final Map<Category, List<Product>> categoryMap = new HashMap<>();
 
+    @SuppressWarnings("checkstyle:CommentsIndentation")
     public void addProduct(Category category, String name) {
+        if (name == null || name.isEmpty()) {
+            throw new IllegalArgumentException("Name cannot be null or empty");
+        }
         Product product = new Product(name, category);
         if (!products.add(product)) {
             System.out.println("The product already exists");
             return;
         }
+//        addProductToCategory(category, product);
+    }
+
+    private void addProductToCategory(Category category, Product product) {
         if (categoryMap.containsKey(category)) {
             categoryMap.get(category).add(product);
         } else {
@@ -27,8 +35,10 @@ public class ProductManager {
     public void removeProduct(Category category, String name) {
         Product product = new Product(name, category);
         if (!products.remove(product)) {
-            System.out.println("This product does not exist");
+            System.out.printf("This product %s in category %s does not exist\n", name, category);
             return;
+        } else {
+            System.out.printf("The product %s is removed\n", name);
         }
         if (categoryMap.containsKey(category)) {
             categoryMap.get(category).remove(product);
@@ -37,11 +47,20 @@ public class ProductManager {
 
     public void printAllProducts() {
         for (Map.Entry<Category, List<Product>> entry : categoryMap.entrySet()) {
-            System.out.printf("Category: %s%nProduct:%n", entry.getKey());
+            System.out.printf("""
+                    Category: %s
+                    Products:
+                    """, entry.getKey());
             for (Product product : entry.getValue()) {
                 System.out.println("- " + product);
             }
             System.out.println();
+        }
+    }
+
+    public void groupProductsByCategory() {
+        for (Product product : products) {
+            addProductToCategory(product.getCategory(), product);
         }
     }
 }
