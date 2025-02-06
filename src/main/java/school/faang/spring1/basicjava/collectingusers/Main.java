59a513b73c445159fadce47def75523818c61f57
Evgeny Sledkov <elsledkov@gmail.com>
2025-02-06 13:19:13 +0300

    task: user hobbies - first commit

---
+++ src/main/java/school/faang/spring1/basicjava/collectingusers/Main.java
@@ -0,0 +1,23 @@
+package school.faang.spring1.basicjava.collectingusers;
+
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+public class Main {
+    public static void main(String[] args) {
+        Set<String> hobbies =Set.of(
+                "Чтение", "Футбол", "Шахматы", "Плавание", "Кулинария", "Горные лыжи", "Йога");
+
+        User user1 = new User(1, "Кирилл", 25, Set.of("Чтение", "Шахматы"));
+        User user2 = new User(2, "Мария", 30, Set.of("Рисование", "Йога"));
+        User user3 = new User(3, "Алексей", 28, Set.of( "Охота", "Рыбалка"));
+        User user4 = new User(4, "Ольга", 22, Set.of("Кулинария", "Аэробика", "Плавание"));
+
+        List<User> users = List.of(user1, user2, user3, user4);
+        System.out.println(new User().findHobbyLovers(users, hobbies));
+
+
+    }
+}
