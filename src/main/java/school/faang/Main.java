73f815c8bf2f8ce45a5195d49f2df1a497a143d3
Dmitry Rogulkin <rogulkind@yandex.ru>
2025-02-06 13:54:10 +0500

    https://faang-school.atlassian.net/browse/BJS2-56900

--- src/main/java/school/faang/Main.java
+++ src/main/java/school/faang/Main.java
@@ -1,13 +1,22 @@
 package school.faang;
 
 import school.faang.service.Category;
-import school.faang.service.Product;
 import school.faang.service.ProductManager;
 
 public class Main {
     public static void main(String[] args) {
         ProductManager productManager = new ProductManager();
         productManager.addProduct(Category.FOOD, "Шоколадка");
+        productManager.addProduct(Category.FOOD, "Батон");
         productManager.addProduct(Category.ELECTRONICS, "Телевизор");
+        productManager.addProduct(Category.ELECTRONICS, "Наушники");
+        productManager.addProduct(Category.CLOTHING, "Шуба");
+        productManager.addProduct(Category.CLOTHING, "Трусы");
+        productManager.addProduct(Category.OTHER, "Космический корабль");
+
+        productManager.removeProduct(Category.FOOD, "Батон");
+        productManager.removeProduct(Category.ELECTRONICS, "Наушники");
+
+        productManager.printAllProducts();
     }
 }
