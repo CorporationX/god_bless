33ade3c372b0dc43fc93cc8597983b23b1d84c62
Marsel <mars1212@inbox.ru>
2025-02-07 13:58:33 +0300

    Абстракция, абстракция!

--- src/main/java/school/faang/userregistration/Main.java
+++ src/main/java/school/faang/userregistration/Main.java
@@ -2,15 +2,11 @@ package school.faang.userregistration;
 
 public class Main {
     public static void main(String[] args) {
-
         try {
             User normalUser = new User("Alex", 20, "Google", "London");
-
             User illegalUser = new User(" ", 17, "faang", "Street1");
-
         } catch (IllegalArgumentException e) {
             System.out.println(e.getMessage());
         }
-
     }
 }
--- src/main/java/school/faang/userregistration/User.java
+++ src/main/java/school/faang/userregistration/User.java
@@ -13,34 +13,25 @@ public class User {
 
     private static final Set<String> VALID_JOBS = Set.of("Google", "Uber", "Amazon");
     private static final Set<String> VALID_ADDRESSES = Set.of("London", "New York", "Amsterdam");
-    private static final int ADULT = 18;
+    private static final int MIN_LEGAL_AGE = 18;
 
+    @SuppressWarnings("checkstyle:Indentation")
     public User(String name, int age, String job, String address) {
-
-        if (name == null || name.trim().isEmpty()) {
+        if (name == null || name.isBlank()) {
             throw new IllegalArgumentException("Name cannot be null or empty");
-        } else {
-            this.name = name;
         }
-
-        if (age < ADULT) {
+        this.name = name;
+        if (age < MIN_LEGAL_AGE) {
             throw new IllegalArgumentException("Age cannot be less than 18");
-        } else {
-            this.age = age;
         }
-
-        if (!VALID_JOBS.contains(job)) {
+        this.age = age;
+        if (job == null || !VALID_JOBS.contains(job)) {
             throw new IllegalArgumentException("Job is not a valid job");
-        } else {
-            this.job = job;
         }
-
-        if (!VALID_ADDRESSES.contains(address)) {
+        this.job = job;
+        if (address == null || !VALID_ADDRESSES.contains(address)) {
             throw new IllegalArgumentException("Address is not a valid address");
-        } else {
-            this.address = address;
         }
-
+        this.address = address;
     }
-
 }
