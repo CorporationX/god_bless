47b1582314e2a62f21bc4037da8d862d492a90a5
Evgeny Sledkov <elsledkov@gmail.com>
2025-02-07 09:37:08 +0300

    fix: compile error - replace the names with english ones

--- src/main/java/school/faang/spring1/basicjava/collectingusers/Main.java
+++ src/main/java/school/faang/spring1/basicjava/collectingusers/Main.java
@@ -7,16 +7,14 @@ import java.util.Set;
 
 public class Main {
     public static void main(String[] args) {
-        Set<String> hobbies =Set.of("Чтение", "Футбол", "Шахматы", "Плавание", "Кулинария", "Горные лыжи", "Йога");
+        Set<String> hobbies = Set.of("Reading", "Football", "Chess", "Swimming", "Cooking", "Skiing", "Yoga");
 
-        User user1 = new User(1, "Кирилл", 25, Set.of("Чтение", "Шахматы"));
-        User user2 = new User(2, "Мария", 30, Set.of("Рисование", "Йога"));
-        User user3 = new User(3, "Алексей", 28, Set.of( "Охота", "Рыбалка"));
-        User user4 = new User(4, "Ольга", 22, Set.of("Кулинария", "Аэробика", "Плавание"));
+        User user1 = new User(1, "John", 25, Set.of("Reading", "Chess"));
+        User user2 = new User(2, "Mary", 30, Set.of("Drawing", "Yoga"));
+        User user3 = new User(3, "Alex", 28, Set.of("Hunting", "Fishing"));
+        User user4 = new User(4, "Olivia", 22, Set.of("Cooking", "Aerobics", "Swimming"));
 
         List<User> users = List.of(user1, user2, user3, user4);
         System.out.println(new User().findHobbyLovers(users, hobbies));
-
-
     }
 }
