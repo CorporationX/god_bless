42372a4b6e49779e11a71587af8e4df37195cdf8
amanzholtanat <amanzholtanat@gmail.com>
2025-02-07 14:46:34 +0500

    Абстракция, абстракция -BJS2-56440

---
+++ src/main/java/school/faang/abstraction/Warrior.java
@@ -0,0 +1,16 @@
+package school.faang.abstraction;
+
+public class Warrior extends Character {
+
+    public Warrior(String name) {
+        super(name);
+        super.setStrength(10);
+        super.setAgility(5);
+        super.setIntelligence(3);
+    }
+
+    @Override
+    void attack(Character opponent) {
+        opponent.HP -= getStrength();
+    }
+}
