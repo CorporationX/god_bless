77ff328262138e976842cf6906130aebdf105b5c
Hayotbek Farhodov <hayotbekfarhodov020@gmail.com>
2025-02-07 21:47:33 +0500

    Ловим события

---
+++ src/main/java/school/faang/HogwartsSpells.java
@@ -0,0 +1,73 @@
+package school.faang;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.ArrayList;
+
+public class HogwartsSpells {
+    private static final Map<Integer, SpellEvent> SPELL_BY_ID = new HashMap<>(); //я сделал ее static final
+    // чтобы метод print работал!
+    private final Map<String, List<SpellEvent>> spellByType = new HashMap<>();
+
+    public void addSpellEvent(String eventType, String eventDescription) {
+        if (eventType.isBlank()) {
+            throw new IllegalArgumentException("Ошибка! Тип события не может быть пустым!");
+        }
+        if (eventDescription.isBlank()) {
+            throw new IllegalArgumentException("Ошибка! Описание события не может быть пустым!");
+        }
+
+        SpellEvent spellEvent = new SpellEvent(eventType, eventDescription);
+        SPELL_BY_ID.put(spellEvent.getId(), spellEvent);
+
+        // я знаю про метод computeIfAbsent, но не смог его понять. Надеюсь когда начнем лямбды пойму!
+        // (так что пока не обращяйте внимание на это)
+
+        spellByType.putIfAbsent(spellEvent.getEventType(), new ArrayList<>());
+        spellByType.get(spellEvent.getEventType()).add(spellEvent);
+    }
+
+    public SpellEvent getSpellEventById(int id) {
+        if (!SPELL_BY_ID.containsKey(id)) {
+            throw new IllegalArgumentException("Ошибка указан не сушествующий Id!!!");
+        }
+        return SPELL_BY_ID.get(id);
+    }
+
+    public List<SpellEvent> getSpellEventsByType(String eventType) {
+        if (!spellByType.containsKey(eventType)) {
+            throw new IllegalArgumentException("Ошибка указан неверный тип события!");
+        }
+        return spellByType.get(eventType);
+    }
+
+    public void deleteSpellEvent(int id) {
+        String key = SPELL_BY_ID.get(id).getEventType();
+
+        if (!SPELL_BY_ID.containsKey(id)) {
+            throw new IllegalArgumentException("Ошибка! Вы вели не существующий ID!!!");
+        } else {
+            SPELL_BY_ID.remove(id);
+        }
+
+        List<SpellEvent> spellEvents = spellByType.get(key);
+        if (spellEvents.isEmpty()) {
+            spellByType.remove(key);
+        } else {
+            for (SpellEvent spellEvent : spellEvents) {
+                if (spellEvent.getId() == id) {
+                    spellEvents.remove(spellEvent);
+                    break;
+                }
+            }
+        }
+    }
+
+    public static void printAllSpellEvents() {
+        for (Map.Entry<Integer, SpellEvent> entry : SPELL_BY_ID.entrySet()) {
+            System.out.println("ID события: " + entry.getKey() + ". Тип закленания: " +
+                    entry.getValue().getEventType() + ". Описание события: " + entry.getValue().getAction());
+        }
+    }
+}
--- src/main/java/school/faang/Main.java
+++ src/main/java/school/faang/Main.java
@@ -1,4 +1,35 @@
 package school.faang;
 
+
+import static school.faang.HogwartsSpells.printAllSpellEvents;
+
 public class Main {
+    public static void main(String[] args) {
+
+        HogwartsSpells hogwartsSpells = new HogwartsSpells();
+        hogwartsSpells.addSpellEvent("магия", "Защищает от атак");
+        printAllSpellEvents();
+
+        System.out.println("---------------------------");
+
+        hogwartsSpells.addSpellEvent("Трансфигурация", "Преобразует предмет");
+        hogwartsSpells.addSpellEvent("Трансфигурация", "Преобразует предмет2");
+        hogwartsSpells.addSpellEvent("Трансфигурация", "Преобразует предмет1");
+        hogwartsSpells.addSpellEvent("магия", "Защищает от атак3");
+        hogwartsSpells.addSpellEvent("магия", "Защищает от атак2");
+        hogwartsSpells.addSpellEvent("магия", "Защищает от атак1");
+
+        printAllSpellEvents();
+
+        System.out.println("---------------------------");
+        hogwartsSpells.deleteSpellEvent(1);
+        printAllSpellEvents();
+
+        System.out.println("---------------------------");
+        hogwartsSpells.getSpellEventById(2);
+        printAllSpellEvents();
+
+        System.out.println("---------------------------");
+        System.out.println(hogwartsSpells.getSpellEventsByType("Трансфигурация"));
+    }
 }
