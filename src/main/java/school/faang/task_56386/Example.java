d114a415cc8516841f1b83c54c6413223c6dc1f3
Artem Doronin <doronintema2018@yandex.ru>
2025-02-07 13:15:22 +0500

    code fix

--- src/main/java/school/faang/task_56386/Example.java
+++ src/main/java/school/faang/task_56386/Example.java
@@ -2,11 +2,17 @@ package school.faang.task_56386;
 
 public class Example {
     public int[] reverse(int[] array) {
-        for (int i = 0; i < array.length / 2; i++) {
-            int temp = array[i];
-            array[i] = array[array.length - i - 1];
-            array[array.length - i - 1] = temp;
+        if (array == null || array.length == 0) {
+            System.out.println("array is null or empty");
+        } else if (array.length <= 1) {
+            return array;
+        } else {
+            for (int i = 0; i < array.length / 2; i++) {
+                int temp = array[i];
+                array[i] = array[array.length - i - 1];
+                array[array.length - i - 1] = temp;
+            }
         }
-        return array;
+        return  array;
     }
 }
