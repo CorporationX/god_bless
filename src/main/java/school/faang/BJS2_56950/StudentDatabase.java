f1e8888545f4f111e409502227324f2232d49ad2
RamilNazmiev192 <nazmiev633@gmail.com>
2025-02-07 17:02:47 +0300

    fix: change structure

--- src/main/java/school/faang/BJS2_56950/StudentDatabase.java
+++ src/main/java/school/faang/BJS2_56950/StudentDatabase.java
@@ -3,54 +3,29 @@ package school.faang.BJS2_56950;
 import java.util.*;
 
 public class StudentDatabase {
-    private static final String RAMIL = "Ramil";
-    private static final String ALBERT = "Albert";
-    private static final String KAMIL = "Kamil";
-    private static final String MATH = "Math";
-    private static final String ART = "Art";
 
-    public static void main(String[] args) {
-        System.out.printf("Работа с переменной studentSubjects ");
-        addStudentAndSubjectWithInteger(RAMIL, MATH, 3);
-        addStudentAndSubjectWithInteger(ALBERT, MATH, 5);
-        showStudentWithSubject();
-        addNewSubjectWithInteger(RAMIL, ART, 2);
-        addNewSubjectWithInteger(ALBERT, ART, 5);
-        showStudentWithSubject();
-        deleteStudent(RAMIL);
-        showStudentWithSubject();
+    private final Map<Student, Map<Subject, Integer>> studentSubjects = new HashMap<>();
+    private final Map<Subject, List<Student>> subjectStudents = new HashMap<>();
 
-        System.out.printf("%nРабота с переменной subjectStudents");
-        addNewSubjectWithStudents(new HashSet<>(Set.of(RAMIL, ALBERT)), MATH);
-        showSubjectWithStudent();
-        addNewStudentForSubject(KAMIL, MATH);
-        showSubjectWithStudent();
-        deleteStudentFromSubject(ALBERT, MATH);
-        showSubjectWithStudent();
-    }
-
-    private static final Map<Student, Map<Subject, Integer>> studentSubjects = new HashMap<>();
-    private static final Map<Subject, List<Student>> subjectStudents = new HashMap<>();
-
-    private static void addStudentAndSubjectWithInteger(String studentName, String subjectName, int integer) {
+    public  void addStudentAndSubjectWithInteger(String studentName, String subjectName, int integer) {
         Student key = new Student(studentName);
         Map<Subject, Integer> value = new HashMap<>(Map.of(new Subject(subjectName), integer));
 
-        studentSubjects.put(key, value);
+        this.studentSubjects.put(key, value);
     }
 
-    private static Student findStudent(String studentName) {
+    public  Student findStudent(String studentName) {
         Student student = new Student(studentName);
 
-        if (!studentSubjects.containsKey(student)) {
-            System.out.printf("Студента с  именем %s нету в списке студентов", studentName);
+        if (!getStudentSubjects().containsKey(student)) {
+            System.out.printf("Студента с именем %s нету в списке студентов", studentName);
             return null;
         }
 
         return student;
     }
 
-    private static void addNewSubjectWithInteger(String studentName, String subjectName, int integer) {
+    public  void addNewSubjectWithInteger(String studentName, String subjectName, int integer) {
         Student student = findStudent(studentName);
 
         if (student == null) {
@@ -61,8 +36,7 @@ public class StudentDatabase {
         mapSubject.put(new Subject(subjectName), integer);
     }
 
-
-    private static void deleteStudent(String studentName) {
+    public  void deleteStudent(String studentName) {
         Student student = findStudent(studentName);
 
         if (student == null) {
@@ -72,8 +46,8 @@ public class StudentDatabase {
         studentSubjects.remove(student);
     }
 
-    private static void showStudentWithSubject() {
-        studentSubjects.forEach((student, subjectMap) -> {
+    public  void showStudentWithSubject() {
+        getStudentSubjects().forEach((student, subjectMap) -> {
             System.out.printf("%nИмя студенда %s%n", student.getName());
             System.out.printf("Предметы:%n");
             subjectMap.forEach((subject, integer) -> {
@@ -83,7 +57,7 @@ public class StudentDatabase {
         });
     }
 
-    private static void addNewSubjectWithStudents(Set<String> students, String subjectName) {
+    public  void addNewSubjectWithStudents(Set<String> students, String subjectName) {
         Subject subject = new Subject(subjectName);
         List<Student> value = new ArrayList<>();
 
@@ -91,28 +65,30 @@ public class StudentDatabase {
         subjectStudents.put(subject, value);
     }
 
-    private static Subject findSubject(String subjectName) {
+    public  Subject findSubject(String subjectName) {
         Subject subject = new Subject(subjectName);
 
         if (!subjectStudents.containsKey(subject)) {
-            System.out.printf("Предмета с названием %s нету в subjectStudents ", subjectName);
+            System.out.printf("Предмета с названием %s нету в subjectStudents", subjectName);
+            return null;
         }
 
         return subject;
     }
 
-    private static void addNewStudentForSubject(String studentName, String subjectName) {
+    public  void addNewStudentForSubject(String studentName, String subjectName) {
         Subject subject = findSubject(subjectName);
 
         if (subject == null) {
             return;
         }
+
         subjectStudents.get(subject).add(new Student(studentName));
     }
 
-    private static void deleteStudentFromSubject(String studentName, String subjectName) {
+    public  void deleteStudentFromSubject(String studentName, String subjectName) {
         Subject subject = findSubject(subjectName);
-        
+
         if (subject == null) {
             return;
         }
@@ -120,7 +96,7 @@ public class StudentDatabase {
         subjectStudents.get(subject).remove(new Student(studentName));
     }
 
-    private static void showSubjectWithStudent() {
+    public  void showSubjectWithStudent() {
         subjectStudents.forEach((subject, studentList) -> {
             System.out.printf("%nПредмет: %s%n", subject.getName());
             System.out.printf("Студенты изучающий предмет:%n");
@@ -131,4 +107,11 @@ public class StudentDatabase {
         });
     }
 
+    public  Map<Student, Map<Subject, Integer>> getStudentSubjects() {
+        return Collections.unmodifiableMap(studentSubjects);
+    }
+
+    public  Map<Subject, List<Student>> getSubjectStudents() {
+        return Collections.unmodifiableMap(subjectStudents);
+    }
 }
