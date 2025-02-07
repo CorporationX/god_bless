b65783fd5e2dc35c697585c62efec37d69cfbb3d
Sergey <bigwave2915@gmail.com>
2025-02-07 23:57:30 +0500

    начало

--- src/main/java/school/faang/amazon/ProductManager.java
+++ src/main/java/school/faang/amazon/ProductManager.java
@@ -1,5 +1,6 @@
 package school.faang.amazon;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.List;
@@ -8,20 +9,21 @@ import java.util.Set;
 
 public class ProductManager {
 
-    private Set products = new HashSet<>();
-    private Map<Category, List> categoryMap = new HashMap<>();
-
+    private static Set<Product> products = new HashSet<>();
+    private static Map<Category, List<Product>> categoryMap = new HashMap<>();
 
     public void addProduct(Category category, String name) {
         Product product = new Product(name, category);
         products.add(product);
-        System.out.println("Добавления товаров");
+        categoryMap.putIfAbsent(category, new ArrayList<>());
     }
 
     public void removeProduct(Category category, String name) {
-        for (Category : products) {
+        for (Product product : products) {
 
         }
-    }
 
+
+    }
 }
+
