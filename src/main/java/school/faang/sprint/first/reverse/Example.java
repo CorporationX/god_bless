62c85461627e09da4e669c0bc224684e2d171b4d
vtroshin <vtroshin@rvision.ru>
2025-02-07 10:15:19 +0300

    BJS2-56391 Переработал логику реверса, добавил проверки

--- src/main/java/school/faang/sprint/first/reverse/Example.java
+++ src/main/java/school/faang/sprint/first/reverse/Example.java
@@ -1,11 +1,21 @@
 package school.faang.sprint.first.reverse;
 
-import java.util.stream.IntStream;
+import lombok.NonNull;
 
 public class Example {
-    public static int[] reverse(int[] array) {
-        return IntStream.rangeClosed(1, array.length)
-                .map(i -> array[array.length - i])
-                .toArray();
+    public static int[] reverse(@NonNull int[] array) {
+        int arrayLength = array.length;
+
+        if (arrayLength == 0 || arrayLength == 1) {
+            return array;
+        }
+
+        int[] tempArray = new int[arrayLength];
+
+        for (int i = 0; i < arrayLength; i++) {
+            tempArray[i] = array[arrayLength - i - 1];
+        }
+
+        return tempArray;
     }
 }
