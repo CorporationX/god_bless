c76862718082717a12f836788a79511370701201
saroyangor <dr.saroyan98@gmail.com>
2025-02-08 02:43:31 +0400

    Группировка пользователей по возрасту

--- src/main/java/BJS2_56398/User.java
+++ src/main/java/BJS2_56398/User.java
@@ -19,8 +19,16 @@ public class User {
     private String workplace;
     private String address;
 
+    /**
+     * @param users - list of users
+     * @return - map where key is age and value is list of users with that age
+     */
     public static Map<Integer, List<User>> groupUsers(List<User> users) {
         Map<Integer, List<User>> groupedUsers = new HashMap<>();
+
+        if (users == null) {
+            return groupedUsers;
+        }
 
         for (User user : users) {
             groupedUsers.putIfAbsent(user.age, new ArrayList<>());
