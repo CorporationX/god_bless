7a1d63e38d4906f116542dff48dedfea74e69f5d
Ilyas Azhdar <azhdar902@gmail.com>
2025-02-07 10:03:57 +0500

    added javadoc

--- src/main/java/school/faang/turnaround/Example.java
+++ src/main/java/school/faang/turnaround/Example.java
@@ -1,17 +1,31 @@
 package school.faang.turnaround;
 
+import java.util.Arrays;
+
 public class Example {
 
     public static void main(String[] args) {
         int[] nums = {1, 2, 3, 4, 5};
 
         reverse(nums);
-        for (int num : nums) {
-            System.out.print(num + " ");
-        }
     }
 
+    /**
+     * Переворачивает массив целых чисел в обратном порядке.
+     * <p>
+     * Если переданный массив равен {@code null}, метод просто завершает выполнение.
+     * </p>
+     *
+     * @param nums массив целых чисел, который нужно перевернуть
+     */
     private static void reverse(int[] nums) {
+        if (nums == null) {
+            System.out.println("Пустой массив, разворот невозможен");
+            return;
+        }
+
+        System.out.println("Исходный массив: " + Arrays.toString(nums));
+
         int left = 0;
         int right = nums.length - 1;
         while (left < right) {
@@ -22,5 +36,7 @@ public class Example {
             left++;
             right--;
         }
+
+        System.out.println("Перевернутый массив: " + Arrays.toString(nums));
     }
 }
