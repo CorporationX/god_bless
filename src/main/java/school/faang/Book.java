717c7f44631fcb3a2aaaa646ba8a2351acc664b4
WhatINeedToDo <zsdfghjkl111@gmail.com>
2025-02-07 15:00:45 +0700

    * replace sout on String Builder * Change method addBook logic

--- src/main/java/school/faang/Book.java
+++ src/main/java/school/faang/Book.java
@@ -1,33 +1,17 @@
 package school.faang;
 
-import lombok.AllArgsConstructor;
-import lombok.Data;
+import lombok.*;
 
 import java.util.Objects;
 
-@Data
+@Getter
+@Setter
+@EqualsAndHashCode
 @AllArgsConstructor
 public class Book {
     private String title;
     private String author;
     private int year;
 
-    /**
-     * переопределение equlas и hascode
-     */
-
-    @Override
-    public boolean equals(Object o) {
-        if (o == null || getClass() != o.getClass()) {
-            return false;
-        }
-        Book book = (Book) o;
-        return year == book.year && Objects.equals(title, book.title) && Objects.equals(author, book.author);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(title, author, year);
-    }
 
 }
--- src/main/java/school/faang/LibrarySystem.java
+++ src/main/java/school/faang/LibrarySystem.java
@@ -1,26 +1,64 @@
 package school.faang;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
 
 public class LibrarySystem {
 
     private final HashMap<Book, String> bookLocation = new HashMap<>();
-    Book book;
 
     /**
      *
-     * метод addBook пытается добавить книгу , если не заполнены поля
-     * , то выкидывет исключчение NullPointerException
+     * метод addBook пытается добавить книгу, проверяет на null,а также проверяет на пустоту отдельные поля
+     * после чего заполняется массив field значениями полей из книги, местоположение полей в массиве(индекс поля)
+     * соответствует местоположению параметрам в методе
+     * если одно из полей пустое, то перебираем поля в массиве и получаем имя поля с помощью switch\case
      */
 
     public void addBook(String title, String author, int year, String location) {
-        book = new Book(title, author, year);
-        try {
-            bookLocation.put(book, location);
-            System.out.println("Book added: " + book);
-        } catch (NullPointerException e) {
-            System.out.println("Book is empty");
+        Book book = new Book(title, author, year);
+
+        ArrayList<String> fields = new ArrayList<>();
+        fields.add(book.getTitle());
+        fields.add(book.getAuthor());
+        fields.add(String.valueOf(book.getYear()));
+        fields.add(location);
+
+        if (book != null) {
+            if (!book.getAuthor().isEmpty() && !book.getTitle().isEmpty()
+                    && !String.valueOf(book.getYear()).isEmpty() && !location.isEmpty()) {
+                bookLocation.put(book, location);
+                StringBuilder addedBook = new StringBuilder();
+                addedBook.append("Added Book: ")
+                        .append(book.getTitle()).append(", ")
+                        .append(book.getAuthor()).append(", ")
+                        .append(book.getYear()).append(", ")
+                        .append(location).append("\n");
+                System.out.println(addedBook);
+            } else {
+                for (String field : fields) {
+                    if (field.isEmpty()) {
+                        switch (fields.indexOf(field)) {
+                            case 0:
+                                System.out.println("Book Title cannot be empty ");
+                                break;
+                            case 1:
+                                System.out.println("Book Author cannot be empty ");
+                                break;
+                            case 2:
+                                System.out.println("Book Year cannot be empty ");
+                                break;
+                            case 3:
+                                System.out.println("Book Location cannot be empty ");
+                                break;
+
+                            default:
+                                throw new IllegalArgumentException();
+                        }
+                    }
+                }
+            }
         }
     }
 
@@ -30,12 +68,17 @@ public class LibrarySystem {
      * если содержится то удаляет
      */
     public void removeBook(String title, String author, int year) {
-        book.setTitle(title);
-        book.setAuthor(author);
-        book.setYear(year);
+        Book book = new Book(title, author, year);
+        StringBuilder removedBook = new StringBuilder();
         try {
             if (bookLocation.containsKey(book)) {
-                System.out.println("Book deleted: " + book + bookLocation.get(book) + "\n");
+                removedBook.append("Removed Book: ")
+                        .append(book.getTitle() + ", ")
+                        .append(book.getAuthor() + ", ")
+                        .append(book.getYear() + ", ")
+                        .append(bookLocation.get(book) + "\n");
+                System.out.println(removedBook);
+
                 bookLocation.remove(book);
             }
         } catch (NullPointerException e) {
@@ -51,10 +94,18 @@ public class LibrarySystem {
      *
      */
     public void findBook(String title, String author, int year) {
-        book.setTitle(title);
-        book.setAuthor(author);
-        book.setYear(year);
-        System.out.println(bookLocation.getOrDefault(book, "This book does not exist"));
+        Book book = new Book(title, author, year);
+        StringBuilder foundBook = new StringBuilder();
+        foundBook.append("Found Book: ").append("\n")
+                .append(book.getTitle()).append(", ")
+                .append(book.getAuthor()).append(", ")
+                .append(book.getYear()).append(", ").append("\n");
+        if (bookLocation.containsKey(book)) {
+            foundBook.append("Location: ").append(bookLocation.get(book)).append("\n");
+        } else {
+            foundBook.append("Book not found");
+        }
+        System.out.println(foundBook);
 
     }
 
@@ -62,12 +113,20 @@ public class LibrarySystem {
      * printAllBooks - выводит список всех книг с помощью цикла
      */
     public void printAllBooks() {
+        StringBuilder allBooks = new StringBuilder();
         if (bookLocation.isEmpty()) {
-            System.out.println("No books found");
+            allBooks.append("No books found");
         } else {
             for (Map.Entry<Book, String> entry : bookLocation.entrySet()) {
-                System.out.println("| Book | : " + entry.getKey() + " | Location |: " + entry.getValue() + "\n");
+                allBooks.append("| Book | : ")
+                        .append(entry.getKey().getAuthor()).append(", ")
+                        .append(entry.getKey().getTitle()).append(", ")
+                        .append(entry.getKey().getYear()).append(", ")
+                        .append(" | Location | : ")
+                        .append(entry.getValue())
+                        .append("\n");
             }
         }
+        System.out.println(allBooks);
     }
 }
