2b40ba7e3a3c3a0e2d320b22c126f3cb56a9ea95
Laura Lomberg <lomberglaura@gmail.com>
2025-02-07 20:10:50 +0300

    Add classes SpellEvent, HogwartsSpells and Main.

---
+++ src/main/java/hogwarts/SpellEvent.java
@@ -0,0 +1,13 @@
+package hogwarts;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+
+@Data
+@AllArgsConstructor
+public class SpellEvent {
+    private final int id;
+    private final String eventType;
+    private final String action;
+}
