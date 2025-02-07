d4d9bcd7a4838da14cc20c107e7ecbe4c15fb7c6
RamilNazmiev192 <nazmiev633@gmail.com>
2025-02-07 19:40:30 +0300

    fix: correctly load books.json from resources using ClassLoader

--- src/main/java/school/faang/BJS2_56994/Main.java
+++ src/main/java/school/faang/BJS2_56994/Main.java
@@ -1,30 +1,16 @@
 package school.faang.BJS2_56994;
 
-import java.util.Arrays;
+import school.faang.BJS2_56994.data.BookData;
+import school.faang.BJS2_56994.data.JsonLoader;
+
 import java.util.List;
-import java.util.Map;
 
 public class Main {
     public static void main(String[] args) {
-        List<Map.Entry<Book, String>> books = Arrays.asList(
-                Map.entry(new Book("Clean Code", "Robert C. Martin", 2008), "Shelf A1"),
-                Map.entry(new Book("Effective Java", "Joshua Bloch", 2017), "Shelf B2"),
-                Map.entry(new Book("The Pragmatic Programmer", "Andrew Hunt", 1999), "Shelf C3"),
-                Map.entry(new Book("Refactoring", "Martin Fowler", 1999), "Shelf A1"),
-                Map.entry(new Book("Head First Java", "Kathy Sierra", 2005), "Shelf D4"),
-                Map.entry(new Book("Design Patterns", "Erich Gamma", 1994), "Shelf B2"),
-                Map.entry(new Book("You Don't Know JS", "Kyle Simpson", 2014), "Shelf E5"),
-                Map.entry(new Book("Introduction to Algorithms", "Thomas H. Cormen", 2009), "Shelf F6"),
-                Map.entry(new Book("The Art of Computer Programming", "Donald Knuth", 1968), "Shelf G7")
-        );
+        List<BookData> books = JsonLoader.loadBooks("books.json");
 
-        books.forEach(entry ->
-                LibrarySystem.addBook(
-                        entry.getKey().getTitle(),
-                        entry.getKey().getAuthor(),
-                        entry.getKey().getYear(),
-                        entry.getValue()
-                )
+        books.forEach(book ->
+                LibrarySystem.addBook(book.title, book.author, book.year, book.shelf)
         );
 
         LibrarySystem.printAllBooks();
---
+++ src/main/java/school/faang/BJS2_56994/data/BookData.java
@@ -0,0 +1,8 @@
+package school.faang.BJS2_56994.data;
+
+public class BookData {
+    public String title;
+    public String author;
+    public int year;
+    public String shelf;
+}
