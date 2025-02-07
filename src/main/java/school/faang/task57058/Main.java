1e74a1d4ba9d2f970c5abf5d8cae9933eb8323a9
Dmitry Rogulkin <rogulkind@yandex.ru>
2025-02-07 14:46:27 +0500

    https://faang-school.atlassian.net/browse/BJS2-57058

--- src/main/java/school/faang/task57058/Main.java
+++ src/main/java/school/faang/task57058/Main.java
@@ -4,9 +4,6 @@ import school.faang.task57058.service.HogwartsSpells;
 
 public class Main {
     public static void main(String[] args) {
-        //SpellEvent defense = new SpellEvent("Защита", "создает силовое поле");
-        //SpellEvent attack = new SpellEvent("Нападение", "добавляет силу для атаки");
-
         HogwartsSpells hogwartsSpells = new HogwartsSpells();
         hogwartsSpells.addSpellEvent("Защита", "создает силовое поле");
         hogwartsSpells.addSpellEvent("Нападение", "добавляет силу для атаки");
