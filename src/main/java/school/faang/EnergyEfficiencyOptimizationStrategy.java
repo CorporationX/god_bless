1b6d7138916279021ca15760dc3ea24da50eb4e3
Sergey Ermoshenko <ermoshenko.work@gmail.com>
2025-02-07 18:06:32 +0300

    delete canDistributeLoad

--- src/main/java/school/faang/EnergyEfficiencyOptimizationStrategy.java
+++ src/main/java/school/faang/EnergyEfficiencyOptimizationStrategy.java
@@ -8,23 +8,21 @@ public class EnergyEfficiencyOptimizationStrategy implements OptimizationStrateg
     public void optimize(DataCenter dataCenter) {
         List<Server> servers = dataCenter.servers;
         servers.sort(Comparator.comparingDouble(Server::getEnergyConsumption));
-        distributeLoad(dataCenter, servers);
+        distributeLoad(servers);
         servers.sort(Comparator.comparingDouble(Server::getEnergyConsumption).reversed());
-        distributeLoad(dataCenter, servers);
+        distributeLoad(servers);
 
         for (Server server : servers) {
             if (server.getLoad() == 0) {
                 server.setEnergyConsumption(0.0);
             }
         }
+        System.out.println(servers);
     }
 
-    private void distributeLoad(DataCenter dataCenter, List<Server> servers) {
+    private void distributeLoad(List<Server> servers) {
         for (int first = 0, last = servers.size() - 1; first < last; first++) {
             Server firstServer = servers.get(first);
-            if (!canDistributeLoad(dataCenter, first, last)) {
-                break;
-            }
             for (; firstServer.getLoad() > 0 && first < last; last--) {
                 Server lastServer = servers.get(last);
                 double take = Math.min(firstServer.getLoad(), lastServer.getMaxLoad() - lastServer.getLoad());
@@ -32,15 +30,5 @@ public class EnergyEfficiencyOptimizationStrategy implements OptimizationStrateg
                 firstServer.setLoad(firstServer.getLoad() - take);
             }
         }
-    }
-
-    private boolean canDistributeLoad(DataCenter dataCenter, int first, int last) {
-        double distributedLoad = 0;
-        List<Server> servers = dataCenter.servers;
-        for (; first < last; --last) {
-            Server lastServer = servers.get(last);
-            distributedLoad += lastServer.getMaxLoad() - lastServer.getLoad();
-        }
-        return distributedLoad >= servers.get(first).getLoad();
     }
 }
