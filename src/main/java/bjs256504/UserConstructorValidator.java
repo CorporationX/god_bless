6bada1f41825074b6924ce569566ade1e3559b28
Vladimir Mamaev <ipmamaev.v@gmail.com>
2025-02-07 13:44:59 +0300

    Внесены изменения в код по рекомендациям ревью. Все сообщения в исходном коде только на английском языке, поскольку настроить кириллицу в кодировке проблематично

--- src/main/java/bjs256504/UserConstructorValidator.java
+++ src/main/java/bjs256504/UserConstructorValidator.java
@@ -1,20 +1,29 @@
 package bjs256504;
 
+import java.util.Objects;
 import java.util.Set;
 
 public class UserConstructorValidator {
-    static void validate(String name, int age, String job, String address, Set<String> jobs, Set<String> addresses) {
+    static void validate(String name, int age, String job, String address, int validAge, Set<String> validJobs,
+                         Set<String> validAddresses) {
+
+        if (Objects.isNull(name) || Objects.isNull(job) || Objects.isNull(address)) {
+            throw new IllegalArgumentException("All fields must be filled in");
+        }
+
         if (name.isBlank()) {
             throw new IllegalArgumentException("The name cannot be empty");
         }
-        if (age < 18) {
-            throw new IllegalArgumentException("The age must not be less than 18");
+        if (age < validAge) {
+            throw new IllegalArgumentException("The age must not be less than " + validAge);
         }
-        if (!jobs.contains(job)) {
-            throw new IllegalArgumentException("The place of work must be included in the set");
+        if (!validJobs.contains(job)) {
+            throw new IllegalArgumentException("Profession " + job + " must be one of the allowed ones: "
+                    + validJobs);
         }
-        if (!addresses.contains(address)) {
-            throw new IllegalArgumentException("The address must be included in the set");
+        if (!validAddresses.contains(address)) {
+            throw new IllegalArgumentException("Address " + address + " must be one of the allowed ones: "
+                    + validAddresses);
         }
     }
 }
