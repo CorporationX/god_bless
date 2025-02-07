e0357586d8a2f4ee520129fb433a77c3874311a5
pstratonni <79229338+pstratonni@users.noreply.github.com>
2025-02-07 23:23:04 +0100

    Corrected

--- src/main/java/school/faang/library/LibrarySystem.java
+++ src/main/java/school/faang/library/LibrarySystem.java
@@ -1,40 +1,54 @@
 package school.faang.library;
 
+import java.time.LocalDate;
 import java.util.HashMap;
 import java.util.Map;
 
 
 public class LibrarySystem {
     private final Map<Book, String> bookLocations = new HashMap<>();
+    private static final String REMOVE_BOOK = "We have removed book %s from the shelf %s%n";
+    private static final String NO_REMOVE_BOOK = """
+                    We can't delete this %s.
+                    It's not in the system.
+                    """;
+    private static final String FIND_BOOK = """
+                    The book has been found.
+                    %s is on the shelf %s
+                    """;
+    private static final String NO_FIND_BOOK = "Unfortunately we don't have %s%n";
 
     public void addBook(String title, String author, int year, String location) {
+        if (checkData(title, author, year) || location == null) {
+            return;
+        }
         bookLocations.put(new Book(title, author, year), location);
     }
 
     public void removeBook(String title, String author, int year) {
+        if (checkData(title, author, year)) {
+            return;
+        }
         Book book = new Book(title, author, year);
         if (bookLocations.containsKey(book)) {
 
-            System.out.printf("We have removed book %s from the shelf %s%n", book,
-                    bookLocations.remove(new Book(title, author, year)));
+            System.out.printf(REMOVE_BOOK, book,
+                    bookLocations.remove(book));
         } else {
-            System.out.printf("""
-                    We can't delete this %s.
-                    It's not in the system.
-                    """, book);
+            System.out.printf(NO_REMOVE_BOOK, book);
         }
 
     }
 
     public void findBook(String title, String author, int year) {
+        if (checkData(title, author, year)) {
+            return;
+        }
         Book book = new Book(title, author, year);
         if (bookLocations.containsKey(book)) {
-            System.out.printf("""
-                    The book has been found.
-                    %s is on the shelf %s
-                    """, book, bookLocations.get(book));
+            System.out.printf(FIND_BOOK, book, bookLocations.get(book));
         } else {
-            System.out.printf("Unfortunately we don't have %s%n", book);
+            System.out.printf(NO_FIND_BOOK, book);
         }
 
     }
@@ -44,4 +58,17 @@ public class LibrarySystem {
             System.out.printf("%s is on the shelf %s%n", bookLocation.getKey(), bookLocation.getValue());
         }
     }
+
+    private boolean checkData(String title, String author, int year) {
+        if (title == null || author == null) {
+            System.out.println("Invalid data");
+            return true;
+        }
+        if (year < 0 || year > LocalDate.now().getYear()) {
+            System.out.println("Invalid data");
+            return true;
+        }
+        return false;
+    }
+
 }
