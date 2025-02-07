3726c2cc660d19fc413914e4bc662ec42e7ab067
Anton Kamyshin <anton.kamyshin@gmail.com>
2025-02-07 07:20:40 +0300

    в соответствии с выданными рекмендациями в код внесены исправления: 1. Исправлен new HashMap<>(List.of..) на Set.of(...) 2. Удалены комментарии (хороший код говорит сам за себя) 3. Вместо явного прописывания конструкторов и гетторов использовал методы lombok для генерации конструктора и геттеров 4. Переменная hobbies вынесена в константу.

--- src/main/java/school/faang/task_BJS2_56323/Main.java
+++ src/main/java/school/faang/task_BJS2_56323/Main.java
@@ -1,25 +1,20 @@
 package school.faang.task_BJS2_56323;
 
-import java.util.HashMap;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
 
-
 public class Main {
+    public static final Set<String> HOBBIES = new HashSet<>(List.of("Swimming", "Running"));
+
     public static void main(String[] args) {
-        //создать список пользователей с их хобби
         List<User> users = List.of(
-                new User(1, "Anton", 10, new HashSet<>(List.of("Swimming", "Book reading"))),
-                new User(2, "Anna", 20, new HashSet<>(List.of("Running", "Book listening"))),
-                new User(2, "Oleg", 30, new HashSet<>(List.of("Fitness", "TV watching")))
+                new User(1, "Anton", 10, java.util.Set.of("Swimming", "Book reading")),
+                new User(2, "Anna", 20, java.util.Set.of("Running", "Book listening")),
+                new User(2, "Oleg", 30, java.util.Set.of("Fitness", "TV watching"))
         );
-        //создать список хобби
-        Set<String> hobbies = new HashSet<>(List.of("Swimming", "Running"));
-        //создать  hashMap и вызвать его для получения результатов соответствия
-        Map<String, String> hobbyLovers = User.findHobbyLovers(users, hobbies);
-        //вывести полученную hashMap на экран
+        Map<String, String> hobbyLovers = User.findHobbyLovers(users, HOBBIES);
         for (Map.Entry<String, String> entry : hobbyLovers.entrySet()) {
             System.out.println(entry.getKey() + ": " + entry.getValue());
         }
--- src/main/java/school/faang/task_BJS2_56323/User.java
+++ src/main/java/school/faang/task_BJS2_56323/User.java
@@ -1,33 +1,22 @@
 package school.faang.task_BJS2_56323;
 
-import java.util.ArrayList;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
 
 import java.util.HashMap;
 import java.util.List;
-import java.util.Set;
 import java.util.Map;
+import java.util.Set;
 
+
+@AllArgsConstructor
+@Getter
 
 public class User {
     private int id;
     private String name;
     private int age;
     private Set<String> hobbies;
-
-    public User(int id, String name, int age, Set<String> hobbies) {
-        this.id = id;
-        this.name = name;
-        this.age = age;
-        this.hobbies = hobbies;
-    }
-
-    public Set<String> getHobbies() {
-        return hobbies;
-    }
-
-    public String getName() {
-        return name;
-    }
 
     public static Map<String, String> findHobbyLovers(List<User> users, Set<String> hobbies) {
         Map<String, String> hobbyLovers = new HashMap<>();
