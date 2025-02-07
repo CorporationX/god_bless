aeb225d269abbe63874fb22ed62678b4d99bd6fe
Artpyak <art.pyak@yandex.ru>
2025-02-07 21:38:09 +0300

    BJS2-57001 1. Класс Book теперь record. 2. Добавлена валидация методам. 3. Добавленно логирование.

--- src/main/java/school/faang/library_of_westeros/BJS2_57001/Book.java
+++ src/main/java/school/faang/library_of_westeros/BJS2_57001/Book.java
@@ -1,14 +1,4 @@
 package school.faang.library_of_westeros.BJS2_57001;
 
-import lombok.*;
-
-@Getter
-@AllArgsConstructor
-@EqualsAndHashCode
-@ToString
-public class Book {
-
-    private final String title;
-    private final String author;
-    private final int year;
+public record Book(String title, String author, int year) {
 }
--- src/main/java/school/faang/library_of_westeros/BJS2_57001/LibrarySystem.java
+++ src/main/java/school/faang/library_of_westeros/BJS2_57001/LibrarySystem.java
@@ -3,30 +3,88 @@ package school.faang.library_of_westeros.BJS2_57001;
 import lombok.Getter;
 import lombok.NonNull;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.time.Year;
 import java.util.HashMap;
 import java.util.Map;
 
 @Getter
 public class LibrarySystem {
+    private static final Logger LOGGER = LoggerFactory.getLogger(LibrarySystem.class);
 
     private final Map<Book, String> books = new HashMap<>();
 
     public void addBook(@NonNull String title, @NonNull String author, int year, @NonNull String location) {
+        validateTitle(title);
+        validateAuthor(author);
+        validateYear(year);
+        validateLocation(location);
+
         books.put(new Book(title, author, year), location);
+
+        LOGGER.info("Added book {} to library system", title);
     }
 
     public void removeBook(@NonNull String title, @NonNull String author, int year) {
+        validateTitle(title);
+        validateAuthor(author);
+        validateYear(year);
+
         books.remove(new Book(title, author, year));
+
+        LOGGER.info("Removed book {} from library system", title);
     }
 
     public void findBook(@NonNull String title, @NonNull String author, int year) {
-        System.out.println(books.getOrDefault(new Book(title, author, year), "Book not found"));
+        validateTitle(title);
+        validateAuthor(author);
+        validateYear(year);
+
+        String result = books.getOrDefault(new Book(title, author, year), "Book not found");
+        System.out.println(result);
+
+        if (result.equals("Book not found")) {
+            LOGGER.info("Book not found");
+        } else {
+            LOGGER.info("Found book {} in library system", result);
+        }
     }
 
     public void printAllBooks() {
         for (var entry : books.entrySet()) {
             System.out.println(entry.getKey() + " " + entry.getValue());
         }
+
+        LOGGER.info("Printing all books in library system");
     }
 
+    private static void validateYear(int year) {
+        if (year < 0 || year > Year.now().getValue()) {
+            LOGGER.error("Year must be between 0 and {}", Year.now().getValue());
+            throw new IllegalArgumentException(year + " must be between 0 and " + Year.now().getValue());
+        }
+    }
+
+    private static void validateLocation(String location) {
+        if (location.isBlank()) {
+            LOGGER.error("Location is blank");
+            throw new IllegalArgumentException("Location cannot be blank");
+        }
+    }
+
+    private static void validateAuthor(String author) {
+        if (author.isBlank()) {
+            LOGGER.error("Author is blank");
+            throw new IllegalArgumentException("Author cannot be blank");
+        }
+    }
+
+    private static void validateTitle(String title) {
+        if (title.isBlank()) {
+            LOGGER.error("Title is blank");
+            throw new IllegalArgumentException("Title cannot be blank");
+        }
+    }
 }
