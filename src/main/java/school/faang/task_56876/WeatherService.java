5e0777c1611db5b107033c1bbd363796c95f7b42
Yaroslav Olkin <shrelok.pupkin@yandex.ru>
2025-02-08 02:12:18 +0300

    .

---
+++ src/main/java/school/faang/task_56876/WeatherService.java
@@ -0,0 +1,15 @@
+package school.faang.task_56876;
+
+import java.util.Random;
+
+public class WeatherService implements WeatherProvider{
+    private Random random = new Random();
+
+    @Override
+    public WeatherData fetchWeatherData(String city) {
+        double temperature = random.nextDouble(-20, 20);
+        double humidity = random.nextDouble(45, 100);
+        long timestamp = System.currentTimeMillis();
+        return new WeatherData(city, temperature, humidity, timestamp);
+    }
+}
