3110e589be84a62236dccd212f59a2cdbd895c9a
Ilhan Mustafa <ilhnmst@gmail.com>
2025-02-07 12:58:00 +0300

    add JavaDoc

--- src/main/java/school/faang/sprint1/task_BJS2_57004/Student.java
+++ src/main/java/school/faang/sprint1/task_BJS2_57004/Student.java
@@ -1,5 +1,10 @@
 package school.faang.sprint1.task_BJS2_57004;
 
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+
+@AllArgsConstructor
+@EqualsAndHashCode
 public class Student {
     private String name;
 }
--- src/main/java/school/faang/sprint1/task_BJS2_57004/StudentDatabase.java
+++ src/main/java/school/faang/sprint1/task_BJS2_57004/StudentDatabase.java
@@ -1,5 +1,7 @@
 package school.faang.sprint1.task_BJS2_57004;
 
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
@@ -7,7 +9,29 @@ public class StudentDatabase {
     private Map<Student, Map<Subject, Integer>> studentSubjects;
     private Map<Subject, List<Student>> subjectStudents;
 
+    /**
+     * Добавляет в базу студента с его предметами и их оценками
+     * Если студент уже существует, то добавляет ему предметы и их оценки
+     * Если у студента уже есть такие предметы то оценки обновятся на новые
+     *
+     * @param name не может быть null или пустым
+     *             если {@param name} null или пустым будет выброшено исключение IllegalArgumentException
+     * @param subjectsWithRatings не может быть null или пустым
+     *             если {@param subjectsWithRatings} null или пустым будет выброшено исключение IllegalArgumentException
+     */
+
     public void addStudent(String name, Map<Subject, Integer> subjectsWithRatings) {
+        Student student = new Student(name);
+        if (studentSubjects.containsKey(student)) {
+            studentSubjects.get(student).putAll(subjectsWithRatings);
+        } else {
+            studentSubjects.put(student, subjectsWithRatings);
+        }
+
+        subjectsWithRatings.forEach((subject, ratings) -> {
+            subjectStudents.computeIfAbsent(subject, s -> new ArrayList<>());
+
+        });
 
     }
 
