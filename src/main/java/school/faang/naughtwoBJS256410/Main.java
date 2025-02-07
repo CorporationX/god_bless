7458a51c0aa5e61d108e9ea7edc86d1232d5c017
naught_wo <catrinesaxon@yandex.ru>
2025-02-07 14:27:32 +0300

    Корректировка. Под задачу создала отдельный пакет.

--- src/main/java/school/faang/naughtwoBJS256410/Main.java
+++ src/main/java/school/faang/naughtwoBJS256410/Main.java
@@ -1,10 +1,10 @@
-package school.faang;
+package school.faang.naughtwoBJS256410;
 
 import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) {
         int[] nums = {10, 8, 9, 7, 6, 4, 5, 3, 2, 0, 1};
-        System.out.println(Arrays.toString(nums) + " rewerse: " + Arrays.toString(Example.reverse(nums)));
+        System.out.println(Arrays.toString(nums) + " reverse: " + Arrays.toString(Example.reverse(nums)));
     }
 }
