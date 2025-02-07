e4ca9e9e51dae474aea312c7d2a321ed64f96ac2
Andrey Ievlev <avievlev@gmail.com>
2025-02-07 22:54:06 +0300

    BJS2-57039 Библиотека Вестеросса! Устранение замечаний #2

--- src/main/java/school/faang/westeros/library/Book.java
+++ src/main/java/school/faang/westeros/library/Book.java
@@ -3,11 +3,28 @@ package school.faang.westeros.library;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.RequiredArgsConstructor;
+
+import java.util.Objects;
 
 @Data
-@AllArgsConstructor
+@RequiredArgsConstructor
 public class Book {
-    private String title;
-    private String author;
-    private int year;
+    private final String title;
+    private final String author;
+    private final int year;
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        Book book = (Book) o;
+        return year == book.year && Objects.equals(title, book.title) && Objects.equals(author, book.author);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(title, author, year);
+    }
 }
