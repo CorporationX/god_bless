4cf0186e6b8db320dcd09439301fb93d4426f9f2
Marsel <mars1212@inbox.ru>
2025-02-08 00:12:01 +0300

    reverse Array fix name value and any little things

--- src/main/java/school/faang/arrayreverse/Example.java
+++ src/main/java/school/faang/arrayreverse/Example.java
@@ -1,34 +1,34 @@
-package school.faang.arrayturn;
+package school.faang.arrayreverse;
 
 public class Example {
     public static void main(String[] args) {
         int[] array = {1, 2, 3, 4, 5};
         System.out.println("Array before");
         Example.printArray(array);
-        Example.turnArray(array);
+        Example.reverse(array);
         System.out.println("\nArray after");
         Example.printArray(array);
     }
 
-    private static void turnArray(int[] array) {
-        if (array.length == 0) {
+    private static void reverse(int[] array) {
+        if (array == null || array.length == 0) {
             return;
         }
-        int var;
+        int tempValue;
         int left = 0;
         int right = array.length - 1;
         while (left < right) {
-            var = array[left];
+            tempValue = array[left];
             array[left] = array[right];
-            array[right] = var;
+            array[right] = tempValue;
             left++;
             right--;
         }
     }
 
     private static void printArray(int[] array) {
-        for (int i = 0; i < array.length; i++) {
-            System.out.print(array[i] + " ");
+        for (int element : array) {
+            System.out.print(element + " ");
         }
     }
 }
