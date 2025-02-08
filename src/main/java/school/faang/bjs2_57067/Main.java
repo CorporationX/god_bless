ff99ffe242da30ae55f7a8680ac81e16faf64f74
Pavel Chernobay <mr.chernobay1990@gmail.com>
2025-02-08 20:11:45 +0300

    Сделал тесты

--- src/main/java/school/faang/bjs2_57067/Main.java
+++ src/main/java/school/faang/bjs2_57067/Main.java
@@ -2,5 +2,17 @@ package school.faang.bjs2_57067;
 
 public class Main {
     public static void main(String[] args) {
+        try {
+            HogwartsSpells hs = new HogwartsSpells();
+            hs.addSpellEvent("Трансфигурация", "Преобразует предмет");
+            hs.addSpellEvent("Защита", "Защищает от атак");
+            hs.printAllSpellEvents();
+            System.out.println(hs.getSpellEventById(2));
+            System.out.println(hs.getSpellEventsByType("Трансфигурация"));
+            hs.deleteSpellEvent(1);
+            hs.printAllSpellEvents();
+        } catch (IllegalArgumentException e) {
+            System.out.println(e.getMessage());
+        }
     }
 }
