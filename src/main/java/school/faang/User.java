84dff62021f3e00a160c051936bfd7d9815654a3
Filippka <Filipp_Chudaev@mail.ru>
2025-02-08 11:55:27 +0500

    second work

--- src/main/java/school/faang/User.java
+++ src/main/java/school/faang/User.java
@@ -1,4 +1,51 @@
 package school.faang;
 
+import java.util.Set;
+
 public class User {
+
+    private String name;
+    private int age;
+    private String address;
+    private String job;
+
+    private static final Set<String> VALID_JOBS = Set.of("Google", "Uber", "Amazon");
+    private static final Set<String> VALID_ADDRESSES = Set.of("London", "New York", "Amsterdam");
+
+    public User(String name, int age, String address, String job) {
+
+        if (name.isEmpty()) {
+            throw new IllegalStateException("Name mot can empty");
+        }
+        if (age < 18) {
+            throw new IllegalStateException("Age not can less 18");
+        }
+        if (!VALID_JOBS.contains(job)) {
+            throw new IllegalStateException("job only from the list");
+        }
+        if (!VALID_ADDRESSES.contains(address)) {
+            throw new IllegalStateException("city only from the list");
+        }
+        this.name = name;
+        this.age = age;
+        this.address = address;
+        this.job = job;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public String getJob() {
+        return job;
+    }
 }
+
