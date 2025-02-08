1bfcf8039ab7bd12c95d2fd0e1cb458174b0e8f8
Valerii Enin <enin_v_a@mail.ru>
2025-02-08 11:43:10 +0300

    Finished for checking

---
+++ src/main/java/school/faang/catch_events/SpellEvent.java
@@ -0,0 +1,17 @@
+package school.faang.catch_events;
+
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@ToString
+public class SpellEvent {
+    private  int id;
+    private String eventType;
+    private String action;
+}
