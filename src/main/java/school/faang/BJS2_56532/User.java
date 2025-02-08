e67e1d7defd09c124e40638fbd18785ff77b2f39
Gulnaz <gumerova.gulnaz2014@yandex.ru>
2025-02-09 02:55:50 +0300

    Исправлены недочеты в классе User

--- src/main/java/school/faang/BJS2_56532/User.java
+++ src/main/java/school/faang/BJS2_56532/User.java
@@ -1,31 +1,48 @@
 package school.faang.BJS2_56532;
 
 import lombok.Getter;
-import lombok.Setter;
 
-import java.util.List;
+import java.util.Set;
 
 @Getter
 public class User {
-    private static final List<String> VALID_JOBS = List.of("Google", "Uber", "Amazon");
-    private static final List<String> VALID_ADDRESSES = List.of("London", "New York", "Amsterdam");
-    private String name;
-    private int age;
-    private String job;
-    private String address;
+    private static final int MIN_AGE = 18;
+    private static final Set<String> VALID_JOBS = Set.of("Google", "Uber", "Amazon");
+    private static final Set<String> VALID_ADDRESSES = Set.of("London", "New York", "Amsterdam");
+    private final String name;
+    private final int age;
+    private final String job;
+    private final String address;
 
     public User(String name, int age, String job, String address) {
-        if (isValid(name, age, job, address)) {
-            this.name = name;
-            this.age = age;
-            this.job = job;
-            this.address = address;
-        } else {
-            throw new IllegalArgumentException("Данные пользователя некорректные");
+        if (!validateName(name)) {
+            throw new IllegalArgumentException("Invalid user name");
+        } else if (!validateAge(age)) {
+            throw new IllegalArgumentException("Age must not be less than 18");
+        } else if (!validateJob(job)) {
+            throw new IllegalArgumentException("Invalid job");
+        } else if (!validateAddress(address)) {
+            throw new IllegalArgumentException("Invalid address");
         }
+        this.name = name;
+        this.age = age;
+        this.job = job;
+        this.address = address;
     }
 
-    private boolean isValid(String name, int age, String job, String address) {
-        return !name.isEmpty() && age >= 18 && VALID_JOBS.contains(job) && VALID_ADDRESSES.contains(address);
+    private boolean validateName(String name) {
+        return name != null && !name.isBlank();
+    }
+
+    private boolean validateAge(int age) {
+        return age >= MIN_AGE;
+    }
+
+    private boolean validateJob(String job) {
+        return job != null && VALID_JOBS.contains(job);
+    }
+
+    private boolean validateAddress(String address) {
+        return address != null && VALID_ADDRESSES.contains(address);
     }
 }
