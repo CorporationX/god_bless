9a6f669b0a2d0d2b2d093e65c4cea53ce6d7ade5
bambum <gwalka885@gmai.com>
2025-02-09 00:34:19 +0500

    next text

--- src/main/java/school/faang/User.java
+++ src/main/java/school/faang/User.java
@@ -30,8 +30,8 @@ public class User {
     public static Map<Integer, List<User>> groupUsers(List<User> users) {
         Map<Integer, List<User>> map = new HashMap<>();
         for (User user : users) {
-            if (user != null && !user.getName().isBlank() && !user.getAddress().isBlank()
-            && !user.getWork().isBlank()) {
+                if (user != null && !user.getName().isBlank() && !user.getAddress().isBlank()
+                && !user.getWork().isBlank()) {
                 if (!map.containsKey(user.getAge())) {
                     map.put(user.getAge(), new ArrayList<>());
                 }
