354f25e4c8b5e2a46d57e73fb80c73d78f76c714
Evgeny Sledkov <elsledkov@gmail.com>
2025-02-08 12:27:29 +0300

    style: add whitespace in DuplicateBookException

--- src/main/java/school/faang/sprint1/collections/vesteroslibrary/Book.java
+++ src/main/java/school/faang/sprint1/collections/vesteroslibrary/Book.java
@@ -4,15 +4,9 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 
 @Data
-//@AllArgsConstructor
+@AllArgsConstructor
 public class Book {
     private final String title;
     private final String author;
     private final int year;
-
-    public Book(String title, String author, int year) {
-        this.title = title;
-        this.author = author;
-        this.year = year;
-    }
 }
--- src/main/java/school/faang/sprint1/collections/vesteroslibrary/exceptions/DuplicateBookException.java
+++ src/main/java/school/faang/sprint1/collections/vesteroslibrary/exceptions/DuplicateBookException.java
@@ -1,6 +1,6 @@
 package school.faang.sprint1.collections.vesteroslibrary.exceptions;
 
-public class DuplicateBookException extends RuntimeException{
+public class DuplicateBookException extends RuntimeException {
     public DuplicateBookException(String message) {
         super(message);
     }
