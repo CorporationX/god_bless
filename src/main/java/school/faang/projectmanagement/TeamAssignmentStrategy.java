5f0ea6ff28729e70e2a8ac1f42734612e3ab132d
Anton Kapshukov <kapshukov.project@gmail.com>
2025-02-08 20:23:50 +0300

    Двойной кэш-кэш (с учетом комментариев)

--- src/main/java/school/faang/projectmanagement/TeamAssignmentStrategy.java
+++
@@ -1,7 +0,0 @@
-package school.faang.projectmanagement;
-
-import java.util.List;
-
-public interface TeamAssignmentStrategy {
-    List<Employee> assignTeam(Project project, List<Employee> employees);
-}
