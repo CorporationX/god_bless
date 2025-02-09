bbc31caaae9bbdf533dddacfb44a418598486b95
Vladimir Mamaev <ipmamaev.v@gmail.com>
2025-02-09 16:17:43 +0300

    Добавлены тестовые классы, класс Main и файл MANIFEST.MF. Создан модуль для тестирования, добавлена Test Source директория java.  Main нужен для того, чтобы указать его главным классом при добавлении файла jar в артефакты ( File -> Project Structure -> Artifacts -> + -> JAR -> From modules with dependencies). Файл jar нужен, чтобы связать тестовые классы с проектом: при сборке в директории build будет директория build c соответствующим jar файлом. Протестированы специальные методы StudentDatabase, кроме вывода в консоль printAllStudents и printAllSubjects, там вывод из логгера, который протестировать проблемно

--- src/main/java/bjs256991/StudentDatabase.java
+++ src/main/java/bjs256991/StudentDatabase.java
@@ -1,22 +1,27 @@
 package bjs256991;
 
+import lombok.Getter;
+import lombok.ToString;
 import lombok.extern.slf4j.Slf4j;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
 import java.util.Objects;
 
 @Slf4j
+@Getter
+@ToString
 public class StudentDatabase {
     private final Map<Student, Map<Subject, Integer>> studentSubjects = new HashMap<>();
     private final Map<Subject, List<Student>> subjectStudents = new HashMap<>();
     private static final Logger logger = LoggerFactory.getLogger(StudentDatabase.class);
 
-    protected void addStudentSubjectsEstimates(Student student, Subject subject, Integer estimate) {
+    public void addStudentSubjectsEstimates(Student student, Subject subject, Integer estimate) {
         Map<Subject, Integer> subjectsToEstimates = studentSubjects.get(student);
         if (Objects.nonNull(student) && Objects.nonNull(subject) && Objects.nonNull(estimate)) {
             if (subjectsToEstimates == null) {
@@ -33,7 +38,7 @@ public class StudentDatabase {
         subjectStudents.put(subject, students);
     }
 
-    protected void addSubjectForStudentWithEstimate(Subject subject, Student student, Integer estimate) {
+    public void addSubjectForStudentWithEstimate(Subject subject, Student student, Integer estimate) {
         Map<Subject, Integer> subjectsToEstimates = studentSubjects.get(student);
         if (Objects.nonNull(subject) && Objects.nonNull(student) && Objects.nonNull(estimate)) {
             if (subjectsToEstimates == null) {
@@ -51,7 +56,7 @@ public class StudentDatabase {
         }
     }
 
-    protected void removeStudentSubjects(Student student) {
+    public void removeStudentSubjects(Student student) {
         Map<Subject, Integer> subjectsToEstimates = studentSubjects.get(student);
         if (Objects.nonNull(student)) {
             if (subjectsToEstimates != null) {
@@ -60,7 +65,7 @@ public class StudentDatabase {
         }
     }
 
-    protected void printAllStudents() {
+    public void printAllStudents() {
         studentSubjects.forEach((student, subjectsToEstimates) -> {
             logger.info("Student: {}", student);
             subjectsToEstimates.forEach((subject, estimate) -> {
@@ -69,7 +74,7 @@ public class StudentDatabase {
         });
     }
 
-    protected void addSubjectStudents(Subject subject, List<Student> students) {
+    public void addSubjectStudents(Subject subject, List<Student> students) {
         if (Objects.nonNull(subject) && Objects.nonNull(students)) {
             Map<Subject, Integer> subjectsToEstimates = new HashMap<>();
             students.forEach(student -> {
@@ -80,7 +85,7 @@ public class StudentDatabase {
         }
     }
 
-    protected void addStudentSubject(Student student, Subject subject) {
+    public void addStudentSubject(Student student, Subject subject) {
         Map<Subject, Integer> subjectsToEstimates = studentSubjects.get(student);
         if (Objects.nonNull(student) && Objects.nonNull(subject)) {
             if (subjectsToEstimates == null) {
@@ -97,15 +102,17 @@ public class StudentDatabase {
         subjectStudents.put(subject, students);
     }
 
-    protected void removeStudentFromSubject(Student student, Subject subject) {
+    public void removeStudentFromSubject(Student student, Subject subject) {
         if (Objects.nonNull(student) && Objects.nonNull(subject)) {
             Map<Subject, Integer> subjectsToEstimates = studentSubjects.get(student);
             if (subjectsToEstimates != null) {
-                subjectsToEstimates.forEach((streamSubject, estimates) -> {
-                    if (streamSubject.equals(subject)) {
-                        subjectsToEstimates.remove(streamSubject);
+                Iterator<Map.Entry<Subject, Integer>> subjectsToEstimatesIterator = subjectsToEstimates.entrySet().iterator();
+                if (subjectsToEstimatesIterator.hasNext()) {
+                    Map.Entry<Subject, Integer> subjectToEstimate = subjectsToEstimatesIterator.next();
+                    if (subjectToEstimate.getKey().equals(subject)) {
+                        subjectsToEstimatesIterator.remove();
                     }
-                });
+                }
             }
             studentSubjects.put(student, subjectsToEstimates);
             if (subjectStudents.get(subject) != null) {
