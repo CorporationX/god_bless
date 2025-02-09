9a751ddfe28a3a55be75eab8780921e498712b82
Vladimir Mamaev <ipmamaev.v@gmail.com>
2025-02-09 21:15:18 +0300

    Добавлены изменения в код после ревью

--- src/main/java/bjs257119/Main.java
+++ src/main/java/bjs257119/Main.java
@@ -16,17 +16,23 @@ public class Main {
         System.out.printf("Size of spellById before addSpellEvent method: %d %n", spellById.size());
         System.out.printf("Size of spellsByType before addSpellEvent method: %d %n", spellsByType.size());
         hogwartsSpells.addSpellEvent("Cemetery Magic", "Find active tomb");
-        hogwartsSpells.addSpellEvent("Cemetery Magic", "Establish contact with the Owner of the cemetery");
-        hogwartsSpells.addSpellEvent("Cemetery Magic", "Summon the Spirit of the Dead for advice");
-        hogwartsSpells.addSpellEvent("Klippotic Magic", "Initiation with Naahema");
-        hogwartsSpells.addSpellEvent("Klippotic Magic", "Activate the Sigil of Bairiron");
-        hogwartsSpells.addSpellEvent("Klippotic Magic", "Make a request to the Other Party");
+        hogwartsSpells.addSpellEvent("Cemetery Magic",
+                "Establish contact with the Owner of the cemetery");
+        hogwartsSpells.addSpellEvent("Cemetery Magic",
+                "Summon the Spirit of the Dead for advice");
+        hogwartsSpells.addSpellEvent("Klippotic Magic",
+                "Initiation with Naahema");
+        hogwartsSpells.addSpellEvent("Klippotic Magic",
+                "Activate the Sigil of Bairiron");
+        hogwartsSpells.addSpellEvent("Klippotic Magic",
+                "Make a request to the Other Party");
         System.out.printf("Size of spellById after addSpellEvent method: %d %n", spellById.size());
         System.out.printf("Size of spellsByType after addSpellEvent method: %d %n", spellsByType.size());
 
         System.out.printf("Found SpellEvent by id: %s %n", hogwartsSpells.getSpellEventById(4));
 
-        System.out.printf("Found SpellEvents by type: %s %n", hogwartsSpells.getSpellEventsByType("Klippotic Magic"));
+        System.out.printf("Found SpellEvents by type: %s %n",
+                hogwartsSpells.getSpellEventsByType("Klippotic Magic"));
 
         System.out.printf("Size of spellById before deleteSpellEvent method: %d %n", spellById.size());
         System.out.printf("Size of spellsByType before deleteSpellEvent method: %d %n", spellsByType.size());
