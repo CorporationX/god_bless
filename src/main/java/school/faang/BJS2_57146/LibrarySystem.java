1047d5a970a861512b8134917f83a4214cdc199a
Gulnaz <gumerova.gulnaz2014@yandex.ru>
2025-02-09 03:31:34 +0300

    Исправлены недочеты в классе LibrarySystem

--- src/main/java/school/faang/BJS2_57146/LibrarySystem.java
+++ src/main/java/school/faang/BJS2_57146/LibrarySystem.java
@@ -7,6 +7,7 @@ public class LibrarySystem {
     private static final Map<Book, String> LIBRARY = new HashMap<>();
 
     public void addBook(String title, String author, int year, String location) {
+        isValid(title, author);
         Book book = new Book(title, author, year);
         LIBRARY.put(book, location);
         System.out.println("Вы добавили книгу " + book + " на полку " + location);
@@ -17,6 +18,8 @@ public class LibrarySystem {
         if (LIBRARY.containsKey(book)) {
             LIBRARY.remove(book);
             System.out.println("Книга " + book + " успешно удалена");
+        } else {
+            System.out.println("Извините, но такой книги в библиотеке нет");
         }
     }
 
@@ -31,9 +34,29 @@ public class LibrarySystem {
     }
 
     public void printAllBooks() {
+        StringBuilder allBooks = new StringBuilder();
         System.out.println("В библиотеке имеются:");
         for (Map.Entry<Book, String> entry : LIBRARY.entrySet()) {
-            System.out.println(entry.getKey() + " полка " + entry.getValue());
+            allBooks.append(entry.getKey())
+                    .append(" полка ").append(entry.getValue()).append("\n");
         }
+        System.out.println(allBooks);
+    }
+
+    private void validatingTitle(String title) {
+        if (title == null || title.isBlank()) {
+            throw new IllegalArgumentException("Invalid title");
+        }
+    }
+
+    private void validatingAuthor(String author) {
+        if (author == null || author.isBlank()) {
+            throw new IllegalArgumentException("Invalid author");
+        }
+    }
+
+    private void isValid(String title, String author) {
+        validatingTitle(title);
+        validatingAuthor(author);
     }
 }
