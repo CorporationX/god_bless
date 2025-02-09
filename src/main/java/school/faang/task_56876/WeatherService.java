3fe0a44e60cc75e473ab54b47e5d27023c4b8bc4
Yaroslav Olkin <shrelok.pupkin@yandex.ru>
2025-02-09 18:46:29 +0300

    добавил методы в main

--- src/main/java/school/faang/task_56876/WeatherService.java
+++ src/main/java/school/faang/task_56876/WeatherService.java
@@ -3,14 +3,16 @@ package school.faang.task_56876;
 import java.util.Random;
 
 public class WeatherService implements WeatherProvider {
-    private Random random = new Random();
-    private static final double minTemperature = -20;
-    private static final double maxTemperature = 20;
+    private final Random random = new Random();
+    private static final double MIN_TEMPERATURE = -20;
+    private static final double MAX_TEMPERATURE = 20;
+    private static final double MIN_HUMIDITY = 45;
+    private static final double MAX_HUMIDITY = 20;
 
     @Override
     public WeatherData fetchWeatherData(String city) {
-        double temperature = random.nextDouble(minTemperature, maxTemperature);
-        double humidity = random.nextDouble(45, 100);
+        double temperature = MIN_TEMPERATURE + (MAX_TEMPERATURE - MIN_TEMPERATURE) * random.nextDouble();
+        double humidity = MIN_HUMIDITY + (MAX_HUMIDITY - MIN_HUMIDITY) * random.nextDouble();
         long timestamp = System.currentTimeMillis();
         return new WeatherData(city, temperature, humidity, timestamp);
     }
