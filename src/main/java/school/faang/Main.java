e93fb8eb1a87da6b9ea0aeb0d4ef730c0b073b1b
Sergey Ermoshenko <ermoshenko.work@gmail.com>
2025-02-09 19:38:57 +0300

    add inventory manager

--- src/main/java/school/faang/Main.java
+++ src/main/java/school/faang/Main.java
@@ -1,4 +1,22 @@
 package school.faang;
 
 public class Main {
+    public static void main(String[] args) {
+        Character frodo = new Character("Frodo");
+        Item ring = new Item("The One Ring", 1000);
+        InventoryManager manager = new InventoryManager();
+
+        manager.addItem(frodo, ring, (item) ->
+                System.out.printf("%s был добавлен в инвентарь.\n", item.getName())
+        );
+        manager.removeItem(frodo, (item) -> item.getName().equals("The One Ring"));
+        manager.addItem(frodo, ring, (item) -> System.out.println(item.getName() + " снова добавлен."));
+
+        manager.updateItem(frodo, (item) -> item.getName().equals("The One Ring"),
+                (item) -> new Item(item.getName(), item.getValue() * 2)
+        );
+
+        frodo.getInventory().forEach(item -> System.out.printf("%s : %d\n", item.getName(), item.getValue()));
+
+    }
 }
