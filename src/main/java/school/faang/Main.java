e82d2fbfa143fceaa2df086e728a26410fa2b5c9
Evgeny Morzeev <morzeevevg@gmail.com>
2025-02-09 16:56:48 +0300

    BJS2-56395, task completed

--- src/main/java/school/faang/Main.java
+++ src/main/java/school/faang/Main.java
@@ -4,7 +4,7 @@ import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) {
-        int[] evenNumOfNumsArray = {1, 2, 3, 4, 5, 6, 7, 8};
+        int[] evenNumOfNumsArray = {1, 2, 3, 4, 5, 6};
         System.out.print("Source array with an even number of numbers: ");
         System.out.println(Arrays.toString(evenNumOfNumsArray));
         System.out.print("Reversed array with an even number of numbers: ");
