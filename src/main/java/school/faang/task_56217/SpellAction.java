3d411a96e2c7a6804470155643aceadf055e7806
Artem Doronin <doronintema2018@yandex.ru>
2025-02-09 22:10:00 +0500

    Хогвартс, Хогвартс, наш любимый Хогвартс

---
+++ src/main/java/school/faang/task_56217/SpellAction.java
@@ -0,0 +1,6 @@
+package school.faang.task_56217;
+
+@FunctionalInterface
+public interface SpellAction {
+    String action(String str);
+}
---
+++ src/main/java/school/faang/task_56217/SpellCaster.java
@@ -0,0 +1,7 @@
+package school.faang.task_56217;
+
+public class SpellCaster {
+    public void cast(String str, SpellAction spellAction) {
+        System.out.println(spellAction.action(str));
+    }
+}
