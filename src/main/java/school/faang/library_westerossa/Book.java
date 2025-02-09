c25714f50bbf84dce6deb4b28f2932cc71959643
User Rody4457 <rodyon0212@gmail.com>
2025-02-09 11:13:14 +0400

    первый коммит

---
+++ src/main/java/school/faang/library_westerossa/Book.java
@@ -0,0 +1,17 @@
+package school.faang.library_westerossa;
+
+import lombok.Data;
+
+@Data
+public class Book {
+
+    private final String title;
+    private final String author;
+    private final int year;
+
+    public Book(String title, String author, int year) {
+        this.title = title;
+        this.author = author;
+        this.year = year;
+    }
+}
