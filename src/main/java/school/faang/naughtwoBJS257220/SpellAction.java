39c90dd68300f964aecfafc61f76c106b6685a8d
naught_wo <catrinesaxon@yandex.ru>
2025-02-09 18:18:26 +0300

    Задание "Хогвартс, Хогвартс, наш любимый Хогвартс"

---
+++ src/main/java/school/faang/naughtwoBJS257220/SpellAction.java
@@ -0,0 +1,6 @@
+package school.faang.naughtwoBJS257220;
+
+@FunctionalInterface
+public interface SpellAction {
+    String castSpell(String spellName);
+}
---
+++ src/main/java/school/faang/naughtwoBJS257220/SpellCaster.java
@@ -0,0 +1,7 @@
+package school.faang.naughtwoBJS257220;
+
+public class SpellCaster {
+    public void cast(String spellName, SpellAction spellAction) {
+        System.out.println(spellAction.castSpell(spellName));
+    }
+}
