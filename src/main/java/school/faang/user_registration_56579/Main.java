3882fbe090a613f6762a3779ac4cbfb80a27ed87
Anton <basanton@mail.ru>
2025-02-09 13:23:57 +0300

    Выполнение задачи по "регистрации пользователей" версия 1

---
+++ src/main/java/school/faang/user_registration_56579/Main.java
@@ -0,0 +1,7 @@
+package school.faang.user_registration_56579;
+
+public class Main {
+    public static void main(String[] args) {
+        User user1 = new User("John", 20, "Uber", "London");
+    }
+}
