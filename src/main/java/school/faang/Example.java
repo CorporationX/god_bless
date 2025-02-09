873efb06efe9bbc8064e208be27eac087b0bd993
Evgeny Morzeev <morzeevevg@gmail.com>
2025-02-09 12:47:21 +0300

    BJS2-56395, в выводе на экран добавил исходный массив для наглядности; изменил имена переменных left и right на leftIndex и rightIndex соответственно; добавил тесты для массивов с одним элементом, с четным и нечетным количество элементов, и пустого массива; добавил проверку на ноль и пустой массив в методе reverse.

--- src/main/java/school/faang/Example.java
+++ src/main/java/school/faang/Example.java
@@ -2,15 +2,20 @@ package school.faang;
 
 public class Example {
     public static int[] reverse(int[] nums) {
-        int left = 0;
-        int right = nums.length - 1;
+        if (nums == null || nums.length == 0) {
+            System.out.print("Error! The input array is null or empty! ");
+            return nums;
+        }
 
-        while (left < right) {
-            int temp = nums[left];
-            nums[left] = nums[right];
-            nums[right] = temp;
-            left++;
-            right--;
+        int leftIndex = 0;
+        int rightIndex = nums.length - 1;
+
+        while (leftIndex < rightIndex) {
+            int temp = nums[leftIndex];
+            nums[leftIndex] = nums[rightIndex];
+            nums[rightIndex] = temp;
+            leftIndex++;
+            rightIndex--;
         }
         return nums;
     }
--- src/main/java/school/faang/Main.java
+++ src/main/java/school/faang/Main.java
@@ -4,7 +4,32 @@ import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) {
-        int[] nums = {2, 55, -3, 4, 19, -7, 0, 1};
-        System.out.println(Arrays.toString(Example.reverse(nums)));
+        int[] evenNumOfNumsArray = {1, 2, 3, 4, 5, 6, 7, 8};
+        System.out.print("Source array with an even number of numbers: ");
+        System.out.println(Arrays.toString(evenNumOfNumsArray));
+        System.out.print("Reversed array with an even number of numbers: ");
+        System.out.println(Arrays.toString(Example.reverse(evenNumOfNumsArray)));
+        System.out.println();
+
+        int[] oddNumOFNumsArray = {1, 2, 3, 4, 5, 6, 7, 8, 9};
+        System.out.print("Source array with an odd number of numbers: ");
+        System.out.println(Arrays.toString(oddNumOFNumsArray));
+        System.out.print("Reversed array with an odd number of numbers: ");
+        System.out.println(Arrays.toString(Example.reverse(oddNumOFNumsArray)));
+        System.out.println();
+
+        int[] oneElementArray = {1};
+        System.out.print("Source array with one element: ");
+        System.out.println(Arrays.toString(oneElementArray));
+        System.out.print("Reversed array with one element: ");
+        System.out.println(Arrays.toString(Example.reverse(oneElementArray)));
+        System.out.println();
+
+        int[] emptyArray = {};
+        System.out.print("Source empty array: ");
+        System.out.println(Arrays.toString(emptyArray));
+        System.out.print("Reversed empty array: ");
+        System.out.println(Arrays.toString(Example.reverse(emptyArray)));
+        System.out.println();
     }
 }
