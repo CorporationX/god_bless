0080019a03d746f2dbf4a271c11690d9b9c94aef
romankondratev <romankondratev.com@gmail.com>
2025-02-10 15:09:53 -0800

    style: исправить цитату в классе Main
    - удалена лишняя часть цитаты в методе main

--- src/main/java/school/faang/Main.java
+++ src/main/java/school/faang/Main.java
@@ -2,6 +2,6 @@ package school.faang;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Всего одна ошибка – и ты ошибся! — Jason Statham");
+        System.out.println("Всего одна ошибка – и ты ошибся!");
     }
 }
