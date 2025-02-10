85b3e7dbfaa0803c6c55f19089d0094b592a5f93
Sultan <73115468+SultanGG505@users.noreply.github.com>
2025-02-10 12:03:45 +0300

    BJS2-56505
    Группировка пользователей по возрасту
    Добавил использование аннотаций lombock, а также поправил мелкие ошибки.

--- src/main/java/school/faang/task_56505/User.java
+++ src/main/java/school/faang/task_56505/User.java
@@ -2,39 +2,25 @@ package school.faang.task_56505;
 
 import java.util.*;
 
+import lombok.Data;
+
+@Data
 public class User {
     private final String name;
     private final int age;
     private final String workplace;
     private final String address;
 
-    public User(String name, int age, String workplace, String address) {
-        this.name = name;
-        this.age = age;
-        this.workplace = workplace;
-        this.address = address;
-    }
-
-    public int getAge() {
-        return age;
-    }
-
     public static Map<Integer, List<User>> groupUsers(List<User> users) {
         Map<Integer, List<User>> groupedUsers = new HashMap<>();
-
-        for (User user : users) {
-            int age = user.getAge();
-            if (!groupedUsers.containsKey(age)) {
-                groupedUsers.put(age, new ArrayList<>());
+        try {
+            for (User user : users) {
+                int age = user.getAge();
+                groupedUsers.computeIfAbsent(age, ageKey -> new ArrayList<>()).add(user);
             }
-            groupedUsers.get(age).add(user);
+        } catch (NullPointerException e) {
+            System.out.println("Exception for users = " + e);
         }
-
         return groupedUsers;
-    }
-
-    @Override
-    public String toString() {
-        return "User{name='" + name + "', age=" + age + ", workplace='" + workplace + "', address='" + address + "'}";
     }
 }
