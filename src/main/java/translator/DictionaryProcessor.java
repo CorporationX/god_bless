64275b4b9ee793684bd78ddf1c6f26a85e33d8f5
Alex P <a117a117a@gmail.com>
2025-02-10 12:57:49 +0100

    completed DictionaryProcessor feature

---
+++ src/main/java/translator/DictionaryProcessor.java
@@ -0,0 +1,10 @@
+package BJS257082;
+import java.util.function.BiConsumer;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+public class DictionaryProcessor {
+    public void processWord(String word, String translation, BiConsumer<String, String> consumer) {
+
+    }
+}
