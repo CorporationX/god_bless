4ea074486a7edba207254f919fc1918492f23c09
Ilhan Mustafa <ilhnmst@gmail.com>
2025-02-10 13:29:41 +0300

    Solving BJS2-57502

---
+++ src/main/java/school/faang/sprint2/task_BJS2_57502/SpellAction.java
@@ -0,0 +1,6 @@
+package school.faang.sprint2.task_BJS2_57502;
+
+@FunctionalInterface
+public interface SpellAction {
+    public String act(String act);
+}
---
+++ src/main/java/school/faang/sprint2/task_BJS2_57502/SpellCaster.java
@@ -0,0 +1,7 @@
+package school.faang.sprint2.task_BJS2_57502;
+
+public class SpellCaster {
+    public void cast(String spell, SpellAction action) {
+        System.out.println(action.act(spell));
+    }
+}
