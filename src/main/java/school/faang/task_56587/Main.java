e8b6b7f5eb0984a3de5add967a182535a16eccc4
Sultan <73115468+SultanGG505@users.noreply.github.com>
2025-02-10 15:26:56 +0300

    BJS2-56587
    Регистрация пользователей
    убрал после ревью магические числа, убрал лишний else и добавил аннотацию @Log.

--- src/main/java/school/faang/task_56587/Main.java
+++ src/main/java/school/faang/task_56587/Main.java
@@ -1,12 +1,15 @@
 package school.faang.task_56587;
 
+import lombok.extern.java.Log;
+
+@Log
 public class Main {
     public static void main(String[] args) {
         try {
-            User badUser = new User("", 17, "Microsoft", "Berlin");
             User goodUser = new User("Anna", 22, "Google", "London");
+            System.out.println(goodUser);
         } catch (IllegalArgumentException e) {
-            System.out.println(e.getMessage());  // Print the error message to the console
+            log.severe("Error creating user = " + e);
         }
     }
 }
