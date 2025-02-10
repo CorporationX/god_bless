0d17411a59a05443f38fd7db33bab754e9fbbda8
alex198 <abykovskih@gmail.com>
2025-02-11 00:42:34 +0300

    BJS2-57037 created classes Student, Subject and StudentDatabase. Implemented methods for StudentDatabase class

---
+++ src/main/java/school/faang/bjs2_57037/Student.java
@@ -0,0 +1,9 @@
+package school.faang.bjs2_57037;
+
+import lombok.Data;
+
+@Data
+public class Student {
+    private String name;
+
+}
---
+++ src/main/java/school/faang/bjs2_57037/Subject.java
@@ -0,0 +1,8 @@
+package school.faang.bjs2_57037;
+
+import lombok.Data;
+
+@Data
+public class Subject {
+    private String name;
+}
