7609af245a9ef6de8c62c1ba29bcb0346167f09e
Tony King <a.korolev9927@gmail.com>
2025-02-10 20:24:32 +0300

    start

---
+++ src/main/java/school/faang/task_57279/EmailProcessor.java
@@ -0,0 +1,10 @@
+package school.faang.task_57279;
+
+import java.util.List;
+
+public class EmailProcessor {
+
+    public void isImportant (List<Email> emails,) {
+
+    }
+}
---
+++ src/main/java/school/faang/task_57279/Main.java
@@ -0,0 +1,4 @@
+package school.faang.task_57279;
+
+public class Main {
+}
