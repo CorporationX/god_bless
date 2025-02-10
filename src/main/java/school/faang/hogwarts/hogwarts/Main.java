1d9bcc67b80c08a6ada5141432714cfb10c6319e
Andrey Ievlev <avievlev@gmail.com>
2025-02-11 00:15:57 +0300

    BJS2-57320 Хогвартс, Хогвартс, наш любимый Хогвартс Устранение замечаний

--- src/main/java/school/faang/hogwarts/hogwarts/Main.java
+++ src/main/java/school/faang/hogwarts/hogwarts/Main.java
@@ -3,20 +3,30 @@ package school.faang.hogwarts.hogwarts;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.util.List;
+
 public class Main {
     private static final Logger logger = LoggerFactory.getLogger(Main.class);
+    private static final String ALOHOMORA = "Alohomora";
+    private static final String LUMOS = "";
+    private static final String EXPELLIARMUS = "Expelliarmus";
 
     public static void main(String[] args) {
         SpellCaster spellCaster = new SpellCaster();
-        String alohomora = "Alohomora";
-        String lumos = "Lumos";
-        String expelliarmus = "Expelliarmus";
         try {
-            spellCaster.cast(alohomora, (spell) -> "The door is unlocked by " + spell);
-            spellCaster.cast(lumos, (spell) -> "A beam of light is created by " + spell);
-            spellCaster.cast(expelliarmus, (spell) -> "The opponent is disarmed by " + spell);
+            spellCaster.cast(ALOHOMORA, (spell) -> "The door is unlocked by " + spell);
         } catch (IllegalArgumentException e) {
-            logger.error(e.getMessage());
+            logger.info("There was a mistake with the spell {}.", ALOHOMORA);
+        }
+        try {
+            spellCaster.cast(LUMOS, (spell) -> "A beam of light is created by " + spell);
+        } catch (IllegalArgumentException e) {
+            logger.info("There was a mistake with the spell {}.", LUMOS);
+        }
+        try {
+            spellCaster.cast(EXPELLIARMUS, (spell) -> "The opponent is disarmed by " + spell);
+        } catch (IllegalArgumentException e) {
+            logger.info("There was a mistake with the spell {}.", EXPELLIARMUS);
         }
     }
 }
