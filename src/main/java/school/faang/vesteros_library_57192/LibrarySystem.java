987376029a1373b001b4900f79d308e04b8b5124
Anton <basanton@mail.ru>
2025-02-10 18:18:11 +0300

    Выполнение задачи по "библиотека вестероса" версия 2 (внес изменения по комментариям ментора)

--- src/main/java/school/faang/vesteros_library_57192/LibrarySystem.java
+++ src/main/java/school/faang/vesteros_library_57192/LibrarySystem.java
@@ -1,19 +1,31 @@
 package school.faang.vesteros_library_57192;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import java.util.HashMap;
 import java.util.Map;
 
 public class LibrarySystem {
     private Map<Book, String> bookPlace = new HashMap<>();
+    private static final Logger logger = LoggerFactory.getLogger(LibrarySystem.class);
 
     public void addBook(String title, String author, int year, String location) {
         Book book = new Book(title, author, year);
+        if (title == null || title.isBlank()) {
+            throw new IllegalArgumentException("Название книги не должно быть пустым");
+        }
+        if (author == null || author.isBlank()) {
+            throw new IllegalArgumentException("Информация об авторе книги не должна быть пустой");
+        }
+        if (year <= 0) {
+            throw new IllegalArgumentException("Год издания должен быть положительным числом");
+        }
         if (location == null || location.isBlank()) {
             throw new IllegalArgumentException("Информация о местоположении книги не должна быть пустой");
         }
         if (bookPlace.get(book) != null) {
-            System.out.println("Данная книга (или ее копия) уже хранится в библиотеке на месте: "
-                    + bookPlace.get(book));
+            logger.info("Данная книга (или ее копия) уже хранится в библиотеке на месте: {}", bookPlace.get(book));
         } else {
             bookPlace.put(book, location);
         }
@@ -21,14 +33,16 @@ public class LibrarySystem {
 
     public void removeBook(String title, String author, int year) {
         Book book = new Book(title, author, year);
-        if (bookPlace.isEmpty()) {
-            System.out.println("В библиотеке нет книг");
-        }
         if (bookPlace.get(book) == null) {
             System.out.println("Такая книга не обнаружена");
         } else {
-            bookPlace.remove(book);
+            if (bookPlace.isEmpty()) {
+                System.out.println("В библиотеке нет книг");
+            } else {
+                bookPlace.remove(book);
+            }
         }
+
     }
 
     public void findBook(String title, String author, int year) {
@@ -41,7 +55,7 @@ public class LibrarySystem {
     }
 
     public void printAllBooks() {
-        for (Map.Entry book : bookPlace.entrySet()) {
+        for (Map.Entry<Book, String> book : bookPlace.entrySet()) {
             System.out.println("Книга: " + book.getKey() + " Местоположение: " + book.getValue());
         }
     }
