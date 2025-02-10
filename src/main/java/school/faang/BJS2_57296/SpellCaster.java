c2630c354de221600f8429174cacc993d12625ba
Pavel Chernobay <mr.chernobay1990@gmail.com>
2025-02-10 19:18:34 +0300

    BJS2-57296 <task>

---
+++ src/main/java/school/faang/BJS2_57296/SpellCaster.java
@@ -0,0 +1,8 @@
+package school.faang.BJS2_57296;
+
+public class SpellCaster {
+
+    public void cast(String spell, SpellAction action) {
+        System.out.println(action.castSpell(spell));
+    }
+}
