70be5b7c6946f9c1df89099087eba0eade98fff7
RamilNazmiev192 <nazmiev633@gmail.com>
2025-02-10 10:48:38 +0300

    method structure and add main

--- src/main/java/school/faang/BJS2_56917/ProductManager.java
+++ src/main/java/school/faang/BJS2_56917/ProductManager.java
@@ -5,17 +5,17 @@ import java.util.concurrent.atomic.AtomicInteger;
 
 
 public class ProductManager {
-    private static final Set<Product> products = new HashSet<>();
-    private static final Map<Category, List<Product>> categoryMap = new HashMap<>();
-    private static final AtomicInteger counter = new AtomicInteger(1);
+    private final Set<Product> products = new HashSet<>();
+    private final Map<Category, List<Product>> categoryMap = new HashMap<>();
+    private final AtomicInteger counter = new AtomicInteger(1);
 
-    private static void addProduct(Category category, String name) {
+    public void addProduct(Category category, String name) {
         Product newProduct = new Product(counter.getAndIncrement(), name, category);
         products.add(newProduct);
         categoryMap.computeIfAbsent(category, (k -> new ArrayList<>())).add(newProduct);
     }
 
-    private static void removeProduct(Category category, String name) {
+    public void removeProduct(Category category, String name) {
         if (category == null) {
             System.out.println("⚠️ Category cannot be null.");
             return;
@@ -32,7 +32,7 @@ public class ProductManager {
         categoryMap.get(category).remove(newProduct);
     }
 
-    private static List<Product> findProductsByCategory(Category category) {
+    public List<Product> findProductsByCategory(Category category) {
         if (category == null) {
             System.out.println("⚠️ Category cannot be null.");
             return new ArrayList<>();
@@ -53,8 +53,7 @@ public class ProductManager {
         return categoryMap.get(category);
     }
 
-    private static void groupProductsByCategory() {
-        // этот метод я добаил только иза задания в нем нет нужды так как и так уже все группируется автомаический
+    public void groupProductsByCategory() {
         categoryMap.clear();
 
         products.forEach(product -> {
@@ -62,7 +61,7 @@ public class ProductManager {
         });
     }
 
-    private static void printAllProducts() {
+    public void printAllProducts() {
         categoryMap.forEach((key, value) -> {
             System.out.printf("Категория: %s%n", key);
             value.forEach(product -> {
@@ -73,31 +72,4 @@ public class ProductManager {
         });
     }
 
-    public static void main(String[] args) {
-        // FOOD (2 продукта)
-        addProduct(Category.FOOD, "Hamburger");
-        addProduct(Category.FOOD, "Pizza");
-
-        // ELECTRONICS (2 продукта)
-        addProduct(Category.ELECTRONICS, "Laptop");
-        addProduct(Category.ELECTRONICS, "Smartphone");
-
-        // CLOTHING (2 продукта)
-        addProduct(Category.CLOTHING, "T-Shirt");
-        addProduct(Category.CLOTHING, "Jeans");
-
-        // OTHER (2 продукта)
-        addProduct(Category.OTHER, "Book");
-        addProduct(Category.OTHER, "Toy");
-        printAllProducts();
-
-        System.out.print("вызов метода groupProductsByCategory" + "\n");
-        groupProductsByCategory();
-        printAllProducts();
-
-
-        findProductsByCategory(Category.CLOTHING);
-        removeProduct(Category.CLOTHING, "Jeans");
-        printAllProducts();
-    }
 }
