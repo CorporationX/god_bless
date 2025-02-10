3a6ae3fb671cb1d8b195df6b75b8ede5bb6ad0a9
Alexander Makarov <makar.a.v100@gmail.com>
2025-02-10 23:27:31 +0300

    Регистрация пользователей исправленная

--- src/main/java/school/faang/User.java
+++ src/main/java/school/faang/User.java
@@ -6,51 +6,81 @@ import static school.faang.Addresses.VALID_ADDRESSES;
 import static school.faang.Jobs.VALID_JOBS;
 
 public class User {
+    public static final int MIN_AGE = 18;
+    static Scanner console = new Scanner(System.in);
+
     public static void main(String[] args) {
-        Scanner console = new Scanner(System.in);
 
-        System.out.println("Enter your name");
-        String name = console.nextLine().trim();
-
-        if (name.isEmpty()) {
-            System.out.println("Try again");
+        if (enterName() || enterAge()) {
             return;
         }
 
+        console.nextLine();
+
+        if (enterJob() || enterAddress()) {
+            return;
+        }
+
+        console.close();
+    }
+
+    private static boolean enterName() {
+        System.out.println("Enter your name");
+        String name = console.nextLine();
+        if (name == null || name.isEmpty()) {
+
+            try {
+                throw new IllegalArgumentException();
+            } catch (IllegalArgumentException e) {
+                System.out.println("Try again");
+                return true;
+            }
+        }
+        return false;
+    }
+
+    private static boolean enterAge() {
         System.out.println("Enter your age");
         int age = console.nextInt();
 
-        if (age <= 0) {
-            System.out.println("This is impossible");
-            return;
-        } else if (age < 18) {
-            System.out.println("Sorry, you are too young");
-            return;
+        if (age < MIN_AGE) {
+            try {
+                throw new IllegalArgumentException();
+            } catch (IllegalArgumentException e) {
+                System.out.println("Sorry, you are too young");
+                return true;
+            }
         }
-        console.nextLine();
+        return false;
+    }
 
-        System.out.println("Enter your place of work");
-        String jobs = console.nextLine().trim();
-        if (jobs.isEmpty()) {
-            System.out.println("Error");
-            return;
-        }
-        if (VALID_JOBS.contains(jobs)) {
-            System.out.println("This company suits us");
-        } else {
-            System.out.println("This company is not suitable for us");
-            return;
-        }
+    private static boolean enterJob() {
+        System.out.println("Enter your place of job");
+        String job = console.nextLine().trim();
 
+        if (!VALID_JOBS.contains(job)) {
+            try {
+                throw new IllegalArgumentException();
+            } catch (IllegalArgumentException e) {
+                System.out.println("This company is not suitable for us");
+                return true;
+            }
+        }
+        return false;
+    }
+
+    private static boolean enterAddress() {
         System.out.println("Enter the location of the company");
-        String addresses = console.nextLine().trim();
+        String address = console.nextLine().trim();
 
-        if (VALID_ADDRESSES.contains(addresses)) {
-            System.out.println("This city suits us");
-        } else {
-            System.out.println("This city is not suitable for us");
-            return;
+        if (!VALID_ADDRESSES.contains(address)) {
+            try {
+                throw new IllegalArgumentException();
+            } catch (IllegalArgumentException e) {
+                System.out.println("This city is not suitable for us");
+                return true;
+            }
         }
-        console.close();
+        return false;
     }
 }
