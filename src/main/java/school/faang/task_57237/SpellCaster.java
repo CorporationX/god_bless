a5b6d12838748036b32c016a9efbe188a9166a7e
Tony King <a.korolev9927@gmail.com>
2025-02-10 13:07:07 +0300

    Правка в сигнатуре SpellAction<String>

--- src/main/java/school/faang/task_57237/SpellCaster.java
+++ src/main/java/school/faang/task_57237/SpellCaster.java
@@ -5,7 +5,7 @@ import lombok.extern.slf4j.Slf4j;
 @Slf4j
 public class SpellCaster {
 
-    public void cast(String spellName, SpellAction action) {
+    public void cast(String spellName, SpellAction<String> action) {
 
         if (spellName == null || spellName.isBlank()) {
             log.info("название заклинания либо NULL либо пустое!");
@@ -19,7 +19,7 @@ public class SpellCaster {
         System.out.println(action.execute(spellName));
     }
 
-    public interface SpellAction<S> {
-        String execute(S action);
+    public interface SpellAction<T> {
+        String execute(T action);
     }
 }
