f7f5b3946062a5932041728b21dc596df5934530
WhatINeedToDo <zsdfghjkl111@gmail.com>
2025-02-10 13:58:46 +0700

    * Add methods

--- src/main/java/BJS256905/Product.java
+++ src/main/java/BJS256905/Product.java
@@ -2,9 +2,11 @@ package BJS256905;
 
 import lombok.*;
 
+import java.util.Objects;
+
 @Getter
 @Setter
-@EqualsAndHashCode
+@ToString
 public class Product {
     private int id;
     private String name;
@@ -13,5 +15,23 @@ public class Product {
     public Product(String name, Category category) {
         this.name = name;
         this.category = category;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == this) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        Product product = (Product) o;
+        return Objects.equals(name, product.name)
+                && Objects.equals(category, product.category);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, category);
     }
 }
--- src/main/java/BJS256905/ProductManager.java
+++ src/main/java/BJS256905/ProductManager.java
@@ -3,49 +3,83 @@ package BJS256905;
 import java.util.*;
 
 public class ProductManager {
+
     private Set<Product> products = new HashSet<>();
     private Map<Category, ArrayList<Product>> categoryMap = new HashMap<>();
     private int id;
 
-
+    /**
+     * метод добавляет в мап, только если произошло добавление в множество,
+     * т.е. продукта раньше не было
+     */
     public void addProduct(Category category, String name) {
         if (category != null && !name.isEmpty()) {
             Product product = new Product(name, category);
             product.setId(++id);
-            ArrayList<Product> productToList = new ArrayList<>();
-            productToList.add(product);
-            products.add(product);
-            categoryMap.put(category, productToList);
-            StringBuilder addedProduct = new StringBuilder();
+            if (products.add(product)) {
+                // Добавление в Map
+                categoryMap.putIfAbsent(category, new ArrayList<>());
+                categoryMap.get(category).add(product);
+                StringBuilder addedProduct = new StringBuilder();
 
-            addedProduct.append("Added product: ")
-                    .append(product.getName()).append(", ")
-                    .append(product.getCategory()).append("\n");
-            System.out.println(addedProduct);
+                addedProduct.append("Added product: ")
+                        .append(product.getName()).append(", ")
+                        .append(product.getCategory()).append("\n");
+                System.out.println(addedProduct);
+            }
         }
     }
 
     public void removeProduct(Category category, String name) {
         if (category != null && !name.isEmpty()) {
             Product product = new Product(name, category);
-            if (categoryMap.equals(product.getCategory())) {
-                if (products.contains(product)) {
-                    StringBuilder removedProduct = new StringBuilder();
-                    removedProduct.append("Removed product: ")
-                            .append(product.getName()).append(", ")
-                            .append(product.getCategory()).append("\n");
-                    products.remove(product);
-                    categoryMap.remove(product);
+            // Проверяем наличие категории и продукта в коллекциях
+            if (products.contains(product) && categoryMap.containsKey(category)) {
+                // Удаляем из множества
+                products.remove(product);
 
-                    System.out.println(removedProduct.toString());
+                // Удаляем из списка продуктов, связанного с категорией
+                ArrayList<Product> productList = categoryMap.get(category);
+                productList.remove(product);
+
+                // Удаляем пустую категорию из мапы
+                if (productList.isEmpty()) {
+                    categoryMap.remove(category);
                 }
+
+                System.out.println("Removed product: " + product.getName() + ", " + product.getCategory());
+            } else {
+                System.out.println("Product or category not found.");
             }
+        } else {
+            System.out.println("Invalid category or product name.");
         }
 
     }
 
     public ArrayList<Product> findProductByCategory(Category category) {
         return categoryMap.getOrDefault(category, new ArrayList<>());
+    }
+
+    public Map<Category, List<Product>> groupProductsByCategory() {
+        Map<Category, List<Product>> productMap = new HashMap<>();
+        for (Product product : products) {
+            if (!productMap.containsKey(product.getCategory())) {
+                productMap.put(product.getCategory(), new ArrayList<>());
+            }
+            productMap.get(product.getCategory()).add(product);
+        }
+        System.out.println(productMap);
+        return productMap;
+    }
+
+    public void printAllProducts() {
+        for (Map.Entry<Category, ArrayList<Product>> entry : categoryMap.entrySet()) {
+            System.out.println("Категория: " + entry.getKey());
+            for (Product product : entry.getValue()) {
+                System.out.println("  - " + product.getName());
+            }
+        }
     }
 
 
