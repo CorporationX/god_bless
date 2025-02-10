929d494468aa11faeb7dab695fbd962739545d59
Artem Doronin <doronintema2018@yandex.ru>
2025-02-10 14:13:12 +0500

    fixes checkstyle

--- src/main/java/school/faang/task_57248/SpellCaster.java
+++ src/main/java/school/faang/task_57248/SpellCaster.java
@@ -4,8 +4,8 @@ import java.util.Objects;
 
 public class SpellCaster {
     public void cast(String str, SpellAction spellAction) {
-        Objects.requireNonNull(spellAction, " входящий параметр spellAction не может быть null");
-        Objects.requireNonNull(str, "входящий параметр str не может быть null");
+        Objects.requireNonNull(spellAction, " Входящий параметр spellAction не может быть null");
+        Objects.requireNonNull(str, "Входящий параметр str не может быть null");
         System.out.println(spellAction.action(str));
     }
 }
