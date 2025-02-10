67939fb46bac9426a9c6bdebad2db6cd5ba85bbe
Sofiya Mutovkina <sofiyamutovkina@gmail.com>
2025-02-10 13:55:44 +0300

    BJS2-56477

---
+++ src/main/java/school/faang/User.java
@@ -0,0 +1,52 @@
+package school.faang;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class User {
+    private final String name;
+    private final int age;
+    private final String workplace;
+    private final String address;
+
+    public User(String name, int age, String workplace, String address) {
+        this.name = name;
+        this.age = age;
+        this.workplace = workplace;
+        this.address = address;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public String getWorkplace() {
+        return workplace;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public static Map<Integer, List<User>> groupUsers(List<User> users) {
+        Map<Integer, List<User>> groupUsers = new HashMap<>();
+
+        for (User user : users) {
+            groupUsers.putIfAbsent(user.getAge(), new ArrayList<>());
+            groupUsers.get(user.getAge()).add(user);
+        }
+
+        return groupUsers;
+    }
+
+    @Override
+    public String toString() {
+        return "User{name='" + name + "' , age=" + age + ", workplace='" + workplace + "' , address='" + address + "'}";
+    }
+}
