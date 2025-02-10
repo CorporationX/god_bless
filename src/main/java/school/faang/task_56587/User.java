819e42f62df3905d598c52ea26e32a373590ba5f
Sultan <73115468+SultanGG505@users.noreply.github.com>
2025-02-10 15:26:56 +0300

    BJS2-56528
    Абстракция, абстракция!
    Настроил конструкторы для всех классов, учёл факт, что здоровье не может быть меньше нуля, провёл проверку на созданных в мэйне экземплярах классов + убрал после ревью магические числа, убрал лишний else и добавил аннотацию @Log.

--- src/main/java/school/faang/task_56587/User.java
+++ src/main/java/school/faang/task_56587/User.java
@@ -1,6 +1,7 @@
 package school.faang.task_56587;
 
 import lombok.Data;
+import lombok.NonNull;
 
 import java.util.Set;
 
@@ -13,21 +14,22 @@ public class User {
 
     private static final Set<String> VALID_JOBS = Set.of("Google", "Uber", "Amazon");
     private static final Set<String> VALID_ADDRESSES = Set.of("London", "New York", "Amsterdam");
+    private static final int correctAge = 18;
 
-    public User(String name, int age, String job, String address) {
+    public User(@NonNull String name, int age, String job, String address) {
         if (name.isEmpty()) {
             throw new IllegalArgumentException("Name is empty!");
-        } else if (age < 18) {
+        } else if (age < correctAge) {
             throw new IllegalArgumentException("Age less than 18");
         } else if (!VALID_ADDRESSES.contains(address)) {
             throw new IllegalArgumentException("Incorrect address");
         } else if (!VALID_JOBS.contains(job)) {
             throw new IllegalArgumentException("Incorrect job");
-        } else {
-            this.name = name;
-            this.age = age;
-            this.address = address;
-            this.job = job;
         }
+        this.name = name;
+        this.age = age;
+        this.address = address;
+        this.job = job;
+
     }
 }
