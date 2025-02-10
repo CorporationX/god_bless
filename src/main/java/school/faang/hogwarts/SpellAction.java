093803ff140f3464a2dd7c8788819bfc32738adb
Ilyas Azhdar <azhdar902@gmail.com>
2025-02-10 13:30:06 +0500

    added validateInputs and javadoc

--- src/main/java/school/faang/hogwarts/SpellAction.java
+++ src/main/java/school/faang/hogwarts/SpellAction.java
@@ -1,6 +1,15 @@
 package school.faang.hogwarts;
 
+/**
+ * Функциональный интерфейс для выполнения заклинаний.
+ */
 @FunctionalInterface
 public interface SpellAction {
+    /**
+     * Применяет заклинание и возвращает результат
+     *
+     * @param spell название заклинания
+     * @return результат выполнения
+     */
     String cast(String spell);
 }
