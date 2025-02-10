18b43fe470f56a2dd00615b8fe96b773c52b2c6d
amanzholtanat <amanzholtanat@gmail.com>
2025-02-10 11:29:05 +0500

    Хогвартс, Хогвартс, наш любимый Хогвартс - BJS2-59649

--- src/main/java/school/faang/hogwardshogwards/Main.java
+++ src/main/java/school/faang/hogwardshogwards/Main.java
@@ -1,17 +1,17 @@
 package school.faang.hogwardshogwards;
 
 public class Main {
-    public static void main(String[] args) {
-        String accio = "Accio";
-        String nox = "Nox";
-        String avadaKedavra = "Avada Kedavra";
-        String makingAlive = "Making alive spell";
+    public static final String ACCIO = "Accio";
+    public static final String NOX = "Nox";
+    public static final String AVADA_KEDAVRA = "Avada Kedavra";
+    public static final String MAKING_ALIVE = "Making alive spell";
 
+    public static void main(String[] args) {
         SpellCaster spellCaster = new SpellCaster();
 
-        spellCaster.cast(accio, (spell) -> "The object was found by " + spell);
-        spellCaster.cast(nox, (spell) -> "The light was turned off by " + spell);
-        spellCaster.cast(avadaKedavra, (spell) -> "The man was killed by " + spell);
-        spellCaster.cast(makingAlive, (spell) -> "The man became alive by " + spell);
+        spellCaster.cast(ACCIO, (spell) -> "The object was found by " + spell);
+        spellCaster.cast(NOX, (spell) -> "The light was turned off by " + spell);
+        spellCaster.cast(AVADA_KEDAVRA, (spell) -> "The man was killed by " + spell);
+        spellCaster.cast(MAKING_ALIVE, (spell) -> "The man became alive by " + spell);
     }
 }
