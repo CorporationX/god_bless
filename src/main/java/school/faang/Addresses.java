d38f2f8a5b86d664f30d7955adac2e57013dcaf8
Alexander Makarov <makar.a.v100@gmail.com>
2025-02-11 14:18:09 +0300

    Регистрация пользователей исправленная. Конфликт исправлен

--- src/main/java/school/faang/Addresses.java
+++ src/main/java/school/faang/Addresses.java
@@ -3,5 +3,9 @@ package school.faang;
 import java.util.Set;
 
 public class Addresses {
+    private Addresses() {
+        throw new IllegalArgumentException("Utility class");
+    }
+
     public static final Set<String> VALID_ADDRESSES = Set.of("London", "New York", "Amsterdam");
 }
--- src/main/java/school/faang/Jobs.java
+++ src/main/java/school/faang/Jobs.java
@@ -3,5 +3,9 @@ package school.faang;
 import java.util.Set;
 
 public class Jobs {
+    private Jobs() {
+        throw new IllegalArgumentException("Utility class");
+    }
+
     public static final Set<String> VALID_JOBS = Set.of("Google", "Uber", "Amazon");
 }
