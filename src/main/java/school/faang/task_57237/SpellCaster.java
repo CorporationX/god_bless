32a06587520402e5705bef1cb6a6016716e76310
Tony King <a.korolev9927@gmail.com>
2025-02-11 11:21:56 +0300

    - Формирую исключения через IllegalArgumentException - Добавил анотацию @FunctionalInterface

--- src/main/java/school/faang/task_57237/SpellCaster.java
+++ src/main/java/school/faang/task_57237/SpellCaster.java
@@ -6,19 +6,17 @@ import lombok.extern.slf4j.Slf4j;
 public class SpellCaster {
 
     public void cast(String spellName, SpellAction<String> action) {
-
         if (spellName == null || spellName.isBlank()) {
-            log.info("название заклинания либо NULL либо пустое!");
-            return;
+            throw new IllegalArgumentException("spellName cannot be null or empty");
         }
 
         if (action == null) {
-            log.info("Поле заклинания не должно быть пустым");
-            return;
+            throw new IllegalArgumentException("action cannot be null");
         }
         System.out.println(action.execute(spellName));
     }
 
+    @FunctionalInterface
     public interface SpellAction<T> {
         String execute(T action);
     }
