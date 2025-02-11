8fc8d1dbc7b2e4728720b93c9e5e3bcd254c6579
Laura Lomberg <lomberglaura@gmail.com>
2025-02-11 22:38:31 +0300

    Add null checks. Add JavaDoc comments to the interface SpellAction. Refactor code.

--- src/main/java/spell/SpellAction.java
+++ src/main/java/spell/SpellAction.java
@@ -1,5 +1,14 @@
 package spell;
 
+/**
+ * Performs an action based on the given spell name.
+ * This method takes a spell name and returns a string that describes
+ * the result of the spell action.
+ *
+ * @param sellName the name of the spell to be cast.
+ * @return a string describing the result of the spell action.
+ */
+
 @FunctionalInterface
 public interface SpellAction {
     String castSpell(String spellName);
--- src/main/java/spell/SpellCaster.java
+++ src/main/java/spell/SpellCaster.java
@@ -1,7 +1,11 @@
 package spell;
 
+import java.util.Objects;
+
 public class SpellCaster {
     public void cast(String spellName, SpellAction spellAction) {
+        Objects.requireNonNull(spellName, "Spell name cannot be null.");
+        Objects.requireNonNull(spellAction, "Spell action cannot be null.");
         System.out.println(spellAction.castSpell(spellName));
     }
 }
