e8b35597c308b3ab75ecb8d40d9bdd241dc30783
Dmitry Rogulkin <rogulkind@yandex.ru>
2025-02-11 15:12:48 +0500

    https://faang-school.atlassian.net/browse/BJS2-57670

---
+++ src/main/java/school/faang/task57670/service/FilterProcessor.java
@@ -0,0 +1,4 @@
+package school.faang.task57670.service;
+
+public class FilterProcessor {
+}
---
+++ src/main/java/school/faang/task57670/service/Main.java
@@ -0,0 +1,6 @@
+package school.faang.task57670.service;
+
+public class Main {
+    public static void main(String[] args) {
+    }
+}
