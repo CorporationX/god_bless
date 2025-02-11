50d8bf2caddf8a197c185cd2c98cac93cac29490
Yaroslav Olkin <shrelok.pupkin@yandex.ru>
2025-02-11 14:49:52 +0300

    complete task

---
+++ src/main/java/school/faang/task_57368/InventoryManager.java
@@ -0,0 +1,27 @@
+package school.faang.task_57368;
+
+import java.util.List;
+import java.util.function.Consumer;
+import java.util.function.Function;
+import java.util.function.Predicate;
+
+public class InventoryManager {
+    public void addItem(Character character, Item item, Consumer<Item> consumer) {
+        character.getInventory().add(item);
+        consumer.accept(item);
+    }
+
+    public void removeItem(Character character, Predicate<Item> predicate) {
+        character.getInventory().removeIf(predicate);
+    }
+
+    public void updateItem(Character character, Predicate<Item> predicate, Function<Item, Item> function) {
+        List<Item> inventory = character.getInventory();
+        for (int i = 0; i < inventory.size(); i++) {
+            Item item = inventory.get(i);
+            if (predicate.test(item)) {
+                inventory.set(i, function.apply(item));
+            }
+        }
+    }
+}
