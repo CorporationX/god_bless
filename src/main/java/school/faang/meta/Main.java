578ee7b57f3754d2e49e6c9100a6ec359fe26a98
Sergey <bigwave2915@gmail.com>
2025-02-11 13:05:22 +0500

    решение задачи

--- src/main/java/school/faang/meta/Main.java
+++ src/main/java/school/faang/meta/Main.java
@@ -1,17 +1,24 @@
 package school.faang.meta;
 
-import java.util.function.Consumer;
-
 public class Main {
     public static void main(String[] args) {
         NotificationManager manager = new NotificationManager();
-        Consumer<Notification> consumer = new Consumer<Notification>() {
-            @Override
-            public void accept(Notification notification) {
-                System.out.println("Email: " + notification.getMessage());
-            }
-        };
-        manager.registerHandler(NotificationType.EMAIL, consumer);
-    }
 
+        manager.registerHandler(NotificationType.EMAIL, notification ->
+                System.out.println("Email: " + notification.getMessage()));
+
+        manager.registerHandler(NotificationType.SMS,
+                notification -> System.out.println("SMS: " + notification.getMessage()));
+
+        manager.registerHandler(NotificationType.PUSH,
+                notification -> System.out.println("Push : " + notification.getMessage()));
+
+        Notification email = new Notification(NotificationType.EMAIL, "Молодец");
+        Notification sms = new Notification(NotificationType.SMS, "Пришла СМС");
+        Notification push = new Notification(NotificationType.PUSH, "отправилось");
+
+        manager.sendNotification(email);
+        manager.sendNotification(sms);
+        manager.sendNotification(push);
+    }
 }
