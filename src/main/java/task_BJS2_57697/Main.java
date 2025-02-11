e3d4f112008f24746946426df54225552f88349a
Anton Lukyanchikov <antontotenkopf@gmail.com>
2025-02-11 17:34:46 +0300

    done task BJS2-57697

--- src/main/java/task_BJS2_57697/Main.java
+++ src/main/java/task_BJS2_57697/Main.java
@@ -14,10 +14,24 @@ public class Main {
         notificationManager.registerHandler(NotificationType.PUSH,
                 notification -> System.out.println("Push Notification: " + notification.getMessage())
         );
+        System.out.println();
 
         Notification emailNotification = new Notification(NotificationType.EMAIL, "Ваш аккаунт активирован");
         Notification smsNotification = new Notification(NotificationType.SMS, "Ваш пароль изменен");
         Notification pushNotification = new Notification(NotificationType.PUSH, "У вас новое сообщение!");
+
+        notificationManager.addSignature(emailNotification, notification -> {
+            notification.setMessage(emailNotification.getMessage() + emailNotification.getSignature());
+            return notification;
+        });
+        notificationManager.addSignature(smsNotification, notification -> {
+            notification.setMessage(smsNotification.getMessage() + emailNotification.getSignature());
+            return notification;
+        });
+        notificationManager.addSignature(pushNotification, notification -> {
+            notification.setMessage(pushNotification.getMessage() + emailNotification.getSignature());
+            return notification;
+        });
 
         notificationManager.sendNotification(emailNotification);
         notificationManager.sendNotification(smsNotification);
--- src/main/java/task_BJS2_57697/NotificationManager.java
+++ src/main/java/task_BJS2_57697/NotificationManager.java
@@ -1,35 +1,53 @@
 package task_BJS2_57697;
 
 import lombok.Data;
+import lombok.extern.slf4j.Slf4j;
 
 import java.util.HashMap;
 import java.util.Map;
 import java.util.function.Consumer;
+import java.util.function.Function;
 
 @Data
+@Slf4j
 public class NotificationManager {
     private final Map<NotificationType, Consumer<Notification>> notifications = new HashMap<>();
 
     public void registerHandler(NotificationType type, Consumer<Notification> handler) {
-
-        switch (type) {
-            case EMAIL -> {
-                handler.accept(new Notification(type, "просим подтвердить вашу почту!"));
-                notifications.put(type, handler);
+        try {
+            switch (type) {
+                case EMAIL -> {
+                    handler.accept(new Notification(type, "просим подтвердить вашу почту!"));
+                    notifications.put(type, handler);
+                }
+                case SMS -> {
+                    handler.accept(new Notification(type, "сообщение с кодом уже у вас в смс"));
+                    notifications.put(type, handler);
+                }
+                case PUSH -> {
+                    handler.accept(new Notification(type, "получено новое сообщение"));
+                    notifications.put(type, handler);
+                }
+                default -> throw new IllegalStateException("Unexpected value: " + type);
             }
-            case SMS -> {
-                handler.accept(new Notification(type, "сообщение с кодом уже у вас в смс"));
-                notifications.put(type, handler);
-            }
-            case PUSH -> {
-                handler.accept(new Notification(type, "получено новое сообщение"));
-                notifications.put(type, handler);
-            }
-            default -> throw new IllegalStateException("Unexpected value: " + type);
+        } catch (NullPointerException exception) {
+            log.error("Попытка передать null в параметры метода registerHandler: ", exception);
         }
     }
 
     public void sendNotification(Notification notification) {
-        registerHandler(notification.getType(), handler -> System.out.printf("%s\n", notification.getMessage()));
+        try {
+            registerHandler(notification.getType(), handler -> System.out.printf("%s\n", notification.getMessage()));
+        } catch (NullPointerException exception) {
+            log.error("Попытка передать null в параметры метода sendNotification: ", exception);
+        }
+    }
+
+    public void addSignature(Notification notification, Function<Notification, Notification> function) {
+        try {
+            function.apply(notification);
+        } catch (NullPointerException exception) {
+            log.error("Попытка передать null в параметры метода sendNotification: ", exception);
+        }
     }
 }
