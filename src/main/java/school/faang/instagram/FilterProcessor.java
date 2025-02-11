ff9a8cff6307479daf8bc388bd5586820aaec177
Sergey <bigwave2915@gmail.com>
2025-02-11 17:36:34 +0500

    решения задачи

--- src/main/java/school/faang/instagram/FilterProcessor.java
+++ src/main/java/school/faang/instagram/FilterProcessor.java
@@ -1,4 +1,31 @@
 package school.faang.instagram;
 
+import java.util.function.Function;
+
 public class FilterProcessor {
+
+    public Image applyFilter(Image image, Function<Image, Image> filter) {
+        checkImage(image);
+        checkFilter(filter);
+        return filter.apply(image);
+    }
+
+    public Function<Image, Image> combineFilters(Function<Image,
+            Image> filterA, Function<Image, Image> filterB) {
+        checkFilter(filterA);
+        checkFilter(filterB);
+        return filterA.andThen(filterB);
+    }
+
+    private void checkImage(Image image) {
+        if (image == null) {
+            throw new IllegalArgumentException("Не может быть пустым");
+        }
+    }
+
+    private void checkFilter(Function<Image, Image> filter) {
+        if (filter == null) {
+            throw new IllegalArgumentException("Фильтр не может быть пустым");
+        }
+    }
 }
