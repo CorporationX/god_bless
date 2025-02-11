ecb5e7ded609f486e5bfbde642910bb58c92a324
Winrida <winridas@gmail.com>
2025-02-11 15:21:59 +0300

    BJS2-57219 Библиотека Вестеросса Исправлены замечания
    https://faang-school.atlassian.net/browse/BJS2-57219

--- src/main/java/school/faang/task57219/Main.java
+++ src/main/java/school/faang/task57219/Main.java
@@ -4,12 +4,16 @@ import school.faang.task57219.library.LibrarySystem;
 
 public class Main {
     public static void main(String[] args) {
-        LibrarySystem.addBook("Вокруг света в 80 дней", "Жюль Верн", 1872, "Полка №1");
-        LibrarySystem.addBook("В зоне тумана", "Алексей Гравицкий", 2009, "Полка №2");
-        LibrarySystem.addBook("Грокаем алгоритмы", "Адитья Бхаргава", 2022, "Полка №3");
-        LibrarySystem.findBook("В зоне тумана", "Алексей Гравицкий", 2009);
-        LibrarySystem.findBook("Тренируй свой мозг", "Рюта Кавашима", 2022);
-        LibrarySystem.removeBook("В зоне тумана", "Алексей Гравицкий", 2009);
-        LibrarySystem.printAllBooks();
+        LibrarySystem librarySystem = new LibrarySystem();
+        System.out.println(librarySystem.addBook("Вокруг света в 80 дней", "Жюль Верн", 1872, "Полка №1"));
+        System.out.println(librarySystem.addBook("В зоне тумана", "Алексей Гравицкий", 2009, "Полка №2"));
+        System.out.println(librarySystem.addBook(null, "", 2009, "Полка №2"));
+        System.out.println(librarySystem.addBook("Грокаем алгоритмы", "Адитья Бхаргава", 2022, "Полка №3"));
+        System.out.println(librarySystem.addBook("Грокаем алгоритмы", "Адитья Бхаргава", 2022, "Полка №3"));
+        System.out.println(librarySystem.findBook("В зоне тумана", "Алексей Гравицкий", 2009));
+        System.out.println(librarySystem.findBook("Тренируй свой мозг", "Рюта Кавашима", 2022));
+        System.out.println(librarySystem.findBook("", null, 2020));
+        System.out.println(librarySystem.removeBook("В зоне тумана", "Алексей Гравицкий", 2009));
+        librarySystem.printAllBooks();
     }
 }
--- src/main/java/school/faang/task57219/library/Book.java
+++ src/main/java/school/faang/task57219/library/Book.java
@@ -2,10 +2,12 @@ package school.faang.task57219.library;
 
 import lombok.AllArgsConstructor;
 import lombok.EqualsAndHashCode;
+import lombok.Getter;
 import lombok.ToString;
 
 @EqualsAndHashCode
 @AllArgsConstructor
+@Getter
 @ToString
 public class Book {
     private String title;
