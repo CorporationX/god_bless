d00a526caa2cefbf124d063b353d1bcbe57ccdc2
Tony King <a.korolev9927@gmail.com>
2025-02-11 22:07:42 +0300

    Внес правки: - сделал более информативние обработку исключений

--- src/main/java/school/faang/task_57716/Notification.java
+++ src/main/java/school/faang/task_57716/Notification.java
@@ -1,14 +1,11 @@
 package school.faang.task_57716;
 
-import lombok.Data;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
 
-@Data
+@Getter
+@AllArgsConstructor
 public class Notification {
     private NotificationType type;
     private String message;
-
-    public Notification(NotificationType type, String message) {
-        this.type = type;
-        this.message = message;
-    }
 }
--- src/main/java/school/faang/task_57716/NotificationManager.java
+++ src/main/java/school/faang/task_57716/NotificationManager.java
@@ -1,5 +1,6 @@
 package school.faang.task_57716;
 
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.function.Consumer;
@@ -8,9 +9,8 @@ public class NotificationManager {
     private final Map<NotificationType, Consumer<Notification>> notificationConsumers = new HashMap<>();
 
     public void registerHandler(NotificationType type, Consumer<Notification> handler) {
-        if (type == null || handler == null) {
-            throw new IllegalArgumentException("Тип не может быть NULL");
-        }
+        checkType(type);
+        checkHandler(handler);
         notificationConsumers.put(type, handler);
     }
 
@@ -19,8 +19,21 @@ public class NotificationManager {
             throw new IllegalArgumentException("Уведомление не может быть NULL");
         }
         if (!notificationConsumers.containsKey(notification.getType())) {
-            throw new IllegalArgumentException("Данный тип не создан: " + notification);
+            throw new IllegalArgumentException("Данный тип не создан: " + notification +
+                    " Доступны только: " + Arrays.stream(NotificationType.values()).toList());
         }
         notificationConsumers.get(notification.getType()).accept(notification);
+    }
+
+    private void checkType(NotificationType type) {
+        if (type == null) {
+            throw new IllegalArgumentException("Поле type не может быть NULL");
+        }
+    }
+
+    private void checkHandler(Consumer<Notification> handler) {
+        if (handler == null) {
+            throw new IllegalArgumentException("Поле handler не может быть NULL");
+        }
     }
 }
