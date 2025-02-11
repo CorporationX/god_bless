3b8e1f6e28c8bbae9fa8652e7796b8f0f4c404e0
Ilhan Mustafa <ilhnmst@gmail.com>
2025-02-11 09:02:19 +0300

    SpellCaster cast() add validate parameters make spells static final

--- src/main/java/school/faang/sprint2/task_BJS2_57502/Main.java
+++ src/main/java/school/faang/sprint2/task_BJS2_57502/Main.java
@@ -1,15 +1,14 @@
 package school.faang.sprint2.task_BJS2_57502;
 
 public class Main {
+    private static final SpellCaster SPELL_CASTER = new SpellCaster();
+    private static final String ALOHOMORA = "Alohomora";
+    private static final String LUMOS = "Lumos";
+    private static final String EXPELLIARMUS = "Expelliarmus";
+
     public static void main(String[] args) {
-        SpellCaster spellCaster = new SpellCaster();
-
-        String alohomora = "Alohomora";
-        String lumos = "Lumos";
-        String expelliarmus = "Expelliarmus";
-
-        spellCaster.cast(alohomora, (spell) -> "The door is unlocked by " + spell);
-        spellCaster.cast(lumos, (spell) -> "A beam of light is created by " + spell);
-        spellCaster.cast(expelliarmus, (spell) -> "The opponent is disarmed by " + spell);
+        SPELL_CASTER.cast(ALOHOMORA, (spell) -> "The door is unlocked by " + spell);
+        SPELL_CASTER.cast(LUMOS, (spell) -> "A beam of light is created by " + spell);
+        SPELL_CASTER.cast(EXPELLIARMUS, (spell) -> "The opponent is disarmed by " + spell);
     }
 }
--- src/main/java/school/faang/sprint2/task_BJS2_57502/SpellCaster.java
+++ src/main/java/school/faang/sprint2/task_BJS2_57502/SpellCaster.java
@@ -2,6 +2,21 @@ package school.faang.sprint2.task_BJS2_57502;
 
 public class SpellCaster {
     public void cast(String spell, SpellAction action) {
+        checkSpell(spell);
+        checkSpellAction(action);
+
         System.out.println(action.act(spell));
+    }
+
+    private void checkSpell(String spell) {
+        if (spell == null || spell.isBlank()) {
+            throw new IllegalArgumentException("Заклинание не может быть пустым");
+        }
+    }
+
+    private void checkSpellAction(SpellAction action) {
+        if (action == null) {
+            throw new IllegalArgumentException("Действие не может быть пустым");
+        }
     }
 }
