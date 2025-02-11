80ac09700a04fb8356e0cf5cff87bfca2d642595
Vladimir Mamaev <ipmamaev.v@gmail.com>
2025-02-11 10:55:57 +0300

    Добавлены модификаторы к методам, Optional заменены применением методов Map

--- src/main/java/bjs257119/HogwartsSpells.java
+++ src/main/java/bjs257119/HogwartsSpells.java
@@ -1,51 +1,61 @@
 package bjs257119;
 
 import lombok.Getter;
+import lombok.extern.slf4j.Slf4j;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.Optional;
 
 @Getter
+@Slf4j
 public class HogwartsSpells {
     private final Map<Integer, SpellEvent> spellById = new HashMap<>();
     private final Map<String, List<SpellEvent>> spellsByType = new HashMap<>();
     private static int globalId = 0;
+    private final Logger logger = LoggerFactory.getLogger(HogwartsSpells.class);
 
-    void addSpellEvent(String eventType, String actionDescription) {
+    public void addSpellEvent(String eventType, String actionDescription) {
         SpellEvent spellEvent = new SpellEvent(++globalId, eventType, actionDescription);
-        spellById.put(spellEvent.getId(), spellEvent);
+        spellById.putIfAbsent(spellEvent.getId(), spellEvent);
 
-        Optional.ofNullable(spellsByType.get(eventType))
-                .ifPresentOrElse(events -> events.add(spellEvent), () -> {
-                    List<SpellEvent> spellEvents = new ArrayList<>();
-                    spellsByType.put(eventType, spellEvents);
+        spellsByType.computeIfPresent(eventType,
+                (eventTypeInMap, spellEventsInMap) -> {
+                    List<SpellEvent> spellEvents = spellsByType.get(eventTypeInMap);
+                    spellEvents.add(spellEvent);
+                    return spellEvents;
+                });
+        spellsByType.computeIfAbsent(eventType,
+                (eventTypeInMap) -> {
+                    ArrayList<SpellEvent> spellEvents = new ArrayList<>();
+                    spellEvents.add(spellEvent);
+                    return spellEvents;
                 });
     }
 
-    SpellEvent getSpellEventById(int id) {
-        return Optional.ofNullable(spellById.get(id))
-                .orElseThrow(() -> new IllegalArgumentException("Wrong id"));
+    public SpellEvent getSpellEventById(int id) {
+        return spellById.getOrDefault(id, new SpellEvent(0, null, null));
     }
 
-    List<SpellEvent> getSpellEventsByType(String eventType) {
-        return Optional.ofNullable(spellsByType.get(eventType))
-                .orElseThrow(() -> new IllegalArgumentException("Wrong type"));
+    public List<SpellEvent> getSpellEventsByType(String eventType) {
+        return spellsByType.getOrDefault(eventType, new ArrayList<>());
     }
 
-    void deleteSpellEvent(int id) {
-        SpellEvent spellEvent = Optional.ofNullable(spellById.get(id))
-                .orElseThrow(() -> new IllegalArgumentException("Wrong id"));
-        List<SpellEvent> spellEvents = Optional.ofNullable(spellsByType.get(spellEvent.getEventType()))
-                .orElseThrow(() -> new IllegalArgumentException("Wrong type"));
-        spellEvents.remove(spellEvent);
-        spellById.remove(id);
+    public void deleteSpellEvent(int id) {
+        SpellEvent removed = spellById.remove(id);
+        if (removed != null) {
+            List<SpellEvent> events = spellsByType.get(removed.getEventType());
+            if (events != null) {
+                events.remove(removed);
+            }
+        }
     }
 
-    void printAllSpellEvents() {
-        spellById.forEach((key, value) -> System.out.printf("ID: %d, Event type: %s, Action: %s %n",
+    public void printAllSpellEvents() {
+        spellById.forEach((key, value) -> logger.info("ID: {}, Event type: {}, Action: {}",
                 value.getId(), value.getEventType(), value.getAction()));
     }
 }
