665e282e458adb544b998cf0683b982a1be57e53
saroyangor <dr.saroyan98@gmail.com>
2025-02-12 01:53:23 +0400

    Секреты дроидов

--- src/main/java/BJS2_57383/SpellCaster.java
+++ src/main/java/BJS2_57383/SpellCaster.java
@@ -1,7 +1,9 @@
 package BJS2_57383;
 
+import lombok.NonNull;
+
 public class SpellCaster {
-    public void cast(String spell, SpellAction action) {
+    public void cast(@NonNull String spell, @NonNull SpellAction action) {
         System.out.println(action.cast(spell));
     }
 }
