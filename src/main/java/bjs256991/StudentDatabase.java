56b3da4ac4ba3212597b1cf87f5e6ae2a8a82289
Vladimir Mamaev <ipmamaev.v@gmail.com>
2025-02-11 13:57:43 +0300

    Сделал изменения, но в связи с ними слетели тесты. Можете проверить правильность нового кода без учёта тестов? Ещё такой вопрос, можно ли сохранить работоспособность тестов при изменении кода, если при этом в тестах не используется мокирование, объекты @Mock и @Spy ?

--- src/main/java/bjs256991/StudentDatabase.java
+++ src/main/java/bjs256991/StudentDatabase.java
@@ -1,50 +1,34 @@
 package bjs256991;
 
-import lombok.Getter;
-import lombok.ToString;
 import lombok.extern.slf4j.Slf4j;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
 import java.util.Objects;
+import java.util.Set;
 
 @Slf4j
-@Getter
-@ToString
 public class StudentDatabase {
     private final Map<Student, Map<Subject, Integer>> studentSubjects = new HashMap<>();
     private final Map<Subject, List<Student>> subjectStudents = new HashMap<>();
     private static final Logger logger = LoggerFactory.getLogger(StudentDatabase.class);
 
     public void addStudentSubjectsEstimates(Student student, Subject subject, Integer estimate) {
-        Map<Subject, Integer> subjectsToEstimates = studentSubjects.get(student);
         if (Objects.nonNull(student) && Objects.nonNull(subject) && Objects.nonNull(estimate)) {
-            if (subjectsToEstimates == null) {
-                subjectsToEstimates = new HashMap<>();
-            }
-            subjectsToEstimates.put(subject, estimate);
-            studentSubjects.put(student, subjectsToEstimates);
+            Map<Subject, Integer> subjectsToEstimates = new HashMap<>();
+            subjectsToEstimates.putIfAbsent(subject, estimate);
+            studentSubjects.putIfAbsent(student, subjectsToEstimates);
         }
-        List<Student> students = subjectStudents.get(subject);
-        if (students == null) {
-            students = new ArrayList<>();
-        }
-        students.add(student);
-        subjectStudents.put(subject, students);
     }
 
     public void addSubjectForStudentWithEstimate(Subject subject, Student student, Integer estimate) {
-        Map<Subject, Integer> subjectsToEstimates = studentSubjects.get(student);
         if (Objects.nonNull(subject) && Objects.nonNull(student) && Objects.nonNull(estimate)) {
-            if (subjectsToEstimates == null) {
-                subjectsToEstimates = new HashMap<>();
-            }
-            subjectsToEstimates.put(subject, estimate);
+            Map<Subject, Integer> subjectsToEstimates = studentSubjects.get(student);
+            subjectsToEstimates.putIfAbsent(subject, estimate);
             studentSubjects.put(student, subjectsToEstimates);
 
             List<Student> students = subjectStudents.get(subject);
@@ -52,36 +36,32 @@ public class StudentDatabase {
                 students = new ArrayList<>();
             }
             students.add(student);
-            subjectStudents.put(subject, students);
+            subjectStudents.putIfAbsent(subject, students);
         }
     }
 
     public void removeStudentSubjects(Student student) {
-        Map<Subject, Integer> subjectsToEstimates = studentSubjects.get(student);
-        if (Objects.nonNull(student)) {
-            if (subjectsToEstimates != null) {
-                studentSubjects.remove(student);
-            }
-        }
+        studentSubjects.remove(student);
     }
 
     public void printAllStudents() {
         studentSubjects.forEach((student, subjectsToEstimates) -> {
             logger.info("Student: {}", student);
-            subjectsToEstimates.forEach((subject, estimate) -> {
-                logger.info("Subject: {}, Estimate: {}", subject, estimate);
-            });
+            subjectsToEstimates.forEach((subject, estimate) ->
+                logger.info("Subject: {}, Estimate: {}", subject, estimate)
+            );
         });
     }
 
-    public void addSubjectStudents(Subject subject, List<Student> students) {
+    public void addSubjectStudents(Subject subject, Set<Student> students) {
         if (Objects.nonNull(subject) && Objects.nonNull(students)) {
-            Map<Subject, Integer> subjectsToEstimates = new HashMap<>();
-            students.forEach(student -> {
-                subjectsToEstimates.put(subject, null);
-                studentSubjects.put(student, subjectsToEstimates);
+            Map<Subject, Integer> subjectToEstimate = new HashMap<>();
+            List<Student> studentsWithoutDuplicates = new ArrayList<>(students);
+            studentsWithoutDuplicates.forEach(studentWithoutDuplicate -> {
+                subjectToEstimate.put(subject, null);
+                studentSubjects.put(studentWithoutDuplicate, subjectToEstimate);
             });
-            subjectStudents.put(subject, students);
+            subjectStudents.put(subject, studentsWithoutDuplicates);
         }
     }
 
@@ -103,31 +83,16 @@ public class StudentDatabase {
     }
 
     public void removeStudentFromSubject(Student student, Subject subject) {
-        if (Objects.nonNull(student) && Objects.nonNull(subject)) {
-            Map<Subject, Integer> subjectsToEstimates = studentSubjects.get(student);
-            if (subjectsToEstimates != null) {
-                Iterator<Map.Entry<Subject, Integer>> subjectsToEstimatesIterator = subjectsToEstimates
-                        .entrySet().iterator();
-                if (subjectsToEstimatesIterator.hasNext()) {
-                    Map.Entry<Subject, Integer> subjectToEstimate = subjectsToEstimatesIterator.next();
-                    if (subjectToEstimate.getKey().equals(subject)) {
-                        subjectsToEstimatesIterator.remove();
-                    }
-                }
-            }
-            studentSubjects.put(student, subjectsToEstimates);
-            if (subjectStudents.get(subject) != null) {
-                subjectStudents.get(subject).remove(student);
-            }
-        }
+        List<Subject> subjects = studentSubjects.get(student).keySet().stream().toList();
+        subjects.stream().filter(streamSubject -> subjectStudents.get(streamSubject) != null)
+                .forEach(streamSubject -> subjectStudents.get(streamSubject).remove(student));
     }
 
     protected void printAllSubjects() {
         subjectStudents.forEach((subject, students) -> {
             logger.info("Subject: {}", subject);
-            students.forEach(student -> {
-                logger.info("The student who studies the subject: {}", student);
-            });
+            students.forEach(student ->
+                logger.info("The student who studies the subject: {}", student));
         });
     }
 }
