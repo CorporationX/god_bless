fdd9108771edae3215ab2e694b46b3148c4e8cfc
saroyangor <dr.saroyan98@gmail.com>
2025-02-11 21:56:57 +0400

    Хогвартс, Хогвартс, наш любимый Хогвартс

---
+++ src/main/java/BJS2_57383/SpellAction.java
@@ -0,0 +1,6 @@
+package BJS2_57383;
+
+@FunctionalInterface
+public interface SpellAction {
+    String cast(String spell);
+}
---
+++ src/main/java/BJS2_57383/SpellCaster.java
@@ -0,0 +1,7 @@
+package BJS2_57383;
+
+public class SpellCaster {
+    public void cast(String spell, SpellAction action) {
+        System.out.println(action.cast(spell));
+    }
+}
