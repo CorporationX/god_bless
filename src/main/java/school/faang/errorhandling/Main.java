6ea3584c9ff9a873d369c2ea4f4e06bacc06c6c9
Vadim Naletov <naletov96@gmail.com>
2025-02-11 09:03:12 +0300

    comments removed

--- src/main/java/school/faang/errorhandling/Main.java
+++ src/main/java/school/faang/errorhandling/Main.java
@@ -4,8 +4,8 @@ public class Main {
 
     public static void main(String[] args) {
         String result = ErrorHandler.withErrorHandling(
-                () -> RemoteService.call("someParam"),  // Основное действие
-                e -> {  // Обработка ошибки
+                () -> RemoteService.call("someParam"),
+                e -> {
                     System.out.println("Ошибка при вызове сервиса, возвращаем дефолтное значение");
                     return "DEFAULT";
                 }
--- src/main/java/school/faang/errorhandling/RemoteService.java
+++ src/main/java/school/faang/errorhandling/RemoteService.java
@@ -3,7 +3,6 @@ package school.faang.errorhandling;
 public class RemoteService {
 
     public static String call(String param) {
-        // Здесь возникнет ошибка для демонстрации
         throw new RuntimeException("Сервис недоступен");
     }
 }
