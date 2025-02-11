e7d17cced449624c62b7b61e54b9ed0c8e0dcc24
Max Goloviznin <roxet59rus@mail.ru>
2025-02-11 05:16:01 +0500

    BJS2-57641 Фильтры Instagram

--- src/main/java/school/faang/filtersofinstagram/FilterProcessor.java
+++ src/main/java/school/faang/filtersofinstagram/FilterProcessor.java
@@ -4,11 +4,19 @@ import java.util.function.Function;
 
 public class FilterProcessor {
     public Image applyFilter(Image image, Function<Image, Image> filter) {
+        if (image == null) {
+            throw new IllegalArgumentException("Image is incorrect");
+        } else if (filter == null) {
+            throw new IllegalArgumentException("Filter is incorrect");
+        }
         return filter.apply(image);
     }
 
     public Function<Image, Image> combineFilters(Function<Image, Image> firstFilter,
                                                  Function<Image, Image> secondFilter) {
+        if (firstFilter == null || secondFilter == null) {
+            throw new IllegalArgumentException("One of the filters is incorrect");
+        }
         return firstFilter.andThen(secondFilter);
     }
 }
