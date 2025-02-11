dd612e6cff3675cca9c4bc464d6fc377fcd10f54
vvz3zzv <vladislav.v.zhurbin@gmail.com>
2025-02-11 12:43:30 +0300

    BJS2-59635: Хогвартс, Хогвартс, наш любимый Хогвартс

--- src/main/java/school/faang/task_59635/Main.java
+++ src/main/java/school/faang/task_59635/Main.java
@@ -1,15 +1,29 @@
 package school.faang.task_59635;
 
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
 public class Main {
+    public static final String ALOHOMORA = "Alohomora";
+    public static final String LUMOS = "Lumos";
+    public static final String EXPELLIARMUS = "Expelliarmus";
+
     public static void main(String[] args) {
         SpellCaster spellCaster = new SpellCaster();
 
-        String alohomora = "Alohomora";
-        String lumos = "Lumos";
-        String expelliarmus = "Expelliarmus";
+        String[] spells = {ALOHOMORA, LUMOS, EXPELLIARMUS};
+        for (String spell : spells) {
+            try {
+                spellCaster.cast(spell, (spellName) -> "The opponent is disarmed by " + spellName);
+            } catch (IllegalArgumentException e) {
+                log.info("Error casting spell '{}': {}", spell, e.getMessage());
+            }
+        }
 
-        spellCaster.cast(alohomora, (spell) -> "The door is unlocked by " + spell);
-        spellCaster.cast(lumos, (spell) -> "A beam of light is created by " + spell);
-        spellCaster.cast(expelliarmus, (spell) -> "The opponent is disarmed by " + spell);
+        try {
+            spellCaster.cast(null, (spell) -> "The opponent is disarmed by " + spell);
+        } catch (IllegalArgumentException e) {
+            log.info("Error using null value: {}", e.getMessage());
+        }
     }
 }
--- src/main/java/school/faang/task_59635/SpellCaster.java
+++ src/main/java/school/faang/task_59635/SpellCaster.java
@@ -1,12 +1,24 @@
 package school.faang.task_59635;
 
 public class SpellCaster implements SpellAction {
+    private static final String SPELL_NAME_NULL_ERROR = "spellName must not be null";
+    private static final String SPELL_ACTION_NULL_ERROR = "spellAction must not be null";
+
     public void cast(String spellName, SpellAction spellAction) {
+        if (spellName == null) {
+            throw new IllegalArgumentException(SPELL_NAME_NULL_ERROR);
+        }
+        if (spellAction == null) {
+            throw new IllegalArgumentException(SPELL_ACTION_NULL_ERROR);
+        }
         System.out.printf("Spell result: %s\n", spellAction.castSpell(spellName));
     }
 
     @Override
     public String castSpell(String spellName) {
+        if (spellName == null) {
+            throw new IllegalArgumentException(SPELL_NAME_NULL_ERROR);
+        }
         return spellName;
     }
 }
