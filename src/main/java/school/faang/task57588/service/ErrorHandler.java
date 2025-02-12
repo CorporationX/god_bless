c10224d91ef339f315bfcb3b7869436e1f12a380
Dmitry Rogulkin <rogulkind@yandex.ru>
2025-02-12 22:43:33 +0500

    https://faang-school.atlassian.net/browse/BJS2-57588

---
+++ src/main/java/school/faang/task57588/service/ErrorHandler.java
@@ -0,0 +1,13 @@
+package school.faang.task57588.service;
+
+import java.util.function.Supplier;
+
+public class ErrorHandler {
+    public static <T> T withErrorHandling(Supplier<T> action, ExceptionHandler<T> errorHandler) {
+        try {
+            return action.get();
+        } catch (Exception e) {
+            return errorHandler.handle(e);
+        }
+    }
+}
