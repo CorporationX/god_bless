fa417092c0fd62def258f1f526713c4e6b816529
Artem Doronin <doronintema2018@yandex.ru>
2025-02-13 00:29:42 +0500

    fixed bugs

--- src/main/java/school/faang/task_56910/FrequentUpdateWeatherCache.java
+++ src/main/java/school/faang/task_56910/FrequentUpdateWeatherCache.java
@@ -14,6 +14,6 @@ public class FrequentUpdateWeatherCache extends WeatherCacheTemplate {
 
     @Override
     public boolean isCacheExpired(WeatherData data, long maxCacheAgeMillis) {
-        return System.currentTimeMillis() - data.getTimestamp() > maxCacheAgeMillis ;
+        return System.currentTimeMillis() - data.getTimestamp() > maxCacheAgeMillis;
     }
 }
