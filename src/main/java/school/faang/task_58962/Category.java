c2c63cd9d5d7811c7fc3c80f1f51e97d6df98fe3
Артемий <xkmitx@gmail.com>
2025-02-13 00:27:38 +0300

    Task 58962

---
+++ src/main/java/school/faang/task_58962/Category.java
@@ -0,0 +1,5 @@
+package school.faang.task_58962;
+
+public enum Category {
+    FOOD, ELECTRONICS, CLOTHING, OTHER
+}
---
+++ src/main/java/school/faang/task_58962/Product.java
@@ -0,0 +1,28 @@
+package school.faang.task_58962;
+
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NonNull;
+import lombok.ToString;
+
+@Getter
+@EqualsAndHashCode
+@ToString
+public class Product {
+    private final int id;
+    private final String name;
+    private final Category category;
+
+    private void checkNullName(@NonNull String name) {
+        if (name.isBlank()) {
+            throw new IllegalArgumentException("Name cant be blank.");
+        }
+    }
+
+    public Product(int id, String name, Category category) {
+        checkNullName(name);
+        this.id = id;
+        this.name = name;
+        this.category = category;
+    }
+}
