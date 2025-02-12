efcc28ad6c4d4a125fe9a72dc08048d7f6257971
elsledkov <elsledkov@gmail.com>
2025-02-13 00:40:06 +0300

    Refactor

--- src/main/java/school/faang/spring1/basicjava/collectingusers/User.java
+++ src/main/java/school/faang/spring1/basicjava/collectingusers/User.java
@@ -2,22 +2,21 @@ package school.faang.spring1.basicjava.collectingusers;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
-import lombok.NoArgsConstructor;
 
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
+
 @Data
 @AllArgsConstructor
-@NoArgsConstructor
 public class User {
-    private int id;
-    private String name;
-    private int age;
-    private Set<String> userHobbies;
+    private final int id;
+    private final String name;
+    private final int age;
+    private final Set<String> userHobbies;
 
-    public Map<User, String> findHobbyLovers(List<User> users, Set<String> hobbies) {
+    public static Map<User, String> findHobbyLovers(List<User> users, Set<String> hobbies) {
         Map<User, String> map = new HashMap<>();
         for (User user : users) {
             for (String hobby : user.getUserHobbies()) {
