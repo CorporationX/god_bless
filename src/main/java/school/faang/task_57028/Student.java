8d5d70c393b6e8e3622671b74ab07f8aef2fb908
Artem Doronin <doronintema2018@yandex.ru>
2025-02-12 23:47:21 +0500

    fixed bugs

--- src/main/java/school/faang/task_57028/Student.java
+++ src/main/java/school/faang/task_57028/Student.java
@@ -1,9 +1,5 @@
 package school.faang.task_57028;
 
 public record Student(String name) {
-    @Override
-    public String name() {
-        return name;
-    }
 }
 
--- src/main/java/school/faang/task_57028/Subject.java
+++ src/main/java/school/faang/task_57028/Subject.java
@@ -1,8 +1,5 @@
 package school.faang.task_57028;
 
 public record Subject( String name) {
-    @Override
-    public String name() {
-        return name;
-    }
+
 }
