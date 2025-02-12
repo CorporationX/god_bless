07e68f1fc53f4ea3b98ceb0e5072da7a1da08220
Ilhan Mustafa <ilhnmst@gmail.com>
2025-02-12 10:34:27 +0300

    Notification:  add check params

--- src/main/java/school/faang/sprint2/task_BJS2_57772/Notification.java
+++ src/main/java/school/faang/sprint2/task_BJS2_57772/Notification.java
@@ -1,11 +1,24 @@
 package school.faang.sprint2.task_BJS2_57772;
 
-import lombok.AllArgsConstructor;
 import lombok.Getter;
 
-@AllArgsConstructor
 @Getter
 public class Notification {
     private NotificationType type;
     private String message;
+
+    public Notification(NotificationType type, String message) {
+        checkParam(type, message);
+        this.type = type;
+        this.message = message;
+    }
+
+    private void checkParam(NotificationType type, String message) {
+        if (type == null) {
+            throw new IllegalArgumentException("Тип не может быть null");
+        }
+        if (message == null || message.isBlank()) {
+            throw new IllegalArgumentException("Сообщение не может быть пустым или null");
+        }
+    }
 }
