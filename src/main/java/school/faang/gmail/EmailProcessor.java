74a82894d216921a5f7aa6ed01b6e17c0b64b9a0
Vadim Naletov <naletov96@gmail.com>
2025-02-12 14:44:04 +0300

    code cleaned up

--- src/main/java/school/faang/gmail/EmailProcessor.java
+++ src/main/java/school/faang/gmail/EmailProcessor.java
@@ -1,6 +1,7 @@
 package school.faang.gmail;
 
 import java.util.List;
+import java.util.Objects;
 import java.util.function.Consumer;
 import java.util.function.Function;
 import java.util.function.Predicate;
@@ -8,12 +9,16 @@ import java.util.function.Predicate;
 public class EmailProcessor {
 
     public void processEmails(List<Email> incomingMessages, Predicate<Email> filter,
-                             Function<Email, String> converter, Consumer<Email> handler) {
+                             Function<Email, String> transformer, Consumer<Email> action) {
+        Objects.requireNonNull(incomingMessages);
+        Objects.requireNonNull(filter);
+        Objects.requireNonNull(transformer);
+        Objects.requireNonNull(action);
         for (Email email : incomingMessages) {
             if (filter.test(email)) {
-                String resultEmailBody = converter.apply(email);
+                String resultEmailBody = transformer.apply(email);
                 email.setBody(resultEmailBody);
-                handler.accept(email);
+                action.accept(email);
             }
         }
     }
--- src/main/java/school/faang/gmail/Main.java
+++ src/main/java/school/faang/gmail/Main.java
@@ -17,12 +17,12 @@ public class Main {
                 new Email("Спам", "Текст спама", false)
         );
 
-        Predicate<Email> importantFilter = email -> email.isImportant();
+        Predicate<Email> importantFilter = Email::isImportant;
         Consumer<Email> printEmail = email -> System.out.println("Обработано письмо: " + email.getSubject());
         Function<Email, String> toUpperCase = email -> email.getBody().toUpperCase();
         emailProcessor.processEmails(emails, importantFilter, toUpperCase, printEmail);
 
         emails.forEach(email ->
-                System.out.println("Тема: " + email.getSubject() + ", Тело письма: " + email.getBody()));
+                System.out.printf("Тема: %s, Тело письма: %s%n", email.getSubject(), email.getBody()));
     }
 }
