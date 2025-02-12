f545dd4bfce8c89de1004bf4f260506591b6219e
Ekaterina Ramakoti <zubkova-92@mail.ru>
2025-02-12 21:05:10 +0300

    checkstyle BJS2-57340

--- src/main/java/school/faang/BJS2_57340/Main.java
+++ src/main/java/school/faang/BJS2_57340/Main.java
@@ -6,11 +6,11 @@ import java.util.Map;
 
 public class Main {
     public static void main(String[] args) {
-        SpellCaster spellCaster = new SpellCaster();
         Map<String, String> spelNames = new HashMap<>();
         spelNames.put("Alohomora", "The door is unlocked by ");
         spelNames.put("Lumos", "A beam of light is created by ");
         spelNames.put("Expelliarmus", "The opponent is disarmed by ");
+        SpellCaster spellCaster = new SpellCaster();
         for (Map.Entry<String, String> entry : spelNames.entrySet()) {
             spellCaster.cast(entry.getKey(), (spell) -> entry.getValue() + spell);
         }
