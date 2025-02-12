155da7019393772c943ed57ad9dc6c41e2293b09
elsledkov <elsledkov@gmail.com>
2025-02-12 23:19:51 +0300

    Refactor: Add null-check for method sendNotification()

--- src/main/java/school/faang/sprint2/lambda/metauniversal/NotificationManager.java
+++ src/main/java/school/faang/sprint2/lambda/metauniversal/NotificationManager.java
@@ -1,12 +1,18 @@
 package school.faang.sprint2.lambda.metauniversal;
 
+import lombok.extern.slf4j.Slf4j;
+
 import java.util.HashMap;
 import java.util.Map;
 import java.util.function.Consumer;
 
-
+@Slf4j
 public class NotificationManager {
-    private Map<NotificationType, Consumer<Notification>> notificationMap = new HashMap<>();
+    private final Map<NotificationType, Consumer<Notification>> notificationMap;
+
+    public NotificationManager() {
+        notificationMap  = new HashMap<>();
+    }
 
     public void registerHandler(NotificationType notificationType, Consumer<Notification> handler) {
         notificationMap.put(notificationType, handler);
@@ -14,6 +20,11 @@ public class NotificationManager {
 
     public void sendNotification(Notification notification) {
         NotificationType notificationType = notification.getNotificationType();
-        notificationMap.get(notificationType).accept(notification);
+        Consumer<Notification> handler = notificationMap.get(notificationType);
+        if (handler != null) {
+            notificationMap.get(notificationType).accept(notification);
+        } else {
+            log.warn("Для уведомления {} не найден обработчик", notificationType);
+        }
     }
 }
