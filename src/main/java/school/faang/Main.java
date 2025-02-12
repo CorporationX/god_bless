c5b8a8b367ab39a0e59ad478cfce8deedee800ed
Ivan Prygunov <88151612+juzu400@users.noreply.github.com>
2025-02-12 20:46:01 +0300

    Внес исправления

--- src/main/java/school/faang/Main.java
+++ src/main/java/school/faang/Main.java
@@ -3,6 +3,11 @@ package school.faang;
 public class Main {
     public static void main(String[] args) {
         User ivan = new User("Ivan", 22, "Uber", "London");
-        System.out.printf("Вывод данных пользователя: %s \n", ivan.toString());
+        try {
+            User kid = new User("Matthew", 16, "Google", "London");
+        } catch (IllegalArgumentException e) {
+            System.out.println(e.getMessage());
+        }
+        System.out.printf("Вывод данных пользователя: %s \n", ivan);
     }
 }
--- src/main/java/school/faang/User.java
+++ src/main/java/school/faang/User.java
@@ -1,28 +1,37 @@
 package school.faang;
 
 import java.util.Set;
+
+import lombok.Getter;
+import lombok.NonNull;
 import lombok.ToString;
 
 @ToString
+@Getter
+@NonNull
 public class User {
 
+    private static final int MIN_AGE = 18;
     private static final Set<String> VALID_JOBS = Set.of("Google", "Uber", "Amazon");
     private static final Set<String> VALID_ADDRESSES = Set.of("London", "New York", "Amsterdam");
 
-    private String name;
-    private int age;
-    private String job;
-    private String address;
+    private final String name;
+    private final int age;
+    private final String job;
+    private final String address;
 
     public User(String name, int age, String job, String address) {
-        if (name.isEmpty()) {
-            throw new IllegalArgumentException("Name cannot be empty");
-        } else if (age < 18) {
-            throw new IllegalArgumentException("Age cannot be less than 18");
-        } else if (!VALID_JOBS.contains(job)) {
-            throw new IllegalArgumentException("Invalid job");
-        } else if (!VALID_ADDRESSES.contains(address)) {
-            throw new IllegalArgumentException("Invalid address");
+        if (name.isBlank()) {
+            throw new IllegalArgumentException("Name cannot be empty: " + name);
+        }
+        if (age < MIN_AGE) {
+            throw new IllegalArgumentException("Age cannot be less than 18: " + age);
+        }
+        if (!VALID_JOBS.contains(job)) {
+            throw new IllegalArgumentException("Invalid job: " + job);
+        }
+        if (!VALID_ADDRESSES.contains(address)) {
+            throw new IllegalArgumentException("Invalid address: " + address);
         }
         this.name = name;
         this.age = age;
