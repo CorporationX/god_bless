fe10283ba9d78e1294295b7e8c2e4ea8baa7e9f4
Marsel <mars1212@inbox.ru>
2025-02-12 17:15:11 +0300

    Hogwarts fix logger

--- src/main/java/school/faang/hogwarts/Main.java
+++ src/main/java/school/faang/hogwarts/Main.java
@@ -1,11 +1,12 @@
 package school.faang.hogwarts;
 
 public class Main {
+    private static final String lumos = "Lumos";
+    private static final String fairball = "Fairball";
+
     public static void main(String[] args) {
         SpellCaster spellCaster = new SpellCaster();
-        String lumos = "Lumos";
-        String fairball = "Fairball";
-        spellCaster.cast(lumos, spell -> spell + " is used");
-        spellCaster.cast(fairball, spell -> spell + " is used");
+        spellCaster.cast(lumos, spell -> spell + " has been cast successfully");
+        spellCaster.cast(fairball, spell -> spell + " has been cast successfully");
     }
 }
--- src/main/java/school/faang/hogwarts/SpellCaster.java
+++ src/main/java/school/faang/hogwarts/SpellCaster.java
@@ -1,7 +1,13 @@
 package school.faang.hogwarts;
 
+import lombok.NonNull;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 public class SpellCaster {
-    public void cast(String spell, SpellAction action) {
-        System.out.println(action.perform(spell));
+    private static final Logger logger = LoggerFactory.getLogger(SpellCaster.class);
+
+    public void cast(@NonNull String spell, @NonNull SpellAction action) {
+        logger.info(action.perform(spell));
     }
 }
