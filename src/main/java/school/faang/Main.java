97e09e4fc3cd69ebf050bc4b46c8a49bd435c4b5
Ivan Prygunov <88151612+juzu400@users.noreply.github.com>
2025-02-12 23:01:13 +0300

    откат

--- src/main/java/school/faang/Main.java
+++ src/main/java/school/faang/Main.java
@@ -1,13 +1,7 @@
 package school.faang;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.List;
-
 public class Main {
     public static void main(String[] args) {
-        // Задача Регистрация пользователей
         User ivan = new User("Ivan", 22, "Uber", "London");
         try {
             User kid = new User("Matthew", 16, "Google", "London");
@@ -15,21 +9,5 @@ public class Main {
             System.out.println(e.getMessage());
         }
         System.out.printf("Вывод данных пользователя: %s \n", ivan);
-        
-        // Задача Развернись
-        List<int[]> list = new ArrayList<>();
-        int[] array = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
-        int[] arrayNull = null;
-        int[] arrayOneElement = {7};
-        Collections.addAll(list, array, arrayNull, arrayOneElement);
-
-        for (int[] element : list) {
-            try {
-                System.out.printf("Исходный массив: %s \n", Arrays.toString(element));
-                System.out.printf("Полученный массив: %s \n", Arrays.toString(Example.reverse(element)));
-            } catch (IllegalArgumentException e) {
-                System.out.printf("Ошибка: %s \n", e.getMessage());
-            }
-        }
     }
 }
