5fb44aaa231c958795ccc6d1a01753fd6e98d798
Iaroslava Seregina <yaroslavaseregina4@gmail.com>
2025-02-12 14:31:37 +0300

    The task "Библиотека Вестеросса" completed

---
+++ src/main/java/school/faang/Book.java
@@ -0,0 +1,37 @@
+package school.faang;
+
+import java.util.Objects;
+
+public class Book {
+    private String title;
+    private String author;
+    private int year;
+
+    public Book(String title, String author, int year) {
+        this.author = author;
+        this.title = title;
+        this.year = year;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj == this) {
+            return true;
+        }
+        if (obj == null || getClass() != obj.getClass()) {
+            return false;
+        }
+        Book book = (Book) obj;
+        return year == book.year && Objects.equals(title, book.title) && Objects.equals(author, book.author);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(title, author, year);
+    }
+
+    @Override
+    public String toString(){
+        return "Title: " + title + ", Author: " + author + ", Year: " + year;
+    }
+}
--- src/main/java/school/faang/Main.java
+++ src/main/java/school/faang/Main.java
@@ -1,4 +1,14 @@
 package school.faang;
 
 public class Main {
+    public static void main(String[] args) {
+        LibrarySystem library = new LibrarySystem();
+        library.addBook("Winnie-the-Pooh", "A. A. Milne, E. H. Shepard", 1921);
+        library.addBook("The Secret Garden", "Frances Hodgson Burnett", 1911);
+        library.addBookToShelf("Green Eggs and Ham", "Dr.Seuss", 1960, "2nd shelf");
+        library.printAllBooks();
+        library.findBook("Winnie-the-Pooh", "A. A. Milne, E. H. Shepard", 1921);
+        library.removeBook("Winnie-the-Pooh", "A. A. Milne, E. H. Shepard", 1921);
+        library.findBook("Winnie-the-Pooh", "A. A. Milne, E. H. Shepard", 1921);
+    }
 }
