6dc62ec9b98397bcba62575db02c83496920c015
shanteho <tamasha38609@mail.ru>
2025-02-12 04:08:56 +0300

    The solve problem

--- src/main/java/school/faang/Main.java
+++ src/main/java/school/faang/Main.java
@@ -1,4 +1,26 @@
 package school.faang;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+import static school.faang.User.groupUsers;
+
 public class Main {
+    public static void main(String[] argc) {
+        List<User> users = new ArrayList<User>();
+        users.add(new User("Pave", 24, "Google", "London"));
+        users.add(new User("Alex", 29, "Apple", "Apple Park Way"));
+        users.add(new User("Dima", 27, "Amazon", "Berlin"));
+        users.add(new User("Dima", 24, "Amazon", "Berlin"));
+
+        Map<Integer, List<User>> usersMap  = groupUsers(users);
+        for (Map.Entry<Integer, List<User>> entry : usersMap.entrySet()) {
+            System.out.println( "Возраст: " +
+                    entry.getKey());
+            for (User user : entry.getValue()) {
+                System.out.println(user.getName());
+            }
+        }
+    }
 }
---
+++ src/main/java/school/faang/User.java
@@ -0,0 +1,38 @@
+package school.faang;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class User {
+    private String name;
+    private int age;
+    private String workplace;
+    private String address;
+
+    public User(String name, int age, String workplace, String address) {
+        this.name = name;
+        this.age = age;
+        this.workplace = workplace;
+        this.address = address;
+    }
+
+    public int getAge() {
+        return this.age;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public static Map<Integer, List<User>> groupUsers(List<User> users) {
+        Map<Integer, List<User>> usersMap = new HashMap<>();
+
+        for (User user : users) {
+            usersMap.putIfAbsent(user.getAge(), new ArrayList<>());
+            usersMap.get(user.getAge()).add(user);
+        }
+        return usersMap;
+    }
+}
