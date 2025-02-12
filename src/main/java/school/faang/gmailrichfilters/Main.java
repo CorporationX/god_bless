a6391c8c3450cd2f883ae0daa89d4a4a34edb96a
Ilyas Azhdar <azhdar902@gmail.com>
2025-02-12 12:10:59 +0500

    added soutf

--- src/main/java/school/faang/gmailrichfilters/Main.java
+++ src/main/java/school/faang/gmailrichfilters/Main.java
@@ -18,7 +18,7 @@ public class Main {
         );
 
         Predicate<Email> importantFilter = Email::isImportant;
-        Consumer<Email> printEmail = email -> System.out.println("Обработано письмо: " + email.getSubject());
+        Consumer<Email> printEmail = email -> System.out.printf("Обработано письмо: %s%n", email.getSubject());
         Function<Email, String> toUpperCase = email -> {
             email.setBody(email.getBody().toUpperCase());
             return email.getBody();
@@ -27,6 +27,6 @@ public class Main {
         emailProcessor.processEmails(emails, importantFilter, printEmail, toUpperCase);
 
         emails.forEach(email ->
-                System.out.println("Тема: " + email.getSubject() + ", Тело письма: " + email.getBody()));
+                System.out.printf("Тема: %s, Тело письма: %s%n", email.getSubject(), email.getBody()));
     }
 }
