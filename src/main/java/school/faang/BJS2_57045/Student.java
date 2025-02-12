05f969ca8bb0644ef9aaa0d075d709b4e7b670ba
Gulnaz <gumerova.gulnaz2014@yandex.ru>
2025-02-12 14:18:18 +0300

    Task BJS2-57045 completed

---
+++ src/main/java/school/faang/BJS2_57045/Student.java
@@ -0,0 +1,14 @@
+package school.faang.BJS2_57045;
+
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.ToString;
+
+@AllArgsConstructor
+@EqualsAndHashCode
+@ToString
+@Getter
+public class Student {
+    private final String name;
+}
---
+++ src/main/java/school/faang/BJS2_57045/Subject.java
@@ -0,0 +1,12 @@
+package school.faang.BJS2_57045;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.ToString;
+
+@AllArgsConstructor
+@ToString
+@Getter
+public class Subject {
+    private final String name;
+}
