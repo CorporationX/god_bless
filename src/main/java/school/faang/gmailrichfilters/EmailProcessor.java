5bbe07e6b61dac9fd8398baad71900b4593b54c3
Ilyas Azhdar <azhdar902@gmail.com>
2025-02-12 12:10:43 +0500

    added null checks

--- src/main/java/school/faang/gmailrichfilters/EmailProcessor.java
+++ src/main/java/school/faang/gmailrichfilters/EmailProcessor.java
@@ -1,6 +1,7 @@
 package school.faang.gmailrichfilters;
 
 import java.util.List;
+import java.util.Objects;
 import java.util.function.Consumer;
 import java.util.function.Function;
 import java.util.function.Predicate;
@@ -9,6 +10,11 @@ public class EmailProcessor {
 
     public void processEmails(List<Email> emails, Predicate<Email> filter,
                               Consumer<Email> consumer, Function<Email, String> function) {
+        Objects.requireNonNull(emails, "emails required");
+        Objects.requireNonNull(filter, "filter required");
+        Objects.requireNonNull(consumer, "consumer required");
+        Objects.requireNonNull(function, "function required");
+
         emails.stream()
                 .filter(filter)
                 .peek(function::apply)
