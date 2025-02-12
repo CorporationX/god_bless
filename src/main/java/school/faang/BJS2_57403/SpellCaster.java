01808a25fa92f79442ea7f44c6ec3a659ae8d1e2
Gulnaz <gumerova.gulnaz2014@yandex.ru>
2025-02-13 01:14:13 +0300

    checksty

--- src/main/java/school/faang/BJS2_57403/SpellCaster.java
+++ src/main/java/school/faang/BJS2_57403/SpellCaster.java
@@ -3,7 +3,7 @@ package school.faang.BJS2_57403;
 import lombok.NonNull;
 
 public class SpellCaster {
-    public void cast(@NonNull String nameSpell,@NonNull SpellAction spellAction) {
+    public void cast(@NonNull String nameSpell, @NonNull SpellAction spellAction) {
         System.out.printf("%s\n", spellAction.castSpell(nameSpell));
     }
 }
