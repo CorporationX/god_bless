fbdd3585cc458a5a53c8b4d264db65ecf8974255
Anton Lukyanchikov <antontotenkopf@gmail.com>
2025-02-12 23:30:07 +0300

    refactored method addItem and updateItem

--- src/main/java/task_BJS2_57435/InventoryManager.java
+++ src/main/java/task_BJS2_57435/InventoryManager.java
@@ -2,15 +2,17 @@ package task_BJS2_57435;
 
 import lombok.NonNull;
 
+import java.util.List;
 import java.util.function.Consumer;
 import java.util.function.Function;
 import java.util.function.Predicate;
 
 public class InventoryManager {
 
-    public void addItem(@NonNull Character character, Item item, Consumer<Item> consumer) {
-        character.getInventory().add(item);
-        consumer.accept(item);
+    public void addItem(@NonNull Character character, Item item, Consumer<Item> itemAction) {
+        List<Item> inventory = character.getInventory();
+        inventory.add(item);
+        itemAction.accept(item);
     }
 
     public void removeItem(@NonNull Character character, Predicate<Item> predicate) {
@@ -18,10 +20,11 @@ public class InventoryManager {
     }
 
     public void updateItem(@NonNull Character character, Predicate<Item> predicate, Function<Item, Item> function) {
-        for (Item item : character.getInventory()) {
+        List<Item> inventory = character.getInventory();
+        for (int i = 0; i < inventory.size(); i++) {
+            Item item = inventory.get(i);
             if (predicate.test(item)) {
-                item.setValue(function.apply(item).getValue());
-                item.setName(function.apply(item).getName());
+                inventory.set(i, function.apply(item));
             }
         }
     }
