da1dedd9fe7f915bd042eedb7ebe1e66945afda9
Yaroslav Olkin <shrelok.pupkin@yandex.ru>
2025-02-12 16:12:21 +0300

    2

--- src/main/java/school/faang/task_57567/FilterProcessor.java
+++ src/main/java/school/faang/task_57567/FilterProcessor.java
@@ -1,13 +1,15 @@
 package school.faang.task_57567;
 
+import javax.validation.constraints.NotNull;
 import java.util.function.Function;
 
 public class FilterProcessor {
-    public Image applyFilter(Image originalImage, Function<Image, Image> filter) {
+    public Image applyFilter(@NotNull Image originalImage, @NotNull Function<Image, Image> filter) {
         return filter.apply(originalImage);
     }
 
-    public Function<Image, Image> combineFilters(Function<Image, Image> filter1, Function<Image, Image> filter2) {
-        return image -> filter2.apply(filter1.apply(image));
+    public Function<Image, Image> combineFilters(@NotNull Function<Image, Image> filter1,
+                                                 @NotNull Function<Image, Image> filter2) {
+        return filter1.andThen(filter2);
     }
 }
--- src/main/java/school/faang/task_57567/Main.java
+++ src/main/java/school/faang/task_57567/Main.java
@@ -3,17 +3,21 @@ package school.faang.task_57567;
 import java.util.function.Function;
 
 public class Main {
+    private static final String GRAYSCALE_DESCRIPTION = " | Filter: Black and white";
+    private static final String SEPIA_DESCRIPTION = " | Filter: Sepia";
+    private static final String VIGNETTE_DESCRIPTION = " | Filter: Vignette";
+
     public static void main(String[] args) {
         Image originalImage = new Image("original.jpg", "The original image");
 
         FilterProcessor filterProcessor = new FilterProcessor();
 
-        Function<Image, Image> grayscaleFilter = (image) ->
-                new Image(image.getName(), image.getDescription() + " | Filter: Black and white");
-        Function<Image, Image> sepiaFilter = (image) ->
-                new Image(image.getName(), image.getDescription() + " | Filter: Sepia");
-        Function<Image, Image> vignetteFilter = (image) ->
-                new Image(image.getName(), image.getDescription() + " | Filter: Vignette");
+        Function<Image, Image> grayscaleFilter = image -> new Image(image.getName(), image.getDescription() +
+                GRAYSCALE_DESCRIPTION);
+        Function<Image, Image> sepiaFilter = image -> new Image(image.getName(), image.getDescription() +
+                SEPIA_DESCRIPTION);
+        Function<Image, Image> vignetteFilter = image -> new Image(image.getName(), image.getDescription() +
+                VIGNETTE_DESCRIPTION);
 
         Image grayscaleImage = filterProcessor.applyFilter(originalImage, grayscaleFilter);
         System.out.println(grayscaleImage.getDescription());
