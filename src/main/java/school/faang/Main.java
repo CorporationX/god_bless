abfd15ac56cd21417ec35e4ca980f9d34a49b35a
update <filipp_Chudaev@mail.ru>
2025-02-13 23:55:48 +0500

    Решение

--- src/main/java/school/faang/Main.java
+++ src/main/java/school/faang/Main.java
@@ -1,4 +1,14 @@
 package school.faang;
 
 public class Main {
+    public static void main(String[] args) {
+
+        SpellCaster spellCaster = new SpellCaster();
+
+        String avadaKedavra = "avadaKedavra";
+        String protego = "protego";
+
+        spellCaster.cast(avadaKedavra, (spell) -> "Death spell " + spell);
+        spellCaster.cast(protego, (spell) -> "Reflection spell " + spell);
+    }
 }
