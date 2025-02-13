c66244a14d0e0f15eac64d831f5c235466639791
Gulnaz <gumerova.gulnaz2014@yandex.ru>
2025-02-13 17:24:51 +0300

    Task BJS2-57521 completed

---
+++ src/main/java/school/faang/BJS2_57521/Droid.java
@@ -0,0 +1,41 @@
+package school.faang.BJS2_57521;
+
+import lombok.AllArgsConstructor;
+import lombok.NonNull;
+
+@AllArgsConstructor
+public class Droid {
+    private static final int ALPHABET_SIZE = 26;
+    private final String name;
+
+    private String encryptMessage(@NonNull String message, int key) {
+        DroidMessageEncryptor encryptor = (msg, encryptorKey) -> {
+            StringBuilder encryptedMessage = new StringBuilder();
+            for (int i = 0; i < msg.length(); i++) {
+                char ch = msg.charAt(i);
+                if (Character.isLetter(ch)) {
+                    char base = Character.isUpperCase(ch) ? 'A' : 'a';
+                    ch = (char) ((ch + key - base) % ALPHABET_SIZE + base);
+                }
+                encryptedMessage.append(ch);
+            }
+            return encryptedMessage.toString();
+        };
+        return encryptor.encrypt(message, key);
+    }
+
+    private String decryptMessage(@NonNull String message, int key) {
+        return encryptMessage(message, ALPHABET_SIZE - key);
+    }
+
+    public void sendMessage(@NonNull Droid droid, @NonNull String message, int key) {
+        String encryptedMessage = encryptMessage(message, key);
+        System.out.printf("%s отправил зашифрованное сообщение: %s\n", name, encryptedMessage);
+        droid.receiveMessage(encryptedMessage, key);
+    }
+
+    public void receiveMessage(@NonNull String message, int key) {
+        String decryptedMessage = decryptMessage(message, key);
+        System.out.printf("%s получил расшифрованное сообщение: %s\n", name, decryptedMessage);
+    }
+}
---
+++ src/main/java/school/faang/BJS2_57521/DroidMessageEncryptor.java
@@ -0,0 +1,6 @@
+package school.faang.BJS2_57521;
+
+@FunctionalInterface
+public interface DroidMessageEncryptor {
+    String encrypt(String message, int key);
+}
