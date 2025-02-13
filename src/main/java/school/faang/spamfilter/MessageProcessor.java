a437ddf75d8e46cd247d666f2ca5fb3b35db637d
aleksey.kazakov <alxej.kazakov@gmail.ru>
2025-02-13 19:41:43 +0300

    BJS2-57739

--- src/main/java/school/faang/spamfilter/MessageProcessor.java
+++ src/main/java/school/faang/spamfilter/MessageProcessor.java
@@ -7,10 +7,10 @@ public class MessageProcessor {
     public boolean processMessage(String message, List<MessageFilter> filters) {
         for (MessageFilter filter : filters) {
             if (!filter.filter(message)) {
-                return false; // Сообщение не прошло фильтр
+                return false;
             }
         }
-        return true; // Сообщение прошло все фильтры
+        return true;
     }
 
 }
