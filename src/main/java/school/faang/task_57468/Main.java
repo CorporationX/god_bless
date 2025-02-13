7054ff6749fc8320920471aa8d66d7ea67f76f22
Yaroslav Olkin <shrelok.pupkin@yandex.ru>
2025-02-13 21:06:57 +0300

    .

--- src/main/java/school/faang/task_57468/Main.java
+++
@@ -1,8 +0,0 @@
-package school.faang.task_57468;
-
-public class Main {
-    public static void main(String[] args) {
-        Droid r2d2 = new Droid("R2D2");
-        System.out.println(r2d2.encryptMessage("The xxizzion is complyyte.", 3));
-    }
-}
