777d1dfa3468f0c050213ce007f1f939a2d12d9e
Dmitry Rogulkin <rogulkind@yandex.ru>
2025-02-13 20:04:10 +0500

    https://faang-school.atlassian.net/browse/BJS2-58236

--- src/main/java/school/faang/task58236/Main.java
+++ src/main/java/school/faang/task58236/Main.java
@@ -8,11 +8,33 @@ import java.util.List;
 public class Main {
     public static void main(String[] args) {
         List<Integer> numbers = new ArrayList<>();
-        for (int i = 0; i < 20; i++) {
+        for (int i = 0; i < 10; i++) {
             numbers.add((int) (Math.random() * 1000));
         }
+        System.out.println("Вывод массива\n" + numbers + "\n");
         ListOperations listOperations = new ListOperations();
+        System.out.println("Сумма четных элементов: " + listOperations.summaChet(numbers) + "\n");
+        System.out.println("Максимальный элемент: " + listOperations.maxElement(numbers) + "\n");
+        System.out.println("Среднее арифметическое: " + listOperations.midElement(numbers) + "\n");
 
-        System.out.println(listOperations.summaChet(numbers));
+        List<String> strings = List.of(
+                "Тихий вечер, звезды мерцают",
+                "Ветер шепчет, листья кружатся",
+                "Солнце встает, тихий рассвет",
+                "Волны бегут, ветер стихает",
+                "Звезды гаснут, солнце сияет",
+                "Время бежит, листья опадают"
+        );
+
+        System.out.println("Набор строк");
+        strings.stream()
+                .forEach(System.out::println);
+
+        char startSymbol = 'В';
+        System.out.println("Количество строк на букву " + startSymbol + " = " + listOperations.findStartChar(strings, startSymbol));
+        String repeat = "ветер";
+        System.out.println("Повторение слова \"" + repeat + "\" в строках" +listOperations.findSubString(strings, "ветер"));
+
+
     }
 }
--- src/main/java/school/faang/task58236/service/ListOperations.java
+++ src/main/java/school/faang/task58236/service/ListOperations.java
@@ -7,7 +7,37 @@ public class ListOperations {
         int result = 0;
         result = numb.stream()
                 .filter((num) -> num % 2 == 0)
-                .reduce(0, (sum, current) -> sum + current);
+                .reduce(0, Integer::sum);
         return result;
     }
+
+    public int maxElement(List<Integer> numb) {
+        int result = 0;
+        result = numb.stream()
+                .max(Integer::compareTo).orElse(0);
+        return result;
+    }
+
+    public double midElement(List<Integer> numb) {
+        return numb.stream()
+                .mapToDouble(value -> (double) value)
+                .average()
+                .orElse(0.0);
+    }
+
+    public long findStartChar(List<String> strings, char startSymbol) {
+        return strings.stream()
+                .filter(startCh -> startCh.charAt(0) == startSymbol)
+                .count();
+    }
+
+    public List<String> findSubString(List<String> strings, String subString) {
+        List<String> result = strings.stream()
+                .map(String::toLowerCase)
+                .filter(subStr -> subStr.contains(subString))
+                .toList();
+        return result;
+    }
+
+
 }
