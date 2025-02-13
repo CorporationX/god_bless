e79cbba769f6640e5cf399fca80e5769ccea369f
Vitaliy <luckydollar@MacBook-Air-Vitalij.local>
2025-02-13 18:22:48 +0300

    Внесены правки в задачу BJS2-57356

--- src/main/java/school/faang/khogvarts/Main.java
+++ src/main/java/school/faang/khogvarts/Main.java
@@ -4,12 +4,8 @@ public class Main {
     public static void main(String[] args) {
         SpellCaster spellCaster = new SpellCaster();
 
-        String alohomora = "Alohomora";
-        String lumos = "Lumos";
-        String expelliarmus = "Expelliarmus";
-
-        spellCaster.cast(alohomora, spell -> "The door is unlocked by " + spell);
-        spellCaster.cast(lumos, spell -> "A beam of light is created by " + spell);
-        spellCaster.cast(expelliarmus, spell -> "The opponent is disarmed by " + spell);
+        spellCaster.cast(SpellConstants.alohomora, spell -> "The door is unlocked by " + spell);
+        spellCaster.cast(SpellConstants.lumos, spell -> "A beam of light is created by " + spell);
+        spellCaster.cast(SpellConstants.expelliarmus, spell -> "The opponent is disarmed by " + spell);
     }
 }
--- src/main/java/school/faang/khogvarts/SpellCaster.java
+++ src/main/java/school/faang/khogvarts/SpellCaster.java
@@ -1,7 +1,9 @@
 package school.faang.khogvarts;
 
+import lombok.NonNull;
+
 public class SpellCaster {
-    public void cast(String spell, SpellAction action) {
+    public void cast(@NonNull String spell, @NonNull SpellAction action) {
         System.out.println(action.actionRun(spell));
     }
 }
