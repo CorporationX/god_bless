e8f8437162b7dca809ea2124dfb371380dc233e1
Vitaliy <luckydollar@MacBook-Air-Vitalij.local>
2025-02-13 18:58:31 +0300

    Внесены правки в задачу BJS2-57463

--- src/main/java/school/faang/lordOfTheRings/InventoryManager.java
+++ src/main/java/school/faang/lordOfTheRings/InventoryManager.java
@@ -8,7 +8,8 @@ import java.util.function.Predicate;
 public class InventoryManager {
 
     public void addItem(Character character, Item item, Consumer<Item> consumer) {
-        character.getInventory().add(item);
+        List<Item> inventory = character.getInventory();
+        inventory.add(item);
         consumer.accept(item);
     }
 
@@ -19,11 +20,10 @@ public class InventoryManager {
 
     public void updateItem(Character character,
                            Predicate<Item> predicate, Function<Item, Item> function) {
-        Item updateItem;
         List<Item> inventory = character.getInventory();
         for (Item item : inventory) {
             if (predicate.test(item)) {
-                updateItem = function.apply(item);
+                Item updateItem = function.apply(item);
                 inventory.set(inventory.indexOf(item), updateItem);
             }
         }
