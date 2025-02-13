4a3ecb1ab6dc0adcf02d2e40c4cce0100902247c
Scar163 <trejdert15@gmail.com>
2025-02-13 20:26:51 +0400

    решил задачу библиотека вестероса исправил ошибки

--- src/main/java/school/faang/sprint1/BJS2_56517/User.java
+++ src/main/java/school/faang/sprint1/BJS2_56517/User.java
@@ -6,6 +6,7 @@ import java.util.Set;
 
 
 public class User {
+
     public static final Set<String> VALID_JOBS = Set.of(
             "Google",
             "Uber",
@@ -25,26 +26,38 @@ public class User {
 
     @NonNull
     public User(String name, int age, String job, String address) {
-        valid(name, age, job, address);
+        validName(name);
+        validAge(age);
+        validJob(job);
+        validAddres(address);
         this.name = name;
         this.age = age;
         this.job = job;
         this.address = address;
     }
 
-    private void valid(String name, int age, String job, String address) {
 
-        if (name.isBlank()) {
-            throw new IllegalArgumentException("Имя не может быть пустым.");
+    private void validName(String name) {
+        if (name == null || name.isBlank()) {
+            throw new IllegalArgumentException("Имя не может быть пустым." + name);
         }
+    }
+
+    private void validAge(int age) {
         if (age < MIN_LEGAL_AGE) {
-            throw new IllegalArgumentException("Возраст не может быть меньше 18.");
+            throw new IllegalArgumentException("Возраст не может быть меньше 18." + age);
         }
+    }
+
+    private void validJob(String  job) {
         if (!VALID_JOBS.contains(job)) {
-            throw new IllegalArgumentException("Место работы должно содержаться в наборе VALID_JOBS.");
+            throw new IllegalArgumentException("Место работы должно содержаться в наборе VALID_JOBS." + job);
         }
+    }
+
+    private void validAddres(String address) {
         if (!VALID_ADDRESSES.contains(address)) {
-            throw new IllegalArgumentException("Адрес должен содержаться в наборе VALID_ADDRESSES.");
+            throw new IllegalArgumentException("Адрес должен содержаться в наборе VALID_ADDRESSES." + address);
         }
     }
 }
