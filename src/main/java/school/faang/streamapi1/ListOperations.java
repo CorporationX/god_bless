c54aa6b8bbc300641898aabf67c90253cb1a272b
Anton Kapshukov <kapshukov.project@gmail.com>
2025-02-13 20:36:21 +0300

    Потренируем StreamAPI 1 (с учетом комментариев)

--- src/main/java/school/faang/streamapi1/ListOperations.java
+++ src/main/java/school/faang/streamapi1/ListOperations.java
@@ -6,6 +6,8 @@ import java.util.NoSuchElementException;
 import java.util.function.Predicate;
 
 public class ListOperations {
+    public static double DOUBLE_ZERO = 0.0;
+
     public static int sumOfEvenNumbers(List<Integer> numbers) {
         validateList(numbers);
         return numbers
@@ -29,14 +31,14 @@ public class ListOperations {
                 .stream()
                 .mapToInt(Integer::intValue)
                 .average()
-                .orElse(0.0);
+                .orElse(DOUBLE_ZERO);
     }
 
     public static int countStringsStartingWith(List<String> strings, char ch) {
         validateList(strings);
         return (int) strings
                 .stream()
-                .filter(s -> s.charAt(0) == ch)
+                .filter(s -> s != null && !s.isEmpty() && s.charAt(0) == ch)
                 .count();
     }
 
