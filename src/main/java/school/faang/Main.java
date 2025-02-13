010a5e75020a93f2e2d2761f932bdc1caaa3e315
Sergey Ermoshenko <ermoshenko.work@gmail.com>
2025-02-13 08:56:45 +0300

    add calculator

--- src/main/java/school/faang/Main.java
+++ src/main/java/school/faang/Main.java
@@ -1,4 +1,11 @@
 package school.faang;
 
+import java.util.List;
+
 public class Main {
+    public static void main(String[] args) {
+        List<Integer> list1 = List.of(1, 5, 8, 76, 22, 90);
+        System.out.println(Operations.sum(list1));
+        System.out.println(Operations.product(list1));
+    }
 }
