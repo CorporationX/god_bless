25f1c2dcb1f41afaf5af2b5584ee4b084cc35788
Takoi <takoihero17@gmail.com>
2025-02-13 14:53:02 +0500

    Валидация полей пользователя

--- src/main/java/school/faang/BJS2_58700/Main.java
+++ src/main/java/school/faang/BJS2_58700/Main.java
@@ -3,8 +3,10 @@ package school.faang.BJS2_58700;
 public class Main {
     public static void main(String[] args) {
         try {
-            User userOne = new User("Uber", 8, "Google", "Amsterdam");
-        }catch (IllegalArgumentException e){
+            User userOne = new User("Sam", 8, "Google", "Amsterdam");
+            User userTwo = new User("Yan", 29, "Apple", "Washington");
+            User userThree = new User("Artur", 19, "Uber", "Berlin");
+        } catch (IllegalArgumentException e) {
             System.out.println(e);
         }
 
--- src/main/java/school/faang/BJS2_58700/User.java
+++ src/main/java/school/faang/BJS2_58700/User.java
@@ -8,25 +8,23 @@ import java.util.Set;
 @Getter
 @Setter
 public class User {
+    private static final int MIN_AGE = 18;
     private static final Set<String> VALID_JOBS = Set.of("Google", "Uber", "Amazon");
     private static final Set<String> VALID_ADDRESSES = Set.of("London", "New York", "Amsterdam");
 
-    private String name;
-    private int age;
-    private String job;
-    private String address;
+    private final String name;
+    private final int age;
+    private final String job;
+    private final String address;
 
     public User(String name, int age, String job, String address) {
-        if (name.isEmpty()) {
+        if (!validateName(name)) {
             throw new IllegalArgumentException("Имя не может быть пустым.");
-        }
-        if (age < 18) {
-            throw new IllegalArgumentException("Возраст не может быть меньше 18.");
-        }
-        if (!VALID_JOBS.contains(job)) {
+        } else if (!validateAge(age)) {
+            throw new IllegalArgumentException("Возраст не может быть меньше " + MIN_AGE);
+        } else if (validateJob(job)) {
             throw new IllegalArgumentException("Место работы должно быть одно из следующих: " + VALID_JOBS);
-        }
-        if (!VALID_ADDRESSES.contains(address)) {
+        } else if (validateAddress(address)) {
             throw new IllegalArgumentException("Адрес должен быть одним из следующих: " + VALID_ADDRESSES);
         }
 
@@ -36,4 +34,31 @@ public class User {
         this.address = address;
     }
 
+    private boolean validateName(String name) {
+        if (name.isBlank()) {
+            return false;
+        }
+        return true;
+    }
+
+    private boolean validateAge(int age) {
+        if (age < MIN_AGE) {
+            return false;
+        }
+        return true;
+    }
+
+    private boolean validateJob(String job) {
+        if (!VALID_JOBS.contains(job)) {
+            return false;
+        }
+        return true;
+    }
+
+    private boolean validateAddress(String address) {
+        if (!VALID_ADDRESSES.contains(address)) {
+            return false;
+        }
+        return true;
+    }
 }
