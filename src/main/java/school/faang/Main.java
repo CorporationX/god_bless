c607d9547308e90dd393ff2c07af40da4f7a765d
Ivan Prygunov <88151612+juzu400@users.noreply.github.com>
2025-02-13 15:34:14 +0300

    задача Разбираем склад Amazon

--- src/main/java/school/faang/Main.java
+++ src/main/java/school/faang/Main.java
@@ -2,26 +2,5 @@ package school.faang;
 
 public class Main {
     public static void main(String[] args) {
-        // Задача Абстракция Абстракция
-        Character warrior = new Warrior("Warrior");
-        Character archer = new Archer("Archer");
-
-        System.out.printf("Параметры варвара: %s \n", warrior);
-        System.out.printf("Параметры лучника: %s \n", archer);
-
-        warrior.attack(archer);
-        archer.attack(warrior);
-
-        System.out.println(warrior);
-        System.out.println(archer);
-        
-        // Задача Развернитесь
-        User ivan = new User("Ivan", 22, "Uber", "London");
-        try {
-            User kid = new User("Matthew", 16, "Google", "London");
-        } catch (IllegalArgumentException e) {
-            System.out.println(e.getMessage());
-        }
-        System.out.printf("Вывод данных пользователя: %s \n", ivan);
     }
 }
