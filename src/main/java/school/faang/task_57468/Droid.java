9bcf2e98d416694888769570292d850d58c81508
Yaroslav Olkin <shrelok.pupkin@yandex.ru>
2025-02-13 18:31:13 +0300

    BJS2-57468

--- src/main/java/school/faang/task_57468/Droid.java
+++ src/main/java/school/faang/task_57468/Droid.java
@@ -10,7 +10,7 @@ import java.util.List;
 public class Droid {
     @NonNull
     private final String name;
-    private List<Character> alphabet;
+    private final List<Character> alphabet;
 
     public Droid(String name) {
         validateName(name);
@@ -30,6 +30,7 @@ public class Droid {
     }
 
     public String encryptMessage(String message, int key) {
+        validateMessage(message);
         DroidMessageEncryptor encryptor = (decryptionMessage, decryptionKey) -> {
             StringBuilder builder = new StringBuilder();
             for (char c : decryptionMessage.toCharArray()) {
@@ -42,7 +43,7 @@ public class Droid {
                     if (Character.isLowerCase(c)) {
                         builder.append(Character.toLowerCase(encryptedChar));
                     } else {
-                        builder.append(alphabet.get(newIndex));
+                        builder.append(encryptedChar);
                     }
                 }
             }
@@ -51,9 +52,51 @@ public class Droid {
         return encryptor.encrypt(message, key);
     }
 
+    public String decryptMessage(String message, int key) {
+        validateMessage(message);
+        DroidMessageEncryptor encryptor = (decryptionMessage, decryptionKey) -> {
+            StringBuilder builder = new StringBuilder();
+            for (char c : decryptionMessage.toCharArray()) {
+                int index = alphabet.indexOf(c);
+                if (index == -1) {
+                    builder.append(c);
+                } else {
+                    int newIndex = (index - decryptionKey + alphabet.size()) % alphabet.size();
+                    char decryptedChar = alphabet.get(newIndex);
+                    if (Character.isLowerCase(c)) {
+                        builder.append(Character.toLowerCase(decryptedChar));
+                    } else {
+                        builder.append(decryptedChar);
+                    }
+                }
+            }
+            return builder.toString();
+        };
+        return encryptor.encrypt(message, key);
+    }
+
+    public void sendMessage(String message, int key, Droid droid) {
+        String messageToSend = encryptMessage(message, key);
+        System.out.printf("%s sent an encrypted message: %s\n",
+                getName(), messageToSend);
+        receiveMessage(messageToSend, key, droid);
+    }
+
+    public void receiveMessage(String message, int key, Droid droid) {
+        String messageToReceive = decryptMessage(message, key);
+        System.out.printf("%s received the decrypted message: %s\n",
+                droid.getName(), messageToReceive);
+    }
+
+    private void validateMessage(String message) {
+        if (message == null) {
+            throw new NullPointerException("Message cant be null");
+        }
+    }
+
     private void validateName(String name) {
         if (name == null) {
-            throw new NullPointerException("Null cant be null");
+            throw new NullPointerException("Name cant be null");
         }
     }
 }
--- src/main/java/school/faang/task_57468/Main.java
+++ src/main/java/school/faang/task_57468/Main.java
@@ -2,9 +2,17 @@ package school.faang.task_57468;
 
 public class Main {
     public static void main(String[] args) {
+        Droid r2d2 = new Droid("R2D2");
+        Droid cp0 = new Droid("CP0");
+
+        int key = 3;
+
         try {
-            Droid r2d2 = new Droid("R2D2");
-            System.out.println(r2d2.encryptMessage("The xxizzion is complyyte.", 3));
+            r2d2.sendMessage("Hello!", key, cp0);
+            cp0.sendMessage("Hello!", key, r2d2);
+            r2d2.sendMessage("How are you?", key, cp0);
+            cp0.sendMessage("Im fine, and you?", key, r2d2);
+            r2d2.sendMessage("Im too!", key, cp0);
         } catch (NullPointerException e) {
             System.out.println(e.getMessage());
         }
