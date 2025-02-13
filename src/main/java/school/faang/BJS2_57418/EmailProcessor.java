2d1305942c9ea0ad2d718c13fa2fb7c8b1dc970f
Pavel Chernobay <mr.chernobay1990@gmail.com>
2025-02-13 16:37:09 +0300

    Проверка на null в ProcessEmail

--- src/main/java/school/faang/BJS2_57418/EmailProcessor.java
+++ src/main/java/school/faang/BJS2_57418/EmailProcessor.java
@@ -1,6 +1,7 @@
 package school.faang.BJS2_57418;
 
 import java.util.List;
+import java.util.Objects;
 import java.util.function.Consumer;
 import java.util.function.Function;
 import java.util.function.Predicate;
@@ -9,6 +10,11 @@ public class EmailProcessor {
 
     public void processEmails(List<Email> emails, Predicate<Email> isImportant,
                               Function<Email, String> converter, Consumer<Email> handler) {
+        Objects.requireNonNull(emails, "Emails list cannot be null");
+        Objects.requireNonNull(isImportant, "Predicate cannot be null");
+        Objects.requireNonNull(converter, "Function cannot be null");
+        Objects.requireNonNull(handler, "Consumer cannot be null");
+
         for (Email email : emails) {
             if (isImportant.test(email)) {
                 converter.apply(email);
