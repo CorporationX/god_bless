c4bd4a2816f267a877ee45871c23d26446dad3c9
Sergey Ermoshenko <ermoshenko.work@gmail.com>
2025-02-13 18:54:57 +0300

    add CSV converter

--- src/main/java/school/faang/Main.java
+++ src/main/java/school/faang/Main.java
@@ -1,4 +1,37 @@
 package school.faang;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class Main {
+    public static void main(String[] args) {
+        VectorJoiner<String> vectorJoiner = vector -> {
+            if (vector == null || vector.isEmpty()) {
+                throw new IllegalArgumentException("Vector can't be null or empty.");
+            }
+            return vector.stream()
+                    .map(Object::toString)
+                    .collect(Collectors.joining(", "));
+
+        };
+
+        MatrixJoiner<String> matrixJoiner = vector -> {
+            if (vector == null || vector.isEmpty()) {
+                throw new IllegalArgumentException("Vector can't be null or empty.");
+            }
+            return vector.stream()
+                    .map(vectorJoiner::join)
+                    .collect(Collectors.joining("\n"));
+        };
+        List<List<String>> toCsv = (
+                List.of(
+                        List.of("1", "2", "3", "4", "5"),
+                        List.of("6", "7", "8", "9", "10"),
+                        List.of("11", "12", "13", "14", "15"),
+                        List.of("16", "17", "18", "19", "20"),
+                        List.of("21", "22", "23", "24", "25")
+                )
+        );
+        System.out.println(matrixJoiner.join(toCsv));
+    }
 }
---
+++ src/main/java/school/faang/MatrixJoiner.java
@@ -0,0 +1,8 @@
+package school.faang;
+
+import java.util.List;
+
+@FunctionalInterface
+public interface MatrixJoiner<T> {
+    T join(List<List<T>> matrix);
+}
