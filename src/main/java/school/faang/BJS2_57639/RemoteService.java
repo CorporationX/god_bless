386cadf2571546f8c71b100d8d7e248ba17c764b
Pavel Chernobay <mr.chernobay1990@gmail.com>
2025-02-14 00:22:24 +0300

    BJS2-57639 <task>

---
+++ src/main/java/school/faang/BJS2_57639/RemoteService.java
@@ -0,0 +1,8 @@
+package school.faang.BJS2_57639;
+
+public class RemoteService {
+
+    public static String call(String param) {
+        throw new RuntimeException("Сервис не доступен");
+    }
+}
