014a6a5ea160440779d917559ffcf56cfedbaefd
Sergei Ananev <sa.ananyev@yandex.ru>
2025-02-14 22:07:39 +0300

    BJS2-56899 added boundary constants for temperature and humidity in class WeatherService

--- src/main/java/school/faang/task_56899/WeatherService.java
+++ src/main/java/school/faang/task_56899/WeatherService.java
@@ -5,10 +5,15 @@ import lombok.NonNull;
 import java.util.Random;
 
 public class WeatherService implements WeatherProvider {
+    private static final double MIN_TEMPERATURE = -30.0;
+    private static final double MAX_TEMPERATURE = 30.0;
+    private static final double MIN_HUMIDITY = 30.0;
+    private static final double MAX_HUMIDITY = 85.0;
+
     @Override
     public WeatherData fetchWeatherData(@NonNull String city) {
-        double temperature = Math.round(new Random().nextDouble(-30, 30) * 10.0) / 10.0;
-        double humidity = Math.round(new Random().nextDouble(30, 85) * 10.0) / 10.0;
+        double temperature = Math.round(new Random().nextDouble(MIN_TEMPERATURE, MAX_TEMPERATURE) * 10.0) / 10.0;
+        double humidity = Math.round(new Random().nextDouble(MIN_HUMIDITY, MAX_HUMIDITY) * 10.0) / 10.0;
         long timestamp = System.currentTimeMillis();
         return new WeatherData(city, temperature, humidity, timestamp);
     }
