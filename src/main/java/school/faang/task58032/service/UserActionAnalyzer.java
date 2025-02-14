bed3afd22b9ca04280c87c1a0d8f20cb0c3cf4aa
Dmitry Rogulkin <rogulkind@yandex.ru>
2025-02-14 19:47:35 +0500

    https://faang-school.atlassian.net/browse/BJS2-58032

--- src/main/java/school/faang/task58032/service/UserActionAnalyzer.java
+++ src/main/java/school/faang/task58032/service/UserActionAnalyzer.java
@@ -4,11 +4,17 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 public class UserActionAnalyzer {
     public static List<String> topActiveUsers(List<UserAction> actions, int numb) {
-
-        return new ArrayList<>();
+        return actions.stream()
+                .collect(Collectors.groupingBy(UserAction::getName, Collectors.counting()))
+                .entrySet().stream()
+                .sorted(Map.Entry.<String, Long>comparingByValue().reversed())
+                .limit(numb)
+                .map(Map.Entry::getKey)
+                .collect(Collectors.toList());
     }
 
     public static List<String> topPopularHashtags(List<UserAction> actions, int numb) {
