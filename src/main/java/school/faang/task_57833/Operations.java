a13c1bdfa7ca71e87dc8fb1f9d5f8a921bfd778b
Yaroslav Olkin <shrelok.pupkin@yandex.ru>
2025-02-14 13:24:27 +0300

    BJS2-57833

---
+++ src/main/java/school/faang/task_57833/Operations.java
@@ -0,0 +1,43 @@
+package school.faang.task_57833;
+
+import java.util.*;
+import java.util.stream.Collectors;
+
+public class Operations {
+    public static Set<List<Integer>> uniquePairs(Set<Integer> set, int sum) {
+        return set.stream()
+                .filter(num -> num != sum - num && set.contains(sum - num))
+                .map(num -> Arrays.asList(num, sum - num))
+                .peek(Collections::sort)
+                .collect(Collectors.toSet());
+    }
+
+    public static List<String> getCapitals(Map<String, String> countries) {
+        return countries.entrySet().stream()
+                .map(Map.Entry::getValue)
+                .sorted()
+                .toList();
+    }
+
+    public static List<String> filterAndSortStrings(List<String> list, char c) {
+        return list.stream()
+                .filter(s -> !s.isBlank() && s.charAt(0) == c)
+                .sorted(Comparator.comparingInt(String::length))
+                .toList();
+    }
+
+    public static List<String> convertToBinary(List<Integer> list) {
+        return list.stream()
+                .map(Integer::toBinaryString)
+                .toList();
+    }
+
+    public static List<String> filterAndSortByLength(List<String> strings, String alphabet) {
+        String regex = "[" + alphabet + "]+";
+
+        return strings.stream()
+                .filter(s -> s.matches(regex))
+                .sorted(Comparator.comparingInt(String::length))
+                .toList();
+    }
+}
