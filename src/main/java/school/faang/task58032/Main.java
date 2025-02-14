1c6302f451807cb737fdb054a20b7ad30ccbe7a1
Dmitry Rogulkin <rogulkind@yandex.ru>
2025-02-14 19:38:21 +0500

    https://faang-school.atlassian.net/browse/BJS2-58032

--- src/main/java/school/faang/task58032/Main.java
+++ src/main/java/school/faang/task58032/Main.java
@@ -1,7 +1,26 @@
 package school.faang.task58032;
 
+import school.faang.task58032.service.UserAction;
+import school.faang.task58032.service.UserActionAnalyzer;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
 public class Main {
     public static void main(String[] args) {
+        List<UserAction> actions = new ArrayList<>();// данные о действиях пользователей
 
+        // Получение результатов
+        List<String> top10Users = UserActionAnalyzer.topActiveUsers(actions, 10);
+        List<String> top5Hashtags = UserActionAnalyzer.topPopularHashtags(actions, 5);
+        List<String> top3Commenters = UserActionAnalyzer.topCommentersLastMonth(actions, 3);
+        Map<String, Double> actionPercentages = UserActionAnalyzer.actionTypePercentages(actions);
+
+        // Вывод результатов
+        System.out.println("Топ-10 активных пользователей: " + top10Users);
+        System.out.println("Топ-5 популярных хэштегов: " + top5Hashtags);
+        System.out.println("Топ-3 комментаторов за последний месяц: " + top3Commenters);
+        System.out.println("Процент действий по типам: " + actionPercentages);
     }
 }
---
+++ src/main/java/school/faang/task58032/service/UserActionAnalyzer.java
@@ -0,0 +1,28 @@
+package school.faang.task58032.service;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class UserActionAnalyzer {
+    public static List<String> topActiveUsers(List<UserAction> actions, int numb) {
+
+        return new ArrayList<>();
+    }
+
+    public static List<String> topPopularHashtags(List<UserAction> actions, int numb) {
+
+        return new ArrayList<>();
+    }
+
+    public static List<String> topCommentersLastMonth(List<UserAction> actions, int i) {
+
+        return new ArrayList<>();
+    }
+
+    public static Map<String, Double> actionTypePercentages(List<UserAction> actions) {
+
+        return new HashMap<>();
+    }
+}
