f17ba6a5a1ccacd2b6d26286754b83d93f419cb9
phtanya <112714509+phtanya@users.noreply.github.com>
2025-02-15 00:15:00 +0100

    BJS2-57423

--- src/main/java/school/faang/hogwarts/Main.java
+++ src/main/java/school/faang/hogwarts/Main.java
@@ -4,11 +4,10 @@ public class Main {
     public static void main(String[] args) {
         SpellCaster spellCaster = new SpellCaster();
 
-        String alohomora = "Alohomora";
-        String lumos = "Lumos";
-        String expelliarmus = "Expelliarmus";
-        String blankus = "   ";
-
+        String lumos = SpellConstants.lumos;
+        String expelliarmus = SpellConstants.expelliarmus;
+        String blankus = SpellConstants.blankus;
+        String alohomora = SpellConstants.alohomora;
         spellCaster.cast(alohomora, (spell) -> "The door is unlocked by " + spell);
         spellCaster.cast(lumos, (spell) -> "A beam of light is created by " + spell);
         spellCaster.cast(expelliarmus, (spell) -> "The opponent is disarmed by " + spell);
---
+++ src/main/java/school/faang/hogwarts/SpellConstants.java
@@ -0,0 +1,9 @@
+package school.faang.hogwarts;
+
+public final class SpellConstants {
+    public static final String alohomora = "Alohomora";
+    public static final String lumos = "Lumos";
+    public static final String expelliarmus = "Expelliarmus";
+    public static final String blankus = "   ";
+
+}
