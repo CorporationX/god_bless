08a8d3c663facae3e127a3ae7f7136cc9d3de44e
Anton Kapshukov <kapshukov.04@mail.ru>
2025-02-14 21:11:00 +0300

    Анализ активности пользователей в социальной сети

--- src/main/java/school/faang/useractivityanalysis/UserAction.java
+++ src/main/java/school/faang/useractivityanalysis/UserAction.java
@@ -14,7 +14,7 @@ import java.time.LocalDate;
 public class UserAction {
     private final int id;
     private final String name;
-    private final ActionType actionType ;
-    private final LocalDate actionDate ;
-    private final String content ;
+    private final ActionType actionType;
+    private final LocalDate actionDate;
+    private final String content;
 }
