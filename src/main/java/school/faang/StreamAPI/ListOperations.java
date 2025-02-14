cce04c79383708fd0fdf31dfa826a0ad4de9d4a7
Sergey <bigwave2915@gmail.com>
2025-02-14 13:20:56 +0500

    решения задачи

--- src/main/java/school/faang/StreamAPI/ListOperations.java
+++ src/main/java/school/faang/StreamAPI/ListOperations.java
@@ -1,6 +1,10 @@
 package school.faang.StreamAPI;
 
+import java.util.Comparator;
 import java.util.List;
+import java.util.NoSuchElementException;
+import java.util.function.Predicate;
+import java.util.stream.Collectors;
 
 public class ListOperations {
     /*1*/
@@ -30,15 +34,75 @@ public class ListOperations {
     }
 
     /*4*/
+    @SuppressWarnings("checkstyle:AvoidEscapedUnicodeCharacters")
     public static long countStringsStartingWith(List<String> strings, char А) {
+        chekStrings(strings);
         return strings.stream()
                 .filter(s -> !s.isEmpty()
                         && ((s.charAt(0) >= '\u0410' && s.charAt(0) <= '\u042F')
                         || ((s.charAt(0) >= '\u0430' && s.charAt(0) <= '\u044F'))))
                 .count();
     }
-    /*5*/
 
+    /*5*/
+    public static List<String> filterStringsBySubstring(List<String> strings,
+                                                        String substring) {
+        chekStrings(strings);
+        if (substring.length() <= 1 || (substring.isBlank())) {
+            throw new IllegalArgumentException("ведите подстроку");
+        }
+        return strings.stream()
+                .filter((string -> string.contains(substring)))
+                .toList();
+    }
+
+    /*6*/
+    public static List<String> sortByLength(List<String> strings) {
+        chekStrings(strings);
+        return strings.stream()
+                .sorted(Comparator.comparingInt(String::length))
+                .collect(Collectors.toList());
+    }
+
+    /*6.1*/
+    public static List<String> sortByLength1(List<String> strings) {
+        chekStrings(strings);
+        return strings.stream()
+                .sorted(Comparator.reverseOrder())
+                .collect(Collectors.toList());
+    }
+
+    /*7*/
+    public static boolean allMatchConditionList(
+            List<Integer> numbers, Predicate<Integer> predicate) {
+        chekNumbers(numbers);
+        return numbers.stream()
+                .allMatch(predicate);
+    }
+
+    /*8*/
+    public static int findMinGreaterThan(
+            List<Integer> numbers, int threshold) {
+        chekNumbers(numbers);
+        return numbers.stream()
+                .filter(x -> x > threshold)
+                .min(Integer::compareTo)
+                .orElseThrow(() -> new NoSuchElementException("Нет элементов больше " + threshold));
+    }
+
+    /*9*/
+    public static List<Integer> mapToLengths(List<String> strings) {
+        chekStrings(strings);
+        return strings.stream()
+                .map(String::length)
+                .toList();
+    }
+
+    public static void chekStrings(List<String> strings) {
+        if (strings.isEmpty() || strings.size() < 2) {
+            throw new IllegalArgumentException("List пуст или мало символов");
+        }
+    }
 
     public static void chekNumbers(List<Integer> numbers) {
         if (numbers == null || numbers.isEmpty()) {
--- src/main/java/school/faang/StreamAPI/Main.java
+++ src/main/java/school/faang/StreamAPI/Main.java
@@ -1,21 +1,31 @@
 package school.faang.StreamAPI;
 
-
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
-        List<Integer> list = Arrays.asList(123, 3222, 13, -4, 345, 86, 34);
+        List<Integer> listNumbers = Arrays.asList(123, 3222, 13, -4, 345, 86, 34);
         List<String> fruits = Arrays.asList("яблоко", "банан", "апельсин", "виноград", "киви", "манго");
         //проверка на сумму четных чисел в списке
-        System.out.println(ListOperations.sumOfEvenNumbers(list));
+        System.out.println(ListOperations.sumOfEvenNumbers(listNumbers));
         //проверка на максимальный элемент в списке чисел
-        System.out.println(ListOperations.maxNumbers(list));
+        System.out.println(ListOperations.maxNumbers(listNumbers));
         //проверка на среднее арифметическое число списке
-        System.out.println(ListOperations.findAverage(list));
-
+        System.out.println(ListOperations.findAverage(listNumbers));
+        // проверка на количество строк
         System.out.println(ListOperations.countStringsStartingWith(fruits, 'а'));
+        // проверка на фильтрацию строк
+        System.out.println(ListOperations.filterStringsBySubstring(fruits, "ew"));
+        // проверка на список строк по длине от меньшей к большей
+        System.out.println(ListOperations.sortByLength(fruits));
+        // проверка на список строк по длине от большей к меньшей
+        System.out.println(ListOperations.sortByLength1(fruits));
+        //проверка на условие
+        System.out.println(ListOperations.allMatchConditionList(listNumbers, n -> n % 2 == 0));
+        //проверка на наименьший элемент в списке, который больше заданного числа.
+        System.out.println(ListOperations.findMinGreaterThan(listNumbers, 3));
+        //проверка преобразовать список строк в список их длин
+        System.out.println(ListOperations.mapToLengths(fruits));
     }
 }
