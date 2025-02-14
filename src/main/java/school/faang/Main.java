aac386c1adaeb437f0e92397b82150ec635c8693
KAS1990 <34664788+KAS1990@users.noreply.github.com>
2025-02-14 19:00:37 +0300

    refactor: Converted var to specific type

--- src/main/java/school/faang/Main.java
+++ src/main/java/school/faang/Main.java
@@ -6,16 +6,16 @@ import java.util.Map;
 
 public class Main {
     public static void main(String[] args) {
-        var users = new ArrayList<User>();
+        List<User> users = new ArrayList<User>();
         users.add(new User("Alex", 20, "Facebook", "London"));
         users.add(new User("Ivan", 25, "Amazon", "Berlin"));
         users.add(new User("Pavel", 20, "Google", "Paris"));
         users.add(new User("Kate", 10, "VKontakte", "Dubai"));
 
-        var map = User.groupUsers(users);
+        Map<Integer, List<User>> map = User.groupUsers(users);
 
         for (Map.Entry<Integer, List<User>> entry : map.entrySet()) {
-            var names = new ArrayList<String>();
+            List<String> names = new ArrayList<>();
             entry.getValue().stream().map(User::name).forEach(names::add);
             System.out.printf("%d: %s%n", entry.getKey(), String.join(", ", names));
         }
--- src/main/java/school/faang/User.java
+++ src/main/java/school/faang/User.java
@@ -7,14 +7,14 @@ import java.util.Map;
 
 public record User(String name, int age, String job, String address) {
     public static Map<Integer, List<User>> groupUsers(List<User> users) {
-        var result = new HashMap<Integer, List<User>>();
+        Map<Integer, List<User>> result = new HashMap<>();
 
         for (User user : users) {
             if (result.containsKey(user.age)) {
-                var usersWithSameAge = result.get(user.age);
+                List<User> usersWithSameAge = result.get(user.age);
                 usersWithSameAge.add(user);
             } else {
-                var usersWithSameAge = new ArrayList<User>();
+                List<User> usersWithSameAge = new ArrayList<>();
                 usersWithSameAge.add(user);
                 result.put(user.age, usersWithSameAge);
             }
