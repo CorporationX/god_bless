07edf2ae81b2656829f19dfdf788c264a2ff4e36
Savchenko <savchenko.ra@yandex.ru>
2025-02-14 19:09:22 +0300

    BJS2-57878 checkstyle.

--- src/main/java/school/faang/bjs2_57878/Main.java
+++ src/main/java/school/faang/bjs2_57878/Main.java
@@ -10,19 +10,22 @@ import java.util.Set;
 public class Main {
     public static void main(String[] args) {
         Set<Integer> seen = new HashSet<>(Arrays.asList(1, 2, 3, 4, 5, 6));
-        List<String> fruits = Arrays.asList("apple", "banana", "avocado", "apricot");
-        List<Integer> numbers = Arrays.asList(1, 2, 3, 4);
+        System.out.println(CollectionOperations.findUniquePairs(seen, 6));
+
         Map<String, String> countries = new HashMap<>();
-        List<String> strings = Arrays.asList("apple", "banana", "cherry", "date", "fig", "grape");
-        String alphabet = "abcdefghijklmnopqrstuvwxyz";
         countries.put("Russia", "Moscow");
         countries.put("USA", "Washington");
         countries.put("Germany", "Berlin");
-
-        System.out.println(CollectionOperations.findUniquePairs(seen, 6));
         System.out.println(CollectionOperations.sortCountriesByName(countries));
+
+        List<String> fruits = Arrays.asList("apple", "banana", "avocado", "apricot");
         System.out.println(CollectionOperations.filterAndSortStrings(fruits, 'a'));
+
+        List<Integer> numbers = Arrays.asList(1, 2, 3, 4);
         System.out.println(CollectionOperations.convertToBinary(numbers));
+
+        List<String> strings = Arrays.asList("apple", "banana", "cherry", "date", "fig", "grape");
+        String alphabet = "abcdefghijklmnopqrstuvwxyz";
         System.out.println(CollectionOperations.filterByAlphabetAndSort(strings, alphabet));
     }
 }
