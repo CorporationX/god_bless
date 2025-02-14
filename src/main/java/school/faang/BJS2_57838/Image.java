b2f8385e2af731df6ac8d3c736038c8361b012cb
Ekaterina Ramakoti <zubkova-92@mail.ru>
2025-02-14 16:05:43 +0300

    task BJS2-57898

---
+++ src/main/java/school/faang/BJS2_57838/Image.java
@@ -0,0 +1,16 @@
+package school.faang.BJS2_57838;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@AllArgsConstructor
+@Getter
+@ToString
+@Setter
+public class Image {
+    private String name;
+    public String description;
+    
+}
