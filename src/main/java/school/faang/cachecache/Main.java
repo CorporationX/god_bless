b83b0c21f29bf02fd083bb7f0847bb10fb033011
Vitaliy <luckydollar@MacBook-Air-Vitalij.local>
2025-02-14 16:40:04 +0300

    Задача BJS2-56927 решена.

--- src/main/java/school/faang/cachecache/Main.java
+++ src/main/java/school/faang/cachecache/Main.java
@@ -4,12 +4,10 @@ public class Main {
     public static void main(String[] args) throws InterruptedException {
         WeatherProvider provider = new WeatherService();
 
-        WeatherCacheTemplate standardCache = new StandardWeatherCache(provider);
-        WeatherCacheTemplate frequentCache = new FrequentUpdateWeatherCache(provider);
-
         String city = "Moscow";
         long cacheAge = 5000;
 
+        WeatherCacheTemplate standardCache = new StandardWeatherCache(provider);
         System.out.println("Standard cache:");
         System.out.println(standardCache.getWeatherData(city, cacheAge));
         Thread.sleep(10000);
@@ -17,6 +15,7 @@ public class Main {
         Thread.sleep(10000);
         System.out.println(standardCache.getWeatherData(city, cacheAge));
 
+        WeatherCacheTemplate frequentCache = new FrequentUpdateWeatherCache(provider);
         System.out.println("\nFrequent update cache:");
         System.out.println(frequentCache.getWeatherData(city, cacheAge));
         System.out.println(frequentCache.getWeatherData(city, cacheAge));
