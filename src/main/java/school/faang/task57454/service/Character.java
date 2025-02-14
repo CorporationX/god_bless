00123c24545755f589ea08b7d09a8caa519b1ec9
Dmitry Rogulkin <rogulkind@yandex.ru>
2025-02-14 20:25:38 +0500

    https://faang-school.atlassian.net/browse/BJS2-57454

--- src/main/java/school/faang/task57454/service/Character.java
+++ src/main/java/school/faang/task57454/service/Character.java
@@ -1,11 +1,11 @@
 package school.faang.task57454.service;
 
-import lombok.AllArgsConstructor;
 import lombok.Data;
 
 import java.util.ArrayList;
 import java.util.List;
-@Data @AllArgsConstructor
+
+@Data
 public class Character {
     private String name;
     private List<Item> inventory;
--- src/main/java/school/faang/task57454/service/InventoryManager.java
+++ src/main/java/school/faang/task57454/service/InventoryManager.java
@@ -5,6 +5,7 @@ import java.util.List;
 import java.util.function.Consumer;
 import java.util.function.Function;
 import java.util.function.Predicate;
+import java.util.stream.Collectors;
 
 public class InventoryManager {
     public void addItem(Character pers, Item item, Consumer<Item> consumer) {
@@ -15,23 +16,15 @@ public class InventoryManager {
     public List<Item> removeItem(Character pers, Predicate<Item> condition) {
         List<Item> removeItems = new ArrayList<>();
         List<Item> inventory = pers.getInventory();
-
-        inventory.removeIf(item -> {
-            if (condition.test(item)) {
-                return true;
-            }
-            return false;
-        });
+        inventory.removeIf(condition::test);
         return removeItems;
     }
 
     public void updateItem(Character character, Predicate<Item> predicate, Function<Item, Item> updater) {
-        List<Item> inventory = character.getInventory();
-        for (int i = 0; i < inventory.size(); i++) {
-            Item item = inventory.get(i);
-            if (predicate.test(item)) {
-                inventory.set(i, updater.apply(item));  // Обновляем предмет в списке
-            }
-        }
+        character.setInventory(
+                character.getInventory().stream()
+                        .map(item -> predicate.test(item) ? updater.apply(item) : item)
+                        .collect(Collectors.toList())
+        );
     }
 }
