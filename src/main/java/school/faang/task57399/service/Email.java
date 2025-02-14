b23dd00ee7c3fd35e4a8c39b210010eda757c7b5
Dmitry Rogulkin <rogulkind@yandex.ru>
2025-02-14 20:12:25 +0500

    https://faang-school.atlassian.net/browse/BJS2-57399

--- src/main/java/school/faang/task57399/service/Email.java
+++ src/main/java/school/faang/task57399/service/Email.java
@@ -1,10 +1,12 @@
 package school.faang.task57399.service;
 
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 
 @Data
 @AllArgsConstructor
+@Builder
 public class Email {
     private String subject;
     private String body;
