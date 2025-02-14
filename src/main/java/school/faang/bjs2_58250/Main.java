1c449b052934dc4b93cefce8411c8bd22be9dd83
Savchenko <savchenko.ra@yandex.ru>
2025-02-14 15:31:18 +0300

    BJS2-58250 checkstyle.

--- src/main/java/school/faang/bjs2_58250/Main.java
+++ src/main/java/school/faang/bjs2_58250/Main.java
@@ -11,7 +11,7 @@ public class Main {
         System.out.println("Sum of even numbers : " + ListOperations.sumOfEvenNumbers(numbers));
         System.out.println("Maximum: " + ListOperations.findMax(numbers));
         System.out.println("Average: " + ListOperations.findAverage(numbers));
-        System.out.println("Number of lines starting with 'a': " + ListOperations.countStringsStartingWith(strings, 'a'));
+        System.out.println("Number of 'a'-starting lines: " + ListOperations.countStringsStartingWith(strings, 'a'));
         System.out.println("Lines containing 'an': " + ListOperations.filterStringsContainingSubstring(strings, "an"));
         System.out.println("Sorted by string length: " + ListOperations.sortByLength(strings));
         System.out.println("Are all numbers even? " + ListOperations.allMatchCondition(numbers, n -> n % 2 == 0));
