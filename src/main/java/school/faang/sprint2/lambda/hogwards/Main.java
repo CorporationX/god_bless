d2a5eabb6e64b193938f6301ef985850694bee3c
elsledkov <elsledkov@gmail.com>
2025-02-14 15:35:26 +0300

    refactor 2

--- src/main/java/school/faang/sprint2/lambda/hogwards/Main.java
+++ src/main/java/school/faang/sprint2/lambda/hogwards/Main.java
@@ -4,7 +4,7 @@ public class Main {
     public static void main(String[] args) {
         SpellCaster spellCaster = new SpellCaster();
         spellCaster.cast("Lumos", (spell) -> "A beam of light is created by " + spell);
-        System.out.println();
         spellCaster.cast("Alohomora", (spell) -> "The door is unlocked by " + spell);
+
     }
 }
