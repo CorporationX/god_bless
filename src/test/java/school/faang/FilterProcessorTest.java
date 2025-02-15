488cce3ba65734e7c1f1fd3f6707ea656bc1b157
KAS1990 <34664788+KAS1990@users.noreply.github.com>
2025-02-16 00:54:27 +0300

    test: Initial

---
+++ src/test/java/school/faang/FilterProcessorTest.java
@@ -0,0 +1,21 @@
+package school.faang;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class FilterProcessorTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @Test
+    void applyFilter() {
+    }
+
+    @Test
+    void combineFilters() {
+    }
+}
