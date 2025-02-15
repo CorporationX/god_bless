dc05722bbc43e4f06fbc453e7063b2d811b312a8
saroyangor <dr.saroyan98@gmail.com>
2025-02-15 18:32:10 +0400

    BJS2-57756: fix comments

--- src/main/java/BJS2_57756/Main.java
+++ src/main/java/BJS2_57756/Main.java
@@ -1,21 +1,25 @@
 package BJS2_57756;
 
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
 public class Main {
     public static void main(String[] args) {
         NotificationManager notificationManager = new NotificationManager();
 
-        notificationManager.registerHandler(NotificationType.EMAIL,
-                                            notification -> System.out.println("Email: " + notification.getMessage())
+        notificationManager.registerHandler(
+                NotificationType.EMAIL,
+                notification -> log.info("Email: {}", notification.getMessage())
         );
 
-        notificationManager.registerHandler(NotificationType.SMS,
-                                            notification -> System.out.println("SMS: " + notification.getMessage())
+        notificationManager.registerHandler(
+                NotificationType.SMS,
+                notification -> log.info("SMS: {}", notification.getMessage())
         );
 
-        notificationManager.registerHandler(NotificationType.PUSH,
-                                            notification -> {
-                                                System.out.println("Push Notification: " + notification.getMessage());
-                                            }
+        notificationManager.registerHandler(
+                NotificationType.PUSH,
+                notification -> log.info("Push Notification: {}", notification.getMessage())
         );
 
         Notification emailNotification = new Notification(NotificationType.EMAIL, "Ваш аккаунт активирован");
--- src/main/java/BJS2_57756/NotificationManager.java
+++ src/main/java/BJS2_57756/NotificationManager.java
@@ -5,13 +5,10 @@ import java.util.List;
 import java.util.Map;
 import java.util.function.Consumer;
 import java.util.function.Function;
-import java.util.function.Predicate;
 
 public class NotificationManager {
     private final Map<NotificationType, Consumer<Notification>> consumers = new HashMap<>();
     private final List<String> badwordsList = List.of("fuck", "suicide", "bitch");
-    private final Predicate<Notification> predicate = notification -> badwordsList.stream().anyMatch(
-            badword -> notification.getMessage().contains(badword));
     private final Function<Notification, Notification> notificationChanger = notification
             -> new Notification(notification.getType(), notification.getMessage() + " (c) Company name");
 
@@ -21,10 +18,16 @@ public class NotificationManager {
 
     public void sendNotification(Notification notification) {
         notification = notificationChanger.apply(notification);
-        if (predicate.test(notification)) {
-            throw new IllegalArgumentException("This message contains bad words");
-        }
+        Notification finalNotification = notification;
+        badwordsList.stream()
+                .filter(badWord -> finalNotification.getMessage().contains(badWord))
+                .findFirst()
+                .ifPresent(badWord -> {
+                    throw new IllegalArgumentException("This message contains a bad word: " + badWord);
+                });
 
-        consumers.get(notification.getType()).accept(notification);
+        if (consumers.get(notification.getType()) != null) {
+            consumers.get(notification.getType()).accept(notification);
+        }
     }
 }
