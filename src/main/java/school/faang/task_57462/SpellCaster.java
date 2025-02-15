767608045e104688a79582261b8ede89742785a4
Sergei Ananev <sa.ananyev@yandex.ru>
2025-02-15 23:21:18 +0300

    BJS2-57462 added checking for null in class SpellCaster

--- src/main/java/school/faang/task_57462/SpellCaster.java
+++ src/main/java/school/faang/task_57462/SpellCaster.java
@@ -1,7 +1,9 @@
 package school.faang.task_57462;
 
+import lombok.NonNull;
+
 public class SpellCaster {
-    public void cast(String spellTitle, SpellAction spellAction) {
+    public void cast(@NonNull String spellTitle, @NonNull SpellAction spellAction) {
         System.out.println(spellAction.action(spellTitle));
     }
 }
