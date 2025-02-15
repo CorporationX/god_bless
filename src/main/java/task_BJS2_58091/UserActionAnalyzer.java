d06248a76618e617aa4b1a5f7f0add71a760c627
Anton Lukyanchikov <antontotenkopf@gmail.com>
2025-02-15 13:25:42 +0300

    made method topActiveUsers(), topPopularHashtags()

--- src/main/java/task_BJS2_58091/UserActionAnalyzer.java
+++ src/main/java/task_BJS2_58091/UserActionAnalyzer.java
@@ -1,4 +1,39 @@
 package task_BJS2_58091;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+
 public class UserActionAnalyzer {
+
+    public static List<String> topActiveUsers(List<UserAction> actions, int limit) {
+        Map<String, Long> map = actions.stream()
+                .collect(Collectors.groupingBy(UserAction::getName, Collectors.counting()));
+
+        return map.entrySet().stream()
+                .sorted(Map.Entry.<String, Long>comparingByValue().reversed())
+                .limit(limit)
+                .map(Map.Entry::getKey)
+                .toList();
+    }
+
+    public static List<String> topPopularHashtags(List<UserAction> actions, int limit) {
+        Map<String, Long> words = actions.stream()
+                .filter(userAction -> userAction.getContent() != null)
+                .filter(userAction -> ActionType.POST.equals(userAction.getActionType())
+                        || ActionType.COMMENT.equals(userAction.getActionType()))
+                .flatMap(userAction -> Arrays.stream(userAction.getContent().split("\\\\s+")))
+                .filter(word -> word.startsWith("#"))
+                .collect(Collectors.groupingBy(Function.identity(), Collectors.counting()));
+
+        return words.entrySet().stream()
+                .sorted(Map.Entry.<String, Long>comparingByValue().reversed())
+                .limit(limit)
+                .map(Map.Entry::getKey)
+                .toList();
+    }
 }
+
