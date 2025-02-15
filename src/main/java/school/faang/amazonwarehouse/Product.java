79d396fa90808c83cfb1900aba6c54050c957f35
phtanya <112714509+phtanya@users.noreply.github.com>
2025-02-15 02:07:05 +0100

    BJS2-57127

--- src/main/java/school/faang/amazonwarehouse/Product.java
+++ src/main/java/school/faang/amazonwarehouse/Product.java
@@ -1,42 +1,15 @@
 package school.faang.amazonwarehouse;
 
-import java.util.Objects;
+import lombok.Getter;
+import lombok.NonNull;
+import lombok.ToString;
+import lombok.AllArgsConstructor;
 
+@AllArgsConstructor
+@Getter
+@ToString
 public class Product {
     private final int id;
-    private static int counter = 0;
-    private final String name;
-    private final Category category;
-
-    public Product(String name, Category category) {
-        this.id = ++counter;
-        this.name = name;
-        this.category = category;
-    }
-
-    public int getId() {
-        return id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public Category getCategory() {
-        return category;
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (o == null || getClass() != o.getClass()) {
-            return false;
-        }
-        Product product = (Product) o;
-        return id == product.id && Objects.equals(name, product.name) && category == product.category;
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(id, name, category);
-    }
+    @NonNull private final String name;
+    @NonNull private final Category category;
 }
--- src/main/java/school/faang/amazonwarehouse/ProductManager.java
+++ src/main/java/school/faang/amazonwarehouse/ProductManager.java
@@ -11,19 +11,31 @@ import java.util.Set;
 public class ProductManager {
     private final Set<Product> products = new HashSet<>();
     private final Map<Category, List<Product>> categoryMap = new HashMap<>();
-
+    private int currentId;
 
     public void addProduct(Category category, String name) {
-        Product product = new Product(name, category);
+        Product product = new Product(currentId++, name, category);
         products.add(product);
         categoryMap.putIfAbsent(category, new ArrayList<>());
         categoryMap.get(category).add(product);
     }
 
     public void removeProduct(Category category, String name) {
-        Product product = new Product(name, category);
-        categoryMap.get(category).remove(product);
-        products.remove(product);
+        Product productToRemove = null;
+        for (Product product : products) {
+            if (product.getName().equals(name) && product.getCategory().equals(category)) {
+                productToRemove = product;
+                break;
+            }
+        }
+        if (productToRemove != null) {
+            products.remove(productToRemove);
+            List<Product> categoryProducts = categoryMap.get(category);
+            categoryProducts.remove(productToRemove);
+            System.out.println("Product " + productToRemove + " removed");
+        } else {
+            System.out.println("Product " + productToRemove + " not found.");
+        }
     }
 
     public List<Product> findProductsByCategory(Category category) {
@@ -34,8 +46,7 @@ public class ProductManager {
         Map<Category, List<Product>> productMap = new HashMap<>();
         for (Product product : products) {
             Category category = product.getCategory();
-            productMap.putIfAbsent(category, new ArrayList<>());
-            productMap.get(category).add(product);
+            productMap.computeIfAbsent(category, k -> new ArrayList<>()).add(product);
         }
         return productMap;
     }
@@ -44,7 +55,7 @@ public class ProductManager {
         for (Map.Entry<Category, List<Product>> entry : categoryMap.entrySet()) {
             System.out.println("Category: " + entry.getKey());
             for (Product product : entry.getValue()) {
-                System.out.println(" - " + product.getName());
+                System.out.println(" - " + product);
             }
         }
     }
