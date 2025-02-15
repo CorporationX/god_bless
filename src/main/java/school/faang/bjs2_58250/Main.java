c53d4f7c235527825129c0eb917261e3ac6de8ed
Savchenko <savchenko.ra@yandex.ru>
2025-02-15 18:27:22 +0300

    BJS2-58250 refactoring v2.

--- src/main/java/school/faang/bjs2_58250/Main.java
+++ src/main/java/school/faang/bjs2_58250/Main.java
@@ -5,6 +5,7 @@ import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
+
         List<Integer> numbers = Arrays.asList(1, 2, 3, 4, 5, 6);
         List<String> strings = Arrays.asList("apple", "banana", "cherry", "date");
 
