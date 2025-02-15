2bc8f3fe8a1265203a248793bb5568f1be7ea498
Anton Lukyanchikov <antontotenkopf@gmail.com>
2025-02-16 00:58:13 +0300

    refactored task BJS2-60546

--- src/main/java/task_BJS2_60546/ListOperations.java
+++ src/main/java/task_BJS2_60546/ListOperations.java
@@ -1,5 +1,7 @@
 package task_BJS2_60546;
 
+import lombok.NonNull;
+
 import java.util.Comparator;
 import java.util.List;
 import java.util.NoSuchElementException;
@@ -8,57 +10,60 @@ import java.util.stream.Collectors;
 
 public class ListOperations {
 
-    public static int getMaxNumber(List<Integer> numbers) {
+    public static int getMaxNumber(@NonNull List<Integer> numbers) {
         return numbers.stream()
                 .mapToInt(number -> number)
                 .max()
-                .orElseThrow(() -> new NoSuchElementException("Список пуст"));
+                .orElseThrow(() -> new NoSuchElementException("Ошибка на уровне max()\nПричина: Список пуст"));
     }
 
-    public static int getSumOfEvenNumbers(List<Integer> numbers) {
+    public static int getSumOfEvenNumbers(@NonNull List<Integer> numbers) {
         return numbers.stream()
                 .filter(number -> number % 2 == 0)
                 .reduce(0, Integer::sum);
     }
 
-    public static double getAverageNumber(List<Integer> numbers) {
+    public static double getAverageNumber(@NonNull List<Integer> numbers) {
         return numbers.stream()
                 .mapToDouble(number -> number)
                 .average()
-                .orElseThrow(() -> new NoSuchElementException("Список пуст"));
+                .orElseThrow(() -> new NoSuchElementException("Ошибка на уровне average()\n" +
+                        "Причина: Список пуст"));
     }
 
-    public static long countStringsStartingWith(List<String> strings, char symbol) {
+    public static long countStringsStartingWith(@NonNull List<String> strings, char symbol) {
         return strings.stream()
                 .filter(string -> string.startsWith(String.valueOf(symbol)))
                 .count();
     }
 
-    public static List<String> filterStringsContainingSubstring(List<String> strings, String substring) {
+    public static List<String> filterStringsContainingSubstring(@NonNull List<String> strings,
+                                                                @NonNull String substring) {
         return strings.stream()
                 .filter(string -> string.contains(substring))
                 .collect(Collectors.toList());
     }
 
-    public static List<String> sortByLength(List<String> strings) {
+    public static List<String> sortByLength(@NonNull List<String> strings) {
         return strings.stream()
                 .sorted(Comparator.comparingInt(String::length))
                 .toList();
     }
 
-    public static boolean allMatchCondition(List<Integer> numbers, Predicate<Integer> condition) {
+    public static boolean allMatchCondition(@NonNull List<Integer> numbers, Predicate<Integer> condition) {
         return numbers.stream()
                 .allMatch(condition);
     }
 
-    public static int findMinGreaterThan(List<Integer> numbers, int targetNumber) {
+    public static int findMinGreaterThan(@NonNull List<Integer> numbers, int targetNumber) {
         return numbers.stream()
                 .filter(number -> number > targetNumber)
                 .min(Integer::compareTo)
-                .orElseThrow(() -> new NoSuchElementException("В списке нет числа больше " + targetNumber));
+                .orElseThrow(() -> new NoSuchElementException("Ошибка на уровне min()\nВ списке нет числа больше "
+                        + targetNumber));
     }
 
-    public static List<Integer> convertToLengths(List<String> strings) {
+    public static List<Integer> convertToLengths(@NonNull List<String> strings) {
         return strings.stream()
                 .map(String::length)
                 .sorted()
