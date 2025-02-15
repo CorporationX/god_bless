078ec142c8ac1bd3c10737c834115c340d3882b0
amanzholtanat <amanzholtanat@gmail.com>
2025-02-15 22:47:50 +0500

    Gmail Rich Filters - BJS2-59684

--- src/main/java/school/faang/gmail/Main.java
+++ src/main/java/school/faang/gmail/Main.java
@@ -2,9 +2,6 @@ package school.faang.gmail;
 
 import java.util.Arrays;
 import java.util.List;
-import java.util.function.Consumer;
-import java.util.function.Function;
-import java.util.function.Predicate;
 
 public class Main {
 
