5bff299a117b478db3d1409db721bdff9b7d814f
Sergei Ananev <sa.ananyev@yandex.ru>
2025-02-15 21:33:54 +0300

    BJS2-57803 created FilterProcessor for Image

---
+++ src/main/java/school/faang/task_57803/FilterProcessor.java
@@ -0,0 +1,14 @@
+package school.faang.task_57803;
+
+import java.util.function.Function;
+
+public class FilterProcessor {
+    public Image applyFilter(Image image, Function<Image, Image> filter) {
+        return filter.apply(image);
+    }
+
+    public Function<Image, Image> combineFilters(Function<Image, Image> firstFilter,
+                                                 Function<Image, Image> secondFilter) {
+        return image -> secondFilter.apply(firstFilter.apply(image));
+    }
+}
