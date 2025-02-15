b214b4bbca054d95dd7f72201b234d474326be57
Vladimir Mamaev <ipmamaev.v@gmail.com>
2025-02-15 18:08:16 +0300

    Удалены комментарии, добавлены null check

--- src/main/java/bjs257813/FilterProcessor.java
+++ src/main/java/bjs257813/FilterProcessor.java
@@ -2,16 +2,17 @@ package bjs257813;
 
 import lombok.Getter;
 
+import java.util.Objects;
 import java.util.function.Function;
 
 @Getter
 public class FilterProcessor {
     public Image applyFilter(Image image, Function<Image, Image> converter) {
-        return converter.apply(image);
+        return Objects.requireNonNull(converter.apply(image));
     }
 
     public Function<Image, Image> combineFilters(Function<Image, Image> filterBefore,
                                                  Function<Image, Image> filterAfter) {
-        return filterBefore.andThen(filterAfter);
+        return Objects.requireNonNull(filterBefore.andThen(filterAfter));
     }
 }
--- src/main/java/bjs257813/Main.java
+++ src/main/java/bjs257813/Main.java
@@ -8,24 +8,19 @@ public class Main {
 
         Image originalImage = new Image("original.jpg", "Original image");
 
-        // Фильтры
+
         Function<Image, Image> grayscaleFilter = (image)
                 -> new Image(image.getName(), image.getDescription() + " | Filter: black and white");
         Function<Image, Image> sepiaFilter = (image)
                 -> new Image(image.getName(), image.getDescription() + " | Filter: sepia");
 
-        // Применение фильтров
         Image grayscaleImage = filterProcessor.applyFilter(originalImage, grayscaleFilter);
-        System.out.println(grayscaleImage.getDescription()); // Вывод: Оригинальное изображение | Фильтр: черно-белый
-
+        System.out.println(grayscaleImage.getDescription());
         Image sepiaImage = filterProcessor.applyFilter(grayscaleImage, sepiaFilter);
         System.out.println(sepiaImage.getDescription());
-        // Вывод: Оригинальное изображение | Фильтр: черно-белый | Фильтр: сепия
 
-        // Комбинирование фильтров
         Function<Image, Image> combinedFilter = filterProcessor.combineFilters(grayscaleFilter, sepiaFilter);
         Image combinedImage = filterProcessor.applyFilter(originalImage, combinedFilter);
         System.out.println(combinedImage.getDescription());
-        // Вывод: Оригинальное изображение | Фильтр: черно-белый | Фильтр: сепия
     }
 }
