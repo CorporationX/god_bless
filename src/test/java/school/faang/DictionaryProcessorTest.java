670ea131ceb837ff9d6cf4b65cc0d5f203fa01b4
KAS1990 <34664788+KAS1990@users.noreply.github.com>
2025-02-16 00:00:14 +0300

    tests: Added tests

---
+++ src/test/java/school/faang/DictionaryProcessorTest.java
@@ -0,0 +1,62 @@
+package school.faang;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.function.BiConsumer;
+
+class DictionaryProcessorTest {
+    private DictionaryProcessor sut;
+    private Map<String, String> dictionaryForTest;
+    BiConsumer<String, String> addWordToDictionary;
+
+    @BeforeEach
+    void setUp() {
+        sut = new DictionaryProcessor();
+        dictionaryForTest = new HashMap<>();
+        addWordToDictionary = dictionaryForTest::put;
+    }
+
+    @Test
+    void processWord() {
+        // Arrange
+        String word = "привет";
+        String translation = "hello";
+
+        // Act
+        sut.processWord(word, translation, addWordToDictionary);
+
+        // Assert
+        Assertions.assertEquals(1, dictionaryForTest.size());
+        Assertions.assertTrue(dictionaryForTest.containsKey(word));
+        Assertions.assertEquals(translation, dictionaryForTest.get(word));
+    }
+
+    @Test
+    void processSomeWords() {
+        // Arrange
+        String[] words = {"привет", "мир"};
+        String[] translations = {"hello", "world"};
+
+        // Act
+        for (int i = 0; i < words.length; i++) {
+            String word = words[i];
+            String translation = translations[i];
+
+            sut.processWord(word, translation, addWordToDictionary);
+        }
+
+        // Assert
+        Assertions.assertEquals(words.length, dictionaryForTest.size());
+        for (int i = 0; i < words.length; i++) {
+            String word = words[i];
+            String translation = translations[i];
+
+            Assertions.assertTrue(dictionaryForTest.containsKey(word));
+            Assertions.assertEquals(translation, dictionaryForTest.get(word));
+        }
+    }
+}
