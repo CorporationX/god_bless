994a56feae6903637d993c71ca01102b3eee257e
KAS1990 <34664788+KAS1990@users.noreply.github.com>
2025-02-15 13:33:32 +0300

    refactor: Fixed reviewer issues

--- src/main/java/school/faang/Main.java
+++ src/main/java/school/faang/Main.java
@@ -2,26 +2,22 @@ package school.faang;
 
 public class Main {
     public static void main(String[] args) {
-        try {
-            User user = new User("", 25, "Google", "London");
-        } catch (Exception ex) {
-            System.out.println(ex.getMessage());
-        }
+        // Проверка на то, что имя не должно быть пустым
+        tryCreateUser("", 25, "Google", "London");
 
-        try {
-            User user = new User("Alex", 17, "Google", "London");
-        } catch (Exception ex) {
-            System.out.println(ex.getMessage());
-        }
+        // Проверка на то, что возраст должен быть больше 17
+        tryCreateUser("Alex", 17, "Google", "London");
 
-        try {
-            User user = new User("Alex", 18, "VKontakte", "London");
-        } catch (Exception ex) {
-            System.out.println(ex.getMessage());
-        }
+        // Проверка на то, что работа должна быть одной из возможных
+        tryCreateUser("Alex", 18, "VKontakte", "London");
 
+        // Проверка на то, что адрес должен быть одним из возможных
+        tryCreateUser("Alex", 18, "Google", "Moscow");
+    }
+
+    private static void tryCreateUser(String name, int age, String job, String address) {
         try {
-            User user = new User("Alex", 18, "Google", "Moscow");
+            new User(name, age, job, address);
         } catch (Exception ex) {
             System.out.println(ex.getMessage());
         }
