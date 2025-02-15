16c2b291ce5efd3b570fb7744b049b00554eb494
KAS1990 <34664788+KAS1990@users.noreply.github.com>
2025-02-15 19:16:47 +0300

    feat: Array is reversed in-place

--- src/main/java/school/faang/Example.java
+++ src/main/java/school/faang/Example.java
@@ -2,24 +2,17 @@ package school.faang;
 
 public class Example {
     public static int[] reverse(int[] nums) {
-        int[] result = new int[nums.length];
-
-        if (result.length == 0) {
-            return result;
+        if (nums == null || nums.length == 0) {
+            return nums;
         }
 
-        int middleIndex = nums.length >> 1;
-        for (int leftIndex = 0; leftIndex <= middleIndex; leftIndex++) {
-            int rightIndex = nums.length - 1 - leftIndex;
-
-            result[leftIndex] = nums[rightIndex];
-            result[rightIndex] = nums[leftIndex];
+        int middleIndex = nums.length / 2;
+        for (int leftIndex = 0, rightIndex = nums.length - 1; leftIndex < middleIndex; leftIndex++, rightIndex--) {
+            int leftIndexValue = nums[leftIndex];
+            nums[leftIndex] = nums[rightIndex];
+            nums[rightIndex] = leftIndexValue;
         }
 
-        if (nums.length % 2 != 0) {
-            result[middleIndex + 1] = nums[middleIndex + 1];
-        }
-
-        return result;
+        return nums;
     }
 }
--- src/main/java/school/faang/Main.java
+++ src/main/java/school/faang/Main.java
@@ -5,17 +5,17 @@ import java.util.Arrays;
 public class Main {
     public static void main(String[] args) {
         int[] nums = new int[] { 1, 2, 3, 4, 5 };
-        int[] reversedArray = Example.reverse(nums);
+        int[] reversedArray = Example.reverse(Arrays.copyOf(nums, nums.length));
 
         System.out.printf("%s -> %s%n", Arrays.toString(nums), Arrays.toString(reversedArray));
 
         nums = new int[] { 1, 2, 3, 4 };
-        reversedArray = Example.reverse(nums);
+        reversedArray = Example.reverse(Arrays.copyOf(nums, nums.length));
 
         System.out.printf("%s -> %s%n", Arrays.toString(nums), Arrays.toString(reversedArray));
 
         nums = new int[0];
-        reversedArray = Example.reverse(nums);
+        reversedArray = Example.reverse(Arrays.copyOf(nums, nums.length));
 
         System.out.printf("%s -> %s%n", Arrays.toString(nums), Arrays.toString(reversedArray));
     }
