1d90a73b9bd9cf656b62c12e362d017003c51831
Valerii Enin <enin_v_a@mail.ru>
2025-02-15 19:07:38 +0300

    BJS2-57787. Train StreamApi 2

--- src/main/java/school/faang/train_streamapi2/Main.java
+++ src/main/java/school/faang/train_streamapi2/Main.java
@@ -1,43 +1,50 @@
 package school.faang.train_streamapi2;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.HashSet;
+import lombok.NonNull;
+
+import java.util.Comparator;
 import java.util.List;
+import java.util.Map;
 import java.util.Set;
 
 public class Main {
 
+    public static List<String> getUniquePairs(@NonNull Set<Integer> initialSet, int aimNumber) {
+        return initialSet.stream()
+                .flatMap(num1 -> initialSet.stream()
+                        .filter(num2 -> num1 + num2 == aimNumber && num1 < num2)
+                        .map(num2 -> String.format("(%d, %d)", num1, num2)))
+                .toList();
+    }
+
+    public static List<String> getCapitals(@NonNull Map<String, String> inputMap) {
+        return inputMap.entrySet().stream().sorted(Map.Entry.comparingByKey())
+                .map(Map.Entry::getValue).toList();
+    }
+
+    public static List<String> getIntegerToBinaryString(@NonNull List<String> listStrings, Character startCharacter) {
+        return listStrings.stream().filter(s -> s.startsWith("" + startCharacter))
+                .sorted(Comparator.comparingInt(String::length))
+                .toList();
+    }
+
+    public static List<String> getIntegerToBinaryString(@NonNull List<Integer> listInteger) {
+        return listInteger.stream().map(Integer::toBinaryString).toList();
+    }
+
+    public static List<String> getFilteredWords(@NonNull List<String> listWords, @NonNull String alphabet) {
+        return listWords.stream().filter(s ->
+                        s.chars().allMatch(ch -> alphabet.indexOf(ch) != -1))
+                .sorted(Comparator.comparingInt(String::length))
+                .toList();
+    }
+
+    @SuppressWarnings("checkstyle:LineLength")
     public static void main(String[] args) {
-
-        var initSet = Set.of(1, 2, 3, 4, 5, 6);
-        int aim = 6;
-
-        var list = new ArrayList<>(initSet);
-        Set<List<Integer>> uniquePairs = new HashSet<>();
-
-        /*for (Integer num : initSet) {
-            var comp = aim - num;
-            if (initSet.contains(comp) && comp > num) {
-                uniquePairs.add(List.of(num, comp));
-            }
-        }
-        System.out.println(uniquePairs);*/
-
-
-        /*for (int i = 0; i < list.size(); i++) {
-            for (int j = i + 1; j < list.size(); j++) {
-                if (list.get(i) + list.get(j) == 6) {
-                    //map.putIfAbsent(list.get(list.get(j)), j);
-                    System.out.println(String.format("[i] = %d, [j] = %d", list.get(i), list.get(j)));
-                }
-            }
-        }*/
-
-        initSet.stream()
-                .peek(num1 -> System.out.println("Внешний поток: num1 = " + num1))
-                .flatMap(num1 -> initSet.stream()
-                    .peek(num2 -> System.out.println("   Внутренний поток: num1 = " + num1 + "; num2 = " + num2))
-                        .map(num2 -> num2)).toList();
+        System.out.println(getFilteredWords(List.of("apple", "banana", "avocado", "apricote"), "abcdefghijklmnopqrstuvwxyz"));
+        System.out.println(getIntegerToBinaryString(List.of("apple", "banana", "avocado", "apricote"), 'a'));
+        System.out.println(getUniquePairs(Set.of(1, 2, 3, 4, 5, 6), 6));
+        System.out.println(getCapitals(Map.of("Russia", "Moscow", "USA", "Washington", "Germany", "Berlin")));
+        System.out.println(getIntegerToBinaryString(List.of(1, 2, 3, 4)));
     }
 }
