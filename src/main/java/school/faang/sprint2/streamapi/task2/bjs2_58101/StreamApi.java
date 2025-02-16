2f3c8bfc4a4463df5d9afbdb4c60406e2afb25a2
elsledkov <elsledkov@gmail.com>
2025-02-16 15:22:55 +0300

    first commit

---
+++ src/main/java/school/faang/sprint2/streamapi/task2/bjs2_58101/StreamApi.java
@@ -0,0 +1,10 @@
+package school.faang.sprint2.streamapi.task2.bjs2_58101;
+
+import java.util.Set;
+
+public class StreamApi {
+    public static void main(String[] args) {
+        Set<Integer> integerSet = Set.of(2, 6, 7, 5, -4, 0, 10, 3, 1, 13, 12);
+        /*integerSet.stream().filter((x, y) -> x + y == 8),*/
+    }
+}
