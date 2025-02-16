03044fe747850de8e8be76c5bb02714c1dbd88e2
Winrida <winridas@gmail.com>
2025-02-16 13:40:12 +0300

    BJS2-57532 Хогвартс, Хогвартс, наш любимый Хогвартс Правки
    https://faang-school.atlassian.net/browse/BJS2-57532

--- src/main/java/school/faang/task57532/spell/SpellCaster.java
+++ src/main/java/school/faang/task57532/spell/SpellCaster.java
@@ -1,7 +1,9 @@
 package school.faang.task57532.spell;
 
+import lombok.NonNull;
+
 public class SpellCaster {
-    public void cast(String spellName, SpellAction spellAction) {
+    public void cast(@NonNull String spellName, @NonNull SpellAction spellAction) {
         System.out.println(spellAction.spell(spellName));
     }
 }
