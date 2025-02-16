03decf1adfbad30a73fb2ce6bda363e97eec231b
Sergey Ermoshenko <ermoshenko.work@gmail.com>
2025-02-16 23:42:29 +0300

    add shutdownNow

--- src/main/java/school/faang/GriffinsFoodDelivery.java
+++ src/main/java/school/faang/GriffinsFoodDelivery.java
@@ -3,6 +3,7 @@ package school.faang;
 import java.util.Random;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
 
 public class GriffinsFoodDelivery {
     private static final int THREAD_POOL_SIZE = 3;
@@ -15,6 +16,15 @@ public class GriffinsFoodDelivery {
         for (String name : characterNames) {
             executorService.execute(new FoodDeliveryTask(name, random.nextInt(100) + 1));
         }
+
         executorService.shutdown();
+        try {
+            if (!executorService.awaitTermination(10, TimeUnit.SECONDS)){
+                System.out.println("Processes haven't been completed in time.");
+                executorService.shutdownNow();
+            }
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
