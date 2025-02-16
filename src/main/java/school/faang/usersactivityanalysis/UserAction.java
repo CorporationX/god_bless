7426bb0dcb2f165c1409c55731679690c6c56ed2
Marsel <mars1212@inbox.ru>
2025-02-16 21:53:24 +0300

    BJS2-58053 Анализ активности пользователей в социальной сети

---
+++ src/main/java/school/faang/usersactivityanalysis/UserAction.java
@@ -0,0 +1,19 @@
+package school.faang.usersactivityanalysis;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+import java.time.LocalDate;
+
+@Getter
+@AllArgsConstructor
+public class UserAction {
+    private final int id;
+    private final String name;
+    private final ActionType actionType;
+    private final LocalDate actionDate;
+    private final String content;
+
+
+
+}
---
+++ src/main/java/school/faang/usersactivityanalysis/UserActionAnalyzer.java
@@ -0,0 +1,71 @@
+package school.faang.usersactivityanalysis;
+
+import lombok.NonNull;
+
+import java.time.LocalDate;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+
+public class UserActionAnalyzer {
+    public List<String> findTopActiveUsers(@NonNull List<UserAction> actions, int topUsersN) {
+        Map<String, Long> userActionCounter = actions.stream()
+                .collect(Collectors.groupingBy(UserAction::getName, Collectors.counting()));
+
+        List<String> topUsers = userActionCounter.entrySet().stream()
+                .sorted(Map.Entry.<String, Long>comparingByValue().reversed())
+                .limit(topUsersN)
+                .map(Map.Entry::getKey)
+                .toList();
+        return topUsers;
+    }
+
+    public List<String> findTopPopularContent(@NonNull List<UserAction> actions, int topContentN) {
+        Map<String, Long> contentCounter = actions.stream()
+                .filter(action -> action.getContent() != null
+                        && ActionType.COMMENT.equals(action.getActionType())
+                        || ActionType.POST.equals(action.getActionType()))
+                .flatMap(action -> Arrays.stream(action.getContent().split("\\s+")))
+                .filter(word -> word.startsWith("#"))
+                .map(word -> word.replaceAll("[^#\\w]", ""))
+                .collect(Collectors.groupingBy(Function.identity(), Collectors.counting()));
+
+        List<String> topContents = contentCounter.entrySet().stream()
+                .sorted(Map.Entry.<String, Long>comparingByValue().reversed())
+                .limit(topContentN)
+                .map(Map.Entry::getKey)
+                .toList();
+        return topContents;
+    }
+
+    public List<String> findMaxUsersWithComment(@NonNull List<UserAction> actions, int topUsersN) {
+        LocalDate oneMonth = LocalDate.now().minusMonths(1);
+        Map<String, Long> userActionCounter = actions.stream()
+                .filter(action -> action.getActionType() != null
+                        && ActionType.COMMENT.equals(action.getActionType())
+                        && action.getActionDate().isBefore(oneMonth))
+                .collect(Collectors.groupingBy(UserAction::getName, Collectors.counting()));
+
+        List<String> topUsers = userActionCounter.entrySet().stream()
+                .sorted(Map.Entry.<String, Long>comparingByValue().reversed())
+                .limit(topUsersN)
+                .map(Map.Entry::getKey)
+                .toList();
+        return topUsers;
+    }
+
+    public Map<ActionType, Double> actionTypePercentages(@NonNull List<UserAction> actions) {
+        long actionSize = actions.size();
+        Map<ActionType, Long> actionTypePercentages = actions.stream()
+                .collect(Collectors.groupingBy(UserAction::getActionType, Collectors.counting()));
+
+        Map<ActionType, Double> actionPercentages = actionTypePercentages.entrySet().stream()
+                .collect(Collectors.toMap(Map.Entry::getKey,
+                        entry -> (entry.getValue() * 100.0) / actionSize
+                ));
+        return actionPercentages;
+    }
+
+}
