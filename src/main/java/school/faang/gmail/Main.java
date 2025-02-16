3dd9e2e58b95f7002e74bade502a0059653c677b
Sofiya Mutovkina <sofiyamutovkina@gmail.com>
2025-02-16 15:35:06 +0300

    исправила код

--- src/main/java/school/faang/gmail/Main.java
+++ src/main/java/school/faang/gmail/Main.java
@@ -11,18 +11,17 @@ public class Main {
         List<Email> emails = new ArrayList<>();
         emails.add(new Email("Important System Update",
                 "Don't forget to update your security system to the latest version.", true));
-        emails.add(new Email("Payment Reminder", "Your payment invoice expires in 3 days. Please pay on time.",
-                true));
+        emails.add(new Email("Payment Reminder", "Your payment invoice expires in 3 days. Please pay on time.", true));
         emails.add(new Email("Urgent! Your Card Has Been Blocked",
                 " Suspicious activity detected. Confirm your details to avoid being blocked.", false));
 
-        Predicate<Email> importantFilter = email -> email.isImportant();
+        Predicate<Email> importantFilter = Email::isImportant;
         Consumer<Email> printEmail = email -> System.out.println("The email has been processed. " +
                 email.getSubject());
-        Function<Email, String> toUpperCase = email -> email.getSubject().toUpperCase();
-        EmailProcessor.processEmails(emails, importantFilter, toUpperCase, printEmail);
+        Function<Email, String> subjectToUpperCase = email -> email.getSubject().toUpperCase();
+        EmailProcessor.processEmails(emails, importantFilter, subjectToUpperCase, printEmail);
 
-        emails.forEach(email -> System.out.println("Subject: " + email.getSubject() + ", the body of the letter:" +
-                email.getBody()));
+        emails.forEach(email -> System.out.println(String.format("Subject: %s, the body of the letter: %s",
+                email.getSubject(), email.getBody())));
     }
 }
