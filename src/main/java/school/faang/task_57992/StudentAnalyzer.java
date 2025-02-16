471da9a6005e61124173f4ec8f0e7a785f142b38
s3nkyh <olkin.yaroslaw@yandex.ru>
2025-02-16 11:30:00 +0300

    .

--- src/main/java/school/faang/task_57992/StudentAnalyzer.java
+++ src/main/java/school/faang/task_57992/StudentAnalyzer.java
@@ -3,7 +3,7 @@ package school.faang.task_57992;
 import java.util.Map;
 
 public class StudentAnalyzer {
-    public Map<> getAverageGrades() {
+    public Map<Student> getAverageGrades() {
 
     }
 }
