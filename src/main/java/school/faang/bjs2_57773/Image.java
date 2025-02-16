7d14fc8430bade9d3dfaaf9606df7bf118f57e84
Anton <basanton@mail.ru>
2025-02-16 14:50:44 +0300

    Выполнение задачи по "Фильтры Instagram" версия 1

---
+++ src/main/java/school/faang/bjs2_57773/Image.java
@@ -0,0 +1,11 @@
+package school.faang.bjs2_57773;
+
+public record Image(String name, String description) {
+    public String getName() {
+        return name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+}
