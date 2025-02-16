78e99373c772621469ee7cf7a5f6015f65945d21
vtroshin <vtroshin@rvision.ru>
2025-02-16 12:23:58 +0300

    BJS2-57975 Доработки

--- src/main/java/school/faang/sprint/second/socialactivity/UserActionAnalyzer.java
+++ src/main/java/school/faang/sprint/second/socialactivity/UserActionAnalyzer.java
@@ -25,7 +25,7 @@ public final class UserActionAnalyzer {
                         UserAction::getName,
                         action -> 1,
                         Integer::sum,
-                        HashMap::new
+                        TreeMap::new
                 ));
 
         return mapToListWithSorting(topActiveUsersMap, top);
@@ -38,7 +38,7 @@ public final class UserActionAnalyzer {
                         userAction -> getHashTag(userAction).get(),
                         action -> 1,
                         Integer::sum,
-                        HashMap::new
+                        TreeMap::new
                 ));
 
         return mapToListWithSorting(topPopularHashtagsMap, top);
