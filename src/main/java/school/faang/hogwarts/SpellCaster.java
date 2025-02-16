bef350c3213ab6d93458c9206e0e7dfe834d28cd
sunmajor <makar.a.v100@gmail.com>
2025-02-16 21:55:26 +0300

    Hogwarts.Hogwarts.

---
+++ src/main/java/school/faang/hogwarts/SpellCaster.java
@@ -0,0 +1,21 @@
+package school.faang.hogwarts;
+
+public class SpellCaster {
+    public void cast(String spellName, SpellAction action) {
+        String result = action.castSpell(spellName);
+        System.out.println("Результат заклинания: " + result);
+    }
+
+    public static void main(String[] args) {
+
+        SpellCaster spellCaster = new SpellCaster();
+
+        String alohomora = "Alohomora";
+        String lumos = "Lumos";
+        String expelliarmus = "Expelliarmus";
+
+        spellCaster.cast(alohomora,spell -> "The door is unlocked by " + spell);
+        spellCaster.cast(lumos, spell -> "A beam of light is created by " + spell);
+        spellCaster.cast(expelliarmus, spell -> "The opponent is disarmed by " + spell);
+    }
+}
