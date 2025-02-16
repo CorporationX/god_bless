581a644f6c9c5029eeeea970720683c2c2dbddf7
Anton <basanton@mail.ru>
2025-02-16 14:27:18 +0300

    Выполнение задачи по "Meta-вселенная?" версия 1

---
+++ src/main/java/school/faang/bjs2_57916/Notification.java
@@ -0,0 +1,21 @@
+package school.faang.bjs2_57916;
+
+import lombok.Getter;
+import lombok.NonNull;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@Getter
+public class Notification {
+    private final NotificationType type;
+    private final String message;
+
+    public Notification(@NonNull NotificationType type, @NonNull String message) {
+        if (message.isBlank()) {
+            log.error("Сообщение не может состоять из пробелов");
+            throw new IllegalArgumentException("Сообщение не может состоять из пробелов");
+        }
+        this.type = type;
+        this.message = message;
+    }
+}
---
+++ src/main/java/school/faang/bjs2_57916/NotificationManager.java
@@ -0,0 +1,21 @@
+package school.faang.bjs2_57916;
+
+import lombok.NonNull;
+import lombok.extern.slf4j.Slf4j;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.function.Consumer;
+
+@Slf4j
+public class NotificationManager {
+    private final Map<NotificationType, Consumer<Notification>> notifications = new HashMap<>();
+
+    public void registerHandler(@NonNull NotificationType type, @NonNull Consumer<Notification> handler) {
+        notifications.put(type, handler);
+    }
+
+    public void sendNotification(@NonNull Notification notification) {
+        notifications.get(notification.getType()).accept(notification);
+    }
+}
