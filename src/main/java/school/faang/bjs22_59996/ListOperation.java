320dc24cd018b52841d9074f5cf60cf090d082af
Ekaterina Ramakoti <zubkova-92@mail.ru>
2025-02-16 21:48:27 +0300

    task BJS2-59996

---
+++ src/main/java/school/faang/bjs22_59996/ListOperation.java
@@ -0,0 +1,10 @@
+package school.faang.bjs22_59996;
+
+import java.util.List;
+import java.util.Set;
+
+public class ListOperation {
+    public static List<Integer> findUniquePairsOfNumbers (Set<Integer> manyIntegers, int integer) {
+        return manyIntegers.stream().;
+    }
+}
---
+++ src/main/java/school/faang/bjs22_59996/Main.java
@@ -0,0 +1,7 @@
+package school.faang.bjs22_59996;
+
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
