9fe19e23ef01c0b72b6653a18989f78a6d46da55
Evgeny Morzeev <morzeevevg@gmail.com>
2025-02-16 13:17:52 +0300

    task completed

--- src/main/java/faang/school/BJS2_57346/Main.java
+++ src/main/java/faang/school/BJS2_57346/Main.java
@@ -4,7 +4,7 @@ public class Main {
     public static void main(String[] args) {
         SpellCaster spellCaster = new SpellCaster();
 
-        spellCaster.cast(Spell.ALOHOMORA, (spell) -> "The door is unlocked!");
+        spellCaster.cast(Spell.ALOHOMORA, (spell) -> "The door is unlocked");
         spellCaster.cast(Spell.LUMOS, (spell) -> "A beam of light is created!");
         spellCaster.cast(Spell.EXPELLIARMUS, (spell) -> "The opponent is disarmed!");
     }
