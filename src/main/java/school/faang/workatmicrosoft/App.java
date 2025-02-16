968c513e8e5c89f595db701bbdd4b53a56d49b0d
Anton Kapshukov <kapshukov.04@mail.ru>
2025-02-16 16:14:34 +0300

    Вы работаете в Microsoft!

---
+++ src/main/java/school/faang/workatmicrosoft/App.java
@@ -0,0 +1,18 @@
+package school.faang.workatmicrosoft;
+
+public class App {
+    public static final int EMAILS_AMOUNT_PER_THREAD = 200;
+    public static final int THREADS_AMOUNT = 5;
+
+    public static void main(String[] args) {
+        for (int i = 0; i < THREADS_AMOUNT; i++) {
+            SenderRunnable senderRunnable = new SenderRunnable(
+                    EMAILS_AMOUNT_PER_THREAD * i + 1,
+                    EMAILS_AMOUNT_PER_THREAD * (i + 1) - 1
+            );
+
+            Thread thread = new Thread(senderRunnable);
+            thread.start();
+        }
+    }
+}
---
+++ src/main/java/school/faang/workatmicrosoft/SenderRunnable.java
@@ -0,0 +1,20 @@
+package school.faang.workatmicrosoft;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@AllArgsConstructor
+@Getter
+public class SenderRunnable implements Runnable {
+    private final int startIndex;
+    private final int endIndex;
+
+    @Override
+    public void run() {
+        for (int i = startIndex; i < endIndex; i++) {
+            log.info("Письмо отправлено {}", i);
+        }
+    }
+}
