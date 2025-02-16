d1d23e66ea1f20c700b5169f70752f44c88899e9
KAS1990 <34664788+KAS1990@users.noreply.github.com>
2025-02-16 20:57:06 +0300

    refactor: Fix code style

--- src/test/java/bjs2_57242/MainTest.java
+++ src/test/java/bjs2_57242/MainTest.java
@@ -24,16 +24,8 @@ class MainTest {
     @Test
     void flipMatrix2x2Vertical() {
         // Arrange
-        int[][] matrix =
-                {
-                        {1, 2},
-                        {3, 4}
-                };
-        int[][] expectedResult =
-                {
-                        {3, 4},
-                        {1, 2}
-                };
+        int[][] matrix = { {1, 2}, {3, 4} };
+        int[][] expectedResult = { {3, 4}, {1, 2} };
 
         runTest(matrix, expectedResult, FlipDirection.VERTICAL);
     }
@@ -41,16 +33,8 @@ class MainTest {
     @Test
     void flipMatrix2x2Horizontal() {
         // Arrange
-        int[][] matrix =
-                {
-                        {1, 2},
-                        {3, 4}
-                };
-        int[][] expectedResult =
-                {
-                        {2, 1},
-                        {4, 3}
-                };
+        int[][] matrix = { {1, 2}, {3, 4} };
+        int[][] expectedResult = { {2, 1}, {4, 3} };
 
         runTest(matrix, expectedResult, FlipDirection.HORIZONTAL);
     }
@@ -58,18 +42,8 @@ class MainTest {
     @Test
     void flipMatrix3x3Vertical() {
         // Arrange
-        int[][] matrix =
-                {
-                        {1, 2, 3},
-                        {4, 5, 6},
-                        {7, 8, 9}
-                };
-        int[][] expectedResult =
-                {
-                        {7, 8, 9},
-                        {4, 5, 6},
-                        {1, 2, 3}
-                };
+        int[][] matrix = { {1, 2, 3}, {4, 5, 6}, {7, 8, 9} };
+        int[][] expectedResult = { {7, 8, 9}, {4, 5, 6}, {1, 2, 3} };
 
         runTest(matrix, expectedResult, FlipDirection.VERTICAL);
     }
@@ -77,18 +51,8 @@ class MainTest {
     @Test
     void flipMatrix3x3Horizontal() {
         // Arrange
-        int[][] matrix =
-                {
-                        {1, 2, 3},
-                        {4, 5, 6},
-                        {7, 8, 9}
-                };
-        int[][] expectedResult =
-                {
-                        {3, 2, 1},
-                        {6, 5, 4},
-                        {9, 8, 7}
-                };
+        int[][] matrix = { {1, 2, 3}, {4, 5, 6}, {7, 8, 9} };
+        int[][] expectedResult = { {3, 2, 1}, {6, 5, 4}, {9, 8, 7} };
 
         runTest(matrix, expectedResult, FlipDirection.HORIZONTAL);
     }
