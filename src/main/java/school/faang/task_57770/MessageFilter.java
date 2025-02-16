e33ec37ccec499b5f4dee136d95dfec50f44774b
Yaroslav Olkin <shrelok.pupkin@yandex.ru>
2025-02-16 11:24:46 +0300

    bjs2-57107

---
+++ src/main/java/school/faang/task_57770/MessageFilter.java
@@ -0,0 +1,6 @@
+package school.faang.task_57770;
+
+
+public interface MessageFilter {
+    boolean filter(String message);
+}
---
+++ src/main/java/school/faang/task_57770/MessageProcessor.java
@@ -0,0 +1,5 @@
+package school.faang.task_57770;
+
+public class MessageProcessor {
+    
+}
