4df32d7a8f60594160bda7487dea740b87798342
Sergey Ermoshenko <ermoshenko.work@gmail.com>
2025-02-16 23:37:36 +0300

    add shutdownNow

---
+++ src/main/java/school/faang/BigBangTheory.java
@@ -0,0 +1,32 @@
+package school.faang;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
+
+public class BigBangTheory {
+    private static final int THREAD_POOL_SIZE = 4;
+
+    public static void main(String[] args) {
+        ExecutorService executor = Executors.newFixedThreadPool(THREAD_POOL_SIZE);
+        Task sheldonTask = new Task("Шелдон", "подготовка теории");
+        Task leonardTask = new Task("Леонард", "моделирование эксперимента");
+        Task howardTask = new Task("Говард", "разработка инструментов");
+        Task rajeshTask = new Task("Раджеш", "анализ данных");
+
+        executor.execute(sheldonTask);
+        executor.execute(leonardTask);
+        executor.execute(howardTask);
+        executor.execute(rajeshTask);
+        executor.shutdown();
+
+        try {
+            if (!executor.awaitTermination(10, TimeUnit.SECONDS)){
+                System.out.println("Tasks haven't been completed in time.");
+                executor.shutdownNow();
+            }
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
---
+++ src/main/java/school/faang/Task.java
@@ -0,0 +1,36 @@
+package school.faang;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Task implements Runnable {
+    private static final Logger logger = LoggerFactory.getLogger(Task.class);
+    private final String name;
+    private final String task;
+
+    @Override
+    public void run() {
+        try {
+            logger.info("{} started {}.", name, task);
+            Thread.sleep(2000);
+            logger.info("{} finished {}.", name, task);
+        } catch (InterruptedException e) {
+            logger.info("{} interrupted {}.", name, task);
+            throw new RuntimeException(e);
+        }
+
+    }
+
+    public Task(String name, String task) {
+        validateString(name, "Name");
+        validateString(task, "Task");
+        this.name = name;
+        this.task = task;
+    }
+
+    private void validateString(String name, String message) {
+        if (name == null || name.isBlank()) {
+            throw new IllegalArgumentException(message + " can't be null or blank.");
+        }
+    }
+}
