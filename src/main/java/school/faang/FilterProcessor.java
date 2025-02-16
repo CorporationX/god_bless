d95408b91329f04d772d13e74a54ee5d97c29999
Filippka <Filipp_Chudaev@mail.ru>
2025-02-16 16:08:01 +0500

    решение

--- src/main/java/school/faang/FilterProcessor.java
+++ src/main/java/school/faang/FilterProcessor.java
@@ -1,9 +1,15 @@
 package school.faang;
 
-public class FilterProcessor {
+import java.util.function.Function;
 
-    public Image applyFilter(Image image, FilterFunction filterFunction) {
+class FilterProcessor {
+
+    public Image applyFilter(Image image, Function<Image, Image> filter) {
+        return filter.apply(image);
+    }
 
 
+    public Function<Image, Image> combineFilters(Function<Image, Image> filter1, Function<Image, Image> filter2) {
+        return image -> filter2.apply(filter1.apply(Image));
     }
 }
