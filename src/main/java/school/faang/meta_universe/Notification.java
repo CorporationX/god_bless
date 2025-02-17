2f53415b74e1f3ff5d980e271633c3165bd7c0d7
User Rody4457 <rodyon0212@gmail.com>
2025-02-17 22:44:11 +0400

    задача выполнена и протестирована

---
+++ src/main/java/school/faang/meta_universe/Notification.java
@@ -0,0 +1,15 @@
+package school.faang.meta_universe;
+
+import lombok.Getter;
+
+@Getter
+public class Notification {
+
+    NotificationType type;
+    String message;
+
+    public Notification(NotificationType type, String message) {
+        this.type = type;
+        this.message = message;
+    }
+}
