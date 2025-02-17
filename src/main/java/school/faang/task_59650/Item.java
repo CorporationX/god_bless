2f3714865b5a5315f6de780c13970c45b3b7233d
Артемий <xkmitx@gmail.com>
2025-02-17 13:38:45 +0300

    Task 59650

---
+++ src/main/java/school/faang/task_59650/Item.java
@@ -0,0 +1,11 @@
+package school.faang.task_59650;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class Item {
+    private String name;
+    private int value;
+}
