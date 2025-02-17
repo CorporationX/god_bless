072b0bec2a569a7e2b713e01e7b1803a6b0b9e2f
Sofiya Mutovkina <sofiyamutovkina@gmail.com>
2025-02-17 16:10:19 +0300

    добавила константу и printf

--- src/main/java/school/faang/filter/Main.java
+++ src/main/java/school/faang/filter/Main.java
@@ -3,8 +3,10 @@ package school.faang.filter;
 import java.util.List;
 
 public class Main {
+    private static final int MAX_MESSAGE_LENGTH = 20;
+
     public static void main(String[] args) {
-        MessageFilter lengthFilter = message -> message.length() <= 20;
+        MessageFilter lengthFilter = message -> message.length() <= MAX_MESSAGE_LENGTH;
         MessageFilter spamFilter = message -> !message.toLowerCase().contains("spam");
         MessageFilter offensiveFilter = message -> !message.toLowerCase().matches(".*(badword1|badword2).*");
 
@@ -12,12 +14,12 @@ public class Main {
 
         MessageProcessor messageProcessor = new MessageProcessor();
 
-        String[] testMessages = {"Hello!", "This is a spam message!", "This message contains badword1",
+        String[] testMessages = {"Hello!", "This is a spam message!", "This message contains badword1.",
                 "Hello, you have won a million dollars, click on the link to receive the winnings."};
 
         for (String message : testMessages) {
             boolean isFiltered = messageProcessor.processMessage(message, messageFilters);
-            System.out.println("Message: " + message + "| passed all filters: " + isFiltered);
+            System.out.printf("Message: %s | passed all filters: %b%n", message, isFiltered);
         }
     }
 }
