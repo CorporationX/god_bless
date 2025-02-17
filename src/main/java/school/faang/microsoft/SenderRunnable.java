0b05a051da350f15753035c111c22e4be03a6e8d
Sergey <bigwave2915@gmail.com>
2025-02-17 14:42:00 +0500

    Решение Задачи Поправил Код Спасибо

--- src/main/java/school/faang/microsoft/SenderRunnable.java
+++ src/main/java/school/faang/microsoft/SenderRunnable.java
@@ -4,20 +4,24 @@ import lombok.AllArgsConstructor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+
 @AllArgsConstructor
 public class SenderRunnable implements Runnable {
     private final int startIndex;
     private final int endIndex;
 
     private static final Logger logger = LoggerFactory.getLogger(SenderRunnable.class);
+    // Часто используемые сообщения для логирования
+    private static final String MESSAGE_SENT_TEMPLATE = "Сообщение {} отправлено!";
+    private static final String THREAD_STARTED_MESSAGE = "Новый Поток {} начал работу";
 
     @Override
     public void run() {
-        logger.info("Новый Поток {} начал работу", Thread.currentThread().getName());
+        logger.info(THREAD_STARTED_MESSAGE, Thread.currentThread().getName());
 
         validateIndices(startIndex, endIndex);
         for (int i = startIndex; i < endIndex; i++) {
-            logger.info("Сообщение {} отправлено!", i);
+            logger.info(MESSAGE_SENT_TEMPLATE, i);
         }
     }
 
