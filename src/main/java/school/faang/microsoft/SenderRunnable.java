da3458a56364eceb89fc6ff9672b774908d09c4d
Sergey <bigwave2915@gmail.com>
2025-02-17 10:50:52 +0500

    Решение Задачи

---
+++ src/main/java/school/faang/microsoft/SenderRunnable.java
@@ -0,0 +1,32 @@
+package school.faang.microsoft;
+
+import lombok.AllArgsConstructor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+@AllArgsConstructor
+public class SenderRunnable implements Runnable {
+    private final int startIndex;
+    private final int endIndex;
+
+    private static final Logger logger = LoggerFactory.getLogger(SenderRunnable.class);
+
+    @Override
+    public void run() {
+        logger.info("Новый Паток {} начал работу", Thread.currentThread().getName());
+
+        validateIndices(startIndex, endIndex);
+        for (int i = startIndex; i < endIndex; i++) {
+            logger.info("Сообщение {} отправлено!", i);
+        }
+    }
+
+    private void validateIndices(int startIndex, int endIndex) {
+        if (startIndex < 0 || endIndex < 0) {
+            throw new IllegalArgumentException("Индекс не может быть 0.");
+        } else if (endIndex < startIndex) {
+            throw new IllegalArgumentException("Начальный индекс не может быть больше конечного.");
+        }
+    }
+}
+
