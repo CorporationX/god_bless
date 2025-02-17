e9c18985ee0d3fd6f519fdb708e52205d6c7c55a
Anton Kamyshin <anton.kamyshin@gmail.com>
2025-02-17 18:10:34 +0300

    В соответствии с ваданными рекомендациями внес исправления: выданные 1. В классе Main переменные вынес в статические константы. 2. В классе SpellCaster ввел дополнительную проверку на Null с выбрасыванием эксепшина.

--- src/main/java/school/faang/task_BJS2_57471/Main.java
+++ src/main/java/school/faang/task_BJS2_57471/Main.java
@@ -1,15 +1,15 @@
 package school.faang.task_BJS2_57471;
 
 public class Main {
+    public static final String ALOHOMORA = "Alohomora";
+    public static final String LUMOS = "Lumos";
+    public static final String EXPELLIIARMUS = "Expelliarmus";
+
     public static void main(String[] args) {
         SpellCaster spellCaster = new SpellCaster();
 
-        String alohomora = "Alohomora";
-        String lumos = "Lumos";
-        String expelliarmus = "Expelliarmus";
-
-        spellCaster.cast(alohomora, (spell) -> "The door is unlocked by " + spell);
-        spellCaster.cast(lumos, (spell) -> "A beam of light is created by " + spell);
-        spellCaster.cast(expelliarmus, (spell) -> "The opponent is disarmed by " + spell);
+        spellCaster.cast(ALOHOMORA, (spell) -> "The door is unlocked by " + spell);
+        spellCaster.cast(LUMOS, (spell) -> "A beam of light is created by " + spell);
+        spellCaster.cast(EXPELLIIARMUS, (spell) -> "The opponent is disarmed by " + spell);
     }
 }
--- src/main/java/school/faang/task_BJS2_57471/SpellCaster.java
+++ src/main/java/school/faang/task_BJS2_57471/SpellCaster.java
@@ -2,6 +2,11 @@ package school.faang.task_BJS2_57471;
 
 public class SpellCaster {
     public void cast(String spellName, SpellAction spellAction) {
+
+        if (spellName == null || spellAction == null) {
+            throw new IllegalArgumentException("spellName or spellAction cannot be null");
+        }
+
         String result = spellAction.castSpell(spellName);
         System.out.println(result);
     }
