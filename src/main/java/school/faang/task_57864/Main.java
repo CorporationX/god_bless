ef7f2ad3f86a9acf39a3a76cd829672ced702105
Артемий <xkmitx@gmail.com>
2025-02-17 10:54:33 +0300

    fix

--- src/main/java/school/faang/task_57864/Main.java
+++ src/main/java/school/faang/task_57864/Main.java
@@ -9,12 +9,16 @@ public class Main {
         FilterProcessor filterProcessor = new FilterProcessor();
 
 // Фильтры
-        Function<Image, Image> grayscaleFilter = (image) -> new Image(image.getName(), image.getDescription() + " | Фильтр: черно-белый");
-        Function<Image, Image> sepiaFilter = (image) -> new Image(image.getName(), image.getDescription() + " | Фильтр: сепия");
-        Function<Image, Image> vignetteFilter = (image) -> new Image(image.getName(), image.getDescription() + " | Фильтр: виньетка");
+        Function<Image, Image> grayscaleFilter = (image) ->
+                new Image(image.getName(), image.getDescription() + " | Фильтр: черно-белый");
+        Function<Image, Image> sepiaFilter = (image) ->
+                new Image(image.getName(), image.getDescription() + " | Фильтр: сепия");
+        Function<Image, Image> vignetteFilter = (image) ->
+                new Image(image.getName(), image.getDescription() + " | Фильтр: виньетка");
 
 // Применение фильтров
-        Image grayscaleImage = filterProcessor.applyFilter(originalImage, grayscaleFilter);
+        Image grayscaleImage = filterProcessor.applyFilter(originalImage,
+                grayscaleFilter);
         System.out.println(grayscaleImage.getDescription()); // Вывод: Оригинальное изображение | Фильтр: черно-белый
 
         Image sepiaImage = filterProcessor.applyFilter(grayscaleImage, sepiaFilter);
@@ -24,7 +28,8 @@ public class Main {
         System.out.println(vignetteImage.getDescription()); // Вывод: Оригинальное изображение | Фильтр: виньетка
 
 // Комбинирование фильтров
-        Function<Image, Image> combinedFilter = filterProcessor.combineFilters(grayscaleFilter, sepiaFilter);
+        Function<Image, Image> combinedFilter =
+                filterProcessor.combineFilters(grayscaleFilter, sepiaFilter);
         Image combinedImage = filterProcessor.applyFilter(originalImage, combinedFilter);
         System.out.println(combinedImage.getDescription()); // Вывод: Оригинальное изображение | Фильтр: черно-белый | Фильтр: сепия
     }
