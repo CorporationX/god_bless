017b43d674dc3cea39ec5e97208215b6136d726d
Max Goloviznin <roxet59rus@mail.ru>
2025-02-17 11:35:59 +0500

    BJS2-60696 They were very nice but extremely poor

---
+++ src/main/java/school/faang/theywereverynice/Chore.java
@@ -0,0 +1,22 @@
+package school.faang.theywereverynice;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@Getter
+@AllArgsConstructor
+public class Chore implements Runnable {
+    private String title;
+
+    @Override
+    public void run() {
+        try {
+            log.info("{} execute task: {}", Thread.currentThread().getName(), this.getTitle());
+            Thread.sleep(1000);
+        } catch (InterruptedException exception) {
+            log.error("Thread has been interrupted", exception);
+        }
+    }
+}
---
+++ src/main/java/school/faang/theywereverynice/WeasleyFamily.java
@@ -0,0 +1,33 @@
+package school.faang.theywereverynice;
+
+import lombok.extern.slf4j.Slf4j;
+
+import java.util.List;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+@Slf4j
+public class WeasleyFamily {
+    private static final List<Chore> chores = List.of(
+            new Chore("task 1"), new Chore("task 2"), new Chore("task 3"),
+            new Chore("task 4"), new Chore("task 5"), new Chore("task 6"),
+            new Chore("task 7"), new Chore("task 8"), new Chore("task 9")
+    );
+
+    public static void main(String[] args) {
+        try {
+            ExecutorService executorService = Executors.newCachedThreadPool();
+            if (!chores.isEmpty()) {
+                chores.forEach(executorService::submit);
+            } else {
+                throw new IllegalArgumentException("No chores available");
+            }
+            while (!executorService.isTerminated()) {
+                executorService.shutdown();
+            }
+            log.info("All done");
+        } catch (Exception exception) {
+            log.error(exception.getMessage(), exception);
+        }
+    }
+}
