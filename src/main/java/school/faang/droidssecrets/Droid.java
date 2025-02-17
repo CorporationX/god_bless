4a7e65eeb919f769749081d9d977769f544e97fc
Ilyas Azhdar <azhdar902@gmail.com>
2025-02-17 16:14:54 +0500

    finished ecnrypt and decrypt messages

--- src/main/java/school/faang/droidssecrets/Droid.java
+++ src/main/java/school/faang/droidssecrets/Droid.java
@@ -1,36 +1,53 @@
 package school.faang.droidssecrets;
 
-import java.util.List;
+import lombok.extern.slf4j.Slf4j;
 
-public class Droid {
-    private static final String ALPHABET = "abcdefghijklmnopqrstuvwxyz";
-
-    private final String name;
-    private DroidMessageEncryptor droidMessageEncryptor;
-
-    public Droid(String name) {
-        this.name = name;
-    }
+@Slf4j
+public record Droid(String name) {
 
     private String encryptMessage(String message, int encryptionKey) {
         DroidMessageEncryptor droidMessageEncryptor = (msg, key) -> {
             StringBuilder encryptedMessage = new StringBuilder();
-            for (int i=0; i< message.length(); i++) {
-
+            for (char letter : message.toCharArray()) {
+                if (Character.isUpperCase(letter)) {
+                    encryptedMessage.append((char) (((letter - 'A' + key + 26) % 26) + 'A'));
+                } else if (Character.isLowerCase(letter)) {
+                    encryptedMessage.append((char) (((letter - 'a' + key + 26) % 26) + 'a'));
+                } else {
+                    encryptedMessage.append(letter);
+                }
             }
-            System.out.println(encryptedMessage);
+            log.info("EncryptedMessage {}", encryptedMessage);
             return encryptedMessage.toString();
         };
         return droidMessageEncryptor.processMessage(message, encryptionKey);
     }
 
+    private String decryptMessage(String message, int decryptionKey) {
+        DroidMessageEncryptor droidMessageDecryptor = (msg, key) -> {
+            StringBuilder decryptedMessage = new StringBuilder();
+            for (char letter : message.toCharArray()) {
+                if (Character.isUpperCase(letter)) {
+                    decryptedMessage.append((char) (((letter - 'A' - key + 26) % 26) + 'A'));
+                } else if (Character.isLowerCase(letter)) {
+                    decryptedMessage.append((char) (((letter - 'a' - key + 26) % 26) + 'a'));
+                } else {
+                    decryptedMessage.append(letter);
+                }
+            }
+            log.info("DecryptedMessage {}", decryptedMessage);
+            return decryptedMessage.toString();
+        };
+        return droidMessageDecryptor.processMessage(message, decryptionKey);
+    }
+
     public void sendMessage(Droid droidRecipient, String message, int encryptionKey) {
+        log.info("Send message for {}", droidRecipient.name());
         String encryptedMessage = encryptMessage(message, encryptionKey);
         droidRecipient.receiveMessage(encryptedMessage, encryptionKey);
     }
 
     private void receiveMessage(String encryptedMessage, int encryptionKey) {
-
+        decryptMessage(encryptedMessage, encryptionKey);
     }
-
 }
--- src/main/java/school/faang/droidssecrets/Main.java
+++ src/main/java/school/faang/droidssecrets/Main.java
@@ -7,19 +7,8 @@ public class Main {
         Droid c3po = new Droid("C3PO");
         Droid bb8 = new Droid("BB-8");
 
-        // Оригинальные сообщения
-        String message1 = "Join the Rebellion!";
-        String message2 = "The Death Star is operational!";
-        int encryptionKey1 = 3; // Ключ шифрования для первого сообщения
-        int encryptionKey2 = 7; // Ключ шифрования для второго сообщения
-
-        // R2D2 отправляет сообщение C3PO
-        r2d2.sendMessage(c3po, message1, encryptionKey1);
-//
-//        // C3PO отвечает R2D2
-//        c3po.sendMessage(r2d2, message2, encryptionKey2);
-//
-//        // BB-8 отправляет сообщение C3PO
-//        bb8.sendMessage(c3po, "The mission is complete.", encryptionKey1);
+        r2d2.sendMessage(c3po, "Join the Rebellion!", 3);
+        c3po.sendMessage(r2d2, "The Death Star is operational!", 7);
+        bb8.sendMessage(c3po, "The mission is complete.", 3);
     }
 }
