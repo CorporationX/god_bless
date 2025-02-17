eb9528c3e9a1bae9b8c02a2c32bcf60d96bea9d2
Sofiya Mutovkina <sofiyamutovkina@gmail.com>
2025-02-17 16:00:30 +0300

    добавила константы и enum

--- src/main/java/school/faang/uber/Main.java
+++ src/main/java/school/faang/uber/Main.java
@@ -1,27 +1,15 @@
 package school.faang.uber;
 
-import java.util.function.BiFunction;
-
 public class Main {
+    private static final double DEFAULT_DISTANCE = 5.0;
+    private static final double DEFAULT_TIME = 10.0;
+
     public static void main(String[] args) {
         FareCalculator fareCalculator = new FareCalculator();
 
-        BiFunction<Double, Double, Double> economyFare = (distance, time) -> 1.0 * distance + 0.3 * time;
-        BiFunction<Double, Double, Double> comfortFare = (distance, time) -> 1.5 * distance + 0.5 * time;
-        BiFunction<Double, Double, Double> comfortPlusFare = (distance, time) -> 2.0 * distance + 0.7 * time;
-        BiFunction<Double, Double, Double> businessFare = (distance, time) -> 2.5 * distance + 1.0 * time;
-
-        double distance = 5.0;
-        double time = 10.0;
-
-        double economyFareCost = fareCalculator.calculateFare(distance, time, economyFare);
-        double comfortFareCost = fareCalculator.calculateFare(distance, time, comfortFare);
-        double comfortPlusFareCost = fareCalculator.calculateFare(distance, time, comfortPlusFare);
-        double businessFareCost = fareCalculator.calculateFare(distance, time, businessFare);
-
-        System.out.println("The cost of the trip on the Economy fare: " + economyFareCost);
-        System.out.println("The cost of the trip on the Comfort fare: " + comfortFareCost);
-        System.out.println("The cost of the trip on the Comfort plus fare: " + comfortPlusFareCost);
-        System.out.println("The cost of the trip on the Business fare: " + businessFareCost);
+        for (FareType fareType : FareType.values()) {
+            double cost = fareCalculator.calculateFare(DEFAULT_DISTANCE, DEFAULT_TIME, fareType.getFareFunction());
+            System.out.println(String.format("The cost of the trip on the %s fare: %.2f", fareType.name(), cost));
+        }
     }
 }
