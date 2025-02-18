f04d0bc09ee3f03a4698b956136fa1cd7cdcb3d0
Savchenko <savchenko.ra@yandex.ru>
2025-02-18 10:56:37 +0300

    BJS2-61124 is done.

---
+++ src/main/java/school/faang/bjs2_61124/Main.java
@@ -0,0 +1,19 @@
+package school.faang.bjs2_61124;
+
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) throws InterruptedException {
+        Army army = new Army();
+        Squad archers = new Squad("Archers", List.of(new Archer(25), new Archer(30)));
+        Squad swordsmen = new Squad("Swordsmen", List.of(new Swordsman(40), new Swordsman(35)));
+        Squad mages = new Squad("Mages", List.of(new Mage(50), new Mage(45)));
+
+        army.addSquad(archers);
+        army.addSquad(swordsmen);
+        army.addSquad(mages);
+
+        int totalPower = army.calculateTotalPower();
+        System.out.println("Total army strength: " + totalPower);
+    }
+}
