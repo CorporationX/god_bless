1ba11c5563813b809f7f4c88bc95af1daa6c03f8
Anton Lukyanchikov <antontotenkopf@gmail.com>
2025-02-18 19:35:48 +0300

    created required classes

---
+++ src/main/java/task_BJS2_61403/Main.java
@@ -0,0 +1,7 @@
+package task_BJS2_61403;
+
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/main/java/task_BJS2_61403/Swordsman.java
@@ -0,0 +1,7 @@
+package task_BJS2_61403;
+
+public class Swordsman extends Character{
+    public Swordsman(int power) {
+        super(power);
+    }
+}
