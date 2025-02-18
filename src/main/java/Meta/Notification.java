c6d5df1f856da6902778e3da7e7e63a0aa99eaf9
Denis Popov <popoffdn@mail.ru>
2025-02-18 21:42:59 +0300

    ревью BJS2-58014, добавил проверку на стоп слова

--- src/main/java/Meta/Notification.java
+++ src/main/java/Meta/Notification.java
@@ -1,22 +1,19 @@
 package Meta;
 
+import lombok.Getter;
+import lombok.Setter;
+
 public class Notification {
+    @Getter
     private final NotificationType type;
+    @Getter
     private final String message;
 
     public Notification(NotificationType type, String message) {
         this.type = type;
         this.message = message;
     }
-
-    public NotificationType getType() {
-        return type;
-    }
-
-    public String getMessage() {
-        return message;
-    }
-
+    
     @Override
     public String toString() {
         return "Notification{" +
