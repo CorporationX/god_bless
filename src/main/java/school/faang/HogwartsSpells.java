8567038a5848ef2ca31352de8785c851fe482f9e
Iaroslava Seregina <yaroslavaseregina4@gmail.com>
2025-02-18 21:11:07 +0300

    task "Ловим события" completed

---
+++ src/main/java/school/faang/HogwartsSpells.java
@@ -0,0 +1,53 @@
+package school.faang;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Random;
+
+public class HogwartsSpells {
+    private static final Random random = new Random();
+    private Map<Integer, SpellEvent> spellById = new HashMap<>();
+    private Map<String, List<SpellEvent>> spellByType = new HashMap<>();
+
+    public void addSpellEvent(String eventType, String actionDescription) {
+        int uniqueId;
+        do {
+            uniqueId = random.nextInt(Integer.MAX_VALUE);
+        } while (spellById.containsKey(uniqueId));
+        SpellEvent spellEvent = new SpellEvent(uniqueId, eventType, actionDescription);
+        spellById.put(spellEvent.getId(), spellEvent);
+        spellByType.computeIfAbsent(spellEvent.getEventType(), k -> new ArrayList<>()).add(spellEvent);
+    }
+
+    public SpellEvent getSpellEventById(int id) {
+        return spellById.get(id);
+    }
+
+    public List<SpellEvent> getSpellEventsByType(String eventType) {
+        return spellByType.get(eventType);
+    }
+
+    public void deleteSpellEvent(int id) {
+        SpellEvent spellEvent = spellById.remove(id);
+        if (spellEvent != null) {
+            List<SpellEvent> events = spellByType.get(spellEvent.getEventType());
+            if (events != null) {
+                events.remove(spellEvent);
+                if (events.isEmpty()) {
+                    spellByType.remove(spellEvent.getEventType());
+                }
+            }
+        }
+    }
+
+    public void printAllSpellEvents() {
+        for (Map.Entry<Integer, SpellEvent> entry : spellById.entrySet()) {
+            SpellEvent spell = entry.getValue();
+            System.out.println("ID: " + spell.getId() +
+                    ", Тип: " + spell.getEventType() +
+                    ", Действие: " + spell.getAction());
+        }
+    }
+}
