69445b90891605d0c9b40bf8ba5f2d675acbbec1
KAS1990 <34664788+KAS1990@users.noreply.github.com>
2025-02-18 18:25:28 +0300

    refactor: Moved code to separate class

--- src/main/java/bjs2_57894/Main.java
+++ src/main/java/bjs2_57894/Main.java
@@ -1,15 +1,9 @@
 package bjs2_57894;
 
 import java.util.ArrayList;
-import java.util.Comparator;
 import java.util.HashMap;
-import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
-import java.util.Optional;
-import java.util.Set;
-import java.util.stream.Collectors;
-import java.util.stream.Stream;
 
 public class Main {
     public static void main(String[] args) {
@@ -20,7 +14,7 @@ public class Main {
         insertItemToTask1(data1, "Charlie", List.of("Alice", "David"));
         insertItemToTask1(data1, "David", List.of("Bob", "Charlie"));
 
-        List<Pair> friendOfFriends = getFriendOfFriends(data1);
+        List<Pair> friendOfFriends = Utils.getFriendOfFriends(data1);
         System.out.println(friendOfFriends);
         System.out.println();
 
@@ -33,7 +27,7 @@ public class Main {
         insertItemToTask2(data2, "Ivan", 5, "Department 3");
         insertItemToTask2(data2, "Sven", 58, "Department 2");
 
-        Map<String, Double> averageSalaryByDepartment = getAverageSalaryByDepartment(data2);
+        Map<String, Double> averageSalaryByDepartment = Utils.getAverageSalaryByDepartment(data2);
         System.out.println(averageSalaryByDepartment);
         System.out.println();
 
@@ -41,14 +35,14 @@ public class Main {
         int from = 100;
         int to = 200;
         System.out.printf("[%d, %d]%n", from, to);
-        List<Integer> palindroms = getPalindroms(from, to);
+        List<Integer> palindroms = Utils.getPalindroms(from, to);
         System.out.println(palindroms);
         System.out.println();
 
         System.out.println("Task 4");
         String string = "abaca";
         System.out.println(string);
-        List<String> substringsPalindroms = getSubstringsPalindroms(string);
+        List<String> substringsPalindroms = Utils.getSubstringsPalindroms(string);
         System.out.println(substringsPalindroms);
         System.out.println();
 
@@ -56,7 +50,7 @@ public class Main {
         from = 1;
         to = 1000;
         System.out.printf("[%d, %d]%n", from, to);
-        List<Integer> perfectDigits = getPerfectDigits(from, to);
+        List<Integer> perfectDigits = Utils.getPerfectDigits(from, to);
         System.out.println(perfectDigits);
         System.out.println();
     }
@@ -69,135 +63,5 @@ public class Main {
     private static void insertItemToTask2(List<Employee> data, String name, double salary, String department) {
         data.add(new Employee(name, salary, department));
         System.out.printf("%s %.2f %s%n", name, salary, department);
-    }
-
-    private static List<Pair> getFriendOfFriends(Map<String, List<String>> data) {
-        List<Pair> sortedUniquePairs = data.entrySet()
-                .stream()
-                .map(kvp -> kvp.getValue()
-                        .stream()
-                        .map(friend -> new Pair(kvp.getKey(), friend))
-                        .toList())
-                .flatMap(List::stream)
-                .distinct()    // Удаляем повторяющиеся пары
-                .sorted(Comparator.comparing(Pair::first))
-                .toList();
-
-        Set<String> processedNames = new HashSet<>();
-        List<Pair> result = new ArrayList<>();
-
-        for (Pair pair : sortedUniquePairs) {
-            String name = pair.first();
-            String friendName = pair.second();
-
-            if (processedNames.contains(name) || processedNames.contains(friendName)) {
-                continue;
-            }
-
-            Optional<Pair> friendPair = sortedUniquePairs.stream()
-                    .filter(p -> p.first().equals(friendName))
-                    .findFirst();
-            if (friendPair.isEmpty()) {
-                continue;
-            }
-
-            result.add(new Pair(name, friendPair.get().second()));
-            processedNames.add(friendName);
-        }
-
-        return result;
-    }
-
-    private static Map<String, Double> getAverageSalaryByDepartment(List<Employee> data) {
-        return data.stream()
-                .collect(Collectors.groupingBy(Employee::department, Collectors.averagingDouble(Employee::salary)));
-    }
-
-    private static List<Integer> getPalindroms(int from, int to) {
-        List<Integer> result = new ArrayList<>();
-
-        List<Integer> invertedDigits = new ArrayList<>();
-
-        int firstDigit = from;
-        while (firstDigit > 0) {
-            invertedDigits.add(firstDigit % 10);
-            firstDigit /= 10;
-        }
-
-        for (int i = from; i <= to; i++) {
-            if (checkPalindrom(invertedDigits)) {
-                result.add(i);
-            }
-
-            int j = 0;
-            for (; j < invertedDigits.size() - 1; j++) {
-                int digit = invertedDigits.get(j);
-                if (digit < 9) {
-                    invertedDigits.set(j, digit + 1);
-                    break;
-                } else {
-                    invertedDigits.set(j, 0);
-                }
-            }
-
-            if (j == invertedDigits.size() - 1 && invertedDigits.get(j) <= 9) {
-                invertedDigits.set(j, invertedDigits.get(j) + 1);
-            }
-        }
-
-        return result;
-    }
-
-    private static List<String> getSubstringsPalindroms(String string) {
-        List<String> result = new ArrayList<>();
-
-        int len = string.length();
-        for (int i = 0; i < len; i++) {
-            result.add(string.substring(i, i + 1));
-
-            for (int j = i + 2; j <= len; j++) {
-                String substring = string.substring(i, j);
-                List<Character> charList = substring.chars()
-                        .mapToObj(c -> (char) c)
-                        .toList();
-
-                if (checkPalindrom(charList)) {
-                    result.add(substring);
-                }
-            }
-        }
-
-        return result.stream().distinct().toList();
-    }
-
-    private static List<Integer> getPerfectDigits(int from, int to) {
-        return Stream.iterate(from, n -> n + 1)
-                .limit(to - from)
-                .filter(number -> {
-                    List<Integer> dividers = new ArrayList<>();
-                    for (int divider = 1; divider <= number / 2; divider++) {
-                        if (number % divider == 0) {
-                            dividers.add(divider);
-                        }
-                    }
-
-                    return dividers.stream().mapToInt(x -> x).sum() == number;
-                })
-                .toList();
-    }
-
-    private static <T> boolean checkPalindrom(List<T> list) {
-        int leftIndex = 0;
-        int rightIndex = list.size() - 1;
-        while (leftIndex < rightIndex) {
-            if (!list.get(leftIndex).equals(list.get(rightIndex))) {
-                break;
-            }
-
-            leftIndex++;
-            rightIndex--;
-        }
-
-        return leftIndex == rightIndex;
     }
 }
