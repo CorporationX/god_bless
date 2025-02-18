89d1be609e851423c53018af9be7ca182f3f56e8
Artem Doronin <doronintema2018@yandex.ru>
2025-02-18 16:31:46 +0500

    Анализ активности пользователей в социальной сети

---
+++ src/main/java/school/faang/task_58261/UserAction.java
@@ -0,0 +1,17 @@
+package school.faang.task_58261;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import java.time.LocalDate;
+
+@AllArgsConstructor
+@Getter
+
+public class UserAction {
+    private int id;
+    private String name;
+    private ActionType actionType;
+    private LocalDate actionDate;
+    private String content;
+
+}
