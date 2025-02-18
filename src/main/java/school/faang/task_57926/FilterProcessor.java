1eae9844e40db68a05025dbbc2260b664e44a1b3
IgnatovEI <ignatovei96@mail.ru>
2025-02-18 06:55:50 +0300

    Изменил метод combineFilters с помощью andThen

--- src/main/java/school/faang/task_57926/FilterProcessor.java
+++ src/main/java/school/faang/task_57926/FilterProcessor.java
@@ -11,12 +11,6 @@ public class FilterProcessor {
 
     public Function<Image, Image> combineFilters(@NonNull Function<Image, Image> firstFilter,
                                                  @NonNull Function<Image, Image> secondFilter) {
-        Function<Image, Image> combinedFilter = image -> {
-            Image afterFirstFilter = firstFilter.apply(image);
-            Image afterSecondFilter = secondFilter.apply(afterFirstFilter);
-            return afterSecondFilter;
-        };
-
-        return combinedFilter;
+        return firstFilter.andThen(secondFilter);
     }
 }
