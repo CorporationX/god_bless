f8ae10154f0ed3b0be43b6488f069703aee37098
Ekaterina Ramakoti <zubkova-92@mail.ru>
2025-02-18 23:26:54 +0300

    task BJS2-58285

---
+++ src/main/java/school/faang/bjs2_58285/UserActionAnalyzer.java
@@ -0,0 +1,59 @@
+package school.faang.bjs2_58285;
+
+import lombok.NonNull;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+
+public class UserActionAnalyzer {
+    public List<String> topActiveUsers(@NonNull List<UserAction> actions, int limit) {
+        Map<String, Long> usersToActionsCount = actions
+                .stream()
+                .collect(Collectors.groupingBy(UserAction::getName, Collectors.counting()));
+        return usersToActionsCount.entrySet().stream()
+                .sorted(Map.Entry.<String, Long>comparingByValue().reversed())
+                .limit(limit)
+                .map(Map.Entry::getKey)
+                .toList();
+    }
+
+    public List<String> topPopularHashtags(List<UserAction> actions, int limit) {
+        Map<String, Long> wordCount = actions
+                .stream()
+                .filter(action -> action.getContent() != null)
+                .flatMap(action -> Arrays.stream(action.getContent().split(" ")))
+                .filter(word -> word.startsWith("#"))
+                .collect(Collectors.groupingBy(Function.identity(), Collectors.counting()));
+        return wordCount
+                .entrySet()
+                .stream()
+                .sorted(Map.Entry.<String, Long>comparingByValue().reversed())
+                .limit(limit)
+                .map(Map.Entry::getKey)
+                .toList();
+    }
+
+    public List<String> topCommentersLastMonth(List<UserAction> actions, int limit) {
+        LocalDate oneMonth = LocalDate.now().minusMonths(1);
+        Map<String, Long> comment = actions
+                .stream()
+                .filter(action -> ActionType.COMMENT.equals(action.getActionType()) && action.getActionDate().isAfter(oneMonth))
+                .collect(Collectors.groupingBy(UserAction::getName, Collectors.counting()));
+        return comment
+                .entrySet()
+                .stream()
+                .sorted(Map.Entry.<String, Long>comparingByValue().reversed())
+                .limit(limit)
+                .map(Map.Entry::getKey)
+                .toList();
+    }
+
+    public void actionTypePercentages() {
+
+    }
+}
