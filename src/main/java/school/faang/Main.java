eca56a4a64cdae7a16853d9719e2bf829447c02a
Iaroslava Seregina <yaroslavaseregina4@gmail.com>
2025-02-18 15:51:21 +0300

    task "Абстракция, абстракция!" completed

--- src/main/java/school/faang/Main.java
+++ src/main/java/school/faang/Main.java
@@ -1,4 +1,15 @@
 package school.faang;
 
 public class Main {
+    public static void main(String[] args) {
+        Character warrior = new Warrior("Mike");
+        Character archer = new Archer("Gon");
+        try {
+            for (int i = 0; i < 11; i++) {
+                archer.attack(warrior);
+            }
+        } catch (RuntimeException e) {
+            System.out.println(e);
+        }
+    }
 }
