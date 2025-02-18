b40c1374847adb15594ff0b37b9e4faa7be4c65d
KAS1990 <34664788+KAS1990@users.noreply.github.com>
2025-02-18 21:56:20 +0300

    BJS2-60571 выполнение задачи

--- src/main/java/bjs2_60571/Main.java
+++ src/main/java/bjs2_60571/Main.java
@@ -1,7 +1,53 @@
 package bjs2_60571;
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.Random;
+import java.util.UUID;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
+
 public class Main {
-    public static void main(String[] args) {
-        
+    public static void main(String[] args) throws InterruptedException {
+        final int personCount = 10_000;
+        Person[] persons = new Person[personCount];
+        Random rnd = new Random();
+
+        for (int i = 0; i < personCount; i++) {
+            persons[i] = new Person(
+                    UUID.randomUUID().toString(),
+                    UUID.randomUUID().toString(),
+                    rnd.nextInt(10, 80),
+                    UUID.randomUUID().toString());
+        }
+
+        final int threadsCount = 10;
+        final int chunkSize = personCount / threadsCount;
+
+        ExecutorService executorService = Executors.newFixedThreadPool(threadsCount);
+
+        for (int i = 0; i < threadsCount; i++) {
+            List<Person> chunkData = List.of(Arrays.copyOfRange(persons, chunkSize * i, chunkSize * (i + 1)));
+
+            PersonInfoPrinter printer = new PersonInfoPrinter(chunkData);
+
+            executorService.submit(printer);
+        }
+
+        executorService.shutdown();
+
+        try {
+            if (executorService.awaitTermination(3, TimeUnit.SECONDS)) {
+                System.out.println("Операция завершилась, менее чем за 3 секунды");
+
+                return;
+            }
+        } catch (InterruptedException e) {
+            executorService.shutdownNow();
+        }
+
+        Thread.sleep(3000);
+        System.out.println("Операция не успела завершиться за 3 секунды");
     }
 }
--- src/main/java/bjs2_60571/PersonInfoPrinter.java
+++ src/main/java/bjs2_60571/PersonInfoPrinter.java
@@ -1,10 +1,18 @@
 package bjs2_60571;
 
-public class PersonInfoPrinter implements Runnable {
+import java.util.List;
 
+public class PersonInfoPrinter implements Runnable {
+    private final List<Person> persons;
+
+    public PersonInfoPrinter(List<Person> persons) {
+        this.persons = persons;
+    }
 
     @Override
     public void run() {
-
+        for (Person person : persons) {
+            System.out.printf("Name = %s, surname = %s, workplace = %s%n", person.name(), person.surname(), person.workplace());
+        }
     }
 }
