c6c5fcd3fa2f85aae919de4fda01a1a0e0b11ad3
Sergey Ermoshenko <ermoshenko.work@gmail.com>
2025-02-18 16:35:40 +0300

    fix checkstyle error

--- src/main/java/school/faang/CashierThread.java
+++ src/main/java/school/faang/CashierThread.java
@@ -2,15 +2,15 @@ package school.faang;
 
 import java.util.Random;
 
-public class CashierThread extends Thread{
+public class CashierThread extends Thread {
     private static final Random random = new Random();
     private static final int MIN_SLEEP_TIME = 1000;
     private static final int MAX_SLEEP_TIME = 3000;
-    private final int cashierId ;
+    private final int cashierId;
     private final int[] customerItems;
 
-    public CashierThread(int cashierId , int[] customerItems) {
-        this.cashierId  = cashierId ;
+    public CashierThread(int cashierId, int[] customerItems) {
+        this.cashierId = cashierId;
         this.customerItems = customerItems;
     }
 
