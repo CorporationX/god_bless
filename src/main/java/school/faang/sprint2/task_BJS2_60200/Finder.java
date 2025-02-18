abf3e1d4d3fb65e2b67e587762e158f9f268b789
Ilhan Mustafa <ilhnmst@gmail.com>
2025-02-18 17:26:38 +0300

    Resolving

--- src/main/java/school/faang/sprint2/task_BJS2_60200/Finder.java
+++ src/main/java/school/faang/sprint2/task_BJS2_60200/Finder.java
@@ -3,26 +3,26 @@ package school.faang.sprint2.task_BJS2_60200;
 import school.faang.sprint2.task_BJS2_60200.employee.SalaryDepartmentAverager;
 import school.faang.sprint2.task_BJS2_60200.employee.Department;
 import school.faang.sprint2.task_BJS2_60200.employee.Employee;
-import school.faang.sprint2.task_BJS2_60200.palindrome.number.PalindromeGenerator;
 import school.faang.sprint2.task_BJS2_60200.people.Pair;
 import school.faang.sprint2.task_BJS2_60200.people.PairGenerator;
 import school.faang.sprint2.task_BJS2_60200.people.Person;
 
 import java.util.HashSet;
-import java.util.List;
 import java.util.Map;
 import java.util.Set;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 public class Finder {
     // /people
     public Set<Pair> findPeopleWithMutualFriends(Map<Person, Set<Person>> peopleWithFriends) {
-        validMap(peopleWithFriends);
+        validateMap(peopleWithFriends);
         PairGenerator pairGenerator = new PairGenerator(peopleWithFriends);
         peopleWithFriends.values().forEach(pairGenerator::generateAllPairs);
         return pairGenerator.getPairOfPeople();
     }
 
-    private <T, U> void validMap(Map<T, U> map) {
+    private <T, U> void validateMap(Map<T, U> map) {
         if (map == null) {
             throw new IllegalArgumentException("Карта не может быть пустой");
         }
@@ -38,55 +38,52 @@ public class Finder {
 
     // /palindrome
     public Set<Integer> getPalindromesBetween(int start, int end) {
-        Set<Integer> palindromeNumbers = new HashSet<>();
-        PalindromeGenerator palindromeGenerator = new PalindromeGenerator();
-        int number = palindromeGenerator.getNextPalindrome();
+        return IntStream.range(start, end)
+                .filter(this::isPalindrome)
+                .boxed()
+                .collect(Collectors.toSet());
+    }
 
-        while (number < end) {
-            if (number > start) {
-                palindromeNumbers.add(number);
-            }
-            number = palindromeGenerator.getNextPalindrome();
-        }
-        return palindromeNumbers;
+    private boolean isPalindrome(int number) {
+        String val = String.valueOf(number);
+        String reversed = new StringBuilder(val).reverse().toString();
+        return val.equals(reversed);
     }
 
     public Set<String> getPalindromesSubstring(String string) {
         Set<String> palindromeSubstring = new HashSet<>();
-        int stringLength = string.length();
-        int stringStart = 0;
-        int rootPointer = 0;
-        int leftPointer;
-        int rightPinter;
 
-        while (rootPointer < stringLength) {
-            leftPointer = rootPointer;
-            rightPinter = rootPointer;
+        IntStream.range(0, string.length())
+                .forEach(pointer -> {
+                    int leftPointer = pointer + 1;
+                    int rightPinter = pointer - 1;
 
-            while (leftPointer >= stringStart && rightPinter < stringLength) {
-                if (string.charAt(leftPointer) == string.charAt(rightPinter)) {
-                    String substring = string.substring(leftPointer, rightPinter + 1);
-                    palindromeSubstring.add(substring);
-                }
-                leftPointer--;
-                rightPinter++;
-            }
-            rootPointer++;
-        }
+                    while (--leftPointer >= 0 && ++rightPinter < string.length()) {
+                        if (string.charAt(leftPointer) == string.charAt(rightPinter)) {
+                            String substring = string.substring(leftPointer, rightPinter + 1);
+                            palindromeSubstring.add(substring);
+                        } else {
+                            break;
+                        }
+                    }
+                });
+
         return palindromeSubstring;
     }
 
     public Set<Integer> getPerfectNumbersBetween(int start, int end) {
-        List<Integer> primeNumbers = List.of(2, 3, 5, 7);
-        Set<Integer> perfectNumbers = new HashSet<>();
+        return IntStream.range(start, end)
+                .filter(this::isPerfect)
+                .boxed()
+                .collect(Collectors.toSet());
+    }
 
-        primeNumbers.forEach(primeNumber -> {
-            int perfectNumber = (int) Math.pow(2, (primeNumber - 1)) * ((int) Math.pow(2, primeNumber) - 1);
-            if (start <= perfectNumber && perfectNumber < end) {
-                perfectNumbers.add(perfectNumber);
-            }
-        });
-
-        return perfectNumbers;
+    private boolean isPerfect(int num) {
+        if (num < 1) {
+            return false;
+        }
+        return IntStream.range(1, num)
+                .filter(i -> num % i == 0)
+                .sum() == num;
     }
 }
