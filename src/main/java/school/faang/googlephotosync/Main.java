8517117a1a3e9af949b139c7d460c57d038bea33
Marsel <mars1212@inbox.ru>
2025-02-18 23:05:33 +0300

    BJS2-61632 Google Photo sync

---
+++ src/main/java/school/faang/googlephotosync/Main.java
@@ -0,0 +1,17 @@
+package school.faang.googlephotosync;
+
+import java.util.Random;
+
+public class Main {
+    public static void main(String[] args) {
+        GooglePhotosAutoUploader googlePhotosAutoUploader = new GooglePhotosAutoUploader();
+        googlePhotosAutoUploader.onNewPhotoAdded("Anapa2006");
+
+        Random rand = new Random();
+        for (int i = 0; i < 10; i++) {
+            String photo = "photo#" + rand.nextInt(100);
+            new Thread(() -> googlePhotosAutoUploader.onNewPhotoAdded(photo)).start();
+            new Thread(googlePhotosAutoUploader::startAutoUpload).start();
+        }
+    }
+}
