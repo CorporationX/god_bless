72a30686e24e7a20f5926fba0c49d46ae09cd1f5
s3nkyh <olkin.yaroslaw@yandex.ru>
2025-02-18 18:42:14 +0300

    BJS2-60622

--- src/main/java/school/faang/task_60622/CashierThread.java
+++ src/main/java/school/faang/task_60622/CashierThread.java
@@ -5,14 +5,28 @@ import lombok.AllArgsConstructor;
 import java.util.Arrays;
 
 @AllArgsConstructor
-public class CashierThread extends Thread{
+public class CashierThread extends Thread {
     private final int cashierId;
     private final int[] customerItems;
 
     @Override
     public void run() {
-        Arrays.asList(customerItems).forEach(item -> {
-            System.out.printf("Casheir%d scans item: %d\n", cashierId, item);
-        });
+        System.out.printf("Started %d serving the customer with %d the items.\n", cashierId, customerItems.length);
+
+        int totalItems = 0;
+        int totalPrice = 0;
+
+        for (int item : customerItems) {
+            totalItems++;
+            totalPrice += item * 10;
+            try {
+                Thread.sleep(500);
+            } catch (InterruptedException e) {
+                Thread.currentThread().interrupt();
+                throw new RuntimeException(e);
+            }
+        }
+        System.out.printf("The cashier %d has completed the service. Processed items: %d. Total cost: %d.\n",
+                cashierId, totalItems, totalPrice);
     }
 }
--- src/main/java/school/faang/task_60622/Main.java
+++ src/main/java/school/faang/task_60622/Main.java
@@ -1,7 +1,38 @@
 package school.faang.task_60622;
 
-public class Main {
-    public static void main(String[] args) {
+import java.util.Random;
 
+public class Main {
+    private static final int NUM_OF_CASHIERS = 3;
+    private static final Random random = new Random();
+
+    public static void main(String[] args) {
+        int[][] customers = {
+                {5, 7, 3, 2},
+                {1, 2, 3},
+                {6, 5, 2},
+                {10, 2, 1, 3, 5}
+        };
+
+        CashierThread[] cashiers = new CashierThread[customers.length];
+
+        for (int i = 0; i < customers.length; i++) {
+            int cashierId = random.nextInt(NUM_OF_CASHIERS) + 1;
+            cashiers[i] = new CashierThread(cashierId, customers[i]);
+        }
+
+        for (CashierThread cashier : cashiers) {
+            cashier.start();
+        }
+
+        for (CashierThread cashier : cashiers) {
+            try {
+                cashier.join();
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+        }
+
+        System.out.println("All cashiers have finished their work.");
     }
 }
