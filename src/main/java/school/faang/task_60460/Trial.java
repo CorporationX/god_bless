a4620e9986cb037666b304460184bc03c392357a
Yaroslav Olkin <shrelok.pupkin@yandex.ru>
2025-02-18 22:02:33 +0300

    вынес sleep в константу

--- src/main/java/school/faang/task_60460/Trial.java
+++ src/main/java/school/faang/task_60460/Trial.java
@@ -4,14 +4,15 @@ import lombok.AllArgsConstructor;
 
 @AllArgsConstructor
 public class Trial implements Runnable {
+    private static final long SLEEP_TIME = 1000;
     private final String knightName;
     private final String trialName;
 
     @Override
     public void run() {
-        System.out.printf("%s began the trail: %s\n", knightName, trialName);
+        System.out.printf("%s began the trial: %s\n", knightName, trialName);
         try {
-            Thread.sleep(1000);
+            Thread.sleep(SLEEP_TIME);
         } catch (InterruptedException e) {
             Thread.currentThread().interrupt();
             throw new RuntimeException(e);
