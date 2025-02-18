7e31313ac73dafe1d647bf82520c0af0d70d7b00
Max Goloviznin <roxet59rus@mail.ru>
2025-02-18 22:22:41 +0500

    BJS2-61676 Блокировка Spotify

---
+++ src/main/java/school/faang/blockingspotify/Music.java
@@ -0,0 +1,45 @@
+package school.faang.blockingspotify;
+
+import lombok.extern.slf4j.Slf4j;
+
+import java.util.List;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
+
+@Slf4j
+public class Music {
+    private static final Player PLAYER = new Player();
+    private static final ExecutorService EXECUTOR = Executors.newFixedThreadPool(4);
+    private static final int TIME_EXECUTION = 1;
+    private static final TimeUnit TIME_EXECUTION_UNIT = TimeUnit.MINUTES;
+    private static final List<Runnable> playTask = List.of(
+            () -> executeTask(PLAYER::play),
+            () -> executeTask(PLAYER::pause),
+            () -> executeTask(PLAYER::skip),
+            () -> executeTask(PLAYER::previous)
+    );
+
+    public static void main(String[] args) throws InterruptedException {
+        playTask.forEach(EXECUTOR::submit);
+        EXECUTOR.shutdown();
+        boolean isTerminated = EXECUTOR.awaitTermination(TIME_EXECUTION, TIME_EXECUTION_UNIT);
+        if (isTerminated) {
+            log.info("All done");
+        } else {
+            log.warn("Execution timed out: {} {}", TIME_EXECUTION, TIME_EXECUTION_UNIT);
+            EXECUTOR.shutdownNow();
+        }
+    }
+
+    private static void executeTask(RunnableWithException task) {
+        try {
+            task.run();
+        } catch (InterruptedException exception) {
+            log.error("Thread interrupted. Thread name: {}\nException: {}\nStack trace: {}",
+                    Thread.currentThread().getName(), exception, exception.getStackTrace());
+            Thread.currentThread().interrupt();
+            EXECUTOR.shutdownNow();
+        }
+    }
+}
---
+++ src/main/java/school/faang/blockingspotify/Player.java
@@ -0,0 +1,54 @@
+package school.faang.blockingspotify;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+public class Player {
+    private static final Object LOCK = new Object();
+    private static final long MAX_TIME_EXECUTION = 3000;
+    private static final long MIN_TIME_EXECUTION = 1000;
+
+    private boolean isPlaying = false;
+
+    public void play() throws InterruptedException {
+        synchronized (LOCK) {
+            while (isPlaying) {
+                LOCK.wait();
+            }
+            simulateDelay();
+            isPlaying = true;
+            log.info("Music playing");
+            LOCK.notify();
+        }
+    }
+
+    public void pause() throws InterruptedException {
+        synchronized (LOCK) {
+            while (!isPlaying) {
+                LOCK.wait();
+            }
+            simulateDelay();
+            isPlaying = false;
+            log.info("Music paused");
+            LOCK.notify();
+        }
+    }
+
+    public void skip() throws InterruptedException {
+        synchronized (LOCK) {
+            simulateDelay();
+            log.info("Next music");
+        }
+    }
+
+    public void previous() throws InterruptedException {
+        synchronized (LOCK) {
+            simulateDelay();
+            log.info("Previous music");
+        }
+    }
+
+    private void simulateDelay() throws InterruptedException {
+        Thread.sleep((long) (Math.random() * MAX_TIME_EXECUTION + MIN_TIME_EXECUTION));
+    }
+}
