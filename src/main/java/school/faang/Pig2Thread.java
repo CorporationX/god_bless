b4d7cafffb1d3b34d821534fa1cafb0bdcde9a8b
Sergey Ermoshenko <ermoshenko.work@gmail.com>
2025-02-18 22:56:44 +0300

    fix inheritance

--- src/main/java/school/faang/Pig2Thread.java
+++ src/main/java/school/faang/Pig2Thread.java
@@ -1,34 +1,10 @@
 package school.faang;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
-import java.time.Instant;
-import java.util.Random;
-
 public class Pig2Thread extends PigThread {
-    private static final Logger logger = LoggerFactory.getLogger(Pig2Thread.class);
-    private static final Random random = new Random();
     private static final int MIN_BUILDING_TIME = 2000;
     private static final int MAX_BUILDING_TIME = 3000;
 
     public Pig2Thread() {
-        super("Nuf-Nuf", "sticks");
-    }
-
-    @Override
-    public void run() {
-        logger.info("{} started building the house using {} at {}",
-                getPigName(), getMaterial(), Instant.now());
-        try {
-            Thread.sleep(MIN_BUILDING_TIME + random.nextInt(MAX_BUILDING_TIME));
-        } catch (InterruptedException e) {
-            logger.info("{} failed building the house using {} at {}",
-                    getPigName(), getMaterial(), Instant.now());
-            Thread.currentThread().interrupt();
-            throw new RuntimeException(e);
-        }
-        logger.info("{} finished building the house using {} at {}",
-                getPigName(), getMaterial(), Instant.now());
+        super("Nuf-Nuf", "sticks", MIN_BUILDING_TIME, MAX_BUILDING_TIME);
     }
 }
