23fa3c43d715f11075c9de70cc335afc3a646702
amanzholtanat <amanzholtanat@gmail.com>
2025-02-18 22:14:22 +0500

    Вы работаете в Microsoft! - BJS2-60929

--- src/main/java/school/faang/microsoft/MailSender.java
+++ src/main/java/school/faang/microsoft/MailSender.java
@@ -1,35 +1,34 @@
 package school.faang.microsoft;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 public class MailSender {
-    public static void main(String[] args) throws InterruptedException {
-        SenderRunnable senderRunnable = new SenderRunnable(1, 200);
+    private static final int TOTAL_MESSAGES = 1000;
+    private static final int THREADS_COUNT = 5;
+    private static final Logger LOGGER = Logger.getLogger(MailSender.class.getName());
 
-        var thread2 = new Thread(senderRunnable::run);
-        thread2.start();
+    public static void main(String[] args) {
+        int groupSize = TOTAL_MESSAGES / THREADS_COUNT;
+        LOGGER.setLevel(Level.INFO);
+        List<Thread> threads = new ArrayList<>();
 
-        SenderRunnable senderRunnable1 = new SenderRunnable(201, 400);
+        for (int i = 0; i < THREADS_COUNT; i++) {
+            int startIndex = i * groupSize;
+            int endIndex = (i + 1) * groupSize;
+            threads.add(new Thread(new SenderRunnable(startIndex, endIndex)));
+            threads.get(i).start();
+        }
 
-        var thread3 = new Thread(senderRunnable1::run);
-        thread3.start();
-
-        SenderRunnable senderRunnable2 = new SenderRunnable(401, 600);
-
-        var thread4 = new Thread(senderRunnable2::run);
-        thread4.start();
-
-        SenderRunnable senderRunnable3 = new SenderRunnable(601, 800);
-
-        var thread5 = new Thread(senderRunnable3::run);
-        thread5.start();
-
-        SenderRunnable senderRunnable4 = new SenderRunnable(801, 1000);
-        senderRunnable4.run(); //main thread1
-
-        thread2.join();
-        thread3.join();
-        thread4.join();
-        thread5.join();
-
-        System.out.println("All messages were sent");
+        for (Thread thread : threads) {
+            try {
+                thread.join();
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+        }
+        LOGGER.info("All messages were sent");
     }
 }
--- src/main/java/school/faang/microsoft/SenderRunnable.java
+++ src/main/java/school/faang/microsoft/SenderRunnable.java
@@ -3,16 +3,21 @@ package school.faang.microsoft;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 @AllArgsConstructor
 @Getter
 public class SenderRunnable implements Runnable {
     private int startIndex;
     private int endIndex;
+    private static final Logger LOGGER = Logger.getLogger(SenderRunnable.class.getName());
 
     @Override
     public void run() {
         for (int i = startIndex; i <= endIndex; i++) {
-            System.out.println("The message was sent. Message number - " + i);
+            LOGGER.setLevel(Level.INFO);
+            LOGGER.info("The message was sent. Message number - " + i);
         }
     }
 }
