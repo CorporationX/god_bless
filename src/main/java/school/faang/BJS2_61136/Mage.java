4242535d3aa9d156f142d02580c45def00b223ce
Pavel Chernobay <mr.chernobay1990@gmail.com>
2025-02-18 17:57:36 +0300

    BJS2-61136 <task>

---
+++ src/main/java/school/faang/BJS2_61136/Mage.java
@@ -0,0 +1,13 @@
+package school.faang.BJS2_61136;
+
+import lombok.Getter;
+
+@Getter
+public class Mage extends Warrior {
+
+    private static final int POWER = 50;
+
+    public Mage() {
+        super(POWER);
+    }
+}
