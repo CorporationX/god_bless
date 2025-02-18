6d81daac4a58f95fe87af1897587c87138bcbac5
Max Goloviznin <roxet59rus@mail.ru>
2025-02-18 17:27:32 +0500

    BJS2-61186 The Big Bang Theory

--- src/main/java/school/faang/thebigbangtheory/Task.java
+++ src/main/java/school/faang/thebigbangtheory/Task.java
@@ -6,6 +6,9 @@ import lombok.extern.slf4j.Slf4j;
 @Slf4j
 @AllArgsConstructor
 public class Task implements Runnable {
+    private static final long MAX_TIME_EXECUTED_TASK = 5000;
+    private static final long MIN_TIME_EXECUTED_TASK = 1000;
+
     private String name;
     private String task;
 
@@ -13,10 +16,11 @@ public class Task implements Runnable {
     public void run() {
         log.info("{} starting task {}", name, task);
         try {
-            Thread.sleep(3000);
+            Thread.sleep((long) (Math.random() * MAX_TIME_EXECUTED_TASK + MIN_TIME_EXECUTED_TASK));
             log.info("{} finished task {}", name, task);
         } catch (InterruptedException exception) {
-            log.error("Task interrupted. {}\n{}", exception, Thread.currentThread().getName());
+            log.error("Task interrupted. Exception: {}\nThread name: {}\nStack trace: {}",
+                    exception, Thread.currentThread().getName(), exception.getStackTrace());
             Thread.currentThread().interrupt();
         }
     }
