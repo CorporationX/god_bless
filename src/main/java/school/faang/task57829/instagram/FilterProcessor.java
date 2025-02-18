b1f3f772ccba83276b19cfc4177ff40296138195
Winrida <winridas@gmail.com>
2025-02-18 14:01:21 +0300

    BJS2-57829 Фильтры Instagram Правки
    https://faang-school.atlassian.net/browse/BJS2-57829

--- src/main/java/school/faang/task57829/instagram/FilterProcessor.java
+++ src/main/java/school/faang/task57829/instagram/FilterProcessor.java
@@ -6,16 +6,12 @@ import java.util.function.Function;
 
 public class FilterProcessor {
 
-    public Image applyFilter(@NonNull Image image, @NonNull Function<Image, Image> filer) {
-        return filer.apply(image);
+    public Image applyFilter(@NonNull Image image, @NonNull Function<Image, Image> filter) {
+        return filter.apply(image);
     }
 
     public Function<Image, Image> combineFilters(@NonNull Function<Image, Image> firstFilter,
                                                  @NonNull Function<Image, Image> secondFilter) {
-        return  image -> {
-            image = applyFilter(image, firstFilter);
-            image = applyFilter(image, secondFilter);
-            return image;
-        };
+        return firstFilter.andThen(secondFilter);
     }
 }
