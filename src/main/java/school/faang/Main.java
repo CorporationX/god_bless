a83a16b07a0626ee844ba36212b2a832ed5f9c0b
Maria Sahno <sahnomyu@m2.ru>
2025-02-18 15:29:51 +0500

    init commit

--- src/main/java/school/faang/Main.java
+++ src/main/java/school/faang/Main.java
@@ -2,6 +2,6 @@ package school.faang;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("hello!");
+        System.out.println("Hello, world!");
     }
 }
