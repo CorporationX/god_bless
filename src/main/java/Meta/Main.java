c9ce0762f9656c31e0bb3f24252b6f89217e5eb8
Denis Popov <popoffdn@mail.ru>
2025-02-18 21:41:52 +0300

    ревью BJS2-58014, добавил проверку на стоп слова

---
+++ src/main/java/Meta/Main.java
@@ -0,0 +1,34 @@
+package Meta;
+
+public class Main {
+    public static void main(String[] args) {
+        NotificationManager notificationManager = new NotificationManager();
+
+        notificationManager.registerHandler(NotificationType.EMAIL,
+                notification -> System.out.println("Email: " + notification.getMessage()));
+
+        notificationManager.registerHandler(NotificationType.SMS,
+                notification -> System.out.println("SMS: " + notification.getMessage()));
+
+        notificationManager.registerHandler(NotificationType.PUSH,
+                notification -> System.out.println("Push Notification: " + notification.getMessage()));
+
+        notificationManager.registerHandler(NotificationType.STOP,
+                notification -> System.out.println("STOP:" + notification.getMessage()));
+
+        notificationManager.setFilter(notification -> {
+            String message = notification.getMessage().toLowerCase();
+            return !message.contains("Цензура");});
+
+        Notification emailNotification = new Notification(NotificationType.EMAIL, "Ваш аккаунт активирован");
+        Notification smsNotification = new Notification(NotificationType.SMS, "Ваш пароль изменен");
+        Notification pushNotification = new Notification(NotificationType.PUSH, "У вас новое сообщение!");
+        Notification blockedNotification = new Notification(NotificationType.STOP, "Сообщение заблокировано модератором!");
+
+        notificationManager.sendNotification(emailNotification);
+        notificationManager.sendNotification(smsNotification);
+        notificationManager.sendNotification(pushNotification);
+        notificationManager.sendNotification(blockedNotification);
+    }
+}
+
---
+++ src/main/java/Meta/NotificationType.java
@@ -0,0 +1,8 @@
+package Meta;
+
+public enum NotificationType {
+    EMAIL,
+    SMS,
+    PUSH,
+    STOP
+}
