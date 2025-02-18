d50eaa7a4456da74825f700414dcac4650f3b23f
bambum <gwalka885@gmai.com>
2025-02-18 21:17:49 +0500

    sout заменен на log.info enum вынесен в отдельный класс topN == maxResult

--- src/main/java/school/faang/UserAction.java
+++ src/main/java/school/faang/UserAction.java
@@ -1,30 +1,17 @@
 package school.faang;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.Getter;
 
 import java.time.LocalDate;
 
 @Data
+@AllArgsConstructor
 public class UserAction {
     private int id;
     private String name;
     private ActionType actionType;
     private LocalDate date;
     private String content;
-
-    public UserAction(int id, String name, ActionType actionType, LocalDate date, String content) {
-        this.id = id;
-        this.name = name;
-        this.actionType = actionType;
-        this.date = date;
-        this.content = content;
-    }
-
-    public enum ActionType {
-        POST,
-        COMMENT,
-        LIKE,
-        SHARE
-    }
 }
--- src/main/java/school/faang/UserActionAnalyzer.java
+++ src/main/java/school/faang/UserActionAnalyzer.java
@@ -1,5 +1,7 @@
 package school.faang;
 
+import lombok.extern.slf4j.Slf4j;
+
 import java.time.LocalDate;
 import java.util.Arrays;
 import java.util.List;
@@ -7,40 +9,41 @@ import java.util.Map;
 import java.util.function.Function;
 import java.util.stream.Collectors;
 
+@Slf4j
 public class UserActionAnalyzer {
 
     public static void main(String[] args) {
         UserActionAnalyzer analyzer = new UserActionAnalyzer();
         List<UserAction> actions = Arrays.asList(
-                new UserAction(1, "Alice", UserAction.ActionType.POST,
+                new UserAction(1, "Alice", ActionType.POST,
                         LocalDate.of(2025, 2, 1), "Check out this amazing #newfeature!"),
-                new UserAction(2, "Bob", UserAction.ActionType.COMMENT,
+                new UserAction(2, "Bob", ActionType.COMMENT,
                         LocalDate.of(2025, 2, 2), "I totally agree with #newfeature."));
 
         List<String> top10Users = UserActionAnalyzer.topActiveUsers(actions, 10);
-        System.out.println("Топ 10 активных юзеров: " + top10Users);
+        log.info("Топ 10 активных юзеров: " + top10Users);
 
         List<String> top5users = UserActionAnalyzer.topThemes(actions, 5);
-        System.out.println("Топ 5 популярных хештегов: " + top5users);
+        log.info("Топ 5 популярных хештегов: " + top5users);
 
         List<String> top3Commenters = UserActionAnalyzer.topCommenters(actions, 3);
-        System.out.println("Топ 3 комментаторов: " + top3Commenters);
+        log.info("Топ 3 комментаторов: " + top3Commenters);
 
-        Map<UserAction.ActionType, Double> actionPercentages = UserActionAnalyzer.calculateActionPercentages(actions);
-        System.out.println("Проценты действий: " + actionPercentages);
+        Map<ActionType, Double> actionPercentages = UserActionAnalyzer.calculateActionPercentages(actions);
+        log.info("Проценты действий: " + actionPercentages);
     }
 
-    public static List<String> topActiveUsers(List<UserAction> actions, int topN) {
+    public static List<String> topActiveUsers(List<UserAction> actions, int maxResult) {
         Map<String, Long> usersToActionsCount = actions.stream()
                 .collect(Collectors.groupingBy(UserAction::getName, Collectors.counting()));
         return usersToActionsCount.entrySet().stream()
                 .sorted(Map.Entry.<String, Long>comparingByValue().reversed())
-                .limit(topN)
+                .limit(maxResult)
                 .map(Map.Entry::getKey)
                 .toList();
     }
 
-    public static List<String> topThemes(List<UserAction> actions, int topN) {
+    public static List<String> topThemes(List<UserAction> actions, int maxResult) {
         Map<String, Long> usersToThemesCount = actions.stream()
                 .filter(action -> action.getContent() != null) // фильтрация
                 .flatMap(action -> Arrays.stream(action.getContent().split("\\s+")))
@@ -48,27 +51,27 @@ public class UserActionAnalyzer {
                 .collect(Collectors.groupingBy(Function.identity(), Collectors.counting()));
         return usersToThemesCount.entrySet().stream()
                 .sorted(Map.Entry.<String, Long>comparingByValue().reversed())
-                .limit(topN)
+                .limit(maxResult)
                 .map(Map.Entry::getKey)
                 .toList();
     }
 
-    public static List<String> topCommenters(List<UserAction> actions, int topN) {
+    public static List<String> topCommenters(List<UserAction> actions, int maxResult) {
         LocalDate oneMonthAgo = LocalDate.now().minusMonths(1);
         Map<String, Long> usersToTopCommenters = actions.stream()
-                .filter(action -> action.getActionType() == UserAction.ActionType.COMMENT
+                .filter(action -> action.getActionType() == ActionType.COMMENT
                         && action.getDate().isAfter(oneMonthAgo))
                 .collect(Collectors.groupingBy(UserAction::getName, Collectors.counting()));
         return usersToTopCommenters.entrySet().stream()
                 .sorted(Map.Entry.<String, Long>comparingByValue().reversed())
-                .limit(topN)
+                .limit(maxResult)
                 .map(Map.Entry::getKey)
                 .toList();
     }
 
-    public static Map<UserAction.ActionType, Double> calculateActionPercentages(List<UserAction> actions) {
+    public static Map<ActionType, Double> calculateActionPercentages(List<UserAction> actions) {
         long totalActions = actions.size();
-        Map<UserAction.ActionType, Long> actionTypeToCount = actions.stream()
+        Map<ActionType, Long> actionTypeToCount = actions.stream()
                 .collect(Collectors.groupingBy(UserAction::getActionType, Collectors.counting()));
         return actionTypeToCount.entrySet().stream()
                         .collect(Collectors.toMap(
