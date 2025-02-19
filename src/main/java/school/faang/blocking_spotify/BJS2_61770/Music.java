13953b8fac92aa75fa44ad0678729e5adee722b6
Artpyak <art.pyak@yandex.ru>
2025-02-19 13:59:18 +0300

    BJS2-61770 1. Checkstyle bug fix

--- src/main/java/school/faang/blocking_spotify/BJS2_61770/Music.java
+++ src/main/java/school/faang/blocking_spotify/BJS2_61770/Music.java
@@ -1,13 +1,18 @@
 package school.faang.blocking_spotify.BJS2_61770;
 
 public class Music {
+    private Thread playThread;
+    private Thread pauseThread;
+    private Thread skipThread;
+    private Thread previousThread;
+
     public void multiUserSimulation() {
         Player player = new Player();
 
-        Thread playThread = new Thread(() -> player.play());
-        Thread pauseThread = new Thread(() -> player.pause());
-        Thread skipThread = new Thread(() -> player.skip());
-        Thread previousThread = new Thread(() -> player.previous());
+        playThread = new Thread(() -> player.play());
+        pauseThread = new Thread(() -> player.pause());
+        skipThread = new Thread(() -> player.skip());
+        previousThread = new Thread(() -> player.previous());
 
         playThread.start();
         pauseThread.start();
