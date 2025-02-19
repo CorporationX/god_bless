6dbdccb3723b2ee80f1005570b2906a5966c8030
Anton Kapshukov <kapshukov.04@mail.ru>
2025-02-19 10:40:57 +0300

    Блокировка Spotify

---
+++ src/main/java/school/faang/blockingspotify/Music.java
@@ -0,0 +1,17 @@
+package school.faang.blockingspotify;
+
+public class Music {
+    public static void main(String[] args) {
+        Player player = new Player();
+
+        Thread user1 = new Thread(player::play, "Пользователь 1");
+        Thread user2 = new Thread(player::pause, "Пользователь 2");
+        Thread user3 = new Thread(player::skip, "Пользователь 3");
+        Thread user4 = new Thread(player::previous, "Пользователь 4");
+
+        user1.start();
+        user2.start();
+        user3.start();
+        user4.start();
+    }
+}
