85de12e562d295e576053378e7d57b430adb1036
amanzholtanat <amanzholtanat@gmail.com>
2025-02-19 08:50:40 +0500

    WeasleyFamily - BJS2-61164

--- src/main/java/school/faang/chores/Chore.java
+++ src/main/java/school/faang/chores/Chore.java
@@ -4,15 +4,20 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.SneakyThrows;
 
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 @Getter
 @AllArgsConstructor
 public class Chore implements Runnable {
     private final String title;
+    private static final Logger LOGGER = Logger.getLogger(Chore.class.getName());
 
     @SneakyThrows
     @Override
     public void run() {
-        System.out.println(Thread.currentThread().getName() + " is doing " + getTitle());
+        LOGGER.setLevel(Level.INFO);
+        LOGGER.info(Thread.currentThread().getName() + " is doing " + getTitle());
         Thread.sleep(1000);
     }
 }
--- src/main/java/school/faang/chores/WeasleyFamily.java
+++ src/main/java/school/faang/chores/WeasleyFamily.java
@@ -2,18 +2,22 @@ package school.faang.chores;
 
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 public class WeasleyFamily {
-    public static void main(String[] args) {
-        String[] chores = {"Wash dishes", "Clean the house", "Water plants"};
-        ExecutorService executor = Executors.newCachedThreadPool();
+    private static final String[] CHORES = {"Wash dishes", "Clean the house", "Water plants"};
+    private static final Logger LOGGER = Logger.getLogger(WeasleyFamily.class.getName());
 
-        for (String string : chores) {
+    public static void main(String[] args) {
+        ExecutorService executor = Executors.newCachedThreadPool();
+        LOGGER.setLevel(Level.INFO);
+        for (String string : CHORES) {
             Chore chore = new Chore(string);
             executor.execute(chore);
             chore.run();
         }
         executor.shutdownNow();
-        System.out.println("The work is done");
+        LOGGER.info("The work is done");
     }
 }
