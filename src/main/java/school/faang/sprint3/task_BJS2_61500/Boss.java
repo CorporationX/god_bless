04c660a6143b1205371546bdfad130eeaca43538
Ilhan Mustafa <ilhnmst@gmail.com>
2025-02-19 15:29:47 +0300

    create logs

--- src/main/java/school/faang/sprint3/task_BJS2_61500/Boss.java
+++ src/main/java/school/faang/sprint3/task_BJS2_61500/Boss.java
@@ -1,6 +1,7 @@
 package school.faang.sprint3.task_BJS2_61500;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -8,32 +9,37 @@ import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.stream.IntStream;
 
+@Slf4j
 @RequiredArgsConstructor
 public class Boss {
     private static final ExecutorService EXECUTOR = Executors.newFixedThreadPool(4);
     private static final Boss BOSS = new Boss(2);
+    private static final String logForm = "Игрок {} {} бой";
+    private static final String wait = "ждет";
+    private static final String join = "идет в";
+    private static final String leave = "покидает";
 
     public static void main(String[] args) {
         IntStream.range(1, 11)
-                .forEach(num -> {
-                    Player player = new Player("Player" + num);
-                    EXECUTOR.submit(() -> player.doBattle(BOSS));
-                });
+                .forEach(num -> EXECUTOR.submit(() -> new Player("Player" + num).doBattle(BOSS)));
         EXECUTOR.shutdown();
     }
 
     private final int maxPlayers;
-    private volatile List<Player> players = new ArrayList<>();
+    private final List<Player> players = new ArrayList<>();
 
     public synchronized void joinBattle(Player player) throws InterruptedException {
         while (players.size() == maxPlayers) {
+            log.info(logForm, player, wait);
             this.wait();
         }
+        log.info(logForm, player, join);
         players.add(player);
     }
 
     public synchronized void leaveBattle(Player player) {
         if (!players.isEmpty()) {
+            log.info(logForm, player, leave);
             players.remove(player);
         }
         this.notify();
