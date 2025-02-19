f31c49f6da5a426e62f636f3ce22f2803b488587
Ivan Prygunov <88151612+juzu400@users.noreply.github.com>
2025-02-19 14:20:50 +0300

    Задача Обрабатываем ошибки красиво

--- src/main/java/school/faang/Main.java
+++ src/main/java/school/faang/Main.java
@@ -1,6 +1,30 @@
 package school.faang;
 
+import java.util.function.Supplier;
+
 public class Main {
     public static void main(String[] args) {
+        String result = withErrorHandling(
+                () -> RemoteService.call("some Param"),
+                e -> {
+                    System.out.printf("Ошибка при вызове сервиса: %s\n", e.getMessage());
+                    return e.getMessage();
+                }
+        );
+        System.out.println(result);
+    }
+
+    public static class RemoteService {
+        public static String call(String param) {
+            throw new RuntimeException("Сервис недоступен");
+        }
+    }
+
+    public static <T> T withErrorHandling(Supplier<T> action, ErrorHandler<T> errorHandler) {
+        try {
+            return action.get();
+        } catch (Exception e) {
+            return errorHandler.handle(e);
+        }
     }
 }
