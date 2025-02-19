c8ae324d151450dd9bb03893a32454533d47ffdb
Ilhan Mustafa <ilhnmst@gmail.com>
2025-02-19 10:49:17 +0300

    Resolving

--- src/main/java/school/faang/sprint3/BJS2_60648/MailSender.java
+++ src/main/java/school/faang/sprint3/BJS2_60648/MailSender.java
@@ -3,21 +3,19 @@ package school.faang.sprint3.BJS2_60648;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
 
 
 public class MailSender {
     private static final List<String> EMAILS = new ArrayList<>();
     private static final int COUNT_EMAILS = 200;
-    private static final ExecutorService EXECUTOR = Executors.newFixedThreadPool(COUNT_EMAILS);
+    private static final int startIndex = 0;
 
     static {
         String[] strings = {"one", "two", "three"};
         Random random = new Random();
 
         for (int i = 0; i < 1000; i++) {
-            EMAILS.add(strings[random.nextInt(0, 3)]);
+            EMAILS.add(strings[random.nextInt(startIndex, strings.length)]);
         }
     }
 
@@ -28,12 +26,20 @@ public class MailSender {
 
         for (int i = 0; i < size; i += COUNT_EMAILS) {
             end = Math.min(i + COUNT_EMAILS, size);
-            EXECUTOR.submit(new SenderRunnable(threadNumber++, EMAILS.subList(i, end)));
+            Thread thread = new Thread(new SenderRunnable(threadNumber++, EMAILS.subList(i, end)));
+            thread.start();
         }
 
-        EXECUTOR.shutdown();
-        while (!EXECUTOR.isTerminated()) {}
+        sleep();
         // почему-то не всегда 1000 из 1000, бывает 997-1000
         System.out.printf("Обработано %d писем из %d\n", SenderRunnable.emailNumber - 1, EMAILS.size());
+    }
+
+    private static void sleep() {
+        try {
+            Thread.sleep(3_000);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
