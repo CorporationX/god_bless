406c246713467f04e4437466978667ba7a0b0411
Ivan Prygunov <88151612+juzu400@users.noreply.github.com>
2025-02-19 03:18:14 +0300

    Задача Мета-вселенная

--- src/main/java/school/faang/Main.java
+++ src/main/java/school/faang/Main.java
@@ -1,6 +1,26 @@
 package school.faang;
 
 public class Main {
+    private static final NotificationManager NOTIFICATION_MANAGER = new NotificationManager();
+
     public static void main(String[] args) {
+        registerNotification(NotificationType.EMAIL);
+        registerNotification(NotificationType.SMS);
+        registerNotification(NotificationType.PUSH);
+
+        sendNotification(NotificationType.SMS, "Код доступа 7765");
+        sendNotification(NotificationType.EMAIL, "Скидки до 23%");
+        sendNotification(NotificationType.PUSH, "Sent a reel by faangschool");
+    }
+
+    public static void registerNotification(NotificationType notificationType) {
+        NOTIFICATION_MANAGER.registerHandler(notificationType,
+                notification -> System.out.printf("%s: %s\n",
+                        notification.getNotificationType(), notification.getMessage()));
+    }
+
+    public static void sendNotification(NotificationType notificationType, String message) {
+        Notification notification = new Notification(notificationType, message);
+        NOTIFICATION_MANAGER.sendNotification(notification);
     }
 }
