062c8b3502ff93a0c2db4c51b25452929ed46b60
Ilya Yanchenko <zsdfghjkl111@gmail.com>
2025-02-19 19:03:02 +0700

    BJS2-58102 fix on comment

--- src/main/java/BJS258102/UserAction.java
+++ src/main/java/BJS258102/UserAction.java
@@ -1,9 +1,10 @@
 package BJS258102;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
-
 import java.time.LocalDate;
 
+@AllArgsConstructor
 @Data
 public class UserAction {
     private int id;
@@ -11,13 +12,5 @@ public class UserAction {
     private ActionType actionType;
     private LocalDate date;
     private String content;
-
-    public UserAction(Integer id, String name, ActionType actionType, LocalDate date, String content) {
-        this.id = id;
-        this.name = name;
-        this.actionType = actionType;
-        this.date = date;
-        this.content = content;
-    }
 
 }
