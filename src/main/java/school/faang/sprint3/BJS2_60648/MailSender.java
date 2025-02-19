76c94dbd155f33ea0f0d67dc73eeee9b556151e6
Ilhan Mustafa <ilhnmst@gmail.com>
2025-02-19 15:35:50 +0300

    create constants

--- src/main/java/school/faang/sprint3/BJS2_60648/MailSender.java
+++ src/main/java/school/faang/sprint3/BJS2_60648/MailSender.java
@@ -8,14 +8,17 @@ import java.util.Random;
 public class MailSender {
     private static final List<String> EMAILS = new ArrayList<>();
     private static final int COUNT_EMAILS = 200;
-    private static final int startIndex = 0;
+    private static final int START_INDEX = 0;
+    private static final int ZERO = 0;
+    private static final int EMAIL_COUNT = 1000;
+    private static final int THREAD_SLEEP = 3000;
 
     static {
         String[] strings = {"one", "two", "three"};
         Random random = new Random();
 
-        for (int i = 0; i < 1000; i++) {
-            EMAILS.add(strings[random.nextInt(startIndex, strings.length)]);
+        for (int i = ZERO; i < EMAIL_COUNT; i++) {
+            EMAILS.add(strings[random.nextInt(START_INDEX, strings.length)]);
         }
     }
 
@@ -31,13 +34,12 @@ public class MailSender {
         }
 
         sleep();
-        // почему-то не всегда 1000 из 1000, бывает 997-1000
         System.out.printf("Обработано %d писем из %d\n", SenderRunnable.emailNumber - 1, EMAILS.size());
     }
 
     private static void sleep() {
         try {
-            Thread.sleep(3_000);
+            Thread.sleep(THREAD_SLEEP);
         } catch (InterruptedException e) {
             throw new RuntimeException(e);
         }
