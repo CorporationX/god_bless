e3becfb0f7490cebcdd9319c12d0a10ada3bf0ee
Anton Lukyanchikov <antontotenkopf@gmail.com>
2025-02-19 19:29:41 +0300

    refactored checkstyle

--- src/main/java/task_BJS2_63835/Music.java
+++ src/main/java/task_BJS2_63835/Music.java
@@ -1,17 +1,24 @@
 package task_BJS2_63835;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Music {
+    private static final List<Thread> threads = new ArrayList<>();
+
     public static void main(String[] args) {
         Player player = new Player(false);
-
         Thread firstThread = new Thread(player::play);
         Thread secondThread = new Thread(player::pause);
         Thread thirdThread = new Thread(player::skip);
         Thread fourthThread = new Thread(player::previous);
 
-        firstThread.start();
-        secondThread.start();
-        thirdThread.start();
-        fourthThread.start();
+        threads.add(firstThread);
+        threads.add(secondThread);
+        threads.add(thirdThread);
+        threads.add(fourthThread);
+
+        threads.stream().forEach(Thread::start);
+
     }
 }
