5d7e4e77ad64d19030ab31ac6821d108c38cdaa1
Artpyak <art.pyak@yandex.ru>
2025-02-19 09:48:24 +0300

    BJS2-57733 1. Метод call удалён. 2. Строковое значение DEFAULT теперь константа. 3. Входное значение help заменено на requestMessage.

--- src/main/java/school/faang/handling_mistakes_beautifully/BJS2_57733/Main.java
+++ src/main/java/school/faang/handling_mistakes_beautifully/BJS2_57733/Main.java
@@ -1,12 +1,14 @@
 package school.faang.handling_mistakes_beautifully.BJS2_57733;
 
 public class Main {
+    private static final String DEFAULT = "DEFAULT";
+
     public static void main(String[] args) {
         String result = ErrorHandler.withErrorHandling(
                 () -> RemoteService.callService("Gondor is calling for help!"),
                 e -> {
                     System.out.println("Ошибка при вызове сервиса, возвращаем дефолтное значение");
-                    return "DEFAULT";
+                    return DEFAULT;
                 }
         );
 
@@ -15,12 +17,8 @@ public class Main {
 
 
     public static class RemoteService {
-        public static String call(String param) {
-            throw new RuntimeException("Сервис недоступен");
-        }
-
-        public static String callService(String help) {
-            return help;
+        public static String callService(String requestMessage) {
+            return requestMessage;
         }
     }
 }
