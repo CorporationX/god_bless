c18ebc31e88492904b38e5968051cdbfa98291a6
Denis Popov <popoffdn@mail.ru>
2025-02-19 23:09:24 +0300

    ревью BJS2-58381

---
+++ src/main/java/StreamAPIOne/Main.java
@@ -0,0 +1,21 @@
+package StreamAPIOne;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) {
+        List<Integer> numbers = Arrays.asList(1, 2, 3, 4, 5, 6);
+        List<String> strings = Arrays.asList("apple", "banana", "cherry", "date");
+
+        System.out.println("Сумма четных чисел: " + ListOperations.sumOfEvenNumbers(numbers));
+        System.out.println("Максимум: " + ListOperations.findMaxNumber(numbers));
+        System.out.println("Среднее: " + ListOperations.findAverageNumber(numbers));
+        System.out.println("Количество строк, начинающихся на 'a': " + ListOperations.numberOfLines(strings, 'a'));
+        System.out.println("Строки, содержащие 'an': " + ListOperations.stringFilter(strings, "an"));
+        System.out.println("Отсортированные по длине строки: " + ListOperations.sortedList(strings));
+        System.out.println("Все ли числа чётные? " + ListOperations.satisfactionOfCondition(numbers, n -> n % 2 == 0));
+        System.out.println("Наименьшее число больше 4: " + ListOperations.findMinNumber(numbers, 4));
+        System.out.println("Длины строк: " + ListOperations.stringLength(strings));
+    }
+}
