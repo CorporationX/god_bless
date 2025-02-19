1df559ca3ba7d2a58d42dd23934a790d30eb6878
Ivan Prygunov <88151612+juzu400@users.noreply.github.com>
2025-02-19 15:04:31 +0300

    Задача Google Maps

---
+++ src/main/java/school/faang/LocationSearchEngine.java
@@ -0,0 +1,42 @@
+package school.faang;
+
+import lombok.NonNull;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.function.Consumer;
+import java.util.function.Function;
+import java.util.function.Predicate;
+
+public class LocationSearchEngine {
+    public List<Location> filterLocation(@NonNull List<Location> locations, Predicate<Location> predicate) {
+        List<Location> filteredLocations = new ArrayList<>();
+        for (Location location : locations) {
+            if (predicate.test(location)) {
+                filteredLocations.add(location);
+            }
+        }
+        return filteredLocations;
+    }
+
+    public void processLocations(@NonNull List<Location> locations, Consumer<Location> consumer) {
+        for (Location location : locations) {
+            consumer.accept(location);
+        }
+    }
+
+    public List<Double> calculateDistances(@NonNull List<Location> locations,
+                                           Function<Location, Double> distanceLocation) {
+        List<Double> distances = new ArrayList<>();
+        for (Location location : locations) {
+            distances.add(distanceLocation.apply(location));
+        }
+        return distances;
+    }
+
+    public void processDistances(@NonNull List<Double> locations, Consumer<Double> consumer) {
+        for (Double location : locations) {
+            consumer.accept(location);
+        }
+    }
+}
--- src/main/java/school/faang/Main.java
+++ src/main/java/school/faang/Main.java
@@ -1,6 +1,33 @@
 package school.faang;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
+        LocationSearchEngine engine = new LocationSearchEngine();
+
+        List<Location> locations = new ArrayList<>(Arrays.asList(
+                new Location("Eiffel Tower", 48.8584, 2.2945),
+                new Location("Statue of Liberty", 40.6892, -74.0445),
+                new Location("Great Wall of China", 40.4319, 116.5704)
+        ));
+
+        List<Location> filteredLocations = engine.filterLocation(locations,
+                (location) -> location.getLongitude() > 0);
+        engine.processLocations(filteredLocations,
+                (location) -> System.out.printf("Локация: %s\n", location.getName()));
+
+        double baseLatitude = 37.4220;
+        double baseLongitude = -122.0841;
+        List<Double> distances = engine.calculateDistances(locations, (location) -> {
+            double latitudeDiff = Math.abs(location.getLatitude() - baseLatitude);
+            double longitudeDiff = Math.abs(location.getLongitude() - baseLongitude);
+            return Math.sqrt(Math.pow(latitudeDiff, 2) + Math.pow(longitudeDiff, 2));
+        });
+
+        engine.processDistances(distances,
+                (distance) -> System.out.printf("Distance: %s\n", distance));
     }
 }
