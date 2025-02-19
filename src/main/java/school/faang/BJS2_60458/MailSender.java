cf60f348b12afc69d5c0b666c602a99ce62a6e22
RamilNazmiev192 <nazmiev633@gmail.com>
2025-02-19 14:30:35 +0300

    Bugfix: Handle remainder messages when dividing workload

--- src/main/java/school/faang/BJS2_60458/MailSender.java
+++ src/main/java/school/faang/BJS2_60458/MailSender.java
@@ -6,19 +6,26 @@ import org.slf4j.LoggerFactory;
 public class MailSender {
     private static final Logger logger = LoggerFactory.getLogger(MailSender.class);
 
-    private static final int THREAD_MESSAGE = 1000;
+    private static final int THREAD_MESSAGE = 1003;
     private static final int THREAD_COUNT = 5;
 
     public static void main(String[] args) {
         int batchSize = THREAD_MESSAGE / THREAD_COUNT;
+        int remainder = THREAD_MESSAGE % THREAD_COUNT;
+
         Thread[] threads = new Thread[THREAD_COUNT];
 
+        int start = 1;
         for (int i = 0; i < THREAD_COUNT; i++) {
-            int start = i * batchSize;
-            int end = (i + 1) * batchSize;
+            int end = start + batchSize;
+
+            if (i == THREAD_COUNT - 1) {
+                end += remainder;
+            }
 
             threads[i] = new Thread(new SenderRunnable(start, end));
             threads[i].start();
+            start = end;
         }
 
         for (Thread thread : threads) {
