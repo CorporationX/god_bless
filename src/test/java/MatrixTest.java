a5555549df75b5b74bfb44fbabea80db71ac73cc
Ivan Prygunov <88151612+juzu400@users.noreply.github.com>
2025-02-19 17:59:32 +0300

    Добавил DisplayName к тестам

--- src/test/java/MatrixTest.java
+++ src/test/java/MatrixTest.java
@@ -1,10 +1,12 @@
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import school.faang.FlipDirection;
 import school.faang.Matrix;
 
 class MatrixTest {
     @Test
+    @DisplayName("Проверка невалидных входных данных (матрица = null)")
     void flipNullMatrixTest() {
         int[][] matrix = null;
 
@@ -13,6 +15,7 @@ class MatrixTest {
     }
 
     @Test
+    @DisplayName("Проверка невалидных входных данных (непредусмотренный тип поворота)")
     void invalidFlipDirectionTest() {
         int[][] matrix = {{1, 2, 3}, {4, 5, 6}, {7, 8, 9}};
 
@@ -21,6 +24,7 @@ class MatrixTest {
     }
 
     @Test
+    @DisplayName("Проверка горизонтального поворота матрицы 3 на 3")
     void flipMatrix3x3HorizontalTest() {
         int[][] matrix = {{1, 2, 3}, {4, 5, 6}, {7, 8, 9}};
         int[][] expected = {{3, 2, 1}, {6, 5, 4}, {9, 8, 7}};
@@ -30,6 +34,7 @@ class MatrixTest {
     }
 
     @Test
+    @DisplayName("Проверка вертикального поворота матрицы 3 на 3")
     void flipMatrix3x3VerticalTest() {
         int[][] matrix = {{1, 2, 3}, {4, 5, 6}, {7, 8, 9}};
         int[][] expected = {{7, 8, 9}, {4, 5, 6}, {1, 2, 3}};
@@ -39,6 +44,7 @@ class MatrixTest {
     }
 
     @Test
+    @DisplayName("Проверка горизонтального поворота матрицы 4 на 4")
     void flipMatrix4x4HorizontalTest() {
         int[][] matrix = {{5, 6, 7, 8}, {2, 3, 5, 1}, {1, 4, 0, 4}, {0, 5, 6, 7}};
         int[][] expected = {{8, 7, 6, 5}, {1, 5, 3, 2}, {4, 0, 4, 1}, {7, 6, 5, 0}};
@@ -48,6 +54,7 @@ class MatrixTest {
     }
 
     @Test
+    @DisplayName("Проверка вертикального поворота матрицы 4 на 4")
     void flipMatrix4x4VerticalTest() {
         int[][] matrix = {{5, 6, 7, 8}, {2, 3, 5, 1}, {1, 4, 0, 4}, {0, 5, 6, 7}};
         int[][] expected = {{0, 5, 6, 7}, {1, 4, 0, 4}, {2, 3, 5, 1}, {5, 6, 7, 8}};
