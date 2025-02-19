7ef38b405a5f59f3137d139e228d8f86ed863300
Petr Stratonnikov <pstratonni@gmail.com>
2025-02-19 12:22:03 +0100

    The task BJS2-60864

---
+++ src/main/java/school/faang/path/City.java
@@ -0,0 +1,10 @@
+package school.faang.path;
+
+import lombok.Data;
+
+@Data
+public class City {
+
+    private final String name;
+    private final Location location;
+}
---
+++ src/main/java/school/faang/path/Witcher.java
@@ -0,0 +1,51 @@
+package school.faang.path;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+public class Witcher {
+    public static void main(String[] args) {
+        List<String> cityNames = List.of("Tokyo", "Delhi", "Shanghai", "São Paulo", "Mexico City",
+                "Cairo", "Mumbai", "Beijing", "Dhaka", "Osaka",
+                "New York", "Karachi", "Buenos Aires", "Istanbul", "Chongqing",
+                "Kolkata", "Lagos", "Kinshasa", "Manila", "Rio de Janeiro");
+        List<String> monsterNames = List.of(
+                "Leshen", "Griffin", "Drowner", "Alghoul", "Basilisk",
+                "Fiend", "Striga", "Ekhidna", "Katakan", "Bruxa"
+        );
+
+        List<City> cities = new ArrayList<>();
+        for (String cityName : cityNames) {
+            cities.add(new City(cityName,
+                    new Location(new Random().nextInt(351) + 50,
+                            new Random().nextInt(351) + 50)));
+        }
+
+        List<Monster> monsters = new ArrayList<>();
+        for (String monsterName : monsterNames) {
+            monsters.add(new Monster(monsterName,
+                    new Location(new Random().nextInt(351) + 50,
+                            new Random().nextInt(351) + 50)));
+        }
+
+        List<CityWorker> cityWorkers = new ArrayList<>();
+        for (City city : cities) {
+            cityWorkers.add(new CityWorker(city, monsters));
+        }
+
+        int numThreads = 10;
+        ExecutorService executorService = Executors.newFixedThreadPool(numThreads);
+        long startTime = System.currentTimeMillis();
+        for (CityWorker cityWorker : cityWorkers) {
+            executorService.submit(cityWorker);
+        }
+        executorService.shutdown();
+        long endTime = System.currentTimeMillis();
+        System.out.println("Total time taken: " + (endTime - startTime) + "ms");
+    }
+
+
+}
