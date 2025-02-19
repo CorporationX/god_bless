e2de47b4b27f0c45b942f48da0527811bb367a68
Ivan Prygunov <88151612+juzu400@users.noreply.github.com>
2025-02-19 18:05:28 +0300

    Создал отдельные классы ExceptionHandler и RemoteService

--- src/main/java/school/faang/Main.java
+++ src/main/java/school/faang/Main.java
@@ -1,30 +1,17 @@
 package school.faang;
 
-import java.util.function.Supplier;
-
 public class Main {
     public static void main(String[] args) {
-        String result = withErrorHandling(
-                () -> RemoteService.call("some Param"),
+        RemoteService remoteService = new RemoteService();
+        ExceptionHandler exceptionHandler = new ExceptionHandler();
+
+        String result = exceptionHandler.withErrorHandling(
+                () -> remoteService.call("some Param"),
                 e -> {
                     System.out.printf("Ошибка при вызове сервиса: %s\n", e.getMessage());
                     return e.getMessage();
                 }
         );
         System.out.println(result);
-    }
-
-    public static class RemoteService {
-        public static String call(String param) {
-            throw new RuntimeException("Сервис недоступен");
-        }
-    }
-
-    public static <T> T withErrorHandling(Supplier<T> action, ErrorHandler<T> errorHandler) {
-        try {
-            return action.get();
-        } catch (Exception e) {
-            return errorHandler.handle(e);
-        }
     }
 }
