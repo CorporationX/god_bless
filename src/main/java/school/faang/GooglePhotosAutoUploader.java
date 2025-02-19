485dabd083926168ae171eb258096ce7668e48a3
Sergey Ermoshenko <ermoshenko.work@gmail.com>
2025-02-19 15:08:57 +0300

    add photos uploader

---
+++ src/main/java/school/faang/GooglePhotosAutoUploader.java
@@ -0,0 +1,40 @@
+package school.faang;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class GooglePhotosAutoUploader {
+    private static final Logger logger = LoggerFactory.getLogger(GooglePhotosAutoUploader.class);
+    private final Object lock = new Object();
+    private final List<String> photosToUpload = new ArrayList<>();
+
+    public void startAutoUpload() {
+        synchronized (lock) {
+            if (photosToUpload.isEmpty()) {
+                try {
+                    lock.wait();
+                } catch (InterruptedException e) {
+                    logger.error("Wait was interrupted.");
+                    throw new RuntimeException(e);
+                }
+            }
+            updatePhotos();
+        }
+    }
+
+    public void onNewPhotoAdded(String photoPath) {
+        synchronized (lock) {
+            logger.info("{} has been uploaded to photos", photoPath);
+            photosToUpload.add(photoPath);
+            lock.notify();
+        }
+    }
+
+    private void updatePhotos() {
+        logger.info("The photos have been uploaded.");
+        photosToUpload.clear();
+    }
+}
