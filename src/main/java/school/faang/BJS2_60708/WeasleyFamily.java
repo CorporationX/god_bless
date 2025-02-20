0b798d8eb071e3432da3e85e8a4e1cc1fd83d80a
RamilNazmiev192 <nazmiev633@gmail.com>
2025-02-20 15:51:42 +0300

    fix: add awaitTermination in  WeasleyFamily

--- src/main/java/school/faang/BJS2_60708/WeasleyFamily.java
+++ src/main/java/school/faang/BJS2_60708/WeasleyFamily.java
@@ -1,19 +1,24 @@
 package school.faang.BJS2_60708;
 
 import lombok.AllArgsConstructor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.util.List;
 import java.util.Objects;
 import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.ExecutorService;
+import java.util.concurrent.TimeUnit;
 
 import static java.util.concurrent.Executors.newCachedThreadPool;
 
 @AllArgsConstructor
 public class WeasleyFamily {
     private final List<Chore> chores;
+    private final Logger logger = LoggerFactory.getLogger(WeasleyFamily.class);
 
     public void actionsCore() throws InterruptedException {
+
         Objects.requireNonNull(chores, "List<Chore> can't be null");
 
         CountDownLatch latch = new CountDownLatch(chores.size());
@@ -31,6 +36,10 @@ public class WeasleyFamily {
             latch.await();
         } finally {
             executor.shutdown();
+            if (!executor.awaitTermination(10, TimeUnit.SECONDS)) {
+                logger.error("Не все потоки завершились вовремя, принудительно завершаем...");
+                executor.shutdownNow();
+            }
         }
 
         System.out.println("Все домашние дела семьи Уизли выполнены");
