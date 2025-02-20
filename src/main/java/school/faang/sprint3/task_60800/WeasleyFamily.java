9e7b9758f70ea886fc67139da49d5095a61f0e0e
Ilhan Mustafa <ilhnmst@gmail.com>
2025-02-20 13:58:15 +0300

    Resolving

--- src/main/java/school/faang/sprint3/task_60800/WeasleyFamily.java
+++ src/main/java/school/faang/sprint3/task_60800/WeasleyFamily.java
@@ -1,24 +1,41 @@
 package school.faang.sprint3.task_60800;
 
+import lombok.extern.slf4j.Slf4j;
+
 import java.util.Arrays;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
 
+@Slf4j
 public class WeasleyFamily {
     private static final String[] CHORES = {"помыть посуду", "подмести пол", "приготовить ужин"};
     private static final ExecutorService EXECUTOR = Executors.newFixedThreadPool(3);
+    private static final int WAIT_TIMEOUT_MS = 3000;
 
     public static void main(String[] args) {
         Arrays.stream(CHORES).forEach(chore -> EXECUTOR.submit(new Chore(chore)));
         EXECUTOR.shutdown();
+
+        try {
+            if (EXECUTOR.awaitTermination(1, TimeUnit.SECONDS)) {
+                EXECUTOR.shutdownNow();
+            }
+        } catch (InterruptedException e) {
+            EXECUTOR.shutdownNow();
+            log.error("Ошибка прерывания", e);
+        }
+
         sleep();
-        System.out.printf("%d задачи из %d выполнены", Chore.count - 1, CHORES.length);
+        System.out.printf("%d задачи из %d выполнены", Chore.COUNT.get(), CHORES.length);
     }
 
     private static void sleep() {
         try {
-            Thread.sleep(3000);
+            Thread.sleep(WAIT_TIMEOUT_MS);
         } catch (InterruptedException e) {
+            Thread.currentThread().interrupt();
+            log.error("Поток прерван", e);
             throw new RuntimeException(e);
         }
     }
