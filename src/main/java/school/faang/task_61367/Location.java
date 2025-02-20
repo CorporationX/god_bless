3602491fe6a976a3f5e587042a50e556a58bdcd8
s3nkyh <olkin.yaroslaw@yandex.ru>
2025-02-20 19:17:47 +0300

    BJS2-61367

---
+++ src/main/java/school/faang/task_61367/Location.java
@@ -0,0 +1,11 @@
+package school.faang.task_61367;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class Location {
+    private final int positionX;
+    private final int positionY;
+}
---
+++ src/main/java/school/faang/task_61367/Witcher.java
@@ -0,0 +1,43 @@
+package school.faang.task_61367;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
+
+public class Witcher {
+    private static final int NUM_THREADS = 4;
+
+    public static void main(String[] args) throws InterruptedException {
+        List<City> cities = new ArrayList<>();
+        cities.add(new City("Novigrad", new Location(0, 60)));
+        cities.add(new City("Oxenfurt", new Location(60, 0)));
+        cities.add(new City("Vizima", new Location(120, 50)));
+        cities.add(new City("Kaer Morhen", new Location(180, 70)));
+
+        List<Monster> monsters = new ArrayList<>();
+        monsters.add(new Monster("Griffin", new Location(10, 15)));
+        monsters.add(new Monster("Basilisk", new Location(235, 121)));
+        monsters.add(new Monster("Cockatrice", new Location(-124, 36)));
+        monsters.add(new Monster("Chort", new Location(374, -237)));
+
+        ExecutorService executorService = Executors.newFixedThreadPool(NUM_THREADS);
+
+        for (City city : cities) {
+            CityWorker worker = new CityWorker(city, monsters);
+            executorService.submit(worker);
+        }
+
+        executorService.shutdown();
+        try {
+            if (!executorService.awaitTermination(1, TimeUnit.MINUTES)) {
+                executorService.shutdownNow();
+            }
+        } catch (InterruptedException e) {
+            executorService.shutdownNow();
+        }
+
+        System.out.println("All tasks are completed.");
+    }
+}
