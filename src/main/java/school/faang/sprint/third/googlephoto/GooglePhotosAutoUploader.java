53bd64202009f1efea8026941ccc6c7633a5b6ee
vtroshin <vtroshin@rvision.ru>
2025-02-20 12:54:23 +0300

    BJS2-62015 Google Photo sync

---
+++ src/main/java/school/faang/sprint/third/googlephoto/GooglePhotosAutoUploader.java
@@ -0,0 +1,49 @@
+package school.faang.sprint.third.googlephoto;
+
+import lombok.extern.slf4j.Slf4j;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Slf4j
+public class GooglePhotosAutoUploader {
+    private final List<String> photosToUpload = new ArrayList<>();
+
+    public void startAutoUpload() {
+        synchronized (photosToUpload) {
+            log.info("Starting auto upload...");
+            if (photosToUpload.isEmpty()) {
+                log.info("No photos to upload");
+                try {
+                    photosToUpload.wait();
+                } catch (InterruptedException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+            uploadPhotos();
+            log.info("Auto upload complete.");
+        }
+    }
+
+    public void onNewPhotoAdded(String photoPath) {
+        synchronized (photosToUpload) {
+            validatePhotoPath(photoPath);
+            log.info("New photo added: {}", photoPath);
+            photosToUpload.add(photoPath);
+            photosToUpload.notifyAll();
+        }
+    }
+
+    private void uploadPhotos() {
+        log.info("Uploading photos...");
+        photosToUpload.forEach(googlePhotoUrl -> log.info("{} photos uploaded", googlePhotoUrl));
+        photosToUpload.clear();
+        photosToUpload.notifyAll();
+    }
+
+    private void validatePhotoPath(String photoPath) {
+        if (photoPath == null || photoPath.isEmpty()) {
+            throw new IllegalArgumentException("Invalid photoPath");
+        }
+    }
+}
