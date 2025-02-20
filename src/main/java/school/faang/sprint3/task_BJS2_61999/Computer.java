59d3484134412b06eebebb5e03b2bf2dd5a87d0b
Ilhan Mustafa <ilhnmst@gmail.com>
2025-02-20 13:40:17 +0300

    Resolving

--- src/main/java/school/faang/sprint3/task_BJS2_61999/Computer.java
+++ src/main/java/school/faang/sprint3/task_BJS2_61999/Computer.java
@@ -1,31 +1,41 @@
 package school.faang.sprint3.task_BJS2_61999;
 
+import lombok.extern.slf4j.Slf4j;
+
 import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
+import java.util.stream.IntStream;
 
+@Slf4j
 public class Computer {
     private static final GooglePhotosAutoUploader GOOGLE_PHOTOS_AUTO_UPLOADER = new GooglePhotosAutoUploader();
-    private static final ExecutorService ADD_PHOTO_THREAD = Executors.newFixedThreadPool(1);
-    private static final ExecutorService UPLOAD_PHOTO_THREAD = Executors.newFixedThreadPool(1);
+    private static final ExecutorService EXECUTOR = Executors.newFixedThreadPool(4);
     private static final List<String> PATH_TO_PHOTOS = new ArrayList<>();
+    private static final int COUNT_PHOTO = 20;
 
     public static void main(String[] args) {
-        addNewPhoto("first");
-        addNewPhoto("second");
-        addNewPhoto("third");
-        uploadPhotos();
-        ADD_PHOTO_THREAD.shutdown();
-        UPLOAD_PHOTO_THREAD.shutdown();
+        IntStream.range(0, COUNT_PHOTO).forEach(num -> addNewPhoto("Photo" + num));
+        EXECUTOR.submit(GOOGLE_PHOTOS_AUTO_UPLOADER::startAutoUpload);
+
+        EXECUTOR.shutdown();
+
+        try {
+            if (!EXECUTOR.awaitTermination(1, TimeUnit.SECONDS)) {
+                EXECUTOR.shutdownNow();
+            }
+        } catch (InterruptedException e) {
+            EXECUTOR.shutdownNow();
+            log.error("Поток прерван", e);
+        }
+
+        GOOGLE_PHOTOS_AUTO_UPLOADER.printResult();
     }
 
     public static void addNewPhoto(String pathToPhoto) {
         PATH_TO_PHOTOS.add(pathToPhoto);
-        ADD_PHOTO_THREAD.submit(() -> GOOGLE_PHOTOS_AUTO_UPLOADER.onNewPhotoAdded(pathToPhoto));
-    }
-
-    private static void uploadPhotos() {
-        UPLOAD_PHOTO_THREAD.submit(GOOGLE_PHOTOS_AUTO_UPLOADER::startAutoUpload);
+        EXECUTOR.submit(() -> GOOGLE_PHOTOS_AUTO_UPLOADER.onNewPhotoAdded(pathToPhoto));
     }
 }
--- src/main/java/school/faang/sprint3/task_BJS2_61999/GooglePhotosAutoUploader.java
+++ src/main/java/school/faang/sprint3/task_BJS2_61999/GooglePhotosAutoUploader.java
@@ -1,34 +1,54 @@
 package school.faang.sprint3.task_BJS2_61999;
 
+
+import lombok.extern.slf4j.Slf4j;
+
 import java.util.ArrayList;
 import java.util.List;
 
+@Slf4j
 public class GooglePhotosAutoUploader {
-
     private final List<String> photosToUpload = new ArrayList<>();
+    private final Object lock = new Object();
+    private int photosAdded;
+    private int photosDownloaded;
 
-    public synchronized void startAutoUpload() {
-        if (photosToUpload.isEmpty()) {
-            waiting();
-        }
-        uploadPhotos();
+    public void printResult() {
+        log.debug("Добавлено {}, загружено {}, остались {}", photosAdded, photosDownloaded, photosToUpload);
     }
 
-    public synchronized void onNewPhotoAdded(String photoPath) {
-        photosToUpload.add(photoPath);
-        this.notify();
+    public void startAutoUpload() {
+        while (true) {
+            synchronized (lock) {
+                while (photosToUpload.isEmpty()) {
+                    try {
+                        lock.wait();
+                    } catch (InterruptedException e) {
+                        log.error("Поток прерван", e);
+                        throw new RuntimeException(e);
+                    }
+                }
+                uploadPhotos();
+            }
+        }
+    }
+
+    public void onNewPhotoAdded(String photo) {
+        synchronized (lock) {
+            log.debug("Фото {} добавлено", photo);
+            photosToUpload.add(photo);
+            photosAdded++;
+            lock.notify();
+        }
     }
 
     private void uploadPhotos() {
-        photosToUpload.forEach(photo -> System.out.printf("Фото %s загружено\n", photo));
+        photosToUpload.forEach(this::uploadPhoto);
         photosToUpload.clear();
     }
 
-    private void waiting() {
-        try {
-            this.wait();
-        } catch (InterruptedException e) {
-            throw new RuntimeException(e);
-        }
+    private void uploadPhoto(String photo) {
+        log.debug("Фото {} загружено", photo);
+        photosDownloaded++;
     }
 }
