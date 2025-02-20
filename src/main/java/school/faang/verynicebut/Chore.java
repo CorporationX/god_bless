8929bc6ebcc95386eabe3d9df192b726d48255a0
Marsel <mars1212@inbox.ru>
2025-02-21 00:03:33 +0300

    BJS2-60701 fix: add const, add Slf4j

--- src/main/java/school/faang/verynicebut/Chore.java
+++ src/main/java/school/faang/verynicebut/Chore.java
@@ -1,18 +1,22 @@
 package school.faang.verynicebut;
 
 import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 
+@Slf4j
 @AllArgsConstructor
 public class Chore implements Runnable {
+    private static final Integer TASK_DURATION_MS = 10;
     private final String title;
 
     @Override
     public void run() {
         try {
             System.out.printf("Выполняем задачу %s в потоке %s\n", title, Thread.currentThread().getName());
-            Thread.sleep(10);
+            Thread.sleep(TASK_DURATION_MS);
         } catch (InterruptedException e) {
-            throw new RuntimeException(e.getMessage());
+            Thread.currentThread().interrupt();
+            log.warn(e.getMessage());
         }
 
     }
--- src/main/java/school/faang/verynicebut/WeasleyFamily.java
+++ src/main/java/school/faang/verynicebut/WeasleyFamily.java
@@ -1,28 +1,34 @@
 package school.faang.verynicebut;
 
+import lombok.extern.slf4j.Slf4j;
+
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.TimeUnit;
 
+@Slf4j
 public class WeasleyFamily {
+    private static final String[] chores = {"помыть посуду", "подмести пол", "приготовить ужин",
+            "постирать одежду", "покормить Рона"};
+    private static final Integer TIMEOUT_SECONDS = 5;
+
     public static void main(String[] args) {
-        String[] chores = {"помыть посуду", "подмести пол", "приготовить ужин",
-                "постирать одежду", "покормить Рона"};
+
         ExecutorService service = Executors.newCachedThreadPool();
         for (String chore : chores) {
             service.submit(new Chore(chore));
         }
         service.shutdown();
 
-        int second = 5;
         try {
-            if (!service.awaitTermination(second, TimeUnit.SECONDS)) {
+            if (!service.awaitTermination(TIMEOUT_SECONDS, TimeUnit.SECONDS)) {
                 service.shutdownNow();
-                System.out.printf("Задачи не успели выполнится за %s секунд", second);
+                log.info("Задачи не успели выполнится за {} секунд", TIMEOUT_SECONDS);
             }
         } catch (InterruptedException e) {
-            throw new RuntimeException(e.getMessage());
+            log.warn(e.getMessage());
+            throw new RuntimeException();
         }
-        System.out.println("Все задачи выполниилсь");
+        log.info("Все задачи выполнились");
     }
 }
