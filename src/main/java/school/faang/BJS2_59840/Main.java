cf04e8b75db259c35144ee61c82e5489e4658168
Takoi <takoihero17@gmail.com>
2025-02-21 03:15:06 +0500

    Фильтры Instagram

--- src/main/java/school/faang/BJS2_59840/Main.java
+++ src/main/java/school/faang/BJS2_59840/Main.java
@@ -8,11 +8,13 @@ public class Main {
 
         FilterProcessor filterProcessor = new FilterProcessor();
 
-        Function<Image, Image> grayscaleFilter = (image) -> new Image(image.getName(), image.getDescription() + " | Фильтр: черно-белый");
-        Function<Image, Image> sepiaFilter = (image) -> new Image(image.getName(), image.getDescription() + " | Фильтр: сепия");
+        Function<Image, Image> grayscaleFilter =
+                (image) -> new Image(image.getName(), image.getDescription() + " | Фильтр: черно-белый");
+        Function<Image, Image> sepiaFilter =
+                (image) -> new Image(image.getName(), image.getDescription() + " | Фильтр: сепия");
 
         Image grayscaleImage = filterProcessor.applyFilter(originalImage, grayscaleFilter);
-        System.out.println(grayscaleImage.getDescription()); // Вывод: Оригинальное изображение | Фильтр: черно-белый
+        System.out.println(grayscaleImage.getDescription());
 
         Image sepiaImage = filterProcessor.applyFilter(grayscaleImage, sepiaFilter);
         System.out.println(sepiaImage.getDescription());
