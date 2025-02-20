c0a06e93b3b7d7ee10c5ebec0f1ac748f93d3052
Artem Doronin <doronintema2018@yandex.ru>
2025-02-20 23:08:37 +0500

    Объявление переменной 'previousThread' и её первое использование находятся на расстоянии 4 строк(и)

--- src/main/java/school/faang/task_61850/Music.java
+++ src/main/java/school/faang/task_61850/Music.java
@@ -4,10 +4,10 @@ public class Music {
     public static void main(String[] args) {
         Player player = new Player();
 
-        Thread playThread = new Thread(player::play);
-        Thread pauseThread = new Thread(player::pause);
-        Thread skipThread = new Thread(player::skip);
-        Thread previousThread = new Thread(player::previous);
+        final Thread playThread = new Thread(player::play);
+        final Thread pauseThread = new Thread(player::pause);
+        final Thread skipThread = new Thread(player::skip);
+        final Thread previousThread = new Thread(player::previous);
 
         playThread.start();
         pauseThread.start();
