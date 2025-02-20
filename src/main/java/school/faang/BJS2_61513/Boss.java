61cd80dc641e7bfbd1fbdd687163aeea1fcf8535
Sergey <bigwave2915@gmail.com>
2025-02-20 17:40:08 +0500

    решения задачи

--- src/main/java/school/faang/BJS2_61513/Boss.java
+++ src/main/java/school/faang/BJS2_61513/Boss.java
@@ -1,26 +1,31 @@
 package school.faang.BJS2_61513;
 
-import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 
+@Slf4j
 @RequiredArgsConstructor
 @Getter
 public class Boss {
-    private final int maxPlayers; //5
+    private final int maxPlayers;
     private int currentPlayers = 0;
 
-    private synchronized void joinBattle(Player player) throws InterruptedException {
+    public synchronized void joinBattle(Player player) throws InterruptedException {
         while (currentPlayers == maxPlayers) {
             this.wait();
+            log.info("{} ожидает сражения ", player.name());
         }
         currentPlayers++;
     }
 
-    private synchronized void leaveBattle(Player player) {
+    public synchronized void leaveBattle(Player player) {
         if (currentPlayers > 0) {
             currentPlayers--;
+            log.info("{} покинул сражение.", player.name());
+            this.notifyAll();
+        } else {
+            System.out.println("Ошибка: Нет игроков для выхода из сражения.");
         }
-        this.notify();
     }
 }
