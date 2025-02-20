ca543b6070bfbffec91e1def7b07c309a8059d35
Marsel <mars1212@inbox.ru>
2025-02-20 03:37:05 +0300

    BJS2-61632 fiw while loop

--- src/main/java/school/faang/googlephotosync/Main.java
+++ src/main/java/school/faang/googlephotosync/Main.java
@@ -8,10 +8,12 @@ public class Main {
         googlePhotosAutoUploader.onNewPhotoAdded("Anapa2006");
 
         Random rand = new Random();
+        Thread autoUploadThread = new Thread(googlePhotosAutoUploader::startAutoUpload);
+        autoUploadThread.setDaemon(true);
+        autoUploadThread.start();
         for (int i = 0; i < 10; i++) {
             String photo = "photo#" + rand.nextInt(100);
             new Thread(() -> googlePhotosAutoUploader.onNewPhotoAdded(photo)).start();
-            new Thread(googlePhotosAutoUploader::startAutoUpload).start();
         }
     }
 }
