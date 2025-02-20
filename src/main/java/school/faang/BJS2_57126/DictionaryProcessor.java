78b4316064d10093a7135504e228058f747e6b2b
Gulnaz <gumerova.gulnaz2014@yandex.ru>
2025-02-20 11:44:08 +0300

    В классе DictionaryProcessor в методе processWord добавлена проверка аргументов на null

--- src/main/java/school/faang/BJS2_57126/DictionaryProcessor.java
+++ src/main/java/school/faang/BJS2_57126/DictionaryProcessor.java
@@ -1,9 +1,17 @@
 package school.faang.BJS2_57126;
 
+import lombok.NonNull;
+
 import java.util.function.BiConsumer;
 
 public class DictionaryProcessor {
-    public void processWord(String word, String translation, BiConsumer<String, String> addWordToDictionary) {
+    public void processWord(@NonNull String word,
+                            @NonNull String translation,
+                            @NonNull BiConsumer<String, String> addWordToDictionary) {
+        if (word.isBlank() || translation.isBlank()) {
+            System.err.print("Error: word or translation are empty\n");
+            return;
+        }
         addWordToDictionary.accept(word, translation);
     }
 }
