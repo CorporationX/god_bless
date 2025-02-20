ace987c6c7a87673dfb5ff69b232bd926fd9cf3f
Savchenko <savchenko.ra@yandex.ru>
2025-02-20 21:55:59 +0300

    BJS2-61673 is done.

---
+++ src/main/java/school/faang/bjs2_61673/ActionType.java
@@ -0,0 +1,7 @@
+package school.faang.bjs2_61673;
+
+public enum ActionType {
+    SCORE_POINT,
+    LOSE_LIVE,
+    KILL_ENEMY
+}
---
+++ src/main/java/school/faang/bjs2_61673/Game.java
@@ -0,0 +1,57 @@
+package school.faang.bjs2_61673;
+
+import lombok.extern.slf4j.Slf4j;
+
+
+@Slf4j
+public class Game {
+    private int score;
+    private int lives = 10;
+    private final Object scoreLock = new Object();
+    private final Object livesLock = new Object();
+    private volatile boolean isGameOver = false;
+
+    public void update(ActionType action) {
+        synchronized (livesLock) {
+            if (isGameOver) return;
+
+            switch (action) {
+                case SCORE_POINT:
+                    synchronized (scoreLock) {
+                        if (isGameOver) return;
+                        score += 5;
+                        log.info("Points added! New score: {}", score);
+                    }
+                    break;
+
+                case LOSE_LIVE:
+                    lives--;
+                    log.info("Life lost! Remaining lives: {}", lives);
+                    if (lives == 0) {
+                        gameOver();
+                        return;
+                    }
+                    break;
+
+                case KILL_ENEMY:
+                    synchronized (scoreLock) {
+                        if (isGameOver) return;
+                        score += 20;
+                        log.info("Enemy killed! New score: {}", score);
+                    }
+                    break;
+            }
+        }
+    }
+
+    private void gameOver() {
+        if (!isGameOver) {
+            isGameOver = true;
+            log.warn("☠️ GAME OVER! Final score: {}", score);
+        }
+    }
+
+    public boolean isGameOver() {
+        return isGameOver;
+    }
+}
