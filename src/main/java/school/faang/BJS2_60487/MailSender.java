1e771c0d21be5c568a49afdfc819ad71f8971a35
Valerii Enin <enin_v_a@mail.ru>
2025-02-20 21:58:43 +0300

    Fixed

--- src/main/java/school/faang/BJS2_60487/MailSender.java
+++ src/main/java/school/faang/BJS2_60487/MailSender.java
@@ -2,19 +2,21 @@ package school.faang.BJS2_60487;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.concurrent.CopyOnWriteArrayList;
 import java.util.stream.IntStream;
 
 public class MailSender {
+    private static final int TOTAL_MESSAGES = 1000;
+    private static final int THREAD_AMOUNT = 5;
+
     public static void main(String[] args) {
-        int threadsAmount = 5;
+
         List<Thread> listThreads = new ArrayList<>();
 
-        CopyOnWriteArrayList<String> messageBox = new CopyOnWriteArrayList<>();
-        IntStream.range(0, 1000).forEach(i -> messageBox.add("Email message number %d".formatted(i)));
+        ArrayList<String> messageBox = new ArrayList<>();
+        IntStream.range(0, TOTAL_MESSAGES).forEach(i -> messageBox.add("Email message number %d".formatted(i)));
 
         IntStream
-                .range(0, threadsAmount)
+                .range(0, THREAD_AMOUNT)
                 .forEach(i -> {
                     Thread thread = new Thread(new SenderRunnable("\"Runnable object №%d\""
                             .formatted(i), i * 200, i * 200 + 199, messageBox));
@@ -33,6 +35,6 @@ public class MailSender {
             }
         });
 
-        System.out.println("All messages have send!");
+        System.out.println("All messages have been sent!");
     }
 }
--- src/main/java/school/faang/BJS2_60487/SenderRunnable.java
+++ src/main/java/school/faang/BJS2_60487/SenderRunnable.java
@@ -2,8 +2,8 @@ package school.faang.BJS2_60487;
 
 import lombok.AllArgsConstructor;
 
+import java.util.ArrayList;
 import java.util.Optional;
-import java.util.concurrent.CopyOnWriteArrayList;
 import java.util.stream.IntStream;
 
 @AllArgsConstructor
@@ -12,13 +12,15 @@ public class SenderRunnable implements Runnable {
     private final String threadObjectName;
     private final int startIndex;
     private final int endIndex;
-    private CopyOnWriteArrayList<String> messageBox;
+    private final ArrayList<String> messageBox;
+    private static final String MESSAGE_TEMPLATE =
+            "Runnable object named %s, with startIndex: %d\n" +
+            "and endIndex: %d, send message: %s";
 
     @Override
     public void run() {
         IntStream.range(startIndex, endIndex + 1)
-                .forEach(i -> System.out.println(("Runnable object named %s, with startIndex: " +
-                        "%d and endIndex: %d, send message: %s")
+                .forEach(i -> System.out.println(MESSAGE_TEMPLATE
                         .formatted(threadObjectName, startIndex, endIndex,
                                 Optional.ofNullable(messageBox.get(i)).orElse(""))));
     }
