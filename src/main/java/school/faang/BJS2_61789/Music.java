923da40a37a167e92a5fd89a9410249ff9c030de
Pavel Chernobay <mr.chernobay1990@gmail.com>
2025-02-20 20:46:57 +0300

    BJS2-61789 <task>

---
+++ src/main/java/school/faang/BJS2_61789/Music.java
@@ -0,0 +1,18 @@
+package school.faang.BJS2_61789;
+
+public class Music {
+
+    public static void main(String[] args) {
+        Player player = new Player();
+
+        Thread playThread = new Thread(player::play);
+        Thread pauseThread = new Thread(player::pause);
+        Thread skipThread = new Thread(player::skip);
+        Thread previousThread = new Thread(player::previous);
+
+        playThread.start();
+        pauseThread.start();
+        skipThread.start();
+        previousThread.start();
+    }
+}
