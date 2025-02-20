bbbe9604cdd834aa24116090f854b931e0fc133e
Артемий <xkmitx@gmail.com>
2025-02-20 20:12:13 +0300

    task 58363

---
+++ src/main/java/school/faang/task_58363/ListOperations.java
@@ -0,0 +1,68 @@
+package school.faang.task_58363;
+
+import lombok.NonNull;
+
+import java.util.Comparator;
+import java.util.List;
+import java.util.NoSuchElementException;
+import java.util.function.Predicate;
+
+public class ListOperations {
+
+    public static int sumOfEvenNumbers(@NonNull List<Integer> numbers) {
+        return numbers.stream()
+                .filter(n -> n % 2 == 0)
+                .mapToInt(Integer::intValue)
+                .sum();
+    }
+
+    public static int findMax(@NonNull List<Integer> numbers) {
+        return numbers.stream()
+                .max(Integer::compareTo)
+                .orElseThrow(() -> new NoSuchElementException("Empty list"));
+    }
+
+    public static double findAverage(@NonNull List<Integer> numbers) {
+        return numbers.stream()
+                .mapToInt(Integer::intValue)
+                .average()
+                .orElse(0.0);
+    }
+
+    public static long countStringsStartingWith(@NonNull List<String> strings, char ch) {
+        return strings.stream()
+                .filter(s -> !s.isEmpty() && s.charAt(0) == ch)
+                .count();
+    }
+
+    public static List<String> filterStringsContainingSubstring(@NonNull List<String> strings, @NonNull String substring) {
+        return strings.stream()
+                .filter(s -> s.contains(substring))
+                .toList();
+    }
+
+    public static List<String> sortByLength(@NonNull List<String> strings) {
+        return strings.stream()
+                .sorted(Comparator.comparingInt(String::length))
+                .toList();
+    }
+
+    public static boolean allMatchCondition(@NonNull List<Integer> numbers, @NonNull Predicate<Integer> conditions) {
+        return numbers.stream()
+                .allMatch(conditions);
+    }
+
+    public static int findMinGreaterThan(@NonNull List<Integer> numbers, int threshold) {
+        return numbers.stream()
+                .filter(x -> x > threshold)
+                .min(Integer::compareTo)
+                .orElseThrow(() -> new NoSuchElementException("No elements greater than "
+                        + threshold));
+    }
+
+    public static List<Integer> convertToLengths(@NonNull List<String> strings) {
+        return strings.stream()
+                .map(String::length)
+                .toList();
+    }
+}
