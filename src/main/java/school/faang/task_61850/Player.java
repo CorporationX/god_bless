baa2a202895fdde86f1cb2ed11a9b5a8e03ab0cb
Artem Doronin <doronintema2018@yandex.ru>
2025-02-20 23:01:07 +0500

    Блокировка Spotify

---
+++ src/main/java/school/faang/task_61850/Player.java
@@ -0,0 +1,40 @@
+package school.faang.task_61850;
+
+public class Player {
+    private final Object lock = new Object();
+    private boolean isPlaying;
+
+    public void play() {
+        synchronized (lock) {
+            if (!isPlaying) {
+                isPlaying = true;
+                System.out.println("Музыка воспроизводится");
+            } else {
+                System.out.println("Музыка уже играет");
+            }
+        }
+    }
+
+    public void pause() {
+        synchronized (lock) {
+            if (isPlaying) {
+                isPlaying = false;
+                System.out.println("Музыка не паузе");
+            } else {
+                System.out.println("Музыка и так не играет");
+            }
+        }
+    }
+
+    public void skip() {
+        synchronized (lock) {
+            System.out.println("Трек пропущен.");
+        }
+    }
+
+    public void previous() {
+        synchronized (lock) {
+            System.out.println("Вернулись к предыдущему треку");
+        }
+    }
+}
