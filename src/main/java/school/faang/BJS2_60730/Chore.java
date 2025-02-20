3fa7759bc5498f7efe6b08d66742eea1fda7bf6e
Gulnaz <gumerova.gulnaz2014@yandex.ru>
2025-02-20 11:37:08 +0300

    Исправлены недочеты в классах

--- src/main/java/school/faang/BJS2_60730/Chore.java
+++ src/main/java/school/faang/BJS2_60730/Chore.java
@@ -2,17 +2,23 @@ package school.faang.BJS2_60730;
 
 import lombok.AllArgsConstructor;
 
+import java.time.LocalDateTime;
+
 @AllArgsConstructor
 public class Chore implements Runnable {
+    private static final int TASK_DURATION_MS = 1000;
     private String title;
 
     @Override
     public void run() {
         try {
-            System.out.printf("%s выполняет задачу %s\n", Thread.currentThread().getName(), title);
-            Thread.sleep(100);
+            System.out.printf("%tT - %s выполняет задачу %s\n",
+                    LocalDateTime.now(), Thread.currentThread().getName(), title);
+            Thread.sleep(TASK_DURATION_MS);
+            System.out.printf("%tT - %s выполнил задачу\n", LocalDateTime.now(), Thread.currentThread().getName());
         } catch (InterruptedException e) {
-            throw new RuntimeException(e);
+            System.err.printf("InterruptedException: %s\n", e.getMessage());
+            Thread.currentThread().interrupt();
         }
 
     }
