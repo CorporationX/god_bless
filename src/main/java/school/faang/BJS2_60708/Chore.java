b821d23194022e88c8903b77cb803dabb36916bd
RamilNazmiev192 <nazmiev633@gmail.com>
2025-02-20 15:40:07 +0300

    fix: add logger

--- src/main/java/school/faang/BJS2_60708/Chore.java
+++ src/main/java/school/faang/BJS2_60708/Chore.java
@@ -1,17 +1,24 @@
 package school.faang.BJS2_60708;
 
 import lombok.AllArgsConstructor;
-import lombok.SneakyThrows;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 @AllArgsConstructor
 public class Chore implements Runnable {
     private final String chore;
+    private final Logger logger = LoggerFactory.getLogger(Chore.class);
+    private final int taskTime = 100;
 
-    @SneakyThrows
     @Override
     public void run() {
         System.out.printf("Имя потока: %s выполняет задачу: %s%n",
                 Thread.currentThread().getName(), chore);
-        Thread.sleep(100);
+        try {
+            Thread.sleep(taskTime);
+        } catch (InterruptedException e) {
+            logger.error("Задача {} не завершина", chore, e);
+            Thread.currentThread().interrupt();
+        }
     }
 }
