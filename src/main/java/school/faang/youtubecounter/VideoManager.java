00d64934a6bd346ae62a8f628516f27039eb2998
Marsel <mars1212@inbox.ru>
2025-02-21 00:38:43 +0300

    BJS2-61573 fix: synchronized methods

--- src/main/java/school/faang/youtubecounter/VideoManager.java
+++ src/main/java/school/faang/youtubecounter/VideoManager.java
@@ -5,33 +5,20 @@ import lombok.extern.slf4j.Slf4j;
 
 import java.util.HashMap;
 import java.util.Map;
-import java.util.concurrent.locks.ReentrantLock;
 
 @Slf4j
 public class VideoManager {
     private final Map<String, Integer> viewsMap;
-    private final ReentrantLock lock;
 
     public VideoManager() {
         viewsMap = new HashMap<>();
-        lock = new ReentrantLock();
     }
 
-    public void addView(@NonNull String videoId) {
-        lock.lock();
-        try {
-            viewsMap.put(videoId, viewsMap.getOrDefault(videoId, 0) + 1);
-        } finally {
-            lock.unlock();
-        }
+    public synchronized void addView(@NonNull String videoId) {
+        viewsMap.put(videoId, viewsMap.getOrDefault(videoId, 0) + 1);
     }
 
-    public int getViewCount(@NonNull String videoId) {
-        lock.lock();
-        try {
-            return viewsMap.getOrDefault(videoId, 0);
-        } finally {
-            lock.unlock();
-        }
+    public synchronized int getViewCount(@NonNull String videoId) {
+        return viewsMap.getOrDefault(videoId, 0);
     }
 }
