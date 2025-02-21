de1c454082e1a8c2bcf45894b982085d386b439a
Anton Lukyanchikov <antontotenkopf@gmail.com>
2025-02-21 13:25:34 +0300

    refactored task 61403

--- src/main/java/task_BJS2_61403/Army.java
+++ src/main/java/task_BJS2_61403/Army.java
@@ -2,10 +2,12 @@ package task_BJS2_61403;
 
 import lombok.Data;
 import lombok.NonNull;
+import lombok.extern.slf4j.Slf4j;
 
 import java.util.ArrayList;
 import java.util.List;
 
+@Slf4j
 @Data
 public class Army {
     private final List<Squad> squadsInArmy = new ArrayList<>();
@@ -14,7 +16,7 @@ public class Army {
         squadsInArmy.add(squad);
     }
 
-    public int calculateTotalPower() throws InterruptedException {
+    public int calculateTotalPower() {
         List<Thread> threadAmount = new ArrayList<>();
         List<Integer> results = new ArrayList<>();
 
@@ -24,7 +26,12 @@ public class Army {
             thread.start();
         }
         for (Thread thread : threadAmount) {
-            thread.join();
+            try {
+                thread.join();
+            } catch (InterruptedException exception) {
+                Thread.currentThread().interrupt();
+                log.info("Thread: {} was interrupted", Thread.currentThread().getName());
+            }
         }
 
         return results.stream().mapToInt(Integer::intValue).sum();
