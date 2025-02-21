c27d852dc833dc4f50f7f9bc3b62a23c878867f0
s3nkyh <olkin.yaroslaw@yandex.ru>
2025-02-21 14:43:54 +0300

    исправил ошибки

--- src/main/java/school/faang/task_61141/FoodDeliveryTask.java
+++ src/main/java/school/faang/task_61141/FoodDeliveryTask.java
@@ -8,6 +8,8 @@ import java.util.Random;
 @AllArgsConstructor
 @Getter
 public class FoodDeliveryTask implements Runnable {
+    private static final int ORIGIN_SLEEP = 1000;
+    private static final int BOUND_SLEEP = 5000;
     private final Random random = new Random();
     private final String character;
     private final int foodAmount;
@@ -17,7 +19,7 @@ public class FoodDeliveryTask implements Runnable {
         String foodType = getFoodType().name();
         System.out.printf("%s gets %d %s\n", character, foodAmount, foodType);
         try {
-            Thread.sleep(random.nextInt(1000, 5000));
+            Thread.sleep(random.nextInt(ORIGIN_SLEEP, BOUND_SLEEP));
         } catch (InterruptedException e) {
             System.out.println(character + " не смог получить " + foodType + " из-за прерывания.");
             Thread.currentThread().interrupt();
@@ -27,8 +29,8 @@ public class FoodDeliveryTask implements Runnable {
     }
 
     private FoodType getFoodType() {
-        FoodType[] foodFoodTypes = FoodType.values();
-        return foodFoodTypes[random.nextInt(foodFoodTypes.length)];
+        FoodType[] foodTypes = FoodType.values();
+        return foodTypes[random.nextInt(foodTypes.length)];
     }
 
 }
--- src/main/java/school/faang/task_61141/GriffinsFoodDelivery.java
+++ src/main/java/school/faang/task_61141/GriffinsFoodDelivery.java
@@ -4,16 +4,25 @@ import java.util.Arrays;
 import java.util.Random;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
 
 public class GriffinsFoodDelivery {
     private static final int THREADS_COUNT = 3;
+    private static final Random RANDOM = new Random();
 
     public static void main(String[] args) {
-        Random random = new Random();
         String[] characterNames = {"Peter", "Lois", "Meg", "Chris", "Stewie"};
         ExecutorService executor = Executors.newFixedThreadPool(THREADS_COUNT);
         Arrays.stream(characterNames)
-                .forEach(name -> executor.execute(new FoodDeliveryTask(name, random.nextInt(0, 100))));
+                .forEach(name -> executor.execute(new FoodDeliveryTask(name, RANDOM.nextInt(0, 100))));
         executor.shutdown();
+        try {
+            if (!executor.awaitTermination(3, TimeUnit.MINUTES)) {
+                executor.shutdownNow();
+            }
+        } catch (InterruptedException e) {
+            executor.shutdownNow();
+            throw new RuntimeException("Error in the thread operation", e);
+        }
     }
 }
