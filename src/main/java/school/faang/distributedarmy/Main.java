200e0b297cfbe571966d8bf96882b43438f81bb6
Vitaliy <vitaliy.zhilitskiy@gmail.com>
2025-02-21 15:50:30 +0300

    Решение задачи BJS2-61202.

--- src/main/java/school/faang/distributedarmy/Main.java
+++ src/main/java/school/faang/distributedarmy/Main.java
@@ -4,7 +4,6 @@ import java.util.concurrent.ExecutionException;
 
 public class Main {
     public static void main(String[] args) {
-        Army army = new Army();
         Squad archers = new Squad();
         archers.addHero(new Archer(25));
         archers.addHero(new Archer(30));
@@ -17,6 +16,7 @@ public class Main {
         mages.addHero(new Mage(40));
         mages.addHero(new Mage(35));
 
+        Army army = new Army();
         army.addSquad(archers);
         army.addSquad(swordsmen);
         army.addSquad(mages);
