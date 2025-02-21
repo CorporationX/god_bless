51e34593851b358330c448ab423db847d0064e04
Ivan Prygunov <88151612+juzu400@users.noreply.github.com>
2025-02-21 15:44:34 +0300

    задача Потренируем StreamAPI 2

--- src/main/java/school/faang/Main.java
+++ src/main/java/school/faang/Main.java
@@ -1,6 +1,40 @@
 package school.faang;
 
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
 public class Main {
     public static void main(String[] args) {
+
+        Set<Integer> set = Set.of(0, 1, 2, 3, 4, 5, 6, 7, 8, 9);
+        int sum = 7;
+        Set<List<Integer>> result1 = StreamOperations.findPairs(set, sum);
+        System.out.printf("Список элементов: %s\nСумма пар: %s\nПары элементов: %s\n\n", set, sum, result1);
+
+        Map<String, String> countriesWithCapitals = Map.of(
+                "Russia", "Moscow",
+                "USA", "Washington",
+                "Germany", "Berlin",
+                "Italy", "Rome");
+        List<String> result2 = StreamOperations.filtrateCapitals(countriesWithCapitals);
+        System.out.printf("Страны со столицами: %s\nСтолицы: %s\n\n", countriesWithCapitals, result2);
+
+        List<String> strings = List.of("apple", "banana", "avocado", "apricot", "coconut", "mango");
+        char character = 'a';
+        List<String> result3 = StreamOperations.filtrateByLengthStartsWithCharacter(strings, character);
+        System.out.printf("Изначальный список строк: %s\nПервая буква: %s\nОтсортированные строки: %s\n\n",
+                strings, character, result3);
+
+        List<Integer> nums = List.of(1, 2, 3, 4, 5, 6, 7, 8, 9);
+        List<String> result4 = StreamOperations.turn10to2(nums);
+        System.out.printf("Список десятичных элементов: %s\nСписок двоичных элементов: %s\n\n", nums, result4);
+
+        List<String> strings1 = List.of("apple", "banana", "avocado", "apricot", "coconut", "mango",
+                "яблоко", "виноград", "апельсин");
+        String alphabet = "abcdefghijklmnopqrstuvwxyz";
+        List<String> result5 = StreamOperations.filtrateByLengthContainingAlphabet(strings1, alphabet);
+        System.out.printf("Изначальный список: %s\nИспользующийся алфавит: %s\nПолученный список: %s\n\n",
+                strings1, alphabet, result5);
     }
 }
---
+++ src/main/java/school/faang/StreamOperations.java
@@ -0,0 +1,53 @@
+package school.faang;
+
+import lombok.NonNull;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+public class StreamOperations {
+
+    public static Set<List<Integer>> findPairs(@NonNull Set<Integer> numbers, int sum) {
+        return numbers.stream()
+                .filter(num -> num != sum - num && numbers.contains(sum - num))
+                .map(num -> Arrays.asList(num, sum - num))
+                .peek(Collections::sort)
+                .collect(Collectors.toSet());
+    }
+
+    public static List<String> filtrateCapitals(@NonNull Map<String, String> countryWithCapitals) {
+        return countryWithCapitals.values().stream()
+                .sorted(Comparator.naturalOrder())
+                .toList();
+
+    }
+
+    public static List<String> filtrateByLengthStartsWithCharacter(@NonNull List<String> strings, char character) {
+        return strings.stream()
+                .filter(s -> s.charAt(0) == character)
+                .sorted(Comparator.comparingInt(String::length))
+                .toList();
+    }
+
+    public static List<String> turn10to2(@NonNull List<Integer> nums) {
+        return nums.stream()
+                .map(Integer::toBinaryString)
+                .toList();
+    }
+
+    public static List<String> filtrateByLengthContainingAlphabet(
+            @NonNull List<String> strings,
+            @NonNull String alphabet) {
+
+        String regex = "[" + alphabet + "]+";
+        return strings.stream()
+                .filter(s -> s.matches(regex))
+                .sorted(Comparator.comparingInt(String::length))
+                .toList();
+    }
+}
