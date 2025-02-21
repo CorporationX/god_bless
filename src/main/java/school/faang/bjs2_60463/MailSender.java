6e1774dfc63d93c7307ba963a12a6908df199bb5
Savchenko <savchenko.ra@yandex.ru>
2025-02-21 12:40:31 +0300

    BJS2-60463 refactoring.

--- src/main/java/school/faang/bjs2_60463/MailSender.java
+++ src/main/java/school/faang/bjs2_60463/MailSender.java
@@ -2,25 +2,27 @@ package school.faang.bjs2_60463;
 
 
 import java.util.concurrent.CountDownLatch;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
 
 public class MailSender {
+    private static final int THREAD_COUNT = 5;
+    private static final int TOTAL_EMAILS = 1000;
+
     public static void main(String[] args) throws InterruptedException {
-        ExecutorService executor = Executors.newFixedThreadPool(5);
+        int emailsPerThread = TOTAL_EMAILS / THREAD_COUNT;
+
         CountDownLatch countDownLatch = new CountDownLatch(5);
+        Thread[] threads = new Thread[THREAD_COUNT];
 
-        int totalEmails = 1000;
-        int emailsPerThread = totalEmails / 5;
-
-        for (int i = 0; i < 5; i++) {
+        for (int i = 0; i < THREAD_COUNT; i++) {
             int startIndex = i * emailsPerThread + 1;
             int endIndex = startIndex + emailsPerThread - 1;
-            executor.execute(new SenderRunnable(startIndex, endIndex, countDownLatch));
+            threads[i] = new Thread(new SenderRunnable(startIndex, endIndex, countDownLatch));
+            threads[i].start();
         }
 
-        countDownLatch.await();
-        executor.shutdown();
+        for (Thread thread : threads) {
+            thread.join();
+        }
         System.out.println("The letter has been sent!");
     }
 }
