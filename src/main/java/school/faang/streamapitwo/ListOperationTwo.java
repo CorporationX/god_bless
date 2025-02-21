f7017d4a590504dfc2ca2df224bba54a446fd68f
sunmajor <makar.a.v100@gmail.com>
2025-02-21 22:14:47 +0300

    Потренируем StreamAPI 2

---
+++ src/main/java/school/faang/streamapitwo/ListOperationTwo.java
@@ -0,0 +1,47 @@
+package school.faang.streamapitwo;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+public class ListOperationTwo {
+    public static Set<List<Integer>> findPairs(Set<Integer> numbers, int sum) {
+        return numbers.stream()
+                .filter(num -> num != sum - num && numbers.contains(sum - num))
+                .map(num -> Arrays.asList(num, sum - num))
+                .peek(Collections::sort)
+                .collect(Collectors.toSet());
+    }
+
+    public List<String> getSortedCapitals(Map<String, String> countries) {
+        return countries.entrySet().stream()
+                .sorted(Map.Entry.comparingByKey())
+                .map(Map.Entry::getValue)
+                .toList();
+    }
+
+    public List<String> filterAndSort(List<String> strings, char letter) {
+        return strings.stream()
+                .filter(s -> s.startsWith(String.valueOf(letter)))
+                .sorted(Comparator.comparingInt(String::length))
+                .toList();
+    }
+
+    public List<String> convertToBinary(List<Integer> nums) {
+        return nums.stream()
+                .map(Integer::toBinaryString)
+                .toList();
+    }
+
+    public List<String> filterAndSortByLength(List<String> strings, String alphabet) {
+        String regex = "[" + alphabet + "]+";
+        return strings.stream()
+                .filter(s -> s.matches(regex))
+                .sorted(Comparator.comparingInt(String::length))
+                .toList();
+    }
+}
