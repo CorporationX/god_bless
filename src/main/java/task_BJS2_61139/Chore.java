cdc91306b13d1e234f804f10b147e29eb080e457
Anton Lukyanchikov <antontotenkopf@gmail.com>
2025-02-21 13:14:29 +0300

    refactored task 61139

--- src/main/java/task_BJS2_61139/Chore.java
+++ src/main/java/task_BJS2_61139/Chore.java
@@ -4,13 +4,14 @@ import lombok.AllArgsConstructor;
 
 @AllArgsConstructor
 public class Chore implements Runnable {
+    private static final long TASK_DURATION_MS = 1000;
     private final String name;
 
     @Override
     public void run() {
         try {
             System.out.printf("Задача \"%s\" - выполняется потоком \"%s\"\n", name, Thread.currentThread().getName());
-            Thread.sleep(1000);
+            Thread.sleep(TASK_DURATION_MS);
             System.out.printf("Задача \"%s\" - выполнена потоком \"%s\"\n", name, Thread.currentThread().getName());
         } catch (InterruptedException exception) {
             System.out.printf("Задача \"%s\" - была прервана\n", name);
--- src/main/java/task_BJS2_61139/WeasleyFamily.java
+++ src/main/java/task_BJS2_61139/WeasleyFamily.java
@@ -3,6 +3,8 @@ package task_BJS2_61139;
 import java.util.Arrays;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
+import java.util.concurrent.RejectedExecutionException;
+import java.util.concurrent.TimeUnit;
 
 public class WeasleyFamily {
     private static final String[] CHORES = {"помыть посуду", "подмести пол", "приготовить ужин"};
@@ -10,7 +12,24 @@ public class WeasleyFamily {
     public static void main(String[] args) {
         ExecutorService executor = Executors.newCachedThreadPool();
 
-        Arrays.stream(CHORES).forEach(chore -> executor.execute(new Chore(chore)));
+        try {
+            Arrays.stream(CHORES).forEach(chore -> executor.execute(new Chore(chore)));
+        } catch (RejectedExecutionException exception) {
+            System.out.println("Очередь пула заполнена");
+        }
+
         executor.shutdown();
+
+        try {
+            boolean tasksFinished = executor.awaitTermination(3, TimeUnit.SECONDS);
+            if (tasksFinished) {
+                System.out.println("Все задачи завершены в течение времени ожидания.");
+            } else {
+                System.out.println("Таймаут истек до завершения всех задач.");
+            }
+        } catch (InterruptedException exception) {
+            System.out.println("Await terminate прервано.");
+            Thread.currentThread().interrupt();
+        }
     }
 }
