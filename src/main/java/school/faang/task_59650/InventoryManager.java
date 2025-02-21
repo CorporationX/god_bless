51d38c99b348374d151820cdbe1a683a9322e60a
Артемий <xkmitx@gmail.com>
2025-02-21 11:59:55 +0300

    fix

--- src/main/java/school/faang/task_59650/InventoryManager.java
+++ src/main/java/school/faang/task_59650/InventoryManager.java
@@ -1,27 +1,29 @@
 package school.faang.task_59650;
 
+import lombok.NonNull;
+
 import java.util.function.Consumer;
 import java.util.function.Function;
 import java.util.function.Predicate;
 
 public class InventoryManager {
-    public void addItem(Character character, Item item,
-                        Consumer<Item> action) {
+    public void addItem(@NonNull Character character, @NonNull Item item,
+                        @NonNull Consumer<Item> action) {
         character.getInventory().add(item);
         action.accept(item);
     }
 
-    public void removeItem(Character character, Predicate<Item> condition) {
+    public void removeItem(@NonNull Character character, @NonNull Predicate<Item> condition) {
         character.getInventory().removeIf(condition);
     }
 
-    public void updateItem(Character character, Predicate<Item> condition,
-                           Function<Item, Item> updateFunction) {
-        for (int i = 0; i < character.getInventory().size(); i++) {
-            Item item = character.getInventory().get(i);
-            if (condition.test(item)) {
-                character.getInventory().set(i, updateFunction.apply(item));
-            }
-        }
+    public void updateItem(@NonNull Character character, @NonNull Predicate<Item> condition,
+                           @NonNull Function<Item, Item> updateFunction) {
+        character.getInventory()
+                .replaceAll(item ->
+                        condition.test(item) ? updateFunction.apply(item) : item);
     }
 }
+
+
+
