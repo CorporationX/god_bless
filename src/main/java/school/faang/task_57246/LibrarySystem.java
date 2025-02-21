1c18f85ac364943dc7e8e5adc8fbb6195d04ffd0
Iaroslava Seregina <yaroslavaseregina4@gmail.com>
2025-02-21 21:41:11 +0300

    в папку перенесла

--- src/main/java/school/faang/task_57246/LibrarySystem.java
+++ src/main/java/school/faang/task_57246/LibrarySystem.java
@@ -1,4 +1,4 @@
-package school.faang;
+package school.faang.task_57246;
 
 import java.util.HashMap;
 import java.util.Map;
--- src/main/java/school/faang/task_57246/Main.java
+++ src/main/java/school/faang/task_57246/Main.java
@@ -1,4 +1,4 @@
-package school.faang;
+package school.faang.task_57246;
 
 public class Main {
     public static void main(String[] args) {
