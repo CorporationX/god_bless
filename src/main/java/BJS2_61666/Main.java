4388904b73b858b4409226a25bd2726c464aa003
saroyangor <dr.saroyan98@gmail.com>
2025-02-22 02:42:17 +0300

    BJS2-61666: Счетчик просмотров на YouTube

---
+++ src/main/java/BJS2_61666/Main.java
@@ -0,0 +1,40 @@
+package BJS2_61666;
+
+import lombok.extern.slf4j.Slf4j;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
+
+@Slf4j
+public class Main {
+    private static final int NUM_THREADS = 4;
+    private static final int NUM_VIDEOS = 10;
+
+    public static void main(String[] args) {
+        VideoManager videoManager = new VideoManager();
+
+        ExecutorService executor = Executors.newFixedThreadPool(NUM_THREADS);
+
+        for (int i = 0; i < NUM_VIDEOS; i++) {
+            String videoId = "video" + i;
+            for (int j = 0; j < NUM_THREADS; j++) {
+                executor.submit(() -> {
+                    videoManager.addView(videoId);
+                    log.info("Video {} has {} views", videoId, videoManager.getViewsCount(videoId));
+                });
+            }
+        }
+
+        executor.shutdown();
+
+        try {
+            if (!executor.awaitTermination(1, TimeUnit.SECONDS)) {
+                executor.shutdownNow();
+            }
+        } catch (InterruptedException e) {
+            executor.shutdownNow();
+            log.error("Executor was interrupted", e);
+        }
+    }
+}
