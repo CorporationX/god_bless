efe1056d187ebcdc27d0f6e5fe416a441f142ff8
naught_wo <catrinesaxon@yandex.ru>
2025-02-22 15:23:26 +0300

    Задание "Вы работаете в Microsoft!"

--- src/main/java/school/faang/naughtwoBJS258208/UserAction.java
+++ src/main/java/school/faang/naughtwoBJS258208/UserAction.java
@@ -1,24 +1,11 @@
 package school.faang.naughtwoBJS258208;
 
-import lombok.NonNull;
-import lombok.Value;
-
 import java.time.LocalDate;
 
-@Value
-public class UserAction {
-    int id;
-    String name;
-    ActionType actionType;
-    LocalDate actionDate;
-    String content;
-
-    public UserAction(int id, @NonNull String name, ActionType actionType,
-                      LocalDate actionDate, String content) {
-        this.id = id;
-        this.name = name;
-        this.actionType = actionType;
-        this.actionDate = actionDate;
-        this.content = content;
-    }
-}
+public record UserAction(
+    int id,
+    String name,
+    ActionType actionType,
+    LocalDate actionDate,
+    String content
+) {}
--- src/main/java/school/faang/naughtwoBJS258208/UserActionAnalyzer.java
+++ src/main/java/school/faang/naughtwoBJS258208/UserActionAnalyzer.java
@@ -4,25 +4,24 @@ import java.time.LocalDate;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Map;
+import java.util.stream.Collector;
 import java.util.stream.Collectors;
 
 public class UserActionAnalyzer {
 
     public static List<String> topActiveUsers(List<UserAction> actions, int n) {
         Map<String, Long> activeUsers = actions.stream()
-                //возникает дубликат ключа, я не понимаю почему, и даже не уверена что именно здесь возникает
-                //при попытке найти решение выдает то же самое, что уже написано.....
-                .collect(Collectors.groupingBy(UserAction::getName, Collectors.counting()));
+                .collect(userActionToMap());
         return sortedList(activeUsers, n);
     }
 
     public static List<String> topPopularHashtags(List<UserAction> actions, int n) {
         Map<String, Long> hashtags = actions.stream()
                 .filter(action ->
-                        action.getContent() != null
-                                && (ActionType.POST.equals(action.getActionType())
-                                || ActionType.COMMENT.equals(action.getActionType())))
-                .flatMap(action -> Arrays.stream(action.getContent().split("\\s+")))
+                        action.content() != null
+                                && (ActionType.POST.equals(action.actionType())
+                                || ActionType.COMMENT.equals(action.actionType())))
+                .flatMap(action -> Arrays.stream(action.content().split("\\s+")))
                 .filter(word -> word.startsWith("#"))
                 .collect(Collectors.groupingBy(word -> word, Collectors.counting()));
         return sortedList(hashtags, n);
@@ -32,24 +31,32 @@ public class UserActionAnalyzer {
         LocalDate oneMonthAgo = LocalDate.now().minusMonths(1);
         Map<String, Long> activeUsers = actions.stream()
                 .filter(action ->
-                        action.getContent() != null
-                                && ActionType.COMMENT.equals(action.getActionType())
-                                && action.getActionDate().isAfter(oneMonthAgo))
-                .collect(Collectors.groupingBy(UserAction::getName, Collectors.counting()));
+                        action.content() != null
+                                && ActionType.COMMENT.equals(action.actionType())
+                                && action.actionDate().isAfter(oneMonthAgo))
+                .collect(userActionToMap());
         return sortedList(activeUsers, n);
     }
 
     public static Map<String, Double> actionTypePercentages(List<UserAction> actions) {
         Map<ActionType, Long> actionType = actions.stream()
-                .collect(Collectors.groupingBy(UserAction::getActionType, Collectors.counting()));
+                .collect(Collectors.groupingBy(UserAction::actionType, Collectors.counting()));
         return actionType.entrySet().stream()
                 .collect(Collectors.toMap(entry -> entry.getValue().toString(),
                         entry -> entry.getValue() * 100.0 / actions.size()));
     }
 
+    private static Collector<UserAction, ?, Map<String, Long>> userActionToMap() {
+        return Collectors.toMap(
+                UserAction::name,
+                action -> 1L,
+                Long::sum
+        );
+    }
+
     private static List<String> sortedList(Map<String, Long> map, int n) {
         return map.entrySet().stream()
-                .sorted(Map.Entry.comparingByValue())
+                .sorted(Map.Entry.<String, Long>comparingByValue().reversed())
                 .limit(n)
                 .map(Map.Entry::getKey)
                 .toList();
