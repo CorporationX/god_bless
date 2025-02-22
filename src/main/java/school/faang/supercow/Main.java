50604e387f7347ce59d261bc8982bef89320c7ff
Vitaliy <vitaliy.zhilitskiy@gmail.com>
2025-02-22 21:13:10 +0300

    Задача BJS2-61552 решена.

--- src/main/java/school/faang/supercow/Main.java
+++ src/main/java/school/faang/supercow/Main.java
@@ -2,21 +2,18 @@ package school.faang.supercow;
 
 public class Main {
     public static void main(String[] args) {
-        Boss boss = new Boss(2); // Босс с максимум 2 игроками одновременно
-
+        Boss boss = new Boss(2);
         Player player1 = new Player("Игрок 1");
         Player player2 = new Player("Игрок 2");
         Player player3 = new Player("Игрок 3");
         Player player4 = new Player("Игрок 4");
-
         Thread thread1 = new Thread(() -> player1.doBattle(boss));
-        Thread thread2 = new Thread(() -> player2.doBattle(boss));
-        Thread thread3 = new Thread(() -> player3.doBattle(boss));
-        Thread thread4 = new Thread(() -> player4.doBattle(boss));
-
         thread1.start();
+        Thread thread2 = new Thread(() -> player2.doBattle(boss));
         thread2.start();
+        Thread thread3 = new Thread(() -> player3.doBattle(boss));
         thread3.start();
+        Thread thread4 = new Thread(() -> player4.doBattle(boss));
         thread4.start();
     }
 }
