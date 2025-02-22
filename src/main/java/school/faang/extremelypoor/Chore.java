decb2942a2a21f21436d7165f2376cae42cac085
Ilyas Azhdar <azhdar902@gmail.com>
2025-02-22 23:41:42 +0500

    fix

--- src/main/java/school/faang/extremelypoor/Chore.java
+++ src/main/java/school/faang/extremelypoor/Chore.java
@@ -2,6 +2,8 @@ package school.faang.extremelypoor;
 
 import lombok.extern.slf4j.Slf4j;
 
+import java.util.concurrent.ThreadLocalRandom;
+
 @Slf4j
 public class Chore implements Runnable {
     private final String chore;
@@ -12,12 +14,13 @@ public class Chore implements Runnable {
 
     @Override
     public void run() {
-        log.info("Выполнение потока: {}", Thread.currentThread().getName());
+        log.info("Выполнение потока: {} для задачи - {}", Thread.currentThread().getName(), chore);
         try {
-            Thread.sleep(1000);
+            Thread.sleep(ThreadLocalRandom.current().nextInt(1000, 5001));
         } catch (InterruptedException e) {
             log.error("Поток {} прерван: {}", Thread.currentThread().getName(), e.getMessage(), e);
             Thread.currentThread().interrupt();
         }
+        log.info("Потока: {} для задачи - {} завершен", Thread.currentThread().getName(), chore);
     }
 }
