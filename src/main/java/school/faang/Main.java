e14292c59c0a79ac979e4ea2d3308d71003ef24c
bambum <gwalka885@gmai.com>
2025-02-22 21:34:13 +0500

    BJS2-61680 решение задачи

--- src/main/java/school/faang/Main.java
+++ src/main/java/school/faang/Main.java
@@ -1,4 +1,39 @@
 package school.faang;
 
+import lombok.extern.slf4j.Slf4j;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
+
+@Slf4j
 public class Main {
+    private static final int NUM_THREADS = 10;
+    private static final int NUM_VIDEOS = 5;
+
+
+    public static void main(String[] args) {
+        VideoManager manager = new VideoManager();
+        ExecutorService executor = Executors.newFixedThreadPool(NUM_THREADS);
+
+        for (int i = 0; i < NUM_VIDEOS; i++) {
+            String videoId = "video" + i;
+            for (int j = 0; j < NUM_THREADS; j++) {
+                executor.submit(() -> {
+                    manager.addView(videoId);
+                    log.info("Video " + videoId + " просмотров " + manager.getViewCount(videoId));
+                });
+            }
+        }
+        executor.shutdown();
+        try {
+            if (executor.awaitTermination(1, TimeUnit.MINUTES)) {
+                executor.shutdown();
+            }
+        } catch (InterruptedException e) {
+            executor.shutdownNow();
+            Thread.currentThread().interrupt();
+        }
+    }
+
 }
