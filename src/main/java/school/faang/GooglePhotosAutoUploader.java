a44bea89049fa32e74800b4c426e42f46c10026d
bambum <gwalka885@gmai.com>
2025-02-23 01:35:21 +0500

    добавлен объект для лока

--- src/main/java/school/faang/GooglePhotosAutoUploader.java
+++ src/main/java/school/faang/GooglePhotosAutoUploader.java
@@ -7,13 +7,14 @@ import java.util.List;
 
 @Slf4j
 public class GooglePhotosAutoUploader {
+    Object lock = new Object();
     private List<String> photosToUpload = new ArrayList<>();
 
     public void startAutoUpload() {
-        synchronized (photosToUpload) {
+        synchronized (lock) {
             while (photosToUpload.isEmpty()) {
                 try {
-                    photosToUpload.wait();
+                    lock.wait();
                 } catch (InterruptedException e) {
                     Thread.currentThread().interrupt();
                     log.info("Поток был прерван");
@@ -24,9 +25,9 @@ public class GooglePhotosAutoUploader {
     }
 
     public void onNewPhotoAdded(String photoPath) {
-        synchronized (photosToUpload) {
+        synchronized (lock) {
             photosToUpload.add(photoPath);
-            photosToUpload.notify();
+            lock.notify();
         }
     }
 
