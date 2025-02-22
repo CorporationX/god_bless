883ced807f3aea2b3deae3988cf7401a73fd8a70
Vitaliy <vitaliy.zhilitskiy@gmail.com>
2025-02-22 21:15:46 +0300

    Задача BJS2-61552 решена.

--- src/main/java/school/faang/supercow/Main.java
+++ src/main/java/school/faang/supercow/Main.java
@@ -4,15 +4,15 @@ public class Main {
     public static void main(String[] args) {
         Boss boss = new Boss(2);
         Player player1 = new Player("Игрок 1");
-        Player player2 = new Player("Игрок 2");
-        Player player3 = new Player("Игрок 3");
-        Player player4 = new Player("Игрок 4");
         Thread thread1 = new Thread(() -> player1.doBattle(boss));
         thread1.start();
+        Player player2 = new Player("Игрок 2");
         Thread thread2 = new Thread(() -> player2.doBattle(boss));
         thread2.start();
+        Player player3 = new Player("Игрок 3");
         Thread thread3 = new Thread(() -> player3.doBattle(boss));
         thread3.start();
+        Player player4 = new Player("Игрок 4");
         Thread thread4 = new Thread(() -> player4.doBattle(boss));
         thread4.start();
     }
