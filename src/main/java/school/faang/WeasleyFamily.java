6773e551a76f89048e84b41b3a7c8ca2016423e7
bambum <gwalka885@gmai.com>
2025-02-23 01:31:18 +0500

    MAX_THREADS == maxThreads чек стайл на дает так назвать

--- src/main/java/school/faang/WeasleyFamily.java
+++ src/main/java/school/faang/WeasleyFamily.java
@@ -6,10 +6,7 @@ import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.TimeUnit;
 
-@AllArgsConstructor
 public class WeasleyFamily {
-    private final String[] chores;
-
 
     public static void main(String[] args) {
         String[] chores = {"Помыть посуду", "Подмести пол", "Приготовить ужин", "Постирать вещи", "Почитать книгу"};
