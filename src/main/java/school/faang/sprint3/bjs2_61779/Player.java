6bbaf6285f5457446ca8b18db7cbf507caff7bd1
Anton <basanton@mail.ru>
2025-02-22 18:43:02 +0300

    Выполнение задачи по "Блокировка Spotify" версия 2

--- src/main/java/school/faang/sprint3/bjs2_61779/Player.java
+++ src/main/java/school/faang/sprint3/bjs2_61779/Player.java
@@ -1,6 +1,13 @@
 package school.faang.sprint3.bjs2_61779;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
 public class Player {
+    private static final Logger log = LoggerFactory.getLogger(Player.class);
     private final Object lock = new Object();
     private boolean isPlaying = false;
 
@@ -8,9 +15,13 @@ public class Player {
         synchronized (lock) {
             if (!isPlaying) {
                 isPlaying = true;
-                System.out.println("Музыка воспроизводится.");
+                log.info(String.format("[%s] [Поток: %d] Музыка воспроизводится.%n",
+                        LocalDateTime.now().format(DateTimeFormatter.ISO_LOCAL_DATE_TIME),
+                        Thread.currentThread().getId()));
             } else {
-                System.out.println("Музыка уже играет.");
+                log.info(String.format("[%s] [Поток: %d] Музыка уже играет.%n",
+                        LocalDateTime.now().format(DateTimeFormatter.ISO_LOCAL_DATE_TIME),
+                        Thread.currentThread().getId()));
             }
         }
     }
@@ -19,22 +30,30 @@ public class Player {
         synchronized (lock) {
             if (isPlaying) {
                 isPlaying = false;
-                System.out.println("Музыка поставлена на паузу.");
+                log.info(String.format("[%s] [Поток: %d] Музыка поставлена на паузу.%n",
+                        LocalDateTime.now().format(DateTimeFormatter.ISO_LOCAL_DATE_TIME),
+                        Thread.currentThread().getId()));
             } else {
-                System.out.println("Музыка уже на паузе.");
+                log.info(String.format("[%s] [Поток: %d] Музыка уже на паузе.%n",
+                        LocalDateTime.now().format(DateTimeFormatter.ISO_LOCAL_DATE_TIME),
+                        Thread.currentThread().getId()));
             }
         }
     }
 
     public void skip() {
         synchronized (lock) {
-            System.out.println("Трек пропущен.");
+            log.info(String.format("[%s] [Поток: %d] Трек пропущен.%n",
+                    LocalDateTime.now().format(DateTimeFormatter.ISO_LOCAL_DATE_TIME),
+                    Thread.currentThread().getId()));
         }
     }
 
     public void previous() {
         synchronized (lock) {
-            System.out.println("Вернулись к предыдущему треку.");
+            log.info(String.format("[%s] [Поток: %d] Вернулись к предыдущему треку.%n",
+                    LocalDateTime.now().format(DateTimeFormatter.ISO_LOCAL_DATE_TIME),
+                    Thread.currentThread().getId()));
         }
     }
 }
