11ffdf15efb2d764d1401693a92c8bf6616f7225
Anton <basanton@mail.ru>
2025-02-22 11:13:30 +0300

    Выполнение задачи по "Счетчик просмотров на YouTube" версия 1

---
+++ src/main/java/school/faang/sprint3/bjs2_61595/VideoManager.java
@@ -0,0 +1,16 @@
+package school.faang.sprint3.bjs2_61595;
+
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
+public class VideoManager {
+    private final Map<String, Integer> viewsMap = new ConcurrentHashMap<>();
+
+    public void addView(String videoId) {
+        viewsMap.merge(videoId, 1, Integer::sum);
+    }
+
+    public int getViewCount(String videoId) {
+        return viewsMap.getOrDefault(videoId, 0);
+    }
+}
