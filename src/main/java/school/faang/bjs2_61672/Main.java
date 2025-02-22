92adbb67df46215ebcb17f4a36cf650a11146d59
Savchenko <savchenko.ra@yandex.ru>
2025-02-22 13:24:59 +0300

    BJS2-61672 refactoring.

--- src/main/java/school/faang/bjs2_61672/Main.java
+++ src/main/java/school/faang/bjs2_61672/Main.java
@@ -1,38 +1,40 @@
 package school.faang.bjs2_61672;
 
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
 public class Main {
     public static void main(String[] args) {
         GooglePhotosAutoUploader uploader = new GooglePhotosAutoUploader();
 
-        Thread addPhoto = new Thread(new Runnable() {
-            @Override
-            public void run() {
-                int photoNumber = 1;
-                while (true) {
-                    String photoPath = "photo_" + photoNumber + ".jpg";
-                    uploader.onNewPhotoAdded(photoPath);
-                    photoNumber++;
-                    try {
-                        Thread.sleep(3000);
-                    } catch (InterruptedException e) {
-                        throw new RuntimeException(e);
-                    }
-                }
-            }
-        });
-        addPhoto.start();
-
-        Thread uploadPhoto = new Thread(new Runnable() {
-            @Override
-            public void run() {
+        Thread addPhoto = new Thread(() -> {
+            int photoNumber = 1;
+            while (photoNumber <= 10) {
+                String photoPath = "photo_" + photoNumber + ".jpg";
+                uploader.onNewPhotoAdded(photoPath);
+                photoNumber++;
                 try {
-                    uploader.startAutoUpload();
+                    Thread.sleep(3000);
                 } catch (InterruptedException e) {
-                    throw new RuntimeException(e);
+                    Thread.currentThread().interrupt();
+                    return;
                 }
             }
         });
+
+        Thread uploadPhoto = new Thread(uploader::startAutoUpload);
+
+        addPhoto.start();
         uploadPhoto.start();
 
+        try {
+            addPhoto.join();
+            uploader.stop();
+            uploadPhoto.join();
+        } catch (InterruptedException e) {
+            Thread.currentThread().interrupt();
+            log.warn("Main thread interrupted.", e);
+        }
     }
 }
