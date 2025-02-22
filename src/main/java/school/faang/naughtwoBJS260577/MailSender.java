03a5d316be5c7420b3a0d2d46f8f9d42935f429d
naught_wo <catrinesaxon@yandex.ru>
2025-02-22 14:15:49 +0300

    Задание "Вы работаете в Microsoft!"

---
+++ src/main/java/school/faang/naughtwoBJS260577/MailSender.java
@@ -0,0 +1,24 @@
+package school.faang.naughtwoBJS260577;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+public class MailSender {
+    private static final int ALL_LETTERS = 1000;
+    private static final int ALL_THREADS = 5;
+
+    public static void main(String[] args) {
+        int letterGroup = ALL_LETTERS / ALL_THREADS;
+
+        ExecutorService executor = Executors.newFixedThreadPool(ALL_THREADS);
+
+        for (int i = 0; i < ALL_THREADS; i++) {
+            int startIndex = i * letterGroup +1;
+            int endIndex = startIndex + letterGroup;
+            executor.submit(new SenderRunnable(startIndex, endIndex));
+        }
+
+        executor.shutdown();
+        System.out.println("All letters have been sent!");
+    }
+}
