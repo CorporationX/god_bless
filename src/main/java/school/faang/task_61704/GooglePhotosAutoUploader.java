b60262a9ef8d78dd2b8980a97dcc68c7ceaa7c18
Yaroslav Olkin <shrelok.pupkin@yandex.ru>
2025-02-22 23:48:20 +0300

    --

--- src/main/java/school/faang/task_61704/GooglePhotosAutoUploader.java
+++ src/main/java/school/faang/task_61704/GooglePhotosAutoUploader.java
@@ -6,7 +6,7 @@ import org.slf4j.LoggerFactory;
 import java.util.ArrayList;
 import java.util.List;
 
-public class GooglePhotosAutoUploader{
+public class GooglePhotosAutoUploader {
     private static final Logger LOG = LoggerFactory.getLogger(GooglePhotosAutoUploader.class);
     private final Object lock = new Object();
     private final List<String> photosToUpload = new ArrayList<>();
