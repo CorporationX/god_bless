89fa2e382d4d14128757261b779d013412a08694
naught_wo <catrinesaxon@yandex.ru>
2025-02-23 10:01:36 +0300

    Задание "Путь пальцем проложи…"

---
+++ src/main/java/school/faang/naughtwoBJS260907/City.java
@@ -0,0 +1,6 @@
+package school.faang.naughtwoBJS260907;
+
+public record City(
+        String name,
+        Location location
+) {}
---
+++ src/main/java/school/faang/naughtwoBJS260907/Location.java
@@ -0,0 +1,13 @@
+package school.faang.naughtwoBJS260907;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+public class Location {
+    private int x;
+    private int y;
+}
