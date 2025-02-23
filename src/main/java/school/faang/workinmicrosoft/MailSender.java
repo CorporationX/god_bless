def2782654706981de7ee7d198ec9d2dda429fbb
sunmajor <makar.a.v100@gmail.com>
2025-02-23 22:26:08 +0300

    Вы работаете в Microsoft

---
+++ src/main/java/school/faang/workinmicrosoft/MailSender.java
@@ -0,0 +1,25 @@
+package school.faang.workinmicrosoft;
+
+public class MailSender {
+    private static final int TOTAL_MESSAGES = 1000;
+    private static final int THREADS_COUNT = 5;
+
+    public static void main(String[] args) throws InterruptedException {
+        int batchSize = TOTAL_MESSAGES / THREADS_COUNT;
+
+        Thread[] threads = new Thread[THREADS_COUNT];
+
+        for (int i = 0; i < THREADS_COUNT; i++) {
+            int start = i * batchSize;
+            int end = (i + 1) * batchSize;
+            threads[i] = new Thread(new SenderRunnable(start, end));
+            threads[i].start();
+        }
+
+        for (Thread thread : threads) {
+            thread.join();
+        }
+
+        System.out.println("Все письма отправлены!");
+    }
+}
---
+++ src/main/java/school/faang/workinmicrosoft/SenderRunnable.java
@@ -0,0 +1,16 @@
+package school.faang.workinmicrosoft;
+
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
+public class SenderRunnable implements Runnable {
+    private final int startIndex;
+    private final int endIndex;
+
+    @Override
+    public void run() {
+        for (int i = startIndex; i < endIndex; i++) {
+            System.out.println("Письмо отправлено: " + i);
+        }
+    }
+}
