0605074138c4875ef96ce2376afde16a336c2cda
Sergey Ermoshenko <ermoshenko.work@gmail.com>
2025-02-23 17:59:53 +0300

    add ingredients gathering

--- src/main/java/school/faang/PotionGathering.java
+++ src/main/java/school/faang/PotionGathering.java
@@ -8,16 +8,18 @@ import java.util.concurrent.atomic.AtomicInteger;
 
 @Slf4j
 public class PotionGathering {
-    public static int gatherIngredients(Potion potion) {
-        try {
-            Thread.sleep(2000);
-        } catch (InterruptedException e) {
-            log.error("Sleep was interrupted.");
-            Thread.currentThread().interrupt();
-            throw new RuntimeException(e);
-        }
-        log.info("All ingredients for {} have been collected.", potion.getName());
-        return potion.getRequiredIngredients();
+    public static CompletableFuture<Integer> gatherIngredients(Potion potion) {
+        return CompletableFuture.supplyAsync(() -> {
+            try {
+                Thread.sleep(2000);
+            } catch (InterruptedException e) {
+                log.error("Sleep was interrupted.");
+                Thread.currentThread().interrupt();
+                throw new RuntimeException(e);
+            }
+            log.info("All ingredients for {} have been collected.", potion.getName());
+            return potion.getRequiredIngredients();
+        });
     }
 
     public static void main(String[] args) {
@@ -27,13 +29,13 @@ public class PotionGathering {
                 new Potion("Stamina Potion", 4)
         );
 
-        AtomicInteger totalIngredients = new AtomicInteger(0);
         List<CompletableFuture<Integer>> futures = potions.stream()
-                .map(potion -> CompletableFuture.supplyAsync(() -> gatherIngredients(potion))
-                        .thenApply(totalIngredients::addAndGet))
+                .map(PotionGathering::gatherIngredients)
                 .toList();
 
         CompletableFuture.allOf(futures.toArray(new CompletableFuture[0])).join();
+        AtomicInteger totalIngredients = new AtomicInteger(0);
+        futures.forEach(future -> future.thenApply(totalIngredients::addAndGet));
         log.info("Total amount of collected ingredients: {}", totalIngredients.get());
     }
 }
