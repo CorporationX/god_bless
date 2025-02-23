45e0778de0c809d8573fe3059cf0c0b3001eddb3
Andrey Ievlev <avievlev@gmail.com>
2025-02-23 21:17:37 +0300

    BJS2-61694 Bro Force!

---
+++ src/main/java/school/faang/BJS2_61694/ConstAndMessages.java
@@ -0,0 +1,9 @@
+package school.faang.BJS2_61694;
+
+public final class ConstAndMessages {
+    public static final int DEFAULT_LIVES = 3;
+    public static final String WINNER_OF_GAME = "There's only one player {} left, he's the winner.";
+    public static final String TOTAL_RESULT = "Total score: {}\tTotal count lives: {}";
+
+    private ConstAndMessages() {}
+}
---
+++ src/main/java/school/faang/BJS2_61694/Main.java
@@ -0,0 +1,25 @@
+package school.faang.BJS2_61694;
+
+import lombok.extern.slf4j.Slf4j;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import static school.faang.BJS2_61694.ConstAndMessages.TOTAL_RESULT;
+import static school.faang.BJS2_61694.ConstAndMessages.WINNER_OF_GAME;
+
+@Slf4j
+public class Main {
+    private static final Game game = new Game();
+
+    public static void main(String[] args) {
+        ExecutorService executorService = Executors.newFixedThreadPool(5);
+        for (int i = 0; i < 7; i++) {
+            game.addPlayer(new Bro("Bro#" + i));
+        }
+        while (game.getPlayers().size() > 1) {
+            executorService.submit(game::update);
+        }
+        executorService.shutdown();
+        log.info(WINNER_OF_GAME, game.getPlayers().get(0).getName());
+        log.info(TOTAL_RESULT, game.getScore(), game.getLives());
+    }
+}
