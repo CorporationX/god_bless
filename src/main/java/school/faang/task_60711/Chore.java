88c1a7dd9e5633ee62b05c717cc7e886d14d5cfc
Tony King <a.korolev9927@gmail.com>
2025-02-23 22:20:08 +0300

    Внес правки по константам и awaitTermination

--- src/main/java/school/faang/task_60711/Chore.java
+++ src/main/java/school/faang/task_60711/Chore.java
@@ -9,12 +9,14 @@ import java.util.concurrent.ThreadLocalRandom;
 @AllArgsConstructor
 public class Chore implements Runnable {
     private String chore;
+    private static final int START_TIME = 1000;
+    private static final int FINAL_TIME = 10000;
 
     @Override
     public void run() {
         try {
             log.info("{} Выполняет задачу по дому: {}", Thread.currentThread().getName(), chore);
-            Thread.sleep(ThreadLocalRandom.current().nextInt(1, 11));
+            Thread.sleep(ThreadLocalRandom.current().nextInt(START_TIME, FINAL_TIME));
             log.info("Задача завершена: {}", chore);
         } catch (InterruptedException e) {
             Thread.currentThread().interrupt();
