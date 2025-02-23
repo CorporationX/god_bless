2c3936a8fb40e1e3aae56f7daf59dcdcc23cd089
KhafizovaIlona <anolihazz@gmail.com>
2025-02-23 20:31:45 +0300

    Bro Force! (стиль Main)

--- src/main/java/school/faang/broforce/Main.java
+++ src/main/java/school/faang/broforce/Main.java
@@ -5,10 +5,12 @@ import lombok.extern.slf4j.Slf4j;
 @Slf4j
 public class Main {
     private static final Game GAME = new Game();
+
     public static void main(String[] args) {
         for (int i = 0; i < 10; i++) {
             boolean isEarnedPoints = Math.random() >= 0.5;
             boolean isLostLife = Math.random() >= 0.5;
+
             try {
                 Thread thread = new Thread(() -> GAME.update(isEarnedPoints, isLostLife));
                 thread.start();
