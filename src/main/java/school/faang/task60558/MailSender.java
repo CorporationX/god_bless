efb31ca97a43896eec73d8ab6a3096f5d5de69a8
Winrida <winridas@gmail.com>
2025-02-23 17:49:45 +0300

    BJS2-61218 Распределенная армия героев меча и магии
    https://faang-school.atlassian.net/browse/BJS2-61218

--- src/main/java/school/faang/task60558/MailSender.java
+++ src/main/java/school/faang/task60558/MailSender.java
@@ -1,15 +1,14 @@
 package school.faang.task60558;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import lombok.extern.slf4j.Slf4j;
 
+@Slf4j
 public class MailSender {
-    private static final Logger LOGGER = LoggerFactory.getLogger(MailSender.class);
     private static final String SUCCESS_SEND_MESSAGE = "Все письма были отправлены";
     private static final int THREAD_AMOUNT = 5;
     private static final int BATCH_SIZE = 200;
 
-    public static void main(String[] args) throws InterruptedException {
+    public static void main(String[] args) {
         Thread[] threads = new Thread[THREAD_AMOUNT];
 
         for (int i = 0; i < THREAD_AMOUNT; i++) {
@@ -19,10 +18,14 @@ public class MailSender {
             threads[i].start();
         }
 
-        for (Thread thread : threads) {
-            thread.join();
+        try {
+            for (Thread thread : threads) {
+                thread.join();
+            }
+        } catch (InterruptedException e) {
+            throw new RuntimeException("Error threads join", e);
         }
 
-        LOGGER.info(SUCCESS_SEND_MESSAGE);
+        log.info(SUCCESS_SEND_MESSAGE);
     }
 }
--- src/main/java/school/faang/task60558/SenderRunnable.java
+++ src/main/java/school/faang/task60558/SenderRunnable.java
@@ -1,23 +1,18 @@
 package school.faang.task60558;
 
 import lombok.AllArgsConstructor;
-import lombok.Getter;
 import lombok.extern.slf4j.Slf4j;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 @AllArgsConstructor
-@Getter
 @Slf4j
 public class SenderRunnable implements Runnable {
-    private static final Logger LOGGER = LoggerFactory.getLogger(SenderRunnable.class);
     private int startIndex;
     private int endIndex;
 
     @Override
     public void run() {
         for (int i = startIndex; i < endIndex; i++) {
-            LOGGER.info("Письмо {} отправлено", i);
+            log.info("Письмо {} отправлено", i);
         }
     }
 }
