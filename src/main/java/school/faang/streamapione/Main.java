19dcdbf68056dac24ddaa589903a2cd5bf5935b3
sunmajor <makar.a.v100@gmail.com>
2025-02-23 14:43:35 +0300

    Потренируем StreamAPI 1. Исправленная

--- src/main/java/school/faang/streamapione/Main.java
+++ src/main/java/school/faang/streamapione/Main.java
@@ -15,16 +15,16 @@ public class Main {
         System.out.println(String.format("Среднее: %.2f",
                 ListOperations.findAverage(numbers)));
         System.out.println(String.format("Количество строк, начинающихся на 'a': %d",
-                ListOperations.countStringsStartingWith(strings,'a')));
+                ListOperations.countStringsStartingWith(strings, 'a')));
         System.out.println(String.format("Строки, содержащие 'an': %s",
-                ListOperations.filterStringsContainingSubstring(strings,"an")));
+                ListOperations.filterStringsContainingSubstring(strings, "an")));
         System.out.println(String.format("Отсортированные по длине строки: %s",
                 ListOperations.sortByLength(strings)));
         System.out.println(String.format("Все ли числа чётные? %b",
-                ListOperations.allMatchCondition(numbers,n ->n %2==0)));
+                ListOperations.allMatchCondition(numbers, n -> n % 2 == 0)));
         System.out.println(String.format("Наименьшее число больше 6: %d",
-                ListOperations.findMinGreaterThan(numbers,6)));
+                ListOperations.findMinGreaterThan(numbers, 6)));
         System.out.println(String.format("Количество букв в словах: %s",
                 ListOperations.convertToLengths(strings)));
-}
+    }
 }
