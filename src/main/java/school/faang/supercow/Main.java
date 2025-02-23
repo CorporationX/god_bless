26b307a2f1ec2d26b2294fe6a5a2f201777bcf5f
KhafizovaIlona <anolihazz@gmail.com>
2025-02-23 20:14:03 +0300

    Supercow! (корректировки Main)

--- src/main/java/school/faang/supercow/Main.java
+++ src/main/java/school/faang/supercow/Main.java
@@ -7,21 +7,21 @@ import java.util.List;
 
 @Slf4j
 public class Main {
-    public static void main(String[] args) {
-        List<Player> players = new ArrayList<>();
-        Boss boss = new Boss(2, 3);
+    private static final Boss BOSS = new Boss(2, 3);
+    private static final List<Player> PLAYERS = new ArrayList<>();
 
+    public static void main(String[] args) {
         Player playerHarry = new Player("Harry");
         Player playerRon = new Player("Ron");
         Player playerDraco = new Player("Draco");
-        players.add(playerDraco);
-        players.add(playerRon);
-        players.add(playerHarry);
+        PLAYERS.add(playerDraco);
+        PLAYERS.add(playerRon);
+        PLAYERS.add(playerHarry);
 
-        for (Player player : players) {
+        for (Player player : PLAYERS) {
             Thread thread = new Thread(() -> {
                 try {
-                    player.doBattle(boss);
+                    player.doBattle(BOSS);
                 } catch (InterruptedException | RuntimeException e) {
                     log.error(e.getMessage(), e);
                 }
