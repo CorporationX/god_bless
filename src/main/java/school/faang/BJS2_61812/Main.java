af06cf81e6f26c6c8d7d801d329b164e7dd4179b
Sergey <bigwave2915@gmail.com>
2025-02-24 11:03:22 +0500

    исправил

--- src/main/java/school/faang/BJS2_61812/Main.java
+++ src/main/java/school/faang/BJS2_61812/Main.java
@@ -1,7 +1,9 @@
 package school.faang.BJS2_61812;
 
+import java.util.concurrent.ExecutionException;
+
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws ExecutionException, InterruptedException {
         MasterCardService masterCardService = new MasterCardService();
         masterCardService.doAll();
     }
--- src/main/java/school/faang/BJS2_61812/MasterCardService.java
+++ src/main/java/school/faang/BJS2_61812/MasterCardService.java
@@ -1,31 +1,47 @@
 package school.faang.BJS2_61812;
 
 import lombok.extern.slf4j.Slf4j;
-
+import java.util.concurrent.*;
 
 @Slf4j
 public class MasterCardService {
     private static final int TEN_SECONDS_IN_MS = 10_000;
     private static final int ONE_SECOND_IN_MS = 1_000;
 
-    static int collectPayment() {
+    private static int collectPayment() {
         try {
             Thread.sleep(TEN_SECONDS_IN_MS);
             return 5_000;
         } catch (InterruptedException e) {
             Thread.currentThread().interrupt();
+            log.error("Прерван процесс сбора платежа", e);
             throw new RuntimeException(e);
         }
     }
 
-    static int sendAnalytics() {
+    private static int sendAnalytics() {
         try {
             Thread.sleep(ONE_SECOND_IN_MS);
             return 17_000;
         } catch (InterruptedException e) {
             Thread.currentThread().interrupt();
+            log.error("Прерван процесс отправки аналитики", e);
             throw new RuntimeException(e);
         }
+    }
 
+    private static final  ExecutorService pool = Executors.newSingleThreadExecutor();
+
+    static void doAll() throws ExecutionException, InterruptedException {
+        Future<Integer> paymentFuture = pool.submit(MasterCardService::collectPayment);
+
+        CompletableFuture<Integer> analyticsFuture = CompletableFuture.supplyAsync(MasterCardService::sendAnalytics);
+
+        Integer paymentResult = paymentFuture.get();
+        log.debug("Платеж прошел : {}", paymentResult);
+        Integer analyticsResult = analyticsFuture.join();
+        log.debug("Аналитика отправлена :{}", analyticsResult);
+
+        pool.shutdown();
     }
 }
