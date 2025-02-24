5dc1e95e8e73f4f124e4c3847c148a1271c248c1
Ilyas Azhdar <azhdar902@gmail.com>
2025-02-24 18:38:47 +0500

    init spotify

---
+++ src/main/java/school/faang/synchronization/spotify/Music.java
@@ -0,0 +1,20 @@
+package school.faang.synchronization.spotify;
+
+public class Music {
+
+    public static void main(String[] args) {
+        Player player = new Player();
+
+        Thread play = new Thread(player::play);
+        play.start();
+
+        Thread pause = new Thread(player::pause);
+
+        pause.start();
+        Thread skip = new Thread(player::skip);
+        skip.start();
+
+        Thread previous = new Thread(player::previous);
+        previous.start();
+    }
+}
