65d7c9a8a6049bcd8df52be5bc0176bffc1b1a90
Sergey <bigwave2915@gmail.com>
2025-02-24 17:39:45 +0500

    начало решения  задачи

---
+++ src/main/java/school/faang/BJS2_62347/Student.java
@@ -0,0 +1,13 @@
+package school.faang.BJS2_62347;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@AllArgsConstructor
+@Getter
+public class Student {
+    private String name;
+    private int year;
+    private int points;
+}
---
+++ src/main/java/school/faang/BJS2_62347/Task.java
@@ -0,0 +1,13 @@
+package school.faang.BJS2_62347;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class Task {
+    private String name;
+    private int difficulty;
+    private int reward;
+}
