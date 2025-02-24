b5c697522714ac13bb73b033adba4e3eeb0d183c
Denis Popov <popoffdn@mail.ru>
2025-02-24 21:32:32 +0300

    ревью BJS2-60619

--- src/main/java/WorkOrMicrosoft/MailSender.java
+++ src/main/java/WorkOrMicrosoft/MailSender.java
@@ -4,34 +4,28 @@ public class MailSender {
     private static final int TOTAL_MESSAGE = 1000;
     private static final int COUNT_THREAD = 5;
 
-    public static void main(String[] args) throws InterruptedException {
-        int size = TOTAL_MESSAGE / COUNT_THREAD;
+    public static void main(String[] args)  {
+        try {
+            int size = TOTAL_MESSAGE / COUNT_THREAD;
 
-        Thread[] threads = new Thread[COUNT_THREAD];
+            Thread[] threads = new Thread[COUNT_THREAD];
 
-        for (int i = 0; i < COUNT_THREAD; i++) {
-            int start = i * size;
-            int end = (i + 1) * size;
-            threads[i] = new Thread(new SenderRunnable(start, end));
-            threads[i].start();
+            for (int i = 0; i < COUNT_THREAD; i++) {
+                int start = i * size;
+                int end = (i + 1) * size;
+                threads[i] = new Thread(new SenderRunnable(start, end));
+                threads[i].start();
+            }
+
+            for (Thread thread : threads) {
+                thread.join();
+            }
+
+            System.out.println("All messages have been sent");
+        } catch (InterruptedException e) {
+            System.out.println("Error. Program execution was interrupted");
+            e.printStackTrace();
         }
 
-        for (Thread thread : threads) {
-            thread.join();
-        }
-
-        /*
-        либо можно было написать не используя new Thread, как говорилось в лекции
-        int size = TOTAL_MESSAGE / COUNT_THREAD;
-        ExecutorService executor = Executors.newFixedThreadPool(COUNT_THREAD);
-
-        for (int i = 0; i < COUNT_THREAD; i++) {
-            int start = i * size;
-            int end = (i + 1) * size;
-            executor.submit(new SenderRunnable(start, end));
-        }
-        executor.shutdown();
-
-         */
     }
 }
