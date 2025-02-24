d53158c274529a4acdb2149a0bc9a0ac6fe7ebca
Hayotbek Farhodov <hayotbekfarhodov020@gmail.com>
2025-02-24 10:49:52 +0500

    Асинхронность и Future

---
+++ src/main/java/school/faang/asynchronyandfuture/Main.java
@@ -0,0 +1,8 @@
+package school.faang.asynchronyandfuture;
+
+public class Main {
+    public static void main(String[] args) {
+        MasterCardService service = new MasterCardService();
+        service.doAll();
+    }
+}
