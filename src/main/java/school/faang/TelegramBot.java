cc05d61fb4eb427debc911f62b444803f7629c3a
Sergey Ermoshenko <ermoshenko.work@gmail.com>
2025-02-24 09:29:07 +0300

    small fix

--- src/main/java/school/faang/TelegramBot.java
+++ src/main/java/school/faang/TelegramBot.java
@@ -3,25 +3,27 @@ package school.faang;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 
+import java.time.Duration;
 import java.time.LocalDateTime;
-import java.time.temporal.ChronoUnit;
 
 @Slf4j
 @AllArgsConstructor
 public class TelegramBot {
     private static final int REQUEST_LIMIT = 5;
+    private static final int MILLIS_IN_SECOND = 1000;
     private int requestCounter;
     private LocalDateTime lastRequestTime;
 
     public synchronized void sendMessage(String message) {
         LocalDateTime now = LocalDateTime.now();
-        long timeElapsedMillis = ChronoUnit.MILLIS.between(lastRequestTime, now);
-        if (timeElapsedMillis < 1000) {
+        long timeElapsedMillis = Duration.between(lastRequestTime, now).toMillis();
+        if (timeElapsedMillis < MILLIS_IN_SECOND) {
             requestCounter++;
             if (requestCounter > REQUEST_LIMIT) {
                 try {
-                    Thread.sleep(1000 - timeElapsedMillis);
+                    Thread.sleep(MILLIS_IN_SECOND - timeElapsedMillis);
                 } catch (InterruptedException e) {
+                    Thread.currentThread().interrupt();
                     throw new RuntimeException(e);
                 }
             }
