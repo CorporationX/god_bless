85c103346dd10dc2b6b8b51715ac084fb94c85dd
Sergey <bigwave2915@gmail.com>
2025-02-24 16:06:16 +0500

    решение задачи

---
+++ src/main/java/school/faang/BJS2_62096/QuestSystem.java
@@ -0,0 +1,27 @@
+package school.faang.BJS2_62096;
+
+import lombok.extern.slf4j.Slf4j;
+import java.util.concurrent.CompletableFuture;
+
+@Slf4j
+public class QuestSystem {
+    private static final int MILLISECONDS_IN_SECOND = 1000;
+
+    public CompletableFuture<Player> startQuest(Player player, Quest quest) {
+        if (player == null || quest == null) {
+            throw new IllegalArgumentException("Player и  Quest не могут быть null");
+        }
+        return CompletableFuture.supplyAsync(() -> {
+            try {
+                log.info("{} начал {} квест", player.getName(), quest.getName());
+                Thread.sleep((long) quest.getDifficulty() * MILLISECONDS_IN_SECOND);
+            } catch (InterruptedException e) {
+                Thread.currentThread().interrupt();
+                log.error("Поток был прерван во время выполнения квеста", e);
+                throw new RuntimeException("Поток был прерван", e);
+            }
+            player.addExperience(quest.getReward());
+            return player;
+        });
+    }
+}
