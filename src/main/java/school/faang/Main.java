f7a75d0fc562a359329c86dd9fafae0aac65738f
Sergey Ermoshenko <ermoshenko.work@gmail.com>
2025-02-24 20:06:20 +0300

    add square numbers calculation

--- src/main/java/school/faang/Main.java
+++ src/main/java/school/faang/Main.java
@@ -1,4 +1,35 @@
 package school.faang;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
 public class Main {
+    private static final int THREAD_POOL_SEIZE = 20;
+    private static final ExecutorService executor = Executors.newFixedThreadPool(THREAD_POOL_SEIZE);
+
+    private static void fanOutFanIn(List<SquareRequest> requests, ResultConsumer resultConsumer) {
+        List<CompletableFuture<Void>> futures = requests.stream()
+                .map(request -> CompletableFuture.runAsync(() -> request.longTimeSquare(resultConsumer), executor))
+                .toList();
+
+        CompletableFuture.allOf(futures.toArray(new CompletableFuture[0])).join();
+    }
+
+    private static void launch() {
+        ResultConsumer resultConsumer = new ResultConsumer(0L);
+        List<SquareRequest> requests = new ArrayList<>();
+        for (int i = 1; i <= 1000; ++i) {
+            requests.add(new SquareRequest((long) i));
+        }
+        fanOutFanIn(requests, resultConsumer);
+        executor.shutdown();
+        System.out.println("Sum of squares = " + resultConsumer.getSum());
+    }
+
+    public static void main(String[] args) {
+        launch();
+    }
 }
