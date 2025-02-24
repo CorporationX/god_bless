ea5ce3faf747db76d3fec04672edfc411a65f852
Sofiya Mutovkina <sofiyamutovkina@gmail.com>
2025-02-24 22:46:01 +0300

    исправила чекстайл

--- src/main/java/school/faang/supercow/Main.java
+++ src/main/java/school/faang/supercow/Main.java
@@ -5,19 +5,18 @@ public class Main {
         Boss boss = new Boss(2);
 
         Player player1 = new Player("Player 1");
-        Player player2 = new Player("Player 2");
-        Player player3 = new Player("Player 3");
-        Player player4 = new Player("Player 4");
-
         Thread thread1 = new Thread(() -> player1.doBattle(boss));
         thread1.start();
 
+        Player player2 = new Player("Player 2");
         Thread thread2 = new Thread(() -> player2.doBattle(boss));
         thread2.start();
 
+        Player player3 = new Player("Player 3");
         Thread thread3 = new Thread(() -> player3.doBattle(boss));
         thread3.start();
 
+        Player player4 = new Player("Player 4");
         Thread thread4 = new Thread(() -> player4.doBattle(boss));
         thread4.start();
     }
