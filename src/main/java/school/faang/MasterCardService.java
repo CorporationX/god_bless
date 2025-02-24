e12ea90aaac62774645dfe03ea29a9c2aa5ae327
Sergey Ermoshenko <ermoshenko.work@gmail.com>
2025-02-24 21:42:21 +0300

    small fix

--- src/main/java/school/faang/MasterCardService.java
+++ src/main/java/school/faang/MasterCardService.java
@@ -1,46 +1,50 @@
 package school.faang;
 
+import lombok.extern.slf4j.Slf4j;
+
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.Future;
 
+@Slf4j
 public class MasterCardService {
     private static final int THREAD_POOL_SIZE = 1;
     private final ExecutorService executorService = Executors.newFixedThreadPool(THREAD_POOL_SIZE);
+    private static final int PAYMENT_PROCESSING_DELAY_MS = 10000;
+    private static final int COLLECTING_PROCESSING_DELAY_MS = 10000;
 
-    public static int collectPayment() {
+    public int collectPayment() {
         try {
-            Thread.sleep(10000);
+            Thread.sleep(PAYMENT_PROCESSING_DELAY_MS);
         } catch (InterruptedException e) {
             Thread.currentThread().interrupt();
             e.printStackTrace();
         }
-        return 1000;
+        return 5000;
     }
 
-    public static int sendAnalytics() {
+    public int sendAnalytics() {
         try {
-            Thread.sleep(1000);
+            Thread.sleep(COLLECTING_PROCESSING_DELAY_MS);
         } catch (InterruptedException e) {
             Thread.currentThread().interrupt();
             e.printStackTrace();
         }
-        return 3200;
+        return 17000;
     }
 
     public void doAll() {
-        Future<Integer> futurePayment = executorService.submit(MasterCardService::collectPayment);
-        CompletableFuture<Integer> futureAnalytics = CompletableFuture.supplyAsync(
-                MasterCardService::sendAnalytics);
-
-        futureAnalytics.thenAccept(result -> System.out.println("Analytics has been sent: " + result));
+        Future<Integer> futurePayment = executorService.submit(this::collectPayment);
+        CompletableFuture<Integer> futureAnalytics = CompletableFuture.supplyAsync(this::sendAnalytics);
         try {
+            System.out.println("Analytics has been sent: " + futureAnalytics.get());
             System.out.println("Payment has been completed: " + futurePayment.get());
         } catch (InterruptedException | ExecutionException e) {
-            throw new RuntimeException(e);
+            log.error("Error: {} in method \"doAll\"", e.getMessage());
+        } finally {
+            executorService.shutdown();
         }
-        executorService.shutdown();
     }
 }
