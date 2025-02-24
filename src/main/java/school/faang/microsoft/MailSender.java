1b62f903c7345cc8528bf34d825b0cbc4a0e0a5f
Sofiya Mutovkina <sofiyamutovkina@gmail.com>
2025-02-24 20:02:03 +0300

    исправила

--- src/main/java/school/faang/microsoft/MailSender.java
+++ src/main/java/school/faang/microsoft/MailSender.java
@@ -1,24 +1,36 @@
 package school.faang.microsoft;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 public class MailSender {
     private static final int TOTAL_MESSAGES = 1000;
     private static final int THREADS_COUNT = 5;
+    public static final Logger logger = LoggerFactory.getLogger(MailSender.class);
 
-    public static void main(String[] args) throws InterruptedException {
-        int batchSize = TOTAL_MESSAGES / THREADS_COUNT;
-        Thread[] threads = new Thread[THREADS_COUNT];
+    public static void main(String[] args) {
+        try {
+            int batchSize = TOTAL_MESSAGES / THREADS_COUNT;
+            int remainder = TOTAL_MESSAGES % THREADS_COUNT;
+            Thread[] threads = new Thread[THREADS_COUNT];
 
-        for (int i = 0; i < THREADS_COUNT; i++) {
-            int start = i * batchSize;
-            int end = (i + 1) * batchSize;
-            threads[i] = new Thread(new SenderRunnable(start, end));
-            threads[i].start();
+            for (int i = 0; i < THREADS_COUNT; i++) {
+                int start = i * batchSize;
+                int end = start + batchSize + (i == THREADS_COUNT - 1 ? remainder : 0);
+                threads[i] = new Thread(new SenderRunnable(start, end));
+                threads[i].start();
+            }
+
+            for (Thread thread : threads) {
+                thread.join();
+            }
+
+            logger.info("All emails have been sent!");
+        } catch (InterruptedException e) {
+            logger.error("Thread execution was interrupted", e);
+            Thread.currentThread().interrupt();
+        } catch (Exception e) {
+            logger.error("An unexpected error occurred", e);
         }
-
-        for (Thread thread : threads) {
-            thread.join();
-        }
-
-        System.out.println("All emails have been sent!");
     }
 }
--- src/main/java/school/faang/microsoft/SenderRunnable.java
+++ src/main/java/school/faang/microsoft/SenderRunnable.java
@@ -1,17 +1,24 @@
 package school.faang.microsoft;
 
 import lombok.RequiredArgsConstructor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.util.stream.IntStream;
 
 @RequiredArgsConstructor
 public class SenderRunnable implements Runnable {
+    private static final Logger logger = LoggerFactory.getLogger(SenderRunnable.class);
     private final int startIndex;
     private final int endIndex;
 
     @Override
     public void run() {
-        IntStream.range(startIndex, endIndex)
-                .forEach(i -> System.out.println("The email has been sent " + i));
+        try {
+            IntStream.range(startIndex, endIndex)
+                    .forEach(i -> logger.info("The email has been sent {}", i));
+        } catch (Exception e) {
+            logger.error("Error occurred while sending emails", e);
+        }
     }
 }
