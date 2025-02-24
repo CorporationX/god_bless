4a15163d3ce88006d20876ff7b0a76fb8b1d98e6
Gulnaz <gumerova.gulnaz2014@yandex.ru>
2025-02-24 14:15:20 +0300

    Исправлены недочеты в классе BigBangTheory

--- src/main/java/school/faang/BJS2_61182/BigBangTheory.java
+++ src/main/java/school/faang/BJS2_61182/BigBangTheory.java
@@ -4,18 +4,22 @@ import java.util.List;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.Future;
-import java.util.stream.Collectors;
+import java.util.concurrent.TimeUnit;
 
 public class BigBangTheory {
+    private static final int THREAD_POOL_SIZE = 4;
+    private static final int TIMEOUT = 5;
+
     public static void main(String[] args) {
-        ExecutorService executor = Executors.newFixedThreadPool(4);
+        ExecutorService executor = Executors.newFixedThreadPool(THREAD_POOL_SIZE);
         List<Task> tasks = List.of(new Task("Шелдон", "подготовка теории"),
                 new Task("Леонард", "моделирование эксперимента"),
                 new Task("Говард", "разработка инструментов"),
                 new Task("Раджеш", "анализ данных")
         );
-        List<Future<?>> futures = tasks.stream()
-                .map(executor::submit).collect(Collectors.toList());
+        List<? extends Future<?>> futures = tasks.stream()
+                .map(executor::submit).toList();
+
         for (Future<?> future : futures) {
             try {
                 future.get();
@@ -25,5 +29,14 @@ public class BigBangTheory {
             }
         }
         executor.shutdown();
+        try {
+            if (!executor.awaitTermination(TIMEOUT, TimeUnit.SECONDS)) {
+                System.out.print("Timed out waiting for threads to finish\n");
+                executor.shutdownNow();
+            }
+        } catch (InterruptedException e) {
+            System.err.print("Thread interrupted\n");
+            executor.shutdownNow();
+        }
     }
 }
