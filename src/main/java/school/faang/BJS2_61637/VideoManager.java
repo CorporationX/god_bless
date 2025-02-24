770ed54d996ad79a8711cede66ee3d49bfa0aecb
Pavel Chernobay <mr.chernobay1990@gmail.com>
2025-02-24 13:50:47 +0300

    ConcurrentHashMap и getOrDefault

--- src/main/java/school/faang/BJS2_61637/VideoManager.java
+++ src/main/java/school/faang/BJS2_61637/VideoManager.java
@@ -2,19 +2,19 @@ package school.faang.BJS2_61637;
 
 import lombok.Getter;
 
-import java.util.HashMap;
 import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
 
 @Getter
 public class VideoManager {
 
-    private final Map<String, Integer> viewsMap = new HashMap<>();
+    private final Map<String, Integer> viewsMap = new ConcurrentHashMap<>();
 
-    public synchronized void addView(String videold) {
+    public void addView(String videold) {
         viewsMap.compute(videold, (k, v) -> (v == null) ? 1 : v + 1);
     }
 
     public int getViewCount(String videold) {
-        return viewsMap.get(videold);
+        return viewsMap.getOrDefault(videold, 0);
     }
 }
