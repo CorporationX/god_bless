f8a2ed2d370d93247a0bd666fc872afdf2162bee
Artem Doronin <doronintema2018@yandex.ru>
2025-02-26 00:49:54 +0500

    Качаем алхимию

---
+++ src/main/java/school/faang/task_62493/Potion.java
@@ -0,0 +1,13 @@
+package school.faang.task_62493;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@AllArgsConstructor
+@Getter
+public class Potion {
+    private String name;
+    private int requiredIngredients;
+
+
+}
---
+++ src/main/java/school/faang/task_62493/PotionGathering.java
@@ -0,0 +1,45 @@
+package school.faang.task_62493;
+
+import lombok.extern.slf4j.Slf4j;
+import java.util.List;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.atomic.AtomicInteger;
+
+@Slf4j
+public class PotionGathering {
+    public static void main(String[] args) {
+        List<Potion> potions = List.of(
+                new Potion("Healing Potion", 5),
+                new Potion("Mana Potion", 3),
+                new Potion("Stamina Potion", 4)
+        );
+
+        // Асинхронный сбор ингредиентов
+        gatherAllIngredients(potions);
+    }
+
+    public static CompletableFuture<Integer> gatherIngredients(Potion potion) {
+        return CompletableFuture.supplyAsync(() -> {
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                Thread.currentThread().interrupt();
+                throw new RuntimeException(e);
+            }
+            return potion.getRequiredIngredients();
+        });
+    }
+
+    public static void gatherAllIngredients(List<Potion> potions) {
+        AtomicInteger atomicInteger = new AtomicInteger(0);
+        List<CompletableFuture<Void>> futures = potions.stream()
+                .map(potion -> CompletableFuture.supplyAsync(() -> potion.getRequiredIngredients())
+                        .thenAccept(x -> atomicInteger.addAndGet(x)))
+                .toList();
+
+        CompletableFuture.allOf(futures.toArray(new CompletableFuture[0]))
+                .thenRun(() -> log.info("Общее количество собранных ингредиентов: " + atomicInteger.get()));
+
+    }
+
+}
