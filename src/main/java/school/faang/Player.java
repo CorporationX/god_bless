72eac7a8052f8ea774f70450acd2b349d8d9bd9c
bambum <gwalka885@gmai.com>
2025-02-25 19:59:51 +0500

    BJS2-62044 решение задачи

---
+++ src/main/java/school/faang/Player.java
@@ -0,0 +1,14 @@
+package school.faang;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@AllArgsConstructor
+@Data
+public class Player {
+    private String name;
+    private int level;
+    private int experience;
+
+
+}
