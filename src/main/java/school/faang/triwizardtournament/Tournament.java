cc0a4a2d9da44e3bdbc524f332127a611c97a144
Anton Kapshukov <kapshukov.04@mail.ru>
2025-02-25 16:23:08 +0300

    Турнир трех волшебников (с учетом комментариев)

--- src/main/java/school/faang/triwizardtournament/Tournament.java
+++ src/main/java/school/faang/triwizardtournament/Tournament.java
@@ -10,7 +10,6 @@ public class Tournament {
                 TimeUnit.SECONDS.sleep(task.getDifficulty());
                 school.getStudents()
                         .forEach(student -> student.setPoints(student.getPoints() + task.getReward()));
-                Thread.sleep(15L * task.getDifficulty());
             } catch (InterruptedException e) {
                 Thread.currentThread().interrupt();
                 throw new RuntimeException(e);
