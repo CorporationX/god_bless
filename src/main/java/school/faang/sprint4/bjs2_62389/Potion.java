037e2aabedde9638e6551a3935ccc624a458cdc2
Anton <basanton@mail.ru>
2025-02-25 12:49:39 +0300

    Выполнение задачи по "Качаем алхимию" версия 1

---
+++ src/main/java/school/faang/sprint4/bjs2_62389/Potion.java
@@ -0,0 +1,11 @@
+package school.faang.sprint4.bjs2_62389;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@AllArgsConstructor
+@Getter
+public class Potion {
+    private String name;
+    private int requiredIngredients;
+}
---
+++ src/main/java/school/faang/sprint4/bjs2_62389/PotionGathering.java
@@ -0,0 +1,49 @@
+package school.faang.sprint4.bjs2_62389;
+
+import lombok.NonNull;
+
+import java.util.List;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.atomic.AtomicInteger;
+
+public class PotionGathering {
+
+    public static CompletableFuture<Integer> gatherIngredients(@NonNull Potion potion) {
+        return CompletableFuture.supplyAsync(() -> {
+            try {
+                Thread.sleep(potion.getRequiredIngredients() * 1000L);
+            } catch (InterruptedException e) {
+                Thread.currentThread().interrupt();
+            }
+            return potion.getRequiredIngredients();
+        });
+    }
+
+    public static void gatherAllIngredients(@NonNull List<Potion> potions) {
+        List<CompletableFuture<Integer>> futures = potions.stream()
+                .map(PotionGathering::gatherIngredients)
+                .toList();
+
+        CompletableFuture<Void> allFutures = CompletableFuture.allOf(
+                futures.toArray(new CompletableFuture[0])
+        );
+
+        AtomicInteger totalIngredients = new AtomicInteger(0);
+
+        allFutures.thenRun(() -> {
+            for (CompletableFuture<Integer> future : futures) {
+                totalIngredients.addAndGet(future.join());
+            }
+            System.out.println("Общее количество собранных ингредиентов: " + totalIngredients.get());
+        }).join();
+    }
+
+    public static void main(String[] args) {
+        List<Potion> potions = List.of(
+                new Potion("Healing Potion", 5),
+                new Potion("Mana Potion", 3),
+                new Potion("Stamina Potion", 4)
+        );
+        gatherAllIngredients(potions);
+    }
+}
