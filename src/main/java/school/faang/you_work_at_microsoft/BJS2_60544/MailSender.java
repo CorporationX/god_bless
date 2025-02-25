e3119b86ba33897e0abdec7fc9dec46274842b7c
Artpyak <art.pyak@yandex.ru>
2025-02-25 20:48:25 +0300

    BJS2-60544 1. Класс MailSender доработан так что если TOTAL_MESSAGES делится на THREADS_COUNT с остатком остаток добавляется в последний поток. 2. Добавлена обработка ошибки в классе MailSender.

--- src/main/java/school/faang/you_work_at_microsoft/BJS2_60544/MailSender.java
+++ src/main/java/school/faang/you_work_at_microsoft/BJS2_60544/MailSender.java
@@ -1,24 +1,40 @@
 package school.faang.you_work_at_microsoft.BJS2_60544;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 public class MailSender {
-    private static final int TOTAL_MESSAGES = 1000;
+    private static final Logger log = LoggerFactory.getLogger(MailSender.class);
+
+    private static final int TOTAL_MESSAGES = 1003;
     private static final int THREADS_COUNT = 5;
 
-    public static void main(String[] args) throws InterruptedException {
+    public static void main(String[] args) {
         int batchSize = TOTAL_MESSAGES / THREADS_COUNT;
+        int remainder = TOTAL_MESSAGES % THREADS_COUNT;
         Thread[] threads = new Thread[THREADS_COUNT];
 
         for (int i = 0; i < threads.length; i++) {
             int start = i * batchSize;
             int end = start + batchSize;
+
+            if (i == threads.length - 1) {
+                end += remainder;
+            }
+
             threads[i] = new Thread(new SenderRunnable(start, end));
             threads[i].start();
         }
 
         for (Thread thread : threads) {
-            thread.join();
+            try {
+                thread.join();
+            } catch (InterruptedException e) {
+                log.error("Главный поток был прерван.{}", e.getMessage(), e);
+                Thread.currentThread().interrupt();
+                return;
+            }
         }
-
-        System.out.println("Все письма отправлены!");
+        log.info("Все письма отправлены!");
     }
 }
--- src/main/java/school/faang/you_work_at_microsoft/BJS2_60544/SenderRunnable.java
+++ src/main/java/school/faang/you_work_at_microsoft/BJS2_60544/SenderRunnable.java
@@ -2,17 +2,21 @@ package school.faang.you_work_at_microsoft.BJS2_60544;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 @AllArgsConstructor
 @Getter
 public class SenderRunnable implements Runnable {
+    private static final Logger log = LoggerFactory.getLogger(SenderRunnable.class);
+
     private final int startIndex;
     private final int endIndex;
 
     @Override
     public void run() {
         for (int i = startIndex; i <= endIndex; i++) {
-            System.out.println("Письмо отправлено");
+            log.info("Письмо отправлено");
         }
     }
 }
