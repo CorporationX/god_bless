8284c3028c6700e160e0e60fc0f9537dc2981fbd
Vadim Naletov <naletov96@gmail.com>
2025-02-25 15:47:10 +0400

    QuestSystem: done with CompletableFuture

---
+++ src/main/java/school/faang/wow/QuestSystem.java
@@ -0,0 +1,23 @@
+package school.faang.wow;
+
+import lombok.extern.slf4j.Slf4j;
+
+import java.util.concurrent.CompletableFuture;
+
+@Slf4j
+public class QuestSystem {
+
+    private static final long DIFFICULTY_COEFFICIENT = 1000;
+
+    public CompletableFuture<Player> startQuest(Player player, Quest quest) {
+        return CompletableFuture.supplyAsync(() -> {
+            try {
+                Thread.sleep(quest.getDifficulty() * DIFFICULTY_COEFFICIENT);
+                player.addExperience(quest.getReward());
+            } catch (InterruptedException e) {
+                log.error("Поток прерван во время выполнения квеста", e);
+            }
+            return player;
+        });
+    }
+}
