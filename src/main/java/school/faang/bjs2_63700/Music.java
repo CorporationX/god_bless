37fa703b0a42d2fbd95dbd5cd1f8555f4a6edfd3
Ekaterina Ramakoti <zubkova-92@mail.ru>
2025-02-26 22:52:49 +0300

    правки BJS2-63700

--- src/main/java/school/faang/bjs2_63700/Music.java
+++ src/main/java/school/faang/bjs2_63700/Music.java
@@ -1,18 +1,34 @@
 package school.faang.bjs2_63700;
 
+import lombok.extern.slf4j.Slf4j;
+
+import java.util.ArrayList;
 import java.util.List;
 
+@Slf4j
 public class Music {
+    private static List<Thread> THREADS = new ArrayList<>();
+
     public static void main(String[] args) {
         Player player = new Player();
-        List<Thread> threads = List.of(
+        THREADS = List.of(
                 new Thread(player::pause),
                 new Thread(player::skip),
                 new Thread(player::previous),
                 new Thread(player::play)
         );
-        for (Thread thread : threads) {
+
+        for (Thread thread : THREADS) {
             thread.start();
+        }
+
+        for (Thread thread : THREADS) {
+            try {
+                thread.join();
+            } catch (InterruptedException e) {
+                log.error("Ошибка при ожидании потока", e);
+                throw new RuntimeException("Ошибка при ожидании потока", e);
+            }
         }
     }
 }
--- src/main/java/school/faang/bjs2_63700/Player.java
+++ src/main/java/school/faang/bjs2_63700/Player.java
@@ -5,7 +5,7 @@ import lombok.extern.slf4j.Slf4j;
 @Slf4j
 public class Player {
     private final Object lock = new Object();
-    private boolean isPlaying = true;
+    private boolean isPlaying = false;
 
     public void play() {
         synchronized (lock) {
