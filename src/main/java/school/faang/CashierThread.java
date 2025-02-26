b5278dbde15e51dff6fb49b43969694258bbf4ba
Ivan Prygunov <88151612+juzu400@users.noreply.github.com>
2025-02-26 15:14:03 +0300

    внес правки

--- src/main/java/school/faang/CashierThread.java
+++ src/main/java/school/faang/CashierThread.java
@@ -2,31 +2,28 @@ package school.faang;
 
 import lombok.AllArgsConstructor;
 
-import java.util.List;
-import java.util.concurrent.atomic.AtomicInteger;
-
 @AllArgsConstructor
 public class CashierThread extends Thread {
     private final int id;
-    private final List<String> customerItems;
+    private final String[] customerItems;
 
     public void run() {
-        System.out.printf("Кассир %s начал обслуживание покупателя с товарами %s\n", id, customerItems.size());
-        AtomicInteger totalPrice = new AtomicInteger();
-        AtomicInteger totalQuantity = new AtomicInteger();
+        System.out.printf("Кассир %s начал обслуживание покупателя с товарами %s\n", id, customerItems.length);
+        int totalPrice = 0;
+        int totalQuantity = 0;
 
-        customerItems
-                .forEach((item) -> {
-                    totalQuantity.getAndIncrement();
-                    totalPrice.addAndGet(50);
-                    try {
-                        Thread.sleep(1000);
-                    } catch (InterruptedException e) {
-                        System.out.println("Выполнение задачи было прервано");
-                        Thread.currentThread().interrupt();
-                        throw new RuntimeException(e);
-                    }
-                });
+
+        for (int i = 0; i < customerItems.length; i++) {
+            totalPrice += 50;
+            totalQuantity += 1;
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                System.out.println("Выполнение задачи было прервано");
+                Thread.currentThread().interrupt();
+                throw new RuntimeException(e);
+            }
+        }
         System.out.printf("Кассир %s закончил обслуживание покупателя. " +
                 "Итоговое количество товаров %s, общая стоимость %s\n",
                 id, totalQuantity, totalPrice);
--- src/main/java/school/faang/Main.java
+++ src/main/java/school/faang/Main.java
@@ -1,7 +1,5 @@
 package school.faang;
 
-import java.util.Arrays;
-import java.util.List;
 import java.util.Random;
 
 public class Main {
@@ -9,16 +7,16 @@ public class Main {
     private static final Random RANDOM = new Random();
 
     public static void main(String[] args) throws InterruptedException {
-        List<List<String>> customers = Arrays.asList(
-                Arrays.asList("Колбаса", "Мороженое"),
-                Arrays.asList("Раки", "Пиво", "Кальмар"),
-                Arrays.asList("Йогурт", "Гранола", "Протеиновый батончик", "Курица")
-        );
+        String[][] customers = {
+                {"Колбаса", "Мороженое"},
+                {"Раки", "Пиво", "Кальмар"},
+                {"Йогурт", "Гранола", "Протеиновый батончик", "Курица"}
+        };
 
-        CashierThread[] cashiers = new CashierThread[customers.size()];
+        CashierThread[] cashiers = new CashierThread[customers.length];
 
         int i = 0;
-        for (List<String> customer : customers) {
+        for (String[] customer : customers) {
             int cashierId = RANDOM.nextInt(NUMS_OF_CASHIERS) + 1;
             cashiers[i++] = new CashierThread(cashierId, customer);
         }
