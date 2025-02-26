aed4fa4807c0d5919edc8eab84acf82e36979485
Dmitry Rogulkin <rogulkind@yandex.ru>
2025-02-26 12:24:11 +0500

    https://faang-school.atlassian.net/browse/BJS2-61978

--- src/main/java/school/faang/task61978/service/Player.java
+++ src/main/java/school/faang/task61978/service/Player.java
@@ -1,4 +1,40 @@
 package school.faang.task61978.service;
 
 public class Player {
+    private final Object lock = new Object();
+    private boolean isPlaying;
+
+    public void play() {
+        synchronized (lock) {
+            if (!isPlaying) {
+                isPlaying = true;
+                System.out.println("Музыка воспроизводится.");
+            } else {
+                System.out.println("Музыка уже играет.");
+            }
+        }
+    }
+
+    public void pause() {
+        synchronized (lock) {
+            if (isPlaying) {
+                isPlaying = false;
+                System.out.println("Музыка на паузе.");
+            } else {
+                System.out.println("Музыка уже на паузе.");
+            }
+        }
+    }
+
+    public void skip() {
+        synchronized (lock) {
+            System.out.println("Пропускаем трек");
+        }
+    }
+
+    public void previous() {
+        synchronized (lock) {
+            System.out.println("Возврат на предыдущий трек");
+        }
+    }
 }
