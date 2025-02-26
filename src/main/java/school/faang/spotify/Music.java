ea44151dc29c2db72385e3bfce3c5e7c1e36d354
Sofiya Mutovkina <sofiyamutovkina@gmail.com>
2025-02-26 23:32:11 +0300

    добавила логгер и автоматизацию в классе Music

--- src/main/java/school/faang/spotify/Music.java
+++ src/main/java/school/faang/spotify/Music.java
@@ -4,16 +4,15 @@ public class Music {
     public static void main(String[] args) {
         Player player = new Player();
 
-        Thread playThread = new Thread(player::play);
-        playThread.start();
+        Thread playThread = createAndStartThread(player::play);
+        Thread pauseThread = createAndStartThread(player::pause);
+        Thread skipThread = createAndStartThread(player::skip);
+        Thread previousThread = createAndStartThread(player::previous);
+    }
 
-        Thread pauseThread = new Thread(player::pause);
-        pauseThread.start();
-
-        Thread skipThread = new Thread(player::skip);
-        skipThread.start();
-
-        Thread previousThread = new Thread(player::previous);
-        previousThread.start();
+    private static Thread createAndStartThread(Runnable task) {
+        Thread thread = new Thread(task);
+        thread.start();
+        return thread;
     }
 }
--- src/main/java/school/faang/spotify/Player.java
+++ src/main/java/school/faang/spotify/Player.java
@@ -1,40 +1,47 @@
 package school.faang.spotify;
 
 import lombok.Getter;
-import lombok.Synchronized;
+
+import java.util.logging.Logger;
 
 public class Player {
+    private static final Logger logger = Logger.getLogger(Player.class.getName());
     private final Object lock = new Object();
+
     @Getter
-    private boolean isPlaying = false;
+    private volatile boolean isPlaying = false;
 
-    @Synchronized("lock")
     public void play() {
-        if (!isPlaying) {
-            isPlaying = true;
-            System.out.println("The music is playing.");
-        } else {
-            System.out.println("The music is already playing.");
+        synchronized (lock) {
+            if (!isPlaying) {
+                isPlaying = true;
+                logger.info("The music is playing.");
+            } else {
+                logger.info("The music is already playing.");
+            }
         }
     }
 
-    @Synchronized("lock")
     public void pause() {
-        if (isPlaying) {
-            isPlaying = false;
-            System.out.println("The music is paused.");
-        } else {
-            System.out.println("The music is already on pause.");
+        synchronized (lock) {
+            if (isPlaying) {
+                isPlaying = false;
+                logger.info("The music is paused.");
+            } else {
+                logger.info("The music is already on pause.");
+            }
         }
     }
 
-    @Synchronized("lock")
     public void skip() {
-        System.out.println("Track skipped.");
+        synchronized (lock) {
+            logger.info("Track skipped.");
+        }
     }
 
-    @Synchronized("lock")
     public void previous() {
-        System.out.println("Went back to the previous track.");
+        synchronized (lock) {
+            logger.info("Went back to the previous track.");
+        }
     }
 }
