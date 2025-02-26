4c8e1903f4372b9df2ab203293daa76d74001b6d
Takoi <takoihero17@gmail.com>
2025-02-27 00:32:43 +0500

    Распределенная армия героев меча и магии

--- src/main/java/school/faang/BJS2_61322/Main.java
+++ src/main/java/school/faang/BJS2_61322/Main.java
@@ -6,12 +6,11 @@ public class Main {
         Squad archers = new Squad();
         archers.addUnit(new Archer(25));
         archers.addUnit(new Archer(30));
+        army.addSquad(archers);
 
         Squad swordsmen = new Squad();
         swordsmen.addUnit(new Swordsman(40));
         swordsmen.addUnit(new Swordsman(35));
-
-        army.addSquad(archers);
         army.addSquad(swordsmen);
 
         System.out.println("Общая сила армии: " + army.calculateTotalPower());
