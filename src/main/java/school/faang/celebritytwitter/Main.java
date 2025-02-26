90553a34d41cdd1cb235a5605d6fe6a13fa3f6c2
Vitaliy <vitaliy.zhilitskiy@gmail.com>
2025-02-27 00:22:58 +0300

    Задача BJS2-62453 решена.

--- src/main/java/school/faang/celebritytwitter/Main.java
+++ src/main/java/school/faang/celebritytwitter/Main.java
@@ -8,9 +8,10 @@ import java.util.concurrent.atomic.AtomicInteger;
 @Slf4j
 public class Main {
     private static final int FOLLOWERS_COUNT = 200_000_000;
+
     public static void main(String[] args) {
         TwitterSubscriptionSystem twitterSubscription = new TwitterSubscriptionSystem();
-        TwitterAccount lovelyMentor = new TwitterAccount("Rinat" , new AtomicInteger(0));
+        TwitterAccount lovelyMentor = new TwitterAccount("Rinat", new AtomicInteger(0));
 
         CompletableFuture.allOf(CompletableFuture.runAsync(() -> {
             for (int i = 0; i < FOLLOWERS_COUNT; i++) {
