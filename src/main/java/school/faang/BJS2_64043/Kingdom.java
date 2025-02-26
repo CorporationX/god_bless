e1b140ff5d45c8b7bc397ee08bf893a0984a56d1
Sergey <bigwave2915@gmail.com>
2025-02-26 22:24:19 +0500

    решения задачи

--- src/main/java/school/faang/BJS2_64043/Kingdom.java
+++ src/main/java/school/faang/BJS2_64043/Kingdom.java
@@ -2,34 +2,35 @@ package school.faang.BJS2_64043;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
-import lombok.Setter;
 import lombok.extern.slf4j.Slf4j;
 
-import java.util.Random;
 import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.TimeUnit;
 
 @Slf4j
 @Getter
-@Setter
 @AllArgsConstructor
 public class Kingdom {
     private String name;
 
-    private void sendMessage(Kingdom receiver) {
-        log.debug("Королество {}отправляет сообщение королеству {}", this.name, receiver.getName());
+    public void sendMessage(Kingdom receiver) {
+        log.debug("Королевство {} отправляет сообщение королевству {}", this.name, receiver.getName());
     }
 
-    private static CompletableFuture<String> sendRaven(Kingdom sender, Kingdom receiver) {
-        Random random = new Random();
+    public static CompletableFuture<String> sendRaven(Kingdom sender, Kingdom receiver) {
         return CompletableFuture.supplyAsync(() -> {
-                try {
-
+            try {
+                log.debug("Письмо отправлено...");
+                TimeUnit.SECONDS.sleep(1);
+                if (Math.random() < 0.5) {
+                    return "Письмо успешно доставлено из " + sender.getName() + " в " + receiver.getName();
+                } else {
+                    throw new RuntimeException("письмо не доставлено, ворон пропал");
                 }
-
-
-
-
+            } catch (InterruptedException e) {
+                Thread.currentThread().interrupt();
+                throw new RuntimeException("Произошла ошибка при отправке письма", e);
+            }
         });
-
     }
 }
--- src/main/java/school/faang/BJS2_64043/Main.java
+++ src/main/java/school/faang/BJS2_64043/Main.java
@@ -1,4 +1,32 @@
 package school.faang.BJS2_64043;
 
+import lombok.extern.slf4j.Slf4j;
+
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.TimeUnit;
+
+@Slf4j
 public class Main {
+    private static final long SLEEP_TIME_SECONDS = (3);
+
+    public static void main(String[] args) {
+
+        Kingdom kingdom1 = new Kingdom("Московское");
+        Kingdom kingdom2 = new Kingdom("Ростовское");
+
+        kingdom1.sendMessage(kingdom2);
+
+        CompletableFuture<String> future = Kingdom.sendRaven(kingdom1, kingdom2);
+
+        future.thenAccept(System.out::println).exceptionally(ex -> {
+            log.warn("Ошибка: {}", ex.getMessage());
+            throw new RuntimeException("Ошибка при доставке сообщения", ex);
+        });
+
+        try {
+            TimeUnit.SECONDS.sleep(SLEEP_TIME_SECONDS);
+        } catch (InterruptedException e) {
+            throw new RuntimeException("Ошибка ", e);
+        }
+    }
 }
