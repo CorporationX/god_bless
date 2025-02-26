2f7fe1fd116921a9124145f1f63479a9996a8309
Tony King <a.korolev9927@gmail.com>
2025-02-26 22:55:52 +0300

    Оптимизировал количество кода через List

--- src/main/java/school/faang/task_61904/Music.java
+++ src/main/java/school/faang/task_61904/Music.java
@@ -2,30 +2,33 @@ package school.faang.task_61904;
 
 import lombok.extern.slf4j.Slf4j;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Slf4j
 public class Music {
 
     public static void main(String[] args) {
         Player player = new Player();
 
-        final Thread playThread = new Thread(() -> player.play());
-        final Thread pauseThread = new Thread(() -> player.pause());
-        final Thread skipThread = new Thread(() -> player.skip());
-        final Thread previousThread = new Thread(() -> player.previous());
+        final List<Thread> threads = new ArrayList<>();
 
-        playThread.start();
-        pauseThread.start();
-        skipThread.start();
-        previousThread.start();
+        threads.add(new Thread(player::play));
+        threads.add(new Thread(player::pause));
+        threads.add(new Thread(player::skip));
+        threads.add(new Thread(player::previous));
+
+        for (Thread thread : threads) {
+            thread.start();
+        }
 
         try {
-            playThread.join();
-            pauseThread.join();
-            skipThread.join();
-            previousThread.join();
+            for (Thread thread : threads) {
+                thread.join();
+            }
         } catch (InterruptedException e) {
             Thread.currentThread().interrupt();
-            log.error("Поток прерван");
+            log.error("Поток прерван. Причина: {}", e.getMessage());
         }
     }
 }
