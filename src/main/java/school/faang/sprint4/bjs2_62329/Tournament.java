7ffecbe89c592501ba67defb840c863e93135241
Anton <basanton@mail.ru>
2025-02-26 09:47:31 +0300

    Выполнение задачи по "Турнир трех волшебников" версия 1

--- src/main/java/school/faang/sprint4/bjs2_62329/Tournament.java
+++ src/main/java/school/faang/sprint4/bjs2_62329/Tournament.java
@@ -1,12 +1,13 @@
 package school.faang.sprint4.bjs2_62329;
 
 import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.TimeUnit;
 
 public class Tournament {
     public CompletableFuture<School> startTask(School school, Task task) {
         return CompletableFuture.supplyAsync(() -> {
             try {
-                Thread.sleep(task.getDifficulty() * 1000L);
+                TimeUnit.SECONDS.sleep(task.getDifficulty());
             } catch (InterruptedException e) {
                 Thread.currentThread().interrupt();
             }
