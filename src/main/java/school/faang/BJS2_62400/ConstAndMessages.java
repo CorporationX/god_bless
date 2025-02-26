a6f20042e6d80f9807cadf2a311bb0fe34887c28
Andrey Ievlev <avievlev@gmail.com>
2025-02-26 22:44:36 +0300

    BJS2-62400 Турнир трех волшебников

---
+++ src/main/java/school/faang/BJS2_62400/ConstAndMessages.java
@@ -0,0 +1,11 @@
+package school.faang.BJS2_62400;
+
+public final class ConstAndMessages {
+    public static final String SCHOOL_TOTAL_POINTS = "School {}, TotalPoints: {}";
+    public static final String WINNER_TOURNAMENT = "The winner of the tournament - {}";
+    public static final String WINNER_NOT_DETERMINED = "The winner of the tournament has not been determined";
+    public static final String ERROR_THREAD = "An execution error occurred in the thread";
+    public static final String SCHOOL_COMPLETED_TASK = "{} School has successfully completed the task {}";
+
+    private ConstAndMessages() {}
+}
---
+++ src/main/java/school/faang/BJS2_62400/Main.java
@@ -0,0 +1,53 @@
+package school.faang.BJS2_62400;
+
+import lombok.extern.slf4j.Slf4j;
+
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+import java.util.concurrent.CompletableFuture;
+
+@Slf4j
+public class Main {
+
+    public static void main(String[] args) {
+        Tournament tournament = new Tournament();
+        List<School> schools = new ArrayList<>();
+
+        // Создание школ
+        List<Student> hogwartsTeam = List.of(new Student("Harry", 5, 0), new Student("Hermione", 5, 0));
+        List<Student> beauxbatonsTeam = List.of(new Student("Fleur", 6, 0), new Student("Gabrielle", 6, 0));
+        List<Student> durmstrangTeam = List.of(new Student("Viktor", 6, 0), new Student("Igor", 6, 0));
+        School hogwarts = new School("Hogwarts", hogwartsTeam);
+        School beauxbatons = new School("Beauxbatons", beauxbatonsTeam);
+        School durmstrang = new School("Durmstrang", durmstrangTeam);
+
+        schools.add(hogwarts);
+        schools.add(beauxbatons);
+        schools.add(durmstrang);
+
+        // Создание заданий
+        Task task1 = new Task("Triwizard Tournament", 10, 100);
+        Task task2 = new Task("Yule Ball Preparations", 5, 50);
+        Task task3 = new Task("Do not breathe underwater", 10, 80);
+
+        // Запуск заданий для школ
+        CompletableFuture<School> hogwartsTask = tournament.startTask(hogwarts, task2);
+        CompletableFuture<School> beauxbatonsTask = tournament.startTask(beauxbatons, task3);
+        CompletableFuture<School> durmstrangTask = tournament.startTask(durmstrang, task1);
+
+        CompletableFuture<Void> allTasks = CompletableFuture.allOf(hogwartsTask, beauxbatonsTask, durmstrangTask);
+        // Обработка результатов всех заданий и определение победителя
+        allTasks.join();
+        allTasks.thenRun(() -> {
+            schools.forEach(school -> log.info(ConstAndMessages.SCHOOL_TOTAL_POINTS,
+                    school.getName(), school.getTotalPoints()));
+            schools.stream().max(Comparator.comparingInt(School::getTotalPoints))
+                    .ifPresentOrElse(s -> log.info(ConstAndMessages.WINNER_TOURNAMENT, s.getName()),
+                            () -> log.info(ConstAndMessages.WINNER_NOT_DETERMINED));
+        });
+
+        tournament.shutdown();
+    }
+
+}
