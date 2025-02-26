022f01aea321b451fdf986bb8b94b65bb86d0d69
Артемий <xkmitx@gmail.com>
2025-02-26 16:09:30 +0300

    fix

--- src/main/java/school/faang/task_61614/Boss.java
+++ src/main/java/school/faang/task_61614/Boss.java
@@ -6,13 +6,13 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 @Getter
 public class Boss {
-    private final int MaxPlayers;
+    private final int maxPlayers;
     private int currentPlayers = 0;
     private final Object lock = new Object();
 
     public void joinBattle(Player player) {
         synchronized (lock) {
-            while (currentPlayers >= MaxPlayers) {
+            while (currentPlayers >= maxPlayers) {
                 try {
                     System.out.printf("%s ждет свободный слот для сражения%n", player.getName());
                     lock.wait();
@@ -25,7 +25,7 @@ public class Boss {
             }
             currentPlayers++;
             System.out.printf("%s вступил в бой с боссом! Кол-во свободных мест %d%n",
-                    player.getName(), (MaxPlayers - currentPlayers));
+                    player.getName(), (maxPlayers - currentPlayers));
         }
     }
 
@@ -33,7 +33,7 @@ public class Boss {
         synchronized (lock) {
             currentPlayers--;
             System.out.printf("%s покинул сражение с боссом. Кол-во свободных мест: %d%n",
-                    player.getName(), (MaxPlayers - currentPlayers));
+                    player.getName(), (maxPlayers - currentPlayers));
             lock.notify();
         }
     }
