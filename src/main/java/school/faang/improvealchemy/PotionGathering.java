b90529312e99410230a96a1ac1fc5d2ecd464876
Max Goloviznin <roxet59rus@mail.ru>
2025-02-26 11:20:19 +0500

    BJS2-62477 Качаем алхимию

---
+++ src/main/java/school/faang/improvealchemy/PotionGathering.java
@@ -0,0 +1,42 @@
+package school.faang.improvealchemy;
+
+import lombok.extern.slf4j.Slf4j;
+
+import java.util.List;
+import java.util.Objects;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.atomic.AtomicInteger;
+
+@Slf4j
+public class PotionGathering {
+    private static final List<Potion> POTIONS = List.of(
+            new Potion("Healing Potion", 5),
+            new Potion("Mana Potion", 3),
+            new Potion("Stamina Potion", 4)
+    );
+    private static final int COLLECTING_TIME_PER_ONE_INGREDIENT = 1000;
+
+    public static void main(String[] args) {
+        AtomicInteger totalIngredients = new AtomicInteger(0);
+
+        List<CompletableFuture<Void>> futurePotions = POTIONS.stream()
+                .map(potion -> CompletableFuture.supplyAsync(() -> gatherAllIngredients(potion))
+                        .thenAccept(totalIngredients::addAndGet)
+                        .thenRun(() -> log.info("{} ingredients for potion {} collected",
+                                potion.requiredIngredients(), potion.name())))
+                .toList();
+
+        CompletableFuture.allOf(futurePotions.toArray(new CompletableFuture[0])).join();
+        log.info("Total ingredients: {}", totalIngredients);
+    }
+
+    private static int gatherAllIngredients(Potion potion) {
+        Objects.requireNonNull(potion, "Invalid potion value");
+        try {
+            Thread.sleep((long) potion.requiredIngredients() * COLLECTING_TIME_PER_ONE_INGREDIENT);
+        } catch (InterruptedException e) {
+            log.error("Async tasks failed. Exception: {}\nStack trace: {}", e, e.getStackTrace());
+        }
+        return potion.requiredIngredients();
+    }
+}
