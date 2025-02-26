960a8feb7feba52379b4bc252182852496882de3
sunmajor <makar.a.v100@gmail.com>
2025-02-26 13:40:11 +0300

    Вы работаете в Microsoft! Исправленная

--- src/main/java/school/faang/workinmicrosoft/MailSender.java
+++ src/main/java/school/faang/workinmicrosoft/MailSender.java
@@ -1,23 +1,31 @@
 package school.faang.workinmicrosoft;
 
 public class MailSender {
-    private static final int TOTAL_MESSAGES = 1000;
+    private static final int TOTAL_MESSAGES = 1003;
     private static final int THREADS_COUNT = 5;
 
-    public static void main(String[] args) throws InterruptedException {
-        int batchSize = TOTAL_MESSAGES / THREADS_COUNT;
+    public static void main(String[] args) {
+        try {
+            int batchSize = TOTAL_MESSAGES / THREADS_COUNT;
+            int remaining = TOTAL_MESSAGES % THREADS_COUNT;
 
-        Thread[] threads = new Thread[THREADS_COUNT];
+            Thread[] threads = new Thread[THREADS_COUNT];
 
-        for (int i = 0; i < THREADS_COUNT; i++) {
-            int start = i * batchSize;
-            int end = (i + 1) * batchSize;
-            threads[i] = new Thread(new SenderRunnable(start, end));
-            threads[i].start();
-        }
+            int start = 0;
 
-        for (Thread thread : threads) {
-            thread.join();
+            for (int i = 0; i < THREADS_COUNT; i++) {
+                int end = start + batchSize + (i < remaining ? 1 : 0); // Распределяем остаток по первым потокам
+                threads[i] = new Thread(new SenderRunnable(start, end));
+                threads[i].start();
+                start = end;
+            }
+
+            for (Thread thread : threads) {
+                thread.join();
+            }
+        } catch (InterruptedException e) {
+            Thread.currentThread().interrupt();
+            throw new IllegalArgumentException(e);
         }
 
         System.out.println("Все письма отправлены!");
--- src/main/java/school/faang/workinmicrosoft/SenderRunnable.java
+++ src/main/java/school/faang/workinmicrosoft/SenderRunnable.java
@@ -1,16 +1,19 @@
 package school.faang.workinmicrosoft;
 
 import lombok.RequiredArgsConstructor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 @RequiredArgsConstructor
 public class SenderRunnable implements Runnable {
+    private static final Logger logger = LoggerFactory.getLogger(SenderRunnable.class);
     private final int startIndex;
     private final int endIndex;
 
     @Override
     public void run() {
         for (int i = startIndex; i < endIndex; i++) {
-            System.out.println("Письмо отправлено: " + i);
+            logger.info("Письмо отправлено: " + i);
         }
     }
 }
