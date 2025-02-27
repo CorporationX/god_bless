c0114600bb43b9678a751889e2fd203739a9ad94
Artpyak <art.pyak@yandex.ru>
2025-02-27 17:08:15 +0300

    BJS2-61770 1. Использовал аннотацию @Slf4j

--- src/main/java/school/faang/blocking_spotify/BJS2_61770/Music.java
+++ src/main/java/school/faang/blocking_spotify/BJS2_61770/Music.java
@@ -4,7 +4,7 @@ public class Music {
     private final Player player = new Player();
     private final Thread playThread = new Thread(() -> player.play());
     private final Thread pauseThread  = new Thread(() -> player.pause());
-    private final Thread skipThread  = new Thread(() -> player.play());
+    private final Thread skipThread  = new Thread(() -> player.skip());
     private final Thread previousThread  = new Thread(() -> player.previous());
 
     public void multiUserSimulation() {
--- src/main/java/school/faang/blocking_spotify/BJS2_61770/Player.java
+++ src/main/java/school/faang/blocking_spotify/BJS2_61770/Player.java
@@ -1,11 +1,9 @@
 package school.faang.blocking_spotify.BJS2_61770;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import lombok.extern.slf4j.Slf4j;
 
+@Slf4j
 public class Player {
-    private static final Logger log = LoggerFactory.getLogger(Player.class);
-
     private final Object lock = new Object();
 
     private boolean isPlaying;
