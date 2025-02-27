eac093f2aad2cfca2a4e32457fb6940de0ababdf
elsledkov <elsledkov@gmail.com>
2025-02-27 20:58:57 +0300

    refactor after review task BJS2-62550

--- src/main/java/school/faang/sprint3/multithreading/task61990/VideoManager.java
+++ src/main/java/school/faang/sprint3/multithreading/task61990/VideoManager.java
@@ -1,30 +1,25 @@
 package school.faang.sprint3.multithreading.task61990;
 
-import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.extern.slf4j.Slf4j;
 
 import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ConcurrentMap;
 
-@Slf4j
-@AllArgsConstructor
 @Getter
 public class VideoManager {
-    private volatile Map<String, Integer> viewsMap;
+    private Map<String, Integer> viewsMap;
+
+    public VideoManager(Map<String, Integer> viewsMap) {
+        this.viewsMap = new ConcurrentHashMap<>(viewsMap);
+    }
 
     public synchronized void addView(String videoId) {
-        if (viewsMap.containsKey(videoId)) {
-            viewsMap.put(videoId, viewsMap.get(videoId) + 1);
-        } else {
-            log.warn("\nA video with id = {} not found", videoId);
-        }
+     viewsMap.merge(videoId, 1, Integer::sum);
     }
 
     public synchronized Integer getViewCount(String videoId) {
-        if (viewsMap.containsKey(videoId)) {
-            return viewsMap.get(videoId);
-        } else {
-            throw new RuntimeException("\nVideo with id = " + videoId + " not found");
-        }
+            return viewsMap.getOrDefault(videoId, 0);
     }
 }
