d2efe78ef873420442b295b6c0c936f92b8a41aa
Max Goloviznin <roxet59rus@mail.ru>
2025-02-27 15:53:32 +0500

    BJS2-62627 Поиграем в банк

--- src/main/java/school/faang/playingbank/Main.java
+++ src/main/java/school/faang/playingbank/Main.java
@@ -6,8 +6,10 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
 import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.CompletionException;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
 
 @Slf4j
 public class Main {
@@ -21,35 +23,42 @@ public class Main {
     private static final int MAX_AMOUNT = 1500;
     private static final int MIN_AMOUNT = 500;
     private static final int DEPOSIT_AMOUNT = 5000;
+    private static final int TIME_EXECUTION = 1;
+    private static final TimeUnit TIME_UNIT = TimeUnit.MINUTES;
 
     public static void main(String[] args) {
-        for (int i = 0; i < COUNT_ACCOUNTS; i++) {
-            Account account = new Account();
-            BANK.addAccount(account);
-            account.replenishAccountBalance(DEPOSIT_AMOUNT);
-        }
+        try {
+            for (int i = 0; i < COUNT_ACCOUNTS; i++) {
+                Account account = new Account();
+                BANK.addAccount(account);
+                account.replenishAccountBalance(DEPOSIT_AMOUNT);
+            }
 
-        for (int i = 0; i < COUNT_OPERATIONS; i++) {
-            FUTURES.add(CompletableFuture.runAsync(() -> {
-                int countAccounts = BANK.getAccounts().size();
-                int fromAccountId = 1 + RANDOM.nextInt(countAccounts);
-                int toAccountId = 1 + RANDOM.nextInt(countAccounts);
-                if (fromAccountId == toAccountId) {
-                    if (fromAccountId == BANK.getAccounts().size()) {
-                        fromAccountId--;
-                    } else {
-                        fromAccountId++;
+            for (int i = 0; i < COUNT_OPERATIONS; i++) {
+                FUTURES.add(CompletableFuture.runAsync(() -> {
+                    int countAccounts = BANK.getAccounts().size();
+                    int fromAccountId = 1 + RANDOM.nextInt(countAccounts);
+                    int toAccountId = 1 + RANDOM.nextInt(countAccounts);
+                    if (fromAccountId == toAccountId) {
+                        if (fromAccountId == BANK.getAccounts().size()) {
+                            fromAccountId--;
+                        } else {
+                            fromAccountId++;
+                        }
                     }
-                }
-                int amount = MIN_AMOUNT + (int) (Math.random() * (MAX_AMOUNT - MIN_AMOUNT + 1));
-                BANK.transfer(fromAccountId, toAccountId, amount);
-            }, EXECUTOR));
-        }
-        CompletableFuture.allOf(FUTURES.toArray(CompletableFuture[]::new)).join();
-        BANK.getTotalBalance();
-        BANK.getAccounts().values().forEach(account ->
-                log.info("Id: {}, balance: {}", account.getId(), account.getBalance()));
+                    int amount = MIN_AMOUNT + (int) (Math.random() * (MAX_AMOUNT - MIN_AMOUNT + 1));
+                    BANK.transfer(fromAccountId, toAccountId, amount);
+                }, EXECUTOR).orTimeout(TIME_EXECUTION, TIME_UNIT));
+            }
+            CompletableFuture.allOf(FUTURES.toArray(CompletableFuture[]::new)).join();
+            BANK.getTotalBalance();
+            BANK.getAccounts().values().forEach(account ->
+                    log.info("Id: {}, balance: {}", account.getId(), account.getBalance()));
 
-        EXECUTOR.shutdown();
+            EXECUTOR.shutdown();
+        } catch (CompletionException e) {
+            log.error("Timed out {} {}", TIME_EXECUTION, TIME_UNIT);
+            EXECUTOR.shutdownNow();
+        }
     }
 }
