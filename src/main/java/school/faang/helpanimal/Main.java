3fc769dbfaf7ab414dfef2b54aae64fad3b95a21
Max Goloviznin <roxet59rus@mail.ru>
2025-02-27 15:55:40 +0500

    BJS2-63054 Поддержим животных

--- src/main/java/school/faang/helpanimal/Main.java
+++ src/main/java/school/faang/helpanimal/Main.java
@@ -5,8 +5,10 @@ import lombok.extern.slf4j.Slf4j;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.CompletionException;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
 
 @Slf4j
 public class Main {
@@ -18,16 +20,24 @@ public class Main {
     private static final Organization ORGANIZATION = new Organization();
     private static final int MAX_DONATION = 1000;
     private static final int MIN_DONATION = 500;
+    private static final int TIME_EXECUTION = 1;
+    private static final TimeUnit TIME_UNIT = TimeUnit.MINUTES;
 
     public static void main(String[] args) {
-        for (int i = 0; i < COUNT_DONATIONS; i++) {
-            createDonation();
+        try {
+            for (int i = 0; i < COUNT_DONATIONS; i++) {
+                createDonation();
+            }
+            DONATIONS.forEach(donation -> FUTURES.add(CompletableFuture
+                    .runAsync(() -> ORGANIZATION.addDonation(donation), EXECUTOR)
+                    .orTimeout(TIME_EXECUTION, TIME_UNIT)));
+            CompletableFuture.allOf(FUTURES.toArray(CompletableFuture[]::new)).join();
+            log.info("Total sum organization: {}", ORGANIZATION.getTotalSum().get());
+            EXECUTOR.shutdown();
+        } catch (CompletionException e) {
+            log.error("Timed out {} {}", TIME_EXECUTION, TIME_UNIT);
+            EXECUTOR.shutdownNow();
         }
-        DONATIONS.forEach(donation -> FUTURES.add(CompletableFuture
-                .runAsync(() -> ORGANIZATION.addDonation(donation), EXECUTOR)));
-        CompletableFuture.allOf(FUTURES.toArray(CompletableFuture[]::new)).join();
-        log.info("Total sum organization: {}", ORGANIZATION.getTotalSum().get());
-        EXECUTOR.shutdown();
     }
 
     private static void createDonation() {
