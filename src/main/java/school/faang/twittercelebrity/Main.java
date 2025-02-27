64f643a39c02ec4e922a7c43f31faa43929cfa6a
Max Goloviznin <roxet59rus@mail.ru>
2025-02-27 15:49:43 +0500

    BJS2-62391 Становимся celebrity в Twitter

--- src/main/java/school/faang/twittercelebrity/Main.java
+++ src/main/java/school/faang/twittercelebrity/Main.java
@@ -5,6 +5,7 @@ import lombok.extern.slf4j.Slf4j;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.CompletionException;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.TimeUnit;
@@ -17,31 +18,23 @@ public class Main {
     private static final List<CompletableFuture<Void>> FUTURES = new ArrayList<>();
     private static final TwitterAccount MY_ACCOUNT = new TwitterAccount("Yom");
     private static final int TOTAL_FOLLOWERS = 1000000;
-    private static final int TIME_EXECUTION = 1;
-    private static final TimeUnit TIME_UNIT = TimeUnit.MINUTES;
+    private static final int TIME_EXECUTION = 3;
+    private static final TimeUnit TIME_UNIT = TimeUnit.SECONDS;
 
     public static void main(String[] args) {
         try {
             for (int i = 0; i < TOTAL_FOLLOWERS; i++) {
                 FUTURES.add(CompletableFuture
-                        .runAsync(() -> SUBSCRIPTION_SYSTEM.followAccount(MY_ACCOUNT), EXECUTOR));
+                        .runAsync(() -> SUBSCRIPTION_SYSTEM.followAccount(MY_ACCOUNT), EXECUTOR)
+                        .orTimeout(TIME_EXECUTION, TIME_UNIT));
             }
 
             CompletableFuture.allOf(FUTURES.toArray(CompletableFuture[]::new)).join();
-
+            log.info("Total account {} followers: {}", MY_ACCOUNT.getUsername(), MY_ACCOUNT.getFollowers());
             EXECUTOR.shutdown();
-            if (EXECUTOR.awaitTermination(TIME_EXECUTION, TIME_UNIT)) {
-                log.info("Total account {} followers: {}", MY_ACCOUNT.getUsername(), MY_ACCOUNT.getFollowers());
-            } else {
-                log.warn("Timed out {} {}", TIME_EXECUTION, TIME_UNIT);
-                EXECUTOR.shutdownNow();
-            }
-        } catch (InterruptedException e) {
-            log.error("Thread interrupted name: {}\nException: {}\n{}",
-                    Thread.currentThread().getName(), e, e.getStackTrace());
-            Thread.currentThread().interrupt();
+        } catch (CompletionException e) {
+            log.error("Execution timed out {} {}", TIME_EXECUTION, TIME_UNIT);
             EXECUTOR.shutdownNow();
         }
-
     }
 }
