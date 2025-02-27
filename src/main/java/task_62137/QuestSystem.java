7f4537e27734ffde4d6a1c9988a1f940ad2dcd24
Yaroslav Olkin <shrelok.pupkin@yandex.ru>
2025-02-27 19:39:07 +0300

    BJS2-62137

---
+++ src/main/java/task_62137/QuestSystem.java
@@ -0,0 +1,20 @@
+package task_62137;
+
+import lombok.extern.slf4j.Slf4j;
+
+import java.util.concurrent.CompletableFuture;
+
+@Slf4j
+public class QuestSystem {
+    public CompletableFuture<Player> startQuest(Player player, Quest quest) {
+        return CompletableFuture.supplyAsync(() -> {
+            try {
+                Thread.sleep(quest.getDifficulty() * 1000L);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+            player.addExperience(quest.getDifficulty());
+            return player;
+        });
+    }
+}
