9b78f9723be5230719ebcb8b943d3901d5793a75
alex198 <abykovskih@gmail.com>
2025-02-27 14:27:14 +0300

    BJS2-60855 added method awaitTerminetion and variable TASK_DURATION_MS, cycle for replaced on stream

--- src/main/java/school/faang/bjs2_60855/Chore.java
+++ src/main/java/school/faang/bjs2_60855/Chore.java
@@ -5,13 +5,15 @@ import lombok.AllArgsConstructor;
 @AllArgsConstructor
 public class Chore implements Runnable {
 
+    private static final int TASK_DURATION_MS = 1000;
+
     private String title;
 
     @Override
     public void run() {
         System.out.printf("Задача: \"%s\" выполняется в потоке %s%n", title, Thread.currentThread().getName());
         try {
-            Thread.sleep(1000);
+            Thread.sleep(TASK_DURATION_MS);
             System.out.printf("Задача: \"%s\" - выполнена!%n", title);
         } catch (InterruptedException e) {
             throw new RuntimeException(e);
--- src/main/java/school/faang/bjs2_60855/Main.java
+++ src/main/java/school/faang/bjs2_60855/Main.java
@@ -1,17 +1,25 @@
 package school.faang.bjs2_60855;
 
+import java.util.Arrays;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
 
 public class Main {
     public static void main(String[] args) {
-        ExecutorService pool = Executors.newCachedThreadPool();
 
+        ExecutorService pool = Executors.newCachedThreadPool();
         WeasleyFamily weasleyFamily = new WeasleyFamily();
 
-        for (int i = 0; i < weasleyFamily.getChores().length; i++) {
-            pool.submit(weasleyFamily.getChores()[i]);
-        }
+        Arrays.stream(weasleyFamily.getChores())
+                .forEach(pool::submit);
+
         pool.shutdown();
+        try {
+            pool.awaitTermination(5, TimeUnit.SECONDS);
+        } catch (InterruptedException e) {
+            Thread.currentThread().interrupt();
+        }
+
     }
 }
