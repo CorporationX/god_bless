10fb10df8342d2144d6d95c26ba0e5f37008681f
romankondratev <romankondratev.com@gmail.com>
2025-02-28 15:20:35 -0800

    BJS2-61907 feat: добавить автоматическую загрузку фотографий в Google Photos
    - Создан класс `GooglePhotosAutoUploader` для обработки загрузки фотографий
    - Реализован метод `onNewPhotoAdded` для добавления фотографий в очередь
    - Добавлен метод `startAutoUpload` для ожидания и триггера загрузки
    - Создан метод `uploadPhotos` для загрузки фотографий и очистки очереди
    - Реализован основной класс `Main` для демонстрации работы загрузчика с использованием многопоточности

---
+++ src/main/java/school/faang/task_61907/Main.java
@@ -0,0 +1,48 @@
+package school.faang.task_61907;
+
+import lombok.extern.slf4j.Slf4j;
+
+import java.util.List;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
+
+@Slf4j
+public class Main {
+    public static final int EXECUTOR_TIMEOUT = 5;
+    public static final int THREADS_COUNT = 10;
+
+    public static void main(String[] args) {
+        final GooglePhotosAutoUploader uploader = new GooglePhotosAutoUploader();
+        final ExecutorService executor = Executors.newFixedThreadPool(THREADS_COUNT);
+        final List<String> photosToUpload = getPhotosToUpload();
+
+        photosToUpload.forEach(photoPath -> executor.execute(() -> uploader.onNewPhotoAdded(photoPath)));
+
+        executor.submit(uploader::startAutoUpload);
+        executor.shutdown();
+
+        try {
+            if (!executor.awaitTermination(EXECUTOR_TIMEOUT, TimeUnit.SECONDS)) {
+                log.info("Executor terminated successfully or timeout occurred.");
+                executor.shutdownNow();
+            }
+        } catch (InterruptedException e) {
+            log.error("Error waiting for executor to terminate", e);
+        }
+    }
+
+    private static List<String> getPhotosToUpload() {
+        return List.of(
+                "/photos/1.jpg",
+                "/photos/2.jpg",
+                "/photos/3.jpg",
+                "/photos/4.jpg",
+                "/photos/5.jpg",
+                "/photos/6.jpg",
+                "/photos/7.jpg",
+                "/photos/8.jpg",
+                "/photos/9.jpg",
+                "/photos/10.jpg");
+    }
+}
