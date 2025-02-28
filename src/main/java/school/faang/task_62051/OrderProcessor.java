19d83fcde29e51ab63b8665064ceeb640b90dde9
Artem Doronin <doronintema2018@yandex.ru>
2025-02-28 14:07:15 +0500

    fix bugs

--- src/main/java/school/faang/task_62051/OrderProcessor.java
+++ src/main/java/school/faang/task_62051/OrderProcessor.java
@@ -1,20 +1,23 @@
 package school.faang.task_62051;
 
 import lombok.extern.slf4j.Slf4j;
+
 import java.util.List;
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.atomic.AtomicInteger;
 
 @Slf4j
 public class OrderProcessor {
+    private static final int TIME_MILLISECONDS = 1000;
     private final AtomicInteger totalProcessedOrders = new AtomicInteger(0);
 
     public CompletableFuture<Void> processOrder(Order order) {
         return CompletableFuture.runAsync(() -> {
             try {
-                Thread.sleep(2000);
+                Thread.sleep(TIME_MILLISECONDS);
             } catch (InterruptedException e) {
-                throw new RuntimeException(e);
+                log.error("Поток был прерван: {}", e.getMessage(), e);
+                throw new RuntimeException("Поток был прерван: " + e.getMessage(), e);
             }
             order.setStatus("Обработано");
             totalProcessedOrders.incrementAndGet();
