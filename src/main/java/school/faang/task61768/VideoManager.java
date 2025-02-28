f115c449a3be75e30e9a0e3468fd7c6ee5ff9b38
Winrida <winridas@gmail.com>
2025-02-28 19:09:14 +0300

    BJS2-61768 Счетчик просмотров на YouTube Правки
    https://faang-school.atlassian.net/browse/BJS2-61768

--- src/main/java/school/faang/task61768/VideoManager.java
+++ src/main/java/school/faang/task61768/VideoManager.java
@@ -2,23 +2,17 @@ package school.faang.task61768;
 
 import lombok.extern.slf4j.Slf4j;
 
-import java.util.HashMap;
-import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
 
 @Slf4j
 public class VideoManager {
-    private final Map<String, Integer> viewsMap = new HashMap<>();
-    private final Object lock = new Object();
+    private final ConcurrentHashMap<String, Integer> viewsMap = new ConcurrentHashMap<>();
 
     public void addView(String videoId) {
-        synchronized (lock) {
-            viewsMap.put(videoId, viewsMap.getOrDefault(videoId, 0) + 1);
-        }
+        viewsMap.merge(videoId, 1, Integer::sum);
     }
 
     public int getViewCount(String videoId) {
-        synchronized (lock) {
-            return viewsMap.getOrDefault(videoId, 0);
-        }
+        return viewsMap.getOrDefault(videoId, 0);
     }
 }
