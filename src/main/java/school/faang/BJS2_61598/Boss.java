8cac577b699f13d5e37f3cbfb383a0428a6574c2
Gulnaz <gumerova.gulnaz2014@yandex.ru>
2025-02-28 13:14:37 +0300

    В классе Boss в методах joinBattle() и leaveBattle() блоки synchronized заменены на методы synchronized

--- src/main/java/school/faang/BJS2_61598/Boss.java
+++ src/main/java/school/faang/BJS2_61598/Boss.java
@@ -10,27 +10,24 @@ public class Boss {
     private final int maxPlayers;
     private int currentPlayers;
 
-    public void joinBattle(Player player) {
-        synchronized (this) {
-            if (currentPlayers == maxPlayers) {
-                try {
-                    System.out.printf("Места для битвы с %s заполнены. Ожидайте...\n", name);
-                    this.wait();
-                } catch (InterruptedException e) {
-                    System.err.println("Произошла ошибка при ожидании места в битве.");
-                    return;
-                }
+    public synchronized void joinBattle(Player player) {
+        if (currentPlayers >= maxPlayers) {
+            try {
+                System.out.printf("Места для битвы с %s заполнены. Ожидайте...\n", name);
+                this.wait();
+            } catch (InterruptedException e) {
+                System.err.println("Произошла ошибка при ожидании места в битве.");
+                return;
             }
-            currentPlayers++;
-            System.out.printf("Игрок %s вступил в битву против %s.\n", player.getName(), name);
         }
+        currentPlayers++;
+        System.out.printf("Игрок %s вступил в битву против %s.\n", player.getName(), name);
+
     }
 
-    public void leaveBattle(Player player) {
-        synchronized (this) {
-            currentPlayers--;
-            System.out.printf("Игрок %s покинул битву.\n", player.getName());
-            this.notify();
-        }
+    public synchronized void leaveBattle(Player player) {
+        currentPlayers--;
+        System.out.printf("Игрок %s покинул битву.\n", player.getName());
+        this.notify();
     }
 }
