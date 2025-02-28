3c33c26533e9019468e723f5bb888bc0456915ca
Sergey Ermoshenko <ermoshenko.work@gmail.com>
2025-02-28 17:41:31 +0300

    add conference streaming

--- src/main/java/school/faang/Main.java
+++ src/main/java/school/faang/Main.java
@@ -1,4 +1,17 @@
 package school.faang;
 
+import java.util.concurrent.CompletableFuture;
+import java.util.stream.IntStream;
+
 public class Main {
+    public static void main(String[] args) {
+        Conference conference = new Conference(10);
+        CompletableFuture.allOf(IntStream.range(0, 20)
+                .mapToObj(i -> CompletableFuture.runAsync(() -> {
+                    Participant participant = new Participant(conference);
+                    participant.joinConference();
+                }))
+                .toArray(CompletableFuture[]::new)
+        ).join();
+    }
 }
