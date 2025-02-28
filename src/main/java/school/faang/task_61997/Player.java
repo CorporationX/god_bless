26780c313972d6f5dc377ab12e841ca4c2793163
romankondratev <romankondratev.com@gmail.com>
2025-02-27 20:43:20 -0800

    BJS2-61997 refactor: упрощение логики управления воспроизведением
    - Удалена сложная логика блокировки команд (lockHolder, currentCommand, isLocked, releaseLock)
    - Упрощено состояние воспроизведения с использованием флага isPlaying
    - Добавлена проверка на повторное выполнение play/pause
    - Обновлены лог-сообщения для команд play, pause, skip, previous

--- src/main/java/school/faang/task_61997/Player.java
+++ src/main/java/school/faang/task_61997/Player.java
@@ -1,101 +1,43 @@
 package school.faang.task_61997;
 
-import lombok.SneakyThrows;
 import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
 public class Player {
     private final Object lock = new Object();
+    private boolean isPlaying = false; // Tracks playback state
 
-    private Thread lockHolder = null;
-    private String currentCommand = "None";
-
-    @SneakyThrows
     public void play() {
-        if (isLocked("Play")) {
-            logBlockedCommand("Play");
-            return;
-        }
-
         synchronized (lock) {
-            try {
-                currentCommand = "Play";
-                log.info("Playing... [Thread: {}]", Thread.currentThread().getId());
-            } finally {
-                releaseLock();
+            if (isPlaying) {
+                log.info("Play command ignored. Already playing. [Thread: {}]", Thread.currentThread().getId());
+                return;
             }
+            isPlaying = true;
+            log.info("Playing... [Thread: {}]", Thread.currentThread().getId());
         }
     }
 
-    @SneakyThrows
     public void pause() {
-        if (isLocked("Pause")) {
-            logBlockedCommand("Pause");
-            return;
-        }
         synchronized (lock) {
-            try {
-                currentCommand = "Pause";
-                log.info("Paused... [Thread: {}]", Thread.currentThread().getId());
-            } finally {
-                releaseLock();
+            if (!isPlaying) {
+                log.info("Pause command ignored. Already paused. [Thread: {}]", Thread.currentThread().getId());
+                return;
             }
+            isPlaying = false;
+            log.info("Paused... [Thread: {}]", Thread.currentThread().getId());
         }
     }
 
-    @SneakyThrows
     public void skip() {
-        if (isLocked("Skip")) {
-            logBlockedCommand("Skip");
-            return;
-        }
-
         synchronized (lock) {
-            try {
-                currentCommand = "Skip";
-                log.info("Skipped... [Thread: {}]", Thread.currentThread().getId());
-            } finally {
-                releaseLock();
-            }
+            log.info("Skipped to the next track. [Thread: {}]", Thread.currentThread().getId());
         }
     }
 
-    @SneakyThrows
     public void previous() {
-        if (isLocked("Previous")) {
-            logBlockedCommand("Previous");
-            return;
-        }
-
         synchronized (lock) {
-            try {
-                currentCommand = "Previous";
-                log.info("Previous... [Thread: {}]", Thread.currentThread().getId());
-            } finally {
-                releaseLock();
-            }
+            log.info("Returned to the previous track. [Thread: {}]", Thread.currentThread().getId());
         }
-    }
-
-    private boolean isLocked(String command) {
-        if (lockHolder == null) {
-            lockHolder = Thread.currentThread();
-            currentCommand = command;
-            return false;
-        }
-        return true;
-    }
-
-    private void releaseLock() {
-        lockHolder = null;
-        currentCommand = "None";
-    }
-
-    private void logBlockedCommand(String command) {
-        log.info(
-                "Block command: {}, Reason: Executing '{}', Thread ID: {}",
-                command,
-                currentCommand,
-                lockHolder != null ? lockHolder.getId() : "None");
     }
 }
