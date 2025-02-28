6c42370884b07a01f9910571f3247aeb48e14344
Artem Doronin <doronintema2018@yandex.ru>
2025-02-28 13:15:12 +0500

    Становимся celebrity в Twitter

--- src/main/java/school/faang/task_62497/Main.java
+++ src/main/java/school/faang/task_62497/Main.java
@@ -3,7 +3,6 @@ package school.faang.task_62497;
 public class Main {
     public static void main(String[] args) {
         TwitterSubscriptionSystem system = new TwitterSubscriptionSystem();
-
         system.followAccount(new TwitterAccount("Alex", 2));
     }
 }
