171a4172506caea85842c19eb634dd32e07cb9fc
Pavel Chernobay <mr.chernobay1990@gmail.com>
2025-02-28 14:12:46 +0300

    Логирование ошибок

--- src/main/java/school/faang/BJS2_61937/MasterCardService.java
+++ src/main/java/school/faang/BJS2_61937/MasterCardService.java
@@ -13,11 +13,13 @@ public class MasterCardService {
 
     private static final int TEN_SECONDS_IN_MS = 10_000;
     private static final int ONE_SECOND_IN_MS = 1_000;
+    public static final int PAYMENT_AMOUNT = 5_000;
+    public static final int ANALYTICS_AMOUNT = 17_000;
 
     public static int collectPayment() {
         try {
             Thread.sleep(TEN_SECONDS_IN_MS);
-            return 5_000;
+            return PAYMENT_AMOUNT;
         } catch (InterruptedException e) {
             Thread.currentThread().interrupt();
             throw new RuntimeException(e);
@@ -27,7 +29,7 @@ public class MasterCardService {
     public static int sendAnalytics() {
         try {
             Thread.sleep(ONE_SECOND_IN_MS);
-            return 17_000;
+            return ANALYTICS_AMOUNT;
         } catch (InterruptedException e) {
             Thread.currentThread().interrupt();
             throw new RuntimeException(e);
@@ -36,8 +38,11 @@ public class MasterCardService {
 
     public void doAll() {
         ExecutorService es = Executors.newSingleThreadScheduledExecutor();
+
+        log.info("Запуск операции по сбору платежа...");
         Future<Integer> futureResult = es.submit(MasterCardService::collectPayment);
 
+        log.info("Запуск операции по отправке аналитики...");
         CompletableFuture<Integer> completableResult = CompletableFuture.supplyAsync(MasterCardService::sendAnalytics);
 
         log.info("Аналитика отправлена: {}", completableResult.join());
@@ -45,6 +50,7 @@ public class MasterCardService {
         try {
             log.info("Результат платежа: {}", futureResult.get());
         } catch (InterruptedException | ExecutionException e) {
+            log.error("Ошибка выполнения операций: {}", e.getMessage(), e);
             throw new RuntimeException(e);
         } finally {
             es.shutdown();
