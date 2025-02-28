039c13f81f457b5afe9790e630d3d498cfbaba94
Evgeny Morzeev <morzeevevg@gmail.com>
2025-02-28 23:31:10 +0300

    task completed

--- src/main/java/chores/Chore.java
+++ src/main/java/chores/Chore.java
@@ -9,18 +9,19 @@ import java.util.logging.Logger;
 @Getter
 public class Chore implements Runnable {
     private static final Logger logger = Logger.getLogger(Chore.class.getName());
+    private static final int TASK_DURATION_MS = 1500;
     private final String title;
 
     @Override
     public void run() {
         try {
             String threadName = Thread.currentThread().getName();
-            logger.info(threadName + " is starting the chore: " + title);
-            Thread.sleep(1500);
-            logger.info(threadName + " has finished the chore: " + title);
+            logger.info(String.format("%s is starting the chore: %s", threadName, title));
+            Thread.sleep(TASK_DURATION_MS);
+            logger.info(String.format("%s has finished the chore: %s", threadName, title));
         } catch (InterruptedException e) {
+            logger.severe(String.format("Task was interrupted: %s.", title));
             Thread.currentThread().interrupt();
-            logger.severe("Task was interrupted: " + title);
         }
     }
 }
--- src/main/java/chores/WeasleyFamily.java
+++ src/main/java/chores/WeasleyFamily.java
@@ -5,27 +5,28 @@ import java.util.concurrent.Executors;
 import java.util.concurrent.TimeUnit;
 
 public class WeasleyFamily {
-    public static void main(String[] args) throws InterruptedException {
-        String[] chores = {
-                "Watering the plants...",
-                "Vacuuming the floor...",
-                "Cooking...",
-                "Ironing the clothes...",
-                "Making the bed...",
-                "Cleaning the kitchen...",
-                "Washing the dishes...",
-                "Doing the laundry..."
-        };
+    private static final String[] CHORES = {
+            "Watering the plants...",
+            "Vacuuming the floor...",
+            "Cooking...",
+            "Ironing the clothes...",
+            "Making the bed...",
+            "Cleaning the kitchen...",
+            "Washing the dishes...",
+            "Doing the laundry..."
+    };
+    private static final int SHUTDOWN_TIMEOUT_SECONDS = 60;
 
+    public static void main(String[] args) throws InterruptedException {
         ExecutorService executorService = Executors.newCachedThreadPool();
-        for (String choreTitle : chores) {
+        for (String choreTitle : CHORES) {
             Chore chore = new Chore(choreTitle);
             executorService.submit(chore);
 
         }
 
         executorService.shutdown();
-        if (!executorService.awaitTermination(60, TimeUnit.SECONDS)) {
+        if (!executorService.awaitTermination(SHUTDOWN_TIMEOUT_SECONDS, TimeUnit.SECONDS)) {
             executorService.shutdownNow();
         }
     }
