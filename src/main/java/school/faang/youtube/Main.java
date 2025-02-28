c3e8c0942734edbdb88d783aacc6b6a527aa7b00
Sofiya Mutovkina <sofiyamutovkina@gmail.com>
2025-02-28 16:54:58 +0300

    исправила

--- src/main/java/school/faang/youtube/Main.java
+++ src/main/java/school/faang/youtube/Main.java
@@ -3,12 +3,15 @@ package school.faang.youtube;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.TimeUnit;
+import java.util.logging.Logger;
 import java.util.stream.IntStream;
+
 
 public class Main {
     private static final int NUM_THREADS = 10;
     private static final int NUM_VIDEOS = 5;
     private static final int VIEWS_PER_THREAD = 100;
+    private static final Logger logger = Logger.getLogger(Main.class.getName());
 
     public static void main(String[] args) {
         VideoManager videoManager = new VideoManager();
@@ -27,12 +30,14 @@ public class Main {
                 executorService.shutdownNow();
             }
         } catch (InterruptedException e) {
+            Thread.currentThread().interrupt();
+            logger.severe("ExecutorService was interrupted: " + e.getMessage());
             executorService.shutdownNow();
         }
 
         IntStream.range(0, NUM_VIDEOS).forEach(i -> {
-            String videoId = "video " + i;
-            System.out.printf("%s views %d%n", videoId, videoManager.getViewCount(videoId));
+            String videoId = "video" + i;
+            logger.info(videoId + " views: " + videoManager.getViewCount(videoId));
         });
     }
 }
