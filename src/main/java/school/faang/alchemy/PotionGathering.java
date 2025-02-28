42e8e1f8675eb79e6e1adc8f694e904dba8aa6ee
Vitaliy <vitaliy.zhilitskiy@gmail.com>
2025-02-28 16:40:54 +0300

    Внесены правки в задачу BJS2-62470.

--- src/main/java/school/faang/alchemy/PotionGathering.java
+++ src/main/java/school/faang/alchemy/PotionGathering.java
@@ -22,23 +22,24 @@ public class PotionGathering {
     public static void gatherAllIngredients(List<Potion> potions) {
         AtomicInteger totalIngredients = new AtomicInteger(0);
 
-        List<CompletableFuture<Void>> futures = potions.stream()
-                .map(potion -> CompletableFuture.supplyAsync(() -> gatherIngredients(potion))
-                        .thenAccept(totalIngredients::addAndGet))
-                .collect(Collectors.toList());
+        List<CompletableFuture<Integer>> futures = potions.stream()
+                .map(PotionGathering::gatherIngredients)
+                .toList();
 
         CompletableFuture.allOf(futures.toArray(new CompletableFuture[0])).join();
         System.out.println("Общее количество собранных ингредиентов: " + totalIngredients.get());
     }
 
-    public static int gatherIngredients(Potion potion) {
-        try {
-            System.out.println("Сбор ингредиентов для " + potion.getName());
-            Thread.sleep(1000);
-        } catch (InterruptedException e) {
-            log.info(e.getMessage(), e);
-            Thread.currentThread().interrupt();
-        }
-        return potion.getRequiredIngredients();
+    public static CompletableFuture<Integer> gatherIngredients(Potion potion) {
+        return CompletableFuture.supplyAsync(() -> {
+            try {
+                System.out.println("Сбор ингредиентов для " + potion.getName());
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                log.info(e.getMessage(), e);
+                Thread.currentThread().interrupt();
+            }
+            return potion.getRequiredIngredients();
+        });
     }
 }
