4c275607a3be6b759928e15b97ddf77012fe8b49
Артемий <xkmitx@gmail.com>
2025-03-01 15:45:57 +0300

    Task 63915

---
+++ src/main/java/school/faang/task_63915/Player.java
@@ -0,0 +1,16 @@
+package school.faang.task_63915;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@AllArgsConstructor
+@Getter
+public class Player {
+    private final String name;
+    private int level;
+    private int experience;
+
+    public int addExperience(int experience) {
+        return this.experience += experience;
+    }
+}
---
+++ src/main/java/school/faang/task_63915/QuestSystem.java
@@ -0,0 +1,18 @@
+package school.faang.task_63915;
+
+import java.util.concurrent.CompletableFuture;
+
+public class QuestSystem {
+    public CompletableFuture<Player> startQuest(Player player, Quest quest) {
+        return CompletableFuture.supplyAsync(() -> {
+            try {
+                Thread.sleep(quest.getDifficulty() * 1000L);
+            } catch (InterruptedException e) {
+                Thread.currentThread().interrupt();
+                throw new RuntimeException("Поток прерван: " + e);
+            }
+            return new Player(player.getName(), player.getLevel(),
+                    player.addExperience(quest.getReward()));
+        });
+    }
+}
