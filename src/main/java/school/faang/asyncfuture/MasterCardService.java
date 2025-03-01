fb575f0eb70acc833ba8284b3cff22bcd8df6a3f
phtanya <112714509+phtanya@users.noreply.github.com>
2025-03-01 21:35:42 +0100

    BJS2-63810

---
+++ src/main/java/school/faang/asyncfuture/MasterCardService.java
@@ -0,0 +1,58 @@
+package school.faang.asyncfuture;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+
+public class MasterCardService {
+    private static final int TEN_SECONDS_IN_MS = 10_000;
+    private static final int ONE_SECOND_IN_MS = 1_000;
+
+    private static final Logger logger = LoggerFactory.getLogger(MasterCardService.class);
+
+    static int collectPayment() {
+        try {
+            Thread.sleep(TEN_SECONDS_IN_MS);
+            return 5_000;
+        } catch (InterruptedException e) {
+            Thread.currentThread().interrupt();
+            throw new RuntimeException(e);
+        }
+    }
+
+    static int sendAnalytics() {
+        try {
+            Thread.sleep(ONE_SECOND_IN_MS);
+            return 17_000;
+        } catch (InterruptedException e) {
+            Thread.currentThread().interrupt();
+            throw new RuntimeException(e);
+        }
+    }
+
+    public void doAll() {
+        ExecutorService executor = Executors.newSingleThreadExecutor();
+        try {
+            Future<Integer> paymentFuture = executor.submit(() -> MasterCardService.collectPayment());
+
+            CompletableFuture<Integer> analyticsFuture = CompletableFuture.supplyAsync(
+                    () -> MasterCardService.sendAnalytics()
+            );
+            Integer analyticsResult = analyticsFuture.join();
+            logger.info("Analytics sent: {}", analyticsResult);
+
+            Integer paymentResult = paymentFuture.get();
+            logger.info("Payment sent: {}", paymentResult);
+        } catch (InterruptedException | ExecutionException e) {
+            Thread.currentThread().interrupt();
+            throw new RuntimeException(e);
+        } finally {
+            executor.shutdown();
+        }
+    }
+}
