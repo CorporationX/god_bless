ab4702610147fac25a2685fd5c24c3863eec336c
Ilyas Azhdar <azhdar902@gmail.com>
2025-03-02 01:25:59 +0500

    fix

--- src/main/java/school/faang/amazonorders/OrderProcessor.java
+++ src/main/java/school/faang/amazonorders/OrderProcessor.java
@@ -9,7 +9,7 @@ import java.util.concurrent.atomic.AtomicInteger;
 @Slf4j
 public class OrderProcessor {
     private static final AtomicInteger TOTAL_PROCESS_ORDERS = new AtomicInteger();
-    private static final int ORDER_PROCESSING_TIME = 3000;
+    private static final int ORDER_PROCESSING_TIME_MS = 3000;
 
     public void processAllOrders(List<Order> orders) {
         List<CompletableFuture<Order>> features = orders.stream()
@@ -24,7 +24,7 @@ public class OrderProcessor {
     private Order processOrder(Order order) {
         try {
             log.info("Обработка заказа номер {}", order.getId());
-            Thread.sleep(ORDER_PROCESSING_TIME);
+            Thread.sleep(ORDER_PROCESSING_TIME_MS);
             log.info("Заказ номер {} обработан", order.getId());
             order.setStatus(Status.PROCESSED.getName());
             TOTAL_PROCESS_ORDERS.incrementAndGet();
