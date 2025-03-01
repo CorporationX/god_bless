4d6afc078974c4ec0314b1eb5ed4457c136b4b05
phtanya <112714509+phtanya@users.noreply.github.com>
2025-03-01 20:00:50 +0100

    BJS2-61670

--- src/main/java/school/faang/supercow/Boss.java
+++ src/main/java/school/faang/supercow/Boss.java
@@ -9,31 +9,25 @@ import org.slf4j.LoggerFactory;
 public class Boss {
     private final int maxPlayers;
     private int currentPlayers = 0;
-    private final Object lock = new Object();
     private static final Logger logger = LoggerFactory.getLogger(Boss.class);
 
-    public void joinBattle(Player player) {
-        synchronized (lock) {
-            while (currentPlayers >= maxPlayers) {
-                try {
-                    logger.info("{} waiting for a free spot...", player.getName());
-                    lock.wait();
-                } catch (InterruptedException e) {
-                    Thread.currentThread().interrupt();
-                    throw new RuntimeException(e);
-                }
+    public synchronized void joinBattle(Player player) {
+        while (currentPlayers >= maxPlayers) {
+            try {
+                logger.info("{} waiting for a free spot...", player.getName());
+                this.wait();
+            } catch (InterruptedException e) {
+                Thread.currentThread().interrupt();
+                throw new RuntimeException(e);
             }
-            currentPlayers++;
-            logger.info("{} joined the battle", player.getName());
         }
+        currentPlayers++;
+        logger.info("{} joined the battle", player.getName());
     }
 
-    public void leaveBattle(Player player) {
-        synchronized (lock) {
-            currentPlayers--;
-            logger.info("{} left the battle", player.getName());
-            lock.notify();
-        }
+    public synchronized void leaveBattle(Player player) {
+        currentPlayers--;
+        logger.info("{} left the battle", player.getName());
+        this.notify();
     }
-
 }
