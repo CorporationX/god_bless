20bca53e9345a19f29b7766b48eef79e4ccaaaa7
Artem Doronin <doronintema2018@yandex.ru>
2025-03-02 19:00:48 +0500

    fix bugs

--- src/main/java/school/faang/task_63094/Main.java
+++ src/main/java/school/faang/task_63094/Main.java
@@ -4,16 +4,18 @@ import lombok.extern.slf4j.Slf4j;
 
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
 
 @Slf4j
 public class Main {
-    private static final  int FACTOR = 100;
+    private static final int FACTOR = 100;
+    private static final int NUMBER_OF_DONATIONS = 10;
 
     public static void main(String[] args) {
         Organization organization = new Organization();
         ExecutorService service = Executors.newCachedThreadPool();
 
-        for (int i = 0; i < 10; i++) {
+        for (int i = 0; i < NUMBER_OF_DONATIONS; i++) {
             final int donationId = i;
             service.submit(() -> {
                 Donation donation = new Donation(donationId, Math.random() * FACTOR);
@@ -21,8 +23,17 @@ public class Main {
             });
         }
         service.shutdown();
-        while (!service.isTerminated()) {
+        try {
+
+            if (!service.awaitTermination(5, TimeUnit.SECONDS)) {
+                service.shutdownNow();
+                System.out.println("Задачи не завершились в течение 5 секунд, принудительное завершение.");
+            }
+        } catch (InterruptedException e) {
+            service.shutdownNow();
+            Thread.currentThread().interrupt();
         }
+
 
         log.info("Итоговый баланс организации: " + organization.getTotalBalance());
 
--- src/main/java/school/faang/task_63094/Organization.java
+++ src/main/java/school/faang/task_63094/Organization.java
@@ -3,6 +3,8 @@ package school.faang.task_63094;
 import lombok.Getter;
 import lombok.extern.slf4j.Slf4j;
 
+import java.util.Collection;
+import java.util.Collections;
 import java.util.HashSet;
 import java.util.Set;
 import java.util.concurrent.atomic.AtomicReference;
@@ -11,12 +13,12 @@ import java.util.concurrent.atomic.AtomicReference;
 public class Organization {
     @Getter
     private final AtomicReference<Double> totalBalance = new AtomicReference<>(0.0);
-    private final Set<Integer> setInt = new HashSet<>();
+    private final Set<Integer> donationRegistr = Collections.synchronizedSet(new HashSet<>());
 
     public void addDonation(Donation donation) {
-        if (!setInt.contains(donation.id())) {
+        if (!donationRegistr.contains(donation.id())) {
             totalBalance.updateAndGet(totalBalance -> totalBalance + donation.amount());
-            setInt.add(donation.id());
+            donationRegistr.add(donation.id());
             log.info("пожертвование принято");
         } else {
             log.info("запись одного и того же пожертвования запрещена");
