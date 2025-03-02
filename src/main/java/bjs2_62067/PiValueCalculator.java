2da4bba614a4668c0bd329f89c81b2d3d9e51150
KAS1990 <34664788+KAS1990@users.noreply.github.com>
2025-03-02 16:23:50 +0300

    fix: Threads are created by ExecutorService to limit thread count

--- src/main/java/bjs2_62067/PiValueCalculator.java
+++ src/main/java/bjs2_62067/PiValueCalculator.java
@@ -5,16 +5,23 @@ import lombok.extern.slf4j.Slf4j;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.Executors;
 import java.util.concurrent.ThreadLocalRandom;
+import java.util.concurrent.TimeUnit;
 
 @Slf4j
 public class PiValueCalculator {
+    public static final int THREAD_POOL_SIZE = Runtime.getRuntime().availableProcessors() + 1;
+    public static final int WAITING_TERMINATION_TIMEOUT_SECONDS = 1;
+
     public static double calculatePi(int n) {
+        var executor = Executors.newFixedThreadPool(THREAD_POOL_SIZE);
         List<CompletableFuture<Boolean>> futures = new ArrayList<>(n);
 
         for (int i = 0; i < n; i++) {
-            futures.add(CompletableFuture.supplyAsync(PiValueCalculator::generatePoint));
+            futures.add(CompletableFuture.supplyAsync(PiValueCalculator::generatePoint, executor));
         }
+        executor.shutdown();
 
         CompletableFuture.allOf(futures.toArray(new CompletableFuture[0])).join();
 
@@ -22,6 +29,17 @@ public class PiValueCalculator {
                 .map(CompletableFuture::join)
                 .filter(isInside -> isInside)
                 .count();
+
+        try {
+            if (!executor.awaitTermination(WAITING_TERMINATION_TIMEOUT_SECONDS, TimeUnit.SECONDS)) {
+                executor.shutdownNow();
+                log.info("Не дождались завершения потоков за заданное время");
+            }
+        } catch (InterruptedException e) {
+            Thread.currentThread().interrupt();
+            executor.shutdownNow();
+            log.info("Не дождались завершения потоков за заданное время: {}", e.getMessage(), e);
+        }
 
         log.info("Внутри точек {} из {}", insideCount, n);
 
