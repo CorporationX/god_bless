014fbf3fc06ead014c4d0c8fd5b735ab0c300b3b
alex198 <abykovskih@gmail.com>
2025-03-02 17:28:25 +0300

    BJS2-62590 created classes Potion and PotionGathering. Implemented methods using CompletableFuture

---
+++ src/main/java/school/faang/bjs2_62590/PotionGathering.java
@@ -0,0 +1,32 @@
+package school.faang.bjs2_62590;
+
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+import java.util.concurrent.CompletableFuture;
+
+@NoArgsConstructor
+public class PotionGathering {
+
+    private static final int ONE_SECOND_IN_MS = 1000;
+
+    public CompletableFuture<Integer> gatherIngredients(Potion potion) {
+        return CompletableFuture.supplyAsync(() -> {
+            try {
+                Thread.sleep((long) potion.getRequiredIngredients() * ONE_SECOND_IN_MS);
+                System.out.printf("Сбор ингредиентов для зелья - %s завершен!\n", potion.getName());
+            } catch (InterruptedException e) {
+                System.out.println("Ошибка: " + e.getMessage());
+                throw new RuntimeException(e);
+            }
+            return potion.getRequiredIngredients();
+        });
+    }
+
+    public List<CompletableFuture<Integer>> gatherAllIngredients(List<Potion> potions) {
+        return potions.stream()
+                .map(this::gatherIngredients)
+                .toList();
+    }
+
+}
