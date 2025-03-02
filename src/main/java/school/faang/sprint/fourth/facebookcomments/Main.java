b0764e51ab31fa5d44cba5b3a9e4e7da861209dc
vtroshin <moron19666@yandex.ru>
2025-03-02 12:38:30 +0300

    BJS2-62615 Доработки

--- src/main/java/school/faang/sprint/fourth/facebookcomments/Main.java
+++ src/main/java/school/faang/sprint/fourth/facebookcomments/Main.java
@@ -5,7 +5,6 @@ import lombok.extern.slf4j.Slf4j;
 import java.sql.Timestamp;
 import java.util.Date;
 import java.util.Optional;
-import java.util.concurrent.ExecutionException;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.TimeUnit;
@@ -13,18 +12,21 @@ import java.util.concurrent.TimeUnit;
 @Slf4j
 public class Main {
     private static final int AWAIT_TIME = 20;
+    private static final int THREAD_POOL_SIZE = 10;
+    private static final int POSTS_COUNT = 10;
+    private static final int COMMENTS_COUNT = 5;
 
-    public static void main(String[] args) throws ExecutionException, InterruptedException {
-        ExecutorService executorService = Executors.newFixedThreadPool(10);
+    public static void main(String[] args) {
+        ExecutorService executorService = Executors.newFixedThreadPool(THREAD_POOL_SIZE);
 
         PostService postService = new PostService();
-        for (int i = 0; i < 10; i++) {
+        for (int i = 0; i < POSTS_COUNT; i++) {
             int finalI = i;
             executorService.submit(() -> {
                 Post post = new Post(finalI, "Some post " + finalI, "Post body " + finalI, "Post author " + finalI);
                 postService.addPost(post);
 
-                for (int j = 1; j <= 5; j++) {
+                for (int j = 1; j <= COMMENTS_COUNT; j++) {
                     Comment comment =
                             new Comment("Some comment", "Comment author" + j, new Timestamp(new Date().getTime()));
                     postService.addComment(1, comment);
