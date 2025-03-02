b1643969803fd2fe68f093a3558d17947716af7d
Dmitry Rogulkin <rogulkind@yandex.ru>
2025-03-02 17:34:40 +0500

    https://faang-school.atlassian.net/browse/BJS2-62113

--- src/main/java/school/faang/task62113/service/Order.java
+++ src/main/java/school/faang/task62113/service/Order.java
@@ -1,4 +1,11 @@
 package school.faang.task62113.service;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
 public class Order {
+    private int id;
+    private String status;
 }
