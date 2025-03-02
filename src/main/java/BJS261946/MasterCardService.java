2d72e2e1e89a5ff4cb593f09c0d72d7b186497df
Ilya Yanchenko <zsdfghjkl111@gmail.com>
2025-03-02 14:10:20 +0700

    BJS2-61946 Fix

--- src/main/java/BJS261946/MasterCardService.java
+++ src/main/java/BJS261946/MasterCardService.java
@@ -8,12 +8,15 @@ import java.util.concurrent.*;
 public class MasterCardService {
     private static final int TEN_SECONDS_IN_MS = 10_000;
     private static final int ONE_SECOND_IN_MS = 1_000;
+    private static final int FIVE_SECONDS_IN_MS = 5_000;
+    private static final int SEVENTEEN_SECONDS_IN_MS = 17_000;
 
     public int collectPayment() {
         try {
             Thread.sleep(TEN_SECONDS_IN_MS);
-            return 5_000;
+            return FIVE_SECONDS_IN_MS;
         } catch (InterruptedException e) {
+            log.error("We have interrupted while waiting for payment to be collected", e);
             Thread.currentThread().interrupt();
             throw new RuntimeException(e);
         }
@@ -22,7 +25,7 @@ public class MasterCardService {
     public int sendAnalytics() {
         try {
             Thread.sleep(ONE_SECOND_IN_MS);
-            return 17_000;
+            return SEVENTEEN_SECONDS_IN_MS;
         } catch (InterruptedException e) {
             Thread.currentThread().interrupt();
             throw new RuntimeException(e);
@@ -32,18 +35,18 @@ public class MasterCardService {
     public void doAll() {
         ExecutorService executor = Executors.newSingleThreadExecutor();
         Future<Integer> paymentfuture = executor.submit(this::collectPayment);
-        CompletableFuture<Integer> analyticsFuture = CompletableFuture.supplyAsync(()
-                -> sendAnalytics(), executor);
+        CompletableFuture<Integer> analyticsFuture = CompletableFuture.supplyAsync(this::sendAnalytics, executor);
         try {
-            Integer paymentResult = analyticsFuture.join();
             Integer analyticsResult = paymentfuture.get();
-            executor.shutdown();
+            Integer paymentResult = analyticsFuture.join();
             System.out.println("Analytic  result: " + analyticsResult);
             System.out.println("Payment result: " + paymentResult);
         } catch (ExecutionException | InterruptedException e) {
             log.error("Error in method do All", e);
             Thread.currentThread().interrupt();
             throw new RuntimeException(e);
+        } finally {
+            executor.shutdown();
         }
 
     }
