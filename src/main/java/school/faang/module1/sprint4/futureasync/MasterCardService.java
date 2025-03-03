0b78d6ca08004b1525c84cb354e4edab3b9ceddd
KhafizovaIlona <anolihazz@gmail.com>
2025-03-03 18:06:16 +0300

    Асинхронность и Future (корректировки)

--- src/main/java/school/faang/module1/sprint4/futureasync/MasterCardService.java
+++ src/main/java/school/faang/module1/sprint4/futureasync/MasterCardService.java
@@ -11,17 +11,19 @@ public class MasterCardService {
     private static final int AWAIT_MIN = 1;
 
     static int collectPayment() {
+        log.info("Процесс проведение оплаты запущен");
         try {
             Thread.sleep(TEN_SECONDS_IN_MS);
             return 5_000;
         } catch (InterruptedException e) {
-            log.debug("Поток был прерван во время процесса оплаты");
+            log.debug("Поток был прерван во время процесса оплаты", e);
             Thread.currentThread().interrupt();
-            throw new RuntimeException(e);
+            throw new RuntimeException("Поток был прерван во время процесса оплаты", e);
         }
     }
 
     static int sendAnalytics() {
+        log.info("Процесс отправки аналитики запущен");
         try {
             Thread.sleep(ONE_SECOND_IN_MS);
             return 17_000;
@@ -39,7 +41,8 @@ public class MasterCardService {
         CompletableFuture<Integer> resultAnalytics = CompletableFuture.supplyAsync(MasterCardService::sendAnalytics);
 
         try {
-            log.info("\nАналитика отправлена: {}\nПлатеж выполнен: {}", resultAnalytics.get(), resultPayment.get());
+            log.info("\nАналитика отправлена: {}\nПлатеж выполнен: {}",
+                    resultAnalytics.get(1, TimeUnit.MINUTES), resultPayment.get(1, TimeUnit.MINUTES));
         } catch (InterruptedException e) {
             log.debug("Поток был прерван");
             Thread.currentThread().interrupt();
@@ -47,8 +50,15 @@ public class MasterCardService {
         } catch (ExecutionException e) {
             log.debug("Ошибка при выполнении");
             throw new RuntimeException("Ошибка при выполнении", e);
+        } catch (TimeoutException e) {
+            log.debug("Ошибка при ожидании получения результата");
+            throw new RuntimeException("Ошибка при ожидании получения результата", e);
         }
 
+        shutdownExecutor(executorService);
+    }
+
+    private void shutdownExecutor(ExecutorService executorService) {
         executorService.shutdown();
 
         try {
