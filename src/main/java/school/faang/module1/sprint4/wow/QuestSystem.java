b832c35d6f4b751291b1955f2f4ed97c03970297
KhafizovaIlona <anolihazz@gmail.com>
2025-03-03 18:22:38 +0300

    WOW (корректировки)

--- src/main/java/school/faang/module1/sprint4/wow/QuestSystem.java
+++ src/main/java/school/faang/module1/sprint4/wow/QuestSystem.java
@@ -6,6 +6,7 @@ import java.util.concurrent.*;
 
 @Slf4j
 public class QuestSystem {
+    private final Object lock = new Object();
 
     public CompletableFuture<Player> startQuest(Player player, Quest quest) {
         try {
@@ -13,17 +14,20 @@ public class QuestSystem {
         } catch (InterruptedException e) {
             log.debug("Поток был прерван во время ожидания");
             Thread.currentThread().interrupt();
-            throw new RuntimeException(e);
+            throw new RuntimeException("Поток был прерван во время ожидания", e);
         }
+
         CompletableFuture.supplyAsync(() -> player)
                 .thenAccept(player1 -> {
-                    int level = player1.getLevel();
-                    player1.setLevel(++level);
-                    log.info("Уровень игрока {} повышен, квест {} пройден", player1.getName(), quest.name());
+                    synchronized (lock) {
+                        int level = player1.getLevel();
+                        player1.setLevel(++level);
+                        log.info("Уровень игрока {} повышен, квест {} пройден", player1.getName(), quest.name());
 
-                    int experience = player1.getExperience() + quest.reward();
-                    player1.setExperience(experience);
-                    log.info("Опыт игрока {} составляет {}", player1.getName(), player1.getExperience());
+                        int experience = player1.getExperience() + quest.reward();
+                        player1.setExperience(experience);
+                        log.info("Опыт игрока {} составляет {}", player1.getName(), player1.getExperience());
+                    }
                 })
                 .exceptionally(ex -> {
                     log.debug("Вовремя выполнения квеста возникла ошибка", ex);
