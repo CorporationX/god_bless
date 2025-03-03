35b6deaf33d964b9fac921d024c364e506825832
Артемий <xkmitx@gmail.com>
2025-03-04 00:09:01 +0300

    Task 62093 fix

--- src/main/java/school/faang/task_62093/MasterCardService.java
+++ src/main/java/school/faang/task_62093/MasterCardService.java
@@ -1,36 +1,41 @@
 package school.faang.task_62093;
 
+import lombok.extern.slf4j.Slf4j;
+
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.Future;
+import java.util.concurrent.TimeUnit;
 
+@Slf4j
 public class MasterCardService {
     private static final int TEN_SECONDS_IN_MS = 10_000;
     private static final int ONE_SECOND_IN_MS = 1_000;
+    private static final int TIMEOUT = 30;
 
     public int collectPayment(int sum) {
-        System.out.println("Ожидаем ответ от сервера для платежа...");
+        log.info("Ожидаем ответ от сервера для платежа...");
         try {
             Thread.sleep(TEN_SECONDS_IN_MS);
             System.out.println("Оплата прошла успешно!");
             return sum;
         } catch (InterruptedException e) {
-            System.out.println("Ошибка оплаты, поток прерван! " + e);
+            log.info("Ошибка оплаты, поток прерван! " + e);
             Thread.currentThread().interrupt();
             throw new RuntimeException(e);
         }
     }
 
     public int sendAnalytics(int data) {
-        System.out.println("Ожидаем ответ от сервера для аналитики...");
+        log.info("Ожидаем ответ от сервера для аналитики...");
         try {
             Thread.sleep(ONE_SECOND_IN_MS);
             System.out.println("Аналитика отправлена успешна!");
             return data;
         } catch (InterruptedException e) {
-            System.out.println("Ошибка отправки, поток прерван! " + e);
+            log.info("Ошибка отправки, поток прерван! " + e);
             Thread.currentThread().interrupt();
             throw new RuntimeException(e);
         }
@@ -45,10 +50,20 @@ public class MasterCardService {
             Integer paymentResult = paymentFuture.get();
             System.out.printf("Платеж выполнен: %d%n ", paymentResult);
         } catch (InterruptedException | ExecutionException e) {
-            System.out.println("Ошибка выполнения задачи: " + e);
+            log.info("Ошибка выполнения задачи: " + e);
             Thread.currentThread().interrupt();
         } finally {
             executorService.shutdown();
+            try {
+                if (!executorService.awaitTermination(TIMEOUT, TimeUnit.SECONDS)) {
+                    log.info("Не все потоки завершились, принудительное завершение");
+                    executorService.shutdownNow();
+                }
+            } catch (InterruptedException e) {
+                log.info("Поток прерван" + e);
+            } finally {
+                executorService.shutdownNow();
+            }
         }
     }
 }
