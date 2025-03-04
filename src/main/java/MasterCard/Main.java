519bb21c3e2c9c3e7e53f8bd0050fe9bd743e499
Denis Popov <popoffdn@mail.ru>
2025-03-04 21:12:57 +0300

    ревью BJS2-62205

--- src/main/java/MasterCard/Main.java
+++ src/main/java/MasterCard/Main.java
@@ -3,6 +3,6 @@ package MasterCard;
 public class Main {
     public static void main(String[] args) {
         MasterCardService service = new MasterCardService();
-        service.doAll();
+        service.processPaymentAndAnalytics();
     }
 }
--- src/main/java/MasterCard/MasterCardService.java
+++ src/main/java/MasterCard/MasterCardService.java
@@ -5,17 +5,25 @@ import java.util.concurrent.ExecutionException;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.Future;
+import java.util.concurrent.TimeUnit;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 public class MasterCardService {
     private static final int TEN_SECONDS_IN_MS = 10_000;
     private static final int ONE_SECOND_IN_MS = 1_000;
+    private static final int PAYMENT_AMOUNT = 5_000;
+    private static final int ANALYTICS_RESULT = 17_000;
+    private static final Logger logger = Logger.getLogger(MasterCardService.class.getName());
+
 
     static int collectPayment() {
         try {
             Thread.sleep(TEN_SECONDS_IN_MS);
-            return 5_000;
+            return PAYMENT_AMOUNT;
         } catch (InterruptedException e) {
             Thread.currentThread().interrupt();
+            logger.log(Level.SEVERE, "Ошибка во время сбора платежа", e);
             throw new RuntimeException(e);
         }
     }
@@ -23,14 +31,14 @@ public class MasterCardService {
     static int sendAnalytics() {
         try {
             Thread.sleep(ONE_SECOND_IN_MS);
-            return 17_000;
+            return ANALYTICS_RESULT;
         } catch (InterruptedException e) {
             Thread.currentThread().interrupt();
             throw new RuntimeException(e);
         }
     }
 
-    public void doAll() {
+    public void processPaymentAndAnalytics() {
         ExecutorService executor = Executors.newSingleThreadExecutor();
         Future<Integer> paymentFuture = executor.submit(MasterCardService::collectPayment);
         CompletableFuture<Integer> analyticsFuture = CompletableFuture.supplyAsync(MasterCardService::sendAnalytics);
@@ -45,6 +53,17 @@ public class MasterCardService {
             Thread.currentThread().interrupt();
         } finally {
             executor.shutdown();
+            try {
+                if (!executor.awaitTermination(5, TimeUnit.SECONDS)) {
+                    System.out.println("Принудительное завершение задач");
+                    executor.shutdownNow();
+                }
+            } catch (InterruptedException e) {
+                System.out.println("Ожидание завершения было прервано: " + e.getMessage());
+                executor.shutdownNow();
+                Thread.currentThread().interrupt();
+                // что то мне кажется я тут нагородил
+            }
         }
     }
 }
