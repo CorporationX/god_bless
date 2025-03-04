aa3eadf8aa9e45c5dc2e66e11db021ce7f43ee89
Tony King <a.korolev9927@gmail.com>
2025-03-04 22:07:25 +0300

    Сделал отдельный класс PaymentProcessingException

--- src/main/java/school/faang/task_61998/Main.java
+++ src/main/java/school/faang/task_61998/Main.java
@@ -1,7 +1,7 @@
 package school.faang.task_61998;
 
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws PaymentProcessingException {
         MasterCardService masterCardService = new MasterCardService();
         masterCardService.doAll();
     }
--- src/main/java/school/faang/task_61998/MasterCardService.java
+++ src/main/java/school/faang/task_61998/MasterCardService.java
@@ -12,37 +12,55 @@ import java.util.concurrent.Future;
 public class MasterCardService {
     private static final int TEN_SECONDS_IN_MS = 10_000;
     private static final int ONE_SECOND_IN_MS = 1_000;
+    private static final int ONE_PRICE = 5_000;
+    private static final int TWO_PRICE = 17_000;
 
-    static int collectPayment() {
+    static int collectPayment() throws PaymentProcessingException {
         try {
             Thread.sleep(TEN_SECONDS_IN_MS);
-            return 5_000;
+            return ONE_PRICE;
         } catch (InterruptedException e) {
             Thread.currentThread().interrupt();
-            throw new RuntimeException(e);
+            throw new PaymentProcessingException("Ошибка при сборе платежа", e);
         }
     }
 
-    static int sendAnalytics() {
+    static int sendAnalytics() throws PaymentProcessingException {
         try {
             Thread.sleep(ONE_SECOND_IN_MS);
-            return 17_000;
+            return TWO_PRICE;
         } catch (InterruptedException e) {
             Thread.currentThread().interrupt();
-            throw new RuntimeException(e);
+            throw new PaymentProcessingException("Ошибка при отправке аналитики", e);
         }
     }
 
-    public void doAll() {
+    public void doAll() throws PaymentProcessingException {
         ExecutorService executor = Executors.newSingleThreadExecutor();
-        Future<Integer> futureCollectPayment = executor.submit(MasterCardService::collectPayment);
+        Future<Integer> futureCollectPayment = executor.submit(() -> {
+            try {
+                return collectPayment();
+            } catch (PaymentProcessingException e) {
+                log.error("Ошибка при обработке платежа: {}", e.getMessage());
+                return 0;
+            }
+        });
+
         CompletableFuture<Integer> futureSendAnalytics = CompletableFuture
-                .supplyAsync(MasterCardService::sendAnalytics);
+                .supplyAsync(() -> {
+                    try {
+                        return sendAnalytics();
+                    } catch (PaymentProcessingException e) {
+                        log.error("Ошибка при отправке аналитики: {}", e.getMessage());
+                        return 0;
+                    }
+                });
+
         log.info("Произведена отправка аналитики: {}", futureSendAnalytics.join());
         try {
             log.info("Платеж: {}", futureCollectPayment.get());
         } catch (InterruptedException | ExecutionException e) {
-            throw new RuntimeException(e);
+            throw new PaymentProcessingException("Ошибка при получении результата платежа", e);
         }
         executor.shutdown();
     }
