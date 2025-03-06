7ba121b856d8109baeaafe8c1bbfd849e2bd9ce0
Gulnaz <gumerova.gulnaz2014@yandex.ru>
2025-03-06 14:38:53 +0300

    Задача BJS2-62214 выполнена

---
+++ src/main/java/school/faang/BJS2_62214/Player.java
@@ -0,0 +1,16 @@
+package school.faang.BJS2_62214;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class Player {
+    private String name;
+    private int level;
+    private int experience;
+
+    public void addExperience(int experience) {
+        this.experience += experience;
+    }
+}
