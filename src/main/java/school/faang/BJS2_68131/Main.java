a621ee81d7ad5660c741cfd5523c5314d956af73
Nikita Nikitin <nnandpartners@gmail.com>
2025-04-06 22:10:44 +0300

    BJS2-68100 Done

---
+++ src/main/java/school/faang/BJS2_68131/Main.java
@@ -0,0 +1,11 @@
+package school.faang.BJS2_68131;
+
+public class Main {
+    public static void main(String[] args) {
+        User correctUser = new User("Mike",19,"Google","London");
+        System.out.println(correctUser);
+
+        User incorrectUser = new User("Mike",5,"Roga&Kopyta","Hell");
+        System.out.println(incorrectUser);
+    }
+}
---
+++ src/main/java/school/faang/BJS2_68131/User.java
@@ -0,0 +1,36 @@
+package school.faang.BJS2_68131;
+
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+import java.util.Set;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@EqualsAndHashCode
+@ToString
+public class User {
+
+    private static final Set<String> VALID_JOBS = Set.of( "Google", "Uber", "Amazon");
+    private static final Set<String> VALID_ADDRESSES = Set.of( "London", "New York", "Amsterdam");
+
+    private String name;
+    private int age;
+    private String job;
+    private String address;
+
+    public User(String name, int age, String job, String address) {
+        this.name = name;
+        this.age = age;
+        this.job = job;
+        this.address = address;
+
+        if(!VALID_ADDRESSES.contains(address) || !VALID_JOBS.contains(job) || age<18) {
+            throw new IllegalArgumentException("Incorrect user values");
+        }
+    }
+}
