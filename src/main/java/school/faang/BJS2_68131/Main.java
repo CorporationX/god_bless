d42f5bcc1e6ad0f5e22e8b390e68fde4a1e4ab02
Nikita Nikitin <nnandpartners@gmail.com>
2025-04-06 22:47:09 +0300

    BJS2-68100 Fix codestyle according to plugin

--- src/main/java/school/faang/BJS2_68131/Main.java
+++ src/main/java/school/faang/BJS2_68131/Main.java
@@ -2,10 +2,10 @@ package school.faang.BJS2_68131;
 
 public class Main {
     public static void main(String[] args) {
-        User correctUser = new User("Mike",19,"Google","London");
+        User correctUser = new User("Mike", 19, "Google", "London");
         System.out.println(correctUser);
 
-        User incorrectUser = new User("Mike",5,"Roga&Kopyta","Hell");
+        User incorrectUser = new User("Mike", 5, "Roga&Kopyta", "Hell");
         System.out.println(incorrectUser);
     }
 }
