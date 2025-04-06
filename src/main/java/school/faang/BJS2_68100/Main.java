d5b376e7f5b05fee4e2c3262d46de165f7a3e40f
Nikita Nikitin <nnandpartners@gmail.com>
2025-04-06 23:50:29 +0300

    BJS2-68086 Add List.of() call and add space to separate logic

--- src/main/java/school/faang/BJS2_68100/Main.java
+++ src/main/java/school/faang/BJS2_68100/Main.java
@@ -5,15 +5,17 @@ import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
-        List<User> users = new ArrayList<>();
-        users.add(new User("Nick", 22, "T", "Tralala str, 23"));
-        users.add(new User("Sam", 23, "VTB", "Tralala str, 23"));
-        users.add(new User("Frodo", 22, "SBER", "Tralala str, 24"));
-        users.add(new User("Fulgrim", 24, "T", "Tralala str, 24"));
-        users.add(new User("Angron", 24, "T", "Tralala str, 25"));
-        users.add(new User("Perturabo", 25, "SBER", "Tralala str, 25"));
-        users.add(new User("Fedya", 25, "VTB", "Tralala str, 26"));
-        users.add(new User("Felix", 22, "Alpha", "Tralala str, 26"));
+        List<User> users = List.of(
+                new User("Nick", 22, "T", "Tralala str, 23"),
+                new User("Sam", 23, "VTB", "Tralala str, 23"),
+                new User("Frodo", 22, "SBER", "Tralala str, 24"),
+                new User("Fulgrim", 24, "T", "Tralala str, 24"),
+                new User("Angron", 24, "T", "Tralala str, 25"),
+                new User("Perturabo", 25, "SBER", "Tralala str, 25"),
+                new User("Fedya", 25, "VTB", "Tralala str, 26"),
+                new User("Felix", 22, "Alpha", "Tralala str, 26")
+        );
+
         System.out.println(User.groupUsers(users));
     }
 }
