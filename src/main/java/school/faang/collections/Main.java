50bf496e347b77210be3059e94ee39b540556724
Our_bundle_of_joy <76647473+Daniyal-Akhadov@users.noreply.github.com>
2025-04-07 18:26:42 +0300

    resolve issues after review

--- src/main/java/school/faang/collections/Main.java
+++ src/main/java/school/faang/collections/Main.java
@@ -1,6 +1,5 @@
-package school.faang;
+package school.faang.collections;
 
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
--- src/main/java/school/faang/collections/User.java
+++ src/main/java/school/faang/collections/User.java
@@ -1,4 +1,4 @@
-package school.faang;
+package school.faang.collections;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
