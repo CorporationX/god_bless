5df40142ee61beea7e1931b1c7f29c26c6989c0f
Our_bundle_of_joy <76647473+Daniyal-Akhadov@users.noreply.github.com>
2025-04-08 00:26:21 +0300

    resolve review problems. version - 1

--- src/main/java/school/faang/abstraction/entitiy/Creature.java
+++ src/main/java/school/faang/abstraction/entitiy/Creature.java
@@ -1,5 +1,8 @@
 package school.faang.abstraction.entitiy;
 
+import lombok.Getter;
+
+@Getter
 public abstract class Creature {
     private static final int DEFAULT_HEALTH = 100;
     private static final int AVERAGE_PROPERTIES_VALUE = 5;
@@ -10,7 +13,6 @@ public abstract class Creature {
     private final int intelligence;
 
     private int health;
-    private boolean isAlive;
 
     public Creature(String name, int strength, int agility, int intelligence) {
         this.name = name;
@@ -18,7 +20,6 @@ public abstract class Creature {
         this.agility = agility;
         this.intelligence = intelligence;
         this.health = DEFAULT_HEALTH;
-        this.isAlive = true;
     }
 
     public Creature(String name) {
@@ -27,21 +28,20 @@ public abstract class Creature {
         this.agility = AVERAGE_PROPERTIES_VALUE;
         this.intelligence = AVERAGE_PROPERTIES_VALUE;
         this.health = DEFAULT_HEALTH;
-        this.isAlive = true;
     }
 
     public abstract void attack(Creature target);
 
     public void takeDamage(int damage) {
-        this.health -= damage;
-
-        if (this.health <= 0 && !this.isAlive) {
+        if (this.health <= 0) {
             die();
+            return;
         }
+
+        this.health -= damage;
     }
 
     private void die() {
         System.out.println("Creature died");
-        isAlive = false;
     }
 }
