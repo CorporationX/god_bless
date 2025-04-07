5bc504bc887b6592ad95f366f63acc59fd7d501b
sm <sm@servpoint.org>
2025-04-07 20:05:10 +0300

    решение задачи BJS2-68170

---
+++ src/main/java/school/faang/bjs2_68170/Main.java
@@ -0,0 +1,26 @@
+package school.faang.bjs2_68170;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) {
+        List<User> usersList = new ArrayList<>(
+                List.of(new User("Ivan", 23, "home", "Moscow"),
+                        new User("Petr", 32, "office", "St.Peterburg"),
+                        new User("Oleg", 24, "office", "Orel"),
+                        new User("Alex", 32, "home", "Tver"),
+                        new User("Pavel", 30, "office", "Tula"),
+                        new User("Anna", 24, "office", "Kursk")
+                ));
+
+        for (var ageGroup : User.groupUsers(usersList).entrySet()) {
+            System.out.println(ageGroup.getKey());
+            for (User user : ageGroup.getValue()) {
+                System.out.println(user.toString());
+            }
+            System.out.println();
+        }
+    }
+}
+
---
+++ src/main/java/school/faang/bjs2_68170/User.java
@@ -0,0 +1,24 @@
+package school.faang.bjs2_68170;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.ToString;
+
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+@Getter
+@AllArgsConstructor
+@ToString
+public class User {
+    private String name;
+    private Integer age;
+    private String workplace;
+    private String address;
+
+    public static Map<Integer, List<User>> groupUsers(List<User> userList) {
+        return userList.stream().collect(
+                Collectors.groupingBy(User::getAge));
+    }
+}
