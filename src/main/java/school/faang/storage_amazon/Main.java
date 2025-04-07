0488546699c46b54d6e791b9bb289282fe030b0f
BogatyrevDL <d.l.bogatyrev@gmail.com>
2025-04-07 13:12:06 +0300

    BJS2-68654: style changed

--- src/main/java/school/faang/storage_amazon/Main.java
+++ src/main/java/school/faang/storage_amazon/Main.java
@@ -6,37 +6,38 @@ import java.util.stream.Collectors;
 
 public class Main {
     public static void main(String[] args) {
-        ProductManager pManager = new ProductManager();
+        ProductManager productManager = new ProductManager();
 
-        pManager.addProduct(Category.FOOD, "Orange");
-        pManager.addProduct(Category.FOOD, "Apple");
-        pManager.addProduct(Category.FOOD, "Bread");
-        pManager.addProduct(Category.ELECTRONICS, "PC");
-        pManager.addProduct(Category.ELECTRONICS, "Smartphone");
-        pManager.addProduct(Category.CLOTHING, "T-Shirt");
-        pManager.addProduct(Category.OTHER, "Car");
+        productManager.addProduct(Category.FOOD, "Orange");
+        productManager.addProduct(Category.FOOD, "Apple");
+        productManager.addProduct(Category.FOOD, "Bread");
+        productManager.addProduct(Category.ELECTRONICS, "PC");
+        productManager.addProduct(Category.ELECTRONICS, "Smartphone");
+        productManager.addProduct(Category.CLOTHING, "T-Shirt");
+        productManager.addProduct(Category.OTHER, "Car");
 
         System.out.println("\nВсе продукты:");
-        pManager.printAllProducts();
+        productManager.printAllProducts();
 
-        pManager.removeProduct(Category.FOOD, "Apple");
-        pManager.removeProduct(Category.FOOD, "Tomato");
-        pManager.removeProduct(Category.CLOTHING, "T-Shirt");
+        productManager.removeProduct(Category.FOOD, "Apple");
+        productManager.removeProduct(Category.FOOD, "Tomato");
+        productManager.removeProduct(Category.CLOTHING, "T-Shirt");
 
         System.out.println("\nПродукты в категории FOOD:");
-        List<Product> products = pManager.findProductsByCategory(Category.FOOD);
+        List<Product> products = productManager.findProductsByCategory(Category.FOOD);
         products.forEach(System.out::println);
         System.out.println("\nПродукты в категории CLOTHING:");
-        products = pManager.findProductsByCategory(Category.CLOTHING);
+        products = productManager.findProductsByCategory(Category.CLOTHING);
         products.forEach(System.out::println);
 
         System.out.println("\nГруппировка продуктов по категориям:");
-        Map<Category, List<Product>> productsByCategory = pManager.groupProductsByCategory();
+        Map<Category, List<Product>> productsByCategory = productManager.groupProductsByCategory();
         productsByCategory.forEach(
-                (k, v) -> System.out.printf("%s: %s\n", k, v.stream().map(Product::getName).collect(Collectors.joining(", ")))
+                (k, v) -> System.out.printf("%s: %s\n",
+                        k, v.stream().map(Product::getName).collect(Collectors.joining(", ")))
         );
 
         System.out.println("\nФинальный список продуктов:");
-        pManager.printAllProducts();
+        productManager.printAllProducts();
     }
 }
