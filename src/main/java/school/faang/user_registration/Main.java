6505f3bd5900b9fdb251cce5d30791342b012409
Vladimir Ekgardt <dj.estet@yandex.ru>
2025-04-07 17:28:58 +0300

    Регистрация пользователя BJS2-68144

---
+++ src/main/java/school/faang/user_registration/Main.java
@@ -0,0 +1,8 @@
+package school.faang.user_registration;
+
+public class Main {
+    public static void main(String[] args) {
+        User user = new User("John11", 19, "Google", "New York");
+        User user1 = new User("John11", 17, "Google", "New York");
+    }
+}
---
+++ src/main/java/school/faang/user_registration/User.java
@@ -0,0 +1,27 @@
+package school.faang.user_registration;
+
+import lombok.Data;
+
+import java.util.Set;
+
+@Data
+public class User {
+    private String name;
+    private int age;
+    private String job;
+    private String address;
+
+    final static Set<String> VALID_JOBS = Set.of("Google", "User", "Amazon");
+    final static Set<String> VALID_ADDRESSES = Set.of("London", "New York", "Amsterdam");
+
+    public User(String name, int age, String job, String address) {
+        if (name!=null && age>=18 && VALID_JOBS.contains(job) && VALID_ADDRESSES.contains(address)) {
+            this.name = name;
+            this.age = age;
+            this.job = job;
+            this.address = address;
+        }
+        else
+            throw new IllegalArgumentException("Exception: Illegal argument");
+    }
+}
