608f2743404474eb9b453f81fc96022901b0c49e
psvm500 <faribo.gafur@gmail.com>
2025-04-07 21:04:02 +0300

    BJS2-68150 Регистрация пользователя

---
+++ src/main/java/school/faang/bjs2_68150/Main.java
@@ -0,0 +1,7 @@
+package school.faang.bjs2_68150;
+
+public class Main {
+    public static void main(String[] args) {
+        User user = new User("Ivan Ivanov", 18, "Google", "london");
+    }
+}
---
+++ src/main/java/school/faang/bjs2_68150/User.java
@@ -0,0 +1,33 @@
+package school.faang.bjs2_68150;
+
+import java.util.Set;
+
+public class User {
+    private static final Set<String> VALID_JOBS;
+    private static final Set<String> VALID_ADDRESSES;
+
+    static {
+        VALID_JOBS = Set.of("Google".toLowerCase(), "Uber".toLowerCase(), "Amazon".toLowerCase());
+        VALID_ADDRESSES = Set.of("London".toLowerCase(), "New York".toLowerCase(), "Amsterdam".toLowerCase());
+    }
+
+    private String name;
+    private int age;
+    private String job;
+    private String address;
+
+    public User(String name, int age, String job, String address) {
+        if (name != null) {
+            this.name = name;
+        } else throw new IllegalArgumentException("Введите имя");
+        if (age >= 18) {
+            this.age = age;
+        } else throw new IllegalArgumentException("Недопустимый возраст для регистрации");
+        if (VALID_JOBS.contains(job.toLowerCase())) {
+            this.job = job;
+        } else throw new IllegalArgumentException("Недопустимое место работы");
+        if (VALID_ADDRESSES.contains(address.toLowerCase())) {
+            this.address = address;
+        } else throw new IllegalArgumentException("Недопустимый адрес");
+    }
+}
