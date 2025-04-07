1a449ddccd61b6ca67983409309fac9f3781a232
Danila Bogatyrev <d.l.bogatyrev@gmail.com>
2025-04-07 18:55:04 +0300

    BJS2-68650: fix format log, delete Optional for location

--- src/main/java/school/faang/library_westeros/LibrarySystem.java
+++ src/main/java/school/faang/library_westeros/LibrarySystem.java
@@ -13,39 +13,39 @@ public class LibrarySystem {
     public void addBook(String title, String author, int year, String location) {
         Book book = new Book(title, author, year);
         library.put(book, location);
-        log.info(String.format("Книга \"%s\" добавлена на полку %s\n", book, location));
+        log.info("Книга \"{}\" добавлена на полку {}", book, location);
     }
 
     public void removeBook(String title, String author, int year) {
         Book bookToRemove = new Book(title, author, year);
         if (library.remove(bookToRemove) != null) {
-            log.info(String.format("Книга \"%s\" удалена из библиотеки.\n", bookToRemove));
+            log.info("Книга \"{}\" удалена из библиотеки.", bookToRemove);
         } else {
-            log.info(String.format("Книга \"%s\" не найдена в библиотеке.\n", bookToRemove));
+            log.info("Книга \"{}\" не найдена в библиотеке.", bookToRemove);
         }
     }
 
     public Optional<Book> findBook(String title, String author, int year) {
         Book bookToFind = new Book(title, author, year);
-        Optional<String> location = Optional.ofNullable(library.get(bookToFind));
+        String location = library.get(bookToFind);
 
-        location.ifPresentOrElse(
-                loc -> log.info(String.format("Книга \"%s\" найдена на полке %s.%n", title, loc)),
-                () -> log.info(String.format("Книга \"%s\" не найдена в библиотеке.%n", title))
-        );
-
-        return location.isPresent() ? Optional.of(bookToFind) : Optional.empty();
+        if (location == null) {
+            log.info("Книга \"{}\" не найдена в библиотеке.", title);
+            return Optional.empty();
+        }
+        log.info("Книга \"{}\" найдена на полке {}.", title, location);
+        return Optional.of(bookToFind);
     }
 
     public void printAllBooks() {
         if (library.isEmpty()) {
-            log.info("Библиотека пуста.\n");
+            log.info("Библиотека пуста.");
             return;
         }
 
         System.out.println("Список всех книг в библиотеке:");
         for (Map.Entry<Book, String> entry : library.entrySet()) {
-            log.info(String.format("\"%s\" - полка: %s.\n", entry.getKey(), entry.getValue()));
+            log.info("\"{}\" - полка: {}.", entry.getKey(), entry.getValue());
         }
     }
 }
