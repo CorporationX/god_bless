500406858479b1bb38be1d0241f59623804e3852
psvm500 <faribo.gafur@gmail.com>
2025-04-07 19:11:42 +0300

    BJS2-68093 fix VariableDeclarationUsageDistance

--- src/main/java/school/faang/bjs2_68093/Main.java
+++ src/main/java/school/faang/bjs2_68093/Main.java
@@ -7,13 +7,13 @@ public class Main {
     public static void main(String[] args) {
         Character archer = new Archer("Комиссар Горгонитов");
         Character warrior = new Warrior("Зельда");
-        Character strongWarrior = new Warrior("Супермен", 110, 100, 1);
-        log.info("Здоровье warrior: {}", warrior.health);
-        log.info("Ловкость archer: {}", archer.agility);
+
+        log.info("Здоровье warrior: {}\nЛовкость archer: {}", warrior.health, archer.agility);
         archer.attack(warrior);
         log.info("Здоровье warrior после атаки archer: {}", warrior.health);
-        log.info("Сила strongWarrior: {}", strongWarrior.strength);
-        log.info("Здоровье archer: {}", archer.health);
+
+        Character strongWarrior = new Warrior("Супермен", 110, 100, 1);
+        log.info("Сила strongWarrior: {}\nЗдоровье archer: {}", strongWarrior.strength, archer.health);
         strongWarrior.attack(archer);
         log.info("Здоровье archer после атаки strongWarrior: {}", archer.health);
     }
