64d8af4c931a2020f21a5d1d9a19311608282deb
Sergey Selischev <ssb22@mail.ru>
2025-04-07 22:02:05 +0300

    исправление замечаний

--- src/main/java/school/faang/bjs2_68170/Main.java
+++ src/main/java/school/faang/bjs2_68170/Main.java
@@ -1,23 +1,21 @@
 package school.faang.bjs2_68170;
 
-import java.util.ArrayList;
 import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
-        List<User> usersList = new ArrayList<>(
-                List.of(new User("Ivan", 23, "home", "Moscow"),
-                        new User("Petr", 32, "office", "St.Peterburg"),
-                        new User("Oleg", 24, "office", "Orel"),
-                        new User("Alex", 32, "home", "Tver"),
-                        new User("Pavel", 30, "office", "Tula"),
-                        new User("Anna", 24, "office", "Kursk")
-                ));
+        List<User> usersList = List.of(new User("Ivan", 23, "home", "Moscow"),
+                new User("Petr", 32, "office", "St.Peterburg"),
+                new User("Oleg", 24, "office", "Orel"),
+                new User("Alex", 32, "home", "Tver"),
+                new User("Pavel", 30, "office", "Tula"),
+                new User("Anna", 24, "office", "Kursk")
+        );
 
         for (var ageGroup : User.groupUsers(usersList).entrySet()) {
-            System.out.println("Возраст: " + ageGroup.getKey());
+            System.out.printf("Возраст: %d \n", ageGroup.getKey());
             for (User user : ageGroup.getValue()) {
-                System.out.println(user.toString());
+                System.out.printf("%s \n", user.toString());
             }
         }
     }
