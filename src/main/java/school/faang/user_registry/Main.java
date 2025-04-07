750c6290f0cb33f4c7532f6e14e6a62b7f3710b7
Keriod <nikolainovopashin98@gmail.com>
2025-04-07 22:55:24 +0500

    BJS2-68099 - Создание пакет user_registry - Merge с основной веткой (Pelork)

--- src/main/java/school/faang/user_registry/Main.java
+++ src/main/java/school/faang/user_registry/Main.java
@@ -1,4 +1,4 @@
-package school.faang;
+package school.faang.user_registry;
 
 import lombok.extern.slf4j.Slf4j;
 
--- src/main/java/school/faang/user_registry/User.java
+++ src/main/java/school/faang/user_registry/User.java
@@ -1,4 +1,4 @@
-package school.faang;
+package school.faang.user_registry;
 
 import lombok.Getter;
 import lombok.ToString;
