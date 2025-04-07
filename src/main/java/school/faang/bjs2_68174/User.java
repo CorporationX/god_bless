cc22c8e363e9051b6f4c12b18554c61257ffa436
stanislav075539 <re1971reg@gmail.com>
2025-04-08 01:00:55 +0300

    stanislav075539 исправление замечаний BJS2-68174

--- src/main/java/school/faang/bjs2_68174/User.java
+++ src/main/java/school/faang/bjs2_68174/User.java
@@ -10,6 +10,7 @@ import java.util.Set;
 public class User {
     private static final Set<String> VALID_JOBS = Set.of("google", "uber", "amazon");
     private static final Set<String> VALID_ADDRESSES = Set.of("london", "new york", "amsterdam");
+    private static final Integer MIN_AGE = 18;
 
     private final String name;
     private final Integer age;
@@ -18,52 +19,33 @@ public class User {
 
     public User(String name, Integer age, String job, String address) {
         StringBuilder errors = new StringBuilder();
-        if (name.isBlank()) {
+        if (name == null || name.isBlank()) {
             errors.append("Имя не может быть пустым;");
         }
-        if (age < 18) {
+        if (age < MIN_AGE) {
             if (!errors.isEmpty()) {
                 errors.append(" ");
             }
-            errors.append("Возраст не может быть меньше 18;");
+            errors.append("Возраст не может быть меньше %s;".formatted(MIN_AGE));
         }
         if (!VALID_JOBS.contains(job.toLowerCase())) {
             if (!errors.isEmpty()) {
                 errors.append(" ");
             }
-            errors.append("Место работы должно содержаться в наборе VALID_JOBS;");
+            errors.append("Место работы должно содержаться в наборе %s;".formatted(VALID_JOBS));
         }
         if (!VALID_ADDRESSES.contains(address.toLowerCase())) {
             if (!errors.isEmpty()) {
                 errors.append(" ");
             }
-            errors.append("Адрес должен содержаться в наборе VALID_ADDRESSES;");
+            errors.append("Адрес должен содержаться в наборе %s;".formatted(VALID_ADDRESSES));
         }
-        if (errors.isEmpty()) {
-            this.name = name;
-            this.age = age;
-            this.job = job;
-            this.address = address;
-        } else {
+        if (!errors.isEmpty()) {
             throw new IllegalArgumentException(errors.toString());
         }
+        this.name = name;
+        this.age = age;
+        this.job = job;
+        this.address = address;
     }
-
-    public static User createUser(String name, Integer age, String job, String address) {
-        User user = null;
-        try {
-            user = new User(name, age, job, address);
-        } catch (RuntimeException e) {
-            System.out.printf("Ошибка создания пользователя: %s\n", e);
-        }
-        return user;
-    }
-
-    public static void main(String[] args) {
-        User ivan = createUser("Ivan", 30, "uber", "London");
-        User nonameUser = createUser("", 10, "Uber", "Amsterdam");
-        User stepan = createUser("Stepan", 10, "Uber", "London");
-        User john = createUser("John", 41, "Yahoo!", "Berlin");
-    }
-
 }
