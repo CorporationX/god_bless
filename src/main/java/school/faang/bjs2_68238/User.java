bdba306ee7a8c8f0f95c32e8ed4bcb30bab375e6
AlexandrPopov <164056516+Aleck210popov@users.noreply.github.com>
2025-04-07 22:33:22 +0300

    fix bug "name.isBlank()"

--- src/main/java/school/faang/bjs2_68238/User.java
+++ src/main/java/school/faang/bjs2_68238/User.java
@@ -6,17 +6,17 @@ import java.util.Set;
 
 @ToString
 public class User {
-    private String name;
-    private int age;
-    private String job;
-    private String address;
+    private final String name;
+    private final int age;
+    private final String job;
+    private final String address;
     private static final Set<String> VALID_JOBS =
             Set.of("Google", "Uber", "Amazon");
     private static final Set<String> VALID_ADDRESSES =
             Set.of("London", "New York", "Amsterdam");
 
     public User(String name, int age, String job, String address) {
-        if (name == null || age < 18 || !VALID_JOBS
+        if (name.isBlank() || age < 18 || !VALID_JOBS
                 .contains(job) || !VALID_ADDRESSES.contains(address)) {
             throw new IllegalArgumentException("validation exception");
         }
