73943300f09dc26b504bd6daeba6e09df69bf4cf
JolyGolden <nazi00696@mail.ru>
2025-04-07 12:44:47 +0200

    loved hobbies

--- src/main/java/school/faang/Main.java
+++ src/main/java/school/faang/Main.java
@@ -1,4 +1,20 @@
 package school.faang;
+import java.util.*;
 
 public class Main {
+    public static void main(String[] args) {
+        HashSet<String> jakeHobbies = new HashSet<>(Set.of("hobby horsing", "box", "swimming"));
+        HashSet<String> mikeHobbies = new HashSet<>(Set.of("water polo", "hiking", "dnd"));
+
+        User jake = new User(1, "Jake", 23, jakeHobbies);
+        User mike = new User(2, "Mike", 40, mikeHobbies);
+        List<User> users = new ArrayList<>();
+
+        users.add(mike);
+        users.add(jake);
+
+        HashSet<String> targetActivities = new HashSet<>(Set.of("box", "swimming"));
+        Map<User, String> hobbyLovers = jake.findHobbyLovers(users, targetActivities);
+        System.out.println(hobbyLovers.toString());
+    }
 }
---
+++ src/main/java/school/faang/User.java
@@ -0,0 +1,51 @@
+package school.faang;
+
+import lombok.Getter;
+import java.util.*;
+
+public class User {
+    private Integer id;
+    private String name;
+    private  Integer age;
+
+    @Getter
+    private HashSet<String> activities;
+
+    public User(Integer id, String name, Integer age, HashSet<String> activities) {
+        this.id = id;
+        this.name = name;
+        this.age = age;
+        this.activities = activities;
+    }
+
+    public Map<User, String> findHobbyLovers(List<User>users, HashSet<String> targetActivities){
+        Map<User, String> usersWithActivities = new HashMap<>();
+        for(User user: users){
+            Set<String> userActivities = user.getActivities();
+            for(String activity: userActivities){
+                if(targetActivities.contains(activity)){
+                    usersWithActivities.put(user, activity);
+                    break;
+                }
+            }
+        }
+        return usersWithActivities;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        User user = (User) o;
+        return Objects.equals(id, user.id) && Objects.equals(name, user.name) && Objects.equals(age, user.age) && Objects.equals(activities, user.activities);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, name, age, activities);
+    }
+    @Override
+    public String toString() {
+        return name;
+    }
+}
