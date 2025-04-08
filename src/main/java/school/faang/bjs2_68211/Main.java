e317a9763fa1fb71a4f61c062566cd55284e1760
SAM19991 <s.artur.m87@gmail.com>
2025-04-08 12:26:10 -0700

    Task BJS2-68211

---
+++ src/main/java/school/faang/bjs2_68211/Main.java
@@ -0,0 +1,21 @@
+package school.faang.bjs2_68211;
+
+import lombok.extern.slf4j.Slf4j;
+
+import java.util.List;
+import java.util.Map;
+
+@Slf4j
+public class Main {
+    public static void main(String[] args) {
+        User userArtur = new User("Artur", 37, "Google", "New York");
+        User userJoseph = new User("Joseph", 23, "Facebook", "Los Angeles");
+        User userJane = new User("Jane", 23, "Twitter", "New York");
+        User userJohn = new User("John", 23, "LinkedIn", "New York");
+        List<User> users = List.of(userArtur, userJoseph, userJane, userJohn);
+        Map<Integer, List<User>> usersAge = User.groupUsers(users);
+        for (var entry : usersAge.entrySet()) {
+            log.info("Age: {} - Users: {}", entry.getKey(), entry.getValue());
+        }
+    }
+}
---
+++ src/main/java/school/faang/bjs2_68211/User.java
@@ -0,0 +1,60 @@
+package school.faang.bjs2_68211;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.extern.slf4j.Slf4j;
+
+import java.util.*;
+
+@Getter
+@Setter
+@Slf4j
+public class User {
+    private String name;
+    private int age;
+    private String job;
+    private String location;
+
+    public User(String name, int age, String job, String location) {
+        if (name == null || name.trim().isEmpty()) {
+            log.error("Name cannot be null or empty.");
+            throw new IllegalArgumentException("Name cannot be null or empty.");
+        }
+        this.name = name;
+        if (age < 0 || age > 110) {
+            log.error("Age must be between 0 and 110.");
+            throw new IllegalArgumentException("Age must be between 0 and 110.");
+        }
+        this.age = age;
+        if (job == null || job.trim().isEmpty()) {
+            log.error("Job cannot be null or empty.");
+            throw new IllegalArgumentException("Job cannot be null or empty.");
+        }
+        this.job = job;
+        if (location == null || location.trim().isEmpty()) {
+            log.error("Location cannot be null or empty.");
+            throw new IllegalArgumentException("Location cannot be null or empty.");
+        }
+        this.location = location;
+    }
+
+    public static Map<Integer, List<User>> groupUsers(List<User> userslist) {
+        Map<Integer, List<User>> usersMap = new HashMap<>();
+        for (User user : userslist) {
+            int age = user.getAge();
+            usersMap.putIfAbsent(age, new ArrayList<>());
+            usersMap.get(age).add(user);
+        }
+        return usersMap;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "name='" + name + '\'' +
+                ", age=" + age +
+                ", job='" + job + '\'' +
+                ", location='" + location + '\'' +
+                '}';
+    }
+}
