fb3f35883798a7b2771ced47b488896fef6ffec5
Ramil Burgart <ramilburgart7006@gmail.com>
2025-04-08 22:39:17 +0300

    pick cherry
    ramil-burgart-bjs2-68214

--- src/main/java/school/faang/Main.java
+++ src/main/java/school/faang/Main.java
@@ -1,10 +1,7 @@
 package school.faang;
 
 public class Main {
-
     public static void main(String[] args) {
-
-
-        System.out.println("Очистил метод Main");
+        System.out.println("Cherry pick");
     }
 }
