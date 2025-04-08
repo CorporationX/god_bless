4d211c27eae035d4e18484e699b5fa9903c5751f
Ramil Burgart <ramilburgart7006@gmail.com>
2025-04-08 22:38:32 +0300

    add first commit
    ramil-burgart-bjs2-68214

--- src/main/java/school/faang/Main.java
+++ src/main/java/school/faang/Main.java
@@ -4,5 +4,7 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("Создание новой ветки ");
+
+        System.out.println("Добавление первого коммита ");
     }
 }
