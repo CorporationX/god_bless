5cdaff21c99bb0d46dfdb9bce335decd352c4f0f
vasidol123rus <vasekku25@gmail.com>
2025-04-08 21:32:03 +0700

    исправил

--- src/main/java/school/faang/reverse/Main.java
+++ src/main/java/school/faang/reverse/Main.java
@@ -2,24 +2,24 @@ package school.faang.reverse;
 
 import java.util.Arrays;
 
-import static school.faang.reverse.Reverse.reverse;
+import static school.faang.reverse.Example.reverse;
 
 public class Main {
     public static void main(String[] args) {
         int[] nums = {1, 2, 3, 4, 5};
-        System.out.println("Исходный массив: " + Arrays.toString(nums));
+        System.out.printf("Исходный массив: %s%n", Arrays.toString(nums));
 
         reverse(nums);
-        System.out.println("Развернутый массив: " + Arrays.toString(nums));
+        System.out.printf("Развернутый массив: %s%n", Arrays.toString(nums));
 
         int[] emptyArray = {};
-        System.out.println("Исходный пустой массив: " + Arrays.toString(emptyArray));
+        System.out.printf("Исходный пустой массив: %s%n", Arrays.toString(emptyArray));
         reverse(emptyArray);
-        System.out.println("Развернутый пустой массив: " + Arrays.toString(emptyArray));
+        System.out.printf("Развернутый пустой массив: %s%n", Arrays.toString(emptyArray));
 
         int[] singleElementArray = {7};
-        System.out.println("Исходный массив с одним элементом: " + Arrays.toString(singleElementArray));
+        System.out.printf("Исходный массив с одним элементом: %s%n", Arrays.toString(singleElementArray));
         reverse(singleElementArray);
-        System.out.println("Развернутый массив с одним элементом: " + Arrays.toString(singleElementArray));
+        System.out.printf("Развернутый массив с одним элементом: %s%n", Arrays.toString(singleElementArray));
     }
 }
