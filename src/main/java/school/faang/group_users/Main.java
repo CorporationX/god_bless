210dbee731254a58bc3efa5584ba20bdbf63a06d
Hitow22 <Hitow22@gmail.com>
2025-04-08 21:57:50 +0300

    Группировка пользователей по возрасту (доработка)

--- src/main/java/school/faang/group_users/Main.java
+++ src/main/java/school/faang/group_users/Main.java
@@ -1,20 +1,19 @@
 package school.faang.group_users;
 
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 
 public class Main {
     public static void main(String[] args) {
-        List<User> users = new ArrayList<>();
-        users.add(new User("Jake", 22, "Moscow", "Long street"));
-        users.add(new User("Sam", 22, "Moscow", "Long street"));
-        users.add(new User("Liam", 42, "Moscow", "Long street"));
-        users.add(new User("Alex", 42, "Moscow", "Long street"));
-        users.add(new User("Tom", 18, "Moscow", "Long street"));
-        users.add(new User("Will", 18, "Moscow", "Long street"));
+        List<User> users = List.of(
+                new User("Jake", 22, "Moscow", "Long street"),
+                new User("Sam", 22, "Moscow", "Long street"),
+                new User("Liam", 42, "Moscow", "Long street"),
+                new User("Alex", 42, "Moscow", "Long street"),
+                new User("Tom", 18, "Moscow", "Long street"),
+                new User("Will", 18, "Moscow", "Long street"));
 
-        Map<Integer, List<User>> userGroups = User.groupUsers(users);
-        System.out.println(userGroups);
+        Map<Integer, List<User>> groupedUsersByAge = User.groupUsers(users);
+        System.out.println(groupedUsersByAge);
     }
 }
--- src/main/java/school/faang/group_users/User.java
+++ src/main/java/school/faang/group_users/User.java
@@ -1,6 +1,5 @@
 package school.faang.group_users;
 
-
 import lombok.Data;
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -16,17 +15,11 @@ public class User {
     private final String address;
 
     public static Map<Integer, List<User>> groupUsers(List<User> users) {
-        Map<Integer, List<User>> userGroups = new HashMap<>();
+        Map<Integer, List<User>> groupedUsersByAge = new HashMap<>();
         for (User user : users) {
-            if (userGroups.containsKey(user.getAge())) {
-                List<User> usersWithSameAge = userGroups.get(user.getAge());
-                usersWithSameAge.add(user);
-            } else {
-                List<User> newList = new ArrayList<>();
-                newList.add(user);
-                userGroups.put(user.getAge(), newList);
-            }
+            groupedUsersByAge.putIfAbsent(user.getAge(), new ArrayList<>());
+            groupedUsersByAge.get(user.getAge()).add(user);
         }
-        return userGroups;
+        return groupedUsersByAge;
     }
 }
