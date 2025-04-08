a0a6ae7489499f29346a4051e00bce28906e8966
psvm500 <faribo.gafur@gmail.com>
2025-04-08 13:58:16 +0300

    BJS2-68652 Библиотека Вестеросса

---
+++ src/main/java/school/faang/bjs2_68652/Book.java
@@ -0,0 +1,14 @@
+package school.faang.bjs2_68652;
+
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.ToString;
+
+@EqualsAndHashCode
+@AllArgsConstructor
+@ToString
+public class Book {
+    private String title;
+    private String author;
+    private int year;
+}
---
+++ src/main/java/school/faang/bjs2_68652/LibrarySystem.java
@@ -0,0 +1,35 @@
+package school.faang.bjs2_68652;
+
+import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
+import java.text.MessageFormat;
+import java.util.Map;
+import java.util.NoSuchElementException;
+
+@Slf4j
+@AllArgsConstructor
+public class LibrarySystem {
+    protected Map<Book, String> catalogue;
+
+    protected void printAllBooks() {
+        this.catalogue.forEach((book, place) ->
+                log.info("Книга {} находится в {}", book, place));
+    }
+
+    protected void addBook(String title, String author, int year, String location) {
+        this.catalogue.put(new Book(title, author, year), location);
+    }
+
+    protected void removeBook(String title, String author, int year) {
+        this.catalogue.remove(new Book(title, author, year));
+    }
+
+    protected String findBook(String title, String author, int year) {
+        Book book = new Book(title, author, year);
+        String place = this.catalogue.get(book);
+        if (!place.isEmpty()) {
+            return place;
+        } else throw new NoSuchElementException(MessageFormat.format("Книга {} не найдена", book));
+    }
+}
