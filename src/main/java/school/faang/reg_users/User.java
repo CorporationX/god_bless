18037504f8a274e7e76f039012157340d10562d3
Hitow22 <Hitow22@gmail.com>
2025-04-08 23:09:12 +0300

    Регистрация пользователя(доработка)

--- src/main/java/school/faang/reg_users/User.java
+++ src/main/java/school/faang/reg_users/User.java
@@ -1,12 +1,14 @@
 package school.faang.reg_users;
 
 import lombok.ToString;
+
 import java.util.Set;
 
 @ToString
 public class User {
     private static final Set<String> VALID_JOBS = Set.of("Google", "Uber", "Amazon");
     private static final Set<String> VALID_ADDRESSES = Set.of("London", "New York", "Amsterdam");
+    private static final int ALLOWED_AGE = 18;
 
     private final String name;
     private final int age;
@@ -14,30 +16,28 @@ public class User {
     private final String address;
 
     public User(String name, int age, String job, String address) {
-        if (name.isEmpty()) {
-            throw new IllegalArgumentException("Name cannot be empty");
-        } else {
-            this.name = name;
+        if (name == null) {
+            throw new IllegalArgumentException("Name cannot be null");
         }
 
-        if (age < 18) {
-            throw new IllegalArgumentException("Age cannot be less than 18");
-        } else {
-            this.age = age;
+        if (name.isEmpty()) {
+            throw new IllegalArgumentException("Name cannot be empty");
         }
+        this.name = name;
+
+        if (age < ALLOWED_AGE) {
+            throw new IllegalArgumentException("Age cannot be less than " + ALLOWED_AGE);
+        }
+        this.age = age;
 
         if (!VALID_JOBS.contains(job)) {
             throw new IllegalArgumentException("Invalid job");
-        } else {
-            this.job = job;
         }
+        this.job = job;
 
         if (!VALID_ADDRESSES.contains(address)) {
             throw new IllegalArgumentException("Invalid address");
-        } else {
-            this.address = address;
         }
+        this.address = address;
     }
-
-
 }
