dc9a0bdcd6ea9377008f735e138d2926dbd9afec
SAM19991 <s.artur.m87@gmail.com>
2025-04-08 11:25:35 -0700

    Методы переименованы для более понятные описания. Метод main вынесен в отдельный класс Main. Лишние пустые строки перед началом метода main и в конце вывода в main удалены. Названия в циклах ForEach заменены на element.

--- src/main/java/school/faang/bjs2_68089/Main.java
+++ src/main/java/school/faang/bjs2_68089/Main.java
@@ -3,12 +3,12 @@ package school.faang.bjs2_68089;
 public class Main {
     public static void main(String[] args) {
         int[] array = {1, 2, 3, 4, 5};
-        Example example = new Example();
         System.out.println("Original Array:");
         for (int element : array) {
             System.out.println(element);
         }
         System.out.println("=====================");
+        Example example = new Example();
         example.reverseArray(array);
         System.out.println("Reverse Using for loop:");
         for (int element : array) {
