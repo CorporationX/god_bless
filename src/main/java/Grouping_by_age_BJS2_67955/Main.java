50d9031e58630a61f74147806ec92b82878c1f19
Slava Vermilion <beg.slava@bk.ru>
2025-04-08 12:39:50 +0700

    With notes

--- src/main/java/Grouping_by_age_BJS2_67955/Main.java
+++ src/main/java/Grouping_by_age_BJS2_67955/Main.java
@@ -1,7 +1,44 @@
 package Grouping_by_age_BJS2_67955;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 public class Main {
-    int age = 0;
-    String str;
-    String fname;
+    public static void main(String[] args) {
+        List<User> users = new ArrayList<>();   //создаем список пользователей
+        //вносим пользователей в список
+        users.add(new User("Alex", 20, "Theatre", "London"));
+        users.add(new User("Sonya", 29, "Mosque", "Budapest"));
+        users.add(new User("Margaret", 48, "Hospital", "Los Angeles"));
+        users.add(new User("Fred", 20, "Shop", "Vienna"));
+
+        Map<Integer, List<User>> groupedUsers = groupUsers(users); //группируем список пользователей по возрасту
+
+        for (Map.Entry<Integer, List<User>> entry : groupedUsers.entrySet()) { //используем цикл for each, Map.Entry - для итерации сразу по паре ключ - значение, а не по каждому отдельно, entrySet возвращает набор всех пар ключ - значение
+            System.out.println("Возраст: " + entry.getKey());
+            for (User user : entry.getValue()) {
+                System.out.println("Имя: " + user.getName());
+                System.out.println("Место работы: " + user.getWorkplace());
+                System.out.println("Адрес: " + user.getAddress());
+            }
+
+        }
+
+
+    }
+
+    public static Map<Integer, List<User>> groupUsers(List<User> users){
+        Map<Integer, List<User>> userGroups = new HashMap<>(); //создаем новую карту для хранения сгруппированных по возрасту пользователей
+        for (User user : users) { // используем цикл for each, где User - тип переменной, users - список пользователей, который передается в метод, user - представление текущего элемента при итерации по users
+            int age = user.getAge(); //
+            if(!userGroups.containsKey(age)){ //проверяем, есть ли список пользователей, сгруппированных по этому возрасту
+                userGroups.put(age, new ArrayList<>()); //если такого списка нет, то создаем его (используем ArrayList так как заранее не знаем сколько пользователей будет в конкретном списке)
+            }
+            userGroups.get(age).add(user); //добавляем пользователя в его возрастную группу
+        }
+        return userGroups;
+    }
 }
+
--- src/main/java/Grouping_by_age_BJS2_67955/User.java
+++ src/main/java/Grouping_by_age_BJS2_67955/User.java
@@ -1,5 +1,24 @@
 package Grouping_by_age_BJS2_67955;
 
+import lombok.Getter;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@Getter
 public class User {
+    private String name;
+    private int age;
+    private String workplace;
+    private String address;
+
+    public User(String name, int age, String workplace, String address) { //конструктор
+        this.name = name;
+        this.age = age;
+        this.workplace = workplace;
+        this.address = address;
+    }
 
 }
