dddfc9613327fc067a4fd2365a7e63adc18bdc62
Lusivar <jad.fale@mail.ru>
2025-04-08 19:54:22 +0300

    BJS2-68219 Группировка пользователей по возрасту

---
+++ src/main/java/school/faang/bjs2_68219/User.java
@@ -0,0 +1,23 @@
+package school.faang.bjs2_68219;
+
+import lombok.Builder;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+import java.util.*;
+import java.util.stream.Collectors;
+
+import static java.util.stream.Collectors.*;
+
+@Data
+@Builder
+public class User {
+    private String name;
+    private Integer age;
+    private String workPlace;
+    private String address;
+
+    public static Map<Integer, List<User>> groupUsers(List<User> users) {
+        return users.stream().collect(Collectors.groupingBy(User::getAge, TreeMap::new, Collectors.toList()));
+    }
+}
