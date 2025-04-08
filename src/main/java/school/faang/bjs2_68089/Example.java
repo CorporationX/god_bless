ba8013cadeea1ec4403740c80f84de5b456c04b0
SAM19991 <s.artur.m87@gmail.com>
2025-04-08 11:19:17 -0700

    Методы переименованы для более понятные описания. Метод main вынесен в отдельный класс Main. Лишние пустые строки перед началом метода main и в конце вывода в main удалены. Названия в циклах ForEach заменены на element.

--- src/main/java/school/faang/bjs2_68089/Example.java
+++ src/main/java/school/faang/bjs2_68089/Example.java
@@ -4,7 +4,7 @@ import java.util.ArrayList;
 
 public class Example {
 
-    public void reverse(int[] array) {
+    public void reverseArray(int[] array) {
         int temp;
         for (int i = 0; i < array.length / 2; i++) {
             temp = array[i];
@@ -13,47 +13,14 @@ public class Example {
         }
     }
 
-    public void reverse2(int[] array) {
-        for (int i = 0; i < array.length; i++) {
-            int temp = array[i];
-            array[i] = array[array.length - i - 1];
-            array[array.length - i - 1] = temp;
-        }
-    }
-
-    public void reverse3(int[] array) {
+    public void reverseUsingList (int[] array) {
         ArrayList<Integer> list = new ArrayList<>();
-        for (int j : array) {
-            list.add(j);
+        for (int element : array) {
+            list.add(element);
         }
         list.sort((o1, o2) -> o2 - o1);
         for (int i = 0; i < array.length; i++) {
             array[i] = list.get(i);
         }
-    }
-
-    public static void main(String[] args) {
-
-        int[] array = {1, 2, 3, 4, 5};
-        Example example = new Example();
-        for (int j : array) {
-            System.out.println(j);
-        }
-        System.out.println("=====================");
-        example.reverse(array);
-        for (int j : array) {
-            System.out.println(j);
-        }
-        System.out.println("=====================");
-        example.reverse2(array);
-        for (int j : array) {
-            System.out.println(j);
-        }
-        System.out.println("=====================");
-        example.reverse3(array);
-        for (int j : array) {
-            System.out.println(j);
-        }
-
     }
 }
---
+++ src/main/java/school/faang/bjs2_68089/Main.java
@@ -0,0 +1,24 @@
+package school.faang.bjs2_68089;
+
+public class Main {
+    public static void main(String[] args) {
+        int[] array = {1, 2, 3, 4, 5};
+        Example example = new Example();
+        System.out.println("Original Array:");
+        for (int element : array) {
+            System.out.println(element);
+        }
+        System.out.println("=====================");
+        example.reverseArray(array);
+        System.out.println("Reverse Using for loop:");
+        for (int element : array) {
+            System.out.println(element);
+        }
+        System.out.println("=====================");
+        example.reverseUsingList (array);
+        System.out.println("Reversed using list:");
+        for (int element : array) {
+            System.out.println(element);
+        }
+    }
+}
