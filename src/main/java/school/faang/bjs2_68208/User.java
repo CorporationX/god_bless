bd7f1e7d5cc32ab6ae229d603e49f00332e62675
Lusivar <jad.fale@mail.ru>
2025-04-08 22:31:29 +0300

    BJS2-68208 refactoring

--- src/main/java/school/faang/bjs2_68208/User.java
+++ src/main/java/school/faang/bjs2_68208/User.java
@@ -2,24 +2,27 @@ package school.faang.bjs2_68208;
 
 import lombok.Data;
 
-import java.util.*;
+import java.util.Objects;
+import java.util.Set;
 
 @Data
 public class User {
-    private String name;
-    private int age;
-    private String job;
-    private String address;
+    private final int MINIMUM_AGE = 18;
     private static final Set<String> VALID_JOBS = Set.of(
             "Google",
             "Uber",
             "Amazon"
     );
-    private static final Set<String> VALID_ADDRESSES  = Set.of(
+    private static final Set<String> VALID_ADDRESSES = Set.of(
             "London",
             "New York",
             "Amsterdam"
     );
+
+    private String name;
+    private int age;
+    private String job;
+    private String address;
 
     public User(String name, int age, String job, String address) {
         validateName(name);
@@ -34,27 +37,26 @@ public class User {
     }
 
     private void validateName(String name) {
-        if (!Objects.nonNull(name)) {
+        if (!Objects.isNull(name)) {
             throw new IllegalArgumentException("Field 'name' is not valid!");
         }
     }
 
     private void validateAge(int age) {
-        if (age < 18) {
+        if (age < MINIMUM_AGE) {
             throw new IllegalArgumentException("Field 'age' is not valid!");
         }
     }
 
     private void validateJob(String job) {
-        if (!Objects.nonNull(address) || !VALID_JOBS.contains(job)) {
+        if (!Objects.isNull(address) || !VALID_JOBS.contains(job)) {
             throw new IllegalArgumentException("Field 'job' is not valid!");
         }
     }
 
     private void validateAddress(String address) {
-        if (!Objects.nonNull(address) || !VALID_ADDRESSES.contains(address)) {
+        if (!Objects.isNull(address) || !VALID_ADDRESSES.contains(address)) {
             throw new IllegalArgumentException("Field 'address' is not valid!");
         }
     }
-
 }
