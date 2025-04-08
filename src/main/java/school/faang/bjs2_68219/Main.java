0dd8f865227f448874bbda6a2d327ad451aa7f92
Lusivar <jad.fale@mail.ru>
2025-04-08 22:25:14 +0300

    BJS2-68219 refactoring2

--- src/main/java/school/faang/bjs2_68219/Main.java
+++ src/main/java/school/faang/bjs2_68219/Main.java
@@ -4,7 +4,8 @@ import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
-        List<User> infoUsers = List.of(User.builder().name("Alena").age(50).address("Moscow").workPlace("Amazon").build(),
+        List<User> infoUsers =
+                List.of(User.builder().name("Alena").age(50).address("Moscow").workPlace("Amazon").build(),
                 User.builder().name("Elena").age(34).address("London").workPlace("Loc").build(),
                 User.builder().name("Mark").age(13).address("NY").workPlace("Google").build(),
                 User.builder().name("Alex").age(65).address("Kemer").workPlace("Walmart").build());
