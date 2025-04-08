811f51f0cd56888b29efb6b922bf61b073a538da
psvm500 <faribo.gafur@gmail.com>
2025-04-08 16:16:23 +0300

    BJS2-68652 обработка null в методах

--- src/main/java/school/faang/bjs2_68652/LibrarySystem.java
+++ src/main/java/school/faang/bjs2_68652/LibrarySystem.java
@@ -13,20 +13,39 @@ public class LibrarySystem {
     protected Map<Book, String> catalogue;
 
     protected void printAllBooks() {
-        this.catalogue.forEach((book, place) ->
-                log.info("Книга {} находится в {}", book, place));
+        if (this.catalogue.isEmpty()) {
+            log.info("Книг нет");
+        } else {
+            this.catalogue.forEach((book, place) ->
+                    log.info("Книга {} находится в {}", book, place));
+        }
     }
 
     protected void addBook(String title, String author, int year, String location) {
-        this.catalogue.put(new Book(title, author, year), location);
+        if (!title.isEmpty() || !author.isEmpty() || !location.isEmpty()) {
+            Book book = new Book(title, author, year);
+            this.catalogue.put(book, location);
+            log.info("Книга {} добавлена", book);
+        } else {
+            log.info("Проверьте корректность введённых данных");
+        }
     }
 
     protected void removeBook(String title, String author, int year) {
-        this.catalogue.remove(new Book(title, author, year));
+        Book book = new Book(title, author, year);
+        if (this.catalogue.containsKey(book)) {
+            this.catalogue.remove(book);
+            log.info("Книга {} удалена", book);
+        } else {
+            throw new NoSuchElementException(String.format("Книга %s не найдена", book));
+        }
     }
 
     protected String findBook(String title, String author, int year) {
         Book book = new Book(title, author, year);
+        if (!this.catalogue.containsKey(book)) {
+            throw new NoSuchElementException(String.format("Книга %s не найдена", book));
+        }
         String place = this.catalogue.get(book);
         if (!place.isEmpty()) {
             return place;
