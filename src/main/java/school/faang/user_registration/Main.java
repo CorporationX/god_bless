0907f9b080c6f38128f2ad67a781e9e728a23ce4
Vladimir Ekgardt <75535761+VladimirEkgardt@users.noreply.github.com>
2025-04-08 09:17:53 +0300

    Revert "Регистрация пользователя BJS2-68144"

--- src/main/java/school/faang/user_registration/Main.java
+++
@@ -1,8 +0,0 @@
-package school.faang.user_registration;
-
-public class Main {
-    public static void main(String[] args) {
-        User user = new User("John11", 19, "Google", "New York");
-        User user1 = new User("John11", 17, "Google", "New York");
-    }
-}
