9d7ffa468399528c4ea7e3035ca407c32cbf3aea
Ramil Burgart <ramilburgart7006@gmail.com>
2025-04-08 22:40:05 +0300

    clear method Main
    ramil-burgart-bjs2-68214

--- src/main/java/school/faang/Main.java
+++ src/main/java/school/faang/Main.java
@@ -3,10 +3,8 @@ package school.faang;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Создание новой ветки ");
 
-        System.out.println("Добавление первого коммита ");
 
-        System.out.println("Удалил строчки номер 6");
+        System.out.println("Очистил метод Main");
     }
 }
