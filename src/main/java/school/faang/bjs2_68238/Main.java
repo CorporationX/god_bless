80a5eebcc9f3510aefecd1ba4bd592ea3f4f559c
AlexandrPopov <164056516+Aleck210popov@users.noreply.github.com>
2025-04-08 18:34:03 +0300

    fix bug NullPointerException

--- src/main/java/school/faang/bjs2_68238/Main.java
+++ src/main/java/school/faang/bjs2_68238/Main.java
@@ -1,34 +1,36 @@
 package school.faang.bjs2_68238;
 
-import java.util.List;
+import java.util.Set;
+
+import static java.util.Set.of;
 
 public class Main {
     public static void main(String[] arguments) {
         try {
-            new User(null, 25, "Google", "London");
-        } catch (IllegalArgumentException exception) {
-            System.out.println("Error creating user: " + exception.getMessage());
+            new User("", 25, "Google", "London");
+        } catch (IllegalArgumentException ex) {
+            System.out.println("Error creating user: " + ex.getMessage());
         }
 
         try {
             new User("Anna", 17, "Google", "London");
-        } catch (IllegalArgumentException exception) {
-            System.out.println("Error creating user: " + exception.getMessage());
+        } catch (IllegalArgumentException ex) {
+            System.out.println("Error creating user: " + ex.getMessage());
         }
 
         try {
             new User("Peter", 25, "Yandex", "London");
-        } catch (IllegalArgumentException exception) {
-            System.out.println("Error creating user: " + exception.getMessage());
+        } catch (IllegalArgumentException ex) {
+            System.out.println("Error creating user: " + ex.getMessage());
         }
 
         try {
             new User("Elena", 25, "Google", "Moscow");
-        } catch (IllegalArgumentException exception) {
-            System.out.println("Error creating user: " + exception.getMessage());
+        } catch (IllegalArgumentException ex) {
+            System.out.println("Error creating user: " + ex.getMessage());
         }
 
-        List<User> validUsers = List.of(
+        Set<User> validUsers = of(
                 new User("Ivan Petrov", 25, "Google", "London"),
                 new User("Maria Sidorova", 30, "Uber", "New York"),
                 new User("Alexey Nikolaev", 45, "Amazon", "Amsterdam")
--- src/main/java/school/faang/bjs2_68238/User.java
+++ src/main/java/school/faang/bjs2_68238/User.java
@@ -10,15 +10,28 @@ public class User {
     private final int age;
     private final String job;
     private final String address;
+    private static final int MIN_AGE = 18;
+
     private static final Set<String> VALID_JOBS =
             Set.of("Google", "Uber", "Amazon");
     private static final Set<String> VALID_ADDRESSES =
             Set.of("London", "New York", "Amsterdam");
 
     public User(String name, int age, String job, String address) {
-        if (name.isBlank() || age < 18 || !VALID_JOBS
-                .contains(job) || !VALID_ADDRESSES.contains(address)) {
-            throw new IllegalArgumentException("validation exception");
+        if (name == null || job == null || address == null) {
+            throw new IllegalArgumentException("params is empty");
+        }
+        if (name.isBlank()) {
+            throw new IllegalArgumentException("name is empty");
+        }
+        if (age < MIN_AGE) {
+            throw new IllegalArgumentException("age unger " + 18);
+        }
+        if (!VALID_JOBS.contains(job)) {
+            throw new IllegalArgumentException("incorrect job");
+        }
+        if (!VALID_ADDRESSES.contains(address)) {
+            throw new IllegalArgumentException("incorrect address");
         }
         this.name = name;
         this.age = age;
