6e6788d96ca88b2fbff2cc10feb12f19c7babdd9
JolyGolden <nazi00696@mail.ru>
2025-04-08 23:40:22 +0200

    smth

---
+++ src/main/java/school/faang/hogwarts/Main.java
@@ -0,0 +1,26 @@
+package school.faang.hogwarts;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class Main {
+    public static void main(String[] args) {
+        HogwartsSpells hogwartsSpells = new HogwartsSpells();
+        hogwartsSpells.addSpellEvent("Attack", "Damaging");
+        hogwartsSpells.addSpellEvent("Fireball", "fire in a face");
+        hogwartsSpells.addSpellEvent("Sunstrike", "sun on a head");
+        hogwartsSpells.addSpellEvent("Snowwall", "add a snow wall");
+        System.out.println();
+        System.out.println(hogwartsSpells.getSpellEventById(3));
+        System.out.println(hogwartsSpells.getSpellEventsByType("Fireball"));
+
+        hogwartsSpells.deleteSpellEvent(2);
+        System.out.println();
+        hogwartsSpells.printAllSpellEvents();
+        System.out.println();
+
+
+
+    }
+}
