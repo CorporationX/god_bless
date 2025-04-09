096baeec146e6c33b527cb6376dae887d6358fee
Miras <mirasospan62@gmail.com>
2025-04-09 18:31:13 +0500

    fixed the default message for unfound books, map to instance variable and removed unnecessary empty line

--- src/main/java/school/faang/bjs2_68666/LibrarySystem.java
+++ src/main/java/school/faang/bjs2_68666/LibrarySystem.java
@@ -7,32 +7,31 @@ import java.util.Map;
 import java.util.Objects;
 
 public class LibrarySystem {
-    private static final Map<Book, String> BOOKS_PLACE = new HashMap<>();
+    private final Map<Book, String> booksPlace = new HashMap<>();
+    private static final String BOOK_NOT_FOUND_MSG = "Book not found!";
 
     public void addBook(String title, String author, int year, String location) {
-        BOOKS_PLACE.put(new Book(title, author, year), location);
+        this.booksPlace.put(new Book(title, author, year), location);
     }
 
     public String findBook(String title, String author, int year) {
         String location;
         Book book = new Book(title, author, year);
-        location = BOOKS_PLACE.getOrDefault(book, "book not found");
-
+        location = this.booksPlace.getOrDefault(book, BOOK_NOT_FOUND_MSG);
         return location;
     }
     
     public void removeBook(String title, String author, int year) {
-        if (!Objects.equals(findBook(title, author, year), "book not found")) {
-            BOOKS_PLACE.remove(new Book(title, author, year));
+        if (!Objects.equals(findBook(title, author, year), BOOK_NOT_FOUND_MSG)) {
+            this.booksPlace.remove(new Book(title, author, year));
         }
     }
     
     public List<String> printAllBooks() {
         List<String> bookList = new ArrayList<>();
-        for (Map.Entry<Book, String> entry : BOOKS_PLACE.entrySet()) {
+        for (Map.Entry<Book, String> entry : this.booksPlace.entrySet()) {
             bookList.add(entry.getKey().toString() + " on " + entry.getValue());
         }
-
         return bookList;
     }
 }
