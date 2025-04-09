595f4beaae5405c0c7b9a16a1bb6451e3906d3bb
Likhtin_Nick <kolyangrom2000500@gmail.com>
2025-04-09 19:32:59 +0700

    BJS2-68255 На проверку по замечаниям "@iBerbasov commented on this pull request."

--- src/main/java/school/faang/Main.java
+++ src/main/java/school/faang/Main.java
@@ -6,19 +6,19 @@ import java.util.List;
 public class Main {
     public static void main(String[] args) {
 
-        List<User> users = new ArrayList<>();
-        users.add(new User("DONAT", 23, "Uber", "Amsterdam"));
-        users.add(new User("POLAND", 24, "Uber", "London"));
-        users.add(new User("RICKY", 22, "Uber", "Amsterdam"));
-        users.add(new User("ABBY", 19, "Amazon", "Amsterdam"));
-        users.add(new User("DUCK", 100, "Uber", "Amsterdam"));
-        users.add(new User("POLICY", 55, "Google", "New York"));
-        users.add(new User("SAX", 33, "Google", "New York"));
-        users.add(new User("BOLT", 21, "Google", "London"));
-        users.add(new User("QWERTY", 22, "Amazon", "New York"));
-        users.add(new User("ALIC", 18, "Amazon", "New York"));
-        users.add(new User("ALEX", 19, "Amazon", "New York"));
+        List<User> user = new ArrayList<>();
+        user.add(new User("DONAT", 23, "Uber", "Amsterdam"));
+        user.add(new User("POLAND", 24, "Uber", "London"));
+        user.add(new User("RICKY", 22, "Uber", "Amsterdam"));
+        user.add(new User("ABBY", 19, "Amazon", "Amsterdam"));
+        user.add(new User("DUCK", 100, "Uber", "Amsterdam"));
+        user.add(new User("POLICY", 55, "Google", "New York"));
+        user.add(new User("SAX", 33, "Google", "New York"));
+        user.add(new User("BOLT", 21, "Google", "London"));
+        user.add(new User("QWERTY", 22, "Amazon", "New York"));
+        user.add(new User("ALIC", 18, "Amazon", "New York"));
+        user.add(new User("ALEX", 19, "Amazon", "New York"));
 
-        System.out.println(users);
+        System.out.println(user);
     }
 }
