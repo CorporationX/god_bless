edcc086851a56e8af2fedebf81d5ed5c33d8a231
Raidanmark <Onidanil@Gmail.com>
2025-04-09 13:50:08 +0200

    reverse method called without instance

--- src/main/java/school/faang/turn_around/Main.java
+++ src/main/java/school/faang/turn_around/Main.java
@@ -4,11 +4,9 @@ import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) {
-        Example example = new Example();
-
         int[] array = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
 
         System.out.println(Arrays.toString(array));
-        System.out.println(Arrays.toString(example.reverse(array)));
+        System.out.println(Arrays.toString(Example.reverse(array)));
     }
 }
