523808ba0abdc3b416c809ab5f4d7efc159b64ed
pirojok-yt <75632276+pirojok-yt@users.noreply.github.com>
2025-04-09 21:53:28 +0500

    done

--- src/main/java/school/faang/ageofusers/Main.java
+++ src/main/java/school/faang/ageofusers/Main.java
@@ -4,7 +4,6 @@ import java.util.ArrayList;
 
 public class Main {
     public static void main(String[] args) {
-
         ArrayList<User> users = new ArrayList<>();
 
         users.add(new User("Anton", 17, "home", "18 street"));
