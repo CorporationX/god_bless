b89753e80d29763813716a2c423f0c964d9cff7a
Raidanmark <Onidanil@Gmail.com>
2025-04-09 14:26:58 +0200

    find method optimized import style fixed

--- src/main/java/school/faang/user_collection/User.java
+++ src/main/java/school/faang/user_collection/User.java
@@ -1,29 +1,31 @@
 package school.faang.user_collection;
 
 import lombok.AllArgsConstructor;
-import lombok.Data;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
 
-import java.util.*;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
 
-@Data
+@Getter
 @AllArgsConstructor
 @NoArgsConstructor
 public class User {
     private int id;
     private String name;
     private int age;
-    Set<String> activity;
+    private Set<String> activity;
 
     public Map<User, String> findHobbyLovers(List<User> users, Set<String> activities) {
         Map<User, String> hobbyLovers = new HashMap<>();
 
         for (User user : users) {
-            Optional<String> matched = user.getActivity().stream()
+            user.getActivity().stream()
                     .filter(activities::contains)
-                    .findFirst();
-
-            matched.ifPresent(activity -> hobbyLovers.put(user, activity));
+                    .findFirst()
+                    .ifPresent(activity -> hobbyLovers.put(user, activity));
         }
         return hobbyLovers;
     }
