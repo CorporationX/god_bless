343d986160686e615048bd3aa15686292bb1c6e9
Scar163 <trejdert15@gmail.com>
2025-04-09 15:48:45 +0400

    решил задачу

--- src/main/java/school/faang/Main.java
+++ src/main/java/school/faang/Main.java
@@ -1,4 +1,16 @@
 package school.faang;
 
 public class Main {
+    public static void main(String[] args) {
+        User user = new User("Ben", 23, "Uber", "Amsterdam");
+        User user2 = new User("Mira", 19, "Google", "London");
+        System.out.println(user2 + " " + user);
+
+        try {
+            User incorrectUser = new User(" ", 9, "Google", "London");
+            System.out.println(incorrectUser);
+        } catch (IllegalArgumentException e) {
+            System.out.println("the information is filled out incorrectly" + e.getMessage());
+        }
+    }
 }
---
+++ src/main/java/school/faang/User.java
@@ -0,0 +1,45 @@
+package school.faang;
+
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+import java.util.Set;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@EqualsAndHashCode
+@ToString
+public class User {
+
+    private static final Set<String> VALID_JOBS = Set.of("Google", "Uber", "Amazon");
+    private static final Set<String> VALID_ADDRESSES = Set.of("London", "New York", "Amsterdam");
+    private static final int VALID_AGE = 18;
+    private String name;
+    private int age;
+    private String job;
+    private String address;
+
+    public User(String name, int age, String job, String address) {
+        this.name = name;
+        this.age = age;
+        this.job = job;
+        this.address = address;
+
+        if (name == null || name.isBlank()) {
+            throw new IllegalArgumentException("The name cannot be empty");
+        }
+        if (age <= VALID_AGE) {
+            throw new IllegalArgumentException("The age must not be less than 18" + VALID_AGE);
+        }
+        if (!VALID_JOBS.contains(job)) {
+            throw new IllegalArgumentException("Invalid job" + job);
+        }
+        if (!VALID_ADDRESSES.contains(address)) {
+            throw new IllegalArgumentException("Invalid job" + address);
+        }
+    }
+}
