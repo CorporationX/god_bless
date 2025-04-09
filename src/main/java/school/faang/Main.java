c94c9843b37ecda33bb202cd177da90a35b83571
Likhtin_Nick <kolyangrom2000500@gmail.com>
2025-04-09 17:35:50 +0700

    BJS2-68255 Rename

--- src/main/java/school/faang/Main.java
+++ src/main/java/school/faang/Main.java
@@ -3,7 +3,7 @@ package school.faang;
 import java.util.ArrayList;
 import java.util.List;
 
-public class Mains {
+public class Main {
     public static void main(String[] args) {
 
         List<Users> users = new ArrayList<>();
