b940916ccd867197996d6b3e9298cded5480a6dc
Slava Vermilion <beg.slava@bk.ru>
2025-04-09 15:33:30 +0700

    fixed

--- src/main/java/gratsio_BJS2_68181/Main.java
+++ src/main/java/gratsio_BJS2_68181/Main.java
@@ -2,6 +2,10 @@ package gratsio_BJS2_68181;
 
 public class Main {
     public static void main(String[] args) {
+        User alex = new User("Alex", 20, "Google", "London");
+        User bob = new User("Bob", 45, "Facebook", "Amsterdam");
+        User jane = new User("Jane", 16, "Amazon", "New York");
+        User john = new User("John", 33, "Uber", "Washington");
     }
 }
 
--- src/main/java/gratsio_BJS2_68181/User.java
+++ src/main/java/gratsio_BJS2_68181/User.java
@@ -1,14 +1,10 @@
 package gratsio_BJS2_68181;
-
 import java.util.Set;
 
 public class User {
     public static final Set<String> VALID_JOBS = Set.of("Google", "Uber", "Amazon");
-
     public static final Set<String> VALID_ADDRESS = Set.of("London", "New York", "Amsterdam");
-
     public static final int MIN_AGE = 18;
-
     private String name;
     private int age;
     private String job;
@@ -16,16 +12,16 @@ public class User {
 
     public User(String name, int age, String job, String address) {
         if (name == null || name.isEmpty()) {
-            throw new IllegalArgumentException("Invalid user name");
+            throw new IllegalArgumentException("Invalid user name!An empty line has been entered.");
         }
         if (age < MIN_AGE) {
-            throw new IllegalArgumentException("Invalid age");
+            throw new IllegalArgumentException("Invalid age! The entered age is less than 18.");
         }
         if (!VALID_JOBS.contains(job)) {
-            throw new IllegalArgumentException("Invalid job");
+            throw new IllegalArgumentException("Invalid job!Incorrect place of work has been entered.");
         }
         if (!VALID_ADDRESS.contains(address)) {
-            throw new IllegalArgumentException("Invalid address");
+            throw new IllegalArgumentException("Invalid address!Incorrect address is entered.");
         }
         this.name = name;
         this.age = age;
