7d8dca2453ecdac7ea11e67945e7c39a4c5e3b48
Tanya <hummingbird_t@mail.ru>
2025-04-09 11:45:33 +0300

    Форматировала код. Удалила описание задания

--- src/main/java/school/faang/User.java
+++ src/main/java/school/faang/User.java
@@ -16,10 +16,6 @@ public class User {
     private Set<String> activities;
 
     public Map<User, String> findHobbyLovers(List<User> people, Set<String> activities) {
-/*     Метод должен в переданном списке пользователей находить тех,
-       у кого в ИХ множестве активностей есть хотя бы один элемент из переданного в метод множества активностей.
-       Если так, то сохранить этого пользователя в виде ключа в результирующую мапу,
-       а значением ему выставить первую совпавшую активность в двух множествах. */
 
         Map<User, String> hobbyLovers = new HashMap<>();
 
