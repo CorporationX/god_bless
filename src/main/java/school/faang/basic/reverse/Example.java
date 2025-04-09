ea3e314b202efd4407b4fc4ae8180cb565541434
Kirill Frolov <frolov.kirill.17@yandex.ru>
2025-04-10 02:13:36 +0300

    PR issues resolved
    1) Reverse method refactored

--- src/main/java/school/faang/basic/reverse/Example.java
+++ src/main/java/school/faang/basic/reverse/Example.java
@@ -1,25 +1,12 @@
 package school.faang.basic.reverse;
 
-import java.util.Arrays;
-
 public class Example {
 
     public static int[] reverse(int[] input) {
-        int pivotIndex;
-        int[] output = Arrays.copyOf(input, input.length);
+        int[] output = new int[input.length];
 
-        if (output.length < 2) {
-            return output;
-        } else if (output.length % 2 == 0) {
-            pivotIndex = input.length / 2;
-        } else {
-            pivotIndex = (input.length - 1) / 2;
-        }
-
-        for (int i = 0; i < pivotIndex; i++) {
-            int opposite = output[output.length - 1 - i];
-            output[output.length - 1 - i] = output[i];
-            output[i] = opposite;
+        for (int i = 0; i < input.length; i++) {
+            output[i] = input[input.length - 1 - i];
         }
         return output;
     }
