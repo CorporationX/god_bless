097c714baafbadfdfb88939fc7fe513ee75352bf
pirojok_yt <tamik34@mail.ru>
2025-04-09 20:53:27 +0500

    fixed codestyle errors

--- src/main/java/school/faang/ageofusers/User.java
+++ src/main/java/school/faang/ageofusers/User.java
@@ -12,14 +12,14 @@ public class User {
     private String name;
     private int age;
     private String workplace;
-    private String adress;
+    private String address;
 
-    public User(String name, int age, String workplace, String adress) {
+    public User(String name, int age, String workplace, String address) {
 
         this.name = name;
         this.age = age;
         this.workplace = workplace;
-        this.adress = adress;
+        this.address = address;
     }
 
     public static Map<Integer, List<User>> groupUsers(List<User> users) {
