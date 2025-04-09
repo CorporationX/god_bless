36a91845ef2ab7566b0391cc476bbecc6570c6b6
pirojok_yt <tamik34@mail.ru>
2025-04-09 22:00:56 +0500

    finish

--- src/main/java/school/faang/ageofusers/User.java
+++ src/main/java/school/faang/ageofusers/User.java
@@ -1,34 +1,34 @@
-package school.faang.Group_Users_By_Age;
+package school.faang.ageofusers;
 
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+
 @lombok.Getter
 @lombok.Setter
-public class Userr {
+
+public class User {
 
     private String name;
     private int age;
     private String workplace;
-    private String adress;
+    private String address;
 
-    public Userr(String name, int age, String workplace, String adress) {
+    public User(String name, int age, String workplace, String address) {
         this.name = name;
         this.age = age;
         this.workplace = workplace;
-        this.adress = adress;
+        this.address = address;
     }
 
 
-    public static Map<Integer, List<Userr>> groupUsers(List<Userr> users) {
+    public static Map<Integer, List<User>> groupUsers(List<User> users) {
+        Map<Integer, List<User>> usersMap = new HashMap<>();
 
-        Map<Integer, List<Userr>> usersMap = new HashMap<>();
-
-        for(Userr user: users) {
+        for(User user: users) {
             usersMap.computeIfAbsent(user.getAge(), a -> new ArrayList<>()).add(user);
         }
         return usersMap;
     }
-
 }
