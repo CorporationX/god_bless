5827ebf27b5f096564e348516fb339b4cd99f7f4
SAM19991 <s.artur.m87@gmail.com>
2025-04-09 12:07:09 -0700

    Добавил константу MIN_YEAR для проверки года, заменил объявление HashMap на Map, использовал put вместо computeIfAbsent для добавления книг, применил this для обращения к полям класса, удалил лишние пустые строки и использовал аннотацию @EqualsAndHashCode Lombok-а для методов equals и hashCode.

--- src/main/java/school/faang/bjs2_68674/Book.java
+++ src/main/java/school/faang/bjs2_68674/Book.java
@@ -1,44 +1,29 @@
 package school.faang.bjs2_68674;
 
+import lombok.EqualsAndHashCode;
 import lombok.ToString;
 import lombok.extern.slf4j.Slf4j;
 
-import java.util.Objects;
-
 @Slf4j
+@EqualsAndHashCode
 @ToString
 public class Book {
     private final String title;
     private final String author;
     private final int year;
+    private static final int MIN_YEAR = 0;
 
     public Book(String title, String author, int year) {
         if (title == null || author == null) {
             log.error("Attempted to create Book with null title or author");
             throw new IllegalArgumentException("Title and author cannot be null");
         }
-        if (year < 0) {
+        if (year < MIN_YEAR) {
             log.error("Attempted to create Book with negative year: {}", year);
             throw new IllegalArgumentException("Year cannot be negative: " + year);
         }
         this.year = year;
         this.title = title;
         this.author = author;
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (o == null || getClass() != o.getClass()) {
-            return false;
-        }
-        Book book = (Book) o;
-        return year == book.year
-                && Objects.equals(title, book.title)
-                && Objects.equals(author, book.author);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(title, author, year);
     }
 }
--- src/main/java/school/faang/bjs2_68674/LibrarySystem.java
+++ src/main/java/school/faang/bjs2_68674/LibrarySystem.java
@@ -3,40 +3,40 @@ package school.faang.bjs2_68674;
 import lombok.extern.slf4j.Slf4j;
 
 import java.util.HashMap;
+import java.util.Map;
 
 @Slf4j
 public class LibrarySystem {
-    HashMap<Book, String> books = new HashMap<>();
+    Map<Book, String> books = new HashMap<>();
 
     public void addBook(String title, String author, int year, String location) {
         Book book = new Book(title, author, year);
-        books.computeIfAbsent(book, k -> location);
+        this.books.put(book, location);
         log.info("Book added: {} in location: {}", book, location);
     }
 
     public void removeBook(String title, String author, int year) {
         Book book = new Book(title, author, year);
-        if (books.containsKey(book)) {
-            books.remove(book);
+        if (this.books.containsKey(book)) {
+            this.books.remove(book);
             log.info("Book removed: {}", book);
         } else {
             log.warn("Attempted to remove book not found in the library: {}", book);
-
         }
     }
 
     public void findBook(String title, String author, int year) {
         Book book = new Book(title, author, year);
-        if (books.containsKey(book)) {
-            log.info("Book: {} found in location: {}", book, books.get(book));
+        if (this.books.containsKey(book)) {
+            log.info("Book: {} found in location: {}", book, this.books.get(book));
         } else {
             log.warn("Book not found: {}", book);
         }
     }
 
     public void printAllBooks() {
-        for (Book book : books.keySet()) {
-            log.info("Printing book: {} in location: {}", book, books.get(book));
+        for (Book book : this.books.keySet()) {
+            log.info("Printing book: {} in location: {}", book, this.books.get(book));
         }
     }
 }
