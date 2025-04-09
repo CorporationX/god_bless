8a559766fadcc3519567fdfc99b8d6e4a13a0cb1
Tanya <hummingbird_t@mail.ru>
2025-04-09 10:43:01 +0300

    Задача BJS2-68059 "Коллекционируем пользователей". Добавила модификаторы доступа, геттеры и др. через аннотацию @Data для доступа к значениям полей. Форматировала код через Ctrl + Alt+ l

--- src/main/java/school/faang/User.java
+++ src/main/java/school/faang/User.java
@@ -1,18 +1,21 @@
 package school.faang;
 
+import lombok.Data;
+
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
 
+@Data
 public class User {
 
-    Long id;
-    String name;
-    int age;
-    Set<String> activities;
+    private Long id;
+    private String name;
+    private int age;
+    private Set<String> activities;
 
-    Map<User, String> findHobbyLovers(List<User> people, Set<String> activities) {
+    public Map<User, String> findHobbyLovers(List<User> people, Set<String> activities) {
 /*     Метод должен в переданном списке пользователей находить тех,
        у кого в ИХ множестве активностей есть хотя бы один элемент из переданного в метод множества активностей.
        Если так, то сохранить этого пользователя в виде ключа в результирующую мапу,
@@ -20,9 +23,9 @@ public class User {
 
         Map<User, String> hobbyLovers = new HashMap<>();
 
-        for (User user: people){
-            for (String activity: activities){
-                if (user.activities.contains(activity)){
+        for (User user : people) {
+            for (String activity : activities) {
+                if (user.getActivities().contains(activity)) {
                     hobbyLovers.put(user, activity);
                     break;
                 }
