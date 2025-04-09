a7c07dd38037d04dd1019f358fb25586210b0a27
Lusivar <jad.fale@mail.ru>
2025-04-09 22:04:07 +0300

    BJS2-68692 refactoring

--- src/main/java/school/faang/bjs2_68677/Book.java
+++ src/main/java/school/faang/bjs2_68677/Book.java
@@ -1,35 +1,18 @@
 package school.faang.bjs2_68677;
 
 import lombok.Builder;
+import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
-
-import java.util.Objects;
 
 @Getter
 @Setter
 @ToString
 @Builder
+@EqualsAndHashCode
 public class Book {
     private String title;
     private String author;
     private int year;
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) {
-            return true;
-        }
-        if (o == null || getClass() != o.getClass()) {
-            return false;
-        }
-        Book book = (Book) o;
-        return year == book.year && Objects.equals(title, book.title) && Objects.equals(author, book.author);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(title, author, year);
-    }
 }
--- src/main/java/school/faang/bjs2_68677/LibrarySystem.java
+++ src/main/java/school/faang/bjs2_68677/LibrarySystem.java
@@ -1,32 +1,28 @@
 package school.faang.bjs2_68677;
 
-import lombok.Setter;
 import lombok.extern.slf4j.Slf4j;
 
 import java.util.HashMap;
 import java.util.Map;
 
-@Setter
 @Slf4j
 public class LibrarySystem {
-    private static final String BOOK_NOT_FIND = "Книга не найдена";
-
     private final Map<Book, String> books = new HashMap<>();
 
     public void addBook(String title, String author, int year, String location) {
-        this.books.put(createBook(title, author, year), location);
+        books.put(createBook(title, author, year), location);
     }
 
     public void removeBook(String title, String author, int year) {
-        this.books.remove(createBook(title, author, year));
+        books.remove(createBook(title, author, year));
     }
 
     public String findBook(String title, String author, int year) {
-        return this.books.getOrDefault(createBook(title, author, year), BOOK_NOT_FIND);
+        return books.getOrDefault(createBook(title, author, year), null);
     }
 
     public void printAllBooks() {
-        this.books.forEach((book, location) -> log.info("Книга {} находится на полке {}", book, location));
+        books.forEach((book, location) -> log.info("Книга {} находится на полке {}", book, location));
     }
 
     private Book createBook(String title, String author, int year) {
