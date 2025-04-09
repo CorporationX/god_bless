05f45ee855169988896babe4dc0709a07cc92261
Likhtin_Nick <kolyangrom2000500@gmail.com>
2025-04-09 16:59:36 +0700

    fix: 1.Вынос запускающего кода в отдельный класс Main. 2. Переопределение toString через библиотеку Lombok. 3. Значение допустимого возраста следует определено через константу. 4. Удаление условия на возраст более 100 лет. 5. Возврат сиска валидных значений реализован через константы.

---
+++ src/main/java/school/faang/Main.java
@@ -0,0 +1,23 @@
+package school.faang;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) {
+        List<User> users = new ArrayList<>();
+        users.add(new User("DONAT", 23, "Uber", "Amsterdam"));
+        users.add(new User("POLAND", 24, "Uber", "London"));
+        users.add(new User("RICKY", 22, "Uber", "Amsterdam"));
+        users.add(new User("ABBY", 19, "Amazon", "Amsterdam"));
+        users.add(new User("DUCK", 100, "Uber", "Amsterdam"));
+        users.add(new User("POLICY", 55, "Google", "New York"));
+        users.add(new User("SAX", 33, "Google", "New York"));
+        users.add(new User("BOLT", 21, "Google", "London"));
+        users.add(new User("QWERTY", 22, "Amazon", "New York"));
+        users.add(new User("ALIC", 18, "Amazon", "New York"));
+        users.add(new User("ALEX", 19, "Amazon", "New York"));
+
+        System.out.println(users);
+    }
+}
--- src/main/java/school/faang/User.java
+++ src/main/java/school/faang/User.java
@@ -1,9 +1,10 @@
 package school.faang;
 
-import java.util.ArrayList;
-import java.util.List;
+import lombok.ToString;
+
 import java.util.Set;
 
+@ToString
 public class User {
     private final String name;
     private final int age;
@@ -12,49 +13,24 @@ public class User {
 
     public static final Set<String> VALID_JOBS = Set.of("Google", "Uber", "Amazon");
     public static final Set<String> VALID_ADDRESSES = Set.of("London", "New York", "Amsterdam");
+    public static final int VALID_AGE = 18;
 
     public User(String name, int age, String job, String address) {
         if (name == null || name.isEmpty()) {
             throw new IllegalArgumentException("Имя не может быть пустым или не заданным");
         }
-        if (age < 18 || age > 100) {
-            throw new IllegalArgumentException("Возраст пользователя не может быть меньше 18");
+        if (age < VALID_AGE) {
+            throw new IllegalArgumentException("Возраст пользователя не может быть меньше" + VALID_AGE);
         }
         if (!VALID_JOBS.contains(job)) {
-            throw new IllegalArgumentException("Место работы должно содержаться в наборе VALID_JOBS.");
+            throw new IllegalArgumentException("Место работы должно содержаться в наборе: " + VALID_JOBS);
         }
         if (!VALID_ADDRESSES.contains(address)) {
-            throw new IllegalArgumentException("Адрес должен содержаться в наборе VALID_ADDRESSES.");
+            throw new IllegalArgumentException("Адрес должен содержаться в наборе: " + VALID_ADDRESSES);
         }
         this.name = name;
         this.age = age;
         this.job = job;
         this.address = address;
-    }
-
-    @Override
-    public String toString() {
-        return String.format(
-                "User[name=%s, age=%d, job=%s, address=%s]",
-                name, age, job, address);
-    }
-
-    public static void main(String[] args) {
-
-        List<User> users = new ArrayList<>();
-        users.add(new User("DONAT", 23, "Uber", "Amsterdam"));
-        users.add(new User("POLAND", 24, "Uber", "London"));
-        users.add(new User("RICKY", 22, "Uber", "Amsterdam"));
-        users.add(new User("ABBY", 19, "Amazon", "Amsterdam"));
-        users.add(new User("DUCK", 100, "Uber", "Amsterdam"));
-        users.add(new User("POLICY", 55, "Google", "New York"));
-        users.add(new User("SAX", 33, "Google", "New York"));
-        users.add(new User("BOLT", 21, "Google", "London"));
-        users.add(new User("QWERTY", 22, "Amazon", "New York"));
-        users.add(new User("ALIC", 18, "Amazon", "New York"));
-        users.add(new User("ALEX", 19, "Amazon", "New York"));
-
-        System.out.println(users);
-
     }
 }
