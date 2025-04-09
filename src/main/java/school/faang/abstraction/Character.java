4c79dec4d4e2da773e0e5a33a6ff1e2cf1b28b2c
AndreyK <you@example.com>
2025-04-09 23:41:08 +0300

    kenig8649: BJS2-68206 абстракция init

---
+++ src/main/java/school/faang/abstraction/Character.java
@@ -0,0 +1,26 @@
+package school.faang.abstraction;
+
+public abstract class Character {
+    private static final int BASE_VOLUME = 5;
+
+    public static int personHealth = 100;
+
+    protected String name;
+    protected int strength;
+    protected int agility;
+    protected int intelligence;
+
+    public Character(String name) {
+        this.name = name;
+        this.strength = BASE_VOLUME;
+        this.agility = BASE_VOLUME;
+        this.intelligence = BASE_VOLUME;
+    }
+
+    public Character(String name, int strength, int agility, int intelligence) {
+        this.name = name;
+        this.strength = strength;
+        this.agility = agility;
+        this.intelligence = intelligence;
+    }
+}
