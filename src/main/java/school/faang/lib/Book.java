6709db03adfa4c740ef52c831d104fc6c57084a4
JolyGolden <nazi00696@mail.ru>
2025-04-09 21:07:57 +0200

    fixes

--- src/main/java/school/faang/lib/Book.java
+++ src/main/java/school/faang/lib/Book.java
@@ -2,30 +2,14 @@ package school.faang.lib;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
-
-import java.util.Objects;
+import lombok.EqualsAndHashCode;
 
 @Data
 @AllArgsConstructor
+@EqualsAndHashCode
 public class Book {
     private String title;
     private String author;
     private int year;
 
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) {
-            return true;
-        }
-        if (o == null || getClass() != o.getClass()) {
-            return false;
-        }
-        Book book = (Book) o;
-        return year == book.year && Objects.equals(title, book.title) && Objects.equals(author, book.author);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(title, author, year);
-    }
 }
