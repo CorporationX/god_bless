75f3e8dc71e598084216bd4c1f7915315f7f9d62
Slava Vermilion <beg.slava@bk.ru>
2025-04-09 12:21:56 +0700

    The task is completed

---
+++ src/main/java/grouping_by_age_BJS2_68142/User.java
@@ -0,0 +1,28 @@
+package grouping_by_age_BJS2_68142;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@Getter
+@AllArgsConstructor
+public class User {
+    private String name;
+    private int age;
+    private String workplace;
+    private String address;
+
+    public static Map<Integer, List<User>> groupUsers(List<User> users){
+        Map<Integer, List<User>> groupedUsersByAge = new HashMap<>();
+        for (User user : users) {
+            int age = user.getAge();
+            groupedUsersByAge.putIfAbsent(age, new ArrayList<>());
+            groupedUsersByAge.get(age).add(user);
+        }
+        return groupedUsersByAge;
+    }
+}
