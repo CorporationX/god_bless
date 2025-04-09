85f72071a5cab54d04cb7449f6869af5829a6caf
Sergey Selischev <ssb22@mail.ru>
2025-04-09 21:07:11 +0300

    небольшие правки

--- src/main/java/school/faang/bjs2_68750/SpellEvent.java
+++ src/main/java/school/faang/bjs2_68750/SpellEvent.java
@@ -1,21 +1,19 @@
 package school.faang.bjs2_68750;
 
-import lombok.AllArgsConstructor;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.ToString;
 
 import java.util.concurrent.atomic.AtomicInteger;
 
-@AllArgsConstructor
-@EqualsAndHashCode
 @Getter
 @ToString
+@EqualsAndHashCode
 public class SpellEvent {
     private static final AtomicInteger idCounter = new AtomicInteger(1);
-    private Integer id;
-    private String eventType;
-    private String action;
+    private final Integer id;
+    private final String eventType;
+    private final String action;
 
     public SpellEvent(String eventType, String action) {
         this.id = createId();
