da3ea8a160516dfac0114a14079af762970e6532
Buga Dan <danbil.942@gmail.com>
2025-04-10 17:16:36 +0300

    Westeros biblioteque task BJS2-68683. Fixed method remove and rewrite method fing book. Also added method Main.

--- src/main/java/westeros/biblioteque/LibrarySystem.java
+++ src/main/java/westeros/biblioteque/LibrarySystem.java
@@ -7,25 +7,28 @@ public class LibrarySystem {
     private Map<Book, String> library = new HashMap<>();
 
     protected void addBook(String title, String author, int year, String location) {
-        library.put(new Book(title, author, year), location);
+        this.library.put(new Book(title, author, year), location);
     }
 
     protected void removeBook(String title, String author, int year) {
-        library.remove(new Book(title, author, year));
+        if (this.library.containsKey(new Book(title, author, year))) {
+            this.library.remove(new Book(title, author, year));
+        } else {
+            System.out.printf("Book named %s is not found in library!", title);
+        }
     }
 
     protected void findBook(String title, String author, int year) {
-        String bookLocation = library.get(new Book(title, author, year));
-
-        if (bookLocation == null) {
-            System.out.printf("Book named %s is not found in library!", title);
-        } else {
+        if (this.library.containsKey(new Book(title, author, year))) {
+            String bookLocation = this.library.get(new Book(title, author, year));
             System.out.printf("Book named %s is located on %s", title, bookLocation);
+        } else {
+            System.out.printf("Book named %s is not found in library!", title);
         }
     }
 
     protected void printAllBooks() {
-        for (Map.Entry<Book, String> bookEntry : library.entrySet()) {
+        for (Map.Entry<Book, String> bookEntry : this.library.entrySet()) {
             Book key = bookEntry.getKey();
             System.out.printf("%s %s %d located on %s %n", key.getTitle(),
                     key.getAuthor(),
