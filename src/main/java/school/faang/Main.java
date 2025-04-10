82d24b8c1fadeb7356376cb8564191f4767fa19c
Likhtin_Nick <kolyangrom2000500@gmail.com>
2025-04-10 13:46:31 +0700

    Исправление

--- src/main/java/school/faang/Main.java
+++ src/main/java/school/faang/Main.java
@@ -6,9 +6,12 @@ public class Main {
     public static void main(String[] args) {
         int[] numbers = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
 
-        int[] reverse = Example.reverse(numbers);
-
-        System.out.println(Arrays.toString(reverse));
+        try {
+            int[] reversed = Example.reverse(numbers);
+            System.out.println("Original: " + Arrays.toString(numbers));
+            System.out.println("Reversed: " + Arrays.toString(reversed));
+        } catch (IllegalArgumentException e) {
+            System.err.println("Error: " + e.getMessage());
+        }
     }
-
 }
