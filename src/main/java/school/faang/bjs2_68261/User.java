cc9f6902c0c744978cc75804f4f50884f3734582
Petr Martyshov <dem9968@gmail.com>
2025-04-10 19:39:14 +0300

    Выполнена задача BJS2-68261 "Группировка пользователей по возрасту"

---
+++ src/main/java/school/faang/bjs2_68261/User.java
@@ -0,0 +1,33 @@
+package school.faang.bjs2_68261;
+
+import lombok.RequiredArgsConstructor;
+import lombok.ToString;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@RequiredArgsConstructor
+@ToString
+public class User {
+    private final String name;
+    private final int age;
+    private final String workplace;
+    private final String address;
+
+    public static Map<Integer, List<User>> groupUsers(List<User> users) {
+        var map = new HashMap<Integer, List<User>>();
+        for (var user : users) {
+
+            if (!map.containsKey(user.age)) {
+                map.put(user.age, new ArrayList<>());
+            }
+
+            map.get(user.age).add(user);
+        }
+
+        return map;
+    }
+
+}
