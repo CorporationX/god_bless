57e40e6d07d1a3dc494fedc87a0f7094c3f6506e
Nikita Nikitin <nnandpartners@gmail.com>
2025-04-10 18:17:08 +0300

    BJS2-68665 fix object creation in main

--- src/main/java/school/faang/bjs2_68662/Main.java
+++ src/main/java/school/faang/bjs2_68662/Main.java
@@ -5,14 +5,14 @@ import java.util.HashSet;
 public class Main {
     public static void main(String[] args) {
         ProductManager manager = new ProductManager(new HashSet<>());
-        manager.addProduct(Category.FOOD, "BigMac");
-        manager.addProduct(Category.FOOD, "Nugget");
-        manager.addProduct(Category.CLOTHING, "Sweater");
-        manager.addProduct(Category.CLOTHING, "Sock");
-        manager.addProduct(Category.ELECTRONICS, "PC");
-        manager.addProduct(Category.ELECTRONICS, "Phone");
-        manager.addProduct(Category.OTHER, "Paints");
-        manager.addProduct(Category.OTHER, "Brushes");
+        manager.addProduct(Category.FOOD, "BigMac", 1);
+        manager.addProduct(Category.FOOD, "Nugget", 2);
+        manager.addProduct(Category.CLOTHING, "Sweater", 3);
+        manager.addProduct(Category.CLOTHING, "Sock", 4);
+        manager.addProduct(Category.ELECTRONICS, "PC", 5);
+        manager.addProduct(Category.ELECTRONICS, "Phone", 6);
+        manager.addProduct(Category.OTHER, "Paints", 7);
+        manager.addProduct(Category.OTHER, "Brushes", 8);
         manager.printAllProducts();
 
         System.out.println("-------GETTING PRODUCT BY CATEGORY-------");
