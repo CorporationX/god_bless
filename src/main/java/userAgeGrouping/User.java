99a954ec0b7835be4937cfd1b49127cbf22c0612
Emil Dzheenbaev <emil.dzheenbaev@gmail.com>
2025-04-10 21:28:32 +0200

    опять это честайл)) 4

--- src/main/java/userAgeGrouping/User.java
+++ src/main/java/userAgeGrouping/User.java
@@ -7,8 +7,6 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.function.Function;
-import java.util.stream.Collectors;
 
 @Data
 @AllArgsConstructor
