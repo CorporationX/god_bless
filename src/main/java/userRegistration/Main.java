dd94d3a11a759a6df22e6d2012435b6c7c9c4e89
Emil Dzheenbaev <emil.dzheenbaev@gmail.com>
2025-04-10 21:30:17 +0200

    опять это честайл)) 5

--- src/main/java/userRegistration/Main.java
+++ src/main/java/userRegistration/Main.java
@@ -2,7 +2,7 @@ package userRegistration;
 
 public class Main {
     public static void main(String[] args) {
-        User user = new User("Neymar", 30, "Santos FC", "some Street in Brazil");
+        User user = new User("Neymar", 30, "Santos FC", " some Street in Brazil");
 
     }
 }
