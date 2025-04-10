4cde5b2c46879d4c438237b670898da92a60b088
Emil Dzheenbaev <emil.dzheenbaev@gmail.com>
2025-04-10 21:13:53 +0200

    опять этот чекстайл))

--- src/main/java/userAgeGrouping/Main.java
+++ src/main/java/userAgeGrouping/Main.java
@@ -7,7 +7,7 @@ import java.util.List;
 public class Main {
     public static void main(String[] args) {
         List<User> usersList = new ArrayList<>(Arrays.asList(
-                new User("Frodo", 18,"Shire", "Shire street 12"),
+                new User("Frodo", 18, "Shire", "Shire street 12"),
                 new User("Gimli", 120, "Underway", "Ered Luin"),
                 new User("Aragorn", 70, "Gondor", "Gondor"),
                 new User("Sam", 18, "Shire", "Shire street 15"),
