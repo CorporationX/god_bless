199388c9f1a004f0cbf5fcd5b5164e155215713c
Ramil Burgart <ramilburgart7006@gmail.com>
2025-04-10 20:11:44 +0300

    added package for task number 68301

---
+++ src/main/java/school/faang/groupByAge/User.java
@@ -0,0 +1,37 @@
+package school.faang.groupByAge;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@ToString
+public class User {
+
+    private String name;
+    private int age;
+    private String work;
+    private String address;
+
+    public static Map<Integer, List<User>> groupUserByAge(int ageUser, List<User> userAgeList){
+        Map<Integer,List<User>> sortedUserByAgeMap = new HashMap<>();
+        List<User> ageList = new ArrayList<>();
+
+        for(User userAge : userAgeList){
+            if(ageUser == userAge.getAge()){
+                ageList.add(userAge);
+                sortedUserByAgeMap.put(ageUser, ageList);
+            }
+        }
+
+        return sortedUserByAgeMap;
+    }
+}
