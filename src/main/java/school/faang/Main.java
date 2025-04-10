cab7068a7f053578c6923d3fcec863849a26e93f
Likhtin_Nick <kolyangrom2000500@gmail.com>
2025-04-10 13:33:39 +0700

    Выполнение задания Развернитесь! "На проверку"

--- src/main/java/school/faang/Main.java
+++ src/main/java/school/faang/Main.java
@@ -1,5 +1,14 @@
 package school.faang;
 
-public class Main{
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) {
+        int[] numbers = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+
+        int[] reverse = Example.reverse(numbers);
+
+        System.out.println(Arrays.toString(reverse));
+    }
 
 }
