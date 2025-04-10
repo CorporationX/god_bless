8b7181fdc423eb103eac8f89146aa3b666b8ccac
Emil Dzheenbaev <emil.dzheenbaev@gmail.com>
2025-04-10 19:32:53 +0200

    Регистрация пользователя решаю эту задачу

---
+++ src/main/java/userRegistration/Main.java
@@ -0,0 +1,8 @@
+package userRegistration;
+
+public class Main {
+    public static void main(String[] args) {
+        User user = new User("Neymar", 30, "Santos FC", "some Street in Brazil");
+
+    }
+}
