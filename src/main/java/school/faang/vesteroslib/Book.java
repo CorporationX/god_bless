7a1cad5d16304dd3cb6db5aa41e8fa2cb8d6251e
Aleksandr <alexandr.sidorkin@gmail.com>
2025-04-11 23:25:11 +0200

    Grammar style sutisfier

--- src/main/java/school/faang/vesteroslib/Book.java
+++ src/main/java/school/faang/vesteroslib/Book.java
@@ -14,8 +14,12 @@ public class Book {
 
     @Override
     public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
         Book book = (Book) o;
         return year == book.year && title.equals(book.title) && author.equals(book.author);
     }
