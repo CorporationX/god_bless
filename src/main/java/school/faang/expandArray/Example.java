578d37f59fe8d87860ef88803149c6e51e957b61
Vladislav Kozlov <littlerock2877@mail.ru>
2025-04-11 20:15:07 +0300

    add: array reversing

---
+++ src/main/java/school/faang/expandArray/Example.java
@@ -0,0 +1,23 @@
+package school.faang.expandArray;
+
+import lombok.extern.slf4j.Slf4j;
+
+import java.util.Comparator;
+import java.util.stream.IntStream;
+
+@Slf4j
+public class Example {
+    public static void main(String[] args) {
+        int[] array = new int[]{5, 4, 3, 2, 1, 0};
+        log.info("Original array - [{}]", array);
+        log.info("Reversed array - [{}]", reverse(array));
+    }
+
+    private static int[] reverse(int[] nums) {
+        return IntStream.of(nums)
+                .boxed()
+                .sorted(Comparator.reverseOrder())
+                .mapToInt(Integer::intValue) // обратно в int
+                .toArray();
+    }
+}
