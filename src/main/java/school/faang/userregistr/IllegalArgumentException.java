4610e99fc47ae6aa1ea9e9b4685ff24833ae9f06
pirojok_yt <tamik34@mail.ru>
2025-04-11 17:27:26 +0500

    User registration done

--- src/main/java/school/faang/userregistr/IllegalArgumentException.java
+++ src/main/java/school/faang/userregistr/IllegalArgumentException.java
@@ -1,6 +1,6 @@
 package school.faang.userregistr;
 
-public class IllegalArgumentException extends Throwable{
+public class IllegalArgumentException extends Throwable {
     public IllegalArgumentException(String message) {
         super(message);
     }
