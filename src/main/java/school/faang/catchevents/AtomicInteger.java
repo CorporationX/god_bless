1312e57d08f40313ab08fc0e61090584a325fa15
Fagan Gafulov <gafulov@yandex.ru>
2025-04-11 15:38:57 +0300

    BJS2-68673 fix style

--- src/main/java/school/faang/catchevents/AtomicInteger.java
+++ src/main/java/school/faang/catchevents/AtomicInteger.java
@@ -3,7 +3,7 @@ package school.faang.catchevents;
 public class AtomicInteger {
     private static int id = 0;
 
-    public static int getId () {
+    public static int getId() {
         return id++;
     }
 }
