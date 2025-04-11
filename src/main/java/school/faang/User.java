427bf84288cc56571d839638c09c789d7e5d0d30
Tanya <hummingbird_t@mail.ru>
2025-04-11 17:39:11 +0300

    Задача "Группировка пользователей по возрасту" 68328 готова с тестовым мейном

--- src/main/java/school/faang/User.java
+++ src/main/java/school/faang/User.java
@@ -1,4 +1,37 @@
 package school.faang;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+
+@Data
+@AllArgsConstructor
 public class User {
+
+    private String name;
+    private int age;
+    private String placeOfWork;
+    private String adress;
+
+    public static Map<Integer, List<User>> groupUsers(List<User> people) {
+        Map<Integer, List<User>> usersGroupedByAge = new HashMap<>();
+        for (User user : people) {
+            int userAge = user.age;
+            if (usersGroupedByAge.containsKey(userAge)) {
+                List<User> peers = usersGroupedByAge.get(userAge);
+                peers.add(user);
+                usersGroupedByAge.put(userAge, peers);
+            } else {
+                List<User> peers = new ArrayList<>();
+                peers.add(user);
+                usersGroupedByAge.put(userAge, peers);
+            }
+        }
+        return usersGroupedByAge;
+    }
 }
