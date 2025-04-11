79abac1a0b4e5405fc53c79e72800534c7b564f8
Ilyas Kystaubayev <kystaubaev.iliyas@mail.ru>
2025-04-11 17:15:10 +0500

    .

---
+++ src/main/java/school/faang/BJS2_68253/Warrior.java
@@ -0,0 +1,19 @@
+package school.faang.BJS2_68253;
+
+public class Warrior extends Character {
+
+    public static final int AGILITY = 5;
+    public static final int INTELLIGENCE = 3;
+    public static final int STRENGTH = 10;
+
+    public Warrior(int agility, int intelligence, int strength, String name) {
+        super(AGILITY, INTELLIGENCE, STRENGTH, name);
+    }
+
+    @Override
+    void attack(Character opponent) {
+        opponent.setHealth(opponent.getHealth() - getAgility());
+        System.out.println(getName() + " attacks " + opponent.getName() + " and deals " +
+                getAgility() + " damage ");
+    }
+}
