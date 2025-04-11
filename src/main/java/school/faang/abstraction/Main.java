bafa0484c08e8c6bab46de3698ac7ac489c14282
pirojok_yt <tamik34@mail.ru>
2025-04-11 21:34:26 +0500

    User registration done

--- src/main/java/school/faang/abstraction/Main.java
+++ src/main/java/school/faang/abstraction/Main.java
@@ -4,11 +4,5 @@ public class Main {
     public static void main(String[] args) {
         Character warrior = new Warrior("король");
         Character archer = new Archer("лучник");
-
-        for(int i = 0; i < 11; i++)
-            archer.attack(warrior);
-        System.out.println(warrior.health);
-        warrior.attack(archer);
-        System.out.println((archer.health));
     }
 }
