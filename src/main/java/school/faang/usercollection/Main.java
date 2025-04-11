9accd3cb474a5279f1eabc30a3f69a0ac7479635
AndreyK <you@example.com>
2025-04-11 23:06:21 +0300

    kenig8649: BJS2-68083 Коллекционируем пользователей

---
+++ src/main/java/school/faang/usercollection/Main.java
@@ -0,0 +1,27 @@
+package school.faang.usercollection;
+
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
+public class Main {
+    public static void main(String[] args) {
+        Set<String> activities1 = new HashSet<>(Arrays.asList("Футбол", "Шахматы"));
+        User user1 = new User(1, "Iван", 25, activities1);
+
+        Set<String> activities2 = new HashSet<>(Arrays.asList("Плавание", "Теннис"));
+        User user2 = new User(2, "Ольга", 30, activities2);
+
+        Map<User, String> result = User.findHobbyLovers(
+                List.of(user1, user2),
+                Set.of("Теннис", "Шахматы")
+        );
+
+        System.out.println("Вот пользователи, которые выбрали наши активности:");
+        for (Map.Entry<User, String> entry : result.entrySet()) {
+            System.out.printf("Пользователь: %s; Активность - %s%n", entry.getKey().getName(), entry.getValue());
+        }
+    }
+}
