4779d9606ff32f544327d2fe394d4d885e04bd60
pirojok_yt <tamik34@mail.ru>
2025-04-11 17:16:44 +0500

    User registration done

---
+++ src/main/java/school/faang/IllegalArgumentException.java
@@ -0,0 +1,7 @@
+package school.faang;
+
+public class IllegalArgumentException extends Throwable{
+    public IllegalArgumentException(String message) {
+        super(message);
+    }
+}
--- src/main/java/school/faang/Main.java
+++ src/main/java/school/faang/Main.java
@@ -1,5 +1,10 @@
 package school.faang;
 
-public class Main{
-
+public class Main {
+    public static void main(String[] args) {
+        User user = new User("", 19, "Google", "New York");
+        User user1 = new User("Kirill", 16, "Google", "New York");
+        User user2 = new User("Grey", 19, "Dodo", "New York");
+        User user3 = new User("Anton", 19, "Google", "Almaty");
+    }
 }
