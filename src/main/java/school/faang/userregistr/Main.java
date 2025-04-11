44e98318c3abcd640f40563f3197dc378913bdf2
pirojok_yt <tamik34@mail.ru>
2025-04-11 18:30:03 +0500

    User registration done

--- src/main/java/school/faang/userregistr/Main.java
+++ src/main/java/school/faang/userregistr/Main.java
@@ -1,7 +1,7 @@
 package school.faang.userregistr;
 
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IllegalArgumentException {
         User user = new User("", 19, "Google", "New York");
         User user1 = new User("Kirill", 16, "Google", "New York");
         User user2 = new User("Grey", 19, "Dodo", "New York");
--- src/main/java/school/faang/userregistr/User.java
+++ src/main/java/school/faang/userregistr/User.java
@@ -18,24 +18,22 @@ public class User {
             new ArrayList<>(Arrays.asList("Google", "Uber", "Amazon"));
     private static ArrayList<String> VALID_ADDRESSES =
             new ArrayList<>(Arrays.asList("London", "New York", "Amsterdam"));
+    private static int VALID_AGE = 18;
 
-    public User(String name, int age, String job, String address) {
+    public User(String name, int age, String job, String address) throws IllegalArgumentException {
+        if (name.isBlank()) {
+            throw new IllegalArgumentException("Поле имя не может быть пустым");
+        } else if (age < VALID_AGE) {
+            throw new IllegalArgumentException("Возрастное ограничение 18+");
+        } else if (!VALID_JOBS.contains(job)) {
+            throw new IllegalArgumentException("Некорректное место работы");
+        } else if (!VALID_ADDRESSES.contains(address)) {
+            throw new IllegalArgumentException("Некорректный адрес");
+        }
+
         this.name = name;
         this.age = age;
         this.job = job;
         this.address = address;
-        try {
-            if (name.isEmpty()) {
-                throw new IllegalArgumentException("Поле имя не может быть пустым");
-            } else if (age < 18) {
-                throw new IllegalArgumentException("Возрастное ограничение 18+");
-            } else if (!VALID_JOBS.contains(job)) {
-                throw new IllegalArgumentException("Некорректное место работы");
-            } else if (!VALID_ADDRESSES.contains(address)) {
-                throw new IllegalArgumentException("Некорректный адрес");
-            }
-        } catch (IllegalArgumentException e) {
-            e.printStackTrace();
-        }
     }
 }
