e44264193011f8a3a8063765384937deef1025fb
Tanya <hummingbird_t@mail.ru>
2025-04-11 16:56:08 +0300

    В какой-то из инструкций-видеороликов говорилось, что пока мы создаём "тесты" для проверки в мейне. Создала для задачи тестовый мейн. Убрала всё из основного мейна во избежание конфликтов

--- src/main/java/school/faang/Main.java
+++ src/main/java/school/faang/Main.java
@@ -1,22 +1,6 @@
 package school.faang;
 
-import java.util.Arrays;
-import java.util.List;
-import java.util.Map;
 
 public class Main {
-    public static void main(String[] args) {
 
-        List<User> people = Arrays.asList(
-                new User("Emy", 18, "somewhere", "somewhere"),
-                new User("Penny", 18, "somewhere", "somewhere"),
-                new User("Bernadette", 19, "somewhere", "somewhere"),
-                new User("Leslie Winkle", 25, "somewhere", "somewhere"));
-
-        Map<Integer, List<User>> usersGroupedByAge = User.groupUsers(people);
-        for (Map.Entry<Integer, List<User>> entry : usersGroupedByAge.entrySet()) {
-            System.out.println(entry);
-
-        }
-    }
 }
