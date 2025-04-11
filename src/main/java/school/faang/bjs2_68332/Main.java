a87a38290a140f6fb54e08cf4af242fe071950f1
Petr Martyshov <dem9968@gmail.com>
2025-04-11 10:28:28 +0300

    Исправлены замечания

--- src/main/java/school/faang/bjs2_68332/Main.java
+++ src/main/java/school/faang/bjs2_68332/Main.java
@@ -5,13 +5,19 @@ package school.faang.bjs2_68332;
  */
 public class Main {
     public static void main(String[] args) {
+        registerUser("Петя", 18, "Google", "New York");
+        registerUser("Коля", 45, "Google", "Москва");
+        registerUser("Вася", 13, "Школьник", "London");
+        registerUser("Дима", 22, "Сбер", "Amsterdam");
+    }
+
+    public static void registerUser(String name, int age, String job, String address) {
         try {
-            var user = new User("Петя", 18, "Сбер", "London"); //кидает Exception
-            //var user = new User("Петя", 18, "Google", "London"); //проходит валидацию
+            var user = new User(name, age, job, address);
 
             System.out.println(user);
         } catch (Exception e) {
-            System.out.println("Ошибка валидации пользователя: " + e.getMessage());
+            System.out.printf("Ошибка валидации пользователя: %s%n", e.getMessage());
         }
     }
 }
