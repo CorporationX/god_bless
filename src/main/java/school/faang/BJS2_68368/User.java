4852ecf141d8605a2a63b16c69866d303f4b8af0
Tanya <hummingbird_t@mail.ru>
2025-04-12 00:49:48 +0300

    Надеюсь, правильно использовала статические константы. Задача BJS2-68368 "Регистрация пользователя" готова

---
+++ src/main/java/school/faang/BJS2_68368/User.java
@@ -0,0 +1,36 @@
+package school.faang.BJS2_68368;
+
+import lombok.Data;
+
+import java.util.Set;
+
+@Data
+public class User {
+
+    public static final Set<String> VALID_JOBS = Set.of("Google", "Uber", "Amazon");
+    public static final Set<String> VALID_ADDRESSES = Set.of("London", "New York", "Amsterdam");
+
+    private String name;
+    private int age;
+    private String job;
+    private String address;
+
+    public User(String name, int age, String job, String address) {
+        if (name == null || name.trim().isEmpty()) {
+            throw new IllegalArgumentException("Имя не может быть пустым.");
+        }
+        if (age < 18) {
+            throw new IllegalArgumentException("Возраст не может быть меньше 18.");
+        }
+        if (!VALID_JOBS.contains(job)) {
+            throw new IllegalArgumentException("Место работы должно содержаться в наборе VALID_JOBS.");
+        }
+        if (!VALID_ADDRESSES.contains(address)) {
+            throw new IllegalArgumentException("Адрес должен содержаться в наборе VALID_ADDRESSES.");
+        }
+        this.name = name;
+        this.age = age;
+        this.job = job;
+        this.address = address;
+    }
+}
