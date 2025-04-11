2fbfd64ae36ad10f3bd579ac0e1994aaa9c7b24d
Vladislav Kozlov <littlerock2877@mail.ru>
2025-04-11 23:34:28 +0300

    fix: pr comments

--- src/main/java/school/faang/expandArray/Example.java
+++ src/main/java/school/faang/expandArray/Example.java
@@ -1,19 +1,10 @@
 package school.faang.expandArray;
 
-import lombok.extern.slf4j.Slf4j;
-
 import java.util.Comparator;
 import java.util.stream.IntStream;
 
-@Slf4j
 public class Example {
-    public static void main(String[] args) {
-        int[] array = new int[]{5, 4, 3, 2, 1, 0};
-        log.info("Original array - [{}]", array);
-        log.info("Reversed array - [{}]", reverse(array));
-    }
-
-    private static int[] reverse(int[] nums) {
+    public static int[] reverse(int[] nums) {
         return IntStream.of(nums)
                 .boxed()
                 .sorted(Comparator.reverseOrder())
