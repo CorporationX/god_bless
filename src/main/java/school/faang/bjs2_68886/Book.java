9dbccb13a51a493edc2d1d56b32f300a6d918156
Valentin Lezhnin <v888.rus@gmail.com>
2025-04-11 08:12:51 +0200

    Changes before implementing lombok EqualsAndHashCode

--- src/main/java/school/faang/bjs2_68886/Book.java
+++ src/main/java/school/faang/bjs2_68886/Book.java
@@ -1,12 +1,14 @@
 package school.faang.bjs2_68886;
 
 import lombok.AllArgsConstructor;
+//import lombok.EqualsAndHashCode;
 import lombok.Getter;
 
 import java.util.Objects;
 
 @Getter
 @AllArgsConstructor
+//@EqualsAndHashCode
 public class Book {
     private String title;
     private String author;
