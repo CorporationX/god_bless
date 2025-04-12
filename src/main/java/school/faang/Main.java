29356ed2d7a18bc05fdc74cfb97351e7e6f65229
Gigabyte <012_erasyl@mail.ru>
2025-04-12 23:17:42 +0500

    Abstract Commit

--- src/main/java/school/faang/Main.java
+++ src/main/java/school/faang/Main.java
@@ -2,8 +2,8 @@ package school.faang;
 
 public class Main {
     public static void main(String[] args) {
-        Character warrior = new Warrior("Gojo");
-        Character archer = new Archer("Sukuna");
+        Character warrior = new Warrior("Thorin");
+        Character archer = new Archer("Legolas");
 
         System.out.printf("%s HP: %d%n", warrior.getName(), warrior.getHealth());
         System.out.printf("%s HP: %d%n", archer.getName(), archer.getHealth());
