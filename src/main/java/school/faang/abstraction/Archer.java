3e32c6ae5850a622f543893d2f4c6c95a588b2f9
Tanya <hummingbird_t@mail.ru>
2025-04-12 21:17:25 +0300

    убрала путь из наименования класса-родителя

--- src/main/java/school/faang/abstraction/Archer.java
+++ src/main/java/school/faang/abstraction/Archer.java
@@ -1,6 +1,6 @@
 package school.faang.abstraction;
 
-public class Archer extends school.faang.abstraction.Character {
+public class Archer extends Character {
     public Archer(String name) {
         super(name, 3, 10, 5);
     }
--- src/main/java/school/faang/abstraction/Warrior.java
+++ src/main/java/school/faang/abstraction/Warrior.java
@@ -1,6 +1,6 @@
 package school.faang.abstraction;
 
-public class Warrior extends school.faang.abstraction.Character {
+public class Warrior extends Character {
 
     public Warrior(String name) {
         super(name, 10, 5, 3);
