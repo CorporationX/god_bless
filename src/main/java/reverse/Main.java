a6689afb17fb950b3ad79758b79dbcb46e851897
Emil Dzheenbaev <emil.dzheenbaev@gmail.com>
2025-04-12 19:10:12 +0200

    поправка задачи Развернитесь!

--- src/main/java/reverse/Main.java
+++ src/main/java/reverse/Main.java
@@ -5,7 +5,7 @@ import java.util.Arrays;
 public class Main {
     public static void main(String[] args) {
         Example example = new Example();
-        int[] digits = new int[]{1,2,3,4,5};
+        int[] digits = new int[]{1, 2, 3, 4, 5};
 
         example.reverse(digits);
         System.out.println(Arrays.toString(digits));
