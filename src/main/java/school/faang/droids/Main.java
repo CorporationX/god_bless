45c6d97067dc0e1966d3ca46ed27732f417980a7
Kirill Frolov <frolov.kirill.17@yandex.ru>
2025-04-13 01:57:45 +0300

    Task BJS2-70285
    1) Droid class added
    2) DroidMessageEncryptor functional interface added

---
+++ src/main/java/school/faang/droids/Main.java
@@ -0,0 +1,28 @@
+package school.faang.droids;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        // Создаём трёх дроидов
+        Droid r2d2 = new Droid("R2D2");
+        Droid c3po = new Droid("C3PO");
+        Droid bb8 = new Droid("BB-8");
+
+        // Оригинальные сообщения
+        String message1 = "Join the Rebellion!";
+        String message2 = "The Death Star is operational!";
+        String message3 = "The mission is complete.";
+        int encryptionKey1 = 3; // Ключ шифрования для первого сообщения
+        int encryptionKey2 = 7; // Ключ шифрования для второго сообщения
+
+        // R2D2 отправляет сообщение C3PO
+        r2d2.sendMessage(c3po, message1, encryptionKey1);
+
+        // C3PO отвечает R2D2
+        c3po.sendMessage(r2d2, message2, encryptionKey2);
+
+        // BB-8 отправляет сообщение C3PO
+        bb8.sendMessage(c3po, message3, encryptionKey1);
+    }
+}
