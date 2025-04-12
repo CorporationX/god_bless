5977b103a484f652755e6d47eabbd8d4de3c6507
Kirill <kirill551@yandex.ru>
2025-04-12 18:05:43 +0200

    "Style"

--- src/main/java/school/faang/meta/Main.java
+++ src/main/java/school/faang/meta/Main.java
@@ -3,18 +3,27 @@ package school.faang.meta;
 public class Main {
     public static void main(String[] args) {
         NotificationManager notificationManager = new NotificationManager();
-        notificationManager.registerHandler(NotificationType.EMAIL, notification -> System.out.println("here we send email to user with a message: " + notification.getMessage()));
-        notificationManager.registerHandler(NotificationType.SMS, notification -> System.out.println("here we send sms to user with a message: " + notification.getMessage()));
-        notificationManager.registerHandler(NotificationType.PUSH, notification -> System.out.println("here we send PUSH notification to user with a message: " + notification.getMessage()));
+        notificationManager.registerHandler(NotificationType.EMAIL, notification ->
+                System.out.println("here we send email to user with a message: " + notification.getMessage()));
+        notificationManager.registerHandler(NotificationType.SMS, notification ->
+                System.out.println("here we send sms to user with a message: " + notification.getMessage()));
+        notificationManager.registerHandler(NotificationType.PUSH, notification ->
+                System.out.println("here we send PUSH notification to user with a message: " + notification.getMessage()));
 
-        Notification emailNotification = new Notification(NotificationType.EMAIL, "Your notification #1 sent by email");
-        Notification smsNotification = new Notification(NotificationType.SMS, "Your notification #2 sent by sms");
-        Notification pushNotification = new Notification(NotificationType.PUSH, "Your notification #3 sent by PUSH");
-        Notification notValidEmailNotification = new Notification(NotificationType.EMAIL, "Your notification sent by email");
+        Notification emailNotification = new Notification(NotificationType.EMAIL,
+                "Your notification #1 sent by email");
+        Notification smsNotification = new Notification(NotificationType.SMS,
+                "Your notification #2 sent by sms");
+        Notification pushNotification = new Notification(NotificationType.PUSH,
+                "Your notification #3 sent by PUSH");
+        Notification notValidEmailNotification = new Notification(NotificationType.EMAIL,
+                "Your notification sent by email");
         Notification emptyEmailNotification = new Notification(NotificationType.EMAIL, "");
 
-        notificationManager.registerFilter(NotificationType.EMAIL, notification -> notification.getMessage().contains("#"));
-        notificationManager.registerFilter(NotificationType.EMAIL, notification -> !notification.getMessage().isBlank());
+        notificationManager.registerFilter(NotificationType.EMAIL, notification ->
+                notification.getMessage().contains("#"));
+        notificationManager.registerFilter(NotificationType.EMAIL, notification ->
+                !notification.getMessage().isBlank());
 
         notificationManager.sendNotification(emailNotification);
         notificationManager.sendNotification(smsNotification);
