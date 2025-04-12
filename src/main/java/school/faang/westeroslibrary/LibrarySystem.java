7cd8879a63fc68d54ac8e2f90cfb772e293be664
AndreyK <you@example.com>
2025-04-12 13:25:29 +0300

    kenig8649: BJS2-68704 - fix согласно замечаниям из ПР

--- src/main/java/school/faang/westeroslibrary/LibrarySystem.java
+++ src/main/java/school/faang/westeroslibrary/LibrarySystem.java
@@ -1,18 +1,15 @@
 package school.faang.westeroslibrary;
 
-import lombok.Data;
-
 import java.util.HashMap;
 import java.util.Map;
 
-@Data
 public class LibrarySystem {
-    private final HashMap<Book, String> map = new HashMap<>();
+    private final HashMap<Book, String> library = new HashMap<>();
 
     public void addBook(String title, String author, int year, String location) {
         Book newBook = new Book(title, author, year);
-        if (!map.containsKey(newBook)) {
-            map.put(newBook, location);
+        if (!library.containsKey(newBook)) {
+            library.put(newBook, location);
             System.out.printf("Книга '%s' добавлена на полку %s\n", title, location);
         } else {
             System.out.printf("Ошибка: книга '%s' уже существует в системе\n", title);
@@ -21,7 +18,7 @@ public class LibrarySystem {
 
     public void removeBook(String title, String author, int year) {
         Book book = new Book(title, author, year);
-        if (map.remove(book) != null) {
+        if (library.remove(book) != null) {
             System.out.printf("Книга '%s' удалена из системы\n", title);
         } else {
             System.out.printf("Ошибка: книга '%s' не найдена\n", title);
@@ -30,17 +27,17 @@ public class LibrarySystem {
 
     public String findBook(String title, String author, int year) {
         Book book = new Book(title, author, year);
-        return map.getOrDefault(book, "Книга не найдена");
+        return library.getOrDefault(book, null);
     }
 
     public void printAllBooks() {
-        if (map.isEmpty()) {
+        if (library.isEmpty()) {
             System.out.println("Библиотека пуста");
             return;
         }
 
         System.out.println("\nСписок всех книг:");
-        for (Map.Entry<Book, String> entry : map.entrySet()) {
+        for (Map.Entry<Book, String> entry : library.entrySet()) {
             Book book = entry.getKey();
             System.out.printf("▸ %s | Автор: %s | Год: %d | Полка: %s\n",
                     book.getTitle(), book.getAuthor(), book.getYear(), entry.getValue());
