4e064e4c4ebcec390f5c3c1d3a975b9d7b935fb4
DernovskiyAnton <dernovski@icloud.com>
2025-04-12 16:50:51 +0700

    fix comments changed variable listOfMatches on result deleted empty lines

--- src/main/java/BJS2_69069/Main.java
+++ src/main/java/BJS2_69069/Main.java
@@ -7,7 +7,7 @@ public class Main {
             User user1 = new User("Антон", 45, "Uzum", "Novosibirsk");
             User user2 = new User("Петя", 20, "Uber", "Amsterdam");
             User user3 = new User("Саша", 15, "Uber", "Novosibirsk");
-        } catch (IllegalArgumentException e){
+        } catch (IllegalArgumentException e) {
             System.out.println("Ошибка при создании пользователя: " + e.getMessage());
         }
     }
