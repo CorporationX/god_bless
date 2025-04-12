7c24cb2f8fa5ff33972ddd3f9d80dd6f13bde6f9
Hitow22 <Hitow22@gmail.com>
2025-04-12 12:43:42 +0300

    Коллекционируем пользователей

---
+++ src/main/java/school/faang/collect_users/User.java
@@ -0,0 +1,34 @@
+package school.faang.collect_users;
+
+import lombok.Data;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
+@Data
+public class User {
+    private int id;
+    private String name;
+    private int age;
+    private Set<String> userHobbies;
+
+    public User(int id, String name, int age, Set<String> userHobbies) {
+        this.id = id;
+        this.name = name;
+        this.age = age;
+        this.userHobbies = userHobbies;
+    }
+
+    public static Map<User, String> findHobbyLovers(List<User> users, Set<String> hobbies) {
+        Map<User, String> hobbyLovers = new HashMap<>();
+        for (User user : users) {
+            for (String hobby : user.userHobbies) {
+                if (hobbies.contains(hobby)) {
+                    hobbyLovers.put(user, hobby);
+                }
+            }
+        }
+        return hobbyLovers;
+    }
+}
