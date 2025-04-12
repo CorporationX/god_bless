0efea7cdd5b0cc49875c7afbf9fb5e621dd8014d
Aleksandr <alexandr.sidorkin@gmail.com>
2025-04-12 10:24:19 +0200

    lastIndexHolder - I've made it static first, however another manager could have it's own index
    Main class with inner logic commit.

--- src/main/java/school/faang/amazonwarehouse/ProductManager.java
+++ src/main/java/school/faang/amazonwarehouse/ProductManager.java
@@ -1,43 +1,61 @@
 package school.faang.amazonwarehouse;
 
-import lombok.RequiredArgsConstructor;
-
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
 import java.util.Set;
 
-@RequiredArgsConstructor
 public class ProductManager {
-    private final Set<Product> products;
+    private int lastIndexHolder;
+    private final Set<Product> products = new HashSet<>();
+    private final Map<Category, List<Product>> productsByCategory = new HashMap<>();
 
-    //: создает новый продукт с указанной категорией и именем, добавляет его в множество продуктов products.
     public Product addProduct(Category category, String name) {
+        Product product = new Product(nextIndex(), name, category);
+        products.add(product);
+        productsByCategory.computeIfAbsent((category), k -> new ArrayList<>());
+        productsByCategory.get(category).add(product);
+        return product;
     }
 
-    //: находит продукт с указанным именем в указанной категории и удаляет его из множества продуктов products.
-    removeProduct(Category category, String name) {
+    public boolean removeProduct(Category category, String name) {
+        List<Product> productsOfCategory = productsByCategory.get(category);
+        Optional<Product> optProduct = productsOfCategory
+                .stream()
+                .filter(p -> p.getName().equals(name))
+                .findFirst();
+        if (optProduct.isPresent()) {
+            Product product = optProduct.get();
+            productsOfCategory.remove(product);
+            return products.remove(product);
+        }
+        return false;
     }
 
-    //: возвращает список всех продуктов в указанной категории. Если категория отсутствует, метод возвращает пустой список.
-    findProductsByCategory(Category category) {
+    public List<Product> findProductsByCategory(Category category) {
+        return productsByCategory.getOrDefault(category, Collections.emptyList());
     }
 
-    //: возвращает Map<Category, List<Product>> , где ключом выступает категория,
-    // а значением — список продуктов, принадлежащих этой категории.
-    // Для этого необходимо сгруппировать все текущие продукты из множества products по их категориям.
-    groupProductsByCategory() {
+    public Map<Category, List<Product>> groupProductsByCategory() {
+        return productsByCategory;
     }
 
-    /*  Категория: FOOD
-        Продукты:
-        - Apple
-        - Bread
+    public void printAllProducts() {
+        for (Map.Entry<Category, List<Product>> productsOfCategory : productsByCategory.entrySet()) {
+            System.out.printf("Категория: %s \nПродукты:\n%s", productsOfCategory.getKey());
+            for (Product product : productsOfCategory.getValue()) {
+                System.out.println("- " + product.getName());
+            }
+            System.out.println();
+        }
+    }
 
-        Категория: ELECTRONICS
-        Продукты:
-        - Laptop
-        - Smartphone
-        */
-    printAllProducts() {
-
+    private int nextIndex() {
+        return lastIndexHolder++;
     }
 
 }
