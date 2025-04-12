0ff1841ab1241a47261186188e5b82861503638b
Kirill <kirill551@yandex.ru>
2025-04-12 19:28:35 +0200

    "Выполнить задачу Gmail Rich Filters"

---
+++ src/main/java/school/faang/gmail_reach_filters/EmailProcessor.java
@@ -0,0 +1,16 @@
+package school.faang.gmail_reach_filters;
+
+import java.util.List;
+import java.util.function.Consumer;
+import java.util.function.Function;
+import java.util.function.Predicate;
+
+public class EmailProcessor {
+    public List<Email> processEmails(List<Email> emails, Predicate<Email> filter, Function<Email, String> function, Consumer<Email> consumer) {
+        emails.forEach(consumer);
+        return emails.stream()
+                .filter(filter)
+                .peek(email -> email.setBody(function.apply(email)))
+                .toList();
+    }
+}
---
+++ src/main/java/school/faang/gmail_reach_filters/Main.java
@@ -0,0 +1,26 @@
+package school.faang.gmail_reach_filters;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.function.Consumer;
+import java.util.function.Function;
+import java.util.function.Predicate;
+
+public class Main {
+    public static void main(String[] args) {
+        EmailProcessor emailProcessor = new EmailProcessor();
+
+        List<Email> emails = Arrays.asList(
+                new Email("Письмо 1", "Текст письма 1", true),
+                new Email("Письмо 2", "Текст письма 2", true),
+                new Email("Спам", "Текст спама", false)
+        );
+
+        Predicate<Email> importantFilter = Email::isImportant;
+        Consumer<Email> printEmail = email -> System.out.println("Письмо получено обработчиком: " + email.getSubject());
+        Function<Email, String> signatureFunction = email -> email.getBody().concat("\nSignature");
+
+        List<Email> emailsList = emailProcessor.processEmails(emails, importantFilter, signatureFunction, printEmail);
+        emailsList.forEach(email -> System.out.println("Тема: " + email.getSubject() + ", Тело письма с подписью: " + email.getBody()));
+    }
+}
