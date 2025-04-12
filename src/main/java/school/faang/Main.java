4c00035d3963bd3623e46ab99e45f6e8c2925052
Likhtin_Nick <kolyangrom2000500@gmail.com>
2025-04-12 11:40:46 +0700

    Исправление замечаи от "iBerbasov"

--- src/main/java/school/faang/Main.java
+++ src/main/java/school/faang/Main.java
@@ -1,31 +1,5 @@
 package school.faang;
 
 public class Main {
-    public static void main(String[] args) {
-        LibrarySystem library = new LibrarySystem();
 
-        Book book1 = new Book("The Lord of the Rings", "J.R.R. Tolkien", 1954);
-        Book book2 = new Book("Sunny day", "Paul Anderson", 1978);
-        Book book3 = new Book("Alan Wake", "J.R.R. Tolkien", 1954);
-        Book book4 = new Book("One Republic", "J.R.R. Tolkien", 1954);
-        Book book5 = new Book("Diary", "For me", 2025);
-
-        library.addBook(book1, "A1");
-        library.addBook(book2, "B3");
-        library.addBook(book3, "A2");
-        library.addBook(book4, "B4");
-        library.addBook(book5, "B1");
-
-
-        library.removeBook("Sunny day", "Paul Anderson", 1978);
-        boolean isRemoved = library.removeBook("Diary", "For me", 2025);
-        if (isRemoved) {
-            System.out.println("Книга 'Diary' успешно удалена из библиотеки!");
-        } else {
-            System.out.println("Книга 'Diary' не найдена, удаление не выполнено.");
-        }
-
-        System.out.println(library.getBookLocation(book2));
-        library.printAllBooks();
-    }
 }
---
+++ src/main/java/school/faang/westeroslibrary/LibrarySystem.java
@@ -0,0 +1,32 @@
+package school.faang.westeroslibrary;
+
+import lombok.extern.slf4j.Slf4j;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@Slf4j
+public class LibrarySystem {
+    private final Map<Book, String> bookLocationMap = new HashMap<>();
+
+    public void addBook(Book book, String location) {
+        this.bookLocationMap.put(book, location);
+    }
+
+    public String getBookLocation(Book book) {
+        return this.bookLocationMap.get(book) != null ? this.bookLocationMap.get(book) : "Книга " + book.getTitle() + "в библиотеке на найдена.";
+    }
+
+    public boolean removeBook(String title, String author, int year) {
+        Book searchKey = new Book(title, author, year);
+        String removedLocation = this.bookLocationMap.remove(searchKey);
+        return removedLocation != null;
+    }
+
+    public void printAllBooks() {
+        log.info("Books in the library:");
+        this.bookLocationMap.forEach((book, location) ->
+                log.info("{} → Shelf: {}", book.getTitle(), location)
+        );
+    }
+}
