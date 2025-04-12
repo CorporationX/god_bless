85a175c039a37cd37a6efd1cac122697ec9f30ff
Aleksandr <alexandr.sidorkin@gmail.com>
2025-04-12 08:06:17 +0200

    retrigger PR19092 after PR19091(to master) has been closed

--- src/main/java/school/faang/vesteroslib/SandBox.java
+++ src/main/java/school/faang/vesteroslib/SandBox.java
@@ -4,10 +4,10 @@ public class SandBox {
     public static void main(String[] args) {
         LibrarySystem system = new LibrarySystem();
         system.addBook("Crime and Panishment", "Dostoyevsky", 1867, "3rd shelf, on the right");
-        system.addBook("Evgeny Onegin", "Pushkin", 1827, "3rd shelf, on the left");
+        system.addBook("Evgeny Onegin", "Pushkin", 1828, "3rd shelf, on the left");
         system.printAllBooks();
-        System.out.println(system.findBook("Evgeny Onegin", "Pushkin", 1827));
-        System.out.println(system.removeBook("Evgeny Onegin", "Pushkin", 1827));
+        System.out.println(system.findBook("Evgeny Onegin", "Pushkin", 1828));
+        System.out.println(system.removeBook("Evgeny Onegin", "Pushkin", 1828));
         system.printAllBooks();
     }
 }
