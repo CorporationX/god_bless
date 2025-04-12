718f8f2f079ee841cd6268b26c98692781a047b0
Valentin Lezhnin <v888.rus@gmail.com>
2025-04-12 18:04:43 +0200

    Исправление (2).

--- src/main/java/school/faang/bjs2_68886/Main.java
+++ src/main/java/school/faang/bjs2_68886/Main.java
@@ -1,19 +1,19 @@
 package school.faang.bjs2_68886;
 
 public class Main {
-    private static final LibrarySystem library = new LibrarySystem();
+    private static final LibrarySystem LIBRARY = new LibrarySystem();
 
     public static void main(String[] args) {
-        library.addBook("Задача трех тел", "Лю Цисинь", 2012, "Секция 3.1");
-        library.addBook("Мы", "Евгений Замянит", 1920, "Секция 2.1");
-        library.addBook("Хоббит", "Джон Толкин", 1937, "Секция 1.2");
-        library.addBook("Хоббит", "Джон Толкин", 1937, "Секция 1.2");
-        library.addBook("Хоббит", "Джон Толкин", 1937, "Секция 1.3");
+        LIBRARY.addBook("Задача трех тел", "Лю Цисинь", 2012, "Секция 3.1");
+        LIBRARY.addBook("Мы", "Евгений Замянит", 1920, "Секция 2.1");
+        LIBRARY.addBook("Хоббит", "Джон Толкин", 1937, "Секция 1.2");
+        LIBRARY.addBook("Хоббит", "Джон Толкин", 1937, "Секция 1.2");
+        LIBRARY.addBook("Хоббит", "Джон Толкин", 1937, "Секция 1.3");
 
-        System.out.println(library.findBook("Мы", "Евгений Замянит", 1920));
-        library.removeBook("Мы", "Евгений Замянит", 1920);
-        System.out.println(library.findBook("Мы", "Евгений Замянит", 1920));
+        System.out.println(LIBRARY.findBook("Мы", "Евгений Замянит", 1920));
+        LIBRARY.removeBook("Мы", "Евгений Замянит", 1920);
+        System.out.println(LIBRARY.findBook("Мы", "Евгений Замянит", 1920));
 
-        library.printAllBooks();
+        LIBRARY.printAllBooks();
     }
 }
