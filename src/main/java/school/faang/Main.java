1022adcd1e6757b24c21ce2ede22d32b57147b78
Gigabyte <012_erasyl@mail.ru>
2025-04-12 17:38:50 +0500

    Second Commit

--- src/main/java/school/faang/Main.java
+++ src/main/java/school/faang/Main.java
@@ -1,4 +1,13 @@
 package school.faang;
 
+import java.util.Arrays;
+
 public class Main {
+    public static void main(String[] args) {
+        int[] nums = {1, 2, 3, 4, 5};
+
+        System.out.println("До разворота: " + Arrays.toString(nums));
+        Example.reverse(nums);
+        System.out.println("После разворота: " + Arrays.toString(nums));
+    }
 }
