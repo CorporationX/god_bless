b702ea8c5d29f9b1903e190f86796860daa2a18b
Ilyas Kystaubayev <kystaubaev.iliyas@mail.ru>
2025-04-13 17:39:49 +0500

    Задача Развернитесь!

---
+++ src/main/java/school/faang/BJS2_68110/Main.java
@@ -0,0 +1,11 @@
+package school.faang.BJS2_68110;
+
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) {
+        int [] nums = {1,2,3,4,5};
+        Example.reverse(nums);
+        System.out.println(Arrays.toString(nums));
+    }
+}
