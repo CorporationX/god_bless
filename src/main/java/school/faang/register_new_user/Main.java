0c5b1ab7e239b85f370e4228d0045ae7761eedbb
Robert Abdrakhmanov <rap215@icloud.com>
2025-04-13 23:25:51 +0300

    Добавлен Класс Main в котором добавил пользователя и вывел информацию о нем.

---
+++ src/main/java/school/faang/register_new_user/Main.java
@@ -0,0 +1,8 @@
+package school.faang.register_new_user;
+
+public class Main {
+    public static void main(String[] args) {
+        User user = new User("Goga", 27, "Uber", "New York");
+        System.out.println(user);
+    }
+}
