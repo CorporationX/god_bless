033138bd1b029ffdc80cd57d7f1c84451a50ff57
vasidol123rus <vasekku25@gmail.com>
2025-04-13 19:01:28 +0700

    поправил

--- src/main/java/school/faang/grouping_users_by_age/User.java
+++ src/main/java/school/faang/grouping_users_by_age/User.java
@@ -2,12 +2,14 @@ package school.faang.grouping_users_by_age;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.ToString;
 
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+@ToString
 @Getter
 @AllArgsConstructor
 public class User {
@@ -18,10 +20,8 @@ public class User {
 
     public static Map<Integer, List<User>> groupUsers(List<User> users) {
         Map<Integer, List<User>> groupUsers = new HashMap<>();
-
         for (User user : users) {
             int age = user.getAge();
-
             groupUsers.putIfAbsent(age, new ArrayList<>());
             groupUsers.get(age).add(user);
         }
