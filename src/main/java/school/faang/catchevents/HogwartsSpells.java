4a03b34635c2b9fd84428e01d0c1e0abc1b36d5d
Vladislav Kozlov <littlerock2877@mail.ru>
2025-04-13 22:02:59 +0300

    fix: PR comments

--- src/main/java/school/faang/catchevents/HogwartsSpells.java
+++ src/main/java/school/faang/catchevents/HogwartsSpells.java
@@ -21,31 +21,31 @@ public class HogwartsSpells {
     public SpellEvent addSpellEvent(String eventType, String actionDescription) {
         int spellId = generateSpellId();
         SpellEvent spellEvent = new SpellEvent(spellId, eventType, actionDescription);
-        this.spellsByType.computeIfAbsent(eventType, key -> new ArrayList<>()).add(spellEvent);
-        this.spellById.put(spellId, spellEvent);
+        spellsByType.computeIfAbsent(eventType, key -> new ArrayList<>()).add(spellEvent);
+        spellById.put(spellId, spellEvent);
         return spellEvent;
     }
 
     public SpellEvent getSpellEventById(int id) {
-        return Optional.ofNullable(this.spellById.get(id))
+        return Optional.ofNullable(spellById.get(id))
                 .orElseThrow(() -> new NoSuchElementException(String.format("Spell with id %d doesn't exist", id)));
     }
 
     public List<SpellEvent> getSpellEventsByType(String eventType) {
-        return Optional.ofNullable(this.spellsByType.get(eventType))
+        return Optional.ofNullable(spellsByType.get(eventType))
                 .orElseThrow(() -> new NoSuchElementException(String.format("EventType %s doesn't exist", eventType)));
     }
 
     public boolean deleteSpellEvent(int id) {
-        SpellEvent removedSpellEvent = this.spellById.remove(id);
+        SpellEvent removedSpellEvent = spellById.remove(id);
         if (removedSpellEvent != null) {
-            this.spellsByType.get(removedSpellEvent.getEventType()).remove(removedSpellEvent);
+            spellsByType.get(removedSpellEvent.getEventType()).remove(removedSpellEvent);
         }
         return removedSpellEvent != null;
     }
 
     public void printAllSpellEvents() {
-        for (Map.Entry<Integer, SpellEvent> spellEventEntry : this.spellById.entrySet()) {
+        for (Map.Entry<Integer, SpellEvent> spellEventEntry : spellById.entrySet()) {
             SpellEvent spellEvent = spellEventEntry.getValue();
             log.info("Spell id = [{}], type = [{}], action = [{}]", spellEventEntry.getKey(), spellEvent.getEventType(),
                     spellEvent.getAction());
