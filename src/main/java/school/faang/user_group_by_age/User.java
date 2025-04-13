1ee73ab71461988efcee524f4a7197d30c582ed4
Robert Abdrakhmanov <rap215@icloud.com>
2025-04-13 20:28:59 +0300

    Регистрация пользователя. Фикс с пакетом, именем Класса и пустой строкой.

--- src/main/java/school/faang/user_group_by_age/User.java
+++ src/main/java/school/faang/user_group_by_age/User.java
@@ -1,4 +1,4 @@
-package school.faang;
+package school.faang.user_group_by_age;
 
 import lombok.Data;
 
@@ -8,21 +8,20 @@ import java.util.List;
 import java.util.Map;
 
 @Data
-public class Users {
+public class User {
     private final String name;
     private final int age;
     private final String workplace;
     private final String address;
 
-    public static Map<Integer, List<Users>> groupUsers(List<Users> users) {
-        Map<Integer, List<Users>> usersGroupByAge = new HashMap<>();
+    public static Map<Integer, List<User>> groupUsers(List<User> users) {
+        Map<Integer, List<User>> usersGroupByAge = new HashMap<>();
 
-        for (Users user : users) {
+        for (User user : users) {
             int age = user.getAge();
             if (!usersGroupByAge.containsKey(age)) {
                 usersGroupByAge.put(age, new ArrayList<>());
             }
-
             usersGroupByAge.get(age).add(user);
         }
 
