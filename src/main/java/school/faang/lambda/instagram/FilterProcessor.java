5cfb52700387c322c134f32b5199cfc2e8a4d575
Keriod <nikolainovopashin98@gmail.com>
2025-04-13 15:46:59 +0500

    BJS2-70390. Фильтры Instagram

---
+++ src/main/java/school/faang/lambda/instagram/FilterProcessor.java
@@ -0,0 +1,14 @@
+package school.faang.lambda.instagram;
+
+import java.util.function.Function;
+
+public class FilterProcessor {
+    public Image applyFilter(Image image, Function<Image, Image> filter) {
+        return filter.apply(image);
+    }
+
+    public Function<Image, Image> combineFilters(Function<Image, Image> firstFilter,
+                                                 Function<Image, Image> secondFilter) {
+        return (image) -> secondFilter.apply(firstFilter.apply(image));
+    }
+}
---
+++ src/main/java/school/faang/lambda/instagram/Image.java
@@ -0,0 +1,9 @@
+package school.faang.lambda.instagram;
+
+import lombok.Data;
+
+@Data
+public class Image {
+    private final String name;
+    private final String description;
+}
