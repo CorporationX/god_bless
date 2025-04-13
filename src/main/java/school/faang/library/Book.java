11922513d4634b22e9e716715463024d3ad318fa
pirojok_yt <tamik34@mail.ru>
2025-04-13 12:43:45 +0500

    first try of library

---
+++ src/main/java/school/faang/library/Book.java
@@ -0,0 +1,31 @@
+package school.faang.library;
+
+import java.util.Objects;
+
+import lombok.Getter;
+
+@Getter
+public class Book {
+
+    private final String title;
+    private final String author;
+    private final int year;
+
+    public Book(String title, String author, int year) {
+        this.title = title;
+        this.author = author;
+        this.year = year;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        Book book = (Book) o;
+        return year == book.year && Objects.equals(title, book.title) && Objects.equals(author, book.author);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(title, author, year);
+    }
+}
---
+++ src/main/java/school/faang/library/LibrarySystem.java
@@ -0,0 +1,31 @@
+package school.faang.library;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class LibrarySystem {
+
+    Map<Book, String> books = new HashMap<>();
+
+    public void addBook(String title, String author, int year, String location) {
+        books.put(new Book(title, author, year), location);
+    }
+
+    public void removeBook(String title, String author, int year) {
+        books.remove(new Book(title, author, year));
+    }
+
+    public String findBook(String title, String author, int year) {
+        return books.getOrDefault(new Book(title, author, year), "Book not found");
+    }
+
+    public String printAllBooks() {
+        StringBuilder titles = new StringBuilder();
+        for (Map.Entry<Book, String> entry : books.entrySet()) {
+            titles.append(entry.getKey().getTitle())
+                    .append(" находится тут: ")
+                    .append(entry.getValue()).append("\n");
+        }
+        return titles.toString();
+    }
+}
