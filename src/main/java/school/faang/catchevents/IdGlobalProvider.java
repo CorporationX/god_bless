444a416c288ff4734358081c8b484662a6b04ea8
Fagan Gafulov <gafulov@yandex.ru>
2025-04-13 11:27:15 +0300

    BJS2-68673 fix style

--- src/main/java/school/faang/catchevents/IdGlobalProvider.java
+++ src/main/java/school/faang/catchevents/IdGlobalProvider.java
@@ -3,7 +3,7 @@ package school.faang.catchevents;
 import java.util.concurrent.atomic.AtomicInteger;
 
 public class IdGlobalProvider {
-    private final static AtomicInteger id = new AtomicInteger(0);
+    private static final AtomicInteger id = new AtomicInteger(0);
 
     public static int getId() {
         return id.incrementAndGet();
