f68d2d410a10990f8b55f643f42aa52ba43f32f2
Andrew Sonin <sonin.andrei2009@yandex.ru>
2025-04-13 16:40:55 +0300

    Task BJS2-68151
    1)Rework method reverse

--- src/main/java/school/faang/reverse/Example.java
+++ src/main/java/school/faang/reverse/Example.java
@@ -2,10 +2,13 @@ package school.faang.reverse;
 
 public class Example {
     public static int[] reverse(int[] numbers) {
-        int[] newNumbers = new int[numbers.length];
-        for (int i = 0; i < numbers.length; i++) {
-            newNumbers[i] = numbers[numbers.length - i - 1];
+
+        for (int i = 0; i < numbers.length / 2; i++) {
+            int temp = numbers[i];
+            numbers[i] = numbers[numbers.length - 1 - i];
+            numbers[numbers.length - 1 - i] = temp;
         }
-        return newNumbers;
+        return numbers;
     }
 }
+
--- src/main/java/school/faang/reverse/Main.java
+++ src/main/java/school/faang/reverse/Main.java
@@ -6,8 +6,8 @@ public class Main {
 
     public static void main(String[] args) {
         int[] numbers = new int[]{1, 2, 3, 4, 5};
-        int[] result = Example.reverse(numbers);
         System.out.println(Arrays.toString(numbers));
+        int[] result = Example.reverse(numbers);
         System.out.println(Arrays.toString(result));
     }
 }
