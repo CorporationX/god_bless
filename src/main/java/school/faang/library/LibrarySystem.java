700ace78144ba6c1d6d85b9a95bb611891bf000a
pirojok_yt <tamik34@mail.ru>
2025-04-13 16:08:34 +0500

    spell events finished

--- src/main/java/school/faang/library/LibrarySystem.java
+++ src/main/java/school/faang/library/LibrarySystem.java
@@ -4,24 +4,28 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class LibrarySystem {
-
+    private final String notFound = "Book not found";
     Map<Book, String> books = new HashMap<>();
 
     public void addBook(String title, String author, int year, String location) {
-        books.put(new Book(title, author, year), location);
+        this.books.put(new Book(title, author, year), location);
     }
 
     public void removeBook(String title, String author, int year) {
-        books.remove(new Book(title, author, year));
+        if (findBook(title, author, year).equals(notFound)) {
+            System.out.println(notFound);
+        } else {
+            this.books.remove(new Book(title, author, year));
+        }
     }
 
     public String findBook(String title, String author, int year) {
-        return books.getOrDefault(new Book(title, author, year), "Book not found");
+        return this.books.getOrDefault(new Book(title, author, year), notFound);
     }
 
     public String printAllBooks() {
         StringBuilder titles = new StringBuilder();
-        for (Map.Entry<Book, String> entry : books.entrySet()) {
+        for (Map.Entry<Book, String> entry : this.books.entrySet()) {
             titles.append(entry.getKey().getTitle())
                     .append(" находится тут: ")
                     .append(entry.getValue()).append("\n");
