ac3eb77923e03115a7a4dbaec11c9b95480b35bd
Sergey Selischev <ssb22@mail.ru>
2025-04-14 21:04:22 +0300

    исправление замечаний

--- src/main/java/school/faang/bjs2_70190/SpellCaster.java
+++ src/main/java/school/faang/bjs2_70190/SpellCaster.java
@@ -1,7 +1,7 @@
 package school.faang.bjs2_70190;
 
 public class SpellCaster {
-    void cast(String spellName, SpellAction spellAction) {
+    public void cast(String spellName, SpellAction spellAction) {
         System.out.println(spellAction.doSpell(spellName));
     }
 }
