2e8f88e2df4c6976c4e04ea178f4814fa336a747
Fagan Gafulov <gafulov@yandex.ru>
2025-04-14 14:08:18 +0300

    BJS2-70445 create task

---
+++ src/main/java/school/faang/instagram/FilterProcessor.java
@@ -0,0 +1,17 @@
+package school.faang.instagram;
+
+import java.util.function.Function;
+
+public class FilterProcessor {
+
+    public Image applyFilter(Image image, Function<Image, Image> filter) {
+        if (image == null) {
+            throw new IllegalArgumentException("Изображение отсутствует");
+        }
+        return filter.apply(image);
+    }
+
+    public Function<Image, Image> combineFilters(Function<Image, Image> filterA, Function<Image, Image> filterB) {
+        return image -> filterA.apply(filterB.apply(image));
+    }
+}
---
+++ src/main/java/school/faang/instagram/Image.java
@@ -0,0 +1,13 @@
+package school.faang.instagram;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class Image {
+    private final String name;
+    private String description;
+}
+
+
