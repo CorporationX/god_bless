17e2211e9593f78f4ae6bfeb8072af2b63040c4e
Vladislav Kozlov <littlerock2877@mail.ru>
2025-04-14 23:38:34 +0300

    fix: PR comments

--- src/main/java/school/faang/datacenter/service/DataCenterService.java
+++ src/main/java/school/faang/datacenter/service/DataCenterService.java
@@ -11,45 +11,23 @@ public class DataCenterService {
     private final OptimizationStrategy optimizationStrategy;
 
     public Server addServer(DataCenter dataCenter, Server server) {
-        dataCenter.getServers().add(server);
-        return server;
+        return dataCenter.addServer(server);
     }
 
     public boolean removeServer(DataCenter dataCenter, Server server) {
-        return dataCenter.getServers().remove(server);
+        return dataCenter.removeServer(server);
     }
 
     public double getTotalEnergyConsumption(DataCenter dataCenter) {
-        return dataCenter.getServers().stream().mapToDouble(Server::getEnergyConsumption).sum();
+        return dataCenter.getTotalEnergyConsumption();
     }
 
     public boolean allocateResources(DataCenter dataCenter, ResourceRequest request) {
-        double remainingLoad = request.getLoad();
-        for (Server server : dataCenter.getServers()) {
-            double remainingOnServer = server.getMaxLoad() - server.getLoad();
-            if (remainingOnServer >= remainingLoad) {
-                server.setLoad(remainingOnServer - remainingLoad + server.getLoad());
-                remainingLoad = 0;
-                break;
-            } else {
-                remainingLoad -= server.getMaxLoad() - server.getLoad();
-                server.setLoad(server.getMaxLoad());
-            }
-        }
-        return remainingLoad == 0;
+        return dataCenter.allocateResources(request);
     }
 
     public void releaseResources(DataCenter dataCenter, ResourceRequest request) {
-        double loadToRemain = request.getLoad();
-        for (Server server : dataCenter.getServers()) {
-            if (server.getLoad() > loadToRemain) {
-                server.setLoad(server.getLoad() - loadToRemain);
-                break;
-            } else {
-                loadToRemain -= server.getLoad();
-                server.setLoad(0);
-            }
-        }
+        dataCenter.releaseResources(request);
     }
 
     public void optimize(DataCenter dataCenter, OptimizationStrategy strategy) {
