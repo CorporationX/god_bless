77be86682fac747be02ad1c960933b653db64933
Yulia <bualex1983@mail.ru>
2025-04-15 00:36:01 +0300

    Fix compilation error

--- src/main/java/school/faang/bjs_68133/Main.java
+++ src/main/java/school/faang/bjs_68133/Main.java
@@ -5,7 +5,7 @@ import static school.faang.bjs_68133.Example.*;
 public class Main {
     public static void main(String[] args) {
         int[] nums = {1, 2, 3, 4, 5};
-        for (int i = 0; i < nums.length; i ++) {
+        for (int i = 0; i < nums.length; i++) {
             printArray(nums);
             Example.reverse(nums);
             System.out.println("Развёрнутый массив:");
