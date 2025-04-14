38c579ea59e4ba6a1f6cb14d9b88f5ea2236c66b
Kirill <kirill551@yandex.ru>
2025-04-14 16:34:34 +0200

    "Внести правки согласно комментариям"

--- src/main/java/school/faang/gmail_reach_filters/EmailProcessor.java
+++ src/main/java/school/faang/gmail_reach_filters/EmailProcessor.java
@@ -6,12 +6,17 @@ import java.util.function.Function;
 import java.util.function.Predicate;
 
 public class EmailProcessor {
-    public List<Email> processEmails(List<Email> emails, Predicate<Email> filter, Function<Email, String> function,
-                                     Consumer<Email> consumer) {
-        emails.forEach(consumer);
+    public List<Email> processEmails(List<Email> emails,
+                                     Predicate<Email> filter,
+                                     Function<Email, String> signatureFunction,
+                                     Consumer<Email> printEmailSubject) {
         return emails.stream()
                 .filter(filter)
-                .peek(email -> email.setBody(function.apply(email)))
+                .peek(printEmailSubject)
+                .map(email -> {
+                    email.setBody(signatureFunction.apply(email));
+                    return email;
+                })
                 .toList();
     }
 }
