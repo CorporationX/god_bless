cedf560ab574baadfc87108590cb12a2dfd2070b
Valeriia <valeriia.shurupina@yahoo.com>
2025-04-14 18:41:59 +0200

    BJS2-70040 fix naming

--- src/main/java/school/faang/bjs2_70040/LocationSearchEngine.java
+++ src/main/java/school/faang/bjs2_70040/LocationSearchEngine.java
@@ -7,26 +7,26 @@ import java.util.function.Function;
 import java.util.function.Predicate;
 
 public class LocationSearchEngine {
-    public List<Location> filterLocations(List<Location> locations, Predicate<Location> predicate) {
+    public List<Location> filterLocations(List<Location> locations, Predicate<Location> filter) {
         List<Location> filteredLocations = new ArrayList<>();
         for (Location location : locations) {
-            if (predicate.test(location)) {
+            if (filter.test(location)) {
                 filteredLocations.add(location);
             }
         }
         return filteredLocations;
     }
 
-    public void processLocations(List<Location> locations, Consumer<Location> consumer) {
+    public void processLocations(List<Location> locations, Consumer<Location> action) {
         for (Location location : locations) {
-            consumer.accept(location);
+            action.accept(location);
         }
     }
 
-    public List<Double> calculateDistances(List<Location> locations, Function<Location, Double> function) {
+    public List<Double> calculateDistances(List<Location> locations, Function<Location, Double> calculatorDistance) {
         List<Double> distances = new ArrayList<>();
         for (Location location : locations) {
-            distances.add(function.apply(location));
+            distances.add(calculatorDistance.apply(location));
         }
         return distances;
     }
