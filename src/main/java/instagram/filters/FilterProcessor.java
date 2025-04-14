7404aa0a73f4861347d079000f7049cb3b52fa1f
Buga Dan <danbil.942@gmail.com>
2025-04-14 22:21:27 +0300

    Instagram filters task BSJ2-70429.

---
+++ src/main/java/instagram/filters/FilterProcessor.java
@@ -0,0 +1,17 @@
+package instagram.filters;
+
+import java.util.function.Function;
+
+public class FilterProcessor {
+    public Image applyFilter(Image image, Function<Image, Image> transformFunction) {
+        return transformFunction.apply(image);
+    }
+
+    public Function<Image, Image> combineFilters(Function<Image, Image> firstFilter, Function<Image, Image> secondFilter) {
+        Function<Image, Image> combinedFunction =
+                image -> new Image("Combined filters", firstFilter
+                        .apply(new Image("Combined", secondFilter
+                                .apply(image).getDescription())).getDescription());
+        return combinedFunction;
+    }
+}
