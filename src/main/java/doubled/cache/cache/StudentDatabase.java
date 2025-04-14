eba206c3137bd2d6d833e4b48ce82809c6774a33
Dan <danbil.942@gmail.com>
2025-04-14 07:32:38 +0300

    Double cache cache task BJS2-68789. (#19082)
    * Double cache cache task BJS2-68789.
    
    * Double cache cache task BJS2-68789. Fixed all issues and make methods more efficient.

---
+++ src/main/java/doubled/cache/cache/StudentDatabase.java
@@ -0,0 +1,64 @@
+package doubled.cache.cache;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class StudentDatabase {
+    private Map<Student, Map<Subject, Integer>> studentSubjects = new HashMap<>();
+    private Map<Subject, List<Student>> subjectStudents = new HashMap<>();
+
+    public void addStudent(String studentName, Map<Subject, Integer> subjectsAndGrades) {
+        if (studentName == null || subjectsAndGrades == null || studentName.isBlank()) {
+            System.out.println("Student name or his map with grades is null!");
+            return;
+        }
+
+        this.studentSubjects.putIfAbsent(new Student(studentName), subjectsAndGrades);
+
+        for (Map.Entry<Subject, Integer> subIntEntry : subjectsAndGrades.entrySet()) {
+            this.subjectStudents.computeIfAbsent(subIntEntry.getKey(),
+                    key -> new ArrayList<>()).add(new Student(studentName));
+        }
+    }
+
+    public void addSubjectToExistingStudent(String existStudent, String subject, Integer grade) {
+        if (existStudent == null || subject == null || grade == null) {
+            System.out.println("Student name, subject or grades is null!");
+        } else if (!this.studentSubjects.containsKey(new Student(existStudent))) {
+            System.out.printf("Student %s doesn t exist!", existStudent);
+        } else if (this.studentSubjects.get(new Student(existStudent)).containsKey(new Subject(subject))) {
+            System.out.printf("Subject %s for student %s already exists!");
+        } else {
+            this.studentSubjects.get(new Student(existStudent)).put(new Subject(subject), grade);
+            this.subjectStudents.computeIfAbsent(new Subject(subject),
+                    key -> new ArrayList<>()).add(new Student(existStudent));
+        }
+    }
+
+    public void removeStudentAndData(String studentName) {
+        if (studentName == null) {
+            System.out.println("Student name, subject or grades is null!");
+            return;
+        } else if (!this.studentSubjects.containsKey(new Student(studentName))) {
+            System.out.printf("Student %s doesn t exist!", studentName);
+            return;
+        }
+        this.studentSubjects.remove(new Student(studentName));
+        for (Map.Entry<Subject, List<Student>> subEntry : this.subjectStudents.entrySet()) {
+            subEntry.getValue().remove(new Student(studentName));
+        }
+    }
+
+    public void printAllStudentsInfo() {
+        for (Map.Entry<Student, Map<Subject, Integer>> studentMapEntry : this.studentSubjects.entrySet()) {
+            System.out.printf("Student %s %nHas next subjects and grades: %n", studentMapEntry.getKey().getName());
+            for (Map.Entry<Subject, Integer> subjectIntegerEntry : studentMapEntry.getValue().entrySet()) {
+                System.out.printf("Subject %s: grade - %d %n", subjectIntegerEntry.getKey().getName(),
+                        subjectIntegerEntry.getValue());
+            }
+            System.out.println();
+        }
+    }
+}
