16284b865432e503c74b33340a81d3539b0dadce
Fagan Gafulov <gafulov@yandex.ru>
2025-04-14 14:21:38 +0300

    BJS2-70445 fix style

--- src/main/java/school/faang/instagram/Main.java
+++ src/main/java/school/faang/instagram/Main.java
@@ -11,19 +11,24 @@ public class Main {
         FilterProcessor filterProcessor = new FilterProcessor();
 
         // Фильтры
-        Function<Image, Image> grayscaleFilter = (image) -> new Image(image.getName(), image.getDescription() + " | Фильтр: черно-белый");
-        Function<Image, Image> sepiaFilter = (image) -> new Image(image.getName(), image.getDescription() + " | Фильтр: сепия");
+        Function<Image, Image> grayscaleFilter = (image) -> {
+            new Image(image.getName(), image.getDescription() + " | Фильтр: черно-белый");
+            return image;
+        };
+        Function<Image, Image> sepiaFilter = (image) -> {
+            new Image(image.getName(), image.getDescription() + " | Фильтр: сепия");
+            return image;
+        };
 
         // Применение фильтров
         Image grayscaleImage = filterProcessor.applyFilter(originalImage, grayscaleFilter);
-        System.out.println(grayscaleImage.getDescription()); // Вывод: Оригинальное изображение | Фильтр: черно-белый
+        System.out.println(grayscaleImage.getDescription());
 
         Image sepiaImage = filterProcessor.applyFilter(grayscaleImage, sepiaFilter);
-        System.out.println(sepiaImage.getDescription()); // Вывод: Оригинальное изображение | Фильтр: черно-белый | Фильтр: сепия
-
+        System.out.println(sepiaImage.getDescription());
         // Комбинирование фильтров
         Function<Image, Image> combinedFilter = filterProcessor.combineFilters(grayscaleFilter, sepiaFilter);
         Image combinedImage = filterProcessor.applyFilter(originalImage, combinedFilter);
-        System.out.println(combinedImage.getDescription()); // Вывод: Оригинальное изображение | Фильтр: черно-белый | Фильтр: сепия
+        System.out.println(combinedImage.getDescription());
     }
 }
