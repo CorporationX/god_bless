3b9a35b39f6bd13f38d1f750a6ebc1260c8f85bb
Ilyas Kystaubayev <kystaubaev.iliyas@mail.ru>
2025-04-14 11:51:15 +0500

    Задача Развернитесь!

--- src/main/java/school/faang/BJS2_68110/Main.java
+++ src/main/java/school/faang/BJS2_68110/Main.java
@@ -4,7 +4,7 @@ import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) {
-        int [] nums = {1,2,3,4,5,6};
+        int[] nums = {1, 2, 3, 4, 5, 6};
         Example.reverse(nums);
         System.out.println(Arrays.toString(nums));
     }
