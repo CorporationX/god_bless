d57aa456eb3e79a62e0e87eac426860200e6dcd3
Keriod <nikolainovopashin98@gmail.com>
2025-04-14 21:35:49 +0500

    BJS2-70070. Обрабатываем ошибки красиво

--- src/main/java/school/faang/lambda/error_handler/Main.java
+++ src/main/java/school/faang/lambda/error_handler/Main.java
@@ -6,7 +6,7 @@ import lombok.extern.slf4j.Slf4j;
 public class Main {
     public static void main(String[] args) {
         String resultTwo = ErrorHandler.withErrorHandling(
-                () -> RemoteService.call("someParam"),
+                RemoteService::call,
                 e -> {
                     log.info("Ошибка при вызове сервиса, возвращаем дефолтное значение");
                     return "DEFAULT";
--- src/main/java/school/faang/lambda/error_handler/RemoteService.java
+++ src/main/java/school/faang/lambda/error_handler/RemoteService.java
@@ -1,7 +1,7 @@
 package school.faang.lambda.error_handler;
 
 public class RemoteService {
-    public static String call(String param) {
+    public static String call() {
         throw new RuntimeException("Сервис недоступен");
     }
 }
