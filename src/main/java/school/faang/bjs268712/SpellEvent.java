a24acca14d5726a7c76de310e3b08617926c4df9
SAM <SULEYMANOV.USA@GMAIL.COM>
2025-04-15 08:40:48 -0700

    bjs268712

--- src/main/java/school/faang/bjs268712/SpellEvent.java
+++ src/main/java/school/faang/bjs268712/SpellEvent.java
@@ -17,12 +17,14 @@ public class SpellEvent {
     public SpellEvent(String eventType, String action) {
         this.id = ++counter;
         if (eventType == null) {
-            log.error("EventType cannot be null. Please provide a valid EventType instance to initialize the SpellEvent.");
+            log.error("EventType cannot be null. " +
+                    "Please provide a valid EventType instance to initialize the SpellEvent.");
             throw new IllegalArgumentException("EventType cannot be null. A valid EventType instance is required.");
         }
         this.eventType = eventType;
         if (action == null || action.trim().isEmpty()) {
-            log.error("Action cannot be null or empty. Please provide a valid action string to initialize the SpellEvent.");
+            log.error("Action cannot be null or empty. " +
+                    "Please provide a valid action string to initialize the SpellEvent.");
             throw new IllegalArgumentException("Action cannot be null or empty. A valid action string is required.");
         }
         this.action = action;
