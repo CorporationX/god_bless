f8f90f14f4dca81cda9d230876160bd710b9daf4
Petr Martyshov <dem9968@gmail.com>
2025-04-15 15:16:37 +0300

    Выполнена задача BJS2-68748 "Библиотека Вестеросса"

---
+++ src/main/java/school/faang/bjs2_68748/LibrarySystem.java
@@ -0,0 +1,34 @@
+package school.faang.bjs2_68748;
+
+import lombok.NoArgsConstructor;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@NoArgsConstructor
+public class LibrarySystem {
+    private final Map<Book, String> library = new HashMap<>();
+
+    public void addBook(String title, String author, int year, String location) {
+        library.put(new Book(title, author, year), location);
+    }
+
+    public void removeBook(String title, String author, int year) {
+        library.remove(new Book(title, author, year));
+    }
+
+    public void findBook(String title, String author, int year) {
+        System.out.printf("Местоположение книги \"%s (%s, %d)\": %s%n",
+                title, author, year, library.getOrDefault(
+                        new Book(title, author, year), "Такой книги нет в библиотеке"
+                ));
+    }
+
+    public void printAllBooks() {
+        System.out.println("Все книги в библиотеке:");
+        for (Map.Entry<Book, String> entry : library.entrySet()) {
+            System.out.printf("Книга \"%s (%s, %d)\" находится здесь: %s%n",
+                    entry.getKey().getTitle(), entry.getKey().getAuthor(), entry.getKey().getYear(), entry.getValue());
+        }
+    }
+}
