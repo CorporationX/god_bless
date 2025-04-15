c577c43dc2d2162945f401d3e29d69a35a9568d4
Kirill <kirill551@yandex.ru>
2025-04-15 12:02:47 +0200

    "Использовать String.join() для лаконичности кода."

--- src/main/java/school/faang/build_csv/CsvFormatter.java
+++ src/main/java/school/faang/build_csv/CsvFormatter.java
@@ -4,16 +4,7 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 public class CsvFormatter {
-    private static final VectorJoiner<String> vectorJoiner = list -> {
-        StringBuilder lineWithCommas = new StringBuilder();
-        for (int i = 0; i < list.size(); i++) {
-            lineWithCommas.append(list.get(i));
-            if (i != list.size() - 1) {
-                lineWithCommas.append(",");
-            }
-        }
-        return lineWithCommas.toString();
-    };
+    private static final VectorJoiner<String> vectorJoiner = list -> String.join(",", list);
 
     private static final MatrixJoiner<String> matrixJoiner = list ->
             list.stream().map(vectorJoiner::join).collect(Collectors.joining("\n"));
