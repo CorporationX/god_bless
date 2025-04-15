07c0c6f200bf59d6e8f835cc73a9f5dab51f3fda
KirillSMv <88364531+KirillSMv@users.noreply.github.com>
2025-04-15 06:18:31 +0200

    Kirill 28377 bjs2 69998 (#19237)
    * "Выполнить задачу BJS2-69998"
    
    * "Style"

--- src/main/java/school/faang/Main.java
+++ src/main/java/school/faang/Main.java
@@ -1,4 +1,15 @@
 package school.faang;
 
+import school.faang.remember_school.TriangleSquareCalculator;
+
 public class Main {
+    public static void main(String[] args) {
+        Double square = TriangleSquareCalculator.calculateTriangleArea(3, 4, 5);
+        try {
+            TriangleSquareCalculator.calculateTriangleArea(3, 4, 45);
+        } catch (RuntimeException e) {
+            System.out.println(e.getMessage());
+        }
+        System.out.println("Площадь треугольника: " + square);
+    }
 }
---
+++ src/main/java/school/faang/remember_school/TriangleSquareCalculator.java
@@ -0,0 +1,34 @@
+package school.faang.remember_school;
+
+import java.util.function.BiFunction;
+import java.util.function.Function;
+
+public class TriangleSquareCalculator {
+    private static final BiFunction<Double, Double, Double> ADD = Double::sum;
+    private static final BiFunction<Double, Double, Double> SUBTRACT = (a, b) -> a - b;
+    private static final BiFunction<Double, Double, Double> MULTIPLY = (a, b) -> a * b;
+    private static final BiFunction<Double, Double, Double> DIVIDE = (a, b) -> a / b;
+    private static final Function<Double, Double> SQUARE_ROOT = Math::sqrt;
+
+    public static Double calculateTriangleArea(double a, double b, double c) {
+        validateIfTriangle(a, b, c);
+        double halfPerimeter = DIVIDE.apply(ADD.apply(a, ADD.apply(b, c)), 2.0);
+        double halfPerimeterSubtractA = SUBTRACT.apply(halfPerimeter, a);
+        double halfPerimeterSubtractB = SUBTRACT.apply(halfPerimeter, b);
+        double halfPerimeterSubtractC = SUBTRACT.apply(halfPerimeter, c);
+        return SQUARE_ROOT.apply(MULTIPLY.apply(halfPerimeter, MULTIPLY.apply(halfPerimeterSubtractA,
+                MULTIPLY.apply(halfPerimeterSubtractB, halfPerimeterSubtractC))));
+    }
+
+    private static void validateIfTriangle(double a, double b, double c) {
+        if (a <= 0 || b <= 0 || c <= 0) {
+            throw new RuntimeException("Value cannot be less than zero");
+        }
+        double comparisonWithA = ADD.apply(b, c);
+        double comparisonWithB = ADD.apply(a, c);
+        double comparisonWithC = ADD.apply(a, b);
+        if (comparisonWithA <= a || comparisonWithB <= b || comparisonWithC <= c) {
+            throw new RuntimeException("This is not a triangle!");
+        }
+    }
+}
