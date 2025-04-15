e7a44e1ff59e5f80e7873de31b1b3e6eae4845cc
vasidol123rus <vasekku25@gmail.com>
2025-04-15 20:31:58 +0700

    ver.2

--- src/main/java/school/faang/hogwarts/Main.java
+++ src/main/java/school/faang/hogwarts/Main.java
@@ -4,16 +4,16 @@ public class Main {
     public static void main(String[] args) {
         SpellCaster spellCaster = new SpellCaster();
         spellCaster.cast("Левитация", spell -> {
-            return spell + " - Вы поднимались в воздух!";
+            return String.format("%s - Вы поднимались в воздух!", spell);
         });
         spellCaster.cast("Авада Кедавра", spell -> {
-            return spell + " - Уничтожение! ";
+            return String.format("%s - Уничтожение! ", spell);
         });
         spellCaster.cast("Экспелиармус", spell -> {
-            return spell + " - Оружие было выбито!";
+            return String.format("%s - Оружие было выбито!", spell);
         });
         spellCaster.cast("Патронус", spell -> {
-            return spell + " - Призван защитник!";
+            return String.format("%s - Призван защитник!", spell);
         });
     }
 }
