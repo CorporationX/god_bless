5b22b68ba8d9f6ada18bbd8851950df9ddbc244d
andre <andrey.kuimov2015@yandex.ru>
2025-04-15 22:33:46 +0300

    kenig8649: BJS2-70132 fix ветки

--- src/main/java/school/faang/sprinttwo/ourbelovedhogwarts/SpellCaster.java
+++
@@ -1,8 +0,0 @@
-package school.faang.sprinttwo.ourbelovedhogwarts;
-
-public class SpellCaster {
-    public void cast(String spellName, SpellAction action) {
-        String result = action.castSpell(spellName);
-        System.out.println("Результат заклинания - " + result);
-    }
-}
