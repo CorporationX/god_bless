a2acd117e81df2828066f9b24a78f4ef49ff8f8c
Sergey Filippovich <filippovich.sergey@gmail.com>
2025-04-15 21:59:38 +0400

    updated titles and authors

--- src/main/java/school/faang/Main.java
+++ src/main/java/school/faang/Main.java
@@ -2,10 +2,10 @@ package school.faang;
 
 public class Main {
     public static void main(String[] args) {
-        Book book1 = new Book("Каласы пад сярпом тваім","Караткевіч",2005);
-        Book book2 = new Book("Каласы пад сярпом тваім","Караткевіч",2005);
-        Book book3 = new Book("Жалейка","Купала",1908);
-        Book book4 = new Book("Спадчына","Купала",1922);
+        Book book1 = new Book("The Vegetarian","Han Kang",2015);
+        Book book2 = new Book("The Vegetarian","Han Kang",2015);
+        Book book3 = new Book("Morning and Evening","Jon Fosse",2015);
+        Book book4 = new Book("A Girl’s Story","Annie Ernaux",2019);
 
         System.out.println(book1.equals(book2));
 
@@ -14,11 +14,11 @@ public class Main {
         librarySystem.addBook(book2,"2nd shelf");
         librarySystem.addBook(book3,"3rd shelf");
         librarySystem.addBook(book4,"4th shelf");
-        librarySystem.addBook("Новая зямля","Колас",1923,"1st shelf");
-        librarySystem.addBook("Бывший сын","Филипенко",2014,"5th shelf");
+        librarySystem.addBook("Pilgrims Way","Abdulrazak Gurnah",1988,"1st shelf");
+        librarySystem.addBook("Vita Nova","Louise Glück",1999,"5th shelf");
 
         librarySystem.findBook(book1);
-        librarySystem.findBook("Бывший сын","Филипенко",2014);
+        librarySystem.findBook("Vita Nova","Louise Glück",1999);
         librarySystem.printAllBooks();
         System.out.println();
         librarySystem.removeBook(book1);
