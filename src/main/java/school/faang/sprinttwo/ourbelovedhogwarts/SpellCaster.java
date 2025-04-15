1435ce74a2dd0e56d4a418cbabc7d3214b3a991d
andre <andrey.kuimov2015@yandex.ru>
2025-04-15 19:52:40 +0300

    kenig8649: BJS2-70132 Хогвартс, Хогвартс, наш любимый Хогвартс

--- src/main/java/school/faang/sprinttwo/ourbelovedhogwarts/SpellCaster.java
+++ src/main/java/school/faang/sprinttwo/ourbelovedhogwarts/SpellCaster.java
@@ -3,6 +3,6 @@ package school.faang.sprinttwo.ourbelovedhogwarts;
 public class SpellCaster {
     public void cast(String spellName, SpellAction action) {
         String result = action.castSpell(spellName);
-        System.out.println("Результат заклинания - " + result);
+        System.out.println("Результат заклинания: " + result);
     }
 }
