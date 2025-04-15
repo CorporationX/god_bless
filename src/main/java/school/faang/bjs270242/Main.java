8c2df4583828ce8ca07da0cdb9e6186b87f0da82
SAM <SULEYMANOV.USA@GMAIL.COM>
2025-04-15 11:09:25 -0700

    Replaced concatenation with String.format()

--- src/main/java/school/faang/bjs270242/Main.java
+++ src/main/java/school/faang/bjs270242/Main.java
@@ -7,8 +7,8 @@ public class Main {
         String lumos = "Lumos";
         String expelliarmus = "Expelliarmus";
 
-        spellCaster.cast(alohomora, (spellName) -> "The door is unlocked by " + spellName);
-        spellCaster.cast(lumos, (spellName) -> "A beam of light is created by " + spellName);
-        spellCaster.cast(expelliarmus, (spellName) -> "The opponent is disarmed by " + spellName);
+        spellCaster.cast(alohomora, (spellName) -> String.format("The door is unlocked by %s", spellName));
+        spellCaster.cast(lumos, (spellName) -> String.format("A beam of light is created by %s", spellName));
+        spellCaster.cast(expelliarmus, (spellName) -> String.format("The opponent is disarmed by %s", spellName));
     }
 }
