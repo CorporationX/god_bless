2f7db5bda280c2cf2d792765d249537782275be4
P-Lidia <Lydiasmolyakova@gmail.com>
2025-04-15 22:18:24 +0300

    Move withErrorHandling() to ErrorHandler class

--- src/main/java/school/faang/errorhandling/main/ErrorHandler.java
+++ src/main/java/school/faang/errorhandling/main/ErrorHandler.java
@@ -1,6 +1,16 @@
 package school.faang.errorhandling.main;
 
+import java.util.function.Supplier;
+
 @FunctionalInterface
 public interface ErrorHandler<T> {
     T handler(Exception e);
+
+    static <T> T withErrorHandling(Supplier<T> action, ErrorHandler<T> handle) {
+        try {
+            return action.get();
+        } catch (Exception e) {
+            return handle.handler(e);
+        }
+    }
 }
--- src/main/java/school/faang/errorhandling/main/Main.java
+++ src/main/java/school/faang/errorhandling/main/Main.java
@@ -2,13 +2,11 @@ package school.faang.errorhandling.main;
 
 import lombok.extern.slf4j.Slf4j;
 
-import java.util.function.Supplier;
-
 @Slf4j
 public class Main {
 
     public static void main(String[] args) {
-        String result = withErrorHandling(
+        String result = ErrorHandler.withErrorHandling(
                 () -> new RemoteService().getData(),
                 e -> {
                     log.warn("Error: " + e.getMessage());
@@ -17,13 +15,5 @@ public class Main {
         );
         System.out.println("Result: " + result);
 
-    }
-
-    public static <T> T withErrorHandling(Supplier<T> action, ErrorHandler<T> handle) {
-        try {
-            return action.get();
-        } catch (Exception e) {
-            return handle.handler(e);
-        }
     }
 }
