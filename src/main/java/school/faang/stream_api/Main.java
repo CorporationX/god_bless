5b3d612033a082b7dfd12e3088e2e55f3fcd03a9
Kirill <kirill551@yandex.ru>
2025-04-15 17:55:51 +0200

    "Style"

--- src/main/java/school/faang/stream_api/Main.java
+++ src/main/java/school/faang/stream_api/Main.java
@@ -7,9 +7,11 @@ import java.util.Set;
 public class Main {
     public static void main(String[] args) {
         System.out.println(StreamHandler.findUniquePairs(Set.of(1, 2, 3, 4, 5, 6), 6));
-        System.out.println(StreamHandler.getCapitals(Map.of("Russia", "Moscow", "USA", "Washington", "Germany", "Berlin")));
+        System.out.println(StreamHandler.getCapitals(Map.of("Russia", "Moscow", "USA", "Washington",
+                "Germany", "Berlin")));
         System.out.println(StreamHandler.transferList(List.of("apple", "banana", "avocado", "apricot"), 'a'));
         System.out.println(StreamHandler.transferToBinaryFormat(List.of(1, 2, 3, 4)));
-        System.out.println(StreamHandler.filterAndSort(List.of("apple", "banana", "cherry", "date", "fig", "grape"), "abcdefghijklmnopqrstuvwxyz"));
+        System.out.println(StreamHandler.filterAndSort(List.of("apple", "banana", "cherry", "date", "fig", "grape"),
+                "abcdefghijklmnopqrstuvwxyz"));
     }
 }
