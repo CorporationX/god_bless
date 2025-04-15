a0149f1077862c696888b45bee18bef102c36ea5
AdminS <neptunchik1997@gmail.com>
2025-04-15 11:37:07 +0300

    task Библиотека Вестероса исправления

--- src/main/java/school/faang/group_users/Main.java
+++ src/main/java/school/faang/group_users/Main.java
@@ -2,25 +2,21 @@ package school.faang.group_users;
 
 import school.faang.group_users.models.User;
 
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 
 public class Main {
     public static void main(String[] args) {
-        List<User> users = new ArrayList<>();
-        users.add(new User("Nik", 50, "SperBankMoney", " Money St 777"));
-        users.add(new User("cucumber", 20, "banduel", "vegetable 15 st"));
-        users.add(new User("tomato", 20, "banduel", "vegetable 15 st"));
+        List<User> users = List.of(
+                new User("Nik", 50, "SperBankMoney", " Money St 777"),
+                new User("cucumber", 20, "Banduel", "vegetable 15 st"),
+                new User("tomato", 20, "Banduel", "vegetable 15 st")
+        );
 
         Map<Integer, List<User>> groupedUsers = User.groupUsers(users);
-        for (Map.Entry<Integer, List<User>> entry : groupedUsers.entrySet()) {
-            int age = entry.getKey();
-            List<User> userList = entry.getValue();
+        groupedUsers.forEach((age, userList) -> {
             System.out.println("Age: " + age);
-            for (User user : userList) {
-                System.out.println("  - " + user.getName());
-            }
-        }
+            userList.forEach(user -> System.out.println("  - " + user.getName()));
+        });
     }
 }
