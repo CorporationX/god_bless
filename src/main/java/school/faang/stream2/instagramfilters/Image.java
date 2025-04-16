cb6f63ce8313b447f3fe04455aa09f091edec944
Aleksandr <alexandr.sidorkin@gmail.com>
2025-04-16 21:30:43 +0200

    Task BJS2-70533 * tasks done

---
+++ src/main/java/school/faang/stream2/instagramfilters/Image.java
@@ -0,0 +1,11 @@
+package school.faang.stream2.instagramfilters;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
+@Getter
+public class Image {
+    private final String name;
+    private final String description;
+}
