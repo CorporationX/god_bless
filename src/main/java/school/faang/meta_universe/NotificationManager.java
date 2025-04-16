e93dc5e59fc6f7aab2a52902d2ee7e8df638eaa6
Likhtin_Nick <kolyangrom2000500@gmail.com>
2025-04-17 03:01:25 +0700

    BJS2-70646
    Исправление замечаний
    @iBerbasov спасибо за замечания! все по существу

--- src/main/java/school/faang/meta_universe/NotificationManager.java
+++ src/main/java/school/faang/meta_universe/NotificationManager.java
@@ -1,22 +1,25 @@
 package school.faang.meta_universe;
 
+import lombok.extern.slf4j.Slf4j;
+
 import java.util.HashMap;
 import java.util.Map;
 import java.util.function.Consumer;
 
+@Slf4j
 public class NotificationManager {
-    Map<NotificationType, Consumer<Notification>> hendlerMap = new HashMap<>();
+    Map<NotificationType, Consumer<Notification>> handlers = new HashMap<>();
 
     public void registerHandler(NotificationType type, Consumer<Notification> handler) {
-        this.hendlerMap.put(type, handler);
+        this.handlers.put(type, handler);
     }
 
     public void sendNotification(Notification notification) {
-        Consumer<Notification> handler = this.hendlerMap.get(notification.getType());
+        Consumer<Notification> handler = this.handlers.get(notification.getType());
         if (handler != null) {
             handler.accept(notification);
         } else {
-            System.out.println("Warning: no found type of notification " + notification.getType());
+            log.info("Warning: no found type of notification {}", notification.getType());
         }
     }
 }
