c0b91b5c4ad222bd8628f76f62368063b9988927
SAM <SULEYMANOV.USA@GMAIL.COM>
2025-04-16 08:46:03 -0700

    Removed extra lines Added @Data

--- src/main/java/school/faang/bjs270450/Email.java
+++ src/main/java/school/faang/bjs270450/Email.java
@@ -1,18 +1,10 @@
 package school.faang.bjs270450;
 
-import lombok.Getter;
-import lombok.Setter;
+import lombok.Data;
 
-@Getter
-@Setter
+@Data
 public class Email {
-    private String subject;
-    private String body;
-    public boolean isImportant;
-
-    public Email(String subject, String body, boolean isImportant) {
-        this.subject = subject;
-        this.body = body;
-        this.isImportant = isImportant;
-    }
+    private final String subject;
+    private final String body;
+    public final boolean isImportant;
 }
--- src/main/java/school/faang/bjs270450/Main.java
+++ src/main/java/school/faang/bjs270450/Main.java
@@ -17,14 +17,10 @@ public class Main {
         );
 
         Predicate<Email> importantFilter = Email::isImportant;
-
         Consumer<Email> printEmail = email -> System.out.printf(
                 "Обработано письмо: %s%n", email.getSubject());
-
         Function<Email, String> toUpperCase = email -> email.getBody().toUpperCase();
-
         emailProcessor.processEmails(emails, importantFilter, toUpperCase, printEmail);
-
         emails.forEach(email -> System.out.printf(
                 "Тема: %s Тело письма: %s%n", email.getSubject(), email.getBody()));
     }
