27df294034d78f00f1dc0ad3346531df3f863e2d
vasidol123rus <vasekku25@gmail.com>
2025-04-16 23:44:41 +0700

    rev.3 у меня нет других классов в этой задаче

--- src/main/java/school/faang/meta/Main.java
+++ src/main/java/school/faang/meta/Main.java
@@ -1,5 +1,7 @@
 package school.faang.meta;
 
+import java.sql.SQLOutput;
+
 public class Main {
     public static void main(String[] args) {
         NotificationManager manager = new NotificationManager();
