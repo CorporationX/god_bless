118867e022f929724d9244f8dff6696196f58a22
Kirill <kirill551@yandex.ru>
2025-04-16 09:50:39 +0200

    "Внести правки согласно комментариям"

--- src/main/java/school/faang/analyze_activity/ActivityAnalyzer.java
+++ src/main/java/school/faang/analyze_activity/ActivityAnalyzer.java
@@ -1,7 +1,6 @@
 package school.faang.analyze_activity;
 
 import java.time.LocalDate;
-import java.util.Collection;
 import java.util.Comparator;
 import java.util.List;
 import java.util.Map;
@@ -14,24 +13,20 @@ public class ActivityAnalyzer {
 
     public static List<String> topUsersByActions(List<UserAction> actions, int topToReturn) {
         return actions.stream()
-                .collect(Collectors.groupingBy(action ->
-                        Map.of(action.getId(), action.getName()), Collectors.counting()))
+                .collect(Collectors.groupingBy(Function.identity(), Collectors.counting()))
                 .entrySet()
                 .stream()
-                .sorted(Comparator.comparingLong((Map.Entry<Map<Integer, String>, Long> list) ->
-                        list.getValue()).reversed())
+                .sorted(Comparator.comparingLong((Map.Entry<UserAction, Long> list) -> list.getValue()).reversed())
                 .limit(topToReturn)
-                .map(Map.Entry::getKey)
-                .map(Map::values)
-                .flatMap(Collection::stream)
+                .map(entry -> entry.getKey().getName())
                 .collect(Collectors.toList());
     }
 
     public static List<String> topHashTags(List<UserAction> actions, int topToReturn) {
         return actions.stream()
-                .filter(action -> (action.getActionType() == ActionType.POST
+                .filter(action -> action.getActionType() == ActionType.POST
                         || action.getActionType() == ActionType.COMMENT)
-                        && !action.getContent().isBlank())
+                .filter(action -> !action.getContent().isBlank())
                 .flatMap(action -> tagPattern.matcher(action.getContent())
                         .results()
                         .map(match -> match.group(1)))
@@ -43,19 +38,16 @@ public class ActivityAnalyzer {
                 .collect(Collectors.toList());
     }
 
+
     public static List<String> topCommentersLastMonth(List<UserAction> actions, int topToReturn) {
         return actions.stream()
                 .filter(action -> action.getActionType() == ActionType.COMMENT
                         && action.getActionDate().isAfter(LocalDate.now().minusMonths(1)))
-                .collect(Collectors.groupingBy(action ->
-                        Map.of(action.getId(), action.getName()), Collectors.counting()))
+                .collect(Collectors.groupingBy(Function.identity(), Collectors.counting()))
                 .entrySet().stream()
-                .sorted(Comparator.comparingLong((Map.Entry<Map<Integer, String>, Long> list) ->
-                        list.getValue()).reversed())
+                .sorted(Comparator.comparingLong((Map.Entry<UserAction, Long> list) -> list.getValue()).reversed())
                 .limit(topToReturn)
-                .map(Map.Entry::getKey)
-                .map(Map::values)
-                .flatMap(Collection::stream)
+                .map(entry -> entry.getKey().getName())
                 .collect(Collectors.toList());
     }
 
