b4c5a03b0034c9e98040aabed761e880dc942eb4
Vladislav Kozlov <littlerock2877@mail.ru>
2025-04-16 21:15:21 +0300

    fix: pr comments

--- src/main/java/school/faang/bjs2_70274/Main.java
+++ src/main/java/school/faang/bjs2_70274/Main.java
@@ -4,8 +4,8 @@ public class Main {
     public static void main(String[] args) {
         SpellCaster spellCaster = new SpellCaster();
 
-        spellCaster.cast("Alohomora", (spell) -> "The door is unlocked by " + spell);
-        spellCaster.cast("Lumos", (spell) -> "A beam of light is created by " + spell);
-        spellCaster.cast("Expelliarmus", (spell) -> "The opponent is disarmed by " + spell);
+        spellCaster.cast("Alohomora", (spell) -> String.format("The door is unlocked by %s", spell));
+        spellCaster.cast("Lumos", (spell) -> String.format("A beam of light is created by %s", spell));
+        spellCaster.cast("Expelliarmus", (spell) -> String.format("The opponent is disarmed by %s", spell));
     }
 }
