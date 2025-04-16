861fb0c3b913fbdf911bb1e3865b1a6a79bc0abc
Likhtin_Nick <kolyangrom2000500@gmail.com>
2025-04-16 14:42:36 +0700

    BJS2-70646
    Первый вариант решения

--- src/main/java/school/faang/meta_universe/NotificationManager.java
+++ src/main/java/school/faang/meta_universe/NotificationManager.java
@@ -8,11 +8,13 @@ public class NotificationManager {
     Map<NotificationType, Consumer<Notification>> map = new HashMap<>();
 
     public void registerHandler(NotificationType type, Consumer<Notification> handler) {
-
+        map.put(type, handler);
     }
 
     public void sendNotification(Notification notification) {
-
+        Consumer<Notification> handler = map.get(notification.getType());
+        if (handler != null) {
+            handler.accept(notification);
+        }
     }
-
 }
