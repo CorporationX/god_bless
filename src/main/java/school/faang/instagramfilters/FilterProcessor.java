badd3fc9c9ee88cd3b01e2b7ccdbe8bdc19d2028
Tanya <hummingbird_t@mail.ru>
2025-04-16 19:27:06 +0300

    начало

---
+++ src/main/java/school/faang/instagramfilters/FilterProcessor.java
@@ -0,0 +1,14 @@
+package school.faang.instagramfilters;
+
+import java.util.function.Function;
+
+public class FilterProcessor {
+
+    public Image applyFilter(Image image, Function<Image, Image> filter){
+
+    }
+
+    public Function<Image, Image> combineFilters(Function<Image, Image> filter1, Function<Image, Image> filter2){
+
+    }
+}
---
+++ src/main/java/school/faang/instagramfilters/Image.java
@@ -0,0 +1,11 @@
+package school.faang.instagramfilters;
+
+import lombok.Data;
+
+import java.util.List;
+
+@Data
+public class Image {
+    private final String name;
+    private final String description;
+}
