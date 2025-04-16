843029015ea099ce6b4e86582afc10c077e0da74
Likhtin_Nick <kolyangrom2000500@gmail.com>
2025-04-16 22:01:13 +0700

    BJS2-70611
    Первое решение

---
+++ src/main/java/school/faang/filters_instagram/Image.java
@@ -0,0 +1,14 @@
+package school.faang.filters_instagram;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.ToString;
+
+@Getter
+@ToString
+@AllArgsConstructor
+public class Image {
+
+    private String name;
+    private String description;
+}
--- src/main/java/school/faang/filters_instagram/Main.java
+++ src/main/java/school/faang/filters_instagram/Main.java
@@ -1,7 +1,31 @@
 package school.faang.filters_instagram;
 
+import lombok.extern.slf4j.Slf4j;
+
+import java.util.function.Function;
+
+@Slf4j
 public class Main {
     public static void main(String[] args) {
+
+        Image originalImage = new Image("original.jpg", "Оригинальное изображение");
+
+        FilterProcessor filterProcessor = new FilterProcessor();
+
+        Function<Image, Image> grayscaleFilter = (image) -> new Image(image.getName(),
+                image.getDescription() + " | Фильтр: черно-белый");
+        Function<Image, Image> sepiaFilter = (image) -> new Image(image.getName(),
+                image.getDescription() + " | Фильтр: сепия");
+
+        Image grayscaleImage = filterProcessor.applyFilter(originalImage, grayscaleFilter);
+        log.info(grayscaleImage.getDescription());
+
+        Image sepiaImage = filterProcessor.applyFilter(grayscaleImage, sepiaFilter);
+        log.info(sepiaImage.getDescription());
+
+        Function<Image, Image> combinedFilter = filterProcessor.combineFilters(grayscaleFilter, sepiaFilter);
+        Image combinedImage = filterProcessor.applyFilter(originalImage, combinedFilter);
+        log.info(combinedImage.getDescription());
 
     }
 }
