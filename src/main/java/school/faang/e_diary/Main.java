c3358d3e7f3158db45cddd4b7037e9514a265477
Kirill <kirill551@yandex.ru>
2025-04-16 22:34:09 +0200

    "Выполнить задачи 1,2"

---
+++ src/main/java/school/faang/e_diary/Main.java
@@ -0,0 +1,24 @@
+package school.faang.e_diary;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+public class Main {
+    public static void main(String[] args) {
+        List<Student> students = new ArrayList<>(
+                List.of(new Student("Иван", "Иванов", Map.of(
+                                "Математика", List.of(5, 4, 3, 4),
+                                "Химия", List.of(4, 5, 4),
+                                "Литература", List.of(3, 3, 4)
+                        )),
+                        new Student("Илья", "Шатков", Map.of(
+                                "Математика", List.of(5, 5, 5, 5),
+                                "Химия", List.of(5, 5, 5),
+                                "Литература", List.of(5, 5, 5))
+                        )));
+        System.out.println(DiaryHandler.getSubjectAvgGrades(students));
+        System.out.println(DiaryHandler.getAvgGradesForStudent(students, "Иван", "Иванов"));
+
+    }
+}
