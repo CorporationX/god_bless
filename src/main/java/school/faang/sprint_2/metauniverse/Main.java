b761b365bae62da02f978bc72109153d3705c366
Raidanmark <Onidanil@Gmail.com>
2025-04-16 23:16:55 +0200

    task finished

--- src/main/java/school/faang/sprint_2/metauniverse/Main.java
+++ src/main/java/school/faang/sprint_2/metauniverse/Main.java
@@ -12,15 +12,19 @@ public class Main {
         notificationManager.registerHandler(NotificationType.SMS,
                 notification -> System.out.println("SMS " + notification.getMessage()));
 
-        Notification emailNotification = new Notification(NotificationType.EMAIL, "Ваш аккаунт активирован");
-        Notification smsNotification = new Notification(NotificationType.SMS, "Ваш пароль изменен");
+        Notification emailNotification = new Notification(NotificationType.EMAIL, "Ваш аккаунт активирован.");
+        Notification smsNotification = new Notification(NotificationType.SMS, "Ваш пароль изменен stupid.");
         Notification pushNotification = new Notification(NotificationType.PUSH, "У вас новое сообщение!");
+
+        notificationManager.registerBadWord(notification -> notification.getMessage().contains("dull"));
+        notificationManager.registerBadWord(notification -> notification.getMessage().contains("moron"));
+        notificationManager.registerBadWord(notification -> notification.getMessage().contains("stupid"));
+
+        notificationManager.registerCorrection(notification ->
+                new Notification(notification.getType(), notification.getMessage() + " from Boss"));
 
         notificationManager.sendNotification(emailNotification);
         notificationManager.sendNotification(smsNotification);
         notificationManager.sendNotification(pushNotification);
-
-        notificationManager.registerBadWord();
-
     }
 }
--- src/main/java/school/faang/sprint_2/metauniverse/NotificationManager.java
+++ src/main/java/school/faang/sprint_2/metauniverse/NotificationManager.java
@@ -7,26 +7,41 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.function.Consumer;
+import java.util.function.Function;
 import java.util.function.Predicate;
 
 @Getter
 public class NotificationManager {
     private Map<NotificationType, Consumer<Notification>> notificationHandler = new HashMap<>();
-    private List<Predicate> badWords = new ArrayList<>();
+    private List<Predicate<Notification>> badWords = new ArrayList<>();
+    private Function<Notification, Notification> notificationCorrector;
 
     public void registerHandler(NotificationType type, Consumer<Notification> handler) {
         notificationHandler.put(type, handler);
     }
 
     public void sendNotification(Notification notification) {
+        if (checkBadWord(notification)) {
+            notification = editNotification(notification);
+        }
+
         notificationHandler.get(notification.getType()).accept(notification);
     }
 
-    public void registerBadWord(Predicate predicate) {
+    public void registerBadWord(Predicate<Notification> predicate) {
         badWords.add(predicate);
     }
 
-    public void checkBadWord(Predicate predicate) {
+    public boolean checkBadWord(Notification notification) {
+        return badWords.stream()
+                .anyMatch(predicate -> predicate.test(notification));
+    }
 
+    public void registerCorrection(Function<Notification, Notification> notificationCorrector) {
+        this.notificationCorrector = notificationCorrector;
+    }
+
+    public Notification editNotification(Notification notification) {
+        return notificationCorrector.apply(notification);
     }
 }
