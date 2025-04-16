8069d2808848c8e0cba3a6c9b307f0917573a077
pirojok_yt <tamik34@mail.ru>
2025-04-16 17:32:22 +0500

    finish

--- src/main/java/school/faang/bjs2_69309/Warrior.java
+++ src/main/java/school/faang/bjs2_69309/Warrior.java
@@ -2,18 +2,18 @@ package school.faang.bjs2_69309;
 
 public class Warrior extends Character {
 
-    public static final int power = 3;
-    public static final int agility = 10;
-    public static final int intelligence = 5;
+    public static final int POWER = 3;
+    public static final int AGILITY = 10;
+    public static final int INTELLIGENCE = 5;
 
     public Warrior(String name) {
-        super(name, power, agility, intelligence);
+        super(name, POWER, AGILITY, INTELLIGENCE);
     }
 
     @Override
     void attack(Character opponent) {
-        if (healthCheck(power, opponent.health)) {
-            opponent.health = opponent.health - power;
+        if (healthCheck(POWER, opponent.health)) {
+            opponent.health = opponent.health - POWER;
         }
     }
 }
