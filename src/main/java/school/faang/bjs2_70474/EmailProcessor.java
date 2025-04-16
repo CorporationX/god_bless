2d9f249a3cb91f39588d7d6f1ed64e0e2962085d
Vladislav Kozlov <littlerock2877@mail.ru>
2025-04-16 21:40:50 +0300

    add: gmail filters

---
+++ src/main/java/school/faang/bjs2_70474/EmailProcessor.java
@@ -0,0 +1,16 @@
+package school.faang.bjs2_70474;
+
+import java.util.List;
+import java.util.function.Consumer;
+import java.util.function.Function;
+import java.util.function.Predicate;
+
+public class EmailProcessor {
+    public void processEmails(List<Email> emails, Predicate<Email> filter, Function<Email, String> converter,
+                              Consumer<Email> handler) {
+        emails.stream()
+                .filter(filter)
+                .peek(email -> email.setBody(converter.apply(email)))
+                .forEach(handler);
+    }
+}
