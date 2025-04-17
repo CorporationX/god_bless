9d4754b1e7e04b0896b0bab533e478c13c8872c0
Nikita Semenov <semenov.nikita@phystech.edu>
2025-04-17 21:15:58 +0300

    Refactoring

--- src/main/java/school/faang/bjs2_69397/Main.java
+++ src/main/java/school/faang/bjs2_69397/Main.java
@@ -16,5 +16,3 @@ public class Main {
         System.out.println(groupedUsers);
     }
 }
-
-
--- src/main/java/school/faang/bjs2_69397/User.java
+++ src/main/java/school/faang/bjs2_69397/User.java
@@ -1,11 +1,13 @@
 package school.faang.bjs2_69397;
 
 import lombok.AllArgsConstructor;
+import lombok.Getter;
 
 import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
 
+@Getter
 @AllArgsConstructor
 public class User {
     private String name;
@@ -14,6 +16,6 @@ public class User {
     private String address;
 
     public static Map<Integer, List<User>> groupUsers(List<User> users) {
-        return users.stream().collect(Collectors.groupingBy(user -> user.age));
+        return users.stream().collect(Collectors.groupingBy(User::getAge));
     }
 }
