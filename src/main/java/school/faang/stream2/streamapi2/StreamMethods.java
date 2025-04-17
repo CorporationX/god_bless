52f16d50dc2ea1d4ea809d34b6b05566aeb5a08f
Aleksandr <alexandr.sidorkin@gmail.com>
2025-04-17 23:49:49 +0200

    BJS2-70807 Init with methods framework

---
+++ src/main/java/school/faang/stream2/streamapi2/StreamMethods.java
@@ -0,0 +1,27 @@
+package school.faang.stream2.streamapi2;
+
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
+public class StreamMethods {
+    public static List<Set<Integer>> findCouples(Set<Integer> set, Integer target) {
+        return null;
+    }
+
+    public static List<String> sortCountriesCapitals(Map<String, String> countryCapitals) {
+        return null;
+    }
+
+    public static List<String> filterAndSort(List<String> strings, Character character) {
+        return null;
+    }
+
+    public static List<String> convertToBinary(List<Integer> numbers) {
+        return null;
+    }
+
+    public static List<String> filterByDictionary(List<String> words, String dictionary) {
+        return null;
+    }
+}
