995b4a503261e403f7fc46eae5d41e75ffc6497b
Robert Abdrakhmanov <rap215@icloud.com>
2025-04-18 00:15:44 +0300

    Products Fixed equals

--- src/main/java/school/faang/sort_amazon_warehouse/Product.java
+++ src/main/java/school/faang/sort_amazon_warehouse/Product.java
@@ -2,15 +2,28 @@ package school.faang.sort_amazon_warehouse;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
-import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
+
+import java.util.Objects;
 
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
-@EqualsAndHashCode
 public class Product {
     private int id;
     private String name;
     private Category category;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Product product = (Product) o;
+        return Objects.equals(name, product.name) && category == product.category;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, name, category);
+    }
 }
