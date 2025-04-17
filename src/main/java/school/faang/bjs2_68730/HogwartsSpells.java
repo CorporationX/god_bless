1e407d1cb93928e283dc2ed25dcfe7939241d968
Gafurova Faribo <faribo.gafur@gmail.com>
2025-04-17 18:01:38 +0300

    BJS2-68730 * генерация айди вынесена в метод создания объекта * var -> типы объектов

--- src/main/java/school/faang/bjs2_68730/HogwartsSpells.java
+++ src/main/java/school/faang/bjs2_68730/HogwartsSpells.java
@@ -7,60 +7,61 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.NoSuchElementException;
+import java.util.concurrent.atomic.AtomicInteger;
 
 @Slf4j
 class HogwartsSpells {
-    private static final Map<Integer, SpellEvent> spellById = new HashMap<>();
-    private static final Map<EventType, List<SpellEvent>> spellsByType = new HashMap<>();
+    private final Map<Integer, SpellEvent> spellById = new HashMap<>();
+    private final Map<EventType, List<SpellEvent>> spellsByType = new HashMap<>();
+    private final Map<SpellEvent, Integer> spellEventIndexes = new HashMap<>();
+    private AtomicInteger counter = new AtomicInteger(0);
 
-    protected static void addSpellEvent(EventType eventType, String actionDescription) {
-        SpellEvent spellEvent = new SpellEvent(eventType, actionDescription);
-        spellById.put(spellEvent.getId(), spellEvent);
+    public void addSpellEvent(EventType eventType, String actionDescription) {
+        SpellEvent spellEvent = new SpellEvent(counter.incrementAndGet(), eventType, actionDescription);
+        this.spellById.put(spellEvent.getId(), spellEvent);
         spellsByType.computeIfAbsent(eventType, spell -> new ArrayList<>()).add(spellEvent);
     }
 
-    protected static SpellEvent getSpellEventById(Integer id) {
-        var spellEvent = spellById.get(id);
+    public SpellEvent getSpellEventById(Integer id) {
+        var spellEvent = this.spellById.get(id);
         if (spellEvent == null) {
             throw new NoSuchElementException("Заклинание с id %d отсутствует".formatted(id));
         }
         return spellEvent;
     }
 
-    protected static List<SpellEvent> getSpellEventsByType(EventType eventType) {
-        var spellEvents = spellsByType.get(eventType);
+    public List<SpellEvent> getSpellEventsByType(EventType eventType) {
+        var spellEvents = this.spellsByType.get(eventType);
         if (spellEvents == null || spellEvents.isEmpty()) {
             throw new NoSuchElementException("Заклинание с eventType %s отсутствует".formatted(eventType));
         }
         return spellEvents;
     }
 
-    protected static void deleteSpellEvent(Integer id) {
-        var spellEvent = spellById.remove(id);
+    public void deleteSpellEvent(Integer id) {
+        var spellEvent = this.spellById.remove(id);
         if (spellEvent == null) {
             throw new NoSuchElementException("Заклинание с id %d отсутствует".formatted(id));
         }
-        var eventType = spellEvent.getEventType();
-        var eventTypes = spellsByType.get(eventType);
-        var indexes = generateIndexes(eventTypes);
-        int index = indexes.get(spellEvent);
+        EventType eventType = spellEvent.getEventType();
+        List<SpellEvent> eventTypes = spellsByType.get(eventType);
+        generateIndexes(eventTypes);
+        int index = spellEventIndexes.get(spellEvent);
         eventTypes.remove(index);
     }
 
-    protected static void printAllSpellEvents() {
+    public void printAllSpellEvents() {
         spellById.forEach((id, event) ->
                 log.info("{} - {}", id, event));
     }
 
-    protected static void printAllSpellsByType() {
+    public void printAllSpellsByType() {
         spellsByType.forEach((event, spell) ->
                 log.info("{} - {}", event, spell));
     }
 
-    private static Map<SpellEvent, Integer> generateIndexes(List<SpellEvent> spellEvents) {
-        var indexes = new HashMap<SpellEvent, Integer>();
+    private void generateIndexes(List<SpellEvent> spellEvents) {
         spellEvents.forEach(event ->
-                indexes.put(event, spellEvents.indexOf(event)));
-        return indexes;
+                spellEventIndexes.put(event, spellEvents.indexOf(event)));
     }
 }
