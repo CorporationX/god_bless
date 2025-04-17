4df48eea22ebd68f274b8532abbc51005db67287
Aleksandr <alexandr.sidorkin@gmail.com>
2025-04-17 12:22:22 +0200

    Task BJS2-702123 Init commit

---
+++ src/main/java/school/faang/stream2/exceptionshandler/ErrorHandler.java
@@ -0,0 +1,7 @@
+package school.faang.stream2.exceptionshandler;
+
+public class ErrorHandler {
+    public static String withErrorHandling(Object someParam, Object o) {
+        return "iasjdf";
+    }
+}
