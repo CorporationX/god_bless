bc2c2f4af8d6da89eb4a7f96a0e4a05975053e1f
Nikita Nikitin <nnandpartners@gmail.com>
2025-04-17 17:13:10 +0300

    BJS2-70769 initial commit

---
+++ src/main/java/school/faang/bjs2_70769/StreamTrainer.java
@@ -0,0 +1,34 @@
+package school.faang.bjs2_70769;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+public class StreamTrainer {
+
+    public static Set<List<Integer>> findUniquePairs(Set<Integer> set, int sum) {
+        return set.stream().filter(x -> set.contains(sum - x))
+                .map(x -> Arrays.asList(x, sum - x)).peek(Collections::sort).collect(Collectors.toSet());
+    }
+
+    public static List<String> getSortedCapitals(Map<String, String> map) {
+        return map.values().stream().sorted().collect(Collectors.toList());
+    }
+
+    public static List<String> filterSortedStrings(List<String> list, char c) {
+        return list.stream().filter(string -> string.startsWith(String.valueOf(c)))
+                .sorted(Comparator.comparingInt(String::length)).toList();
+    }
+
+    public static List<String> convertToBinary(List<Integer> list) {
+        return list.stream().map(Integer::toBinaryString).toList();
+    }
+
+    public static List<String> filterStringsViaAlphabet(List<String> list, String s) {
+        return list.stream().filter(string -> s.chars().anyMatch(c -> string.equals(String.valueOf(c)))).toList();
+    }
+}
---
+++ src/test/java/StreamTrainerTest.java
@@ -0,0 +1,69 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import school.faang.bjs2_70769.StreamTrainer;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
+public class StreamTrainerTest {
+
+    Set<Integer> set;
+    Map<String, String> map;
+    List<String> strings;
+    List<Integer> integers;
+    char a;
+    String s;
+
+    @BeforeEach
+    void setUp() {
+        set = Set.of(1, 2, 3, 4, 5);
+        map = new HashMap<>();
+        map.put("a", "b");
+        map.put("c", "d");
+        map.put("e", "f");
+        strings = List.of("a2", "a13", "a114", "d1115", "e11116");
+        a = 'a';
+        integers = List.of(1, 2, 3, 4);
+        s = "abcfg";
+    }
+
+    @Test
+    public void findUniquePairs() {
+        Set<List<Integer>> result = StreamTrainer.findUniquePairs(set, 5);
+        Set<List<Integer>> expectedSet = Set.of(
+                List.of(1, 4),
+                List.of(2, 3)
+        );
+        Assertions.assertEquals(expectedSet, result);
+    }
+
+    @Test
+    public void getSortedCapitals() {
+        List<String> result = StreamTrainer.getSortedCapitals(map);
+        List<String> expectedList = List.of("b", "d", "f");
+        Assertions.assertEquals(expectedList, result);
+    }
+
+    @Test
+    public void filterSortedStrings() {
+        List<String> result = StreamTrainer.filterSortedStrings(strings, a);
+        List<String> expectedList = List.of("a2", "a13", "a114");
+        Assertions.assertEquals(expectedList, result);
+    }
+
+    @Test
+    public void converToBinary() {
+        List<String> result = StreamTrainer.convertToBinary(integers);
+        List<String> expectedList = List.of("1", "10", "11", "100");
+        Assertions.assertEquals(expectedList, result);
+    }
+
+    @Test
+    public void filterStringsViaAlphabet() {
+        List<String> result = StreamTrainer.filterStringsViaAlphabet(strings, s);
+        List<String> expectedList = List.of("a2", "a13", "a114");
+    }
+}
