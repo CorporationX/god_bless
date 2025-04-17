c8cf829981c9bb7b9b2fc701060825bee3d028d6
Aleksandr <alexandr.sidorkin@gmail.com>
2025-04-17 22:55:46 +0200

    BJS2-70726 Init commit, task framework

---
+++ src/main/java/school/faang/stream2/streamapi1/ListOperations.java
@@ -0,0 +1,33 @@
+package school.faang.stream2.streamapi1;
+
+import java.util.List;
+import java.util.function.Predicate;
+
+public class ListOperations {
+    public static String sumOfEvenNumbers(List<Integer> numbers) {
+    }
+
+    public static String findMax(List<Integer> numbers) {
+    }
+
+    public static String findAverage(List<Integer> numbers) {
+    }
+
+    public static String countStringsStartingWith(List<String> strings, char a) {
+    }
+
+    public static String filterStringsContainingSubstring(List<String> strings, String an) {
+    }
+
+    public static String sortByLength(List<String> strings) {
+    }
+
+    public static <T> String allMatchCondition(List<T> numbers, Predicate<T> predicate) {
+    }
+
+    public static String findMinGreaterThan(List<Integer> numbers, int i) {
+    }
+
+    public static String convertToLengths(List<String> strings) {
+    }
+}
---
+++ src/main/java/school/faang/stream2/streamapi1/Main.java
@@ -0,0 +1,22 @@
+package school.faang.stream2.streamapi1;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) {
+        List<Integer> numbers = Arrays.asList(1, 2, 3, 4, 5, 6);
+        List<String> strings = Arrays.asList("apple", "banana", "cherry", "date");
+
+        // Пример использования методов:
+        System.out.println("Сумма четных чисел: " + ListOperations.sumOfEvenNumbers(numbers));
+        System.out.println("Максимум: " + ListOperations.findMax(numbers));
+        System.out.println("Среднее: " + ListOperations.findAverage(numbers));
+        System.out.println("Количество строк, начинающихся на 'a': " + ListOperations.countStringsStartingWith(strings, 'a'));
+        System.out.println("Строки, содержащие 'an': " + ListOperations.filterStringsContainingSubstring(strings, "an"));
+        System.out.println("Отсортированные по длине строки: " + ListOperations.sortByLength(strings));
+        System.out.println("Все ли числа чётные? " + ListOperations.allMatchCondition(numbers, n -> n % 2 == 0));
+        System.out.println("Наименьшее число больше 4: " + ListOperations.findMinGreaterThan(numbers, 4));
+        System.out.println("Длины строк: " + ListOperations.convertToLengths(strings));
+    }
+}
