e88f1288a76e4861ea4dfca2d560cc3bce7c612e
Daniyal-Akhadov <d.ahanewmail@mail.ru>
2025-04-17 13:06:05 +0300

    feat: бизнес-логика

---
+++ src/main/java/BJS2_69983/DictionaryProcessor.java
@@ -0,0 +1,9 @@
+package BJS2_69983;
+
+import java.util.function.BiConsumer;
+
+public class DictionaryProcessor {
+    public void processWord(String word, String translation, BiConsumer<String, String> consumer) {
+        consumer.accept(word, translation);
+    }
+}
