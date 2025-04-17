27f679dde652208a82bf1fe93aa58c94e12fcbbc
Nikita Nikitin <nnandpartners@gmail.com>
2025-04-17 22:45:10 +0300

    BJS2-70826 fix imports

--- src/main/java/school/faang/bjs2_70826/UserActionAnalyzer.java
+++ src/main/java/school/faang/bjs2_70826/UserActionAnalyzer.java
@@ -1,10 +1,7 @@
 package school.faang.bjs2_70826;
 
 import java.time.LocalDate;
-import java.util.AbstractMap;
 import java.util.Arrays;
-import java.util.Collections;
-import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
 import java.util.Objects;
