07ca2a4139ee065ff578ce12c37ceb0b9132f724
Kirill <kirill551@yandex.ru>
2025-04-17 15:24:47 +0200

    "Внести правки согласно комментариям"

--- src/main/java/school/faang/streamapi1/Main.java
+++ src/main/java/school/faang/streamapi1/Main.java
@@ -4,10 +4,10 @@ import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println(StreamApiHandler.getSum(List.of(1, 2, 3, 4, 5)));
-        System.out.println(StreamApiHandler.maxElem(List.of(1, 2, 3, 4, 5)));
-        System.out.println(StreamApiHandler.getAvg(List.of(1, 2)));
-        System.out.println(StreamApiHandler.getNumberOfStrings(List.of("abc", "atc", "brd"), 'a'));
-        System.out.println(StreamApiHandler.getStringsWithSubstring(List.of("abc", "ybc", "brd"), "bc"));
+        System.out.println(StreamApiOperations.getSum(List.of(1, 2, 3, 4, 5)));
+        System.out.println(StreamApiOperations.maxElement(List.of(1, 2, 3, 4, 5)));
+        System.out.println(StreamApiOperations.getAvg(List.of(1, 2)));
+        System.out.println(StreamApiOperations.getNumberOfStrings(List.of("abc", "atc", "brd"), 'a'));
+        System.out.println(StreamApiOperations.getStringsWithSubstring(List.of("abc", "ybc", "brd"), "bc"));
     }
 }
---
+++ src/main/java/school/faang/streamapi1/StreamApiOperations.java
@@ -0,0 +1,44 @@
+package school.faang.streamapi1;
+
+import java.util.List;
+
+public class StreamApiOperations {
+    public static int getSum(List<Integer> numbers) {
+        return numbers.stream()
+                .filter(num -> num % 2 == 0)
+                .mapToInt(num -> num)
+                .sum();
+    }
+
+    public static int maxElement(List<Integer> numbers) {
+        if (numbers.isEmpty()) {
+            return 0;
+        }
+        return numbers.stream()
+                .mapToInt(num -> num)
+                .max()
+                .getAsInt();
+    }
+
+    public static double getAvg(List<Integer> numbers) {
+        if (numbers.isEmpty()) {
+            return 0;
+        }
+        return numbers.stream()
+                .mapToInt(num -> num)
+                .average()
+                .getAsDouble();
+    }
+
+    public static long getNumberOfStrings(List<String> strings, char symbol) {
+        return strings.stream()
+                .filter(s -> s.startsWith(String.valueOf(symbol)))
+                .count();
+    }
+
+    public static List<String> getStringsWithSubstring(List<String> strings, String str) {
+        return strings.stream()
+                .filter(s -> s.contains(str))
+                .toList();
+    }
+}
