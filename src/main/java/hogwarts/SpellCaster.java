e0adba1bb30692d36b45dad36a24cfce4e5021c4
Emil Dzheenbaev <emil.dzheenbaev@gmail.com>
2025-04-17 20:55:07 +0200

    поправка задачи Хогвартс, Хогвартс, наш любимый Хогвартс!

--- src/main/java/hogwarts/SpellCaster.java
+++ src/main/java/hogwarts/SpellCaster.java
@@ -4,6 +4,6 @@ public class SpellCaster {
 
     public void cast(String spell, SpellAction spellAction) {
         String spellName = spellAction.castSpell(spell);
-        System.out.println("Результат заклинания: " + spellName);
+        System.out.println("Result of the spell: " + spellName);
     }
 }
