1ae1f4e1af92362905eb1654a1c29286bef96cc3
Daniyal-Akhadov <d.ahanewmail@mail.ru>
2025-04-17 14:13:23 +0300

    feat: бизнес-логика

---
+++ src/main/java/Main.java
@@ -0,0 +1,22 @@
+import java.util.function.BiFunction;
+
+public class Main {
+    public static void main(String[] args) {
+        FareCalculator fareCalculator = new FareCalculator();
+
+        BiFunction<Double, Double, Double> economyFare = (distance, time) -> 1.0 * distance + 0.5 * time;
+        BiFunction<Double, Double, Double> comfortFare = (distance, time) -> 1.5 * distance + 0.8 * time;
+        BiFunction<Double, Double, Double> premiumFare = (distance, time) -> 2.0 * distance + 1.5 * time;
+
+        double distance = 10.0;
+        double time = 15.0;
+
+        double economyTripCost = fareCalculator.calculateFare(distance, time, economyFare);
+        double comfortTripCost = fareCalculator.calculateFare(distance, time, comfortFare);
+        double premiumTripCost = fareCalculator.calculateFare(distance, time, premiumFare);
+
+        System.out.println("Стоимость поездки на тарифе Economy: " + economyTripCost);
+        System.out.println("Стоимость поездки на тарифе Comfort: " + comfortTripCost);
+        System.out.println("Стоимость поездки на тарифе Premium: " + premiumTripCost);
+    }
+}
