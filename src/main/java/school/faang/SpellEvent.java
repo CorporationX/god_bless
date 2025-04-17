eec4ad21ce83af37a89d44300c05ab16de256ffb
Sergey Filippovich <filippovich.sergey@gmail.com>
2025-04-17 04:18:14 +0400

    changed class SpellEvent to record SpellEvent

--- src/main/java/school/faang/SpellEvent.java
+++ src/main/java/school/faang/SpellEvent.java
@@ -1,19 +1,3 @@
 package school.faang;
 
-import lombok.Data;
-import lombok.Getter;
-import lombok.Setter;
-
-@Data
-public class SpellEvent {
-    private int id;
-    @Getter @Setter
-    private String eventType;
-    private String action;
-
-    public SpellEvent(int id, String eventType, String action) {
-        this.id = id;
-        this.eventType = eventType;
-        this.action = action;
-    }
-}
+public record SpellEvent (int id, String eventType, String action){}
