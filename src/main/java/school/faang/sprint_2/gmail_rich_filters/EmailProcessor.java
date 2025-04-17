c2dc624dc7aa35adb202f978b807dd403d2e5824
Raidanmark <Onidanil@Gmail.com>
2025-04-17 13:31:39 +0200

    Comments fixes

--- src/main/java/school/faang/sprint_2/gmail_rich_filters/EmailProcessor.java
+++ src/main/java/school/faang/sprint_2/gmail_rich_filters/EmailProcessor.java
@@ -1,18 +1,22 @@
 package school.faang.sprint_2.gmail_rich_filters;
 
+import lombok.extern.slf4j.Slf4j;
+
 import java.util.List;
 import java.util.function.Consumer;
 import java.util.function.Function;
 import java.util.function.Predicate;
 
+@Slf4j
 public class EmailProcessor {
     public void processEmails(List<Email> emails,
                               Predicate<Email> emailFilter,
-                              Function<Email, Email> emailEditor,
+                              Function<Email, String> emailEditor,
                               Consumer<Email> emailHandler) {
         emails.stream()
                 .filter(emailFilter)
+                .peek(emailHandler)
                 .map(emailEditor)
-                .forEach(emailHandler);
+                .forEach(log::info);
     }
 }
--- src/main/java/school/faang/sprint_2/gmail_rich_filters/Main.java
+++ src/main/java/school/faang/sprint_2/gmail_rich_filters/Main.java
@@ -1,11 +1,14 @@
 package school.faang.sprint_2.gmail_rich_filters;
 
+import lombok.extern.slf4j.Slf4j;
+
 import java.util.Arrays;
 import java.util.List;
 import java.util.function.Consumer;
 import java.util.function.Function;
 import java.util.function.Predicate;
 
+@Slf4j
 public class Main {
     public static void main(String[] args) {
         List<Email> emails = Arrays.asList(
@@ -17,16 +20,10 @@ public class Main {
         EmailProcessor emailProcessor = new EmailProcessor();
 
         Predicate<Email> important = Email::isImportant;
-        Function<Email, Email> toUpperCase =
-                email -> new Email(email.subject().toUpperCase(), email.body(), email.isImportant());
+        Function<Email, String> toUpperCase =
+                email -> email.body().toUpperCase();
         Consumer<Email> printEmail = email -> System.out.printf("Обработано письмо: %s%n", email.subject());
 
-        Predicate<Email> spamFilter = email -> email.subject().equals("Спам");
-        Function<Email, Email> censoring = email -> new Email(email.subject(), "*CENSORED*", email.isImportant());
-        Consumer<Email> deletedEmail =
-                email -> System.out.printf("Удаленно письмо: %s - %s%n", email.subject(), email.body());
-
         emailProcessor.processEmails(emails, important, toUpperCase, printEmail);
-        emailProcessor.processEmails(emails, spamFilter, censoring, deletedEmail);
     }
 }
