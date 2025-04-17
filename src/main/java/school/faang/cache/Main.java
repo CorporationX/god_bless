63f2492c018a690b46acadee3cab37ebf5fb53ce
Fagan Gafulov <gafulov@yandex.ru>
2025-04-17 13:28:15 +0300

    BJS2-68738 update task

--- src/main/java/school/faang/cache/Main.java
+++ src/main/java/school/faang/cache/Main.java
@@ -6,26 +6,20 @@ import java.util.Map;
 public class Main {
     public static void main(String[] args) {
 
+        StudentDatabase studentDatabase = new StudentDatabase();
+
         Subject math = new Subject("Математика");
         Subject russia = new Subject("Русский");
         Subject biology = new Subject("Биологи");
         Student student1 = new Student("Олег");
         Student student2 = new Student("Алекс");
 
-        Map<Subject, Ball> subjectAndBallForOleg = new HashMap<>();
-        Map<Subject, Ball> subjectAndBallForAlex = new HashMap<>();
-
-        subjectAndBallForOleg.put(math, Ball.THREE);
-        subjectAndBallForOleg.put(russia, Ball.TWO);
-        subjectAndBallForAlex.put(math, Ball.FIVE);
-        subjectAndBallForAlex.put(biology, Ball.FIVE);
-        subjectAndBallForAlex.put(russia, Ball.FIVE);
-
-        StudentDatabase studentDatabase = new StudentDatabase();
+        Map<Subject, Ball> subjectAndBallForOleg = new HashMap<>(Map.of(math, Ball.THREE, russia, Ball.TWO));
+        Map<Subject, Ball> subjectAndBallForAlex = new HashMap<>(Map.of(math, Ball.FIVE,
+                biology, Ball.FIVE, russia, Ball.FIVE));
 
         studentDatabase.addNewStudentAndSubjectWithBall(student1, subjectAndBallForOleg);
         studentDatabase.addNewStudentAndSubjectWithBall(student2, subjectAndBallForAlex);
-
         studentDatabase.printAllStudentAndSubjectWithBall();
 
         studentDatabase.addSubjectWithBallForOldStudent(student2, math, Ball.THREE);
