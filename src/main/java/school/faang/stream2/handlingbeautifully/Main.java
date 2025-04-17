2724c5fb11736ae7f9856d54461aa430d29b12f5
Tanya <hummingbird_t@mail.ru>
2025-04-17 19:39:54 +0300

    fixed style errors

--- src/main/java/school/faang/stream2/handlingbeautifully/Main.java
+++ src/main/java/school/faang/stream2/handlingbeautifully/Main.java
@@ -4,9 +4,10 @@ public class Main {
     public static void main(String[] args) {
 
         String result = ErrorHandler.withErrorHandling(() -> RemoteService.call("someParam"),
-                e -> {System.out.println("Ошибка при вызове сервиса, возвращаем дефолтное значение");
-            return "DEFAULT";
-        });
+                e -> {
+                    System.out.println("Ошибка при вызове сервиса, возвращаем дефолтное значение");
+                    return "DEFAULT";
+                });
         System.out.println(result);
     }
 
