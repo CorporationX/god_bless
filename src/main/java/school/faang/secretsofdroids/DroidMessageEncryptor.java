8990cd201952255c3dde7f9feafa6926d262bd1f
Anatolii Izotov <izotov.anatoly.e@gmail.com>
2025-04-17 22:42:49 +0200

    redundant code deleted

--- src/main/java/school/faang/secretsofdroids/DroidMessageEncryptor.java
+++ src/main/java/school/faang/secretsofdroids/DroidMessageEncryptor.java
@@ -1,6 +1,5 @@
 package school.faang.secretsofdroids;
 
-import java.util.Arrays;
 import java.util.stream.Collectors;
 
 @FunctionalInterface
@@ -8,22 +7,19 @@ public interface DroidMessageEncryptor {
     String process(String message, int key);
 
     static DroidMessageEncryptor caesar() {
-        return (m, k) ->
-                Arrays.stream(m.split(" "))
-                        .map(word -> word.chars()
-                                .mapToObj(c -> {
-                                    if (Character.isLetter(c)) {
-                                        char base = Character.isUpperCase(c) ? 'A' : 'a';
-                                        int offset = (c - base + k) % 26;
-                                        if (offset < 0) {
-                                            offset += 26;
-                                        }
-                                        return String.valueOf((char) (base + offset));
-                                    } else {
-                                        return String.valueOf((char) (c));
-                                    }
-                                })
-                                .collect(Collectors.joining()))
-                        .collect(Collectors.joining(" "));
+        return (m, k) -> m.chars()
+                .mapToObj(c -> {
+                    if (Character.isLetter(c)) {
+                        char base = Character.isUpperCase(c) ? 'A' : 'a';
+                        int offset = (c - base + k) % 26;
+                        if (offset < 0) {
+                            offset += 26;
+                        }
+                        return String.valueOf((char) (base + offset));
+                    } else {
+                        return String.valueOf((char) (c));
+                    }
+                })
+                .collect(Collectors.joining());
     }
 }
