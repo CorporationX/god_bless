b2179a4e64c712652b603efaf1c1fe588a2585b9
Likhtin_Nick <kolyangrom2000500@gmail.com>
2025-04-17 20:14:49 +0700

    BJS2-70611
    Исправление замечаний от "SnurnicynKonstantin"

--- src/main/java/school/faang/lord_of_the_rings_rpg/Character.java
+++ src/main/java/school/faang/lord_of_the_rings_rpg/Character.java
@@ -8,10 +8,9 @@ import java.util.List;
 @Getter
 public class Character {
     private final String name;
-    private final List<Item> inventory;
+    private final List<Item> inventory = new ArrayList<>();
 
     public Character(String name) {
         this.name = name;
-        this.inventory = new ArrayList<>();
     }
 }
