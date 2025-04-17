06ee85c607b7e2d4347b184744f5355296f4c8eb
Tanya <hummingbird_t@mail.ru>
2025-04-17 10:48:06 +0300

    использовала andThen

--- src/main/java/school/faang/instagramfilters/FilterProcessor.java
+++ src/main/java/school/faang/instagramfilters/FilterProcessor.java
@@ -9,6 +9,6 @@ public class FilterProcessor {
     }
 
     public Function<Image, Image> combineFilters(Function<Image, Image> filter1, Function<Image, Image> filter2) {
-        return image -> filter2.apply(filter1.apply(image));
+        return filter1.andThen(filter2);
     }
 }
