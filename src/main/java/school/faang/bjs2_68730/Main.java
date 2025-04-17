10eb937bcdac109745533baa14da0f771d9c50f1
Gafurova Faribo <faribo.gafur@gmail.com>
2025-04-17 18:01:38 +0300

    BJS2-68730 * генерация айди вынесена в метод создания объекта * var -> типы объектов

--- src/main/java/school/faang/bjs2_68730/Main.java
+++ src/main/java/school/faang/bjs2_68730/Main.java
@@ -5,20 +5,21 @@ import lombok.extern.slf4j.Slf4j;
 @Slf4j
 public class Main {
     public static void main(String[] args) {
-        HogwartsSpells.addSpellEvent(EventType.PROTECTION, "левиосААА");
-        HogwartsSpells.addSpellEvent(EventType.PROTECTION, "Алохамора");
+        HogwartsSpells hogwartsSpells = new HogwartsSpells();
+        hogwartsSpells.addSpellEvent(EventType.PROTECTION, "левиосААА");
+        hogwartsSpells.addSpellEvent(EventType.PROTECTION, "Алохамора");
 
-        HogwartsSpells.printAllSpellEvents();
-        HogwartsSpells.printAllSpellsByType();
+        hogwartsSpells.printAllSpellEvents();
+        hogwartsSpells.printAllSpellsByType();
 
-        var spellEvent = HogwartsSpells.getSpellEventById(1);
+        var spellEvent = hogwartsSpells.getSpellEventById(1);
         log.info(String.valueOf(spellEvent));
 
-        var spellEvents = HogwartsSpells.getSpellEventsByType(EventType.PROTECTION);
+        var spellEvents = hogwartsSpells.getSpellEventsByType(EventType.PROTECTION);
         log.info(String.valueOf(spellEvents));
 
-        HogwartsSpells.deleteSpellEvent(1);
-        HogwartsSpells.printAllSpellEvents();
-        HogwartsSpells.printAllSpellsByType();
+        hogwartsSpells.deleteSpellEvent(1);
+        hogwartsSpells.printAllSpellEvents();
+        hogwartsSpells.printAllSpellsByType();
     }
 }
