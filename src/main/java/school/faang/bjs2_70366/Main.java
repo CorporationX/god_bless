099ed133446b920ed1124d9387da182d5dec3e26
Gigabyte <012_erasyl@mail.ru>
2025-04-18 19:16:21 +0500

    MetaUniverse Commit

--- src/main/java/school/faang/bjs2_70366/Main.java
+++ src/main/java/school/faang/bjs2_70366/Main.java
@@ -1,15 +1,18 @@
 package school.faang.bjs2_70366;
 
 public class Main {
+
+    private static final String ALOHOMORA = "Alohomora";
+    private static final String LUMOS = "Lumos";
+    private static final String EXPELLIARMUS = "Expelliarmus";
+
     public static void main(String[] args) {
         SpellCaster spellCaster = new SpellCaster();
 
-        String alohomora = "Alohomora";
-        String lumos = "Lumos";
-        String expelliarmus = "Expelliarmus";
 
-        spellCaster.cast(alohomora, (spell) -> "The door is unlocked by " + spell);
-        spellCaster.cast(lumos, (spell) -> "A beam of light is created by " + spell);
-        spellCaster.cast(expelliarmus, (spell) -> "The opponent is disarmed by " + spell);
+
+        spellCaster.cast(ALOHOMORA, (spell) -> "The door is unlocked by " + spell);
+        spellCaster.cast(LUMOS, (spell) -> "A beam of light is created by " + spell);
+        spellCaster.cast(EXPELLIARMUS, (spell) -> "The opponent is disarmed by " + spell);
     }
 }
--- src/main/java/school/faang/bjs2_70366/SpellCaster.java
+++ src/main/java/school/faang/bjs2_70366/SpellCaster.java
@@ -2,6 +2,9 @@ package school.faang.bjs2_70366;
 
 public class SpellCaster {
     public void cast(String spellName, SpellAction action) {
+        if (spellName == null || action == null) {
+            throw new IllegalArgumentException("Spell name and action cannot be null");
+        }
         String result = action.perform(spellName);
         System.out.println(result);
     }
