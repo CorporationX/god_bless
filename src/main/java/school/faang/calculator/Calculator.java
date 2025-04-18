d83080ba5b90bbc8f4db1f1186a49b0c7c763e39
Fagan Gafulov <gafulov@yandex.ru>
2025-04-18 09:58:16 +0300

    BJS2-69949 update code

--- src/main/java/school/faang/calculator/Calculator.java
+++ src/main/java/school/faang/calculator/Calculator.java
@@ -1,6 +1,6 @@
 package school.faang.calculator;
 
 @FunctionalInterface
-public interface Calculator<T> {
+public interface Calculator<T extends Number> {
     T calculate(T valueOne, T valueTwo);
 }
--- src/main/java/school/faang/calculator/CalculatorExample.java
+++ src/main/java/school/faang/calculator/CalculatorExample.java
@@ -3,22 +3,21 @@ package school.faang.calculator;
 import java.util.List;
 
 public class CalculatorExample {
-    public static <T> T calculate(List<T> nums, Calculator<T> calculator) {
+    public static <T extends Number> T calculate(List<T> nums, Calculator<T> calculator) {
         if (nums.isEmpty()) {
             throw new IllegalArgumentException("empty list");
         }
-        T result = nums.get(0);
-        for (int i = 1; i < nums.size(); ++i) {
-            if (nums.get(i) == null) {
-                throw new NullPointerException("Null элемент в листе");
+        nums.forEach(num -> {
+            if (num == null) {
+                throw new IllegalArgumentException("Null элемент в листе");
             }
-            result = calculator.calculate(result, nums.get(i));
-        }
-        return result;
+        });
+
+       return nums.stream().reduce(calculator::calculate).get();
     }
 
     public static int sum(List<Integer> nums) {
-        return calculate(nums, (a, b) -> a + b);
+        return calculate(nums, Integer::sum);
     }
 
     public static int product(List<Integer> nums) {
