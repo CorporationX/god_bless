c194a6923e421ef7cd49bb3a61a192b78133f35b
Anatolii Izotov <izotov.anatoly.e@gmail.com>
2025-04-18 10:10:23 +0200

    method process() renamed to encryptOrDecrypt(), caesar field moved to Droid.java class

--- src/main/java/school/faang/secretsofdroids/Droid.java
+++ src/main/java/school/faang/secretsofdroids/Droid.java
@@ -1,14 +1,31 @@
 package school.faang.secretsofdroids;
 
-import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 
+import java.util.stream.Collectors;
+
 @Slf4j
-@AllArgsConstructor
+@RequiredArgsConstructor
 @Getter
 public class Droid {
     private final String name;
+
+    private final DroidMessageEncryptor caesar = (m, k) -> m.chars()
+            .mapToObj(c -> {
+                if (Character.isLetter(c)) {
+                    char base = Character.isUpperCase(c) ? 'A' : 'a';
+                    int offset = (c - base + k) % 26;
+                    if (offset < 0) {
+                        offset += 26;
+                    }
+                    return String.valueOf((char) (base + offset));
+                } else {
+                    return String.valueOf((char) (c));
+                }
+            })
+            .collect(Collectors.joining());
 
     public void sendMessage(String message, int key, Droid receiver) {
         String encryptedMessage = encryptMessage(message, key);
@@ -25,10 +42,10 @@ public class Droid {
     }
 
     private String encryptMessage(String message, int key) {
-        return DroidMessageEncryptor.caesar().process(message, key);
+        return caesar.encryptOrDecrypt(message, key);
     }
 
     private String decryptMessage(String message, int key) {
-        return DroidMessageEncryptor.caesar().process(message, -key);
+        return caesar.encryptOrDecrypt(message, -key);
     }
 }
--- src/main/java/school/faang/secretsofdroids/DroidMessageEncryptor.java
+++ src/main/java/school/faang/secretsofdroids/DroidMessageEncryptor.java
@@ -1,25 +1,6 @@
 package school.faang.secretsofdroids;
 
-import java.util.stream.Collectors;
-
 @FunctionalInterface
 public interface DroidMessageEncryptor {
-    String process(String message, int key);
-
-    static DroidMessageEncryptor caesar() {
-        return (m, k) -> m.chars()
-                .mapToObj(c -> {
-                    if (Character.isLetter(c)) {
-                        char base = Character.isUpperCase(c) ? 'A' : 'a';
-                        int offset = (c - base + k) % 26;
-                        if (offset < 0) {
-                            offset += 26;
-                        }
-                        return String.valueOf((char) (base + offset));
-                    } else {
-                        return String.valueOf((char) (c));
-                    }
-                })
-                .collect(Collectors.joining());
-    }
+    String encryptOrDecrypt(String message, int key);
 }
