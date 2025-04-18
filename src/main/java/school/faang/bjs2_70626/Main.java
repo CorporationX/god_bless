6545b1db7ebe0e50e278659505964b653bdd8745
Gafurova Faribo <faribo.gafur@gmail.com>
2025-04-18 13:50:52 +0300

    BJS2-70626 fix checkStyle

--- src/main/java/school/faang/bjs2_70626/Main.java
+++ src/main/java/school/faang/bjs2_70626/Main.java
@@ -7,9 +7,12 @@ class Main {
     public static void main(String[] args) {
         NotificationManager notificationManager = new NotificationManager();
 
-        notificationManager.registerHandler(NotificationType.EMAIL, notification -> log.info("email: {}", notification.getMessage()));
-        notificationManager.registerHandler(NotificationType.SMS, notification -> log.info("sms: {}", notification.getMessage()));
-        notificationManager.registerHandler(NotificationType.PUSH, notification -> log.info("push: {}", notification.getMessage()));
+        notificationManager.registerHandler(NotificationType.EMAIL, notification ->
+                log.info("email: {}", notification.getMessage()));
+        notificationManager.registerHandler(NotificationType.SMS, notification ->
+                log.info("sms: {}", notification.getMessage()));
+        notificationManager.registerHandler(NotificationType.PUSH, notification ->
+                log.info("push: {}", notification.getMessage()));
 
         Notification emailNotification = new Notification(NotificationType.EMAIL, "Ваш аккаунт активирован");
         Notification smsNotification = new Notification(NotificationType.SMS, "Ваш пароль изменен");
