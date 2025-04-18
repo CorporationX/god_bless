99c66c5c839de227ef9704d1ab523fb11008925e
Gafurova Faribo <faribo.gafur@gmail.com>
2025-04-18 13:45:58 +0300

    BJS2-70626 добавлена обработка нецензурных выражений

--- src/main/java/school/faang/bjs2_70626/NotificationManager.java
+++ src/main/java/school/faang/bjs2_70626/NotificationManager.java
@@ -1,23 +1,33 @@
 package school.faang.bjs2_70626;
 
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 import java.util.function.Consumer;
 
 class NotificationManager {
-    private final Map<NotificationType, Consumer<Notification>> settings = new HashMap<>();
+    private final Map<NotificationType, Consumer<Notification>> notificationSettings = new HashMap<>();
+    private final List<String> censoredWords = List.of("хуй", "пизда", "говно");
 
     public void registerHandler(NotificationType type, Consumer<Notification> handler) {
-        settings.put(type, handler);
+        notificationSettings.put(type, handler);
     }
 
     public void sendNotification(Notification notification) {
-        Consumer<Notification> handler = settings.get(notification.getNotificationType());
+        Consumer<Notification> handler = notificationSettings.get(notification.getNotificationType());
         if (handler != null) {
             handler.accept(notification);
         } else {
             throw new HandlerNotFoundException("Уведомление через %s не реализовано"
                     .formatted(notification.getNotificationType()));
+        }
+    }
+
+    public void censorChecker(Notification notification) {
+        boolean isCensored = censoredWords.stream()
+                .anyMatch(word -> notification.getMessage().contains(word));
+        if (isCensored) {
+            throw new IllegalArgumentException("У нас не матерятся!");
         }
     }
 }
