0a825cc8f84b8e03e3a6975c1daaa6b438bcc1a1
Ilyas Kystaubayev <kystaubaev.iliyas@mail.ru>
2025-04-18 23:45:35 +0500

    Склад Амазон!

---
+++ src/main/java/school/faang/warehouse/Product.java
@@ -0,0 +1,32 @@
+package school.faang.warehouse;
+
+import lombok.Data;
+
+import java.util.Objects;
+
+@Data
+public class Product {
+    private static int counter = 1;
+
+    private int id;
+    private String name;
+    private Category category;
+
+    public Product(String name, Category category) {
+        counter++;
+        this.name = name;
+        this.category = category;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        Product product = (Product) o;
+        return Objects.equals(name, product.name) && category == product.category;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, category);
+    }
+}
