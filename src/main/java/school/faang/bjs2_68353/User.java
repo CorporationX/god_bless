a73fcd99f95f6bfb85991999e4d83a6840ae4797
Nikita Semenov <semenov.nikita@phystech.edu>
2025-04-18 10:53:44 +0300

    Коллекционируем пользователей

---
+++ src/main/java/school/faang/bjs2_68353/User.java
@@ -0,0 +1,31 @@
+package school.faang.bjs2_68353;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import java.util.UUID;
+import java.util.stream.Collectors;
+
+@Getter
+@AllArgsConstructor
+public class User {
+    private UUID id;
+    private String name;
+    private Integer age;
+    private Set<String> activities;
+
+    public static Map<User, String> findHobbyLovers(List<User> users, Set<String> requestedActivities) {
+        return users.stream()
+                .flatMap(user ->
+                        user.getActivities().stream()
+                                .filter(requestedActivities::contains)
+                                .findFirst()
+                                .stream()
+                                .map(activity -> Map.entry(user, activity))
+                )
+                .collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue));
+    }
+}
