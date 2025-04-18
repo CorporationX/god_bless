df45f1974767d0f80802667cfff35ac515a5832f
Artmasterrr <artmasterrr@hotmail.com>
2025-04-18 19:46:41 +0200

    Done

--- src/main/java/school/faang/weather_data/test/Test.java
+++ src/main/java/school/faang/weather_data/test/Test.java
@@ -8,30 +8,19 @@ import school.faang.weather_data.WeatherProvider;
 import school.faang.weather_data.WeatherService;
 
 public class Test {
-
-        public static void main(String[] args) {
-
-            WeatherProvider provider = new WeatherService();
-            WeatherCacheTemplate standardCache = new StandardWeatherCache(provider);
-            WeatherCacheTemplate frequentCache = new FrequentUpdateWeatherCache(provider);
-
-            System.out.println("Standard cache:");
-            WeatherData data1 = standardCache.getWeatherData("Moscow", 5000);
-            System.out.println(data1);
-
-            try {
-                Thread.sleep(2000);
-                WeatherData data2 = standardCache.getWeatherData("Moscow", 5000);
-                System.out.println("Same data (from cache): " + data2);
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
-
-            System.out.println("\nFrequent update cache:");
-            WeatherData data3 = frequentCache.getWeatherData("London", 10000);
-            System.out.println(data3);
-            WeatherData data4 = frequentCache.getWeatherData("London", 10000);
-            System.out.println("Different data (forced update): " + data4);
+    public static void main(String[] args) {
+        WeatherProvider provider = new WeatherService();
+        WeatherCacheTemplate standardCache = new StandardWeatherCache(provider);
+        System.out.println("Standard cache:");
+        WeatherData data1 = standardCache.getWeatherData("Moscow", 5000);
+        System.out.println(data1);
+        try {
+            Thread.sleep(2000);
+            WeatherData data2 = standardCache.getWeatherData("Moscow", 5000);
+            System.out.println("Same data (from cache): " + data2);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
         }
+    }
 }
 
