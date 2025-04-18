dff8f10397bded895dc2306f55aa28eae05c4789
Kirill Frolov <frolov.kirill.17@yandex.ru>
2025-04-18 18:58:58 +0300

    Reduce method replace by collect method

--- src/main/java/school/faang/analayzer/UserActionAnalyzer.java
+++ src/main/java/school/faang/analayzer/UserActionAnalyzer.java
@@ -2,56 +2,28 @@ package school.faang.analayzer;
 
 import java.time.LocalDate;
 import java.util.Comparator;
-import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.function.BiFunction;
-import java.util.function.BinaryOperator;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
+import java.util.stream.Collectors;
 
 public class UserActionAnalyzer {
 
-    private static final BiFunction<Map<String, Integer>, UserAction, Map<String, Integer>> USER_ACTION_COUNTER =
-            (map, userAction) -> {
-                int count = map.getOrDefault(userAction.name(), 0);
-                map.put(userAction.name(), count + 1);
-                return map;
-            };
-
     private static final Pattern HASHTAG_PATTERN = Pattern.compile("#(\\w+)");
 
-    private static final BiFunction<Map<String, Integer>, UserAction, Map<String, Integer>> HASHTAG_COUNTER =
-            (map, userAction) -> {
-                Matcher matcher = HASHTAG_PATTERN.matcher(userAction.content());
-                String hashtag = matcher.find() ? matcher.group() : "";
-                int count = map.getOrDefault(hashtag, 0);
-                map.put(hashtag, count + 1);
-                return map;
-            };
+    private static String getHashtag(UserAction action) {
+        Matcher matcher = HASHTAG_PATTERN.matcher(action.content());
+        return matcher.find() ? matcher.group() : "";
+    }
 
-    private static final BiFunction<Map<String, Integer>, UserAction, Map<String, Integer>> ACTION_TYPE_COUNTER =
-            (map, userAction) -> {
-                int count = map.getOrDefault(userAction.actionType().name(), 0);
-                map.put(userAction.actionType().name(), count + 1);
-                return map;
-            };
-
-    private static final BinaryOperator<Map<String, Integer>> COMBINER =
-            (totalMap, intermidiateMap) -> {
-                totalMap.putAll(intermidiateMap);
-                return totalMap;
-            };
-
-    private static final Comparator<Map.Entry<String, Integer>> MAP_BY_VALUE_REVERSED_COMPARATOR =
+    private static final Comparator<Map.Entry<String, Long>> MAP_BY_VALUE_REVERSED_COMPARATOR =
             Map.Entry
-                    .comparingByValue(Comparator.comparingInt(Integer::intValue).reversed());
+                    .comparingByValue(Comparator.comparingInt(Long::intValue).reversed());
 
     public static List<String> topActiveUsers(List<UserAction> actions, int limit) {
         return actions.stream()
-                .reduce(new HashMap<>(),
-                        USER_ACTION_COUNTER,
-                        COMBINER)
+                .collect(Collectors.groupingBy(UserAction::name, Collectors.counting()))
                 .entrySet()
                 .stream()
                 .sorted(MAP_BY_VALUE_REVERSED_COMPARATOR)
@@ -63,9 +35,7 @@ public class UserActionAnalyzer {
     public static List<String> topPopularHashtags(List<UserAction> actions, int limit) {
         return actions.stream()
                 .filter(userAction -> userAction.content().contains("#"))
-                .reduce(new HashMap<>(),
-                        HASHTAG_COUNTER,
-                        COMBINER)
+                .collect(Collectors.groupingBy(UserActionAnalyzer::getHashtag, Collectors.counting()))
                 .entrySet()
                 .stream()
                 .sorted(MAP_BY_VALUE_REVERSED_COMPARATOR)
@@ -78,9 +48,7 @@ public class UserActionAnalyzer {
         return actions.stream()
                 .filter(userAction -> userAction.actionType().equals(ActionType.COMMENT)
                         && userAction.localDate().isAfter(LocalDate.now().minusMonths(1)))
-                .reduce(new HashMap<>(),
-                        USER_ACTION_COUNTER,
-                        COMBINER)
+                .collect(Collectors.groupingBy(UserAction::name, Collectors.counting()))
                 .entrySet()
                 .stream()
                 .sorted(MAP_BY_VALUE_REVERSED_COMPARATOR)
@@ -90,13 +58,12 @@ public class UserActionAnalyzer {
     }
 
     public static Map<String, String> actionTypePercentages(List<UserAction> actions) {
-        Map<String, String> result = new HashMap<>();
-        actions.stream()
-                .reduce(new HashMap<>(),
-                        ACTION_TYPE_COUNTER,
-                        COMBINER)
-                .forEach((key, value) ->
-                        result.put(key, "%.1f %%".formatted(Double.valueOf(value) / actions.size() * 100)));
-        return result;
+        return actions.stream()
+                .collect(Collectors.groupingBy(userAction ->
+                        userAction.actionType().name(), Collectors.counting()))
+                .entrySet()
+                .stream()
+                .collect(Collectors.toMap(Map.Entry::getKey, entry ->
+                        "%.1f %%".formatted(Double.valueOf(entry.getValue()) / actions.size() * 100)));
     }
 }
