4e1a44522c049f34dc277ee81702d754f85c897b
pirojok_yt <tamik34@mail.ru>
2025-04-18 19:05:47 +0500

    finish

--- src/main/java/school/faang/bjs2_71594/Main.java
+++ src/main/java/school/faang/bjs2_71594/Main.java
@@ -6,8 +6,10 @@ public class Main {
     public static void main(String[] args) {
         NotificationManager notificationManager = new NotificationManager();
         Consumer<Notification> smsConsumer = notification -> System.out.println("SMS: " + notification.getMessage());
-        Consumer<Notification> emailConsumer = notification -> System.out.println("Email: " + notification.getMessage() + "@meta.com");
-        Consumer<Notification> pushConsumer = notification -> System.out.println("Push notification: " + notification.getMessage());
+        Consumer<Notification> emailConsumer = notification ->
+                System.out.println("Email: " + notification.getMessage() + "@meta.com");
+        Consumer<Notification> pushConsumer = notification ->
+                System.out.println("Push notification: " + notification.getMessage());
 
         notificationManager.registerHandler(NotificationType.SMS, smsConsumer);
         notificationManager.registerHandler(NotificationType.EMAIL, emailConsumer);
