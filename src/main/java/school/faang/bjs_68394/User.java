4cdb6791ed1422c1a9936a91ef82966da26e8b27
Yulia <bualex1983@mail.ru>
2025-04-18 21:10:19 +0300

    Fix compilation error

--- src/main/java/school/faang/bjs_68394/User.java
+++ src/main/java/school/faang/bjs_68394/User.java
@@ -1,10 +1,10 @@
 package school.faang.bjs_68394;
 
-import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -17,6 +17,7 @@ public class User {
     private int age;
     private String workplace;
     private String address;
+    private static String users;
 
     public User(String name, int age, String workplace, String address) {
         this.name = name;
@@ -27,6 +28,19 @@ public class User {
 
     public static Map<Integer, List<User>> groupUsers() {
         Map<Integer, List<User>> result = new HashMap<>();
+
+        for (User user : users) {
+            int userAge = user.getAge();
+
+            if (result.containsKey(userAge)) {
+                result.get(userAge).add(user);
+            } else {
+                List<User> list = new ArrayList<>();
+                list.add(user);
+                result.put(userAge, list);
+            }
+        }
+
         return result;
     }
 }
