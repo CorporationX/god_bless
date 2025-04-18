1aa7b383108e8997c5e6958e95543dfc14687c72
Gafurova Faribo <faribo.gafur@gmail.com>
2025-04-18 16:20:47 +0300

    BJS2-70626 fix censorChecker

--- src/main/java/school/faang/bjs2_70626/NotificationManager.java
+++ src/main/java/school/faang/bjs2_70626/NotificationManager.java
@@ -4,29 +4,30 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.function.Consumer;
+import java.util.function.Predicate;
 
 class NotificationManager {
-    private final Map<NotificationType, Consumer<Notification>> notificationSettings = new HashMap<>();
+    private final Map<NotificationType, Consumer<Notification>> handler = new HashMap<>();
     private final List<String> censoredWords = List.of("хуй", "пизда", "говно");
 
     public void registerHandler(NotificationType type, Consumer<Notification> handler) {
-        notificationSettings.put(type, handler);
+        this.handler.put(type, handler);
     }
 
     public void sendNotification(Notification notification) {
-        Consumer<Notification> handler = notificationSettings.get(notification.getNotificationType());
-        if (handler != null) {
-            handler.accept(notification);
+        censorChecker(notification1 -> censoredWords.stream().anyMatch(word ->
+                notification1.getMessage().contains(word)), notification);
+        Consumer<Notification> handle = this.handler.get(notification.getNotificationType());
+        if (handle != null) {
+            handle.accept(notification);
         } else {
             throw new HandlerNotFoundException("Уведомление через %s не реализовано"
                     .formatted(notification.getNotificationType()));
         }
     }
 
-    public void censorChecker(Notification notification) {
-        boolean isCensored = censoredWords.stream()
-                .anyMatch(word -> notification.getMessage().contains(word));
-        if (isCensored) {
+    private void censorChecker(Predicate<Notification> checker, Notification notification) {
+        if (checker.test(notification)) {
             throw new IllegalArgumentException("У нас не матерятся!");
         }
     }
