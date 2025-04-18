d853160660a30f15f6812c8a1348ce0acf0699b3
loofaxu <sabeta.sabawa@gmail.com>
2025-04-18 20:22:47 +0300

    Пожалуйста, не говорите убрать комментарии. Решила задачу с Божьей помощью и хотелось бы иметь возможность вернуться к задаче и увидеть краткие комментари.

---
+++ src/main/java/school/faang/bjs270519/Image.java
@@ -0,0 +1,19 @@
+package school.faang.bjs270519;
+
+public class Image {
+    private String name;
+    private String description;
+
+    public Image(String name, String description) {
+        this.name = name;
+        this.description = description;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+}
