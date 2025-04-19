daa473beeec5a6bd312f7b36bc5e15c6ba570790
Vladislav Kozlov <littlerock2877@mail.ru>
2025-04-19 20:38:33 +0300

    fix: PR comments

--- src/main/java/school/faang/bjs2_70739/ListOperations.java
+++ src/main/java/school/faang/bjs2_70739/ListOperations.java
@@ -1,54 +1,57 @@
 package school.faang.bjs2_70739;
 
+import lombok.experimental.UtilityClass;
+
 import java.util.Comparator;
 import java.util.List;
 import java.util.function.Predicate;
 
+@UtilityClass
 public class ListOperations {
-    public static int sumOfEvenNumbers(List<Integer> numbers) {
+    public int sumOfEvenNumbers(List<Integer> numbers) {
         return numbers.stream()
                 .filter(num -> num % 2 == 0)
-                .mapToInt(num -> num)
-                .sum();
+                .reduce((a, b) -> a + b)
+                .orElseThrow(() -> new IllegalArgumentException("List doesn't contain even numbers"));
     }
 
-    public static int findMaxNumber(List<Integer> numbers) {
+    public int findMaxNumber(List<Integer> numbers) {
         return numbers.stream()
                 .max(Comparator.comparingInt(a -> a))
                 .orElseThrow(() -> new IllegalArgumentException("List is empty"));
     }
 
-    public static double countAverageNumber(List<Integer> numbers) {
+    public double countAverageNumber(List<Integer> numbers) {
         return numbers.stream()
                 .mapToInt(num -> num)
                 .average()
                 .orElseThrow(() -> new IllegalArgumentException("List is empty"));
     }
 
-    public static long countStringsStartWith(List<String> strings, char symbol) {
+    public long countStringsStartWith(List<String> strings, char symbol) {
         return strings.stream()
                 .filter(string -> string.charAt(0) == symbol)
                 .count();
     }
 
-    public static List<String> stringContainsSubstring(List<String> strings, String substring) {
+    public List<String> stringContainsSubstring(List<String> strings, String substring) {
         return strings.stream()
                 .filter(string -> string.contains(substring))
                 .toList();
     }
 
-    public static List<String> sortStringsByLength(List<String> strings) {
+    public List<String> sortStringsByLength(List<String> strings) {
         return strings.stream()
                 .sorted(Comparator.comparingInt(String::length))
                 .toList();
     }
 
-    public static boolean checkListOfIntegers(List<Integer> numbers, Predicate<Integer> predicate) {
+    public boolean checkListOfIntegers(List<Integer> numbers, Predicate<Integer> predicate) {
         return numbers.stream()
                 .allMatch(predicate);
     }
 
-    public static int findFirstBiggerThanCondition(List<Integer> numbers, int condition) {
+    public int findFirstBiggerThanCondition(List<Integer> numbers, int condition) {
         return numbers.stream()
                 .sorted()
                 .filter(num -> num > condition)
@@ -57,7 +60,7 @@ public class ListOperations {
                         new IllegalArgumentException("List don't contains number bigger than %s".formatted(condition)));
     }
 
-    public static List<Integer> mapStringsIntoLengths(List<String> strings) {
+    public List<Integer> mapStringsIntoLengths(List<String> strings) {
         return strings.stream()
                 .map(String::length)
                 .toList();
