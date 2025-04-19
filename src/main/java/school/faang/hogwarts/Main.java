0e3edab7e70639bf5b3a44b8c59087ada3d857c5
AdminS <neptunchik1997@gmail.com>
2025-04-19 21:17:35 +0300

    fix: hogwarts

--- src/main/java/school/faang/hogwarts/Main.java
+++ src/main/java/school/faang/hogwarts/Main.java
@@ -8,8 +8,8 @@ public class Main {
         String lumos = "Lumos";
         String expelliarmus = "Expelliarmus";
 
-        spellCaster.cast(alohomora, (cast) -> "The door is unlocked by " + cast);
-        spellCaster.cast(lumos, (cast) -> "A beam of light is created by " + cast);
+        spellCaster.cast(alohomora, (cast) -> "The door is unlocked by %s".formatted(cast));
+        spellCaster.cast(lumos, (cast) -> "A beam of light is created by %s".formatted(cast));
         spellCaster.cast(expelliarmus, (cast) -> cast + " - " + cast);
     }
 }
