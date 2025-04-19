4ea2a0335fcaf79ee0273ce0a1436b20384e2557
Raidanmark <Onidanil@Gmail.com>
2025-04-19 17:19:26 +0200

    Task finished

--- src/main/java/school/faang/sprint_2/calculator/Main.java
+++ src/main/java/school/faang/sprint_2/calculator/Main.java
@@ -1,7 +1,12 @@
 package school.faang.sprint_2.calculator;
 
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
+        List<Integer> data = List.of(2, 3, 4);
 
+        System.out.println("Сумма: " + CalculatorManager.sum(data));       // 9
+        System.out.println("Произведение: " + CalculatorManager.product(data)); // 24
     }
 }
