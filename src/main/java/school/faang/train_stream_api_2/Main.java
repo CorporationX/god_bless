46a3bcc1438a3a858bd2d8ef47fd0075fcb17564
Likhtin_Nick <kolyangrom2000500@gmail.com>
2025-04-19 17:47:18 +0700

    BJS2-70953
    Для проверки (2 часть)

--- src/main/java/school/faang/train_stream_api_2/Main.java
+++ src/main/java/school/faang/train_stream_api_2/Main.java
@@ -1,5 +1,7 @@
 package school.faang.train_stream_api_2;
 
+import lombok.extern.slf4j.Slf4j;
+
 import java.util.Arrays;
 import java.util.HashMap;
 import java.util.HashSet;
@@ -7,32 +9,36 @@ import java.util.List;
 import java.util.Map;
 import java.util.Set;
 
-import static school.faang.train_stream_api_2.ListOperations.getCapitalsSortedByCountry;
-import static school.faang.train_stream_api_2.ListOperations.uniqueCouples;
-
+@Slf4j
 public class Main {
     public static void main(String[] args) {
 
         Set<Integer> numbers = new HashSet<>(Arrays.asList(1, 2, 3, 4, 5, 6));
         int target = 7;
+        log.info("1️⃣ Найдите уникальные пары чисел {}",
+                ListOperations.uniqueCouples(numbers, target));
 
-        Set<Set<Integer>> pairs = uniqueCouples(numbers, target);
-        System.out.println(pairs);
+        Map<String, String> countries = new HashMap<>() {{
+                put("Germany", "Berlin");
+                put("France", "Paris");
+                put("Italy", "Rome");
+                put("Spain", "Madrid");
+                }};
 
-        Map<String, String> countries = new HashMap<>();
-        countries.put("Germany", "Berlin");
-        countries.put("France", "Paris");
-        countries.put("Italy", "Rome");
-        countries.put("Spain", "Madrid");
-
-        List<String> capitals = getCapitalsSortedByCountry(countries);
-        System.out.println(capitals);
-
+        log.info("2️⃣ Отсортируйте страны и выведите столицы {}",
+                ListOperations.getCapitalsSortedByCountry(countries));
 
         List<String> strings = Arrays.asList("apple", "banana", "avocado", "apricot");
-        ListOperations.filteringAndSortingLines(strings, "");
-        System.out.println(strings);
+        log.info("3️⃣ Фильтрация и сортировка строк, начинающихся на 'a': {}",
+                ListOperations.filteringAndSortingLines(strings, "ap"));
 
+        List<Integer> numbers2 = Arrays.asList(19, 2, 3, 4, 5, 6, 9, 10, 13, 14, 19);
+        log.info("4️⃣ Преобразование чисел в двоичный формат : {}",
+                ListOperations.transformationOfNumbersIntoBinaryFormat(numbers2));
 
+        List<String> words = Arrays.asList("apple", "banana", "cherry", "date", "fig", "grape");
+        log.info("5️⃣ Фильтрация строк по алфавиту и сортировка по длине : {}",
+                ListOperations.filterAndSortByAlphabetAndLength(words,
+                        "abcdefghijklmnopqrstuvwxyz"));
     }
 }
