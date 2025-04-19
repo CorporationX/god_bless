debacd124421bbb999291ac96078cee1b23b620b
Emil Dzheenbaev <emil.dzheenbaev@gmail.com>
2025-04-19 18:36:18 +0200

    поправка задачи Потренируем StreamAPI 1

--- src/main/java/trainstreamapi1/Main.java
+++ src/main/java/trainstreamapi1/Main.java
@@ -13,10 +13,12 @@ public class Main {
         System.out.println("Сумма четных чисел: " + ListOperations.sumEvenNumbers(numbers));
         System.out.println("Максимум: " + ListOperations.findMaxNumber(numbers));
         System.out.println("Среднее: " + ListOperations.findAvgNumber(numbers));
-        System.out.println("Количество строк, начинающихся на 'a': " + ListOperations.countStringsStartingWith(strings, 'a'));
+        System.out.println("Количество строк, начинающихся на 'a': " +
+                ListOperations.countStringsStartingWith(strings, 'a'));
         System.out.println("Строки, содержащие 'an': " + ListOperations.filterStringsContaining(strings, "an"));
         System.out.println("Отсортированные по длине строки: " + ListOperations.sortStringsByLength(strings));
-        System.out.println("Все ли числа чётные? " + ListOperations.allElementsMatchCondition(numbers, n -> n % 2 == 0));
+        System.out.println("Все ли числа чётные? " +
+                ListOperations.allElementsMatchCondition(numbers, n -> n % 2 == 0));
         System.out.println("Наименьшее число больше 4: " + ListOperations.findSmallestElementGreaterThan(numbers, 4));
         System.out.println("Длины строк: " + ListOperations.mapStringsToLengths(strings));
     }
