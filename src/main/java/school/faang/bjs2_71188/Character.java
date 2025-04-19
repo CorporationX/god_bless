12ebf94e1e20e26e24ac838ee2b6648620db13a9
v888.rus <v888.rus@gmail.com>
2025-04-19 17:07:34 +0000

    Испралвение (2). 71188 Lord of the Rings RPG.

--- src/main/java/school/faang/bjs2_71188/Character.java
+++ src/main/java/school/faang/bjs2_71188/Character.java
@@ -4,17 +4,13 @@ import java.util.ArrayList;
 import java.util.List;
 
 import lombok.Getter;
-import lombok.NonNull;
 import lombok.RequiredArgsConstructor;
-import lombok.Setter;
 
 @Getter
-@Setter
 @RequiredArgsConstructor
 public class Character {
-    @NonNull
-    private String name;
-    private List<Item> inventory = new ArrayList<>();
+    private final String name;
+    private final List<Item> inventory = new ArrayList<>();
 
     public void addItemToInventory(Item item) {
         inventory.add(item);
