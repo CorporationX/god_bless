f67d7ba9cad7b7cb5b1dfc2f088598dfa77661a3
loofaxu <sabeta.sabawa@gmail.com>
2025-04-19 20:17:48 +0300

    Пожалуйста, не говорите убрать комментарии. Решила задачу с Божьей помощью и хотелось бы иметь возможность вернуться к задаче и увидеть краткие комментари.

--- src/main/java/school/faang/bjs270519/Image.java
+++ src/main/java/school/faang/bjs270519/Image.java
@@ -1,19 +1,12 @@
 package school.faang.bjs270519;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
 public class Image {
     private String name;
     private String description;
 
-    public Image(String name, String description) {
-        this.name = name;
-        this.description = description;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public String getDescription() {
-        return description;
-    }
 }
