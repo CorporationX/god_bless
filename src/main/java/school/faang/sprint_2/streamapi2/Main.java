b315d81a7fd1f2f51ee921833e342b51cfb5e99e
Daniil-Raider <Onidanil@Gmail.com>
2025-04-19 18:42:58 +0200

    BJS2-70748: Stream API 2 (#19391)
    * branch created
    
    * first three methods
    
    * task finished
    
    * Comments fixes
    + additional tasks

---
+++ src/main/java/school/faang/sprint_2/streamapi2/Main.java
@@ -0,0 +1,33 @@
+package school.faang.sprint_2.streamapi2;
+
+import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
+public class Main {
+    public static void main(String[] args) {
+        Set<Integer> numbers = new HashSet<>(Set.of(1, 2, 3, 4, 5, 6, 7, 8, 9, 10));
+        Integer target = 10;
+
+        System.out.println(Processor.findPairsEqualTarget(numbers, target));
+
+        Map<String, String> countriesWithCapital = Map.of(
+                "Denmark", "Copenhagen",
+                "USA", "Washington",
+                "Germany", "Berlin"
+        );
+
+        System.out.println(Processor.sortCountryByAlphabetAndReturnCapitals(countriesWithCapital));
+
+        List<Integer> numbersToBinary = List.of(1, 2, 5, 10);
+        System.out.println(Processor.convertingNumbersToBinaryFormat(numbersToBinary));
+
+        List<String> fruits = List.of("apple", "banana", "avocado", "apricot");
+        System.out.println(Processor.filterAndSortWords(fruits, 'a'));
+
+        List<String> words = List.of("apple", "banana", "cherry", "date", "fig", "grape");
+        String alphabet = "abcdefghijklmnopqrstuvwxyz";
+        System.out.println(Processor.filterByAlphabetAndSortWords(words, alphabet));
+    }
+}
---
+++ src/main/java/school/faang/sprint_2/streamapi2/Processor.java
@@ -0,0 +1,48 @@
+package school.faang.sprint_2.streamapi2;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import java.util.regex.Pattern;
+import java.util.stream.Collectors;
+
+public class Processor {
+    public static Set<List<Integer>> findPairsEqualTarget(Set<Integer> numbers, Integer target) {
+        return numbers
+                .stream()
+                .filter(number -> numbers.contains(target - number))
+                .filter(number -> number != target - number)
+                .map(number -> Arrays.asList(number, target - number))
+                .peek(Collections::sort)
+                .collect(Collectors.toSet());
+    }
+
+    public static List<String> sortCountryByAlphabetAndReturnCapitals(Map<String, String> countriesWithCapitals) {
+        return countriesWithCapitals.keySet().stream().sorted(Comparator.naturalOrder())
+                .map(country -> countriesWithCapitals.get(country))
+                .toList();
+    }
+
+    public static List<String> filterAndSortWords(List<String> words, char symbol) {
+        return words.stream()
+                .filter(word -> word.startsWith(String.valueOf(symbol)))
+                .sorted(Comparator.comparingInt(String::length))
+                .toList();
+    }
+
+    public static List<String> convertingNumbersToBinaryFormat(List<Integer> numbers) {
+        return numbers.stream()
+                .map(Integer::toBinaryString)
+                .toList();
+    }
+
+    public static List<String> filterByAlphabetAndSortWords(List<String> words, String alphabet) {
+        return words.stream()
+                .filter(word -> word.matches("^[" + Pattern.quote(alphabet) + "]+$"))
+                .sorted(Comparator.comparingInt(String::length))
+                .toList();
+    }
+}
