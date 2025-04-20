c5b9b28bfae690a5d3bfb5279a89ce092f0480e1
tanufa 25 <tanufa25@gmail.com>
2025-04-20 16:26:18 +0300

    bjs2-70559 Instagram filters task is done

---
+++ src/main/java/school/faang/bjs2_70559/instagram_filters/FilterNotFound.java
@@ -0,0 +1,7 @@
+package school.faang.bjs2_70559.instagram_filters;
+
+public class FilterNotFound extends RuntimeException {
+    public FilterNotFound(String message) {
+        super(message);
+    }
+}
