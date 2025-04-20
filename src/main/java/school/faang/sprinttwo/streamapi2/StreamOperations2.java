23315f0bac21a48829c46728f2fbf81f2c6ed647
andre <andrey.kuimov2015@yandex.ru>
2025-04-21 00:19:28 +0300

    kenig8649: BJS2-70743 - Потренируем StreamAPI 2

--- src/main/java/school/faang/sprinttwo/streamapi2/StreamOperations2.java
+++ src/main/java/school/faang/sprinttwo/streamapi2/StreamOperations2.java
@@ -1,12 +1,16 @@
 package school.faang.sprinttwo.streamapi2;
 
+import lombok.experimental.UtilityClass;
+
+import java.util.Comparator;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
 import java.util.stream.Collectors;
 
+@UtilityClass
 public class StreamOperations2 {
-    public static Set<Set<Integer>> unicPairOfNumbers(Set<Integer> numbers, int target) {
+    public Set<Set<Integer>> unicPairOfNumbers(Set<Integer> numbers, int target) {
         return numbers.stream()
                 .flatMap(a -> numbers.stream()
                         .filter(b -> a + b == target && a < b)
@@ -14,14 +18,31 @@ public class StreamOperations2 {
                 .collect(Collectors.toSet());
     }
 
-    public static List<String> getCapitalOfCountry(Map<String, String> capital) {
+    public List<String> getCapitalOfCountry(Map<String, String> capital) {
         return capital.entrySet().stream()
                 .sorted(Map.Entry.comparingByKey())
                 .map(Map.Entry::getValue)
                 .toList();
     }
 
+    public List<String> sortbyChareAndOrderByLenght(List<String> fruits, Character ch) {
+        return fruits.stream()
+                .filter(fr -> fr.startsWith(Character.toString(ch)))
+                .sorted(Comparator.comparingInt(String::length))
+                .toList();
+    }
 
+    public List<String> getListOfPrimeNumbers(List<Integer> listOfPrimeNumbers) {
+        return listOfPrimeNumbers.stream()
+                .map(Integer::toBinaryString)
+                .toList();
+    }
 
-
+    public List<String> getWordsWithAlphabetLetter(List<String> fruits, String alphabet) {
+        Set<Integer> alphabetSet = alphabet.codePoints().boxed().collect(Collectors.toSet());
+        return fruits.stream()
+                .filter(word -> word.codePoints().allMatch(alphabetSet::contains))
+                .sorted(Comparator.comparingInt(String::length))
+                .collect(Collectors.toList());
+    }
 }
