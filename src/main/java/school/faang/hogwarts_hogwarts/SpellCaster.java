e9d9be46adf6befb4b1ce122a65867e18f497056
Robert Abdrakhmanov <rap215@icloud.com>
2025-04-20 20:37:49 +0300

    Hogwarts Fix

--- src/main/java/school/faang/hogwarts_hogwarts/SpellCaster.java
+++ src/main/java/school/faang/hogwarts_hogwarts/SpellCaster.java
@@ -1,7 +1,7 @@
 package school.faang.hogwarts_hogwarts;
 
 public class SpellCaster {
-    public void cast(String spellName, SpellAction spellAction){
+    public void cast(String spellName, SpellAction spellAction) {
         String result = spellAction.castSpell(spellName);
         System.out.println("Result of spell is: " + result);
 
