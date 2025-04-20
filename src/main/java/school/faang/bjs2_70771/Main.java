a1eccd2960f27217b6de11818f7f79e16eab9750
Valeria <valeriia.shurupina@yahoo.com>
2025-04-20 18:03:38 +0200

    BJS2-70771 too long lines

--- src/main/java/school/faang/bjs2_70771/Main.java
+++ src/main/java/school/faang/bjs2_70771/Main.java
@@ -11,11 +11,16 @@ public class Main {
         System.out.println("Sum of even numbers: " + ListOperations.sumOfEvenNumbers(numbers));
         System.out.println("Maximum: " + ListOperations.findMax(numbers));
         System.out.println("Average: " + ListOperations.findAverage(numbers));
-        System.out.println("Number of strings starting with 'a': " + ListOperations.countStringsStartingWith(strings, 'a'));
-        System.out.println("Strings containing 'an': " + ListOperations.filterStringsContainingSubstring(strings, "an"));
-        System.out.println("Strings sorted by length: " + ListOperations.sortByLength(strings));
-        System.out.println("Are all numbers even? " + ListOperations.allMatchCondition(numbers, n -> n % 2 == 0));
-        System.out.println("Smallest number greater than 4: " + ListOperations.findMinGreaterThan(numbers, 4));
+        System.out.println("Number of strings starting with 'a': " +
+                ListOperations.countStringsStartingWith(strings, 'a'));
+        System.out.println("Strings containing 'an': " +
+                ListOperations.filterStringsContainingSubstring(strings, "an"));
+        System.out.println("Strings sorted by length: " +
+                ListOperations.sortByLength(strings));
+        System.out.println("Are all numbers even? " +
+                ListOperations.allMatchCondition(numbers, n -> n % 2 == 0));
+        System.out.println("Smallest number greater than 4: " +
+                ListOperations.findMinGreaterThan(numbers, 4));
         System.out.println("String lengths: " + ListOperations.convertToLengths(strings));
     }
 }
