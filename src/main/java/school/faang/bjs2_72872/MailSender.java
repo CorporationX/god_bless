9b4a97869e718faa75a9e4b0a58b28df67681423
AlexandrPopov <164056516+Aleck210popov@users.noreply.github.com>
2025-04-20 17:34:40 +0300

    BJS2-72872 fix hardkod

--- src/main/java/school/faang/bjs2_72872/MailSender.java
+++ src/main/java/school/faang/bjs2_72872/MailSender.java
@@ -2,17 +2,29 @@ package school.faang.bjs2_72872;
 
 import lombok.extern.slf4j.Slf4j;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Slf4j
 public class MailSender {
+    private static final int TOTAL_LETTERS = 1000;
+    private static final int THREADS_COUNT = 5;
+
     public static void main(String[] args) {
-        List<Thread> threads = List.of(
-                new Thread(new SenderRunnable(1, 200)),
-                new Thread(new SenderRunnable(201, 400)),
-                new Thread(new SenderRunnable(401, 600)),
-                new Thread(new SenderRunnable(601, 800)),
-                new Thread(new SenderRunnable(801, 1000)));
+        List<Integer> letters = new ArrayList<>();
+        for (int i = 1; i <= TOTAL_LETTERS; i++) {
+            letters.add(i);
+        }
+
+        List<Thread> threads = new ArrayList<>();
+        int batchSize = TOTAL_LETTERS / THREADS_COUNT;
+        for (int i = 0; i < THREADS_COUNT; i++) {
+            int start = letters.get(i * batchSize);
+            int end = letters.get((i == THREADS_COUNT - 1) ? TOTAL_LETTERS - 1 : ((i + 1) * batchSize - 1));
+            System.out.println(start);
+            System.out.println(end);
+            threads.add(new Thread(new SenderRunnable(start, end)));
+        }
 
         threads.forEach(Thread::start);
 
--- src/main/java/school/faang/bjs2_72872/SenderRunnable.java
+++ src/main/java/school/faang/bjs2_72872/SenderRunnable.java
@@ -1,20 +1,17 @@
 package school.faang.bjs2_72872;
 
+import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
+@AllArgsConstructor
 public class SenderRunnable implements Runnable {
     private final int startIndex;
     private final int endIndex;
 
-    public SenderRunnable(int startIndex, int endIndex) {
-        this.startIndex = startIndex;
-        this.endIndex = endIndex;
-    }
-
     @Override
     public void run() {
-        for (int i = startIndex; i < endIndex; i++) {
+        for (int i = startIndex; i <= endIndex; i++) {
             log.info(String.format("Letter %d have been sent", i));
         }
     }
