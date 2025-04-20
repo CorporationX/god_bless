5b14f4831d528f6b0f2020b67f1c74a38064026b
Kirill Frolov <frolov.kirill.17@yandex.ru>
2025-04-20 18:13:27 +0300

    Task BJS2-72687
    1) Sender runnable class added

---
+++ src/main/java/school/faang/microsoft/Main.java
@@ -0,0 +1,33 @@
+package school.faang.microsoft;
+
+import lombok.extern.slf4j.Slf4j;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Slf4j
+public class Main {
+
+    private static final int THREAD_COUNT = 5;
+    private static final int BATCH_SIZE = 200;
+
+    public static void main(String[] args) {
+        List<Thread> threads = new ArrayList<>();
+        for (int i = 0; i < THREAD_COUNT; i++) {
+            Thread newThread = new Thread(new SenderRunnable(i * BATCH_SIZE, (i + 1) * BATCH_SIZE));
+
+            threads.add(newThread);
+            newThread.start();
+            log.info("{} - started}", newThread.getName());
+        }
+
+        threads.forEach(thread -> {
+            try {
+                thread.join();
+                log.info("{} - joined}", thread.getName());
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+        });
+    }
+}
---
+++ src/main/java/school/faang/microsoft/SenderRunnable.java
@@ -0,0 +1,22 @@
+package school.faang.microsoft;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+public class SenderRunnable implements Runnable {
+
+    private final int startIndex;
+    private final int endIndex;
+
+    public SenderRunnable(int startIndex, int endIndex) {
+        this.startIndex = startIndex;
+        this.endIndex = endIndex;
+    }
+
+    @Override
+    public void run() {
+        for (int i = startIndex; i < endIndex; i++) {
+            log.info("{}: Message number {} is send", Thread.currentThread().getName(), i + 1);
+        }
+    }
+}
