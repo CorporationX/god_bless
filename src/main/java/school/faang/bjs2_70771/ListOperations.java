e00ed3d49f3d9a447cd1d2d6e355358e2f14b976
Valeria <valeriia.shurupina@yahoo.com>
2025-04-20 17:35:30 +0200

    BJS2-70771 use stream api

---
+++ src/main/java/school/faang/bjs2_70771/ListOperations.java
@@ -0,0 +1,64 @@
+package school.faang.bjs2_70771;
+
+import java.util.Comparator;
+import java.util.List;
+import java.util.function.Predicate;
+import java.util.stream.Collectors;
+
+public class ListOperations {
+
+    public static int sumOfEvenNumbers(List<Integer> numbers) {
+        return numbers.stream()
+                .filter(n -> n % 2 == 0)
+                .reduce(0, Integer::sum);
+    }
+
+    public static int findMax(List<Integer> numbers) {
+        return numbers.stream()
+                .max(Integer::compareTo)
+                .orElseThrow(() -> new IllegalArgumentException("List is empty"));
+    }
+
+    public static double findAverage(List<Integer> numbers) {
+        return numbers.stream()
+                .mapToInt(Integer::intValue)
+                .average()
+                .orElse(0.0);
+    }
+
+    public static long countStringsStartingWith( List<String> strings, char character) {
+        return strings.stream()
+                .filter(s -> s.startsWith(String.valueOf(character)))
+                .count();
+    }
+
+    public static List<String> filterStringsContainingSubstring(List<String> numbers, String string) {
+        return numbers.stream()
+                .filter(s -> s.contains(string))
+                .collect(Collectors.toList());
+    }
+
+    public static List<String> sortByLength(List<String> strings) {
+        return strings.stream()
+                .sorted(Comparator.comparingInt(String::length))
+                .collect(Collectors.toList());
+    }
+
+    public static boolean allMatchCondition(List<Integer> numbers, Predicate<Integer> condition) {
+        return numbers.stream()
+                .allMatch(condition);
+    }
+
+    public static int findMinGreaterThan(List<Integer> numbers, Integer number) {
+        return numbers.stream()
+                .filter(n -> n > number)
+                .min(Integer::compareTo)
+                .orElseThrow(() -> new IllegalArgumentException("Not found"));
+    }
+
+    public static List<Integer> convertToLengths(List<String> strings) {
+        return strings.stream()
+                .map(String::length)
+                .collect(Collectors.toList());
+    }
+}
