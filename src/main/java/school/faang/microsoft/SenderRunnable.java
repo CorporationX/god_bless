e1dc9a4375723db8829ba0efaa7ff8d8d235d279
Fagan Gafulov <gafulov@yandex.ru>
2025-04-20 22:48:19 +0300

    BJS2-72666 CORRECTION OF COMMENTS

--- src/main/java/school/faang/microsoft/SenderRunnable.java
+++ src/main/java/school/faang/microsoft/SenderRunnable.java
@@ -1,16 +1,13 @@
 package school.faang.microsoft;
 
+import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 
+@AllArgsConstructor
 @Slf4j
 public class SenderRunnable implements Runnable {
     private final int startIndex;
     private final int endIndex;
-
-    public SenderRunnable(int startIndex, int endIndex) {
-        this.startIndex = startIndex;
-        this.endIndex = endIndex;
-    }
 
     @Override
     public void run() {
