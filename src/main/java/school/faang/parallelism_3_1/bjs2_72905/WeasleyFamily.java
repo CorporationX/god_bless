120003a740c872c89b2517f09fe46d0908254cec
Gafurova Faribo <faribo.gafur@gmail.com>
2025-04-21 17:51:04 +0300

    * реализовано завершение задач * List<Chore> -> List<String> * добавлено логирование завершения потоков

--- src/main/java/school/faang/parallelism_3_1/bjs2_72905/WeasleyFamily.java
+++ src/main/java/school/faang/parallelism_3_1/bjs2_72905/WeasleyFamily.java
@@ -1,19 +1,35 @@
 package school.faang.parallelism_3_1.bjs2_72905;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 
 import java.util.List;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
 
+@Slf4j
 @RequiredArgsConstructor
 public class WeasleyFamily {
-    static final List<Chore> chores = List.of(
-            new Chore("помыть посуду"), new Chore("подмести пол"), new Chore("приготовить ужин"));
+    static final List<String> chores = List.of("помыть посуду", "подмести пол", "приготовить ужин");
 
     public static void main(String[] args) {
         ExecutorService cachedThreadPool = Executors.newCachedThreadPool();
-        chores.forEach(cachedThreadPool::execute);
+
+        for (String chore : chores) {
+            Chore task = new Chore(chore);
+            cachedThreadPool.execute(task);
+        }
         cachedThreadPool.shutdown();
+        try {
+            if (!cachedThreadPool.awaitTermination(5, TimeUnit.MINUTES)) {
+                log.warn("Задачи не завершились за 5 минут, принудительно останавливаем...");
+                cachedThreadPool.shutdownNow(); // Принудительное завершение, если задачи зависли
+            }
+        } catch (InterruptedException e) {
+            // Если главный поток был прерван во время ожидания, принудительно останавливаем пул
+            cachedThreadPool.shutdownNow();
+        }
+        log.info("Все задачи выполнены!");
     }
 }
