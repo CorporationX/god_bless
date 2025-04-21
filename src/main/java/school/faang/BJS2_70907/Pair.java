7beb034b7b776daf68184b118dbf7e6f7ad48b23
Prigozhaeva-Anastasia <prigozhaeva03@mail.ru>
2025-04-21 21:32:04 +0300

    fix

--- src/main/java/school/faang/BJS2_70907/Pair.java
+++ src/main/java/school/faang/BJS2_70907/Pair.java
@@ -15,9 +15,13 @@ public class Pair <T> {
 
     @Override
     public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        Pair<?> pair = (Pair<?>) o;
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        Pair <?> pair = (Pair <?>) o;
         return (Objects.equals(key, pair.key) && Objects.equals(value, pair.value))
                 || (Objects.equals(key, pair.value) && Objects.equals(value, pair.key));
     }
