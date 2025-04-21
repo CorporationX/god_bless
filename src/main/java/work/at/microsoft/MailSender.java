dc193ddfac571c4cc2139614ef69d599f5ba5c03
Buga Dan <danbil.942@gmail.com>
2025-04-21 16:59:12 +0300

    Work at Microsoft task BSJ2-72733.

--- src/main/java/work/at/microsoft/MailSender.java
+++ src/main/java/work/at/microsoft/MailSender.java
@@ -3,7 +3,7 @@ package work.at.microsoft;
 public class MailSender {
     public static void main(String[] args) {
         final Thread threadOne = new Thread(new SenderRunnable(0, 200));
-        final Thread threadTwo = new Thread(new SenderRunnable(2001, 400));
+        final Thread threadTwo = new Thread(new SenderRunnable(201, 400));
         final Thread threadThree = new Thread(new SenderRunnable(401, 600));
         final Thread threadFour = new Thread(new SenderRunnable(601, 800));
         final Thread threadFive = new Thread(new SenderRunnable(801, 1000));
