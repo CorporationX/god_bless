8ea7f0c0b5cc93debc7e3461056b500047dc4f7f
Nikita Semenov <semenov.nikita@phystech.edu>
2025-04-21 15:23:08 +0300

    Meta-вселенная?

---
+++ src/main/java/school/faang/bjs2_71975/Main.java
@@ -0,0 +1,27 @@
+package school.faang.bjs2_71975;
+
+public class Main {
+    public static void main(String [] args) {
+        NotificationManager notificationManager = new NotificationManager();
+
+        notificationManager.registerHandler(NotificationType.EMAIL,
+                notification -> System.out.println("Email: " + notification.message())
+        );
+
+        notificationManager.registerHandler(NotificationType.SMS,
+                notification -> System.out.println("SMS: " + notification.message())
+        );
+
+        notificationManager.registerHandler(NotificationType.PUSH,
+                notification -> System.out.println("Push Notification: " + notification.message())
+        );
+
+        Notification emailNotification = new Notification(NotificationType.EMAIL, "Ваш аккаунт активирован");
+        Notification smsNotification = new Notification(NotificationType.SMS, "Ваш пароль изменен");
+        Notification pushNotification = new Notification(NotificationType.PUSH, "У вас новое сообщение!");
+
+        notificationManager.sendNotification(emailNotification);
+        notificationManager.sendNotification(smsNotification);
+        notificationManager.sendNotification(pushNotification);
+    }
+}
