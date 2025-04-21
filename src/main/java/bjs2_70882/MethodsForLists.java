b2cd4a7eeff2ba9aa9971e156fe91eb6d709204b
Slava Vermilion <beg.slava@bk.ru>
2025-04-21 21:13:37 +0700

    Attempt #2

--- src/main/java/bjs2_70882/MethodsForLists.java
+++ src/main/java/bjs2_70882/MethodsForLists.java
@@ -1,12 +1,15 @@
 package bjs2_70882;
 
+import lombok.experimental.UtilityClass;
+
 import java.util.*;
 import java.util.stream.Collectors;
 
+@UtilityClass
 public class MethodsForLists {
     public static Set<List<Integer>> findUniquePairsOfNumbers(Set<Integer> numbers, int targetNumber) {
         return numbers.stream()
-                .filter(num -> numbers.contains(targetNumber-num) && num != targetNumber - num)
+                .filter(num -> numbers.contains(targetNumber - num) && num != targetNumber - num)
                 .map(num -> Arrays.asList(num, targetNumber - num))
                 .peek(Collections::sort)
                 .collect(Collectors.toSet());
