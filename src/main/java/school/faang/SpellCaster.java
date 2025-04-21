5e0a168d88ac4837bd3b08d163e080c6d3f4d120
Andrew Sonin <sonin.andrei2009@yandex.ru>
2025-04-21 23:25:46 +0300

    Task BJS2-70537
    1) Create Class
    2) Create Interface

---
+++ src/main/java/school/faang/SpellCaster.java
@@ -0,0 +1,10 @@
+package school.faang;
+
+public class SpellCaster {
+
+    public void cast(String spellName, SpellAction action) {
+        String result = action.spell(spellName);
+        System.out.println("Результат заклинания '" + spellName + "': " + result);
+    }
+
+}
