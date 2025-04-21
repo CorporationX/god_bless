e206677694a1f26b707fcb80a4773129373039e4
JolyGolden <nazi00696@mail.ru>
2025-04-21 11:35:53 +0200

    Stream Api 1st task

---
+++ src/main/java/school/faang/stream_api_first_task/ListOperations.java
@@ -0,0 +1,49 @@
+package school.faang.stream_api_first_task;
+
+import java.util.Comparator;
+import java.util.List;
+import java.util.NoSuchElementException;
+import java.util.function.Predicate;
+
+public class ListOperations {
+    public static int sumEvenNums(List<Integer> nums) {
+        return nums.stream().filter(num -> num % 2 == 0)
+                .mapToInt(Integer::intValue).sum();
+    }
+
+    public static int findBiggestElement(List<Integer> nums) {
+        return nums.stream().max(Integer::compareTo)
+                .orElseThrow(() -> new NoSuchElementException("List is empty"));
+    }
+
+    public static double getAverage(List<Integer> nums) {
+        return nums.stream().mapToInt(Integer::intValue)
+                .average().orElse(0.0);
+    }
+
+    public static long findAmountOfStringsStartsWith(List<String> strings, char symbol) {
+        return strings.stream()
+                .filter(string -> !string.isEmpty() && string
+                        .charAt(0) == symbol).count();
+    }
+
+    public static List<String> sortStringsByLength(List<String> strings) {
+        return strings.stream()
+                .sorted(Comparator.comparingInt(String::length)).toList();
+    }
+
+    public static boolean isElementToCondition(List<Integer> nums, Predicate<Integer> predicate) {
+        return nums.stream().allMatch(predicate);
+    }
+
+    public static int findLowestElementAtList(List<Integer> nums, int num) {
+        return nums.stream()
+                .filter(n -> n > num)
+                .min(Integer::compareTo)
+                .orElseThrow(() -> new NoSuchElementException("No elements greater than" + num));
+    }
+
+    public static List<Integer> transformStringsToLengths(List<String> nums) {
+        return nums.stream().map(String::length).toList();
+    }
+}
---
+++ src/main/java/school/faang/stream_api_first_task/Main.java
@@ -0,0 +1,23 @@
+package school.faang.stream_api_first_task;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) {
+        List<Integer> numbers = Arrays.asList(1, 2, 3, 4, 5, 6);
+        List<String> strings = Arrays.asList("apple", "banana", "cherry", "date");
+
+        System.out.println("Сумма четных чисел: " + ListOperations.sumEvenNums(numbers));
+
+        System.out.println("Максимум: " + ListOperations.findBiggestElement(numbers));
+        System.out.println("Среднее: " + ListOperations.getAverage(numbers));
+        System.out.println("Количество строк, начинающихся на 'a': " +
+                ListOperations.findAmountOfStringsStartsWith(strings, 'a'));
+        System.out.println("Строки, содержащие 'an': " + ListOperations.findAmountOfStringsStartsWith(strings, 'd'));
+        System.out.println("Отсортированные по длине строки: " + ListOperations.sortStringsByLength(strings));
+        System.out.println("Все ли числа чётные? " + ListOperations.isElementToCondition(numbers, n -> n % 2 == 0));
+        System.out.println("Наименьшее число больше 4: " + ListOperations.findLowestElementAtList(numbers, 4));
+        System.out.println("Длины строк: " + ListOperations.transformStringsToLengths(strings));
+    }
+}
