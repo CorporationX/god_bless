b8e8ecd8ae99d1cd528a9e36d2e502682a384b35
Gafurova Faribo <faribo.gafur@gmail.com>
2025-04-21 11:52:18 +0300

    fix LineLength

--- src/main/java/school/faang/java_core_1_2/bjs2_68093/Warrior.java
+++ src/main/java/school/faang/java_core_1_2/bjs2_68093/Warrior.java
@@ -2,7 +2,9 @@ package school.faang.java_core_1_2.bjs2_68093;
 
 public class Warrior extends Character {
     public Warrior(String name) {
-        super(name, GameProperties.DEFAULT_WARRIOR_STRENGTH, GameProperties.DEFAULT_WARRIOR_AGILITY, GameProperties.DEFAULT_WARRIOR_INTELLECT);
+        super(name, GameProperties.DEFAULT_WARRIOR_STRENGTH,
+                GameProperties.DEFAULT_WARRIOR_AGILITY,
+                GameProperties.DEFAULT_WARRIOR_INTELLECT);
     }
 
     public Warrior(String name, int strength, int agility, int intellect) {
