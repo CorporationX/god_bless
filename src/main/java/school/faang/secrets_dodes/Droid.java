4cf8201ccb162bc4d3268414bab7da3e4ab1355d
Likhtin_Nick <kolyangrom2000500@gmail.com>
2025-04-21 21:35:44 +0700

    BJS2-70607
    Исправление замечаний от "Yusupov Marsel"

--- src/main/java/school/faang/secrets_dodes/Droid.java
+++ src/main/java/school/faang/secrets_dodes/Droid.java
@@ -1,37 +1,39 @@
 package school.faang.secrets_dodes;
 
+import lombok.AllArgsConstructor;
+import school.faang.functional_interface.DroidMessageEncryptor;
+
+@AllArgsConstructor
 public class Droid {
 
-    public Droid(String s) {
+    private String name;
+
+    private static final DroidMessageEncryptor ENCRYPTOR = (msg, key) -> {
+        StringBuilder encrypted = new StringBuilder();
+        for (char c : msg.toCharArray()) {
+            encrypted.append((char) (c + key));
+        }
+        return encrypted.toString();
+    };
+
+    public void sendMessage(Droid a, String message, int encryptionKey) {
+
+        String encryptedMessage = encryptMessage(message, encryptionKey);
+
+        a.receiveMessage(encryptedMessage, encryptionKey);
     }
 
-    public void sendMessage(Droid c3po, String message1, int encryptionKey1) {
-
-        String encryptedMessage = encryptMessage(message1, encryptionKey1);
-
-        c3po.receiveMessage(encryptedMessage);
-    }
-
-    public void receiveMessage(String encryptedMessage) {
-        int encryptionKey = 3;
+    public void receiveMessage(String encryptedMessage, int encryptionKey) {
         String decryptedMessage = decryptMessage(encryptedMessage, encryptionKey);
 
         System.out.println("Received and decrypted message: " + decryptedMessage);
     }
 
     private String decryptMessage(String encryptedMessage, int encryptionKey) {
-        StringBuilder decrypted = new StringBuilder();
-        for (char c : encryptedMessage.toCharArray()) {
-            decrypted.append((char) (c - encryptionKey));
-        }
-        return decrypted.toString();
+        return ENCRYPTOR.encrypt(encryptedMessage, -encryptionKey);
     }
 
     private String encryptMessage(String message, int encryptionKey) {
-        StringBuilder encrypted = new StringBuilder();
-        for (char c : message.toCharArray()) {
-            encrypted.append((char) (c + encryptionKey));
-        }
-        return encrypted.toString();
+        return ENCRYPTOR.encrypt(message, encryptionKey);
     }
 }
