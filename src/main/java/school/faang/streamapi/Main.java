06325d5f4f3c44d59afb1f4a98f78391c9244b48
Gigabyte <012_erasyl@mail.ru>
2025-04-22 12:16:09 +0500

    Stream API part 1 Commit

--- src/main/java/school/faang/streamapi/Main.java
+++ src/main/java/school/faang/streamapi/Main.java
@@ -11,10 +11,13 @@ public class Main {
         System.out.printf("Сумма четных чисел: %d%n", ListOperations.sumOfEvenNumbers(numbers));
         System.out.printf("Максимум: %d%n", ListOperations.findMax(numbers));
         System.out.printf("Среднее: %.2f%n", ListOperations.findAverage(numbers));
-        System.out.printf("Количество строк, начинающихся на 'a': %d%n", ListOperations.countStringsStartingWith(strings, 'a'));
-        System.out.printf("Строки, содержащие 'an': %s%n", ListOperations.filterStringsContainingSubstring(strings, "an"));
+        System.out.printf("Количество строк, начинающихся на 'a': %d%n",
+                ListOperations.countStringsStartingWith(strings, 'a'));
+        System.out.printf("Строки, содержащие 'an': %s%n",
+                ListOperations.filterStringsContainingSubstring(strings, "an"));
         System.out.printf("Отсортированные по длине строки: %s%n", ListOperations.sortByLength(strings));
-        System.out.printf("Все ли числа чётные? %b%n", ListOperations.allMatchCondition(numbers, n -> n % 2 == 0));
+        System.out.printf("Все ли числа чётные? %b%n",
+                ListOperations.allMatchCondition(numbers, n -> n % 2 == 0));
         System.out.printf("Наименьшее число больше 4: %d%n", ListOperations.findMinGreaterThan(numbers, 4));
         System.out.printf("Длины строк: %s%n", ListOperations.convertToLengths(strings));
     }
