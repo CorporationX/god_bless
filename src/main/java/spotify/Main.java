3fcd85479ccef0005b2ee29ef61bd285886c8131
Emil Dzheenbaev <emil.dzheenbaev@gmail.com>
2025-04-22 21:38:28 +0200

    решение задачи Блокировка Spotify

---
+++ src/main/java/spotify/Main.java
@@ -0,0 +1,27 @@
+package spotify;
+
+public class Main {
+
+    public static void main(String[] args) throws InterruptedException {
+        Player player = new Player();
+
+        Thread playThread = new Thread(player::play);
+        playThread.start();
+
+        Thread pauseThread = new Thread(player::pause);
+        pauseThread.start();
+
+        Thread skipThread = new Thread(player::skip);
+        skipThread.start();
+
+        Thread previousThread = new Thread(player::previous);
+        previousThread.start();
+
+        playThread.join();
+        pauseThread.join();
+        skipThread.join();
+        previousThread.join();
+
+        System.out.println("Done");
+    }
+}
