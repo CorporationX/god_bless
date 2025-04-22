312a6624648503c6ebd283b88c3be60529ff4efa
Gigabyte <012_erasyl@mail.ru>
2025-04-22 12:13:17 +0500

    Stream API part 1 Commit

---
+++ src/main/java/school/faang/streamapi/ListOperations.java
@@ -0,0 +1,65 @@
+package school.faang.streamapi;
+
+import java.util.Comparator;
+import java.util.List;
+import java.util.function.Predicate;
+import java.util.stream.Collectors;
+
+public class ListOperations {
+    public static int sumOfEvenNumbers(List<Integer> numbers) {
+        return numbers.stream()
+                .filter(n -> n % 2 == 0)
+                .mapToInt(Integer::intValue)
+                .sum();
+    }
+
+    public static int findMax(List<Integer> numbers) {
+        return numbers.stream()
+                .mapToInt(Integer::intValue)
+                .max()
+                .orElseThrow(() -> new IllegalArgumentException("Список пуст"));
+    }
+
+    public static double findAverage(List<Integer> numbers) {
+        return numbers.stream()
+                .mapToInt(Integer::intValue)
+                .average()
+                .orElseThrow(() -> new IllegalArgumentException("Список пуст"));
+    }
+
+    public static long countStringsStartingWith(List<String> strings, char startingLetter) {
+        return strings.stream()
+                .filter(s -> !s.isEmpty() && s.charAt(0) == startingLetter)
+                .count();
+    }
+
+    public static List<String> filterStringsContainingSubstring(List<String> strings, String substring) {
+        return strings.stream()
+                .filter(s -> s.contains(substring))
+                .collect(Collectors.toList());
+    }
+
+    public static List<String> sortByLength(List<String> strings) {
+        return strings.stream()
+                .sorted(Comparator.comparingInt(String::length))
+                .collect(Collectors.toList());
+    }
+
+    public static boolean allMatchCondition(List<Integer> numbers, Predicate<Integer> condition) {
+        return numbers.stream()
+                .allMatch(condition);
+    }
+
+    public static int findMinGreaterThan(List<Integer> numbers, int number) {
+        return numbers.stream()
+                .filter(num -> num > number)
+                .min(Integer::compare)
+                .orElseThrow(() -> new IllegalArgumentException("Нет элемента больше " + number));
+    }
+
+    public static List<Integer> convertToLengths(List<String> strings) {
+        return strings.stream()
+                .map(String::length)
+                .collect(Collectors.toList());
+    }
+}
