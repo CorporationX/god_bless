df7675582e08a551dfd4598ba53abdc9bc85e6c2
Raidanmark <Onidanil@Gmail.com>
2025-04-22 17:37:35 +0200

    Comments fixes

--- src/main/java/school/faang/sprint_2/remember_school/TriangleAreaCalculator.java
+++ src/main/java/school/faang/sprint_2/remember_school/TriangleAreaCalculator.java
@@ -1,12 +1,13 @@
 package school.faang.sprint_2.remember_school;
 
+import java.util.function.BiFunction;
 import java.util.function.Function;
 
 public class TriangleAreaCalculator {
-    private static final Function<Double, Function<Double, Double>> ADD = (x) -> (y) -> x + y;
-    private static final Function<Double, Function<Double, Double>> MULTIPLY = (x) -> (y) -> x * y;
-    private static final Function<Double, Function<Double, Double>> SUBTRACT = (x) -> (y) -> x - y;
-    private static final Function<Double, Function<Double, Double>> DIVIDE = (x) -> (y) -> x / y;
+    private static final BiFunction<Double, Double, Double> ADD = Double::sum;
+    private static final BiFunction<Double, Double, Double> MULTIPLY = (x, y) -> x * y;
+    private static final BiFunction<Double, Double, Double> SUBTRACT = (x, y) -> x - y;
+    private static final BiFunction<Double, Double, Double> DIVIDE = (x, y) -> x / y;
     private static final Function<Double, Double> SQUARE_ROOT = Math::sqrt;
 
     public static Double calculateTriangleArea(double a, double b, double c) {
@@ -14,11 +15,7 @@ public class TriangleAreaCalculator {
             throw new IllegalArgumentException("Такого треугольника не существует");
         }
 
-        double s = DIVIDE.apply(ADD.apply(ADD.apply(a).apply(b)).apply(c)).apply(2.0);
-        return MULTIPLY
-                .apply(s).andThen(val1 -> MULTIPLY.apply(val1).apply(s - a))
-                .andThen(val2 -> MULTIPLY.apply(val2).apply(s - b))
-                .andThen(val3 -> MULTIPLY.apply(val3).apply(s - c))
-                .andThen(SQUARE_ROOT).apply(1.0);
+        double s = DIVIDE.apply(ADD.apply(ADD.apply(a, b), c), 2.0);
+        return SQUARE_ROOT.apply(MULTIPLY.apply(MULTIPLY.apply(MULTIPLY.apply(s, s - a), s - b), s - c));
     }
 }
