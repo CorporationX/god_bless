3b085f2f95b76e0eca92fcc170cc3441ec8976c5
AdminS <neptunchik1997@gmail.com>
2025-04-22 17:02:04 +0300

    add: Gmail Rich Filters

--- src/main/java/school/faang/gmailrichfilters/Main.java
+++ src/main/java/school/faang/gmailrichfilters/Main.java
@@ -16,12 +16,14 @@ public class Main {
         );
         Predicate<Email> importantFilter = email -> email.isImportant();
 
-        Consumer<Email> printEmail = email -> System.out.println("Обработано письмо: " + email.getSubject());
+        Consumer<Email> printEmail = email -> System.out.println("Обработано письмо: " +
+                email.getSubject());
 
         Function<Email, String> toUpperCase = email -> email.getBody().toUpperCase();
 
         emailProcessor.processEmails(emails, importantFilter, printEmail, toUpperCase);
 
-        emails.forEach(email -> System.out.println("Тема: " + email.getSubject() + ", Тело письма: " + email.getBody()));
+        emails.forEach(email -> System.out.println("Тема: " + email.getSubject() +
+                ", Тело письма: " + email.getBody()));
     }
 }
