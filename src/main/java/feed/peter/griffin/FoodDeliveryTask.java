c112f76a9053c3da494a5475d432b30d193a5970
Buga Dan <danbil.942@gmail.com>
2025-04-22 21:10:06 +0300

    Feed Peter Griffin task BSJ2-72117.

---
+++ src/main/java/feed/peter/griffin/FoodDeliveryTask.java
@@ -0,0 +1,35 @@
+package feed.peter.griffin;
+
+import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
+import java.util.Random;
+import java.util.concurrent.ThreadLocalRandom;
+
+@Slf4j
+@AllArgsConstructor
+public class FoodDeliveryTask implements Runnable {
+    private String characterName;
+    private int foodAmount;
+    final Random random = new Random();
+
+    @Override
+    public void run() {
+        FoodType randomFood = getFoodType();
+        log.info("{} получает заказ из {} едениц {}...", characterName, foodAmount, randomFood);
+
+        int randomMilisecodsForSleep = ThreadLocalRandom.current().nextInt(1000, 5001);
+        try {
+            Thread.sleep(randomMilisecodsForSleep);
+        } catch (InterruptedException e) {
+            throw new IllegalArgumentException(e.getMessage());
+        }
+
+        log.info("{} получил заказ и ест {} едениц {}", characterName, foodAmount, randomFood);
+    }
+
+    private FoodType getFoodType() {
+        FoodType[] foodTypes = FoodType.values();
+        return foodTypes[random.nextInt(foodTypes.length)];
+    }
+}
