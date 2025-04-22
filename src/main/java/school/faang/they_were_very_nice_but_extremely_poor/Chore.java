798b95ea35421435fff7a1c4e8822363bba73b19
Likhtin_Nick <kolyangrom2000500@gmail.com>
2025-04-22 13:14:42 +0700

    BJS2-73041
    Первое решение.

---
+++ src/main/java/school/faang/they_were_very_nice_but_extremely_poor/Chore.java
@@ -0,0 +1,13 @@
+package school.faang.they_were_very_nice_but_extremely_poor;
+
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
+public class Chore implements Runnable {
+    private final String chore;
+
+    @Override
+    public void run() {
+
+    }
+}
