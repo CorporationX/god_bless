30f3776744878fb149cece1cad74eaec16cf8812
Kirill <kirill551@yandex.ru>
2025-04-22 20:47:24 +0200

    "Выполнить задачу согласно условию"

--- src/main/java/school/faang/peter_griffen/GriffinsFoodDelivery.java
+++ src/main/java/school/faang/peter_griffen/GriffinsFoodDelivery.java
@@ -1,9 +1,13 @@
 package school.faang.peter_griffen;
 
+import lombok.extern.slf4j.Slf4j;
+
 import java.util.Random;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
 
+@Slf4j
 public class GriffinsFoodDelivery {
     public static void main(String[] args) {
         final Random rand = new Random();
@@ -14,5 +18,13 @@ public class GriffinsFoodDelivery {
             executorService.execute(new FoodDeliveryTask(characterName, rand.nextInt(1, 50)));
         }
         executorService.shutdown();
+        try {
+            if (!executorService.awaitTermination(800, TimeUnit.MILLISECONDS)) {
+                executorService.shutdownNow();
+            }
+        } catch (InterruptedException e) {
+            log.error("InterruptedException was thrown", e);
+            executorService.shutdownNow();
+        }
     }
 }
