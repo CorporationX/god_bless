471c4e8ce77b94b1887c1a8c55d8562173ed9935
Likhtin_Nick <kolyangrom2000500@gmail.com>
2025-04-22 11:40:50 +0700

    BJS2-72323
    Первое решение(20

--- src/main/java/school/faang/distributed_armsey_hero_me_i_magia/Army.java
+++ src/main/java/school/faang/distributed_armsey_hero_me_i_magia/Army.java
@@ -1,5 +1,27 @@
 package school.faang.distributed_armsey_hero_me_i_magia;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Army {
+
+    private final List<Squad> squads = new ArrayList<>();
+
+    public void addSquad(Squad squad){
+        squads.add(squad);
+
+    }
+
+    public int calculateSquadPower() throws InterruptedException {
+
+        Thread[] threads = new Thread[3];
+        int totalPower = 0;
+
+//        for (Squad squad: squads) {
+//            Thread thread = new Thread(() -> );
+//        }
+
+    }
+
 
 }
