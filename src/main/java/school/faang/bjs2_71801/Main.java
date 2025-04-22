5fb588a2a8f14e5780319a88499fd44249a8dc83
pirojok_yt <tamik34@mail.ru>
2025-04-22 14:20:50 +0500

    finish

--- src/main/java/school/faang/bjs2_71801/Main.java
+++ src/main/java/school/faang/bjs2_71801/Main.java
@@ -2,6 +2,7 @@ package school.faang.bjs2_71801;
 
 import java.util.Comparator;
 import java.util.List;
+import java.util.NoSuchElementException;
 
 public class Main {
     public static void main(String[] args) {
@@ -19,35 +20,54 @@ public class Main {
 
     public static class Operations {
         public static int evenSum(List<Integer> numbers) {
-            return numbers.stream().filter(a -> a % 2 == 0).reduce(Integer::sum).orElseThrow();
+            return numbers.stream()
+                    .filter(a -> a % 2 == 0)
+                    .reduce(Integer::sum)
+                    .orElseThrow(() -> new NoSuchElementException("Не найден подходящий элемент"));
         }
 
         public static int max(List<Integer> numbers) {
-            return numbers.stream().max(Comparator.naturalOrder()).orElseThrow();
+            return numbers.stream()
+                    .max(Comparator.naturalOrder())
+                    .orElseThrow(() -> new NoSuchElementException("Не найден подходящий элемент"));
         }
 
         public static double average(List<Integer> numbers) {
-            return (double) numbers.stream().reduce(Integer::sum).orElseThrow() / numbers.size();
+            return numbers.stream()
+                    .mapToInt(a -> a)
+                    .average()
+                    .orElseThrow(() -> new NoSuchElementException("Не найдены подходящие элементы"));
         }
 
         public static long startsWithLetter(List<String> strings, char firstLetter) {
-            return strings.stream().filter(s -> s.startsWith(String.valueOf(firstLetter))).toArray().length;
+            return strings.stream()
+                    .filter(s -> s.startsWith(String.valueOf(firstLetter)))
+                    .toArray().length;
         }
 
         public static List<String> containString(List<String> strings, String subString) {
-            return strings.stream().filter(s -> s.contains(subString)).toList();
+            return strings.stream()
+                    .filter(s -> s.contains(subString))
+                    .toList();
         }
 
         public static List<String> sortByLength(List<String> strings) {
-            return strings.stream().sorted(Comparator.comparingInt(String::length).reversed()).toList();
+            return strings.stream()
+                    .sorted(Comparator.comparingInt(String::length).reversed())
+                    .toList();
         }
 
         public static int findLowest(List<Integer> numbers, int min) {
-            return numbers.stream().filter(a -> a > min).min(Comparator.naturalOrder()).orElseThrow();
+            return numbers.stream()
+                    .filter(a -> a > min)
+                    .min(Comparator.naturalOrder())
+                    .orElseThrow(() -> new NoSuchElementException("Не найден подходящий элемент"));
         }
 
         public static List<Integer> transform(List<String> strings) {
-            return strings.stream().map(String::length).toList();
+            return strings.stream()
+                    .map(String::length)
+                    .toList();
         }
     }
 }
