d3807871c5912ba539e6aec68c1390f9d20b9a74
Daniyal-Akhadov <d.ahanewmail@mail.ru>
2025-04-23 22:11:42 +0300

    feat: добавил забытые final перед переменными

--- src/main/java/faang/Main.java
+++ src/main/java/faang/Main.java
@@ -10,15 +10,15 @@ public class Main {
     public static void main(String[] args) {
         final Army army = new Army();
 
-        Squad archers = new Squad("Лучники", List.of(new Archer(25), new Archer(30)));
-        Squad swordsmen = new Squad("Мечники", List.of(new Swordsman(40), new Swordsman(35)));
-        Squad mages = new Squad("Маги", List.of(new Mage(50), new Mage(45)));
+        final Squad archers = new Squad("Лучники", List.of(new Archer(25), new Archer(30)));
+        final Squad swordsmen = new Squad("Мечники", List.of(new Swordsman(40), new Swordsman(35)));
+        final Squad mages = new Squad("Маги", List.of(new Mage(50), new Mage(45)));
 
         army.addSquad(archers);
         army.addSquad(swordsmen);
         army.addSquad(mages);
 
-        int totalPower = army.calculateTotalPower();
+        final int totalPower = army.calculateTotalPower();
         System.out.println("Общая сила армии: " + totalPower);
     }
 }
