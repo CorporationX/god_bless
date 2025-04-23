f81f29c113e92ea2b15bf79d43407eb81a838c69
Slava Vermilion <beg.slava@bk.ru>
2025-04-23 16:14:22 +0700

    Attempt #3

--- src/main/java/bjs2_70412/Droid.java
+++ src/main/java/bjs2_70412/Droid.java
@@ -5,42 +5,32 @@ import lombok.AllArgsConstructor;
 @AllArgsConstructor
 public class Droid {
     private String name;
+    private static final DroidMessageEncryptor ENCRYPTOR = (message, key) -> {
+        StringBuilder result = new StringBuilder();
+        for (char character : message.toCharArray()) {
+            if (Character.isLetter(character)) {
+                char characterCase = Character.isLowerCase(character) ? 'a' : 'A';
+                char encryptedCharacter = (char) ((character - characterCase + key + 26) % 26 + characterCase);
+                result.append(encryptedCharacter);
+            } else {
+                result.append(character);
+            }
+        }
+        return result.toString();} ;
 
     @FunctionalInterface
     public interface DroidMessageEncryptor {
         String encrypt(String message, int key);
     }
 
-    public static String processMessage(String message, int shift) {
-            StringBuilder result = new StringBuilder();
-            for (char character : message.toCharArray()) {
-                if (Character.isLetter(character)) {
-                    char characterCase = Character.isLowerCase(character) ? 'a' : 'A';
-                    char encryptedCharacter = (char) ((character - characterCase + shift + 26) % 26 + characterCase);
-                    result.append(encryptedCharacter);
-                } else {
-                    result.append(character);
-                }
-            }
-            return result.toString();
-    }
-
-    public String encryptMessage(String message, int key) {
-        return processMessage(message, -key);
-    }
-
-    public String decryptMessage(String message, int key) {
-        return processMessage(message, key);
-    }
-
     public void sendMessage(Droid droid, String message, int key) {
-        String encryptedMessage = encryptMessage(message, key);
+        String encryptedMessage = ENCRYPTOR.encrypt(message, key);
         System.out.println("Encrypted message: " + encryptedMessage);
         droid.receiveMessage(encryptedMessage, key);
     }
 
     public void receiveMessage(String message, int key) {
-        String decryptedMessage = decryptMessage(message, key);
+        String decryptedMessage = ENCRYPTOR.encrypt(message, -key);
         System.out.println("Decrypted message: " + decryptedMessage);
     }
 }
