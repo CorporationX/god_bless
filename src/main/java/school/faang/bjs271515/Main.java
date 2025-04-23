444b1b00f4c802fcfff63353080ea82f7f2a95c8
Maksym Kharabash <maksymkharabash@gmail.com>
2025-04-22 22:15:10 -0500

    Task completed.

---
+++ src/main/java/school/faang/bjs271515/Main.java
@@ -0,0 +1,17 @@
+package school.faang.bjs271515;
+
+public class Main {
+    public static void main(String[] args) {
+        SpellCaster spellCaster = new SpellCaster();
+
+        String alohomora = "Alohomora";
+        String lumos = "Lumos";
+        String expelliarmus = "Expelliarmus";
+        String expectoPatronum = "Expecto Patronum";
+
+        spellCaster.cast(alohomora, (spell) -> "The door is unlocked by " + spell);
+        spellCaster.cast(lumos, (spell) -> "A beam of light is created by " + spell);
+        spellCaster.cast(expelliarmus, (spell) -> "The opponent is disarmed by " + spell);
+        spellCaster.cast(expectoPatronum, (spell) -> "Dementor can be hit by " + spell);
+    }
+}
---
+++ src/main/java/school/faang/bjs271515/SpellAction.java
@@ -0,0 +1,6 @@
+package school.faang.bjs271515;
+
+@FunctionalInterface
+public interface SpellAction {
+    String spell(String spellName);
+}
