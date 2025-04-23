f7093e598274e7905ff4c5f567d6c35bfed04218
Vladislav Kozlov <littlerock2877@mail.ru>
2025-04-23 20:33:44 +0300

    fix: pr comments

--- src/main/java/school/faang/bjs2_70746/ElectronicDiaryService.java
+++ src/main/java/school/faang/bjs2_70746/ElectronicDiaryService.java
@@ -10,12 +10,12 @@ import java.util.stream.Collectors;
 @UtilityClass
 public class ElectronicDiaryService {
     public Map<String, Double> findAverageMark(List<Student> students) {
-        return students.stream()
+        Map<String, List<Integer>> groupedStudents = students.stream()
                 .flatMap(student -> student.subjects().entrySet().stream())
                 .collect(Collectors.groupingBy(
                         Map.Entry::getKey,
-                        Collectors.flatMapping(entry -> entry.getValue().stream(), Collectors.toList())))
-                .entrySet().stream()
+                        Collectors.flatMapping(entry -> entry.getValue().stream(), Collectors.toList())));
+        return groupedStudents.entrySet().stream()
                 .collect(Collectors.toMap(Map.Entry::getKey, entry -> entry.getValue().stream()
                         .mapToInt(Integer::intValue)
                         .average()
@@ -23,14 +23,14 @@ public class ElectronicDiaryService {
     }
 
     public Map<String, Integer> findStudentsMarks(List<Student> students, String firstName, String lastName) {
-        return students.stream()
+        Student foundStudent = students.stream()
                 .filter(student -> student.firstName().equals(firstName) && student.lastName().equals(lastName))
                 .findFirst()
                 .orElseThrow(() -> new IllegalArgumentException(
                         String.format("Student with firstName %s and lastName %s doesn't exist in the list",
-                                firstName, lastName)))
-                .subjects().entrySet().stream()
-                .collect(Collectors.toMap(entry -> entry.getKey(),
+                                firstName, lastName)));
+        return foundStudent.subjects().entrySet().stream()
+                .collect(Collectors.toMap(Map.Entry::getKey,
                         entry -> (int) Math.round(
                                 entry.getValue().stream()
                                         .mapToInt(Integer::intValue)
@@ -42,10 +42,8 @@ public class ElectronicDiaryService {
     public String findHardestSubject(List<Student> students) {
         return findAverageMark(students).entrySet().stream()
                 .sorted(Comparator.comparingDouble(Map.Entry<String, Double>::getValue).reversed())
-                .map(entry -> entry.getKey())
+                .map(Map.Entry::getKey)
                 .findFirst()
                 .orElseThrow(() -> new IllegalArgumentException("Students list is empty"));
     }
-
-
 }
