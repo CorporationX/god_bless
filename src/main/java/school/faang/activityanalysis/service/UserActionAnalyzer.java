fbfc907c7f42bfbb7508bac36df4d2928004c985
P-Lidia <Lydiasmolyakova@gmail.com>
2025-04-25 00:48:41 +0300

    Fix UserActionAnalyzer class

--- src/main/java/school/faang/activityanalysis/service/UserActionAnalyzer.java
+++ src/main/java/school/faang/activityanalysis/service/UserActionAnalyzer.java
@@ -12,18 +12,13 @@ import java.util.stream.Collectors;
 
 public class UserActionAnalyzer {
     public static List<String> getTopActiveUsers(List<UserAction> actions, int num) {
-        Map<Long, String> idAndNames = actions.stream()
-                .collect(Collectors.toMap(
-                        UserAction::getId,
-                        UserAction::getName,
-                        (a, b) -> a
-                ));
-        return actions.stream()
+        Map<Long, String> idAndNames = getIdAndNames(actions);
+        Map<Long, Long> actionCounts = actions.stream()
                 .collect(Collectors.groupingBy(
                         UserAction::getId,
                         Collectors.counting()
-                ))
-                .entrySet().stream()
+                ));
+        return actionCounts.entrySet().stream()
                 .sorted(Map.Entry.<Long, Long>comparingByValue().reversed())
                 .limit(num)
                 .map(entry -> idAndNames.get(entry.getKey()))
@@ -31,7 +26,7 @@ public class UserActionAnalyzer {
     }
 
     public static List<String> getTopPopularHashtags(List<UserAction> actions, int num) {
-        return actions.stream()
+        Map<String, Long> hashtagCounts = actions.stream()
                 .filter(u -> u.getContent() != null && !u.getContent().isBlank())
                 .flatMap(user -> Arrays.stream(user.getContent().split("\\s+")))
                 .filter(word -> word.startsWith("#"))
@@ -39,8 +34,8 @@ public class UserActionAnalyzer {
                 .collect(Collectors.groupingBy(
                         Function.identity(),
                         Collectors.counting()
-                ))
-                .entrySet().stream()
+                ));
+        return hashtagCounts.entrySet().stream()
                 .sorted(Map.Entry.<String, Long>comparingByValue().reversed())
                 .limit(num)
                 .map(Map.Entry::getKey)
@@ -49,21 +44,15 @@ public class UserActionAnalyzer {
 
     public static List<String> getTopCommentersLastMonth(List<UserAction> actions, int num) {
         LocalDate lastMonth = LocalDate.now().minusMonths(1);
-        Map<Long, String> idAndNames = actions.stream()
-                .collect(Collectors.toMap(
-                        UserAction::getId,
-                        UserAction::getName,
-                        (a, b) -> a
-                ));
-        return actions.stream()
-                .filter(user ->
-                        user.getActionType() == ActionType.COMMENT
-                                && user.getActionDate().isAfter(lastMonth))
+        Map<Long, String> idAndNames = getIdAndNames(actions);
+        Map<Long, Long> commentActionCount = actions.stream()
+                .filter(user -> user.getActionType() == ActionType.COMMENT
+                        && user.getActionDate().isAfter(lastMonth))
                 .collect(Collectors.groupingBy(
                         UserAction::getId,
                         Collectors.counting()
-                ))
-                .entrySet().stream()
+                ));
+        return commentActionCount.entrySet().stream()
                 .sorted(Map.Entry.<Long, Long>comparingByValue().reversed())
                 .limit(num)
                 .map(entry -> idAndNames.get(entry.getKey()))
@@ -72,15 +61,24 @@ public class UserActionAnalyzer {
 
     public static Map<ActionType, Double> getActionTypePercent(List<UserAction> actions) {
         long allActions = actions.size();
-        return actions.stream()
+        Map<ActionType, Long> actionTypeCount = actions.stream()
                 .collect(Collectors.groupingBy(
                         UserAction::getActionType,
                         Collectors.counting()
-                ))
-                .entrySet().stream()
+                ));
+        return actionTypeCount.entrySet().stream()
                 .collect(Collectors.toMap(
                         Map.Entry::getKey,
                         v -> (v.getValue() * 100.0) / allActions
+                ));
+    }
+
+    private static Map<Long, String> getIdAndNames(List<UserAction> actions) {
+        return actions.stream()
+                .collect(Collectors.toMap(
+                        UserAction::getId,
+                        UserAction::getName,
+                        (a, b) -> a
                 ));
     }
 }
