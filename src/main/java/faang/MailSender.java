9a8f8932b61951b78c9b5f3505c0be4e2cb7a306
Daniyal-Akhadov <d.ahanewmail@mail.ru>
2025-04-24 18:40:57 +0300

    feat: Исправил магические значения

--- src/main/java/faang/MailSender.java
+++ src/main/java/faang/MailSender.java
@@ -5,13 +5,14 @@ import java.util.List;
 
 public class MailSender {
     private static final int MULTIPLIER = 200;
+    public static final int THREADS_COUNT = 5;
 
     public static void main(final String[] args) throws InterruptedException {
         final List<Thread> threads = new ArrayList<>();
 
-        for (int i = 0; i < 5; i++) {
-            final int startIndex = i * MULTIPLIER;
-            final int endIndex = (i + 1) * MULTIPLIER - 1;
+        for (int i = 0; i < THREADS_COUNT; i++) {
+            final int startIndex = getStartIndex(i);
+            final int endIndex = getEndIndex(i);
 
             Thread thread = new Thread(new SenderRunnable(startIndex, endIndex));
             threads.add(thread);
@@ -21,6 +22,14 @@ public class MailSender {
         for (Thread thread : threads) {
             thread.join();
         }
+    }
+
+    private static int getEndIndex(int i) {
+        return (i + 1) * MULTIPLIER - 1;
+    }
+
+    private static int getStartIndex(int i) {
+        return i * MULTIPLIER;
     }
 }
 
