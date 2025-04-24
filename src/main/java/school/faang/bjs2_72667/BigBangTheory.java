aa45c49be80457606d547c8a8cc276c246be09da
Vladislav Kozlov <littlerock2877@mail.ru>
2025-04-25 00:33:04 +0300

    add: big bang theory

---
+++ src/main/java/school/faang/bjs2_72667/BigBangTheory.java
@@ -0,0 +1,32 @@
+package school.faang.bjs2_72667;
+
+import lombok.extern.slf4j.Slf4j;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
+
+@Slf4j
+public class BigBangTheory {
+    private static final int THREADS_COUNT = 4;
+    private static final int EXECUTION_TIMEOUT = 30;
+
+    public static void main(String[] args) {
+        ExecutorService executorService = Executors.newFixedThreadPool(THREADS_COUNT);
+
+        executorService.execute(new Task("Sheldon", "preparing theory"));
+        executorService.execute(new Task("Leonard", "modeling experiment"));
+        executorService.execute(new Task("Howard", "developing instruments"));
+        executorService.execute(new Task("Rajesh", "data analysing"));
+
+        executorService.shutdown();
+        try {
+            if (!executorService.awaitTermination(EXECUTION_TIMEOUT, TimeUnit.SECONDS)) {
+                executorService.shutdownNow();
+            }
+        } catch (InterruptedException e) {
+            log.error("Interrupted exception with message {} was thrown", e.getMessage());
+            executorService.shutdownNow();
+        }
+    }
+}
---
+++ src/main/java/school/faang/bjs2_72667/Task.java
@@ -0,0 +1,22 @@
+package school.faang.bjs2_72667;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@RequiredArgsConstructor
+public class Task implements Runnable {
+    private final String name;
+    private final String task;
+
+    @Override
+    public void run() {
+        log.info("{} started executing task {}", name, task);
+        try {
+            Thread.sleep(1000);
+            log.info("{} finished executing task {}", name, task);
+        } catch (InterruptedException e) {
+            log.error("Task was interrupted: {}", e.getMessage());
+        }
+    }
+}
