64de150301c11888f0b0b556a151a0791021702c
Ramil Burgart <ramilburgart7006@gmail.com>
2025-04-24 22:11:20 +0300

    fixed code

--- src/main/java/school/faang/spotify/Music.java
+++ src/main/java/school/faang/spotify/Music.java
@@ -1,40 +1,45 @@
 package school.faang.spotify;
 
+import lombok.extern.slf4j.Slf4j;
+
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.TimeUnit;
 
+@Slf4j
 public class Music {
 
+    private static final int COUNT_THREAD_POOL = 5;
+
     public static void main(String[] args) {
-        ExecutorService service = Executors.newFixedThreadPool(5);
+        ExecutorService service = Executors.newFixedThreadPool(COUNT_THREAD_POOL);
         Player player = new Player();
         service.submit(() -> {
             try {
                 player.play();
             } catch (Exception e) {
-                e.printStackTrace();
+                log.error("Some unexpected error occurred while trying to play music", e);
             }
         });
         service.submit(() -> {
             try {
                 player.pause();
             } catch (Exception e) {
-                e.printStackTrace();
+                log.error("Some unexpected error occurred while trying to pause music", e);
             }
         });
         service.submit(() -> {
             try {
                 player.previous();
             } catch (Exception e) {
-                e.printStackTrace();
+                log.error("Some unexpected error occurred while trying to previous music", e);
             }
         });
         service.submit(() -> {
             try {
                 player.skip();
             } catch (Exception e) {
-                e.printStackTrace();
+                log.error("Some unexpected error occurred while trying to skip music", e);
             }
         });
         service.shutdown();
