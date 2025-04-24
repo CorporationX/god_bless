01a7490417cee20ec70e27761348159c35b6269e
Daniyal-Akhadov <d.ahanewmail@mail.ru>
2025-04-24 18:29:28 +0300

    feat: изменил Random на ThreadLocalRandom

--- src/main/java/faang/FoodDeliveryTask.java
+++ src/main/java/faang/FoodDeliveryTask.java
@@ -9,7 +9,6 @@ import java.util.concurrent.TimeUnit;
 @RequiredArgsConstructor
 public class FoodDeliveryTask implements Runnable {
     private static final int TASK_DURATION_IN_SECONDS = 5;
-    private static final ThreadLocalRandom RANDOM = ThreadLocalRandom.current();
 
     private final String character;
     private final int foodCount;
@@ -18,14 +17,17 @@ public class FoodDeliveryTask implements Runnable {
     @Override
     public void run() {
         final FoodType foodType = getFoodType();
+        final ThreadLocalRandom threadLocalRandom = ThreadLocalRandom.current();
+
         System.out.println(character + ": " + foodType + " в количестве " + foodCount);
-        TimeUnit.SECONDS.sleep(RANDOM.nextInt(TASK_DURATION_IN_SECONDS) + 1);
+        TimeUnit.SECONDS.sleep(threadLocalRandom.nextInt(TASK_DURATION_IN_SECONDS) + 1);
         System.out.println(character + ": съест " + foodCount + " " + foodType);
     }
 
     private FoodType getFoodType() {
-        FoodType[] foodTypes = FoodType.values();
-        return foodTypes[RANDOM.nextInt(foodTypes.length)];
+        final FoodType[] foodTypes = FoodType.values();
+        final ThreadLocalRandom threadLocalRandom = ThreadLocalRandom.current();
+        return foodTypes[threadLocalRandom.nextInt(foodTypes.length)];
     }
 }
 
