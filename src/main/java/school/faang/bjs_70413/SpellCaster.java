d3f57c1788001015572e83d72c2675927ca236da
Yulia <bualex1983@mail.ru>
2025-04-24 17:57:54 +0300

    improve line

--- src/main/java/school/faang/bjs_70413/SpellCaster.java
+++ src/main/java/school/faang/bjs_70413/SpellCaster.java
@@ -3,6 +3,6 @@ package school.faang.bjs_70413;
 public class SpellCaster {
     public void cast(String spellName, SpellAction action) {
         String result = action.perform(spellName);
-        System.out.println("Заклинание " + spellName + "выполнено: " + result);
+        System.out.printf("Заклинание %s выполнено: %s", spellName, result);
     }
 }
