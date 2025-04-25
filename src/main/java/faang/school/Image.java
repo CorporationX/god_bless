642c1909b604f43a09dca97cbafc7d78b76223fa
Ilyas Kystaubayev <kystaubaev.iliyas@mail.ru>
2025-04-26 00:34:04 +0500

    Фильтры Инстраграм!

---
+++ src/main/java/faang/school/Image.java
@@ -0,0 +1,18 @@
+package faang.school;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+public class Image {
+    private String name;
+    private String description;
+
+}
