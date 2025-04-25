b19fd971f169a0f7de4a662cf66b8c5bc51f8f9d
Vladislav Kozlov <littlerock2877@mail.ru>
2025-04-25 20:08:49 +0300

    fix: PR comments

--- src/main/java/school/faang/bjs2_73013/Chore.java
+++ src/main/java/school/faang/bjs2_73013/Chore.java
@@ -6,16 +6,19 @@ import lombok.extern.slf4j.Slf4j;
 @Slf4j
 @RequiredArgsConstructor
 public class Chore implements Runnable {
+    private static final int SLEEP_DURATION = 1000;
+
     private final String chore;
 
     @Override
     public void run() {
         try {
             log.info("{} выполняет задачу {}", Thread.currentThread().getName(), chore);
-            Thread.sleep(1000);
+            Thread.sleep(SLEEP_DURATION);
             log.info("{} закончил выполнение задачи {}", Thread.currentThread().getName(), chore);
         } catch (InterruptedException e) {
             log.error("Во время выполнения задачи выпало исключение {}", e.getMessage());
+            Thread.currentThread().interrupt();
         }
     }
 }
