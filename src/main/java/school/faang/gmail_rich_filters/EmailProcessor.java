aec8f305782be008c33b9ba501e1b2065ddad853
Robert Abdrakhmanov <rap215@icloud.com>
2025-04-25 21:54:23 +0300

    1

---
+++ src/main/java/school/faang/gmail_rich_filters/EmailProcessor.java
@@ -0,0 +1,14 @@
+package school.faang.gmail_rich_filters;
+
+import java.util.List;
+import java.util.function.Consumer;
+import java.util.function.Function;
+import java.util.function.Predicate;
+
+public class EmailProcessor {
+    public void processEmails(List<Email> inputEmails, Predicate<Email> emailFilter, Function<Email, String> emailBody, Consumer<Email> emailConsumer){
+
+
+    }
+    
+}
