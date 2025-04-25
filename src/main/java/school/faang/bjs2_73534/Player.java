be912a3d33f09b7f5fb02ae90823063d6ddeafbd
Stanislaff Fedyanin <re1971reg@gmail.com>
2025-04-25 10:09:20 +0300

    stanislav075539 correct task BJS2-73534

--- src/main/java/school/faang/bjs2_73534/Player.java
+++ src/main/java/school/faang/bjs2_73534/Player.java
@@ -1,10 +1,12 @@
 package school.faang.bjs2_73534;
 
 public record Player(String name) {
+    private static final Integer BATTLE_TIME = 3000;
+
     public void doBattle(Boss boss) {
         boss.joinBattle(this);
         try {
-            Thread.sleep(3000);
+            Thread.sleep(BATTLE_TIME);
         } catch (InterruptedException e) {
             Thread.currentThread().interrupt();
             throw new RuntimeException(e);
