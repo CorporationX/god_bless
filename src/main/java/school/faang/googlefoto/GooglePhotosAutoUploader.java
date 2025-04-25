9d0cbee055928402537e221866e4879302889e40
Fagan Gafulov <gafulov@yandex.ru>
2025-04-25 15:54:13 +0300

    BJS2-73800 create task

--- src/main/java/school/faang/googlefoto/GooglePhotosAutoUploader.java
+++ src/main/java/school/faang/googlefoto/GooglePhotosAutoUploader.java
@@ -3,28 +3,31 @@ package school.faang.googlefoto;
 import lombok.Getter;
 import lombok.extern.slf4j.Slf4j;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.ArrayDeque;
+import java.util.Deque;
 
 @Slf4j
 @Getter
 public class GooglePhotosAutoUploader {
     private final Object lock = new Object();
-    private final List<String> photosToUpload = new ArrayList<>();
+    private final Deque<String> photosToUpload = new ArrayDeque<>();
 
     public void startAutoUpload() {
-        synchronized (lock) {
-            while (photosToUpload.isEmpty()) {
-                try {
-                    lock.wait();
-                    log.info("wait");
-                } catch (InterruptedException e) {
-                    log.error("поток прерван", e);
-                    Thread.currentThread().interrupt();
+        while (!Thread.currentThread().isInterrupted()) {
+            try {
+                synchronized (lock) {
+                    if (photosToUpload.isEmpty()) {
+                        log.info("Проверяем папку на пустоту");
+                        lock.wait();
+                        log.info("Поток заснул");
+                    }
                 }
+                uploadPhotos();
+            } catch (InterruptedException e) {
+                log.error("поток прерван исключением", e);
+                Thread.currentThread().interrupt();
+                log.error("Восстанавливаем флаг прерывания, для завершения цикла");
             }
-            uploadPhotos();
-            startAutoUpload();
         }
     }
 
@@ -33,16 +36,20 @@ public class GooglePhotosAutoUploader {
             photosToUpload.add(photoPath);
             log.info("Фото: {} добавлено", photoPath);
             lock.notify();
-            log.info("notify");
+            log.info("Отправили сигнал на пробуждение другим потокам");
+        }
+        try {
+            Thread.sleep(300);
+        } catch (InterruptedException e) {
+            log.error("Процесс добавления фото в папку прерван", e);
         }
     }
 
-    public void uploadPhotos() {
+    public void uploadPhotos() throws InterruptedException {
         synchronized (lock) {
-            photosToUpload.forEach((f) -> {
-                log.info("Фото: {} загружено", f);
-            });
-            photosToUpload.clear();
+            String foto = photosToUpload.poll();
+            log.info("Фото: {} загружено", foto);
         }
+        Thread.sleep(200);
     }
 }
--- src/main/java/school/faang/googlefoto/Main.java
+++ src/main/java/school/faang/googlefoto/Main.java
@@ -1,16 +1,27 @@
 package school.faang.googlefoto;
 
+import lombok.extern.slf4j.Slf4j;
+
 import java.util.List;
 
+@Slf4j
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
         GooglePhotosAutoUploader autoUploader = new GooglePhotosAutoUploader();
         List<String> fotosList = List.of(
                 "Foto21.png",
                 "Foto22.png",
                 "Foto23.png",
                 "Foto24.png",
-                "Foto25.png"
+                "Foto25.png",
+                "Foto26.png",
+                "Foto27.png",
+                "Foto28.png",
+                "Foto29.png",
+                "Foto30.png",
+                "Foto31.png",
+                "Foto32.png",
+                "Foto33.png"
         );
 
         autoUploader.getPhotosToUpload().addAll(fotosList);
@@ -30,13 +41,15 @@ public class Main {
                 "Foto12.png"
         );
 
-
         Thread uploadingPhotos = new Thread(autoUploader::startAutoUpload);
-        Thread addPhotos = new Thread(() -> {
-            fotos.forEach(autoUploader::onNewPhotoAdded);
-        });
+        Thread addPhotos = new Thread(() -> fotos.forEach(autoUploader::onNewPhotoAdded));
 
         uploadingPhotos.start();
         addPhotos.start();
+
+        Thread.sleep(10000);
+
+        uploadingPhotos.interrupt();
+        log.info("Поток main завершен");
     }
 }
