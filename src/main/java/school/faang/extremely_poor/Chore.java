770c74761f0faa41129bc64c03c6188b0599e347
Hitow22 <Hitow22@gmail.com>
2025-04-25 10:05:13 +0300

    They were very nice but extremely poor(доработка)

--- src/main/java/school/faang/extremely_poor/Chore.java
+++ src/main/java/school/faang/extremely_poor/Chore.java
@@ -1,18 +1,21 @@
 package school.faang.extremely_poor;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 
+@Slf4j
 @RequiredArgsConstructor
 public class Chore implements Runnable {
+    public static final int SLEEP_MILLIS = 2000;
     private final String chore;
 
     @Override
     public void run() {
-        System.out.println(Thread.currentThread().getName() + " выполняет задачу " + chore);
+        log.info("{} выполняет задачу {}", Thread.currentThread().getName(), chore);
         try {
-            Thread.sleep(2000);
+            Thread.sleep(SLEEP_MILLIS);
         } catch (InterruptedException e) {
-            throw new RuntimeException(e);
+            log.error("Действие текущего потока прервано.");
         }
     }
 }
--- src/main/java/school/faang/extremely_poor/WeasleyFamily.java
+++ src/main/java/school/faang/extremely_poor/WeasleyFamily.java
@@ -1,11 +1,16 @@
 package school.faang.extremely_poor;
 
+import lombok.extern.slf4j.Slf4j;
+
 import java.util.Arrays;
 import java.util.List;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
 
+@Slf4j
 public class WeasleyFamily {
+    public static final int MAX_WAIT_MINUTES = 1;
     private static final List<String> CHORES = Arrays.asList("помыть посуду", "подмести пол", "приготовить ужин",
             "вынести мусор", "сделать домашнее задание", "покормить рыбок");
 
@@ -15,5 +20,15 @@ public class WeasleyFamily {
             executor.execute(new Thread(new Chore(chore)));
         }
         executor.shutdown();
+        try {
+            if (!executor.awaitTermination(MAX_WAIT_MINUTES, TimeUnit.MINUTES)) {
+                log.info("Не все задачи завершены за {} минут. Завершаем принудительно...", MAX_WAIT_MINUTES);
+                executor.shutdownNow();
+            }
+        } catch (InterruptedException e) {
+            log.error("Ожидание завершения потоков прервано.");
+            executor.shutdownNow();
+        }
+        log.info("Все задачи выполнены!");
     }
 }
