9d74fdacab75df2e065e1e048665e44f74a1633a
Daniyal-Akhadov <d.ahanewmail@mail.ru>
2025-04-25 22:53:24 +0300

    feat: бизнес-логика

---
+++ src/main/java/faang/Main.java
@@ -0,0 +1,18 @@
+package faang;
+
+public final class Main {
+    public static void main(final String[] args) throws InterruptedException {
+        final Player player = new Player();
+        final Thread[] threads = {
+                new Thread(player::pause),
+                new Thread(player::play),
+                new Thread(player::skip),
+                new Thread(player::previous)
+        };
+
+        for (Thread thread : threads) {
+            thread.start();
+            thread.join();
+        }
+    }
+}
