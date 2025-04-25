7e30f9d11e1817f50995932c539998d153ca4c03
Likhtin_Nick <kolyangrom2000500@gmail.com>
2025-04-25 08:02:29 +0700

    BJS2-73583
    Исправление замечаний

--- src/main/java/school/faang/supercow/Player.java
+++ src/main/java/school/faang/supercow/Player.java
@@ -4,11 +4,14 @@ import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
 public record Player(String name) {
+
+    private static final int SLEEP_TIME = 2000;
+
     public void doBattle(Boss boss) {
         boss.joinBattle(this);
         try {
             log.info("{} сражается с боссом...", name);
-            Thread.sleep(2000);
+            Thread.sleep(SLEEP_TIME);
         } catch (InterruptedException e) {
             log.info("{} был прерван во время сражения.", name);
             Thread.currentThread().interrupt();
