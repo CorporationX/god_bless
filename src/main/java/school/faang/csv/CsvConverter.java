34286f179843c7e7fda7ed37e17cf525e88b1f14
Gigabyte <012_erasyl@mail.ru>
2025-04-25 22:22:55 +0500

    csv Commit

---
+++ src/main/java/school/faang/csv/CsvConverter.java
@@ -0,0 +1,19 @@
+package school.faang.csv;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class CsvConverter {
+    public static String toCsv(List<List<String>> table) {
+        VectorJoiner<String> vectorJoiner = vector ->
+                vector.stream().collect(Collectors.joining(", "));
+
+        MatrixJoiner<String> matrixJoiner = matrix ->
+                matrix.stream()
+                        .map(vectorJoiner::join)
+                        .collect(Collectors.joining("\n"));
+
+        return matrixJoiner.join(table);
+    }
+
+}
