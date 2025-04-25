199057bf050e0fe8a235da30626a745a13b20c92
Gigabyte <012_erasyl@mail.ru>
2025-04-25 20:31:20 +0500

    Griffins thread Commit

---
+++ src/main/java/school/faang/griffin/FoodType.java
@@ -0,0 +1,8 @@
+package school.faang.griffin;
+
+public enum FoodType {
+    PIZZA,
+    BURGER,
+    TACO,
+    CHICKEN_WINGS
+}
---
+++ src/main/java/school/faang/griffin/GriffinsFoodDelivery.java
@@ -0,0 +1,21 @@
+package school.faang.griffin;
+
+import java.util.Random;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+public class GriffinsFoodDelivery {
+    public static void main(String[] args) {
+        ExecutorService executor = Executors.newFixedThreadPool(3);
+        String[] characterNames = {"Peter", "Lois", "Meg", "Chris", "Stewie"};
+        Random random = new Random();
+
+        for (String character : characterNames) {
+            int foodAmount = random.nextInt(10,100);
+            FoodDeliveryTask task = new FoodDeliveryTask(character, foodAmount);
+            executor.submit(task);
+        }
+
+        executor.shutdown();
+    }
+}
