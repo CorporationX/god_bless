d758a15e22e335cf10d613aca3e64ee47bd6fce0
JolyGolden <nazi00696@mail.ru>
2025-04-26 12:51:34 +0200

    the witcher task

---
+++ src/main/java/school/faang/the_witcher/Location.java
@@ -0,0 +1,11 @@
+package school.faang.the_witcher;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class Location {
+    private int coordinateX;
+    private int coordinateY;
+}
