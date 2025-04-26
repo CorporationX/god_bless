665d39313db7601ca5b9275213043f6a5153ff7f
Gigabyte <012_erasyl@mail.ru>
2025-04-26 11:53:09 +0500

    Big Bang Theory Commit

--- src/main/java/school/faang/threadpool/Chore.java
+++ src/main/java/school/faang/threadpool/Chore.java
@@ -4,15 +4,17 @@ import lombok.AllArgsConstructor;
 
 @AllArgsConstructor
 public class Chore implements Runnable {
+    private static final int TASK_DURATION_MS = 3000;
     private final String chore;
 
     public void run() {
         try {
             System.out.println(Thread.currentThread().getName() + " выполняет: " + chore);
-            Thread.sleep(1000);
+            Thread.sleep(TASK_DURATION_MS);
             System.out.println(Thread.currentThread().getName() + " закончил: " + chore);
         } catch (InterruptedException e) {
             System.out.println("Задача была прервана: " + chore);
+            Thread.currentThread().interrupt();
         }
     }
 }
--- src/main/java/school/faang/threadpool/WeasleyFamily.java
+++ src/main/java/school/faang/threadpool/WeasleyFamily.java
@@ -2,24 +2,47 @@ package school.faang.threadpool;
 
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
 
 public class WeasleyFamily {
+    private static final int AWAITING_SECONDS = 10;
+
+    private static final String[] CHORES = {
+            "помыть посуду",
+            "подмести пол",
+            "приготовить ужин",
+            "постирать бельё",
+            "покормить сову",
+            "почистить камин",
+            "прополоть огород"
+    };
+
+    public static void softShutdown(ExecutorService executor) {
+        executor.shutdown();
+
+        try {
+            // ждём завершения всех задач
+            if (!executor.awaitTermination(AWAITING_SECONDS, TimeUnit.SECONDS)) {
+                System.out.println("Execution is not terminated in time. Forced shutdown!");
+                executor.shutdownNow();
+            } else {
+                System.out.println("Execution terminated successfully!");
+            }
+        } catch (InterruptedException e) {
+            System.out.println("Main thread was interrupted. Forcing shutdown...");
+            executor.shutdownNow();
+            Thread.currentThread().interrupt();
+        }
+    }
+
     public static void main(String[] args) {
-        String[] chores = {
-                "помыть посуду",
-                "подмести пол",
-                "приготовить ужин",
-                "постирать бельё",
-                "покормить сову",
-                "почистить камин",
-                "прополоть огород"
-        };
         ExecutorService executor = Executors.newCachedThreadPool();
 
-        for (String choreName : chores) {
+        for (String choreName : CHORES) {
             Chore chore = new Chore(choreName);
             executor.submit(chore);
         }
-        executor.shutdown();
+
+        softShutdown(executor);
     }
 }
