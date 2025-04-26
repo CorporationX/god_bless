59f222a85dc7c3e99ac27b0c28f9bdeb17d0ef24
Robert Abdrakhmanov <rap215@icloud.com>
2025-04-26 14:19:57 +0300

    Email

--- src/main/java/school/faang/gmail_rich_filters/Test.java
+++ src/main/java/school/faang/gmail_rich_filters/Test.java
@@ -9,8 +9,8 @@ public class Test {
     public static void main(String[] args) {
         EmailProcessor emailProcessor = new EmailProcessor();
         List<Email> emailList = List.of(
-                Email.builder().subject("Nigerian Prince").
-                        body("You are a prince you win 100000000 dollars").isImportant(true).build(),
+                Email.builder().subject("Nigerian Prince")
+                                .body("You are a prince you win 100000000 dollars").isImportant(true).build(),
                 Email.builder().body("Ololololo").isImportant(true).build(),
                 Email.builder().isImportant(true).body("Hello").subject("Hello").build(),
                 Email.builder().subject("Stupid Joke").isImportant(false).build(),
@@ -30,7 +30,7 @@ public class Test {
 
         System.out.println();
 
-        emailList.forEach(email -> System.out.println("Email's Theme: " + email.getSubject() + " Important email: " + email.getIsImportant()
-                + " Email's body: " + email.getBody()));
+        emailList.forEach(email -> System.out.println("Email's Theme: " + email.getSubject()
+                + " Important email: " + email.getIsImportant() + " Email's body: " + email.getBody()));
     }
 }
