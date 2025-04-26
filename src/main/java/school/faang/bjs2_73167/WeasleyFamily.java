6dc6ff213dfa157ddbf4c759bf7f20dc2c481a44
pirojok_yt <tamik34@mail.ru>
2025-04-26 17:47:27 +0500

    finish

--- src/main/java/school/faang/bjs2_73167/WeasleyFamily.java
+++ src/main/java/school/faang/bjs2_73167/WeasleyFamily.java
@@ -6,9 +6,10 @@ import java.util.concurrent.Executors;
 import java.util.concurrent.TimeUnit;
 
 public class WeasleyFamily {
-    private final static int minutesToWait = 5;
-    private final static List<String> choreList = List.of("Wash dishes", "Take out the trash",
+    private static final int minutesToWait = 5;
+    private static final List<String> choreList = List.of("Wash dishes", "Take out the trash",
             "Fly to the moon", "Create new thing");
+
     public static void main(String[] args) {
         List<Chore> chores = choreList.stream().map(Chore::new).toList();
         ExecutorService executorService = Executors.newCachedThreadPool();
