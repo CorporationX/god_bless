1f14150c6217c1c0909158d4653fa734bb9413fa
pirojok_yt <tamik34@mail.ru>
2025-04-26 09:14:52 +0500

    BJS2-72133

--- src/main/java/school/faang/bjs2_73138/MailSender.java
+++ src/main/java/school/faang/bjs2_73138/MailSender.java
@@ -2,20 +2,22 @@ package school.faang.bjs2_73138;
 
 public class MailSender {
     public static void main(String[] args) {
-        Thread[] threads = new Thread[5];
+        int amountOfThreads = 5;
+        Thread[] threads = new Thread[amountOfThreads];
         int lettersPerThread = 200;
 
-        for (int i = 0; i < 5; i++) {
+        for (int i = 0; i < amountOfThreads; i++) {
             String name = "Поток - " + i;
             int start = i * lettersPerThread + 1;
             int end = (i + 1) * lettersPerThread;
             threads[i] = new Thread(new SenderRunnable(start, end), name);
         }
 
-        for (int i = 0; i < 5; i++) {
+        for (int i = 0; i < amountOfThreads; i++) {
             try {
                 threads[i].join();
             } catch (InterruptedException e) {
+                Thread.currentThread().interrupt();
                 System.out.println("Поток прерван: " + threads[i].getName());
             }
         }
--- src/main/java/school/faang/bjs2_73138/SenderRunnable.java
+++ src/main/java/school/faang/bjs2_73138/SenderRunnable.java
@@ -5,13 +5,13 @@ import lombok.AllArgsConstructor;
 @AllArgsConstructor
 public class SenderRunnable implements Runnable {
 
-    private int startIndex;
-    private int endIndex;
+    private final int startIndex;
+    private final int endIndex;
 
     @Override
     public void run() {
         for (int i = startIndex; i <= endIndex; i++) {
-            System.out.println("Письмо" + i + "отправлено");
+            System.out.printf("Письмо %d отправлено\n", i);
         }
     }
 }
