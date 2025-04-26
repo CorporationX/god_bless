202bddc777b69eaae3624f26d5b9ea8002d9f4a9
Fagan Gafulov <gafulov@yandex.ru>
2025-04-26 18:01:53 +0300

    BJS2-73700

--- src/main/java/school/faang/mice/House.java
+++ src/main/java/school/faang/mice/House.java
@@ -1,20 +1,34 @@
 package school.faang.mice;
 
+import lombok.extern.slf4j.Slf4j;
+
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 import java.util.Objects;
+import java.util.Random;
+import java.util.concurrent.ThreadLocalRandom;
 
+@Slf4j
 public class House {
     private final List<Room> rooms = new ArrayList<>();
     public final List<Food> collectedFood = new ArrayList<>();
 
     public void collectFood() {
-        Collections.shuffle(rooms);
-        for (int i = 0; i < 2; i++) {
-            collectedFood.addAll(rooms.get(i).getFoods());
-            rooms.get(i).getFoods().clear();
-        }
+        Room roomOne = rooms.get(ThreadLocalRandom.current().nextInt(rooms.size()));
+        Room roomTwo;
+        do {
+            roomTwo = rooms.get(ThreadLocalRandom.current().nextInt(rooms.size()));
+        } while (roomOne == roomTwo);
+        log.info("Рандомно выбрали комнаты [{} и {}]", roomTwo, roomTwo);
+
+        Food foodOne = roomOne.getFoods().remove(0);
+        log.info("Убрали еду {} из комнаты {}", foodOne, roomOne);
+        Food foodTwo = roomTwo.getFoods().remove(0);
+        log.info("Убрали еду {} из комнаты {}", foodTwo, roomTwo);
+
+        collectedFood.add(foodOne);
+        collectedFood.add(foodTwo);
     }
 
     public boolean checkCollectedFood() {
--- src/main/java/school/faang/mice/Room.java
+++ src/main/java/school/faang/mice/Room.java
@@ -1,11 +1,16 @@
 package school.faang.mice;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 
 import java.util.ArrayList;
 import java.util.List;
 
 @Getter
+@AllArgsConstructor
 public class Room {
+    private final int roomNumber;
     private final List<Food> foods = new ArrayList<>();
+
 }
+
