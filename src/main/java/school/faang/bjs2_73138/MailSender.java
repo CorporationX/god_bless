628e756fdfad252b300425ce965bbc7271476e22
pirojok_yt <tamik34@mail.ru>
2025-04-26 22:24:57 +0500

    finish

--- src/main/java/school/faang/bjs2_73138/MailSender.java
+++ src/main/java/school/faang/bjs2_73138/MailSender.java
@@ -1,20 +1,20 @@
 package school.faang.bjs2_73138;
 
 public class MailSender {
-    private static final int amountOfThreads = 5;
-    private static final int lettersPerThread = 200;
+    private static final int AMOUNT_OF_THREADS = 5;
+    private static final int LETTERS_PER_THREAD = 200;
 
     public static void main(String[] args) {
-        Thread[] threads = new Thread[amountOfThreads];
+        Thread[] threads = new Thread[AMOUNT_OF_THREADS];
 
-        for (int i = 0; i < amountOfThreads; i++) {
+        for (int i = 0; i < AMOUNT_OF_THREADS; i++) {
             String name = "Поток - " + i;
-            int start = i * lettersPerThread + 1;
-            int end = (i + 1) * lettersPerThread;
+            int start = i * LETTERS_PER_THREAD + 1;
+            int end = (i + 1) * LETTERS_PER_THREAD;
             threads[i] = new Thread(new SenderRunnable(start, end), name);
         }
 
-        for (int i = 0; i < amountOfThreads; i++) {
+        for (int i = 0; i < AMOUNT_OF_THREADS; i++) {
             try {
                 threads[i].join();
             } catch (InterruptedException e) {
