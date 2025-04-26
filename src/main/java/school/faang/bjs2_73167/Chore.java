5b6d76d758d8ad34d683e6588a25d1132f14dfad
pirojok_yt <tamik34@mail.ru>
2025-04-26 22:22:55 +0500

    finish

--- src/main/java/school/faang/bjs2_73167/Chore.java
+++ src/main/java/school/faang/bjs2_73167/Chore.java
@@ -6,13 +6,14 @@ import lombok.Getter;
 @Getter
 @AllArgsConstructor
 public class Chore implements Runnable {
+    private static final int SLEEP_TIME = 1000;
     private String chore;
 
     @Override
     public void run() {
         try {
             System.out.printf("Thread is starting task: %s\n", chore);
-            Thread.sleep(1000);
+            Thread.sleep(SLEEP_TIME);
         } catch (InterruptedException e) {
             Thread.currentThread().interrupt();
             System.out.println("Thread's work was interrupted");
