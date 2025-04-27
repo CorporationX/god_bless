81558c18a9c2d1e46ac80facb29d853f4d50a8b4
pirojok_yt <tamik34@mail.ru>
2025-04-27 21:24:33 +0500

    finish

--- src/main/java/school/faang/bjs2_63405/Player.java
+++ src/main/java/school/faang/bjs2_63405/Player.java
@@ -2,34 +2,33 @@ package school.faang.bjs2_63405;
 
 public class Player {
 
-    private String isPlaying = "Not playing";
+    private boolean isPlaying = false;
+    private final Object lock = new Object();
 
     public void play() {
-        synchronized (isPlaying) {
-            isPlaying = "Playing";
-            System.out.println("Music is playing");
+        if (!isPlaying) {
+            synchronized (lock) {
+                isPlaying = true;
+                System.out.println("Music is playing");
+            }
         }
     }
 
     public void pause() {
-        synchronized (isPlaying) {
-            isPlaying = "On pause";
-            System.out.println("Music is stopped");
+        if (isPlaying) {
+            synchronized (lock) {
+                isPlaying = false;
+                System.out.println("Music is stopped");
+            }
         }
     }
 
     public void skip() {
-        synchronized (isPlaying) {
-            isPlaying = "Playing";
             System.out.println("Next song");
-        }
     }
 
     public void previous() {
-        synchronized (isPlaying) {
-            isPlaying = "Playing";
             System.out.println("Previous song");
-        }
     }
 }
 
