7452f39c511113a59f8333456e658fd272c7fb94
Ramil Burgart <ramilburgart7006@gmail.com>
2025-04-27 21:21:40 +0300

    fixed code

--- src/main/java/school/faang/bro_force/Game.java
+++ src/main/java/school/faang/bro_force/Game.java
@@ -1,57 +1,58 @@
 package school.faang.bro_force;
 
-import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import lombok.Setter;
+import lombok.extern.slf4j.Slf4j;
+
+import java.util.function.Predicate;
 
 @RequiredArgsConstructor
 @Getter
 @Setter
+@Slf4j
 public class Game {
 
     private int score = 0;
     private int lives = 5;
-
     private final Object scoreLock = new Object();
     private final Object livesLock = new Object();
-    private boolean isGameOver = true;
+    private boolean isGameOver = false;
+    private static final Predicate<Integer> predicateScore = scores -> scores == 10;
+    private static final Predicate<Integer> predicateLives = live -> live == 0;
 
     public void update(boolean isScorePoints, boolean isLosesLives) {
         synchronized (scoreLock) {
-            if (!isGameOver) {
+            if (isGameOver) {
+                log.info("Вы набрали необходимое количество очков, вы выиграли");
                 return;
-            }
-            if (isScorePoints) {
+            } else if (isScorePoints) {
                 score++;
                 System.out.printf("игрок увеличил очки на  %d \n", score);
-            } else {
-                gameOver();
+                checkIfGameOver();
             }
         }
 
         synchronized (livesLock) {
-            if (!isGameOver) {
+            if (isGameOver) {
+                log.info("Жизни потрачены вы проиграли");
                 return;
-            }
-            if (isLosesLives) {
+            } else if (isLosesLives) {
                 lives--;
                 System.out.printf("игрок теряет прочность на  %d \n", lives);
-            } else {
-                gameOver();
+                checkIfGameOver();
             }
         }
     }
 
-
-    public void gameOver() {
-        if (score == 10) {
+    public void checkIfGameOver() {
+        if (predicateScore.test(score)) {
             System.out.println("win");
-            isGameOver = false;
+            isGameOver = true;
         }
-        if (lives == 0) {
+        if (predicateLives.test(lives)) {
             System.out.println("Game Over");
-            isGameOver = false;
+            isGameOver = true;
         }
     }
 }
