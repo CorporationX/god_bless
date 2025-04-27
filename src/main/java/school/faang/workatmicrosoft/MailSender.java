1c5ea280e61e4723d84dab5d5fd13030b0a26fb9
AdminS <neptunchik1997@gmail.com>
2025-04-27 16:38:21 +0300

    add: Вы работаете в Microsoft!

---
+++ src/main/java/school/faang/workatmicrosoft/MailSender.java
@@ -0,0 +1,32 @@
+package school.faang.workatmicrosoft;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MailSender {
+    private static final int TOTAL_MESSAGES = 100;
+    private static final int THREADS_COUNT = 5;
+
+    public static void main(String[] args) throws InterruptedException {
+        int batchSize = TOTAL_MESSAGES / THREADS_COUNT;
+        List<Thread> threads = new ArrayList<>();
+
+        for (int i = 0; i < THREADS_COUNT; i++) {
+            int start = i * batchSize;
+            int end = start + batchSize;
+            Thread thread = new Thread(new SenderRunnable(start, end));
+            threads.add(thread);
+            thread.start();
+        }
+
+        for (Thread thread : threads) {
+            try {
+                thread.join();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+
+        System.out.println("Все письма отправлены!");
+    }
+}
