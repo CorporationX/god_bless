41cba059f14ca8995cff3190e1277f5b37e24967
AlexandrPopov <164056516+Aleck210popov@users.noreply.github.com>
2025-04-27 21:39:38 +0300

    BJS2-74874 create task "Twitter"

---
+++ src/main/java/school/faang/bjs2_74752/Main.java
@@ -0,0 +1,50 @@
+package school.faang.bjs2_74752;
+
+import lombok.extern.slf4j.Slf4j;
+
+import java.util.List;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+import static java.util.concurrent.TimeUnit.SECONDS;
+
+@Slf4j
+public class Main {
+    private static final int MAX_EXPECTATION = 60;
+    private static final int NUM_THREADS = 10;
+
+    public static void main(String[] args) {
+        List<TwitterAccount> twitterAccountList = List.of(
+                new TwitterAccount("Twitter", 100),
+                new TwitterAccount("Elon Musk", 203),
+                new TwitterAccount("Cristiano Ronaldo", 10200));
+        TwitterSubscriptionSystem system = new TwitterSubscriptionSystem();
+
+
+        ExecutorService pool = Executors.newFixedThreadPool(NUM_THREADS);
+
+        CompletableFuture.allOf(
+                twitterAccountList.stream()
+                        .map(account -> CompletableFuture.runAsync(() -> system.followAccount(account, pool))
+                                .thenRun(() -> log.info("Everyone signed up for an account {}", account.getUsername())))
+                        .toArray(CompletableFuture[]::new)).join();
+
+        log.info("All tasks completed");
+        gracefullyShutdown(pool);
+    }
+
+
+    private static void gracefullyShutdown(ExecutorService pool) {
+        pool.shutdown();
+        try {
+            boolean isClose = pool.awaitTermination(MAX_EXPECTATION, SECONDS);
+            if (!isClose) {
+                pool.shutdownNow();
+            }
+        } catch (InterruptedException ex) {
+            log.error("Thread stoppage error");
+            pool.shutdownNow();
+        }
+    }
+}
---
+++ src/main/java/school/faang/bjs2_74752/TwitterSubscriptionSystem.java
@@ -0,0 +1,24 @@
+package school.faang.bjs2_74752;
+
+import lombok.extern.slf4j.Slf4j;
+
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutorService;
+import java.util.stream.IntStream;
+
+@Slf4j
+public final class TwitterSubscriptionSystem {
+
+    public void followAccount(TwitterAccount account, ExecutorService pool) {
+        CompletableFuture.allOf(
+                IntStream.range(0, 10)
+                        .mapToObj(i -> CompletableFuture.runAsync(() -> addFollower(account), pool)
+                                .thenRun(() -> log.info("Number {} subscribed to the {} account.",
+                                        i, account.getUsername())))
+                        .toArray(CompletableFuture[]::new));
+    }
+
+    private void addFollower(TwitterAccount account) {
+        account.getFollowers().incrementAndGet();
+    }
+}
