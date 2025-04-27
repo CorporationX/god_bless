a1995e992d6314e198c2f164bc20698cb69c1cf1
trytofixme <semenov.nikita@phystech.edu>
2025-04-27 13:54:23 +0300

    Повернём матрицу

---
+++ src/main/java/school/faang/bjs2_71595/Coordinates.java
@@ -0,0 +1,4 @@
+package school.faang.bjs2_71595;
+
+public record Coordinates(int coordinateX, int coordinateY) {
+}
---
+++ src/main/java/school/faang/bjs2_71595/MatrixTransformer.java
@@ -0,0 +1,6 @@
+package school.faang.bjs2_71595;
+
+@FunctionalInterface
+public interface MatrixTransformer {
+    Coordinates transform(int row, int column);
+}
