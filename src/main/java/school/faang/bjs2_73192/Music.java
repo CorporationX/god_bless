f436a2121f79c5ebf291c81ffaa68eecec3958e7
Jegor <gaanjara3@gmail.com>
2025-04-27 12:41:38 +0300

    [UPDATE] BJS2-73192 Исправил оформление.

--- src/main/java/school/faang/bjs2_73192/Music.java
+++ src/main/java/school/faang/bjs2_73192/Music.java
@@ -1,10 +1,16 @@
 package school.faang.bjs2_73192;
 
+import lombok.extern.slf4j.Slf4j;
+
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.TimeUnit;
 
+@Slf4j
 public class Music {
+
+    private static final int THREAD_AMOUNT = 4;
+    private static final int WAITING_TIME = 5;
 
     public static void main(String[] args) {
         Player player = new Player();
@@ -14,7 +20,7 @@ public class Music {
         Runnable skip = player::skip;
         Runnable previous = player::previous;
 
-        ExecutorService executor = Executors.newFixedThreadPool(4);
+        ExecutorService executor = Executors.newFixedThreadPool(THREAD_AMOUNT);
 
         executor.execute(play);
         executor.execute(pause);
@@ -24,8 +30,8 @@ public class Music {
         executor.shutdown();
 
         try {
-            if (!executor.awaitTermination(5, TimeUnit.MINUTES)) {
-                System.out.println("Задачи не завершились за 5 минут, принудительно останавливаем...");
+            if (!executor.awaitTermination(WAITING_TIME, TimeUnit.MINUTES)) {
+                log.info("Задачи не завершились за {} минут, принудительно останавливаем...", WAITING_TIME);
                 executor.shutdownNow();
             }
         } catch (InterruptedException e) {
--- src/main/java/school/faang/bjs2_73192/Player.java
+++ src/main/java/school/faang/bjs2_73192/Player.java
@@ -10,6 +10,16 @@ public class Player {
 
     public void play() {
         synchronized (LOCK) {
+            if (isPlaying) {
+                log.info("Thread -> {} in Player.play(), music is already playing",
+                        Thread.currentThread().getId());
+                try {
+                    LOCK.wait(3000);
+                    play();
+                } catch (InterruptedException e) {
+                    throw new RuntimeException(e);
+                }
+            }
             log.info("Thread -> {} in Player.play(), isPlaying -> {} | BEFORE",
                     Thread.currentThread().getId(), isPlaying);
             isPlaying = true;
@@ -20,6 +30,16 @@ public class Player {
 
     public void pause() {
         synchronized (LOCK) {
+            if (!isPlaying) {
+                log.info("Thread -> {} in Player.pause(), music is already on pause",
+                        Thread.currentThread().getId());
+                try {
+                    LOCK.wait(3000);
+                    pause();
+                } catch (InterruptedException e) {
+                    throw new RuntimeException(e);
+                }
+            }
             log.info("Thread -> {} in Player.pause(), isPlaying -> {} | BEFORE",
                     Thread.currentThread().getId(), isPlaying);
             isPlaying = false;
