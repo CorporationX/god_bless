10637568d809811f11a112d9bc84bb275a26333a
Andrew Sonin <sonin.andrei2009@yandex.ru>
2025-04-27 22:50:56 +0300

    Task BJS2-72931 1) Add class SenderRunnable 2) Do task (maybe)

---
+++ src/main/java/school/faang/reverse/microsoft/Main.java
@@ -0,0 +1,24 @@
+package school.faang.reverse.microsoft;
+
+public class Main {
+    public static void main(String[] args) {
+        Thread[] threads = new Thread[5];
+
+        for (int i = 0; i < 5; i++) {
+            int startIndex = i * 200;
+            int endIndex = startIndex + 200;
+            threads[i] = new Thread(new SenderRunnable(startIndex, endIndex));
+            threads[i].start();
+        }
+
+        for (Thread thread : threads) {
+            try {
+                thread.join();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+
+        System.out.println("Все письма успешно отправлены.");
+    }
+}
