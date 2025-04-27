03911bda36770156639e33c53b333b465cfc383d
Tanya <hummingbird_t@mail.ru>
2025-04-27 15:39:02 +0300

    initial commit

---
+++ src/main/java/school/faang/stream4/asyncandfuture/Main.java
@@ -0,0 +1,7 @@
+package school.faang.stream4.asyncandfuture;
+
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/main/java/school/faang/stream4/asyncandfuture/MasterCardService.java
@@ -0,0 +1,6 @@
+package school.faang.stream4.asyncandfuture;
+
+public class MasterCardService {
+
+    public String collectPayment(String )
+}
