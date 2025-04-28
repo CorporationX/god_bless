85a4ba6d686919ccb1ce2a5de7c6edb09e916cdb
P-Lidia <Lydiasmolyakova@gmail.com>
2025-04-28 12:53:10 +0300

    BJS2-73672: fix Main class

--- src/main/java/school/faang/broforce/Main.java
+++ src/main/java/school/faang/broforce/Main.java
@@ -1,12 +1,14 @@
 package school.faang.broforce;
 
 public class Main {
+    private static final double EARN_POINTS_PROBABILITY = 0.6;
+    private static final double LOSE_LIFE_PROBABILITY = 0.8;
 
     public static void main(String[] args) {
         Game game = new Game();
         for (int i = 0; i < 100; i++) {
-            boolean isScored = Math.random() < 0.6;
-            boolean isLostLive = Math.random() < 0.8;
+            boolean isScored = Math.random() < EARN_POINTS_PROBABILITY;
+            boolean isLostLive = Math.random() < LOSE_LIFE_PROBABILITY;
             new Thread(() -> game.update(isScored, isLostLive)).start();
         }
     }
