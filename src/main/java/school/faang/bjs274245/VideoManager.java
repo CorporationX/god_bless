8e60af5c5f3779f5b95cfc3977e59b778c358a3c
loofaxu <sabeta.sabawa@gmail.com>
2025-04-28 03:31:19 +0300

    task

--- src/main/java/school/faang/bjs274245/VideoManager.java
+++ src/main/java/school/faang/bjs274245/VideoManager.java
@@ -6,7 +6,6 @@ import java.util.Map;
 public class VideoManager {
     private Map<String, Integer> viewsMap = new HashMap<>();
 
-
     public synchronized void addView(String videoId) {
         viewsMap.merge(videoId, 1, Integer::sum);
     }
