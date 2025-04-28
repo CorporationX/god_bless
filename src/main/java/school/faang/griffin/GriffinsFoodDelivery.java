753621f33d0a3e1541564f40782ebd656a69caa8
Gigabyte <012_erasyl@mail.ru>
2025-04-28 18:17:35 +0500

    Griffins Commit

--- src/main/java/school/faang/griffin/GriffinsFoodDelivery.java
+++ src/main/java/school/faang/griffin/GriffinsFoodDelivery.java
@@ -3,10 +3,13 @@ package school.faang.griffin;
 import java.util.Random;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
 
 public class GriffinsFoodDelivery {
+    private static final int THREAD_POOL_SIZE = 3;
+
     public static void main(String[] args) {
-        ExecutorService executor = Executors.newFixedThreadPool(3);
+        ExecutorService executor = Executors.newFixedThreadPool(THREAD_POOL_SIZE);
         String[] characterNames = {"Peter", "Lois", "Meg", "Chris", "Stewie"};
         Random random = new Random();
 
@@ -17,5 +20,14 @@ public class GriffinsFoodDelivery {
         }
 
         executor.shutdown();
+        try {
+            if (!executor.awaitTermination(10, TimeUnit.SECONDS)) {
+                System.out.println("Принудительная остановка.");
+                executor.shutdownNow();
+            }
+        } catch (InterruptedException e) {
+            System.out.println("Главный поток был прерван.");
+            executor.shutdownNow();
+        }
     }
 }
