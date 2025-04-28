772bd12c6d637a9cffa583f134a7669b54c1c695
Sergey Selischev <ssb22@mail.ru>
2025-04-28 17:18:27 +0300

    решение задачи BJS2-73055

---
+++ src/main/java/school/faang/bjs2_73055/MailSender.java
@@ -0,0 +1,26 @@
+package school.faang.bjs2_73055;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+public class MailSender {
+    private static final int TOTAL_COUNT = 5000;
+    private static final int TOTAL_THREAD = 5;
+
+    public static void main(String[] args) throws InterruptedException {
+        int batch = TOTAL_COUNT / TOTAL_THREAD;
+        Thread[] threadPool = new Thread[TOTAL_THREAD];
+        for (int i = 0; i < TOTAL_THREAD; i++) {
+            int startIndex = i * batch;
+            int endIndex = (i + 1) * batch;
+            threadPool[i] = new Thread(new SenderRunnable(startIndex, endIndex));
+            threadPool[i].start();
+        }
+
+        for (Thread thread : threadPool) {
+            thread.join();
+        }
+
+        log.info("Complete.");
+    }
+}
