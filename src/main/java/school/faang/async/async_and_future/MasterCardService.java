aceb3587b726a3f224ed4d6d1b7fed6b047e4c38
Keriod <nikolainovopashin98@gmail.com>
2025-04-28 19:13:39 +0500

    BJS2-74403 - PR changes

--- src/main/java/school/faang/async/async_and_future/MasterCardService.java
+++ src/main/java/school/faang/async/async_and_future/MasterCardService.java
@@ -15,21 +15,26 @@ public class MasterCardService {
     private static final int ONE_SECOND_IN_MS = 1_000;
     private static final int TERMINATION_IN_MS = 20_000;
 
-    public static int collectPayment() {
+    public static int collectPayment() throws InterruptedException {
+        Thread.sleep(TEN_SECONDS_IN_MS);
         return 5_000;
     }
 
-    public static int sendAnalytics() {
+    public static int sendAnalytics() throws InterruptedException {
+        Thread.sleep(ONE_SECOND_IN_MS);
         return 17_000;
     }
 
     public void doAll() throws ExecutionException, InterruptedException {
-        ScheduledExecutorService executorService = Executors.newScheduledThreadPool(1);
-        Future<Integer> payment = executorService.schedule(MasterCardService::collectPayment, TEN_SECONDS_IN_MS,
-                TimeUnit.MILLISECONDS);
-
-        CompletableFuture<Integer> analytics = CompletableFuture.supplyAsync(MasterCardService::sendAnalytics,
-                CompletableFuture.delayedExecutor(ONE_SECOND_IN_MS, TimeUnit.MILLISECONDS));
+        ScheduledExecutorService executorService = Executors.newSingleThreadScheduledExecutor();
+        Future<Integer> payment = executorService.submit(MasterCardService::collectPayment);
+        CompletableFuture<Integer> analytics = CompletableFuture.supplyAsync(() -> {
+            try {
+                return sendAnalytics();
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+        });
 
         log.info("Платеж выполнен: {}", payment.get());
         log.info("Аналитика отправлена: {}", analytics.join());
