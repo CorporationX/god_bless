19a8a59c9bdc474c6471b41311945aa5894f4813
Kirill <kirill551@yandex.ru>
2025-04-28 20:37:52 +0200

    "Style"

--- src/main/java/school/faang/wow/QuestSystem.java
+++ src/main/java/school/faang/wow/QuestSystem.java
@@ -11,14 +11,14 @@ public class QuestSystem {
     public CompletableFuture<Player> startQuest(Player player, Quest quest) {
         log.debug("Starting quest {} by player {}", quest.getName(), player.getName());
         return CompletableFuture.supplyAsync(() -> {
-                    try {
-                        TimeUnit.SECONDS.sleep(quest.getDifficulty());
-                    } catch (InterruptedException e) {
-                        log.error(e.getMessage(), e);
-                        Thread.currentThread().interrupt();
-                    }
-                    return player;
-                }, ThreadPullSupplier.executor)
+            try {
+                TimeUnit.SECONDS.sleep(quest.getDifficulty());
+            } catch (InterruptedException e) {
+                log.error(e.getMessage(), e);
+                Thread.currentThread().interrupt();
+            }
+            return player;
+            }, ThreadPullSupplier.executor)
                 .thenApply(eachPlayer -> {
                     eachPlayer.updateExperience(quest.getReward());
                     return eachPlayer;
