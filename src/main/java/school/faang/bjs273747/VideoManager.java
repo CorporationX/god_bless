c4f90b8bf0cf1a594e4ef391de6f4721091cff9b
SAM <SULEYMANOV.USA@GMAIL.COM>
2025-04-28 07:53:31 -0700

    73747

--- src/main/java/school/faang/bjs273747/VideoManager.java
+++ src/main/java/school/faang/bjs273747/VideoManager.java
@@ -6,12 +6,12 @@ import java.util.Map;
 public class VideoManager {
     private final Map<String, Integer> viewsMap = new HashMap<>();
 
-    public synchronized void addView(String videoId){
+    public synchronized void addView(String videoId) {
         viewsMap.merge(videoId, 1, Integer::sum);
         System.out.printf("Video added: %s (%s)\n", videoId, Thread.currentThread().getName());
     }
 
-    public synchronized int getView(String videoId){
+    public synchronized int getView(String videoId) {
         return viewsMap.get(videoId);
     }
 }
