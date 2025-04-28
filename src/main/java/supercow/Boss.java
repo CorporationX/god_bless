38fb458d4dff31704a8e74e5f33a3d1767b4a21b
Buga Dan <danbil.942@gmail.com>
2025-04-28 15:38:27 +0300

    BSJ2-73457 task. Fixed all issues

--- src/main/java/supercow/Boss.java
+++ src/main/java/supercow/Boss.java
@@ -11,35 +11,31 @@ public class Boss {
     private int maxPlayers = 5;
     private int currentPlayers;
 
-    public void joinBattle(Player player) {
-        synchronized (this) {
-            if (Objects.isNull(player)) {
-                log.error("Null argument!");
-                return;
-            }
-
-            if (currentPlayers == maxPlayers) {
-                try {
-                    this.wait();
-                } catch (InterruptedException e) {
-                    log.error("Thread is interrupted!" + e.getMessage());
-                    Thread.currentThread().interrupt();
-                }
-            }
-            currentPlayers++;
-            log.info("Player {} join the battle with boss...", player.getName());
+    public synchronized void joinBattle(Player player) {
+        if (Objects.isNull(player)) {
+            log.error("Null argument!");
+            return;
         }
+
+        while (currentPlayers == maxPlayers) {
+            try {
+                this.wait();
+            } catch (InterruptedException e) {
+                log.error("Thread is interrupted!" + e.getMessage());
+                Thread.currentThread().interrupt();
+            }
+        }
+        currentPlayers++;
+        log.info("Player {} join the battle with boss...", player.getName());
     }
 
-    public void leaveBattle(Player player) {
-        synchronized (this) {
-            if (Objects.isNull(player)) {
-                log.error("Null argument!");
-                return;
-            }
-            currentPlayers--;
-            this.notify();
-            log.info("Player {} leave the battle with boss...", player.getName());
+    public synchronized void leaveBattle(Player player) {
+        if (Objects.isNull(player)) {
+            log.error("Null argument!");
+            return;
         }
+        currentPlayers--;
+        this.notify();
+        log.info("Player {} leave the battle with boss...", player.getName());
     }
 }
