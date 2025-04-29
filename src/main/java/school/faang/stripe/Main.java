370f500191b929dd8be9ccd147882aa5ce856a84
Ramil Burgart <ramilburgart7006@gmail.com>
2025-04-29 22:35:07 +0300

    fixed code

--- src/main/java/school/faang/stripe/Main.java
+++ src/main/java/school/faang/stripe/Main.java
@@ -11,6 +11,8 @@ public class Main {
     private static final int AMOUNT_PAYMENT = 3000;
     private static final int AMOUNT_ANALYTICS = 10000;
 
+    private static final int TIME_WAIT = 1;
+
     public static void main(String[] args) {
         MasterCardService cardService = new MasterCardService();
         ExecutorService service = Executors.newCachedThreadPool();
@@ -29,6 +31,10 @@ public class Main {
         int analytics = analyticsFuture.join();
         System.out.printf("Analytics %d", analytics);
 
+        gracefullyShutdown(service);
+    }
+
+    private static void gracefullyShutdown(ExecutorService service) {
         service.shutdown();
         try {
             if (!service.awaitTermination(1, TimeUnit.MINUTES)) {
