10f697410893f73c8ffca75b870001c59268142f
Aleksandr <alexandr.sidorkin@gmail.com>
2025-04-29 23:42:30 +0200

    BJS2-75057 Task done

---
+++ src/main/java/school/faang/stream4/alchemylevelingup/PotionGathering.java
@@ -0,0 +1,64 @@
+package school.faang.stream4.alchemylevelingup;
+
+import lombok.extern.slf4j.Slf4j;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.function.IntFunction;
+
+@Slf4j
+public class PotionGathering {
+    private static ExecutorService service;
+
+    public static void main(String[] args) {
+        service = Executors.newCachedThreadPool();
+
+        List<Potion> potions = List.of(
+                new Potion("Healing Potion", 5),
+                new Potion("Mana Potion", 3),
+                new Potion("Stamina Potion", 4)
+        );
+
+        System.out.println(gatherAllIngredients(potions));
+        endGathering(service);
+    }
+
+    private static int gatherAllIngredients(List<Potion> potions) {
+
+        CompletableFuture<Integer>[] completableFutures = potions.stream()
+                .map(potion -> CompletableFuture.supplyAsync(() -> gatherIngredients(potion), service))
+                .toArray((IntFunction<CompletableFuture<Integer>[]>) CompletableFuture[]::new);
+
+        return CompletableFuture.allOf(completableFutures)
+                .thenApply(done ->
+                        Arrays.stream(completableFutures)
+                                .mapToInt(CompletableFuture::join)
+                                .sum())
+                .join();
+    }
+
+    private static int gatherIngredients(Potion potion) {
+        log.info("Gathering ingridients for {} started", potion.getName());
+        long requiredTime = potion.getRequiredIngredients() * 1500L;
+        makeDelayOnGathering(requiredTime);
+        log.info("Gathering ingridients for {} is over", potion.getName());
+        return potion.getRequiredIngredients();
+    }
+
+    private static void makeDelayOnGathering(long delay) {
+        try {
+            Thread.sleep(delay);
+        } catch (InterruptedException e) {
+            log.error("interrupted exception during delay: {}", e.getMessage());
+            Thread.currentThread().interrupt();
+        }
+    }
+
+    public static void endGathering(ExecutorService service) {
+        log.info("Gathering is over!");
+        service.shutdown();
+    }
+}
