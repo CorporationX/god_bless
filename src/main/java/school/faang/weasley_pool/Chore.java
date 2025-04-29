01b717e0ee4e1f7e44aa652e72aa3b2e8a6a1199
Artmasterrr <artmasterrr@hotmail.com>
2025-04-29 19:42:58 +0200

    Commit 2

--- src/main/java/school/faang/weasley_pool/Chore.java
+++ src/main/java/school/faang/weasley_pool/Chore.java
@@ -5,6 +5,7 @@ import lombok.extern.slf4j.Slf4j;
 @Slf4j
 public class Chore implements Runnable {
     private String chore;
+    private static final long DELAY_MS = 5L;
 
     public Chore(String chore) {
         this.chore = chore;
@@ -13,8 +14,9 @@ public class Chore implements Runnable {
     @Override
     public void run() {
         try {
-            Thread.sleep(5L);
+            Thread.sleep(DELAY_MS);
         } catch (InterruptedException e) {
+            Thread.currentThread().interrupt();
             throw new RuntimeException(e);
         }
         log.info("Поток {} выполняет зачаду {}", Thread.currentThread().getName(), chore);
