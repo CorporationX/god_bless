da797e8f44727b4bf06e994d6a532cf50f839504
Daniyal-Akhadov <d.ahanewmail@mail.ru>
2025-04-29 07:25:36 +0300

    feat: бизнес-логика

---
+++ src/main/java/src/faang/VideoManager.java
@@ -0,0 +1,20 @@
+package src.faang;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class VideoManager {
+    private final Map<String, Integer> viewsMap = new HashMap<>();
+
+    public synchronized void addView(String videoId) {
+        this.viewsMap.put(videoId, viewsMap.getOrDefault(videoId, 0) + 1);
+    }
+
+    public synchronized int getViewCount(String videoId) {
+        return this.viewsMap.get(videoId);
+    }
+
+    public synchronized Map<String, Integer> getViewsMap() {
+        return new HashMap<>(this.viewsMap);
+    }
+}
