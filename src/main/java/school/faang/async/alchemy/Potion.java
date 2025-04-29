a2c2e46aa55aead4d56b15f668e0e07e35be1634
Keriod <nikolainovopashin98@gmail.com>
2025-04-29 23:31:46 +0500

    BJS2-75054 - Качаем алхимию

---
+++ src/main/java/school/faang/async/alchemy/Potion.java
@@ -0,0 +1,12 @@
+package school.faang.async.alchemy;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
+public class Potion {
+
+    private final String name;
+    private final int requiredIngredients;
+}
