6b9335480cc479714790af02dbda3eaaa14c0bf4
BogatyrevDL <d.l.bogatyrev@gmail.com>
2025-04-29 09:19:54 +0300

    BJS2-74542: fix comment PR

--- src/main/java/school/faang/amazon/OrderProcessor.java
+++ src/main/java/school/faang/amazon/OrderProcessor.java
@@ -7,6 +7,7 @@ import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.ThreadLocalRandom;
+import java.util.concurrent.TimeUnit;
 import java.util.concurrent.atomic.AtomicInteger;
 
 @Slf4j
@@ -43,7 +44,23 @@ public class OrderProcessor {
                 .toList();
 
         CompletableFuture.allOf(futures.toArray(new CompletableFuture[0]))
-                .thenRun(executor::shutdown)
+                .thenRun(this::shutdown)
+                .exceptionally(ex -> {
+                    log.error("Ошибка при обработке заказов", ex);
+                    shutdown();
+                    return null;
+                })
                 .join();
+    }
+
+    public void shutdown() {
+        executor.shutdown();
+        try {
+            if (!executor.awaitTermination(5, TimeUnit.SECONDS)) {
+                executor.shutdownNow();
+            }
+        } catch (InterruptedException e) {
+            executor.shutdownNow();
+        }
     }
 }
