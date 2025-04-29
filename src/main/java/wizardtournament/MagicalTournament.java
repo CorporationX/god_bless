4496519c7f2f37a2b315f768b925554d12cf08af
Emil Dzheenbaev <emil.dzheenbaev@gmail.com>
2025-04-29 21:26:39 +0200

    поправка задачи Турнир трех волшебников

--- src/main/java/wizardtournament/MagicalTournament.java
+++ src/main/java/wizardtournament/MagicalTournament.java
@@ -43,9 +43,6 @@ public class MagicalTournament {
             int hogwartsPoints = hogwarts.getTotalPoints();
             int durmstrangPoints = durmstrang.getTotalPoints();
             int beauxbatonsPoints = beauxbatons.getTotalPoints();
-            System.out.println("Hogwarts: " + hogwarts.getTotalPoints());
-            System.out.println("Durmstrang: " + durmstrang.getTotalPoints());
-            System.out.println("Beauxbatons: " + beauxbatons.getTotalPoints());
 
             if (hogwartsPoints > durmstrangPoints && hogwartsPoints > beauxbatonsPoints) {
                 System.out.println("Hogwarts wins!");
