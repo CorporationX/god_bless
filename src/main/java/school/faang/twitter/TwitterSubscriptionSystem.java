89da1df398d5840bb97a507338a4eb21e5dc4a40
Kirill <kirill551@yandex.ru>
2025-04-29 20:03:33 +0200

    "Выполнить задачу согласно условиям"

---
+++ src/main/java/school/faang/twitter/TwitterSubscriptionSystem.java
@@ -0,0 +1,27 @@
+package school.faang.twitter;
+
+import lombok.extern.slf4j.Slf4j;
+
+import java.util.concurrent.CompletableFuture;
+
+@Slf4j
+public class TwitterSubscriptionSystem {
+    private static final int ONE_SECOND_IN_MS = 1000;
+
+    public CompletableFuture<Void> followAccount(TwitterAccount account) {
+        log.debug("Following account: {}", account);
+        return CompletableFuture.runAsync(() -> {
+            addFollower(account);
+            try {
+                Thread.sleep(ONE_SECOND_IN_MS);
+            } catch (InterruptedException e) {
+                log.debug("Following account was interrupted", e);
+            }
+        }, ThreadPullProvider.executor);
+    }
+
+    private void addFollower(TwitterAccount account) {
+        log.debug("Adding follower: {}", account);
+        account.addOneFollower();
+    }
+}
