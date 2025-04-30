5f62bcb15d5e92c8bdf9b890694d131db116005c
EmilDzh <126710982+EmilDzh@users.noreply.github.com>
2025-04-30 09:35:56 +0200

    решение задачи Счетчик просмотров на YouTube (#19942)
    * решение задачи Счетчик просмотров на YouTube
    
    * поправка задачи Счетчик просмотров на YouTube

---
+++ src/main/java/youtube/Main.java
@@ -0,0 +1,34 @@
+package youtube;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
+
+public class Main {
+    private static final int NUM_THREADS = 5;
+    private static final int NUM_VIDEOS = 25;
+
+    public static void main(String[] args) {
+        VideoManager manager = new VideoManager();
+
+        ExecutorService service = Executors.newFixedThreadPool(NUM_THREADS);
+
+        for (int i = 0; i < NUM_VIDEOS; i++) {
+            String videoId = String.format("video-%d", i);
+            for (int j = 0; j < NUM_THREADS; j++) {
+                service.submit(() -> {
+                    manager.addView(videoId);
+                    System.out.printf("View count for video %s: %d%n", videoId, manager.getViewCount(videoId));
+                });
+            }
+        }
+        service.shutdown();
+        try {
+            if (!service.awaitTermination(1, TimeUnit.MINUTES)) {
+                service.shutdownNow();
+            }
+        } catch (InterruptedException e) {
+            service.shutdownNow();
+        }
+    }
+}
