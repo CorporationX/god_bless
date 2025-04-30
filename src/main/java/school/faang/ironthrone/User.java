51b943a7cf59b18c348dc92c0c687464964fbe9b
P-Lidia <Lydiasmolyakova@gmail.com>
2025-04-30 19:57:21 +0300

    Fix User class

--- src/main/java/school/faang/ironthrone/User.java
+++ src/main/java/school/faang/ironthrone/User.java
@@ -11,16 +11,24 @@ public class User {
     private House house;
 
     public void joinHouse(House house) {
-        this.house = house;
-        assignedRole = house.assignRole();
-        log.info("{} got the role {}", name, assignedRole);
+        synchronized (house) {
+            this.house = house;
+            assignedRole = house.assignRole();
+            log.info("{} got the role {}", name, assignedRole);
+        }
     }
 
     public void leaveHouse(House house) {
-        if (house != null && assignedRole != null) {
-            house.releaseRole(assignedRole);
-            log.info("{} has left the role {}", name, assignedRole);
-            assignedRole = null;
+        synchronized (house) {
+            if (house != null && assignedRole != null) {
+                try {
+                    house.releaseRole(assignedRole);
+                    log.info("{} has left the role {}", name, assignedRole);
+                } finally {
+                    assignedRole = null;
+                    house = null;
+                }
+            }
         }
     }
 }
