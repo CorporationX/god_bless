a02bc29b61a75a89214eb0d3267bd0ce28f03e6c
Kirill <kirill551@yandex.ru>
2025-04-30 16:23:17 +0200

    "Внести правки согласно комментариям"

--- src/main/java/school/faang/magicians/School.java
+++ src/main/java/school/faang/magicians/School.java
@@ -3,6 +3,8 @@ package school.faang.magicians;
 import lombok.Data;
 
 import java.util.List;
+import java.util.Objects;
+import java.util.concurrent.atomic.AtomicInteger;
 
 @Data
 public class School {
@@ -10,7 +12,7 @@ public class School {
     private List<Student> students;
 
     public int getPoints() {
-        return students.stream().mapToInt(student -> student.getPoints().get()).sum();
+        return students.stream().map(Student::getPoints).filter(Objects::nonNull).mapToInt(AtomicInteger::get).sum();
     }
 
     public School(String name, List<Student> students) {
