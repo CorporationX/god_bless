6414828e6e479aed1f40b699e8b2f10e0368cd1f
andre <andrey.kuimov2015@yandex.ru>
2025-04-30 15:59:41 +0300

    kenig8649: перенес Счетчик просмотров на YouTube

--- src/main/java/school/faang/sprint3multythreading/youtubecounter/Main.java
+++ src/main/java/school/faang/sprint3multythreading/youtubecounter/Main.java
@@ -1,4 +1,4 @@
-package school.faang.youtubecounter;
+package school.faang.sprint3multythreading.youtubecounter;
 
 import lombok.extern.slf4j.Slf4j;
 
--- src/main/java/school/faang/sprint3multythreading/youtubecounter/VideoManager.java
+++ src/main/java/school/faang/sprint3multythreading/youtubecounter/VideoManager.java
@@ -1,4 +1,4 @@
-package school.faang.youtubecounter;
+package school.faang.sprint3multythreading.youtubecounter;
 
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
