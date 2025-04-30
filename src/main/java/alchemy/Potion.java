5366ae9e1ca2f06379b7e4e027e3b97b3e26b08e
Dan <danbil.942@gmail.com>
2025-05-01 01:42:41 +0300

    Danbg12 bjs2 75061 (#20133)
    * BSJ2-74859 task.
    
    * BSJ2-75061 task.

---
+++ src/main/java/alchemy/Potion.java
@@ -0,0 +1,14 @@
+package alchemy;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@Getter
+@AllArgsConstructor
+public class Potion {
+    private String name;
+    private int requiredIngredients;
+
+}
