223ae38d17d8ae2eed489beda02ed5bdfc152286
andre <andrey.kuimov2015@yandex.ru>
2025-04-30 15:00:43 +0300

    kenig8649: BJS2-73708 - fix ПР Счетчик просмотров на YouTube

--- src/main/java/school/faang/youtubecounter/VideoManager.java
+++ src/main/java/school/faang/youtubecounter/VideoManager.java
@@ -2,15 +2,23 @@ package school.faang.youtubecounter;
 
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.atomic.AtomicInteger;
 
 public class VideoManager {
-    private final Map<String, Integer> viewsMap = new ConcurrentHashMap<>();
+    private final Map<String, AtomicInteger> viewsMap = new ConcurrentHashMap<>();
 
     public void addView(String videoId) {
-        viewsMap.compute(videoId, (k, v) -> (v == null) ? 1 : v + 1);
+        viewsMap.compute(videoId, (k, v) -> {
+            if (v == null) {
+                return new AtomicInteger(1);
+            }
+            v.incrementAndGet();
+            return v;
+        });
     }
 
     public int getViewCount(String videoId) {
-        return viewsMap.getOrDefault(videoId, 0);
+        AtomicInteger counter = viewsMap.get(videoId);
+        return counter != null ? counter.get() : 0;
     }
 }
