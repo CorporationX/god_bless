2ab53f724cb3ff6dc54558fb87dbc84136a4af2c
Vladimir Ekgardt <dj.estet@yandex.ru>
2025-04-30 11:03:45 +0300

    Все числа выделил в константы. В catch добавил флаг прерывания потока

--- src/main/java/school/faang/asynchtony_and_future/MasterCardService.java
+++ src/main/java/school/faang/asynchtony_and_future/MasterCardService.java
@@ -2,16 +2,27 @@ package school.faang.asynchtony_and_future;
 
 import lombok.extern.slf4j.Slf4j;
 
-import java.util.concurrent.*;
+import java.util.Random;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+
 
 @Slf4j
 public class MasterCardService {
+    private static final int THREAD_SLEEP_PAYMENT = 10000;
+    private static final int THREAD_SLEEP_ANALYTICS = 1000;
+    private static final Random RANDOM = new Random();
+
     public int collectPayment() {
         log.info("Произвожу оплату");
         try {
-            Thread.sleep(10000);
-            return 15;
+            Thread.sleep(THREAD_SLEEP_PAYMENT);
+            return RANDOM.nextInt();
         } catch (InterruptedException e) {
+            Thread.currentThread().interrupt();
             throw new RuntimeException("Error");
         } finally {
             log.info("Оплата прошла успешно");
@@ -21,9 +32,10 @@ public class MasterCardService {
     public int sendAnalytics() {
         log.info("Отправляю данные");
         try {
-            Thread.sleep(1000);
-            return 17;
+            Thread.sleep(THREAD_SLEEP_ANALYTICS);
+            return RANDOM.nextInt();
         } catch (InterruptedException e) {
+            Thread.currentThread().interrupt();
             throw new RuntimeException("Error");
         } finally {
             log.info("Данные отправлены успешно");
@@ -32,16 +44,20 @@ public class MasterCardService {
     }
 
     public void doAll() throws ExecutionException, InterruptedException {
-        ExecutorService service = Executors.newFixedThreadPool(2);
+        final int threadPool = 2;
+        ExecutorService service = Executors.newFixedThreadPool(threadPool);
         Future<Integer> future = service.submit(this::collectPayment);
-        CompletableFuture<Integer> completableFuture = CompletableFuture.supplyAsync(this::sendAnalytics);
+        CompletableFuture<Integer> completableFuture = CompletableFuture.supplyAsync(this::sendAnalytics, service);
 
+        int resultFuture = 0;
         while (!future.isDone() && !completableFuture.isDone()) {
+            completableFuture.join();
+            resultFuture = future.get();
             log.info("Ждем ответа");
-            Thread.sleep(1000);
+            Thread.sleep(THREAD_SLEEP_ANALYTICS);
         }
 
-        log.info("{} and {}", future.get(), completableFuture.get());
+        log.info("{} and {}", resultFuture, completableFuture.get());
         service.shutdown();
     }
 }
