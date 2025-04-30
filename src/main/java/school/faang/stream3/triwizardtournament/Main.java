25d7ae8dd6a1758abad8341b46b3baef5169dce7
Tanya <hummingbird_t@mail.ru>
2025-04-30 11:27:02 +0300

    initial commit

---
+++ src/main/java/school/faang/stream3/triwizardtournament/Main.java
@@ -0,0 +1,11 @@
+package school.faang.stream3.triwizardtournament;
+
+import java.util.concurrent.ThreadLocalRandom;
+
+public class Main {
+    public static void main(String[] args) {
+         ThreadLocalRandom.current().nextInt();
+         int d =6;
+         d += d;
+    }
+}
