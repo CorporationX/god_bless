4521b31c6b176119f4190c31f6aad38ff0f90cd2
Likhtin_Nick <kolyangrom2000500@gmail.com>
2025-04-30 14:26:00 +0700

    BJS2-75193
    Первое решение для проверки

---
+++ src/main/java/school/faang/async_and_future/MasterCardService.java
@@ -0,0 +1,47 @@
+package school.faang.async_and_future;
+
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+public class MasterCardService {
+
+    private static final int TEN_SECONDS_IN_MS = 10_000;
+    private static final int ONE_SECOND_IN_MS = 1_000;
+
+    static int collectPayment() {
+        try {
+            Thread.sleep(TEN_SECONDS_IN_MS);
+            return 5_000;
+        } catch (InterruptedException e) {
+            Thread.currentThread().interrupt();
+            throw new RuntimeException(e);
+        }
+    }
+
+    static int sendAnalytics() {
+        try {
+            Thread.sleep(ONE_SECOND_IN_MS);
+            return 17_000;
+        } catch (InterruptedException e) {
+            Thread.currentThread().interrupt();
+            throw new RuntimeException(e);
+        }
+    }
+
+    public void doAll() throws InterruptedException, ExecutionException {
+        ExecutorService executor = Executors.newFixedThreadPool(2);
+        try {
+            CompletableFuture<Integer> paymentFuture = CompletableFuture.supplyAsync(
+                    MasterCardService::collectPayment, executor);
+            CompletableFuture<Integer> analiticFuture = CompletableFuture.supplyAsync(
+                    MasterCardService::sendAnalytics, executor);
+            CompletableFuture.allOf(paymentFuture, analiticFuture).join();
+            System.out.println("Аналитика отправлена: " + analiticFuture.get());
+            System.out.println("Платеж выполнен: " + paymentFuture.get());
+        } finally {
+            executor.shutdown();
+        }
+    }
+}
