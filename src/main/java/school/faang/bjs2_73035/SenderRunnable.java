f4677351c4f645f5fd9b902e8c47c56b058a56b8
Lusivar <jad.fale@mail.ru>
2025-05-01 22:37:42 +0300

    BJS2-73035 refactoring

--- src/main/java/school/faang/bjs2_73035/SenderRunnable.java
+++ src/main/java/school/faang/bjs2_73035/SenderRunnable.java
@@ -5,7 +5,7 @@ import lombok.Setter;
 
 @AllArgsConstructor
 @Setter
-public class SenderRunnable implements Runnable{
+public class SenderRunnable implements Runnable {
     private int startIndex;
     private int endIndex;
 
