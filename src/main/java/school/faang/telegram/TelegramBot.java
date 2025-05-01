f98e5c20ce709899c85ba0a9e3517d5bd309de68
Kirill Frolov <frolov.kirill.17@yandex.ru>
2025-05-01 18:12:57 +0300

    PR issues resolved
    1) Counter changed to atomic integer
    2) Message collection removed

--- src/main/java/school/faang/telegram/TelegramBot.java
+++ src/main/java/school/faang/telegram/TelegramBot.java
@@ -2,109 +2,50 @@ package school.faang.telegram;
 
 import lombok.extern.slf4j.Slf4j;
 
-import java.util.ArrayDeque;
-import java.util.Deque;
+import java.util.concurrent.atomic.AtomicInteger;
 
 import static school.WaitUtils.sleep;
-import static school.WaitUtils.threadWait;
 
 @Slf4j
 public class TelegramBot {
 
-    private static final int REQUEST_LIMIT = 10;
+    private static final int REQUEST_LIMIT = 5;
     private static final int REQUEST_TIME_LIMIT_MILLIS = 1000;
-    private static final int MESSAGE_SEND_PROCESS_MILLIS = 90;
+    private static final int MESSAGE_SEND_PROCESS_MILLIS = 50;
 
-    private int requestCounter;
+    private AtomicInteger requestCounter;
     private long lastRequestTimeMillis;
-    private final Deque<Message> messages;
-    private boolean botActive;
 
     public TelegramBot() {
-        this.requestCounter = 0;
+        this.requestCounter = new AtomicInteger(0);
         this.lastRequestTimeMillis = System.currentTimeMillis();
-        this.messages = new ArrayDeque<>();
-        this.botActive = false;
     }
 
-    public void activateBot() {
-        log.info("Activate bot");
-        botActive = true;
-        while (botActive) {
-            log.info("Start sending messages");
-            proceedMessages();
-        }
-    }
-
-    public void deactivateBot() {
-        log.info("Deactivate bot");
-        synchronized (messages) {
-            if (!messages.isEmpty()) {
-                log.info("Wait all messages is send");
-                threadWait(messages);
-            }
-            log.info("Turn off bot");
-            botActive = false;
-            log.info("Notify message sender");
-            messages.notify();
-        }
-    }
-
-    public void sendMessage(Message message) {
-        synchronized (messages) {
-            messages.add(message);
-            messages.notify();
-        }
-    }
-
-    private void proceedMessages() {
-        emptyMessagePollHandle();
-
-        if (!botActive) {
-            log.info("Deactivating bot");
-            return;
-        }
-
-        increaseMessageSendPerLimitCounter();
+    public synchronized void sendMessage(Message message) {
+        increaseRequestCounter();
         overflowHandle();
         resetMessageSendPerLimitCounter();
+        sendMessageProcess(message);
+    }
 
-        sendMessageProcess();
+    private void increaseRequestCounter() {
+        if (timeFromLastRequest() < REQUEST_TIME_LIMIT_MILLIS) {
+            requestCounter.incrementAndGet();
+        }
     }
 
     private long timeFromLastRequest() {
         return System.currentTimeMillis() - lastRequestTimeMillis;
     }
 
-    private void emptyMessagePollHandle() {
-        if (messages.isEmpty()) {
-            synchronized (messages) {
-                log.info("Notify deactivating thread");
-                messages.notify();
-                log.info("Waiting new messages");
-                threadWait(messages);
-            }
-        }
-    }
-
-    private void sendMessageProcess() {
-        Message messageToSend;
-        synchronized (messages) {
-            messageToSend = messages.pop();
-        }
-        log.info("Sending: {}", messageToSend.content());
+    private void sendMessageProcess(Message message) {
+        log.info("Sending: {}", message.content());
         sleep(MESSAGE_SEND_PROCESS_MILLIS);
-        log.info("Message is send: {}", messageToSend.content());
-    }
-
-    private void increaseMessageSendPerLimitCounter() {
-        if (timeFromLastRequest() < REQUEST_TIME_LIMIT_MILLIS) {
-            requestCounter++;
-        }
+        log.info("Message is send: {}", message.content());
     }
 
     private void overflowHandle() {
-        if (requestCounter > REQUEST_LIMIT) {
+        if (requestCounter.get() > REQUEST_LIMIT) {
             long restTime = REQUEST_TIME_LIMIT_MILLIS - timeFromLastRequest();
             log.info("overflow, rest for {} millis", restTime);
             sleep(restTime < 0 ? 0 : restTime);
@@ -114,7 +55,7 @@ public class TelegramBot {
 
     private void resetMessageSendPerLimitCounter() {
         if (timeFromLastRequest() > REQUEST_TIME_LIMIT_MILLIS) {
-            requestCounter = 0;
+            requestCounter = new AtomicInteger(0);
             lastRequestTimeMillis = System.currentTimeMillis();
         }
     }
