1c2e7e4f8ea68dda5d00487f71ea276a5b8ce71d
AdminS <neptunchik1997@gmail.com>
2025-05-01 16:38:21 +0300

    fix: Google Photo sync

--- src/main/java/school/faang/googlephotosync/GooglePhotosAutoUploader.java
+++ src/main/java/school/faang/googlephotosync/GooglePhotosAutoUploader.java
@@ -1,35 +1,41 @@
 package school.faang.googlephotosync;
 
+import lombok.extern.slf4j.Slf4j;
+
 import java.util.ArrayList;
 import java.util.List;
 
+@Slf4j
 public class GooglePhotosAutoUploader {
     private final Object lock = new Object();
     private final List<String> photosToUpload = new ArrayList<>();
 
     public void startAutoUpload() {
-        synchronized (lock) {
-            try {
-                while (photosToUpload.isEmpty()) {
-                    lock.wait();
-                    System.out.println("waiting new photo...");
+        while (!Thread.currentThread().isInterrupted()) {
+            synchronized (lock) {
+                try {
+                    while (photosToUpload.isEmpty()) {
+                        System.out.println("waiting new photo...");
+                        lock.wait();
+                    }
+                    System.out.println("Uploading photo:");
+                    uploadPhotos();
+                } catch (InterruptedException e) {
+                    log.error("Поток загрузки был прерван");
+                    Thread.currentThread().interrupt();
+                } finally {
+                    log.info("Поток загрузки завершает работу");
                 }
-                System.out.println("Photo uploading:");
-                uploadPhotos();
-            } catch (InterruptedException e) {
-                throw new RuntimeException(e);
             }
         }
     }
 
     public void uploadPhotos() {
-        synchronized (lock) {
-            for (String photo : photosToUpload) {
-                System.out.println("Uploading new photo: - " + photo);
-            }
-            photosToUpload.clear();
-            System.out.println("Photos are uploaded!!!");
+        for (String photo : photosToUpload) {
+            System.out.println("Uploading new photo: - " + photo);
         }
+        photosToUpload.clear();
+        System.out.println("Photos are uploaded!!!");
     }
 
     public void onNewPhotoAdded(String photoPath) {
