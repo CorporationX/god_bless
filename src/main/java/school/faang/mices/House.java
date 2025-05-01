7ae35fa245eb6f5bd1eb427c9f3ad280110a1e68
Kirill Frolov <frolov.kirill.17@yandex.ru>
2025-05-01 19:48:40 +0300

    PR issues resolved
    1) Concurrent collections added
    2) Room peek method refactored
    3) Thread creation refactored

--- src/main/java/school/faang/mices/House.java
+++ src/main/java/school/faang/mices/House.java
@@ -3,61 +3,33 @@ package school.faang.mices;
 import lombok.Getter;
 import lombok.extern.slf4j.Slf4j;
 
-import java.security.SecureRandom;
-import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
+import java.util.concurrent.ConcurrentLinkedQueue;
+import java.util.concurrent.CopyOnWriteArrayList;
 
 @Slf4j
+@Getter
 public class House {
-    @Getter
-    private final List<Room> initialRoomsList;
-    @Getter
-    private final List<Food> collectedFood;
-    private List<Room> rooms;
+
+    private final ConcurrentLinkedQueue<Food> collectedFood;
+    private final CopyOnWriteArrayList<Room> rooms;
 
     public House(List<Room> rooms) {
-        this.initialRoomsList = rooms;
-        this.rooms = initialRoomsList;
-        this.collectedFood = new ArrayList<>();
+        Collections.shuffle(rooms);
+        this.rooms = new CopyOnWriteArrayList<>(rooms);
+        this.collectedFood = new ConcurrentLinkedQueue<>();
     }
 
-    public void collectFood() {
-        List<Room> peekedRoom;
-
-        synchronized (initialRoomsList) {
-            if (isAllRoomsEmpty()) {
-                return;
-            }
-            peekedRoom = randomRoomIndexes().stream()
-                    .map(rooms::get)
-                    .toList();
-        }
-
-        List<Food> pickedFood = new ArrayList<>();
-        peekedRoom.stream()
+    public void collectFood(int startRoomIndex, int endRoomIndex) {
+        rooms
+                .subList(startRoomIndex, endRoomIndex)
+                .stream()
                 .filter(Room::isFoodInTheRoom)
-                .forEach(room -> pickedFood.add(room.peekFood()));
-
-        synchronized (collectedFood) {
-            collectedFood.addAll(pickedFood);
-        }
-
-        synchronized (initialRoomsList) {
-            this.rooms = rooms.stream().filter(Room::isFoodInTheRoom).toList();
-            log.info("{}", rooms);
-        }
+                .forEach(room -> collectedFood.add(room.peekFood()));
     }
 
-    private List<Integer> randomRoomIndexes() {
-        SecureRandom randomized = new SecureRandom();
-        int firstRoomIndex = randomized.nextInt(0, rooms.size());
-        int secondRoomIndex = randomized.nextInt(0, rooms.size());
-        return firstRoomIndex == secondRoomIndex ? List.of(firstRoomIndex) : List.of(firstRoomIndex, secondRoomIndex);
-    }
-
-    public boolean isAllRoomsEmpty() {
-        synchronized (initialRoomsList) {
-            return rooms.stream().noneMatch(Room::isFoodInTheRoom);
-        }
+    public synchronized boolean isAllRoomsEmpty() {
+        return rooms.stream().noneMatch(Room::isFoodInTheRoom);
     }
 }
