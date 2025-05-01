68759897b0432942f3506fd2df1424f5cef2c664
andre <andrey.kuimov2015@yandex.ru>
2025-05-02 01:48:19 +0300

    kenig8649: BJS2-75053 - Качаем алхимию

---
+++ src/main/java/school/faang/sprint4_future_async/alchemy/Potion.java
@@ -0,0 +1,11 @@
+package school.faang.sprint4_future_async.alchemy;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class Potion {
+    private String name;
+    private int requiredIngredients;
+}
