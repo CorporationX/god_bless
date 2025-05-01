b741add3afc9a90a42eeeb933b2e0dc124d20456
Aleksandr <alexandr.sidorkin@gmail.com>
2025-05-01 09:00:21 +0200

    BJS2-74947 after PR 2

--- src/main/java/school/faang/stream4/triwizardtournament/Tournament.java
+++ src/main/java/school/faang/stream4/triwizardtournament/Tournament.java
@@ -5,18 +5,20 @@ import lombok.extern.slf4j.Slf4j;
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
 
 @Slf4j
 public class Tournament {
-    private final int threadCount = 2;
+    private static final int THREAD_COUNT = 2;
+    private static final long FACTOR = 1000L;
     private final ExecutorService service
-         = Executors.newFixedThreadPool(threadCount);
+            = Executors.newFixedThreadPool(THREAD_COUNT);
 
     public CompletableFuture<School> startTask(School school, Task task) {
 
         return CompletableFuture.supplyAsync(() -> {
             log.info("{} started to work on {} task", school.getName(), task.name());
-            makeDelayOnTaskPerform(task);
+            makeDelayOnTaskPerform(task.difficulty() * FACTOR);
             school.updateScore(task.reward());
             log.info("{} finished with task {} and earn {} points!\n The total score of {} is now {} points!",
                     school.getName(), task.name(), task.reward(), school.getName(), school.getTotalPoints());
@@ -26,10 +28,9 @@ public class Tournament {
         service);
     }
 
-    private void makeDelayOnTaskPerform(Task task) {
-        long millis = 1000L * task.difficulty();
+    private void makeDelayOnTaskPerform(long delay) {
         try {
-            Thread.sleep(millis);
+            Thread.sleep(delay);
         } catch (InterruptedException e) {
             log.error("interrupted exception during task delay: {}", e.getMessage());
             Thread.currentThread().interrupt();
@@ -38,6 +39,19 @@ public class Tournament {
 
     public void endTournament() {
         log.info("Tournament is over!");
-        this.service.shutdown();
+        service.shutdown();
+            {
+            log.info("Gathering is over!");
+            int timeout = 30;
+            service.shutdown();
+            try {
+                if (!service.awaitTermination(timeout, TimeUnit.SECONDS)) {
+                    service.shutdownNow();
+                }
+            } catch (InterruptedException e) {
+                service.shutdownNow();
+                Thread.currentThread().interrupt();
+            }
+            }
     }
 }
