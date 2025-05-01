d8c83d2c2ce2c0dcf34659e978ab86a56ad3ad4d
andre <andrey.kuimov2015@yandex.ru>
2025-05-01 16:41:20 +0300

    kenig8649: BJS2-74560 fix ПР - WOW

--- src/main/java/school/faang/sprint4_future_async/wow/Main.java
+++ src/main/java/school/faang/sprint4_future_async/wow/Main.java
@@ -1,13 +1,14 @@
 package school.faang.sprint4_future_async.wow;
 
 import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.atomic.AtomicInteger;
 
 public class Main {
     public static void main(String[] args) {
         QuestSystem questSystem = new QuestSystem();
 
-        Player player1 = new Player("Thrall", 10, 250);
-        Player player2 = new Player("Sylvanas", 12, 450);
+        Player player1 = new Player("Thrall", 10, new AtomicInteger(250));
+        Player player2 = new Player("Sylvanas", 12, new AtomicInteger(450));
 
         Quest quest1 = new Quest("Defeat the Lich King", 10, 150);
         Quest quest2 = new Quest("Retrieve the Sword of Azeroth", 8, 100);
@@ -19,7 +20,7 @@ public class Main {
                 })
                 .thenAccept(player ->
                         System.out.printf("%s завершил квест. Опыт: %d%n",
-                                player.getName(), player.getExperience()));
+                                player.getName(), player.getExperience().get()));
 
         CompletableFuture<Void> player2Quest = questSystem.startQuest(player2, quest2)
                 .exceptionally(ex -> {
@@ -27,10 +28,12 @@ public class Main {
                     return player2;
                 }).thenAccept(player ->
                         System.out.printf("%s завершил квест. Опыт: %d%n",
-                                player.getName(), player.getExperience()));
+                                player.getName(), player.getExperience().get()));
 
         CompletableFuture.allOf(player1Quest, player2Quest)
                 .thenRun(() -> System.out.println("Все квесты завершены!\n"))
                 .join();
+
+        questSystem.executorShutdown();
     }
 }
--- src/main/java/school/faang/sprint4_future_async/wow/Player.java
+++ src/main/java/school/faang/sprint4_future_async/wow/Player.java
@@ -3,14 +3,16 @@ package school.faang.sprint4_future_async.wow;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
+import java.util.concurrent.atomic.AtomicInteger;
+
 @Getter
 @AllArgsConstructor
 public class Player {
     private String name;
     private int level;
-    private int experience;
+    private AtomicInteger experience;
 
     public void addExperience(int experience) {
-        this.experience += experience;
+        this.experience.addAndGet(experience);
     }
 }
