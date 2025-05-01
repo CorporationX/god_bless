faf9550d7a37a92421f43947c2db295c0780b8de
Anatolii Izotov <izotov.anatoly.e@gmail.com>
2025-05-01 12:01:07 +0200

    fix: thread safe

--- src/main/java/school/faang/triwizardtournament/Student.java
+++ src/main/java/school/faang/triwizardtournament/Student.java
@@ -1,16 +1,30 @@
 package school.faang.triwizardtournament;
 
-import lombok.AllArgsConstructor;
+import lombok.AccessLevel;
 import lombok.Getter;
 
-@AllArgsConstructor
+import java.util.concurrent.atomic.AtomicInteger;
+
 @Getter
 public class Student {
     private final String name;
-    private int year;
-    private int points;
+    private final int year;
 
-    public void gainPoints(int reward) {
-        points += reward;
+    @Getter(AccessLevel.NONE)
+    private final AtomicInteger points;
+
+    public Student(String name, int year, int points) {
+        this.name = name;
+        this.year = year;
+        this.points = new AtomicInteger(points);
+    }
+
+
+    void gainPoints(int reward) {
+        points.addAndGet(reward);
+    }
+
+    public int getPoints() {
+        return points.get();
     }
 }
