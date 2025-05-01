d0672604eb6359831dd90e1894a927295450a70c
AdminS <neptunchik1997@gmail.com>
2025-05-01 16:45:17 +0300

    fix: Supercow!

--- src/main/java/school/faang/supercow/Player.java
+++ src/main/java/school/faang/supercow/Player.java
@@ -6,13 +6,14 @@ import lombok.Getter;
 @Getter
 @AllArgsConstructor
 public class Player {
+    private static final int SLEEP_TIME = 2000;
     private String name;
 
     public void doBattle(Boss boss) {
         try {
             boss.joinBattle(this);
             System.out.printf("\n%s начинает сражение с боссом...", name);
-            Thread.sleep(2000);
+            Thread.sleep(SLEEP_TIME);
             boss.leaveBattle(this);
         } catch (InterruptedException e) {
             Thread.currentThread().interrupt();
