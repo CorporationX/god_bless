ca0dbca8d8a37bc1f1175ee3c17dc2a543dacf8e
Tanya <hummingbird_t@mail.ru>
2025-05-01 19:12:25 +0300

    провела задачи в созданном пуле потоков, сделала синхронизацию по школе

--- src/main/java/school/faang/stream4/triwizardtournament/Main.java
+++ src/main/java/school/faang/stream4/triwizardtournament/Main.java
@@ -1,7 +1,13 @@
 package school.faang.stream4.triwizardtournament;
 
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
 
 public class Main {
     public static void main(String[] args) {
@@ -15,18 +21,45 @@ public class Main {
         Task task1 = new Task("Triwizard Tournament", 10, 100);
         Task task2 = new Task("Yule Ball Preparations", 5, 50);
 
-        CompletableFuture<School> hogwartsTask = tournament.startTask(hogwarts, task1);
-        CompletableFuture<School> beauxbatonsTask = tournament.startTask(beauxbatons, task2);
+        ExecutorService executor = Executors.newFixedThreadPool(2);
+        List<CompletableFuture<School>> futures = new ArrayList<>();
 
-        CompletableFuture<Void> allTasks = CompletableFuture.allOf(hogwartsTask, beauxbatonsTask)
+        futures.add(tournament.startTask(hogwarts, task1, executor));
+        futures.add(tournament.startTask(beauxbatons, task2, executor));
+        futures.add(tournament.startTask(beauxbatons, task1, executor));
+        futures.add(tournament.startTask(hogwarts, task2, executor));
+        futures.add(tournament.startTask(hogwarts, task1, executor));
+
+        Map<String, Long> schoolPoints = new HashMap<>();
+
+        CompletableFuture<Void> allTasks = CompletableFuture.allOf(futures.toArray(new CompletableFuture[0]))
                 .thenRun(() -> {
-                    long hogwartsTotalPoints = hogwartsTask.join().getTotalPoints();
-                    long beauxbatonsTotalPoints1 = beauxbatonsTask.join().getTotalPoints();
-                    String winner = hogwartsTotalPoints > beauxbatonsTotalPoints1
-                            ? hogwarts.getName() : beauxbatons.getName();
-                    System.out.printf("Школа - победитель: %s\n", winner);
+                    schoolPoints.put(hogwarts.getName(), hogwarts.getTotalPoints());
+                    schoolPoints.put(beauxbatons.getName(), beauxbatons.getTotalPoints());
+
+                    long maxPoints = schoolPoints
+                            .values().stream()
+                            .max(Long::compare).orElse(0L);
+                    String winnerName = schoolPoints.entrySet()
+                            .stream()
+                            .filter(entry -> entry.getValue() == maxPoints)
+                            .map(Map.Entry::getKey)
+                            .findFirst().orElse("");
+
+                    System.out.printf("Школа - победитель: %s набрала %d очков\n", winnerName, maxPoints);
                 });
 
         allTasks.join();
+        executor.shutdown();
+
+        try {
+            if (!executor.awaitTermination(1000, TimeUnit.MILLISECONDS)) {
+                executor.shutdownNow();
+            }
+        } catch (
+                InterruptedException e) {
+            System.out.println(e + " - завершение пула потоков прервано");
+            executor.shutdownNow();
+        }
     }
 }
--- src/main/java/school/faang/stream4/triwizardtournament/Tournament.java
+++ src/main/java/school/faang/stream4/triwizardtournament/Tournament.java
@@ -3,22 +3,26 @@ package school.faang.stream4.triwizardtournament;
 import lombok.extern.slf4j.Slf4j;
 
 import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.TimeUnit;
 
 @Slf4j
 public class Tournament {
-    private static final int COEFFICIENT = 300;
+    private static final int COEFFICIENT = 3;
 
-    public CompletableFuture<School> startTask(School school, Task task) {
+    public CompletableFuture<School> startTask(School school, Task task, ExecutorService executor) {
         return CompletableFuture.supplyAsync(() -> {
             try {
-                Thread.sleep(task.getDifficulty() * COEFFICIENT);
+                TimeUnit.SECONDS.sleep((long) task.getDifficulty() * COEFFICIENT);
             } catch (InterruptedException e) {
                 log.info(e.getMessage());
             }
-            long pointsPerStudent = task.getReward() / school.getTeam().size();
-            school.getTeam()
-                    .forEach(student -> student.setPoints(student.getPoints() + pointsPerStudent));
-            return school;
-        });
+            synchronized (school) {
+                long pointsPerStudent = task.getReward() / school.getTeam().size();
+                school.getTeam()
+                        .forEach(student -> student.setPoints(student.getPoints() + pointsPerStudent));
+                return school;
+            }
+        }, executor);
     }
 }
