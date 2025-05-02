88ca0af7e587a0801c5a8715a31c832130aedf4b
Slava Vermilion <beg.slava@bk.ru>
2025-05-02 12:43:38 +0700

    Attempt #2

--- src/main/java/bjs2_74658/Player.java
+++ src/main/java/bjs2_74658/Player.java
@@ -1,16 +1,26 @@
 package bjs2_74658;
 
-import lombok.AllArgsConstructor;
 import lombok.Getter;
 
-@AllArgsConstructor
+import java.util.concurrent.atomic.AtomicInteger;
+
 @Getter
 public class Player {
     private String name;
     private int level;
-    private int experience;
+    private AtomicInteger experience;
+
+    public Player(String name, int level, int experience) {
+        this.name = name;
+        this.level = level;
+        this.experience = new AtomicInteger(experience);
+    }
+
+    public int getExperience() {
+        return experience.get();
+    }
 
     public void addExperience(int experience) {
-        this.experience += experience;
+        this.experience.addAndGet(experience);
     }
 }
--- src/main/java/bjs2_74658/QuestSystem.java
+++ src/main/java/bjs2_74658/QuestSystem.java
@@ -3,13 +3,14 @@ package bjs2_74658;
 import lombok.extern.slf4j.Slf4j;
 
 import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.TimeUnit;
 
 @Slf4j
 public class QuestSystem {
     public CompletableFuture<Player> startQuest(Player player, Quest quest) {
         CompletableFuture<Player> playerFuture = CompletableFuture.supplyAsync(() -> {
             try {
-                Thread.sleep(quest.getDifficulty() * 1000);
+                TimeUnit.SECONDS.sleep(quest.getDifficulty());
             } catch (InterruptedException e) {
                 throw new RuntimeException(e);
             }
