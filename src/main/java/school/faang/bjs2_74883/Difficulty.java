93678f3bd466f05d6bc2163156a9b1502507c308
Nikita Nikitin <nnandpartners@gmail.com>
2025-05-02 18:39:13 +0300

    BJS2-74883 fix enum structure, added constants and synch block for opration to ba atomic

--- src/main/java/school/faang/bjs2_74883/Difficulty.java
+++ src/main/java/school/faang/bjs2_74883/Difficulty.java
@@ -1,7 +1,17 @@
 package school.faang.bjs2_74883;
 
+import lombok.Getter;
+
+@Getter
 public enum Difficulty {
-    EASY,
-    MEDIUM,
-    HARD
+    EASY(1000),
+    MEDIUM(2000),
+    HARD(3000);
+
+    private final int sleepTime;
+
+    Difficulty(int sleepTime) {
+        this.sleepTime = sleepTime;
+    }
+
 }
--- src/main/java/school/faang/bjs2_74883/Tournament.java
+++ src/main/java/school/faang/bjs2_74883/Tournament.java
@@ -15,37 +15,45 @@ public class Tournament {
     private static final int THREAD_SLEEP_EASY = 1000;
     private static final int THREAD_SLEEP_MED = 3000;
     private static final int THREAD_SLEEP_HARD = 4000;
+    private static final int DEFAULT_REWARD = 0;
     private List<Task> tasks;
     private final ExecutorService executor = Executors.newFixedThreadPool(THREADS);
 
     public CompletableFuture<School> startTask(School school, Task task) {
         return CompletableFuture.supplyAsync(() -> {
-            switch (task.getDifficulty()) {
-                case EASY -> {
-                    System.out.printf("%s started doing an EASY task\n", school.getName());
-                    changeStudentScore(school.getTeam(), 1, THREAD_SLEEP_EASY);
+            synchronized (this) {
+                switch (task.getDifficulty()) {
+                    case EASY -> {
+                        System.out.printf("%s started doing an EASY task\n", school.getName());
+                        school.getTeam().forEach(student -> {
+                            changeStudentScore(school.getTeam(), task.getReward(), Difficulty.EASY);
+                        });
+                    }
+                    case MEDIUM -> {
+                        System.out.printf("%s started doing an MEDIUM task\n", school.getName());
+                        school.getTeam().forEach(student -> {
+                            changeStudentScore(school.getTeam(), task.getReward(), Difficulty.MEDIUM);
+                        });
+                    }
+                    case HARD -> {
+                        System.out.printf("%s started doing an HARD task\n", school.getName());
+                        school.getTeam().forEach(student -> {
+                            changeStudentScore(school.getTeam(), task.getReward(), Difficulty.HARD);
+                        });
+                    }
+                    default -> school.getTeam().forEach(student -> student.setPoints(DEFAULT_REWARD));
                 }
-                case MEDIUM -> school.getTeam().forEach(student -> {
-                    System.out.printf("%s started doing an MEDIUM task\n", school.getName());
-                    changeStudentScore(school.getTeam(), 2, THREAD_SLEEP_MED);
-                });
-                case HARD -> school.getTeam().forEach(student -> {
-                    System.out.printf("%s started doing an HARD task\n", school.getName());
-                    changeStudentScore(school.getTeam(), 3, THREAD_SLEEP_HARD);
-                });
-                default -> school.getTeam().forEach(student -> student.setPoints(0));
+                return school;
             }
-            return school;
         }, executor);
     }
 
-    private void changeStudentScore(List<Student> students, int points, int time) {
+    private void changeStudentScore(List<Student> students, int points, Difficulty difficulty) {
         try {
-            Thread.sleep(time);
+            Thread.sleep(difficulty.getSleepTime());
             students.forEach(student -> student.setPoints(student.getPoints() + points));
         } catch (InterruptedException e) {
             throw new IllegalStateException("Interrupted while waiting for task", e);
         }
-
     }
 }
