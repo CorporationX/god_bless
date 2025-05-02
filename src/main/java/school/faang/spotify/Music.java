75b54b17ddcdf18c5b988dc1cc268af4b511fe12
Ilyas Kystaubayev <kystaubaev.iliyas@mail.ru>
2025-05-02 23:30:35 +0500

    Spotify

--- src/main/java/school/faang/spotify/Music.java
+++ src/main/java/school/faang/spotify/Music.java
@@ -11,28 +11,34 @@ public class Music {
             player.play();
             try {
                 Thread.sleep(100);
-            } catch (InterruptedException ignored) {
+            } catch (InterruptedException e) {
+                log.error("User1 was interrupted", e);
+                Thread.currentThread().interrupt();
             }
             player.pause();
-        }, "First user ");
+        }, "First user");
 
         Thread user2 = new Thread(() -> {
             player.skip();
             try {
                 Thread.sleep(50);
-            } catch (InterruptedException ignored) {
+            } catch (InterruptedException e) {
+                log.error("User2 was interrupted", e);
+                Thread.currentThread().interrupt();
             }
             player.play();
-        }, " Second user ");
+        }, "Second user");
 
         Thread user3 = new Thread(() -> {
             player.previous();
             try {
                 Thread.sleep(70);
-            } catch (InterruptedException ignored) {
+            } catch (InterruptedException e) {
+                log.error("User3 was interrupted", e);
+                Thread.currentThread().interrupt();
             }
             player.pause();
-        }, "Third user ");
+        }, "Third user");
 
         user1.start();
         user2.start();
@@ -42,9 +48,10 @@ public class Music {
             user1.join();
             user2.join();
             user3.join();
-        } catch (InterruptedException ignored) {
+        } catch (InterruptedException e) {
+            log.error("Main thread was interrupted", e);
+            Thread.currentThread().interrupt();
         }
-
         log.info("Group session has ended.");
     }
 }
