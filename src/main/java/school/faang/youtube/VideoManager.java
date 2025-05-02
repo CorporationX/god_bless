2336f97451c7f8635fa9ec6e7492b0e5937a6db8
JolyGolden <nazi00696@mail.ru>
2025-05-02 10:29:36 +0200

    qustion for reviewer

--- src/main/java/school/faang/youtube/VideoManager.java
+++ src/main/java/school/faang/youtube/VideoManager.java
@@ -5,6 +5,8 @@ import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.atomic.AtomicInteger;
 
 public class VideoManager {
+    // если не ошибаюсь, то операция будет не атомарна,
+    // если не использоваьт AtomicInteger и при высокой конкуренции будет состояние гонки.
     private Map<String, AtomicInteger> views = new ConcurrentHashMap<>();
 
     public void addView(String videoId) {
