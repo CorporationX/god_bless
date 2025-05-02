377628eba48bdf4e0e544bad5b96b0bafc378178
Kirill <kirill551@yandex.ru>
2025-05-02 19:56:45 +0200

    "Выполнить задачу Становимся celebrity в Twitter"

--- src/main/java/school/faang/twitter/TwitterAccount.java
+++ src/main/java/school/faang/twitter/TwitterAccount.java
@@ -9,7 +9,7 @@ import java.util.concurrent.atomic.AtomicInteger;
 @Data
 public class TwitterAccount {
     private final String username;
-    private AtomicInteger followers;
+    private final AtomicInteger followers;
 
     public TwitterAccount(String username) {
         this.username = username;
