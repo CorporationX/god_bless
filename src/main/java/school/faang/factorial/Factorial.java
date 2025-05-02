30256a44f3e28438c00cda635848180bf4790526
Vladimir Ekgardt <dj.estet@yandex.ru>
2025-05-02 08:36:22 +0300

    Великий и ужасный факториал! BJS2-75600

--- src/main/java/school/faang/factorial/Factorial.java
+++ src/main/java/school/faang/factorial/Factorial.java
@@ -4,14 +4,13 @@ import java.math.BigInteger;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.CompletableFuture;
-import java.util.concurrent.ExecutionException;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
 public class Factorial {
     private static final int MAX_INT_FACTORIAL = 12;
     private static final int MAX_LONG_FACTORIAL = 19;
-    private static final int FIXED_THREAD = 5;
+    private static final int FIXED_THREAD = 2;
 
     private static int factorialInt(int n) {
         if (n > MAX_INT_FACTORIAL) {
@@ -55,30 +54,14 @@ public class Factorial {
         return result;
     }
 
-    public static void calculate(List<Integer> numbers) {
+    public static List<CompletableFuture<BigInteger>> calculate(List<Integer> numbers) {
         ExecutorService service = Executors.newFixedThreadPool(FIXED_THREAD);
         List<CompletableFuture<BigInteger>> listFuture = new ArrayList<>();
         for (Integer num : numbers) {
             CompletableFuture<BigInteger> future = CompletableFuture.supplyAsync(() -> factorialBig(num), service);
             listFuture.add(future);
         }
-
-        CompletableFuture<Void> allFutures = CompletableFuture.allOf(listFuture.toArray(new CompletableFuture[0]));
-
-        CompletableFuture<List<BigInteger>> resultsFuture = allFutures.thenApply(v -> {
-            List<BigInteger> results = new ArrayList<>();
-            listFuture.forEach(future -> {
-                try {
-                    results.add(future.get());
-                } catch (InterruptedException | ExecutionException e) {
-                    results.add(new BigInteger(String.valueOf(0)));
-                }
-            });
-            return results;
-        });
-
-        resultsFuture.join();
-        resultsFuture.thenAccept(result -> result.forEach(System.out::println));
         service.shutdown();
+        return listFuture;
     }
 }
--- src/main/java/school/faang/factorial/Main.java
+++ src/main/java/school/faang/factorial/Main.java
@@ -1,10 +1,35 @@
 package school.faang.factorial;
 
+import java.math.BigInteger;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Random;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.atomic.AtomicInteger;
 
 public class Main {
     public static void main(String[] args) {
-        List<Integer> numbers = List.of(5, 9, 15, 20, 25, 7);
-        Factorial.calculate(numbers);
+        List<Integer> numbers = new ArrayList<>();
+        Random random = new Random();
+        for (int i = 0; i < 50; i++) {
+            numbers.add(random.nextInt(5000));
+        }
+        List<CompletableFuture<BigInteger>> list = Factorial.calculate(numbers);
+
+        AtomicInteger atom = new AtomicInteger(0);
+        for (int i = 0; i < list.size(); i++) {
+            int index = i;
+            new Thread(() -> {
+                try {
+                    System.out.println(numbers.get(index) + "! = " + list.get(index).get());
+                    atom.incrementAndGet();
+                } catch (InterruptedException | ExecutionException e) {
+                    throw new RuntimeException(e);
+                }
+            }).start();
+        }
+
+        System.out.println(numbers);
     }
 }
