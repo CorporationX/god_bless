c6b076f74185f4057847e8e4db9a9416c7707a71
Nikita Nikitin <nnandpartners@gmail.com>
2025-05-02 19:29:22 +0300

    BJS2-74484 fix doAll logic and move executor shutdown to main

--- src/main/java/school/faang/bjs2_74484/Main.java
+++ src/main/java/school/faang/bjs2_74484/Main.java
@@ -1,8 +1,22 @@
 package school.faang.bjs2_74484;
 
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.TimeUnit;
+
 public class Main {
+    private static final int WAIT_TIME = 10;
+
     public static void main(String[] args) {
         MasterCardService masterCardService = new MasterCardService();
+        ExecutorService executor = masterCardService.getSingleThreadExecutor();
         masterCardService.doAll();
+        executor.shutdown();
+        try {
+            if (!executor.awaitTermination(WAIT_TIME, TimeUnit.SECONDS)) {
+                executor.shutdownNow();
+            }
+        } catch (InterruptedException e) {
+            throw new IllegalStateException("Interrupted");
+        }
     }
 }
--- src/main/java/school/faang/bjs2_74484/MasterCardService.java
+++ src/main/java/school/faang/bjs2_74484/MasterCardService.java
@@ -1,16 +1,18 @@
 package school.faang.bjs2_74484;
 
+import lombok.Data;
+
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.Future;
-import java.util.concurrent.TimeUnit;
 
+@Data
 public class MasterCardService {
     private static final int SlEEP_TIME_FOR_PAYMENTS = 2000;
-    private static final int SlEEP_TIME_FOR_ANALYTICS = 10000;
-    ExecutorService singleThreadExecutor = Executors.newSingleThreadExecutor();
+    private static final int SlEEP_TIME_FOR_ANALYTICS = 3000;
+    private final ExecutorService singleThreadExecutor = Executors.newSingleThreadExecutor();
 
     public int collectPayment() {
         try {
@@ -19,6 +21,7 @@ public class MasterCardService {
         } catch (InterruptedException e) {
             Thread.currentThread().interrupt();
             throw new IllegalStateException("Iterrupted");
+
         }
     }
 
@@ -34,24 +37,15 @@ public class MasterCardService {
 
     public void doAll() {
         Future<Integer> paymentFuture = singleThreadExecutor.submit(this::collectPayment);
+        CompletableFuture<Integer> analyticsFuture = CompletableFuture.supplyAsync(
+                this::sendAnalytics);
         try {
-            Integer result = paymentFuture.get();
-            System.out.printf("Payment result: %d\n", result);
+            Integer paymentResult = paymentFuture.get();
+            Integer analyticsResult = analyticsFuture.join();
+            System.out.printf("Analytics result: %d\n", analyticsResult);
+            System.out.printf("Payment result: %d\n", paymentResult);
         } catch (InterruptedException | ExecutionException e) {
             throw new IllegalStateException("Execution exception or Interrupted");
-        }
-        CompletableFuture<Integer> analyticsFuture = CompletableFuture.supplyAsync(
-                this::sendAnalytics
-        );
-        Integer result = analyticsFuture.join();
-        System.out.printf("Analytics result: %d\n", result);
-        singleThreadExecutor.shutdown();
-        try {
-            if (!singleThreadExecutor.awaitTermination(10, TimeUnit.SECONDS)) {
-                singleThreadExecutor.shutdownNow();
-            }
-        } catch (InterruptedException e) {
-            throw new IllegalStateException("Interrupted");
         }
     }
 }
