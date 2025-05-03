0fe86591ca7f19164a587e572e7311f83e0c7ed1
Emil Dzheenbaev <emil.dzheenbaev@gmail.com>
2025-05-03 23:44:12 +0200

    решение задачи Работа стоит (чекстайл)

--- src/main/java/workiswaiting/DesignDepartment.java
+++ src/main/java/workiswaiting/DesignDepartment.java
@@ -11,25 +11,27 @@ public class DesignDepartment implements Runnable {
         this.designResources = designResources;
     }
 
+    /*
+    @Override
+    public void run() {
+        System.out.println(Thread.currentThread().getName() + " пытается получить доступ к Marketing...");
 
-//    @Override
-//    public void run() {
-//        System.out.println(Thread.currentThread().getName() + " пытается получить доступ к Marketing...");
-//
-//        synchronized (marketingResources) {
-//            System.out.println(Thread.currentThread().getName() + " получил доступ к Marketing.");
-//            List<String> marketingFiles = marketingResources.getFiles();
-//            if (!marketingFiles.isEmpty()) {
-//                String fileToCopy = marketingFiles.get(0) + "_design";
-//                System.out.println(Thread.currentThread().getName() + " пытается получить доступ к Design...");
-//                synchronized (designResources) {
-//                    System.out.println(Thread.currentThread().getName() + " получил доступ к Design.");
-//                    designResources.addFiles(fileToCopy);
-//                }
-//            }
-//        }
-//        System.out.println(Thread.currentThread().getName() + " завершил работу.");
-//    }
+        synchronized (marketingResources) {
+            System.out.println(Thread.currentThread().getName() + " получил доступ к Marketing.");
+            List<String> marketingFiles = marketingResources.getFiles();
+            if (!marketingFiles.isEmpty()) {
+                String fileToCopy = marketingFiles.get(0) + "_design";
+                System.out.println(Thread.currentThread().getName() + " пытается получить доступ к Design...");
+                synchronized (designResources) {
+                    System.out.println(Thread.currentThread().getName() + " получил доступ к Design.");
+                    designResources.addFiles(fileToCopy);
+                }
+            }
+        }
+        System.out.println(Thread.currentThread().getName() + " завершил работу.");
+    }
+
+    */
 
 
     @Override
--- src/main/java/workiswaiting/MarketingDepartment.java
+++ src/main/java/workiswaiting/MarketingDepartment.java
@@ -11,23 +11,26 @@ public class MarketingDepartment implements Runnable {
         this.designResources = designResources;
     }
 
-//    @Override
-//    public void run() {
-//        System.out.println(Thread.currentThread().getName() + " пытается получить доступ к Design...");
-//        synchronized (designResources) {
-//            System.out.println(Thread.currentThread().getName() + " получил доступ к Design.");
-//            List<String> designFiles = designResources.getFiles();
-//            if (!designFiles.isEmpty()) {
-//                String fileToCopy = designFiles.get(0) + "_marketing";
-//                System.out.println(Thread.currentThread().getName() + " пытается получить доступ к Marketing...");
-//                synchronized (marketingResources) {
-//                    System.out.println(Thread.currentThread().getName() + " получил доступ к Marketing.");
-//                    marketingResources.addFiles(fileToCopy);
-//                }
-//            }
-//        }
-//        System.out.println(Thread.currentThread().getName() + " завершил работу.");
-//    }
+    /*
+    @Override
+    public void run() {
+        System.out.println(Thread.currentThread().getName() + " пытается получить доступ к Design...");
+        synchronized (designResources) {
+            System.out.println(Thread.currentThread().getName() + " получил доступ к Design.");
+            List<String> designFiles = designResources.getFiles();
+            if (!designFiles.isEmpty()) {
+                String fileToCopy = designFiles.get(0) + "_marketing";
+                System.out.println(Thread.currentThread().getName() + " пытается получить доступ к Marketing...");
+                synchronized (marketingResources) {
+                    System.out.println(Thread.currentThread().getName() + " получил доступ к Marketing.");
+                    marketingResources.addFiles(fileToCopy);
+                }
+            }
+        }
+        System.out.println(Thread.currentThread().getName() + " завершил работу.");
+    }
+
+     */
 
     @Override
     public void run() {
