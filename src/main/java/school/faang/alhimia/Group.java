1ad9fe4b38208970d1645da155db12776296821d
Fagan Gafulov <gafulov@yandex.ru>
2025-05-03 09:05:58 +0300

    BJS2-75100

---
+++ src/main/java/school/faang/alhimia/Group.java
@@ -0,0 +1,20 @@
+package school.faang.alhimia;
+
+import lombok.extern.slf4j.Slf4j;
+
+import java.util.concurrent.TimeUnit;
+
+@Slf4j
+public class Group {
+
+    public int gatherIngredients(Potion potion) {
+        int countIngredients = potion.getRequiredIngredients();
+        try {
+            TimeUnit.SECONDS.sleep(countIngredients);
+            log.info("имитация сбора средств для зелья: {} ", potion.getName());
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+        return countIngredients;
+    }
+}
---
+++ src/main/java/school/faang/alhimia/Main.java
@@ -0,0 +1,4 @@
+package school.faang.alhimia;
+
+public class Main {
+}
