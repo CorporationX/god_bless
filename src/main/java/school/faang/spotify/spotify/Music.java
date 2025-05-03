adc6cb61802c49e1383608e1a6175361598bf65c
Andrew Sonin <sonin.andrei2009@yandex.ru>
2025-05-03 18:29:26 +0300

    Task BJS2-73296 1) Добавил классы Music , Player 2) Написал логику

--- src/main/java/school/faang/spotify/spotify/Music.java
+++ src/main/java/school/faang/spotify/spotify/Music.java
@@ -3,12 +3,10 @@ package school.faang.spotify.spotify;
 public class Music {
     public static void main(String[] args) {
         Player player = new Player();
-
         Thread playThread = new Thread(player::play);
         Thread pauseThread = new Thread(player::pause);
         Thread skipThread = new Thread(player::skip);
         Thread previousThread = new Thread(player::previous);
-
         playThread.start();
         pauseThread.start();
         skipThread.start();
