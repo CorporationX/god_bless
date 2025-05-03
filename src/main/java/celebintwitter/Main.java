67da6612071a93a1cc20ee00733625564107eabb
Emil Dzheenbaev <emil.dzheenbaev@gmail.com>
2025-05-04 00:02:34 +0200

    поправка задачи celebrity in Twitter!

--- src/main/java/celebintwitter/Main.java
+++ src/main/java/celebintwitter/Main.java
@@ -24,6 +24,6 @@ public class Main {
         allFeaturesCompleted.get();
 
         System.out.println("Итоговое количество подписчиков у @" + celebrityAccount.getUsername() + ": "
-                + celebrityAccount.getFollowers());
+                + celebrityAccount.getFollowersCount());
     }
 }
--- src/main/java/celebintwitter/TwitterAccount.java
+++ src/main/java/celebintwitter/TwitterAccount.java
@@ -4,10 +4,17 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
 
+import java.util.concurrent.atomic.AtomicInteger;
+
 @AllArgsConstructor
 @Getter
 @Setter
 public class TwitterAccount {
     private String username;
-    private int followers;
+    private AtomicInteger followersCount;
+
+    public TwitterAccount(String username, int initialFollowersCount) {
+        this.username = username;
+        this.followersCount = new AtomicInteger(initialFollowersCount);
+    }
 }
