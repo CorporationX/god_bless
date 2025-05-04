3a4fe265a229a8eb3ed49ca52b436a1b526a0705
Likhtin_Nick <kolyangrom2000500@gmail.com>
2025-05-04 21:34:12 +0700

    BJS2-76477
    Правки на "AtomicReference"

--- src/main/java/school/faang/support_animals/Organization.java
+++ src/main/java/school/faang/support_animals/Organization.java
@@ -1,23 +1,19 @@
 package school.faang.support_animals;
 
-import java.util.Queue;
-import java.util.concurrent.ConcurrentLinkedDeque;
+import java.util.concurrent.atomic.AtomicReference;
 
 public class Organization {
-    private final Queue<Donation> balance = new ConcurrentLinkedDeque<>();
+    private final AtomicReference<Double> balance = new AtomicReference<>(0.0);
 
     public void addDonation(Donation donation) {
-        balance.add(donation);
+        balance.updateAndGet(current -> current + donation.amount());
     }
 
     public double sumDonation() {
-        return balance.stream()
-                .mapToDouble(Donation::amount)
-                .sum();
+        return balance.get();
     }
 
     public void printAll() {
-        System.out.println(balance.size());
-        balance.forEach(System.out::println);
+        System.out.println(balance.get());
     }
 }
