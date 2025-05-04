bea58a5d95bf5c64460a0f4e61d680f780caff6b
Raidanmark <Onidanil@Gmail.com>
2025-05-04 12:37:07 +0200

    task created

---
+++ src/main/java/school/faang/sprint_4/async_and_future/MasterCardService.java
@@ -0,0 +1,39 @@
+package school.faang.sprint_4.async_and_future;
+
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+
+public class MasterCardService {
+    private static final int TEN_SECONDS_IN_MS = 10_000;
+    private static final int ONE_SECOND_IN_MS = 1_000;
+
+    ExecutorService executor = Executors.newSingleThreadExecutor();
+
+    private static int collectPayment() {
+        try {
+            Thread.sleep(TEN_SECONDS_IN_MS);
+            return 5_000;
+        } catch (InterruptedException e) {
+            Thread.currentThread().interrupt();
+            throw new RuntimeException(e);
+        }
+    }
+
+    private static int sendAnalytics() {
+        try {
+            Thread.sleep(ONE_SECOND_IN_MS);
+            return 17_000;
+        } catch (InterruptedException e) {
+            Thread.currentThread().interrupt();
+            throw new RuntimeException(e);
+        }
+    }
+
+    public void doAll() {
+        Thread collectPayment = new Thread(MasterCardService::collectPayment);
+        CompletableFuture.runAsync(() -> sendAnalytics());
+        Future<Integer> sendAnalytics = executor.submit(MasterCardService::sendAnalytics);
+    }
+}
