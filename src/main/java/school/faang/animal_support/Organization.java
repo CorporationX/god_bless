7a933e60b818395caf3033c5f85858863ec146cd
Raidanmark <Onidanil@Gmail.com>
2025-05-04 16:11:23 +0200

    Comment fixes

--- src/main/java/school/faang/animal_support/Organization.java
+++ src/main/java/school/faang/animal_support/Organization.java
@@ -3,25 +3,21 @@ package school.faang.animal_support;
 import lombok.Getter;
 import lombok.extern.slf4j.Slf4j;
 
-import java.util.concurrent.Semaphore;
+import java.util.concurrent.atomic.AtomicReference;
 
 @Getter
 @Slf4j
 public class Organization {
-    private double balance = 0;
-    private static final Semaphore semaphore = new Semaphore(1);
+    private final AtomicReference<Double> balance = new AtomicReference<>(0.0);
 
     public void addDonation(Donation donation) {
         try {
-            semaphore.acquire();
-            balance += donation.getAmount();
+            balance.updateAndGet(currentBalance -> currentBalance + donation.getAmount());
             log.info("Donation {} with amount {} is successful added to balance!",
                     donation.getId(),
                     donation.getAmount());
-        } catch (InterruptedException e) {
+        } catch (Exception e) {
             throw new RuntimeException(e);
-        } finally {
-            semaphore.release();
         }
     }
 }
