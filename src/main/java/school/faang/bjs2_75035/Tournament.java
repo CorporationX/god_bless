3543a879351e7ebf77b2e1267a6a2500b8fe30d7
pirojok_yt <tamik34@mail.ru>
2025-05-04 12:38:30 +0500

    finish

--- src/main/java/school/faang/bjs2_75035/Tournament.java
+++ src/main/java/school/faang/bjs2_75035/Tournament.java
@@ -4,11 +4,12 @@ import java.util.concurrent.CompletableFuture;
 
 public class Tournament {
     private static final long WAIT_PER_DIFFICULTY = 1000;
+
     public CompletableFuture<School> startTask(School school, Task task) {
         return CompletableFuture.supplyAsync(() -> {
             try {
                 System.out.println("Doing task...");
-                Thread.sleep( task.getDifficulty() * WAIT_PER_DIFFICULTY);
+                Thread.sleep(task.getDifficulty() * WAIT_PER_DIFFICULTY);
 
             } catch (InterruptedException e) {
                 System.out.println("Error, thread is interrupted");
