1ce6e6bcfa65ade8d94aa3122e45c503a80fbebd
AdminS <neptunchik1997@gmail.com>
2025-05-04 17:19:56 +0300

    Add: Турнир трех волшебников

---
+++ src/main/java/school/faang/tournamentwizards/Task.java
@@ -0,0 +1,12 @@
+package school.faang.tournamentwizards;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class Task {
+    private String name;
+    private int difficulty;
+    private int reward;
+}
