9ef3a8044dcaa090a72ee68317a5305ebe78397c
KirillSMv <88364531+KirillSMv@users.noreply.github.com>
2025-05-04 19:34:32 +0200

    Kirill 28377 bjs2 76664 (#20237)
    * "Выполнить задачу Поиграем в банк"
    
    * "Выполнить задачу Поиграем в банк"
    
    * "Выполнить задачу Поиграем в банк"

---
+++ src/main/java/school/faang/bank/Bank.java
@@ -0,0 +1,50 @@
+package school.faang.bank;
+
+import lombok.extern.slf4j.Slf4j;
+
+import java.util.concurrent.ConcurrentHashMap;
+
+@Slf4j
+public class Bank {
+    private final ConcurrentHashMap<Integer, Account> accounts = new ConcurrentHashMap<>();
+
+    public void addAccount(Account account) {
+        log.debug("Thread: {}, executing addAccount()", Thread.currentThread().getName());
+        accounts.put(account.getId(), account);
+    }
+
+    public boolean transfer(int fromAccountId, int toAccountId, double amount) {
+        log.debug("Thread: {}, executing transfer()", Thread.currentThread().getName());
+        final Account fromAccount = accounts.get(fromAccountId);
+        final Account toAccount = accounts.get(toAccountId);
+
+        if (fromAccount == null || toAccount == null) {
+            log.error("One of accounts was not found");
+            return false;
+        }
+
+        Account firstLock = fromAccount.getId() < toAccount.getId() ? fromAccount : toAccount;
+        Account secondLock = fromAccount.getId() < toAccount.getId() ? toAccount : fromAccount;
+
+        synchronized (firstLock) {
+            log.debug("Thread: {}, executing transfer(), entered synchronized (fromAccount)",
+                    Thread.currentThread().getName());
+            synchronized (secondLock) {
+                log.debug("Thread: {}, executing transfer(), entered synchronized (toAccount)",
+                        Thread.currentThread().getName());
+                if (fromAccount.getBalance() < amount) {
+                    log.error("Insufficient balance");
+                    return false;
+                }
+                fromAccount.withdraw(amount);
+                toAccount.deposit(amount);
+            }
+        }
+        return true;
+    }
+
+    public double getTotalBalance() {
+        log.debug("Thread: {}, executing getTotalBalance()", Thread.currentThread().getName());
+        return accounts.values().stream().mapToDouble(Account::getBalance).sum();
+    }
+}
