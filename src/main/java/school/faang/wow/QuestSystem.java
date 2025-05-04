f2ccc5ca15ce5a259b1efa59489e4dc80628dee4
Raidanmark <Onidanil@Gmail.com>
2025-05-04 18:02:11 +0200

    task created

--- src/main/java/school/faang/wow/QuestSystem.java
+++ src/main/java/school/faang/wow/QuestSystem.java
@@ -1,4 +1,18 @@
 package school.faang.wow;
 
+import java.util.concurrent.CompletableFuture;
+
 public class QuestSystem {
+
+    public CompletableFuture<Player> startQuest(Player player, Quest quest) {
+        return CompletableFuture.supplyAsync(() -> {
+            try {
+                Thread.sleep(quest.getDifficulty() * 1000L);
+                player.setExperience(player.getExperience() + quest.getReward());
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            return player;
+        });
+    }
 }
