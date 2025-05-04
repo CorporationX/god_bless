1e7a9ee7721683ffd22b04c5ee9465eebf89189c
Sergey Filippovich <filippovich.sergey@gmail.com>
2025-05-04 20:53:44 +0400

    class Main, object uploadThread. Returned lambda

--- src/main/java/school/faang/BJS2_73782/Main.java
+++ src/main/java/school/faang/BJS2_73782/Main.java
@@ -1,14 +1,17 @@
 package school.faang.BJS2_73782;
 
-import lombok.SneakyThrows;
-
 public class Main {
-    @SneakyThrows
     public static void main(String[] args) {
 
         GooglePhotosAutoUploader uploader = new GooglePhotosAutoUploader();
 
-        Thread uploadThread = new Thread(uploader::startAutoUpload);
+        Thread uploadThread = new Thread(() -> {
+            try {
+                uploader.startAutoUpload();
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+        });
 
         Thread photoAdderThread = new Thread(() -> {
             try {
