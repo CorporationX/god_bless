33c4463a2aef29fe614a410bccf6d67a581cd702
Raidanmark <Onidanil@Gmail.com>
2025-05-04 18:38:52 +0200

    commment fixes

--- src/main/java/school/faang/wow/QuestSystem.java
+++ src/main/java/school/faang/wow/QuestSystem.java
@@ -1,18 +1,23 @@
 package school.faang.wow;
 
 import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 public class QuestSystem {
+    public static final long CONVERT_TO_SECONDS = 1000L;
+    private final ExecutorService executor = Executors.newFixedThreadPool(2);
 
-    public CompletableFuture<Player> startQuest(Player player, Quest quest) {
+
+    public synchronized CompletableFuture<Player> startQuest(Player player, Quest quest) {
         return CompletableFuture.supplyAsync(() -> {
             try {
-                Thread.sleep(quest.getDifficulty() * 1000L);
+                Thread.sleep(quest.getDifficulty() * CONVERT_TO_SECONDS);
                 player.setExperience(player.getExperience() + quest.getReward());
             } catch (InterruptedException e) {
                 e.printStackTrace();
             }
             return player;
-        });
+        }, executor);
     }
 }
