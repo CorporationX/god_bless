d9551869cac27b3a252335cd5ef3fb8bd6578883
Vladislav Kozlov <littlerock2877@mail.ru>
2025-05-05 22:27:36 +0300

    add: master card service

---
+++ src/main/java/school/faang/bjs2_74582/Main.java
@@ -0,0 +1,9 @@
+package school.faang.bjs2_74582;
+
+public class Main {
+    public static void main(String[] args) {
+        MasterCardService service = new MasterCardService();
+        service.doAll();
+        service.dispose();
+    }
+}
