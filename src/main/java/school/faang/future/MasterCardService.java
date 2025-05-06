2d57c2ce4cb75a28001d36dc552dfe8c1ae4b7f3
Gigabyte <012_erasyl@mail.ru>
2025-05-06 18:20:23 +0500

    MasterCard Commit

--- src/main/java/school/faang/future/MasterCardService.java
+++ src/main/java/school/faang/future/MasterCardService.java
@@ -9,11 +9,13 @@ import java.util.concurrent.Future;
 public class MasterCardService {
     private static final int TEN_SECONDS_IN_MS = 10000;
     private static final int ONE_SECOND_IN_MS = 1000;
+    private static final int PAYMENT_AMOUNT = 5_000;
+    private static final int ANALYTICS_RESULT = 17_000;
 
     static int collectPayment() {
         try {
             Thread.sleep(TEN_SECONDS_IN_MS);
-            return 5_000;
+            return PAYMENT_AMOUNT;
         } catch (InterruptedException e) {
             Thread.currentThread().interrupt();
             throw new RuntimeException(e);
@@ -23,7 +25,7 @@ public class MasterCardService {
     static int sendAnalytics() {
         try {
             Thread.sleep(ONE_SECOND_IN_MS);
-            return 17000;
+            return ANALYTICS_RESULT;
         } catch (InterruptedException e) {
             Thread.currentThread().interrupt();
             throw new RuntimeException(e);
@@ -32,10 +34,10 @@ public class MasterCardService {
 
     public void doAll() {
         ExecutorService executor = Executors.newSingleThreadExecutor();
+        Future<Integer> paymentFuture = executor.submit(MasterCardService::collectPayment);
 
-        Future<Integer> paymentFuture = executor.submit(() -> collectPayment());
+        CompletableFuture<Integer> analyticsFuture = CompletableFuture.supplyAsync(MasterCardService::sendAnalytics, executor);
 
-        CompletableFuture<Integer> analyticsFuture = CompletableFuture.supplyAsync(() -> sendAnalytics());
 
         try {
             int analyticsResult = analyticsFuture.get();
