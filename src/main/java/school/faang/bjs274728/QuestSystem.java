e7ff64ec90d8668268833cd09e28ff86bfd1c875
Miras <mirasospan62@gmail.com>
2025-05-07 19:36:54 +0500

    fixed problems

--- src/main/java/school/faang/bjs274728/QuestSystem.java
+++ src/main/java/school/faang/bjs274728/QuestSystem.java
@@ -1,19 +1,24 @@
 package school.faang.bjs274728;
 
 import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 public class QuestSystem {
+
+    private static final long ONE_SECOND = 1000L;
+    private static final ExecutorService service = Executors.newFixedThreadPool(3);
 
     public CompletableFuture<Player> startQuest(Player player, Quest quest) {
         return CompletableFuture
                 .supplyAsync(() -> {
                     try {
-                        Thread.sleep(quest.getDifficulty() * 1000L);
+                        Thread.sleep(quest.getDifficulty() * ONE_SECOND);
                     } catch (InterruptedException e) {
-                        throw new RuntimeException(e);
+                        throw new IllegalArgumentException(e);
                     }
                     player.addExperience((quest.getReward()));
                     return player;
-                });
+                }, service);
     }
 }
