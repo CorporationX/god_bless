f96b21e7bd6f9a47036fe4f7849673d798202424
Lusivar <jad.fale@mail.ru>
2025-05-07 18:17:08 +0300

    BJS2-75660 WOW

---
+++ src/main/java/school/faang/bjs2_75660/Main.java
@@ -0,0 +1,21 @@
+package school.faang.bjs2_75660;
+
+import java.util.concurrent.CompletableFuture;
+
+public class Main {
+    public static void main(String[] args) {
+        Player player = new Player("Join", 12, 124);
+        Player player2 = new Player("Alice", 18, 329);
+        Quest quest = new Quest("1", 5, 46);
+        Quest quest2 = new Quest("2", 8, 89);
+        QuestSystem questSystem = new QuestSystem();
+        CompletableFuture<Player> playerQuest = questSystem.startQuest(player, quest);
+        CompletableFuture<Player> player2Quest = questSystem.startQuest(player2, quest2);
+        playerQuest.thenAccept(play -> System.out.println(play.getName() +
+                " has completed the quest and now has " + play.getExperience() +
+                " experience points.")).join();
+        player2Quest.thenAccept(play -> System.out.println(play.getName() +
+                " has completed the quest and now has " + play.getExperience() +
+                " experience points.")).join();
+    }
+}
---
+++ src/main/java/school/faang/bjs2_75660/QuestSystem.java
@@ -0,0 +1,20 @@
+package school.faang.bjs2_75660;
+
+import lombok.extern.slf4j.Slf4j;
+
+import java.util.concurrent.CompletableFuture;
+
+@Slf4j
+public class QuestSystem {
+    public CompletableFuture<Player> startQuest(Player player, Quest quest) {
+        return CompletableFuture.supplyAsync(() -> {
+            try {
+                Thread.sleep(quest.getDifficulty());
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+            player.setExperience(player.getExperience() + quest.getReward());
+            return player;
+        });
+    }
+}
