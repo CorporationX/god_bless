e3aa19edf4ab249920643254f7b9224b396d49f5
Andrew Sonin <sonin.andrei2009@yandex.ru>
2025-05-09 11:53:30 +0300

    Task BJS2-74636 1) Добавил класс MasterCardService 2) Написал логику

---
+++ src/main/java/school/faang/future/MasterCardService.java
@@ -0,0 +1,48 @@
+package school.faang.future;
+
+import java.util.concurrent.*;
+
+public class MasterCardService {
+    private static final int TEN_SECONDS_IN_MS = 10_000;
+    private static final int ONE_SECOND_IN_MS = 1_000;
+
+    static int collectPayment() {
+        try {
+            Thread.sleep(TEN_SECONDS_IN_MS);
+            return 5000;
+        } catch (InterruptedException e) {
+            Thread.currentThread().interrupt();
+            throw new RuntimeException(e);
+        }
+    }
+
+    static int sendAnalytics() {
+        try {
+            Thread.sleep(ONE_SECOND_IN_MS);
+            return 17000;
+        } catch (InterruptedException e) {
+            Thread.currentThread().interrupt();
+            throw new RuntimeException(e);
+        }
+    }
+
+    public void doAll() {
+        ExecutorService executor = Executors.newFixedThreadPool(2);
+
+        try {
+            Future<Integer> paymentFuture = executor.submit(MasterCardService::collectPayment);
+
+            CompletableFuture<Integer> analyticsFuture = CompletableFuture.supplyAsync(MasterCardService::sendAnalytics, executor);
+
+            Integer analyticsResult = analyticsFuture.get();
+            System.out.println("Аналитика отправлена: " + analyticsResult);
+
+            Integer paymentResult = paymentFuture.get();
+            System.out.println("Платеж выполнен: " + paymentResult);
+        } catch (InterruptedException | ExecutionException e) {
+            e.printStackTrace();
+        } finally {
+            executor.shutdown();
+        }
+    }
+}
