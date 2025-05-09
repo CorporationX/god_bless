096b3ae91bc4a08fdb3a6f992d919371b0f432c8
Ilyas Kystaubayev <kystaubaev.iliyas@mail.ru>
2025-05-10 04:07:40 +0500

    Leave comments

--- src/main/java/school/faang/comments/Main.java
+++ src/main/java/school/faang/comments/Main.java
@@ -10,26 +10,26 @@ public class Main {
             Post post = new Post(1, "Hello", "First post!", "Alice");
             postService.addPost(post);
         });
+        t1.start();
+
+        sleep(DELAY_TIME);
 
         Thread t2 = new Thread(() -> {
             Comment comment = new Comment("Nice post!", "Bob");
             postService.addComment(1, comment);
         });
+        t2.start();
 
         Thread t3 = new Thread(() -> {
             Comment comment = new Comment("Thanks for sharing.", "Charlie");
             postService.addComment(1, comment);
         });
+        t3.start();
 
         Thread t4 = new Thread(() -> postService.deletePost(1, "Bob"));
+        t4.start();
 
         Thread t5 = new Thread(() -> postService.deletePost(1, "Alice"));
-
-        t1.start();
-        sleep(DELAY_TIME);
-        t2.start();
-        t3.start();
-        t4.start();
         t5.start();
     }
 
