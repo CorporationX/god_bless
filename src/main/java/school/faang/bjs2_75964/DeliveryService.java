cd2735504ea60d4ed4fe843c4c89e298796e7fd4
AlexandrPopov <164056516+Aleck210popov@users.noreply.github.com>
2025-05-10 16:58:19 +0300

    BJS2-75964 add lock

--- src/main/java/school/faang/bjs2_75964/DeliveryService.java
+++ src/main/java/school/faang/bjs2_75964/DeliveryService.java
@@ -12,12 +12,14 @@ import java.util.Queue;
 import java.util.Set;
 import java.util.concurrent.ConcurrentLinkedQueue;
 import java.util.concurrent.ConcurrentSkipListSet;
+import java.util.concurrent.locks.ReentrantLock;
 
 @Slf4j
 public class DeliveryService {
     private final Set<PromoCode> promoCodesActive =
             new ConcurrentSkipListSet<>(Comparator.comparing(PromoCode::getCode));
     private final Queue<Order> processedOrders = new ConcurrentLinkedQueue<>();
+    private final ReentrantLock lock = new ReentrantLock();
 
     public void addPromoCode(PromoCode promoCode) {
         checkNull(promoCode);
@@ -35,14 +37,18 @@ public class DeliveryService {
     }
 
     public void processOrder(Order order, List<PromoCode> promoCodes) {
-        checkNull(order);
-        checkNull(promoCodes);
-        promoCodes.forEach(this::checkNull);
+        checkNull(order, promoCodes);
         if (order.getIsDiscount().get()) {
             log.error("Discount already applied to order {}", order.getNumber());
             throw new DiscountAlreadyAppliedToOrderException(order.getNumber());
         }
-        PromoCode bestPromoCode = promoCodes.stream()
+        PromoCode bestPromoCode = getBestPromoCode(order, promoCodes);
+
+        applyDiscount(order, bestPromoCode);
+    }
+
+    private PromoCode getBestPromoCode(Order order, List<PromoCode> promoCodes) {
+        return promoCodes.stream()
                 .filter(promoCodesActive::contains)
                 .filter(promoCode -> promoCode.isValidForOrder(order))
                 .max(Comparator.comparingDouble(PromoCode::getDiscount))
@@ -50,13 +56,26 @@ public class DeliveryService {
                     log.error("Proper promo code not found");
                     return new PromoCodeNotFoundException();
                 });
+    }
 
-        bestPromoCode.markAsUsed();
-        double discountedPrice = order.applyDiscount(bestPromoCode.getDiscount());
-        log.info("Apply discount {} for order {}, discounted price {}",
-                bestPromoCode.getDiscount(), order.getNumber(), discountedPrice);
-        promoCodesActive.remove(bestPromoCode);
-        processedOrders.add(order);
+    private void applyDiscount(Order order, PromoCode bestPromoCode) {
+        lock.lock();
+        try {
+            bestPromoCode.markAsUsed();
+            double discountedPrice = order.applyDiscount(bestPromoCode.getDiscount());
+            log.info("Apply discount {} for order {}, discounted price {}",
+                    bestPromoCode.getDiscount(), order.getNumber(), discountedPrice);
+            promoCodesActive.remove(bestPromoCode);
+            processedOrders.add(order);
+        } finally {
+            lock.unlock();
+        }
+    }
+
+    private <T> void checkNull(T object, List<?> objectList) {
+        checkNull(object);
+        checkNull(objectList);
+        objectList.forEach(this::checkNull);
     }
 
     private <T> void checkNull(T object) {
