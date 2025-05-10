e1c52f95bdf376095176764ac9ef5edeff1b8614
Andrew Sonin <sonin.andrei2009@yandex.ru>
2025-05-10 19:06:52 +0300

    Task BJS2-75921 1) Добавил класс Bank , Account 2) Написал логику

---
+++ src/main/java/school/faang/bank/Account.java
@@ -0,0 +1,41 @@
+package school.faang.bank;
+
+import lombok.Getter;
+
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
+@Getter
+class Account {
+    private final int id;
+    private double balance;
+    private final Lock lock = new ReentrantLock();
+
+    public Account(int id, double initialBalance) {
+        this.id = id;
+        this.balance = initialBalance;
+    }
+
+    public void deposit(double amount) {
+        lock.lock();
+        try {
+            balance += amount;
+        } finally {
+            lock.unlock();
+        }
+    }
+
+    public boolean withdraw(double amount) {
+        lock.lock();
+        try {
+            if (amount > balance) {
+                return false;
+            }
+            balance -= amount;
+            return true;
+        } finally {
+            lock.unlock();
+        }
+    }
+
+}
