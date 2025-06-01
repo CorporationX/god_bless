28df79e0c60963a855a3726283ffb4bf905fc950
Linempy <mychinnik@yandex.ru>
2025-06-01 23:39:16 +0300

    BJS2-79141 done

--- src/main/java/school/faang/BJS2_79141/Main.java
+++ src/main/java/school/faang/BJS2_79141/Main.java
@@ -2,16 +2,11 @@ package school.faang.BJS2_79141;
 
 public class Main {
     public static void main(String[] args) {
-
         try {
             User user1 = new User("Nikita", 19, "Student", "Петергоф");
             User user2 = new User("Steve", 20, "Google", "New York");
         } catch (IllegalArgumentException e) {
             System.out.println(e.getMessage());
         }
-
-
-
-
     }
 }
