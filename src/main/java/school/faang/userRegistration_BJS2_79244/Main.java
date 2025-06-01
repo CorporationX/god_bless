45c821d3ad185a8ede1af6b1a8cfa84f554f9988
Bookinisto <bookinisto@mail.ru>
2025-06-02 02:40:35 +0700

    реализовал метод groupUsers и вывел на экран результат

---
+++ src/main/java/school/faang/userRegistration_BJS2_79244/Main.java
@@ -0,0 +1,9 @@
+package school.faang.userRegistration_BJS2_79244;
+
+public class Main {
+    public static void main(String[] args) {
+        User user = new User("Dima", 19, "Uber", "London");
+        System.out.println(user);
+    }
+}
+
