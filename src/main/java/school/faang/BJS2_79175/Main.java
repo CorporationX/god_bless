12c78de87e7278c6560c1c559bcdd57c4a987731
JasonRon <ubervladocity@gmail.com>
2025-06-02 18:15:33 +0300

    Исправил недоработки и немного улучшил код.

--- src/main/java/school/faang/BJS2_79175/Main.java
+++ src/main/java/school/faang/BJS2_79175/Main.java
@@ -12,7 +12,7 @@ public class Main {
         User user3 = new User("Sam", 24, "Yandex", "Pobeda 22");
         User user4 = new User("Vlad", 32, "Sberbank", "Lenina 12");
 
-        List.of(user1,user2,user3,user4).forEach(user -> users.add(user));
+        List.of(user1, user2, user3, user4).forEach(user -> users.add(user));
         System.out.println(User.groupUsers(users));
     }
 }
