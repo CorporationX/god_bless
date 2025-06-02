6abd1da0d5b93715da2ae6c26758c94a580b38f7
Vlad Nikitin <nikitin.v2407@gmail.com>
2025-06-02 15:10:27 +0300

    Task completed

--- src/main/java/school/faang/groupingUsersByAge/Main.java
+++ src/main/java/school/faang/groupingUsersByAge/Main.java
@@ -7,6 +7,7 @@ import java.util.Random;
 public class Main {
 
     public static void main(String[] args) {
+
         ArrayList<String> city = new ArrayList<>() {{
             add("Moskov");
             add("Omsk");
--- src/main/java/school/faang/groupingUsersByAge/User.java
+++ src/main/java/school/faang/groupingUsersByAge/User.java
@@ -1,41 +1,34 @@
 package school.faang.groupingUsersByAge;
-import lombok.AllArgsConstructor;
-import lombok.EqualsAndHashCode;
-import lombok.Getter;
-import lombok.ToString;
+
+import lombok.Data;
+
 import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
-import java.util.Set;
 
-@AllArgsConstructor
-@Getter
-@ToString
-@EqualsAndHashCode
+@Data
+
 public class User {
     private String name;
-    private Integer age;
+    private int age;
     private String addressWork;
     private String address;
 
-    public static Map<Integer, List<User>> groupUsers(List<User> userList) {
-        Set<Integer> ageUsers = new HashSet<>();
-        for (User user : userList) {
-            ageUsers.add(user.age);
-        }
-        Map<Integer, List<User>> groupUsers = new HashMap<>();
-        for (Integer age : ageUsers) {
-            List<User> usersToAge = new ArrayList<>();
-            for (User user : userList) {
-                if (user.getAge().equals(age)) {
-                    usersToAge.add(user);
-                }
-            }
-            groupUsers.put(age, usersToAge);
-        }
-        return groupUsers;
+    public User(String name, int age, String addressWork, String address) {
+        this.name = name;
+        this.age = age;
+        this.addressWork = addressWork;
+        this.address = address;
     }
 
+    public static Map<Integer, List<User>> groupUsers(List<User> users) {
+        Map<Integer, List<User>> result = new HashMap<>();
+
+        for (User user : users) {
+            result.computeIfAbsent(user.getAge(), k -> new ArrayList<>()).add(user);
+        }
+
+        return result;
+    }
 }
