6bbd768a1ba8f65c688d7a997b6dfed7f31c4e32
JasonRon <ubervladocity@gmail.com>
2025-06-02 20:12:46 +0300

    Сделал пробелы для запятых

--- src/main/java/school/faang/BJS2_79156/Main.java
+++ src/main/java/school/faang/BJS2_79156/Main.java
@@ -6,8 +6,8 @@ import java.util.Set;
 
 public class Main {
     public static void main(String[] args) {
-        User user1 = new User(1, "Ахилес", 18, Set.of("Бег", "Пробежка","Драка"));
-        User user2 = new User(2, "Архимед", 44, Set.of("Бег", "Качалка","Спиритизм"));
+        User user1 = new User(1, "Ахилес", 18, Set.of("Бег", "Пробежка", "Драка"));
+        User user2 = new User(2, "Архимед", 44, Set.of("Бег", "Качалка", "Спиритизм"));
         User user3 = new User(3, "Архимонд", 328, Set.of("Аниматор"));
 
         List<User> usersCompare = List.of(user1, user2, user3);
