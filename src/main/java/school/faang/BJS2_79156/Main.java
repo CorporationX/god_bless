bb8a43fb29eef3908aabf16f22fc2997beebf7be
JasonRon <ubervladocity@gmail.com>
2025-06-02 20:08:52 +0300

    Реализовал метод, который возвращает Имя +1 активность

---
+++ src/main/java/school/faang/BJS2_79156/Main.java
@@ -0,0 +1,25 @@
+package school.faang.BJS2_79156;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
+public class Main {
+    public static void main(String[] args) {
+        User user1 = new User(1, "Ахилес", 18, Set.of("Бег", "Пробежка","Драка"));
+        User user2 = new User(2, "Архимед", 44, Set.of("Бег", "Качалка","Спиритизм"));
+        User user3 = new User(3, "Архимонд", 328, Set.of("Аниматор"));
+
+        List<User> usersCompare = List.of(user1, user2, user3);
+
+        Set<String> activitiesCompare = Set.of("Качалка", "Бег");
+
+        Map<User, String> users = User.findHobbyLovers(usersCompare, activitiesCompare);
+        System.out.println(users);
+
+
+
+    }
+}
---
+++ src/main/java/school/faang/BJS2_79156/User.java
@@ -0,0 +1,38 @@
+package school.faang.BJS2_79156;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.ToString;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
+@AllArgsConstructor
+@ToString(onlyExplicitlyIncluded = true)
+public class User {
+    private int id;
+    @ToString.Include
+    private String name;
+    private int age;
+    @Getter
+    private Set<String> activities;
+
+    public static Map<User, String> findHobbyLovers(List<User> users, Set<String> activities) {
+        Map<User, String> result = new HashMap<>();
+
+        for (User user : users) {
+            Set<String> userActivities = user.getActivities();
+
+            for (String activity : activities) {
+                if (userActivities.contains(activity)) {
+                    result.put(user, activity);
+                    break;
+                }
+            }
+        }
+
+        return result;
+    }
+}
