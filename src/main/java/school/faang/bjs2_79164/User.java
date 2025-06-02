52758eb36fcf65271ec5577982d9a0c48569f47d
Linempy <mychinnik@yandex.ru>
2025-06-02 09:12:03 +0300

    BJS2-79164 fix

--- src/main/java/school/faang/bjs2_79164/User.java
+++ src/main/java/school/faang/bjs2_79164/User.java
@@ -1,5 +1,6 @@
 package school.faang.bjs2_79164;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.ToString;
 
@@ -10,27 +11,22 @@ import java.util.Map;
 
 @Getter
 @ToString
+@AllArgsConstructor
 public class User {
     private final String name;
     private final int age;
     private final String placeWork;
     private final String address;
 
-    public User(String name, int age, String placeWork, String address) {
-        this.name = name;
-        this.age = age;
-        this.placeWork = placeWork;
-        this.address = address;
-    }
 
     public static Map<Integer, List<User>> groupUsers(List<User> users) {
-        Map<Integer, List<User>> groupUserByAge = new HashMap<>();
+        Map<Integer, List<User>> groupedByAgeUsers = new HashMap<>();
 
         for (User user : users) {
-            groupUserByAge.computeIfAbsent(user.getAge(), (key) -> new ArrayList<>()).add(user);
+            groupedByAgeUsers.computeIfAbsent(user.getAge(), key -> new ArrayList<>()).add(user);
         }
 
-        return groupUserByAge;
+        return groupedByAgeUsers;
     }
 
 }
