313a3839513f0d9a9b96070aa843aa15f89b09d3
ddddddasha <daria.zhelobkovich@gmail.com>
2025-06-02 19:25:27 +0300

    group users by age and print results

---
+++ src/main/java/school/faang/module1/BJS2_79161/User.java
@@ -0,0 +1,29 @@
+package school.faang.module1.BJS2_79161;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@Data
+@AllArgsConstructor
+public class User {
+
+    private final String name;
+    private final int age;
+    private final String workplace;
+    private final String address;
+
+    public static Map<Integer, List<User>> groupUsers(List<User> users) {
+
+        Map<Integer, List<User>> groupedUsers = new HashMap<>();
+        for (User user : users) {
+            List<User> userList = groupedUsers.computeIfAbsent(user.getAge(), k -> new ArrayList<>());
+            userList.add(user);
+        }
+        return groupedUsers;
+    }
+}
