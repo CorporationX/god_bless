5407378d0e0ae01598af584dfb412212b47333ef
Andrey Fomchenko <35478@mail.ru>
2025-06-02 18:24:54 +0500

    Создал класс User, в котором реализовал метод сортировки по возрасту

--- src/main/java/school/faang/Main.java
+++ src/main/java/school/faang/Main.java
@@ -1,4 +1,15 @@
 package school.faang;
 
 public class Main {
+    public static void main(String[] args) {
+
+        User Andrey = new User("Andrey", 25, "Uber", "London");
+
+        System.out.println("Имя пользователя - " + Andrey.getName() + "\n"
+                            + "Возраст пользователя - " + Andrey.getAge() + " лет" + "\n"
+                            + "Место работы пользователя - " + Andrey.getJob() + "\n"
+                            + "Адрес пользователя - " + Andrey.getAddress());
+
+        User Dasha = new User("Dasha", 24, "Amazon", "Moscow");
+    }
 }
