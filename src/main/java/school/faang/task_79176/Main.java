c92ac73de20117af5c39c7401d020236d8bc76bc
Pavel Hohlov <hohlov.ps95@yandex.ru>
2025-06-02 22:52:27 +0300

    Предфинальная версия

--- src/main/java/school/faang/task_79176/Main.java
+++ src/main/java/school/faang/task_79176/Main.java
@@ -5,8 +5,7 @@ import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
-        Example example = new Example();
         List<Integer> array = List.of(1, 2, 3, 4, 5);
-        System.out.println(example.reverse(array).toString());
+        System.out.println(new Example().reverse(array));
     }
 }
