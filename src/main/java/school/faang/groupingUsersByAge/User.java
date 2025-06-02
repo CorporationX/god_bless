8edc0b22cdbf71a9a30b8837edae4f9e8737d930
Vlad Nikitin <nikitin.v2407@gmail.com>
2025-06-02 11:40:18 +0300

    Task completed

--- src/main/java/school/faang/groupingUsersByAge/User.java
+++ src/main/java/school/faang/groupingUsersByAge/User.java
@@ -1,10 +1,8 @@
 package school.faang.groupingUsersByAge;
-
 import lombok.AllArgsConstructor;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.ToString;
-
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.HashSet;
