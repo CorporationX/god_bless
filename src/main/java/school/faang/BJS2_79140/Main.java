5e81cb8ced32bf7b79ac178086df17cef2dd49b6
Setched <kir32kir@gmail.com>
2025-06-02 18:22:00 +0300

    Измененено объявление списка пользователей

--- src/main/java/school/faang/BJS2_79140/Main.java
+++ src/main/java/school/faang/BJS2_79140/Main.java
@@ -7,19 +7,16 @@ import java.util.Map;
 
 public class Main {
     public static void main(String[] args) {
-        ArrayList<User> users = new ArrayList<>();
-
         User user1 = new User("John", 25, "Uber", "Izmailovskaya 5");
         User user2 = new User("Jane", 19, "Google", "Izmailovskaya 8");
         User user3 = new User("Alex", 30, "Yandex", "Leninskaya 2");
         User user4 = new User("Lucky", 30, "Google", "Mira 2");
         User user5 = new User("Bob", 19, "Netflix", "Leninskaya 1");
 
-        List.of(user1, user2, user3, user4, user5).forEach(user -> users.add(user));
+        List<User> users = List.of(user1, user2, user3, user4, user5);
 
         Map<Integer, List<User>> groupedUsers = User.groupUsers(users);
 
         System.out.println(groupedUsers);
     }
-
 }
