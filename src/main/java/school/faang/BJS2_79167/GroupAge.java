5856a47562374effa0fe08786a13cf81224e30f7
Reverend17 <marklajer1@gmail.com>
2025-06-02 19:13:45 +0300

    группировка пользователей по возрасту bug fixed

--- src/main/java/school/faang/BJS2_79167/GroupAge.java
+++ src/main/java/school/faang/BJS2_79167/GroupAge.java
@@ -7,7 +7,7 @@ import java.util.Map;
 public class GroupAge {
     public static void main(String[] args) {
         List<User> users = new ArrayList<>();
-        users.add(new User("John", 30, "microsoft", "US"));
+        users.add(new User("John", 30, "microsoft", "USA"));
         users.add(new User("Mary", 25, "google", "Russia"));
         users.add(new User("Kevin", 23, "apple", "France"));
         users.add(new User("Nicholas", 24, "apple", "France"));
