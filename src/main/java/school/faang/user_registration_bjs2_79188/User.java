7d1776f6ca1d66651247ceec74b32686d14050ad
Setched <kir32kir@gmail.com>
2025-06-03 01:33:11 +0300

    Style Fix

--- src/main/java/school/faang/user_registration_bjs2_79188/User.java
+++ src/main/java/school/faang/user_registration_bjs2_79188/User.java
@@ -14,18 +14,17 @@ public class User {
     private String address;
 
     User(String name, int age, String job, String address) {
+        if (name.toLowerCase()==null || name=="") { throw new IllegalArgumentException("name cant be null");}
+        if (age < 18) { throw new IllegalArgumentException("age cant be less than 18");}
+        if (!VALID_JOBS.contains(job.toLowerCase())) { throw new IllegalArgumentException("job not valid");}
+        if (!VALID_ADDRESSES.contains(address.toLowerCase())) { throw new IllegalArgumentException("address not valid");}
 
-        if (name.toLowerCase()==null || name == "") throw new IllegalArgumentException("name cant be null");
-        if (age < 18) throw new IllegalArgumentException("age cant be less than 18");
-        if (!VALID_JOBS.contains(job.toLowerCase())) throw new IllegalArgumentException("job not valid");
-        if (!VALID_ADDRESSES.contains(address.toLowerCase())) throw new IllegalArgumentException("address not valid");
-
-        this.name = name;
-        this.age = age;
-        this.job = job;
-        this.address = address;
+        this.name=name;
+        this.age=age;
+        this.job=job;
+        this.address=address;
     }
 
-    private static final Set<String> VALID_JOBS = Set.of("google", "uber", "amazon");
-    private static final Set<String> VALID_ADDRESSES = Set.of("london", "new york", "amsterdam");
+    private static final Set<String> VALID_JOBS=Set.of("google", "uber", "amazon");
+    private static final Set<String> VALID_ADDRESSES=Set.of("london", "new york", "amsterdam");
 }
