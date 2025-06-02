a0fec570892aae8e6ab68b0441dee917715d25e8
Pavel Hohlov <hohlov.ps95@yandex.ru>
2025-06-02 22:36:23 +0300

    Предфинальная версия

---
+++ src/main/java/school/faang/task_79176/Example.java
@@ -0,0 +1,23 @@
+package school.faang.task_79176;
+
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@NoArgsConstructor
+public class Example {
+    public List<Integer> reverse (List<Integer> array){
+        ArrayList<Integer> resultArray = new ArrayList<>();
+        int arraySize = array.size();
+        int i = 0;
+        while (i < array.size()){
+            resultArray.add(array.get(arraySize - 1));
+            arraySize--;
+            i++;
+        }
+        return resultArray;
+    }
+}
+
---
+++ src/main/java/school/faang/task_79176/Main.java
@@ -0,0 +1,12 @@
+package school.faang.task_79176;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) {
+        Example example = new Example();
+        List<Integer> array = List.of(1, 2, 3, 4, 5);
+        System.out.println(example.reverse(array).toString());
+    }
+}
