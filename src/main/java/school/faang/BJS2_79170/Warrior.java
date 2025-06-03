b4b28248d75b341aca80f5cbf15e0b1b599b38d6
Мозгаправ всегда прав <asklooka@gmail.com>
2025-06-03 09:08:34 -0400

    Update Warrior.java

--- src/main/java/school/faang/BJS2_79170/Warrior.java
+++ src/main/java/school/faang/BJS2_79170/Warrior.java
@@ -13,10 +13,12 @@ public class Warrior extends Character {
 
     @Override
     public void attack(Character opponent) {
-        if(opponent.getHealth() != 0)
+        if(opponent.getHealth() != 0) {
             opponent.setHealth(opponent.getHealth() - power);
             if (opponent.getHealth() < 0) {
                 opponent.setHealth(0);
-        }
+        
+            }   
+        }        
     }
 }
