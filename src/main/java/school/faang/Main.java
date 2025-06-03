93e1b009a0f9cf9e988529e66254ba0bb2b3f854
Andrey Fomchenko <35478@mail.ru>
2025-06-03 23:44:48 +0500

    BJS2-79148 реализовал группировку

--- src/main/java/school/faang/Main.java
+++ src/main/java/school/faang/Main.java
@@ -3,13 +3,13 @@ package school.faang;
 public class Main {
     public static void main(String[] args) {
 
-        User Andrey = new User("Andrey", 25, "Uber", "London");
+        User andrey = new User("Andrey", 25, "Uber", "London");
 
-        System.out.println("Имя пользователя - " + Andrey.getName() + "\n"
-                + "Возраст пользователя - " + Andrey.getAge() + " лет" + "\n"
-                + "Место работы пользователя - " + Andrey.getJob() + "\n"
-                + "Адрес пользователя - " + Andrey.getAddress());
+        System.out.println("Имя пользователя - " + andrey.getName() + "\n"
+                + "Возраст пользователя - " + andrey.getAge() + " лет" + "\n"
+                + "Место работы пользователя - " + andrey.getJob() + "\n"
+                + "Адрес пользователя - " + andrey.getAddress());
 
-        User Dasha = new User("Dasha", 24, "Amazon", "Orenburg");
+        User dasha = new User("Dasha", 24, "Amazon", "Orenburg");
     }
 }
