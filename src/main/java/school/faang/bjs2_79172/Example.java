cdcd5173fe397bc4096deab635ea1bde637a1f11
Linempy <mychinnik@yandex.ru>
2025-06-03 10:02:29 +0300

    BJS2-79172 done

---
+++ src/main/java/school/faang/bjs2_79172/Example.java
@@ -0,0 +1,13 @@
+package school.faang.bjs2_79172;
+
+public class Example {
+
+    public static void reverse(int[] nums) {
+        int length = nums.length;
+        for (int i = 0; i < length / 2; i++) {
+            int temp = nums[i];
+            nums[i] = nums[length - i - 1];
+            nums[length - i - 1] = temp;
+        }
+    }
+}
