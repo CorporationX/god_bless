d8cbd5cfcc91bee9e961706ccada677ca0af3294
Setched <kir32kir@gmail.com>
2025-06-03 19:00:52 +0300

    Создан класс Example. Реализован Main

---
+++ src/main/java/school/faang/Reverse_bjs2_79184/Example.java
@@ -0,0 +1,17 @@
+package school.faang.Reverse_bjs2_79184;
+
+public class Example {
+    static public int[] reverse(int[] arr) {
+        for (int i = 0; i < arr.length / 2; i++ ) {
+            int tmp = arr[i];
+            arr[i] = arr[arr.length - i - 1];
+            arr[arr.length - i - 1] = tmp;
+        }
+        return arr;
+    }
+    static public void printArr(int[] arr) {
+        for (int i = 0; i < arr.length; i++) {
+            System.out.printf("%d ", arr[i]);
+        }
+    }
+}
