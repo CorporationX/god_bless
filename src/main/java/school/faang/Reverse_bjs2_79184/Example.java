09666dee8af62532f9e8a5502daa4dea770bb5c8
Setched <kir32kir@gmail.com>
2025-06-03 19:05:11 +0300

    style fix

--- src/main/java/school/faang/Reverse_bjs2_79184/Example.java
+++ src/main/java/school/faang/Reverse_bjs2_79184/Example.java
@@ -1,15 +1,16 @@
 package school.faang.Reverse_bjs2_79184;
 
 public class Example {
-    static public int[] reverse(int[] arr) {
-        for (int i = 0; i < arr.length / 2; i++ ) {
+    public static int[] reverse(int[] arr) {
+        for (int i = 0; i < arr.length / 2; i++) {
             int tmp = arr[i];
             arr[i] = arr[arr.length - i - 1];
             arr[arr.length - i - 1] = tmp;
         }
         return arr;
     }
-    static public void printArr(int[] arr) {
+
+    public static void printArr(int[] arr) {
         for (int i = 0; i < arr.length; i++) {
             System.out.printf("%d ", arr[i]);
         }
--- src/main/java/school/faang/Reverse_bjs2_79184/Main.java
+++ src/main/java/school/faang/Reverse_bjs2_79184/Main.java
@@ -2,7 +2,7 @@ package school.faang.Reverse_bjs2_79184;
 
 public class Main {
     public static void main(String[] args) {
-        int[] nums = {1,2,3,4,5};
+        int[] nums = {1, 2, 3, 4, 5};
         Example.reverse(nums);
         Example.printArr(nums);
     }
