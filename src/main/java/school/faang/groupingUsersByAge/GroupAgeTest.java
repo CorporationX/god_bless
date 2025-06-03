c44e4d84bad9869a92e4d38b6a5a17192c5e5730
Safixl <nikitin.v2407@gmail.com>
2025-06-03 10:52:35 +0300

    Реализация абстракции плюс тесты

--- src/main/java/school/faang/groupingUsersByAge/GroupAgeTest.java
+++ src/main/java/school/faang/groupingUsersByAge/GroupAgeTest.java
@@ -4,12 +4,14 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 
-public class GroupAge {
+public class GroupAgeTest {
     public static void main(String[] args) {
         List<User> users = new ArrayList<>();
         users.add(new User("John", 30, "microsoft", "USA"));
         users.add(new User("Mary", 25, "google", "Russia"));
         users.add(new User("Kevin", 23, "apple", "France"));
+        users.add(new User("Nicholas", 22, "apple", "France"));
+        users.add(new User("Nicholas", 22, "apple", "France"));
         users.add(new User("Nicholas", 24, "apple", "France"));
 
         Map<Integer, List<User>> groupUsers = User.groupUsers(users);
--- src/main/java/school/faang/groupingUsersByAge/User.java
+++ src/main/java/school/faang/groupingUsersByAge/User.java
@@ -1,47 +1,37 @@
 package school.faang.groupingUsersByAge;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
+import java.util.Set;
 
+@Data
+@AllArgsConstructor
 public class User {
     private String name;
     private int age;
-    private String workplace;
+    private String addressWork;
     private String address;
 
-    public static Map<Integer, List<User>> groupUsers(List<User> checkUsers) {
+    public static Map<Integer, List<User>> groupUsers(List<User> users) {
+        Set<Integer> ages = new HashSet<>();
+        for (User user : users) {
+            ages.add(user.getAge());
+        }
         Map<Integer, List<User>> result = new HashMap<>();
-
-        for (User user : checkUsers) {
-            int age = user.getAge();
-            if (!result.containsKey(age)) {
-                result.put(age, new ArrayList<>());
+        for (int age : ages) {
+            List<User> userListToAge = new ArrayList<>();
+            for (User user : users) {
+                if (user.getAge() == age) {
+                    userListToAge.add(user);
+                }
             }
-            result.get(age).add(user);
+            result.put(age,userListToAge);
         }
         return result;
-    }
-
-    public User(String name, int age, String workplace, String address) {
-        this.name = name;
-        this.age = age;
-        this.workplace = workplace;
-        this.address = address;
-    }
-
-    public int getAge() {
-        return age;
-    }
-
-    @Override
-    public String toString() {
-        return "User{" +
-                "name='" + name + '\'' +
-                ", age=" + age +
-                ", workplace='" + workplace + '\'' +
-                ", address='" + address + '\'' +
-                '}';
     }
 }
