168d99d91da26a428364e13fcb5f087d01df71b6
Мозгаправ всегда прав <asklooka@gmail.com>
2025-06-03 09:05:10 -0400

    Update Archer.java

--- src/main/java/school/faang/BJS2_79170/Archer.java
+++ src/main/java/school/faang/BJS2_79170/Archer.java
@@ -4,7 +4,11 @@ public class Archer extends Character {
     public Archer(String name, int power, int dexterity, int intelligence) {
         super(name, power, dexterity, intelligence);
     }
-
+    
+    public Archer(String name) {
+        super(name, 3, 10, 5);
+    }
+    
     @Override
     public void attack(Character opponent) {
         opponent.setHealth(opponent.getHealth() - dexterity);
@@ -14,7 +18,5 @@ public class Archer extends Character {
 
     }
 
-    public Archer(String name) {
-        super(name, 3, 10, 5);
-    }
+
 }
