ebe56f9120236527f115a8ffe884bcd5d1ddbb76
Reverend17 <marklajer1@gmail.com>
2025-06-03 10:55:41 +0300

    добавил аннотации, изменил метод убрав блок if

--- src/main/java/school/faang/BJS2_79167/User.java
+++ src/main/java/school/faang/BJS2_79167/User.java
@@ -1,10 +1,17 @@
 package school.faang.BJS2_79167;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.ToString;
+
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+@AllArgsConstructor
+@Getter
+@ToString
 public class User {
     private String name;
     private int age;
@@ -16,32 +23,34 @@ public class User {
 
         for (User user : checkUsers) {
             int age = user.getAge();
-            if (!result.containsKey(age)) {
-                result.put(age, new ArrayList<>());
-            }
+            result.putIfAbsent(age, new ArrayList<>());
             result.get(age).add(user);
         }
         return result;
     }
-
-    public User(String name, int age, String workplace, String address) {
-        this.name = name;
-        this.age = age;
-        this.workplace = workplace;
-        this.address = address;
-    }
-
-    public int getAge() {
-        return age;
-    }
-
-    @Override
-    public String toString() {
-        return "User{" +
-                "name='" + name + '\'' +
-                ", age=" + age +
-                ", workplace='" + workplace + '\'' +
-                ", address='" + address + '\'' +
-                '}';
-    }
 }
+//            if (!result.containsKey(age)) {
+//                result.put(age, new ArrayList<>());
+//            }
+//            result.get(age).add(user);
+//        }
+
+//    public User(String name, int age, String workplace, String address) {
+//        this.name = name;
+//        this.age = age;
+//        this.workplace = workplace;
+//        this.address = address;
+//    }
+
+//    public int getAge() {
+//        return age;
+//    }
+
+//    @Override
+//    public String toString() {
+//        return "User{" +
+//                "name='" + name + '\'' +
+//                ", age=" + age +
+//                ", workplace='" + workplace + '\'' +
+//                ", address='" + address + '\'' +
+//                '}';
