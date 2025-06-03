b0d1ef0e3e44cb42070e13ba5b0482f1a9fe0fd9
Safixl <nikitin.v2407@gmail.com>
2025-06-03 14:16:08 +0300

    Реализация сортировки по возрасту плюс тесты

--- src/main/java/school/faang/Abstraction/Character.java
+++ src/main/java/school/faang/Abstraction/Character.java
@@ -10,15 +10,21 @@ public abstract class Character {
     private int intelligence;
     private int healPoint = 100;
 
-    public void setHealPoint(int healPoint) {
-        this.healPoint = Math.max(healPoint, 0);
-    }
-
     public Character(String name) {
         this.name = name;
         this.strength = 5;
         this.agility = 5;
         this.intelligence = 5;
+    }
+    public Character(String name,int strength, int agility, int intelligence) {
+        this.name = name;
+        this.strength = strength;
+        this.agility = agility;
+        this.intelligence = intelligence;
+    }
+
+    public void setHealPoint(int healPoint) {
+        this.healPoint = Math.max(healPoint, 0);
     }
 
     public abstract void attack(Character opponent);
--- src/main/java/school/faang/Abstraction/FightTest.java
+++ src/main/java/school/faang/Abstraction/FightTest.java
@@ -4,6 +4,12 @@ public class FightTest {
     public static void main(String[] args) {
         Character userOne = new Warrior("bob");
         Character userTwo = new Archer("Jo");
+        Character userNoFight = new Character("Tom", 12, 20, 8) {
+            @Override
+            public void attack(Character opponent) {
+                opponent.setHealPoint(opponent.getHealPoint() - getIntelligence());
+            }
+        };
         System.out.println(userOne);
         userOne.attack(userTwo);
         userOne.attack(userTwo);
@@ -12,5 +18,6 @@ public class FightTest {
         userTwo.attack(userOne);
         System.out.println(userOne);
         System.out.println(userTwo);
+        System.out.println(userNoFight);
     }
 }
