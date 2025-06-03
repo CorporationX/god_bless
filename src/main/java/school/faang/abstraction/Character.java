9097e68dc08edee4167dc68d752f441b6723542c
Safixl <nikitin.v2407@gmail.com>
2025-06-03 16:51:53 +0300

    Реализация сортировки по возрасту плюс тесты

--- src/main/java/school/faang/abstraction/Character.java
+++ src/main/java/school/faang/abstraction/Character.java
@@ -1,4 +1,4 @@
-package school.faang.Abstraction;
+package school.faang.abstraction;
 
 import lombok.Data;
 
@@ -10,20 +10,25 @@ public abstract class Character {
     private int intelligence;
     private int healPoint = 100;
 
+    private static final int DEFAULT_STRENGTH = 5;
+    private static final int DEFAULT_AGILITY = 5;
+    private static final int DEFAULT_INTELLIGENCE = 5;
+
     public Character(String name) {
         this.name = name;
-        this.strength = 5;
-        this.agility = 5;
-        this.intelligence = 5;
+        this.strength = DEFAULT_STRENGTH;
+        this.agility = DEFAULT_AGILITY;
+        this.intelligence = DEFAULT_INTELLIGENCE;
     }
-    public Character(String name,int strength, int agility, int intelligence) {
+
+    public Character(String name, int strength, int agility, int intelligence) {
         this.name = name;
         this.strength = strength;
         this.agility = agility;
         this.intelligence = intelligence;
     }
 
-    public void setHealPoint(int healPoint) {
+    public void healPoint(int healPoint) {
         this.healPoint = Math.max(healPoint, 0);
     }
 
--- src/main/java/school/faang/abstraction/FightTest.java
+++ src/main/java/school/faang/abstraction/FightTest.java
@@ -1,15 +1,10 @@
-package school.faang.Abstraction;
+package school.faang.abstraction;
 
 public class FightTest {
     public static void main(String[] args) {
         Character userOne = new Warrior("bob");
         Character userTwo = new Archer("Jo");
-        Character userNoFight = new Character("Tom", 12, 20, 8) {
-            @Override
-            public void attack(Character opponent) {
-                opponent.setHealPoint(opponent.getHealPoint() - getIntelligence());
-            }
-        };
+
         System.out.println(userOne);
         userOne.attack(userTwo);
         userOne.attack(userTwo);
@@ -18,6 +13,6 @@ public class FightTest {
         userTwo.attack(userOne);
         System.out.println(userOne);
         System.out.println(userTwo);
-        System.out.println(userNoFight);
+
     }
 }
