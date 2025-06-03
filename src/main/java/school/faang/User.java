30b7ca5f6ba64602ca3d28c8b437e5fadec85179
Andrey Fomchenko <35478@mail.ru>
2025-06-03 23:44:22 +0500

    BJS2-79148 реализовал группировку

---
+++ src/main/java/school/faang/User.java
@@ -0,0 +1,57 @@
+package school.faang;
+
+import java.util.Set;
+
+public class User {
+    private static final Set<String> VALID_JOBS = Set.of("Google", "Uber", "Amazon");
+    private static final Set<String> VALID_ADDRESSES = Set.of("London", "New York", "Amsterdam");
+    private String name;
+    private int age;
+    private String job;
+    private String address;
+
+    public User(String name, int age, String job, String address) {
+        if (name.isEmpty()) {
+            throw new IllegalArgumentException("The name field cannot be empty.");
+        }
+        if (age < 18) {
+            throw new IllegalArgumentException("Unacceptable age.");
+        }
+        if (!VALID_JOBS.contains(job)) {
+            throw new IllegalArgumentException("Unacceptable place of work.");
+        }
+        if (!VALID_ADDRESSES.contains(address)) {
+            throw new IllegalArgumentException("Invalid residential address.");
+        }
+        this.name = name;
+        this.age = age;
+        this.job = job;
+        this.address = address;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public String getJob() {
+        return job;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "name='" + name + '\'' +
+                ", age=" + age +
+                ", job='" + job + '\'' +
+                ", address='" + address + '\'' +
+                '}';
+    }
+}
