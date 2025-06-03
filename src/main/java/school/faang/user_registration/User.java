64a43585794fde64378c7d2dd99246b3fbd0ae31
yagor. <warneverchang3s@gmail.com>
2025-06-03 20:53:02 +0300

    Регистрация пользователя - добавил утилитный класс и метод, который проверяет на null и empty

--- src/main/java/school/faang/user_registration/User.java
+++ src/main/java/school/faang/user_registration/User.java
@@ -3,6 +3,7 @@ package school.faang.user_registration;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.ToString;
+import school.faang.user_registration.utility.Validator;
 
 import java.util.Set;
 
@@ -11,17 +12,19 @@ import java.util.Set;
 @EqualsAndHashCode
 public class User {
 
+    private static Set<String> VALID_JOBS = Set.of("Google", "Uber", "Amazon");
+
+    private static Set<String> VALID_ADDRESSES = Set.of("London", "New York");
+
     private final String name;
     private final int age;
     private final String job;
     private final String address;
 
-    static Set<String> VALID_JOBS = Set.of("Google", "Uber", "Amazon");
 
-    static Set<String> VALID_ADDRESSES = Set.of("London", "New York");
 
     public User(String name, int age, String job, String address) {
-        if (name.isEmpty()) {
+        if (!Validator.validateStringForNotNullAndNotEmpty(name)) {
             throw new IllegalArgumentException("Name cannot be empty!");
         }
         this.name = name;
@@ -31,12 +34,12 @@ public class User {
         }
         this.age = age;
 
-        if (!VALID_JOBS.contains(job)) {
+        if (!Validator.validateStringForNotNullAndNotEmpty(job) || !VALID_JOBS.contains(job)) {
             throw new IllegalArgumentException(job + " - this job not allowed!");
         }
         this.job = job;
 
-        if (!VALID_ADDRESSES.contains(address)) {
+        if (!Validator.validateStringForNotNullAndNotEmpty(address) || !VALID_ADDRESSES.contains(address)) {
             throw new IllegalArgumentException(address + " - this address not allowed!");
         }
         this.address = address;
