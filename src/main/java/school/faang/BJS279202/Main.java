47760ec48c8e96a1aa949cbd0e036cc03d11151c
Askar <asklooka@gmail.com>
2025-06-03 17:14:49 -0400

    BJS2-79202 от мозгоправ всегда прав

---
+++ src/main/java/school/faang/BJS279202/Main.java
@@ -0,0 +1,11 @@
+package school.faang.BJS279202;
+
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) {
+        List<Integer> list = List.of(1, 2, 3, 4, 5);
+        Example e = new Example();
+        System.out.println(e.reverse(list));
+    }
+}
