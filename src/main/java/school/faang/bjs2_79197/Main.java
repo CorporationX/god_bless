99bbdc8b117c014365684ad2705e1e13544cceba
JasonRon <ubervladocity@gmail.com>
2025-06-03 17:46:47 +0300

    Реализовал метод Reverse без методов

---
+++ src/main/java/school/faang/bjs2_79197/Main.java
@@ -0,0 +1,10 @@
+package school.faang.bjs2_79197;
+
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) {
+        Example.reverse(new int[]{1,2,3,4,5});
+
+    }
+}
