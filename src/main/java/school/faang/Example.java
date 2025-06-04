7c0a94d63790bfac93bc74b8d6b498e9eb660604
Andrey Fomchenko <35478@mail.ru>
2025-06-04 19:53:32 +0500

    BJS2-79192

--- src/main/java/school/faang/Example.java
+++ src/main/java/school/faang/Example.java
@@ -1,4 +1,18 @@
 package school.faang;
 
+
 public class Example {
+
+    public static void reverse(int[] nums) {
+        if (nums == null) {
+            return;
+        }
+
+        int length = nums.length;
+        for (int i = 0; i < length / 2; i++) {
+            int temp = nums[i];
+            nums[i] = nums[length - i - 1];
+            nums[length - i - 1] = temp;
+        }
+    }
 }
--- src/main/java/school/faang/Main.java
+++ src/main/java/school/faang/Main.java
@@ -1,4 +1,15 @@
 package school.faang;
 
+import java.util.Arrays;
+
 public class Main {
+
+    public static void main(String[] args) {
+
+        int[] massive = {1, 2, 3, 4, 5, 6, 7};
+        Example.reverse(massive);
+        System.out.println(Arrays.toString(massive));
+    }
+
+
 }
