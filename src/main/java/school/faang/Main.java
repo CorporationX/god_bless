2122f2a7099f845a4518d6d56ab745873723d4ac
mrnght <misterday116@gmail.com>
2025-06-04 11:44:52 +0800

    implemented a list of users and their hobbies

--- src/main/java/school/faang/Main.java
+++ src/main/java/school/faang/Main.java
@@ -1,4 +1,20 @@
 package school.faang;
 
+import java.util.*;
+
 public class Main {
+    public static void main(String[] args) {
+        List<User> users = List.of(new User(1, "John", 25, new HashSet<>(Set.of("swim", "jumping"))),
+                new User(2, "Sanni", 30, new HashSet<>(Set.of("rodeo", "running"))),
+                new User(3, "Peter", 23, new HashSet<>(Set.of("sky diving", "photographing"))),
+                new User(4, "Josh", 28, new HashSet<>(Set.of("sky diving", "ping pong"))),
+                new User(5, "Hanna", 23, new HashSet<>(Set.of("tennis", "running"))));
+
+        Set<String> hobbies = Set.of("ping pong", "sky diving");
+
+        Map<User, String> userHobbiesMap = User.findHobbyLovers(users, hobbies);
+        for (var userHobby : userHobbiesMap.entrySet()) {
+            System.out.println(userHobby.getKey().getName() + ", " + userHobby.getValue());
+        }
+    }
 }
