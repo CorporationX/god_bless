0e07a3222490e9c20860a81d05262f2787977e00
Safixl <nikitin.v2407@gmail.com>
2025-06-04 23:50:19 +0300

    Реализация всего функционала (Ловим события)

--- src/main/java/school/faang/catching_events/HogwartsSpells.java
+++ src/main/java/school/faang/catching_events/HogwartsSpells.java
@@ -6,43 +6,43 @@ import java.util.List;
 import java.util.Map;
 
 public class HogwartsSpells {
-    private final Map<Integer, SpellEvent> SPELL_BY_ID = new HashMap<>();
-    private final Map<String, List<SpellEvent>> SPELL_BY_TYPE = new HashMap<>();
+    private final Map<Integer, SpellEvent> spellById = new HashMap<>();
+    private final Map<String, List<SpellEvent>> spellByType = new HashMap<>();
 
     public void addSpellEvent(String eventType, String actionDescription) {
         SpellEvent spellEvent = new SpellEvent(eventType, actionDescription);
-        SPELL_BY_ID.put(spellEvent.getId(), spellEvent);
-        if (SPELL_BY_TYPE.containsKey(eventType)) {
-            SPELL_BY_TYPE.get(eventType).add(spellEvent);
+        spellById.put(spellEvent.getId(), spellEvent);
+        if (spellByType.containsKey(eventType)) {
+            spellByType.get(eventType).add(spellEvent);
         } else {
-            SPELL_BY_TYPE.putIfAbsent(eventType, new ArrayList<>() {{
+            spellByType.putIfAbsent(eventType, new ArrayList<>() {{
                 add(spellEvent);
             }});
         }
     }
 
     public SpellEvent getSpellEventById(int id) {
-        return SPELL_BY_ID.get(id);
+        return spellById.get(id);
     }
 
     public List<SpellEvent> getSpellEventsByType(String eventType) throws IllegalArgumentException {
-        if (SPELL_BY_TYPE.containsKey(eventType)) {
-            return SPELL_BY_TYPE.get(eventType);
+        if (spellByType.containsKey(eventType)) {
+            return spellByType.get(eventType);
         } else {
             throw new IllegalArgumentException("Not event type");
         }
     }
 
     public void deleteSpellEvent(int id) {
-        if (SPELL_BY_ID.containsKey(id)) {
-            SPELL_BY_ID.remove(id);
+        if (spellById.containsKey(id)) {
+            spellById.remove(id);
         } else {
             throw new IllegalArgumentException("Not id");
         }
     }
 
     public void printAllSpellEvents() {
-        for (Map.Entry<Integer, SpellEvent> spell : SPELL_BY_ID.entrySet()) {
+        for (Map.Entry<Integer, SpellEvent> spell : spellById.entrySet()) {
             System.out.println(spell.getValue());
         }
         System.out.println("\n********************************************\n");
