65cc0b87e9543cf6c2cd9c929e129506a31b22c1
mrnght <misterday116@gmail.com>
2025-06-04 18:04:54 +0800

    implemented a spell storage and access system

--- src/main/java/school/faang/catcher/Main.java
+++ src/main/java/school/faang/catcher/Main.java
@@ -1,5 +1,27 @@
 package school.faang.catcher;
 
-public class Main {
+import java.util.List;
 
+public class Main {
+    public static void main(String[] args) {
+        HogwartsSpells.addSpellEvent("Атака", "Кастует огненный шар");
+        HogwartsSpells.addSpellEvent("Трансфигурация", "Превращает стекло в воду");
+        HogwartsSpells.addSpellEvent("Защита", "Энергетическая сфера");
+        HogwartsSpells.addSpellEvent("Защита", "Ледяной щит");
+
+        System.out.println("Заклинание по айди: " + HogwartsSpells.getSpellEventById(2).getEventType() + "\n");
+
+        List<SpellEvent> spellEvents = (HogwartsSpells.getSpellEventsByType("Защита"));
+        System.out.print("Заклинание по типу: ");
+        for (SpellEvent event : spellEvents) {
+            System.out.print(event.getAction() + "\t");
+        }
+        System.out.println("\n");
+
+        System.out.println("До удаления по id: ");
+        HogwartsSpells.printAllSpellEvents();
+        HogwartsSpells.deleteSpellEvent(2);
+        System.out.println("После удаления по id: ");
+        HogwartsSpells.printAllSpellEvents();
+    }
 }
--- src/main/java/school/faang/catcher/SpellEvent.java
+++ src/main/java/school/faang/catcher/SpellEvent.java
@@ -1,12 +1,16 @@
 package school.faang.catcher;
 
 import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.ToString;
 
+@ToString
+@Getter
+@EqualsAndHashCode
 @AllArgsConstructor
 public class SpellEvent {
     private int id;
     private String eventType;
     private String action;
-
-
 }
