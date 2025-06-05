3e253e72e5fe2d9dfac4bec7a50f5d3f4a1928a3
JasonRon <ubervladocity@gmail.com>
2025-06-05 23:06:11 +0300

    Bugfix

--- src/main/java/school/faang/bjs2_79975/Main.java
+++ src/main/java/school/faang/bjs2_79975/Main.java
@@ -4,7 +4,7 @@ public class Main {
     public static void main(String[] args) {
         LibrarySystem.addBook("Гарри", "Джоан", 20, "6-zona");
         LibrarySystem.addBook("Хоббит", "Джон Рональд", 40, "4-zona");
-        LibrarySystem.addBook(null, null, 0, "null");
+        LibrarySystem.addBook(null, null, 0, null);
         LibrarySystem.addBook("Идиот", "Достоевский", 100, "1-zona");
         LibrarySystem.printAllBooks();
         LibrarySystem.findBook("Хоббит", "Джон Рональд", 40);
@@ -12,7 +12,5 @@ public class Main {
         LibrarySystem.findBook("Хоббит", "Джон Рональд", 40);
         LibrarySystem.printAllBooks();
         LibrarySystem.removeBook(null, null, 0);
-        LibrarySystem.removeBook(null, null, 0);
-
     }
 }
