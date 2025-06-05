7221d8c1fc5ce58219438cfb6895a01b90c60508
myrzakhmet <smagul.myrzakhmet@mail.ru>
2025-06-06 01:43:59 +0500

    removed equals and hashCode methods, added annotation

--- src/main/java/school/faang/bjs2_79677/Book.java
+++ src/main/java/school/faang/bjs2_79677/Book.java
@@ -1,5 +1,6 @@
 package school.faang.bjs2_79677;
 
+import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import lombok.Setter;
@@ -12,41 +13,9 @@ import java.util.Objects;
 @Setter
 @RequiredArgsConstructor
 @ToString
+@EqualsAndHashCode
 public class Book {
     private final String title;
     private final String author;
     private final int year;
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) {
-            return true;
-        }
-        if (o == null || getClass() != o.getClass()) {
-            return false;
-        }
-
-        Book book = (Book) o;
-        if (year != book.year) {
-            return false;
-        }
-
-        if (title != null ? !title.equals(book.title) : book.title != null) {
-            return false;
-        }
-
-        if (author != null ? !author.equals(book.author) : book.author != null) {
-            return false;
-        }
-
-        return true;
-    }
-
-    @Override
-    public int hashCode() {
-        int result = title != null ? title.hashCode() : 0;
-        result = 31 * result + (author != null ? author.hashCode() : 0);
-        result = 31 * result + year;
-        return result;
-    }
 }
