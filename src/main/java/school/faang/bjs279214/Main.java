e4bdfa5bdccb60c020794d3d3590ccdfc882b8b3
Dum <dimak22858@gmail.com>
2025-06-05 04:57:22 +0200

    BJS2-79214 Как настроение? что нового в жизни? Если ты это читаешь то спасибо что проверяешь и даешь оценку. Очень ценю

--- src/main/java/school/faang/bjs279214/Main.java
+++ src/main/java/school/faang/bjs279214/Main.java
@@ -4,7 +4,8 @@ public class Main {
     public static void main(String[] args) {
         Character war = new Warrior("Dimon");
         Character arch = new Archer("Silf");
-        for (int i = 0; i <= 12; i++)
+        for (int i = 0; i <= 12; i++) {
             war.attack(arch);
+        }
     }
 }
