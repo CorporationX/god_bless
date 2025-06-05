8869d9db9a9be439bf61d2348eda53f2a85e517c
Safixl <nikitin.v2407@gmail.com>
2025-06-05 13:39:03 +0300

    Реализация функционала (Развернуть)

--- src/main/java/school/faang/the_library_of_westeros/LibrarySystem.java
+++ src/main/java/school/faang/the_library_of_westeros/LibrarySystem.java
@@ -4,24 +4,24 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class LibrarySystem {
-    private final Map<Book, String> library = new HashMap<>();
+    private final Map<Book, String> LIBRARY = new HashMap<>();
 
     public void addBook(String title, String author, int year, String location) {
-        library.putIfAbsent(new Book(title, author, year), location);
+        LIBRARY.putIfAbsent(new Book(title, author, year), location);
     }
 
     public void removeBook(String title, String author, int year) {
-        library.remove(new Book(title, author, year));
+        LIBRARY.remove(new Book(title, author, year));
     }
 
     public void findBook(String title, String author, int year) {
-        System.out.println(library.getOrDefault(new Book(title, author, year), String.format("\"Book %s not found\"", title)));
+        System.out.println(LIBRARY.getOrDefault(new Book(title, author, year), String.format("\"Book %s not found\"", title)));
     }
 
     public void printAllBooks() {
-        for (Book book : library.keySet()) {
-            System.out.printf("Книга: %s (Автор: %s, Год: %d) - Расположение: %s%n",
-                    book.getTitle(), book.getAuthor(), book.getYear(), library.get(book));
+        for (Book book : LIBRARY.keySet()) {
+            System.out.printf("Title: %s (Author: %s, Year: %d) - Location: %s%n",
+                    book.getTitle(), book.getAuthor(), book.getYear(), LIBRARY.get(book));
         }
     }
 }
