54462f05fb4851d03fc1e623fe37ecfd85feec87
Jevgeni Rogotski <jevgeni.rogotski@gmail.com>
2025-06-05 18:22:06 +0300

    Array to array list

--- src/main/java/school/faang/bjs2_79157/Main.java
+++ src/main/java/school/faang/bjs2_79157/Main.java
@@ -1,34 +1,35 @@
 package school.faang.bjs2_79157;
 
+import java.util.List;
 import java.util.Random;
 
 public class Main {
     public static void main(String[] args) {
-        Character[] fighters = {
+        List<Character> fighters = List.of(
                 new Warrior("Berserk"),
                 new Warrior("Viking"),
                 new Archer("Robin"),
                 new Archer("Legolas")
-        };
+        );
 
         doBattle(fighters, 10);
     }
 
-    public static void doBattle(Character[] fighters, int rounds) {
+    public static void doBattle(List<Character> fighters, int rounds) {
         System.out.println("📣 Battle started! 📣");
 
         for (int r = 0; r < rounds; r++) {
             System.out.println("Round " + (r + 1));
 
             Random random = new Random();
-            int attackerIndex = random.nextInt(fighters.length);
-            int opponentIndex = random.nextInt(fighters.length);
+            int attackerIndex = random.nextInt(fighters.size());
+            int opponentIndex = random.nextInt(fighters.size());
 
             while (attackerIndex == opponentIndex) {
-                attackerIndex = random.nextInt(fighters.length);
+                attackerIndex = random.nextInt(fighters.size());
             }
 
-            fighters[attackerIndex].attack(fighters[opponentIndex]);
+            fighters.get(attackerIndex).attack(fighters.get(opponentIndex));
         }
     }
 }
