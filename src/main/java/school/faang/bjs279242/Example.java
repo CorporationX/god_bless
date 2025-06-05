af2df3184b25c77bd52ef12e1666ae7f3f21f485
DimonDem123 <DIMAK22858@GMAIL.COM>
2025-06-06 00:22:43 +0200

    Update Example.java

--- src/main/java/school/faang/bjs279242/Example.java
+++ src/main/java/school/faang/bjs279242/Example.java
@@ -3,7 +3,7 @@ package school.faang.bjs279242;
 public class Example {
     public int[] revers(int[] array) {
         int[] result = new int[array.length];
-        for(int i = 1; i < array.length; i++) {
+        for (int i = 1; i < array.length; i++) {
             result[i] = array[array.length - i];
         }
         return result;
