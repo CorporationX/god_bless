4ce37270bdd938a1c5560943288383b633c4ab17
Jevgeni Rogotski <jevgeni.rogotski@gmail.com>
2025-06-05 18:33:35 +0300

    Better logging

--- src/main/java/school/faang/bjs2_79687_spell_events/Main.java
+++ src/main/java/school/faang/bjs2_79687_spell_events/Main.java
@@ -1,7 +1,10 @@
 package school.faang.bjs2_79687_spell_events;
 
+import lombok.extern.slf4j.Slf4j;
+
 import java.util.List;
 
+@Slf4j
 public class Main {
     public static void main(String[] args) {
         HogwartsSpells.addSpellEvent("Buff", "Strength bless");
@@ -10,25 +13,25 @@ public class Main {
         SpellEvent hex = HogwartsSpells.addSpellEvent("Hex", "Strength curse");
         HogwartsSpells.printAllSpellEvents();
 
-        System.out.println("\n- Ensure spell deletion deletes from both registries. -");
+        log.info("Ensure spell deletion deletes from both registries.");
         HogwartsSpells.deleteSpellEvent(hex.getId());
         HogwartsSpells.printAllSpellEvents();
         List<SpellEvent> spells = HogwartsSpells.getSpellEventsByType("hex");
-        System.out.println("Found " + spells.size() + " hex type spells");
+        log.info("Found " + spells.size() + " hex type spells");
 
-        System.out.println("\n- Print only evocation spells -");
+        log.info("Print only evocation spells");
         spells = HogwartsSpells.getSpellEventsByType("Evocation");
         spells.forEach(System.out::println);
 
-        System.out.println("\n- Print only buff spells -");
+        log.info("Print only buff spells");
         spells = HogwartsSpells.getSpellEventsByType("Buff");
         spells.forEach(System.out::println);
 
-        System.out.println("\n- Find 'non-existent' type spells -");
+        log.info("Find 'non-existent' type spells");
         spells = HogwartsSpells.getSpellEventsByType("non-existent");
-        System.out.println("Found " + spells.size() + " 'non-existent' type spells");
+        log.info("Found " + spells.size() + " 'non-existent' type spells");
 
-        System.out.println("\n- Print spell from registry with id = 1 -");
-        System.out.println(HogwartsSpells.getSpellEventById(1));
+        log.info("Print spell from registry with id = 1");
+        log.info("Found {}", HogwartsSpells.getSpellEventById(1));
     }
 }
