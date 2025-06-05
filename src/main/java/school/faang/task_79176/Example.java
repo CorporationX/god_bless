ffa403c59427cda7328900fd67de675510e9a9b8
Pavel Hohlov <hohlov.ps95@yandex.ru>
2025-06-05 23:25:40 +0300

    fix

--- src/main/java/school/faang/task_79176/Example.java
+++ src/main/java/school/faang/task_79176/Example.java
@@ -8,11 +8,11 @@ import java.util.List;
 
 @NoArgsConstructor
 public class Example {
-    public List<Integer> reverse (List<Integer> array){
+    public List<Integer> reverse(List<Integer> array) {
         ArrayList<Integer> resultArray = new ArrayList<>();
         int arraySize = array.size();
         int i = 0;
-        while (i < array.size()){
+        while (i < array.size()) {
             resultArray.add(array.get(arraySize - 1));
             arraySize--;
             i++;
