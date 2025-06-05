81dbe1ba69616440d289b4b658bfb8f6ff706cb7
Ivi-Anastasia <qwwsoul@gmail.com>
2025-06-06 00:31:56 +0100

    1st Task complete

---
+++ src/main/java/school/faang/bjs2_79211/Main.java
@@ -0,0 +1,32 @@
+package school.faang.bjs2_79211;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+public class Main {
+    public static void main(String[] args) {
+        User Ana = new User("Ana", 21);
+        User Max = new User("Max", 21);
+        User Dina = new User("Dina", 17);
+        User Kate = new User("Kate", 41);
+        User Den = new User("Den", 31);
+
+        List<User> userList = new ArrayList<>();
+        userList.add(Ana);
+        userList.add(Max);
+        userList.add(Dina);
+        userList.add(Kate);
+        userList.add(Den);
+
+        Map<Integer, List<User>> resultUserList = User.groupUsers(userList);
+
+        for (Map.Entry<Integer, List<User>> entry : resultUserList.entrySet()){
+            System.out.print("Age group: " + entry.getKey() + ", users: ");
+            for (User user: entry.getValue()) {
+                System.out.print(user.getName());
+            }
+            System.out.println();
+        }
+    }
+}
