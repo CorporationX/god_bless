dace88a0c6c63ad7ab2f1945647658e512ae734d
Dmitrii <dmitriimaiorov.dev@gmail.com>
2025-06-06 00:00:49 +0200

    сделал вторую задачу в первом спринте, проба пера так сказать

---
+++ src/main/java/school/faang/bjs2_79272/User.java
@@ -0,0 +1,38 @@
+package school.faang.bjs2_79272;
+
+import lombok.Getter;
+
+import java.util.Set;
+
+@Getter
+public class User {
+    private String name;
+    private int age;
+    private String job;
+    private String address;
+
+
+    public static final Set<String> VALID_JOBS = Set.of("Google", "Uber", "Amazon");
+    public static final Set<String> VALID_ADDRESSES = Set.of("London", "New York", "Amsterdam");
+
+
+    public User(String name, int age, String job, String address) {
+        if (name == null || name.trim().isEmpty()) {
+            throw new IllegalArgumentException("Имя не может быть пустым");
+        }
+        if (age < 18) {
+            throw new IllegalArgumentException("Возраст должен быть не меньше 18");
+        }
+        if (!VALID_JOBS.contains(job)) {
+            throw new IllegalArgumentException("недопустимая работа " + job);
+        }
+        if (!VALID_ADDRESSES.contains(address)) {
+            throw new IllegalArgumentException("недопустимый адрес " + address);
+        }
+
+        this.name = name;
+        this.age = age;
+        this.job = job;
+        this.address = address;
+    }
+}
