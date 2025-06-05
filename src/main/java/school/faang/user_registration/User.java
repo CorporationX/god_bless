d0e6f9323aca989cc69d5de2a87fc8b3dd4bb9e9
Safixl <nikitin.v2407@gmail.com>
2025-06-05 12:39:13 +0300

    Реализация функционала (Развернуть)

--- src/main/java/school/faang/user_registration/User.java
+++ src/main/java/school/faang/user_registration/User.java
@@ -1,23 +1,23 @@
 package school.faang.user_registration;
 
 import lombok.Data;
-import java.util.List;
+import java.util.Set;
 
 @Data
 public class User {
+    public final Set<String> VALID_JOBS = Set.of("Google", "Uber", "Amazon");
+    public final Set<String> VALID_ADDRESSES = Set.of("London", "New York", "Amsterdam");
+
     private String name;
     private int age;
     private String job;
     private String address;
 
-    public final List<String> VALID_JOBS = List.of("Google", "Uber", "Amazon");
-    public final List<String> VALID_ADDRESSES = List.of("London", "New York", "Amsterdam");
-
     public User(String name, int age, String job, String address) {
+        this.name = name;
         if (name.isEmpty() || age < 18 || !VALID_JOBS.contains(job) || !VALID_ADDRESSES.contains(address)) {
-            throw new IllegalArgumentException("Вы не прошли валидацию");
+            throw new IllegalArgumentException( name +" вы не прошли валидацию");
         } else {
-            this.name = name;
             this.age = age;
             this.job = job;
             this.address = address;
