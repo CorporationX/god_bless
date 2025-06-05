fef4a1b9964763da961b421b7120326f0f4aafa7
Setched <kir32kir@gmail.com>
2025-06-05 21:51:48 +0300

    Исправлены навзвания переменных, добавлено логгирование, добавлен пропущенный модификатор доступа

--- src/main/java/school/faang/the_library_of_westeros_bjs2_79622/LibrarySystem.java
+++ src/main/java/school/faang/the_library_of_westeros_bjs2_79622/LibrarySystem.java
@@ -1,36 +1,49 @@
 package school.faang.the_library_of_westeros_bjs2_79622;
 
+import lombok.extern.slf4j.Slf4j;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Set;
 
+@Slf4j
 public class LibrarySystem {
-    HashMap<Book, String> map = new HashMap<>();
+    private final HashMap<Book, String> library = new HashMap<>();
+    private static final Logger logger = LoggerFactory.getLogger(LibrarySystem.class);
 
     public void addBook(String title, String author, int year, String location) {
         Book book = new Book(title, author, year);
-        map.put(book, location);
+        library.put(book, location);
         System.out.printf("Книга %s добавлена\n", title);
     }
 
     public void removeBook(String title, String author, int year) {
-        Book keyToRemove = new Book(title, author, year);
-        map.remove(keyToRemove);
-        System.out.printf("Книга %s удалена\n", title);
+        Book bookToRemove = new Book(title, author, year);
+        Object removed = library.remove(bookToRemove);
+
+        if (removed == null) {
+            logger.warn("Книга не найдена в библиотеке\n");
+            System.out.printf("Книга %s не найдена\n", title);
+        } else {
+            logger.info("Книга успешно удалена\n");
+            System.out.printf("Книга %s удалена\n", title);
+        }
     }
 
     public void findBook(String title, String author, int year) {
-        Book keyToFind = new Book(title, author, year);
+        Book bookToFind = new Book(title, author, year);
 
-        if (map.get(keyToFind) == null) {
+        if (library.get(bookToFind) == null) {
             System.out.println("Книга не найдена\n");
         } else {
-            System.out.println(map.get(keyToFind));
+            System.out.println(library.get(bookToFind));
         }
     }
 
     public void printAllBooks() {
-        Set<Map.Entry<Book, String>> entries = map.entrySet();
+        Set<Map.Entry<Book, String>> entries = library.entrySet();
 
         for (Map.Entry<Book, String> entry : entries) {
             Book key = entry.getKey();
--- src/main/java/school/faang/the_library_of_westeros_bjs2_79622/Main.java
+++ src/main/java/school/faang/the_library_of_westeros_bjs2_79622/Main.java
@@ -2,16 +2,16 @@ package school.faang.the_library_of_westeros_bjs2_79622;
 
 public class Main {
     public static void main(String[] args) {
-        LibrarySystem ls1 = new LibrarySystem();
-        ls1.addBook("title1", "author1", 2000, "Sector D");
-        ls1.addBook("title2", "author2", 2001, "Sector B");
-        ls1.addBook("title3", "author3", 2002, "Sector D");
-        ls1.addBook("title4", "author4", 2003, "Sector C");
+        LibrarySystem ls = new LibrarySystem();
+        ls.addBook("title1", "author1", 2000, "Sector D");
+        ls.addBook("title2", "author2", 2001, "Sector B");
+        ls.addBook("title3", "author3", 2002, "Sector D");
+        ls.addBook("title4", "author4", 2003, "Sector C");
 
-        ls1.removeBook("title4", "author4", 2003);
+        ls.removeBook("title4", "author4", 2003);
 
-        ls1.findBook("title2", "author2", 2001);
+        ls.findBook("title2", "author2", 2001);
 
-        ls1.printAllBooks();
+        ls.printAllBooks();
     }
 }
