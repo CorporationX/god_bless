54018b92adc3acf60b439b437b3cb4454fab06ca
Reverend17 <marklajer1@gmail.com>
2025-06-05 19:27:45 +0300

    array reverse

---
+++ src/main/java/school/faang/bjs2_79232/Example.java
@@ -0,0 +1,13 @@
+package school.faang.bjs2_79232;
+
+public class Example {
+    public static int[] reverse(int[] array) {
+        int temp;
+        for (int i = 0; i < array.length / 2; i++) {
+            temp = array[i];
+            array[i] = array[array.length - 1 - i];
+            array[array.length - 1 - i] = temp;
+        }
+        return array;
+    }
+}
