d3bb1121b0088c65558312856b59f6e04ac2804f
Safixl <nikitin.v2407@gmail.com>
2025-06-05 12:18:51 +0300

    Реализация функционала (Развернуть)

--- src/main/java/school/faang/turn_around/Example.java
+++ src/main/java/school/faang/turn_around/Example.java
@@ -3,6 +3,10 @@ package school.faang.turn_around;
 public class Example {
 
     public int[] revers(int[] array) {
-        return revers(array);
+        int[] result = new int[array.length];
+        for (int i = 0; i < array.length; i++) {
+            result[i] = array[array.length - i - 1];
+        }
+        return result;
     }
 }
