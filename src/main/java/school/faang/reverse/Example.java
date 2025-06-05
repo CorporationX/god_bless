faf5073bd1656868ca9dbe53ca12abb4e24d6f73
yagor. <warneverchang3s@gmail.com>
2025-06-05 13:43:01 +0300

    Развернитесь! - переделал без создания нового массива

--- src/main/java/school/faang/reverse/Example.java
+++ src/main/java/school/faang/reverse/Example.java
@@ -3,12 +3,11 @@ package school.faang.reverse;
 public class Example {
 
     public static int[] reverse(int[] array) {
-        int k = 0;
-        int[] result = new int[array.length];
-        for (int i = array.length; i > 0; i--) {
-            result[k] = array[i - 1];
-            k++;
+        for (int i = 0; array.length / 2 > i; i++) {
+            int temp = array[i];
+            array[i] = array[array.length - 1 - i];
+            array[array.length - 1 - i] = temp;
         }
-        return result;
+        return array;
     }
 }
