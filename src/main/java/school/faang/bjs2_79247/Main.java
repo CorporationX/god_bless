4dce0cdd2e0b4be8e06e339c98291c3453e3a0e4
White <ilyakrivitskiyn@gmail.com>
2025-06-06 21:07:12 +0300

    Create class Example and reverse method

---
+++ src/main/java/school/faang/bjs2_79247/Main.java
@@ -0,0 +1,11 @@
+package school.faang.bjs2_79247;
+
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) {
+        int[] numbs = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+        new Example().reverse(numbs);
+        System.out.println(Arrays.toString(numbs));
+    }
+}
