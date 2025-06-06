1978930f190f568a37e8dc619f6b5a1a3fa61aa7
FrolovSI <frolovlab024@gmail.com>
2025-06-06 15:48:48 +0300

    group users BJS2-79152

---
+++ src/main/java/school/faang/bjc2_79152/User.java
@@ -0,0 +1,38 @@
+package school.faang.bjc2_79152;
+
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.ToString;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@EqualsAndHashCode
+@AllArgsConstructor
+@Getter
+@ToString
+public class User {
+    private String name;
+    private int age;
+    private String placeOfWork;
+    private String address;
+
+    public static Map<Integer, List<User>> groupUsers(List<User> users) {
+        Map<Integer, List<User>> result = new HashMap<>();
+
+        for (User user : users) {
+            int age = user.getAge();
+
+            if (!result.containsKey(age)) {
+                result.put(age, new ArrayList<>());
+            }
+
+            result.get(age).add(user);
+        }
+
+        return result;
+    }
+}
