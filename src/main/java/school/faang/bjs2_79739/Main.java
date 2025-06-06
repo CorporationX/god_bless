76ad17fc646ff22e734cdfd3ef2dcc9c0faecc77
Viktor Romanenko <romanenkoviktor080@gmail.com>
2025-06-06 17:05:46 +0300

    fixes (code style)

--- src/main/java/school/faang/bjs2_79739/Main.java
+++ src/main/java/school/faang/bjs2_79739/Main.java
@@ -2,18 +2,18 @@ package school.faang.bjs2_79739;
 
 public class Main {
     public static void main(String[] args) {
-        OptimizationStrategy efficiencyOptimizationStrategy = new EnergyEfficiencyOptimizationStrategy();
-        OptimizationStrategy loadBalancingOptimizationStrategy = new LoadBalancingOptimizationStrategy();
         DataCenterService service = new DataCenterService();
         DataCenter dataCenter = new DataCenter();
         service.addServer(dataCenter, new Server(0));
         service.addServer(dataCenter, new Server(4));
         service.addServer(dataCenter, new Server(10));
 
+        OptimizationStrategy efficiencyOptimizationStrategy = new EnergyEfficiencyOptimizationStrategy();
         dataCenter.getServers().forEach(System.out::println);
         service.optimize(dataCenter, efficiencyOptimizationStrategy);
         dataCenter.getServers().forEach(System.out::println);
 
+        OptimizationStrategy loadBalancingOptimizationStrategy = new LoadBalancingOptimizationStrategy();
         service.addServer(dataCenter, new Server(31));
         Server server = new Server(75);
         service.addServer(dataCenter, server);
