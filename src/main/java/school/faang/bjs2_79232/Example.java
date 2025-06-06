2b3574b6ff6960ffd6e1aef860eb62ef1ef73335
Reverend17 <marklajer1@gmail.com>
2025-06-06 18:50:10 +0300

    array reverse fix

--- src/main/java/school/faang/bjs2_79232/Example.java
+++ src/main/java/school/faang/bjs2_79232/Example.java
@@ -3,6 +3,9 @@ package school.faang.bjs2_79232;
 public class Example {
     public static int[] reverse(int[] array) {
         int temp;
+        if (array == null || array.length <= 1) {
+            return array;
+        }
         for (int i = 0; i < array.length / 2; i++) {
             temp = array[i];
             array[i] = array[array.length - 1 - i];
