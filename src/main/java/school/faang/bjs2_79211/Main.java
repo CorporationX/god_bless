5e0ff6be3a9dbb4aa92a73239cdb7ed24a87b880
Ivi-Anastasia <qwwsoul@gmail.com>
2025-06-06 13:32:42 +0100

    Addressed feedback

--- src/main/java/school/faang/bjs2_79211/Main.java
+++ src/main/java/school/faang/bjs2_79211/Main.java
@@ -1,23 +1,13 @@
 package school.faang.bjs2_79211;
 
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 
 public class Main {
     public static void main(String[] args) {
-        User ana = new User("Ana", 21);
-        User max = new User("Max", 21);
-        User dina = new User("Dina", 17);
-        User kate = new User("Kate", 41);
-        User den = new User("Den", 31);
-
-        List<User> userList = new ArrayList<>();
-        userList.add(ana);
-        userList.add(max);
-        userList.add(dina);
-        userList.add(kate);
-        userList.add(den);
+        List<User> userList = List.of(new User("Ana", 21), new User("Max", 21),
+                                      new User("Dina", 17), new User("Kate", 41),
+                                      new User("Den", 31));
 
         Map<Integer, List<User>> resultUserList = User.groupUsers(userList);
 
--- src/main/java/school/faang/bjs2_79211/User.java
+++ src/main/java/school/faang/bjs2_79211/User.java
@@ -1,44 +1,20 @@
 package school.faang.bjs2_79211;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+@AllArgsConstructor
 public class User {
+    @Getter
     private String name;
+    @Getter
     private int age;
-    private String workplace;
-    private String address;
 
-    public User(String name, int age) {
-        this.name = name;
-        this.age = age;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public int getAge() {
-        return age;
-    }
-
-    public String getWorkplace() {
-        return workplace;
-    }
-
-    public void setWorkplace(String workplace) {
-        this.workplace = workplace;
-    }
-
-    public String getAddress() {
-        return address;
-    }
-
-    public void setAddress(String address) {
-        this.address = address;
-    }
 
     public static Map<Integer, List<User>> groupUsers(List<User> userList) {
         Map<Integer, List<User>> resultMap = new HashMap<>();
@@ -46,10 +22,9 @@ public class User {
         for (User user : userList) {
             if (!resultMap.containsKey(user.getAge())) {
                 resultMap.put(user.getAge(), new ArrayList<>());
-                resultMap.get(user.getAge()).add(user);
-            } else {
-                resultMap.get(user.getAge()).add(user);
             }
+            resultMap.get(user.getAge()).add(user);
+
         }
 
         return resultMap;
