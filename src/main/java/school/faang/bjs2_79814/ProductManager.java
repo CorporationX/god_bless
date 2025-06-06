b4728470d673dc9a01ea5a1f72bf66a54b534664
Viktor Romanenko <romanenkoviktor080@gmail.com>
2025-06-06 12:45:45 +0300

    fixes (code style)

--- src/main/java/school/faang/bjs2_79814/ProductManager.java
+++ src/main/java/school/faang/bjs2_79814/ProductManager.java
@@ -8,14 +8,15 @@ import java.util.concurrent.atomic.AtomicInteger;
 import java.util.stream.Collectors;
 
 public class ProductManager {
+    public static final AtomicInteger ID = new AtomicInteger(0);
+
     private final Set<Product> products = new HashSet<>();
 
-    public final static AtomicInteger id = new AtomicInteger(0);
-
     public Product addProduct(Category category, String name) {
-        Product product = new Product(id.incrementAndGet(), name, category);
+        Product product = new Product(ID.incrementAndGet(), name, category);
         //Но тут используется Set<Product>, все равно нужно делать проверку на наличие товара, для оптимизации?
-        //при добавлении он же просто перетрет уже находящийся товар и не будет дубликатов. IDE пишет "Unnecessary 'contains()' check"
+        //при добавлении он же просто перетрет уже находящийся товар и не будет дубликатов.
+        //IDE пишет "Unnecessary 'contains()' check"
         //id не используется в hashcode и equals
         boolean idAdded = products.add(product);
         System.out.println(idAdded ? "Товар добавлен" : "Товар уже в системе");
