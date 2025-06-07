4523d73d3f044059d38a0b0db53d8bd96cdab506
Reverend17 <marklajer1@gmail.com>
2025-06-07 15:52:54 +0300

    library

---
+++ src/main/java/school/faang/bjs2_79697/Library.java
@@ -0,0 +1,16 @@
+package school.faang.bjs2_79697;
+
+public class Library {
+    public static void main(String[] args) {
+        LibrarySystem library = new LibrarySystem();
+        library.addBook("title1", "author1", 2020, "стеллаж 1");
+        library.addBook("title2", "author2", 2021, "стеллаж 2");
+        library.addBook("title3", "author3", 2022, "стеллаж 3");
+
+        System.out.println(library.removeBook("title2", "author2", 2021));
+
+        System.out.println(library.findBook("title3", "author3", 2022));
+
+        library.printAllBooks();
+    }
+}
