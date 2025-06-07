7622573afe8bb42f31689876ab8e696b1649decd
Ivi-Anastasia <qwwsoul@gmail.com>
2025-06-07 20:38:29 +0100

    Initial commit with complete task

---
+++ src/main/java/school/faang/bjs2_79291/Main.java
@@ -0,0 +1,11 @@
+package school.faang.bjs2_79291;
+
+public class Main {
+    public static void main(String[] args) {
+        try {
+            User mark = new User("Mark", 15, "Google", "London");
+        } catch (IllegalArgumentException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+}
