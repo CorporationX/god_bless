494ed567e636f41a44b87065015bdbb21b554559
Linempy <mychinnik@yandex.ru>
2025-06-08 01:20:44 +0300

    hogwarts done

--- src/main/java/school/faang/Main.java
+++ src/main/java/school/faang/Main.java
@@ -1,4 +1,13 @@
 package school.faang;
 
 public class Main {
+    public static void main(String[] args) {
+        SpellCaster spellCaster = new SpellCaster();
+
+        spellCaster.cast("FireBall", nameSpell -> "FireBall3000");
+        spellCaster.cast("Левитация", nameSpell -> "Тут должно что-то полететь");
+        spellCaster.cast("Телепортация", nameSpell -> "Ты телепортировался");
+
+
+    }
 }
