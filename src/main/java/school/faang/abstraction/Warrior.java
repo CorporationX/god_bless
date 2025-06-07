5e74498193b2bd8256af4acbdb6aaaabea5afcf6
mrnght <misterday116@gmail.com>
2025-06-07 11:40:18 +0800

    fix warrior's constructor

--- src/main/java/school/faang/abstraction/Warrior.java
+++ src/main/java/school/faang/abstraction/Warrior.java
@@ -2,7 +2,7 @@ package school.faang.abstraction;
 
 public class Warrior extends Character {
 
-    public Warrior(String name, int strength, int agility, int intelligence) {
+    public Warrior(String name) {
         super(name, 10, 5, 3);
     }
 
