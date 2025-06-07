e358eb77f7a9c2d7c08dc14504169a987800684c
White <ilyakrivitskiyn@gmail.com>
2025-06-07 18:00:35 +0300

    Library of Vesteros BJS2-79703

---
+++ src/main/java/school/faang/bjs2_79703/Main.java
@@ -0,0 +1,24 @@
+package school.faang.bjs2_79703;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Main {
+    public static void main(String[] args) {
+        Map<Book, String> libraryMap = new HashMap<>() {
+            {
+                put(new Book("1", "1", 1), "1");
+                put(new Book("2", "2", 2), "2");
+                put(new Book("3", "3", 3), "3");
+                put(new Book("4", "4", 4), "4");
+            }
+        };
+
+        LibrarySystem librarySystem = new LibrarySystem(libraryMap);
+
+        librarySystem.addBook("5", "5", 5, "5");
+        librarySystem.removeBook("3", "3", 3);
+        System.out.println(librarySystem.findBook("2", "2", 2));
+        librarySystem.printAllBooks();
+    }
+}
