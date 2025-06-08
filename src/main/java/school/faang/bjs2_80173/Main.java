e16f591757e60edd28c6a22b12500e58338f8582
Linempy <mychinnik@yandex.ru>
2025-06-08 11:13:53 +0300

    BJS2-80173 done

--- src/main/java/school/faang/bjs2_80173/Main.java
+++ src/main/java/school/faang/bjs2_80173/Main.java
@@ -1,4 +1,4 @@
-package school.faang;
+package school.faang.bjs2_80173;
 
 public class Main {
     public static void main(String[] args) {
@@ -7,7 +7,5 @@ public class Main {
         spellCaster.cast("FireBall", nameSpell -> "FireBall3000");
         spellCaster.cast("Левитация", nameSpell -> "Тут должно что-то полететь");
         spellCaster.cast("Телепортация", nameSpell -> "Ты телепортировался");
-
-
     }
 }
--- src/main/java/school/faang/bjs2_80173/SpellCaster.java
+++ src/main/java/school/faang/bjs2_80173/SpellCaster.java
@@ -1,4 +1,4 @@
-package school.faang;
+package school.faang.bjs2_80173;
 
 import lombok.extern.slf4j.Slf4j;
 
