55b86508c9b715b465f36e85f5208c06cd918c98
Amirzhan Alimov <amirzhan.alimov@yandex.ru>
2025-06-09 22:37:10 +0500

    implemented BJS2-79226

---
+++ src/main/java/school/faang/BJS2_79226/model/User.java
@@ -0,0 +1,28 @@
+package school.faang.BJS2_79226.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import java.util.*;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class User {
+    private String name;
+    private int age;
+    private String workplace;
+    private String address;
+
+    public static Map<Integer, List<User>> groupUsers(List<User> users) {
+        var usersByAge = new HashMap<Integer, List<User>>();
+        for (var user : users) {
+            if (!usersByAge.containsKey(user.age)) {
+                usersByAge.put(user.age, new ArrayList<>());
+            }
+            usersByAge.get(user.age).add(user);
+        }
+        return usersByAge;
+        // return users.stream().collect(Collectors.groupingBy(User::getAge));
+    }
+}
