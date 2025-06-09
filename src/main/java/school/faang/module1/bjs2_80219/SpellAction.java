fb25b1af6e8d9cb59dec1eeaa8388abe34034d09
ddddddasha <daria.zhelobkovich@gmail.com>
2025-06-09 21:39:01 +0300

    Создан функциональный интерфейс SpellAction. Исправлено

--- src/main/java/school/faang/module1/bjs2_80219/SpellAction.java
+++ src/main/java/school/faang/module1/bjs2_80219/SpellAction.java
@@ -2,5 +2,5 @@ package school.faang.module1.bjs2_80219;
 
 @FunctionalInterface
 public interface SpellAction {
-    String castSpell(String spellName);
+    String cast(String spellName);
 }
--- src/main/java/school/faang/module1/bjs2_80219/SpellCaster.java
+++ src/main/java/school/faang/module1/bjs2_80219/SpellCaster.java
@@ -5,6 +5,6 @@ import lombok.extern.slf4j.Slf4j;
 @Slf4j
 public class SpellCaster {
     public void cast(String spellName, SpellAction spellAction) {
-        log.info(spellAction.castSpell(spellName));
+        log.info(spellAction.cast(spellName));
     }
 }
