5c41a68a174d5de6724bd1d2d6d8d8fcb6aa12b9
Setched <kir32kir@gmail.com>
2025-06-09 15:52:13 +0300

    Исправил замечания

--- src/main/java/school/faang/cathing_events_bjs2_79681/HogwartsSpells.java
+++ src/main/java/school/faang/cathing_events_bjs2_79681/HogwartsSpells.java
@@ -1,14 +1,18 @@
 package school.faang.cathing_events_bjs2_79681;
 
+import lombok.Getter;
+import lombok.extern.slf4j.Slf4j;
+
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.Set;
 
+@Slf4j
 public class HogwartsSpells {
-    HashMap<Integer, SpellEvent> spellById = new HashMap<>();
-    HashMap<String, List<SpellEvent>> spellsByType = new HashMap<>();
+    private HashMap<Integer, SpellEvent> spellById = new HashMap<>();
+    private HashMap<String, List<SpellEvent>> spellsByType = new HashMap<>();
     private int nextId = 0;
 
     public int generateId() {
@@ -26,18 +30,33 @@ public class HogwartsSpells {
     }
 
     public SpellEvent getSpellEventById(int id) {
-        System.out.printf("Событие: %s\n", spellById.get(id).toString());
-        return spellById.get(id);
+        if (spellById.get(id) == null) {
+            log.warn("Такого события нет");
+            return null;
+        } else {
+            log.info("Событие по id: {}", spellById.get(id).toString());
+            return spellById.get(id);
+        }
     }
 
     public List<SpellEvent> getSpellEventsByType(String eventType) {
-        System.out.printf("События: %s\n", spellsByType.get(eventType).toString());
-        return spellsByType.get(eventType);
+        List<SpellEvent> events = spellsByType.get(eventType);
+        if (events == null) {
+            log.warn("Такого элемента нет: {}", eventType);
+            return Collections.emptyList();
+        }
+        System.out.printf("События по типу:\n", events);
+        return events;
     }
 
     public void deleteSpellEvent(int id) {
-        spellById.remove(id);
-        spellsByType.remove(spellById.get(id));
+        if (getSpellEventById(id) == null) {
+            log.warn("Такого id нет");
+        } else {
+            log.info("Элемент {} удален", id);
+            spellById.remove(id);
+            spellsByType.remove(spellById.get(id));
+        }
     }
 
     public void printAllSpellEvents() {
