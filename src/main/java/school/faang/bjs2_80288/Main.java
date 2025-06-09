bd8a4b8c1e9d47ab257b72533dee60d324fa42ea
Linempy <mychinnik@yandex.ru>
2025-06-09 11:55:03 +0300

    BJS2-80288 done

--- src/main/java/school/faang/bjs2_80288/Main.java
+++ src/main/java/school/faang/bjs2_80288/Main.java
@@ -11,14 +11,14 @@ public class Main {
         InventoryManager inventoryManager = new InventoryManager();
         Character hero = new Character("Steve");
 
-        Item item1 = new Item("Земля", 64);
-        Item item2 = new Item("Булыжник", 32);
-        Item item3 = new Item("Семечка", 12);
-        Item item4 = new Item("Грязь", 10);
+        Item earth = new Item("Земля", 64);
+        Item cobblestone = new Item("Булыжник", 32);
+        Item seed = new Item("Семечка", 12);
+        Item dirt = new Item("Грязь", 10);
 
         Consumer<Item> action = item -> log.info("Предмет %s был добавлен в инвентарь".formatted(item.getName()));
 
-        inventoryManager.addItem(hero, item1, action);
+        inventoryManager.addItem(hero, earth, action);
         inventoryManager.removeItem(hero, item -> item.getValue() == 13);
 
         inventoryManager.updateItem(hero,
@@ -26,9 +26,11 @@ public class Main {
                 item -> new Item("Грязь", 1)
         );
 
-        inventoryManager.addItem(hero, item4, action);
-        inventoryManager.addItem(hero, item3, action);
-        inventoryManager.addItem(hero, item2, action);
+        inventoryManager.addItem(hero, dirt, action);
+        inventoryManager.addItem(hero, seed, action);
+        inventoryManager.addItem(hero, cobblestone, action);
+        inventoryManager.removeItem(hero, item -> item.getValue() == 10);
+
 
         hero.getInventory().forEach(
                 item -> log.info("Предмет: %s; Кол-во: %s".formatted(item.getName(), item.getValue()))
