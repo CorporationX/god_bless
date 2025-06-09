71bd9005e9098a061b59ecb11da045f360e0f3b3
licosty <d.chigantseva@gmail.com>
2025-06-09 16:49:06 +0300

    реализованы фильтры Instagram

---
+++ src/main/java/school/faang/bjs2_80432/Image.java
@@ -0,0 +1,13 @@
+package school.faang.bjs2_80432;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.RequiredArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@RequiredArgsConstructor
+public class Image {
+    private final String name;
+    private String description;
+}
