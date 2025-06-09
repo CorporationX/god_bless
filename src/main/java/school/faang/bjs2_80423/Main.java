6273f4448503d1b2d676dc3da2c9fa35d4264a25
itechnik <itechnik-swd@gmail.com>
2025-06-09 16:03:52 +0300

    fix errors checkstyle

--- src/main/java/school/faang/bjs2_80423/Main.java
+++ src/main/java/school/faang/bjs2_80423/Main.java
@@ -19,12 +19,14 @@ public class Main {
         System.out.println(grayscaleImage.getDescription()); // Вывод: Оригинальное изображение | Фильтр: черно-белый
 
         Image sepiaImage = filterProcessor.applyFilter(grayscaleImage, sepiaFilter);
-        System.out.println(sepiaImage.getDescription()); // Вывод: Оригинальное изображение | Фильтр: черно-белый | Фильтр: сепия
+        System.out.println(sepiaImage.getDescription());
+        // Вывод: Оригинальное изображение | Фильтр: черно-белый | Фильтр: сепия
 
         // Комбинирование фильтров
         Function<Image, Image> combinedFilter = filterProcessor.combineFilters(grayscaleFilter, sepiaFilter);
         Image combinedImage = filterProcessor.applyFilter(originalImage, combinedFilter);
-        System.out.println(combinedImage.getDescription()); // Вывод: Оригинальное изображение | Фильтр: черно-белый | Фильтр: сепия
+        System.out.println(combinedImage.getDescription());
+        // Вывод: Оригинальное изображение | Фильтр: черно-белый | Фильтр: сепия
 
     }
 }
