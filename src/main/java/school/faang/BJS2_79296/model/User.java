394476e131a9e3d6a43dd7b5bab53a1d6707e392
Amirzhan Alimov <amirzhan.alimov@yandex.ru>
2025-06-10 03:38:44 +0500

    Used hints

--- src/main/java/school/faang/BJS2_79296/model/User.java
+++ src/main/java/school/faang/BJS2_79296/model/User.java
@@ -7,18 +7,18 @@ import java.util.*;
 public class User {
     private static final Set<String> VALID_JOBS = Set.of("Google", "Uber", "Amazon");
     private static final Set<String> VALID_ADDRESSES = Set.of("London", "New York", "Amsterdam");
-
+    private static final int MIN_AGE = 18;
     private String name;
     private int age;
     private String job;
     private String address;
 
     public User(String name, int age, String job, String address) {
-        if (name == null || name.trim().isEmpty()) {
+        if (name == null || name.isBlank()) {
             String errorMessage = "Name cannot be null or empty.";
             throw new IllegalArgumentException(errorMessage);
         }
-        if (age < 18) {
+        if (age < MIN_AGE) {
             String errorMessage = "Age cannot be lower than 18.";
             throw new IllegalArgumentException(errorMessage);
         }
