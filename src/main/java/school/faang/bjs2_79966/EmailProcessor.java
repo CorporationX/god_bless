8bd16df74da4b827e0d85fac9a212d40dff56103
Viktor Romanenko <romanenkoviktor080@gmail.com>
2025-06-10 18:59:55 +0300

    BJS2-79966

---
+++ src/main/java/school/faang/bjs2_79966/EmailProcessor.java
@@ -0,0 +1,24 @@
+package school.faang.bjs2_79966;
+
+import java.util.List;
+import java.util.function.Consumer;
+import java.util.function.Function;
+import java.util.function.Predicate;
+
+public class EmailProcessor {
+    public void processEmails(
+            List<Email> emails,
+            Predicate<Email> filter,
+            Consumer<Email> handler,
+            Function<Email, String> converter
+    ) {
+        emails.forEach(email -> {
+            if (filter.test(email)) {
+                handler.accept(email);
+                String updatedBody = converter.apply(email);
+                email.setBody(updatedBody);
+            }
+        });
+
+    }
+}
