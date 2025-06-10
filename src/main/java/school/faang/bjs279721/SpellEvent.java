3e28919db23be985ba678730f4b06e6769e2da06
Dum <dimak22858@gmail.com>
2025-06-10 15:21:29 +0200

    BJS2-79877 добавл @Data вместо ручного переопределения equals and hashCode

--- src/main/java/school/faang/bjs279721/SpellEvent.java
+++ src/main/java/school/faang/bjs279721/SpellEvent.java
@@ -1,32 +1,12 @@
 package school.faang.bjs279721;
 
 import lombok.AllArgsConstructor;
-import lombok.Getter;
-
-import java.util.Objects;
+import lombok.Data;
 
 @AllArgsConstructor
-@Getter
+@Data
 public class SpellEvent {
     private long id;
     private String eventType;
     private String action;
-
-    @Override
-    public boolean equals(Object o) {
-        if (o == this) {
-            return true;
-        }
-        if (o == null || getClass() != o.getClass()) {
-            return false;
-        }
-        SpellEvent that = (SpellEvent) o;
-
-        return id == that.id && Objects.equals(eventType, that.eventType) && Objects.equals(action, that.action);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(id, eventType, action);
-    }
 }
