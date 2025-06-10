3f429d1c4d072afb9b5ab96a7d1fff6485faabe7
Jevgeni Rogotski <jevgeni.rogotski@gmail.com>
2025-06-10 20:55:44 +0300

    Stream API 2. Improvements and readability

--- src/main/java/school/faang/bjs2_80634_stream_2/ListOperations.java
+++ src/main/java/school/faang/bjs2_80634_stream_2/ListOperations.java
@@ -1,6 +1,7 @@
 package school.faang.bjs2_80634_stream_2;
 
-import java.util.ArrayList;
+import lombok.NonNull;
+
 import java.util.Comparator;
 import java.util.HashSet;
 import java.util.List;
@@ -10,17 +11,14 @@ import java.util.stream.Collectors;
 
 public class ListOperations {
 
-    public static Set<IntPair> uniqueSumPairs(Set<Integer> numbers, int targetSum) {
+    public static Set<IntPair> uniqueSumPairs(@NonNull Set<Integer> numbers, int targetSum) {
         Set<IntPair> pairs = new HashSet<>();
-        if (numbers == null) {
-            return pairs;
-        }
         Set<Integer> extraNumbers = new HashSet<>(numbers);
 
         numbers.forEach(number -> {
-            List<Integer> matches = extraNumbers.stream().filter(
-                    newNumber -> number + newNumber == targetSum
-            ).toList();
+            List<Integer> matches = extraNumbers.stream()
+                    .filter(newNumber -> number + newNumber == targetSum)
+                    .toList();
             matches.forEach(matchedNumber -> {
                 if (!matchedNumber.equals(number)) {
                     pairs.add(new IntPair(number, matchedNumber));
@@ -31,44 +29,40 @@ public class ListOperations {
         return pairs;
     }
 
-    public static List<String> capitalsSortedByCountry(Map<String, String> countries) {
-        if (countries == null) {
-            return new ArrayList<>();
-        }
-        return countries.entrySet().stream().sorted(Map.Entry.comparingByKey()).map(Map.Entry::getValue).toList();
+    public static List<String> capitalsSortedByCountry(@NonNull Map<String, String> countries) {
+        return countries.entrySet().stream()
+                .sorted(Map.Entry.comparingByKey())
+                .map(Map.Entry::getValue)
+                .toList();
     }
 
-    public static List<String> stringsSortedByLengthAndFilteredByStartingChar(List<String> strings, char target) {
-        if (strings == null) {
-            return new ArrayList<>();
-        }
-
-        return strings.stream().filter(
-                string -> string.startsWith(String.valueOf(target))
-        ).sorted(Comparator.comparingInt(String::length)).toList();
+    public static List<String> stringsSortedByLengthAndFilteredByStartingChar(
+            @NonNull List<String> strings,
+            char target) {
+        return strings.stream()
+                .filter(string -> string.startsWith(String.valueOf(target)))
+                .sorted(Comparator.comparingInt(String::length))
+                .toList();
     }
 
-    public static List<String> toBinary(List<Integer> numbers) {
-        if (numbers == null) {
-            return new ArrayList<>();
-        }
-
-        return numbers.stream().map(Integer::toBinaryString).toList();
+    public static List<String> toBinary(@NonNull List<Integer> numbers) {
+        return numbers.stream()
+                .map(Integer::toBinaryString)
+                .toList();
     }
 
-    public static List<String> sortAndFilterByAlphabet(List<String> strings, String alphabet) {
-        if (strings == null || alphabet == null) {
-            return new ArrayList<>();
-        }
-
+    public static List<String> sortAndFilterByAlphabet(@NonNull List<String> strings, @NonNull String alphabet) {
         Set<Character> alphabetSet = alphabet.chars()
                 .mapToObj(c -> (char) c)
                 .collect(Collectors.toCollection(HashSet::new));
 
-        return strings.stream().filter(s ->
-            s.chars().mapToObj(c -> (char) c).allMatch(alphabetSet::contains)
-        ).sorted(Comparator.comparingInt(String::length)).toList();
-
+        return strings.stream()
+                .filter(s -> s.chars()
+                        .mapToObj(c -> (char) c)
+                        .allMatch(alphabetSet::contains)
+                )
+                .sorted(Comparator.comparingInt(String::length))
+                .toList();
     }
 }
 
