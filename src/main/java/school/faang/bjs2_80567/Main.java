efdf85ce771060a89b2be7f529c5ab680f71aedc
Andrey Fomchenko <35478@mail.ru>
2025-06-10 08:00:51 +0500

    BJS2-80567 реализовал функциональный интерфейс

--- src/main/java/school/faang/bjs2_80567/Main.java
+++ src/main/java/school/faang/bjs2_80567/Main.java
@@ -5,17 +5,11 @@ public class Main {
         NotificationManager notificationManager = new NotificationManager();
 
 // Регистрация обработчиков оповещений
-        notificationManager.registerHandler(NotificationType.EMAIL,
-                notification -> System.out.println("Email: " + notification.getMessage())
-        );
+        notificationManager.registerHandler(NotificationType.EMAIL, notification -> System.out.println("Email: " + notification.getMessage()));
 
-        notificationManager.registerHandler(NotificationType.SMS,
-                notification -> System.out.println("SMS: " + notification.getMessage())
-        );
+        notificationManager.registerHandler(NotificationType.SMS, notification -> System.out.println("SMS: " + notification.getMessage()));
 
-        notificationManager.registerHandler(NotificationType.PUSH,
-                notification -> System.out.println("Push Notification: " + notification.getMessage())
-        );
+        notificationManager.registerHandler(NotificationType.PUSH, notification -> System.out.println("Push Notification: " + notification.getMessage()));
 
 // Отправка оповещений
         Notification emailNotification = new Notification(NotificationType.EMAIL, "Ваш аккаунт активирован");
