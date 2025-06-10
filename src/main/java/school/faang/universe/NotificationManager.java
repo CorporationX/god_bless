c2069760f7fb38dfd614919d49779702c5727d15
mrnght <misterday116@gmail.com>
2025-06-10 23:02:11 +0800

    added some logs

--- src/main/java/school/faang/universe/NotificationManager.java
+++ src/main/java/school/faang/universe/NotificationManager.java
@@ -1,20 +1,24 @@
 package school.faang.universe;
 
+import lombok.extern.slf4j.Slf4j;
+
 import java.util.HashMap;
 import java.util.Map;
 import java.util.function.Consumer;
 
+@Slf4j
 public class NotificationManager {
-    private Map<NotificationType, Consumer<Notification>> manager = new HashMap<>();
+    private Map<NotificationType, Consumer<Notification>> handlersMap = new HashMap<>();
 
     public void registerHandler(NotificationType type, Consumer<Notification> handler) {
-        manager.put(type, handler);
+        handlersMap.put(type, handler);
     }
 
     public void sendNotification(Notification notification) {
-        if (!manager.containsKey(notification.getType())) {
-            return;
+        if (handlersMap.containsKey(notification.getType())) {
+            handlersMap.get(notification.getType()).accept(notification);
+        } else {
+            log.info("Обработчик данного типа не был найден");
         }
-        manager.get(notification.getType()).accept(notification);
     }
 }
