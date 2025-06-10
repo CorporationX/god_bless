f3561330992755a2efff49f8e556ec42dc789654
Jevgeni Rogotski <jevgeni.rogotski@gmail.com>
2025-06-10 20:32:10 +0300

    Stream API 2. First solution

--- src/main/java/school/faang/bjs2_80710_stream_1/ListOperations.java
+++ src/main/java/school/faang/bjs2_80710_stream_1/ListOperations.java
@@ -1,82 +1,66 @@
 package school.faang.bjs2_80710_stream_1;
 
-import java.util.ArrayList;
+import lombok.NonNull;
+
 import java.util.Comparator;
 import java.util.List;
 import java.util.Optional;
+import java.util.OptionalDouble;
 import java.util.function.Predicate;
 
 public class ListOperations {
 
-    public static int sumOfEvenNumbers(List<Integer> numbers) {
-        return numbers.stream().reduce(0, (sum, number) ->
-                number % 2 == 0 ? sum + number : sum
-        );
+    public static int sumOfEvenNumbers(@NonNull List<Integer> numbers) {
+        return numbers.stream()
+                .reduce(0, (sum, number) ->
+                        number % 2 == 0 ? sum + number : sum
+                );
     }
 
-    public static Optional<Integer> findMax(List<Integer> numbers) {
-        return numbers.stream().max(Comparator.comparingInt(a -> a));
+    public static Optional<Integer> findMax(@NonNull List<Integer> numbers) {
+        return numbers.stream()
+                .max(Comparator.comparingInt(a -> a));
     }
 
-    public static Optional<Double> findAverage(List<Integer> numbers) {
-        if (numbers == null || numbers.isEmpty()) {
-            return Optional.empty();
-        }
-
-        int total = numbers.stream().reduce(0, Integer::sum);
-        return Optional.of((double) total / numbers.size());
+    public static OptionalDouble findAverage(@NonNull List<Integer> numbers) {
+        return numbers.stream()
+                .mapToInt(Integer::valueOf)
+                .average();
     }
 
-    public static Integer countStringsStartingWith(List<String> strings, char start) {
-        if (strings == null) {
-            return 0;
-        }
-
+    public static Integer countStringsStartingWith(@NonNull List<String> strings, char start) {
         return (int) strings.stream()
                 .filter(s -> s.startsWith(String.valueOf(start)))
                 .count();
     }
 
-    public static List<String> filterStringsContainingSubstring(List<String> strings, String needle) {
-        if (strings == null || needle == null) {
-            return new ArrayList<>();
-        }
-
+    public static List<String> filterStringsContainingSubstring(@NonNull List<String> strings, @NonNull String needle) {
         return strings.stream()
-                .filter(s -> s.contains(needle)).toList();
+                .filter(s -> s.contains(needle))
+                .toList();
     }
 
-    public static List<String> sortByLength(List<String> strings) {
-        if (strings == null) {
-            return new ArrayList<>();
-        }
-
+    public static List<String> sortByLength(@NonNull List<String> strings) {
         return strings.stream()
-                .sorted(Comparator.comparingInt(String::length)).toList();
+                .sorted(Comparator.comparingInt(String::length))
+                .toList();
     }
 
-    public static boolean allMatchCondition(List<Integer> numbers, Predicate<Integer> predicate) {
-        if (numbers == null) {
-            return false;
-        }
-
-        return numbers.stream().allMatch(predicate);
+    public static boolean allMatchCondition(@NonNull List<Integer> numbers, Predicate<Integer> predicate) {
+        return numbers.stream()
+                .allMatch(predicate);
     }
 
-    public static Optional<Integer> findMinGreaterThan(List<Integer> numbers, int baseline) {
-        if (numbers == null) {
-            return Optional.empty();
-        }
-
-        return numbers.stream().filter(num -> num > baseline).min(Integer::compare);
+    public static Optional<Integer> findMinGreaterThan(@NonNull List<Integer> numbers, int baseline) {
+        return numbers.stream()
+                .filter(num -> num > baseline)
+                .min(Integer::compare);
     }
 
-    public static List<Integer> convertToLengths(List<String> strings) {
-        if (strings == null) {
-            return new ArrayList<>();
-        }
-
-        return strings.stream().map(String::length).toList();
+    public static List<Integer> convertToLengths(@NonNull List<String> strings) {
+        return strings.stream()
+                .map(String::length)
+                .toList();
     }
 
 }
