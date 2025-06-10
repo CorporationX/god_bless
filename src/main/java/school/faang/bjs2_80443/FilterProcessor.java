ed2599080feff8755cc3b9a36994e6be04bb0174
Andrey Fomchenko <35478@mail.ru>
2025-06-10 22:39:16 +0500

    BJS2-80443 создал фильтры и применил их

---
+++ src/main/java/school/faang/bjs2_80443/FilterProcessor.java
@@ -0,0 +1,17 @@
+package school.faang.bjs2_80443;
+
+import java.util.function.Function;
+
+public class FilterProcessor {
+    public Image applyFilter(Image image, Function<Image, Image> function) {
+        if (image == null) {
+            throw new NullPointerException("image is null");
+        }
+        return function.apply(image);
+    }
+
+    public Function<Image, Image> combineFilters(Function<Image, Image> firstFilter,
+                                                 Function<Image, Image> secondFilter) {
+        return firstFilter.andThen(secondFilter);
+    }
+}
