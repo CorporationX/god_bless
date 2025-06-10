a38dd402b66634f4f20cd0adcf941bb4ef2a006d
ddddddasha <daria.zhelobkovich@gmail.com>
2025-06-10 22:27:38 +0300

    Фильтры Instagram. Исправлено

--- src/main/java/school/faang/module1/bjs2_80463/FilterProcessor.java
+++ src/main/java/school/faang/module1/bjs2_80463/FilterProcessor.java
@@ -4,12 +4,24 @@ import java.util.function.Function;
 
 public class FilterProcessor {
 
-    public Image applyFilter(Image image, Function<Image, Image> filterImage) {
-        return filterImage.apply(image);
+    public Image applyFilter(Image image, Function<Image, Image> imageFilter) {
+        if (image == null) {
+            throw new IllegalArgumentException("Image cannot be null");
+        }
+        if (imageFilter == null) {
+            throw new IllegalArgumentException("Image filter function cannot be null");
+        }
+        return imageFilter.apply(image);
     }
 
     public Function<Image, Image> combineFilters(Function<Image, Image> filterFirst,
                                                  Function<Image, Image> filterSecond) {
-        return image -> filterFirst.andThen(filterSecond).apply(image);
+        if (filterFirst == null) {
+            throw new IllegalArgumentException("First filter cannot be null");
+        }
+        if (filterSecond == null) {
+            throw new IllegalArgumentException("Second filter cannot be null");
+        }
+        return filterFirst.andThen(filterSecond);
     }
 }
