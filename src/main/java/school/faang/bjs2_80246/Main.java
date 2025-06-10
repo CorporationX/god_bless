650e99d32db3b30e6fdb2604b9d8b9a0dfea78ba
Andrey Fomchenko <35478@mail.ru>
2025-06-10 06:38:43 +0500

    BJS2-80246 реализовал функциональный интерфейс

---
+++ src/main/java/school/faang/bjs2_80246/Main.java
@@ -0,0 +1,15 @@
+package school.faang.bjs2_80246;
+
+public class Main {
+    public static void main(String[] args) {
+        SpellCaster spellCaster = new SpellCaster();
+
+        String expelliarmus = "Expelliarmus";
+        String lumos = "Lumos";
+        String avadaKedavra = "AvadaKedavra";
+
+        spellCaster.cast(avadaKedavra, (spellName) -> "You can kill an oponent using " + spellName);
+        spellCaster.cast(lumos, (spell) -> "A beam of light is created by " + spell);
+        spellCaster.cast(expelliarmus, (spell) -> "The opponent is disarmed by " + spell);
+    }
+}
