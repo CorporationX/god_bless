645954c5acb0785cd55f838c102e84b72fad25dd
Ivi-Anastasia <qwwsoul@gmail.com>
2025-06-10 13:34:27 +0100

    item 4 - first tries

--- src/main/java/school/faang/bjs2_80553/NotificationManager.java
+++ src/main/java/school/faang/bjs2_80553/NotificationManager.java
@@ -3,6 +3,7 @@ package school.faang.bjs2_80553;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.function.Consumer;
+import java.util.function.Predicate;
 
 public class NotificationManager {
     private Map<NotificationType, Consumer<Notification>> processorMap = new HashMap<>();
@@ -13,6 +14,11 @@ public class NotificationManager {
 
     public void sendNotification(Notification notification) {
         processorMap.get(notification.getType()).accept(notification);
+    }
+
+    //TODO: Filtering messages - architecture, code
+    public boolean filterMessage(Notification notification, Predicate<Notification> filter) {
+        return filter.test(notification);
     }
 
 }
