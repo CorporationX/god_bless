db7ce9d675d50e669b3c63634d5086247a6be9d0
DimonDem123 <DIMAK22858@GMAIL.COM>
2025-06-11 05:20:46 +0200

    Update Main.java

--- src/main/java/school/faang/bjs280575/Main.java
+++ src/main/java/school/faang/bjs280575/Main.java
@@ -4,8 +4,8 @@ public class Main {
     public static void main(String[] args) {
         NotificationManager dataManager = new NotificationManager();
         dataManager.registerHandler(NotificationType.PUSH, notification -> {
-                                    System.out.println("Push уведомление: " + notification.getMessage()));
-            };
+            System.out.println("Push уведомление: " + notification.getMessage());
+        });
         dataManager.sendNotification(new Notification(NotificationType.PUSH, "Бака как ты мог купить 4 пельменя"));
         String message = "Вы звонили нам неделю назад с прозьбой.\n Она еще актуальна?";
         dataManager.sendNotification(new Notification(NotificationType.PUSH, message));
