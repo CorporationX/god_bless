733d8b32187eab00e180e9bf988ee4f42742ce30
DimonDem123 <DIMAK22858@GMAIL.COM>
2025-06-11 05:14:11 +0200

    Update Main.java
    переделал чтобы прошло по длине строк

--- src/main/java/school/faang/bjs280575/Main.java
+++ src/main/java/school/faang/bjs280575/Main.java
@@ -3,9 +3,12 @@ package school.faang.bjs280575;
 public class Main {
     public static void main(String[] args) {
         NotificationManager dataManager = new NotificationManager();
-        dataManager.registerHandler(NotificationType.PUSH, notification -> System.out.println("Push уведомление: " + notification.getMessage()));
+        dataManager.registerHandler(NotificationType.PUSH, notification -> {
+                                    System.out.println("Push уведомление: " + notification.getMessage()))
+            };
         dataManager.sendNotification(new Notification(NotificationType.PUSH, "Бака как ты мог купить 4 пельменя"));
-        dataManager.sendNotification(new Notification(NotificationType.PUSH, "Вы звонили нам неделю назад с прозьбой. Она еще актуальна?"));
+        String message = "Вы звонили нам неделю назад с прозьбой.\n Она еще актуальна?"
+        dataManager.sendNotification(new Notification(NotificationType.PUSH, message));
 
     }
 }
