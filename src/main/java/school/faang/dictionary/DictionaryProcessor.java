045747178a8fa1c4fe23c77a45585241a08e1396
Aleksey <avzag@yandex.ru>
2025-06-11 15:29:18 +0300

    is corrected one more

--- src/main/java/school/faang/dictionary/DictionaryProcessor.java
+++ src/main/java/school/faang/dictionary/DictionaryProcessor.java
@@ -7,7 +7,8 @@ import java.util.function.BiConsumer;
 
 public class DictionaryProcessor {
     public void processWord(String word, String translation, BiConsumer<String, String> addWordToDictionary) {
-        if ((word != null || !word.isEmpty()) && (translation != null || !translation.isEmpty()))
+        if ((word != null || !word.isEmpty()) && (translation != null || !translation.isEmpty())) {
             addWordToDictionary.accept(word, translation);
+        }
     }
 }
