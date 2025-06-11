2491e941f4c71787686fad8e957ff22fc00ecbf6
itechnik <itechnik-swd@gmail.com>
2025-06-11 10:52:01 +0300

    implement EmailProcessor class with Predicate, Consumer & Function

---
+++ src/main/java/school/faang/bjs2_79986/Email.java
@@ -0,0 +1,12 @@
+package school.faang.bjs2_79986;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class Email {
+    private String subject;
+    private String body;
+    private boolean isImportant;
+}
