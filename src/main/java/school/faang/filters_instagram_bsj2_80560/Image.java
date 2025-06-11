ff435e66f05fcc7825a9c53dae18ea857e5bc2ee
Setched <kir32kir@gmail.com>
2025-06-11 19:05:55 +0300

    filters instagram bjs2-80560

---
+++ src/main/java/school/faang/filters_instagram_bsj2_80560/Image.java
@@ -0,0 +1,13 @@
+package school.faang.filters_instagram_bsj2_80560;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+public class Image {
+    String name;
+    String description;
+}
