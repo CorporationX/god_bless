406dd4382a06ea04731276ba1af6294873fb2c97
JekaCAP <agentgeka@yandex.ru>
2025-06-12 18:38:52 +0500

    lord-of-the-rings BJS2-80405

---
+++ src/main/java/school/faang/bjs2_80405/Item.java
@@ -0,0 +1,11 @@
+package school.faang.bjs2_80405;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@AllArgsConstructor
+@Data
+public class Item {
+    private String name;
+    private int value;
+}
