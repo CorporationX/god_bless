21306b31e76a266870d711b448d1dcf8d081a27b
Dum <dimak22858@gmail.com>
2025-06-12 16:53:17 +0200

    BJS2-80575 Ответил на коменты, переделал под требования. Вывел фильтры и изменение текста сообщения в отдельные методы

--- src/main/java/school/faang/bjs280575/Main.java
+++ src/main/java/school/faang/bjs280575/Main.java
@@ -2,13 +2,13 @@ package school.faang.bjs280575;
 
 public class Main {
     public static void main(String[] args) {
-        NotificationManager dataManager = new NotificationManager();
-        dataManager.registerHandler(NotificationType.PUSH, notification -> {
+        NotificationManager notificationManager = new NotificationManager();
+        notificationManager.registerHandler(NotificationType.PUSH, notification -> {
             System.out.println("Push уведомление: " + notification.getMessage());
         });
-        dataManager.sendNotification(new Notification(NotificationType.PUSH, "Бака как ты мог купить 4 пельменя"));
-        String message = "Вы звонили нам неделю назад с прозьбой.\n Она еще актуальна?";
-        dataManager.sendNotification(new Notification(NotificationType.PUSH, message));
+        notificationManager.sendNotification(new Notification(NotificationType.PUSH, "Бака как ты мог купить 4 пельменя"));
+        String message = "Вы звонили нам неделю назад с просьбой.\n Она еще актуальна?";
+        notificationManager.sendNotification(new Notification(NotificationType.PUSH, message));
 
     }
 }
--- src/main/java/school/faang/bjs280575/NotificationManager.java
+++ src/main/java/school/faang/bjs280575/NotificationManager.java
@@ -12,43 +12,51 @@ import java.util.function.Predicate;
 
 @Slf4j
 public class NotificationManager {
-    private Map<NotificationType, Consumer<Notification>> mapManager = new HashMap<>();
+    private Map<NotificationType, Consumer<Notification>> handlersMap = new HashMap<>();
     public static Set<String> foulLanguage = Set.of(
             "блин", "хрен", "бака"
     );
 
+
     public void registerHandler(NotificationType type, Consumer<Notification> handler) {
-        mapManager.put(type, handler);
+        handlersMap.put(type, handler);
+    }
+
+    protected boolean filter(Notification notification) {
+        Predicate<Notification> findFoulLanguage = not -> {
+            String message = not.getMessage();
+            List<String> listWord = foulLanguage.stream()
+                    .filter(message.toLowerCase()::contains)
+                    .toList();
+            if (listWord.isEmpty()) {
+                return false;
+            } else {
+
+                return true;
+            }
+        };
+        return findFoulLanguage.test(notification);
+    }
+    protected Notification addSignatureTheMessage(Notification notification) {
+        Function<Notification, Notification> addSignature = notif -> {
+            String message = notif.getMessage() + "\n\n\nНаша компания желает вам всего самого наилучшего";
+            NotificationType type = notif.getType();
+            return new Notification(type, message);
+        };
+        return addSignature.apply(notification);
     }
 
     public void sendNotification(Notification notification) {
 
-
-        if (mapManager.get(notification.getType()) == null) {
-            log.warn("Таких уведоблений нету.");
+        if (handlersMap.get(notification.getType()) == null) {
+            log.warn("Таких уведомлений нету.");
             return;
         } else {
-            Predicate<Notification> findFoulLanguage = not -> {
-                String massag = not.getMessage();
-                List<String> listWord = foulLanguage.stream()
-                        .filter(massag.toLowerCase()::contains)
-                        .toList();
-                if (listWord.isEmpty()) {
-                    return false;
-                } else {
-                    return true;
-                }
-            };
-            Function<Notification, Notification> addSignatur = notif -> {
-                String message = notif.getMessage() + "\n\n\nНаша компания желает вам всего самого наилучшего";
-                NotificationType type = notif.getType();
-                return new Notification(type, message);
-            };
-            if (findFoulLanguage.test(notification)) {
+            if (filter(notification)) {
                 System.out.println("На нашем сайте запрещена отправка сообщений с такими словами: " + foulLanguage);
             } else {
-                Notification finalNotification = addSignatur.apply(notification);
-                mapManager.get(notification.getType()).accept(finalNotification);
+                Notification finalNotification = addSignatureTheMessage(notification);
+                handlersMap.get(notification.getType()).accept(finalNotification);
                 log.info("Сообщение отправлено");
             }
 
