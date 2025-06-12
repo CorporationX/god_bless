5ae00604af7f535ae10a288f86cef75f5790a359
Volodymyr <dqkvii@gmail.com>
2025-06-12 21:58:36 +0200

    исправления по рекомендациям ревью, BJS2-79720

--- src/main/java/school/faang/bjs279307/User.java
+++ src/main/java/school/faang/bjs279307/User.java
@@ -3,13 +3,17 @@ package school.faang.bjs279307;
 import java.util.Set;
 
 import lombok.Setter;
+import lombok.ToString;
 
 @Setter
+@ToString
 public class User {
     private String name;
     private int age;
     private String job;
     private String address;
+
+    public static final int MIN_AGE = 18;
 
     public static final Set<String> VALID_JOBS = Set.of("Google", "Uber", "Amazon");
     public static final Set<String> VALID_ADDRESS = Set.of("London", "New York", "Amsterdam");
@@ -20,24 +24,19 @@ public class User {
         }
         this.name = name;
 
-        if (age < 18) {
-            throw new IllegalArgumentException("Возраст должен быть 18 или старше.");
+        if (age < MIN_AGE) {
+            throw new IllegalArgumentException("Возраст должен быть " + MIN_AGE + " или старше.");
         }
         this.age = age;
 
         if (!VALID_JOBS.contains(job)) {
-            throw new IllegalArgumentException("Вы должны работать в Гугл, Убер или Амазон.");
+            throw new IllegalArgumentException("Вы должны работать в: " + job);
         }
         this.job = job;
 
         if (!VALID_ADDRESS.contains(address)) {
-            throw new IllegalArgumentException("Адрес должен быть Лондон, Нью-Йорк или Амстердам.");
+            throw new IllegalArgumentException("Адрес должен быть одним из: " + VALID_ADDRESS);
         }
         this.address = address;
-    }
-
-    @Override
-    public String toString() {
-        return "Работник " + name;
     }
 }
