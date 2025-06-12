e4ef7aff35387a1dbebd400f3c6da357b7024032
Volodymyr <dqkvii@gmail.com>
2025-06-12 18:23:07 +0200

    fix, BJS2-79266

--- src/main/java/school/faang/bjs279266/Main.java
+++ src/main/java/school/faang/bjs279266/Main.java
@@ -1,5 +1,8 @@
 package school.faang.bjs279266;
 
 public class Main {
-    public static void main(String[] args) {}
+    public static void main(String[] args) {
+        Character vasya = new Warrior("Vasya");
+        Character vasan = new Warrior("Vasan");
+    }
 }
