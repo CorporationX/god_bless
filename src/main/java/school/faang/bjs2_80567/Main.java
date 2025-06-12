6694991520355b6815b397c2683ac41de49c90db
Andrey Fomchenko <35478@mail.ru>
2025-06-12 23:03:26 +0500

    BJS2-79148 сделал правки

--- src/main/java/school/faang/bjs2_80567/Main.java
+++ src/main/java/school/faang/bjs2_80567/Main.java
@@ -4,9 +4,12 @@ public class Main {
     public static void main(String[] args) {
         NotificationManager notificationManager = new NotificationManager();
 
-        notificationManager.registerHandler(NotificationType.EMAIL, notification -> System.out.println("Email: " + notification.getMessage()));
-        notificationManager.registerHandler(NotificationType.SMS, notification -> System.out.println("SMS: " + notification.getMessage()));
-        notificationManager.registerHandler(NotificationType.PUSH, notification -> System.out.println("Push Notification: " + notification.getMessage()));
+        notificationManager.registerHandler(NotificationType.EMAIL,
+                notification -> System.out.println("Email: " + notification.getMessage()));
+        notificationManager.registerHandler(NotificationType.SMS,
+                notification -> System.out.println("SMS: " + notification.getMessage()));
+        notificationManager.registerHandler(NotificationType.PUSH,
+                notification -> System.out.println("Push Notification: " + notification.getMessage()));
 
         Notification emailNotification = new Notification(NotificationType.EMAIL, "Ваш аккаунт активирован");
         Notification smsNotification = new Notification(NotificationType.SMS, "Ваш пароль изменен");
