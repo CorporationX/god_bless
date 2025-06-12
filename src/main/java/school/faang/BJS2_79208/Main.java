98f57fdcae50756e9c811c44dce2d845fbceb62b
Andrey Fomchenko <35478@mail.ru>
2025-06-12 23:16:50 +0500

    BJS2-79208 сделал правки

--- src/main/java/school/faang/BJS2_79208/Main.java
+++ src/main/java/school/faang/BJS2_79208/Main.java
@@ -4,12 +4,10 @@ public class Main {
     public static void main(String[] args) {
 
         User andrey = new User("Andrey", 25, "Uber", "London");
-
         System.out.println("Имя пользователя - " + andrey.getName() + "\n"
                 + "Возраст пользователя - " + andrey.getAge() + " лет" + "\n"
                 + "Место работы пользователя - " + andrey.getJob() + "\n"
                 + "Адрес пользователя - " + andrey.getAddress());
-
         User dasha = new User("Dasha ", 24, "Amazon", "Orenburg");
     }
 }
--- src/main/java/school/faang/BJS2_79208/User.java
+++ src/main/java/school/faang/BJS2_79208/User.java
@@ -9,16 +9,18 @@ import java.util.Set;
 public class User {
     private static final Set<String> VALID_JOBS = Set.of("Google", "Uber", "Amazon");
     private static final Set<String> VALID_ADDRESSES = Set.of("London", "New York", "Amsterdam");
+    private static final int VALID_AGE = 18;
+
     private String name;
     private int age;
     private String job;
     private String address;
 
     public User(String name, int age, String job, String address) {
-        if (name.isEmpty()) {
-            throw new IllegalArgumentException("The name field cannot be empty.");
+        if (name == null || name.isEmpty()) {
+            System.out.println("The name field cannot be empty.");
         }
-        if (age < 18) {
+        if (age < VALID_AGE) {
             throw new IllegalArgumentException("Unacceptable age.");
         }
         if (!VALID_JOBS.contains(job)) {
