1e441f58dcfab5f7c6fff0b419282703062b71e3
Volodymyr <dqkvii@gmail.com>
2025-06-12 21:34:29 +0200

    fix, BJS2-79720

--- src/main/java/school/faang/bjs279720/Book.java
+++ src/main/java/school/faang/bjs279720/Book.java
@@ -16,8 +16,12 @@ public class Book {
 
     @Override
     public boolean equals(Object obj) {
-        if (this == obj) {return true;}
-        if (obj == null || getClass() != obj.getClass()) {return false;}
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null || getClass() != obj.getClass()) {
+            return false;
+        }
 
         Book book = (Book) obj;
         return year == book.year
