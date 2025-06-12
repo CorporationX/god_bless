8a29304acf71b99d34ed7941807e19bea05546e8
Andrey Fomchenko <35478@mail.ru>
2025-06-12 23:57:17 +0500

    BJS2-80024 реализовал метод с 3 функциональными интерфейсами

---
+++ src/main/java/school/faang/bjs2_80024/Email.java
@@ -0,0 +1,12 @@
+package school.faang.bjs2_80024;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class Email {
+    private String subject;
+    private String body;
+    private boolean isImportant;
+}
