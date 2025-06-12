af6f02e1bfb5698ebdce336a250ffd6d74d83f9c
Amirzhan Alimov <amirzhan.alimov@yandex.ru>
2025-06-13 03:56:00 +0500

    Implemented BJS2-79228

---
+++ src/main/java/school/faang/BJS2_79228/model/User.java
@@ -0,0 +1,28 @@
+package school.faang.BJS2_79228.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
+@Data
+@AllArgsConstructor
+public class User {
+    private int id;
+    private String name;
+    private int age;
+    private Set<String> activities;
+
+    public static Map<User, String> findHobbyLovers(List<User> users, Set<String> activities) {
+        Map<User, String> hobbyLovers = new HashMap<>();
+        users.forEach(user -> {
+            user.activities.stream()
+                    .filter(activities::contains)
+                    .findFirst()
+                    .ifPresent(activity -> hobbyLovers.put(user, activity));
+        });
+        return hobbyLovers;
+    }
+}
