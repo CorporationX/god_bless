65ce0fb449d910604609617d9b1056946f4f83a1
Volodymyr <dqkvii@gmail.com>
2025-06-12 21:30:44 +0200

    fix, BJS2-79720

--- src/main/java/school/faang/bjs279720/Book.java
+++ src/main/java/school/faang/bjs279720/Book.java
@@ -16,13 +16,13 @@ public class Book {
 
     @Override
     public boolean equals(Object obj) {
-        if (this == obj) return true;
-        if (obj == null || getClass() != obj.getClass()) return false;
+        if (this == obj) {return true;}
+        if (obj == null || getClass() != obj.getClass()) {return false;}
 
         Book book = (Book) obj;
-        return year == book.year &&
-                Objects.equals(title, book.title) &&
-                Objects.equals(author, book.author);
+        return year == book.year
+                && Objects.equals(title, book.title)
+                && Objects.equals(author, book.author);
     }
 
     @Override
