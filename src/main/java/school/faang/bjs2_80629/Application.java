b48932688aa603b037d21024cad1d91a1b6dbd0c
JekaCAP <agentgeka@yandex.ru>
2025-06-12 13:45:22 +0500

    notification manager BJS2-80629

---
+++ src/main/java/school/faang/bjs2_80629/Application.java
@@ -0,0 +1,38 @@
+package school.faang.bjs2_80629;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+public class Application {
+    public static void main(String[] args) {
+        NotificationManager notificationManager = new NotificationManager();
+
+        // Регистрация обработчиков
+        notificationManager.registerHandler(NotificationType.EMAIL,
+                notification -> System.out.println("Email: " + notification.message()));
+
+        notificationManager.registerHandler(NotificationType.SMS,
+                notification -> System.out.println("SMS: " + notification.message()));
+
+        notificationManager.registerHandler(NotificationType.PUSH,
+                notification -> System.out.println("Push Notification: " + notification.message()));
+
+        notificationManager.setFilter(notification ->
+                !notification.message().toLowerCase().contains("badword")
+        );
+
+        notificationManager.setCorrector(notification ->
+                new Notification(notification.type(), notification.message() + " [Meta Notification]")
+        );
+
+        Notification emailNotification = new Notification(NotificationType.EMAIL, "Ваш аккаунт активирован");
+        Notification smsNotification = new Notification(NotificationType.SMS, "Ваш пароль изменен");
+        Notification pushNotification = new Notification(NotificationType.PUSH, "У вас новое сообщение!");
+        Notification badNotification = new Notification(NotificationType.EMAIL, "This contains badword");
+
+        notificationManager.sendNotification(emailNotification);
+        notificationManager.sendNotification(smsNotification);
+        notificationManager.sendNotification(pushNotification);
+        notificationManager.sendNotification(badNotification);
+    }
+}
---
+++ src/main/java/school/faang/bjs2_80629/Notification.java
@@ -0,0 +1,4 @@
+package school.faang.bjs2_80629;
+
+public record Notification(NotificationType type, String message) {
+}
