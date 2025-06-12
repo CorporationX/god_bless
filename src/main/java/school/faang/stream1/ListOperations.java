10060eda496cc077c2ed5c07f8d3aa9705ee9319
Danil Pudovkin <dvpudovkin@yandex.ru>
2025-06-12 14:50:25 +0500

    Fix sonar

--- src/main/java/school/faang/stream1/ListOperations.java
+++ src/main/java/school/faang/stream1/ListOperations.java
@@ -2,6 +2,7 @@ package school.faang.stream1;
 
 import lombok.AccessLevel;
 import lombok.NoArgsConstructor;
+import lombok.NonNull;
 
 import java.util.List;
 import java.util.function.Predicate;
@@ -13,45 +14,45 @@ import java.util.function.Predicate;
 @NoArgsConstructor(access = AccessLevel.PRIVATE)
 public class ListOperations {
 
-    public static int sumOfEvenNumbers(List<Integer> integers) {
+    public static int sumOfEvenNumbers(@NonNull List<Integer> integers) {
         return integers.stream()
                 .filter(i -> i % 2 == 0)
                 .reduce(0, Integer::sum);
     }
 
-    public static int findMax(List<Integer> integers) {
+    public static int findMax(@NonNull List<Integer> integers) {
         return integers.stream()
                 .max(Integer::compare)
                 .orElse(0);
     }
 
-    public static double findAverage(List<Integer> integers) {
+    public static double findAverage(@NonNull List<Integer> integers) {
         return (double) integers.stream().reduce(0, Integer::sum) / integers.size();
     }
 
-    public static long countStringsStartingWith(List<String> strings, char prefix) {
+    public static long countStringsStartingWith(@NonNull List<String> strings, char prefix) {
         return strings.stream()
                 .filter(s -> s.charAt(0) == prefix)
                 .count();
     }
 
-    public static long filterStringsContainingSubstring(List<String> strings, String substring) {
+    public static long filterStringsContainingSubstring(@NonNull List<String> strings, @NonNull String substring) {
         return strings.stream()
                 .filter(s -> s.contains(substring))
                 .count();
     }
 
-    public static List<String> sortByLength(List<String> strings) {
+    public static List<String> sortByLength(@NonNull List<String> strings) {
         return strings.stream()
                 .sorted()
                 .toList();
     }
 
-    public static boolean allMatchCondition(List<Integer> integers, Predicate<Integer> predicate) {
+    public static boolean allMatchCondition(@NonNull List<Integer> integers, @NonNull Predicate<Integer> predicate) {
         return integers.stream().allMatch(predicate);
     }
 
-    public static int findMinGreaterThan(List<Integer> integers, int max) {
+    public static int findMinGreaterThan(@NonNull List<Integer> integers, int max) {
         return integers.stream()
                 .filter(i -> i > max)
                 .min(Integer::compare)
