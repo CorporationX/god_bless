acc168c61e47b9782c7f89d4a578d150a99b2244
Volodymyr <dqkvii@gmail.com>
2025-06-12 18:14:12 +0200

    сделал абстракцию, BJS2-79266

---
+++ src/main/java/school/faang/bjs279266/Archer.java
@@ -0,0 +1,12 @@
+package school.faang.bjs279266;
+
+public class Archer extends Character {
+    public Archer(String name) {
+        super(name, 3, 10, 5);
+    }
+
+    @Override
+    public void attack(Character opponent) {
+        opponent.setHealth(opponent.getHealth()-this.getDexterity());
+    }
+}
---
+++ src/main/java/school/faang/bjs279266/Main.java
@@ -0,0 +1,9 @@
+package school.faang.bjs279266;
+
+public class Main {
+    public static void main(String[] args) {
+        Character Vasya = new Warrior("Vasya");
+        Character Vasan = new Warrior("Vasan");
+
+    }
+}
