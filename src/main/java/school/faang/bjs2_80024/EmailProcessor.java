98fbc1a970807db71d524b38b4de71fd3147769d
Andrey Fomchenko <35478@mail.ru>
2025-06-13 00:05:05 +0500

    BJS2-80024 реализовал метод с 3 функциональными интерфейсами

--- src/main/java/school/faang/bjs2_80024/EmailProcessor.java
+++ src/main/java/school/faang/bjs2_80024/EmailProcessor.java
@@ -6,7 +6,8 @@ import java.util.function.Function;
 import java.util.function.Predicate;
 
 public class EmailProcessor {
-    public void processEmails(List<Email> emails, Predicate<Email> filter, Consumer<Email> action, Function<Email, String> transformer) {
+    public void processEmails(List<Email> emails, Predicate<Email> filter,
+                              Consumer<Email> action, Function<Email, String> transformer) {
         for (Email email : emails) {
             if (filter.test(email)) {
                 String subject = transformer.apply(email);
