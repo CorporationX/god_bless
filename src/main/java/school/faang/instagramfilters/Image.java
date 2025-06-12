4502b72eb5fa999b87d1c886ddf412dbc7707f2a
Tien-PhanT <112714509+phtanya@users.noreply.github.com>
2025-06-12 05:15:11 +0200

    BJS2-80579 Фильтры Instagram

---
+++ src/main/java/school/faang/instagramfilters/Image.java
@@ -0,0 +1,13 @@
+package school.faang.instagramfilters;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.ToString;
+
+@Getter
+@ToString
+@AllArgsConstructor
+public class Image {
+    private final String name;
+    private final String description;
+}
