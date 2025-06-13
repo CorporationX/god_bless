36365b7455d8f678ac2ae3c2c5e95a0d1131240b
DimonDem123 <DIMAK22858@GMAIL.COM>
2025-06-13 16:58:43 +0200

    Update Main.java

--- src/main/java/school/faang/bjs280542/Main.java
+++ src/main/java/school/faang/bjs280542/Main.java
@@ -5,10 +5,13 @@ import java.util.function.Function;
 public class Main {
     public static void main(String[] args) {
         Image originalImage = new Image("original.jpg", "Оригинальное изображение");
-
         FilterProcessor filterProcessor = new FilterProcessor();
-        Function<Image, Image> grayscaleFilter = (image) -> new Image(image.getName(), image.getDescription() + " | Фильтр: черно-белый");
-        Function<Image, Image> sepiaFilter = (image) -> new Image(image.getName(), image.getDescription() + " | Фильтр: сепия");
+        Function<Image, Image> grayscaleFilter = (image -> {
+            return new Image(image.getName(), image.getDescription() + " | Фильтр: черно-белый");
+        });
+        Function<Image, Image> sepiaFilter = (image -> {
+            return new Image(image.getName(), image.getDescription() + " | Фильтр: сепия");
+        });
         Image grayscaleImage = filterProcessor.applyFilter(originalImage, grayscaleFilter);
         System.out.println(grayscaleImage.getDescription());
         Image sepiaImage = filterProcessor.applyFilter(grayscaleImage, sepiaFilter);
