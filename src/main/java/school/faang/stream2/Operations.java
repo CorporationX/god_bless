6bb0f81302b9347534a575d64dc95de3b0dbc6af
Danil Pudovkin <dvpudovkin@yandex.ru>
2025-06-13 14:19:53 +0500

    Add solution for Stream 2

--- src/main/java/school/faang/stream2/Operations.java
+++ src/main/java/school/faang/stream2/Operations.java
@@ -2,6 +2,7 @@ package school.faang.stream2;
 
 import lombok.AccessLevel;
 import lombok.NoArgsConstructor;
+import lombok.NonNull;
 
 import java.util.Arrays;
 import java.util.Collections;
@@ -18,7 +19,7 @@ import java.util.stream.Collectors;
 @NoArgsConstructor(access = AccessLevel.PRIVATE)
 public class Operations {
 
-    public static Set<List<Integer>> findUniquePairs(Set<Integer> numbers, int sum) {
+    public static Set<List<Integer>> findUniquePairs(@NonNull Set<Integer> numbers, int sum) {
         return numbers.stream()
                 .filter(num -> num != sum - num && numbers.contains(sum - num))
                 .map(num -> Arrays.asList(num, sum - num))
@@ -26,27 +27,27 @@ public class Operations {
                 .collect(Collectors.toSet());
     }
 
-    public static List<String> getCountriesCapitals(Map<String, String> capitalsByCountry) {
+    public static List<String> getCountriesCapitals(@NonNull Map<String, String> capitalsByCountry) {
         return capitalsByCountry.entrySet().stream()
                 .sorted(Map.Entry.comparingByKey())
                 .map(Map.Entry::getValue)
                 .toList();
     }
 
-    public static List<String> filterAndSortStrings(List<String> strings, char prefix) {
+    public static List<String> filterAndSortStrings(@NonNull List<String> strings, char prefix) {
         return strings.stream()
                 .filter(s -> s.charAt(0) == prefix)
                 .sorted(Comparator.comparingInt(String::length))
                 .toList();
     }
 
-    public static List<String> convertToBinary(List<Integer> numbers) {
+    public static List<String> convertToBinary(@NonNull List<Integer> numbers) {
         return numbers.stream()
                 .map(Integer::toBinaryString)
                 .toList();
     }
 
-    public static List<String> filterAndSortByLength(List<String> strings, String alphabet) {
+    public static List<String> filterAndSortByLength(@NonNull List<String> strings, String alphabet) {
         String regex = "[" + alphabet + "]+";
         return strings.stream()
                 .filter(s -> s.matches(regex))
