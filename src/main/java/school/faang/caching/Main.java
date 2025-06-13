7bef49fddc7bc63a99057d48aa0c90d1624c15eb
Vlad Nikitin <nikitin.v2407@gmail.com>
2025-06-13 17:30:55 +0300

    Кэширование, кэширование

--- src/main/java/school/faang/caching/Main.java
+++ src/main/java/school/faang/caching/Main.java
@@ -1,23 +1,21 @@
 package school.faang.caching;
 
 public class Main {
-        public static void main(String[] args) throws InterruptedException {
-            WeatherCacheTemplate standardCache = new StandardWeatherCache(new WeatherService());
-            WeatherCacheTemplate frequentCache = new FrequentUpdateWeatherCache(new WeatherService());
-
-            System.out.println(frequentCache.getWeatherData("Moskov", 50000));
-            System.out.println(frequentCache.getWeatherData("Moskov", 50000));
-            System.out.println(frequentCache.getWeatherData("Moskov", 0));
-            System.out.println(standardCache.getWeatherData("Moskov", 30000));
-            System.out.println(standardCache.getWeatherData("Moskov", 30000));
-            System.out.println(standardCache.getWeatherData("Moskov", 0));
-            System.out.println(standardCache.getWeatherData("Moskov", 90));
-
-            System.out.println(standardCache.getWeatherData("Test", 50000));
-            Thread.sleep(100000);
-            standardCache.clearExpiredCache(200);
-            System.out.println(standardCache.getWeatherData("SBP", 50000));
-            System.out.println(standardCache.getWeatherData("Moskov", 10));
-            System.out.println();
-        }
+    public static void main(String[] args) throws InterruptedException {
+        WeatherCacheTemplate standardCache = new StandardWeatherCache(new WeatherService());
+        WeatherCacheTemplate frequentCache = new FrequentUpdateWeatherCache(new WeatherService());
+        System.out.println(frequentCache.getWeatherData("Moskov", 10000));
+        System.out.println(frequentCache.getWeatherData("Moskov", 10000));
+        System.out.println(frequentCache.getWeatherData("Moskov", 0));
+        System.out.println(standardCache.getWeatherData("Moskov", 10000));
+        System.out.println(standardCache.getWeatherData("Moskov", 10000));
+        System.out.println(standardCache.getWeatherData("Moskov", 0));
+        System.out.println(standardCache.getWeatherData("Moskov", 10));
+        System.out.println(standardCache.getWeatherData("Test", 10000));
+        Thread.sleep(1000);
+        standardCache.clearExpiredCache(100);
+        System.out.println(standardCache.getWeatherData("Test", 10000));
+        System.out.println(standardCache.getWeatherData("Moskov", 10));
+        System.out.println();
     }
+}
