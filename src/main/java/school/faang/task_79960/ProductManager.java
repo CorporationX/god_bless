b21ab024f653d6fc831b9ab3b1902389157dc419
Pavel Hohlov <hohlov.ps95@yandex.ru>
2025-06-14 00:54:27 +0300

    Финальная версия

--- src/main/java/school/faang/task_79960/ProductManager.java
+++ src/main/java/school/faang/task_79960/ProductManager.java
@@ -22,7 +22,8 @@ public class ProductManager {
     }
 
     public List<Product> findProductsByCategory(Category category) {
-        List<Product> productList = products.stream().filter(product1 -> product1.getCategory().equals(category)).toList();
+        List<Product> productList = products.stream()
+                .filter(product1 -> product1.getCategory().equals(category)).toList();
         productList.forEach(element -> System.out.println(element.getName()));
         return productList;
     }
