9ebc41891a94cebad714f77e1695da5fbbb7415d
Dum <dimak22858@gmail.com>
2025-06-13 16:53:39 +0200

    BJS2-80542

---
+++ src/main/java/school/faang/bjs280542/FilterProcessor.java
@@ -0,0 +1,12 @@
+package school.faang.bjs280542;
+
+import java.util.function.Function;
+
+public class FilterProcessor {
+    public Image applyFilter(Image image, Function<Image, Image> filter) {
+        return filter.apply(image);
+    }
+    public Function<Image, Image> combineFilters(Function<Image, Image> firstFilter, Function<Image, Image> secondFilter) {
+        return image -> firstFilter.compose(secondFilter).apply(image);
+    }
+}
