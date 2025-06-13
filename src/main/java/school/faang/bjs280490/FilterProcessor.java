67789e2af3be76f059c3027394f45b78807a5c24
Volodymyr <dqkvii@gmail.com>
2025-06-13 18:23:42 +0200

    BJS2-80490

--- src/main/java/school/faang/bjs280490/FilterProcessor.java
+++ src/main/java/school/faang/bjs280490/FilterProcessor.java
@@ -7,8 +7,7 @@ public class FilterProcessor {
         return filter.apply(image);
     }
 
-    public static Function<Image, Image> combineFilters
-            (Function<Image, Image> filter1, Function<Image, Image> filter2) {
+    public static Function<Image, Image> combineFilters(Function<Image, Image> filter1, Function<Image, Image> filter2) {
         return filter1.andThen(filter2);
     }
 }
