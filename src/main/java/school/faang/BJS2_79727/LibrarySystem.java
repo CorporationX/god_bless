a6247d5032888dd54ed338a0315fc6d3723b38fb
Amirzhan Alimov <amirzhan.alimov@yandex.ru>
2025-06-13 18:20:14 +0500

    Rename field

--- src/main/java/school/faang/BJS2_79727/LibrarySystem.java
+++ src/main/java/school/faang/BJS2_79727/LibrarySystem.java
@@ -8,25 +8,25 @@ import java.util.Map;
 import java.util.NoSuchElementException;
 
 public class LibrarySystem {
-    private final Map<Book, String> booksLocations = new HashMap<>();
+    private final Map<Book, String> locationByBook = new HashMap<>();
 
     public void addBook(String title, String author, int year, String location) {
         var newBook = new Book(title, author, Year.of(year));
-        this.booksLocations.put(newBook, location);
+        this.locationByBook.put(newBook, location);
     }
 
     public boolean removeBook(String title, String author, int year) {
         var bookToRemove = new Book(title, author, Year.of(year));
-        return this.booksLocations.remove(bookToRemove) != null;
+        return this.locationByBook.remove(bookToRemove) != null;
     }
 
     public String findBook(String title, String author, int year) {
         var book = new Book(title, author, Year.of(year));
-        return this.booksLocations.getOrDefault(book, "The book is not found.");
+        return this.locationByBook.getOrDefault(book, "The book is not found.");
     }
 
     public void printAllBooks() {
-        this.booksLocations.forEach((book, location) -> {
+        this.locationByBook.forEach((book, location) -> {
             System.out.printf("%s is placed %s%n", book.getTitle(), location);
         });
     }
