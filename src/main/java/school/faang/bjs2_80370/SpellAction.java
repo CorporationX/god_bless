ea68c388546d7ee0727a1f1b9d26dd662aeb21a7
JasonRon <ubervladocity@gmail.com>
2025-06-13 23:37:18 +0300

    Первая реализация лямбды

--- src/main/java/school/faang/bjs2_80370/SpellAction.java
+++
@@ -1,6 +0,0 @@
-package school.faang.bjs2_80370;
-
-@FunctionalInterface
-public interface SpellAction {
-    String spell(String spell);
-}
--- src/main/java/school/faang/bjs2_80370/SpellCaster.java
+++
@@ -1,8 +0,0 @@
-package school.faang.bjs2_80370;
-
-public class SpellCaster {
-    public void cast(String spellName, SpellAction action) {
-        String result = action.spell(spellName);
-        System.out.println("Результат заклинания:" + result);
-    }
-}
