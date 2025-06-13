2baec7eb0c5fc447344955aac6810a16c2ecbc2f
Tien-PhanT <112714509+phtanya@users.noreply.github.com>
2025-06-13 05:15:54 +0200

    Replace sout`s with logger

--- src/main/java/school/faang/instagramfilters/Main.java
+++ src/main/java/school/faang/instagramfilters/Main.java
@@ -1,7 +1,10 @@
 package school.faang.instagramfilters;
 
+import lombok.extern.slf4j.Slf4j;
+
 import java.util.function.Function;
 
+@Slf4j
 public class Main {
     public static void main(String[] args) {
         Image originalImage = new Image("original.jpg", "Оригинальное изображение");
@@ -14,13 +17,13 @@ public class Main {
                 image.getDescription() + " | Фильтр: сепия");
 
         Image grayscaleImage = filterProcessor.applyFilter(originalImage, grayscaleFilter);
-        System.out.println(grayscaleImage.getDescription());
+        log.info(grayscaleImage.getDescription());
 
         Image sepiaImage = filterProcessor.applyFilter(grayscaleImage, sepiaFilter);
-        System.out.println(sepiaImage.getDescription());
+        log.info(sepiaImage.getDescription());
 
         Function<Image, Image> combinedFilter = filterProcessor.combineFilters(grayscaleFilter, sepiaFilter);
         Image combinedImage = filterProcessor.applyFilter(originalImage, combinedFilter);
-        System.out.println(combinedImage.getDescription());
+        log.info(combinedImage.getDescription());
     }
 }
