a0124656e059747d284ef4afb3f28f9eb9b75eea
Dum <dimak22858@gmail.com>
2025-06-14 20:37:12 +0200

    BJS2-80542 Если хотите чтобы названия были более четкими и отражали суть уберите это ограничения на строку в 120 симвооло. Постоянно приходиться выкручиваться с условиями и переносить строки что ухудшает читаемость кода и переименовывать переменные в скращенные слова

--- src/main/java/school/faang/bjs280542/FilterProcessor.java
+++ src/main/java/school/faang/bjs280542/FilterProcessor.java
@@ -1,12 +1,14 @@
 package school.faang.bjs280542;
 
 import java.util.function.Function;
+import java.util.function.UnaryOperator;
 
 public class FilterProcessor {
-    public Image applyFilter(Image image, Function<Image, Image> filter) {
+    public Image applyFilter(Image image, UnaryOperator<Image> filter) {
         return filter.apply(image);
     }
-    public Function<Image, Image> combineFilters(Function<Image, Image> firstFilter, Function<Image, Image> secondFilter) {
-        return image -> firstFilter.compose(secondFilter).apply(image);
+
+    public UnaryOperator<Image> combineFilters(UnaryOperator<Image> filtImag1, UnaryOperator<Image> filtImag2) {
+        return image -> filtImag1.compose(filtImag2).apply(image);
     }
 }
--- src/main/java/school/faang/bjs280542/Main.java
+++ src/main/java/school/faang/bjs280542/Main.java
@@ -1,19 +1,21 @@
 package school.faang.bjs280542;
 
 import java.util.function.Function;
+import java.util.function.UnaryOperator;
 
 public class Main {
     public static void main(String[] args) {
         Image originalImage = new Image("original.jpg", "Оригинальное изображение");
-
         FilterProcessor filterProcessor = new FilterProcessor();
-        Function<Image, Image> grayscaleFilter = (image) -> new Image(image.getName(), image.getDescription() + " | Фильтр: черно-белый");
-        Function<Image, Image> sepiaFilter = (image) -> new Image(image.getName(), image.getDescription() + " | Фильтр: сепия");
-        Image grayscaleImage = filterProcessor.applyFilter(originalImage, grayscaleFilter);
+        UnaryOperator<Image> grayFilter =
+                image -> new Image(image.getName(), image.getDescription() + "| Фильтр: серый.");
+        UnaryOperator<Image> sepiaFilter =
+                image -> new Image(image.getName(), image.getDescription() + " Фильтр: сепия");
+        Image grayscaleImage = filterProcessor.applyFilter(originalImage, grayFilter);
         System.out.println(grayscaleImage.getDescription());
         Image sepiaImage = filterProcessor.applyFilter(grayscaleImage, sepiaFilter);
         System.out.println(sepiaImage.getDescription());
-        Function<Image, Image> combinedFilter = filterProcessor.combineFilters(grayscaleFilter, sepiaFilter);
+        UnaryOperator<Image> combinedFilter = filterProcessor.combineFilters(grayFilter, sepiaFilter);
         Image combinedImage = filterProcessor.applyFilter(originalImage, combinedFilter);
         System.out.println(combinedImage.getDescription());
     }
