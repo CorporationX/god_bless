3399f85f73785c0034e5498b24cca07e655f111e
Vlad Nikitin <nikitin.v2407@gmail.com>
2025-06-14 12:46:45 +0300

    gmailrichfilters

---
+++ src/main/java/school/faang/gmail_rich_filters/EmailProcessor.java
@@ -0,0 +1,14 @@
+package school.faang.gmail_rich_filters;
+
+import java.util.List;
+import java.util.function.Consumer;
+import java.util.function.Function;
+import java.util.function.Predicate;
+
+public class EmailProcessor {
+
+    void processEmails(List<Email> emails, Predicate<Email> predicate,
+                       Consumer<Email> consumer, Function<Email, String> function) {
+
+    }
+}
