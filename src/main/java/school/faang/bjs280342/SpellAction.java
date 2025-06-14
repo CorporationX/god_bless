28512f9fdfd5d60ae5c7c7b168f94c1987fb8a60
Volodymyr <dqkvii@gmail.com>
2025-06-14 11:49:37 +0200

    nothing

--- src/main/java/school/faang/bjs280342/SpellAction.java
+++
@@ -1,6 +0,0 @@
-package school.faang.bjs280342;
-
-@FunctionalInterface
-public interface SpellAction {
-    String perform(String spellName);
-}
--- src/main/java/school/faang/bjs280342/SpellCaster.java
+++
@@ -1,8 +0,0 @@
-package school.faang.bjs280342;
-
-public class SpellCaster {
-    public void cast(String spellName, SpellAction action) {
-        String result = action.perform(spellName);
-        System.out.println(result);
-    }
-}
