e0dd7519a51140d5824e39070b9942805de6180c
licosty <d.chigantseva@gmail.com>
2025-06-15 17:21:02 +0300

    hashCode & equals заменен @EqualsAndHashCode

--- src/main/java/school/faang/bjs2_81010/UserAction.java
+++ src/main/java/school/faang/bjs2_81010/UserAction.java
@@ -1,36 +1,19 @@
 package school.faang.bjs2_81010;
 
 import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.ToString;
-
 import java.time.LocalDate;
-import java.util.Objects;
 
 @AllArgsConstructor
 @Getter
 @ToString
+@EqualsAndHashCode(exclude = {"actionType", "actionDate", "content"})
 public class UserAction {
     private int id;
     private String name;
     private ActionType actionType;
     private LocalDate actionDate;
     private String content;
-
-    @Override
-    public boolean equals(Object o) {
-        if (o == null || getClass() != o.getClass()) {
-            return false;
-        }
-
-        UserAction that = (UserAction) o;
-        return id == that.id && Objects.equals(name, that.name);
-    }
-
-    @Override
-    public int hashCode() {
-        int result = id;
-        result = 31 * result + Objects.hashCode(name);
-        return result;
-    }
 }
