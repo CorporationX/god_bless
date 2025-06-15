8090d60a54b438ed690e2f550e2b635e7d4d606f
Viktor Romanenko <romanenkoviktor080@gmail.com>
2025-06-15 16:28:34 +0300

    2-5 пункты задачи

---
+++ src/main/java/school/faang/bjs2_80743/Employee.java
@@ -0,0 +1,12 @@
+package school.faang.bjs2_80743;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class Employee {
+    private String name;
+    private String department;
+    private double salary;
+}
--- src/main/java/school/faang/bjs2_80743/Main.java
+++ src/main/java/school/faang/bjs2_80743/Main.java
@@ -2,13 +2,13 @@ package school.faang.bjs2_80743;
 
 import lombok.EqualsAndHashCode;
 
-import java.util.List;
-import java.util.Map;
-import java.util.Set;
+import java.util.*;
 import java.util.stream.Collectors;
 
 public class Main {
     public static void main(String[] args) {
+        //Намеренно не выносил логику в ф-ции
+        //part 1
         Map<String, List<String>> peoples = Map.of(
                 "Alice", List.of("Bob", "Charlie"),
                 "Bob", List.of("Alice", "David"),
@@ -18,6 +18,66 @@ public class Main {
 
         Set<Pair<String, String>> result = findNonFriendMutualPairs(peoples);
         result.forEach(System.out::println);
+
+        //part 2
+        List<Employee> employees = List.of(
+                new Employee("1", "1", 100),
+                new Employee("2", "2", 200),
+                new Employee("3", "1", 300),
+                new Employee("4", "3", 400),
+                new Employee("5", "2", 500),
+                new Employee("6", "4", 600),
+                new Employee("7", "4", 700)
+        );
+        System.out.println("Средние зарплаты по отделам" +
+                employees.stream().collect(Collectors.groupingBy(
+                        Employee::getDepartment,
+                        Collectors.averagingDouble(Employee::getSalary)
+                ))
+        );
+
+        //part 3
+        List<Integer> palindromes = new ArrayList<>();
+        for (int i = 100; i < 200; i++) {
+            String str = Integer.toString(i);
+            if (isPalindrome(str)) {
+                palindromes.add(i);
+            }
+        }
+        System.out.println("Полиндромы: " + palindromes);
+
+        //part 4
+        char[] charts = "abac".toCharArray();
+        Set<String> strPalindromes = new HashSet<>();
+        for (int i = 0; i < charts.length; i++) {
+            StringBuilder str = new StringBuilder();
+            str.append(charts[i]);
+            if (isPalindrome(str.toString())) {
+                strPalindromes.add(str.toString());
+            }
+            for (int j = i + 1; j < charts.length; j++) {
+                str.append(charts[j]);
+                if (isPalindrome(str.toString())) {
+                    strPalindromes.add(str.toString());
+                }
+            }
+        }
+        System.out.println("Полиндромы подстроки: " + strPalindromes);
+
+        //part 5
+        List<Integer> perfectNumbers = new ArrayList<>();
+        for (int i = 1; i < 100000; i++) {
+            int sum = 0;
+            for (int j = 1; j < i; j++) {
+                if (i % j == 0) {
+                    sum += j;
+                }
+            }
+            if (sum == i) {
+                perfectNumbers.add(i);
+            }
+        }
+        System.out.println("Идеальные числа: " + perfectNumbers);
     }
 
     public static Set<Pair<String, String>> findNonFriendMutualPairs(Map<String, List<String>> map) {
@@ -48,5 +108,9 @@ public class Main {
         public String toString() {
             return "(" + first + ", " + second + ")";
         }
+    }
+
+    public static boolean isPalindrome(String str) {
+        return str != null && !str.isBlank() && str.contentEquals(new StringBuilder(str).reverse());
     }
 }
