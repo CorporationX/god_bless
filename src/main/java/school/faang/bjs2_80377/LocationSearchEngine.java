cdb13a7e2b48e405526ccc819449b5a36414ccb1
itechnik <itechnik-swd@gmail.com>
2025-06-15 20:49:08 +0300

    replace .collect(Collectors.toList()) with .toList()

--- src/main/java/school/faang/bjs2_80377/LocationSearchEngine.java
+++ src/main/java/school/faang/bjs2_80377/LocationSearchEngine.java
@@ -4,13 +4,12 @@ import java.util.List;
 import java.util.function.Consumer;
 import java.util.function.Function;
 import java.util.function.Predicate;
-import java.util.stream.Collectors;
 
 public class LocationSearchEngine {
     public List<Location> filterLocations(List<Location> locations, Predicate<Location> predicate) {
         return locations.stream()
                 .filter(predicate)
-                .collect(Collectors.toList());
+                .toList();
     }
 
     public void processLocations(List<Location> locations, Consumer<Location> process) {
@@ -20,6 +19,6 @@ public class LocationSearchEngine {
     public List<Double> calculateDistances(List<Location> locations, Function<Location, Double> distanceCalculator) {
         return locations.stream()
                 .map(distanceCalculator)
-                .collect(Collectors.toList());
+                .toList();
     }
 }
