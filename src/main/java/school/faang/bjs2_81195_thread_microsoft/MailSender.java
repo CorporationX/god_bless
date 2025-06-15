cb4a0bef0103c64136c632e07b6e7740a9613f28
Jevgeni Rogotski <jevgeni.rogotski@gmail.com>
2025-06-15 22:15:52 +0300

    Threads Heroes. Less magic

--- src/main/java/school/faang/bjs2_81195_thread_microsoft/MailSender.java
+++ src/main/java/school/faang/bjs2_81195_thread_microsoft/MailSender.java
@@ -8,15 +8,15 @@ import java.util.List;
 @Slf4j
 public class MailSender {
     private static final int BATCH_COUNT = 200;
+    private static final int TOTAL_MAILS = 1000;
 
     public static void main(String[] args) {
-        int totalMails = 1000;
-        int batchesCount = (int) Math.ceil((double) totalMails / BATCH_COUNT);
+        int batchesCount = (int) Math.ceil((double) TOTAL_MAILS / BATCH_COUNT);
         List<Thread> senderThreads = new ArrayList<>();
 
         for (int i = 0; i < batchesCount; i++) {
             int startIndex = i * BATCH_COUNT;
-            int endIndex = Math.min(startIndex + BATCH_COUNT, totalMails) - 1;
+            int endIndex = Math.min(startIndex + BATCH_COUNT, TOTAL_MAILS) - 1;
             Thread sender = new Thread(new SenderRunnable(startIndex, endIndex));
             senderThreads.add(sender);
             sender.start();
