8adf52c4c6fe3372677052f80e7ce9d6e47ce9af
yagor. <warneverchang3s@gmail.com>
2025-06-15 21:15:34 +0300

    Потренируем StreamAPI - правки по pr

--- src/main/java/school/faang/streamtraining1/Main.java
+++ src/main/java/school/faang/streamtraining1/Main.java
@@ -8,31 +8,31 @@ public class Main {
     public static void main(String[] args) {
         List<Integer> integerList = Arrays.asList(13, 5, 21, 7, 6, 10, 18);
         List<String> charList = Arrays.asList("Wolf", "Cat", "Wolverine", "Cow", "Dog", "Fox", "Deer");
-        int testSum = StreamProducer.listSum(integerList);
+        int testSum = StreamExecutor.listSum(integerList);
         System.out.println(testSum);
 
-        int maxNum = StreamProducer.maxNumber(integerList);
+        int maxNum = StreamExecutor.maxNumber(integerList);
         System.out.println(maxNum);
 
-        double averageNum = StreamProducer.averageNumber(integerList);
+        double averageNum = StreamExecutor.averageNumber(integerList);
         System.out.println(averageNum);
 
-        long contFirstChar = StreamProducer.innerCharCount(charList, 'C');
+        long contFirstChar = StreamExecutor.innerCharCount(charList, 'C');
         System.out.println(contFirstChar);
 
-        List<String> substring = StreamProducer.filterStringBySubstring(charList, "Dog");
+        List<String> substring = StreamExecutor.filterStringContainsSubstring(charList, "Dog");
         System.out.println(substring);
 
-        List<String> sortByLength = StreamProducer.sortByStringLength(charList);
+        List<String> sortByLength = StreamExecutor.sortByStringLength(charList);
         System.out.println(sortByLength);
 
-        boolean allMatchPred = StreamProducer.isAllTrue(integerList, pred -> pred > 0);
+        boolean allMatchPred = StreamExecutor.isAllTrue(integerList, pred -> pred > 0);
         System.out.println(allMatchPred);
 
-        int lowestAfterNum = StreamProducer.lowestElementByHighest(integerList, 18);
+        int lowestAfterNum = StreamExecutor.lowestElementByHighest(integerList, 18);
         System.out.println(lowestAfterNum);
 
-        List<Integer> stringToLength = StreamProducer.stringsToLength(charList);
+        List<Integer> stringToLength = StreamExecutor.stringsToLength(charList);
         System.out.println(stringToLength);
     }
 
--- src/main/java/school/faang/streamtraining1/StreamExecutor.java
+++ src/main/java/school/faang/streamtraining1/StreamExecutor.java
@@ -4,25 +4,27 @@ import java.util.Comparator;
 import java.util.List;
 import java.util.function.Predicate;
 
-public class StreamProducer {
+public class StreamExecutor {
 
-    public static int listSum(List<Integer> incList) {
-        return incList.stream().filter(integer -> integer % 2 == 0).mapToInt(Integer::intValue).sum();
+    public static int listSum(List<Integer> numList) {
+        return numList.stream()
+                .filter(integer -> integer % 2 == 0)
+                .mapToInt(Integer::intValue).sum();
     }
 
     public static int maxNumber(List<Integer> incList) {
-        return incList.stream().max(Integer::compare).get();
+        return incList.stream().max(Integer::compare).orElseThrow();
     }
 
     public static double averageNumber(List<Integer> incList) {
-        return incList.stream().mapToDouble(Integer::doubleValue).average().getAsDouble();
+        return incList.stream().mapToDouble(Integer::doubleValue).average().orElseThrow();
     }
 
     public static long innerCharCount(List<String> stringList, char firstChar) {
         return stringList.stream().filter(sym -> sym.startsWith(String.valueOf(firstChar))).count();
     }
 
-    public static List<String> filterStringBySubstring(List<String> stringList, String substring) {
+    public static List<String> filterStringContainsSubstring(List<String> stringList, String substring) {
         return stringList.stream().filter(string -> string.contains(substring)).toList();
     }
 
@@ -31,13 +33,13 @@ public class StreamProducer {
     }
 
     public static boolean isAllTrue(List<Integer> list, Predicate<Integer> predicate) {
-        return list.stream().allMatch(predicate);
+        return !list.isEmpty() && list.stream().allMatch(predicate);
     }
 
     public static int lowestElementByHighest(List<Integer> list, int num) {
         return list.stream().filter(integer -> integer > num)
                 .min(Integer::compareTo)
-                .get();
+                .orElseThrow();
     }
 
     public static List<Integer> stringsToLength(List<String> stringList) {
