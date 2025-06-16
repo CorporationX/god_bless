2516ef425f929db9049a03a87e9b5762d67d94b9
ddddddasha <daria.zhelobkovich@gmail.com>
2025-06-16 12:17:48 +0300

    Потренируем StreamAPI 3

---
+++ src/main/java/school/faang/module1/bjs2_80802/Employee.java
@@ -0,0 +1,12 @@
+package school.faang.module1.bjs2_80802;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class Employee {
+    private String name;
+    private double salary;
+    private String department;
+}
