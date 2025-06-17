ee0fc516e7585df30d90e9970408deb9f42f746b
White <ilyakrivitskiyn@gmail.com>
2025-06-17 17:49:12 +0300

    Instagram filters

---
+++ src/main/java/school/faang/bjs2_80552/Image.java
@@ -0,0 +1,13 @@
+package school.faang.bjs2_80552;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@Setter
+@Getter
+@AllArgsConstructor
+public class Image {
+    private String name;
+    private String description;
+}
