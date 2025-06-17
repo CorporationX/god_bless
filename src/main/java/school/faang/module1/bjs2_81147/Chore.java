1e9e423d851539764b083248791e38ad86040ede
ddddddasha <daria.zhelobkovich@gmail.com>
2025-06-17 22:20:00 +0300

    They were very nice but extremely poor. добавлена аннотация @RequiredArgsConstructor

--- src/main/java/school/faang/module1/bjs2_81147/Chore.java
+++ src/main/java/school/faang/module1/bjs2_81147/Chore.java
@@ -1,20 +1,17 @@
 package school.faang.module1.bjs2_81147;
 
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
+@RequiredArgsConstructor
 public class Chore implements Runnable {
 
     private final String chore;
     private static final int TIMEOUT = 3000;
 
-    public Chore(String chore) {
-        this.chore = chore;
-    }
-
     @Override
     public void run() {
-
         log.info("Поток {} начинает задачу {}", Thread.currentThread().getName(), chore);
         try {
             Thread.sleep(TIMEOUT);
