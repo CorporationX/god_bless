7bcde6c5d102a557f4f26297673b258c2a438dc9
itechnik <itechnik-swd@gmail.com>
2025-06-17 13:25:36 +0300

    add awaitTermination() to shutdown(), remove Thread.currentThread().getName() from log.error()

--- src/main/java/school/faang/bjs2_81136/Chore.java
+++ src/main/java/school/faang/bjs2_81136/Chore.java
@@ -3,6 +3,8 @@ package school.faang.bjs2_81136;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 
+import java.util.concurrent.TimeUnit;
+
 @Slf4j
 @RequiredArgsConstructor
 public class Chore implements Runnable {
@@ -11,11 +13,11 @@ public class Chore implements Runnable {
     @Override
     public void run() {
         try {
-            log.info("{} выполняет задачу: {}", Thread.currentThread().getName(), choreName);
-            Thread.sleep(5000);
-            log.info("Задача {} выполнена", choreName);
+            log.info("Поток выполняет задачу: {}", choreName);
+            TimeUnit.SECONDS.sleep(3);
+            log.info("Задача \"{}\" успешно выполнена", choreName);
         } catch (InterruptedException e) {
-            log.error("Interrupted while doing the chore {}", Thread.currentThread().getName());
+            log.error("Interrupted while doing the chore: {}", choreName);
             Thread.currentThread().interrupt();
             log.error("Stack trace: ", e);
         }
--- src/main/java/school/faang/bjs2_81136/WeasleyFamily.java
+++ src/main/java/school/faang/bjs2_81136/WeasleyFamily.java
@@ -1,8 +1,12 @@
 package school.faang.bjs2_81136;
 
+import lombok.extern.slf4j.Slf4j;
+
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
 
+@Slf4j
 public class WeasleyFamily {
     private static final String[] chores = {"помыть посуду", "подмести пол", "приготовить ужин", "постирать белье"};
 
@@ -14,5 +18,14 @@ public class WeasleyFamily {
         }
 
         executor.shutdown();
+        try {
+            if (!executor.awaitTermination(1, TimeUnit.MINUTES)) {
+                log.info("Не все задачи завершены в указанный период времени.");
+                executor.shutdownNow();
+            }
+        } catch (InterruptedException e) {
+            log.error("Ошибка при ожидании завершения задач.", e);
+        }
+
     }
 }
