0abfc25817690ca81d260325593e829333356d59
Viktor Romanenko <romanenkoviktor080@gmail.com>
2025-06-17 11:38:04 +0300

    fixes

--- src/main/java/school/faang/bjs2_81095/UserActionAnalyzer.java
+++ src/main/java/school/faang/bjs2_81095/UserActionAnalyzer.java
@@ -2,50 +2,61 @@ package school.faang.bjs2_81095;
 
 import java.time.LocalDate;
 import java.util.Arrays;
+import java.util.Comparator;
 import java.util.List;
 import java.util.Map;
 import java.util.function.Function;
 import java.util.stream.Collectors;
 
 public class UserActionAnalyzer {
+    public static final String HASHTAG_SYMBOL = "#";
+    public static final String SPACE_SYMBOL = "\\s+";
+
+
     public static List<String> topActiveUsers(List<UserAction> list, int limit) {
         return list.stream()
-                .collect(Collectors.groupingBy(
-                        UserAction::getName,
-                        Collectors.counting()
-                ))
-                .entrySet().stream()
-                .sorted(Map.Entry.<String, Long>comparingByValue().reversed())
-                .limit(limit)
-                .map(entry -> entry.getKey() + "(" + entry.getValue() + ")")
-                .toList();
+                .collect(Collectors.collectingAndThen(
+                        Collectors.groupingBy(
+                                UserAction::getName,
+                                Collectors.counting()
+                        ),
+                        map -> map.entrySet().stream()
+                                .sorted(Map.Entry.<String, Long>comparingByValue().reversed())
+                                .limit(limit)
+                                .map(entry -> entry.getKey() + "(" + entry.getValue() + ")")
+                                .toList()
+                ));
     }
 
     public static List<String> topPopularHashtags(List<UserAction> list, int limit) {
         return list.stream()
                 .map(UserAction::getContent)
-                .filter(content -> content.contains("#"))
-                .flatMap(content -> Arrays.stream(content.trim().split("\\s+")))
-                .filter(word -> word.startsWith("#"))
-                .collect(Collectors.groupingBy(
-                        Function.identity(),
-                        Collectors.reducing(0, tag -> 1, Integer::sum)
-                ))
-                //или использовать Collectors.collectingAndThen?
-                .entrySet().stream()
-                .sorted(Map.Entry.<String, Integer>comparingByValue().reversed())
-                .limit(limit)
-                .map(entry -> entry.getKey() + "(" + entry.getValue() + ")")
-                .collect(Collectors.toList());
+                .filter(content -> content.contains(HASHTAG_SYMBOL))
+                .flatMap(content -> Arrays.stream(content.trim().split(SPACE_SYMBOL)))
+                .filter(word -> word.startsWith(HASHTAG_SYMBOL))
+                .collect(Collectors.collectingAndThen(
+                        Collectors.groupingBy(
+                                Function.identity(),
+                                Collectors.reducing(0, tag -> 1, Integer::sum)
+                        ), map ->
+                                map.entrySet().stream()
+                                        .sorted(Map.Entry.<String, Integer>comparingByValue().reversed())
+                                        .limit(limit)
+                                        .map(entry -> entry.getKey() + "(" + entry.getValue() + ")")
+                                        .toList()
+                ));
     }
 
     public static List<String> topCommentersLastMonth(List<UserAction> list, int limit) {
+        LocalDate fromDate = list.stream()
+                .map(UserAction::getActionDate)
+                .max(Comparator.naturalOrder())
+                .orElse(LocalDate.now()).minusMonths(1);
+
         return list.stream()
                 .filter(userAction ->
                         userAction.getActionType() == ActionType.COMMENT
-                                && userAction.getActionDate().isAfter(
-                                LocalDate.now().minusMonths(1)
-                        )
+                                && userAction.getActionDate().isAfter(fromDate)
                 )
                 .collect(Collectors.collectingAndThen(
                         Collectors.groupingBy(
@@ -63,14 +74,15 @@ public class UserActionAnalyzer {
     public static Map<ActionType, Double> actionTypePercentages(List<UserAction> list) {
         int size = list.size();
         return list.stream()
-                .collect(Collectors.groupingBy(
-                        UserAction::getActionType,
-                        Collectors.counting()
-                ))
-                .entrySet().stream()
-                .collect(Collectors.toMap(
-                        Map.Entry::getKey,
-                        entry -> (entry.getValue() * 100.0) / size
-                ));
+                .collect(Collectors.collectingAndThen(
+                        Collectors.groupingBy(
+                                UserAction::getActionType,
+                                Collectors.counting()
+                        ),
+                        map -> map.entrySet().stream().collect(Collectors.toMap(
+                                Map.Entry::getKey,
+                                entry -> (entry.getValue() * 100.0) / size
+                        )))
+                );
     }
 }
