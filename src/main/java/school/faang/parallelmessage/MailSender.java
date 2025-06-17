bd82e12c466a439a6a2e32b5e5bdb094e6780520
mrnght <misterday116@gmail.com>
2025-06-17 15:12:36 +0800

    replaced sout with logs

--- src/main/java/school/faang/parallelmessage/MailSender.java
+++ src/main/java/school/faang/parallelmessage/MailSender.java
@@ -1,15 +1,27 @@
 package school.faang.parallelmessage;
 
+import lombok.extern.slf4j.Slf4j;
 
+@Slf4j
 public class MailSender {
     public static void main(String[] args) {
         int batch = 1000 / 5;
-        Thread[] thread = new Thread[5];
+        Thread[] threads = new Thread[5];
 
         for (int i = 0; i < 5; i++) {
-            thread[i] = new Thread(new SenderRunnable(i * batch, (i + 1) * batch));
-            thread[i].start();
+            threads[i] = new Thread(new SenderRunnable(i * batch, (i + 1) * batch));
+            threads[i].start();
         }
 
+        for (Thread thread : threads) {
+            try {
+                thread.join();
+            } catch (InterruptedException e) {
+                Thread.currentThread().interrupt();
+                log.error("Поток main был прерван.");
+            }
+        }
+
+        log.info("Все письма отправлены!");
     }
 }
