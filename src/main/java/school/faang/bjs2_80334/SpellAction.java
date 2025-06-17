1f22b344d34c3472423a399b18eb89ffab897421
White <ilyakrivitskiyn@gmail.com>
2025-06-17 15:53:10 +0300

    Hogwarts, hogwarts

---
+++ src/main/java/school/faang/bjs2_80334/SpellAction.java
@@ -0,0 +1,6 @@
+package school.faang.bjs2_80334;
+
+@FunctionalInterface
+public interface SpellAction {
+    String action(String spell);
+}
---
+++ src/main/java/school/faang/bjs2_80334/SpellCaster.java
@@ -0,0 +1,7 @@
+package school.faang.bjs2_80334;
+
+public class SpellCaster {
+    public void cast(String spell, SpellAction action) {
+        System.out.println(action.action(spell));
+    }
+}
