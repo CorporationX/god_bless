d981a74f02897cb06460a1b4ca5f0fb01c47f819
licosty <d.chigantseva@gmail.com>
2025-06-17 11:38:00 +0300

    добавлен рассчет писем на поток

--- src/main/java/school/faang/bjs2_81252/MailSender.java
+++ src/main/java/school/faang/bjs2_81252/MailSender.java
@@ -6,22 +6,23 @@ import java.util.List;
 
 @Slf4j
 public class MailSender {
+    private static final int EMAILS_TOTAL = 1000;
     private static final int THREAD_COUNT = 5;
     private static final int START_INDEX = 0;
-    private static final int LIMIT_LETTERS = 200;
+    private static final int EMAILS_PER_THREAD = EMAILS_TOTAL / THREAD_COUNT;
 
     public static void main(String[] args) {
         try {
             int startIndex = START_INDEX;
-            int endIndex = LIMIT_LETTERS;
+            int endIndex = EMAILS_PER_THREAD;
 
             List<Thread> pool = new ArrayList<>(THREAD_COUNT);
 
             for (int i = 0; i < THREAD_COUNT; i++) {
                 Thread thread = new Thread(new SenderRunnable(startIndex, endIndex));
                 pool.add(thread);
-                startIndex += LIMIT_LETTERS;
-                endIndex += LIMIT_LETTERS;
+                startIndex += EMAILS_PER_THREAD;
+                endIndex += EMAILS_PER_THREAD;
 
                 thread.start();
             }
