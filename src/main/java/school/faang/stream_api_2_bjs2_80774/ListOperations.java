1c0dd855dcfd730cfe1ab6303f6148c5611705d8
Setched <kir32kir@gmail.com>
2025-06-18 13:00:06 +0300

    v1

---
+++ src/main/java/school/faang/stream_api_2_bjs2_80774/ListOperations.java
@@ -0,0 +1,24 @@
+package school.faang.stream_api_2_bjs2_80774;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+public class ListOperations {
+    public static Set<List<Integer>> findUniqPairs(Set<Integer> numbers, Integer targetNumber) {
+        return numbers.stream()
+                .filter(n -> n < targetNumber - n && numbers.contains(targetNumber - n))
+                .map(n -> Arrays.asList(n, targetNumber - n))
+                .collect(Collectors.toSet());
+    }
+
+    public static List<String> sortCountries(Map<String, String> countries) {
+        return countries.entrySet().stream().map(n -> n.getValue()).sorted().toList();
+    }
+
+    public static List<String> sortFruits(Character ch, List<String> strings) {
+        return strings.stream().findAny(String::indexOf(ch))
+    }
+}
