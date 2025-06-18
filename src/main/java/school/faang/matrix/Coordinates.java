08f417c8e5c35ffb19fea5656e56cca82f080a53
Danil Pudovkin <dvpudovkin@yandex.ru>
2025-06-18 23:09:29 +0500

    Add solution for Matrix Transformer

---
+++ src/main/java/school/faang/matrix/Coordinates.java
@@ -0,0 +1,10 @@
+package school.faang.matrix;
+
+/**
+ * @param x Индекс строки
+ * @param y Индекс столбца
+ * @author Danil Pudovkin
+ * @since 18.06.2025
+ */
+public record Coordinates(int x, int y) {
+}
