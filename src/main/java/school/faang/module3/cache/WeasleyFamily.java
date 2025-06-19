d4e029b1d66c3bd35e6a24ade89750dd770d251b
myrzakhmet <smagul.myrzakhmet@mail.ru>
2025-06-19 19:27:34 +0500

    changed termination method

--- src/main/java/school/faang/module3/cache/WeasleyFamily.java
+++ src/main/java/school/faang/module3/cache/WeasleyFamily.java
@@ -5,7 +5,7 @@ import java.util.concurrent.Executors;
 import java.util.concurrent.TimeUnit;
 
 public class WeasleyFamily {
-    public static void main(String[] args) throws InterruptedException {
+    public static void main(String[] args) {
         Chore[] chores = new Chore[]{
                 new Chore("Do the dishes"),
                 new Chore("Take out the trash"),
@@ -21,12 +21,20 @@ public class WeasleyFamily {
 
         ExecutorService executor = Executors.newCachedThreadPool();
         for (Chore chore : chores) {
-            executor.submit(chore::run);
+            executor.execute(chore::run);
         }
 
-        executor.awaitTermination(3, TimeUnit.SECONDS);
-        System.out.println("All chores done");
         executor.shutdown();
-        System.out.printf("executor is shutdown ? %s\n", executor.isShutdown());
+        try {
+            if (!executor.awaitTermination(3, TimeUnit.SECONDS)) {
+                System.out.println("not all chores are terminated before timeout. start force termination");
+                executor.shutdownNow();
+            }
+            System.out.println("All chores done");
+        } catch (InterruptedException interruptedException) {
+            System.out.println(interruptedException);
+            executor.shutdownNow();
+        }
+
     }
 }
