2d0f69cc5cc7fd2e3583af19fbb2709febd64d19
Bookinisto <bookinisto@mail.ru>
2025-06-19 19:30:37 +0700

    81272

--- src/main/java/school/faang/microsoft81272/MailSender.java
+++ src/main/java/school/faang/microsoft81272/MailSender.java
@@ -3,13 +3,14 @@ package school.faang.microsoft81272;
 public class MailSender {
     private static final int TOTAL_MESSAGES = 1000;
     private static final int THREADS_COUNT = 5;
+    private static final int BATCH_SIZE = TOTAL_MESSAGES / THREADS_COUNT;
 
     public static void main(String[] args) throws InterruptedException {
-        int batchSize = TOTAL_MESSAGES / THREADS_COUNT;
+
         Thread[] threads = new Thread[THREADS_COUNT];
         for (int i = 0; i < THREADS_COUNT; i++) {
-            int start = i * batchSize;
-            int end = (i + 1) * batchSize;
+            int start = i * BATCH_SIZE;
+            int end = (i + 1) * BATCH_SIZE;
             threads[i] = new Thread(new SenderRunnable(start, end));
             threads[i].start();
         }
--- src/main/java/school/faang/microsoft81272/SenderRunnable.java
+++ src/main/java/school/faang/microsoft81272/SenderRunnable.java
@@ -3,11 +3,13 @@ package school.faang.microsoft81272;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
+import java.util.concurrent.atomic.AtomicInteger;
+
 
 @AllArgsConstructor
 public class SenderRunnable implements Runnable {
     @Getter
-    private static int count = 0;
+    private static AtomicInteger count = new AtomicInteger(0);
     private int startIndex;
     private int endIndex;
 
@@ -17,7 +19,7 @@ public class SenderRunnable implements Runnable {
         for (int i = startIndex; i < endIndex; i++) {
             String nunThread = Thread.currentThread().getName();
             System.out.printf("Письмо %s отправлено из потока %s %n", i, nunThread);
-            count++;
+            count.incrementAndGet();
         }
     }
 }
