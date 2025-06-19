ebd4c20c75737115eebc1f4dcd064e7066fdc2a3
ddddddasha <daria.zhelobkovich@gmail.com>
2025-06-19 23:58:04 +0300

    Bro Force!

---
+++ src/main/java/school/faang/module1/bjs2_82060/Game.java
@@ -0,0 +1,52 @@
+package school.faang.module1.bjs2_82060;
+
+import lombok.extern.slf4j.Slf4j;
+
+import static school.faang.module1.bjs2_82060.UpdaterType.LIVES;
+import static school.faang.module1.bjs2_82060.UpdaterType.SCORE;
+
+@Slf4j
+public class Game {
+
+    private int score = 0;
+    private int lives = 10;
+    private boolean isGameOver = false;
+
+    private final Object scoreLock = new Object();
+    private final Object livesLock = new Object();
+
+    public void update(UpdaterType updaterType) {
+        if (isGameOver) {
+            return;
+        }
+        if (updaterType == SCORE) {
+            synchronized (scoreLock) {
+                if (isGameOver) {
+                    log.warn("Попытка добавить очки после завершения игры");
+                    return;
+                }
+                score++;
+                log.info("Очки увеличены, всего очков: {}", score);
+            }
+        }
+        if (updaterType == LIVES) {
+            synchronized (livesLock) {
+                if (isGameOver) {
+                    return;
+                }
+                if (lives > 0) {
+                    lives--;
+                    log.info("Жизни уменьшены, осталось жизней: {}", lives);
+                    if (lives == 0) {
+                        gameOver();
+                    }
+                }
+            }
+        }
+    }
+
+    private void gameOver() {
+        isGameOver = true;
+        log.info("Все жизни потеряны! игра окончена");
+    }
+}
