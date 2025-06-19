7060f18f468a68886655bcd0612bde8f87d8f859
JasonRon <ubervladocity@gmail.com>
2025-06-20 01:26:45 +0300

    bugfix

--- src/main/java/school/faang/bjs2_80234/EmailProcessor.java
+++ src/main/java/school/faang/bjs2_80234/EmailProcessor.java
@@ -2,7 +2,6 @@ package school.faang.bjs2_80234;
 
 import lombok.extern.slf4j.Slf4j;
 
-import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
 import java.util.function.Consumer;
