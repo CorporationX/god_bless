057fae393681772c8fb957f9f02eaf13f522cdc9
FrolovSI <frolovlab024@gmail.com>
2025-06-19 18:56:00 +0300

    frolovlab BJS2-80589 Instagram Filters

---
+++ src/main/java/school/faang/instagram_filters/FilterProcessor.java
@@ -0,0 +1,22 @@
+package school.faang.instagram_filters;
+
+import java.util.function.Function;
+
+public class FilterProcessor {
+    public Image applyFilter(Image image, Function<Image, Image> function) {
+        if (image == null) {
+            throw new NullPointerException("Image is null");
+        }
+        return function.apply(image);
+    }
+
+    public Function<Image, Image> combineFilters(Function<Image, Image> firstFilter,
+                                                 Function<Image, Image> secondFilter) {
+        return firstFilter.andThen(secondFilter);
+    }
+
+}
+
+
+
+
---
+++ src/main/java/school/faang/instagram_filters/Image.java
@@ -0,0 +1,13 @@
+package school.faang.instagram_filters;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.experimental.Accessors;
+
+@Getter
+@AllArgsConstructor
+public class Image {
+    private String name;
+    private String description;
+}
+
