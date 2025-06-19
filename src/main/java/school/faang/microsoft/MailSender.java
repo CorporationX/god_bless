7a175e590b02f0422ecb57e95c23a7bb5c41b79d
myrzakhmet <smagul.myrzakhmet@mail.ru>
2025-06-19 10:08:27 +0500

    corrected the remarks

--- src/main/java/school/faang/microsoft/MailSender.java
+++ src/main/java/school/faang/microsoft/MailSender.java
@@ -6,13 +6,14 @@ import java.util.List;
 public class MailSender {
     public static final int TOTAL_MESSAGE_COUNT = 1000;
     public static final int THREADS_COUNT = 5;
+    public static final int BATCH_SIZE = TOTAL_MESSAGE_COUNT / THREADS_COUNT;
 
     public static void main(String[] args) throws InterruptedException {
-        int batchSize = TOTAL_MESSAGE_COUNT / THREADS_COUNT;
         Thread[] threads = new Thread[THREADS_COUNT];
         for (int i = 0; i < THREADS_COUNT; i++) {
-            SenderRunnable sender = new SenderRunnable(i + 1,
-                    (i * batchSize) + 1, (i + 1) * batchSize);
+            int startIndex = i * BATCH_SIZE + 1;
+            int endIndex = (i + 1) * BATCH_SIZE;
+            SenderRunnable sender = new SenderRunnable(i + 1, startIndex, endIndex);
             Thread thread = new Thread(sender);
             thread.start();
             threads[i] = thread;
