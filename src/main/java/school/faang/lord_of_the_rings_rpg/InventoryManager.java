ebb69ba500a9ba77ecfc1b786093c9f7a980cc2c
Vlad Nikitin <nikitin.v2407@gmail.com>
2025-06-19 16:16:50 +0300

    внесены корректировки согласно комментариям

--- src/main/java/school/faang/lord_of_the_rings_rpg/InventoryManager.java
+++ src/main/java/school/faang/lord_of_the_rings_rpg/InventoryManager.java
@@ -7,7 +7,7 @@ import java.util.function.Predicate;
 
 public class InventoryManager {
 
-    void addItem(Character character, Item item, Consumer<Item> consumer) {
+    public void addItem(Character character, Item item, Consumer<Item> consumer) {
         character.getInventory().add(item);
         consumer.accept(item);
     }
@@ -18,9 +18,10 @@ public class InventoryManager {
 
     void updateItem(Character character, Predicate<Item> predicate, Function<Item, Item> function) {
         List<Item> items = character.getInventory();
-        for (Item item : items) {
+        for (int i = 0; i < items.size(); i++) {
+            Item item = items.get(i);
             if (predicate.test(item)) {
-                items.set(items.indexOf(item), function.apply(item));
+                items.set(i, function.apply(item));
             }
         }
     }
