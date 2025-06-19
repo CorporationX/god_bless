b55ddd3d206db9ae546379a290df374fc5ed36ec
FrolovSI <frolovlab024@gmail.com>
2025-06-19 19:23:20 +0300

    frolovlab BJS2-80589 Instagram Filters

--- src/main/java/school/faang/instagram_filters/Image.java
+++ src/main/java/school/faang/instagram_filters/Image.java
@@ -2,7 +2,7 @@ package school.faang.instagram_filters;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
-import lombok.experimental.Accessors;
+
 
 @Getter
 @AllArgsConstructor
@@ -10,4 +10,6 @@ public class Image {
     private String name;
     private String description;
 }
+
+
 
