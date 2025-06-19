6848d73a2e64a9684dd26e66e6d7a4daa7d1a054
Maxim Dorofeev <maxim.dp.dorofeev@gmail.com>
2025-06-19 19:13:52 +0300

    80351 for code review

--- src/main/java/school/faang/bjs2_80351/SpellCaster.java
+++ src/main/java/school/faang/bjs2_80351/SpellCaster.java
@@ -1,8 +1,8 @@
 package school.faang.bjs2_80351;
 
 public class SpellCaster {
-    public static void cast (String spellName, SpellAction action) {
-String result = action.perform(spellName);
+    public static void cast(String spellName, SpellAction action) {
+        String result = action.perform(spellName);
         System.out.println(result);
     }
 }
