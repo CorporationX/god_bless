bbf117b62a127d5aeb6c8a7ab53c3037ac7d188d
mrnght <misterday116@gmail.com>
2025-06-19 12:03:48 +0800

    added magic constant and try block

--- src/main/java/school/faang/routine/Chore.java
+++ src/main/java/school/faang/routine/Chore.java
@@ -3,16 +3,18 @@ package school.faang.routine;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 
+
 @Slf4j
 @AllArgsConstructor
 public class Chore implements Runnable {
+    private final int TIME_SLEEP_MS = 1000;
     private final String chore;
 
     @Override
     public void run() {
         log.info("Поток {} начал выполнение задачи: {}", Thread.currentThread().getName(), chore);
         try {
-            Thread.sleep(1000);
+            Thread.sleep(TIME_SLEEP_MS);
         } catch (InterruptedException e) {
             log.error("Поток <<{}>> был прерван", Thread.currentThread().getName());
         }
--- src/main/java/school/faang/routine/WeasleyFamily.java
+++ src/main/java/school/faang/routine/WeasleyFamily.java
@@ -1,9 +1,14 @@
 package school.faang.routine;
 
+import lombok.extern.slf4j.Slf4j;
+
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
 
+@Slf4j
 public class WeasleyFamily {
+    private final int MAX_WAIT_SECONDS = 10;
     private final String[] chores = {"помыть посуду", "подмести пол",
             "приготовить ужин", "погладить кота", "почистить диван"};
 
@@ -15,5 +20,17 @@ public class WeasleyFamily {
         }
 
         executor.shutdown();
+
+        try {
+            if (!executor.awaitTermination(MAX_WAIT_SECONDS, TimeUnit.SECONDS)) {
+                log.info("Не все задачи завершены за {} секунд. Завершаем принудительно...", MAX_WAIT_SECONDS);
+                executor.shutdownNow();
+            }
+        } catch (InterruptedException e) {
+            log.error("Ожидание завершения потоков прервано.");
+            executor.shutdownNow();
+        }
+
+        log.info("Все задачи выполнены!");
     }
 }
