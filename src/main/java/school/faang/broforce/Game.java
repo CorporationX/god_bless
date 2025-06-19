ee8c6bf699527821be444c8dec54f6f11d9f14e9
Danil Pudovkin <dvpudovkin@yandex.ru>
2025-06-19 08:53:56 +0500

    Fix by comments

--- src/main/java/school/faang/broforce/Game.java
+++ src/main/java/school/faang/broforce/Game.java
@@ -3,6 +3,9 @@ package school.faang.broforce;
 import lombok.Getter;
 import lombok.extern.slf4j.Slf4j;
 
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
 /**
  * @author Danil Pudovkin
  * @since 17.06.2025
@@ -10,8 +13,8 @@ import lombok.extern.slf4j.Slf4j;
 @Slf4j
 public class Game {
 
-    private final Object scoreLock = new Object();
-    private final Object livesLock = new Object();
+    private final Lock scoreLock = new ReentrantLock();
+    private final Lock livesLock = new ReentrantLock();
     @Getter
     private int score = 0;
     @Getter
@@ -19,27 +22,34 @@ public class Game {
 
     public void update(EventType eventType) {
         switch (eventType) {
-            case SCORES_UP -> {
-                synchronized (scoreLock) {
-                    score++;
-                    scoreLock.notifyAll();
-                    log.info("+ 1 очко опыта");
-                }
-            }
-            case LIVES_DOWN -> {
-                synchronized (livesLock) {
-                    lives++;
-                    livesLock.notifyAll();
-                    log.info("+ 1 потерянная единица жизни");
-                }
-            }
-            default -> {
+            case SCORES_UP -> tryLockAndDo(scoreLock, () -> {
+                score++;
+                log.info("+ 1 очко опыта");
+            });
+            case LIVES_DOWN -> tryLockAndDo(livesLock, () -> {
+                lives++;
+                log.info("+ 1 потерянная единица жизни");
+            });
+            default -> log.warn("Неизвестный тип события {}", eventType);
+        }
+    }
+
+    private void tryLockAndDo(Lock lock, Runnable action) {
+        boolean success = false;
+        while (!success) {
+            if (lock.tryLock()) {
                 try {
-                    scoreLock.wait();
-                    livesLock.wait();
+                    action.run();
+                    success = true;
+                } finally {
+                    lock.unlock();
+                }
+            } else {
+                try {
+                    Thread.sleep(1);
                 } catch (InterruptedException e) {
                     Thread.currentThread().interrupt();
-                    log.error("Поток {} прерван", Thread.currentThread().getName(), e.getCause());
+                    break;
                 }
             }
         }
