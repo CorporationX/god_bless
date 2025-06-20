031f7b78719aa9531472834e67730d2472394632
Andrey Fomchenko <35478@mail.ru>
2025-06-20 20:47:52 +0500

    BJS2-81051 organized a delivery service

--- src/main/java/school/faang/bjs2_81051/GriffinsFoodDelivery.java
+++ src/main/java/school/faang/bjs2_81051/GriffinsFoodDelivery.java
@@ -6,6 +6,7 @@ import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.ThreadLocalRandom;
 import java.util.concurrent.TimeUnit;
+
 @Slf4j
 public class GriffinsFoodDelivery {
     public static void main(String[] args) {
