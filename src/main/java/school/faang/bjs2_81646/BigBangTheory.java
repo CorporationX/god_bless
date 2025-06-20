7a18e11ef939eb10abbc64747f3156167a36659f
Reverend17 <marklajer1@gmail.com>
2025-06-20 20:13:36 +0300

    big boom, fix, BJS2-81646

--- src/main/java/school/faang/bjs2_81646/BigBangTheory.java
+++ src/main/java/school/faang/bjs2_81646/BigBangTheory.java
@@ -1,10 +1,15 @@
 package school.faang.bjs2_81646;
 
+import lombok.extern.slf4j.Slf4j;
+
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.TimeUnit;
 
+@Slf4j
 public class BigBangTheory {
+    public static final int MAX_WAIT_MINUTES = 1;
+
     public static void main(String[] args) throws InterruptedException {
         String[] names = {"Шелдон", "Леонард", "Говард", "Лало"};
         String[] tasks = {"подготовка теории", "моделирование эксперимента",
@@ -17,6 +22,14 @@ public class BigBangTheory {
         }
 
         executor.shutdown();
-        executor.awaitTermination(10, TimeUnit.SECONDS);
+        try {
+            if (!executor.awaitTermination(MAX_WAIT_MINUTES, TimeUnit.MINUTES)) {
+                log.info("Не все задачи завершены за {} минут. Завершаем принудительно...", MAX_WAIT_MINUTES);
+                executor.shutdownNow();
+            }
+        } catch (InterruptedException e) {
+            log.error("Ожидание завершения потоков прервано.");
+            executor.shutdownNow();
+        }
     }
 }
