e83eea3b24e3034465af86be5c8842662d8b4761
vtvtv26 <victortichonov2001@gmail.com>
2025-06-20 12:36:26 +0300

    Hogwarts Hogwarts

--- src/main/java/school/faang/hogwartshogwartsourbelovedhogwarts/SpellCaster.java
+++ src/main/java/school/faang/hogwartshogwartsourbelovedhogwarts/SpellCaster.java
@@ -1,6 +1,5 @@
 package school.faang.hogwartshogwartsourbelovedhogwarts;
 
-
 public class SpellCaster {
     public void cast(String spellName, SpellAction action) {
         System.out.println(action.castSpell(spellName));
