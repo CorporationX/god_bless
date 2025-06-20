e95674563c9f12d31a7520f17ec42dadd9b42476
Bookinisto <bookinisto@mail.ru>
2025-06-20 18:59:46 +0700

    81240

--- src/main/java/school/faang/weasleyFamily81240/Main.java
+++ src/main/java/school/faang/weasleyFamily81240/Main.java
@@ -6,11 +6,14 @@ import java.util.concurrent.Executors;
 public class Main {
     public static void main(String[] args) throws InterruptedException {
         System.out.println("начинаем работу");
-        try (ExecutorService executor = Executors.newFixedThreadPool(WeasleyFamily.getChores().length)) {
+        ExecutorService executor = Executors.newFixedThreadPool(WeasleyFamily.getChores().length);
+        try {
             for (Chore chore : WeasleyFamily.getChores()) {
                 Thread.sleep(1000);
                 executor.submit(chore);
             }
+        } finally {
+            executor.shutdown();
         }
         Thread.sleep(2000);
         System.out.println("все действия выполненны");
