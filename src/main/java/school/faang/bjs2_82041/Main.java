dab9a33f00537d072a9e80e220dd52f3d9168fef
itechnik <itechnik-swd@gmail.com>
2025-06-20 18:44:55 +0300

    implement Game class

---
+++ src/main/java/school/faang/bjs2_82041/Main.java
@@ -0,0 +1,30 @@
+package school.faang.bjs2_82041;
+
+import lombok.extern.slf4j.Slf4j;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
+
+@Slf4j
+public class Main {
+    public static void main(String[] args) {
+        Game game = new Game(10);
+
+        ExecutorService executor = Executors.newFixedThreadPool(3);
+
+        for (int i = 0; i < 100; i++) {
+            executor.submit(() -> {
+                game.update();
+                try {
+                    TimeUnit.SECONDS.sleep(1);
+                } catch (InterruptedException e) {
+                    log.error("InterruptedException", e);
+                    Thread.currentThread().interrupt();
+                }
+            });
+        }
+
+        executor.shutdown();
+    }
+}
