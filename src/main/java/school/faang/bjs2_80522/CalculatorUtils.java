14203f94883677ebcc396c948d5cb22747b35f38
JasonRon <ubervladocity@gmail.com>
2025-06-20 12:14:42 +0300

    исправил ошибки

--- src/main/java/school/faang/bjs2_80522/CalculatorUtils.java
+++ src/main/java/school/faang/bjs2_80522/CalculatorUtils.java
@@ -3,14 +3,14 @@ package school.faang.bjs2_80522;
 import java.util.List;
 
 public class CalculatorUtils {
-    private static final int START_NUM = 0;
+    private static final int START_INDEX = 0;
 
-    public static int calculate(List<Integer> nums, school.faang.bjs2_80522.Calculator<Integer> calculator) {
+    public static int calculate(List<Integer> nums, Calculator<Integer> calculator) {
         if (nums == null || nums.isEmpty()) {
             throw new IllegalArgumentException("Список не должен быть пустым");
         }
 
-        int result = nums.get(START_NUM);
+        int result = nums.get(START_INDEX);
         for (int i = 1; i < nums.size(); i++) {
             result = calculator.apply(result, nums.get(i));
         }
