b9085e06bb2ce01ba2c5ba94b1221ac6c433f392
Reverend17 <marklajer1@gmail.com>
2025-06-20 11:31:12 +0300

    Chores, fix, BJS2-81182

--- src/main/java/school/faang/bjs2_81182/Chore.java
+++ src/main/java/school/faang/bjs2_81182/Chore.java
@@ -5,11 +5,12 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public class Chore implements Runnable {
     private final String chore;
+    private static final int SLEEP_TIME = 5000;
 
     public void run() {
         try {
             System.out.printf("%s взял на себя задачу: %s%n", Thread.currentThread().getName(), chore);
-            Thread.sleep(5000);
+            Thread.sleep(SLEEP_TIME);
             System.out.printf("задача %s выполнена%n", chore);
         } catch (InterruptedException e) {
             System.out.printf("задача %s прервалась%n", chore);
