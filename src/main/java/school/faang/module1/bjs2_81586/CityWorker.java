97204bae18da874c2e13916df56405b9ae6d8a66
ddddddasha <daria.zhelobkovich@gmail.com>
2025-06-20 20:24:00 +0300

    Путь пальцем проложи…

---
+++ src/main/java/school/faang/module1/bjs2_81586/CityWorker.java
@@ -0,0 +1,41 @@
+package school.faang.module1.bjs2_81586;
+
+import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
+import java.util.Comparator;
+import java.util.List;
+import java.util.Optional;
+
+@AllArgsConstructor
+@Slf4j
+public class CityWorker implements Runnable {
+
+    private City city;
+    private List<Monster> monsters;
+
+    private static final Location WITCHER_CASTLE = new Location(0, 0);
+
+    @Override
+    public void run() {
+        double distanceToCity = calculateDistance(WITCHER_CASTLE, city.getLocation());
+        Optional<Monster> nearestMonsterOpt = monsters.stream()
+                .min(Comparator.comparingDouble(
+                        monster -> calculateDistance(city.getLocation(), monster.getLocation())
+                ));
+        if (nearestMonsterOpt.isPresent()) {
+            Monster nearestMonster = nearestMonsterOpt.get();
+            double distanceToMonster = calculateDistance(city.getLocation(), nearestMonster.getLocation());
+            log.info("Расстояние от замка до города: {}. Расстояние От города до ближайшего монстра: {}", distanceToCity, distanceToMonster);
+        } else {
+            log.info("Для города {} не найдено ни одного монстра", city.getName());
+        }
+    }
+
+    private double calculateDistance(Location location1, Location location2) {
+        int deltaX = location1.getX() - location2.getX();
+        int deltaY = location1.getY() - location2.getY();
+        double distance = Math.sqrt(deltaX * deltaX + deltaY * deltaY);
+        return Math.round(distance * 100.0) / 100.0;
+    }
+}
