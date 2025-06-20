031f77c76edd2e6daecfbdf3974b14d33d8979f1
Dum <dimak22858@gmail.com>
2025-06-21 00:50:57 +0200

    BJS2-80542 1. Исправил вывод в консоль логированием 2. Изменил метод комбинации фильтров

--- src/main/java/school/faang/bjs280542/FilterProcessor.java
+++ src/main/java/school/faang/bjs280542/FilterProcessor.java
@@ -7,7 +7,8 @@ public class FilterProcessor {
         return filter.apply(image);
     }
 
-    public UnaryOperator<Image> combineFilters(UnaryOperator<Image> filtImag1, UnaryOperator<Image> filtImag2) {
-        return image -> filtImag1.compose(filtImag2).apply(image);
+    public UnaryOperator<Image> combineFilters(UnaryOperator<Image> firstFilter, UnaryOperator<Image> secondFilter) {
+        var filter = (UnaryOperator<Image>) firstFilter.andThen(secondFilter);
+        return filter;
     }
 }
--- src/main/java/school/faang/bjs280542/Main.java
+++ src/main/java/school/faang/bjs280542/Main.java
@@ -1,7 +1,10 @@
 package school.faang.bjs280542;
 
+import lombok.extern.slf4j.Slf4j;
+
 import java.util.function.UnaryOperator;
 
+@Slf4j
 public class Main {
     public static void main(String[] args) {
         Image originalImage = new Image("original.jpg", "Оригинальное изображение");
@@ -11,11 +14,11 @@ public class Main {
         UnaryOperator<Image> sepiaFilter =
                 image -> new Image(image.getName(), image.getDescription() + " Фильтр: сепия");
         Image grayscaleImage = filterProcessor.applyFilter(originalImage, grayFilter);
-        System.out.println(grayscaleImage.getDescription());
+        log.info(grayscaleImage.getDescription());
         Image sepiaImage = filterProcessor.applyFilter(grayscaleImage, sepiaFilter);
-        System.out.println(sepiaImage.getDescription());
+        log.info(sepiaImage.getDescription());
         UnaryOperator<Image> combinedFilter = filterProcessor.combineFilters(grayFilter, sepiaFilter);
         Image combinedImage = filterProcessor.applyFilter(originalImage, combinedFilter);
-        System.out.println(combinedImage.getDescription());
+        log.info(combinedImage.getDescription());
     }
 }
