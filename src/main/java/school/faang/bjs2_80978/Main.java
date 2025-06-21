5c149e97157a8a186d603f0cf4d1fff53f350c3b
JasonRon <ubervladocity@gmail.com>
2025-06-21 08:56:27 +0300

    rename

--- src/main/java/school/faang/bjs2_80978/Main.java
+++ src/main/java/school/faang/bjs2_80978/Main.java
@@ -3,23 +3,26 @@ package school.faang.bjs2_80978;
 import java.util.function.BiFunction;
 
 public class Main {
-    private static final BiFunction<Double, Double, Double> ECONOMY
-            = (DISTANCE, TIME) -> 1.0 * DISTANCE + 0.4 * TIME;
-    private static final BiFunction<Double, Double, Double> COMFORT
-            = (DISTANCE, TIME) -> 1.7 * DISTANCE + 1.0 * TIME;
-    private static final BiFunction<Double, Double, Double> PREMIUM
-            = (DISTANCE, TIME) -> 2.0 * DISTANCE + 1.5 * TIME;
+    private static final BiFunction<Double, Double, Double> ECONOMY_INITIALIZATION
+            = (DISTANCE_INITIALIZATION, TIME_INITIALIZATION) -> 1.0 * DISTANCE_INITIALIZATION + 0.4 * TIME_INITIALIZATION;
+    private static final BiFunction<Double, Double, Double> COMFORT_INITIALIZATION
+            = (DISTANCE_INITIALIZATION, TIME_INITIALIZATION) -> 1.7 * DISTANCE_INITIALIZATION + 1.0 * TIME_INITIALIZATION;
+    private static final BiFunction<Double, Double, Double> PREMIUM_INITIALIZATION
+            = (DISTANCE_INITIALIZATION, TIME_INITIALIZATION) -> 2.0 * DISTANCE_INITIALIZATION + 1.5 * TIME_INITIALIZATION;
 
-    private static final double DISTANCE = 10.0;
-    private static final double TIME = 15.0;
+    private static final double DISTANCE_INITIALIZATION = 10.0;
+    private static final double TIME_INITIALIZATION = 15.0;
 
     public static void main(String[] args) {
         FareCalculator calculator = new FareCalculator();
 
-        double economyCost = calculator.calculateFare(DISTANCE, TIME, ECONOMY
-        );
-        double comfortCost = calculator.calculateFare(DISTANCE, TIME, COMFORT);
-        double premiumCost = calculator.calculateFare(DISTANCE, TIME, PREMIUM);
+        double economyCost
+                = calculator.calculateFare(DISTANCE_INITIALIZATION, TIME_INITIALIZATION, ECONOMY_INITIALIZATION);
+        double comfortCost
+                = calculator.calculateFare(DISTANCE_INITIALIZATION, TIME_INITIALIZATION, COMFORT_INITIALIZATION);
+        double premiumCost
+                = calculator.calculateFare(DISTANCE_INITIALIZATION, TIME_INITIALIZATION, PREMIUM_INITIALIZATION);
+
         System.out.println("Economy: " + economyCost);
         System.out.println("Comfort: " + comfortCost);
         System.out.println("Premium: " + premiumCost);
