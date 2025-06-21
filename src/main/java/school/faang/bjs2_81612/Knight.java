cd7217b0b0ffc406065ba0d984643f638d85478d
itechnik <itechnik-swd@gmail.com>
2025-06-21 15:16:45 +0300

    implement Knight & Trial classes

---
+++ src/main/java/school/faang/bjs2_81612/Knight.java
@@ -0,0 +1,27 @@
+package school.faang.bjs2_81612;
+
+import lombok.Getter;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.ExecutorService;
+
+@Getter
+public class Knight {
+    private String name;
+    private List<Trial> trials = new ArrayList<>();
+
+    public Knight(String name) {
+        this.name = name;
+    }
+
+    public void addTrial(Trial trial) {
+        trials.add(trial);
+    }
+
+    public void startTrials(ExecutorService executorService) {
+        for (Trial trial : trials) {
+            executorService.submit(trial);
+        }
+    }
+}
