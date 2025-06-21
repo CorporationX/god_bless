c85a6890bb508e136d34d2889f77fc868c94a8f2
Dum <dimak22858@gmail.com>
2025-06-21 23:32:53 +0200

    BJS2-81369 Первая попытка

---
+++ src/main/java/school/faang/bjs281369/MailSender.java
@@ -0,0 +1,26 @@
+package school.faang.bjs281369;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+public class MailSender {
+    private static final int TOTAL_MESSAGE = 1000;
+    private static final int THREADS_COUNT = 5;
+
+    public static void main(String[] args) throws InterruptedException {
+        int batchSize = TOTAL_MESSAGE / THREADS_COUNT;
+        Thread[] threads = new Thread[THREADS_COUNT];
+        for (int i = 0; i < THREADS_COUNT; i++) {
+            int start = i * batchSize;
+            int end = (i + 1) * batchSize;
+            threads[i] = new Thread(new SenderRunnable(start, end));
+            threads[i].start();
+        }
+
+        for (Thread thread : threads) {
+            thread.join();
+        }
+            log.info("Cообщения отправлены");
+
+    }
+}
