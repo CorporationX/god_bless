8ac657040a8f5ce238ff9a6a909e30127b1ff3a3
Danil Pudovkin <dvpudovkin@yandex.ru>
2025-06-21 22:16:49 +0500

    Add solution for King Tour

---
+++ src/main/java/school/faang/kingtour/Main.java
@@ -0,0 +1,36 @@
+package school.faang.kingtour;
+
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
+
+/**
+ * @author Danil Pudovkin
+ * @since 21.06.2025
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        var knight1Name = "Knight 1";
+        var knight2Name = "Knight 2";
+        var knight1 = new Knight(knight1Name);
+        var knight2 = new Knight(knight2Name);
+        knight1.addTrial(new Trial(knight1Name, "Trial 1"));
+        knight1.addTrial(new Trial(knight1Name, "Trial 2"));
+        knight2.addTrial(new Trial(knight2Name, "Trial 3"));
+        knight2.addTrial(new Trial(knight2Name, "Trial 4"));
+
+        var executor = Executors.newFixedThreadPool(2);
+        knight1.startTrials(executor);
+        knight2.startTrials(executor);
+
+        executor.shutdown();
+        try {
+            if (!executor.awaitTermination(2, TimeUnit.SECONDS)) {
+                executor.shutdownNow();
+            }
+        } catch (InterruptedException e) {
+            Thread.currentThread().interrupt();
+            executor.shutdownNow();
+        }
+    }
+}
