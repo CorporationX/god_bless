bf76820eca3ff6f00498f29d884d337bac6157e5
myrzakhmet <smagul.myrzakhmet@mail.ru>
2025-06-21 15:50:31 +0500

    created photo auto uploader for the google

---
+++ src/main/java/school/faang/module3/google/photo/GooglePhotosAutoUploader.java
@@ -0,0 +1,42 @@
+package school.faang.module3.google.photo;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class GooglePhotosAutoUploader {
+    private Object lock = new Object();
+    private List<String> photosToUpload = new ArrayList<>();
+
+    public void startAutoUpload() {
+        synchronized (lock) {
+            if (photosToUpload.isEmpty()) {
+                try {
+                    System.out.println("wait until someone add photo to the queue");
+                    lock.wait();
+                } catch (InterruptedException e) {
+                    Thread.currentThread().interrupt();
+                    throw new RuntimeException(e);
+                }
+            }
+        }
+
+        uploadPhotos();
+    }
+
+    public void onNewPhotoPath(String photoPath) {
+        synchronized (lock) {
+            photosToUpload.add(photoPath);
+            System.out.println("add photo to the queue");
+            lock.notify();
+        }
+    }
+
+    private void uploadPhotos() {
+        synchronized (lock) {
+            for (String path : photosToUpload) {
+                System.out.printf("photo in path %s uploaded\n", path);
+            }
+            photosToUpload.clear();
+        }
+    }
+}
