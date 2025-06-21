ee9de184f2d3b396964e78de5ea9c74b109fd6d0
yagor. <warneverchang3s@gmail.com>
2025-06-21 21:58:31 +0300

    Вы работаете в Microsoft! - правки по pr

--- src/main/java/school/faang/microsoft/SenderRunnable.java
+++ src/main/java/school/faang/microsoft/SenderRunnable.java
@@ -11,7 +11,7 @@ public class SenderRunnable implements Runnable {
     @Override
     public void run() {
         for (int i = startIndex; i < endIndex; i++) {
-            System.out.println("Message send " + i);
+            System.out.printf("Message send %d; ", i);
         }
     }
 }
