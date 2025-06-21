cbf5bdfbea79fc956b121afb371c9ebeb38cbb1d
Vlad Nikitin <nikitin.v2407@gmail.com>
2025-06-21 13:34:51 +0300

    внес изменения согласно комментариям

--- src/main/java/school/faang/microsoft/MailSender.java
+++ src/main/java/school/faang/microsoft/MailSender.java
@@ -1,11 +1,15 @@
 package school.faang.microsoft;
 
 public class MailSender {
+    private static final int ALL_EMAIL = 1000;
+    private static final int COUNT_FLOW = 5;
+
     public static void main(String[] args) throws InterruptedException {
-        Thread[] threads = new Thread[5];
-        for (int i = 0; i < 5; i++) {
-            int first = i * 200;
-            int last = (i + 1) * 200;
+        Thread[] threads = new Thread[COUNT_FLOW];
+        int emailOneFlow = ALL_EMAIL / COUNT_FLOW;
+        for (int i = 0; i < COUNT_FLOW; i++) {
+            int first = i * emailOneFlow;
+            int last = (i + 1) * emailOneFlow;
             threads[i] = new Thread(new SenderRunnable(first, last));
             threads[i].start();
         }
