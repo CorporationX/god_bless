59258affff851b3de9b59341fe27589d1109495e
Volodymyr <dqkvii@gmail.com>
2025-06-21 17:32:06 +0200

    BJS2-81157

---
+++ src/main/java/school/faang/bjs281157/UserAction.java
@@ -0,0 +1,21 @@
+package school.faang.bjs281157;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+import java.time.LocalDate;
+@AllArgsConstructor
+@NoArgsConstructor
+@Setter
+@Getter
+@ToString
+public class UserAction {
+    private int id;
+    private String name;
+    private ActionType actionType;
+    private LocalDate actionDate;
+    private String content;
+}
