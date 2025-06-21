e852f6ff5ebb0a98c500df5bc867d74303d6b609
Dum <dimak22858@gmail.com>
2025-06-22 01:12:32 +0200

    BJS2-81213 Первая попытка Философия "Без власти над собой невозможно обрести свободу".
    Эпиктет

---
+++ src/main/java/school/faang/bjs281213/Swordsman.java
@@ -0,0 +1,7 @@
+package school.faang.bjs281213;
+
+public class Swordsman extends Unit {
+    public Swordsman(int power) {
+        super(power);
+    }
+}
---
+++ src/main/java/school/faang/bjs281213/Unit.java
@@ -0,0 +1,10 @@
+package school.faang.bjs281213;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@AllArgsConstructor
+@Data
+public abstract class Unit {
+    private int power;
+}
