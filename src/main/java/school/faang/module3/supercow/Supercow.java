b80eaa842465aeb5345400cd6fb10c068d22cea4
myrzakhmet <smagul.myrzakhmet@mail.ru>
2025-06-22 19:19:35 +0500

    optimize import

--- src/main/java/school/faang/module3/supercow/Supercow.java
+++ src/main/java/school/faang/module3/supercow/Supercow.java
@@ -1,7 +1,5 @@
 package school.faang.module3.supercow;
 
-import java.util.ArrayList;
-import java.util.List;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.TimeUnit;
