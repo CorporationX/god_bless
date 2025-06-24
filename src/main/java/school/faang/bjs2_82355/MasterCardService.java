c22eeb1244a8346f656558ab135b40453e2319af
licosty <d.chigantseva@gmail.com>
2025-06-24 11:30:31 +0300

    - ExecutorService вынесен в Main - doAll переименован processPaymentWithAnalytics

--- src/main/java/school/faang/bjs2_82355/MasterCardService.java
+++ src/main/java/school/faang/bjs2_82355/MasterCardService.java
@@ -1,19 +1,21 @@
 package school.faang.bjs2_82355;
 
 import lombok.extern.slf4j.Slf4j;
-
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
 import java.util.concurrent.Future;
-import java.util.concurrent.TimeUnit;
 
 @Slf4j
 public class MasterCardService {
     private static final int TEN_SECONDS_IN_MS = 10_000;
     private static final int ONE_SECOND_IN_MS = 1_000;
-    private static final long POOL_AWAIT_TIMEOUT = 20;
+
+    private ExecutorService threadPool;
+
+    public MasterCardService(ExecutorService threadPool) {
+        this.threadPool = threadPool;
+    }
 
     public static String collectPayment() {
         log.info("Начата транзакция по оплате...");
@@ -41,33 +43,20 @@ public class MasterCardService {
         }
     }
 
-    public void doAll() {
-        ExecutorService fixedPool = Executors.newFixedThreadPool(2);
-        Future<String> collectPaymentResult = fixedPool.submit(MasterCardService::collectPayment);
+    public void processPaymentWithAnalytics() {
+        Future<String> collectPaymentResult = threadPool.submit(MasterCardService::collectPayment);
         CompletableFuture<String> sendAnalyticsResult =
-                CompletableFuture.supplyAsync(MasterCardService::sendAnalytics, fixedPool);
+                CompletableFuture.supplyAsync(MasterCardService::sendAnalytics, threadPool);
 
         try {
             log.info("Результат отправки аналитики: {}", sendAnalyticsResult.join());
             log.info("Результат отправки оплаты: {}", collectPaymentResult.get());
 
-            fixedPool.shutdown();
-
-            while (!fixedPool.awaitTermination(POOL_AWAIT_TIMEOUT, TimeUnit.SECONDS)) {
-                log.info("Не все задачи завершены");
-                fixedPool.shutdownNow();
-            }
-
         } catch (InterruptedException e) {
             log.error("Поток был прерван", e);
             Thread.currentThread().interrupt();
-            fixedPool.shutdownNow();
         } catch (ExecutionException e) {
             log.error("Не удалось получить результат оплаты", e);
-        } catch (RuntimeException e) {
-            Throwable cause = e.getCause();
-            log.error("Ошибка выполнения задачи: ", cause);
         }
-        log.info("Все задачи завершены");
     }
 }
