77092e42b812980bd95f94285313ccfee5813a9a
Bookinisto <bookinisto@mail.ru>
2025-06-24 23:10:41 +0700

    81992

--- src/main/java/school/faang/googlephoto81992/GooglePhotosAutoUploader.java
+++ src/main/java/school/faang/googlephoto81992/GooglePhotosAutoUploader.java
@@ -1,6 +1,8 @@
 package school.faang.googlephoto81992;
 
 import lombok.Getter;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -9,18 +11,14 @@ public class GooglePhotosAutoUploader {
     private final Object lock = new Object();
     @Getter
     private List<String> photosToUpload = new ArrayList<>();
+    private static final Logger logger = LoggerFactory.getLogger(GooglePhotosAutoUploader.class);
 
     public void startAutoUpload() throws InterruptedException {
-
         while (true) {
             synchronized (lock) {
                 while (photosToUpload.isEmpty()) {
-                    try {
-                        System.out.println("фотографий готовых к загрузке нет");
-                        lock.wait();
-                    } catch (InterruptedException e) {
-                        e.printStackTrace();
-                    }
+                    System.out.println("фотографий готовых к загрузке нет");
+                    lock.wait();
                 }
                 uploadPhotos();
             }
@@ -30,13 +28,13 @@ public class GooglePhotosAutoUploader {
     public void onNewPhotoAdded(String photoPath) {
         synchronized (lock) {
             photosToUpload.add(photoPath);
-            lock.notify();
+            lock.notifyAll();
         }
     }
 
-    public void uploadPhotos() throws InterruptedException {
+    public void uploadPhotos()  {
         for (String photo : photosToUpload) {
-            System.out.printf("фото с адресом %s загруженно на сайт %n", photo);
+            logger.info("Фото с адресом {} загружено на сайт", photo);
         }
         photosToUpload.clear();
     }
