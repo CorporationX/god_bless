97b53759b6f5d8bc26ab071317663d924790a2d4
ddddddasha <daria.zhelobkovich@gmail.com>
2025-06-24 13:37:34 +0300

    Асинхронность и Future

---
+++ src/main/java/school/faang/module1/bjs2_82369/Main.java
@@ -0,0 +1,8 @@
+package school.faang.module1.bjs2_82369;
+
+public class Main {
+    public static void main(String[] args) {
+        MasterCardService service = new MasterCardService();
+        service.doAll();
+    }
+}
---
+++ src/main/java/school/faang/module1/bjs2_82369/MasterCardService.java
@@ -0,0 +1,68 @@
+package school.faang.module1.bjs2_82369;
+
+import lombok.extern.slf4j.Slf4j;
+
+import java.util.concurrent.*;
+
+@Slf4j
+public class MasterCardService {
+
+    private static final int TEN_SECONDS_IN_MS = 10_000;
+    private static final int ONE_SECOND_IN_MS = 1_000;
+    private static final int TIMEOUT = 5;
+    private static final int PAYMENT_AMOUNT = 5_000;
+    private static final int ANALYTICS_CODE = 17_000;
+
+    private int collectPayment() {
+        try {
+            Thread.sleep(TEN_SECONDS_IN_MS);
+            return PAYMENT_AMOUNT;
+        } catch (InterruptedException e) {
+            Thread.currentThread().interrupt();
+            throw new RuntimeException(e);
+        }
+    }
+
+    private int sendAnalytics() {
+        try {
+            Thread.sleep(ONE_SECOND_IN_MS);
+            return ANALYTICS_CODE;
+        } catch (InterruptedException e) {
+            Thread.currentThread().interrupt();
+            throw new RuntimeException(e);
+        }
+    }
+
+    public void doAll() {
+        ExecutorService executorService = Executors.newSingleThreadExecutor();
+
+        Future<Integer> paymentFuture = executorService.submit(this::collectPayment);
+        CompletableFuture<Integer> analyticsFuture = CompletableFuture.supplyAsync(this::sendAnalytics);
+
+        try {
+            int paymentResult = paymentFuture.get();
+            log.info("Обработан платеж: {}", paymentResult);
+            int analyticsResult = analyticsFuture.get();
+            log.info("Аналитика: {}", analyticsResult);
+        } catch (InterruptedException | ExecutionException e) {
+            Thread.currentThread().interrupt();
+            throw new RuntimeException(e);
+        } finally {
+            shutdownExecutor(executorService);
+        }
+    }
+
+    private void shutdownExecutor(ExecutorService executor) {
+        executor.shutdown();
+        try {
+            if (!executor.awaitTermination(TIMEOUT, TimeUnit.SECONDS)) {
+                log.warn("Задачи не завершились вовремя, принудительная остановка");
+                executor.shutdownNow();
+            }
+        } catch (InterruptedException e) {
+            log.error("Ожидание завершения потока прервано");
+            executor.shutdownNow();
+            Thread.currentThread().interrupt();
+        }
+    }
+}
