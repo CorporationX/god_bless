57c88ed0293b5dd2c307058923decdc5548f115f
Askar <asklooka@gmail.com>
2025-06-24 15:36:04 -0400

    BJS2-81327

--- src/main/java/school/faang/bjs280861/MailSender.java
+++ src/main/java/school/faang/bjs280861/MailSender.java
@@ -1,13 +1,24 @@
 package school.faang.bjs280861;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class MailSender {
+    private static final int THREAD_COUNT = 5;
+    private static final int TOTAL_MESSAGES = 1000;
+    private static final int EMAILS_PER_THREAD = TOTAL_MESSAGES / THREAD_COUNT;
+
     public static void main(String[] args) throws InterruptedException {
+        Thread[] threads = new Thread[THREAD_COUNT];
+        for (int i = 0; i < THREAD_COUNT; i++) {
+            int startindex = i * EMAILS_PER_THREAD;
+            int end = startindex + EMAILS_PER_THREAD;
 
+            threads[i] = new Thread(new Thread(new SenderRunnable(startindex, end)));
+            threads[i].start();
+        }
 
-        for (int i = 0; i < 5; i++) {
-            SenderRunnable senderRunnable = new SenderRunnable(0, 200);
-            Thread thread = new Thread(senderRunnable);
-            thread.start();
+        for (Thread thread: threads) {
             thread.join();
         }
 
