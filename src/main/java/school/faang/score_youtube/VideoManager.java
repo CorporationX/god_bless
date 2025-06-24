9410a107d6d300131f9e1063b0c1687aba31e380
Vlad Nikitin <nikitin.v2407@gmail.com>
2025-06-24 22:10:08 +0300

    внес изменения согласно комментариям

--- src/main/java/school/faang/score_youtube/VideoManager.java
+++ src/main/java/school/faang/score_youtube/VideoManager.java
@@ -11,18 +11,13 @@ public class VideoManager {
 
     public synchronized void addView(String videoId) {
         if (viewsMap.containsKey(videoId)) {
-            viewsMap.put(videoId, viewsMap.get(videoId) + 1);
+            viewsMap.merge(videoId, 1, Integer::sum);
         } else {
             viewsMap.put(videoId, 1);
         }
     }
 
     public synchronized int getViewCount(String videoId) {
-        if (!viewsMap.containsKey(videoId)) {
-            System.out.println("Такого фильма нет");
-            return 1;
-        } else {
-            return viewsMap.get(videoId) + 1;
-        }
+        return viewsMap.getOrDefault(videoId, 0) + 1;
     }
 }
