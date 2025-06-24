c73a96bad6dbd2e407f2f16df8cdda3f29480f10
Askar <asklooka@gmail.com>
2025-06-24 15:36:47 -0400

    BJS2-81327

--- src/main/java/school/faang/bjs280861/MailSender.java
+++ src/main/java/school/faang/bjs280861/MailSender.java
@@ -18,7 +18,7 @@ public class MailSender {
             threads[i].start();
         }
 
-        for (Thread thread: threads) {
+        for (Thread thread : threads) {
             thread.join();
         }
 
