e7bd25295e9474d82aab381d68ff5383d90acf74
Danil Pudovkin <dvpudovkin@yandex.ru>
2025-06-24 10:23:52 +0500

    Update LibrarySystem

--- src/main/java/school/faang/vesteros/Main.java
+++ src/main/java/school/faang/vesteros/Main.java
@@ -7,16 +7,17 @@ package school.faang.vesteros;
 public class Main {
 
     public static void main(String[] args) {
-        LibrarySystem.addBook("Book 1", "Author 1", 1970, "Location 1");
-        LibrarySystem.addBook("Book 2", "Author 2", 1971, "Location 2");
-        LibrarySystem.addBook("Book 3", "Author 3", 1972, "Location 3");
-        LibrarySystem.addBook("Book 4", "Author 4", 1973, "Location 4");
+        var librarySystem = new LibrarySystem();
+        librarySystem.addBook("Book 1", "Author 1", 1970, "Location 1");
+        librarySystem.addBook("Book 2", "Author 2", 1971, "Location 2");
+        librarySystem.addBook("Book 3", "Author 3", 1972, "Location 3");
+        librarySystem.addBook("Book 4", "Author 4", 1973, "Location 4");
 
-        LibrarySystem.removeBook("Book 4", "Author 4", 1973);
+        librarySystem.removeBook("Book 4", "Author 4", 1973);
 
-        var location = LibrarySystem.findBook("Book 3", "Author 3", 1972);
+        var location = librarySystem.findBook("Book 3", "Author 3", 1972);
         System.out.println("Book 3: " + location + "\n");
 
-        LibrarySystem.printBooks();
+        librarySystem.printBooks();
     }
 }
