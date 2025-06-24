00a2f5de486542fbea52058adcd85301939e44a3
Danil Pudovkin <dvpudovkin@yandex.ru>
2025-06-24 10:11:35 +0500

    Fix by comments

--- src/main/java/school/faang/telegram/TelegramBot.java
+++ src/main/java/school/faang/telegram/TelegramBot.java
@@ -2,9 +2,6 @@ package school.faang.telegram;
 
 import lombok.extern.slf4j.Slf4j;
 
-import java.util.concurrent.atomic.AtomicInteger;
-import java.util.concurrent.atomic.AtomicLong;
-
 /**
  * @author Danil Pudovkin
  * @since 22.06.2025
@@ -15,23 +12,18 @@ public class TelegramBot {
     private static final int REQUEST_LIMIT = 5;
     private static final int ONE_SECOND_MS = 1000;
 
-    private final AtomicInteger requestCounter;
-    private final AtomicLong lastRequestTime;
+    private int requestCounter = 0;
+    private long lastRequestTime = System.currentTimeMillis();
 
-    public TelegramBot() {
-        this.requestCounter = new AtomicInteger(0);
-        this.lastRequestTime = new AtomicLong(System.currentTimeMillis());
-    }
-
-    public void sendMessage(String message) {
+    public synchronized void sendMessage(String message) {
         var currentTime = System.currentTimeMillis();
-        var timePassed = currentTime - lastRequestTime.get();
+        var timePassed = currentTime - lastRequestTime;
         if (timePassed < ONE_SECOND_MS) {
-            requestCounter.incrementAndGet();
-            if (requestCounter.get() > REQUEST_LIMIT) {
+            requestCounter++;
+            if (requestCounter > REQUEST_LIMIT) {
                 try {
                     log.info("Превышено максимальное количество запросов в секунду. Ожидание...");
-                    Thread.sleep(Math.max(1, ONE_SECOND_MS - timePassed));
+                    wait(Math.max(1, ONE_SECOND_MS - timePassed));
                 } catch (InterruptedException e) {
                     Thread.currentThread().interrupt();
                     log.error("Поток прерван: {}", e.getMessage());
@@ -39,8 +31,8 @@ public class TelegramBot {
                 }
             }
         } else {
-            requestCounter.getAndSet(0);
-            lastRequestTime.getAndSet(System.currentTimeMillis());
+            requestCounter = 0;
+            lastRequestTime = System.currentTimeMillis();
         }
         log.info("Отправка сообщения: {}", message);
     }
