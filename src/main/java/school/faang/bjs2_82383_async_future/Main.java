ac3251c0be66891b41aa824c42e540c759cfa4e2
Jevgeni Rogotski <jevgeni.rogotski@gmail.com>
2025-06-24 11:34:15 +0300

    MasterCard future async. First solution

---
+++ src/main/java/school/faang/bjs2_82383_async_future/Main.java
@@ -0,0 +1,8 @@
+package school.faang.bjs2_82383_async_future;
+
+public class Main {
+    public static void main(String[] args) {
+        MasterCardService mcService = new MasterCardService();
+        mcService.doAll();
+    }
+}
---
+++ src/main/java/school/faang/bjs2_82383_async_future/MasterCardService.java
@@ -0,0 +1,58 @@
+package school.faang.bjs2_82383_async_future;
+
+import lombok.extern.slf4j.Slf4j;
+import school.faang.utils.ThreadUtils;
+
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+
+@Slf4j
+public class MasterCardService {
+
+    private static final int TEN_SECONDS_IN_MS = 10_000;
+    private static final int ONE_SECOND_IN_MS = 1_000;
+    private final ExecutorService executorService = Executors.newCachedThreadPool();
+
+    private int collectPayment() {
+        try {
+            Thread.sleep(TEN_SECONDS_IN_MS);
+            return 5_000;
+        } catch (InterruptedException e) {
+            Thread.currentThread().interrupt();
+            throw new RuntimeException(e);
+        }
+    }
+
+    private int sendAnalytics() {
+        try {
+            Thread.sleep(ONE_SECOND_IN_MS);
+            return 17_000;
+        } catch (InterruptedException e) {
+            Thread.currentThread().interrupt();
+            throw new RuntimeException(e);
+        }
+    }
+
+    public void doAll() {
+        Future<Integer> paymentFuture = executorService.submit(this::collectPayment);
+        CompletableFuture<Void> analyticsFuture = CompletableFuture
+                .supplyAsync(this::sendAnalytics, executorService)
+                .thenAccept(sum -> log.info("Analytics result: {}", sum.toString()));
+
+        analyticsFuture.join();
+        try {
+            Integer paymentResult = paymentFuture.get();
+            log.info("Payment result: {}", paymentResult.toString());
+        } catch (InterruptedException | ExecutionException e) {
+            log.error("Payment processing could not complete: {}", e.getMessage());
+            throw new RuntimeException(e);
+        }
+
+        ThreadUtils.executorGracefulShutdown(executorService, 1);
+
+    }
+
+}
