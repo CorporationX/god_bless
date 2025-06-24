bf106a9878d697fec5d2dbb709b977cd9cd4c7a2
Danil Pudovkin <dvpudovkin@yandex.ru>
2025-06-24 10:43:08 +0500

    Add solution for Async & Future

---
+++ src/main/java/school/faang/future/exception/CollectPaymentException.java
@@ -0,0 +1,11 @@
+package school.faang.future.exception;
+
+/**
+ * @author Danil Pudovkin
+ * @since 24.06.2025
+ */
+public class CollectPaymentException extends RuntimeException {
+    public CollectPaymentException(Throwable cause) {
+        super(cause);
+    }
+}
