63c05957e2dac5efd0e53ccfb42287d7986a2b32
licosty <d.chigantseva@gmail.com>
2025-06-25 11:43:31 +0300

    - использование AtomicInteger для id

--- src/main/java/school/faang/bjs2_82188/Order.java
+++ src/main/java/school/faang/bjs2_82188/Order.java
@@ -2,14 +2,16 @@ package school.faang.bjs2_82188;
 
 import lombok.Getter;
 
+import java.util.concurrent.atomic.AtomicInteger;
+
 @Getter
 public class Order {
-    private static int currentId;
+    private static AtomicInteger currentId = new AtomicInteger(0);
     private final int id;
     private OrderStatus status;
 
     public Order() {
-        this.id = ++currentId;
+        this.id = currentId.incrementAndGet();
         this.status = OrderStatus.CREATED;
     }
 
