502d31003a8a439f0c22b53cf047996d7d8cafcd
mrnght <misterday116@gmail.com>
2025-06-25 15:35:59 +0800

    added two getters

--- src/main/java/school/faang/tournament/Student.java
+++ src/main/java/school/faang/tournament/Student.java
@@ -4,10 +4,10 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 
 @AllArgsConstructor
+@Getter
 public class Student {
     private final String name;
     private final int year;
-    @Getter
     private int points;
 
     public void addPoints(int points) {
