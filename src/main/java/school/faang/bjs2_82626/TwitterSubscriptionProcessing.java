f337ca6106cfc79a16bccc88e644de75d1e482f5
licosty <d.chigantseva@gmail.com>
2025-06-26 14:06:14 +0300

    - убран fixedPool для запуска задач через стандартный commonPool

--- src/main/java/school/faang/bjs2_82626/TwitterSubscriptionProcessing.java
+++ src/main/java/school/faang/bjs2_82626/TwitterSubscriptionProcessing.java
@@ -4,19 +4,13 @@ import lombok.extern.slf4j.Slf4j;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.CompletableFuture;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
-import java.util.concurrent.TimeUnit;
 
 @Slf4j
 public class TwitterSubscriptionProcessing {
-    private static final long POOL_AWAIT_TIMEOUT = 20;
     private static final long FOLLOWERS_COUNT = 200_000;
 
-    private static ExecutorService fixedPool = Executors.newFixedThreadPool(Runtime.getRuntime().availableProcessors());
-
     public static void main(String[] args) {
-        TwitterSubscriptionSystem system = new TwitterSubscriptionSystem(fixedPool);
+        TwitterSubscriptionSystem system = new TwitterSubscriptionSystem();
         TwitterAccount account = new TwitterAccount("Me");
 
         List<CompletableFuture<Void>> futures = new ArrayList<>();
@@ -26,17 +20,5 @@ public class TwitterSubscriptionProcessing {
 
         CompletableFuture.allOf(futures.toArray(new CompletableFuture[0])).join();
         log.info("Количество подписчиков {}: {}", account.getUsername(), account.getFollowers().get());
-
-        fixedPool.shutdown();
-        try {
-            if (!fixedPool.awaitTermination(POOL_AWAIT_TIMEOUT, TimeUnit.SECONDS)) {
-                log.info("Еще не все задачи завершены");
-                fixedPool.shutdownNow();
-            }
-        } catch (InterruptedException e) {
-            log.error("Поток был прерван");
-            fixedPool.shutdownNow();
-        }
-        log.info("Все задачи завершены");
     }
 }
--- src/main/java/school/faang/bjs2_82626/TwitterSubscriptionSystem.java
+++ src/main/java/school/faang/bjs2_82626/TwitterSubscriptionSystem.java
@@ -1,18 +1,13 @@
 package school.faang.bjs2_82626;
 
-import lombok.RequiredArgsConstructor;
 import java.util.concurrent.CompletableFuture;
-import java.util.concurrent.ExecutorService;
 
-@RequiredArgsConstructor
 public class TwitterSubscriptionSystem {
-    private final ExecutorService threadPool;
-
     private void addFollower(TwitterAccount account) {
         account.getFollowers().incrementAndGet();
     }
 
     public CompletableFuture<Void> followAccount(TwitterAccount account) {
-        return CompletableFuture.runAsync(() -> addFollower(account), threadPool);
+        return CompletableFuture.runAsync(() -> addFollower(account));
     }
 }
