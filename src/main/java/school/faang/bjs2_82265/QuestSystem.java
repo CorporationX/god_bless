d66b1d3560e9cae99f827607f4def570b3d78249
itechnik <itechnik-swd@gmail.com>
2025-06-27 00:02:56 +0300

    pass ExecutorService as second argument

--- src/main/java/school/faang/bjs2_82265/QuestSystem.java
+++ src/main/java/school/faang/bjs2_82265/QuestSystem.java
@@ -3,10 +3,14 @@ package school.faang.bjs2_82265;
 import lombok.extern.slf4j.Slf4j;
 
 import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 import java.util.concurrent.TimeUnit;
 
 @Slf4j
 public class QuestSystem {
+    private final ExecutorService executorService = Executors.newSingleThreadExecutor();
+
     public CompletableFuture<Player> startQuest(Player player, Quest quest) {
         return CompletableFuture.supplyAsync(() -> {
             try {
@@ -19,6 +23,6 @@ public class QuestSystem {
                 throw new RuntimeException(e);
             }
             return player;
-        });
+        }, executorService);
     }
 }
