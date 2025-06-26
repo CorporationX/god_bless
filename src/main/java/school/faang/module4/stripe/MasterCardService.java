2a3fc4bb883fea4fa9fe74cb50ab627b8805df42
myrzakhmet <smagul.myrzakhmet@mail.ru>
2025-06-26 10:44:45 +0500

    corrected remarks

--- src/main/java/school/faang/module4/stripe/MasterCardService.java
+++ src/main/java/school/faang/module4/stripe/MasterCardService.java
@@ -1,6 +1,7 @@
 package school.faang.module4.stripe;
 
 import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutionException;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.Future;
@@ -11,11 +12,13 @@ public class MasterCardService {
     private static final long ANALYZE_TIME_IN_MS = 1_000L;
     private static final long TERMINATION_TIME_IN_S = 11L;
     private static final int THREAD_COUNT = 2;
+    private static final int COLLECT_DEFAULT_VALUE = 5_000;
+    private static final int ANALYTICS_DEFAULT_VALUE = 17_000;
 
     public int collectPayment() {
         try {
             Thread.sleep(COLLECT_TIME_IN_MS);
-            return 5_000;
+            return COLLECT_DEFAULT_VALUE;
         } catch (InterruptedException e) {
             Thread.currentThread().interrupt();
             throw new RuntimeException(e);
@@ -25,7 +28,7 @@ public class MasterCardService {
     public int sendAnalytics() {
         try {
             Thread.sleep(ANALYZE_TIME_IN_MS);
-            return 17_000;
+            return ANALYTICS_DEFAULT_VALUE;
         } catch (InterruptedException e) {
             Thread.currentThread().interrupt();
             throw new RuntimeException(e);
@@ -34,11 +37,22 @@ public class MasterCardService {
 
     public void doAll() {
         ExecutorService executors = Executors.newFixedThreadPool(THREAD_COUNT);
-        CompletableFuture<Integer> collector = CompletableFuture.supplyAsync(this::collectPayment, executors);
-        collector.thenAccept((sum) -> System.out.printf("payment collected: %d\n", sum));
+        Future<Integer> collector = executors.submit(this::collectPayment);
 
         CompletableFuture<Integer> analyzer = CompletableFuture.supplyAsync(this::sendAnalytics, executors);
         analyzer.thenAccept((sum) -> System.out.printf("analytics sent: %d\n", sum));
+
+        executors.execute(() -> {
+            try {
+                System.out.printf("payment collected: %d\n", collector.get());
+            } catch (InterruptedException e) {
+                Thread.currentThread().interrupt();
+                throw new RuntimeException(e);
+            } catch (ExecutionException e) {
+                Thread.currentThread().interrupt();
+                throw new RuntimeException(e);
+            }
+        });
 
         executors.shutdown();
         try {
