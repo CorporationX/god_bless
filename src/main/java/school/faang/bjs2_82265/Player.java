9701b8b7bc1abe951c2009cc8613ff85aa3f9c65
itechnik <itechnik-swd@gmail.com>
2025-06-26 13:18:33 +0300

    implement QuestSystem, Player & Quest classes

---
+++ src/main/java/school/faang/bjs2_82265/Player.java
@@ -0,0 +1,18 @@
+package school.faang.bjs2_82265;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+public class Player {
+    private String name;
+    private int level;
+    private int experience;
+
+    public void addExperience(int experience) {
+        this.experience += experience;
+    }
+}
---
+++ src/main/java/school/faang/bjs2_82265/QuestSystem.java
@@ -0,0 +1,24 @@
+package school.faang.bjs2_82265;
+
+import lombok.extern.slf4j.Slf4j;
+
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.TimeUnit;
+
+@Slf4j
+public class QuestSystem {
+    public CompletableFuture<Player> startQuest(Player player, Quest quest) {
+        return CompletableFuture.supplyAsync(() -> {
+            try {
+                log.info("{} is starting quest {}", player.getName(), quest.getName());
+                TimeUnit.SECONDS.sleep(quest.getDifficulty());
+                player.addExperience(quest.getReward());
+            } catch (InterruptedException e) {
+                log.error("Error while sleeping", e);
+                Thread.currentThread().interrupt();
+                throw new RuntimeException(e);
+            }
+            return player;
+        });
+    }
+}
