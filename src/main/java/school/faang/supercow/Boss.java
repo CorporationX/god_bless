1272740e6c90ff517c637c8fd94657c28998cab4
Vlad Nikitin <nikitin.v2407@gmail.com>
2025-06-26 16:33:14 +0300

    внес изменения согласно комментариям

--- src/main/java/school/faang/supercow/Boss.java
+++ src/main/java/school/faang/supercow/Boss.java
@@ -4,31 +4,36 @@ import lombok.Data;
 
 @Data
 public class Boss {
-    private final int maxPlayer;
+    private final int maxPlayers;
     private final Object lock = new Object();
     private int currentPlayers = 0;
 
     public void joinBattle(Player player) {
         synchronized (lock) {
-            while (currentPlayers >= maxPlayer) {
+            while (currentPlayers >= maxPlayers) {
                 System.out.println("Нет свободных мест\n");
                 try {
                     lock.wait();
                 } catch (InterruptedException e) {
+                    Thread.currentThread().interrupt();
                     throw new RuntimeException(e);
                 }
             }
             currentPlayers++;
-            System.out.printf("Добавлен игрок %s, теперь игроков: %d\n", player.name(), currentPlayers);
+            player.setBattle(true);
+            System.out.printf("Добавлен игрок %s, теперь игроков: %d\n", player.getName(), currentPlayers);
         }
     }
 
     public void leaveBattle(Player player) {
         synchronized (lock) {
-            currentPlayers--;
-            System.out.printf("Есть свободное место, %s покидает битву\n", player.name());
-            lock.notifyAll();
-            System.out.println("Уведомили очередь\n");
+            if (player.isBattle()) {
+                currentPlayers--;
+                System.out.printf("Есть свободное место, %s покидает битву\n", player.getName());
+                lock.notifyAll();
+                System.out.println("Уведомили очередь\n");
+                player.setBattle(false);
+            }
         }
     }
 }
