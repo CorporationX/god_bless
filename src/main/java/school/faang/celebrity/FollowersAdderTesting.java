337a270acdc7baf3693309337e45b83d2b5a9db5
mrnght <misterday116@gmail.com>
2025-06-26 16:37:15 +0800

    added thread pool

--- src/main/java/school/faang/celebrity/FollowersAdderTesting.java
+++ src/main/java/school/faang/celebrity/FollowersAdderTesting.java
@@ -2,6 +2,7 @@ package school.faang.celebrity;
 
 import lombok.extern.slf4j.Slf4j;
 
+import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.atomic.AtomicInteger;
 
 @Slf4j
@@ -13,7 +14,15 @@ public class FollowersAdderTesting {
         log.info("Стартовое число подписчиков аккаунта {} равно {}.",
                 account.getUsername(), account.getFollowers());
 
-        subscriptionSystem.followAccount(account, 100);
+        CompletableFuture<Void>[] futures = new CompletableFuture[100];
+
+        for (int i = 0; i < 100; i++) {
+            futures[i] = subscriptionSystem.followAccount(account);
+        }
+
+        CompletableFuture.allOf(futures).join();
+        subscriptionSystem.closeExecutor();
+
         log.info("Обновленное число подписчиков аккаунта {} равно {}.",
                 account.getUsername(), account.getFollowers());
     }
--- src/main/java/school/faang/celebrity/TwitterSubscriptionSystem.java
+++ src/main/java/school/faang/celebrity/TwitterSubscriptionSystem.java
@@ -1,24 +1,36 @@
 package school.faang.celebrity;
 
-import java.util.concurrent.CompletableFuture;
+import lombok.extern.slf4j.Slf4j;
 
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
+
+@Slf4j
 public class TwitterSubscriptionSystem {
+    private static final int THREAD_COUNT = 3;
+    private ExecutorService executor = Executors.newFixedThreadPool(THREAD_COUNT);
 
     private void addFollower(TwitterAccount account) {
         account.followersIncrement();
     }
 
-    public void followAccount(TwitterAccount account, int newFollowersCount) {
-        CompletableFuture<Void>[] futures = new CompletableFuture[newFollowersCount];
+    public CompletableFuture<Void> followAccount(TwitterAccount account) {
+        return CompletableFuture.runAsync(() -> addFollower(account), executor);
+    }
 
-        for (int i = 0; i < newFollowersCount; i++) {
-            CompletableFuture<Void> future = CompletableFuture.supplyAsync(() -> {
-                addFollower(account);
-                return null;
-            });
-            futures[i] = future;
+    public void closeExecutor() {
+        executor.shutdown();
+        try {
+            if (!executor.awaitTermination(60, TimeUnit.SECONDS)) {
+                log.error("Задачи не были завершены вовремя. Принудительная остановка.");
+                executor.shutdownNow();
+            }
+        } catch (InterruptedException e) {
+            log.error("Поток был прерван.");
+            executor.shutdownNow();
+            Thread.currentThread().interrupt();
         }
-
-        CompletableFuture.allOf(futures).join();
     }
 }
