38bee852911ba63f2a37e680576710b17fd9232c
ddddddasha <daria.zhelobkovich@gmail.com>
2025-06-27 18:46:55 +0300

    Carry отожрался

---
+++ src/main/java/school/faang/module1/bjs2_82746/Item.java
@@ -0,0 +1,13 @@
+package school.faang.module1.bjs2_82746;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import lombok.ToString;
+
+@ToString
+@Getter
+@RequiredArgsConstructor
+public class Item {
+    private final String name;
+    private final int power;
+}
