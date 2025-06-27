3449c34e692c41339306467f4c0ab889d59b07fd
yagor. <warneverchang3s@gmail.com>
2025-06-27 13:46:33 +0300

    Качаем алхимию - правки по pr, добавил кастомное исключение

--- src/main/java/school/faang/alchemy/PotionMaker.java
+++ src/main/java/school/faang/alchemy/PotionMaker.java
@@ -1,24 +1,25 @@
 package school.faang.alchemy;
 
 import lombok.Data;
+import lombok.extern.slf4j.Slf4j;
 
 import java.util.List;
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.atomic.AtomicInteger;
 
+@Slf4j
 @Data
 public class PotionMaker {
-
+    private static final int threadSleepDelay = 2000;
     private static AtomicInteger potionCounter = new AtomicInteger(0);
-
 
     public static CompletableFuture<Integer> gatherIngredient(Potion potion) {
         return CompletableFuture.supplyAsync(() -> {
             try {
-                Thread.sleep(2000);
+                Thread.sleep(threadSleepDelay);
             } catch (InterruptedException e) {
                 Thread.currentThread().interrupt();
-                throw new RuntimeException(e);
+                throw new PotionInterruptedException("Произошла ошибка при получении ингредиента ", e);
             }
             return potion.getIngredient();
         });
@@ -31,7 +32,7 @@ public class PotionMaker {
         completableFutureList.forEach(future -> future.thenApply(potionCounter::addAndGet));
         CompletableFuture.allOf(completableFutureList.toArray(new CompletableFuture[0])).join();
         int total = potionCounter.get();
-        System.out.println(total);
+        log.info("Общее количество приготовленных зелий {}", total);
     }
 
 }
