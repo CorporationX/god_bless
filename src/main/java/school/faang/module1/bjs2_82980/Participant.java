c24fdbcca05b10c3ceb427574acb871c698e93a9
ddddddasha <daria.zhelobkovich@gmail.com>
2025-06-27 21:31:25 +0300

    Мини Zoom

--- src/main/java/school/faang/module1/bjs2_82980/Participant.java
+++ src/main/java/school/faang/module1/bjs2_82980/Participant.java
@@ -5,7 +5,7 @@ import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
 @RequiredArgsConstructor
-public class Participant implements Runnable{
+public class Participant implements Runnable {
 
     private final String name;
     private final Conference conference;
