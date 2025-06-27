23e5f50dc48f4c18c056612d9f53c8a76483fa7d
Viktor Romanenko <romanenkoviktor080@gmail.com>
2025-06-27 18:51:13 +0300

    fixes

--- src/main/java/school/faang/sprint3/bjs2_81830/TelegramBot.java
+++ src/main/java/school/faang/sprint3/bjs2_81830/TelegramBot.java
@@ -1,60 +1,36 @@
 package school.faang.sprint3.bjs2_81830;
 
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
-import java.util.concurrent.TimeUnit;
-
 public class TelegramBot {
-    public static final int THREAD_BATCH = 5;
     public static final int REQUEST_LIMIT = 5;
     public static final int LIMIT_INTERVAL = 1000;
-    public int requestCounter;
-    public long lastRequestTime;
+    private int requestCounter;
+    private long lastRequestTime;
 
-    TelegramBot() {
+    public TelegramBot() {
         this.requestCounter = 0;
         this.lastRequestTime = System.currentTimeMillis();
     }
 
     public synchronized void sendMessage(String message) {
-        long dateTime = System.currentTimeMillis();
-        long diff = dateTime - lastRequestTime;
+        long currentTimeMillis = System.currentTimeMillis();
+        long diff = currentTimeMillis - lastRequestTime;
         try {
             Thread.sleep(100);
             if (diff < LIMIT_INTERVAL) {
                 requestCounter++;
                 if (requestCounter > REQUEST_LIMIT) {
                     long timeoutMillis = LIMIT_INTERVAL - diff;
-                    wait(LIMIT_INTERVAL - diff);
+                    wait(timeoutMillis);
                     System.out.println("Превышение лимита, ожидание: " + timeoutMillis);
-                    requestCounter = 0;
+                    requestCounter = 1;
                 }
             } else {
                 requestCounter = 1;
-                lastRequestTime = dateTime;
+                lastRequestTime = currentTimeMillis;
                 notifyAll();
             }
             System.out.println("Отправлено сообщение: " + message);
         } catch (InterruptedException e) {
-            Thread.currentThread().interrupt();
-            throw new RuntimeException(e);
-        }
-    }
-
-    public static void main(String[] args) {
-        ExecutorService executor = Executors.newFixedThreadPool(THREAD_BATCH);
-        TelegramBot bot = new TelegramBot();
-        for (int i = 0; i < 30; i++) {
-            String message = "message " + i;
-            executor.execute(() -> bot.sendMessage(message));
-        }
-        executor.shutdown();
-        try {
-            if (!executor.awaitTermination(1, TimeUnit.MINUTES)) {
-                executor.shutdownNow();
-            }
-        } catch (InterruptedException e) {
-            executor.shutdownNow();
             Thread.currentThread().interrupt();
             throw new RuntimeException(e);
         }
