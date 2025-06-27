cf879c993438c023a44189d299106abd49d24dcb
Andrey Fomchenko <35478@mail.ru>
2025-06-28 01:27:38 +0500

    BJS2-82667 potion READY

--- src/main/java/school/faang/bjs2_82667/Main.java
+++ src/main/java/school/faang/bjs2_82667/Main.java
@@ -7,28 +7,28 @@ import java.util.concurrent.Executors;
 
 public class Main {
     public static void main(String[] args) {
-    List<Potion> potions = List.of(
-            new Potion("Healing Potion", 5),
-            new Potion("Mana Potion", 3),
-            new Potion("Stamina Potion", 4)
-    );
-    ExecutorService executor = Executors.newFixedThreadPool(5);
-    PotionService potionService = new PotionService(executor);
+        List<Potion> potions = List.of(
+                new Potion("Healing Potion", 5),
+                new Potion("Mana Potion", 3),
+                new Potion("Stamina Potion", 4)
+        );
+        ExecutorService executor = Executors.newFixedThreadPool(5);
+        PotionService potionService = new PotionService(executor);
 
-    List<CompletableFuture<Integer>> ingridietnsFuture = potions.stream()
-            .map(potion -> potionService.collectIngridients(potion))
-            .toList();
+        List<CompletableFuture<Integer>> ingridietnsFuture = potions.stream()
+                .map(potion -> potionService.collectIngridients(potion))
+                .toList();
 
-    CompletableFuture<Void> allDone = CompletableFuture
-            .allOf(ingridietnsFuture.toArray(new CompletableFuture[0])
-    );
-    allDone.join();
+        CompletableFuture<Void> allDone = CompletableFuture
+                .allOf(ingridietnsFuture.toArray(new CompletableFuture[0])
+                );
+        allDone.join();
 
-    int result = ingridietnsFuture.stream()
-            .map(CompletableFuture::join)
-            .mapToInt(Integer::intValue)
-            .sum();
+        int result = ingridietnsFuture.stream()
+                .map(CompletableFuture::join)
+                .mapToInt(Integer::intValue)
+                .sum();
         System.out.println("Общее количество собранных ингредиентов: " + result);
-    executor.shutdown();
-}
+        executor.shutdown();
+    }
 }
