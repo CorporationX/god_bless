b60bfbd6e0f41119bfe046200250e292f8c1b5f6
White <ilyakrivitskiyn@gmail.com>
2025-06-30 23:55:34 +0300

    add changes

--- src/main/java/school/faang/bjs2_82294/QuestSystem.java
+++ src/main/java/school/faang/bjs2_82294/QuestSystem.java
@@ -4,7 +4,12 @@ import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.ExecutorService;
 
 public class QuestSystem {
-    public CompletableFuture<Player> startQuest(Player player, Quest quest, ExecutorService executorService) {
+    private static final int TIME_TO_COMPLETE_EASY_QUEST = 1;
+    private static final int TIME_TO_COMPLETE_MEDIUM_QUEST = 2;
+    private static final int TIME_TO_COMPLETE_HARD_QUEST = 3;
+
+    public synchronized CompletableFuture<Player> startQuest(
+            Player player, Quest quest, ExecutorService executorService) {
         final int timeToCompliedQuest = getTime(quest);
         return CompletableFuture.supplyAsync(() -> {
             try {
@@ -19,9 +24,9 @@ public class QuestSystem {
 
     private int getTime(Quest quest) {
         return switch (quest.difficulty()) {
-            case EASY -> 1;
-            case MEDIUM -> 2;
-            case HARD -> 3;
+            case EASY -> TIME_TO_COMPLETE_EASY_QUEST;
+            case MEDIUM -> TIME_TO_COMPLETE_MEDIUM_QUEST;
+            case HARD -> TIME_TO_COMPLETE_HARD_QUEST;
         };
     }
 }
