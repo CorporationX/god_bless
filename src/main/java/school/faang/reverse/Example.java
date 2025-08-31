1a74d92a95d0b5454887d17df5051a348be6bb93
leto007 <liga07@bk.ru>
2025-08-31 21:11:00 +0300

    code style поменял

--- src/main/java/school/faang/reverse/Example.java
+++ src/main/java/school/faang/reverse/Example.java
@@ -4,23 +4,23 @@ import java.util.Arrays;
 
 public class Example {
 
-    public static int[] reverse(int[] nums){
+    public static int[] reverse(int[] nums) {
 
         int[] reversed = new int[nums.length];
 
-        for (int i = 0; i < nums.length; i++ ){
+        for (int i = 0; i < nums.length; i++) {
 
             reversed[i] = nums[nums.length - 1 - i];
- 
+
         }
         return reversed;
 
-          
     }
+
     public static void main(String[] args) {
-        int[] nums = {1, 2, 3, 4, 5};
+        int[] nums = { 1, 2, 3, 4, 5 };
         int[] reversedNums = reverse(nums);
-    
+
         System.out.println(Arrays.toString(nums));
         System.out.println(Arrays.toString(reversedNums));
     }
--- src/main/java/school/faang/reverse/Example_main.java
+++ src/main/java/school/faang/reverse/Example_main.java
@@ -4,10 +4,10 @@ import java.util.Arrays;
 
 public class Example_main {
     public static void main(String[] args) {
-        
-        int[] nums = {1, 2, 3, 4, 5};
+
+        int[] nums = { 1, 2, 3, 4, 5 };
         int[] reversedNums = Example.reverse(nums);
-    
+
         System.out.println(Arrays.toString(nums));
         System.out.println(Arrays.toString(reversedNums));
     }
