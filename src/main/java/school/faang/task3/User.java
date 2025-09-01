f3d967ac69f04f0c5903b6aed311b63b1368bfce
Lowdude1 <sanitaronlain@gmail.com>
2025-09-01 23:47:07 +0300

    Группировка пользователей по возрасту

---
+++ src/main/java/school/faang/task3/User.java
@@ -0,0 +1,35 @@
+package school.faang.task3;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.List;
+
+@Getter
+@AllArgsConstructor
+public class User {
+    private String name;
+    private int age;
+    private String job;
+    private String address;
+
+    public static Map<Integer, List<User>> groupUsers(List<User> users) {
+        Map<Integer, List<User>> result = new HashMap<>();
+
+        for (User user : users) {
+            int age = user.getAge();
+
+            if (result.containsKey(age)) {
+                result.get(age).add(user);
+            } else {
+                List<User> users1 = new ArrayList<>();
+                users1.add(user);
+                result.put(age, users1);
+            }
+        }
+        return result;
+    }
+}
