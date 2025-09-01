4b6f3e70fa06ec42e831de16ba76b68acb98d5c2
Lowdude1 <sanitaronlain@gmail.com>
2025-09-01 15:19:00 +0300

    task - Коллекционируем пользователей,попробовал поменять code style

--- src/main/java/school/faang/task2/Main.java
+++ src/main/java/school/faang/task2/Main.java
@@ -7,7 +7,7 @@ import static school.faang.task2.User.findHobbyLovers;
 public class Main {
     public static void main(String[] args) {
         User user1 = new User(1, "Егор", 18, new HashSet<>(Arrays.asList("волейбол", "чтение", "гитара")));
-        User user2 = new User(2, "Ярослав", 19, new HashSet<>(Arrays.asList("Баскетбол","Волейбол")));
+        User user2 = new User(2, "Ярослав", 19, new HashSet<>(Arrays.asList("Баскетбол", "Волейбол")));
         User user3 = new User(3, "Антон", 18, new HashSet<>(Arrays.asList("футбол", "программирование")));
         User user4 = new User(4, "Анна", 19, new HashSet<>(Arrays.asList("чтение", "кулинария")));
 
