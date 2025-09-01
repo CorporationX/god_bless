4f705fdc85a7de57acdd611064f4336e9ba26ea7
Viks <viktor.naroznoi@gmail.com>
2025-09-01 15:50:33 +0300

    --Gotovo i vrode rabotaet

---
+++ src/main/java/school/faang/Example.java
@@ -0,0 +1,22 @@
+package school.faang;
+
+public class Example {
+    //Method dlya razvorota massiva
+    public static int[] reverseArray(int[] nums){
+        int[] reversed = new int[nums.length];
+        for(int i=0; i<nums.length; i++) {
+            reversed[i]=nums[nums.length-1-i];
+        }
+        return reversed;
+    }
+
+    public static void main(String[] args) {
+
+        int[] original = {10, 20, 30, 40, 50, 55, 60, 65, 70}; //nachal'nqy massiv
+        int[] reversed = reverseArray(original); //razvernutqy massiv
+
+        for (int num : reversed) {
+            System.out.print(num + " "); //pechataem rezul'tat :)
+        }
+    }
+}
