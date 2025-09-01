e719cb46a93fb3ae13695c1af3bff47a0278d896
EdelweissPC <shherbakov99ilya@mail.ru>
2025-09-01 21:27:30 +0300

    create method reverse masive

---
+++ src/main/java/school/faang/faang/bjs2_85530/Main.java
@@ -0,0 +1,19 @@
+package school.faang.faang.bjs2_85530;
+
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) {
+       int[] massiveIntNumber = {1,2,3,4,5};
+        System.out.println(Arrays.toString(reverseMassive(massiveIntNumber)));
+     }
+    public static int[] reverseMassive(int[] masTemp){
+        int[] reverseMas = new int[masTemp.length];
+        for (int i = masTemp.length-1; i>=0; i--){
+            reverseMas[masTemp.length-i-1] = masTemp[i];
+        }
+
+        return reverseMas;
+    }
+
+}
