26b5343cde4d9679963c702ac1453a2d640905b9
Pavel-Lukyanov <PavelLukyanov2017@yandex.ru>
2025-09-02 00:51:57 +0500

    BJS2-85490

---
+++ src/main/java/school/faang/bjs2_85490/Main.java
@@ -0,0 +1,33 @@
+package school.faang.bjs2_85490;
+
+public class Main {
+    public static void main(String[] args) {
+        User pavel = new User("Pavel", 29, "Google", "New York");
+        User oleg = new User("Oleg", 45, "Amazon", "London");
+        User egor = new User("Egor", 23, "Uber", "Amsterdam");
+
+        try {
+            User user = new User("", 11, "Alt Studio", "Surgut");
+        } catch (IllegalArgumentException e) {
+            System.out.println(e.getMessage());
+        }
+
+        try {
+            User user = new User("Pavel", 11, "Alt Studio", "Surgut");
+        } catch (IllegalArgumentException e) {
+            System.out.println(e.getMessage());
+        }
+
+        try {
+            User user = new User("Pavel", 20, "Alt Studio", "Surgut");
+        } catch (IllegalArgumentException e) {
+            System.out.println(e.getMessage());
+        }
+
+        try {
+            User user = new User("Pavel", 20, "Google", "Surgut");
+        } catch (IllegalArgumentException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+}
---
+++ src/main/java/school/faang/bjs2_85490/User.java
@@ -0,0 +1,42 @@
+package school.faang.bjs2_85490;
+
+import java.util.Arrays;
+
+public class User {
+    private static final String[] VALID_JOBS = { "Google", "Uber", "Amazon" };
+    private static final String[] VALID_ADDRESSES = { "London", "New York", "Amsterdam" };
+    private static final int MIN_AGE = 18;
+
+    private String name;
+    private int age;
+    private String job;
+    private String address;
+
+    public User(String name, int age, String job, String address) {
+
+        validateParams(name, age, job, address);
+
+        this.name = name;
+        this.age = age;
+        this.job = job;
+        this.address = address;
+    }
+
+    private void validateParams(String name, int age, String job, String address) {
+        if (name == null || name.isBlank()) {
+            throw new IllegalArgumentException("Имя не может быть пустым");
+        }
+
+        if (age < MIN_AGE) {
+            throw new IllegalArgumentException("Возраст не может быть меньше " + MIN_AGE);
+        }
+
+        if (job == null || !Arrays.asList(VALID_JOBS).contains(job)) {
+            throw new IllegalArgumentException("Место работы должно содержаться в наборе: " + Arrays.toString(VALID_JOBS));
+        }
+
+        if (address == null || !Arrays.asList(VALID_ADDRESSES).contains(address)) {
+            throw new IllegalArgumentException("Адрес должен содержаться в наборе: " + Arrays.toString(VALID_ADDRESSES));
+        }
+    }
+}
