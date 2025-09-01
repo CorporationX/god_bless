0328f3c48c6b24bfa69ce338bcfa95ba6b9b05cc
Mikhail EASY <mghievrie@bk.ru>
2025-09-01 21:12:00 +0500

    BJS2-85452 finish code in branch BJS2-85452

---
+++ src/main/java/BJS2_85452/Example.java
@@ -0,0 +1,16 @@
+package BJS2_85452;
+
+public class Example {
+    public static void main(String[] args) {
+        int[] array = new int[]{1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+        reverse(array);
+    }
+
+    private static void reverse(int[] array) {
+        for (int i = 0; i < array.length / 2; i++) {
+            int temp = array[i];
+            array[i] = array[array.length - i - 1];
+            array[array.length - i - 1] = temp;
+        }
+    }
+}
