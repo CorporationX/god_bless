d07b2c70c51caf640f5acab0c311f533e4e0be0f
saserka <hamelionik1@mail.ru>
2025-09-02 18:04:49 +0300

    Проверка

--- src/main/java/school/faang/bjs2_85463/User.java
+++ src/main/java/school/faang/bjs2_85463/User.java
@@ -1,27 +1,21 @@
 package school.faang.bjs2_85463;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
 import java.util.List;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
 
+@Getter
+@AllArgsConstructor
 public class User { // список всех пользователей
 
     private final String name;
     private final int age; // ключ для Map
     private final String work;
     private final String address;
-
-    public User(String name, int age, String work, String address) {
-        this.name = name;
-        this.age = age;
-        this.work = work;
-        this.address = address;
-    }
-
-    public int getAge() {
-        return age;
-    }
 
     public static Map<Integer, List<User>> groupUsers(List<User> users) {
         Map<Integer, List<User>> usersByAge = new HashMap<>();
