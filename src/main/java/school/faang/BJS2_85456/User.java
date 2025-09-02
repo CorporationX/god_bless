9966060cd030e3ee556036919abdafbfe84c71d1
derzhko <a.derzhko@yandex.ru>
2025-09-02 13:31:30 +0300

    create access modificators and create getter and toString annotations

--- src/main/java/school/faang/BJS2_85456/User.java
+++ src/main/java/school/faang/BJS2_85456/User.java
@@ -1,59 +1,33 @@
 package school.faang.BJS2_85456;
 
-import java.util.HashMap;
-import java.util.LinkedList;
-import java.util.List;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.ToString;
+
+import java.util.ArrayList;
 import java.util.Map;
+import java.util.HashMap;
+import java.util.List;
 
+@Getter
+@ToString(includeFieldNames = true)
+@AllArgsConstructor
 public class User {
-    String name;
-    int age;
-    String workplace;
-    String address;
-
-    public User(String name, int age, String workplace, String address) {
-        this.name = name;
-        this.age = age;
-        this.workplace = workplace;
-        this.address = address;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public int getAge() {
-        return age;
-    }
-
-    public String getWorkplace() {
-        return workplace;
-    }
-
-    public String getAddress() {
-        return address;
-    }
+    private final String name;
+    private final int age;
+    private final String workplace;
+    private final String address;
 
     static Map<Integer, List<User>> groupUsers(List<User> users) {
         Map<Integer, List<User>> result = new HashMap<>();
         for (User user : users) {
             int userAge = user.getAge();
-            List<User> list = result.getOrDefault(userAge, new LinkedList<>());
+            List<User> list = result.getOrDefault(userAge, new ArrayList<>());
             list.add(user);
             if (!result.containsKey(userAge)) {
                 result.put(userAge, list);
             }
         }
         return result;
-    }
-
-    @Override
-    public String toString() {
-        return "User{" +
-                "name='" + name + '\'' +
-                ", age=" + age +
-                ", workplace='" + workplace + '\'' +
-                ", address='" + address + '\'' +
-                '}';
     }
 }
