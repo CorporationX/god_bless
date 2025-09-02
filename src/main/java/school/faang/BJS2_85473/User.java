b2be186f99fb962dca2cbaf073e3b5154553e23a
derzhko <a.derzhko@yandex.ru>
2025-09-02 13:25:12 +0300

    create access modificators and create nonnull annotations

--- src/main/java/school/faang/BJS2_85473/User.java
+++ src/main/java/school/faang/BJS2_85473/User.java
@@ -1,24 +1,30 @@
 package school.faang.BJS2_85473;
 
+import lombok.NonNull;
+
 import java.util.Set;
 
 public class User {
-    String name;
-    int age;
-    String job;
-    String address;
+    private final String name;
+    private final int age;
+    private final String job;
+    private final String address;
+    private final int minAge = 18;
 
     static final Set<String> VALID_JOBS = Set.of("Google", "Uber", "Amazon");
     static final Set<String> VALID_ADDRESSES = Set.of("London", "New York", "Amsterdam");
 
-    public User(String name, int age, String job, String address) {
-        if (name.isEmpty()) {
+    public User(@NonNull String name, @NonNull int age, @NonNull  String job, @NonNull  String address) {
+        if (name.isBlank()) {
             throw new IllegalArgumentException("Name should not be empty");
-        } else if (age < 18) {
-            throw new IllegalArgumentException("Age should be grater than 18");
-        } else if (!VALID_JOBS.contains(job)) {
+        }
+        if (age < minAge) {
+            throw new IllegalArgumentException("Age should be grater than " + minAge);
+        }
+        if (!VALID_JOBS.contains(job)) {
             throw new IllegalArgumentException("Job is not valid");
-        } else if (!VALID_ADDRESSES.contains(address)) {
+        }
+        if (!VALID_ADDRESSES.contains(address)) {
             throw new IllegalArgumentException("Address is not valid");
         }
         this.name = name;
