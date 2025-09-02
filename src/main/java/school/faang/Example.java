dcffc7dec88eedc5099a67700f2492a460dc4fd8
Viks <viktor.naroznoi@gmail.com>
2025-09-02 21:15:15 +0300

    --popravil uslovie, no ne ponyal, zachem tam return. Kak ya vizhu i bez nego uslovie vqpolnyaetsya i bol'she nichego ne proishodit.

--- src/main/java/school/faang/Example.java
+++ src/main/java/school/faang/Example.java
@@ -3,7 +3,9 @@ package school.faang;
 public class Example {
 
     public static void reverse(int[] nums) {
-        if (nums != null && nums.length > 1) {
+        if (nums == null || nums.length <= 1) {
+            System.out.println("NULL/empty array or containing single element");
+        } else {
             for (int i = 0; i < nums.length / 2; i++) {
                 int saved = nums[i];
                 nums[i] = nums[nums.length - 1 - i];
@@ -11,18 +13,18 @@ public class Example {
             }
             for (int num : nums) {
                 System.out.print(num + " ");
-            } System.out.println("<---reversed array");
-        } else {
-            System.out.println("NULL/empty array or containing single element");
+            }
+            System.out.println("<---reversed array");
         }
     }
 
-    public static void main(String[] args) {
 
+    public static void main(String[] args) {
         int[] original = {10, 20, 30, 40, 50, 55, 60, 65, 70};
-        for (int num : original) {System.out.print(num + " ");}
+        for (int num : original) {
+            System.out.print(num + " ");
+        }
         System.out.println("<---original array");
         reverse(original);
-
     }
 }
