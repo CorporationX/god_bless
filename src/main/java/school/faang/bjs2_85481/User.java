20adc5f6722a36bf9b2e9ad5cbf0b89594e2e55d
AndreySN7 <andrey__n@mail.ru>
2025-09-02 20:38:31 +0300

    Исправил задачу

--- src/main/java/school/faang/bjs2_85481/User.java
+++ src/main/java/school/faang/bjs2_85481/User.java
@@ -1,33 +1,29 @@
 package school.faang.bjs2_85481;
 
-import java.util.HashSet;
 import java.util.Set;
 
 public class User {
-    private String name; // имя пользователя.
-    private int age; // возраст пользователя.
-    private String job; // место работы.
-    private String address; // адрес.
+    private String name;
+    private int age;
+    private String job;
+    private String address;
 
     private static final int VALID_AGE = 18;
-    private static final Set<String> VALID_JOBS =
-            new HashSet<String>(Set.of(new String[]{"Google", "Uber", "Amazon"}));
-    private static final Set<String> VALID_ADDRESSES =
-            new HashSet<String>(Set.of(new String[]{"London", "New York", "Amsterdam"}));
+    private static final Set<String> VALID_JOBS = Set.of(new String[]{"Google", "Uber", "Amazon"});
+    private static final Set<String> VALID_ADDRESSES = Set.of(new String[]{"London", "New York", "Amsterdam"});
 
     public User(String name, int age, String job, String address) {
-        if (name.isEmpty()) {
+        if (name == null || name.isBlank()) {
             throw new IllegalArgumentException("Username cannot be empty");
         }
         if (age < VALID_AGE) {
             throw new IllegalArgumentException("User age cannot be less than " + VALID_AGE);
         }
         if (!VALID_JOBS.contains(job)) {
-            throw new IllegalArgumentException("The job description should include: \"Google\", \"Uber\", \"Amazon\"");
+            throw new IllegalArgumentException("The job description should include: " + VALID_JOBS);
         }
         if (!VALID_ADDRESSES.contains(address)) {
-            throw new IllegalArgumentException("The address description should include: " +
-                    "\"London\", \"New York\", \"Amsterdam\"");
+            throw new IllegalArgumentException("The address description should include: " + VALID_ADDRESSES);
         }
         this.name = name;
         this.age = age;
