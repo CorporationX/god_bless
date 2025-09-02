c420e35c53db0e50465ce385698b664271d79cd3
Andre` <acmisatyuk@gmail.com>
2025-09-03 00:33:24 +0800

    BJS2-85524: reverse array

---
+++ src/main/java/school/faang/Example.java
@@ -0,0 +1,21 @@
+package school.faang;
+
+import java.util.Arrays;
+
+public class Example {
+    static int[] revers(int[] arr) {
+        for (int i = 0; i < arr.length / 2; i++) {
+            int tmp = arr[i];
+            arr[i] = arr[arr.length - i - 1];
+            arr[arr.length - i - 1] = tmp;
+        }
+        return arr;
+    }
+
+    public static void main(String[] args) {
+        int[] example = {1, 2, 3, 4, 5, 5, -1};
+        Example.revers(example);
+        assert Arrays.equals(example, new int[]{-1, 5, 5, 4, 3, 2, 1});
+        System.out.println(Arrays.toString(Example.revers(example)));
+    }
+}
