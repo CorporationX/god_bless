6a70248a0a962e7df2bb7bf3fdd658e4e1013167
saserka <hamelionik1@mail.ru>
2025-09-02 21:29:58 +0300

    BJS2-85463 - реализовал сортировку по возрасту

--- src/main/java/school/faang/bjs2_85463/User.java
+++ src/main/java/school/faang/bjs2_85463/User.java
@@ -2,56 +2,28 @@ package school.faang.bjs2_85463;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.ToString;
 
-import java.util.List;
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
-@Getter
 @AllArgsConstructor
-public class User { // список всех пользователей1
-
-    private final String name;
-    private final int age; // ключ для Map
-    private final String work;
-    private final String address;
+@Getter
+@ToString
+public class User {
+    private String name;
+    private int age;
+    private String workplace;
+    private String address;
 
     public static Map<Integer, List<User>> groupUsers(List<User> users) {
-        Map<Integer, List<User>> usersByAge = new HashMap<>();
-
-        // для каждого пользователя в списке users выполнить следующий код
+        Map<Integer, List<User>> ageToUser = new HashMap<>();
         for (User user : users) {
-            // получить возраст пользователя и продолжить обработку1
-            int age = user.getAge();
-
-            // если нет списка, создаем
-            if (!usersByAge.containsKey(age)) {
-                usersByAge.put(age, new ArrayList<>());
-            }
-            // Добавляем пользователя в список соответствующего возраста11
-            usersByAge.get(age).add(user);
+            ageToUser.putIfAbsent(user.getAge(), new ArrayList<>());
+            ageToUser.get(user.getAge()).add(user);
         }
-
-        return usersByAge;
-    }
-
-    public static void main(String[] args) {
-        List<User> users = new ArrayList<>();
-
-        users.add(new User("Иван ", 20, "яндекс", "1"));
-        users.add(new User("Мария", 30, "сбер", "2"));
-        users.add(new User("Петр", 20, "сбер", "2"));
-        users.add(new User("Анна", 30, "сбер", "2"));
-        users.add(new User("Сергей", 20, "яндекс", "1"));
-
-        System.out.println(User.groupUsers(users));
-    }
-
-    @Override
-    public String toString() {
-        return "User{" +
-                "name= " + name + ' ' + ", age= " + age +
-                ", work= " + work + ' ' + ", address= " + address + ' ' + '}';
+        return ageToUser;
     }
 }
