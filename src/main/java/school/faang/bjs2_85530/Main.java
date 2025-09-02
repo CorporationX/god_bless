27296982d26077aaf7ede54161cefac52dc9c732
EdelweissPC <shherbakov99ilya@mail.ru>
2025-09-02 13:21:56 +0300

    refactor naming reverseNums

--- src/main/java/school/faang/bjs2_85530/Main.java
+++ src/main/java/school/faang/bjs2_85530/Main.java
@@ -9,11 +9,11 @@ public class Main {
     }
 
     public static int[] reversedArray(int[] sourceNums) {
-        int[] reverseMas = new int[sourceNums.length];
+        int[] reverseNums = new int[sourceNums.length];
         for (int i = sourceNums.length - 1; i >= 0; i--) {
-            reverseMas[sourceNums.length - i - 1] = sourceNums[i];
+            reverseNums[sourceNums.length - i - 1] = sourceNums[i];
         }
-        return reverseMas;
+        return reverseNums;
     }
 
 }
