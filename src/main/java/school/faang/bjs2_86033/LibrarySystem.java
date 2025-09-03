08d6c06eabd28f5acdcc0c4f14a182dcadc72623
DeniAkhmadov <daglossteckin@gmail.com>
2025-09-03 14:17:10 +0300

    Рекордный класс реализован корректно, НО, я решил изменить toString, такой вывод мне больше нравится.
    Все мои ошибки, указанные вами, бли исправлены. (Я совсем забыл про форматирование тогда, изменение типа location в методе addBook я убрал и теперь там просто строка принимается, конструктор LibrarySystem теперь приватный - никто не сможет создавать экземпляры, методы вызываются через LibrarySystem, ДОБАВИЛ ПРОВЕРКИ в методы findBook и removeBook - реагируют если искомой/удаляемой книги нет в Библиотеке.

--- src/main/java/school/faang/bjs2_86033/LibrarySystem.java
+++ src/main/java/school/faang/bjs2_86033/LibrarySystem.java
@@ -6,23 +6,35 @@ import java.util.Map;
 public class LibrarySystem {
     private static final HashMap<Book, String> BOOK_LIBRARY = new HashMap<>();
 
-    public static void addBook(String title, String author, int year, int location) {
-        String index = "" + location;
-        Book book = new Book(title, author, year);
-        BOOK_LIBRARY.put(book, index);
+    private LibrarySystem() {
+
+    }
+
+    public static void addBook(String title, String author, int year, String location) {
+        BOOK_LIBRARY.put(new Book(title, author, year), location);
     }
 
     public static void removeBook(String title, String author, int year) {
-        BOOK_LIBRARY.remove(new Book(author, title, year));
+        Book book = new Book(title, author, year);
+        if (BOOK_LIBRARY.containsKey(book)) {
+            BOOK_LIBRARY.remove(book);
+        } else {
+            System.out.println("Не удалось удалить книгу. Неверно введены данные книги или такой книги нет!");
+        }
     }
 
     public static void findBook(String title, String author, int year) {
-        System.out.println(BOOK_LIBRARY.get(new Book(title, author, year)));
+        Book book = new Book(title, author, year);
+        if (BOOK_LIBRARY.containsKey(book)) {
+            System.out.println(BOOK_LIBRARY.get(book));
+        } else {
+            System.out.println("Такой книги нет!");
+        }
     }
 
     public static void printAllBooks() {
         for (Map.Entry<Book, String> keyValue : BOOK_LIBRARY.entrySet()) {
-            System.out.println(keyValue.getKey() + " [" + keyValue.getValue() + "]");
+            System.out.printf("%s [%s]%n", keyValue.getKey(), keyValue.getValue());
         }
     }
 }
--- src/main/java/school/faang/bjs2_86033/Main.java
+++ src/main/java/school/faang/bjs2_86033/Main.java
@@ -2,11 +2,16 @@ package school.faang.bjs2_86033;
 
 public class Main {
     public static void main(String[] args) {
-        LibrarySystem library = new LibrarySystem();
-        library.addBook("Как закалялась сталь", "В.Островский", 1948, 2);
-        library.addBook("Цветы для Элджернона", "Дэниел Киз", 1952, 12);
+        LibrarySystem.addBook("Как закалялась сталь", "В.Островский", 1948, "2");
+        LibrarySystem.addBook("Цветы для Элджернона", "Дэниел Киз", 1952, "12");
+        LibrarySystem.addBook("Питер Пен", "Джеймс Барри", 1901, "17");
 
-        library.findBook("Как закалялась сталь", "В.Островский", 1948);
-        library.printAllBooks();
+        LibrarySystem.findBook("Как закалялась стал", "В.Островский", 1948);
+        LibrarySystem.findBook("Как закалялась сталь", "В.Островский", 1948);
+
+        LibrarySystem.removeBook("Питер Пен", "Джеймс Барри", 1900);
+        LibrarySystem.removeBook("Питер Пен", "Джеймс Барри", 1901);
+
+        LibrarySystem.printAllBooks();
     }
 }
