ba9a5ea8ff9a8001043ebc5d446a7cfacf84b82b
Kateryna Kramarchuk <alexnjfort@gmail.com>
2025-09-03 11:25:24 +0200

    add formatted and fix

--- src/main/java/school/faang/User.java
+++ src/main/java/school/faang/User.java
@@ -11,26 +11,28 @@ public class User {
     private static final Set<String> VALID_JOBS = Set.of("Google", "Uber", "Amazon");
     private static final Set<String> VALID_ADDRESSES = Set.of("London", "New York", "Amsterdam");
 
+    private static final int PERMISSION_AGE = 18;
 
-    @SuppressWarnings("checkstyle:Indentation")
+
     public User(String name, int age, String work, String address) {
 
-         if (name == null || name.isBlank()) {
-             throw new IllegalArgumentException("Имя не может быть пустым.");
-         }
-         if (age < 18) {
-             throw new IllegalArgumentException("Возраст не может быть меньше 18.");
-         }
-         if (!VALID_JOBS.contains(work)) {
-             throw new IllegalArgumentException("Место работы должно содержаться в наборе VALID_JOBS.");
-         }
-         if (!VALID_ADDRESSES.contains(address)) {
-             throw new IllegalArgumentException("Адрес должен содержаться в наборе VALID_ADDRESSES.");
-         }
+        if (name == null || name.isBlank()) {
+            throw new IllegalArgumentException("Имя не может быть пустым.");
+        }
+        if (age < PERMISSION_AGE) {
+            throw new IllegalArgumentException("Возраст не может быть меньше 18.");
+        }
+        if (!VALID_JOBS.contains(work)) {
+            throw new IllegalArgumentException("Место работы должно содержаться в наборе %s.".formatted(VALID_JOBS));
+        }
+        if (!VALID_ADDRESSES.contains(address)) {
+            throw new IllegalArgumentException("Адрес должен содержаться в наборе %s.".formatted(VALID_ADDRESSES));
+        }
 
         this.name = name;
         this.age = age;
         this.work = work;
+        this.address = address;
 
     }
 }
