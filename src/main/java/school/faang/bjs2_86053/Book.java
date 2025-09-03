fb4dfd92bbc63e9dda0253176f456275c16bb7cd
Lowdude1 <sanitaronlain@gmail.com>
2025-09-04 01:49:42 +0300

    Библиотека Вестеросса

--- src/main/java/school/faang/bjs2_86053/Book.java
+++ src/main/java/school/faang/bjs2_86053/Book.java
@@ -16,13 +16,16 @@ public class Book {
 
     @Override
     public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
+        if (this == o)
+        {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
 
         Book book = (Book) o;
-        return year == book.year &&
-                title.equals(book.title) &&
-                author.equals(book.author);
+        return year == book.year && title.equals(book.title) && author.equals(book.author);
     }
 
     @Override
