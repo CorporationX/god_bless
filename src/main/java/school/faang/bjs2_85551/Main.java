beccf41f7cad05fb874a07866d45aee6a6f6442a
Mikhail EASY <mghievrie@bk.ru>
2025-09-04 14:55:06 +0500

    Добавил try-catch в Main

--- src/main/java/school/faang/bjs2_85551/Main.java
+++ src/main/java/school/faang/bjs2_85551/Main.java
@@ -1,9 +1,11 @@
 package school.faang.bjs2_85551;
 
-import java.util.Set;
-
 public class Main {
     public static void main(String[] args) {
-        User testUser = new User("Edvard", 20, "Actor", "Moscow");
+        try {
+            new User("Edvard", 20, "Actor", "Moscow");
+        } catch (IllegalArgumentException e) {
+            System.out.println(e.getMessage());
+        }
     }
 }
--- src/main/java/school/faang/bjs2_85551/User.java
+++ src/main/java/school/faang/bjs2_85551/User.java
@@ -2,26 +2,32 @@ package school.faang.bjs2_85551;
 
 import java.util.Set;
 
+
 public class User {
     private static final Set<String> VALID_JOBS = Set.of("Google", "Uber", "Amazon");
     private static final Set<String> VALID_ADDRESSES = Set.of("London", "New York", "Amsterdam");
+    private static final int VALID_AGE = 18;
     private final String name;
     private final int age;
     private final String job;
     private final String address;
 
     public User(String name, int age, String job, String address) {
-        if (name == null || name.isEmpty()) {
+        if (name == null || name.isBlank()) {
             throw new IllegalArgumentException("Name is null or empty");
         }
-        if (age <= 18) {
-            throw new IllegalArgumentException("Age must not be younger than 18");
+        if (age < VALID_AGE) {
+            throw new IllegalArgumentException("Age must not be younger than " + VALID_AGE);
         }
         if (!VALID_JOBS.contains(job)) {
-            throw new IllegalArgumentException("The place of work must be included in the set");
+            throw new IllegalArgumentException("The correct place of work must be indicated.\n"
+                    + job + " - your place of work\n"
+                    + "List of acceptable jobs:" + VALID_JOBS);
         }
         if (!VALID_ADDRESSES.contains(address)) {
-            throw new IllegalArgumentException("The address must be included in the set");
+            throw new IllegalArgumentException("The correct address must be specified.\n"
+                    + address + " - your address\n"
+                    + "Correct addresses:" + VALID_ADDRESSES);
         }
         this.name = name;
         this.age = age;
