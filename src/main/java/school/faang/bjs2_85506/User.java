6e5584739607de2440f8081f5139d3993956f792
Viks <viktor.naroznoi@gmail.com>
2025-09-04 15:55:54 +0300

    --

--- src/main/java/school/faang/bjs2_85506/User.java
+++ src/main/java/school/faang/bjs2_85506/User.java
@@ -3,8 +3,6 @@ package school.faang.bjs2_85506;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
-import lombok.ToString;
-
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
