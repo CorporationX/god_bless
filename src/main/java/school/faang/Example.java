b7632bda40cce9583a7f294b7d20407c53abb477
Artem <kuldyrkaevam@gmail.com>
2025-09-04 23:51:18 +1000

    Как я понял, проблема в пробелах

--- src/main/java/school/faang/Example.java
+++ src/main/java/school/faang/Example.java
@@ -3,12 +3,12 @@ package school.faang;
 import java.util.Arrays;
 
 public class Example {
-    public static void main (String[] args) {
+    public static void main(String[] args) {
         int[] myArray = {1, 2, 3, 4, 5};
         System.out.println(Arrays.toString(reverse(myArray)));
     }
 
-    public static int[] reverse (int[] array) {
+    public static int[] reverse(int[] array) {
         int[] revArray = new int[array.length];
         int x = 0;
         for (int i = array.length - 1; i >= 0; i--) {
