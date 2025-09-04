57ffe67861f0ede1d42ac9e527d40fe61600caae
Yasin <yasin.akhmadov555@inbox.ru>
2025-09-04 15:57:09 +0300

    User registration

--- src/main/java/school/faang/Main.java
+++ src/main/java/school/faang/Main.java
@@ -1,4 +1,10 @@
 package school.faang;
 
 public class Main {
+    public static void main(String[] args) {
+//        User user = new User("", 34, "Google", "New York");
+//        User user = new User("John", 15, "Google", "New York");
+//        User user = new User("John", 34, "Twitter", "New York");
+//        User user = new User("John", 34, "Google", "Philadelphia");
+    }
 }
---
+++ src/main/java/school/faang/bjs2_85521/exception/JobNotAllowedException.java
@@ -0,0 +1,7 @@
+package school.faang.bjs2_85521.exception;
+
+public class JobNotAllowedException extends RuntimeException {
+    public JobNotAllowedException(String message) {
+        super(message);
+    }
+}
