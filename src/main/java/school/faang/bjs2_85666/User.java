87bd97c4ff48e7e2c7a06b29c88cec81bdc7f67c
Alexey Ulov <ulov.alexey@gmail.com>
2025-09-04 13:01:19 +0300

    BJS2-85666 Группировка пользователей по возрасту

---
+++ src/main/java/school/faang/bjs2_85666/User.java
@@ -0,0 +1,35 @@
+package school.faang.bjs2_85666;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@Getter
+@Setter
+@ToString
+@AllArgsConstructor
+public class User {
+    private String name;
+    private int age;
+    private String workplace;
+    private String address;
+
+    public static Map<Integer, List<User>> groupUsers(List<User> users) {
+        Map<Integer, List<User>> result = new HashMap<>();
+        for (User user : users) {
+            if (!result.containsKey(user.getAge())) {
+                result.put(user.getAge(), new ArrayList<>());
+                result.get(user.getAge()).add(user);
+            } else {
+                result.get(user.getAge()).add(user);
+            }
+        }
+        return result;
+    }
+}
