3f19b65f31397178a2c96d4f0d6a44a9429ff763
Kuanysh <skn_88@mail.ru>
2025-09-04 23:36:13 +0500

    BJS2-86668 fix problems with coding style (меня заставил checkstyle check константы написать в нижнем регистре)

--- src/main/java/school/faang/bsj2_86668/Character.java
+++ src/main/java/school/faang/bsj2_86668/Character.java
@@ -4,20 +4,20 @@ import lombok.Data;
 
 @Data
 public abstract class Character {
-    private static final int DEFAULT_HEALTH_VALUE = 100;
-    private static final int DEFAULT_COMMON_VALUE = 5;
+    private static final int Default_Health_Value = 100;
+    private static final int Default_Common_Value = 5;
 
     String name;
     int strength;
     int agility;
     int intelligence;
-    int health = DEFAULT_HEALTH_VALUE;
+    int health = Default_Health_Value;
 
     public Character(String name) {
         this.name = name;
-        this.strength = DEFAULT_COMMON_VALUE;
-        this.agility = DEFAULT_COMMON_VALUE;
-        this.intelligence = DEFAULT_COMMON_VALUE;
+        this.strength = Default_Common_Value;
+        this.agility = Default_Common_Value;
+        this.intelligence = Default_Common_Value;
     }
 
     public Character(String name, int strength, int agility, int intelligence) {
