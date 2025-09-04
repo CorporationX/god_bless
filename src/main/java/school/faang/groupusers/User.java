75ca4525f0ee85aa3c356c7a699fea19cbd2e6e7
leto007 <liga07@bk.ru>
2025-09-04 19:09:45 +0300

    задача Группировка пользователей по возрасту

---
+++ src/main/java/school/faang/groupusers/User.java
@@ -0,0 +1,53 @@
+package school.faang.groupusers;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class User {
+    private String name;
+    private int age;
+    private String workplace;
+    private String address;
+
+
+    public User(String name, int age, String workplace, String address) {
+        this.name = name;
+        this.age = age;
+        this.workplace = workplace;
+        this.address = address;
+    }
+
+
+    public String getName() {
+        return name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public String getWorkplace() {
+        return workplace;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+
+    public static Map<Integer, List<User>> groupUsers(List<User> users) {
+        Map<Integer, List<User>> groupedUsers = new HashMap<>();
+
+        for (User user : users) {
+            int age = user.getAge();
+
+            groupedUsers.putIfAbsent(age, new ArrayList<>());
+
+            groupedUsers.get(age).add(user);
+        }
+
+        return groupedUsers;
+    }
+}
---
+++ src/main/java/school/faang/groupusers/Usermain.java
@@ -0,0 +1,30 @@
+package school.faang.groupusers;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+public class Usermain {
+    @SuppressWarnings("checkstyle:NoWhitespaceBefore")
+    public static void main(String[] args) {
+        List<User> userlist = new ArrayList<>();
+        userlist.add(new User("name1", 20, "rabota1", "adres1"));
+        userlist.add(new User("name2", 20, "rabota2", "adres2"));
+        userlist.add(new User("name3", 30, "rabota3", "adres3"));
+        userlist.add(new User("name4", 20, "rabota4", "adres4"));
+        userlist.add(new User("name5", 30, "rabota5", "adres5"));
+
+        Map<Integer, List<User>> groupedUsers = User.groupUsers(userlist);
+
+        for (Map.Entry<Integer, List<User>> entry : groupedUsers.entrySet()) {
+
+            System.out.println("Возраст: " + entry.getKey());
+
+            for (User user : entry.getValue()) {
+                System.out.println(" - Имя: " + user.getName() + ", Место работы: " +
+                        user.getWorkplace() + ", Адрес: " + user.getAddress());
+            }
+
+        }
+    }
+}
