7ce3f52cfa1b978fc9e6ba2bc04bd9c71c4f243b
saserka <hamelionik1@mail.ru>
2025-09-04 21:43:44 +0300

    BJS2-85544 - реализовал множество Set проверку условий и обработки исключений

--- src/main/java/school/faang/bjs2_85544/User.java
+++ src/main/java/school/faang/bjs2_85544/User.java
@@ -3,31 +3,37 @@ package school.faang.bjs2_85544;
 import java.util.Set;
 
 public class User {
-    public static final Set<String> VALID_JOBS = Set.of("Google", "Uber", "Amazon");
-    public static final Set<String> VALID_ADDRESS = Set.of("London", "New York", "Amsterdam");
+    private static final Set<String> VALID_JOBS = Set.of("Google", "Uber", "Amazon");
+    private static final Set<String> VALID_ADDRESS = Set.of("London", "New York", "Amsterdam");
+    private static final int VALID_AGE = 18;
 
-    private final String name;
-    private final int age;
-    private final String job;
-    private final String address;
+    private String name;
+    private int age;
+    private String job;
+    private String address;
 
-    public User(String name, int age, String job, String address) {
+    private static void validateUser(String name, int age, String job, String address) {
         if (name == null || name.isBlank()) {
             throw new IllegalArgumentException("имя пользователя не может быть null или пустым");
         }
-        if (age <= 18) {
-            throw new IllegalArgumentException("возраст пользователя меньше 18");
+        if (age < VALID_AGE) {
+            throw new IllegalArgumentException("возраст пользователя меньше " + VALID_AGE);
         }
         if (VALID_JOBS.contains(job)) {
-            throw new IllegalArgumentException("вы здесь не работаете");
+            throw new IllegalArgumentException("вы здесь не работаете, вы можете работать только в " + VALID_JOBS);
         }
         if (VALID_ADDRESS.contains(address)) {
-            throw new IllegalArgumentException("вы здесь не живете");
+            throw new IllegalArgumentException("вы здесь не живете, вы можете жить только в " + VALID_ADDRESS);
         }
-
-        this.address = address;
-        this.job = job;
-        this.age = age;
-        this.name = name;
     }
+
+    public User(String name, int age, String job, String address) {
+        validateUser(name, age, job, address);
+
+        this.name = name;
+        this.age = age;
+        this.job = job;
+        this.address = address;
+    }
+
 }
