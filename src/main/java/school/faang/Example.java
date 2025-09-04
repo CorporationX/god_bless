c0b2294445f24cd9a81a99e9595994b7f59c0a9d
Artem <kuldyrkaevam@gmail.com>
2025-09-04 23:29:30 +1000

    Развернитесь!

---
+++ src/main/java/school/faang/Example.java
@@ -0,0 +1,20 @@
+package school.faang;
+
+import java.util.Arrays;
+
+public class Example {
+    public static void main (String[] args) {
+        int[] myArray = {1, 2, 3, 4, 5};
+        System.out.println(Arrays.toString(reverse(myArray)));
+    }
+
+    public static int[] reverse (int[] array) {
+        int[] revArray = new int[array.length];
+        int x = 0;
+        for (int i = array.length - 1; i >= 0; i--) {
+            revArray[x] = array[i];
+            x++;
+        }
+        return revArray;
+    }
+}
