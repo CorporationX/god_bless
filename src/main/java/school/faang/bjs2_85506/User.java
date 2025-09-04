4c129ec8a1d57a6ac7549ef9eb342fbabf3dfbc2
Viks <viktor.naroznoi@gmail.com>
2025-09-04 15:51:41 +0300

    --Seems to work, please review ;)

---
+++ src/main/java/school/faang/bjs2_85506/User.java
@@ -0,0 +1,42 @@
+package school.faang.bjs2_85506;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@Getter
+@Setter
+@AllArgsConstructor
+public class User {
+
+    private String name;
+    private int age;
+    private String workplace;
+    private String address;
+
+    static Map<Integer, List<User>> groupUsers(List<User> users) {
+
+        Map<Integer, List<User>> map = new HashMap<>();
+
+        for (User user : users) {
+
+            int age = user.getAge();
+            if (map.containsKey(age)) {
+                map.get(age).add(user);
+
+            } else {
+
+                List<User> usersOfAge = new ArrayList<>();
+                usersOfAge.add(user);
+                map.put(age, usersOfAge);
+            }
+        }
+        return map;
+    }
+}
