3c35d1d52801016f868bb4d94b17982d951533b3
Yasin <yasin.akhmadov555@inbox.ru>
2025-09-04 22:18:23 +0300

    Grouping users by age

---
+++ src/main/java/school/faang/bjs2_85521/user/Main.java
@@ -0,0 +1,10 @@
+package school.faang.bjs2_85521.user;
+
+public class Main {
+    public static void main(String[] args) {
+        User name = new User("", 34, "Google", "New York");
+        // User age = new User("John", 15, "Google", "New York");
+        // User job = new User("John", 34, "Twitter", "New York");
+        // User address = new User("John", 34, "Google", "Philadelphia");
+    }
+}
