b8fb1333e83559a10e4b63e3c9dfe946c0e1a6b5
Lowdude1 <sanitaronlain@gmail.com>
2025-09-06 00:34:25 +0300

    Ловим события

---
+++ src/main/java/school/faang/bjs2_85821/SpellEvent.java
@@ -0,0 +1,12 @@
+package school.faang.bjs2_85821;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@AllArgsConstructor
+@Getter
+public class SpellEvent {
+    private int id;
+    private String eventType;
+    private String action;
+}
