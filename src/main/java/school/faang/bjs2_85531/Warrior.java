e1a4a949cf9c794f77a9648008cd26d3661135ad
saserka <hamelionik1@mail.ru>
2025-09-05 14:21:58 +0300

    BJS2-85531 - реализовал битву, где 2 война дерутся и их файт выводится на экран - реализовал: Имя передается извне - реализовал цикл, что у противника не может быть меньше 0

--- src/main/java/school/faang/bjs2_85531/Warrior.java
+++ src/main/java/school/faang/bjs2_85531/Warrior.java
@@ -1,29 +1,22 @@
 package school.faang.bjs2_85531;
 
 public class Warrior extends Character {
+    private static final int strength = 9;
+    private static final int agility = 5;
+    private static final int intelligence = 3;
 
-    public Warrior() {
-        super("Conan", 10, 5, 3);
+    public Warrior(String name) {
+        super(name, strength, agility, intelligence);
     }
+
 
     @Override
     public void attack(Character opponent) {
-        int damage = this.getHealth() - getStrength();
-        System.out.println(this.getName() +
-                " бьет палкой по "
-                + opponent.getName() +
-                " и отнимает "
-                + getStrength() +
-                " ХП, остается "
-                + damage);
-    }
-
-
-
-
-
-    @Override
-    public void defeat() {
-        System.out.println("Повержен");
+        health = health - opponent.getAgility();
+        if (isKilling()) {
+            System.out.println(getName() + " повержен");
+            return;
+        }
+        System.out.println(getName() + " " + health + " ХП");
     }
 }
