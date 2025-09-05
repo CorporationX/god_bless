952cb1e6217b34fcd4203081c2b9a9d623d75b42
Elya Khamzina <ekkhamzi@mail.ru>
2025-09-05 20:54:21 +0300

    Коллекционируем пользователей

--- src/main/java/school/faang/reverse/Example.java
+++ src/main/java/school/faang/reverse/Example.java
@@ -2,6 +2,15 @@ package school.faang.reverse;
 
 public class Example {
     public static void reverse(int[] array) {
+        // Проверка на null
+        if (array == null) {
+            throw new IllegalArgumentException("Массив не может быть null");
+        }
+        // Проверка на пустой массив или массив из одного элемента
+        if (array.length <= 1) {
+            return; // Нечего reverse
+        }
+
         for (int i = 0; i < array.length / 2; i++) {
             int temp = array[i];
             array[i] = array[array.length - 1 - i];
