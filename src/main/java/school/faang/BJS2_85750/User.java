a2a83bc67df443f223b4ebbe3031fcbc8826d64f
Kuanysh <skn_88@mail.ru>
2025-09-05 12:17:28 +0500

    BJS2-86668 fix comments after code review

--- src/main/java/school/faang/BJS2_85750/User.java
+++ src/main/java/school/faang/BJS2_85750/User.java
@@ -4,8 +4,9 @@ import java.util.HashSet;
 import java.util.Set;
 
 public class User {
-    private static final Set<String> VALID_JOBS = getJobsSet();
-    private static final Set<String> VALID_ADDRESSES = getAddressesSet();
+    private static final int RESTRICTION_AGE = 18;
+    private static final Set<String> VALID_JOBS = Set.of("Google", "Uber", "Amazon", "Tesla");
+    private static final Set<String> VALID_ADDRESSES = Set.of("New York", "Amsterdam", "LA", "Chicago");
 
     private String name;
     private int age;
@@ -13,11 +14,11 @@ public class User {
     private String address;
 
     public User(String name, int age, String job, String address) {
-        if (name == null) {
+        if (name == null || name.isBlank()) {
             throw new IllegalArgumentException("Имя не может быть пустым");
         }
-        if (age < 18) {
-            throw new IllegalArgumentException("Возраст не может быть меньше 18");
+        if (age < RESTRICTION_AGE) {
+            throw new IllegalArgumentException("Возраст не может быть меньше " + RESTRICTION_AGE);
         }
         if (!VALID_JOBS.contains(job)) {
             throw new IllegalArgumentException("Место работы должно содержаться в наборе VALID_JOBS");
@@ -29,23 +30,5 @@ public class User {
         this.age = age;
         this.job = job;
         this.address = address;
-    }
-
-    private static Set<String> getJobsSet() {
-        Set<String> jobs = new HashSet<>();
-        jobs.add("Google");
-        jobs.add("Uber");
-        jobs.add("Amazon");
-        jobs.add("Tesla");
-        return jobs;
-    }
-
-    private static Set<String> getAddressesSet() {
-        Set<String> addresses = new HashSet<>();
-        addresses.add("New York");
-        addresses.add("Amsterdam");
-        addresses.add("LA");
-        addresses.add("Chicago");
-        return addresses;
     }
 }
