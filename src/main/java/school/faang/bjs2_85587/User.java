21275d05a89ca279afd416fabb3dc1f9d66b557a
Oleg_Korovin <olegk.99@mail.ru>
2025-09-05 21:05:36 +0300

    BJS2-85587

---
+++ src/main/java/school/faang/bjs2_85587/User.java
@@ -0,0 +1,43 @@
+package school.faang.bjs2_85587;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class User {
+    private String name;
+    private int age;
+    private String workplace;
+    private String address;
+
+    public User(String name, int age, String workplace, String address) {
+        this.name = name;
+        this.age = age;
+        this.workplace = workplace;
+        this.address = address;
+    }
+
+    public static Map<Integer, List<User>> groupUsers(List<User> users) {
+        Map<Integer, List<User>> groupedUsers = new HashMap<>();
+        for (User user : users) {
+            List<User> userWithAge = groupedUsers.get(user.age);
+            if (userWithAge == null) {
+                userWithAge = new ArrayList<>();
+            }
+            userWithAge.add(user);
+            groupedUsers.put(user.age, userWithAge);
+        }
+        return groupedUsers;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "name='" + name + '\'' +
+                ", age=" + age +
+                ", workplace='" + workplace + '\'' +
+                ", address='" + address + '\'' +
+                '}';
+    }
+}
