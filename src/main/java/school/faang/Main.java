60be94a16766c741de367045e7b66811d9cffa1c
Ruslan <ruslan8020110@gmail.com>
2025-09-07 02:02:48 +0300

    1

--- src/main/java/school/faang/Main.java
+++ src/main/java/school/faang/Main.java
@@ -1,4 +1,9 @@
 package school.faang;
 
 public class Main {
+    public static void main(String[] args) {
+        User user = new User("Rusya", 21, "Google", "London");
+        User user1 = new User("Vasya", 14, "Janitor", "London");
+
+    }
 }
---
+++ src/main/java/school/faang/User.java
@@ -0,0 +1,41 @@
+package school.faang;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.Set;
+
+@Getter
+@Setter
+@NoArgsConstructor
+public class User {
+    private String name;
+    private int age;
+    private String job;
+    private String address;
+    public static final Set<String> VALID_JOBS = Set.of("Google", "Uber", "Amazon");
+    public static final Set<String> VALID_ADDRESSES = Set.of("London", "New York");
+
+    public User(String name, int age, String job, String address) {
+        if(name == null) {
+            throw new IllegalArgumentException("Имя не может быть пустым");
+        }
+        if(age < 18) {
+            throw new IllegalArgumentException("Возраст не может быть меньше 18");
+        }
+
+        if(!VALID_JOBS.contains(job)) {
+            throw new IllegalArgumentException("Место работы должно содержаться в VALID_JOBS");
+        }
+
+        if(!VALID_ADDRESSES.contains(address)) {
+            throw new IllegalArgumentException("Адрес должен содержаться в VALID_ADDRESSES");
+        }
+
+        this.name = name;
+        this.age = age;
+        this.job = job;
+        this.address = address;
+    }
+}
