add0ebf4c44acb8fe1b15e2e72713a4f685ab1d2
Kuanysh <skn_88@mail.ru>
2025-09-06 22:01:33 +0500

    BJS2-85855 solution of the task

---
+++ src/main/java/school/faang/BSJ2_85855/SpellEvent.java
@@ -0,0 +1,40 @@
+package school.faang.BSJ2_85855;
+
+import java.util.Objects;
+
+public class SpellEvent {
+    private final int id;
+    private final String eventType;
+    private final String action;
+
+    public SpellEvent(int id, String eventType, String action) {
+        this.id = id;
+        this.eventType = eventType;
+        this.action = action;
+    }
+
+    public String getEventType() {
+        return eventType;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        SpellEvent that = (SpellEvent) o;
+        return id == that.id && Objects.equals(eventType, that.eventType) && Objects.equals(action, that.action);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, eventType, action);
+    }
+
+    @Override
+    public String toString() {
+        return "SpellEvent{" +
+                "id=" + id +
+                ", eventType='" + eventType + '\'' +
+                ", action='" + action + '\'' +
+                '}';
+    }
+}
--- src/main/java/school/faang/Main.java
+++ src/main/java/school/faang/Main.java
@@ -1,50 +1,33 @@
 package school.faang;
 
-import school.faang.BJS2_85750.User;
+import school.faang.BSJ2_85855.HogwartsSpells;
+import java.util.Objects;
 
 public class Main {
     public static void main(String[] args) {
-        try {
-            User user1 = new User("Name1", 25, "Apple", "San Francisko");
-            System.out.println(user1);
-        } catch (Exception e) {
-            System.out.println("create user1 error: " + e);
-        }
-        try {
-            User user2 = new User("Name2", 26, "Tesla", "New York");
-            System.out.println(user2);
-        } catch (Exception e) {
-            System.out.println("create user2 error: " + e);
-        }
-        try {
-            User user3 = new User("Name3", 27, "Google", "London");
-            System.out.println(user3);
-        } catch (Exception e) {
-            System.out.println("create user3 error: " + e);
-        }
-        try {
-            User user4 = new User("Name4", 28, "Amazon", "Chicago");
-            System.out.println(user4);
-        } catch (Exception e) {
-            System.out.println("create user4 error: " + e);
-        }
-        try {
-            User user5 = new User("Name5", 29, "Uber", "Paris");
-            System.out.println(user5);
-        } catch (Exception e) {
-            System.out.println("create user4 error: " + e);
-        }
-        try {
-            User user6 = new User("Name6", 17, "Uber", "Paris");
-            System.out.println(user6);
-        } catch (Exception e) {
-            System.out.println("create user6 error: " + e);
-        }
-        try {
-            User user7 = new User(null, 29, "Uber", "Paris");
-            System.out.println(user7);
-        } catch (Exception e) {
-            System.out.println("create user7 error: " + e);
-        }
+        HogwartsSpells hogwartsSpells = new HogwartsSpells();
+
+        hogwartsSpells.addSpellEvent("eventType1", "actionDescription1");
+        hogwartsSpells.addSpellEvent("eventType2", "actionDescription2");
+        hogwartsSpells.addSpellEvent("eventType3", "actionDescription3");
+        hogwartsSpells.addSpellEvent("eventType4", "actionDescription4");
+        hogwartsSpells.addSpellEvent("eventType5", "actionDescription5");
+
+        System.out.println(hogwartsSpells.getSpellEventById(Objects.hash("eventType3", "actionDescription3")));
+        System.out.println(hogwartsSpells.getSpellEventById(Objects.hash("eventType6", "actionDescription6")));
+
+        System.out.println("");
+
+        System.out.println(hogwartsSpells.getSpellEventsByType("eventType5"));
+        System.out.println(hogwartsSpells.getSpellEventsByType("eventType0"));
+
+        System.out.println("");
+
+        System.out.println(hogwartsSpells.deleteSpellEvent(Objects.hash("eventType10", "actionDescription10")));
+        System.out.println(hogwartsSpells.deleteSpellEvent(Objects.hash("eventType1", "actionDescription1")));
+
+        System.out.println("");
+
+        hogwartsSpells.printAllSpellEvents();
     }
 }
