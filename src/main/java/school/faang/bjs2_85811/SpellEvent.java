db562a762255d730bf9033192dd3a64bbcec1e3e
Bekhter Roman <curses93@mail.ru>
2025-09-06 08:36:20 +0300

    bjs-85811-catching-events done

---
+++ src/main/java/school/faang/bjs2_85811/SpellEvent.java
@@ -0,0 +1,45 @@
+package school.faang.bjs2_85811;
+
+import lombok.Getter;
+
+import java.util.Objects;
+
+public class SpellEvent {
+    private static int idCounter = 1;
+    private final Integer id;
+    @Getter
+    private final String eventType;
+    @Getter
+    private final String action;
+
+    public SpellEvent(String eventType, String action) {
+        this.id = idCounter++;
+        this.eventType = eventType;
+        this.action = action;
+    }
+
+    Integer uniqueId() {
+        return id;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        SpellEvent that = (SpellEvent) o;
+        return Objects.equals(id, that.id) && Objects.equals(eventType, that.eventType) && Objects.equals(action, that.action);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, eventType, action);
+    }
+
+    @Override
+    public String toString() {
+        return "SpellEvent{" +
+                "id=" + id +
+                ", eventType='" + eventType + '\'' +
+                ", action='" + action + '\'' +
+                '}';
+    }
+}
