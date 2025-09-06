814c3cf585ec87c1959daeecd875bc5f51d5253c
Yasin <yasin.akhmadov555@inbox.ru>
2025-09-06 17:58:16 +0300

    The Library of Westeros (new) 2.0

--- src/main/java/school/faang/bjs2_86104/exception/BookNotFoundException.java
+++ src/main/java/school/faang/bjs2_86104/exception/BookNotFoundException.java
@@ -4,6 +4,7 @@ public class BookNotFoundException extends RuntimeException {
     public BookNotFoundException(String message) {
         super(message);
     }
+
     public BookNotFoundException(String message, RuntimeException e) {
         super(message, e);
     }
