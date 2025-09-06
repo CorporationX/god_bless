7541932fe84c2ef26ecbe3e3ec592fd7cecb953d
ayazs <ayaz-22@yandex.ru>
2025-09-07 01:28:38 +0500

    Subtask BJS2-85674 done

---
+++ src/main/java/school/faang/Bjs2_85674/Main.java
@@ -0,0 +1,27 @@
+package school.faang.Bjs2_85674;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) {
+        User user1 = new User("Ivan", 20, "Store", "Lenina 22");
+        User user2 = new User("Gleb", 21, "Store", "Lenina 21");
+        User user3 = new User("Petr", 20, "Store", "Lenina 20");
+        User user4 = new User("Inna", 22, "Store", "Lenina 19");
+        User user5 = new User("Olga", 21, "Store", "Lenina 18");
+        User user6 = new User("Anna", 30, "Store", "Lenina 17");
+        List<User> users = new ArrayList<>() {
+            {
+                add(user1);
+                add(user2);
+                add(user3);
+                add(user4);
+                add(user5);
+                add(user6);
+            }
+        };
+
+        System.out.println(User.groupUsers(users));
+    }
+}
---
+++ src/main/java/school/faang/Bjs2_85674/User.java
@@ -0,0 +1,46 @@
+package school.faang.Bjs2_85674;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@Getter
+@AllArgsConstructor
+public class User {
+    private String name;
+    private int age;
+    private String workplace;
+    private String address;
+
+    public static Map<Integer, List<User>> groupUsers(List<User> users) {
+        Map<Integer, List<User>> groupedUsers = new HashMap<>();
+        for (User user : users) {
+            if (groupedUsers.containsKey(user.age)) {
+                groupedUsers.get(user.age).add(user);
+                continue;
+            }
+            groupedUsers.put(user.age, new ArrayList<>() {
+                {
+                    add(user);
+                }
+            });
+        }
+
+        return groupedUsers;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "name='" + name + '\'' +
+                ", age=" + age +
+                ", workplace='" + workplace + '\'' +
+                ", address='" + address + '\'' +
+                '}';
+    }
+}
