211ce98696d4dcf25546e61542177bf04f5bf2b2
AndreySN7 <andrey__n@mail.ru>
2025-09-06 18:05:09 +0300

    перенес пакеты

---
+++ src/main/java/school/faang/java_collection/bjs2_85891/Category.java
@@ -0,0 +1,5 @@
+package school.faang.java_collection.bjs2_85891;
+
+public enum Category {
+    FOOD, ELECTRONICS, CLOTHING, OTHER
+}
---
+++ src/main/java/school/faang/java_collection/bjs2_85891/Product.java
@@ -0,0 +1,14 @@
+package school.faang.java_collection.bjs2_85891;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.ToString;
+
+@AllArgsConstructor
+@Getter
+@ToString
+public class Product {
+    private int id; // (уникальный идентификатор);
+    private String name;
+    private Category category;
+}
