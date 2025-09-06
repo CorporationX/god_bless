fd2c0c9bbbacf5944b61434945397c3ed6d0f097
Rukk Taras <tarrukk@gmail.com>
2025-09-06 19:18:24 +0300

    Variant 2

--- src/main/java/school/faang/BJS2_85629/Main.java
+++ src/main/java/school/faang/BJS2_85629/Main.java
@@ -1,7 +1,26 @@
 package school.faang.BJS2_85629;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
 public class Main {
     public static void main(String[] args) {
-
+        //Test:
+        User user1 = new User("Петя", 22, "Google", "Деревня Чуваки");
+        User user2 = new User("Маша", 35, "Google", "Деревня Чуваки");
+        User user3 = new User("Дима", 22, "Google", "Деревня Чуваки");
+        User user4 = new User("Люся", 18, "Google", "Деревня Чуваки");
+        User user5 = new User("Глаша", 18, "Google", "Деревня Чуваки");
+        User user6 = new User("Цицилия", 22, "Google", "Деревня Чуваки");
+        List<User> users = new ArrayList<>();
+        users.add(user1);
+        users.add(user2);
+        users.add(user3);
+        users.add(user4);
+        users.add(user5);
+        users.add(user6);
+        Map<Integer, List<User>> result = User.groupUsers(users);
+        result.forEach((key, value) -> System.out.println(key + " " + value));
     }
 }
--- src/main/java/school/faang/BJS2_85629/User.java
+++ src/main/java/school/faang/BJS2_85629/User.java
@@ -1,28 +1,27 @@
 package school.faang.BJS2_85629;
 
+import lombok.AllArgsConstructor;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+@AllArgsConstructor
 public class User {
-    String name;
-    int age;
-    String job;
-    String address;
+    private String name;
+    private int age;
+    private String job;
+    private String address;
 
     public static Map<Integer, List<User>> groupUsers(List<User> users) {
         Map<Integer, List<User>> ageBrackets = new HashMap<>();
-        if (users != null) {
-            for (User user : users) {
-                if (ageBrackets.containsKey(user.age)) {
-                    ageBrackets.get(user.age).add(user);
-                } else {
-                    List<User> peers = new ArrayList<>();
-                    peers.add(user);
-                    ageBrackets.put(user.age, peers);
-                }
-            }
+        if (users == null || users.isEmpty()) {
+            return ageBrackets;
+        }
+        for (User user : users) {
+            List<User> peers = new ArrayList<>();
+            ageBrackets.putIfAbsent(user.age, peers);
+            ageBrackets.get(user.age).add(user);
         }
         return ageBrackets;
     }
