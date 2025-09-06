00660631fb17d01b41a93048aaaca5c4d57ff4a2
AndreySN7 <andrey__n@mail.ru>
2025-09-06 13:53:48 +0300

    Решил задачу: Двойной кэш-кэш

---
+++ src/main/java/school/faang/java_collection/bjs2_85960/Student.java
@@ -0,0 +1,12 @@
+package school.faang.java_collection.bjs2_85960;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.ToString;
+
+@AllArgsConstructor
+@Getter
+@ToString
+public class Student {
+    private String name; // имя студента
+}
---
+++ src/main/java/school/faang/java_collection/bjs2_85960/Subject.java
@@ -0,0 +1,12 @@
+package school.faang.java_collection.bjs2_85960;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.ToString;
+
+@AllArgsConstructor
+@Getter
+@ToString
+public class Subject {
+    private String name; // название предмета
+}
