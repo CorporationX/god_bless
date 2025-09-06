d0ec57e8bc4a379cac2c046e771a6bf413511ebb
EdelweissPC <shherbakov99ilya@mail.ru>
2025-09-06 13:36:50 +0300

    BJS-85792 create logic LibrarySystem

---
+++ src/main/java/school/faang/bjs2_85792/Book.java
@@ -0,0 +1,24 @@
+package school.faang.bjs2_85792;
+
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
+
+@Setter
+@Getter
+@EqualsAndHashCode
+@AllArgsConstructor
+public class Book {
+       private String title;
+       private String author;
+       private int year;
+
+    @Override
+    public String toString() {
+        return " Book-" +
+                "title:" + title +
+                "author:'" + author +
+                "year:" + year + " ";
+    }
+}
