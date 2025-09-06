056332e52ad61c5588fb52a7c0b297cebfb88de0
Gleb Pavlovskyi <7glebpavlovskij7@gmail.com>
2025-09-06 18:33:53 +0200

    BJS2-85577
    Тут есть еще третий файл с настройками версии java и SDK. Не знаю если тоже надо ее пушить чтобы и у вас все работало когда открыли проект

--- src/main/java/school/faang/Main.java
+++ src/main/java/school/faang/Main.java
@@ -1,4 +1,13 @@
 package school.faang;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
+    public static void main(String[] args) {
+        int[] integers = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+
+        Example example = new Example();
+        example.reverseIt(integers);
+    }
 }
