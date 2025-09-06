c3a9a1a2500e1f510c728bf64a470924f20c2888
Rukk Taras <tarrukk@gmail.com>
2025-09-06 12:38:51 +0300

    Вопрос по поводу Lombok: а можно просто всегда при создании классов сразу писать @Data? Или это будет лишней нагрузкой на программу?

--- src/main/java/school/faang/BJS2_85507/Main.java
+++ src/main/java/school/faang/BJS2_85507/Main.java
@@ -1,7 +1,22 @@
 package school.faang.BJS2_85507;
 
-public class Main {
-    public static void main(String[] args) {
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
 
+public class Main {
+    public static void main(String[] args) throws Exception {
+        //Test:
+        Set<String> hobbies = new HashSet<>(Arrays.asList("Керлинг", "баскетбол", "хоккей"));
+        List<String> hobbies2 = Arrays.asList("Вышивание", "хоккей", "футбол");
+        User user1 = new User(1L, "Вася", 25, hobbies);
+        User user2 = new User(2L, "Петя", 23, hobbies);
+        User user3 = new User(3L, "Оля", 18, hobbies);
+        User user4 = new User(4L, "Глаша", 31, hobbies);
+        Set<User> users = new HashSet<>(Arrays.asList(user1, user2, user3, user4));
+        HashMap<User, String> usersAndTheirHobbies = User.findHobbyLovers(users, hobbies2);
+        System.out.println(List.of(usersAndTheirHobbies));
     }
 }
--- src/main/java/school/faang/BJS2_85507/User.java
+++ src/main/java/school/faang/BJS2_85507/User.java
@@ -1,22 +1,30 @@
 package school.faang.BJS2_85507;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
 import java.util.HashMap;
 import java.util.List;
+import java.util.Set;
 
+@AllArgsConstructor
+@Getter
 public class User {
-    Long id;
-    String name;
-    Long age;
-    List<String> hobbies;
+    private Long id;
+    private String name;
+    private int age;
+    private Set<String> hobbies;
 
-    public static HashMap<User, String> findHobbyLovers(User[] users, String[] hobbies) {
+    public static HashMap<User, String> findHobbyLovers(Set<User> users, List<String> hobbies) throws Exception {
         HashMap<User, String> usersAndTheirHobbies = new HashMap<>();
-        if (users != null && hobbies != null && users.length > 0 && hobbies.length > 0) {
-            for (User user : users) {
-                for (String hobby : hobbies) {
-                    if (user.hobbies.contains(hobby)) {
-                        usersAndTheirHobbies.put(user, hobby);
-                    }
+        if (users == null || hobbies == null || users.isEmpty() || hobbies.isEmpty()) {
+            throw new Exception("Список пуст или не существует.");
+        }
+        for (User user : users) {
+            for (String hobby : hobbies) {
+                if (user.hobbies.contains(hobby)) {
+                    usersAndTheirHobbies.put(user, hobby);
+                    break;
                 }
             }
         }
