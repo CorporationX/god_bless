91eb28b61cf2a28674c6d03758712664c8410563
aynarike <aynarike@gmail.com>
2025-09-08 02:39:11 +0500

    реализован метод reverse в классе Example

---
+++ src/main/java/school/faang/Example.java
@@ -0,0 +1,17 @@
+package school.faang;
+
+public class Example {
+    public void reverse(int[] arr){
+        int left = 0;
+        int right = arr.length-1;
+
+        while (left<right){
+            int temp  = arr[left];
+            arr[left] = arr[right];
+            arr[right] = temp;
+
+            left++;
+            right--;
+        }
+    }
+}
--- src/main/java/school/faang/Main.java
+++ src/main/java/school/faang/Main.java
@@ -1,4 +1,25 @@
 package school.faang;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Scanner;
+
 public class Main {
+    public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Введите длину массива: ");
+        int n = scanner.nextInt();
+        int[] nums = new int[n];
+
+        System.out.println("Введите элементы массива: ");
+        for (int i = 0; i < n; i++){
+            nums[i] = scanner.nextInt();
+        }
+        System.out.println("Исходный массив: " + Arrays.toString(nums));
+
+        Example example = new Example();
+        example.reverse(nums);
+        System.out.println("Перевернутый массив: " + Arrays.toString(nums));
+    }
 }
