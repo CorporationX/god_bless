bbf9945e0497c31a9d3cd65d557f307c5939fde1
kamil_fai <faiz.kamilka@gmail.com>
2025-09-08 00:45:28 +0400

    BJS2-85669, User registration

---
+++ src/main/java/school/faang/bjs2_85669/Main.java
@@ -0,0 +1,36 @@
+package school.faang.bjs2_85669;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class Main {
+    public static void main(String[] args) {
+        Set<User> users = new HashSet<>();
+
+        Object[][] inputData = {
+                {"Kamil", 22, "Google", "London"},
+                {"Tair", 17, "Uber", "New York"},
+                {"Elena", 30, "VRT", "London"},
+                {"Asya", 25, "Amazon", "Amsterdam"},
+        };
+
+        for (Object[] data : inputData) {
+            try {
+                User user = new User(
+                        (String) data[0],
+                        (int) data[1],
+                        (String) data[2],
+                        (String) data[3]
+                );
+                users.add(user);
+            } catch (IllegalArgumentException e) {
+                System.out.println(data[0] + ": " + e.getMessage());
+            }
+        }
+
+        System.out.println("\nSelected people:");
+        for (User user : users) {
+            System.out.println(user);
+        }
+    }
+}
---
+++ src/main/java/school/faang/bjs2_85669/User.java
@@ -0,0 +1,42 @@
+package school.faang.bjs2_85669;
+
+import lombok.Getter;
+
+import java.util.List;
+
+@Getter
+public class User {
+    public static final List<String> VALID_ADDRESSES = List.of("London", "New York", "Amsterdam");
+    public static final List<String> VALID_JOBS = List.of("Google", "Uber", "Amazon");
+
+    private String name;
+    private int age;
+    private String job;
+    private String address;
+
+    public User(String name, int age, String job, String address) {
+        if (name == null || name.isEmpty()) {
+            throw new IllegalArgumentException("Name cannot be empty.");
+        }
+        if (age < 18) {
+            throw new IllegalArgumentException("Age cannot be less than 18.");
+        }
+        if (!VALID_JOBS.contains(job)) {
+            throw new IllegalArgumentException("Must work in " + VALID_JOBS);
+        }
+        if (!VALID_ADDRESSES.contains(address)) {
+            throw new IllegalArgumentException("The residential address must be in " +
+                    VALID_ADDRESSES);
+        }
+
+        this.name = name;
+        this.age = age;
+        this.job = job;
+        this.address = address;
+    }
+
+    @Override
+    public String toString() {
+        return name + " (" + age + " y.o., " + job + ", " + address + ")";
+    }
+}
