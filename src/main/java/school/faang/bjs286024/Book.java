baa44bfce0b0d5eb611dd7cd19716c076ee77777
Admin <sofiya.chebotareva@bk.ru>
2025-09-07 19:39:18 +0200

    task BJS2-86024, fix after review

--- src/main/java/school/faang/bjs286024/Book.java
+++ src/main/java/school/faang/bjs286024/Book.java
@@ -1,53 +1,17 @@
 package school.faang.bjs286024;
 
-import java.util.Objects;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import lombok.ToString;
 
+@RequiredArgsConstructor
+@Getter
+@ToString
+@EqualsAndHashCode
 public class Book {
 
     private final String title;
     private final String author;
     private final int year;
-
-    public Book(String title, String author, int publishedYear) {
-        this.title = title;
-        this.author = author;
-        this.year = publishedYear;
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) {
-            return true;
-        }
-        if (!(o instanceof Book book)) {
-            return false;
-        }
-        return year == book.year && Objects.equals(title, book.title) && Objects.equals(author, book.author);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(title, author, year);
-    }
-
-    @Override
-    public String toString() {
-        return "Book{" +
-                "title='" + title + '\'' +
-                ", author='" + author + '\'' +
-                ", year=" + year +
-                '}';
-    }
-
-    public String getTitle() {
-        return title;
-    }
-
-    public String getAuthor() {
-        return author;
-    }
-
-    public int getYear() {
-        return year;
-    }
 }
--- src/main/java/school/faang/bjs286024/LibrarySystem.java
+++ src/main/java/school/faang/bjs286024/LibrarySystem.java
@@ -11,21 +11,13 @@ public class LibrarySystem {
     }
 
     public static void removeBook(String title, String author, int year) {
-        books.entrySet().removeIf(book -> book.getKey().getTitle().equals(title)
-                && book.getKey().getAuthor().equals(author)
-                && book.getKey().getYear() == year);
+        Book bookToRemove = new Book(title, author, year);
+        books.remove(bookToRemove);
     }
 
     public static void findBook(String title, String author, int year) {
-        for (Map.Entry<Book, String> book : books.entrySet()) {
-            if (book.getKey().getTitle().equals(title)
-                    && book.getKey().getAuthor().equals(author)
-                    && book.getKey().getYear() == year) {
-                System.out.println(book + " is located here:" + book.getValue());
-                return;
-            }
-        }
-        System.out.println("Book is not found!");
+        Book bookToFind = new Book(title, author, year);
+        System.out.println(books.getOrDefault(bookToFind, "Book is not found!"));
     }
 
     public static void printAllBooks() {
