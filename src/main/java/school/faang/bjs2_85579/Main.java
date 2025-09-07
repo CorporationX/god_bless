ac1bb8c666c97a77d23ae1d1eaad50357e5d255b
Roman Chipura <connor.ueyn@gmail.com>
2025-09-08 01:18:23 +0500

    Замечания учтены. Выполняется теперь через один цикл, вместо реализации используется общий класс (List)

--- src/main/java/school/faang/bjs2_85579/Main.java
+++ src/main/java/school/faang/bjs2_85579/Main.java
@@ -7,15 +7,15 @@ import java.util.Map;
 
 public class Main {
     public static void main(String[] args) {
-        Map<Integer, List<User>> groupedUsersByAge = User.groupUsers(new ArrayList<>(Arrays.asList(
-                new User("Andrew", 22),
-                new User("Roman", 22),
-                new User("Anya", 1),
-                new User("Oleg", 99),
-                new User("Vlad", 23),
-                new User("Alisa", 1)
+        Map<Integer, List<User>> groupedUsersByAge = User.groupUsers(List.of(
+                new User("Andrew", 22, "workplace", "address"),
+                new User("Roman", 22, "workplace", "address"),
+                new User("Anya", 1, "workplace", "address"),
+                new User("Oleg", 99, "workplace", "address"),
+                new User("Vlad", 23, "workplace", "address"),
+                new User("Alisa", 1, "workplace", "address")
 
-                )));
+                ));
 
         for (Map.Entry<Integer, List<User>> entry : groupedUsersByAge.entrySet()) {
             System.out.print("\n---\nВозраст: " + entry.getKey() + "\nПользователи: ");
--- src/main/java/school/faang/bjs2_85579/User.java
+++ src/main/java/school/faang/bjs2_85579/User.java
@@ -7,33 +7,30 @@ import java.util.HashMap;
 import java.util.Set;
 import java.util.HashSet;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 
+@AllArgsConstructor
 @Getter
 public class User {
     private String name;
     private int age;
-    private String workAddress;
-    private String homeAddress;
+    private String workplace;
+    private String address;
 
-    User(String name, int age) {
-        this.name = name;
-        this.age = age;
-        this.workAddress = "Lorem Ipsum";
-        this.homeAddress = "Lorem Ipsum";
-    }
-
-    public static Map<Integer, List<User>> groupUsers(ArrayList<User> users) {
+    public static Map<Integer, List<User>> groupUsers(List<User> users) {
         Map<Integer, List<User>> agesAndUsers = new HashMap<>();
-        Set<Integer> userAges = new HashSet<>();
+
         for (User user : users) {
-            userAges.add(user.getAge());
+            List<User> sameAgeUsers = agesAndUsers.get(user.getAge());
+            if (sameAgeUsers == null) {
+                sameAgeUsers = new ArrayList<>();
+                agesAndUsers.put(user.getAge(), sameAgeUsers);
+            }
+            sameAgeUsers.add(user);
         }
-        for (int age :  userAges) {
-            ArrayList<User> sameAgeUsers = new ArrayList<>(users);
-            sameAgeUsers.removeIf(user -> user.getAge() != age);
-            agesAndUsers.put(age, sameAgeUsers);
-        }
+
+        System.out.println("Содержание:   " + agesAndUsers);
 
         return agesAndUsers;
     }
