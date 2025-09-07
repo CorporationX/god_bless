b814db08eb5d1fb6ca8f7e513359f402f9d06131
Lily <lilly.liho@gmail.com>
2025-09-07 13:08:43 +0300

    add user class with fields validation

--- src/main/java/school/faang/Main.java
+++ src/main/java/school/faang/Main.java
@@ -1,4 +1,11 @@
 package school.faang;
 
 public class Main {
+    public static void main(String[] args) {
+        try {
+            new User("lily", 36, "Google", "Amsterdam");
+        } catch (IllegalArgumentException e) {
+            System.err.println("Validation failed: " + e.getMessage());
+        }
+    }
 }
