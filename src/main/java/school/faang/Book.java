108058269f034b60f22b6a7e608b611c55e96095
Lily <lilly.liho@gmail.com>
2025-09-07 18:06:31 +0300

    add book library system

---
+++ src/main/java/school/faang/Book.java
@@ -0,0 +1,32 @@
+package school.faang;
+
+import java.util.Objects;
+
+public class Book {
+    private final String title;
+    private final String author;
+    private final int year;
+
+    public Book(String title, String author, int year) {
+        this.title = title;
+        this.author = author;
+        this.year = year;
+    }
+
+    @Override
+    public boolean equals(Object bootToCheck) {
+        if (!(bootToCheck instanceof Book book)) return false;
+        return Objects.equals(book.title, this.title) && Objects.equals(book.author, this.author) && Objects.equals(book.year, this.year);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(title, author, year);
+    }
+
+    @Override
+    public String toString() {
+        return "title:'" + title + "', author:'" + author + "', year:" + year + " ";
+    }
+
+}
--- src/main/java/school/faang/Main.java
+++ src/main/java/school/faang/Main.java
@@ -1,4 +1,27 @@
 package school.faang;
 
+import java.util.NoSuchElementException;
+
 public class Main {
+    public static void main(String[] args) {
+        LibrarySystem library = new LibrarySystem();
+
+        library.addBook("Dune", "Frank Herbert", 1965, "123");
+        library.addBook("Clean Code", "Robert Martin", 2008, "345");
+
+        library.printAllBooks();
+
+        String duneLoc = library.findBook("Dune", "Frank Herbert", 1965);
+        System.out.println("Dune location: " + duneLoc);
+
+        library.removeBook("Clean Code", "Robert Martin", 2008);
+        library.printAllBooks();
+
+        try {
+            library.findBook("Clean Code", "Robert Martin", 2008);
+        } catch (NoSuchElementException e) {
+            System.out.println(e.getMessage());
+        }
+
+    }
 }
