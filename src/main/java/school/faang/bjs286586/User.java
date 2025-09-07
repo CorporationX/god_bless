673d1922d7b30f6e2f161be7dc57bc20a7e20e71
Admin <sofiya.chebotareva@bk.ru>
2025-09-07 18:46:35 +0200

    task BJS2-86586, fix after review

--- src/main/java/school/faang/bjs286586/User.java
+++ src/main/java/school/faang/bjs286586/User.java
@@ -1,12 +1,19 @@
 package school.faang.bjs286586;
 
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.ToString;
+
 import java.util.Objects;
 import java.util.Set;
 
+@Getter
+@EqualsAndHashCode
+@ToString
 public class User {
     private static final Set<String> VALID_JOBS = Set.of("Google", "Uber", "Amazon");
     private static final Set<String> VALID_ADDRESSES = Set.of("London", "New York", "Amsterdam");
-
+    private final int MIN_AGE = 18;
 
     private final String name;
     private final int age;
@@ -16,59 +23,23 @@ public class User {
     public User(String name, int age, String job, String address) {
         if (name.isBlank()) {
             throw new IllegalArgumentException("Name can`t be empty: " + name);
-        } else if (age < 18) {
-            throw new IllegalArgumentException("Age can`t be under 18: " + age);
-        } else if (!VALID_JOBS.contains(job)) {
-            throw new IllegalArgumentException("Job have to be in list of VALID_JOBS: " + job);
-        } else if (!VALID_ADDRESSES.contains(address)) {
-            throw new IllegalArgumentException("Address have to be in list of VALID_ADDRESSES: " + address);
+        }
+
+        if (age < MIN_AGE) {
+            throw new IllegalArgumentException("Age can`t be under " + MIN_AGE + ": " + age);
+        }
+
+        if (!VALID_JOBS.contains(job)) {
+            throw new IllegalArgumentException("Job have to be in list of %s: %s".formatted(VALID_JOBS, job));
+        }
+
+        if (!VALID_ADDRESSES.contains(address)) {
+            throw new IllegalArgumentException("Address have to be in list of %s: %s".formatted(VALID_ADDRESSES,
+                    address));
         }
         this.name = name;
         this.age = age;
         this.job = job;
         this.address = address;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public int getAge() {
-        return age;
-    }
-
-    public String getJob() {
-        return job;
-    }
-
-    public String getAddress() {
-        return address;
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) {
-            return true;
-        }
-        if (!(o instanceof User user)) {
-            return false;
-        }
-        return getAge() == user.getAge() && Objects.equals(getName(), user.getName()) && Objects.equals(getJob(),
-                user.getJob()) && Objects.equals(getAddress(), user.getAddress());
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(getName(), getAge(), getJob(), getAddress());
-    }
-
-    @Override
-    public String toString() {
-        return "User{" +
-                "name='" + name + '\'' +
-                ", age=" + age +
-                ", job='" + job + '\'' +
-                ", address='" + address + '\'' +
-                '}';
     }
 }
