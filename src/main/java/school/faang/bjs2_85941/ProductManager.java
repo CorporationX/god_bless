f46937ac7c824d6e1e7d2d98aefd2489d98678b5
Pavel-Lukyanov <PavelLukyanov2017@yandex.ru>
2025-09-07 21:10:16 +0500

    Правки после код ревью

--- src/main/java/school/faang/bjs2_85941/ProductManager.java
+++ src/main/java/school/faang/bjs2_85941/ProductManager.java
@@ -1,10 +1,10 @@
 package school.faang.bjs2_85941;
 
-import lombok.EqualsAndHashCode;
+import lombok.extern.slf4j.Slf4j;
 
 import java.util.*;
 
-@EqualsAndHashCode
+@Slf4j
 public class ProductManager {
     private Set<Product> products = new HashSet<>();
     private Map<Category, List<Product>> productGroups = new HashMap<>();
@@ -13,24 +13,24 @@ public class ProductManager {
     public void addProduct(Category category, String name) {
         for (Product product : products) {
             if (product.getName().equals(name) && product.getCategory().equals(category)) {
-                System.out.println("Продукт с таким именем уже существует в категории " + category);
+                log.warn("Продукт с таким именем уже существует в категории {}", category);
                 return;
             }
         }
 
         Product product = new Product(id++, name, category);
         products.add(product);
-        System.out.println("Добавлен продукт " + product);
+        log.info("Добавлен продукт {}", product);
     }
 
     public void removeProduct(Category category, String name) {
-        boolean isRemoved = products.removeIf(item
-            -> item.getName().equals(name) && item.getCategory().equals(category));
+        boolean isRemoved = products.removeIf(item ->
+                item.getName().equals(name) && item.getCategory().equals(category));
 
         if (isRemoved) {
-            System.out.println("Продукт " + category + " " + name + " удален");
+            log.info("Продукт {} {} удален", category, name);
         } else {
-            System.out.println("Продукт " + category + " " + name + " не найден");
+            log.warn("Продукт {} {} не найден", category, name);
         }
     }
 
@@ -59,10 +59,9 @@ public class ProductManager {
         Map<Category, List<Product>> productGroups = groupProductsByCategory();
 
         productGroups.forEach((category, productList) -> {
-            System.out.println("Категория: " + category);
-            System.out.println("Продукты:");
-            productList.forEach(product -> System.out.println(" - " + product.getName()));
-            System.out.println();
+            log.info("Категория: {}", category);
+            log.info("Продукты:");
+            productList.forEach(product -> log.info(" - {}", product.getName()));
         });
     }
 }
