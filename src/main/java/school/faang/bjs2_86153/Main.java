5475d55b90a18a234644ecd2186b70c67901c4d2
Kirill Mir <loocyep@gmail.com>
2025-09-07 22:35:00 +0300

    completed task BJS2-86153

---
+++ src/main/java/school/faang/bjs2_86153/Main.java
@@ -0,0 +1,23 @@
+package school.faang.bjs2_86153;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Main {
+    public static void main(String[] args) {
+        HashMap<Book, String> bookLocation = new HashMap<>();
+        LibrarySystem librarySystem = new LibrarySystem(bookLocation);
+
+        librarySystem.addBook("History of world", "Marsel", 3, "shelf 1");
+        librarySystem.addBook("Math of world", "Alan", 2, "shelf 2");
+        librarySystem.addBook("Biological of world", "Mark", 4, "shelf 3");
+        librarySystem.addBook("Physics of world", "Septi", 5, "shelf 4");
+
+        librarySystem.removeBook("History of world", "Marsel", 3);
+
+        librarySystem.findBook("Math of world", "Alan", 2);
+
+        librarySystem.printAllBooks();
+
+    }
+}
