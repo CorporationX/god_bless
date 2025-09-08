f183b3c83ef5c3c8431ca932ebf9d22b5c0cf10e
Kuanysh <skn_88@mail.ru>
2025-09-08 21:29:46 +0500

    BJS2-88488 Solution of the task

---
+++ src/main/java/school/faang/bsj2_88488/SpellAction.java
@@ -0,0 +1,6 @@
+package school.faang.bsj2_88488;
+
+@FunctionalInterface
+public interface SpellAction {
+    String spellResult(String spellName);
+}
---
+++ src/main/java/school/faang/bsj2_88488/SpellCaster.java
@@ -0,0 +1,7 @@
+package school.faang.bsj2_88488;
+
+public class SpellCaster {
+    public void cast(String spellName, SpellAction spellAction) {
+        System.out.println(spellName + ", " + spellAction.spellResult(spellName));
+    }
+}
