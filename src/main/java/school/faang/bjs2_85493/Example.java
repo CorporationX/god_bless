201fed71c39fcbf983d06ab916ab37e0df073523
K0I2T <hiro94337@gmail.com>
2025-09-08 15:07:56 +0300

    переделанная задача

--- src/main/java/school/faang/bjs2_85493/Example.java
+++ src/main/java/school/faang/bjs2_85493/Example.java
@@ -2,10 +2,21 @@ package school.faang.bjs2_85493;
 
 public class Example {
     public int[] reverse(int[] nums) {
-        for (int i = 0; i < nums.length; i++) {
-            int temp = nums[i];
-            nums[i] = nums[nums.length - 1 - i];
-            nums[nums.length - 1 - i] = temp;
+        if (nums == null) {
+            System.err.println("Нулевой массив");
+            return null;
+        }
+
+        int length = nums.length;
+        if (length <= 1) {
+            return nums;
+        } else {
+            long center = Math.round((float) length / 2); //ищем середину для переворота массива
+            for (int i = 0; i < center; i++) {
+                int temp = nums[i];
+                nums[i] = nums[length - 1 - i];
+                nums[length - 1 - i] = temp;
+            }
         }
         return nums;
     }
--- src/main/java/school/faang/bjs2_85493/Main.java
+++ src/main/java/school/faang/bjs2_85493/Main.java
@@ -5,8 +5,20 @@ import java.util.Arrays;
 public class Main {
     public static void main(String[] args) {
         Example arr1 = new Example();
-        int[] nums = {1, 2, 3, 4, 5};
-        int[] numsReverse = arr1.reverse(nums);
-        System.out.println(Arrays.toString(numsReverse));
+
+        //пример с null массивом
+        int[] nums1 = null;
+        int[] numsReverse1 = arr1.reverse(nums1);
+        System.out.println(Arrays.toString(numsReverse1));
+
+        //пример с массивом в один символ
+        int[] nums2 = {0};
+        int[] numsReverse2 = arr1.reverse(nums2);
+        System.out.println(Arrays.toString(numsReverse2));
+
+        //пример с большим массивом
+        int[] nums3 = {1, 2, 3, 5, 2};
+        int[] numsReverse3 = arr1.reverse(nums3);
+        System.out.println(Arrays.toString(numsReverse3));
     }
 }
