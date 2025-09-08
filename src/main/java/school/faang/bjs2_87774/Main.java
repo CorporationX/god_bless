889d89e0c25a4b905c7f3773e96f75af96a17b2a
Yasin <yasin.akhmadov555@inbox.ru>
2025-09-08 20:40:45 +0300

    Instagram Filters

---
+++ src/main/java/school/faang/bjs2_87774/Main.java
@@ -0,0 +1,28 @@
+package school.faang.bjs2_87774;
+
+import java.util.function.Function;
+
+public class Main {
+    public static void main(String[] args) {
+        FilterProcessor filterProcessor = new FilterProcessor();
+
+        Function<Image, Image> blackAndWhite = image -> new Image(image.name(),
+                image.description() + " | Наложен Черно-Белый фильтр");
+
+        Function<Image, Image> cassetteFilter = image -> new Image(image.name(),
+                image.description() + " | Наложен фильтр Нуар");
+
+        Function<Image, Image> videocamFilter = image -> new Image(image.name(),
+                image.description() + " | Наложен фильтр Видеокамера");
+
+        Image simpleImage = new Image("party-photo.jpg", "Оригинальное изображение");
+        System.out.println(simpleImage);
+
+        simpleImage = filterProcessor.applyFilter(simpleImage, blackAndWhite);
+        System.out.println(simpleImage);
+
+        Function<Image, Image> combinedFilter = filterProcessor.combineFilters(cassetteFilter, videocamFilter);
+        simpleImage = filterProcessor.applyFilter(simpleImage, combinedFilter);
+        System.out.println(simpleImage);
+    }
+}
