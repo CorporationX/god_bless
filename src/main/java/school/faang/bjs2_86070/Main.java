be0f5ba7e32188fadeaa181177bc2ec00fa1f0ea
Mikhail EASY <mghievrie@bk.ru>
2025-09-08 19:18:26 +0500

    Выполнил задачу - Разбираем склад Amazon

---
+++ src/main/java/school/faang/bjs2_86070/Main.java
@@ -0,0 +1,26 @@
+package school.faang.bjs2_86070;
+
+import java.util.List;
+import java.util.Map;
+
+public class Main {
+    public static void main(String[] args) {
+        ProductManager productManager = new ProductManager();
+        try {
+            productManager.addProduct(Category.ELECTRONICS, "iPhone 15 Pro");
+            productManager.addProduct(Category.ELECTRONICS, "Samsung Galaxy S24");
+            productManager.addProduct(Category.CLOTHING, "Джинсы Levi's 501");
+            productManager.addProduct(Category.CLOTHING, "Футболка хлопковая");
+            productManager.addProduct(Category.FOOD, "Молоко 3.2%");
+            productManager.addProduct(Category.FOOD, "Хлеб ржаной");
+            productManager.addProduct(Category.OTHER, "Подарочный сертификат");
+            productManager.addProduct(Category.OTHER, "Набор для вышивания");
+            productManager.removeProduct(Category.CLOTHING, "Футболка хлопковая");
+            productManager.removeProduct(Category.CLOTHING, "Платье");
+            productManager.findProductsByCategory(Category.CLOTHING);
+        } catch (IllegalArgumentException e) {
+            System.out.println(e.getMessage());
+        }
+        productManager.printAllProducts();
+    }
+}
---
+++ src/main/java/school/faang/bjs2_86070/Product.java
@@ -0,0 +1,21 @@
+package school.faang.bjs2_86070;
+
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.ToString;
+
+@EqualsAndHashCode
+@Getter
+@ToString
+
+public class Product {
+    private static int id;
+    private String name;
+    private Category category;
+
+    public Product(Category category, String name) {
+        this.id = id++;
+        this.name = name;
+        this.category = category;
+    }
+}
