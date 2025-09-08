d6ad6a7f8c85e4d0574c4862e181d32fe49c49f1
Denis Chudov <Chdnru@gmail.com>
2025-09-09 00:51:39 +0300

    fix Задачи BJS2-85510(Группировка пользователей по возрасту)

--- src/main/java/school/faang/BJS2_85510/Main.java
+++ src/main/java/school/faang/BJS2_85510/Main.java
@@ -1,4 +1,4 @@
-package school.faang;
+package school.faang.BJS2_85510;
 
 import java.util.ArrayList;
 import java.util.List;
--- src/main/java/school/faang/BJS2_85510/User.java
+++ src/main/java/school/faang/BJS2_85510/User.java
@@ -1,4 +1,4 @@
-package school.faang;
+package school.faang.BJS2_85510;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -9,27 +9,22 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-
 @Getter
 @Setter
 @AllArgsConstructor
 public class User {
     private String name;
     private int age;
-    private String placeOfWork;
+    private String workplace;
     private String address;
-
 
     public static Map<Integer, List<User>> groupUsers(List<User> users) {
         Map<Integer, List<User>> usersByAge = new HashMap<>();
 
         for (User user : users) {
-            if (!usersByAge.containsKey(user.age)) {
-                usersByAge.put(user.age, new ArrayList<>());
-            }
+            usersByAge.putIfAbsent(user.age, new ArrayList<>());
             usersByAge.get(user.age).add(user);
         }
         return usersByAge;
     }
-
 }
