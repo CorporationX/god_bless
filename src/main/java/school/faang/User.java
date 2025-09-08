7b8a0eb48addeb7183397a25b4000874b941db10
Ruslan <ruslan8020110@gmail.com>
2025-09-08 14:21:39 +0300

    1

--- src/main/java/school/faang/User.java
+++ src/main/java/school/faang/User.java
@@ -25,7 +25,7 @@ public class User {
 
     public static Map<Integer, List<User>> groupUsers(List<User> users) {
         Map<Integer, List<User>> sort = new HashMap<>();
-        for(User user : users) {
+        for (User user : users) {
             sort.computeIfAbsent(user.getAge(), i -> new ArrayList<>()).add(user);
         }
         return sort;
