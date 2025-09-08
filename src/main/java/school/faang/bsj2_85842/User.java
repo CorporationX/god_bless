cddf0a6084960f43fb76c764e27513d1472a4011
Kuanysh <skn_88@mail.ru>
2025-09-08 13:18:51 +0500

    BJS2-85842 fix comments from PR

--- src/main/java/school/faang/bsj2_85842/User.java
+++ src/main/java/school/faang/bsj2_85842/User.java
@@ -23,7 +23,7 @@ public class User {
     public static Map<Integer, List<User>> groupUsers(List<User> userList) {
         Map<Integer, List<User>> groupUsers = new HashMap<>();
         for (User user : userList) {
-                List<User> valueUserList = new ArrayList<>();
+            List<User> valueUserList = new ArrayList<>();
             if (groupUsers.get(user.age) != null) {
                 valueUserList = groupUsers.get(user.age);
             }
