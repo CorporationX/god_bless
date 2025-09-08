124fb8894469e366fe7de4af9c661061f86817f1
Viks <viktor.naroznoi@gmail.com>
2025-09-08 18:16:41 +0300

    --BJS2-85892 - Ловим события

---
+++ src/main/java/school/faang/BJS2_85892/SpellEvent.java
@@ -0,0 +1,15 @@
+package school.faang.BJS2_85892;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+public class SpellEvent {
+
+    private int id;
+    private String eventType;
+    private String action;
+}
