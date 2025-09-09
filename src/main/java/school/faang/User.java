11869e14823a9322ad0f637e68208514b0e9e07e
Elya Khamzina <ekkhamzi@mail.ru>
2025-09-09 12:22:51 +0300

    Корректировки: 1.main: import; 2. user: add import lombok, correct import, correct @DaTa, correct String workplace

--- src/main/java/school/faang/User.java
+++ src/main/java/school/faang/User.java
@@ -1,15 +1,23 @@
 package school.faang;
 
-import lombok.Data;
+import lombok.Getter;
+import lombok.ToString;
 import lombok.AllArgsConstructor;
-import java.util.*;
+import lombok.EqualsAndHashCode;
 
-@Data
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@Getter
+@ToString
 @AllArgsConstructor
+@EqualsAndHashCode
 public class User {
     private String name;
     private int age;
-    private String placeOfWork;
+    private String workplace;
     private String address;
 
     public static Map<Integer, List<User>> groupUsers(List<User> users) {
