5c7083d09af64535dece8e4f0ea7f6b97a5db5f3
leto007 <liga07@bk.ru>
2025-09-10 00:57:46 +0300

    Исправил задачу BJS2-85589

--- src/main/java/school/faang/userregistration/Usermain.java
+++ src/main/java/school/faang/userregistration/Usermain.java
@@ -4,40 +4,35 @@ package school.faang.userregistration;
 
 public class Usermain {
     public static void main(String[] args) {
-        ValidatorRegistrator validator = new ValidatorRegistrator();
 
         User user = new User("name", 17, "Google", "London");
         User user1 = new User("name1", 18, "Googl", "London");
         User user2 = new User("name2", 18, "Google", "Londo");
         User user3 = new User("name3", 18, "Google", "London");
         User user4 = new User("name4", 18, "Google", "London");
-        try {
 
-            validator.validate(user);
+        try {
+            ValidatorRegistrator.validate(user);
         } catch (Exception e) {
             System.err.println(e.getMessage());
         }
         try {
-
-            validator.validate(user1);
+            ValidatorRegistrator.validate(user1);
         } catch (Exception e) {
             System.err.println(e.getMessage());
         }
         try {
-
-            validator.validate(user2);
+            ValidatorRegistrator.validate(user2);
         } catch (Exception e) {
             System.err.println(e.getMessage());
         }
         try {
-
-            validator.validate(user3);
+            ValidatorRegistrator.validate(user3);
         } catch (Exception e) {
             System.err.println(e.getMessage());
         }
         try {
-
-            validator.validate(user4);
+            ValidatorRegistrator.validate(user4);
         } catch (Exception e) {
             System.err.println(e.getMessage());
         }
--- src/main/java/school/faang/userregistration/ValidatorRegistrator.java
+++ src/main/java/school/faang/userregistration/ValidatorRegistrator.java
@@ -5,22 +5,22 @@ import java.util.Set;
 public class ValidatorRegistrator {
     private static final Set<String> VALID_JOBS = Set.of("Google", "Uber", "Amazon");
     private static final Set<String> VALID_ADDRESSES = Set.of("London", "New York", "Amsterdam");
-    private static final int minAge = 18;
+    private static final int MIN_AGE = 18;
 
-    public void validate(User user) {
+    public static void validate(User user) {
         if (user.getName().isBlank()) {
             throw new IllegalArgumentException("%s имя не может быть пустым".formatted(user.getName()));
         }
-        if (user.getAge() < minAge) {
+        if (user.getAge() < MIN_AGE) {
             throw new IllegalArgumentException("%s Возраст не может быть меньше 18".formatted(user.getName()));
         }
         if (!VALID_ADDRESSES.contains(user.getAddress())) {
-            throw new IllegalArgumentException(("%s Адрес может быть только: ".formatted(user.getName())
-                                                + VALID_ADDRESSES));
+            throw new IllegalArgumentException(("%s Адрес может быть только: %s".formatted(user.getName(),
+                    VALID_ADDRESSES)));
         }
         if (!VALID_JOBS.contains(user.getWorkplace())) {
-            throw new IllegalArgumentException(("%s Место работы может быть только таким: ".formatted(user.getName())
-                                                + VALID_JOBS));
+            throw new IllegalArgumentException(("%s Место работы может быть только таким: %s".formatted(user.getName(),
+                    VALID_JOBS)));
         }
     }
 }
