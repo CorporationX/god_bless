c922194f693d068e25e5279fcfcaf95c963b9d7b
EdelweissPC <shherbakov99ilya@mail.ru>
2025-09-09 16:17:05 +0300

    BJS2-87879 learning Function<U,V>

---
+++ src/main/java/school/faang/bjs2_87879/Image.java
@@ -0,0 +1,18 @@
+package school.faang.bjs2_87879;
+
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@ToString
+@EqualsAndHashCode
+public class Image {
+    private String name;
+    private String description;
+
+}
--- src/main/java/school/faang/bjs2_87879/Main.java
+++ src/main/java/school/faang/bjs2_87879/Main.java
@@ -1,9 +1,25 @@
 package school.faang.bjs2_87879;
 
 
+import java.util.function.Function;
+
 public class Main {
     public static void main(String[] args) {
 
+        Image imageOrig = new Image("dog", "dog on table");
+        Function<Image, Image> blackAndWhite = image -> (new Image(image.getName(), ("%s | black and white")
+                .formatted(image.getDescription())));
+
+        FilterProcessor filterProcessor = new FilterProcessor();
+        System.out.println(imageOrig);
+        Image imageAfterFilter = filterProcessor.applyFilter(imageOrig, blackAndWhite);
+        System.out.println(imageAfterFilter);
+
+        Image imageOrigTwo = new Image("cat", "Cat on table");
+        Function<Image, Image> filterNet = image -> (new Image(image.getName(), ("%s | Net")
+                .formatted(image.getDescription())));
+        Function<Image, Image> filter = filterProcessor.combineFilters(blackAndWhite, filterNet);
+        System.out.println(filter.apply(imageOrigTwo));
     }
 
 }
