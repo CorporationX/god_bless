01b6ebb161e3e480cd9f7f921a8feda97924f1e9
mipkasnow <steamer08@mail.ru>
2025-09-09 17:08:22 +0300

    BJS2-87276: Added Lotr inventory manager

---
+++ src/main/java/school/faang/module2/lotr/CharacterException.java
@@ -0,0 +1,4 @@
+package school.faang.module2.lotr;
+
+public class CharacterException extends RuntimeException{
+}
---
+++ src/main/java/school/faang/module2/lotr/InventoryManager.java
@@ -0,0 +1,59 @@
+package school.faang.module2.lotr;
+
+import java.util.HashSet;
+import java.util.List;
+import java.util.Optional;
+import java.util.Set;
+import java.util.function.Consumer;
+import java.util.function.Function;
+import java.util.function.Predicate;
+
+public class InventoryManager {
+
+    private final Set<Character> characters = new HashSet<>();
+
+    public Character createCharacter(Character characterToCreate) {
+        Optional<Character> foundCharacter = findCharacter(characterToCreate);
+        if (foundCharacter.isEmpty()) {
+            characters.add(characterToCreate);
+        }
+
+        return characterToCreate;
+    }
+
+    public void addItem(Character character, Item item, Consumer<Item> itemConsumer) {
+        Optional<Character> foundCharacter = findCharacter(character);
+        if (foundCharacter.isEmpty()) {
+            throw new CharacterException();
+        }
+
+        foundCharacter.get().inventory().add(item);
+        itemConsumer.accept(item);
+    }
+
+    public void removeItem(Character character, Predicate<Item> itemPredicate) {
+        Optional<Character> foundCharacter = findCharacter(character);
+        if (foundCharacter.isEmpty()) {
+            throw new CharacterException();
+        }
+
+        foundCharacter.get().inventory().removeIf(itemPredicate);
+    }
+
+    public void updateItem(Character character, Predicate<Item> itemPredicate, Function<Item, Item> itemFunction) {
+        Optional<Character> foundCharacter = findCharacter(character);
+        if (foundCharacter.isEmpty()) {
+            throw new CharacterException();
+        }
+
+        List<Item> inventory = foundCharacter.get().inventory();
+        inventory.replaceAll(item -> itemPredicate.test(item) ? itemFunction.apply(item) : item);
+    }
+
+
+    private Optional<Character> findCharacter(Character character) {
+        return characters.stream()
+                .filter(c -> c.equals(character))
+                .findFirst();
+    }
+}
