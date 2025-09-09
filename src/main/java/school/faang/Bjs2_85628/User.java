ce6126301e05f1de6ec6af95d5481b2eedea0906
ayazs <ayaz-22@yandex.ru>
2025-09-09 22:20:05 +0500

    access modifiers is fixed

--- src/main/java/school/faang/Bjs2_85628/User.java
+++ src/main/java/school/faang/Bjs2_85628/User.java
@@ -4,13 +4,13 @@ import java.util.Set;
 
 public class User {
     private static final int MINIMUM_AGE = 18;
+    private static final Set<String> VALID_JOB = Set.of("Google", "Uber", "Amazon");
+    private static final Set<String> VALID_ADDRESS = Set.of("Лондон", "Нью-Йорк", "Амстердам");
+
     private String name;
     private int age;
     private String job;
     private String address;
-
-    public static final Set<String> VALID_JOB = Set.of("Google", "Uber", "Amazon");
-    public static final Set<String> VALID_ADDRESS = Set.of("Лондон", "Нью-Йорк", "Амстердам");
 
     public User(String name, int age, String job, String address) {
         validateName(name);
