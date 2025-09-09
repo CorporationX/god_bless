17ef6cc7151d87c4659ed7f2475e33d7483f2d36
Mikhail EASY <mghievrie@bk.ru>
2025-09-09 17:05:21 +0500

    поправил вывод BJS2-87448

--- src/main/java/school/faang/bjs2_87448/Main.java
+++ src/main/java/school/faang/bjs2_87448/Main.java
@@ -3,9 +3,13 @@ package school.faang.bjs2_87448;
 public class Main {
     public static void main(String[] args) {
         SpellCaster caster = new SpellCaster();
-        caster.cast("Алахомора", spellName -> {
-            System.out.println("Произноситься заклинание: " + spellName);
-            return "Заклинание открыло магический замок";
-        });
+
+        String alohomora = "Alohomora";
+        String lumos = "Lumos";
+        String expelliarmus = "Expelliarmus";
+
+        caster.cast(alohomora, spellName -> ("Дверь открыта при помощи заклинания " + spellName));
+        caster.cast(lumos, spellName -> ("Сильный источник света при помощи заклинания " + spellName));
+        caster.cast(expelliarmus, spellName -> ("Палочка выпрыгнула из рук при помощи заклинания ") + spellName);
     }
 }
