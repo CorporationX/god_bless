79fe535fc3e874c03f6de02a08823945b1ae992c
EdelweissPC <shherbakov99ilya@mail.ru>
2025-09-10 01:52:46 +0300

    BJS2-87372 delete StringBuilder

--- src/main/java/school/faang/bjs2_87372/SpellCast.java
+++ src/main/java/school/faang/bjs2_87372/SpellCast.java
@@ -3,8 +3,6 @@ package school.faang.bjs2_87372;
 public class SpellCast {
     public void cast(String spellName, SpellAction spellAction) {
         String result = spellAction.castSpell(spellName);
-        StringBuilder sb = new StringBuilder(result);
-        result = sb.reverse().toString();
         System.out.println(result.toUpperCase());
     }
 }
