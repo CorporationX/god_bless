14e866fc9230e520fd36468422357b534649348d
Admin <sofiya.chebotareva@bk.ru>
2025-09-09 09:29:56 +0200

    task BJS2-87104, fix after review

--- src/main/java/bjs287104/Product.java
+++ src/main/java/bjs287104/Product.java
@@ -1,65 +1,16 @@
 package bjs287104;
 
-import java.util.Objects;
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.ToString;
 
+@Getter
+@EqualsAndHashCode
+@ToString
+@AllArgsConstructor
 public class Product {
     private int id;
     private String name;
     private Category category;
-
-    public Product(int id, String name, Category category) {
-        this.id = id;
-        this.name = name;
-        this.category = category;
-    }
-
-    public int getId() {
-        return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public Category getCategory() {
-        return category;
-    }
-
-    public void setCategory(Category category) {
-        this.category = category;
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) {
-            return true;
-        }
-        if (!(o instanceof Product product)) {
-            return false;
-        }
-        return getId() == product.getId() && Objects.equals(getName(),
-                product.getName()) && getCategory() == product.getCategory();
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(getId(), getName(), getCategory());
-    }
-
-    @Override
-    public String toString() {
-        return "Product{" +
-                "id=" + id +
-                ", name='" + name + '\'' +
-                ", category=" + category +
-                '}';
-    }
 }
--- src/main/java/bjs287104/ProductManager.java
+++ src/main/java/bjs287104/ProductManager.java
@@ -8,25 +8,31 @@ import java.util.Map;
 import java.util.Set;
 
 public class ProductManager {
-    private Set<Product> products;
-    Map<Category, List<Product>> categoryAndProducts;
-    static int id = 1;
-
-    public ProductManager() {
-        this.products = new HashSet<>();
-    }
+    private Set<Product> products = new HashSet<>();
+    private Map<Category, List<Product>> categoryAndProducts = new HashMap<>();
+    private static int id = 1;
 
     public void addProduct(Category category, String name) {
+        if (category == null || name == null) {
+            throw new IllegalArgumentException("category and name must not be null");
+        }
         Product product = new Product(id, name, category);
+        id++;
         products.add(product);
     }
 
     public void removeProduct(Category category, String name) {
+        if (category == null || name == null) {
+            throw new IllegalArgumentException("category and name must not be null");
+        }
         products.removeIf(product -> product.getCategory().equals(category)
                 && product.getName().equals(name));
     }
 
     public List<Product> findProductsByCategory(Category category) {
+        if (category == null) {
+            throw new IllegalArgumentException("category must not be null");
+        }
         List<Product> productsByCategory = new ArrayList<>();
         for (Product product : products) {
             if (product.getCategory().equals(category)) {
@@ -37,7 +43,6 @@ public class ProductManager {
     }
 
     public Map<Category, List<Product>> groupProductsByCategory() {
-        categoryAndProducts = new HashMap<>();
         for (Product product : products) {
             if (!categoryAndProducts.containsKey(product.getCategory())) {
                 categoryAndProducts.put(product.getCategory(), new ArrayList<>());
