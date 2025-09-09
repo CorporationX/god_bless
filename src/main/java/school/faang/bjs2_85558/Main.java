48859feec91f363a165ea65fc3f305e24b107683
Asta <charlyfazbear@gmail.com>
2025-09-09 10:39:59 +0300

    made new corrections

--- src/main/java/school/faang/bjs2_85558/Main.java
+++ src/main/java/school/faang/bjs2_85558/Main.java
@@ -3,8 +3,10 @@ package school.faang.bjs2_85558;
 public class Main {
     public static void main(String[] args) {
         try {
-            new User("Margaret", 39, "Google", "London");
-            new User("", 30, "Amazon", "Amsterdam");
+            User user1 = new User("Margaret", 39, "Google", "London");
+            System.out.println(user1);
+            User user2 = new User("Dan", 30, "Amazon", "Moscow");
+            System.out.println(user2);
         } catch (IllegalArgumentException e) {
             System.out.println("Error creating user: " + e.getMessage());
         }
--- src/main/java/school/faang/bjs2_85558/User.java
+++ src/main/java/school/faang/bjs2_85558/User.java
@@ -1,8 +1,11 @@
 package school.faang.bjs2_85558;
 
 import java.util.Set;
-import lombok.Getter;
 
+import lombok.Getter;
+import lombok.ToString;
+
+@ToString
 @Getter
 public class User {
     private final String name;
@@ -10,14 +13,24 @@ public class User {
     private final String job;
     private final String address;
 
+    private static final int MIN_AGE = 18;
+
     public static final Set<String> VALID_JOBS = Set.of("Google", "Uber", "Amazon");
     public static final Set<String> VALID_ADDRESS = Set.of("London", "New York", "Amsterdam");
 
     public User(String name, int age, String job, String address) {
+        validate(name, age, job, address);
+        this.name = name;
+        this.age = age;
+        this.job = job;
+        this.address = address;
+    }
+
+    private void validate(String name, int age, String job, String address) {
         if (name == null || name.isBlank()) {
             throw new IllegalArgumentException("Name cannot be empty");
         }
-        if (age < 18) {
+        if (age < MIN_AGE) {
             throw new IllegalArgumentException("Age cannot be less than 18");
         }
         if (!VALID_JOBS.contains(job)) {
@@ -26,11 +39,5 @@ public class User {
         if (!VALID_ADDRESS.contains(address)) {
             throw new IllegalArgumentException("Invalid address: " + address);
         }
-        this.name = name;
-        this.age = age;
-        this.job = job;
-        this.address = address;
     }
 }
-
-
