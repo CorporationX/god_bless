c8f0284302e124a4f0bde98b01b3e879a83947f6
Oleg_Korovin <olegk.99@mail.ru>
2025-09-09 12:44:44 +0300

    BJS2-86123/fix#3

--- src/main/java/school/faang/bjs2_86123/LibrarySystem.java
+++ src/main/java/school/faang/bjs2_86123/LibrarySystem.java
@@ -6,32 +6,32 @@ import java.util.Map;
 public class LibrarySystem {
     private Map<Book, String> books = new HashMap<>();
 
-    public LibrarySystem() {
-
-    }
-
     public void addBook(String title, String author, int year, String location) {
         books.put(new Book(title, author, year), location);
     }
 
     public void removeBook(String title, String author, int year) {
-        books.remove(new Book(title, author, year));
+        Book book = new Book(title, author, year);
+        if (books.remove(book) != null) {
+            System.out.printf("Книга %s удалена%n%n", book);
+        } else {
+            System.out.printf("Книга %s не найдена. Книга не удалена%n%n", book);
+        }
     }
 
     public void findBook(String title, String author, int year) {
-        String location = books.get(new Book(title, author, year));
+        Book book = new Book(title, author, year);
+        String location = books.get(book);
         if (location != null) {
-            System.out.println("Местонахождение: " + location);
+            System.out.printf("Книга %s найдена. Её местонахождение: %s%n%n", book, location);
         } else {
-            System.out.println("Книга не найдена");
+            System.out.printf("Книга %s не найдена%n%n", book);
         }
     }
 
     public void printAllBooks() {
         for (Map.Entry<Book, String> entry : books.entrySet()) {
-            System.out.println(entry.getKey());
-            System.out.println("Местонахождение: " + entry.getValue());
-            System.out.println();
+            System.out.printf("%s%nМестоположение: %s%n%n", entry.getKey(), entry.getValue());
         }
     }
 }
