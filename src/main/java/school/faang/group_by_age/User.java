d473955ebd5050ec97ef84444c38c50a8917ea70
Artem <kuldyrkaevam@gmail.com>
2025-09-10 02:27:01 +1000

    BJS2-85695 Внес изменения в соответствии с комментариями

--- src/main/java/school/faang/group_by_age/User.java
+++ src/main/java/school/faang/group_by_age/User.java
@@ -1,46 +1,33 @@
-package school.faang;
+package school.faang.group_by_age;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.ToString;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+@ToString
+@AllArgsConstructor
 public class User {
     private String name;
+    @Getter
     private int age;
     private String workplace;
     private String address;
 
-    public User(String name, int age, String workplace, String address) {
-        this.name = name;
-        this.age = age;
-        this.workplace = workplace;
-        this.address = address;
-    }
-
-    public int getAge() {
-        return age;
-    }
-
-    public static Map<Integer, List<User>> groupUsers(List<User> someList) {
+    public static Map<Integer, List<User>> groupUsers(List<User> userList) {
         Map<Integer, List<User>> users = new HashMap<>();
-        for (User someUser : someList) {
+        for (User someUser : userList) {
             users.computeIfAbsent(someUser.getAge(), k -> new ArrayList<>()).add(someUser);
         }
         return users;
     }
 
-    @Override
-    public String toString() {
-        return name;
-    }
-
     public static void print(Map<Integer, List<User>> myMap) {
         for (Map.Entry<Integer, List<User>> entry : myMap.entrySet()) {
-            System.out.println(entry.getKey() + " : " + entry.getValue());
+            System.out.printf("%d : %s%n", entry.getKey(), entry.getValue());
         }
     }
 }
-
-
-
