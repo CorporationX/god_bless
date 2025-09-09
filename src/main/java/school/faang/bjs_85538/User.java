5e17089fec5aad9c8f8736fe5b8bff85cd7c2b31
svxrslf <svxrslf@yandex.ru>
2025-09-09 14:46:32 +0300

    bjs2_85538 Добавил константу вместо маг числа, и поменял проверку на name.isBlank вынес ваидацию в отдельные методы

--- src/main/java/school/faang/bjs_85538/User.java
+++ src/main/java/school/faang/bjs_85538/User.java
@@ -1,14 +1,12 @@
 package school.faang.bjs_85538;
 
-import lombok.Data;
-
 import java.util.Set;
 
-@Data
 public class User {
 
     private static final Set<String> VALID_JOBS = Set.of("Google", "Uber", "Amazon");
     private static final Set<String> VALID_ADDRESSES = Set.of("London", "New York", "Amsterdam");
+    private static final int VALID_AGE = 18;
 
     private String name;
     private int age;
@@ -16,21 +14,37 @@ public class User {
     private String address;
 
     public User(String name, int age, String job, String address) {
-        if (name == null || name.trim().isEmpty()) {
+        validateName(name);
+        validateAge(age);
+        validateJob(job);
+        validateAddress(address);
+        this.name = name;
+        this.age = age;
+        this.job = job;
+        this.address = address;
+    }
+
+    private void validateName(String name) {
+        if (this.name == null || this.name.isBlank()) {
             throw new IllegalArgumentException("Name cannot be empty");
         }
-        if (age < 18) {
+    }
+
+    private void validateAge(int age) {
+        if (age < VALID_AGE) {
             throw new IllegalArgumentException("Age must be 18 and over");
         }
+    }
+
+    private void validateJob(String job) {
         if (!VALID_JOBS.contains(job)) {
             throw new IllegalArgumentException("Invalid job");
         }
+    }
+
+    private void validateAddress(String address) {
         if (!VALID_ADDRESSES.contains(address)) {
             throw new IllegalArgumentException("Invalid address");
         }
-        this.name = name;
-        this.age = age;
-        this.address = address;
-        this.job = job;
     }
 }
