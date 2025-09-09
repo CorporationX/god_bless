bdb743cc84151bdb08fae4988dd427ed5f2430a4
Mikhail EASY <mghievrie@bk.ru>
2025-09-09 20:20:36 +0500

    решение задачи BJS2-87821

---
+++ src/main/java/school/faang/bjs2_87821/Image.java
@@ -0,0 +1,11 @@
+package school.faang.bjs2_87821;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@AllArgsConstructor
+@Getter
+public class Image {
+    private String name;
+    private String description;
+}
