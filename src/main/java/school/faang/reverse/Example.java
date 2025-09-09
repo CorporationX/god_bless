8e5f640760770e72ed02e8ba1ea8f18a94e640bc
Vitaliy Mikhaylov <vitaly.mikhaylov94@yandex.ru>
2025-09-10 00:02:16 +0500

    Improved array check, fixed code style, removed @Suppress annotations

--- src/main/java/school/faang/reverse/Example.java
+++ src/main/java/school/faang/reverse/Example.java
@@ -3,13 +3,12 @@ package school.faang.reverse;
 import java.util.Arrays;
 
 public class Example {
-    public static void main(String[] args) {
-        int[] nums = {1, 2, 3, 4, 5};
-        reverse(nums);
-        printArray(nums);
-    }
-
     public static void reverse(int[] array) {
+        if (array == null || array.length <= 1) {
+            System.out.println("\nМассив пустой или равен null");
+            return;
+        }
+
         for (int i = 0; i < array.length / 2; i++) {
             int value = array[i];
             array[i] = array[array.length - 1 - i];
@@ -17,8 +16,9 @@ public class Example {
         }
     }
 
-    @SuppressWarnings({"checkstyle:EmptyLineSeparator", "checkstyle:WhitespaceAround"})
     public static void printArray(int[] array) {
-        Arrays.stream(array).mapToObj(numsArray -> numsArray + " ").forEach(System.out::print);
+        Arrays.stream(array)
+                .mapToObj(numsArray -> numsArray + " ")
+                .forEach(System.out::print);
     }
 }
---
+++ src/main/java/school/faang/reverse/Main.java
@@ -0,0 +1,12 @@
+package school.faang.reverse;
+
+public class Main {
+    public static void main(String[] args) {
+        int[] nums = {1, 2, 3, 4, 5};
+        int[] nums1 = {};
+
+        Example.reverse(nums);
+        Example.printArray(nums);
+        Example.reverse(nums1);
+    }
+}
