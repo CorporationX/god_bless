3300e94c41acead5fe5d87521317207dddb79a50
Pavel-Lukyanov <PavelLukyanov2017@yandex.ru>
2025-09-10 22:29:28 +0500

    BJS2-87842

---
+++ src/main/java/school/faang/bjs2_87842/Image.java
@@ -0,0 +1,13 @@
+package school.faang.bjs2_87842;
+
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+
+@EqualsAndHashCode
+@AllArgsConstructor
+@Getter
+public class Image {
+    private String name;
+    private String description;
+}
