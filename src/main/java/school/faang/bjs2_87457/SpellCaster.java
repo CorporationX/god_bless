b4c0a6f196ddf60cb59c47aab0cfd40b105fdc17
Pavel-Lukyanov <PavelLukyanov2017@yandex.ru>
2025-09-10 13:19:12 +0500

    Правки после код ревью

--- src/main/java/school/faang/bjs2_87457/SpellCaster.java
+++ src/main/java/school/faang/bjs2_87457/SpellCaster.java
@@ -2,6 +2,6 @@ package school.faang.bjs2_87457;
 
 public class SpellCaster {
     public void cast(String spellName, SpellAction action) {
-        System.out.println("Результат заклинания: " + action.castSpell(spellName));
+        System.out.printf("Результат заклинания: %s%n", action.castSpell(spellName));
     }
 }
