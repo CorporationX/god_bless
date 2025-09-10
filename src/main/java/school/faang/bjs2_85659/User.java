50b57589f84158f8a1cb20e7b8119c03e3691e65
K0I2T <hiro94337@gmail.com>
2025-09-10 15:49:09 +0300

    BJS2-85602 большое изменение класса User

--- src/main/java/school/faang/bjs2_85659/User.java
+++ src/main/java/school/faang/bjs2_85659/User.java
@@ -1,50 +1,37 @@
 package school.faang.bjs2_85659;
 
-import java.util.HashSet;
+import lombok.ToString;
+
 import java.util.Set;
 
+@ToString
 public class User {
     private String name;
     private int age;
     private String job;
     private String address;
 
-    private static final Set<String> VALID_JOBS = new HashSet<>(Set.of("Google", "Uber", "Amazon"));
-    private static final Set<String> VALID_ADDRESSES = new HashSet<>(Set.of("London", "New York", "Amsterdam"));
+    private static final Set<String> VALID_JOBS = Set.of("Google", "Uber", "Amazon");
+    private static final Set<String> VALID_ADDRESSES = Set.of("London", "New York", "Amsterdam");
+    private static final int ageMin = 18;
 
     public User(String name, int age, String job, String address) {
-        if (name.isBlank()) {
+        if (name == null || name.isBlank()) {
             throw new IllegalArgumentException("имя не должно быть пустым");
-        } else {
-            this.name = name;
         }
-
-        if (age < 18) {
-            //System.err.println("возраст не может быть меньше 18");
+        if (age < ageMin) {
             throw new IllegalArgumentException("возраст не может быть меньше 18");
-        } else {
-            this.age = age;
         }
-
-        if (VALID_JOBS.contains(job)) {
-            this.job = job;
-        } else {
+        if (!VALID_JOBS.contains(job)) {
             throw new IllegalArgumentException("недопустимое место работы");
         }
-
-        if (VALID_ADDRESSES.contains(address)) {
-            this.address = address;
-        } else {
+        if (!VALID_ADDRESSES.contains(address)) {
             throw new IllegalArgumentException("недопустимый город");
         }
-    }
 
-    public String toString() {
-        return "User{" +
-                "name='" + name + '\'' +
-                ", age=" + age +
-                ", job=" + job +
-                ", address=" + address +
-                '}';
+        this.name = name;
+        this.age = age;
+        this.job = job;
+        this.address = address;
     }
 }
