766926561fd18cb8d72dac58fb7276dbcdcd3185
derzhko <a.derzhko@yandex.ru>
2025-09-10 20:37:52 +0300

    initial commit

---
+++ src/main/java/school/faang/BJS2_87420/SpellAction.java
@@ -0,0 +1,6 @@
+package school.faang.BJS2_87420;
+
+@FunctionalInterface
+public interface SpellAction {
+    String action(String spell);
+}
---
+++ src/main/java/school/faang/BJS2_87420/SpellCaster.java
@@ -0,0 +1,7 @@
+package school.faang.BJS2_87420;
+
+public class SpellCaster {
+    public void cast(String spellName, SpellAction spellAction) {
+        System.out.println(spellAction.action(spellName));
+    }
+}
