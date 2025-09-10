51f186b339b3c66a2e5eacf837a0051e6a665e35
brocksvar <saskeltz@mail.ru>
2025-09-10 13:33:49 +0300

    BJS2-87584 Обрабатываем ошибки красиво

---
+++ src/main/java/school/faang/m2_1_functional_interfaces/errorhandling/ErrorHandler.java
@@ -0,0 +1,14 @@
+package school.faang.m2_1_functional_interfaces.errorhandling;
+
+import java.util.function.Supplier;
+
+public class ErrorHandler {
+
+    public static <T> T  withErrorHandling(Supplier<T> supplier, ExceptionHandler<T> exceptionHandler) {
+        try {
+            return supplier.get();
+        } catch (Exception e) {
+            return exceptionHandler.handle(e);
+        }
+    }
+}
