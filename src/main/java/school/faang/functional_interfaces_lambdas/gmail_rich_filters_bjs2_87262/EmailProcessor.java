ecb6310a90eb1f6679bd964d29f86e098738dd62
Denis Chudov <Chdnru@gmail.com>
2025-09-10 10:10:05 +0300

    fix BJS2-87262(Gmail Rich Filters). Упрощение кода метода processEmails

--- src/main/java/school/faang/functional_interfaces_lambdas/gmail_rich_filters_bjs2_87262/EmailProcessor.java
+++ src/main/java/school/faang/functional_interfaces_lambdas/gmail_rich_filters_bjs2_87262/EmailProcessor.java
@@ -1,7 +1,5 @@
 package school.faang.functional_interfaces_lambdas.gmail_rich_filters_bjs2_87262;
 
-
-import java.util.ArrayList;
 import java.util.List;
 import java.util.function.Consumer;
 import java.util.function.Function;
@@ -13,14 +11,11 @@ public class EmailProcessor {
                               Consumer<Email> emailHandler,
                               Function<Email, String> emailBodyTransformer) {
 
-        List<Email> filteredEmails = new ArrayList<>();
         for (Email email : emails) {
             if (email != null && predicateForEmails.test(email)) {
-                filteredEmails.add(email);
+                emailHandler.accept(email);
+                emailBodyTransformer.apply(email);
             }
         }
-
-        filteredEmails.forEach(emailHandler);
-        filteredEmails.forEach(emailBodyTransformer::apply);
     }
 }
