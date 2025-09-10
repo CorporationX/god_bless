315ea88a70bed495d894afcad9108fc3973c09eb
Bimcore <vzeten@gmail.com>
2025-09-10 15:50:08 +0200

    Решил задачу Хогвартс

---
+++ src/main/java/school/faang/bjs2_87396_Hogvarts/SpellCaster.java
@@ -0,0 +1,7 @@
+package school.faang.bjs2_87396_Hogvarts;
+
+public class SpellCaster {
+    public void cast(String spellName, SpellAction spellAction) {
+        System.out.println(spellAction.cast(spellName));
+    }
+}
