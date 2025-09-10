111ade5546e88a49cf25abc6abd246f778ee6108
ayazs <ayaz-22@yandex.ru>
2025-09-10 08:24:09 +0500

    subtask BJS2-87410 is done

---
+++ src/main/java/school/faang/Bjs2_87410/SpellAction.java
@@ -0,0 +1,6 @@
+package school.faang.Bjs2_87410;
+
+@FunctionalInterface
+public interface SpellAction {
+    String act(String s);
+}
---
+++ src/main/java/school/faang/Bjs2_87410/SpellCaster.java
@@ -0,0 +1,7 @@
+package school.faang.Bjs2_87410;
+
+public class SpellCaster {
+    void cast(String spell, SpellAction action) {
+        System.out.println(action.act(spell));
+    }
+}
