9909a651b90264ae100bda6265aca3e0fe604d63
Denis Chudov <Chdnru@gmail.com>
2025-09-10 10:30:34 +0300

    fix BJS2-87438((Хогвартс, Хогвартс, наш любимый Хогвартс). Привел лямбды к общепринятому виду.

--- src/main/java/school/faang/functional_interfaces_lambdas/khogvarts_khogvarts_bjs2_87438/SpellAction.java
+++ src/main/java/school/faang/functional_interfaces_lambdas/khogvarts_khogvarts_bjs2_87438/SpellAction.java
@@ -3,5 +3,5 @@ package school.faang.functional_interfaces_lambdas.khogvarts_khogvarts_bjs2_8743
 
 @FunctionalInterface
 public interface SpellAction {
-    public String castSpell(String spell);
+    String castSpell(String spell);
 }
