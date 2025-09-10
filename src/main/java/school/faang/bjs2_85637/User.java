19d1b1eac55ac3f1ddc182b629a400e1cd2e58be
Deathroels <loocyep@gmail.com>
2025-09-10 17:56:59 +0300

    fix review comments

--- src/main/java/school/faang/bjs2_85637/User.java
+++ src/main/java/school/faang/bjs2_85637/User.java
@@ -1,7 +1,9 @@
 package school.faang.bjs2_85637;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
+import lombok.ToString;
 
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -10,43 +12,27 @@ import java.util.Map;
 
 @Setter
 @Getter
-
+@AllArgsConstructor
+@ToString
 public class User {
     private String name;
     private int age;
-    private String workAddress;
+    private String address;
     private String homeAddress;
 
-    public User(String name, int age, String workAddress, String homeAddress) {
-        this.name = name;
-        this.age = age;
-        this.workAddress = workAddress;
-        this.homeAddress = homeAddress;
-    }
-
     public static Map<Integer, List<User>> groupUsers(List<User> userList) {
-
         Map<Integer, List<User>> usersByAge = new HashMap<>();
 
         for (User user : userList) {
             int age = user.getAge();
 
-            if (!usersByAge.containsKey(age)) {
-                usersByAge.put(age, new ArrayList<>());
-            }
+            usersByAge.putIfAbsent(age, new ArrayList<>());
             usersByAge.get(age).add(user);
         }
 
         for (Map.Entry<Integer, List<User>> entry : usersByAge.entrySet()) {
-            System.out.println("Age - " + entry.getKey() + ":" + entry.getValue());
+            System.out.printf("Age - %d: %s%n", entry.getKey(), entry.getValue());
         }
         return usersByAge;
     }
-
-    @Override
-    public String toString() {
-        return String.format(name);
-    }
-
-
 }
