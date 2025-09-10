f591111396a67d965e6375da119c9ef38565025f
Rukk Taras <tarrukk@gmail.com>
2025-09-10 16:50:20 +0300

    Variant 2

--- src/main/java/school/faang/BJS2_85610/User.java
+++ src/main/java/school/faang/BJS2_85610/User.java
@@ -2,17 +2,14 @@ package school.faang.BJS2_85610;
 
 import lombok.Getter;
 import lombok.Setter;
-
-import java.util.Arrays;
-import java.util.HashSet;
 import java.util.Set;
 
 @Getter
 @Setter
 public class User {
-    public static final Set<String> VALID_JOBS = new HashSet<>(Arrays.asList("Google", "Uber", "Amazon"));
-    public static final Set<String> VALID_ADDRESSES = new HashSet<>(
-            Arrays.asList("London", "New York", "Amsterdam"));
+    private static final Set<String> VALID_JOBS = Set.of("Google", "Uber", "Amazon");
+    private static final Set<String> VALID_ADDRESSES = Set.of("London", "New York", "Amsterdam");
+    private static final int ALLOWED_AGE = 18;
 
     private String name;
     private int age;
@@ -20,21 +17,21 @@ public class User {
     private String address;
 
     public User(String name, int age, String job, String address) {
-        this.name = name;
-        if (this.name.isEmpty()) {
+        if (name.isEmpty()) {
             throw new IllegalArgumentException("Имя не может быть пустым.");
         }
-        this.age = age;
-        if (this.age < 18) {
-            throw new IllegalArgumentException("Возраст не может быть меньше 18.");
+        if (age < ALLOWED_AGE) {
+            throw new IllegalArgumentException("Возраст не может быть меньше " + ALLOWED_AGE);
         }
-        this.job = job;
-        if (!VALID_JOBS.contains(this.job)) {
+        if (!VALID_JOBS.contains(job)) {
             throw new IllegalArgumentException("Место работы должно содержаться в наборе VALID_JOBS.");
         }
-        this.address = address;
-        if (!VALID_ADDRESSES.contains(this.address)) {
+        if (!VALID_ADDRESSES.contains(address)) {
             throw new IllegalArgumentException("Адрес должен содержаться в наборе VALID_ADDRESSES.");
         }
+        this.name = name;
+        this.age = age;
+        this.job = job;
+        this.address = address;
     }
 }
