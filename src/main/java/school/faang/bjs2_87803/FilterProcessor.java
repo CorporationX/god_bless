bb18f61363ff26dd435a6f57ea9f2f68dc906314
Lowdude1 <sanitaronlain@gmail.com>
2025-09-11 23:30:51 +0300

    Исправил замечания

--- src/main/java/school/faang/bjs2_87803/FilterProcessor.java
+++ src/main/java/school/faang/bjs2_87803/FilterProcessor.java
@@ -4,15 +4,10 @@ import java.util.function.Function;
 
 public class FilterProcessor {
     public Image applyFilter(Image image, Function<Image, Image> function) {
-        Image filter = function.apply(image);
-        return filter;
+        return function.apply(image);
     }
 
     public Function<Image, Image> combineFilters(Function<Image, Image> filter1, Function<Image, Image> filter2) {
-        return (Image input) -> {
-            Image firstResult = filter1.apply(input);
-            Image secondResult = filter2.apply(firstResult);
-            return secondResult;
-        };
+        return filter1.andThen(filter2);
     }
 }
