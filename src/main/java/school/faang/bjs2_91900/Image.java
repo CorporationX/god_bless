97f3d783fbca2422549dd9e79c5d59afa5a2b91e
Fadeborn <117921253+AlexeyUlov@users.noreply.github.com>
2025-09-11 22:45:12 +0300

    BJS2-91900 Фильтры Instagram (#21552)

---
+++ src/main/java/school/faang/bjs2_91900/Image.java
@@ -0,0 +1,13 @@
+package school.faang.bjs2_91900;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@AllArgsConstructor
+@Getter
+@Setter
+public class Image {
+    private String name;
+    private String description;
+}
