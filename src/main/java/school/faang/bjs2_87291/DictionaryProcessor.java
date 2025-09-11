e7151d882126ad9b8f31ae1fc1363fe3fc6bcb74
Bekhter Roman <curses93@mail.ru>
2025-09-11 20:30:22 +0300

    BJS2-87291 done

---
+++ src/main/java/school/faang/bjs2_87291/DictionaryProcessor.java
@@ -0,0 +1,11 @@
+package school.faang.bjs2_87291;
+
+import java.util.function.BiConsumer;
+
+public class DictionaryProcessor {
+    void processWord(String word, String translation, BiConsumer<String, String> consumer) {
+        if (word != null && translation != null && consumer != null) {
+            consumer.accept(word, translation);
+        }
+    }
+}
