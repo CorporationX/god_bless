931ad7b8a3ab1d689c12b0500fa648f0455cd810
EdelweissPC <shherbakov99ilya@mail.ru>
2025-09-11 23:03:48 +0300

    BJS2-87900 create logic

--- src/main/java/school/faang/bjs2_87900/Main.java
+++ src/main/java/school/faang/bjs2_87900/Main.java
@@ -1,7 +1,27 @@
 package school.faang.bjs2_87900;
 
+import java.util.List;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
+
 public class Main {
     public static void main(String[] args) {
+        System.out.println(ListOperation.findPairs(Set.of(1,2,3,4,5,6),6));
+        Map<String, String> mapCountry = new HashMap<>() {{
+                 put("Russia", "Moscow");
+                put("USA", "Washington");
+                put("Germany", "Berlin");
+        }};
+        System.out.println(ListOperation.getSortedCountry(mapCountry));
 
+        System.out.println(ListOperation.getSortedStrings(List.of("apple", "banana", "avocado", "apricot"),'a'));
+
+        System.out.println(ListOperation.convertToBinary(List.of(1,2,3,4)));
+
+        System.out.println(ListOperation
+                .getSortedByAlphabet(List.of("apple", "banana", "cherry", "date", "fig", "grape"),
+                "abcdefghijklmnopqrstuvwxyz"));
     }
 }
