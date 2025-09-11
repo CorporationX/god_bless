d7155e3a2add9a685fb906e14c110b88b49da58b
Alexey Ulov <ulov.alexey@gmail.com>
2025-09-11 14:24:08 +0300

    deleted comments

--- src/main/java/school/faang/bjs2_90777/Main.java
+++ src/main/java/school/faang/bjs2_90777/Main.java
@@ -8,19 +8,14 @@ public class Main {
     public static void main(String[] args) {
         DictionaryProcessor dictionaryProcessor = new DictionaryProcessor();
 
-        // Создание словаря
         Map<String, String> dictionary = new HashMap<>();
 
-        // Лямбда-выражение для добавления слова и его перевода в словарь
         BiConsumer<String, String> addWordToDictionary = dictionary::put;
 
-        // Добавление слов и их переводов
         dictionaryProcessor.processWord("привет", "hello", addWordToDictionary);
         dictionaryProcessor.processWord("мир", "world", addWordToDictionary);
         dictionaryProcessor.processWord("программирование", "programming", addWordToDictionary);
 
-        // Выводим словарь
         System.out.println("Словарь: " + dictionary);
-
     }
 }
