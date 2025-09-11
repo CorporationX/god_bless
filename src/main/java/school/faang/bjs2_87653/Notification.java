74afe0931cebcc4350a830abc5b6e50848d21bbc
Viks <viktor.naroznoi@gmail.com>
2025-09-11 13:52:23 +0300

    --BJS2-87653 - Meta-вселенная?

---
+++ src/main/java/school/faang/bjs2_87653/Notification.java
@@ -0,0 +1,13 @@
+package school.faang.bjs2_87653;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+public class Notification {
+    NotificationType type;
+    String message;
+}
