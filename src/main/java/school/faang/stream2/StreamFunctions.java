01ccdc57804004419993ef39738b43313247118e
Kateryna Kramarchuk <alexnjfort@gmail.com>
2025-09-11 09:33:01 +0200

    Stream2

---
+++ src/main/java/school/faang/stream2/StreamFunctions.java
@@ -0,0 +1,14 @@
+package school.faang.stream2;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Set;
+import java.util.stream.IntStream;
+
+public class StreamFunctions {
+    public List<int[]> uniquePair(Set<Integer> set, int pairSum) {
+        List<int[]> pairs = new ArrayList<>();
+        IntStream stream = IntStream.range(0, set.size());
+        
+    }
+}
