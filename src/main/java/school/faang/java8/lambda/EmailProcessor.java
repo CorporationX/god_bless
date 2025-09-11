2cfc2ce8d9c675ec0ce7f01d2e33584c60db9b34
Andre` <acmisatyuk@gmail.com>
2025-09-11 22:36:18 +0800

    BJS2-85847: used stream API to process mails: bugfix

--- src/main/java/school/faang/java8/lambda/EmailProcessor.java
+++ src/main/java/school/faang/java8/lambda/EmailProcessor.java
@@ -6,7 +6,9 @@ import java.util.function.Function;
 import java.util.function.Predicate;
 
 public class EmailProcessor {
-    public void processEmails(List<Email> emails, Predicate<Email> importantFilter, Consumer<Email> printEmail,
+    public void processEmails(List<Email> emails,
+                              Predicate<Email> importantFilter,
+                              Consumer<Email> printEmail,
                               Function<Email, String> toUpperCase) {
         emails.stream().filter(importantFilter).peek(toUpperCase::apply).forEach(printEmail);
     }
--- src/test/java/school/faang/java8/lambda/EmailProcessorTest.java
+++ src/test/java/school/faang/java8/lambda/EmailProcessorTest.java
@@ -10,39 +10,42 @@ import java.util.function.Predicate;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-
+/*
+  Список входящих писем
+  Фильтр, который пропускает только важные письма
+  Обработчик, который выводит тему письма в консоль
+  Преобразователь, который переводит текст письма в верхний регистр и сохраняет изменения
+  Обработка писем
+  Выводим обновленные письма, чтобы убедиться, что изменения сохранились
+ */
 class EmailProcessorTest {
     @Test
     void emailProcessorTest() {
         EmailProcessor emailProcessor = new EmailProcessor();
 
-// Список входящих писем
         List<Email> emails = Arrays.asList(
                 new Email("Письмо 1", "Текст письма 1", false),
                 new Email("Письмо 2", "Текст письма 2", true),
                 new Email("Спам", "Текст спама", false)
         );
 
-// Фильтр, который пропускает только важные письма
         Predicate<Email> importantFilter = Email::isImportant;
 
-// Обработчик, который выводит тему письма в консоль
         Consumer<Email> printEmail = email -> System.out.println("Обработано письмо: " + email.getSubject());
 
-// Преобразователь, который переводит текст письма в верхний регистр и сохраняет изменения
         Function<Email, String> toUpperCase = email -> {
             email.setBody(email.getBody().toUpperCase());
-            return email.getBody();  // Возвращает преобразованный текст
+            return email.getBody();
         };
         assertEquals("Текст письма 1", emails.get(0).getBody());
         assertEquals("Текст письма 2", emails.get(1).getBody());
         assertEquals("Текст спама", emails.get(2).getBody());
-// Обработка писем
+
         emailProcessor.processEmails(emails, importantFilter, printEmail, toUpperCase);
         assertEquals("Текст письма 1", emails.get(0).getBody());
         assertEquals("ТЕКСТ ПИСЬМА 2", emails.get(1).getBody());
         assertEquals("Текст спама", emails.get(2).getBody());
-// Выводим обновленные письма, чтобы убедиться, что изменения сохранились
+
         emails.forEach(email -> System.out.println("Тема: " + email.getSubject() + ", Тело письма: " + email.getBody()));
     }
 }
