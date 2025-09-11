073900acd662033997589910b569e4507c844b79
kamil_fai <faiz.kamilka@gmail.com>
2025-09-11 17:58:21 +0400

    BJS2-85620, bug fix

--- src/main/java/school/faang/bjs2_85620/Main.java
+++ src/main/java/school/faang/bjs2_85620/Main.java
@@ -6,19 +6,22 @@ import java.util.Map;
 
 public class Main {
     public static void main(String[] args) {
-        List<User> users = new ArrayList<>();
-        users.add(new User("Kamil", 22, "Address1", "Work1"));
-        users.add(new User("Tair", 12, "Address1", "School"));
-        users.add(new User("Asya", 15, "Address1", "School"));
-        users.add(new User("Elena", 48, "Address1", "Work2"));
-        users.add(new User("Albert", 48, "Address1", "Work3"));
+        List<User> users = new ArrayList<>(
+                List.of(
+                        new User("Kamil", 22, "Address1", "Work1"),
+                        new User("Tair", 12, "Address1", "School"),
+                        new User("Asya", 15, "Address1", "School"),
+                        new User("Elena", 48, "Address1", "Work2"),
+                        new User("Albert", 48, "Address1", "Work3")
+                )
+        );
 
         Map<Integer, List<User>> grouped = User.groupUsers(users);
 
         for (Integer age : grouped.keySet()) {
-            System.out.println("Age " + age + ":");
+            System.out.printf("Age %d:%n", age);
             for (User user : grouped.get(age)) {
-                System.out.println(" - " + user.getName());
+                System.out.printf(" - %s%n", user.getName());
             }
         }
     }
