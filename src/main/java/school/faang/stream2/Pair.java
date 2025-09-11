7fb24c00cbc3b3c93794f631f3b27d307b64e995
Kateryna Kramarchuk <alexnjfort@gmail.com>
2025-09-11 10:56:24 +0200

    Stream1 fix

---
+++ src/main/java/school/faang/stream2/Pair.java
@@ -0,0 +1,9 @@
+package school.faang.stream2;
+
+import lombok.AllArgsConstructor;
+
+@AllArgsConstructor
+public class Pair<F,S> {
+    private F first;
+    private S second;
+}
--- src/main/java/school/faang/stream2/StreamFunctions.java
+++ src/main/java/school/faang/stream2/StreamFunctions.java
@@ -2,13 +2,31 @@ package school.faang.stream2;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 import java.util.Set;
+import java.util.stream.Collectors;
 import java.util.stream.IntStream;
+import java.util.stream.Stream;
 
 public class StreamFunctions {
     public List<int[]> uniquePair(Set<Integer> set, int pairSum) {
         List<int[]> pairs = new ArrayList<>();
-        IntStream stream = IntStream.range(0, set.size());
-        
+        List<Integer> list = new ArrayList<>(set);
+        return IntStream.range(0, list.size())
+                .boxed()
+                .flatMap(i ->
+                        IntStream.range(i + 1, list.size())
+                                .filter(j -> list.get(i) + list.get(j) == pairSum)
+                                .mapToObj(j -> new int[]{list.get(i), list.get(j)})
+                )
+                .collect(Collectors.toList());
+    }
+
+    public List<String> sortLand(Map<String, String> map) {
+        return map.entrySet()
+                .stream()
+                .filter(entry -> getKey()
     }
 }
+
+
