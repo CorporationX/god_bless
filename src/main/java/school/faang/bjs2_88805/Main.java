dfcb1af46ced7bccecf2d445f779fe84052f25d0
Mikhail EASY <mghievrie@bk.ru>
2025-09-11 15:57:18 +0500

    реализованы все 4 пункты задачи

--- src/main/java/school/faang/bjs2_88805/Main.java
+++ src/main/java/school/faang/bjs2_88805/Main.java
@@ -1,16 +1,10 @@
 package school.faang.bjs2_88805;
 
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-import java.util.Objects;
-import java.util.function.Predicate;
+import java.util.function.Function;
 
 public class Main {
-
     public static void main(String[] args) {
         NotificationManager notificationManager = new NotificationManager();
-
 
         notificationManager.registerHandler(NotificationType.EMAIL,
                 notification -> System.out.println("Email: " + notification.getMessage())
@@ -24,12 +18,31 @@ public class Main {
                 notification -> System.out.println("Push Notification: " + notification.getMessage())
         );
 
+        Function<Notification, Notification> correct = message ->
+                new Notification(message.getType(), message.getMessage() + " Meta Inc.");
+
         Notification emailNotification = new Notification(NotificationType.EMAIL, "Ваш аккаунт активирован");
         Notification smsNotification = new Notification(NotificationType.SMS, "Ваш пароль изменен");
         Notification pushNotification = new Notification(NotificationType.PUSH, "У вас новое сообщение!");
 
-        notificationManager.sendNotification(emailNotification);
-        notificationManager.sendNotification(smsNotification);
-        notificationManager.sendNotification(pushNotification);
+        notificationManager.sendNotification(emailNotification, correct);
+        notificationManager.sendNotification(smsNotification, correct);
+        notificationManager.sendNotification(pushNotification, correct);
+
+        //Реализация фильтра
+        Notification testBanWord = new Notification(NotificationType.EMAIL, "Плохое Слово");
+        Notification testBanWordTwo = new Notification(NotificationType.EMAIL, "Плохое Слово 2");
+        Notification testBanWordThree = new Notification(NotificationType.EMAIL, "Банан");
+        Notification testBanNotificationThree = new Notification(NotificationType.EMAIL, "Норм сообщение");
+
+        notificationManager.registerPredicateFilter(testBanWord.getType(), "Плохое Слово");
+        notificationManager.registerPredicateFilter(testBanWordTwo.getType(), "Плохое Слово 2");
+        notificationManager.registerPredicateFilter(testBanWordThree.getType(), "Банан");
+        notificationManager.sendNotification(testBanWord, correct);
+        notificationManager.sendNotification(testBanWordTwo, correct);
+        notificationManager.sendNotification(testBanWordThree, correct);
+        notificationManager.sendNotification(testBanNotificationThree, correct);
+
+
     }
 }
--- src/main/java/school/faang/bjs2_88805/NotificationManager.java
+++ src/main/java/school/faang/bjs2_88805/NotificationManager.java
@@ -5,20 +5,42 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.function.Consumer;
+import java.util.function.Function;
 import java.util.function.Predicate;
 
 public class NotificationManager {
 
     private final Map<NotificationType, Consumer<Notification>> handlerMap = new HashMap<>();
+    private Map<NotificationType, Predicate<Notification>> banWordsPredicate = new HashMap<>();
+    private List<String> banWords = new ArrayList<>();
 
     protected void registerHandler(NotificationType type, Consumer<Notification> handler) {
         handlerMap.put(type, handler);
     }
 
-    protected void sendNotification(Notification notification) {
+    protected void sendNotification(Notification notification, Function<Notification, Notification> corrector) {
+        Predicate<Notification> filter = banWordsPredicate.get(notification.getType());
+        if (filter != null && filter.test(notification)) {
+            System.out.println("Вы написали плохое слово!" + notification.getMessage());
+            return;
+        }
         Consumer<Notification> notificationConsumer = handlerMap.get(notification.getType());
         if (notificationConsumer != null) {
-            notificationConsumer.accept(notification);
+            notificationConsumer.accept(corrector.apply(notification));
         }
     }
+
+    protected void registerPredicateFilter(NotificationType type, String word) {
+        Predicate<Notification> testBanWord = (notification -> {
+            for (String banWord : banWords) {
+                if (notification.getMessage().equals(banWord)) {
+                    return true;
+                }
+            }
+            return false;
+        });
+        banWords.add(word);
+        banWordsPredicate.put(type, testBanWord);
+    }
+
 }
