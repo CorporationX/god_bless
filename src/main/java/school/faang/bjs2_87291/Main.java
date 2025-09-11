26114b42e3eff1fcc138c114db6fb6984c694ce0
darkwing-d <curses93@mail.ru>
2025-09-11 21:14:58 +0300

    Update Main.java

--- src/main/java/school/faang/bjs2_87291/Main.java
+++ src/main/java/school/faang/bjs2_87291/Main.java
@@ -8,19 +8,15 @@ public class Main {
     public static void main(String[] args) {
         DictionaryProcessor dictionaryProcessor = new DictionaryProcessor();
 
-        // Создание словаря
         Map<String, String> dictionary = new HashMap<>();
 
-        // Лямбда-выражение для добавления слова и его перевода в словарь
         BiConsumer<String, String> addWordToDictionary = (word, translation) ->
                 dictionary.put(word, translation);
 
-        // Добавление слов и их переводов
         dictionaryProcessor.processWord("привет", "hello", addWordToDictionary);
         dictionaryProcessor.processWord("мир", "world", addWordToDictionary);
         dictionaryProcessor.processWord("программирование", "programming", addWordToDictionary);
 
-        // Выводим словарь
         System.out.println("Словарь: " + dictionary);
     }
 }
