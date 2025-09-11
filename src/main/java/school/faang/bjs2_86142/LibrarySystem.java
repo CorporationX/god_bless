6e333851888d5444972e4587991e4373c6b6fa3e
svxrslf <svxrslf@yandex.ru>
2025-09-11 10:20:29 +0300

    bjs2_86142 поменял System.out.print на log.info и добавил забытый модификатор в map

--- src/main/java/school/faang/bjs2_86142/LibrarySystem.java
+++ src/main/java/school/faang/bjs2_86142/LibrarySystem.java
@@ -1,41 +1,44 @@
 package school.faang.bjs2_86142;
 
+import lombok.extern.slf4j.Slf4j;
+
 import java.util.HashMap;
 import java.util.Map;
 
+@Slf4j
 public class LibrarySystem {
 
-    Map<Book, String> booksAndLocation = new HashMap<>();
+    private final Map<Book, String> booksLocations = new HashMap<>();
 
     public void addBook(String title, String author, int year, String location) {
         Book book = new Book(title, author, year);
-        booksAndLocation.put(book, location);
-        System.out.println(String.format("Добавлена новая книга: %s", book.getTitle()));
+        booksLocations.put(book, location);
+        log.info("Добавлена новая книга: {}", book.getTitle());
     }
 
     public void removeBook(String title, String author, int year) {
         Book book = new Book(title, author, year);
-        String delete = booksAndLocation.remove(book);
+        String delete = booksLocations.remove(book);
         if (delete == null) {
-            System.out.println("Такой книги не существует");
+            log.info("Такой книги не существует");
         } else {
-            System.out.println(String.format("Удалена книга: %s, она находилась в %s", book.getTitle(), delete));
+            log.info("Удалена книга: {}, она находилась в {}", book.getTitle(), delete);
         }
     }
 
     public void findBook(String title, String author, int year) {
         Book book = new Book(title, author, year);
-        String location = booksAndLocation.get(book);
+        String location = booksLocations.get(book);
         if (location == null) {
-            System.out.println(String.format("Книга  %s не найдена", book.getTitle()));
+            log.info("Книга {} не найдена", book.getTitle());
         } else {
-            System.out.println(String.format("Книга %s найдена, ее местоположение %s", book.getTitle(), location));
+            log.info("Книга {} найдена, ее местоположение {}", book.getTitle(), location);
         }
     }
 
     public void printAllBooks() {
-        for (Map.Entry<Book, String> entry : booksAndLocation.entrySet()) {
-            System.out.println(entry.getKey() + "->" + entry.getValue());
+        for (Map.Entry<Book, String> entry : booksLocations.entrySet()) {
+            log.info("{} -> {}", entry.getKey(), entry.getValue());
         }
     }
 }
