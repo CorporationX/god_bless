11d6e48e68ed26b9fbabd8fa3641e115733cff65
Monkeylust <mo9keylust@mail.ru>
2025-09-11 13:00:05 +0300

    BJS2-87403 Implementation completed

--- src/main/java/school/faang/bjs2_87403/SpellCaster.java
+++ src/main/java/school/faang/bjs2_87403/SpellCaster.java
@@ -3,6 +3,6 @@ package school.faang.bjs2_87403;
 public class SpellCaster {
     public void cast(String spellName, SpellAction spellAction) {
         String castSpell = spellAction.castSpell(spellName);
-        System.out.println(castSpell);
+        System.out.print(castSpell);
     }
 }
