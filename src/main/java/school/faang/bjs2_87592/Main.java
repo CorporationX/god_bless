f63c0e0dc07368c22b03c435b96e2763cf4da0bc
Lenovo <ddrd@gdgf>
2025-09-11 21:40:40 +0300

    saserka-BJS2-87592 - реализовал вывод плохих слов

---
+++ src/main/java/school/faang/bjs2_87592/Main.java
@@ -0,0 +1,31 @@
+package school.faang.bjs2_87592;
+
+public class Main {
+    public static void main(String[] args) {
+        NotificationManager notificationManager = new NotificationManager();
+
+// Регистрация обработчиков оповещений
+        notificationManager.registerHandler(NotificationType.EMAIL,
+                notification -> System.out.println("Email: " + notification.getMessage())
+        );
+
+        notificationManager.registerHandler(NotificationType.SMS,
+                notification -> System.out.println("SMS: " + notification.getMessage())
+        );
+
+        notificationManager.registerHandler(NotificationType.PUSH,
+                notification -> System.out.println("Push Notification: " + notification.getMessage())
+        );
+
+// Отправка оповещений
+        Notification badWordNotification = new Notification(NotificationType.EMAIL, "Вы сказали тварь");
+        Notification emailNotification = new Notification(NotificationType.EMAIL, "Ваш аккаунт активирован");
+        Notification smsNotification = new Notification(NotificationType.SMS, "Ваш пароль изменен");
+        Notification pushNotification = new Notification(NotificationType.PUSH, "У вас новое сообщение!");
+
+        notificationManager.sendNotification(badWordNotification);
+        notificationManager.sendNotification(emailNotification);
+        notificationManager.sendNotification(smsNotification);
+        notificationManager.sendNotification(pushNotification);
+    }
+}
