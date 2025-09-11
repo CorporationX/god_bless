e44eb75fbc1be6c14f227fd01261b7b157acc3bc
Mikhail EASY <mghievrie@bk.ru>
2025-09-12 01:26:58 +0500

    форматирование задачи BJS2-90900

--- src/main/java/school/faang/bjs2_90900/InventoryManager.java
+++ src/main/java/school/faang/bjs2_90900/InventoryManager.java
@@ -1,11 +1,8 @@
 package school.faang.bjs2_90900;
 
-import java.util.ArrayList;
-import java.util.List;
 import java.util.function.Consumer;
 import java.util.function.Function;
 import java.util.function.Predicate;
-import java.util.function.Supplier;
 
 public class InventoryManager {
 
