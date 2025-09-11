b094de52261f86dc8a770e50e82618fff0ba3450
darkwing-d <curses93@mail.ru>
2025-09-11 21:14:39 +0300

    Update DictionaryProcessor.java

--- src/main/java/school/faang/bjs2_87291/DictionaryProcessor.java
+++ src/main/java/school/faang/bjs2_87291/DictionaryProcessor.java
@@ -3,7 +3,7 @@ package school.faang.bjs2_87291;
 import java.util.function.BiConsumer;
 
 public class DictionaryProcessor {
-    void processWord(String word, String translation, BiConsumer<String, String> consumer) {
+    public void processWord(String word, String translation, BiConsumer<String, String> consumer) {
         if (word != null && translation != null && consumer != null) {
             consumer.accept(word, translation);
         }
