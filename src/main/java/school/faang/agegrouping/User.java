032520559b1a85a1f751fae8cd733e1f9cdbdd2c
Vitaliy Mikhaylov <vitaly.mikhaylov94@yandex.ru>
2025-09-11 14:34:17 +0500

    Added Lombok dependencies

--- src/main/java/school/faang/agegrouping/User.java
+++ src/main/java/school/faang/agegrouping/User.java
@@ -1,45 +1,22 @@
 package school.faang.agegrouping;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.ToString;
+
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+@AllArgsConstructor
+@Getter
+@ToString
 public class User {
     private String name;
     private int age;
     private String workplace;
     private String address;
-
-    public User(String name, int age, String workplace, String address) {
-        this.name = name;
-        this.age = age;
-        this.workplace = workplace;
-        this.address = address;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public int getAge() {
-        return age;
-    }
-
-    public String getWorkplace() {
-        return workplace;
-    }
-
-    public String getAddress() {
-        return address;
-    }
-
-    @Override
-    public String toString() {
-        return "User{имя: " + name + ", возраст: " + age +
-                ", место работы: " + workplace + ", город: " +
-                address + "}";
-    }
 
     public static Map<Integer, List<User>> groupUsers(List<User> users) {
         Map<Integer, List<User>> groupedUsers = new HashMap<>();
