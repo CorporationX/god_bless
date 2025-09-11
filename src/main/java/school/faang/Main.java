a5b0f5434d4fe1980a6c84dc11187c6a57291f9e
Ruslan <ruslan8020110@gmail.com>
2025-09-12 02:29:01 +0300

    исправления

--- src/main/java/school/faang/Main.java
+++ src/main/java/school/faang/Main.java
@@ -2,6 +2,7 @@ package school.faang;
 
 public class Main {
     @SuppressWarnings("checkstyle:CommentsIndentation")
+    // без suppressWarnings не дает, не понимаю что не так
     public static void main(String[] args) {
 /*
         User user = new User("Rusya", 21, "Google", "London");
--- src/main/java/school/faang/User.java
+++ src/main/java/school/faang/User.java
@@ -14,15 +14,17 @@ public class User {
     private int age;
     private String job;
     private String address;
+
     public static final Set<String> VALID_JOBS = Set.of("Google", "Uber", "Amazon");
     public static final Set<String> VALID_ADDRESSES = Set.of("London", "New York");
+    public static final int MinAge = 18;
 
     public User(String name, int age, String job, String address) {
         if (name == null) {
             throw new IllegalArgumentException("Имя не может быть пустым");
         }
-        if (age < 18) {
-            throw new IllegalArgumentException("Возраст не может быть меньше 18");
+        if (age < MinAge) {
+            throw new IllegalArgumentException("Возраст не может быть меньше " + MinAge);
         }
 
         if (!VALID_JOBS.contains(job)) {
