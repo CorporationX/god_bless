2d7223adb62e704ccd0cd656f240b3d7d43d9d03
derzhko <a.derzhko@yandex.ru>
2025-09-12 20:33:41 +0300

    add constructors

--- src/main/java/school/faang/BJS2_86058/FrequentUpdateWeatherCache.java
+++ src/main/java/school/faang/BJS2_86058/FrequentUpdateWeatherCache.java
@@ -1,6 +1,10 @@
 package school.faang.BJS2_86058;
 
 public class FrequentUpdateWeatherCache extends WeatherCacheTemplate {
+    public FrequentUpdateWeatherCache(WeatherProvider weatherProvider) {
+        super(weatherProvider);
+    }
+
     @Override
     boolean isCacheExpired(WeatherData data, long maxCacheAgeMillis) {
         return true;
--- src/main/java/school/faang/BJS2_86058/StandardWeatherCache.java
+++ src/main/java/school/faang/BJS2_86058/StandardWeatherCache.java
@@ -1,6 +1,10 @@
 package school.faang.BJS2_86058;
 
 public class StandardWeatherCache extends WeatherCacheTemplate {
+    public StandardWeatherCache(WeatherProvider weatherProvider) {
+        super(weatherProvider);
+    }
+
     @Override
     boolean isCacheExpired(WeatherData data, long maxCacheAgeMillis) {
         return System.currentTimeMillis() - data.getTimestamp() > maxCacheAgeMillis;
