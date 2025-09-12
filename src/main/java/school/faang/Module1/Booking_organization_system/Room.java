bfea1a4aed60772199ac604367a39f717e5c7a1d
7glebalyot <7glebpavlovskij7@gmail.com>
2025-09-12 19:09:47 +0200

    BJS2-85836 (#21528)
    * BJS2-85836
    
    Это было сложно, где-то я отошел от условия задания и сделал по своему, мне показалось, что так будет правильнее
    
    * Сократил 78 строку класса BookingSystem
    
    * Исправил ошибки. Еще хотел спросить, этот код который я написал, вообще считается хорошим и надежным или это все еще пока чисто учебный средненький код?

---
+++ src/main/java/school/faang/Module1/Booking_organization_system/Room.java
@@ -0,0 +1,15 @@
+package school.faang.Module1.Booking_organization_system;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.ToString;
+import java.util.Set;
+
+@Getter
+@AllArgsConstructor // Генерирует конструктор со всеми полями
+@ToString // Генерирует метод toString()
+public class Room {
+    private int roomNumber;
+    private String roomType;
+    private Set<String> roomAmenities;
+}
