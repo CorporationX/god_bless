db16db71ad0482dcb2edd7aea023882c06f638d5
Monkeylust <mo9keylust@mail.ru>
2025-09-12 17:56:07 +0300

    BJS2-88075 // Done nine requirement

--- src/main/java/school/faang/bjs2_88075/ListOperations.java
+++ src/main/java/school/faang/bjs2_88075/ListOperations.java
@@ -3,13 +3,18 @@ package school.faang.bjs2_88075;
 import java.util.Comparator;
 import java.util.List;
 import java.util.NoSuchElementException;
+import java.util.function.Predicate;
 
 public class ListOperations {
 
-    public static int sumEvenNumbers(List<Integer> numbers) {
+    private static void printingListIsEmpty(List<?> numbers) {
         if (numbers.isEmpty()) {
             throw new IllegalArgumentException("List is empty");
         }
+    }
+
+    public static int findSumEvenNumbers(List<Integer> numbers) {
+        printingListIsEmpty(numbers);
 
         return numbers.stream()
                 .filter(x -> x % 2 == 0)
@@ -17,45 +22,64 @@ public class ListOperations {
                 .sum();
     }
 
-    public static int maxElement(List<Integer> numbers) {
+    public static int findMax(List<Integer> numbers) {
         return numbers.stream()
                 .max(Integer::compareTo)
                 .orElseThrow(() -> new NoSuchElementException("List is empty"));
     }
 
-    public static double averageValueNumbers(List<Integer> numbers) {
+    public static double findAverageOfNumbers(List<Integer> numbers) {
         return numbers.stream()
                 .mapToDouble(Integer::doubleValue)
                 .average()
                 .orElseThrow(() -> new NoSuchElementException("List is empty"));
     }
 
-    public static long numberSpecialLines(List<String> strings, char sign) {
-        if (strings.isEmpty()) {
-            throw new IllegalArgumentException("List is empty");
-        }
+    public static long countStringsStartingWith(List<String> strings, char sign) {
+        printingListIsEmpty(strings);
+
         return strings.stream()
                 .filter(word -> word.charAt(0) == sign)
                 .count();
     }
 
-    public static List<String> stringFilter(List<String> strings, String substring) {
-        if (substring.isEmpty()) {
-            throw new IllegalArgumentException("Substring is empty");
-        }
+    public static List<String> filterStringsContainingSpecificSubstring(List<String> strings, String substring) {
+        printingListIsEmpty(strings);
+
         return strings.stream()
                 .filter(word -> word.contains(substring))
                 .toList();
     }
 
     public static List<String> sortingByLength(List<String> strings) {
-        if (strings.isEmpty()) {
-            throw new IllegalArgumentException("List is empty");
-        }
+        printingListIsEmpty(strings);
+
         return strings.stream()
                 .sorted(Comparator.comparingInt(String::length))
                 .toList();
     }
 
+    public static boolean satisfyCertainElementsCondition(List<Integer> numbers, Predicate<Integer> condition) {
+        printingListIsEmpty(numbers);
 
+        return numbers.stream()
+                .allMatch(condition);
+    }
+
+    public static int findMinGreaterThan(List<Integer> numbers, int number) {
+        printingListIsEmpty(numbers);
+
+        return numbers.stream()
+                .filter(n -> n > number)
+                .min(Integer::compareTo)
+                .orElseThrow(() -> new NoSuchElementException("List is empty"));
+    }
+
+    public static List<Integer> convertToLengths(List<String> strings) {
+        printingListIsEmpty(strings);
+
+        return strings.stream()
+                .map(String::length)
+                .toList();
+    }
 }
--- src/main/java/school/faang/bjs2_88075/Main.java
+++ src/main/java/school/faang/bjs2_88075/Main.java
@@ -8,11 +8,14 @@ public class Main {
         List<Integer> numbers = Arrays.asList(1, 2, 3, 4, 5, 6);
         List<String> strings = Arrays.asList("apple", "banana", "cherry", "date", "banana");
 
-        System.out.println("Sum even numbers: " + ListOperations.sumEvenNumbers(numbers));
-        System.out.println("Max number: " + ListOperations.maxElement(numbers));
-        System.out.println("Average value numbers: " + ListOperations.averageValueNumbers(numbers));
-        System.out.println("Number special lines: " + ListOperations.numberSpecialLines(strings, 'b'));
-        System.out.println("String filter: " + ListOperations.stringFilter(strings, "ba"));
-        System.out.println("Sorting by length: " + ListOperations.sortingByLength(strings));
+        System.out.println("Sum even numbers equal: " + ListOperations.findSumEvenNumbers(numbers));
+        System.out.println("Max number equal: " + ListOperations.findMax(numbers));
+        System.out.println("Average of numbers equals: " + ListOperations.findAverageOfNumbers(numbers));
+        System.out.println("The number lines starting with 'b': " + ListOperations.countStringsStartingWith(strings, 'b'));
+        System.out.println("Strings that contain 'ba' are equal: " + ListOperations.filterStringsContainingSpecificSubstring(strings, "ba"));
+        System.out.println("Sorted by string length: " + ListOperations.sortingByLength(strings));
+        System.out.println("Numbers greater than zero and less than seven: " + ListOperations.satisfyCertainElementsCondition(numbers, n -> n > 0 && n < 7));
+        System.out.println("The smallest number is greater than three: " + ListOperations.findMinGreaterThan(numbers, 3));
+        System.out.println("String lengths: " + ListOperations.convertToLengths(strings));
     }
 }
