6e53a87c45ab7e7989f8cce8e9c196c1f7c7a381
Roman Chipura <connor.ueyn@gmail.com>
2025-09-12 17:02:54 +0500

    Задача выполнена

---
+++ src/main/java/school/faang/bjs2_87871/FilterProcessor.java
@@ -0,0 +1,14 @@
+package school.faang.bjs2_87871;
+
+import java.util.function.Function;
+
+public class FilterProcessor {
+
+    Image applyFilter(Image image, Function<Image, Image> filter) {
+        return filter.apply(image);
+    }
+
+    Function<Image, Image> combineFilters(Function<Image, Image> filterA, Function<Image, Image> filterB) {
+        return filterA.andThen(filterB);
+    }
+}
