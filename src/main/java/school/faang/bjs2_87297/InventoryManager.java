614c68b3202e1a64c5ef352fe95f6199fc6f0794
Bekhter Roman <curses93@mail.ru>
2025-09-12 19:18:26 +0300

    BJS2-87297 fix

--- src/main/java/school/faang/bjs2_87297/InventoryManager.java
+++ src/main/java/school/faang/bjs2_87297/InventoryManager.java
@@ -1,19 +1,27 @@
 package school.faang.bjs2_87297;
 
+import lombok.extern.slf4j.Slf4j;
+
 import java.util.Iterator;
 import java.util.List;
 import java.util.function.Consumer;
 import java.util.function.Function;
 import java.util.function.Predicate;
 
+@Slf4j
 public class InventoryManager {
-    void addItem(Character character, Item item, Consumer<Item> callback) {
-        if (character == null || item == null) {
+    public void addItem(Character character, Item item, Consumer<Item> callback) {
+        if (character == null) {
+            log.error("Attempt to add item to null character");
+            throw new IllegalArgumentException("Character cannot be null");
+        }
+        if (item == null) {
+            log.error("Attempt to add null item to character: {}", character.getName());
             return;
         }
         if (character.getInventory() == null) {
-            System.out.println("Inventory cannot be null");
-            return;
+            log.error("Inventory is null for character: {}", character.getName());
+            throw new IllegalStateException("Inventory is not initialized for character: " + character.getName());
         }
         character.getInventory().add(item);
         if (callback != null) {
@@ -21,14 +29,17 @@ public class InventoryManager {
         }
     }
 
-    void removeItem(Character character, Predicate<Item> condition) {
+    public void removeItem(Character character, Predicate<Item> condition) {
         if (character == null) {
+            log.error("Attempt to remove item to null character");
             throw new IllegalArgumentException("Character cannot be null");
         }
         if (condition == null) {
+            log.error("Attempt to pass a null predicate");
             throw new IllegalArgumentException("Condition cannot be null");
         }
         if (character.getInventory() == null) {
+            log.error("Inventory is null for character: {}", character.getName());
             throw new IllegalStateException("Character inventory is not initialized");
         }
         Iterator<Item> iterator = character.getInventory().iterator();
@@ -40,7 +51,7 @@ public class InventoryManager {
         }
     }
 
-    void updateItem(Character character, Predicate<Item> condition, Function<Item, Item> updater) {
+    public void updateItem(Character character, Predicate<Item> condition, Function<Item, Item> updater) {
         if (character != null && condition != null && updater != null) {
             List<Item> inventory = character.getInventory();
             for (int i = 0; i < inventory.size(); i++) {
