b98adcd88a2b54ee090b82210a7cf00fa0d31e83
Kateryna Kramarchuk <alexnjfort@gmail.com>
2025-09-12 12:20:21 +0200

    stream2

--- src/main/java/school/faang/stream2/Pair.java
+++ src/main/java/school/faang/stream2/Pair.java
@@ -3,7 +3,7 @@ package school.faang.stream2;
 import lombok.AllArgsConstructor;
 
 @AllArgsConstructor
-public class Pair<F,S> {
+public class Pair<F, S> {
     private F first;
     private S second;
 
