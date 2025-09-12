cd6d76fe6bfcfa78a27657e1e5e42cf03a1dbc72
Lowdude1 <sanitaronlain@gmail.com>
2025-09-12 20:19:56 +0300

    Фильтры Instagram (#21511)
    * Фильтры Instagram
    
    * Фильтры Instagram
    
    * Исправил замечания

---
+++ src/main/java/school/faang/bjs2_87803/Image.java
@@ -0,0 +1,11 @@
+package school.faang.bjs2_87803;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class Image {
+    private String name;
+    private String description;
+}
