ba042dfb2dcb04b15b877a35c7ad83a4be3fe44b
Kateryna Kramarchuk <alexnjfort@gmail.com>
2025-09-12 08:29:28 +0200

    fix: initialize inventory

--- src/main/java/school/faang/rpg/Character.java
+++ src/main/java/school/faang/rpg/Character.java
@@ -3,16 +3,14 @@ package school.faang.rpg;
 import java.util.ArrayList;
 import java.util.List;
 
-import lombok.AllArgsConstructor;
 import lombok.Getter;
 
-@AllArgsConstructor
 @Getter
 public class Character {
     private String name;
-    private List<Item> inventory;
+    private List<Item> inventory = new ArrayList<>();
 
     public Character(String name) {
-        this(name, new ArrayList<>());
+        this.name = name;
     }
 }
