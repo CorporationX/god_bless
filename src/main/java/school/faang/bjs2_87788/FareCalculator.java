bb7cb4f33a1eb67e27a84d3a3fd82448dfbfd16c
Mikhail EASY <mghievrie@bk.ru>
2025-09-12 20:47:55 +0500

    исправленны замечания BJS2-87788

--- src/main/java/school/faang/bjs2_87788/FareCalculator.java
+++ src/main/java/school/faang/bjs2_87788/FareCalculator.java
@@ -1,11 +1,10 @@
 package school.faang.bjs2_87788;
 
-import java.util.function.BiConsumer;
 import java.util.function.BiFunction;
 
 public class FareCalculator {
 
-    protected double calculateFare(Double distance, Double time, BiFunction<Double, Double, Double> consumer) {
-        return consumer.apply(distance, time);
+    protected double calculateFare(double distance, double time, BiFunction<Double, Double, Double> fareFunction) {
+        return fareFunction.apply(distance, time);
     }
 }
--- src/main/java/school/faang/bjs2_87788/Main.java
+++ src/main/java/school/faang/bjs2_87788/Main.java
@@ -11,13 +11,13 @@ public class Main {
         BiFunction<Double, Double, Double> premiumFare = (distance, time) -> 2.0 * distance + 1.5 * time;
         BiFunction<Double, Double, Double> economyFareOne = (distance, time) -> 1.5 * distance + 0.8 * time;
 
-        double distance = 10.0;
-        double time = 15.0;
+        double distanceKm = 10.0;
+        double timeMin = 15.0;
 
-        double economyTripCost = fareCalculator.calculateFare(distance, time, economyFare);
-        double comfortTripCost = fareCalculator.calculateFare(distance, time, comfortFare);
-        double premiumTripCost = fareCalculator.calculateFare(distance, time, premiumFare);
-        double economyTest = fareCalculator.calculateFare(distance, time, economyFareOne);
+        double economyTripCost = fareCalculator.calculateFare(distanceKm, timeMin, economyFare);
+        double comfortTripCost = fareCalculator.calculateFare(distanceKm, timeMin, comfortFare);
+        double premiumTripCost = fareCalculator.calculateFare(distanceKm, timeMin, premiumFare);
+        double economyTest = fareCalculator.calculateFare(distanceKm, timeMin, economyFareOne);
 
         System.out.println("Стоимость поездки на тарифе Economy: " + economyTripCost);
         System.out.println("Стоимость поездки на тарифе Comfort: " + comfortTripCost);
