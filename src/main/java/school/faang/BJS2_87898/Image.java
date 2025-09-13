3b0355c123bc8e6972e393049bfef409bc8a4e86
Monkeylust <mo9keylust@mail.ru>
2025-09-13 22:28:09 +0300

    BJS2-87898 // done task

---
+++ src/main/java/school/faang/BJS2_87898/Image.java
@@ -0,0 +1,11 @@
+package school.faang.BJS2_87898;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@AllArgsConstructor
+@Getter
+public class Image {
+    private String name;
+    private String description;
+}
