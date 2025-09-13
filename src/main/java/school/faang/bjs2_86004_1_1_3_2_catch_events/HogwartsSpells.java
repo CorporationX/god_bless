8080832dd1ea1e6066a153e26fa98f6a91341d24
= <mtgvow@gmail.com>
2025-09-14 00:30:14 +0300

    1.1.2.3 BJSL2-85621 Complete task

--- src/main/java/school/faang/bjs2_86004_1_1_3_2_catch_events/HogwartsSpells.java
+++ src/main/java/school/faang/bjs2_86004_1_1_3_2_catch_events/HogwartsSpells.java
@@ -1,36 +1,44 @@
 package school.faang.bjs2_86004_1_1_3_2_catch_events;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 
 public class HogwartsSpells {
     private Map<Integer, SpellEvent> spellById;
-    private Map<Integer, List<SpellEvent>> spellsByType;
+    private Map<String, List<SpellEvent>> spellsByType;
 
     public void addSpellEvent(String eventType, String actionDescription) {
         SpellEvent spellEvent = new SpellEvent(SpellEvent.ids, eventType, actionDescription);
         SpellEvent.ids++;
+
         spellById.put(spellEvent.getId(), spellEvent);
+        spellsByType.putIfAbsent(eventType, new ArrayList<SpellEvent>());
+
     }
 
-    public SpellEvent getSpellById(Integer id) {
+    public SpellEvent getSpellEventById(Integer id) {
         return this.spellById.get(id);
     }
 
-    public List<SpellEvent> getSpellByType(Integer id) {
-        return this.spellsByType.get(id);
+    public List<SpellEvent> getSpellEventByType(String eventType) {
+        return this.spellsByType.get(eventType);
     }
 
     public void deleteSpellEvent(Integer id) {
-        this.spellById.remove(id);
-        this.spellsByType.remove(id);
+        SpellEvent deletingEvent = this.spellById.remove(id);
+
+        if (deletingEvent != null) {
+            List<SpellEvent> listForDeleting = this.spellsByType.get(deletingEvent.getEventType());
+            if (listForDeleting != null) {
+                listForDeleting.remove(deletingEvent);
+            }
+        }
     }
 
     public void printAllSpellEvents() {
         for (Map.Entry<Integer, SpellEvent> entry : this.spellById.entrySet()) {
-            System.out.println(entry.getValue().getId());
-            System.out.println(entry.getValue().getEventType());
-            System.out.println(entry.getValue().getAction());
+            System.out.println(entry.getValue());
         }
     }
 
