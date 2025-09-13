4752c38867022acea618657d3a2f701f5d8776a0
IyaLobach <lobach.iya@gmail.com>
2025-09-13 18:08:43 +0300

    fix: code style

--- src/main/java/school/faang/user/registration/User.java
+++ src/main/java/school/faang/user/registration/User.java
@@ -7,7 +7,7 @@ import java.util.List;
 @ToString
 public class User {
 
-    public static final List<String> VALID_JOBS  = List.of("Google", "Uber", "Amazon");
+    public static final List<String> VALID_JOBS = List.of("Google", "Uber", "Amazon");
     public static final List<String> VALID_ADDRESSES = List.of("London", "New York", "Amsterdam");
 
     private String name;
@@ -16,9 +16,12 @@ public class User {
     private String address;
 
     public User(String name, Byte age, String job, String address) {
-        if (name == null || name.isBlank() || age < 18 ||
-                !VALID_JOBS.contains(job) || !VALID_ADDRESSES.contains(address))
+        if (name == null
+                || name.isBlank() || age < 18
+                || !VALID_JOBS.contains(job)
+                || !VALID_ADDRESSES.contains(address)) {
             throw new IllegalArgumentException("Valid error");
+        }
         this.name = name;
         this.age = age;
         this.job = job;
