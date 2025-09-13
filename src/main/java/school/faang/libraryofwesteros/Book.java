0ded9f9fa552dc0081eddbdfb01416e24e879c56
Vitaliy Mikhaylov <vitaly.mikhaylov94@yandex.ru>
2025-09-13 21:16:34 +0500

    Refactored the task

--- src/main/java/school/faang/libraryofwesteros/Book.java
+++ src/main/java/school/faang/libraryofwesteros/Book.java
@@ -1,43 +1,16 @@
 package school.faang.libraryofwesteros;
 
-import java.util.Objects;
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.ToString;
 
+@AllArgsConstructor
+@ToString
+@EqualsAndHashCode
+@Getter
 public class Book {
     private String tittle;
     private String author;
     private int year;
-
-    public Book(String tittle, String author, int year) {
-        this.tittle = tittle;
-        this.author = author;
-        this.year = year;
-    }
-
-    public String getTittle() {
-        return tittle;
-    }
-
-    public String getAuthor() {
-        return author;
-    }
-
-    public int getYear() {
-        return year;
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (o == null || getClass() != o.getClass()) {
-            return false;
-        }
-        Book book = (Book) o;
-        return year == book.year
-                && Objects.equals(tittle, book.tittle)
-                && Objects.equals(author, book.author);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(tittle, author, year);
-    }
 }
--- src/main/java/school/faang/libraryofwesteros/Main.java
+++ src/main/java/school/faang/libraryofwesteros/Main.java
@@ -1,17 +1,24 @@
 package school.faang.libraryofwesteros;
 
-import java.util.HashMap;
-import java.util.Map;
-
 public class Main {
-    private static Map<Book, String> library = new HashMap<>();
-
     public static void main(String[] args) {
-        library.put(new Book("Head First Java", "B.Bates & K.Sierra",
-                2005), "Стеллаж К, полка №4");
-        library.put(new Book("Harry Potter and the Chamber of Secrets", "J.K.Rowling",
-                2014), "Стеллаж Ф, полка №2");
-        library.put(new Book("Harry Potter and the Methods of Rational Thinking", "E. Yudkovsky",
-                2018), "Стеллаж Х, полка №4");
+        LibrarySystem librarySystem = new LibrarySystem();
+
+        Book book1 = new Book("Head First Java", "B.Bates & K.Sierra",
+                2005);
+        Book book2 = new Book("Harry Potter and the Chamber of Secrets", "J.K.Rowling",
+                2014);
+        Book book3 = new Book("Harry Potter and the Methods of Rational Thinking", "E. Yudkovsky",
+                2018);
+
+        librarySystem.addBook(book1, "Стеллаж К, полка №4");
+        librarySystem.addBook(book2, "Стеллаж Ф, полка №2");
+        librarySystem.addBook(book3, "Стеллаж Х, полка №4");
+
+        librarySystem.findBook(book2);
+
+        librarySystem.removeBook(book1);
+
+        librarySystem.printAllBooks();
     }
 }
