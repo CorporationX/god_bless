dc06d769f96c00bd7bce2ffb09fe8ef67d20a317
Oleg_Korovin <olegk.99@mail.ru>
2025-09-13 16:20:55 +0300

    BJS2-88210/fix#1

--- src/main/java/school/faang/bjs2_88210/Main.java
+++ src/main/java/school/faang/bjs2_88210/Main.java
@@ -1,21 +1,25 @@
 package school.faang.bjs2_88210;
 
-import org.w3c.dom.ls.LSOutput;
-
 import java.util.Comparator;
 import java.util.List;
 import java.util.Optional;
+import java.util.OptionalDouble;
 import java.util.function.Predicate;
 
 public class Main {
     public static void main(String[] args) {
         List<Integer> numbers = List.of(5, 49, 98, 37, 81, 9, 41, 44, 54, 88);
         System.out.printf("1. Сумма четных чисел в списке: %d%n", sumEvenNumbers(numbers));
-        System.out.printf("2. Максимальное число в списке: %d%n", getMaxNumber(numbers).orElse(-1));
-        System.out.printf("3. Среднее арифметическое списка: %.3f%n", evaluateArithmeticMean(numbers));
+        getMaxNumber(numbers).ifPresentOrElse(
+                obj -> System.out.printf("2. Максимальное число в списке: %d%n", obj),
+                () -> System.out.println("Список пуст\n"));
+        evaluateArithmeticMean(numbers).ifPresentOrElse(
+                obj -> System.out.printf("3. Среднее арифметическое списка: %.3f%n", obj),
+                () -> System.out.println("Список пуст\n"));
         System.out.printf("4. Список соответствует условию: %b%n", isMatch(numbers, number -> number > 20));
-        System.out.printf("5. Минимальное число после заданного: %d%n",
-                getMinNumberAfterBound(numbers, 50).orElse(-1));
+        getNumberGreaterThan(numbers, 50).ifPresentOrElse(
+                obj -> System.out.printf("5. Минимальное число после заданного: %d%n", obj),
+                () -> System.out.println("Список пуст\n"));
 
         List<String> strings = List.of(
                 "Метод должен принимать",
@@ -30,19 +34,20 @@ public class Main {
                 "Стримы позволяют написать более компактный и выразительный код"
         );
         System.out.printf("6. Количество строк, начинающихся с заданного символа: %d%n",
-                getAmountOfStringsStartingWithSymbol(strings, 'в'));
+                countStringsStartingWith(strings, 'в'));
         System.out.println("7. Строки, включающие с заданную строку:");
-        getStringsWithSubstring(strings, "список").forEach(System.out::println);
+        getStringsContainsSubstring(strings, "список").forEach(System.out::println);
         System.out.println("8. Список, отсортированный по длине строки:");
         sortStringsByLength(strings).forEach(System.out::println);
         System.out.println("9. Список, переведенный в длину строки:");
-        convertStringToNumbers(strings).forEach(System.out::println);
+        convertToLength(strings).forEach(System.out::println);
     }
 
     public static int sumEvenNumbers(List<Integer> numbers) {
         return numbers.stream()
                 .filter(number -> number % 2 == 0)
-                .reduce(0, Integer::sum);
+                .mapToInt(Integer::intValue)
+                .sum();
     }
 
     public static Optional<Integer> getMaxNumber(List<Integer> numbers) {
@@ -50,20 +55,19 @@ public class Main {
                 .max(Comparator.naturalOrder());
     }
 
-    public static double evaluateArithmeticMean(List<Integer> numbers) {
-        int sum = numbers.stream()
-                .reduce(0, Integer::sum);
-        long count = numbers.size();
-        return sum * 1.0 / count;
+    public static OptionalDouble evaluateArithmeticMean(List<Integer> numbers) {
+        return numbers.stream()
+                .mapToInt(Integer::intValue)
+                .average();
     }
 
-    public static long getAmountOfStringsStartingWithSymbol(List<String> strings, char symbol) {
+    public static long countStringsStartingWith(List<String> strings, char symbol) {
         return strings.stream()
                 .filter(string -> string.startsWith(String.valueOf(symbol)))
                 .count();
     }
 
-    public static List<String> getStringsWithSubstring(List<String> strings, String substring) {
+    public static List<String> getStringsContainsSubstring(List<String> strings, String substring) {
         return strings.stream()
                 .filter(string -> string.contains(substring))
                 .toList();
@@ -80,13 +84,13 @@ public class Main {
                 .allMatch(predicate);
     }
 
-    public static Optional<Integer> getMinNumberAfterBound(List<Integer> numbers, int bound) {
+    public static Optional<Integer> getNumberGreaterThan(List<Integer> numbers, int bound) {
         return numbers.stream()
                 .filter(number -> number > bound)
                 .min(Comparator.naturalOrder());
     }
 
-    public static List<Integer> convertStringToNumbers(List<String> strings) {
+    public static List<Integer> convertToLength(List<String> strings) {
         return strings.stream()
                 .map(String::length)
                 .toList();
