1480bd6409e92f2ced323f531d1ea5d39f7d080f
IyaLobach <lobach.iya@gmail.com>
2025-09-13 23:40:35 +0300

    fix: code review

--- src/main/java/school/faang/cache/StudentDatabase.java
+++ src/main/java/school/faang/cache/StudentDatabase.java
@@ -2,6 +2,7 @@ package school.faang.cache;
 
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
@@ -10,65 +11,55 @@ import java.util.stream.Collectors;
 public class StudentDatabase {
 
     private final Map<Student, Map<Subject, Integer>> studentSubjects = new HashMap<>();
-    private final Map<Subject, List<Student>> subjectStudents = new HashMap<>();
-
-    private final Map<String, Student> studentLocalDatabase = new HashMap<>();
-    private final Map<String, Subject> subjectLocalDatabase = new HashMap<>();
-
+    private final Map<Subject, Set<Student>> subjectStudents = new HashMap<>();
 
     public void addStudentWithGrade(String studentName, List<GradedItem> gradedItems) {
-        Student currentStudent = studentLocalDatabase.computeIfAbsent(studentName,
-                name -> new Student(studentName));
+        Student currentStudent = new Student(studentName);
         Map<Subject, Integer> currentStudentGrade = studentSubjects.get(currentStudent) == null
                 ? new HashMap<>() : studentSubjects.get(currentStudent);
         for (GradedItem gradedItem : gradedItems) {
-            Subject currentSubject = subjectLocalDatabase.computeIfAbsent(gradedItem.subjectName(),
-                    name -> new Subject(gradedItem.subjectName()));
+            Subject currentSubject = new Subject(gradedItem.subjectName());
             currentStudentGrade.put(currentSubject, gradedItem.mark());
-            List<Student> students = subjectStudents.computeIfAbsent(currentSubject, sub -> new ArrayList<>());
-            if (!students.contains(currentStudent)) {
-                students.add(currentStudent);
-            }
+            subjectStudents.computeIfAbsent(currentSubject, sub -> new HashSet<>())
+                    .add(currentStudent);
         }
         studentSubjects.put(currentStudent, currentStudentGrade);
     }
 
     public void addSubjectWithStudents(String subjectName, List<String> studentNames) {
-        Subject currentSubject = subjectLocalDatabase.computeIfAbsent(subjectName,
-                name -> new Subject(subjectName));
-        List<Student> studentBySubject = subjectStudents.get(currentSubject) == null
-                ? new ArrayList<>() : subjectStudents.get(currentSubject);
+        Subject currentSubject = new Subject(subjectName);
+        Set<Student> studentBySubject = subjectStudents.get(currentSubject) == null
+                ? new HashSet<>() : subjectStudents.get(currentSubject);
         for (String studentName : studentNames) {
-            Student currentStudent = studentLocalDatabase.computeIfAbsent(studentName,
-                    name -> new Student(studentName));
-            if (!studentBySubject.contains(currentStudent)) {
-                subjectStudents.computeIfAbsent(currentSubject,
-                        sub -> new ArrayList<>()).add(currentStudent);
-            }
+            Student currentStudent = new Student(studentName);
+            studentBySubject.add(currentStudent);
             studentSubjects.computeIfAbsent(currentStudent,
                     student -> new HashMap<>()).putIfAbsent(currentSubject, null);
         }
+        subjectStudents.put(currentSubject, studentBySubject);
     }
 
     public void deleteStudentByName(String studentName) {
-        Student studentForDeleting = studentLocalDatabase.get(studentName);
-        if (studentForDeleting != null) {
+        Student studentForDeleting = new Student(studentName);
+        if (studentSubjects.containsKey(studentForDeleting)) {
             Set<Subject> subjectByStudent = studentSubjects.get(studentForDeleting).keySet();
             for (Subject subject : subjectByStudent) {
                 subjectStudents.get(subject).remove(studentForDeleting);
             }
+            studentSubjects.remove(studentForDeleting);
         }
-        studentSubjects.remove(studentForDeleting);
     }
 
     public void deleteStudentFromSubject(String studentName, String subjectName) {
-        Student studentForDeleting = studentLocalDatabase.get(studentName);
-        if (studentForDeleting != null) {
-            Subject currentSubject = subjectLocalDatabase.computeIfAbsent(subjectName,
-                    name -> new Subject(subjectName));
+        Student studentForDeleting = new Student(studentName);
+        Subject currentSubject = new Subject(subjectName);
+        if (studentSubjects.containsKey(studentForDeleting)) {
             studentSubjects.get(studentForDeleting).remove(currentSubject);
+        }
+        if (subjectStudents.containsKey(currentSubject)) {
             subjectStudents.get(currentSubject).remove(studentForDeleting);
         }
+
     }
 
 
@@ -86,7 +77,7 @@ public class StudentDatabase {
 
     public void printSubjectWithStudents() {
         StringBuilder resultList = new StringBuilder();
-        for (Map.Entry<Subject, List<Student>> entry : subjectStudents.entrySet()) {
+        for (Map.Entry<Subject, Set<Student>> entry : subjectStudents.entrySet()) {
             resultList.append(entry.getKey().getName()).append(": ").append(entry.getValue().stream()
                     .map(Student::getName)
                     .collect(Collectors.joining(", ")));
