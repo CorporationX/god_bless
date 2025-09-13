3a0fe7324a4d731fe9325ccb98285167ec5b3190
Vitaliy Mikhaylov <vitaly.mikhaylov94@yandex.ru>
2025-09-13 19:59:44 +0500

    Moved id generation to SpellEvent class and added null checks for eventType and action

--- src/main/java/school/faang/handlers/HogwartsSpells.java
+++ src/main/java/school/faang/handlers/HogwartsSpells.java
@@ -8,15 +8,20 @@ import java.util.Map;
 public class HogwartsSpells {
     private Map<Integer, SpellEvent> spellById = new HashMap<>();
     private Map<String, List<SpellEvent>> spellsByType = new HashMap<>();
-    private int currentId = 1;
 
     public void addSpellEvent(String eventType, String actionDescription) {
-        SpellEvent spellEvent = new SpellEvent(currentId++, eventType, actionDescription);
+        if (eventType == null || actionDescription == null) {
+            throw new IllegalArgumentException("eventType и actionDescription не могут быть null");
+        }
+
+        SpellEvent spellEvent = new SpellEvent(eventType, actionDescription);
 
         spellById.put(spellEvent.getId(), spellEvent);
 
         spellsByType.putIfAbsent(eventType, new ArrayList<>());
-        spellsByType.get(eventType).add(spellEvent);
+        spellsByType
+                .get(eventType)
+                .add(spellEvent);
     }
 
     public SpellEvent getSpellEventById(int id) {
--- src/main/java/school/faang/handlers/SpellEvent.java
+++ src/main/java/school/faang/handlers/SpellEvent.java
@@ -8,7 +8,19 @@ import lombok.ToString;
 @Getter
 @ToString
 public class SpellEvent {
+    private static int currentId = 1;
+
     private int id;
     private String eventType;
     private String action;
+
+    public SpellEvent(String eventType, String action) {
+        if (eventType == null || action == null) {
+            throw new IllegalArgumentException("eventType и action не могут быть null");
+        }
+
+        this.id = currentId++;
+        this.eventType = eventType;
+        this.action = action;
+    }
 }
