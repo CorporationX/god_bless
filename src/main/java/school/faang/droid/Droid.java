32afa4348d888253920e18297fb12b768ff658c0
Kateryna Kramarchuk <alexnjfort@gmail.com>
2025-09-14 18:55:35 +0200

    add default interface

--- src/main/java/school/faang/droid/Droid.java
+++ src/main/java/school/faang/droid/Droid.java
@@ -7,29 +7,23 @@ public class Droid {
 
     private String name;
 
-    public String encryptMessage(String message, int key) {
-        DroidMessageEncryptor encryptor = (m, k) -> {
-            StringBuilder encrypted = new StringBuilder();
-            for (int i = 0; i < message.length(); i++) {
-                encrypted.append((char) (message.charAt(i) + key));
-            }
-            return encrypted.toString();
-        };
-        return encryptor.massageEncrypt(message, key);
-    }
+    private final DroidMessageEncryptor encryptor = (message, key) -> {
+        StringBuilder encrypted = new StringBuilder();
+        for (int i = 0; i < message.length(); i++) {
+            encrypted.append((char) (message.charAt(i) + key));
+        }
+        return encrypted.toString();
+    };
 
-    public String decryptMessage(String message, int key) {
-        return encryptMessage(message, -key);
-    }
 
     public void sendMessage(Droid receiver, String message, int key) {
-        String encrypted = encryptMessage(message, key);
+        String encrypted = encryptor.massageEncrypt(message, key);
         System.out.println(name + " sent an encrypted message: " + encrypted);
         receiver.receiveMessage(encrypted, key, this);
     }
 
     public void receiveMessage(String encryptedMessage, int key, Droid sender) {
-        String decrypted = decryptMessage(encryptedMessage, key);
+        String decrypted = encryptor.massageDecrypt(encryptedMessage, key);
         System.out.println(name + " received a decrypted message: " + decrypted);
     }
 }
--- src/main/java/school/faang/droid/DroidMessageEncryptor.java
+++ src/main/java/school/faang/droid/DroidMessageEncryptor.java
@@ -2,4 +2,8 @@ package school.faang.droid;
 
 public interface DroidMessageEncryptor {
     String massageEncrypt(String message, int key);
+
+    default String massageDecrypt(String message, int key) {
+        return massageEncrypt(message, -key);
+    }
 }
