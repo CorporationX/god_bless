344ab43f3cfb5440b2cd5894502b5783d044690e
Mikhail EASY <mghievrie@bk.ru>
2025-09-14 14:26:39 +0500

    декрипторы-крипторы сделаны BJS2-88888

---
+++ src/main/java/school/faang/bjs2_88888/Droid.java
@@ -0,0 +1,56 @@
+package school.faang.bjs2_88888;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@AllArgsConstructor
+public class Droid {
+    @Getter
+    private String name;
+
+    //Поставить потом private
+    public String encryptMessage(String message, int key) {
+        DroidMessageEncryptor cryptMessage = ((words, keys) -> {
+            String crypt = "";
+            for (int i = 0; i < words.length(); i++) {
+                if (Character.isLetter(words.charAt(i))) {
+                    char base = Character.isLowerCase(words.charAt(i)) ? 'a' : 'A';
+                    char encryptChar = (char) ((words.charAt(i) - base + keys) % 26 + base);
+                    crypt += encryptChar;
+                } else {
+                    crypt += words.charAt(i);
+                }
+            }
+            return crypt;
+        });
+        return cryptMessage.encrypt(message, key);
+    }
+
+    public String decryptMessage(String message, int key) {
+        DroidMessageEncryptor decrypt = ((words, keys) -> {
+            String decryptMessage = "";
+            for (int i = 0; i < words.length(); i++) {
+                if (Character.isLetter(words.charAt(i))) {
+                    char base = Character.isLowerCase(words.charAt(i)) ? 'a' : 'A';
+                    char encryptChar = (char) ((words.charAt(i) - base - keys + 26) % 26 + base);
+                    decryptMessage += encryptChar;
+                } else {
+                    decryptMessage += words.charAt(i);
+                }
+            }
+            return decryptMessage;
+        });
+        return decrypt.encrypt(message, key);
+    }
+
+    public void sendMessage(Droid name, String message, int key) {
+        String encryptMessage = encryptMessage(message, key);
+        System.out.printf("%s - Зашифровал сообщение : %s%n", this.name, encryptMessage);
+        receiveMessage(name, encryptMessage, key);
+    }
+
+    public void receiveMessage(Droid name, String message, int key) {
+        String decrypt = decryptMessage(message, key);
+        System.out.printf("%s - Расшифровал сообщение : %s%n", name.getName(), decrypt);
+    }
+}
---
+++ src/main/java/school/faang/bjs2_88888/DroidMessageEncryptor.java
@@ -0,0 +1,6 @@
+package school.faang.bjs2_88888;
+
+@FunctionalInterface
+public interface DroidMessageEncryptor {
+    String encrypt(String word, int key);
+}
