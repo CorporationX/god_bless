cf51462898c6659536c7159aa53e3056c4d6cb45
Lowdude1 <sanitaronlain@gmail.com>
2025-09-14 18:18:05 +0300

    Дроиды

--- src/main/java/school/faang/bjs2_91020/Main.java
+++ src/main/java/school/faang/bjs2_91020/Main.java
@@ -2,9 +2,7 @@ package school.faang.bjs2_91020;
 
 public class Main {
     public static void main(String[] args) {
-
         Droid droid1 = new Droid();
-        Droid droid2 = new Droid();
 
         String message = "Hello, World!";
         int key = 3;
@@ -18,6 +16,7 @@ public class Main {
         System.out.println("Decrypted: " + decrypted);
 
         System.out.println("\nSending message between droids:");
+        Droid droid2 = new Droid();
         droid1.sendMessage("Secret Message", 5, droid2);
     }
 }
