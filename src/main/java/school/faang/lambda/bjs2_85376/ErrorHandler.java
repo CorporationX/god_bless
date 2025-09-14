6422e24d1f74bb381a1dd93666c35a74834cacaf
teymuruae <baratov.teymur@yandex.ru>
2025-09-14 16:39:16 +0300

    BJS2-85376 Обрабатываем ошибки красиво

---
+++ src/main/java/school/faang/lambda/bjs2_85376/ErrorHandler.java
@@ -0,0 +1,16 @@
+package school.faang.lambda.bjs2_85376;
+
+import java.util.function.Supplier;
+
+public class ErrorHandler {
+
+    public static <T> T withErrorHandling(Supplier<T> supplier, Handler<T> handler) {
+        T result = null;
+        try {
+            result = supplier.get();
+        } catch (Exception e) {
+            result = handler.handle(e);
+        }
+        return result;
+    }
+}
---
+++ src/main/java/school/faang/lambda/bjs2_85376/Handler.java
@@ -0,0 +1,5 @@
+package school.faang.lambda.bjs2_85376;
+
+public interface Handler<T> {
+    T handle(Throwable throwable);
+}
