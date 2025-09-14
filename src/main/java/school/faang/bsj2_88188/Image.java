89efab1c477a307fdf4f405388c08c110568cd64
Kuanysh <skn_88@mail.ru>
2025-09-14 15:44:34 +0500

    bsj2-87500 fix PR comments

--- src/main/java/school/faang/bsj2_88188/Image.java
+++ src/main/java/school/faang/bsj2_88188/Image.java
@@ -1,16 +1,10 @@
 package school.faang.bsj2_88188;
 
 import lombok.AllArgsConstructor;
-import lombok.EqualsAndHashCode;
-import lombok.Getter;
-import lombok.Setter;
-import lombok.ToString;
+import lombok.Data;
 
 @AllArgsConstructor
-@Getter
-@Setter
-@EqualsAndHashCode
-@ToString
+@Data
 public class Image {
     private String name;
     private String descripton;
