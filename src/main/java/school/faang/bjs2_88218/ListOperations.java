53ac5bf02ba54bd05ecf139c93b07273fea5b6c1
ayazs <ayaz-22@yandex.ru>
2025-09-15 00:36:55 +0500

    is fixed

--- src/main/java/school/faang/bjs2_88218/ListOperations.java
+++ src/main/java/school/faang/bjs2_88218/ListOperations.java
@@ -8,41 +8,62 @@ import java.util.function.Predicate;
 public class ListOperations {
 
     public static int sumOfEvenNumbers(List<Integer> numbers) {
-        return numbers.stream().filter(n -> n % 2 == 0).mapToInt(Integer::intValue).sum();
+        return numbers.stream()
+                .filter(n -> n % 2 == 0)
+                .mapToInt(Integer::intValue)
+                .sum();
     }
 
     public static int findMax(List<Integer> numbers) {
-        return numbers.stream().max(Integer::compareTo).orElseThrow(NoSuchElementException::new);
+        return numbers.stream()
+                .max(Integer::compareTo)
+                .orElseThrow(() -> new NoSuchElementException("Максимальное число не найдено"));
     }
 
     public static double findAverage(List<Integer> numbers) {
-        return numbers.stream().mapToInt(Integer::intValue).average().orElse(0.0);
+        return numbers.stream()
+                .mapToInt(Integer::intValue)
+                .average()
+                .orElseThrow(() -> new NoSuchElementException("Среднее не найдено"));
     }
 
-    public static long countStringsStartingWith(List<String> strings, char letter) {
-        return strings.stream().filter(s -> !s.isEmpty() && s.charAt(0) == letter).count();
+    public static long countStringsStartingWith(List<String> strings,
+                                                char letter) {
+        return strings.stream()
+                .filter(s -> !s.isEmpty() && s.charAt(0) == letter)
+                .count();
     }
 
     public static List<String> filterStringsContainingSubstring(List<String> strings,
-                                                          String particle) {
-        return strings.stream().filter(s -> s.contains(particle)).toList();
+                                                                String particle) {
+        return strings.stream()
+                .filter(s -> s.contains(particle))
+                .toList();
     }
 
     public static List<String> sortByLength(List<String> strings) {
-        return strings.stream().sorted(Comparator.comparing(String::length)).toList();
+        return strings.stream()
+                .sorted(Comparator.comparing(String::length))
+                .toList();
     }
 
-    public static boolean allMatchCondition(List<Integer> numbers, Predicate<Integer> condition) {
-        return numbers.stream().allMatch(condition);
+    public static boolean allMatchCondition(List<Integer> numbers,
+                                            Predicate<Integer> condition) {
+        return numbers.stream()
+                .allMatch(condition);
     }
 
     public static int findMinGreaterThan(List<Integer> numbers, int numb) {
-        return numbers.stream().filter(n -> n > numb).findFirst()
+        return numbers.stream()
+                .filter(n -> n > numb)
+                .findFirst()
                 .orElseThrow(() -> new NoSuchElementException("Числа в коллекции не найдены"));
     }
 
 
     public static List<Integer> convertToLengths(List<String> strings) {
-        return strings.stream().map(String::length).toList();
+        return strings.stream()
+                .map(String::length)
+                .toList();
     }
 }
