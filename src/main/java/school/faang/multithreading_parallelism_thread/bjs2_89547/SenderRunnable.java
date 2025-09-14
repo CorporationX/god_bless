af9c04facbc3e384655e3d635e8fa2dae4ae629d
AndreySN7 <andrey__n@mail.ru>
2025-09-14 22:19:52 +0300

    Решил задачу: Вы работаете в Microsoft!

---
+++ src/main/java/school/faang/multithreading_parallelism_thread/bjs2_89547/SenderRunnable.java
@@ -0,0 +1,16 @@
+package school.faang.multithreading_parallelism_thread.bjs2_89547;
+
+import lombok.AllArgsConstructor;
+
+@AllArgsConstructor
+public class SenderRunnable implements Runnable {
+    private final int startIndex;
+    private final int endIndex;
+
+    @Override
+    public void run() {
+        for (int i = startIndex; i <= endIndex; i++) {
+            System.out.println("Письмо отправлено: " + i);
+        }
+    }
+}
