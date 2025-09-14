21ca5392e286eebd8621e0edbdadef5880e7416c
Rukk Taras <tarrukk@gmail.com>
2025-09-14 17:18:16 +0300

    Variant 1

--- src/main/java/school/faang/bjs2_88033/Decision.java
+++ src/main/java/school/faang/bjs2_88033/Decision.java
@@ -1,21 +1,48 @@
 package school.faang.bjs2_88033;
 
 import java.util.Arrays;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
+import java.util.Map;
 import java.util.Set;
-import java.util.stream.IntStream;
+import java.util.stream.Collectors;
 
 public class Decision {
-    public static List<List<Integer>> findCouplesBySum(Set<Integer> numbers, int requiredSum) {
+    public static Set<List<Integer>> findNumbersBySum(Set<Integer> numbers, int requiredSum) {
+        return numbers.stream()
+                .filter(num -> num != requiredSum - num && numbers.contains(requiredSum - num))
+                .map(num -> Arrays.asList(num, requiredSum - num))
+                .peek(Collections::sort)
+                .collect(Collectors.toSet());
+    }
 
+    public static List<String> sortCapitalsByCountries(Map<String, String> countriesAndCapitals) {
+        return countriesAndCapitals.entrySet()
+                .stream()
+                .sorted(Map.Entry.comparingByKey())
+                .map(Map.Entry::getValue)
+                .collect(Collectors.toList());
+    }
 
-        IntStream intStream = Arrays.stream(numbers);
+    public static List<String> sortStringsStartWithSymbol(List<String> strings, char symbol) {
+        return strings.stream()
+                .filter(str -> str.startsWith(String.valueOf(symbol)))
+                .sorted(Comparator.comparing(String::length))
+                .collect(Collectors.toList());
+    }
 
-        numbers.stream().range(0,  numbers.length)
-                .forEach(i -> IntStream.range(0,  input.length)
-                        .filter(j -> i != j && input[i] + input[j] == sum)
-                        .forEach(j -> addPairs(input[i], input[j]))
-                );
-        return numbers.stream().reduce((a, b) -> requiredSum);
+    public static List<String> numbersToBinaryFormat(List<Integer> numbers) {
+        return numbers.stream()
+                .map(Integer::toBinaryString)
+                .collect(Collectors.toList());
+    }
+
+    public static List<String> selectStringsWithDefiniteLetters(List<String> strings, String letters) {
+        String lettersInRegular = "[" + letters + "]+";
+        return strings.stream()
+                .filter(string -> string.matches(lettersInRegular))
+                .sorted(Comparator.comparing(String::length))
+                .collect(Collectors.toList());
     }
 }
