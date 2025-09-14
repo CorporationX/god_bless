7be0d2b5f6b9c67716ffae7edf0f95bc4811681f
Kulsinbai <mcc.kulsinbai@gmail.com>
2025-09-15 02:25:28 +0500

    refactor(BJS2-86197): доделал задачу "Библиотека Вестеросса"

--- src/main/java/school/faang/bjs2_86197/LibrarySystem.java
+++ src/main/java/school/faang/bjs2_86197/LibrarySystem.java
@@ -4,31 +4,27 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class LibrarySystem {
-    Map<Book, String> bookStringMap = new HashMap<>();
+    public final Map<Book, String> bookLocations = new HashMap<>();
 
-    void addBook(String title, String author, int year, String location) {
-        bookStringMap.put(new Book(title, author, year), location);
+    public void addBook(String title, String author, int year, String location) {
+        bookLocations.put(new Book(title, author, year), location);
     }
 
-    void removeBook(String title, String author, int year) {
-        bookStringMap.remove(new Book(title, author, year));
+    public void removeBook(String title, String author, int year) {
+        bookLocations.remove(new Book(title, author, year));
     }
 
-    void findBook(String title, String author, int year) {
-        String location = bookStringMap.get(new Book(title, author, year));
-        if (location == null) {
-            System.out.println("Книга не найдена");
-        } else {
-            System.out.println("Книга находится в " + location);
-        }
+    public void findBook(String title, String author, int year) {
+        String location = bookLocations.getOrDefault(new Book(title, author, year), "Книга не найдена");
+        System.out.println(location);
     }
 
-    void printAllBooks() {
-        if (bookStringMap.isEmpty()) {
+    public void printAllBooks() {
+        if (bookLocations.isEmpty()) {
             System.out.println("Библиотека пуста");
         }
 
-        for (Map.Entry<Book, String> entry : bookStringMap.entrySet()) {
+        for (Map.Entry<Book, String> entry : bookLocations.entrySet()) {
             Book key = entry.getKey();
             String value = entry.getValue();
             System.out.println(key + " → " + value);
