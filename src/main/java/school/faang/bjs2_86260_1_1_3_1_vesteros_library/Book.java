53eb0e25cbd4670e573c7cd6ea28107c0707d9d0
= <mtgvow@gmail.com>
2025-09-14 20:35:54 +0300

    1.1.2.3 BJSL2-85600 Complete task

--- src/main/java/school/faang/bjs2_86260_1_1_3_1_vesteros_library/Book.java
+++ src/main/java/school/faang/bjs2_86260_1_1_3_1_vesteros_library/Book.java
@@ -1,28 +1,14 @@
 package school.faang.bjs2_86260_1_1_3_1_vesteros_library;
 
-import java.util.Objects;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.EqualsAndHashCode;
 
 @AllArgsConstructor
+@EqualsAndHashCode
 public class Book {
     @Getter
     private String title;
     private String author;
     private Integer year;
-
-    @Override
-    public boolean equals(Object o) {
-        if (o == null || getClass() != o.getClass()) {
-            return false;
-        }
-        Book book = (Book) o;
-        return Objects.equals(title, book.title) && Objects.equals(author, book.author) && Objects.equals(year,
-                book.year);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(title, author, year);
-    }
 }
--- src/main/java/school/faang/bjs2_86260_1_1_3_1_vesteros_library/LibrarySystem.java
+++ src/main/java/school/faang/bjs2_86260_1_1_3_1_vesteros_library/LibrarySystem.java
@@ -7,7 +7,7 @@ import java.util.Map;
 
 @NoArgsConstructor
 public class LibrarySystem {
-    Map<Book, String> locations = new HashMap<Book, String>();
+    private Map<Book, String> locations = new HashMap<Book, String>();
 
     public void addBook(String title, String author, int year, String location) {
         Book book = new Book(title, author, year);
