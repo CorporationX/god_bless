d54e53972a2a95c648517b3b87538a61f5250439
= <mtgvow@gmail.com>
2025-09-14 16:36:08 +0300

    1.1.2.3 BJSL2-85600 Complete task

--- src/main/java/school/faang/bjs2_85600_1_1_2_5_turn_around/Example.java
+++ src/main/java/school/faang/bjs2_85600_1_1_2_5_turn_around/Example.java
@@ -1,7 +1,21 @@
 package school.faang.bjs2_85600_1_1_2_5_turn_around;
 
+import lombok.NoArgsConstructor;
 
+import java.util.Arrays;
 
+@NoArgsConstructor
 public class Example {
+
+    public void reverse(int[] array) {
+
+        int[] reverseArray = new int[array.length];
+        for (int l = 0, r = array.length - 1; l < r; l++, r--) {
+            int temp = array[l];
+            array[l] = array[r];
+            array[r] = temp;
+        }
+    }
+
 
 }
--- src/main/java/school/faang/bjs2_85600_1_1_2_5_turn_around/Main.java
+++ src/main/java/school/faang/bjs2_85600_1_1_2_5_turn_around/Main.java
@@ -1,7 +1,14 @@
 package school.faang.bjs2_85600_1_1_2_5_turn_around;
 
+import java.lang.reflect.Array;
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
+        Example example = new Example();
+        int[] array = {1, 2, 3, 4, 5};
 
+        example.reverse(array);
+        System.out.println(Arrays.toString(array));
     }
 }
