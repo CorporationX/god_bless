1b2be73dbb9f156f668b634a604cea401471c72e
= <mtgvow@gmail.com>
2025-09-14 23:24:54 +0300

    1.1.3.4 BJS2-86940 Complete task

--- src/main/java/school/faang/bjs2_86940_1_1_3_4_double_cache_cache/Student.java
+++ src/main/java/school/faang/bjs2_86940_1_1_3_4_double_cache_cache/Student.java
@@ -1,5 +1,12 @@
 package school.faang.bjs2_86940_1_1_3_4_double_cache_cache;
 
+import lombok.Getter;
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+
+@AllArgsConstructor
+@EqualsAndHashCode
 public class Student {
+    @Getter
     private String name;
 }
--- src/main/java/school/faang/bjs2_86940_1_1_3_4_double_cache_cache/StudentDatabase.java
+++ src/main/java/school/faang/bjs2_86940_1_1_3_4_double_cache_cache/StudentDatabase.java
@@ -8,13 +8,12 @@ import java.util.Map;
 public class StudentDatabase {
     private Map<Student, Map<Subject, Integer>> studentSubjects = new HashMap<>();
     private Map<Subject, List<Student>> subjectStudents = new HashMap<>();
-    private Map<Student, >
 
     public void addNewStudentInfo(Student student, Map<Subject, Integer> info) {
         studentSubjects.put(student, info);
-        for (Map.Entry<Subject, Integer> entry : info.entrySet()) {
-            subjectStudents.putIfAbsent(entry.getKey(), new ArrayList<Student>());
-            subjectStudents.get(entry.getKey()).add(student);
+        for (Subject subject : info.keySet()) {
+            subjectStudents.putIfAbsent(subject, new ArrayList<Student>());
+            subjectStudents.get(subject).add(student);
         }
     }
 
@@ -22,11 +21,69 @@ public class StudentDatabase {
         studentSubjects.putIfAbsent(student, new HashMap<Subject, Integer>());
         studentSubjects.get(student).put(subject, grade);
         subjectStudents.putIfAbsent(subject, new ArrayList<Student>());
-        subjectStudents.get(subject).add(student);
+        if (!subjectStudents.get(subject).contains(student)) {
+            subjectStudents.get(subject).add(student);
+        }
     }
 
-    public void removeStudent(Student student){
-
+    public void removeStudent(Student student) {
+        Map<Subject, Integer> deletingInfo = studentSubjects.remove(student);
+        if (deletingInfo == null) {
+            return;
+        }
+        for (Subject subject : deletingInfo.keySet()) {
+            List<Student> students = subjectStudents.get(subject);
+            if (students != null) {
+                students.remove(student);
+            }
+        }
     }
+
+    public void printStudentsWithGrades() {
+        for (Student student : studentSubjects.keySet()) {
+            System.out.println("Студент: " + student.getName());
+            for (Map.Entry<Subject, Integer> entry : studentSubjects.get(student).entrySet()) {
+                System.out.println("\tПредмет: " + entry.getKey().getName() + "\tоценка: " + entry.getValue());
+            }
+        }
+    }
+
+    public void addNewSubjectWithStudents(Subject subject, List<Student> students) {
+        subjectStudents.put(subject, students);
+        for (Student student : students) {
+            studentSubjects.putIfAbsent(student, new HashMap<Subject, Integer>());
+            studentSubjects.get(student).put(subject, null);
+        }
+    }
+
+    public void addStudentToSubject(Student student, Subject subject) {
+        subjectStudents.putIfAbsent(subject, new ArrayList<Student>());
+        if (!subjectStudents.get(subject).contains(student)) {
+            subjectStudents.get(subject).add(student);
+        }
+        studentSubjects.putIfAbsent(student, new HashMap<Subject, Integer>());
+        studentSubjects.get(student).put(subject, null);
+    }
+
+    public void removeStudentFromSubject(Student student, Subject subject) {
+        List<Student> students = subjectStudents.get(subject);
+        if (students != null) {
+            subjectStudents.get(subject).remove(student);
+        }
+        Map<Subject, Integer> info = studentSubjects.get(student);
+        if (info != null) {
+            info.remove(subject);
+        }
+    }
+
+    public void printSubjectsAndStudents() {
+        for (Subject subject : subjectStudents.keySet()) {
+            System.out.println("Предмет: " + subject.getName());
+            for (Student student : subjectStudents.get(subject)) {
+                System.out.println("\tСтудент: " + student.getName());
+            }
+        }
+    }
+
 
 }
