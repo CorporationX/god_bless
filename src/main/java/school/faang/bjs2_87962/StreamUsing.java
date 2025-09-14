b554ba255be9b18e6004e483a2cd1896bb92a0c3
ayazs <ayaz-22@yandex.ru>
2025-09-15 00:23:50 +0500

    BJS2-87962 is done

--- src/main/java/school/faang/bjs2_87962/StreamUsing.java
+++ src/main/java/school/faang/bjs2_87962/StreamUsing.java
@@ -11,16 +11,29 @@ import java.util.stream.Collectors;
 public class StreamUsing {
 
     public static Set<List<Integer>> findUniqueNumbers(Set<Integer> numbers, int numb) {
-
         return numbers.stream().filter(num -> num != numb - num && numbers.contains(numb - num))
                 .map(num -> Arrays.asList(num, numb - num))
                 .peek(Collections::sort)
                 .collect(Collectors.toSet());
     }
 
-    public static List<String> sortContries(Map<String, String> capitals) {
+    public static List<String> sortCountries(Map<String, String> capitals) {
         return capitals.entrySet().stream()
                 .sorted(Map.Entry.comparingByKey())
                 .map(Map.Entry::getValue).toList();
+    }
+
+    public static List<String> filterStrings(List<String> stringList, char symbol) {
+        return stringList.stream().filter(s -> s.charAt(0) == symbol)
+                .sorted(Comparator.comparing(String::length)).toList();
+    }
+
+    public static List<String> convertToBinary(List<Integer> numbers) {
+        return numbers.stream().map(Integer::toBinaryString).toList();
+    }
+
+    public static List<String> filterAndSortStringsByLength(List<String> stringList, String alphabet) {
+        return stringList.stream().filter(s -> s.matches("\\b\\w*[" + alphabet + "]\\w*\\b"))
+                .sorted(Comparator.comparingInt(String::length)).toList();
     }
 }
