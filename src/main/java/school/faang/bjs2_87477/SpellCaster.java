8f548e150e51f1603717d6b0ace9e958e0e5d117
Roman Chipura <connor.ueyn@gmail.com>
2025-09-15 17:11:02 +0500

    Убрать скобки в лямбдах в Main
    Добавить public в SpellCaster.cast

--- src/main/java/school/faang/bjs2_87477/SpellCaster.java
+++ src/main/java/school/faang/bjs2_87477/SpellCaster.java
@@ -1,7 +1,7 @@
 package school.faang.bjs2_87477;
 
 public class SpellCaster {
-    void cast(String spellName, SpellAction spellAction) {
+    public void cast(String spellName, SpellAction spellAction) {
         String castResult = spellAction.cast(spellName);
         System.out.println(castResult);
     }
