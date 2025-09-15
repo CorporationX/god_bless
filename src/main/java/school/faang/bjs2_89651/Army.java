62421794d5733c328dd43dffb97f00b0ea8918bc
EdelweissPC <shherbakov99ilya@mail.ru>
2025-09-15 19:05:29 +0300

    BJS2-89651 logic army power

---
+++ src/main/java/school/faang/bjs2_89651/Army.java
@@ -0,0 +1,36 @@
+package school.faang.bjs2_89651;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Army {
+    private final List<Squad> squads = new ArrayList<>();
+
+    public void addSquad(Squad squad) {
+        squads.add(squad);
+    }
+
+    public int calculateTotalPower() throws InterruptedException {
+        List<Integer> resultPowerSquad = new ArrayList<>();
+        List<Thread> threads  = new ArrayList<>();
+
+        for (Squad s : squads) {
+            Thread thread = new Thread(() -> resultPowerSquad.add(s.calculateSquadPower()));
+            thread.start();
+            threads.add(thread);
+        }
+
+        threads.forEach(thread -> {
+            try {
+                thread.join();
+            } catch (InterruptedException e) {
+                Thread.currentThread().interrupt();
+            }
+        });
+
+        return resultPowerSquad.stream()
+                .mapToInt(Integer::intValue)
+                .sum();
+    }
+
+}
---
+++ src/main/java/school/faang/bjs2_89651/Swordsman.java
@@ -0,0 +1,7 @@
+package school.faang.bjs2_89651;
+
+public class Swordsman extends Unit {
+    public Swordsman(int power) {
+        super(power);
+    }
+}
