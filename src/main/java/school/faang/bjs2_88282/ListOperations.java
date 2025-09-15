7ae2c217eb585d7e6eb02e05e5e65c61cfe34647
Rukk Taras <tarrukk@gmail.com>
2025-09-15 14:58:39 +0300

    Variant 2

--- src/main/java/school/faang/bjs2_88282/ListOperations.java
+++ src/main/java/school/faang/bjs2_88282/ListOperations.java
@@ -1,104 +1,70 @@
 package school.faang.bjs2_88282;
 
+import lombok.NonNull;
+
 import java.util.Comparator;
 import java.util.List;
+import java.util.NoSuchElementException;
 import java.util.function.Predicate;
-import java.util.stream.Collectors;
-import java.util.stream.IntStream;
 
 public class ListOperations {
-    public static int countEvenNumbersSum(List<Integer> numbers) throws Exception {
-        if (numbers == null) {
-            throw new RuntimeException("Список чисел равен null.");
-        }
+    public static int countEvenNumbersSum(@NonNull List<Integer> numbers) {
         return numbers.stream()
                 .filter(n -> n % 2 == 0)
                 .mapToInt(Integer::intValue)
                 .sum();
     }
 
-    public static int maxNumber(List<Integer> numbers) throws Exception {
-        if (numbers == null) {
-            throw new RuntimeException("Список чисел равен null.");
-        }
+    public static int maxNumber(@NonNull List<Integer> numbers) {
         return numbers.stream()
-                .mapToInt(Integer::intValue).max()
-                .orElse(0);
+                .mapToInt(Integer::intValue)
+                .max()
+                .orElseThrow(() -> new NoSuchElementException("Невозможно найти максимальное число"));
     }
 
-    public static Double averageValue(List<Integer> numbers) throws Exception {
-        if (numbers == null) {
-            throw new RuntimeException("Список чисел равен null.");
-        }
-        IntStream intStream = numbers.stream()
-                .mapToInt(Integer::intValue);
-        return intStream.average()
-                .orElse(0);
+    public static double averageValue(@NonNull List<Integer> numbers) {
+        return numbers.stream()
+                .mapToInt(Integer::intValue)
+                .average()
+                .orElseThrow(() -> new NoSuchElementException("Невозможно посчитать среднее значение."));
     }
 
-    public static long countStringsStartWithSymbol(List<String> strings, char symbol) throws Exception {
-        if (strings == null) {
-            throw new RuntimeException("Список строк равен null.");
-        }
+    public static long countStringsStartWithSymbol(@NonNull List<String> strings, char symbol) {
         return strings.stream()
                 .filter(letter -> letter.startsWith(String.valueOf(symbol)))
                 .count();
     }
 
-    public static List<String> selectStringsWithSubstring(List<String> strings, String substring)
-            throws Exception {
-        if (strings == null || substring == null) {
-            throw new RuntimeException("Список строк или/и подстрока равны null.");
-        }
+    public static List<String> selectStringsWithSubstring(
+            @NonNull List<String> strings, @NonNull String substring) {
         return strings.stream()
                 .filter(text -> text.contains(substring))
                 .toList();
     }
 
-    public static List<String> sortByLength(List<String> strings) throws Exception {
-        if (strings == null) {
-            throw new RuntimeException("Список строк равен null.");
-        }
+    public static List<String> sortByLength(@NonNull List<String> strings) {
         return strings.stream()
                 .sorted(Comparator.comparingInt(String::length))
-                .collect(Collectors
-                        .toList());
+                .toList();
     }
 
-    public static boolean checkCondition(List<Integer> numbers, Predicate<Integer> predicate) {
-        if (numbers == null || predicate == null) {
-            throw new RuntimeException("Список чисел или/и число для проверки равны null");
-        }
+    public static boolean checkCondition(
+            @NonNull List<Integer> numbers, @NonNull Predicate<Integer> predicate) {
         return numbers.stream()
                 .allMatch(predicate);
     }
 
-    public static int findMinSatisfyCondition(List<Integer> numbers, int conditionNumber) {
-        if (numbers == null) {
-            throw new RuntimeException("Список чисел или/и число для проверки равны null");
-        }
+    public static int findMinSatisfyCondition(@NonNull List<Integer> numbers, int conditionNumber) {
         return numbers.stream()
                 .mapToInt(Integer::intValue)
                 .filter(x -> x > conditionNumber)
                 .min()
-                .orElse(0);
+                .orElseThrow(() -> new NoSuchElementException("Невозможно найти минимальное число"));
     }
 
-    public static List<Integer> convertListStringToListLength(List<String> strings) {
-        if (strings == null) {
-            throw new RuntimeException("Список строк равен null.");
-        }
-        return strings.stream().map(String::length).collect(Collectors.toList());
+    public static List<Integer> convertListStringToListLength(@NonNull List<String> strings) {
+        return strings.stream()
+                .map(String::length)
+                .toList();
     }
-
-
-
-
-
-
-
-
-
-
-
 }
