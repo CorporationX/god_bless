e687eb283ebc72bf6400ee4ca8e42204b0178323
Rukk Taras <tarrukk@gmail.com>
2025-09-16 18:47:50 +0300

    Variant 1

--- src/main/java/school/faang/bjs2_91444/Army.java
+++ src/main/java/school/faang/bjs2_91444/Army.java
@@ -5,7 +5,7 @@ import java.util.List;
 public class Army {
     public List<List<Unit>> wholeArmy;
 
-    public static int calculateTotalPower(List<List<Unit>> army) {
+    public static void calculateTotalPower(List<List<Unit>> army) {
         for (List<Unit> squad : army) {
 
         }
