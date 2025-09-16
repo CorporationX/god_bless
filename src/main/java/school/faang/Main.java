cb7b8c7fd1e80880690a4a8df5ac2ca96f5bc9df
Rukk Taras <tarrukk@gmail.com>
2025-09-16 16:50:20 +0300

    Variant 1

--- src/main/java/school/faang/Main.java
+++ src/main/java/school/faang/Main.java
@@ -6,7 +6,7 @@ public class Main {
     public static void main(String[] args) {
         Swordsman petro = new Swordsman(92);
         Squad swordsmen1 = new Squad("Мечники 1", List.of(new Swordsman(35), new Swordsman(33),
-                new Swordsman(39), new Swordsman(-41), petro, new Swordsman(35)));
+                    new Swordsman(39), new Swordsman(-41), petro, new Swordsman(35)));
         System.out.println("Общая сила команды: " + Squad.calculateSquadPower(swordsmen1.getUnits()));
     }
 }
