d77715491e16caa8238b2e0a5349271e6ac61574
Pavel-Lukyanov <PavelLukyanov2017@yandex.ru>
2025-09-16 23:05:36 +0500

    BJS2-91071

---
+++ src/main/java/school/faang/bjs2_91071/MailSender.java
@@ -0,0 +1,40 @@
+package school.faang.bjs2_91071;
+
+import lombok.extern.slf4j.Slf4j;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
+
+@Slf4j
+public class MailSender {
+    private static final int COUNT_THREADS = 5;
+    private static final int COUNT_MESSAGE = 1000;
+
+    public static void main(String[] args) {
+        ExecutorService executor = Executors.newFixedThreadPool(COUNT_THREADS);
+        int batchSize = COUNT_MESSAGE / COUNT_THREADS;
+
+        try {
+            for (int i = 0; i < COUNT_THREADS; i++) {
+                int startIndex = i * batchSize;
+                int endIndex = (i + 1) * batchSize;
+
+                executor.submit(new SenderRunnable(startIndex, endIndex));
+            }
+        } finally {
+            executor.shutdown();
+        }
+
+        try {
+            if (!executor.awaitTermination(1, TimeUnit.MINUTES)) {
+                executor.shutdownNow();
+            }
+        } catch (InterruptedException e) {
+            Thread.currentThread().interrupt();
+            log.warn("Не все задачи завершились за отведенное время");
+        }
+
+        log.info("Все письма успешно отправлены");
+    }
+}
