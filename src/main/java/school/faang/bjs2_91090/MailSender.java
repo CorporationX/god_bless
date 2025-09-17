94f2ba83b1326e1d3c0861e523ddffb6d11b11be
Oleg_Korovin <olegk.99@mail.ru>
2025-09-17 21:40:00 +0300

    BJS2-91090/fix#1

--- src/main/java/school/faang/bjs2_91090/MailSender.java
+++ src/main/java/school/faang/bjs2_91090/MailSender.java
@@ -2,11 +2,11 @@ package school.faang.bjs2_91090;
 
 public class MailSender {
     public static void main(String[] args) throws InterruptedException {
-        Thread firstThread = new Thread(new SenderRunnable(1, 200));
-        Thread secondThread = new Thread(new SenderRunnable(201, 400));
-        Thread thirdThread = new Thread(new SenderRunnable(401, 600));
-        Thread forthThread = new Thread(new SenderRunnable(601, 800));
-        Thread fifthThread = new Thread(new SenderRunnable(801, 1000));
+        final Thread firstThread = new Thread(new SenderRunnable(1, 200));
+        final Thread secondThread = new Thread(new SenderRunnable(201, 400));
+        final Thread thirdThread = new Thread(new SenderRunnable(401, 600));
+        final Thread forthThread = new Thread(new SenderRunnable(601, 800));
+        final Thread fifthThread = new Thread(new SenderRunnable(801, 1000));
 
         firstThread.start();
         secondThread.start();
