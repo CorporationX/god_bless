eca5b99873978093ca8a319f0df8676521fe3362
Asta <charlyfazbear@gmail.com>
2025-09-17 16:55:43 +0300

    Implemented parallel task processing through a new cached thread pool

---
+++ src/main/java/school/faang/bjs2_89611/WeasleyFamily.java
@@ -0,0 +1,18 @@
+package school.faang.bjs2_89611;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+public class WeasleyFamily {
+    public static void main(String[] args) {
+        String[] chores = {"помыть посуду", "подмести пол", "приготовить ужин"};
+
+        ExecutorService executor = Executors.newCachedThreadPool();
+
+        for (String chore : chores) {
+            executor.submit(new Chore(chore));
+        }
+
+        executor.shutdown();
+    }
+}
