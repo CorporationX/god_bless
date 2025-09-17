d82dae627fac6e8522ae6c31af39d44bb1ed4240
Asta <charlyfazbear@gmail.com>
2025-09-17 16:55:43 +0300

    Implemented parallel task processing through a new cached thread pool

---
+++ src/main/java/school/faang/bjs2_89611/Chore.java
@@ -0,0 +1,22 @@
+package school.faang.bjs2_89611;
+
+import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@AllArgsConstructor
+public class Chore implements Runnable {
+    private String chore;
+
+    @Override
+    public void run() {
+        log.info("Поток {} выполняет задачу: {}", Thread.currentThread().getName(), chore);
+        try {
+            Thread.sleep(1000);
+        } catch (InterruptedException e) {
+            Thread.currentThread().interrupt();
+            log.info("Задача была прервана");
+        }
+        log.info("Поток {} завершил задачу: {}", Thread.currentThread().getName(), chore);
+    }
+}
