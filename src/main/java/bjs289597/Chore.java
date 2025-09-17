1ec4a3890d84dc1491756a2e6c7d4697b86119ca
Admin <sofiya.chebotareva@bk.ru>
2025-09-17 11:29:40 +0200

    task BJS2-89597, fix after review

--- src/main/java/bjs289597/Chore.java
+++ src/main/java/bjs289597/Chore.java
@@ -4,15 +4,17 @@ import lombok.RequiredArgsConstructor;
 
 @RequiredArgsConstructor
 public class Chore implements Runnable {
+    private static final int TASK_TIMEOUT_MILLISECONDS = 2000;
     private final String chore;
 
     @Override
     public void run() {
         try {
-            System.out.println("Thread " + Thread.currentThread().getName() + " starts the task: " + chore);
-            Thread.sleep(2000);
-            System.out.println("Thread " + Thread.currentThread().getName() + " starts the task" + chore);
+            System.out.printf("Thread %s starts the task: %s%n", Thread.currentThread().getName(), chore);
+            Thread.sleep(TASK_TIMEOUT_MILLISECONDS);
+            System.out.printf("Thread %s finished the task %s%n", Thread.currentThread().getName(), chore);
         } catch (InterruptedException e) {
+            System.out.printf("Task %s was terminated!", chore);
             Thread.currentThread().interrupt();
             throw new RuntimeException(e);
         }
