47e7762cc4c867e17e8bd80aaa3258be010131a1
teymuruae <baratov.teymur@yandex.ru>
2025-09-17 22:36:57 +0300

    BJS2-87304 Google Переводчик под нашим контролем… fixed

--- src/main/java/school/faang/lambda/bjs2_87304/DictionaryProcessor.java
+++ src/main/java/school/faang/lambda/bjs2_87304/DictionaryProcessor.java
@@ -3,7 +3,7 @@ package school.faang.lambda.bjs2_87304;
 import java.util.function.BiConsumer;
 
 public class DictionaryProcessor {
-    public void processWord(String word, String translatedWord, BiConsumer<String, String> biConsumer) {
-        biConsumer.accept(word, translatedWord);
+    public void processWord(String word, String translatedWord, BiConsumer<String, String> dictionaryConsumer) {
+        dictionaryConsumer.accept(word, translatedWord);
     }
 }
