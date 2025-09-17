6e9be2dec7c5fd525db0f4fb29c41dcbd6ceb5bd
svxrslf <svxrslf@yandex.ru>
2025-09-17 13:54:30 +0300

    90669 Переписал метод removeItem, добавил еще несколько проверок, теперь можно удалять сразу несколько предметов

--- src/main/java/school/faang/bjs2_90669/InventoryManager.java
+++ src/main/java/school/faang/bjs2_90669/InventoryManager.java
@@ -12,18 +12,21 @@ public class InventoryManager {
         itemConsumer.accept(item);
     }
 
-    public void removeItem(Character character, Predicate<Item> predicate) {
-        character.getInventory().removeIf(predicate);
+    public void removeItem(Character character, Predicate<Item> predicate, Consumer<Item> itemConsumer) {
+        List<Item> inventory = character.getInventory();
+        List<Item> toRemove = inventory.stream()
+                .filter(predicate)
+                .toList();
+        inventory.removeIf(predicate);
+        toRemove.forEach(itemConsumer);
     }
 
     public void updateItem(Character character, Predicate<Item> predicate, UnaryOperator<Item> updater) {
         List<Item> inventory = character.getInventory();
-
         List<Item> updatedItems = inventory.stream()
                 .filter(predicate)
                 .map(updater)
                 .toList();
-
         inventory.removeIf(predicate);
         inventory.addAll(updatedItems);
     }
