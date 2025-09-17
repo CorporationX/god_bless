447b3af13d8dd0bf528a75ce0bc043206abe33f1
Asta <charlyfazbear@gmail.com>
2025-09-17 22:57:11 +0300

    task completed

---
+++ src/main/java/school/faang/bjs2_91304/Fighter.java
@@ -0,0 +1,10 @@
+package school.faang.bjs2_91304;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+abstract class Fighter {
+    protected int power;
+}
---
+++ src/main/java/school/faang/bjs2_91304/Swordsman.java
@@ -0,0 +1,8 @@
+package school.faang.bjs2_91304;
+
+
+public class Swordsman extends Fighter {
+    public Swordsman(int power) {
+        super(power);
+    }
+}
