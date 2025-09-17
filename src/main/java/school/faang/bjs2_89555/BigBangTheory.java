d74611e7576585e32502ffb7305f1e0e23e79372
Bekhter Roman <curses93@mail.ru>
2025-09-17 11:51:25 +0300

    BJS2-89555 fix

--- src/main/java/school/faang/bjs2_89555/BigBangTheory.java
+++ src/main/java/school/faang/bjs2_89555/BigBangTheory.java
@@ -1,12 +1,19 @@
 package school.faang.bjs2_89555;
 
+import lombok.extern.slf4j.Slf4j;
+
 import java.util.List;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
 
+@Slf4j
 public class BigBangTheory {
+    private static final int THREAD = 4;
+    private static final int AWAIT_TIME = 1;
+
     public static void main(String[] args) {
-        ExecutorService executor = Executors.newFixedThreadPool(4);
+        ExecutorService executor = Executors.newFixedThreadPool(THREAD);
         Task taskOne = new Task("Sheldon", "theory preparation");
         Task taskTwo = new Task("Leonard", "simulation of experiment");
         Task taskThree = new Task("Howard", "tool development");
@@ -17,5 +24,16 @@ public class BigBangTheory {
             executor.submit(tasks.get(i));
         }
         executor.shutdown();
+        try {
+            if (!executor.awaitTermination(AWAIT_TIME, TimeUnit.MINUTES)) {
+                log.warn("не все задачи завершились вовремя, закрываем пул потоков.");
+                executor.shutdownNow();
+            } else {
+                log.info("все доставки выполнены успешно.");
+            }
+        } catch (InterruptedException e) {
+            log.error("главный поток прерван");
+            executor.shutdownNow();
+        }
     }
 }
--- src/main/java/school/faang/bjs2_89555/Task.java
+++ src/main/java/school/faang/bjs2_89555/Task.java
@@ -1,26 +1,23 @@
 package school.faang.bjs2_89555;
 
+import lombok.AllArgsConstructor;
 import lombok.SneakyThrows;
 
+@AllArgsConstructor
 public class Task implements Runnable {
     private String name;
     private String task;
-
-    public Task(String name, String task) {
-        this.name = name;
-        this.task = task;
-    }
+    private static final int COUNT_SLEEP = 5000;
 
     @SneakyThrows
     public void run() {
-        System.out.println("Start task: " +
-                "Employee: " + name +
-                "Task: " + task);
+        System.out.printf("Start task:%nEmployee: %s%nTask: %s%n", name, task);
         try {
-            Thread.sleep(5000);
+            Thread.sleep(COUNT_SLEEP);
             System.out.println("Task " + task + " completed");
         } catch (InterruptedException e) {
             System.out.println("Task " + task + " was interrupted");
+            Thread.currentThread().interrupt();
         }
     }
 }
