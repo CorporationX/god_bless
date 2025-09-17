4014fe51a46d70117e3d4481d1855c1d203d5112
Admin <sofiya.chebotareva@bk.ru>
2025-09-17 10:34:53 +0200

    task BJS2-91065, fix after review

--- src/main/java/bjs291065/MailSender.java
+++ src/main/java/bjs291065/MailSender.java
@@ -3,20 +3,20 @@ package bjs291065;
 public class MailSender {
     private static final int MESSAGES_AMOUNT = 1000;
     private static final int THREADS_AMOUNT = 5;
+    private static final int MAILS_FOR_THREAD = MESSAGES_AMOUNT / THREADS_AMOUNT;
 
     public static void main(String[] args) throws InterruptedException {
         Thread[] threads = new Thread[THREADS_AMOUNT];
-        int mailsForThread = MESSAGES_AMOUNT / THREADS_AMOUNT;
         int startLetterNum = 1;
-        int finishLetterNum = mailsForThread;
+        int finishLetterNum = MAILS_FOR_THREAD;
 
         for (int i = 0; i < THREADS_AMOUNT; i++) {
             threads[i] = new Thread(new SenderRunnable(startLetterNum, finishLetterNum));
-            startLetterNum += mailsForThread;
-            finishLetterNum += mailsForThread;
+            startLetterNum += MAILS_FOR_THREAD;
+            finishLetterNum += MAILS_FOR_THREAD;
+            threads[i].start();
         }
         for (Thread thread : threads) {
-            thread.start();
             thread.join();
         }
 
--- src/main/java/bjs291065/SenderRunnable.java
+++ src/main/java/bjs291065/SenderRunnable.java
@@ -10,7 +10,7 @@ public class SenderRunnable implements Runnable {
     @Override
     public void run() {
         for (int i = startIndex; i <= endIndex; i++) {
-            System.out.println(Thread.currentThread() + " Mail number " + i + " was sent successfully!");
+            System.out.printf("%s. Mail number %d was sent successfully!%n", Thread.currentThread(),i);
         }
     }
 }
