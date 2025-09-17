573517e92888cdfbd93ab90e4e1c6b272224c6c1
Rukk Taras <tarrukk@gmail.com>
2025-09-17 13:39:40 +0300

    Variant 2

--- src/main/java/school/faang/bjs2_89585/MailSender.java
+++ src/main/java/school/faang/bjs2_89585/MailSender.java
@@ -1,41 +1,26 @@
 package school.faang.bjs2_89585;
 
+@SuppressWarnings({"checkstyle:MemberName", "checkstyle:AbbreviationAsWordInName"})
 public class MailSender {
-    @SuppressWarnings({"checkstyle:AbbreviationAsWordInName", "checkstyle:MemberName"})
-    final int THREAD_COUNT = 5;
-    @SuppressWarnings({"checkstyle:MemberName", "checkstyle:AbbreviationAsWordInName"})
-    final int TOTAL_MESSAGES = 1000;
-    @SuppressWarnings("checkstyle:EmptyLineSeparator")
     public static void main(String[] args) {
-        
-
-
-
-
-
-
-
-
-        Thread thread1 = new Thread(new SenderRunnable(1, 200));
-        thread1.start();
-        Thread thread2 = new Thread(new SenderRunnable(201, 400));
-        thread2.start();
-        Thread thread3 = new Thread(new SenderRunnable(401, 600));
-        thread3.start();
-        Thread thread4 = new Thread(new SenderRunnable(601, 800));
-        thread4.start();
-        Thread thread5 = new Thread(new SenderRunnable(801, 1000));
-        thread5.start();
-        try {
-            thread1.join();
-            thread2.join();
-            thread3.join();
-            thread4.join();
-            thread5.join();
-            System.out.println("Все письма разосланы.");
-        } catch (InterruptedException e) {
-            Thread.currentThread().interrupt();
-            System.out.println("Поток main не смог дождаться, его прервали.");
+        final int THREAD_COUNT = 5;
+        final int TOTAL_MESSAGES = 1000;
+        int batchSize = TOTAL_MESSAGES / THREAD_COUNT;
+        Thread[] threads = new Thread[THREAD_COUNT];
+        for (int i = 0; i < THREAD_COUNT; i++) {
+            int startIndex = i * batchSize;
+            int endIndex = (i + 1) * batchSize - 1;
+            threads[i] = new Thread(new SenderRunnable(startIndex, endIndex));
+            threads[i].start();
         }
+        for (Thread thread : threads) {
+            try {
+                thread.join();
+            } catch (InterruptedException e) {
+                Thread.currentThread().interrupt();
+                System.out.println("Поток main не смог дождаться, его прервали.");
+            }
+        }
+        System.out.println("Все письма разосланы.");
     }
 }
