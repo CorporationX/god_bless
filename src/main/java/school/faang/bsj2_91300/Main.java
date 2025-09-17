7de90a6be4cd6819c8707538741cb206acb647e1
Kuanysh <skn_88@mail.ru>
2025-09-17 06:28:12 +0500

    BSJ2-91300 Solution of the task

---
+++ src/main/java/school/faang/bsj2_91300/Main.java
@@ -0,0 +1,9 @@
+package school.faang.bsj2_91300;
+
+public class Main {
+    public static void main(String[] args) throws InterruptedException {
+
+        WeasleyFamily weasleyFamily = new WeasleyFamily();
+        weasleyFamily.makeChores();
+    }
+}
