942cf890f6435114204023669efa1478f4fb0bf1
Oleg_Korovin <olegk.99@mail.ru>
2025-09-17 21:31:13 +0300

    BJS2-91090

---
+++ src/main/java/school/faang/bjs2_91090/MailSender.java
@@ -0,0 +1,25 @@
+package school.faang.bjs2_91090;
+
+public class MailSender {
+    public static void main(String[] args) throws InterruptedException {
+        Thread firstThread = new Thread(new SenderRunnable(1, 200));
+        Thread secondThread = new Thread(new SenderRunnable(201, 400));
+        Thread thirdThread = new Thread(new SenderRunnable(401, 600));
+        Thread forthThread = new Thread(new SenderRunnable(601, 800));
+        Thread fifthThread = new Thread(new SenderRunnable(801, 1000));
+
+        firstThread.start();
+        secondThread.start();
+        thirdThread.start();
+        forthThread.start();
+        fifthThread.start();
+
+        firstThread.join();
+        secondThread.join();
+        thirdThread.join();
+        forthThread.join();
+        fifthThread.join();
+
+        System.out.println("Все сообщения отправлены!");
+    }
+}
