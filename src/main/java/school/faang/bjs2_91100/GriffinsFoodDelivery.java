3854d91bd03f74b00aa09a6478651d285dd8ec0c
Mikhail EASY <mghievrie@bk.ru>
2025-09-18 14:02:47 +0500

    добавил awaitTermination BJS2-91100

--- src/main/java/school/faang/bjs2_91100/GriffinsFoodDelivery.java
+++ src/main/java/school/faang/bjs2_91100/GriffinsFoodDelivery.java
@@ -1,13 +1,18 @@
 package school.faang.bjs2_91100;
 
+import lombok.extern.slf4j.Slf4j;
+
 import java.util.Random;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
 
+@Slf4j
 public class GriffinsFoodDelivery {
     private static final int THREAD_COUNT = 3;
     private static final int SEEDS_START_RANDOM_FOOD = 1;
     private static final int SEEDS_END_RANDOM_FOOD = 30;
+    private static final int MAX_WAIT_SECOND = 1000;
 
     public static void main(String[] args) {
         ExecutorService execute = Executors.newFixedThreadPool(THREAD_COUNT);
@@ -19,5 +24,13 @@ public class GriffinsFoodDelivery {
             execute.submit(taskDelivery);
         }
         execute.shutdown();
+        try {
+            if (!execute.awaitTermination(MAX_WAIT_SECOND, TimeUnit.SECONDS)) {
+                log.info("Не все задачи завершены за {} секунд. Завершаем принудительно", MAX_WAIT_SECOND);
+            }
+        } catch (InterruptedException e) {
+            log.info("Ожидание завершения потоков прервано");
+            execute.shutdownNow();
+        }
     }
 }
