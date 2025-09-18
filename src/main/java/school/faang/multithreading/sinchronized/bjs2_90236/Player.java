ccd4659a41aed6f3bdb20aecc6a68956c9606463
AndreySN7 <andrey__n@mail.ru>
2025-09-18 21:39:33 +0300

    Решил задачу: Блокировка Spotify

---
+++ src/main/java/school/faang/multithreading/sinchronized/bjs2_90236/Player.java
@@ -0,0 +1,41 @@
+package school.faang.multithreading.sinchronized.bjs2_90236;
+
+public class Player {
+    private final Object lock = new Object();
+    private boolean isPlaying =  false;
+
+    public void play() {
+        synchronized (lock) {
+            if (!isPlaying) {
+                isPlaying = true;
+                System.out.println("Музыка начала играть");
+            } else {
+                System.out.println("Музыка уже играет");
+            }
+        }
+    }
+
+    public void pause() {
+        synchronized (lock) {
+            if (isPlaying) {
+                isPlaying = false;
+                System.out.println("Музыка поставлена на паузу");
+            }  else {
+                System.out.println("Музыка уже остановлена");
+            }
+        }
+    }
+
+    public void skip() {
+        synchronized (lock) {
+            System.out.println("Трек пропущен");
+        }
+
+    }
+
+    public void previous() {
+        synchronized (lock) {
+            System.out.println("Вернулись к предыдущему треку");
+        }
+    }
+}
