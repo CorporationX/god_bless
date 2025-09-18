4686ed9192de24b75f11bddd151746e9b03fcd09
Mikhail EASY <mghievrie@bk.ru>
2025-09-18 16:21:05 +0500

    добавил пару блоков if-else BJS2-92200

--- src/main/java/school/faang/bjs2_92220/Player.java
+++ src/main/java/school/faang/bjs2_92220/Player.java
@@ -1,12 +1,13 @@
 package school.faang.bjs2_92220;
 
 import lombok.extern.slf4j.Slf4j;
+
 import java.util.concurrent.atomic.AtomicBoolean;
 
 @Slf4j
 public class Player {
     private final String lock;
-    private AtomicBoolean isPlaying = new AtomicBoolean();
+    private final AtomicBoolean isPlaying = new AtomicBoolean();
 
     public Player(String lock) {
         this.lock = lock;
@@ -14,22 +15,30 @@ public class Player {
 
     public void play() {
         synchronized (lock) {
-            isPlaying.getAndSet(true);
-            log.info("Пользователь - {}, Включилась музыка? {}", Thread.currentThread().getName(), isPlaying.get());
+            if (!isPlaying.get()) {
+                isPlaying.getAndSet(true);
+                log.info("Пользователь - {}, Включает музыку", Thread.currentThread().getName());
+            } else {
+                log.info("Пользователь - {}, Музыка уже включена", Thread.currentThread().getName());
+            }
         }
     }
 
     public void pause() {
         synchronized (lock) {
-            isPlaying.getAndSet(false);
-            log.info("Пользователь - {}, Поставил Музыку на паузу, Она включенна? {}",
-                    Thread.currentThread().getName(), isPlaying.get());
+            if (isPlaying.get()) {
+                isPlaying.getAndSet(false);
+                log.info("Пользователь - {}, Поставил Музыку на паузу",
+                        Thread.currentThread().getName());
+            } else {
+                log.info("Музыка уже на паузе");
+            }
         }
     }
 
     public void skip() {
         synchronized (lock) {
-            isPlaying.getAndSet(false);
+            isPlaying.getAndSet(true);
             log.info("Пользователь - {}, Переключили музыку вперед, Играет музыка? - {}",
                     Thread.currentThread().getName(), isPlaying.get());
         }
@@ -37,7 +46,7 @@ public class Player {
 
     public void previous() {
         synchronized (lock) {
-            isPlaying.getAndSet(false);
+            isPlaying.getAndSet(true);
             log.info("Пользователь - {}, Переключили музыку назад, Играет ли музыка? {}",
                     Thread.currentThread().getName(), isPlaying.get());
         }
