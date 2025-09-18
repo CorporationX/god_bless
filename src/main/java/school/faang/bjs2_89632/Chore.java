30ff65e5c890a41421380cf1645665bb129724cc
DeniAkhmadov <daglossteckin@gmail.com>
2025-09-19 01:43:03 +0300

    Wesley Family (Обновлено)

--- src/main/java/school/faang/bjs2_89632/Chore.java
+++ src/main/java/school/faang/bjs2_89632/Chore.java
@@ -1,17 +1,20 @@
 package school.faang.bjs2_89632;
 
+import lombok.extern.slf4j.Slf4j;
+import java.util.concurrent.TimeUnit;
+
+@Slf4j
 public record Chore(String chore) implements Runnable {
+    private static final int THREAD_SLEEP_TIME = 200;
 
     @Override
     public void run() {
         try {
-            System.out.printf(Thread.currentThread().getName() + " Выполняет задачу: %s\n", chore);
-            Thread.sleep(200);
-            System.out.printf(Thread.currentThread().getName() + ": Задача %s выполнена\n", chore);
+            log.info("Выполняет задачу: {}\n", chore);
+            TimeUnit.SECONDS.sleep(THREAD_SLEEP_TIME);
+            log.info("Задача {} выполнена\n", chore);
         } catch (InterruptedException e) {
-            System.out.printf(
-                    "Поток %s: Меня прервали в процессе выполнения задачи\n", Thread.currentThread().getName()
-            );
+            log.info("Поток %s: Меня прервали в процессе выполнения задачи\n");
             Thread.currentThread().interrupt();
         }
     }
--- src/main/java/school/faang/bjs2_89632/WesleyFamily.java
+++ src/main/java/school/faang/bjs2_89632/WesleyFamily.java
@@ -1,42 +1,43 @@
 package school.faang.bjs2_89632;
 
-import lombok.Getter;
+import lombok.extern.slf4j.Slf4j;
 
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.TimeUnit;
 
-@Getter
+@Slf4j
 public class WesleyFamily {
+    private static final int WAITING_TIME = 5;
+
     private static final String[] CHORES = {
             "Помыть посуду", "Подмести пол", "Выбросить мусор",
             "Пропылесосить весь дом", "Полить цветы", "Засыпать растения огорода удобрениями",
             "Выгулять драконов", "Очень сильно ударить Рона", "Почистить мётла для полёта"
     };
 
-    private static final ExecutorService EXECUTORS = Executors.newCachedThreadPool();
+    private static final ExecutorService EXECUTOR = Executors.newCachedThreadPool();
 
     private WesleyFamily() {}
 
     public static void main(String[] args) {
         for (String choreName : CHORES) {
-            EXECUTORS.submit(new Chore(choreName));
+            EXECUTOR.submit(new Chore(choreName));
         }
 
-        EXECUTORS.shutdown();
+        EXECUTOR.shutdown();
 
         try {
-            if (!EXECUTORS.awaitTermination(5, TimeUnit.SECONDS)) {
-                System.out.println(
-                        "Не все задачи выполнились за достаточный промежуток времени. Прерываем работу потоков..."
-                );
-                EXECUTORS.shutdownNow();
+            if (!EXECUTOR.awaitTermination(WAITING_TIME, TimeUnit.SECONDS)) {
+                log.info("Не все задачи выполнились за достаточный промежуток времени. Прерываем работу потоков...");
+                EXECUTOR.shutdownNow();
             } else {
-                System.out.printf("%s: Все задачи были выполнены вовремя!\n", Thread.currentThread().getName());
+                log.info("Все задачи были выполнены вовремя!\n");
             }
         } catch (InterruptedException e) {
-            System.out.printf("%s: Меня прервали в процессе ожидания!\n", Thread.currentThread().getName());
+            log.info("Меня прервали в процессе ожидания!\n");
             Thread.currentThread().interrupt();
+            EXECUTOR.shutdownNow();
         }
     }
 }
