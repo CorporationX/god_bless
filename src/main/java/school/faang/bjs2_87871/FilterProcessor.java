82b48d7a0402819ab4add3850ec7290f492e6694
Roman Chipura <connor.ueyn@gmail.com>
2025-09-19 00:26:15 +0500

    Revert "Добавить модификатор доступа в FilterProcessor.applyFilter"
    This reverts commit 9ea01a8f9ab55d20f57ccf407dbef1cdaf4bfc24.

--- src/main/java/school/faang/bjs2_87871/FilterProcessor.java
+++ src/main/java/school/faang/bjs2_87871/FilterProcessor.java
@@ -4,7 +4,7 @@ import java.util.function.Function;
 
 public class FilterProcessor {
 
-    public Image applyFilter(Image image, Function<Image, Image> filter) {
+    Image applyFilter(Image image, Function<Image, Image> filter) {
         return filter.apply(image);
     }
 
--- src/main/java/school/faang/bjs2_87871/Image.java
+++ src/main/java/school/faang/bjs2_87871/Image.java
@@ -1,13 +1,16 @@
 package school.faang.bjs2_87871;
 
-import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
 
-@AllArgsConstructor
 @Getter
 @Setter
 public class Image {
     private String name;
     private String description;
+
+    public Image(String name, String description) {
+        this.name = name;
+        this.description = description;
+    }
 }
