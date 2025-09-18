24a83aa17b1892ca39b9b1921a2b2b74adcd82f9
Rukk Taras <tarrukk@gmail.com>
2025-09-18 19:00:21 +0300

    Variant 1

--- src/main/java/school/faang/bjs2_89777/Main.java
+++ src/main/java/school/faang/bjs2_89777/Main.java
@@ -5,26 +5,40 @@ import java.util.List;
 import java.util.Random;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
 
 public class Main {
     @SuppressWarnings({"checkstyle:LocalVariableName", "checkstyle:AbbreviationAsWordInName"})
     public static void main(String[] args) {
         int THREAD_AMOUNT = 10;
+        int WAITING_TIME = 3;
         List<Person> persons = new ArrayList<>(10000);
         Random random = new Random();
         random.nextInt();
-        for (Person person : persons) {
-            person.setName(String.valueOf(random.nextInt(1, 1000)));
-            person.setSurname(String.valueOf(random.nextInt(1, 1000)));
-            person.setAge(random.nextInt(1, 1000));
-            person.setWorkplace(String.valueOf(random.nextInt(1, 1000)));
+        for (int i = 0; i < 10000; i++) {
+            persons.add(new Person(
+                    String.valueOf(random.nextInt(1, 1000)),
+                    String.valueOf(random.nextInt(1, 1000)),
+                    random.nextInt(1, 1000),
+                    String.valueOf(random.nextInt(1, 1000))
+                    )
+            );
         }
-
+        int groupSize = persons.size() / THREAD_AMOUNT;
         ExecutorService executor = Executors.newFixedThreadPool(THREAD_AMOUNT);
-        for (Person person : persons) {
-            executor.execute(new PersonInfoPrinter(person));
+        for (int i = 0; i < THREAD_AMOUNT; i++) {
+            List<Person> group = persons.subList(i * groupSize, (i + 1) * groupSize - 1);
+            executor.execute(new PersonInfoPrinter(group));
         }
-
-
+        executor.shutdown();
+        try {
+            if (!executor.awaitTermination(WAITING_TIME, TimeUnit.MINUTES)) {
+                System.out.println("Поток main не дождался окончания работы остальных потоков.");
+            }
+        } catch (InterruptedException e) {
+            Thread.currentThread().interrupt();
+            System.out.println("Ождиания потока main были прерваны.");
+        }
+        System.out.println("Все потоки завершили свою работу успешно.");
     }
 }
--- src/main/java/school/faang/bjs2_89777/PersonInfoPrinter.java
+++ src/main/java/school/faang/bjs2_89777/PersonInfoPrinter.java
@@ -1,13 +1,20 @@
 package school.faang.bjs2_89777;
 
+import lombok.AllArgsConstructor;
+
+import java.util.List;
+
+@AllArgsConstructor
 public class PersonInfoPrinter implements Runnable {
-    Person person;
+    List<Person> persons;
 
     @Override
     public void run() {
-        System.out.printf("Имя: %s%n", person.getName());
-        System.out.printf("Фамилия: %s%n", person.getSurname());
-        System.out.printf("Возраст: %d%n", person.getAge());
-        System.out.printf("Место работы: %s%n", person.getWorkplace());
+        for (Person person : persons) {
+            System.out.printf("Имя: %s. ", person.getName());
+            System.out.printf("Фамилия: %s. ", person.getSurname());
+            System.out.printf("Возраст: %d. ", person.getAge());
+            System.out.printf("Место работы: %s.%n", person.getWorkplace());
+        }
     }
 }
