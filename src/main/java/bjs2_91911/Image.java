d05fe8cf9879939b725374aaffb38926d1a9c3ed
Abzal Samekeyev <samekeyev@gmail.com>
2025-09-18 17:42:27 +0500

    Задача "Фильтры Instagram" на проверку

---
+++ src/main/java/bjs2_91911/Image.java
@@ -0,0 +1,20 @@
+package bjs2_91911;
+
+
+public class Image {
+    private final String name;
+    private final String description;
+
+    public Image(String name, String description) {
+        this.name = name;
+        this.description = description;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+}
