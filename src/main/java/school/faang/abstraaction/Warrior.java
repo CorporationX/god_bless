ad853d76089ce3f0c58daac0fccff48d52442d73
nataliiaiallina <natalia.iallina@gmail.com>
2025-09-18 17:14:45 -0400

    BasicJava: Add Abstractions problem solution

---
+++ src/main/java/school/faang/abstraaction/Warrior.java
@@ -0,0 +1,14 @@
+package school.faang.abstraaction;
+
+public class Warrior extends Character {
+
+  public Warrior(String name) {
+    super(name,10,5,3);
+  }
+
+  @Override
+  public void attack(Character opponent) {
+    opponent.applyDamage(this.strength);
+  }
+
+}
