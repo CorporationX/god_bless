19163502d10c65f565e35462368c69cc94a83a84
nataliiaiallina <natalia.iallina@gmail.com>
2025-09-19 14:34:18 -0400

    Java: Add Catching Events problem solution

---
+++ src/main/java/school/faang/sprint1/catchevent/SpellEvent.java
@@ -0,0 +1,19 @@
+package school.faang.sprint1.catchevent;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import lombok.ToString;
+
+/**
+ * Represents a magical spell event in Hogfwarts.
+ */
+@RequiredArgsConstructor
+@ToString
+@Getter
+public class SpellEvent {
+
+  private final int id;
+  private final String eventType;
+  private final String action;
+
+}
