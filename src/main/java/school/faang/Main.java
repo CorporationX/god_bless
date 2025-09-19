e793de1c5ea105b119df88ac9ce322b6cd729839
nthmn666 <nthmn666@gmail.com>
2025-09-19 21:18:28 +0500

    hobby

--- src/main/java/school/faang/Main.java
+++ src/main/java/school/faang/Main.java
@@ -4,10 +4,10 @@ import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
-        Set<String> hob = new HashSet<String>();
+        Set<String> hob = new HashSet<>();
         hob.add("play");
         hob.add("swim");
-        Set<String> hob1 = new HashSet<String>();
+        Set<String> hob1 = new HashSet<>();
         hob.add("read");
         hob.add("learn");
         User Nariman=new User(1, "Nariman", 24, hob);
@@ -17,7 +17,7 @@ public class Main {
         users.add(Nariman);
         users.add(Era);
         users.add(Shifu);
-        Set<String> hob2 = new HashSet<String>();
+        Set<String> hob2 = new HashSet<>();
         hob2.add("play");
         Map<String,String> map=User.findHobbyLovers(users, hob2);
         map.forEach((name,hobby)->
