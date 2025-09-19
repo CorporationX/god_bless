84e3fe3a6f99d5e425aeb819bb454868c3b3f7c7
Abzal Samekeyev <samekeyev@gmail.com>
2025-09-19 21:04:28 +0500

    Также преобразовал класс в рекорд, как в предыдущей задачке Lord of the Rings

--- src/main/java/bjs2_91911/Main.java
+++ src/main/java/bjs2_91911/Main.java
@@ -10,30 +10,30 @@ public class Main {
 
         // Определяем фильтры как лямбда-функции
         Function<Image, Image> grayscaleFilter =
-                (image) -> new Image(image.getName(), image.getDescription() + " | Фильтр: черно-белый");
+                (image) -> new Image(image.name(), image.description() + " | Фильтр: черно-белый");
 
         Function<Image, Image> sepiaFilter =
-                (image) -> new Image(image.getName(), image.getDescription() + " | Фильтр: сепия");
+                (image) -> new Image(image.name(), image.description() + " | Фильтр: сепия");
 
         Function<Image, Image> vignetteFilter =
-                (image) -> new Image(image.getName(), image.getDescription() + " | Фильтр: виньетка");
+                (image) -> new Image(image.name(), image.description() + " | Фильтр: виньетка");
 
         // Применение отдельных фильтров
         Image grayscaleImage = filterProcessor.applyFilter(originalImage, grayscaleFilter);
-        System.out.println(grayscaleImage.getDescription());
+        System.out.println(grayscaleImage.description());
 
         Image sepiaImage = filterProcessor.applyFilter(grayscaleImage, sepiaFilter);
-        System.out.println(sepiaImage.getDescription());
+        System.out.println(sepiaImage.description());
 
         // Комбинация фильтров (черно-белый + сепия)
         Function<Image, Image> combinedFilter = filterProcessor.combineFilters(grayscaleFilter, sepiaFilter);
         Image combinedImage = filterProcessor.applyFilter(originalImage, combinedFilter);
-        System.out.println(combinedImage.getDescription());
+        System.out.println(combinedImage.description());
 
         // Комбинация трёх фильтров
         Function<Image, Image> tripleFilter = filterProcessor
                 .combineFilters(grayscaleFilter, sepiaFilter.andThen(vignetteFilter));
         Image tripleImage = filterProcessor.applyFilter(originalImage, tripleFilter);
-        System.out.println(tripleImage.getDescription());
+        System.out.println(tripleImage.description());
     }
 }
