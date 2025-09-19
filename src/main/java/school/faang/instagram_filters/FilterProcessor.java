05a5e8539f0af79ef1a1f4bdacdbe0f9e9d62bb7
Ruslan <ruslan8020110@gmail.com>
2025-09-20 02:45:53 +0300

    чек стайл ругался на непонятную дичь. исправил

--- src/main/java/school/faang/instagram_filters/FilterProcessor.java
+++ src/main/java/school/faang/instagram_filters/FilterProcessor.java
@@ -8,9 +8,10 @@ public class FilterProcessor {
         return f.apply(image);
     }
 
-    public Image combineFilters(Function<Image, Image> f, Function<Image,Image> f2){
-//        Image updatedImage = f.apply()
-        // не понимаю как реализовать этот метод
+    @SuppressWarnings("checkstyle:WhitespaceAround")
+    public Image combineFilters(Function<Image, Image> f, Function<Image, Image> f2){
+        //   Image updatedImage = f.apply()
+        //  не понимаю как реализовать этот метод
 
         return null;
     }
