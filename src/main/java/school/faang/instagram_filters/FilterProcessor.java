df152475098b7a428118537cb5952c13fe6ab339
Ruslan <ruslan8020110@gmail.com>
2025-09-20 02:32:58 +0300

    Instagram_filters

---
+++ src/main/java/school/faang/instagram_filters/FilterProcessor.java
@@ -0,0 +1,17 @@
+package school.faang.instagram_filters;
+
+import java.util.function.Function;
+
+public class FilterProcessor {
+
+    public Image applyFilter(Image image, Function<Image, Image> f) {
+        return f.apply(image);
+    }
+
+    public Image combineFilters(Function<Image, Image> f, Function<Image,Image> f2){
+//        Image updatedImage = f.apply()
+        // не понимаю как реализовать этот метод
+
+        return null;
+    }
+}
