0addd777431eb64199fc512ad9c5414c18a09744
DeniAkhmadov <daglossteckin@gmail.com>
2025-09-20 01:05:54 +0300

    SuperCow! Users and the Boss

---
+++ src/main/java/school/faang/bjs2_90281/Boss.java
@@ -0,0 +1,33 @@
+package school.faang.bjs2_90281;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+public class Boss {
+    private static final int MAX_PLAYERS = 3;
+    private int currentPlayers;
+    private final Object lock = new Object();
+
+    public void joinBattle(Player player) {
+        synchronized (lock) {
+            while (currentPlayers >= MAX_PLAYERS) {
+                try {
+                    log.info("{} Ждёт свободный слот, чтобы присоединиться к бою", player.name());
+                    lock.wait();
+                } catch (InterruptedException e) {
+                    log.info("Меня прервали в процессе ожидания");
+                    Thread.currentThread().interrupt();
+                }
+            }
+            currentPlayers++;
+        }
+    }
+
+    public void leaveBattle(Player player) {
+        synchronized (lock) {
+            log.info("{} Выходит из боя и освобождает слот", player.name());
+            currentPlayers--;
+            lock.notifyAll();
+        }
+    }
+}
