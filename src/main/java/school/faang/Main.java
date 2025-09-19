d570e72a19263dbec3a7b8f6d4bcef958d1fce13
nthmn666 <nthmn666@gmail.com>
2025-09-19 21:14:48 +0500

    hobby

--- src/main/java/school/faang/Main.java
+++ src/main/java/school/faang/Main.java
@@ -1,11 +1,27 @@
 package school.faang;
 
-import java.util.ArrayList;
+import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
-        Example example = new Example();
-        ArrayList<Integer> list = new ArrayList<>();
-        System.out.println(example.reverse(list));
+        Set<String> hob = new HashSet<String>();
+        hob.add("play");
+        hob.add("swim");
+        Set<String> hob1 = new HashSet<String>();
+        hob.add("read");
+        hob.add("learn");
+        User Nariman=new User(1, "Nariman", 24, hob);
+        User Era=new User(2, "Era", 25, hob1);
+        User Shifu=new User(3, "Shifu", 27, hob);
+        ArrayList<User> users=new ArrayList<>();
+        users.add(Nariman);
+        users.add(Era);
+        users.add(Shifu);
+        Set<String> hob2 = new HashSet<String>();
+        hob2.add("play");
+        Map<String,String> map=User.findHobbyLovers(users, hob2);
+        map.forEach((name,hobby)->{
+            System.out.println("name: "+name+ "   hobby: " + hobby);
+        });
     }
 }
---
+++ src/main/java/school/faang/User.java
@@ -0,0 +1,38 @@
+package school.faang;
+import lombok.Getter;
+import lombok.Setter;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
+
+@Getter
+@Setter
+
+public class User {
+    private int id;
+    private String name;
+    private int age;
+    private Set<String> hobbies;
+
+    public User(int id, String name, int age, Set<String> hobbies) {
+        this.id = id;
+        this.name = name;
+        this.age = age;
+        this.hobbies = hobbies;
+    }
+    public static Map<String, String> findHobbyLovers(ArrayList<User> users, Set<String> hobbies) {
+        Map<String, String> map = new HashMap<>();
+        for(int i=0;i<users.size();i++){
+            for (String hobby:users.get(i).getHobbies()){
+                if (hobbies.contains(hobby)){
+                    map.put(users.get(i).getName(), hobby);
+                }
+            }
+        }
+        return map;
+    }
+    public String toString() {
+        return "User{" + "id=" + id + ", name='" + name + '\'' + ", age=" + age + '}';
+    }
+}
