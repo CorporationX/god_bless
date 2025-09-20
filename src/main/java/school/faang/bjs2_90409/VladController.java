0ce3ddbb8c5c74e842d4e7e873540b6122abde47
Rukk Taras <tarrukk@gmail.com>
2025-09-20 13:20:18 +0300

    Variant 1

--- src/main/java/school/faang/bjs2_90409/VladController.java
+++ src/main/java/school/faang/bjs2_90409/VladController.java
@@ -1,36 +1,34 @@
 package school.faang.bjs2_90409;
 
+import lombok.AllArgsConstructor;
+
 import java.util.List;
 
+@AllArgsConstructor
 public class VladController {
     List<TamagotchiVlad> tamagochis;
 
-    public void feedAll() {
+    public void feedAll() throws InterruptedException {
         for (TamagotchiVlad tamagochi : tamagochis) {
             tamagochi.feed();
         }
     }
 
-    public void playAll() {
+    public void playAll() throws InterruptedException {
         for (TamagotchiVlad tamagochi : tamagochis) {
             tamagochi.play();
         }
     }
 
-    public void cleanAll() {
+    public void cleanAll() throws InterruptedException {
         for (TamagotchiVlad tamagochi : tamagochis) {
             tamagochi.clean();
         }
     }
 
-    public void sleepAll() {
+    public void sleepAll() throws InterruptedException {
         for (TamagotchiVlad tamagochi : tamagochis) {
             tamagochi.sleep();
         }
     }
-
-    //Дальше делаем пункт 3. Но в конце надо будет все-таки все пункты перечитать, вдруг на что не обратил
-    //внимание.
-
-
 }
