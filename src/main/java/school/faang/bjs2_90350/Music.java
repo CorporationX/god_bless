e3cb3ecd9ced9fdb0e1c494d07ad518fb9a9115e
Oleg_Korovin <olegk.99@mail.ru>
2025-09-20 12:10:16 +0300

    BJS2-90350/fix#1

--- src/main/java/school/faang/bjs2_90350/Music.java
+++ src/main/java/school/faang/bjs2_90350/Music.java
@@ -1,9 +1,5 @@
 package school.faang.bjs2_90350;
 
-
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
-
 public class Music {
     public static void main(String[] args) {
         Player player = new Player();
