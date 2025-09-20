f6e0c5ac7283838d18d2dba763b7f98a9298eec3
brocksvar <saskeltz@mail.ru>
2025-09-20 09:41:51 +0300

    BJS2-91319

---
+++ src/main/java/school/faang/m3_1_parallelism/mightandmagic/Character.java
@@ -0,0 +1,12 @@
+package school.faang.m3_1_parallelism.mightandmagic;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import lombok.ToString;
+
+@Getter
+@ToString
+@RequiredArgsConstructor
+public abstract class Character {
+    private final int power;
+}
---
+++ src/main/java/school/faang/m3_1_parallelism/mightandmagic/Swordsman.java
@@ -0,0 +1,8 @@
+package school.faang.m3_1_parallelism.mightandmagic;
+
+public class Swordsman extends Character {
+
+    public Swordsman(int power) {
+        super(power);
+    }
+}
