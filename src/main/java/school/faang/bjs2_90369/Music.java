9ab4b040c611e47cf83362c9e3932b926831fb9e
Elya Khamzina <ekkhamzi@mail.ru>
2025-09-21 00:19:19 +0300

    completed Блокировка Spotify

---
+++ src/main/java/school/faang/bjs2_90369/Music.java
@@ -0,0 +1,27 @@
+package school.faang.bjs2_90369;
+
+public class Music {
+    public static void main(String[] args) {
+        Player player = new Player();
+
+        Thread playThread = new Thread(player::play);
+        Thread pauseThread = new Thread(player::pause);
+        Thread skipThread = new Thread(player::skip);
+        Thread previousThread = new Thread(player::previous);
+
+        playThread.start();
+        pauseThread.start();
+        skipThread.start();
+        previousThread.start();
+
+        try {
+            playThread.join();
+            pauseThread.join();
+            skipThread.join();
+            previousThread.join();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        System.out.println("Все операции выполнены");
+    }
+}
---
+++ src/main/java/school/faang/bjs2_90369/Player.java
@@ -0,0 +1,44 @@
+package school.faang.bjs2_90369;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+public class Player {
+    private final Object lock = new Object();
+    private boolean isPlaying = false;
+    private int currentTrack = 1;
+    private final int totalTracks = 10;
+
+    public void play() {
+        synchronized (lock) {
+            if (!isPlaying) {
+                isPlaying = true;
+                log.info("Музыка воспроизводится.");
+            } else {
+                log.info("Музыка уже играет.");
+            }
+        }
+    }
+    public void pause() {
+        synchronized (lock) {
+            if (isPlaying) {
+                isPlaying = false;
+                log.info("Музыка поставлена на паузу.");
+            } else {
+                log.info("Музыка уже на паузе.");
+            }
+        }
+    }
+
+    public void skip() {
+        synchronized (lock) {
+            log.info("Трек пропущен.");
+        }
+    }
+
+    public void previous() {
+        synchronized (lock) {
+            log.info("Вернулись к предыдущему треку.");
+        }
+    }
+}
