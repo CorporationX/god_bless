735529e9329d34acc26f8f69800c90b7fe7cd02e
AndreySN7 <andrey__n@mail.ru>
2025-09-20 22:14:05 +0300

    Решил задачу: Счетчик просмотров на YouTube

---
+++ src/main/java/school/faang/multithreading/sinchronized/bjs2_90044/VideoManager.java
@@ -0,0 +1,20 @@
+package school.faang.multithreading.sinchronized.bjs2_90044;
+
+import lombok.extern.slf4j.Slf4j;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@Slf4j
+public class VideoManager {
+    private final Map<String, Integer> viewsMap = new HashMap<>();
+
+    public synchronized void addView(String videoId) {
+        viewsMap.merge(videoId, 1, Integer::sum);
+        log.info("Количество просмотров '{}' увеличено на 1, всего просмотров: {}", videoId, getViewCount(videoId));
+    }
+
+    public synchronized int getViewCount(String videoId) {
+        return viewsMap.get(videoId);
+    }
+}
