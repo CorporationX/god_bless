5868359e4ce3f1df5e0ca4acc3691acf89e03193
svxrslf <svxrslf@yandex.ru>
2025-09-20 20:25:08 +0300

    88229

--- src/main/java/school/faang/bjs2_88229/Runner.java
+++ src/main/java/school/faang/bjs2_88229/Runner.java
@@ -60,6 +60,7 @@ public class Runner {
                 new UserAction(2, "Bob", ActionType.POST, LocalDate.of(2025, 10, 18), "Can't wait for the next #update."),
                 new UserAction(4, "Dave", ActionType.SHARE, LocalDate.of(2025, 10, 19), "")
         );
+
         UserActionAnalyser analyser = new UserActionAnalyser();
         log.info("Топ-5 активных пользователей: {}", analyser.topActiveUsers(actions, 5));
         log.info("Топ-5 обсуждаемых хештегов: {}", analyser.topUsableHashtags(actions, 5));
