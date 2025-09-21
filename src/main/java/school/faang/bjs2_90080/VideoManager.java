816bb355ecc4a6cfec218b2a748b7df08c1d2005
Lowdude1 <sanitaronlain@gmail.com>
2025-09-22 01:48:29 +0300

    Исправил замечания

--- src/main/java/school/faang/bjs2_90080/VideoManager.java
+++ src/main/java/school/faang/bjs2_90080/VideoManager.java
@@ -7,8 +7,8 @@ public class VideoManager {
     private final Map<String, Integer> viewsMap = new HashMap<>();
 
     public synchronized void addView(String videoId) {
-            viewsMap.put(videoId, viewsMap.getOrDefault(videoId, 0) + 1);
-        }
+        viewsMap.put(videoId, viewsMap.getOrDefault(videoId, 0) + 1);
+    }
 
     public int getViewCount(String videoId) {
         return viewsMap.getOrDefault(videoId, 0);
