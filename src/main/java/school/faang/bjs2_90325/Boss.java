a622312303050fc1d5be746cb4091495f58abf1c
Lowdude1 <sanitaronlain@gmail.com>
2025-09-22 02:01:46 +0300

    Исправил замечания

--- src/main/java/school/faang/bjs2_90325/Boss.java
+++ src/main/java/school/faang/bjs2_90325/Boss.java
@@ -10,28 +10,23 @@ import lombok.extern.slf4j.Slf4j;
 public class Boss {
     private final int maxPlayers;
     private int currentPlayers;
-    private final Object lock = new Object();
 
-    public void joinBattle(Player player) throws InterruptedException {
-        synchronized (lock) {
-            if (currentPlayers < maxPlayers) {
-                currentPlayers++;
-                System.out.printf("%s присоединился к битве. Игроков: %d/%d%n",
-                        player.getName(), currentPlayers, maxPlayers);
-            } else if (currentPlayers == maxPlayers) {
-                lock.wait();
-            }
+    public synchronized void joinBattle(Player player) throws InterruptedException {
+        if (currentPlayers < maxPlayers) {
+            currentPlayers++;
+            System.out.printf("%s присоединился к битве. Игроков: %d/%d%n",
+                    player.getName(), currentPlayers, maxPlayers);
+        } else if (currentPlayers == maxPlayers) {
+            this.wait();
         }
     }
 
-    public void leaveBattle(Player player) throws InterruptedException {
-        synchronized (lock) {
-            if (currentPlayers > 0) {
-                currentPlayers--;
-                System.out.printf("%s вышел из битвы. Игроков: %d/%d%n",
-                        player.getName(), currentPlayers, maxPlayers);
-                lock.notify();
-            }
+    public synchronized void leaveBattle(Player player) throws InterruptedException {
+        if (currentPlayers > 0) {
+            currentPlayers--;
+            System.out.printf("%s вышел из битвы. Игроков: %d/%d%n",
+                    player.getName(), currentPlayers, maxPlayers);
+            this.notifyAll();
         }
     }
 }
