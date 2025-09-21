4e824c01c828163b742445cd49eda72cf90a0752
Pavel-Lukyanov <PavelLukyanov2017@yandex.ru>
2025-09-21 17:39:21 +0500

    BJS2-90054

--- src/main/java/school/faang/bjs2_90054/VideoManager.java
+++ src/main/java/school/faang/bjs2_90054/VideoManager.java
@@ -4,13 +4,15 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class VideoManager {
+    private static final int DEFAULT_COUNT_VIDEO = 0;
+    private static final int INCREMENT_VIDEO = 1;
     private final Map<String, Integer> viewsMap = new HashMap<>();
 
     public synchronized void addView(String videoId) {
-        viewsMap.put(videoId, viewsMap.getOrDefault(videoId, 0) + 1);
+        viewsMap.put(videoId, viewsMap.getOrDefault(videoId, DEFAULT_COUNT_VIDEO) + INCREMENT_VIDEO);
     }
 
     public synchronized int getViewCount(String videoId) {
-        return viewsMap.getOrDefault(videoId, 0);
+        return viewsMap.getOrDefault(videoId, DEFAULT_COUNT_VIDEO);
     }
 }
