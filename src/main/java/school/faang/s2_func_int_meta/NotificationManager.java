11e48b44c8bea7c2a92527cb797e42893fe85938
pinchalex <pinchalex484@gmail.com>
2025-09-21 14:53:06 +0300

    Исправление после автотестов

--- src/main/java/school/faang/s2_func_int_meta/NotificationManager.java
+++ src/main/java/school/faang/s2_func_int_meta/NotificationManager.java
@@ -5,11 +5,12 @@ import java.util.Map;
 import java.util.function.Consumer;
 
 public class NotificationManager {
-    private Map<NotificationType,Consumer<Notification>> handlers = new HashMap<>();
+    private Map<NotificationType, Consumer<Notification>> handlers = new HashMap<>();
 
     public void registerHandler(NotificationType type, Consumer<Notification> handler) {
         handlers.put(type, handler);
     }
+
     public void sendNotification(Notification notification) {
         if (handlers.containsKey(notification.getType())) {
             handlers.get(notification.getType()).accept(notification);
