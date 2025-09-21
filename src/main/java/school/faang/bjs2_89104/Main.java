84d5315b8d039295c55c85f36ffb560713f6055b
K0I2T <hiro94337@gmail.com>
2025-09-21 17:01:06 +0300

    BJS2-89104 исправление checkstyle

--- src/main/java/school/faang/bjs2_89104/Main.java
+++ src/main/java/school/faang/bjs2_89104/Main.java
@@ -8,9 +8,12 @@ public class Main {
     public static void main(String[] args) {
         NotificationManager notificationManager = new NotificationManager();
 
-        notificationManager.registerHandler(SMS, notification -> System.out.println("SMS: " + notification.getMessage()));
-        notificationManager.registerHandler(PUSH, notification -> System.out.println("PUSH: " + notification.getMessage()));
-        notificationManager.registerHandler(EMAIL, notification -> System.out.println("EMAIL: " + notification.getMessage()));
+        notificationManager.registerHandler(SMS, notification ->
+                System.out.println("SMS: " + notification.getMessage()));
+        notificationManager.registerHandler(PUSH, notification ->
+                System.out.println("PUSH: " + notification.getMessage()));
+        notificationManager.registerHandler(EMAIL, notification ->
+                System.out.println("EMAIL: " + notification.getMessage()));
 
         Notification emailNotification = new Notification(EMAIL, "Your account is activated");
         Notification smsNotification = new Notification(SMS, "Your password has been changed");
