0f843e18c35f375fc40157a02309672cd6aef106
DeniAkhmadov <daglossteckin@gmail.com>
2025-09-21 21:12:35 +0300

    Google photo sync - UPDATED

--- src/main/java/school/faang/bjs2_90393/GooglePhotosAutoUploader.java
+++ src/main/java/school/faang/bjs2_90393/GooglePhotosAutoUploader.java
@@ -7,32 +7,27 @@ import java.util.List;
 
 @Slf4j
 public class GooglePhotosAutoUploader {
-    private final Object lock = new Object();
     private final List<String> photosToUpload = new ArrayList<>();
 
-    public void startAutoUpload() {
-        synchronized (lock) {
-            while (photosToUpload.isEmpty()) {
-                try {
-                    lock.wait();
-                } catch (InterruptedException e) {
-                    log.info("Меня прервали во время ожидания");
-                    Thread.currentThread().interrupt();
-                }
+    public synchronized void startAutoUpload() {
+        while (photosToUpload.isEmpty()) {
+            try {
+                this.wait();
+                log.info("Отправляю фотографии на сервер...");
+                uploadPhotos();
+            } catch (InterruptedException e) {
+                log.info("Меня прервали во время ожидания");
+                Thread.currentThread().interrupt();
             }
-            log.info("Отправляю фотографии на сервер...");
-            uploadPhotos();
         }
     }
 
-    public void onNewPhotoAdded(String photoPath) {
-        synchronized (lock) {
-            photosToUpload.add(photoPath);
-            lock.notify();
-        }
+    public synchronized void onNewPhotoAdded(String photoPath) {
+        photosToUpload.add(photoPath);
+        this.notify();
     }
 
-    public void uploadPhotos() {
+    private void uploadPhotos() {
         for (String photo : photosToUpload) {
             log.info("На сервер отправлена фотография {}", photo);
         }
