686d6babc985ad149c0890ae877d7b91ce2645b3
teymuruae <baratov.teymur@yandex.ru>
2025-09-21 20:52:11 +0300

    BJS2-90441 Google Photo sync

---
+++ src/main/java/school/faang/multithreading/synchronize/bjs2_90441/GooglePhotosAutoUploader.java
@@ -0,0 +1,42 @@
+package school.faang.multithreading.synchronize.bjs2_90441;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+public class GooglePhotosAutoUploader {
+
+    private final List<String> photosToUpload = new ArrayList<>();
+
+    public void startAutoUpload() {
+        synchronized (photosToUpload) {
+            if (photosToUpload.isEmpty()) {
+                try {
+                    photosToUpload.wait();
+                } catch (InterruptedException e) {
+                    Thread.currentThread().interrupt();
+                }
+                uploadPhotos();
+            }
+        }
+    }
+
+    public void onNewPhotoAdded(String photoPath) {
+        synchronized (photosToUpload) {
+            photosToUpload.add(photoPath);
+            System.out.println("Фотографии добавлены в список");
+            photosToUpload.notify();
+        }
+    }
+
+    private void uploadPhotos() {
+        synchronized (photosToUpload) {
+            System.out.println("Фотографии загружены на сервер");
+            Iterator<String> iterator = photosToUpload.iterator();
+            while (iterator.hasNext()) {
+                iterator.next();
+                iterator.remove();
+            }
+        }
+    }
+}
