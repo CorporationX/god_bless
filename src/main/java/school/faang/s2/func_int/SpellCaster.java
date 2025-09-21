9b1ec0c1a636409d4e8f1da01fc82097bf3a66e4
pinchalex <pinchalex484@gmail.com>
2025-09-21 14:20:55 +0300

    Исправил после ревью.

--- src/main/java/school/faang/s2/func_int/SpellCaster.java
+++ src/main/java/school/faang/s2/func_int/SpellCaster.java
@@ -3,6 +3,6 @@ package school.faang.s2.func_int;
 public class SpellCaster {
     public void cast(String spellName, SpellAction action) {
         String result = action.castSpell(spellName);
-        System.out.println("Результат заклинания: " + result);
+        System.out.printf("Результат заклинания: %s%n", result);
     }
 }
