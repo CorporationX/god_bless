902c021d500d93dac6937d8b884bee7923f9cae2
teymuruae <baratov.teymur@yandex.ru>
2025-09-21 11:00:21 +0300

    BJS2-89296 Потренируем StreamAPI 3 fixed

--- src/main/java/school/faang/stream_optional/bjs2_89296/Operations.java
+++ src/main/java/school/faang/stream_optional/bjs2_89296/Operations.java
@@ -1,6 +1,5 @@
 package school.faang.stream_optional.bjs2_89296;
 
-import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.List;
@@ -15,18 +14,19 @@ public class Operations {
         final Map<String, Set<String>> notFriendsMap = new HashMap<>();
 
         personFriendsMap.entrySet()
-                .forEach(personFriendsMapEntry1 -> {
-                    for (Map.Entry<String, List<String>> personFriendsMapEntry2 : personFriendsMap.entrySet()) {
-                        if (personFriendsMapEntry2.getKey().equals(personFriendsMapEntry1.getKey())
-                                && personFriendsMapEntry2.getValue().contains(personFriendsMapEntry1.getKey())) {
+                .forEach(personFriendsMapEntryMain -> {
+                    for (Map.Entry<String, List<String>> personFriendsMapEntryForFilter : personFriendsMap.entrySet()) {
+                        if (personFriendsMapEntryForFilter.getKey().equals(personFriendsMapEntryMain.getKey())
+                                && personFriendsMapEntryForFilter
+                                .getValue().contains(personFriendsMapEntryMain.getKey())) {
                             continue;
                         }
 
-                        personFriendsMapEntry2.getValue().forEach(
+                        personFriendsMapEntryForFilter.getValue().forEach(
                                 friend -> {
-                                    if (!personFriendsMapEntry1.getValue().contains(friend)
+                                    if (!personFriendsMapEntryMain.getValue().contains(friend)
                                     ) {
-                                        notFriendsMap.computeIfAbsent(personFriendsMapEntry1.getKey(),
+                                        notFriendsMap.computeIfAbsent(personFriendsMapEntryMain.getKey(),
                                                 key -> new HashSet<>()).add(friend);
                                     }
                                 });
@@ -49,50 +49,34 @@ public class Operations {
     }
 
     public static Map<String, Double> getAvgSalaryByDepartment(List<Employee> employees) {
-        final Map<String, Double> depAvgSalaryMap = new HashMap<>();
-
-        employees.stream()
-                .collect(Collectors.groupingBy(Employee::department))
-                .entrySet()
-                .forEach(entry -> {
-
-                    final double avgSalary = entry.getValue().stream()
-                            .mapToInt(Employee::salary)
-                            .average()
-                            .orElseThrow();
-
-                    depAvgSalaryMap.put(entry.getKey(), avgSalary);
-                });
-
-        return depAvgSalaryMap;
+        return employees.stream()
+                .collect(Collectors.groupingBy(
+                        Employee::department,
+                        Collectors.averagingDouble(Employee::salary)
+                ));
     }
 
     public static List<Integer> getPalindromeBetweenTwoInts(int start, int end) {
-        final List<Integer> intList = IntStream
+        return IntStream
                 .rangeClosed(start, end).boxed()
-                .toList();
-
-        return intList.stream()
                 .filter(i -> {
-                    final StringBuilder strBuilder = new StringBuilder(String.valueOf(i));
-                    return Integer.parseInt(strBuilder.reverse().toString()) == i;
-                })
-                .toList();
+                    String str = String.valueOf(i);
+                    return str.contentEquals(new StringBuilder(str).reverse());
+                }).toList();
     }
 
-    public static List<String> getPalindromesFromString(String strForPalindrome) {
-        final List<String> listForSubstrings = new ArrayList<>();
+    public static Set<String> getPalindromesFromString(String strForPalindrome) {
+        final Set<String> listForSubstrings = new HashSet<>();
 
         for (int i = 0; i < strForPalindrome.length(); i++) {
-            for (int j = 1; j <= strForPalindrome.length() - i; j++) {
-                listForSubstrings.add(strForPalindrome.substring(i, j + i));
+            for (int j = i + 1; j <= strForPalindrome.length(); j++) {
+                String subString = strForPalindrome.substring(i, j);
+                if (subString.contentEquals(new StringBuilder(subString).reverse())) {
+                    listForSubstrings.add(subString);
+                }
             }
         }
-
-        return listForSubstrings.stream()
-                .filter(str -> new StringBuilder(str).reverse().toString().equals(str))
-                .distinct()
-                .toList();
+        return listForSubstrings;
     }
 
     public static List<Integer> getPerfectNumbers(int start, int end) {
