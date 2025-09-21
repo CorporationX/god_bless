ff184fb4f248622be948dccb1f7a589bab64327d
Oleg_Korovin <olegk.99@mail.ru>
2025-09-21 16:48:04 +0300

    BJS2-92400

---
+++ src/main/java/school/faang/bjs2_92400/User.java
@@ -0,0 +1,27 @@
+package school.faang.bjs2_92400;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.extern.slf4j.Slf4j;
+
+@AllArgsConstructor
+@Getter
+@Slf4j
+public class User {
+    private String name;
+    private String assignedRole;
+
+    public void joinHouse(House house) {
+        try {
+            house.assignRole(this);
+        } catch (InterruptedException e) {
+            log.error(e.getMessage());
+        }
+        log.info("Пользователь {} присоединился к {} как {}", name, house.getName(), assignedRole);
+    }
+
+    public void leaveHouse(House house) {
+        house.releaseRole(this);
+        log.info("Пользователь {} покинул {}", name, house.getName());
+    }
+}
