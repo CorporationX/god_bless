4a983b997902fdb9641d4b77114a456d976c6063
Alexey Ulov <ulov.alexey@gmail.com>
2025-09-22 13:58:55 +0300

    refactoring

--- src/main/java/school/faang/bjs2_88858/CsvConverter.java
+++ src/main/java/school/faang/bjs2_88858/CsvConverter.java
@@ -3,15 +3,16 @@ package school.faang.bjs2_88858;
 import java.util.List;
 
 public class CsvConverter {
+    private static final VectorJoiner<String> VECTOR_JOINER =
+            list -> String.join(", ", list);
+
+    private static final MatrixJoiner<String> MATRIX_JOINER =
+            matrix -> String.join("\n",
+                    matrix.stream()
+                            .map(VECTOR_JOINER::join)
+                            .toList());
 
     public static String toCsv(List<List<String>> table) {
-        VectorJoiner<String> vectorJoiner = list -> String.join(", ", list);
-
-        MatrixJoiner<String> matrixJoiner =
-                matrix -> String.join("\n", matrix.stream()
-                        .map(vectorJoiner::join)
-                        .toList());
-
-        return matrixJoiner.join(table);
+        return MATRIX_JOINER.join(table);
     }
 }
