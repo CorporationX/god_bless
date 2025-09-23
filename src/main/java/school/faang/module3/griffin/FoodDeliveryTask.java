1d668b1b0cd4636aa89c34248ad53c67aa170437
mipkasnow <steamer08@mail.ru>
2025-09-23 10:03:28 +0300

    fix

--- src/main/java/school/faang/module3/griffin/FoodDeliveryTask.java
+++ src/main/java/school/faang/module3/griffin/FoodDeliveryTask.java
@@ -1,11 +1,17 @@
 package school.faang.module3.griffin;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 
 import java.util.Random;
 
 @RequiredArgsConstructor
+@Slf4j
 public class FoodDeliveryTask implements Runnable {
+    private static final int LOWER_BOUND_DELAY = 1;
+    private static final int UPPER_BOUND_DELAY = 1;
+    private static final long ONE_SEC_IN_MILLIS = 1000;
+
     private final String character;
     private final int foodAmount;
     private final Random random = new Random();
@@ -13,14 +19,14 @@ public class FoodDeliveryTask implements Runnable {
     @Override
     public void run() {
         FoodType rndFood = getFoodType();
-        System.out.printf("%s получает %d %s%n", character, foodAmount, rndFood);
+        log.info("{} получает {} {}", character, foodAmount, rndFood);
         try {
             Thread.sleep(getDelay());
         } catch (InterruptedException e) {
             Thread.currentThread().interrupt();
-            System.out.printf("%s was interrupted%n", Thread.currentThread().getName());
+            log.error("Thread was interrupted", e);
         }
-        System.out.printf("%s съел %d %s%n", character, foodAmount, rndFood);
+        log.info("{} съел {} {}", character, foodAmount, rndFood);
     }
 
     private FoodType getFoodType() {
@@ -29,6 +35,6 @@ public class FoodDeliveryTask implements Runnable {
     }
 
     private long getDelay() {
-        return random.nextInt(1, 5) * 1000L;
+        return random.nextInt(LOWER_BOUND_DELAY, UPPER_BOUND_DELAY) * ONE_SEC_IN_MILLIS;
     }
 }
--- src/main/java/school/faang/module3/griffin/GriffinsFoodDelivery.java
+++ src/main/java/school/faang/module3/griffin/GriffinsFoodDelivery.java
@@ -1,20 +1,37 @@
 package school.faang.module3.griffin;
 
+import lombok.extern.slf4j.Slf4j;
+
 import java.util.Random;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
 
+@Slf4j
 public class GriffinsFoodDelivery {
+    private static final int LOWER_BOUND_FOOD_AMOUNT = 1;
+    private static final int UPPER_BOUND_FOOD_AMOUNT = 50;
+    private static final int THREAD_COUNT = 3;
+
     private static final Random random = new Random();
 
     public static void main(String[] args) {
-        ExecutorService executor = Executors.newFixedThreadPool(3);
+        ExecutorService executor = Executors.newFixedThreadPool(THREAD_COUNT);
         String[] characterNames = {"Peter", "Lois", "Meg", "Chris", "Stewie"};
 
         for (String character : characterNames) {
-            executor.submit(new FoodDeliveryTask(character,  random.nextInt(1, 50)));
+            executor.submit(new FoodDeliveryTask(
+                    character, random.nextInt(LOWER_BOUND_FOOD_AMOUNT, UPPER_BOUND_FOOD_AMOUNT))
+            );
         }
 
         executor.shutdown();
+
+        try {
+            executor.awaitTermination(10, TimeUnit.SECONDS);
+        } catch (InterruptedException e) {
+            Thread.currentThread().interrupt();
+            log.error("Thread was interrupted", e);
+        }
     }
 }
