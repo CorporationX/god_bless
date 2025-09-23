06806960bfe87f6daea5c4154c58f2d2ec5491fc
Alexey Ulov <ulov.alexey@gmail.com>
2025-09-23 15:22:58 +0300

    refactoring

--- src/main/java/school/faang/bjs2_91177/MailSender.java
+++ src/main/java/school/faang/bjs2_91177/MailSender.java
@@ -1,35 +1,24 @@
 package school.faang.bjs2_91177;
 
 public class MailSender {
-    @SuppressWarnings("checkstyle:VariableDeclarationUsageDistance")
-    public static void main(String[] args) {
-        SenderRunnable pack1 = new SenderRunnable(0, 200);
-        SenderRunnable pack2 = new SenderRunnable(200, 400);
-        SenderRunnable pack3 = new SenderRunnable(400, 600);
-        SenderRunnable pack4 = new SenderRunnable(600, 800);
-        SenderRunnable pack5 = new SenderRunnable(800, 1000);
+    private static final int EMAILS_COUNT = 1000;
+    private static final int THREADS_COUNT = 5;
+    private static final int BATCH_SIZE = EMAILS_COUNT / THREADS_COUNT;
 
-        Thread thread1 = new Thread(pack1);
-        Thread thread2 = new Thread(pack2);
-        Thread thread3 = new Thread(pack3);
-        Thread thread4 = new Thread(pack4);
-        Thread thread5 = new Thread(pack5);
+    public static void main(String[] args) throws InterruptedException {
+        Thread[] threads = new Thread[THREADS_COUNT];
 
-        thread1.start();
-        thread2.start();
-        thread3.start();
-        thread4.start();
-        thread5.start();
-
-        try {
-            thread1.join();
-            thread2.join();
-            thread3.join();
-            thread4.join();
-            thread5.join();
-            System.out.println("Письма отправлены.");
-        } catch (InterruptedException e) {
-            throw new RuntimeException(e);
+        for (int i = 0; i < THREADS_COUNT; i++) {
+            int start = i * BATCH_SIZE;
+            int end = (i + 1) * BATCH_SIZE;
+            threads[i] = new Thread(new SenderRunnable(start, end));
+            threads[i].start();
         }
+
+        for (Thread thread : threads) {
+            thread.join();
+        }
+
+        System.out.println("Письма отправлены.");
     }
 }
