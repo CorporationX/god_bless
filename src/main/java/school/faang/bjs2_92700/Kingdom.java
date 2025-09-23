c47999fdb0f87d274f5d5630c0b1bf48b711430d
Rukk Taras <tarrukk@gmail.com>
2025-09-23 16:34:59 +0300

    Variant 1

--- src/main/java/school/faang/bjs2_92700/Kingdom.java
+++ src/main/java/school/faang/bjs2_92700/Kingdom.java
@@ -2,9 +2,11 @@ package school.faang.bjs2_92700;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.Setter;
 
 @AllArgsConstructor
 @Getter
+@Setter
 public class Kingdom {
     private String name;
 }
