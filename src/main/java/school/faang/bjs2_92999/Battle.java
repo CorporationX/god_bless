a543a239e1e28fa298c7c59be1a26f8d5ec6903c
Rukk Taras <tarrukk@gmail.com>
2025-09-23 14:03:35 +0300

    Variant 2

--- src/main/java/school/faang/bjs2_92999/Battle.java
+++ src/main/java/school/faang/bjs2_92999/Battle.java
@@ -5,14 +5,21 @@ import lombok.extern.slf4j.Slf4j;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.Future;
+import java.util.concurrent.TimeUnit;
 
 @Slf4j
 public class Battle {
-    ExecutorService executor = Executors.newCachedThreadPool();
+    static final int FIGHT_DURATION = 5;
+    private static final ExecutorService EXECUTOR = Executors.newCachedThreadPool();
 
     Future<Robot> fight(Robot robot1, Robot robot2) {
-        log.info("Начинается битва между роботами {} и {}", robot1.getName(), robot2.getName());
-        return executor.submit(() -> {
+        return EXECUTOR.submit(() -> {
+            log.info("Начинается битва между роботами {} и {}", robot1.getName(), robot2.getName());
+            try {
+                TimeUnit.SECONDS.sleep(FIGHT_DURATION);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
             if ((robot1.getAttackPower() - robot1.getDefensePower())
                     > (robot2.getAttackPower() - robot2.getDefensePower())) {
                 return robot1;
--- src/main/java/school/faang/bjs2_92999/Main.java
+++ src/main/java/school/faang/bjs2_92999/Main.java
@@ -1,7 +1,37 @@
 package school.faang.bjs2_92999;
 
-public class Main {
-    public static void main(String[] args) {
+import lombok.extern.slf4j.Slf4j;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.Future;
+
+@Slf4j
+public class Main {
+    public static void main(String[] args) throws ExecutionException, InterruptedException {
+        Battle battle = new Battle();
+
+        List<Future<Robot>> futuresBattles = List.of(
+                battle.fight(
+                        new Robot("T1000", 200, 15),
+                        new Robot("S1000", 300, 80)),
+                battle.fight(
+                        new Robot("B1000", 350, 150),
+                        new Robot("W1000", 180, 10)),
+                battle.fight(
+                        new Robot("D1000", 200, 99),
+                        new Robot("K1000", 300, 65)),
+                battle.fight(
+                        new Robot("L1000", 250, 44),
+                        new Robot("P1000", 300, 80))
+        );
+        List<Robot> winners = new ArrayList<>();
+        for (Future<Robot> futureWinner : futuresBattles) {
+            winners.add(futureWinner.get());
+        }
+        for (Robot winner : winners) {
+            log.info("Победил робот {}!", winner.getName());
+        }
     }
 }
