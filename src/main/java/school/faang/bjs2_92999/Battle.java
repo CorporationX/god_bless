f09e5854b28c490955589d1df12df581328fee0b
Rukk Taras <tarrukk@gmail.com>
2025-09-23 13:27:41 +0300

    Variant 2

--- src/main/java/school/faang/bjs2_92999/Battle.java
+++ src/main/java/school/faang/bjs2_92999/Battle.java
@@ -2,19 +2,23 @@ package school.faang.bjs2_92999;
 
 import lombok.extern.slf4j.Slf4j;
 
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 import java.util.concurrent.Future;
 
 @Slf4j
 public class Battle {
+    ExecutorService executor = Executors.newCachedThreadPool();
 
     Future<Robot> fight(Robot robot1, Robot robot2) {
         log.info("Начинается битва между роботами {} и {}", robot1.getName(), robot2.getName());
-        if ((robot1.getAttackPower() - robot1.getDefensePower())
-                > robot2.getAttackPower() - robot2.getDefensePower()) {
-            return robot1;
-
-        } else {
-            return robot2;
-        }
+        return executor.submit(() -> {
+            if ((robot1.getAttackPower() - robot1.getDefensePower())
+                    > (robot2.getAttackPower() - robot2.getDefensePower())) {
+                return robot1;
+            } else {
+                return robot2;
+            }
+        });
     }
 }
--- src/main/java/school/faang/bjs2_92999/Robot.java
+++ src/main/java/school/faang/bjs2_92999/Robot.java
@@ -1,7 +1,9 @@
 package school.faang.bjs2_92999;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 
+@AllArgsConstructor
 @Getter
 public class Robot {
     private String name;
