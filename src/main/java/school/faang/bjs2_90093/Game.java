289280a374b0df913dc468c77d088485f06a152b
YasinJD <yasin.akhmadov555@inbox.ru>
2025-09-23 19:11:38 +0300

    BroForce! (#21944)
    * BroForce!
    
    * BroForce! (after fixes)

---
+++ src/main/java/school/faang/bjs2_90093/Game.java
@@ -0,0 +1,43 @@
+package school.faang.bjs2_90093;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+public class Game {
+    private int score = 0;
+    private int lives;
+    public boolean isGameInProgress = true;
+
+    private final Object scoreLock = new Object();
+    private final Object livesLock = new Object();
+
+    public Game(int lives) {
+        this.lives = lives;
+    }
+
+    public void update(boolean isPlayerEarningPoints, boolean isPlayerLosingLive) {
+        if (isGameInProgress) {
+            if (isPlayerEarningPoints) {
+                synchronized (scoreLock) {
+                    score++;
+                    log.info("игрок зарабатывает очко -- текущие очки: {}", score);
+                }
+            }
+
+            if (isPlayerLosingLive && lives > 0) {
+                synchronized (livesLock) {
+                    lives--;
+                    log.info("игрок теряет жизнь -- текущие жизни: {}", lives);
+                }
+            }
+            if (lives <= 0) {
+                gameOver();
+            }
+        }
+    }
+
+    private void gameOver() {
+        isGameInProgress = false;
+        log.info("игрок потерял последнюю жизнь - игра окончена");
+    }
+}
