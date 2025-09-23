8540b61d016935f22faeb4b8ecf34a023b619451
Bekhter Roman <curses93@mail.ru>
2025-09-23 10:16:13 +0300

    BJS2-92542 fix

--- src/main/java/school/faang/bjs2_92542/Main.java
+++ src/main/java/school/faang/bjs2_92542/Main.java
@@ -15,6 +15,9 @@ public class Main {
         CompletableFuture<Player> player1Quest = questSystem.startQuest(player1, quest1);
         CompletableFuture<Player> player2Quest = questSystem.startQuest(player2, quest2);
 
+        player1Quest.join();
+        player2Quest.join();
+
         player1Quest.thenAccept(player ->
                 System.out.printf("%s has completed the quest and now has %s experience points.%n",
                         player.getName(), player.getExperience()));
--- src/main/java/school/faang/bjs2_92542/QuestSystem.java
+++ src/main/java/school/faang/bjs2_92542/QuestSystem.java
@@ -5,18 +5,18 @@ import java.util.concurrent.CompletableFuture;
 public class QuestSystem {
 
     public CompletableFuture<Player> startQuest(Player player, Quest quest) {
-        CompletableFuture<Player> future = CompletableFuture.supplyAsync(() -> {
-            try {
-                Thread.sleep(quest.getDifficulty() * 1000L);
-                int experience = player.getExperience() + quest.getReward();
-                player.setExperience(experience);
+        return CompletableFuture.supplyAsync(() -> {
+            synchronized (player) {
+                try {
+                    Thread.sleep(quest.getDifficulty() * 1000L);
+                    int experience = player.getExperience() + quest.getReward();
+                    player.setExperience(experience);
+                } catch (InterruptedException e) {
+                    Thread.currentThread().interrupt();
+                    throw new RuntimeException("Quest has been interrupted.");
+                }
                 return player;
-            } catch (InterruptedException e) {
-                Thread.currentThread().interrupt();
-                throw new RuntimeException("Quest has been interrupted.");
             }
         });
-        future.join();
-        return future;
     }
 }
