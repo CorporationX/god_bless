32982567c6ba2b4fe6584898d664636185e33374
DeniAkhmadov <daglossteckin@gmail.com>
2025-09-24 21:07:25 +0300

    UPDATED

--- src/main/java/school/faang/bjs2_92543/MasterCardService.java
+++ src/main/java/school/faang/bjs2_92543/MasterCardService.java
@@ -2,18 +2,25 @@ package school.faang.bjs2_92543;
 
 import lombok.extern.slf4j.Slf4j;
 
-import java.util.concurrent.*;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+import java.util.concurrent.ExecutionException;
 
 @Slf4j
 public class MasterCardService {
     private static final int TEN_SECONDS_IN_MS = 10_000;
     private static final int ONE_SECOND_IN_MS = 1_000;
-    private static final ExecutorService EXECUTOR_SERVICE = Executors.newSingleThreadExecutor();
+    private static final int PAYMENT_COUNT = 5_000;
+    private static final int ANALYTICS_COUNT = 17_000;
+
+    private final ExecutorService executorService = Executors.newSingleThreadExecutor();
 
     private static Integer collectPayment() {
         try {
             Thread.sleep(TEN_SECONDS_IN_MS);
-            return 5_000;
+            return PAYMENT_COUNT;
         } catch (InterruptedException e) {
             Thread.currentThread().interrupt();
             throw new RuntimeException(e);
@@ -23,7 +30,7 @@ public class MasterCardService {
     private static Integer sendAnalytics() {
         try {
             Thread.sleep(ONE_SECOND_IN_MS);
-            return 17_000;
+            return ANALYTICS_COUNT;
         } catch (InterruptedException e) {
             Thread.currentThread().interrupt();
             throw new RuntimeException(e);
@@ -31,21 +38,22 @@ public class MasterCardService {
     }
 
     public void doAll() {
-        CompletableFuture<Integer> analytics = CompletableFuture.supplyAsync(MasterCardService::sendAnalytics);
+        CompletableFuture<Integer> analytics = CompletableFuture.supplyAsync(
+                MasterCardService::sendAnalytics, executorService);
         Integer analyticsResult = analytics.join();
         System.out.printf("Отправлена аналитика: %s\n", analyticsResult);
 
-        Future<Integer> collected = EXECUTOR_SERVICE.submit(MasterCardService::collectPayment);
+        Future<Integer> collected = executorService.submit(MasterCardService::collectPayment);
         try {
             Integer result = collected.get();
-            EXECUTOR_SERVICE.shutdownNow();
+            executorService.shutdownNow();
             System.out.printf("Выполнен платёж: %d\n", result);
         } catch (InterruptedException e) {
             log.info("Прервали в процессе ожидания");
-            EXECUTOR_SERVICE.shutdownNow();
+            executorService.shutdownNow();
         } catch (ExecutionException e) {
             log.info("Ошибка в процессе обработки платежа");
-            EXECUTOR_SERVICE.shutdownNow();
+            executorService.shutdownNow();
         }
     }
 
