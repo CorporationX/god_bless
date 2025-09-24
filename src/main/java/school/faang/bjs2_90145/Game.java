4dfa7e3f9c45edf2d2eb2e3fd943480a4e3218cc
Elya Khamzina <ekkhamzi@mail.ru>
2025-09-24 11:08:36 +0300

    убрала private volatile boolean isGameRunning = true; и         isGameRunning = false;

--- src/main/java/school/faang/bjs2_90145/Game.java
+++ src/main/java/school/faang/bjs2_90145/Game.java
@@ -6,7 +6,6 @@ import lombok.extern.slf4j.Slf4j;
 public class Game {
     private int score = 0;
     private int lives = 10;
-    private volatile boolean isGameRunning = true;
 
     private final Object scoreLock = new Object();
     private final Object livesLock = new Object();
@@ -23,6 +22,7 @@ public class Game {
             synchronized (livesLock) {
                 if (lives <= 0) {
                     gameOver();
+                    return;
                 } else {
                     lives--;
                     log.info("Жизни уменьшены, оставшиеся жизни: {}", lives);
@@ -32,7 +32,6 @@ public class Game {
     }
 
     private void gameOver() {
-        isGameRunning = false;
         log.info("Игра окончена! Все жизни потеряны.");
     }
 }
