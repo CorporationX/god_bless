79984109208ff1efda45035efab8593e86d52b79
Andre` <acmisatyuk@gmail.com>
2025-09-24 11:17:26 +0800

    BJS2-91370 Распределенная армия героев меча и магии: simplified

--- src/main/java/school/faang/m1/thread/army/Main.java
+++ src/main/java/school/faang/m1/thread/army/Main.java
@@ -7,10 +7,10 @@ import school.faang.m1.thread.army.units.Swordsman;
 import java.util.List;
 
 public class Main {
-    public static void main(String[] args) throws InterruptedException {
-        Squad<Archer> archers = new Squad<>("Лучники", List.of(new Archer(25), new Archer(30)));
-        Squad<Swordsman> swordsmen = new Squad<>("Мечники", List.of(new Swordsman(40), new Swordsman(35)));
-        Squad<Mage> mages = new Squad<>("Маги", List.of(new Mage(50), new Mage(45)));
+    public static void main(String[] args) {
+        Squad archers = new Squad("Лучники", List.of(new Archer(25), new Archer(30)));
+        Squad swordsmen = new Squad("Мечники", List.of(new Swordsman(40), new Swordsman(35)));
+        Squad mages = new Squad("Маги", List.of(new Mage(50), new Mage(45)));
 
         Army army = new Army();
 
