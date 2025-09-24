88893d8aaddb42050a89311ae91b5d419b36f037
Mikhail EASY <mghievrie@bk.ru>
2025-09-24 16:38:21 +0500

    Revert "рефакторинг BJS2-94444"
    This reverts commit c5d93d6806627752d09622a1e0f7b34262899137.

--- src/main/java/school/faang/bjs2_94444/Player.java
+++ src/main/java/school/faang/bjs2_94444/Player.java
@@ -3,8 +3,6 @@ package school.faang.bjs2_94444;
 import lombok.Getter;
 import lombok.extern.slf4j.Slf4j;
 
-import java.util.concurrent.atomic.AtomicInteger;
-
 @Slf4j
 @Getter
 public class Player {
@@ -12,21 +10,21 @@ public class Player {
     private static final int DEFAULT_EXPERIENCE_NEXT_LEVEL = 100;
     private final String name;
     private int level;
-    private AtomicInteger experience = new AtomicInteger();
+    private double experience;
     private double nextLevel;
 
-    public Player(String name, int level, int experience) {
+    public Player(String name, int level, double experience) {
         this.name = name;
         this.level = level;
-        this.experience.addAndGet(experience);
+        this.experience = experience;
     }
 
-    public Player addExperience(AtomicInteger experience) {
-        this.experience.addAndGet(experience.get());
-        if (this.experience.get() >= nextLevel) {
+    public Player addExperience(double experience) {
+        this.experience += experience;
+        if (this.experience >= nextLevel) {
             level += LEVEL_UPGRADE;
             log.info("{} - С повышением уровня!, теперь у вас {} лвл, так держать!", name, level);
-            nextLevel = experience.get() + DEFAULT_EXPERIENCE_NEXT_LEVEL;
+            nextLevel = experience + DEFAULT_EXPERIENCE_NEXT_LEVEL;
         }
         return this;
     }
--- src/main/java/school/faang/bjs2_94444/QuestSystem.java
+++ src/main/java/school/faang/bjs2_94444/QuestSystem.java
@@ -3,37 +3,34 @@ package school.faang.bjs2_94444;
 import lombok.extern.slf4j.Slf4j;
 
 import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.Executor;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.TimeUnit;
-import java.util.concurrent.atomic.AtomicInteger;
 
 @Slf4j
 public class QuestSystem {
     private static final int DEFAULT_TIME_TO_QUEST = 1000;
     private static final double LEVEL_QUEST = 1.5;
     private static final int TIME_AWAIT_MINUTE = 1;
-    private final ExecutorService executor = Executors.newSingleThreadScheduledExecutor();
 
     public CompletableFuture<Player> startQuest(Player player, Quest quest) {
+        ExecutorService executor = Executors.newSingleThreadScheduledExecutor();
         int timeQuest = DEFAULT_TIME_TO_QUEST * quest.getDifficulty();
-        return CompletableFuture.supplyAsync(() -> {
-            AtomicInteger experience = new AtomicInteger();
+        CompletableFuture<Player> future = CompletableFuture.supplyAsync(() -> {
+            double experience = 0;
             try {
                 log.info("{} Начал квест {} - сложностью {}",
                         player.getName(), quest.getName(), quest.getDifficulty());
                 Thread.sleep(timeQuest);
                 log.info("{} - Завершил квест {}!, {} - сложностью и получает за квест {} - опыта!",
                         player.getName(), quest.getName(), quest.getDifficulty(), quest.getReward());
-                experience.addAndGet((int) Math.round(LEVEL_QUEST * quest.getDifficulty()));
+                experience = (int) Math.round(LEVEL_QUEST * quest.getDifficulty());
             } catch (InterruptedException e) {
                 log.error("Ошибка в имитации выполнения квеста");
             }
             return player.addExperience(experience);
         }, executor);
-    }
-
-    public void gracefullyShutdown() {
         executor.shutdown();
         try {
             if (!executor.awaitTermination(TIME_AWAIT_MINUTE, TimeUnit.MINUTES)) {
@@ -44,5 +41,6 @@ public class QuestSystem {
             Thread.currentThread().interrupt();
             log.error("Ошибка при остановке потоков");
         }
+        return future;
     }
 }
