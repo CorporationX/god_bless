e2f262224db0da4c850fc30da545b6f3f3ef2edd
AndreySN7 <andrey__n@mail.ru>
2025-09-26 00:14:43 +0300

    поправил

--- src/main/java/school/faang/multithreading/async/bjs2_92551/QuestSystem.java
+++ src/main/java/school/faang/multithreading/async/bjs2_92551/QuestSystem.java
@@ -5,14 +5,15 @@ import lombok.extern.slf4j.Slf4j;
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
 
 @Slf4j
 public class QuestSystem {
     private static final long ONE_SECOND_DELAY = 1000;
+    private static final long MAX_WAIT_TIME = 2;
+    private final ExecutorService executor = Executors.newFixedThreadPool(2);
 
     public CompletableFuture<Player> startQuest(Player player, Quest quest) {
-        ExecutorService executor = Executors.newSingleThreadExecutor();
-
         CompletableFuture<Player> future =
                 CompletableFuture.supplyAsync(() -> {
                     log.info("{} начинает выполнять квест {} ", player.getName(), quest.name());
@@ -24,10 +25,25 @@ public class QuestSystem {
                         throw new IllegalStateException("Выполнение квеста " + quest.name() + " прервано", e);
                     }
                     return player;
-                }, executor);
+                }, executor); // Не хочет работать с executor
 
-        executor.shutdown();
+        gracefullyShutdown();
 
         return future;
+    }
+
+    public void gracefullyShutdown() {
+        executor.shutdown();
+        try {
+            if (!executor.awaitTermination(MAX_WAIT_TIME, TimeUnit.MINUTES)) {
+                log.warn("Не все задачи завершились вовремя, принудительно закрываем пул потоков...");
+                executor.shutdownNow();
+            } else {
+                log.info("Все задачи выполнены успешно.");
+            }
+        } catch (InterruptedException e) {
+            log.error("Главный поток был прерван.");
+            executor.shutdownNow();
+        }
     }
 }
