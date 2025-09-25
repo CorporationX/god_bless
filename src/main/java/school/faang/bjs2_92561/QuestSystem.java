bc14a9aaf4fa829f44651f725fde0fcef3e17e49
Asta <charlyfazbear@gmail.com>
2025-09-25 23:59:54 +0300

    task complete

---
+++ src/main/java/school/faang/bjs2_92561/QuestSystem.java
@@ -0,0 +1,21 @@
+package school.faang.bjs2_92561;
+
+import java.util.concurrent.CompletableFuture;
+
+public class QuestSystem {
+
+    public CompletableFuture<Player> startQuest(Player player, Quest quest) {
+        return CompletableFuture.supplyAsync(() -> {
+            try {
+                Thread.sleep(quest.getDifficulty() * 1000L);
+            } catch (InterruptedException e) {
+                Thread.currentThread().interrupt();
+                throw new RuntimeException("Quest execution interrupted", e);
+            }
+
+            player.addExperience(quest.getReward());
+
+            return player;
+        });
+    }
+}
