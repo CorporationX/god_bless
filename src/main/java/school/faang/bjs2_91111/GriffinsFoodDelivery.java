1a46a23609207e0544d931deae5b4d43f222c273
Alexey Ulov <ulov.alexey@gmail.com>
2025-09-25 08:26:54 +0300

    add constant thread_pool_size

--- src/main/java/school/faang/bjs2_91111/GriffinsFoodDelivery.java
+++ src/main/java/school/faang/bjs2_91111/GriffinsFoodDelivery.java
@@ -6,16 +6,16 @@ import java.util.concurrent.Executors;
 
 public class GriffinsFoodDelivery {
     private static final int MAX_FOOD_AMOUNT = 100;
+    private static final int THREAD_POOL_SIZE = 3;
     private static final Random random = new Random();
 
     public static void main(String[] args) {
-        ExecutorService executor = Executors.newFixedThreadPool(3);
+        ExecutorService executor = Executors.newFixedThreadPool(THREAD_POOL_SIZE);
         String[] characterNames = {"Peter", "Lois", "Meg", "Chris", "Stewie"};
 
         for (String character : characterNames) {
             int foodAmount = random.nextInt(MAX_FOOD_AMOUNT) + 1;
-            FoodDeliveryTask foodDeliveryTask = new FoodDeliveryTask(character, foodAmount);
-            executor.submit(foodDeliveryTask);
+            executor.submit(new FoodDeliveryTask(character, foodAmount));
         }
 
         executor.shutdown();
