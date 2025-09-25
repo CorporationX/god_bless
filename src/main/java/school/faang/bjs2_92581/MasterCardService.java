487487fb7e59c6aecd79ab3e39c3b4cc505b8a69
Deathroels <loocyep@gmail.com>
2025-09-25 16:18:50 +0300

    BJS2-92581: task complete

---
+++ src/main/java/school/faang/bjs2_92581/MasterCardService.java
@@ -0,0 +1,53 @@
+package school.faang.bjs2_92581;
+
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+
+public class MasterCardService {
+    private static final int TEN_SECONDS_IN_MS = 10_000;
+    private static final int ONE_SECOND_IN_MS = 1_000;
+    private static final ExecutorService executor = Executors.newFixedThreadPool(2);
+
+    private static int collectPayment() {
+        try {
+            Thread.sleep(TEN_SECONDS_IN_MS);
+            return 5_000;
+        } catch (InterruptedException e) {
+            Thread.currentThread().interrupt();
+            throw new RuntimeException(e);
+        }
+    }
+
+    private static int sendAnalytics() {
+        try {
+            Thread.sleep(ONE_SECOND_IN_MS);
+            return 17_000;
+        } catch (InterruptedException e) {
+            Thread.currentThread().interrupt();
+            throw new RuntimeException(e);
+        }
+    }
+
+    public void doAll() {
+        Future<Integer> paymentFuture = executor.submit(MasterCardService::collectPayment);
+
+        try {
+            Integer sendResult = CompletableFuture
+                    .supplyAsync(MasterCardService::sendAnalytics, executor)
+                            .get();
+            System.out.printf("Аналитика отправлена: %s%n", sendResult);
+
+            int paymentResult = paymentFuture.get();
+            System.out.printf("Платеж выполнен: %s%n", paymentResult);
+
+        } catch (InterruptedException | ExecutionException e) {
+            Thread.currentThread().interrupt();
+            System.out.println("Что-то прервало операцию, не завершено.");
+        } finally {
+            executor.shutdown();
+        }
+    }
+}
