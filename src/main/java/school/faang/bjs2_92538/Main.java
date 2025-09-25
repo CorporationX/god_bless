5e31df8ef5591a4bec9a7d3063156b43baca61b6
Viks <viktor.naroznoi@gmail.com>
2025-09-25 22:27:43 +0300

    --BJS2-92538 - Corrections

--- src/main/java/school/faang/bjs2_92538/Main.java
+++ src/main/java/school/faang/bjs2_92538/Main.java
@@ -1,30 +1,12 @@
 package school.faang.bjs2_92538;
 
-import java.util.concurrent.CompletableFuture;
-import java.util.concurrent.ExecutionException;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
-import java.util.concurrent.Future;
 
 public class Main {
-    private static final ExecutorService executor = Executors.newFixedThreadPool(2);
+    private static final ExecutorService exec = Executors.newFixedThreadPool(2);
 
     public static void main(String[] args) {
-        doAll();
-    }
-
-    static void doAll() {
-        CompletableFuture.supplyAsync(() -> MasterCardService.sendAnalytics(), executor)
-                .thenAccept((result) -> System.out.println(result));
-        Future<Integer> paymentResult = executor.submit(() -> MasterCardService.collectPayment());
-
-        try {
-            System.out.println(paymentResult.get());
-            executor.shutdown();
-        } catch (InterruptedException | ExecutionException e) {
-            Thread.currentThread().interrupt();
-            executor.shutdownNow();
-            throw new RuntimeException(e);
-        }
+        MasterCardService.doAll(exec);
     }
 }
