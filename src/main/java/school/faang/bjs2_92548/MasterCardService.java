cb93fdc3c793124b38454e8b3c0c11b1263b5cad
Asta <charlyfazbear@gmail.com>
2025-09-25 13:41:35 +0300

    made corrections

--- src/main/java/school/faang/bjs2_92548/MasterCardService.java
+++ src/main/java/school/faang/bjs2_92548/MasterCardService.java
@@ -1,19 +1,26 @@
 package school.faang.bjs2_92548;
 
+import lombok.extern.slf4j.Slf4j;
+
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.Future;
+import java.util.concurrent.TimeUnit;
 
+@Slf4j
 public class MasterCardService {
     private static final int TEN_SECONDS_IN_MS = 10_000;
     private static final int ONE_SECOND_IN_MS = 1_000;
+    private static final int PAYMENT_AMOUNT = 5_000;
+    private static final int ANALYTICS_AMOUNT = 17_000;
+    private static final int MAX_WAIT_TIME = 1;
 
     static int collectPayment() {
         try {
             Thread.sleep(TEN_SECONDS_IN_MS);
-            return 5_000;
+            return PAYMENT_AMOUNT;
         } catch (InterruptedException e) {
             Thread.currentThread().interrupt();
             throw new RuntimeException(e);
@@ -23,7 +30,7 @@ public class MasterCardService {
     static int sendAnalytics() {
         try {
             Thread.sleep(ONE_SECOND_IN_MS);
-            return 17_000;
+            return ANALYTICS_AMOUNT;
         } catch (InterruptedException e) {
             Thread.currentThread().interrupt();
             throw new RuntimeException(e);
@@ -37,7 +44,7 @@ public class MasterCardService {
             Future<Integer> paymentFuture = executor.submit(MasterCardService::collectPayment);
 
             CompletableFuture<Integer> analyticsFuture =
-                    CompletableFuture.supplyAsync(MasterCardService::sendAnalytics);
+                    CompletableFuture.supplyAsync(MasterCardService::sendAnalytics, executor);
 
             int analyticsResult = analyticsFuture.join();
             System.out.println("Аналитика отправленна: " + analyticsResult);
@@ -49,6 +56,21 @@ public class MasterCardService {
             e.printStackTrace();
         } finally {
             executor.shutdown();
+        }
+    }
+
+    private static void gracefullyShutdown(ExecutorService executor) {
+        executor.shutdown();
+        try {
+            if (!executor.awaitTermination(MAX_WAIT_TIME, TimeUnit.MINUTES)) {
+                log.warn("Не все задачи завершились вовремя, принудительно закрываем пул потоков...");
+                executor.shutdownNow();
+            } else {
+                log.info("Все задачи выполнены успешно.");
+            }
+        } catch (InterruptedException e) {
+            log.error("Главный поток был прерван.");
+            executor.shutdownNow();
         }
     }
 
