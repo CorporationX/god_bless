9e408eb6e273c31fa28a7c07e4296baafaa54ce6
Bekhter Roman <curses93@mail.ru>
2025-09-25 10:41:46 +0300

    BJS2-92726 done

---
+++ src/main/java/school/faang/bjs2_92726/Pair.java
@@ -0,0 +1,9 @@
+package school.faang.bjs2_92726;
+
+import lombok.AllArgsConstructor;
+
+@AllArgsConstructor
+public class Pair<F, S> {
+    public final F first;
+    public final S second;
+}
---
+++ src/main/java/school/faang/bjs2_92726/SuperheroBattle.java
@@ -0,0 +1,29 @@
+package school.faang.bjs2_92726;
+
+import java.util.List;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+import java.util.concurrent.ThreadLocalRandom;
+import java.util.stream.Collectors;
+
+public class SuperheroBattle {
+    private final ExecutorService executor;
+
+    public SuperheroBattle(int thread) {
+        executor = Executors.newFixedThreadPool(thread);
+    }
+
+    public List<Future<Superhero>> runCompetitions(List<Pair<Superhero, Superhero>> pairs) {
+        return pairs.stream()
+                .map(pair -> executor.submit(() -> {
+                    int score1 = pair.first.getAgility() + pair.first.getStrength();
+                    int score2 = pair.second.getAgility() + pair.second.getStrength();
+                    executor.shutdown();
+                    return score1 > score2 ? pair.first :
+                            score1 < score2 ? pair.second :
+                                    ThreadLocalRandom.current().nextBoolean() ? pair.first : pair.second;
+                }))
+                .collect(Collectors.toList());
+    }
+}
