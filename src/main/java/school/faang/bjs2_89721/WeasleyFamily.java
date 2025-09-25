574e103e8e6be3acea23814e9041529314d79b7e
Viks <viktor.naroznoi@gmail.com>
2025-09-25 12:50:50 +0300

    --BJS2-89721 - They were very nice but extremely poor - Correction bases on reviewer pointers

--- src/main/java/school/faang/bjs2_89721/WeasleyFamily.java
+++ src/main/java/school/faang/bjs2_89721/WeasleyFamily.java
@@ -1,8 +1,12 @@
 package school.faang.bjs2_89721;
 
+import lombok.extern.slf4j.Slf4j;
+
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
 
+@Slf4j
 public class WeasleyFamily {
 
     public static void main(String[] args) {
@@ -16,5 +20,17 @@ public class WeasleyFamily {
             executors.submit(chore);
         }
         executors.shutdown();
+        try {
+            if (!executors.awaitTermination(5, TimeUnit.SECONDS)) {
+                log.warn("Some chose were not finish in time ...");
+                executors.shutdownNow();
+            } else {
+                log.info("All chores completed successfully");
+            }
+        } catch (InterruptedException e) {
+            log.error(Thread.currentThread().getName() + " got interrupted while waiting");
+            executors.shutdownNow();
+            Thread.currentThread().interrupt();
+        }
     }
 }
