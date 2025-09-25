b882a1347e87d666b8f6e0943715cf8c7b8f9192
Georgy Silkin <devprogandgame17062024@gmail.com>
2025-09-25 19:52:45 +1000

    Задача: WOW.

---
+++ src/main/java/school/faang/wow/Player.java
@@ -0,0 +1,5 @@
+package school.faang.wow;
+
+
+public record Player(String name, int level, int experience) {
+}
---
+++ src/main/java/school/faang/wow/QuestSystem.java
@@ -0,0 +1,57 @@
+package school.faang.wow;
+
+import lombok.extern.slf4j.Slf4j;
+
+import java.util.concurrent.CompletableFuture;
+
+@Slf4j
+public class QuestSystem {
+
+    public static void print(Player player) {
+        log.info("{} has completed the quest and now has {} experience points.", player.name(), player.experience());
+    }
+
+    CompletableFuture<Player> startQuest(Player player, Quest quest) {
+        return CompletableFuture.supplyAsync(() -> {
+            try {
+                log.info("Player {} started quest: {}", player.name(), quest.name());
+                Thread.sleep(quest.difficulty() * 1000L);
+            } catch (InterruptedException e) {
+                Thread.currentThread().interrupt();
+                throw new RuntimeException("Quest was interrupted: " + quest.name(), e);
+            }
+
+            Player updatedPlayer = new Player(
+                    player.name(),
+                    player.level(),
+                    player.experience() + quest.reward()
+            );
+
+            log.info("Player {} completed quest: {}", player.name(), quest.name());
+            return updatedPlayer;
+        }).exceptionally(throwable -> {
+            log.error("Quest failed for player {}: {}", player.name(), throwable.getMessage());
+            return player;
+        });
+    }
+
+    public static void main(String[] args) {
+        QuestSystem questSystem = new QuestSystem();
+
+        Player player1 = new Player("Thrall", 10, 250);
+        Player player2 = new Player("Sylvanas", 12, 450);
+
+        Quest quest1 = new Quest("Defeat the Lich King", 10, 150);
+        Quest quest2 = new Quest("Retrieve the Sword of Azeroth", 8, 100);
+
+        CompletableFuture<Player> player1Quest = questSystem.startQuest(player1, quest1);
+        CompletableFuture<Player> player2Quest = questSystem.startQuest(player2, quest2);
+
+        player1Quest.thenAccept(QuestSystem::print);
+        player2Quest.thenAccept(QuestSystem::print);
+
+        CompletableFuture.allOf(player1Quest, player2Quest).join();
+
+        log.info("All quests completed!");
+    }
+}
