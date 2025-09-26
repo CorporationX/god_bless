2eaab9860e8beba505594695578c332eb433908d
Admin <sofiya.chebotareva@bk.ru>
2025-09-26 08:38:35 +0200

    task BJS2-92637, first commit

---
+++ src/main/java/bjs292637/Player.java
@@ -0,0 +1,4 @@
+package bjs292637;
+
+public record Player(String name, int level, int experience) {
+}
---
+++ src/main/java/bjs292637/Quest.java
@@ -0,0 +1,5 @@
+package bjs292637;
+
+public record Quest(String name, int difficulty, int reward) {
+
+}
