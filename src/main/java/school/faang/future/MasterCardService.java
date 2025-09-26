be089fb54add338ff341f06cfbe4669804c6caab
Kateryna Kramarchuk <alexnjfort@gmail.com>
2025-09-27 00:08:30 +0200

    fix

--- src/main/java/school/faang/future/MasterCardService.java
+++ src/main/java/school/faang/future/MasterCardService.java
@@ -1,8 +1,8 @@
 package school.faang.future;
 
 import java.util.concurrent.CompletableFuture;
-import java.util.concurrent.CompletionService;
-import java.util.concurrent.Future;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 import java.util.concurrent.FutureTask;
 
 public class MasterCardService {
@@ -30,14 +30,21 @@ public class MasterCardService {
     }
 
     public void doAll() {
+        ExecutorService executor = Executors.newFixedThreadPool(2);
+
         FutureTask<Integer> paymentFuture = new FutureTask<>(MasterCardService::collectPayment);
+
         new Thread(paymentFuture).start();
-        CompletableFuture<Integer> analyticsFuture = CompletableFuture.supplyAsync((MasterCardService::sendAnalytics));
+
+        CompletableFuture<Integer> analyticsFuture = CompletableFuture
+                .supplyAsync(MasterCardService::sendAnalytics, executor);
         try {
             System.out.println("Аналитика отправлена: " + analyticsFuture.get());
             System.out.println("Платеж выполнен: " + paymentFuture.get());
         } catch (Exception e) {
             e.printStackTrace();
+        } finally {
+            executor.shutdown();
         }
     }
 }
