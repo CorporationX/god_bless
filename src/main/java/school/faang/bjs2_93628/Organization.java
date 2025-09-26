95fc653a42d1419dc8c6ed4dad8ba4501892646d
EdelweissPC <shherbakov99ilya@mail.ru>
2025-09-27 00:30:50 +0300

    BJS2-93628 fix

--- src/main/java/school/faang/bjs2_93628/Organization.java
+++ src/main/java/school/faang/bjs2_93628/Organization.java
@@ -2,28 +2,28 @@ package school.faang.bjs2_93628;
 
 import lombok.extern.slf4j.Slf4j;
 
+import java.math.BigDecimal;
 import java.util.List;
 import java.util.concurrent.CopyOnWriteArrayList;
-import java.util.concurrent.atomic.AtomicInteger;
 
 @Slf4j
 public class Organization {
-    private final AtomicInteger balance = new AtomicInteger(0);
+    private BigDecimal balance = new BigDecimal(0);
     private final List<Donation> historyDonation = new CopyOnWriteArrayList<>();
 
     public void addDonation(Donation donation) {
-        balance.addAndGet(donation.getAmount());
-        log.info("Пришел донат - {}, баланс составляет {}", donation, balance.get());
+        balance = balance.add(BigDecimal.valueOf(donation.getAmount()));
+        log.info("Пришел донат - {}, баланс составляет {}", donation, balance);
         historyDonation.add(donation);
     }
 
     public void printBalance() {
-        log.info("Баланс составляет {}", balance.get());
+        log.info("Баланс составляет {}", balance);
     }
 
     public void printHistoryDonat() {
         log.info("История донатов: ");
-        historyDonation.stream().forEach(donation -> System.out.println(donation));
+        historyDonation.forEach(System.out::println);
 
     }
 }
