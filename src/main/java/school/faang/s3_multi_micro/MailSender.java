1b04d26ca16421e42356663e1ac867db2b1f0870
pinchalex <pinchalex484@gmail.com>
2025-09-26 14:37:34 +0300

    Исправлена после ревью BJS2-91137 "Вы работаете в Microsoft!"

--- src/main/java/school/faang/s3_multi_micro/MailSender.java
+++ src/main/java/school/faang/s3_multi_micro/MailSender.java
@@ -3,10 +3,9 @@ package school.faang.s3_multi_micro;
 public class MailSender {
     private static final int TOTAL_MESSAGES = 1000;
     private static final int THREADS_COUNT = 5;
+    private static final int batchSize = TOTAL_MESSAGES / THREADS_COUNT;
 
     public static void main(String[] args) throws InterruptedException {
-        int batchSize = TOTAL_MESSAGES / THREADS_COUNT;
-
         Thread[] threads = new Thread[THREADS_COUNT];
 
         for (int i = 0; i < THREADS_COUNT; i++) {
--- src/main/java/school/faang/s3_multi_micro/SenderRunnable.java
+++ src/main/java/school/faang/s3_multi_micro/SenderRunnable.java
@@ -10,7 +10,7 @@ public class SenderRunnable implements Runnable {
     @Override
     public void run() {
         for (int i = startIndex; i < endIndex; i++) {
-            System.out.println("Письмо отправлено: " + i);
+            System.out.printf("Письмо отправлено: %d%n", i);
         }
     }
 }
