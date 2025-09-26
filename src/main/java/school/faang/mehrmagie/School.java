9238631e8d8d7583cd443a24c91dc44177712c7e
Kateryna Kramarchuk <alexnjfort@gmail.com>
2025-09-26 13:24:48 +0200

    Magi

---
+++ src/main/java/school/faang/mehrmagie/School.java
@@ -0,0 +1,13 @@
+package school.faang.mehrmagie;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+import java.util.List;
+
+@AllArgsConstructor
+@Getter
+public class School {
+    private String name;
+    private List<Student> team;
+}
