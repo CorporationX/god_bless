1134e093845abb0c48ee7e284fd4f86acd2ba4db
Kuanysh <skn_88@mail.ru>
2025-09-26 22:10:36 +0500

    BJS2-93093 Fix PR comment 1. remove synchronized block from startTask method

--- src/main/java/school/faang/bjs2_93093/Tournament.java
+++ src/main/java/school/faang/bjs2_93093/Tournament.java
@@ -10,20 +10,18 @@ public class Tournament {
     private final ExecutorService executorService = Executors.newFixedThreadPool(5);
 
     public CompletableFuture<School> startTask(School school, Task task) {
-        synchronized (task) {
-            return CompletableFuture.supplyAsync(() -> {
-                try {
-                    Thread.sleep((long) task.difficulty() * GET_MILLIS);
-                } catch (InterruptedException e) {
-                    System.err.printf("Thread of school %s on task %s was interrupted", school, task);
-                    Thread.currentThread().interrupt();
-                }
-                for (Student student : school.team) {
-                    student.addPoints(task.reward());
-                }
-                return school;
-            }, executorService);
-        }
+        return CompletableFuture.supplyAsync(() -> {
+            try {
+                Thread.sleep((long) task.difficulty() * GET_MILLIS);
+            } catch (InterruptedException e) {
+                System.err.printf("Thread of school %s on task %s was interrupted", school, task);
+                Thread.currentThread().interrupt();
+            }
+            for (Student student : school.team) {
+                student.addPoints(task.reward());
+            }
+            return school;
+        }, executorService);
     }
 
     public void shutdownExecutorService(long timeoutInSeconds) {
