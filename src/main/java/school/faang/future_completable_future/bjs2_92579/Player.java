3d006d63391aee2028cf2b2a1d0e09cce6e3fa88
Denis Chudov <Chdnru@gmail.com>
2025-09-27 07:23:05 +0300

    Fix.Сделал метод addExperience synchronized Задача BJS2-92579(WOW)

--- src/main/java/school/faang/future_completable_future/bjs2_92579/Player.java
+++ src/main/java/school/faang/future_completable_future/bjs2_92579/Player.java
@@ -2,18 +2,15 @@ package school.faang.future_completable_future.bjs2_92579;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
-import lombok.Setter;
 
 @AllArgsConstructor
 @Getter
 public class Player {
     private String name;
-    @Setter
     private long level;
-    @Setter
     private long experience;
 
-    public  <T extends Number> void addExperience(T experience) {
+    public synchronized <T extends Number> void addExperience(T experience) {
         this.experience += experience.longValue();
     }
 }
