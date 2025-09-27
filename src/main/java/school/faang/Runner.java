7c44cb20ff9f4f834de8afe5afb19b41b2b51bf6
svxrslf <svxrslf@yandex.ru>
2025-09-27 15:45:04 +0300

    91101

--- src/main/java/school/faang/Runner.java
+++ src/main/java/school/faang/Runner.java
@@ -1,4 +1,10 @@
 package school.faang;
 
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
 public class Runner {
+    public static void main(String[] args) {
+        log.info("Hello world!");
+    }
 }
