0a4d0cd70a43fe1b3541b5b3e2b1b5503917caa5
K0I2T <hiro94337@gmail.com>
2025-09-27 14:56:23 +0300

    BJS2-91155 выполнил

---
+++ src/main/java/school/faang/bjs2_91155/SenderRunnable.java
@@ -0,0 +1,16 @@
+package school.faang.bjs2_91155;
+
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
+public class SenderRunnable implements Runnable {
+    private final int startIndex;
+    private final int endIndex;
+
+    @Override
+    public void run() {
+        for (int i = startIndex; i < endIndex; i++) {
+            System.out.println("Письмо №" + i + " отправлено");
+        }
+    }
+}
