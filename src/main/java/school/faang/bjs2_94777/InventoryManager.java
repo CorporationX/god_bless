4fdb1a83aeb859f3c4495dcbcf8525e097fb75c2
Mikhail EASY <mghievrie@bk.ru>
2025-09-27 18:09:13 +0500

    внес изменения BJS2-94777

--- src/main/java/school/faang/bjs2_94777/InventoryManager.java
+++ src/main/java/school/faang/bjs2_94777/InventoryManager.java
@@ -2,8 +2,6 @@ package school.faang.bjs2_94777;
 
 import lombok.extern.slf4j.Slf4j;
 
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Random;
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.ExecutionException;
