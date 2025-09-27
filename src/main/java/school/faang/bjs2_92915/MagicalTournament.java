0f9c4b83955cad15249152cb43d3311e1fab8908
Viks <viktor.naroznoi@gmail.com>
2025-09-27 23:18:51 +0300

    --BJS2-92915 - Wizard Tournament - corrections

--- src/main/java/school/faang/bjs2_92915/MagicalTournament.java
+++ src/main/java/school/faang/bjs2_92915/MagicalTournament.java
@@ -20,9 +20,7 @@ public class MagicalTournament {
         CompletableFuture<School> hogwartsTask = tournament.startTask(hogwarts, task1);
         CompletableFuture<School> beauxbatonsTask = tournament.startTask(beauxbatons, task2);
 
-        CompletableFuture<Void> allTasks = CompletableFuture.allOf(hogwartsTask, beauxbatonsTask);
-
-        allTasks.thenRun(() -> {
+        CompletableFuture.allOf(hogwartsTask, beauxbatonsTask).thenRun(() -> {
             if (hogwarts.getTotalPoints() > beauxbatons.getTotalPoints()) {
                 System.out.printf("%s wins the tournament!", hogwarts.getName());
             }
