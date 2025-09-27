cf83294f2aeb3fab91bef41e0b5652a29ecf8944
Bekhter Roman <curses93@mail.ru>
2025-09-27 13:09:26 +0300

    BJS2-93337 fix

--- src/main/java/school/faang/bjs2_93337/Bank.java
+++ src/main/java/school/faang/bjs2_93337/Bank.java
@@ -1,11 +1,13 @@
 package school.faang.bjs2_93337;
 
 import lombok.Getter;
+import lombok.extern.slf4j.Slf4j;
 
 import java.util.Map;
 import java.util.concurrent.locks.Lock;
 import java.util.concurrent.locks.ReentrantLock;
 
+@Slf4j
 public class Bank {
     @Getter
     private Map<Integer, Account> accounts;
@@ -16,17 +18,33 @@ public class Bank {
     }
 
     public boolean transfer(int fromAccountId, int toAccountId, double amount) {
-        synchronized (lock) {
-            Account fromAccount = accounts.get(fromAccountId);
-            Account toAccount = accounts.get(toAccountId);
-            if (fromAccount.getBalance() > 0 && fromAccount.getBalance() > amount) {
-                fromAccount.withdraw(amount);
-                toAccount.deposit(amount);
-                return true;
-            } else {
-                return false;
+        Account fromAccount = accounts.get(fromAccountId);
+        Account toAccount = accounts.get(toAccountId);
+        Account first = fromAccountId < toAccountId ? fromAccount : toAccount;
+        Account second = fromAccountId < toAccountId ? toAccount : fromAccount;
+
+        if (first.getLock().tryLock()) {
+            try {
+                if (second.getLock().tryLock()) {
+                    try {
+                        if (fromAccount.getBalance() > 0 && fromAccount.getBalance() > amount) {
+                            fromAccount.withdraw(amount);
+                            toAccount.deposit(amount);
+                            log.info("transaction success: {} -> {} | {}", fromAccountId, toAccountId, amount);
+                            return true;
+                        } else {
+                            log.info("transaction failure");
+                            return false;
+                        }
+                    } finally {
+                        second.getLock().unlock();
+                    }
+                }
+            } finally {
+                first.getLock().unlock();
             }
         }
+        return false;
     }
 
     public double getTotalBalance() {
