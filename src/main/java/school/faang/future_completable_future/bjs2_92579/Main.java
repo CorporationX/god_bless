25086a034e1d5368a5043526cf50274dbaed43d5
Denis Chudov <Chdnru@gmail.com>
2025-09-27 07:34:09 +0300

    Fix.Цепочка строится сразу от startQuest(...) Задача BJS2-92579(WOW)

--- src/main/java/school/faang/future_completable_future/bjs2_92579/Main.java
+++ src/main/java/school/faang/future_completable_future/bjs2_92579/Main.java
@@ -1,12 +1,10 @@
 package school.faang.future_completable_future.bjs2_92579;
 
 import lombok.extern.slf4j.Slf4j;
-import java.util.concurrent.CompletableFuture;
 
 @Slf4j
 public class Main {
 
-    @SuppressWarnings("checkstyle:CommentsIndentation")
     public static void main(String[] args) {
         QuestSystem questSystem = new QuestSystem();
 
@@ -16,13 +14,14 @@ public class Main {
         Quest quest1 = new Quest("Defeat the Lich King", 10, 150);
         Quest quest2 = new Quest("Retrieve the Sword of Azeroth", 8, 100);
 
-        CompletableFuture<Player> player1Quest = questSystem.startQuest(player1, quest1);
-        CompletableFuture<Player> player2Quest = questSystem.startQuest(player2, quest2);
+        questSystem.startQuest(player1, quest1)
+                .thenAccept(Main::printMessage);
+
+        questSystem.startQuest(player2, quest2)
+                .thenAccept(Main::printMessage);
 
         QuestSystem.stopQuestProcessingAndAwaitTermination();
 
-        player1Quest.thenAccept(Main::printMessage);
-        player2Quest.thenAccept(Main::printMessage);
     }
 
     private static void printMessage(Player player) {
