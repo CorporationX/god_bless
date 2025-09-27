30b7c0ccd3a3a6ee648e9d25182862945ff33bce
derzhko <a.derzhko@yandex.ru>
2025-09-27 03:41:52 +0300

    initial commit

--- src/main/java/school/faang/bjs2_91375/Main.java
+++ src/main/java/school/faang/bjs2_91375/Main.java
@@ -5,9 +5,9 @@ import java.util.List;
 public class Main {
     public static void main(String[] args) throws InterruptedException {
         // Создаём отряды
-        Squad archers = new Squad("Лучники", List.of(new Archer(25), new Archer(30)));
-        Squad swordsmen = new Squad("Мечники", List.of(new Swordsman(40), new Swordsman(35)));
-        Squad mages = new Squad("Маги", List.of(new Mage(50), new Mage(45)));
+        Squad<Archer> archers = new Squad<>("Лучники", List.of(new Archer(25), new Archer(30)));
+        Squad<Swordsman> swordsmen = new Squad<>("Мечники", List.of(new Swordsman(40), new Swordsman(35)));
+        Squad<Mage> mages = new Squad<>("Маги", List.of(new Mage(50), new Mage(45)));
 
         Army army = new Army();
         // Добавляем отряды в армию
