0e9d5e043d51295482bacdd6b097afcb779efacc
Mikhail EASY <mghievrie@bk.ru>
2025-09-27 18:17:26 +0500

    внес изменения BJS2-93141

--- src/main/java/school/faang/bjs2_93141/MagicalTournament.java
+++ src/main/java/school/faang/bjs2_93141/MagicalTournament.java
@@ -20,10 +20,10 @@ public class MagicalTournament {
     private static final int THREAD_COUNT = 3;
     private static final int AWAIT_MINUTE = 1;
     private static final ExecutorService execute = Executors.newFixedThreadPool(THREAD_COUNT);
+    private static final Random random = new Random();
 
     public static void main(String[] args) {
         Tournament tournament = new Tournament();
-        Random random = new Random();
         List<Student> hogwartsTeam = List.of(
                 new Student("Harry", YEARS_HOGWARTS_STUDENT, DEFAULT_POINTS_STUDENT),
                 new Student("Hermione", YEARS_HOGWARTS_STUDENT, DEFAULT_POINTS_STUDENT)
@@ -34,16 +34,8 @@ public class MagicalTournament {
         );
         School hogwarts = new School("Hogwarts", hogwartsTeam);
         School beauxbatons = new School("Beauxbatons", beauxbatonsTeam);
-        Task task1 = new Task(
-                "Triwizard Tournament",
-                random.nextInt(START_SEEDS, DEFAULT_DIFFICULTY),
-                random.nextInt(START_SEEDS, DEFAULT_REWARD)
-        );
-        Task task2 = new Task(
-                "Yule Ball Preparations",
-                random.nextInt(START_SEEDS, DEFAULT_DIFFICULTY),
-                random.nextInt(START_SEEDS, DEFAULT_REWARD)
-        );
+        Task task1 = createTask("Triwizard Tournament");
+        Task task2 = createTask("Yule Ball Preparations");
         CompletableFuture<School> hogwartsTask = tournament.startTask(hogwarts, task1, execute);
         CompletableFuture<School> beauxbatonsTask = tournament.startTask(beauxbatons, task2, execute);
         CompletableFuture<Void> allTasks = CompletableFuture.allOf(hogwartsTask, beauxbatonsTask)
@@ -54,10 +46,16 @@ public class MagicalTournament {
                         log.info("{} - Школа победила в соревновании трех волшебников!", beauxbatons.getName());
                     }
                 });
-        shutdown();
+        shutdownExecutor();
     }
 
-    private static void shutdown() {
+    private static Task createTask(String task) {
+        return new Task(
+                task, random.nextInt(START_SEEDS, DEFAULT_DIFFICULTY), random.nextInt(START_SEEDS, DEFAULT_REWARD)
+        );
+    }
+
+    private static void shutdownExecutor() {
         execute.shutdown();
         try {
             if (!execute.awaitTermination(AWAIT_MINUTE, TimeUnit.MINUTES)) {
