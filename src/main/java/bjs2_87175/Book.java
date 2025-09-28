83d919002f4be7781121247ca09a5c7310770579
Amir <amirdautaliev@gmail.com>
2025-09-28 19:49:45 +0500

    Задача на проверку библиотека  Вестороса

---
+++ src/main/java/bjs2_87175/Book.java
@@ -0,0 +1,39 @@
+package bjs2_87175;
+
+import java.util.Objects;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@AllArgsConstructor
+@Getter
+public class Book {
+    private String title;
+    private String author;
+    private int year;
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        Book book = (Book) o;
+        return year == book.year && Objects.equals(title, book.title) && Objects.equals(author, book.author);
+    }
+
+    @Override
+    public int hashCode() {
+        int result = Objects.hashCode(title);
+        result = 31 * result + Objects.hashCode(author);
+        result = 31 * result + year;
+        return result;
+    }
+
+    @Override
+    public String toString() {
+        return "Book{" +
+                "title='" + title + '\'' +
+                ", author='" + author + '\'' +
+                ", year=" + year +
+                '}';
+    }
+}
