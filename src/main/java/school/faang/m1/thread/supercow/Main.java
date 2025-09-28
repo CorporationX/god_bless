5aac1524581e190b3e02129aa0370f010cd8d14c
Andre` <acmisatyuk@gmail.com>
2025-09-28 11:34:34 +0800

    BJS2-90536 Supercow! Thread creation optimized

--- src/main/java/school/faang/m1/thread/supercow/Main.java
+++ src/main/java/school/faang/m1/thread/supercow/Main.java
@@ -3,23 +3,15 @@ package school.faang.m1.thread.supercow;
 public class Main {
     public static void main(String[] args) throws InterruptedException {
         Boss boss = new Boss(3);
+
         Thread[] threads = new Thread[4];
-        threads[0] = new Thread(() -> {
-            Player player = new Player("1");
-            player.doBattle(boss);
-        });
-        threads[1] = new Thread(() -> {
-            Player player = new Player("2");
-            player.doBattle(boss);
-        });
-        threads[2] = new Thread(() -> {
-            Player player = new Player("3");
-            player.doBattle(boss);
-        });
-        threads[3] = new Thread(() -> {
-            Player player = new Player("4");
-            player.doBattle(boss);
-        });
+        for (int i = 0; i < 4; i++) {
+            int finalI = i;
+            threads[i] = new Thread(() -> {
+                Player player = new Player("P" + finalI);
+                player.doBattle(boss);
+            });
+        }
 
         for (Thread thread : threads) {
             thread.start();
