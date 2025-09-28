8b380efa72e94ed54fd3961df9f649b75e207963
Abzal Samekeyev <samekeyev@gmail.com>
2025-09-28 15:12:19 +0500

    задача Асинхронность и Future на проверку

---
+++ src/main/java/bjs2_92593/Main.java
@@ -0,0 +1,8 @@
+package bjs2_92593;
+
+public class Main {
+    public static void main(String[] args) {
+        MasterCardService service = new MasterCardService();
+        service.doAll();
+    }
+}
