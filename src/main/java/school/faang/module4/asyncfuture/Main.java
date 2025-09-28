d6f98e9bd36c2cbca8cc492835170e7a80798bfe
mipkasnow <steamer08@mail.ru>
2025-09-28 14:11:42 +0300

    MasterCardService

---
+++ src/main/java/school/faang/module4/asyncfuture/Main.java
@@ -0,0 +1,9 @@
+package school.faang.module4.asyncfuture;
+
+public class Main {
+
+    public static void main(String[] args) {
+        MasterCardService service = new MasterCardService();
+        service.doAll();
+    }
+}
