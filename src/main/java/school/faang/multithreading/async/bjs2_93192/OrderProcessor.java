02659818baffc915692d3a9244ae53ae26eccc67
AndreySN7 <andrey__n@mail.ru>
2025-09-28 21:01:46 +0300

    Поправил

--- src/main/java/school/faang/multithreading/async/bjs2_93192/OrderProcessor.java
+++ src/main/java/school/faang/multithreading/async/bjs2_93192/OrderProcessor.java
@@ -12,23 +12,28 @@ public class OrderProcessor {
 
     private final AtomicInteger totalProcessedOrders = new AtomicInteger(0);
 
-    public CompletableFuture<Order> processOrder(Order order) {
-        try {
-            log.info("Cтатус {} заказа: {}", order.getId(), order.getStatus());
-            Thread.sleep(DELAY_OPERATION);
-            order.statusChange(order, OrderStatus.PROCESSED);
-            log.info("Новый статус {} заказа: {}", order.getId(), order.getStatus());
-            totalProcessedOrders.incrementAndGet();
-        } catch (InterruptedException e) {
-            throw new RuntimeException(e);
-        }
-        return CompletableFuture.completedFuture(order);
+    public CompletableFuture<Void> processOrder(Order order) {
+        return CompletableFuture.runAsync(() -> {
+            try {
+                Thread.sleep(DELAY_OPERATION);
+                order.statusChange(order, OrderStatus.PROCESSED);
+                log.info("Новый статус {} заказа: {}", order.getId(), order.getStatus());
+                totalProcessedOrders.incrementAndGet();
+            } catch (InterruptedException e) {
+                Thread.currentThread().interrupt();
+                throw new RuntimeException("Ошибка обработки заказа", e);
+            }
+        });
     }
 
     public void processAllOrders(List<Order> orders) {
-        List<CompletableFuture<Order>> futures = orders.stream()
+        List<CompletableFuture<Void>> futures = orders.stream()
                 .map(order -> processOrder(order))
                 .toList();
+
+        for (CompletableFuture<Void> future : futures) {
+            future.join();
+        }
 
         CompletableFuture.allOf(futures.toArray(new CompletableFuture[0]));
 
