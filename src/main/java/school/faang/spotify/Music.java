0222c932b0878a42c233297ecb83642c9fdd2974
leto007 <liga07@bk.ru>
2025-09-29 20:47:53 +0300

    Решил задачу Spotify BJS2-90405

--- src/main/java/school/faang/spotify/Music.java
+++ src/main/java/school/faang/spotify/Music.java
@@ -1,38 +1,43 @@
 package school.faang.spotify;
 
+import java.util.Arrays;
+import java.util.List;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicBoolean;
+import java.util.stream.Stream;
 
 public class Music {
+    private static final int FIXED_TREADS = 3;
+
     public static void main(String[] args) {
-        ExecutorService executorService = Executors.newFixedThreadPool(10);
-        Player player1 = new Player();
-        Player player2 = new Player();
-        Player player3 = new Player();
-        executorService.submit(player1::play);
-        executorService.submit(player1::pause);
-        executorService.submit(player1::skip);
-        executorService.submit(player1::previous);
+        Object lock = new Object();
+        AtomicBoolean isPlaying = new AtomicBoolean();
+        ExecutorService executorService = Executors.newFixedThreadPool(FIXED_TREADS);
+        List<Player> players = Arrays.asList(
+                new Player(" a", lock, isPlaying),
+                new Player(" s", lock, isPlaying),
+                new Player(" d", lock, isPlaying)
+        );
 
-        executorService.submit(player2::play);
-        executorService.submit(player2::pause);
-        executorService.submit(player2::skip);
-        executorService.submit(player2::previous);
-
-        executorService.submit(player3::play);
-        executorService.submit(player3::pause);
-        executorService.submit(player3::skip);
-        executorService.submit(player3::previous);
+        players.stream()
+                .flatMap(player -> Stream.of(
+                        (Runnable) player::play,
+                        (Runnable) player::pause,
+                        (Runnable) player::skip,
+                        (Runnable) player::previous
+                ))
+                .forEach(executorService::submit);
 
         executorService.shutdown();
 
         try {
             if (!executorService.awaitTermination(10, TimeUnit.SECONDS)) {
-                executorService.shutdown();
+                executorService.shutdownNow();
             }
         } catch (InterruptedException e) {
-            executorService.shutdown();
+            executorService.shutdownNow();
             Thread.currentThread().interrupt();
         }
         System.out.println("Все действия завершили");
--- src/main/java/school/faang/spotify/Player.java
+++ src/main/java/school/faang/spotify/Player.java
@@ -1,40 +1,52 @@
 package school.faang.spotify;
 
+import lombok.Getter;
+
+import java.util.concurrent.atomic.AtomicBoolean;
+
+@Getter
 public class Player {
-    private Boolean isPlaying = false;
-    private final Object lock = new Object();
+    private final AtomicBoolean isPlaying;
+    private final Object lock;
+    String name;
+
+    public Player(String name, Object lock, AtomicBoolean isPlaying) {
+        this.name = name;
+        this.isPlaying = isPlaying;
+        this.lock = lock;
+    }
 
     public void play() {
         synchronized (lock) {
-            if (!isPlaying) {
-                isPlaying = true;
-                System.out.println(Thread.currentThread().getName() +  " Запустили воспроизведение музыки");
+            if (!isPlaying.get()) {
+                isPlaying.set(true);
+                System.out.println(Thread.currentThread().getName() + name + " Запустили воспроизведение музыки");
             } else {
-                System.out.println(Thread.currentThread().getName() + " Музыка уже воспроизводится");
+                System.out.println(Thread.currentThread().getName() + name + " Музыка уже воспроизводится");
             }
         }
     }
 
     public void pause() {
         synchronized (lock) {
-            if (isPlaying) {
-                isPlaying = false;
-                System.out.println(Thread.currentThread().getName() + " Остановили воспроизведение музыки");
+            if (isPlaying.get()) {
+                isPlaying.set(false);
+                System.out.println(Thread.currentThread().getName() + name + " Остановили воспроизведение музыки");
             } else {
-                System.out.println(Thread.currentThread().getName() + " Музыка на паузе");
+                System.out.println(Thread.currentThread().getName() + name + " Музыка на паузе");
             }
         }
     }
 
     public void skip() {
         synchronized (lock) {
-            System.out.println(Thread.currentThread().getName() + " Пропустили трек");
+            System.out.println(Thread.currentThread().getName() + name + " Пропустили трек");
         }
     }
 
     public void previous() {
         synchronized (lock) {
-            System.out.println(Thread.currentThread().getName() + " Вернулись к предыдущему треку");
+            System.out.println(Thread.currentThread().getName() + name + " Вернулись к предыдущему треку");
         }
     }
 }
