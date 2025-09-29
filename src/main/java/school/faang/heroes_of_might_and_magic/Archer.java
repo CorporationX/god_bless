4ec219d3500646bf8d1dd445dc1b3de5a1c3751c
Vitaliy Mikhaylov <vitaly.mikhaylov94@yandex.ru>
2025-09-30 03:01:41 +0500

    Task is BJS2-91364 is completed.

---
+++ src/main/java/school/faang/heroes_of_might_and_magic/Archer.java
@@ -0,0 +1,13 @@
+package school.faang.heroes_of_might_and_magic;
+
+/**
+ * Проект: god_bless
+ * Класс Archer
+ * Автор: Vital
+ */
+
+public class Archer extends Unit {
+    public Archer(int power) {
+        super(power);
+    }
+}
---
+++ src/main/java/school/faang/heroes_of_might_and_magic/Swordsman.java
@@ -0,0 +1,13 @@
+package school.faang.heroes_of_might_and_magic;
+
+/**
+ * Проект: god_bless
+ * Класс Swordsman
+ * Автор: Vital
+ */
+
+public class Swordsman extends Unit {
+    public Swordsman(int power) {
+        super(power);
+    }
+}
