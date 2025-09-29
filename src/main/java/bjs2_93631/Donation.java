00293921b138be52b2e14c0c7ab35b32840457d5
Abzal Samekeyev <samekeyev@gmail.com>
2025-09-29 14:22:45 +0500

    Задача Поддержим животных на проверку

---
+++ src/main/java/bjs2_93631/Donation.java
@@ -0,0 +1,11 @@
+package bjs2_93631;
+
+import lombok.Getter;
+import lombok.AllArgsConstructor;
+
+@AllArgsConstructor
+@Getter
+class Donation {
+    private final int id;
+    private final double amount;
+}
