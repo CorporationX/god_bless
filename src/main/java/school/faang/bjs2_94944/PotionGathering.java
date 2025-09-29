c185409a5289a98b4544a46b67f217949c5a48f6
Kuanysh <skn_88@mail.ru>
2025-09-29 12:47:14 +0500

    BJS2-94944 Fix PR comment: divide a logic between two methods

--- src/main/java/school/faang/bjs2_94944/PotionGathering.java
+++ src/main/java/school/faang/bjs2_94944/PotionGathering.java
@@ -4,45 +4,38 @@ import java.util.List;
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
-import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicInteger;
 
 public class PotionGathering {
     private static final int THREAD_COUNT = 5;
+    private static final int SLEEP_TIME = 1000;
+    private final ExecutorService executorService = Executors.newFixedThreadPool(THREAD_COUNT);
 
-    public int gatherIngredients(Potion potion) {
-        try {
-            System.out.printf("Gathering ingredients for potion %s\n", potion.name());
-            Thread.sleep(1000);
-        } catch (InterruptedException e) {
-            System.err.printf("Gathering ingredients was interrupted for potion %s\n", potion.name());
-            Thread.currentThread().interrupt();
-            throw new RuntimeException(e);
-        }
-        System.out.printf("Gathered %d ingredients for potion %s\n", potion.requiredIngredients(), potion.name());
-        return potion.requiredIngredients();
+    public CompletableFuture<Integer> gatherIngredients(Potion potion, ExecutorService executorService) {
+        return CompletableFuture.supplyAsync(() -> {
+            try {
+                System.out.printf("Gathering ingredients for potion %s\n", potion.name());
+                Thread.sleep(SLEEP_TIME);
+                System.out.printf("Gathered %d ingredients for potion %s\n",
+                        potion.requiredIngredients(),
+                        potion.name());
+                return potion.requiredIngredients();
+            } catch (InterruptedException e) {
+                Thread.currentThread().interrupt();
+                throw new RuntimeException("Gathering ingredients was interrupted for potion " + potion.name(), e);
+            }
+        }, executorService);
     }
 
     public void gatherAllIngredients(List<Potion> potions) {
-        ExecutorService executorService = Executors.newFixedThreadPool(THREAD_COUNT);
-        List<CompletableFuture<Integer>> futures = potions.stream()
-                .map(potion ->
-                        CompletableFuture.supplyAsync(() -> gatherIngredients(potion), executorService)
-                                .orTimeout(2L, TimeUnit.SECONDS)
-                                .exceptionally(ex -> {
-                                    System.out.printf("Error while gathering ingredients for potion %s, %s\n",
-                                            potion.name(),
-                                            ex.toString());
-                                    return 0;
-                                })
-                ).toList();
-
+        AtomicInteger totalIngredients = new AtomicInteger(0);
+        List<CompletableFuture<Void>> futures = potions.stream()
+                .map(potion -> gatherIngredients(potion, executorService)
+                        .thenAccept(totalIngredients::addAndGet))
+                .toList();
         CompletableFuture<Void> allFinished = CompletableFuture.allOf(futures.toArray(new CompletableFuture[0]));
-        CompletableFuture<Integer> totalIngredientsFuture = allFinished.thenApply(future -> futures.stream()
-                .map(CompletableFuture::join)
-                .reduce(0, Integer::sum));
-
-        int totalIngredients = totalIngredientsFuture.join();
-        System.out.printf("Gathered ingredients count: %d\n", totalIngredients);
+        allFinished.join();
+        System.out.printf("Gathered ingredients count: %d\n", totalIngredients.get());
         executorService.shutdown();
     }
 }
