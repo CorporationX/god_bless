3394eb91fc835cd3ce430ae80a34049addb37c6a
Monkeylust <mo9keylust@mail.ru>
2025-09-30 20:51:58 +0300

    BJS2-93612 // In progress

--- src/main/java/school/faang/bjs2_93612/PostService.java
+++ src/main/java/school/faang/bjs2_93612/PostService.java
@@ -2,11 +2,33 @@ package school.faang.bjs2_93612;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 import java.util.concurrent.locks.ReentrantLock;
 
 public class PostService {
+//    private final List<Post> posts = new ArrayList<>();
+//    private final ReentrantLock lock = new ReentrantLock();
+//
+//    public void addPost(Post post) {
+//        lock.lock();
+//        try {
+//            posts.add(post);
+//        } finally {
+//            lock.unlock();
+//        }
+//    }
+//
+//    public void addComment(int postId, Comment comment) {
+//        lock.lock();
+//        try {
+//
+//        } finally {
+//            lock.unlock();
+//        }
+//    }
+
+
     private final List<Post> posts = new ArrayList<>();
-    private final List<Comment> comments = new ArrayList<>();
     private final ReentrantLock lock = new ReentrantLock();
 
     public void addPost(Post post) {
@@ -21,9 +43,17 @@ public class PostService {
     public void addComment(int postId, Comment comment) {
         lock.lock();
         try {
-            comments.add(comment);
+            Post post = findById(postId).orElseThrow(() -> new RuntimeException("Post is not found"));
+
         } finally {
             lock.unlock();
         }
     }
+
+    public Optional<Post> findById(int postID) {
+        return posts.stream()
+                .filter(post -> post.getId() == postID)
+                .findFirst();
+    }
+
 }
