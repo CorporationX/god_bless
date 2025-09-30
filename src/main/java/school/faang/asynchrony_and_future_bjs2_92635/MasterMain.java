ab5742643377796337e0d94a66c35d9b45f948d6
leto007 <liga07@bk.ru>
2025-09-30 18:16:50 +0300

    Решил задачу Asynchrony and Future BJS2-92635

---
+++ src/main/java/school/faang/asynchrony_and_future_bjs2_92635/MasterMain.java
@@ -0,0 +1,9 @@
+package school.faang.asynchrony_and_future_bjs2_92635;
+
+public class MasterMain {
+    public static void main(String[] args) {
+        MasterCardService masterCardService = new MasterCardService();
+
+        masterCardService.doAll();
+    }
+}
