cf6aa200f15afa069f766e2c9aaada51d476db94
Roman Chipura <connor.ueyn@gmail.com>
2025-10-01 03:05:54 +0500

    теперь реализовано через lock

--- src/main/java/school/faang/bjs2_90411/Player.java
+++ src/main/java/school/faang/bjs2_90411/Player.java
@@ -3,26 +3,44 @@ package school.faang.bjs2_90411;
 public class Player {
     private boolean isPlaying = false;
     private int currentTrack = 0;
-    private final String [] tracks = {"Artist1 = Song1", "Artist2 = Song2", "Artist3 = Song3"};
+    private final String[] tracks = {"Artist1 = Song1", "Artist2 = Song2", "Artist3 = Song3"};
 
-    public synchronized void play() {
-        System.out.println("Музыка играет...");
-        isPlaying = true;
+    public final Object lock = new Object();
+
+    public void play() {
+        synchronized (lock) {
+            if (!isPlaying) {
+                isPlaying = true;
+                System.out.println("Музыка воспроизводится.");
+            } else {
+                System.out.println("Музыка уже играет.");
+            }
+        }
     }
 
-    public synchronized void pause() {
-        System.out.println("Музыка поставлена на паузу");
-        isPlaying = false;
+    public void pause() {
+        synchronized (lock) {
+            if (isPlaying) {
+                isPlaying = false;
+                System.out.println("Музыка поставлена на паузу.");
+            } else {
+                System.out.println("Музыка уже на паузе.");
+            }
+        }
     }
 
-    public synchronized void skip() {
-        currentTrack = ((currentTrack + 1) + tracks.length) % tracks.length;
-        System.out.println("Следующая песня: " + tracks[currentTrack]);
+    public void skip() {
+        synchronized (lock) {
+            currentTrack = ((currentTrack + 1) + tracks.length) % tracks.length;
+            System.out.println("Следующая песня: " + tracks[currentTrack]);
+        }
     }
 
-    public synchronized void previous() {
-        currentTrack = (currentTrack - 1 + tracks.length) % tracks.length;
-        System.out.println("Следующая песня: " + tracks[currentTrack]);
+    public void previous() {
+        synchronized (lock) {
+            currentTrack = (currentTrack - 1 + tracks.length) % tracks.length;
+            System.out.println("Следующая песня: " + tracks[currentTrack]);
+        }
     }
 
 
