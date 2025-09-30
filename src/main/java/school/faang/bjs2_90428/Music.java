263d0e8a6f7593f949d895d188dbcd7efcef6cac
K0I2T <hiro94337@gmail.com>
2025-09-30 14:54:09 +0300

    BJS2-90487 переоформление Thread-ов

--- src/main/java/school/faang/bjs2_90428/Music.java
+++ src/main/java/school/faang/bjs2_90428/Music.java
@@ -4,16 +4,15 @@ public class Music {
     public static void main(String[] args) {
         Player player = new Player();
 
-        Thread playThread = new Thread(player::play);
-        playThread.start();
+        Thread[] threads = new Thread[] {
+                new Thread(player::play),
+                new Thread(player::pause),
+                new Thread(player::skip),
+                new Thread(player::previous)
+        };
 
-        Thread pauseThread = new Thread(player::pause);
-        pauseThread.start();
-
-        Thread skipThread = new Thread(player::skip);
-        skipThread.start();
-
-        Thread previousThread = new Thread(player::previous);
-        previousThread.start();
+        for (Thread thread : threads) {
+            thread.start();
+        }
     }
 }
