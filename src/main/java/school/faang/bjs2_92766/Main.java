f86303db379f8923b9ea9f81357a8323af9014e7
ayazs <ayaz-22@yandex.ru>
2025-10-01 16:50:33 +0500

    BJS2-92766 is fixed/2

--- src/main/java/school/faang/bjs2_92766/Main.java
+++ src/main/java/school/faang/bjs2_92766/Main.java
@@ -1,8 +1,5 @@
 package school.faang.bjs2_92766;
 
-import java.util.concurrent.ExecutionException;
-import java.util.concurrent.Future;
-
 public class Main {
 
     public static void main(String[] args) {
