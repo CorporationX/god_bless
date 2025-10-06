ee42e8cda7adcc76e434f9979216e7fd64de5d91
Vitaliy Mikhaylov <vitaly.mikhaylov94@yandex.ru>
2025-10-07 04:35:00 +0500

    Refactored the code.

--- src/main/java/school/faang/wow/Main.java
+++ src/main/java/school/faang/wow/Main.java
@@ -12,59 +12,59 @@ import java.util.stream.IntStream;
 
 public class Main {
 
-        //Игроки
-        private static final String PLAYER1_NAME = "Тралл";
-        private static final int PLAYER1_LEVEL = 10;
-        private static final int PLAYER1_XP = 250;
+    //Игроки
+    private static final String PLAYER1_NAME = "Тралл";
+    private static final int PLAYER1_LEVEL = 10;
+    private static final int PLAYER1_XP = 250;
 
-        private static final String PLAYER2_NAME = "Сильвана";
-        private static final int PLAYER2_LEVEL = 12;
-        private static final int PLAYER2_XP = 450;
+    private static final String PLAYER2_NAME = "Сильвана";
+    private static final int PLAYER2_LEVEL = 12;
+    private static final int PLAYER2_XP = 450;
 
-        private static final String PLAYER3_NAME = "Джайна";
-        private static final int PLAYER3_LEVEL = 15;
-        private static final int PLAYER3_XP = 700;
+    private static final String PLAYER3_NAME = "Джайна";
+    private static final int PLAYER3_LEVEL = 15;
+    private static final int PLAYER3_XP = 700;
 
-        //Квесты
-        private static final String QUEST1_NAME = "Победить Короля-лича";
-        private static final int QUEST1_DIFFICULTY = 10;
-        private static final int QUEST1_REWARD = 150;
+    //Квесты
+    private static final String QUEST1_NAME = "Победить Короля-лича";
+    private static final int QUEST1_DIFFICULTY = 10;
+    private static final int QUEST1_REWARD = 150;
 
-        private static final String QUEST2_NAME = "Найти Меч Азерота";
-        private static final int QUEST2_DIFFICULTY = 8;
-        private static final int QUEST2_REWARD = 100;
+    private static final String QUEST2_NAME = "Найти Меч Азерота";
+    private static final int QUEST2_DIFFICULTY = 8;
+    private static final int QUEST2_REWARD = 100;
 
-        private static final String QUEST3_NAME = "Защитить деревню";
-        private static final int QUEST3_DIFFICULTY = 5;
-        private static final int QUEST3_REWARD = 50;
+    private static final String QUEST3_NAME = "Защитить деревню";
+    private static final int QUEST3_DIFFICULTY = 5;
+    private static final int QUEST3_REWARD = 50;
 
-        public static void main(String[] args) {
-            QuestSystem questSystem = new QuestSystem();
+    public static void main(String[] args) {
+        QuestSystem questSystem = new QuestSystem();
 
-            List<Player> players = List.of(
-                    new Player(PLAYER1_NAME, PLAYER1_LEVEL, PLAYER1_XP),
-                    new Player(PLAYER2_NAME, PLAYER2_LEVEL, PLAYER2_XP),
-                    new Player(PLAYER3_NAME, PLAYER3_LEVEL, PLAYER3_XP)
-            );
+        List<Player> players = List.of(
+                new Player(PLAYER1_NAME, PLAYER1_LEVEL, PLAYER1_XP),
+                new Player(PLAYER2_NAME, PLAYER2_LEVEL, PLAYER2_XP),
+                new Player(PLAYER3_NAME, PLAYER3_LEVEL, PLAYER3_XP)
+        );
 
-            List<Quest> quests = List.of(
-                    new Quest(QUEST1_NAME, QUEST1_DIFFICULTY, QUEST1_REWARD),
-                    new Quest(QUEST2_NAME, QUEST2_DIFFICULTY, QUEST2_REWARD),
-                    new Quest(QUEST3_NAME, QUEST3_DIFFICULTY, QUEST3_REWARD)
-            );
+        List<Quest> quests = List.of(
+                new Quest(QUEST1_NAME, QUEST1_DIFFICULTY, QUEST1_REWARD),
+                new Quest(QUEST2_NAME, QUEST2_DIFFICULTY, QUEST2_REWARD),
+                new Quest(QUEST3_NAME, QUEST3_DIFFICULTY, QUEST3_REWARD)
+        );
 
-            List<CompletableFuture<Player>> futures = startAllQuests(questSystem, players, quests);
+        List<CompletableFuture<Player>> futures = startAllQuests(questSystem, players, quests);
 
-            futures.forEach(future -> future.thenAccept(player ->
-                    System.out.println("Игрок " + player.getName() +
-                            " завершил квест и теперь имеет " +
-                            player.getExperience() + " очков опыта и уровень " + player.getLevel())
-            ));
+        futures.forEach(future -> future.thenAccept(player ->
+                System.out.println("Игрок " + player.getName() +
+                        " завершил квест и теперь имеет " +
+                        player.getExperience() + " очков опыта и уровень " + player.getLevel())
+        ));
 
-            CompletableFuture<Void> allQuests = CompletableFuture.allOf(futures.toArray(new CompletableFuture[0]));
-            allQuests.thenRun(() -> System.out.println("Все квесты успешно завершены!"));
-            allQuests.join();
-        }
+        CompletableFuture<Void> allQuests = CompletableFuture.allOf(futures.toArray(new CompletableFuture[0]));
+        allQuests.thenRun(() -> System.out.println("Все квесты успешно завершены!"));
+        allQuests.join();
+    }
 
     private static List<CompletableFuture<Player>> startAllQuests(QuestSystem questSystem,
                                                                   List<Player> players,
