c18cd51b673452b7d3c6af2dfbf627a14cbb2c00
svxrslf <svxrslf@yandex.ru>
2025-10-06 16:04:37 +0300

    У

--- src/main/java/school/faang/bjs2_92960/PotionServices.java
+++ src/main/java/school/faang/bjs2_92960/PotionServices.java
@@ -1,5 +1,35 @@
 package school.faang.bjs2_92960;
 
+import lombok.extern.slf4j.Slf4j;
+
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.atomic.AtomicInteger;
+
+@Slf4j
 public class PotionServices {
     private static final long BASIC_DURATION = 2_000;
+    private AtomicInteger atomInt = new AtomicInteger();
+
+
+    public CompletableFuture<Void> gatherIngredient(Potion potion) {
+        return CompletableFuture.
+                supplyAsync(() -> {
+                    log.info("Starting to gather ingredients for {}", potion.getName());
+                    gatherImitation(BASIC_DURATION * potion.getRequiredCount());
+                    return potion.getRequiredCount();
+                })
+                .thenAccept(this::increment);
+    }
+
+    private void gatherImitation(long time) {
+        try {
+            Thread.sleep(time);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private void increment(int delta) {
+        atomInt.addAndGet(delta);
+    }
 }
