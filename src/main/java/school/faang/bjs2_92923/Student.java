0a6c67e30ec78b1a2b1af4f38276726eca12bcbb
svxrslf <svxrslf@yandex.ru>
2025-10-07 12:09:18 +0300

    Чекстайл из вообще друго пра жалуется на этот класс

--- src/main/java/school/faang/bjs2_92923/Student.java
+++ src/main/java/school/faang/bjs2_92923/Student.java
@@ -6,13 +6,12 @@ import java.util.concurrent.atomic.AtomicInteger;
 
 @Getter
 @AllArgsConstructor
-@SuppressWarnings("checkstyle:Indentation")
 public class Student {
     private final String name;
     private final int year;
     private final AtomicInteger points = new AtomicInteger();
 
-    public void addPoints(int addedPoints) {
+    public void addPoints( int addedPoints) {
         points.addAndGet(addedPoints);
     }
 
